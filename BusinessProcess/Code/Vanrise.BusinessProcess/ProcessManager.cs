f27c74110c3e3fb42df67e7b2e7a991a77047fc3
Ali Ballouk <VANRISE\ali.ballouk>
2015-01-19 08:32:47 +0000

    added operation
    git-tfs-id: [http://developmentvlan:8585/tfs/vanrise.collection]$/;C9602

--- BusinessProcess/Code/Vanrise.BusinessProcess/ProcessManager.cs
+++ BusinessProcess/Code/Vanrise.BusinessProcess/ProcessManager.cs
@@ -2,10 +2,17 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using Vanrise.BusinessProcess.Data;
+using Vanrise.BusinessProcess.Entities;
 
 namespace Vanrise.BusinessProcess
 {
     public class ProcessManager
     {
+        public List<BPDefinition> GetDefinitions()
+        {
+            IBPDataManager dataManager = BPDataManagerFactory.GetDataManager<IBPDataManager>();
+            return dataManager.GetDefinitions();
+        }
     }
 }
