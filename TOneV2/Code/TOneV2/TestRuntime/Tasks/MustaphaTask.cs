5f54d66b85c7016cc46218228f7278105c682934
mustafa.hawi <mustafa.hawi@vanrise.tfs.local>
2015-01-22 15:28:50 +0000

    Add Task Class
    git-tfs-id: [http://developmentvlan:8585/tfs/vanrise.collection]$/;C20192

---
+++ TOneV2/Code/TOneV2/TestRuntime/Tasks/MustaphaTask.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TestRuntime.Tasks
+{
+    public class MustaphaTask : ITask
+    {
+        public void Execute()
+        {
+            while (true)
+            {
+                Console.WriteLine("hello");
+            }
+        }
+    }
+}
