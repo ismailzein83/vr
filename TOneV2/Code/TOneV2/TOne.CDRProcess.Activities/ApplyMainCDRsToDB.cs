1dd0d2bd73db385a2521c5dfaa609ec74c188fa5
mustafa.hawi <mustafa.hawi@vanrise.tfs.local>
2015-01-29 09:59:58 +0000

    Add ApplyMainCDRsToDB Activity
    git-tfs-id: [http://developmentvlan:8585/tfs/vanrise.collection]$/;C20326

---
+++ TOneV2/Code/TOneV2/TOne.CDRProcess.Activities/ApplyMainCDRsToDB.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Activities;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TOne.CDR.Data;
+using TOne.Entities;
+using Vanrise.BusinessProcess;
+
+namespace TOne.CDRProcess.Activities
+{
+    #region Argument Classes
+
+    public class ApplyMainCDRsToDBInput
+    {
+        public TOneQueue<Object> InputQueue { get; set; }
+    }
+
+    #endregion
+
+    public sealed class ApplyMainCDRsToDB : DependentAsyncActivity<ApplyMainCDRsToDBInput>
+    {
+        [RequiredArgument]
+        public InArgument<TOneQueue<Object>> InputQueue { get; set; }
+
+        protected override void DoWork(ApplyMainCDRsToDBInput inputArgument, AsyncActivityStatus previousActivityStatus, AsyncActivityHandle handle)
+        {
+            ICDRDataManager dataManager = CDRDataManagerFactory.GetDataManager<ICDRDataManager>();
+            DoWhilePreviousRunning(previousActivityStatus, handle, () =>
+            {
+                bool hasItem = false;
+                do
+                {
+                    hasItem = inputArgument.InputQueue.TryDequeue(
+                        (preparedMainCDRs) =>
+                        {
+                            dataManager.ApplyMainCDRsToDB(preparedMainCDRs);
+                        });
+                }
+                while (!ShouldStop(handle) && hasItem);
+            });
+        }
+
+        protected override ApplyMainCDRsToDBInput GetInputArgument2(AsyncCodeActivityContext context)
+        {
+            return new ApplyMainCDRsToDBInput
+            {
+                InputQueue = this.InputQueue.Get(context)
+            };
+        }
+    }
+}
