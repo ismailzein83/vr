7c0e73902f7b053d3823227b6cdd4e9ea3e08f17
ali.baghdadi <ali.baghdadi@vanrise.tfs.local>
2015-01-29 09:35:54 +0000

    Add A Task
    git-tfs-id: [http://developmentvlan:8585/tfs/vanrise.collection]$/;C20324

---
+++ TOneV2/Code/TOneV2/TestRuntime/Tasks/ABaghdadiTask.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Timers;
+using Vanrise.BusinessProcess;
+using Vanrise.BusinessProcess.Entities;
+
+namespace TestRuntime.Tasks
+{
+    public class ABaghdadiTask : ITask
+    {
+
+        public void Execute()
+        {
+            Console.WriteLine("Host Started");
+            BusinessProcessRuntime.Current.TerminatePendingProcesses();
+            Timer timer = new Timer(1000);
+            timer.Elapsed += new ElapsedEventHandler(timer_Elapsed);
+            timer.Start();
+
+            ProcessManager processManager = new ProcessManager();
+            processManager.CreateNewProcess(new CreateProcessInput
+            {
+                ProcessName = "UpdateZoneRateProcess"
+                //InputArguments = new TOne.LCRProcess.Arguments.UpdateCodeZoneMatchProcessInput
+                //{
+                //    IsFuture = false,
+                //    CodeEffectiveOn = DateTime.Today,
+                //    GetChangedCodeGroupsOnly = false
+                //}
+            });
+
+
+        }
+
+        static bool _isRunning;
+        static object _lockObj = new object();
+        static void timer_Elapsed(object sender, ElapsedEventArgs e)
+        {
+            lock (_lockObj)
+            {
+                if (_isRunning)
+                    return;
+                _isRunning = true;
+            }
+            try
+            {
+                //BusinessProcessRuntime.Current.LoadAndExecutePendings();
+
+                BusinessProcessRuntime.Current.ExecutePendings();
+                BusinessProcessRuntime.Current.TriggerPendingEvents();
+            }
+            finally
+            {
+                lock (_lockObj)
+                {
+                    _isRunning = false;
+                }
+            }
+        }
+    }
+}
