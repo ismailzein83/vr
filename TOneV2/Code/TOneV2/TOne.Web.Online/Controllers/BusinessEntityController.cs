21cca77c17ecb270393f6762c5e25d74a43ba239
rayan.kazma <rayan.kazma@vanrise.tfs.local>
2015-01-30 08:30:53 +0000

    Addin BusinessEntityController to the project
    git-tfs-id: [http://developmentvlan:8585/tfs/vanrise.collection]$/;C20338

---
+++ TOneV2/Code/TOneV2/TOne.Web.Online/Controllers/BusinessEntityController.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using TOne.Entities;
+using TOne.Data;
+using TOne.Business;
+
+namespace TOne.Web.Online.Controllers
+{
+    public class BusinessEntityController : ApiController
+    {
+        // GET api/<controller>
+        [HttpGet]
+        public List<TOne.Entities.CarrierInfo> GetCarriers()
+        {
+            BusinessEntityManager businessmanager = new BusinessEntityManager();
+
+            return businessmanager.GetCarriers();
+        }
+    }
+}
