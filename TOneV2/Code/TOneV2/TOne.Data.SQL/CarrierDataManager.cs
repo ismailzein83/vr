dba755e88b659ae8c97e8f0c4038ce27e5624167
rayan.kazma <rayan.kazma@vanrise.tfs.local>
2015-01-30 08:31:47 +0000

    Adding CarrierDataManager
    git-tfs-id: [http://developmentvlan:8585/tfs/vanrise.collection]$/;C20340

---
+++ TOneV2/Code/TOneV2/TOne.Data.SQL/CarrierDataManager.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace TOne.Data.SQL
+{
+    public class CarrierDataManager : BaseTOneDataManager, ICarrierDataManager
+    {
+        public List<Entities.CarrierInfo> GetCarriers()
+        {
+
+            return GetItemsSP("Main.sp_Carriers_GetCarriers", (reader) =>
+                {
+                    return new Entities.CarrierInfo
+                    {
+                        CarrierAccountID = reader["CarrierAccountID"] as string,
+                        Name = reader["Name"] as string
+                    };
+                });
+
+            //   throw new NotImplementedException();
+        }
+    }
+}
