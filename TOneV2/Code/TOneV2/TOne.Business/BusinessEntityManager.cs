966bc3dff021bfefa81c32bf69673c17ec39f6d2
rayan.kazma <rayan.kazma@vanrise.tfs.local>
2015-01-30 08:34:13 +0000

    Adding BusinessEntityManager
    git-tfs-id: [http://developmentvlan:8585/tfs/vanrise.collection]$/;C20342

---
+++ TOneV2/Code/TOneV2/TOne.Business/BusinessEntityManager.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TOne.Data;
+
+namespace TOne.Business
+{
+    public class BusinessEntityManager
+    {
+        public List<TOne.Entities.CarrierInfo> GetCarriers()
+        {
+            ICarrierDataManager datamanager = DataManagerFactory.GetDataManager<ICarrierDataManager>();
+
+            return datamanager.GetCarriers();
+        }
+    }
+}
