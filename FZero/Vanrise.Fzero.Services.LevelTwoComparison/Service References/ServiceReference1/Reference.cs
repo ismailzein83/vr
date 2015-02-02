080a8d44643e169aa834c3baf1f1752edb985d16
walid.mogharbel <walid.mogharbel@vanrise.tfs.local>
2015-02-02 08:51:53 +0000

    git-tfs-id: [http://developmentvlan:8585/tfs/vanrise.collection]$/;C20385

--- FZero/Vanrise.Fzero.Services.LevelTwoComparison/Service References/ServiceReference1/Reference.cs
+++ FZero/Vanrise.Fzero.Services.LevelTwoComparison/Service References/ServiceReference1/Reference.cs
@@ -15,7 +15,7 @@ namespace Vanrise.Fzero.Services.LevelTwoComparison.ServiceReference1 {
     
     [System.Diagnostics.DebuggerStepThroughAttribute()]
     [System.CodeDom.Compiler.GeneratedCodeAttribute("System.Runtime.Serialization", "4.0.0.0")]
-    [System.Runtime.Serialization.DataContractAttribute(Name="RecievedEmail", Namespace="http://schemas.datacontract.org/2004/07/FMS.DataLayer")]
+    [System.Runtime.Serialization.DataContractAttribute(Name="RecievedEmail", Namespace="http://schemas.datacontract.org/2004/07/Vanrise.Fzero.Bypass")]
     [System.SerializableAttribute()]
     public partial class RecievedEmail : object, System.Runtime.Serialization.IExtensibleDataObject, System.ComponentModel.INotifyPropertyChanged {
         
