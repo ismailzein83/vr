110e4b5f0840d0d0d28d3a85806cbfa30ef1d899
Ismail Zein <VANRISE\ismail.zein>
2015-02-04 16:36:21 +0000

    added Compressor and changed framework to 4.5
    git-tfs-id: [http://developmentvlan:8585/tfs/vanrise.collection]$/;C20447

--- Vanrise/Code/Vanrise.Common/Compressor.cs
+++ Vanrise/Code/Vanrise.Common/Compressor.cs
@@ -1,5 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
+using System.IO.Compression;
 using System.Linq;
 using System.Text;
 
@@ -9,7 +11,42 @@ namespace Vanrise.Common
     {
         public static byte[] Compress(byte[] data)
         {
-            return null;
+            byte[] compressedData = null;
+            using (MemoryStream memory = new MemoryStream())
+            {
+                using (GZipStream gzip = new GZipStream(memory,  CompressionLevel.Optimal))
+                {
+                    gzip.Write(data, 0, data.Length);
+                }
+                compressedData = memory.ToArray();
+            }
+            return compressedData;
+        }
+
+        public static byte[] Decompress(byte[] compressedData)
+        {
+            byte[] data = null;
+            const int size = 4096;
+            using (GZipStream stream = new GZipStream(new MemoryStream(compressedData), CompressionMode.Decompress))
+            {
+
+                byte[] buffer = new byte[size];
+                using (MemoryStream memory = new MemoryStream())
+                {
+                    int count = 0;
+                    do
+                    {
+                        count = stream.Read(buffer, 0, size);
+                        if (count > 0)
+                        {
+                            memory.Write(buffer, 0, count);
+                        }
+                    }
+                    while (count > 0);
+                    data = memory.ToArray();
+                }
+            }
+            return data;
         }
     }
 }
