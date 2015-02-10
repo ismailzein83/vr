dc7a87b1b27ca47cf6010715cebc73b27b2c826a
Ismail Zein <VANRISE\ismail.zein>
2015-02-10 09:27:56 +0000

    Added Utitlities class
    git-tfs-id: [http://developmentvlan:8585/tfs/vanrise.collection]$/;C20530

---
+++ Vanrise/Code/Vanrise.Common/Utilities.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Vanrise.Common
+{
+    public static class Utilities
+    {
+        public static Dictionary<T,Q> GetEnumAttributes<T,Q>() where T : struct
+            where Q : Attribute
+        {
+            Type enumType = typeof(T);
+            if (!enumType.IsEnum)
+                throw new Exception(String.Format("{0} is not an Enum type", enumType));
+
+            Type attributeType = typeof(Q);
+            Dictionary<T, Q> enumAttributes = new Dictionary<T, Q>();
+            foreach (var member in enumType.GetFields())
+            {
+                Q mbrAttribute = member.GetCustomAttributes(attributeType, true).FirstOrDefault() as Q;
+                if (mbrAttribute != null)
+                    enumAttributes.Add((T)Enum.Parse(enumType, member.Name), mbrAttribute);
+            }
+            return enumAttributes;
+        }
+
+        public static Q GetEnumAttribute<T, Q>(T enumItem)
+            where T : struct
+            where Q : Attribute
+        {
+            Type enumType = typeof(T);
+            if (!enumType.IsEnum)
+                throw new Exception(String.Format("{0} is not an Enum type", enumType));
+
+            Type attributeType = typeof(Q);
+            Dictionary<T, Q> enumAttributes = new Dictionary<T, Q>();
+            foreach (var member in enumType.GetFields())
+            {
+                if (enumItem.Equals((T)Enum.Parse(enumType, member.Name)))
+                    return member.GetCustomAttributes(attributeType, true).FirstOrDefault() as Q;
+            }
+            return default(Q);
+        }
+    }
+}
