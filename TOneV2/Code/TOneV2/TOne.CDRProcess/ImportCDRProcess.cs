f5936b35695e70a9f87f5b628b68f776ec496058
mustafa.hawi <mustafa.hawi@vanrise.tfs.local>
2015-02-12 11:04:49 +0000

    Import CDR Process
    git-tfs-id: [http://developmentvlan:8585/tfs/vanrise.collection]$/;C20609

---
+++ TOneV2/Code/TOneV2/TOne.CDRProcess/ImportCDRProcess.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Activities;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TOne.CDRProcess.Arguments;
+using Vanrise.BusinessProcess;
+using Vanrise.BusinessProcess.Entities;
+
+namespace TOne.CDRProcess
+{
+    public partial class ImportCDRProcess : Activity, IBPWorkflow
+    {
+
+        #region IBPWorkflow Members
+
+        public string GetTitle(CreateProcessInput createProcessInput)
+        {
+            ImportCDRProcessInput inputArg = createProcessInput.InputArguments as ImportCDRProcessInput;
+            if (inputArg == null)
+                throw new ArgumentNullException("ImportCDRProcessInput");
+            return String.Format("Import CDR Process for Switch {0}", inputArg.SwitchID);
+        }
+
+        #endregion
+
+    }
+}
