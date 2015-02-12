de06531fda810277c25610d4ea8ee65c60133091
ali.baghdadi <ali.baghdadi@vanrise.tfs.local>
2015-02-12 14:28:39 +0000

    Add Analytics Module
    git-tfs-id: [http://developmentvlan:8585/tfs/vanrise.collection]$/;C20626

---
+++ TOneV2/Code/TOneV2/TOne.Analytics.Entities/TopNDestinationView.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TOne.Analytics.Entities
+{
+    public class TopNDestinationView
+    {
+        public int OurZoneID { get; set; }
+        public string ZoneName { get; set; }
+        public string SupplierID { get; set; }
+        public int Attempts { get; set; }
+        public decimal DurationInMinutes { get; set; }
+        public decimal ASR { get; set; }
+        public int SuccessfulAttempts { get; set; }
+        public decimal ACD { get; set; }
+        public decimal DeliveredASR { get; set; }
+        public decimal AveragePDD { get; set; }
+        public string CodeGroup { get; set; }
+
+    }
+}
