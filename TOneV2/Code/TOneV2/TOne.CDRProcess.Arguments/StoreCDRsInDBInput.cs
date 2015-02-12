eccc617c2f68c3936372944480de653ea5850bee
mustafa.hawi <mustafa.hawi@vanrise.tfs.local>
2015-02-12 12:59:23 +0000

    Store CDRs In DB
    git-tfs-id: [http://developmentvlan:8585/tfs/vanrise.collection]$/;C20615

---
+++ TOneV2/Code/TOneV2/TOne.CDRProcess.Arguments/StoreCDRsInDBInput.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TOne.CDRProcess.Arguments
+{
+    public class StoreCDRsInDBInput
+    {
+        public int SwitchID { get; set; }
+
+    }
+}
