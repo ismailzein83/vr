d9a083e54a2a4fa95af5c022415cd19f74996a44
ali.baghdadi <ali.baghdadi@vanrise.tfs.local>
2015-02-13 13:15:19 +0000

    update webapi
    git-tfs-id: [http://developmentvlan:8585/tfs/vanrise.collection]$/;C20667

--- TOneV2/Code/TOneV2/TOne.Web.Online/Controllers/AnalyticsController.cs
+++ TOneV2/Code/TOneV2/TOne.Web.Online/Controllers/AnalyticsController.cs
@@ -17,18 +17,24 @@ namespace TOne.Web.Online.Controllers
         }
 
         [HttpGet]
-        public List<TOne.Analytics.Entities.TopNDestinationView> GetTopNDestinations(int topCount, DateTime fromDate, DateTime toDate, string sortOrder, string customerID, string supplierID, int? switchID, char groupByCodeGroup
-            , string codeGroup, char showSupplier, int from, int to)
+        public List<TOne.Analytics.Entities.TopNDestinationView> GetTopNDestinations(int topCount, DateTime fromDate, DateTime toDate, string sortOrder, char groupByCodeGroup
+            , char showSupplier, int from, int to, string customerID = null, string supplierID = null, int? switchID = null, string codeGroup = null)
         {
            
             return _analyticsManager.GetTopNDestinations(topCount, fromDate, toDate, sortOrder, customerID, supplierID, switchID, groupByCodeGroup, codeGroup, showSupplier, from, to);
         }
 
         [HttpGet]
-        public List<TOne.Analytics.Entities.AlertView> GetAlerts(int topCount, char showHiddenAlerts, int alertLevel, string tag, string source, int? userID)
+        public List<TOne.Analytics.Entities.AlertView> GetAlerts(int topCount, char showHiddenAlerts, int alertLevel, string tag = null, string source = null, int? userID = null)
         {
             
             return _analyticsManager.GetAlerts(topCount, showHiddenAlerts, alertLevel, tag, source, userID);
+        }
+
+        [HttpGet]
+        public List<TOne.Analytics.Entities.CarrierRateView> GetRates(string carrierType, DateTime effectiveOn, string carrierID, int from, int to,string codeGroup = null)
+        {
+            return _analyticsManager.GetRates(carrierType, effectiveOn, carrierID, codeGroup, from, to);
         }
     }
 }
