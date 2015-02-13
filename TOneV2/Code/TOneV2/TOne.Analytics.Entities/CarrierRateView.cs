c843ddbd06b4370263317be4fa5670939f9830b3
ali.baghdadi <ali.baghdadi@vanrise.tfs.local>
2015-02-13 13:15:06 +0000

    update webapi
    git-tfs-id: [http://developmentvlan:8585/tfs/vanrise.collection]$/;C20666

---
+++ TOneV2/Code/TOneV2/TOne.Analytics.Entities/CarrierRateView.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TOne.Analytics.Entities
+{
+    public class CarrierRateView
+    {
+        public int ZoneID { get; set; }
+        public string CodeGroup { get; set; }
+        public string ZoneName { get; set; }
+        public string Code { get; set; }
+        public int RateID { get; set; }
+        public int ServiceFlag { get; set; }
+        public decimal? Rate { get; set; }
+        public string CurrencyID { get; set; }
+        public decimal? OffPeakRate { get; set; }
+        public decimal? WeekendRate { get; set; }
+        public int ChangeID { get; set; }
+        public string Change { get; set; }
+        public DateTime? RateBeginEffectiveDate { get; set; }
+        public DateTime? RateEndEffectiveDate { get; set; }
+        public DateTime? CodeBeginEffectiveDate { get; set; }
+        public DateTime? CodeEndEffectiveDate { get; set; }
+        public int PricelistID { get; set; }
+        public DateTime? PricelistBeginEffectiveDate { get; set; }
+        public string UserName { get; set; }
+
+    }
+}
