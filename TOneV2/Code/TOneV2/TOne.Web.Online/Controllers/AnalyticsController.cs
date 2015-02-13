830eca6a5a65744b94c94c70179b45f31f59c9e8
ali.baghdadi <ali.baghdadi@vanrise.tfs.local>
2015-02-13 08:27:49 +0000

    Web API
    git-tfs-id: [http://developmentvlan:8585/tfs/vanrise.collection]$/;C20641

---
+++ TOneV2/Code/TOneV2/TOne.Web.Online/Controllers/AnalyticsController.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using TOne.Analytics.Business;
+
+namespace TOne.Web.Online.Controllers
+{
+    public class AnalyticsController : ApiController
+    {
+        private readonly AnalyticsManager _analyticsManager;
+        public AnalyticsController()
+        {
+            _analyticsManager = new AnalyticsManager() ;
+        }
+
+        [HttpGet]
+        public List<TOne.Analytics.Entities.TopNDestinationView> GetTopNDestinations(int topCount, DateTime fromDate, DateTime toDate, string sortOrder, string customerID, string supplierID, int? switchID, char groupByCodeGroup
+            , string codeGroup, char showSupplier, int from, int to)
+        {
+           
+            return _analyticsManager.GetTopNDestinations(topCount, fromDate, toDate, sortOrder, customerID, supplierID, switchID, groupByCodeGroup, codeGroup, showSupplier, from, to);
+        }
+
+        [HttpGet]
+        public List<TOne.Analytics.Entities.AlertView> GetAlerts(int topCount, char showHiddenAlerts, int alertLevel, string tag, string source, int? userID)
+        {
+            
+            return _analyticsManager.GetAlerts(topCount, showHiddenAlerts, alertLevel, tag, source, userID);
+        }
+    }
+}
