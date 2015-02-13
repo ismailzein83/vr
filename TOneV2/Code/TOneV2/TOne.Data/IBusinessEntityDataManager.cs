d7296a41e31fb215602394000050e8447fd067e9
ali.baghdadi <ali.baghdadi@vanrise.tfs.local>
2015-02-13 16:05:00 +0000

    Update Web API
    git-tfs-id: [http://developmentvlan:8585/tfs/vanrise.collection]$/;C20681

---
+++ TOneV2/Code/TOneV2/TOne.Data/IBusinessEntityDataManager.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using TOne.Entities;
+
+namespace TOne.Data
+{
+    public interface IBusinessEntityDataManager :IDataManager
+    {
+        List<CarrierInfo> GetCarriers(string carrierType);
+        List<CodeGroupInfo> GetCodeGroups();
+        List<SwitchInfo> GetSwitches();
+    }
+}
