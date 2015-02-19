8b3d097399ce5e9cd2148e4377a93ca8bd2ef981
hassan.ali <hassan.ali@vanrise.tfs.local>
2015-02-19 09:54:28 +0000

    Refactoring of process billing
    git-tfs-id: [http://developmentvlan:8585/tfs/vanrise.collection]$/;C20821

--- TOneV2/Code/TOneV2/TOne.CDR.Entities/BillingCDRCost.cs
+++ TOneV2/Code/TOneV2/TOne.CDR.Entities/BillingCDRCost.cs
@@ -8,6 +8,30 @@ namespace TOne.CDR.Entities
 {
     public class BillingCDRCost : BillingCDRPricingBase
     {
-
+        public virtual void Copy(BillingCDRPricingBase other)
+        {
+            this.BillingCDRMainID = other.BillingCDRMainID;
+            this.CommissionID = other.CommissionID;
+            this.CommissionValue = other.CommissionValue;
+            this.CurrencySymbol = other.CurrencySymbol;
+            this.Discount = other.Discount;
+            this.ExtraChargeID = other.ExtraChargeID;
+            this.ExtraChargeValue = other.ExtraChargeValue;
+            this.FirstPeriod = other.FirstPeriod;
+            this.FractionUnit = other.FractionUnit;
+            this.Net = other.Net;
+            this.RateID = other.RateID;
+            this.RateType = other.RateType;
+            this.RateValue = other.RateValue;
+            this.RepeatFirstperiod = other.RepeatFirstperiod;
+            this.TariffID = other.TariffID;
+            this.ToDConsiderationID = other.ToDConsiderationID;
+            this.Updated = other.Updated;
+            //this.User = other.User;
+            this.ZoneID = other.ZoneID;
+            this.DurationInSeconds = other.DurationInSeconds;
+            this.Code = other.Code;
+            this.Attempt = other.Attempt;
+        }
     }
 }
