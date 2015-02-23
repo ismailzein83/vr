f07473f397616c830b6df07c276c8f52ad320fb9
mustafa.hawi <mustafa.hawi@vanrise.tfs.local>
2015-02-23 14:45:00 +0000

    Add Pricing Generator
    git-tfs-id: [http://developmentvlan:8585/tfs/vanrise.collection]$/;C20951

---
+++ TOneV2/Code/TOneV2/TOne.BusinessEntity.Business/PricingGenerator.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TOne.Caching;
+
+namespace TOne.BusinessEntity.Business
+{
+    public class PricingGenerator 
+    {
+        TOneCacheManager _cacheManager;
+
+        public PricingGenerator(TOneCacheManager cacheManager)
+        {
+            _cacheManager = cacheManager;
+        }
+
+    }
+}
