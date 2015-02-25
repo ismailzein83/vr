d4637d9f9b2726f1a0b1186c9e27160dd1c910d6
Ismail Zein <VANRISE\ismail.zein>
2015-02-25 09:40:46 +0000

    added SupplierCodes classes
    git-tfs-id: [http://developmentvlan:8585/tfs/vanrise.collection]$/;C20994

---
+++ TOneV2/Code/TOneV2/TOne.BusinessEntity.Entities/SupplierCodes.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TOne.BusinessEntity.Entities
+{
+    public class SupplierCodes
+    {
+        /// <summary>
+        /// Key is Code number, value is Code details
+        /// </summary>
+        public Dictionary<string, Code> Codes { get; set; }
+    }
+}
---
+++ TOneV2/Code/TOneV2/TOne.BusinessEntity.Entities/SuppliersCodes.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TOne.BusinessEntity.Entities
+{
+    public class SuppliersCodes
+    {
+        /// <summary>
+        /// Key is SupplierID, Value is Supplier Codes
+        /// </summary>
+        public Dictionary<string, SupplierCodes> Codes { get; set; }
+    }
+}
