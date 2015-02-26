e85c770a0d91dff78943e7927f9135a34c4fc898
mustafa.hawi <mustafa.hawi@vanrise.tfs.local>
2015-02-26 15:35:43 +0000

    Add PricingGenerator
    git-tfs-id: [http://developmentvlan:8585/tfs/vanrise.collection]$/;C21065

---
+++ TOneV2/Code/TOneV2/TOne.CDR.Business/PricingGenerator.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TOne.BusinessEntity.Entities;
+using TOne.Caching;
+using TOne.CDR.Entities;
+
+namespace TOne.CDR.Business
+{
+    public class PricingGenerator
+    {
+
+        TOneCacheManager _cacheManager;
+
+        public PricingGenerator(TOneCacheManager cacheManager)
+        {
+            _cacheManager = cacheManager;
+        }
+
+        public T GetRepricing<T>(BillingCDRMain main) where T : BillingCDRPricingBase, new()
+        {
+            T pricing = new T();
+
+            
+            return pricing;
+        }
+    }
+}
