c441d4336742bcea5197673d31d5500f0dd13bc2
mustafa.hawi <mustafa.hawi@vanrise.tfs.local>
2015-03-03 10:05:38 +0000

    Store Main Invalid CDRs
    git-tfs-id: [http://developmentvlan:8585/tfs/vanrise.collection]$/;C21159

---
+++ TOneV2/Code/TOneV2/TOne.CDRProcess/StoreInvalidCDRsInDBProcess.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Activities;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TOne.CDRProcess.Arguments;
+using Vanrise.BusinessProcess;
+using Vanrise.BusinessProcess.Entities;
+
+public partial class StoreInvalidCDRsInDBProcess : Activity, IBPWorkflow
+{
+    #region IBPWorkflow Members
+
+    public string GetTitle(CreateProcessInput createProcessInput)
+    {
+        StoreInvalidCDRsInDBProcessInput inputArg = createProcessInput.InputArguments as StoreInvalidCDRsInDBProcessInput;
+        if (inputArg == null)
+            throw new ArgumentNullException("StoreInvalidCDRsInDBProcessInput");
+        return String.Format("Store Invalid CDRs for Switch {0}",inputArg.SwitchID);
+    }
+
+    #endregion
+}
---
+++ TOneV2/Code/TOneV2/TOne.CDRProcess/StoreMainCDRsInDBProcess.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Activities;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TOne.CDRProcess.Arguments;
+using Vanrise.BusinessProcess;
+using Vanrise.BusinessProcess.Entities;
+
+namespace TOne.CDRProcess
+{
+    public partial class StoreMainCDRsInDBProcess : Activity, IBPWorkflow
+    {
+        #region IBPWorkflow Members
+
+        public string GetTitle(CreateProcessInput createProcessInput)
+        {
+            StoreMainCDRsInDBProcessInput inputArg = createProcessInput.InputArguments as StoreMainCDRsInDBProcessInput;
+            if (inputArg == null)
+                throw new ArgumentNullException("StoreMainCDRsInDBProcessInput");
+            return String.Format("Store Main CDRs for Switch {0}", inputArg.SwitchID);
+        }
+
+        #endregion
+    }
+}
