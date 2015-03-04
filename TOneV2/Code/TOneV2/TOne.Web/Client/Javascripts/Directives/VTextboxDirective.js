f687e2f0a1d35c251047a7f0859abe2e25a83bb2
mustafa.hawi <mustafa.hawi@vanrise.tfs.local>
2015-03-04 11:35:28 +0000

    Rename Input
    git-tfs-id: [http://developmentvlan:8585/tfs/vanrise.collection]$/;C21199

---
+++ TOneV2/Code/TOneV2/TOne.Web/Client/Javascripts/Directives/VTextboxDirective.js
@@ -0,0 +1,14 @@
+﻿'use strict';
+
+app.directive('vTextbox', function () {
+    return {
+        restrict: 'E',
+        scope: {
+            holder: '@placeholder',
+            icon:'@icon'
+        },
+        templateUrl: function(elem, attr){
+            return '../../Client/Templates/Directives/VTextbox' + attr.type + '.html';
+        }
+    };
+});
