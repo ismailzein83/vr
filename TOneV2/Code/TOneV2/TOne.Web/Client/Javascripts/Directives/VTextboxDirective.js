9e524e3aa060a19bcd25c366ae5f97546c066e5c
mustafa.hawi <mustafa.hawi@vanrise.tfs.local>
2015-03-04 15:08:03 +0000

    Refactoring Textbox Directive
    git-tfs-id: [http://developmentvlan:8585/tfs/vanrise.collection]$/;C21210

--- TOneV2/Code/TOneV2/TOne.Web/Client/Javascripts/Directives/VTextboxDirective.js
+++ TOneV2/Code/TOneV2/TOne.Web/Client/Javascripts/Directives/VTextboxDirective.js
@@ -1,6 +1,31 @@
 ﻿'use strict';
 
-var defaultCompile = function (attrs, obj) {
+var templates = {
+    dTemplate: "../../Client/Templates/Directives/VTextboxStandard.html",
+    templateIcon: "../../Client/Templates/Directives/VTextboxIcon.html",
+    getTemplateByType: function (type) {
+        return '../../Client/Templates/Directives/VTextbox' + type + '.html'
+    }
+};
+
+var allDir = [
+        {
+            name: "search",
+            dPlaceholder: "Search ...",
+            dIcon: "search",
+            dTemplateURL: templates.templateIcon
+        }
+        ,
+        {
+            name: "mail",
+            dPlaceholder: "Mail ...",
+            dIcon: "mail",
+            dTemplateURL: templates.dTemplate
+        }
+];
+
+
+var compileObj = function (attrs, obj) {
     if (attrs.type.toLowerCase() == obj.name) {
         if (attrs.icon == undefined) attrs.$set("icon", obj.dIcon);
         if (attrs.placeholder == undefined) attrs.$set("placeholder", obj.dPlaceholder);
@@ -8,55 +33,9 @@ var defaultCompile = function (attrs, obj) {
     return attrs;
 };
 
-var defaultCheckType = function (attrs, obj) {
+var checkType = function (attrs, obj) {
     return (attrs.type.toLowerCase() == obj.name);
 };
-
-var templates = {
-    dTemplate: "../../Client/Templates/Directives/VTextboxStandard.html",
-    tempIcon: "../../Client/Templates/Directives/VTextboxIcon.html"
-};
-
-var defaultConfiguration = {
-    name: "default",
-    dPlaceholder: "",
-    dIcon: "",
-    dTemplateURL: templates.dTemplate,
-    compile: function (attrs) {
-        return defaultCompile(attrs, this);
-    },
-    checkType: function (attrs) {
-        return defaultCheckType(attrs, this);
-    }
-};
-
-var allTextBox = [
-        {
-            name: "search",
-            dPlaceholder: "Search ...",
-            dIcon: "search",
-            dTemplateURL: templates.tempIcon,
-            compile : function(attrs) {
-                return defaultCompile(attrs, this);
-            },
-            checkType: function (attrs) {
-                return defaultCheckType(attrs, this);
-            }
-        }
-        ,
-        {
-            name: "mail",
-            dPlaceholder: "Mail ...",
-            dIcon: "mail",
-            dTemplateURL: templates.tempIcon,
-            compile: function (attrs) {
-                return defaultCompile(attrs, this);
-            },
-            checkType: function (attrs) {
-                return defaultCheckType(attrs, this);
-            }
-        }
-];
 
 app.directive('vTextbox', function () {
     return {
@@ -67,16 +46,15 @@ app.directive('vTextbox', function () {
             text:'=text'
         },
         compile: function (element, attrs) {
-            allTextBox.forEach(function (item) {
-                attrs = item.compile(attrs);
+            allDir.forEach(function (item) {
+                attrs = compileObj(attrs, item);
             });
         },
         templateUrl: function (element, attrs) {
-            for (var index = 0; index < allTextBox.length; ++index) {
-                if (allTextBox[index].checkType(attrs)) return allTextBox[index].dTemplateURL;
+            for (var index = 0; index < allDir.length; ++index) {
+                if (checkType(attrs,allDir[index])) return allDir[index].dTemplateURL;
             }
-            return '../../Client/Templates/Directives/VTextbox' + attrs.type + '.html';
-
+            return templates.getTemplateByType(attrs.type);
         }
     };
 });
