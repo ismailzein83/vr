20aa3f3f66c60825bf1e4ed1e6c1ca3328b67948
mustafa.hawi <mustafa.hawi@vanrise.tfs.local>
2015-03-04 11:04:10 +0000

    Input Directive
    git-tfs-id: [http://developmentvlan:8585/tfs/vanrise.collection]$/;C21197

--- TOneV2/Code/TOneV2/TOne.Web/Client/Javascripts/Directives/VInputDirective.js
+++ TOneV2/Code/TOneV2/TOne.Web/Client/Javascripts/Directives/VInputDirective.js
@@ -1,1 +1,13 @@
-﻿
+﻿'use strict';
+
+app.directive('vInput', function () {
+    return {
+        restrict: 'E',
+        scope: {
+            holder: '@placeholder'
+        },
+        templateUrl: function(elem, attr){
+            return '../../Client/Templates/Directives/VInput' + attr.type + '.html';
+        }
+    };
+});
