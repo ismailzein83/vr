9490ad6acb17c69c84ac6013c59a443ca7800bf7
Ismail Zein <VANRISE\ismail.zein>
2015-03-05 10:24:05 +0000

    Added first webapi to TOne.Web
    git-tfs-id: [http://developmentvlan:8585/tfs/vanrise.collection]$/;C21258

---
+++ TOneV2/Code/TOneV2/TOne.Web/Controllers/BusinessEntityController.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using TOne.BusinessEntity.Business;
+using TOne.BusinessEntity.Entities;
+
+namespace TOne.Web.Controllers
+{
+    public class BusinessEntityController : ApiController
+    {
+        [HttpGet]
+        public List<CarrierInfo> GetCarriers(CarrierType carrierType)
+        {
+            CarrierManager manager = new CarrierManager();
+            return manager.GetCarriers(carrierType);
+        }
+    }
+}
