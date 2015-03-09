d99b94e84c26fe60a73f6e9ba43d9add4157e2a3
mustafa.hawi <mustafa.hawi@vanrise.tfs.local>
2015-03-09 10:03:13 +0000

    rename Js, Html files.
    git-tfs-id: [http://developmentvlan:8585/tfs/vanrise.collection]$/;C21320

---
+++ TOneV2/Code/TOneV2/TOne.Web/Client/Javascripts/Directives/vr-textbox-directive.js
@@ -0,0 +1,63 @@
+﻿'use strict';
+
+app.service('TemplatesService', function () { 
+
+    this.directiveMainURL = "../../Client/Templates/Directives/";
+
+    });
+
+app.service('TextBoxService', ['TemplatesService', function (TemplatesService) {
+
+    this.templates = {
+        dTemplate: TemplatesService.directiveMainURL + "VTextboxStandard.html",
+        templateIcon: TemplatesService.directiveMainURL + "VTextboxIcon.html",
+        templateButton: TemplatesService.directiveMainURL + "VTextboxButton.html",
+    };
+
+    this.getTemplatesByType = function (type) {
+
+        return TemplatesService.directiveMainURL + 'VTextbox' + type + '.html';
+
+    };
+
+    this.allDirective =[
+
+        {
+            name: "search",
+            dPlaceholder: "Search ...",
+            dIcon: "search",
+            dTemplateURL: this.templates.templateIcon
+        },
+        {
+            name: "mail",
+            dPlaceholder: "Mail ...",
+            dIcon: "mail",
+            dTemplateURL: this.templates.templateIcon
+        }
+    ];
+
+}]);
+
+app.directive('vTextbox', ['TextBoxService', function (TextBoxService) {
+    return {
+        restrict: 'E',
+        scope: {
+            type:'@',
+            placeholder: '@',
+            icon: '@',
+            buttontext: '@',
+            value: '=',
+            btnClick: '&'
+        },
+        controller: function () {
+        },
+        controllerAs: 'ctrl',
+        bindToController : true,
+        templateUrl: function (element, attrs) {
+            for (var index = 0; index < TextBoxService.allDirective.length; ++index) {
+                if(attrs.type.toLowerCase() == TextBoxService.allDirective[index].name) return TextBoxService.allDirective[index].dTemplateURL;
+            }
+            return TextBoxService.getTemplatesByType(attrs.type);
+        }
+    };
+}]);
