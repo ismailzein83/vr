70db2416e124d004ddb448768f078d4eeec472e2
Ismail Zein <VANRISE\ismail.zein>
2015-03-13 14:47:18 +0000

    added references
    git-tfs-id: [http://developmentvlan:8585/tfs/vanrise.collection]$/;C21508

--- TOneV2/Code/TOneV2/TOne.Web/Controllers/RoutingController.cs
+++ TOneV2/Code/TOneV2/TOne.Web/Controllers/RoutingController.cs
@@ -4,10 +4,15 @@ using System.Linq;
 using System.Net;
 using System.Net.Http;
 using System.Web.Http;
+using TOne.LCR.Entities;
 
 namespace TOne.Web.Controllers
 {
     public class RoutingController : ApiController
     {
+        public void SaveRouteRule(RouteRule rule)
+        {
+
+        }
     }
 }
