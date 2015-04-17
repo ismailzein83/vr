3faeec8de9a0f664fd71e275071c5cc758813da8
Ismail Zein <VANRISE\ismail.zein>
2015-04-17 09:52:04 +0000

    Added Projects
    git-tfs-id: [http://developmentvlan:8585/tfs/vanrise.collection]$/;C22433

---
+++ FZero/Vanrise.Fzero.CDRImport.BP/CDRImportProcess.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Activities;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Vanrise.BusinessProcess;
+using Vanrise.BusinessProcess.Entities;
+
+namespace Vanrise.Fzero.CDRImport.BP
+{
+    public partial class CDRImportProcess : Activity, IBPWorkflow
+    {
+        public string GetTitle(CreateProcessInput createProcessInput)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
