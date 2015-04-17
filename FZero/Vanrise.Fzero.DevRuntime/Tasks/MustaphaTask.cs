b6b203dff6f8d155b4a3bfbf06d7cb6fb29de0b9
mustafa.hawi <mustafa.hawi@vanrise.tfs.local>
2015-04-17 11:33:44 +0000

    Add Task
    git-tfs-id: [http://developmentvlan:8585/tfs/vanrise.collection]$/;C22441

---
+++ FZero/Vanrise.Fzero.DevRuntime/Tasks/MustaphaTask.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Vanrise.Fzero.DevRuntime.Tasks
+{
+    public class MustaphaTask : ITask
+    {
+        public void Execute()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
