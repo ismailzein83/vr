0b026e3b9615943d307aa3d4509df631946df373
mustafa.hawi <mustafa.hawi@vanrise.tfs.local>
2015-04-20 08:56:37 +0000

    Mustapha Task
    git-tfs-id: [http://developmentvlan:8585/tfs/vanrise.collection]$/;C22478

--- CDRAnalysis/Vanrise.Fzero.DevRuntime/Tasks/MustaphaTask.cs
+++ CDRAnalysis/Vanrise.Fzero.DevRuntime/Tasks/MustaphaTask.cs
@@ -1,8 +1,15 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Vanrise.BusinessProcess;
+using Vanrise.BusinessProcess.Client;
+using Vanrise.BusinessProcess.Entities;
+using Vanrise.Fzero.CDRImport.Entities;
+using Vanrise.Queueing;
+using Vanrise.Runtime;
 
 namespace Vanrise.Fzero.DevRuntime.Tasks
 {
@@ -10,7 +17,36 @@ namespace Vanrise.Fzero.DevRuntime.Tasks
     {
         public void Execute()
         {
-            throw new NotImplementedException();
+            Console.WriteLine("Mustapha Task started");
+            BusinessProcessService bpService = new BusinessProcessService() { Interval = new TimeSpan(0, 0, 2) };
+            QueueActivationService queueActivationService = new QueueActivationService() { Interval = new TimeSpan(0, 0, 2) };
+
+            var runtimeServices = new List<RuntimeService>();
+            runtimeServices.Add(queueActivationService);
+
+            runtimeServices.Add(bpService);
+
+            RuntimeHost host = new RuntimeHost(runtimeServices);
+            host.Start();
+            PersistentQueue<ImportedCDRBatch> queue =  Vanrise.Fzero.CDRImport.Business.CDRQueueFactory.GetImportedCDRQueue("DS1");
+
+            String fileName = "";
+
+            queue.Enqueue(new ImportedCDRBatch()
+            {
+                file =File.ReadAllBytes(fileName)
+            });
+            
+            BPClient bpClient = new BPClient();
+            bpClient.CreateNewProcess(new CreateProcessInput
+            {
+                ProcessName = "CDRImportProcess",
+                InputArguments = new Vanrise.Fzero.CDRImport.BP.Arguments.CDRImportProcessInput
+                {
+                }
+            });
+
+            Console.ReadKey();
         }
     }
 }
