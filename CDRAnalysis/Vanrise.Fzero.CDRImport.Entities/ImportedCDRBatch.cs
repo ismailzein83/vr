554a71d590634b7772df7e81e067d2e3b618f7c6
mustafa.hawi <mustafa.hawi@vanrise.tfs.local>
2015-04-20 08:56:20 +0000

    Imported CDR Batch
    git-tfs-id: [http://developmentvlan:8585/tfs/vanrise.collection]$/;C22477

--- CDRAnalysis/Vanrise.Fzero.CDRImport.Entities/ImportedCDRBatch.cs
+++ CDRAnalysis/Vanrise.Fzero.CDRImport.Entities/ImportedCDRBatch.cs
@@ -9,9 +9,17 @@ namespace Vanrise.Fzero.CDRImport.Entities
 {
     public class ImportedCDRBatch : PersistentQueueItem
     {
+        static ImportedCDRBatch()
+        {
+            Vanrise.Common.ProtoBufSerializer.AddSerializableType(typeof(ImportedCDRBatch), "file");
+        }
+
         public override string GenerateDescription()
         {
-            return "";
+            return String.Format("Imported CDR Batch of {0} CDRs", file.Length);
         }
+
+        public byte[] file;
+
     }
 }
