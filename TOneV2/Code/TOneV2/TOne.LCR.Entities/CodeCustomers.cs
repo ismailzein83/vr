252bd8f82b5e3a43819c4fb69383784df0266106
rabih.fashwal:2 <rabih.fashwal:2@vanrise.tfs.local>
2015-04-21 12:27:15 +0000

    Differential Routing
    git-tfs-id: [http://developmentvlan:8585/tfs/vanrise.collection]$/;C22530

---
+++ TOneV2/Code/TOneV2/TOne.LCR.Entities/CodeCustomers.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TOne.LCR.Entities
+{
+    /// <summary>
+    ///Key is Code, Value is HashSet of Customer IDs
+    /// </summary>
+    public class CodeCustomers : Dictionary<string, HashSet<string>>
+    {
+
+    }
+}
---
+++ TOneV2/Code/TOneV2/TOne.LCR.Entities/Routing/DifferentialRouteState.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TOne.LCR.Entities.Routing
+{
+  public  class DifferentialRouteState
+    {
+        public DateTime LastRun { get; set; }
+    }
+}
