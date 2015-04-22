99f1da603afacb59660e0fb5017192b07bfa1a28
Ali Ballouk <VANRISE\ali.ballouk>
2015-04-22 06:33:24 +0000

    re-fix
    git-tfs-id: [http://developmentvlan:8585/tfs/vanrise.collection]$/;C22547

--- TOneV2/Code/TOneV2/TOne.Web/Client/Javascripts/Directives/vr-select-directive.js
+++ TOneV2/Code/TOneV2/TOne.Web/Client/Javascripts/Directives/vr-select-directive.js
@@ -198,6 +198,7 @@ app.directive('vrSelect', ['SelectService', 'BaseDirService', 'ValidationMessage
                     angular.element(element[0].querySelector('.dropdown-container1')).attr('style', 'position: relative;');
                     angular.element(element[0].querySelector('.dropdown-container2')).attr('style', 'position: absolute;right: -10px;top: -16px;');
                     divDropdown.prepend(lblTemplate);
+                    divDropdown.addClass('menu-right-parent');
                     ulDropdown.addClass('menu-right');
                 }
                 else if (SelectService.isActionBarTop(attrs.type)) {
