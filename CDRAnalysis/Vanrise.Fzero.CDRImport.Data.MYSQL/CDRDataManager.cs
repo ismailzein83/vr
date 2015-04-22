cc8c6864b082b9d4b78d388e3d463d50a5673c9a
hadi.hawi <hadi.hawi@vanrise.tfs.local>
2015-04-22 10:44:36 +0000

    Add CDR Manager
    git-tfs-id: [http://developmentvlan:8585/tfs/vanrise.collection]$/;C22564

---
+++ CDRAnalysis/Vanrise.Fzero.CDRImport.Data.MYSQL/CDRDataManager.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Vanrise.Fzero.CDRImport.Entities;
+using Vanrise.Data;
+
+namespace Vanrise.Fzero.CDRImport.Data.MYSQL
+{
+    public class CDRDataManager : BaseDataManager, ICDRDataManager
+    {
+        public CDRDataManager()
+            : base("CDRDBConnectionString")
+        {
+
+        }
+
+
+        public object PrepareCDRsForDBApply(List<CDR> cdrs)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void ApplyCDRsToDB(object preparedInvalidCDRs)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
