ae815318134dd12d1cdc731c7da7b1bfaeecbcad
Ismail Zein <VANRISE\ismail.zein>
2015-04-23 14:19:00 +0000

    Added MySQL project
    git-tfs-id: [http://developmentvlan:8585/tfs/vanrise.collection]$/;C22598

---
+++ Vanrise/Code/Vanrise.Data.MySQL/BaseMySQLDataManager.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Vanrise.Data.MySQL
+{
+    public class BaseMySQLDataManager : BaseDataManager
+    {
+        protected StreamForBulkInsert InitializeStreamForBulkInsert()
+        {
+            string filePath = GetFilePathForBulkInsert();
+            return new StreamForBulkInsert(filePath);
+        }
+
+        protected string GetFilePathForBulkInsert()
+        {
+            return System.IO.Path.GetTempFileName();
+        }
+    }
+}
