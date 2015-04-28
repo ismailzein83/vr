b8b46f6b2002f7f91ace9b33fe855470764f5320
walid.mogharbel <walid.mogharbel@vanrise.tfs.local>
2015-04-28 12:04:49 +0000

    git-tfs-id: [http://developmentvlan:8585/tfs/vanrise.collection]$/;C22666

---
+++ CDRAnalysis/Vanrise.Fzero.FraudAnalysis.Data/ICDRDataManager.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Vanrise.Fzero.FraudAnalysis.Entities;
+
+namespace Vanrise.Fzero.FraudAnalysis.Data
+{
+    public interface ICDRDataManager : IDataManager
+    {
+        void LoadNumberProfile(DateTime from, DateTime to, int? batchSize, Action<List<NumberProfile>> onBatchReady);
+    }
+}
