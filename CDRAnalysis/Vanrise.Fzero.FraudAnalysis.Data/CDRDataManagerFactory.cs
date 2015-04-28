39877feaec0889b594410daf0df7da8366aef0fc
walid.mogharbel <walid.mogharbel@vanrise.tfs.local>
2015-04-28 11:55:15 +0000

    git-tfs-id: [http://developmentvlan:8585/tfs/vanrise.collection]$/;C22665

---
+++ CDRAnalysis/Vanrise.Fzero.FraudAnalysis.Data/CDRDataManagerFactory.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+using Vanrise.Common;
+
+namespace Vanrise.Fzero.FraudAnalysis.Data
+{
+    public static class CDRDataManagerFactory
+    {
+        static ObjectFactory s_objectFactory;
+        static CDRDataManagerFactory()
+        {
+            s_objectFactory = new ObjectFactory(Assembly.Load("Vanrise.Fzero.FraudAnalysis.Data.MYSQL"));
+        }
+
+        public static T GetDataManager<T>() where T : class, IDataManager
+        {
+            return s_objectFactory.CreateObjectFromType<T>();
+        }
+    }
+}
