90847d4e54436a1a4ea9a2d7c5e7f4dfa659b7a4
walid.mogharbel <walid.mogharbel@vanrise.tfs.local>
2015-04-28 09:58:00 +0000

    git-tfs-id: [http://developmentvlan:8585/tfs/vanrise.collection]$/;C22661

--- CDRAnalysis/Vanrise.Fzero.FraudAnalysis.Entities/NumberProfile.cs
+++ CDRAnalysis/Vanrise.Fzero.FraudAnalysis.Entities/NumberProfile.cs
@@ -8,35 +8,32 @@ namespace Vanrise.Fzero.FraudAnalysis.Entities
 {
     public class NumberProfile
     {
-         public string Number { get; set; }
-         public DateTime? Date_Day { get; set; }
-         public int?  Day_Hour { get; set; }
-         public int?  Count_Out_Calls { get; set; }
-         public int   Count_In_Calls { get; set; }
-         public int ? Count_Out_Fail { get; set; }
-         public int   Count_In_Fail { get; set; }
-         public decimal?  Call_Out_Dur_Avg { get; set; }
-         public decimal   Call_In_Dur_Avg { get; set; }
-         public decimal?  Total_Out_Volume { get; set; }
-         public decimal   Total_In_Volume { get; set; }
-         public int?      Diff_Output_Numb { get; set; }
-         public int       Diff_Input_Numbers { get; set; }
-         public int?      Diff_Dest_net { get; set; }
-         public int?      Diff_Sources_net { get; set; }
-         public int?      Total_BTS_In { get; set; }
-         public int?      Total_BTS_Out { get; set; }
-         public int?      Count_Out_SMS { get; set; }
-         public int?      Count_In_SMS { get; set; }
-         public int?      Total_IMEI { get; set; }
-         public int ?     Count_Out_OnNet { get; set; }
-         public int?      Count_In_OnNet { get; set; }
-         public int?      Count_Out_OffNet { get; set; }
-         public int?      Count_In_OffNet { get; set; }
-         public int?      Total_BTS { get; set; }
-         public int?      Count_Out_Inter { get; set; }
-         public int?      Count_In_Inter { get; set; }
-         public int?      IsOnNet { get; set; }
-         public decimal?  Total_Data_Volume { get; set; }
+        public string SubscriberNumber { get; set; }
+        public DateTime? FromDate { get; set; }
+        public DateTime? ToDate { get; set; }
+        public int?  Count_Out_Calls { get; set; }
+        public int?      Diff_Output_Numb { get; set; }
+        public int?      Count_Out_Inter { get; set; }
+        public int?      Count_In_Inter { get; set; }
+        public decimal?  Call_Out_Dur_Avg { get; set; }
+        public int ? Count_Out_Fail { get; set; }
+        public int   Count_In_Fail { get; set; }
+        public decimal?  Total_Out_Volume { get; set; }
+        public decimal   Total_In_Volume { get; set; }
+        public int       Diff_Input_Numbers { get; set; }
+        public int?      Count_Out_SMS { get; set; }
+        public int?      Total_IMEI { get; set; }
+        public int?      Total_BTS { get; set; }
+        public int?      IsOnNet { get; set; }
+        public decimal?  Total_Data_Volume { get; set; }
+        public int?  PeriodId { get; set; }                
+        public int   Count_In_Calls { get; set; }
+        public decimal   Call_In_Dur_Avg { get; set; }
+        public int ?     Count_Out_OnNet { get; set; }
+        public int?      Count_In_OnNet { get; set; }
+        public int?      Count_Out_OffNet { get; set; }
+        public int?      Count_In_OffNet { get; set; }
+        
 
     }
 }
