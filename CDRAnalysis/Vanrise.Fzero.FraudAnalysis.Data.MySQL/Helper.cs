4fdb18b3fe1626bde6190d96ed154ef120298ab4
walid.mogharbel <walid.mogharbel@vanrise.tfs.local>
2015-04-29 11:56:04 +0000

    git-tfs-id: [http://developmentvlan:8585/tfs/vanrise.collection]$/;C22704

---
+++ CDRAnalysis/Vanrise.Fzero.FraudAnalysis.Data.MySQL/Helper.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Vanrise.Fzero.FraudAnalysis.Entities;
+using Vanrise.Data;
+using MySql.Data.MySqlClient;
+using System.IO;
+using Vanrise.Data.MySQL;
+using Vanrise.Fzero.FraudAnalysis.Data.MySQL;
+
+namespace Vanrise.Fzero.FraudAnalysis.Data.MySQL
+{
+    public static class Helper
+    {
+
+        public static int? AsNullableInt(this string s)
+        {
+            int value;
+            if (int.TryParse(s, out value))
+                return value;
+
+            return null;
+        }
+
+        public static decimal? AsNullableDecimal(this string s)
+        {
+            decimal value;
+            if (decimal.TryParse(s, out value))
+                return value;
+
+            return null;
+        }
+
+        public static DateTime? AsNullableDateTime(this string s)
+        {
+            DateTime value;
+            if (DateTime.TryParse(s, out value))
+                return value;
+
+            return null;
+        }
+
+        public static int AsInt(this string s)
+        {
+            int value;
+            if (int.TryParse(s, out value))
+                return value;
+
+            return 0;
+        }
+
+        public static decimal AsDecimal(this string s)
+        {
+            decimal value;
+            if (decimal.TryParse(s, out value))
+                return value;
+
+            return 0;
+        }
+    }
+}
