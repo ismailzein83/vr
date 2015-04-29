f4d06a0410c5f60bf5365ad1148c0af7dc4f8ce0
hassan.ali <hassan.ali@vanrise.tfs.local>
2015-04-29 08:51:32 +0000

    Refactoring of Daily Repricing process
    git-tfs-id: [http://developmentvlan:8585/tfs/vanrise.collection]$/;C22692

---
+++ TOneV2/Code/TOneV2/TOne.CDRProcess.Activities/PrepareInvalidCDRsForDBApply.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Activities;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TOne.CDR.Data;
+using Vanrise.BusinessProcess;
+using Vanrise.Queueing;
+
+namespace TOne.CDRProcess.Activities
+{
+    #region Argument Classes
+    public class PrepareInvalidCDRsForDBApplyInput
+    {
+        public BaseQueue<TOne.CDR.Entities.CDRInvalidBatch> InputQueue { get; set; }
+
+        public BaseQueue<Object> OutputQueue { get; set; }
+    }
+
+    #endregion
+
+    public sealed class PrepareInvalidCDRsForDBApply : DependentAsyncActivity<PrepareInvalidCDRsForDBApplyInput>
+    {
+        [RequiredArgument]
+        public InArgument<BaseQueue<TOne.CDR.Entities.CDRInvalidBatch>> InputQueue { get; set; }
+
+
+        [RequiredArgument]
+        public InOutArgument<BaseQueue<Object>> OutputQueue { get; set; }
+
+        protected override void OnBeforeExecute(AsyncCodeActivityContext context, AsyncActivityHandle handle)
+        {
+            if (this.OutputQueue.Get(context) == null)
+                this.OutputQueue.Set(context, new MemoryQueue<object>());
+            base.OnBeforeExecute(context, handle);
+        }
+
+        protected override PrepareInvalidCDRsForDBApplyInput GetInputArgument2(AsyncCodeActivityContext context)
+        {
+            return new PrepareInvalidCDRsForDBApplyInput
+            {
+                InputQueue = this.InputQueue.Get(context),
+                OutputQueue = this.OutputQueue.Get(context)
+            };
+        }
+
+        protected override void DoWork(PrepareInvalidCDRsForDBApplyInput inputArgument, AsyncActivityStatus previousActivityStatus, AsyncActivityHandle handle)
+        {
+            TimeSpan totalTime = default(TimeSpan);
+            ICDRDataManager dataManager = CDRDataManagerFactory.GetDataManager<ICDRDataManager>();
+            DoWhilePreviousRunning(previousActivityStatus, handle, () =>
+            {
+                bool hasItem = false;
+                do
+                {
+                    hasItem = inputArgument.InputQueue.TryDequeue(
+                        (mainCDR) =>
+                        {
+                            DateTime start = DateTime.Now;
+                            Object preparedInvalidCDRs = dataManager.PrepareInvalidCDRsForDBApply(mainCDR.InvalidCDRs);
+                            inputArgument.OutputQueue.Enqueue(preparedInvalidCDRs);
+                            totalTime += (DateTime.Now - start);
+                        });
+                }
+                while (!ShouldStop(handle) && hasItem);
+            });
+        }
+    }
+}
