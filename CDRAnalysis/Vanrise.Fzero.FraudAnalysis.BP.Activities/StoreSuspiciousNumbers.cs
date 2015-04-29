e61e2e8a4a498809f95010ee27673cc8eb1a730a
hadi.hawi <hadi.hawi@vanrise.tfs.local>
2015-04-29 14:21:48 +0000

    create Activity
    git-tfs-id: [http://developmentvlan:8585/tfs/vanrise.collection]$/;C22711

---
+++ CDRAnalysis/Vanrise.Fzero.FraudAnalysis.BP.Activities/StoreSuspiciousNumbers.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Activities;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Vanrise.BusinessProcess;
+using Vanrise.Fzero.FraudAnalysis.Data;
+using Vanrise.Fzero.FraudAnalysis.Entities;
+using Vanrise.Queueing;
+
+
+namespace Vanrise.Fzero.FraudAnalysis.BP.Activities
+{
+    #region Arguments Classes
+
+    public class GetStoreSuspiciousNumbersInput
+    {
+        public BaseQueue<SuspiciousNumberBatch> InputQueue { get; set; }
+
+    }
+
+    #endregion
+
+    public class StoreSuspiciousNumbers : DependentAsyncActivity<GetStoreSuspiciousNumbersInput>
+    {
+        #region Arguments
+
+        [RequiredArgument]
+        public InArgument<BaseQueue<SuspiciousNumberBatch>> InputQueue { get; set; }
+
+        #endregion
+
+
+        protected override void DoWork(GetStoreSuspiciousNumbersInput inputArgument, AsyncActivityStatus previousActivityStatus, AsyncActivityHandle handle)
+        {
+            handle.SharedInstanceData.WriteTrackingMessage(BusinessProcess.Entities.BPTrackingSeverity.Information, "Start StoreSuspiciousNumbers.DoWork.Start {0}", DateTime.Now);
+            IFraudDataManager dataManager = FraudDataManagerFactory.GetDataManager<IFraudDataManager>();
+            
+            DoWhilePreviousRunning(previousActivityStatus, handle, () =>
+            {
+                bool hasItem = false;
+                do
+                {
+                    hasItem = inputArgument.InputQueue.TryDequeue(
+                        (item) =>
+                        {
+                            handle.SharedInstanceData.WriteTrackingMessage(BusinessProcess.Entities.BPTrackingSeverity.Information, "End StoreSuspiciousNumbers.DoWork.Dequeued {0}", DateTime.Now);
+                            dataManager.StoreSuspiciousNumbers(item.suspiciousNumbers);
+                            handle.SharedInstanceData.WriteTrackingMessage(BusinessProcess.Entities.BPTrackingSeverity.Information, "End StoreSuspiciousNumbers.DoWork.SavedtoDB {0}", DateTime.Now);
+                        });
+                }
+                while (!ShouldStop(handle) && hasItem);
+            });
+            handle.SharedInstanceData.WriteTrackingMessage(BusinessProcess.Entities.BPTrackingSeverity.Information, "End StoreSuspiciousNumbers.DoWork.End {0}", DateTime.Now);
+        }
+
+        protected override GetStoreSuspiciousNumbersInput GetInputArgument2(AsyncCodeActivityContext context)
+        {
+            return new GetStoreSuspiciousNumbersInput
+            {
+                InputQueue = this.InputQueue.Get(context)
+            };
+        }
+    }
+}
