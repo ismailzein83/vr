ae114dd0c272140364bce43b960f1c99535fec78
mustafa.hawi <mustafa.hawi@vanrise.tfs.local>
2015-04-29 12:35:15 +0000

    Load Number Profile
    git-tfs-id: [http://developmentvlan:8585/tfs/vanrise.collection]$/;C22705

--- CDRAnalysis/Vanrise.Fzero.FraudAnalysis.BP.Activities/LoadNumberProfile.cs
+++ CDRAnalysis/Vanrise.Fzero.FraudAnalysis.BP.Activities/LoadNumberProfile.cs
@@ -8,6 +8,7 @@ using Vanrise.Fzero.FraudAnalysis.Entities;
 using Vanrise.Queueing;
 using System.Configuration;
 using Vanrise.Fzero.FraudAnalysis.Data.MySQL;
+using Vanrise.Fzero.FraudAnalysis.Data;
 
 namespace Vanrise.Fzero.FraudAnalysis.BP.Activities
 {
@@ -28,7 +29,7 @@ namespace Vanrise.Fzero.FraudAnalysis.BP.Activities
         #region Arguments
 
         [RequiredArgument]
-        public  InArgument<BaseQueue<NumberProfileBatch>> OutputQueue { get; set; }
+        public  InOutArgument<BaseQueue<NumberProfileBatch>> OutputQueue { get; set; }
 
         #endregion
 
@@ -36,16 +37,19 @@ namespace Vanrise.Fzero.FraudAnalysis.BP.Activities
         protected override void DoWork(LoadNumberProfilesInput inputArgument, AsyncActivityHandle handle)
         {
            int BatchSize = int.Parse( System.Configuration.ConfigurationManager.AppSettings["NumberProfileBatchSize"].ToString());
-           //handle.SharedInstanceData.WriteTrackingMessage(BusinessProcess.Entities.BPTrackingSeverity.Information, "Start LoadNumberProfiles.DoWork.Start {0}", DateTime.Now);
-           //new NumberProfileDataManager().LoadNumberProfile(DateTime.Now.AddHours(-1), DateTime.Now, BatchSize, null);
+           handle.SharedInstanceData.WriteTrackingMessage(BusinessProcess.Entities.BPTrackingSeverity.Information, "Start LoadNumberProfiles.DoWork.Start {0}", DateTime.Now);
+
+           INumberProfileDataManager dataManager = FraudDataManagerFactory.GetDataManager<INumberProfileDataManager>();
+           dataManager.LoadNumberProfile(DateTime.Now.AddHours(-1), DateTime.Now, BatchSize, (numberProfiles) =>
+           {
+               inputArgument.OutputQueue.Enqueue(new NumberProfileBatch()
+               {
+                   numberProfiles = numberProfiles
+               });
+           });
+
            //handle.SharedInstanceData.WriteTrackingMessage(BusinessProcess.Entities.BPTrackingSeverity.Information, "Start LoadNumberProfiles.DoWork.LoadNumberProfile {0}", DateTime.Now);
-           List<NumberProfile> NumberProfiles = new List<NumberProfile>();
-           NumberProfile NumberProfile = new NumberProfile();
-           NumberProfiles.Add(NumberProfile);
-           inputArgument.OutputQueue.Enqueue(new NumberProfileBatch()
-             {
-                numberProfiles = NumberProfiles
-             });
+           
            //handle.SharedInstanceData.WriteTrackingMessage(BusinessProcess.Entities.BPTrackingSeverity.Information, "Start LoadNumberProfiles.DoWork.Enqueued {0}", DateTime.Now);
            //handle.SharedInstanceData.WriteTrackingMessage(BusinessProcess.Entities.BPTrackingSeverity.Information, "End LoadNumberProfiles.DoWork {0}", DateTime.Now);
         }
