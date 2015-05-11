72462d7cc5d945ad7fc8aa30f223be1a9c7940cb
walid.mogharbel <walid.mogharbel@vanrise.tfs.local>
2015-05-11 15:03:41 +0000

    git-tfs-id: [http://developmentvlan:8585/tfs/vanrise.collection]$/;C22859

---
+++ CDRAnalysis/Vanrise.Fzero.FraudAnalysis.Entities/AverageAggregate.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Vanrise.Fzero.FraudAnalysis.Entities
+{
+    public class AverageAggregate:IAggregate
+    {
+
+        public AverageAggregate(string conditionExpression)
+        {
+
+        }
+
+        public void Reset()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void EvaluateCDR(NormalCDR normalCDR)
+        {
+            throw new NotImplementedException();
+        }
+
+        public decimal GetResult()
+        {
+            throw new NotImplementedException();
+        }
+    }
+
+    
+}
