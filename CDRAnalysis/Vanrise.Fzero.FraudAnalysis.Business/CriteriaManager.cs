6e3437ce70432284a6eee30fc856065f1dd3bc57
walid.mogharbel <walid.mogharbel@vanrise.tfs.local>
2015-05-13 10:04:59 +0000

    git-tfs-id: [http://developmentvlan:8585/tfs/vanrise.collection]$/;C22917

--- CDRAnalysis/Vanrise.Fzero.FraudAnalysis.Business/CriteriaManager.cs
+++ CDRAnalysis/Vanrise.Fzero.FraudAnalysis.Business/CriteriaManager.cs
@@ -27,33 +27,25 @@ namespace Vanrise.Fzero.FraudAnalysis.Business
             decimal? result = 0;
 
             decimal CountOutCalls = numberProfile.AggregateValues["CountOutCalls"];
-            decimal DiffOutputNumb = numberProfile.AggregateValues.Where(x => x.Key == "DiffOutputNumb").FirstOrDefault().Value;
-            decimal CountOutInter = numberProfile.AggregateValues.Where(x => x.Key == "CountOutInter").FirstOrDefault().Value;
-            decimal CountInInter = numberProfile.AggregateValues.Where(x => x.Key == "CountInInter").FirstOrDefault().Value;
-            decimal CallOutDurAvg = numberProfile.AggregateValues.Where(x => x.Key == "CallOutDurAvg").FirstOrDefault().Value;
-            decimal CountOutFail = numberProfile.AggregateValues.Where(x => x.Key == "CountOutFail").FirstOrDefault().Value;
-            decimal CountInFail = numberProfile.AggregateValues.Where(x => x.Key == "CountInFail").FirstOrDefault().Value;
-            decimal TotalOutVolume = numberProfile.AggregateValues.Where(x => x.Key == "TotalOutVolume").FirstOrDefault().Value;
-            decimal TotalInVolume = numberProfile.AggregateValues.Where(x => x.Key == "TotalInVolume").FirstOrDefault().Value;
-            decimal DiffInputNumbers = numberProfile.AggregateValues.Where(x => x.Key == "DiffInputNumbers").FirstOrDefault().Value;
-            decimal CountOutSMS = numberProfile.AggregateValues.Where(x => x.Key == "CountOutSMS").FirstOrDefault().Value;
-            decimal TotalIMEI = numberProfile.AggregateValues.Where(x => x.Key == "TotalIMEI").FirstOrDefault().Value;
-            decimal TotalBTS = numberProfile.AggregateValues.Where(x => x.Key == "TotalBTS").FirstOrDefault().Value;
-            decimal CountInCalls = numberProfile.AggregateValues.Where(x => x.Key == "CountInCalls").FirstOrDefault().Value;
-            decimal CallInDurAvg = numberProfile.AggregateValues.Where(x => x.Key == "CallInDurAvg").FirstOrDefault().Value;
-            decimal CountOutOnNet = numberProfile.AggregateValues.Where(x => x.Key == "CountOutOnNet").FirstOrDefault().Value;
-            decimal CountInOnNet = numberProfile.AggregateValues.Where(x => x.Key == "CountInOnNet").FirstOrDefault().Value;
-            decimal CountOutOffNet = numberProfile.AggregateValues.Where(x => x.Key == "CountOutOffNet").FirstOrDefault().Value;
-            decimal CountInOffNet = numberProfile.AggregateValues.Where(x => x.Key == "CountInOffNet").FirstOrDefault().Value;
-            decimal TotalDataVolume = numberProfile.AggregateValues.Where(x => x.Key == "TotalDataVolume").FirstOrDefault().Value;
-
-
-
-
-
-
-
-
+            decimal DiffOutputNumb = numberProfile.AggregateValues["DiffOutputNumb"];
+            decimal CountOutInter = numberProfile.AggregateValues["CountOutInter"];
+            decimal CountInInter = numberProfile.AggregateValues["CountInInter"];
+            decimal CallOutDurAvg = numberProfile.AggregateValues["CallOutDurAvg"];
+            decimal CountOutFail = numberProfile.AggregateValues["CountOutFail"];
+            decimal CountInFail = numberProfile.AggregateValues["CountInFail"];
+            decimal TotalOutVolume = numberProfile.AggregateValues["TotalOutVolume"];
+            decimal TotalInVolume = numberProfile.AggregateValues["TotalInVolume"];
+            decimal DiffInputNumbers = numberProfile.AggregateValues["DiffInputNumbers"];
+            decimal CountOutSMS = numberProfile.AggregateValues["CountOutSMS"];
+            decimal TotalIMEI = numberProfile.AggregateValues["TotalIMEI"];
+            decimal TotalBTS = numberProfile.AggregateValues["TotalBTS"];
+            decimal CountInCalls = numberProfile.AggregateValues["CountInCalls"];
+            decimal CallInDurAvg = numberProfile.AggregateValues["CallInDurAvg"];
+            decimal CountOutOnNet = numberProfile.AggregateValues["CountOutOnNet"];
+            decimal CountInOnNet = numberProfile.AggregateValues["CountInOnNet"];
+            decimal CountOutOffNet = numberProfile.AggregateValues["CountOutOffNet"];
+            decimal CountInOffNet = numberProfile.AggregateValues["CountInOffNet"];
+            decimal TotalDataVolume = numberProfile.AggregateValues["TotalDataVolume"];
 
 
 
@@ -202,31 +194,7 @@ namespace Vanrise.Fzero.FraudAnalysis.Business
         }
 
 
-        //enum Criteria 
-        //{ 
-        //    Ratio_Incoming_Calls_vs_Outgoing_Calls = 1,
-        //    Count_of_Distinct_Destinations = 2,
-        //    Count_outgoing_calls = 3,
-        //    Count_of_Total_BTS_Per_MSISDN = 4,
-        //    Total_Originated_Volume = 5,
-        //    Count_of_Total_IMEI_Per_MSISDN = 6,
-        //    Ratio_Average_Incoming_Duration_vs_Average_Outgoing_Duration = 7,
-        //    Ratio_OffNet_Originated_Calls_vs_OnNet_Originated_Calls = 8,
-        //    Count_of_daily_active_hours = 9,
-        //    Distinct_Destination_of_Night_Calls = 10,
-        //    Voice_Only_Service_Usage = 11,
-        //    Ratio_of_Distinct_Destination_vs_Total_Number_of_Calls = 12,
-        //    Ratio_International_Originated_Vs_Outgoing_Calls = 13,
-        //    Count_of_outgoing_during_peak_hours = 14,
-        //    Data_Usage = 15
-        //};
-
-
-        //enum Period
-        //{
-        //    Day = 6,
-        //    Hour = 1
-        //};
+        
 
     }
 }
