cd46a74bf767639f76491a418d09b013409b4130
walid.mogharbel <walid.mogharbel@vanrise.tfs.local>
2015-05-18 14:41:43 +0000

    git-tfs-id: [http://developmentvlan:8585/tfs/vanrise.collection]$/;C23006

--- CDRAnalysis/Vanrise.Fzero.CDRImport.BP.Activities/ImportCDRs.cs
+++ CDRAnalysis/Vanrise.Fzero.CDRImport.BP.Activities/ImportCDRs.cs
@@ -56,7 +56,7 @@ namespace Vanrise.Fzero.CDRImport.BP.Activities
                         {
                             String filePath = SFTP_Dir + "/" + fileObj.Name;
                             String newFilePath = SFTP_Dir + "/" + fileObj.Name.Replace(".DAT", ".old");
-                            
+
                             var stream = new MemoryStream();
                             sftp.GetFile(filePath, stream);
                             handle.SharedInstanceData.WriteTrackingMessage(BusinessProcess.Entities.BPTrackingSeverity.Information, "Start ImportCDRs.DoWork.FileStreamedtoMemory {0}", DateTime.Now);
@@ -68,13 +68,13 @@ namespace Vanrise.Fzero.CDRImport.BP.Activities
                                 ms.Position = 0;
                                 var sr = new StreamReader(ms);
 
-                                while (! sr.EndOfStream)
+                                while (!sr.EndOfStream)
                                 {
                                     var i = sr.ReadLine();
 
                                     CDR cdr = new CDR();
-                                    cdr.MSISDN = i.Substring(145,20).Trim();
-                                    cdr.IMSI = i.Substring(125,20).Trim();
+                                    cdr.MSISDN = i.Substring(145, 20).Trim();
+                                    cdr.IMSI = i.Substring(125, 20).Trim();
                                     cdr.Destination = i.Substring(198, 20).Trim();
                                     cdr.Call_Class = i.Substring(434, 10).Trim();
                                     cdr.Sub_Type = i.Substring(165, 10).Trim();
@@ -94,28 +94,28 @@ namespace Vanrise.Fzero.CDRImport.BP.Activities
                                     int Call_Type = 0;
                                     if (int.TryParse(i.Substring(102, 3).Trim(), out Call_Type))
                                         cdr.Call_Type = Call_Type;
-                                    
-                                    decimal Cell_Latitude ;
+
+                                    decimal Cell_Latitude;
                                     if (decimal.TryParse(i.Substring(609, 9).Trim(), out Cell_Latitude))
                                         cdr.Cell_Latitude = Cell_Latitude;
 
 
-                                    decimal DurationInSeconds ;
+                                    decimal DurationInSeconds;
                                     if (decimal.TryParse(i.Substring(588, 10).Trim(), out DurationInSeconds))
                                         cdr.DurationInSeconds = DurationInSeconds;
 
 
-                                    decimal Up_Volume ;
+                                    decimal Up_Volume;
                                     if (decimal.TryParse(i.Substring(609, 9).Trim(), out Up_Volume))
                                         cdr.UpVolume = Up_Volume;
 
 
-                                    decimal Cell_Longitude ;
+                                    decimal Cell_Longitude;
                                     if (decimal.TryParse(i.Substring(618, 9).Trim(), out Cell_Longitude))
                                         cdr.Cell_Longitude = Cell_Longitude;
 
 
-                                    decimal Down_Volume ;
+                                    decimal Down_Volume;
                                     if (decimal.TryParse(i.Substring(598, 10).Trim(), out Down_Volume))
                                         cdr.DownVolume = Down_Volume;
 
@@ -132,7 +132,7 @@ namespace Vanrise.Fzero.CDRImport.BP.Activities
                             handle.SharedInstanceData.WriteTrackingMessage(BusinessProcess.Entities.BPTrackingSeverity.Information, "Start ImportCDRs.DoWork.Enqueued {0}", DateTime.Now);
                             sftp.Rename(filePath, newFilePath);
                             handle.SharedInstanceData.WriteTrackingMessage(BusinessProcess.Entities.BPTrackingSeverity.Information, "Start ImportCDRs.DoWork.Renamed {0}", DateTime.Now);
-                            break;
+
                         }
                     }
                 }
@@ -140,6 +140,32 @@ namespace Vanrise.Fzero.CDRImport.BP.Activities
             }
             handle.SharedInstanceData.WriteTrackingMessage(BusinessProcess.Entities.BPTrackingSeverity.Information, "End ImportCDRs.DoWork {0}", DateTime.Now);
         }
+
+        //protected override void DoWork(ImportCDRsInput inputArgument, AsyncActivityHandle handle)
+        //{
+        //    string SFTP_Dir = System.Configuration.ConfigurationManager.AppSettings["SFTP_Dir"].ToString();
+        //    var sftp = new Rebex.Net.Sftp();
+        //    sftp.Connect(System.Configuration.ConfigurationManager.AppSettings["SERVER"].ToString());
+        //    sftp.Login(System.Configuration.ConfigurationManager.AppSettings["Server_Username"].ToString(), System.Configuration.ConfigurationManager.AppSettings["Server_Pasword"].ToString());
+        //    if (sftp.GetConnectionState().Connected)
+        //    {
+        //        sftp.ChangeDirectory(SFTP_Dir);
+        //        SftpItemCollection currentItems = sftp.GetList();
+        //        if (currentItems.Count > 0)
+        //        {
+        //            foreach (var fileObj in currentItems)
+        //            {
+
+        //                String filePath = SFTP_Dir + "/" + fileObj.Name;
+        //                String newFilePath = SFTP_Dir + "/" + fileObj.Name.Replace(".old", ".DAT");
+        //                try { sftp.Rename(filePath, newFilePath); }
+        //                catch { }
+
+        //            }
+        //        }
+        //        sftp.Disconnect();
+        //    }
+        //}
 
         protected override ImportCDRsInput GetInputArgument(System.Activities.AsyncCodeActivityContext context)
         {
--- CDRAnalysis/Vanrise.Fzero.CDRImport.Data/CDRDataManagerFactory.cs
+++ CDRAnalysis/Vanrise.Fzero.CDRImport.Data/CDRDataManagerFactory.cs
@@ -13,7 +13,7 @@ namespace Vanrise.Fzero.CDRImport.Data
         static ObjectFactory s_objectFactory;
         static CDRDataManagerFactory()
         {
-            s_objectFactory = new ObjectFactory(Assembly.Load("Vanrise.Fzero.CDRImport.Data.MYSQL"));
+            s_objectFactory = new ObjectFactory(Assembly.Load("Vanrise.Fzero.CDRImport.Data.SQL"));
         }
 
         public static T GetDataManager<T>() where T : class, IDataManager
