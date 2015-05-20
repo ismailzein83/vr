417f4674b6bad79454693d95f15fe455136960c6
rayan.kazma <rayan.kazma@vanrise.tfs.local>
2015-05-20 10:55:02 +0000

    adding this class for hassan ali since the project is checked out by my side
    git-tfs-id: [http://developmentvlan:8585/tfs/vanrise.collection]$/;C23063

---
+++ TOneV2/Code/TOneV2/TOne.BusinessEntity.Entities/Switch.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TOne.BusinessEntity.Entities
+{
+    public class Switch
+    {
+        public int SwitchId { get; set; }
+
+        public string Name { get; set; }
+        public string Symbol { get; set; }
+        public string SwitchManagerName { get; set; }
+        public virtual string Description { get; set; }
+        public bool Enable_CDR_Import { get; set; }
+        public bool Enable_Routing { get; set; }
+        public DateTime? LastImport { get; set; }
+        public DateTime? LastAttempt { get; set; }
+        public DateTime? LastRouteUpdate { get; set; }
+        public string LastCDRImportTag { get; set; }
+    }
+}
