d074725376c46d641e1c60ca9b1a3b1e6ce4e690
hassan.ali <hassan.ali@vanrise.tfs.local>
2015-05-20 10:53:19 +0000

    adding new properties
    git-tfs-id: [http://developmentvlan:8585/tfs/vanrise.collection]$/;C23062

--- TOneV2/Code/TOneV2/TOne.BusinessEntity.Entities/SwitchInfo.cs
+++ TOneV2/Code/TOneV2/TOne.BusinessEntity.Entities/SwitchInfo.cs
@@ -11,5 +11,14 @@ namespace TOne.BusinessEntity.Entities
         public int SwitchId { get; set; }
 
         public string Name { get; set; }
+        public string Symbol { get; set; }
+        public string SwitchManagerName { get; set; }
+        public virtual string Description { get; set; }
+        public bool Enable_CDR_Import { get; set; }
+        public bool Enable_Routing { get; set; }
+        public DateTime? LastImport { get; set; }
+        public DateTime? LastAttempt { get; set; }
+        public DateTime? LastRouteUpdate { get; set; }
+        public string LastCDRImportTag { get; set; }
     }
 }
