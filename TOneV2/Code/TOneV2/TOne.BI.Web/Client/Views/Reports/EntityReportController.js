97305ea1217bb87ff821f45e6ea3223e56bedc6f
Ali Ballouk <VANRISE\ali.ballouk>
2015-05-26 14:58:52 +0000

    Begin IHM Implementation
    git-tfs-id: [http://developmentvlan:8585/tfs/vanrise.collection]$/;C23186

--- TOneV2/Code/TOneV2/TOne.BI.Web/Client/Views/Reports/EntityReportController.js
+++ TOneV2/Code/TOneV2/TOne.BI.Web/Client/Views/Reports/EntityReportController.js
@@ -1,13 +1,19 @@
-﻿EntityReportController.$inject = ['$scope', 'UtilsService', 'BITimeDimensionTypeEnum', 'BIEntityTypeEnum', 'BIMeasureTypeEnum', 'VRModalService'];
+﻿EntityReportController.$inject = ['$scope', 'UtilsService', 'BITimeDimensionTypeEnum', 'BIEntityTypeEnum', 'BIMeasureTypeEnum'];
 
-function EntityReportController($scope, UtilsService, BITimeDimensionTypeEnum, BIEntityTypeEnum, BIMeasureTypeEnum, VRModalService) {
+function EntityReportController($scope, UtilsService, BITimeDimensionTypeEnum, BIEntityTypeEnum, BIMeasureTypeEnum) {
 
         defineScope();
         load();
 
         function defineScope() {
+            defineEntityTypes()
+            defineEntity()
 
-          
+            $scope.onchangeEntityType = function () {
+                
+                console.log($scope.selectedEntityType)
+
+            }
         }
 
         function load() {
@@ -15,6 +21,20 @@ function EntityReportController($scope, UtilsService, BITimeDimensionTypeEnum, B
         }
 
         
+        function defineEntityTypes() {
+            $scope.entityTypes = [];
+            for (var e in BIEntityTypeEnum) {
+                $scope.entityTypes.push(BIEntityTypeEnum[e]);
+            }
+
+            $scope.selectedEntityType = $scope.entityTypes[0];
+        }
+        function defineEntity() {
+            $scope.entitiesData = [
+            {value:1 , label: "test"}
+            ];
+            $scope.selectedEntity = $scope.entitiesData[0];
+        }
 
 }
 
