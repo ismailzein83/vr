66d67e003ae7be6075d3b06356284b50be35cb35
rabih.fashwal:2 <rabih.fashwal:2@vanrise.tfs.local>
2015-05-27 08:07:50 +0000

    RouteManager
    git-tfs-id: [http://developmentvlan:8585/tfs/vanrise.collection]$/;C23196

---
+++ TOneV2/Code/TOneV2/TOne.LCR.Entities/Enumerations.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TOne.LCR.Entities
+{
+    public enum RouteDetailFilterOrder
+    {
+        CustomerId = 0,
+        Code = 1,
+        Zone = 2,
+        Rate = 3,
+        ServiceFlag = 4
+    }
+}
---
+++ TOneV2/Code/TOneV2/TOne.LCR.Entities/Routing/RouteDetailFilter.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TOne.LCR.Entities.Routing
+{
+    public class RouteDetailFilter
+    {
+        public List<string> CustomerIds { get; set; }
+        public List<int> ZoneIds { get; set; }
+        public List<string> Code { get; set; }
+    }
+}
