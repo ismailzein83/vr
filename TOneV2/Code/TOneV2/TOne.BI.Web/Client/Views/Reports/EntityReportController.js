f72af2d4007a94b446b71a89ef57c99cc871fd4c
Ali Ballouk <VANRISE\ali.ballouk>
2015-05-27 14:12:29 +0000

    EntityReport grid implementation
    git-tfs-id: [http://developmentvlan:8585/tfs/vanrise.collection]$/;C23224

--- TOneV2/Code/TOneV2/TOne.BI.Web/Client/Views/Reports/EntityReportController.js
+++ TOneV2/Code/TOneV2/TOne.BI.Web/Client/Views/Reports/EntityReportController.js
@@ -1,40 +1,70 @@
-﻿EntityReportController.$inject = ['$scope', 'UtilsService', 'BITimeDimensionTypeEnum', 'BIEntityTypeEnum', 'BIMeasureTypeEnum'];
+﻿EntityReportController.$inject = ['$scope', 'UtilsService', 'BITimeDimensionTypeEnum', 'BIEntityTypeEnum', 'BIMeasureTypeEnum',  'BIAPIService', 'BIUtilitiesService'];
 
-function EntityReportController($scope, UtilsService, BITimeDimensionTypeEnum, BIEntityTypeEnum, BIMeasureTypeEnum) {
+function EntityReportController($scope, UtilsService, BITimeDimensionTypeEnum, BIEntityTypeEnum, BIMeasureTypeEnum, BIAPIService, BIUtilitiesService) {
 
         defineScope();
+        var params = {}
         load();
-
+       
         function defineScope() {
-            defineEntityTypes()
-            defineEntity()
+            $scope.fromDate = "2015-04-01";
+            $scope.data = [];
+            $scope.toDate = "2015-04-30";
+            defineMeasureTypes();
+            
+            
 
-            $scope.onchangeEntityType = function () {
-                
-                console.log($scope.selectedEntityType)
+            defineTimeDimensionTypes();
+
+
+            $scope.onchangeEntityType = function () {                
+                loadEntityData()
+
+            }
+            $scope.search = function () {
+                var measureTypes = [];
+                for (var prop in BIMeasureTypeEnum) {
+                    measureTypes.push(BIMeasureTypeEnum.value);
+                }
+                return BIAPIService.GetEntityMeasuresValues(params.EntityType, params.EntityValue, $scope.selectedTimeDimensionType.value, $scope.fromDate, $scope.toDate, measureTypes)
+                 .then(function (response) {
+
+                     $scope.data.length = 0;
+                     BIUtilitiesService.fillDateTimeProperties(response, $scope.selectedTimeDimensionType.value, $scope.fromDate, $scope.toDate, true);
+                     angular.forEach(response, function (itm) {
+                         $scope.data.push(itm);
+                     });
+
+                 }).catch(function (error) {
+
+                 });
 
             }
         }
 
         function load() {
-           
+            params.EntityType = 0;
+            params.EntityValue = 27708;
         }
-
-        
-        function defineEntityTypes() {
-            $scope.entityTypes = [];
-            for (var e in BIEntityTypeEnum) {
-                $scope.entityTypes.push(BIEntityTypeEnum[e]);
+        function defineMeasureTypes() {
+            $scope.measureTypes = [];
+            for (var m in BIMeasureTypeEnum) {
+                $scope.measureTypes.push(BIMeasureTypeEnum[m]);
             }
 
-            $scope.selectedEntityType = $scope.entityTypes[0];
+            $scope.selectedMeasureTypes = [];
         }
-        function defineEntity() {
-            $scope.entitiesData = [
-            {value:1 , label: "test"}
-            ];
-            $scope.selectedEntity = $scope.entitiesData[0];
+
+        function defineTimeDimensionTypes() {
+            $scope.timeDimensionTypes = [];
+            for (var td in BITimeDimensionTypeEnum)
+                $scope.timeDimensionTypes.push(BITimeDimensionTypeEnum[td]);
+
+            $scope.selectedTimeDimensionType = $.grep($scope.timeDimensionTypes, function (t) {
+                return t == BITimeDimensionTypeEnum.Daily;
+            })[0];
         }
+        
 
 }
 
