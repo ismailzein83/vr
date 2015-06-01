c7815d4ee81af2fceb3570da8c71bbafda3eec14
sara.yahfoufi <sara.yahfoufi@vanrise.tfs.local>
2015-06-01 15:11:23 +0000

    GetBillingStatistics function
    git-tfs-id: [http://developmentvlan:8585/tfs/vanrise.collection]$/;C23310

--- TOneV2/Code/TOneV2/TOne.Analytics.Web/Controllers/BillingStatisticsController.cs
+++ TOneV2/Code/TOneV2/TOne.Analytics.Web/Controllers/BillingStatisticsController.cs
@@ -1,14 +1,43 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.Web;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using TOne.Analytics.Business;
+using TOne.Analytics.Entities;
+
 
 namespace TOne.Analytics.Web.Controllers
 {
+    
     public class BillingStatisticsController : Vanrise.Web.Base.BaseAPIController
     {
-        public string GetTest(string name) {
-            return "Sara"+name;
+        private readonly BillingStatisticManager __billingStatisticsManager;
+
+        public BillingStatisticsController() {
+
+            __billingStatisticsManager = new BillingStatisticManager();
+        }
+
+        public List<string>  GetTest(string name) {
+            List<string> l = new List<string>();
+            l.Add("TEST 1" + name);
+            l.Add("TEST 2" + name);
+            l.Add("TEST 3" + name);
+            l.Add("TEST 4" + name);
+            return l ;
+        }
+       
+        public List<ZoneProfitFormatted> GetZoneProfit(DateTime date1,DateTime date2) {
+            return __billingStatisticsManager.GetZoneProfit(date1,date2, "Y");
+        }
+
+        [HttpGet]
+        public List<BillingStatistic> GetBillingStatistics(DateTime fromDate, DateTime toDate)
+        {
+
+            return __billingStatisticsManager.GetBillingStatistics(fromDate,toDate);
         }
     }
 }
