04b6befe4a297b86d57db478c67989b0c829d283
sara.yahfoufi <sara.yahfoufi@vanrise.tfs.local>
2015-06-01 15:11:11 +0000

    GetBillingStatistics function
    git-tfs-id: [http://developmentvlan:8585/tfs/vanrise.collection]$/;C23309

--- TOneV2/Code/TOneV2/TOne.Analytics.Web/Analytics/Views/Billing Statistics/VariationReportsController.js
+++ TOneV2/Code/TOneV2/TOne.Analytics.Web/Analytics/Views/Billing Statistics/VariationReportsController.js
@@ -1,26 +1,18 @@
 ﻿VariationReportsController.$inject = ['$scope', 'BillingStatisticsAPIService'];
 
 function VariationReportsController($scope, BillingStatisticsAPIService) {
-
-    var mainGridapi;
-    var sortColumn;
-    var sortDescending = true;
-
-    $scope.data = [
-        { name: 'Jani', country: 'Norway' },
-        { name: 'Hege', country: 'Sweden' },
-        { name: 'Kai', country: 'Denmark' }
-    ];
-    $scope.fromDate = '2015/05/28';
-
-    //loadParameters();
+    $scope.name = "test";
+    $scope.isInitializing = false;
+    
+    var mainGridAPI;
+    $scope.data = [];
+    $scope.PeriodType = ['Days','Weeks','Months']
+   // $scope.fromDate = '2014/12/01';
+  //  $scope.toDate = '2015/05/01'
     defineScope();
-    load();
-
-    //function loadParameters() { }
+   
     function defineScope() {
-
-        $scope.testModel = 'VariationReportsController';
+       
         $scope.mainGridPagerSettings = {
             currentPage: 1,
             totalDataCount: 0,
@@ -31,93 +23,54 @@ function VariationReportsController($scope, BillingStatisticsAPIService) {
         $scope.onMainGridReady = function (api) {
             mainGridAPI = api;
         }
+  
         $scope.getData = function () {
-            alert('$scope.getData ');
-            //$scope.mainGridPagerSettings.currentPage = 1;
-            //resultKey = null;
-            //mainGridAPI.resetSorting();
-            //resetSorting();
-            return getData(true);
+            $scope.mainGridPagerSettings.currentPage = 1;
+            resultKey = null;
+        //    mainGridAPI.resetSorting();
+        //    resetSorting();
+          //  return getData(true);
+           return getData(true);
         };
+       
+        $scope.getZoneProfit = getZoneProfit;
+        $scope.getBillingStats = getBillingStats;
+        
     }
-    function load() { }
-    function resetSorting() {
-        sortColumn = TrafficStatisticsMeasureEnum.Attempts;
-        sortDescending = true;
-    }
+   
+
     function getData(withSummary) {
-        alert($scope.name);
-        if ($scope.name == undefined)
-            $scope.name = "test";
-        BillingStatisticsAPIService.GetTest($scope.name).then(function (response) {
-            alert(response);
+        // alert($scope.name);
+        //if ($scope.name == undefined)
+        //    $scope.name = "test";
+        $scope.isInitializing = true;
+          BillingStatisticsAPIService.GetTest($scope.name).then(function (response) {
+              alert(response);
+              $scope.isInitializing = false;
+          });
+
+    }
+    function getZoneProfit() {
+        BillingStatisticsAPIService.GetZoneProfit($scope.fromDate,$scope.toDate).then(function (response) {
+            //  alert(response);
+            console.log(response);
+            $scope.isInitializing = false;
+            angular.forEach(response, function (itm) {
+                $scope.data.push(itm);
+            });
+
         });
-        //    if (!chartSelectedMeasureAPI)
-        //        return;
-        //    if (sortColumn == undefined)
-        //        return;
-        //    if (withSummary == undefined)
-        //        withSummary = false;
-
-
-        //    if (chartSelectedEntityAPI)
-        //        chartSelectedEntityAPI.hideChart();
-
-        //    var count = $scope.mainGridPagerSettings.itemsPerPage;
-        //    var groupKeys = [];
-
-        //    angular.forEach($scope.selectedGroupKeys, function (group) {
-        //        groupKeys.push(group.groupKeyEnumValue);
-        //    });
-        //    var pageInfo = $scope.mainGridPagerSettings.getPageInfo();
-        //    var filter = buildFilter();
-        //    var getTrafficStatisticSummaryInput = {
-        //        TempTableKey: resultKey,
-        //        Filter: filter,
-        //        WithSummary: withSummary,
-        //        GroupKeys: groupKeys,
-        //        From: $scope.fromDate,
-        //        To: $scope.toDate,
-        //        FromRow: pageInfo.fromRow,
-        //        ToRow: pageInfo.toRow,
-        //        OrderBy: sortColumn.value,
-        //        IsDescending: sortDescending
-        //    };
-        //    var isSucceeded;
-        //    $scope.showResult = true;
-        //    $scope.isGettingData = true;
-        //    $scope.data.length = 0;
-        //    $scope.currentSearchCriteria.groupKeys.length = 0;
-        //    angular.forEach($scope.selectedGroupKeys, function (group) {
-        //        $scope.currentSearchCriteria.groupKeys.push(group);
-        //    });
-
-        //    return AnalyticsAPIService.GetTrafficStatisticSummary(getTrafficStatisticSummaryInput).then(function (response) {
-
-        //        currentData = response.Data;
-        //        if (currentSortedColDef != undefined)
-        //            currentSortedColDef.currentSorting = sortDescending ? 'DESC' : 'ASC';
-
-        //        resultKey = response.ResultKey;
-        //        $scope.mainGridPagerSettings.totalDataCount = response.TotalCount;
-        //        if (withSummary) {
-        //            $scope.trafficStatisticSummary = response.Summary;
-        //            $scope.overallData[0] = response.Summary;
-        //        }
-        //        angular.forEach(response.Data, function (itm) {
-        //            $scope.data.push(itm);
-        //        });
-        //        renderOverallChart();
-        //        isSucceeded = true;
-        //    })
-        //        .finally(function () {
-        //            $scope.isGettingData = false;
-        //        });
-        //}
+    }
+    function getBillingStats() {
+        $scope.isInitializing = true;
+        BillingStatisticsAPIService.GetBillingStatistics($scope.fromDate, $scope.toDate).then(function (response) {
+            console.log(response);
+            $scope.isInitializing = false;
+            angular.forEach(response, function (itm) { $scope.data.push(itm);});
+        });
     }
 
 };
-
 
 
 appControllers.controller('Analytics_VariationReportsController', VariationReportsController);
