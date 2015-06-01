84d3629116f9fb47f5bfd0a9dd0e58c4d021def9
sara.yahfoufi <sara.yahfoufi@vanrise.tfs.local>
2015-06-01 15:10:49 +0000

    GetBillingStatistics
    git-tfs-id: [http://developmentvlan:8585/tfs/vanrise.collection]$/;C23307

--- TOneV2/Code/TOneV2/TOne.Analytics.Web/Analytics/Services/APIServices/BillingStatisticsAPIService.js
+++ TOneV2/Code/TOneV2/TOne.Analytics.Web/Analytics/Services/APIServices/BillingStatisticsAPIService.js
@@ -2,7 +2,9 @@
 app.service('BillingStatisticsAPIService', function (BaseAPIService) {
 
     return ({
-        GetTest: GetTest
+        GetTest: GetTest,
+        GetZoneProfit: GetZoneProfit,
+        GetBillingStatistics: GetBillingStatistics
     });
 
     function GetTest(name){
@@ -12,5 +14,25 @@ app.service('BillingStatisticsAPIService', function (BaseAPIService) {
             }
            );
     }
+        function GetZoneProfit(date1,date2) {
+            return BaseAPIService.get("/api/BillingStatistics/GetZoneProfit",
+            {
+                date1: date1,
+                date2: date2
+            }
+            );
+        }
+
+        function GetBillingStatistics(fromDate, toDate) {
+            return BaseAPIService.get("/api/BillingStatistics/GetBillingStatistics",
+            {
+                fromDate: fromDate,
+                toDate: toDate
+            }
+            );
+        }
+
+
+    
 
 });
