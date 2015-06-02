75f01158f1832e2b68441c7ba688f3854503159d
walid.mogharbel <walid.mogharbel@vanrise.tfs.local>
2015-06-02 11:34:29 +0000

    git-tfs-id: [http://developmentvlan:8585/tfs/vanrise.collection]$/;C23334

--- CDRAnalysis/Vanrise.Fzero.FraudAnalysis.Data.SQL/StrategyDataManager.cs
+++ CDRAnalysis/Vanrise.Fzero.FraudAnalysis.Data.SQL/StrategyDataManager.cs
@@ -17,7 +17,7 @@ namespace Vanrise.Fzero.FraudAnalysis.Data.SQL
 
         public Strategy GetStrategy(int strategyId)
         {
-            string query0 = "SELECT Id, Name, Description, IsDefault, StrategyContent FROM Strategy WHERE Id = @StrategyId";
+            string query0 = "SELECT StrategyContent FROM Strategy WHERE Id = @StrategyId";
             string query = "SELECT MaxValue, CriteriaID FROM StrategyThreshold Where StrategyId = @StrategyId";
             string query1 = "SELECT PeriodId, Value, CriteriaID FROM StrategyPeriods Where StrategyId = @StrategyId";
             string query2 = "SELECT LevelId, CriteriaId1, Cr1Per ,  CriteriaId2 ,  Cr2Per ,  CriteriaId3 ,  Cr3Per ,  CriteriaId4 ,  Cr4Per ,  CriteriaId5 ,  Cr5Per ,  CriteriaId6 ,  Cr6Per ,  CriteriaId7 ,  Cr7Per ,  CriteriaId8 ,  Cr8Per ,  CriteriaId9 ,  Cr9Per ,  CriteriaId10 ,  Cr10Per ,  CriteriaId11 ,  Cr11Per ,  CriteriaId12 ,  Cr12Per ,  CriteriaId13 ,  Cr13Per ,  CriteriaId14 ,  Cr14Per ,  CriteriaId15 ,  Cr15Per,  CriteriaId16 ,  Cr16Per   FROM  Strategy_Suspicion_Level Where StrategyId = @StrategyId and LevelId<>1  ";
@@ -100,7 +100,7 @@ namespace Vanrise.Fzero.FraudAnalysis.Data.SQL
 
         public List<Strategy> GetAllStrategies()
         {
-            string query_GetStrategies = @"SELECT Id, Name, Description FROM Strategy; ";
+            string query_GetStrategies = @"SELECT StrategyContent  FROM Strategy; ";
             List<Strategy> strategies = new List<Strategy>();
 
 
@@ -108,11 +108,7 @@ namespace Vanrise.Fzero.FraudAnalysis.Data.SQL
             {
                 while (reader.Read())
                 {
-                    Strategy strategy = new Strategy();
-                    strategy.Id = (int)reader["Id"];
-                    strategy.Name = reader["Name"] as string;
-                    strategy.Description = reader["Description"] as string;
-                    strategies.Add(strategy);
+                    strategies.Add(Vanrise.Common.Serializer.Deserialize<Strategy>(GetReaderValue<string>(reader, "StrategyContent")));
                 }
              }, null);
             return strategies;
@@ -120,7 +116,7 @@ namespace Vanrise.Fzero.FraudAnalysis.Data.SQL
 
         public List<Strategy> GetFilteredStrategies(int fromRow, int toRow, string name, string description)
         {
-            string query_GetStrategies = @"SELECT Id, Name, Description, IsDefault FROM Strategy where Name like @name and Description like @description ; ";
+            string query_GetStrategies = @"SELECT  StrategyContent  FROM Strategy where Name like @name and Description like @description ; ";
             List<Strategy> strategies = new List<Strategy>();
 
 
@@ -128,12 +124,7 @@ namespace Vanrise.Fzero.FraudAnalysis.Data.SQL
             {
                 while (reader.Read())
                 {
-                    Strategy strategy = new Strategy();
-                    strategy.Id = (int)reader["Id"];
-                    strategy.Name = reader["Name"] as string;
-                    strategy.Description = reader["Description"] as string;
-                    strategy.IsDefault = GetReaderValue<bool>(reader, "IsDefault"); 
-                    strategies.Add(strategy);
+                    strategies.Add(Vanrise.Common.Serializer.Deserialize<Strategy>(GetReaderValue<string>(reader, "StrategyContent")));
                 }
             }
             , (cmd) =>
@@ -154,12 +145,9 @@ namespace Vanrise.Fzero.FraudAnalysis.Data.SQL
 
         public bool AddStrategy(Strategy strategyObject)
         {
-            string query = "INSERT INTO Strategy(Name, Description, IsDefault, StrategyContent) values(@Name, @Description, @IsDefault, @StrategyContent)";
+            string query = "INSERT INTO Strategy(StrategyContent) values(@StrategyContent)";
 
             ExecuteNonQueryText(query,  (cmd) =>            {
-                cmd.Parameters.Add(new SqlParameter() { ParameterName = "@Name", Value = strategyObject.Name });
-                cmd.Parameters.Add(new SqlParameter() { ParameterName = "@Description", Value = strategyObject.Description });
-                cmd.Parameters.Add(new SqlParameter() { ParameterName = "@IsDefault", Value = strategyObject.IsDefault });
                 cmd.Parameters.Add(new SqlParameter() { ParameterName = "@StrategyContent", Value = Vanrise.Common.Serializer.Serialize(strategyObject) });
             });
 
@@ -175,14 +163,11 @@ namespace Vanrise.Fzero.FraudAnalysis.Data.SQL
 
         public bool UpdateStrategy(Strategy strategyObject)
         {
-            string query = "Update Strategy set Name=@Name, Description=@Description, IsDefault=@IsDefault, StrategyContent=@StrategyContent where Id=@Id ";
+            string query = "Update Strategy set StrategyContent=@StrategyContent where Id=@Id ";
 
             ExecuteNonQueryText(query, (cmd) =>
             {
                 cmd.Parameters.Add(new SqlParameter() { ParameterName = "@Id", Value = strategyObject.Id });
-                cmd.Parameters.Add(new SqlParameter() { ParameterName = "@Name", Value = strategyObject.Name });
-                cmd.Parameters.Add(new SqlParameter() { ParameterName = "@Description", Value = strategyObject.Description });
-                cmd.Parameters.Add(new SqlParameter() { ParameterName = "@IsDefault", Value = strategyObject.IsDefault });
                 cmd.Parameters.Add(new SqlParameter() { ParameterName = "@StrategyContent", Value = Vanrise.Common.Serializer.Serialize(strategyObject) });
             });
             return true;
