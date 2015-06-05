5d0c9f07145f4ee5de43b207f23775940d9e7e65
Ismail Zein <VANRISE\ismail.zein>
2015-06-05 08:22:32 +0000

    added dynamic routing
    git-tfs-id: [http://developmentvlan:8585/tfs/vanrise.collection]$/;C23426

--- Vanrise/Code/Vanrise.Web/Client/Javascripts/Modules/appRouting.js
+++ Vanrise/Code/Vanrise.Web/Client/Javascripts/Modules/appRouting.js
@@ -1,6 +1,37 @@
 ﻿var appRouting = angular.module('appRouting', ['ngRoute']);
 appRouting.config(['$routeProvider',
   function ($routeProvider) {
+      var folderPart = '';
+      for (var i = 1; i <= 10; i++) {
+          
+          $routeProvider.
+                when('/view/:module' + folderPart + '/:pagename', {
+                    templateUrl: function (params) {
+                        return getTemplateURLFromParams(params);
+                    }
+                }).
+                when('/viewwithparams/:module' + folderPart + '/:pagename/:params', {
+                    templateUrl: function (params) {
+                        return getTemplateURLFromParams(params);
+                    }
+                });
+          folderPart += '/:folder' + i;
+      }
+
+      function getTemplateURLFromParams(params) {
+          var templateURL = '/Client/Modules/' + params.module;
+          var folder = '';
+          var current = 1;
+          while (folder != undefined) {
+              folder = params['folder' + current];
+              if (folder != undefined)
+                  templateURL += '/' + folder;
+              current++;
+          }
+          templateURL += '/' + params.pagename + '.html';
+          console.log(templateURL);
+          return templateURL;
+      }
       $routeProvider.
           when('/Error/:params', {
               templateUrl: '/Client/Modules/Common/Views/Error.html'
