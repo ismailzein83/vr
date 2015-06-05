fd2224ec093c8f71c9d4a74c0c29f52a0cd8b482
mustafa.hawi <mustafa.hawi@vanrise.tfs.local>
2015-06-05 08:41:24 +0000

    Add Generate Statistics WF
    git-tfs-id: [http://developmentvlan:8585/tfs/vanrise.collection]$/;C23428

---
+++ TOneV2/Code/TOneV2/TOne.CDRProcess.Arguments/GenerateStatisticsProcessInput.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TOne.CDRProcess.Arguments
+{
+    public class GenerateStatisticsProcessInput
+    {
+        public int SwitchID { get; set; }
+    }
+}
