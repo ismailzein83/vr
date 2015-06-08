f5b44661ef2466b90edae811595eff60ccbc93b1
Samer Haidar <VANRISE\samer.haidar>
2015-06-08 10:17:42 +0000

    Implementing temptable
    git-tfs-id: [http://developmentvlan:8585/tfs/vanrise.collection]$/;C23480

--- TOneV2/Code/TOneV2/TOne.Analytics.Data.SQL/CDRDataManager.cs
+++ TOneV2/Code/TOneV2/TOne.Analytics.Data.SQL/CDRDataManager.cs
@@ -8,12 +8,15 @@ using TOne.Analytics.Entities;
 using TOne.Analytics.Data;
 using System.Data;
 using System.Data.SqlClient;
+using TOne.Entities;
 namespace TOne.Analytics.Data.SQL
 {
     class CDRDataManager : BaseTOneDataManager, ICDRDataManager
     {
-        public List<CDR> GetCDRData(CDRFilter filter,DateTime fromDate, DateTime toDate, int nRecords, string CDROption)
+        public CDRBigResult GetCDRData(string tempTableKey, CDRFilter filter, DateTime fromDate, DateTime toDate, int fromRow, int toRow, int nRecords, string CDROption,BillingCDRMeasures orderBy, bool isDescending)
         {
+            StringBuilder whereBuilder = new StringBuilder();
+
             string queryData = "";
             if (CDROption == "Successful")
             {
@@ -27,20 +30,85 @@ namespace TOne.Analytics.Data.SQL
             {
                 queryData = String.Format(@"{0} UNION ALL {1}", GetSingleQuery("Billing_CDR_Main", "MainCDR"), GetSingleQuery("Billing_CDR_Invalid", "Invalid"));
             }
-            string query="";
+            //string query="";
+           
+            TempTableName tempTableName = null;
+            if (tempTableKey != null)
+                tempTableName = GetTempTableName(tempTableKey);
+            else
+                tempTableName = GenerateTempTableName();
+            CDRBigResult rslt = new CDRBigResult()
+            {
+                ResultKey = tempTableName.Key
+            };
 
+            CreateTempTableIfNotExists(tempTableName.TableName, filter, queryData, fromDate, toDate, nRecords);
 
-            query = String.Format(@"SELECT Top (@nRecords) * FROM ({0})as newtable where Attempt between @FromDate AND @ToDate ORDER BY Attempt;", queryData);
-                return GetItemsText(query, CDRDataMapper,
-                    (cmd) =>
-                    {
-                        cmd.Parameters.Add(new SqlParameter("@FromDate", fromDate));
-                        cmd.Parameters.Add(new SqlParameter("@ToDate", toDate));
-                        cmd.Parameters.Add(new SqlParameter("@nRecords", nRecords));
-                    });
+            int totalDataCount;
+            rslt.Data = GetData(tempTableName.TableName, fromRow, toRow, out totalDataCount);
+            rslt.TotalCount = totalDataCount;
+            return rslt;
             
         }
+
+        private void CreateTempTableIfNotExists(string tempTableName, CDRFilter filter, String queryData, DateTime from, DateTime to, int nRecords)
+        {
+            StringBuilder whereBuilder = new StringBuilder();
+            StringBuilder queryBuilder = new StringBuilder(@"
+                            IF NOT OBJECT_ID('#TEMPTABLE#', N'U') IS NOT NULL
+	                            BEGIN
+
+                            WITH
+                            
+		                        AllResult AS
+		                        (
+			                        SELECT Top (@nRecords) * FROM (#0#)as newtable where Attempt between @FromDate AND @ToDate #FILTER#
+		                        )
+		                        SELECT * INTO #TEMPTABLE# FROM AllResult
+                            END");
+            queryBuilder.Replace("#TEMPTABLE#", tempTableName);
+            queryBuilder.Replace("#0#", queryData);
+            AddFilterToQuery(filter, whereBuilder);
+            queryBuilder.Replace("#FILTER#", whereBuilder.ToString());
+
+            ExecuteNonQueryText(queryBuilder.ToString(), (cmd) =>
+            {
+                cmd.Parameters.Add(new SqlParameter("@FromDate", from));
+                cmd.Parameters.Add(new SqlParameter("@ToDate", to));
+                cmd.Parameters.Add(new SqlParameter("@nRecords", nRecords));
+            });
+        }
+
+        private IEnumerable<BillingCDR> GetData(string tempTableName, int fromRow, int toRow, out int totalCount)
+        {
+            string query = String.Format(@"WITH OrderedResult AS (SELECT *, ROW_NUMBER()  OVER(ORDER BY Attempt DESC)  AS rowNumber FROM {0})
+	                                    SELECT * FROM OrderedResult WHERE rowNumber between @FromRow AND @ToRow", tempTableName);
+
+            totalCount = (int)ExecuteScalarText(String.Format("SELECT COUNT(*) FROM {0}", tempTableName), null);
+            return GetItemsText(query,
+                (reader) =>
+                {
+                    var obj = new BillingCDR();
+                    FillCDRDataFromReader(obj, reader);
+                    return obj;
+                },
+                (cmd) =>
+                {
+                    cmd.Parameters.Add(new SqlParameter("@FromRow", fromRow));
+                    cmd.Parameters.Add(new SqlParameter("@ToRow", toRow));
+                });
+        }
+        private string GetColumnName(BillingCDRMeasures column)
+        {
+            switch (column)
+            {
+                //case BillingCDRMeasures.DurationsInMinutes: return "DurationsInSeconds";
+                //case BillingCDRMeasures.MaxDurationInMinutes: return "MaxDurationsInSeconds";
+                default: return column.ToString();
+            }
+        }
         private string GetSingleQuery(string tableName,string alias){
+            StringBuilder whereBuilder = new StringBuilder();
             return String.Format(@"
                         SELECT 
                         {1}.ID,
@@ -72,14 +140,33 @@ namespace TOne.Analytics.Data.SQL
                             ", tableName, alias);
                         }
         
-        private CDR CDRDataMapper(IDataReader reader)
+        private BillingCDR CDRDataMapper(IDataReader reader)
         {
 
-            CDR instance = new CDR();       
+            BillingCDR instance = new BillingCDR();       
             FillCDRDataFromReader(instance, reader);
             return instance;
         }
-        void FillCDRDataFromReader(CDR cdr, IDataReader reader)
+        private void AddFilterToQuery(CDRFilter filter, StringBuilder whereBuilder)
+        {
+            AddFilter(whereBuilder, filter.SwitchIds, "newtable.SwitchId");
+            AddFilter(whereBuilder, filter.CustomerIds, "newtable.CustomerID");
+            AddFilter(whereBuilder, filter.SupplierIds, "newtable.SupplierID");
+            AddFilter(whereBuilder, filter.ZoneIds, "newtable.OurZoneID");
+        }
+
+        void AddFilter<T>(StringBuilder whereBuilder, IEnumerable<T> values, string column)
+        {
+            if (values != null && values.Count() > 0)
+            {
+                if (typeof(T) == typeof(string))
+                    whereBuilder.AppendFormat("AND {0} IN ('{1}')", column, String.Join("', '", values));
+                else
+                    whereBuilder.AppendFormat("AND {0} IN ({1})", column, String.Join(", ", values));
+            }
+        }
+
+        void FillCDRDataFromReader(BillingCDR cdr, IDataReader reader)
         {
                 cdr.ID = GetReaderValue<Int64>(reader, "ID");
                 cdr.Attempt = GetReaderValue<DateTime>(reader,"Attempt");
