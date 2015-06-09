dfe0bf08d0362c50c8a55555ed9027056db1b387
Ismail Zein <VANRISE\ismail.zein>
2015-06-09 11:19:59 +0000

    fixed RouteConfig
    git-tfs-id: [http://developmentvlan:8585/tfs/vanrise.collection]$/;C23519

--- Vanrise/Code/Vanrise.Web/App_Start/RouteConfig.cs
+++ Vanrise/Code/Vanrise.Web/App_Start/RouteConfig.cs
@@ -16,7 +16,7 @@ namespace Vanrise.Web
             routes.MapRoute(
                 name: "Default",
                 url: "{controller}/{action}/{id}",
-                defaults: new { controller = "Security", action = "Login", id = UrlParameter.Optional }
+                defaults: new { controller = "Home", action = "Index", id = UrlParameter.Optional }
             );
         }
     }
