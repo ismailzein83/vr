48d264791fd55b33b13e5363470bb855bbe1a597
mustafa.hawi <mustafa.hawi@vanrise.tfs.local>
2015-06-12 11:36:23 +0000

    Add store statistics to db activity
    git-tfs-id: [http://developmentvlan:8585/tfs/vanrise.collection]$/;C23660

---
+++ TOneV2/Code/TOneV2/TOne.CDRProcess.Activities/StoreStatisticsToDB.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Activities;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Vanrise.BusinessProcess;
+
+namespace TOne.CDRProcess.Activities
+{
+
+    #region Argument Classes
+    public class StoreStatisticsToDBInput
+    {
+        public int SwitchId { get; set; }
+    }
+
+    #endregion
+
+    public sealed class StoreStatisticsToDB : DependentAsyncActivity<StoreStatisticsToDBInput>
+    {
+
+        [RequiredArgument]
+        public InArgument<int> SwitchId { get; set; }
+
+        protected override void DoWork(StoreStatisticsToDBInput inputArgument, AsyncActivityStatus previousActivityStatus, AsyncActivityHandle handle)
+        {
+            throw new NotImplementedException();
+        }
+
+        protected override StoreStatisticsToDBInput GetInputArgument2(System.Activities.AsyncCodeActivityContext context)
+        {
+            return new StoreStatisticsToDBInput
+            {
+                SwitchId = this.SwitchId.Get(context),
+            };
+        }
+    }
+}
