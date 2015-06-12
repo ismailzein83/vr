11a894cb805aa2e76e433f585edb40586e79607c
hadi.hawi <hadi.hawi@vanrise.tfs.local>
2015-06-12 10:25:00 +0000

    DetailedCarrierSummary
    git-tfs-id: [http://developmentvlan:8585/tfs/vanrise.collection]$/;C23645

---
+++ TOneV2/Code/TOneV2/TOne.Analytics.Business/BillingReports/DetailedCarrierSummaryReportGenerator.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TOne.Analytics.Entities;
+using TOne.Entities;
+
+namespace TOne.Analytics.Business.BillingReports
+{
+    public class DetailedCarrierSummaryReportGenerator : TOne.Entities.IReportGenerator
+    {
+        public Dictionary<string, System.Collections.IEnumerable> GenerateDataSources(TOne.Entities.ReportParameters parameters)
+        {
+            BillingStatisticManager manager = new BillingStatisticManager();
+            List<DailySummaryFormatted> dailySummary = manager.GetDailySummary(parameters.FromTime, parameters.ToTime, parameters.SupplierAMUId, parameters.CustomerAMUId);
+
+            Dictionary<string, System.Collections.IEnumerable> dataSources = new Dictionary<string, System.Collections.IEnumerable>();
+            dataSources.Add("DailySummary", dailySummary);
+            return dataSources;
+        }
+
+        public Dictionary<string, RdlcParameter> GetRdlcReportParameters(TOne.Entities.ReportParameters parameters)
+        {
+
+            Dictionary<string, RdlcParameter> list = new Dictionary<string, RdlcParameter>();
+            //list.Add("GroupByCustomer", new RdlcParameter() { Value = parameters.GroupByCustomer.ToString(), IsVisible = false });
+            list.Add("FromDate", new RdlcParameter { Value = parameters.FromTime.ToString(), IsVisible = true });
+            list.Add("ToDate", new RdlcParameter { Value = parameters.ToTime.ToString(), IsVisible = true });
+            list.Add("Title", new RdlcParameter { Value = "Daily Summary", IsVisible = true });
+            list.Add("Currency", new RdlcParameter { Value = "[USD] United States Dollars", IsVisible = true });
+            list.Add("LogoPath", new RdlcParameter { Value = "logo", IsVisible = true });
+            //list.Add("Customer", new RdlcParameter { Value = "", IsVisible = true });
+            //list.Add("Supplier", new RdlcParameter { Value = "", IsVisible = true });
+            list.Add("DigitRate", new RdlcParameter { Value = "2", IsVisible = true });
+
+            return list;
+        }
+    }
+}
---
+++ TOneV2/Code/TOneV2/TOne.Analytics.Entities/Billing/DetailedCarrierSummary.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TOne.Analytics.Entities
+{
+    public class DetailedCarrierSummary
+    {
+        public DateTime CallDate { get; set; }
+        public string CustomerID { get; set; }
+        public string SupplierID { get; set; }
+        public int CostZoneID { get; set; }
+        public int SaleZoneID { get; set; }
+        public string Cost_Currency { get; set; }
+        public string Sale_Currency { get; set; }
+        public double Cost_Nets { get; set; }
+        public double Sale_Nets { get; set; }
+        public double Sale_Rate { get; set; }
+        public double Cost_Rate { get; set; }
+        public decimal SaleDuration { get; set; }
+        public decimal CostDuration { get; set; }
+        public string SaleZoneName { get; set; }
+        public int SaleRateChange { get; set; }
+        public DateTime SaleRateEffectiveDate { get; set; }
+        public double SaleAmount { get; set; }
+        public int ZoneID { get; set; }
+        public string CostZoneName { get; set; }
+        public string CostRateChange { get; set; }
+        public DateTime CostRateEffectiveDate { get; set; }
+        public double CostAmount { get; set; }
+        public double profit { get; set; }
+    }
+}
