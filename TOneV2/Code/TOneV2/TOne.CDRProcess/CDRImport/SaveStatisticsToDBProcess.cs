92ca584131fd3d83d7fe2f2796660de1e9f84e94
mustafa.hawi <mustafa.hawi@vanrise.tfs.local>
2015-06-12 10:43:49 +0000

    Add save statistics to database wf
    git-tfs-id: [http://developmentvlan:8585/tfs/vanrise.collection]$/;C23648

---
+++ TOneV2/Code/TOneV2/TOne.CDRProcess/CDRImport/SaveStatisticsToDBProcess.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Activities;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TOne.CDRProcess.Arguments;
+using Vanrise.BusinessProcess;
+
+namespace TOne.CDRProcess
+{
+    public partial class SaveStatisticsToDBProcess : Activity, IBPWorkflow
+    {
+        public string GetTitle(Vanrise.BusinessProcess.Entities.CreateProcessInput createProcessInput)
+        {
+            SaveStatisticsToDBProcessInput inputArg = createProcessInput.InputArguments as SaveStatisticsToDBProcessInput;
+            if (inputArg == null)
+                throw new ArgumentNullException("SaveStatisticsToDBProcessInput");
+            return String.Format("Save Statistics To DB Process for Switch {0}", inputArg.SwitchID);
+        }
+    }
+}
