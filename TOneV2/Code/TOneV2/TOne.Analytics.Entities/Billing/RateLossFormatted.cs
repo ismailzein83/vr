f4ea66772efb78705d2d2ca39c3db12b4e817959
Ali Ballouk <VANRISE\ali.ballouk>
2015-06-12 10:30:35 +0000

    Add Entities RateLoss and RateLossFormatted
    git-tfs-id: [http://developmentvlan:8585/tfs/vanrise.collection]$/;C23646

---
+++ TOneV2/Code/TOneV2/TOne.Analytics.Entities/Billing/RateLossFormatted.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TOne.Analytics.Entities
+{
+    public class RateLossFormatted
+    {
+        public string CostZone { get; set; }
+        public string SaleZone { get; set; }
+        public string SupplierID { get; set; }
+        public string Supplier { get; set; }
+        public string CustomerID { get; set; }
+        public string Customer { get; set; }
+        public double? SaleRate { get; set; }
+        public string SaleRateFormatted { get; set; }
+        public double? CostRate { get; set; }
+        public string CostRateFormatted { get; set; }
+        public decimal? CostDuration { get; set; }
+        public string CostDurationFormatted { get; set; }
+        public decimal? SaleDuration { get; set; }
+        public string SaleDurationFormatted { get; set; }
+        public double? CostNet { get; set; }
+        public string CostNetFormatted { get; set; }
+        public double? SaleNet { get; set; }
+        public string SaleNetFormatted { get; set; }
+        public int SaleZoneID { get; set; }
+        public string LossFormatted { get; set; }
+    }
+}
