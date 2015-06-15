9f2caa78f342c80478dc41eed6244dfef8bedf2c
walid.mogharbel <walid.mogharbel@vanrise.tfs.local>
2015-06-15 16:46:14 +0000

    git-tfs-id: [http://developmentvlan:8585/tfs/vanrise.collection]$/;C23733

--- CDRAnalysis/Vanrise.Fzero.FraudAnalysis.Data.SQL/StrategyDataManager.cs
+++ CDRAnalysis/Vanrise.Fzero.FraudAnalysis.Data.SQL/StrategyDataManager.cs
@@ -23,121 +23,13 @@ namespace Vanrise.Fzero.FraudAnalysis.Data.SQL
         {
             return GetItemsSP("FraudAnalysis.sp_Strategy_GetStrategy", StrategyMapper, strategyId).FirstOrDefault();
         }
-        //public Strategy GetStrategy(int strategyId)
-        //{
-        //    string query0 = "SELECT Id FROM Strategy WHERE Id = @StrategyId";
-        //    string query = "SELECT MaxValue, CriteriaID FROM StrategyThreshold Where StrategyId = @StrategyId";
-        //    string query1 = "SELECT PeriodId, Value, CriteriaID FROM StrategyPeriods Where StrategyId = @StrategyId";
-        //    string query2 = "SELECT LevelId, CriteriaId1, Cr1Per ,  CriteriaId2 ,  Cr2Per ,  CriteriaId3 ,  Cr3Per ,  CriteriaId4 ,  Cr4Per ,  CriteriaId5 ,  Cr5Per ,  CriteriaId6 ,  Cr6Per ,  CriteriaId7 ,  Cr7Per ,  CriteriaId8 ,  Cr8Per ,  CriteriaId9 ,  Cr9Per ,  CriteriaId10 ,  Cr10Per ,  CriteriaId11 ,  Cr11Per ,  CriteriaId12 ,  Cr12Per ,  CriteriaId13 ,  Cr13Per ,  CriteriaId14 ,  Cr14Per ,  CriteriaId15 ,  Cr15Per,  CriteriaId16 ,  Cr16Per,  CriteriaId17 ,  Cr17Per,  CriteriaId18 ,  Cr18Per   FROM  Strategy_Suspicion_Level Where StrategyId = @StrategyId and LevelId<>1  ";
-
-        //    Strategy strategy = new Strategy();
-
-        //    strategy.Id = GetItemText<int>(query0, (reader) =>
-        //    {
-        //        return GetReaderValue<int>(reader, "Id");
-        //    }, (cmd) =>
-        //    {
-        //        cmd.Parameters.Add(new SqlParameter() { ParameterName = "@StrategyId", Value = strategyId });
-        //    });
-
-
-        //    strategy.StrategyFilters = GetItemsText<StrategyFilter>(query, (reader) =>
-        //    {
-
-        //        StrategyFilter strategyFilter = new StrategyFilter();
-        //        strategyFilter.Threshold = GetReaderValue<decimal>(reader, "MaxValue");
-        //        strategyFilter.FilterId = GetReaderValue<int>(reader, "CriteriaID");
-        //        return strategyFilter;
-        //    }, (cmd) =>
-        //    {
-
-        //        cmd.Parameters.Add(new SqlParameter() { ParameterName = "@StrategyId", Value = strategyId });
-
-        //    });
-
-
-
-
-
-        //    List<StrategyFilter> strategyFilters = GetItemsText<StrategyFilter>(query1, (reader) =>
-        //    {
-        //        StrategyFilter strategyFilter = new StrategyFilter();
-        //        strategyFilter.PeriodId = GetReaderValue<int>(reader, "PeriodId");
-        //        strategyFilter.FilterId = GetReaderValue<int>(reader, "CriteriaID");
-        //        return strategyFilter;
-        //    }, (cmd) =>
-        //    {
-
-        //        cmd.Parameters.Add(new SqlParameter() { ParameterName = "@StrategyId", Value = strategyId });
-
-        //    });
-
-
-
-        //    foreach (var i in strategy.StrategyFilters)
-        //    {
-        //        i.PeriodId = strategyFilters.Where(x => x.FilterId == i.FilterId).First().PeriodId;
-        //    }
-
-
-
-
-        //    strategy.StrategyLevels = GetItemsText<StrategyLevel>(query2, (reader) =>
-        //    {
-        //        StrategyLevel strategyLevel = new StrategyLevel();
-
-        //        List<StrategyLevelCriteria> Lstslc = new List<StrategyLevelCriteria>();
-
-        //        strategyLevel.SuspicionLevelId = GetReaderValue<int>(reader, "LevelId");
-
-        //        for (int i = 1; i <= 18; i++)
-        //        {
-        //            StrategyLevelCriteria slc = new StrategyLevelCriteria();
-        //            string CrId = "CriteriaId" + i.ToString();
-        //            string CrPer = "Cr" + i.ToString() + "Per";
-        //            if (reader[CrId].ToString() != "0")
-        //            {
-        //                slc.FilterId = i;
-        //                slc.Percentage = GetReaderValue<decimal>(reader, CrPer);
-        //                Lstslc.Add(slc);
-        //            }
-        //        }
-        //        strategyLevel.StrategyLevelCriterias = Lstslc;
-        //        return strategyLevel;
-        //    }, (cmd) =>
-        //    {
-        //        cmd.Parameters.Add(new SqlParameter() { ParameterName = "@StrategyId", Value = strategyId });
-        //    });
-
-        //    return strategy;
-        //}
-        public List<Strategy> GetAllStrategies2()
+       
+        public List<Strategy> GetAllStrategies()
         {
             return GetItemsSP("FraudAnalysis.sp_Strategy_GetAll", StrategyMapper);
         }
 
-
-        public List<Strategy> GetAllStrategies()
-        {
-            string query_GetStrategies = @"SELECT Id, Name, Description FROM Strategy; ";
-            List<Strategy> strategies = new List<Strategy>();
-
-
-            ExecuteReaderText(query_GetStrategies, (reader) =>
-            {
-                while (reader.Read())
-                {
-                    Strategy strategy = new Strategy();
-                    strategy.Id = (int)reader["Id"];
-                    strategy.Name = reader["Name"] as string;
-                    strategy.Description = reader["Description"] as string;
-                    strategies.Add(strategy);
-                }
-            }, null);
-            return strategies;
-        }
-
-
+        
         public List<Strategy> GetFilteredStrategies(int fromRow, int toRow, string name, string description)
         {
             return GetItemsSP("FraudAnalysis.sp_Strategy_GetFilteredStrategies", StrategyMapper, fromRow, toRow, name, description);
