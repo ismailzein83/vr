27d2ecbf1704df0ee0f95a3f3be96a14fc0e0f2c
mustafa.hawi <mustafa.hawi@vanrise.tfs.local>
2015-06-15 14:31:38 +0000

    Add daily traffic stat WF
    git-tfs-id: [http://developmentvlan:8585/tfs/vanrise.collection]$/;C23718

--- TOneV2/Code/TOneV2/TOne.CDRProcess/CDRImport/GenerateDailyStatisticsProcess.cs
+++ TOneV2/Code/TOneV2/TOne.CDRProcess/CDRImport/GenerateDailyStatisticsProcess.cs
@@ -1,12 +1,27 @@
 ﻿using System;
+using System.Activities;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using TOne.CDRProcess.Arguments;
+using Vanrise.BusinessProcess;
+using Vanrise.BusinessProcess.Entities;
 
 namespace TOne.CDRProcess
 {
-    public partial class GenerateDailyStatisticsProcess
+    public partial class GenerateDailyStatisticsProcess : Activity, IBPWorkflow
     {
+        #region IBPWorkflow Members
+
+        public string GetTitle(CreateProcessInput createProcessInput)
+        {
+            GenerateDailyStatisticsProcessInput inputArg = createProcessInput.InputArguments as GenerateDailyStatisticsProcessInput;
+            if (inputArg == null)
+                throw new ArgumentNullException("GenerateDailyStatisticsProcessInput");
+            return String.Format("Generate Daily Statistics Process for Switch {0}", inputArg.SwitchID);
+        }
+
+        #endregion
     }
 }
