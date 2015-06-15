652332804f78b1a74a11033eb1b397b641aaff3a
mustafa.hawi <mustafa.hawi@vanrise.tfs.local>
2015-06-15 14:52:42 +0000

    Add GenerateDailyTrafficStatistics Activity
    git-tfs-id: [http://developmentvlan:8585/tfs/vanrise.collection]$/;C23721

---
+++ TOneV2/Code/TOneV2/TOne.CDRProcess.Activities/GenerateDailyTrafficStatistics.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Activities;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TOne.CDR.Entities;
+using Vanrise.BusinessProcess;
+using Vanrise.Queueing;
+
+namespace TOne.CDRProcess.Activities
+{
+    #region Argument Classes
+
+    public class GenerateDailyTrafficStatisticsInput
+    {
+        public BaseQueue<TOne.CDR.Entities.CDRBillingBatch> InputQueue { get; set; }
+
+        public BaseQueue<TrafficStatisticDailyBatch> OutputQueue { get; set; }
+    }
+
+    #endregion
+
+    public sealed class GenerateDailyTrafficStatistics : DependentAsyncActivity<GenerateDailyTrafficStatisticsInput>
+    {
+        [RequiredArgument]
+        public InArgument<BaseQueue<TOne.CDR.Entities.CDRBillingBatch>> InputQueue { get; set; }
+
+        [RequiredArgument]
+        public InOutArgument<BaseQueue<TrafficStatisticDailyBatch>> OutputQueue { get; set; }
+
+        protected override void DoWork(GenerateDailyTrafficStatisticsInput inputArgument, AsyncActivityStatus previousActivityStatus, AsyncActivityHandle handle)
+        {
+            throw new NotImplementedException();
+        }
+
+        protected override GenerateDailyTrafficStatisticsInput GetInputArgument2(System.Activities.AsyncCodeActivityContext context)
+        {
+            return new GenerateDailyTrafficStatisticsInput
+            {
+                InputQueue = this.InputQueue.Get(context),
+                OutputQueue = this.OutputQueue.Get(context)
+            };
+        }
+
+        protected override void OnBeforeExecute(AsyncCodeActivityContext context, AsyncActivityHandle handle)
+        {
+            if (this.OutputQueue.Get(context) == null)
+                this.OutputQueue.Set(context, new MemoryQueue<TrafficStatisticDailyBatch>());
+
+            base.OnBeforeExecute(context, handle);
+        }
+
+    }
+}
