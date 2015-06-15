9b09f2c8e0f99d82fa7a2b4a227120b81df0a1f1
mustafa.hawi <mustafa.hawi@vanrise.tfs.local>
2015-06-15 14:26:30 +0000

    Add Daily Stat WF
    git-tfs-id: [http://developmentvlan:8585/tfs/vanrise.collection]$/;C23716

--- TOneV2/Code/TOneV2/TOne.CDRProcess/CDRImport/GenerateDailyStatisticsProcess.cs
+++ TOneV2/Code/TOneV2/TOne.CDRProcess/CDRImport/GenerateDailyStatisticsProcess.cs
@@ -6,7 +6,7 @@ using System.Threading.Tasks;
 
 namespace TOne.CDRProcess
 {
-    class GenerateDailyStatisticsProcess
+    public partial class GenerateDailyStatisticsProcess
     {
     }
 }
--- TOneV2/Code/TOneV2/TOne.CDRProcess/CDRImport/SaveDailyStatisticsToDBProcess.cs
+++ TOneV2/Code/TOneV2/TOne.CDRProcess/CDRImport/SaveDailyStatisticsToDBProcess.cs
@@ -6,7 +6,7 @@ using System.Threading.Tasks;
 
 namespace TOne.CDRProcess
 {
-    class SaveDailyStatisticsToDBProcess
+    public partial class SaveDailyStatisticsToDBProcess
     {
     }
 }
