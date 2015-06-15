5d7e9ebd36708273b21b025af4c6667e54ba8a54
Ali Ballouk <VANRISE\ali.ballouk>
2015-06-15 10:02:52 +0000

    Carrier Summary Entities
    git-tfs-id: [http://developmentvlan:8585/tfs/vanrise.collection]$/;C23695

---
+++ TOneV2/Code/TOneV2/TOne.Analytics.Entities/Billing/CarrierSummary.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TOne.Analytics.Entities
+{
+    public class CarrierSummary
+    {
+
+        public string SupplierID { get; set; }
+        public string CustomerID { get; set; }
+        public decimal? SaleDuration { get; set; }
+        public decimal? CostDuration { get; set; }
+        public double? CostNet { get; set; }
+        public double? SaleNet { get; set; }
+        public double? CostCommissionValue { get; set; }
+        public double? SaleCommissionValue { get; set; }
+        public double? CostExtraChargeValue { get; set; }
+        public double? SaleExtraChargeValue { get; set; }
+
+    }
+}
---
+++ TOneV2/Code/TOneV2/TOne.Analytics.Entities/Billing/CarrierSummaryFormatted.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TOne.Analytics.Entities
+{
+    public class CarrierSummaryFormatted
+    {
+        public string SupplierID { get; set; }
+        public string Supplier { get; set; }
+        public string CustomerID { get; set; }
+        public string Customer { get; set; }
+        public decimal? SaleDuration { get; set; }
+        public string SaleDurationFormatted { get; set; }
+        public decimal? CostDuration { get; set; }
+        public string CostDurationFormatted { get; set; }
+        public double? CostNet { get; set; }
+        public string CostNetFormatted { get; set; }
+        public double? SaleNet { get; set; }
+        public string SaleNetFormatted { get; set; }
+        public double? CostCommissionValue { get; set; }
+        public string CostCommissionValueFormatted { get; set; }
+        public double? SaleCommissionValue { get; set; }
+        public string SaleCommissionValueFormatted { get; set; }
+        public double? CostExtraChargeValue { get; set; }
+        public string CostExtraChargeValueFormatted { get; set; }
+        public double? SaleExtraChargeValue { get; set; }
+        public string SaleExtraChargeValueFormatted { get; set; }
+        public string Profit { get; set; }
+        public string AvgMin { get; set; }
+
+    }
+}
