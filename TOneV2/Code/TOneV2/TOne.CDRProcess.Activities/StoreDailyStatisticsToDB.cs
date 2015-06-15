0dd0b14e10de68ea5f949051f4f4db5870f9759b
mustafa.hawi <mustafa.hawi@vanrise.tfs.local>
2015-06-15 15:16:00 +0000

    Daily Stat WF
    git-tfs-id: [http://developmentvlan:8585/tfs/vanrise.collection]$/;C23725

---
+++ TOneV2/Code/TOneV2/TOne.CDRProcess.Activities/StoreDailyStatisticsToDB.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Activities;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TOne.CDR.Data;
+using TOne.CDR.Entities;
+using Vanrise.BusinessProcess;
+using Vanrise.Queueing;
+
+namespace TOne.CDRProcess.Activities
+{
+    #region Argument Classes
+    public class StoreDailyStatisticsToDBInput
+    {
+        public BaseQueue<TrafficStatisticDailyBatch> InputQueue { get; set; }
+    }
+
+    #endregion
+
+    public sealed class StoreDailyStatisticsToDB : DependentAsyncActivity<StoreDailyStatisticsToDBInput>
+    {
+        [RequiredArgument]
+        public InOutArgument<BaseQueue<TrafficStatisticDailyBatch>> InputQueue { get; set; }
+
+        protected override void DoWork(StoreDailyStatisticsToDBInput inputArgument, AsyncActivityStatus previousActivityStatus, AsyncActivityHandle handle)
+        {
+            ITrafficStatisticDataManager dataManager = CDRDataManagerFactory.GetDataManager<ITrafficStatisticDataManager>();
+
+            bool hasItem = false;
+            DoWhilePreviousRunning(previousActivityStatus, handle, () =>
+            {
+                do
+                {
+                    hasItem = inputArgument.InputQueue.TryDequeue((dailyTrafficStatisticsBatch) =>
+                    {
+                        
+                      // dataManager.UpdateTrafficStatisticDailyBatch(dailyTrafficStatisticsBatch.BatchStart, dailyTrafficStatisticsBatch.BatchEnd, dailyTrafficStatisticsBatch.TrafficStatistics);
+                    });
+                }
+                while (!ShouldStop(handle) && hasItem);
+            });
+        }
+
+        protected override StoreDailyStatisticsToDBInput GetInputArgument2(System.Activities.AsyncCodeActivityContext context)
+        {
+            return new StoreDailyStatisticsToDBInput
+            {
+                InputQueue = this.InputQueue.Get(context),
+            };
+        }
+    }
+}
