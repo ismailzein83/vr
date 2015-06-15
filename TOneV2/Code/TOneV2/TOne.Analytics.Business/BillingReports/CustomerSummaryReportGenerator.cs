2a575a40755b6549674aae86b4814107aa717ae9
hadi.hawi <hadi.hawi@vanrise.tfs.local>
2015-06-15 12:32:54 +0000

    Add CustomerSummaryReportGenerator
    git-tfs-id: [http://developmentvlan:8585/tfs/vanrise.collection]$/;C23704

---
+++ TOneV2/Code/TOneV2/TOne.Analytics.Business/BillingReports/CustomerSummaryReportGenerator.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TOne.Analytics.Entities;
+using TOne.Entities;
+
+namespace TOne.Analytics.Business.BillingReports
+{
+    public class CustomerSummaryReportGenerator : TOne.Entities.IReportGenerator
+    {
+        public Dictionary<string, System.Collections.IEnumerable> GenerateDataSources(TOne.Entities.ReportParameters parameters)
+        {
+            BillingStatisticManager manager = new BillingStatisticManager();
+            List<CustomerSummaryFormatted> customerSummary = new List<CustomerSummaryFormatted>();
+                //manager.GetZoneProfit(parameters.FromTime, parameters.ToTime, parameters.SupplierId, parameters.CustomerId, parameters.GroupByCustomer, parameters.SupplierAMUId, parameters.CustomerAMUId);
+
+            //List<CarrierSummaryDailyFormatted> zs = manager.GetDailyCarrierSummary(DateTime.Parse("2012-05-01 00:00:00"), DateTime.Parse("2015-05-01 00:00:00"), null, null, false, true, null , null );
+            Dictionary<string, System.Collections.IEnumerable> dataSources = new Dictionary<string, System.Collections.IEnumerable>();
+            dataSources.Add(" CustomerSummary", customerSummary);
+            return dataSources;
+        }
+
+        public Dictionary<string, RdlcParameter> GetRdlcReportParameters(TOne.Entities.ReportParameters parameters)
+        {
+
+            Dictionary<string, RdlcParameter> list = new Dictionary<string, RdlcParameter>();
+            list.Add("GroupByCustomer", new RdlcParameter() { Value = parameters.GroupByCustomer.ToString(), IsVisible = false });
+            list.Add("FromDate", new RdlcParameter { Value = parameters.FromTime.ToString(), IsVisible = true });
+            list.Add("ToDate", new RdlcParameter { Value = parameters.ToTime.ToString(), IsVisible = true });
+            list.Add("Title", new RdlcParameter { Value = "Zone Profit", IsVisible = true });
+            list.Add("Currency", new RdlcParameter { Value = "[USD] United States Dollars", IsVisible = true });
+            list.Add("LogoPath", new RdlcParameter { Value = "logo", IsVisible = true });
+            list.Add("Customer", new RdlcParameter { Value = "", IsVisible = true });
+            list.Add("Supplier", new RdlcParameter { Value = "", IsVisible = true });
+            list.Add("DigitRate", new RdlcParameter { Value = "2", IsVisible = true });
+
+            return list;
+        }
+    }
+}
--- TOneV2/Code/TOneV2/TOne.Analytics.Business/BillingStatisticManagerPartial.cs
+++ TOneV2/Code/TOneV2/TOne.Analytics.Business/BillingStatisticManagerPartial.cs
@@ -3,10 +3,39 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using TOne.Analytics.Data;
+using TOne.Analytics.Entities;
+using TOne.BusinessEntity.Business;
 
 namespace TOne.Analytics.Business
 {
-    partial class BillingStatisticManager
+    public partial class BillingStatisticManager
     {
+        public List<CustomerSummaryFormatted> GetCustomerSummary(DateTime fromDate, DateTime toDate, string customerId, string supplierId, bool isCost, string currencyId, string supplierGroup, string customerGroup, int? customerAMUId, int? supplierAMUId, bool groupBySupplier)
+        {
+            return FormatCustomerSummaries(_datamanager.GetCustomerSummary(fromDate, toDate, customerId, customerAMUId, supplierAMUId));
+        }
+        private List<CustomerSummaryFormatted> FormatCustomerSummaries(List<CustomerSummary> zoneSummariesDetailed)
+        {
+            List<CustomerSummaryFormatted> models = new List<CustomerSummaryFormatted>();
+            if (zoneSummariesDetailed != null)
+                foreach (var z in zoneSummariesDetailed)
+                {
+                    models.Add(FormatCustomerSummary(z));
+                }
+            return models;
+        }
+        private CustomerSummaryFormatted FormatCustomerSummary(CustomerSummary customerSummary)
+        {
+            return new CustomerSummaryFormatted
+            {
+                Carrier = customerSummary.Carrier,
+                SaleDuration = customerSummary.SaleDuration,
+                SaleNet = customerSummary.SaleNet,
+                CostDuration = customerSummary.CostDuration,
+                CostNet = customerSummary.CostNet
+            };
+        }
+
     }
 }
