c3e1c12a294bcb757eb770d41c9233df001bb9eb
hadi.hawi <hadi.hawi@vanrise.tfs.local>
2015-06-15 13:29:43 +0000

    CustomerSummaryFormatted
    git-tfs-id: [http://developmentvlan:8585/tfs/vanrise.collection]$/;C23708

--- TOneV2/Code/TOneV2/TOne.Analytics.Entities/Billing/CustomerSummaryFormatted.cs
+++ TOneV2/Code/TOneV2/TOne.Analytics.Entities/Billing/CustomerSummaryFormatted.cs
@@ -4,9 +4,22 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace TOne.Analytics.Entities.Billing
+namespace TOne.Analytics.Entities
 {
-    class CustomerSummaryFormatted
+    public class CustomerSummaryFormatted
     {
+        public string Carrier { get; set; }
+        public decimal? SaleDuration { get; set; }
+        public string SaleDurationFormatted { get; set; }
+        public double? SaleNet { get; set; }
+        public string SaleNetFormatted { get; set; }
+        public decimal? CostDuration { get; set; }
+        public string CostDurationFormatted { get; set; }
+        public double? CostNet { get; set; }
+        public string CostNetFormatted { get; set; }
+        public string ProfitFormatted { get; set; }
+        public string ProfitPercentageFormatted { get; set; }
+        public string ServicesFeesFormatted { get; set; }
+
     }
 }
