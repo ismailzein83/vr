e204e0d4b0f608f912404df80bd76890c8c2bfc2
walid.mogharbel <walid.mogharbel@vanrise.tfs.local>
2015-06-15 15:32:59 +0000

    git-tfs-id: [http://developmentvlan:8585/tfs/vanrise.collection]$/;C23727

---
+++ CDRAnalysis/Vanrise.Fzero.MobileCDRAnalysis/Classes/Strategy.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using Vanrise.CommonLibrary;
+
+
+
+namespace Vanrise.Fzero.MobileCDRAnalysis
+{
+    public partial class Strategy
+    {
+        public static Strategy Load(int id)
+        {
+            Strategy strategy = new Strategy();
+            try
+            {
+                using (Entities context = new Entities())
+                {
+
+                    strategy = context.Strategies
+                        .Where(s => s.Id == id)
+                        .FirstOrDefault();
+                }
+            }
+            catch (Exception err)
+            {
+                FileLogger.Write("DataLayer.strategy.Load(" + id + ")", err);
+            }
+            return strategy;
+        }
+
+        
+        public static List<Strategy> GetAll()
+        {
+            List<Strategy> strategies = new List<Strategy>();
+            try
+            {
+                using (Entities context = new Entities())
+                {
+                    strategies = context.Strategies
+                       .ToList(); 
+                }
+            }
+            catch (Exception err)
+            {
+                FileLogger.Write("DataLayer.Strategy.GetAll()", err);
+            }
+            return strategies;
+        }
+    }
+}
