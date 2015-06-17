0cd15c8ccdb458876b74c6bc1529fb859f82805f
mustafa.hawi <mustafa.hawi@vanrise.tfs.local>
2015-06-17 10:32:57 +0000

    Activator queue
    git-tfs-id: [http://developmentvlan:8585/tfs/vanrise.collection]$/;C23804

--- TOneV2/Code/TOneV2/TOne.CDR.Business/CDRQueueFactory.cs
+++ TOneV2/Code/TOneV2/TOne.CDR.Business/CDRQueueFactory.cs
@@ -72,17 +72,20 @@ namespace TOne.CDR.Business
             lock (s_CreatedSwitchesQueues)
             {
                 if (s_CreatedSwitchesQueues.Contains(switchId))
-                    return;               
+                    return;
 
-                CreateSwitchQueueIfNotExists<CDRBatch>(switchId, QueueName.CDRRaw);
-                CreateSwitchQueueIfNotExists<CDRBatch>(switchId, QueueName.CDRRawForBilling, QueueName.CDRRaw);
-                CreateSwitchQueueIfNotExists<CDRBillingBatch>(switchId, QueueName.CDRBilling);
-                CreateSwitchQueueIfNotExists<CDRBillingBatch>(switchId, QueueName.CDRBillingForStats, QueueName.CDRBilling);
-                CreateSwitchQueueIfNotExists<CDRBillingBatch>(switchId, QueueName.CDRBillingForStatsDaily, QueueName.CDRBilling);
-                CreateSwitchQueueIfNotExists<CDRMainBatch>(switchId, QueueName.CDRMain);
-                CreateSwitchQueueIfNotExists<CDRInvalidBatch>(switchId, QueueName.CDRInvalid);
+                CreateSwitchQueueIfNotExists<CDRBatch>(switchId, QueueName.CDRRaw, null, new QueueSettings { SingleConcurrentReader = true, QueueActivator = new Vanrise.Queueing.Extensions.CreateProcessQueueActivator { ProcessName = "StoreCDRsInDBProcess", ProcessInputArguments = new StoreCDRsInDBProcessInput { SwitchID = switchId } } });
+                CreateSwitchQueueIfNotExists<CDRBatch>(switchId, QueueName.CDRRawForBilling, QueueName.CDRRaw, new QueueSettings { SingleConcurrentReader = true, QueueActivator = new Vanrise.Queueing.Extensions.CreateProcessQueueActivator { ProcessName = "RawCDRsProcess", ProcessInputArguments = new RawCDRsProcessInput { SwitchID = switchId } } });
+
+                CreateSwitchQueueIfNotExists<CDRBillingBatch>(switchId, QueueName.CDRBilling, null, new QueueSettings { SingleConcurrentReader = true, QueueActivator = new Vanrise.Queueing.Extensions.CreateProcessQueueActivator { ProcessName = "BillingCDRsProcess", ProcessInputArguments = new BillingCDRsProcessInput { SwitchID = switchId } } });
+                CreateSwitchQueueIfNotExists<CDRBillingBatch>(switchId, QueueName.CDRBillingForStats, QueueName.CDRBilling , new QueueSettings { SingleConcurrentReader = true, QueueActivator = new Vanrise.Queueing.Extensions.CreateProcessQueueActivator { ProcessName = "GenerateStatisticsProcess", ProcessInputArguments = new GenerateStatisticsProcessInput { SwitchID = switchId } } });
+                CreateSwitchQueueIfNotExists<CDRBillingBatch>(switchId, QueueName.CDRBillingForStatsDaily, QueueName.CDRBilling, new QueueSettings { SingleConcurrentReader = true, QueueActivator = new Vanrise.Queueing.Extensions.CreateProcessQueueActivator { ProcessName = "GenerateDailyStatisticsProcess", ProcessInputArguments = new GenerateDailyStatisticsProcessInput { SwitchID = switchId } } });
+
+                CreateSwitchQueueIfNotExists<CDRMainBatch>(switchId, QueueName.CDRMain, null, new QueueSettings { SingleConcurrentReader = true, QueueActivator = new Vanrise.Queueing.Extensions.CreateProcessQueueActivator { ProcessName = "StoreMainCDRsInDBProcess", ProcessInputArguments = new StoreMainCDRsInDBProcessInput { SwitchID = switchId } } });
+                CreateSwitchQueueIfNotExists<CDRInvalidBatch>(switchId, QueueName.CDRInvalid, null, new QueueSettings { SingleConcurrentReader = true, QueueActivator = new Vanrise.Queueing.Extensions.CreateProcessQueueActivator { ProcessName = "StoreInvalidCDRsInDBProcess", ProcessInputArguments = new StoreInvalidCDRsInDBProcessInput { SwitchID = switchId } } });
                 CreateSwitchQueueIfNotExists<TrafficStatisticBatch>(switchId, QueueName.TrafficStats, null, new QueueSettings { SingleConcurrentReader = true, QueueActivator = new Vanrise.Queueing.Extensions.CreateProcessQueueActivator { ProcessName = "SaveStatisticsToDBProcess", ProcessInputArguments = new SaveStatisticsToDBProcessInput { SwitchID = switchId } } });
-                CreateSwitchQueueIfNotExists<TrafficStatisticDailyBatch>(switchId, QueueName.TrafficStatsDaily, null, new QueueSettings { SingleConcurrentReader = true });
+                CreateSwitchQueueIfNotExists<TrafficStatisticDailyBatch>(switchId, QueueName.TrafficStatsDaily, null, new QueueSettings { SingleConcurrentReader = true, QueueActivator = new Vanrise.Queueing.Extensions.CreateProcessQueueActivator { ProcessName = "SaveDailyStatisticsToDBProcess", ProcessInputArguments = new SaveDailyStatisticsToDBProcessInput { SwitchID = switchId } } });
+                
                 s_CreatedSwitchesQueues.Add(switchId);
             }
         }
--- TOneV2/Code/TOneV2/TOne.CDR.Data.SQL/TrafficStatisticDataManager.cs
+++ TOneV2/Code/TOneV2/TOne.CDR.Data.SQL/TrafficStatisticDataManager.cs
@@ -220,7 +220,7 @@ namespace TOne.CDR.Data.SQL
         private void AddDailyTrafficStatisticToStream(StreamForBulkInsert stream, TrafficStatisticDaily trafficStatistic)
         {
             stream.WriteRecord("0^{0}^{1}^{2}^{3}^{4}^{5}^{6}^{7}^{8}^{9}^{10}^{11}^{12}^{13}^{14}^{15}^{16}^{17}^{18}^{19}",
-                trafficStatistic.CallDate,
+                trafficStatistic.CallDate.ToString("yyyy-MM-dd HH:mm:ss.fff"),
                 trafficStatistic.SwitchId,
                 trafficStatistic.CustomerId,
                 trafficStatistic.OurZoneId,
