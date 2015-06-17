57fb69e4851e1c53377b382e8353d7cf6064e3f2
walid.mogharbel <walid.mogharbel@vanrise.tfs.local>
2015-06-17 11:49:14 +0000

    git-tfs-id: [http://developmentvlan:8585/tfs/vanrise.collection]$/;C23818

---
+++ CDRAnalysis/Vanrise.Fzero.FraudAnalysis.Web/FraudAnalysis/Views/ResultsManagementController.js
@@ -0,0 +1,103 @@
+﻿StrategyManagementController.$inject = ['$scope', 'StrategyAPIService', '$routeParams', 'notify', 'VRModalService', 'VRNotificationService', 'VRNavigationService'];
+
+function StrategyManagementController($scope, StrategyAPIService, $routeParams, notify, VRModalService, VRNotificationService, VRNavigationService) {
+
+    var mainGridAPI;
+    var arrMenuAction = [];
+    
+    defineScope();
+    load();
+
+       
+   
+
+    function defineScope() {
+
+        $scope.gridMenuActions = [];
+
+        $scope.strategies = [];
+
+        $scope.onMainGridReady = function (api) {
+            mainGridAPI = api;
+            getData();
+        };
+
+        $scope.loadMoreData = function () {
+           return getData();
+        }
+
+        $scope.searchClicked = function () {
+            mainGridAPI.clearDataAndContinuePaging();
+            return getData();
+        };
+
+        $scope.resetClicked = function () {            
+            $scope.name = '';
+            $scope.description = '';
+            mainGridAPI.clearDataAndContinuePaging();
+            return getData();
+        };
+
+        $scope.addNewStrategy = addNewStrategy;
+
+        defineMenuActions();
+    }
+
+    function load() {
+        
+    }
+
+    function defineMenuActions() {
+        $scope.gridMenuActions = [{
+            name: "Edit",
+            clicked: editStrategy
+        }];
+    }
+
+    function getData() {
+        var name = $scope.name != undefined ? $scope.name : '';
+        var description = $scope.description != undefined ? $scope.description : '';
+        var pageInfo = mainGridAPI.getPageInfo();
+
+        return StrategyAPIService.GetFilteredStrategies(pageInfo.fromRow, pageInfo.toRow, name, description).then(function (response) {
+            angular.forEach(response, function (itm) {
+               
+                itm.IsDefaultText = itm.IsDefault ? "Default" : "Not Default";
+                $scope.strategies.push(itm);
+            });
+        });
+    }
+
+    function addNewStrategy() {
+        var settings = {};
+
+        settings.onScopeReady = function (modalScope) {
+            modalScope.title = "New Strategy";
+            modalScope.onStrategyAdded = function (strategy) {
+                strategy.IsDefaultText = strategy.IsDefault ? "Default" : "Not Default";
+                mainGridAPI.itemAdded(strategy);
+            };
+        };
+        VRModalService.showModal('/Client/Modules/FraudAnalysis/Views/StrategyEditor.html', null, settings);
+    }
+
+    function editStrategy(strategy) {
+        var params = {
+            strategyId: strategy.Id
+        };
+
+        var settings = {
+           
+        };
+
+        settings.onScopeReady = function (modalScope) {
+            modalScope.title = "Edit Strategy";
+            modalScope.onStrategyUpdated = function (strategy) {
+                strategy.IsDefaultText = strategy.IsDefault ? "Default" : "Not Default";
+                mainGridAPI.itemUpdated(strategy);
+            };
+        };
+        VRModalService.showModal("/Client/Modules/FraudAnalysis/Views/StrategyEditor.html", params, settings);
+    }
+}
+appControllers.controller('FraudAnalysis_StrategyManagementController', StrategyManagementController);
