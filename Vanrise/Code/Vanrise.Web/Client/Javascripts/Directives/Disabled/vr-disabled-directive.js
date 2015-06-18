58a5a241d62c67f9b407f51d1df4ef7ed6a5513f
Ismail Zein <VANRISE\ismail.zein>
2015-06-18 13:17:17 +0000

    added vr-disabled
    git-tfs-id: [http://developmentvlan:8585/tfs/vanrise.collection]$/;C23880

---
+++ Vanrise/Code/Vanrise.Web/Client/Javascripts/Directives/Disabled/vr-disabled-directive.js
@@ -0,0 +1,23 @@
+﻿'use strict';
+
+
+app.directive('vrDisabled', ['$compile', function ($compile) {
+
+    var directiveDefinitionObject = {
+        restrict: 'A',
+        scope: false,
+        compile: function (tElement, tAttrs) {
+            tElement.attr('ng-class', '{\'divDisabled\': ' + tAttrs.vrDisabled + '}');
+            tElement.removeAttr("vr-disabled");
+            return {
+                post: function postLink(scope, iElement, iAttrs, controller) {
+                    $compile(iElement)(scope);
+                }
+            };
+        }
+
+    };
+    return directiveDefinitionObject;
+
+}]);
+
