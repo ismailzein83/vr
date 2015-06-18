46054c0fd9829335a920e82290f4a181e9a94881
sara.yahfoufi <sara.yahfoufi@vanrise.tfs.local>
2015-06-18 06:53:27 +0000

    Add Enumeration class
    git-tfs-id: [http://developmentvlan:8585/tfs/vanrise.collection]$/;C23838

---
+++ TOneV2/Code/TOneV2/TOne.Analytics.Entities/Billing/Enumerations.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TOne.Analytics.Entities
+{
+    public enum TimePeriod 
+    {
+        Days = 0,
+        Weeks = 1,
+        Months = 2
+    }
+
+    public enum VariationReportOptions 
+    {
+        InBoundMinutes = 0,
+        OutBoundMinutes = 1,
+        InOutBoundMinutes = 2,
+        TopDestinationMinutes = 3,
+        InBoundAmount = 4,
+        OutBoundAmount = 5,
+        InOutBoundAmount = 6,
+        TopDestinationAmount = 7,
+        Profit = 8
+    }
+
+}
+
