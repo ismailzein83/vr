6afa2947130114a797d006864a53a0dd9d93d5a5
sara.yahfoufi <sara.yahfoufi@vanrise.tfs.local>
2015-06-18 08:07:10 +0000

    GetVariationReportQuery
    git-tfs-id: [http://developmentvlan:8585/tfs/vanrise.collection]$/;C23845

--- TOneV2/Code/TOneV2/TOne.Analytics.Data.SQL/BillingStatisticDataManagerPartial.cs
+++ TOneV2/Code/TOneV2/TOne.Analytics.Data.SQL/BillingStatisticDataManagerPartial.cs
@@ -30,6 +30,44 @@ namespace TOne.Analytics.Data.SQL
             );
         }
 
+        public string GetVariationReportQuery(DateTime selectedDate, int periodCount, TimePeriod TimePeriodEnum, VariationReportOptions VariationReportOptionsEnum)
+        {
+         StringBuilder query = new StringBuilder(query_Common);
+         query.Append(@" SELECT  #NameColumn# , 
+            0.0 as [AVG],
+            0.0 as [%], 
+            0.0 as [Prev %],
+            CallDate,
+            (#ValueColumn#)/60 as Total,
+            #IDColumn# as ID
+            From Billing_Stats BS With(Nolock,INDEX(IX_Billing_Stats_Date))
+            LEFT JOIN @ExchangeRates ERC ON ERC.Currency = BS.Cost_Currency AND ERC.Date = BS.CallDate			
+            LEFT JOIN @ExchangeRates ERS ON ERS.Currency = BS.Sale_Currency AND ERS.Date = BS.CallDate
+            #JoinStatement#
+            #WhereStatement#  
+            GROUP BY #NameColumn# ,#IDColumn#, CallDate");
+           // foreach(var variationReportOptions in Enum.GetValues(VariationReportOptionsEnum.GetType()))
+            switch (VariationReportOptionsEnum.ToString())
+                {
+                case "0":
+                    query.Replace("#NameColumn#", " cpc.Name ");
+                    query.Replace("#ValueColumn#"," SUM(SaleDuration) ");
+                    query.Replace("#IDColumn#", " cac.CarrierAccountID as ID " );
+                    query.Replace("#JoinStatement#", @" JOIN CarrierAccount cac With(Nolock) ON cac.CarrierAccountID=BS.CustomerID
+                                                        JOIN CarrierProfile cpc With(Nolock) ON cpc.ProfileID = cac.ProfileID ");
+                    query.Replace("#WhereStatement#"," WHERE CallDate BETWEEN DATEADD(@TimePeriod, -@PeriodCount+1, @FromDate) AND @FromDate ");
+                    break;
+                case "1": break;
+                case "2": break;
+                case "3": break;
+                case "4": break;
+                case "5": break;
+                case "6": break;
+                case "7": break;
+            }
+            return query.ToString();
+        }
+
         #region privateMethods
         private CustomerSummary CustomerSummaryMapper(IDataReader reader)
         {
