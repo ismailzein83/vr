cb9b041084ff5182a83562f36d48c8f0c0ecfcfb
Ismail Zein <VANRISE\ismail.zein>
2015-06-22 08:28:48 +0000

    Added MortadaTask
    git-tfs-id: [http://developmentvlan:8585/tfs/vanrise.collection]$/;C23947

---
+++ TOneV2/Code/TOneV2/TestRuntime/Tasks/MortadaTask.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Vanrise.BusinessProcess;
+using Vanrise.BusinessProcess.Client;
+using Vanrise.BusinessProcess.Entities;
+using Vanrise.Queueing;
+using Vanrise.Runtime;
+
+namespace TestRuntime.Tasks
+{
+    class MortadaTask : ITask
+    {
+        public void Execute()
+        {
+            System.Threading.ThreadPool.SetMaxThreads(10000, 10000);
+
+            BusinessProcessService bpService = new BusinessProcessService() { Interval = new TimeSpan(0, 0, 2) };
+            QueueActivationService queueActivationService = new QueueActivationService() { Interval = new TimeSpan(0, 0, 2) };
+
+            var runtimeServices = new List<RuntimeService>();
+            runtimeServices.Add(queueActivationService);
+
+            runtimeServices.Add(bpService);
+
+            RuntimeHost host = new RuntimeHost(runtimeServices);
+            host.Start();
+
+            BPClient bpClient = new BPClient();
+            bpClient.CreateNewProcess(new CreateProcessInput
+            {
+                ProcessName = "DailyRepricingProcess",
+                InputArguments = new TOne.CDRProcess.Arguments.DailyRepricingProcessInput
+                {
+                    RepricingDay = DateTime.Parse("2013-03-29")
+                }
+            });
+
+
+        }
+    }
+}
