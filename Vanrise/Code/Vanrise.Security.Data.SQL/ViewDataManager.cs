72ac33733e932becabb288174c32b463b606b6fd
Mortada Issa <VANRISE\mortada.issa>
2015-06-22 10:40:55 +0000

    Security Module - Changed the Required Permissions Column data to a normal string in View Table
    git-tfs-id: [http://developmentvlan:8585/tfs/vanrise.collection]$/;C23960

--- Vanrise/Code/Vanrise.Security.Data.SQL/ViewDataManager.cs
+++ Vanrise/Code/Vanrise.Security.Data.SQL/ViewDataManager.cs
@@ -30,10 +30,36 @@ namespace Vanrise.Security.Data.SQL
                 Name = reader["Name"] as string,
                 Url = reader["Url"] as string,
                 ModuleId = (int) reader["Module"],
-                RequiredPermissions = ((reader["RequiredPermissions"] as string) != null) ? Common.Serializer.Deserialize<Dictionary<string, List<string>>>(reader["RequiredPermissions"] as string) : null,
+                RequiredPermissions = this.ParseRequiredPermissionsString(GetReaderValue<string>(reader, "RequiredPermissions")),
                 Audience = ((reader["Audience"] as string) != null) ? Common.Serializer.Deserialize<AudienceWrapper>(reader["Audience"] as string) : null
             };
             return view;
+        }
+
+        private Dictionary<string, List<string>> ParseRequiredPermissionsString(string value)
+        {
+            Dictionary<string, List<string>> requiredPermissions = null;
+
+            if(value != null)
+            {
+                requiredPermissions = new Dictionary<string, List<string>>();
+
+                string[] arrayOfPermissions = value.Split('|');
+
+                foreach (string permission in arrayOfPermissions)
+                {
+                    string[] keyValuesArray = permission.Split(':');
+                    List<string> flags = new List<string>();
+                    foreach (string flag in keyValuesArray[1].Split(','))
+                    {
+                        flags.Add(flag.Trim());
+                    }
+
+                    requiredPermissions.Add(keyValuesArray[0].Trim(), flags);
+                }
+            }
+
+            return requiredPermissions;
         }
     }
 
