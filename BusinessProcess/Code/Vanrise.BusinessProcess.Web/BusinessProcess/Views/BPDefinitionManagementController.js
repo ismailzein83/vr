ecfa9d3902574417bfae1950e353e41480891640
Ismail Zein <VANRISE\ismail.zein>
2015-06-22 13:36:09 +0000

    added BPDefinitionManagement
    git-tfs-id: [http://developmentvlan:8585/tfs/vanrise.collection]$/;C23972

---
+++ BusinessProcess/Code/Vanrise.BusinessProcess.Web/BusinessProcess/Views/BPDefinitionManagementController.js
@@ -0,0 +1,20 @@
+﻿BPDefinitionManagementController.$inject = ['$scope'];
+
+function BPDefinitionManagementController($scope) {
+
+  
+    defineScope();
+    load();
+
+    function defineScope() {
+        $scope.testModel = 'BusinessProcess_BPDefinitionManagementController';
+        
+    }
+
+    function load() {
+       
+    }
+
+};
+
+appControllers.controller('BusinessProcess_BPDefinitionManagementController', BPDefinitionManagementController);
