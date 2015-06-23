03c69b6a4dfcc8eb99de3eea3e1b84a92521d6e4
Ismail Zein <VANRISE\ismail.zein>
2015-06-23 06:53:25 +0000

    updated SQL projects
    git-tfs-id: [http://developmentvlan:8585/tfs/vanrise.collection]$/;C23977

---
+++ BusinessProcess/Code/Vanrise.BusinessProcess.SQLTrackingDatabase/bp/Stored Procedures/sp_BPTrackings_GetByInstanceId.sql
@@ -0,0 +1,18 @@
+﻿CREATE PROCEDURE [bp].[sp_BPTrackings_GetByInstanceId]	
+	@ProcessInstanceID bigint
+AS
+BEGIN
+	-- SET NOCOUNT ON added to prevent extra result sets from
+	-- interfering with SELECT statements.
+	SET NOCOUNT ON;
+
+    SELECT  [ID]
+      ,[ProcessInstanceID]
+      ,[ParentProcessID]
+      ,[TrackingMessage]
+      ,[Severity]
+      ,[EventTime]
+	FROM [TOneWFTracking].[bp].[BPTracking] as bpt WITH(NOLOCK)
+	WHERE
+		bpt.ProcessInstanceID = @ProcessInstanceID
+END
