3aa71c34df5ede89d8987da51965196362d19789
walid.mogharbel <walid.mogharbel@vanrise.tfs.local>
2015-06-24 13:59:20 +0000

    bp def
    git-tfs-id: [http://developmentvlan:8585/tfs/vanrise.collection]$/;C24067

--- BusinessProcess/Code/Vanrise.BusinessProcess.Web/BusinessProcess/Views/InstanceEditorController.js
+++ BusinessProcess/Code/Vanrise.BusinessProcess.Web/BusinessProcess/Views/InstanceEditorController.js
@@ -1,8 +1,32 @@
 ﻿InstanceEditorController.$inject = ['$scope', 'BusinessProcessAPIService', '$routeParams', 'notify', 'VRModalService', 'VRNotificationService', 'VRNavigationService', 'UtilsService'];
 
 function InstanceEditorController($scope, BusinessProcessAPIService, $routeParams, notify, VRModalService, VRNotificationService, VRNavigationService, UtilsService) {
-
+    defineScope();
     loadParameters();
+
+    function defineScope() {
+        $scope.subViewExecuteStrategyProcessInput = {};
+        $scope.close = function () {
+            $scope.modalContext.closeModal()
+        };
+        $scope.startInstance = function () {
+
+
+            //$scope.issaving = true;
+            //var routeRuleObject = buildRouteRuleObjFromScope();
+            //return RoutingRulesAPIService.InsertRouteRule(routeRuleObject).then(function (response) {
+            //    if (VRNotificationService.notifyOnItemAdded("RouteRule", response)) {
+            //        if ($scope.onRouteRuleAdded != undefined)
+            //            $scope.onRouteRuleAdded(response.InsertedObject);
+            //        $scope.modalContext.closeModal();
+            //    }
+            //}).catch(function (error) {
+            //    VRNotificationService.notifyException(error);
+            //});
+
+
+        };
+    }
 
     function loadParameters() {
         var parameters = VRNavigationService.getParameters($scope);
