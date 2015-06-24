f18052519768eab75b7fe06a077d9a0200348046
Ismail Zein <VANRISE\ismail.zein>
2015-06-24 06:30:12 +0000

    added JSONWithTypeAttribute
    git-tfs-id: [http://developmentvlan:8585/tfs/vanrise.collection]$/;C24011

---
+++ Vanrise/Code/Vanrise.Web.Base/JSONWithTypeAttribute.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Http.Controllers;
+
+namespace Vanrise.Web.Base
+{
+    public class JSONWithTypeAttribute : Attribute, IControllerConfiguration
+    {
+        public void Initialize(HttpControllerSettings controllerSettings, HttpControllerDescriptor controllerDescriptor)
+        {
+            controllerSettings.Formatters.Clear();
+            var jsonFormatter = new System.Net.Http.Formatting.JsonMediaTypeFormatter();
+            jsonFormatter.SerializerSettings.TypeNameHandling = Newtonsoft.Json.TypeNameHandling.Objects;
+            controllerSettings.Formatters.Add(jsonFormatter);
+        }
+    }
+}
