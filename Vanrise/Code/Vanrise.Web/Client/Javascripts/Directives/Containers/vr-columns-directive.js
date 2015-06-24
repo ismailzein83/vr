e29835225a49448eeb37837d279a11b87a464e6f
Ismail Zein <VANRISE\ismail.zein>
2015-06-24 11:48:53 +0000

    added width attribute vr-columns
    git-tfs-id: [http://developmentvlan:8585/tfs/vanrise.collection]$/;C24049

--- Vanrise/Code/Vanrise.Web/Client/Javascripts/Directives/Containers/vr-columns-directive.js
+++ Vanrise/Code/Vanrise.Web/Client/Javascripts/Directives/Containers/vr-columns-directive.js
@@ -9,6 +9,20 @@ app.directive('vrColumns', ['$compile', function ($compile) {
             var numberOfColumns = tAttrs.colnum;
             if (numberOfColumns == undefined)
                 numberOfColumns = 1;
+
+            if (tAttrs.width != undefined) {
+                switch(tAttrs.width)
+                {
+                    case "normal": numberOfColumns = 3; break;
+                    case "small": numberOfColumns = 2; break;
+                    case "large": numberOfColumns = 4; break;
+                    case "fullrow": numberOfColumns = 12; break;
+                    case "1/2row": numberOfColumns = 6; break;
+                    case "1/4row": numberOfColumns = 3; break;
+                    case "1/3row": numberOfColumns = 4; break;
+                }
+            }
+
             var nbrcolsm = (numberOfColumns <= 3) ? numberOfColumns * 2 : 12;
 
             if (tAttrs.colnumsm != undefined) {
