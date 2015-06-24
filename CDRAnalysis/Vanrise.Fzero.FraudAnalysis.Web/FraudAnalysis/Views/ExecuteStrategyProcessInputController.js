d243f84b133863f461aa27c96fb89b51a6d505e4
walid.mogharbel <walid.mogharbel@vanrise.tfs.local>
2015-06-24 13:15:47 +0000

    git-tfs-id: [http://developmentvlan:8585/tfs/vanrise.collection]$/;C24062

--- CDRAnalysis/Vanrise.Fzero.FraudAnalysis.Web/FraudAnalysis/Views/ExecuteStrategyProcessInputController.js
+++ CDRAnalysis/Vanrise.Fzero.FraudAnalysis.Web/FraudAnalysis/Views/ExecuteStrategyProcessInputController.js
@@ -1,103 +1,218 @@
-﻿ExecuteStrategyProcessInputController.$inject = ['$scope', 'StrategyAPIService', '$routeParams', 'notify', 'VRModalService', 'VRNotificationService', 'VRNavigationService'];
-
-function ExecuteStrategyProcessInputController($scope, StrategyAPIService, $routeParams, notify, VRModalService, VRNotificationService, VRNavigationService) {
-
-    var mainGridAPI;
-    var arrMenuAction = [];
-    
+﻿var ExecuteStrategyProcessInputController = function ($scope, $http, StrategyAPIService, $routeParams, notify, VRModalService, VRNotificationService, VRNavigationService) {
     defineScope();
-    load();
-
-       
-   
-
+    //load();
+    //loadForm();
     function defineScope() {
 
-        $scope.gridMenuActions = [];
-
-        $scope.strategies = [];
-
-        $scope.onMainGridReady = function (api) {
-            mainGridAPI = api;
-            getData();
+        $scope.customvalidateFrom = function (fromDate) {
+            return validateDates(fromDate, $scope.toDate);
         };
-
-        $scope.loadMoreData = function () {
-           return getData();
+        $scope.customvalidateTo = function (toDate) {
+            return validateDates($scope.fromDate, toDate);
+        };
+        function validateDates(fromDate, toDate) {
+            if (fromDate == undefined || toDate == undefined)
+                return null;
+            var from = new Date(fromDate);
+            var to = new Date(toDate);
+            if (from.getTime() > to.getTime())
+                return "Start should be before end";
+            else
+                return null;
         }
 
-        $scope.searchClicked = function () {
-            mainGridAPI.clearDataAndContinuePaging();
-            return getData();
-        };
+        $scope.strategies = [];
+        loadStrategies();
+        $scope.selectedStrategies = [];
 
-        $scope.resetClicked = function () {            
-            $scope.name = '';
-            $scope.description = '';
-            mainGridAPI.clearDataAndContinuePaging();
-            return getData();
-        };
 
-        $scope.addNewStrategy = addNewStrategy;
+        $scope.periods = [];
+        loadPeriods();
+        $scope.selectedPeriod = "";
 
-        defineMenuActions();
+
+        //$scope.code = "";
+        //$scope.codeList = [];
+        //$scope.codeInpute = '';
+
+        //var numberReg = /^\d+$/;
+        //$scope.isNumber = function (s) {
+        //    return String(s).search(numberReg) != -1
+        //};
+
+        //$scope.muteAction = function (e) {
+        //    e.preventDefault();
+        //    e.stopPropagation();
+        //}
+        //$scope.getCodes = function () {
+        //    var label = '';
+        //    if ($scope.codeList.length == 0)
+        //        label = "Fill codes...";
+        //    else if ($scope.codeList.length == 1) {
+        //        label += $scope.codeList[0];
+        //    }
+        //    else if ($scope.codeList.length < 3) {
+        //        $.each($scope.codeList, function (i, value) {
+        //            if (i < $scope.codeList.length - 1)
+        //                label += value + ',';
+        //            else
+        //                label += value;
+        //        });
+        //    }
+        //    else
+        //        label = $scope.codeList.length + " Codes selected";
+        //    return label;
+        //};
+
+        //$scope.addCodeEnter = function (e) {
+        //    if (e.keyCode == 13) {
+        //        $scope.addCode(e);
+        //    }
+        //}
+        //$scope.addCode = function (e) {
+        //    var valid = $scope.isNumber($scope.codeInpute);
+        //    if (valid) {
+        //        var index = null;
+        //        var index = $scope.codeList.indexOf($scope.codeInpute);
+        //        if (index >= 0) {
+        //            $scope.codeInpute = '';
+        //            return;
+        //        }
+        //        else {
+        //            $scope.codeList.push($scope.codeInpute);
+        //            $scope.codeInpute = '';
+        //        }
+
+        //    }
+        //    else {
+        //        $scope.codeInpute = '';
+        //    }
+        //}
+        //$scope.removeCode = function (e, s) {
+        //    e.preventDefault();
+        //    e.stopPropagation();
+        //    var index = $scope.codeList.indexOf(s);
+        //    $scope.codeList.splice(index, 1);
+        //}
+
+
+        //$scope.subViewCodeSetConnector.getData = function () {
+        //    return {
+        //        $type: "TOne.LCR.Entities.CodeSelectionSet, TOne.LCR.Entities",
+        //        Code: $scope.code,
+        //        WithSubCodes: ($scope.subCodes == true) ? true : false,
+        //        ExcludedCodes: $scope.codeList
+        //    };
+        //};
+
+        //$scope.subViewCodeSetConnector.setData = function (data) {
+        //    $scope.subViewCodeSetConnector.data = data;
+        //    loadForm();
+        //}
+
     }
 
-    function load() {
-        
-    }
 
-    function defineMenuActions() {
-        $scope.gridMenuActions = [{
-            name: "Edit",
-            clicked: editStrategy
-        }];
-    }
+ 
 
-    function getData() {
-        var name = $scope.name != undefined ? $scope.name : '';
-        var description = $scope.description != undefined ? $scope.description : '';
-        var pageInfo = mainGridAPI.getPageInfo();
-
-        return StrategyAPIService.GetFilteredStrategies(pageInfo.fromRow, pageInfo.toRow, name, description).then(function (response) {
+    function loadPeriods() {
+        return StrategyAPIService.GetPeriods().then(function (response) {
             angular.forEach(response, function (itm) {
-               
-                itm.IsDefaultText = itm.IsDefault ? "Default" : "Not Default";
-                $scope.strategies.push(itm);
+                $scope.periods.push(itm);
             });
         });
     }
 
-    function addNewStrategy() {
-        var settings = {};
 
-        settings.onScopeReady = function (modalScope) {
-            modalScope.title = "New Strategy";
-            modalScope.onStrategyAdded = function (strategy) {
-                strategy.IsDefaultText = strategy.IsDefault ? "Default" : "Not Default";
-                mainGridAPI.itemAdded(strategy);
-            };
-        };
-        VRModalService.showModal('/Client/Modules/FraudAnalysis/Views/StrategyEditor.html', null, settings);
+    function loadStrategies() {
+        return StrategyAPIService.GetAllStrategies().then(function (response) {
+            angular.forEach(response, function (itm) {
+                $scope.strategies.push({ id: itm.Id, name: itm.Name });
+            });
+        });
     }
 
-    function editStrategy(strategy) {
-        var params = {
-            strategyId: strategy.Id
-        };
 
-        var settings = {
-           
-        };
 
-        settings.onScopeReady = function (modalScope) {
-            modalScope.title = "Edit Strategy";
-            modalScope.onStrategyUpdated = function (strategy) {
-                strategy.IsDefaultText = strategy.IsDefault ? "Default" : "Not Default";
-                mainGridAPI.itemUpdated(strategy);
-            };
-        };
-        VRModalService.showModal("/Client/Modules/FraudAnalysis/Views/StrategyEditor.html", params, settings);
+    function getData() {
+        //var fromDate = $scope.fromDate != undefined ? $scope.fromDate : '';
+        //var toDate = $scope.toDate != undefined ? $scope.toDate : '';
+
+        //var strategyId = $scope.selectedStrategies.id;
+        //var suspicionLevelsList = '';
+
+        //angular.forEach($scope.selectedSuspicionLevels, function (itm) {
+        //    suspicionLevelsList = suspicionLevelsList + itm.id + ','
+        //});
+
+
+        //var pageInfo = mainGridAPI.getPageInfo();
+
+
+        //return SuspicionAnalysisAPIService.GetFilteredSuspiciousNumbers(pageInfo.fromRow, pageInfo.toRow, fromDate, toDate, strategyId, suspicionLevelsList.slice(0, -1)).then(function (response) {
+        //    angular.forEach(response, function (itm) {
+        //        //var date = $filter('date')(new Date(), 'MMM dd, yyyy');
+        //        //itm.FormattedDate = date;
+
+        //        $scope.fraudResults.push(itm);
+        //    });
+        //});
     }
+
+
+    //function loadForm() {
+    //    if ($scope.subViewCodeSetConnector.data == undefined)
+    //        return;
+    //    var data = $scope.subViewCodeSetConnector.data;
+    //    if (data != null) {
+    //        $scope.codeList = data.ExcludedCodes;
+    //        $scope.code = data.Code;
+    //        $scope.subCodes = data.WithSubCodes;
+    //    }
+    //    else {
+    //        $scope.zoneSelectionOption = 1;
+    //        $scope.codeList = [];
+    //        $scope.code = '';
+    //        $scope.subCodes = false;
+    //    }
+    //}
+
+    //function load() {
+    //    $('#CodeListddl').on('show.bs.dropdown', function (e) {
+    //        $(this).find('.dropdown-menu').first().stop(true, true).slideDown();
+    //    });
+
+    //    //ADD SLIDEUP ANIMATION TO DROPDOWN //
+    //    $('#CodeListddl').on('hide.bs.dropdown', function (e) {
+    //        $(this).find('.dropdown-menu').first().stop(true, true).slideUp();
+    //    });
+    //    $('div[id="CodeListddl"]').on('click', '.dropdown-toggle', function (event) {
+
+    //        var self = $(this);
+    //        var selfHeight = $(this).parent().height();
+    //        var selfWidth = $(this).parent().width();
+    //        var selfOffset = $(self).offset();
+    //        var selfOffsetRigth = $(document).width() - selfOffset.left - selfWidth;
+    //        var dropDown = self.parent().find('ul');
+    //        $(dropDown).css({ position: 'fixed', top: selfOffset.top + selfHeight, left: 'auto' });
+    //    });
+
+    //    var fixDropdownPosition = function () {
+    //        $('.drop-down-inside-modal').find('.dropdown-menu').hide();
+    //        $('.drop-down-inside-modal').removeClass("open");
+
+    //    };
+
+    //    $(".modal-body").unbind("scroll");
+    //    $(".modal-body").scroll(function () {
+    //        fixDropdownPosition();
+    //    });
+    //}
+
 }
-appControllers.controller('FraudAnalysis_ExecuteStrategyProcessInputController', ExecuteStrategyProcessInputController);
+
+ExecuteStrategyProcessInputController.$inject = ['$scope', '$http', 'StrategyAPIService', '$routeParams', 'notify', 'VRModalService', 'VRNotificationService', 'VRNavigationService'];
+appControllers.controller('FraudAnalysis_ExecuteStrategyProcessInputController', ExecuteStrategyProcessInputController)
+
+
+
