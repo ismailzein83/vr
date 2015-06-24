2e63fd91ffd6d3c4a88a4134aa06f7d6cc36a3bd
Zeinab Mokdad <VANRISE\zeinab.mokdad>
2015-06-24 13:32:02 +0000

    adding PriceList page
    git-tfs-id: [http://developmentvlan:8585/tfs/vanrise.collection]$/;C24063

---
+++ TOneV2/Code/TOneV2/TOne.BusinessEntity.Web/BusinessEntity/Views/CarrierPriceListController.js
@@ -0,0 +1,21 @@
+﻿CarrierPriceListController.$inject = ['$scope', 'CarrierAPIService', 'CarrierTypeEnum'];
+function CarrierPriceListController($scope, CarrierAPIService, CarrierTypeEnum) {
+    defineScope();
+    load();
+
+    function defineScope() {
+        $scope.suppliers = [];
+        $scope.selectedSuppliers = [];
+    }
+    function load() {
+        loadSuppliers();
+    }
+    function loadSuppliers() {
+        return CarrierAPIService.GetCarriers(CarrierTypeEnum.Supplier.value).then(function (response) {
+            angular.forEach(response, function (itm) {
+                $scope.suppliers.push(itm);
+            });
+        });
+    }
+}
+appControllers.controller('Carrier_CarrierPriceListController', CarrierPriceListController);
