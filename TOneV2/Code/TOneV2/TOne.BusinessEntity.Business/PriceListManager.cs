b84ca857a487e45a5f2a18e40c5f598e372c96cc
Zeinab Mokdad <VANRISE\zeinab.mokdad>
2015-06-25 13:23:43 +0000

    adding PriceList
    git-tfs-id: [http://developmentvlan:8585/tfs/vanrise.collection]$/;C24131

---
+++ TOneV2/Code/TOneV2/TOne.BusinessEntity.Business/PriceListManager.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TOne.BusinessEntity.Data;
+using TOne.BusinessEntity.Entities;
+
+namespace TOne.BusinessEntity.Business
+{
+   public class PriceListManager
+    {
+        IPriceListDataManager _dataManager;
+        public PriceListManager()
+        {
+            _dataManager = BEDataManagerFactory.GetDataManager<IPriceListDataManager>();
+        }
+        public List<PriceList> GetPriceList()
+        {
+            return _dataManager.GetPriceList();
+        }
+    }
+}
