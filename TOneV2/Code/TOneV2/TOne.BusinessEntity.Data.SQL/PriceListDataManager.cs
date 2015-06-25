65b4866489c38d4c65158cff302525e8edb1ce1e
Zeinab Mokdad <VANRISE\zeinab.mokdad>
2015-06-25 13:23:27 +0000

    adding PriceList
    git-tfs-id: [http://developmentvlan:8585/tfs/vanrise.collection]$/;C24129

---
+++ TOneV2/Code/TOneV2/TOne.BusinessEntity.Data.SQL/PriceListDataManager.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TOne.BusinessEntity.Entities;
+using TOne.Data.SQL;
+
+namespace TOne.BusinessEntity.Data.SQL
+{
+    public class PriceListDataManager : BaseTOneDataManager, IPriceListDataManager
+    {
+        public List<PriceList> GetPriceList()
+        {
+            return GetItemsSP("[BEntity].sp_PriceList_GetByCarrierAccountID", (reader) =>
+                {
+                    return new PriceList
+                    {
+                        PriceListID = (int)reader["PriceListID"],
+                        Description = reader["[Description]"] as string,
+                        BeginEffectiveDate = reader["BeginEffectiveDate"] as Nullable<DateTime>,
+                        UserName = reader["Name"] as string
+                    };
+                }
+                );
+        }
+    }
+}
