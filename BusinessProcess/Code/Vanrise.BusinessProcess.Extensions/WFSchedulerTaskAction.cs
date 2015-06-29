26c282d00678dade9ed9d7454aee3467f33224aa
Mortada Issa <VANRISE\mortada.issa>
2015-06-29 12:07:36 +0000

    Runtime Module - Scheduler Service - Implmenting Real Time Trigger
    git-tfs-id: [http://developmentvlan:8585/tfs/vanrise.collection]$/;C24204

--- BusinessProcess/Code/Vanrise.BusinessProcess.Extensions/WFSchedulerTaskAction.cs
+++ BusinessProcess/Code/Vanrise.BusinessProcess.Extensions/WFSchedulerTaskAction.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Vanrise.BusinessProcess.Client;
 using Vanrise.BusinessProcess.Entities;
 using Vanrise.Runtime.Entities;
 
@@ -12,11 +13,19 @@ namespace Vanrise.BusinessProcess.Extensions
     {
         public int BPDefinitionID { get; set; }
 
-        public BaseProcessInputArgument BaseProcessInputArgument { get; set; }
+        public BaseProcessInputArgument ProcessInputArguments { get; set; }
 
         public override void Execute()
         {
-            Console.WriteLine("WFSchedulerTaskAction is running...");
+            Console.WriteLine("WFSchedulerTaskAction started...");
+
+            BPClient bpClient = new BPClient();
+            bpClient.CreateNewProcess(new BusinessProcess.Entities.CreateProcessInput
+            {
+                InputArguments = this.ProcessInputArguments
+            });
+
+            Console.WriteLine("WFSchedulerTaskAction finished...");
         }
     }
 }
