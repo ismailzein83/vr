1c412a17beaeaad081dd04f92fc7e81ec45e5133
mustafa.hawi <mustafa.hawi@vanrise.tfs.local>
2015-06-29 13:03:13 +0000

    Add QueueItemType
    git-tfs-id: [http://developmentvlan:8585/tfs/vanrise.collection]$/;C24212

---
+++ BusinessProcess/Code/Vanrise.Queueing.Entities/QueueItemType.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Vanrise.Queueing.Entities
+{
+    public class QueueItemType
+    {
+        public int Id { get; set; }
+
+        public string ItemFQTN { get; set; }
+
+        public string Title { get; set; }
+
+        public string DefaultQueueSettings { get; set; }
+
+        public DateTime CreatedTime { get; set; }
+
+    }
+}
