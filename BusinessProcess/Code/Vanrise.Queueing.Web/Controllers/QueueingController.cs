2ef90948cef9b93b6f2f555afaf4ab68538ebfa3
mustafa.hawi <mustafa.hawi@vanrise.tfs.local>
2015-06-30 08:08:24 +0000

    Add Queueing files
    git-tfs-id: [http://developmentvlan:8585/tfs/vanrise.collection]$/;C24239

---
+++ BusinessProcess/Code/Vanrise.Queueing.Web/Controllers/QueueingController.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Vanrise.Queueing.Web.Controllers
+{
+    public class QueueingController : Vanrise.Web.Base.BaseAPIController
+    {
+
+    }
+}
