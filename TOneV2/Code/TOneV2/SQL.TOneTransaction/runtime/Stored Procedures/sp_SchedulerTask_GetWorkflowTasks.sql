c5b9c81ce0cd8d63fda64ac0c9fc702959f01617
Mortada Issa <VANRISE\mortada.issa>
2015-06-30 11:39:11 +0000

    Adding new precedure
    git-tfs-id: [http://developmentvlan:8585/tfs/vanrise.collection]$/;C24259

---
+++ TOneV2/Code/TOneV2/SQL.TOneTransaction/runtime/Stored Procedures/sp_SchedulerTask_GetWorkflowTasks.sql
@@ -0,0 +1,25 @@
+﻿-- =============================================
+-- Author:		<Author,,Name>
+-- Create date: <Create Date,,>
+-- Description:	<Description,,>
+-- =============================================
+CREATE PROCEDURE [runtime].sp_SchedulerTask_GetWorkflowTasks
+@ActionTypeId int
+AS
+BEGIN
+	-- SET NOCOUNT ON added to prevent extra result sets from
+	-- interfering with SELECT statements.
+	SET NOCOUNT ON;
+
+    -- Insert statements for procedure here
+	SELECT [ID]
+      ,[Name]
+      ,[IsEnabled]
+      ,[Status]
+      ,[TriggerTypeId]
+      ,[TaskTrigger]
+      ,[ActionTypeId]
+      ,[TaskAction]
+      from runtime.ScheduleTask
+      WHERE runtime.ScheduleTask.ActionTypeId = @ActionTypeId
+END
