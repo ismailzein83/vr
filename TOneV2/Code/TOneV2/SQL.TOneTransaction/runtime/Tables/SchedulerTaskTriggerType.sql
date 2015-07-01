34faaaf743fa078a27f6ce90761127ce36ff28fb
Mortada Issa <VANRISE\mortada.issa>
2015-07-01 07:58:36 +0000

    Runtime Module - Scehduler Service - Updating Scripts
    git-tfs-id: [http://developmentvlan:8585/tfs/vanrise.collection]$/;C24288

--- TOneV2/Code/TOneV2/SQL.TOneTransaction/runtime/Tables/SchedulerTaskTriggerType.sql
+++ TOneV2/Code/TOneV2/SQL.TOneTransaction/runtime/Tables/SchedulerTaskTriggerType.sql
@@ -1,7 +1,9 @@
 ﻿CREATE TABLE [runtime].[SchedulerTaskTriggerType] (
-    [ID]              INT           IDENTITY (1, 1) NOT NULL,
+    [ID]              INT           NOT NULL,
     [Name]            VARCHAR (50)  NOT NULL,
     [TriggerTypeInfo] VARCHAR (255) NOT NULL,
     CONSTRAINT [PK_SchedulerTaskTriggerTemplate] PRIMARY KEY CLUSTERED ([ID] ASC)
 );
+
+
 
