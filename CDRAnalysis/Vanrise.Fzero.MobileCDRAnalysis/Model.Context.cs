8f54bcf8fbef9ff9857893e8cc3beb79b5cc8222
walid.mogharbel <walid.mogharbel@vanrise.tfs.local>
2015-07-02 13:03:00 +0000

    git-tfs-id: [http://developmentvlan:8585/tfs/vanrise.collection]$/;C24353

--- CDRAnalysis/Vanrise.Fzero.MobileCDRAnalysis/Model.Context.cs
+++ CDRAnalysis/Vanrise.Fzero.MobileCDRAnalysis/Model.Context.cs
@@ -28,6 +28,49 @@ namespace Vanrise.Fzero.MobileCDRAnalysis
             throw new UnintentionalCodeFirstException();
         }
     
+        public DbSet<CellProfile> CellProfiles { get; set; }
+        public DbSet<ControlTable> ControlTables { get; set; }
+        public DbSet<Criteria_Profile> Criteria_Profile { get; set; }
+        public DbSet<EmailReceiver> EmailReceivers { get; set; }
+        public DbSet<EmailReceiverType> EmailReceiverTypes { get; set; }
+        public DbSet<EmailTemplate> EmailTemplates { get; set; }
+        public DbSet<EmailToken> EmailTokens { get; set; }
+        public DbSet<Import> Imports { get; set; }
+        public DbSet<ImportType> ImportTypes { get; set; }
+        public DbSet<NormalizationRule> NormalizationRules { get; set; }
+        public DbSet<NumberProfile> NumberProfiles { get; set; }
+        public DbSet<Ol_NumberProfile> Ol_NumberProfile { get; set; }
+        public DbSet<OperationType> OperationTypes { get; set; }
+        public DbSet<Peak_Time> Peak_Time { get; set; }
+        public DbSet<Period> Periods { get; set; }
+        public DbSet<Permission> Permissions { get; set; }
+        public DbSet<PredefinedColumn> PredefinedColumns { get; set; }
+        public DbSet<Related_Criteria> Related_Criteria { get; set; }
+        public DbSet<Report> Reports { get; set; }
+        public DbSet<ReportDetail> ReportDetails { get; set; }
+        public DbSet<ReportingStatu> ReportingStatus { get; set; }
+        public DbSet<Set_CallClass> Set_CallClass { get; set; }
+        public DbSet<Set_CallType> Set_CallType { get; set; }
+        public DbSet<Set_SubType> Set_SubType { get; set; }
+        public DbSet<SourceMapping> SourceMappings { get; set; }
+        public DbSet<Strategy> Strategies { get; set; }
+        public DbSet<Strategy_Min_Values> Strategy_Min_Values { get; set; }
+        public DbSet<Strategy_Suspicion_Level> Strategy_Suspicion_Level { get; set; }
+        public DbSet<StrategyPeriod> StrategyPeriods { get; set; }
+        public DbSet<StrategyThreshold> StrategyThresholds { get; set; }
+        public DbSet<Subscriber_Values> Subscriber_Values { get; set; }
+        public DbSet<SubscriberThreshold> SubscriberThresholds { get; set; }
+        public DbSet<Suspicion_Level> Suspicion_Level { get; set; }
+        public DbSet<Switch_DatabaseConnections> Switch_DatabaseConnections { get; set; }
+        public DbSet<SwitchProfile> SwitchProfiles { get; set; }
+        public DbSet<sysdiagram> sysdiagrams { get; set; }
+        public DbSet<UserPermission> UserPermissions { get; set; }
+        public DbSet<User> Users { get; set; }
+        public DbSet<NormalCDR> NormalCDRs { get; set; }
+        public DbSet<vwDashboard> vwDashboards { get; set; }
+        public DbSet<vwReportedNumberNormalCDR> vwReportedNumberNormalCDRs { get; set; }
+        public DbSet<vwReportVariable> vwReportVariables { get; set; }
+        public DbSet<vwSuspectionAnalysi> vwSuspectionAnalysis { get; set; }
     
         public virtual int prCollectNormalizedCDR()
         {
@@ -611,6 +654,31 @@ namespace Vanrise.Fzero.MobileCDRAnalysis
                 new ObjectParameter("StrategyContent", typeof(string));
     
             return ((IObjectContextAdapter)this).ObjectContext.ExecuteFunction("sp_Strategy_Update", idParameter, userIdParameter, nameParameter, descriptionParameter, creationDateParameter, isDefaultParameter, strategyContentParameter);
+        }
+    
+        public virtual int sp_GetSuspiciousNumbers(Nullable<System.DateTime> fromDate, Nullable<System.DateTime> toDate, Nullable<int> strategyId, string suspectionList, Nullable<int> minimumOccurance)
+        {
+            var fromDateParameter = fromDate.HasValue ?
+                new ObjectParameter("fromDate", fromDate) :
+                new ObjectParameter("fromDate", typeof(System.DateTime));
+    
+            var toDateParameter = toDate.HasValue ?
+                new ObjectParameter("ToDate", toDate) :
+                new ObjectParameter("ToDate", typeof(System.DateTime));
+    
+            var strategyIdParameter = strategyId.HasValue ?
+                new ObjectParameter("strategyId", strategyId) :
+                new ObjectParameter("strategyId", typeof(int));
+    
+            var suspectionListParameter = suspectionList != null ?
+                new ObjectParameter("SuspectionList", suspectionList) :
+                new ObjectParameter("SuspectionList", typeof(string));
+    
+            var minimumOccuranceParameter = minimumOccurance.HasValue ?
+                new ObjectParameter("MinimumOccurance", minimumOccurance) :
+                new ObjectParameter("MinimumOccurance", typeof(int));
+    
+            return ((IObjectContextAdapter)this).ObjectContext.ExecuteFunction("sp_GetSuspiciousNumbers", fromDateParameter, toDateParameter, strategyIdParameter, suspectionListParameter, minimumOccuranceParameter);
         }
     }
 }
