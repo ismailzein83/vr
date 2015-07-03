6953ff0d783a65246ad2db2d38ff056e8336c5a7
walid.mogharbel <walid.mogharbel@vanrise.tfs.local>
2015-07-03 09:19:50 +0000

    Scheduler
    git-tfs-id: [http://developmentvlan:8585/tfs/vanrise.collection]$/;C24377

--- Vanrise/Code/Vanrise.Runtime.Entities/SchedulerTask.cs
+++ Vanrise/Code/Vanrise.Runtime.Entities/SchedulerTask.cs
@@ -1,12 +1,25 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel;
 using System.Linq;
+using System.Reflection;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace Vanrise.Runtime.Entities
 {
-    public enum SchedulerTaskStatus { NotStarted = 0, Started = 1, Stopped = 2 } 
+
+    public enum SchedulerTaskStatus
+    {
+        [Description("Not Started")]
+        NotStarted = 0,
+
+        [Description("Started")]
+        Started = 1,
+
+        [Description("Stopped")]
+        Stopped = 2
+    } 
 
     public class SchedulerTask
     {
@@ -30,5 +43,23 @@ namespace Vanrise.Runtime.Entities
 
         public SchedulerTaskAction TaskAction { get; set; }
 
+
+
+        public static string GetEnumDescription(Enum value)
+        {
+            FieldInfo fi = value.GetType().GetField(value.ToString());
+
+            DescriptionAttribute[] attributes =
+                (DescriptionAttribute[])fi.GetCustomAttributes(typeof(DescriptionAttribute), false);
+
+            if (attributes != null && attributes.Length > 0)
+                return attributes[0].Description;
+            else
+                return value.ToString();
+        }
+
     }
+
+
+
 }
