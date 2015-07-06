3e66db2cb093df660024ed9f8b0ae0a42987e018
mohamad.elhariri <mohamad.elhariri@vanrise.tfs.local>
2015-07-06 07:21:52 +0000

    added TestPage to BusinessEntity
    git-tfs-id: [http://developmentvlan:8585/tfs/vanrise.collection]$/;C24415

---
+++ TOneV2/Code/TOneV2/TOne.BusinessEntity.Web/BusinessEntity/Views/TestPageController.js
@@ -0,0 +1,18 @@
+﻿TestPageController.$inject = ['$scope'];
+
+function TestPageController($scope) {
+  
+    defineScope();
+    load();
+
+    function defineScope() {
+
+        $scope.testModel = 'BusinessEntity_TestPageController';
+    }
+
+    function load() {
+       
+    }
+}
+
+appControllers.controller('BusinessEntity_TestPageController', TestPageController);
