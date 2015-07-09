5a4387c2e540c8ad29b634d4dc1af03c9404a006
Ismail Zein <VANRISE\ismail.zein>
2015-07-09 12:20:42 +0000

    added StreamReaderImportedData
    git-tfs-id: [http://developmentvlan:8585/tfs/vanrise.collection]$/;C24596

--- Vanrise/Code/Vanrise.Integration.Entities/ImportedDataTypes/TextImportedData.cs
+++ Vanrise/Code/Vanrise.Integration.Entities/ImportedDataTypes/TextImportedData.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -14,6 +15,16 @@ namespace Vanrise.Integration.Entities
         public string Description
         {
             get { return FileName; }
+        }
+    }
+
+    public class StreamReaderImportedData : IImportedData
+    {
+        public StreamReader StreamReader { get; set; }
+
+        public string Description
+        {
+            get { return null; }
         }
     }
 }
