4826183cec774877ec8f08985e9c4382a9698c71
walid.mogharbel <walid.mogharbel@vanrise.tfs.local>
2015-07-09 08:23:31 +0000

    FTP Adapter
    git-tfs-id: [http://developmentvlan:8585/tfs/vanrise.collection]$/;C24567

---
+++ Vanrise/Code/Vanrise.Integration.Adapters.FtpReceiveAdapter/FtpReceiverAdapter.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Vanrise.Integration.Entities;
+
+namespace Vanrise.Integration.Adapters.FtpReceiverAdapter
+{
+    public class FileReceiveAdapter : BaseReceiveAdapter
+    {
+        public string FolderPath { get; set; }
+
+        public override void ImportData(Action<object> receiveData)
+        {
+            //foreach (var fileObj in Directory.EnumerateFiles(this.FolderPath, "*.txt"))
+            //{
+                
+            //}
+
+            receiveData("Extracted Data");
+        }
+    }
+}
