ae8c88110e4d021060e8b64965d3ce1669043154
hadi.hawi <hadi.hawi@vanrise.tfs.local>
2015-07-09 07:38:30 +0000

    Billing Manager Partial
    git-tfs-id: [http://developmentvlan:8585/tfs/vanrise.collection]$/;C24556

--- TOneV2/Code/TOneV2/TOne.Analytics.Business/BillingStatisticManagerPartial.cs
+++ TOneV2/Code/TOneV2/TOne.Analytics.Business/BillingStatisticManagerPartial.cs
@@ -42,10 +42,10 @@ namespace TOne.Analytics.Business
                        Customer = s.Key.Customer,
                        Destination = s.Key.Destination,
                        SaleDuration = string.Format("{0:#,##0.00}", s.Sum(k => k.Sale_Duration)),
-                       SaleRate = string.Format("{0:0.00000}", (s.Sum(k => k.Sale_Rate * (double) k.Sale_Duration) / (double)s.Sum(k => k.Sale_Duration))),
+                       SaleRate = string.Format("{0:0.0000}", (s.Sum(k => k.Sale_Rate * (double) k.Sale_Duration) / (double)s.Sum(k => k.Sale_Duration))),
                        Supplier = s.Key.Supplier,
                        CostDuration = string.Format("{0:#,##0.00}", s.Sum(k => k.Cost_Duration)),
-                       CostRate = string.Format("{0:0.00000}", (s.Sum(k => k.Cost_Rate * (double)k.Cost_Duration) / (double)s.Sum(k => k.Cost_Duration))),
+                       CostRate = string.Format("{0:0.0000}", (s.Sum(k => k.Cost_Rate * (double)k.Cost_Duration) / (double)s.Sum(k => k.Cost_Duration))),
                        Profit = FormatNumber( s.Sum(k => k.Profit)),
                        ProfitPerc = string.Format("{0:#,##0.00}%", s.Average(k => k.Profit_Perc))
                    }).OrderBy(s => s.Customer + s.Destination + s.Supplier).ToList();
@@ -57,97 +57,6 @@ namespace TOne.Analytics.Business
         {
 
             return FormatRoutingsAnalyics(_datamanager.GetRoutingAnalysis(fromDate, toDate, customerId, supplierId, top, supplierAMUId, customerAMUId));
-        }
-
-        public VariationReportResult GetInOutReportsData(DateTime selectedDate, int periodCount, TimePeriod timePeriod, VariationReportOptions variationReportOptions, int fromRow, int toRow, EntityType entityType, string entityID, GroupingBy groupingBy)
-        {
-            List<VariationReportsData> customersList = new List<VariationReportsData>();
-            List<VariationReportsData> onlyCustomersList = new List<VariationReportsData>();
-            List<VariationReportsData> suppliersList = new List<VariationReportsData>();
-            List<VariationReportsData> customersAndSuppliersList = new List<VariationReportsData>();
-            List<TimeRange> timeRanges;
-
-
-
-         //   switch (variationReportOptions)
-        //    {
-              //  case VariationReportOptions.InOutBoundMinutes:
-                    var customersResult = GetVariationReportsData(selectedDate, periodCount, timePeriod, VariationReportOptions.InBoundMinutes, fromRow, toRow, entityType, entityID, groupingBy);
-                   timeRanges = customersResult.TimeRange;
-                    foreach (var customer in customersResult.VariationReportsData)
-                    {
-                        customersList.Add(customer);
-                    
-                    }
-                    var suppliersResult = GetVariationReportsData(selectedDate, periodCount, timePeriod, VariationReportOptions.OutBoundMinutes, fromRow, toRow, entityType, entityID, groupingBy);
-                    foreach (var supplier in suppliersResult.VariationReportsData)
-                    {
-                        suppliersList.Add(supplier);
-                    
-                    }
-                    
-                    foreach (var customer in customersList)
-                    { 
-                        var matchedSupplier = suppliersList.FirstOrDefault(s=>s.ID==customer.ID);
-                        if (matchedSupplier!=null)
-                            {                            
-                               //create 3 item in reslut list : IN, Out ,Total
-                                var name = customer.Name;
-                              customer.Name = string.Format("{0}/IN",name);  
-                              customersAndSuppliersList.Add(customer); 
-                              matchedSupplier.Name = string.Format("{0}/OUT",name);
-                              customersAndSuppliersList.Add(matchedSupplier);
-                            
-                            //Total:
-                            var total = new VariationReportsData();
-                            total.Name = string.Format("{0}/TOTAL", name);  
-                            total.PeriodTypeValueAverage = customer.PeriodTypeValueAverage - matchedSupplier.PeriodTypeValueAverage  ;
-                            total.PeriodTypeValuePercentage =  customer.PeriodTypeValuePercentage - matchedSupplier.PeriodTypeValuePercentage ;
-                            total.PreviousPeriodTypeValuePercentage = matchedSupplier.PreviousPeriodTypeValuePercentage - customer.PreviousPeriodTypeValuePercentage;
-                           // List<float> totalValues = new List<float>();
-                            List<decimal> totalValues = new List<decimal>();
-                            for(int i=0; i<customer.Values.Count;i++)
-                            // totalValues.Add( float.Parse(matchedSupplier.Values[i].ToString()) - float.Parse(customer.Values[i].ToString() ) );
-                            totalValues.Add(customer.Values[i] - matchedSupplier.Values[i] );
-                            total.Values = totalValues;
-                            customersAndSuppliersList.Add(total);
-
-                            //remove out element from out list
-                            suppliersList.Remove(matchedSupplier);
-
-                            }
-                            else {//add item to in list
-                                   onlyCustomersList.Add(customer); }
-                       }
-                     customersAndSuppliersList =  customersAndSuppliersList.OrderBy(item=>item.Name).ToList();
-
-                        //add items in in list
-                        //add items remaining in out list
-                    foreach(var item in onlyCustomersList)
-                          item.Name = string.Format("{0}/IN",item.Name);
-                    foreach (var item in suppliersList)
-                        item.Name = string.Format("{0}/OUT", item.Name);
-
-                    customersAndSuppliersList.AddRange(onlyCustomersList.OrderBy(customer => customer.Name).ToList());
-
-                    customersAndSuppliersList.AddRange(suppliersList.OrderBy(supplier=>supplier.Name).ToList());
-                   
-                 // return //  VariationReportResult inOutData = 
-               //     break;
-
-                 
-
-                    
-
-
-                //case VariationReportOptions.InOutBoundAmount:
-                //    resultInList.Add(GetVariationReportsData(selectedDate, periodCount, timePeriod, VariationReportOptions.InBoundAmount, fromRow, toRow, entityType, entityID, groupingBy));
-                //    resultOutList.Add(GetVariationReportsData(selectedDate, periodCount, timePeriod, VariationReportOptions.OutBoundAmount, fromRow, toRow, entityType, entityID, groupingBy));
-                //    break;
-          //  }
-            return new VariationReportResult() { VariationReportsData = customersAndSuppliersList, TimeRange = timeRanges };
-
-           
         }
 
         #region PrivateMethode
@@ -212,14 +121,14 @@ namespace TOne.Analytics.Business
                 DurationFormatted = FormatNumber(obj.Duration),
 
                 ACD = obj.ACD ,
-                ACDFormatted = FormatNumber(obj.ACD,5),
+                ACDFormatted = FormatNumber(obj.ACD,4),
 
 
                 ASR = obj.ASR,
-                ASRFormatted = FormatNumber(obj.ASR, 5),
+                ASRFormatted = FormatNumber(obj.ASR, 4),
 
-                Profit = FormatNumber((double)obj.SaleNet - (double)obj.CostNet),
-
+                Profit = (double)((double)obj.SaleNet - (double)obj.CostNet),
+                ProfitFormatted = FormatNumber((double)obj.SaleNet - (double)obj.CostNet),
                 AVGCost = ((double)obj.Duration == 0 || (double)obj.CostNet == 0) ? 0 : (double)((double)obj.CostNet / (double)obj.Duration),
                 AVGCostFormatted = ((double)obj.Duration == 0 || (double)obj.CostNet == 0) ? "0" : FormatNumber((double)((double)obj.CostNet / (double)obj.Duration), 5),
                 AVGSale = ((double)obj.Duration == 0 || (double)obj.SaleNet == 0) ? 0 : (double)((double)obj.SaleNet / (double)obj.Duration),
