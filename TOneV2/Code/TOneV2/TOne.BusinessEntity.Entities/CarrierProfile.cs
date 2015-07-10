42baa94db5fdf2810e87adbc968fb1a33ce647c2
rayan.kazma <rayan.kazma@vanrise.tfs.local>
2015-07-10 06:59:33 +0000

    Adding CarrierProfile Entity
    git-tfs-id: [http://developmentvlan:8585/tfs/vanrise.collection]$/;C24611

---
+++ TOneV2/Code/TOneV2/TOne.BusinessEntity.Entities/CarrierProfile.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TOne.BusinessEntity.Entities
+{
+    public class CarrierProfile
+    {
+        public int ProfileID { get; set; }
+        public string Name { get; set; }
+        public string CompanyName { get; set; }
+        public string BillingEmail { get; set; }
+    }
+}
