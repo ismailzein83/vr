0bc62234530554239a6621f9ea0c37de9fb8e6f8
walid.mogharbel <walid.mogharbel@vanrise.tfs.local>
2015-07-13 13:52:13 +0000

    BaseDB
    git-tfs-id: [http://developmentvlan:8585/tfs/vanrise.collection]$/;C24688

---
+++ Vanrise/Code/Vanrise.Integration.Adapters.IBaseDB/DBReceiveAdapter.cs
@@ -0,0 +1,18 @@
+﻿using Vanrise.Integration.Entities;
+
+namespace Vanrise.Integration.Adapters.BaseDB
+{
+    public abstract class DBReceiveAdapter : BaseReceiveAdapter
+    {
+        #region Properties
+
+        public string ConnectionString { get; set; }
+
+        public string Description { get; set; }
+
+        public string Query { get; set; }
+
+
+        # endregion
+    }
+}
