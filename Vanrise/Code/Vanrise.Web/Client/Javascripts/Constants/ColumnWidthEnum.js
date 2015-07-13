09507285c6d5a1d60487579abcb1b8a085fe8cd5
Samer Haidar <VANRISE\samer.haidar>
2015-07-13 13:32:15 +0000

    Adding columnWidthEnum
    git-tfs-id: [http://developmentvlan:8585/tfs/vanrise.collection]$/;C24684

---
+++ Vanrise/Code/Vanrise.Web/Client/Javascripts/Constants/ColumnWidthEnum.js
@@ -0,0 +1,7 @@
+﻿app.constant('ColumnWidthEnum', {
+    QuarterRow: { name: "QuarterRow", value: 2, description: "Quarter Row" },
+    FullRow: { name: "FullRow", value: 12, description: "Full Row" },
+    HalfRow: { name: "HalfRow", value: 6, description: "Half Row" },
+    OneThirdRow: { name: "OneThirdRow", value: 4, description: "1/3 Row" },
+});
+
