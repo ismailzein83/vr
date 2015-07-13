dc8ff4e5fb50cfcebbfd8a78e6ed927be7fc47a2
walid.mogharbel <walid.mogharbel@vanrise.tfs.local>
2015-07-13 13:28:20 +0000

    SQL, MYSQL Adapters updated
    git-tfs-id: [http://developmentvlan:8585/tfs/vanrise.collection]$/;C24683

---
+++ Vanrise/Code/Vanrise.Integration.Adapters.MySQLReceiveAdapter/MySQLReceiveAdapter.cs
@@ -0,0 +1,39 @@
+﻿using MySql.Data.MySqlClient;
+using System;
+using System.Data.SqlClient;
+using Vanrise.Integration.Entities;
+
+namespace Vanrise.Integration.Adapters.MSQLReceiveAdapter
+{
+
+    public class MySQLReceiveAdapter : BaseReceiveAdapter
+    {
+        #region Properties
+
+        public string ConnectionString { get; set; }
+
+        public string Description { get; set; }
+
+        public string Query { get; set; }
+
+
+        # endregion
+
+
+
+        public override void ImportData(Action<IImportedData> receiveData)
+        {
+            using (var connection = new MySqlConnection(this.ConnectionString))
+            {
+                connection.Open();
+                var command = new MySqlCommand(Query, connection);
+                DBReaderImportedData data = new DBReaderImportedData();
+                data.Reader = command.ExecuteReader();
+                Description = data.Description;
+                receiveData(data);
+            }
+
+        }
+
+    }
+}
---
+++ Vanrise/Code/Vanrise.Integration.Adapters.SQLReceiveAdapter/SQLReceiveAdapter.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Data.SqlClient;
+using Vanrise.Integration.Entities;
+
+namespace Vanrise.Integration.Adapters.SQLReceiveAdapter
+{
+
+    public class SQLReceiveAdapter : BaseReceiveAdapter
+    {
+        #region Properties
+
+        public string ConnectionString { get; set; }
+
+        public string Description { get; set; }
+
+        public string Query { get; set; }
+       
+
+        # endregion
+
+
+
+        public override void ImportData(Action<IImportedData> receiveData)
+        {
+            using (var connection = new SqlConnection(this.ConnectionString))
+            {
+                connection.Open();
+                var command = new SqlCommand(Query, connection);
+                DBReaderImportedData data = new DBReaderImportedData();
+                data.Reader = command.ExecuteReader();
+                Description = data.Description;
+                receiveData(data);
+            }
+
+        }
+
+    }
+}
