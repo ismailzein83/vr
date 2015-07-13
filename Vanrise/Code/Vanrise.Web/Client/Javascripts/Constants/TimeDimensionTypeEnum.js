85eeb96503383c7a9d6feaf0b06ab8632924d56d
Samer Haidar <VANRISE\samer.haidar>
2015-07-13 10:13:04 +0000

    Adding two enum JS
    git-tfs-id: [http://developmentvlan:8585/tfs/vanrise.collection]$/;C24662

---
+++ Vanrise/Code/Vanrise.Web/Client/Javascripts/Constants/TimeDimensionTypeEnum.js
@@ -0,0 +1,8 @@
+﻿
+app.constant('TimeDimensionTypeEnum', {
+    Yearly: { value: 0, description: "Yearly" },
+    Monthly: { value: 1, description: "Monthly" },
+    Weekly: { value: 2, description: "Weekly" },
+    Daily: { value: 3, description: "Daily" },
+    Hourly: { value: 4, description: "Hourly" }
+});
