5bfb2da28ef94329a776d6686776f5faff5cbd77
walid.mogharbel <walid.mogharbel@vanrise.tfs.local>
2015-07-13 13:56:04 +0000

    adpater
    git-tfs-id: [http://developmentvlan:8585/tfs/vanrise.collection]$/;C24692

--- Vanrise/Code/Vanrise.Integration.Adapters.SQLReceiveAdapter/SQLReceiveAdapter.cs
+++ Vanrise/Code/Vanrise.Integration.Adapters.SQLReceiveAdapter/SQLReceiveAdapter.cs
@@ -1,25 +1,13 @@
 ﻿using System;
 using System.Data.SqlClient;
+using Vanrise.Integration.Adapters.BaseDB;
 using Vanrise.Integration.Entities;
 
 namespace Vanrise.Integration.Adapters.SQLReceiveAdapter
 {
 
-    public class SQLReceiveAdapter : BaseReceiveAdapter
+    public class SQLReceiveAdapter :  DBReceiveAdapter
     {
-        #region Properties
-
-        public string ConnectionString { get; set; }
-
-        public string Description { get; set; }
-
-        public string Query { get; set; }
-       
-
-        # endregion
-
-
-
         public override void ImportData(Action<IImportedData> receiveData)
         {
             using (var connection = new SqlConnection(this.ConnectionString))
