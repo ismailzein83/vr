2c3b109e4c5df5d25b4e6a02fa0adcf7fc3622dc
sara.yahfoufi <sara.yahfoufi@vanrise.tfs.local>
2015-07-13 08:29:56 +0000

    Variation reports data
    git-tfs-id: [http://developmentvlan:8585/tfs/vanrise.collection]$/;C24656

--- TOneV2/Code/TOneV2/TOne.Analytics.Web/Analytics/Constants/EntityTypeEnum.js
+++ TOneV2/Code/TOneV2/TOne.Analytics.Web/Analytics/Constants/EntityTypeEnum.js
@@ -2,5 +2,6 @@
     none: { value: 0, description: "none" },
     Customer: { value: 1, description: "Customer" },
     Supplier: { value: 2, description: "Supplier" },
-    Zone: { value: 3, description: "Zone" }
+    Zone: { value: 3, description: "Zone" },
+    Profit:{value:4 ,dexcription:"Profit"}
 });
