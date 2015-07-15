165617b779d7a9d48d62982e6c432d3356400df0
sara.yahfoufi <sara.yahfoufi@vanrise.tfs.local>
2015-07-15 13:38:08 +0000

    Volume Reports Data
    git-tfs-id: [http://developmentvlan:8585/tfs/vanrise.collection]$/;C24828

--- TOneV2/Code/TOneV2/TOne.Analytics.Web/Analytics/Views/Billing Statistics/Volume Reports/Volume Reports Templates/TrafficVolumeTemplateController.js
+++ TOneV2/Code/TOneV2/TOne.Analytics.Web/Analytics/Views/Billing Statistics/Volume Reports/Volume Reports Templates/TrafficVolumeTemplateController.js
@@ -1,1 +1,87 @@
-﻿
+﻿TrafficVolumeTemplateController.$inject = ['$scope', 'BillingStatisticsAPIService', 'VolumeReportsTimePeriodEnum', 'VolumeReportsOptionsEnum', 'CarrierTypeEnum', 'CarrierAPIService', 'ZonesService'];
+function TrafficVolumeTemplateController($scope, BillingStatisticsAPIService, VolumeReportsTimePeriodEnum, VolumeReportsOptionsEnum, CarrierTypeEnum, CarrierAPIService, ZonesService) {
+
+    var fromDate;
+    var toDate;
+    var selectedTrafficReport;
+    var timePeriod;
+    var customerId;
+    var supplierId;
+    var zoneId;
+    var attempts;
+    var chartAPI;
+   
+
+    defineScope();
+    load();
+
+
+    function defineScope() {
+        $scope.chartData = [];
+        $scope.chartReady = function (api) {
+            chartAPI = api;
+        };
+    }
+
+    function load() {
+        loadFilters();
+        loadTrafficVolumeData();
+    }
+    function loadFilters() {
+        fromDate = $scope.filter.fromDate;
+        toDate =  $scope.filter.toDate;
+        selectedTrafficReport = $scope.filter.selectedTrafficReport;
+        $scope.selectedTrafficReport = selectedTrafficReport;
+        timePeriod = $scope.filter.timePeriod;
+        customerId = $scope.filter.customerId;
+        supplierId = $scope.filter.supplierId; 
+        zoneId = $scope.filter.zoneId;
+        attempts = $scope.filter.attempts;
+    }
+
+
+    function loadTrafficVolumeData() {
+        $scope.isLoading = true;
+        return BillingStatisticsAPIService.GetVolumeReportData(fromDate, toDate, customerId, supplierId, zoneId, attempts, timePeriod.description, selectedTrafficReport.value).then(function (response) {
+            $scope.chartData.length = 0;
+            angular.forEach(response, function (item) {
+                var attempts = item.Attempts;
+                var duration = item.Duration;
+                $scope.chartData.push(item);
+            });
+            updateChart($scope.chartData);
+
+        }).finally(function () {
+            $scope.isLoading = false;
+        });
+    }
+    function updateChart(chartData) {
+        var chartDefinition = {
+            type: "column",
+            title: "Traffic volume"
+        };
+        var xAxisDefinition = {
+            titlePath: "Values"
+        };
+        var seriesDefinitions = [];
+        var seriesDefinitions = [{
+            title: "Attempts",
+            valuePath: "Values[0]",
+            type: "column"
+        }, {
+            title: "Durations",
+            valuePath: "Values[1]",
+            type: "column"
+        }
+        ];
+
+        console.log(seriesDefinitions);
+        chartAPI.renderChart(chartData, chartDefinition, seriesDefinitions, xAxisDefinition);
+
+
+
+    }
+
+
+};
+appControllers.controller('Analytics_TrafficVolumeTemplateController', TrafficVolumeTemplateController);
--- TOneV2/Code/TOneV2/TOne.Analytics.Web/Analytics/Views/Billing Statistics/Volume Reports/VolumeReportsController.js
+++ TOneV2/Code/TOneV2/TOne.Analytics.Web/Analytics/Views/Billing Statistics/Volume Reports/VolumeReportsController.js
@@ -1,7 +1,7 @@
 ﻿VolumeReportsController.$inject = ['$scope', 'BillingStatisticsAPIService', 'VolumeReportsTimePeriodEnum', 'VolumeReportsOptionsEnum', 'CarrierTypeEnum', 'CarrierAPIService', 'ZonesService'];
 function VolumeReportsController($scope, BillingStatisticsAPIService, VolumeReportsTimePeriodEnum, VolumeReportsOptionsEnum, CarrierTypeEnum, CarrierAPIService, ZonesService) {
 
-    var chartAPI;
+    //var chartAPI;
     defineScope();
     load();
 
@@ -32,15 +32,20 @@ function VolumeReportsController($scope, BillingStatisticsAPIService, VolumeRepo
         $scope.onSearch = function () {
              return getVolumeReportsData();         
         }
-        $scope.chartReady = function (api) {
-            chartAPI = api;
-        };
+        //$scope.chartReady = function (api) {
+        //    chartAPI = api;
+        //};
     }
     function buildFilter() {
         var filter = {};
-        filter.CustomerId = $scope.selectedCustomer.CarrierAccountID;//getFilterIds($scope.selectedCustomer, "CarrierAccountID");
-        filter.SupplierId = $scope.selectedSupplier.CarrierAccountID; //getFilterIds($scope.selectedSupplier, "CarrierAccountID");
-        filter.ZoneId = $scope.selectedZone.ZoneId;//getFilterIds($scope.selectedZone, "ZoneId");  
+        filter.fromDate =  $scope.fromDate;
+        filter.toDate = $scope.toDate;
+        filter.selectedTrafficReport = $scope.selectedTrafficReport;
+        filter.timePeriod = $scope.selectedTimePeriod;
+        filter.customerId = $scope.selectedCustomer.CarrierAccountID;//getFilterIds($scope.selectedCustomer, "CarrierAccountID");
+        filter.supplierId = $scope.selectedSupplier.CarrierAccountID; //getFilterIds($scope.selectedSupplier, "CarrierAccountID");
+        filter.zoneId = $scope.selectedZone.ZoneId;//getFilterIds($scope.selectedZone, "ZoneId");  
+        filter.attempts = $scope.attempts;
         return filter;
     }
 
@@ -86,51 +91,53 @@ function VolumeReportsController($scope, BillingStatisticsAPIService, VolumeRepo
     function getVolumeReportsData() {
          $scope.isLoading = true;
          var filter = buildFilter();
-         $scope.showTrafficVolume = $scope.selectedTrafficReport == VolumeReportsOptionsEnum.TrafficVolumes ;
-         $scope.showInOutTaffic = $scope.selectedTrafficReport == VolumeReportsOptionsEnum.CompareInOutTraffic;
-         $scope.destinationTrafficvolume = $scope.selectedTrafficReport == VolumeReportsOptionsEnum.DestinationTrafficVolumes;
+         $scope.filter = filter;
+         $scope.showTrafficVolume = filter.selectedTrafficReport == VolumeReportsOptionsEnum.TrafficVolumes ;
+         $scope.showInOutTaffic = filter.selectedTrafficReport == VolumeReportsOptionsEnum.CompareInOutTraffic;
+         $scope.destinationTrafficvolume = filter.selectedTrafficReport == VolumeReportsOptionsEnum.DestinationTrafficVolumes;
+         $scope.isLoading = false;
 
-         return BillingStatisticsAPIService.GetVolumeReportData($scope.fromDate, $scope.toDate, filter.CustomerId, filter.SupplierId, filter.ZoneId, $scope.attempts, $scope.selectedTimePeriod.description, $scope.selectedTrafficReport.value).then(function (response) {
-            $scope.chartData.length = 0;
-            angular.forEach(response, function (item) {
-                var attempts = item.Attempts;
-                var duration = item.Duration;
-                $scope.chartData.push(item);       
-              });      
-            updateChart( $scope.chartData);
+    //     return BillingStatisticsAPIService.GetVolumeReportData($scope.fromDate, $scope.toDate, filter.CustomerId, filter.SupplierId, filter.ZoneId, $scope.attempts, $scope.selectedTimePeriod.description, $scope.selectedTrafficReport.value).then(function (response) {
+    //        $scope.chartData.length = 0;
+    //        angular.forEach(response, function (item) {
+    //            var attempts = item.Attempts;
+    //            var duration = item.Duration;
+    //            $scope.chartData.push(item);       
+    //          });      
+    //        updateChart( $scope.chartData);
 
-        }).finally(function () {
-            $scope.isLoading = false;
-        });
+    //    }).finally(function () {
+    //        $scope.isLoading = false;
+    //    });
     }
 
 
-    function updateChart(chartData) {
-        var chartDefinition = {
-            type: "column",
-            title: "Traffic volume"
-        };
-        var xAxisDefinition = {
-            titlePath: "Values"
-        };
-        var seriesDefinitions = [];
-        var seriesDefinitions = [{
-            title: "Attempts",
-            valuePath: "Values[0]",
-            type: "column"
-        }, {
-            title: "Durations",
-            valuePath: "Values[1]",
-            type: "column"
-        }
-        ];
+    //function updateChart(chartData) {
+    //    var chartDefinition = {
+    //        type: "column",
+    //        title: "Traffic volume"
+    //    };
+    //    var xAxisDefinition = {
+    //        titlePath: "Values"
+    //    };
+    //    var seriesDefinitions = [];
+    //    var seriesDefinitions = [{
+    //        title: "Attempts",
+    //        valuePath: "Values[0]",
+    //        type: "column"
+    //    }, {
+    //        title: "Durations",
+    //        valuePath: "Values[1]",
+    //        type: "column"
+    //    }
+    //    ];
 
-        console.log(seriesDefinitions);
-        chartAPI.renderChart(chartData, chartDefinition, seriesDefinitions, xAxisDefinition);
+    //    console.log(seriesDefinitions);
+    //    chartAPI.renderChart(chartData, chartDefinition, seriesDefinitions, xAxisDefinition);
      
 
             
-    }
+    //}
    
 };
 
