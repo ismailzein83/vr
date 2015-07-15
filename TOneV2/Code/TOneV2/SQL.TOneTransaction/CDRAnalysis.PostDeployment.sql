ad2155c011b2e5f9c04f66fc15791445fa7fdaef
Ismail Zein <VANRISE\ismail.zein>
2015-07-15 08:51:09 +0000

    structured post deployment script
    git-tfs-id: [http://developmentvlan:8585/tfs/vanrise.collection]$/;C24792

--- TOneV2/Code/TOneV2/SQL.TOneTransaction/CDRAnalysis.PostDeployment.sql
+++ TOneV2/Code/TOneV2/SQL.TOneTransaction/CDRAnalysis.PostDeployment.sql
@@ -9,29 +9,7 @@ Post-Deployment Script Template
                SELECT * FROM [$(TableName)]					
 --------------------------------------------------------------------------------------
 */
-MERGE INTO bp.LKUP_ExecutionStatus AS Target 
-USING (VALUES 
-	(0, N'New'),
-	(10, N'Running'),
-	(20, N'Process Failed'),
-	(50, N'Completed'),
-	(60, N'Aborted'),
-	(70, N'Suspended'),
-	(80, N'Terminated')
 
-) 
-AS Source ([ID], [Description])
-ON Target.[ID] = Source.[ID] 
--- update matched rows 
-WHEN MATCHED THEN 
-UPDATE SET [Description] = Source.[Description] 
--- insert new rows 
-WHEN NOT MATCHED BY TARGET THEN 
-INSERT ([ID], [Description])
-VALUES ([ID], [Description]) 
--- delete rows that are in the target but not the source 
-WHEN NOT MATCHED BY SOURCE THEN 
-DELETE;
 
 
 MERGE INTO bp.[BPDefinition] AS Target 
@@ -56,46 +34,3 @@ VALUES ([Name], [Title], [FQTN], [Config])
 WHEN NOT MATCHED BY SOURCE THEN 
 DELETE
 ;
-
-
-MERGE INTO runtime.[SchedulerTaskTriggerType] AS Target 
-USING (VALUES 
-	(1, N'Timer', N'{"URL":"/Client/Modules/Runtime/Views/TriggerTemplates/TimerTriggerTemplate.html"}')
-) 
-AS Source ([ID], [Name], [TriggerTypeInfo])
-ON Target.[ID] = Source.[ID] 
--- update matched rows 
-WHEN MATCHED THEN 
-UPDATE SET	[ID] = Source.[ID],
-			[Name] = Source.[Name],
-			[TriggerTypeInfo]  = Source.[TriggerTypeInfo]
--- insert new rows 
-WHEN NOT MATCHED BY TARGET THEN 
-INSERT ([ID], [Name], [TriggerTypeInfo])
-VALUES ([ID], [Name], [TriggerTypeInfo])
----- delete rows that are in the target but not the source 
-WHEN NOT MATCHED BY SOURCE THEN 
-DELETE
-;
-
-MERGE INTO runtime.[SchedulerTaskActionType] AS Target 
-USING (VALUES 
-	(1, N'Workflow', N'{"URL":"/Client/Modules/Runtime/Views/ActionTemplates/WFActionTemplate.html"}'),
-	(2, N'Data Source', N'{"URL":""}')
-) 
-AS Source ([ID], [Name], [ActionTypeInfo])
-ON Target.[ID] = Source.[ID] 
--- update matched rows 
-WHEN MATCHED THEN 
-UPDATE SET	[ID] = Source.[ID],
-			[Name] = Source.[Name],
-			[ActionTypeInfo]  = Source.[ActionTypeInfo]
--- insert new rows 
-WHEN NOT MATCHED BY TARGET THEN 
-INSERT ([ID], [Name], [ActionTypeInfo])
-VALUES ([ID], [Name], [ActionTypeInfo])
----- delete rows that are in the target but not the source 
-WHEN NOT MATCHED BY SOURCE THEN 
-DELETE
-;
-
---
+++ TOneV2/Code/TOneV2/SQL.TOneTransaction/Common.PostDeployment.sql
@@ -0,0 +1,78 @@
+﻿/*
+Post-Deployment Script Template							
+--------------------------------------------------------------------------------------
+ This file contains SQL statements that will be appended to the build script.		
+ Use SQLCMD syntax to include a file in the post-deployment script.			
+ Example:      :r .\myfile.sql								
+ Use SQLCMD syntax to reference a variable in the post-deployment script.		
+ Example:      :setvar TableName MyTable							
+               SELECT * FROM [$(TableName)]					
+--------------------------------------------------------------------------------------
+*/
+MERGE INTO bp.LKUP_ExecutionStatus AS Target 
+USING (VALUES 
+	(0, N'New'),
+	(10, N'Running'),
+	(20, N'Process Failed'),
+	(50, N'Completed'),
+	(60, N'Aborted'),
+	(70, N'Suspended'),
+	(80, N'Terminated')
+
+) 
+AS Source ([ID], [Description])
+ON Target.[ID] = Source.[ID] 
+-- update matched rows 
+WHEN MATCHED THEN 
+UPDATE SET [Description] = Source.[Description] 
+-- insert new rows 
+WHEN NOT MATCHED BY TARGET THEN 
+INSERT ([ID], [Description])
+VALUES ([ID], [Description]) 
+-- delete rows that are in the target but not the source 
+WHEN NOT MATCHED BY SOURCE THEN 
+DELETE;
+
+
+
+MERGE INTO runtime.[SchedulerTaskTriggerType] AS Target 
+USING (VALUES 
+	(1, N'Timer', N'{"URL":"/Client/Modules/Runtime/Views/TriggerTemplates/TimerTriggerTemplate.html"}')
+) 
+AS Source ([ID], [Name], [TriggerTypeInfo])
+ON Target.[ID] = Source.[ID] 
+-- update matched rows 
+WHEN MATCHED THEN 
+UPDATE SET	[ID] = Source.[ID],
+			[Name] = Source.[Name],
+			[TriggerTypeInfo]  = Source.[TriggerTypeInfo]
+-- insert new rows 
+WHEN NOT MATCHED BY TARGET THEN 
+INSERT ([ID], [Name], [TriggerTypeInfo])
+VALUES ([ID], [Name], [TriggerTypeInfo])
+---- delete rows that are in the target but not the source 
+WHEN NOT MATCHED BY SOURCE THEN 
+DELETE
+;
+
+MERGE INTO runtime.[SchedulerTaskActionType] AS Target 
+USING (VALUES 
+	(1, N'Workflow', N'{"URL":"/Client/Modules/Runtime/Views/ActionTemplates/WFActionTemplate.html"}'),
+	(2, N'Data Source', N'{"URL":""}')
+) 
+AS Source ([ID], [Name], [ActionTypeInfo])
+ON Target.[ID] = Source.[ID] 
+-- update matched rows 
+WHEN MATCHED THEN 
+UPDATE SET	[ID] = Source.[ID],
+			[Name] = Source.[Name],
+			[ActionTypeInfo]  = Source.[ActionTypeInfo]
+-- insert new rows 
+WHEN NOT MATCHED BY TARGET THEN 
+INSERT ([ID], [Name], [ActionTypeInfo])
+VALUES ([ID], [Name], [ActionTypeInfo])
+---- delete rows that are in the target but not the source 
+WHEN NOT MATCHED BY SOURCE THEN 
+DELETE
+;
+
