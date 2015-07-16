6d2ca970c1a8534642c013e50c58c91a81aff0ab
walid.mogharbel <walid.mogharbel@vanrise.tfs.local>
2015-07-16 08:18:56 +0000

    file
    git-tfs-id: [http://developmentvlan:8585/tfs/vanrise.collection]$/;C24842

--- Vanrise/Code/Vanrise.Integration.Adapters.FileReceiverAdapter/FileReceiverAdapter.cs
+++ Vanrise/Code/Vanrise.Integration.Adapters.FileReceiverAdapter/FileReceiverAdapter.cs
@@ -10,28 +10,74 @@ namespace Vanrise.Integration.Adapters.FileReceiveAdapter
 {
     public class FileReceiveAdapter : BaseReceiveAdapter
     {
+
+        public enum Actions
+        {
+            Rename = 0,
+            Delete = 1,
+            Move = 2 // Move to Folder
+        }
+
+        #region Properties
+        public string Extension { get; set; }
+
         public string FolderPath { get; set; }
 
+        public string DirectorytoMoveFile { get; set; }
+
+        public int ActionAfterImport { get; set; }
+
+        # endregion 
+
+        #region Private Functions
+
+        private void CreateStreamReader(Action<IImportedData> receiveData, FileInfo file)
+        {
+            receiveData(new StreamReaderImportedData()
+            {
+                StreamReader = new StreamReader(this.FolderPath + "/" + file.Name),
+                Modified = file.LastWriteTime,
+                Name = file.Name,
+                Size = file.Length
+            });
+        }
+
+        private void AfterImport(FileInfo file)
+        {
+            if (ActionAfterImport == (int)Actions.Rename)
+            {
+                file.MoveTo(Path.Combine(file.DirectoryName, file.Name.Replace(this.Extension,".Imported")));
+            }
+            else if (ActionAfterImport == (int)Actions.Delete)
+            {
+                file.Delete();
+            }
+            else if (ActionAfterImport == (int)Actions.Move)
+            {
+                if (Directory.Exists(this.FolderPath))
+                {
+                    file.MoveTo(Path.Combine(DirectorytoMoveFile, file.Name.Replace(this.Extension, ".Imported")));
+                }
+            }
+        }
+
+        #endregion
+
+
+       
         public override void ImportData(Action<IImportedData> receiveData)
         {
-            //foreach (var fileObj in Directory.EnumerateFiles(this.FolderPath, "*.txt"))
-            //{
-                
-            //}
-
-
-
-            DirectoryInfo d = new DirectoryInfo(this.FolderPath);//Assuming Test is your Folder
-            FileInfo[] Files = d.GetFiles("*.DAT"); //Getting Text files
-            string str = "";
-            foreach (FileInfo file in Files)
+            if (Directory.Exists(this.FolderPath))
             {
-                str = str + ", " + file.Name;
+                DirectoryInfo d = new DirectoryInfo(this.FolderPath);//Assuming Test is your Folder
+                FileInfo[] Files = d.GetFiles("*.DAT"); //Getting Text files
+                foreach (FileInfo file in Files)
+                {
+                    CreateStreamReader(receiveData, file);
+                    AfterImport(file);
+
+                }
             }
-
-
-
-            receiveData(new TextFileImportedData { Content = "Extracted Data" });
         }
     }
 }
