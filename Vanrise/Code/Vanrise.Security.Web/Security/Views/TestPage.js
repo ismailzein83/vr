0846373b380ae68813e9b83c295c6fa40b8060e5
Samer Haidar <VANRISE\samer.haidar>
2015-07-16 07:12:10 +0000

    testing tree
    git-tfs-id: [http://developmentvlan:8585/tfs/vanrise.collection]$/;C24837

--- Vanrise/Code/Vanrise.Security.Web/Security/Views/TestPage.js
+++ Vanrise/Code/Vanrise.Security.Web/Security/Views/TestPage.js
@@ -4,28 +4,188 @@ function testpage1($scope) {
     var widgetAPI;
     defineScope();
     load();
-
+    var i = 100;
     function defineScope() {
-        var data = [];
-        for (var i = 0; i < 20; i++) {
+      
+        $scope.values = [
+        {
+            id: "1",
+            Name: "child 1",
+            cheldren: [{
+                id: "1",
+                Name: "child 1",
+            }
+            ,
+            {
+                id: "1",
+                Name: "child 1",
+            }
+            
+            ]
+        },
+        {
+            id: "2",
+            Name: "child 2",
+            cheldren: []
+        }];
+        $scope.Search = function () {
+         
+            $scope.data[0].state.selected = true;
+            $scope.data[0].state.disabled = true;
+        }
+        $scope.add = function () {
             var obj = {
-                id: "child1",
-                parent:"#",
+                "text": "Root",
+                "state": { "opened": true },
+                "children": [
+                    {
+                        "text": "Child node 1",
+                        "state": { "selected": false },
+                        "icon": "jstree-file"
+                    }]
+            }
+             $scope.data.push(obj);
+        }
+        $scope.data =  [
+				{
+				    "id": 1,
+                    "Name":"samer",
+				    "text": "Root",
+				    "state": { "opened": true },
+				    "children": [
+						{
+						    "id": 2,
+						    "text": "Child node 1",
+						    "state": { "selected": false },
+						    "icon": "jstree-file",
+						},
+						{
+						    "id": 3,
+						    "text": "Child node 2",
+						    "state": { "disabled": true }
+						},
+                        {
+                            "id": 4,
+                            "text": "Child node 3",
+                            "state": { "disabled": true }
+                        },
+                        {
+                            "id": 5,
+                            "text": "Child node 2",
+                            "state": { "disabled": true, "opened": true },
+                            "children": [
+                                {
+                                    "id": 6,
+                                    "text": "Inner Child node 1",
+                                    "state": { "selected": false },
+                                    "icon": "jstree-file"
+                                },
+                                {
+                                    "id": 7,
+                                    "text": "Inner Child node 2",
+                                    "state": { "selected": false, "opened": false},
+                                    "children": [
+                                                    {
+                                                        "id": 10,
+                                                        "text": "Inner Child node 1",
+                                                        "state": { "selected": false },
+                                                        "icon": "jstree-file"
+                                                    },
+                                                    {
+                                                        "id": 11,
+                                                        "text": "Inner Child node 2",
+                                                        "state": { "selected": false },
+                                                    }
+                                                ]
+                                },
+                                {
+                                    "id": 9,
+                                    "text": "Inner Child node 3",
+                                    "state": { "selected": false, "opened": false },
+                                    "children": [
+                                                    {
+                                                        "id": 12,
+                                                        "text": "Inner Child node 1",
+                                                        "state": { "selected": false },
+                                                        "icon": "jstree-file"
+                                                    },
+                                                    {
+                                                        "id": 13,
+                                                        "text": "Inner Child node 2",
+                                                        "state": { "selected": false },
+                                                        "children": [
+                                                                     {
+                                                                         "id": 14,
+                                                                         "text": "Inner Child node 1",
+                                                                         "state": { "selected": false },
+                                                                         "icon": "jstree-file"
+                                                                     },
+                                                                    {
+                                                                        "id": 15,
+                                                                         "text": "Inner Child node 2",
+                                                                         "state": { "selected": false },
+                                                                    }
+                                                                ]
+                                                    }
+                                    ]
+                                }
+                            ]
+                        }
+
+				    ]
+				}
+        ];
+        $scope.typesConfig = {
+            "default": {
+                "icon": "http://jstree.com/tree.png"
+            },
+            "demo": {
+                "icon": "glyphicon glyphicon-leaf"
             }
         }
-        $('#using_json_2').jstree({
-            'core': {
-                'data': [
-                   { "id": "ajson1", "parent": "#", "text": "Simple root node" },
-                   { "id": "ajson2", "parent": "#", "text": "Root node 2" },
-                   { "id": "ajson3", "parent": "ajson2", "text": "Child 1" },
-                   { "id": "ajson4", "parent": "ajson2", "text": "Child 2" },
-                ]
-            }
-        });
-    }
-    function load() {
 
+        $scope.readyCB = function () {
+            console.log('ready event call back');
+        };
+        $scope.changedCB = function (e, data) {
+            var obj = {
+                "id":i++,
+                "text": "Root",
+                "state": { "opened": true },
+                "children": [
+                    {
+                        "text": "Child node 1",
+                        "state": { "selected": false },
+                        "icon": "jstree-file"
+                    }]
+            }
+            console.log(data.node.original.Name);
+            addNode($scope.data, 0, obj, data.node.id);
+            console.log('changed event call back');
+
+        };
+        $scope.openNodeCB = function (e, data) {
+            console.log('open-node event call back');
+        };
+    }
+    function addNode(array, i, obj, nodeId) {
+        if (i == array.length)
+            return;
+        else if (array[i].id == nodeId) {
+            array[i].children.push(obj);
+            return;
+        }
+        else {
+            addNode(array, i + 1, obj, nodeId);
+            if (array[i].children.length > 0) {
+                addNode(array[i].children, 0, obj, nodeId);
+            }
+           
+        }
+    }
+
+    function load() {
+        $('#test').jstree();
         $scope.isGettingData = false;
 
     }
