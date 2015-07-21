a34adfa51087d82ab1558aacf29b60cff10095fa
Mortada Issa <VANRISE\mortada.issa>
2015-07-21 15:49:45 +0000

    Implementing data of OrgChart Linked Entity Manager
    git-tfs-id: [http://developmentvlan:8585/tfs/vanrise.collection]$/;C24909

---
+++ Vanrise/Code/Vanrise.Security.Data.SQL/OrgChartLinkedEntityDataManager.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Vanrise.Data.SQL;
+
+namespace Vanrise.Security.Data.SQL
+{
+    public class OrgChartLinkedEntityDataManager : BaseSQLDataManager, IOrgChartLinkedEntityDataManager
+    {
+
+        public OrgChartLinkedEntityDataManager()
+            : base(GetConnectionStringName("SecurityDBConnStringKey", "SecurityDBConnString"))
+        {
+
+        }
+
+        public int? GetLinkedOrgChartId(string linkedEntityIdentifier)
+        {
+            int? orgChartId = null;
+
+            ExecuteReaderSP("sec.sp_OrgChartLinkedEntity_GetLinkedOrgChartId", (reader) =>
+                {
+                    while (reader.Read())
+                    {
+                        orgChartId = (int)reader["OrgChartID"];
+                    }
+
+                }, linkedEntityIdentifier);
+
+            return orgChartId;
+        }
+
+        public void InsertOrUpdate(int orgChartId, string linkedEntityIdentifier)
+        {
+            ExecuteNonQuerySP("sec.sp_OrgChartLinkedEntity_Insert", orgChartId, linkedEntityIdentifier);
+        }
+    }
+}
