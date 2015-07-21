0109928740da00e5cfec5933f74c334b1f79c930
Samer Haidar <VANRISE\samer.haidar>
2015-07-21 08:49:00 +0000

    Adiing new enum for charts
    git-tfs-id: [http://developmentvlan:8585/tfs/vanrise.collection]$/;C24886

---
+++ Vanrise/Code/Vanrise.Web/Client/Javascripts/Constants/ChartDefinitionTypeEnum.js
@@ -0,0 +1,11 @@
+﻿
+app.constant('ChartDefinitionTypeEnum', {
+    Column: { value: "column", description: "Column" },
+    Line: { value: "line", description: "Line" },
+    Spline: { value: "spline", description: "Spline" },
+    Area: { value: "area", description: "Area" },
+    AreaSpline: { value: "areaspline", description: "AreaSpline" },
+    Scatter: { value: "scatter", description: "Scatter" },
+    Bar: { value: "bar", description: "Bar" }
+
+});
