b5d0fc2289a3a47510d62aa00502d085d38e3e01
Ismail Zein <VANRISE\ismail.zein>
2015-07-22 11:55:18 +0000

    refactored Export method
    git-tfs-id: [http://developmentvlan:8585/tfs/vanrise.collection]$/;C24947

--- TOneV2/Code/TOneV2/TOne.Analytics.Web/Analytics/Views/Billing Reports/BillingReportsController.js
+++ TOneV2/Code/TOneV2/TOne.Analytics.Web/Analytics/Views/Billing Reports/BillingReportsController.js
@@ -1,18 +1,125 @@
-﻿BillingReportsController.$inject = ['$scope', 'ReportAPIService', 'CarrierAPIService', 'ZonesService', 'BillingStatisticsAPIService', 'MainService'];
+﻿BillingReportsController.$inject = ['$scope', 'ReportAPIService', 'CarrierAPIService', 'ZonesService', 'BillingStatisticsAPIService', 'MainService', 'BaseAPIService'];
 
-function BillingReportsController($scope, ReportAPIService, CarrierAPIService, ZonesService, BillingStatisticsAPIService, MainService) {
+function BillingReportsController($scope, ReportAPIService, CarrierAPIService, ZonesService, BillingStatisticsAPIService, MainService, BaseAPIService) {
    
     defineScope();
     load();
 
     $scope.export = function () {
-        var paramsurl = "?";
-        paramsurl += "fromDate=" + $scope.dateToString($scope.params.fromDate);
-        paramsurl += "&toDate=" + $scope.dateToString($scope.params.toDate);
+        //var paramsurl = "?";
+        //paramsurl += "fromDate=" + $scope.dateToString($scope.params.fromDate);
+        //paramsurl += "&toDate=" + $scope.dateToString($scope.params.toDate);
 
-        window.open(MainService.getBaseURL() + BillingStatisticsAPIService.Export() + paramsurl, "_self");
+        //window.open(MainService.getBaseURL() + BillingStatisticsAPIService.Export() + paramsurl, "_self");
        
 
+        return BaseAPIService.get("/api/BillingStatistics/Export",
+            {
+                fromDate: "01/01/2014",
+                toDate: "01/07/2015"
+            },
+            {
+                returnAllResponseParameters: true,
+                responseTypeAsBufferArray: true
+            }
+            ).then(function (response) {
+                var data = response.data;
+                var status = response.status;
+                var headers = response.headers;
+                var octetStreamMime = 'application/octet-stream';
+                var success = false;
+
+                // Get the headers
+                headers = headers();
+
+                //// Get the filename from the x-filename header or default to "download.bin"
+                //console.log(headers['content-disposition']);
+                //var matcher = /filename[^;=\n]*=((['"]).*?\2|[^;\n]*)/i;
+
+                //var results = headers['content-disposition'].match(matcher);
+                var filename ="myfile44.xls";
+
+                // Determine the content type from the header or default to "application/octet-stream"
+                var contentType = headers['content-type'] || octetStreamMime;
+                console.log(contentType);
+                //contentType = "application/vnd.openxmlformats-officedocument.spreadsheetml.sheet;";
+                try {
+                    // Try using msSaveBlob if supported
+                    console.log("Trying saveBlob method ...");
+                    var blob = new Blob([data], { type: contentType });
+                    if (navigator.msSaveBlob)
+                        navigator.msSaveBlob(blob, filename);
+                    else {
+                        // Try using other saveBlob implementations, if available
+                        var saveBlob = navigator.webkitSaveBlob || navigator.mozSaveBlob || navigator.saveBlob;
+                        if (saveBlob === undefined) throw "Not supported";
+                        saveBlob(blob, filename);
+                    }
+                    console.log("saveBlob succeeded");
+                    success = true;
+                } catch (ex) {
+                    console.log("saveBlob method failed with the following exception:");
+                    console.log(ex);
+                }
+
+                if (!success) {
+                    // Get the blob url creator
+                    var urlCreator = window.URL || window.webkitURL || window.mozURL || window.msURL;
+                    if (urlCreator) {
+                        // Try to use a download link
+                        var link = document.createElement('a');
+                        if ('download' in link) {
+                            // Try to simulate a click
+                            try {
+                                // Prepare a blob URL
+                                console.log("Trying download link method with simulated click ...");
+                                var blob = new Blob([data], { type: contentType });
+                                var url = urlCreator.createObjectURL(blob);
+                                console.log(url);
+                                link.setAttribute('href', url);
+
+                                // Set the download attribute (Supported in Chrome 14+ / Firefox 20+)
+                                link.setAttribute("download", filename);
+
+                                // Simulate clicking the download link
+                                var event = document.createEvent('MouseEvents');
+                                event.initMouseEvent('click', true, true, window, 1, 0, 0, 0, 0, false, false, false, false, 0, null);
+                                link.dispatchEvent(event);
+                                console.log("Download link method with simulated click succeeded");
+                                success = true;
+
+                            } catch (ex) {
+                                console.log("Download link method with simulated click failed with the following exception:");
+                                console.log(ex);
+                            }
+                        }
+
+                        if (!success) {
+                            // Fallback to window.location method
+                            try {
+                                // Prepare a blob URL
+                                // Use application/octet-stream when using window.location to force download
+                                console.log("Trying download link method with window.location ...");
+                                var blob = new Blob([data], { type: octetStreamMime });
+                                var url = urlCreator.createObjectURL(blob);
+                                window.location = url;
+                                console.log("Download link method with window.location succeeded");
+                                success = true;
+                            } catch (ex) {
+                                console.log("Download link method with window.location failed with the following exception:");
+                                console.log(ex);
+                            }
+                        }
+
+                    }
+                }
+
+                if (!success) {
+                    // Fallback to window.open method
+                    console.log("No methods worked for saving the arraybuffer, using last resort window.open");
+                    window.open(httpPath, '_blank', '');
+                }
+            });
        
 
 
