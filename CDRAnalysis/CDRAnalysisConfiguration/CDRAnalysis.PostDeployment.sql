5de1bae1655ba87765d7935a2550ac7762156525
walid.mogharbel <walid.mogharbel@vanrise.tfs.local>
2015-07-22 12:15:46 +0000

    git-tfs-id: [http://developmentvlan:8585/tfs/vanrise.collection]$/;C24949

--- CDRAnalysis/CDRAnalysisConfiguration/CDRAnalysis.PostDeployment.sql
+++ CDRAnalysis/CDRAnalysisConfiguration/CDRAnalysis.PostDeployment.sql
@@ -15,9 +15,9 @@ Post-Deployment Script Template
 
 MERGE INTO sec.[Module] AS Target 
 USING (VALUES 
-	( N'Administration', N'NULL', N'NULL', NULL, N'glyphicon-flash', NULL, N'True'),
-	( N'Fraud Analysis', N'NULL', N'NULL', NULL, N'glyphicon-flash', NULL, N'True'),
-	( N'Business Process', N'NULL', N'NULL', N'1', N'NULL', NULL, N'True')
+	( N'Administration', NULL, NULL, NULL, N'glyphicon-flash', NULL, N'True'),
+	( N'Fraud Analysis', NULL, NULL, NULL, N'glyphicon-flash', NULL, N'True'),
+	( N'Business Process', NULL, NULL, N'1', NULL, NULL, N'True')
 ) 
 AS Source ([Name]  ,[Title]  ,[Url]  ,[ParentId]  ,[Icon]  ,[Rank]  ,[AllowDynamic])
 ON Target.[Name] = Source.[Name] 
@@ -44,16 +44,16 @@ DELETE
 
 MERGE INTO sec.[View] AS Target 
 USING (VALUES 
-	(	N'Users',	N'Users',	N'#/view/Security/Views/UserManagement',	N'1',	N'NULL'	,N'NULL',	N'NULL',	N'0'	,NULL),
-	(	N'Groups',	N'Groups',	N'#/view/Security/Views/RoleManagement',	N'1',	N'NULL',	N'NULL',	N'NULL',	N'0',	NULL),
-	(	N'System Entites',	N'System Entites',	N'#/view/Security/Views/BusinessEntityManagement',	N'1',	N'NULL',	N'NULL',	N'NULL',	N'0',	NULL),
-	(	N'Strategy Management',	N'Strategy Management',	N'#/view/FraudAnalysis/Views/Strategy/StrategyManagement',	N'2'	,N'NULL',	N'NULL'	,N'NULL',	N'0',	NULL),
-	(	N'Suspicion Analysis',	N'Suspicion Analysis',	N'#/view/FraudAnalysis/Views/SuspiciousAnalysis/SuspicionAnalysis',	N'2',	N'NULL',	N'NULL',	N'NULL',	N'0',	NULL),
-	(	N'History',	N'Business Process History'	,N'#view/BusinessProcess/Views/BPManagement',	N'3',	N'NULL',	N'NULL',	N'NULL',	N'0'	,NULL),
-	(	N'Management',	N'Business Process Management',	N'#view/BusinessProcess/Views/BPDefinitionManagement',	N'3',	N'NULL',	N'NULL',	N'NULL',	N'0',	NULL),
-	(	N'Scheduler Service',	N'Scheduler Service',	N'#/view/Runtime/Views/SchedulerTaskManagement',	N'1',	N'NULL',	N'NULL'	,N'NULL',	N'0',	NULL),
-	(	N'Data Sources'	,N'Data Sources'	,N'#/view/Integration/Views/DataSourceManagement',	N'1',	N'NULL'	,N'NULL',	N'NULL',	N'0',	NULL),
-	(	N'Dashboard'	,N'Dashboard',	N'#/view/FraudAnalysis/Views/Output/Dashboard',	N'2',	N'NULL',	N'NULL',	N'NULL',	N'0'	, NULL)
+	(	N'Users',	N'Users',	N'#/view/Security/Views/UserManagement',	N'1',	NULL	,NULL,	NULL,	N'0'	,NULL),
+	(	N'Groups',	N'Groups',	N'#/view/Security/Views/RoleManagement',	N'1',	NULL,	NULL,	NULL,	N'0',	NULL),
+	(	N'System Entites',	N'System Entites',	N'#/view/Security/Views/BusinessEntityManagement',	N'1',	NULL,	NULL,	NULL,	N'0',	NULL),
+	(	N'Strategy Management',	N'Strategy Management',	N'#/view/FraudAnalysis/Views/Strategy/StrategyManagement',	N'2'	,NULL,	NULL	,NULL,	N'0',	NULL),
+	(	N'Suspicion Analysis',	N'Suspicion Analysis',	N'#/view/FraudAnalysis/Views/SuspiciousAnalysis/SuspicionAnalysis',	N'2',	NULL,	NULL,	NULL,	N'0',	NULL),
+	(	N'History',	N'Business Process History'	,N'#view/BusinessProcess/Views/BPManagement',	N'3',	NULL,	NULL,	NULL,	N'0'	,NULL),
+	(	N'Management',	N'Business Process Management',	N'#view/BusinessProcess/Views/BPDefinitionManagement',	N'3',	NULL,	NULL,	NULL,	N'0',	NULL),
+	(	N'Scheduler Service',	N'Scheduler Service',	N'#/view/Runtime/Views/SchedulerTaskManagement',	N'1',	NULL,	NULL	,NULL,	N'0',	NULL),
+	(	N'Data Sources'	,N'Data Sources'	,N'#/view/Integration/Views/DataSourceManagement',	N'1',	NULL	,NULL,	NULL,	N'0',	NULL),
+	(	N'Dashboard'	,N'Dashboard',	N'#/view/FraudAnalysis/Views/Output/Dashboard',	N'2',	NULL,	NULL,	NULL,	N'0'	, NULL)
 ) 
 AS Source ([Name]  ,[Title]  ,[Url]  ,[Module]  ,[RequiredPermissions]  ,[Audience]  ,[Content]  ,[Type]    ,[Rank])
 ON Target.[Name] = Source.[Name] 
