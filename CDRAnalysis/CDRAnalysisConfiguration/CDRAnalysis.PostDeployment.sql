732f0108f4facc284eaf978a05c465866802e8dd
walid.mogharbel <walid.mogharbel@vanrise.tfs.local>
2015-07-22 07:46:05 +0000

    git-tfs-id: [http://developmentvlan:8585/tfs/vanrise.collection]$/;C24921

--- CDRAnalysis/CDRAnalysisConfiguration/CDRAnalysis.PostDeployment.sql
+++ CDRAnalysis/CDRAnalysisConfiguration/CDRAnalysis.PostDeployment.sql
@@ -12,25 +12,72 @@ Post-Deployment Script Template
 
 
 
-MERGE INTO bp.[BPDefinition] AS Target 
-USING (VALUES 
-	(N'Vanrise.Fzero.CDRImport.BP.Arguments.CDRImportProcessInput', N'CDR Import Process', N'Vanrise.Fzero.CDRImport.BP.CDRImportProcess, Vanrise.Fzero.CDRImport.BP', N'{"$type":"Vanrise.BusinessProcess.Entities.BPConfiguration, Vanrise.BusinessProcess.Entities","MaxConcurrentWorkflows":10,"RetryOnProcessFailed":false,"Url":"/Client/Modules/FraudAnalysis/Views/ProcessInputTemplate/Normal/CDRImportProcessInput.html", "ScheduleTemplateURL":"/Client/Modules/FraudAnalysis/Views/ProcessInputTemplate/Scheduled/CDRImportProcessInput_Scheduled.html"}'),
-	(N'Vanrise.Fzero.CDRImport.BP.Arguments.SaveCDRToDBProcessInput', N'Save CDR To DB Process', N'Vanrise.Fzero.CDRImport.BP.SaveCDRToDBProcess, Vanrise.Fzero.CDRImport.BP', N'{"$type":"Vanrise.BusinessProcess.Entities.BPConfiguration, Vanrise.BusinessProcess.Entities","MaxConcurrentWorkflows":10,"RetryOnProcessFailed":false,"Url":"/Client/Modules/FraudAnalysis/Views/ProcessInputTemplate/Normal/SaveCDRToDBProcessInput.html", "ScheduleTemplateURL":"/Client/Modules/FraudAnalysis/Views/ProcessInputTemplate/Scheduled/SaveCDRToDBProcessInput_Scheduled.html"}'),
-	(N'Vanrise.Fzero.FraudAnalysis.BP.Arguments.ExecuteStrategyProcessInput', N'Execute Strategy Process', N'Vanrise.Fzero.FraudAnalysis.BP.ExecuteStrategyProcess, Vanrise.Fzero.FraudAnalysis.BP', N'{"$type":"Vanrise.BusinessProcess.Entities.BPConfiguration, Vanrise.BusinessProcess.Entities","MaxConcurrentWorkflows":10,"RetryOnProcessFailed":false,"Url":"/Client/Modules/FraudAnalysis/Views/ProcessInputTemplate/Normal/ExecuteStrategyProcessInput.html", "ScheduleTemplateURL":"/Client/Modules/FraudAnalysis/Views/ProcessInputTemplate/Scheduled/ExecuteStrategyProcessInput_Scheduled.html"}')
 
+MERGE INTO bp.[Module] AS Target 
+USING (VALUES 
+	(N'1', N'Administration', N'NULL', N'NULL', N'NULL', N'glyphicon-flash', N'NULL', N'True'),
+	(N'2', N'Fraud Analysis', N'NULL', N'NULL', N'NULL', N'glyphicon-flash', N'NULL', N'True'),
+	(N'3', N'Business Process', N'NULL', N'NULL', N'1', N'NULL', N'NULL', N'True')
 ) 
-AS Source ([Name], [Title], [FQTN], [Config])
+AS Source ([Id] ,[Name]  ,[Title]  ,[Url]  ,[ParentId]  ,[Icon]  ,[Rank]  ,[AllowDynamic])
 ON Target.[Name] = Source.[Name] 
 -- update matched rows 
 WHEN MATCHED THEN 
 UPDATE SET	[Title] = Source.[Title],
-			[FQTN] = Source.[FQTN],
-			[Config]  = Source.[Config]
+			[Url]  = Source.[Url], 
+			[ParentId]  = Source.[ParentId], 
+			[Icon]  = Source.[Icon], 
+			[Rank]  = Source.[Rank], 
+			[AllowDynamic]  = Source.[AllowDynamic] 
+			
 -- insert new rows 
 WHEN NOT MATCHED BY TARGET THEN 
-INSERT ([Name], [Title], [FQTN], [Config])
-VALUES ([Name], [Title], [FQTN], [Config])
+INSERT ([Name]  ,[Title]  ,[Url]  ,[ParentId]  ,[Icon]  ,[Rank]  ,[AllowDynamic])
+VALUES ([Name]  ,[Title]  ,[Url]  ,[ParentId]  ,[Icon]  ,[Rank]  ,[AllowDynamic])
 ---- delete rows that are in the target but not the source 
 WHEN NOT MATCHED BY SOURCE THEN 
 DELETE
 ;
+
+
+
+
+MERGE INTO bp.[View] AS Target 
+USING (VALUES 
+	(N'3',	N'Users',	N'Users',	N'#/view/Security/Views/UserManagement',	N'1',	N'NULL'	,N'NULL',	N'NULL',	N'0'	,N'NULL'),
+	(N'4',	N'Groups',	N'Groups',	N'#/view/Security/Views/RoleManagement',	N'1',	N'NULL',	N'NULL',	N'NULL',	N'0',	N'NULL'),
+	(N'5'	,N'System Entites',	N'System Entites',	N'#/view/Security/Views/BusinessEntityManagement',	N'1',	N'NULL',	N'NULL',	N'NULL',	N'0',	N'NULL'),
+	(N'7',	N'Strategy Management',	N'Strategy Management',	N'#/view/FraudAnalysis/Views/Strategy/StrategyManagement',	N'2'	,N'NULL',	N'NULL'	,N'NULL',	N'0',	N'NULL'),
+	(N'8',	N'Suspicion Analysis',	N'Suspicion Analysis',	N'#/view/FraudAnalysis/Views/SuspiciousAnalysis/SuspicionAnalysis',	N'2',	N'NULL',	N'NULL',	N'NULL',	N'0',	N'NULL'),
+	(N'11',	N'History',	N'Business Process History'	,N'#view/BusinessProcess/Views/BPManagement',	N'3',	N'NULL',	N'NULL',	N'NULL',	N'0'	,N'NULL'),
+	(N'12',	N'Management',	N'Business Process Management',	N'#view/BusinessProcess/Views/BPDefinitionManagement',	N'3',	N'NULL',	N'NULL',	N'NULL',	N'0',	N'NULL'),
+	(N'13',	N'Scheduler Service',	N'Scheduler Service',	N'#/view/Runtime/Views/SchedulerTaskManagement',	N'1',	N'NULL',	N'NULL'	,N'NULL',	N'0',	N'NULL'),
+	(N'17',	N'Data Sources'	,N'Data Sources'	,N'#/view/Integration/Views/DataSourceManagement',	N'1',	N'NULL'	,N'NULL',	N'NULL',	N'0',	N'NULL'),
+	(N'18',	N'Dashboard'	,N'Dashboard',	N'#/view/FraudAnalysis/Views/Output/Dashboard',	N'2',	N'NULL',	N'NULL',	N'NULL',	N'0'	,N'NULL')
+) 
+AS Source ([Name]  ,[Title]  ,[Url]  ,[Module]  ,[RequiredPermissions]  ,[Audience]  ,[Content]  ,[Type]    ,[Rank])
+ON Target.[Name] = Source.[Name] 
+-- update matched rows 
+WHEN MATCHED THEN 
+UPDATE SET	[Title] = Source.[Title],
+			[Url] = Source.[Url],
+			[Module]  = Source.[Module],
+			[RequiredPermissions]  = Source.[RequiredPermissions],
+			[Audience]  = Source.[Audience],
+			[Content]  = Source.[Content],
+			[Type]  = Source.[Type],
+			[Rank]  = Source.[Rank]
+-- insert new rows 
+WHEN NOT MATCHED BY TARGET THEN 
+INSERT ([Name]  ,[Title]  ,[Url]  ,[Module]  ,[RequiredPermissions]  ,[Audience]  ,[Content]  ,[Type]    ,[Rank])
+VALUES ([Name]  ,[Title]  ,[Url]  ,[Module]  ,[RequiredPermissions]  ,[Audience]  ,[Content]  ,[Type]    ,[Rank])
+---- delete rows that are in the target but not the source 
+WHEN NOT MATCHED BY SOURCE THEN 
+DELETE
+;
+
+
+
+
+
+
