6b27f6feb137e8cc4b4ebfc15799d4923c325939
Samer Haidar <VANRISE\samer.haidar>
2015-07-23 11:31:34 +0000

    export api service
    git-tfs-id: [http://developmentvlan:8585/tfs/vanrise.collection]$/;C25009

--- TOneV2/Code/TOneV2/TOne.Analytics.Web/Analytics/Services/APIServices/CDRAPIService.js
+++ TOneV2/Code/TOneV2/TOne.Analytics.Web/Analytics/Services/APIServices/CDRAPIService.js
@@ -3,13 +3,20 @@ app.service('CDRAPIService', function (BaseAPIService) {
 
     return ({
        
-        GetCDRData: GetCDRData
+        GetCDRData: GetCDRData,
+        ExportCDRData: ExportCDRData
        
     });
 
  
     function GetCDRData(getCDRLogSummaryInput) {
         return BaseAPIService.post("/api/CDR/GetCDRData", getCDRLogSummaryInput);
+    }
+    function ExportCDRData(CDRLogSummaryInput) {
+        return BaseAPIService.post("/api/CDR/ExportCDRData", CDRLogSummaryInput, {
+            returnAllResponseParameters: true,
+            responseTypeAsBufferArray: true
+        });
     }
 
 });
