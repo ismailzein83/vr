cbce1dd9cfb12156d8207d795d517edf34162f1c
Ali Ballouk <VANRISE\ali.ballouk>
2015-07-24 12:59:47 +0000

    BSDateTimePicker 3.1 look&feel enhancement
    git-tfs-id: [http://developmentvlan:8585/tfs/vanrise.collection]$/;C25068

--- Vanrise/Code/Vanrise.Web/Client/Javascripts/Directives/Inputs/DatePicker/vr-datetimepicker-directive.js
+++ Vanrise/Code/Vanrise.Web/Client/Javascripts/Directives/Inputs/DatePicker/vr-datetimepicker-directive.js
@@ -265,14 +265,14 @@ app.directive('vrDatetimepicker', ['ValidationMessagesEnum', 'BaseDirService', f
                 labelTemplate = '<vr-label>' + attrs.label + '</vr-label>';
             var icontemplate = "";
             if (attrs.type == 'date' || attrs.type == 'dateTime')
-                icontemplate += ' <span class="input-group-addon " ng-click="ctrl.toggleDate($event)" ><i class="glyphicon glyphicon-calendar"></i></span>';
+                icontemplate += ' <span ng-show="showtd" class="input-group-addon vr-small-addon " ng-click="ctrl.toggleDate($event)" ><i class="glyphicon glyphicon-calendar"></i></span>';
             if (attrs.type == 'time' || attrs.type == 'dateTime')
-                icontemplate += ' <span class="input-group-addon " ng-click="ctrl.toggleTime($event)" > <i class="glyphicon glyphicon-time"></i></span>';
+                icontemplate += ' <span ng-show="showtd"  class="input-group-addon vr-small-addon " ng-click="ctrl.toggleTime($event)" > <i class="glyphicon glyphicon-time"></i></span>';
             var dateTemplate =
-                 '<div  >'
-                  + '<div id="mainInput" ng-model="ctrl.value">'
+                 '<div ng-mouseenter="showtd=true" ng-mouseleave="showtd=false"  >'
+                  + '<div id="mainInput" ng-model="ctrl.value" style="border-radius: 4px;">'
                  + '<div  class="input-group date datetime-controle" style="width:100%" id="divDatePicker"  >'
-                                + '<input class="form-control" style="height:30px" data-autoclose="1" placeholder="Date" type="text" ctrltype="' + attrs.type + '">'
+                                + '<input class="form-control" style="height:30px;" ng-class="showtd==true? \'fix-border-radius\':\'border-radius\'" data-autoclose="1" placeholder="Date" type="text" ctrltype="' + attrs.type + '">'
                                 + icontemplate
                             + '</div>'
                       + '</div>'
