30f331d367abfd8c4d3cc4ea6aff8326d8d3177c
walid.mogharbel <walid.mogharbel@vanrise.tfs.local>
2015-07-29 11:15:02 +0000

    git-tfs-id: [http://developmentvlan:8585/tfs/vanrise.collection]$/;C25195

--- CDRAnalysis/Vanrise.Fzero.FraudAnalysis.Web/FraudAnalysis/Views/OutPut/DashboardController.js
+++ CDRAnalysis/Vanrise.Fzero.FraudAnalysis.Web/FraudAnalysis/Views/OutPut/DashboardController.js
@@ -3,6 +3,9 @@
 function DashboardController($scope, DashboardAPIService, $routeParams, notify, VRModalService, VRNotificationService, VRNavigationService) {
 
     var mainGridAPI_CasesSummary;
+    var mainGridAPI_StrategyCases;
+    var mainGridAPI_BTSCases;
+    var mainGridAPI_CellCases;
 
     defineScope();
     load();
@@ -46,6 +49,10 @@ function DashboardController($scope, DashboardAPIService, $routeParams, notify, 
 
 
         $scope.casesSummary = [];
+        $scope.strategyCases = [];
+        $scope.bTSCases = [];
+        $scope.cellCases = [];
+
 
         $scope.onMainGridReady_CasesSummary = function (api) {
             mainGridAPI_CasesSummary = api;
@@ -56,9 +63,46 @@ function DashboardController($scope, DashboardAPIService, $routeParams, notify, 
             return getData_CasesSummary();
         }
 
+        $scope.onMainGridReady_StrategyCases = function (api) {
+            mainGridAPI_StrategyCases = api;
+            getData_StrategyCases();
+        };
+
+
+        $scope.loadMoreData_StrategyCases = function () {
+            return getData_StrategyCases();
+        }
+
+        $scope.onMainGridReady_BTSCases = function (api) {
+            mainGridAPI_BTSCases = api;
+            getData_BTSCases();
+        };
+
+        $scope.loadMoreData_BTSCases = function () {
+            return getData_BTSCases();
+        }
+
+        $scope.onMainGridReady_CellCases = function (api) {
+            mainGridAPI_CellCases = api;
+            getData_CellCases();
+        };
+
+        $scope.loadMoreData_CellCases = function () {
+            return getData_CellCases();
+        }
+
         $scope.searchClicked = function () {
             mainGridAPI_CasesSummary.clearDataAndContinuePaging();
-            return getData_CasesSummary();
+            mainGridAPI_StrategyCases.clearDataAndContinuePaging();
+            mainGridAPI_BTSCases.clearDataAndContinuePaging();
+            mainGridAPI_CellCases.clearDataAndContinuePaging();
+
+            getData_CasesSummary();
+            getData_StrategyCases();
+            getData_BTSCases();
+            getData_CellCases();
+
+            //return true;
         };
 
     }
@@ -82,6 +126,54 @@ function DashboardController($scope, DashboardAPIService, $routeParams, notify, 
             });
         }).finally(function () {
             $scope.isGettingCasesSummary = false;
+        });
+    }
+
+    function getData_StrategyCases() {
+
+        $scope.isGettingStrategyCases = true;
+
+        var fromDate = $scope.fromDate != undefined ? $scope.fromDate : '';
+        var toDate = $scope.toDate != undefined ? $scope.toDate : '';
+
+        return DashboardAPIService.GetStrategyCases(fromDate, toDate).then(function (response) {
+            angular.forEach(response, function (itm) {
+                $scope.strategyCases.push(itm);
+            });
+        }).finally(function () {
+            $scope.isGettingStrategyCases = false;
+        });
+    }
+
+    function getData_BTSCases() {
+
+        $scope.isGettingBTSCases = true;
+
+        var fromDate = $scope.fromDate != undefined ? $scope.fromDate : '';
+        var toDate = $scope.toDate != undefined ? $scope.toDate : '';
+
+        return DashboardAPIService.GetBTSCases(fromDate, toDate).then(function (response) {
+            angular.forEach(response, function (itm) {
+                $scope.bTSCases.push(itm);
+            });
+        }).finally(function () {
+            $scope.isGettingBTSCases = false;
+        });
+    }
+
+    function getData_CellCases() {
+
+        $scope.isGettingCellCases = true;
+
+        var fromDate = $scope.fromDate != undefined ? $scope.fromDate : '';
+        var toDate = $scope.toDate != undefined ? $scope.toDate : '';
+
+        return DashboardAPIService.GetCellCases(fromDate, toDate).then(function (response) {
+            angular.forEach(response, function (itm) {
+                $scope.cellCases.push(itm);
+            });
+        }).finally(function () {
+            $scope.isGettingCellCases = false;
         });
     }
 }
