d9ecbb8c6b1c92b8d036943019c8492ed6a4c574
mustafa.hawi <mustafa.hawi@vanrise.tfs.local>
2015-07-30 10:37:00 +0000

    Remove client directory
    git-tfs-id: [http://developmentvlan:8585/tfs/vanrise.collection]$/;C25248

--- BusinessProcess/Code/Vanrise.Queueing.Web/Client/Constants/QueueItemStatusEnum.js
+++
@@ -1,6 +0,0 @@
-﻿app.constant('QueueItemStatusEnum', {
-    New: { value: 0, description: "New" },
-    Processing: { value: 10, description: "Processing" },
-    Failed: { value: 20, description: "Failed" },
-    Processed: { value: 30, description: "Processed" }
-});
