4ef8529e2ea55c8f7994c048d153c0fa152d0b5b
mustafa.hawi <mustafa.hawi@vanrise.tfs.local>
2015-07-30 14:43:35 +0000

    Refactoring
    git-tfs-id: [http://developmentvlan:8585/tfs/vanrise.collection]$/;C25281

--- BusinessProcess/Code/Vanrise.Queueing.Web/Models/EnumModel.cs
+++ BusinessProcess/Code/Vanrise.Queueing.Web/Models/EnumModel.cs
@@ -1,8 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Web;
-
+﻿
 namespace Vanrise.Queueing.Web.Models
 {
     public class EnumModel
--- BusinessProcess/Code/Vanrise.Queueing.Web/Models/QueueItemHeaderModel.cs
+++ BusinessProcess/Code/Vanrise.Queueing.Web/Models/QueueItemHeaderModel.cs
@@ -1,7 +1,4 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Web;
 using Vanrise.Queueing.Entities;
 
 namespace Vanrise.Queueing.Web.Models
