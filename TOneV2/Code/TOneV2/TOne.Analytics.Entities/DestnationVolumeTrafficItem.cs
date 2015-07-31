5acdb0b2c6ec3077aad09aaa03efcbe1b245c7ee
sara.yahfoufi <sara.yahfoufi@vanrise.tfs.local>
2015-07-31 10:44:43 +0000

    add DestnationVolumeTrafficItem class
    git-tfs-id: [http://developmentvlan:8585/tfs/vanrise.collection]$/;C25296

---
+++ TOneV2/Code/TOneV2/TOne.Analytics.Entities/DestnationVolumeTrafficItem.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TOne.Analytics.Entities
+{
+    public class DestnationVolumeTrafficItem
+    {
+        public string SaleZoneName { get; set; }
+        public string Date { get; set; }
+        public decimal Value { get; set; }
+    }
+}
