93d718bc1facf66a00fc7671a8927363e55a10ff
Ismail Zein <VANRISE\ismail.zein>
2015-08-03 08:26:38 +0000

    added Runtime Service configuration
    git-tfs-id: [http://developmentvlan:8585/tfs/vanrise.collection]$/;C25328

--- CDRAnalysis/Vanrise.Fzero.DevRuntime/Program.cs
+++ CDRAnalysis/Vanrise.Fzero.DevRuntime/Program.cs
@@ -1,8 +1,11 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Configuration;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Vanrise.BusinessProcess;
+using Vanrise.Runtime;
 
 namespace Vanrise.Fzero.DevRuntime
 {
@@ -10,10 +13,28 @@ namespace Vanrise.Fzero.DevRuntime
     {
         static void Main(string[] args)
         {
-            MainForm f = new MainForm();
-            f.ShowDialog();
-            Console.ReadKey();
-            return;
+            if(ConfigurationManager.AppSettings["IsRuntimeService"] == "true")
+            {
+                BusinessProcessService bpService = new BusinessProcessService() { Interval = new TimeSpan(0, 0, 2) };
+                //QueueActivationService queueActivationService = new QueueActivationService() { Interval = new TimeSpan(0, 0, 2) };
+
+                var runtimeServices = new List<RuntimeService>();
+                //runtimeServices.Add(queueActivationService);
+
+                runtimeServices.Add(bpService);
+
+                RuntimeHost host = new RuntimeHost(runtimeServices);
+                host.Start();
+                Console.ReadKey();
+            }
+            else
+            {
+                MainForm f = new MainForm();
+                f.ShowDialog();
+                Console.ReadKey();
+                return;
+            }
+            
 
             //WF f = new WF();
             //f.ShowDialog();
