c60cfd228d97d90fa7982eed26f7f53284498b34
Mortada Issa <VANRISE\mortada.issa>
2015-08-03 13:59:37 +0000

    Sync DB , adding a new proc to get execution flows
    git-tfs-id: [http://developmentvlan:8585/tfs/vanrise.collection]$/;C25358

---
+++ TOneV2/Code/TOneV2/SQL.TOneTransaction/queue/Stored Procedures/sp_ExecutionFlow_GetAll.sql
@@ -0,0 +1,17 @@
+﻿-- =============================================
+-- Author:		<Author,,Name>
+-- Create date: <Create Date,,>
+-- Description:	<Description,,>
+-- =============================================
+CReate PROCEDURE [queue].sp_ExecutionFlow_GetAll 
+AS
+BEGIN
+	
+	SET NOCOUNT ON;
+
+    -- Insert statements for procedure here
+	SELECT ef.ID, ef.ExecutionFlowDefinitionID, ef.Name, efd.ExecutionTree
+	FROM queue.ExecutionFlow ef
+	JOIN queue.ExecutionFlowDefinition efd ON ef.ExecutionFlowDefinitionID = efd.ID
+	
+END
