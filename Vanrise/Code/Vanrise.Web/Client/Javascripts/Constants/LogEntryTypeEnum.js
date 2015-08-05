b9e4cdebe33dea983237ecd0b2e6dd2782cc8f5b
mustafa.hawi <mustafa.hawi@vanrise.tfs.local>
2015-08-05 08:12:55 +0000

    Added LogEntryTypeEnum
    git-tfs-id: [http://developmentvlan:8585/tfs/vanrise.collection]$/;C25421

---
+++ Vanrise/Code/Vanrise.Web/Client/Javascripts/Constants/LogEntryTypeEnum.js
@@ -0,0 +1,8 @@
+﻿app.constant('LogEntryTypeEnum', {
+    Error: { value: 1, description: "Error" },
+    Warning: { value: 2, description: "Warning" },
+    Information: { value: 4, description: "Information" },
+    Verbose: { value: 8, description: "Verbose" }
+});
+
+
