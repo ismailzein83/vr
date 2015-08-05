ffd30c7552cad7525a8cb02411543616f9a336d7
Wissam Ajamy <VANRISE\wissam.ajamy>
2015-08-05 06:32:41 +0000

    Add carrier account and carrier group
    git-tfs-id: [http://developmentvlan:8585/tfs/vanrise.collection]$/;C25415

---
+++ TOneV2/Code/TOneV2/SQL.TOneDB/dbo/Tables/CarrierGroup.sql
@@ -0,0 +1,15 @@
+﻿CREATE TABLE [dbo].[CarrierGroup] (
+    [CarrierGroupID]   INT            IDENTITY (1, 1) NOT NULL,
+    [CarrierGroupName] NVARCHAR (255) NOT NULL,
+    [ParentID]         INT            NULL,
+    [ParentPath]       VARCHAR (255)  NULL,
+    [Path]             AS             (case when [parentID] IS NULL then [CarrierGroupName] else ([ParentPath]+'/')+[CarrierGroupName] end),
+    [timestamp]        ROWVERSION     NOT NULL,
+    CONSTRAINT [PK_CarrierGroup] PRIMARY KEY CLUSTERED ([CarrierGroupID] ASC)
+);
+
+
+GO
+CREATE NONCLUSTERED INDEX [IX_CarrierGroup]
+    ON [dbo].[CarrierGroup]([ParentPath] ASC);
+
