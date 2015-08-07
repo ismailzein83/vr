ff14c5fb6748910d0d68b2f6878ca54725c3f5e7
Ismail Zein <VANRISE\ismail.zein>
2015-08-07 09:53:49 +0000

    commented License Check
    git-tfs-id: [http://developmentvlan:8585/tfs/vanrise.collection]$/;C25545

--- Vanrise/Code/Vanrise.Web/App_Start/WebApiConfig.cs
+++ Vanrise/Code/Vanrise.Web/App_Start/WebApiConfig.cs
@@ -11,7 +11,7 @@ namespace Vanrise.Web
     {
         public static void Register(HttpConfiguration config)
         {
-            config.Filters.Add(new LicenseCheckFilter());
+            //config.Filters.Add(new LicenseCheckFilter());
             // Web API configuration and services
 
             // Web API routes
@@ -30,18 +30,18 @@ namespace Vanrise.Web
         }
     }
 
-    public class MyDateConverter : DateTimeConverterBase
-    {
-        public override object ReadJson(Newtonsoft.Json.JsonReader reader, Type objectType, object existingValue, Newtonsoft.Json.JsonSerializer serializer)
-        {
-            return DateTime.Parse(reader.Value as string);
-        }
+    //public class MyDateConverter : DateTimeConverterBase
+    //{
+    //    public override object ReadJson(Newtonsoft.Json.JsonReader reader, Type objectType, object existingValue, Newtonsoft.Json.JsonSerializer serializer)
+    //    {
+    //        return DateTime.Parse(reader.Value as string);
+    //    }
 
-        public override void WriteJson(Newtonsoft.Json.JsonWriter writer, object value, Newtonsoft.Json.JsonSerializer serializer)
-        {
-            writer.WriteValue(value.ToString());
-        }
-    }
+    //    public override void WriteJson(Newtonsoft.Json.JsonWriter writer, object value, Newtonsoft.Json.JsonSerializer serializer)
+    //    {
+    //        writer.WriteValue(((DateTime)value).ToString("yyyy-MM-ddTHH:mm:ss"));
+    //    }
+    //}
 
 
     //public class MyAuthorizationFilter : System.Web.Http.Filters.AuthorizationFilterAttribute
