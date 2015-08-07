36fcac4a48daa6a76ccc4946f60b9cf140d829a7
hassan.ali <hassan.ali@vanrise.tfs.local>
2015-08-07 09:32:44 +0000

    License Temp Checker
    git-tfs-id: [http://developmentvlan:8585/tfs/vanrise.collection]$/;C25541

--- Vanrise/Code/Vanrise.Web/App_Start/LicenseCheckFilter.cs
+++ Vanrise/Code/Vanrise.Web/App_Start/LicenseCheckFilter.cs
@@ -7,10 +7,31 @@ namespace Vanrise.Web
 {
     public class LicenseCheckFilter : System.Web.Http.Filters.AuthorizationFilterAttribute
     {
+        #region Local Licensed Variables
+        private static bool? s_isLicensed;//Tone Lisenced value
+        private static DateTime? s_licensedActivatationDate;//TOne Licensed Date
+        #endregion
+
         public override void OnAuthorization(System.Web.Http.Controllers.HttpActionContext actionContext)
         {
-            //throw new Exception("License Key is expired. key is: ");
+            LicenseCheker();
             base.OnAuthorization(actionContext);
         }
+
+        private static void LicenseCheker()
+        {
+            string licenseKey = string.Empty;
+            if (s_licensedActivatationDate.HasValue && (DateTime.Now - s_licensedActivatationDate.Value).TotalHours >= 6.0)
+                s_isLicensed = null;
+            if (!s_isLicensed.HasValue)
+            {
+                s_isLicensed = Vanrise.Components.Security.LicenceManagerControl.CheckLicence("TOne License", out licenseKey);
+                if (s_isLicensed.Value) s_licensedActivatationDate = DateTime.Now;
+            }
+            if (!s_isLicensed.Value)
+                throw new Exception(string.Format("License Key is expired. key is:{0}", licenseKey));
+        }
+
+
     }
 }
