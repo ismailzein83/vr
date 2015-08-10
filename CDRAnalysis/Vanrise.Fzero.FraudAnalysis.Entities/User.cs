3f6f1cf92e4dba3e2fd34b41ebcf6cb9fb3d2dd3
walid.mogharbel <walid.mogharbel@vanrise.tfs.local>
2015-08-10 16:00:57 +0000

    git-tfs-id: [http://developmentvlan:8585/tfs/vanrise.collection]$/;C25640

---
+++ CDRAnalysis/Vanrise.Fzero.FraudAnalysis.Entities/User.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace Vanrise.Fzero.FraudAnalysis.Entities
+{
+
+    public class User
+    {
+        public int UserId { get; set; }
+
+        public string Name { get; set; }
+
+    }
+}
--- CDRAnalysis/Vanrise.Fzero.FraudAnalysis.Web/Controllers/UserController.cs
+++ CDRAnalysis/Vanrise.Fzero.FraudAnalysis.Web/Controllers/UserController.cs
@@ -1,7 +1,7 @@
 ﻿using System.Collections.Generic;
 using System.Web.Http;
 using Vanrise.Security.Business;
-using Vanrise.Security.Entities;
+using Vanrise.Fzero.FraudAnalysis.Entities;
 using Vanrise.Web.Base;
 
 namespace  Vanrise.Fzero.FraudAnalysis.Web.Controllers
@@ -16,7 +16,19 @@ namespace  Vanrise.Fzero.FraudAnalysis.Web.Controllers
         {
             UserManager manager = new UserManager();
 
-            return manager.GetUsers();
+            return UserMapper(manager.GetUsers());
+        }
+
+
+        private List<User> UserMapper(List<Vanrise.Security.Entities.User> listUsers)
+        {
+            List<User> CDRUsers = new List<User>();
+
+            foreach ( var user in listUsers)
+            {
+                CDRUsers.Add(new User() { Name = user.Name, UserId = user.UserId });
+            }
+            return CDRUsers;
         }
 
     }
