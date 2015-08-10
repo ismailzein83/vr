832006e156f0601c21fe5932b02354df18563210
Ali Ballouk <VANRISE\ali.ballouk>
2015-08-10 15:20:59 +0000

    implementation of vr-circle container
    git-tfs-id: [http://developmentvlan:8585/tfs/vanrise.collection]$/;C25636

--- Vanrise/Code/Vanrise.BI.Web/BI/Directives/vr-bi-summary-directive.js
+++ Vanrise/Code/Vanrise.BI.Web/BI/Directives/vr-bi-summary-directive.js
@@ -13,11 +13,11 @@ app.directive('vrBiSummary', ['UtilsService', 'BIConfigurationAPIService', 'BIAP
         controller: function ($scope, $element, $attrs) {
             var ctrl = this;
             var retrieveDataOnLoad = $scope.$parent.$eval($attrs.retrievedataonload);
-            
+
             var biSummary = new BISummary(ctrl, ctrl.settings, retrieveDataOnLoad, BIAPIService, BIVisualElementService, BIConfigurationAPIService, UtilsService);
             biSummary.initializeController();
 
-          
+
             //$scope.openReportEntityModal = function (item) {
 
             //    BIUtilitiesService.openEntityReport(item.EntityType, item.EntityId, item.EntityName);
@@ -39,12 +39,33 @@ app.directive('vrBiSummary', ['UtilsService', 'BIConfigurationAPIService', 'BIAP
 
     };
 
+
     function getSummaryTemplate(previewmode) {
         if (previewmode != 'true') {
-            return '<vr-section title="{{ctrl.title}}"><div ng-if="ctrl.isAllowed==false"  class="alert alert-danger" role="alert"> <span class="glyphicon glyphicon-exclamation-sign" aria-hidden="true"></span><span class="sr-only">Error:</span> You Don\'t Have Permission, Please Contact Your Administrator..!!</div><div ng-if="ctrl.isAllowed" vr-loader="ctrl.isGettingData"><div width="normal"><table class="table  table-striped" ><tr ng-repeat="value in ctrl.dataSource" ><td><vr-label isValue="{{value.description.DisplayName}}">{{value.description.DisplayName}}</vr-label></td><td><vr-label isValue="{{value.value}}">{{value.value}}</vr-label></td></tr></table></div></div></vr-section>';
+            return '<vr-section title="{{ctrl.title}}">'
+                    + '<div ng-if="ctrl.isAllowed==false"  class="alert alert-danger" role="alert">'
+                        + '<span class="glyphicon glyphicon-exclamation-sign" aria-hidden="true"></span>'
+                        + '<span class="sr-only">Error:</span> You Don\'t Have Permission, Please Contact Your Administrator..!!'
+                    + '</div>'
+                    + '<div ng-if="ctrl.isAllowed" vr-loader="ctrl.isGettingData">'
+                        + '<div width="normal">'
+                            + '<div class="circle-div" ng-repeat="value in ctrl.dataSource" >'
+                                + '<div class="circle-value" >'
+                                  + '<vr-label isValue="{{value.value}}">{{value.value}}</vr-label>'
+                                + '</div>'
+                                + '<div  class="circle-label" >'
+                                 + '<vr-label isValue="{{value.description.DisplayName}}">{{value.description.DisplayName}}</vr-label>'
+                                + '</div>'
+                            + '</div>'
+                        + '</div>'
+                    + '</div>'
+                + '</vr-section>'
+
+
         }
         else
-            return '<vr-section title="{{ctrl.title}}"><div><table class="table  table-striped" ><tr ng-repeat="value in ctrl.measureTypes" ><td><vr-label isValue="{{value}}">{{value.DisplayName}}</vr-label></td></tr></table></vr-section>';
+            //  return '<vr-section title="{{ctrl.title}}"><div><table class="table  table-striped" ><tr ng-repeat="value in ctrl.measureTypes" ><td><vr-label isValue="{{value}}">{{value.DisplayName}}</vr-label></td></tr></table></vr-section>';
+            return '<vr-section title="{{ctrl.title}}"><div><div class="circle-div" ng-repeat="value in ctrl.measureTypes" ><div class="circle-value" > </vr-label></div><div  class="circle-label" >{{value.DisplayName}}</div></vr-section>';
 
 
 
@@ -56,7 +77,7 @@ app.directive('vrBiSummary', ['UtilsService', 'BIConfigurationAPIService', 'BIAP
         function initializeController() {
             UtilsService.waitMultipleAsyncOperations([loadMeasures])
            .then(function () {
-               
+
                if (!BIUtilitiesService.checkPermissions(measures)) {
                    ctrl.isAllowed = false;
                    return;
@@ -83,8 +104,8 @@ app.directive('vrBiSummary', ['UtilsService', 'BIConfigurationAPIService', 'BIAP
 
 
 
-        
-            
+
+
         }
 
         function defineAPI() {
@@ -100,21 +121,21 @@ app.directive('vrBiSummary', ['UtilsService', 'BIConfigurationAPIService', 'BIAP
             ctrl.isGettingData = true;
             return BIAPIService.GetMeasureValues1(filter.fromDate, filter.toDate, settings.MeasureTypes)
                         .then(function (response) {
-                          
+
                             for (var i = 0; i < response.length; i++) {
                                 ctrl.dataSource[i] = {
                                     value: response[i].toFixed(2),
                                     description: ctrl.measureTypes[i]
                                 }
                             }
-                           
-                           
+
+
                         }).finally(function () {
                             ctrl.isGettingData = false;
                         });
         }
         function loadMeasures() {
-           
+
             return BIConfigurationAPIService.GetMeasures().then(function (response) {
                 for (var i = 0; i < settings.MeasureTypes.length; i++) {
                     var value = UtilsService.getItemByVal(response, settings.MeasureTypes[i], 'Name');
