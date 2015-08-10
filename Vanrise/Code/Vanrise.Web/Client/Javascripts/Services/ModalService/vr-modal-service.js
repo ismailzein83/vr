d9bda22e65f637c0ae61658b45d7d3189a9bdad9
mustafa.hawi <mustafa.hawi@vanrise.tfs.local>
2015-08-10 12:55:26 +0000

    Refactoring
    git-tfs-id: [http://developmentvlan:8585/tfs/vanrise.collection]$/;C25625

--- Vanrise/Code/Vanrise.Web/Client/Javascripts/Services/ModalService/vr-modal-service.js
+++ Vanrise/Code/Vanrise.Web/Client/Javascripts/Services/ModalService/vr-modal-service.js
@@ -1,46 +1,57 @@
-﻿'use strict';
+﻿(function (app) {
+    
+    "use strict";
 
-app.service('VRModalService', function ($modal, $rootScope, VRNavigationService, $q) {
-
-    return ({
-        showModal: showModal
-    });
-
-    function showModal(viewUrl, parameters, settings) {
-        var deferred = $q.defer();
-        var modalScope = $rootScope.$new();
-        
-        var modalUrl = viewUrl;
-        var modalInstance;
-        modalScope.modalContext = {};
-        modalScope.modalContext.closeModal = function () {
-            if (modalInstance) modalInstance.hide();
-            deferred.resolve();
-        };
-
-        VRNavigationService.setParameters(modalScope, parameters);
-
-        if (settings != undefined && settings != null) {
-            if (settings.useModalTemplate == true) {
-                modalUrl = '/Client/Javascripts/Services/ModalService/vr-modal-service.html';
-                modalScope.templateUrl = viewUrl;
-            }
-
-            modalScope.modalWidth = settings.width;
-            modalScope.modalMaxHeight = settings.maxHeight;
-
-            modalScope.title = settings.title;
-
-            if (settings.onScopeReady != undefined)
-                settings.onScopeReady(modalScope);
-        }
-
-        modalScope.$on('modal.hide.before', function () {
-            if (typeof (modalScope.modalContext.onModalHide) == "function") modalScope.modalContext.onModalHide();
+    function serviceObj($modal, $rootScope, VRNavigationService, $q) {
+        return ({
+            showModal: showModal
         });
 
-        modalInstance = $modal({ scope: modalScope, templateUrl: modalUrl, show: true, animation: "am-fade-and-scale" });
-        return deferred.promise;
+        function showModal(viewUrl, parameters, settings) {
+            var deferred = $q.defer();
+            var modalScope = $rootScope.$new();
+
+            var modalUrl = viewUrl;
+            var modalInstance;
+            modalScope.modalContext = {};
+            modalScope.modalContext.closeModal = function () {
+                if (modalInstance) modalInstance.hide();
+                deferred.resolve();
+            };
+
+            VRNavigationService.setParameters(modalScope, parameters);
+
+            if (settings != undefined && settings != null) {
+                if (settings.useModalTemplate === true) {
+                    modalUrl = '/Client/Javascripts/Services/ModalService/vr-modal-service.html';
+                    modalScope.templateUrl = viewUrl;
+                }
+
+                modalScope.modalWidth = settings.width;
+                modalScope.modalMaxHeight = settings.maxHeight;
+
+                modalScope.title = settings.title;
+
+                if (settings.onScopeReady != undefined)
+                    settings.onScopeReady(modalScope);
+            }
+
+            modalScope.$on('modal.hide.before', function () {
+                if (typeof (modalScope.modalContext.onModalHide) == "function") modalScope.modalContext.onModalHide();
+            });
+
+            modalInstance = $modal({ scope: modalScope, templateUrl: modalUrl, show: true, animation: "am-fade-and-scale" });
+            return deferred.promise;
+        }
     }
-});
+
+    serviceObj.$inject = ['$modal', '$rootScope', 'VRNavigationService', '$q'];
+    app.service('VRModalService', serviceObj);
+
+
+})(app);
+
+
+
+
 
