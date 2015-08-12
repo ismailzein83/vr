fade38a1bcf3ac722618d0868e0bb4c59a39fb7e
mustafa.hawi <mustafa.hawi@vanrise.tfs.local>
2015-08-12 14:48:06 +0000

    refactoring
    git-tfs-id: [http://developmentvlan:8585/tfs/vanrise.collection]$/;C25729

--- TOneV2/Code/TOneV2/TOne.Analytics.Web/Analytics/Views/Traffic Statistics/ReleaseCodeStatisticsController.js
+++ TOneV2/Code/TOneV2/TOne.Analytics.Web/Analytics/Views/Traffic Statistics/ReleaseCodeStatisticsController.js
@@ -1,93 +1,105 @@
 ﻿(function(appControllers) {
 
     "use strict";
-	
-    releaseCodeStatisticsController.$inject = ['$scope', 'AnalyticsService', 'BusinessEntityAPIService_temp', 'CarrierAccountAPIService'];
 
     function releaseCodeStatisticsController($scope, analyticsService, businessEntityApiService, carrierAccountApiService) {
         
-		function defineScope(){
-			
-		}
+        function defineScope() {
+
+            $scope.showResult = false;
+
+            $scope.searchClicked = function () {
+
+            };
+        }
 		
-		function defineGrid() {
+        function defineGrid() {
+         
+            //$scope.dataRetrievalFunction = function (dataRetrievalInput, onResponseReady) {
+            //    return AnalyticsAPIService.GetTrafficStatisticSummary(dataRetrievalInput).then(function(response) {
+                    
+            //        onResponseReady(response);
+            //        $scope.showResult = true;
+            //    });
+            //};
             
         }
 		
-		function loadSwitches() {
-		    return businessEntityApiService.GetSwitches().then(function (response) {
-		        $scope.switches = response;
-		    });
-		}
+        function loadSwitches() {
+            return businessEntityApiService.GetSwitches().then(function (response) {
+                $scope.switches = response;
+            });
+        }
 
-		function loadCodeGroups() {
-		    return businessEntityApiService.GetCodeGroups().then(function (response) {
-		        $scope.codeGroups = response;
-		    });
-		}
+        function loadCodeGroups() {
+            return businessEntityApiService.GetCodeGroups().then(function (response) {
+                $scope.codeGroups = response;
+            });
+        }
 
-		function loadCustomers() {
-		    return carrierAccountApiService.getCustomers().then(function (response) {
-		        $scope.customers = response;
-		    });
-		}
+        function loadCustomers() {
+            return carrierAccountApiService.getCustomers().then(function (response) {
+                $scope.customers = response;
+            });
+        }
 
-		function loadSuppliers() {
-		    return carrierAccountApiService.getSuppliers().then(function (response) {
-		        $scope.suppliers = response;
-		    });
-		}
+        function loadSuppliers() {
+            return carrierAccountApiService.getSuppliers().then(function (response) {
+                $scope.suppliers = response;
+            });
+        }
 
-		function defineFilters() {
+        function defineFilters() {
 
-		    $scope.fromDate = '';
-		    $scope.toDate = '';
+            $scope.fromDate = '';
+            $scope.toDate = '';
 
 
-		    $scope.groupKeys = analyticsService.getTrafficStatisticGroupKeys();
-		    $scope.selectedGroupKeys = analyticsService.getDefaultTrafficStatisticGroupKeys();
+            $scope.groupKeys = analyticsService.getTrafficStatisticGroupKeys();
+            $scope.selectedGroupKeys = analyticsService.getDefaultTrafficStatisticGroupKeys();
 
-		    $scope.switches = [];
-		    $scope.selectedSwitches = [];
+            $scope.switches = [];
+            $scope.selectedSwitches = [];
 
-		    $scope.codeGroups = [];
-		    $scope.selectedCodeGroups = [];
+            $scope.codeGroups = [];
+            $scope.selectedCodeGroups = [];
 
-		    $scope.customers = [];
-		    $scope.selectedCustomers = [];
+            $scope.customers = [];
+            $scope.selectedCustomers = [];
 
-		    $scope.suppliers = [];
-		    $scope.selectedSuppliers = [];
+            $scope.suppliers = [];
+            $scope.selectedSuppliers = [];
 
-		    $scope.periods = analyticsService.getPeriods();
-		    $scope.selectedPeriod = $scope.periods[0];
+            $scope.periods = analyticsService.getPeriods();
+            $scope.selectedPeriod = $scope.periods[0];
 
-		    $scope.periodSelectionChanged = function () {
-		        if ($scope.selectedPeriod != undefined && $scope.selectedPeriod.value !== -1) {
-		            var date = $scope.selectedPeriod.getInterval();
-		            $scope.fromDate = date.from;
-		            $scope.toDate = date.to;
-		        }
+            $scope.onPeriodSelectionChanged = function () {
+                if ($scope.selectedPeriod != undefined && $scope.selectedPeriod.value !== -1) {
+                    var date = $scope.selectedPeriod.getInterval();
+                    $scope.fromDate = date.from;
+                    $scope.toDate = date.to;
+                }
 
-		    }
+            }
 
-		    $scope.customvalidateDate = function (toDate) {
-		        return analyticsService.validateDates($scope.fromDate, toDate);
-		    };
+            $scope.customvalidateDate = function (toDate) {
+                return analyticsService.validateDates($scope.fromDate, toDate);
+            };
 
-		    loadSwitches();
-		    loadCodeGroups();
-		    loadCustomers();
-		    loadSuppliers();
-		}
+            loadSwitches();
+            loadCodeGroups();
+            loadCustomers();
+            loadSuppliers();
+        }
 
 		
 		
-		defineScope();
-		defineGrid();
-		defineFilters();
+        defineScope();
+        defineGrid();
+        defineFilters();
     }
 
+    releaseCodeStatisticsController.$inject = ['$scope', 'AnalyticsService', 'BusinessEntityAPIService_temp', 'CarrierAccountAPIService'];
     appControllers.controller('Analytics_ReleaseCodeStatisticsController', releaseCodeStatisticsController);
 
 })(appControllers);
