34512e671ca3419cc3ab6c777bb292c8465c604b
mustafa.hawi <mustafa.hawi@vanrise.tfs.local>
2015-08-12 14:44:54 +0000

    refactoring columns directive
    git-tfs-id: [http://developmentvlan:8585/tfs/vanrise.collection]$/;C25728

--- Vanrise/Code/Vanrise.Web/Client/Javascripts/Directives/Containers/vr-columns-directive.js
+++ Vanrise/Code/Vanrise.Web/Client/Javascripts/Directives/Containers/vr-columns-directive.js
@@ -1,39 +1,46 @@
-﻿'use strict';
+﻿(function (app) {
 
-app.directive('vrColumns', ['$compile', function ($compile) {
+    "use strict";
 
-    var directiveDefinitionObject = {
-        restrict: 'E',
-        scope: {},
-        compile: function (tElement, tAttrs) {
-            var numberOfColumns = tAttrs.colnum;
-            if (numberOfColumns == undefined)
-                numberOfColumns = 1;
+    app.directive('vrColumns', function () {
 
-            if (tAttrs.width != undefined) {
-                switch(tAttrs.width)
-                {
-                    case "normal": numberOfColumns = 2; break;
-                    case "small": numberOfColumns = 1; break;
-                    case "large": numberOfColumns = 3; break;
-                    case "fullrow": numberOfColumns = 12; break;
-                    case "1/2row": numberOfColumns = 6; break;
-                    case "1/4row": numberOfColumns = 3; break;
-                    case "1/3row": numberOfColumns = 4; break;
+        var directiveDefinitionObject = {
+            restrict: 'E',
+            scope: {},
+            compile: function (tElement, tAttrs) {
+                var numberOfColumns = tAttrs.colnum;
+                if (numberOfColumns == undefined)
+                    numberOfColumns = 1;
+
+                if (tAttrs.width != undefined) {
+                    switch (tAttrs.width) {
+                        case "normal": numberOfColumns = 2; break;
+                        case "small": numberOfColumns = 1; break;
+                        case "large": numberOfColumns = 3; break;
+                        case "fullrow": numberOfColumns = 12; break;
+                        case "1/2row": numberOfColumns = 6; break;
+                        case "1/4row": numberOfColumns = 3; break;
+                        case "1/3row": numberOfColumns = 4; break;
+                    }
                 }
+
+                var nbrcolsm = (numberOfColumns <= 3) ? numberOfColumns * 2 : 12;
+
+                if (tAttrs.colnumsm != undefined) {
+                    nbrcolsm = tAttrs.colnumsm;
+                }
+                var otherCol = ' col-md-' + numberOfColumns + ' col-sm-' + nbrcolsm;
+                var newElement = '<div class="col-lg-' + numberOfColumns + otherCol + ' " >' + tElement.context.innerHTML + '</div>';
+                tElement.html(newElement);
             }
+        };
 
-            var nbrcolsm = (numberOfColumns <= 3) ? numberOfColumns * 2 : 12;
+        return directiveDefinitionObject;
 
-            if (tAttrs.colnumsm != undefined) {
-                nbrcolsm = tAttrs.colnumsm
-            }
-            var otherCol = ' col-md-' + numberOfColumns + ' col-sm-' + nbrcolsm;
-            var newElement = '<div class="col-lg-' + numberOfColumns + otherCol + ' " >' + tElement.context.innerHTML + '</div>';
-            tElement.html(newElement);
-        }
-    };
+    });
 
-    return directiveDefinitionObject;
 
-}]);
+})(app);
+
+
+
