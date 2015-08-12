d483bcf709a393461712f6961a47105201d9d756
mustafa.hawi <mustafa.hawi@vanrise.tfs.local>
2015-08-12 10:46:59 +0000

    refactoring vr textbox directive
    git-tfs-id: [http://developmentvlan:8585/tfs/vanrise.collection]$/;C25692

--- Vanrise/Code/Vanrise.Web/Client/Javascripts/Directives/Inputs/Textbox/vr-textbox-directive.js
+++ Vanrise/Code/Vanrise.Web/Client/Javascripts/Directives/Inputs/Textbox/vr-textbox-directive.js
@@ -1,91 +1,106 @@
-﻿'use strict';
+﻿(function (app) {
 
-app.directive('vrTextbox', ['ValidationMessagesEnum', 'BaseDirService', function (ValidationMessagesEnum, BaseDirService) {
+    "use strict";
 
-    var directiveDefinitionObject = {
-        restrict: 'E',
-        require: '^form',
-        scope: {
-            value: '=',
-            customvalidate: '&'
-        },
-        controller: function ($scope, $element) {
+    vrTextbox.$inject = ['BaseDirService', 'TextboxTypeEnum'];
 
-        },
-        compile: function (element, attrs) {
+    function vrTextbox(BaseDirService, TextboxTypeEnum) {
 
-            var inputElement = element.find('#mainInput');
-            var validationOptions = {};
-            if (attrs.isrequired !== undefined)
-                validationOptions.requiredValue = true;
-            if (attrs.customvalidate !== undefined)
-                validationOptions.customValidation = true;
-            if (attrs.type == "email")
-                validationOptions.emailValidation = true;
+        return {
+            restrict: 'E',
+            require: '^form',
+            scope: {
+                value: '=',
+                customvalidate: '&'
+            },
+            controller: function ($scope, $element) {
 
-            var elementName = BaseDirService.prepareDirectiveHTMLForValidation(validationOptions, inputElement, inputElement, element.find('#rootDiv'));
-            return {
-                pre: function ($scope, iElem, iAttrs, formCtrl) {
-                    var ctrl = $scope.ctrl;
+            },
+            compile: function (element, attrs) {
 
-                    var isUserChange;
-                    $scope.$watch('ctrl.value', function (newValue, oldValue) {
-                        //if (!isValidElem())
-                        //    return;
+                var inputElement = element.find('#mainInput');
+                var validationOptions = {};
+                if (attrs.isrequired !== undefined)
+                    validationOptions.requiredValue = true;
+                if (attrs.customvalidate !== undefined)
+                    validationOptions.customValidation = true;
+                if (attrs.type === TextboxTypeEnum.Email.name)
+                    validationOptions.emailValidation = true;
 
-                        if (!isUserChange)//this condition is used because the event will occurs in two cases: if the user changed the value, and if the value is received from the view controller
-                            return;
-                        isUserChange = false;//reset the flag
-                        if (iAttrs.type == "number") {
-                            var arr = String(newValue).split("");
-                            if (arr.length === 0) return;
-                            if (arr.length === 1 && (arr[0] == '-' || arr[0] === '.')) return;
-                            if (arr.length === 2 && newValue === '-.') return;
-                            if (isNaN(newValue)) {
-                                ctrl.value = oldValue;
+                var elementName = BaseDirService.prepareDirectiveHTMLForValidation(validationOptions, inputElement, inputElement, element.find('#rootDiv'));
+                return {
+                    pre: function ($scope, iElem, iAttrs, formCtrl) {
+                        var ctrl = $scope.ctrl;
+
+                        var isUserChange;
+                        $scope.$watch('ctrl.value', function (newValue, oldValue) {
+                            //if (!isValidElem())
+                            //    return;
+
+                            if (!isUserChange)//this condition is used because the event will occurs in two cases: if the user changed the value, and if the value is received from the view controller
+                                return;
+                            isUserChange = false;//reset the flag
+                            if (iAttrs.type === TextboxTypeEnum.Number.name) {
+                                var arr = String(newValue).split("");
+                                if (arr.length === 0) return;
+                                if (arr.length === 1 && (arr[0] === '-' || arr[0] === '.')) return;
+                                if (arr.length === 2 && newValue === '-.') return;
+                                if (isNaN(newValue)) {
+                                    ctrl.value = oldValue;
+                                }
                             }
-                        }                       
-                        if (iAttrs.onvaluechanged != undefined) {
-                            var onvaluechangedMethod = $scope.$parent.$eval(iAttrs.onvaluechanged);
-                            if (onvaluechangedMethod != undefined && onvaluechangedMethod != null && typeof (onvaluechangedMethod) == 'function') {
-                                onvaluechangedMethod();
+                            if (iAttrs.onvaluechanged != undefined) {
+                                var onvaluechangedMethod = $scope.$parent.$eval(iAttrs.onvaluechanged);
+                                if (onvaluechangedMethod != undefined && typeof (onvaluechangedMethod) == 'function') {
+                                    onvaluechangedMethod();
+                                }
                             }
-                        }
-                    });
+                        });
 
-                    ctrl.notifyUserChange = function () {
-                        isUserChange = true;
-                    };
+                        ctrl.notifyUserChange = function () {
+                            isUserChange = true;
+                        };
 
-                    BaseDirService.addScopeValidationMethods(ctrl, elementName, formCtrl);
+                        BaseDirService.addScopeValidationMethods(ctrl, elementName, formCtrl);
 
+                    }
                 }
+            },
+
+            controllerAs: 'ctrl',
+            bindToController: true,
+            template: function (element, attrs) {
+                var startTemplate = '<div id="rootDiv" style="position: relative;">';
+                var endTemplate = '</div>';
+
+                var labelTemplate = '';
+                if (attrs.label != undefined)
+                    labelTemplate = '<vr-label>' + attrs.label + '</vr-label>';
+                var type = 'text';
+                if (attrs.type != undefined && attrs.type === TextboxTypeEnum.Password.name)
+                    type = 'password';
+                var textboxTemplate = '<div ng-mouseenter="showtd=true" ng-mouseleave="showtd=false">'
+                                    + '<input id="mainInput" ng-model="ctrl.value" ng-change="ctrl.notifyUserChange()" size="10" class="form-control" data-autoclose="1" type="' + type + '" >'
+                                + '</div>';
+
+                var validationTemplate = BaseDirService.getValidationMessageTemplate(true, false, true, true, true, true, attrs.label != undefined);
+
+                return startTemplate + labelTemplate + textboxTemplate + validationTemplate + endTemplate;
             }
-        },
 
-        controllerAs: 'ctrl',
-        bindToController: true,
-        template: function (element, attrs) {
-            var startTemplate = '<div id="rootDiv" style="position: relative;">';
-            var endTemplate = '</div>';
+        };
 
-            var labelTemplate = '';
-            if (attrs.label != undefined)
-                labelTemplate = '<vr-label>' + attrs.label + '</vr-label>';
-            var type = 'text';
-            if (attrs.type != undefined && attrs.type == 'password')
-                type = 'password';
-            var textboxTemplate = '<div ng-mouseenter="showtd=true" ng-mouseleave="showtd=false">'
-                                + '<input id="mainInput" ng-model="ctrl.value" ng-change="ctrl.notifyUserChange()" size="10" class="form-control" data-autoclose="1" type="' + type + '" >'
-                            + '</div>';
+    }
 
-            var validationTemplate = BaseDirService.getValidationMessageTemplate(true, false, true, true, true, true, attrs.label != undefined);
+    app.directive('vrTextbox', vrTextbox);
 
-            return startTemplate + labelTemplate + textboxTemplate + validationTemplate + endTemplate;
-        }
+    app.constant('TextboxTypeEnum', {
+        Email: { name: "email" },
+        Number: { name: "number" },
+        Password: { name: "password" }
+    });
 
-    };
+})(app);
 
-    return directiveDefinitionObject;
 
-}]);
+
