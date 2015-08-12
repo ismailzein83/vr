cd6492274947e6a972c415e188ac45dabc8f6b5e
mustafa.hawi <mustafa.hawi@vanrise.tfs.local>
2015-08-12 12:12:23 +0000

    add release code statistics page - story 185
    git-tfs-id: [http://developmentvlan:8585/tfs/vanrise.collection]$/;C25701

---
+++ TOneV2/Code/TOneV2/TOne.Analytics.Web/Analytics/Views/Traffic Statistics/ReleaseCodeStatisticsController.js
@@ -0,0 +1,37 @@
+﻿(function(appControllers) {
+
+    "use strict";
+	
+    releaseCodeStatisticsController.$inject = ['$scope'];
+
+    function releaseCodeStatisticsController($scope) {
+        
+		function defineScope(){
+			$scope.onSearchClicked = function () {
+                mainGridApi.clearDataAndContinuePaging();
+                return getGridData();
+            };
+		}
+		
+		function defineGrid() {
+            $scope.gridDataSource = [];
+            $scope.loadMoreData = function () {
+                return getGridData();
+            };
+            $scope.onGridReady = function (api) {
+                mainGridApi = api;
+            };
+        }
+		
+		function defineFilters() {
+			console.log("Define filters");
+		}
+		
+		defineScope();
+		defineGrid();
+		defineFilters();
+    }
+
+    appControllers.controller('Analytics_ReleaseCodeStatisticsController', releaseCodeStatisticsController);
+
+})(appControllers);
