dc1ad0c5c63526eb091c889a8d7439d3d3d92cda
Ali Ballouk <VANRISE\ali.ballouk>
2015-08-12 10:02:54 +0000

    Add custom warning levels + adjust vr-circle container label class
    git-tfs-id: [http://developmentvlan:8585/tfs/vanrise.collection]$/;C25689

--- Vanrise/Code/Vanrise.Web/Client/Javascripts/Constants/LabelColorsEnum.js
+++ Vanrise/Code/Vanrise.Web/Client/Javascripts/Constants/LabelColorsEnum.js
@@ -2,6 +2,8 @@
 app.constant('LabelColorsEnum', {
     Success: { Color: "label label-success" },
     Warning: { Color: "label label-warning" },
+    WarningLevel1: { Color: "label label-warning-1" },
+    WarningLevel2: { Color: "label label-warning-2" },
     Error: { Color: "label label-danger" },
     Info: { Color: "label label-info" },
     Primary: { Color: "label label-primary" },
