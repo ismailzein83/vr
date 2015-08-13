1cc055b634bde8ec4da18a52d3847b5048bae261
Samer Haidar <VANRISE\samer.haidar>
2015-08-13 10:56:45 +0000

    new design
    git-tfs-id: [http://developmentvlan:8585/tfs/vanrise.collection]$/;C25765

--- TOneV2/Code/TOneV2/TOne.Analytics.Web/Analytics/Constants/TrafficStatisticGroupKeysEnum.js
+++ TOneV2/Code/TOneV2/TOne.Analytics.Web/Analytics/Constants/TrafficStatisticGroupKeysEnum.js
@@ -6,8 +6,8 @@
     CodeGroup: { title: "Code Group", value: 4, description: "Code Group", gridHeader: "Code Group", isShownInGroupKey: true },
     Switch: { title: "Switch", value: 5, description: "Switch", gridHeader: "Switch", isShownInGroupKey: true },
 
-    GateWayIn: { title: "GateWay-In", value: 6, description: "GateWay-In", gridHeader: "GateWay In", isShownInGroupKey: true },
-    GateWayOut: { title: "GateWay-Out", value: 7, description: "GateWay-Out", gridHeader: "GateWay-Out", isShownInGroupKey: true },
+    GateWayIn: { title: "GateWayIn", value: 6, description: "GateWayIn", gridHeader: "GateWay In", isShownInGroupKey: true },
+    GateWayOut: { title: "GateWayOut", value: 7, description: "GateWayOut", gridHeader: "GateWay Out", isShownInGroupKey: true },
 
     PortIn: { title: "Port In", value: 8, description: "Port In", gridHeader: "Port In", isShownInGroupKey: true },
     PortOut: { title: "Port Out", value: 9, description: "Port Out", gridHeader: "Port Out",  isShownInGroupKey: true },
