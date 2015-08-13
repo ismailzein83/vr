3c6f7bb649d9c7c89af25d53d93bcba5d8dba9d7
walid.mogharbel <walid.mogharbel@vanrise.tfs.local>
2015-08-13 08:44:54 +0000

    git-tfs-id: [http://developmentvlan:8585/tfs/vanrise.collection]$/;C25749

---
+++ CDRAnalysis/CDRAnalysis/FraudAnalysis/Stored Procedures/sp_Strategy_DeleteStrategyResults.sql
@@ -0,0 +1,13 @@
+﻿
+
+CREATE PROCEDURE [FraudAnalysis].[sp_Strategy_DeleteStrategyResults] 
+    @StrategiesList varchar(200), 
+	@FromDate DateTime,
+	@ToDate DateTime
+AS
+BEGIN
+
+	EXEC('Delete FraudAnalysis.[NumberProfile] where StrategyId in ('+@StrategiesList+') and FromDate>='''+@FromDate+''' and ToDate <='''+@ToDate+'''')
+    EXEC('Delete FraudAnalysis.[SubscriberThreshold] where StrategyId in ('+@StrategiesList+') and DateDay>='''+@FromDate+''' and DateDay <='''+@ToDate+'''')
+    
+END
