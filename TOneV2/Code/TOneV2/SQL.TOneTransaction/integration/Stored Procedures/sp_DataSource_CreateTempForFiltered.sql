1b01f01cfc68b8c64f90e6e6e4a0a5454aee00e6
Mortada Issa <VANRISE\mortada.issa>
2015-08-13 13:22:52 +0000

    Applying changes to DB after updating data source grid
    git-tfs-id: [http://developmentvlan:8585/tfs/vanrise.collection]$/;C25771

---
+++ TOneV2/Code/TOneV2/SQL.TOneTransaction/integration/Stored Procedures/sp_DataSource_CreateTempForFiltered.sql
@@ -0,0 +1,32 @@
+﻿-- =============================================
+-- Author:		<Author,,Name>
+-- Create date: <Create Date,,>
+-- Description:	<Description,,>
+-- =============================================
+CREATE PROCEDURE [integration].[sp_DataSource_CreateTempForFiltered]
+	@TempTableName VARCHAR(200)
+AS
+BEGIN
+	SET NOCOUNT ON;
+
+    IF NOT OBJECT_ID(@TempTableName, N'U') IS NOT NULL
+	    BEGIN
+			SELECT ds.[ID],
+			ds.[Name],
+			ds.[adapterID],
+			ad.[Info],
+			ds.[TaskId],
+			ds.[Settings]
+			
+			INTO #RESULT
+			
+			FROM [integration].[DataSource] AS ds
+			JOIN adapterType AS ad ON ds.adapterID = ad.ID
+			
+			DECLARE @sql VARCHAR(1000)
+			SET @sql = 'SELECT * INTO ' + @TempTableName + ' FROM #RESULT';
+			EXEC(@sql)
+		END
+
+	SET NOCOUNT OFF
+END
---
+++ TOneV2/Code/TOneV2/SQL.TOneTransaction/integration/Stored Procedures/sp_DataSource_Delete.sql
@@ -0,0 +1,12 @@
+﻿-- =============================================
+-- Author:		<Author,,Name>
+-- Create date: <Create Date,,>
+-- Description:	<Description,,>
+-- =============================================
+CREATE PROCEDURE [integration].[sp_DataSource_Delete]
+	@Id INT
+AS
+BEGIN
+	DELETE FROM [integration].[DataSource]
+	WHERE ID = @Id
+END
