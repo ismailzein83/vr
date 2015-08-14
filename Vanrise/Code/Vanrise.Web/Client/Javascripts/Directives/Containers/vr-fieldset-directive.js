05468ac0e87bcb659b0e5b217798d7fe6260c190
Ali Ballouk <VANRISE\ali.ballouk>
2015-08-14 12:25:09 +0000

    Add vr-fieldset directive
    git-tfs-id: [http://developmentvlan:8585/tfs/vanrise.collection]$/;C25805

---
+++ Vanrise/Code/Vanrise.Web/Client/Javascripts/Directives/Containers/vr-fieldset-directive.js
@@ -0,0 +1,17 @@
+﻿'use strict';
+
+app.directive('vrFieldset', [function () {
+
+    var directiveDefinitionObject = {
+        restrict: 'E',
+        scope: false,
+        compile: function (tElement, tAttrs) {
+            var newElement = '<div class="panel-primary fieldset-vr"><div class="panel-heading"><span class="title">' + tAttrs.title + '</span></div><div class="panel-body">' + tElement.context.innerHTML + '</div></div>';
+            tElement.html(newElement);
+        }
+
+    };
+
+    return directiveDefinitionObject;
+
+}]);
