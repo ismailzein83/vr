ba2e95fde543030c5f773f370cf2f336f612994f
walid.mogharbel <walid.mogharbel@vanrise.tfs.local>
2015-08-17 08:47:53 +0000

    git-tfs-id: [http://developmentvlan:8585/tfs/vanrise.collection]$/;C25830

---
+++ CDRAnalysis/CDRAnalysis/FraudAnalysis/Tables/AccountCase.sql
@@ -0,0 +1,7 @@
+﻿CREATE TABLE [FraudAnalysis].[AccountCase] (
+    [AccountNumber] VARCHAR (50) NOT NULL,
+    [StatusID]      INT          NOT NULL,
+    [ValidTill]     DATETIME     NULL,
+    CONSTRAINT [PK_SubscriberCase] PRIMARY KEY CLUSTERED ([AccountNumber] ASC)
+);
+
