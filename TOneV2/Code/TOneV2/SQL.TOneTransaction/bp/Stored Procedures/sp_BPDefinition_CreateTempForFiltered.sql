bf26da0af9b3c2ea530254e33831bf39da6906d9
walid.mogharbel <walid.mogharbel@vanrise.tfs.local>
2015-08-17 06:41:42 +0000

    updated sql of transaction
    git-tfs-id: [http://developmentvlan:8585/tfs/vanrise.collection]$/;C25823

---
+++ TOneV2/Code/TOneV2/SQL.TOneTransaction/bp/Stored Procedures/sp_BPDefinition_CreateTempForFiltered.sql
@@ -0,0 +1,25 @@
+﻿
+Create PROCEDURE [bp].[sp_BPDefinition_CreateTempForFiltered]
+	@TempTableName nvarchar(200),
+	@Title nvarchar(255)
+AS
+BEGIN
+	-- SET NOCOUNT ON added to prevent extra result sets from
+	-- interfering with SELECT statements.
+	SET NOCOUNT ON;
+	
+	IF NOT OBJECT_ID(@TempTableName, N'U') IS NOT NULL
+	    BEGIN
+		
+			SELECT bpDef.ID ,Name ,Title ,bpDef.FQTN ,bpDef.Config
+			INTO #RESULT
+			FROM [bp].[BPDefinition] bpDef WITH(NOLOCK)
+			Where  (Title like '%'+@Title +'%' or @Title is null) 
+			ORDER BY Name 
+			
+			DECLARE @sql VARCHAR(1000)
+			SET @sql = 'SELECT * INTO ' + @TempTableName + ' FROM #RESULT';
+			EXEC(@sql)
+		END
+
+END
