60c3434150c855399ccd53d5873ed1c8e05513a0
hadi.hawi <hadi.hawi@vanrise.tfs.local>
2015-08-17 13:55:29 +0000

    Carrier Summary Stats
    git-tfs-id: [http://developmentvlan:8585/tfs/vanrise.collection]$/;C25858

---
+++ TOneV2/Code/TOneV2/TOne.Analytics.Web/Analytics/Views/CarrierSummaryController.js
@@ -0,0 +1,73 @@
+﻿(function (appControllers) {
+
+    "use strict";
+
+    CarrierSummaryController.$inject = ['$scope', 'CarrierSummaryStatsAPIService', 'CarrierAccountAPIService', 'CurrencyAPIService', 'CarrierTypeEnum', 'VRModalService'];
+    function CarrierSummaryController($scope, CarrierSummaryStatsAPIService, CarrierAccountAPIService, CurrencyAPIService, CarrierTypeEnum, VRModalService) {
+
+        var mainGridApi;
+
+        function retrieveData() {
+            return mainGridApi.retrieveData({
+                Name: $scope.name
+            });
+        }
+
+
+        function defineGrid() {
+
+            $scope.datasource = [];
+
+            $scope.onGridReady = function (api) {
+                mainGridApi = api;
+                return retrieveData();
+            };
+
+            $scope.dataRetrievalFunction = function (dataRetrievalInput, onResponseReady) {
+                return CarrierSummaryStatsAPIService.GetFilteredCarrierMasks(dataRetrievalInput)
+                .then(function (response) {
+
+                    onResponseReady(response);
+                });
+            };
+        }
+
+        function defineScope() {
+
+            $scope.datasource = [];
+
+            $scope.optionsCurrencies = {
+                selectedvalues: '',
+                datasource: []
+            };
+        }
+
+        function load() {
+
+            loadCarriers();
+            loadCurrencies();
+
+        }
+
+        function loadCarriers() {
+            return CarrierAccountAPIService.GetCarriers(CarrierTypeEnum.SaleZone.value).then(function (response) {
+                angular.forEach(response, function (itm) {
+                    $scope.datasource.push(itm);
+                });
+            });
+        }
+
+        function loadCurrencies() {
+            return CurrencyAPIService.GetCurrencies().then(function (response) {
+                $scope.optionsCurrencies.datasource = response;
+            });
+
+        }
+
+
+        defineScope();
+        load();
+    }
+    appControllers.controller('Carrier_CarrierSummaryController', CarrierSummaryController);
+
+})(appControllers);
---
+++ TOneV2/Code/TOneV2/TOne.Analytics.Web/Controllers/CarrierSummaryStatsController.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using TOne.Analytics.Business;
+using TOne.Analytics.Entities;
+using Vanrise.Entities;
+
+namespace TOne.Analytics.Web.Controllers
+{
+    public class CarrierSummaryStatsController : Vanrise.Web.Base.BaseAPIController
+    {
+        //[HttpPost]
+        //public Object GetFilteredCarrierSummaryStats(Vanrise.Entities.DataRetrievalInput<CarrierMaskQuery> input)
+        //{
+        //    CarrierSummaryStatsManager manager = new CarrierSummaryStatsManager();
+        //    return GetWebResponse(input, manager.GetCarrierSummaryStats(input));
+        //}
+
+    }
+}
