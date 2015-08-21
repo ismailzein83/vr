48b795e57d01abdfd59fb0d840fd9e2c5a559cdf
Ali Ballouk <VANRISE\ali.ballouk>
2015-08-21 14:41:03 +0000

    IToDConsiderationDataManager implement service data access layer
    git-tfs-id: [http://developmentvlan:8585/tfs/vanrise.collection]$/;C26003

--- TOneV2/Code/TOneV2/TOne.BusinessEntity.Data/IToDConsiderationDataManager.cs
+++ TOneV2/Code/TOneV2/TOne.BusinessEntity.Data/IToDConsiderationDataManager.cs
@@ -10,5 +10,6 @@ namespace TOne.BusinessEntity.Data
     public interface IToDConsiderationDataManager : IDataManager
     {
         List<ToDConsideration> GetToDConsideration(string customerId, int zoneId, DateTime when);
+        Vanrise.Entities.BigResult<TODConsiderationInfo> GetCarrierMasksByCriteria(Vanrise.Entities.DataRetrievalInput<TODCustomerQuery> input);
     }
 }
