8c849147cfa50091184cc46075946e368c38c9d0
Mortada Issa <VANRISE\mortada.issa>
2015-08-21 11:13:27 +0000

    Adding new procedure
    git-tfs-id: [http://developmentvlan:8585/tfs/vanrise.collection]$/;C25987

---
+++ TOneV2/Code/TOneV2/SQL.TOneTransaction/integration/Stored Procedures/sp_DataSource_UpdateSettings.sql
@@ -0,0 +1,14 @@
+﻿-- =============================================
+-- Author:		<Author,,Name>
+-- Create date: <Create Date,,>
+-- Description:	<Description,,>
+-- =============================================
+CREATE PROCEDURE [integration].[sp_DataSource_UpdateSettings]
+	@DataSourceID int,
+	@Settings varchar(max)
+AS
+BEGIN
+	Update integration.DataSource 
+	Set	Settings = @Settings
+	Where ID = @DataSourceID
+END
