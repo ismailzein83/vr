b2f339451a1b6f27f57ccf1bfc03549df4ccc577
walid.mogharbel <walid.mogharbel@vanrise.tfs.local>
2015-08-24 08:18:16 +0000

    git-tfs-id: [http://developmentvlan:8585/tfs/vanrise.collection]$/;C26020

--- CDRAnalysis/CDRAnalysis/Security/RoleMemberships.sql
+++
@@ -1,6 +0,0 @@
-﻿EXECUTE sp_addrolemember @rolename = N'db_securityadmin', @membername = N'VANRISE\development';
-
-
-GO
-EXECUTE sp_addrolemember @rolename = N'db_securityadmin', @membername = N'development';
-
