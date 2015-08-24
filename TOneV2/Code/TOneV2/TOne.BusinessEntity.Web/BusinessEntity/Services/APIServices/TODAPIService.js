c932c177d64510b2dc7753fdc5ef6be29b11a67e
Ali Ballouk <VANRISE\ali.ballouk>
2015-08-24 14:48:03 +0000

    Add TODAPIService
    git-tfs-id: [http://developmentvlan:8585/tfs/vanrise.collection]$/;C26042

---
+++ TOneV2/Code/TOneV2/TOne.BusinessEntity.Web/BusinessEntity/Services/APIServices/TODAPIService.js
@@ -0,0 +1,13 @@
+﻿'use strict'
+var serviceObj = function (BaseAPIService) {
+    return ({
+        GetFilteredCustomerTOD: GetFilteredCustomerTOD,
+    });
+
+    function GetFilteredCustomerTOD(input) {
+        return BaseAPIService.post("/api/TOD/GetCutomerTODFromTempTable", input);
+    }
+   
+}
+serviceObj.$inject = ['BaseAPIService'];
+appControllers.service('TODAPIService', serviceObj);
