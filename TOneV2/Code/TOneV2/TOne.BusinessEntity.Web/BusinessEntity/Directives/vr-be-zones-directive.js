267a1a7664d39c559564b07d49fbd61eeca2a91a
mustafa.hawi <mustafa.hawi@vanrise.tfs.local>
2015-08-25 10:27:35 +0000

    add new zones directive
    git-tfs-id: [http://developmentvlan:8585/tfs/vanrise.collection]$/;C26081

---
+++ TOneV2/Code/TOneV2/TOne.BusinessEntity.Web/BusinessEntity/Directives/vr-be-zones-directive.js
@@ -0,0 +1,34 @@
+﻿(function (angular,app) {
+
+    "use strict";
+
+    app.directive('vrBeZones', ['ZoneAPIService', function (zoneApiService) {
+
+        var directiveDefinitionObject = {
+            restrict: 'E',
+            scope: {
+                selectedvalues: "="
+            },
+            controller: function () {
+                
+                function zonesDatasource(text) {
+                    return zoneApiService.GetOwnZones(text);
+                }
+
+                angular.extend(this, {
+                    zonesDatasource: zonesDatasource
+                });
+
+            },
+            controllerAs: 'ctrl',
+            bindToController: true,
+            template: function () {
+                return '<vr-select ismultipleselection datasource="ctrl.zonesDatasource" label="Zones" selectedvalues="ctrl.selectedvalues" datatextfield="Name" datavaluefield="ZoneId" entityname="Zones"></vr-select>';
+            }
+
+        };
+        
+        return directiveDefinitionObject;
+    }]);
+
+})(angular,app);
