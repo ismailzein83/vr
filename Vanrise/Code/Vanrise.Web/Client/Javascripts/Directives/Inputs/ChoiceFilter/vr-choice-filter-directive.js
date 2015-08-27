ef4faf3b653f3d08ef81a57f76d2e261c468a4da
Ali Ballouk <VANRISE\ali.ballouk>
2015-08-27 09:09:30 +0000

    frame-work look and feel enhancement + add vr-choices-filter directive
    git-tfs-id: [http://developmentvlan:8585/tfs/vanrise.collection]$/;C26163

---
+++ Vanrise/Code/Vanrise.Web/Client/Javascripts/Directives/Inputs/ChoiceFilter/vr-choice-filter-directive.js
@@ -0,0 +1,58 @@
+﻿'use strict';
+
+
+app.directive('vrChoiceFilter', [function () {
+
+    var directiveDefinitionObject = {
+        restrict: 'E',
+        transclude: true,
+        scope: {
+            isselected: '=?',
+            onselectionchanged: '='
+        },
+        require: '^vrChoicesFilter',
+        controller: function ($scope, $element, $attrs) {
+            var ctrl = this;
+        },
+        controllerAs: 'ctrl',
+        bindToController: true,
+        compile: function (element, attrs) {
+
+            //element.html(' <label class="hand-cursor" ng-class="ctrl.isselected?\'clicked-btn\':\'\'">{{ctrl.isselected}}' + element.html() + '</label><label>|</label>');
+            return {
+                pre: function ($scope, iElem, iAttrs, choicesCtrl) {
+                    var ctrl = $scope.ctrl;
+
+                    ctrl.selectionChanged = function () {
+                        ctrl.isselected = ctrl.isSelected;
+                    };
+
+                    choicesCtrl.addChoiceCtrl(ctrl);
+
+                    ctrl.choiceClicked = function () {
+                        choicesCtrl.selectChoice(ctrl);
+                    };
+
+                    $scope.$watch("ctrl.isselected", function (value) {
+                        if (ctrl.isSelected != value) {
+                            if (value)
+                                choicesCtrl.selectChoice(ctrl);
+                            else
+                                choicesCtrl.unselectChoice(ctrl);
+                        }
+                        if (ctrl.onselectionchanged && typeof (ctrl.onselectionchanged) == 'function') {
+                            ctrl.onselectionchanged();
+                        }
+                    });
+                }
+            }
+        },
+        template: '<label class="hand-cursor" ng-class="ctrl.isSelected?\'clicked-btn\':\'\'"  ng-click="ctrl.choiceClicked()" ng-transclude></label>'
+    };
+
+    return directiveDefinitionObject;
+
+
+
+}]);
+
---
+++ Vanrise/Code/Vanrise.Web/Client/Javascripts/Directives/Inputs/ChoiceFilter/vr-choices-filter-directive.js
@@ -0,0 +1,105 @@
+﻿'use strict';
+
+
+app.directive('vrChoicesFilter', [function () {
+
+    var directiveDefinitionObject = {
+        restrict: 'E',
+        scope: {
+            onReady: '=',
+            selectedindex: '=?',
+            onselectionchanged: '&'
+
+        },
+        controller: function ($scope, $element, $attrs) {
+            var ctrl = this;
+
+            var choiceCtrls = [];
+
+            ctrl.addChoiceCtrl = function (choiceCtrl) {
+                choiceCtrls.push(choiceCtrl);
+                setDefaultChoiceSeletion();
+            }
+            var triggerSelectionChanged = false;
+            ctrl.selectChoice = function (choiceCtrl) {
+                triggerSelectionChanged = true;
+                angular.forEach(choiceCtrls, function (t) {
+                    if (t != choiceCtrl)
+                        setChoiceSelection(t, false);
+                });
+                setChoiceSelection(choiceCtrl, true);
+            }
+
+            ctrl.unselectChoice = function (choiceCtrl) {
+                setChoiceSelection(choiceCtrl, false);
+                setDefaultChoiceSeletion();
+            }
+            ctrl.isradio = $attrs.isradio != undefined;
+            function setDefaultChoiceSeletion() {
+                if (choiceCtrls.length == 0 || ctrl.selectedindex > -1)
+                    return;
+                var isAnySelected = false;
+                angular.forEach(choiceCtrls, function (t) {
+                    if (t.isSelected)
+                        isAnySelected = true;
+                });
+                if (!isAnySelected)
+                    setChoiceSelection(choiceCtrls[0], true);
+            }
+
+            function setChoiceSelection(choiceCtrl, isSelected) {
+                if (choiceCtrl.isSelected != isSelected) {
+                    choiceCtrl.isSelected = isSelected;
+                    choiceCtrl.selectionChanged();
+                    if (isSelected == true)
+                        ctrl.selectedindex = choiceCtrls.indexOf(choiceCtrl);                        
+                }
+            }
+
+            $scope.$watch("ctrl.selectedindex", function (value) {
+                if (!choiceCtrls[ctrl.selectedindex].isSelected)
+                    ctrl.selectChoice(choiceCtrls[ctrl.selectedindex]);
+                else {
+                    if (ctrl.onselectionchanged && typeof (ctrl.onselectionchanged) == 'function') {
+                        if (triggerSelectionChanged == true)
+                            ctrl.onselectionchanged();
+                    }
+                    triggerSelectionChanged = false;
+                }
+            });
+
+            var api = {};
+            api.selectChoice = function (choiceIndex) {
+                var choiceCtrl = choiceCtrls[choiceIndex];
+                if (choiceCtrl != undefined)
+                    ctrl.selectChoice(choiceCtrl);
+            };
+            api.unselectChoice = function (choiceIndex) {
+                var choiceCtrl = choiceCtrls[choiceIndex];
+                if (choiceCtrl != undefined)
+                    ctrl.unselectChoice(choiceCtrl);
+            };
+            if (ctrl.onReady != null)
+                ctrl.onReady(api);
+        },
+        controllerAs: 'ctrl',
+        bindToController: true,
+        compile: function (element, attrs) {
+            //var radioclass = (attrs.isradio != undefined) ? " radio-btn-groupe" : "";
+            element.html('<div class="btn-group btn-group-custom vr-tabs vr-choices-filter "   >' + element.html() + '</div>');
+
+            return {
+                pre: function ($scope, iElem, iAttrs, ctrl) {
+                   
+                }
+            }
+        }
+
+    };
+
+    return directiveDefinitionObject;
+
+
+
+}]);
+
