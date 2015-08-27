362c30287861869925311f1be75d5744a9f9a6e0
walid.mogharbel <walid.mogharbel@vanrise.tfs.local>
2015-08-27 13:18:05 +0000

    bpdefintion
    git-tfs-id: [http://developmentvlan:8585/tfs/vanrise.collection]$/;C26193

--- BusinessProcess/Code/Vanrise.BusinessProcess.Web/BusinessProcess/Views/BPDefinitionManagementController.js
+++ BusinessProcess/Code/Vanrise.BusinessProcess.Web/BusinessProcess/Views/BPDefinitionManagementController.js
@@ -94,7 +94,7 @@ function BPDefinitionManagementController($scope, BusinessProcessService, Busine
         interval = $interval(function callAtInterval() {
             getRecentInstancesData();
             getScheduledTasksData();
-        }, 10000);
+        }, 1000);
     }
 
     function stopGetData() {
