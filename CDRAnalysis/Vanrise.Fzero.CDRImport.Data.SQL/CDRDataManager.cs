51f8159e1d257fd98d1e2a05766d7649402749b8
walid.mogharbel <walid.mogharbel@vanrise.tfs.local>
2015-08-31 10:36:28 +0000

    git-tfs-id: [http://developmentvlan:8585/tfs/vanrise.collection]$/;C26249

--- CDRAnalysis/Vanrise.Fzero.CDRImport.Data.SQL/CDRDataManager.cs
+++ CDRAnalysis/Vanrise.Fzero.CDRImport.Data.SQL/CDRDataManager.cs
@@ -24,28 +24,28 @@ namespace Vanrise.Fzero.CDRImport.Data.SQL
             foreach (CDR cdr in cdrs)
             {
                 stream.WriteRecord("0,{0},{1},{2},{3},{4},{5},{6},{7},{8},{9},{10},{11},{12},{13},{14},{15},{16},{17},{18},{19},{20},{21}",
-                              new[]  {   cdr.MSISDN.ToString()
-                                  , cdr.IMSI.ToString()
-                                  , cdr.ConnectDateTime.ToString()
-                                  , cdr.Destination.ToString()
-                                  , cdr.DurationInSeconds.ToString()
+                                 cdr.MSISDN
+                                  , cdr.IMSI
+                                  , cdr.ConnectDateTime
+                                  , cdr.Destination
+                                  , cdr.DurationInSeconds
                                   , ""
-                                  , cdr.CallClass.ToString()
-                                  , cdr.IsOnNet.ToString()
-                                  , cdr.CallType.ToString()
-                                  , cdr.SubType.ToString()
-                                  , cdr.IMEI.ToString()
+                                  , cdr.CallClass
+                                  , cdr.IsOnNet
+                                  , (int) cdr.CallType
+                                  , cdr.SubType
+                                  , cdr.IMEI
                                   , ""
-                                  , cdr.CellId.ToString()
+                                  , cdr.CellId
                                   , ""
                                   , ""
                                   , ""
-                                  , cdr.CellLatitude.ToString()
-                                  , cdr.CellLongitude.ToString()
-                                  , cdr.InTrunk.ToString()
-                                  , cdr.OutTrunk.ToString()
+                                  , cdr.CellLatitude
+                                  , cdr.CellLongitude
+                                  , cdr.InTrunk
+                                  , cdr.OutTrunk
                                   , ""
-                                  , ""}
+                                  , ""
                 );
             }
 
