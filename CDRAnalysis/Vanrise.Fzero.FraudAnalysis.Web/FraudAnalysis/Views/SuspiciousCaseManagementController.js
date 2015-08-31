e17fa53b386a5354dbc979e8527e6639ef237c0f
mohamad.elhariri <mohamad.elhariri@vanrise.tfs.local>
2015-08-31 13:31:35 +0000

    Created the new Suspicious Cases page
    git-tfs-id: [http://developmentvlan:8585/tfs/vanrise.collection]$/;C26258

---
+++ CDRAnalysis/Vanrise.Fzero.FraudAnalysis.Web/FraudAnalysis/Views/SuspiciousCaseManagementController.js
@@ -0,0 +1,13 @@
+﻿SuspiciousCaseManagementController.$inject = ["$scope"];
+
+function SuspiciousCaseManagementController($scope) {
+
+    defineScope();
+    load();
+
+    function defineScope() { }
+
+    function load() { }
+}
+
+appControllers.controller("FraudAnalysis_SuspiciousCaseManagementController", SuspiciousCaseManagementController);
