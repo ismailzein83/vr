867a71cd6d63c2311c78b1b4e5d6bd9a7c4c8e49
walid.mogharbel <walid.mogharbel@vanrise.tfs.local>
2015-08-31 07:42:25 +0000

    git-tfs-id: [http://developmentvlan:8585/tfs/vanrise.collection]$/;C26242

---
+++ CDRAnalysis/Vanrise.Fzero.DevRuntime/Tasks/HaririTask.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Vanrise.BusinessProcess;
+using Vanrise.Queueing;
+using Vanrise.Runtime;
+
+namespace Vanrise.Fzero.DevRuntime.Tasks
+{
+    class HaririTask : ITask
+    {
+        public void Execute()
+        {
+            BusinessProcessService bpService = new BusinessProcessService() { Interval = new TimeSpan(0, 0, 2) };
+            QueueActivationService queueActivationService = new QueueActivationService() { Interval = new TimeSpan(0, 0, 2) };
+            SchedulerService schedulerService = new SchedulerService() { Interval = new TimeSpan(0, 0, 5) };
+
+
+
+            var runtimeServices = new List<RuntimeService>();
+
+            runtimeServices.Add(queueActivationService);
+
+            runtimeServices.Add(bpService);
+
+            runtimeServices.Add(schedulerService);
+
+            RuntimeHost host = new RuntimeHost(runtimeServices);
+            host.Start();
+
+            //PersistentQueueFactory.Default.CreateQueueIfNotExists<Vanrise.Fzero.CDRImport.Entities.ImportedCDRBatch>(0, "TestQueue");
+
+        }
+    }
+}
