5cdef40a7884ea35835d63e2e181c14fc00c02bd
mustafa.hawi <mustafa.hawi@vanrise.tfs.local>
2015-09-03 10:45:18 +0000

    Fix duration mapper
    git-tfs-id: [http://developmentvlan:8585/tfs/vanrise.collection]$/;C26367

--- TOneV2/Code/TOneV2/TOne.Analytics.Web/Analytics/Constants/ReleaseCodeMeasureEnum.js
+++ TOneV2/Code/TOneV2/TOne.Analytics.Web/Analytics/Constants/ReleaseCodeMeasureEnum.js
@@ -4,7 +4,7 @@
     FailedAttempts: { value: 2, propertyName: "FailedAttempts", description: "Failed Attempts", type: "Number", fieldName: "Data.FailedAttempts" },
     Attempts: { value: 3, propertyName: "Attempts", description: "Attempts", type: "Number", fieldName: "Data.Attempts" },
     Percentage: { value: 4, propertyName: "Percentage", description: "%", type: "Number", fieldName: "Data.Percentage" },
-    DurationInMinutes: { value: 5, propertyName: "DurationInMinutes", description: "(Minutes)", type: "Number", fieldName: "Data.DurationInMinutes" },
+    DurationInMinutes: { value: 5, propertyName: "DurationInMinutes", description: "(Minutes)", type: "Number", fieldName: "Data.DurationsInMinutes" },
     FirstCall: { value: 6, propertyName: "FirstCall", description: "First Call", type: "Datetime", fieldName: "Data.FirstAttempt" },
     LastCall: { value: 7, propertyName: "LastCall", description: "Last Call", type: "Datetime", fieldName: "Data.LastAttempt" }
 });
