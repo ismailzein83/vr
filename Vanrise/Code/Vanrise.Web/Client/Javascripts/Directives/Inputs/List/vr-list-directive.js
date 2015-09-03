e5607379526cbd636e7340cd2d29da8cd13c346a
Ali Ballouk <VANRISE\ali.ballouk>
2015-09-03 07:45:23 +0000

    add vr-list
    git-tfs-id: [http://developmentvlan:8585/tfs/vanrise.collection]$/;C26349

---
+++ Vanrise/Code/Vanrise.Web/Client/Javascripts/Directives/Inputs/List/vr-list-directive.js
@@ -0,0 +1,51 @@
+﻿'use strict';
+
+
+app.directive('vrList', [ 'MultiTranscludeService', function ( MultiTranscludeService) {
+
+    var directiveDefinitionObject = {
+        restrict: 'E',
+        transclude: true,
+        scope: {
+            datasource: '='
+        },
+        controller: function ($scope, $element, $attrs) {
+            var ctrl = this;
+            ctrl.itemsSortable = { animation: 150 };
+
+            this.getdatasource = function () {
+                return ctrl.datasource;
+            };
+           
+        },
+        controllerAs: 'ctrl',
+        bindToController: true,
+        //compile: function (tElement, tAttrs, transclude) {
+        //    var rpt = document.createAttribute('ng-repeat');
+        //    rpt.nodeValue = tAttrs.element;
+        //    tElement[0].children[0].attributes.setNamedItem(rpt);
+        //    return function (scope, element, attr) {
+        //        var rhs = attr.element.split(' in ')[1];
+        //        scope.items = $parse(rhs)(scope);
+        //        console.log(scope.items);
+        //    }        
+        //},
+        link: function (scope, elem, attr, ctrl, transcludeFn) {
+            MultiTranscludeService.transclude(elem, transcludeFn);
+        },
+        template: function (element, attrs) {
+            var template = '';
+            template =  '<div transclude-id="header"></div>'
+                      + '<div transclude-id="rowdiv"></div><div transclude-id="rowdiv"></div>';
+            return template;
+        }
+
+
+    };
+
+    return directiveDefinitionObject;
+
+
+
+}]);
+
---
+++ Vanrise/Code/Vanrise.Web/Client/Javascripts/Directives/Inputs/List/vr-list-item-directive.js
@@ -0,0 +1,36 @@
+﻿'use strict';
+
+
+app.directive('vrListItem', [function () {
+
+    var directiveDefinitionObject = {
+        restrict: 'E',
+        scope: {
+            
+        },
+        require: '^vrList',
+        controller: function ($scope, $element, $attrs) {
+            var ctrl = this;
+        },
+        controllerAs: 'ctrl',
+        bindToController: true,
+        compile: function (element, attrs) {
+            element.html('<li class="list-group-item" >' + element.html() + '</li>');
+
+
+            return {
+                pre: function ($scope, iElem, iAttrs, choicesCtrl) {
+                    //var ctrl = $scope.ctrl;
+
+                   
+                }
+            }
+        }
+    };
+
+    return directiveDefinitionObject;
+
+
+
+}]);
+
