865bd53090b5c81d369bfa2544ce3b2b1d1aea48
mohamad.elhariri <mohamad.elhariri@vanrise.tfs.local>
2015-09-04 12:21:03 +0000

    git-tfs-id: [http://developmentvlan:8585/tfs/vanrise.collection]$/;C26427

---
+++ CDRAnalysis/Vanrise.Fzero.FraudAnalysis.Data/IAccountCaseDataManager.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Vanrise.Fzero.FraudAnalysis.Entities;
+
+namespace Vanrise.Fzero.FraudAnalysis.Data
+{
+    public interface IAccountCaseDataManager : IDataManager
+    {
+        AccountCase1 GetLastAccountCaseByAccountNumber(string accountNumber);
+        bool InsertAccountCase(AccountCase1 accountCase, out int insertedID);
+        bool UpdateAccountCase(AccountCase1 accountCase);
+    }
+}
