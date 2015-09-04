da2a739dca12eeaf9a1d845199335cd98d8044f2
mustafa.hawi <mustafa.hawi@vanrise.tfs.local>
2015-09-04 13:59:28 +0000

    refactoring vr label
    git-tfs-id: [http://developmentvlan:8585/tfs/vanrise.collection]$/;C26442

--- Vanrise/Code/Vanrise.Web/Client/Javascripts/Directives/Inputs/Label/vr-label-directive.js
+++ Vanrise/Code/Vanrise.Web/Client/Javascripts/Directives/Inputs/Label/vr-label-directive.js
@@ -1,29 +1,31 @@
-﻿'use strict';
+﻿(function (app) {
 
-app.directive('vrLabel', [function () {
+    "use strict";
 
-    var directiveDefinitionObject = {
-        restrict: 'E',
-        scope: {
-        },
-        controller: function ($scope, $element) {
-            
-        },
-        controllerAs: 'ctrl',
-        bindToController: true,
-        compile: function (tElement, tAttrs) {
-            var isStandalone = tAttrs.standalone;
-            var isValue = tAttrs.isvalue != undefined;
-            var newElement = '<label class="control-label vr-control-label" style="' + (isStandalone == "true" ? 'padding-top:6px;' : '') + (isValue ? 'font-weight:bold;' : '') + '" >'
-                + tElement.context.innerHTML + '</label>';
-            tElement.html(newElement);
-        }//,
-        //template: function (element, attrs) {
-        //    return '<label class="control-label" >{{ctrl.text}}</label>';
-        //}
+    app.directive('vrLabel', function () {
 
-    };
+        var directiveDefinitionObject = {
+            restrict: 'E',
+            scope: {
+            },
+            controller: function () {
 
-    return directiveDefinitionObject;
+            },
+            controllerAs: 'ctrl',
+            bindToController: true,
+            compile: function (tElement, tAttrs) {
+                var isStandalone = tAttrs.standalone;
+                var isValue = tAttrs.isvalue != undefined;
+                var newElement = '<label class="control-label vr-control-label" style="' + (isStandalone === "true" ? 'padding-top:6px;' : '') + (isValue ? 'font-weight:bold;' : '') + '" >'
+                    + tElement.context.innerHTML + '</label>';
+                tElement.html(newElement);
+            }
+        };
 
-}]);
+        return directiveDefinitionObject;
+
+    });
+
+})(app);
+
+
