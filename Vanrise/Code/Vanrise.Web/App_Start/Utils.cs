808619ef518c641c66f153bad4e0f570038d57e6
Mortada Issa <VANRISE\mortada.issa>
2015-09-04 13:44:38 +0000

    Fixing response code
    git-tfs-id: [http://developmentvlan:8585/tfs/vanrise.collection]$/;C26437

--- Vanrise/Code/Vanrise.Web/App_Start/Utils.cs
+++ Vanrise/Code/Vanrise.Web/App_Start/Utils.cs
@@ -11,7 +11,7 @@ namespace Vanrise.Web.App_Start
     {
         public static HttpResponseMessage CreateResponseMessage(HttpStatusCode statusCode, string message)
         {
-            HttpResponseMessage msg = new HttpResponseMessage(System.Net.HttpStatusCode.Unauthorized);
+            HttpResponseMessage msg = new HttpResponseMessage(statusCode);
             msg.Content = new System.Net.Http.StringContent(message);
 
             return msg;
