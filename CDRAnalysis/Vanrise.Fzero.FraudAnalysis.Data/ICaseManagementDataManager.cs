7f1db51b517cbcea7626badc05e7dc9f2f56c98f
walid.mogharbel <walid.mogharbel@vanrise.tfs.local>
2015-09-09 07:50:13 +0000

    git-tfs-id: [http://developmentvlan:8585/tfs/vanrise.collection]$/;C26541

--- CDRAnalysis/Vanrise.Fzero.FraudAnalysis.Data/ICaseManagementDataManager.cs
+++ CDRAnalysis/Vanrise.Fzero.FraudAnalysis.Data/ICaseManagementDataManager.cs
@@ -8,25 +8,25 @@ namespace Vanrise.Fzero.FraudAnalysis.Data
 {
     public interface ICaseManagementDataManager : IDataManager 
     {
-        bool SaveAccountCase(AccountCase accountCaseObject);
-        BigResult<AccountCase> GetFilteredAccountCases(Vanrise.Entities.DataRetrievalInput<AccountCaseResultQuery> input);
-
         bool UpdateAccountCase(string accountNumber, CaseStatus caseStatus, DateTime? validTill);
-        AccountCase GetLastAccountCaseByAccountNumber(string accountNumber);
-        bool InsertAccountCase(out int insertedID, string accountNumber, int userID, CaseStatus caseStatus, DateTime? validTill);
-        bool UpdateAccountCaseStatus(int caseID, CaseStatus statusID, DateTime? validTill);
-        bool InsertAccountCaseHistory(int caseID, int userID, CaseStatus caseStatus);
-        bool InsertOrUpdateAccountStatus(string accountNumber, CaseStatus caseStatus);
-        bool LinkDetailToCase(string accountNumber, int caseID, CaseStatus caseStatus);
 
+        AccountCase GetLastAccountCaseByAccountNumber(string accountNumber);
+
+        bool InsertAccountCase(out int insertedID, string accountNumber, int userID, CaseStatus caseStatus, DateTime? validTill);
+
+        bool UpdateAccountCaseStatus(int caseID, CaseStatus statusID, DateTime? validTill);
+
+        bool InsertAccountCaseHistory(int caseID, int userID, CaseStatus caseStatus);
+
+        bool InsertOrUpdateAccountStatus(string accountNumber, CaseStatus caseStatus);
+
+        bool LinkDetailToCase(string accountNumber, int caseID, CaseStatus caseStatus);
 
         BigResult<AccountSuspicionSummary> GetFilteredAccountSuspicionSummaries(Vanrise.Entities.DataRetrievalInput<AccountSuspicionSummaryQuery> input);
 
         AccountSuspicionSummary GetAccountSuspicionSummaryByAccountNumber(string accountNumber, DateTime from, DateTime to);
 
         BigResult<AccountSuspicionDetail> GetFilteredAccountSuspicionDetails(Vanrise.Entities.DataRetrievalInput<AccountSuspicionDetailQuery> input);
-
-        void ApplySuspiciousNumbersToDB(object preparedSuspiciousNumbers);
 
     }
 }
