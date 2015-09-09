7535de4066b2c019ee39abd6f93e903c5f332f82
mohamad.elhariri <mohamad.elhariri@vanrise.tfs.local>
2015-09-09 08:37:08 +0000

    git-tfs-id: [http://developmentvlan:8585/tfs/vanrise.collection]$/;C26553

---
+++ CDRAnalysis/Vanrise.Fzero.FraudAnalysis.Entities/AccountCaseQuery.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Vanrise.Fzero.FraudAnalysis.Entities
+{
+    public class AccountCaseQuery
+    {
+        public string AccountNumber { get; set; }
+
+        public DateTime From { get; set; }
+
+        public DateTime To { get; set; }
+    }
+}
