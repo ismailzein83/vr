2b4570f5b23f0b558e64cec612e9a76aaa0cb556
sara.yahfoufi <sara.yahfoufi@vanrise.tfs.local>
2015-09-10 10:39:40 +0000

    Add CustomerInvoice page and CustomerInvoiceController
    git-tfs-id: [http://developmentvlan:8585/tfs/vanrise.collection]$/;C26626

---
+++ TOneV2/Code/TOneV2/TOne.Billing.Web/Controllers/CustomerInvoiceController.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace TOne.Billing.Web.Controllers
+{
+    public class CustomerInvoiceController
+    {
+    }
+}
