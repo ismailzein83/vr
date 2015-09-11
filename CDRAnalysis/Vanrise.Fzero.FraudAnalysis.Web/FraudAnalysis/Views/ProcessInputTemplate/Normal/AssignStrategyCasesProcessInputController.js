59cf4cccc0caad871e5784393175d91d57e7bb11
walid.mogharbel <walid.mogharbel@vanrise.tfs.local>
2015-09-11 15:41:31 +0000

    git-tfs-id: [http://developmentvlan:8585/tfs/vanrise.collection]$/;C26708

---
+++ CDRAnalysis/Vanrise.Fzero.FraudAnalysis.Web/FraudAnalysis/Views/ProcessInputTemplate/Normal/AssignStrategyCasesProcessInputController.js
@@ -0,0 +1,34 @@
+﻿var AssignStrategyCasesProcessInputController = function ($scope, $http, $routeParams, notify, VRModalService, VRNotificationService, VRNavigationService) {
+    var pageLoaded = false;
+
+    defineScope();
+
+    function defineScope() {
+
+        $scope.createProcessInputObjects = [];
+
+        $scope.createProcessInput.getData = function () {
+
+            $scope.createProcessInputObjects.length = 0;
+
+            $scope.createProcessInputObjects.push({
+                InputArguments: {
+                    $type: "Vanrise.Fzero.FraudAnalysis.BP.Arguments.AssignStrategyCasesProcessInput, Vanrise.Fzero.FraudAnalysis.BP.Arguments"
+                }
+            });
+
+            return $scope.createProcessInputObjects;
+
+        };
+
+
+    }
+
+
+}
+
+AssignStrategyCasesProcessInputController.$inject = ['$scope', '$http', '$routeParams', 'notify', 'VRModalService', 'VRNotificationService', 'VRNavigationService'];
+appControllers.controller('FraudAnalysis_AssignStrategyCasesProcessInputController', AssignStrategyCasesProcessInputController)
+
+
+
---
+++ CDRAnalysis/Vanrise.Fzero.FraudAnalysis.Web/FraudAnalysis/Views/ProcessInputTemplate/Scheduled/AssignStrategyCasesProcessInput_ScheduledController.js
@@ -0,0 +1,36 @@
+﻿var AssignStrategyExecutionCasesProcess_Scheduled = function ($scope, $http, $routeParams, notify, VRModalService, VRNotificationService, VRNavigationService, UtilsService) {
+    var pageLoaded = false;
+    defineScope();
+    load();
+
+    function defineScope() {
+
+        $scope.processInputArguments = [];
+
+
+
+        $scope.schedulerTaskAction.rawExpressions.getData = function () {
+            return { "ScheduleTime": "ScheduleTime" };
+        };
+
+
+
+        $scope.schedulerTaskAction.processInputArguments.getData = function () {
+            return {
+                $type: "Vanrise.Fzero.FraudAnalysis.BP.Arguments.AssignStrategyExecutionCasesProcess, Vanrise.Fzero.FraudAnalysis.BP.Arguments"
+            };
+        };
+
+    };
+
+    function load() {
+
+    }
+
+}
+
+AssignStrategyExecutionCasesProcess_Scheduled.$inject = ['$scope', '$http', '$routeParams', 'notify', 'VRModalService', 'VRNotificationService', 'VRNavigationService', 'UtilsService'];
+appControllers.controller('FraudAnalysis_AssignStrategyExecutionCasesProcess_Scheduled', AssignStrategyExecutionCasesProcess_Scheduled)
+
+
+
