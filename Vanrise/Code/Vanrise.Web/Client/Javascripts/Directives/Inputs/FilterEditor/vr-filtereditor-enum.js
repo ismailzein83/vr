c86b1898f0d1a929f5032c07db6dd4d16ba596ac
mustafa.hawi <mustafa.hawi@vanrise.tfs.local>
2015-09-16 13:14:52 +0000

    Change type double to number
    git-tfs-id: [http://developmentvlan:8585/tfs/vanrise.collection]$/;C26896

--- Vanrise/Code/Vanrise.Web/Client/Javascripts/Directives/Inputs/FilterEditor/vr-filtereditor-enum.js
+++ Vanrise/Code/Vanrise.Web/Client/Javascripts/Directives/Inputs/FilterEditor/vr-filtereditor-enum.js
@@ -13,7 +13,7 @@
     var typeEnum = {
         String: { value: 0, description: "string", input: inputEnum.Text },
         //Integer: { value: 1, description: "integer", input: inputEnum.Text },
-        Double: { value: 2, description: "double", input: inputEnum.Number },
+        Number: { value: 2, description: "number", input: inputEnum.Number },
         //Date: { value: 3, description: "date", input: inputEnum.Datetime },
         //Time: { value: 4, description: "time", input: inputEnum.Datetime },
         Datetime: { value: 5, description: "datetime", input: inputEnum.Datetime },
@@ -27,18 +27,18 @@
     };
     //
     app.constant('FilterEditorOperatorEnum', {
-        Equal: { value: 0, description: "equal", types: [typeEnum.Boolean, typeEnum.Double, typeEnum.Datetime, typeEnum.String] },
-        NotEqual: { value: 1, description: "not equal", types: [typeEnum.Double, typeEnum.Datetime, typeEnum.String] },
+        Equal: { value: 0, description: "equal", types: [typeEnum.Boolean, typeEnum.Number, typeEnum.Datetime, typeEnum.String] },
+        NotEqual: { value: 1, description: "not equal", types: [typeEnum.Number, typeEnum.Datetime, typeEnum.String] },
         In: { value: 2, description: "in", types: [typeEnum.Array] },
         NotIn: { value: 3, description: "not in", types: [typeEnum.Array] },
-        IsNull: { value: 4, description: "is null", types: [typeEnum.Array, typeEnum.Double, typeEnum.Datetime, typeEnum.String], InputType: inputTypeEnum.NoInput },
-        IsNotNull: { value: 5, description: "is not null", types: [typeEnum.Array, typeEnum.Double, typeEnum.Datetime, typeEnum.String], InputType: inputTypeEnum.NoInput },
-        Less: { value: 6, description: "less", types: [typeEnum.Double, typeEnum.Datetime] },
-        LessOrEqual: { value: 7, description: "less or equal", types: [typeEnum.Double, typeEnum.Datetime] },
-        Greater: { value: 8, description: "greater", types: [typeEnum.Double, typeEnum.Datetime] },
-        GreaterOrEqual: { value: 9, description: "greater or equal", types: [typeEnum.Double, typeEnum.Datetime] },
-        Between: { value: 10, description: "between", types: [typeEnum.Double, typeEnum.Datetime], InputType: inputTypeEnum.MultipleInput },
-        NotBetween: { value: 11, description: "not between", types: [typeEnum.Double, typeEnum.Datetime], InputType: inputTypeEnum.MultipleInput }
+        IsNull: { value: 4, description: "is null", types: [typeEnum.Array, typeEnum.Number, typeEnum.Datetime, typeEnum.String], InputType: inputTypeEnum.NoInput },
+        IsNotNull: { value: 5, description: "is not null", types: [typeEnum.Array, typeEnum.Number, typeEnum.Datetime, typeEnum.String], InputType: inputTypeEnum.NoInput },
+        Less: { value: 6, description: "less", types: [typeEnum.Number, typeEnum.Datetime] },
+        LessOrEqual: { value: 7, description: "less or equal", types: [typeEnum.Number, typeEnum.Datetime] },
+        Greater: { value: 8, description: "greater", types: [typeEnum.Number, typeEnum.Datetime] },
+        GreaterOrEqual: { value: 9, description: "greater or equal", types: [typeEnum.Number, typeEnum.Datetime] },
+        Between: { value: 10, description: "between", types: [typeEnum.Number, typeEnum.Datetime], InputType: inputTypeEnum.MultipleInput },
+        NotBetween: { value: 11, description: "not between", types: [typeEnum.Number, typeEnum.Datetime], InputType: inputTypeEnum.MultipleInput }
 
     });
 
