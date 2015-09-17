fa1c5dc222716c24448fa018fcc770b59db8c6f1
Ali Ballouk <VANRISE\ali.ballouk>
2015-09-17 12:28:07 +0000

    quick fix
    git-tfs-id: [http://developmentvlan:8585/tfs/vanrise.collection]$/;C26927

--- Vanrise/Code/Vanrise.Web/Client/Javascripts/Directives/Inputs/DatePicker/vr-datetimepicker-directive.js
+++ Vanrise/Code/Vanrise.Web/Client/Javascripts/Directives/Inputs/DatePicker/vr-datetimepicker-directive.js
@@ -209,8 +209,8 @@ app.directive('vrDatetimepicker', ['ValidationMessagesEnum', 'BaseDirService', f
 
             $scope.ctrl.onBlurDirective = function (e) {
                 var dateTab = (ctrl.value).toString().split("T")[0].split("-");
-                var year = parseInt(dateTab[0].split(" ")[3])
-                if (year < 1970) {
+                var year = parseInt(dateTab[0].split(" ")[3]);
+                if (year < 1970 || isNaN(year)) {
                     ctrl.value = new Date();
                 }
                 if ($attrs.onblurdatetime != undefined) {
