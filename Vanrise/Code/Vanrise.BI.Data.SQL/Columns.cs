6f0c60230a46d0f2c98cffee4b362f1f53d674ab
Ismail Zein <VANRISE\ismail.zein>
2015-09-18 07:52:49 +0000

    set BI date dimensions configurable
    git-tfs-id: [http://developmentvlan:8585/tfs/vanrise.collection]$/;C26955

--- Vanrise/Code/Vanrise.BI.Data.SQL/Columns.cs
+++ Vanrise/Code/Vanrise.BI.Data.SQL/Columns.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Configuration;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -7,19 +8,78 @@ using System.Threading.Tasks;
 namespace Vanrise.BI.Data.SQL
 {
 
-    internal class DateTimeColumns
+    internal static class DateTimeColumns
     {
-        public const string DATE = "[Date].[Date]";
+        static DateTimeColumns()
+        {
+            s_DATE = GetDimensionName("BIDimension_DATE", "[Date].[Date]");
+            s_YEAR = GetDimensionName("BIDimension_YEAR", "[Date].[Year]");
+            s_MONTHOFYEAR = GetDimensionName("BIDimension_MONTHOFYEAR", "[Date].[Month Of Year]");
+            s_WEEKOFMONTH = GetDimensionName("BIDimension_WEEKOFMONTH", "[Date].[Week Of Month]");
+            s_DAYOFMONTH = GetDimensionName("BIDimension_DAYOFMONTH", "[Date].[Day Of Month]");
+            s_HOUR = GetDimensionName("BIDimension_HOUR", "[Time].[Hour]");
+        }
 
-        public const string YEAR = "[Date].[Year]";
+        static string GetDimensionName(string configKey, string defaultValue)
+        {
+            return ConfigurationManager.AppSettings[configKey] ?? defaultValue;
+        }
 
-        public const string MONTHOFYEAR = "[Date].[Month Of Year]";
+        static string s_DATE;
+        static string s_YEAR;
+        static string s_MONTHOFYEAR;
+        static string s_WEEKOFMONTH;
+        static string s_DAYOFMONTH;
+        static string s_HOUR;
+        
 
-        public const string WEEKOFMONTH = "[Date].[Week Of Month]";
+        public static string DATE
+        {
+            get
+            {
+                return s_DATE;
+            }
+        }
 
-        public const string DAYOFMONTH = "[Date].[Day Of Month]";
+        public static string YEAR
+        {
+            get
+            {
+                return s_YEAR;
+            }
+        }
 
-        public const string HOUR = "[Time].[Hour]";
+        public static string MONTHOFYEAR
+        {
+            get
+            {
+                return s_MONTHOFYEAR;
+            }
+        }
+
+        public static string WEEKOFMONTH
+        {
+            get
+            {
+                return s_WEEKOFMONTH;
+            }
+        }
+
+        public static string DAYOFMONTH
+        {
+            get
+            {
+                return s_DAYOFMONTH;
+            }
+        }
+
+        public static string HOUR
+        {
+            get
+            {
+                return s_HOUR;
+            }
+        }
     }
 
 
