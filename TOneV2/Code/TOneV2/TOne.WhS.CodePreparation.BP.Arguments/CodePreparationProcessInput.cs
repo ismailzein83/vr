cbd501fcfb8cea2a55a48df131d73054e296c5a4
Ismail Zein <VANRISE\ismail.zein>
2015-09-22 06:18:07 +0000

    added Code Preparation workflow projects
    git-tfs-id: [http://developmentvlan:8585/tfs/vanrise.collection]$/;C27036

---
+++ TOneV2/Code/TOneV2/TOne.WhS.CodePreparation.BP.Arguments/CodePreparationProcessInput.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Vanrise.BusinessProcess.Entities;
+
+namespace TOne.WhS.CodePreparation.BP.Arguments
+{
+    public class CodePreparationProcessInput : BaseProcessInputArgument
+    {
+        public override string GetTitle()
+        {
+            return String.Format("CodePreparationProcessInput");
+        }
+    }
+}
