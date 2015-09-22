a33c0ce67403ef43defd0c80f1530d24a8f6a9c1
walid.mogharbel <walid.mogharbel@vanrise.tfs.local>
2015-09-22 08:31:15 +0000

    git-tfs-id: [http://developmentvlan:8585/tfs/vanrise.collection]$/;C27043

---
+++ CDRAnalysis/Vanrise.Fzero.CDRImport.BP.Activities/ApplyNumberProfilesToDB.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Activities;
+using Vanrise.BusinessProcess;
+using Vanrise.Fzero.CDRImport.Data;
+using Vanrise.Queueing;
+
+namespace Vanrise.Fzero.CDRImport.BP.Activities
+{
+    public class ApplyNumberProfilesToDBInput
+    {
+        public BaseQueue<Object> InputQueue { get; set; }
+    }
+
+    public sealed class ApplyNumberProfilesToDB : DependentAsyncActivity<ApplyNumberProfilesToDBInput>
+    {
+        //[RequiredArgument]
+        //public InArgument<BaseQueue<Object>> InputQueue { get; set; }
+
+        //protected override void DoWork(ApplyNumberProfilesToDBInput inputArgument, AsyncActivityStatus previousActivityStatus, AsyncActivityHandle handle)
+        //{
+        //    INumberProfileDataManager dataManager = FraudDataManagerFactory.GetDataManager<INumberProfileDataManager>();
+
+        //    DoWhilePreviousRunning(previousActivityStatus, handle, () =>
+        //    {
+        //        bool hasItems = false;
+        //        do
+        //        {
+        //            hasItems = inputArgument.InputQueue.TryDequeue(
+        //                (preparedNumberProfiles) =>
+        //                {
+        //                    dataManager.ApplyNumberProfilesToDB(preparedNumberProfiles);
+        //                });
+        //        } while (!ShouldStop(handle) && hasItems);
+        //    }
+        //        );
+        //}
+
+        //protected override ApplyNumberProfilesToDBInput GetInputArgument2(AsyncCodeActivityContext context)
+        //{
+        //    return new ApplyNumberProfilesToDBInput()
+        //    {
+        //        InputQueue = this.InputQueue.Get(context)
+        //    };
+        //}
+        protected override void DoWork(ApplyNumberProfilesToDBInput inputArgument, AsyncActivityStatus previousActivityStatus, AsyncActivityHandle handle)
+        {
+            throw new NotImplementedException();
+        }
+
+        protected override ApplyNumberProfilesToDBInput GetInputArgument2(AsyncCodeActivityContext context)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ CDRAnalysis/Vanrise.Fzero.CDRImport.BP.Activities/LoadCDRs.cs
@@ -0,0 +1,103 @@
+﻿using System;
+using System.Activities;
+using System.Collections.Generic;
+using System.Configuration;
+using Vanrise.BusinessProcess;
+using Vanrise.Common;
+using Vanrise.Fzero.CDRImport.Entities;
+using Vanrise.Fzero.CDRImport.Data;
+using Vanrise.Queueing;
+
+namespace Vanrise.Fzero.CDRImport.BP.Activities
+{
+    #region Arguments Classes
+
+    public class LoadCDRsInput
+    {
+        //public BaseQueue<CDRBatch> OutputQueue { get; set; }
+
+        //public DateTime FromDate { get; set; }
+
+
+        //public DateTime ToDate { get; set; }
+
+    }
+
+    #endregion
+
+    public sealed class LoadCDRs : BaseAsyncActivity<LoadCDRsInput>
+    {
+        //#region Arguments
+
+        //[RequiredArgument]
+        //public InOutArgument<BaseQueue<CDRBatch>> OutputQueue { get; set; }
+
+        //[RequiredArgument]
+        //public InArgument<DateTime> FromDate { get; set; }
+
+
+        //[RequiredArgument]
+        //public InArgument<DateTime> ToDate { get; set; }
+
+
+        //#endregion
+
+        protected override void OnBeforeExecute(AsyncCodeActivityContext context, AsyncActivityHandle handle)
+        {
+            //if (this.OutputQueue.Get(context) == null)
+            //    this.OutputQueue.Set(context, new MemoryQueue<CDRBatch>());
+
+            base.OnBeforeExecute(context, handle);
+        }
+
+        //static string configuredDirectory = ConfigurationManager.AppSettings["LoadCDRsDirectory"];
+
+        protected override void DoWork(LoadCDRsInput inputArgument, AsyncActivityHandle handle)
+        {
+            //List<CDR> cdrs = new List<CDR>();
+            //int totalCount = 0;
+            //INormalCDRDataManager dataManager = FraudDataManagerFactory.GetDataManager<INormalCDRDataManager>();
+            //dataManager.LoadCDR(inputArgument.FromDate, inputArgument.ToDate, 0, (cdr) =>
+            //    {
+            //        cdrs.Add(cdr);
+            //        if (cdrs.Count >= 100000)
+            //        {
+            //            totalCount += cdrs.Count;
+            //            inputArgument.OutputQueue.Enqueue(BuildCDRBatch(cdrs));
+            //            handle.SharedInstanceData.WriteTrackingMessage(LogEntryType.Verbose, "{0} CDRs loaded", totalCount);
+            //            cdrs = new List<CDR>();
+            //        }
+            //    });
+            //if (cdrs.Count > 0)
+            //{
+            //    inputArgument.OutputQueue.Enqueue(BuildCDRBatch(cdrs));
+            //}
+        }
+
+        //private CDRBatch BuildCDRBatch(List<CDR> cdrs)
+        //{
+        //    var cdrsBytes = Vanrise.Common.Compressor.Compress(Vanrise.Common.ProtoBufSerializer.Serialize(cdrs));
+        //    string filePath = !String.IsNullOrEmpty(configuredDirectory) ? System.IO.Path.Combine(configuredDirectory, Guid.NewGuid().ToString()) : System.IO.Path.GetTempFileName();
+        //    System.IO.File.WriteAllBytes(filePath, cdrsBytes);
+        //    return new CDRBatch
+        //    {
+        //        CDRBatchFilePath = filePath
+        //    };
+        //}
+
+        //protected override LoadCDRsInput GetInputArgument(AsyncCodeActivityContext context)
+        //{
+        //    return new LoadCDRsInput
+        //    {
+        //        FromDate = this.FromDate.Get(context),
+        //        ToDate = this.ToDate.Get(context),
+        //        OutputQueue = this.OutputQueue.Get(context)
+        //    };
+        //}
+
+        protected override LoadCDRsInput GetInputArgument(AsyncCodeActivityContext context)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
