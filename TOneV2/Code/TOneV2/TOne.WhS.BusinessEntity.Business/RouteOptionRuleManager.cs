bfe6f0b7f559a577b8149e6f0adfcd501c5d720f
Ismail Zein <VANRISE\ismail.zein>
2015-09-23 13:02:44 +0000

    added RouteOptionRuleManager
    git-tfs-id: [http://developmentvlan:8585/tfs/vanrise.collection]$/;C27119

---
+++ TOneV2/Code/TOneV2/TOne.WhS.BusinessEntity.Business/RouteOptionRuleManager.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TOne.WhS.BusinessEntity.Entities;
+
+namespace TOne.WhS.BusinessEntity.Business
+{
+    public class RouteOptionRuleManager
+    {
+        public StructuredBlockRouteOptionRules StructureRouteOptionRules(List<BlockRouteOptionRule> rules)
+        {
+            foreach(var optionRule in rules)
+            {
+                //optionRule.Settings.
+            }
+            return null; 
+        }
+    }
+}
