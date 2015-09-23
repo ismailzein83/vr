4e3a457bd08f5f87f8d9df20ef515fbe7662ae27
Samer Haidar <VANRISE\samer.haidar>
2015-09-23 07:13:31 +0000

    Creating Classes needed
    git-tfs-id: [http://developmentvlan:8585/tfs/vanrise.collection]$/;C27093

---
+++ TOneV2/Code/TOneV2/TOne.WhS.BusinessEntity.Data.SQL/SupplierCodeDataManager.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TOne.Data.SQL;
+
+namespace TOne.WhS.BusinessEntity.Data.SQL
+{
+    public class SupplierCodeDataManager : BaseTOneDataManager, ISupplierCodeDataManager
+    {
+    }
+}
