25b9255ba1600b0e4a740a3c76b392dec8e65c32
Mortada Issa <VANRISE\mortada.issa>
2015-09-23 07:25:17 +0000

    Checkin to work
    git-tfs-id: [http://developmentvlan:8585/tfs/vanrise.collection]$/;C27094

---
+++ TOneV2/Code/TOneV2/TOne.WhS.BusinessEntity.Web/WhS_BusinessEntity/Views/Templates/SelectiveSaleZonesTemplateController.js
@@ -0,0 +1,61 @@
+﻿SelectiveSaleZonesTemplateController.$inject = ['$scope', 'WhS_BE_SaleZoneAPIService', 'UtilsService', 'VRNotificationService'];
+
+function SelectiveSaleZonesTemplateController($scope, WhS_BE_SaleZoneAPIService, UtilsService, VRNotificationService) {
+
+    defineScope();
+    load();
+
+    function defineScope() {
+
+        $scope.searchZones = function (filter) {
+            return WhS_BE_SaleZoneAPIService.GetSaleZonesInfo($scope.saleZoneGroups.saleZonePackageId, filter);
+        }
+
+        $scope.selectedSaleZones = [];
+
+        $scope.saleZoneGroups.getData = function () {
+
+            return {
+                $type: "TOne.WhS.BusinessEntity.Entities.SelectiveSaleZonesSettings, TOne.WhS.BusinessEntity.Entities",
+                ZoneIds: UtilsService.getPropValuesFromArray($scope.selectedSaleZones, "SaleZoneId")
+            };
+        };
+
+        $scope.saleZoneGroups.loadTemplateData = function () {
+            loadForm();
+        }
+
+        $scope.saleZoneGroups.resetSaleZoneSelection = function () {
+            $scope.selectedSaleZones = [];
+        }
+    }
+
+    var isFormLoaded;
+    function loadForm() {
+
+        if ($scope.saleZoneGroups.data == undefined || isFormLoaded)
+            return;
+
+        var data = $scope.saleZoneGroups.data;
+        if (data != null) {
+
+            if ($scope.saleZoneGroups.data.ZoneIds != undefined) {
+                var input = { PackageId: $scope.saleZoneGroups.saleZonePackageId, SaleZoneIds: $scope.saleZoneGroups.data.ZoneIds };
+                WhS_BE_SaleZoneAPIService.GetSaleZonesInfoByIds(input).then(function (response) {
+                    angular.forEach(response, function (item) {
+                        $scope.selectedSaleZones.push(item);
+                    });
+                }).catch(function (error) {
+                    VRNotificationService.notifyExceptionWithClose(error, $scope);
+                });
+            }
+        }
+
+        isFormLoaded = true;
+    }
+
+    function load() {
+        loadForm();
+    }
+}
+appControllers.controller('WhS_BE_SelectiveSaleZonesTemplateController', SelectiveSaleZonesTemplateController);
