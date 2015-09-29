e14d187e8efe114fe3cb9a8f7c6849719d035804
Ismail Zein <VANRISE\ismail.zein>
2015-09-29 15:27:19 +0000

    added ExtensionMethods
    git-tfs-id: [http://developmentvlan:8585/tfs/vanrise.collection]$/;C27183

---
+++ Vanrise/Code/Vanrise.Common/ExtensionMethods.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Vanrise.Common
+{
+    public static class ExtensionMethods
+    {
+        public static Q GetOrCreateItem<T, Q>(this Dictionary<T, Q> dictionary, T itemKey)
+        {
+            Q value;
+            if(!dictionary.TryGetValue(itemKey, out value))
+            {
+                value = Activator.CreateInstance<Q>();
+                dictionary.Add(itemKey, value);
+            }
+            return value;
+        }
+    }
+}
