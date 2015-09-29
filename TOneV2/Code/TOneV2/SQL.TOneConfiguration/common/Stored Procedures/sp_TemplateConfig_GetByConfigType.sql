035535ce4a1a5c9c7cf86cc5b13e444d3d21cc04
Wissam Ajamy <VANRISE\wissam.ajamy>
2015-09-29 12:43:24 +0000

    update from TOneConfiguration on .185
    git-tfs-id: [http://developmentvlan:8585/tfs/vanrise.collection]$/;C27174

---
+++ TOneV2/Code/TOneV2/SQL.TOneConfiguration/common/Stored Procedures/sp_TemplateConfig_GetByConfigType.sql
@@ -0,0 +1,22 @@
+﻿-- =============================================
+-- Author:		<Author,,Name>
+-- Create date: <Create Date,,>
+-- Description:	<Description,,>
+-- =============================================
+CREATE PROCEDURE [common].[sp_TemplateConfig_GetByConfigType] 
+@ConfigType nvarchar(255)
+AS
+BEGIN
+	-- SET NOCOUNT ON added to prevent extra result sets from
+	-- interfering with SELECT statements.
+	SET NOCOUNT ON;
+
+    -- Insert statements for procedure here
+	SELECT [ID]
+      ,[Name]
+      ,[ConfigType]
+      ,[TemplateURL]
+      ,[BehaviorFQTN]
+      from common.TemplateConfig
+      where ConfigType = @ConfigType
+END
---
+++ TOneV2/Code/TOneV2/SQL.TOneConfiguration/common/Tables/TemplateConfig.sql
@@ -0,0 +1,9 @@
+﻿CREATE TABLE [common].[TemplateConfig] (
+    [ID]           INT             IDENTITY (1, 1) NOT NULL,
+    [Name]         NVARCHAR (255)  NOT NULL,
+    [ConfigType]   NVARCHAR (255)  NOT NULL,
+    [TemplateURL]  NVARCHAR (1000) NULL,
+    [BehaviorFQTN] NVARCHAR (1000) NULL,
+    CONSTRAINT [PK_TemplateConfig] PRIMARY KEY CLUSTERED ([ID] ASC)
+);
+
