cdb5aba1cc59002f6fd3565120687d9d7b560ba7
walid.mogharbel <walid.mogharbel@vanrise.tfs.local>
2015-09-30 10:12:03 +0000

    git-tfs-id: [http://developmentvlan:8585/tfs/vanrise.collection]$/;C27200

---
+++ CDRAnalysis/PSTN.BusinessEntity.Web/PSTN_BusinessEntity/Views/ProcessInputTemplate/Normal/FindRelatedNumbersProcessInputController.js
@@ -0,0 +1,39 @@
+﻿"use strict";
+
+FindRelatedNumbersProcessInputController.$inject = ['$scope', '$http', '$routeParams', 'notify', 'VRModalService', 'VRNotificationService', 'VRNavigationService'];
+
+function FindRelatedNumbersProcessInputController($scope, $http, $routeParams, notify, VRModalService, VRNotificationService, VRNavigationService) {
+
+    defineScope();
+
+    function defineScope() {
+
+        var yesterday = new Date();
+        yesterday.setDate(yesterday.getDate() - 1);
+
+        $scope.fromDate = yesterday;
+        $scope.toDate = new Date();
+
+        $scope.createProcessInputObjects = [];
+
+        $scope.createProcessInput.getData = function () {
+
+            $scope.createProcessInputObjects.length = 0;
+
+            $scope.createProcessInputObjects.push({
+                InputArguments: {
+                    $type: "Vanrise.Fzero.FraudAnalysis.BP.Arguments.FindRelatedNumbersProcessInput, Vanrise.Fzero.FraudAnalysis.BP.Arguments",
+                    FromDate: new Date($scope.fromDate),
+                    ToDate: new Date($scope.toDate)
+                }
+            });
+
+            return $scope.createProcessInputObjects;
+        };
+    }
+}
+
+appControllers.controller('FraudAnalysis_FindRelatedNumbersProcessInputController', FindRelatedNumbersProcessInputController)
+
+
+
---
+++ CDRAnalysis/PSTN.BusinessEntity.Web/PSTN_BusinessEntity/Views/ProcessInputTemplate/Scheduled/FindRelatedNumbersProcessInput_ScheduledController.js
@@ -0,0 +1,36 @@
+﻿"use strict";
+
+FindRelatedNumbersProcessInput_Scheduled.$inject = ['$scope', '$http', '$routeParams', 'notify', 'VRModalService', 'VRNotificationService', 'VRNavigationService', 'UtilsService'];
+
+FindRelatedNumbersProcessInput_Scheduled($scope, $http, $routeParams, notify, VRModalService, VRNotificationService, VRNavigationService, UtilsService) {
+    defineScope();
+
+    function defineScope() {
+
+        $scope.processInputArguments = [];
+
+
+
+        $scope.schedulerTaskAction.rawExpressions.getData = function () {
+            return { "ScheduleTime": "ScheduleTime" };
+        };
+
+
+
+        $scope.schedulerTaskAction.processInputArguments.getData = function () {
+            return {
+                $type: "Vanrise.Fzero.FraudAnalysis.BP.Arguments.FindRelatedNumbersProcess, Vanrise.Fzero.FraudAnalysis.BP.Arguments"
+            };
+        };
+
+    };
+
+   
+
+}
+
+
+appControllers.controller('FraudAnalysis_FindRelatedNumbersProcessInput_Scheduled', FindRelatedNumbersProcessInput_Scheduled)
+
+
+
