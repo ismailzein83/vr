29caa0a48413a77cb346d9d67747f0d41bbfaf6c
Ali Ballouk <VANRISE\ali.ballouk>
2015-10-01 11:30:02 +0000

    differents fixes
    git-tfs-id: [http://developmentvlan:8585/tfs/vanrise.collection]$/;C27246

--- Vanrise/Code/Vanrise.Web/Client/Javascripts/Directives/Modal/vr-modalbody-directive.js
+++ Vanrise/Code/Vanrise.Web/Client/Javascripts/Directives/Modal/vr-modalbody-directive.js
@@ -8,7 +8,7 @@ app.directive('vrModalbody', [function () {
         compile: function (tElement, tAttrs) {
             var maxHeightPart = "";
             if (tAttrs.maxheight != undefined)
-                maxHeightPart = '\'max-height\': ' + tAttrs.maxheight + ', \'overflow\': \'auto\', \'padding\': \'5px 15px\'';
+                maxHeightPart = '\'max-height\': ' + tAttrs.maxheight + ', \'overflow\': \'auto\', \'padding\': \'15px\'';
             var newElement = '<div class="modal-body" ng-style="{ ' + maxHeightPart + ' }" >'
                                 +  tElement.context.innerHTML                             
                             + '</div>';
