bfd3c7972dd63fc7bd7e88c38c4806aae7bf3588
Mortada Issa <VANRISE\mortada.issa>
2015-10-01 08:24:03 +0000

    Changing references
    git-tfs-id: [http://developmentvlan:8585/tfs/vanrise.collection]$/;C27236

---
+++ TOneV2/Code/TOneV2/TOne.WhS.BusinessEntity.Web/WhS_BusinessEntity/Directives/vr-whs-be-selectivesalezones-directive.js
@@ -0,0 +1,62 @@
+﻿'use strict';
+app.directive('vrWhsBeSelectivesalezones', ['WhS_BE_SaleZoneAPIService', function (WhS_BE_SaleZoneAPIService) {
+
+    var directiveDefinitionObject = {
+        restrict: 'E',
+        scope: {
+            salezonepackageid: "=",
+            label: "@",
+            selectedvalues: "="
+        },
+        controller: function ($scope, $element, $attrs) {
+            var ctrl = this;
+            $scope.selectedSaleZones = selectedvalues;
+
+            var beSaleZones = new beSaleZones(ctrl, WhS_BE_SaleZoneAPIService);
+            beSaleZones.initializeController();
+
+            $scope.onselectionvalueschanged = function () {
+                ctrl.selectedvalues = $scope.selectedSaleZones;
+            }
+
+            $scope.searchZones = function (filter) {
+                return WhS_BE_SaleZoneAPIService.GetSaleZonesInfo(ctrl.salezonepackageid, filter);
+            }
+        },
+        controllerAs: 'ctrl',
+        bindToController: true,
+        compile: function (element, attrs) {
+            return {
+                pre: function ($scope, iElem, iAttrs, ctrl) {
+                   
+                }
+            }
+        },
+        template: function (element, attrs) {
+            return getBeCarrierGroupTemplate(attrs);
+        }
+
+    };
+
+    function getBeSelectiveSaleZonesTemplate(attrs) {
+
+        if (attrs.salezonepackageid != undefined)
+            return '<div style="display:inline-block;width: calc(100% - 18px);">'
+                       + '<vr-label >' + label + '</vr-label>'
+                   + ' <vr-select ismultipleselection datasource="searchZones" selectedvalues="selectedSaleZones" onselectionchanged="onselectionvalueschanged" datatextfield="Name" datavaluefield="SaleZoneId"'
+                   + 'entityname="Sale Zone"></vr-select></div>';
+        else
+            return '<span>Nothing Selected</span>'
+    }
+
+    function beSaleZones(ctrl, WhS_BE_SaleZoneAPIService) {
+        
+        function initializeController() {
+
+        }
+
+        this.initializeController = initializeController;
+
+    }
+    return directiveDefinitionObject;
+}]);
