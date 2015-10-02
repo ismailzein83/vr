51dc7b57ce217f9fccbdb917074afc0f9c07453c
mohamad.elhariri <mohamad.elhariri@vanrise.tfs.local>
2015-10-02 09:53:19 +0000

    Edited the sp_DataSource_GetAll procedure
    git-tfs-id: [http://developmentvlan:8585/tfs/vanrise.collection]$/;C27282

--- TOneV2/Code/TOneV2/SQL.TOneTransaction/integration/Stored Procedures/sp_DataSource_GetAll.sql
+++ TOneV2/Code/TOneV2/SQL.TOneTransaction/integration/Stored Procedures/sp_DataSource_GetAll.sql
@@ -4,21 +4,13 @@
 -- Description:	<Description,,>
 -- =============================================
 CREATE PROCEDURE [integration].[sp_DataSource_GetAll] 
-	
+
 AS
 BEGIN
-	-- SET NOCOUNT ON added to prevent extra result sets from
-	-- interfering with SELECT statements.
 	SET NOCOUNT ON;
 
-    -- Insert statements for procedure here
-	SELECT DS.[ID]
-      ,DS.[Name]
-      ,DS.[AdapterID]
-      ,DS.[AdapterState]
-      ,AD.[Info]
-      ,DS.[TaskId]
-      ,DS.[Settings]
-       from integration.DataSource as DS
-       Join AdapterType as AD on DS.AdapterID = AD.ID
+	SELECT DS.[ID], DS.[Name]
+    FROM integration.DataSource AS DS
+    
+    SET NOCOUNT OFF;
 END
