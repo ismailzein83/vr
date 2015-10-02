46b588f6d6f0b51b32f004553d8b2c5102ad40e7
mohamad.elhariri <mohamad.elhariri@vanrise.tfs.local>
2015-10-02 12:01:42 +0000

    Add datasourcedetail file
    git-tfs-id: [http://developmentvlan:8585/tfs/vanrise.collection]$/;C27296

---
+++ Vanrise/Code/Vanrise.Integration.Entities/DataSourceDetail.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Vanrise.Integration.Entities
+{
+    public class DataSourceDetail
+    {
+        public int DataSourceId { get; set; }
+
+        public string Name { get; set; }
+
+        public int AdapterTypeId { get; set; }
+
+        public string AdapterName { get; set; }
+
+        public AdapterTypeInfo AdapterInfo { get; set; }
+
+        public BaseAdapterState AdapterState { get; set; }
+
+        public int TaskId { get; set; }
+
+        public bool IsEnabled { get; set; }
+
+        public DataSourceSettings Settings { get; set; }
+    }
+}
