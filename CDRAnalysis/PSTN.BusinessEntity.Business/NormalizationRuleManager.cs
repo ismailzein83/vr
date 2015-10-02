f732a7baff94856d3335831f992bf1673f873824
Ismail Zein <VANRISE\ismail.zein>
2015-10-02 12:02:00 +0000

    Normalization
    git-tfs-id: [http://developmentvlan:8585/tfs/vanrise.collection]$/;C27297

--- CDRAnalysis/PSTN.BusinessEntity.Business/NormalizationRuleManager.cs
+++ CDRAnalysis/PSTN.BusinessEntity.Business/NormalizationRuleManager.cs
@@ -9,14 +9,44 @@ namespace PSTN.BusinessEntity.Business
 {
     public class NormalizationRuleManager
     {
+        static Vanrise.Rules.Entities.StructuredRules _rules = (new NormalizationRuleManager()).GetStructuredRules();
         public void Normalize(Vanrise.Fzero.CDRImport.Entities.StagingCDR cdr)
+        {
+
+            CDRToNormalizeInfo cdrInfo = new CDRToNormalizeInfo { PhoneNumber = cdr.CDPN, SwitchId = cdr.SwitchID.Value };
+            NormalizationRule matchRule = GetMostMatchedRule(_rules, cdrInfo);
+            if(matchRule != null)
+            {
+                string phoneNumber = cdr.CDPN;
+                foreach (var actionSettings in matchRule.Settings.Actions)
+                {
+                    var behavior = GetActionBehavior(actionSettings.BehaviorId);
+                    behavior.Execute(actionSettings, ref phoneNumber);
+                }
+                cdr.CDPN = phoneNumber;
+            }
+                
+        }
+
+        NormalizationRuleActionBehavior GetActionBehavior(int behaviorId)
+        {
+            Vanrise.Common.TemplateConfigManager templateConfigManager = new Vanrise.Common.TemplateConfigManager();
+            return templateConfigManager.GetBehavior<NormalizationRuleActionBehavior>(behaviorId);
+        }
+
+        
+        public void Normalize(Vanrise.Fzero.CDRImport.Entities.CDR cdr)
         {
 
         }
 
-        public void Normalize(Vanrise.Fzero.CDRImport.Entities.CDR cdr)
+        public Vanrise.Rules.Entities.StructuredRules GetStructuredRules()
         {
-
+            List<Vanrise.Rules.Entities.BaseRule> rules = new List<Vanrise.Rules.Entities.BaseRule>();//GEt rules from database
+            rules.Add(new NormalizationRule { Criteria = new NormalizationRuleCriteria { SwitchIds= new List<int> { 2, 4 }, PhoneNumberPrefix = "961" } });
+            var ruleSets = GetRuleSets();
+            Vanrise.Rules.Business.RuleManager ruleManager = new Vanrise.Rules.Business.RuleManager();
+            return ruleManager.StructureRules(rules, ruleSets);
         }
 
         IEnumerable<Vanrise.Rules.Entities.BaseRuleSet> GetRuleSets()
