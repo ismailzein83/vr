a976ae778af73e89644cd7896e7047ab69e31167
mohamad.elhariri <mohamad.elhariri@vanrise.tfs.local>
2015-10-05 11:21:25 +0000

    Created the Normalization Rules page
    git-tfs-id: [http://developmentvlan:8585/tfs/vanrise.collection]$/;C27320

---
+++ CDRAnalysis/PSTN.BusinessEntity.Web/PSTN_BusinessEntity/Views/NormalizationRule/NormalizationRuleManagementController.js
@@ -0,0 +1,29 @@
+﻿NormalizationRuleManagementController.$inject = ["$scope"];
+
+function NormalizationRuleManagementController($scope) {
+
+    defineScope();
+    load();
+
+    function defineScope() {
+
+    }
+
+    function load() {
+
+    }
+
+    function loadFilters() {
+        
+    }
+
+    function setFiltersToDefaultValues() {
+
+    }
+
+    function getFilterObject() {
+        
+    }
+}
+
+appControllers.controller("PSTN_BusinessEntity_NormalizationRuleManagementController", NormalizationRuleManagementController);
