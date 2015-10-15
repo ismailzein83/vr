75e45a21fc1997fd1e5e93b63cb1440d921d132a
mohamad.elhariri <mohamad.elhariri@vanrise.tfs.local>
2015-10-15 10:18:18 +0000

    Added a new constant
    git-tfs-id: [http://developmentvlan:8585/tfs/vanrise.collection]$/;C27613

--- CDRAnalysis/PSTN.BusinessEntity.Business/Constants.cs
+++ CDRAnalysis/PSTN.BusinessEntity.Business/Constants.cs
@@ -5,6 +5,6 @@ namespace PSTN.BusinessEntity.Business
     {
         public const string NormalizationRuleActionBehaviorConfigType = "PSTN_BE_NormalizationRuleActionBehavior";
 
-        public const string NormalizationRuleSettingsConfigType = "PSTN_BE_NormalizationRuleSettings";
+        public const string AdjustNumberActionConfigType = "PSTN_BE_AdjustNumberAction";
     }
 }
