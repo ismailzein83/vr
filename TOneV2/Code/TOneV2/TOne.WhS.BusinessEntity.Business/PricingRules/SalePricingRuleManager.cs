cf14ad797501b717a8ac59e89cbbd7d432dd1711
Ismail Zein <VANRISE\ismail.zein>
2015-10-15 08:10:16 +0000

    added SalePricingRuleManager
    git-tfs-id: [http://developmentvlan:8585/tfs/vanrise.collection]$/;C27592

---
+++ TOneV2/Code/TOneV2/TOne.WhS.BusinessEntity.Business/PricingRules/SalePricingRuleManager.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TOne.WhS.BusinessEntity.Entities;
+
+namespace TOne.WhS.BusinessEntity.Business.PricingRules
+{
+    public class SalePricingRuleManager : Vanrise.Rules.RuleManager<SalePricingRule>
+    {
+
+    }
+}
