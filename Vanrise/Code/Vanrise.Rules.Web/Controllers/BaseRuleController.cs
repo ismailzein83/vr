c9f88eff340fcc769c62f97f2dbe0f9f72db2efa
Ismail Zein <VANRISE\ismail.zein>
2015-10-15 07:57:59 +0000

    added BaseRuleController
    git-tfs-id: [http://developmentvlan:8585/tfs/vanrise.collection]$/;C27585

---
+++ Vanrise/Code/Vanrise.Rules.Web/Controllers/BaseRuleController.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Http;
+using Vanrise.Entities;
+
+namespace Vanrise.Rules.Web.Controllers
+{
+    public abstract class BaseRuleController<T,Q> : Vanrise.Web.Base.BaseAPIController where T : BaseRule where Q : RuleManager<T>
+    {
+        [HttpPost]
+        public InsertOperationOutput<T> AddRule(T rule)
+        {
+            Q manager = Activator.CreateInstance<Q>();
+            return manager.AddRule(rule);
+        }
+
+        [HttpPost]
+        public Vanrise.Entities.UpdateOperationOutput<T> UpdateRule(T rule)
+        {
+            Q manager = Activator.CreateInstance<Q>();
+            return manager.UpdateRule(rule);
+        }
+
+        [HttpPost]
+        public Vanrise.Entities.DeleteOperationOutput<T> DeleteRule(int ruleId)
+        {
+            Q manager = Activator.CreateInstance<Q>();
+            return manager.DeleteRule(ruleId);
+        }
+
+        [HttpGet]
+        public T GetRule(int ruleId)
+        {
+            Q manager = Activator.CreateInstance<Q>();
+            return manager.GetRule(ruleId);
+        }
+    }
+}
