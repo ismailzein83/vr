1af814ab4c45c6978ce928c9208adbe73c09ca55
walid.mogharbel <walid.mogharbel@vanrise.tfs.local>
2015-10-19 07:01:52 +0000

    updated brandmanager
    git-tfs-id: [http://developmentvlan:8585/tfs/vanrise.collection]$/;C27704

--- CDRAnalysis/PSTN.BusinessEntity.Data.SQL/BrandDataManager.cs
+++ CDRAnalysis/PSTN.BusinessEntity.Data.SQL/BrandDataManager.cs
@@ -10,27 +10,27 @@ namespace PSTN.BusinessEntity.Data.SQL
 
         public List<SwitchBrand> GetBrands()
         {
-            return GetItemsSP("PSTN_BE.sp_SwitchType_GetAll", BrandMapper);
+            return GetItemsSP("PSTN_BE.sp_SwitchBrand_GetAll", BrandMapper);
         }
 
         public Vanrise.Entities.BigResult<SwitchBrand> GetFilteredBrands(Vanrise.Entities.DataRetrievalInput<SwitchBrandQuery> input)
         {
             return RetrieveData(input, (tempTableName) =>
             {
-                ExecuteNonQuerySP("PSTN_BE.sp_SwitchType_CreateTempByName", tempTableName, input.Query.Name);
+                ExecuteNonQuerySP("PSTN_BE.sp_SwitchBrand_CreateTempByName", tempTableName, input.Query.Name);
             }, (reader) => BrandMapper(reader));
         }
 
         public SwitchBrand GetBrandById(int brandId)
         {
-            return GetItemSP("PSTN_BE.sp_SwitchType_GetByID", BrandMapper, brandId);
+            return GetItemSP("PSTN_BE.sp_SwitchBrand_GetByID", BrandMapper, brandId);
         }
 
         public bool AddBrand(SwitchBrand brandObj, out int insertedId)
         {
             object brandId;
 
-            int recordsAffected = ExecuteNonQuerySP("PSTN_BE.sp_SwitchType_Insert", out brandId, brandObj.Name);
+            int recordsAffected = ExecuteNonQuerySP("PSTN_BE.sp_SwitchBrand_Insert", out brandId, brandObj.Name);
 
             insertedId = (recordsAffected > 0) ? (int)brandId : -1;
             return (recordsAffected > 0);
@@ -38,13 +38,13 @@ namespace PSTN.BusinessEntity.Data.SQL
 
         public bool UpdateBrand(SwitchBrand brandObj)
         {
-            int recordsAffected = ExecuteNonQuerySP("PSTN_BE.sp_SwitchType_Update", brandObj.BrandId, brandObj.Name);
+            int recordsAffected = ExecuteNonQuerySP("PSTN_BE.sp_SwitchBrand_Update", brandObj.BrandId, brandObj.Name);
             return (recordsAffected > 0);
         }
 
         public bool DeleteBrand(int brandId)
         {
-            int recordsEffected = ExecuteNonQuerySP("PSTN_BE.sp_SwitchType_Delete", brandId);
+            int recordsEffected = ExecuteNonQuerySP("PSTN_BE.sp_SwitchBrand_Delete", brandId);
             return (recordsEffected > 0);
         }
 
