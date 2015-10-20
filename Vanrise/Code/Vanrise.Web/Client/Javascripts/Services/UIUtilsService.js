3ca5639e7e26f7f81d8ec5fb2dbf5f96acb46d8c
Ismail Zein <VANRISE\ismail.zein>
2015-10-20 07:50:19 +0000

    added VRUIUtilsService
    git-tfs-id: [http://developmentvlan:8585/tfs/vanrise.collection]$/;C27763

---
+++ Vanrise/Code/Vanrise.Web/Client/Javascripts/Services/UIUtilsService.js
@@ -0,0 +1,26 @@
+﻿(function (appControllers) {
+
+    "use strict";
+    VRUIUtilsService.$inject = ['VRNotificationService'];
+
+    function VRUIUtilsService(VRNotificationService) {
+
+        function loadDirective(scope, directiveAPI, loaderProperty) {
+            var promise = directiveAPI.load();
+            if (promise != undefined) {
+                scope[loaderProperty] = true;
+                promise.catch(function (error) {
+                    VRNotificationService.notifyException(error, scope);
+                }).finally(function () {
+                    scope[loaderProperty] = false;
+                });
+            }
+        }
+
+        return ({
+            loadDirective: loadDirective
+        });
+    }
+
+    appControllers.service('VRUIUtilsService', VRUIUtilsService);
+})(appControllers);
