789ef132842af7cc92fa7775949716a3152d3db2
Ali Ballouk <VANRISE\ali.ballouk>
2015-10-21 10:38:48 +0000

    change height
    git-tfs-id: [http://developmentvlan:8585/tfs/vanrise.collection]$/;C27840

--- Vanrise/Code/Vanrise.Web/Client/Javascripts/Directives/Inputs/DatePicker/vr-datetimepicker-directive.js
+++ Vanrise/Code/Vanrise.Web/Client/Javascripts/Directives/Inputs/DatePicker/vr-datetimepicker-directive.js
@@ -281,7 +281,7 @@ app.directive('vrDatetimepicker', ['ValidationMessagesEnum', 'BaseDirService', f
                              + ' <span ng-show="showtd"  class="input-group-addon vr-small-addon " ng-click="ctrl.toggleTime($event)" > <i class="glyphicon glyphicon-time"></i></span>';
 
             var dateTemplate =
-                 '<div ng-mouseenter="showtd=true" ng-mouseleave="showtd=false"  style="height:29px;" >'
+                 '<div ng-mouseenter="showtd=true" ng-mouseleave="showtd=false"  style="height:26px;" >'
                   + '<div id="mainInput" ng-model="ctrl.value" class="form-control " ng-style="ctrl.getInputeStyle()" style="border-radius: 4px;height: auto;padding: 0px;">'
                         + '<div  class="input-group date datetime-controle"  id="divDatePicker"  style="width:100%;"  >'
                                 + '<input class="form-control vr-date-input"  placeholder="{{ctrl.placelHolder}}" ng-style="ctrl.getInputeStyle()" style="padding:0px 5px;"  ng-keyup="ctrl.updateModelOnKeyUp($event)" ng-blur="ctrl.onBlurDirective($event)" ng-class="showtd==true? \'fix-border-radius\':\'border-radius\'" data-autoclose="1" placeholder="Date" type="text" ctrltype="' + attrs.type + '">'
