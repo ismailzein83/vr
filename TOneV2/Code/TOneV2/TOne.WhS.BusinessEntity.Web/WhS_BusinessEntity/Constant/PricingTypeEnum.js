68fec3e3c8d0755ae3abdec812bd68e9fefcf02c
Samer Haidar <VANRISE\samer.haidar>
2015-10-21 09:05:29 +0000

    released for ismail
    git-tfs-id: [http://developmentvlan:8585/tfs/vanrise.collection]$/;C27832

--- TOneV2/Code/TOneV2/TOne.WhS.BusinessEntity.Web/WhS_BusinessEntity/Constant/PricingTypeEnum.js
+++ TOneV2/Code/TOneV2/TOne.WhS.BusinessEntity.Web/WhS_BusinessEntity/Constant/PricingTypeEnum.js
@@ -1,4 +1,4 @@
 ﻿ app.constant('WhS_Be_PricingTypeEnum', {
         Sale: { value: 0, description: "Sale", directive: "vr-whs-be-salepricingrulecriteria"},
-        Purchase: { value: 1, description: "Purchase", directive: "vr-whs-be-purchasepricingrule" },
+        Purchase: { value: 1, description: "Purchase", directive: "vr-whs-be-purchasepricingrulecriteria" },
     });
