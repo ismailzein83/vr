a97ef2053e647c4050df4a4726cfa680f8d01e98
Wissam Ajamy <VANRISE\wissam.ajamy>
2015-10-27 08:58:50 +0000

    Update structure from TOneConfiguration DB
    git-tfs-id: [http://developmentvlan:8585/tfs/vanrise.collection]$/;C28022

--- TOneV2/Code/TOneV2/SQL.TOneConfiguration/common/Tables/TemplateConfig.sql
+++ TOneV2/Code/TOneV2/SQL.TOneConfiguration/common/Tables/TemplateConfig.sql
@@ -2,11 +2,13 @@
     [ID]           INT             IDENTITY (1, 1) NOT NULL,
     [Name]         NVARCHAR (255)  NOT NULL,
     [ConfigType]   NVARCHAR (255)  NOT NULL,
-    [Editor]       NVARCHAR (1000) NULL,
     [BehaviorFQTN] NVARCHAR (1000) NULL,
+    [Editor]       NVARCHAR (1000) NULL,
     [Settings]     NVARCHAR (MAX)  NULL,
     CONSTRAINT [PK_TemplateConfig] PRIMARY KEY CLUSTERED ([ID] ASC)
 );
+
+
 
 
 
