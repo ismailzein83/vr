3d921bc6465e9c523e9ddac927cadfc23f1ae2cb
walid.mogharbel <walid.mogharbel@vanrise.tfs.local>
2015-10-27 08:10:01 +0000

    updated sql schema for config Database
    git-tfs-id: [http://developmentvlan:8585/tfs/vanrise.collection]$/;C28019

--- TOneV2/Code/TOneV2/SQL.TOneConfiguration/sec/Tables/Group_2.sql
+++ TOneV2/Code/TOneV2/SQL.TOneConfiguration/sec/Tables/Group_2.sql
@@ -2,6 +2,9 @@
     [ID]          INT            IDENTITY (1, 1) NOT NULL,
     [Name]        NVARCHAR (255) NOT NULL,
     [Description] NTEXT          NULL,
+    [timestamp]   ROWVERSION     NULL,
     CONSTRAINT [PK_Role_2] PRIMARY KEY CLUSTERED ([ID] ASC)
 );
+
+
 
