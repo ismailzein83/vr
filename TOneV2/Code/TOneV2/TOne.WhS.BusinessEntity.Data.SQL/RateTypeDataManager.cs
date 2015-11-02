565d553e7770d1f0a429fbbbedb16122d28c9b53
Samer Haidar <VANRISE\samer.haidar>
2015-11-02 13:46:26 +0000

    Creating RateType Classes
    git-tfs-id: [http://developmentvlan:8585/tfs/vanrise.collection]$/;C28222

---
+++ TOneV2/Code/TOneV2/TOne.WhS.BusinessEntity.Data.SQL/RateTypeDataManager.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Vanrise.Data.SQL;
+
+namespace TOne.WhS.BusinessEntity.Data.SQL
+{
+    public class RateTypeDataManager:BaseSQLDataManager,IRateTypeDataManager
+    {
+
+    }
+}
---
+++ TOneV2/Code/TOneV2/TOne.WhS.BusinessEntity.Web/Controllers/RateTypeController.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using Vanrise.Web.Base;
+
+namespace TOne.WhS.BusinessEntity.Web.Controllers
+{
+    public class RateTypeController:BaseAPIController
+    {
+    }
+}
