4d881ad60c80a2f146969a9402f880d3b6cafa1a
Ali Ballouk <VANRISE\ali.ballouk>
2015-11-04 15:30:38 +0000

    Add VRCommon currency selector directive
    git-tfs-id: [http://developmentvlan:8585/tfs/vanrise.collection]$/;C28312

---
+++ Vanrise/Code/Vanrise.Common.Web/Common/Directives/Currency/vr-common-currency-selector.js
@@ -0,0 +1,105 @@
+﻿'use strict';
+app.directive('vrCommonCurrencySelector', ['VRCommon_CurrencyAPIService', 'UtilsService', 'VRUIUtilsService',
+    function (VRCommon_CurrencyAPIService, UtilsService, VRUIUtilsService) {
+
+        var directiveDefinitionObject = {
+            restrict: 'E',
+            scope: {
+                onReady: '=',
+                ismultipleselection: "@",
+                onselectionchanged: '=',
+                selectedvalues: '=',
+                isrequired: "@",
+                onselectitem: "=",
+                ondeselectitem: "="
+            },
+            controller: function ($scope, $element, $attrs) {
+
+                var ctrl = this;
+
+                ctrl.selectedvalues;
+                if ($attrs.ismultipleselection != undefined)
+                    ctrl.selectedvalues = [];
+
+                ctrl.datasource = [];
+
+                var ctor = new currencyCtor(ctrl, $scope, $attrs);
+                ctor.initializeController();
+                
+            },
+            controllerAs: 'ctrl',
+            bindToController: true,
+            compile: function (element, attrs) {
+                return {
+                    pre: function ($scope, iElem, iAttrs, ctrl) {
+
+                    }
+                }
+            },
+            template: function (element, attrs) {
+                return getCurrencyTemplate(attrs);
+            }
+
+        };
+
+
+        function getCurrencyTemplate(attrs) {
+
+            var multipleselection = "";
+            var label = "Currency";
+            if (attrs.ismultipleselection != undefined)
+            {
+                label = "Currencies";
+                multipleselection = "ismultipleselection";
+            }
+
+            var required = "";
+            if (attrs.isrequired != undefined)
+                required = "isrequired";
+
+            return '<div>'
+                + '<vr-select ' + multipleselection + '  datatextfield="Name" datavaluefield="CurrencyId" '
+            + required + ' label="' + label + '" datasource="ctrl.datasource" selectedvalues="ctrl.selectedvalues" onselectionchanged="ctrl.onselectionchanged" entityName="Currency" onselectitem="ctrl.onselectitem" ondeselectitem="ctrl.ondeselectitem"></vr-select>'
+               + '</div>'
+        }
+
+        function currencyCtor(ctrl, $scope, attrs) {
+
+            function initializeController() {
+                defineAPI();
+            }
+
+            function defineAPI() {
+                var api = {};
+
+                api.load = function (payload) {
+
+                    var selectedIds;
+                    if (payload != undefined) {
+                        selectedIds = payload.selectedIds;
+                    }
+
+                    return VRCommon_CurrencyAPIService.GetAllCurrencies().then(function (response) {
+                        angular.forEach(response, function (itm) {
+                            ctrl.datasource.push(itm);
+                        });
+
+                        if (selectedIds != undefined) {
+                            VRUIUtilsService.setSelectedValues(selectedIds, 'CurrencyId', attrs, ctrl);
+                        }
+                    });
+                }
+
+                api.getSelectedIds = function () {
+                    return VRUIUtilsService.getIdSelectedIds('CurrencyId', attrs, ctrl);
+                }
+
+                if (ctrl.onReady != null)
+                    ctrl.onReady(api);
+            }
+            
+            this.initializeController = initializeController;
+        }
+
+        return directiveDefinitionObject;
+    }]);
