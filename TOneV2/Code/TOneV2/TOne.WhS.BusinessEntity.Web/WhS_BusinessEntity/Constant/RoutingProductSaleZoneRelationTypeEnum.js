3b028a96c4ac6f086c25818de02982f21c9663f2
Mortada Issa <VANRISE\mortada.issa>
2015-11-05 11:41:32 +0000

    Release to walid
    git-tfs-id: [http://developmentvlan:8585/tfs/vanrise.collection]$/;C28333

---
+++ TOneV2/Code/TOneV2/TOne.WhS.BusinessEntity.Web/WhS_BusinessEntity/Constant/RoutingProductSaleZoneRelationTypeEnum.js
@@ -0,0 +1,4 @@
+﻿app.constant('WhS_Be_RoutingProductSaleZoneRelationTypeEnum', {
+    AllZones: { value: "0", description: "All" },
+    SpecificZones: { value: "1", description: "Specific" }
+});
---
+++ TOneV2/Code/TOneV2/TOne.WhS.BusinessEntity.Web/WhS_BusinessEntity/Constant/RoutingProductSupplierRelationTypeEnum.js
@@ -0,0 +1,4 @@
+﻿app.constant('WhS_Be_RoutingProductSupplierRelationTypeEnum', {
+    AllSuppliers: { value: "0", description: "All" },
+    SpecificSuppliers: { value: "1", description: "Specific" }
+});
