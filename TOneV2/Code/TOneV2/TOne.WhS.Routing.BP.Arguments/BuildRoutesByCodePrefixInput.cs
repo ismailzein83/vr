a551bfc68b4d60fd400b60c51cb921fb82f45845
Ismail Zein <VANRISE\ismail.zein>
2015-11-10 13:46:47 +0000

    Added BuildRoutesByCodePrefix
    git-tfs-id: [http://developmentvlan:8585/tfs/vanrise.collection]$/;C28443

---
+++ TOneV2/Code/TOneV2/TOne.WhS.Routing.BP.Arguments/BuildRoutesByCodePrefixInput.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TOne.WhS.Routing.BP.Arguments
+{
+    public class BuildRoutesByCodePrefixInput
+    {
+        public string CodePreifx { get; set; }
+
+        public DateTime? EffectiveOn { get; set; }
+
+        public bool IsFuture { get; set; }
+    }
+}
