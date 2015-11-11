64bee6ac78281310bdae052cd0af9acec990f6d8
Samer Haidar <VANRISE\samer.haidar>
2015-11-11 09:39:45 +0000

    adding ShouldSetCacheExpired
    git-tfs-id: [http://developmentvlan:8585/tfs/vanrise.collection]$/;C28460

--- TOneV2/Code/TOneV2/TOne.WhS.BusinessEntity.Business/SaleZone/SaleRateCacheManager.cs
+++ TOneV2/Code/TOneV2/TOne.WhS.BusinessEntity.Business/SaleZone/SaleRateCacheManager.cs
@@ -3,10 +3,18 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using TOne.WhS.BusinessEntity.Data;
 
 namespace TOne.WhS.BusinessEntity.Business
 {
     public class SaleRateCacheManager : Vanrise.Caching.BaseCacheManager
     {
+        ISaleRateDataManager _dataManager = BEDataManagerFactory.GetDataManager<ISaleRateDataManager>();
+        object _updateHandle;
+
+        protected override bool ShouldSetCacheExpired(object parameter)
+        {
+            return _dataManager.AreSaleRatesUpdated(ref _updateHandle);
+        }
     }
 }
