caf9bca901d03ba3b15d10d903d8f5ef1cdb647d
rabih.fashwal:2 <rabih.fashwal:2@vanrise.tfs.local>
2015-11-12 11:16:22 +0000

    CustomerInfo moved to BE
    git-tfs-id: [http://developmentvlan:8585/tfs/vanrise.collection]$/;C28513

--- TOneV2/Code/TOneV2/TOne.WhS.Routing.BP.Activities/LoadActiveCustomerInfos.cs
+++ TOneV2/Code/TOneV2/TOne.WhS.Routing.BP.Activities/LoadActiveCustomerInfos.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Text;
 using System.Activities;
 using TOne.WhS.Routing.Entities;
+using TOne.WhS.BusinessEntity.Entities;
 
 namespace TOne.WhS.Routing.BP.Activities
 {
