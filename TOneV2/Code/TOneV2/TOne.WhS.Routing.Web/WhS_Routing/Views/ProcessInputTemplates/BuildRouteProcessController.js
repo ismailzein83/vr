bf7db7b47b34698945faee49fd681e3d224bedb3
rabih.fashwal:2 <rabih.fashwal:2@vanrise.tfs.local>
2015-11-12 08:04:45 +0000

    Add Routing Process Page
    git-tfs-id: [http://developmentvlan:8585/tfs/vanrise.collection]$/;C28495

---
+++ TOneV2/Code/TOneV2/TOne.WhS.Routing.Web/WhS_Routing/Views/ProcessInputTemplates/BuildRouteProcessController.js
@@ -0,0 +1,30 @@
+﻿"use strict";
+
+BuildRouteProcessController.$inject = ['$scope'];
+
+function BuildRouteProcessController($scope) {
+
+    defineScope();
+    load();
+
+    function defineScope() {
+        $scope.createProcessInput.getData = function () {
+            return {
+                InputArguments: {
+                    $type: "TOne.WhS.Routing.BP.Arguments.RoutingProcessInput, TOne.WhS.Routing.BP.Arguments",
+                    EffectiveTime: $scope.effectiveOn,
+                    RoutingDatabaseType: $scope.isFuture
+                }
+            };
+        };
+    }
+
+    function load() {
+
+    }
+}
+
+appControllers.controller('WhS_Routing_BuildRouteProcessController', BuildRouteProcessController)
+
+
+
