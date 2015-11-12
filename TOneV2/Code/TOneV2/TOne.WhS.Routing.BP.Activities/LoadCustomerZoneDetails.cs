0e2c78294eaa3b27345a580298fdae5eca2b3a36
Mortada Issa <VANRISE\mortada.issa>
2015-11-12 14:04:30 +0000

    Added a new activity to load customer zone details
    git-tfs-id: [http://developmentvlan:8585/tfs/vanrise.collection]$/;C28524

--- TOneV2/Code/TOneV2/TOne.WhS.Routing.BP.Activities/LoadCustomerZoneDetails.cs
+++ TOneV2/Code/TOneV2/TOne.WhS.Routing.BP.Activities/LoadCustomerZoneDetails.cs
@@ -3,21 +3,79 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Activities;
+using TOne.WhS.BusinessEntity.Entities;
+using TOne.WhS.Routing.Entities;
+using Vanrise.BusinessProcess;
 
 namespace TOne.WhS.Routing.BP.Activities
 {
 
-    public sealed class LoadCustomerZoneDetails : CodeActivity
+    public class LoadCustomerZoneDetailsInput
     {
-        // Define an activity input argument of type string
-        public InArgument<string> Text { get; set; }
+        public IEnumerable<SaleCode> SaleCodes { get; set; }
+    }
 
-        // If your activity returns a value, derive from CodeActivity<TResult>
-        // and return the value from the Execute method.
-        protected override void Execute(CodeActivityContext context)
+    public class LoadCustomerZoneDetailsOutput
+    {
+        public CustomerZoneDetailByZone CustomerZoneDetails { get; set; }
+    }
+
+    public sealed class LoadCustomerZoneDetails : BaseAsyncActivity<LoadCustomerZoneDetailsInput, LoadCustomerZoneDetailsOutput>
+    {
+
+        [RequiredArgument]
+        public InArgument<IEnumerable<SaleCode>> SaleCodes { get; set; }
+
+        [RequiredArgument]
+        public OutArgument<CustomerZoneDetailByZone> CustomerZoneDetails { get; set; }
+
+        protected override LoadCustomerZoneDetailsOutput DoWorkWithResult(LoadCustomerZoneDetailsInput inputArgument, AsyncActivityHandle handle)
         {
-            // Obtain the runtime value of the Text input argument
-            string text = context.GetValue(this.Text);
+            CustomerZoneDetailByZone customerZoneDetails = new CustomerZoneDetailByZone();
+            
+            foreach (SaleCode code in inputArgument.SaleCodes)
+            {
+                if (!customerZoneDetails.ContainsKey(code.ZoneId))
+                {
+                    CustomerZoneDetail zoneDetail1 = new CustomerZoneDetail()
+                    {
+                        CustomerId = 1,
+                        SaleZoneId = code.ZoneId,
+                        SellingProductId = 1
+                    };
+
+                    CustomerZoneDetail zoneDetail2 = new CustomerZoneDetail()
+                    {
+                        CustomerId = 2,
+                        SaleZoneId = code.ZoneId,
+                        SellingProductId = 1
+                    };
+
+                    List<CustomerZoneDetail> zoneDetails = new List<CustomerZoneDetail>();
+                    zoneDetails.Add(zoneDetail1);
+                    zoneDetails.Add(zoneDetail2);
+
+                    customerZoneDetails.Add(code.ZoneId, zoneDetails);
+                }
+            }
+
+            return new LoadCustomerZoneDetailsOutput
+            {
+                CustomerZoneDetails = customerZoneDetails
+            };
+        }
+
+        protected override LoadCustomerZoneDetailsInput GetInputArgument(AsyncCodeActivityContext context)
+        {
+            return new LoadCustomerZoneDetailsInput
+            {
+                SaleCodes = this.SaleCodes.Get(context)
+            };
+        }
+
+        protected override void OnWorkComplete(AsyncCodeActivityContext context, LoadCustomerZoneDetailsOutput result)
+        {
+            this.CustomerZoneDetails.Set(context, result.CustomerZoneDetails);
         }
     }
 }
