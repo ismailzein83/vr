261afa58eda2b8bad8083bb1f88be13681eb06e3
walid.mogharbel <walid.mogharbel@vanrise.tfs.local>
2015-11-12 14:54:51 +0000

    same as last
    git-tfs-id: [http://developmentvlan:8585/tfs/vanrise.collection]$/;C28531

--- Vanrise/Code/Vanrise.Common.Web/Common/Views/City/CityEditorController.js
+++ Vanrise/Code/Vanrise.Common.Web/Common/Views/City/CityEditorController.js
@@ -6,9 +6,12 @@
 
     function cityEditorController($scope, VRCommon_CityAPIService, VRNotificationService, VRNavigationService) {
 
-        
         var cityId;
+        var countryId;
         var editMode;
+        var countryDirectiveApi;
+        var disableCountry;
+
         defineScope();
         loadParameters();
         load();
@@ -16,17 +19,26 @@
             var parameters = VRNavigationService.getParameters($scope);
             if (parameters != undefined && parameters != null) {
                 cityId = parameters.CityId;
+                countryId = parameters.CountryId;
+                disableCountry = parameters.disableCountry
             }
             editMode = (cityId != undefined);
+            $scope.disableCountry = ((countryId != undefined) && !editMode) || disableCountry == true;
         }
         function defineScope() {
+
+            $scope.onCountryDirectiveReady = function (api) {
+                countryDirectiveApi = api;
+                load();
+            }
+
             $scope.saveCity = function () {
-                    if (editMode) {
-                        return updateCity();
-                    }
-                    else {
-                        return insertCity();
-                    }
+                if (editMode) {
+                    return updateCity();
+                }
+                else {
+                    return insertCity();
+                }
             };
 
             $scope.close = function () {
@@ -37,15 +49,27 @@
         }
 
         function load() {
-                $scope.isGettingData = true;
-                if (editMode) {
+
+            $scope.isGettingData = true;
+            if (countryDirectiveApi == undefined)
+                return;
+            countryDirectiveApi.load().then(function () {
+                if ($scope.disableCountry && countryId != undefined) {
+                    countryDirectiveApi.setData(countryId);
+                    $scope.isGettingData = false;
+                }
+                else if (editMode) {
                     getCity();
                 }
                 else {
                     $scope.isGettingData = false;
                 }
-          
+            }).catch(function (error) {
+                VRNotificationService.notifyExceptionWithClose(error, $scope);
+                $scope.isGettingData = false;
+            });
         }
+
         function getCity() {
             return VRCommon_CityAPIService.GetCity(cityId).then(function (city) {
                 fillScopeFromCityObj(city);
@@ -60,18 +84,20 @@
             var obj = {
                 CityId: (cityId != null) ? cityId : 0,
                 Name: $scope.name,
+                CountryId: countryDirectiveApi.getDataId()
             };
             return obj;
         }
 
         function fillScopeFromCityObj(city) {
             $scope.name = city.Name;
+            countryDirectiveApi.setData(city.CountryId)
         }
         function insertCity() {
             var cityObject = buildCityObjFromScope();
             return VRCommon_CityAPIService.AddCity(cityObject)
             .then(function (response) {
-                if (VRNotificationService.notifyOnItemAdded("City", response ,"Name")) {
+                if (VRNotificationService.notifyOnItemAdded("City", response, "Name")) {
                     if ($scope.onCityAdded != undefined)
                         $scope.onCityAdded(response.InsertedObject);
                     $scope.modalContext.closeModal();
