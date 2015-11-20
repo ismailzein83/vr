88d8b1caf744b65cf7c53b5ff41c433179b5c92c
rabih.fashwal:2 <rabih.fashwal:2@vanrise.tfs.local>
2015-11-20 15:43:07 +0000

    Add RoutingProductOption DataManager
    git-tfs-id: [http://developmentvlan:8585/tfs/vanrise.collection]$/;C28735

---
+++ TOneV2/Code/TOneV2/TOne.WhS.Routing.Data/IRoutingProductOptionDataManager.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Vanrise.Data;
+
+namespace TOne.WhS.Routing.Data
+{
+    public interface IRoutingProductOptionDataManager : IDataManager, 
+        //IBulkApplyDataManager<RoutingProductOption>, 
+        IRoutingDataManager
+    {
+    }
+}
+ 
