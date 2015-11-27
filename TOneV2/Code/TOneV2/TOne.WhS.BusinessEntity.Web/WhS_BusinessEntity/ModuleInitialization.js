52f435562ce3c10fd74dc00e48e1ce4c339f6c27
walid.mogharbel <walid.mogharbel@vanrise.tfs.local>
2015-11-27 12:46:59 +0000

    updated supplier zone to contain drill down of supplier code
    git-tfs-id: [http://developmentvlan:8585/tfs/vanrise.collection]$/;C28861

--- TOneV2/Code/TOneV2/TOne.WhS.BusinessEntity.Web/WhS_BusinessEntity/ModuleInitialization.js
+++ TOneV2/Code/TOneV2/TOne.WhS.BusinessEntity.Web/WhS_BusinessEntity/ModuleInitialization.js
@@ -1,5 +1,6 @@
-﻿app.run(['WhS_BE_CodeGroupService', 'VRCommon_SaleZoneService', 'WhS_BE_SupplierRateService', function (WhS_BE_CodeGroupService, VRCommon_SaleZoneService, WhS_BE_SupplierRateService) {
+﻿app.run(['WhS_BE_CodeGroupService', 'VRCommon_SaleZoneService', 'WhS_BE_SupplierRateService', 'WhS_BE_SupplierCodeService', function (WhS_BE_CodeGroupService, VRCommon_SaleZoneService, WhS_BE_SupplierRateService, WhS_BE_SupplierCodeService) {
     WhS_BE_CodeGroupService.registerDrillDownToCountry();
     VRCommon_SaleZoneService.registerDrillDownToSellingNumberPlan();
     WhS_BE_SupplierRateService.registerDrillDownToSupplierZone();
+    WhS_BE_SupplierCodeService.registerDrillDownToSupplierZone();
 }]);
---
+++ TOneV2/Code/TOneV2/TOne.WhS.BusinessEntity.Web/WhS_BusinessEntity/Services/SupplierCodeService.js
@@ -0,0 +1,30 @@
+﻿
+app.service('WhS_BE_SupplierCodeService', ['VRModalService', 'VRNotificationService', 'UtilsService', 'WhS_BE_SupplierZoneService',
+    function (VRModalService, VRNotificationService, UtilsService, WhS_BE_SupplierZoneService) {
+        var drillDownDefinitions = [];
+        return ({
+            registerDrillDownToSupplierZone: registerDrillDownToSupplierZone
+        });
+
+        function registerDrillDownToSupplierZone() {
+            var drillDownDefinition = {};
+
+            drillDownDefinition.title = "Supplier Codes";
+            drillDownDefinition.directive = "vr-whs-be-suppliercode-grid";
+
+            drillDownDefinition.loadDirective = function (directiveAPI, supplierZoneItem) {
+                supplierZoneItem.supplierCodeGridAPI = directiveAPI;
+              
+
+                var query = {
+                    ZoneIds: [supplierZoneItem.Entity.SupplierZoneId],
+                };
+               
+                return supplierZoneItem.supplierCodeGridAPI.loadGrid(query);
+            };
+
+            WhS_BE_SupplierZoneService.addDrillDownDefinition(drillDownDefinition);
+        }
+      
+
+    }]);
