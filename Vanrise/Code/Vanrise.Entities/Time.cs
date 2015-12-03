168c489fb95434c2ba5160c03992b924dbbc636d
Samer Haidar <VANRISE\samer.haidar>
2015-12-03 10:28:39 +0000

    adding new functions
    git-tfs-id: [http://developmentvlan:8585/tfs/vanrise.collection]$/;C29004

--- Vanrise/Code/Vanrise.Entities/Time.cs
+++ Vanrise/Code/Vanrise.Entities/Time.cs
@@ -8,6 +8,28 @@ namespace Vanrise.Entities
 {
     public class Time
     {
+        public Time() { }
+        public Time(string time)
+        {
+            string[] timeArray = time.Split(':');
+            for (var i = 0; i < timeArray.Length; i++)
+            {
+                switch (i)
+                {
+                    case 0: this.Hour = Convert.ToInt32(timeArray[i]); break;
+                    case 1: this.Minute = Convert.ToInt32(timeArray[i]); break;
+                    case 2: this.Second = Convert.ToInt32(timeArray[i]); break;
+                    case 3: this.MilliSecond = Convert.ToInt32(timeArray[i]); break;
+                }
+            }
+        }
+        public Time(DateTime time)
+        {
+            this.Hour = time.Hour;
+            this.Minute = time.Minute;
+            this.Second = time.Second;
+            this.MilliSecond = time.Millisecond;
+        }
         public int Hour { get; set; }
 
         public int Minute { get; set; }
@@ -15,5 +37,31 @@ namespace Vanrise.Entities
         public int Second { get; set; }
 
         public int MilliSecond { get; set; }
+
+        public string ToShortTimeString()
+        {
+            return String.Format("{0}:{1}:{2}", this.Hour, this.Minute, this.Second);
+        }
+        public string ToLongTimeString()
+        {
+            return String.Format("{0}:{1}:{2}:{3}", this.Hour, this.Minute, this.Second, this.MilliSecond);
+        }
+        public bool isGreaterThan(DateTime time)
+        {
+            if (this.Hour > time.Hour)
+                return true;
+            else if (this.Hour == time.Hour && this.Minute > time.Minute)
+                return true;
+            else if (this.Hour == time.Hour && this.Minute == time.Minute && this.Second > time.Second)
+                return true;
+            else if (this.Hour == time.Hour && this.Minute == time.Minute && this.Second == time.Second && this.MilliSecond > time.Millisecond)
+                return true;
+            else
+                return false;
+        }
+        public bool isLessThan(DateTime time)
+        {
+            return !isGreaterThan(time);
+        }
     }
 }
