98d039168827a37762acd0eb8ecbc084131ba744
Ismail Zein <VANRISE\ismail.zein>
2015-12-08 07:29:38 +0000

    added SupplierSyncTaskAction
    git-tfs-id: [http://developmentvlan:8585/tfs/vanrise.collection]$/;C29079

---
+++ QualityMeasurement/Code/QM.BusinessEntity.Business/Supplier/SupplierSyncTaskAction.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Vanrise.Runtime.Entities;
+
+namespace QM.BusinessEntity.Business
+{
+    public class SupplierSyncTaskAction : SchedulerTaskAction
+    {
+        public override void Execute(SchedulerTask task, BaseTaskActionArgument taskActionArgument, Dictionary<string, object> evaluatedExpressions)
+        {
+            //throw new NotImplementedException();
+        }
+    }
+}
