1cf55443eebd23b8848d11c95ecdaf0ff350c340
Ismail Zein <VANRISE\ismail.zein>
2015-12-08 09:02:23 +0000

    changed namespace
    git-tfs-id: [http://developmentvlan:8585/tfs/vanrise.collection]$/;C29083

--- QualityMeasurement/Code/QM.BusinessEntity.MainExtensions/SourceSuppliersReaders/SupplierTOneV2SQLReader.cs
+++ QualityMeasurement/Code/QM.BusinessEntity.MainExtensions/SourceSuppliersReaders/SupplierTOneV2SQLReader.cs
@@ -5,7 +5,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace QM.BusinessEntity.MainExtensions.SuppliersReaders
+namespace QM.BusinessEntity.MainExtensions.SourceSuppliersReaders
 {
     public class SupplierTOneV2SQLReader : SourceSupplierReader 
     {
