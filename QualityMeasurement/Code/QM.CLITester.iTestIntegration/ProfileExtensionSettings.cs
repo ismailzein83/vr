0516248c7feaa93df1af37fae1624af57a4b66ce
Ismail Zein <VANRISE\ismail.zein>
2015-12-10 08:39:56 +0000

    added Profile
    git-tfs-id: [http://developmentvlan:8585/tfs/vanrise.collection]$/;C29143

---
+++ QualityMeasurement/Code/QM.CLITester.iTestIntegration/ProfileExtensionSettings.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace QM.CLITester.iTestIntegration
+{
+    public enum ProfileType { Voice = 1, SMS = 2}
+    public class ProfileExtensionSettings : QM.CLITester.Entities.ExtendedProfileSetting
+    {
+        public ProfileType Type { get; set; }
+
+        public string GatewayIP { get; set; }
+
+        public string GatewayPort { get; set; }
+
+        public string SourceNumber { get; set; }
+
+        public int CallTime { get; set; }
+
+        public int RingTime { get; set; }
+
+        public override void Apply(Entities.Profile profile)
+        {
+          //  throw new NotImplementedException();
+        }
+    }
+}
