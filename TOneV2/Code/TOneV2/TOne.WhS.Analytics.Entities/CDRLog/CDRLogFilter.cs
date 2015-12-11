88aac7d12860f7d30186b9836fd0c393f597db3c
Samer Haidar <VANRISE\samer.haidar>
2015-12-11 07:38:10 +0000

    adding file to source control
    git-tfs-id: [http://developmentvlan:8585/tfs/vanrise.collection]$/;C29167

---
+++ TOneV2/Code/TOneV2/TOne.WhS.Analytics.Entities/CDRLog/CDRLogFilter.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace TOne.WhS.Analytics.Entities
+{
+    public class CDRLogFilter
+    {
+        public List<int> SwitchIds { get; set; }
+        public List<int> CustomerIds { get; set; }
+        public List<int> SupplierIds { get; set; }
+        public List<long> SaleZoneIds { get; set; }
+        public List<long> SupplierZoneIds { get; set; } 
+        
+    }
+}
