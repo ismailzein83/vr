6abee0f9c527d21c0d9de7327a16e2b18f012436
rabih.fashwal:2 <rabih.fashwal:2@vanrise.tfs.local>
2015-12-16 07:53:34 +0000

    Deserialize SaleLCR
    git-tfs-id: [http://developmentvlan:8585/tfs/vanrise.collection]$/;C29280

--- TOneV2/Code/TOneV2/TOne.Web.Online/Controllers/SalesController.cs
+++ TOneV2/Code/TOneV2/TOne.Web.Online/Controllers/SalesController.cs
@@ -11,9 +11,10 @@ namespace TOne.Web.Online.Controllers
     public class SalesController : ApiController
     {
         [HttpGet]
-        public bool SavePriceList(string customerId, List<SaleLcrRate> saleLcrRates, string currencyId, bool sendEmail)
+        public bool SavePriceList(string customerId, string saleLcrRatesString, string currencyId, bool sendEmail)
         {
             SaleLCRManager manager = new SaleLCRManager();
+            List<SaleLcrRate> saleLcrRates = Vanrise.Common.Serializer.Deserialize<List<SaleLcrRate>>(saleLcrRatesString);
             return manager.SavePriceList(customerId, saleLcrRates, currencyId, sendEmail);
         }
     }
