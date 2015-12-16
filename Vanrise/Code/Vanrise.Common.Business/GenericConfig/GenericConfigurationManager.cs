fd9e02252ec0df8343f5fb849dde065d406f6937
Ismail Zein <VANRISE\ismail.zein>
2015-12-16 10:52:28 +0000

    added GenericConfiguration
    git-tfs-id: [http://developmentvlan:8585/tfs/vanrise.collection]$/;C29290

---
+++ Vanrise/Code/Vanrise.Common.Business/GenericConfig/GenericConfigurationManager.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Vanrise.Entities;
+
+namespace Vanrise.Common.Business
+{
+    public abstract class GenericConfigurationManager
+    {
+        public void UpdateConfiguration(GenericConfiguration genericConfig)
+        {
+            string ownerKey = genericConfig.OwnerKey;
+            if (ownerKey == null)
+                ownerKey = Guid.Empty.ToString();
+            int typeId = TypeManager.Instance.GetTypeId(this.GetType());
+        }
+
+        public GenericConfiguration GetConfiguration(string ownerKey)
+        {
+            if (ownerKey == null)
+                ownerKey = Guid.Empty.ToString();
+            int typeId = TypeManager.Instance.GetTypeId(this.GetType());
+            return null;
+        }
+    }
+}
