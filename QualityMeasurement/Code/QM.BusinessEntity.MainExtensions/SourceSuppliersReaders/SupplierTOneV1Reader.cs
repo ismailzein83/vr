ee144376fd57f54018d8bb059e8939c485f06669
Ali Ballouk <VANRISE\ali.ballouk>
2015-12-17 10:34:25 +0000

    Synch supplier with TOneV1
    git-tfs-id: [http://developmentvlan:8585/tfs/vanrise.collection]$/;C29313

--- QualityMeasurement/Code/QM.BusinessEntity.MainExtensions/SourceSuppliersReaders/SupplierTOneV1Reader.cs
+++ QualityMeasurement/Code/QM.BusinessEntity.MainExtensions/SourceSuppliersReaders/SupplierTOneV1Reader.cs
@@ -18,18 +18,44 @@ namespace QM.BusinessEntity.MainExtensions.SourceSuppliersReaders
 
         public override IEnumerable<SourceSupplier> GetChangedItems(ref object updatedHandle)
         {
-            List<SourceSupplier> lst = new List<SourceSupplier>();
-            lst.Add(new SourceSupplier
+            DataManager dataManager = new DataManager(this.ConnectionString);
+            return dataManager.GetUpdatedSuppliers(ref updatedHandle);
+        }
+
+        private class DataManager : Vanrise.Data.SQL.BaseSQLDataManager
+        {
+            public DataManager(string connectionString)
+                : base(connectionString, false)
             {
-                Name = "TOne V1",
-                SourceId = "TV11"
-            });
-            lst.Add(new SourceSupplier
+            }
+
+            public List<SourceSupplier> GetUpdatedSuppliers(ref object updateHandle)
             {
-                Name = "TOne V1",
-                SourceId = "TV12"
-            });
-            return lst;
+                return GetItemsText(query_getUpdatedSuppliers, SourceSupplierMapper, null);
+            }
+
+            private SourceSupplier SourceSupplierMapper(System.Data.IDataReader arg)
+            {
+                SourceSupplier sourceSupplier = new SourceSupplier()
+                {
+                    SourceId = arg["ID"].ToString(),
+                    Name = GetCarrierAccountName(arg["ProfileName"] as string, arg["NameSuffix"] as string)
+                };
+                return sourceSupplier;
+            }
+
+            const string query_getUpdatedSuppliers = @"SELECT
+                ca.CarrierAccountID as ID,
+                cp.Name as ProfileName,
+                ca.NameSuffix as NameSuffix
+                FROM CarrierAccount ca
+                INNER JOIN CarrierProfile cp on ca.ProfileID = cp.ProfileID
+                WHERE ca.IsDeleted = 'N' AND ca.ActivationStatus = 2 and (ca.AccountType = 1 or ca.AccountType = 2)";
+
+            internal static string GetCarrierAccountName(string carrierName, string nameSuffix)
+            {
+                return string.Format("{0}{1}", carrierName, string.IsNullOrEmpty(nameSuffix) ? string.Empty : " (" + nameSuffix + ")");
+            }
         }
     }
 }
