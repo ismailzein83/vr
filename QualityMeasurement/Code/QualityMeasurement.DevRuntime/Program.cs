7d5adf6e13ad73ab93237393deb37611401a01b3
Ismail Zein <VANRISE\ismail.zein>
2015-12-17 09:00:29 +0000

    configured RuntimeService
    git-tfs-id: [http://developmentvlan:8585/tfs/vanrise.collection]$/;C29304

--- QualityMeasurement/Code/QualityMeasurement.DevRuntime/Program.cs
+++ QualityMeasurement/Code/QualityMeasurement.DevRuntime/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Configuration;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -12,10 +13,26 @@ namespace QualityMeasurement.DevRuntime
     {
         static void Main(string[] args)
         {
-            MainForm f = new MainForm();
-            f.ShowDialog();
-            Console.ReadKey();
-            return;
+            if (ConfigurationManager.AppSettings["IsRuntimeService"] == "true")
+            {
+                
+                SchedulerService schedulerService = new SchedulerService() { Interval = new TimeSpan(0, 0, 5) };
+
+                var runtimeServices = new List<RuntimeService>();
+
+                runtimeServices.Add(schedulerService);
+
+                RuntimeHost host = new RuntimeHost(runtimeServices);
+                host.Start();
+                Console.ReadKey();
+            }
+            else
+            {
+                MainForm f = new MainForm();
+                f.ShowDialog();
+                Console.ReadKey();
+                return;
+            }
 
             
         }
