add0e4c3df97bba4bf703c7d2090c666d38960eb
Ismail Zein <VANRISE\ismail.zein>
2015-12-18 12:47:05 +0000

    added GenericDataRecord
    git-tfs-id: [http://developmentvlan:8585/tfs/vanrise.collection]$/;C29353

---
+++ Vanrise/Code/Vanrise.Entities/GenericDataRecord/DataRecordType.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Vanrise.Entities.GenericDataRecord
+{
+    public class DataRecordType
+    {
+        public int DataRecordTypeId { get; set; }
+
+        public string Name { get; set; }
+
+        public int ParentId { get; set; }
+
+        public List<DataRecordField> Fields { get; set; }
+    }
+}
