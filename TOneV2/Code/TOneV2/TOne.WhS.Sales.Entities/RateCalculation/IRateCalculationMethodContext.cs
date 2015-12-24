2fc30847a31231057f521fbb8d753ee6ad54aa36
Ismail Zein <VANRISE\ismail.zein>
2015-12-24 08:54:45 +0000

    added RateCalculation Method
    git-tfs-id: [http://developmentvlan:8585/tfs/vanrise.collection]$/;C29447

---
+++ TOneV2/Code/TOneV2/TOne.WhS.Sales.Entities/RateCalculation/IRateCalculationMethodContext.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TOne.WhS.Sales.Entities
+{
+    public interface IRateCalculationMethodContext
+    {
+        Decimal? Cost { get; }
+
+        Decimal? Rate { set; }
+    }
+}
---
+++ TOneV2/Code/TOneV2/TOne.WhS.Sales.Entities/RateCalculation/RateCalculationMethod.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TOne.WhS.Sales.Entities
+{
+    public abstract class RateCalculationMethod
+    {
+        public int ConfigId { get; set; }
+
+        public abstract void CalculateRate(IRateCalculationMethodContext context);
+    }
+}
