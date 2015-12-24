f404d65513f29398669f1d59e81a9e6edf718f0b
Mortada Issa <VANRISE\mortada.issa>
2015-12-24 12:15:23 +0000

    Adding a new table to save validation messages
    git-tfs-id: [http://developmentvlan:8585/tfs/vanrise.collection]$/;C29455

---
+++ TOneV2/Code/TOneV2/SQL.TOneLogging/bp/Tables/ValidationMessage.sql
@@ -0,0 +1,11 @@
+﻿CREATE TABLE [bp].[ValidationMessage] (
+    [ID]                BIGINT         IDENTITY (1, 1) NOT NULL,
+    [ProcessInstanceID] BIGINT         NOT NULL,
+    [ParentProcessID]   BIGINT         NULL,
+    [TargetKey]         VARCHAR (50)   NOT NULL,
+    [TargetType]        VARCHAR (50)   NOT NULL,
+    [Severity]          INT            NOT NULL,
+    [Message]           NVARCHAR (MAX) NULL,
+    CONSTRAINT [PK_ValidationMessage] PRIMARY KEY CLUSTERED ([ID] ASC)
+);
+
