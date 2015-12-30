1c4f79e96b1d266bce4f610eccd8aa3c7e3887ef
Mortada Issa <VANRISE\mortada.issa>
2015-12-30 11:43:28 +0000

    Adding testing data scripts
    git-tfs-id: [http://developmentvlan:8585/tfs/vanrise.collection]$/;C29510

---
+++ TOneV2/Code/TOneV2/SQL.TOneConfiguration/TOneWhs.TestDataScenario.sql
@@ -0,0 +1,142 @@
+﻿/*
+Post-Deployment Script Template							
+--------------------------------------------------------------------------------------
+ This file contains SQL statements that will be appended to the build script.		
+ Use SQLCMD syntax to include a file in the post-deployment script.			
+ Example:      :r .\myfile.sql								
+ Use SQLCMD syntax to reference a variable in the post-deployment script.		
+ Example:      :setvar TableName MyTable							
+               SELECT * FROM [$(TableName)]					
+--------------------------------------------------------------------------------------
+*/
+--common.Country------------------------------------------------------------------------------------
+----------------------------------------------------------------------------------------------------
+set nocount on;
+;with cte_data([ID],[Name],[SourceID])
+as (select * from (values
+--//////////////////////////////////////////////////////////////////////////////////////////////////
+(1,'Lebanon',null),
+(2,'Syria',null),
+(3,'Germany',null),
+(4,'France',null),
+(5,'Iran',null)
+--\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\
+)c([ID],[Name],[SourceID]))
+merge	[common].[Country] as t
+using	cte_data as s
+on		1=1 and t.[ID] = s.[ID]
+when matched then
+	update set
+	[Name] = s.[Name],[SourceID] = s.[SourceID]
+when not matched by target then
+	insert([ID],[Name],[SourceID])
+	values(s.[ID],s.[Name],s.[SourceID])
+when not matched by source then
+	delete;
+	
+--common.Currency-----------------------------------------------------------------------------------
+----------------------------------------------------------------------------------------------------
+set nocount on;
+set identity_insert [common].[Currency] on;
+;with cte_data([ID],[Symbol],[Name])
+as (select * from (values
+--//////////////////////////////////////////////////////////////////////////////////////////////////
+(1,'USD','USD')
+--\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\
+)c([ID],[Symbol],[Name]))
+merge	[common].[Currency] as t
+using	cte_data as s
+on		1=1 and t.[ID] = s.[ID]
+when matched then
+	update set
+	[Symbol] = s.[Symbol],[Name] = s.[Name]
+when not matched by target then
+	insert([ID],[Symbol],[Name])
+	values(s.[ID],s.[Symbol],s.[Name])
+when not matched by source then
+	delete;
+set identity_insert [common].[Currency] off;
+
+--[common].[City]-----------------------------------------------------------------------------------
+----------------------------------------------------------------------------------------------------
+set nocount on;
+set identity_insert [common].[City] on;
+;with cte_data([ID],[Name],[CountryID])
+as (select * from (values
+--//////////////////////////////////////////////////////////////////////////////////////////////////
+(1,'Beirut',1)
+--\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\
+)c([ID],[Name],[CountryID]))
+merge	[common].[City] as t
+using	cte_data as s
+on		1=1 and t.[ID] = s.[ID]
+when matched then
+	update set
+	[Name] = s.[Name],[CountryID] = s.[CountryID]
+when not matched by target then
+	insert([ID],[Name],[CountryID])
+	values(s.[ID],s.[Name],s.[CountryID])
+when not matched by source then
+	delete;
+set identity_insert [common].[City] off;
+
+--common.IDManager----------------------------------------------------------------------------------
+----------------------------------------------------------------------------------------------------
+set nocount on;
+;with cte_data([TypeID],[LastTakenID])
+as (select * from (values
+--//////////////////////////////////////////////////////////////////////////////////////////////////
+(1,0),
+(2,0),
+(3,0),
+(4,0),
+(5,0),
+(6,5),
+(7,0),
+(8,0),
+(9,0),
+(10,0),
+(12,0)
+--\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\
+)c([TypeID],[LastTakenID]))
+merge	[common].[IDManager] as t
+using	cte_data as s
+on		1=1 and t.[TypeID] = s.[TypeID]
+when matched then
+	update set
+	[LastTakenID] = s.[LastTakenID]
+when not matched by target then
+	insert([TypeID],[LastTakenID])
+	values(s.[TypeID],s.[LastTakenID])
+when not matched by source then
+	delete;
+	
+--[rules].[Rule]------------------------------------------------------------------------------------
+----------------------------------------------------------------------------------------------------
+set nocount on;
+set identity_insert [rules].[Rule] on;
+;with cte_data([ID],[TypeID],[RuleDetails],[BED],[EED])
+as (select * from (values
+--//////////////////////////////////////////////////////////////////////////////////////////////////
+(1,10,'{"$type":"TOne.WhS.Routing.Entities.RouteRule, TOne.WhS.Routing.Entities","Criteria":{"$type":"TOne.WhS.Routing.Entities.RouteRuleCriteria, TOne.WhS.Routing.Entities","RoutingProductId":1,"ExcludedCodes":{"$type":"System.Collections.Generic.List`1[[System.String, mscorlib]], mscorlib","$values":[]}},"Settings":{"$type":"TOne.WhS.Routing.Business.RegularRouteRule, TOne.WhS.Routing.Business","ConfigId":15},"RuleId":0,"BeginEffectiveTime":"2015-12-29T16:51:00Z"}','2015-12-29 16:51:00.000',null),
+(2,6,'{"$type":"TOne.WhS.CDRProcessing.Entities.CustomerIdentificationRule, TOne.WhS.CDRProcessing.Entities","Criteria":{"$type":"TOne.WhS.CDRProcessing.Entities.CustomerIdentificationRuleCriteria, TOne.WhS.CDRProcessing.Entities","InTrunks":{"$type":"System.Collections.Generic.List`1[[System.String, mscorlib]], mscorlib","$values":[]},"InCarriers":{"$type":"System.Collections.Generic.List`1[[System.String, mscorlib]], mscorlib","$values":["1-0:9"]},"CDPNPrefixes":{"$type":"System.Collections.Generic.List`1[[System.String, mscorlib]], mscorlib","$values":[]}},"Settings":{"$type":"TOne.WhS.CDRProcessing.Entities.CustomerIdentificationRuleSettings, TOne.WhS.CDRProcessing.Entities","CustomerId":1},"Description":"Protel","RuleId":0,"BeginEffectiveTime":"2014-12-29T00:00:00Z"}','2014-12-29 00:00:00.000',null),
+(3,6,'{"$type":"TOne.WhS.CDRProcessing.Entities.CustomerIdentificationRule, TOne.WhS.CDRProcessing.Entities","Criteria":{"$type":"TOne.WhS.CDRProcessing.Entities.CustomerIdentificationRuleCriteria, TOne.WhS.CDRProcessing.Entities","InTrunks":{"$type":"System.Collections.Generic.List`1[[System.String, mscorlib]], mscorlib","$values":[]},"InCarriers":{"$type":"System.Collections.Generic.List`1[[System.String, mscorlib]], mscorlib","$values":["1-0:10"]},"CDPNPrefixes":{"$type":"System.Collections.Generic.List`1[[System.String, mscorlib]], mscorlib","$values":[]}},"Settings":{"$type":"TOne.WhS.CDRProcessing.Entities.CustomerIdentificationRuleSettings, TOne.WhS.CDRProcessing.Entities","CustomerId":2},"Description":"Spactron","RuleId":0,"BeginEffectiveTime":"2014-12-29T00:00:00Z"}','2014-12-29 00:00:00.000',null),
+(4,7,'{"$type":"TOne.WhS.CDRProcessing.Entities.SupplierIdentificationRule, TOne.WhS.CDRProcessing.Entities","Criteria":{"$type":"TOne.WhS.CDRProcessing.Entities.SupplierIdentificationRuleCriteria, TOne.WhS.CDRProcessing.Entities","OutTrunks":{"$type":"System.Collections.Generic.List`1[[System.String, mscorlib]], mscorlib","$values":[]},"OutCarriers":{"$type":"System.Collections.Generic.List`1[[System.String, mscorlib]], mscorlib","$values":["1-0:11"]},"CDPNPrefixes":{"$type":"System.Collections.Generic.List`1[[System.String, mscorlib]], mscorlib","$values":[]}},"Settings":{"$type":"TOne.WhS.CDRProcessing.Entities.SupplierIdentificationRuleSettings, TOne.WhS.CDRProcessing.Entities","SupplierId":3},"Description":"Sama","RuleId":0,"BeginEffectiveTime":"2014-12-29T00:00:00Z"}','2014-12-29 00:00:00.000',null),
+(5,7,'{"$type":"TOne.WhS.CDRProcessing.Entities.SupplierIdentificationRule, TOne.WhS.CDRProcessing.Entities","Criteria":{"$type":"TOne.WhS.CDRProcessing.Entities.SupplierIdentificationRuleCriteria, TOne.WhS.CDRProcessing.Entities","OutTrunks":{"$type":"System.Collections.Generic.List`1[[System.String, mscorlib]], mscorlib","$values":[]},"OutCarriers":{"$type":"System.Collections.Generic.List`1[[System.String, mscorlib]], mscorlib","$values":["1-0:12"]},"CDPNPrefixes":{"$type":"System.Collections.Generic.List`1[[System.String, mscorlib]], mscorlib","$values":[]}},"Settings":{"$type":"TOne.WhS.CDRProcessing.Entities.SupplierIdentificationRuleSettings, TOne.WhS.CDRProcessing.Entities","SupplierId":4},"Description":"MHD","RuleId":0,"BeginEffectiveTime":"2014-12-29T00:00:00Z"}','2014-12-29 00:00:00.000',null),
+(6,7,'{"$type":"TOne.WhS.CDRProcessing.Entities.SupplierIdentificationRule, TOne.WhS.CDRProcessing.Entities","Criteria":{"$type":"TOne.WhS.CDRProcessing.Entities.SupplierIdentificationRuleCriteria, TOne.WhS.CDRProcessing.Entities","OutTrunks":{"$type":"System.Collections.Generic.List`1[[System.String, mscorlib]], mscorlib","$values":[]},"OutCarriers":{"$type":"System.Collections.Generic.List`1[[System.String, mscorlib]], mscorlib","$values":["1-0:13"]},"CDPNPrefixes":{"$type":"System.Collections.Generic.List`1[[System.String, mscorlib]], mscorlib","$values":[]}},"Settings":{"$type":"TOne.WhS.CDRProcessing.Entities.SupplierIdentificationRuleSettings, TOne.WhS.CDRProcessing.Entities","SupplierId":5},"Description":"NetTalk","RuleId":0,"BeginEffectiveTime":"2014-12-29T00:00:00Z"}','2014-12-29 00:00:00.000',null),
+(7,12,'{"$type":"TOne.WhS.CDRProcessing.Entities.SwitchIdentificationRule, TOne.WhS.CDRProcessing.Entities","Description":"Default Switch Identity","Criteria":{"$type":"TOne.WhS.CDRProcessing.Entities.SwitchIdentificationRuleCriteria, TOne.WhS.CDRProcessing.Entities","DataSources":{"$type":"System.Collections.Generic.List`1[[System.Int32, mscorlib]], mscorlib","$values":[1]}},"Settings":{"$type":"TOne.WhS.CDRProcessing.Entities.SwitchIdentificationRuleSettings, TOne.WhS.CDRProcessing.Entities","SwitchId":1},"RuleId":0,"BeginEffectiveTime":"2014-12-29T00:00:00Z"}','2014-12-29 00:00:00.000',null)
+--\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\
+)c([ID],[TypeID],[RuleDetails],[BED],[EED]))
+merge	[rules].[Rule] as t
+using	cte_data as s
+on		1=1 and t.[ID] = s.[ID]
+when matched then
+	update set
+	[TypeID] = s.[TypeID],[RuleDetails] = s.[RuleDetails],[BED] = s.[BED],[EED] = s.[EED]
+when not matched by target then
+	insert([ID],[TypeID],[RuleDetails],[BED],[EED])
+	values(s.[ID],s.[TypeID],s.[RuleDetails],s.[BED],s.[EED])
+when not matched by source then
+	delete;
+set identity_insert [rules].[Rule] off;
+	
---
+++ TOneV2/Code/TOneV2/SQL.TOneWhS_BE/TOneWhs.TestDataScenario.sql
@@ -0,0 +1,244 @@
+﻿/*
+Post-Deployment Script Template							
+--------------------------------------------------------------------------------------
+ This file contains SQL statements that will be appended to the build script.		
+ Use SQLCMD syntax to include a file in the post-deployment script.			
+ Example:      :r .\myfile.sql								
+ Use SQLCMD syntax to reference a variable in the post-deployment script.		
+ Example:      :setvar TableName MyTable							
+               SELECT * FROM [$(TableName)]					
+--------------------------------------------------------------------------------------
+*/
+--TOneWhS_BE.CarrierAccount-------------------------------------------------------------------------
+----------------------------------------------------------------------------------------------------
+set nocount on;
+set identity_insert [TOneWhS_BE].[CarrierAccount] on;
+;with cte_data([ID],[Name],[CarrierProfileID],[AccountType],[SupplierSettings],[CustomerSettings],[CarrierAccountSettings],[SellingNumberPlanID])
+as (select * from (values
+--//////////////////////////////////////////////////////////////////////////////////////////////////
+(1,'Protel',1,1,'{"$type":"TOne.WhS.BusinessEntity.Entities.CarrierAccountSupplierSettings, TOne.WhS.BusinessEntity.Entities","RoutingStatus":0}','{"$type":"TOne.WhS.BusinessEntity.Entities.CarrierAccountCustomerSettings, TOne.WhS.BusinessEntity.Entities","RoutingStatus":0}','{"$type":"TOne.WhS.BusinessEntity.Entities.CarrierAccountSettings, TOne.WhS.BusinessEntity.Entities","ActivationStatus":0}',1),
+(2,'Spactron',2,3,'{"$type":"TOne.WhS.BusinessEntity.Entities.CarrierAccountSupplierSettings, TOne.WhS.BusinessEntity.Entities","RoutingStatus":0}','{"$type":"TOne.WhS.BusinessEntity.Entities.CarrierAccountCustomerSettings, TOne.WhS.BusinessEntity.Entities","RoutingStatus":0}','{"$type":"TOne.WhS.BusinessEntity.Entities.CarrierAccountSettings, TOne.WhS.BusinessEntity.Entities","ActivationStatus":0}',1),
+(3,'Sama',3,2,'{"$type":"TOne.WhS.BusinessEntity.Entities.CarrierAccountSupplierSettings, TOne.WhS.BusinessEntity.Entities","RoutingStatus":0}','{"$type":"TOne.WhS.BusinessEntity.Entities.CarrierAccountCustomerSettings, TOne.WhS.BusinessEntity.Entities","RoutingStatus":0}','{"$type":"TOne.WhS.BusinessEntity.Entities.CarrierAccountSettings, TOne.WhS.BusinessEntity.Entities","ActivationStatus":0}',null),
+(4,'MHD',4,2,'{"$type":"TOne.WhS.BusinessEntity.Entities.CarrierAccountSupplierSettings, TOne.WhS.BusinessEntity.Entities","RoutingStatus":0}','{"$type":"TOne.WhS.BusinessEntity.Entities.CarrierAccountCustomerSettings, TOne.WhS.BusinessEntity.Entities","RoutingStatus":0}','{"$type":"TOne.WhS.BusinessEntity.Entities.CarrierAccountSettings, TOne.WhS.BusinessEntity.Entities","ActivationStatus":0}',null),
+(5,'Nettalk',5,2,'{"$type":"TOne.WhS.BusinessEntity.Entities.CarrierAccountSupplierSettings, TOne.WhS.BusinessEntity.Entities","RoutingStatus":0}','{"$type":"TOne.WhS.BusinessEntity.Entities.CarrierAccountCustomerSettings, TOne.WhS.BusinessEntity.Entities","RoutingStatus":0}','{"$type":"TOne.WhS.BusinessEntity.Entities.CarrierAccountSettings, TOne.WhS.BusinessEntity.Entities","ActivationStatus":0}',null)
+--\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\
+)c([ID],[Name],[CarrierProfileID],[AccountType],[SupplierSettings],[CustomerSettings],[CarrierAccountSettings],[SellingNumberPlanID]))
+merge	[TOneWhS_BE].[CarrierAccount] as t
+using	cte_data as s
+on		1=1 and t.[ID] = s.[ID]
+when matched then
+	update set
+	[Name] = s.[Name],[CarrierProfileID] = s.[CarrierProfileID],[AccountType] = s.[AccountType],[SupplierSettings] = s.[SupplierSettings],[CustomerSettings] = s.[CustomerSettings],[CarrierAccountSettings] = s.[CarrierAccountSettings],[SellingNumberPlanID] = s.[SellingNumberPlanID]
+when not matched by target then
+	insert([ID],[Name],[CarrierProfileID],[AccountType],[SupplierSettings],[CustomerSettings],[CarrierAccountSettings],[SellingNumberPlanID])
+	values(s.[ID],s.[Name],s.[CarrierProfileID],s.[AccountType],s.[SupplierSettings],s.[CustomerSettings],s.[CarrierAccountSettings],s.[SellingNumberPlanID])
+when not matched by source then
+	delete;
+set identity_insert [TOneWhS_BE].[CarrierAccount] off;
+
+--TOneWhS_BE.CarrierProfile-------------------------------------------------------------------------
+----------------------------------------------------------------------------------------------------
+set nocount on;
+set identity_insert [TOneWhS_BE].[CarrierProfile] on;
+;with cte_data([ID],[Name],[Settings])
+as (select * from (values
+--//////////////////////////////////////////////////////////////////////////////////////////////////
+(1,'Protel Profile','{"$type":"TOne.WhS.BusinessEntity.Entities.CarrierProfileSettings, TOne.WhS.BusinessEntity.Entities","Company":"Protel","CountryId":1,"CityId":1,"CompanyLogo":0,"Contacts":{"$type":"System.Collections.Generic.List`1[[TOne.WhS.BusinessEntity.Entities.CarrierContact, TOne.WhS.BusinessEntity.Entities]], mscorlib","$values":[]}}'),
+(2,'Spactron Profile','{"$type":"TOne.WhS.BusinessEntity.Entities.CarrierProfileSettings, TOne.WhS.BusinessEntity.Entities","Company":"Spactron LTD","CountryId":1,"CityId":1,"CompanyLogo":0,"Contacts":{"$type":"System.Collections.Generic.List`1[[TOne.WhS.BusinessEntity.Entities.CarrierContact, TOne.WhS.BusinessEntity.Entities]], mscorlib","$values":[]}}'),
+(3,'Sama Profile','{"$type":"TOne.WhS.BusinessEntity.Entities.CarrierProfileSettings, TOne.WhS.BusinessEntity.Entities","Company":"Sama","CountryId":1,"CityId":1,"CompanyLogo":0,"Contacts":{"$type":"System.Collections.Generic.List`1[[TOne.WhS.BusinessEntity.Entities.CarrierContact, TOne.WhS.BusinessEntity.Entities]], mscorlib","$values":[]}}'),
+(4,'MHD Profile','{"$type":"TOne.WhS.BusinessEntity.Entities.CarrierProfileSettings, TOne.WhS.BusinessEntity.Entities","Company":"MHD","CountryId":1,"CityId":1,"CompanyLogo":0,"Contacts":{"$type":"System.Collections.Generic.List`1[[TOne.WhS.BusinessEntity.Entities.CarrierContact, TOne.WhS.BusinessEntity.Entities]], mscorlib","$values":[]}}'),
+(5,'Nettalk Profile','{"$type":"TOne.WhS.BusinessEntity.Entities.CarrierProfileSettings, TOne.WhS.BusinessEntity.Entities","Company":"Nettalk S.A.L","CountryId":1,"CityId":1,"CompanyLogo":0,"Contacts":{"$type":"System.Collections.Generic.List`1[[TOne.WhS.BusinessEntity.Entities.CarrierContact, TOne.WhS.BusinessEntity.Entities]], mscorlib","$values":[]}}')
+--\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\
+)c([ID],[Name],[Settings]))
+merge	[TOneWhS_BE].[CarrierProfile] as t
+using	cte_data as s
+on		1=1 and t.[ID] = s.[ID]
+when matched then
+	update set
+	[Name] = s.[Name],[Settings] = s.[Settings]
+when not matched by target then
+	insert([ID],[Name],[Settings])
+	values(s.[ID],s.[Name],s.[Settings])
+when not matched by source then
+	delete;
+set identity_insert [TOneWhS_BE].[CarrierProfile] off;
+
+--TOneWhS_BE.CodeGroup------------------------------------------------------------------------------
+----------------------------------------------------------------------------------------------------
+set nocount on;
+set identity_insert [TOneWhS_BE].[CodeGroup] on;
+;with cte_data([ID],[CountryID],[Code])
+as (select * from (values
+--//////////////////////////////////////////////////////////////////////////////////////////////////
+(11,1,'961'),
+(12,2,'963'),
+(13,3,'49'),
+(14,4,'33'),
+(15,5,'98')
+--\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\
+)c([ID],[CountryID],[Code]))
+merge	[TOneWhS_BE].[CodeGroup] as t
+using	cte_data as s
+on		1=1 and t.[ID] = s.[ID]
+when matched then
+	update set
+	[CountryID] = s.[CountryID],[Code] = s.[Code]
+when not matched by target then
+	insert([ID],[CountryID],[Code])
+	values(s.[ID],s.[CountryID],s.[Code])
+when not matched by source then
+	delete;
+set identity_insert [TOneWhS_BE].[CodeGroup] off;
+
+--TOneWhS_BE.CustomerSellingProduct-----------------------------------------------------------------
+----------------------------------------------------------------------------------------------------
+set nocount on;
+set identity_insert [TOneWhS_BE].[CustomerSellingProduct] on;
+;with cte_data([ID],[CustomerID],[SellingProductID],[AllDestinations],[BED],[EED])
+as (select * from (values
+--//////////////////////////////////////////////////////////////////////////////////////////////////
+(1,1,1,null,'2015-12-28 14:28:00.000',null),
+(2,2,1,null,'2015-12-28 14:30:00.000',null)
+--\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\
+)c([ID],[CustomerID],[SellingProductID],[AllDestinations],[BED],[EED]))
+merge	[TOneWhS_BE].[CustomerSellingProduct] as t
+using	cte_data as s
+on		1=1 and t.[ID] = s.[ID]
+when matched then
+	update set
+	[CustomerID] = s.[CustomerID],[SellingProductID] = s.[SellingProductID],[AllDestinations] = s.[AllDestinations],[BED] = s.[BED],[EED] = s.[EED]
+when not matched by target then
+	insert([ID],[CustomerID],[SellingProductID],[AllDestinations],[BED],[EED])
+	values(s.[ID],s.[CustomerID],s.[SellingProductID],s.[AllDestinations],s.[BED],s.[EED])
+when not matched by source then
+	delete;
+set identity_insert [TOneWhS_BE].[CustomerSellingProduct] off;
+
+--TOneWhS_BE.RoutingProduct-------------------------------------------------------------------------
+----------------------------------------------------------------------------------------------------
+set nocount on;
+set identity_insert [TOneWhS_BE].[RoutingProduct] on;
+;with cte_data([ID],[Name],[SellingNumberPlanID],[Settings])
+as (select * from (values
+--//////////////////////////////////////////////////////////////////////////////////////////////////
+(1,'Default Routing Product',1,'{"$type":"TOne.WhS.BusinessEntity.Entities.RoutingProductSettings, TOne.WhS.BusinessEntity.Entities","DefaultServicesFlag":0,"ZoneRelationType":0,"SupplierRelationType":0}')
+--\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\
+)c([ID],[Name],[SellingNumberPlanID],[Settings]))
+merge	[TOneWhS_BE].[RoutingProduct] as t
+using	cte_data as s
+on		1=1 and t.[ID] = s.[ID]
+when matched then
+	update set
+	[Name] = s.[Name],[SellingNumberPlanID] = s.[SellingNumberPlanID],[Settings] = s.[Settings]
+when not matched by target then
+	insert([ID],[Name],[SellingNumberPlanID],[Settings])
+	values(s.[ID],s.[Name],s.[SellingNumberPlanID],s.[Settings])
+when not matched by source then
+	delete;
+set identity_insert [TOneWhS_BE].[RoutingProduct] off;
+
+--TOneWhS_BE.SellingNumberPlan----------------------------------------------------------------------
+----------------------------------------------------------------------------------------------------
+set nocount on;
+set identity_insert [TOneWhS_BE].[SellingNumberPlan] on;
+;with cte_data([ID],[Name])
+as (select * from (values
+--//////////////////////////////////////////////////////////////////////////////////////////////////
+(1,'Default')
+--\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\
+)c([ID],[Name]))
+merge	[TOneWhS_BE].[SellingNumberPlan] as t
+using	cte_data as s
+on		1=1 and t.[ID] = s.[ID]
+when matched then
+	update set
+	[Name] = s.[Name]
+when not matched by target then
+	insert([ID],[Name])
+	values(s.[ID],s.[Name])
+when not matched by source then
+	delete;
+set identity_insert [TOneWhS_BE].[SellingNumberPlan] off;
+
+--TOneWhS_BE.SellingProduct-------------------------------------------------------------------------
+----------------------------------------------------------------------------------------------------
+set nocount on;
+set identity_insert [TOneWhS_BE].[SellingProduct] on;
+;with cte_data([ID],[Name],[DefaultRoutingProductID],[SellingNumberPlanID],[Settings])
+as (select * from (values
+--//////////////////////////////////////////////////////////////////////////////////////////////////
+(1,'Default Selling Product',null,1,'null')
+--\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\
+)c([ID],[Name],[DefaultRoutingProductID],[SellingNumberPlanID],[Settings]))
+merge	[TOneWhS_BE].[SellingProduct] as t
+using	cte_data as s
+on		1=1 and t.[ID] = s.[ID]
+when matched then
+	update set
+	[Name] = s.[Name],[DefaultRoutingProductID] = s.[DefaultRoutingProductID],[SellingNumberPlanID] = s.[SellingNumberPlanID],[Settings] = s.[Settings]
+when not matched by target then
+	insert([ID],[Name],[DefaultRoutingProductID],[SellingNumberPlanID],[Settings])
+	values(s.[ID],s.[Name],s.[DefaultRoutingProductID],s.[SellingNumberPlanID],s.[Settings])
+when not matched by source then
+	delete;
+set identity_insert [TOneWhS_BE].[SellingProduct] off;
+
+--TOneWhS_BE.Switch---------------------------------------------------------------------------------
+----------------------------------------------------------------------------------------------------
+set nocount on;
+set identity_insert [TOneWhS_BE].[Switch] on;
+;with cte_data([ID],[Name])
+as (select * from (values
+--//////////////////////////////////////////////////////////////////////////////////////////////////
+(1,'Default Switch')
+--\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\
+)c([ID],[Name]))
+merge	[TOneWhS_BE].[Switch] as t
+using	cte_data as s
+on		1=1 and t.[ID] = s.[ID]
+when matched then
+	update set
+	[Name] = s.[Name]
+when not matched by target then
+	insert([ID],[Name])
+	values(s.[ID],s.[Name])
+when not matched by source then
+	delete;
+set identity_insert [TOneWhS_BE].[Switch] off;
+
+
+DELETE FROM TOneWhS_BE.SalePriceList
+DBCC CHECKIDENT ('TOneWhS_BE.SalePriceList',RESEED, 0)
+
+TRUNCATE TABLE TOneWhS_BE.SupplierCode
+TRUNCATE TABLE TOneWhS_BE.SupplierRate
+DELETE FROM TOneWhS_BE.SupplierZone
+
+DELETE FROM TOneWhS_BE.SupplierPriceList
+
+TRUNCATE TABLE TOneWhS_BE.SaleCode
+TRUNCATE TABLE TOneWhS_BE.SaleRate
+DELETE FROM TOneWhS_BE.SaleZone
+
+TRUNCATE TABLE TOneWhS_BE.CP_SaleCode_Changed
+TRUNCATE TABLE TOneWhS_BE.CP_SaleCode_New
+TRUNCATE TABLE TOneWhS_BE.CP_SaleZone_Changed
+TRUNCATE TABLE TOneWhS_BE.CP_SaleZone_New
+TRUNCATE TABLE TOneWhS_BE.CustomerZone
+
+
+TRUNCATE TABLE TOneWhS_BE.SPL_SupplierCode_Changed
+TRUNCATE TABLE TOneWhS_BE.SPL_SupplierCode_New
+TRUNCATE TABLE TOneWhS_BE.SPL_SupplierRate_Changed
+TRUNCATE TABLE TOneWhS_BE.SPL_SupplierRate_New
+TRUNCATE TABLE TOneWhS_BE.SPL_SupplierZone_Changed
+TRUNCATE TABLE TOneWhS_BE.SPL_SupplierZone_New
+
+
+ 
+TRUNCATE TABLE TOneWhs_CP.CodePreparation
+TRUNCATE TABLE TOneWhS_Routing.RoutingDatabase
+TRUNCATE TABLE TOneWhS_Sales.RatePlan
+TRUNCATE TABLE TOneWhS_SPL.SupplierCode_Preview
+TRUNCATE TABLE TOneWhS_SPL.SupplierRate_Preview
+TRUNCATE TABLE TOneWhS_SPL.SupplierZone_Preview
