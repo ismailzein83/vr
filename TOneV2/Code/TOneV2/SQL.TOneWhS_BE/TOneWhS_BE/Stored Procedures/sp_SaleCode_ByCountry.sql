1907ee240985b6f98ccc4ddda651b9d524444072
Wissam Ajamy <VANRISE\wissam.ajamy>
2016-01-05 10:55:24 +0000

    update structure from TonV2_Dev
    git-tfs-id: [http://developmentvlan:8585/tfs/vanrise.collection]$/;C29562

---
+++ TOneV2/Code/TOneV2/SQL.TOneWhS_BE/TOneWhS_BE/Stored Procedures/sp_SaleCode_ByCountry.sql
@@ -0,0 +1,27 @@
+﻿-- =============================================
+-- Author:		<Author,,Name>
+-- Create date: <Create Date,,>
+-- Description:	<Description,,>
+-- =============================================
+CREATE PROCEDURE TOneWhS_BE.sp_SaleCode_ByCountry
+	-- Add the parameters for the stored procedure here
+	@SellingNumberPlanID int,
+	@CountryId int,
+	@When DateTime
+AS
+BEGIN
+	-- SET NOCOUNT ON added to prevent extra result sets from
+	-- interfering with SELECT statements.
+	SET NOCOUNT ON;
+/****** Script for SelectTopNRows command from SSMS  ******/
+	SELECT  sc.[ID],
+			sc.[Code],
+			sc.[ZoneID],
+			sc.[BED],
+			sc.[EED]
+	FROM	[TOneWhS_BE].[SaleCode] sc
+	join [TOneWhS_BE].[SaleZone] sz on sz.Id = sc.[ZoneID]
+	WHERE sz.SellingNumberPlanID = @SellingNumberPlanID and sz.CountryID = @CountryId 
+	   and ((sc.BED <= @when ) and (sc.EED is null or sc.EED > @when))
+        
+END
---
+++ TOneV2/Code/TOneV2/SQL.TOneWhS_BE/dbo/Tables/StatisticUpdateManager.sql
@@ -0,0 +1,9 @@
+﻿CREATE TABLE [dbo].[StatisticUpdateManager] (
+    [ID]                         BIGINT         IDENTITY (1, 1) NOT NULL,
+    [StatisticBatchGroupKey]     VARCHAR (900)  NOT NULL,
+    [StatisticBatchGroupContent] NVARCHAR (MAX) NOT NULL,
+    [LockedByProcessID]          INT            NULL,
+    CONSTRAINT [PK_StatisticUpdateManager] PRIMARY KEY CLUSTERED ([ID] ASC),
+    CONSTRAINT [IX_StatisticUpdateManager_BatchGroupKey] UNIQUE NONCLUSTERED ([StatisticBatchGroupKey] ASC)
+);
+
