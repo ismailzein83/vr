c1ae61a13a9bee97028f83edf912eeb3bffd9294
Ismail Zein <VANRISE\ismail.zein>
2016-01-06 13:07:01 +0000

    added EnqueueItem activity
    git-tfs-id: [http://developmentvlan:8585/tfs/vanrise.collection]$/;C29587

---
+++ BusinessProcess/Code/Vanrise.BusinessProcess.WFActivities/EnqueueItem.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Activities;
+
+namespace Vanrise.BusinessProcess.WFActivities
+{
+
+    public sealed class EnqueueItem<T> : CodeActivity
+    {
+        [RequiredArgument]
+        public InArgument<T> Item { get; set; }
+
+        [RequiredArgument]
+        public InArgument<Vanrise.Queueing.BaseQueue<T>> Queue { get; set; }
+
+        protected override void Execute(CodeActivityContext context)
+        {
+            this.Queue.Get(context).Enqueue(this.Item.Get(context));
+        }
+    }
+}
