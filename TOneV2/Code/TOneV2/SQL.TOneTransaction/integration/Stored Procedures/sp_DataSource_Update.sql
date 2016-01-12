d7c11b936eec3a12ef1839acf832d0f39f330024
Mortada Issa <VANRISE\mortada.issa>
2016-01-12 07:56:26 +0000

    Updating data source proc
    git-tfs-id: [http://developmentvlan:8585/tfs/vanrise.collection]$/;C29744

--- TOneV2/Code/TOneV2/SQL.TOneTransaction/integration/Stored Procedures/sp_DataSource_Update.sql
+++ TOneV2/Code/TOneV2/SQL.TOneTransaction/integration/Stored Procedures/sp_DataSource_Update.sql
@@ -7,14 +7,12 @@ CREATE PROCEDURE [integration].[sp_DataSource_Update]
 	@ID int,
 	@Name varchar(100),
 	@AdapterTypeId int,
-	@AdapterState varchar(1000),
 	@Settings varchar(max)
 AS
 BEGIN
 	Update integration.DataSource 
 	Set Name = @Name,
 		AdapterID = @AdapterTypeId,
-		AdapterState = @AdapterState,
 		Settings = @Settings
 	Where ID = @ID
 END
