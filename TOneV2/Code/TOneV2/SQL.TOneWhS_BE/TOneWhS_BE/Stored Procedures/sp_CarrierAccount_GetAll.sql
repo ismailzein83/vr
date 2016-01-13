8349a00452f9c8cefdf676b3e6736b1a8e69877f
Wissam Ajamy <VANRISE\wissam.ajamy>
2016-01-13 08:22:41 +0000

    Update structure from TOneV2_Dev
    git-tfs-id: [http://developmentvlan:8585/tfs/vanrise.collection]$/;C29783

--- TOneV2/Code/TOneV2/SQL.TOneWhS_BE/TOneWhS_BE/Stored Procedures/sp_CarrierAccount_GetAll.sql
+++ TOneV2/Code/TOneV2/SQL.TOneWhS_BE/TOneWhS_BE/Stored Procedures/sp_CarrierAccount_GetAll.sql
@@ -4,15 +4,14 @@ BEGIN
 	-- SET NOCOUNT ON added to prevent extra result sets from
 	-- interfering with SELECT statements.
 	SET NOCOUNT ON;
-	SELECT
-			ca.ID,
-			ca.AccountType,
-			ca.CarrierProfileID,
-		    ca.Name,
-			ca.CustomerSettings,
-			ca.SupplierSettings,
-			ca.SellingNumberPlanID,
-			ca.CarrierAccountSettings
-	FROM TOneWhS_BE.CarrierAccount ca                   
+	SELECT ca.ID,
+		ca.AccountType,
+		ca.CarrierProfileID,
+		ca.Name, -- The Name column should be renamed to NameSuffix
+		ca.CustomerSettings,
+		ca.SupplierSettings,
+		ca.SellingNumberPlanID,
+		ca.CarrierAccountSettings
+	FROM TOneWhS_BE.CarrierAccount ca
 	SET NOCOUNT OFF
 END
