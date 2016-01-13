65195fd9942e61e6fb782d7575dc3b43b89602ca
walid.mogharbel <walid.mogharbel@vanrise.tfs.local>
2016-01-13 11:04:48 +0000

    added get prefix activity
    git-tfs-id: [http://developmentvlan:8585/tfs/vanrise.collection]$/;C29797

---
+++ CDRAnalysis/Vanrise.Fzero.FraudAnalysis.BP.Activities/GetPrefixes.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Activities;
+using System.Collections.Generic;
+using Vanrise.BusinessProcess;
+using Vanrise.Common;
+using Vanrise.Fzero.FraudAnalysis.Business;
+using Vanrise.Fzero.FraudAnalysis.Entities;
+
+namespace Vanrise.Fzero.FraudAnalysis.BP.Activities
+{
+
+    public class GetPrefixes : CodeActivity
+    {
+        #region Arguments
+
+        [RequiredArgument]
+        public InArgument<int> PrefixLength { get; set; }
+
+        public OutArgument<List<string>> Prefixes { get; set; }
+
+
+        #endregion
+
+        protected override void Execute(CodeActivityContext context)
+        {
+            ContextExtensions.WriteTrackingMessage(context, LogEntryType.Verbose, "Started creating prefix list with length {0}", PrefixLength);
+
+            List<string> prefixes = new List<string>();
+            int prefixLength = this.PrefixLength.Get(context);
+            string prefixMax = "";
+
+            for (int i = 0; i < prefixLength; i++)
+                prefixMax = prefixMax + "9";
+
+            int maxValue = 0;
+            int.TryParse(prefixMax, out maxValue);
+
+            for (int i = 1; i <= maxValue; i++)
+                prefixes.Add(i.ToString("D" + prefixLength as string));
+
+
+            ContextExtensions.WriteTrackingMessage(context, LogEntryType.Verbose, "Finished creating prefix list ");
+
+            context.SetValue(Prefixes, prefixes);
+        }
+    }
+}
