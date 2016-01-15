a1ffe95c64e7897e0fbd4daf6e14187ae075ab0d
Zeinab Mokdad <VANRISE\zeinab.mokdad>
2016-01-15 07:51:28 +0000

    adding carrier portal controller and in we.Config to allow more than 4M object size
    git-tfs-id: [http://developmentvlan:8585/tfs/vanrise.collection]$/;C29830

---
+++ TOneV2/Code/TOneV2/TOne.Web.Online/Controllers/SupplierPriceListController.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Web;
+using System.Web.Http;
+using Vanrise.Entities;
+
+namespace TOne.Web.Online.Controllers
+{
+    public class SupplierPriceListController : ApiController
+    {
+        [HttpPost]
+        public bool UploadPriceList(SupplierPriceListUserInput userInput)
+        {
+            bool success = true;
+
+            return success;
+        }
+
+        [HttpGet]
+        public string ping()
+        {
+            return "hello wolrd";
+        }
+        public class SupplierPriceListUserInput
+        {
+            public int UserId { get; set; }
+            public string PriceListType { get; set; }
+            public byte[] ContentFile { get; set; }
+            public string FileName { get; set; }
+            public DateTime EffectiveOnDateTime { get; set; }
+        }
+    }
+}
