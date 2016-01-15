888f65cc42b7f54611a962e5aee8937ad728340e
Samer Haidar <VANRISE\samer.haidar>
2016-01-15 12:50:03 +0000

    Adding New Classes for widget Definition
    git-tfs-id: [http://developmentvlan:8585/tfs/vanrise.collection]$/;C29846

---
+++ Vanrise/Code/Vanrise.Security.Data.SQL/WidgetDefinitionDataManager.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Vanrise.Data.SQL;
+
+namespace Vanrise.Security.Data.SQL
+{
+    public class WidgetDefinitionDataManager:BaseSQLDataManager,IWidgetDefinitionDataManager
+    {
+    }
+}
