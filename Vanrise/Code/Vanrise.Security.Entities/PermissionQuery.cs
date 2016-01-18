276556451fc3cdc905def50499e9623babff3b8a
mohamad.elhariri <mohamad.elhariri@vanrise.tfs.local>
2016-01-18 10:22:25 +0000

    Added the PermissionQuery entity
    git-tfs-id: [http://developmentvlan:8585/tfs/vanrise.collection]$/;C29870

---
+++ Vanrise/Code/Vanrise.Security.Entities/PermissionQuery.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Vanrise.Security.Entities
+{
+    public class PermissionQuery
+    {
+        public string EntityId { get; set; }
+        public EntityType EntityType { get; set; }
+    }
+}
