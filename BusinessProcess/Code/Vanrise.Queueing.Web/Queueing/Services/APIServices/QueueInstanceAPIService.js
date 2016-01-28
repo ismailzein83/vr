79e9b0ec3e9984c1ec85ee7ad8d45421e1129776
mostafa.jawhari <mostafa.jawhari@vanrise.tfs.local>
2016-01-28 14:37:08 +0000

    Execution Flow Select Directive Added
    git-tfs-id: [http://developmentvlan:8585/tfs/vanrise.collection]$/;C30115

---
+++ BusinessProcess/Code/Vanrise.Queueing.Web/Queueing/Services/APIServices/QueueInstanceAPIService.js
@@ -0,0 +1,26 @@
+﻿(function (appControllers) {
+
+    'use strict';
+
+    QueueInstanceAPIService.$inject = ['BaseAPIService', 'UtilsService', 'VR_Queueing_ModuleConfig'];
+
+    function QueueInstanceAPIService(BaseAPIService, UtilsService, VR_Queueing_ModuleConfig) {
+        return ({
+            GetExecutionFlows: GetExecutionFlows,
+            GetStageNames: GetStageNames
+        });
+
+        function GetExecutionFlows() {
+            return BaseAPIService.get(UtilsService.getServiceURL(VR_Queueing_ModuleConfig.moduleName, 'ExecutionFlow', 'GetExecutionFlows'));
+        }
+
+        function GetStageNames() {
+            return BaseAPIService.get(UtilsService.getServiceURL(VR_Queueing_ModuleConfig.moduleName, 'QueueInstance', 'GetStageNames'));
+        }
+       
+
+    }
+
+    appControllers.service('VR_Queueing_QueueInstanceAPIService', QueueInstanceAPIService);
+
+})(appControllers);
