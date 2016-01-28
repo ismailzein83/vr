5d8941128e9c8fc2c145aaac91dfc4b4c36fb7b5
Ismail Zein <VANRISE\ismail.zein>
2016-01-28 13:04:36 +0000

    implemented IBusinessEntityGroup in SaleZoneGroupSettings
    git-tfs-id: [http://developmentvlan:8585/tfs/vanrise.collection]$/;C30109

--- TOneV2/Code/TOneV2/TOne.WhS.BusinessEntity.Entities/SaleZone/SaleZoneGroupSettings.cs
+++ TOneV2/Code/TOneV2/TOne.WhS.BusinessEntity.Entities/SaleZone/SaleZoneGroupSettings.cs
@@ -3,10 +3,11 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Vanrise.GenericData.Entities.BusinessEntities;
 
 namespace TOne.WhS.BusinessEntity.Entities
 {
-    public abstract class SaleZoneGroupSettings
+    public abstract class SaleZoneGroupSettings : Vanrise.GenericData.Entities.IBusinessEntityGroup
     {
         public int ConfigId { get; set; }
 
@@ -15,5 +16,15 @@ namespace TOne.WhS.BusinessEntity.Entities
         public abstract IEnumerable<long> GetZoneIds(ISaleZoneGroupContext context);
 
         public abstract string GetDescription(ISaleZoneGroupContext context);
+
+        IEnumerable<object> Vanrise.GenericData.Entities.IBusinessEntityGroup.GetIds(IBusinessEntityGroupContext context)
+        {
+            return this.GetZoneIds(null) as IEnumerable<Object>;
+        }
+
+        string Vanrise.GenericData.Entities.IBusinessEntityGroup.GetDescription(IBusinessEntityGroupContext context)
+        {
+            return this.GetDescription(null);
+        }
     }
 }
