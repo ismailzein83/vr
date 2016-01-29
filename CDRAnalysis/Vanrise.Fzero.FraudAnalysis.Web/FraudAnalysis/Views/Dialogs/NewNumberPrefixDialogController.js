1e732772c2a886cea003a394ac0feceacbf55aa9
walid.mogharbel <walid.mogharbel@vanrise.tfs.local>
2016-01-29 12:26:19 +0000

    still working on updating number preifx
    git-tfs-id: [http://developmentvlan:8585/tfs/vanrise.collection]$/;C30142

--- CDRAnalysis/Vanrise.Fzero.FraudAnalysis.Web/FraudAnalysis/Views/Dialogs/NewNumberPrefixDialogController.js
+++ CDRAnalysis/Vanrise.Fzero.FraudAnalysis.Web/FraudAnalysis/Views/Dialogs/NewNumberPrefixDialogController.js
@@ -2,17 +2,12 @@
 
     "use strict";
 
-    newNumberPrefixDialogController.$inject = ['$scope', 'FraudAnalysis_NumberPrefixAPIService', 'VRNotificationService', 'VRNavigationService', 'UtilsService', 'VRUIUtilsService', 'FraudAnalysis_NewFAOutputResultEnum'];
+    newNumberPrefixDialogController.$inject = ['$scope', 'VRNavigationService', 'UtilsService'];
 
-    function newNumberPrefixDialogController($scope, FraudAnalysis_NumberPrefixAPIService, VRNotificationService, VRNavigationService, UtilsService, VRUIUtilsService, FraudAnalysis_NewFAOutputResultEnum) {
+    function newNumberPrefixDialogController($scope, VRNavigationService, UtilsService) {
 
         var treeNumberPrefixes;
-        var numberPrefixId;
-        var countryId;
-        var editMode;
         var numberPrefixEntity;
-        var sellingNumberPlanId;
-        var disableCountry;
 
         defineScope();
         loadParameters();
@@ -20,33 +15,30 @@
         function loadParameters() {
             var parameters = VRNavigationService.getParameters($scope);
             if (parameters != undefined && parameters != null) {
-                numberPrefixId = parameters.NumberPrefixId;
-                countryId = parameters.CountryId;
-                $scope.countryName = parameters.CountryName;
-                sellingNumberPlanId = parameters.SellingNumberPlanId;
                 treeNumberPrefixes = parameters.NumberPrefixes;
             }
-            editMode = (numberPrefixId != undefined);
-            load();
         }
 
         function defineScope() {
-            $scope.bed;
-            $scope.eed;
-            $scope.countryName;
+
+            $scope.title = UtilsService.buildTitleForAddEditor("Number Prefix");
             $scope.numberPrefixes = [];
 
-            $scope.validateNumberPrefixs = function () {
+            $scope.validateNumberPrefixes = function () {
                 if ($scope.numberPrefixes != undefined && $scope.numberPrefixes.length == 0)
                     return "Enter at least one prefix.";
                 return null;
             };
             $scope.saveNumberPrefix = function () {
-                if (editMode) {
-                    return updateNumberPrefix();
-                }
-                else {
-                    return insertNumberPrefix();
+                if ($scope.onNumberPrefixAdded != undefined) {
+                    var addedNumberPrefixes = [];
+                    for (var i = 0; i < $scope.numberPrefixes.length; i++) {
+                        addedNumberPrefixes.push({ Prefix: $scope.numberPrefixes[i].prefix });
+                    }
+
+                    $scope.onNumberPrefixAdded(addedNumberPrefixes);
+                    $scope.numberPrefixes.length = 0;
+                    $scope.close();
                 }
             };
 
@@ -64,58 +56,7 @@
                 $scope.disabledNumberPrefix = true;
             };
         }
-
-        function load() {
-            $scope.isGettingData = true;
-            if (countryId != undefined) {
-                $scope.title = UtilsService.buildTitleForAddEditor("NumberPrefix for Country " + $scope.countryName);
-                loadAllControls();
-            }
-            else if (editMode) {
-                getNumberPrefix().then(function () {
-                    loadAllControls()
-                        .finally(function () {
-                            numberPrefixEntity = undefined;
-                        });
-                }).catch(function () {
-                    VRNotificationService.notifyExceptionWithClose(error, $scope);
-                });
-            }
-            else {
-                loadAllControls();
-                $scope.title = UtilsService.buildTitleForAddEditor("Number Prefix");
-            }
-        }
-
-        function loadAllControls() {
-            $scope.isGettingData = false;
-        }
-
-        function getNumberPrefix() {
-
-        }
-
-        function fillScopeFromNumberPrefixObj(prefix) {
-            $scope.name = prefix.Name;
-            $scope.title = UtilsService.buildTitleForUpdateEditor($scope.name, "NumberPrefix");
-        }
-
-        function insertNumberPrefix() {
-            if ($scope.onNumberPrefixAdded != undefined) {
-                var addedNumberPrefixes = [];
-                for (var i = 0; i < $scope.numberPrefixes.length; i++) {
-                    addedNumberPrefixes.push({ Prefix: $scope.numberPrefixes[i].prefix });
-                }
-
-                $scope.onNumberPrefixAdded(addedNumberPrefixes);
-                $scope.numberPrefixes.length = 0;
-                $scope.modalContext.closeModal();
-            }
-        }
-
-        function updateNumberPrefix() {
-
-        }
+       
     }
 
     appControllers.controller('cdranalysis-fa-numberprefix-newprefixdialog', newNumberPrefixDialogController);
