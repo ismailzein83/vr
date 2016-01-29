366a487b0c8206555e3abaf899ea849c523fe4d1
mohamad.elhariri <mohamad.elhariri@vanrise.tfs.local>
2016-01-29 10:32:14 +0000

    Created the StrategyFilter class
    git-tfs-id: [http://developmentvlan:8585/tfs/vanrise.collection]$/;C30130

---
+++ CDRAnalysis/Vanrise.Fzero.FraudAnalysis.Entities/Strategy/StrategyFilter.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Vanrise.Fzero.FraudAnalysis.Entities
+{
+    public class StrategyFilter
+    {
+        public int PeriodId { get; set; }
+        public bool? IsEnabled { get; set; }
+    }
+}
