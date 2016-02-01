86fcd61e7f894c8cefb6e43809a10573c27e33a7
mohamad.elhariri <mohamad.elhariri@vanrise.tfs.local>
2016-02-01 10:51:35 +0000

    Added the GenericRuleDefinitionEditor files
    git-tfs-id: [http://developmentvlan:8585/tfs/vanrise.collection]$/;C30180

--- Vanrise/Code/Vanrise.GenericData.Web/VR_GenericData/Services/GenericRuleDefinitionService.js
+++ Vanrise/Code/Vanrise.GenericData.Web/VR_GenericData/Services/GenericRuleDefinitionService.js
@@ -1,1 +1,29 @@
-﻿
+﻿(function (appControllers) {
+
+    'use strict';
+
+    GenericRuleDefinitionService.$inject = ['VRModalService'];
+
+    function GenericRuleDefinitionService(VRModalService) {
+        return {
+            editGenericRuleDefinition: editGenericRuleDefinition
+        };
+
+        function editGenericRuleDefinition(genericRuleDefinitionId, onGenericRuleDefinitionUpdated) {
+            var modalParameters = {
+                genericRuleDefinitionId: genericRuleDefinitionId
+            };
+
+            var modalSettings = {};
+
+            modalSettings.onScopeReady = function (modalScope) {
+                modalScope.onGenericRuleDefinitionUpdated = onGenericRuleDefinitionUpdated;
+            };
+
+            VRModalService.showModal('/Client/Modules/VR_GenericData/Views/GenericRuleDefinition/GenericRuleDefinitionEditor.html', modalParameters, modalSettings);
+        }
+    }
+
+    appControllers.service('VR_GenericData_GenericRuleDefinitionService', GenericRuleDefinitionService);
+
+})(appControllers);
