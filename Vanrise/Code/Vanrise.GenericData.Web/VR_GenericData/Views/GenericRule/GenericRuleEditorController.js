85d0267d30928bf961d3dcc8fa079198f0e25f65
Mortada Issa <VANRISE\mortada.issa>
2016-02-01 09:52:28 +0000

    Adding a new service for generic rule
    git-tfs-id: [http://developmentvlan:8585/tfs/vanrise.collection]$/;C30173

--- Vanrise/Code/Vanrise.GenericData.Web/VR_GenericData/Views/GenericRule/GenericRuleEditorController.js
+++ Vanrise/Code/Vanrise.GenericData.Web/VR_GenericData/Views/GenericRule/GenericRuleEditorController.js
@@ -1,1 +1,53 @@
-﻿
+﻿(function (appControllers) {
+
+    "use strict";
+
+    genericRuleManagementController.$inject = ['$scope', 'UtilsService'];
+
+    function genericRuleManagementController($scope, UtilsService) {
+        var gridAPI;
+
+        defineScope();
+        load();
+
+        function defineScope() {
+            $scope.sellingNumberPlans = [];
+            $scope.selectedSellingNumberPlans = [];
+
+            $scope.onGridReady = function (api) {
+                gridAPI = api;
+                api.loadGrid({});
+            }
+
+            $scope.searchClicked = function () {
+                if (!$scope.isGettingData && gridAPI != undefined)
+                    return gridAPI.loadGrid(getFilterObject());
+            };
+
+            $scope.AddNewRoutingProduct = AddNewRoutingProduct;
+
+            function getFilterObject() {
+                var data = {
+                    Name: $scope.name,
+                    SellingNumberPlanIds: UtilsService.getPropValuesFromArray($scope.selectedSellingNumberPlans, "SellingNumberPlanId")
+                };
+                return data;
+            }
+        }
+
+        function load() {
+
+        }
+
+        function AddNewRoutingProduct() {
+
+            var onRoutingProductAdded = function (addedItem) {
+                gridAPI.onRoutingProductAdded(addedItem);
+            };
+
+            WhS_BE_RoutingProductService.addRoutingProduct(onRoutingProductAdded);
+        }
+    }
+
+    appControllers.controller('VR_GenericData_GenericRuleManagementController', genericRuleManagementController);
+})(appControllers);
