a109a87bdfeb6019a00147ae0af203e5fd4fdbb1
mostafa.jawhari <mostafa.jawhari@vanrise.tfs.local>
2016-02-02 06:58:58 +0000

    timestamp column added to ExecutionFlowDefiniton Table
    git-tfs-id: [http://developmentvlan:8585/tfs/vanrise.collection]$/;C30203

--- TOneV2/Code/TOneV2/SQL.TOneTransaction/queue/Tables/ExecutionFlowDefinition.sql
+++ TOneV2/Code/TOneV2/SQL.TOneTransaction/queue/Tables/ExecutionFlowDefinition.sql
@@ -3,7 +3,10 @@
     [Name]          VARCHAR (255)  NOT NULL,
     [Title]         NVARCHAR (255) NOT NULL,
     [ExecutionTree] NVARCHAR (MAX) NOT NULL,
+    [timestamp]     ROWVERSION     NULL,
     CONSTRAINT [PK_ExecutionFlowType] PRIMARY KEY CLUSTERED ([ID] ASC),
     CONSTRAINT [IX_ExecutionFlowType_Name] UNIQUE NONCLUSTERED ([Name] ASC)
 );
+
+
 
