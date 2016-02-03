93f2dfa68da5301157af5298f9e25c1fe23c9fde
mohamad.elhariri <mohamad.elhariri@vanrise.tfs.local>
2016-02-03 08:20:13 +0000

    Finished vr-genericdata-genericruledefinitionsettings-mapping and added vr-genericdata-genericrulesettings-selective
    git-tfs-id: [http://developmentvlan:8585/tfs/vanrise.collection]$/;C30250

--- Vanrise/Code/Vanrise.GenericData.Web/VR_GenericData/Directives/MainExtensions/GenericRule/MappingRule/vr-genericdata-genericruledefinitionsettings-mapping.js
+++ Vanrise/Code/Vanrise.GenericData.Web/VR_GenericData/Directives/MainExtensions/GenericRule/MappingRule/vr-genericdata-genericruledefinitionsettings-mapping.js
@@ -1,1 +1,70 @@
-﻿
+﻿(function (app) {
+
+    'use strict';
+
+    MappingRuleDefinitionSettingsDirective.$inject = ['UtilsService', 'VRUIUtilsService'];
+
+    function MappingRuleDefinitionSettingsDirective(UtilsService, VRUIUtilsService) {
+        return {
+            restrict: "E",
+            scope:
+            {
+                onReady: "=",
+            },
+            controller: function ($scope, $element, $attrs) {
+                var ctrl = this;
+                var mappingRuleDefinitionSettingsDirective = new MappingRuleDefinitionSettingsDirective($scope, ctrl, $attrs);
+                mappingRuleDefinitionSettingsDirective.initializeController();
+            },
+            controllerAs: "ctrl",
+            bindToController: true,
+            compile: function (element, attrs) {
+
+            },
+            templateUrl: "/Client/Modules/VR_GenericData/Directives/MainExtensions/GenericRule/MappingRule/Templates/MappingRuleDefinitionSettingsTemplate.html"
+        };
+
+        function MappingRuleDefinitionSettingsDirective($scope, ctrl, $attrs) {
+            this.initializeController = initializeController;
+
+            var fieldTypeSelectiveAPI;
+
+            function initializeController() {
+                ctrl.onFieldTypeSelectiveReady = function (api) {
+                    fieldTypeSelectiveAPI = api;
+                    
+                    if (ctrl.onReady != undefined && typeof (ctrl.onReady) == 'function') {
+                        ctrl.onReady(getDirectiveAPI());
+                    }
+                };
+            }
+
+            function getDirectiveAPI() {
+                var api = {};
+
+                api.load = function (payload) {
+                    if (payload != undefined) {
+                        ctrl.fieldName = payload.FieldName;
+                    }
+
+                    var fieldTypeSelectiveLoadDeferred = UtilsService.createPomiseDeferred();
+                    VRUIUtilsService.callDirectiveLoad(fieldTypeSelectiveAPI, payload, fieldTypeSelectiveLoadDeferred);
+                    return fieldTypeSelectiveLoadDeferred.promise;
+                };
+
+                api.getData = function () {
+                    return {
+                        $type: 'Vanrise.GenericData.Transformation.Entities.MappingRuleDefinitionSettings, Vanrise.GenericData.Transformation.Entities',
+                        FieldName: ctrl.fieldName,
+                        FieldType: fieldTypeSelectiveAPI.getData()
+                    };
+                };
+
+                return api;
+            }
+        }
+    }
+
+    app.directive('vrGenericdataGenericruledefinitionsettingsMapping', MappingRuleDefinitionSettingsDirective);
+
+})(app);
