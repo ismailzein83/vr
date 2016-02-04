ecd15687a9d7b9d320b4528c4f41281e0400d2da
mohamad.elhariri <mohamad.elhariri@vanrise.tfs.local>
2016-02-04 08:45:48 +0000

    Refactored vr-genericdata-number to include a DataType property
    git-tfs-id: [http://developmentvlan:8585/tfs/vanrise.collection]$/;C30293

--- Vanrise/Code/Vanrise.GenericData.Web/VR_GenericData/Constants/FieldNumberDataTypeEnum.js
+++ Vanrise/Code/Vanrise.GenericData.Web/VR_GenericData/Constants/FieldNumberDataTypeEnum.js
@@ -1,1 +1,5 @@
-﻿
+﻿app.constant('VR_GenericData_FieldNumberDataTypeEnum', {
+    Decimal: { value: 0, description: 'Decimal' },
+    Int: { value: 1, description: 'Int' },
+    BigInt: { value: 2, description: 'BigInt' },
+});
--- Vanrise/Code/Vanrise.GenericData.Web/VR_GenericData/Directives/MainExtensions/DataRecordFields/vr-genericdata-number.js
+++ Vanrise/Code/Vanrise.GenericData.Web/VR_GenericData/Directives/MainExtensions/DataRecordFields/vr-genericdata-number.js
@@ -1,59 +1,75 @@
 ﻿'use strict';
-app.directive('vrGenericdataNumber', ['UtilsService',
-    function (UtilsService) {
+app.directive('vrGenericdataNumber', ['VR_GenericData_FieldNumberDataTypeEnum', 'UtilsService', function (VR_GenericData_FieldNumberDataTypeEnum, UtilsService) {
+    return {
+        restrict: 'E',
+        scope: {
+            onReady: '='
+        },
+        controller: function ($scope, $element, $attrs) {
 
-        var directiveDefinitionObject = {
-            restrict: 'E',
-            scope:
-            {
-                onReady: '='
-            },
-            controller: function ($scope, $element, $attrs) {
+            var ctrl = this;
 
-                var ctrl = this;
+            var ctor = new numberTypeCtor(ctrl, $scope);
+            ctor.initializeController();
 
-                var ctor = new numberTypeCtor(ctrl, $scope);
-                ctor.initializeController();
+        },
+        controllerAs: 'ctrl',
+        bindToController: true,
+        compile: function (element, attrs) {
+            return {
+                pre: function ($scope, iElem, iAttrs, ctrl) {
 
-            },
-            controllerAs: 'ctrl',
-            bindToController: true,
-            compile: function (element, attrs) {
-                return {
-                    pre: function ($scope, iElem, iAttrs, ctrl) {
-
-                    }
                 }
-            },
-            templateUrl: function (element, attrs) {
-                return '/Client/Modules/VR_GenericData/Directives/MainExtensions/DataRecordFields/Templates/NumberDirectiveTemplate.html';
             }
-
-        };
-
-        function numberTypeCtor(ctrl, $scope) {
-
-            function initializeController() {
-                defineAPI();
-            }
-
-            function defineAPI() {
-                var api = {};
-
-                api.load = function (payload) { }
-
-                api.getData = function () {
-                    return {
-                        $type: "Vanrise.GenericData.MainExtensions.DataRecordFields.FieldNumberType, Vanrise.GenericData.MainExtensions",
-                    };
-                }
-
-                if (ctrl.onReady != null)
-                    ctrl.onReady(api);
-            }
-
-            this.initializeController = initializeController;
+        },
+        templateUrl: function (element, attrs) {
+            return '/Client/Modules/VR_GenericData/Directives/MainExtensions/DataRecordFields/Templates/NumberDirectiveTemplate.html';
         }
-        return directiveDefinitionObject;
+    };
+
+    function numberTypeCtor(ctrl, $scope) {
+
+        var selectorAPI;
+
+        function initializeController() {
+            ctrl.numberDataTypes = [];
+
+            ctrl.onSelectorReady = function (api) {
+                selectorAPI = api;
+                defineAPI();
+            };
+        }
+
+        function defineAPI() {
+            var api = {};
+
+            api.load = function (payload) {
+                var dataTypeValue;
+
+                if (payload != undefined) {
+                    dataTypeValue = payload.DataType
+                }
+
+                selectorAPI.clearDataSource();
+                ctrl.numberDataTypes = UtilsService.getArrayEnum(VR_GenericData_FieldNumberDataTypeEnum);
+
+                if (dataTypeValue != undefined) {
+                    ctrl.selectedNumberDataType = UtilsService.getItemByVal(ctrl.numberDataTypes, dataTypeValue, 'value');
+                }
+            };
+
+            api.getData = function () {
+                return {
+                    $type: "Vanrise.GenericData.MainExtensions.DataRecordFields.FieldNumberType, Vanrise.GenericData.MainExtensions",
+                    DataType: ctrl.selectedNumberDataType.value
+                };
+            };
+
+            if (ctrl.onReady != undefined && typeof (ctrl.onReady) == 'function') {
+                ctrl.onReady(api);
+            }
+        }
+
+        this.initializeController = initializeController;
     }
-]);
+ }]);
