250724fba0c08397969a1ae6673628e370319ced
Ismail Zein <VANRISE\ismail.zein>
2016-02-04 07:58:26 +0000

    updated SQL project
    git-tfs-id: [http://developmentvlan:8585/tfs/vanrise.collection]$/;C30287

--- TOneV2/Code/TOneV2/SQL.TOneQueueing/queue/Stored Procedures/sp_QueueItemHeader_GetItemStatusSummary_1.sql
+++ TOneV2/Code/TOneV2/SQL.TOneQueueing/queue/Stored Procedures/sp_QueueItemHeader_GetItemStatusSummary_1.sql
@@ -8,7 +8,7 @@ CREATE PROCEDURE [queue].[sp_QueueItemHeader_GetItemStatusSummary]
 AS
 BEGIN
   SELECT QueueID, Status, COUNT(*) as Count
-  FROM [queue].[QueueItemHeader]
+  FROM [queue].[QueueItemHeader] WITH(NOLOCK)
   WHERE Status <> 30
   GROUP BY QueueID, Status
 END
