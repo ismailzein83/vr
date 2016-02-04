42ce45673dd24c3b5161225429ec1fdcb6f6a7f9
Samer Haidar <VANRISE\samer.haidar>
2016-02-04 16:40:27 +0000

    addingapiservice to source
    git-tfs-id: [http://developmentvlan:8585/tfs/vanrise.collection]$/;C30331

--- Vanrise/Code/Vanrise.GenericData.Web/VR_GenericData/Directives/MainExtensions/MappingSteps/RuleSteps/vr-genericdata-datatransformation-ratevaluerulestep.js
+++ Vanrise/Code/Vanrise.GenericData.Web/VR_GenericData/Directives/MainExtensions/MappingSteps/RuleSteps/vr-genericdata-datatransformation-ratevaluerulestep.js
@@ -1,1 +1,110 @@
-﻿
+﻿'use strict';
+app.directive('vrGenericdataDatatransformationRatevaluerulestep', ['UtilsService', 'VR_GenericData_GenericRuleTypeConfigAPIService', 'VRUIUtilsService',
+    function (UtilsService, VR_GenericData_GenericRuleTypeConfigAPIService, VRUIUtilsService) {
+
+        var directiveDefinitionObject = {
+            restrict: 'E',
+            scope:
+            {
+                onReady: '='
+            },
+            controller: function ($scope, $element, $attrs) {
+
+                var ctrl = this;
+
+                var ctor = new ratevaluerulestepCtor(ctrl, $scope);
+                ctor.initializeController();
+
+            },
+            controllerAs: 'ctrl',
+            bindToController: true,
+            compile: function (element, attrs) {
+                return {
+                    pre: function ($scope, iElem, iAttrs, ctrl) {
+
+                    }
+                }
+            },
+            templateUrl: function (element, attrs) {
+                return '/Client/Modules/VR_GenericData/Directives/MainExtensions/MappingSteps/RuleSteps/Templates/AssignValueRuleStepTemplate.html';
+            }
+
+        };
+
+        function ratevaluerulestepCtor(ctrl, $scope) {
+            var ruleTypeName = "GenericRuleMapping";
+            var ruleTypeEntity;
+
+            var ruleStepCommonDirectiveAPI;
+            var ruleStepCommonDirectiveReadyPromiseDeferred = UtilsService.createPromiseDeferred();
+
+            var targetDirectiveAPI;
+            var targetDirectiveReadyPromiseDeferred = UtilsService.createPromiseDeferred();
+            function initializeController() {
+                $scope.onRuleStepCommonReady = function (api) {
+                    ruleStepCommonDirectiveAPI = api;
+                    ruleStepCommonDirectiveReadyPromiseDeferred.resolve();
+                }
+                $scope.onTargetDirectiveReady = function (api) {
+                    targetDirectiveAPI = api;
+                    targetDirectiveReadyPromiseDeferred.resolve();
+                }
+                defineAPI();
+            }
+
+            function defineAPI() {
+                var api = {};
+
+                api.load = function (payload) {
+                    var promises = [];
+                    var loadRuleStepCommonDirectivePromiseDeferred = UtilsService.createPromiseDeferred();
+                    ruleStepCommonDirectiveReadyPromiseDeferred.promise.then(function () {
+                        var payloadRuleStep = { ruleTypeName: ruleTypeName };
+                        if (payload != undefined && payload.Context != undefined)
+                            payloadRuleStep.context = payload.Context;
+                        if (payload != undefined && payload.stepDetails) {
+                            payloadRuleStep.ruleFieldsMappings = payload.stepDetails.RuleFieldsMappings;
+                            payloadRuleStep.effectiveTime = payload.stepDetails.EffectiveTime;
+                            payloadRuleStep.ruleDefinitionId = payload.stepDetails.RuleDefinitionId;
+                        }
+                        VRUIUtilsService.callDirectiveLoad(ruleStepCommonDirectiveAPI, payloadRuleStep, loadRuleStepCommonDirectivePromiseDeferred);
+                    });
+
+                    promises.push(loadRuleStepCommonDirectivePromiseDeferred.promise);
+
+                    var loadTargetDirectivePromiseDeferred = UtilsService.createPromiseDeferred();
+                    targetDirectiveReadyPromiseDeferred.promise.then(function () {
+                        var payloadtarget;
+                        if (payload != undefined) {
+                            payloadtarget = {};
+                            if (payload != undefined && payload.Context != undefined)
+                                payloadtarget.context = payload.Context;
+                            if (payload != undefined && payload.stepDetails != undefined)
+                                payloadtarget.selectedRecords = payload.stepDetails.Target;
+                        }
+                        VRUIUtilsService.callDirectiveLoad(targetDirectiveAPI, payloadtarget, loadTargetDirectivePromiseDeferred);
+                    });
+
+                    promises.push(loadTargetDirectivePromiseDeferred.promise);
+
+                    return UtilsService.waitMultiplePromises(promises);
+                }
+
+                api.getData = function () {
+                    var obj = {
+                        $type: "Vanrise.GenericData.Transformation.MainExtensions.MappingSteps.AssignValueRuleStep, Vanrise.GenericData.Transformation.MainExtensions",
+                        Target: targetDirectiveAPI != undefined ? targetDirectiveAPI.getData() : undefined
+                    }
+                    ruleStepCommonDirectiveAPI.setData(obj);
+                    return obj;
+                }
+
+                if (ctrl.onReady != null)
+                    ctrl.onReady(api);
+            }
+
+            this.initializeController = initializeController;
+        }
+        return directiveDefinitionObject;
+    }
+]);
---
+++ Vanrise/Code/Vanrise.GenericData.Web/VR_GenericData/Services/APIServices/DataTransformationStepConfigAPIService.js
@@ -0,0 +1,19 @@
+﻿(function (appControllers) {
+
+    'use strict';
+
+    DataTransformationStepConfigAPIService.$inject = ['BaseAPIService', 'UtilsService', 'VR_GenericData_ModuleConfig'];
+
+    function DataTransformationStepConfigAPIService(BaseAPIService, UtilsService, VR_GenericData_ModuleConfig) {
+        return ({
+            GetDataTransformationSteps: GetDataTransformationSteps,
+        });
+        function GetDataTransformationSteps(filter) {
+            return BaseAPIService.get(UtilsService.getServiceURL(VR_GenericData_ModuleConfig.moduleName, "DataTransformationStepConfig", "GetDataTransformationSteps"), { filter: filter });
+        }
+      
+    }
+
+    appControllers.service('VR_GenericData_DataTransformationStepConfigAPIService', DataTransformationStepConfigAPIService);
+
+})(appControllers);
