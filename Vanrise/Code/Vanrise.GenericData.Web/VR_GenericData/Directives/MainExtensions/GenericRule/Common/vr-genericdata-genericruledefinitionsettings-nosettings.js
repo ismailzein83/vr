922c2e5a4fa4b37eceaa1f744e6ed9bfaa90766e
Ismail Zein <VANRISE\ismail.zein>
2016-02-05 10:49:04 +0000

    added vr-genericdata-genericruledefinitionsettings-nosettings
    git-tfs-id: [http://developmentvlan:8585/tfs/vanrise.collection]$/;C30346

---
+++ Vanrise/Code/Vanrise.GenericData.Web/VR_GenericData/Directives/MainExtensions/GenericRule/Common/vr-genericdata-genericruledefinitionsettings-nosettings.js
@@ -0,0 +1,54 @@
+﻿(function (app) {
+
+    'use strict';
+
+    NosettingsRuleDefinitionSettingsDirective.$inject = [];
+
+    function NosettingsRuleDefinitionSettingsDirective() {
+        return {
+            restrict: "E",
+            scope:
+            {
+                onReady: "=",
+            },
+            controller: function ($scope, $element, $attrs) {
+                var ctrl = this;
+                var obj = new NosettingsRuleDefinitionSettings($scope, ctrl, $attrs);
+                obj.initializeController();
+            },
+            controllerAs: "ctrl",
+            bindToController: true,
+            compile: function (element, attrs) {
+
+            },
+            template: ""
+        };
+
+        function NosettingsRuleDefinitionSettings($scope, ctrl, $attrs) {
+            this.initializeController = initializeController;           
+
+            function initializeController() {
+                if (ctrl.onReady != undefined && typeof (ctrl.onReady) == 'function') {
+                    ctrl.onReady(getDirectiveAPI());
+                }
+            }
+
+            function getDirectiveAPI() {
+                var api = {};
+
+                api.load = function (payload) {
+                    
+                };
+
+                api.getData = function () {
+                    return {};
+                };
+
+                return api;
+            }
+        }
+    }
+
+    app.directive('vrGenericdataGenericruledefinitionsettingsNosettings', NosettingsRuleDefinitionSettingsDirective);
+
+})(app);
