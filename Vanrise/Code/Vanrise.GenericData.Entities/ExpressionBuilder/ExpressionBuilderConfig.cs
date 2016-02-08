715012f3f04ebde0e7124f212c233c0b926ec49b
Samer Haidar <VANRISE\samer.haidar>
2016-02-08 09:51:21 +0000

    adding new class entity
    git-tfs-id: [http://developmentvlan:8585/tfs/vanrise.collection]$/;C30393

--- Vanrise/Code/Vanrise.GenericData.Entities/ExpressionBuilder/ExpressionBuilderConfig.cs
+++ Vanrise/Code/Vanrise.GenericData.Entities/ExpressionBuilder/ExpressionBuilderConfig.cs
@@ -8,5 +8,12 @@ namespace Vanrise.GenericData.Entities.ExpressionBuilder
 {
     public class ExpressionBuilderConfig
     {
+        public int ExpressionBuilderConfigId { get; set; }
+
+        public string Name { get; set; }
+
+        public string Title { get; set; }
+
+        public string Editor { get; set; }
     }
 }
