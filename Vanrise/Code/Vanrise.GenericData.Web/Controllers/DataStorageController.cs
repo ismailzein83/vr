464d340e168b9fbe59e9004ec808e675423a5093
Ali Ballouk <VANRISE\ali.ballouk>
2016-02-08 10:11:40 +0000

    Add data storage base implementation
    git-tfs-id: [http://developmentvlan:8585/tfs/vanrise.collection]$/;C30397

---
+++ Vanrise/Code/Vanrise.GenericData.Web/Controllers/DataStorageController.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Http;
+using Vanrise.GenericData.Business;
+using Vanrise.GenericData.Entities;
+using Vanrise.Web.Base;
+
+namespace Vanrise.GenericData.Web.Controllers
+{
+    [JSONWithTypeAttribute]
+    [RoutePrefix(Constants.ROUTE_PREFIX + "DataStorage")]
+    public class DataStorageController : BaseAPIController
+    {
+        
+    }
+}
