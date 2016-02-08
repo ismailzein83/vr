9d807fce31de803eb206083c04282e4c07b340be
Ali Ballouk <VANRISE\ali.ballouk>
2016-02-08 14:22:16 +0000

    data source config getAll implementation
    git-tfs-id: [http://developmentvlan:8585/tfs/vanrise.collection]$/;C30421

---
+++ Vanrise/Code/Vanrise.GenericData.Business/DataStoreConfigManager.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Vanrise.Caching;
+using Vanrise.Common;
+using Vanrise.GenericData.Data;
+using Vanrise.GenericData.Entities;
+
+namespace Vanrise.GenericData.Business
+{
+    public class DataStoreConfigManager
+    {
+        #region Public Methods
+        public IEnumerable<DataStoreConfig> GetDataStoreConfigs()
+        {
+            var cachedDataStore = GetCachedDataStoreConfigs();
+            return cachedDataStore.Values;
+        }
+
+
+
+        public DataStoreConfig GeDataStoreConfigById(int dataStoreConfigId)
+        {
+            var cachedGenericRuleTypes = GetCachedDataStoreConfigs();
+            return cachedGenericRuleTypes.FindRecord(x => x.DataStoreConfigId == dataStoreConfigId);
+        }
+
+        #endregion
+
+        #region Private Methods
+        private Dictionary<int, DataStoreConfig> GetCachedDataStoreConfigs()
+        {
+            return CacheManagerFactory.GetCacheManager<CacheManager>().GetOrCreateObject("GetDataStoreConfigs",
+               () =>
+               {
+                   IDataStoreConfigDataManager dataManager = GenericDataDataManagerFactory.GetDataManager<IDataStoreConfigDataManager>();
+                   IEnumerable<DataStoreConfig> denericRuleTypes = dataManager.GetDataStoreConfigs();
+                   return denericRuleTypes.ToDictionary(c => c.DataStoreConfigId , c => c);
+               });
+        }
+
+        #endregion
+
+        #region Private Classes
+        private class CacheManager : Vanrise.Caching.BaseCacheManager
+        {
+            IDataStoreConfigDataManager _dataManager = GenericDataDataManagerFactory.GetDataManager<IDataStoreConfigDataManager>();
+            object _updateHandle;
+
+            protected override bool ShouldSetCacheExpired(object parameter)
+            {
+                return _dataManager.AreDataStoreConfigUpdated(ref _updateHandle);
+            }
+        }
+        #endregion
+    }
+}
---
+++ Vanrise/Code/Vanrise.GenericData.Data.SQL/DataStoreConfigDataManager.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Vanrise.Data.SQL;
+using Vanrise.GenericData.Entities;
+
+namespace Vanrise.GenericData.Data.SQL
+{
+    public class DataStoreConfigDataManager : BaseSQLDataManager, IDataStoreConfigDataManager
+    {
+        public DataStoreConfigDataManager()
+            : base(GetConnectionStringName("ConfigurationDBConnStringKey", "ConfigurationDBConnStringKey"))
+        {
+
+        }
+
+        #region Public Methods
+        public List<DataStoreConfig> GetDataStoreConfigs()
+        {
+            return GetItemsSP("genericdata.sp_DataStoreConfig_GetAll", DataStoreConfigMapper);
+        }
+        public bool AreDataStoreConfigUpdated(ref object updateHandle)
+        {
+            return base.IsDataUpdated("genericdata.DataStoreConfig", ref updateHandle);
+        }
+
+        #endregion
+
+        #region Mappers
+
+        DataStoreConfig DataStoreConfigMapper(IDataReader reader)
+        {
+            DataStoreConfig dataStoreConfig = Vanrise.Common.Serializer.Deserialize<DataStoreConfig>(reader["Details"] as string);
+            if (dataStoreConfig != null)
+            {
+                dataStoreConfig.DataStoreConfigId = Convert.ToInt32(reader["ID"]);
+                dataStoreConfig.Name = reader["Name"] as string;
+            }
+            return dataStoreConfig;
+        }
+
+        #endregion
+    }
+}
