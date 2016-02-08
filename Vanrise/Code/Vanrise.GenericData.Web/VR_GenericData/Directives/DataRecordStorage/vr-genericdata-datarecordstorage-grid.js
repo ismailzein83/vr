7dbe10937ecabfbef9db026fd5c2c3c66241eb9f
mohamad.elhariri <mohamad.elhariri@vanrise.tfs.local>
2016-02-08 10:28:28 +0000

    Added the service files of the DataRecordStorage entity and implemented its grid directive
    git-tfs-id: [http://developmentvlan:8585/tfs/vanrise.collection]$/;C30400

---
+++ Vanrise/Code/Vanrise.GenericData.Web/VR_GenericData/Directives/DataRecordStorage/vr-genericdata-datarecordstorage-grid.js
@@ -0,0 +1,85 @@
+﻿(function (app) {
+
+    'use strict';
+
+    DataRecordStorageGridDirective.$inject = ['VR_GenericData_DataRecordStorageAPIService', 'VR_GenericData_DataRecordStorageService', 'VRNotificationService'];
+
+    function DataRecordStorageGridDirective(VR_GenericData_DataRecordStorageAPIService, VR_GenericData_DataRecordStorageService, VRNotificationService) {
+        return {
+            restrict: 'E',
+            scope: {
+                onReady: '='
+            },
+            controller: function ($scope, $element, $attrs) {
+                var ctrl = this;
+                var dataRecordStorageGrid = new DataRecordStorageGrid($scope, ctrl, $attrs);
+                dataRecordStorageGrid.initializeController();
+            },
+            controllerAs: 'ctrl',
+            bindToController: true,
+            compile: function (element, attrs) {
+
+            },
+            templateUrl: '/Client/Modules/VR_GenericData/Directives/DataRecordStorage/Templates/DataRecordStorageGridTemplate.html'
+        };
+
+        function DataRecordStorageGrid($scope, ctrl, $attrs) {
+            this.initializeController = initializeController;
+
+            var gridAPI;
+
+            function initializeController() {
+                ctrl.dataRecordStorages = [];
+
+                ctrl.onGridReady = function (api) {
+                    gridAPI = api;
+
+                    if (ctrl.onReady != undefined && typeof (ctrl.onReady) == 'function') {
+                        ctrl.onReady(getDirectiveAPI());
+                    }
+                };
+
+                ctrl.dataRetrievalFunction = function (dataRetrievalInput, onResponseReady) {
+                    return VR_GenericData_DataRecordStorageAPIService.GetFilteredDataRecordStorages(dataRetrievalInput).then(function (response) {
+                        onResponseReady(response);
+                    }).catch(function (error) {
+                        VRNotificationService.notifyExceptionWithClose(error, $scope);
+                    });
+                };
+
+                defineMenuActions();
+            }
+
+            function getDirectiveAPI() {
+                var api = {};
+
+                api.loadGrid = function (query) {
+                    return gridAPI.retrieveData(query);
+                };
+
+                api.onGenericRuleDefinitionAdded = function (addedGenericRuleDefinition) {
+                    gridAPI.itemAdded(addedGenericRuleDefinition);
+                };
+
+                return api;
+            }
+
+            function defineMenuActions() {
+                ctrl.menuActions = [{
+                    name: 'Edit',
+                    clicked: editDataRecordStorage,
+                }];
+            }
+
+            function editDataRecordStorage(dataRecordStorage) {
+                var onDataRecordStorageUpdated = function (updatedDataRecordStorage) {
+                    gridAPI.itemUpdated(updatedDataRecordStorage);
+                };
+                VR_GenericData_DataRecordStorageService.editDataRecordStorage(dataRecordStorage.DataRecordStorageId, onDataRecordStorageUpdated);
+            }
+        }
+    }
+
+    app.directive('vrGenericdataDatarecordstorageGrid', DataRecordStorageGridDirective);
+
+})(app);
--- Vanrise/Code/Vanrise.GenericData.Web/VR_GenericData/Views/DataRecordStorage/DataRecordStorageManagementController.js
+++ Vanrise/Code/Vanrise.GenericData.Web/VR_GenericData/Views/DataRecordStorage/DataRecordStorageManagementController.js
@@ -1,1 +1,72 @@
-﻿
+﻿(function (appControllers) {
+
+    'use strict';
+
+    DataRecordStorageManagementController.$inject = ['$scope', 'VR_GenericData_DataRecordStorageService', 'UtilsService'];
+
+    function DataRecordStorageManagementController($scope, VR_GenericData_DataRecordStorageService, UtilsService) {
+
+        var dataRecordTypeSelectorAPI;
+        var dataRecordTypeSelectorReadyDeferred = UtilsService.createPromiseDeferred();
+
+        var gridAPI;
+        var gridQuery = {};
+
+        defineScope();
+        load();
+
+        function defineScope() {
+            $scope.onDataRecordTypeSelectorReady = function (api) {
+                dataRecordTypeSelectorAPI = api;
+                dataRecordTypeSelectorReadyDeferred.resolve();
+            };
+            $scope.onGridReady = function (api) {
+                gridAPI = api;
+                gridAPI.loadGrid(gridQuery);
+            };
+
+            $scope.search = function () {
+                setGridQuery();
+                return gridAPI.loadGrid(gridQuery);
+            };
+            $scope.addDataRecordStorage = function () {
+                var onDataRecordStorageAdded = function (addedDataRecordStorage) {
+                    gridAPI.onDataRecordStorageAdded(addedDataRecordStorage);
+                };
+                VR_GenericData_DataRecordStorageService.addDataRecordStorage(onDataRecordStorageAdded);
+            };
+        }
+
+        function load() {
+            $scope.isLoading = true;
+
+            UtilsService.waitMultipleAsyncOperations([loadDataRecordTypeSelector, loadDataStoreSelector]).catch(function (error) {
+                VRNotificationService.notifyException(error, $scope);
+            }).finally(function () {
+                $scope.isLoading = false;
+            });
+
+            function loadDataRecordTypeSelector() {
+                var dataRecordTypeSelectorLoadDeferred = UtilsService.createPromiseDeferred();
+
+                dataRecordTypeSelectorReadyDeferred.promise.then(function () {
+                    VRUIUtilsService.callDirectiveLoad(dataRecordTypeSelectorAPI, undefined, dataRecordTypeSelectorLoadDeferred);
+                });
+
+                return dataRecordTypeSelectorLoadDeferred.promise;
+            }
+            function loadDataStoreSelector() { }
+        }
+
+        function setGridQuery() {
+            gridQuery = {
+                Name: $scope.name,
+                DataRecordTypeIds: null,
+                DataStoreIds: null
+            };
+        }
+    }
+
+    appControllers.controller('VR_GenericData_DataRecordStorageManagementController', DataRecordStorageManagementController);
+
+})(appControllers);
