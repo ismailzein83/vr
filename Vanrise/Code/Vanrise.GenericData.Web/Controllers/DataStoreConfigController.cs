6ea728bffd94084f32cfa023de94e0b821ec91be
Ali Ballouk <VANRISE\ali.ballouk>
2016-02-08 13:59:45 +0000

    datastore config base implementation
    git-tfs-id: [http://developmentvlan:8585/tfs/vanrise.collection]$/;C30420

---
+++ Vanrise/Code/Vanrise.GenericData.Web/Controllers/DataStoreConfigController.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Http;
+using Vanrise.GenericData.Business;
+using Vanrise.GenericData.Entities;
+using Vanrise.Web.Base;
+
+namespace Vanrise.GenericData.Web.Controllers
+{
+    [JSONWithTypeAttribute]
+    [RoutePrefix(Constants.ROUTE_PREFIX + "DataStoreConfig")]
+    public class DataStoreConfigController:BaseAPIController
+    {
+        [HttpGet]
+        [Route("GetGenericRuleTypes")]
+        public IEnumerable<DataStoreConfig> GetDataStoreConfigs()
+        {
+            return null;
+        }
+
+        
+
+    }
+}
