4176a1962e3eb6590fa454f8a2f158a23edcd733
mostafa.jawhari <mostafa.jawhari@vanrise.tfs.local>
2016-02-09 07:26:39 +0000

    sp_QueueItemHeader_CreateTempByFiltered2 added to TOneQueueing DB
    git-tfs-id: [http://developmentvlan:8585/tfs/vanrise.collection]$/;C30432

---
+++ TOneV2/Code/TOneV2/SQL.TOneQueueing/queue/Stored Procedures/sp_QueueItemHeader_CreateTempByFiltered2.sql
@@ -0,0 +1,41 @@
+﻿
+
+CREATE PROCEDURE [queue].[sp_QueueItemHeader_CreateTempByFiltered2]
+	@TempTableName varchar(200),	
+	@QueueIDs varchar(max), 
+	@QueueStatusIds nvarchar(max),
+	@CreatedTimeFrom DateTime,
+	@CreatedTimeTo DateTime
+AS
+BEGIN
+IF NOT OBJECT_ID(@TempTableName, N'U') IS NOT NULL
+	    BEGIN
+	    DECLARE @QueueIDsTable TABLE (QueueID int)
+		INSERT INTO @QueueIDsTable (QueueID)
+		select Convert(int, ParsedString) from [TOneQueueDB].[queue].[ParseStringList](@QueueIDs)
+		 DECLARE @QueueStatusIDsTable TABLE (StatusID int)
+		INSERT INTO @QueueStatusIDsTable (StatusID)
+		select Convert(int, ParsedString) from [TOneQueueDB].[queue].[ParseStringList](@QueueStatusIds)
+			SELECT itemHeader.ItemID,
+				   itemHeader.QueueID,
+			       itemHeader.[Description],
+			       itemHeader.[Status],
+			       itemHeader.ExecutionFlowTriggerItemID,
+			       itemHeader.RetryCount,
+			       itemHeader.SourceItemID,
+			       itemHeader.ErrorMessage,
+				   itemHeader.CreatedTime,
+				   itemHeader.LastUpdatedTime
+			INTO #RESULT
+			FROM [queue].[QueueItemHeader] itemHeader
+			Where (@QueueIDs  is null or itemHeader.QueueID in(select QueueID from @QueueIDsTable))
+			and (@QueueStatusIds  is null or itemHeader.Status in(select StatusID from @QueueStatusIDsTable))and (@CreatedTimeFrom is null or itemHeader.CreatedTime>=@CreatedTimeFrom)
+			and (@CreatedTimeTo is null or itemHeader.CreatedTime<=@CreatedTimeTo)
+			
+			DECLARE @sql VARCHAR(1000)
+			SET @sql = 'SELECT * INTO ' + @TempTableName + ' FROM #RESULT';
+			EXEC(@sql)
+		END                    
+
+	SET NOCOUNT OFF
+END
