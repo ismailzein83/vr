6eb09a392d5b335e4d8c3643ec6c04b19e16f97b
Ismail Zein <VANRISE\ismail.zein>
2016-02-09 13:48:28 +0000

    added DataRecordTypeSettings
    git-tfs-id: [http://developmentvlan:8585/tfs/vanrise.collection]$/;C30453

---
+++ Vanrise/Code/Vanrise.GenericData.Entities/DataRecords/DataRecordTypeSettings.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Vanrise.GenericData.Entities
+{
+    public class DataRecordTypeSettings
+    {
+        public int AssemblyId { get; set; }
+
+        public string RuntimeFQTN { get; set; }
+
+        public int BatchAssemblyId { get; set; }
+
+        public string BatchRuntimeFQTN { get; set; }
+    }
+}
