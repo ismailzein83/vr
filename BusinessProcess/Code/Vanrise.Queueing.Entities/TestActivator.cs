f55d85fc955d8943d087be36bfe17cdfbe3877ea
Ismail Zein <VANRISE\ismail.zein>
2016-02-10 14:58:10 +0000

    added TestActivator
    git-tfs-id: [http://developmentvlan:8585/tfs/vanrise.collection]$/;C30525

---
+++ BusinessProcess/Code/Vanrise.Queueing.Entities/TestActivator.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Vanrise.Queueing.Entities
+{
+    public class TestActivator : QueueActivator
+    {
+        public string DummyProp { get; set; }
+
+        public override void ProcessItem(PersistentQueueItem item, ItemsToEnqueue outputItems)
+        {
+            throw new NotImplementedException();
+        }
+
+        public override void OnDisposed()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
