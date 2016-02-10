567a5d63edbebd227117ff11a7288c84cbd22ee2
Ismail Zein <VANRISE\ismail.zein>
2016-02-10 09:35:05 +0000

    added Vanrise.GenericData.SQLDataStorage
    git-tfs-id: [http://developmentvlan:8585/tfs/vanrise.collection]$/;C30484

---
+++ Vanrise/Code/Vanrise.GenericData.SQLDataStorage/SQLDataRecordStorageSettings.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Vanrise.GenericData.Entities;
+
+namespace Vanrise.GenericData.SQLDataStorage
+{
+    public class SQLDataRecordStorageSettings : DataRecordStorageSettings
+    {
+        public string TableName { get; set; }
+
+        public List<SQLDataRecordStorageColumn> Columns { get; set; }
+    }
+
+    public class SQLDataRecordStorageColumn
+    {
+        public string ColumnName { get; set; }
+
+        public string SQLDataType { get; set; }
+
+        public string ValueExpression { get; set; }
+    }
+}
---
+++ Vanrise/Code/Vanrise.GenericData.SQLDataStorage/SQLDataRecordStorageState.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Vanrise.GenericData.SQLDataStorage
+{    
+    public class SQLDataRecordStorageState
+    {
+        public SQLDataRecordStorageSettings ExistingSettings { get; set; }
+    }
+}
