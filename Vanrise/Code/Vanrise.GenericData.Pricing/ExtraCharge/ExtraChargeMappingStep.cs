6111fe23029f60ec76f0cf04d0c069057821f162
Samer Haidar <VANRISE\samer.haidar>
2016-02-10 15:56:44 +0000

    adding two rules
    git-tfs-id: [http://developmentvlan:8585/tfs/vanrise.collection]$/;C30531

---
+++ Vanrise/Code/Vanrise.GenericData.Pricing/ExtraCharge/ExtraChargeMappingStep.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Vanrise.GenericData.Transformation.Entities;
+
+namespace Vanrise.GenericData.Pricing.ExtraCharge
+{
+    public class ExtraChargeMappingStep : BaseGenericRuleMappingStep
+    {
+        public string InitialRate { get; set; }
+        public string EffectiveRate { get; set; }
+        public override void GenerateExecutionCode(IDataTransformationCodeGenerationContext context)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ Vanrise/Code/Vanrise.GenericData.Pricing/Tariff/TariffMappingStep.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Vanrise.GenericData.Transformation.Entities;
+
+namespace Vanrise.GenericData.Pricing.Tariff
+{
+    public class TariffMappingStep : BaseGenericRuleMappingStep
+    {
+        public string  InitialRate { get; set; }
+        public string DurationInSeconds { get; set; }
+        public string EffectiveRate { get; set; }
+        public string EffectiveDurationInSeconds { get; set; }
+        public string TotalAmount { get; set; }
+        public override void GenerateExecutionCode(IDataTransformationCodeGenerationContext context)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
