aea303a47bdf8046458350a87b08eb5d21c6fa41
Zeinab Mokdad <VANRISE\zeinab.mokdad>
2016-02-11 15:42:39 +0000

    adding uploa dinfo class
    git-tfs-id: [http://developmentvlan:8585/tfs/vanrise.collection]$/;C30570

---
+++ TOneV2/Code/TOneV2/TOne.BusinessEntity.Entities/UploadInfo.cs
@@ -0,0 +1,10 @@
+﻿
+namespace TOne.BusinessEntity.Entities
+{
+    public class UploadInfo
+    {
+        public byte[] ContentBytes { get; set; }
+        public string FileName { get; set; }
+        public int Status { get; set; }
+    }
+}
