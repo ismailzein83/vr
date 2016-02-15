48ebe88126129b375436d7b9126f9e0992e2bd84
Ismail Zein <VANRISE\ismail.zein>
2016-02-15 11:58:06 +0000

    updated sql project
    git-tfs-id: [http://developmentvlan:8585/tfs/vanrise.collection]$/;C30619

--- TOneV2/Code/TOneV2/SQL.TOneTransaction/queue/Tables/QueueItemType.sql
+++ TOneV2/Code/TOneV2/SQL.TOneTransaction/queue/Tables/QueueItemType.sql
@@ -4,7 +4,10 @@
     [Title]                VARCHAR (255)  NOT NULL,
     [DefaultQueueSettings] NVARCHAR (MAX) NOT NULL,
     [CreatedTime]          DATETIME       CONSTRAINT [DF_QueueItemType_CreatedTime] DEFAULT (getdate()) NULL,
+    [timestamp]            ROWVERSION     NULL,
     CONSTRAINT [PK_QueueItemType] PRIMARY KEY CLUSTERED ([ID] ASC),
     CONSTRAINT [IX_QueueItemType_ItemFQTN] UNIQUE NONCLUSTERED ([ItemFQTN] ASC)
 );
+
+
 
