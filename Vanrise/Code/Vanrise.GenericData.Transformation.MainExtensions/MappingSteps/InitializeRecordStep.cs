cc4c74b867fcbe4dd3054d5290fcbb40b64ea023
Ismail Zein <VANRISE\ismail.zein>
2016-02-15 13:56:46 +0000

    fixed InitializeRecordStep
    git-tfs-id: [http://developmentvlan:8585/tfs/vanrise.collection]$/;C30643

--- Vanrise/Code/Vanrise.GenericData.Transformation.MainExtensions/MappingSteps/InitializeRecordStep.cs
+++ Vanrise/Code/Vanrise.GenericData.Transformation.MainExtensions/MappingSteps/InitializeRecordStep.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Vanrise.GenericData.Business;
 using Vanrise.GenericData.Transformation.Entities;
 
 namespace Vanrise.GenericData.Transformation.MainExtensions.MappingSteps
@@ -12,8 +13,17 @@ namespace Vanrise.GenericData.Transformation.MainExtensions.MappingSteps
         public string RecordName { get; set; }
         public override void GenerateExecutionCode(IDataTransformationCodeGenerationContext context)
         {
-            var recordName = context.GenerateUniqueMemberName(this.RecordName.ToLower());
-            context.AddCodeToCurrentInstanceExecutionBlock("{0} = {1}();", recordName, this.RecordName);
+            var record = context.Records.FirstOrDefault(itm => itm.RecordName == this.RecordName);
+            if (record == null)
+                throw new Exception(String.Format("Record '{0}' not found", this.RecordName));
+            DataRecordTypeManager dataRecordTypeManager = new DataRecordTypeManager();
+            var runtimeType = dataRecordTypeManager.GetDataRecordRuntimeType(record.DataRecordTypeId);
+            if (runtimeType == null)
+                throw new NullReferenceException("runtimeType");
+            if (!record.IsArray)
+                context.AddCodeToCurrentInstanceExecutionBlock("{0} = new {1}();", this.RecordName, runtimeType.FullName);
+            else
+                context.AddCodeToCurrentInstanceExecutionBlock("{0} = new List<{1}>();", this.RecordName, runtimeType.FullName);
         }
     }
 }
