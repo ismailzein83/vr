8849c1645eee489cb72c39021004c170cc2c7577
walid.mogharbel <walid.mogharbel@vanrise.tfs.local>
2016-02-16 08:12:24 +0000

    check in skeleton
    git-tfs-id: [http://developmentvlan:8585/tfs/vanrise.collection]$/;C30663

---
+++ Demo.project/Code/Demo.Module.Data/ICarrierAccountDataManager.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Demo.Module.Entities;
+
+
+namespace Demo.Module.Data
+{
+    public interface ICarrierAccountDataManager:IDataManager
+    {
+        List<CarrierAccount> GetCarrierAccounts();
+        bool Insert(CarrierAccount carrierAccount,out int carrierAccountId);
+        bool Update(CarrierAccount carrierAccount);
+        bool AreCarrierAccountsUpdated(ref object updateHandle);
+    }
+}
---
+++ Demo.project/Code/Demo.Module.Data/ICarrierProfileDataManager.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Demo.Module.Entities;
+
+namespace Demo.Module.Data
+{
+    public interface ICarrierProfileDataManager:IDataManager
+    {
+        List<CarrierProfile> GetCarrierProfiles();
+        bool Insert(CarrierProfile carrierProfile, out int carrierProfileId);
+        bool Update(CarrierProfile carrierProfile);
+        bool AreCarrierProfilesUpdated(ref object updateHandle);
+    }
+}
