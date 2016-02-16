50e6ed58d9d86f3c5fe1d27e6758bdb01bf07d64
walid.mogharbel <walid.mogharbel@vanrise.tfs.local>
2016-02-16 08:24:00 +0000

    added skeleton
    git-tfs-id: [http://developmentvlan:8585/tfs/vanrise.collection]$/;C30667

---
+++ Demo.project/Code/Demo.Module.Business/CarrierAccountManager.cs
@@ -0,0 +1,286 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Demo.Module.Data;
+using Demo.Module.Entities;
+using Vanrise.Common;
+using Vanrise.Common.Business;
+
+namespace Demo.Module.Business
+{
+    public class CarrierAccountManager
+    {
+     
+        #region ctor/Local Variables
+        CarrierProfileManager _carrierProfileManager;
+        public CarrierAccountManager()
+        {
+            _carrierProfileManager = new CarrierProfileManager();
+        }
+        
+        #endregion
+        
+        #region Public Methods
+        public Vanrise.Entities.IDataRetrievalResult<CarrierAccountDetail> GetFilteredCarrierAccounts(Vanrise.Entities.DataRetrievalInput<CarrierAccountQuery> input)
+        {
+            var allCarrierAccounts = GetCachedCarrierAccounts();
+
+            Func<CarrierAccount, bool> filterExpression = (item) =>
+                 (input.Query.Name == null || item.NameSuffix.ToLower().Contains(input.Query.Name.ToLower()))
+                 &&
+                 (input.Query.CarrierProfilesIds == null || input.Query.CarrierProfilesIds.Contains(item.CarrierProfileId))
+                  &&
+                 (input.Query.CarrierAccountsIds == null || input.Query.CarrierAccountsIds.Contains(item.CarrierAccountId))
+                   &&
+                 (input.Query.AccountsTypes == null || input.Query.AccountsTypes.Contains(item.AccountType))
+                  &&
+                 (input.Query.SellingNumberPlanIds == null || input.Query.SellingNumberPlanIds.Contains(item.SellingNumberPlanId));
+
+            return Vanrise.Common.DataRetrievalManager.Instance.ProcessResult(input, allCarrierAccounts.ToBigResult(input, filterExpression, CarrierAccountDetailMapper));
+        }
+        public IEnumerable<CarrierAccount> GetAllCarriers()
+        {
+            return GetCachedCarrierAccounts().Values;
+        }
+        public CarrierAccount GetCarrierAccount(int carrierAccountId)
+        {
+            var CarrierAccounts = GetCachedCarrierAccounts();
+            return CarrierAccounts.GetRecord(carrierAccountId);
+        }
+        public string GetDescription(IEnumerable<int> carrierAccountsIds, bool getCustomers, bool getSuppliers)
+        {
+            if(carrierAccountsIds.Count() > 0 )
+            {
+                StringBuilder stringBuilder = new StringBuilder();
+                foreach (int carrierAccount in carrierAccountsIds)
+                {
+                    if (stringBuilder != null && stringBuilder.Length > 0)
+                        stringBuilder.Append(",");
+                    stringBuilder.AppendFormat("{0}", GetCarrierAccountName(carrierAccount));
+                }
+                return stringBuilder.ToString();
+            }
+
+            return string.Empty;
+        }
+        public IEnumerable<CarrierAccountInfo> GetCarrierAccountInfo(CarrierAccountInfoFilter filter)
+        {
+            IEnumerable<CarrierAccount> carrierAccounts = null;
+
+            if (filter != null)
+            {
+                if (filter.AssignableToSellingProductId != null)
+                    carrierAccounts = this.GetNotAssignableCustomersToSellingProduct((int)filter.AssignableToSellingProductId);
+                else if (filter.AssignableToUserId != null)
+                {
+                    //carrierAccounts = this.GetCarriersAssignableToUserId(filter.GetCustomers, filter.GetSuppliers).ToList();
+                    //return carrierAccounts.MapRecords(AccountManagerCarrierMapper);
+                }
+
+                else
+                    carrierAccounts = this.GetCarrierAccountsByType(filter.GetCustomers, filter.GetSuppliers, filter.SupplierFilterSettings, filter.CustomerFilterSettings);
+            }
+            else
+            {
+                var cachedCarrierAccounts = GetCachedCarrierAccounts();
+                if (cachedCarrierAccounts != null)
+                    carrierAccounts = cachedCarrierAccounts.Values;
+            }
+
+            return carrierAccounts.MapRecords(CarrierAccountInfoMapper);
+        }
+       
+        public Vanrise.Entities.InsertOperationOutput<CarrierAccountDetail> AddCarrierAccount(CarrierAccount carrierAccount)
+        {
+            Vanrise.Entities.InsertOperationOutput<CarrierAccountDetail> insertOperationOutput = new Vanrise.Entities.InsertOperationOutput<CarrierAccountDetail>();
+
+            insertOperationOutput.Result = Vanrise.Entities.InsertOperationResult.Failed;
+            insertOperationOutput.InsertedObject = null;
+
+            int carrierAccountId = -1;
+
+            if ((carrierAccount.AccountType == CarrierAccountType.Customer || carrierAccount.AccountType == CarrierAccountType.Exchange) && carrierAccount.SellingNumberPlanId == null)
+                throw new ArgumentNullException("Missing SellingNumberPlanId");
+
+            ICarrierAccountDataManager dataManager = BEDataManagerFactory.GetDataManager<ICarrierAccountDataManager>();
+            bool insertActionSucc = dataManager.Insert(carrierAccount, out carrierAccountId);
+            if (insertActionSucc)
+            {
+                Vanrise.Caching.CacheManagerFactory.GetCacheManager<CacheManager>().SetCacheExpired();
+                carrierAccount.CarrierAccountId = carrierAccountId;
+                CarrierAccountDetail carrierAccountDetail = CarrierAccountDetailMapper(carrierAccount);
+                insertOperationOutput.Result = Vanrise.Entities.InsertOperationResult.Succeeded;
+                insertOperationOutput.InsertedObject = carrierAccountDetail;
+            }
+            else
+                insertOperationOutput.Result = Vanrise.Entities.InsertOperationResult.SameExists;
+
+
+            return insertOperationOutput;
+        }
+        public Vanrise.Entities.UpdateOperationOutput<CarrierAccountDetail> UpdateCarrierAccount(CarrierAccount carrierAccount)
+        {
+            ICarrierAccountDataManager dataManager = BEDataManagerFactory.GetDataManager<ICarrierAccountDataManager>();
+
+            bool updateActionSucc = dataManager.Update(carrierAccount);
+            Vanrise.Entities.UpdateOperationOutput<CarrierAccountDetail> updateOperationOutput = new Vanrise.Entities.UpdateOperationOutput<CarrierAccountDetail>();
+
+            updateOperationOutput.Result = Vanrise.Entities.UpdateOperationResult.Failed;
+            updateOperationOutput.UpdatedObject = null;
+
+            if (updateActionSucc)
+            {
+                Vanrise.Caching.CacheManagerFactory.GetCacheManager<CacheManager>().SetCacheExpired();
+                CarrierAccountDetail carrierAccountDetail = CarrierAccountDetailMapper(carrierAccount);
+                updateOperationOutput.Result = Vanrise.Entities.UpdateOperationResult.Succeeded;
+                updateOperationOutput.UpdatedObject = carrierAccountDetail;
+            }
+            else
+                updateOperationOutput.Result = Vanrise.Entities.UpdateOperationResult.SameExists;
+            return updateOperationOutput;
+        }
+        public int GetSellingNumberPlanId(int carrierAccountId, CarrierAccountType carrierAccountType)
+        {
+            if (carrierAccountType == CarrierAccountType.Supplier)
+                return -1;
+
+            return (int)this.GetCarrierAccount(carrierAccountId).SellingNumberPlanId;
+        }
+        public IEnumerable<CarrierAccount> GetNotAssignableCustomersToSellingProduct(int sellingProductId)
+        {
+            //SellingProductManager sellingProductManager = new SellingProductManager();
+            //SellingProduct sellingProduct = sellingProductManager.GetSellingProduct(sellingProductId);
+            //var cachedCarrierAccounts = GetCarrierAccountsByType(true, false, null, null);
+            //CustomerSellingProductManager customerSellingProductManager = new CustomerSellingProductManager();
+            //IEnumerable<CustomerSellingProduct> customerSellingProducts = customerSellingProductManager.GetEffectiveInFutureCustomerSellingProduct();
+            //return cachedCarrierAccounts.Where(x => x.SellingNumberPlanId == sellingProduct.SellingNumberPlanId
+            //    && (!customerSellingProducts.Any(y => y.CustomerId == x.CarrierAccountId)));
+            return null;
+
+        }
+       
+        public string GetCarrierAccountName(int carrierAccountId)
+        {
+            CarrierAccount carrierAccount = GetCarrierAccount(carrierAccountId);
+            if (carrierAccount == null)
+                return null;
+            string profileName = _carrierProfileManager.GetCarrierProfileName(carrierAccount.CarrierProfileId);
+            return GetCarrierAccountName(profileName, carrierAccount.NameSuffix);
+        }
+     
+        #endregion
+
+        #region Private Methods
+        private IEnumerable<CarrierAccount> GetCarriersAssignableToUserId(bool getCustomers, bool getSuppliers)
+        {
+            //AccountManagerManager AccountManagerManager = new AccountManagerManager();
+            //IEnumerable<CarrierAccount> carriers = GetCarrierAccountsByType(getCustomers, getSuppliers, null, null);
+            //IEnumerable<AssignedCarrier> assignedCarriers = AccountManagerManager.GetAssignedCarriers();
+
+            //Func<CarrierAccount, bool> filterExpression = (carrierAccount) =>
+            //{
+            //    if (carrierAccount.AccountType == CarrierAccountType.Exchange && assignedCarriers.Where(z => z.CarrierAccountId == carrierAccount.CarrierAccountId).Count() > 1)
+            //        return false;
+            //    if (carrierAccount.AccountType != CarrierAccountType.Exchange && assignedCarriers.Any(y => y.CarrierAccountId == carrierAccount.CarrierAccountId))
+            //        return false;
+            //    return true;
+
+            //};
+
+            //return carriers.FindAllRecords(filterExpression);
+            return null;
+        }
+        Dictionary<int, CarrierAccount> GetCachedCarrierAccounts()
+        {
+            return Vanrise.Caching.CacheManagerFactory.GetCacheManager<CacheManager>().GetOrCreateObject("GetCarrierAccounts",
+               () =>
+               {
+                   ICarrierAccountDataManager dataManager = BEDataManagerFactory.GetDataManager<ICarrierAccountDataManager>();
+                   IEnumerable<CarrierAccount> carrierAccounts = dataManager.GetCarrierAccounts();
+                   return carrierAccounts.ToDictionary(kvp => kvp.CarrierAccountId, kvp => kvp);
+               });
+        }
+        private class CacheManager : Vanrise.Caching.BaseCacheManager
+        {
+            ICarrierAccountDataManager _dataManager = BEDataManagerFactory.GetDataManager<ICarrierAccountDataManager>();
+            object _updateHandle;
+
+            protected override bool ShouldSetCacheExpired(object parameter)
+            {
+                return _dataManager.AreCarrierAccountsUpdated(ref _updateHandle);
+            }
+        }  
+       
+        private IEnumerable<CarrierAccount> GetCarrierAccountsByType(bool getCustomers, bool getSuppliers, SupplierFilterSettings supplierFilterSettings, CustomerFilterSettings customerFilterSettings)
+        {
+            return null;
+            //Dictionary<int, CarrierAccount> carrierAccounts = GetCachedCarrierAccounts();
+
+            //HashSet<int> filteredSupplierIds = SupplierGroupContext.GetFilteredSupplierIds(supplierFilterSettings);
+            //HashSet<int> filteredCustomerIds = CustomerGroupContext.GetFilteredCustomerIds(customerFilterSettings);
+            //Func<CarrierAccount, bool> filterExpression = (carrierAccount) =>
+            //{
+            //    bool isSupplier = carrierAccount.AccountType == CarrierAccountType.Supplier || carrierAccount.AccountType == CarrierAccountType.Exchange;
+            //    bool isCustomer = carrierAccount.AccountType == CarrierAccountType.Customer || carrierAccount.AccountType == CarrierAccountType.Exchange;
+            //    if (getCustomers && getSuppliers)
+            //        return true;
+            //    if (getCustomers && !isCustomer)
+            //        return false;
+            //    if (getSuppliers && !isSupplier)
+            //        return false;
+            //    if (isSupplier && filteredSupplierIds != null && !filteredSupplierIds.Contains(carrierAccount.CarrierAccountId))
+            //        return false;
+            //    if (isCustomer && filteredCustomerIds != null && !filteredCustomerIds.Contains(carrierAccount.CarrierAccountId))
+            //        return false;
+            //    return true;
+            //};
+           // return carrierAccounts.FindAllRecords(filterExpression);
+        }   
+        private static string GetCarrierAccountName(string profileName, string nameSuffix)
+        {
+            return string.Format("{0}{1}", profileName, string.IsNullOrEmpty(nameSuffix) ? string.Empty : " (" + nameSuffix + ")");
+        }
+
+        #endregion
+  
+        #region  Mappers
+        private CarrierAccountInfo CarrierAccountInfoMapper(CarrierAccount carrierAccount)
+        {
+            return new CarrierAccountInfo()
+            {
+                CarrierAccountId = carrierAccount.CarrierAccountId,
+                Name = GetCarrierAccountName(_carrierProfileManager.GetCarrierProfileName(carrierAccount.CarrierProfileId), carrierAccount.NameSuffix),
+            };
+        }
+       
+        private CarrierAccountDetail CarrierAccountDetailMapper(CarrierAccount carrierAccount)
+        {
+            CarrierAccountDetail carrierAccountDetail = new CarrierAccountDetail();
+            carrierAccountDetail.Entity = carrierAccount;
+
+            var carrierProfile = _carrierProfileManager.GetCarrierProfile(carrierAccount.CarrierProfileId);
+
+            if (carrierProfile != null)
+            {
+                carrierAccountDetail.CarrierProfileName = carrierProfile.Name;
+                carrierAccountDetail.CarrierAccountName = GetCarrierAccountName(carrierProfile.Name, carrierAccountDetail.Entity.NameSuffix);
+            }
+
+            carrierAccountDetail.AccountTypeDescription = carrierAccount.AccountType.ToString();
+
+            if (carrierAccount.SellingNumberPlanId != null)
+            {
+                //var sellingNumberPlan = _sellingNumberPlanManager.GetSellingNumberPlan((int)carrierAccount.SellingNumberPlanId);
+                //if (sellingNumberPlan != null)
+                //    carrierAccountDetail.SellingNumberPlanName = sellingNumberPlan.Name;
+            }
+
+            return carrierAccountDetail;
+        }
+        #endregion
+
+
+    }
+}
---
+++ Demo.project/Code/Demo.Module.Business/CarrierProfileManager.cs
@@ -0,0 +1,150 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Demo.Module.Data;
+using Demo.Module.Entities;
+using Vanrise.Common;
+using Vanrise.Common.Business;
+using Vanrise.Entities;
+
+namespace Demo.Module.Business
+{
+    public class CarrierProfileManager
+    {
+      
+        #region ctor/Local Variables
+        #endregion
+
+        #region Public Methods
+        public Vanrise.Entities.IDataRetrievalResult<CarrierProfileDetail> GetFilteredCarrierProfiles(Vanrise.Entities.DataRetrievalInput<CarrierProfileQuery> input)
+        {
+            var allCarrierProfiles = GetCachedCarrierProfiles();
+
+            Func<CarrierProfile, bool> filterExpression = (prod) =>
+                 (input.Query.Name == null || prod.Name.ToLower().Contains(input.Query.Name.ToLower()))
+                 &&
+
+                  (input.Query.Company == null || prod.Settings.Company.ToLower().Contains(input.Query.Company.ToLower()))
+                 &&
+
+                 (input.Query.CountriesIds == null || input.Query.CountriesIds.Count == 0 || input.Query.CountriesIds.Contains(prod.Settings.CountryId))
+                 &&
+                 (input.Query.CarrierProfileIds == null || input.Query.CarrierProfileIds.Contains(prod.CarrierProfileId));
+
+
+            return Vanrise.Common.DataRetrievalManager.Instance.ProcessResult(input, allCarrierProfiles.ToBigResult(input, filterExpression, CarrierProfileDetailMapper));
+        }
+        public CarrierProfile GetCarrierProfile(int carrierProfileId)
+        {
+            var carrierProfiles = GetCachedCarrierProfiles();
+            return carrierProfiles.GetRecord(carrierProfileId);
+        }
+        public string GetCarrierProfileName(int carrierProfileId)
+        {
+            CarrierProfile carrierProfile = GetCarrierProfile(carrierProfileId);
+            return carrierProfile != null ? carrierProfile.Name : null;
+        }
+        public IEnumerable<CarrierProfileInfo> GetCarrierProfilesInfo()
+        {
+            var carrierProfiles = GetCachedCarrierProfiles();
+            return carrierProfiles.MapRecords(CarrierProfileInfoMapper);
+        }
+        public Vanrise.Entities.InsertOperationOutput<CarrierProfileDetail> AddCarrierProfile(CarrierProfile carrierProfile)
+        {
+            Vanrise.Entities.InsertOperationOutput<CarrierProfileDetail> insertOperationOutput = new Vanrise.Entities.InsertOperationOutput<CarrierProfileDetail>();
+
+            insertOperationOutput.Result = Vanrise.Entities.InsertOperationResult.Failed;
+            insertOperationOutput.InsertedObject = null;
+
+            int carrierProfileId = -1;
+
+            ICarrierProfileDataManager dataManager = BEDataManagerFactory.GetDataManager<ICarrierProfileDataManager>();
+            bool insertActionSucc = dataManager.Insert(carrierProfile, out carrierProfileId);
+            if (insertActionSucc)
+            {
+                Vanrise.Caching.CacheManagerFactory.GetCacheManager<CacheManager>().SetCacheExpired();
+                insertOperationOutput.Result = Vanrise.Entities.InsertOperationResult.Succeeded;
+                carrierProfile.CarrierProfileId = carrierProfileId;
+                insertOperationOutput.InsertedObject = CarrierProfileDetailMapper(carrierProfile);
+            }
+            else
+                insertOperationOutput.Result = Vanrise.Entities.InsertOperationResult.SameExists;
+
+            return insertOperationOutput;
+        }
+        public Vanrise.Entities.InsertOperationOutput<CarrierProfileDetail> UpdateCarrierProfile(CarrierProfile carrierProfile)
+        {
+            //ICarrierProfileDataManager dataManager = BEDataManagerFactory.GetDataManager<ICarrierProfileDataManager>();
+
+            //bool updateActionSucc = dataManager.Update(carrierProfile);
+            //Vanrise.Entities.InsertOperationOutput<CarrierProfileDetail> updateOperationOutput = new Vanrise.Entities.InsertOperationOutput<CarrierProfileDetail>();
+
+            //updateOperationOutput.Result = UpdateOperationResult.Failed;
+            //updateOperationOutput.UpdatedObject = null;
+
+            //if (updateActionSucc)
+            //{
+            //    Vanrise.Caching.CacheManagerFactory.GetCacheManager<CacheManager>().SetCacheExpired();
+            //    updateOperationOutput.Result = Vanrise.Entities.UpdateOperationResult.Succeeded;
+            //    updateOperationOutput.UpdatedObject = CarrierProfileDetailMapper(carrierProfile);
+            //}
+            //else
+            //    updateOperationOutput.Result = Vanrise.Entities.UpdateOperationResult.SameExists;
+            //return updateOperationOutput;
+            return null;
+        }
+        #endregion
+
+        #region Private Members
+        private Dictionary<int, CarrierProfile> GetCachedCarrierProfiles()
+        {
+            return Vanrise.Caching.CacheManagerFactory.GetCacheManager<CacheManager>().GetOrCreateObject("GetCarrierProfiles",
+               () =>
+               {
+                   ICarrierProfileDataManager dataManager = BEDataManagerFactory.GetDataManager<ICarrierProfileDataManager>();
+                   IEnumerable<CarrierProfile> carrierProfiles = dataManager.GetCarrierProfiles();
+                   return carrierProfiles.ToDictionary(cn => cn.CarrierProfileId, cn => cn);
+               });
+        }
+        private class CacheManager : Vanrise.Caching.BaseCacheManager
+        {
+            ICarrierProfileDataManager _dataManager = BEDataManagerFactory.GetDataManager<ICarrierProfileDataManager>();
+            object _updateHandle;
+
+            protected override bool ShouldSetCacheExpired(object parameter)
+            {
+                return _dataManager.AreCarrierProfilesUpdated(ref _updateHandle);
+            }
+        }
+
+        #endregion
+     
+        #region  Mappers
+        private CarrierProfileInfo CarrierProfileInfoMapper(CarrierProfile carrierProfile)
+        {
+            return new CarrierProfileInfo()
+            {
+                CarrierProfileId = carrierProfile.CarrierProfileId,
+                Name = carrierProfile.Name,
+            };
+        }
+        private CarrierProfileDetail CarrierProfileDetailMapper(CarrierProfile carrierProfile)
+        {
+            CarrierProfileDetail carrierProfileDetail = new CarrierProfileDetail();
+
+            carrierProfileDetail.Entity = carrierProfile;
+
+            CountryManager manager = new CountryManager();
+            if (carrierProfile.Settings != null)
+            {
+                carrierProfileDetail.CountryName = manager.GetCountry(carrierProfile.Settings.CountryId).Name;
+            }
+
+            return carrierProfileDetail;
+        }
+        #endregion
+    }
+
+}
