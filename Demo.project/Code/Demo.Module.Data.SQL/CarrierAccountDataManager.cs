75d0792f721563f91f7aa1baafee052e55402591
walid.mogharbel <walid.mogharbel@vanrise.tfs.local>
2016-02-16 08:12:34 +0000

    check in skeleton
    git-tfs-id: [http://developmentvlan:8585/tfs/vanrise.collection]$/;C30664

---
+++ Demo.project/Code/Demo.Module.Data.SQL/CarrierAccountDataManager.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Demo.Module.Entities;
+using Vanrise.Data.SQL;
+
+namespace Demo.Module.Data.SQL
+{
+    public class CarrierAccountDataManager : BaseSQLDataManager, ICarrierAccountDataManager
+    {
+
+        #region ctor/Local Variables
+        public CarrierAccountDataManager(): base(GetConnectionStringName("TOneWhS_BE_DBConnStringKey", "TOneWhS_BE_DBConnString"))
+        {
+        }
+        #endregion
+
+        #region Public Methods
+        public bool Insert(CarrierAccount carrierAccount, out int insertedId)
+        {
+
+            object carrierAccountId;
+
+            int recordsEffected = ExecuteNonQuerySP("TOneWhS_BE.sp_CarrierAccount_Insert", out carrierAccountId, carrierAccount.NameSuffix, carrierAccount.CarrierProfileId, carrierAccount.AccountType, carrierAccount.SellingNumberPlanId, Vanrise.Common.Serializer.Serialize(carrierAccount.CustomerSettings),
+                Vanrise.Common.Serializer.Serialize(carrierAccount.SupplierSettings), Vanrise.Common.Serializer.Serialize(carrierAccount.CarrierAccountSettings));
+            insertedId = (int)carrierAccountId;
+            return (recordsEffected > 0);
+        }
+        public bool Update(CarrierAccount carrierAccount)
+        {
+            int recordsEffected = ExecuteNonQuerySP("TOneWhS_BE.sp_CarrierAccount_Update", carrierAccount.CarrierAccountId, carrierAccount.NameSuffix, carrierAccount.CarrierProfileId, carrierAccount.AccountType, Vanrise.Common.Serializer.Serialize(carrierAccount.CustomerSettings),
+                 Vanrise.Common.Serializer.Serialize(carrierAccount.SupplierSettings), Vanrise.Common.Serializer.Serialize(carrierAccount.CarrierAccountSettings));
+            return (recordsEffected > 0);
+        }
+        public List<CarrierAccount> GetCarrierAccounts()
+        {
+            return GetItemsSP("TOneWhS_BE.sp_CarrierAccount_GetAll", CarrierAccountMapper);
+        }
+        public bool AreCarrierAccountsUpdated(ref object updateHandle)
+        {
+            return base.IsDataUpdated("TOneWhS_BE.CarrierAccount", ref updateHandle);
+        }
+        #endregion
+
+
+        #region  Mappers
+        private CarrierAccount CarrierAccountMapper(IDataReader reader)
+        {
+            CarrierAccount carrierAccount = new CarrierAccount
+            {
+                CarrierAccountId = (int)reader["ID"],
+                NameSuffix = reader["NameSuffix"] as string,
+                AccountType = (CarrierAccountType)GetReaderValue<int>(reader, "AccountType"),
+                SupplierSettings = Vanrise.Common.Serializer.Deserialize<Entities.CarrierAccountSupplierSettings>(reader["SupplierSettings"] as string),
+                CustomerSettings = Vanrise.Common.Serializer.Deserialize<Entities.CarrierAccountCustomerSettings>(reader["CustomerSettings"] as string),
+                SellingNumberPlanId = GetReaderValue<int?>(reader, "SellingNumberPlanID"),
+                CarrierProfileId = (int)reader["CarrierProfileId"],
+                CarrierAccountSettings = Vanrise.Common.Serializer.Deserialize<Entities.CarrierAccountSettings>(reader["CarrierAccountSettings"] as string),
+
+            };
+            return carrierAccount;
+        }
+        #endregion
+
+    }
+}
---
+++ Demo.project/Code/Demo.Module.Data.SQL/CarrierProfileDataManager.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Demo.Module.Entities;
+using Vanrise.Data.SQL;
+
+namespace Demo.Module.Data.SQL
+{
+    public class CarrierProfileDataManager : BaseSQLDataManager, ICarrierProfileDataManager
+    {
+   
+        #region ctor/Local Variables
+        public CarrierProfileDataManager()
+            : base(GetConnectionStringName("TOneWhS_BE_DBConnStringKey", "TOneWhS_BE_DBConnString"))
+        {
+
+        }
+        #endregion
+
+        #region Public Methods
+        public bool Insert(CarrierProfile carrierProfile, out int insertedId)
+        {
+            object carrierProfileId;
+
+            int recordsEffected = ExecuteNonQuerySP("TOneWhS_BE.sp_CarrierProfile_Insert", out carrierProfileId, carrierProfile.Name, Vanrise.Common.Serializer.Serialize(carrierProfile.Settings));
+            bool insertedSuccesfully = (recordsEffected > 0);
+            if (insertedSuccesfully)
+                insertedId = (int)carrierProfileId;
+            else
+                insertedId = 0;
+            return insertedSuccesfully;
+        }
+        public bool Update(CarrierProfile carrierProfile)
+        {
+            int recordsEffected = ExecuteNonQuerySP("TOneWhS_BE.sp_CarrierProfile_Update", carrierProfile.CarrierProfileId, carrierProfile.Name, Vanrise.Common.Serializer.Serialize(carrierProfile.Settings));
+            return (recordsEffected > 0);
+        }
+        public bool AreCarrierProfilesUpdated(ref object updateHandle)
+        {
+            return base.IsDataUpdated("TOneWhS_BE.CarrierProfile", ref updateHandle);
+        }
+        public List<CarrierProfile> GetCarrierProfiles()
+        {
+            return GetItemsSP("TOneWhS_BE.sp_CarrierProfile_GetAll", CarrierProfileMapper);
+        }
+        #endregion
+
+        #region Private Methods
+
+        #endregion
+
+        #region  Mappers
+        private CarrierProfile CarrierProfileMapper(IDataReader reader)
+        {
+            CarrierProfile carrierProfile = new CarrierProfile
+            {
+                CarrierProfileId = (int)reader["ID"],
+                Name = reader["Name"] as string,
+                Settings = Vanrise.Common.Serializer.Deserialize<CarrierProfileSettings>(reader["Settings"] as string)
+            };
+            return carrierProfile;
+        }
+
+        #endregion
+      
+    }
+}
