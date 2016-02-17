10080ad473d4e1cfed0fe27e2e9f7c644314ab62
walid.mogharbel <walid.mogharbel@vanrise.tfs.local>
2016-02-17 10:32:57 +0000

    added operator declared information entities
    git-tfs-id: [http://developmentvlan:8585/tfs/vanrise.collection]$/;C30717

---
+++ Demo.project/Code/Demo.Module.Entities/OperatorDeclaredInformation/OperatorDeclaredInformation.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Demo.Module.Entities
+{
+    public class OperatorDeclaredInformation
+    {
+        public int OperatorDeclaredInformationId { get; set; }
+
+        public int OperatorId { get; set; }
+
+        public DateTime? FromDate { get; set; }
+
+        public DateTime? ToDate { get; set; }
+
+        public OperatorDeclaredInformationSettings Settings { get; set;  }
+    }
+    public class OperatorDeclaredInformationSettings
+    {
+        public List<string> Range { get; set; }
+
+    }
+}
---
+++ Demo.project/Code/Demo.Module.Entities/OperatorDeclaredInformation/OperatorDeclaredInformationQuery.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Demo.Module.Entities
+{
+   public class OperatorDeclaredInformationQuery
+    {
+       public List<int> OperatorDeclaredInformationIds { get; set; }
+
+       public DateTime? FromDate { get; set; }
+
+       public DateTime? ToDate { get; set; }
+
+       public List<int> OperatorIds { get; set; }
+
+    }
+}
