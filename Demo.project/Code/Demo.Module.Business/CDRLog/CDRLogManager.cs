aa5cac5e14c12ac4539ba5f4e30232b31e0c860f
Ali Ballouk <VANRISE\ali.ballouk>
2016-02-18 10:01:00 +0000

    Main CDR Log basic implemenation
    git-tfs-id: [http://developmentvlan:8585/tfs/vanrise.collection]$/;C30747

---
+++ Demo.project/Code/Demo.Module.Business/CDRLog/CDRLogManager.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Demo.Module.Data;
+using Demo.Module.Entities;
+using Vanrise.Entities;
+using Vanrise.Common;
+using Vanrise.Integration.Business;
+using Vanrise.Integration.Entities;
+using System.ComponentModel;
+namespace Demo.Module.Business
+{
+    public class CDRLogManager
+    {
+       public Vanrise.Entities.IDataRetrievalResult<CDRLogDetail> GetCDRLogData(Vanrise.Entities.DataRetrievalInput<CDRQuery> input)
+        {
+            ICDRLogDataManager datamanager = DemoModuleDataManagerFactory.GetDataManager<ICDRLogDataManager>();
+            var cdrLogResult = datamanager.GetCDRLogData(input);
+            BigResult<CDRLogDetail> cDRLogBigResultDetailMapper = new BigResult<CDRLogDetail>
+            {
+                Data = cdrLogResult.Data.MapRecords(CDRLogDetailMapper),
+                ResultKey = cdrLogResult.ResultKey,
+                TotalCount = cdrLogResult.TotalCount
+            };
+            return Vanrise.Common.DataRetrievalManager.Instance.ProcessResult(input, cDRLogBigResultDetailMapper);
+        }
+       public CDRLogDetail CDRLogDetailMapper(CDRLog cDRLog)
+        {
+            string dataSourceName = "";
+            DataSourceDetail dataSource = new DataSourceManager().GetDataSource(cDRLog.DataSourceId);
+            if (dataSource != null)
+                dataSourceName = dataSource.Name;
+
+            ServiceType serviceType = new ServiceTypeManager().GetServiceType(cDRLog.ServiceTypeId);
+            string serviceTypeDescription = "";
+            if (serviceType != null)
+                serviceTypeDescription = serviceType.Description;
+
+            CDRLogDetail cDRLogDetail = new CDRLogDetail
+            {
+                Entity = cDRLog,
+                DataSourceName = dataSourceName,
+                ServiceTypeDescription = serviceTypeDescription
+
+            };
+
+            var directionAttribute = Utilities.GetEnumAttribute<Direction, DescriptionAttribute>(cDRLog.DirectionType);
+            if (directionAttribute != null)
+                cDRLogDetail.DirectionDescription = directionAttribute.Description;
+
+            var cdrType = Utilities.GetEnumAttribute<Demo.Module.Entities.Type, DescriptionAttribute>(cDRLog.CDRType);
+            if (cdrType != null)
+                cDRLogDetail.CDRTypeDescription = cdrType.Description;
+
+            return cDRLogDetail;
+        }
+    }
+}
---
+++ Demo.project/Code/Demo.Module.Web/Demo_Module/Services/APIServices/CDRLogAPIService.js
@@ -0,0 +1,19 @@
+﻿(function (appControllers) {
+
+    "use strict";
+    cdrLogAPIService.$inject = ['BaseAPIService', 'UtilsService', 'Demo_ModuleConfig'];
+
+    function cdrLogAPIService(BaseAPIService, UtilsService, Demo_ModuleConfig) {
+
+        function GetCDRLogData(input) {
+
+            return BaseAPIService.post(UtilsService.getServiceURL(Demo_ModuleConfig.moduleName, "CDRLog", "GetCDRLogData"), input);
+        }
+
+        return ({
+            GetCDRLogData: GetCDRLogData
+        });
+    }
+
+    appControllers.service('WhS_Analytics_CDRLogAPIService', cdrLogAPIService);
+})(appControllers);
