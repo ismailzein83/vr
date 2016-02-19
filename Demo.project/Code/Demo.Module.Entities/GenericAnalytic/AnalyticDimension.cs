f9afcdbe77ed5940614f9c367bb29a4a548ba18a
Ali Ballouk <VANRISE\ali.ballouk>
2016-02-19 14:04:06 +0000

    TrafficMonitor
    git-tfs-id: [http://developmentvlan:8585/tfs/vanrise.collection]$/;C30793

---
+++ Demo.project/Code/Demo.Module.Entities/GenericAnalytic/AnalyticDimension.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Demo.Module.Entities
+{
+    public enum AnalyticDimension
+    {
+        Operator = 0,
+        Zone = 1,
+        Country = 2,
+        GateWayIn = 3,
+        GateWayOut = 4,
+        PortIn = 7,
+        PortOut = 5,
+        Date = 6,
+        Hour = 7,
+        Currency = 8,
+        Day =9,
+        Week = 10,
+        Month = 11,
+        ServiceType = 12 ,
+        Direction = 13 ,
+        CDRType = 14
+    }
+
+    public enum AnalyticSummary
+    {
+        Sum,
+        Max,
+        Avg
+    }
+}
---
+++ Demo.project/Code/Demo.Module.Entities/GenericAnalytic/AnalyticRecord.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Demo.Module.Entities
+{
+    public class AnalyticRecord
+    {
+        public AnalyticDimensionValue[] DimensionValues { get; set; }
+
+        //public Object[] MeasureValues { get; set; }
+
+        public Dictionary<AnalyticMeasureField,Object> MeasureValues { get; set; }
+    }
+}
