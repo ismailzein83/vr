7534b28c72fd279c8613257562fc292949d293ab
Ismail Zein <VANRISE\ismail.zein>
2016-02-22 12:41:10 +0000

    implemented multiple runtime instances
    git-tfs-id: [http://developmentvlan:8585/tfs/vanrise.collection]$/;C30826

--- TOneV2/Code/TOneV2/TestRuntime/Program.cs
+++ TOneV2/Code/TOneV2/TestRuntime/Program.cs
@@ -20,19 +20,7 @@ namespace TestRuntime
         {
             if (ConfigurationManager.AppSettings["IsRuntimeService"] == "true")
             {
-                BusinessProcessService bpService = new BusinessProcessService() { Interval = new TimeSpan(0, 0, 2) };
-                QueueActivationService queueActivationService = new QueueActivationService() { Interval = new TimeSpan(0, 0, 2) };
-                SchedulerService schedulerService = new SchedulerService() { Interval = new TimeSpan(0, 0, 5) };
-
-                var runtimeServices = new List<RuntimeService>();
-
-                runtimeServices.Add(queueActivationService);
-
-                runtimeServices.Add(bpService);
-
-                runtimeServices.Add(schedulerService);
-
-                RuntimeHost host = new RuntimeHost(runtimeServices);
+                RuntimeHost host = new RuntimeHost(args);
                 host.Start();
                 Console.ReadKey();
             }
