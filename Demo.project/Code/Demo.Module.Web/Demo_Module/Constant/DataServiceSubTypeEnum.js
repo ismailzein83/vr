f1f01603e0496d5b17696490ba1be488b53275e4
walid.mogharbel <walid.mogharbel@vanrise.tfs.local>
2016-02-23 14:53:03 +0000

    added other sub types
    git-tfs-id: [http://developmentvlan:8585/tfs/vanrise.collection]$/;C30870

---
+++ Demo.project/Code/Demo.Module.Web/Demo_Module/Constant/DataServiceSubTypeEnum.js
@@ -0,0 +1,6 @@
+﻿app.constant('Demo_DataSubTypeEnum', {
+    Peak: { value: 1, description: "Audio" },
+    OffPeak: { value: 2, description: "Audio & Video" },
+    OffPeak1: { value: 3, description: "Premium" },
+    OffPeak2: { value: 4, description: "RingBackTone" }
+});
