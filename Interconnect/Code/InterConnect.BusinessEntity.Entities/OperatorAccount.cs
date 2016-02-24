163094cf826ad1fe90d7a24b91732743f643647d
Ismail Zein <VANRISE\ismail.zein>
2016-02-24 09:56:40 +0000

    added OperatorAccount
    git-tfs-id: [http://developmentvlan:8585/tfs/vanrise.collection]$/;C30893

---
+++ Interconnect/Code/InterConnect.BusinessEntity.Entities/OperatorAccount.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace InterConnect.BusinessEntity.Entities
+{
+    public class OperatorAccount
+    {
+        public string Suffix { get; set; }
+
+        public int ProfileId { get; set; }
+
+        public OperatorAccountSettings Settings { get; set; }
+    }
+}
