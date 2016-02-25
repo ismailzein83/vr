947105df218a19253073ef8588b60b3cd4bf583b
mohamad.elhariri <mohamad.elhariri@vanrise.tfs.local>
2016-02-25 08:23:30 +0000

    Refactored GetSystemActionName to handle the case when a controller doesn't implement an attribute
    git-tfs-id: [http://developmentvlan:8585/tfs/vanrise.collection]$/;C30922

--- Vanrise/Code/Vanrise.Web/App_Start/AuthorizationFilter.cs
+++ Vanrise/Code/Vanrise.Web/App_Start/AuthorizationFilter.cs
@@ -31,7 +31,11 @@ namespace Vanrise.Web.App_Start
             var actionMethod = controller.GetMethods().FindRecord(itm => itm.Name == actionContext.ActionDescriptor.ActionName);
             var routeAttribute = actionMethod.GetCustomAttribute(typeof(System.Web.Http.RouteAttribute)) as System.Web.Http.RouteAttribute;
 
-            return String.Format("{0}/{1}", routePrefixAttribute.Prefix.Replace("api/", String.Empty), routeAttribute.Template);
+            // This check was added in case the controller doesn't apply the RoutePrefix and/or Route attributes
+            // If either attribute is null, an empty string is returned to GetSystemAction as it expects a key != null
+            // This check should be removed after all system actions have been defined in the database, and a specific exception should be thrown if the system action wasn't found
+            return (routePrefixAttribute != null && routeAttribute != null) ?
+                String.Format("{0}/{1}", routePrefixAttribute.Prefix.Replace("api/", String.Empty), routeAttribute.Template) : String.Empty;
         }
     }
 }
