7cd9562429284dbc2defaa355f254e94e3cbf6ce
Ismail Zein <VANRISE\ismail.zein>
2016-02-26 08:09:20 +0000

    added Antony task
    git-tfs-id: [http://developmentvlan:8585/tfs/vanrise.collection]$/;C30968

---
+++ TOneV2/Code/TOneV2/TestRuntime/Tasks/AnthonyTask.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Vanrise.BusinessProcess;
+using Vanrise.Runtime;
+
+namespace TestRuntime.Tasks
+{
+    public class AnthonyTask : ITask
+    {
+        public void Execute()
+        {
+            BusinessProcessService bpService = new BusinessProcessService() { Interval = new TimeSpan(0, 0, 2) };
+            var runtimeServices = new List<RuntimeService>();
+            runtimeServices.Add(bpService);
+
+            RuntimeHost host = new RuntimeHost(runtimeServices);
+            host.Start();
+            Console.ReadKey();
+        }
+    }
+}
