70b961ed9521eb74383951f9804363c64ab2f27f
Zeinab Mokdad <VANRISE\zeinab.mokdad>
2016-02-26 14:10:52 +0000

    adding customer directive
    git-tfs-id: [http://developmentvlan:8585/tfs/vanrise.collection]$/;C30991

--- CarrierPortal/Code/CP.SupplierPricelist.Web/CP_SupplierPricelist/Views/SupplierPriceList/SupplierPriceListManagementController.js
+++ CarrierPortal/Code/CP.SupplierPricelist.Web/CP_SupplierPricelist/Views/SupplierPriceList/SupplierPriceListManagementController.js
@@ -4,13 +4,17 @@
 
         function supplierPriceListManagementController($scope, utilsService, vrNotificationService, vruiUtilsService, priceListTypeEnum, supplierPriceListAPIService) {
             var gridAPI;
+            var customerDirectiveApi;
+            var customerReadyPromiseDeferred = utilsService.createPromiseDeferred();
             function importPriceList() {
                 var priceListObject = {
                     FileId: $scope.file.fileId,
                     PriceListType: $scope.selectedpriceListType.pricelistID,
                     EffectiveOnDate: $scope.effectiveOn,
                     UserId: 1,
-                    Satus: 0
+                    Satus: 0,
+                    CustomerId: 1,
+                    CarrierAccountId: 'C169'
                 };
                 supplierPriceListAPIService.importPriceList(priceListObject);
             }
@@ -34,12 +38,30 @@
                 }
                 );
             }
-
-            function load() {
-                loadPriceListType();
+            $scope.onCustomerDirectiveReady = function (api) {
+                customerDirectiveApi = api;
+                customerReadyPromiseDeferred.resolve();
             }
-
+            function load() {
+                loadAllControls();
+            }
+            function loadCustomer() {
+                var customerLoadPromiseDeferred = utilsService.createPromiseDeferred();
+                customerReadyPromiseDeferred.promise.then(function () {
+                    vruiUtilsService.callDirectiveLoad(customerDirectiveApi, { filter: { AssignedToCurrentSupplier: true } }, customerLoadPromiseDeferred);
+                });
+                return customerLoadPromiseDeferred.promise;
+            }
             load();
+            function loadAllControls() {
+                return utilsService.waitMultipleAsyncOperations([loadCustomer, loadPriceListType])
+                   .catch(function (error) {
+                       vrNotificationService.notifyExceptionWithClose(error, $scope);
+                   })
+                  .finally(function () {
+                      $scope.isLoadingFilters = false;
+                  });
+            }
         }
 
         supplierPriceListManagementController.$inject = ['$scope', 'UtilsService', 'VRNotificationService', 'VRUIUtilsService', 'CP_SupplierPriceList_PriceListTypeEnum', 'CP_SupplierPricelist_SupplierPriceListAPIService'];
