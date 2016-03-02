15d312119e925a6fb133d069e15fd78070002132
Wissam Ajamy <VANRISE\wissam.ajamy>
2016-03-02 13:48:24 +0000

    update data values
    git-tfs-id: [http://developmentvlan:8585/tfs/vanrise.collection]$/;C31107

--- TOneV2/Code/TOneV2/SQL.TOneConfiguration/CarrierPortal.PostDeployment.sql
+++ TOneV2/Code/TOneV2/SQL.TOneConfiguration/CarrierPortal.PostDeployment.sql
@@ -37,79 +37,84 @@ set identity_insert [sec].[Module] off;
 ----------------------------------------------------------------------------------------------------
 set nocount on;
 set identity_insert [sec].[View] on;
-;with cte_data([Id],[Name],[Title],[Url],[Module],[RequiredPermissions],[Audience],[Content],[Type],[Rank])
+;with cte_data([Id],[Name],[Title],[Url],[Module],[ActionNames],[Audience],[Content],[Settings],[Type],[Rank])
 as (select * from (values
 --//////////////////////////////////////////////////////////////////////////////////////////////////
-(1,'Users','Users','#/view/Security/Views/User/UserManagement',1,'Root/Administration Module/Users:View',null,null,null,null,0,10),
-(2,'Groups','Groups','#/view/Security/Views/Group/GroupManagement',1,'Root/Administration Module/Groups:View',null,null,null,null,0,11),
-(3,'System Entities','System Entities','#/view/Security/Views/Permission/BusinessEntityManagement',1,'Root/Administration Module/System Entities:View',null,null,null,null,0,12),
-(5,'Ranking Pages','Ranking Pages','#/view/Security/Views/Pages/RankingPageManagement',1,'Root/Administration Module:View',null,null,null,null,0,13),
-(6,'Upload Pricelist','Upload Pricelist','#/view/CP_SupplierPricelist/Views/SupplierPriceList/SupplierPriceListManagement',2,null,null,null,null,null,0,10),
-(7,'Scheduler Service','Scheduler Service','#/view/Runtime/Views/SchedulerTaskManagement',1,null,null,null,null,null,0,14),
-(8,'Supplier Mapping','Supplier Mapping','#/view/CP_SupplierPricelist/Views/SupplierMapping/SupplierMappingManagement',2,null,null,null,null,null,0,11),
-(9,'Customers','Customers Management','#/view/CP_SupplierPricelist/Views/Customer/CustomerManagement',1,null,null,null,null,null,0,15)
+(1,'Users','Users','#/view/Security/Views/User/UserManagement',1,'VR_Sec/Users/GetFilteredUsers',null,null,null,0,10),
+(2,'Groups','Groups','#/view/Security/Views/Group/GroupManagement',1,'VR_Sec/Group/GetFilteredGroups',null,null,null,0,11),
+(3,'System Entities','System Entities','#/view/Security/Views/Permission/BusinessEntityManagement',1,'VR_Sec/BusinessEntityNode/GetEntityNodes',null,null,null,0,12),
+(5,'Ranking Pages','Ranking Pages','#/view/Security/Views/Pages/RankingPageManagement',1,'VR_Sec/Menu/GetMenuItems',null,null,null,0,13),
+(6,'Upload Pricelist','Upload Pricelist','#/view/CP_SupplierPricelist/Views/SupplierPriceList/SupplierPriceListManagement',2,'CP_SupPriceList/PriceList/GetUpdated',null,null,null,0,10),
+(7,'Scheduler Service','Scheduler Service','#/view/Runtime/Views/SchedulerTaskManagement',1,'VR_Runtime/SchedulerTask/GetFilteredTasks',null,null,null,0,14),
+(8,'Supplier Mapping','Supplier Mapping','#/view/CP_SupplierPricelist/Views/SupplierMapping/SupplierMappingManagement',2,'CP_SupPriceList/SupplierMapping/GetFilteredCustomerSupplierMappings',null,null,null,0,11),
+(9,'Customers','Customers Management','#/view/CP_SupplierPricelist/Views/Customer/CustomerManagement',1,'CP_SupPriceList/Customer/GetFilteredCustomers',null,null,null,0,15)
 --\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\
-)c([Id],[Name],[Title],[Url],[Module],[RequiredPermissions],[Audience],[Content],[Type],[Rank]))
+)c([Id],[Name],[Title],[Url],[Module],[ActionNames],[Audience],[Content],[Settings],[Type],[Rank]))
 merge	[sec].[View] as t
 using	cte_data as s
 on		1=1 and t.[Id] = s.[Id]
 when matched then
 	update set
-	[Name] = s.[Name],[Title] = s.[Title],[Url] = s.[Url],[Module] = s.[Module],[RequiredPermissions] = s.[RequiredPermissions],[Audience] = s.[Audience],[Content] = s.[Content],[Type] = s.[Type],[Rank] = s.[Rank]
+	[Name] = s.[Name],[Title] = s.[Title],[Url] = s.[Url],[Module] = s.[Module],[ActionNames] = s.[ActionNames],[Audience] = s.[Audience],[Content] = s.[Content],[Settings] = s.[Settings],[Type] = s.[Type],[Rank] = s.[Rank]
 when not matched by target then
-	insert([Id],[Name],[Title],[Url],[Module],[RequiredPermissions],[Audience],[Content],[Type],[Rank])
-	values(s.[Id],s.[Name],s.[Title],s.[Url],s.[Module],s.[RequiredPermissions],s.[Audience],s.[Content],s.[Type],s.[Rank])
+	insert([Id],[Name],[Title],[Url],[Module],[ActionNames],[Audience],[Content],[Settings],[Type],[Rank])
+	values(s.[Id],s.[Name],s.[Title],s.[Url],s.[Module],s.[ActionNames],s.[Audience],s.[Content],s.[Settings],s.[Type],s.[Rank])
 when not matched by source then
 	delete;
 set identity_insert [sec].[View] off;
 
---[sec].[BusinessEntityModule]-----
+--sec.BusinessEntityModule--------------------------------------------------------------------------
+----------------------------------------------------------------------------------------------------
 set nocount on;
 set identity_insert [sec].[BusinessEntityModule] on;
-;with cte_data([Id],[Name],[ParentId],[BreakInheritance],[PermissionOptions])
+;with cte_data([Id],[Name],[Title],[ParentId],[BreakInheritance],[PermissionOptions])
 as (select * from (values
 --//////////////////////////////////////////////////////////////////////////////////////////////////
-(1,'Root',null,0,'["View","Full Control"]'),
-(6,'Business Process Module',1,0,'["View"]'),
-(8,'Administration Module',1,0,'["View","Add","Edit", "Delete", "Full Control"]')
+(1,'Root','Root',null,0,'["View","Full Control"]'),
+(6,'Business Process Module','Business Process Module',1,0,'["View"]'),
+(8,'Administration Module','Administration Module',1,0,'["View","Add","Edit", "Delete", "Full Control"]')
 --\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\
-)c([Id],[Name],[ParentId],[BreakInheritance],[PermissionOptions]))
+)c([Id],[Name],[Title],[ParentId],[BreakInheritance],[PermissionOptions]))
 merge	[sec].[BusinessEntityModule] as t
 using	cte_data as s
 on		1=1 and t.[Id] = s.[Id]
 when matched then
 	update set
-	[Name] = s.[Name],[ParentId] = s.[ParentId],[BreakInheritance] = s.[BreakInheritance],[PermissionOptions] = s.[PermissionOptions]
+	[Name] = s.[Name],[Title] = s.[Title],[ParentId] = s.[ParentId],[BreakInheritance] = s.[BreakInheritance],[PermissionOptions] = s.[PermissionOptions]
 when not matched by target then
-	insert([Id],[Name],[ParentId],[BreakInheritance],[PermissionOptions])
-	values(s.[Id],s.[Name],s.[ParentId],s.[BreakInheritance],s.[PermissionOptions])
+	insert([Id],[Name],[Title],[ParentId],[BreakInheritance],[PermissionOptions])
+	values(s.[Id],s.[Name],s.[Title],s.[ParentId],s.[BreakInheritance],s.[PermissionOptions])
 when not matched by source then
 	delete;
 set identity_insert [sec].[BusinessEntityModule] off;
 
 
---[sec].[BusinessEntity]-----
+--sec.BusinessEntity--------------------------------------------------------------------------------
+----------------------------------------------------------------------------------------------------
 set nocount on;
 set identity_insert [sec].[BusinessEntity] on;
-;with cte_data([Id],[Name],[ModuleId],[BreakInheritance],[PermissionOptions])
+;with cte_data([Id],[Name],[Title],[ModuleId],[BreakInheritance],[PermissionOptions])
 as (select * from (values
 --//////////////////////////////////////////////////////////////////////////////////////////////////
-(1,'Users',8,0,'["View", "Add", "Edit", "Reset Password"]'),
-(2,'Groups',8,0,'["View", "Add", "Edit", "Delete"]'),
-(3,'System Entities',8,1,'["View", "Assign Permissions"]'),
-(4,'History',6,0,'["View"]'),
-(5,'Managment',6,0,'["View"]')
+(1,'Users','Users',8,0,'["View", "Add", "Edit", "Reset Password"]'),
+(2,'Groups','Groups',8,0,'["View", "Add", "Edit", "Delete"]'),
+(3,'System Entities','System Entities',8,1,'["View", "Assign Permissions"]'),
+(4,'History','History',6,0,'["View"]'),
+(5,'Managment','Managment',6,0,'["View"]'),
+(8,'CP_SupPriceList_Customer','Customer',1,0,'["View", "Add","Edit", "Assign/Unassign User"]'),
+(14,'CP_SupPriceList_SupplierMapping','Supplier Mapping',1,0,'["View", "Add/Edit"]'),
+(15,'CP_SupPriceList_PriceList','PriceList',1,0,'["View", "Import PriceList"]')
 --\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\
-)c([Id],[Name],[ModuleId],[BreakInheritance],[PermissionOptions]))
+)c([Id],[Name],[Title],[ModuleId],[BreakInheritance],[PermissionOptions]))
 merge	[sec].[BusinessEntity] as t
 using	cte_data as s
 on		1=1 and t.[Id] = s.[Id]
 when matched then
 	update set
-	[Name] = s.[Name],[ModuleId] = s.[ModuleId],[BreakInheritance] = s.[BreakInheritance],[PermissionOptions] = s.[PermissionOptions]
+	[Name] = s.[Name],[Title] = s.[Title],[ModuleId] = s.[ModuleId],[BreakInheritance] = s.[BreakInheritance],[PermissionOptions] = s.[PermissionOptions]
 when not matched by target then
-	insert([Id],[Name],[ModuleId],[BreakInheritance],[PermissionOptions])
-	values(s.[Id],s.[Name],s.[ModuleId],s.[BreakInheritance],s.[PermissionOptions])
+	insert([Id],[Name],[Title],[ModuleId],[BreakInheritance],[PermissionOptions])
+	values(s.[Id],s.[Name],s.[Title],s.[ModuleId],s.[BreakInheritance],s.[PermissionOptions])
 when not matched by source then
 	delete;
 set identity_insert [sec].[BusinessEntity] off;
@@ -138,43 +143,40 @@ when not matched by source then
 
 --[sec].[SystemAction]------------------------------------------------------------------------------
 ----------------------------------------------------------------------------------------------------
-set nocount on;
-set identity_insert [sec].[SystemAction] on;
-;with cte_data([ID],[Name],[RequiredPermissions])
+;with cte_data([Name],[RequiredPermissions])
 as (select * from (values
 --//////////////////////////////////////////////////////////////////////////////////////////////////
-(1,'VR_Sec/Users/GetFilteredUsers','Users:View'),
-(2,'VR_Sec/Users/GetMembers','Users:View'),
-(3,'VR_Sec/Users/GetUserbyId','Users:View'),
-(4,'VR_Sec/Users/UpdateUser','Users:Edit'),
-(5,'VR_Sec/Users/AddUser','Users:Add'),
-(6,'VR_Sec/Users/ResetPassword','Users:Reset Password'),
-(7,'VR_Sec/Group/AddGroup','Groups:Add'),
-(8,'VR_Sec/BusinessEntityNode/ToggleBreakInheritance','System Entities:Assign Permissions'),
-(9,'VR_Sec/Permission/UpdatePermissions','System Entities:Assign Permissions'),
-(10,'VR_Sec/View/AddView','Dynamic Pages:Add'),
-(11,'WhS_BE/RoutingProduct/GetFilteredRoutingProducts',null),
-(12,'WhS_BE/RoutingProduct/AddRoutingProduct',null),
-(13,'WhS_Routing/RouteRule/AddRule',null),
-(14,'WhS_BE/CarrierAccount/GetFilteredCarrierAccounts','Carrier:View'),
-(15,'WhS_BE/CarrierAccount/UpdateCarrierAccount','Carrier:Edit'),
-(16,'WhS_BE/CarrierAccount/AddCarrierAccount','Carrier:Add'),
-(17,'VRCommon/Country/AddCountry','Users:Add'),
-(18,'VR_Sec/Group/GetFilteredGroups','Groups:View'),
-(19,'CP_SupPriceList/Customer/GetFilteredCustomers',null),
-(20,'CP_SupPriceList/PriceList/GetUpdated',null),
-(21,'CP_SupPriceList/SupplierMapping/GetFilteredCustomerSupplierMappings',null)
+('VR_Sec/Users/GetFilteredUsers','Users:View'),
+('VR_Sec/Users/GetMembers','Users:View'),
+('VR_Sec/Users/GetUserbyId','Users:View'),
+('VR_Sec/Users/UpdateUser','Users:Edit'),
+('VR_Sec/Users/AddUser','Users:Add'),
+('VR_Sec/Users/ResetPassword','Users:Reset Password'),
+('VR_Sec/Group/AddGroup','Groups:Add'),
+('VR_Sec/BusinessEntityNode/ToggleBreakInheritance','System Entities:Assign Permissions'),
+('VR_Sec/Permission/UpdatePermissions','System Entities:Assign Permissions'),
+('VR_Sec/View/AddView','Dynamic Pages:Add'),
+('VR_Sec/Group/GetFilteredGroups','Groups:View'),
+('CP_SupPriceList/Customer/GetFilteredCustomers','CP_SupPriceList_Customer:View'),
+('CP_SupPriceList/PriceList/GetUpdated','CP_SupPriceList_PriceList:View'),
+('CP_SupPriceList/SupplierMapping/GetFilteredCustomerSupplierMappings','CP_SupPriceList_SupplierMapping:View'),
+('CP_SupPriceList/PriceList/ImportPriceList','CP_SupPriceList_PriceList:ImportPriceList'),
+('CP_SupPriceList/CustomerUser/AddCustomerUser','CP_SupPriceList_Customer:Assign/Unassign User'),
+('CP_SupPriceList/CustomerUser/DeleteCustomerUser','CP_SupPriceList_Customer:Assign/Unassign User'),
+('CP_SupPriceList/SupplierMapping/AddCustomerSupplierMapping','CP_SupPriceList_SupplierMapping:Add/Edit'),
+('CP_SupPriceList/SupplierMapping/UpdateCustomerSupplierMapping','CP_SupPriceList_SupplierMapping:Add/Edit'),
+('CP_SupPriceList/SupplierMapping/GetCustomerSuppliers',null),
+('CP_SupPriceList/SupplierMapping/GetCustomerSupplierMapping','CP_SupPriceList_SupplierMapping:View'),
+('CP_SupPriceList/Customer/GetCustomer','CP_SupPriceList_Customer:View'),
+('CP_SupPriceList/Customer/GetCustomerTemplates',null),
+('CP_SupPriceList/Customer/GetCustomerInfos',null),
+('CP_SupPriceList/Customer/AddCustomer','CP_SupPriceList_Customer:Add'),
+('CP_SupPriceList/Customer/UpdateCustomer','CP_SupPriceList_Customer:Edit')
 --\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\
-)c([ID],[Name],[RequiredPermissions]))
+)c([Name],[RequiredPermissions]))
 merge	[sec].[SystemAction] as t
 using	cte_data as s
-on		1=1 and t.[ID] = s.[ID]
-when matched then
-	update set
-	[Name] = s.[Name],[RequiredPermissions] = s.[RequiredPermissions]
+on		1=1 and t.[Name] = s.[Name]
 when not matched by target then
-	insert([ID],[Name],[RequiredPermissions])
-	values(s.[ID],s.[Name],s.[RequiredPermissions])
-when not matched by source then
-	delete;
-set identity_insert [sec].[SystemAction] off;
+	insert([Name],[RequiredPermissions])
+	values(s.[Name],s.[RequiredPermissions]);
