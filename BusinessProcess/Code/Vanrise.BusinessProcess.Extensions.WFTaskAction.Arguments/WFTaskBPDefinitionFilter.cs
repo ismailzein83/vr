b216d8a1be99e2bc167710b24352ca8555703e6e
Ismail Zein <VANRISE\ismail.zein>
2016-03-02 10:00:11 +0000

    added WFTaskBPDefinitionFilter
    git-tfs-id: [http://developmentvlan:8585/tfs/vanrise.collection]$/;C31079

---
+++ BusinessProcess/Code/Vanrise.BusinessProcess.Extensions.WFTaskAction.Arguments/WFTaskBPDefinitionFilter.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Vanrise.Runtime.Entities;
+
+namespace Vanrise.BusinessProcess.Extensions.WFTaskAction.Arguments
+{
+    public class WFTaskBPDefinitionFilter : ISchedulerTaskFilter
+    {
+        public int BPDefinitionId { get; set; }
+
+        public bool IsMatched(SchedulerTask task)
+        {
+            if(task == null)
+                throw new ArgumentNullException("task");
+
+            if (task.TaskSettings == null)
+                throw new ArgumentNullException("task.TaskSettings");
+            var wfTaskActionArgument = task.TaskSettings.TaskActionArgument as WFTaskActionArgument;
+            if (wfTaskActionArgument == null)
+                return false;
+            if (wfTaskActionArgument.BPDefinitionID != this.BPDefinitionId)
+                return false;
+            return true;
+        }
+    }
+}
