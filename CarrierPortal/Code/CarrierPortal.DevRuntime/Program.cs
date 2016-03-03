7bb6a717a74663701f126ff117bd83d0f6f3ac8a
Ismail Zein <VANRISE\ismail.zein>
2016-03-03 08:38:13 +0000

    configured Runtime Service
    git-tfs-id: [http://developmentvlan:8585/tfs/vanrise.collection]$/;C31130

--- CarrierPortal/Code/CarrierPortal.DevRuntime/Program.cs
+++ CarrierPortal/Code/CarrierPortal.DevRuntime/Program.cs
@@ -1,8 +1,10 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Configuration;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Vanrise.Runtime;
 
 namespace CarrierPortal.DevRuntime
 {
@@ -10,10 +12,25 @@ namespace CarrierPortal.DevRuntime
     {
         static void Main(string[] args)
         {
-            MainForm f = new MainForm();
-            f.ShowDialog();
-            Console.ReadKey();
-            return;
+            if (ConfigurationManager.AppSettings["IsRuntimeService"] == "true")
+            {
+                SchedulerService schedulerService = new SchedulerService() { Interval = new TimeSpan(0, 0, 2) };
+
+                var runtimeServices = new List<RuntimeService>();
+                
+                runtimeServices.Add(schedulerService);
+
+                RuntimeHost host = new RuntimeHost(runtimeServices);
+                host.Start();
+                Console.ReadKey();
+            }
+            else
+            {
+                MainForm f = new MainForm();
+                f.ShowDialog();
+                Console.ReadKey();
+                return;
+            }
 
         }
     }
