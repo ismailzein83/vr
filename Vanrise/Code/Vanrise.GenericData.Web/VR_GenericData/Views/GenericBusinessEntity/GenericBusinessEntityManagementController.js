6abde55dd499eec5aaf49a8afd326ebeb21c41a8
mohamad.elhariri <mohamad.elhariri@vanrise.tfs.local>
2016-03-03 16:09:17 +0000

    Added GenericBusinessEntityManagement
    git-tfs-id: [http://developmentvlan:8585/tfs/vanrise.collection]$/;C31157

---
+++ Vanrise/Code/Vanrise.GenericData.Web/VR_GenericData/Views/GenericBusinessEntity/GenericBusinessEntityManagementController.js
@@ -0,0 +1,23 @@
+﻿(function (appControllers) {
+    
+    'use strict';
+
+    GenericBusinessEntityManagementController.$inject = ['$scope', 'VRNavigationService'];
+
+    function GenericBusinessEntityManagementController($scope, VRNavigationService) {
+        var genericBusinessEntityId;
+
+        loadParameters();
+
+        function loadParameters() {
+            var parameters = VRNavigationService.getParameters($scope);
+
+            if (parameters != undefined) {
+                genericBusinessEntityId = parameters.genericBusinessEntityId;
+            }
+        }
+    }
+
+    appControllers.controller('VR_GenericData_GenericBusinessEntityManagementController', GenericBusinessEntityManagementController);
+
+})(appControllers);
