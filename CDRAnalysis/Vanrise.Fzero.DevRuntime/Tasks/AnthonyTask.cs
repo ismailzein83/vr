1d45ce209af9354eb8108872770e43984c783c5e
anthony douaihy <VANRISE\anthony.douaihy>
2016-03-07 12:17:21 +0000

    Add new Task
    git-tfs-id: [http://developmentvlan:8585/tfs/vanrise.collection]$/;C31229

---
+++ CDRAnalysis/Vanrise.Fzero.DevRuntime/Tasks/AnthonyTask.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Vanrise.BusinessProcess;
+using Vanrise.BusinessProcess.Client;
+using Vanrise.BusinessProcess.Entities;
+using Vanrise.Queueing;
+using Vanrise.Runtime;
+
+namespace Vanrise.Fzero.DevRuntime.Tasks
+{
+    public class AnthonyTask : ITask
+    {
+        public void Execute()
+        {
+            var runtimeServices = new List<RuntimeService>();
+            BusinessProcessService bpService = new BusinessProcessService() { Interval = new TimeSpan(0, 0, 2) };
+            runtimeServices.Add(bpService);
+
+            SchedulerService schedulerService = new SchedulerService() { Interval = new TimeSpan(0, 0, 2) };
+            runtimeServices.Add(schedulerService);
+
+            RuntimeHost host = new RuntimeHost(runtimeServices);
+            host.Start();
+            Console.ReadKey();
+        }
+    }
+}
