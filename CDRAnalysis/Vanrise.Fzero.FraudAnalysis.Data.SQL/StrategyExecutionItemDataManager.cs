5905597c1965281f1294f0173262202e04b39707
walid.mogharbel <walid.mogharbel@vanrise.tfs.local>
2016-03-07 15:58:33 +0000

    solved a bug
    git-tfs-id: [http://developmentvlan:8585/tfs/vanrise.collection]$/;C31247

--- CDRAnalysis/Vanrise.Fzero.FraudAnalysis.Data.SQL/StrategyExecutionItemDataManager.cs
+++ CDRAnalysis/Vanrise.Fzero.FraudAnalysis.Data.SQL/StrategyExecutionItemDataManager.cs
@@ -259,7 +259,9 @@ namespace Vanrise.Fzero.FraudAnalysis.Data.SQL
             item.ID = (long)reader["ID"];
             item.AggregateValues = Vanrise.Common.Serializer.Deserialize<Dictionary<string, decimal>>(GetReaderValue<string>(reader, "AggregateValues"));
             item.FilterValues = Vanrise.Common.Serializer.Deserialize<Dictionary<int, decimal>>(GetReaderValue<string>(reader, "FilterValues"));
-            item.IMEIs = new HashSet<string>((GetReaderValue<string>(reader, "IMEIs")).Split(','));
+            string imies = GetReaderValue<string>(reader, "IMEIs");
+            if (imies != null)
+                item.IMEIs = new HashSet<string>((imies).Split(','));
             item.StrategyExecutionID = (int)reader["StrategyExecutionID"];
             item.AccountNumber = reader["AccountNumber"] as string;
             item.SuspicionLevelID = (int)reader["SuspicionLevelID"];
