a73e805a05700c7d08dc96588ff70b81e7332623
mohamad.elhariri <mohamad.elhariri@vanrise.tfs.local>
2016-03-08 16:11:51 +0000

    Added FieldArrayType
    git-tfs-id: [http://developmentvlan:8585/tfs/vanrise.collection]$/;C31286

---
+++ Vanrise/Code/Vanrise.GenericData.MainExtensions/DataRecordFields/FieldArrayType.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Vanrise.GenericData.Business;
+using Vanrise.GenericData.Entities;
+
+namespace Vanrise.GenericData.MainExtensions.DataRecordFields
+{
+    public class FieldArrayType : DataRecordFieldType
+    {
+        public int DataRecordFieldTypeConfigId { get; set; }
+        public List<DataRecordFieldType> Items { get; set; }
+
+        public override Type GetRuntimeType()
+        {
+            //DataRecordFieldTypeConfig dataRecordFieldTypeConfig = new DataRecordFieldTypeConfigManager().GetDataRecordFieldTypeConfig(DataRecordFieldTypeConfigId);
+            //if (dataRecordFieldTypeConfig == null)
+            //    throw new NullReferenceException("dataRecordFieldTypeConfig");
+            throw new NotImplementedException();
+        }
+
+        public override string GetDescription(object value)
+        {
+            throw new NotImplementedException();
+        }
+
+        public override bool IsMatched(object fieldValue, object filterValue)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
