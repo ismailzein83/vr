77bd24d32cd7fde3a60a9e171eaf0912a14f60fa
walid.mogharbel <walid.mogharbel@vanrise.tfs.local>
2016-03-14 13:19:27 +0000

    removed uneeded method
    git-tfs-id: [http://developmentvlan:8585/tfs/vanrise.collection]$/;C31415

--- CDRAnalysis/Vanrise.Fzero.FraudAnalysis.Data/IAccountInfoDataManager.cs
+++ CDRAnalysis/Vanrise.Fzero.FraudAnalysis.Data/IAccountInfoDataManager.cs
@@ -7,7 +7,6 @@ namespace Vanrise.Fzero.FraudAnalysis.Data
     public interface IAccountInfoDataManager : IDataManager 
     {
         void LoadAccountInfo(IEnumerable<CaseStatus> caseStatuses, Action<AccountInfo> onBatchReady);
-        bool InsertOrUpdateAccountInfo(string accountNumber, InfoDetail infoDetail);
         void SavetoDB(List<AccountInfo> records);
         bool UpdateAccountInfoBatch(List<AccountInfo> accountInfos);
 
