7c987186ae7f7b5fbce9452c36145ef1dcbd9826
Ismail Zein <VANRISE\ismail.zein>
2016-03-15 15:41:11 +0000

    added GenericSummaryItem
    git-tfs-id: [http://developmentvlan:8585/tfs/vanrise.collection]$/;C31464

---
+++ Vanrise/Code/Vanrise.GenericData.Entities/SummaryTransformation/GenericSummaryItem.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Vanrise.Entities.SummaryTransformation;
+
+namespace Vanrise.GenericData.Entities
+{
+    public class GenericSummaryItem : ISummaryItem
+    {
+        public long SummaryItemId { get; set; }
+
+        public DateTime BatchStart { get; set; }
+    }
+}
