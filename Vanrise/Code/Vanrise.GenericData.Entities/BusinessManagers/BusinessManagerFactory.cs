0a5d786bd6a8e6fc999cc703694010cfff1bb385
Ismail Zein <VANRISE\ismail.zein>
2016-03-15 15:10:23 +0000

    added BusinessManagers and IDataRecordTypeManager
    git-tfs-id: [http://developmentvlan:8585/tfs/vanrise.collection]$/;C31462

---
+++ Vanrise/Code/Vanrise.GenericData.Entities/BusinessManagers/BusinessManagerFactory.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+using Vanrise.Common;
+
+namespace Vanrise.GenericData.Entities
+{
+    public static class BusinessManagerFactory
+    {
+        static ObjectFactory s_objectFactory;
+        static BusinessManagerFactory()
+        {
+            s_objectFactory = new ObjectFactory(Assembly.Load("Vanrise.GenericData.Business"));
+        }
+
+        public static T GetDataManager<T>() where T : class, IBusinessManager
+        {
+            return s_objectFactory.CreateObjectFromType<T>();
+        }
+    }    
+}
---
+++ Vanrise/Code/Vanrise.GenericData.Entities/BusinessManagers/IDataRecordTypeManager.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Vanrise.GenericData.Entities
+{
+    public interface IDataRecordTypeManager : IBusinessManager
+    {
+        Type GetDataRecordRuntimeType(int dataRecordTypeId);
+
+        dynamic ConvertDynamicToDataRecord(dynamic dynamicObject, int dataRecordTypeId);
+
+        string SerializeRecord(dynamic record, int dataRecordTypeId);
+
+        dynamic DeserializeRecord(string serializedRecord, int dataRecordTypeId);
+    }
+}
