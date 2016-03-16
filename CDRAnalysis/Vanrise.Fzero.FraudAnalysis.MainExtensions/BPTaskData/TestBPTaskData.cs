492e91918bc1fb24538167d7fab132b251b95234
anthony douaihy <VANRISE\anthony.douaihy>
2016-03-16 08:36:25 +0000

    BP Task concept
    git-tfs-id: [http://developmentvlan:8585/tfs/vanrise.collection]$/;C31481

---
+++ CDRAnalysis/Vanrise.Fzero.FraudAnalysis.MainExtensions/BPTaskData/TestBPTaskData.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Vanrise.BusinessProcess.Entities;
+
+namespace Vanrise.Fzero.FraudAnalysis.MainExtensions
+{
+    public class TestBPTaskData : BPTaskData
+    {
+        public string Content { get; set; }
+    }
+}
