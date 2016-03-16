2bd0c11d6ab5574bfbafa85943e2c80fee0d4e78
Wissam Ajamy <VANRISE\wissam.ajamy>
2016-03-16 15:42:16 +0000

    update business process data script
    git-tfs-id: [http://developmentvlan:8585/tfs/vanrise.collection]$/;C31512

--- TOneV2/Code/TOneV2/SQL.TOneConfiguration/BusinessProcess.PostDeployment.sql
+++ TOneV2/Code/TOneV2/SQL.TOneConfiguration/BusinessProcess.PostDeployment.sql
@@ -11,9 +11,47 @@ Post-Deployment Script Template
 */
 --[sec].[Module]---------------------------601 to 700---------------------------------------------------------
 --------------------------------------------------------------------------------------------------------------
+set nocount on;
+set identity_insert [sec].[Module] on;
+;with cte_data([Id],[Name],[Title],[Url],[ParentId],[Icon],[Rank],[AllowDynamic])
+as (select * from (values
+--//////////////////////////////////////////////////////////////////////////////////////////////////
+(601,'Business Process','Business Process','Business Process',5,null,12,0)
+--\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\
+)c([Id],[Name],[Title],[Url],[ParentId],[Icon],[Rank],[AllowDynamic]))
+merge	[sec].[Module] as t
+using	cte_data as s
+on		1=1 and t.[Id] = s.[Id]
+when matched then
+	update set
+	[Name] = s.[Name],[Title] = s.[Title],[Url] = s.[Url],[ParentId] = s.[ParentId],[Icon] = s.[Icon],[Rank] = s.[Rank],[AllowDynamic] = s.[AllowDynamic]
+when not matched by target then
+	insert([Id],[Name],[Title],[Url],[ParentId],[Icon],[Rank],[AllowDynamic])
+	values(s.[Id],s.[Name],s.[Title],s.[Url],s.[ParentId],s.[Icon],s.[Rank],s.[AllowDynamic]);
+set identity_insert [sec].[Module] off;
 
 --[sec].[View]-----------------------------6001 to 7000-------------------------------------------------------
 --------------------------------------------------------------------------------------------------------------
+set nocount on;
+set identity_insert [sec].[View] on;
+;with cte_data([Id],[Name],[Title],[Url],[Module],[ActionNames],[Audience],[Content],[Settings],[Type],[Rank])
+as (select * from (values
+--//////////////////////////////////////////////////////////////////////////////////////////////////
+(6001,'Management','Management','#/view/BusinessProcess/Views/BPDefinition/BPDefinitionManagement',601,null,null,null,null,0,1),
+(6002,'Monitor','Monitor','#/view/BusinessProcess/Views/BPInstance/BPInstanceMonitor',601,null,null,null,null,0,2),
+(6003,'Log','Log History','#/view/BusinessProcess/Views/BPInstance/BPInstanceHistory',601,null,null,null,null,0,3)
+--\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\
+)c([Id],[Name],[Title],[Url],[Module],[ActionNames],[Audience],[Content],[Settings],[Type],[Rank]))
+merge	[sec].[View] as t
+using	cte_data as s
+on		1=1 and t.[Id] = s.[Id]
+when matched then
+	update set
+	[Name] = s.[Name],[Title] = s.[Title],[Url] = s.[Url],[Module] = s.[Module],[ActionNames] = s.[ActionNames],[Audience] = s.[Audience],[Content] = s.[Content],[Settings] = s.[Settings],[Type] = s.[Type],[Rank] = s.[Rank]
+when not matched by target then
+	insert([Id],[Name],[Title],[Url],[Module],[ActionNames],[Audience],[Content],[Settings],[Type],[Rank])
+	values(s.[Id],s.[Name],s.[Title],s.[Url],s.[Module],s.[ActionNames],s.[Audience],s.[Content],s.[Settings],s.[Type],s.[Rank]);
+set identity_insert [sec].[View] off;
 
 --[sec].[BusinessEntityModule]-------------601 to 700---------------------------------------------------------
 --------------------------------------------------------------------------------------------------------------
