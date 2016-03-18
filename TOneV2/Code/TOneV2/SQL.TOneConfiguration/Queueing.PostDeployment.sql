5b5af6e661cc0ea99949e494f07566f9d924412f
Wissam Ajamy <VANRISE\wissam.ajamy>
2016-03-18 11:34:41 +0000

    update post data script
    git-tfs-id: [http://developmentvlan:8585/tfs/vanrise.collection]$/;C31557

--- TOneV2/Code/TOneV2/SQL.TOneConfiguration/Queueing.PostDeployment.sql
+++ TOneV2/Code/TOneV2/SQL.TOneConfiguration/Queueing.PostDeployment.sql
@@ -13,22 +13,23 @@ Post-Deployment Script Template
 --------------------------------------------------------------------------------------------------------------
 set nocount on;
 set identity_insert [sec].[Module] on;
-;with cte_data([Id],[Name],[Title],[Url],[ParentId],[Icon],[Rank],[AllowDynamic])
+;with cte_data([Id],[Name],[Url],[ParentId],[Icon],[Rank],[AllowDynamic])
 as (select * from (values
 --//////////////////////////////////////////////////////////////////////////////////////////////////
-(701,'Queueing','Queueing',null,1,null,3,null)
+(701,'Queueing',null,1,null,3,null)
 --\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\
-)c([Id],[Name],[Title],[Url],[ParentId],[Icon],[Rank],[AllowDynamic]))
+)c([Id],[Name],[Url],[ParentId],[Icon],[Rank],[AllowDynamic]))
 merge	[sec].[Module] as t
 using	cte_data as s
 on		1=1 and t.[Id] = s.[Id]
 when matched then
 	update set
-	[Name] = s.[Name],[Title] = s.[Title],[Url] = s.[Url],[ParentId] = s.[ParentId],[Icon] = s.[Icon],[Rank] = s.[Rank],[AllowDynamic] = s.[AllowDynamic]
+	[Name] = s.[Name],[Url] = s.[Url],[ParentId] = s.[ParentId],[Icon] = s.[Icon],[Rank] = s.[Rank],[AllowDynamic] = s.[AllowDynamic]
 when not matched by target then
-	insert([Id],[Name],[Title],[Url],[ParentId],[Icon],[Rank],[AllowDynamic])
-	values(s.[Id],s.[Name],s.[Title],s.[Url],s.[ParentId],s.[Icon],s.[Rank],s.[AllowDynamic]);
+	insert([Id],[Name],[Url],[ParentId],[Icon],[Rank],[AllowDynamic])
+	values(s.[Id],s.[Name],s.[Url],s.[ParentId],s.[Icon],s.[Rank],s.[AllowDynamic]);
 set identity_insert [sec].[Module] off;
+
 --[sec].[View]-----------------------------7001 to 8000--------------------------------------------------------
 ---------------------------------------------------------------------------------------------------------------
 set nocount on;
@@ -36,10 +37,10 @@ set identity_insert [sec].[View] on;
 ;with cte_data([Id],[Name],[Title],[Url],[Module],[ActionNames],[Audience],[Content],[Settings],[Type],[Rank])
 as (select * from (values
 --//////////////////////////////////////////////////////////////////////////////////////////////////
-(7001,'Execution Flow Definitions','Execution Flow Definitions','#/view/Queueing/Views/ExecutionFlowDefinition/ExecutionFlowDefinitionManagement',701,null,null,null,'0',0,1),
-(7002,'Execution Flows','Execution Flows','#/view/Queueing/Views/ExecutionFlow/ExecutionFlowManagement',701,null,null,null,'0',0,2),
-(7003,'Queues','Queues','#/view/Queueing/Views/QueueInstance/QueueInstanceManagement',701,null,null,null,'0',0,3),
-(7004,'Queue Items','Queue Items','#/view/Queueing/Views/QueueItemHeader/QueueItemHeaderManagement',701,null,null,null,'0',0,4)
+(7001,'Execution Flow Definitions','Execution Flow Definitions','#/view/Queueing/Views/ExecutionFlowDefinition/ExecutionFlowDefinitionManagement',701,'VR_Queueing/ExecutionFlowDefinition/GetFilteredExecutionFlowDefinitions',null,null,'0',0,1),
+(7002,'Execution Flows','Execution Flows','#/view/Queueing/Views/ExecutionFlow/ExecutionFlowManagement',701,'VR_Queueing/ExecutionFlow/GetFilteredExecutionFlows',null,null,'0',0,2),
+(7003,'Queues','Queues','#/view/Queueing/Views/QueueInstance/QueueInstanceManagement',701,'VR_Queueing/QueueInstance/GetFilteredQueueInstances',null,null,'0',0,3),
+(7004,'Queue Items','Queue Items','#/view/Queueing/Views/QueueItemHeader/QueueItemHeaderManagement',701,'VR_Queueing/QueueItemHeader/GetFilteredQueueItemHeader',null,null,'0',0,4)
 --\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\
 )c([Id],[Name],[Title],[Url],[Module],[ActionNames],[Audience],[Content],[Settings],[Type],[Rank]))
 merge	[sec].[View] as t
@@ -52,6 +53,7 @@ when not matched by target then
 	insert([Id],[Name],[Title],[Url],[Module],[ActionNames],[Audience],[Content],[Settings],[Type],[Rank])
 	values(s.[Id],s.[Name],s.[Title],s.[Url],s.[Module],s.[ActionNames],s.[Audience],s.[Content],s.[Settings],s.[Type],s.[Rank]);
 set identity_insert [sec].[View] off;
+
 --[queue].[QueueActivatorConfig]--------------------------------------------------------------------
 ----------------------------------------------------------------------------------------------------
 set nocount on;
@@ -74,3 +76,90 @@ when not matched by target then
 	insert([ID],[Name],[Details])
 	values(s.[ID],s.[Name],s.[Details]);
 set identity_insert [queue].[QueueActivatorConfig] off;
+
+--[sec].[SystemAction]------------------------------------------------------------------------------
+----------------------------------------------------------------------------------------------------
+set nocount on;
+;with cte_data([Name],[RequiredPermissions])
+as (select * from (values
+--//////////////////////////////////////////////////////////////////////////////////////////////////
+('VR_Queueing/ExecutionFlow/GetExecutionFlows',null),
+('VR_Queueing/ExecutionFlow/GetExecutionFlow',null),
+('VR_Queueing/ExecutionFlow/UpdateExecutionFlow','VR_Queueing_ExecutionFlow: Edit'),
+('VR_Queueing/ExecutionFlow/AddExecutionFlow','VR_Queueing_ExecutionFlow: Add'),
+('VR_Queueing/ExecutionFlow/GetFilteredExecutionFlows','VR_Queueing_ExecutionFlow: View'),
+('VR_Queueing/ExecutionFlowDefinition/GetExecutionFlowDefinitions',null),
+('VR_Queueing/ExecutionFlowDefinition/GetFilteredExecutionFlowDefinitions','VR_Queueing_ExecutionFlowDefinition: View'),
+('VR_Queueing/ExecutionFlowDefinition/GetExecutionFlowDefinition',null),
+('VR_Queueing/ExecutionFlowDefinition/UpdateExecutionFlowDefinition','VR_Queueing_ExecutionFlowDefinition: Edit'),
+('VR_Queueing/ExecutionFlowDefinition/AddExecutionFlowDefinition','VR_Queueing_ExecutionFlowDefinition: Add'),
+('VR_Queueing/QueueActivatorConfig/GetQueueActivatorsConfig',null),
+('VR_Queueing/Queueing/GetQueueItemTypes',null),
+('VR_Queueing/Queueing/GetItemStatusList',null),
+('VR_Queueing/Queueing/GetQueueInstances',null),
+('VR_Queueing/Queueing/GetHeaders',null),
+('VR_Queueing/Queueing/GetQueueItemHeaders',null),
+('VR_Queueing/QueueInstance/GetItemTypes',null),
+('VR_Queueing/QueueInstance/GetFilteredQueueInstances','VR_Queueing_QueueInstance: View'),
+('VR_Queueing/QueueInstance/GetQueueInstances',null),
+('VR_Queueing/QueueItemHeader/GetFilteredQueueItemHeader','VR_Queueing_QueueItemHeader: View'),
+('VR_Queueing/QueueItemHeader/GetItemStatusSummary',null),
+('VR_Queueing/QueueItemHeader/GetExecutionFlowStatusSummary',null),
+('VR_Queueing/QueueItemType/GetItemTypes',null),
+('VR_Queueing/QueueStage/GetStageNames',null)
+--\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\
+)c([Name],[RequiredPermissions]))
+merge	[sec].[SystemAction] as t
+using	cte_data as s
+on		1=1 and t.[Name] = s.[Name]
+when matched then
+	update set
+	[RequiredPermissions] = s.[RequiredPermissions]
+when not matched by target then
+	insert([Name],[RequiredPermissions])
+	values(s.[Name],s.[RequiredPermissions]);
+
+--[sec].[BusinessEntityModule]--------------------701 to 800----------------------------------------
+----------------------------------------------------------------------------------------------------
+set nocount on;
+set identity_insert [sec].[BusinessEntityModule] on;
+;with cte_data([Id],[Name],[ParentId],[BreakInheritance])
+as (select * from (values
+--//////////////////////////////////////////////////////////////////////////////////////////////////
+(701,'Queueing',2,0)
+--\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\
+)c([Id],[Name],[ParentId],[BreakInheritance]))
+merge	[sec].[BusinessEntityModule] as t
+using	cte_data as s
+on		1=1 and t.[Id] = s.[Id]
+when matched then
+	update set
+	[Name] = s.[Name],[ParentId] = s.[ParentId],[BreakInheritance] = s.[BreakInheritance]
+when not matched by target then
+	insert([Id],[Name],[ParentId],[BreakInheritance])
+	values(s.[Id],s.[Name],s.[ParentId],s.[BreakInheritance]);
+set identity_insert [sec].[BusinessEntityModule] off;
+
+--[sec].[BusinessEntity]----------------------------------------------------------------------------
+----------------------------------------------------------------------------------------------------
+set nocount on;
+set identity_insert [sec].[BusinessEntity] on;
+;with cte_data([Id],[Name],[Title],[ModuleId],[BreakInheritance],[PermissionOptions])
+as (select * from (values
+--//////////////////////////////////////////////////////////////////////////////////////////////////
+(1801,'VR_Queueing_ExecutionFlow','Execution Flow',701,0,'["View","Add","Edit"]'),
+(1802,'VR_Queueing_ExecutionFlowDefinition','Execution Flow Definition',701,0,'["View","Add","Edit"]'),
+(1803,'VR_Queueing_QueueInstance','Queue Instance',701,0,'["View"]'),
+(1804,'VR_Queueing_QueueItemHeader','Queue Item',701,0,'["View"]')
+--\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\
+)c([Id],[Name],[Title],[ModuleId],[BreakInheritance],[PermissionOptions]))
+merge	[sec].[BusinessEntity] as t
+using	cte_data as s
+on		1=1 and t.[Id] = s.[Id]
+when matched then
+	update set
+	[Name] = s.[Name],[Title] = s.[Title],[ModuleId] = s.[ModuleId],[BreakInheritance] = s.[BreakInheritance],[PermissionOptions] = s.[PermissionOptions]
+when not matched by target then
+	insert([Id],[Name],[Title],[ModuleId],[BreakInheritance],[PermissionOptions])
+	values(s.[Id],s.[Name],s.[Title],s.[ModuleId],s.[BreakInheritance],s.[PermissionOptions]);
+set identity_insert [sec].[BusinessEntity] off;
