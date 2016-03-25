d19bc26f712f70fa0a22daa0c974db6a44165060
Ismail Zein <VANRISE\ismail.zein>
2016-03-25 08:35:52 +0000

    added test StoreCDRs activity
    git-tfs-id: [http://developmentvlan:8585/tfs/vanrise.collection]$/;C31699

---
+++ CarrierPortal/Code/CDRComparison.BP.Activities/StoreCDRs.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Activities;
+using CDRComparison.Entities;
+using Vanrise.BusinessProcess;
+
+namespace CDRComparison.BP.Activities
+{
+
+    public sealed class StoreCDRs : CodeActivity
+    {
+        [RequiredArgument]
+        public InArgument<CDRSource> CDRSource { get; set; }
+
+        public InArgument<bool> IsPartnerCDRs { get; set; }
+
+        protected override void Execute(CodeActivityContext context)
+        {
+            var cdrSource = this.CDRSource.Get(context);
+            List<CDR> cdrs = null;
+
+            context.WriteTrackingMessage(Vanrise.Common.LogEntryType.Information, "{0} CDRs read", cdrs.Count);
+            
+        }
+    }
+}
--- CarrierPortal/Code/CDRComparison.BP.Arguments/CDRComparsionProcessInput.cs
+++ CarrierPortal/Code/CDRComparison.BP.Arguments/CDRComparsionProcessInput.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using CDRComparison.Entities;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -8,6 +9,9 @@ namespace CDRComparison.BP.Arguments
 {
     public class CDRComparsionProcessInput : Vanrise.BusinessProcess.Entities.BaseProcessInputArgument
     {
+        public CDRSource SystemCDRSource { get; set; }
+
+        public CDRSource PartnerCDRSource { get; set; }
 
         public override string GetTitle()
         {
