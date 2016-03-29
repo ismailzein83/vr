7fd5d951b448c7b916d86a002e803653d591bcff
Samer Haidar <VANRISE\samer.haidar>
2016-03-29 08:33:59 +0000

    released for mohammad
    git-tfs-id: [http://developmentvlan:8585/tfs/vanrise.collection]$/;C31717

--- CarrierPortal/Code/CDRComparison.BP.Activities/LoadOrderedCDRs.cs
+++ CarrierPortal/Code/CDRComparison.BP.Activities/LoadOrderedCDRs.cs
@@ -1,12 +1,24 @@
-﻿using System;
+﻿using CDRComparison.Entities;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Vanrise.BusinessProcess;
+using Vanrise.Queueing;
 
 namespace CDRComparison.BP.Activities
 {
-    public  class LoadOrderedCDRs
+    #region Arguments Classes
+    public class LoadOrderedCDRsInput
     {
+        public BaseQueue<CDRBatch> OutputQueue { get; set; }
+    }
+
+    #endregion
+    public sealed class LoadOrderedCDRs : BaseAsyncActivity<LoadOrderedCDRsInput>
+    {
+        //[RequiredArgument]
+        //public InOutArgument<BaseQueue<CancellingStrategyExecutionBatch>> OutputQueue { get; set; }
     }
 }
