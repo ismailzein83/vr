8b9bc48ee0d0495a5856fc7b26c0a4a7c5f645c9
Samer Haidar <VANRISE\samer.haidar>
2016-03-29 10:46:30 +0000

    Implementing loadorderedCDR Activity
    git-tfs-id: [http://developmentvlan:8585/tfs/vanrise.collection]$/;C31721

--- CarrierPortal/Code/CDRComparison.BP.Activities/LoadOrderedCDRs.cs
+++ CarrierPortal/Code/CDRComparison.BP.Activities/LoadOrderedCDRs.cs
@@ -1,10 +1,13 @@
-﻿using CDRComparison.Entities;
+﻿using CDRComparison.Data;
+using CDRComparison.Entities;
 using System;
+using System.Activities;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using Vanrise.BusinessProcess;
+using Vanrise.Common;
 using Vanrise.Queueing;
 
 namespace CDRComparison.BP.Activities
@@ -18,7 +21,59 @@ namespace CDRComparison.BP.Activities
     #endregion
     public sealed class LoadOrderedCDRs : BaseAsyncActivity<LoadOrderedCDRsInput>
     {
-        //[RequiredArgument]
-        //public InOutArgument<BaseQueue<CancellingStrategyExecutionBatch>> OutputQueue { get; set; }
+        #region Arguments
+        [RequiredArgument]
+        public InOutArgument<BaseQueue<CDRBatch>> OutputQueue { get; set; }
+
+        #endregion
+
+        protected override void OnBeforeExecute(AsyncCodeActivityContext context, AsyncActivityHandle handle)
+        {
+            if (this.OutputQueue.Get(context) == null)
+                this.OutputQueue.Set(context, new MemoryQueue<CDRBatch>());
+
+            base.OnBeforeExecute(context, handle);
+        }
+        protected override void DoWork(LoadOrderedCDRsInput inputArgument, AsyncActivityHandle handle)
+        {
+            handle.SharedInstanceData.WriteTrackingMessage(LogEntryType.Information, "Started Loading CDRs");
+            long batchSize = 10000;
+            List<CDR> CDRs = new List<CDR>();
+            ICDRDataManager dataManager = CDRComparisonDataManagerFactory.GetDataManager<ICDRDataManager>();
+            long batch = 0;
+            long totalBatches = 0;
+            dataManager.LoadCDRs((cdr) =>
+                {
+                    batch++;
+                    totalBatches++;
+                    CDRs.Add(cdr);
+                    if (batch == batchSize)
+                    {
+                        inputArgument.OutputQueue.Enqueue(new CDRBatch() { CDRs = CDRs });
+                        Console.WriteLine("{0} CDRs Loaded", totalBatches);
+                        batch = 0;
+                        CDRs = new List<CDR>();
+                    }
+                });
+            if (CDRs.Count > 0)
+            {
+                inputArgument.OutputQueue.Enqueue(new CDRBatch() { CDRs = CDRs });
+                CDRs = new List<CDR>();
+            }
+
+            handle.SharedInstanceData.WriteTrackingMessage(LogEntryType.Information, "Finished Loading CDRs");
+
+        }
+        private static void BuildandEnqueueBatch(LoadOrderedCDRsInput inputArgument, AsyncActivityHandle handle, ref List<CDR> cdrs)
+        {
+            inputArgument.OutputQueue.Enqueue(new CDRBatch() { CDRs = cdrs});
+        }
+        protected override LoadOrderedCDRsInput GetInputArgument(AsyncCodeActivityContext context)
+        {
+            return new LoadOrderedCDRsInput
+            {
+                OutputQueue = this.OutputQueue.Get(context),
+            };
+        }
     }
 }
---
+++ CarrierPortal/Code/CDRComparison.Data.SQL/CDRDataManager.cs
@@ -0,0 +1,32 @@
+﻿using CDRComparison.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Vanrise.Data.SQL;
+
+namespace CDRComparison.Data.SQL
+{
+    public class CDRDataManager : BaseSQLDataManager, ICDRDataManager
+    {
+
+
+        public void LoadCDRs(Action<CDR> onBatchReady)
+        {
+            ExecuteReaderSP("FraudAnalysis.sp_StrategyExecutionItem_GetByNULLCaseID", (reader) =>
+            {
+                while (reader.Read())
+                {
+                    onBatchReady(new CDR()
+                    {
+                        CDPN = (reader["CDPN"] as string),
+                        CGPN = (reader["CGPN"] as string),
+                        DurationInSec = (GetReaderValue<Decimal>(reader, "Duration")),
+                        Time = (GetReaderValue<DateTime>(reader, "AttemptTime")),
+                    });
+                }
+            });
+        }
+    }
+}
