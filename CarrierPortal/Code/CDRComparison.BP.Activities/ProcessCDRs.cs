3ada35aa8417e37218b479017449577a257258db
Samer Haidar <VANRISE\samer.haidar>
2016-03-29 10:56:09 +0000

    replace reference
    git-tfs-id: [http://developmentvlan:8585/tfs/vanrise.collection]$/;C31723

--- CarrierPortal/Code/CDRComparison.BP.Activities/ProcessCDRs.cs
+++ CarrierPortal/Code/CDRComparison.BP.Activities/ProcessCDRs.cs
@@ -1,12 +1,42 @@
-﻿using System;
+﻿using CDRComparison.Entities;
+using System;
+using System.Activities;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Vanrise.BusinessProcess;
+using Vanrise.Queueing;
 
 namespace CDRComparison.BP.Activities
-{
-    public class ProcessCDRs
+{    
+    #region Arguments Classes
+    public class ProcessCDRsInput
     {
+        public BaseQueue<CDRBatch> InputQueue { get; set; }
+        public BaseQueue<CDRBatch> OutputQueue { get; set; }
+
+    }
+
+    #endregion
+    public class ProcessCDRs : BaseAsyncActivity<ProcessCDRsInput>
+    {
+        #region Arguments
+        [RequiredArgument]
+        public InOutArgument<BaseQueue<CDRBatch>> InputQueue { get; set; }
+       
+        [RequiredArgument]
+        public InOutArgument<BaseQueue<CDRBatch>> OutputQueue { get; set; }
+        #endregion
+
+        protected override void DoWork(ProcessCDRsInput inputArgument, AsyncActivityHandle handle)
+        {
+            throw new NotImplementedException();
+        }
+
+        protected override ProcessCDRsInput GetInputArgument(AsyncCodeActivityContext context)
+        {
+            throw new NotImplementedException();
+        }
     }
 }
