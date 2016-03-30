c64a6904e6896c0258423c7fe1d740cd1b08d254
mohamad.elhariri <mohamad.elhariri@vanrise.tfs.local>
2016-03-30 12:59:16 +0000

    Added MissingCDRQuery
    git-tfs-id: [http://developmentvlan:8585/tfs/vanrise.collection]$/;C31768

---
+++ CarrierPortal/Code/CDRComparison.Entities/MissingCDR/MissingCDRQuery.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CDRComparison.Entities
+{
+    public class MissingCDRQuery
+    {
+        public bool IsPartnerCDRs { get; set; }
+    }
+}
