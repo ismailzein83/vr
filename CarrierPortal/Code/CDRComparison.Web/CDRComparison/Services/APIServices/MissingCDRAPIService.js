673d724821357954793b1e26a0ed2afb6d04d08f
mohamad.elhariri <mohamad.elhariri@vanrise.tfs.local>
2016-03-30 12:55:01 +0000

    Added the api services, controllers and managers for the MissingCDR, PartialMatchCDR and DisputeCDR entities
    git-tfs-id: [http://developmentvlan:8585/tfs/vanrise.collection]$/;C31767

--- CarrierPortal/Code/CDRComparison.Web/CDRComparison/Services/APIServices/MissingCDRAPIService.js
+++ CarrierPortal/Code/CDRComparison.Web/CDRComparison/Services/APIServices/MissingCDRAPIService.js
@@ -1,1 +1,21 @@
-﻿
+﻿(function (appControllers) {
+
+    'use strict';
+
+    MissingCDRAPIService.$inject = ['BaseAPIService', 'UtilsService', 'CDRComparison_ModuleConfig'];
+
+    function MissingCDRAPIService(BaseAPIService, UtilsService, CDRComparison_ModuleConfig) {
+        var controllerName = 'MissingCDR';
+
+        return {
+            GetFilteredMissingCDRs: GetFilteredMissingCDRs
+        };
+
+        function GetFilteredMissingCDRs(input) {
+            return BaseAPIService.post(UtilsService.getServiceURL(CDRComparison_ModuleConfig.moduleName, controllerName, 'GetFilteredMissingCDRs'), input);
+        }
+    }
+
+    appControllers.service('CDRComparison_MissingCDRAPIService', MissingCDRAPIService);
+
+})(appControllers);
