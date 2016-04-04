425f0b2034e9db8580f44f70ff1f6df9bf622072
walid.mogharbel <walid.mogharbel@vanrise.tfs.local>
2016-04-04 14:53:16 +0000

    replaced div by span
    git-tfs-id: [http://developmentvlan:8585/tfs/vanrise.collection]$/;C31852

--- TOneV2/Code/TOneV2/TOne.WhS.BusinessEntity.Web/WhS_BusinessEntity/Directives/CarrierAccount/vr-whs-be-carrierprofile-selector.js
+++ TOneV2/Code/TOneV2/TOne.WhS.BusinessEntity.Web/WhS_BusinessEntity/Directives/CarrierAccount/vr-whs-be-carrierprofile-selector.js
@@ -51,10 +51,10 @@ function (WhS_BE_CarrierProfileAPIService, UtilsService, $compile, VRUIUtilsServ
         if (attrs.isrequired != undefined)
             required = "isrequired";
 
-        return '<div  vr-loader="isLoadingDirective" vr-disabled="ctrl.isdisabled" >'
-            + '<vr-columns colnum="{{ctrl.normalColNum}}"   > <vr-select ' + multipleselection + '  datatextfield="Name" datavaluefield="CarrierProfileId" '
+        return '<span  vr-loader="isLoadingDirective" vr-disabled="ctrl.isdisabled"  >'
+            + '<vr-columns colnum="{{ctrl.normalColNum}}"    > <vr-select ' + multipleselection + '  datatextfield="Name" datavaluefield="CarrierProfileId" '
         + required + ' label="Carrier Profile" datasource="ctrl.datasource" selectedvalues="ctrl.selectedvalues"  onselectionchanged="ctrl.onselectionchanged" ></vr-select></vr-columns>'
-           + '</div>'
+           + '</span>'
     }
 
     function carrierProfileCtor(ctrl, $scope, $attrs) {
