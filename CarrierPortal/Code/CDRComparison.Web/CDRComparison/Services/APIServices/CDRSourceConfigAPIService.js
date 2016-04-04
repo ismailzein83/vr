533936a039efa94eee737864a495fa1c4b4321a9
mohamad.elhariri <mohamad.elhariri@vanrise.tfs.local>
2016-04-04 07:33:47 +0000

    Added the server-side and client-side files of the CDRSourceConfig entity
    git-tfs-id: [http://developmentvlan:8585/tfs/vanrise.collection]$/;C31833

--- CarrierPortal/Code/CDRComparison.Web/CDRComparison/Services/APIServices/CDRSourceConfigAPIService.js
+++ CarrierPortal/Code/CDRComparison.Web/CDRComparison/Services/APIServices/CDRSourceConfigAPIService.js
@@ -1,1 +1,21 @@
-﻿
+﻿(function (appControllers) {
+
+    'use strict';
+
+    CDRSourceConfigAPIService.$inject = ['BaseAPIService', 'UtilsService', 'CDRComparison_ModuleConfig'];
+
+    function CDRSourceConfigAPIService(BaseAPIService, UtilsService, CDRComparison_ModuleConfig) {
+        var controllerName = 'CDRSourceConfig';
+
+        return {
+            GetCDRSourceConfigs: GetCDRSourceConfigs
+        };
+
+        function GetCDRSourceConfigs() {
+            return BaseAPIService.get(UtilsService.getServiceURL(CDRComparison_ModuleConfig.moduleName, controllerName, 'GetCDRSourceConfigs'));
+        }
+    }
+
+    appControllers.service('CDRComparison_CDRSourceConfigAPIService', CDRSourceConfigAPIService);
+
+})(appControllers);
