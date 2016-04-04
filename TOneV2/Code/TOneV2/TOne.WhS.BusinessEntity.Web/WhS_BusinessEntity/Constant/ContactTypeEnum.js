b57e848483a605454876245f76bd96a38478a224
walid.mogharbel <walid.mogharbel@vanrise.tfs.local>
2016-04-04 08:40:52 +0000

    updating UI of carrier profile editor to hide unneded padding and organizing directives in more suitable way
    git-tfs-id: [http://developmentvlan:8585/tfs/vanrise.collection]$/;C31835

--- TOneV2/Code/TOneV2/TOne.WhS.BusinessEntity.Web/WhS_BusinessEntity/Constant/ContactTypeEnum.js
+++ TOneV2/Code/TOneV2/TOne.WhS.BusinessEntity.Web/WhS_BusinessEntity/Constant/ContactTypeEnum.js
@@ -1,16 +1,23 @@
 ﻿app.constant('WhS_BE_ContactTypeEnum', {
-    BillingContactPerson: { value: 1, label: "Billing Contact Person" , type:"text"},
+    BillingContactPerson: { value: 1, label: "Billing Person" , type:"text"},
     BillingEmail: { value: 2, label: "Billing Email", type: "email" },
-    DisputeEmail: { value: 3, label: "Dispute Email", type: "email" },
-    PricingContactPerson: { value: 4, label: "Pricing Contact Person", type: "text" },
+
+    PricingContactPerson: { value: 4, label: "Pricing Person", type: "text" },
     PricingEmail: { value: 5, label: "Pricing Email", type: "email" },
-    AccountManagerContact: { value: 6, label: "Account Manager Contact", type: "text" },
-    AccountManagerEmail: { value: 7, label: "Account Manager Email", type: "email"},
-    SupportContactPerson: { value: 8, label: "Support Contact Person", type: "text" },
+    
+    AccountManagerContact: { value: 6, label: "Account Manager Person", type: "text" },
+    AccountManagerEmail: { value: 7, label: "Account Manager Email", type: "email" },
+
+    SupportContactPerson: { value: 8, label: "Support Person", type: "text" },
     SupportEmail: { value: 9, label: "Support Email ", type: "email" },
-    TechnicalContactPerson: { value: 10, label:"Technical Contact Person" , type:"text"},
+
+    TechnicalContactPerson: { value: 10, label:"Technical Person" , type:"text"},
     TechnicalEmail: { value: 11, label: "Technical Email", type: "email" },
-    CommercialContactPerson: { value: 12, label: "Commercial Contact Person", type: "text" },
+
+    CommercialContactPerson: { value: 12, label: "Commercial Person", type: "text" },
     CommercialEmail: { value: 13, label: "Commercial Email", type: "email" },
-    AlertingSMSPhoneNumbers: { value: 14, label: "Alerting SMS Phone Numbers", type: "number"}
+
+    AlertingSMSPhoneNumbers: { value: 14, label: "Alerting SMS Phone Numbers", type: "number" },
+
+    DisputeEmail: { value: 3, label: "Dispute Email", type: "email" }
 });
