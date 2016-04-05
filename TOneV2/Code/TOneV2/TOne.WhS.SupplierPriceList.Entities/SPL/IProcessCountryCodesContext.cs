b5cbfc710ffee0b9cc08c8fbaa1ef4f2ffcc0930
Zeinab Mokdad <VANRISE\zeinab.mokdad>
2016-04-05 11:16:41 +0000

    adding pricelistdate
    git-tfs-id: [http://developmentvlan:8585/tfs/vanrise.collection]$/;C31879

--- TOneV2/Code/TOneV2/TOne.WhS.SupplierPriceList.Entities/SPL/IProcessCountryCodesContext.cs
+++ TOneV2/Code/TOneV2/TOne.WhS.SupplierPriceList.Entities/SPL/IProcessCountryCodesContext.cs
@@ -16,6 +16,7 @@ namespace TOne.WhS.SupplierPriceList.Entities.SPL
         IEnumerable<ExistingZone> ExistingZones { get; }
 
         DateTime DeletedCodesDate { get; }
+        DateTime PriceListDate { get; }
 
         ZonesByName NewAndExistingZones { set; }
 
