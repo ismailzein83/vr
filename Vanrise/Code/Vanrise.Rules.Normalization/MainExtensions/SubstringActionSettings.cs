aa61ec8dd19d9306dbd6422d19f2cb974ff23ac1
Samer Haidar <VANRISE\samer.haidar>
2016-04-06 14:04:54 +0000

    refactoring
    git-tfs-id: [http://developmentvlan:8585/tfs/vanrise.collection]$/;C31937

--- Vanrise/Code/Vanrise.Rules.Normalization/MainExtensions/SubstringActionSettings.cs
+++ Vanrise/Code/Vanrise.Rules.Normalization/MainExtensions/SubstringActionSettings.cs
@@ -10,17 +10,17 @@ namespace Vanrise.Rules.Normalization.MainExtensions
     {
         public int StartIndex { get; set; }
 
-        public int Length { get; set; }
+        public int? Length { get; set; }
 
         public override string GetDescription()
         {
-            return string.Format("Substring: Start Index = {0}, Length = {1}", StartIndex, Length);
+            return string.Format("Substring: Start Index = {0}, Length = {1}", StartIndex-1, Length);
         }
 
         public override void Execute(INormalizeNumberActionContext context, NormalizeNumberTarget target)
         {
-            if (target.PhoneNumber.Length > this.StartIndex)
-                target.PhoneNumber = target.PhoneNumber.Substring(this.StartIndex, Math.Min(target.PhoneNumber.Length - this.StartIndex, this.Length));
+            if (target.PhoneNumber.Length > this.StartIndex - 1)
+                target.PhoneNumber = target.PhoneNumber.Substring(this.StartIndex - 1, Math.Min(target.PhoneNumber.Length - this.StartIndex - 1, (this.Length != null && (int)this.Length <= target.PhoneNumber.Length) ? (int)this.Length : target.PhoneNumber.Length));
         }
     }
 }
