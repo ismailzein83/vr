bd416c32eea3887d8b466c94e1125af5f609dc5b
Samer Haidar <VANRISE\samer.haidar>
2016-04-06 15:33:02 +0000

    Implementing time format editor
    git-tfs-id: [http://developmentvlan:8585/tfs/vanrise.collection]$/;C31941

---
+++ Vanrise/Code/Vanrise.Web/Client/Javascripts/Directives/TimeFormat/TimeFormateEditorController.js
@@ -0,0 +1,256 @@
+﻿(function (appControllers) {
+
+    "use strict";
+
+    TimeFormatEditorController.$inject = ['$scope', 'UtilsService', 'VRNotificationService', 'VRNavigationService', 'VRUIUtilsService'];
+
+    function TimeFormatEditorController($scope, UtilsService, VRNotificationService, VRNavigationService, VRUIUtilsService) {
+
+        var timeFormatValue;
+        var context;
+        var expressionBuilderDirectiveAPI;
+        loadParameters();
+        defineScope();
+        load();
+
+        function loadParameters() {
+            var parameters = VRNavigationService.getParameters($scope);
+            if (parameters != undefined && parameters != null) {
+                timeFormatValue = parameters.timeFormatValue;
+                context = parameters.Context;
+            }
+        }
+
+        function defineScope() {
+
+            $scope.timeFormatTemplates = [{
+                value: 0,
+                description: "Year"
+            },
+            {
+                value: 1,
+                description: "Month"
+            },
+            {
+                value: 2,
+                description: "Day"
+            },
+            {
+                value:3,
+                description: "Hour"
+            }
+            ,
+            {
+                value: 4,
+                description: "Minute"
+            },
+            {
+                value: 5,
+                description: "Second"
+            },
+            {
+                value: 6,
+                description: "Fraction"
+            }];
+
+            $scope.expressionTemplates = [{
+                value: 0,
+                timeValue:0,
+                description: "yyyy (ex: 2008)",
+                expression: "yyyy"
+
+            }, {
+                value: 1,
+                timeValue: 0,
+                description: "yyy (ex: 008)",
+                expression: "yyy"
+            }, {
+                value: 2,
+                timeValue: 0,
+                description: "yy (ex: 08)",
+                expression: "yy"
+            }, {
+                value: 3,
+                timeValue: 1,
+                description: "MMMM (ex: March)",
+                expression: "MMMM"
+            }, {
+                value: 4,
+                timeValue: 1,
+                description: "MMM (ex: Mar)",
+                expression: "MMM"
+            }, {
+                value: 5,
+                timeValue: 1,
+                description: "MM (ex: 03)",
+                expression: "MM"
+            }, {
+                value: 6,
+                timeValue: 1,
+                description: "M (ex: 3)",
+                expression: "M"
+            }, {
+                value: 7,
+                timeValue: 2,
+                description: "dddd (ex: Sunday)",
+                expression: "dddd"
+            }, {
+                value: 8,
+                timeValue: 2,
+                description: "ddd (ex: Sun)",
+                expression: "ddd"
+            }, {
+                value: 9,
+                timeValue: 2,
+                description: "dd (ex: 09)",
+                expression: "dd"
+            }, {
+                value: 10,
+                timeValue: 2,
+                description: "d (ex: 9)",
+                expression: "d"
+            }, {
+                value: 11,
+                timeValue: 3,
+                description: "HH (ex: 16)",
+                expression: "HH"
+            }, {
+                value: 12,
+                timeValue: 3,
+                description: "H (ex: 16)",
+                expression: "H"
+            }, {
+                value: 13,
+                timeValue: 3,
+                description: "hh (ex: 04)",
+                expression: "hh"
+            }, {
+                value: 14,
+                timeValue: 3,
+                description: "h (ex: 4)",
+                expression: "h"
+            }, {
+                value: 15,
+                timeValue: 4,
+                description: "mm (ex: 05)",
+                expression: "mm"
+            }, {
+                value: 16,
+                timeValue: 4,
+                description: "m (ex: 5)",
+                expression: "m"
+            }, {
+                value: 17,
+                timeValue: 5,
+                description: "ss (ex: 07)",
+                expression: "ss"
+            }, {
+                value: 18,
+                timeValue: 5,
+                description: "s (ex: 7)",
+                expression: "s"
+            }, {
+                value: 19,
+                timeValue: 6,
+                description: "ffff (ex: 1230)",
+                expression: "ffff"
+            }, {
+                value: 20,
+                timeValue: 6,
+                description: "fff (ex: 123)",
+                expression: "fff"
+            }, {
+                value: 21,
+                timeValue: 6,
+                description: "ff (ex: 12)",
+                expression: "ff"
+            }, {
+                value: 22,
+                timeValue: 6,
+                description: "f (ex: 1)",
+                expression: "f"
+            }]
+
+            $scope.onTimeSelectionChanged = function()
+            {
+                if ($scope.selectedTimeFormatTemplate != undefined)
+                {
+                    $scope.selectedExpression = undefined;
+                    $scope.filteredExpressionTemplates = UtilsService.getFilteredArrayFromArray($scope.expressionTemplates, $scope.selectedTimeFormatTemplate.value, "timeValue");
+
+                }
+            }
+
+            $scope.isbuttonDisabled = function () {
+            }
+
+
+
+            $scope.addExpression = function () {
+
+                if ($scope.timeFormatValue != undefined)
+                    $scope.timeFormatValue += $scope.selectedExpression.expression;
+                else
+                    $scope.timeFormatValue = $scope.selectedExpression.expression;
+            }
+
+            $scope.saveTimeFormatBuilder = function () {
+                return saveTimeFormat();
+            };
+
+            $scope.close = function () {
+                $scope.modalContext.closeModal()
+            };
+
+            $scope.validateExpression = function () {
+                if ($scope.timeFormatValue == timeFormatValue)
+                    return 'No changed.';
+                return null;
+            }
+        }
+
+        function load() {
+            $scope.isLoading = true;
+            loadAllControls();
+
+            function loadAllControls() {
+                return UtilsService.waitMultipleAsyncOperations([loadTextAreaSection, setTitle]).then(function () {
+                })
+                    .catch(function (error) {
+                        VRNotificationService.notifyExceptionWithClose(error, $scope);
+                    })
+                   .finally(function () {
+                       $scope.isLoading = false;
+                   });
+
+
+                function setTitle() {
+                    $scope.title = 'Time Format Builder';
+                }
+
+
+
+                function loadTextAreaSection() {
+                    if (timeFormatValue != undefined) {
+                        $scope.timeFormatValue = timeFormatValue;
+                    }
+                }
+            }
+        }
+
+        function buildTimeFormatObjFromScope() {
+            return $scope.timeFormatValue;
+        }
+
+        function saveTimeFormat() {
+
+            var timeFormatValueObject = buildTimeFormatObjFromScope();
+            if ($scope.onSetTimeFormatBuilder != undefined)
+                $scope.onSetTimeFormatBuilder(timeFormatValueObject);
+            $scope.modalContext.closeModal();
+
+        }
+    }
+
+    appControllers.controller('VR_TimeFormatEditorController', TimeFormatEditorController);
+})(appControllers);
---
+++ Vanrise/Code/Vanrise.Web/Client/Javascripts/Directives/TimeFormat/vr-timeformat-directive.js
@@ -0,0 +1,99 @@
+﻿'use strict';
+app.directive('vrTimeformat', ['UtilsService', '$compile', 'VRModalService', function (UtilsService, $compile, VRModalService) {
+
+    var directiveDefinitionObject = {
+        restrict: 'E',
+        scope: {
+            label: '@',
+            hidelabel: '@',
+            isrequired: '=',
+            value: '=',
+        },
+        controller: function ($scope, $element, $attrs) {
+            var ctrl = this;
+            var ctor = new timeFormatCtor(ctrl, $scope, $attrs);
+            ctor.initializeController();
+        },
+        controllerAs: 'ctrl',
+        bindToController: true,
+        compile: function (element, attrs) {
+            return {
+                pre: function ($scope, iElem, iAttrs, ctrl) {
+                    var ctrl = $scope.ctrl;
+                    $scope.$watch('ctrl.value', function (newValue, oldValue) {
+
+                        var retrunedValue;
+                        if (newValue == "") {
+                            setTimeout(function () {
+                                ctrl.value = undefined;
+                                $scope.$apply();
+                            });
+                        }
+                        if (!newValue == "") {
+                            retrunedValue = newValue;
+                        }
+                        if (iAttrs.onvaluechanged != undefined) {
+                            var onvaluechangedMethod = $scope.$parent.$eval(iAttrs.onvaluechanged);
+                            if (onvaluechangedMethod != undefined && typeof (onvaluechangedMethod) == 'function') {
+                                onvaluechangedMethod(retrunedValue);
+                            }
+                        }
+
+                    });
+                }
+            }
+        },
+        template: function (element, attrs) {
+            return getTamplate(attrs);
+        }
+    };
+
+
+    function getTamplate(attrs) {
+        var withemptyline = 'withemptyline';
+        if (attrs.hidelabel != undefined)
+            withemptyline = '';
+        var template =
+            '<vr-row removeline>'
+             + '<vr-columns width="3/4row">'
+             + '<vr-label ng-if="ctrl.hidelabel ==undefined">{{ctrl.label}}</vr-label>'
+             + '<vr-textbox value="ctrl.value" isrequired="ctrl.isrequired"></vr-textbox>'
+             + '</vr-columns>'
+             + '<vr-columns width="1/4row" ' + withemptyline + ' > '
+             + '   <vr-button type="Edit" data-onclick="openTimeFormatBuilder" standalone></vr-button>'
+             + '</vr-columns>'
+            + '</vr-row>';
+        return template;
+
+    }
+    function timeFormatCtor(ctrl, $scope, $attrs) {
+        function initializeController() {
+
+
+            $scope.openTimeFormatBuilder = function () {
+                var modalSettings = {};
+
+                modalSettings.onScopeReady = function (modalScope) {
+                    modalScope.onSetTimeFormatBuilder = function (timeFormatBuilderValue) {
+                        ctrl.value = timeFormatBuilderValue;
+                    };
+                };
+                var parameter;
+                if (ctrl.value != undefined)
+                {
+                     parameter = {
+                        timeFormatValue: ctrl.value
+                    };
+                }
+                   
+                VRModalService.showModal('/Client/Javascripts/Directives/TimeFormat/Templates/TimeFormatEditor.html', parameter, modalSettings);
+            }
+            
+        }
+
+        this.initializeController = initializeController;
+
+    }
+    return directiveDefinitionObject;
+}]);
+
