8c54ac2129df55436ce66423527319859f7f681e
Zeinab Mokdad <VANRISE\zeinab.mokdad>
2016-04-08 07:51:51 +0000

    Adding warning Action
    git-tfs-id: [http://developmentvlan:8585/tfs/vanrise.collection]$/;C31975

---
+++ BusinessProcess/Code/Vanrise.BusinessProcess/BusinessRuleActions/WarningItemAction.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Vanrise.BusinessProcess.Entities;
+
+namespace Vanrise.BusinessProcess
+{
+    public class WarningItemAction : BusinessRuleAction
+    {
+        public override void Execute(IBusinessRuleActionExecutionContext context)
+        {
+            if (context.Target == null)
+                throw new ArgumentNullException("context.Target");
+        }
+
+        public override ActionSeverity GetSeverity()
+        {
+            return ActionSeverity.Warning;
+        }
+    }
+}
