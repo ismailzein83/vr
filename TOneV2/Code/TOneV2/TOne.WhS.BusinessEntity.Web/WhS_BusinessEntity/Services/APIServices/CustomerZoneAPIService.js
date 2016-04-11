9e0617c57a9f90fe6c45ef48f930ea50a41ebf7c
walid.mogharbel <walid.mogharbel@vanrise.tfs.local>
2016-04-11 14:42:44 +0000

    refactoring
    git-tfs-id: [http://developmentvlan:8585/tfs/vanrise.collection]$/;C32058

--- TOneV2/Code/TOneV2/TOne.WhS.BusinessEntity.Web/WhS_BusinessEntity/Services/APIServices/CustomerZoneAPIService.js
+++ TOneV2/Code/TOneV2/TOne.WhS.BusinessEntity.Web/WhS_BusinessEntity/Services/APIServices/CustomerZoneAPIService.js
@@ -5,6 +5,23 @@
     customerZoneAPIService.$inject = ["BaseAPIService", "UtilsService", "WhS_BE_ModuleConfig"];
 
     function customerZoneAPIService(BaseAPIService, UtilsService, WhS_BE_ModuleConfig) {
+        var controllerName = "CustomerZone";
+
+        function GetCustomerZones(customerId) {
+            return BaseAPIService.get(UtilsService.getServiceURL(WhS_BE_ModuleConfig.moduleName, controllerName, "GetCustomerZones"), {
+                customerId: customerId
+            });
+        }
+
+        function GetCountriesToSell(customerId) {
+            return BaseAPIService.get(UtilsService.getServiceURL(WhS_BE_ModuleConfig.moduleName, controllerName, "GetCountriesToSell"), {
+                customerId: customerId
+            });
+        }
+
+        function AddCustomerZones(customerZones) {
+            return BaseAPIService.post(UtilsService.getServiceURL(WhS_BE_ModuleConfig.moduleName, controllerName, "AddCustomerZones"), customerZones);
+        }
 
         return ({
             GetCustomerZones: GetCustomerZones,
@@ -12,21 +29,6 @@
             AddCustomerZones: AddCustomerZones
         });
 
-        function GetCustomerZones(customerId) {
-            return BaseAPIService.get(UtilsService.getServiceURL(WhS_BE_ModuleConfig.moduleName, "CustomerZone", "GetCustomerZones"), {
-                customerId: customerId
-            });
-        }
-
-        function GetCountriesToSell(customerId) {
-            return BaseAPIService.get(UtilsService.getServiceURL(WhS_BE_ModuleConfig.moduleName, "CustomerZone", "GetCountriesToSell"), {
-                customerId: customerId
-            });
-        }
-
-        function AddCustomerZones(customerZones) {
-            return BaseAPIService.post(UtilsService.getServiceURL(WhS_BE_ModuleConfig.moduleName, "CustomerZone", "AddCustomerZones"), customerZones);
-        }
     }
 
     appControllers.service("WhS_BE_CustomerZoneAPIService", customerZoneAPIService);
