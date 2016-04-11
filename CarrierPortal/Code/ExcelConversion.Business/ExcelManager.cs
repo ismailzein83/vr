237251130d40689b1088122399c6bca55ca4eed3
Ali Ballouk <VANRISE\ali.ballouk>
2016-04-11 11:03:07 +0000

    fix ReadExcelFilePage
    git-tfs-id: [http://developmentvlan:8585/tfs/vanrise.collection]$/;C32026

--- CarrierPortal/Code/ExcelConversion.Business/ExcelManager.cs
+++ CarrierPortal/Code/ExcelConversion.Business/ExcelManager.cs
@@ -60,25 +60,32 @@ namespace ExcelConversion.Business
                 ewb.Sheets.Add(eSheet);
                 eSheet.Name = sheet.Name;
                 int nbOfSheetColumns = 0;
-                foreach(Row row in sheet.Cells.Rows)
+                for (int j = 0; j <= 100; j++)
                 {
-                    ExcelRow eRow = new ExcelRow() { Cells = new List<ExcelCell>() };
-                    eSheet.Rows.Add(eRow);
-                    int nbOfRowColumns = row.LastCell.Column + 1;
-                    if (nbOfRowColumns > nbOfSheetColumns)
-                        nbOfSheetColumns = nbOfRowColumns;
-                    for (int colIndex = 0; colIndex < nbOfRowColumns; colIndex++)
+                    Row row = (Row)sheet.Cells.Rows[j];
+
+                    if (row != null && !row.IsBlank)
                     {
-                        var cell = row.GetCellOrNull(colIndex);
-                        ExcelCell eCell = new ExcelCell();
-                        if (cell != null )
+                        ExcelRow eRow = new ExcelRow() { Cells = new List<ExcelCell>() };
+                        eSheet.Rows.Add(eRow);
+                        int nbOfRowColumns = row.LastCell.Column + 1;
+                        if (nbOfRowColumns > nbOfSheetColumns)
+                            nbOfSheetColumns = nbOfRowColumns;
+                        for (int colIndex = 0; colIndex < nbOfRowColumns; colIndex++)
                         {
-                            eCell.Value = cell.Value;
-                            eRow.Cells.Add(eCell);
+                            var cell = row.GetCellOrNull(colIndex);
+                            ExcelCell eCell = new ExcelCell();
+                            if (cell != null)
+                            {
+                                eCell.Value = cell.Value;
+                                eRow.Cells.Add(eCell);
+
+                            }
 
                         }
-                           
                     }
+
+
                 }
                 eSheet.NumberOfColumns = nbOfSheetColumns;
             }
@@ -86,6 +93,83 @@ namespace ExcelConversion.Business
             return ewb;
         }
 
+        public ExcelWorksheet ReadExcelFilePage(ExcelPageQuery Query)
+        {
+            System.Threading.Thread.Sleep(2000);
+
+            var fileManager = new VRFileManager();
+            var file = fileManager.GetFile(Query.FileId);
+            if (file == null)
+                throw new NullReferenceException(String.Format("file '{0}'", Query.FileId));
+            if (file.Content == null)
+                throw new NullReferenceException(String.Format("file.Content '{0}'", Query.FileId));
+            MemoryStream stream = new MemoryStream(file.Content);
+            Workbook workbook = new Workbook(stream);
+
+            ExcelWorkbook ewb = new ExcelWorkbook() { Sheets = new List<ExcelWorksheet>() };
+            var sheet = workbook.Worksheets[Query.SheetIndex];
+            if (sheet == null)
+                throw new NullReferenceException(String.Format("Sheet.Content '{0}'", Query.SheetIndex));
+            
+
+
+                ExcelWorksheet eSheet = new ExcelWorksheet()
+                {
+                    Rows = new List<ExcelRow>(),
+                    MergedCells = new List<MergedCell>()
+                };
+                ArrayList mcells = sheet.Cells.MergedCells;
+
+
+
+                for (int i = 0; i < mcells.Count; i++)
+                {
+
+                    CellArea area = (CellArea)mcells[i];
+                    MergedCell mc = new MergedCell();
+                    mc.col = area.StartColumn;
+                    mc.row = area.StartRow;
+                    mc.rowspan = (area.EndRow - area.StartRow) + 1;
+                    mc.colspan = (area.EndColumn - area.StartColumn) + 1;
+                    eSheet.MergedCells.Add(mc);
+
+                }
+
+                ewb.Sheets.Add(eSheet);
+                eSheet.Name = sheet.Name;
+                int nbOfSheetColumns = 0;
+
+                for (int j = Query.From ; j < Query.To; j++)
+                {
+                    Row row = (Row)sheet.Cells.Rows[j];
+
+                    if (row != null && !row.IsBlank)
+                    {
+                        ExcelRow eRow = new ExcelRow() { Cells = new List<ExcelCell>() };
+                        eSheet.Rows.Add(eRow);
+                        int nbOfRowColumns = row.LastCell.Column + 1;
+                        if (nbOfRowColumns > nbOfSheetColumns)
+                            nbOfSheetColumns = nbOfRowColumns;
+                        for (int colIndex = 0; colIndex < nbOfRowColumns; colIndex++)
+                        {
+                            var cell = row.GetCellOrNull(colIndex);
+                            ExcelCell eCell = new ExcelCell();
+                            if (cell != null)
+                            {
+                                eCell.Value = cell.Value;
+                                eRow.Cells.Add(eCell);
+
+                            }
+
+                        }
+                    }
+            
+                    
+                }
+                eSheet.NumberOfColumns = nbOfSheetColumns;
+               return eSheet;
+           
+        }
         public IEnumerable<TemplateConfig> GetFieldMappingTemplateConfigs()
         {
             var templateConfigManager = new TemplateConfigManager();
