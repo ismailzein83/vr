042037a5a272c2383b7f2db32c643a621b0d9c77
Zeinab Mokdad <VANRISE\zeinab.mokdad>
2016-04-12 11:57:31 +0000

    adding new interface "ICode"
    git-tfs-id: [http://developmentvlan:8585/tfs/vanrise.collection]$/;C32076

---
+++ TOneV2/Code/TOneV2/TOne.WhS.SupplierPriceList.Entities/SPL/ICode.cs
@@ -0,0 +1,10 @@
+﻿using Vanrise.BusinessProcess.Entities;
+
+namespace TOne.WhS.SupplierPriceList.Entities.SPL
+{
+    public interface ICode : Vanrise.Entities.IDateEffectiveSettings, IRuleTarget
+    {
+        string Code { get; set; }
+
+    }
+}
