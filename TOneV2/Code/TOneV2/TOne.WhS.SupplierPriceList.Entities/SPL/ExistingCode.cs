b232cbc71d9bf6eb161759ae7877fcbd2a6c996e
Zeinab Mokdad <VANRISE\zeinab.mokdad>
2016-04-14 11:59:20 +0000

    adding seperated classes
    git-tfs-id: [http://developmentvlan:8585/tfs/vanrise.collection]$/;C32135

---
+++ TOneV2/Code/TOneV2/TOne.WhS.SupplierPriceList.Entities/SPL/ExistingCode.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace TOne.WhS.SupplierPriceList.Entities.SPL
+{
+    public class ExistingCode : ICode
+    {
+        public ExistingZone ParentZone { get; set; }
+
+        public BusinessEntity.Entities.SupplierCode CodeEntity { get; set; }
+
+        public ChangedCode ChangedCode { get; set; }
+
+        public DateTime BED
+        {
+            get { return CodeEntity.BED; }
+        }
+
+        public DateTime? EED
+        {
+            get { return ChangedCode != null ? ChangedCode.EED : CodeEntity.EED; }
+        }
+
+        public string Code { get; set; }
+
+        public object Key
+        {
+            get { return CodeEntity.Code; }
+        }
+
+        public void SetExcluded()
+        {
+            throw new NotImplementedException();
+        }
+
+        public string TargetType
+        {
+            get { return "Code"; }
+        }
+    }
+    public class ExistingCodesByCodeValue : Dictionary<string, List<ExistingCode>>
+    {
+
+    }
+}
---
+++ TOneV2/Code/TOneV2/TOne.WhS.SupplierPriceList.Entities/SPL/ExistingRate.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace TOne.WhS.SupplierPriceList.Entities.SPL
+{
+    public class ExistingRate : Vanrise.Entities.IDateEffectiveSettings
+    {
+        public ExistingZone ParentZone { get; set; }
+
+        public BusinessEntity.Entities.SupplierRate RateEntity { get; set; }
+
+        public ChangedRate ChangedRate { get; set; }
+
+        public DateTime BED
+        {
+            get { return RateEntity.BED; }
+        }
+
+        public DateTime? EED
+        {
+            get { return ChangedRate != null ? ChangedRate.EED : RateEntity.EED; }
+        }
+    }
+
+    public class ExistingRatesByZoneName : Dictionary<string, List<ExistingRate>>
+    {
+
+    }
+}
