3b3b82b483e0d8c5ffdd72f1aaae664547bf286c
Zeinab Mokdad <VANRISE\zeinab.mokdad>
2016-04-18 14:04:00 +0000

    Stroy 1021
    git-tfs-id: [http://developmentvlan:8585/tfs/vanrise.collection]$/;C32220

---
+++ CarrierPortal/Code/CP.SupplierPricelist.Entities/PriceList/PriceListQuery.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CP.SupplierPricelist.Entities
+{
+    public class PriceListQuery
+    {
+        public List<int> CarriersID { get; set; }
+        public List<string> CarrierAccounts { get; set; }
+        public int PriceListType { get; set; }
+        public int PriceListResult { get; set; }
+        public int PriceListStatus { get; set; }
+        public DateTime FromEffectiveOnDate { get; set; }
+        public DateTime ToEffectiveOnDate { get; set; }
+
+        public PriceListQuery()
+        {
+            PriceListResult = -1;
+            PriceListType = -1;
+            PriceListStatus = -1;
+        }
+    }
+}
