27b328c1931071d654ab9a282af208a70f2b735e
anthony douaihy <VANRISE\anthony.douaihy>
2016-04-19 08:46:21 +0000

    User Story 539: adjust Percentage calculation
    git-tfs-id: [http://developmentvlan:8585/tfs/vanrise.collection]$/;C32238

--- TOneV2/Code/TOneV2/TOne.WhS.Routing.Business/RouteRules/Percentages/FixedOptionPercentage.cs
+++ TOneV2/Code/TOneV2/TOne.WhS.Routing.Business/RouteRules/Percentages/FixedOptionPercentage.cs
@@ -1,8 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using TOne.WhS.Routing.Entities;
 
 namespace TOne.WhS.Routing.Business.RouteRules.Percentages
@@ -25,52 +23,27 @@ namespace TOne.WhS.Routing.Business.RouteRules.Percentages
                     assignedPercentages += this.Percentages[j];
             }
 
-            decimal unassignedPercentages = 0;
-            int difference = percentagesCount - optionsCount;
-            if (difference > 0)
-            {
-                for (int j = optionsCount; j < percentagesCount; j++)
-                {
-                    unassignedPercentages += this.Percentages[j];
-                }
-            }
+            decimal unassignedPercentages = 100 - assignedPercentages;
 
             Decimal totalTakenPercentage = 0;
             for (int i = 0; i < optionsCount; i++)
             {
+                var currentOption = optionsList[i];
                 if (i > (percentagesCount - 1))
-                    optionsList[i].Percentage = 0;
+                    currentOption.Percentage = 0;
                 else
                 {
                     if (i == (optionsCount - 1) || i == (percentagesCount - 1))
-                        optionsList[i].Percentage = 100 - totalTakenPercentage;
+                        currentOption.Percentage = 100 - totalTakenPercentage;
                     else
                     {
-                        decimal caluclatedPercentage = decimal.Round(this.Percentages[i] + this.Percentages[i] * unassignedPercentages / assignedPercentages, 2);
+                        var correspondingPercentage = this.Percentages[i];
+                        decimal caluclatedPercentage = decimal.Round(correspondingPercentage + correspondingPercentage * unassignedPercentages / assignedPercentages, 2);
                         optionsList[i].Percentage = caluclatedPercentage;
                         totalTakenPercentage += caluclatedPercentage;
                     }
                 }
             }
-
-            //Decimal totalTakenPercentage = 0;
-            //int percentagesCount = this.Percentages.Count;
-            //List<IRouteOptionPercentageTarget> optionsList = context.Options.ToList();
-            //int optionsCount = optionsList.Count;
-            //for (int i = 0; i < optionsCount; i++)
-            //{
-            //    Decimal optionPercentage = 0;
-
-            //    if (i >= percentagesCount)
-            //        optionPercentage = 100 - totalTakenPercentage;
-            //    else
-            //        optionPercentage = Math.Min(this.Percentages[i], 100 - totalTakenPercentage);
-            //    optionsList[i].Percentage = optionPercentage;
-            //    totalTakenPercentage += optionPercentage;
-
-            //    if (totalTakenPercentage == 100)
-            //        break;
-            //}
         }
     }
 }
