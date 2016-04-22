a6ccc070f7b0720f8cc972a0365f37121d9fb6f2
mohamad.elhariri <mohamad.elhariri@vanrise.tfs.local>
2016-04-22 08:58:34 +0000

    Added VariationReportDimensionEnum
    git-tfs-id: [http://developmentvlan:8585/tfs/vanrise.collection]$/;C32311

---
+++ TOneV2/Code/TOneV2/TOne.WhS.Analytics.Web/WhS_Analytics/Constant/VariationReportDimensionEnum.js
@@ -0,0 +1,5 @@
+﻿app.constant('WhS_Analytics_VariationReportDimensionEnum', {
+    Customer: { value: 0, description: 'Customer' },
+    Supplier: { value: 1, description: 'Supplier' },
+    Zone: { value: 2, description: 'Zone' }
+});
