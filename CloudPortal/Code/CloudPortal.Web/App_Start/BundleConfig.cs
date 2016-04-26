355bfe52b1d50afae174927690b36b4634d8ef5c
Wissam Ajamy <VANRISE\wissam.ajamy>
2016-04-26 14:56:09 +0000

    Add structure for new project CloudPortal
    git-tfs-id: [http://developmentvlan:8585/tfs/vanrise.collection]$/;C32394

---
+++ CloudPortal/Code/CloudPortal.Web/App_Start/BundleConfig.cs
@@ -0,0 +1,18 @@
+﻿using System.Web;
+using System.Web.Optimization;
+
+namespace CloudPortal.Web
+{
+    public class BundleConfig
+    {
+        // For more information on bundling, visit http://go.microsoft.com/fwlink/?LinkId=301862
+        public static void RegisterBundles(BundleCollection bundles)
+        {
+            Vanrise.Web.BundleConfig.RegisterBundles(bundles);
+
+            bundles.Add(new ScriptBundle("~/bundles/ModulesJavascripts").IncludeDirectory(
+                "~/Client/Modules/Common", "*.js", true).IncludeDirectory(
+                "~/Client/Modules/Security", "*.js", true));
+        }
+    }
+}
---
+++ CloudPortal/Code/CloudPortal.Web/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="CloudPortal.Web.WebApiApplication" Language="C#" %>
