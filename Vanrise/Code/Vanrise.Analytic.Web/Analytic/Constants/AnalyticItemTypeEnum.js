36c384ebe886871e4e321741ca378b4999e45831
Samer Haidar <VANRISE\samer.haidar>
2016-04-28 15:09:02 +0000

    adding files needed for analytic
    git-tfs-id: [http://developmentvlan:8585/tfs/vanrise.collection]$/;C32462

--- Vanrise/Code/Vanrise.Analytic.Web/Analytic/Constants/AnalyticItemTypeEnum.js
+++ Vanrise/Code/Vanrise.Analytic.Web/Analytic/Constants/AnalyticItemTypeEnum.js
@@ -1,5 +1,5 @@
 ﻿app.constant('VR_Analytic__AnalyticTypeEnum', {
-    Dimension: { value: 1, description: "Dimension" },
-    Measure: { value: 2, description: "Measure" },
-    Join: { value: 3, description: "Join" }
+    Dimension: { value: 1, description: "Dimension", editor: "vr-analytic-itemconfig-dimension-editor" },
+    Measure: { value: 2, description: "Measure", editor: "vr-analytic-itemconfig-join-editor" },
+    Join: { value: 3, description: "Join", editor: "vr-analytic-itemconfig-measure-editor" }
 });
