aa0052ec93f1b0a72272bcc6408138096271a328
walid.mogharbel <walid.mogharbel@vanrise.tfs.local>
2016-04-28 11:39:13 +0000

    Working on Migration Module Phase 1 : BE
    git-tfs-id: [http://developmentvlan:8585/tfs/vanrise.collection]$/;C32442

--- TOneV2/Code/TOneV2/TOne.WhS.BusinessEntity.Web/WhS_BusinessEntity/Directives/MainExtensions/SourceSwitchReader/whs-be-sourceswitchreader.js
+++ TOneV2/Code/TOneV2/TOne.WhS.BusinessEntity.Web/WhS_BusinessEntity/Directives/MainExtensions/SourceSwitchReader/whs-be-sourceswitchreader.js
@@ -2,122 +2,63 @@
 
 app.directive("whsBeSourceswitchreader", ['UtilsService', 'VRUIUtilsService', 'VRNotificationService', 'WhS_BE_SwitchAPIService',
     function (UtilsService, VRUIUtilsService, VRNotificationService, WhS_BE_SwitchAPIService) {
-    
-    var directiveDefinitionObject = {
-        restrict: "E",
-        scope: {
-            onReady: "="
-        },
-        controller: function ($scope, $element, $attrs) {
-            var ctrl = this;
 
-            var directiveConstructor = new DirectiveConstructor($scope, ctrl);
-            directiveConstructor.initializeController();
-        },
-        controllerAs: "ctrl",
-        bindToController: true,
-        compile: function (element, attrs) {
-            return {
-                pre: function ($scope, iElem, iAttrs, ctrl) {
+        var directiveDefinitionObject = {
+            restrict: "E",
+            scope: {
+                onReady: "="
+            },
+            controller: function ($scope, $element, $attrs) {
+                var ctrl = this;
 
-                }
-            }
-        },
-        templateUrl: "/Client/Modules/Whs_BusinessEntity/Directives/MainExtensions/SourceSwitchReader/Templates/SourceSwitchReader.html"
-    };
+                var directiveConstructor = new DirectiveConstructor($scope, ctrl);
+                directiveConstructor.initializeController();
+            },
+            controllerAs: "ctrl",
+            bindToController: true,
+            compile: function (element, attrs) {
+                return {
+                    pre: function ($scope, iElem, iAttrs, ctrl) {
 
-    function DirectiveConstructor($scope, ctrl) {
-
-        var sourceTemplateDirectiveAPI;
-        var sourceDirectiveReadyPromiseDeferred;
-
-        function initializeController() {
-            $scope.sourceTypeTemplates = [];
-            $scope.onSourceTypeDirectiveReady = function (api) {
-                sourceTemplateDirectiveAPI = api;
-                var setLoader = function (value) { $scope.isLoadingSourceTypeDirective = value };
-                VRUIUtilsService.callDirectiveLoadOrResolvePromise($scope, sourceTemplateDirectiveAPI, undefined, setLoader, sourceDirectiveReadyPromiseDeferred);
-            }
-
-            defineAPI();
-        }
-
-        function defineAPI() {
-           
-            var api = {};
-
-            api.getData = function () {
-                var schedulerTaskAction;
-                if ($scope.selectedSourceTypeTemplate != undefined) {
-                    if (sourceTemplateDirectiveAPI != undefined) {
-                        schedulerTaskAction = {};
-                        schedulerTaskAction.$type = "TOne.WhS.DBSync.Business.SwitchSyncTaskActionArgument, TOne.WhS.DBSync.Business",
-                        schedulerTaskAction.SourceSwitchReader = sourceTemplateDirectiveAPI.getData();
-                        schedulerTaskAction.SourceSwitchReader.ConfigId = $scope.selectedSourceTypeTemplate.TemplateConfigID;
                     }
                 }
-                console.log('schedulerTaskAction')
-                console.log(schedulerTaskAction)
+            },
+            templateUrl: "/Client/Modules/Whs_BusinessEntity/Directives/MainExtensions/SourceSwitchReader/Templates/SourceSwitchReader.html"
+        };
 
-                console.log('sourceTemplateDirectiveAPI.getData()')
-                console.log(sourceTemplateDirectiveAPI.getData())
+        function DirectiveConstructor($scope, ctrl) {
 
-
-                return schedulerTaskAction;
-            };
-
-            api.load = function (payload) {
-                console.log('payload')
-                console.log(payload)
-
-                var promises = [];
-                var sourceConfigId;
-
-                if (payload != undefined && payload.data != undefined && payload.data.SourceSwitchReader != undefined) {
-                    sourceConfigId = payload.data.SourceSwitchReader.ConfigId;
-                }
-
-                var loadSwitchSourcePromise = WhS_BE_SwitchAPIService.GetSwitchSourceTemplates().then(function (response) {
-                    
-                    angular.forEach(response, function (item) {
-                        $scope.sourceTypeTemplates.push(item);
-                    });
-
-                    if (sourceConfigId != undefined)
-                        $scope.selectedSourceTypeTemplate = UtilsService.getItemByVal($scope.sourceTypeTemplates, sourceConfigId, "TemplateConfigID");
-
-                });
-                promises.push(loadSwitchSourcePromise);
-
-                if (sourceConfigId != undefined)
-                {
-                    sourceDirectiveReadyPromiseDeferred = UtilsService.createPromiseDeferred();
-
-                    var loadSourceTemplatePromiseDeferred = UtilsService.createPromiseDeferred();
-                    sourceDirectiveReadyPromiseDeferred.promise.then(function () {
-                        var sourceSwitchReaderPayload;
-
-                        if (payload != undefined && payload.data != undefined && payload.data.SourceSwitchReader != undefined) {
-                            sourceSwitchReaderPayload = {
-                                connectionString: payload.data.SourceSwitchReader.ConnectionString
-                            };
-                        }
-
-                        VRUIUtilsService.callDirectiveLoad(sourceTemplateDirectiveAPI, sourceSwitchReaderPayload, loadSourceTemplatePromiseDeferred);
-                    });
-
-                    promises.push(loadSourceTemplatePromiseDeferred.promise);
-                }
-                
-                return UtilsService.waitMultiplePromises(promises);
+            function initializeController() {
+                defineAPI();
             }
 
-            if (ctrl.onReady != null)
-                ctrl.onReady(api);
+            function defineAPI() {
+
+                var api = {};
+
+                api.getData = function () {
+                    var schedulerTaskAction;
+                        schedulerTaskAction = {};
+                        schedulerTaskAction.$type = "TOne.WhS.DBSync.Business.SwitchSyncTaskActionArgument, TOne.WhS.DBSync.Business",
+                        schedulerTaskAction.SourceSwitchReader = {
+                            $type: "TOne.WhS.DBSync.Business.SourceSwitchesReaders.SwitchTOneV1Reader, TOne.WhS.DBSync.Business",
+                            ConnectionString: $scope.connectionString
+                        };
+                    return schedulerTaskAction;
+                };
+
+                api.load = function (payload) {
+                    if (payload != undefined && payload.data != undefined) {
+                        $scope.connectionString = payload.data.SourceSwitchReader.ConnectionString;
+                    }
+                }
+
+                if (ctrl.onReady != null)
+                    ctrl.onReady(api);
+            }
+
+            this.initializeController = initializeController;
         }
 
-        this.initializeController = initializeController;
-    }
-
-    return directiveDefinitionObject;
-}]);
+        return directiveDefinitionObject;
+    }]);
--- TOneV2/Code/TOneV2/TOne.WhS.DBSync.Entities/Switch/SourceSwitchReader.cs
+++ TOneV2/Code/TOneV2/TOne.WhS.DBSync.Entities/Switch/SourceSwitchReader.cs
@@ -5,8 +5,6 @@ namespace TOne.WhS.DBSync.Entities
 {
     public abstract class SourceSwitchReader : IMigrationSourceItemReader<SourceSwitch> 
     {
-        public int ConfigId { get; set; }
-
         public abstract IEnumerable<SourceSwitch> GetSourceItems();
     }
 }
