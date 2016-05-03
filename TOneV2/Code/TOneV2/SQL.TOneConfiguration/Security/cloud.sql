ad6b39180262870e786521efa5d3dee7caca379d
Ismail Zein <VANRISE\ismail.zein>
2016-05-03 05:58:36 +0000

    update sql project
    git-tfs-id: [http://developmentvlan:8585/tfs/vanrise.collection]$/;C32475

--- TOneV2/Code/TOneV2/SQL.TOneConfiguration/Security/cloud.sql
+++
@@ -1,5 +0,0 @@
-﻿CREATE SCHEMA [cloud]
-    AUTHORIZATION [dbo];
-
-
-
--- TOneV2/Code/TOneV2/SQL.TOneConfiguration/cloud/Tables/CloudApplicationUser.sql
+++
@@ -1,8 +0,0 @@
-﻿CREATE TABLE [cloud].[CloudApplicationUser] (
-    [ApplicationID] INT            NULL,
-    [UserID]        INT            NULL,
-    [Settings]      NVARCHAR (MAX) NULL,
-    [CreatedTime]   DATETIME       NULL,
-    [timestamp]     ROWVERSION     NULL
-);
-
