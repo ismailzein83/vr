7c2ff1dde04bc6c8588b40dc7d6a3123c761e260
Ismail Zein <VANRISE\ismail.zein>
2016-05-04 12:24:04 +0000

    sql project
    git-tfs-id: [http://developmentvlan:8585/tfs/vanrise.collection]$/;C32518

---
+++ TOneV2/Code/TOneV2/SQL.TOneConfiguration/sec/Stored Procedures/sp_CloudAuthServer_GetFirst.sql
@@ -0,0 +1,19 @@
+﻿-- =============================================
+-- Author:		<Author,,Name>
+-- Create date: <Create Date,,>
+-- Description:	<Description,,>
+-- =============================================
+CREATE PROCEDURE sec.sp_CloudAuthServer_GetFirst
+AS
+BEGIN
+	-- SET NOCOUNT ON added to prevent extra result sets from
+	-- interfering with SELECT statements.
+	SET NOCOUNT ON;
+
+    -- Insert statements for procedure here
+	SELECT TOP 1 [ID]
+      ,[Settings]
+      ,[ApplicationIdentification]
+  FROM [sec].[CloudAuthServer]
+  
+END
--- TOneV2/Code/TOneV2/SQL.TOneConfiguration/sec/Tables/CloudAuthServer.sql
+++ TOneV2/Code/TOneV2/SQL.TOneConfiguration/sec/Tables/CloudAuthServer.sql
@@ -1,9 +1,9 @@
-﻿CREATE TABLE [sec].[CloudConnection] (
+﻿CREATE TABLE [sec].[CloudAuthServer] (
     [ID]                        INT            IDENTITY (1, 1) NOT NULL,
     [Settings]                  NVARCHAR (MAX) NULL,
     [ApplicationIdentification] NVARCHAR (MAX) NULL,
-    [CreatedTime]               DATETIME       CONSTRAINT [DF_CloudHost_CreatedTime] DEFAULT (getdate()) NULL,
+    [CreatedTime]               DATETIME       CONSTRAINT [DF_CloudAuthServer_CreatedTime] DEFAULT (getdate()) NULL,
     [timestamp]                 ROWVERSION     NULL,
-    CONSTRAINT [PK_CloudHost] PRIMARY KEY CLUSTERED ([ID] ASC)
+    CONSTRAINT [PK_CloudAuthServer] PRIMARY KEY CLUSTERED ([ID] ASC)
 );
 
