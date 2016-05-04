61627dbc70be9844377bfca6fa0d3214e59ebdfc
Wissam Ajamy <VANRISE\wissam.ajamy>
2016-05-04 11:58:01 +0000

    update structure from CDRAnalysisMobile_WF
    git-tfs-id: [http://developmentvlan:8585/tfs/vanrise.collection]$/;C32510

--- CDRAnalysis/CDRAnalysis/FraudAnalysis/Tables/RelatedNumbers.sql
+++
@@ -1,8 +0,0 @@
-﻿CREATE TABLE [FraudAnalysis].[RelatedNumbers] (
-    [AccountNumber]  VARCHAR (30)  NOT NULL,
-    [RelatedNumbers] VARCHAR (MAX) NOT NULL,
-    PRIMARY KEY CLUSTERED ([AccountNumber] ASC)
-);
-
-
-
