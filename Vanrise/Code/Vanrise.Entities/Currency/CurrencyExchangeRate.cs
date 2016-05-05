f4e33198e77fcf4548f1d2d20fcb5a4c35687685
walid.mogharbel <walid.mogharbel@vanrise.tfs.local>
2016-05-05 15:17:54 +0000

    inherited class Vanrise.Entities.EntitySynchronization.IItem for purpose of migration
    git-tfs-id: [http://developmentvlan:8585/tfs/vanrise.collection]$/;C32585

--- Vanrise/Code/Vanrise.Entities/Currency/CurrencyExchangeRate.cs
+++ Vanrise/Code/Vanrise.Entities/Currency/CurrencyExchangeRate.cs
@@ -3,10 +3,11 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Vanrise.Entities.EntitySynchronization;
 
 namespace Vanrise.Entities
 {
-    public class CurrencyExchangeRate
+    public class CurrencyExchangeRate : Vanrise.Entities.EntitySynchronization.IItem
     {
 
         public long CurrencyExchangeRateId { get; set; }
@@ -15,5 +16,17 @@ namespace Vanrise.Entities
         public Decimal Rate { get; set; }
 
         public DateTime ExchangeDate { get; set; }
+
+        long IItem.ItemId
+        {
+            get
+            {
+                return CurrencyExchangeRateId;
+            }
+            set
+            {
+                this.CurrencyExchangeRateId = (int)value;
+            }
+        }
     }
 }
