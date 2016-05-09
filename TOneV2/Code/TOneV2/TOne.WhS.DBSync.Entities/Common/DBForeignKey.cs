523a6a858ac35008442fe511e6dd4080179c63d9
walid.mogharbel <walid.mogharbel@vanrise.tfs.local>
2016-05-09 15:34:16 +0000

    Working on Migration Module Phase 1 : BE
    git-tfs-id: [http://developmentvlan:8585/tfs/vanrise.collection]$/;C32632

---
+++ TOneV2/Code/TOneV2/TOne.WhS.DBSync.Entities/Common/DBForeignKey.cs
@@ -0,0 +1,10 @@
+﻿
+namespace TOne.WhS.DBSync.Entities
+{
+    public class DBForeignKey
+    {
+        public string ReferencedKey { get; set; }
+        public string ReferencedTable { get; set; }
+        public string ReferencedTableSchema { get; set; }
+    }
+}
---
+++ TOneV2/Code/TOneV2/TOne.WhS.DBSync.Entities/Common/DBTable.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.SqlServer.Management.Smo;
+using System.Collections.Generic;
+
+namespace TOne.WhS.DBSync.Entities
+{
+    public class DBTable
+    {
+        public string Name { get; set; }
+        public string Schema { get; set; }
+        public string Database { get; set; }
+        public string ScriptedIndexes { get; set; }
+        public string ScriptedFKs { get; set; }
+        public string ScriptedTempTable { get; set; }
+        public List<DBForeignKey> DBFKs { get; set; }
+        public Table Info { get; set; }
+        public bool DroppedOriginal { get; set; }
+    }
+}
