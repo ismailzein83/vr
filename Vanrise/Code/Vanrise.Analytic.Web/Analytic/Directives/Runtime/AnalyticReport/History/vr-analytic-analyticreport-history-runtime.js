2d84233b5346e168b990ed69376dc66eb8776455
Samer Haidar <VANRISE\samer.haidar>
2016-05-10 12:21:52 +0000

    adding two new directive for runtime
    git-tfs-id: [http://developmentvlan:8585/tfs/vanrise.collection]$/;C32664

---
+++ Vanrise/Code/Vanrise.Analytic.Web/Analytic/Directives/Runtime/AnalyticReport/History/vr-analytic-analyticreport-history-runtime.js
@@ -0,0 +1,55 @@
+﻿(function (app) {
+
+    'use strict';
+
+    HistoryAnalyticReportDirective.$inject = ["UtilsService", 'VRUIUtilsService', 'Analytic_AnalyticService'];
+
+    function HistoryAnalyticReportDirective(UtilsService, VRUIUtilsService, Analytic_AnalyticService) {
+        return {
+            restrict: "E",
+            scope: {
+                onReady: "=",
+                normalColNum: '@',
+                isrequired: '=',
+                customvalidate: '=',
+                type: '='
+            },
+            controller: function ($scope, $element, $attrs) {
+                var ctrl = this;
+                var historyAnalyticReport = new HistoryAnalyticReport($scope, ctrl, $attrs);
+                historyAnalyticReport.initializeController();
+            },
+            controllerAs: "Ctrl",
+            bindToController: true,
+            templateUrl: "/Client/Modules/Analytic/Directives/Runtime/AnalyticReport/History/Templates/HistoryAnalyticReportRuntimeTemplates.html"
+
+        };
+        function HistoryAnalyticReport($scope, ctrl, $attrs) {
+            this.initializeController = initializeController;
+            
+            function initializeController() {
+                $scope.scopeModel = {};
+
+               
+
+                defineAPI();
+            }
+
+            function defineAPI() {
+                var api = {};
+
+                api.load = function (payload) {
+                   
+
+                };
+
+              
+                if (ctrl.onReady != undefined && typeof (ctrl.onReady) == 'function') {
+                    ctrl.onReady(api);
+                }
+
+            }
+        }
+    }
+    app.directive('vrAnalyticAnalyticreportHistoryRuntime', HistoryAnalyticReportDirective);
+})(app);
