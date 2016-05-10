4551a2ca500568f75d5cfc3cd0bd3795b5434450
Samer Haidar <VANRISE\samer.haidar>
2016-05-10 09:09:08 +0000

    fixing issue
    git-tfs-id: [http://developmentvlan:8585/tfs/vanrise.collection]$/;C32652

--- Vanrise/Code/Vanrise.GenericData.Web/VR_GenericData/Directives/MainExtensions/MappingSteps/GeneralSteps/ExecuteTransformation/vr-genericdata-datatransformation-executetransformationstep.js
+++ Vanrise/Code/Vanrise.GenericData.Web/VR_GenericData/Directives/MainExtensions/MappingSteps/GeneralSteps/ExecuteTransformation/vr-genericdata-datatransformation-executetransformationstep.js
@@ -61,8 +61,8 @@ app.directive('vrGenericdataDatatransformationExecutetransformationstep', ['Util
                                         loadPromiseDeferred: UtilsService.createPromiseDeferred()
                                     };
                                     var payload;
-                                    if (mainPayload != undefined && mainPayload.RecordsMapping != undefined && mainPayload.RecordsMapping.length > 0)
-                                        payload = mainPayload.RecordsMapping[i].Value;
+                                    if (mainPayload != undefined && mainPayload.RecordsMapping != undefined)
+                                        payload = mainPayload.RecordsMapping[i] !=undefined? mainPayload.RecordsMapping[i].Value:undefined;
                                     addFilterItemToGrid(filterItem, payload);
                                 }
                             }
@@ -72,31 +72,7 @@ app.directive('vrGenericdataDatatransformationExecutetransformationstep', ['Util
                     }
                 }
 
-                function addFilterItemToGrid(filterItem, payload) {
-                    var dataItem = {
-                        FieldName: filterItem.Record.RecordName,
-                        Title: filterItem.Record.RecordName
-                    };
-                    var dataItemPayload = {};
-
-                    if (mainPayload != undefined) {
-                        dataItemPayload.context = mainPayload.context;
-
-                    }
-                    if (payload != undefined)
-                        dataItemPayload.selectedRecords = payload;
-                    dataItem.onSourceMappingReady = function (api) {
-                        dataItem.directiveAPI = api;
-                        filterItem.readyPromiseDeferred.resolve();
-                    };
-
-                    filterItem.readyPromiseDeferred.promise
-                        .then(function () {
-                            VRUIUtilsService.callDirectiveLoad(dataItem.directiveAPI, dataItemPayload, filterItem.loadPromiseDeferred);
-                        });
-
-                    $scope.recordsMapping.push(dataItem);
-                }
+              
                 defineAPI();
             }
 
@@ -118,6 +94,28 @@ app.directive('vrGenericdataDatatransformationExecutetransformationstep', ['Util
                         VRUIUtilsService.callDirectiveLoad(dataTransformationSelectorDirectiveReadyAPI, payloadSelector, loadDataTransformationSelectorPromiseDeferred);
                     });
                     promises.push(loadDataTransformationSelectorPromiseDeferred.promise);
+
+                    if (payload != undefined && payload.stepDetails != undefined && payload.stepDetails.DataTransformationId!=undefined)
+                    {
+                        VR_GenericData_DataTransformationDefinitionAPIService.GetDataTransformationDefinition(payload.stepDetails.DataTransformationId).then(function (response) {
+                            if (response && response.RecordTypes) {
+                                $scope.recordsMapping.length = 0;
+                                for (var i = 0; i < response.RecordTypes.length; i++) {
+                                    var record = response.RecordTypes[i];
+                                    var filterItem = {
+                                        Record: record,
+                                        readyPromiseDeferred: UtilsService.createPromiseDeferred(),
+                                        loadPromiseDeferred: UtilsService.createPromiseDeferred()
+                                    };
+                                    promises.push(filterItem.loadPromiseDeferred.promise);
+                                    var payloadData;
+                                    if (payload.stepDetails.RecordsMapping !=undefined)
+                                        payloadData = payload.stepDetails.RecordsMapping[i] != undefined ? mainPayload.RecordsMapping[i].Value : undefined;
+                                    addFilterItemToGrid(filterItem, payloadData);
+                                }
+                            }
+                        });
+                    }
                     return UtilsService.waitMultiplePromises(promises);
                 }
 
@@ -147,6 +145,33 @@ app.directive('vrGenericdataDatatransformationExecutetransformationstep', ['Util
                 if (ctrl.onReady != null)
                     ctrl.onReady(api);
             }
+
+            function addFilterItemToGrid(filterItem, payload) {
+                var dataItem = {
+                    FieldName: filterItem.Record.RecordName,
+                    Title: filterItem.Record.RecordName
+                };
+                var dataItemPayload = {};
+
+                if (mainPayload != undefined) {
+                    dataItemPayload.context = mainPayload.context;
+
+                }
+                if (payload != undefined)
+                    dataItemPayload.selectedRecords = payload;
+                dataItem.onSourceMappingReady = function (api) {
+                    dataItem.directiveAPI = api;
+                    filterItem.readyPromiseDeferred.resolve();
+                };
+
+                filterItem.readyPromiseDeferred.promise
+                    .then(function () {
+                        VRUIUtilsService.callDirectiveLoad(dataItem.directiveAPI, dataItemPayload, filterItem.loadPromiseDeferred);
+                    });
+
+                $scope.recordsMapping.push(dataItem);
+            }
+
             this.initializeController = initializeController;
         }
         return directiveDefinitionObject;
