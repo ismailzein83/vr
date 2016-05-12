f076f88c99aa3d360f79625421835bfa59292074
Ismail Zein <VANRISE\ismail.zein>
2016-05-12 13:22:29 +0000

    added ConnectorZoneInfoManager
    git-tfs-id: [http://developmentvlan:8585/tfs/vanrise.collection]$/;C32711

---
+++ QualityMeasurement/Code/QM.CLITester.Business/ConnectorZoneInfoManager.cs
@@ -0,0 +1,17 @@
+﻿using QM.CLITester.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace QM.CLITester.Business
+{
+    public class ConnectorZoneInfoManager
+    {
+        public List<ConnectorZoneInfo> GetZones(string connectorType)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ QualityMeasurement/Code/QM.CLITester.Entities/ConnectorZoneInfo.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace QM.CLITester.Entities
+{
+    public class ConnectorZoneInfo
+    {
+        public string ConnectorZoneId { get; set; }
+
+        public Object ZoneInfo { get; set; }
+    }
+}
