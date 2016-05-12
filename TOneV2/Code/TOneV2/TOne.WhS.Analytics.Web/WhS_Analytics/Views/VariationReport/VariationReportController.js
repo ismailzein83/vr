11b0b57b780b3b7a558681ed142d8c5d38e08cc1
mohamad.elhariri <mohamad.elhariri@vanrise.tfs.local>
2016-05-12 11:49:28 +0000

    User Story 1220: Added the Top filter and added the avg values to the chart
    git-tfs-id: [http://developmentvlan:8585/tfs/vanrise.collection]$/;C32701

--- TOneV2/Code/TOneV2/TOne.WhS.Analytics.Web/WhS_Analytics/Views/VariationReport/VariationReportController.js
+++ TOneV2/Code/TOneV2/TOne.WhS.Analytics.Web/WhS_Analytics/Views/VariationReport/VariationReportController.js
@@ -7,6 +7,7 @@
     function VariationReportController($scope, WhS_Analytics_VariationReportTypeEnum, WhS_Analytics_VariationReportTimePeriodEnum, UtilsService) {
 
         var gridAPI;
+        var chartAPI;
 
         defineScope();
         load();
@@ -20,24 +21,57 @@
 
             $scope.scopeModel.toDate = new Date('2016-01-04');
 
-            $scope.scopeModel.periodTypes = UtilsService.getArrayEnum(WhS_Analytics_VariationReportTimePeriodEnum);;
+            $scope.scopeModel.periodTypes = UtilsService.getArrayEnum(WhS_Analytics_VariationReportTimePeriodEnum);
             $scope.scopeModel.selectedPeriodType = UtilsService.getEnum(WhS_Analytics_VariationReportTimePeriodEnum, 'value', WhS_Analytics_VariationReportTimePeriodEnum.Daily.value);
 
             $scope.scopeModel.numberOfPeriods = 3;
             $scope.scopeModel.showGrid = false;
+            $scope.scopeModel.showChart = false;
+
+            $scope.scopeModel.top = 5;
+
+            $scope.scopeModel.onReportTypeSelectionChanged = function (selectedReportType) {
+                $scope.scopeModel.showGrid = false;
+                $scope.scopeModel.showChart = false;
+            };
 
             $scope.scopeModel.onGridReady = function (api) {
                 gridAPI = api;
             };
 
+            $scope.scopeModel.onChartReady = function (api) {
+                chartAPI = api;
+            };
+
             $scope.scopeModel.search = function () {
                 $scope.scopeModel.showGrid = true;
-                return gridAPI.load(getGridQuery());
+                return loadAllControls();
             };
         }
 
         function load() {
             
+        }
+
+        function loadAllControls() {
+            var promises = [];
+
+            var loadGridPromise = loadGrid();
+            promises.push(loadGridPromise);
+
+            var loadChartDeferred = UtilsService.createPromiseDeferred();
+            promises.push(loadChartDeferred.promise);
+
+            loadGridPromise.then(function () {
+                loadChart();
+                loadChartDeferred.resolve();
+            });
+
+            return UtilsService.waitMultiplePromises(promises);
+        }
+
+        function loadGrid() {
+            return gridAPI.load(getGridQuery());
         }
 
         function getGridQuery() {
@@ -48,6 +82,93 @@
                 NumberOfPeriods: $scope.scopeModel.numberOfPeriods,
                 GroupByProfile: $scope.scopeModel.groupByProfile
             };
+        }
+
+        function loadChart()
+        {
+            switch ($scope.scopeModel.selectedReportType.value)
+            {
+                case WhS_Analytics_VariationReportTypeEnum.InOutBoundMinutes.value:
+                case WhS_Analytics_VariationReportTypeEnum.InOutBoundAmount.value:
+                    return;
+            }
+
+            $scope.scopeModel.showChart = true;
+
+            var reportData = gridAPI.getData();
+            
+            var chartConfig =
+            {
+                type: 'column',
+                yAxisTitle: getReportResultTypeDescription()
+            };
+
+            var seriesConfig = { titlePath: 'seriesTitle' };
+
+            var seriesList = [];
+
+            seriesList.push
+            ({
+                seriesTitle: 'AVG',
+                seriesData: []
+            });
+
+            for (var i = 0; i < reportData.timePeriods.length; i++)
+            {
+                var seriesItem =
+                {
+                    seriesTitle: reportData.timePeriods[i].PeriodDescription,
+                    seriesData: []
+                };
+                seriesList.push(seriesItem);
+            }
+
+            var seriesDefinition = [];
+            for (var i = 0; i < reportData.records.length && i < $scope.scopeModel.top; i++)
+            {
+                var record = reportData.records[i];
+
+                seriesDefinition.push
+                ({
+                    title: record.DimensionName,
+                    valuePath: 'seriesData[' + i + ']',
+                    type: 'column'
+                });
+
+                seriesList[0].seriesData[i] = record.Average;
+
+                for (var j = 1; j <= reportData.timePeriods.length; j++)
+                {
+                    seriesList[j].seriesData[i] = record.TimePeriodValues[j - 1];
+                }
+            }
+
+            
+
+            chartAPI.renderChart(seriesList, chartConfig, seriesDefinition, seriesConfig);
+        }
+
+        function getReportResultTypeDescription()
+        {
+            switch ($scope.scopeModel.selectedReportType.value)
+            {
+                case WhS_Analytics_VariationReportTypeEnum.InBoundMinutes.value:
+                case WhS_Analytics_VariationReportTypeEnum.OutBoundMinutes.value:
+                case WhS_Analytics_VariationReportTypeEnum.InOutBoundMinutes.value:
+                case WhS_Analytics_VariationReportTypeEnum.TopDestinationMinutes.value:
+                    return 'Duration';
+
+                case WhS_Analytics_VariationReportTypeEnum.InBoundAmount.value:
+                case WhS_Analytics_VariationReportTypeEnum.OutBoundAmount.value:
+                case WhS_Analytics_VariationReportTypeEnum.InOutBoundAmount.value:
+                case WhS_Analytics_VariationReportTypeEnum.TopDestinationAmount.value:
+                    return 'Amount';
+
+                case WhS_Analytics_VariationReportTypeEnum.Profit.value:
+                case WhS_Analytics_VariationReportTypeEnum.OutBoundProfit.value:
+                case WhS_Analytics_VariationReportTypeEnum.TopDestinationProfit.value:
+                    return 'Profit';
+            }
         }
     }
 
