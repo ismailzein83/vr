61a9c0f0a1984b4592c5a98ad269d7ae368ac171
Ismail Zein <VANRISE\ismail.zein>
2016-05-13 06:18:12 +0000

    minor change
    git-tfs-id: [http://developmentvlan:8585/tfs/vanrise.collection]$/;C32728

--- QualityMeasurement/Code/QM.CLITester.iTestIntegration/Constants.cs
+++ QualityMeasurement/Code/QM.CLITester.iTestIntegration/Constants.cs
@@ -8,6 +8,6 @@ namespace QM.CLITester.iTestIntegration
 {
     internal class Constants
     {
-        internal const string CONNECTOR_TYPE = "CLITester_Connector_VA";
+        internal const string CONNECTOR_TYPE = "CLITester_Connector_VI";
     }
 }
