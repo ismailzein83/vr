149cd76fcfbc76e176111868bb15afbb47e5b063
Ismail Zein <VANRISE\ismail.zein>
2016-05-17 09:19:15 +0000

    added BELookupRuleDefinitionManager
    git-tfs-id: [http://developmentvlan:8585/tfs/vanrise.collection]$/;C32817

---
+++ Vanrise/Code/Vanrise.GenericData.Business/BELookupRules/BELookupRuleDefinitionManager.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Vanrise.GenericData.Business
+{
+    public class BELookupRuleDefinitionManager
+    {
+        internal class CacheManager : Vanrise.Caching.BaseCacheManager
+        {
+            protected override bool ShouldSetCacheExpired(object parameter)
+            {
+                throw new NotImplementedException();
+            }
+        }
+    }
+}
