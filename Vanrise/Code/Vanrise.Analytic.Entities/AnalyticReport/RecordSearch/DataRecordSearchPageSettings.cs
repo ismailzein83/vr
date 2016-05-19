6f3466f0e5c378932af729831bca246800bcbd84
anthony douaihy <VANRISE\anthony.douaihy>
2016-05-19 06:24:51 +0000

    Add Settings class for record search
    git-tfs-id: [http://developmentvlan:8585/tfs/vanrise.collection]$/;C32894

---
+++ Vanrise/Code/Vanrise.Analytic.Entities/AnalyticReport/RecordSearch/DataRecordSearchPageSettings.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Vanrise.Analytic.Entities
+{
+    public class DataRecordSearchPageSettings : AnalyticReportSettings
+    {
+        public List<DRSearchPageStorageSource> Sources { get; set; }
+    }
+
+    public class DRSearchPageStorageSource
+    {
+        public string Title { get; set; }
+
+        public int DataRecordTypeId { get; set; }
+
+        public List<int> RecordStorageIds { get; set; }
+
+        public List<DRSearchPageGridColumn> GridColumns { get; set; }
+    }
+
+    public class DRSearchPageGridColumn
+    {
+        public string FieldName { get; set; }
+
+        public string FieldTitle { get; set; }
+    }
+}
