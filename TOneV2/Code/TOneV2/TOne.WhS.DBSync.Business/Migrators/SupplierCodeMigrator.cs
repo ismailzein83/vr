5d128ddaada7d37761514b02eb8c1425c938b83b
walid.mogharbel <walid.mogharbel@vanrise.tfs.local>
2016-05-19 16:08:04 +0000

    Working on Migration Module Phase 1 : BE
    git-tfs-id: [http://developmentvlan:8585/tfs/vanrise.collection]$/;C32933

---
+++ TOneV2/Code/TOneV2/TOne.WhS.DBSync.Business/Migrators/SupplierCodeMigrator.cs
@@ -0,0 +1,94 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using TOne.WhS.BusinessEntity.Business;
+using TOne.WhS.BusinessEntity.Entities;
+using TOne.WhS.DBSync.Data.SQL;
+using TOne.WhS.DBSync.Data.SQL.Common;
+using TOne.WhS.DBSync.Entities;
+using Vanrise.Common.Business;
+using Vanrise.Entities;
+
+namespace TOne.WhS.DBSync.Business
+{
+    public class SupplierCodeMigrator : Migrator<SourceCode, SupplierCode>
+    {
+        SupplierCodeDBSyncDataManager dbSyncDataManager;
+        SourceCodeDataManager dataManager;
+
+        public SupplierCodeMigrator(MigrationContext context)
+            : base(context)
+        {
+            dbSyncDataManager = new SupplierCodeDBSyncDataManager(Context.UseTempTables);
+            dataManager = new SourceCodeDataManager(Context.ConnectionString);
+            TableName = dbSyncDataManager.GetTableName();
+        }
+
+        public override void Migrate()
+        {
+            base.Migrate();
+        }
+
+        public override void AddItems(List<SupplierCode> itemsToAdd)
+        {
+            long startingId;
+            ReserveIDRange(itemsToAdd.Count(), out startingId);
+            dbSyncDataManager.ApplySupplierCodesToTemp(itemsToAdd, startingId);
+            DBTable dbTableSupplierCode = Context.DBTables[DBTableName.SupplierCode];
+            if (dbTableSupplierCode != null)
+                dbTableSupplierCode.Records = dbSyncDataManager.GetSupplierCodes();
+        }
+
+        public override IEnumerable<SourceCode> GetSourceItems()
+        {
+            return dataManager.GetSourceCodes(true);
+        }
+
+        public override SupplierCode BuildItemFromSource(SourceCode sourceItem)
+        {
+            DBTable dbTableSupplierZone = Context.DBTables[DBTableName.SupplierZone];
+            DBTable dbTableCountry = Context.DBTables[DBTableName.Country];
+            DBTable dbTableCodeGroup = Context.DBTables[DBTableName.CodeGroup];
+
+            if (dbTableCountry != null && dbTableSupplierZone != null && dbTableCodeGroup != null)
+            {
+                Dictionary<string, SupplierZone> allSupplierZones = (Dictionary<string, SupplierZone>)dbTableSupplierZone.Records;
+                Dictionary<string, Country> allCountries = (Dictionary<string, Country>)dbTableCountry.Records;
+                Dictionary<string, CodeGroup> allCodeGroups = (Dictionary<string, CodeGroup>)dbTableCodeGroup.Records;
+
+                SupplierZone supplierZone = null;
+                if (allSupplierZones != null)
+                    allSupplierZones.TryGetValue(sourceItem.ZoneId.ToString(), out supplierZone);
+
+                Country country = null;
+                if (allCountries != null)
+                    allCountries.TryGetValue(sourceItem.CodeGroup, out country);
+
+                CodeGroup codeGroup = null;
+                if (allCodeGroups != null & country != null)
+                    allCodeGroups.Values.Where(x => x.CountryId == country.CountryId);
+
+
+                if (codeGroup != null & supplierZone != null)
+                    return new SupplierCode
+                    {
+                        Code = sourceItem.Code,
+                        BED = sourceItem.BeginEffectiveDate,
+                        CodeGroupId = codeGroup.CodeGroupId,
+                        EED = sourceItem.EndEffectiveDate,
+                        ZoneId = supplierZone.SupplierZoneId,
+                        SourceId = sourceItem.SourceId
+                    };
+                else
+                    return null;
+
+            }
+            else
+                return null;
+        }
+
+        internal static void ReserveIDRange(int nbOfIds, out long startingId)
+        {
+            IDManager.Instance.ReserveIDRange(typeof(SupplierCodeManager), nbOfIds, out startingId);
+        }
+    }
+}
