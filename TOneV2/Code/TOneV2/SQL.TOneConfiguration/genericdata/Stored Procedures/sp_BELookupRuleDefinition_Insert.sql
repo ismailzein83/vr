b6607ba9f299f32645723bf7268c8a5659ded973
Wissam Ajamy <VANRISE\wissam.ajamy>
2016-05-19 07:12:14 +0000

    Update from ToneConfiguration DB
    git-tfs-id: [http://developmentvlan:8585/tfs/vanrise.collection]$/;C32895

---
+++ TOneV2/Code/TOneV2/SQL.TOneConfiguration/genericdata/Stored Procedures/sp_BELookupRuleDefinition_Insert.sql
@@ -0,0 +1,18 @@
+﻿-- =============================================
+-- Author:		<Author,,Name>
+-- Create date: <Create Date,,>
+-- Description:	<Description,,>
+-- =============================================
+CREATE PROCEDURE genericdata.sp_BELookupRuleDefinition_Insert
+	@Name NVARCHAR(450),
+	@Details NVARCHAR(MAX),
+	@ID INT	OUT
+AS
+BEGIN
+	IF NOT EXISTS (SELECT 1 FROM genericdata.BELookupRuleDefinition WHERE Name = @Name)
+	BEGIN
+		INSERT INTO genericdata.BELookupRuleDefinition (Name, Details)
+		VALUES (@Name, @Details)
+		SET @ID = @@IDENTITY
+	END
+END
---
+++ TOneV2/Code/TOneV2/SQL.TOneConfiguration/genericdata/Stored Procedures/sp_BELookupRuleDefinition_Update.sql
@@ -0,0 +1,18 @@
+﻿-- =============================================
+-- Author:		<Author,,Name>
+-- Create date: <Create Date,,>
+-- Description:	<Description,,>
+-- =============================================
+CREATE PROCEDURE [genericdata].[sp_BELookupRuleDefinition_Update]
+	@ID INT,
+	@Name NVARCHAR(450),
+	@Details NVARCHAR(MAX)
+AS
+BEGIN
+	IF NOT EXISTS (SELECT 1 FROM genericdata.BELookupRuleDefinition WHERE Name = @Name AND ID != @ID)
+	BEGIN
+		UPDATE genericdata.BELookupRuleDefinition
+		SET Name = @Name, Details = @Details
+		WHERE ID = @ID
+	END
+END
