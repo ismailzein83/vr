1cda163b290e008f9828d166b0d325824112322c
Samer Haidar <VANRISE\samer.haidar>
2016-05-19 11:31:32 +0000

    Implementing data record field choice editor
    git-tfs-id: [http://developmentvlan:8585/tfs/vanrise.collection]$/;C32914

--- Vanrise/Code/Vanrise.GenericData.Web/VR_GenericData/Views/DataRecordFieldChoice/DataRecordFieldChoiceEditorController.js
+++ Vanrise/Code/Vanrise.GenericData.Web/VR_GenericData/Views/DataRecordFieldChoice/DataRecordFieldChoiceEditorController.js
@@ -1,188 +1,189 @@
-﻿//(function (appControllers) {
+﻿(function (appControllers) {
 
-//    "use strict";
-//    DataRecordFieldChoiceEditorController.$inject = ["$scope", "VR_GenericData_DataRecordFieldChoiceAPIService", "VRNavigationService", "VRNotificationService", "UtilsService", "VRUIUtilsService"];
+    "use strict";
+    DataRecordFieldChoiceEditorController.$inject = ["$scope", "VR_GenericData_DataRecordFieldChoiceAPIService", "VRNavigationService", "VRNotificationService", "UtilsService", "VRUIUtilsService"];
 
-//    function DataRecordFieldChoiceEditorController($scope, VR_GenericData_DataRecordFieldChoiceAPIService, VRNavigationService, VRNotificationService, UtilsService, VRUIUtilsService) {
+    function DataRecordFieldChoiceEditorController($scope, VR_GenericData_DataRecordFieldChoiceAPIService, VRNavigationService, VRNotificationService, UtilsService, VRUIUtilsService) {
 
-//        var dataRecordFieldChoiceId;
-//        var dataRecordFieldChoiceEntity;
-//        var isEditMode;
-//        var dataRecordFieldChoiceSelectorAPI;
-//        var dataRecordFieldChoiceReadyPromiseDeferred = UtilsService.createPromiseDeferred();
-//        var dataRecordFieldChoiceDirectiveAPI;
-//        var dataRecordFieldChoiceConfigReadyPromiseDeferred; // = UtilsService.createPromiseDeferred();
-//        loadParameters();
-//        defineScope();
-//        load();
+        var dataRecordFieldChoiceId;
+        var dataRecordFieldChoiceEntity;
+        var isEditMode;
 
-//        function loadParameters() {
-//            var parameters = VRNavigationService.getParameters($scope);
+        loadParameters();
+        defineScope();
+        load();
 
-//            if (parameters != undefined && parameters != null)
-//                dataRecordFieldChoiceId = parameters.DataRecordFieldChoiceId;
+        function loadParameters() {
+            var parameters = VRNavigationService.getParameters($scope);
 
-//            isEditMode = (dataRecordFieldChoiceId != undefined);
-//        }
+            if (parameters != undefined && parameters != null)
+                dataRecordFieldChoiceId = parameters.DataRecordFieldChoiceId;
 
-//        function defineScope() {
+            isEditMode = (dataRecordFieldChoiceId != undefined);
+        }
 
-//            $scope.onReadyDataRecordFieldChoiceConfigSelector = function (api) {
-//                dataRecordFieldChoiceSelectorAPI = api;
-//                dataRecordFieldChoiceReadyPromiseDeferred.resolve();
-//            }
-//            $scope.onReadyDataRecordFieldChoiceConfigDirective = function (api) {
-//                dataRecordFieldChoiceDirectiveAPI = api;
+        function defineScope() {
+            $scope.scopeModel = {};
+            $scope.scopeModel.values = [];
 
-//                var setLoader = function (value) { $scope.isLoadingConfigDirective = value };
-//                VRUIUtilsService.callDirectiveLoadOrResolvePromise($scope, dataRecordFieldChoiceDirectiveAPI, undefined, setLoader, dataRecordFieldChoiceConfigReadyPromiseDeferred);
-//            }
-//            $scope.saveDataRecordFieldChoice = function () {
-//                $scope.isLoading = true;
-//                if (isEditMode)
-//                    return updateDataRecordFieldChoice();
-//                else
-//                    return insertDataRecordFieldChoice();
-//            };
-//            $scope.hasSaveDataRecordFieldChoice = function () {
-//                if (isEditMode) {
-//                    return VR_GenericData_DataRecordFieldChoiceAPIService.HasUpdateDataRecordFieldChoice();
-//                }
-//                else {
-//                    return VR_GenericData_DataRecordFieldChoiceAPIService.HasUpdateDataRecordFieldChoice();
-//                }
-//            };
-//            $scope.close = function () {
-//                $scope.modalContext.closeModal()
-//            }
-//        }
+            $scope.scopeModel.Id = $scope.scopeModel.values.length + 1;
+            $scope.scopeModel.isValid = function () {
+                if ($scope.scopeModel.values != undefined && $scope.scopeModel.values.length > 0)
+                    return null;
+                return "You Should Add At Least One Choice."
+            }
+            $scope.scopeModel.disableAddButton = true;
+            $scope.scopeModel.addValue = function () {
+                $scope.scopeModel.values.push(AddChoice($scope.scopeModel.value));
+                $scope.scopeModel.Id = $scope.scopeModel.values.length + 1;
+                $scope.scopeModel.value = undefined;
+                $scope.scopeModel.disableAddButton = true;
+            }
+            $scope.scopeModel.onValueChange = function (value) {
+                $scope.scopeModel.disableAddButton = value == undefined || (UtilsService.getItemIndexByVal($scope.scopeModel.values, $scope.scopeModel.value, "Text") != -1 || UtilsService.getItemIndexByVal($scope.scopeModel.values, $scope.scopeModel.Id, "Value") != -1);
+            }
 
-//        function load() {
-//            $scope.isLoading = true;
-//            if (isEditMode) {
-//                GetDataRecordFieldChoice().then(function () {
-//                    loadAllControls().finally(function () {
-//                        dataRecordFieldChoiceEntity = undefined;
-//                    }).catch(function (error) {
-//                        VRNotificationService.notifyExceptionWithClose(error, $scope);
-//                        $scope.isLoading = false;
-//                    });
-//                });
-//            }
-//            else {
-//                loadAllControls();
-//            }
+            $scope.scopeModel.saveDataRecordFieldChoice = function () {
+                $scope.isLoading = true;
+                if (isEditMode)
+                    return updateDataRecordFieldChoice();
+                else
+                    return insertDataRecordFieldChoice();
+            };
+            $scope.scopeModel.hasSaveDataRecordFieldChoice = function () {
+                if (isEditMode) {
+                    return VR_GenericData_DataRecordFieldChoiceAPIService.HasUpdateDataRecordFieldChoice();
+                }
+                else {
+                    return VR_GenericData_DataRecordFieldChoiceAPIService.HasUpdateDataRecordFieldChoice();
+                }
+            };
+            $scope.scopeModel.close = function () {
+                $scope.modalContext.closeModal()
+            }
+        }
+        function AddChoice(choice) {
+            var obj = {
+                Value: $scope.scopeModel.Id,
+                Text: choice
+            }
+            return obj;
+        }
 
-//        }
+        function load() {
+            $scope.scopeModel.isLoading = true;
+            if (isEditMode) {
+                getDataRecordFieldChoice().then(function () {
+                    loadAllControls().finally(function () {
+                        dataRecordFieldChoiceEntity = undefined;
+                    }).catch(function (error) {
+                        VRNotificationService.notifyExceptionWithClose(error, $scope);
+                        $scope.scopeModel.isLoading = false;
+                    });
+                });
+            }
+            else {
+                loadAllControls();
+            }
 
-//        function GetDataRecordFieldChoice() {
-//            return VR_GenericData_DataRecordFieldChoiceAPIService.GetDataRecordFieldChoice(dataRecordFieldChoiceId).then(function (response) {
-//                dataRecordFieldChoiceEntity = response;
-//            });
-//        }
+        }
 
-//        function loadAllControls() {
-//            return UtilsService.waitMultipleAsyncOperations([setTitle, loadStaticData, loadDataRecordFieldChoiceConfigSelector, loadDataRecordFieldChoiceConfigDirective])
-//               .catch(function (error) {
-//                   VRNotificationService.notifyExceptionWithClose(error, $scope);
-//               })
-//              .finally(function () {
-//                  $scope.isLoading = false;
-//              });
-//        }
+        function getDataRecordFieldChoice() {
+            return VR_GenericData_DataRecordFieldChoiceAPIService.GetDataRecordFieldChoice(dataRecordFieldChoiceId).then(function (response) {
+                dataRecordFieldChoiceEntity = response;
+            });
+        }
 
-//        function setTitle() {
-//            if (isEditMode && dataRecordFieldChoiceEntity != undefined)
-//                $scope.title = UtilsService.buildTitleForUpdateEditor(dataRecordFieldChoiceEntity.Name, "Data Store");
-//            else
-//                $scope.title = UtilsService.buildTitleForAddEditor("Data Store");
-//        }
+        function loadAllControls() {
+            return UtilsService.waitMultipleAsyncOperations([setTitle, loadStaticData])
+               .catch(function (error) {
+                   VRNotificationService.notifyExceptionWithClose(error, $scope);
+               })
+              .finally(function () {
+                  $scope.scopeModel.isLoading = false;
+              });
+        }
 
-//        function loadStaticData() {
-//            if (dataRecordFieldChoiceEntity == undefined)
-//                return;
-//            $scope.name = dataRecordFieldChoiceEntity.Name;
-//        }
+        function setTitle() {
+            if (isEditMode && dataRecordFieldChoiceEntity != undefined)
+                $scope.title = UtilsService.buildTitleForUpdateEditor(dataRecordFieldChoiceEntity.Name, "Data Record Field Choice");
+            else
+                $scope.title = UtilsService.buildTitleForAddEditor("Data Record Field Choice");
+        }
 
-//        function loadDataRecordFieldChoiceConfigSelector() {
-//            var loadDataRecordFieldChoicePromiseDeferred = UtilsService.createPromiseDeferred();
-//            dataRecordFieldChoiceReadyPromiseDeferred.promise.then(function () {
-//                if (dataRecordFieldChoiceEntity != undefined && dataRecordFieldChoiceEntity.Settings != undefined)
-//                    var payLoad = {
-//                        selectedIds: (dataRecordFieldChoiceEntity != undefined && dataRecordFieldChoiceEntity.Settings != undefined && dataRecordFieldChoiceEntity.Settings.ConfigId != undefined) ? dataRecordFieldChoiceEntity.Settings.ConfigId : undefined
-//                    }
-//                VRUIUtilsService.callDirectiveLoad(dataRecordFieldChoiceSelectorAPI, payLoad, loadDataRecordFieldChoicePromiseDeferred);
-//            });
-//            return loadDataRecordFieldChoicePromiseDeferred.promise;
-//        }
-//        function loadDataRecordFieldChoiceConfigDirective() {
-//            if (dataRecordFieldChoiceEntity != undefined && dataRecordFieldChoiceEntity.Settings != undefined) {
-//                dataRecordFieldChoiceConfigReadyPromiseDeferred = UtilsService.createPromiseDeferred();
-//                var loadDataRecordFieldChoiceConfigPromiseDeferred = UtilsService.createPromiseDeferred();
-//                dataRecordFieldChoiceConfigReadyPromiseDeferred.promise.then(function () {
-//                    dataRecordFieldChoiceConfigReadyPromiseDeferred = undefined;
-//                    var directivePayLoad = {
-//                        data: dataRecordFieldChoiceEntity.Settings
-//                    }
-//                    VRUIUtilsService.callDirectiveLoad(dataRecordFieldChoiceDirectiveAPI, directivePayLoad, loadDataRecordFieldChoiceConfigPromiseDeferred);
-//                });
-//                return loadDataRecordFieldChoiceConfigPromiseDeferred.promise;
-//            }
-
-
-//        }
+        function loadStaticData() {
+            if (dataRecordFieldChoiceEntity == undefined)
+                return;
+            $scope.scopeModel.choiceName = dataRecordFieldChoiceEntity.Name;
+            if(dataRecordFieldChoiceEntity.Settings != undefined && dataRecordFieldChoiceEntity.Settings.Choices !=undefined)
+            {
+                for(var i=0; i<dataRecordFieldChoiceEntity.Settings.Choices.length;i++)
+                {
+                   
+                    var choice = dataRecordFieldChoiceEntity.Settings.Choices[i];
+                    $scope.scopeModel.Id = choice.Value + 1;
+                    $scope.scopeModel.values.push({
+                        Value: choice.Value,
+                        Text: choice.Text
+                    });
+                }
+            }
 
 
-//        function updateDataRecordFieldChoice() {
-//            var dataRecordFieldChoiceObj = buildDataSToreObjFromScope();
-//            return VR_GenericData_DataRecordFieldChoiceAPIService.UpdateDataRecordFieldChoice(dataRecordFieldChoiceObj)
-//                .then(function (response) {
-//                    $scope.isLoading = false;
-//                    if (VRNotificationService.notifyOnItemUpdated("Data Store", response, "Name")) {
+        }
 
-//                        if ($scope.onDataRecordFieldChoiceUpdated != undefined)
-//                            $scope.onDataRecordFieldChoiceUpdated(response.UpdatedObject);
+       
+        function updateDataRecordFieldChoice() {
+            var dataRecordFieldChoiceObj = buildDataSToreObjFromScope();
+            return VR_GenericData_DataRecordFieldChoiceAPIService.UpdateDataRecordFieldChoice(dataRecordFieldChoiceObj)
+                .then(function (response) {
+                    $scope.scopeModel.isLoading = false;
+                    if (VRNotificationService.notifyOnItemUpdated("Data Record Field Choice", response, "Name")) {
 
-//                        $scope.modalContext.closeModal();
-//                    }
-//                })
-//                .catch(function (error) {
-//                    VRNotificationService.notifyException(error, $scope);
-//                }).finally(function () {
-//                    $scope.isLoading = false;
-//                });
-//        }
+                        if ($scope.onDataRecordFieldChoiceUpdated != undefined)
+                            $scope.onDataRecordFieldChoiceUpdated(response.UpdatedObject);
 
-//        function insertDataRecordFieldChoice() {
-//            var dataRecordFieldChoiceObj = buildDataSToreObjFromScope();
-//            return VR_GenericData_DataRecordFieldChoiceAPIService.AddDataRecordFieldChoice(dataRecordFieldChoiceObj)
-//                .then(function (response) {
-//                    $scope.isLoading = false;
-//                    if (VRNotificationService.notifyOnItemAdded("Data Store", response, "Name")) {
-//                        if ($scope.onDataRecordFieldChoiceAdded != undefined)
-//                            $scope.onDataRecordFieldChoiceAdded(response.InsertedObject);
+                        $scope.modalContext.closeModal();
+                    }
+                })
+                .catch(function (error) {
+                    VRNotificationService.notifyException(error, $scope);
+                }).finally(function () {
+                    $scope.scopeModel.isLoading = false;
+                });
+        }
 
-//                        $scope.modalContext.closeModal();
-//                    }
-//                })
-//                .catch(function (error) {
-//                    VRNotificationService.notifyException(error, $scope);
-//                }).finally(function () {
-//                    $scope.isLoading = false;
-//                });
-//        }
+        function insertDataRecordFieldChoice() {
+            var dataRecordFieldChoiceObj = buildDataSToreObjFromScope();
+            return VR_GenericData_DataRecordFieldChoiceAPIService.AddDataRecordFieldChoice(dataRecordFieldChoiceObj)
+                .then(function (response) {
+                    $scope.scopeModel.isLoading = false;
+                    if (VRNotificationService.notifyOnItemAdded("Data Record Field Choice", response, "Name")) {
+                        if ($scope.onDataRecordFieldChoiceAdded != undefined)
+                            $scope.onDataRecordFieldChoiceAdded(response.InsertedObject);
 
-//        function buildDataSToreObjFromScope() {
-//            var settings = dataRecordFieldChoiceDirectiveAPI.getData();
-//            settings.ConfigId = dataRecordFieldChoiceSelectorAPI.getSelectedIds();
-//            return {
-//                DataRecordFieldChoiceId: dataRecordFieldChoiceId,
-//                Name: $scope.name,
-//                Settings: settings
-//            };
-//        }
-//    }
+                        $scope.modalContext.closeModal();
+                    }
+                })
+                .catch(function (error) {
+                    VRNotificationService.notifyException(error, $scope);
+                }).finally(function () {
+                    $scope.scopeModel.isLoading = false;
+                });
+        }
+ 
+        function buildDataSToreObjFromScope() {
+            var settings = {
+                Choices: $scope.scopeModel.values
+            };
+            return {
+                DataRecordFieldChoiceId: dataRecordFieldChoiceId,
+                Name: $scope.scopeModel.choiceName,
+                Settings: settings
+            };
+        }
+    }
 
-//    appControllers.controller("VR_GenericData_DataRecordFieldChoiceEditorController", DataRecordFieldChoiceEditorController);
+    appControllers.controller("VR_GenericData_DataRecordFieldChoiceEditorController", DataRecordFieldChoiceEditorController);
 
-//})(appControllers);
+})(appControllers);
