e09e53226d74b2470f7e94e46eea5ae36e4e9670
Samer Haidar <VANRISE\samer.haidar>
2016-05-25 12:26:59 +0000

    fixing issue
    git-tfs-id: [http://developmentvlan:8585/tfs/vanrise.collection]$/;C33037

--- Vanrise/Code/Vanrise.Web/Client/Javascripts/Directives/TimeRange/vr-timerange-directive.js
+++ Vanrise/Code/Vanrise.Web/Client/Javascripts/Directives/TimeRange/vr-timerange-directive.js
@@ -52,7 +52,7 @@ function (UtilsService, VRUIUtilsService, PeriodEnum, VRValidationService) {
         return   '<vr-row removeline>'
                +  periodSection
                + '<vr-columns width="' + attrs.width + '">'
-               + '    <vr-datetimepicker type="' + attrs.type + '" value="ctrltimerange.from" label="From" customvalidate="validateDateTime()" isrequired="ctrltimerange.isrequired" ' + onblurchanged + '></vr-datetimepicker>'
+               + '    <vr-datetimepicker type="' + attrs.type + '" value="ctrltimerange.from" label="From" customvalidate="validateDateTime()" isrequired="true" ' + onblurchanged + '></vr-datetimepicker>'
                + '</vr-columns>'
                + '<vr-columns width="' + attrs.width + '">'
                + '    <vr-datetimepicker type="' + attrs.type + '" value="ctrltimerange.to" label="To" customvalidate="validateDateTime()" isrequired="ctrltimerange.isrequired" ' + onblurchanged + '></vr-datetimepicker>'
