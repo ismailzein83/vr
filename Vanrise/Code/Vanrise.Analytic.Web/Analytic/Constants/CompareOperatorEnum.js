bd757f635e7cf91f4a53fd43aaf488c7546c7374
Samer Haidar <VANRISE\samer.haidar>
2016-05-26 09:04:12 +0000

    adding enum
    git-tfs-id: [http://developmentvlan:8585/tfs/vanrise.collection]$/;C33073

---
+++ Vanrise/Code/Vanrise.Analytic.Web/Analytic/Constants/CompareOperatorEnum.js
@@ -0,0 +1,8 @@
+﻿app.constant('VR_Analytic_CompareOperatorEnum', {
+    Equals: { value: 0, description: "Equals" },
+    NotEquals: { value: 1, description: "Not Equals" },
+    Greater: { value: 2, description: "Greater" },
+    GreaterOrEquals: { value: 3, description: "Greater Or Equals" },
+    Less: { value: 4, description: "Less" },
+    LessOrEquals: { value: 5, description: "Less Or Equals" },
+});
