61ec0b201fb5823f719298e4d3e7f53a2d14dcf4
Mohammad Mokdad <VANRISE\mohammad.mokdad>
2016-05-26 09:17:27 +0000

    MHD checkin
    git-tfs-id: [http://developmentvlan:8585/tfs/vanrise.collection]$/;C33076

---
+++ TOneV2.Tests/UnitTests/TOneV2.UnitTests/TONEAPI/ClassCode/City.cs
@@ -0,0 +1,35 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Web;
+
+namespace TONEAPI.ClassCode
+{
+    public class City
+    {
+
+        public int CountryId { get; set; }
+        public string Name { get; set; }
+        public int CityId { get; set; }
+
+    }
+
+    public class CityDetail
+    {
+        public City Entity { get; set; }
+        public string CountryName  { get; set; }
+      
+
+    }
+
+
+    public class jasonresp
+    {
+        public List<CityDetail> Data { get; set; }
+    
+    }
+
+
+}
