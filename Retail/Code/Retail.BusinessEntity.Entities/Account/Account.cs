a4a9b8b6a7b7b16089b9970e2242dbe56532c0c7
mohamad.elhariri <mohamad.elhariri@vanrise.tfs.local>
2016-06-02 09:29:28 +0000

    Refactored the editor of the Account entity
    git-tfs-id: [http://developmentvlan:8585/tfs/vanrise.collection]$/;C33334

--- Retail/Code/Retail.BusinessEntity.Entities/Account/Account.cs
+++ Retail/Code/Retail.BusinessEntity.Entities/Account/Account.cs
@@ -19,7 +19,12 @@ namespace Retail.BusinessEntity.Entities
 
     public enum AccountType { Residential = 0, Enterprise = 1 }
 
-    public class AccountSettings { }
+    public class AccountSettings
+    {
+        public AccountContactSettings ContactSettings { get; set; }
+
+        public AccountBillingSettings BillingSettings { get; set; }
+    }
 
     public class Account : BaseAccount
     {
@@ -30,4 +35,44 @@ namespace Retail.BusinessEntity.Entities
     {
 
     }
+
+    public class AccountContactSettings
+    {
+        public int? CountryId { get; set; }
+
+        public int? CityId { get; set; }
+
+        public string Town { get; set; }
+
+        public string Street { get; set; }
+
+        public string POBox { get; set; }
+
+        public string Email { get; set; }
+
+        public string Phone { get; set; }
+
+        public string Fax { get; set; }
+    }
+
+    public class AccountBillingSettings
+    {
+        public string ContactName { get; set; }
+
+        public string ContactEmail { get; set; }
+
+        public string ContactPhone { get; set; }
+
+        public PaymentMethod? PaymentMethod { get; set; }
+
+        public string CardNumber { get; set; }
+
+        public string CardHolderName { get; set; }
+
+        public DateTime? CardExpirationDate { get; set; }
+
+        public string CardSecurityCode { get; set; }
+    }
+
+    public enum PaymentMethod { Card = 0, PayPal = 1 }
 }
--- Retail/Code/Retail.BusinessEntity.Web/Retail_BusinessEntity/Views/Account/AccountEditorController.js
+++ Retail/Code/Retail.BusinessEntity.Web/Retail_BusinessEntity/Views/Account/AccountEditorController.js
@@ -2,15 +2,22 @@
 
     'use strict';
 
-    AccountEditorController.$inject = ['$scope', 'Retail_BE_AccountAPIService', 'Retail_BE_AccountTypeEnum', 'UtilsService', 'VRNavigationService', 'VRNotificationService'];
+    AccountEditorController.$inject = ['$scope', 'Retail_BE_AccountAPIService', 'Retail_BE_AccountTypeEnum', 'Retail_BE_PaymentMethodEnum', 'UtilsService', 'VRUIUtilsService', 'VRNavigationService', 'VRNotificationService'];
 
-    function AccountEditorController($scope, Retail_BE_AccountAPIService, Retail_BE_AccountTypeEnum, UtilsService, VRNavigationService, VRNotificationService)
+    function AccountEditorController($scope, Retail_BE_AccountAPIService, Retail_BE_AccountTypeEnum, Retail_BE_PaymentMethodEnum, UtilsService, VRUIUtilsService, VRNavigationService, VRNotificationService)
     {
         var isEditMode;
 
         var accountId;
         var accountEntity;
         var parentAccountId;
+
+        var countrySelectorAPI;
+        var countrySelectorReadyDeferred = UtilsService.createPromiseDeferred();
+        var countrySelectedDeferred;
+
+        var citySelectorAPI;
+        var citySelectorReadyDeferred = UtilsService.createPromiseDeferred();
 
         loadParameters();
         defineScope();
@@ -32,9 +39,46 @@
         function defineScope()
         {
             $scope.scopeModel = {};
+            $scope.scopeModel.contactSettings = {};
+            $scope.scopeModel.billingSettings = {};
 
             $scope.scopeModel.accountTypes = UtilsService.getArrayEnum(Retail_BE_AccountTypeEnum);
-            $scope.scopeModel.showParentAccountName = (parentAccountId != undefined);
+            $scope.scopeModel.paymentMethods = UtilsService.getArrayEnum(Retail_BE_PaymentMethodEnum);
+
+            $scope.scopeModel.onCountrySelectorReady = function (api) {
+                countrySelectorAPI = api;
+                countrySelectorReadyDeferred.resolve();
+            };
+
+            $scope.scopeModel.onCountrySelectionChanged = function ()
+            {
+                var selectedCountryId = countrySelectorAPI.getSelectedIds();
+                if (selectedCountryId != undefined) {
+                    var setLoader = function (value) { $scope.scopeModel.isLoadingCities = value };
+                    var citySelectorPayload = {
+                        countryId: selectedCountryId
+                    };
+                    VRUIUtilsService.callDirectiveLoadOrResolvePromise($scope, citySelectorAPI, citySelectorPayload, setLoader, countrySelectedDeferred);
+                }
+                else if (citySelectorAPI != undefined) {
+                    citySelectorAPI.clearDataSource();
+                }
+            };
+
+            $scope.scopeModel.onCitySelectorReady = function (api) {
+                citySelectorAPI = api;
+                citySelectorReadyDeferred.resolve();
+            };
+
+            $scope.scopeModel.validateCardNumber = function () {
+                return ($scope.scopeModel.billingSettings.cardNumber == undefined || $scope.scopeModel.billingSettings.cardNumber.length < 16) ?
+                    'Card number is a 16 digit number' : null;
+            };
+
+            $scope.scopeModel.validateCardSecurityCode = function () {
+                return ($scope.scopeModel.billingSettings.cardSecurityCode == undefined || $scope.scopeModel.billingSettings.cardSecurityCode.length < 3) ?
+                    'Card security code is a 3 or 4 digit number' : null;
+            };
 
             $scope.scopeModel.save = function ()
             {
@@ -79,29 +123,32 @@
         }
 
         function loadAllControls() {
-            return UtilsService.waitMultipleAsyncOperations([setTitle, getParentAccountName, loadStaticData]).catch(function (error) {
-                   VRNotificationService.notifyExceptionWithClose(error, $scope);
-               }).finally(function () {
+            return UtilsService.waitMultipleAsyncOperations([setTitle, loadCountryCitySection, loadStaticData]).catch(function (error) {
+                VRNotificationService.notifyExceptionWithClose(error, $scope);
+            }).finally(function () {
                    $scope.scopeModel.isLoading = false;
               });
         }
 
         function setTitle()
         {
+            var title;
             if (isEditMode) {
                 var accountName = (accountEntity != undefined) ? accountEntity.Name : undefined;
-                $scope.title = UtilsService.buildTitleForUpdateEditor(accountName, 'Account');
+                title = UtilsService.buildTitleForUpdateEditor(accountName, 'Account');
             }
             else {
-                $scope.title = UtilsService.buildTitleForAddEditor('Account');
+                title = UtilsService.buildTitleForAddEditor('Account');
             }
-        }
 
-        function getParentAccountName() {
             if (parentAccountId != undefined) {
                 return Retail_BE_AccountAPIService.GetAccountName(parentAccountId).then(function (response) {
-                    $scope.scopeModel.parentAccountName = response;
+                    var titleExtension = ' for ' + response;
+                    $scope.title = title += titleExtension;
                 });
+            }
+            else {
+                $scope.title = title;
             }
         }
 
@@ -112,6 +159,68 @@
 
             $scope.scopeModel.name = accountEntity.Name;
             $scope.scopeModel.selectedAccountType = UtilsService.getItemByVal($scope.scopeModel.accountTypes, accountEntity.Type, 'value');
+
+            $scope.scopeModel.contactSettings.town = accountEntity.Settings.ContactSettings.Town;
+            $scope.scopeModel.contactSettings.street = accountEntity.Settings.ContactSettings.Street;
+            $scope.scopeModel.contactSettings.poBox = accountEntity.Settings.ContactSettings.POBox;
+            $scope.scopeModel.contactSettings.email = accountEntity.Settings.ContactSettings.Email;
+            $scope.scopeModel.contactSettings.phone = accountEntity.Settings.ContactSettings.Phone;
+            $scope.scopeModel.contactSettings.fax = accountEntity.Settings.ContactSettings.Fax;
+
+            $scope.scopeModel.billingSettings.contactName = accountEntity.Settings.BillingSettings.ContactName;
+            $scope.scopeModel.billingSettings.contactEmail = accountEntity.Settings.BillingSettings.ContactEmail;
+            $scope.scopeModel.billingSettings.contactPhone = accountEntity.Settings.BillingSettings.ContactPhone;
+
+            if (accountEntity.Settings.BillingSettings.PaymentMethod != null)
+            {
+                $scope.scopeModel.billingSettings.selectedPaymentMethod =
+                UtilsService.getItemByVal($scope.scopeModel.paymentMethods, accountEntity.Settings.BillingSettings.PaymentMethod, 'value');
+
+                $scope.scopeModel.billingSettings.cardNumber = accountEntity.Settings.BillingSettings.CardNumber;
+                $scope.scopeModel.billingSettings.cardHolderName = accountEntity.Settings.BillingSettings.CardHolderName;
+                $scope.scopeModel.billingSettings.cardExpirationDate = accountEntity.Settings.BillingSettings.CardExpirationDate;
+                $scope.scopeModel.billingSettings.cardSecurityCode = accountEntity.Settings.BillingSettings.CardSecurityCode;
+            }
+        }
+
+        function loadCountryCitySection()
+        {
+            var promises = [];
+
+            var countrySelectorLoadDeferred = UtilsService.createPromiseDeferred();
+            promises.push(countrySelectorLoadDeferred.promise);
+
+            var countrySelectorPayload;
+
+            if (accountEntity != undefined && accountEntity.Settings.ContactSettings != null && accountEntity.Settings.ContactSettings.CountryId != null)
+            {
+                countrySelectorPayload = {
+                    selectedIds: accountEntity.Settings.ContactSettings.CountryId
+                };
+                countrySelectedDeferred = UtilsService.createPromiseDeferred();
+            }
+
+            countrySelectorReadyDeferred.promise.then(function () {
+                VRUIUtilsService.callDirectiveLoad(countrySelectorAPI, countrySelectorPayload, countrySelectorLoadDeferred);
+            });
+
+            if (accountEntity != undefined && accountEntity.Settings.ContactSettings != null && accountEntity.Settings.ContactSettings.CityId != null)
+            {
+                var citySelectorLoadDeferred = UtilsService.createPromiseDeferred();
+                promises.push(citySelectorLoadDeferred.promise);
+
+                UtilsService.waitMultiplePromises([citySelectorReadyDeferred.promise, countrySelectedDeferred.promise]).then(function () {
+                    var citySelectorPayload = {
+                        countryId: accountEntity.Settings.ContactSettings.CountryId,
+                        selectedIds: accountEntity.Settings.ContactSettings.CityId
+                    }
+
+                    VRUIUtilsService.callDirectiveLoad(citySelectorAPI, citySelectorPayload, citySelectorLoadDeferred);
+                    countrySelectedDeferred = undefined;
+                });
+            }
+
+            return UtilsService.waitMultiplePromises(promises);
         }
 
         function insertAccount()
@@ -162,9 +271,36 @@
             var obj = {
                 AccountId: accountId,
                 Name: $scope.scopeModel.name,
-                Type: $scope.scopeModel.selectedAccountType.value,
-                Settings: null
+                Type: $scope.scopeModel.selectedAccountType.value
             };
+
+            obj.Settings = {};
+
+            obj.Settings.ContactSettings = {
+                CountryId: countrySelectorAPI.getSelectedIds(),
+                CityId: citySelectorAPI.getSelectedIds(),
+                Town: $scope.scopeModel.contactSettings.town,
+                Street: $scope.scopeModel.contactSettings.street,
+                POBox: $scope.scopeModel.contactSettings.poBox,
+                Email: $scope.scopeModel.contactSettings.email,
+                Phone: $scope.scopeModel.contactSettings.phone,
+                Fax: $scope.scopeModel.contactSettings.fax
+            };
+
+            obj.Settings.BillingSettings = {
+                ContactName: $scope.scopeModel.billingSettings.contactName,
+                ContactEmail: $scope.scopeModel.billingSettings.contactEmail,
+                ContactPhone: $scope.scopeModel.billingSettings.contactPhone
+            };
+
+            if ($scope.scopeModel.billingSettings.selectedPaymentMethod != undefined)
+            {
+                obj.Settings.BillingSettings.PaymentMethod = $scope.scopeModel.billingSettings.selectedPaymentMethod.value;
+                obj.Settings.BillingSettings.CardNumber = $scope.scopeModel.billingSettings.cardNumber;
+                obj.Settings.BillingSettings.CardHolderName = $scope.scopeModel.billingSettings.cardHolderName;
+                obj.Settings.BillingSettings.CardExpirationDate = $scope.scopeModel.billingSettings.cardExpirationDate;
+                obj.Settings.BillingSettings.CardSecurityCode = $scope.scopeModel.billingSettings.cardSecurityCode;
+            }
 
             if (!isEditMode) {
                 obj.ParentAccountId = parentAccountId;
