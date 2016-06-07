0cd8249666634898310aa9418395778020b522b9
walid.mogharbel <walid.mogharbel@vanrise.tfs.local>
2016-06-07 13:01:04 +0000

    refactoring and cleaning references and dependecies
    git-tfs-id: [http://developmentvlan:8585/tfs/vanrise.collection]$/;C33398

--- FZero/Vanrise.CommonLibrary/Config.cs
+++ FZero/Vanrise.CommonLibrary/Config.cs
@@ -1,17 +1,9 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using System.Web.Configuration;
+﻿using System.Web.Configuration;
 
 namespace Vanrise.CommonLibrary
 {
     public static class Config
     {
-        //public static bool CheckPermission = true;
-        //public static int UserId = 6;
-        
         private static string sqlConnectionString;
         public static string SQLConnectionString
         {
--- FZero/Vanrise.CommonLibrary/DbConnection.cs
+++ FZero/Vanrise.CommonLibrary/DbConnection.cs
@@ -1,10 +1,6 @@
 ﻿using System;
-using System.Collections.Generic;
 using System.Data;
 using System.Data.SqlClient;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace Vanrise.CommonLibrary
 {
@@ -19,36 +15,11 @@ namespace Vanrise.CommonLibrary
             set { connectionString = value; }
         }
 
-
         public DbConnection()
         {
             ConnectionString = Config.SQLConnectionString;
             connection = new SqlConnection(ConnectionString);
             OpenConnection();
-        }
-        public DbConnection(string newConnectionString)
-        {
-            ConnectionString = newConnectionString;
-            connection = new SqlConnection(ConnectionString);
-            OpenConnection();
-        }
-
-        public static bool isConnectionAvailable()
-        {
-            bool result;
-            try
-            {
-                //SqlConnection connection = new SqlConnection(Manager.GetConnectionString(Config.ConnectionString, Config.UserID, Config.Password));
-                SqlConnection connection = new SqlConnection(Config.SQLConnectionString);
-                connection.Open();
-                result = true;
-                connection.Close();
-            }
-            catch (Exception)
-            {
-                result = false;
-            }
-            return result;
         }
 
         public bool OpenConnection()
@@ -80,136 +51,18 @@ namespace Vanrise.CommonLibrary
             return true;
         }
 
-        public SqlDataReader GetDataReader(string query)
+        public SqlCommand CreateCommand()
         {
-            return GetDataReader(query, CommandType.Text);
+            SqlCommand cmd = connection.CreateCommand();
+            cmd.CommandTimeout = 0;
+            return cmd;
         }
 
-        public SqlDataReader GetDataReaderFromStoreProcedure(string procedureName)
+        public void Dispose()
         {
-            return GetDataReader(procedureName, CommandType.StoredProcedure);
+            CloseConnection();
+            connection.Dispose();
         }
-
-        public SqlDataReader GetDataReader(string query, CommandType type)
-        {
-            SqlDataReader dr;
-            SqlCommand command;
-            try
-            {
-                command = new SqlCommand(query, connection);
-                command.CommandType = type;
-                dr = command.ExecuteReader();
-                return dr;
-            }
-            catch (Exception err)
-            {
-                FileLogger.Write("Error in DbConnection.ReturnDataReader()", err);
-                return null;
-            }
-        }
-
-        public Object ReturnScalar(string query)
-        {
-            SqlCommand aCommand;
-            try
-            {
-                aCommand = new SqlCommand(query, connection);
-                aCommand.CommandType = CommandType.Text;
-                return aCommand.ExecuteScalar();
-            }
-            catch (Exception err)
-            {
-                FileLogger.Write("Error in Connection.ReturnScalar()", err);
-                return null;
-            }
-        }
-
-        public DataTable ReturnDataTable(string query)
-        {
-            DataSet ds = new DataSet();
-            SqlDataAdapter da = new SqlDataAdapter(query, connection);
-            da.Fill(ds);
-            return ds.Tables[0];
-        }
-
-        public DataTable ReturnDataTable(SqlCommand cmd)
-        {
-            DataSet ds = new DataSet();
-            cmd.Connection = connection;
-            SqlDataAdapter da = new SqlDataAdapter(cmd);
-            da.Fill(ds);
-            return ds.Tables[0];
-        }
-
-        public DataSet ReturnDataSet(string query)
-        {
-            DataSet ds = new DataSet();
-            try
-            {
-                SqlDataAdapter da = new SqlDataAdapter(query, connection);
-                da.Fill(ds);
-            }
-            catch (Exception err)
-            {
-                FileLogger.Write("Error in DataBaseClass.ReturnDataSet(query)", err);
-            }
-            return ds;
-        }
-
-        public int Excute(string query)
-        {
-            int rowsAffected = 0;
-            try
-            {
-                SqlCommand command = new SqlCommand(query, connection);
-                command.CommandType = CommandType.Text;
-                rowsAffected = command.ExecuteNonQuery();
-            }
-            catch (Exception err)
-            {
-                FileLogger.Write("Error in DbConnection.Excute(" + query + ")", err);
-            }
-            return rowsAffected;
-        }
-
-        public static DataTable ChangeDataReader(SqlDataReader dr)
-        {
-            DataTable dt = new DataTable();
-            try
-            {
-                if (dr.HasRows)
-                {
-                    for (int i = 0; i < dr.FieldCount; i++)
-                    {
-                        dt.Columns.Add(dr.GetSchemaTable().Rows[i][0].ToString(), dr.GetFieldType(i));
-                    }
-                }
-                while (dr.Read())
-                {
-                    try
-                    {
-                        DataRow drow = dt.NewRow();
-                        for (int j = 0; j < dr.FieldCount; j++)
-                        {
-                            drow[j] = dr.GetValue(j).ToString();
-                        }
-                        dt.Rows.Add(drow);
-                    }
-                    catch (Exception err)
-                    {
-                        FileLogger.Write("Error in DbConnection.ChangeDataReader(SqlDataReader).While", err);
-                    }
-                }
-                dr.Close();
-
-            }
-            catch (Exception err)
-            {
-                FileLogger.Write("Error in DbConnection.ChangeDataReader(SqlDataReader)", err);
-            }
-            return dt;
-        }
-
 
         public static bool IsAvailable(string connectionString)
         {
@@ -226,20 +79,6 @@ namespace Vanrise.CommonLibrary
             }
 
             return true;
-        }
-
-
-        public SqlCommand CreateCommand()
-        {
-            SqlCommand cmd = connection.CreateCommand();
-            cmd.CommandTimeout = 0;
-            return cmd;
-        }
-
-        public void Dispose()
-        {
-            CloseConnection();
-            connection.Dispose();
         }
     }
 }
