4cefaf53d2c529c32dea3003a32afd93fd7de85f
Mortada Issa <VANRISE\mortada.issa>
2016-06-07 11:33:44 +0000

    Adding new classes for Routing Product management in WF
    git-tfs-id: [http://developmentvlan:8585/tfs/vanrise.collection]$/;C33394

---
+++ TOneV2/Code/TOneV2/TOne.WhS.Sales.Entities/RateManagement/RoutingProduct/ExistingDefaultRoutingProduct.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TOne.WhS.BusinessEntity.Entities;
+
+namespace TOne.WhS.Sales.Entities
+{
+    public class ExistingDefaultRoutingProduct
+    {
+        public int RoutingProductId { get; set; }
+
+        public DefaultRoutingProduct DefaultRoutingProductEntity { get; set; }
+
+        public ChangedDefaultRoutingProduct ChangedRoutingProduct { get; set; }
+
+        public DateTime BED { get; set; }
+
+        public DateTime? EED { get; set; }
+    }
+}
---
+++ TOneV2/Code/TOneV2/TOne.WhS.Sales.Entities/RateManagement/RoutingProduct/NewDefaultRoutingProduct.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TOne.WhS.Sales.Entities
+{
+    //TODO: Renamed it
+    public class NewDefaultRoutingProduct2
+    {
+        public int RoutingProductId { get; set; }
+
+        public DateTime BED { get; set; }
+
+        public DateTime? EED { get; set; }
+    }
+}
