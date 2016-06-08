98a1cb12cae5f47d3b6bbbe089c93fed6fbe86e5
Wissam Ajamy <VANRISE\wissam.ajamy>
2016-06-08 08:54:09 +0000

    update structure from TOne configuration
    git-tfs-id: [http://developmentvlan:8585/tfs/vanrise.collection]$/;C33412

--- TOneV2/Code/TOneV2/SQL.TOneConfiguration/rules/Stored Procedures/sp_Rule_Delete.sql
+++ TOneV2/Code/TOneV2/SQL.TOneConfiguration/rules/Stored Procedures/sp_Rule_Delete.sql
@@ -3,11 +3,23 @@
 -- Create date: <Create Date,,>
 -- Description:	<Description,,>
 -- =============================================
-create PROCEDURE rules.sp_Rule_Delete 
+CREATE PROCEDURE [rules].[sp_Rule_Delete] 
 	@ID INT
 AS
 BEGIN
-	DELETE FROM [rules].[Rule]
-	WHERE ID=@ID
+	DECLARE @TypeId int
+	SELECT @TypeId = TypeID FROM [rules].[Rule] where ID = @ID
+	
+	DELETE FROM [rules].[Rule] WHERE ID=@ID
+	
+	IF @@ROWCOUNT > 0
+	BEGIN
+		--this query increment the timestamp column in order to clears the caches in the application
+		DECLARE @FirstID int
+		SELECT TOP 1 @FirstID = ID FROM [rules].[Rule] where TypeID = @TypeId
+		UPDATE [rules].[Rule] 
+		SET TypeID = TypeID
+		WHERE ID = @FirstID
+	END
 
 END
