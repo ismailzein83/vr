612460eba70e0ff097f27aeaf95216d492a3a1b2
mostafa.jawhari <mostafa.jawhari@vanrise.tfs.local>
2016-06-10 12:42:20 +0000

    No User Stories: adding two new rules to check if code is numeric and updating post deployment script
    git-tfs-id: [http://developmentvlan:8585/tfs/vanrise.collection]$/;C33495

---
+++ TOneV2/Code/TOneV2/TOne.WhS.CodePreparation.Business/BusinessRules/Conditions/ValidateZones/NumericCodeCondition.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TOne.WhS.CodePreparation.Entities.Processing;
+using Vanrise.BusinessProcess.Entities;
+
+namespace TOne.WhS.CodePreparation.Business
+{
+    public class NumericCodeCondition : BusinessRuleCondition
+    {
+        public override bool ShouldValidate(IRuleTarget target)
+        {
+            return (target as ZoneToProcess != null);
+        }
+
+        public override bool Validate(IBusinessRuleConditionValidateContext context)
+        {
+            ZoneToProcess zoneToProcess = context.Target as ZoneToProcess;
+            
+
+            if(zoneToProcess.CodesToAdd != null)
+            {
+                foreach (CodeToAdd codeToAdd in zoneToProcess.CodesToAdd)
+                {
+                    if (!Vanrise.Common.Utilities.IsNumeric(codeToAdd.Code, false))
+                        return false;
+                }
+            }
+
+            if (zoneToProcess.CodesToMove != null)
+            {
+                foreach (CodeToMove codeToMove in zoneToProcess.CodesToMove)
+                {
+                    if (!Vanrise.Common.Utilities.IsNumeric(codeToMove.Code, false))
+                        return false;
+                }
+            }
+
+            if (zoneToProcess.CodesToClose != null)
+            {
+                foreach (CodeToClose codeToClose in zoneToProcess.CodesToClose)
+                {
+                    if (!Vanrise.Common.Utilities.IsNumeric(codeToClose.Code, false))
+                        return false;
+                }
+            }
+
+            return true;
+        }
+
+        public override string GetMessage(IRuleTarget target)
+        {
+            return string.Format("Zone {0} has a Code which is not a numeric", (target as ZoneToProcess).ZoneName);
+        }
+    }
+}
---
+++ TOneV2/Code/TOneV2/TOne.WhS.SupplierPriceList.Business/BusinessRules/Conditions/ValidateCodesZones/NumericCodeCondition.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TOne.WhS.SupplierPriceList.Entities;
+using TOne.WhS.SupplierPriceList.Entities.SPL;
+using Vanrise.BusinessProcess.Entities;
+
+namespace TOne.WhS.SupplierPriceList.Business
+{
+    public class NumericCodeCondition : BusinessRuleCondition
+    {
+        public override bool ShouldValidate(IRuleTarget target)
+        {
+            return (target as ImportedCode != null);
+        }
+
+        public override bool Validate(IBusinessRuleConditionValidateContext context)
+        {
+            ImportedCode importedCode = context.Target as ImportedCode;
+            return Vanrise.Common.Utilities.IsNumeric(importedCode.Code, false);
+        }
+
+        public override string GetMessage(IRuleTarget target)
+        {
+            return string.Format("Code {0} must be numeric", (target as ImportedCode).Code);
+        }
+    }
+}
