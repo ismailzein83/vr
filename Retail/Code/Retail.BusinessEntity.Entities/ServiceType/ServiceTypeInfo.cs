317f3c9497e5199472a7f4ff561e7e4853b2dcf0
mohamad.elhariri <mohamad.elhariri@vanrise.tfs.local>
2016-06-13 10:43:55 +0000

    No User Story: Added ServiceTypeInfo
    git-tfs-id: [http://developmentvlan:8585/tfs/vanrise.collection]$/;C33532

---
+++ Retail/Code/Retail.BusinessEntity.Entities/ServiceType/ServiceTypeInfo.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Retail.BusinessEntity.Entities.ServiceType
+{
+    public class ServiceTypeInfo
+    {
+        public int ServiceTypeId { get; set; }
+
+        public string Name { get; set; }
+    }
+}
