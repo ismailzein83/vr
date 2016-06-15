16d6a1ec8076ff5ec2b2bdd68997f9374dbe4523
Ismail Zein <VANRISE\ismail.zein>
2016-06-15 12:56:13 +0000

    added Switch
    git-tfs-id: [http://developmentvlan:8585/tfs/vanrise.collection]$/;C33636

---
+++ Retail/Code/Retail.BusinessEntity.MainExtensions/SwitchIntegrations/Radius.cs
@@ -0,0 +1,18 @@
+﻿using Retail.BusinessEntity.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Retail.BusinessEntity.MainExtensions.SwitchIntegrations
+{
+    public class Radius : SwitchIntegration
+    {
+        public string ConnectionString { get; set; }
+
+        public string TableName { get; set; }
+
+        public string MappingLogic { get; set; }
+    }
+}
