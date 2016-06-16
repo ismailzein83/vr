1337f000934c91068633b6089b8ca5b2bb4c1e75
anthony douaihy <VANRISE\anthony.douaihy>
2016-06-16 09:39:51 +0000

    define new script for CDR import processing
    git-tfs-id: [http://developmentvlan:8585/tfs/vanrise.collection]$/;C33670

--- TOneV2/Code/TOneV2/SQL.TOneTransaction/TOne.CDRImport.PostDeployment.sql
+++ TOneV2/Code/TOneV2/SQL.TOneTransaction/TOne.CDRImport.PostDeployment.sql
@@ -1,14 +1,15 @@
 ﻿/*
 Post-Deployment Script Template							
 --------------------------------------------------------------------------------------
- This file contains SQL statements that will be appended to the build script.		
- Use SQLCMD syntax to include a file in the post-deployment script.			
- Example:      :r .\myfile.sql								
- Use SQLCMD syntax to reference a variable in the post-deployment script.		
- Example:      :setvar TableName MyTable							
-               SELECT * FROM [$(TableName)]					
+The target of this script is to define default data source for CDR import processing				
 --------------------------------------------------------------------------------------
 */
+--check if Datasource already defined
+if exists (select 1 from [queue].[ExecutionFlowDefinition] where ID=-100)
+BEGIN
+ select 'Data source already defined, you may loose updated mapping and connection string. if you need to override please remove this condition.'
+ RETURN
+ END
 --[queue].[ExecutionFlowDefinition]-----------------------------------------------------------------
 ----------------------------------------------------------------------------------------------------
 set nocount on;
