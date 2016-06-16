0385815f4d5526fcaa3bc25d2509110df35c14cf
anthony douaihy <VANRISE\anthony.douaihy>
2016-06-16 14:17:27 +0000

    CDR Import Voice + SMS + DATA
    git-tfs-id: [http://developmentvlan:8585/tfs/vanrise.collection]$/;C33700

--- Retail/Code/Retail.Runtime/Mappers/RingoMapper.cs
+++ Retail/Code/Retail.Runtime/Mappers/RingoMapper.cs
@@ -29,14 +29,10 @@ namespace Retail.Runtime.Mappers
             LogVerbose("Started");
             Vanrise.Integration.Entities.StreamReaderImportedData ImportedData = ((Vanrise.Integration.Entities.StreamReaderImportedData)(data));
             var voiceCDRs = new List<dynamic>();
-            //var smsCDRs = new List<dynamic>();
-            //var dataCDRs = new List<dynamic>();
 
             var dataRecordTypeManager = new Vanrise.GenericData.Business.DataRecordTypeManager();
 
             Type voiceCDRRuntimeType = dataRecordTypeManager.GetDataRecordRuntimeType("VoiceCDR");
-            //Type smsCDRRuntimeType = dataRecordTypeManager.GetDataRecordRuntimeType("SmsCDR");
-            //Type dataCDRRuntimeType = dataRecordTypeManager.GetDataRecordRuntimeType("DataCDR");
 
             var currentItemCount = 27;
             var headerText = "H";
@@ -65,9 +61,10 @@ namespace Retail.Runtime.Mappers
                 cdr.StartDate = DateTime.ParseExact(rowData[2], "dd/MM/yyyy HH:mm:ss", System.Globalization.CultureInfo.InvariantCulture);
 
                 string parentIdCDR = rowData[23];
-                cdr.ParentIdCDR = !string.IsNullOrEmpty(parentIdCDR) ? long.Parse(parentIdCDR) : default(long);
+                cdr.ParentIdCDR = !string.IsNullOrEmpty(parentIdCDR) ? long.Parse(parentIdCDR) : default(long?);
 
                 cdr.TrafficType = rowData[3];
+                cdr.TypeCalled = rowData[4];
                 cdr.DirectionTraffic = rowData[5];
                 cdr.Calling = rowData[6];
                 cdr.Called = rowData[7];
@@ -79,12 +76,16 @@ namespace Retail.Runtime.Mappers
                 cdr.DestinationArea = rowData[13];
 
                 string duration = rowData[14];
-                cdr.Duration = !string.IsNullOrEmpty(duration) ? decimal.Parse(duration) : default(decimal);
+                cdr.Duration = !string.IsNullOrEmpty(duration) ? decimal.Parse(duration) : default(decimal?);
 
                 cdr.DurationUnit = rowData[15];
 
+                string balance = rowData[17];
+                cdr.Balance = !string.IsNullOrEmpty(balance) ? decimal.Parse(balance) : default(decimal?);
+
                 string amount = rowData[19];
-                cdr.Amount = !string.IsNullOrEmpty(amount) ? decimal.Parse(amount) : default(decimal);
+                cdr.Amount = !string.IsNullOrEmpty(amount) ? decimal.Parse(amount) : default(decimal?);
+
                 cdr.TypeConsumed = rowData[20];
                 cdr.Bag = rowData[18];
                 cdr.PricePlan = rowData[21];
@@ -99,6 +100,175 @@ namespace Retail.Runtime.Mappers
 
             var batch = Vanrise.GenericData.QueueActivators.DataRecordBatch.CreateBatchFromRecords(voiceCDRs, "#RECORDSCOUNT# of Raw CDRs");
             mappedBatches.Add("Voice CDR Storage Stage", batch);
+
+            Vanrise.Integration.Entities.MappingOutput result = new Vanrise.Integration.Entities.MappingOutput();
+            result.Result = Vanrise.Integration.Entities.MappingResult.Valid;
+            LogVerbose("Finished");
+            return result;
+        }
+
+        public Vanrise.Integration.Entities.MappingOutput ImportingSmsCDR_File()
+        {
+            LogVerbose("Started");
+            Vanrise.Integration.Entities.StreamReaderImportedData ImportedData = ((Vanrise.Integration.Entities.StreamReaderImportedData)(data));
+            var smsCDRs = new List<dynamic>();
+
+            var dataRecordTypeManager = new Vanrise.GenericData.Business.DataRecordTypeManager();
+            Type smsCDRRuntimeType = dataRecordTypeManager.GetDataRecordRuntimeType("SmsCDR");
+
+
+            var currentItemCount = 26;
+            var headerText = "H";
+
+            DateTime creationDate = default(DateTime);
+            System.IO.StreamReader sr = ImportedData.StreamReader;
+            while (!sr.EndOfStream)
+            {
+                string currentLine = sr.ReadLine();
+                if (string.IsNullOrEmpty(currentLine))
+                    continue;
+
+                string[] rowData = currentLine.Split(';');
+
+                if (rowData.Length == 2 && rowData[0] == headerText)
+                {
+                    creationDate = DateTime.ParseExact(rowData[1], "yyyyMMddHHmmss", System.Globalization.CultureInfo.InvariantCulture);
+                    continue;
+                }
+
+                else if (rowData.Length != currentItemCount)
+                    continue;
+
+                dynamic cdr = Activator.CreateInstance(smsCDRRuntimeType) as dynamic;
+                cdr.IdCDR = long.Parse(rowData[0]);
+                cdr.StartDate = DateTime.ParseExact(rowData[2], "dd/MM/yyyy HH:mm:ss", System.Globalization.CultureInfo.InvariantCulture);
+
+                string parentIdCDR = rowData[22];
+                cdr.ParentIdCDR = !string.IsNullOrEmpty(parentIdCDR) ? long.Parse(parentIdCDR) : default(long?);
+
+                cdr.TrafficType = rowData[3];
+                cdr.TypeSms = rowData[4];
+                cdr.DirectionTraffic = rowData[5];
+                cdr.Calling = rowData[6];
+                cdr.Called = rowData[7];
+                cdr.TypeNet = rowData[8];
+                cdr.SourceOperator = rowData[9];
+                cdr.DestinationOperator = rowData[10];
+                cdr.SourceArea = rowData[11];
+                cdr.DestinationArea = rowData[12];
+
+                string bill = rowData[13];
+                cdr.Bill = !string.IsNullOrEmpty(bill) ? int.Parse(bill) : default(int?);
+
+                string credit = rowData[15];
+                cdr.Credit = !string.IsNullOrEmpty(credit) ? decimal.Parse(credit) : default(decimal?);
+
+                cdr.Unit = rowData[14];
+
+                string balance = rowData[16];
+                cdr.Balance = !string.IsNullOrEmpty(balance) ? decimal.Parse(balance) : default(decimal?);
+
+                cdr.Bag = rowData[17];
+
+                string amount = rowData[18];
+                cdr.Amount = !string.IsNullOrEmpty(amount) ? decimal.Parse(amount) : default(decimal?);
+
+                cdr.TypeConsumed = rowData[19];
+                cdr.PricePlan = rowData[20];
+                cdr.Promotion = rowData[21];
+
+                cdr.FileName = rowData[24];
+                cdr.FileDate = DateTime.ParseExact(rowData[25], "dd/MM/yyyy HH:mm:ss", System.Globalization.CultureInfo.InvariantCulture);
+                cdr.CreationDate = creationDate;
+
+                smsCDRs.Add(cdr);
+            }
+
+            var batch = Vanrise.GenericData.QueueActivators.DataRecordBatch.CreateBatchFromRecords(smsCDRs, "#RECORDSCOUNT# of Raw CDRs");
+            mappedBatches.Add("SMS CDR Storage Stage", batch);
+
+            Vanrise.Integration.Entities.MappingOutput result = new Vanrise.Integration.Entities.MappingOutput();
+            result.Result = Vanrise.Integration.Entities.MappingResult.Valid;
+            LogVerbose("Finished");
+            return result;
+        }
+
+        public Vanrise.Integration.Entities.MappingOutput ImportingGprsCDR_File()
+        {
+            LogVerbose("Started");
+            Vanrise.Integration.Entities.StreamReaderImportedData ImportedData = ((Vanrise.Integration.Entities.StreamReaderImportedData)(data));
+
+            var dataCDRs = new List<dynamic>();
+
+            var dataRecordTypeManager = new Vanrise.GenericData.Business.DataRecordTypeManager();
+            Type gprsCDRRuntimeType = dataRecordTypeManager.GetDataRecordRuntimeType("GprsCDR");
+
+            var currentItemCount = 21;
+            var headerText = "H";
+
+            DateTime creationDate = default(DateTime);
+            System.IO.StreamReader sr = ImportedData.StreamReader;
+            while (!sr.EndOfStream)
+            {
+                string currentLine = sr.ReadLine();
+                if (string.IsNullOrEmpty(currentLine))
+                    continue;
+
+                string[] rowData = currentLine.Split(';');
+
+                if (rowData.Length == 2 && rowData[0] == headerText)
+                {
+                    creationDate = DateTime.ParseExact(rowData[1], "yyyyMMddHHmmss", System.Globalization.CultureInfo.InvariantCulture);
+                    continue;
+                }
+
+                else if (rowData.Length != currentItemCount)
+                    continue;
+
+                dynamic cdr = Activator.CreateInstance(gprsCDRRuntimeType) as dynamic;
+                cdr.StartDate = DateTime.ParseExact(rowData[2], "dd/MM/yyyy HH:mm:ss", System.Globalization.CultureInfo.InvariantCulture);
+                cdr.TypeGprs = rowData[4];
+                cdr.Calling = rowData[5];
+                cdr.Zone = rowData[6];
+
+                string bill = rowData[7];
+                cdr.Bill = !string.IsNullOrEmpty(bill) ? int.Parse(bill) : default(int?);
+
+                string credit = rowData[9];
+                cdr.Credit = !string.IsNullOrEmpty(credit) ? decimal.Parse(credit) : default(decimal?);
+
+                cdr.TrafficType = rowData[3];
+                cdr.Unit = rowData[8];
+
+                string balance = rowData[10];
+                cdr.Balance = !string.IsNullOrEmpty(balance) ? decimal.Parse(balance) : default(decimal?);
+
+                cdr.Bag = rowData[11];
+
+                string amount = rowData[12];
+                cdr.Amount = !string.IsNullOrEmpty(amount) ? decimal.Parse(amount) : default(decimal?);
+
+                cdr.TypeConsumed = rowData[13];
+                cdr.PricePlan = rowData[14];
+                cdr.Promotion = rowData[15];
+                cdr.AccessPointName = rowData[16];
+                //cdr.ParentIdCDR
+                cdr.IdCDR = long.Parse(rowData[0]);
+
+                string idCdrGprs = rowData[17];
+                cdr.IdCdrGprs = !string.IsNullOrEmpty(idCdrGprs) ? long.Parse(idCdrGprs) : default(long?);
+
+
+                cdr.FileName = rowData[19];
+                cdr.FileDate = DateTime.ParseExact(rowData[20], "dd/MM/yyyy HH:mm:ss", System.Globalization.CultureInfo.InvariantCulture);
+                cdr.CreationDate = creationDate;
+
+
+                dataCDRs.Add(cdr);
+            }
+
+            var batch = Vanrise.GenericData.QueueActivators.DataRecordBatch.CreateBatchFromRecords(dataCDRs, "#RECORDSCOUNT# of Raw CDRs");
+            mappedBatches.Add("Gprs CDR Storage Stage", batch);
 
             Vanrise.Integration.Entities.MappingOutput result = new Vanrise.Integration.Entities.MappingOutput();
             result.Result = Vanrise.Integration.Entities.MappingResult.Valid;
