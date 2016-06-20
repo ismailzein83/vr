c7e8602bbe5b2c4d084510db7e09c114f32e55b0
Samer Haidar <VANRISE\samer.haidar>
2016-06-20 11:25:09 +0000

    Implementing accounttypeeditor
    git-tfs-id: [http://developmentvlan:8585/tfs/vanrise.collection]$/;C33763

--- Retail/Code/Retail.BusinessEntity.Web/Retail_BusinessEntity/Views/AccountType/AccountTypeEditorController.js
+++ Retail/Code/Retail.BusinessEntity.Web/Retail_BusinessEntity/Views/AccountType/AccountTypeEditorController.js
@@ -1,1 +1,190 @@
-﻿
+﻿(function (appControllers) {
+
+    'use strict';
+
+    AccountTypeEditorController.$inject = ['$scope', 'Retail_BE_AccountTypeAPIService', 'UtilsService', 'VRUIUtilsService', 'VRNavigationService', 'VRNotificationService'];
+
+    function AccountTypeEditorController($scope, Retail_BE_AccountTypeAPIService, UtilsService, VRUIUtilsService, VRNavigationService, VRNotificationService) {
+        var isEditMode;
+
+        var accountTypeId;
+        var accountTypeEntity;
+
+        var accountTypeSelectorAPI;
+        var accountTypeSelectorReadyDeferred = UtilsService.createPromiseDeferred();
+        var accountTypeSelectedDeferred;
+
+        var accountTypePartDefinitionAPI;
+        var accountTypePartDefinitionReadyDeferred = UtilsService.createPromiseDeferred();
+
+        loadParameters();
+        defineScope();
+        load();
+
+        function loadParameters() {
+            var parameters = VRNavigationService.getParameters($scope);
+            if (parameters != undefined) {
+                accountTypeId = parameters.accountTypeId;
+            }
+            isEditMode = (accountTypeId != undefined);
+        }
+
+        function defineScope() {
+            $scope.scopeModel = {};
+
+            $scope.scopeModel.onAccountTypeSelectorReady = function (api) {
+                accountTypeSelectorAPI = api;
+                accountTypeSelectorReadyDeferred.resolve();
+            };
+
+            $scope.scopeModel.onAccountTypePartDefinitionReady = function (api) {
+                accountTypePartDefinitionAPI = api;
+                accountTypePartDefinitionReadyDeferred.resolve();
+            };
+
+            $scope.scopeModel.save = function () {
+                return (isEditMode) ? updateAccountType() : insertAccountType();
+            };
+
+            $scope.scopeModel.hasSaveAccountTypePermission = function () {
+                return (isEditMode) ? Retail_BE_AccountTypeAPIService.HasUpdateAccountTypePermission() : Retail_BE_AccountTypeAPIService.HasAddAccountTypePermission();
+            };
+
+            $scope.scopeModel.close = function () {
+                $scope.modalContext.closeModal()
+            };
+        }
+
+        function load() {
+            $scope.scopeModel.isLoading = true;
+
+            if (isEditMode) {
+                getAccountType().then(function () {
+                    loadAllControls().finally(function () {
+                        accountTypeEntity = undefined;
+                    });
+                }).catch(function (error) {
+                    VRNotificationService.notifyExceptionWithClose(error, $scope);
+                    $scope.scopeModel.isLoading = false;
+                });
+            }
+            else {
+                loadAllControls();
+            }
+        }
+
+        function getAccountType() {
+            return Retail_BE_AccountTypeAPIService.GetAccountType(accountTypeId).then(function (response) {
+                accountTypeEntity = response;
+            });
+        }
+
+        function loadAllControls() {
+            return UtilsService.waitMultipleAsyncOperations([setTitle, loadAccountTypeSection, loadStaticData, loadAccountTypePartDefinitioSection]).catch(function (error) {
+                VRNotificationService.notifyExceptionWithClose(error, $scope);
+            }).finally(function () {
+                $scope.scopeModel.isLoading = false;
+            });
+        }
+
+        function setTitle() {
+            if (isEditMode) {
+                var accountTypeName = (accountTypeEntity != undefined) ? accountTypeEntity.Name : undefined;
+                $scope.title = UtilsService.buildTitleForUpdateEditor(accountTypeName, 'Account Type');
+            }
+            else {
+                $scope.title = UtilsService.buildTitleForAddEditor('Account Type');
+            }
+        }
+
+        function loadStaticData() {
+            if (accountTypeEntity == undefined)
+                return;
+            $scope.scopeModel.name = accountTypeEntity.Name;
+            $scope.scopeModel.title = accountTypeEntity.Title;
+            $scope.scopeModel.canBeRootAccount = accountTypeEntity.CanBeRootAccount;
+        }
+
+        function loadAccountTypeSection() {
+            var accountTypeSelectorLoadDeferred = UtilsService.createPromiseDeferred();
+            accountTypeSelectorReadyDeferred.promise.then(function () {
+                var accountTypeSelectorPayload;
+                if (accountTypeEntity != undefined && accountTypeEntity.Settings != null) {
+                    accountTypeSelectorPayload = {
+                        selectedIds: accountTypeEntity.Settings.SupportedParentAccountTypeIds
+                    };
+                }
+                VRUIUtilsService.callDirectiveLoad(accountTypeSelectorAPI, accountTypeSelectorPayload, accountTypeSelectorLoadDeferred);
+            });
+
+            return accountTypeSelectorLoadDeferred.promise;
+        }
+
+        function loadAccountTypePartDefinitioSection() {
+            var accountTypePartDefinitionLoadDeferred = UtilsService.createPromiseDeferred();
+            accountTypePartDefinitionReadyDeferred.promise.then(function () {
+                var accountTypePartPayload;
+                if (accountTypeEntity != undefined && accountTypeEntity.Settings != null) {
+                    accountTypePartPayload = {
+                        partDefinitions: accountTypeEntity.Settings.PartDefinitions
+                    };
+                }
+                VRUIUtilsService.callDirectiveLoad(accountTypePartDefinitionAPI, accountTypePartPayload, accountTypePartDefinitionLoadDeferred);
+            });
+
+            return accountTypePartDefinitionLoadDeferred.promise;
+        }
+
+        function insertAccountType() {
+            $scope.scopeModel.isLoading = true;
+
+            var accountTypeObj = buildAccountTypeObjFromScope();
+
+            return Retail_BE_AccountTypeAPIService.AddAccountType(accountTypeObj).then(function (response) {
+                if (VRNotificationService.notifyOnItemAdded('Account Type', response, 'Name')) {
+                    if ($scope.onAccountTypeAdded != undefined)
+                        $scope.onAccountTypeAdded(response.InsertedObject);
+                    $scope.modalContext.closeModal();
+                }
+            }).catch(function (error) {
+                VRNotificationService.notifyException(error, $scope);
+            }).finally(function () {
+                $scope.scopeModel.isLoading = false;
+            });
+        }
+
+        function updateAccountType() {
+            $scope.scopeModel.isLoading = true;
+
+            var accountTypeObj = buildAccountTypeObjFromScope();
+
+            return Retail_BE_AccountTypeAPIService.UpdateAccountType(accountTypeObj).then(function (response) {
+                if (VRNotificationService.notifyOnItemUpdated('Account Type', response, 'Name')) {
+                    if ($scope.onAccountTypeUpdated != undefined) {
+                        $scope.onAccountTypeUpdated(response.UpdatedObject);
+                    }
+                    $scope.modalContext.closeModal();
+                }
+            }).catch(function (error) {
+                VRNotificationService.notifyException(error, $scope);
+            }).finally(function () {
+                $scope.scopeModel.isLoading = false;
+            });
+        }
+
+        function buildAccountTypeObjFromScope() {
+            var obj = {
+                AccountTypeId: accountTypeId,
+                Name: $scope.scopeModel.name,
+                Title: $scope.scopeModel.title,
+                CanBeRootAccount:$scope.scopeModel.canBeRootAccount,
+                SupportedParentAccountTypeIds:accountTypeSelectorAPI.getSelectedIds(),
+                PartDefinitions: accountTypePartDefinitionAPI.getData()
+            };
+            return obj;
+        }
+    }
+
+    appControllers.controller('Retail_BE_AccountTypeEditorController', AccountTypeEditorController);
+
+})(appControllers);
