405f12d768d2df273cefda502cb8d7625517863d
Ismail Zein <VANRISE\ismail.zein>
2016-06-20 13:47:55 +0000

    added DataRecordCondition
    git-tfs-id: [http://developmentvlan:8585/tfs/vanrise.collection]$/;C33775

---
+++ Vanrise/Code/Vanrise.GenericData.Entities/DataRecordCondition.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Vanrise.GenericData.Entities
+{
+    public abstract class DataRecordCondition
+    {
+        public abstract bool Evaluate(IDataRecordConditionContext context);
+    }
+
+    public interface IDataRecordConditionContext
+    {
+        dynamic DataRecord { get; }
+    }
+}
