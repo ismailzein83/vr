0323395ba8335196404ab307a03a734ac74c3223
mohamad.elhariri <mohamad.elhariri@vanrise.tfs.local>
2016-06-20 13:31:43 +0000

    Added and implemented retail-be-accounttype-activation-part-definition
    git-tfs-id: [http://developmentvlan:8585/tfs/vanrise.collection]$/;C33773

---
+++ Retail/Code/Retail.BusinessEntity.Web/Retail_BusinessEntity/Directives/MainExtensions/AccountType/Part/Definition/retail-be-accounttype-activation-part-definition.js
@@ -0,0 +1,47 @@
+﻿'use strict';
+
+app.directive('retailBeAccounttypeActivationPartDefinition', [function () {
+    return {
+        restrict: 'E',
+        scope: {
+            onReady: '=',
+            normalColNum: '@'
+        },
+        controller: function ($scope, $element, $attrs) {
+            var ctrl = this;
+            var accountTypeActivationPartDefinition = new AccountTypeActivationPartDefinition($scope, ctrl, $attrs);
+            accountTypeActivationPartDefinition.initializeController();
+        },
+        controllerAs: 'ctrl',
+        bindToController: true,
+        templateUrl: '/Client/Modules/Retail_BusinessEntity/Directives/MainExtensions/AccountType/Part/Definition/Templates/AccountTypeActivationPartDefinitionTemplate.html'
+    };
+
+    function AccountTypeActivationPartDefinition($scope, ctrl, $attrs)
+    {
+        this.initializeController = initializeController;
+
+        function initializeController()
+        {
+            $scope.scopeModel = {};
+            defineAPI();
+        }
+        function defineAPI()
+        {
+            var api = {};
+
+            api.load = function (payload) {
+                
+            };
+
+            api.getData = function () {
+                return {
+                    $type: 'Retail.BusinessEntity.MainExtensions.AccountParts.AccountPartActivationDefinition,Retail.BusinessEntity.MainExtensions'
+                };
+            };
+
+            if (ctrl.onReady != null)
+                ctrl.onReady(api);
+        }
+    }
+}]);
