6c3450d1979955c7da24825af1a2c98ee5b467a3
Samer Haidar <VANRISE\samer.haidar>
2016-06-20 10:37:08 +0000

    change name space
    git-tfs-id: [http://developmentvlan:8585/tfs/vanrise.collection]$/;C33760

--- Retail/Code/Retail.BusinessEntity.Entities/AccountType/AccountTypeDetail.cs
+++ Retail/Code/Retail.BusinessEntity.Entities/AccountType/AccountTypeDetail.cs
@@ -4,7 +4,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace Retail.BusinessEntity.Entities.AccountType
+namespace Retail.BusinessEntity.Entities
 {
     class AccountTypeDetail
     {
