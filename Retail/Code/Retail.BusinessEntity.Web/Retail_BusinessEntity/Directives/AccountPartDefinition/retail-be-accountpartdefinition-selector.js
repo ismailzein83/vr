540032aabe3b07dbcc3eaec0b585d8d513dd307f
mohamad.elhariri <mohamad.elhariri@vanrise.tfs.local>
2016-06-21 08:12:02 +0000

    Added and implemented retail-be-accountpartdefinition-selector
    git-tfs-id: [http://developmentvlan:8585/tfs/vanrise.collection]$/;C33783

--- Retail/Code/Retail.BusinessEntity.Web/Retail_BusinessEntity/Directives/AccountPartDefinition/retail-be-accountpartdefinition-selector.js
+++ Retail/Code/Retail.BusinessEntity.Web/Retail_BusinessEntity/Directives/AccountPartDefinition/retail-be-accountpartdefinition-selector.js
@@ -1,1 +1,100 @@
-﻿
+﻿'use strict';
+
+app.directive('retailBeServicetypeSelector', ['Retail_BE_AccountPartDefinitionAPIService', 'UtilsService', 'VRUIUtilsService', function (Retail_BE_AccountPartDefinitionAPIService, UtilsService, VRUIUtilsService) {
+    return {
+        restrict: 'E',
+        scope: {
+            onReady: '=',
+            ismultipleselection: '@',
+            selectedvalues: '=',
+            onselectionchanged: '=',
+            onselectitem: '=',
+            ondeselectitem: '=',
+            isrequired: '=',
+            hideremoveicon: '@',
+            normalColNum: '@',
+            customvalidate: '='
+        },
+        controller: function ($scope, $element, $attrs) {
+
+            var ctrl = this;
+            ctrl.datasource = [];
+
+            ctrl.selectedvalues;
+            if ($attrs.ismultipleselection != undefined)
+                ctrl.selectedvalues = [];
+
+            var accountPartDefinitionSelector = new AccountPartDefinitionSelector(ctrl, $scope, $attrs);
+            accountPartDefinitionSelector.initializeController();
+
+        },
+        controllerAs: 'ctrl',
+        bindToController: true,
+        template: function (element, attrs) {
+            return getTemplate(attrs);
+        }
+    };
+
+    function AccountPartDefinitionSelector(ctrl, $scope, attrs) {
+
+        this.initializeController = initializeController;
+
+        var selectorAPI;
+
+        function initializeController() {
+            ctrl.onSelectorReady = function (api) {
+                selectorAPI = api;
+                defineAPI();
+            };
+        }
+
+        function defineAPI() {
+            var api = {};
+
+            api.load = function (payload) {
+                selectorAPI.clearDataSource();
+
+                var selectedIds;
+                var filter;
+
+                if (payload != undefined) {
+                    selectedIds = payload.selectedIds;
+                    filter = payload.filter;
+                }
+
+                return Retail_BE_AccountPartDefinitionAPIService.GetAccountPartDefinitionsInfo(UtilsService.serializetoJson(filter)).then(function (response) {
+                    if (response != null) {
+                        for (var i = 0; i < response.length; i++) {
+                            ctrl.datasource.push(response[i]);
+                        }
+
+                        if (selectedIds != undefined) {
+                            VRUIUtilsService.setSelectedValues(selectedIds, 'AccountPartDefinitionId', attrs, ctrl);
+                        }
+                    }
+                });
+            };
+
+            api.getSelectedIds = function () {
+                return VRUIUtilsService.getIdSelectedIds('AccountPartDefinitionId', attrs, ctrl);
+            };
+
+            if (ctrl.onReady != null)
+                ctrl.onReady(api);
+        }
+    }
+
+    function getTemplate(attrs) {
+
+        var multipleselection = "";
+        var label = "Account Part Definition";
+
+        if (attrs.ismultipleselection != undefined) {
+            label = "Account Part Definition";
+            multipleselection = "ismultipleselection";
+        }
+
+        return '<vr-columns colnum="{{ctrl.normalColNum}}"><vr-select ' + multipleselection + ' datatextfield="Title" datavaluefield="AccountPartDefinitionId" isrequired="ctrl.isrequired" label="' + label + '" datasource="ctrl.datasource" on-ready="ctrl.onSelectorReady" selectedvalues="ctrl.selectedvalues" onselectionchanged="ctrl.onselectionchanged" entityName="' + label + '" onselectitem="ctrl.onselectitem" ondeselectitem="ctrl.ondeselectitem" hideremoveicon="ctrl.hideremoveicon" customvalidate="ctrl.customvalidate"></vr-select></vr-columns>';
+    }
+
+}]);
--- Retail/Code/Retail.BusinessEntity.Web/Retail_BusinessEntity/Services/APIServices/AccountPartDefinitionAPIService.js
+++ Retail/Code/Retail.BusinessEntity.Web/Retail_BusinessEntity/Services/APIServices/AccountPartDefinitionAPIService.js
@@ -1,1 +1,66 @@
-﻿
+﻿(function (appControllers) {
+
+    'use strict';
+
+    AccountPartDefinitionAPIService.$inject = ['BaseAPIService', 'UtilsService', 'Retail_BE_ModuleConfig', 'SecurityService'];
+
+    function AccountPartDefinitionAPIService(BaseAPIService, UtilsService, Retail_BE_ModuleConfig, SecurityService)
+    {
+        var controllerName = 'AccountPartDefinition';
+
+        function GetFilteredAccountPartDefinitions(input) {
+            return BaseAPIService.post(UtilsService.getServiceURL(Retail_BE_ModuleConfig.moduleName, controllerName, 'GetFilteredAccountPartDefinitions'), input);
+        }
+
+        function GetAccountPartDefinition(accountPartDefinitionId) {
+            return BaseAPIService.get(UtilsService.getServiceURL(Retail_BE_ModuleConfig.moduleName, controllerName, 'GetAccountPartDefinition'), {
+                accountPartDefinitionId: accountPartDefinitionId
+            });
+        }
+
+        function GetAccountPartDefinitionsInfo(filter) {
+            return BaseAPIService.get(UtilsService.getServiceURL(Retail_BE_ModuleConfig.moduleName, controllerName, "GetAccountPartDefinitionsInfo"), {
+                filter: filter
+            });
+        }
+
+        function GetAccountPartDefinitionPartDefinitionExtensionConfigs() {
+            return BaseAPIService.get(UtilsService.getServiceURL(Retail_BE_ModuleConfig.moduleName, controllerName, "GetAccountPartDefinitionPartDefinitionExtensionConfigs"));
+        }
+
+        function AddAccountPartDefinition(accountPartDefinition) {
+            return BaseAPIService.post(UtilsService.getServiceURL(Retail_BE_ModuleConfig.moduleName, controllerName, 'AddAccountPartDefinition'), accountPartDefinition);
+        }
+
+        function UpdateAccountPartDefinition(accountPartDefinition) {
+            return BaseAPIService.post(UtilsService.getServiceURL(Retail_BE_ModuleConfig.moduleName, controllerName, 'UpdateAccountPartDefinition'), accountPartDefinition);
+        }
+
+        function HasViewAccountPartDefinitionsPermission() {
+            return SecurityService.HasPermissionToActions(UtilsService.getSystemActionNames(Retail_BE_ModuleConfig.moduleName, controllerName, ['GetFilteredAccountPartDefinitions']));
+        }
+
+        function HasAddAccountPartDefinitionPermission() {
+            return SecurityService.HasPermissionToActions(UtilsService.getSystemActionNames(Retail_BE_ModuleConfig.moduleName, controllerName, ['AddAccountPartDefinition']));
+        }
+
+        function HasUpdateAccountPartDefinitionPermission() {
+            return SecurityService.HasPermissionToActions(UtilsService.getSystemActionNames(Retail_BE_ModuleConfig.moduleName, controllerName, ['UpdateAccountPartDefinition']));
+        }
+
+        return {
+            GetFilteredAccountPartDefinitions: GetFilteredAccountPartDefinitions,
+            GetAccountPartDefinition: GetAccountPartDefinition,
+            GetAccountPartDefinitionsInfo: GetAccountPartDefinitionsInfo,
+            GetAccountPartDefinitionPartDefinitionExtensionConfigs: GetAccountPartDefinitionPartDefinitionExtensionConfigs,
+            AddAccountPartDefinition: AddAccountPartDefinition,
+            UpdateAccountPartDefinition: UpdateAccountPartDefinition,
+            HasViewAccountPartDefinitionsPermission: HasViewAccountPartDefinitionsPermission,
+            HasAddAccountPartDefinitionPermission: HasAddAccountPartDefinitionPermission,
+            HasUpdateAccountPartDefinitionPermission: HasUpdateAccountPartDefinitionPermission
+        };
+    }
+
+    appControllers.service('Retail_BE_AccountPartDefinitionAPIService', AccountPartDefinitionAPIService);
+
+})(appControllers);
