add4abe71646bd0742d6e636008e470adfdedc1c
Ismail Zein <VANRISE\ismail.zein>
2016-06-21 08:35:29 +0000

    added DataRecordVanriseType
    git-tfs-id: [http://developmentvlan:8585/tfs/vanrise.collection]$/;C33787

---
+++ Vanrise/Code/Vanrise.GenericData.Entities/DataRecordVanriseType.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Vanrise.GenericData.Entities
+{
+    public class DataRecordVanriseType : Vanrise.Entities.IVanriseType
+    {
+        string _uniqueName;
+        public DataRecordVanriseType(string dataRecordTypeName)
+        {
+            if (String.IsNullOrEmpty(dataRecordTypeName))
+                throw new ArgumentNullException("dataRecordTypeName");
+
+            _uniqueName = string.Format("GenericData_DataRecordType_{0}", dataRecordTypeName);
+        }
+
+        public string UniqueTypeName
+        {
+            get { return _uniqueName; }
+        }
+    }
+}
