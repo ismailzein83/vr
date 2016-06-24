c5be80c5b86a77f02a0d543e5d4d68931d84a5cd
Wissam Ajamy <VANRISE\wissam.ajamy>
2016-06-24 07:07:47 +0000

    update structure from TOneConfiguration
    git-tfs-id: [http://developmentvlan:8585/tfs/vanrise.collection]$/;C33868

---
+++ TOneV2/Code/TOneV2/SQL.TOneConfiguration/common/Stored Procedures/sp_File_Insert_Temp.sql
@@ -0,0 +1,15 @@
+﻿
+Create PROCEDURE [common].[sp_File_Insert_Temp] 
+	@Name Nvarchar(255),
+	@Extension Nvarchar(50),
+	@Content varbinary(max),
+	@ModuleName VARCHAR(255),
+	@UserID INT,
+	@CreatedTime datetime,
+	@Id bigint out
+AS
+BEGIN
+	Insert into common.[File_Temp] ([Name], [Extension], [Content], [ModuleName], [UserID], [CreatedTime])
+	values(@Name, @Extension, @Content, @ModuleName, @UserID, @CreatedTime)
+	SET @Id = @@IDENTITY
+END
