e6cd9c5d748596eb14f70ef36b9b60d6cc63b68a
Zeinab Mokdad <VANRISE\zeinab.mokdad>
2016-06-28 08:49:25 +0000

    adding routing analysis
    git-tfs-id: [http://developmentvlan:8585/tfs/vanrise.collection]$/;C33931

---
+++ TOneV2/Code/TOneV2/TOne.WhS.Analytics.Entities/BillingReport/RoutingAnalysis/RoutingAnalysisFormatted.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TOne.WhS.Analytics.Entities.BillingReport.RoutingAnalysis
+{
+    public class RoutingAnalysisFormatted
+    {
+        public int SaleZoneID { get; set; }
+        public string SaleZone { get; set; }
+        public string SupplierID { get; set; }
+        public string Supplier { get; set; }
+        public decimal Duration { get; set; }
+        public string DurationFormatted { get; set; }
+        public double CostNet { get; set; }
+        public string CostNetFormatted { get; set; }
+        public double SaleNet { get; set; }
+        public string SaleNetFormatted { get; set; }
+        public decimal ASR { get; set; }
+        public string ASRFormatted { get; set; }
+        public decimal ACD { get; set; }
+        public string ACDFormatted { get; set; }
+        public double Profit { get; set; }
+        public string ProfitFormatted { get; set; }
+        public double AVGCost { get; set; }
+        public string AVGCostFormatted { get; set; }
+        public double AVGSale { get; set; }
+        public string AVGSaleFormatted { get; set; }
+    }
+}
