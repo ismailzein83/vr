e9f6288f161190fc1a7814f87de1e252015a7c8e
Zeinab Mokdad <VANRISE\zeinab.mokdad>
2016-06-28 09:16:38 +0000

    addin g routing  by customer entities
    git-tfs-id: [http://developmentvlan:8585/tfs/vanrise.collection]$/;C33936

---
+++ TOneV2/Code/TOneV2/TOne.WhS.Analytics.Entities/BillingReport/RoutingByCustomer/RoutingByCustomer.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TOne.WhS.Analytics.Entities.BillingReport.RoutingByCustomer
+{
+    public class RoutingByCustomer
+    {
+        public DateTime CallDate { get; set; }
+
+        public int SaleZone { get; set; }
+
+        public int CostZone { get; set; }
+
+        public string CustomerID { get; set; }
+
+        public string SupplierID { get; set; }
+
+        public double SaleRate { get; set; }
+
+        public double CostRate { get; set; }
+
+        public decimal SaleDuration { get; set; }
+
+        public decimal CostDuration { get; set; }
+
+        public double SaleNet { get; set; }
+
+        public double CostNet { get; set; }
+
+    }
+}
---
+++ TOneV2/Code/TOneV2/TOne.WhS.Analytics.Entities/BillingReport/RoutingByCustomer/RoutingByCustomerFormatted.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TOne.WhS.Analytics.Entities.BillingReport.RoutingByCustomer
+{
+    public class RoutingByCustomerFormatted
+    {
+        public string Customer { get; set; }
+
+        public string Destination { get; set; }
+
+        public string SaleDuration { get; set; }
+
+        public string SaleRate { get; set; }
+
+        public string Supplier { get; set; }
+
+        public string CostDuration { get; set; }
+
+        public string CostRate { get; set; }
+
+        public string Profit { get; set; }
+        public string ProfitPerc { get; set; }
+    }
+}
