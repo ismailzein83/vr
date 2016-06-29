eadcf5b64eb755e449472e240fc96f8c0bfa2233
Zeinab Mokdad <VANRISE\zeinab.mokdad>
2016-06-29 08:27:58 +0000

    fixing entity name for the report
    git-tfs-id: [http://developmentvlan:8585/tfs/vanrise.collection]$/;C33960

--- TOneV2/Code/TOneV2/TOne.WhS.Analytics.Entities/BillingReport/ProfitByCarrier/ProfitByCarrier.cs
+++ TOneV2/Code/TOneV2/TOne.WhS.Analytics.Entities/BillingReport/ProfitByCarrier/ProfitByCarrier.cs
@@ -4,7 +4,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace TOne.WhS.Analytics.Entities.BillingReport.ProfitByCarrier
+namespace TOne.WhS.Analytics.Entities.BillingReport
 {
     public class ProfitByCarrier
     {
