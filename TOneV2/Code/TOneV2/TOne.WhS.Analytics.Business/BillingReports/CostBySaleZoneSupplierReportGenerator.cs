933bc3de151ec200252fb95ce337a35de88ddb15
Zeinab Mokdad <VANRISE\zeinab.mokdad>
2016-07-01 10:48:22 +0000

    renaming title
    git-tfs-id: [http://developmentvlan:8585/tfs/vanrise.collection]$/;C34022

--- TOneV2/Code/TOneV2/TOne.WhS.Analytics.Business/BillingReports/CostBySaleZoneSupplierReportGenerator.cs
+++ TOneV2/Code/TOneV2/TOne.WhS.Analytics.Business/BillingReports/CostBySaleZoneSupplierReportGenerator.cs
@@ -95,7 +95,7 @@ namespace TOne.WhS.Analytics.Business.BillingReports
             Dictionary<string, RdlcParameter> list = new Dictionary<string, RdlcParameter>();
             list.Add("FromDate", new RdlcParameter { Value = parameters.FromTime.ToString(), IsVisible = true });
             list.Add("ToDate", new RdlcParameter { Value = parameters.ToTime.ToString(), IsVisible = true });
-            list.Add("Title", new RdlcParameter { Value = "Sale Cost Summary", IsVisible = true });
+            list.Add("Title", new RdlcParameter { Value = "Cost by Sale Zone - Grouped by Supplier", IsVisible = true });
             list.Add("LogoPath", new RdlcParameter { Value = "logo", IsVisible = true });
             list.Add("DigitRate", new RdlcParameter { Value = "2", IsVisible = true });
             list.Add("Currency", new RdlcParameter { Value = parameters.CurrencyDescription, IsVisible = true });
