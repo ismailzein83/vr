9bc9d646448860c9b3d72437ff61984faf3e3847
Ismail Zein <VANRISE\ismail.zein>
2016-07-01 13:26:31 +0000

    AccountBalance
    git-tfs-id: [http://developmentvlan:8585/tfs/vanrise.collection]$/;C34036

--- Vanrise/Code/Vanrise.AccountBalance.Entities/UsageBalanceUpdate.cs
+++ Vanrise/Code/Vanrise.AccountBalance.Entities/UsageBalanceUpdate.cs
@@ -6,7 +6,7 @@ using System.Threading.Tasks;
 
 namespace Vanrise.AccountBalance.Entities
 {
-    public class BalanceUpdate
+    public class UsageBalanceUpdate
     {
         public long AccountId { get; set; }
 
