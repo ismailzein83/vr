ff883a1b91e7d2e18a4a950c49d824cff499bfc3
Wissam Ajamy <VANRISE\wissam.ajamy>
2016-07-01 08:15:46 +0000

    Update structure from TOneConfiguration
    git-tfs-id: [http://developmentvlan:8585/tfs/vanrise.collection]$/;C34017

---
+++ TOneV2/Code/TOneV2/SQL.TOneConfiguration/common/Stored Procedures/sp_IDManager_Update.sql
@@ -0,0 +1,17 @@
+﻿
+-- =============================================
+-- Author:		<Author,,Name>
+-- Create date: <Create Date,,>
+-- Description:	<Description,,>
+-- =============================================
+Create PROCEDURE [common].[sp_IDManager_Update]
+	@TypeID int,
+	@LastTakenID bigint
+AS
+BEGIN
+
+	Update common.IDManager
+	Set LastTakenID=@LastTakenID
+	where TypeID=@TypeID
+	
+END
--- TOneV2/Code/TOneV2/SQL.TOneConfiguration/common/Tables/Currency.sql
+++ TOneV2/Code/TOneV2/SQL.TOneConfiguration/common/Tables/Currency.sql
@@ -4,8 +4,11 @@
     [Name]      NVARCHAR (255) NOT NULL,
     [timestamp] ROWVERSION     NULL,
     [SourceID]  VARCHAR (50)   NULL,
-    CONSTRAINT [PK_Currency] PRIMARY KEY CLUSTERED ([ID] ASC)
+    CONSTRAINT [PK_Currency] PRIMARY KEY CLUSTERED ([ID] ASC),
+    CONSTRAINT [IX_Currency_Symbol] UNIQUE NONCLUSTERED ([Symbol] ASC)
 );
+
+
 
 
 
