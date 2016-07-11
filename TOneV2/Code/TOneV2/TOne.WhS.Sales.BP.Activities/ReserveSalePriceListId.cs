21e8ffd60734fe1de03f996403663b4c8b32b022
mohamad.elhariri <mohamad.elhariri@vanrise.tfs.local>
2016-07-11 06:42:19 +0000

    [Ismail]: added missing files
    git-tfs-id: [http://developmentvlan:8585/tfs/vanrise.collection]$/;C34088

---
+++ TOneV2/Code/TOneV2/TOne.WhS.Sales.BP.Activities/ReserveSalePriceListId.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Activities;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TOne.WhS.BusinessEntity.Business;
+
+namespace TOne.WhS.Sales.BP.Activities
+{
+    public class ReserveSalePriceListId : CodeActivity
+    {
+        #region Input Arguments
+
+        [RequiredArgument]
+        public InArgument<int> NewRatesCount { get; set; }
+
+        [RequiredArgument]
+        public InArgument<int> ChangedRatesCount { get; set; }
+
+        #endregion
+
+        #region Output Arguments
+
+        [RequiredArgument]
+        public OutArgument<int?> SalePriceListId { get; set; }
+        
+        #endregion
+
+        protected override void Execute(CodeActivityContext context)
+        {
+            int newRatesCount = NewRatesCount.Get(context);
+            int changedRatesCount = ChangedRatesCount.Get(context);
+            
+            int? salePriceListId = null;
+
+            if (newRatesCount > 0 || changedRatesCount > 0)
+            {
+                var salePriceListManager = new SalePriceListManager();
+                long startingId = salePriceListManager.ReserveIdRange(1);
+                salePriceListId = Convert.ToInt32(startingId);
+            }
+
+            SalePriceListId.Set(context, salePriceListId);
+        }
+    }
+}
---
+++ TOneV2/Code/TOneV2/TOne.WhS.Sales.Data.SQL/ChangedSaleZoneRoutingProductDataManager.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TOne.WhS.Sales.Entities;
+using Vanrise.Data;
+using Vanrise.Data.SQL;
+
+namespace TOne.WhS.Sales.Data.SQL
+{
+    public class ChangedSaleZoneRoutingProductDataManager : BaseSQLDataManager, IChangedSaleZoneRoutingProductDataManager
+    {
+        #region Fields / Properties
+
+        private long _processInstanceId;
+
+        public long ProcessInstanceId
+        {
+            set { _processInstanceId = value; }
+        }
+
+        #endregion
+
+        #region Constructors
+
+        public ChangedSaleZoneRoutingProductDataManager() :
+            base(GetConnectionStringName("TOneWhS_BE_DBConnStringKey", "TOneWhS_BE_DBConnString"))
+        {
+
+        }
+
+        #endregion
+
+        #region Public Methods
+
+        public void ApplyChangedSaleZoneRoutingProductsToDB(IEnumerable<ChangedSaleZoneRoutingProduct> changedSaleZoneRoutingProducts)
+        {
+            IBulkApplyDataManager<ChangedSaleZoneRoutingProduct> bulkApplyDataManager = this;
+            object streamForDBApply = bulkApplyDataManager.InitialiazeStreamForDBApply();
+
+            foreach (var changedSaleZoneRoutingProduct in changedSaleZoneRoutingProducts)
+                bulkApplyDataManager.WriteRecordToStream(changedSaleZoneRoutingProduct, streamForDBApply);
+
+            object bulkInsertInfo = bulkApplyDataManager.FinishDBApplyStream(streamForDBApply);
+            this.InsertBulkToTable(bulkInsertInfo as StreamBulkInsertInfo);
+        }
+
+        #endregion
+
+        #region Private Methods
+
+        object Vanrise.Data.IBulkApplyDataManager<ChangedSaleZoneRoutingProduct>.InitialiazeStreamForDBApply()
+        {
+            return base.InitializeStreamForBulkInsert();
+        }
+
+        void Vanrise.Data.IBulkApplyDataManager<ChangedSaleZoneRoutingProduct>.WriteRecordToStream(ChangedSaleZoneRoutingProduct record, object dbApplyStream)
+        {
+            StreamForBulkInsert streamForBulkInsert = dbApplyStream as StreamForBulkInsert;
+            streamForBulkInsert.WriteRecord
+            (
+                "{0}^{1}^{2}",
+                record.SaleEntityRoutingProductId,
+                _processInstanceId,
+                record.EED
+            );
+        }
+
+        object Vanrise.Data.IBulkApplyDataManager<ChangedSaleZoneRoutingProduct>.FinishDBApplyStream(object dbApplyStream)
+        {
+            StreamForBulkInsert streamForBulkInsert = dbApplyStream as StreamForBulkInsert;
+            streamForBulkInsert.Close();
+            return new StreamBulkInsertInfo
+            {
+                TableName = "TOneWhS_Sales.RP_SaleZoneRoutingProduct_Changed",
+                Stream = streamForBulkInsert,
+                TabLock = false,
+                KeepIdentity = false,
+                FieldSeparator = '^',
+            };
+        }
+
+        #endregion
+    }
+}
