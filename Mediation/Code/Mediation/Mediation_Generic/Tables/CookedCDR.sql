8db6521ea94814f85ab40401fb3ab268923da3da
Wissam Ajamy <VANRISE\wissam.ajamy>
2016-07-20 07:41:01 +0000

    update structure from Mediation_Dev
    git-tfs-id: [http://developmentvlan:8585/tfs/vanrise.collection]$/;C34298

--- Mediation/Code/Mediation/Mediation_Generic/Tables/CookedCDR.sql
+++ Mediation/Code/Mediation/Mediation_Generic/Tables/CookedCDR.sql
@@ -1,18 +1,21 @@
 ﻿CREATE TABLE [Mediation_Generic].[CookedCDR] (
-    [CallId]               NVARCHAR (100) NULL,
-    [ConnectDateTime]      DATETIME       NULL,
-    [DisconnectDateTime]   DATETIME       NULL,
-    [DisconnectReason]     NVARCHAR (100) NULL,
-    [CallProgressState]    NVARCHAR (100) NULL,
-    [Account]              NVARCHAR (100) NULL,
-    [OriginatorId]         NVARCHAR (100) NULL,
-    [OriginatorNumber]     NVARCHAR (100) NULL,
-    [OriginatorFromNumber] NVARCHAR (100) NULL,
-    [OriginalDialedNumber] NVARCHAR (100) NULL,
-    [TerminatorId]         NVARCHAR (100) NULL,
-    [TerminatorNumber]     NVARCHAR (100) NULL,
-    [IncomingGwId]         NVARCHAR (100) NULL,
-    [OutgoingGwId]         NVARCHAR (100) NULL,
-    [TransferredCallId]    NVARCHAR (100) NULL
+    [CallId]               NVARCHAR (100)  NULL,
+    [ConnectDateTime]      DATETIME        NULL,
+    [DisconnectDateTime]   DATETIME        NULL,
+    [DisconnectReason]     NVARCHAR (100)  NULL,
+    [CallProgressState]    NVARCHAR (100)  NULL,
+    [Account]              NVARCHAR (100)  NULL,
+    [OriginatorId]         NVARCHAR (100)  NULL,
+    [OriginatorNumber]     NVARCHAR (100)  NULL,
+    [OriginatorFromNumber] NVARCHAR (100)  NULL,
+    [OriginalDialedNumber] NVARCHAR (100)  NULL,
+    [TerminatorId]         NVARCHAR (100)  NULL,
+    [TerminatorNumber]     NVARCHAR (100)  NULL,
+    [IncomingGwId]         NVARCHAR (100)  NULL,
+    [OutgoingGwId]         NVARCHAR (100)  NULL,
+    [TransferredCallId]    NVARCHAR (100)  NULL,
+    [DurationInSeconds]    DECIMAL (10, 4) NULL
 );
+
+
 
