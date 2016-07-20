5ffe2fc2ecbce7da899d660470afb2dfd427aff4
Samer Haidar <VANRISE\samer.haidar>
2016-07-20 15:38:05 +0000

    Adding classes needed for live balance and billing transaction
    git-tfs-id: [http://developmentvlan:8585/tfs/vanrise.collection]$/;C34328

---
+++ Vanrise/Code/Vanrise.AccountBalance.Data.SQL/LiveBalanceDataManager.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Vanrise.Data.SQL;
+
+namespace Vanrise.AccountBalance.Data.SQL
+{
+    public class LiveBalanceDataManager : BaseSQLDataManager, ILiveBalanceDataManager
+    {
+
+    }
+}
