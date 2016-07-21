9b0a17c8d971bfb1519b36d92c6eeb2156c0a20b
Wissam Ajamy <VANRISE\wissam.ajamy>
2016-07-21 12:14:43 +0000

    update structure from TOneConfiguration
    git-tfs-id: [http://developmentvlan:8585/tfs/vanrise.collection]$/;C34344

--- TOneV2/Code/TOneV2/SQL.TOneConfiguration/common/Stored Procedures/sp_IDManager_Update.sql
+++ TOneV2/Code/TOneV2/SQL.TOneConfiguration/common/Stored Procedures/sp_IDManager_Update.sql
@@ -4,12 +4,15 @@
 -- Create date: <Create Date,,>
 -- Description:	<Description,,>
 -- =============================================
-Create PROCEDURE [common].[sp_IDManager_Update]
+CREATE PROCEDURE [common].[sp_IDManager_Update]
 	@TypeID int,
 	@LastTakenID bigint
 AS
 BEGIN
-
+	IF NOT EXISTS(SELECT 1 FROM common.IDManager WHERE TypeID = @TypeID)
+	Insert Into common.IDManager (TypeID, LastTakenID) values (@TypeID, @LastTakenID)
+	
+	Else
 	Update common.IDManager
 	Set LastTakenID=@LastTakenID
 	where TypeID=@TypeID
