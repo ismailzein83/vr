e8ad69366805724e997a1e8af590c38088e89c5c
Zeinab Mokdad <VANRISE\zeinab.mokdad>
2016-07-21 14:32:15 +0000

    adding IStatusChargingSetDataManager
    git-tfs-id: [http://developmentvlan:8585/tfs/vanrise.collection]$/;C34355

---
+++ Retail/Code/Retail.BusinessEntity.Data/IStatusChargingSetDataManager.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections.Generic;
+using Retail.BusinessEntity.Entities;
+
+namespace Retail.BusinessEntity.Data
+{
+    public interface IStatusChargingSetDataManager : IDataManager
+    {
+        List<StatusChargingSet> GetStatusChargingSets();
+    }
+}
