33181c8f83a07cf441f687c1907208b832d7e8c2
Zeinab Mokdad <VANRISE\zeinab.mokdad>
2016-07-21 14:31:45 +0000

    adding StatusChargingSetDataManager
    git-tfs-id: [http://developmentvlan:8585/tfs/vanrise.collection]$/;C34354

---
+++ Retail/Code/Retail.BusinessEntity.Data.SQL/StatusChargingSetDataManager.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data;
+using Retail.BusinessEntity.Entities;
+using Vanrise.Data.SQL;
+using Vanrise.Common;
+
+namespace Retail.BusinessEntity.Data.SQL
+{
+    public class StatusChargingSetDataManager : BaseSQLDataManager, IStatusChargingSetDataManager
+    {
+        #region Public Methods
+        public List<StatusChargingSet> GetStatusChargingSets()
+        {
+            return GetItemsSP("Retail.sp_StatusChargingSet_GetAll", StatusChargingSetsnMapper);
+        }
+        #endregion
+
+        #region Mappers
+        StatusChargingSet StatusChargingSetsnMapper(IDataReader reader)
+        {
+            StatusChargingSet statusChargingSet = new StatusChargingSet
+            {
+                StatusChargingSetId = (int)reader["ID"],
+                Name = reader["Name"] as string
+            };
+            string settingsSerialized = reader["Settings"] as string;
+            if (settingsSerialized != null)
+                statusChargingSet.Settings = Serializer.Deserialize<StatusChargingSetSettings>(settingsSerialized);
+            return statusChargingSet;
+        }
+        #endregion
+    }
+}
