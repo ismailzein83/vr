7022cac57ba80152563812b8f751fcfb02945765
Ismail Zein <VANRISE\ismail.zein>
2016-07-22 10:01:24 +0000

    added Data Charging Policy Settings
    git-tfs-id: [http://developmentvlan:8585/tfs/vanrise.collection]$/;C34393

---
+++ Retail/Code/Retail.Data.Entities/DataChargingPolicyDefinitionSettings.cs
@@ -0,0 +1,24 @@
+﻿using Retail.BusinessEntity.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Retail.Data.Entities
+{
+    public class DataChargingPolicyDefinitionSettings : ChargingPolicyDefinitionSettings
+    {
+        public override string ChargingPolicyEditor
+        {
+            get
+            {
+                return "retail-data-chargingpolicy-settings";
+            }
+            set
+            {
+
+            }
+        }
+    }
+}
---
+++ Retail/Code/Retail.Data.Entities/DataChargingPolicySettings.cs
@@ -0,0 +1,20 @@
+﻿using Retail.BusinessEntity.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Retail.Data.Entities
+{
+    public class DataChargingPolicySettings : ChargingPolicySettings
+    {
+        public int DownloadSpeedInKbps { get; set; }
+
+        public int UploadSpeedInKbps { get; set; }
+
+        public int DownloadQuotaInMB { get; set; }
+
+        public int UploadQuotaInMB { get; set; }
+    }
+}
