763170acfbce564213f60812dc9986ef003930bc
Samer Haidar <VANRISE\samer.haidar>
2016-07-25 15:12:12 +0000

    adding vractionservice
    git-tfs-id: [http://developmentvlan:8585/tfs/vanrise.collection]$/;C34436

---
+++ Vanrise/Code/Vanrise.Notification.Web/VR_Notification/Services/VRActionService.js
@@ -0,0 +1,41 @@
+﻿
+(function (appControllers) {
+
+    "use strict";
+
+    VRActionService.$inject = ['VRModalService'];
+
+    function VRActionService(VRModalService) {
+
+        function addVRAction(onVRActionAdded) {
+            var settings = {};
+
+            settings.onScopeReady = function (modalScope) {
+                modalScope.onVRActionAdded = onVRActionAdded
+            };
+            VRModalService.showModal('/Client/Modules/VR_Notification/Views/VRActions/VRActionEditor.html', null, settings);
+        };
+
+        function editVRAction(vrActionEntity, onVRActionUpdated) {
+            var settings = {};
+
+            var parameters = {
+                vrActionEntity: vrActionEntity,
+            };
+
+            settings.onScopeReady = function (modalScope) {
+                modalScope.onVRActionUpdated = onVRActionUpdated;
+            };
+            VRModalService.showModal('/Client/Modules/VR_Notification/Views/VRActions/VRActionEditor.html', parameters, settings);
+        }
+
+
+        return {
+            addVRAction: addVRAction,
+            editVRAction: editVRAction
+        };
+    }
+
+    appControllers.service('VR_Notification_VRActionService', VRActionService);
+
+})(appControllers);
