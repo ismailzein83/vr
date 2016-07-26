e2462d0d95dfa49c29facf4929ddbc40a90322d3
Zeinab Mokdad <VANRISE\zeinab.mokdad>
2016-07-26 08:27:59 +0000

    adding grid retrieval functions
    git-tfs-id: [http://developmentvlan:8585/tfs/vanrise.collection]$/;C34451

--- Retail/Code/Retail.BusinessEntity.Web/Retail_BusinessEntity/Directives/StatusChargingSet/retail-be-statuschargingset-grid.js
+++ Retail/Code/Retail.BusinessEntity.Web/Retail_BusinessEntity/Directives/StatusChargingSet/retail-be-statuschargingset-grid.js
@@ -1,9 +1,27 @@
 ﻿'use strict';
 
-app.directive('retailBeStatusdefinitionGrid', ['Retail_BE_StatusChargingSetAPIService', 'Retail_BE_StatusChargingSetService ', 'VRNotificationService',
-    function (retailBeStatusChargingSetApiService, retailBeStatusChargingSetService, vrNotificationService) {
+app.directive('retailBeStatuschargingsetGrid', ['Retail_BE_StatusChargingSetAPIService', 'VRNotificationService',
+    function (retailBeStatusChargingSetApiService, vrNotificationService) {
 
         function retailBeStatusChargingSetGrid($scope, ctrl, $attrs) {
+            function initializeController() {
+                $scope.scopeModel = {};
+                $scope.scopeModel.statusChargingSet = [];
+                $scope.scopeModel.menuActions = [];
+
+                $scope.scopeModel.onGridReady = function (api) {
+                    gridAPI = api;
+                    defineAPI();
+                };
+                $scope.scopeModel.dataRetrievalFunction = function (dataRetrievalInput, onResponseReady) {
+                    return retailBeStatusChargingSetApiService.GetFilteredStatusChargingSet(dataRetrievalInput).then(function (response) {
+                        onResponseReady(response);
+                    }).catch(function (error) {
+                        vrNotificationService.notifyExceptionWithClose(error, $scope);
+                    });
+                };
+            }
+
             this.initializeController = initializeController;
 
             var gridAPI;
@@ -26,33 +44,6 @@ app.directive('retailBeStatusdefinitionGrid', ['Retail_BE_StatusChargingSetAPISe
                 if (ctrl.onReady != null)
                     ctrl.onReady(api);
             }
-
-            function initializeController() {
-                $scope.scopeModel = {};
-                $scope.scopeModel.statusDefinition = [];
-                $scope.scopeModel.menuActions = [];
-
-                $scope.scopeModel.onGridReady = function (api) {
-                    gridAPI = api;
-                    defineAPI();
-                };
-
-
-                // defineMenuActions();
-            } //function defineMenuActions() {
-            //    $scope.scopeModel.menuActions.push({
-            //        name: 'Edit',
-            //        clicked: editStatusDefinition,
-            //    });
-            //}
-
-            //function editStatusDefinition(statusDefinitionItem) {
-            //    var onStatusDefinitionUpdated = function (updatedStatusDefinition) {
-            //        gridAPI.itemUpdated(updatedStatusDefinition);
-            //    };
-
-            //    retailBeStatusChargingService.editStatusDefinition(statusDefinitionItem.Entity.StatusDefinitionId, onStatusDefinitionUpdated);
-            //}
         }
 
         return {
