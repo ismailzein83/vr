45f490b62451029f257c88355bac26699219f463
Zeinab Mokdad <VANRISE\zeinab.mokdad>
2016-07-26 08:28:45 +0000

    adding details
    git-tfs-id: [http://developmentvlan:8585/tfs/vanrise.collection]$/;C34453

---
+++ Retail/Code/Retail.BusinessEntity.Entities/StatusChargingSetDetail.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Retail.BusinessEntity.Entities
+{
+    public class StatusChargingSetDetail
+    {
+        public StatusChargingSet Entity { get; set; }
+        public string Description { get; set; }
+    }
+}
