775e9ce85c58e74ac796913f479c5b7380888218
hadi.hawi <hadi.hawi@vanrise.tfs.local>
2016-07-27 09:23:39 +0000

    validation on group name
    git-tfs-id: [http://developmentvlan:8585/tfs/vanrise.collection]$/;C34494

--- CallGenerator/CallGeneratorLibrary/Repositories/TestNumberGroupRepository.cs
+++ CallGenerator/CallGeneratorLibrary/Repositories/TestNumberGroupRepository.cs
@@ -51,7 +51,7 @@ namespace CallGeneratorLibrary.Repositories
             {
                 using (CallGeneratorModelDataContext context = new CallGeneratorModelDataContext())
                 {
-                    log = context.TestNumberGroups.Where(l => (l.Name == Name &&l.IsDeleted != true)).FirstOrDefault<TestNumberGroup>();
+                    log = context.TestNumberGroups.Where(l => (l.Name == Name && (l.IsDeleted == false || l.IsDeleted == null ) )).FirstOrDefault<TestNumberGroup>();
                 }
             }
             catch (System.Exception ex)
