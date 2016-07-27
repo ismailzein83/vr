24ac3d38ae6a33c1e21581013efd54813b70758d
Wissam Ajamy <VANRISE\wissam.ajamy>
2016-07-27 16:07:52 +0000

    Add Deals Post deployement script
    git-tfs-id: [http://developmentvlan:8585/tfs/vanrise.collection]$/;C34529

---
+++ TOneV2/Code/TOneV2/SQL.TOneConfiguration/Deals.PostDeployment.sql
@@ -0,0 +1,127 @@
+﻿/*
+Post-Deployment Script Template							
+--------------------------------------------------------------------------------------
+ This file contains SQL statements that will be appended to the build script.		
+ Use SQLCMD syntax to include a file in the post-deployment script.			
+ Example:      :r .\myfile.sql								
+ Use SQLCMD syntax to reference a variable in the post-deployment script.		
+ Example:      :setvar TableName MyTable							
+               SELECT * FROM [$(TableName)]					
+--------------------------------------------------------------------------------------
+*/
+--[sec].[Module]---------------------------1701 to 1800---------------------------------------------------------
+--------------------------------------------------------------------------------------------------------------
+set nocount on;
+set identity_insert [sec].[Module] on;
+;with cte_data([Id],[Name],[Url],[ParentId],[Icon],[Rank],[AllowDynamic])
+as (select * from (values
+--//////////////////////////////////////////////////////////////////////////////////////////////////
+(1701,'Deals',null,null,'/images/menu-icons/busines intel.png',55,0)
+--\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\
+)c([Id],[Name],[Url],[ParentId],[Icon],[Rank],[AllowDynamic]))
+merge	[sec].[Module] as t
+using	cte_data as s
+on		1=1 and t.[Id] = s.[Id]
+when matched then
+	update set
+	[Name] = s.[Name],[Url] = s.[Url],[ParentId] = s.[ParentId],[Icon] = s.[Icon],[Rank] = s.[Rank],[AllowDynamic] = s.[AllowDynamic]
+when not matched by target then
+	insert([Id],[Name],[Url],[ParentId],[Icon],[Rank],[AllowDynamic])
+	values(s.[Id],s.[Name],s.[Url],s.[ParentId],s.[Icon],s.[Rank],s.[AllowDynamic]);
+set identity_insert [sec].[Module] off;
+
+--[sec].[View]-----------------------------18001 to 19000---------------------------------------------------------
+-------------------------------------------------------------------------------------------------------------
+set nocount on;
+set identity_insert [sec].[View] on;
+;with cte_data([Id],[Name],[Title],[Url],[Module],[ActionNames],[Audience],[Content],[Settings],[Type],[Rank])
+as (select * from (values
+--//////////////////////////////////////////////////////////////////////////////////////////////////
+(18001,'Deals Analysis','Deals Analysis Management','#/view/WhS_BusinessEntity/Views/Deal/DealAnalysisManagement',1701,'',null,null,null,0,1),
+(18002,'Deals Management','Deals Management','#/view/WhS_BusinessEntity/Views/Deal/DealManagement',1701,'WhS_BE/Deal/GetFilteredDeals',null,null,null,0,2),
+
+(18003,'Bilateral Agreement','Bilateral Agreement','#/view/WhS_BusinessEntity/Views/Deal/DealManagement',1701,'WhS_BE/Deal/GetFilteredDeals',null,null,null,0,3),
+(18004,'Volume Commitment','Volume Commitment','#/view/WhS_BusinessEntity/Views/Deal/DealManagement',1701,'WhS_BE/Deal/GetFilteredDeals',null,null,null,0,4),
+
+(18005,'Progress Reprot','Progress Reprot','#/view/WhS_BusinessEntity/Views/Deal/DealManagement',1701,'WhS_BE/Deal/GetFilteredDeals',null,null,null,0,5),
+(18006,'Over All Progress Report','Over All Progress Report','#/view/WhS_BusinessEntity/Views/Deal/DealManagement',1701,'WhS_BE/Deal/GetFilteredDeals',null,null,null,0,6),
+
+(18007,'Alerts Management','Alerts Management','#/view/WhS_BusinessEntity/Views/Deal/DealManagement',1701,'WhS_BE/Deal/GetFilteredDeals',null,null,null,0,7),
+(18008,'Alerts History','Alerts History','#/view/WhS_BusinessEntity/Views/Deal/DealManagement',1701,'WhS_BE/Deal/GetFilteredDeals',null,null,null,0,8)
+--\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\
+)c([Id],[Name],[Title],[Url],[Module],[ActionNames],[Audience],[Content],[Settings],[Type],[Rank]))
+merge	[sec].[View] as t
+using	cte_data as s
+on		1=1 and t.[Id] = s.[Id]
+when matched then
+	update set
+	[Name] = s.[Name],[Title] = s.[Title],[Url] = s.[Url],[Module] = s.[Module],[ActionNames] = s.[ActionNames],[Audience] = s.[Audience],[Content] = s.[Content],[Settings] = s.[Settings],[Type] = s.[Type],[Rank] = s.[Rank]
+when not matched by target then
+	insert([Id],[Name],[Title],[Url],[Module],[ActionNames],[Audience],[Content],[Settings],[Type],[Rank])
+	values(s.[Id],s.[Name],s.[Title],s.[Url],s.[Module],s.[ActionNames],s.[Audience],s.[Content],s.[Settings],s.[Type],s.[Rank]);
+set identity_insert [sec].[View] off;
+
+--[sec].[BusinessEntityModule]----------1501 to 1600------------------------------------------------
+----------------------------------------------------------------------------------------------------
+set nocount on;
+set identity_insert [sec].[BusinessEntityModule] on;
+;with cte_data([Id],[Name],[ParentId],[BreakInheritance])
+as (select * from (values
+--//////////////////////////////////////////////////////////////////////////////////////////////////
+(1501,'Deals',1,0)
+--\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\
+)c([Id],[Name],[ParentId],[BreakInheritance]))
+merge	[sec].[BusinessEntityModule] as t
+using	cte_data as s
+on		1=1 and t.[Id] = s.[Id]
+when matched then
+	update set
+	[Name] = s.[Name],[ParentId] = s.[ParentId],[BreakInheritance] = s.[BreakInheritance]
+when not matched by target then
+	insert([Id],[Name],[ParentId],[BreakInheritance])
+	values(s.[Id],s.[Name],s.[ParentId],s.[BreakInheritance]);
+set identity_insert [sec].[BusinessEntityModule] off;
+
+--[sec].[BusinessEntity]----------------4501 to 4800------------------------------------------------
+----------------------------------------------------------------------------------------------------
+set nocount on;
+set identity_insert [sec].[BusinessEntity] on;
+;with cte_data([Id],[Name],[Title],[ModuleId],[BreakInheritance],[PermissionOptions])
+as (select * from (values
+--//////////////////////////////////////////////////////////////////////////////////////////////////
+(4501,'WhS_BE_Deal','Deal',1501,0,'["View","Add","Edit"]'),
+(4502,'WhS_BE_Deal_Analysis','Deal Analysis',1501,0,'["View","Add","Edit"]')
+--\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\
+)c([Id],[Name],[Title],[ModuleId],[BreakInheritance],[PermissionOptions]))
+merge	[sec].[BusinessEntity] as t
+using	cte_data as s
+on		1=1 and t.[Id] = s.[Id]
+when matched then
+	update set
+	[Name] = s.[Name],[Title] = s.[Title],[ModuleId] = s.[ModuleId],[BreakInheritance] = s.[BreakInheritance],[PermissionOptions] = s.[PermissionOptions]
+when not matched by target then
+	insert([Id],[Name],[Title],[ModuleId],[BreakInheritance],[PermissionOptions])
+	values(s.[Id],s.[Name],s.[Title],s.[ModuleId],s.[BreakInheritance],s.[PermissionOptions]);
+set identity_insert [sec].[BusinessEntity] off;
+
+--[sec].[SystemAction]------------------------------------------------------------------------------
+----------------------------------------------------------------------------------------------------
+set nocount on;
+;with cte_data([Name],[RequiredPermissions])
+as (select * from (values
+--//////////////////////////////////////////////////////////////////////////////////////////////////
+('WhS_BE/Deal/GetFilteredDeals','WhS_BE_Deal: View'),
+('WhS_BE/Deal/GetDeal','WhS_BE_Deal: View'),
+('WhS_BE/Deal/AddDeal','WhS_BE_Deal: Add'),
+('WhS_BE/Deal/UpdateDeal','WhS_BE_Deal: Edit')
+--\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\
+)c([Name],[RequiredPermissions]))
+merge	[sec].[SystemAction] as t
+using	cte_data as s
+on		1=1 and t.[Name] = s.[Name]
+when matched then
+	update set
+	[RequiredPermissions] = s.[RequiredPermissions]
+when not matched by target then
+	insert([Name],[RequiredPermissions])
+	values(s.[Name],s.[RequiredPermissions]);
