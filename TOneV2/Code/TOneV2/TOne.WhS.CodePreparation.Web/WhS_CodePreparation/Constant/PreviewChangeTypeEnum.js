51061b9c49fda53f482b3921c25a184634630260
mostafa.jawhari <mostafa.jawhari@vanrise.tfs.local>
2016-07-28 12:57:53 +0000

    Adding items to source control
    git-tfs-id: [http://developmentvlan:8585/tfs/vanrise.collection]$/;C34567

---
+++ TOneV2/Code/TOneV2/TOne.WhS.CodePreparation.Web/WhS_CodePreparation/Constant/PreviewChangeTypeEnum.js
@@ -0,0 +1,4 @@
+﻿app.constant("WhS_CP_PreviewChangeTypeEnum", {
+    AllEntities: { value: false, description: "All Entities" },
+    OnlyModifiedEntities: { value: true, description: "Only Modified Entities" }
+});
---
+++ TOneV2/Code/TOneV2/TOne.WhS.CodePreparation.Web/WhS_CodePreparation/Constant/PreviewGroupedBy.js
@@ -0,0 +1,5 @@
+﻿app.constant("WhS_CP_PreviewGroupedBy", {
+    Countries: { dwGrid: "vr-cp-countrypreview-grid", description: "Countries" },
+    Zones: { dwGrid: "vr-cp-zonepreview-grid", description: "Zones" },
+    Codes: { dwGrid: "vr-cp-codepreview-grid", description: "Codes" }
+});
