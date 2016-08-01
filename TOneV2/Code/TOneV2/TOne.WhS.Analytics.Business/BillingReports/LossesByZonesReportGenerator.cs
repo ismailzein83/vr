321c82b5fa408cfdb2d25079f7d7c3de2eed9ebf
Zeinab Mokdad <VANRISE\zeinab.mokdad>
2016-08-01 13:38:30 +0000

    1887:missing customer and supplier filters in header
    git-tfs-id: [http://developmentvlan:8585/tfs/vanrise.collection]$/;C34660

--- TOneV2/Code/TOneV2/TOne.WhS.Analytics.Business/BillingReports/LossesByZonesReportGenerator.cs
+++ TOneV2/Code/TOneV2/TOne.WhS.Analytics.Business/BillingReports/LossesByZonesReportGenerator.cs
@@ -17,21 +17,21 @@ namespace TOne.WhS.Analytics.Business.BillingReports
             List<string> listMeasures = new List<string>();
 
 
-               
-              
-                listGrouping.Add("SupplierZone");
-                listGrouping.Add("SaleZone");
-                listGrouping.Add("Supplier");
-                listGrouping.Add("Customer");
+
+
+            listGrouping.Add("SupplierZone");
+            listGrouping.Add("SaleZone");
+            listGrouping.Add("Supplier");
+            listGrouping.Add("Customer");
 
 
 
-                listMeasures.Add("SaleRate");
-                listMeasures.Add("CostRate");
-                listMeasures.Add("SaleDuration");
-                listMeasures.Add("CostDuration");
-                listMeasures.Add("SaleNet");
-                listMeasures.Add("CostNet");
+            listMeasures.Add("SaleRate");
+            listMeasures.Add("CostRate");
+            listMeasures.Add("SaleDuration");
+            listMeasures.Add("CostDuration");
+            listMeasures.Add("SaleNet");
+            listMeasures.Add("CostNet");
 
 
             Vanrise.Entities.DataRetrievalInput<AnalyticQuery> analyticQuery = new DataRetrievalInput<AnalyticQuery>()
@@ -84,98 +84,121 @@ namespace TOne.WhS.Analytics.Business.BillingReports
             List<LossesByZonesFormatted> listRateLossFromatted = new List<LossesByZonesFormatted>();
 
             var result = analyticManager.GetFilteredRecords(analyticQuery) as AnalyticSummaryBigResult<AnalyticRecord>;
-            if(result != null)
-            foreach (var analyticRecord in result.Data)
-            {
-                LossesByZonesFormatted rateLossFromatted = new LossesByZonesFormatted();
-
-               
-
-
-                var supplierZoneValue = analyticRecord.DimensionValues[0];
-                if (supplierZoneValue != null)
-                    rateLossFromatted.CostZone = supplierZoneValue.Name;
-
-
-
-                var customerZoneValue = analyticRecord.DimensionValues[1];
-                if (customerZoneValue != null)
-                    rateLossFromatted.SaleZone = customerZoneValue.Name;
-
-
-                var supplierValue = analyticRecord.DimensionValues[2];
-                if (supplierValue != null)
-                    rateLossFromatted.Supplier = supplierValue.Name;
-
-                var customerValue = analyticRecord.DimensionValues[3];
-                if (customerValue != null)
-                    rateLossFromatted.Customer = customerValue.Name;
+            if (result != null)
+                foreach (var analyticRecord in result.Data)
+                {
+                    LossesByZonesFormatted rateLossFromatted = new LossesByZonesFormatted();
 
 
 
 
-
-                MeasureValue saleRate;
-                analyticRecord.MeasureValues.TryGetValue("SaleRate", out saleRate);
-                rateLossFromatted.SaleRate = Convert.ToDouble(saleRate.Value ?? 0.0);
-                rateLossFromatted.SaleRateFormatted = ReportHelpers.FormatNumber(rateLossFromatted.SaleRate);
-
-
-                MeasureValue costRate;
-                analyticRecord.MeasureValues.TryGetValue("CostRate", out costRate);
-                rateLossFromatted.CostRate = Convert.ToDouble(costRate.Value ?? 0.0);
-                rateLossFromatted.CostRateFormatted = ReportHelpers.FormatNumber(rateLossFromatted.CostRate);
-
-
-                MeasureValue saleDuration;
-                analyticRecord.MeasureValues.TryGetValue("SaleDuration", out saleDuration);
-                rateLossFromatted.SaleDuration = Convert.ToDecimal(saleDuration.Value ?? 0.0);
-                rateLossFromatted.SaleDurationFormatted = ReportHelpers.FormatNumber(rateLossFromatted.SaleDuration);
-
-
-                MeasureValue costDuration;
-                analyticRecord.MeasureValues.TryGetValue("CostDuration", out costDuration);
-                rateLossFromatted.CostDuration = Convert.ToDecimal(costDuration.Value ?? 0.0);
-                rateLossFromatted.CostDurationFormatted = ReportHelpers.FormatNumber(rateLossFromatted.CostDuration);
+                    var supplierZoneValue = analyticRecord.DimensionValues[0];
+                    if (supplierZoneValue != null)
+                        rateLossFromatted.CostZone = supplierZoneValue.Name;
 
 
 
-                MeasureValue saleNet;
-                analyticRecord.MeasureValues.TryGetValue("SaleNet", out saleNet);
-                rateLossFromatted.SaleNet = Convert.ToDouble(saleNet.Value ?? 0.0);
-                rateLossFromatted.SaleNetFormatted = ReportHelpers.FormatNumber(rateLossFromatted.SaleNet);
+                    var customerZoneValue = analyticRecord.DimensionValues[1];
+                    if (customerZoneValue != null)
+                        rateLossFromatted.SaleZone = customerZoneValue.Name;
 
 
-                MeasureValue costNet;
-                analyticRecord.MeasureValues.TryGetValue("CostNet", out costNet);
-                rateLossFromatted.CostNet = Convert.ToDouble(costNet.Value ?? 0.0);
-                rateLossFromatted.CostNetFormatted = ReportHelpers.FormatNumber(rateLossFromatted.CostNet);
+                    var supplierValue = analyticRecord.DimensionValues[2];
+                    if (supplierValue != null)
+                        rateLossFromatted.Supplier = supplierValue.Name;
+
+                    var customerValue = analyticRecord.DimensionValues[3];
+                    if (customerValue != null)
+                        rateLossFromatted.Customer = customerValue.Name;
 
 
-                rateLossFromatted.LossFormatted = ReportHelpers.FormatNumber(rateLossFromatted.CostNet - rateLossFromatted.SaleNet);
-               rateLossFromatted.LossPerFormatted = ReportHelpers.FormatNumber(((rateLossFromatted.CostNet - rateLossFromatted.SaleNet) * 100) / rateLossFromatted.CostNet);
 
 
-                listRateLossFromatted.Add(rateLossFromatted);
-            }
 
-            Dictionary<string, System.Collections.IEnumerable> dataSources = new Dictionary<string, System.Collections.IEnumerable>();
-            dataSources.Add("RateLoss", listRateLossFromatted);
+                    MeasureValue saleRate;
+                    analyticRecord.MeasureValues.TryGetValue("SaleRate", out saleRate);
+                    rateLossFromatted.SaleRate = Convert.ToDouble(saleRate.Value ?? 0.0);
+                    rateLossFromatted.SaleRateFormatted = ReportHelpers.FormatNumber(rateLossFromatted.SaleRate);
+
+
+                    MeasureValue costRate;
+                    analyticRecord.MeasureValues.TryGetValue("CostRate", out costRate);
+                    rateLossFromatted.CostRate = Convert.ToDouble(costRate.Value ?? 0.0);
+                    rateLossFromatted.CostRateFormatted = ReportHelpers.FormatNumber(rateLossFromatted.CostRate);
+
+
+                    MeasureValue saleDuration;
+                    analyticRecord.MeasureValues.TryGetValue("SaleDuration", out saleDuration);
+                    rateLossFromatted.SaleDuration = Convert.ToDecimal(saleDuration.Value ?? 0.0);
+                    rateLossFromatted.SaleDurationFormatted = ReportHelpers.FormatNumber(rateLossFromatted.SaleDuration);
+
+
+                    MeasureValue costDuration;
+                    analyticRecord.MeasureValues.TryGetValue("CostDuration", out costDuration);
+                    rateLossFromatted.CostDuration = Convert.ToDecimal(costDuration.Value ?? 0.0);
+                    rateLossFromatted.CostDurationFormatted = ReportHelpers.FormatNumber(rateLossFromatted.CostDuration);
+
+
+
+                    MeasureValue saleNet;
+                    analyticRecord.MeasureValues.TryGetValue("SaleNet", out saleNet);
+                    rateLossFromatted.SaleNet = Convert.ToDouble(saleNet.Value ?? 0.0);
+                    rateLossFromatted.SaleNetFormatted = ReportHelpers.FormatNumber(rateLossFromatted.SaleNet);
+
+
+                    MeasureValue costNet;
+                    analyticRecord.MeasureValues.TryGetValue("CostNet", out costNet);
+                    rateLossFromatted.CostNet = Convert.ToDouble(costNet.Value ?? 0.0);
+                    rateLossFromatted.CostNetFormatted = ReportHelpers.FormatNumber(rateLossFromatted.CostNet);
+
+
+                    rateLossFromatted.LossFormatted = ReportHelpers.FormatNumber(rateLossFromatted.CostNet - rateLossFromatted.SaleNet);
+                    rateLossFromatted.LossPerFormatted = ReportHelpers.FormatNumber(((rateLossFromatted.CostNet - rateLossFromatted.SaleNet) * 100) / rateLossFromatted.CostNet);
+
+
+                    listRateLossFromatted.Add(rateLossFromatted);
+                }
+
+            Dictionary<string, System.Collections.IEnumerable> dataSources =
+                new Dictionary<string, System.Collections.IEnumerable> { { "RateLoss", listRateLossFromatted } };
             return dataSources;
         }
 
         public Dictionary<string, RdlcParameter> GetRdlcReportParameters(ReportParameters parameters)
         {
-            Dictionary<string, RdlcParameter> list = new Dictionary<string, RdlcParameter>();
+            Dictionary<string, RdlcParameter> list = new Dictionary<string, RdlcParameter>
+            {
+                {"FromDate", new RdlcParameter {Value = parameters.FromTime.ToString(), IsVisible = true}},
+                {
+                    "ToDate",
+                    new RdlcParameter
+                    {
+                        Value = (parameters.ToTime.HasValue) ? parameters.ToTime.ToString() : null,
+                        IsVisible = true
+                    }
+                },
+                {
+                    "Customer",
+                    new RdlcParameter
+                    {
+                        Value = ReportHelpers.GetCarrierName(parameters.CustomersId, "Customers"),
+                        IsVisible = true
+                    }
+                },
+                {
+                    "Supplier",
+                    new RdlcParameter
+                    {
+                        Value = ReportHelpers.GetCarrierName(parameters.SuppliersId, "Suppliers"),
+                        IsVisible = true
+                    }
+                },
+                {"Title", new RdlcParameter {Value = "Losses by Zones", IsVisible = true}},
+                {"Currency", new RdlcParameter {Value = parameters.CurrencyDescription, IsVisible = true}},
+                {"LogoPath", new RdlcParameter {Value = "logo", IsVisible = true}},
+                {"DigitRate", new RdlcParameter {Value = "2", IsVisible = true}}
+            };
 
-            list.Add("FromDate", new RdlcParameter { Value = parameters.FromTime.ToString(), IsVisible = true });
-            list.Add("ToDate", new RdlcParameter { Value = (parameters.ToTime.HasValue)?parameters.ToTime.ToString():null, IsVisible = true });
-            list.Add("Supplier", new RdlcParameter { Value = "", IsVisible = true });
-            list.Add("Customer", new RdlcParameter { Value = "", IsVisible = true });
-            list.Add("Title", new RdlcParameter { Value = "Losses by Zones", IsVisible = true });
-            list.Add("Currency", new RdlcParameter { Value = parameters.CurrencyDescription, IsVisible = true });
-            list.Add("LogoPath", new RdlcParameter { Value = "logo", IsVisible = true });
-            list.Add("DigitRate", new RdlcParameter { Value = "2", IsVisible = true });
 
 
             return list;
