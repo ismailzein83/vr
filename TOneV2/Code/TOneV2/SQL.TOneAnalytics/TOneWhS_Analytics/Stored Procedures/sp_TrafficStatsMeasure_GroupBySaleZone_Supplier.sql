a8f0b3c909d9f18f80dc719c6f98b49dd2305dd3
Wissam Ajamy <VANRISE\wissam.ajamy>
2016-08-01 15:48:32 +0000

    Update structure from TOneV2_Dev_Analytics
    git-tfs-id: [http://developmentvlan:8585/tfs/vanrise.collection]$/;C34678

---
+++ TOneV2/Code/TOneV2/SQL.TOneAnalytics/TOneWhS_Analytics/Stored Procedures/sp_TrafficStatsMeasure_GroupBySaleZone_Supplier.sql
@@ -0,0 +1,15 @@
+﻿CREATE PROCEDURE [TOneWhS_Analytics].[sp_TrafficStatsMeasure_GroupBySaleZone_Supplier]
+@TimeSpan int
+AS
+BEGIN
+	SELECT [SupplierID], [SaleZoneId]
+      ,Sum([Attempts]) as TotalNumberOfAttempts
+      ,Sum([DurationInSeconds]) as TotalDurationInSeconds
+      ,Sum([DeliveredAttempts]) as TotalDeliveredAttempts
+      ,Sum([SuccessfulAttempts]) as TotalSuccesfulAttempts
+
+  FROM [TOneWhS_Analytics].[TrafficStats15Min] with (nolock)
+  where DATEDIFF(ss, BatchStart, GETDATE()) < @TimeSpan
+group by [SupplierID], [SaleZoneId]
+     
+END
---
+++ TOneV2/Code/TOneV2/SQL.TOneAnalytics/TOneWhS_Analytics/Stored Procedures/sp_TrafficStatsMeasure_GroupBySupplierZone.sql
@@ -0,0 +1,20 @@
+﻿-- =============================================
+-- Author:		<Author,,Name>
+-- Create date: <Create Date,,>
+-- Description:	<Description,,>
+-- =============================================
+CREATE PROCEDURE [TOneWhS_Analytics].[sp_TrafficStatsMeasure_GroupBySupplierZone]
+@TimeSpan int
+AS
+BEGIN
+	SELECT [SupplierZoneID]
+      ,Sum([Attempts]) as TotalNumberOfAttempts
+      ,Sum([DurationInSeconds]) as TotalDurationInSeconds
+      ,Sum([DeliveredAttempts]) as TotalDeliveredAttempts
+      ,Sum([SuccessfulAttempts]) as TotalSuccesfulAttempts
+
+  FROM [TOneWhS_Analytics].[TrafficStats15Min] with (nolock)
+    where DATEDIFF(ss, BatchStart, GETDATE()) < @TimeSpan
+group by [SupplierZoneID]
+     
+END
