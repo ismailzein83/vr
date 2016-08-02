14e2b80d96f7ec3f4b8ebe1ba482c4c4cbde1d25
mohamad.elhariri <mohamad.elhariri@vanrise.tfs.local>
2016-08-02 13:19:47 +0000

    Refactored the GetTitle method
    git-tfs-id: [http://developmentvlan:8585/tfs/vanrise.collection]$/;C34709

--- TOneV2/Code/TOneV2/TOne.WhS.Sales.BP.Arguments/RatePlanInput.cs
+++ TOneV2/Code/TOneV2/TOne.WhS.Sales.BP.Arguments/RatePlanInput.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using TOne.WhS.BusinessEntity.Business;
 using TOne.WhS.BusinessEntity.Entities;
 
 namespace TOne.WhS.Sales.BP.Arguments
@@ -20,7 +21,23 @@ namespace TOne.WhS.Sales.BP.Arguments
         public override string GetTitle()
         {
             string ownerTypeDescription = Vanrise.Common.Utilities.GetEnumDescription(this.OwnerType);
-            return String.Format("Started the Rate Plan process for {0} '{1}'", ownerTypeDescription, this.OwnerId);
+            string ownerName;
+
+            switch (OwnerType)
+            {
+                case SalePriceListOwnerType.SellingProduct:
+                    var sellingProductManager = new SellingProductManager();
+                    ownerName = sellingProductManager.GetSellingProductName(OwnerId);
+                    break;
+                case SalePriceListOwnerType.Customer:
+                    var carrierAccountManager = new CarrierAccountManager();
+                    ownerName = carrierAccountManager.GetCarrierAccountName(OwnerId);
+                    break;
+                default:
+                    throw new NotImplementedException();
+            }
+
+            return String.Format("Started the Rate Plan process for {0} '{1}'", ownerTypeDescription, ownerName);
         }
     }
 }
