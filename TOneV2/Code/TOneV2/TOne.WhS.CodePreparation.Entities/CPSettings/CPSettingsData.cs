c1da19241e7561aa0640a10056a588a0a35a970c
mostafa.jawhari <mostafa.jawhari@vanrise.tfs.local>
2016-08-02 08:25:42 +0000

    adding CPSettingsData file
    git-tfs-id: [http://developmentvlan:8585/tfs/vanrise.collection]$/;C34684

---
+++ TOneV2/Code/TOneV2/TOne.WhS.CodePreparation.Entities/CPSettings/CPSettingsData.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Vanrise.Entities;
+
+namespace TOne.WhS.CodePreparation.Entities
+{
+    public class CPSettingsData : SettingData
+    {
+        public int EffectiveDateOffset { get; set; }
+    }
+}
