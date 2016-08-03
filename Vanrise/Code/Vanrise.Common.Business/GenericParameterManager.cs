43640b0eb40fe522f6589c2a057dae64e6dc6b0d
Ismail Zein <VANRISE\ismail.zein>
2016-08-03 07:59:49 +0000

    added GenericParameterManager
    git-tfs-id: [http://developmentvlan:8585/tfs/vanrise.collection]$/;C34727

---
+++ Vanrise/Code/Vanrise.Common.Business/GenericParameterManager.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Vanrise.Common.Business
+{
+    public class GenericParameterManager
+    {
+        #region Singleton
+
+        static GenericParameterManager s_current = new GenericParameterManager();
+
+        public static GenericParameterManager Current
+        {
+            get
+            {
+                return s_current;
+            }
+        }
+
+        private GenericParameterManager()
+        {
+
+        }
+
+        #endregion
+        public int GetNormalPrecision()
+        {
+            return 2;
+        }
+
+        public int GetLongPrecision()
+        {
+            return 4;
+        }
+    }
+}
