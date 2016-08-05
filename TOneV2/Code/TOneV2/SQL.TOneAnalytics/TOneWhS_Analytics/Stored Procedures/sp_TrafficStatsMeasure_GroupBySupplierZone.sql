10c7757b4bab64a100a22378aa5719b04b1daa2a
Wissam Ajamy <VANRISE\wissam.ajamy>
2016-08-05 08:10:21 +0000

    update structure from TOneV2_Dev_Analytics
    git-tfs-id: [http://developmentvlan:8585/tfs/vanrise.collection]$/;C34796

--- TOneV2/Code/TOneV2/SQL.TOneAnalytics/TOneWhS_Analytics/Stored Procedures/sp_TrafficStatsMeasure_GroupBySupplierZone.sql
+++ TOneV2/Code/TOneV2/SQL.TOneAnalytics/TOneWhS_Analytics/Stored Procedures/sp_TrafficStatsMeasure_GroupBySupplierZone.sql
@@ -1,25 +1,19 @@
-﻿-- =============================================
--- Author:		<Author,,Name>
--- Create date: <Create Date,,>
--- Description:	<Description,,>
--- =============================================
-CREATE PROCEDURE [TOneWhS_Analytics].[sp_TrafficStatsMeasure_GroupBySupplierZone]
+﻿CREATE PROCEDURE [TOneWhS_Analytics].[sp_TrafficStatsMeasure_GroupBySupplierZone]
 @TimeSpan int
 AS
 BEGIN
 
-	DECLARE @FromTime datetime
-	SET @FromTime = DateAdd(ss, -@TimeSpan, getdate())
+DECLARE @FromTime datetime
+SET @FromTime = DateAdd(ss, -@TimeSpan, getdate())
 
-	SELECT [SupplierZoneID]
-      ,Sum([Attempts]) as TotalNumberOfAttempts
-      ,Sum([DurationInSeconds]) as TotalDurationInSeconds
-      ,Sum([DeliveredAttempts]) as TotalDeliveredAttempts
-      ,Sum([SuccessfulAttempts]) as TotalSuccesfulAttempts
-
-  FROM [TOneWhS_Analytics].[TrafficStats15Min] with (nolock)
-    where	BatchStart > @FromTime AND BatchStart<getdate() 
-			and SupplierZoneId is not null
+SELECT	[SupplierZoneID]
+		,Sum([Attempts]) as TotalNumberOfAttempts
+		,Sum([DurationInSeconds]) as TotalDurationInSeconds
+		,Sum([DeliveredAttempts]) as TotalDeliveredAttempts
+		,Sum([SuccessfulAttempts]) as TotalSuccesfulAttempts
+FROM	[TOneWhS_Analytics].[TrafficStats15Min] with (nolock)
+where	BatchStart > @FromTime AND BatchStart<getdate() 
+		AND SupplierZoneId is not null
 group by [SupplierZoneID]
      
 END
