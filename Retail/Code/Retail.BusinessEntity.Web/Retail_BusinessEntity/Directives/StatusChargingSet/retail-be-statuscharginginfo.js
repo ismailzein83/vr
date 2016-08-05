7c3461a31f07d7cbe2e80e28e83b4bcb02a27d5b
Zeinab Mokdad <VANRISE\zeinab.mokdad>
2016-08-05 15:00:56 +0000

    adding StatusChargingInfo directive
    git-tfs-id: [http://developmentvlan:8585/tfs/vanrise.collection]$/;C34834

---
+++ Retail/Code/Retail.BusinessEntity.Web/Retail_BusinessEntity/Directives/StatusChargingSet/retail-be-statuscharginginfo.js
@@ -0,0 +1,22 @@
+﻿'use strict';
+
+app.directive('retailBeStatuscharginginfo', ['Retail_BE_StatusChargingSetAPIService', 'VRNotificationService', 'Retail_BE_StatusChargingSetService',
+    function (retailBeStatusChargingSetApiService, vrNotificationService, retailBeStatusChargingSetService) {
+
+        function retailBeStatusCharginginfo($scope, ctrl, $attrs) {
+
+        }
+
+        return {
+            restrict: 'E',
+            scope: {
+                onReady: '=',
+            },
+            controller: function ($scope, $element, $attrs) {
+                var ctrl = this;
+            },
+            controllerAs: 'ctrl',
+            bindToController: true,
+            templateUrl: '/Client/Modules/Retail_BusinessEntity/Directives/StatusChargingSet/Templates/StatusChargingInfoTemplate.html'
+        };
+    }]);
