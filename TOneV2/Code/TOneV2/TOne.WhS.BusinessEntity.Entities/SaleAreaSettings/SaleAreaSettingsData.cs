00f9a9ec954bc7ddbd36e0f8f29d00f82f12b18f
mostafa.jawhari <mostafa.jawhari@vanrise.tfs.local>
2016-08-05 09:12:55 +0000

    adding SaleAreaSettingsData file to implement SaleAreaSettings
    git-tfs-id: [http://developmentvlan:8585/tfs/vanrise.collection]$/;C34807

---
+++ TOneV2/Code/TOneV2/TOne.WhS.BusinessEntity.Entities/SaleAreaSettings/SaleAreaSettingsData.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Vanrise.Entities;
+
+namespace TOne.WhS.BusinessEntity.Entities
+{
+    public class SaleAreaSettingsData : SettingData
+    {
+        public List<string> FixedKeywords { get; set; }
+        public List<string> MobileKeywords { get; set; }
+
+    }
+}
