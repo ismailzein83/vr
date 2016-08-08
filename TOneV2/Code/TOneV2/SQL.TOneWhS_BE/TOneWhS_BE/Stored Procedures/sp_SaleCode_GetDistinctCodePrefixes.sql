9539ed986bdafa3903bb12f4aaee4da5c1961858
Wissam Ajamy <VANRISE\wissam.ajamy>
2016-08-08 14:59:39 +0000

    Update structure from TOneV2_Dev
    git-tfs-id: [http://developmentvlan:8585/tfs/vanrise.collection]$/;C34863

--- TOneV2/Code/TOneV2/SQL.TOneWhS_BE/TOneWhS_BE/Stored Procedures/sp_SaleCode_GetDistinctCodePrefixes.sql
+++ TOneV2/Code/TOneV2/SQL.TOneWhS_BE/TOneWhS_BE/Stored Procedures/sp_SaleCode_GetDistinctCodePrefixes.sql
@@ -11,13 +11,14 @@ AS
 BEGIN
 	-- SET NOCOUNT ON added to prevent extra result sets from
 	-- interfering with SELECT statements.
-	SET NOCOUNT ON;
-	SELECT DISTINCT LEFT(Code, @PrefixLength) as CodePrefix FROM TOneWhS_BE.SaleCode WITH (NOLOCK) 
-		WHERE 
+	SET NOCOUNT ON;		
+		SELECT LEFT(Code, @PrefixLength) as CodePrefix, 0 as codeCount FROM TOneWhS_BE.SaleCode WITH (NOLOCK) 
+		WHERE
 		(
 			(@IsFuture = 0 AND BED <= @EffectiveOn AND (EED > @EffectiveOn OR EED IS NULL))
 			OR
 			(@IsFuture = 1 AND (BED > GETDATE() OR EED IS NULL))
-		);
+		)
+		group by LEFT(Code, @PrefixLength);
 	SET NOCOUNT OFF;
 END
--- TOneV2/Code/TOneV2/SQL.TOneWhS_BE/TOneWhS_BE/Stored Procedures/sp_SupplierCode_GetDistinctCodePrefixes.sql
+++ TOneV2/Code/TOneV2/SQL.TOneWhS_BE/TOneWhS_BE/Stored Procedures/sp_SupplierCode_GetDistinctCodePrefixes.sql
@@ -12,12 +12,14 @@ BEGIN
 	-- SET NOCOUNT ON added to prevent extra result sets from
 	-- interfering with SELECT statements.
 	SET NOCOUNT ON;
-	SELECT DISTINCT LEFT(Code, @PrefixLength) as CodePrefix FROM TOneWhS_BE.SupplierCode WITH (NOLOCK) 
+	SELECT LEFT(Code, @PrefixLength) as CodePrefix, SUM(1) as codeCount FROM TOneWhS_BE.SupplierCode WITH (NOLOCK) 
 		WHERE
 		(
 			(@IsFuture = 0 AND BED <= @EffectiveOn AND (EED > @EffectiveOn OR EED IS NULL))
 			OR
 			(@IsFuture = 1 AND (BED > GETDATE() OR EED IS NULL))
-		);
+		)
+		group by LEFT(Code, @PrefixLength)
+		order by codeCount desc;
 	SET NOCOUNT OFF;
 END
