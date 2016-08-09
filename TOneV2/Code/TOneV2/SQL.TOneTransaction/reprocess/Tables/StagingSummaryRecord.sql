346c498e6848bb61e1cdf178ec9e5e2fd840ea23
anthony douaihy <VANRISE\anthony.douaihy>
2016-08-09 08:21:56 +0000

    Add StaggingSummaryRecord index
    git-tfs-id: [http://developmentvlan:8585/tfs/vanrise.collection]$/;C34873

--- TOneV2/Code/TOneV2/SQL.TOneTransaction/reprocess/Tables/StagingSummaryRecord.sql
+++ TOneV2/Code/TOneV2/SQL.TOneTransaction/reprocess/Tables/StagingSummaryRecord.sql
@@ -1,14 +1,16 @@
 ﻿CREATE TABLE [reprocess].[StagingSummaryRecord] (
-    [ID]                BIGINT         IDENTITY (1, 1) NOT NULL,
     [ProcessInstanceId] BIGINT         NOT NULL,
     [StageName]         NVARCHAR (255) NOT NULL,
     [BatchStart]        DATETIME       NOT NULL,
-    [Data]              VARCHAR (MAX)  NOT NULL,
-    CONSTRAINT [PK_StagingSummaryRecord] PRIMARY KEY CLUSTERED ([ID] ASC)
+    [Data]              VARCHAR (MAX)  NOT NULL
 );
 
 
+
+
 GO
-CREATE NONCLUSTERED INDEX [IX_StagingSummaryRecord_ProcessId_StageName]
+CREATE CLUSTERED INDEX [IX_StagingSummaryRecord_ProcessId_StageName]
     ON [reprocess].[StagingSummaryRecord]([ProcessInstanceId] ASC, [StageName] ASC);
+
+
 
