89dee3b1515d72ac667ceca101d4e0d90bd0e3fd
Ismail Zein <VANRISE\ismail.zein>
2016-08-09 14:11:23 +0000

    changed Runtime assembly description
    git-tfs-id: [http://developmentvlan:8585/tfs/vanrise.collection]$/;C34897

--- TOneV2/Code/TOneV2/TestRuntime/Properties/AssemblyInfo.cs
+++ TOneV2/Code/TOneV2/TestRuntime/Properties/AssemblyInfo.cs
@@ -5,11 +5,11 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("TestRuntime")]
+[assembly: AssemblyTitle("TOne.WhS.Runtime")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("TestRuntime")]
+[assembly: AssemblyProduct("TOne.WhS.Runtime")]
 [assembly: AssemblyCopyright("Copyright ©  2014")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
