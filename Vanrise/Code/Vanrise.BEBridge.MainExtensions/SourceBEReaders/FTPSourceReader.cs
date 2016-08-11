2fa0e37668f5902be7d84659cc0c9767f8c14f8c
rabih.fashwal:2 <rabih.fashwal:2@vanrise.tfs.local>
2016-08-11 14:01:03 +0000

    Add File and FTP source readers
    git-tfs-id: [http://developmentvlan:8585/tfs/vanrise.collection]$/;C34934

---
+++ Vanrise/Code/Vanrise.BEBridge.MainExtensions/SourceBEReaders/FTPSourceReader.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+using Rebex.Net;
+using Vanrise.BEBridge.Entities;
+
+namespace Vanrise.BEBridge.MainExtensions.SourceBEReaders
+{
+    public class FTPSourceReader : SourceBEReader
+    {
+        public FTPSourceReaderSetting Setting { get; set; }
+        public override void RetrieveUpdatedBEs(ISourceBEReaderRetrieveUpdatedBEsContext context)
+        {
+            var ftp = new Rebex.Net.Ftp();
+            string mask = string.IsNullOrEmpty(Setting.Mask) ? "" : Setting.Mask;
+            Regex regEx = new Regex(mask);
+
+            EstablishConnection(ftp);
+            if (ftp.GetConnectionState().Connected)
+            {
+                if (!ftp.DirectoryExists(Setting.Directory))
+                    throw new DirectoryNotFoundException();
+
+                ftp.ChangeDirectory(Setting.Directory);
+                FtpList currentItems = ftp.GetList(string.Format("*{0}", Setting.Extension));
+
+                if (currentItems.Count > 0)
+                {
+                    foreach (var fileObj in currentItems)
+                    {
+                        if (!fileObj.IsDirectory && regEx.IsMatch(fileObj.Name))
+                        {
+                            String filePath = Setting.Directory + "/" + fileObj.Name;
+                            GetFileContent(context.OnSourceBEBatchRetrieved, ftp, fileObj, filePath);
+                        }
+                    }
+                }
+                CloseConnection(ftp);
+            }
+            else
+            {
+                throw new Exception("FTP adapter could not connect to FTP Server");
+            }
+        }
+
+        void EstablishConnection(Ftp ftp)
+        {
+            ftp.Connect(Setting.ServerIP);
+            ftp.Login(Setting.UserName, Setting.Password);
+        }
+        static void CloseConnection(Ftp ftp)
+        {
+            ftp.Dispose();
+        }
+
+        void GetFileContent(Action<FileSourceBatch, SourceBEBatchRetrievedContext> fileBatchAction, Ftp ftp, FtpItem fileObj, String filePath)
+        {
+            var stream = new MemoryStream();
+            ftp.GetFile(filePath, stream);
+            byte[] content = stream.ToArray();
+            FileSourceBatch batch = new FileSourceBatch
+            {
+                FileName = fileObj.Name,
+                Content = content
+            };
+            fileBatchAction(batch, null);
+        }
+    }
+
+    public class FTPSourceReaderSetting
+    {
+        public string Extension { get; set; }
+        public string Mask { get; set; }
+        public string Directory { get; set; }
+        public string ServerIP { get; set; }
+        public string UserName { get; set; }
+        public string Password { get; set; }
+    }
+}
---
+++ Vanrise/Code/Vanrise.BEBridge.MainExtensions/SourceBEReaders/FileSourceReader.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+using Vanrise.BEBridge.Entities;
+
+namespace Vanrise.BEBridge.MainExtensions.SourceBEReaders
+{
+    public class FileSourceReader : SourceBEReader
+    {
+        public FileSourceReaderSetting Setting { get; set; }
+
+        public override void RetrieveUpdatedBEs(ISourceBEReaderRetrieveUpdatedBEsContext context)
+        {
+            FileSourceBatch fileBatch = new FileSourceBatch();
+            string mask = string.IsNullOrEmpty(Setting.Mask) ? "" : Setting.Mask;
+            Regex regEx = new Regex(mask);
+
+            if (System.IO.Directory.Exists(Setting.Directory))
+            {
+                try
+                {
+                    DirectoryInfo d = new DirectoryInfo(Setting.Directory);
+                    FileInfo[] Files = d.GetFiles("*" + Setting.Extension);
+                    foreach (FileInfo file in Files)
+                    {
+                        if (regEx.IsMatch(file.Name))
+                        {
+                            GetFileContent(context.OnSourceBEBatchRetrieved, file);
+                        }
+                    }
+                }
+                catch (Exception ex)
+                {
+                    throw ex;
+                }
+            }
+            else
+            {
+                throw new DirectoryNotFoundException(string.Format("directory {0}", Setting.Directory));
+            }
+        }
+
+        void GetFileContent(Action<FileSourceBatch, SourceBEBatchRetrievedContext> fileBatchAction, FileInfo fileInfo)
+        {
+            string fileName = string.Format("{0}/{1}", Setting.Directory, fileInfo.Name);
+            byte[] content = null;
+            using (FileStream fs = new FileStream(fileName, FileMode.Open, FileAccess.Read))
+            {
+                content = new byte[fs.Length];
+                fs.Read(content, 0, (int)fs.Length);
+            }
+            if (content == null)
+                throw new NullReferenceException(string.Format("content"));
+            FileSourceBatch batch = new FileSourceBatch
+            {
+                FileName = fileInfo.Name,
+                Content = content
+            };
+            fileBatchAction(batch, null);
+        }
+    }
+
+    public class FileSourceReaderSetting
+    {
+        public string Extension { get; set; }
+        public string Mask { get; set; }
+        public string Directory { get; set; }
+    }
+}
