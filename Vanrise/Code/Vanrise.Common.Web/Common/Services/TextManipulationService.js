61634ae33f59653016d4ee375320f7026abaf8a2
Samer Haidar <VANRISE\samer.haidar>
2016-08-11 13:10:56 +0000

    Adding TextManipulation Files
    git-tfs-id: [http://developmentvlan:8585/tfs/vanrise.collection]$/;C34933

---
+++ Vanrise/Code/Vanrise.Common.Web/Common/Services/TextManipulationService.js
@@ -0,0 +1,20 @@
+﻿
+app.service('VRCommon_TextManipulationService', ['VRModalService',
+    function (VRModalService) {
+        function editTextManipulation(onTextManipulationSave) {
+            var settings = {
+
+            };
+
+            settings.onScopeReady = function (modalScope) {
+                modalScope.onTextManipulationSave = onTextManipulationSave;
+            };
+            var parameters = {
+            };
+
+            VRModalService.showModal('/Client/Modules/Common/Views/TextManipulation/TextManipulationEditor.html', parameters, settings);
+        }
+        return ({
+            editTextManipulation: editTextManipulation
+        });
+    }]);
---
+++ Vanrise/Code/Vanrise.Common.Web/Common/Views/TextManipulation/TextManipulationEditorController.js
@@ -0,0 +1,66 @@
+﻿(function (appControllers) {
+
+    "use strict";
+
+    textManipulationEditorController.$inject = ['$scope', 'VRNotificationService', 'VRNavigationService', 'UtilsService', 'VRUIUtilsService'];
+
+    function textManipulationEditorController($scope,  VRNotificationService, VRNavigationService, UtilsService, VRUIUtilsService) {
+
+        defineScope();
+        loadParameters();
+        load();
+
+        function loadParameters() {
+            var parameters = VRNavigationService.getParameters($scope);
+            if (parameters != undefined && parameters != null) {
+            }
+        }
+        function defineScope() {
+
+            $scope.saveTextManipulation = function () {
+                return saveTextManipulation();
+            };
+
+            $scope.close = function () {
+                $scope.modalContext.closeModal();
+            };
+        }
+        function load() {
+            $scope.isLoading = true;
+            loadAllControls();
+        }
+
+        function loadAllControls() {
+            return UtilsService.waitMultipleAsyncOperations([setTitle, loadStaticData])
+               .catch(function (error) {
+                   VRNotificationService.notifyExceptionWithClose(error, $scope);
+               })
+              .finally(function () {
+                  $scope.isLoading = false;
+              });
+        }
+
+        function setTitle() {
+            $scope.title = "Text Manipulation";
+        }
+
+        function loadStaticData() {
+        }
+
+        function buildTextManipulationObjFromScope() {
+            var obj = {
+            };
+            return obj;
+        }
+
+        function saveTextManipulation() {
+            var textManipulation = buildTextManipulationObjFromScope();
+            if ($scope.onTextManipulationSave != undefined)
+                $scope.onTextManipulationSave(textManipulation);
+            $scope.modalContext.closeModal();
+        }
+
+    }
+
+    appControllers.controller('VRCommon_TextManipulationEditorController', textManipulationEditorController);
+})(appControllers);
