2b74910be83dcc3044ec63e8c071c3567e580b92
rabih.fashwal:2 <rabih.fashwal:2@vanrise.tfs.local>
2016-08-12 17:10:25 +0000

    Add BEBridge Activities
    git-tfs-id: [http://developmentvlan:8585/tfs/vanrise.collection]$/;C34984

---
+++ Vanrise/Code/Vanrise.BEBridge.BP.Activities/ConvertSourceToTargetBEs.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Activities;
+using Vanrise.BEBridge.Entities;
+using Vanrise.BusinessProcess;
+
+namespace Vanrise.BEBridge.BP.Activities
+{
+    public sealed class ConvertSourceToTargetBEs : CodeActivity
+    {
+        public InArgument<SourceBEBatch> SourceBEBatch { get; set; }
+        public InArgument<TargetBEConvertor> TargetConverter { get; set; }
+        public InOutArgument<List<ITargetBE>> TargetBEs { get; set; }
+
+
+        protected override void Execute(CodeActivityContext context)
+        {
+            TargetBEConvertorConvertSourceBEsContext targetBEConvertorContext = new TargetBEConvertorConvertSourceBEsContext();
+            TargetConverter.Get(context).ConvertSourceBEs(targetBEConvertorContext);
+            TargetBEs.Set(context, targetBEConvertorContext.TargetBEs);
+        }
+
+        private class TargetBEConvertorConvertSourceBEsContext : ITargetBEConvertorConvertSourceBEsContext
+        {
+            public SourceBEBatch SourceBEBatch
+            {
+                set;
+                get;
+            }
+
+            public List<ITargetBE> TargetBEs
+            {
+                set;
+                get;
+            }
+        }
+
+    }
+
+}
---
+++ Vanrise/Code/Vanrise.BEBridge.BP.Activities/ReadSourceBEs.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Activities;
+using Vanrise.BEBridge.Entities;
+using Vanrise.BusinessProcess;
+
+namespace Vanrise.BEBridge.BP.Activities
+{
+
+    public class ReadSourceBEsInput
+    {
+        public SourceBEReader SourceReader { get; set; }
+
+        public SourceBEBatch SourceBatch { get; set; }
+    }
+
+    public sealed class ReadSourceBEs : BaseAsyncActivity<ReadSourceBEsInput>
+    {
+        public InArgument<SourceBEReader> SourceReader { get; set; }
+
+        public InOutArgument<SourceBEBatch> SourceBatch { get; set; }
+        protected override void DoWork(ReadSourceBEsInput inputArgument, AsyncActivityHandle handle)
+        {
+            Action<SourceBEBatch, SourceBEBatchRetrievedContext> onSourceBEBatchRetrieved = (sourceBEBatch, sourceRetrievedContext) =>
+            {
+                inputArgument.SourceBatch = sourceBEBatch;
+            };
+            SourceBEReaderRetrieveUpdatedBEsContext context = new SourceBEReaderRetrieveUpdatedBEsContext(onSourceBEBatchRetrieved);
+            inputArgument.SourceReader.RetrieveUpdatedBEs(context);
+        }
+
+        protected override ReadSourceBEsInput GetInputArgument(AsyncCodeActivityContext context)
+        {
+            return new ReadSourceBEsInput
+            {
+                SourceBatch = this.SourceBatch.Get(context),
+                SourceReader = this.SourceReader.Get(context)
+            };
+        }
+
+        private class SourceBEReaderRetrieveUpdatedBEsContext : ISourceBEReaderRetrieveUpdatedBEsContext
+        {
+
+            Action<SourceBEBatch, SourceBEBatchRetrievedContext> _onSourceBEBatchRetrieved;
+
+            public SourceBEReaderRetrieveUpdatedBEsContext(Action<SourceBEBatch, SourceBEBatchRetrievedContext> onSourceBEBatchRetrieved)
+            {
+                _onSourceBEBatchRetrieved = onSourceBEBatchRetrieved;
+            }
+
+            public void OnSourceBEBatchRetrieved(SourceBEBatch sourceBEs, SourceBEBatchRetrievedContext context)
+            {
+                _onSourceBEBatchRetrieved(sourceBEs, context);
+            }
+        }
+
+    }
+
+
+}
