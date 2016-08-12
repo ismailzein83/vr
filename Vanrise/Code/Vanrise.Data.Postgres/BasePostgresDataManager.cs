4077e673736560500880edd264dc0b8b1c3d7108
rabih.fashwal:2 <rabih.fashwal:2@vanrise.tfs.local>
2016-08-12 08:00:11 +0000

    Add Postgres Data Manager
    git-tfs-id: [http://developmentvlan:8585/tfs/vanrise.collection]$/;C34950

---
+++ Vanrise/Code/Vanrise.Data.Postgres/BasePostgresDataManager.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Vanrise.Data.Postgres
+{
+    public class BasePostgresDataManager : BaseDataManager
+    {
+        #region ctor
+
+        public BasePostgresDataManager()
+            : base()
+        {
+
+        }
+
+        public BasePostgresDataManager(string connectionStringKey)
+            : base(connectionStringKey)
+        {
+
+        }
+
+        #endregion
+
+
+    }
+}
