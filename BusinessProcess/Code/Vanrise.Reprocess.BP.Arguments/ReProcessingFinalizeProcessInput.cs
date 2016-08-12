cb0185f47981fa592839684bde8c5c1808e382dc
anthony douaihy <VANRISE\anthony.douaihy>
2016-08-12 08:54:32 +0000

    new input arguments for reprocessing
    git-tfs-id: [http://developmentvlan:8585/tfs/vanrise.collection]$/;C34955

---
+++ BusinessProcess/Code/Vanrise.Reprocess.BP.Arguments/ReProcessingFinalizeProcessInput.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Vanrise.Reprocess.BP.Arguments
+{
+    public class ReProcessingFinalizeProcessInput : Vanrise.BusinessProcess.Entities.BaseProcessInputArgument
+    {
+        public int ReprocessDefinitionId { get; set; }
+
+        public string StageName { get; set; }
+
+        public DateTime BatchStart { get; set; }
+
+        public override string GetTitle()
+        {
+            return String.Format("Stage: {0}. Batch Start: {1}.", StageName, BatchStart);
+        }
+    }
+}
