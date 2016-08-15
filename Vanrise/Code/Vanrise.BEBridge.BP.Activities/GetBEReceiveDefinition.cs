ba9ef0876aec814632b9b78d4fa56a7794f74f08
rabih.fashwal:2 <rabih.fashwal:2@vanrise.tfs.local>
2016-08-15 08:29:36 +0000

    Update BEBridgeProcess
    git-tfs-id: [http://developmentvlan:8585/tfs/vanrise.collection]$/;C34989

---
+++ Vanrise/Code/Vanrise.BEBridge.BP.Activities/GetBEReceiveDefinition.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Activities;
+using Vanrise.BEBridge.Entities;
+using Vanrise.BEBridge.Business;
+
+namespace Vanrise.BEBridge.BP.Activities
+{
+    public sealed class GetBEReceiveDefinition : CodeActivity
+    {
+        [RequiredArgument]
+        public InArgument<Guid> BEReceiveDefinitionId { get; set; }
+        [RequiredArgument]
+        public OutArgument<BEReceiveDefinition> BEReceiveDefinition { get; set; }
+        protected override void Execute(CodeActivityContext context)
+        {
+            BEReceiveDefinitionManager manager = new BEReceiveDefinitionManager();
+            BEReceiveDefinition.Set(context, manager.GetBEReceiveDefinition(BEReceiveDefinitionId.Get(context)));
+        }
+    }
+}
