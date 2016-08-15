60abec40ae67d53ab4f09dc04bd9b0a00a464935
mostafa.jawhari <mostafa.jawhari@vanrise.tfs.local>
2016-08-15 10:39:43 +0000

    adding new BR zone without rates
    git-tfs-id: [http://developmentvlan:8585/tfs/vanrise.collection]$/;C34992

---
+++ TOneV2/Code/TOneV2/TOne.WhS.CodePreparation.Business/BusinessRules/Conditions/ValidateAfterProcessing/NewZoneWithNoRatesCondition.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TOne.WhS.CodePreparation.Entities;
+using TOne.WhS.CodePreparation.Entities.Processing;
+using Vanrise.BusinessProcess.Entities;
+using Vanrise.Common;
+
+namespace TOne.WhS.CodePreparation.Business
+{
+    public class NewZoneWithNoRatesCondition : BusinessRuleCondition
+    {
+
+        public override bool ShouldValidate(IRuleTarget target)
+        {
+            return (target as ZoneToProcess != null);
+        }
+
+        public override bool Validate(IBusinessRuleConditionValidateContext context)
+        {
+            ZoneToProcess zoneTopProcess = context.Target as ZoneToProcess;
+
+            return zoneTopProcess.RatesToAdd.Count() > 0;
+        }
+
+        public override string GetMessage(IRuleTarget target)
+        {
+            return string.Format("A rate plan must be done for zone {0}", (target as ZoneToProcess).ZoneName);
+        }
+
+    }
+}
