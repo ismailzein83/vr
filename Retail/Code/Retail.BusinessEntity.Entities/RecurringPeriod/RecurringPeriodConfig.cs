6c3ca6915b1373a205e302c74bb9e3099ab47799
Zeinab Mokdad <VANRISE\zeinab.mokdad>
2016-08-15 14:56:06 +0000

    adding config for extension
    git-tfs-id: [http://developmentvlan:8585/tfs/vanrise.collection]$/;C35001

---
+++ Retail/Code/Retail.BusinessEntity.Entities/RecurringPeriod/RecurringPeriodConfig.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Vanrise.Entities;
+
+namespace Retail.BusinessEntity.Entities.RecurringPeriod
+{
+    public class RecurringPeriodConfig : ExtensionConfiguration
+    {
+        public const string EXTENSION_TYPE = "Retail_BE_RecurringPeriodDefinition";
+        public string Editor { get; set; }
+    }
+}
