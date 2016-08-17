c91bfed613a031c21b5497b5176db0af1cb18ff0
Wissam Ajamy <VANRISE\wissam.ajamy>
2016-08-17 14:31:43 +0000

    Add security permissions
    git-tfs-id: [http://developmentvlan:8585/tfs/vanrise.collection]$/;C35067

--- TOneV2/Code/TOneV2/SQL.TOneConfiguration/Reprocess.PostDeployment.sql
+++ TOneV2/Code/TOneV2/SQL.TOneConfiguration/Reprocess.PostDeployment.sql
@@ -16,7 +16,7 @@ set identity_insert [sec].[View] on;
 ;with cte_data([Id],[Name],[Title],[Url],[Module],[ActionNames],[Audience],[Content],[Settings],[Type],[Rank])
 as (select * from (values
 --//////////////////////////////////////////////////////////////////////////////////////////////////
-(19001,'Reprocess Definition','Reprocess Definition','#/view/Reprocess/Views/ReprocessDefinition/ReprocessDefinitionManagement',-100,null,null,null,null,0,30)
+(19001,'Reprocess Definition','Reprocess Definition','#/view/Reprocess/Views/ReprocessDefinition/ReprocessDefinitionManagement',-100,'Reprocess/ReprocessDefinition/GetFilteredReprocessDefinitions',null,null,null,0,30)
 --\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\
 )c([Id],[Name],[Title],[Url],[Module],[ActionNames],[Audience],[Content],[Settings],[Type],[Rank]))
 merge	[sec].[View] as t
@@ -30,3 +30,45 @@ when not matched by target then
 	values(s.[Id],s.[Name],s.[Title],s.[Url],s.[Module],s.[ActionNames],s.[Audience],s.[Content],s.[Settings],s.[Type],s.[Rank]);
 set identity_insert [sec].[View] off;
 
+--[sec].[SystemAction]------------------------------------------------------------------------------
+----------------------------------------------------------------------------------------------------
+;with cte_data([Name],[RequiredPermissions])
+as (select * from (values
+--//////////////////////////////////////////////////////////////////////////////////////////////////
+('Reprocess/ReprocessDefinition/GetFilteredReprocessDefinitions','Reprocess_ReprocessDefinition: View'),
+('Reprocess/ReprocessDefinition/GetReprocessDefinition',null),
+('Reprocess/ReprocessDefinition/AddReprocessDefinition','Reprocess_ReprocessDefinition: Add'),
+('Reprocess/ReprocessDefinition/UpdateReprocessDefinition','Reprocess_ReprocessDefinition: Edit'),
+('Reprocess/ReprocessDefinition/GetReprocessDefinitionsInfo',null)
+--\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\
+)c([Name],[RequiredPermissions]))
+merge	[sec].[SystemAction] as t
+using	cte_data as s
+on		1=1 and t.[Name] = s.[Name]
+when matched then
+	update set
+	[Name] = s.[Name],[RequiredPermissions] = s.[RequiredPermissions]
+when not matched by target then
+	insert([Name],[RequiredPermissions])
+	values(s.[Name],s.[RequiredPermissions]);
+
+--[sec].[BusinessEntity]------------------4801 to 5100----------------------------------------------------------
+------------------------------------------------------------------------------------------------------------
+set nocount on;
+set identity_insert [sec].[BusinessEntity] on;
+;with cte_data([Id],[Name],[Title],[ModuleId],[BreakInheritance],[PermissionOptions])
+as (select * from (values
+--//////////////////////////////////////////////////////////////////////////////////////////////////
+(4801,'Reprocess_ReprocessDefinition','Reprocess Definition',601,0,'["View","Add","Edit"]')
+--\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\
+)c([Id],[Name],[Title],[ModuleId],[BreakInheritance],[PermissionOptions]))
+merge	[sec].[BusinessEntity] as t
+using	cte_data as s
+on		1=1 and t.[Id] = s.[Id]
+when matched then
+	update set
+	[Name] = s.[Name],[Title] = s.[Title],[ModuleId] = s.[ModuleId],[BreakInheritance] = s.[BreakInheritance],[PermissionOptions] = s.[PermissionOptions]
+when not matched by target then
+	insert([Id],[Name],[Title],[ModuleId],[BreakInheritance],[PermissionOptions])
+	values(s.[Id],s.[Name],s.[Title],s.[ModuleId],s.[BreakInheritance],s.[PermissionOptions]);
+set identity_insert [sec].[BusinessEntity] off;
