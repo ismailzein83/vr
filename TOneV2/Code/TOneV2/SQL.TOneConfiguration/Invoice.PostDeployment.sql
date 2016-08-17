a184017d49ce9aca18e8a9016319c33a8a396b49
Wissam Ajamy <VANRISE\wissam.ajamy>
2016-08-17 15:13:54 +0000

    Add Invoices module file
    git-tfs-id: [http://developmentvlan:8585/tfs/vanrise.collection]$/;C35072

---
+++ TOneV2/Code/TOneV2/SQL.TOneConfiguration/Invoice.PostDeployment.sql
@@ -0,0 +1,29 @@
+﻿/*
+Post-Deployment Script Template							
+--------------------------------------------------------------------------------------
+ This file contains SQL statements that will be appended to the build script.		
+ Use SQLCMD syntax to include a file in the post-deployment script.			
+ Example:      :r .\myfile.sql								
+ Use SQLCMD syntax to reference a variable in the post-deployment script.		
+ Example:      :setvar TableName MyTable							
+               SELECT * FROM [$(TableName)]					
+--------------------------------------------------------------------------------------
+*/
+--[sec].[viewtype]---------------------------------401 to 500-----------------------------------------
+----------------------------------------------------------------------------------------------------
+set nocount on;
+;with cte_data([ID],[Name],[Title],[Details])
+as (select * from (values
+--//////////////////////////////////////////////////////////////////////////////////////////////////
+(401,'VR_Invoice_GenericInvoice','Invoice','{"ViewTypeId":401,"Name":"VR_Invoice_GenericInvoice"}')
+--\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\
+)c([ID],[Name],[Title],[Details]))
+merge	[sec].[viewtype] as t
+using	cte_data as s
+on		1=1 and t.[ID] = s.[ID]
+when matched then
+	update set
+	[Name] = s.[Name],[Title] = s.[Title],[Details] = s.[Details]
+when not matched by target then
+	insert([ID],[Name],[Title],[Details])
+	values(s.[ID],s.[Name],s.[Title],s.[Details]);
