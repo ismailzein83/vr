6a91509ffd1f96c501dc9c1a7851952484073ad3
mostafa.jawhari <mostafa.jawhari@vanrise.tfs.local>
2016-08-18 11:41:34 +0000

    added new classes NotImportedZone and NotImportedCode
    git-tfs-id: [http://developmentvlan:8585/tfs/vanrise.collection]$/;C35112

---
+++ TOneV2/Code/TOneV2/TOne.WhS.CodePreparation.Entities/PreviewData/NotImportedZone.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TOne.WhS.CodePreparation.Entities.Processing;
+using Vanrise.BusinessProcess.Entities;
+
+namespace TOne.WhS.CodePreparation.Entities
+{
+    public class NotImportedZone : IRuleTarget
+    {
+        public string ZoneName { get; set; }
+
+        public int CountryId { get; set; }
+
+        public DateTime BED { get; set; }
+
+        public DateTime? EED { get; set; }
+
+        public ExistingRate ExistingRate { get; set; }
+
+        public bool HasChanged { get; set; }
+
+        public object Key
+        {
+            get { return this.ZoneName; }
+        }
+
+        public string TargetType
+        {
+            get { return "Zone"; }
+        }
+    }
+}
