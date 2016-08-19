5d31671008bce31c0d2bccc315516391b3beb0ef
Wissam Ajamy <VANRISE\wissam.ajamy>
2016-08-19 08:19:46 +0000

    update error message to display all
    git-tfs-id: [http://developmentvlan:8585/tfs/vanrise.collection]$/;C35139

--- TOneV2/Code/TOneV2/TOne.WhS.DBSync.Business/SwitchMigration/SwitchMappingRulesMigrator.cs
+++ TOneV2/Code/TOneV2/TOne.WhS.DBSync.Business/SwitchMigration/SwitchMappingRulesMigrator.cs
@@ -141,7 +141,7 @@ namespace TOne.WhS.DBSync.Business.SwitchMigration
             }
             catch (Exception exc)
             {
-                Logger.InfoMessage.AppendLine(exc.StackTrace.ToString());
+                Logger.InfoMessage.AppendLine(exc.ToString());
             }
         }
         private MappingRule GetRule(string carrierId, StaticValues trunk, int currentSwitchId, DateTime date, int carrierType)
