467d2ebf2ff0faa6dae4dfc7046dfc27b358d84f
anthony douaihy <VANRISE\anthony.douaihy>
2016-08-19 09:34:31 +0000

    New input argument for build customer zone details BP
    git-tfs-id: [http://developmentvlan:8585/tfs/vanrise.collection]$/;C35153

---
+++ TOneV2/Code/TOneV2/TOne.WhS.Routing.BP.Arguments/BuildCustomerZoneDetailsInput.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using TOne.WhS.BusinessEntity.Entities;
+using Vanrise.BusinessProcess.Entities;
+
+namespace TOne.WhS.Routing.BP.Arguments
+{
+    public class BuildCustomerZoneDetailsInput : BaseProcessInputArgument
+    {
+        public List<RoutingCustomerInfo> CustomerInfos { get; set; }
+
+        public int RoutingDatabaseId { get; set; }
+
+        public DateTime? EffectiveOn { get; set; }
+
+        public bool IsFuture { get; set; }
+
+        public int MinCustomerId { get; set; }
+
+        public int MaxCustomerId { get; set; }
+
+        public override string GetTitle()
+        {
+            return string.Format("#BPDefinitionTitle# By Ids: From {0} to {1}.", MinCustomerId, MaxCustomerId);
+        }
+    }
+}
