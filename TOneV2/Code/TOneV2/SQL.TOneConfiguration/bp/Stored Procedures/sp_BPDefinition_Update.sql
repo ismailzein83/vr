4fd7c6457afeb5addd21eb48838de033bdb09f9d
Wissam Ajamy <VANRISE\wissam.ajamy>
2016-08-23 08:19:14 +0000

    update structure from TOneConfiguration
    git-tfs-id: [http://developmentvlan:8585/tfs/vanrise.collection]$/;C35223

---
+++ TOneV2/Code/TOneV2/SQL.TOneConfiguration/bp/Stored Procedures/sp_BPDefinition_Update.sql
@@ -0,0 +1,16 @@
+﻿
+Create PROCEDURE  [bp].[sp_BPDefinition_Update]
+@BPDefinitionId int,
+@Title nvarchar(255),
+@Config nvarchar(MAX)
+AS
+BEGIN
+  	IF NOT EXISTS(Select Title from [bp].[BPDefinition] WITH(NOLOCK) where Title = @Title And ID != @BPDefinitionId)
+	BEGIN
+		Update [bp].[BPDefinition] set
+		Title = @Title,
+		Config = @Config
+		where ID = @BPDefinitionId
+	END
+
+END
