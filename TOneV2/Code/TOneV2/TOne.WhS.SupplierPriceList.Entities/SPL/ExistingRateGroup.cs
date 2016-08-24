a388752cb6ca0dacb5dbc51bcfee71feba58b560
mostafa.jawhari <mostafa.jawhari@vanrise.tfs.local>
2016-08-24 09:59:47 +0000

    adding needed files to implement OtherRates
    git-tfs-id: [http://developmentvlan:8585/tfs/vanrise.collection]$/;C35258

---
+++ TOneV2/Code/TOneV2/TOne.WhS.SupplierPriceList.Entities/SPL/ExistingRateGroup.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using TOne.WhS.BusinessEntity.Entities;
+
+namespace TOne.WhS.SupplierPriceList.Entities.SPL
+{
+    public class ExistingRateGroup 
+    {
+        public ExistingRateGroup()
+        {
+            NormalRates = new List<ExistingRate>();
+            OtherRates = new Dictionary<int, List<ExistingRate>>();
+        }
+        public string ZoneName { get; set; }
+        public List<ExistingRate> NormalRates { get; set; }
+        public Dictionary<int,List<ExistingRate>> OtherRates { get; set; }
+
+    }
+
+}
---
+++ TOneV2/Code/TOneV2/TOne.WhS.SupplierPriceList.Entities/SPL/ImportedData/ImportedDataByZone.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Vanrise.BusinessProcess.Entities;
+
+namespace TOne.WhS.SupplierPriceList.Entities.SPL
+{
+    public class ImportedDataByZone : IRuleTarget
+    {
+        public ImportedDataByZone()
+        {
+            this.ImportedCodes = new List<ImportedCode>();
+            this.ImportedRates = new List<ImportedRate>();
+        }
+
+        public string ZoneName { get; set; }
+
+        public List<ImportedCode> ImportedCodes { get; set; }
+
+        public List<ImportedRate> ImportedRates { get; set; }
+
+
+        #region IRuleTarget Implementation
+
+        public object Key
+        {
+            get { return this.ZoneName; }
+        }
+
+        #endregion
+
+
+        public string TargetType
+        {
+            get { return "Zone"; }
+        }
+    }
+
+}
