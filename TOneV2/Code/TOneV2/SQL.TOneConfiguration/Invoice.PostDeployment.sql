b33da9018fb340224a80c4a0c05a0dc8d7fe5784
Wissam Ajamy <VANRISE\wissam.ajamy>
2016-08-25 13:10:58 +0000

    Add View Invoice Types
    git-tfs-id: [http://developmentvlan:8585/tfs/vanrise.collection]$/;C35315

--- TOneV2/Code/TOneV2/SQL.TOneConfiguration/Invoice.PostDeployment.sql
+++ TOneV2/Code/TOneV2/SQL.TOneConfiguration/Invoice.PostDeployment.sql
@@ -27,3 +27,24 @@ when matched then
 when not matched by target then
 	insert([ID],[Name],[Title],[Details])
 	values(s.[ID],s.[Name],s.[Title],s.[Details]);
+
+--[sec].[View]-----------------------------20001 to 21000--------------------------------------------------------
+---------------------------------------------------------------------------------------------------------------
+set nocount on;
+set identity_insert [sec].[View] on;
+;with cte_data([Id],[Name],[Title],[Url],[Module],[ActionNames],[Audience],[Content],[Settings],[Type],[Rank])
+as (select * from (values
+--//////////////////////////////////////////////////////////////////////////////////////////////////
+(20001,'Invoice Types','Invoice Types','#/view/VR_Invoice/Views/InvoiceTypeManagement',-100,null,null,null,null,0,50)
+--\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\
+)c([Id],[Name],[Title],[Url],[Module],[ActionNames],[Audience],[Content],[Settings],[Type],[Rank]))
+merge	[sec].[View] as t
+using	cte_data as s
+on		1=1 and t.[Id] = s.[Id]
+when matched then
+	update set
+	[Name] = s.[Name],[Title] = s.[Title],[Url] = s.[Url],[Module] = s.[Module],[ActionNames] = s.[ActionNames],[Audience] = s.[Audience],[Content] = s.[Content],[Settings] = s.[Settings],[Type] = s.[Type],[Rank] = s.[Rank]
+when not matched by target then
+	insert([Id],[Name],[Title],[Url],[Module],[ActionNames],[Audience],[Content],[Settings],[Type],[Rank])
+	values(s.[Id],s.[Name],s.[Title],s.[Url],s.[Module],s.[ActionNames],s.[Audience],s.[Content],s.[Settings],s.[Type],s.[Rank]);
+set identity_insert [sec].[View] off;
