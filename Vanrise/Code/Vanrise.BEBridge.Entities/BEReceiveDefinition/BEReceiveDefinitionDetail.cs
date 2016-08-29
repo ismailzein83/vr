de56e094de3a0bebbb35b3e69488f3ba56fe44b7
Zeinab Mokdad <VANRISE\zeinab.mokdad>
2016-08-29 14:55:32 +0000

    adding classes for ReceiveDefinition's page
    git-tfs-id: [http://developmentvlan:8585/tfs/vanrise.collection]$/;C35388

---
+++ Vanrise/Code/Vanrise.BEBridge.Entities/BEReceiveDefinition/BEReceiveDefinitionDetail.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Vanrise.BEBridge.Entities
+{
+    public class BEReceiveDefinitionDetail
+    {
+        public BEReceiveDefinition Entity { get; set; }
+        public string Description { get; set; }
+    }
+}
