3b77f1d7ff66ad7988cdfc68de61f5a20dd4171d
Zeinab Mokdad <VANRISE\zeinab.mokdad>
2016-08-30 14:55:47 +0000

    adding new extension (convertor and synchronizer)
    git-tfs-id: [http://developmentvlan:8585/tfs/vanrise.collection]$/;C35426

---
+++ Retail/Code/Retail.BusinessEntity.Web/Retail_BusinessEntity/Directives/MainExtensions/TargetSynchronizer/retail-be-targetsynchronizer-directive.js
@@ -0,0 +1,42 @@
+﻿'use strict';
+
+app.directive('retailBeTargetsynchronizerDirective', ['VRNotificationService',
+    function (vrNotificationService) {
+        return {
+            restrict: 'E',
+            scope: {
+                onReady: '=',
+            },
+            controller: function ($scope, $element, $attrs) {
+                var ctrl = this;
+                var targetAnalysis = new targetAnalysisDirective($scope, ctrl, $attrs);
+                targetAnalysis.initializeController();
+            },
+            controllerAs: 'ctrl',
+            bindToController: true,
+            templateUrl: '/Client/Modules/Retail_BusinessEntity/Directives/MainExtensions/TargetSynchronizer/Templates/TargetSynchronizerTemplate.html'
+        };
+
+        function targetAnalysisDirective($scope, ctrl, $attrs) {
+            this.initializeController = initializeController;
+
+            function initializeController() {
+                $scope.scopeModel = {};
+                defineAPI();
+            }
+            function defineAPI() {
+                var api = {};
+                api.load = function (payload) {
+
+                };
+                api.getData = function () {
+                    return {
+                        $type: "Vanrise.BEBridge.Entities.TargetBESynchronizer, Vanrise.BEBridge.Entities"
+
+                    };
+                };
+                if (ctrl.onReady != null)
+                    ctrl.onReady(api);
+            }
+        }
+    }]);
---
+++ Retail/Code/Retail.BusinessEntity.Web/Retail_BusinessEntity/Directives/RingoExtensions/TargetConvertor/retail-be-targetconvertor-directive.js
@@ -0,0 +1,42 @@
+﻿'use strict';
+
+app.directive('retailBeTargetconvertorDirective', ['VRNotificationService',
+    function (vrNotificationService) {
+        return {
+            restrict: 'E',
+            scope: {
+                onReady: '=',
+            },
+            controller: function ($scope, $element, $attrs) {
+                var ctrl = this;
+                var targetConvertor = new targetConvertorDirective($scope, ctrl, $attrs);
+                targetConvertor.initializeController();
+            },
+            controllerAs: 'ctrl',
+            bindToController: true,
+            templateUrl: '/Client/Modules/Retail_BusinessEntity/Directives/RingoExtensions/TargetConvertor/Templates/TargetConvertorTemplate.html'
+        };
+
+        function targetConvertorDirective($scope, ctrl, $attrs) {
+            this.initializeController = initializeController;
+
+            function initializeController() {
+                $scope.scopeModel = {};
+                defineAPI();
+            }
+            function defineAPI() {
+                var api = {};
+                api.load = function (payload) {
+
+                };
+                api.getData = function () {
+                    return {
+                        $type: "Vanrise.BEBridge.Entities.TargetBEConvertor, Vanrise.BEBridge.Entities"
+
+                    };
+                };
+                if (ctrl.onReady != null)
+                    ctrl.onReady(api);
+            }
+        }
+    }]);
