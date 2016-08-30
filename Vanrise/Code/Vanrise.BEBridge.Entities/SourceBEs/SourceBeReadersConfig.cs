b15ecf8bd4b5fb41316c6fb649992af4828646c2
Zeinab Mokdad <VANRISE\zeinab.mokdad>
2016-08-30 12:26:56 +0000

    adding config classes
    git-tfs-id: [http://developmentvlan:8585/tfs/vanrise.collection]$/;C35418

---
+++ Vanrise/Code/Vanrise.BEBridge.Entities/SourceBEs/SourceBeReadersConfig.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Vanrise.Entities;
+
+namespace Vanrise.BEBridge.Entities
+{
+    public class SourceBeReadersConfig : ExtensionConfiguration
+    {
+        public const string EXTENSION_TYPE = "VR_BEBridge_SourceBeReadersDefinition";
+        public string Editor { get; set; }
+    }
+}
---
+++ Vanrise/Code/Vanrise.BEBridge.Entities/TargetBEConvertor/TargetBEConvertorConfig.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Vanrise.Entities;
+
+namespace Vanrise.BEBridge.Entities
+{
+    public class TargetBEConvertorConfig : ExtensionConfiguration
+    {
+        public const string EXTENSION_TYPE = "VR_BEBridge_TargetBEConvertorDefinition";
+        public string Editor { get; set; }
+    }
+}
