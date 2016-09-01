fa448a529ab91ec3a5e6b803aed8e5fd9d3bfc27
Wissam Ajamy <VANRISE\wissam.ajamy>
2016-09-01 08:49:45 +0000

    Update structure from TOneV2_Dev
    git-tfs-id: [http://developmentvlan:8585/tfs/vanrise.collection]$/;C35487

--- TOneV2/Code/TOneV2/SQL.TOneWhS_BE/TOneWhS_SPL/Stored Procedures/sp_SupplierZoneRate_Preview_GetFiltered.sql
+++ TOneV2/Code/TOneV2/SQL.TOneWhS_BE/TOneWhS_SPL/Stored Procedures/sp_SupplierZoneRate_Preview_GetFiltered.sql
@@ -24,13 +24,11 @@ BEGIN
 	
 	from	TOneWhS_SPL.SupplierZoneRate_Preview as Z  with(nolock)
 			join TOneWhS_SPL.SupplierCode_Preview as C  with(nolock) on Z.ZoneName = C.ZoneName OR Z.ZoneName = C.RecentZoneName
-			left outer join TOneWhS_SPL.SupplierOtherRate_Preview Orp  on Orp.ZoneName = Z.ZoneName
-	
+			 	
 	Where	Z.ProcessInstanceID=@ProcessInstanceID 
 			AND c.ProcessInstanceID=@ProcessInstanceID
-			AND Orp.ProcessInstanceID=@ProcessInstanceID
 			AND (@CountryID is null or Z.CountryID = @CountryID) 
-			AND (@OnlyModified = 0 or C.ChangeType != 0 or z.RateChangeType != 0 or Orp.RateChangeType != 0)			
+			AND (@OnlyModified = 0 or C.ChangeType != 0 or z.RateChangeType != 0 or Exists (Select 1 from TOneWhS_SPL.SupplierOtherRate_Preview as Orp with(nolock) Where Orp.ProcessInstanceID =@ProcessInstanceID  and Orp.ZoneName=z.ZoneName and Orp.RateChangeType != 0))			
 
 	Group by Z.ZoneName,Z.RecentZoneName, Z.ZoneChangeType, Z.ZoneBED, Z.ZoneEED, Z.SystemRate, Z.SystemRateBED, Z.SystemRateEED,Z.ImportedRate, Z.ImportedRateBED, Z.RateChangeType
 		
