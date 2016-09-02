a4e51d0ba4ab978d984bb24066e81718be23a7b9
mohamad.elhariri <mohamad.elhariri@vanrise.tfs.local>
2016-09-02 08:19:57 +0000

    Added SaleEntityService
    git-tfs-id: [http://developmentvlan:8585/tfs/vanrise.collection]$/;C35526

---
+++ TOneV2/Code/TOneV2/TOne.WhS.BusinessEntity.Entities/SaleZone/SaleEntityService.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TOne.WhS.BusinessEntity.Entities
+{
+    public class SaleEntityService
+    {
+        public int SaleEntityServiceId { get; set; }
+        public int PriceListId { get; set; }
+        public long? ZoneId { get; set; }
+        public List<ZoneService> Services { get; set; }
+        public DateTime BED { get; set; }
+        public DateTime? EED { get; set; }
+    }
+
+    public class ZoneService
+    {
+        public int ServiceId { get; set; }
+    }
+}
