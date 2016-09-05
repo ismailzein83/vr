ee8ad68310f78d1835b28da5ae30fc7d90afe86a
mostafa.jawhari <mostafa.jawhari@vanrise.tfs.local>
2016-09-05 09:15:10 +0000

    adding SupplierZoneService Entity
    git-tfs-id: [http://developmentvlan:8585/tfs/vanrise.collection]$/;C35551

---
+++ TOneV2/Code/TOneV2/TOne.WhS.BusinessEntity.Entities/SupplierZone/SupplierZoneService.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace TOne.WhS.BusinessEntity.Entities
+{
+    public class SupplierZoneService
+    {
+        public long SupplierZoneServiceId { get; set; }
+
+        public long ZoneId { get; set; }
+
+        public List<ZoneService> ReceivedServices { get; set; }
+
+        public List<ZoneService> EffectiveServices { get; set; }
+
+        public DateTime BED { get; set; }
+
+        public DateTime? EED { get; set; }
+
+    }
+}
