f179cf3eee682faf6939ab3be224fe21f3b60ba2
anthony douaihy <VANRISE\anthony.douaihy>
2016-09-05 09:06:27 +0000

    Ajdust SP in QueueItem
    git-tfs-id: [http://developmentvlan:8585/tfs/vanrise.collection]$/;C35550

--- TOneV2/Code/TOneV2/SQL.TOneQueueing/queue/Stored Procedures/sp_QueueItem_GetInfo.sql
+++ TOneV2/Code/TOneV2/SQL.TOneQueueing/queue/Stored Procedures/sp_QueueItem_GetInfo.sql
@@ -1,8 +1,10 @@
 ﻿
 CREATE PROCEDURE [queue].[sp_QueueItem_GetInfo]	
+	@MaxNbOfRows INT
 AS
 BEGIN
-	SELECT ID, QueueID, ActivatorID
+	SELECT TOP (@MaxNbOfRows) ID, ExecutionFlowTriggerItemID, QueueID, ActivatorID
   FROM [queue].[QueueItem] WITH(NOLOCK)
   WHERE IsNULL(IsSuspended, 0) = 0
+   order by [ExecutionFlowTriggerItemID], ID
 END
