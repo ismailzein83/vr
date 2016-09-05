cecf18a042d5c0c2927608d07a399afa035ee265
Ismail Zein <VANRISE\ismail.zein>
2016-09-05 09:31:51 +0000

    added VolumeCommitment
    git-tfs-id: [http://developmentvlan:8585/tfs/vanrise.collection]$/;C35554

---
+++ TOneV2/Code/TOneV2/TOne.WhS.BusinessEntity.Entities/Deals/VolumeCommitment.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TOne.WhS.BusinessEntity.Entities
+{
+    public enum VolumeCommitmentType { Buy = 0, Sell = 1 }
+    public class VolumeCommitment
+    {
+        public int VolumeCommitmentId { get; set; }
+
+        public VolumeCommitmentSettings Settings { get; set; }
+    }
+
+    public class VolumeCommitmentSettings
+    {
+        public VolumeCommitmentType Type { get; set; }
+        
+        public int CarrierAccountId { get; set; }
+
+        public DateTime BeginDate { get; set; }
+
+        public DateTime? EndDate { get; set; }
+
+        public int GracePeriod { get; set; }
+
+        public string Description { get; set; }
+
+        public bool IsActive { get; set; }
+
+        public List<VolumeCommitmentItem> Items { get; set; }
+    }
+
+    public class VolumeCommitmentItem
+    {
+        public string Name { get; set; }
+
+        public List<long> ZoneIds { get; set; }
+
+        public List<VolumeCommitmentItemRate> Rates { get; set; }
+    }
+
+    public class VolumeCommitmentItemRate
+    {
+        public int UpToVolume { get; set; }
+
+        public Decimal Rate { get; set; }
+
+        public bool IsRetroActive { get; set; }
+    }
+}
