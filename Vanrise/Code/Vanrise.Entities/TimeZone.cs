1c2af6379f96f8eed8e9d38dd5e9c38c9333d432
Ismail Zein <VANRISE\ismail.zein>
2016-09-08 08:17:53 +0000

    added TimeZone
    git-tfs-id: [http://developmentvlan:8585/tfs/vanrise.collection]$/;C35634

---
+++ Vanrise/Code/Vanrise.Entities/TimeZone.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Vanrise.Entities
+{
+    public class TimeZone
+    {
+        public int TimeZoneId { get; set; }
+    }
+
+    public class TimeZoneSettings
+    {
+        public string Title { get; set; }
+
+        public TimeSpan Offset { get; set; }
+    }
+}
