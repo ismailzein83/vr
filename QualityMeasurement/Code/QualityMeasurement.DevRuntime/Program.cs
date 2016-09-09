2e7b8cc3d90c48e42e1a9d4eb5e5aaa0ab612fa8
Wissam Ajamy <VANRISE\wissam.ajamy>
2016-09-09 14:27:24 +0000

    Add cachingDistributorRuntimeService service
    git-tfs-id: [http://developmentvlan:8585/tfs/vanrise.collection]$/;C35707

--- QualityMeasurement/Code/QualityMeasurement.DevRuntime/Program.cs
+++ QualityMeasurement/Code/QualityMeasurement.DevRuntime/Program.cs
@@ -17,10 +17,12 @@ namespace QM.Runtime
             {
                 
                 SchedulerService schedulerService = new SchedulerService() { Interval = new TimeSpan(0, 0, 5) };
-
+                Vanrise.Caching.Runtime.CachingDistributorRuntimeService cachingDistributorRuntimeService = new Vanrise.Caching.Runtime.CachingDistributorRuntimeService { Interval = new TimeSpan(0, 0, 2) };
+                
                 var runtimeServices = new List<RuntimeService>();
 
                 runtimeServices.Add(schedulerService);
+                runtimeServices.Add(cachingDistributorRuntimeService);
 
                 RuntimeHost host = new RuntimeHost(runtimeServices);
                 host.Start();
