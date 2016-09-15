2e22c2932723c940e82d31ce597d18d77e513042
Wissam Ajamy <VANRISE\wissam.ajamy>
2016-09-15 14:46:24 +0000

    Update structure from TOneV2_Dev
    git-tfs-id: [http://developmentvlan:8585/tfs/vanrise.collection]$/;C35753

---
+++ TOneV2/Code/TOneV2/SQL.TOneWhS_BE/TOneWhS_BE/Stored Procedures/sp_SupplierZoneService_GetFiltered.sql
@@ -0,0 +1,31 @@
+﻿CREATE PROCEDURE [TOneWhS_BE].[sp_SupplierZoneService_GetFiltered]	
+	@SupplierID int ,
+	@ZonesIDs varchar(max),
+	@EffectiveOn dateTime = null
+AS
+BEGIN
+	-- SET NOCOUNT ON added to prevent extra result sets from
+	-- interfering with SELECT statements.
+	SET NOCOUNT ON;
+	
+	    BEGIN
+	    DECLARE @ZonesIDsTable TABLE (ZoneID int)
+		INSERT INTO @ZonesIDsTable (ZoneID)
+		select Convert(int, ParsedString) from [TOneWhS_BE].[ParseStringList](@ZonesIDs)
+			SELECT  supzs.ID,
+					supzs.ZoneID,
+					supzs.ReceivedServicesFlag,
+					supzs.EffectiveServiceFlag,
+					supzs.BED,
+					supzs.EED
+			FROM [TOneWhS_BE].SupplierZoneService supzs WITH(NOLOCK) 
+			inner join  [TOneWhS_BE].SupplierZone sz WITH(NOLOCK) on  supzs.ZoneID = sz.ID   
+            WHERE 
+                (@EffectiveOn is null or supzs.BED < = @EffectiveOn)
+            and (@EffectiveOn is null or supzs.EED is null or supzs.EED  > @EffectiveOn)
+            and (@SupplierID is null or @SupplierID = sz.SupplierID)
+            and (@ZonesIDs  is null or supzs.ZoneID in (select ZoneID from @ZonesIDsTable))            
+            and (supzs.BED <>supzs.EED  or  supzs.EED is null)
+		END
+	SET NOCOUNT OFF
+END
