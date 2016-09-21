8a3d7bfc8d4a9b078d8a68fa2a17b98dbc573e82
Samer Haidar <VANRISE\samer.haidar>
2016-09-21 11:07:34 +0000

    adding new script
    git-tfs-id: [http://developmentvlan:8585/tfs/vanrise.collection]$/;C35865

---
+++ TOneV2/Code/TOneV2/SQL.TOneConfiguration/VR_BEBridge.PostDeployment.sql
@@ -0,0 +1,12 @@
+﻿/*
+Post-Deployment Script Template							
+--------------------------------------------------------------------------------------
+ This file contains SQL statements that will be appended to the build script.		
+ Use SQLCMD syntax to include a file in the post-deployment script.			
+ Example:      :r .\myfile.sql								
+ Use SQLCMD syntax to reference a variable in the post-deployment script.		
+ Example:      :setvar TableName MyTable							
+               SELECT * FROM [$(TableName)]					
+--------------------------------------------------------------------------------------
+*/
+--common.ExtensionConfiguration---------------------------------------------------------------------beginset nocount on;;with cte_data([ID],[Name],[Title],[ConfigType],[Settings])as (select * from (values--//////////////////////////////////////////////////////////////////////////////////////////////////('DF73CDCE-01AB-4B55-9511-3A56B495A0D0','FTP Source Reader','FTP Source Reader','VR_BEBridge_SourceBeReaders','{"Editor":"vr-bebridge-sourcebereaders-ftp-directive"}'),('3BECA9AA-8F75-447B-82FE-F50DDB183730','File Source Reader','File Source Reader','VR_BEBridge_SourceBeReaders','{"Editor":"vr-bebridge-sourcebereaders-file-directive"}')--\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\)c([ID],[Name],[Title],[ConfigType],[Settings]))merge	[common].[ExtensionConfiguration] as tusing	cte_data as son		1=1 and t.[ID] = s.[ID]when matched then	update set	[Name] = s.[Name],[Title] = s.[Title],[ConfigType] = s.[ConfigType],[Settings] = s.[Settings]when not matched by target then	insert([ID],[Name],[Title],[ConfigType],[Settings])	values(s.[ID],s.[Name],s.[Title],s.[ConfigType],s.[Settings]);----------------------------------------------------------------------------------------------------end
