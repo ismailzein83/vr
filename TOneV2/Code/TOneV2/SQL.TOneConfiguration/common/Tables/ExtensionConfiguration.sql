733495499914087fac403897eac931b6809cec18
Wissam Ajamy <VANRISE\wissam.ajamy>
2016-09-21 06:52:07 +0000

    Update Structure from TOneConfiguration
    git-tfs-id: [http://developmentvlan:8585/tfs/vanrise.collection]$/;C35846

--- TOneV2/Code/TOneV2/SQL.TOneConfiguration/common/Tables/ExtensionConfiguration.sql
+++ TOneV2/Code/TOneV2/SQL.TOneConfiguration/common/Tables/ExtensionConfiguration.sql
@@ -1,14 +1,15 @@
 ﻿CREATE TABLE [common].[ExtensionConfiguration] (
-    [ID]          UNIQUEIDENTIFIER NULL,
-    [OldID]       INT              NOT NULL,
+    [ID]          UNIQUEIDENTIFIER NOT NULL,
     [Name]        NVARCHAR (255)   NULL,
     [Title]       NVARCHAR (255)   NULL,
     [ConfigType]  NVARCHAR (255)   NULL,
     [Settings]    NVARCHAR (MAX)   NULL,
     [CreatedTime] DATETIME         CONSTRAINT [DF_ExtensionConfiguration_CreatedTime] DEFAULT (getdate()) NULL,
     [timestamp]   ROWVERSION       NULL,
-    CONSTRAINT [PK_ExtensionConfiguration] PRIMARY KEY CLUSTERED ([OldID] ASC)
+    CONSTRAINT [PK_ExtensionConfiguration_1] PRIMARY KEY CLUSTERED ([ID] ASC)
 );
+
+
 
 
 
