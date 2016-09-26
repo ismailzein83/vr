f69481e4210c8ec071a559b9b18554bfb3f3fff6
Wissam Ajamy <VANRISE\wissam.ajamy>
2016-09-26 11:32:23 +0000

    Update Structure from Retail_Dev
    git-tfs-id: [http://developmentvlan:8585/tfs/vanrise.collection]$/;C35934

--- Retail/Code/Retail/Retail/Stored Procedures/sp_Distributor_GetAll.sql
+++ Retail/Code/Retail/Retail/Stored Procedures/sp_Distributor_GetAll.sql
@@ -1,25 +1,12 @@
-﻿-- =============================================
+﻿
+-- =============================================
 -- Author:		<Author,,Name>
 -- Create date: <Create Date,,>
 -- Description:	<Description,,>
 -- =============================================
-create PROCEDURE [Retail].[sp_Distributor_GetAll]
-	@ID INT,
-	@Name NVARCHAR(255),
-	@Type nvarchar(255),
-	@Settings NVARCHAR(MAX),
-	@SourceID nvarchar(255)
+create PROCEDURE [Retail].sp_Distributor_GetAll
 AS
 BEGIN
-	IF NOT EXISTS
-	(
-		SELECT 1 FROM Retail.Distributor
-		WHERE ID != @ID
-			AND Name = @Name
-	)
-	BEGIN
-		UPDATE Retail.Distributor
-		SET Name = @Name, [Type] = @Type, Settings = @Settings, SourceID = @SourceID
-		WHERE ID = @ID
-	END
+	SELECT	ID, Name, Type, Settings, SourceID
+	FROM	Retail.Distributor  with(nolock)
 END
