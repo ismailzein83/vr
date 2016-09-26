22f0c8e8165d46e0f97554ee233cdc4b4f65b11b
Ismail Zein <VANRISE\ismail.zein>
2016-09-26 14:44:44 +0000

    added BPContext
    git-tfs-id: [http://developmentvlan:8585/tfs/vanrise.collection]$/;C35944

---
+++ BusinessProcess/Code/Vanrise.BusinessProcess/BPContext.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Activities;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Vanrise.Entities;
+
+namespace Vanrise.BusinessProcess
+{
+    public class BPContext : IBPContext
+    {
+        AsyncActivityHandle _asyncActivityHandle;
+        ActivityContext _activityContext;
+        public BPContext(AsyncActivityHandle asyncActivityHandle)
+        {
+            if (asyncActivityHandle == null)
+                throw new ArgumentNullException("asyncActivityHandle");
+            _asyncActivityHandle = asyncActivityHandle;
+        }
+
+        public BPContext(ActivityContext activityContext)
+        {
+            if (activityContext == null)
+                throw new ArgumentNullException("activityContext");
+            _activityContext = activityContext;
+        }
+
+        public void WriteTrackingMessage(LogEntryType severity, string messageFormat, params object[] args)
+        {
+            if (_activityContext != null)
+                _activityContext.WriteTrackingMessage(severity, messageFormat, args);
+            else
+                _asyncActivityHandle.SharedInstanceData.WriteTrackingMessage(severity, messageFormat, args);
+
+        }
+    }
+}
---
+++ BusinessProcess/Code/Vanrise.BusinessProcess/IBPContext.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Activities;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Vanrise.Entities;
+
+namespace Vanrise.BusinessProcess
+{    
+    public interface IBPContext
+    {
+        void WriteTrackingMessage(LogEntryType severity, string messageFormat, params object[] args);
+    }
+}
