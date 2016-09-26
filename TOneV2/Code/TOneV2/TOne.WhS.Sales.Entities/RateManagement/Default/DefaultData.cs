d222c8473f7513d0fcaa153beea1f4fa9977a787
mohamad.elhariri <mohamad.elhariri@vanrise.tfs.local>
2016-09-26 12:28:31 +0000

    User Story 2270: Added DefaultData
    git-tfs-id: [http://developmentvlan:8585/tfs/vanrise.collection]$/;C35938

---
+++ TOneV2/Code/TOneV2/TOne.WhS.Sales.Entities/RateManagement/Default/DefaultData.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TOne.WhS.BusinessEntity.Entities;
+using Vanrise.BusinessProcess.Entities;
+
+namespace TOne.WhS.Sales.Entities
+{
+    public class DefaultData : IRuleTarget
+    {
+        public object Key
+        {
+            get { return OwnerType;  }
+        }
+        public string TargetType
+        {
+            get { return "DefaultData"; }
+        }
+
+        public SalePriceListOwnerType OwnerType { get; set; }
+        public IEnumerable<ZoneService> CurrentServices { get; set; }
+        public DefaultServiceToAdd DefaultServiceToAdd { get; set; }
+    }
+}
