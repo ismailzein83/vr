7a0385ed0065d1953edb1ab12b3679ce5c877330
Wissam Ajamy <VANRISE\wissam.ajamy>
2016-09-30 15:27:22 +0000

    move transacations scripts to cpnfiguration
    git-tfs-id: [http://developmentvlan:8585/tfs/vanrise.collection]$/;C36047

--- TOneV2/Code/TOneV2/SQL.TOneTransaction/Common.PostDeployment.sql
+++ TOneV2/Code/TOneV2/SQL.TOneTransaction/Common.PostDeployment.sql
@@ -9,21 +9,3 @@ Post-Deployment Script Template
                SELECT * FROM [$(TableName)]					
 --------------------------------------------------------------------------------------
 */
---[runtime].[SchedulerTaskActionType]-------101 to 200--------------------------------------------------------
-----------------------------------------------------------------------------------------------------
-set nocount on;
-;with cte_data([ID],[Name],[ActionTypeInfo])
-as (select * from (values
---//////////////////////////////////////////////////////////////////////////////////////////////////
-(401,'Exchange Rate','{"URL":"","SystemType":false,"Editor":"vr-common-exchangerate-fxsauder","FQTN":"Vanrise.Common.MainExtensions.ExchangeRateTaskAction, Vanrise.Common.MainExtensions"}')           
---\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\
-)c([ID],[Name],[ActionTypeInfo]))
-merge	[runtime].[SchedulerTaskActionType] as t
-using	cte_data as s
-on		1=1 and t.[ID] = s.[ID]
-when matched then
-	update set
-	[Name] = s.[Name],[ActionTypeInfo] = s.[ActionTypeInfo]
-when not matched by target then
-	insert([ID],[Name],[ActionTypeInfo])
-	values(s.[ID],s.[Name],s.[ActionTypeInfo]);
