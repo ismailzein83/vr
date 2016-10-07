501efce6b239ab8b4bef063125660055959cfd94
Wissam Ajamy <VANRISE\wissam.ajamy>
2016-10-07 07:13:03 +0000

    move [runtime].[SchedulerTaskActionType] scrit to configuration script
    git-tfs-id: [http://developmentvlan:8585/tfs/vanrise.collection]$/;C36168

--- TOneV2/Code/TOneV2/SQL.TOneTransaction/TOne.PostDeployment.sql
+++ TOneV2/Code/TOneV2/SQL.TOneTransaction/TOne.PostDeployment.sql
@@ -9,21 +9,3 @@ Post-Deployment Script Template
                SELECT * FROM [$(TableName)]					
 --------------------------------------------------------------------------------------
 */
---[runtime].[SchedulerTaskActionType]-------501 to 600--------------------------------------------------------
-----------------------------------------------------------------------------------------------------
-set nocount on;
-;with cte_data([ID],[Name],[ActionTypeInfo])
-as (select * from (values
---//////////////////////////////////////////////////////////////////////////////////////////////////
-(501,'Migrate from Tone V1','{"URL":"","SystemType":false,"Editor":"whs-be-sourcemigrationreader","FQTN":"TOne.WhS.DBSync.Business.DBSyncTaskAction, TOne.WhS.DBSync.Business"}')
---\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\
-)c([ID],[Name],[ActionTypeInfo]))
-merge	[runtime].[SchedulerTaskActionType] as t
-using	cte_data as s
-on		1=1 and t.[ID] = s.[ID]
-when matched then
-	update set
-	[Name] = s.[Name],[ActionTypeInfo] = s.[ActionTypeInfo]
-when not matched by target then
-	insert([ID],[Name],[ActionTypeInfo])
-	values(s.[ID],s.[Name],s.[ActionTypeInfo]);
