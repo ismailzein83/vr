34fab1797125acb3dc4857349918c900615156ea
Wissam Ajamy <VANRISE\wissam.ajamy>
2016-10-07 08:33:02 +0000

    Remove temp tables
    git-tfs-id: [http://developmentvlan:8585/tfs/vanrise.collection]$/;C36175

--- TOneV2/Code/TOneV2/SQL.TOneWhS_BE/TOneWhS_BE/Tables/Switch_Temp.sql
+++
@@ -1,8 +0,0 @@
-﻿CREATE TABLE [TOneWhS_BE].[Switch_Temp] (
-    [ID]        INT            IDENTITY (1, 1) NOT NULL,
-    [Name]      VARCHAR (50)   NOT NULL,
-    [Settings]  NVARCHAR (MAX) NULL,
-    [timestamp] ROWVERSION     NULL,
-    [SourceID]  VARCHAR (50)   NULL
-);
-
