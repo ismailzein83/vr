28746e9a9166c6ef4c6f65c72e045eebbcc04187
mostafa.jawhari <mostafa.jawhari@vanrise.tfs.local>
2016-10-14 09:54:20 +0000

    implementing user stories: 2335 and 2336
    git-tfs-id: [http://developmentvlan:8585/tfs/vanrise.collection]$/;C36277

---
+++ TOneV2/Code/TOneV2/TOne.WhS.SupplierPriceList.Business/BusinessRules/Conditions/ValidateAfterProcessing/CodeBEDGreaterThanSystemPeriodCondition.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using TOne.WhS.SupplierPriceList.Entities;
+using TOne.WhS.SupplierPriceList.Entities.SPL;
+using Vanrise.BusinessProcess.Entities;
+
+namespace TOne.WhS.SupplierPriceList.Business
+{
+    class CodeBEDGreaterThanSystemPeriodCondition : BusinessRuleCondition
+    {
+        public override bool ShouldValidate(IRuleTarget target)
+        {
+            return (target as ImportedCode != null);
+        }
+
+        public override bool Validate(IBusinessRuleConditionValidateContext context)
+        {
+            ImportedCode importedCode = context.Target as ImportedCode;
+
+            if (importedCode.ChangeType == CodeChangeType.New || importedCode.ChangeType == CodeChangeType.Moved)
+            {
+                IImportSPLContext importSplContext = context.GetExtension<IImportSPLContext>();
+                return (importedCode.BED > DateTime.Today.Add(importSplContext.CodeCloseDateOffset));
+            }
+
+            return true;
+        }
+
+
+        public override string GetMessage(IRuleTarget target)
+        {
+            return string.Format("Code {0} has been opened in a period greater than system period", (target as ImportedCode).Code);
+
+        }
+    }
+}
---
+++ TOneV2/Code/TOneV2/TOne.WhS.SupplierPriceList.Business/BusinessRules/Conditions/ValidateAfterProcessing/ZoneBEDGreaterThanSystemPeriodCondition.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using TOne.WhS.SupplierPriceList.Entities;
+using TOne.WhS.SupplierPriceList.Entities.SPL;
+using Vanrise.BusinessProcess.Entities;
+
+namespace TOne.WhS.SupplierPriceList.Business
+{
+    class ZoneBEDGreaterThanSystemPeriodCondition : BusinessRuleCondition
+    {
+      
+        public override bool ShouldValidate(IRuleTarget target)
+        {
+            return target as ImportedZone != null;
+        }
+
+        public override bool Validate(IBusinessRuleConditionValidateContext context)
+        {
+            ImportedZone importedZone = context.Target as ImportedZone;
+
+            if(importedZone.ChangeType == ZoneChangeType.New || importedZone.ChangeType == ZoneChangeType.Renamed)
+            {
+                IImportSPLContext importSplContext = context.GetExtension<IImportSPLContext>();
+                return (importedZone.BED > DateTime.Today.Add(importSplContext.CodeCloseDateOffset));
+            }
+
+            return true;
+        }
+
+        public override string GetMessage(IRuleTarget target)
+        {
+            return string.Format("Zone {0} has been opened in a period greater than system period", (target as ImportedZone).ZoneName);
+        }
+
+    }
+}
