0bbad4cbaaaa355df404064e16921e9baee28e07
Ali Ballouk <VANRISE\ali.ballouk>
2016-10-17 08:24:57 +0000

    Add DealSettingsConfig
    git-tfs-id: [http://developmentvlan:8585/tfs/vanrise.collection]$/;C36320

---
+++ TOneV2/Code/TOneV2/TOne.WhS.Deal.Entities/DealSettingsConfig.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Vanrise.Entities;
+
+namespace TOne.WhS.Deal.Entities
+{
+    public class DealSettingsConfig:ExtensionConfiguration
+    {
+        public const string EXTENSION_TYPE = "WhS_Deal_DealSettings";
+    }
+}
