6a7202952214f4534e14be0f9457583c5aa043a8
Samer Haidar <VANRISE\samer.haidar>
2016-10-18 11:04:36 +0000

    adding new balance rule definition settings
    git-tfs-id: [http://developmentvlan:8585/tfs/vanrise.collection]$/;C36346

---
+++ Vanrise/Code/Vanrise.AccountBalance.MainExtensions/BalanceRuleDefinitionSettings.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Vanrise.GenericData.Entities;
+
+namespace Vanrise.AccountBalance.MainExtensions
+{
+    public class BalanceRuleDefinitionSettings : GenericRuleDefinitionSettings
+    {
+        public override Guid ConfigId
+        {
+            get { return new Guid("EBB2BF06-4FD8-4942-8DF4-8892A22AA6FD"); }
+        }
+    }
+}
