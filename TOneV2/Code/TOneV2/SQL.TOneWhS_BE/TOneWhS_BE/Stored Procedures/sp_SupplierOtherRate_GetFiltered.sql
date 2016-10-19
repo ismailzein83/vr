67e2fc22aad76a6dd66b494db449d981810a5eb0
Wissam Ajamy <VANRISE\wissam.ajamy>
2016-10-19 06:23:05 +0000

    update structure fron TOneV2_Dev
    git-tfs-id: [http://developmentvlan:8585/tfs/vanrise.collection]$/;C36366

---
+++ TOneV2/Code/TOneV2/SQL.TOneWhS_BE/TOneWhS_BE/Stored Procedures/sp_SupplierOtherRate_GetFiltered.sql
@@ -0,0 +1,19 @@
+﻿CREATE PROCEDURE  [TOneWhS_BE].[sp_SupplierOtherRate_GetFiltered]
+(
+	@ZoneID bigint, 
+	@EffectiveOn DateTime
+)
+	AS
+	BEGIN
+		SET NOCOUNT ON
+
+		 SELECT rate.[ID], rate.[PriceListID], rate.[ZoneID], rate.[CurrencyID], rate.[OtherRates],rate.RateTypeID, rate.[BED], rate.[EED], rate.[timestamp],rate.Change
+         FROM	[TOneWhS_BE].[SupplierRate] rate WITH(NOLOCK) 
+				inner join [TOneWhS_BE].[SupplierPriceList] priceList WITH(NOLOCK) on rate.PriceListID=priceList.ID
+
+		 WHERE	rate.ZoneID = @ZoneID and rate.RateTypeID is not null
+				 AND   (rate.BED < = @EffectiveOn   and (rate.EED is null or rate.EED  > @EffectiveOn) );			
+			
+		
+		SET NOCOUNT OFF
+	END
---
+++ TOneV2/Code/TOneV2/SQL.TOneWhS_BE/TOneWhS_Deal/Tables/Deal.sql
@@ -0,0 +1,7 @@
+﻿CREATE TABLE [TOneWhS_Deal].[Deal] (
+    [ID]       INT            IDENTITY (1, 1) NOT NULL,
+    [Name]     NVARCHAR (255) NOT NULL,
+    [Settings] NVARCHAR (MAX) NOT NULL,
+    CONSTRAINT [PK_Deal_1] PRIMARY KEY CLUSTERED ([ID] ASC)
+);
+
