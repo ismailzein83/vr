3b7ae563643e84dc5008c9c898e586f70e239fa8
Wissam Ajamy <VANRISE\wissam.ajamy>
2016-10-20 11:52:42 +0000

    Add new solution NetworkProvisioning
    git-tfs-id: [http://developmentvlan:8585/tfs/vanrise.collection]$/;C36421

---
+++ NetworkProvisioning/Code/NetworkProvisioning.Web/App_Start/BundleConfig.cs
@@ -0,0 +1,18 @@
+﻿using System.Web;
+using System.Web.Optimization;
+
+namespace NetworkProvisioning.Web
+{
+    public class BundleConfig
+    {
+        // For more information on Bundling, visit http://go.microsoft.com/fwlink/?LinkId=254725
+        public static void RegisterBundles(BundleCollection bundles)
+        {
+            Vanrise.Web.BundleConfig.RegisterBundles(bundles);
+
+            bundles.Add(new ScriptBundle("~/bundles/ModulesJavascripts").IncludeDirectory(
+                "~/Client/Modules/Common", "*.js", true).IncludeDirectory(
+                "~/Client/Modules/Security", "*.js", true));
+        }
+    }
+}
---
+++ NetworkProvisioning/Code/NetworkProvisioning.Web/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="NetworkProvisioning.Web.WebApiApplication" Language="C#" %>
