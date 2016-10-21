7279a31e464a3eb3c71c23fab1c1da1dad9488eb
Wissam Ajamy <VANRISE\wissam.ajamy>
2016-10-21 12:16:08 +0000

    Update structure from TOneV2_Dev
    git-tfs-id: [http://developmentvlan:8585/tfs/vanrise.collection]$/;C36461

---
+++ TOneV2/Code/TOneV2/SQL.TOneWhS_BE/TOneWhS_Deal/Stored Procedures/sp_Deal_GetAll.sql
@@ -0,0 +1,10 @@
+﻿Create PROCEDURE [TOneWhS_Deal].[sp_Deal_GetAll]
+AS
+BEGIN
+	-- SET NOCOUNT ON added to prevent extra result sets from
+	-- interfering with SELECT statements.
+	SET NOCOUNT ON;
+	SELECT	d.ID,d.Name,d.Settings
+	FROM	[TOneWhS_Deal].Deal d WITH(NOLOCK) 
+	SET NOCOUNT OFF
+END
---
+++ TOneV2/Code/TOneV2/SQL.TOneWhS_BE/TOneWhS_Deal/Stored Procedures/sp_Deal_Insert.sql
@@ -0,0 +1,21 @@
+﻿-- =============================================
+-- Author:		<Author,,Name>
+-- Create date: <Create Date,,>
+-- Description:	<Description,,>
+-- =============================================
+		
+Create PROCEDURE [TOneWhS_Deal].[sp_Deal_Insert]
+	@Name nvarchar(255),
+	@Settings nvarchar(MAX),
+	@Id int out
+AS
+BEGIN
+SET @id =0;
+IF NOT EXISTS(SELECT 1 FROM TOneWhS_Deal.Deal WHERE [Name] = @Name)
+	BEGIN
+		Insert into TOneWhS_Deal.Deal(Name,Settings)
+		Values(@Name,@Settings)
+	
+		Set @Id = @@IDENTITY
+	END
+END
