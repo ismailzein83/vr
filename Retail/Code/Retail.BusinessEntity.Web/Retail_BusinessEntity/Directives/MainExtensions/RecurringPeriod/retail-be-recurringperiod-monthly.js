a358686f2e90aa93bc7a957ffd991a6ecf9d3588
Zeinab Mokdad <VANRISE\zeinab.mokdad>
2016-10-21 13:33:11 +0000

    moving recurring period to main extension
    git-tfs-id: [http://developmentvlan:8585/tfs/vanrise.collection]$/;C36474

---
+++ Retail/Code/Retail.BusinessEntity.Web/Retail_BusinessEntity/Directives/MainExtensions/RecurringPeriod/retail-be-recurringperiod-monthly.js
@@ -0,0 +1,45 @@
+﻿'use strict';
+
+app.directive('retailBeRecurringperiodMonthly', ['VRNotificationService',
+    function (vrNotificationService) {
+        return {
+            restrict: 'E',
+            scope: {
+                onReady: '=',
+            },
+            controller: function ($scope, $element, $attrs) {
+                var ctrl = this;
+                var recurringPeriod = new RecurringPeriodMonthly($scope, ctrl, $attrs);
+                recurringPeriod.initializeController();
+            },
+            controllerAs: 'ctrl',
+            bindToController: true,
+            templateUrl: '/Client/Modules/Retail_BusinessEntity/Directives/MainExtensions/RecurringPeriod/Templates/MonthlyPeriodTemplate.html'
+        };
+
+        function RecurringPeriodMonthly($scope, ctrl, $attrs) {
+            this.initializeController = initializeController;
+
+            function initializeController() {
+                $scope.scopeModel = {};
+                defineAPI();
+            }
+            function defineAPI() {
+                var api = {};
+                api.load = function (payload) {
+                    $scope.scopeModel.DayOfMonth = 1;
+                    if (payload != undefined) {
+                        $scope.scopeModel.DayOfMonth = payload.DayOfMonth;
+                    }
+                };
+                api.getData = function () {
+                    return {
+                        $type: "Retail.BusinessEntity.MainExtensions.RecurringPeriods.MonthlyRecurringPeriodSettings, Retail.BusinessEntity.MainExtensions",
+                        DayOfMonth: $scope.scopeModel.DayOfMonth
+                    };
+                };
+                if (ctrl.onReady != null)
+                    ctrl.onReady(api);
+            }
+        }
+    }]);
---
+++ Retail/Code/Retail.BusinessEntity.Web/Retail_BusinessEntity/Directives/MainExtensions/RecurringPeriod/retail-be-recurringperiod-weekly.js
@@ -0,0 +1,68 @@
+﻿'use strict';
+
+app.directive('retailBeRecurringperiodWeekly', ['VRNotificationService', 'UtilsService', 'VRUIUtilsService',
+    function (vrNotificationService, utilsService, vruiUtilsService) {
+
+        return {
+            restrict: 'E',
+            scope: {
+                onReady: '=',
+            },
+            controller: function ($scope, $element, $attrs) {
+                var ctrl = this;
+                var recurringPeriod = new RecurringPeriodWeekly($scope, ctrl, $attrs);
+                recurringPeriod.initializeController();
+            },
+            controllerAs: 'ctrl',
+            bindToController: true,
+            templateUrl: '/Client/Modules/Retail_BusinessEntity/Directives/MainExtensions/RecurringPeriod/Templates/WeeklyPeriodTemplate.html'
+        };
+
+        function RecurringPeriodWeekly($scope, ctrl, $attrs) {
+            this.initializeController = initializeController;
+            var daysOfWeekApi;
+            var daysOfWeekSelectorReadyDeferred = utilsService.createPromiseDeferred();
+            var dayOfWeekEntity;
+
+            function initializeController() {
+                $scope.scopeModel = {};
+                $scope.scopeModel.onDaysOfWeekSelectorReady = function (api) {
+                    daysOfWeekApi = api;
+                    daysOfWeekSelectorReadyDeferred.resolve();
+                }
+            }
+            function defineApi() {
+
+                var api = {};
+                api.load = function (payload) {
+                    dayOfWeekEntity = 0;
+                    if (payload != undefined) {
+                        dayOfWeekEntity = payload.DayOfWeek;
+                    }
+                };
+                api.getData = function () {
+                    return {
+                        $type: "Retail.BusinessEntity.MainExtensions.RecurringPeriods.WeeklyRecurringPeriodSettings, Retail.BusinessEntity.MainExtensions",
+                        DayOfWeek: daysOfWeekApi.getSelectedIds()
+                    };
+                };
+                if (ctrl.onReady != null)
+                    ctrl.onReady(api);
+                function loadDayOfWeekSelector() {
+                    var dayOfWeekSelectorLoadDeferred = utilsService.createPromiseDeferred();
+                    daysOfWeekSelectorReadyDeferred.promise.then(function () {
+                        var dayOfWeekSelectorPayload = null;
+                        if (dayOfWeekEntity != undefined) {
+                            dayOfWeekSelectorPayload = {
+                                selectedIds: dayOfWeekEntity
+                            };
+                        }
+                        vruiUtilsService.callDirectiveLoad(daysOfWeekApi, dayOfWeekSelectorPayload, dayOfWeekSelectorLoadDeferred);
+                    });
+                    return dayOfWeekSelectorLoadDeferred.promise;
+                }
+                loadDayOfWeekSelector();
+            }
+            defineApi();
+        }
+    }]);
