20ff268d637bd5581a959cfdf1dfbdd1b0148800
Ali Ballouk <VANRISE\ali.ballouk>
2016-10-21 12:36:05 +0000

    refactor vr-icon
    git-tfs-id: [http://developmentvlan:8585/tfs/vanrise.collection]$/;C36467

--- Vanrise/Code/Vanrise.Web/Client/Javascripts/Directives/Icon/vr-icon-directive.js
+++ Vanrise/Code/Vanrise.Web/Client/Javascripts/Directives/Icon/vr-icon-directive.js
@@ -3,14 +3,14 @@
 
 app.directive('vrIcon', [function ($compile) {
     var option = {
-        true:{value: "Client/Images/true.png",isimage:true},
-        false:{value: "Client/Images/onebit_33.png",isimage:true} ,
-        'Y': {value: "Client/Images/true.png",isimage:true},
-        'N': {value: "Client/Images/onebit_33.png",isimage:true},
-        'Enabled':{value: "Client/Images/true.png",isimage:true} ,
-        'Disabled':{value: "Client/Images/onebit_33.png",isimage:true} ,
-        '1': {value: "Client/Images/true.png",isimage:true},
-        '0':{value: "Client/Images/onebit_33.png",isimage:true},
+        true: { value: "Client/Images/true.png", isimage: true },
+        false: { value: "Client/Images/onebit_33.png", isimage: true },
+        'Y': { value: "Client/Images/true.png", isimage: true },
+        'N': { value: "Client/Images/onebit_33.png", isimage: true },
+        'Enabled': { value: "Client/Images/true.png", isimage: true },
+        'Disabled': { value: "Client/Images/onebit_33.png", isimage: true },
+        '1': { value: "Client/Images/true.png", isimage: true },
+        '0': { value: "Client/Images/onebit_33.png", isimage: true },
         'music': { value: "glyphicon-music", isimage: false },
         'above': { value: "glyphicon-arrow-up arrow-above", isimage: false },//#37c737
         'below': { value: "glyphicon-arrow-down arrow-below", isimage: false },//#ff1111
@@ -19,7 +19,7 @@ app.directive('vrIcon', [function ($compile) {
         'explicit': { value: 'glyphicon-lock', isimage: false },
         'inherited': { value: 'Client/Images/inherited.png', isimage: true }
     }
-   
+
     var directiveDefinitionObject = {
 
         restrict: 'E',
@@ -47,8 +47,8 @@ app.directive('vrIcon', [function ($compile) {
         bindToController: true,
         link: function preLink($scope, iElement, iAttrs) {
             var ctrl = $scope.ctrl;
-            var template = getTemplate(ctrl,iAttrs)
-           iElement.replaceWith(template);
+            var template = getTemplate(ctrl, iAttrs)
+            iElement.replaceWith(template);
 
         },
 
@@ -63,19 +63,18 @@ app.directive('vrIcon', [function ($compile) {
         var containerstyle = "";
         if (attr.inline != undefined)
             containerstyle = "display:inline-block";
-        var template = ''
+        var tooltip = ctrl.tooltip != undefined ? ctrl.tooltip : ctrl.icontype
+
+        var template = '';
         if (ctrl.icon.isimage) {
-            if (ctrl.icontype != undefined)
-                template += '<div style="text-align: left;' + containerstyle + '"><img style="width:20px;height:20px" title="' + ctrl.icontype + '"  src="' + ctrl.icon.value + '"  /><span>' + text + '</span></div>'
-            else if (ctrl.tooltip != undefined)
-                template += '<div style="text-align: left;' + containerstyle + '"><img style="width:20px;height:20px" title="' + ctrl.tooltip + '"  src="' + ctrl.icon.value + '"  /><span>' + text + '</span></div>'
+            template += '<div style="text-align: left;' + containerstyle + '"><img style="width:20px;height:20px" title="' + tooltip + '"  src="' + ctrl.icon.value + '"  /><span>' + text + '</span></div>'
         }
         else
-            template += '<div style="text-align: left;' + containerstyle + '"><span class="glyphicon ' + ctrl.icon.value + '" title="' + ctrl.tooltip + '" /></div>'
+            template += '<div style="text-align: left;' + containerstyle + '"><span class="glyphicon ' + ctrl.icon.value + '" title="' + tooltip + '" /></div>'
 
         return template;
     }
-  
+
 
     return directiveDefinitionObject;
 
