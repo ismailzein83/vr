5eb90e787dceac3cadf78c343ef0f6fb7bbc149f
mostafa.jawhari <mostafa.jawhari@vanrise.tfs.local>
2016-10-25 14:22:25 +0000

    adding needed files to implement state backup management
    git-tfs-id: [http://developmentvlan:8585/tfs/vanrise.collection]$/;C36531

---
+++ TOneV2/Code/TOneV2/TOne.WhS.BusinessEntity.Web/WhS_BusinessEntity/Directives/StateBackup/vr-whs-be-statebackup-grid.js
@@ -0,0 +1,65 @@
+﻿"use strict";
+
+app.directive("vrWhsBeStatebackupGrid", ["UtilsService", "VRNotificationService", "WhS_BE_SupplierCodeAPIService",
+function (UtilsService, VRNotificationService, WhS_BE_SupplierCodeAPIService) {
+
+    var directiveDefinitionObject = {
+
+        restrict: "E",
+        scope: {
+            onReady: "="
+        },
+        controller: function ($scope, $element, $attrs) {
+            var ctrl = this;
+            var grid = new SupplierCodeGrid($scope, ctrl, $attrs);
+            grid.initializeController();
+        },
+        controllerAs: "ctrl",
+        bindToController: true,
+        compile: function (element, attrs) {
+
+        },
+        templateUrl: "/Client/Modules/WhS_BusinessEntity/Directives/SupplierCode/Templates/SupplierCodeGridTemplate.html"
+
+    };
+
+    function SupplierCodeGrid($scope, ctrl, $attrs) {
+
+        var gridAPI;
+        this.initializeController = initializeController;
+
+        function initializeController() {
+           
+            $scope.suppliercodes = [];
+            $scope.onGridReady = function (api) {
+                gridAPI = api;
+                
+                if (ctrl.onReady != undefined && typeof (ctrl.onReady) == "function")
+                    ctrl.onReady(getDirectiveAPI());
+                function getDirectiveAPI() {
+                   
+                    var directiveAPI = {};
+                    directiveAPI.loadGrid = function (query) {
+                       
+                        return gridAPI.retrieveData(query);
+                    }
+                   
+                    return directiveAPI;
+                }
+            };
+            $scope.dataRetrievalFunction = function (dataRetrievalInput, onResponseReady) {
+                return WhS_BE_SupplierCodeAPIService.GetFilteredSupplierCodes(dataRetrievalInput)
+                    .then(function (response) {
+                         onResponseReady(response);
+                    })
+                    .catch(function (error) {
+                        VRNotificationService.notifyException(error, $scope);
+                    });
+            };
+        }
+
+    }
+
+    return directiveDefinitionObject;
+
+}]);
