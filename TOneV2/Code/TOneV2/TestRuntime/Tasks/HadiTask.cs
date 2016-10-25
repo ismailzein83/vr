5642353b52980883da4f9c0c1be30301226555c6
Ismail Zein <VANRISE\ismail.zein>
2016-10-25 06:50:58 +0000

    added HadiTask
    git-tfs-id: [http://developmentvlan:8585/tfs/vanrise.collection]$/;C36523

---
+++ TOneV2/Code/TOneV2/TestRuntime/Tasks/HadiTask.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TestRuntime;
+
+namespace TOne.WhS.Runtime.Tasks
+{
+    class HadiTask : ITask
+    {
+        public void Execute()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
