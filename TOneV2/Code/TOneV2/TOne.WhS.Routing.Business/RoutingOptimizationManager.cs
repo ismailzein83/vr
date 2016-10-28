40c073b2b8a7eb2c4ddd79e4c2782a343c95ab8e
Ismail Zein <VANRISE\ismail.zein>
2016-10-28 08:27:43 +0000

    added RoutingOptimizationManager
    git-tfs-id: [http://developmentvlan:8585/tfs/vanrise.collection]$/;C36574

---
+++ TOneV2/Code/TOneV2/TOne.WhS.Routing.Business/RoutingOptimizationManager.cs
@@ -0,0 +1,17 @@
+﻿//using System;
+//using System.Collections.Generic;
+//using System.Linq;
+//using System.Text;
+//using System.Threading.Tasks;
+//using TOne.WhS.Routing.Entities;
+
+//namespace TOne.WhS.Routing.Business
+//{
+//    public class RoutingOptimizationManager
+//    {
+//        public void RunOptimization(RoutingOptimizationSettings optimizationSettings)
+//        {
+
+//        }
+//    }
+//}
