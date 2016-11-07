553b0df1c42df31a5aaa541e2c662db6a4241cc9
Ali Atoui <VANRISE\ali.atoui>
2016-11-07 16:05:09 +0000

    Editing GetCDPNsForIdentificationStep
    git-tfs-id: [http://developmentvlan:8585/tfs/vanrise.collection]$/;C36742

--- TOneV2/Code/TOneV2/TOne.WhS.BusinessEntity.MainExtensions/MappingSteps/GetCDPNsForIdentificationStep.cs
+++ TOneV2/Code/TOneV2/TOne.WhS.BusinessEntity.MainExtensions/MappingSteps/GetCDPNsForIdentificationStep.cs
@@ -12,19 +12,19 @@ namespace TOne.WhS.BusinessEntity.MainExtensions.MappingSteps
         public override Guid ConfigId { get { return new Guid("EE9BDD41-2DEB-4E0A-A2BD-871E5A30E03C"); } }
 
         public int SwitchId { get; set; }
-        public int CDPN { get; set; }
+        public int InputCDPN { get; set; }
         public int CDPNIn { get; set; }
         public int CDPNOut { get; set; }
 
         public int CustomerCDPN { get; set; }
         public int SupplierCDPN { get; set; }
-        public int CDPN { get; set; }
+        public int OutputCDPN { get; set; }
 
 
         public override void GenerateExecutionCode(IDataTransformationCodeGenerationContext context)
         {
             //context.AddCodeToCurrentInstanceExecutionBlock("{0} = {1};",
             //    this.Target, this.Source);
-        }
+        } 
     }
 }
