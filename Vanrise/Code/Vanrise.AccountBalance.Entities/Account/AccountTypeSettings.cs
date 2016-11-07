2e4d57d8d8984e093128acb3df212a89690051b0
Ismail Zein <VANRISE\ismail.zein>
2016-11-07 10:54:52 +0000

    added AccountType to AccountBalance module
    git-tfs-id: [http://developmentvlan:8585/tfs/vanrise.collection]$/;C36727

---
+++ Vanrise/Code/Vanrise.AccountBalance.Entities/Account/AccountTypeSettings.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Vanrise.AccountBalance.Entities
+{
+    public class AccountTypeSettings : Vanrise.Entities.VRComponentTypeSettings
+    {
+        public override Guid VRComponentTypeConfigId
+        {
+            get { return new Guid("7824DFFA-0EBF-4939-93E8-DEC6E5EDFA10"); }
+        }
+
+        public Guid AccountBusinessEntityDefinitionId { get; set; }
+
+        public string AccountSelector { get; set; }
+
+        public Guid UsageTransactionTypeId { get; set; }
+
+        public Guid AlertMailMessageTypeId { get; set; }
+
+        public BalancePeriodSettings BalancePeriod { get; set; }
+    }
+}
