d21d1af3725c3aacd8bedb64751eb6ffaf6160d8
Samer Haidar <VANRISE\samer.haidar>
2016-11-07 09:08:01 +0000

    change string to guid
    git-tfs-id: [http://developmentvlan:8585/tfs/vanrise.collection]$/;C36718

--- Vanrise/Code/Vanrise.Security.Data/IBusinessEntityDataManager.cs
+++ Vanrise/Code/Vanrise.Security.Data/IBusinessEntityDataManager.cs
@@ -11,10 +11,10 @@ namespace Vanrise.Security.Data
     {
         IEnumerable<Vanrise.Security.Entities.BusinessEntity> GetEntities();
 
-        bool ToggleBreakInheritance(string entityId);
+        bool ToggleBreakInheritance(Guid entityId);
         bool AddBusinessEntity(BusinessEntity businessEntity);
         bool UpdateBusinessEntity(BusinessEntity businessEntity);
         bool AreBusinessEntitiesUpdated(ref object updateHandle);
-        bool UpdateBusinessEntityRank(Guid entityId, Guid moduleId);
+        bool UpdateBusinessEntityRank(Guid entityId, Guid moduleId); 
     }
 }
