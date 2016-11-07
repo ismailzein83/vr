53ddc223fb68ccceb44d8bc26885527d780765ee
Ali Atoui <VANRISE\ali.atoui>
2016-11-07 15:35:00 +0000

    Creating GetCDPNsForIdentification and GetCDPNsForZoneMatch classes and directives
    git-tfs-id: [http://developmentvlan:8585/tfs/vanrise.collection]$/;C36739

---
+++ TOneV2/Code/TOneV2/TOne.WhS.BusinessEntity.MainExtensions/MappingSteps/GetCDPNsForIdentificationStep.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Vanrise.GenericData.Transformation.Entities;
+
+namespace TOne.WhS.BusinessEntity.MainExtensions.MappingSteps
+{
+    class GetCDPNsForIdentificationStep : MappingStep
+    {
+        public override Guid ConfigId { get { return new Guid("EE9BDD41-2DEB-4E0A-A2BD-871E5A30E03C"); } }
+
+        public int SwitchId { get; set; }
+        public int CDPN { get; set; }
+        public int CDPNIn { get; set; }
+        public int CDPNOut { get; set; }
+
+        public int CustomerCDPN { get; set; }
+        public int SupplierCDPN { get; set; }
+        public int CDPN { get; set; }
+
+
+        public override void GenerateExecutionCode(IDataTransformationCodeGenerationContext context)
+        {
+            //context.AddCodeToCurrentInstanceExecutionBlock("{0} = {1};",
+            //    this.Target, this.Source);
+        }
+    }
+}
