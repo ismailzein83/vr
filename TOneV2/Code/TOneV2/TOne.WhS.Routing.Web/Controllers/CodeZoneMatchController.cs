676d99a732384f517896998db2b5ed16db14503d
mohamad.elhariri <mohamad.elhariri@vanrise.tfs.local>
2016-11-07 15:00:32 +0000

    Added CodeZoneMatchController and CodeZoneMatchAPIService
    git-tfs-id: [http://developmentvlan:8585/tfs/vanrise.collection]$/;C36737

---
+++ TOneV2/Code/TOneV2/TOne.WhS.Routing.Web/Controllers/CodeZoneMatchController.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace TOne.WhS.Routing.Web.Controllers
+{
+	public class CodeZoneMatchController
+	{
+	}
+}
