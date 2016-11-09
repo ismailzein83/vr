f4679998488e27cd6fe2210a5c09458cf915ff66
mostafa.jawhari <mostafa.jawhari@vanrise.tfs.local>
2016-11-09 09:03:54 +0000

    adding needed files for notification
    git-tfs-id: [http://developmentvlan:8585/tfs/vanrise.collection]$/;C36775

---
+++ TOneV2/Code/TOneV2/TOne.WhS.BusinessEntity.Entities/SalePLNotification/SalePLRateNotification.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TOne.WhS.BusinessEntity.Entities
+{
+    public class SalePLRateNotification
+    {
+        public Decimal Rate { get; set; }
+
+        public DateTime BED { get; set; }
+
+        public DateTime? EED { get; set; }
+    }
+}
---
+++ TOneV2/Code/TOneV2/TOne.WhS.BusinessEntity.Entities/SalePLNotification/SalePLZoneNotification.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TOne.WhS.BusinessEntity.Entities
+{
+   public  class SalePLZoneNotification
+    {
+        private List<SalePLCodeNotification> _codes = new List<SalePLCodeNotification>();
+
+        public string ZoneName { get; set; }
+
+        public List<SalePLCodeNotification> Codes
+        {
+            get
+            {
+                return this._codes;
+            }
+        }
+
+        public SalePLRateNotification Rate { get; set; }    
+    }
+}
