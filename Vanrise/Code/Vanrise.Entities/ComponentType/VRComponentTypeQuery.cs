ac098d21267f1fd751ace17c1d0cd36e8f62b102
rabih.fashwal:2 <rabih.fashwal:2@vanrise.tfs.local>
2016-11-09 10:30:21 +0000

    Add Component Type detail and query class
    git-tfs-id: [http://developmentvlan:8585/tfs/vanrise.collection]$/;C36779

---
+++ Vanrise/Code/Vanrise.Entities/ComponentType/VRComponentTypeQuery.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Vanrise.Entities
+{
+    public class VRComponentTypeQuery
+    {
+        public string Name { get; set; }
+    }
+}
