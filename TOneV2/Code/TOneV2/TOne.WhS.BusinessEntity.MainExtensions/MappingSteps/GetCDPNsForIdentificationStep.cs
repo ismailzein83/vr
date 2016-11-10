db9aac0e05d28901086e0c92b8aa6692b64af1ad
Ali Atoui <VANRISE\ali.atoui>
2016-11-10 09:17:19 +0000

    Adding 2 new steps for CDPN configuration
    git-tfs-id: [http://developmentvlan:8585/tfs/vanrise.collection]$/;C36813

--- TOneV2/Code/TOneV2/TOne.WhS.BusinessEntity.MainExtensions/MappingSteps/GetCDPNsForIdentificationStep.cs
+++ TOneV2/Code/TOneV2/TOne.WhS.BusinessEntity.MainExtensions/MappingSteps/GetCDPNsForIdentificationStep.cs
@@ -3,28 +3,40 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using TOne.WhS.BusinessEntity.Business;
+using TOne.WhS.BusinessEntity.Entities;
 using Vanrise.GenericData.Transformation.Entities;
 
 namespace TOne.WhS.BusinessEntity.MainExtensions.MappingSteps
 {
-    class GetCDPNsForIdentificationStep : MappingStep
+    public class GetCDPNsForIdentificationStep : MappingStep
     {
         public override Guid ConfigId { get { return new Guid("EE9BDD41-2DEB-4E0A-A2BD-871E5A30E03C"); } }
 
-        public int SwitchId { get; set; }
-        public int InputCDPN { get; set; }
-        public int CDPNIn { get; set; }
-        public int CDPNOut { get; set; }
+        public string SwitchId { get; set; }
+        public string InputCDPN { get; set; }
+        public string CDPNIn { get; set; }
+        public string CDPNOut { get; set; }
 
-        public int CustomerCDPN { get; set; }
-        public int SupplierCDPN { get; set; }
-        public int OutputCDPN { get; set; }
+        public string CustomerCDPN { get; set; }
+        public string SupplierCDPN { get; set; }
+        public string OutputCDPN { get; set; }
 
 
         public override void GenerateExecutionCode(IDataTransformationCodeGenerationContext context)
         {
-            //context.AddCodeToCurrentInstanceExecutionBlock("{0} = {1};",
-            //    this.Target, this.Source);
-        } 
+            //var switchManagerVariableName = context.GenerateUniqueMemberName("switchManager");
+            //context.AddCodeToCurrentInstanceExecutionBlock("var {0} = new TOne.WhS.BusinessEntity.Business.SwitchManager();", switchManagerVariableName);
+
+            //var switchCDPNsForIdentificationVariableName = context.GenerateUniqueMemberName("switchCDPNsForIdentification");
+            //context.AddCodeToCurrentInstanceExecutionBlock("TOne.WhS.BusinessEntity.Business.SwitchCDPNsForIdentification {0} = {1}.GetSwitchCDPNsForIdentification({2},{3},{4},{5});", switchCDPNsForIdentificationVariableName, switchManagerVariableName,
+            //    this.SwitchId, this.InputCDPN, this.CDPNIn, this.CDPNOut);
+
+            //context.AddCodeToCurrentInstanceExecutionBlock("{0} = {1}.CustomerCDPN;", this.CustomerCDPN, switchCDPNsForIdentificationVariableName);
+
+            //context.AddCodeToCurrentInstanceExecutionBlock("{0} = {1}.SupplierCDPN;", this.SupplierCDPN, switchCDPNsForIdentificationVariableName);
+
+            //context.AddCodeToCurrentInstanceExecutionBlock("{0} = {1}.OutputCDPN;", this.OutputCDPN, switchCDPNsForIdentificationVariableName);
+        }
     }
 }
--- TOneV2/Code/TOneV2/TOne.WhS.BusinessEntity.MainExtensions/MappingSteps/GetCDPNsForZoneMatchStep.cs
+++ TOneV2/Code/TOneV2/TOne.WhS.BusinessEntity.MainExtensions/MappingSteps/GetCDPNsForZoneMatchStep.cs
@@ -3,10 +3,35 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Vanrise.GenericData.Transformation.Entities;
 
 namespace TOne.WhS.BusinessEntity.MainExtensions.MappingSteps
 {
-    class GetCDPNsForZoneMatchStep
+    public class GetCDPNsForZoneMatchStep : MappingStep
     {
+        public override Guid ConfigId { get { return new Guid("C29BD6EC-35FB-4EDA-8CF0-E889E6F4C000"); } }
+
+        public string SwitchId { get; set; }
+        public string CDPNNormalized { get; set; }
+        public string CDPNIn { get; set; }
+        public string CDPNOut { get; set; }
+
+        public string SaleZoneCDPN { get; set; }
+        public string SupplierZoneCDPN { get; set; }
+
+
+        public override void GenerateExecutionCode(IDataTransformationCodeGenerationContext context)
+        {
+            //var switchManagerVariableName = context.GenerateUniqueMemberName("switchManager");
+            //context.AddCodeToCurrentInstanceExecutionBlock("var {0} = new TOne.WhS.BusinessEntity.Business.SwitchManager();", switchManagerVariableName);
+
+            //var switchCDPNsForZoneMatchVariableName = context.GenerateUniqueMemberName("switchCDPNsForZoneMatch");
+            //context.AddCodeToCurrentInstanceExecutionBlock("TOne.WhS.BusinessEntity.Business.SwitchCDPNsForZoneMatch {0} = {1}.GetSwitchCDPNsForZoneMatch({2},{3},{4},{5});", switchCDPNsForZoneMatchVariableName, switchManagerVariableName,
+            //    this.SwitchId, this.CDPNNormalized, this.CDPNIn, this.CDPNOut);
+
+            //context.AddCodeToCurrentInstanceExecutionBlock("{0} = {1}.SaleZoneCDPN;", this.SaleZoneCDPN, switchCDPNsForZoneMatchVariableName);
+
+            //context.AddCodeToCurrentInstanceExecutionBlock("{0} = {1}.SupplierZoneCDPN;", this.SupplierZoneCDPN, switchCDPNsForZoneMatchVariableName);
+        }
     }
 }
