84aec8c9efb67cf019afa0ea5ec7393a5ea8e4c3
mohamad.elhariri <mohamad.elhariri@vanrise.tfs.local>
2016-11-13 17:44:15 +0000

    Added BasicSalePriceListTemplateSettingsMappedValueExtensionConfig
    git-tfs-id: [http://developmentvlan:8585/tfs/vanrise.collection]$/;C36885

---
+++ TOneV2/Code/TOneV2/TOne.WhS.BusinessEntity.Entities/SalePriceListTemplate/MappedSheet/BasicSalePriceListTemplateSettingsMappedValueExtensionConfig.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TOne.WhS.BusinessEntity.Entities
+{
+	public class BasicSalePriceListTemplateSettingsMappedValueExtensionConfig : Vanrise.Entities.ExtensionConfiguration
+	{
+		public const string EXTENSION_TYPE = "WhS_BE_BasicSalePriceListTemplateSettingsMappedValue";
+
+		public string Editor { get; set; }
+	}
+}
