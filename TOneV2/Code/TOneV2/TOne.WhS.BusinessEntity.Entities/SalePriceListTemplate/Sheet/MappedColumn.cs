e6e55be0c188ebaefd8a304ebfc41f0080d166aa
mohamad.elhariri <mohamad.elhariri@vanrise.tfs.local>
2016-11-13 15:30:23 +0000

    Added SalePriceListTemplate MappedSheet entities
    git-tfs-id: [http://developmentvlan:8585/tfs/vanrise.collection]$/;C36883

---
+++ TOneV2/Code/TOneV2/TOne.WhS.BusinessEntity.Entities/SalePriceListTemplate/Sheet/MappedColumn.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TOne.WhS.BusinessEntity.Entities
+{
+	public class MappedColumn
+	{
+		public int ColumnIndex { get; set; }
+
+		public MappedValue MappedValue { get; set; }
+	}
+}
---
+++ TOneV2/Code/TOneV2/TOne.WhS.BusinessEntity.Entities/SalePriceListTemplate/Sheet/MappedSheet.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TOne.WhS.BusinessEntity.Entities
+{
+	public class MappedSheet
+	{
+		public int SheetIndex { get; set; }
+
+		public int FirstRowIndex { get; set; }
+
+		public List<MappedColumn> MappedColumns { get; set; }
+	}
+}
