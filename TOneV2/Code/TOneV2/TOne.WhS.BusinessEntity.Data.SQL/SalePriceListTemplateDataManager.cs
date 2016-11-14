264ac077efabd2d7dae5d7d0663c9d2baebb20a8
mohamad.elhariri <mohamad.elhariri@vanrise.tfs.local>
2016-11-14 08:51:04 +0000

    Implemented SalePriceListTemplateDataManager
    git-tfs-id: [http://developmentvlan:8585/tfs/vanrise.collection]$/;C36898

--- TOneV2/Code/TOneV2/TOne.WhS.BusinessEntity.Data.SQL/SalePriceListTemplateDataManager.cs
+++ TOneV2/Code/TOneV2/TOne.WhS.BusinessEntity.Data.SQL/SalePriceListTemplateDataManager.cs
@@ -1,12 +1,83 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using TOne.WhS.BusinessEntity.Entities;
 
 namespace TOne.WhS.BusinessEntity.Data.SQL
 {
-	class SalePriceListTemplateDataManager
+	public class SalePriceListTemplateDataManager : Vanrise.Data.SQL.BaseSQLDataManager, ISalePriceListTemplateDataManager
 	{
+		#region Constructors
+
+		public SalePriceListTemplateDataManager()
+            : base(GetConnectionStringName("TOneWhS_BE_DBConnStringKey", "TOneWhS_BE_DBConnString"))
+        {
+
+        }
+
+		#endregion
+
+		#region Public Methods
+
+		public IEnumerable<SalePriceListTemplate> GetAll()
+		{
+			return GetItemsSP("TOneWhS_BE.sp_SalePriceListTemplate_GetAll", SalePriceListTemplateMapper);
+		}
+
+		public bool Insert(SalePriceListTemplate salePriceListTemplate, out int insertedId)
+		{
+			object salePriceListTemplateId;
+			string serializedSettings = GetSerializedObject(salePriceListTemplate.Settings);
+			int affectedRows = ExecuteNonQuerySP("TOneWhS_BE.sp_SalePriceListTemplate_Insert", out salePriceListTemplateId, salePriceListTemplate.Name, serializedSettings);
+			if (affectedRows > 0)
+			{
+				insertedId = (int)salePriceListTemplateId;
+				return true;
+			}
+			insertedId = -1;
+			return false;
+		}
+
+		public bool Update(SalePriceListTemplate salePriceListTemplate)
+		{
+			string serializedSettings = GetSerializedObject(salePriceListTemplate.Settings);
+			int affectedRows = ExecuteNonQuerySP("TOneWhS_BE.sp_SalePriceListTemplate_Update", salePriceListTemplate.SalePriceListTemplateId, salePriceListTemplate.Name, serializedSettings);
+			return (affectedRows > 0);
+		}
+
+		public bool AreSalePriceListTemplatesUpdated(ref object updateHandle)
+		{
+			return base.IsDataUpdated("TOneWhS_BE.SalePriceListTemplate", ref updateHandle);
+		}
+
+		#endregion
+
+		#region Mappers
+
+		private SalePriceListTemplate SalePriceListTemplateMapper(IDataReader reader)
+		{
+			return new SalePriceListTemplate()
+			{
+				SalePriceListTemplateId = (int)reader["ID"],
+				Name = reader["Name"] as string,
+				Settings = Vanrise.Common.Serializer.Deserialize<SalePriceListTemplateSettings>(reader["Settings"] as string)
+			};
+		}
+		
+		#endregion
+
+		#region Private Methods
+		
+		private string GetSerializedObject(object objectToSerialize)
+		{
+			if (objectToSerialize != null)
+				return Vanrise.Common.Serializer.Serialize(objectToSerialize);
+			return null;
+		}
+		
+		#endregion
 	}
 }
--- TOneV2/Code/TOneV2/TOne.WhS.BusinessEntity.Data/ISalePriceListTemplateDataManager.cs
+++ TOneV2/Code/TOneV2/TOne.WhS.BusinessEntity.Data/ISalePriceListTemplateDataManager.cs
@@ -3,10 +3,18 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using TOne.WhS.BusinessEntity.Entities;
 
 namespace TOne.WhS.BusinessEntity.Data
 {
-	class ISalePriceListTemplateDataManager
+	public interface ISalePriceListTemplateDataManager : IDataManager
 	{
+		IEnumerable<SalePriceListTemplate> GetAll();
+
+		bool Insert(SalePriceListTemplate salePriceListTemplate, out int insertedId);
+
+		bool Update(SalePriceListTemplate salePriceListTemplate);
+
+		bool AreSalePriceListTemplatesUpdated(ref object updateHandle);
 	}
 }
