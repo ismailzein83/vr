18afd70340ead472d7a3cedbde973e3ffda91e4d
mohamad.elhariri <mohamad.elhariri@vanrise.tfs.local>
2016-11-14 07:18:55 +0000

    Implemented SalePriceListTemplateSettingsExtensionConfig
    git-tfs-id: [http://developmentvlan:8585/tfs/vanrise.collection]$/;C36886

--- TOneV2/Code/TOneV2/TOne.WhS.BusinessEntity.Entities/SalePriceListTemplate/SalePriceListTemplateSettingsExtensionConfig.cs
+++ TOneV2/Code/TOneV2/TOne.WhS.BusinessEntity.Entities/SalePriceListTemplate/SalePriceListTemplateSettingsExtensionConfig.cs
@@ -6,9 +6,9 @@ using System.Threading.Tasks;
 
 namespace TOne.WhS.BusinessEntity.Entities
 {
-	public class SalePriceListTemplateSettingsType : Vanrise.Entities.ExtensionConfiguration
+	public class SalePriceListTemplateSettingsExtensionConfig : Vanrise.Entities.ExtensionConfiguration
 	{
-		public const string EXTENSION_TYPE = "WhS_BE_SalePriceListTemplateSettingsType";
+		public const string EXTENSION_TYPE = "WhS_BE_SalePriceListTemplateSettings";
 
 		public string Editor { get; set; }
 	}
