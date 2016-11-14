e402dea4cd02ec097cdd4500dc2160964df060a9
Ali Atoui <VANRISE\ali.atoui>
2016-11-14 13:55:40 +0000

    Adding BusinessEntitySettingsData
    git-tfs-id: [http://developmentvlan:8585/tfs/vanrise.collection]$/;C36914

---
+++ TOneV2/Code/TOneV2/TOne.WhS.BusinessEntity.Entities/Settings/BusinessEntitySettingsData.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Vanrise.Entities;
+
+namespace TOne.WhS.BusinessEntity.Entities
+{
+    public class BusinessEntitySettingsData : SettingData
+    {
+        public const string BusinessEntityTechnicalSettings = "WhS_BE_Settings";
+
+
+    }
+
+    public class CachingExpirationIntervals
+    {
+        public int? TodayEntites { get; set; }
+
+        public int? PreviousEntites { get; set; }
+    }
+}
