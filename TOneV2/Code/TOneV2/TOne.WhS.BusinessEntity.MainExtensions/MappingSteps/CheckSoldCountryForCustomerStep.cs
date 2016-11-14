40c392d8beb0818a223ce490c6cbbe4f47dad134
anthony douaihy <VANRISE\anthony.douaihy>
2016-11-14 07:29:10 +0000

    new step to check if country is sold for a customer
    git-tfs-id: [http://developmentvlan:8585/tfs/vanrise.collection]$/;C36888

---
+++ TOneV2/Code/TOneV2/TOne.WhS.BusinessEntity.MainExtensions/MappingSteps/CheckSoldCountryForCustomerStep.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Vanrise.GenericData.Transformation.Entities;
+
+namespace TOne.WhS.BusinessEntity.MainExtensions.MappingSteps
+{
+    public class CheckSoldCountryForCustomerStep : MappingStep
+    {
+        public override Guid ConfigId { get { return new Guid("C42FEB1F-C239-42CA-9ADC-8D525C5E514C"); } }
+
+        public string CustomerId { get; set; }
+        public string CountryId { get; set; }
+        public string IsCountrySold { get; set; }
+
+        public override void GenerateExecutionCode(IDataTransformationCodeGenerationContext context)
+        {
+            context.AddCodeToCurrentInstanceExecutionBlock("{0} = true;", this.IsCountrySold);
+        }
+    }
+}
