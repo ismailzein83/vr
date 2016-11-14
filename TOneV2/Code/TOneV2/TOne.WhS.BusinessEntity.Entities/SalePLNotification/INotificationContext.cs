1d77e1d9a3c6da525911e62b3746de241ec12c5c
mostafa.jawhari <mostafa.jawhari@vanrise.tfs.local>
2016-11-14 13:45:37 +0000

    Adding Notification Context and Updated Price List by adding Price List Type for Sale ENtities
    git-tfs-id: [http://developmentvlan:8585/tfs/vanrise.collection]$/;C36913

---
+++ TOneV2/Code/TOneV2/TOne.WhS.BusinessEntity.Entities/SalePLNotification/INotificationContext.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TOne.WhS.BusinessEntity.Entities
+{
+    public interface INotificationContext
+    {
+        int SellingNumberPlanId { get; }
+
+        IEnumerable<int> CustomerIds { get; }
+
+        IEnumerable<SalePLZoneChange> ZoneChanges { get; }
+
+        DateTime EffectiveDate { get; }
+
+        SalePLChangeType ChangeType { get; }
+
+        int InitiatorId { get; }
+    }
+}
--- TOneV2/Code/TOneV2/TOne.WhS.BusinessEntity.Entities/SalePriceList/SalePriceList.cs
+++ TOneV2/Code/TOneV2/TOne.WhS.BusinessEntity.Entities/SalePriceList/SalePriceList.cs
@@ -10,11 +10,21 @@ namespace TOne.WhS.BusinessEntity.Entities
         [Description("Customer")]
         Customer = 1
     }
+
+    public enum SalePriceListType
+    {
+        Full = 0,
+        Country = 1,
+        RateChange = 2
+    }
+
     public class SalePriceList
     {
         public int PriceListId { get; set; }
 
         public SalePriceListOwnerType OwnerType { get; set; }
+
+        public SalePriceListType PriceListType { get; set; }
 
         public int OwnerId { get; set; }
 
