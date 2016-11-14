94a6b32b33c8afef0d77e63bc3a9bac4cd8af04a
mohamad.elhariri <mohamad.elhariri@vanrise.tfs.local>
2016-11-14 08:55:47 +0000

    Renamed BasicSalePriceListTemplateSettingsBEFieldEnum
    git-tfs-id: [http://developmentvlan:8585/tfs/vanrise.collection]$/;C36899

---
+++ TOneV2/Code/TOneV2/TOne.WhS.BusinessEntity.Web/WhS_BusinessEntity/Constant/BasicSalePriceListTemplateSettingsBEFieldEnum.js
@@ -0,0 +1,9 @@
+﻿app.constant('WhS_BE_BasicSalePriceListTemplateSettingsBEFieldEnum', {
+	Zone: { value: 0, description: "Zone" },
+	Code: { value: 1, description: "Code" },
+	CodeBED: { value: 2, description: "CodeBED" },
+	CodeEED: { value: 3, description: "CodeEED" },
+	Rate: { value: 4, description: "Rate" },
+	RateBED: { value: 5, description: "RateBED" },
+	RateEED: { value: 6, description: "RateEED" },
+});
