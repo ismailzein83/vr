724bd1458c84da407530cf874d7a20006af3d3a0
Wissam Ajamy <VANRISE\wissam.ajamy>
2016-11-15 09:24:32 +0000

    remove vr_invoice
    git-tfs-id: [http://developmentvlan:8585/tfs/vanrise.collection]$/;C36946

--- TOneV2/Code/TOneV2/SQL.TOneWhS_BE/VR_Invoice/Stored Procedures/sp_InvoiceItem_Save.sql
+++
@@ -1,7 +0,0 @@
-﻿CREATE PROCEDURE [VR_Invoice].[sp_InvoiceItem_Save]
-	@InvoiceItemTable VR_Invoice.InvoiceItemType READONLY
-AS
-BEGIN
-	Insert INTO VR_Invoice.InvoiceItem (InvoiceID,ItemSetName,Name,Details)
-	SELECT InvoiceID,ItemSetName,Name,Details FROM @InvoiceItemTable
-END
--- TOneV2/Code/TOneV2/SQL.TOneWhS_BE/VR_Invoice/Tables/InvoiceType.sql
+++
@@ -1,9 +0,0 @@
-﻿CREATE TABLE [VR_Invoice].[InvoiceType] (
-    [ID]          UNIQUEIDENTIFIER NOT NULL,
-    [Name]        NVARCHAR (255)   NOT NULL,
-    [Settings]    NVARCHAR (MAX)   NULL,
-    [timestamp]   ROWVERSION       NULL,
-    [CreatedTime] DATETIME         CONSTRAINT [DF_InvoiceType_CreatedTime] DEFAULT (getdate()) NULL,
-    CONSTRAINT [PK_InvoiceType] PRIMARY KEY CLUSTERED ([ID] ASC)
-);
-
