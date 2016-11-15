fdf73df224ba5c69ef725404b0a2acb6cf50b134
Wissam Ajamy <VANRISE\wissam.ajamy>
2016-11-15 12:23:51 +0000

    Remove last comma from functions and arrays (JSLint : Expected an identifier and instead saw '}')
    git-tfs-id: [http://developmentvlan:8585/tfs/vanrise.collection]$/;C36961

--- TOneV2/Code/TOneV2/TOne.WhS.CodePreparation.Web/WhS_CodePreparation/Constant/CodeChangeTypeEnum.js
+++ TOneV2/Code/TOneV2/TOne.WhS.CodePreparation.Web/WhS_CodePreparation/Constant/CodeChangeTypeEnum.js
@@ -6,5 +6,5 @@
     PendingEffective: { value: 4, label: "Pending Effective", icon: "Client/Modules/WhS_BusinessEntity/Images/PendingEffective.png" },
     PendingClosed: { value: 5, label: "Pending Closed", icon: "Client/Modules/WhS_BusinessEntity/Images/PendingClosed.png" },
     MovedTo: { label: "Moved to", icon: "Client/Modules/WhS_BusinessEntity/Images/MovedTo.png" },
-    MovedFrom: { label: "Moved from", icon: "Client/Modules/WhS_BusinessEntity/Images/MovedFrom.png" },
+    MovedFrom: { label: "Moved from", icon: "Client/Modules/WhS_BusinessEntity/Images/MovedFrom.png" }
 });
--- TOneV2/Code/TOneV2/TOne.WhS.CodePreparation.Web/WhS_CodePreparation/Constant/ZoneChangeTypeEnum.js
+++ TOneV2/Code/TOneV2/TOne.WhS.CodePreparation.Web/WhS_CodePreparation/Constant/ZoneChangeTypeEnum.js
@@ -4,5 +4,5 @@
     Closed: { value: 2, label: "Closed", icon: "Client/Modules/WhS_BusinessEntity/Images/Closed.png" },
     Renamed: { value: 3, label: "Renamed", icon: "Client/Modules/WhS_BusinessEntity/Images/Renamed.png" },
     PendingEffective: { value: 4, label: "Pending Effective", icon: "Client/Modules/WhS_BusinessEntity/Images/PendingEffective.png" },
-    PendingClosed: { value: 5, label: "Pending Closed", icon: "Client/Modules/WhS_BusinessEntity/Images/PendingClosed.png" },
+    PendingClosed: { value: 5, label: "Pending Closed", icon: "Client/Modules/WhS_BusinessEntity/Images/PendingClosed.png" }
 });
