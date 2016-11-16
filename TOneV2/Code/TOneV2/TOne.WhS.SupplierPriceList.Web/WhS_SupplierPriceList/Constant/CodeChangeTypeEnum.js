e34248bf20d06d3b468db493375b8711a03b6cc2
Wissam Ajamy <VANRISE\wissam.ajamy>
2016-11-16 13:35:17 +0000

    Remove last comma from functions and arrays (JSLint : Expected an identifier and instead saw '}')
    git-tfs-id: [http://developmentvlan:8585/tfs/vanrise.collection]$/;C37010

--- TOneV2/Code/TOneV2/TOne.WhS.SupplierPriceList.Web/WhS_SupplierPriceList/Constant/CodeChangeTypeEnum.js
+++ TOneV2/Code/TOneV2/TOne.WhS.SupplierPriceList.Web/WhS_SupplierPriceList/Constant/CodeChangeTypeEnum.js
@@ -4,6 +4,6 @@
     Deleted: { value: 2, description: "Deleted", icon: "Client/Modules/WhS_BusinessEntity/Images/Closed.png" },
     Moved: { value: 3, description: "Moved" },
     MovedFrom: { value: 4, description: "Moved From", icon: "Client/Modules/WhS_BusinessEntity/Images/MovedFrom.png" },
-    MovedTo: { value: 4, description: "Moved To", icon: "Client/Modules/WhS_BusinessEntity/Images/MovedTo.png" },
+    MovedTo: { value: 4, description: "Moved To", icon: "Client/Modules/WhS_BusinessEntity/Images/MovedTo.png" }
 
 });
--- TOneV2/Code/TOneV2/TOne.WhS.SupplierPriceList.Web/WhS_SupplierPriceList/Constant/ZoneChangeTypeEnum.js
+++ TOneV2/Code/TOneV2/TOne.WhS.SupplierPriceList.Web/WhS_SupplierPriceList/Constant/ZoneChangeTypeEnum.js
@@ -3,6 +3,6 @@
     New: { value: 1, description: "New", icon: "Client/Modules/WhS_BusinessEntity/Images/New.png" },
     Deleted: { value: 2, description: "Deleted", icon: "Client/Modules/WhS_BusinessEntity/Images/Closed.png" },
     ReOpened: { value: 3, description: "ReOpened", icon: "Client/Modules/WhS_BusinessEntity/Images/ReOpen.png" },
-    Renamed: { value: 4, description: "Renamed", icon: "Client/Modules/WhS_BusinessEntity/Images/Renamed.png" },
+    Renamed: { value: 4, description: "Renamed", icon: "Client/Modules/WhS_BusinessEntity/Images/Renamed.png" }
 
 });
