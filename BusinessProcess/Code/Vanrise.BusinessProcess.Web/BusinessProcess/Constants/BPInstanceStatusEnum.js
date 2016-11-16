10497ffdbf51b51e9442563d653afa49c2439e3b
Wissam Ajamy <VANRISE\wissam.ajamy>
2016-11-16 14:29:14 +0000

    Remove last comma from functions and arrays (JSLint : Expected an identifier and instead saw '}')
    git-tfs-id: [http://developmentvlan:8585/tfs/vanrise.collection]$/;C37016

--- BusinessProcess/Code/Vanrise.BusinessProcess.Web/BusinessProcess/Constants/BPInstanceStatusEnum.js
+++ BusinessProcess/Code/Vanrise.BusinessProcess.Web/BusinessProcess/Constants/BPInstanceStatusEnum.js
@@ -5,5 +5,5 @@
     Completed: { value: 50, description: "Completed", isOpened: false },
     Aborted: { value: 60, description: "Aborted", isOpened: false },
     Suspended: { value: 70, description: "Suspended", isOpened: false },
-    Terminated: { value: 80, description: "Terminated", isOpened: false },
+    Terminated: { value: 80, description: "Terminated", isOpened: false }
 });
--- BusinessProcess/Code/Vanrise.BusinessProcess.Web/BusinessProcess/Constants/CreateProcessResultEnum.js
+++ BusinessProcess/Code/Vanrise.BusinessProcess.Web/BusinessProcess/Constants/CreateProcessResultEnum.js
@@ -1,4 +1,4 @@
 ﻿app.constant('WhS_BP_CreateProcessResultEnum', {
     Succeeded: { value: 0, description: "Succeeded" },
-    Failed: { value: 1, description: "Failed" },
+    Failed: { value: 1, description: "Failed" }
 });
