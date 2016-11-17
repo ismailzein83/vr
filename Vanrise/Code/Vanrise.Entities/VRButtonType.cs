0dc6ca5e31bf9d7434d42e460295d99bd487b202
Samer Haidar <VANRISE\samer.haidar>
2016-11-17 13:56:28 +0000

    adding VRButtonType
    git-tfs-id: [http://developmentvlan:8585/tfs/vanrise.collection]$/;C37051

---
+++ Vanrise/Code/Vanrise.Entities/VRButtonType.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Vanrise.Entities
+{
+    public enum VRButtonType
+    {
+        Start = 0,
+        Reset = 1,
+        Search = 2,
+        Add = 3,
+        Edit= 4,
+        Remove = 5,
+        Save = 6,
+        Yes = 7,
+        Close = 8,
+        Cancel = 9,
+        CancelChanges = 10,
+        CancelAll = 11,
+        CancelSelected =12,
+        No = 13,
+        Login = 14,
+        BreakInheritance = 15,
+        AllowInheritance=16,
+        ApplyChanges = 17,
+        AssignOrgChart = 18,
+        AssignOperators = 19,
+        AssignCarriers = 20,
+        Validate = 21,
+        Upload = 22,
+        SellNewCountries = 23,
+        Download =24,
+        Settings = 25,
+        Move = 26,
+        Ranking = 27,
+        Pricing = 28,
+        Apply = 29,
+        Split = 30,
+        Merge =31,
+        Compile =32,
+        SelectAll = 33,
+        DeselectAll = 34,
+        Export = 35,
+        Continue = 36,
+        Stop = 37,
+        End = 38,
+        Ok =39,
+        Rename = 40,
+        Help = 41,
+        Next = 42,
+        Back = 43,
+        Convert = 44,
+        TestConversion = 45,
+        GenerateInvoice = 46,
+        Analyze = 47,
+        Preview = 48,
+        SendEmail = 49,
+        ExcludeAll = 50,
+        Load = 51,
+        Run = 52,
+        CalculateRate = 53,
+        Skip = 54
+    }
+}
