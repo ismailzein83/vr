baffe331166eacd91c4b77d32eb1edba1f213b96
Wissam Ajamy <VANRISE\wissam.ajamy>
2016-11-17 12:22:37 +0000

    Update structure from TOneConfiguration
    git-tfs-id: [http://developmentvlan:8585/tfs/vanrise.collection]$/;C37049

---
+++ TOneV2/Code/TOneV2/SQL.TOneConfiguration/VR_Invoice/Stored Procedures/sp_Invoice_CheckOverlaping.sql
@@ -0,0 +1,17 @@
+﻿-- =============================================
+-- Author:		<Author,,Name>
+-- Create date: <Create Date,,>
+-- Description:	<Description,,>
+-- =============================================
+CREATE PROCEDURE [VR_Invoice].sp_Invoice_CheckOverlaping
+		@InvoiceTypeId uniqueidentifier,
+		@PartnerId varchar(50),
+		@FromDate Datetime,
+		@ToDate Datetime
+AS
+BEGIN
+	SELECT	Count(*) as CountNb
+	FROM	VR_Invoice.Invoice with(nolock)
+	where	PartnerId = @PartnerId AND InvoiceTypeId = @InvoiceTypeId AND
+	(ToDate >= @FromDate AND  @ToDate>=FromDate)
+END
--- TOneV2/Code/TOneV2/SQL.TOneConfiguration/genericdata/Tables/DataRecordStorage.sql
+++ TOneV2/Code/TOneV2/SQL.TOneConfiguration/genericdata/Tables/DataRecordStorage.sql
@@ -2,16 +2,18 @@
     [ID]                  UNIQUEIDENTIFIER NOT NULL,
     [OldID]               INT              NULL,
     [Name]                NVARCHAR (1000)  NOT NULL,
-    [OldDataRecordTypeID] INT              NOT NULL,
-    [DataRecordTypeID]    UNIQUEIDENTIFIER NULL,
-    [DataStoreID]         UNIQUEIDENTIFIER NULL,
-    [OldDataStoreID]      INT              NOT NULL,
+    [OldDataRecordTypeID] INT              NULL,
+    [DataRecordTypeID]    UNIQUEIDENTIFIER NOT NULL,
+    [DataStoreID]         UNIQUEIDENTIFIER NOT NULL,
+    [OldDataStoreID]      INT              NULL,
     [Settings]            NVARCHAR (MAX)   NOT NULL,
     [State]               NVARCHAR (MAX)   NULL,
     [CreatedTime]         DATETIME         CONSTRAINT [DF_DataRecordStorage_CreatedTime] DEFAULT (getdate()) NULL,
     [timestamp]           ROWVERSION       NULL,
     CONSTRAINT [PK_DataRecordStorage] PRIMARY KEY CLUSTERED ([ID] ASC)
 );
+
+
 
 
 
