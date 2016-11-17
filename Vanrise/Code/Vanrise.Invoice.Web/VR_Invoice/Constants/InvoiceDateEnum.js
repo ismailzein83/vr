16712b9ac34d9de41f4325b0376c330ea3d107bd
Samer Haidar <VANRISE\samer.haidar>
2016-11-17 07:33:48 +0000

    Adding file needed to source control
    git-tfs-id: [http://developmentvlan:8585/tfs/vanrise.collection]$/;C37035

---
+++ Vanrise/Code/Vanrise.Invoice.Web/VR_Invoice/Constants/InvoiceDateEnum.js
@@ -0,0 +1,6 @@
+﻿app.constant('VR_Invoice_InvoiceDateEnum', {
+    FromDate: { value: 0, description: "FromDate" },
+    ToDate: { value: 1, description: "ToDate" },
+    IssueDate: { value: 2, description: "IssueDate" },
+    DueDate: { value: 3, description: "DueDate" },
+});
