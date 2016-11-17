ea5e5c8631a021f75bbf2eeaf4d9c72955f48df1
Wissam Ajamy <VANRISE\wissam.ajamy>
2016-11-17 09:40:07 +0000

    Remove last comma from functions and arrays (JSLint : Expected an identifier and instead saw '}')
    git-tfs-id: [http://developmentvlan:8585/tfs/vanrise.collection]$/;C37044

--- Vanrise/Code/Vanrise.Common.Web/Common/Constant/ProductInfoPropertyEnum.js
+++ Vanrise/Code/Vanrise.Common.Web/Common/Constant/ProductInfoPropertyEnum.js
@@ -1,4 +1,4 @@
 ﻿app.constant('VRCommon_ProductInfoPropertyEnum', {
     ProductName: { value: 0, description: "Product Name" },
-    VersionNumber: { value: 1, description: "Version Number" },
+    VersionNumber: { value: 1, description: "Version Number" }
 });
