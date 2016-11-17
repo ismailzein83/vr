0d7bdb27f4df99d107e6a547ca09a1b0095ed244
Wissam Ajamy <VANRISE\wissam.ajamy>
2016-11-17 15:18:16 +0000

    Update structure from Mediation_Dev
    git-tfs-id: [http://developmentvlan:8585/tfs/vanrise.collection]$/;C37063

--- Mediation/Code/Mediation/Mediation_Generic/Tables/CookedCDR.sql
+++ Mediation/Code/Mediation/Mediation_Generic/Tables/CookedCDR.sql
@@ -1,21 +1,27 @@
 ﻿CREATE TABLE [Mediation_Generic].[CookedCDR] (
+    [CDRID]                BIGINT          IDENTITY (1, 1) NOT NULL,
     [CallId]               NVARCHAR (100)  NULL,
     [ConnectDateTime]      DATETIME        NULL,
     [DisconnectDateTime]   DATETIME        NULL,
     [DisconnectReason]     NVARCHAR (100)  NULL,
     [CallProgressState]    NVARCHAR (100)  NULL,
     [Account]              NVARCHAR (100)  NULL,
-    [OriginatorId]         NVARCHAR (100)  NULL,
+    [OriginatorId]         NVARCHAR (500)  NULL,
     [OriginatorNumber]     NVARCHAR (100)  NULL,
     [OriginatorFromNumber] NVARCHAR (100)  NULL,
     [OriginalDialedNumber] NVARCHAR (100)  NULL,
-    [TerminatorId]         NVARCHAR (100)  NULL,
+    [TerminatorId]         NVARCHAR (500)  NULL,
     [TerminatorNumber]     NVARCHAR (100)  NULL,
     [IncomingGwId]         NVARCHAR (100)  NULL,
     [OutgoingGwId]         NVARCHAR (100)  NULL,
     [TransferredCallId]    NVARCHAR (100)  NULL,
-    [DurationInSeconds]    DECIMAL (10, 4) NULL
+    [OriginatorIP]         NVARCHAR (50)   NULL,
+    [TerminatorIP]         NVARCHAR (50)   NULL,
+    [DurationInSeconds]    DECIMAL (10, 4) NULL,
+    CONSTRAINT [PK_CookedCDR] PRIMARY KEY CLUSTERED ([CDRID] ASC)
 );
+
+
 
 
 
--- Mediation/Code/Mediation/Mediation_Generic/User Defined Types/CookedCDRType.sql
+++ Mediation/Code/Mediation/Mediation_Generic/User Defined Types/CookedCDRType.sql
@@ -5,16 +5,20 @@
     [DisconnectReason]     NVARCHAR (100)  NULL,
     [CallProgressState]    NVARCHAR (100)  NULL,
     [Account]              NVARCHAR (100)  NULL,
-    [OriginatorId]         NVARCHAR (100)  NULL,
+    [OriginatorId]         NVARCHAR (500)  NULL,
     [OriginatorNumber]     NVARCHAR (100)  NULL,
     [OriginatorFromNumber] NVARCHAR (100)  NULL,
     [OriginalDialedNumber] NVARCHAR (100)  NULL,
-    [TerminatorId]         NVARCHAR (100)  NULL,
+    [TerminatorId]         NVARCHAR (500)  NULL,
     [TerminatorNumber]     NVARCHAR (100)  NULL,
     [IncomingGwId]         NVARCHAR (100)  NULL,
     [OutgoingGwId]         NVARCHAR (100)  NULL,
     [TransferredCallId]    NVARCHAR (100)  NULL,
+    [OriginatorIP]         NVARCHAR (50)   NULL,
+    [TerminatorIP]         NVARCHAR (50)   NULL,
     [DurationInSeconds]    DECIMAL (10, 4) NULL);
+
+
 
 
 
