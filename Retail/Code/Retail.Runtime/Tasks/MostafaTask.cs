5c67f2098693533ffcc15433ca474c5e3099971f
mostafa.jawhari <mostafa.jawhari@vanrise.tfs.local>
2016-11-21 13:21:49 +0000

    adding NumberingPlanKey
    git-tfs-id: [http://developmentvlan:8585/tfs/vanrise.collection]$/;C37106

---
+++ Retail/Code/Retail.Runtime/Tasks/MostafaTask.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Vanrise.BusinessProcess;
+using Vanrise.Runtime;
+
+namespace Retail.Runtime.Tasks
+{
+    class MostafaTask : ITask
+    {
+        public void Execute()
+        {
+            System.Threading.ThreadPool.SetMaxThreads(10000, 10000);
+
+            BusinessProcessService bpService = new BusinessProcessService() { Interval = new TimeSpan(0, 0, 2) };
+            BPRegulatorRuntimeService regulatorRuntimeService = new BPRegulatorRuntimeService() { Interval = new TimeSpan(0, 0, 2) };
+            TransactionLockRuntimeService transactionLockRuntimeService = new Vanrise.Runtime.TransactionLockRuntimeService() { Interval = new TimeSpan(0, 0, 2) };
+            var runtimeServices = new List<RuntimeService>();
+
+            runtimeServices.Add(bpService);
+            runtimeServices.Add(transactionLockRuntimeService);
+            runtimeServices.Add(regulatorRuntimeService);
+
+            RuntimeHost host = new RuntimeHost(runtimeServices);
+            host.Start();
+
+        }
+
+    }
+}
