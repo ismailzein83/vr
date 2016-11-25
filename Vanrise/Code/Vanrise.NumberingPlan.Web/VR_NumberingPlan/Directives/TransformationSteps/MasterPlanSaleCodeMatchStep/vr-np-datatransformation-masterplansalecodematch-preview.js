e18ff22bedc2a12f9d7a11acb13b88489a657a34
Ali Atoui <VANRISE\ali.atoui>
2016-11-25 09:24:33 +0000

    Adding MasterPlanCodeMatchStep UIs
    git-tfs-id: [http://developmentvlan:8585/tfs/vanrise.collection]$/;C37167

---
+++ Vanrise/Code/Vanrise.NumberingPlan.Web/VR_NumberingPlan/Directives/TransformationSteps/MasterPlanSaleCodeMatchStep/vr-np-datatransformation-masterplansalecodematch-preview.js
@@ -0,0 +1,156 @@
+﻿'use strict';
+
+app.directive('vrNpDatatransformationMasterplansalecodematchPreview', ['UtilsService', 'VRUIUtilsService',
+    function (UtilsService, VRUIUtilsService) {
+
+        var directiveDefinitionObject = {
+            restrict: 'E',
+            scope: {
+                onReady: '='
+            },
+            controller: function ($scope, $element, $attrs) {
+
+                var ctrl = this;
+
+                var ctor = new MasterPlanSaleCodeMatchStepPreviewCtor(ctrl, $scope);
+                ctor.initializeController();
+
+            },
+            controllerAs: 'ctrl',
+            bindToController: true,
+            compile: function (element, attrs) {
+                return {
+                    pre: function ($scope, iElem, iAttrs, ctrl) {
+
+                    }
+                };
+            },
+            templateUrl: function (element, attrs) {
+                return "/Client/Modules/VR_NumberingPlan/Directives/TransformationSteps/MasterPlanSaleCodeMatchStep/Templates/MasterPlanSaleCodeMatchStepPreviewTemplate.html";
+            }
+
+        };
+
+        function MasterPlanSaleCodeMatchStepPreviewCtor(ctrl, $scope) {
+            var stepObj = {};
+
+            function initializeController() {
+                ctrl.inputFields = [];
+                ctrl.outputFields = [];
+
+                defineAPI();
+            }
+            function defineAPI() {
+                var api = {};
+
+                api.load = function (payload) {
+                    ctrl.inputFields = [];
+                    ctrl.outputFields = [];
+
+                    if (payload != undefined) {
+                        if (payload.stepDetails != undefined) {
+                            stepObj.stepDetails = payload.stepDetails;
+
+                            fillInputFieldsArray(payload.stepDetails, ctrl.inputFields);
+                            fillOutputFieldsArray(payload.stepDetails, ctrl.outputFields);
+                        }
+                        checkValidation();
+                    }
+
+                };
+
+                api.applyChanges = function (changes) {
+                    ctrl.inputFields = [];
+                    ctrl.outputFields = [];
+
+                    fillInputFieldsArray(changes, ctrl.inputFields);
+                    fillOutputFieldsArray(changes, ctrl.outputFields);
+
+                    stepObj.stepDetails = changes;
+                };
+
+                api.checkValidation = function () {
+
+                    return checkValidation();
+                };
+
+                api.getData = function () {
+                    return stepObj.stepDetails;
+                };
+
+                if (ctrl.onReady != null)
+                    ctrl.onReady(api);
+            }
+
+            function checkValidation() {
+
+                var misisngFieldsArray = [];
+
+                for (var index = 0 ; index < ctrl.inputFields.length; index++) {
+                    if (ctrl.inputFields[index].Value == undefined) {
+                        misisngFieldsArray.push(ctrl.inputFields[index].FieldName);
+                    }
+                }
+
+                for (var index = 0 ; index < ctrl.outputFields.length; index++) {
+                    if (ctrl.outputFields[index].FieldName == undefined) {
+                        misisngFieldsArray.push(ctrl.outputFields[index].Value);
+                    }
+                }
+
+                if (misisngFieldsArray.length == 1)
+                    return "Field " + misisngFieldsArray[0] + " is required!!";
+
+                if (misisngFieldsArray.length > 1)
+                    return "Fields " + misisngFieldsArray.join(", ") + " are required!!";
+
+                return null;
+            }
+            function fillInputFieldsArray(data, array) {
+
+                if (data.Number)
+                {
+                    array.push({
+                        FieldName: "Number",
+                        Value: data.Number
+                    });
+                }
+                if (data.EffectiveOn)
+                {
+                    array.push({
+                        FieldName: "Effective On",
+                        Value: data.EffectiveOn
+                    });
+                }
+            }
+            function fillOutputFieldsArray(data, array) {
+
+                if (data.SaleCode)
+                {
+                    array.push({
+                        FieldName: data.SaleCode,
+                        Value: "Sale Code"
+                    });
+                }
+                if (data.SaleZoneId)
+                {
+                    array.push({
+                        FieldName: data.SaleZoneId,
+                        Value: "Sale Zone Id"
+                    });
+                }
+                if (data.SellingNumberPlanId)
+                {
+                    array.push({
+                        FieldName: data.SellingNumberPlanId,
+                        Value: "Selling Number Plan Id"
+                    });
+                }
+            }
+
+            this.initializeController = initializeController;
+        }
+
+        return directiveDefinitionObject;
+    }
+]);
---
+++ Vanrise/Code/Vanrise.NumberingPlan.Web/VR_NumberingPlan/Directives/TransformationSteps/MasterPlanSaleCodeMatchStep/vr-np-datatransformation-masterplansalecodematch.js
@@ -0,0 +1,196 @@
+﻿'use strict';
+
+app.directive('vrNpDatatransformationMasterplansalecodematch', ['UtilsService', 'VRUIUtilsService',
+    function (UtilsService, VRUIUtilsService) {
+
+        var directiveDefinitionObject = {
+            restrict: 'E',
+            scope:
+            {
+                onReady: '='
+            },
+            controller: function ($scope, $element, $attrs) {
+
+                var ctrl = this;
+
+                var ctor = new MasterPlanSaleCodeMatchStepCtor(ctrl, $scope);
+                ctor.initializeController();
+            },
+            controllerAs: 'ctrl',
+            bindToController: true,
+            compile: function (element, attrs) {
+                return {
+                    pre: function ($scope, iElem, iAttrs, ctrl) {
+
+                    }
+                };
+            },
+            templateUrl: function (element, attrs) {
+                return "/Client/Modules/VR_NumberingPlan/Directives/TransformationSteps/MasterPlanSaleCodeMatchStep/Templates/MasterPlanSaleCodeMatchStepTemplate.html";
+            }
+
+        };
+
+        function MasterPlanSaleCodeMatchStepCtor(ctrl, $scope) {
+            var stepPayload;
+
+            var numberDirectiveReadyAPI;
+            var numberDirectiveReadyPromiseDeferred = UtilsService.createPromiseDeferred();
+
+            var effectiveOnDirectiveReadyAPI;
+            var effectiveOnDirectiveReadyPromiseDeferred = UtilsService.createPromiseDeferred();
+
+            var saleCodeDirectiveReadyAPI;
+            var saleCodeDirectiveReadyPromiseDeferred = UtilsService.createPromiseDeferred();
+
+            var saleZoneIdDirectiveReadyAPI;
+            var saleZoneIdDirectiveReadyPromiseDeferred = UtilsService.createPromiseDeferred();
+
+            var sellingNumberPlanIdDirectiveReadyAPI;
+            var sellingNumberPlanIdDirectiveReadyPromiseDeferred = UtilsService.createPromiseDeferred();
+
+            function initializeController() {
+                $scope.scopeModel = {};
+
+                $scope.scopeModel.onNumberReady = function (api) {
+                    numberDirectiveReadyAPI = api;
+                    numberDirectiveReadyPromiseDeferred.resolve();
+                };
+                $scope.scopeModel.onEffectiveOnReady = function (api) {
+                    effectiveOnDirectiveReadyAPI = api;
+                    effectiveOnDirectiveReadyPromiseDeferred.resolve();
+                };
+                $scope.scopeModel.onSaleCodeReady = function (api) {
+                    saleCodeDirectiveReadyAPI = api;
+                    saleCodeDirectiveReadyPromiseDeferred.resolve();
+                };
+                $scope.scopeModel.onSaleZoneIdReady = function (api) {
+                    saleZoneIdDirectiveReadyAPI = api;
+                    saleZoneIdDirectiveReadyPromiseDeferred.resolve();
+                };
+                $scope.scopeModel.onSellingNumberPlanIdReady = function (api) {
+                    sellingNumberPlanIdDirectiveReadyAPI = api;
+                    sellingNumberPlanIdDirectiveReadyPromiseDeferred.resolve();
+                };
+
+                defineAPI();
+            }
+            function defineAPI() {
+                var api = {};
+
+                api.load = function (payload) {
+                    var promises = [];
+
+                    stepPayload = payload;
+
+                    //Loading Number Directive
+                    var loadNumberDirectivePromiseDeferred = getLoadNumberDirectivePromiseDeferred();
+                    promises.push(loadNumberDirectivePromiseDeferred.promise);
+
+                    //Loading EffectiveOn Directive
+                    var loadEffectiveOnDirectivePromiseDeferred = getLoadEffectiveOnDirectivePromiseDeferred();
+                    promises.push(loadEffectiveOnDirectivePromiseDeferred.promise);
+
+                    //Loading SaleCode Directive
+                    var loadSaleCodeDirectivePromiseDeferred = getLoadSaleCodeDirectivePromiseDeferred();
+                    promises.push(loadSaleCodeDirectivePromiseDeferred.promise);
+
+                    //Loading SaleZoneId Directive
+                    var loadSaleZoneIdDirectivePromiseDeferred = getLoadSaleZoneIdDirectivePromiseDeferred();
+                    promises.push(loadSaleZoneIdDirectivePromiseDeferred.promise);
+
+                    //Loading SaleZoneId Directive
+                    var loadSellingNumberPlanIdDirectivePromiseDeferred = getLoadSellingNumberPlanIdDirectivePromiseDeferred();
+                    promises.push(loadSellingNumberPlanIdDirectivePromiseDeferred.promise);
+
+
+                    function getLoadNumberDirectivePromiseDeferred() {
+                        var loadNumberDirectivePromiseDeferred = UtilsService.createPromiseDeferred();
+                        numberDirectiveReadyPromiseDeferred.promise.then(function () {
+
+                            var numberPayload = { context: payload.context };
+                            if (payload.stepDetails != undefined)
+                                numberPayload.selectedRecords = payload.stepDetails.Number;
+
+                            VRUIUtilsService.callDirectiveLoad(numberDirectiveReadyAPI, numberPayload, loadNumberDirectivePromiseDeferred);
+                        });
+
+                        return loadNumberDirectivePromiseDeferred;
+                    }
+                    function getLoadEffectiveOnDirectivePromiseDeferred() {
+                        var loadEffectiveOnDirectivePromiseDeferred = UtilsService.createPromiseDeferred();
+                        effectiveOnDirectiveReadyPromiseDeferred.promise.then(function () {
+
+                            var effectiveOnPayload = { context: payload.context };
+                            if (payload.stepDetails != undefined)
+                                effectiveOnPayload.selectedRecords = payload.stepDetails.EffectiveOn;
+
+                            VRUIUtilsService.callDirectiveLoad(effectiveOnDirectiveReadyAPI, effectiveOnPayload, loadEffectiveOnDirectivePromiseDeferred);
+                        });
+
+                        return loadEffectiveOnDirectivePromiseDeferred;
+                    }
+                    function getLoadSaleCodeDirectivePromiseDeferred() {
+                        var loadSaleCodeDirectivePromiseDeferred = UtilsService.createPromiseDeferred();
+                        saleCodeDirectiveReadyPromiseDeferred.promise.then(function () {
+
+                            var saleCodePayload = { context: payload.context };
+                            if (payload.stepDetails != undefined)
+                                saleCodePayload.selectedRecords = payload.stepDetails.SaleCode;
+
+                            VRUIUtilsService.callDirectiveLoad(saleCodeDirectiveReadyAPI, saleCodePayload, loadSaleCodeDirectivePromiseDeferred);
+                        });
+
+                        return loadSaleCodeDirectivePromiseDeferred;
+                    }
+                    function getLoadSaleZoneIdDirectivePromiseDeferred() {
+                        var loadSaleZoneIdDirectivePromiseDeferred = UtilsService.createPromiseDeferred();
+                        saleZoneIdDirectiveReadyPromiseDeferred.promise.then(function () {
+
+                            var saleZoneIdPayload = { context: payload.context };
+                            if (payload.stepDetails != undefined)
+                                saleZoneIdPayload.selectedRecords = payload.stepDetails.SaleZoneId;
+
+                            VRUIUtilsService.callDirectiveLoad(saleZoneIdDirectiveReadyAPI, saleZoneIdPayload, loadSaleZoneIdDirectivePromiseDeferred);
+                        });
+
+                        return loadSaleZoneIdDirectivePromiseDeferred;
+                    }
+                    function getLoadSellingNumberPlanIdDirectivePromiseDeferred() {
+                        var loadSellingNumberPlanIdDirectivePromiseDeferred = UtilsService.createPromiseDeferred();
+                        sellingNumberPlanIdDirectiveReadyPromiseDeferred.promise.then(function () {
+
+                            var sellingNumberPlanIdPayload = { context: payload.context };
+                            if (payload.stepDetails != undefined)
+                                sellingNumberPlanIdPayload.selectedRecords = payload.stepDetails.SellingNumberPlanId;
+
+                            VRUIUtilsService.callDirectiveLoad(sellingNumberPlanIdDirectiveReadyAPI, sellingNumberPlanIdPayload, loadSellingNumberPlanIdDirectivePromiseDeferred);
+                        });
+
+                        return loadSellingNumberPlanIdDirectivePromiseDeferred;
+                    }
+
+                    return UtilsService.waitMultiplePromises(promises);
+                };
+
+                api.getData = function () {
+                    return {
+                        $type: "Vanrise.NumberingPlan.Business.TransformationSteps.MasterPlanSaleCodeMatchStep, Vanrise.NumberingPlan.Business",
+                        Number: numberDirectiveReadyAPI.getData(),
+                        EffectiveOn: effectiveOnDirectiveReadyAPI.getData(),
+                        SaleCode: saleCodeDirectiveReadyAPI.getData(),
+                        SaleZoneId: saleZoneIdDirectiveReadyAPI.getData(),
+                        SellingNumberPlanId: sellingNumberPlanIdDirectiveReadyAPI.getData()
+                    };
+                };
+
+                if (ctrl.onReady != null)
+                    ctrl.onReady(api);
+            }
+
+            this.initializeController = initializeController;
+        }
+
+        return directiveDefinitionObject;
+    }
+]);
