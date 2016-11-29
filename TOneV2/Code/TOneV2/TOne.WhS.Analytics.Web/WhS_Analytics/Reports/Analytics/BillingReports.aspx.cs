c1443c7db0700644367005dda6526dda3425668e
hadi.hawi <hadi.hawi@vanrise.tfs.local>
2016-11-29 16:15:41 +0000

    Bug 1869, add condition if from=to add one day to "ToDate"
    git-tfs-id: [http://developmentvlan:8585/tfs/vanrise.collection]$/;C37261

--- TOneV2/Code/TOneV2/TOne.WhS.Analytics.Web/WhS_Analytics/Reports/Analytics/BillingReports.aspx.cs
+++ TOneV2/Code/TOneV2/TOne.WhS.Analytics.Web/WhS_Analytics/Reports/Analytics/BillingReports.aspx.cs
@@ -58,7 +58,8 @@ namespace TOne.WhS.Analytics.Web.Reports.Analytics
                     ReportParameters parameters = new ReportParameters();
 
                     parameters.FromTime = from;
-                    parameters.ToTime = to;
+                    parameters.ToTime = (from == to) ? to.Value.AddDays(1) : to;
+
                     parameters.GroupByCustomer = groupByCustomer;
                     parameters.CustomersId = customers;
                     parameters.SuppliersId = suppliers;
