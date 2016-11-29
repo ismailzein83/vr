40e428b3a0dcf1e6ab5ca3447e8c9eec6c4f78f9
Samer Haidar <VANRISE\samer.haidar>
2016-11-29 07:03:31 +0000

    Implementing send email action
    git-tfs-id: [http://developmentvlan:8585/tfs/vanrise.collection]$/;C37226

---
+++ TOneV2/Code/TOneV2/TOne.WhS.Invoice.Business/ConfigManager.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TOne.WhS.Invoice.Entities;
+using Vanrise.Common.Business;
+
+namespace TOne.WhS.Invoice.Business
+{
+    public class ConfigManager
+    {
+        #region Public Methods
+        public Guid GetDefaultCustomerInvoiceTemplateMessageId()
+        {
+            InvoiceSettings setting = GetInvoiceSettings();
+            if (setting.CustomerInvoiceSettings == null)
+                throw new NullReferenceException("setting.CustomerInvoiceSettings");
+            return setting.CustomerInvoiceSettings.DefaultEmailId;
+        }
+        #endregion
+     
+        #region Private Methods
+        private InvoiceSettings GetInvoiceSettings()
+        {
+            SettingManager settingManager = new SettingManager();
+            InvoiceSettings invoiceSettings = settingManager.GetSetting<InvoiceSettings>(InvoiceSettings.SETTING_TYPE);
+
+            if (invoiceSettings == null)
+                throw new NullReferenceException("invoiceSettings");
+
+            return invoiceSettings;
+        }
+
+        #endregion
+
+    }
+}
--- TOneV2/Code/TOneV2/TOne.WhS.Invoice.Business/Extensions/CarrierInvoiceSettings.cs
+++ TOneV2/Code/TOneV2/TOne.WhS.Invoice.Business/Extensions/CarrierInvoiceSettings.cs
@@ -4,6 +4,9 @@ using System.Linq;
 using System.Net.Mail;
 using System.Text;
 using System.Threading.Tasks;
+using TOne.WhS.Invoice.Entities;
+using Vanrise.Common.Business;
+using Vanrise.Entities;
 using Vanrise.Invoice.Entities;
 
 namespace TOne.WhS.Invoice.Business.Extensions
@@ -28,9 +31,19 @@ namespace TOne.WhS.Invoice.Business.Extensions
 
         public override dynamic GetInfo(IInvoiceTypeExtendedSettingsInfoContext context)
         {
+
+           
             switch(context.InfoType)
             {
-                
+                case "CustomerMailTemplate":
+                     ConfigManager manager = new ConfigManager();
+                    var invoiceTemplateId = manager.GetDefaultCustomerInvoiceTemplateMessageId();
+                    VRMailManager vrMailManager = new VRMailManager();
+                    Dictionary<string, dynamic> objects = new Dictionary<string, dynamic>();
+                    var invoiceDetails = context.Invoice.Details as CustomerInvoiceDetails;
+                    objects.Add("CustomerInvoice", invoiceDetails);
+                    VRMailEvaluatedTemplate template = vrMailManager.EvaluateMailTemplate(invoiceTemplateId, objects);
+                    return template;
             }
             return null;
         }
