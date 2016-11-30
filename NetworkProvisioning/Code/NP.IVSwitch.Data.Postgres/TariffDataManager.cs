944b8efb3de5fedcfdf2c088de84a68fdd4207ed
kawthar.alali <kawthar.alali@vanrise.tfs.local>
2016-11-30 11:25:46 +0000

    Updated Tariff Data Manager
    git-tfs-id: [http://developmentvlan:8585/tfs/vanrise.collection]$/;C37272

--- NetworkProvisioning/Code/NP.IVSwitch.Data.Postgres/TariffDataManager.cs
+++ NetworkProvisioning/Code/NP.IVSwitch.Data.Postgres/TariffDataManager.cs
@@ -36,14 +36,7 @@ namespace NP.IVSwitch.Data.Postgres
                                                     WITH (
                                                       OIDS=FALSE
                                                     );",tariffId.ToString()),
-                                        string.Format("ALTER TABLE  trf{0}  OWNER TO zeinab;",tariffId.ToString())
-//                                        string.Format(@"INSERT INTO trf{0} (dest_code, time_frame,dest_name,init_period ,next_period,init_charge,next_charge) 
-//                                        SELECT  dest_code,time_frame,dest_name,init_period,next_period,init_charge,next_charge
-//                                        FROM  	unnest(ARRAY[0,1,2,3,4,5,6,7,8,9]) dest_code, 
-//                                                unnest(ARRAY['* * * * *']) time_frame,
-//                                                ARRAY_TO_STRING(ARRAY[NULL, dest_code],' ','GLobal ' ) dest_name,
-//                                                unnest(ARRAY[1]) init_period,unnest(ARRAY[1]) next_period,unnest(ARRAY[0.0]) init_charge,
-//                                                unnest(ARRAY[0.0]) next_charge",insertedId.ToString()),
+                                        string.Format("ALTER TABLE  trf{0}  OWNER TO zeinab;",tariffId.ToString()) 
 
                                     };
 
