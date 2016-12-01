58621e6ca554ae67f93c58104eeea3e5cb50b2c7
mohamad.elhariri <mohamad.elhariri@vanrise.tfs.local>
2016-12-01 16:24:50 +0000

    Refactored the Business Rule
    git-tfs-id: [http://developmentvlan:8585/tfs/vanrise.collection]$/;C37343

--- TOneV2/Code/TOneV2/TOne.WhS.Sales.Business/BusinessRules/Conditions/SellingProductNormalRateDoesNotExistCondition.cs
+++ TOneV2/Code/TOneV2/TOne.WhS.Sales.Business/BusinessRules/Conditions/SellingProductNormalRateDoesNotExistCondition.cs
@@ -9,7 +9,7 @@ using TOne.WhS.Sales.Entities;
 
 namespace TOne.WhS.Sales.Business.BusinessRules
 {
-	public class MissingSellingProductZoneRateCondition : Vanrise.BusinessProcess.Entities.BusinessRuleCondition
+	public class SellingProductNormalRateDoesNotExistCondition : Vanrise.BusinessProcess.Entities.BusinessRuleCondition
 	{
 		public override bool ShouldValidate(Vanrise.BusinessProcess.Entities.IRuleTarget target)
 		{
@@ -35,24 +35,13 @@ namespace TOne.WhS.Sales.Business.BusinessRules
 			if (zoneData.NormalRateToClose != null && (sellingProductRate == null || sellingProductRate.Rate == null))
 				return false;
 
-			if (zoneData.OtherRatesToClose != null && zoneData.OtherRatesToClose.Count > 0)
-			{
-				if (sellingProductRate == null || sellingProductRate.RatesByRateType == null)
-					return false;
-				foreach (RateToClose otherRateToClose in zoneData.OtherRatesToClose)
-				{
-					if (!sellingProductRate.RatesByRateType.ContainsKey(otherRateToClose.RateTypeId.Value))
-						return false;
-				}
-			}
-
 			return true;
 		}
 
 		public override string GetMessage(Vanrise.BusinessProcess.Entities.IRuleTarget target)
 		{
 			var zoneData = target as DataByZone;
-			return string.Format("Rate for Zone '{0}' of the assigned Selling Product was not found", zoneData.ZoneName);
+			return string.Format("Selling Product does not have a Normal Rate for Zone '{0}'", zoneData.ZoneName);
 		}
 	}
 }
