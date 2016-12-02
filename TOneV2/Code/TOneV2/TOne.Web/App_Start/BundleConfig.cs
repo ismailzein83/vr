22dcaad1930cc3713218917a7c66991230c7a156
Ismail Zein <VANRISE\ismail.zein>
2016-12-02 09:20:09 +0000

    added extended modules to web bundle
    git-tfs-id: [http://developmentvlan:8585/tfs/vanrise.collection]$/;C37369

--- TOneV2/Code/TOneV2/TOne.Web/App_Start/BundleConfig.cs
+++ TOneV2/Code/TOneV2/TOne.Web/App_Start/BundleConfig.cs
@@ -9,21 +9,20 @@ namespace TOne.Web
         public static void RegisterBundles(BundleCollection bundles)
         {
             Vanrise.Web.BundleConfig.RegisterBundles(bundles);
-
-            bundles.Add(new ScriptBundle("~/bundles/ModulesJavascripts").IncludeDirectory(
+            var modulesJSBundle = new ScriptBundle("~/bundles/ModulesJavascripts").IncludeDirectory(
                 "~/Client/Modules/Common", "*.js", true).IncludeDirectory(
-             //   "~/Client/Modules/Main", "*.js", true).IncludeDirectory(
-              //  "~/Client/Modules/Analytics", "*.js", true).IncludeDirectory(
+                //   "~/Client/Modules/Main", "*.js", true).IncludeDirectory(
+                //  "~/Client/Modules/Analytics", "*.js", true).IncludeDirectory(
                 "~/Client/Modules/BI", "*.js", true).IncludeDirectory(
-              //  "~/Client/Modules/Routing", "*.js", true).IncludeDirectory(
-             //   "~/Client/Modules/BusinessEntity", "*.js", true).IncludeDirectory(
+                //  "~/Client/Modules/Routing", "*.js", true).IncludeDirectory(
+                //   "~/Client/Modules/BusinessEntity", "*.js", true).IncludeDirectory(
                 "~/Client/Modules/Security", "*.js", true).IncludeDirectory(
                 "~/Client/Modules/Runtime", "*.js", true).IncludeDirectory(
                 "~/Client/Modules/Integration", "*.js", true).IncludeDirectory(
                 "~/Client/Modules/BusinessProcess", "*.js", true).IncludeDirectory(
-            //    "~/Client/Modules/Billing", "*.js", true).IncludeDirectory(
+                //    "~/Client/Modules/Billing", "*.js", true).IncludeDirectory(
                 "~/Client/Modules/Queueing", "*.js", true).IncludeDirectory(
-           //     "~/Client/Modules/CDR", "*.js", true).IncludeDirectory(
+                //     "~/Client/Modules/CDR", "*.js", true).IncludeDirectory(
                 "~/Client/Modules/WhS_CodePreparation", "*.js", true).IncludeDirectory(
                 "~/Client/Modules/WhS_BusinessEntity", "*.js", true).IncludeDirectory(
                 "~/Client/Modules/WhS_SupplierPriceList", "*.js", true).IncludeDirectory(
@@ -39,7 +38,20 @@ namespace TOne.Web
                 "~/Client/Modules/VR_Invoice", "*.js", true).IncludeDirectory(
                 "~/Client/Modules/WhS_Invoice", "*.js", true).IncludeDirectory(
                 "~/Client/Modules/VR_Notification", "*.js", true).IncludeDirectory(
-                "~/Client/Modules/WhS_Deal", "*.js", true));
+                "~/Client/Modules/WhS_Deal", "*.js", true);
+            var extendedModulesNames = System.Configuration.ConfigurationManager.AppSettings["Web_ExtendedModulesNames"];
+            if (!string.IsNullOrWhiteSpace(extendedModulesNames))
+            {
+                string[] extendedModulesNamesArray = extendedModulesNames.Split(',');
+                foreach(var moduleName in extendedModulesNamesArray)
+                {
+                    modulesJSBundle.IncludeDirectory(string.Format("~/Client/Modules/{0}", moduleName), "*.js", true);
+                }
+            }
+
+            bundles.Add(modulesJSBundle);
+
+            
         }
     }
 }
