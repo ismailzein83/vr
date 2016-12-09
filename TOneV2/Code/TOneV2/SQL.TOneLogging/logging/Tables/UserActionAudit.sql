73c7a59f23fe8bd5b9a7ac9dd565dfc724d03eb7
Ismail Zein <VANRISE\ismail.zein>
2016-12-09 08:34:30 +0000

    sql project
    git-tfs-id: [http://developmentvlan:8585/tfs/vanrise.collection]$/;C37530

---
+++ TOneV2/Code/TOneV2/SQL.TOneLogging/logging/Tables/UserActionAudit.sql
@@ -0,0 +1,12 @@
+﻿CREATE TABLE [logging].[UserActionAudit] (
+    [ID]         BIGINT        IDENTITY (1, 1) NOT NULL,
+    [UserID]     INT           NOT NULL,
+    [Module]     VARCHAR (50)  NOT NULL,
+    [Controller] VARCHAR (50)  NOT NULL,
+    [Action]     VARCHAR (100) NOT NULL,
+    [LogTime]    DATETIME      CONSTRAINT [DF_UserActionAudit_LogTime] DEFAULT (getdate()) NULL,
+    CONSTRAINT [PK_UserActionAudit] PRIMARY KEY CLUSTERED ([ID] ASC)
+);
+
+
+
