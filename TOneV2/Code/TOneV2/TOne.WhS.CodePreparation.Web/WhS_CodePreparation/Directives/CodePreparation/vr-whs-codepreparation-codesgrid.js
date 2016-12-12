728fc25c782f59198b4b9d9f6d42b50e6b0d160e
mostafa.jawhari <mostafa.jawhari@vanrise.tfs.local>
2016-12-12 07:43:43 +0000

    missing semicolon after statement
    git-tfs-id: [http://developmentvlan:8585/tfs/vanrise.collection]$/;C37550

--- TOneV2/Code/TOneV2/TOne.WhS.CodePreparation.Web/WhS_CodePreparation/Directives/CodePreparation/vr-whs-codepreparation-codesgrid.js
+++ TOneV2/Code/TOneV2/TOne.WhS.CodePreparation.Web/WhS_CodePreparation/Directives/CodePreparation/vr-whs-codepreparation-codesgrid.js
@@ -86,7 +86,7 @@ function (VRNotificationService, VRUIUtilsService, WhS_CP_CodePrepAPIService, Ut
                             }
                         }
                         return true;
-                    }
+                    };
 
                     return directiveAPI;
                 }
