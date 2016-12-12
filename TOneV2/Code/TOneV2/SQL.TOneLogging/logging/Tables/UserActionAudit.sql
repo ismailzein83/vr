6b18f2962f17bef42c3870b46a9976975ccb17d7
Wissam Ajamy <VANRISE\wissam.ajamy>
2016-12-12 14:15:21 +0000

    Set Indexes on UserActionAudit
    git-tfs-id: [http://developmentvlan:8585/tfs/vanrise.collection]$/;C37583

--- TOneV2/Code/TOneV2/SQL.TOneLogging/logging/Tables/UserActionAudit.sql
+++ TOneV2/Code/TOneV2/SQL.TOneLogging/logging/Tables/UserActionAudit.sql
@@ -4,12 +4,19 @@
     [ModuleName]     VARCHAR (50)  NOT NULL,
     [ControllerName] VARCHAR (50)  NOT NULL,
     [ActionName]     VARCHAR (100) NOT NULL,
-    [LogTime]        DATETIME      CONSTRAINT [DF_UserActionAudit_LogTime] DEFAULT (getdate()) NULL,
+    [LogTime]        DATETIME      CONSTRAINT [DF_UserActionAudit_LogTime] DEFAULT (getdate()) NOT NULL,
     [BaseUrl]        VARCHAR (100) NOT NULL,
-    CONSTRAINT [PK_UserActionAudit] PRIMARY KEY CLUSTERED ([ID] ASC)
+    CONSTRAINT [PK_UserActionAudit] PRIMARY KEY NONCLUSTERED ([ID] ASC)
 );
 
 
 
 
+
+
+
+
+GO
+CREATE CLUSTERED INDEX [IX_UserActionAudit_LogTime]
+    ON [logging].[UserActionAudit]([LogTime] ASC);
 
