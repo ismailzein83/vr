05e7a967498b949fc5f24f54640aa3985a6da8f9
mostafa.jawhari <mostafa.jawhari@vanrise.tfs.local>
2016-12-13 09:56:09 +0000

    adding needed files to implement subscriber invoice
    git-tfs-id: [http://developmentvlan:8585/tfs/vanrise.collection]$/;C37606

---
+++ Retail/Code/Retail.Invoice.Business/Extensions/RetailSubscriberInvoiceSettings.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Vanrise.Invoice.Entities;
+
+namespace Retail.Invoice.Business
+{
+    public class RetailSubscriberInvoiceSettings : InvoiceTypeExtendedSettings
+    {
+
+        public override Guid ConfigId
+        {
+            get { return new Guid("2f5c2fb4-4380-4a18-986c-210459134b4b"); }
+        }
+
+        public override BillingPeriod GetBillingPeriod(IExtendedSettingsBillingPeriodContext context)
+        {
+            throw new NotImplementedException();
+        }
+
+        public override dynamic GetInfo(IInvoiceTypeExtendedSettingsInfoContext context)
+        {
+            throw new NotImplementedException();
+        }
+
+        public override void GetInitialPeriodInfo(IInitialPeriodInfoContext context)
+        {
+            throw new NotImplementedException();
+        }
+
+        public override InvoiceGenerator GetInvoiceGenerator()
+        {
+            throw new NotImplementedException();
+        }
+
+        public override InvoicePartnerSettings GetPartnerSettings()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
