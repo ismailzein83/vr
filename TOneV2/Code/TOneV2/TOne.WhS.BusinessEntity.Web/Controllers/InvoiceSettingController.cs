fb4078d56ed24f3f9651d609876ab226ed4fc39d
hadi.hawi <hadi.hawi@vanrise.tfs.local>
2016-12-13 08:06:38 +0000

    Invoice Settings
    git-tfs-id: [http://developmentvlan:8585/tfs/vanrise.collection]$/;C37603

---
+++ TOneV2/Code/TOneV2/TOne.WhS.BusinessEntity.Web/Controllers/InvoiceSettingController.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Http;
+using TOne.WhS.BusinessEntity.Business;
+using TOne.WhS.BusinessEntity.Entities;
+using Vanrise.Web.Base;
+
+namespace TOne.WhS.BusinessEntity.Web.Controllers
+{
+    [JSONWithTypeAttribute]
+    [RoutePrefix(Constants.ROUTE_PREFIX + "InvoiceSetting")]
+    public class InvoiceSettingController : Vanrise.Web.Base.BaseAPIController
+    {
+        ConfigManager _manager = new ConfigManager();
+        
+        [HttpGet]
+        [Route("GetInvoiceSettingsInfo")]
+        public IEnumerable<CustomerInvoiceSettingInfo> GetInvoiceSettingsInfo()
+        {
+            return _manager.GetCustomerInvoiceSettingInfo();
+        }
+    }
+}
