9a0f8559a2b2dd9f9416748c405f61c1e3529b51
Samer Haidar <VANRISE\samer.haidar>
2016-12-14 17:32:27 +0000

    Adding AccountStatementViewSettings
    git-tfs-id: [http://developmentvlan:8585/tfs/vanrise.collection]$/;C37661

---
+++ Vanrise/Code/Vanrise.AccountBalance.Entities/AccountStatement/AccountStatementViewSettings.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Vanrise.AccountBalance.Entities
+{
+    public class AccountStatementViewSettings : Vanrise.Security.Entities.ViewSettings
+    {
+        public Guid AccountTypeId { get; set; }
+        public override string GetURL(Security.Entities.View view)
+        {
+            return String.Format("#/viewwithparams/VR_AccountBalance/Views/AccountStatement/AccountStatementManagement/{{\"accountTypeId\":\"{0}\"}}", this.AccountTypeId);
+        }
+    }
+}
