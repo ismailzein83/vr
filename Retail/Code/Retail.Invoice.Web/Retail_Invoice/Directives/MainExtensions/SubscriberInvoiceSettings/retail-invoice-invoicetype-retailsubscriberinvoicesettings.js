936517f98027a991a37b54ca6298eeb6a862dc77
mostafa.jawhari <mostafa.jawhari@vanrise.tfs.local>
2016-12-15 14:01:58 +0000

    Added Directive for invoice type settings
    git-tfs-id: [http://developmentvlan:8585/tfs/vanrise.collection]$/;C37686

---
+++ Retail/Code/Retail.Invoice.Web/Retail_Invoice/Directives/MainExtensions/SubscriberInvoiceSettings/retail-invoice-invoicetype-retailsubscriberinvoicesettings.js
@@ -0,0 +1,57 @@
+﻿"use strict";
+
+app.directive("retailInvoiceInvoicetypeRetailsubscriberinvoicesettings", ["UtilsService", "VRNotificationService",
+    function (UtilsService, VRNotificationService) {
+
+        var directiveDefinitionObject = {
+
+            restrict: "E",
+            scope:
+            {
+                onReady: "=",
+            },
+            controller: function ($scope, $element, $attrs) {
+                var ctrl = this;
+
+                var ctor = new CarrierInvoiceSettings($scope, ctrl, $attrs);
+                ctor.initializeController();
+            },
+            controllerAs: "ctrl",
+            bindToController: true,
+            compile: function (element, attrs) {
+
+            },
+            templateUrl: "/Client/Modules/Retail_Invoice/Directives/MainExtensions/SubscriberInvoiceSettings/Templates/SubscriberInvoiceSettingsTemplate.html"
+
+        };
+
+        function SubscriberInvoiceSettings($scope, ctrl, $attrs) {
+            this.initializeController = initializeController;
+            $scope.scopeModel = {};
+            function initializeController() {
+                defineAPI();
+            }
+
+            function defineAPI() {
+                var api = {};
+
+                api.load = function (payload) {
+                    
+                };
+
+
+                api.getData = function () {
+                    return {
+                        $type: "Retail.Invoice.Business, Retail.Invoice.Business.RetailSubscriberInvoiceSettings"
+                    };
+                };
+
+                if (ctrl.onReady != null)
+                    ctrl.onReady(api);
+            }
+        }
+
+        return directiveDefinitionObject;
+
+    }
+]);
