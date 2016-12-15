86a3d47d6ae4381ac6b37faeb4af395e63c26cd4
rabih.fashwal:2 <rabih.fashwal:2@vanrise.tfs.local>
2016-12-15 13:41:03 +0000

    Add Source Tariff Rule
    git-tfs-id: [http://developmentvlan:8585/tfs/vanrise.collection]$/;C37681

---
+++ TOneV2/Code/TOneV2/TOne.WhS.DBSync.Entities/Rules/SourceTariffRule.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TOne.WhS.DBSync.Entities
+{
+    public class SourceTariffRule : SourceBaseRule
+    {
+        public int ZoneId { get; set; }
+        public string CustomerId { get; set; }
+        public string SupplierId { get; set; }
+        public decimal CallFee { get; set; }
+        public decimal FirstPeriodRate { get; set; }
+        public int FirstPeriod { get; set; }
+        public bool RepeatFirstPeriod { get; set; }
+        public int FractionUnit { get; set; }
+        
+    }
+}
