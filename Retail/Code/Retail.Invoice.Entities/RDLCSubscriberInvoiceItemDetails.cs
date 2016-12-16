0620d46c284518e6a1c8eaab60bcda07986bb90c
hadi.hawi <hadi.hawi@vanrise.tfs.local>
2016-12-16 07:05:08 +0000

    Add rdlc entities
    git-tfs-id: [http://developmentvlan:8585/tfs/vanrise.collection]$/;C37709

---
+++ Retail/Code/Retail.Invoice.Entities/RDLCSubscriberInvoiceItemDetails.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Retail.Invoice.Entities
+{
+    public class RDLCSubscriberInvoiceItemDetails
+    {
+        public decimal Amount { get; set; }
+
+        public Guid ServiceTypeId { get; set; }
+
+        public RDLCSubscriberInvoiceItemDetails()
+        {
+                
+        }
+
+        public IEnumerable<RDLCSubscriberInvoiceItemDetails> GetRDLCSubscriberInvoiceItemDetailsSchema()
+        {
+            return null;
+        }
+    }
+}
