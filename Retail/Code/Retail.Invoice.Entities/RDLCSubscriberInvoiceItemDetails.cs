f2d327c31cdb3889c109a9af078153a505df7cec
mostafa.jawhari <mostafa.jawhari@vanrise.tfs.local>
2016-12-16 07:17:59 +0000

    Updated RDLC Invoice Item Details
    git-tfs-id: [http://developmentvlan:8585/tfs/vanrise.collection]$/;C37711

--- Retail/Code/Retail.Invoice.Entities/RDLCSubscriberInvoiceItemDetails.cs
+++ Retail/Code/Retail.Invoice.Entities/RDLCSubscriberInvoiceItemDetails.cs
@@ -12,6 +12,14 @@ namespace Retail.Invoice.Entities
 
         public Guid ServiceTypeId { get; set; }
 
+        public long? ZoneId { get; set; }
+
+        public long? InterconnectOperatorId { get; set; }
+
+        public int CountCDRs { get; set; }
+
+        public Decimal TotalDuration { get; set; }
+
         public RDLCSubscriberInvoiceItemDetails()
         {
                 
