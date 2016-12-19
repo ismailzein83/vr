59674d1c40a3b060c1b3d1b837d860ef5d6ad6f5
mostafa.jawhari <mostafa.jawhari@vanrise.tfs.local>
2016-12-19 12:58:58 +0000

    adding needed files to implement Invoice Settings
    git-tfs-id: [http://developmentvlan:8585/tfs/vanrise.collection]$/;C37783

---
+++ Retail/Code/Retail.BusinessEntity.Entities/InvoiceSettings.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Vanrise.Entities;
+using Vanrise.Invoice.Entities;
+
+namespace Retail.BusinessEntity.Entities
+{
+    public class InvoiceSettings : SettingData
+    {
+        public const string SETTING_TYPE = "Retail_Invoice_InvoiceSettings";
+        public List<SubscriberInvoiceSettings> SubscriberInvoiceSettings { get; set; }
+    }
+    public class SubscriberInvoiceSettings
+    {
+        public Guid InvoiceSettingId { get; set; }
+        public string Name { get; set; }
+        public Guid DefaultEmailId { get; set; }
+        public bool IsDefault { get; set; }
+        public int DuePeriod { get; set; }
+        public bool IsFollow { get; set; }
+        public BillingPeriod BillingPeriod { get; set; }
+        public string SerialNumberPattern { get; set; }
+    }
+}
---
+++ Retail/Code/Retail.BusinessEntity.Web/Retail_BusinessEntity/Views/InvoiceSettings/InvoiceSettingEditorController.js
@@ -0,0 +1,14 @@
+﻿
+(function (appControllers) {
+
+    "use strict";
+
+    invoiceSettingEditorController.$inject = ['$scope', 'VRNotificationService', 'VRNavigationService', 'UtilsService', 'VRUIUtilsService'];
+
+    function invoiceSettingEditorController($scope, VRNotificationService, VRNavigationService, UtilsService, VRUIUtilsService) {
+
+       
+    }
+
+    appControllers.controller('Retail_BE_InvoiceSettingEditorController', invoiceSettingEditorController);
+})(appControllers);
