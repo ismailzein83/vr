6a89a6e6ac64e37a7befd55af1845b4aa8f27814
Mortada Issa <VANRISE\mortada.issa>
2016-12-19 13:27:33 +0000

    Fixed a defect parsing dates in excel converter
    git-tfs-id: [http://developmentvlan:8585/tfs/vanrise.collection]$/;C37784

--- Vanrise/Code/Vanrise.ExcelConversion.Web/ExcelConversion/Constant/FieldTypeEnum.js
+++ Vanrise/Code/Vanrise.ExcelConversion.Web/ExcelConversion/Constant/FieldTypeEnum.js
@@ -1,6 +1,6 @@
 ﻿app.constant('VR_ExcelConversion_FieldTypeEnum', {
-    String: { value: "3F29315E-B542-43B8-9618-7DE216CD9653", description: "String" },
-    DateTime: { value: "B8712417-83AB-4D4B-9EE1-109D20CEB909", description: "DateTime" },
-    Decimal: { value: "75AEF329-27BD-4108-B617-F5CC05FF2AA3", description: "Decimal" },
-    Int: { value: "75AEF329-27BD-4108-B617-F5CC05FF2AA3", description: "Int" }
+    String: { value: "0", description: "String" },
+    DateTime: { value: "1", description: "DateTime" },
+    Decimal: { value: "2", description: "Decimal" },
+    Int: { value: "3", description: "Int" }
 });
