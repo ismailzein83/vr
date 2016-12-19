59d809cf4a0ea47ed2e2b4c353dfd88cf13cb2df
anthony douaihy <VANRISE\anthony.douaihy>
2016-12-19 12:04:12 +0000

    Config Id in AccountBalanceAlertRuleTypeSettings
    git-tfs-id: [http://developmentvlan:8585/tfs/vanrise.collection]$/;C37777

--- Vanrise/Code/Vanrise.AccountBalance.Business/Extensions/AccountBalanceAlertRuleTypeSettings.cs
+++ Vanrise/Code/Vanrise.AccountBalance.Business/Extensions/AccountBalanceAlertRuleTypeSettings.cs
@@ -11,7 +11,7 @@ namespace Vanrise.AccountBalance.Business.Extensions
     {
         public override Guid ConfigId
         {
-            get { return new Guid("49200E05-8AB8-4E28-9AB1-B0699CD32258"); }
+            get { return new Guid("ba79cb79-d058-4382-88fc-db1c154b5374"); }
         }
         public Guid AccountTypeId { get; set; }
         public override string VRActionExtensionType
