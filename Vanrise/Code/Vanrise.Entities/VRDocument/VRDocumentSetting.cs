74845a26b4a197940f0f61649059d786b842d31f
mostafa.jawhari <mostafa.jawhari@vanrise.tfs.local>
2016-12-20 16:31:03 +0000

    adding needed entities for VRDocument
    git-tfs-id: [http://developmentvlan:8585/tfs/vanrise.collection]$/;C37857

---
+++ Vanrise/Code/Vanrise.Entities/VRDocument/VRDocumentSetting.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Vanrise.Entities
+{
+    public class VRDocumentSetting
+    {
+        public long FileId { get; set; }
+
+        public Guid CategoryId { get; set; }
+
+        public string Description { get; set; }
+
+        public DateTime CreatedOn { get; set; }
+
+        public DateTime? UpdatedOn { get; set; }
+    }
+}
