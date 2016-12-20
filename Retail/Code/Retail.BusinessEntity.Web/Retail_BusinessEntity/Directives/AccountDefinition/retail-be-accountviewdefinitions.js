5a1c232622e7dcfea2bf7f95b82bd9a2ff6fefc7
Samer Haidar <VANRISE\samer.haidar>
2016-12-20 10:47:55 +0000

    Implementing account view definition
    git-tfs-id: [http://developmentvlan:8585/tfs/vanrise.collection]$/;C37834

--- Retail/Code/Retail.BusinessEntity.Web/Retail_BusinessEntity/Directives/AccountDefinition/retail-be-accountviewdefinitions.js
+++ Retail/Code/Retail.BusinessEntity.Web/Retail_BusinessEntity/Directives/AccountDefinition/retail-be-accountviewdefinitions.js
@@ -1,1 +1,109 @@
-﻿
+﻿"use strict";
+
+app.directive("retailBeAccountviewdefinitions", ["UtilsService", "VRNotificationService", "Retail_BE_AccountDefinitionService",
+    function (UtilsService, VRNotificationService, Retail_BE_AccountDefinitionService) {
+
+        var directiveDefinitionObject = {
+
+            restrict: "E",
+            scope:
+            {
+                onReady: "=",
+            },
+            controller: function ($scope, $element, $attrs) {
+                var ctrl = this;
+
+                var ctor = new AccountViewDefinition($scope, ctrl, $attrs);
+                ctor.initializeController();
+            },
+            controllerAs: "ctrl",
+            bindToController: true,
+            compile: function (element, attrs) {
+
+            },
+            templateUrl: "/Client/Modules/Retail_BusinessEntity/Directives/AccountDefinition/Templates/AccountViewDefinitionsTemplate.html"
+
+        };
+
+        function AccountViewDefinition($scope, ctrl, $attrs) {
+
+            var gridAPI;
+            this.initializeController = initializeController;
+            var context;
+            function initializeController() {
+                ctrl.datasource = [];
+
+                ctrl.addAccountViewDefinition = function () {
+                    var onAccountViewDefinitionAdded = function (accountViewDefinition) {
+                        ctrl.datasource.push({ Entity: accountViewDefinition });
+                    };
+                    Retail_BE_AccountDefinitionService.addAccountViewDefinition(onAccountViewDefinitionAdded, getContext());
+                };
+
+                ctrl.removeAccountViewDefinition = function (dataItem) {
+                    var index = ctrl.datasource.indexOf(dataItem);
+                    ctrl.datasource.splice(index, 1);
+                };
+                defineMenuActions();
+                defineAPI();
+            }
+
+            function defineAPI() {
+                var api = {};
+
+                api.getData = function () {
+                    var accountViewDefinitions;
+                    if (ctrl.datasource != undefined) {
+                        accountViewDefinitions = [];
+                        for (var i = 0; i < ctrl.datasource.length; i++) {
+                            var currentItem = ctrl.datasource[i];
+                            accountViewDefinitions.push(currentItem.Entity);
+                        }
+                    }
+                    return accountViewDefinitions;
+                };
+
+                api.load = function (payload) {
+                    if (payload != undefined) {
+                        context = payload.context;
+                        if (payload.accountViewDefinitions != undefined) {
+                            for (var i = 0; i < payload.accountViewDefinitions.length; i++) {
+                                var accountViewDefinition = payload.accountViewDefinitions[i];
+                                ctrl.datasource.push({ Entity: accountViewDefinition });
+                            }
+                        }
+                    }
+                };
+
+                if (ctrl.onReady != null)
+                    ctrl.onReady(api);
+            }
+
+            function defineMenuActions() {
+                var defaultMenuActions = [
+                {
+                    name: "Edit",
+                    clicked: editAccountViewDefinition,
+                }];
+
+                $scope.gridMenuActions = function (dataItem) {
+                    return defaultMenuActions;
+                };
+            }
+
+            function editAccountViewDefinition(accountViewDefinitionObj) {
+                var onAccountViewDefinitionUpdated = function (accountViewDefinition) {
+                    var index = ctrl.datasource.indexOf(accountViewDefinitionObj);
+                    ctrl.datasource[index] = { Entity: accountViewDefinition };
+                };
+                Retail_BE_AccountDefinitionService.editAccountViewDefinition(accountViewDefinitionObj.Entity, onAccountViewDefinitionUpdated, getContext());
+            }
+            function getContext() {
+                return context;
+            }
+        }
+
+        return directiveDefinitionObject;
+
+    }
+]);
--- Retail/Code/Retail.BusinessEntity.Web/Retail_BusinessEntity/Services/AccountDefinitionService.js
+++ Retail/Code/Retail.BusinessEntity.Web/Retail_BusinessEntity/Services/AccountDefinitionService.js
@@ -1,1 +1,43 @@
-﻿
+﻿(function (appControllers) {
+
+    'use stict';
+
+    AccountDefinitionService.$inject = ['VRModalService'];
+
+    function AccountDefinitionService(VRModalService) {
+
+
+        function addAccountViewDefinitions(onAccountViewDefinitionAdded) {
+
+            var modalSettings = {};
+
+            modalSettings.onScopeReady = function (modalScope) {
+                modalScope.onAccountViewDefinitionAdded = onAccountViewDefinitionAdded
+            };
+
+            VRModalService.showModal('/Client/Modules/Retail_BusinessEntity/Views/AccountDefinition/AccountViewDefinition/AccountViewDefinitionEditor.html', null, modalSettings);
+
+        }
+        function editAccountViewDefinition(accountViewDefinition, onAccountViewDefinitionUpdated) {
+
+            var parameters = {
+                accountViewDefinition: accountViewDefinition
+            };
+
+            var modalSettings = {};
+
+            modalSettings.onScopeReady = function (modalScope) {
+                modalScope.onAccountViewDefinitionUpdated = onAccountViewDefinitionUpdated;
+            };
+
+            VRModalService.showModal('/Client/Modules/Retail_BusinessEntity/Views/AccountDefinition/AccountViewDefinition/AccountViewDefinitionEditor.html', parameters, modalSettings);
+        }
+
+        return {
+            addAccountViewDefinition: addAccountViewDefinition,
+            editAccountViewDefinition: editAccountViewDefinition
+        };
+    }
+    appControllers.service('Retail_BE_AccountDefinitionService', AccountDefinitionService);
+
+})(appControllers);
