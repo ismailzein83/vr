e3f5931cc41e96b720eacbf7b73a62fef6db585f
anthony douaihy <VANRISE\anthony.douaihy>
2016-12-20 12:46:14 +0000

    Add new Business Process for generate alert rule
    git-tfs-id: [http://developmentvlan:8585/tfs/vanrise.collection]$/;C37840

---
+++ Vanrise/Code/Vanrise.Analytic.BP.Arguments/DAProfCalcGenerateAlertInput.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using Vanrise.Analytic.Entities;
+using Vanrise.BusinessProcess.Entities;
+
+namespace Vanrise.Analytic.BP.Arguments
+{
+    public class DAProfCalcGenerateAlertInput : BaseProcessInputArgument
+    {
+        public Guid AlertRuleTypeId { get; set; }
+
+        public DateTime FromTime { get; set; }
+
+        public DateTime ToTime { get; set; }
+
+        public DAProfCalcChunkTimeEnum ChunkTime { get; set; }
+
+        public override string GetTitle()
+        {
+            return String.Format("Data Analysis Profiling And Calculation Generate Alert Process from {0} to {1}", FromTime, ToTime);
+        }
+    }
+}
---
+++ Vanrise/Code/Vanrise.Analytic.Web/Analytic/Directives/DataAnalysis/ProfilingAndCalculation/ProcessInput/vr-analytic-daprofcalc-generatealertprocessinput.js
@@ -0,0 +1,86 @@
+﻿"use strict";
+
+app.directive("vrAnalyticDaprofcalcGeneratealertprocessinput", ['UtilsService', 'VRUIUtilsService', 'VRValidationService', 'DAProfCalcChunkTimeEnum',
+    function (UtilsService, VRUIUtilsService, VRValidationService, DAProfCalcChunkTimeEnum) {
+        var directiveDefinitionObject = {
+            restrict: "E",
+            scope: {
+                onReady: "="
+            },
+            controller: function ($scope, $element, $attrs) {
+                var ctrl = this;
+
+                var directiveConstructor = new DirectiveConstructor($scope, ctrl);
+                directiveConstructor.initializeController();
+            },
+            controllerAs: "ctrl",
+            bindToController: true,
+            compile: function (element, attrs) {
+                return {
+                    pre: function ($scope, iElem, iAttrs, ctrl) {
+
+                    }
+                };
+            },
+            templateUrl: '/Client/Modules/Analytic/Directives/DataAnalysis/ProfilingAndCalculation/ProcessInput/Templates/DAProfCalcGenerateAlertProcessInputTemplate.html'
+        };
+
+        function DirectiveConstructor($scope, ctrl) {
+
+            this.initializeController = initializeController;
+
+            function initializeController() {
+                defineAPI();
+            }
+
+            $scope.scopeModel = {};
+            var alertRuleTypeSelectorAPI;
+            var alertRuleTypeSelectorReadyDeferred = UtilsService.createPromiseDeferred();
+
+            $scope.onAlertRuleTypeSelectorReady = function (api) {
+                alertRuleTypeSelectorAPI = api;
+                alertRuleTypeSelectorReadyDeferred.resolve();
+            };
+
+            $scope.validateTimeRange = function () {
+                return VRValidationService.validateTimeRange($scope.fromDate, $scope.toDate);
+            };
+
+            function defineAPI() {
+
+                var api = {};
+                api.getData = function () {
+                    return {
+                        InputArguments: {
+                            $type: "Vanrise.Analytic.BP.Arguments.DAProfCalcGenerateAlertInput, Vanrise.Analytic.BP.Arguments",
+                            AlertRuleTypeId: alertRuleTypeSelectorAPI.getSelectedIds(),
+                            FromTime: $scope.fromDate,
+                            ToTime: new Date($scope.toDate.setDate($scope.toDate.getDate() + 1)),
+                            ChunkTime: $scope.selectedChunkTime.value
+                        }
+                    };
+                };
+
+                api.load = function (payload) {
+                    $scope.chunkTimes = UtilsService.getArrayEnum(DAProfCalcChunkTimeEnum);
+
+                    var promises = [];
+
+                    var alertRuleTypeSelectorLoadDeferred = UtilsService.createPromiseDeferred();
+
+                    alertRuleTypeSelectorReadyDeferred.promise.then(function () {
+                        VRUIUtilsService.callDirectiveLoad(alertRuleTypeSelectorAPI, undefined, alertRuleTypeSelectorLoadDeferred);
+                    });
+
+                    promises.push(alertRuleTypeSelectorLoadDeferred.promise);
+
+                    return UtilsService.waitMultiplePromises(promises);
+                };
+
+                if (ctrl.onReady != null)
+                    ctrl.onReady(api);
+            }
+        }
+
+        return directiveDefinitionObject;
+    }]);
