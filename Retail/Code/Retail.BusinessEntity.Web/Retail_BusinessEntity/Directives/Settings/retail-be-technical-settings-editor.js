88e716a89b9e06d2bf0eb71cab0ea6382dbfff6d
Ali Atoui <VANRISE\ali.atoui>
2016-12-20 11:48:15 +0000

    Editing TechnicalSettingsEditorTemplate
    git-tfs-id: [http://developmentvlan:8585/tfs/vanrise.collection]$/;C37835

--- Retail/Code/Retail.BusinessEntity.Web/Retail_BusinessEntity/Directives/Settings/retail-be-technical-settings-editor.js
+++ Retail/Code/Retail.BusinessEntity.Web/Retail_BusinessEntity/Directives/Settings/retail-be-technical-settings-editor.js
@@ -1,1 +1,90 @@
-﻿
+﻿'use strict';
+
+app.directive('retailBeTechnicalSettingsEditor', ['UtilsService', 'VRUIUtilsService',
+    function (UtilsService, VRUIUtilsService) {
+
+        var directiveDefinitionObject = {
+            restrict: 'E',
+            scope: {
+                onReady: '=',
+                isrequired: '='
+            },
+            controller: function ($scope, $element, $attrs) {
+                var ctrl = this;
+
+                var ctor = new technicalSettingsEditorCtor(ctrl, $scope, $attrs);
+                ctor.initializeController();
+            },
+            controllerAs: 'ctrl',
+            bindToController: true,
+            compile: function (element, attrs) {
+
+            },
+            templateUrl: '/Client/Modules/Retail_BusinessEntity/Directives/Settings/Templates/TechnicalSettingsEditorTemplate.html'
+        };
+
+        function technicalSettingsEditorCtor(ctrl, $scope, $attrs) {
+
+            var accountGridDefinitionDirectiveAPI;
+            var accountGridDefinitionDirectiveDeferred = UtilsService.createPromiseDeferred();
+
+            function initializeController() {
+                $scope.scopeModel = {};
+
+                $scope.scopeModel.onAccountGridDefinition = function (api) {
+                    accountGridDefinitionDirectiveAPI = api;
+                    accountGridDefinitionDirectiveDeferred.resolve();
+                };
+
+                UtilsService.waitMultiplePromises([accountGridDefinitionDirectiveDeferred.promise]).then(function () {
+                    defineAPI();
+                });
+            }
+            function defineAPI() {
+                var api = {};
+
+                api.load = function (payload) {
+                    var promises = [];
+                    var accountGridDefinition;
+
+                    if (payload != undefined && payload.data != undefined) {
+                        accountGridDefinition = payload.data.GridDefinition;
+                    }
+
+                    //Loading AccountGridDefinition Directive
+                    var accountGridDefinitionLoadPromise = getAccountGridDefinitionLoadPromise();
+                    promises.push(accountGridDefinitionLoadPromise);
+
+
+                    function getAccountGridDefinitionLoadPromise(){
+                        var accountGridDefitnionLoadDeferred = UtilsService.createPromiseDeferred();
+
+                        var accountGridDefinitionPayload = {
+                            accountGridDefinition: accountGridDefinition
+                        };
+                        VRUIUtilsService.callDirectiveLoad(accountGridDefinitionDirectiveAPI, accountGridDefinitionPayload, accountGridDefitnionLoadDeferred);
+
+                        return accountGridDefitnionLoadDeferred.promise;
+                    }
+
+                    return UtilsService.waitMultiplePromises(promises);
+                };
+
+                api.getData = function () {
+                    var obj = {
+                        $type: "Retail.BusinessEntity.Entities.RetailBETechnicalSetting, Retail.BusinessEntity.Entities",
+                        GridDefinition: accountGridDefinitionDirectiveAPI.getData()
+                    };
+
+                    return obj;
+                };
+
+                if (ctrl.onReady != null)
+                    ctrl.onReady(api);
+            }
+
+            this.initializeController = initializeController;
+        }
+
+        return directiveDefinitionObject;
+    }]);
