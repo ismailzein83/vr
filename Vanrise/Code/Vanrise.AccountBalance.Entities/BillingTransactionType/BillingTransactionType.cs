e9e0cfbb7dc909a7d7631c06eace5c4296fc5551
Ismail Zein <VANRISE\ismail.zein>
2016-12-21 11:47:21 +0000

    added BillingTransactionTypeSettings
    git-tfs-id: [http://developmentvlan:8585/tfs/vanrise.collection]$/;C37878

--- Vanrise/Code/Vanrise.AccountBalance.Entities/BillingTransactionType/BillingTransactionType.cs
+++ Vanrise/Code/Vanrise.AccountBalance.Entities/BillingTransactionType/BillingTransactionType.cs
@@ -11,5 +11,15 @@ namespace Vanrise.AccountBalance.Entities
         public Guid BillingTransactionTypeId { get; set; }
         public string Name { get; set; }
         public bool IsCredit { get; set; }
+
+        public BillingTransactionTypeSettings Settings { get; set; }
+    }
+
+    public class BillingTransactionTypeSettings
+    {
+        /// <summary>
+        /// true if the user is not allowed to manually add a transaction of this type
+        /// </summary>
+        public bool ManualAdditionDisabled { get; set; }
     }
 }
