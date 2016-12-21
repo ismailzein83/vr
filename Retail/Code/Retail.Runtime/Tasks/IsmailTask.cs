a063690dcd86338e496ab40d7949c3c3f1d154f2
Ismail Zein <VANRISE\ismail.zein>
2016-12-21 16:45:01 +0000

    added IsmailTask and references to Analytic
    git-tfs-id: [http://developmentvlan:8585/tfs/vanrise.collection]$/;C37910

---
+++ Retail/Code/Retail.Runtime/Tasks/IsmailTask.cs
@@ -0,0 +1,43 @@
+﻿using Retail.BusinessEntity.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Retail.Runtime.Tasks
+{
+    public class IsmailTask : ITask
+    {
+        public void Execute()
+        {
+            Vanrise.Security.Entities.ContextFactory.GetContext().SetContextUserId(1);
+            var accountManager = new Retail.BusinessEntity.Business.AccountManager();
+            IAccountPayment dummy;
+            var financialAccounts = accountManager.GetCachedAccounts().Values.Where(a => accountManager.HasAccountPayment(a.AccountId, false, out dummy));
+            foreach(var a in financialAccounts)
+            {
+                Vanrise.Invoice.Business.InvoiceManager invoiceManager = new Vanrise.Invoice.Business.InvoiceManager();
+                try
+                {
+                    invoiceManager.GenerateInvoice(new Vanrise.Invoice.Entities.GenerateInvoiceInput
+                            {
+                                InvoiceTypeId = new Guid("384c819d-6e21-4e9a-9f08-11c7b81ad329"),
+                                PartnerId =  a.AccountId.ToString(),
+                                IssueDate = DateTime.Today,
+                                FromDate = DateTime.Parse("2016-11-01"),
+                                ToDate = DateTime.Parse("2016-12-01")
+                            }
+                            );
+                }
+            catch(Vanrise.Invoice.Entities.InvoiceGeneratorException ex)
+                {
+                    Console.WriteLine("Invoice has no data");
+                }
+            }
+
+                Console.WriteLine("DONE");
+                Console.ReadKey();
+        }
+    }
+}
