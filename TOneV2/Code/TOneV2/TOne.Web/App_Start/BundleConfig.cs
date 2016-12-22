fb81fa9ed4f9d1e516afe3a657546a4667c29e78
anthony douaihy <VANRISE\anthony.douaihy>
2016-12-22 15:27:28 +0000

    Add references VR_AccountBalance
    git-tfs-id: [http://developmentvlan:8585/tfs/vanrise.collection]$/;C37965

--- TOneV2/Code/TOneV2/TOne.Web/App_Start/BundleConfig.cs
+++ TOneV2/Code/TOneV2/TOne.Web/App_Start/BundleConfig.cs
@@ -38,6 +38,7 @@ namespace TOne.Web
                 "~/Client/Modules/VR_Invoice", "*.js", true).IncludeDirectory(
                 "~/Client/Modules/WhS_Invoice", "*.js", true).IncludeDirectory(
                 "~/Client/Modules/VR_Notification", "*.js", true).IncludeDirectory(
+                "~/Client/Modules/VR_AccountBalance", "*.js", true).IncludeDirectory(
                 "~/Client/Modules/WhS_Deal", "*.js", true);
 
             bundles.Add(modulesJSBundle);
