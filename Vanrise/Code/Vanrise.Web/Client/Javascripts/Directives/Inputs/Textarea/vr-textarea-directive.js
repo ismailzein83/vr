2937acd494fc2b02950c0eda976c6992f602d893
Ali Ballouk <VANRISE\ali.ballouk>
2016-12-22 09:22:34 +0000

    fix vr-textarea layout inside cell grid
    git-tfs-id: [http://developmentvlan:8585/tfs/vanrise.collection]$/;C37923

--- Vanrise/Code/Vanrise.Web/Client/Javascripts/Directives/Inputs/Textarea/vr-textarea-directive.js
+++ Vanrise/Code/Vanrise.Web/Client/Javascripts/Directives/Inputs/Textarea/vr-textarea-directive.js
@@ -107,11 +107,13 @@
                     var labelTemplate = '';
                     if (attrs.label != undefined)
                         labelTemplate = '<vr-label>' + attrs.label + '</vr-label>';
-                    var rows = 3;
+                    var rows = 3;                  
+                    if ($(element).parents('.vr-datagrid-celltext').length > 0)
+                        rows = 2;
                     if (attrs.rows != undefined)
                         rows = attrs.rows;
                     var textboxTemplate = '<div ng-mouseenter="showtd=true" ng-mouseleave="showtd=false" >'
-                            + '<vr-validator validate="ctrl.validate()">'
+                            + '<vr-validator validate="ctrl.validate()" vr-textarea-element>'
                             + '<textarea tabindex="{{ctrl.tabindex}}" readonly="ctrl.readOnly"  placeholder="{{ctrl.placelHolder}}" ng-readonly="ctrl.readOnly" id="mainInput"  ng-model="ctrl.value" ng-change="ctrl.notifyUserChange()" rows="' + rows + '" class="form-control" style="width: 100%; resize: none;" ></textarea>'
                             + '</vr-validator>'
                             + '<span ng-if="ctrl.hint!=undefined" bs-tooltip class="glyphicon glyphicon-question-sign hand-cursor vr-hint-input" html="true"   placement="bottom"  trigger="hover" ng-mouseenter="ctrl.adjustTooltipPosition($event)"  data-type="info" data-title="{{ctrl.hint}}"></span>';
