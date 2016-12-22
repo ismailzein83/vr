76d61ad0e5c87126facd6f0f6c0fa743a5109a26
Samer Haidar <VANRISE\samer.haidar>
2016-12-22 14:54:11 +0000

    addint account360degree editor
    git-tfs-id: [http://developmentvlan:8585/tfs/vanrise.collection]$/;C37960

---
+++ Retail/Code/Retail.BusinessEntity.Web/Retail_BusinessEntity/Views/Account/Account360DegreeEditorController.js
@@ -0,0 +1,65 @@
+﻿(function (appControllers) {
+
+    'use strict';
+
+    Account360DegreeEditorController.$inject = ['$scope', 'UtilsService', 'VRUIUtilsService', 'VRNavigationService', 'VRNotificationService','Retail_BE_AccountAPIService'];
+
+    function Account360DegreeEditorController($scope, UtilsService, VRUIUtilsService, VRNavigationService, VRNotificationService, Retail_BE_AccountAPIService) {
+
+        var accountId;
+        var accountEntity;
+        var accountViewsDefinitions;
+        loadParameters();
+        defineScope();
+        load();
+
+        function loadParameters() {
+            var parameters = VRNavigationService.getParameters($scope);
+            if (parameters != undefined) {
+                accountId = parameters.accountId;
+            }
+        }
+        function defineScope() {
+            $scope.scopeModel = {};
+           
+            $scope.scopeModel.close = function () {
+                $scope.modalContext.closeModal();
+            };
+        }
+        function load() {
+            $scope.scopeModel.isLoading = true;
+            UtilsService.waitMultipleAsyncOperations([getAccount, getAccountViewsDefinitions]).then(function () {
+                loadAllControls();
+            });
+        }
+        function getAccount()
+        {
+            return Retail_BE_AccountAPIService.GetAccount(accountId).then(function (response) {
+                accountEntity = response;
+            });
+        }
+        function getAccountViewsDefinitions()
+        {
+
+        }
+        function loadAllControls() {
+
+            function setTitle() {
+                $scope.title = (accountEntity != undefined) ? accountEntity.Name : undefined;
+            }
+            function loadStaticData() {
+                if (accountEntity == undefined)
+                    return;
+            }
+            
+            return UtilsService.waitMultipleAsyncOperations([setTitle, loadStaticData]).catch(function (error) {
+                VRNotificationService.notifyExceptionWithClose(error, $scope);
+            }).finally(function () {
+                $scope.scopeModel.isLoading = false;
+            });
+        }
+    }
+
+    appControllers.controller('Retail_BE_Account360DegreeEditorController', Account360DegreeEditorController);
+
+})(appControllers);
