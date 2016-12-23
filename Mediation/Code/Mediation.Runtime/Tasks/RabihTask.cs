9d112772f81a938a96781b5560beb6511901976a
rabih.fashwal:2 <rabih.fashwal:2@vanrise.tfs.local>
2016-12-23 15:50:15 +0000

    Add Exposed Connection String
    git-tfs-id: [http://developmentvlan:8585/tfs/vanrise.collection]$/;C38008

--- Mediation/Code/Mediation.Runtime/Tasks/RabihTask.cs
+++ Mediation/Code/Mediation.Runtime/Tasks/RabihTask.cs
@@ -8,6 +8,7 @@ using Mediation.Generic.BP.Arguments;
 using Vanrise.BusinessProcess;
 using Vanrise.BusinessProcess.Business;
 using Vanrise.BusinessProcess.Entities;
+using Vanrise.Caching.Runtime;
 using Vanrise.Queueing;
 using Vanrise.Runtime;
 
@@ -24,18 +25,41 @@ namespace Mediation.Runtime.Tasks
         {
             var runtimeServices = new List<RuntimeService>();
 
-            QueueActivationService queueActivationService = new QueueActivationService() { Interval = new TimeSpan(0, 0, 2) };
-            SchedulerService schedulerService = new SchedulerService() { Interval = new TimeSpan(0, 0, 2) };
-            Vanrise.Integration.Business.DataSourceRuntimeService dsRuntimeService = new Vanrise.Integration.Business.DataSourceRuntimeService { Interval = new TimeSpan(0, 0, 2) };
             BusinessProcessService bpService = new BusinessProcessService() { Interval = new TimeSpan(0, 0, 2) };
-            runtimeServices.Add(queueActivationService);
-            runtimeServices.Add(schedulerService);
-            runtimeServices.Add(dsRuntimeService);
             runtimeServices.Add(bpService);
 
-            RuntimeHost host = new RuntimeHost(runtimeServices);
-            host.Start();   
+            QueueRegulatorRuntimeService queueRegulatorService = new QueueRegulatorRuntimeService() { Interval = new TimeSpan(0, 0, 2) };
+            runtimeServices.Add(queueRegulatorService);
 
+            QueueActivationRuntimeService queueActivationService = new QueueActivationRuntimeService() { Interval = new TimeSpan(0, 0, 2) };
+            runtimeServices.Add(queueActivationService);
+
+            //SummaryQueueActivationRuntimeService summaryQueueActivationService = new SummaryQueueActivationRuntimeService() { Interval = new TimeSpan(0, 0, 2) };
+            //runtimeServices.Add(summaryQueueActivationService);
+
+            SchedulerService schedulerService = new SchedulerService() { Interval = new TimeSpan(0, 0, 1) };
+            runtimeServices.Add(schedulerService);
+
+            //Vanrise.Common.Business.BigDataRuntimeService bigDataService = new Vanrise.Common.Business.BigDataRuntimeService { Interval = new TimeSpan(0, 0, 2) };
+            //runtimeServices.Add(bigDataService);
+
+            TransactionLockRuntimeService transactionLockRuntimeService = new TransactionLockRuntimeService() { Interval = new TimeSpan(0, 0, 1) };
+            runtimeServices.Add(transactionLockRuntimeService);
+
+            Vanrise.Integration.Business.DataSourceRuntimeService dsRuntimeService = new Vanrise.Integration.Business.DataSourceRuntimeService { Interval = new TimeSpan(0, 0, 2) };
+            runtimeServices.Add(dsRuntimeService);
+
+            BPRegulatorRuntimeService bpRegulatorRuntimeService = new BPRegulatorRuntimeService { Interval = new TimeSpan(0, 0, 2) };
+            runtimeServices.Add(bpRegulatorRuntimeService);
+
+            //CachingRuntimeService cachingRuntimeService = new CachingRuntimeService { Interval = new TimeSpan(0, 0, 2) };
+            //runtimeServices.Add(cachingRuntimeService);
+
+            //CachingDistributorRuntimeService cachingDistributorRuntimeService = new CachingDistributorRuntimeService { Interval = new TimeSpan(0, 0, 2) };
+            //runtimeServices.Add(cachingDistributorRuntimeService);
+
+            RuntimeHost host = new RuntimeHost(runtimeServices);
+            host.Start();
             Console.ReadKey();
         }
     }
