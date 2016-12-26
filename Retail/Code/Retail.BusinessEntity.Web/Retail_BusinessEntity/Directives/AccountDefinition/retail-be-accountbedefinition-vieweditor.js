4bb0fca77a1bfa36c7b6c0897194f4bd57913736
Samer Haidar <VANRISE\samer.haidar>
2016-12-26 15:12:03 +0000

    adding account be view directive
    git-tfs-id: [http://developmentvlan:8585/tfs/vanrise.collection]$/;C38041

---
+++ Retail/Code/Retail.BusinessEntity.Web/Retail_BusinessEntity/Directives/AccountDefinition/retail-be-accountbedefinition-vieweditor.js
@@ -0,0 +1,78 @@
+﻿"use strict";
+
+app.directive("retailBeAccountbedefinitionVieweditor", ["UtilsService", "VRNotificationService", "VRUIUtilsService",
+    function (UtilsService, VRNotificationService, VRUIUtilsService) {
+
+        var directiveDefinitionObject = {
+            restrict: "E",
+            scope:
+            {
+                onReady: "=",
+            },
+            controller: function ($scope, $element, $attrs) {
+                var ctrl = this;
+                var accountBEDefinitionViewEditor = new AccountBEDefinitionViewEditor($scope, ctrl, $attrs);
+                accountBEDefinitionViewEditor.initializeController();
+            },
+            controllerAs: "ctrl",
+            bindToController: true,
+            compile: function (element, attrs) {
+
+            },
+            templateUrl: "/Client/Modules/Retail_BusinessEntity/Directives/AccountDefinition/Templates/AccountBEDefinitionViewEditor.html"
+        };
+        function AccountBEDefinitionViewEditor($scope, ctrl, $attrs) {
+            this.initializeController = initializeController;
+
+            var beDefinitionSelectorApi;
+            var beDefinitionSelectorPromiseDeferred = UtilsService.createPromiseDeferred();
+
+            function initializeController() {
+                $scope.scopeModel = {};
+
+                $scope.scopeModel.onDataRecordTypeSelectorDirectiveReady = function (api) {
+                    beDefinitionSelectorApi = api;
+                    beDefinitionSelectorPromiseDeferred.resolve();
+                };
+                defineAPI();
+            }
+
+            function defineAPI() {
+                var api = {};
+
+                api.getData = function () {
+                    return {
+                        $type: "Vanrise.GenericData.Entities.GenericBEViewSettings, Vanrise.GenericData.Entities",
+                        AccountBEDefinitionIds: beDefinitionSelectorApi.getSelectedIds()
+                    };
+                };
+                api.load = function (payload) {
+                    var promises = [];
+                    promises.push(loadBusinessEntityDefinitionSelector());
+                    function loadBusinessEntityDefinitionSelector() {
+                        var businessEntityDefinitionSelectorLoadDeferred = UtilsService.createPromiseDeferred();
+
+                        beDefinitionSelectorPromiseDeferred.promise.then(function () {
+                            var payloadSelector = {
+                                selectedIds: payload != undefined ? payload.BusinessEntityDefinitionId : undefined,
+                                filter: {
+                                    Filters: [{
+                                        $type: "Vanrise.GenericData.Business.GenericBusinessEntityDefinitionFilter,Vanrise.GenericData.Business"
+                                    }]
+                                }
+                            };
+                            VRUIUtilsService.callDirectiveLoad(beDefinitionSelectorApi, payloadSelector, businessEntityDefinitionSelectorLoadDeferred);
+                        });
+                        return businessEntityDefinitionSelectorLoadDeferred.promise;
+                    }
+                    return UtilsService.waitMultiplePromises(promises);
+                };
+                if (ctrl.onReady != null)
+                    ctrl.onReady(api);
+            }
+        }
+
+        return directiveDefinitionObject;
+
+    }
+]);
