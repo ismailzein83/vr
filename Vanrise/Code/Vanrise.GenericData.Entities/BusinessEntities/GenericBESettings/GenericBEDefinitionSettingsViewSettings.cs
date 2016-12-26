737f173c48613615908846345cbe595499cbe88b
Samer Haidar <VANRISE\samer.haidar>
2016-12-26 13:09:59 +0000

    adding GenericBEDefinitionSettingsViewSettings
    git-tfs-id: [http://developmentvlan:8585/tfs/vanrise.collection]$/;C38037

---
+++ Vanrise/Code/Vanrise.GenericData.Entities/BusinessEntities/GenericBESettings/GenericBEDefinitionSettingsViewSettings.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Vanrise.Security.Entities;
+
+namespace Vanrise.GenericData.Entities
+{
+    public class GenericBEDefinitionSettingsViewSettings : Vanrise.Security.Entities.ViewSettings
+    {
+        public Guid BusinessEntityDefinitionId { get; set; }
+        public override string GetURL(Security.Entities.View view)
+        {
+            return String.Format("#/viewwithparams/VR_GenericData/Views/GenericBusinessEntity/Runtime/GenericBusinessEntityManagement/{{\"businessEntityDefinitionId\":\"{0}\"}}", this.BusinessEntityDefinitionId);
+        }
+        public override bool DoesUserHaveAccess(Security.Entities.IViewUserAccessContext context)
+        {
+
+            return BusinessManagerFactory.GetManager<IGenericBusinessEntityManager>().DoesUserHaveViewAccess(context.UserId, this.BusinessEntityDefinitionId);
+        }
+    }
+}
