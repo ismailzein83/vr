8d3e97374aa1d45100019b735565bd320830d30a
Zeinab Mokdad <VANRISE\zeinab.mokdad>
2016-12-26 08:40:28 +0000

    fixing description of RTP mode
    git-tfs-id: [http://developmentvlan:8585/tfs/vanrise.collection]$/;C38018

--- NetworkProvisioning/Code/NP.IVSwitch.Web/NP_IVSwitch/Constant/RtpModeEnum.js
+++ NetworkProvisioning/Code/NP.IVSwitch.Web/NP_IVSwitch/Constant/RtpModeEnum.js
@@ -1,5 +1,5 @@
 ﻿app.constant('NP_IVSwitch_RtpModeEnum', {
-    AdvancedProxying: { value: 1, description: "Advanced Proxying" },
-    PassthruProxying: { value: 2, description: "Pass thru Proxying" },
-    NoProxying: { value: 3, description: "No Proxying" },
-});    
+    AdvancedProxying: { value: 1, description: "Advanced Proxying - (Possible transcoding)" },
+    PassthruProxying: { value: 2, description: "Passthru Proxying - Forwarding only (Packet in / Packet out)" },
+    NoProxying: { value: 3, description: "No Proxying - Bypassing (Media flows direct between Endpoints & Routes)" }
+});
