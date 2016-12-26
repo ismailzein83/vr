c0bf2e30ffac17a2269bee9430a7488f6d8b4aba
Ismail Zein <VANRISE\ismail.zein>
2016-12-26 08:46:26 +0000

    added AccountBEDefinitionSettings
    git-tfs-id: [http://developmentvlan:8585/tfs/vanrise.collection]$/;C38020

---
+++ Retail/Code/Retail.BusinessEntity.Entities/AccountDefinition/AccountBEDefinitionSettings.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Retail.BusinessEntity.Entities
+{   
+    public class AccountBEDefinitionSettings : Vanrise.GenericData.Entities.BusinessEntityDefinitionSettings
+    {
+        public override string DefinitionEditor { get { throw new NotImplementedException(); } }
+
+        public override string SelectorUIControl
+        {
+            get { throw new NotImplementedException(); }
+        }
+        public override string ManagerFQTN
+        {
+            get { return "Retail.BusinessEntity.Business.AccountManager, Retail.BusinessEntity.Business"; }
+        }
+        public override string IdType
+        {
+            get { return "System.Int64"; }
+        }
+
+        public Guid StatusGroupId { get; set; }
+
+        public AccountGridDefinition GridDefinition { get; set; }
+
+        public List<AccountViewDefinition> AccountViewDefinitions { get; set; }
+
+        public FixedChargingDefinition FixedChargingDefinition { get; set; }
+
+        public override Guid ConfigId
+        {
+            get { throw new NotImplementedException(); }
+        }
+    }
+
+    public abstract class AccountBEDefinitionCondition
+    {
+        public abstract Guid ConfigId { get; }
+
+        public abstract bool Evaluate(IAccountConditionEvaluationContext context);
+    }
+
+    public interface IAccountBEDefinitionConditionContext
+    {
+        AccountBEDefinitionSettings AccountBEDefinitionSettings { get; }
+    }
+}
