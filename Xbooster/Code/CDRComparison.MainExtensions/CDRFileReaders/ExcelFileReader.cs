754b04e2984b51587e7cd8ac831884f67a7c57a0
mohamad.elhariri <mohamad.elhariri@vanrise.tfs.local>
2016-12-27 10:46:25 +0000

    Fixed a bug
    git-tfs-id: [http://developmentvlan:8585/tfs/vanrise.collection]$/;C38061

--- Xbooster/Code/CDRComparison.MainExtensions/CDRFileReaders/ExcelFileReader.cs
+++ Xbooster/Code/CDRComparison.MainExtensions/CDRFileReaders/ExcelFileReader.cs
@@ -12,151 +12,161 @@ using System.Threading.Tasks;
 
 namespace CDRComparison.MainExtensions
 {
-    public class ExcelFileReader : CDRFileReader
-    {
-        public override Guid ConfigId { get { return new Guid("61e3f8d9-5e5b-49ef-9ae3-df77bf1d674f"); } }
+	public class ExcelFileReader : CDRFileReader
+	{
+		public override Guid ConfigId { get { return new Guid("61e3f8d9-5e5b-49ef-9ae3-df77bf1d674f"); } }
 
-        #region Properties
+		#region Properties
 
-        public int FirstRowIndex { get; set; }
+		public int FirstRowIndex { get; set; }
 
-        public List<ExcelFileFieldMapping> FieldMappings { get; set; }
+		public List<ExcelFileFieldMapping> FieldMappings { get; set; }
 
-        public string DateTimeFormat { get; set; }
+		public string DateTimeFormat { get; set; }
 
-        #endregion
+		#endregion
 
-        #region Override
+		#region Override
 
-        public override void ReadCDRs(IReadCDRsFromFileContext context)
-        {
-            if (this.FieldMappings == null)
-                throw new ArgumentNullException("FieldMappings");
+		public override void ReadCDRs(IReadCDRsFromFileContext context)
+		{
+			if (this.FieldMappings == null)
+				throw new ArgumentNullException("FieldMappings");
 
-            byte[] fileContent = context.FileContent;
-            var fileStream = new MemoryStream(fileContent);
+			byte[] fileContent = context.FileContent;
+			var fileStream = new MemoryStream(fileContent);
 
-            Workbook workbook = new Workbook(fileStream);
-            Vanrise.Common.Utilities.ActivateAspose();
-            Worksheet worksheet = workbook.Worksheets[0];
+			Workbook workbook = new Workbook(fileStream);
+			Vanrise.Common.Utilities.ActivateAspose();
+			Worksheet worksheet = workbook.Worksheets[0];
 
-            var cdrs = new List<CDR>();
-            Dictionary<string, PropertyInfo> cdrProperties = GetCDRProperties();
+			var cdrs = new List<CDR>();
+			Dictionary<string, PropertyInfo> cdrProperties = GetCDRProperties();
 
-            for (int rowIndex = this.FirstRowIndex; rowIndex < worksheet.Cells.Rows.Count; rowIndex++)
-            {
-                var cdr = new CDR();
-                foreach (ExcelFileFieldMapping fldMapping in this.FieldMappings)
-                {
-                    Cell cell = worksheet.Cells[rowIndex, fldMapping.FieldIndex];
-                    if (cell == null)
-                        throw new NullReferenceException(String.Format("cell[{0}, {1}]", rowIndex, fldMapping.FieldIndex));
-                    object cellValue = cell.Value;
-                    PropertyInfo prop;
-                    if (cdrProperties.TryGetValue(fldMapping.FieldName, out prop))
-                        prop.SetValue(cdr, ConvertValue(cellValue, prop.PropertyType));
-                    else
-                        cdr.ExtraFields.Add(fldMapping.FieldName, cellValue);
-                }
-                cdrs.Add(cdr);
-                if (cdrs.Count == 100000)
-                {
-                    context.OnCDRsReceived(cdrs);
-                    cdrs = new List<CDR>();
-                }
-            }
+			for (int rowIndex = this.FirstRowIndex; rowIndex < worksheet.Cells.Rows.Count; rowIndex++)
+			{
+				var cdr = new CDR();
+				foreach (ExcelFileFieldMapping fldMapping in this.FieldMappings)
+				{
+					Cell cell = worksheet.Cells[rowIndex, fldMapping.FieldIndex];
+					if (cell == null)
+						throw new NullReferenceException(String.Format("cell[{0}, {1}]", rowIndex, fldMapping.FieldIndex));
+					object cellValue = cell.Value;
+					PropertyInfo prop;
+					if (cdrProperties.TryGetValue(fldMapping.FieldName, out prop))
+						prop.SetValue(cdr, ConvertValue(cellValue, prop.PropertyType));
+					else
+						cdr.ExtraFields.Add(fldMapping.FieldName, cellValue);
+				}
+				cdrs.Add(cdr);
+				if (cdrs.Count == 100000)
+				{
+					context.OnCDRsReceived(cdrs);
+					cdrs = new List<CDR>();
+				}
+			}
 
-            if (cdrs.Count > 0)
-                context.OnCDRsReceived(cdrs);
-        }
+			if (cdrs.Count > 0)
+				context.OnCDRsReceived(cdrs);
+		}
 
-        public override CDRSample ReadSample(IReadSampleFromFileContext context)
-        {
-            var sample = new ExcelFileCDRSample();
-            var rows = new List<ExcelFileDataRow>();
+		public override CDRSample ReadSample(IReadSampleFromFileContext context)
+		{
+			var sample = new ExcelFileCDRSample();
+			var rows = new List<ExcelFileDataRow>();
 
-            byte[] fileContent = context.FileContent;
-            var fileStream = new MemoryStream(fileContent);
+			byte[] fileContent = context.FileContent;
+			var fileStream = new MemoryStream(fileContent);
 
-            Workbook workbook;
-            Vanrise.Common.Utilities.ActivateAspose();
-            try
-            {
-                workbook = new Workbook(fileStream);
-            }
-            catch
-            {
-                sample.ColumnCount = 0;
-                sample.ErrorMessage = "Invalid excel file";
-                return sample;
-            }
+			Vanrise.Common.Utilities.ActivateAspose();
 
-            Worksheet worksheet = workbook.Worksheets[0];
+			Workbook workbook;
+			Worksheet worksheet;
 
-            for (int rowIndex = 0; rowIndex < worksheet.Cells.Rows.Count && rowIndex < 10; rowIndex++)
-            {
-                var data = new List<string>();
-                Aspose.Cells.Row row = worksheet.Cells.Rows[rowIndex];
+			try
+			{
+				workbook = new Workbook(fileStream);
+				worksheet = workbook.Worksheets[0];
+				if (worksheet.Cells.MaxDataColumn == -1)
+				{
+					sample.ColumnCount = 0;
+					sample.ErrorMessage = "Excel file is blank";
+					return sample;
+				}
+				sample.ColumnCount = worksheet.Cells.MaxDataColumn + 1;
+			}
+			catch
+			{
+				sample.ColumnCount = 0;
+				sample.ErrorMessage = "Invalid excel file";
+				return sample;
+			}
 
-                for (int columnIndex = 0; columnIndex <= row.LastCell.Column; columnIndex++)
-                {
-                    Cell cell = worksheet.Cells[rowIndex, columnIndex];
-                    if (cell == null)
-                        throw new NullReferenceException(String.Format("cell[{0}, {1}]", rowIndex, columnIndex));
-                    data.Add(cell.StringValue);
-                }
+			for (int rowIndex = 0; rowIndex < worksheet.Cells.Rows.Count && rowIndex < 10; rowIndex++)
+			{
+				var data = new List<string>();
+				Aspose.Cells.Row row = worksheet.Cells.Rows[rowIndex];
 
-                sample.ColumnCount = row.LastCell.Column + 1;
-                rows.Add(new ExcelFileDataRow() { Data = data });
-            }
+				for (int columnIndex = 0; columnIndex <= row.LastCell.Column; columnIndex++)
+				{
+					Cell cell = worksheet.Cells[rowIndex, columnIndex];
+					if (cell == null)
+						throw new NullReferenceException(String.Format("cell[{0}, {1}]", rowIndex, columnIndex));
+					data.Add(cell.StringValue);
+				}
 
-            sample.Rows = rows;
-            return sample;
-        }
+				rows.Add(new ExcelFileDataRow() { Data = data });
+			}
 
-        #endregion
+			sample.Rows = rows;
+			return sample;
+		}
 
-        #region Private Methods
+		#endregion
 
-        private object ConvertValue(object fldValue, Type propertyType)
-        {
-            if (fldValue == null)
-                return null;
-            else if (propertyType == typeof(string))
-                return fldValue.ToString();
-            else if (propertyType == typeof(DateTime)) {
-                if (fldValue is DateTime) {
-                    return fldValue;
-                }
-                DateTime dateTimeValue;
-                if (!DateTime.TryParseExact(fldValue.ToString(), this.DateTimeFormat, CultureInfo.CurrentCulture, DateTimeStyles.None, out dateTimeValue))
-                    throw new Exception(String.Format("Invalid date time format '{0}'. Could not parse date time value '{1}'", this.DateTimeFormat, fldValue));
-                return dateTimeValue;
-            }
-            else if (propertyType == typeof(TimeSpan))
-                return (fldValue is TimeSpan) ? fldValue : TimeSpan.Parse(fldValue.ToString());
-            else if (propertyType == typeof(Boolean))
-                return (fldValue is Boolean) ? fldValue : Boolean.Parse(fldValue.ToString());
-            else
-                return Convert.ChangeType(fldValue, propertyType, NumberFormatInfo.CurrentInfo);
-        }
+		#region Private Methods
 
-        private Dictionary<string, PropertyInfo> GetCDRProperties()
-        {
-            Dictionary<string, PropertyInfo> cdrProperties = new Dictionary<string, PropertyInfo>();
-            foreach (var prop in typeof(CDR).GetProperties(BindingFlags.Public | BindingFlags.Instance))
-            {
-                cdrProperties.Add(prop.Name, prop);
-            }
-            return cdrProperties;
-        }
+		private object ConvertValue(object fldValue, Type propertyType)
+		{
+			if (fldValue == null)
+				return null;
+			else if (propertyType == typeof(string))
+				return fldValue.ToString();
+			else if (propertyType == typeof(DateTime))
+			{
+				if (fldValue is DateTime)
+				{
+					return fldValue;
+				}
+				DateTime dateTimeValue;
+				if (!DateTime.TryParseExact(fldValue.ToString(), this.DateTimeFormat, CultureInfo.CurrentCulture, DateTimeStyles.None, out dateTimeValue))
+					throw new Exception(String.Format("Invalid date time format '{0}'. Could not parse date time value '{1}'", this.DateTimeFormat, fldValue));
+				return dateTimeValue;
+			}
+			else if (propertyType == typeof(TimeSpan))
+				return (fldValue is TimeSpan) ? fldValue : TimeSpan.Parse(fldValue.ToString());
+			else if (propertyType == typeof(Boolean))
+				return (fldValue is Boolean) ? fldValue : Boolean.Parse(fldValue.ToString());
+			else
+				return Convert.ChangeType(fldValue, propertyType, NumberFormatInfo.CurrentInfo);
+		}
 
-        #endregion
-    }
+		private Dictionary<string, PropertyInfo> GetCDRProperties()
+		{
+			Dictionary<string, PropertyInfo> cdrProperties = new Dictionary<string, PropertyInfo>();
+			foreach (var prop in typeof(CDR).GetProperties(BindingFlags.Public | BindingFlags.Instance))
+			{
+				cdrProperties.Add(prop.Name, prop);
+			}
+			return cdrProperties;
+		}
 
-    public class ExcelFileFieldMapping
-    {
-        public int FieldIndex { get; set; }
-        public string FieldName { get; set; }
-    }
+		#endregion
+	}
+
+	public class ExcelFileFieldMapping
+	{
+		public int FieldIndex { get; set; }
+		public string FieldName { get; set; }
+	}
 }
