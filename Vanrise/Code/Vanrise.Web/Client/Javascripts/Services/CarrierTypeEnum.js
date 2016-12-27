aded5bfdf93dad50fa5e0fc61c608cd22de1b454
Ali Ballouk <VANRISE\ali.ballouk>
2016-12-27 11:27:27 +0000

    remove old javascript angular js services andf controllers files
    git-tfs-id: [http://developmentvlan:8585/tfs/vanrise.collection]$/;C38068

--- Vanrise/Code/Vanrise.Web/Client/Javascripts/Services/CarrierTypeEnum.js
+++
@@ -1,6 +0,0 @@
-﻿'use strict';
-app.constant('OperatorTypeEnum', {
-    SaleZone: { value: 1, description: "Exchange" },
-    Customer: { value: 0, description: "Customer" },
-    Supplier: { value: 2, description: "Supplier" }
-});
