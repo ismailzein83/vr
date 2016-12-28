70617de6fa834a6233ec8b27f4b65317f6eb5890
rabih.fashwal:2 <rabih.fashwal:2@vanrise.tfs.local>
2016-12-28 10:20:50 +0000

    Add Post Script for Ringo Retail, All related post data for ringo should be moved to this script
    git-tfs-id: [http://developmentvlan:8585/tfs/vanrise.collection]$/;C38118

---
+++ TOneV2/Code/TOneV2/SQL.TOneConfiguration/Retail_Ringo.PostDeployment.sql
@@ -0,0 +1,33 @@
+﻿/*
+Post-Deployment Script Template							
+--------------------------------------------------------------------------------------
+ This file contains SQL statements that will be appended to the build script.		
+ Use SQLCMD syntax to include a file in the post-deployment script.			
+ Example:      :r .\myfile.sql								
+ Use SQLCMD syntax to reference a variable in the post-deployment script.		
+ Example:      :setvar TableName MyTable							
+               SELECT * FROM [$(TableName)]					
+--------------------------------------------------------------------------------------
+*/
+
+--[sec].[View]--------------------------------------------------------------------------------------
+BEGIN
+
+set nocount on;
+;with cte_data([ID],[Name],[Title],[Url],[Module],[ActionNames],[Audience],[Content],[Settings],[Type],[OldType],[Rank],[IsDeleted])
+as (select * from (values
+--//////////////////////////////////////////////////////////////////////////////////////////////////
+('d6cb8173-769a-424a-a118-3d5474057de8','MNP Report','MNP Report','#/view/Retail_Ringo/Views/RingoMNPReportManagement','eb303a61-929a-4d33-bf50-18f40308bc86',null,null,null,null,'372ed3cb-4b7b-4464-9abf-59cd7b08bd23',null,7,0)
+--\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\
+)c([ID],[Name],[Title],[Url],[Module],[ActionNames],[Audience],[Content],[Settings],[Type],[OldType],[Rank],[IsDeleted]))
+merge	[sec].[View] as t
+using	cte_data as s
+on		1=1 and t.[ID] = s.[ID]
+when matched then
+	update set
+	[Name] = s.[Name],[Title] = s.[Title],[Url] = s.[Url],[Module] = s.[Module],[ActionNames] = s.[ActionNames],[Audience] = s.[Audience],[Content] = s.[Content],[Settings] = s.[Settings],[Type] = s.[Type],[OldType] = s.[OldType],[Rank] = s.[Rank],[IsDeleted] = s.[IsDeleted]
+when not matched by target then
+	insert([ID],[Name],[Title],[Url],[Module],[ActionNames],[Audience],[Content],[Settings],[Type],[OldType],[Rank],[IsDeleted])
+	values(s.[ID],s.[Name],s.[Title],s.[Url],s.[Module],s.[ActionNames],s.[Audience],s.[Content],s.[Settings],s.[Type],s.[OldType],s.[Rank],s.[IsDeleted]);
+----------------------------------------------------------------------------------------------------
+END
