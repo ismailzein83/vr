625e440549bf5b8e111d13968ecacd671cf6ccfb
Ali Atoui <VANRISE\ali.atoui>
2017-01-05 11:33:08 +0000

    Adding ProductDefinitionController
    git-tfs-id: [http://developmentvlan:8585/tfs/vanrise.collection]$/;C38243

---
+++ Retail/Code/Retail.BusinessEntity.Web/Controllers/ProductDefinitionController.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Retail.BusinessEntity.Web
+{
+    public class ProductDefinitionController
+    {
+    }
+}
