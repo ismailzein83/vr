2f2ffe9f638404667529319987873edd50301268
Ali Atoui <VANRISE\ali.atoui>
2017-01-05 14:42:44 +0000

    Adding ProductDefinition and Product Controllers
    git-tfs-id: [http://developmentvlan:8585/tfs/vanrise.collection]$/;C38253

---
+++ Retail/Code/Retail.BusinessEntity.Web/Controllers/ProductController.cs
@@ -0,0 +1,55 @@
+﻿using Retail.BusinessEntity.Business;
+using Retail.BusinessEntity.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Http;
+using Vanrise.Web.Base;
+
+namespace Retail.BusinessEntity.Web.Controllers
+{
+
+    [RoutePrefix(Constants.ROUTE_PREFIX + "Product")]
+    [JSONWithTypeAttribute]
+    public class ProductController : BaseAPIController
+    {
+        ProductManager _manager = new ProductManager();
+
+        [HttpPost]
+        [Route("GetFilteredProducts")]
+        public object GetFilteredProducts(Vanrise.Entities.DataRetrievalInput<ProductQuery> input)
+        {
+            return GetWebResponse(input, _manager.GetFilteredProducts(input));
+        }
+
+        [HttpGet]
+        [Route("GetProduct")]
+        public Product GetProduct(int productId)
+        {
+            return _manager.GetProduct(productId);
+        }
+
+        [HttpPost]
+        [Route("AddProduct")]
+        public Vanrise.Entities.InsertOperationOutput<ProductDetail> AddProduct(Product productItem)
+        {
+            return _manager.AddProduct(productItem);
+        }
+
+        [HttpPost]
+        [Route("UpdateProduct")]
+        public Vanrise.Entities.UpdateOperationOutput<ProductDetail> UpdateProduct(Product productItem)
+        {
+            return _manager.UpdateProduct(productItem);
+        }
+
+        //[HttpGet]
+        //[Route("GetProductesInfo")]
+        //public IEnumerable<ProductInfo> GetProductesInfo(string filter = null)
+        //{
+        //    ProductFilter deserializedFilter = (filter != null) ? Vanrise.Common.Serializer.Deserialize<ProductFilter>(filter) : null;
+        //    return _manager.GetProductesInfo(deserializedFilter);
+        //}
+    }
+}
--- Retail/Code/Retail.BusinessEntity.Web/Controllers/ProductDefinitionController.cs
+++ Retail/Code/Retail.BusinessEntity.Web/Controllers/ProductDefinitionController.cs
@@ -1,11 +1,32 @@
-﻿using System;
+﻿using Retail.BusinessEntity.Business;
+using Retail.BusinessEntity.Entities;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
+using System.Web.Http;
+using Vanrise.Web.Base;
 
 namespace Retail.BusinessEntity.Web
 {
-    public class ProductDefinitionController
+    [RoutePrefix(Constants.ROUTE_PREFIX + "ProductDefinition")]
+    [JSONWithTypeAttribute]
+    public class ProductDefinitionController : BaseAPIController
     {
+        [HttpGet]
+        [Route("GetProductDefinitionsInfo")]
+        public IEnumerable<ProductDefinitionInfo> GetProductDefinitionsInfo(string serializedFilter = null)
+        {
+            ProductDefinitionManager manager = new ProductDefinitionManager();
+            return manager.GetProductDefinitionsInfo();
+        }
+
+        [HttpGet]
+        [Route("GetProductDefinitionExtendedSettingsConfigs")]
+        public IEnumerable<ProductDefinitionConfig> GetProductDefinitionExtendedSettingsConfigs()
+        {
+            ProductDefinitionManager manager = new ProductDefinitionManager();
+            return manager.GetProductDefinitionExtendedSettingsConfigs();
+        }
     }
 }
