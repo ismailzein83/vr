5b30489ad01c4f6efd7e0f786e041b4ee3631d80
Ali Atoui <VANRISE\ali.atoui>
2017-01-05 13:34:44 +0000

    Adding IProductDataManager
    git-tfs-id: [http://developmentvlan:8585/tfs/vanrise.collection]$/;C38248

---
+++ Retail/Code/Retail.BusinessEntity.Data/IProductDataManager.cs
@@ -0,0 +1,20 @@
+﻿using Retail.BusinessEntity.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Retail.BusinessEntity.Data
+{
+    public interface IProductDataManager : IDataManager
+    {
+        List<Product> GetProducts();
+
+        bool AreProductUpdated(ref object updateHandle);
+
+        bool Insert(Product statusDefinitionItem, out int insertedId);
+
+        bool Update(Product statusDefinitionItem);
+    }
+}
