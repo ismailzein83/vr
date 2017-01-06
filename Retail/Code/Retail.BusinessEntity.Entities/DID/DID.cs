e545126db4f6d0cf377413eccc8b4dc4958eee63
Ismail Zein <VANRISE\ismail.zein>
2017-01-06 15:35:40 +0000

    added DID entities
    git-tfs-id: [http://developmentvlan:8585/tfs/vanrise.collection]$/;C38302

---
+++ Retail/Code/Retail.BusinessEntity.Entities/DID/DID.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Retail.BusinessEntity.Entities
+{
+    public class DID
+    {
+        public int DIDId { get; set; }
+
+        public DIDSettings Settings { get; set; }
+    }
+}
---
+++ Retail/Code/Retail.BusinessEntity.Entities/DID/DIDSettings.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Retail.BusinessEntity.Entities
+{
+    public class DIDSettings
+    {
+        public string Number { get; set; }
+
+        public bool IsInternational { get; set; }
+
+        public int NumberOfChannels { get; set; }
+    }
+}
