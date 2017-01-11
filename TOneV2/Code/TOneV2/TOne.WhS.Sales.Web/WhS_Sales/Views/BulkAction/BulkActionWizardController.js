3e9c2683e8a11740337a915b42d186624fd7ef7a
Mortada Issa <VANRISE\mortada.issa>
2017-01-11 16:52:36 +0000

    Adding more files for Bulk Action feature (client side files)
    git-tfs-id: [http://developmentvlan:8585/tfs/vanrise.collection]$/;C38375

---
+++ TOneV2/Code/TOneV2/TOne.WhS.Sales.Web/WhS_Sales/Views/BulkAction/BulkActionWizardController.js
@@ -0,0 +1,65 @@
+﻿(function (appControllers) {
+
+    "use strict";
+
+    BulkActionWizardController.$inject = ["$scope"];
+
+    function BulkActionWizardController($scope) {
+        var settings;
+
+        loadParameters();
+        defineScope();
+        load();
+
+        function loadParameters() {
+            //var parameters = VRNavigationService.getParameters($scope);
+
+            //if (parameters) {
+            //    settings = parameters.settings;
+            //}
+        }
+
+        function defineScope() {
+            //$scope.title = "Edit Rate Plan Settings";
+            //$scope.tabItems = [{
+            //    title: "Cost Columns",
+            //    directive: "vr-whs-sales-costcolumns",
+            //    loadDirective: function (api) {
+            //        return api.load(settings);
+            //    }
+            //}];
+
+            //$scope.saveSettings = function () {
+
+            //    var settings;
+            //    var costCalculationMethods;
+
+            //    var directiveAPI = $scope.tabItems[0].directiveAPI;
+            //    if (directiveAPI != undefined)
+            //        costCalculationMethods = directiveAPI.getData();
+
+            //    if (costCalculationMethods != undefined) {
+            //        settings = {
+            //            costCalculationMethods: costCalculationMethods
+            //        };
+            //    }
+
+            //    if ($scope.onSettingsUpdated != null && typeof $scope.onSettingsUpdated == "function")
+            //        $scope.onSettingsUpdated(settings);
+
+            //    $scope.modalContext.closeModal();
+            //};
+
+            //$scope.close = function () {
+            //    $scope.modalContext.closeModal();
+            //};
+        }
+
+        function load() {
+
+        }
+    }
+
+    appControllers.controller("WhS_Sales_BulkActionWizardController", BulkActionWizardController);
+
+})(appControllers);
