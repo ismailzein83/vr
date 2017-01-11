6b573ef66f47b7fcc893d113b93b064c95e10ae4
hadi.hawi <hadi.hawi@vanrise.tfs.local>
2017-01-11 09:28:47 +0000

    Invoice Settings API service
    git-tfs-id: [http://developmentvlan:8585/tfs/vanrise.collection]$/;C38350

---
+++ TOneV2/Code/TOneV2/TOne.WhS.BusinessEntity.Web/WhS_BusinessEntity/Services/APIServices/InvoiceSettingAPIService.js
@@ -0,0 +1,21 @@
+﻿(function (appControllers) {
+
+    'use strict';
+
+    InvoiceSettingAPIService.$inject = ['BaseAPIService', 'UtilsService', 'WhS_BE_ModuleConfig'];
+
+    function InvoiceSettingAPIService(BaseAPIService, UtilsService, WhS_BE_ModuleConfig) {
+        var controllerName = 'InvoiceSetting';
+
+        function GetInvoiceSettingsInfo() {
+            return BaseAPIService.get(UtilsService.getServiceURL(WhS_BE_ModuleConfig.moduleName, controllerName, 'GetInvoiceSettingsInfo'));
+        }
+
+        return ({
+            GetInvoiceSettingsInfo: GetInvoiceSettingsInfo
+        });
+    }
+
+    appControllers.service('WhS_BE_InvoiceSettingAPIService', InvoiceSettingAPIService);
+
+})(appControllers);
