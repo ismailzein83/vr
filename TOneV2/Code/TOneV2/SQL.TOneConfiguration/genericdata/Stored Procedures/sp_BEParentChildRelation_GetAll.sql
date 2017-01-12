e354ddc2f1e6697f1d0d831e816ebca362877bc2
Wissam Ajamy <VANRISE\wissam.ajamy>
2017-01-12 13:25:38 +0000

    Update structure from TOneConfiguration
    git-tfs-id: [http://developmentvlan:8585/tfs/vanrise.collection]$/;C38408

---
+++ TOneV2/Code/TOneV2/SQL.TOneConfiguration/genericdata/Stored Procedures/sp_BEParentChildRelation_GetAll.sql
@@ -0,0 +1,10 @@
+﻿
+Create Procedure [genericdata].[sp_BEParentChildRelation_GetAll]
+AS
+BEGIN
+	-- SET NOCOUNT ON added to prevent extra result sets from
+	-- interfering with SELECT statements.
+	SET NOCOUNT ON;
+	select	[ID], [RelationDefinitionID], [ParentBEID], [ChildBEID], [BED], [EED]
+	from	[genericdata].[BEParentChildRelation] WITH(NOLOCK)
+END
---
+++ TOneV2/Code/TOneV2/SQL.TOneConfiguration/genericdata/Tables/BEParentChildRelation.sql
@@ -0,0 +1,12 @@
+﻿CREATE TABLE [genericdata].[BEParentChildRelation] (
+    [ID]                   BIGINT           IDENTITY (1, 1) NOT NULL,
+    [RelationDefinitionID] UNIQUEIDENTIFIER NOT NULL,
+    [ParentBEID]           VARCHAR (50)     NOT NULL,
+    [ChildBEID]            VARCHAR (50)     NOT NULL,
+    [BED]                  DATETIME         NOT NULL,
+    [EED]                  DATETIME         NULL,
+    [timestamp]            ROWVERSION       NULL,
+    [CreatedTime]          DATETIME         CONSTRAINT [DF_BEParentChildRelation_CreatedTime] DEFAULT (getdate()) NULL,
+    CONSTRAINT [PK_BEParentChildRelation] PRIMARY KEY CLUSTERED ([ID] ASC)
+);
+
