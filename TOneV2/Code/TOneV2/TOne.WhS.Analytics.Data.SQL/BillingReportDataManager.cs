e6ed674cdde8955541a787933cdcbfe9aeeae27a
hadi.hawi <hadi.hawi@vanrise.tfs.local>
2017-01-12 13:13:34 +0000

    2363 - Business case status - converted exchange rate to currency LEFT JOIN
    git-tfs-id: [http://developmentvlan:8585/tfs/vanrise.collection]$/;C38406

--- TOneV2/Code/TOneV2/TOne.WhS.Analytics.Data.SQL/BillingReportDataManager.cs
+++ TOneV2/Code/TOneV2/TOne.WhS.Analytics.Data.SQL/BillingReportDataManager.cs
@@ -36,32 +36,32 @@ namespace TOne.WhS.Analytics.Data.SQL
             if (toDate.HasValue)
                 _toTodate = (DateTime)toDate;
 
-            string query = String.Format(@"{2}
-                SELECT TOP (@TopDestination)  {3} AS ZoneId into #BillingStatsDailyTemp 
-                From [TOneWhS_Analytics].[BillingStatsDaily] BS WITH(NOLOCK,INDEX(IX_BillingStatsDaily_BatchStart,IX_BillingStatsDaily_Id)), 
-                    @ConvertedExchangeRates as ERC, @ConvertedExchangeRates as ERS
+            string query = String.Format(@"
+                SELECT TOP (@TopDestination)  {2} AS ZoneId into #BillingStatsDailyTemp 
+                From [TOneWhS_Analytics].[BillingStatsDaily] BS WITH(NOLOCK,INDEX(IX_BillingStatsDaily_BatchStart,IX_BillingStatsDaily_Id))
+                LEFT JOIN (select * from Common.getExchangeRatesConvertedToCurrency({3} , @FromDate, @ToDate)) AS ERC ON BS.CostCurrencyId = ERC.CurrencyID 
+                LEFT JOIN (select * from Common.getExchangeRatesConvertedToCurrency({3} , @FromDate, @ToDate)) AS ERS ON BS.SaleCurrencyId = ERS.CurrencyID 
                 WHERE BS.BatchStart >= @FromDate AND BS.BatchStart < @ToDate AND {1} = @CustomerId 
-                And ERC.CurrencyID = BS.CostCurrencyId AND BS.BatchStart >= ERC.BED AND (ERC.EED IS NULL OR BS.BatchStart < ERC.EED) 
-                And ERS.CurrencyID = BS.SaleCurrencyId AND BS.BatchStart >= ERS.BED AND (ERS.EED IS NULL OR BS.BatchStart < ERS.EED)
-                GROUP BY {3} , Year(BS.BatchStart), MONTH(BS.BatchStart) ORDER BY Year(BS.BatchStart) asc, MONTH(BS.BatchStart) asc,
+                GROUP BY {2} , Year(BS.BatchStart), MONTH(BS.BatchStart) ORDER BY Year(BS.BatchStart) asc, MONTH(BS.BatchStart) asc,
                     (cast( (SUM({0} )/60 ) as decimal(13,4) )) desc
 
-                SELECT {3} AS ZoneId ,Year(BS.BatchStart) AS YearDuration, 
+                SELECT {2} AS ZoneId ,Year(BS.BatchStart) AS YearDuration, 
                 MONTH(BS.BatchStart) AS MonthDuration, 
                 cast( (SUM({0} )/60 ) as decimal(13,4) ) AS SaleDuration 
                 From [TOneWhS_Analytics].[BillingStatsDaily] BS WITH(NOLOCK,INDEX(IX_BillingStatsDaily_BatchStart,IX_BillingStatsDaily_Id)) 
-                    JOIN #BillingStatsDailyTemp t on {3} = t.ZoneId,
-                    @ConvertedExchangeRates as ERC, @ConvertedExchangeRates as ERS
+                    JOIN #BillingStatsDailyTemp t on {2} = t.ZoneId
+                LEFT JOIN (select * from Common.getExchangeRatesConvertedToCurrency(4 , @FromDate, @ToDate)) AS ERC 
+                ON BS.CostCurrencyId = ERC.CurrencyID 
+                LEFT JOIN (select * from Common.getExchangeRatesConvertedToCurrency(4 , @FromDate, @ToDate)) AS ERS 
+                ON BS.SaleCurrencyId = ERS.CurrencyID
                 WHERE BS.BatchStart >= @FromDate AND BS.BatchStart < @ToDate AND {1} = @CustomerId 
-                And ERC.CurrencyID = BS.CostCurrencyId AND BS.BatchStart >= ERC.BED AND (ERC.EED IS NULL OR BS.BatchStart < ERC.EED) 
-                And ERS.CurrencyID = BS.SaleCurrencyId AND BS.BatchStart >= ERS.BED AND (ERS.EED IS NULL OR BS.BatchStart < ERS.EED)
-                GROUP BY {3} , Year(BS.BatchStart), MONTH(BS.BatchStart) ORDER BY Year(BS.BatchStart) asc, MONTH(BS.BatchStart) asc,
+                GROUP BY {2} , Year(BS.BatchStart), MONTH(BS.BatchStart) ORDER BY Year(BS.BatchStart) asc, MONTH(BS.BatchStart) asc,
                     (cast( (SUM({0} )/60 ) as decimal(13,4) )) desc drop table #BillingStatsDailyTemp",
-                amountDuration,
-                carrierId,
-                GetExchangeRatesTable(currencyId),
-                zone
-                );
+            amountDuration,
+            carrierId,
+            zone,
+            currencyId
+            );
 
             return GetItemsText(query, BusinessCaseStatusMapper,
             (cmd) =>
