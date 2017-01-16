15c75d583e57601e9cb5695cd546727dd70b339d
mohamad.elhariri <mohamad.elhariri@vanrise.tfs.local>
2017-01-16 07:29:11 +0000

    Added the ConfigId property
    git-tfs-id: [http://developmentvlan:8585/tfs/vanrise.collection]$/;C38459

--- TOneV2/Code/TOneV2/TOne.WhS.Sales.Entities/BulkActions/BulkActionType.cs
+++ TOneV2/Code/TOneV2/TOne.WhS.Sales.Entities/BulkActions/BulkActionType.cs
@@ -6,32 +6,33 @@ using System.Threading.Tasks;
 
 namespace TOne.WhS.Sales.Entities
 {
-    public abstract class BulkActionType
-    {
-        public abstract bool IsApplicableToZone(IActionApplicableToZoneContext context);
+	public abstract class BulkActionType
+	{
+		public abstract Guid ConfigId { get; }
 
-        public abstract void ApplyBulkActionToZoneItem(IApplyBulkActionToZoneItemContext context);
+		public abstract bool IsApplicableToZone(IActionApplicableToZoneContext context);
 
-        public abstract void ApplyBulkActionToZoneDraft(IApplyBulkActionToZoneDraftContext context);
-    }
+		public abstract void ApplyBulkActionToZoneItem(IApplyBulkActionToZoneItemContext context);
 
-    public interface IActionApplicableToZoneContext
-    {
-        long ZoneId { get; }
+		public abstract void ApplyBulkActionToZoneDraft(IApplyBulkActionToZoneDraftContext context);
+	}
 
-        ZoneChanges ZoneDraft { get; }
-    }
-    
-    public interface IApplyBulkActionToZoneDraftContext
-    {
-        ZoneChanges ZoneDraft { get; }
-        
-        ZoneItem GetZoneItem(long zoneId);
-    }
+	public interface IActionApplicableToZoneContext
+	{
+		long ZoneId { get; }
 
-    public interface IApplyBulkActionToZoneItemContext
-    {
-        ZoneItem ZoneItem { get; }
-    }
+		ZoneChanges ZoneDraft { get; }
+	}
 
+	public interface IApplyBulkActionToZoneItemContext
+	{
+		ZoneItem ZoneItem { get; }
+	}
+
+	public interface IApplyBulkActionToZoneDraftContext
+	{
+		ZoneChanges ZoneDraft { get; }
+
+		ZoneItem GetZoneItem(long zoneId);
+	}
 }
