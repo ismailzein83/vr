c8352073d7ff87d41a4916eb5112f5d1134eb2c7
Ismail Zein <VANRISE\ismail.zein>
2017-01-19 14:50:57 +0000

    sql project
    git-tfs-id: [http://developmentvlan:8585/tfs/vanrise.collection]$/;C38563

--- TOneV2/Code/TOneV2/SQL.TOneConfiguration/genericdata/Stored Procedures/sp_BEParentChildRelation_Insert.sql
+++ TOneV2/Code/TOneV2/SQL.TOneConfiguration/genericdata/Stored Procedures/sp_BEParentChildRelation_Insert.sql
@@ -1,14 +1,14 @@
-﻿
-CREATE Procedure [genericdata].[sp_BEParentChildRelation_Insert]
-    @RelationDefinitionID uniqueidentifier = null,
-    @ParentBEID nvarchar(255) = null,
-    @ChildBEID nvarchar(255) = null,
-    @BED datetime = null,
-    @EED datetime = null,
+﻿CREATE Procedure [genericdata].[sp_BEParentChildRelation_Insert]
+    @RelationDefinitionID uniqueidentifier,
+    @ParentBEID nvarchar(255),
+    @ChildBEID nvarchar(255),
+    @BED datetime,
+    @EED datetime,
 	@Id int out
 AS
 BEGIN
-	IF NOT EXISTS(select 1 from [genericdata].[BEParentChildRelation] where ParentBEID = @ParentBEID and ChildBEID = @ChildBEID)
+	IF NOT EXISTS(select 1 from [genericdata].[BEParentChildRelation] 
+			      where RelationDefinitionID = @RelationDefinitionID and ParentBEID = @ParentBEID and ChildBEID = @ChildBEID)
 	BEGIN
 		insert into [genericdata].[BEParentChildRelation] ([RelationDefinitionID],[ParentBEID],[ChildBEID],[BED],[EED])
 		values(@RelationDefinitionID, @ParentBEID, @ChildBEID, @BED, @EED)
