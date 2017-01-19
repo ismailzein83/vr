ada98441b0466dca6cc7ae6308c7eb4e9a6e84d9
Ali Atoui <VANRISE\ali.atoui>
2017-01-19 14:38:41 +0000

    Adding RetailBETechnicalSettings
    git-tfs-id: [http://developmentvlan:8585/tfs/vanrise.collection]$/;C38560

---
+++ Retail/Code/Retail.BusinessEntity.Entities/Settings/RetailBETechnicalSettings.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Vanrise.Entities;
+
+namespace Retail.BusinessEntity.Entities.Settings
+{
+    public class RetailBETechnicalSettings : SettingData
+    {
+        public const string SETTING_TYPE = "Retail_BE_TechnicalSettings";
+
+        public IncludedAccountTypes IncludedAccountTypes { get; set; }
+    }
+
+    public class IncludedAccountTypes
+    {
+        public List<Guid> AcountTypeIds { get; set; }
+    }
+}
