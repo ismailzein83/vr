233a947671132d1479997de777566a0d532a86e8
Ismail Zein <VANRISE\ismail.zein>
2017-01-20 12:57:31 +0000

    created BaseCodeActivity
    git-tfs-id: [http://developmentvlan:8585/tfs/vanrise.collection]$/;C38592

---
+++ BusinessProcess/Code/Vanrise.BusinessProcess/Base Classes/BaseCodeActivity.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Activities;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Vanrise.BusinessProcess
+{
+    public abstract class BaseCodeActivity : CodeActivity
+    {
+        protected override sealed void Execute(CodeActivityContext context)
+        {
+            Vanrise.Security.Entities.ContextFactory.GetContext().SetContextUserId(context.GetSharedInstanceData().InstanceInfo.InitiatorUserId);
+            VRExecute(new BaseCodeActivityContext { ActivityContext = context });
+        }
+
+        protected abstract void VRExecute(IBaseCodeActivityContext context);
+
+        private class BaseCodeActivityContext : IBaseCodeActivityContext
+        {
+            public CodeActivityContext ActivityContext
+            {
+                get;
+                set;
+            }
+        }
+    }
+
+    public interface IBaseCodeActivityContext
+    {
+        CodeActivityContext ActivityContext { get; }
+    }
+
+    
+}
