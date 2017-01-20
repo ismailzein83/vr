3bb8ddc9595539052a6d6755074f0849a5758e72
Samer Haidar <VANRISE\samer.haidar>
2017-01-20 09:45:45 +0000

    adding subprocess for automatic invoice
    git-tfs-id: [http://developmentvlan:8585/tfs/vanrise.collection]$/;C38581

--- Vanrise/Code/Vanrise.Invoice.BP.Activities/GeneratePartnersInvoices.cs
+++ Vanrise/Code/Vanrise.Invoice.BP.Activities/GeneratePartnersInvoices.cs
@@ -14,36 +14,34 @@ namespace Vanrise.Invoice.BP.Activities
         [RequiredArgument]
         public InArgument<Guid> InvoiceTypeId { get; set; }
         [RequiredArgument]
-        public InArgument<IEnumerable<string>> PartnerIds { get; set; }
+        public InArgument<string> PartnerId { get; set; }
         #endregion
 
         protected override void Execute(CodeActivityContext context)
         {
             var invoiceTypeId = context.GetValue(this.InvoiceTypeId);
-            var partnerIds = context.GetValue(this.PartnerIds);
+            var partnerId = context.GetValue(this.PartnerId);
             InvoiceManager invoiceManager = new InvoiceManager();
             invoiceManager.userId = context.GetSharedInstanceData().InstanceInfo.InitiatorUserId;
             var issueDate = DateTime.Now.AddYears(1);
-            if(partnerIds != null)
+            if (partnerId != null)
             {
                 PartnerManager partnerManager = new PartnerManager();
-                foreach(var partnerId in partnerIds)
+
+                var partnerSetting = partnerManager.GetInvoicePartnerSetting(invoiceTypeId, partnerId);
+                if (partnerSetting.InvoiceSetting.Details.EnableAutomaticInvoice)
                 {
-                    var partnerSetting = partnerManager.GetInvoicePartnerSetting(invoiceTypeId, partnerId);
-                    if(partnerSetting.InvoiceSetting.Details.EnableAutomaticInvoice)
+                    var billingPeriod = invoiceManager.GetBillingInterval(invoiceTypeId, partnerId, issueDate);
+                    if (billingPeriod != null)
                     {
-                        var billingPeriod = invoiceManager.GetBillingInterval(invoiceTypeId, partnerId, issueDate);
-                        if (billingPeriod != null)
+                        invoiceManager.GenerateInvoice(new Entities.GenerateInvoiceInput
                         {
-                            invoiceManager.GenerateInvoice(new Entities.GenerateInvoiceInput
-                            {
-                                InvoiceTypeId = invoiceTypeId,
-                                IssueDate = issueDate,
-                                PartnerId = partnerId,
-                                FromDate = billingPeriod.FromDate,
-                                ToDate = billingPeriod.ToDate
-                            });
-                        }
+                            InvoiceTypeId = invoiceTypeId,
+                            IssueDate = issueDate,
+                            PartnerId = partnerId,
+                            FromDate = billingPeriod.FromDate,
+                            ToDate = billingPeriod.ToDate
+                        });
                     }
                 }
             }
