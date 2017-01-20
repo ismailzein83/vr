ec4b3f48bd64ba39dcca70d39481534885d9cb81
mostafa.jawhari <mostafa.jawhari@vanrise.tfs.local>
2017-01-20 15:41:59 +0000

    adding needed file to implement TQI
    git-tfs-id: [http://developmentvlan:8585/tfs/vanrise.collection]$/;C38602

---
+++ TOneV2/Code/TOneV2/TOne.WhS.Sales.Entities/TQI/TQIEvaluatedRateInput.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TOne.WhS.Routing.Entities;
+
+namespace TOne.WhS.Sales.Entities
+{
+    public class TQIEvaluatedRateInput
+    {
+        public TQIMethod TQIMethod { get; set; }
+        public RPRouteDetail RPRouteDetail { get; set; }
+    }
+}
---
+++ TOneV2/Code/TOneV2/TOne.WhS.Sales.Entities/TQI/TQISupplierPercentage.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TOne.WhS.Sales.Entities
+{
+    public class TQISupplierPercentage
+    {
+        public string SupplierName { get; set; }
+        public decimal MarginPercentage { get; set; }
+    }
+}
