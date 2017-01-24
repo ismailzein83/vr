e70447fb28368a51a4911271069fe7719ad0c7f3
Ali Atoui <VANRISE\ali.atoui>
2017-01-24 08:31:23 +0000

    Adding ProductFamilyInfo, Detail, Filter and Query
    git-tfs-id: [http://developmentvlan:8585/tfs/vanrise.collection]$/;C38668

---
+++ Retail/Code/Retail.BusinessEntity.Entities/ProductFamily/ProductFamilyInfo.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Retail.BusinessEntity.Entities
+{
+    public class ProductFamilyInfo
+    {
+        public int ProductFamilyId { get; set; }
+
+        public string Name { get; set; }
+
+        public Guid ProductDefinitionId { get; set; }
+    }
+}
---
+++ Retail/Code/Retail.BusinessEntity.Entities/ProductFamily/ProductFamilyQuery.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Retail.BusinessEntity.Entities
+{
+    public class ProductFamilyQuery
+    {
+        public string Name { get; set; }
+    }
+}
