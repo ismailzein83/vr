c3cdd03b2700abf7b48a7c1104b3de43be7cd880
Ali Ballouk <VANRISE\ali.ballouk>
2017-01-25 14:21:13 +0000

    Add UISettingsData  class
    git-tfs-id: [http://developmentvlan:8585/tfs/vanrise.collection]$/;C38731

---
+++ Vanrise/Code/Vanrise.Entities/UISettingData.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Vanrise.Entities
+{
+    public class GeneralSettingData :SettingData
+    {
+        public UISettingData UIData { get; set; }
+    }
+    public class UISettingData
+    {
+        public Guid? DefaultViewId { get; set; }
+
+        public int? NormalPrecision { get; set; }
+
+        public int? LongPrecision { get; set; }
+
+        public int? GridPageSize { get; set; }
+    }
+}
