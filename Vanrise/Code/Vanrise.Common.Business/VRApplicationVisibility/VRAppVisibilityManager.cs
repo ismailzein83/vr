56fb185bfcbbe491044239af041f5158ee509d71
Ismail Zein <VANRISE\ismail.zein>
2017-01-25 16:31:05 +0000

    added VRAppVisibility
    git-tfs-id: [http://developmentvlan:8585/tfs/vanrise.collection]$/;C38744

---
+++ Vanrise/Code/Vanrise.Common.Business/VRApplicationVisibility/VRAppVisibilityManager.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Vanrise.Entities;
+
+namespace Vanrise.Common.Business
+{
+    public class VRAppVisibilityManager
+    {
+        public T GetModuleVisibility<T>() where T : VRModuleVisibility
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ Vanrise/Code/Vanrise.Entities/VRApplicationVisibility/VRApplicationVisibility.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Vanrise.Entities
+{
+    public class VRApplicationVisibility
+    {
+        public Guid VRApplicationVisibilityId { get; set; }
+
+        public string Name { get; set; }
+
+        public VRApplicationVisibilitySettings Settings { get; set; }
+    }
+
+    public class VRApplicationVisibilitySettings
+    {
+        public Dictionary<Guid, VRModuleVisibility> ModulesVisibility { get; set; }
+    }
+
+    public abstract class VRModuleVisibility
+    {
+        public abstract Guid ConfigId { get; }
+    }
+}
