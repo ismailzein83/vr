72b67a4f2bdb60ead91d3d0b215ff8747e3a3b75
Ismail Zein <VANRISE\ismail.zein>
2017-01-26 17:03:43 +0000

    sql project
    git-tfs-id: [http://developmentvlan:8585/tfs/vanrise.collection]$/;C38801

--- Retail/Code/Retail/Retail_BE/Tables/DID.sql
+++ Retail/Code/Retail/Retail_BE/Tables/DID.sql
@@ -6,8 +6,10 @@
     [timestamp]   ROWVERSION     NULL,
     [CreatedTime] DATETIME       CONSTRAINT [DF_DID_CreatedTime] DEFAULT (getdate()) NULL,
     CONSTRAINT [PK_DID] PRIMARY KEY CLUSTERED ([ID] ASC),
-    CONSTRAINT [IX_DID_Number] UNIQUE NONCLUSTERED ([Old_Number] ASC)
+    CONSTRAINT [IX_DID_Number] UNIQUE NONCLUSTERED ([Number] ASC)
 );
+
+
 
 
 
