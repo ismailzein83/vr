75ec0fe62a3ebba75c4bc6166df8163e88d9dc57
mohamad.elhariri <mohamad.elhariri@vanrise.tfs.local>
2017-01-27 12:44:33 +0000

    Added the Specific Applicable Zone enums
    git-tfs-id: [http://developmentvlan:8585/tfs/vanrise.collection]$/;C38824

---
+++ TOneV2/Code/TOneV2/TOne.WhS.Sales.Entities/BulkActions/Enums.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TOne.WhS.Sales.Entities.BulkActions
+{
+	public enum SpecificApplicableZoneEntityType
+	{
+		Country = 0,
+		Zone = 1
+	}
+
+	public enum SpecificApplicableZoneFilterType
+	{
+		Specific = 0,
+		AllExcept = 1
+	}
+}
