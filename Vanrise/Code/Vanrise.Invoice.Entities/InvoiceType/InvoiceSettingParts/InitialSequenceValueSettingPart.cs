12a1f1b3865e8080cea366305ba065cda02af504
Samer Haidar <VANRISE\samer.haidar>
2017-01-30 13:12:06 +0000

    adding Intial sequence
    git-tfs-id: [http://developmentvlan:8585/tfs/vanrise.collection]$/;C38858

---
+++ Vanrise/Code/Vanrise.Invoice.Entities/InvoiceType/InvoiceSettingParts/InitialSequenceValueSettingPart.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Vanrise.Invoice.Entities
+{
+    public class InitialSequenceValueSettingPart : InvoiceSettingPart
+    {
+        public int InitialValue { get; set; }
+        public override Guid ConfigId
+        {
+            get { return new Guid("D75CD1BA-F26B-4E17-BC3C-59996A00495A"); }
+        }
+
+    }
+}
---
+++ Vanrise/Code/Vanrise.Invoice.Web/VR_Invoice/Directives/InvoiceSetting/MainExtensions/vr-invoice-invoicesetting-runtime-initialsequencevalue.js
@@ -0,0 +1,68 @@
+﻿'use strict';
+app.directive('vrInvoiceInvoicesettingRuntimeInitialsequencevalue', ['UtilsService', 'VRUIUtilsService',
+    function (UtilsService, VRUIUtilsService) {
+
+        var directiveDefinitionObject = {
+            restrict: 'E',
+            scope:
+            {
+                onReady: '=',
+                normalColNum: '@',
+            },
+            controller: function ($scope, $element, $attrs) {
+
+                var ctrl = this;
+
+                var ctor = new DirectiveCtor(ctrl, $scope);
+                ctor.initializeController();
+
+            },
+            controllerAs: 'ctrl',
+            bindToController: true,
+            compile: function (element, attrs) {
+                return {
+                    pre: function ($scope, iElem, iAttrs, ctrl) {
+
+                    }
+                };
+            },
+            templateUrl: function (element, attrs) {
+                return '/Client/Modules/VR_Invoice/Directives/InvoiceSetting/MainExtensions/Templates/InitialSequenceValuePartTemplate.html';
+            }
+
+        };
+
+        function DirectiveCtor(ctrl, $scope) {
+            var currentContext;
+
+            function initializeController() {
+                $scope.scopeModel = {};
+                defineAPI();
+            }
+
+            function defineAPI() {
+                var api = {};
+
+                api.load = function (payload) {
+                    if(payload != undefined && payload.fieldValue != undefined)
+                    {
+                        $scope.scopeModel.initialValue = payload.fieldValue.InitialValue;
+                    }
+                };
+
+                api.getData = function () {
+                    return {
+                        $type: "Vanrise.Invoice.Entities.InitialSequenceValueSettingPart,Vanrise.Invoice.Entities",
+                        InitialValue: $scope.scopeModel.initialValue
+                    };
+                };
+
+                if (ctrl.onReady != null)
+                    ctrl.onReady(api);
+            }
+
+            this.initializeController = initializeController;
+        }
+        return directiveDefinitionObject;
+    }
+]);
