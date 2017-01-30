c75a4bac0cdad8110c3485ce0197a7d603b654d1
anthony douaihy <VANRISE\anthony.douaihy>
2017-01-30 10:13:55 +0000

    SpecialRequestOptionSettings for LCR Route Rule
    git-tfs-id: [http://developmentvlan:8585/tfs/vanrise.collection]$/;C38848

--- TOneV2/Code/TOneV2/TOne.WhS.Routing.Business/RouteRules/LCRRouteRule.cs
+++ TOneV2/Code/TOneV2/TOne.WhS.Routing.Business/RouteRules/LCRRouteRule.cs
@@ -10,6 +10,8 @@ namespace TOne.WhS.Routing.Business
         #region Properties/Ctor
 
         public override Guid ConfigId { get { return new Guid("31B3226E-A2B2-40D5-8C33-83C6601E8730"); } }
+        
+        public List<SpecialRequestOptionSettings> Options { get; set; }
 
         public override bool UseOrderedExecution
         {
--- TOneV2/Code/TOneV2/TOne.WhS.Routing.Entities/RouteOptionSettings.cs
+++ TOneV2/Code/TOneV2/TOne.WhS.Routing.Entities/RouteOptionSettings.cs
@@ -12,4 +12,17 @@ namespace TOne.WhS.Routing.Entities
 
         public Decimal? Percentage { get; set; }
     }
+
+    public class SpecialRequestOptionSettings
+    {
+        public int SupplierId { get; set; }
+
+        public int Position { get; set; }
+
+        public bool ForceOption { get; set; }
+
+        public int NumberOfTries { get; set; }
+
+        public Decimal? Percentage { get; set; }
+    }
 }
