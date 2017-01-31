5d5ca6a6d1208ab409d8180ebc8e1d26c89ba412
Ali Ballouk <VANRISE\ali.ballouk>
2017-01-31 15:13:11 +0000

    Add vr-warning base directive
    git-tfs-id: [http://developmentvlan:8585/tfs/vanrise.collection]$/;C38896

---
+++ Vanrise/Code/Vanrise.Web/Client/Javascripts/Directives/Inputs/Warning/vr-warning-directive.js
@@ -0,0 +1,40 @@
+﻿'use strict';
+
+app.directive('vrWarning', ['SecurityService', function (SecurityService) {
+
+    var directiveDefinitionObject = {
+        restrict: 'E',
+        scope: {
+            value:'@'
+        },
+        controller: function ($scope, $element) {
+
+            $scope.adjustTooltipPosition = function (e) {
+                setTimeout(function () {
+                    var self = angular.element(e.currentTarget);
+                    var selfHeight = $(self).height();
+                    var selfOffset = $(self).offset();
+                    var tooltip = self.parent().find('.tooltip-info')[0];
+                    $(tooltip).css({ display: 'block !important' });
+                    var innerTooltip = self.parent().find('.tooltip-inner')[0];
+                    var innerTooltipArrow = self.parent().find('.tooltip-arrow')[0];
+                    $(innerTooltip).css({ position: 'fixed', top: selfOffset.top - $(window).scrollTop() + selfHeight + 20, left: selfOffset.left - 5 });
+                    $(innerTooltipArrow).css({ position: 'fixed', top: selfOffset.top - $(window).scrollTop() + selfHeight + 15, left: selfOffset.left });
+                }, 1);
+            };
+        },
+        link: function (scope, element, attrs, ctrl) {
+            if (attrs.value != undefined)
+                scope.value = attrs.value;
+           
+            
+        },
+        template: function (element, attrs) {
+            return '<span ng-if="value!=undefined" ng-mouseenter="adjustTooltipPosition($event)" bs-tooltip class="glyphicon glyphicon-exclamation-sign hand-cursor" style="color:#f0ad4e;top:2px" html="true" placement="bottom" trigger="hover" data-type="info" data-title="{{value}}"></span>';
+        }
+
+    };
+
+    return directiveDefinitionObject;
+
+}]);
