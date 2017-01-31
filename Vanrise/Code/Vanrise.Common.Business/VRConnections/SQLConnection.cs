de33d531c58d39cadc186cbcadaa100dc74f20af
Ali Ballouk <VANRISE\ali.ballouk>
2017-01-31 09:10:30 +0000

    Add SQLConnection class
    git-tfs-id: [http://developmentvlan:8585/tfs/vanrise.collection]$/;C38877

---
+++ Vanrise/Code/Vanrise.Common.Business/VRConnections/SQLConnection.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using Vanrise.Entities;
+using Vanrise.Security.Entities;
+
+namespace Vanrise.Common.Business
+{
+    public class SQLConnection : VRConnectionSettings
+    {
+        public override Guid ConfigId { get { return new Guid("8224B27C-C128-4150-A4E4-5E2034BB3A36"); } }
+        
+        public string ConnectionString { get; set; }
+
+        
+    }
+
+   
+}
