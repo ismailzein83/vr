8afe65aee2e818925a0f757bed7f7c2b51325616
mostafa.jawhari <mostafa.jawhari@vanrise.tfs.local>
2017-01-31 08:50:50 +0000

    adjusting the message when clicking on TQI of a zone having no route options
    git-tfs-id: [http://developmentvlan:8585/tfs/vanrise.collection]$/;C38874

--- TOneV2/Code/TOneV2/TOne.WhS.Sales.Web/WhS_Sales/Directives/vr-whs-sales-rateplan-grid.js
+++ TOneV2/Code/TOneV2/TOne.WhS.Sales.Web/WhS_Sales/Directives/vr-whs-sales-rateplan-grid.js
@@ -119,7 +119,7 @@ app.directive("vrWhsSalesRateplanGrid", ["WhS_Sales_RatePlanAPIService", "UtilsS
                 if (dataItem.RPRouteDetail != undefined && dataItem.RPRouteDetail.RouteOptionsDetails != undefined && dataItem.RPRouteDetail.RouteOptionsDetails.length > 0)
                     WhS_Sales_RatePlanService.openTQIEditor(context, onTQIEvaluated);
                 else
-                    VRNotificationService.showInformation("TQI can not be calculated for zones have no route options");
+                    VRNotificationService.showInformation("TQI can not be calculated for zones having no route options");
             };
 
             $scope.onNewRateChanged = function (dataItem) {
