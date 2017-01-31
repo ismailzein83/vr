0cc4a67654e296d95fe4c7afc7d4a9cc1fed360e
Ali Atoui <VANRISE\ali.atoui>
2017-01-31 15:30:26 +0000

    Editing vr-section-directive
    git-tfs-id: [http://developmentvlan:8585/tfs/vanrise.collection]$/;C38897

--- Vanrise/Code/Vanrise.Web/Client/Javascripts/Directives/Containers/vr-section-directive.js
+++ Vanrise/Code/Vanrise.Web/Client/Javascripts/Directives/Containers/vr-section-directive.js
@@ -8,11 +8,12 @@ app.directive('vrSection', ['UtilsService', function (UtilsService) {
         compile: function (tElement, tAttrs) {
             var classlevel = "1";
             var collapsible = tAttrs.collapsible != undefined;
+            var title = tAttrs.header || tAttrs.title;
             if (tAttrs.level != undefined && tAttrs.level == "2")
                 classlevel = " panel-vr-child ";
             var expandname = 'expanded_' + UtilsService.replaceAll(UtilsService.guid(), '-', '');
             var newElement = '<div class="panel-primary panel-vr ' + classlevel + '" >'
-                            + '<div class="panel-heading" ng-init="' + expandname + '=true" expanded="{{' + expandname + '}}"><label><span style="padding-right: 3px;" class="hand-cursor glyphicon " ng-show="' + collapsible + '" ng-click="' + expandname + '=!' + expandname + '" ng-class="' + expandname + '?\'glyphicon-collapse-up\':\'glyphicon-collapse-down\'" ></span>' + tAttrs.title + '</label></div>'
+                            + '<div class="panel-heading" ng-init="' + expandname + '=true" expanded="{{' + expandname + '}}"><label><span style="padding-right: 3px;" class="hand-cursor glyphicon " ng-show="' + collapsible + '" ng-click="' + expandname + '=!' + expandname + '" ng-class="' + expandname + '?\'glyphicon-collapse-up\':\'glyphicon-collapse-down\'" ></span>' + title + '</label></div>'
                             + '<div class="panel-body" ng-show="' + expandname + '">' + tElement.context.innerHTML + '</div></div>';
             tElement.html(newElement);
         }
