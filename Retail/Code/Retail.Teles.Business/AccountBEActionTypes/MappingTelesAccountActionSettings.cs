043ceb920f39174f7065e750b4330b47e49c7b30
Samer Haidar <VANRISE\samer.haidar>
2017-02-01 12:41:37 +0000

    implementing MappingTelesAccountActionSettings
    git-tfs-id: [http://developmentvlan:8585/tfs/vanrise.collection]$/;C38922

---
+++ Retail/Code/Retail.Teles.Business/AccountBEActionTypes/MappingTelesAccountActionSettings.cs
@@ -0,0 +1,24 @@
+﻿using Retail.BusinessEntity.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Retail.Teles.Business.AccountBEActionTypes
+{
+    public class MappingTelesAccountActionSettings : AccountActionDefinitionSettings
+    {
+        public override Guid ConfigId
+        {
+            get { return new Guid("09A1029D-AFC0-48E4-B1B3-FD951462E267"); }
+        }
+
+        public override string ClientActionName
+        {
+            get { return " MappingTelesAccount"; }
+        }
+        public long DomainId { get; set; }
+        public int SwitchId { get; set; }
+    }
+}
