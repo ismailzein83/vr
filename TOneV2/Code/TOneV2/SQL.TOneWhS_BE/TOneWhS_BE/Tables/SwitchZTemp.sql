a61f886557cb7fbf585923ae9f35466d6c7d91fa
Wissam Ajamy <VANRISE\wissam.ajamy>
2017-02-01 13:43:55 +0000

    Update structure from TOneV2_Dev
    git-tfs-id: [http://developmentvlan:8585/tfs/vanrise.collection]$/;C38931

--- TOneV2/Code/TOneV2/SQL.TOneWhS_BE/TOneWhS_BE/Tables/SwitchZTemp.sql
+++
@@ -1,9 +0,0 @@
-﻿CREATE TABLE [TOneWhS_BE].[SwitchZTemp] (
-    [ID]          INT            IDENTITY (1, 1) NOT NULL,
-    [Name]        VARCHAR (50)   NOT NULL,
-    [Settings]    NVARCHAR (MAX) NULL,
-    [timestamp]   ROWVERSION     NULL,
-    [SourceID]    VARCHAR (50)   NULL,
-    [CreatedTime] DATETIME       NULL
-);
-
