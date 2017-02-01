d61e8aca543b514a83238a19ebcdc8fb9cc07bb1
Ismail Zein <VANRISE\ismail.zein>
2017-02-01 11:11:36 +0000

    added Change Routing Group skeleton
    git-tfs-id: [http://developmentvlan:8585/tfs/vanrise.collection]$/;C38918

---
+++ Retail/Code/Retail.Teles.Business/ChangeUsersRoutingGroup.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Vanrise.Integration.Entities;
+
+namespace Retail.Teles.Business
+{
+    public class ChangeUsersRoutingGroup : ActionProvisionerDefinitionSettings
+    {
+        public override Guid ConfigId
+        {
+            get { throw new NotImplementedException(); }
+        }
+
+        public int RoutingGroupId { get; set; }
+    }
+}
