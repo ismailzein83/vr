11f991f148748ee599f2e93bff2e2c657d06278d
Wissam Ajamy <VANRISE\wissam.ajamy>
2017-02-01 09:26:31 +0000

    Update structure from TOneV2_Dev_Backup
    git-tfs-id: [http://developmentvlan:8585/tfs/vanrise.collection]$/;C38909

---
+++ TOneV2/Code/TOneV2/SQL.TOneWhS_BE_StateBackup/TOneWhS_BE_Bkup/Tables/CustomerCountry.sql
@@ -0,0 +1,9 @@
+﻿CREATE TABLE [TOneWhS_BE_Bkup].[CustomerCountry] (
+    [ID]            BIGINT   NOT NULL,
+    [CustomerID]    INT      NOT NULL,
+    [CountryID]     INT      NOT NULL,
+    [BED]           DATETIME NOT NULL,
+    [EED]           DATETIME NULL,
+    [StateBackupID] BIGINT   NOT NULL
+);
+
