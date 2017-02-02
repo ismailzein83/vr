23fa23da285741e39725c16ca21c4337d32764eb
Samer Haidar <VANRISE\samer.haidar>
2017-02-02 07:59:57 +0000

    adding InterpriseAccountMappingInfo
    git-tfs-id: [http://developmentvlan:8585/tfs/vanrise.collection]$/;C38944

---
+++ Retail/Code/Retail.Teles.Entities/InterpriseAccountMappingInfo.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Retail.Teles.Entities
+{
+    public class InterpriseAccountMappingInfo
+    {
+        public int TelesEnterpriseId { get; set; }
+    }
+}
