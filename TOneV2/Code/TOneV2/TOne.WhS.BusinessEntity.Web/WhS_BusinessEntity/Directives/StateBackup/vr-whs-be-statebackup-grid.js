4ba834543c4c0401942722a504446965707d2033
Zeinab Mokdad <VANRISE\zeinab.mokdad>
2017-02-03 07:30:07 +0000

    removing loader on error exception
    git-tfs-id: [http://developmentvlan:8585/tfs/vanrise.collection]$/;C38980

--- TOneV2/Code/TOneV2/TOne.WhS.BusinessEntity.Web/WhS_BusinessEntity/Directives/StateBackup/vr-whs-be-statebackup-grid.js
+++ TOneV2/Code/TOneV2/TOne.WhS.BusinessEntity.Web/WhS_BusinessEntity/Directives/StateBackup/vr-whs-be-statebackup-grid.js
@@ -30,31 +30,31 @@ function (UtilsService, VRNotificationService, WhS_BE_StateBackupAPIService) {
         this.initializeController = initializeController;
 
         function initializeController() {
-           
+
             $scope.stateBackups = [];
 
             defineMenuActions();
 
             $scope.onGridReady = function (api) {
                 gridAPI = api;
-                
+
                 if (ctrl.onReady != undefined && typeof (ctrl.onReady) == "function")
                     ctrl.onReady(getDirectiveAPI());
                 function getDirectiveAPI() {
-                   
+
                     var directiveAPI = {};
                     directiveAPI.loadGrid = function (query) {
                         gquery = query;
                         return gridAPI.retrieveData(query);
                     };
-                   
+
                     return directiveAPI;
                 }
             };
             $scope.dataRetrievalFunction = function (dataRetrievalInput, onResponseReady) {
                 return WhS_BE_StateBackupAPIService.GetFilteredStateBackups(dataRetrievalInput)
                     .then(function (response) {
-                         onResponseReady(response);
+                        onResponseReady(response);
                     })
                     .catch(function (error) {
                         VRNotificationService.notifyException(error, $scope);
@@ -89,7 +89,9 @@ function (UtilsService, VRNotificationService, WhS_BE_StateBackupAPIService) {
                     return WhS_BE_StateBackupAPIService.RestoreData(stateBackupObject.Entity.StateBackupId).then(function (response) {
                         if (VRNotificationService.notifyOnItemUpdated("State Backup", response, ""))
                             onStateBackupRestored(response.UpdatedObject);
-                            ctrl.isRestoring = false;
+                        ctrl.isRestoring = false;
+                    }).finally(function () {
+                        ctrl.isRestoring = false;
                     });
                 }
             });
