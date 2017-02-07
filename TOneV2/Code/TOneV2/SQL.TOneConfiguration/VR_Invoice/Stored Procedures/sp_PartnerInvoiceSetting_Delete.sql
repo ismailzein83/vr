451d68b04b58a0a46604eb4fc09dd3a320e4f5b1
Wissam Ajamy <VANRISE\wissam.ajamy>
2017-02-07 07:50:17 +0000

    Update structure from TOneConfiguration
    git-tfs-id: [http://developmentvlan:8585/tfs/vanrise.collection]$/;C39033

---
+++ TOneV2/Code/TOneV2/SQL.TOneConfiguration/VR_Invoice/Stored Procedures/sp_PartnerInvoiceSetting_Delete.sql
@@ -0,0 +1,12 @@
+﻿-- =============================================
+-- Author:		<Author,,Name>
+-- Create date: <Create Date,,>
+-- Description:	<Description,,>
+-- =============================================
+create PROCEDURE VR_Invoice.sp_PartnerInvoiceSetting_Delete
+	@Id uniqueidentifier
+AS
+BEGIN
+	DELETE FROM VR_Invoice.PartnerInvoiceSetting
+	WHERE ID = @Id
+END
