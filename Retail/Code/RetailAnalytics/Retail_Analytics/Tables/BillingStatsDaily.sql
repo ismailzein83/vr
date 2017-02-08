67543b1a1a9ee5b84aecd03aaeeb2976923b9c16
Ali Atoui <VANRISE\ali.atoui>
2017-02-08 10:16:57 +0000

    Updating Retail_Analytics schema
    git-tfs-id: [http://developmentvlan:8585/tfs/vanrise.collection]$/;C39084

--- Retail/Code/RetailAnalytics/Retail_Analytics/Tables/BillingStatsDaily.sql
+++ Retail/Code/RetailAnalytics/Retail_Analytics/Tables/BillingStatsDaily.sql
@@ -1,17 +1,19 @@
 ﻿CREATE TABLE [Retail_Analytics].[BillingStatsDaily] (
-    [Id]                   BIGINT           NULL,
-    [BatchStart]           DATETIME         NULL,
-    [FinancialAccountId]   BIGINT           NULL,
-    [ServiceTypeId]        UNIQUEIDENTIFIER NULL,
-    [TrafficDirection]     INT              NULL,
-    [InterconnectOperator] BIGINT           NULL,
-    [ZoneId]               BIGINT           NULL,
-    [PackageId]            INT              NULL,
-    [ChargingPolicyId]     INT              NULL,
-    [Rate]                 DECIMAL (20, 8)  NULL,
-    [CurrencyId]           INT              NULL,
-    [TotalDuration]        DECIMAL (20, 4)  NULL,
-    [TotalAmount]          DECIMAL (20, 4)  NULL,
-    [NumberOfCDRs]         INT              NULL
+    [Id]                     BIGINT           NULL,
+    [BatchStart]             DATETIME         NULL,
+    [FinancialAccountId]     BIGINT           NULL,
+    [ServiceTypeId]          UNIQUEIDENTIFIER NULL,
+    [TrafficDirection]       INT              NULL,
+    [InterconnectOperatorId] BIGINT           NULL,
+    [ZoneId]                 BIGINT           NULL,
+    [PackageId]              INT              NULL,
+    [ChargingPolicyId]       INT              NULL,
+    [Rate]                   DECIMAL (20, 8)  NULL,
+    [CurrencyId]             INT              NULL,
+    [TotalDuration]          DECIMAL (20, 4)  NULL,
+    [TotalAmount]            DECIMAL (20, 4)  NULL,
+    [NumberOfCDRs]           INT              NULL
 );
+
+
 
--- Retail/Code/RetailAnalytics/Retail_Analytics/User Defined Types/BillingStatsDailyType.sql
+++ Retail/Code/RetailAnalytics/Retail_Analytics/User Defined Types/BillingStatsDailyType.sql
@@ -1,16 +1,18 @@
 ﻿CREATE TYPE [Retail_Analytics].[BillingStatsDailyType] AS TABLE (
-    [Id]                   BIGINT           NULL,
-    [BatchStart]           DATETIME         NULL,
-    [FinancialAccountId]   BIGINT           NULL,
-    [ServiceTypeId]        UNIQUEIDENTIFIER NULL,
-    [TrafficDirection]     INT              NULL,
-    [InterconnectOperator] BIGINT           NULL,
-    [ZoneId]               BIGINT           NULL,
-    [PackageId]            INT              NULL,
-    [ChargingPolicyId]     INT              NULL,
-    [Rate]                 DECIMAL (20, 8)  NULL,
-    [CurrencyId]           INT              NULL,
-    [TotalDuration]        DECIMAL (20, 4)  NULL,
-    [TotalAmount]          DECIMAL (20, 4)  NULL,
-    [NumberOfCDRs]         INT              NULL);
+    [Id]                     BIGINT           NULL,
+    [BatchStart]             DATETIME         NULL,
+    [FinancialAccountId]     BIGINT           NULL,
+    [ServiceTypeId]          UNIQUEIDENTIFIER NULL,
+    [TrafficDirection]       INT              NULL,
+    [InterconnectOperatorId] BIGINT           NULL,
+    [ZoneId]                 BIGINT           NULL,
+    [PackageId]              INT              NULL,
+    [ChargingPolicyId]       INT              NULL,
+    [Rate]                   DECIMAL (20, 8)  NULL,
+    [CurrencyId]             INT              NULL,
+    [TotalDuration]          DECIMAL (20, 4)  NULL,
+    [TotalAmount]            DECIMAL (20, 4)  NULL,
+    [NumberOfCDRs]           INT              NULL);
+
+
 
