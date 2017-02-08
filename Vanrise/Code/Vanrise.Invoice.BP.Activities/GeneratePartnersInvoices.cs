e8e7511812edceef136e5a667f82ab10d383aa8d
Samer Haidar <VANRISE\samer.haidar>
2017-02-08 14:31:59 +0000

    Implementing CheckIFShouldGenerateInvoice
    git-tfs-id: [http://developmentvlan:8585/tfs/vanrise.collection]$/;C39109

--- Vanrise/Code/Vanrise.Invoice.BP.Activities/GeneratePartnersInvoices.cs
+++ Vanrise/Code/Vanrise.Invoice.BP.Activities/GeneratePartnersInvoices.cs
@@ -18,6 +18,10 @@ namespace Vanrise.Invoice.BP.Activities
         public InArgument<Guid> InvoiceTypeId { get; set; }
         [RequiredArgument]
         public InArgument<string> PartnerId { get; set; }
+         [RequiredArgument]
+        public InArgument<int> EndDateOffsetFromToday { get; set; }
+         [RequiredArgument]
+        public InArgument<int> IssueDateOffsetFromToday { get; set; }
         #endregion
         protected override void VRExecute(IBaseCodeActivityContext context)
         {
@@ -27,13 +31,11 @@ namespace Vanrise.Invoice.BP.Activities
             InvoiceSettingManager invoiceSettingManager = new Business.InvoiceSettingManager();
             InvoiceTypeManager invoiceTypeManager = new Business.InvoiceTypeManager();
            
-
-            var issueDate = DateTime.Now;
             if (partnerId != null)
             {
+
                 var invoiceType = invoiceTypeManager.GetInvoiceType(invoiceTypeId);
                 var invoiceTypePartnerManager = invoiceType.Settings.ExtendedSettings.GetPartnerManager();
-               
 
                 PartnerManager partnerManager = new PartnerManager();
                 var partnerSetting = partnerManager.GetInvoicePartnerSetting(invoiceTypeId, partnerId);
@@ -41,36 +43,48 @@ namespace Vanrise.Invoice.BP.Activities
                 AutomaticInvoiceSettingPart automaticInvoiceSettingPart = invoiceSettingManager.GetInvoiceSettingDetailByType<AutomaticInvoiceSettingPart>(partnerSetting.InvoiceSetting.InvoiceSettingId);
                 if (automaticInvoiceSettingPart.IsEnabled)
                 {
+
+                    int endDateOffsetFromToday = this.EndDateOffsetFromToday.Get(context.ActivityContext);
+                    int issueDateOffsetFromToday = this.IssueDateOffsetFromToday.Get(context.ActivityContext);
+                    var issueDate = DateTime.Now.AddDays(-issueDateOffsetFromToday);
+
                     var billingPeriod = invoiceManager.GetBillingInterval(invoiceTypeId, partnerId, issueDate);
                     if (billingPeriod != null)
                     {
-                      var generatedInvoice =  invoiceManager.GenerateInvoice(new Entities.GenerateInvoiceInput
-                        {
-                            InvoiceTypeId = invoiceTypeId,
-                            IssueDate = issueDate,
-                            PartnerId = partnerId,
-                            FromDate = billingPeriod.FromDate,
-                            ToDate = billingPeriod.ToDate
-                        });
-                      PartnerNameManagerContext PartnerNameManagerContext = new PartnerNameManagerContext
-                      {
-                          PartnerId = partnerId
-                      };
-                        var partnerName = invoiceTypePartnerManager.GetPartnerName(PartnerNameManagerContext);
-                        if(generatedInvoice.Result == InsertOperationResult.Succeeded)
-                        {
 
-                            context.ActivityContext.WriteTrackingMessage(LogEntryType.Information, string.Format("Invoice generated for {0} from period {1} to  period {2}", partnerName, billingPeriod.FromDate.ToShortDateString(), billingPeriod.ToDate.ToShortDateString()));
-                        }
-                        else
+                        if (CheckIFShouldGenerateInvoice(billingPeriod.ToDate, endDateOffsetFromToday, issueDate))
                         {
-                            context.ActivityContext.WriteTrackingMessage(LogEntryType.Information, string.Format("Invoice not generated for {0} from period {1} to  period {2} :{3}", partnerName, billingPeriod.FromDate.Date.ToShortDateString(), billingPeriod.ToDate.ToShortDateString(), generatedInvoice.Message));
+                            var generatedInvoice = invoiceManager.GenerateInvoice(new Entities.GenerateInvoiceInput
+                                                  {
+                                                      InvoiceTypeId = invoiceTypeId,
+                                                      IssueDate = issueDate,
+                                                      PartnerId = partnerId,
+                                                      FromDate = billingPeriod.FromDate,
+                                                      ToDate = billingPeriod.ToDate
+                                                  });
+                            PartnerNameManagerContext PartnerNameManagerContext = new PartnerNameManagerContext
+                            {
+                                PartnerId = partnerId
+                            };
+                            var partnerName = invoiceTypePartnerManager.GetPartnerName(PartnerNameManagerContext);
+                            if (generatedInvoice.Result == InsertOperationResult.Succeeded)
+                            {
+
+                                context.ActivityContext.WriteTrackingMessage(LogEntryType.Information, string.Format("Invoice generated for {0} from period {1} to  period {2}", partnerName, billingPeriod.FromDate.ToShortDateString(), billingPeriod.ToDate.ToShortDateString()));
+                            }
+                            else
+                            {
+                                context.ActivityContext.WriteTrackingMessage(LogEntryType.Warning, string.Format("Invoice not generated for {0} from period {1} to  period {2} :{3}", partnerName, billingPeriod.FromDate.Date.ToShortDateString(), billingPeriod.ToDate.ToShortDateString(), generatedInvoice.Message));
+                            }
                         }
-                       
                     }
-                    
                 }
             }
+        }
+
+        private bool CheckIFShouldGenerateInvoice(DateTime toDate, int endDateOffsetFromToday, DateTime issueDate)
+        {
+            return (toDate <= issueDate.AddDays(-endDateOffsetFromToday));
         }
     }
 }
