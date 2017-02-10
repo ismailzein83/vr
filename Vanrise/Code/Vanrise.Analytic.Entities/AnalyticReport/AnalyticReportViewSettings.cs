8031ffc8e8042cab7ce45a8f5c8050ddc998538f
Ali Atoui <VANRISE\ali.atoui>
2017-02-10 08:30:34 +0000

    Editing AnalyticReportViewSettings
    git-tfs-id: [http://developmentvlan:8585/tfs/vanrise.collection]$/;C39156

--- Vanrise/Code/Vanrise.Analytic.Entities/AnalyticReport/AnalyticReportViewSettings.cs
+++ Vanrise/Code/Vanrise.Analytic.Entities/AnalyticReport/AnalyticReportViewSettings.cs
@@ -8,8 +8,10 @@ namespace Vanrise.Analytic.Entities
 {
     public class AnalyticReportViewSettings : Vanrise.Security.Entities.ViewSettings
     {
+        public Guid TypeId { get; set; }
         public Guid AnalyticReportId { get; set; }
-        public int TypeId { get; set; }
+
+
         public override string GetURL(Security.Entities.View view)
         {
             return String.Format("#/viewwithparams/Analytic/Views/GenericAnalytic/Runtime/GenericAnalyticReport/{{\"analyticReportId\":\"{0}\"}}", this.AnalyticReportId);
