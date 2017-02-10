b10035fac22820a60097d84ba16666018538ade3
Ali Atoui <VANRISE\ali.atoui>
2017-02-10 15:58:12 +0000

    Adding GetRemoteRecordStorageDataManagerContext
    git-tfs-id: [http://developmentvlan:8585/tfs/vanrise.collection]$/;C39188

---
+++ Vanrise/Code/Vanrise.GenericData.Business/DataStorage/GetRemoteRecordStorageDataManagerContext.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Vanrise.GenericData.Entities;
+
+namespace Vanrise.GenericData.MainExtensions.DataStorages
+{
+    public class GetRemoteRecordStorageDataManagerContext : IGetRemoteRecordStorageDataManagerContext
+    {
+        public Entities.DataStore DataStore { get; set; }
+
+        public Entities.DataRecordStorage DataRecordStorage { get; set; }
+    }
+}
