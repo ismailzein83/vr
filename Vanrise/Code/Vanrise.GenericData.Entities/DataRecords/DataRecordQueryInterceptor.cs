3fa8e139c0cdda8f3f495b543ce78f9bf6e76555
Ali Atoui <VANRISE\ali.atoui>
2017-02-13 12:05:24 +0000

    Adding DataRecordQueryInterceptor
    git-tfs-id: [http://developmentvlan:8585/tfs/vanrise.collection]$/;C39202

---
+++ Vanrise/Code/Vanrise.GenericData.Entities/DataRecords/DataRecordQueryInterceptor.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Vanrise.GenericData.Entities
+{
+    public class DataRecordQueryInterceptor
+    {
+        public abstract Guid ConfigId { get; }
+        public abstract void PrepareQuery(IDataRecordQueryInterceptorContext context);
+    }
+
+    public interface IDataRecordQueryInterceptorContext
+    {
+        DataRecordQuery Query { get; set; }
+    }
+}
