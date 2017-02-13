5c9c62bc06313a2bb7dad1490268bd72f60fda84
Ali Atoui <VANRISE\ali.atoui>
2017-02-13 12:09:09 +0000

    Adding VRRestAPIRecordQueryInterceptor
    git-tfs-id: [http://developmentvlan:8585/tfs/vanrise.collection]$/;C39203

---
+++ Vanrise/Code/Vanrise.GenericData.MainExtensions/DataStorages/DataRecordStorage/VRRestAPIRecordQueryInterceptor.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Vanrise.GenericData.Entities;
+
+namespace Vanrise.GenericData.MainExtensions.DataStorages.DataRecordStorage
+{
+    public class VRRestAPIRecordQueryInterceptor : DataRecordQueryInterceptor
+    {
+        //public override Guid ConfigId
+        //{
+        //    get { return new Guid("B3A94A20-92ED-47BF-86D6-1034B720BE73"); }
+        //}
+
+        //public override void PrepareQuery(IDataRecordQueryInterceptorContext context)
+        //{
+        //    throw new NotImplementedException();
+        //}
+    }
+
+    public class VRRestAPIRecordQueryInterceptorContext : IDataRecordQueryInterceptorContext
+    {
+        //public DataRecordQuery Query { get; set; }
+    }
+}
