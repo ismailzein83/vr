037a488303ec216780e35dfa7fa75340a727ba9b
anthony douaihy <VANRISE\anthony.douaihy>
2017-02-14 13:44:42 +0000

    Add Billing Stats 30Min
    git-tfs-id: [http://developmentvlan:8585/tfs/vanrise.collection]$/;C39264

---
+++ TOneV2/Code/TOneV2/SQL.TOneAnalytics/TOneWhS_Analytics/Tables/BillingStats30Min.sql
@@ -0,0 +1,38 @@
+﻿CREATE TABLE [TOneWhS_Analytics].[BillingStats30Min] (
+    [Id]                       BIGINT          NULL,
+    [BatchStart]               DATETIME        NULL,
+    [CustomerId]               INT             NULL,
+    [SupplierId]               INT             NULL,
+    [CustomerProfileId]        INT             NULL,
+    [SupplierProfileId]        INT             NULL,
+    [SupplierZoneId]           BIGINT          NULL,
+    [SaleZoneId]               BIGINT          NULL,
+    [CostCurrencyId]           INT             NULL,
+    [SaleCurrencyId]           INT             NULL,
+    [NumberOfCalls]            INT             NULL,
+    [FirstCallTime]            DATETIME        NULL,
+    [LastCallTime]             DATETIME        NULL,
+    [MinimumDurationInSeconds] DECIMAL (20, 4) NULL,
+    [MaximumDurationInSeconds] DECIMAL (20, 4) NULL,
+    [CostNet]                  DECIMAL (20, 4) NULL,
+    [SaleNet]                  DECIMAL (20, 4) NULL,
+    [SaleDurationInSeconds]    DECIMAL (20, 4) NULL,
+    [CostDurationInSeconds]    DECIMAL (20, 4) NULL,
+    [SaleRateId]               BIGINT          NULL,
+    [CostRateId]               BIGINT          NULL,
+    [DurationInSeconds]        DECIMAL (20, 4) NULL,
+    [CountryId]                INT             NULL,
+    [CostExtraCharges]         DECIMAL (20, 4) NULL,
+    [SaleExtraCharges]         DECIMAL (20, 4) NULL,
+    [SaleRateTypeId]           INT             NULL,
+    [CostRateTypeId]           INT             NULL,
+    [SaleRateValue]            DECIMAL (20, 8) NULL,
+    [CostRateValue]            DECIMAL (20, 8) NULL,
+    CONSTRAINT [IX_BillingStats30Min_Id] UNIQUE NONCLUSTERED ([Id] ASC)
+);
+
+
+GO
+CREATE CLUSTERED INDEX [IX_BillingStats30Min_BatchStart]
+    ON [TOneWhS_Analytics].[BillingStats30Min]([BatchStart] ASC);
+
