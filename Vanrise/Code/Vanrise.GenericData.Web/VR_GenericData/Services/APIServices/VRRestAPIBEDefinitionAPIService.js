3bd1b55ba7867158c0eae48205e74c5fa34bfb86
Ali Atoui <VANRISE\ali.atoui>
2017-02-14 15:23:38 +0000

    Editing VRRestAPIBEDefinitionAPIService
    git-tfs-id: [http://developmentvlan:8585/tfs/vanrise.collection]$/;C39280

---
+++ Vanrise/Code/Vanrise.GenericData.Web/VR_GenericData/Services/APIServices/VRRestAPIBEDefinitionAPIService.js
@@ -0,0 +1,23 @@
+﻿(function (appControllers) {
+
+    'use strict';
+
+    BusinessEntityDefinitionAPIService.$inject = ['BaseAPIService', 'UtilsService', 'SecurityService', 'VR_GenericData_ModuleConfig'];
+
+    function BusinessEntityDefinitionAPIService(BaseAPIService, UtilsService, SecurityService, VR_GenericData_ModuleConfig) {
+
+        function GetBusinessEntityDefinitionsInfo(filter) {
+            return BaseAPIService.get(UtilsService.getServiceURL(VR_GenericData_ModuleConfig.moduleName, 'BusinessEntityDefinition', 'GetBusinessEntityDefinitionsInfo'), {
+                filter: filter
+            });
+        }
+
+
+        return {
+            GetBusinessEntityDefinitionsInfo: GetBusinessEntityDefinitionsInfo
+        };
+    }
+
+    appControllers.service('VR_GenericData_BusinessEntityDefinitionAPIService', BusinessEntityDefinitionAPIService);
+
+})(appControllers);
