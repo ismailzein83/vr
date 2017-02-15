85f0dfad5094693b62454a5db80d60c755a2499a
Ali Atoui <VANRISE\ali.atoui>
2017-02-15 15:56:01 +0000

    add BusinessEntityInfo entity
    git-tfs-id: [http://developmentvlan:8585/tfs/vanrise.collection]$/;C39340

---
+++ Vanrise/Code/Vanrise.GenericData.Entities/BusinessEntities/BusinessEntityInfo.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Vanrise.GenericData.Entities
+{
+    public class BusinessEntityInfo
+    {
+        public object BusinessEntityId { get; set; }
+        public string Description { get; set; }
+    }
+}
