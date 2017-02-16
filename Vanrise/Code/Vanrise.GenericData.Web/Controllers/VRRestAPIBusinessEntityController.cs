e5ea8835448e858260119891876cd0cd35845007
Ali Atoui <VANRISE\ali.atoui>
2017-02-16 09:42:27 +0000

    Adding VRRestAPIBusinessEntityController
    git-tfs-id: [http://developmentvlan:8585/tfs/vanrise.collection]$/;C39361

---
+++ Vanrise/Code/Vanrise.GenericData.Web/Controllers/VRRestAPIBusinessEntityController.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Vanrise.GenericData.Web.Controllers
+{
+    public class VRRestAPIBusinessEntityController
+    {
+        //[HttpGet]
+        //[Route("GetBusinessEntityDefinitionsInfo")]
+        //public IEnumerable<BusinessEntityDefinitionInfo> GetBusinessEntityDefinitionsInfo(string filter = null)
+        //{
+        //    BusinessEntityDefinitionInfoFilter deserializedFilter = (filter != null) ? Vanrise.Common.Serializer.Deserialize<BusinessEntityDefinitionInfoFilter>(filter) : null;
+        //    return _manager.GetBusinessEntityDefinitionsInfo(deserializedFilter);
+        //}
+    }
+}
