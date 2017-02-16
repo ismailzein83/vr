57db08031be0f8ee2a70d599af9bce94044dd1ed
Wissam Ajamy <VANRISE\wissam.ajamy>
2017-02-16 14:15:16 +0000

    Add 'VR_AccountBalance_AccountType' [common].[ExtensionConfiguration]
    git-tfs-id: [http://developmentvlan:8585/tfs/vanrise.collection]$/;C39379

--- TOneV2/Code/TOneV2/SQL.TOneConfiguration/VR_AccountBalance.PostDeployment.sql
+++ TOneV2/Code/TOneV2/SQL.TOneConfiguration/VR_AccountBalance.PostDeployment.sql
@@ -13,6 +13,7 @@ Post-Deployment Script Template
 BEGIN
 set nocount on;;with cte_data([ID],[Name],[Title],[ConfigType],[Settings])as (select * from (values
 --//////////////////////////////////////////////////////////////////////////////////////////////////
+('7824DFFA-0EBF-4939-93E8-DEC6E5EDFA10','VR_AccountBalance_AccountType','Account Balance Type','VR_Common_VRComponentType'													,'{"Editor":"vr-accountbalance-accounttype-settings"}'),
 ('6336D88E-3460-4388-B56C-322FBC336129','Billing Transaction Synchronizer','Billing Transaction Synchronizer','VR_BEBridge_BESynchronizer'									,'{"Editor":"vr-accountbalance-billingtransaction-synchronizer"}'),
 ('BA79CB79-D058-4382-88FC-DB1C154B5374','VR_Notification_VRAlertRuleTypeSettings_AccountBalanceRule','Account Balance Rule Type','VR_Notification_VRAlertRuleTypeSettings'	,'{"Editor":"vr-accountbalance-accountbalancerule-settings"}')
 --\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\
