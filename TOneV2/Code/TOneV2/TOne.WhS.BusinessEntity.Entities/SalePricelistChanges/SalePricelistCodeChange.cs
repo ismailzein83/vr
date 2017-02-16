31d31133e2a465da110cef690a0da3c4ea189506
Mortada Issa <VANRISE\mortada.issa>
2017-02-16 09:12:26 +0000

    Added Pricelist changes entities
    git-tfs-id: [http://developmentvlan:8585/tfs/vanrise.collection]$/;C39356

---
+++ TOneV2/Code/TOneV2/TOne.WhS.BusinessEntity.Entities/SalePricelistChanges/SalePricelistCodeChange.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TOne.WhS.BusinessEntity.Entities
+{
+    public class SalePricelistCodeChange
+    {
+        public string ZoneName { get; set; }
+
+        public string RecentZoneName { get; set; }
+
+        public int CountryId { get; set; }
+
+        public string Code { get; set; }
+
+        //TODO: put it in Sale Code on Business entity and remove it from Numbering Plan
+        //public CodeChaneType ChangeType { get; set; }
+
+        public int PricelistId { get; set; }
+    }
+}
---
+++ TOneV2/Code/TOneV2/TOne.WhS.BusinessEntity.Entities/SalePricelistChanges/SalePricelistRateChange.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TOne.WhS.BusinessEntity.Entities
+{
+    public class SalePricelistRateChange
+    {
+        public string ZoneName { get; set; }
+
+        public int CountryId { get; set; }
+
+        public Decimal Rate { get; set; }
+
+        public Decimal? RecentRate { get; set; }
+
+        public RateChangeType ChangeType { get; set; }
+
+        public int PricelistId { get; set; }
+    }
+}
