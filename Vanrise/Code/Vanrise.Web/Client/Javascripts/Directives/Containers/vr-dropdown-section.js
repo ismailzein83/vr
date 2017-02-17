81c8cad35f81af7a9707392fe3d71c63e03b2cc3
Ali Ballouk <VANRISE\ali.ballouk>
2017-02-17 15:28:34 +0000

    refactor vr-dropdownsection api functions
    git-tfs-id: [http://developmentvlan:8585/tfs/vanrise.collection]$/;C39391

--- Vanrise/Code/Vanrise.Web/Client/Javascripts/Directives/Containers/vr-dropdown-section.js
+++ Vanrise/Code/Vanrise.Web/Client/Javascripts/Directives/Containers/vr-dropdown-section.js
@@ -10,21 +10,84 @@
             restrict: 'E',
             transclude: true,
             scope: {
-                onReady: '='
+                onReady: '=',
+                defaultstate:'='
             },
             controller: function ($scope, $element, $attrs) {
                 var ctrl = this;
-                var api = {};
-                api.collapse = function () {
-                    if (ctrl.showmenu == true)
-                        ctrl.toggelSectionMenu(false);
+                ctrl.id = BaseDirService.generateHTMLElementName();
+                ctrl.menuid = BaseDirService.generateHTMLElementName();
+                ctrl.initializeSection =  function() {
+                    ctrl.showmenu = true;
+                    setTimeout(function () {
+                        calculatePosition(true);
+                    }, 300);
                 };
-                api.expand = function () {
-                    if (ctrl.showmenu == false)
-                        ctrl.toggelSectionMenu(true);
+               
+                ctrl.expandSection = function (e) {
+                    if (e != undefined && $(e.target).hasClass('vanrise-inpute')) {
+                        return;
+                    }
+                    calculatePosition();
+                    setTimeout(function () {                        
+                        $scope.$apply(function () {                          
+                            ctrl.showmenu = true;                           
+                        });
+                        $('#' + ctrl.menuid).slideDown("slow");
+                    });
                 };
-                if (ctrl.onReady != null)
-                    ctrl.onReady(api);
+                ctrl.collapseSection = function () {
+                    $('#' + ctrl.menuid).slideUp("slow", function () {
+                        ctrl.showmenu = false;
+                    });
+                };              
+
+                $('#' + ctrl.id).parents('div').scroll(function () {
+                    fixDropdownPosition();
+                });
+                $(window).scroll(function () {
+                    fixDropdownPosition();
+                });
+                $(window).resize(function () {
+                    fixDropdownPosition();
+                });
+
+                var fixDropdownPosition = function () {
+                    ctrl.collapseSection(undefined);
+                };
+                $scope.$on('start-drag', function (event, args) {
+                    fixDropdownPosition();
+                });
+
+                function calculatePosition(forcedisplay) {
+                  //  setTimeout(function () {
+                        var self = $('#' + ctrl.id).find('.summary-container');
+
+                        var selfHeight = $(self).parent().height();
+                        var selfOffset = $(self).offset();
+                        var dropDown = $('#' + ctrl.menuid);
+                        var top = 0;
+                        var basetop = selfOffset.top - $(window).scrollTop();
+                        var baseleft = selfOffset.left - $(window).scrollLeft();
+
+                        var height = ctrl.bodysectionheight;
+                         console.log($(dropDown).height())
+                        if (innerHeight - basetop < height + 100)
+                            top = basetop - (height);
+                        else
+                            top = selfOffset.top - $(window).scrollTop() - 2;
+                        var style = {
+                            position: 'fixed',
+                            top: top,
+                            left: baseleft,
+                            width: self.parent().width()
+                        };
+                        if (forcedisplay == true)
+                            style.display = 'block';
+                        $(dropDown).css(style);
+                  //  }, 100);
+                };
+               
             },
             compile: function (element, attrs) {
                 return {
@@ -32,76 +95,24 @@
                         var ctrl = $scope.ctrl;
 
 
-                        ctrl.id = BaseDirService.generateHTMLElementName();
-                        ctrl.menuid = BaseDirService.generateHTMLElementName();
-
-                        ctrl.getSummaryText = function () {
-                            return "Summary";
-                        };
-                        ctrl.showmenu = true;
                         
-                        ctrl.toggelSectionMenu = function (e, bool) {    
-                            if (e != undefined && $(e.target).hasClass('vanrise-inpute')) {
-                                return;
-                            }
-                          
-                            if (ctrl.showmenu == false) {
-                                var self = $('#' + ctrl.id).find('.summary-container');
-                                var selfHeight = $(self).parent().height();
-                                var selfOffset = $(self).offset();
-                                var dropDown = $('#' + ctrl.menuid);
-                                var top = 0;
-                                var basetop = selfOffset.top - $(window).scrollTop();
-                                var baseleft = selfOffset.left - $(window).scrollLeft();
-                                ctrl.showmenu = true;
-                                var height = ctrl.bodysectionheight;
-                                if (innerHeight - basetop < height + 100)
-                                    top = basetop - (height);
-                                else
-                                    top = selfOffset.top - $(window).scrollTop() - 2;
-
-                                $(dropDown).css({ position: 'fixed', top: top, left: baseleft, width: self.parent().width() });
-                                setTimeout(function () {
-                                    $scope.$apply(function () {
-                                        ctrl.showmenu = true;
-                                    });
-                                    $('#' + ctrl.menuid).slideDown("slow");
-                                })
-                            }
-                            else {
-                                $('#' + ctrl.menuid).slideUp("slow", function () {
-                                    ctrl.showmenu = false;
-                                });
-                            }
-                        };
-
-                        $('#' + ctrl.id).parents('div').scroll(function () {
-                            fixDropdownPosition();
-                        });
-                        $(window).scroll(function () {
-                            fixDropdownPosition();
-                        });
-                        $(window).resize(function () {
-                            fixDropdownPosition();
-                        });
-
-                        var fixDropdownPosition = function () {
-                            $('.vr-section-dropdown').find('.dropdown-menu').hide();
-
-                        };
-                        $scope.$on('start-drag', function (event, args) {
-                            fixDropdownPosition();
-                        });
-
 
                     },
                     post: function (scope, elem, attr, ctrl, transcludeFn) {
 
                         MultiTranscludeService.transclude(elem, transcludeFn);
-                        setTimeout(function () {
-                            ctrl.toggelSectionMenu(undefined, false);
-                        }, 1000);
-
+                        if (ctrl.defaultstate == true)
+                            ctrl.initializeSection();
+                        //ctrl.collapseSection();
+                        var api = {};
+                        api.collapse = function () {
+                            ctrl.collapseSection();
+                        };
+                        api.expand = function () {
+                            ctrl.expandSection(undefined);
+                        };
+                        if (ctrl.onReady != null)
+                            ctrl.onReady(api);
                     },
                 }
             },
@@ -115,14 +126,14 @@
                 var sectionTemplate = '<div style="position: relative;" >'
                             + '<vr-validation-group validationcontext="ctrl.validationContext">'
                             + '<div class="vr-dropdown-section" id="{{ctrl.id}}" >'
-                                + '<div class="summary-container hand-cursor" ng-click="ctrl.toggelSectionMenu($event)" ng-class="ctrl.validationContext.validate() != null  ? \'invalid-section\':\'\' " >'
+                                + '<div class="summary-container hand-cursor" ng-click="ctrl.expandSection($event)" ng-class="ctrl.validationContext.validate() != null  ? \'invalid-section\':\'\' " >'
                                     + '<div transclude-id="summary" class="summary-main"></div>'
                                     + '<span  class="glyphicon  glyphicon-sort-by-attributes  toogle-icon"></span>'
                                     + '<span ng-if="ctrl.validationContext.validate() != null" class="validation-sign"  title="has validation errors!">*</span>'
                               + '</div>'
                                 + '<ul class="dropdown-menu drop-down-section"   id="{{ctrl.menuid}}" ng-show="ctrl.showmenu" ng-class="ctrl.validationContext.validate() != null  ? \'invalid-section\':\'\' "  >'
                                     + '<li role="presentation" >'
-                                        + '<span class="hand-cursor glyphicon  glyphicon-remove remove-icon" ng-click="ctrl.toggelSectionMenu($event,false)" ></span>'
+                                        + '<span class="hand-cursor glyphicon  glyphicon-remove remove-icon" ng-click="ctrl.collapseSection($event,false)" ></span>'
                                         + '<div class="section-body" transclude-id="body" ng-click="ctrl.muteAction($event);"></div>'
                                     + '</li>'
                                 + '</ul>'
