df391e3d6166c77437dc0104bdceb3edfbd2c1ef
Wissam Ajamy <VANRISE\wissam.ajamy>
2017-02-20 15:44:30 +0000

    Update structure from TOneV2_Dev_Backup
    git-tfs-id: [http://developmentvlan:8585/tfs/vanrise.collection]$/;C39424

--- TOneV2/Code/TOneV2/SQL.TOneWhS_BE_StateBackup/TOneWhS_BE_Bkup/Tables/SalePriceList.sql
+++ TOneV2/Code/TOneV2/SQL.TOneWhS_BE_StateBackup/TOneWhS_BE_Bkup/Tables/SalePriceList.sql
@@ -1,12 +1,18 @@
 ﻿CREATE TABLE [TOneWhS_BE_Bkup].[SalePriceList] (
-    [ID]            INT          NOT NULL,
-    [OwnerType]     INT          NOT NULL,
-    [OwnerID]       INT          NOT NULL,
-    [CurrencyID]    INT          NOT NULL,
-    [EffectiveOn]   DATETIME     NULL,
-    [SourceID]      VARCHAR (50) NULL,
-    [StateBackupID] BIGINT       NOT NULL
+    [ID]                INT          NOT NULL,
+    [OwnerType]         INT          NOT NULL,
+    [OwnerID]           INT          NOT NULL,
+    [CurrencyID]        INT          NOT NULL,
+    [EffectiveOn]       DATETIME     NULL,
+    [PriceListType]     TINYINT      NULL,
+    [SourceID]          VARCHAR (50) NULL,
+    [ProcessInstanceID] BIGINT       NULL,
+    [FileID]            BIGINT       NULL,
+    [CreatedTime]       DATETIME     CONSTRAINT [DF_SalePriceList_CreatedTime_1] DEFAULT (getdate()) NULL,
+    [StateBackupID]     BIGINT       NOT NULL
 );
+
+
 
 
 
