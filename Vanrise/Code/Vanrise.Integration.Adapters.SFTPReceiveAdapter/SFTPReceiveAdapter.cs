1d071526f385666dcd0795937504de5f9ae3901f
rabih.fashwal:2 <rabih.fashwal:2@vanrise.tfs.local>
2017-02-20 19:24:21 +0000

    Update SFTP Adapter
    git-tfs-id: [http://developmentvlan:8585/tfs/vanrise.collection]$/;C39436

--- Vanrise/Code/Vanrise.Integration.Adapters.SFTPReceiveAdapter/SFTPReceiveAdapter.cs
+++ Vanrise/Code/Vanrise.Integration.Adapters.SFTPReceiveAdapter/SFTPReceiveAdapter.cs
@@ -1,6 +1,9 @@
-﻿using Rebex.Net;
-using System;
+﻿using System;
 using System.IO;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using Rebex.Net;
 using Vanrise.Integration.Adapters.FTPReceiveAdapter.Arguments;
 using Vanrise.Integration.Entities;
 
@@ -8,8 +11,88 @@ namespace Vanrise.Integration.Adapters.SFTPReceiveAdapter
 {
     public class SFTPReceiveAdapter : BaseReceiveAdapter
     {
-        #region Private Functions
+        public override void ImportData(IAdapterImportDataContext context)
+        {
+            FTPAdapterArgument ftpAdapterArgument = context.AdapterArgument as FTPAdapterArgument;
 
+            FTPAdapterState ftpAdapterState = SaveOrGetAdapterState(context, ftpAdapterArgument);
+
+            var sftp = new Rebex.Net.Sftp();
+            string mask = string.IsNullOrEmpty(ftpAdapterArgument.Mask) ? "" : string.Format(ftpAdapterArgument.Mask);
+            Regex regEx = new Regex(mask, RegexOptions.IgnoreCase);
+
+            base.LogVerbose("Establishing SFTP Connection");
+
+            EstablishConnection(sftp, ftpAdapterArgument);
+            if (sftp.GetConnectionState().Connected)
+            {
+                base.LogVerbose("SFTP connection is established");
+
+                if (!sftp.DirectoryExists(ftpAdapterArgument.Directory))
+                {
+                    base.LogError("Could not find Directory {0}", ftpAdapterArgument.Directory);
+                    throw new DirectoryNotFoundException();
+                }
+
+                sftp.ChangeDirectory(ftpAdapterArgument.Directory);
+                SftpItemCollection currentItems = sftp.GetList(string.Format("{0}/*{1}", ftpAdapterArgument.Directory, ftpAdapterArgument.Extension));
+
+                base.LogInformation("{0} files are ready to be imported", currentItems.Count);
+
+                if (currentItems.Count > 0)
+                {
+                    DateTime? localLastRetrievedFileTime = null;
+                    foreach (var fileObj in currentItems.OrderBy(c => c.Modified))
+                    {
+                        if (!fileObj.IsDirectory && regEx.IsMatch(fileObj.Name))
+                        {
+                            if (ftpAdapterArgument.BasedOnLastModifiedTime)
+                            {
+                                if ((!localLastRetrievedFileTime.HasValue || DateTime.Compare(localLastRetrievedFileTime.Value, fileObj.Modified) != 0)
+                                    && DateTime.Compare(ftpAdapterState.LastRetrievedFileTime, fileObj.Modified) >= 0)
+                                    continue;
+                            }
+                            String filePath = ftpAdapterArgument.Directory + "/" + fileObj.Name;
+                            CreateStreamReader(context.OnDataReceived, sftp, fileObj, filePath);
+                            AfterImport(sftp, fileObj, filePath, ftpAdapterArgument);
+                            if (ftpAdapterState.LastRetrievedFileTime != fileObj.Modified)
+                            {
+                                ftpAdapterState = SaveOrGetAdapterState(context, ftpAdapterArgument, fileObj.Modified);
+                                localLastRetrievedFileTime = fileObj.Modified;
+                            }
+                        }
+                    }
+                }
+                CloseConnection(sftp);
+            }
+            else
+            {
+                base.LogError("Could not sftp connect to server {0}", ftpAdapterArgument.ServerIP);
+                throw new Exception("SFTP adapter could not connect to SFTP Server");
+            }
+        }
+
+
+        #region Private Functions
+        private FTPAdapterState SaveOrGetAdapterState(IAdapterImportDataContext context, FTPAdapterArgument ftpAdapterArgument, DateTime? fileModifiedDate = null)
+        {
+            FTPAdapterState adapterState = null;
+            context.GetStateWithLock((state) =>
+            {
+                adapterState = state as FTPAdapterState;
+
+                if (adapterState == null)
+                    adapterState = new FTPAdapterState();
+
+                if (fileModifiedDate != null && fileModifiedDate.HasValue)
+                {
+                    adapterState.LastRetrievedFileTime = fileModifiedDate.Value;
+                }
+                return adapterState;
+            });
+
+            return adapterState;
+        }
         private void CreateStreamReader(Action<IImportedData> receiveData, Sftp sftp, SftpItem fileObj, String filePath)
         {
             base.LogVerbose("Creating stream reader for file with name {0}", fileObj.Name);
@@ -69,55 +152,6 @@ namespace Vanrise.Integration.Adapters.SFTPReceiveAdapter
 
 
         #endregion
-
-        public override void ImportData(IAdapterImportDataContext context)
-        {
-            FTPAdapterArgument ftpAdapterArgument = context.AdapterArgument as FTPAdapterArgument;
-
-            var sftp = new Rebex.Net.Sftp();
-
-            base.LogVerbose("Establishing SFTP Connection");
-
-            EstablishConnection(sftp, ftpAdapterArgument);
-            if (sftp.GetConnectionState().Connected)
-            {
-                base.LogVerbose("FTP connection is established");
-
-                sftp.ChangeDirectory(ftpAdapterArgument.Directory);
-                SftpItemCollection currentItems = sftp.GetList();
-
-
-                if (!sftp.DirectoryExists(ftpAdapterArgument.Directory))
-                {
-                    base.LogError("Could not find Directory {0}", ftpAdapterArgument.Directory);
-                    throw new DirectoryNotFoundException();
-                }
-
-
-
-                base.LogInformation("{0} files are ready to be imported", currentItems.Count);
-
-                if (currentItems.Count > 0)
-                {
-                    foreach (var fileObj in currentItems)
-                    {
-                        if (!fileObj.IsDirectory && fileObj.Name.ToUpper().Contains(ftpAdapterArgument.Extension))
-                        {
-                            String filePath = ftpAdapterArgument.Directory + "/" + fileObj.Name;
-                            CreateStreamReader(context.OnDataReceived, sftp, fileObj, filePath);
-                            AfterImport(sftp, fileObj, filePath, ftpAdapterArgument);
-                        }
-                    }
-                }
-                CloseConnection(sftp);
-            }
-            else
-            {
-                base.LogError("Could not ftp connect to server {0}", ftpAdapterArgument.ServerIP);
-                throw new Exception("FTP adapter could not connect to FTP Server");
-            }
-        }
-
 
     }
 }
