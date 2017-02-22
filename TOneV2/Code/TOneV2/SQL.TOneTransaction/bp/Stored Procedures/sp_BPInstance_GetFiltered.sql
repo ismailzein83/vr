6601e4a2d11c43b315e0e24971ed9c048e217ab9
Ali Ballouk <VANRISE\ali.ballouk>
2017-02-22 10:33:17 +0000

    Refactor BPInstance DataManager sql strored procedures
    git-tfs-id: [http://developmentvlan:8585/tfs/vanrise.collection]$/;C39483

---
+++ TOneV2/Code/TOneV2/SQL.TOneTransaction/bp/Stored Procedures/sp_BPInstance_GetFiltered.sql
@@ -0,0 +1,25 @@
+﻿Create PROCEDURE [bp].[sp_BPInstance_GetFiltered]
+	@ArrDefinitionID nvarchar(max),
+	@ArrStatus nvarchar(max),
+	@EntityID nvarchar(50),
+	@DateFrom dateTime,
+	@DateTo dateTime
+AS
+BEGIN
+	-- SET NOCOUNT ON added to prevent extra result sets from
+	-- interfering with SELECT statements.
+SET NOCOUNT ON;
+
+BEGIN	
+	SELECT	[ID],[Title],[ParentID],[DefinitionID],[WorkflowInstanceID],[InputArgument], [CompletionNotifier],[ExecutionStatus],
+			[LastMessage],[CreatedTime],[StatusUpdatedTime],[InitiatorUserId],EntityID
+
+	FROM	bp.[BPInstance] as bps WITH(NOLOCK)
+	WHERE	(@EntityID is null OR EntityID = @EntityID) 
+			and (@ArrStatus is NULL or bps.ExecutionStatus in (SELECT ParsedString FROM ParseStringList(@ArrStatus) ) ) 
+			and (@ArrDefinitionID is NULL or  bps.DefinitionID in (SELECT ParsedString FROM ParseStringList(@ArrDefinitionID) ) ) 
+			and bps.CreatedTime >=  @DateFrom 
+			and (@DateTo is NULL or bps.CreatedTime < @DateTo)
+	ORDER BY CreatedTime DESC
+	END
+END
