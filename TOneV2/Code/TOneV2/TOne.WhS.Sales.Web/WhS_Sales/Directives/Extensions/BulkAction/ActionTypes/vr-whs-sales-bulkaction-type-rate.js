496b22f48d6b8c545f6d2d46af08108fea737e14
mohamad.elhariri <mohamad.elhariri@vanrise.tfs.local>
2017-02-22 15:54:36 +0000

    Refactored the getSummary API method
    git-tfs-id: [http://developmentvlan:8585/tfs/vanrise.collection]$/;C39504

--- TOneV2/Code/TOneV2/TOne.WhS.Sales.Web/WhS_Sales/Directives/Extensions/BulkAction/ActionTypes/vr-whs-sales-bulkaction-type-rate.js
+++ TOneV2/Code/TOneV2/TOne.WhS.Sales.Web/WhS_Sales/Directives/Extensions/BulkAction/ActionTypes/vr-whs-sales-bulkaction-type-rate.js
@@ -1,170 +1,177 @@
 ﻿'use strict';
 
 app.directive('vrWhsSalesBulkactionTypeRate', ['WhS_Sales_RatePlanAPIService', 'WhS_Sales_BulkActionUtilsService', 'UtilsService', 'VRUIUtilsService', function (WhS_Sales_RatePlanAPIService, WhS_Sales_BulkActionUtilsService, UtilsService, VRUIUtilsService) {
-	return {
-		restrict: "E",
-		scope: {
-			onReady: "=",
-			normalColNum: '@',
-			isrequired: '='
-		},
-		controller: function ($scope, $element, $attrs) {
-			var rateBulkActionTypeCtrl = this;
-			var rateBulkActionType = new RateBulkActionType($scope, rateBulkActionTypeCtrl, $attrs);
-			rateBulkActionType.initializeController();
-		},
-		controllerAs: "rateBulkActionTypeCtrl",
-		bindToController: true,
-		templateUrl: '/Client/Modules/WhS_Sales/Directives/Extensions/BulkAction/ActionTypes/Templates/RateBulkActionTypeTemplate.html'
-	};
+    return {
+        restrict: "E",
+        scope: {
+            onReady: "=",
+            normalColNum: '@',
+            isrequired: '='
+        },
+        controller: function ($scope, $element, $attrs) {
+            var rateBulkActionTypeCtrl = this;
+            var rateBulkActionType = new RateBulkActionType($scope, rateBulkActionTypeCtrl, $attrs);
+            rateBulkActionType.initializeController();
+        },
+        controllerAs: "rateBulkActionTypeCtrl",
+        bindToController: true,
+        templateUrl: '/Client/Modules/WhS_Sales/Directives/Extensions/BulkAction/ActionTypes/Templates/RateBulkActionTypeTemplate.html'
+    };
 
-	function RateBulkActionType($scope, rateBulkActionTypeCtrl, $attrs) {
+    function RateBulkActionType($scope, rateBulkActionTypeCtrl, $attrs) {
 
-		this.initializeController = initializeController;
+        this.initializeController = initializeController;
 
-		var bulkActionContext;
+        var bulkActionContext;
 
-		var costCalculationMethodSelectorAPI;
-		var costCalculationMethodSelectorReadyDeferred = UtilsService.createPromiseDeferred();
+        var costCalculationMethodSelectorAPI;
+        var costCalculationMethodSelectorReadyDeferred = UtilsService.createPromiseDeferred();
 
-		var rateCalculationMethodSelectorAPI;
-		var rateCalculationMethodSelectorReadyDeferred = UtilsService.createPromiseDeferred();
+        var rateCalculationMethodSelectorAPI;
+        var rateCalculationMethodSelectorReadyDeferred = UtilsService.createPromiseDeferred();
 
-		var directiveAPI;
-		var directiveReadyDeferred;
+        var directiveAPI;
+        var directiveReadyDeferred;
 
-		function initializeController() {
+        function initializeController() {
 
-			$scope.scopeModel = {};
-			$scope.scopeModel.costCalculationMethods = [];
-			$scope.scopeModel.rateCalculationMethods = [];
+            $scope.scopeModel = {};
+            $scope.scopeModel.costCalculationMethods = [];
+            $scope.scopeModel.rateCalculationMethods = [];
 
-			$scope.scopeModel.onCostCalculationMethodSelectorReady = function (api) {
-				costCalculationMethodSelectorAPI = api;
-				costCalculationMethodSelectorReadyDeferred.resolve();
-			};
+            $scope.scopeModel.onCostCalculationMethodSelectorReady = function (api) {
+                costCalculationMethodSelectorAPI = api;
+                costCalculationMethodSelectorReadyDeferred.resolve();
+            };
 
-			$scope.scopeModel.isCostCalculationMethodRequired = function () {
-				return (directiveAPI != undefined && directiveAPI.isCostColumnRequired());
-			};
+            $scope.scopeModel.isCostCalculationMethodRequired = function () {
+                return (directiveAPI != undefined && directiveAPI.isCostColumnRequired());
+            };
 
-			$scope.scopeModel.onRateCalculationMethodSelectorReady = function (api) {
-				rateCalculationMethodSelectorAPI = api;
-				rateCalculationMethodSelectorReadyDeferred.resolve();
-			};
+            $scope.scopeModel.onRateCalculationMethodSelectorReady = function (api) {
+                rateCalculationMethodSelectorAPI = api;
+                rateCalculationMethodSelectorReadyDeferred.resolve();
+            };
 
-			$scope.scopeModel.onDirectiveReady = function (api) {
-				directiveAPI = api;
-				var directivePayload = {
-					bulkActionContext: bulkActionContext
-				};
-				var setLoader = function (value) {
-					$scope.scopeModel.isLoadingDirective = value;
-				};
-				VRUIUtilsService.callDirectiveLoadOrResolvePromise($scope, directiveAPI, directivePayload, setLoader, directiveReadyDeferred);
-			};
+            $scope.scopeModel.onDirectiveReady = function (api) {
+                directiveAPI = api;
+                var directivePayload = {
+                    bulkActionContext: bulkActionContext
+                };
+                var setLoader = function (value) {
+                    $scope.scopeModel.isLoadingDirective = value;
+                };
+                VRUIUtilsService.callDirectiveLoadOrResolvePromise($scope, directiveAPI, directivePayload, setLoader, directiveReadyDeferred);
+            };
 
-			$scope.scopeModel.onBulkActionChanged = function () {
-				WhS_Sales_BulkActionUtilsService.onBulkActionChanged(bulkActionContext);
-			};
+            $scope.scopeModel.onBulkActionChanged = function () {
+                WhS_Sales_BulkActionUtilsService.onBulkActionChanged(bulkActionContext);
+            };
 
-			UtilsService.waitMultiplePromises([costCalculationMethodSelectorReadyDeferred.promise, rateCalculationMethodSelectorReadyDeferred.promise]).then(function () {
-				defineAPI();
-			});
-		}
+            UtilsService.waitMultiplePromises([costCalculationMethodSelectorReadyDeferred.promise, rateCalculationMethodSelectorReadyDeferred.promise]).then(function () {
+                defineAPI();
+            });
+        }
 
-		function defineAPI() {
+        function defineAPI() {
 
-			var api = {};
+            var api = {};
 
-			api.load = function (payload) {
+            api.load = function (payload) {
 
-				$scope.scopeModel.isLoading = true;
+                $scope.scopeModel.isLoading = true;
 
-				costCalculationMethodSelectorAPI.clearDataSource();
-				rateCalculationMethodSelectorAPI.clearDataSource();
+                costCalculationMethodSelectorAPI.clearDataSource();
+                rateCalculationMethodSelectorAPI.clearDataSource();
 
-				var costCalculationMethod;
-				var rateCalculationMethod;
+                var costCalculationMethod;
+                var rateCalculationMethod;
 
-				if (payload != undefined) {
+                if (payload != undefined) {
 
-					bulkActionContext = payload.bulkActionContext;
+                    bulkActionContext = payload.bulkActionContext;
 
-					if (payload.bulkAction != undefined) {
-						costCalculationMethod = payload.bulkAction.CostCalculationMethod;
-						rateCalculationMethod = payload.bulkAction.RateCalculationMethod;
-						$scope.scopeModel.beginEffectiveDate = payload.bulkAction.BED;
-					}
+                    if (payload.bulkAction != undefined) {
+                        costCalculationMethod = payload.bulkAction.CostCalculationMethod;
+                        rateCalculationMethod = payload.bulkAction.RateCalculationMethod;
+                        $scope.scopeModel.beginEffectiveDate = payload.bulkAction.BED;
+                    }
 
-					if (bulkActionContext.costCalculationMethods != undefined) {
-						for (var i = 0; i < bulkActionContext.costCalculationMethods.length; i++)
-							$scope.scopeModel.costCalculationMethods.push(bulkActionContext.costCalculationMethods[i]);
-					}
+                    if (bulkActionContext.costCalculationMethods != undefined) {
+                        for (var i = 0; i < bulkActionContext.costCalculationMethods.length; i++)
+                            $scope.scopeModel.costCalculationMethods.push(bulkActionContext.costCalculationMethods[i]);
+                    }
 
-					if (costCalculationMethod != undefined)
-						$scope.selectedCostCalculationMethod = UtilsService.getItemByVal($scope.scopeModel.costCalculationMethods, costCalculationMethod.ConfigId, 'ConfigId');
-				}
+                    if (costCalculationMethod != undefined)
+                        $scope.selectedCostCalculationMethod = UtilsService.getItemByVal($scope.scopeModel.costCalculationMethods, costCalculationMethod.ConfigId, 'ConfigId');
+                }
 
-				var promises = [];
+                var promises = [];
 
-				var loadRateCalculationMethodExtensionConfigsPromise = loadRateCalculationMethodExtensionConfigs();
-				promises.push(loadRateCalculationMethodExtensionConfigsPromise);
+                var loadRateCalculationMethodExtensionConfigsPromise = loadRateCalculationMethodExtensionConfigs();
+                promises.push(loadRateCalculationMethodExtensionConfigsPromise);
 
-				if (rateCalculationMethod != undefined) {
-					directiveReadyDeferred = UtilsService.createPromiseDeferred();
-					var loadDirectivePromise = loadDirective();
-					promises.push(loadDirectivePromise);
-				}
+                if (rateCalculationMethod != undefined) {
+                    directiveReadyDeferred = UtilsService.createPromiseDeferred();
+                    var loadDirectivePromise = loadDirective();
+                    promises.push(loadDirectivePromise);
+                }
 
-				function loadRateCalculationMethodExtensionConfigs() {
-					return WhS_Sales_RatePlanAPIService.GetRateCalculationMethodTemplates().then(function (response) {
-						if (response != undefined) {
-							for (var i = 0; i < response.length; i++) {
-								$scope.scopeModel.rateCalculationMethods.push(response[i]);
-							}
-							if (rateCalculationMethod != undefined)
-								$scope.scopeModel.selectedRateCalculationMethod = UtilsService.getItemByVal($scope.scopeModel.rateCalculationMethods, rateCalculationMethod.ConfigId, 'ExtensionConfigurationId');
-						}
-					});
-				}
-				function loadDirective() {
-					var directiveLoadDeferred = UtilsService.createPromiseDeferred();
-					directiveReadyDeferred.promise.then(function () {
-						directiveReadyDeferred = undefined;
-						var directivePayload = {
-							rateCalculationMethod: rateCalculationMethod,
-							bulkActionContext: bulkActionContext
-						};
-						VRUIUtilsService.callDirectiveLoad(directiveAPI, directivePayload, directiveLoadDeferred);
-					});
-					return directiveLoadDeferred.promise;
-				}
+                function loadRateCalculationMethodExtensionConfigs() {
+                    return WhS_Sales_RatePlanAPIService.GetRateCalculationMethodTemplates().then(function (response) {
+                        if (response != undefined) {
+                            for (var i = 0; i < response.length; i++) {
+                                $scope.scopeModel.rateCalculationMethods.push(response[i]);
+                            }
+                            if (rateCalculationMethod != undefined)
+                                $scope.scopeModel.selectedRateCalculationMethod = UtilsService.getItemByVal($scope.scopeModel.rateCalculationMethods, rateCalculationMethod.ConfigId, 'ExtensionConfigurationId');
+                        }
+                    });
+                }
+                function loadDirective() {
+                    var directiveLoadDeferred = UtilsService.createPromiseDeferred();
+                    directiveReadyDeferred.promise.then(function () {
+                        directiveReadyDeferred = undefined;
+                        var directivePayload = {
+                            rateCalculationMethod: rateCalculationMethod,
+                            bulkActionContext: bulkActionContext
+                        };
+                        VRUIUtilsService.callDirectiveLoad(directiveAPI, directivePayload, directiveLoadDeferred);
+                    });
+                    return directiveLoadDeferred.promise;
+                }
 
-				return UtilsService.waitMultiplePromises(promises).finally(function () {
-					$scope.scopeModel.isLoading = false;
-				});
-			};
+                return UtilsService.waitMultiplePromises(promises).finally(function () {
+                    $scope.scopeModel.isLoading = false;
+                });
+            };
 
-			api.getData = function () {
-				return {
-					$type: 'TOne.WhS.Sales.MainExtensions.RateBulkAction, TOne.WhS.Sales.MainExtensions',
-					CostCalculationMethod: $scope.scopeModel.selectedCostCalculationMethod,
-					RateCalculationMethod: (directiveAPI != undefined) ? directiveAPI.getData() : null,
-					BED: ($scope.scopeModel.beginEffectiveDate != undefined) ? UtilsService.getDateFromDateTime($scope.scopeModel.beginEffectiveDate) : null
-				};
-			};
+            api.getData = function () {
+                return {
+                    $type: 'TOne.WhS.Sales.MainExtensions.RateBulkAction, TOne.WhS.Sales.MainExtensions',
+                    CostCalculationMethod: $scope.scopeModel.selectedCostCalculationMethod,
+                    RateCalculationMethod: (directiveAPI != undefined) ? directiveAPI.getData() : null,
+                    BED: ($scope.scopeModel.beginEffectiveDate != undefined) ? UtilsService.getDateFromDateTime($scope.scopeModel.beginEffectiveDate) : null
+                };
+            };
 
-			api.getSummary = function () {
-			    var rateCalculationMethodTitle = ($scope.scopeModel.selectedRateCalculationMethod != undefined) ? $scope.scopeModel.selectedRateCalculationMethod.Title : 'None';
-			    var rateCalculationMethodDescription = (directiveAPI != undefined) ? directiveAPI.getDescription() : 'None';
-			    var bedAsString = ($scope.scopeModel.beginEffectiveDate != undefined) ? UtilsService.getShortDate($scope.scopeModel.beginEffectiveDate) : 'None';
-			    return 'Rate Calculation Method: ' + rateCalculationMethodTitle + ' | Value: ' + rateCalculationMethodDescription + ' | BED: ' + bedAsString;
-			};
+            api.getSummary = function () {
+                var rateCalculationMethodTitle = ($scope.scopeModel.selectedRateCalculationMethod != undefined) ? $scope.scopeModel.selectedRateCalculationMethod.Title : 'None';
 
-			if (rateBulkActionTypeCtrl.onReady != null) {
-				rateBulkActionTypeCtrl.onReady(api);
-			}
-		}
-	}
+                var rateCalculationMethodDescription = 'None';
+                if (directiveAPI != undefined) {
+                    var directiveDescription = directiveAPI.getDescription();
+                    if (directiveDescription != undefined)
+                        rateCalculationMethodDescription = directiveDescription;
+                }
+
+                var bedAsString = ($scope.scopeModel.beginEffectiveDate != undefined) ? UtilsService.getShortDate($scope.scopeModel.beginEffectiveDate) : 'None';
+                return 'Rate Calculation Method: ' + rateCalculationMethodTitle + ' | Value: ' + rateCalculationMethodDescription + ' | BED: ' + bedAsString;
+            };
+
+            if (rateBulkActionTypeCtrl.onReady != null) {
+                rateBulkActionTypeCtrl.onReady(api);
+            }
+        }
+    }
 }]);
