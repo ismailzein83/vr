bf77858307ce033d23f034d2b83872efb5787b29
Ali Ballouk <VANRISE\ali.ballouk>
2017-02-27 13:46:23 +0000

    fix render first time issue
    git-tfs-id: [http://developmentvlan:8585/tfs/vanrise.collection]$/;C39586

--- Vanrise/Code/Vanrise.Web/Client/Javascripts/Directives/Containers/vr-dropdown-section.js
+++ Vanrise/Code/Vanrise.Web/Client/Javascripts/Directives/Containers/vr-dropdown-section.js
@@ -17,108 +17,106 @@
                 var ctrl = this;
                 ctrl.id = BaseDirService.generateHTMLElementName();
                 ctrl.menuid = BaseDirService.generateHTMLElementName();
-                ctrl.initializeSection = function () {
-                    ctrl.showmenu = true;
-                    calculatePosition(ctrl, true);
-                    addBackDrop();
-                    setTimeout(function () {
-
-                        checkOnExpandMethode();
-
-                        $('#' + ctrl.menuid).slideDown("slow");
-                    }, 1000);
-
-                };
-
-                $element.on('$destroy', function () {
-                    fixDropdownSectionPosition();
-                });
-                ctrl.expandSection = function (e) {
-                    if (e != undefined && $(e.target).hasClass('vanrise-inpute')) {
-                        return UtilsService.convertToPromiseIfUndefined(undefined);
-                    }
-                    var expandSectionDeferred = UtilsService.createPromiseDeferred();
-                    setTimeout(function () {
-                        $scope.$apply(function () {
-                            ctrl.showmenu = true;
-                            calculatePosition(ctrl);
-                        });
-                        $('#' + ctrl.menuid).slideDown("slow");
-                        addBackDrop();
-                        expandSectionDeferred.resolve();
-                        checkOnExpandMethode();
-                    }, 1000);
-                    return expandSectionDeferred.promise;
-                };
-                ctrl.collapseSection = function () {
-                    checkOnCollapseMethode();
-                    $('#' + ctrl.menuid).slideUp("slow", function () {
-                        fixDropdownSectionPosition();
-                    });
-                };
-                ctrl.loadSection = function () {
+                ctrl.initializeSection = function () {                   
                     $timeout(function () {
-                        if (ctrl.defaultstate == true) {
-                            ctrl.initializeSection();
-                        }
-                    }, 1000)
+                        $timeout(function () {
+                            scope.$apply(function () {
+                                ctrl.showmenu = true;
+                                calculatePosition(ctrl);
+                            });
+                            $('#' + ctrl.menuid).slideDown("slow");
+                            addBackDrop();
+                            expandSectionDeferred.resolve();
+                            checkOnExpandMethode();
+                        }, 0);
+                    }, 0);
                 };
-                $('#' + ctrl.id).parents('div').scroll(function () {
-                    fixDropdownSectionPosition();
-                });
-                $(window).scroll(function () {
-                    fixDropdownSectionPosition();
-                });
-                $(window).resize(function () {
-                    fixDropdownSectionPosition();
-                });
-                $(document).resize(function () {
-                    fixDropdownSectionPosition();
-                });
-                $(document).scroll(function () {
-                    fixDropdownSectionPosition();
-                });
-                $scope.$on('start-drag', function (event, args) {
-                    fixDropdownSectionPosition();
-                });
-                var fixDropdownSectionPosition = function () {
-                    ctrl.showmenu = false;
-                    $('.vr-backdrop').remove();
-                    $('.vr-backdrop-modal').remove();
-                };
-
-                function checkOnExpandMethode() {
-                    if ($attrs.onexpandsection != undefined) {
-                        var onExpandSectiondMethod = $scope.$parent.$eval($attrs.onexpandsection);
-                        if (onExpandSectiondMethod != undefined && typeof (onExpandSectiondMethod) == 'function') {
-                            onExpandSectiondMethod();
-                        }
-                    }
-                }
-                function checkOnCollapseMethode() {
-                    if ($attrs.oncollapsesection != undefined) {
-                        var onCollapseSectiondMethod = $scope.$parent.$eval($attrs.oncollapsesection);
-                        if (onCollapseSectiondMethod != undefined && typeof (onCollapseSectiondMethod) == 'function') {
-                            onCollapseSectiondMethod();
-                        }
-                    }
-                }
-
-                function addBackDrop() {
-                    if ($element.parents().find('.modal-dialog').length > 0)
-                        $('vr-modalbody').last().prepend("<div class='vr-backdrop-modal'></div>");
-                    else {
-                        $($element.parents().find("vr-form")).prepend("<div class='vr-backdrop'></div>");
-                    }
-                }
-
             },
-
             compile: function (element, attrs) {
                 return {
                     post: function postLink(scope, elem, attr, ctrl, transcludeFn) {
 
                         MultiTranscludeService.transclude(elem, transcludeFn);
+
+                        elem.on('$destroy', function () {
+                            fixDropdownSectionPosition();
+                        });
+                        ctrl.expandSection = function (e) {                          
+                            var expandSectionDeferred = UtilsService.createPromiseDeferred();
+                            $timeout(function () {
+
+                                if (e != undefined && $(e.target).hasClass('vanrise-inpute')) {
+                                    return UtilsService.convertToPromiseIfUndefined(undefined);
+                                }
+                                $timeout(function () {
+                                    scope.$apply(function () {
+                                        ctrl.showmenu = true;
+                                        calculatePosition(ctrl);
+                                    });
+                                    $('#' + ctrl.menuid).slideDown("slow");
+                                    addBackDrop();
+                                    expandSectionDeferred.resolve();
+                                    checkOnExpandMethode();
+                                },0);
+                                   
+                            }, 0);
+                            return expandSectionDeferred.promise;
+                        };
+
+                        ctrl.collapseSection = function () {
+                            checkOnCollapseMethode();
+                            $('#' + ctrl.menuid).slideUp("slow", function () {
+                                fixDropdownSectionPosition();
+                            });
+                        };
+                        $('#' + ctrl.id).parents('div').scroll(function () {
+                            fixDropdownSectionPosition();
+                        });
+                        $(window).scroll(function () {
+                            fixDropdownSectionPosition();
+                        });
+                        $(window).resize(function () {
+                            fixDropdownSectionPosition();
+                        });
+                        $(document).resize(function () {
+                            fixDropdownSectionPosition();
+                        });
+                        $(document).scroll(function () {
+                            fixDropdownSectionPosition();
+                        });
+                        scope.$on('start-drag', function (event, args) {
+                            fixDropdownSectionPosition();
+                        });
+                        var fixDropdownSectionPosition = function () {
+                            ctrl.showmenu = false;
+                            $('.vr-backdrop').remove();
+                            $('.vr-backdrop-modal').remove();
+                        };
+
+                        function checkOnExpandMethode() {
+                            if (attrs.onexpandsection != undefined) {
+                                var onExpandSectiondMethod = scope.$parent.$eval(attrs.onexpandsection);
+                                if (onExpandSectiondMethod != undefined && typeof (onExpandSectiondMethod) == 'function') {
+                                    onExpandSectiondMethod();
+                                }
+                            }
+                        }
+                        function checkOnCollapseMethode() {
+                            if (attrs.oncollapsesection != undefined) {
+                                var onCollapseSectiondMethod = scope.$parent.$eval(attrs.oncollapsesection);
+                                if (onCollapseSectiondMethod != undefined && typeof (onCollapseSectiondMethod) == 'function') {
+                                    onCollapseSectiondMethod();
+                                }
+                            }
+                        }
+
+                        function addBackDrop() {
+                            if (elem.parents().find('.modal-dialog').length > 0)
+                                $('vr-modalbody').last().prepend("<div class='vr-backdrop-modal'></div>");
+                            else {
+                                $(elem.parents().find("vr-form")).prepend("<div class='vr-backdrop'></div>");
+                            }
+                        }
 
                         var api = {};
 
@@ -147,7 +145,7 @@
                             + '<div class="vr-dropdown-section" id="{{ctrl.id}}" >'
                                 + '<div class="summary-container hand-cursor"  ng-click="ctrl.expandSection($event)" ng-class="ctrl.validationContext.validate() != null  ? \'invalid-section\':\'\' " >'
                                     + '<div transclude-id="summary" class="summary-main"></div>'
-                                    + '<span  class="glyphicon  glyphicon-sort-by-attributes  toogle-icon" ng-init="ctrl.loadSection()"></span>'
+                                    + '<span  class="glyphicon  glyphicon-sort-by-attributes  toogle-icon" ></span>'
                                     + '<span ng-if="ctrl.validationContext.validate() != null" class="validation-sign"  title="has validation errors!">*</span>'
                               + '</div>'
                                 + '<ul class="dropdown-menu drop-down-section"   id="{{ctrl.menuid}}" ng-show="ctrl.showmenu" ng-class="ctrl.validationContext.validate() != null  ? \'invalid-section\':\'\' "  >'
