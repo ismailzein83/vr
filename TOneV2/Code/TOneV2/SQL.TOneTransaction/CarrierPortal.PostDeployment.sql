b00cef110dcd1d407eabc9610d8021fbddb8f9a2
Wissam Ajamy <VANRISE\wissam.ajamy>
2017-02-28 12:16:18 +0000

    Move [runtime].[SchedulerTaskActionType] items from Transaction script to Configuration script
    git-tfs-id: [http://developmentvlan:8585/tfs/vanrise.collection]$/;C39626

--- TOneV2/Code/TOneV2/SQL.TOneTransaction/CarrierPortal.PostDeployment.sql
+++ TOneV2/Code/TOneV2/SQL.TOneTransaction/CarrierPortal.PostDeployment.sql
@@ -9,22 +9,3 @@ Post-Deployment Script Template
                SELECT * FROM [$(TableName)]					
 --------------------------------------------------------------------------------------
 */
---[runtime].[SchedulerTaskActionType]------------301 to 400-----------------------------------------
-----------------------------------------------------------------------------------------------------
-set nocount on;
-;with cte_data([ID],[Name],[ActionTypeInfo])
-as (select * from (values
---//////////////////////////////////////////////////////////////////////////////////////////////////
-(301,'Upload PriceList','{"URL":"","SystemType":false,"Editor":"cp-supplierpricelist-uploadsupplierpricelist","FQTN":"CP.SupplierPricelist.Business.PriceListTasks.UploadPriceListTaskAction, CP.SupplierPricelist.Business"}'),
-(302,'Get PriceList Result','{"URL":"","SystemType":false,"Editor":"cp-supplierpricelist-resultsupplierpricelist","FQTN":"CP.SupplierPricelist.Business.PriceListTasks.ResultTaskAction, CP.SupplierPricelist.Business"}')
---\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\
-)c([ID],[Name],[ActionTypeInfo]))
-merge	[runtime].[SchedulerTaskActionType] as t
-using	cte_data as s
-on		1=1 and t.[ID] = s.[ID]
-when matched then
-	update set
-	[Name] = s.[Name],[ActionTypeInfo] = s.[ActionTypeInfo]
-when not matched by target then
-	insert([ID],[Name],[ActionTypeInfo])
-	values(s.[ID],s.[Name],s.[ActionTypeInfo]);
