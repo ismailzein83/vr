0da8219006b1d618c0e07e8a252e36f1721fc773
mohamad.elhariri <mohamad.elhariri@vanrise.tfs.local>
2017-02-28 10:19:26 +0000

    Renamed the class
    git-tfs-id: [http://developmentvlan:8585/tfs/vanrise.collection]$/;C39615

--- TOneV2/Code/TOneV2/TOne.WhS.BusinessEntity.Entities/SaleRate/History/Query/CustomerZoneRateHistoryQuery.cs
+++ TOneV2/Code/TOneV2/TOne.WhS.BusinessEntity.Entities/SaleRate/History/Query/CustomerZoneRateHistoryQuery.cs
@@ -6,7 +6,8 @@ using System.Threading.Tasks;
 
 namespace TOne.WhS.BusinessEntity.Entities
 {
-    class CustomerZonerRateHistoryQuery
+    public class CustomerZoneRateHistoryQuery
     {
+        public int CustomerId { get; set; }
     }
 }
