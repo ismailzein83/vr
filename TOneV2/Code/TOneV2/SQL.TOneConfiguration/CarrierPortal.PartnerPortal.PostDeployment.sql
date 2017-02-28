2af61a2b3ff4cfd2d72c389c9ff0b7ed0bca9c19
Wissam Ajamy <VANRISE\wissam.ajamy>
2017-02-28 09:38:27 +0000

    Change RetailPrtal script name
    git-tfs-id: [http://developmentvlan:8585/tfs/vanrise.collection]$/;C39610

--- TOneV2/Code/TOneV2/SQL.TOneConfiguration/CarrierPortal.PartnerPortal.PostDeployment.sql
+++ TOneV2/Code/TOneV2/SQL.TOneConfiguration/CarrierPortal.PartnerPortal.PostDeployment.sql
@@ -9,27 +9,3 @@ Post-Deployment Script Template
                SELECT * FROM [$(TableName)]					
 --------------------------------------------------------------------------------------
 */
---[common].[Setting]---------------------------801 to 900-------------------------------------------
-begin
-set nocount on;
---set identity_insert [common].[Setting] on;
-;with cte_data([Id],[Name],[Type],[Category],[Settings],[Data],[IsTechnical])
-as (select * from (values
---//////////////////////////////////////////////////////////////////////////////////////////////////
-('509E467B-4562-4CA6-A32E-E50473B74D2C','Product Info','VR_Common_ProductInfoTechnicalSettings','General','{"Editor" : "vr-common-productinfotechnicalsettings-editor"}','{"$type":"Vanrise.Entities.ProductInfoTechnicalSettings, Vanrise.Entities","ProductInfo":{"$type":"Vanrise.Entities.ProductInfo, Vanrise.Entities","ProductName":"Partner Portal","VersionNumber":"version 0.9"}}',1)
---\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\
-)c([Id],[Name],[Type],[Category],[Settings],[Data],[IsTechnical]))
-merge	[common].[Setting] as t
-using	cte_data as s
-on		1=1 and t.[Id] = s.[Id]
-when matched then
-	update set
-	[Name] = s.[Name],[Type] = s.[Type],[Category] = s.[Category],[Settings] = s.[Settings],[Data] = s.[Data],[IsTechnical] = s.[IsTechnical]
-when not matched by target then
-	insert([Id],[Name],[Type],[Category],[Settings],[Data],[IsTechnical])
-	values(s.[Id],s.[Name],s.[Type],s.[Category],s.[Settings],s.[Data],s.[IsTechnical]);
---set identity_insert [common].[Setting] off;
-
-----------------------------------------------------------------------------------------------------
-
-end
