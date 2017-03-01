829d971000c9826f94d03d46264609758d9bcb63
Ali Atoui <VANRISE\ali.atoui>
2017-03-01 15:06:34 +0000

    Adding TOne.WhS.TOneV1Transition Module
    git-tfs-id: [http://developmentvlan:8585/tfs/vanrise.collection]$/;C39683

---
+++ TOneV2/Code/TOneV2/TOne.WhS.TOneV1Transition.Entities/Settings/TOneV1TransitionSettingsData.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Vanrise.Entities;
+
+namespace TOne.WhS.TOneV1Transition.Entities
+{
+    public class TOneV1TransitionSettingsData : SettingData
+    {
+
+    }
+}
