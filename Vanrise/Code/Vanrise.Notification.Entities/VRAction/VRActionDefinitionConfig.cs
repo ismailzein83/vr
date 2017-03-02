cb9c1dea8e2c39b6733f3aba85961a29642df283
anthony douaihy <VANRISE\anthony.douaihy>
2017-03-02 15:15:02 +0000

    add VRActionDefinitions
    git-tfs-id: [http://developmentvlan:8585/tfs/vanrise.collection]$/;C39725

---
+++ Vanrise/Code/Vanrise.Notification.Entities/VRAction/VRActionDefinitionConfig.cs
@@ -0,0 +1,11 @@
+﻿using Vanrise.Entities;
+
+namespace Vanrise.Notification.Entities
+{
+    public class VRActionDefinitionConfig : ExtensionConfiguration
+    {
+        public const string EXTENSION_TYPE = "VR_Notification_VRActionDefinition";
+
+        public string Editor { get; set; }
+    }
+}
