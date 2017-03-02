e6b60b1d76e37da7e1c883ec62813efa35faad4f
Ali Ballouk <VANRISE\ali.ballouk>
2017-03-02 13:00:09 +0000

    Add  vr-overall-widget
    git-tfs-id: [http://developmentvlan:8585/tfs/vanrise.collection]$/;C39715

---
+++ Vanrise/Code/Vanrise.Web/Client/Javascripts/Directives/Containers/vr-overall-widget.js
@@ -0,0 +1,35 @@
+﻿(function (app) {
+
+    "use strict";
+
+    vrOverallWidget.$inject = ['BaseDirService', 'VRValidationService', 'UtilsService'];
+
+    function vrOverallWidget(BaseDirService, VRValidationService, UtilsService) {
+
+        return {
+            restrict: 'E',
+            scope: {
+                datasource: '=',
+                maxitemperrow: '@'
+            },
+            controller: function ($scope, $element, $attrs) {
+                var ctrl = this;                
+            },
+            controllerAs: 'ctrl',
+            bindToController: true,
+            template: function (element, attrs) {
+                return '<vr-datalist maxitemsperrow="{{ctrl.maxitemperrow}}" datasource="ctrl.datasource">'
+                       + '<div class="overallwidget">'
+                        + '<div class="overallwidget-value"><div class="widget-center-text">{{dataItem.value}}</div></div>'
+                        + '<div class="overallwidget-label"><div class="widget-center-text">{{dataItem.label}}</div></div>'
+                       +'</div>'
+                       +'</vr-datalist>';
+            }
+
+        };
+        
+    }
+
+    app.directive('vrOverallWidget', vrOverallWidget);
+
+})(app);
