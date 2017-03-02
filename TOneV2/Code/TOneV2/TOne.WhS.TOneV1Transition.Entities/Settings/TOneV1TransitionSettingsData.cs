c120d58296504a0664636e2dba3a674a468434ac
Ali Atoui <VANRISE\ali.atoui>
2017-03-02 07:34:31 +0000

    Adding TOneV1TransitionSettingsData
    git-tfs-id: [http://developmentvlan:8585/tfs/vanrise.collection]$/;C39696

--- TOneV2/Code/TOneV2/TOne.WhS.TOneV1Transition.Entities/Settings/TOneV1TransitionSettingsData.cs
+++ TOneV2/Code/TOneV2/TOne.WhS.TOneV1Transition.Entities/Settings/TOneV1TransitionSettingsData.cs
@@ -3,12 +3,13 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using TOne.WhS.DBSync.Business;
 using Vanrise.Entities;
 
 namespace TOne.WhS.TOneV1Transition.Entities
 {
     public class TOneV1TransitionSettingsData : SettingData
     {
-
+        public DBSyncTaskActionArgument DBSyncTaskActionArgument { get; set; }
     }
 }
