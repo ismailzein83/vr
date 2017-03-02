663011c23ce228878275491d743d1e1a810938ba
anthony douaihy <VANRISE\anthony.douaihy>
2017-03-02 15:15:37 +0000

    add DAProfCalcSendEmailExtendedSettings
    git-tfs-id: [http://developmentvlan:8585/tfs/vanrise.collection]$/;C39726

---
+++ Vanrise/Code/Vanrise.Analytic.MainExtensions/DataAnalysis/ProfilingAndCalculation/DAProfCalcSendEmailExtendedSettings.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using Vanrise.Notification.Entities;
+
+namespace Vanrise.Analytic.MainExtensions.DataAnalysis.ProfilingAndCalculation
+{
+    public class DAProfCalcSendEmailExtendedSettings : VRActionExtendedSettings
+    {
+
+    }
+}
