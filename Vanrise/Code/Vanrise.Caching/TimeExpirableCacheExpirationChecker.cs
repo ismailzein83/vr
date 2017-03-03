b0c2f54ac570cfe5d76adf2d172b0e0155d8667d
Samer Haidar <VANRISE\samer.haidar>
2017-03-03 10:00:04 +0000

    added TimeExpirableCacheExpirationChecker
    git-tfs-id: [http://developmentvlan:8585/tfs/vanrise.collection]$/;C39752

---
+++ Vanrise/Code/Vanrise.Caching/TimeExpirableCacheExpirationChecker.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Vanrise.Caching
+{
+    public class TimeExpirableCacheExpirationChecker : CacheExpirationChecker
+    {
+        TimeSpan _expiredAfter;
+        public TimeExpirableCacheExpirationChecker(TimeSpan expiredAfter)
+        {
+            _expiredAfter = expiredAfter;
+        }
+        public override bool IsCacheExpired(ICacheExpirationCheckerContext context)
+        {
+            return (DateTime.Now - context.CachedObject.CreatedTime) >= _expiredAfter;
+        }
+    }
+}
