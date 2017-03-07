6c08aba7f4a7c01e84a91e4b1cb9bbfeaa7affce
anthony douaihy <VANRISE\anthony.douaihy>
2017-03-07 08:41:00 +0000

    adjust index name on route table
    git-tfs-id: [http://developmentvlan:8585/tfs/vanrise.collection]$/;C39828

--- TOneV2/Code/TOneV2/TOne.Whs.Routing.Data.TOneV1SQL/CustomerRouteDataManager.cs
+++ TOneV2/Code/TOneV2/TOne.Whs.Routing.Data.TOneV1SQL/CustomerRouteDataManager.cs
@@ -292,7 +292,7 @@ namespace TOne.Whs.Routing.Data.TOneV1SQL
 	                                                      [SupplierZoneID] ASC
                                                       );";
 
-        const string query_CreateRouteIndexes = @"  CREATE CLUSTERED INDEX [PK_TempRouteID] ON [dbo].[Route_Temp] 
+        const string query_CreateRouteIndexes = @"  CREATE CLUSTERED INDEX [PK_RouteID] ON [dbo].[Route_Temp] 
 			                                        (
 				                                        [RouteID] ASC
 			                                        )WITH (PAD_INDEX  = OFF, STATISTICS_NORECOMPUTE  = OFF, SORT_IN_TEMPDB = OFF, IGNORE_DUP_KEY = OFF, DROP_EXISTING = OFF, ONLINE = OFF, ALLOW_ROW_LOCKS  = ON, ALLOW_PAGE_LOCKS  = ON) ON [{0}];
