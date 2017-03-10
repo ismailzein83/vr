762e599079582b62427fb72edb886d773d52c340
Samer Haidar <VANRISE\samer.haidar>
2017-03-10 07:27:47 +0000

    sync with demo database
    git-tfs-id: [http://developmentvlan:8585/tfs/vanrise.collection]$/;C39928

---
+++ TOneV2/Code/TOneV2/SQL.TOneLogging/logging/Stored Procedures/sp_ObjectTracking_GetFiltered.sql
@@ -0,0 +1,23 @@
+﻿-- =============================================
+-- Author:		<Author,,Name>
+-- Create date: <Create Date,,>
+-- Description:	<Description,,>
+-- =============================================
+create PROCEDURE [logging].[sp_ObjectTracking_GetFiltered]
+	(@LoggableEntityId uniqueidentifier ,
+	@ObjectId varchar(255))
+AS
+
+SET NOCOUNT ON;
+
+	BEGIN
+
+	 SELECT ID,UserID
+		  ,ActionID
+		  ,LogTime
+	  FROM [DemoProject_Dev_Log].[logging].[ObjectTracking] as ac
+
+	  where ac.LoggableEntityID=@LoggableEntityId
+	  AND ac.ObjectID=@ObjectId
+	END
+SET NOCOUNT OFF
