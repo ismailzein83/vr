5eed4fce30961a5328ace03b7787618106e7fba9
mohammad.zhour <mohammad.zhour@vanrise.tfs.local>
2017-03-10 15:43:27 +0000

    adding AccountHistoryView
    git-tfs-id: [http://developmentvlan:8585/tfs/vanrise.collection]$/;C39948

---
+++ Retail/Code/Retail.BusinessEntity.MainExtensions/AccountViews/AccountHistoryView.cs
@@ -0,0 +1,36 @@
+﻿    using Retail.BusinessEntity.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Retail.BusinessEntity.MainExtensions.AccountViews
+{
+   public  class AccountHistoryView: AccountViewDefinitionSettings
+    {
+        public override Guid ConfigId
+        {
+            get { return new Guid("FB232763-6AC1-49B5-A410-FA792980055C"); }
+        }
+
+        public override string RuntimeEditor
+        {
+            get
+            {
+                return "retail-be-accounthistory-view";
+            }
+            set
+            {
+
+            }
+        }
+
+        public override bool DoesUserHaveAccess(IAccountViewDefinitionCheckAccessContext context)
+        {
+            return base.DoesUserHaveAccess(context);
+        }
+    }
+}
+
+
