0e84e18a15f2b3e79dfe4912fe3eb22f136d7f2f
Wissam Ajamy <VANRISE\wissam.ajamy>
2017-03-10 07:58:43 +0000

    FIX SP ERROR
    git-tfs-id: [http://developmentvlan:8585/tfs/vanrise.collection]$/;C39931

--- TOneV2/Code/TOneV2/SQL.TOneLogging/logging/Stored Procedures/sp_ObjectTracking_GetFiltered.sql
+++ TOneV2/Code/TOneV2/SQL.TOneLogging/logging/Stored Procedures/sp_ObjectTracking_GetFiltered.sql
@@ -3,21 +3,13 @@
 -- Create date: <Create Date,,>
 -- Description:	<Description,,>
 -- =============================================
-create PROCEDURE [logging].[sp_ObjectTracking_GetFiltered]
-	(@LoggableEntityId uniqueidentifier ,
-	@ObjectId varchar(255))
+CREATE PROCEDURE [logging].[sp_ObjectTracking_GetFiltered]
+@LoggableEntityId uniqueidentifier ,
+@ObjectId varchar(255)
 AS
-
-SET NOCOUNT ON;
-
-	BEGIN
-
-	 SELECT ID,UserID
-		  ,ActionID
-		  ,LogTime
-	  FROM [DemoProject_Dev_Log].[logging].[ObjectTracking] as ac
-
-	  where ac.LoggableEntityID=@LoggableEntityId
-	  AND ac.ObjectID=@ObjectId
-	END
-SET NOCOUNT OFF
+BEGIN
+SELECT	ac.ID,ac.UserID,ac.ActionID,ac.LogTime
+FROM	[logging].[ObjectTracking]  ac WITH(NOLOCK)
+WHERE	ac.LoggableEntityID=@LoggableEntityId
+		AND ac.ObjectID=@ObjectId
+END
