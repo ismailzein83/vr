5b2e0ab32a4c424f92244a0f201a08c1d7274c34
Wissam Ajamy <VANRISE\wissam.ajamy>
2017-03-14 09:16:17 +0000

    update structure from TOneConfiguration
    git-tfs-id: [http://developmentvlan:8585/tfs/vanrise.collection]$/;C39986

---
+++ TOneV2/Code/TOneV2/SQL.TOneConfiguration/runtime/Stored Procedures/sp_SchedulerTask_SetDisable.sql
@@ -0,0 +1,10 @@
+﻿CREATE PROCEDURE [runtime].[sp_SchedulerTask_SetDisable] 
+	@ID uniqueidentifier
+AS
+BEGIN
+	begin
+		UPDATE	[runtime].[ScheduleTask]
+		SET		IsEnabled = 0
+		WHERE	ID = @ID
+	end
+END
---
+++ TOneV2/Code/TOneV2/SQL.TOneConfiguration/runtime/Stored Procedures/sp_SchedulerTask_SetEnable.sql
@@ -0,0 +1,10 @@
+﻿CREATE PROCEDURE [runtime].[sp_SchedulerTask_SetEnable]
+	@ID uniqueidentifier
+AS
+BEGIN
+	begin
+		UPDATE	[runtime].[ScheduleTask]
+		SET		IsEnabled = 1
+		WHERE	ID = @ID
+	end
+END
