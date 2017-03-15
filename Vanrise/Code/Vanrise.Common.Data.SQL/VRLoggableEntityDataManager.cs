9778949131e763fd237dcd50b86b386ccc895107
Samer Haidar <VANRISE\samer.haidar>
2017-03-15 08:48:58 +0000

    adding new key LoggingConfigDBConnStringKey and change the location of loggable entity from local logging to configuration database
    git-tfs-id: [http://developmentvlan:8585/tfs/vanrise.collection]$/;C40007

--- Vanrise/Code/Vanrise.Common.Data.SQL/VRLoggableEntityDataManager.cs
+++ Vanrise/Code/Vanrise.Common.Data.SQL/VRLoggableEntityDataManager.cs
@@ -10,7 +10,7 @@ namespace Vanrise.Common.Data.SQL
     public class VRLoggableEntityDataManager : Vanrise.Data.SQL.BaseSQLDataManager, IVRLoggableEntityDataManager
     {
         public VRLoggableEntityDataManager()
-            : base(GetConnectionStringName("LoggingDBConnStringKey", "LogDBConnString"))
+            : base(GetConnectionStringName("LoggingConfigDBConnStringKey", "LogConfigDBConnString"))
         {
 
         }
