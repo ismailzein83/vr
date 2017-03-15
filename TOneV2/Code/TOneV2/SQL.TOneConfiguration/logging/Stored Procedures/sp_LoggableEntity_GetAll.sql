31e90db78b13d288770c03bae2ab074a5831e3ce
Wissam Ajamy <VANRISE\wissam.ajamy>
2017-03-15 09:54:54 +0000

    Update structure from TOneConfiguration
    git-tfs-id: [http://developmentvlan:8585/tfs/vanrise.collection]$/;C40016

---
+++ TOneV2/Code/TOneV2/SQL.TOneConfiguration/logging/Stored Procedures/sp_LoggableEntity_GetAll.sql
@@ -0,0 +1,11 @@
+﻿-- =============================================
+-- Author:		<Author,,Name>
+-- Create date: <Create Date,,>
+-- Description:	<Description,,>
+-- =============================================
+CREATE PROCEDURE [logging].[sp_LoggableEntity_GetAll]
+AS
+BEGIN
+	SELECT	[ID],[UniqueName],[Settings]
+	FROM	[logging].[LoggableEntity] WITH(NOLOCK)
+END
---
+++ TOneV2/Code/TOneV2/SQL.TOneConfiguration/logging/Stored Procedures/sp_LoggableEntity_InsertOrUpdate.sql
@@ -0,0 +1,29 @@
+﻿-- =============================================
+-- Author:		<Author,,Name>
+-- Create date: <Create Date,,>
+-- Description:	<Description,,>
+-- =============================================
+CREATE PROCEDURE [logging].[sp_LoggableEntity_InsertOrUpdate] 
+	@UniqueName varchar(255),
+	@Settings nvarchar(max)
+AS
+BEGIN
+	DECLARE @ID uniqueidentifier = (SELECT ID FROM [logging].[LoggableEntity] WITH(NOLOCK) WHERE [UniqueName] = @UniqueName)
+	
+	IF @ID IS NULL
+		BEGIN
+			INSERT INTO [logging].[LoggableEntity]
+			(ID, [UniqueName], [Settings])
+			SELECT newid(), @UniqueName, @Settings WHERE NOT EXISTS (SELECT TOP 1 NULL FROM [logging].[LoggableEntity] WHERE [UniqueName] = @UniqueName)
+
+			SET @ID = (SELECT ID FROM [logging].[LoggableEntity] WITH(NOLOCK) WHERE [UniqueName] = @UniqueName)
+		END
+	ELSE
+		BEGIN
+			UPDATE	[logging].[LoggableEntity]
+			SET		Settings = @Settings
+			WHERE	ID = @ID
+		END
+
+	SELECT @ID
+END
