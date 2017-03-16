51958e6d576076d0d6bb4b076484f74244a2e26b
Ali Atoui <VANRISE\ali.atoui>
2017-03-16 15:25:49 +0000

    Editing DataRecordNotificationTypeSettings
    git-tfs-id: [http://developmentvlan:8585/tfs/vanrise.collection]$/;C40066

--- Vanrise/Code/Vanrise.GenericData.Notification/DataRecord/DataRecordNotificationTypeSettings.cs
+++ Vanrise/Code/Vanrise.GenericData.Notification/DataRecord/DataRecordNotificationTypeSettings.cs
@@ -6,7 +6,7 @@ using System.Threading.Tasks;
 using Vanrise.Notification.Entities;
 
 namespace Vanrise.GenericData.Notification
-{
+{ 
     public class DataRecordNotificationTypeSettings : VRNotificationTypeExtendedSettings
     {
         public override Guid ConfigId { get { return new Guid("E64C51A2-08E0-4B7D-96F0-9FF1848A72FA"); } }
@@ -19,27 +19,27 @@ namespace Vanrise.GenericData.Notification
 
         public List<NotificationGridColumnDefinition> GridColumnDefinitions { get; set; }
 
-        public override VRNotificationDetail GetVRNotificationDetailMapper(IGetVRNotificationDetailMapperContext context)
-        {
-            VRNotification vrNotification = context.VRNotification;
-            if (vrNotification == null)
-                throw new NullReferenceException("vrNotification");
+        //public override VRNotificationDetail GetVRNotificationDetailMapper(IGetVRNotificationDetailMapperContext context)
+        //{
+        //    VRNotification vrNotification = context.VRNotification;
+        //    if (vrNotification == null)
+        //        throw new NullReferenceException("vrNotification");
 
-            VRNotificationData vrNotificationData = context.VRNotification.Data;
-            if (vrNotificationData == null)
-                throw new NullReferenceException("vrNotificationData");
+        //    VRNotificationData vrNotificationData = context.VRNotification.Data;
+        //    if (vrNotificationData == null)
+        //        throw new NullReferenceException("vrNotificationData");
 
-            DataRecordAlertRuleActionEventPayload eventPayload = vrNotificationData.EventPayload as DataRecordAlertRuleActionEventPayload;
-            if (eventPayload == null)
-                throw new NullReferenceException("dataRecordAlertRuleActionEventPayload");
+        //    DataRecordAlertRuleActionEventPayload eventPayload = vrNotificationData.EventPayload as DataRecordAlertRuleActionEventPayload;
+        //    if (eventPayload == null)
+        //        throw new NullReferenceException("dataRecordAlertRuleActionEventPayload");
 
-            AlertRuleActionEventPayloadDetail alertRuleActionEventPayloadDetail = new AlertRuleActionEventPayloadDetail()
-            {
-                Entity = context.VRNotification,
-                FieldValues = eventPayload.OutputRecords
-            };
-            return alertRuleActionEventPayloadDetail;
-        }
+        //    AlertRuleActionEventPayloadDetail alertRuleActionEventPayloadDetail = new AlertRuleActionEventPayloadDetail()
+        //    {
+        //        Entity = context.VRNotification,
+        //        FieldValues = eventPayload.OutputRecords
+        //    };
+        //    return alertRuleActionEventPayloadDetail;
+        //}
     }
 
     public class NotificationGridColumnDefinition
