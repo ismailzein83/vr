60e0c176a5937789b7ccddd38e3ac9aa6b8c7a44
Ali Atoui <VANRISE\ali.atoui>
2017-03-16 07:47:12 +0000

    Arranging DAProfCalcRecordTypeExtraFields
    git-tfs-id: [http://developmentvlan:8585/tfs/vanrise.collection]$/;C40035

--- Vanrise/Code/Vanrise.Analytic.Business/DataAnalysis/DataAnalysisItemExtraFields.cs
+++ Vanrise/Code/Vanrise.Analytic.Business/DataAnalysis/DataAnalysisItemExtraFields.cs
@@ -7,11 +7,12 @@ namespace Vanrise.Analytic.Business
 {
     public class DAProfCalcRecordTypeExtraFields : DataRecordTypeExtraField
     {
-        public Guid DataAnalysisItemDefinitionId { get; set; }
+        public override Guid ConfigId { get { return new Guid("93F44A29-235D-4C3F-900E-6D7FE780CEF3"); } }
 
         public Guid DataAnalysisDefinitionId { get; set; }
 
-        public override Guid ConfigId { get { return new Guid("93F44A29-235D-4C3F-900E-6D7FE780CEF3"); } }
+        public Guid DataAnalysisItemDefinitionId { get; set; }
+
 
         public override List<DataRecordField> GetFields(IDataRecordExtraFieldContext context)
         {
