28cff4971710fe62e4b43e1c26eaa73e5352ff9d
Ali Atoui <VANRISE\ali.atoui>
2017-03-16 07:46:03 +0000

    Arranging DataRecordSendEmailDefinitionSettings
    git-tfs-id: [http://developmentvlan:8585/tfs/vanrise.collection]$/;C40034

--- Vanrise/Code/Vanrise.GenericData.Notification/DataRecord/DataRecordSendEmailDefinitionSettings.cs
+++ Vanrise/Code/Vanrise.GenericData.Notification/DataRecord/DataRecordSendEmailDefinitionSettings.cs
@@ -5,19 +5,13 @@ namespace Vanrise.GenericData.Notification
 {
     public class DataRecordSendEmailDefinitionSettings : VRActionDefinitionExtendedSettings
     {
+        public override Guid ConfigId { get { return new Guid("3B904E8C-2AC0-43DB-A4EF-425869D40544"); } }
+
+        public override string RuntimeEditor { get { return "vr-genericdata-datarecord-vraction-sendemail"; } }
+
         public Guid DataRecordTypeId { get; set; }
 
         public Guid MailMessageTypeId { get; set; }
-
-        public override Guid ConfigId
-        {
-            get { return new Guid("3B904E8C-2AC0-43DB-A4EF-425869D40544"); }
-
-        }
-        public override string RuntimeEditor
-        {
-            get { return "vr-genericdata-datarecord-vraction-sendemail"; }
-        }
 
         public override bool IsApplicable(IVRActionDefinitionIsApplicableContext context)
         {
