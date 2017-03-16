5e6f34a3b2668f6c3f0b631615be2487875792b5
Ali Atoui <VANRISE\ali.atoui>
2017-03-16 07:28:43 +0000

    Adding DataRecordNotificationTypeSettings
    git-tfs-id: [http://developmentvlan:8585/tfs/vanrise.collection]$/;C40031

---
+++ Vanrise/Code/Vanrise.GenericData.Notification/DataRecord/DataRecordNotificationTypeSettings.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Vanrise.Notification.Entities;
+
+namespace Vanrise.GenericData.Notification.DataRecord
+{
+    public class DataRecordNotificationTypeSettings : VRNotificationTypeExtendedSettings
+    {
+        public override Guid ConfigId { get { return new Guid("E64C51A2-08E0-4B7D-96F0-9FF1848A72FA"); } }
+
+        public override string SearchRuntimeEditor { get { return "vr-analytic-notification-searcheditor"; } }
+
+        public override string BodyRuntimeEditor { get { return "vr-analytic-notification-bodyeditor"; } }
+
+        public Guid DataRecordTypeId { get; set; }
+
+        public List<string> GridColumns { get; set; }
+    }
+
+    public class DataRecordNotificationColumnDefinition
+    {
+        public string FieldName { get; set; }
+
+        public string Header { get; set; }
+    }
+}
