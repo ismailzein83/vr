f345ee850b0e935faf2e2124236ea2ab99fa1eb0
Ali Atoui <VANRISE\ali.atoui>
2017-03-17 15:51:47 +0000

    Arranging businessprocessBpInstanceMonitorGrid
    git-tfs-id: [http://developmentvlan:8585/tfs/vanrise.collection]$/;C40115

--- BusinessProcess/Code/Vanrise.BusinessProcess.Web/BusinessProcess/Directives/BPInstance/bp-instance-monitor-grid.js
+++ BusinessProcess/Code/Vanrise.BusinessProcess.Web/BusinessProcess/Directives/BPInstance/bp-instance-monitor-grid.js
@@ -1,173 +1,172 @@
 ﻿"use strict";
 
 app.directive("businessprocessBpInstanceMonitorGrid", ["BusinessProcess_BPInstanceAPIService", "BusinessProcess_BPInstanceService", "BusinessProcess_GridMaxSize", "VRTimerService",
-function (BusinessProcess_BPInstanceAPIService, BusinessProcess_BPInstanceService, BusinessProcess_GridMaxSize, VRTimerService) {
+    function (BusinessProcess_BPInstanceAPIService, BusinessProcess_BPInstanceService, BusinessProcess_GridMaxSize, VRTimerService) {
 
-    var directiveDefinitionObject = {
+        var directiveDefinitionObject = {
+            restrict: "E",
+            scope: {
+                onReady: "="
+            },
+            controller: function ($scope, $element, $attrs) {
+                var ctrl = this;
 
-        restrict: "E",
-        scope: {
-            onReady: "="
-        },
-        controller: function ($scope, $element, $attrs) {
-            var ctrl = this;
+                var bpInstanceGrid = new BPInstanceGrid($scope, ctrl, $attrs);
+                bpInstanceGrid.initializeController();
+            },
+            controllerAs: "ctrl",
+            bindToController: true,
+            compile: function (element, attrs) {
 
-            var bpInstanceGrid = new BPInstanceGrid($scope, ctrl, $attrs);
-            bpInstanceGrid.initializeController();
-        },
-        controllerAs: "ctrl",
-        bindToController: true,
-        compile: function (element, attrs) {
-
-        },
-        templateUrl: "/Client/Modules/BusinessProcess/Directives/BPInstance/Templates/BPInstanceMonitorGridTemplate.html"
-
-    };
-
-    function BPInstanceGrid($scope, ctrl) {
-
-        var lastUpdateHandle, lessThanID, nbOfRows, definitionsId, entityId;
-        var input = {
-            LastUpdateHandle: lastUpdateHandle,
-            LessThanID: lessThanID,
-            NbOfRows: nbOfRows,
-            DefinitionsId: definitionsId,
-            EntityId :entityId
+            },
+            templateUrl: "/Client/Modules/BusinessProcess/Directives/BPInstance/Templates/BPInstanceMonitorGridTemplate.html"
         };
 
-        var gridAPI;
-        this.initializeController = initializeController;
+        function BPInstanceGrid($scope, ctrl) {
+            this.initializeController = initializeController;
 
-        $scope.loadMoreData = function () {
-            return getData();
-        };
-
-        var minId = undefined;
-
-        function initializeController() {
-            $scope.bpInstances = [];
+            //Variables
             var isGettingDataFirstTime = true;
+            var minId;
 
-            $scope.onGridReady = function (api) {
-                gridAPI = api;
-                if (ctrl.onReady != undefined && typeof (ctrl.onReady) == "function")
-                    ctrl.onReady(getDirectiveAPI());
+            var lastUpdateHandle, lessThanID, nbOfRows, definitionsId, entityId;
 
-                function getDirectiveAPI() {
-                    var directiveAPI = {};
-                    directiveAPI.loadGrid = function (query) {
-                        $scope.isLoading = true;
-                        input.LastUpdateHandle = undefined;
-                        input.LessThanID = undefined;
-                        input.NbOfRows = undefined;
-                        input.DefinitionsId = query.DefinitionsId;
-                        input.ParentId = query.BPInstanceID;
-                        input.EntityId = query.EntityId;
+            var input = {
+                LastUpdateHandle: lastUpdateHandle,
+                LessThanID: lessThanID,
+                NbOfRows: nbOfRows,
+                DefinitionsId: definitionsId,
+                EntityId: entityId
+            };
 
-                        $scope.bpInstances.length = 0;
-                        isGettingDataFirstTime = true;
-                        minId = undefined;
-                        createTimer();
-                    };
+            //API
+            var gridAPI;
 
-                    directiveAPI.clearTimer = function () {
-                        if ($scope.job) {
-                            VRTimerService.unregisterJob($scope.job);
-                        }
-                    };
-                    return directiveAPI;
-                }
+            function initializeController() {
+                $scope.bpInstances = [];
 
-                function manipulateDataUpdated(response) {
-                    var itemAddedOrUpdatedInThisCall = false;
-                    if (response != undefined) {
-                        for (var i = 0; i < response.ListBPInstanceDetails.length; i++) {
-                            var bpInstance = response.ListBPInstanceDetails[i];
+                $scope.onGridReady = function (api) {
+                    gridAPI = api;
+                    defineAPI();
+                };
 
-                            if (!isGettingDataFirstTime && bpInstance.Entity.ProcessInstanceID < minId) {
-                                continue;
-                            }
+                $scope.loadMoreData = function () {
+                    return getData();
+                };
 
-                            var findBPInstance = false;
+                $scope.getStatusColor = function (dataItem) {
+                    return BusinessProcess_BPInstanceService.getStatusColor(dataItem.Entity.Status);
+                };
 
-                            for (var j = 0; j < $scope.bpInstances.length; j++) {
-                                if ($scope.bpInstances[j].Entity.ProcessInstanceID == bpInstance.Entity.ProcessInstanceID) {
-                                    $scope.bpInstances[j] = bpInstance;
-                                    findBPInstance = true;
-                                    itemAddedOrUpdatedInThisCall = true;
-                                    continue;
-                                }
-                            }
-                            if (!findBPInstance) {
-                                itemAddedOrUpdatedInThisCall = true;
-                                $scope.bpInstances.push(bpInstance);
-                            }
-                        }
+                $scope.processInstanceClicked = function (dataItem) {
+                    BusinessProcess_BPInstanceService.openProcessTracking(dataItem.Entity.ProcessInstanceID);
+                };
+            }
+            function defineAPI() {
+                var api = {};
 
-                        if (itemAddedOrUpdatedInThisCall) {
-                            if ($scope.bpInstances.length > 0) {
-                                $scope.bpInstances.sort(function (a, b) {
-                                    return b.Entity.ProcessInstanceID - a.Entity.ProcessInstanceID;
-                                });
+                api.loadGrid = function (query) {
+                    $scope.isLoading = true;
+                    input.LastUpdateHandle = undefined;
+                    input.LessThanID = undefined;
+                    input.NbOfRows = undefined;
+                    input.DefinitionsId = query.DefinitionsId;
+                    input.ParentId = query.BPInstanceID;
+                    input.EntityId = query.EntityId;
 
-                                if ($scope.bpInstances.length > BusinessProcess_GridMaxSize.maximumCount) {
-                                    $scope.bpInstances.length = BusinessProcess_GridMaxSize.maximumCount;
-                                }
-                                minId = $scope.bpInstances[$scope.bpInstances.length - 1].Entity.ProcessInstanceID;
-                                isGettingDataFirstTime = false;
-                            }
-                        }
+                    $scope.bpInstances.length = 0;
+                    isGettingDataFirstTime = true;
+                    minId = undefined;
 
-                    }
-                    input.LastUpdateHandle = response.MaxTimeStamp;
-                }
+                    createTimer();
+                };
 
-                function createTimer() {
+                api.clearTimer = function () {
                     if ($scope.job) {
                         VRTimerService.unregisterJob($scope.job);
                     }
-                    var pageInfo = gridAPI.getPageInfo();
-                    input.NbOfRows = pageInfo.toRow - pageInfo.fromRow + 1;
-                    VRTimerService.registerJob(onTimerElapsed, $scope);
+                };
+
+                if (ctrl.onReady != null)
+                    ctrl.onReady(api);
+            }
+
+            function getData() {
+                var pageInfo = gridAPI.getPageInfo();
+                input.LessThanID = minId;
+                input.NbOfRows = pageInfo.toRow - pageInfo.fromRow + 1;
+
+                return BusinessProcess_BPInstanceAPIService.GetBeforeId(input).then(function (response) {
+                    if (response != undefined && response) {
+                        for (var i = 0; i < response.length; i++) {
+                            var bpInstance = response[i];
+                            minId = response[i].Entity.ProcessInstanceID;
+                            $scope.bpInstances.push(bpInstance);
+                        }
+                    }
+                });
+            }
+
+            function createTimer() {
+                if ($scope.job) {
+                    VRTimerService.unregisterJob($scope.job);
                 }
 
-                function onTimerElapsed() {
-                    return BusinessProcess_BPInstanceAPIService.GetUpdated(input).then(function (response) {
-                        manipulateDataUpdated(response);
-                        $scope.isLoading = false;
-                    },
-                     function (excpetion) {
-                         console.log(excpetion);
-                         $scope.isLoading = false;
-                     });
-                }
-            };
-        }
+                var pageInfo = gridAPI.getPageInfo();
+                input.NbOfRows = pageInfo.toRow - pageInfo.fromRow + 1;
+                VRTimerService.registerJob(onTimerElapsed, $scope);
+            }
+            function onTimerElapsed() {
+                return BusinessProcess_BPInstanceAPIService.GetUpdated(input).then(function (response) {
+                    manipulateDataUpdated(response);
+                }).finally(function () {
+                    $scope.isLoading = false;
+                });
+            }
+            function manipulateDataUpdated(response) {
+                var itemAddedOrUpdatedInThisCall = false;
 
-        $scope.getStatusColor = function (dataItem) {
-            return BusinessProcess_BPInstanceService.getStatusColor(dataItem.Entity.Status);
-        };
+                if (response != undefined) {
+                    for (var i = 0; i < response.ListBPInstanceDetails.length; i++) {
+                        var bpInstance = response.ListBPInstanceDetails[i];
 
-        $scope.processInstanceClicked = function (dataItem) {
-            BusinessProcess_BPInstanceService.openProcessTracking(dataItem.Entity.ProcessInstanceID);
-        };
+                        if (!isGettingDataFirstTime && bpInstance.Entity.ProcessInstanceID < minId) {
+                            continue;
+                        }
 
-        function getData() {
+                        var findBPInstance = false;
 
-            var pageInfo = gridAPI.getPageInfo();
-            input.LessThanID = minId;
-            input.NbOfRows = pageInfo.toRow - pageInfo.fromRow + 1;
-            return BusinessProcess_BPInstanceAPIService.GetBeforeId(input).then(function (response) {
-                if (response != undefined && response) {
-                    for (var i = 0; i < response.length; i++) {
-                        var bpInstance = response[i];
-                        minId = response[i].Entity.ProcessInstanceID;
-                        $scope.bpInstances.push(bpInstance);
+                        for (var j = 0; j < $scope.bpInstances.length; j++) {
+                            if ($scope.bpInstances[j].Entity.ProcessInstanceID == bpInstance.Entity.ProcessInstanceID) {
+                                $scope.bpInstances[j] = bpInstance;
+                                findBPInstance = true;
+                                itemAddedOrUpdatedInThisCall = true;
+                                continue;
+                            }
+                        }
+                        if (!findBPInstance) {
+                            itemAddedOrUpdatedInThisCall = true;
+                            $scope.bpInstances.push(bpInstance);
+                        }
+                    }
+
+                    if (itemAddedOrUpdatedInThisCall) {
+                        if ($scope.bpInstances.length > 0) {
+                            $scope.bpInstances.sort(function (a, b) {
+                                return b.Entity.ProcessInstanceID - a.Entity.ProcessInstanceID;
+                            });
+
+                            if ($scope.bpInstances.length > BusinessProcess_GridMaxSize.maximumCount) {
+                                $scope.bpInstances.length = BusinessProcess_GridMaxSize.maximumCount;
+                            }
+                            minId = $scope.bpInstances[$scope.bpInstances.length - 1].Entity.ProcessInstanceID;
+                            isGettingDataFirstTime = false;
+                        }
                     }
                 }
-            });
+                input.LastUpdateHandle = response.MaxTimeStamp;
+            }
         }
-    }
-    return directiveDefinitionObject;
 
-}]);
+        return directiveDefinitionObject;
+    }]);
