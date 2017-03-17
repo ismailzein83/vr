583f3ad7ec8e9e2022b773a9974ac3607ce1ab14
Ali Ballouk <VANRISE\ali.ballouk>
2017-03-17 08:14:46 +0000

    Add SchedulerTaskActionType Controller and API js service
    git-tfs-id: [http://developmentvlan:8585/tfs/vanrise.collection]$/;C40081

---
+++ Vanrise/Code/Vanrise.Runtime.Web/Controllers/SchedulerTaskActionTypeController.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Http;
+using Vanrise.Runtime.Business;
+using Vanrise.Runtime.Entities;
+using Vanrise.Web.Base;
+
+namespace Vanrise.Runtime.Web.Controllers
+{
+    [Vanrise.Web.Base.JSONWithType]
+    [RoutePrefix(Constants.ROUTE_PREFIX + "SchedulerTaskActionType")]
+    public class SchedulerTaskActionTypeController : Vanrise.Web.Base.BaseAPIController
+    {
+
+        [HttpGet]
+        [Route("GetSchedulerTaskActionTypes")]
+        public List<SchedulerTaskActionType> GetSchedulerTaskActionTypes()
+        {
+            SchedulerTaskManager manager = new SchedulerTaskManager();
+            return manager.GetSchedulerTaskActionTypes();
+        }
+    }
+}
---
+++ Vanrise/Code/Vanrise.Runtime.Web/Runtime/Services/APIServices/SchedulerTaskActionTypeAPIService.js
@@ -0,0 +1,18 @@
+﻿(function (appService) {
+
+    'use strict';
+    SchedulerTaskActionTypeAPIService.$inject = ['BaseAPIService', 'UtilsService', 'VR_Runtime_ModuleConfig', 'SecurityService'];
+
+    function SchedulerTaskActionTypeAPIService(BaseAPIService, UtilsService, VR_Runtime_ModuleConfig, SecurityService) {
+        var controllerName = 'SchedulerTaskActionType';
+
+        function GetSchedulerTaskActionTypes() {
+            return BaseAPIService.get(UtilsService.getServiceURL(VR_Runtime_ModuleConfig.moduleName, controllerName, 'GetSchedulerTaskActionTypes'));
+        }
+        return ({
+           
+            GetSchedulerTaskActionTypes: GetSchedulerTaskActionTypes
+        });
+    }
+    appService.service('SchedulerTaskActionTypeAPIService', SchedulerTaskActionTypeAPIService);
+})(appService);
