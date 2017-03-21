6fb2aa64f0d590c95d015f0ba1559acd6e192cf5
anthony douaihy <VANRISE\anthony.douaihy>
2017-03-21 14:32:10 +0000

    Adding New Files in GenericData.Notification
    git-tfs-id: [http://developmentvlan:8585/tfs/vanrise.collection]$/;C40165

---
+++ Vanrise/Code/Vanrise.GenericData.Notification/DataRecord/DataRecordAlertRuleConfig.cs
@@ -0,0 +1,11 @@
+﻿using Vanrise.Entities;
+
+namespace Vanrise.GenericData.Notification
+{
+    public class DataRecordAlertRuleConfig : ExtensionConfiguration
+    {
+        public const string EXTENSION_TYPE = "VR_GenericData_DataRecordAlertRuleConfig";
+
+        public string Editor { get; set; }
+    }
+}
---
+++ Vanrise/Code/Vanrise.GenericData.Notification/DataRecord/DataRecordAlertRuleManager.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Vanrise.Common.Business;
+
+namespace Vanrise.GenericData.Notification
+{
+    public class DataRecordAlertRuleManager
+    {
+        public IEnumerable<DataRecordAlertRuleConfig> GetDataRecordAlertRuleConfigs()
+        {
+            var templateConfigManager = new ExtensionConfigurationManager();
+            return templateConfigManager.GetExtensionConfigurations<DataRecordAlertRuleConfig>(DataRecordAlertRuleConfig.EXTENSION_TYPE);
+        }
+    }
+}
