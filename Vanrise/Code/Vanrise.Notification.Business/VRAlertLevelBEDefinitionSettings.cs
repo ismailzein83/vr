a0819e46549bce59bd13622b2853feca93cb76d2
mohammad.zhour <mohammad.zhour@vanrise.tfs.local>
2017-03-23 11:17:59 +0000

    adding new class for alert level
    git-tfs-id: [http://developmentvlan:8585/tfs/vanrise.collection]$/;C40219

---
+++ Vanrise/Code/Vanrise.Notification.Business/VRAlertLevelBEDefinitionSettings.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Vanrise.GenericData.Entities;
+
+namespace Vanrise.Notification.Business
+{
+    public class VRAlertLevelBEDefinitionSettings : BusinessEntityDefinitionSettings
+    {
+
+        public static Guid s_configId = Guid.Empty;
+        public override Guid ConfigId { get { throw new NotImplementedException(); } }
+        public override string SelectorFilterEditor { get; set; }
+        public override string DefinitionEditor
+        {
+            get { throw new NotImplementedException(); }
+        }
+        public override string IdType
+        {
+            get { throw new NotImplementedException(); }
+        }
+        public override string ManagerFQTN
+        {
+            get { throw new NotImplementedException(); }
+        }
+        public override string SelectorUIControl
+        {
+            get { throw new NotImplementedException(); }
+        }
+    }
+}
