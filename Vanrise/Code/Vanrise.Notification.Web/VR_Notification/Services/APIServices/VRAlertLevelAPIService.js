b07c942a194dc9ee870a84ae9b3e34bd213ef26f
mohammad.zhour <mohammad.zhour@vanrise.tfs.local>
2017-03-23 15:20:31 +0000

    adding new files needed
    git-tfs-id: [http://developmentvlan:8585/tfs/vanrise.collection]$/;C40230

---
+++ Vanrise/Code/Vanrise.Notification.Web/VR_Notification/Services/APIServices/VRAlertLevelAPIService.js
@@ -0,0 +1,43 @@
+﻿
+(function (appControllers) {
+
+    "use strict";
+    VRAlertLevelAPIService.$inject = ['BaseAPIService', 'UtilsService', 'VRNotification_ModuleConfig', 'SecurityService'];
+
+    function StatusDefinitionAPIService(BaseAPIService, UtilsService, VRNotification_ModuleConfig, SecurityService) {
+
+        var controllerName = "VRAlertLevel";
+
+
+        function GetFilteredAlertLevels(input) {
+            return BaseAPIService.post(UtilsService.getServiceURL(VRNotification_ModuleConfig.moduleName, controllerName, 'GetFilteredAlertLevels'), input);
+        }
+
+        function AddAlertLevel(alertLevelItem) {
+            return BaseAPIService.post(UtilsService.getServiceURL(VRNotification_ModuleConfig.moduleName, controllerName, 'AddAlertLevel'), alertLevelItem);
+        }
+
+        function UpdateAlertLevel(alertLevelItem) {
+            return BaseAPIService.post(UtilsService.getServiceURL(VRNotification_ModuleConfig.moduleName, controllerName, 'UpdateAlertLevel'), alertLevelItem);
+        }
+
+       
+        function GetAlertLevel(alertLevelId) {
+            return BaseAPIService.get(UtilsService.getServiceURL(VRNotification_ModuleConfig.moduleName, controllerName, 'GetAlertLevel'), {
+                alertLevelId: alertLevelId
+            });
+        }
+       
+
+        return ({
+            GetFilteredAlertLevels: GetFilteredAlertLevels,
+            AddAlertLevel: AddAlertLevel,
+            UpdateAlertLevel: UpdateAlertLevel,
+            GetAlertLevel: GetAlertLevel,
+           
+        });
+    }
+
+    appControllers.service('VR_Notifacation_AlertLevetAPIService', VRAlertLevelAPIService);
+
+})(appControllers);
---
+++ Vanrise/Code/Vanrise.Notification.Web/VR_Notification/Services/VRAlertService.js
@@ -0,0 +1,42 @@
+﻿
+(function (appControllers) {
+
+    'use stict';
+
+    VRAlertLevelService.$inject = ['VRModalService'];
+
+    function VRAlertLevelService(VRModalService) {
+
+        function addAlertLevel(onAlertLevelAdded) {
+            var settings = {};
+
+            settings.onScopeReady = function (modalScope) {
+                modalScope.onAlertLevelAdded = onAlertLevelAdded
+            };
+
+            VRModalService.showModal('/Client/Modules/Notification/Views/VRAlertLevel/VRALertLevelEditor.html', null, settings);
+        };
+
+        function editAlertLevel(alertLevelId, onAlertLevelUpdated) {
+            var settings = {};
+
+            var parameters = {
+                alertLevelId: alertLevelId,
+            };
+
+            settings.onScopeReady = function (modalScope) {
+                modalScope.onAlertLevelUpdated = onAlertLevelUpdated;
+            };
+            VRModalService.showModal('/Client/Modules/Notification/Views/VRAlertLevel/VRALertLevelEditor.html', parameters, settings);
+        }
+
+
+        return {
+            addAlertLevel: addAlertLevel,
+            editAlertLevel: editAlertLevel
+        };
+    }
+
+    appControllers.service('VR_Notifictation_AlertLevelService', StatusDefinitionService);
+
+})(appControllers);
