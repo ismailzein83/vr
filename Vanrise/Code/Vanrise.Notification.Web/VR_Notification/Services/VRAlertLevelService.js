5278432955b2ee67b52fa4aa980cbdff3fdbdb2a
mohammad.zhour <mohammad.zhour@vanrise.tfs.local>
2017-03-24 10:15:36 +0000

    adding reference and new file
    git-tfs-id: [http://developmentvlan:8585/tfs/vanrise.collection]$/;C40261

---
+++ Vanrise/Code/Vanrise.Notification.Web/VR_Notification/Services/VRAlertLevelService.js
@@ -0,0 +1,42 @@
+﻿
+(function (appControllers) {
+
+    'use stict';
+
+    AlertLEvelService.$inject = ['VRModalService'];
+
+    function AlertLEvelService(VRModalService) {
+
+        function addAlertLevel(onAlertLevelAdded) {
+            var settings = {};
+
+            settings.onScopeReady = function (modalScope) {
+                modalScope.onAlertLevelAdded = onAlertLevelAdded
+            };
+
+            VRModalService.showModal('/Client/Modules/VR_Notification/Views/VRAlertLevel/VRAlertLevelEditor.html', null, settings);
+        }
+
+        function editAlertLEvel(alertLevelId, onAlertLevelUpdated) {
+            var settings = {};
+
+            var parameters = {
+                alertLevelId: alertLevelId,
+            };
+
+            settings.onScopeReady = function (modalScope) {
+                modalScope.onAlertLevelUpdated = onAlertLevelUpdated;
+            };
+            VRModalService.showModal('/Client/Modules/VR_Notification/Views/VRAlertLevel/VRAlertLevelEditor.html', parameters, settings);
+        }
+
+
+        return {
+            addAlertLevel: addAlertLevel,
+            editAlertLEvel: editAlertLEvel
+        };
+    }
+
+    appControllers.service('VR_Notification_AlertLevelService', AlertLEvelService);
+
+})(appControllers);
