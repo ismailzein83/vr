ba0a55836c790b6dfe6ab70363a2dea769d9d9fe
mohamad.elhariri <mohamad.elhariri@vanrise.tfs.local>
2017-03-28 12:21:47 +0000

    Added AccountController
    git-tfs-id: [http://developmentvlan:8585/tfs/vanrise.collection]$/;C40334

---
+++ Vanrise/Code/Vanrise.AccountBalance.Web/Controllers/AccountController.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Vanrise.AccountBalance.Web.Controllers
+{
+    public class AccountController
+    {
+    }
+}
