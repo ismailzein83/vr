de13677f0768817eb1bf08e31cfbf10d2a63ce85
anthony douaihy <VANRISE\anthony.douaihy>
2017-03-29 07:42:29 +0000

    add sp_VRNotification_GetNotCleared SP
    git-tfs-id: [http://developmentvlan:8585/tfs/vanrise.collection]$/;C40362

---
+++ TOneV2/Code/TOneV2/SQL.TOneTransaction/VRNotification/Stored Procedures/sp_VRNotification_GetNotCleared.sql
@@ -0,0 +1,32 @@
+﻿CREATE Procedure [VRNotification].[sp_VRNotification_GetNotCleared]
+	@NotificationTypeID uniqueidentifier,
+	@ParentType1 varchar(255) = null,
+	@ParentType2 varchar(255) = null,
+	@EventKeys nvarchar(max) = null,
+	@NotificationCreatedAfter datetime = null
+AS
+BEGIN
+	DECLARE @EventKeysTable TABLE (EventKey nvarchar(900))
+	INSERT INTO @EventKeysTable (EventKey)
+	SELECT Convert(nvarchar(900), ParsedString) FROM [VRNotification].[ParseStringList](@EventKeys)
+
+	SELECT	 [ID]
+			,[UserID]
+			,[TypeID]
+			,[ParentType1]
+			,[ParentType2]
+			,[EventKey]
+			,[BPProcessInstanceID]
+			,[Status]
+			,[Description]
+			,[AlertLevelID]
+			,[ErrorMessage]
+			,[Data]
+			,[CreationTime]
+	FROM [VRNotification].[VRNotification] WITH(NOLOCK)
+	where TypeID = @NotificationTypeID
+	      and (@EventKeys is null or EventKey in (select EventKey from @EventKeysTable))
+		  and (@ParentType1 is null or ParentType1 = @ParentType1)
+		  and (@ParentType2 is null or ParentType2 = @ParentType2)
+		  and (@NotificationCreatedAfter is null or CreationTime > @NotificationCreatedAfter)
+END
