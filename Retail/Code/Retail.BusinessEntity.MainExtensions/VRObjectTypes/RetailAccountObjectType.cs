51551edec88b55ddcc1ec65fb932778f59a7b0c7
anthony douaihy <VANRISE\anthony.douaihy>
2017-03-29 15:19:15 +0000

    Implement CreateObject on VRObjectType
    git-tfs-id: [http://developmentvlan:8585/tfs/vanrise.collection]$/;C40397

--- Retail/Code/Retail.BusinessEntity.MainExtensions/VRObjectTypes/RetailAccountObjectType.cs
+++ Retail/Code/Retail.BusinessEntity.MainExtensions/VRObjectTypes/RetailAccountObjectType.cs
@@ -1,8 +1,6 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using Retail.BusinessEntity.Business;
+using Retail.BusinessEntity.Entities;
+using System;
 using Vanrise.Entities;
 
 namespace Retail.BusinessEntity.MainExtensions.VRObjectTypes
@@ -12,6 +10,16 @@ namespace Retail.BusinessEntity.MainExtensions.VRObjectTypes
         public override Guid ConfigId { get { return new Guid("1dd9cb13-ccbb-47ef-8514-6cca50aef298"); } }
 
         public Guid AccountBEDefinitionId { get; set; }
-    }
 
+        public override object CreateObject(IVRObjectTypeCreateObjectContext context)
+        {
+            AccountBEManager accountBEManager = new AccountBEManager();
+            Account account = accountBEManager.GetAccount(AccountBEDefinitionId, (long)context.ObjectId);
+
+            if (account == null)
+                throw new DataIntegrityValidationException(string.Format("Account not found for AccountBEDefinitionId : '{0}' and Account ID: '{1}'", AccountBEDefinitionId, context.ObjectId));
+
+            return account;
+        }
+    }
 }
