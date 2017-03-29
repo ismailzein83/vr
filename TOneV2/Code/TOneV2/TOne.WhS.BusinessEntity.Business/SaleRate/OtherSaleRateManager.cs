a450c6158e71ecc27340608b92a4251a3032fb35
mohamad.elhariri <mohamad.elhariri@vanrise.tfs.local>
2017-03-29 11:31:13 +0000

    Fixing build error
    git-tfs-id: [http://developmentvlan:8585/tfs/vanrise.collection]$/;C40372

---
+++ TOneV2/Code/TOneV2/TOne.WhS.BusinessEntity.Business/SaleRate/OtherSaleRateManager.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TOne.WhS.BusinessEntity.Entities;
+
+namespace TOne.WhS.BusinessEntity.Business
+{
+    public class OtherSaleRateManager
+    {
+        //public IEnumerable<SaleRateDetail> GetOtherSaleRates(SaleRateQuery query)
+        //{
+        //    IEnumerable<long> zoneIds = null;
+
+        //    if (query.OwnerType == SalePriceListOwnerType.SellingProduct)
+        //    {
+        //        var sellingProductZoneRateHistoryLocator = new SellingProductZoneRateHistoryLocator(new SellingProductZoneRateHistoryReader(query.OwnerId, zoneIds, false, true));
+        //        var rateTypeManager = new Vanrise.Common.Business.RateTypeManager().GetAllRateTypes();
+        //    }
+        //    else
+        //    {
+
+        //    }
+        //}
+    }
+
+    public class OtherSaleRateQuery
+    {
+        public string ZoneName { get; set; }
+
+        public SalePriceListOwnerType OwnerType { get; set; }
+
+        public int OwnerId { get; set; }
+
+        public DateTime EffectiveOn { get; set; }
+
+        public int CurrencyId { get; set; }
+    }
+}
