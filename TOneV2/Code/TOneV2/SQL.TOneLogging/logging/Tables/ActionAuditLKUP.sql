936649c28cf93213af6a26a07da9eb59b346dd4d
Samer Haidar <VANRISE\samer.haidar>
2017-03-31 06:53:02 +0000

    Remove tables from logging database and adding new parameters SQLLogger
    git-tfs-id: [http://developmentvlan:8585/tfs/vanrise.collection]$/;C40465

--- TOneV2/Code/TOneV2/SQL.TOneLogging/logging/Tables/ActionAuditLKUP.sql
+++ TOneV2/Code/TOneV2/SQL.TOneLogging/logging/Tables/ActionAuditLKUP.sql
@@ -1,12 +1,4 @@
-﻿CREATE TABLE [logging].[ActionAuditLKUP] (
-    [ID]          INT           IDENTITY (1, 1) NOT NULL,
-    [Type]        INT           NOT NULL,
-    [Name]        VARCHAR (255) NOT NULL,
-    [CreatedTime] DATETIME      CONSTRAINT [DF_ActionAuditLKUP_CreatedTime] DEFAULT (getdate()) NULL,
-    [timestamp]   ROWVERSION    NULL,
-    CONSTRAINT [PK_ActionAuditLKUP] PRIMARY KEY CLUSTERED ([ID] ASC),
-    CONSTRAINT [IX_ActionAuditLKUP_TypeName] UNIQUE NONCLUSTERED ([Type] ASC, [Name] ASC)
-);
+﻿
 
 
 
--- TOneV2/Code/TOneV2/SQL.TOneLogging/logging/Tables/UserActionAudit.sql
+++ TOneV2/Code/TOneV2/SQL.TOneLogging/logging/Tables/UserActionAudit.sql
@@ -1,13 +1,4 @@
-﻿CREATE TABLE [logging].[UserActionAudit] (
-    [ID]             BIGINT        IDENTITY (1, 1) NOT NULL,
-    [UserID]         INT           NULL,
-    [ModuleName]     VARCHAR (50)  NOT NULL,
-    [ControllerName] VARCHAR (50)  NOT NULL,
-    [ActionName]     VARCHAR (100) NOT NULL,
-    [LogTime]        DATETIME      CONSTRAINT [DF_UserActionAudit_LogTime] DEFAULT (getdate()) NOT NULL,
-    [BaseUrl]        VARCHAR (100) NOT NULL,
-    CONSTRAINT [PK_UserActionAudit] PRIMARY KEY NONCLUSTERED ([ID] ASC)
-);
+﻿
 
 
 
@@ -17,6 +8,5 @@
 
 
 GO
-CREATE CLUSTERED INDEX [IX_UserActionAudit_LogTime]
-    ON [logging].[UserActionAudit]([LogTime] ASC);
+
 
