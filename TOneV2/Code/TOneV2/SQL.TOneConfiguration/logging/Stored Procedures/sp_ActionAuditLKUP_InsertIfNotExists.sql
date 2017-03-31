1e3e31e24324300730808bf53a5a098944eb375d
Wissam Ajamy <VANRISE\wissam.ajamy>
2017-03-31 06:47:45 +0000

    Update structure from TOneConfiguration
    git-tfs-id: [http://developmentvlan:8585/tfs/vanrise.collection]$/;C40462

---
+++ TOneV2/Code/TOneV2/SQL.TOneConfiguration/logging/Stored Procedures/sp_ActionAuditLKUP_InsertIfNotExists.sql
@@ -0,0 +1,18 @@
+﻿
+-- =============================================
+-- Author:		<Author,,Name>
+-- Create date: <Create Date,,>
+-- Description:	<Description,,>
+-- =============================================
+CREATE PROCEDURE [logging].[sp_ActionAuditLKUP_InsertIfNotExists]
+	@Type int,
+	@Name varchar(255)
+AS
+BEGIN
+	INSERT INTO [logging].[ActionAuditLKUP] 
+	([Type], Name)
+	SELECT @Type, @Name
+	WHERE NOT EXISTS (SELECT NULL FROM [logging].[ActionAuditLKUP] WITH (NOLOCK) WHERE [Type] = @Type AND Name = @Name)
+
+    SELECT ID FROM [logging].[ActionAuditLKUP] WITH (NOLOCK) WHERE [Type] = @Type AND Name = @Name
+END
---
+++ TOneV2/Code/TOneV2/SQL.TOneConfiguration/logging/Tables/LogAttribute.sql
@@ -0,0 +1,9 @@
+﻿CREATE TABLE [logging].[LogAttribute] (
+    [ID]            INT           IDENTITY (1, 1) NOT NULL,
+    [AttributeType] INT           NOT NULL,
+    [Description]   VARCHAR (850) NOT NULL,
+    [timestamp]     ROWVERSION    NULL,
+    CONSTRAINT [PK_LogAttribute] PRIMARY KEY CLUSTERED ([ID] ASC),
+    CONSTRAINT [IX_LogAttribute_TypeDescription] UNIQUE NONCLUSTERED ([AttributeType] ASC, [Description] ASC)
+);
+
