5480476b04d6280fa771654e0233481602ed8f42
Wissam Ajamy <VANRISE\wissam.ajamy>
2017-03-31 11:36:47 +0000

    Update structure from TOneV2_Dev
    git-tfs-id: [http://developmentvlan:8585/tfs/vanrise.collection]$/;C40493

---
+++ TOneV2/Code/TOneV2/SQL.TOneWhS_BE/TOneWhS_Deal/Tables/DealSupplierZoneGroup.sql
@@ -0,0 +1,7 @@
+﻿CREATE TABLE [TOneWhS_Deal].[DealSupplierZoneGroup] (
+    [ID]     INT NOT NULL,
+    [DealID] INT NOT NULL,
+    [Volume] INT NOT NULL,
+    CONSTRAINT [PK_DealSupplierZoneGroup] PRIMARY KEY CLUSTERED ([ID] ASC)
+);
+
