ac0583f1d36e325e8bdfbc4743181dcdf6e9d1fa
anthony douaihy <VANRISE\anthony.douaihy>
2017-04-03 13:35:59 +0000

    Implement OnBPExecutionCompleted for ExecuteNotification BP
    git-tfs-id: [http://developmentvlan:8585/tfs/vanrise.collection]$/;C40538

---
+++ Vanrise/Code/Vanrise.Notification.Business/BPDefinitionExtendedSettings/ExecuteNotificationBPDefinitionExtendedSettings.cs
@@ -0,0 +1,28 @@
+﻿using Vanrise.BusinessProcess.Business;
+using Vanrise.BusinessProcess.Entities;
+using Vanrise.Notification.Entities;
+using Vanrise.Common;
+using Vanrise.Notification.BP.Arguments;
+using System;
+
+namespace Vanrise.Notification.Business
+{
+    public class ExecuteNotificationBPDefinitionExtendedSettings : DefaultBPDefinitionExtendedSettings
+    {
+        public override void OnBPExecutionCompleted(IBPDefinitionBPExecutionCompletedContext context)
+        {
+            VRNotificationStatus notificationStatus;
+            switch (context.BPInstance.Status)
+            {
+                case BPInstanceStatus.Completed: notificationStatus = VRNotificationStatus.Executed; break;
+                case BPInstanceStatus.Aborted:
+                case BPInstanceStatus.Terminated:
+                case BPInstanceStatus.Suspended: notificationStatus = VRNotificationStatus.Suspended; break;
+                default: throw new NotSupportedException(string.Format("BPInstanceStatus {0} not supported.", context.BPInstance.Status));
+            }
+
+            ExecuteNotificationProcessInput inputArgument = context.BPInstance.InputArgument.CastWithValidate<ExecuteNotificationProcessInput>("context.BPInstance.InputArgument", context.BPInstance.ProcessInstanceID);
+            new VRNotificationManager().UpdateNotificationStatus(inputArgument.NotificationId, notificationStatus);
+        }
+    }
+}
