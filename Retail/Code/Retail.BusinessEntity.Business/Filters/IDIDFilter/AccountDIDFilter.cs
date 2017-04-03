230c6e77b4a295d50c09e27ffa6f96b84161f0e0
Ali Atoui <VANRISE\ali.atoui>
2017-04-03 09:18:52 +0000

    Editing AccountDIDFilter
    git-tfs-id: [http://developmentvlan:8585/tfs/vanrise.collection]$/;C40533

--- Retail/Code/Retail.BusinessEntity.Business/Filters/IDIDFilter/AccountDIDFilter.cs
+++ Retail/Code/Retail.BusinessEntity.Business/Filters/IDIDFilter/AccountDIDFilter.cs
@@ -1,19 +1,13 @@
 ﻿using Retail.BusinessEntity.Entities;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using Vanrise.GenericData.Business;
 
 namespace Retail.BusinessEntity.Business
 {
     public class AccountDIDFilter : BERelationChildBaseFilter, IDIDFilter
     {
-
         public bool IsMatched(IDIDFilterContext context)
         {
-            return base.IsChildExcluded(context.DID.DIDId);
+            return !base.IsChildExcluded(context.DID.DIDId);
         }
     }
 }
