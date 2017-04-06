bfad25d958c6175ea90a9e1e182495fe6280bbea
Ismail Zein <VANRISE\ismail.zein>
2017-04-06 12:13:49 +0000

    added AccountBalanceUpdateProcessBPSettings
    git-tfs-id: [http://developmentvlan:8585/tfs/vanrise.collection]$/;C40635

---
+++ Vanrise/Code/Vanrise.AccountBalance.Business/AccountBalanceUpdateProcessBPSettings.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Vanrise.AccountBalance.BP.Arguments;
+using Vanrise.Common;
+
+namespace Vanrise.AccountBalance.Business
+{
+    public class AccountBalanceUpdateProcessBPSettings : Vanrise.BusinessProcess.Business.DefaultBPDefinitionExtendedSettings
+    {
+        public override bool CanRunBPInstance(BusinessProcess.Entities.IBPDefinitionCanRunBPInstanceContext context)
+        {
+            context.IntanceToRun.ThrowIfNull("context.IntanceToRun");
+            AccountBalanceUpdateProcessInput inputArg = context.IntanceToRun.InputArgument.CastWithValidate<AccountBalanceUpdateProcessInput>("context.IntanceToRun.InputArgument");
+            foreach (var startedBPInstance in context.GetStartedBPInstances())
+            {
+                AccountBalanceUpdateProcessInput startedBPInstanceInputArg = startedBPInstance.InputArgument as AccountBalanceUpdateProcessInput;
+                if (startedBPInstanceInputArg != null)
+                {
+                    if (startedBPInstanceInputArg.AccountTypeId == inputArg.AccountTypeId)
+                    {
+                        context.Reason = "Another process is running for the same account type";
+                        return false;
+                    }
+                }
+            }
+            return true;
+        }
+    }
+}
