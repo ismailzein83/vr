9f920c50c3c98f21027ec4d96ec8362326a227f2
Ismail Zein <VANRISE\ismail.zein>
2017-04-06 11:00:27 +0000

    sql project
    git-tfs-id: [http://developmentvlan:8585/tfs/vanrise.collection]$/;C40629

---
+++ TOneV2/Code/TOneV2/SQL.TOneTransaction/bp/Stored Procedures/sp_BPInstance_UpdateLastMessage.sql
@@ -0,0 +1,12 @@
+﻿CREATE PROCEDURE [bp].[sp_BPInstance_UpdateLastMessage]	
+	@ID bigint,
+	@Message nvarchar(max)
+AS
+BEGIN
+	-- SET NOCOUNT ON added to prevent extra result sets from
+	-- interfering with SELECT statements.
+	
+    UPDATE bp.BPInstance
+    SET	LastMessage = @Message
+	WHERE ID = @ID
+END
