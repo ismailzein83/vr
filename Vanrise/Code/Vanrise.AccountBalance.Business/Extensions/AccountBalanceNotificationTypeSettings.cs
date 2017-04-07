ace809156048153f351e06e33f76ee908533c652
Ali Atoui <VANRISE\ali.atoui>
2017-04-07 10:02:27 +0000

    Adding AccountBalanceNotificationTypeSettings
    git-tfs-id: [http://developmentvlan:8585/tfs/vanrise.collection]$/;C40664

---
+++ Vanrise/Code/Vanrise.AccountBalance.Business/Extensions/AccountBalanceNotificationTypeSettings.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using Vanrise.GenericData.Business;
+using Vanrise.Notification.Business;
+using Vanrise.Notification.Entities;
+
+namespace Vanrise.AccountBalance.Business
+{
+    public class AccountBalanceNotificationTypeSettings : VRNotificationTypeExtendedSettings
+    {
+        public override Guid ConfigId { get { return new Guid("0FC411D1-90FD-417C-BFDF-EC0C35B1A666"); } }
+
+        public override string SearchRuntimeEditor { get { return "vr-accountbalance-notification-searcheditor"; } }
+
+        public override string BodyRuntimeEditor { get { return "vr-accountbalance-notification-bodyeditor"; } }
+
+        public override VRNotificationDetailEventPayload GetNotificationDetailEventPayload(IVRNotificationTypeGetNotificationEventPayloadContext context)
+        {
+            VRBalanceAlertEventPayload eventPayload = new VRNotificationManager().GetVRNotificationEventPayload<VRBalanceAlertEventPayload>(context.VRNotification);
+
+            Guid BEDefinitionId = Guid.NewGuid();
+
+            AlertRuleActionEventPayload alertRuleActionEventPayload = new AlertRuleActionEventPayload()
+            {
+                BusinessEntityDescription = new BusinessEntityManager().GetEntityDescription(BEDefinitionId, eventPayload.EntityId),
+                CurrentBalance = eventPayload.CurrentBalance,
+                Threshold = eventPayload.Threshold
+            };
+
+            return alertRuleActionEventPayload;
+        }
+
+        public override bool IsVRNotificationMatched(IVRNotificationTypeIsMatchedContext context)
+        {
+            throw new NotImplementedException();
+        }
+    }
+
+    public class AlertRuleActionEventPayload : VRNotificationDetailEventPayload
+    {
+        public string BusinessEntityDescription { get; set; }
+
+        public Decimal CurrentBalance { get; set; }
+
+        public Decimal Threshold { get; set; }
+    }
+}
