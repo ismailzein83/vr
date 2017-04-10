009e733a821ca9134ba4a89c62fb7bc474a41114
Wissam Ajamy <VANRISE\wissam.ajamy>
2017-04-10 10:29:49 +0000

    update stucture from Retail_Dev_CDR
    git-tfs-id: [http://developmentvlan:8585/tfs/vanrise.collection]$/;C40716

--- TOneV2/Code/TOneV2/SQL.TOneConfiguration/VR_AccountBalance/Tables/BalanceUsageQueue1.sql
+++
@@ -1,8 +0,0 @@
-﻿CREATE TABLE [VR_AccountBalance].[BalanceUsageQueue1] (
-    [ID]            BIGINT           IDENTITY (1, 1) NOT NULL,
-    [AccountTypeID] UNIQUEIDENTIFIER NOT NULL,
-    [QueueType]     INT              NULL,
-    [UsageDetails]  VARBINARY (MAX)  NOT NULL,
-    [CreatedTime]   DATETIME         NULL
-);
-
