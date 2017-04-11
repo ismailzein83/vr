7bf4b9c77b8186f67c9adbeddd986ee6cae11acd
Wissam Ajamy <VANRISE\wissam.ajamy>
2017-04-11 14:23:46 +0000

    Update structure from TOneV2_Dev_Analytics
    git-tfs-id: [http://developmentvlan:8585/tfs/vanrise.collection]$/;C40771

---
+++ TOneV2/Code/TOneV2/SQL.TOneAnalytics/Security/TOneWhS_Deal.sql
@@ -0,0 +1,3 @@
+﻿CREATE SCHEMA [TOneWhS_Deal]
+    AUTHORIZATION [dbo];
+
---
+++ TOneV2/Code/TOneV2/SQL.TOneAnalytics/TOneWhS_Deal/Tables/DealProgress.sql
@@ -0,0 +1,13 @@
+﻿CREATE TABLE [TOneWhS_Deal].[DealProgress] (
+    [ID]                   BIGINT           IDENTITY (1, 1) NOT NULL,
+    [ZoneGroupId]          UNIQUEIDENTIFIER NOT NULL,
+    [IsSale]               BIT              NOT NULL,
+    [TierNb]               INT              NOT NULL,
+    [RateTierNb]           INT              NOT NULL,
+    [FromTime]             DATETIME         NOT NULL,
+    [ToTime]               DATETIME         NOT NULL,
+    [ReachedDurationInSec] DECIMAL (20, 4)  NOT NULL,
+    [CreatedTime]          DATETIME         CONSTRAINT [DF_DealProgress_CreatedTime] DEFAULT (getdate()) NULL,
+    CONSTRAINT [PK_DealSaleProgress_1] PRIMARY KEY CLUSTERED ([ID] ASC)
+);
+
