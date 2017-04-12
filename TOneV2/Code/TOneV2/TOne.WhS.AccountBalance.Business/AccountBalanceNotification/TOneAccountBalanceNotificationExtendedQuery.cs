3f1d0e9e18142eb915b5b860b3dce01526c4f2bb
Ali Atoui <VANRISE\ali.atoui>
2017-04-12 08:57:12 +0000

    Editing TOneAccountBalanceNotificationTypeSettings
    git-tfs-id: [http://developmentvlan:8585/tfs/vanrise.collection]$/;C40778

--- TOneV2/Code/TOneV2/TOne.WhS.AccountBalance.Business/AccountBalanceNotification/TOneAccountBalanceNotificationExtendedQuery.cs
+++ TOneV2/Code/TOneV2/TOne.WhS.AccountBalance.Business/AccountBalanceNotification/TOneAccountBalanceNotificationExtendedQuery.cs
@@ -3,10 +3,12 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Vanrise.AccountBalance.Business;
 
 namespace TOne.WhS.AccountBalance.Business
 {
-    public class TOneAccountBalanceNotificationExtendedQuery
+    public class TOneAccountBalanceNotificationExtendedQuery : AccountBalanceNotificationExtendedQuery
     {
+        public List<string> FinancialAccountIds { get; set; }
     }
 }
--- TOneV2/Code/TOneV2/TOne.WhS.AccountBalance.Business/AccountBalanceNotification/TOneAccountBalanceNotificationTypeSettings.cs
+++ TOneV2/Code/TOneV2/TOne.WhS.AccountBalance.Business/AccountBalanceNotification/TOneAccountBalanceNotificationTypeSettings.cs
@@ -3,10 +3,19 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Vanrise.AccountBalance.Business;
 
 namespace TOne.WhS.AccountBalance.Business
 {
-    public class TOneAccountBalanceNotificationTypeSettings
+    public class TOneAccountBalanceNotificationTypeSettings : AccountBalanceNotificationTypeExtendedSettings
     {
+        public override Guid ConfigId { get { return new Guid("EE2C731E-A5F7-481E-B350-7771C8A0F3BC"); } }
+
+        public override string NotificationQueryEditor { get { return "whs-accountbalance-notificationtype-searcheditor"; } }
+
+        public override bool IsVRNotificationMatched(IAccountBalanceNotificationTypeIsMatchedContext context)
+        {
+            throw new NotImplementedException();
+        }
     }
 }
