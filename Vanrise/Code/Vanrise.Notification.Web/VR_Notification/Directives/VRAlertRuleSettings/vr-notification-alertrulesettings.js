a50e2042e9d216a9e62fd8152bad00b5b7ac9ddf
Ali Atoui <VANRISE\ali.atoui>
2017-04-13 12:01:35 +0000

    Arranging vr-notification-alertrulesettings
    git-tfs-id: [http://developmentvlan:8585/tfs/vanrise.collection]$/;C40797

--- Vanrise/Code/Vanrise.Notification.Web/VR_Notification/Directives/VRAlertRuleSettings/vr-notification-alertrulesettings.js
+++ Vanrise/Code/Vanrise.Notification.Web/VR_Notification/Directives/VRAlertRuleSettings/vr-notification-alertrulesettings.js
@@ -1,169 +1,171 @@
 ﻿'use strict';
+
 app.directive('vrNotificationAlertrulesettings', ['UtilsService', 'VR_Notification_AlertRuleSettingsService', 'VR_Notification_VRBalanceAlertRuleAPIService',
-function (UtilsService, VR_Notification_AlertRuleSettingsService, VR_Notification_VRBalanceAlertRuleAPIService) {
+    function (UtilsService, VR_Notification_AlertRuleSettingsService, VR_Notification_VRBalanceAlertRuleAPIService) {
 
-    var directiveDefinitionObject = {
-        restrict: 'E',
-        scope: {
-            onReady: '=',
-        },
-        controller: function ($scope, $element, $attrs) {
-            var ctrl = this;
-            var ctor = new AlertRuleSettings(ctrl, $scope, $attrs);
-            ctor.initializeController();
+        var directiveDefinitionObject = {
+            restrict: 'E',
+            scope: {
+                onReady: '=',
+            },
+            controller: function ($scope, $element, $attrs) {
+                var ctrl = this;
+                var ctor = new AlertRuleSettings(ctrl, $scope, $attrs);
+                ctor.initializeController();
 
-        },
-        controllerAs: 'ctrl',
-        bindToController: true,
-        compile: function (element, attrs) {
+            },
+            controllerAs: 'ctrl',
+            bindToController: true,
+            compile: function (element, attrs) {
 
-        },
-        templateUrl: "/Client/Modules/VR_Notification/Directives/VRAlertRuleSettings/Templates/VRAlertRuleSettingsTemplate.html"
+            },
+            templateUrl: "/Client/Modules/VR_Notification/Directives/VRAlertRuleSettings/Templates/VRAlertRuleSettingsTemplate.html"
+        };
 
-    };
+        function AlertRuleSettings(ctrl, $scope, $attrs) {
+            this.initializeController = initializeController;
 
+            var actionExtensionType;
+            var thresholdExtensionType;
+            var balanceAlertThresholdConfigs = [];
+            var vrActionTarget;
+            var context;
 
-    function AlertRuleSettings(ctrl, $scope, $attrs) {
-        var actionExtensionType;
-        var thresholdExtensionType;
-        var balanceAlertThresholdConfigs = [];
-        var vrActionTarget;
-        var context;
-        function initializeController() {
+            function initializeController() {
+                ctrl.datasource = [];
 
-            ctrl.datasource = [];
+                ctrl.isValid = function () {
+                    if (ctrl.datasource.length > 0)
+                        return null;
 
-            ctrl.isValid = function () {
-                if (ctrl.datasource.length > 0)
-                    return null;
-
-                return "You Should Select at least one threshold action.";
-            };
-
-            ctrl.addThresholdAction = function () {
-                var onAlertRuleSettingsAdded = function (balanceAlertThreshold) {
-                    balanceAlertThreshold.ActionNames = getActionNames(balanceAlertThreshold.Actions);
-                    balanceAlertThreshold.RollbackActionNames = getActionNames(balanceAlertThreshold.RollbackActions);
-                    ctrl.datasource.push({ Entity: balanceAlertThreshold });
+                    return "You Should Select at least one threshold action.";
                 };
-                VR_Notification_AlertRuleSettingsService.addAlertRuleThreshold(onAlertRuleSettingsAdded, actionExtensionType, thresholdExtensionType, vrActionTarget, getContext());
-            };
 
-            ctrl.removeThresholdAction = function (dataItem) {
-                var index = ctrl.datasource.indexOf(dataItem);
-                ctrl.datasource.splice(index, 1);
-            };
+                ctrl.addThresholdAction = function () {
+                    var onAlertRuleSettingsAdded = function (balanceAlertThreshold) {
+                        balanceAlertThreshold.ActionNames = getActionNames(balanceAlertThreshold.Actions);
+                        balanceAlertThreshold.RollbackActionNames = getActionNames(balanceAlertThreshold.RollbackActions);
+                        ctrl.datasource.push({ Entity: balanceAlertThreshold });
+                    };
+                    VR_Notification_AlertRuleSettingsService.addAlertRuleThreshold(onAlertRuleSettingsAdded, actionExtensionType, thresholdExtensionType, vrActionTarget, getContext());
+                };
 
-            defineAPI();
-            defineMenuActions();
-        }
+                ctrl.removeThresholdAction = function (dataItem) {
+                    var index = ctrl.datasource.indexOf(dataItem);
+                    ctrl.datasource.splice(index, 1);
+                };
 
-        function defineAPI() {
-            var api = {};
+                defineAPI();
 
-            api.getData = function () {
-                var data = [];
-                if (ctrl.datasource.length > 0) {
-                    for (var i = 0; i < ctrl.datasource.length; i++) {
-                        data.push(ctrl.datasource[i].Entity);
+                defineMenuActions();
+            }
+
+            function defineAPI() {
+                var api = {};
+
+                api.getData = function () {
+                    var data = [];
+                    if (ctrl.datasource.length > 0) {
+                        for (var i = 0; i < ctrl.datasource.length; i++) {
+                            data.push(ctrl.datasource[i].Entity);
+                        }
                     }
-                }
-                return data;
-            };
+                    return data;
+                };
 
-            api.load = function (payload) {
-                var promises = [];
-                var vrAlertRuleTypeId;
-                if (payload != undefined) {
-                    vrAlertRuleTypeId = payload.vrAlertRuleTypeId;
-                    if (payload.alertTypeSettings != undefined) {
-                        actionExtensionType = payload.alertTypeSettings.VRActionExtensionType;
-                        thresholdExtensionType = payload.alertTypeSettings.ThresholdExtensionType;
-                        context = payload.context;
-                        if (payload.settings != undefined) {
-                            if (payload.settings.ThresholdActions != undefined) {
-                                for (var i = 0; i < payload.settings.ThresholdActions.length; i++) {
-                                    var thresholdAction = payload.settings.ThresholdActions[i];
-                                    if (thresholdAction.Actions != undefined && thresholdAction.Actions.length > 0) {
-                                        thresholdAction.ActionNames = getActionNames(thresholdAction.Actions);
+                api.load = function (payload) {
+                    var promises = [];
+                    var vrAlertRuleTypeId;
+                    if (payload != undefined) {
+                        vrAlertRuleTypeId = payload.vrAlertRuleTypeId;
+                        if (payload.alertTypeSettings != undefined) {
+                            actionExtensionType = payload.alertTypeSettings.VRActionExtensionType;
+                            thresholdExtensionType = payload.alertTypeSettings.ThresholdExtensionType;
+                            context = payload.context;
+                            if (payload.settings != undefined) {
+                                if (payload.settings.ThresholdActions != undefined) {
+                                    for (var i = 0; i < payload.settings.ThresholdActions.length; i++) {
+                                        var thresholdAction = payload.settings.ThresholdActions[i];
+                                        if (thresholdAction.Actions != undefined && thresholdAction.Actions.length > 0) {
+                                            thresholdAction.ActionNames = getActionNames(thresholdAction.Actions);
+                                        }
+                                        if (thresholdAction.RollbackActions != undefined && thresholdAction.RollbackActions.length > 0) {
+                                            thresholdAction.RollbackActionNames = getActionNames(thresholdAction.RollbackActions);
+                                        }
+                                        ctrl.datasource.push({ Entity: thresholdAction });
                                     }
-                                    if (thresholdAction.RollbackActions != undefined && thresholdAction.RollbackActions.length > 0) {
-                                        thresholdAction.RollbackActionNames = getActionNames(thresholdAction.RollbackActions);
-                                    }
-                                    ctrl.datasource.push({ Entity: thresholdAction });
                                 }
                             }
                         }
                     }
+
+                    promises.push(loadActionTargetType());
+
+                    function loadActionTargetType() {
+                        return VR_Notification_VRBalanceAlertRuleAPIService.GetVRBalanceActionTargetTypeByRuleTypeId(vrAlertRuleTypeId).then(function (response) {
+                            vrActionTarget = response;
+                        });
+                    }
+
+                    return UtilsService.waitMultiplePromises(promises);
+                };
+
+                if (ctrl.onReady != null)
+                    ctrl.onReady(api);
+            }
+
+            function defineMenuActions() {
+
+                $scope.gridMenuActions = [{
+                    name: "Edit",
+                    clicked: editThresholdAction,
+                }];
+            }
+
+            function editThresholdAction(dataItem) {
+                var onThresholdActionUpdated = function (thresholdActionObj) {
+                    thresholdActionObj.ActionNames = getActionNames(thresholdActionObj.Actions);
+                    thresholdActionObj.RollbackActionNames = getActionNames(thresholdActionObj.RollbackActions);
+                    ctrl.datasource[ctrl.datasource.indexOf(dataItem)] = { Entity: thresholdActionObj };
+                };
+                VR_Notification_AlertRuleSettingsService.editAlertRuleThreshold(dataItem.Entity, onThresholdActionUpdated, actionExtensionType, thresholdExtensionType, vrActionTarget, getContext());
+            }
+
+            function getVRBalanceAlertThresholdConfigs() {
+                return VR_Notification_VRBalanceAlertRuleAPIService.GetVRBalanceAlertThresholdConfigs(thresholdExtensionType).then(function (response) {
+                    if (response != null) {
+                        for (var i = 0; i < response.length; i++) {
+                            balanceAlertThresholdConfigs.push(response[i]);
+                        }
+                    }
+                });
+            }
+
+            function getActionNames(actions) {
+                if (actions != undefined) {
+                    var actionNames;
+
+                    for (var i = 0; i < actions.length; i++) {
+                        var action = actions[i];
+                        if (actionNames == undefined)
+                            actionNames = "";
+                        else
+                            actionNames += ", ";
+                        actionNames += action.ActionName;
+                    }
+
+                    return actionNames;
                 }
+            }
 
-                promises.push(loadActionTargetType());
-
-                function loadActionTargetType() {
-                    return VR_Notification_VRBalanceAlertRuleAPIService.GetVRBalanceActionTargetTypeByRuleTypeId(vrAlertRuleTypeId).then(function (response) {
-                        vrActionTarget = response;
-                    });
-                }
-
-                return UtilsService.waitMultiplePromises(promises);
-            };
-
-            if (ctrl.onReady != null)
-                ctrl.onReady(api);
-        }
-
-        function getActionNames(actions) {
-            if (actions != undefined) {
-                var actionNames;
-                for (var i = 0; i < actions.length; i++) {
-                    var action = actions[i];
-                    if (actionNames == undefined)
-                        actionNames = "";
-                    else
-                        actionNames += ", ";
-                    actionNames += action.ActionName;
-
-                }
-                return actionNames;
+            function getContext() {
+                var currentContext = context;
+                if (currentContext == undefined)
+                    currentContext = {};
+                return currentContext;
             }
 
         }
 
-        function defineMenuActions() {
-
-            $scope.gridMenuActions = [{
-                name: "Edit",
-                clicked: editThresholdAction,
-            }];
-        }
-
-        function editThresholdAction(dataItem) {
-            var onThresholdActionUpdated = function (thresholdActionObj) {
-                thresholdActionObj.ActionNames = getActionNames(thresholdActionObj.Actions);
-                thresholdActionObj.RollbackActionNames = getActionNames(thresholdActionObj.RollbackActions);
-                ctrl.datasource[ctrl.datasource.indexOf(dataItem)] = { Entity: thresholdActionObj };
-            };
-            VR_Notification_AlertRuleSettingsService.editAlertRuleThreshold(dataItem.Entity, onThresholdActionUpdated, actionExtensionType, thresholdExtensionType, vrActionTarget, getContext());
-        }
-
-        function getVRBalanceAlertThresholdConfigs() {
-            return VR_Notification_VRBalanceAlertRuleAPIService.GetVRBalanceAlertThresholdConfigs(thresholdExtensionType).then(function (response) {
-                if (response != null) {
-                    for (var i = 0; i < response.length; i++) {
-                        balanceAlertThresholdConfigs.push(response[i]);
-                    }
-                }
-            });
-        }
-
-        function getContext() {
-            var currentContext = context;
-            if (currentContext == undefined)
-                currentContext = {};
-            return currentContext;
-        }
-        this.initializeController = initializeController;
-    }
-
-    return directiveDefinitionObject;
-}]);
+        return directiveDefinitionObject;
+    }]);
