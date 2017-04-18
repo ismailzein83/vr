080cfa92bd722690eb700c6c1520606ebca7aecd
Ali Atoui <VANRISE\ali.atoui>
2017-04-18 06:51:11 +0000

    removing console from vrAccountbalanceAccountSelector
    git-tfs-id: [http://developmentvlan:8585/tfs/vanrise.collection]$/;C40822

--- Vanrise/Code/Vanrise.AccountBalance.Web/VR_AccountBalance/Directives/BalanceAccountType/vr-accountbalance-account-selector.js
+++ Vanrise/Code/Vanrise.AccountBalance.Web/VR_AccountBalance/Directives/BalanceAccountType/vr-accountbalance-account-selector.js
@@ -44,9 +44,6 @@ app.directive('vrAccountbalanceAccountSelector', ['VR_AccountBalance_AccountType
             var api = {};
 
             api.load = function (payload) {
-
-                console.log(payload);
-
                 $scope.scopeModel.editor = undefined;
                 directiveReadyDeferred = UtilsService.createPromiseDeferred();
 
