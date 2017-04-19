d0890a979cc948f36175a2675ef4e4b05a4b10c5
Wissam Ajamy <VANRISE\wissam.ajamy>
2017-04-19 08:56:33 +0000

    sync strcuture with TOneConfiguration
    git-tfs-id: [http://developmentvlan:8585/tfs/vanrise.collection]$/;C40850

---
+++ TOneV2/Code/TOneV2/SQL.TOneConfiguration/common/Tables/GenericLKUP.sql
@@ -0,0 +1,15 @@
+﻿CREATE TABLE [common].[GenericLKUP] (
+    [ID]                         UNIQUEIDENTIFIER NOT NULL,
+    [Name]                       NVARCHAR (255)   NOT NULL,
+    [BusinessEntityDefinitionID] UNIQUEIDENTIFIER NOT NULL,
+    [Settings]                   NVARCHAR (MAX)   NULL,
+    [CreatedTime]                DATETIME         CONSTRAINT [DF_GenericLKUP_CreatedTime] DEFAULT (getdate()) NULL,
+    [timestamp]                  ROWVERSION       NULL,
+    CONSTRAINT [PK_GenericLKUP] PRIMARY KEY CLUSTERED ([ID] ASC)
+);
+
+
+GO
+CREATE NONCLUSTERED INDEX [IX_GenericLKUP_BEIdAndName]
+    ON [common].[GenericLKUP]([BusinessEntityDefinitionID] ASC, [Name] ASC);
+
