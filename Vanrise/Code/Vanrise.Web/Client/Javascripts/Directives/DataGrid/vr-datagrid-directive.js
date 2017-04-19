fd34f257fa5e7d5df780df76ba18e91c54633659
Ali Ballouk <VANRISE\ali.ballouk>
2017-04-19 11:09:35 +0000

    adjust setMaxHeight margin
    git-tfs-id: [http://developmentvlan:8585/tfs/vanrise.collection]$/;C40860

--- Vanrise/Code/Vanrise.Web/Client/Javascripts/Directives/DataGrid/vr-datagrid-directive.js
+++ Vanrise/Code/Vanrise.Web/Client/Javascripts/Directives/DataGrid/vr-datagrid-directive.js
@@ -986,7 +986,7 @@ app.directive('vrDatagrid', ['UtilsService', 'SecurityService', 'DataRetrievalRe
                         if (isInModal() == true)
                             h += sh * 0.5;
                         else
-                            h += sh - 367;
+                            h += sh - 332;
 
                         h = h < 30 ? 30 : h;
                         setMaxHeight(h + "px");
