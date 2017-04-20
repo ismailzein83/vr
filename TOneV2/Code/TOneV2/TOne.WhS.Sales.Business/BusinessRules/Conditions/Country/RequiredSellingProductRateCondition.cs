779c2ac0f5ab295a2f009b982e5351ab3a58c112
mohamad.elhariri <mohamad.elhariri@vanrise.tfs.local>
2017-04-20 08:21:52 +0000

    Added RequiredSellingProductRateCondition
    git-tfs-id: [http://developmentvlan:8585/tfs/vanrise.collection]$/;C40880

---
+++ TOneV2/Code/TOneV2/TOne.WhS.Sales.Business/BusinessRules/Conditions/Country/RequiredSellingProductRateCondition.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TOne.WhS.BusinessEntity.Entities;
+using TOne.WhS.Sales.Entities;
+using Vanrise.Common;
+
+namespace TOne.WhS.Sales.Business.BusinessRules
+{
+    public class RequiredSellingProductRateCondition : Vanrise.BusinessProcess.Entities.BusinessRuleCondition
+    {
+        public override bool ShouldValidate(Vanrise.BusinessProcess.Entities.IRuleTarget target)
+        {
+            return target is CustomerCountryToAdd;
+        }
+
+        public override bool Validate(Vanrise.BusinessProcess.Entities.IBusinessRuleConditionValidateContext context)
+        {
+            return true;
+        }
+
+        public override string GetMessage(Vanrise.BusinessProcess.Entities.IRuleTarget target)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
