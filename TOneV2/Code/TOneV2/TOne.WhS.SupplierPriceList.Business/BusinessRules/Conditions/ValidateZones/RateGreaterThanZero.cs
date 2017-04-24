58dd9152c2cf3ed0f3a51dc45f783556990d14fb
Zeinab Mokdad <VANRISE\zeinab.mokdad>
2017-04-24 14:12:35 +0000

    Adding new business Rule for rate >0
    git-tfs-id: [http://developmentvlan:8585/tfs/vanrise.collection]$/;C40940

---
+++ TOneV2/Code/TOneV2/TOne.WhS.SupplierPriceList.Business/BusinessRules/Conditions/ValidateZones/RateGreaterThanZero.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TOne.WhS.SupplierPriceList.Entities.SPL;
+using Vanrise.BusinessProcess.Entities;
+
+namespace TOne.WhS.SupplierPriceList.Business
+{
+    class RateGreaterThanZero : BusinessRuleCondition
+    {
+        public override string GetMessage(IRuleTarget target)
+        {
+            throw new NotImplementedException();
+        }
+
+        public override bool ShouldValidate(IRuleTarget target)
+        {
+            return target is ImportedRate;
+        }
+
+        public override bool Validate(IBusinessRuleConditionValidateContext context)
+        {
+            ImportedRate importedRate = context.Target as ImportedRate;
+            if (importedRate != null && importedRate.Rate <= 0)
+            {
+                context.Message = string.Format("Zone {0} Has Rate {1} < 0 ", importedRate.ZoneName, importedRate.Rate);
+                return false;
+            }
+            return true;
+        }
+    }
+}
