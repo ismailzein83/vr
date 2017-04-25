acdd7c172b5eb17386c06da164539683a2069343
rabih.fashwal:2 <rabih.fashwal:2@vanrise.tfs.local>
2017-04-25 14:33:52 +0000

    Test MySQL
    git-tfs-id: [http://developmentvlan:8585/tfs/vanrise.collection]$/;C40959

--- TOneV2/Code/TOneV2/TestRuntime/DSMappers.cs
+++ TOneV2/Code/TOneV2/TestRuntime/DSMappers.cs
@@ -91,6 +91,88 @@ namespace TestRuntime
             return result;
         }
 
+        public static Vanrise.Integration.Entities.MappingOutput ImportingCDR_MySQL(object localData)
+        {
+            //Retail.Runtime.Mappers.RingoMapper mapper = new Retail.Runtime.Mappers.RingoMapper();
+            //return DSMappers.ImportingCDR_MySQL(data);
+
+            LogVerbose("Started");
+
+            var cdrs = new List<dynamic>();
+            var dataRecordTypeManager = new Vanrise.GenericData.Business.DataRecordTypeManager();
+            Type cdrRuntimeType = dataRecordTypeManager.GetDataRecordRuntimeType("CDR");
+
+            int maximumBatchSize = 15000;
+            var dataRecordVanriseType = new Vanrise.GenericData.Entities.DataRecordVanriseType("CDR");
+
+
+            var importedData = ((Vanrise.Integration.Entities.DBReaderImportedData)(data));
+
+            IDataReader reader = importedData.Reader;
+
+            int rowCount = 0;
+            while (reader.Read())
+            {
+                dynamic cdr = Activator.CreateInstance(cdrRuntimeType) as dynamic;
+                cdr.SwitchId = 83;
+                cdr.IDonSwitch = Utils.GetReaderValue<long>(reader, "CDRId");
+                //cdr.Tag = reader["Tag"] as string;
+                cdr.AttemptDateTime = (DateTime)reader["AttemptDateTime"];
+                //cdr.AlertDateTime = Utils.GetReaderValue<DateTime>(reader, "AlertDateTime");
+                cdr.ConnectDateTime = Utils.GetReaderValue<DateTime>(reader, "ConnectDateTime");
+                cdr.DisconnectDateTime = Utils.GetReaderValue<DateTime>(reader, "DisconnectDateTime");
+                //cdr.DurationInSeconds = Utils.GetReaderValue<Decimal>(reader, "DurationInSeconds");
+                cdr.InTrunk = reader["INTRUNK"] as string;
+                // cdr.InCircuit = reader["IN_CIRCUIT"] != DBNull.Value ? Convert.ToInt64(reader["IN_CIRCUIT"]) : default(Int64);
+                //cdr.InCarrier = reader["IN_CARRIER"] as string;
+                cdr.InIP = reader["INIP"] as string;
+                cdr.OutTrunk = reader["OUTTRUNK"] as string;
+                //cdr.OutCircuit = reader["OUT_CIRCUIT"] != DBNull.Value ? Convert.ToInt64(reader["OUT_CIRCUIT"]) : default(Int64);
+                //cdr.OutCarrier = reader["OUT_CARRIER"] as string;
+                cdr.OutIP = reader["OUTIP"] as string;
+
+                cdr.CGPN = reader["CGPN"] as string;
+                cdr.CDPN = reader["CDPN"] as string;
+                // cdr.CauseFromReleaseCode = reader["CAUSE_FROM_RELEASE_CODE"] as string;
+                // cdr.CauseFrom = reader["CAUSE_FROM"] as string;
+                // cdr.CauseToReleaseCode = reader["CAUSE_TO_RELEASE_CODE"] as string;
+                //cdr.CauseTo = reader["CAUSE_TO"] as string;
+                // cdr.IsRerouted = reader["IsRerouted"] != DBNull.Value ? ((reader["IsRerouted"] as string) == "Y") : false;
+                // cdr.CDPNOut = reader["CDPNOut"] as string;
+                // cdr.CDPNIn = reader["CDPNIn"] as string;
+                // cdr.SIP = reader["SIP"] as string;
+
+                cdrs.Add(cdr);
+                importedData.LastImportedId = reader["CDRID"];
+
+                rowCount++;
+                if (rowCount == maximumBatchSize)
+                    break;
+            }
+
+            long startingId;
+            Vanrise.Common.Business.IDManager.Instance.ReserveIDRange(dataRecordVanriseType, rowCount, out startingId);
+            long currentCDRId = startingId;
+
+            foreach (var cdr in cdrs)
+            {
+                cdr.Id = currentCDRId;
+                currentCDRId++;
+            }
+
+            if (cdrs.Count > 0)
+            {
+                var batch = Vanrise.GenericData.QueueActivators.DataRecordBatch.CreateBatchFromRecords(cdrs, "#RECORDSCOUNT# of Raw CDRs");
+                mappedBatches.Add("CDR Storage Stage", batch);
+            }
+            else
+                importedData.IsEmpty = true;
+
+            Vanrise.Integration.Entities.MappingOutput result = new Vanrise.Integration.Entities.MappingOutput();
+            result.Result = Vanrise.Integration.Entities.MappingResult.Valid;
+            LogVerbose("Finished");
+            return result;
+        }
 
         static int dataSourceId = 24;
         static DBReaderImportedData data = new DBReaderImportedData();
