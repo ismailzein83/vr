aab9d23a89d982961a7a94d97ed00cb26c8389d9
Zeinab Mokdad <VANRISE\zeinab.mokdad>
2017-04-28 08:09:35 +0000

    adding rule to ValidateZones t make sure in case of wrong template it will show error
    git-tfs-id: [http://developmentvlan:8585/tfs/vanrise.collection]$/;C41039

---
+++ TOneV2/Code/TOneV2/TOne.WhS.CodePreparation.Business/BusinessRules/Conditions/ValidateCodesZones/NumericCodeCondition.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using TOne.WhS.CodePreparation.Entities;
+using Vanrise.BusinessProcess.Entities;
+
+namespace TOne.WhS.CodePreparation.Business
+{
+    public class NumericCodeCondition : BusinessRuleCondition
+    {
+        public override bool ShouldValidate(IRuleTarget target)
+        {
+            return (target is ImportedCode);
+        }
+
+        public override bool Validate(IBusinessRuleConditionValidateContext context)
+        {
+            ImportedCode importedCode = context.Target as ImportedCode;
+
+            if (!Vanrise.Common.Utilities.IsNumeric(importedCode.Code, 0))
+            {
+                context.Message = string.Format("Code {0} is not numeric", importedCode.Code);
+                return false;
+            }
+            return true;
+        }
+
+        public override string GetMessage(IRuleTarget target)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
