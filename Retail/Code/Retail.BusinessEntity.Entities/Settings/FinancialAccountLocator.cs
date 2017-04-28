8168157497c7d512c2bf749b39710406f656fb21
Ismail Zein <VANRISE\ismail.zein>
2017-04-28 09:37:34 +0000

    added FinancialAccountLocator
    git-tfs-id: [http://developmentvlan:8585/tfs/vanrise.collection]$/;C41043

---
+++ Retail/Code/Retail.BusinessEntity.Entities/Settings/FinancialAccountLocator.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Retail.BusinessEntity.Entities
+{
+    public abstract class FinancialAccountLocator
+    {
+        public abstract Guid ConfigId { get; }
+
+        public abstract bool TryGetFinancialAccountId(IFinancialAccountLocatorContext context);
+    }
+
+    public interface IFinancialAccountLocatorContext
+    {
+        Guid AccountDefinitionId { get; }
+
+        long AccountId { get; }
+
+        DateTime EffectiveOn { get; }
+
+        long FinancialAccountId { set; }
+    }
+}
