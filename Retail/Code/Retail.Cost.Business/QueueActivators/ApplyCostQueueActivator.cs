4e0e1280e8e849ef1590f33eb91e9c11a8fec2f5
Ali Atoui <VANRISE\ali.atoui>
2017-04-28 13:22:22 +0000

    Editing Retail.Cost
    git-tfs-id: [http://developmentvlan:8585/tfs/vanrise.collection]$/;C41057

---
+++ Retail/Code/Retail.Cost.Business/QueueActivators/ApplyCostQueueActivator.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Vanrise.GenericData.QueueActivators;
+
+namespace Retail.Cost.Business
+{
+    public class ApplyCostQueueActivator : Vanrise.Queueing.Entities.QueueActivator
+    {
+
+
+        #region QueueActivator
+
+        public override void OnDisposed()
+        {
+        }
+
+        public override void ProcessItem(Vanrise.Queueing.Entities.PersistentQueueItem item, Vanrise.Queueing.Entities.ItemsToEnqueue outputItems)
+        {
+        }
+
+        public override void ProcessItem(Vanrise.Queueing.Entities.IQueueActivatorExecutionContext context)
+        {
+            DataRecordBatch dataRecordBatch = context.ItemToProcess as DataRecordBatch;
+            var queueItemType = context.CurrentStage.QueueItemType as DataRecordBatchQueueItemType;
+            if (queueItemType == null)
+                throw new Exception("current stage QueueItemType is not of type DataRecordBatchQueueItemType");
+            var recordTypeId = queueItemType.DataRecordTypeId;
+            var batchRecords = dataRecordBatch.GetBatchRecords(recordTypeId);
+        }
+
+        #endregion
+    }
+}
