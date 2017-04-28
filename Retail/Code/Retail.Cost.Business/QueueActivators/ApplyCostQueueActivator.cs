03e61e86051e746cd8d82f15fc41c122356933d9
Ali Atoui <VANRISE\ali.atoui>
2017-04-28 13:24:45 +0000

    Adding Retail.Cost.BP and Retail.Cost.BP.Activities
    git-tfs-id: [http://developmentvlan:8585/tfs/vanrise.collection]$/;C41058

--- Retail/Code/Retail.Cost.Business/QueueActivators/ApplyCostQueueActivator.cs
+++ Retail/Code/Retail.Cost.Business/QueueActivators/ApplyCostQueueActivator.cs
@@ -7,30 +7,28 @@ using Vanrise.GenericData.QueueActivators;
 
 namespace Retail.Cost.Business
 {
-    public class ApplyCostQueueActivator : Vanrise.Queueing.Entities.QueueActivator
+    public class ApplyCostQueueActivator //: Vanrise.Queueing.Entities.QueueActivator
     {
+        //#region QueueActivator
 
+        //public override void OnDisposed()
+        //{
+        //}
 
-        #region QueueActivator
+        //public override void ProcessItem(Vanrise.Queueing.Entities.PersistentQueueItem item, Vanrise.Queueing.Entities.ItemsToEnqueue outputItems)
+        //{
+        //}
 
-        public override void OnDisposed()
-        {
-        }
+        //public override void ProcessItem(Vanrise.Queueing.Entities.IQueueActivatorExecutionContext context)
+        //{
+        //    DataRecordBatch dataRecordBatch = context.ItemToProcess as DataRecordBatch;
+        //    var queueItemType = context.CurrentStage.QueueItemType as DataRecordBatchQueueItemType;
+        //    if (queueItemType == null)
+        //        throw new Exception("current stage QueueItemType is not of type DataRecordBatchQueueItemType");
+        //    var recordTypeId = queueItemType.DataRecordTypeId;
+        //    var batchRecords = dataRecordBatch.GetBatchRecords(recordTypeId);
+        //}
 
-        public override void ProcessItem(Vanrise.Queueing.Entities.PersistentQueueItem item, Vanrise.Queueing.Entities.ItemsToEnqueue outputItems)
-        {
-        }
-
-        public override void ProcessItem(Vanrise.Queueing.Entities.IQueueActivatorExecutionContext context)
-        {
-            DataRecordBatch dataRecordBatch = context.ItemToProcess as DataRecordBatch;
-            var queueItemType = context.CurrentStage.QueueItemType as DataRecordBatchQueueItemType;
-            if (queueItemType == null)
-                throw new Exception("current stage QueueItemType is not of type DataRecordBatchQueueItemType");
-            var recordTypeId = queueItemType.DataRecordTypeId;
-            var batchRecords = dataRecordBatch.GetBatchRecords(recordTypeId);
-        }
-
-        #endregion
+        //#endregion
     }
 }
