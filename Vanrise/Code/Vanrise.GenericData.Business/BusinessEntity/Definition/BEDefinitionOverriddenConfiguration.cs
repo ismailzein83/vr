d447e14541f0d8497f83a7ab87d6fe9a27090919
Ismail Zein <VANRISE\ismail.zein>
2017-05-03 09:22:04 +0000

    added GenericRuleDefinitionOverriddenConfiguration
    git-tfs-id: [http://developmentvlan:8585/tfs/vanrise.collection]$/;C41090

---
+++ Vanrise/Code/Vanrise.GenericData.Business/BusinessEntity/Definition/BEDefinitionOverriddenConfiguration.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Vanrise.Entities;
+using Vanrise.GenericData.Entities;
+
+namespace Vanrise.GenericData.Business
+{
+    public class BEDefinitionOverriddenConfiguration : OverriddenConfigurationExtendedSettings
+    {
+        public override Guid ConfigId
+        {
+            get { return new Guid("8DC29F02-7197-4C60-8E21-CBDE0C2AE87B"); }
+        }
+
+        public Guid BusinessEntityDefinitionId { get; set; }
+
+        public string OverriddenTitle { get; set; }
+
+        public BusinessEntityDefinitionSettings OverriddenSettings { get; set; }
+    }
+}
---
+++ Vanrise/Code/Vanrise.GenericData.Business/GenericRules/GenericRuleDefinitionOverriddenConfiguration.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Vanrise.Entities;
+using Vanrise.GenericData.Entities;
+
+namespace Vanrise.GenericData.Business
+{
+    public class GenericRuleDefinitionOverriddenConfiguration : OverriddenConfigurationExtendedSettings
+    {
+        public override Guid ConfigId
+        {
+            get { return new Guid("B2903DAC-1980-4C21-82FB-8DCC72E5068D"); }
+        }
+
+        public Guid RuleDefinitionId { get; set; }
+
+        public string OverriddenTitle { get; set; }
+
+        public GenericRuleDefinitionCriteria OverriddenCriteriaDefinition { get; set; }
+
+        public VRObjectVariableCollection OverriddenObjects { get; set; }
+
+        public GenericRuleDefinitionSettings OverriddenSettingsDefinition { get; set; }
+
+        public GenericRuleDefinitionSecurity OverriddenSecurity { get; set; }
+    }
+}
