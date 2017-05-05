fc86b88fb0477482c37611371edff014bef40c25
Zeinab Mokdad <VANRISE\zeinab.mokdad>
2017-05-05 11:45:57 +0000

    removing time from dateTime
    git-tfs-id: [http://developmentvlan:8585/tfs/vanrise.collection]$/;C41163

--- TOneV2/Code/TOneV2/TOne.WhS.SupplierPriceList.Entities.MainExtensions/SupplierPriceListSettings/BasicSupplierPriceListSettings.cs
+++ TOneV2/Code/TOneV2/TOne.WhS.SupplierPriceList.Entities.MainExtensions/SupplierPriceListSettings/BasicSupplierPriceListSettings.cs
@@ -93,7 +93,7 @@ namespace TOne.WhS.SupplierPriceList.MainExtensions.SupplierPriceListSettings
                     if (obj.Fields.TryGetValue("EffectiveDate", out codeEffectiveDateField))
                     {
                         if (codeEffectiveDateField.FieldValue != null && !String.IsNullOrWhiteSpace(codeEffectiveDateField.FieldValue.ToString()))
-                            result = Convert.ToDateTime(codeEffectiveDateField.FieldValue);
+                            result = Convert.ToDateTime(codeEffectiveDateField.FieldValue).Date;
                     };
                     if (obj.Fields.TryGetValue("Zone", out zoneField))
                     {
@@ -199,7 +199,7 @@ namespace TOne.WhS.SupplierPriceList.MainExtensions.SupplierPriceListSettings
                     if (obj.Fields.TryGetValue("EffectiveDate", out rateEffectiveDateField))
                     {
                         if (rateEffectiveDateField.FieldValue != null && !String.IsNullOrWhiteSpace(rateEffectiveDateField.FieldValue.ToString()))
-                            result = Convert.ToDateTime(rateEffectiveDateField.FieldValue);
+                            result = Convert.ToDateTime(rateEffectiveDateField.FieldValue).Date;
                     };
                     if (obj.Fields.TryGetValue("Zone", out zoneField))
                     {
@@ -242,7 +242,7 @@ namespace TOne.WhS.SupplierPriceList.MainExtensions.SupplierPriceListSettings
                             if (obj.Fields.TryGetValue("EffectiveDate", out rateEffectiveDateField))
                             {
                                 if (rateEffectiveDateField.FieldValue != null && !String.IsNullOrWhiteSpace(rateEffectiveDateField.FieldValue.ToString()))
-                                    result = Convert.ToDateTime(rateEffectiveDateField.FieldValue);
+                                    result = Convert.ToDateTime(rateEffectiveDateField.FieldValue).Date;
                             };
                             if (obj.Fields.TryGetValue("Zone", out zoneField))
                             {
@@ -301,7 +301,7 @@ namespace TOne.WhS.SupplierPriceList.MainExtensions.SupplierPriceListSettings
                             if (obj.Fields.TryGetValue("EffectiveDate", out serviceEffectiveDateField))
                             {
                                 if (serviceEffectiveDateField.FieldValue != null && !String.IsNullOrWhiteSpace(serviceEffectiveDateField.FieldValue.ToString()))
-                                    result = Convert.ToDateTime(serviceEffectiveDateField.FieldValue);
+                                    result = Convert.ToDateTime(serviceEffectiveDateField.FieldValue).Date;
                             };
                             if (obj.Fields.TryGetValue("Zone", out zoneField))
                             {
