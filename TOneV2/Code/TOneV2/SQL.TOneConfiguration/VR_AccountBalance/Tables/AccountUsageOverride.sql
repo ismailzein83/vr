290ded77eb26790b0d65d4820c7e3c06be871aa9
Wissam Ajamy <VANRISE\wissam.ajamy>
2017-05-05 11:01:41 +0000

    update structure from TOneconfiguration DB
    git-tfs-id: [http://developmentvlan:8585/tfs/vanrise.collection]$/;C41157

---
+++ TOneV2/Code/TOneV2/SQL.TOneConfiguration/VR_AccountBalance/Tables/AccountUsageOverride.sql
@@ -0,0 +1,13 @@
+﻿CREATE TABLE [VR_AccountBalance].[AccountUsageOverride] (
+    [ID]                        BIGINT           IDENTITY (1, 1) NOT NULL,
+    [AccountTypeID]             UNIQUEIDENTIFIER NOT NULL,
+    [TransactionTypeID]         UNIQUEIDENTIFIER NOT NULL,
+    [AccountID]                 VARCHAR (50)     NOT NULL,
+    [PeriodStart]               DATETIME         NOT NULL,
+    [PeriodEnd]                 DATETIME         NOT NULL,
+    [OverriddenByTransactionID] BIGINT           NOT NULL,
+    [CreatedTime]               DATETIME         CONSTRAINT [DF_AccountUsageOverride_CreatedTime] DEFAULT (getdate()) NULL,
+    [timestamp]                 ROWVERSION       NULL,
+    CONSTRAINT [PK_AccountUsageOverride] PRIMARY KEY CLUSTERED ([ID] ASC)
+);
+
