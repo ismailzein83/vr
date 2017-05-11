6d7884e2bc05933469a0fd6386bc2d3f40286ce1
Ali Atoui <VANRISE\ali.atoui>
2017-05-11 09:09:34 +0000

    Commenting AccountServiceStatusGenericField
    git-tfs-id: [http://developmentvlan:8585/tfs/vanrise.collection]$/;C41223

--- Retail/Code/Retail.BusinessEntity.Business/AccountServiceGenericFields/AccountServiceStatusGenericField.cs
+++ Retail/Code/Retail.BusinessEntity.Business/AccountServiceGenericFields/AccountServiceStatusGenericField.cs
@@ -7,35 +7,35 @@ using System.Threading.Tasks;
 
 namespace Retail.BusinessEntity.Business
 {
-    public class AccountServiceStatusGenericField : AccountServiceGenericField
-    {
-        public override string Name
-        {
-            get
-            {
-                return "Status";
-            }
-        }
+    //public class AccountServiceStatusGenericField : AccountServiceGenericField
+    //{
+    //    public override string Name
+    //    {
+    //        get
+    //        {
+    //            return "Status";
+    //        }
+    //    }
 
-        public override string Title
-        {
-            get
-            {
-                return "Status";
-            }
-        }
+    //    public override string Title
+    //    {
+    //        get
+    //        {
+    //            return "Status";
+    //        }
+    //    }
 
-        public override Vanrise.GenericData.Entities.DataRecordFieldType FieldType
-        {
-            get
-            {
-                return new Vanrise.GenericData.MainExtensions.DataRecordFields.FieldBusinessEntityType { BusinessEntityDefinitionId = StatusDefinition.BUSINESSENTITY_DEFINITION_ID };
-            }
-        }
+    //    public override Vanrise.GenericData.Entities.DataRecordFieldType FieldType
+    //    {
+    //        get
+    //        {
+    //            return new Vanrise.GenericData.MainExtensions.DataRecordFields.FieldBusinessEntityType { BusinessEntityDefinitionId = StatusDefinition.BUSINESSENTITY_DEFINITION_ID };
+    //        }
+    //    }
 
-        public override dynamic GetValue(IAccountServiceGenericFieldContext context)
-        {
-            return context.AccountService.StatusId;
-        }
-    }
+    //    public override dynamic GetValue(IAccountServiceGenericFieldContext context)
+    //    {
+    //        return context.AccountService.StatusId;
+    //    }
+    //}
 }
