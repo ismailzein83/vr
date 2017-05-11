fb3a3251808640b9c075d78f2dbe5dbccb3ac768
Ali Atoui <VANRISE\ali.atoui>
2017-05-11 08:52:32 +0000

    Adding BalanceFinancialAccount
    git-tfs-id: [http://developmentvlan:8585/tfs/vanrise.collection]$/;C41221

---
+++ Retail/Code/Retail.BusinessEntity.Entities/FinancialAccount/AccountFinancialInfo.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Retail.BusinessEntity.Entities
+{
+    public class BalanceFinancialAccount
+    {
+        public long FinancialAccountId { get; set; }
+
+        public string BalanceAccountId { get; set; }
+
+        public Guid BalanceAccountTypeId { get; set; }
+    }
+}
