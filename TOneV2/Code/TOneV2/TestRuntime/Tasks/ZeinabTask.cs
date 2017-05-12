4cb31308283abe3829a056c91991ec169b842bb8
Zeinab Mokdad <VANRISE\zeinab.mokdad>
2017-05-12 10:50:03 +0000

    excluding unused tuntime taskes
    git-tfs-id: [http://developmentvlan:8585/tfs/vanrise.collection]$/;C41258

--- TOneV2/Code/TOneV2/TestRuntime/Tasks/ZeinabTask.cs
+++ TOneV2/Code/TOneV2/TestRuntime/Tasks/ZeinabTask.cs
@@ -21,31 +21,9 @@ namespace TestRuntime
             #region Runtime
             var runtimeServices = new List<RuntimeService>();
 
-
             BusinessProcessService bpService = new BusinessProcessService() { Interval = new TimeSpan(0, 0, 2) };
             runtimeServices.Add(bpService);
-
-            QueueRegulatorRuntimeService queueRegulatorService = new QueueRegulatorRuntimeService() { Interval = new TimeSpan(0, 0, 2) };
-            runtimeServices.Add(queueRegulatorService);
-
-            QueueActivationRuntimeService queueActivationService = new QueueActivationRuntimeService() { Interval = new TimeSpan(0, 0, 2) };
-            runtimeServices.Add(queueActivationService);
-
-            SummaryQueueActivationRuntimeService summaryQueueActivationService = new SummaryQueueActivationRuntimeService() { Interval = new TimeSpan(0, 0, 2) };
-            runtimeServices.Add(summaryQueueActivationService);
-
-            SchedulerService schedulerService = new SchedulerService() { Interval = new TimeSpan(0, 0, 1) };
-            runtimeServices.Add(schedulerService);
-
-            Vanrise.Common.Business.BigDataRuntimeService bigDataService = new Vanrise.Common.Business.BigDataRuntimeService { Interval = new TimeSpan(0, 0, 2) };
-            runtimeServices.Add(bigDataService);
-
-            TransactionLockRuntimeService transactionLockRuntimeService = new TransactionLockRuntimeService() { Interval = new TimeSpan(0, 0, 1) };
-            runtimeServices.Add(transactionLockRuntimeService);
-
-            Vanrise.Integration.Business.DataSourceRuntimeService dsRuntimeService = new Vanrise.Integration.Business.DataSourceRuntimeService { Interval = new TimeSpan(0, 0, 2) };
-            runtimeServices.Add(dsRuntimeService);
-
+            
             BPRegulatorRuntimeService bpRegulatorRuntimeService = new BPRegulatorRuntimeService { Interval = new TimeSpan(0, 0, 2) };
             runtimeServices.Add(bpRegulatorRuntimeService);
 
