0cbf969dedf1b9cb442e1d5368c4b2e80539e054
mohamad.elhariri <mohamad.elhariri@vanrise.tfs.local>
2017-05-16 14:02:24 +0000

    Added AccountUsageOverride
    git-tfs-id: [http://developmentvlan:8585/tfs/vanrise.collection]$/;C41322

---
+++ Vanrise/Code/Vanrise.AccountBalance.Entities/AccountUsageOverride/AccountUsageOverride.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Vanrise.AccountBalance.Entities
+{
+    public class AccountUsageOverride
+    {
+        public Guid AccountTypeId { get; set; }
+
+        public string AccountId { get; set; }
+
+        public Guid TransactionTypeId { get; set; }
+
+        public DateTime PeriodStart { get; set; }
+
+        public DateTime PeriodEnd { get; set; }
+
+        public long OverriddenByTransactionId { get; set; }
+    }
+}
