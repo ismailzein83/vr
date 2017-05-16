0e2adb8e654f650c28db0ece5c3e760b5cac9951
Zeinab Mokdad <VANRISE\zeinab.mokdad>
2017-05-16 06:25:33 +0000

    adding new entity SalePricelistRPChange and SalePricelistRPChangeDetail
    git-tfs-id: [http://developmentvlan:8585/tfs/vanrise.collection]$/;C41301

---
+++ TOneV2/Code/TOneV2/TOne.WhS.BusinessEntity.Entities/SalePricelistChanges/SalePricelistRPChangeDetail.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TOne.WhS.BusinessEntity.Entities
+{
+    public class SalePricelistRPChangeDetail
+    {
+        public string ZoneName { get; set; }
+        public string RoutingProductName { get; set; }
+        public string RecentRoutingProductName { get; set; }
+        public DateTime BED { get; set; }
+        public DateTime? EED { get; set; }
+        public IEnumerable<int> RoutingProductServicesId { get; set; }
+        public IEnumerable<int> RecentRouringProductServicesId { get; set; }
+
+    }
+}
---
+++ TOneV2/Code/TOneV2/TOne.WhS.BusinessEntity.Entities/SalePricelistChanges/SalePricelistRateChangeDetail.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TOne.WhS.BusinessEntity.Entities.SalePricelistChanges
+{
+    public class SalePricelistRateChangeDetail
+    {
+        public string ZoneName { get; set; }
+        public decimal Rate { get; set; }
+        public IEnumerable<int> ServicesId { get; set; }
+        public DateTime BED { get; set; }
+        public DateTime? EED { get; set; }
+    }
+}
