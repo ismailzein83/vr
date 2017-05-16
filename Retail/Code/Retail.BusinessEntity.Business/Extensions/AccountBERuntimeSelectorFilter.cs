fa5408a33dc4f50343e72a30ca6d056983ee8ea3
Ali Atoui <VANRISE\ali.atoui>
2017-05-16 09:34:30 +0000

    Adding AccountBERuntimeSelectorFilter
    git-tfs-id: [http://developmentvlan:8585/tfs/vanrise.collection]$/;C41313

---
+++ Retail/Code/Retail.BusinessEntity.Business/Extensions/AccountBERuntimeSelectorFilter.cs
@@ -0,0 +1,28 @@
+﻿using Retail.BusinessEntity.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Vanrise.GenericData.Entities;
+
+namespace Retail.BusinessEntity.Business
+{
+    public class AccountBERuntimeSelectorFilter //: BERuntimeSelectorFilter
+    {
+        //public AccountCondition AccountCondition { get; set; }
+
+        //public override bool IsMatched(IBERuntimeSelectorFilterSelectorFilterContext context)
+        //{
+        //    if (AccountCondition == null)
+        //        return true;
+
+        //    long accountId;
+        //    if (!long.TryParse(context.BusinessEntityId.ToString(), out accountId))
+        //        return false;
+
+        //    var accountConditionEvaluationContext = new AccountConditionEvaluationContext(context.BusinessEntityDefinitionId, accountId);
+        //    return AccountCondition.Evaluate(accountConditionEvaluationContext);
+        //}
+    }
+}
