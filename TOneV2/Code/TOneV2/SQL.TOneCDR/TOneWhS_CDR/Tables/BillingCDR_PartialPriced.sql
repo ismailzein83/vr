022905a737bd3453980014e7cb7f580377944d58
anthony douaihy <VANRISE\anthony.douaihy>
2017-05-16 12:07:29 +0000

    adjust tone scripts in order tp support null cost rate in billing reports
    git-tfs-id: [http://developmentvlan:8585/tfs/vanrise.collection]$/;C41318

---
+++ TOneV2/Code/TOneV2/SQL.TOneCDR/TOneWhS_CDR/Tables/BillingCDR_PartialPriced.sql
@@ -0,0 +1,67 @@
+﻿CREATE TABLE [TOneWhS_CDR].[BillingCDR_PartialPriced] (
+    [CDRId]                       BIGINT          NOT NULL,
+    [AttemptDateTime]             DATETIME        NOT NULL,
+    [AlertDateTime]               DATETIME        NULL,
+    [ConnectDateTime]             DATETIME        NULL,
+    [DisconnectDateTime]          DATETIME        NULL,
+    [DurationInSeconds]           DECIMAL (20, 4) NULL,
+    [CustomerID]                  INT             NULL,
+    [SaleZoneID]                  BIGINT          NULL,
+    [OriginatingZoneID]           BIGINT          NULL,
+    [SupplierID]                  INT             NULL,
+    [SupplierZoneID]              BIGINT          NULL,
+    [CGPN]                        VARCHAR (50)    NULL,
+    [CDPN]                        VARCHAR (50)    NULL,
+    [CDPNIn]                      VARCHAR (50)    NULL,
+    [CDPNOut]                     VARCHAR (50)    NULL,
+    [SaleCode]                    VARCHAR (20)    NULL,
+    [SupplierCode]                VARCHAR (20)    NULL,
+    [IDonSwitch]                  BIGINT          NULL,
+    [Tag]                         VARCHAR (100)   NULL,
+    [SIP]                         VARCHAR (100)   NULL,
+    [IsRerouted]                  BIT             NULL,
+    [SaleRateID]                  BIGINT          NULL,
+    [SaleRateValue]               DECIMAL (20, 8) NULL,
+    [SaleRateTypeID]              INT             NULL,
+    [SaleNet]                     DECIMAL (22, 6) NULL,
+    [SaleCurrencyId]              INT             NULL,
+    [SaleDurationInSeconds]       DECIMAL (20, 4) NULL,
+    [CostRateID]                  BIGINT          NULL,
+    [CostRateValue]               DECIMAL (20, 8) NULL,
+    [CostRateTypeID]              INT             NULL,
+    [CostNet]                     DECIMAL (22, 6) NULL,
+    [CostCurrencyID]              INT             NULL,
+    [CostDurationInSeconds]       DECIMAL (20, 4) NULL,
+    [ReleaseCode]                 VARCHAR (50)    NULL,
+    [ReleaseSource]               VARCHAR (10)    NULL,
+    [SellingNumberPlanID]         INT             NULL,
+    [MasterPlanZoneID]            BIGINT          NULL,
+    [MasterPlanCode]              VARCHAR (20)    NULL,
+    [MasterPlanOriginatingZoneId] BIGINT          NULL,
+    [PortIN]                      VARCHAR (42)    NULL,
+    [PortOUT]                     VARCHAR (42)    NULL,
+    [SwitchId]                    INT             NULL,
+    [CostRateTypeRuleId]          INT             NULL,
+    [SaleRateTypeRuleId]          INT             NULL,
+    [CostTariffRuleId]            INT             NULL,
+    [SaleTariffRuleId]            INT             NULL,
+    [CostExtraChargeRuleId]       INT             NULL,
+    [SaleExtraChargeRuleId]       INT             NULL,
+    [CostExtraChargeValue]        DECIMAL (22, 6) NULL,
+    [SaleExtraChargeValue]        DECIMAL (22, 6) NULL,
+    [CountryId]                   INT             NULL,
+    [SaleExtraChargeRateValue]    DECIMAL (20, 8) NULL,
+    [CostExtraChargeRateValue]    DECIMAL (20, 8) NULL,
+    [QueueItemId]                 BIGINT          NULL
+);
+
+
+GO
+CREATE NONCLUSTERED INDEX [IX_BillingCDR_PartialPriced_CDRId]
+    ON [TOneWhS_CDR].[BillingCDR_PartialPriced]([CDRId] ASC);
+
+
+GO
+CREATE CLUSTERED INDEX [IX_BillingCDR_PartialPriced_AttemptDateTime]
+    ON [TOneWhS_CDR].[BillingCDR_PartialPriced]([AttemptDateTime] ASC);
+
