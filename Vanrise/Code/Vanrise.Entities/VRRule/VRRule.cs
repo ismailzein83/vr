0c88ec1a9b7e79b7e888d46e8ae4b1f0f1229e71
Ali Atoui <VANRISE\ali.atoui>
2017-05-18 06:16:31 +0000

    Adding VRRules @Vanrise.Common
    git-tfs-id: [http://developmentvlan:8585/tfs/vanrise.collection]$/;C41344

---
+++ Vanrise/Code/Vanrise.Entities/VRRule/VRRule.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Vanrise.Entities
+{
+    public class VRRule<T> where T : class
+    {
+        public long VRRuleId { get; set; }
+
+        public Guid VRRuleDefinitionId { get; set; }
+
+        public T Settings { get; set; }
+    }
+}
