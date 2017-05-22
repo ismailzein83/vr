0f5b81350fea64c295ae295b2c1932039ab01012
Zeinab Mokdad <VANRISE\zeinab.mokdad>
2017-05-22 13:20:54 +0000

    adding vr-whs-be-salepricelisttype-selector.js
    git-tfs-id: [http://developmentvlan:8585/tfs/vanrise.collection]$/;C41423

--- TOneV2/Code/TOneV2/TOne.WhS.BusinessEntity.Web/WhS_BusinessEntity/Constant/SalePriceListTypeEnum.js
+++ TOneV2/Code/TOneV2/TOne.WhS.BusinessEntity.Web/WhS_BusinessEntity/Constant/SalePriceListTypeEnum.js
@@ -1,1 +1,5 @@
-﻿
+﻿app.constant("WhS_BE_SalePriceListTypeEnum", {
+    Full: { value: 0, description: "Full" },
+    Country: { value: 1, description: "Country" },
+    RateChange: { value: 2, description: "Rate Change" }
+});
