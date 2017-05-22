628f4b7963bfbaf9f89432ea9f14ca6b23bcc8dc
Ali Atoui <VANRISE\ali.atoui>
2017-05-22 06:41:42 +0000

    Adding DIDAccountFilter
    git-tfs-id: [http://developmentvlan:8585/tfs/vanrise.collection]$/;C41397

---
+++ Retail/Code/Retail.BusinessEntity.Business/Filters/IAccountFilter/DIDAccountFilter.cs
@@ -0,0 +1,29 @@
+﻿using Retail.BusinessEntity.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Vanrise.GenericData.Business;
+using Vanrise.GenericData.Entities;
+using Vanrise.Common;
+
+namespace Retail.BusinessEntity.Business
+{
+    public class DIDAccountFilter : IAccountFilter
+    {
+        Guid _accountDIDRelationDefinitionId = new ConfigManager().GetAccountDIDRelationDefinitionId();
+
+        public bool IsExcluded(IAccountFilterContext context)
+        {
+            var beParentChildRelationDefinition = new BEParentChildRelationDefinitionManager().GetBEParentChildRelationDefinition(_accountDIDRelationDefinitionId);
+            beParentChildRelationDefinition.ThrowIfNull("beParentChildRelationDefinition", _accountDIDRelationDefinitionId);
+            beParentChildRelationDefinition.Settings.ThrowIfNull("beParentChildRelationDefinition.Settings", _accountDIDRelationDefinitionId);
+
+            var accountBERuntimeSelectorFilter = beParentChildRelationDefinition.Settings.ParentBERuntimeSelectorFilter.CastWithValidate<AccountBERuntimeSelectorFilter>("beParentChildRelationDefinition.Settings.ParentBERuntimeSelectorFilter");
+            var accountConditionAccountFilter = new AccountConditionAccountFilter() { AccountCondition = accountBERuntimeSelectorFilter.AccountCondition };
+
+            return accountConditionAccountFilter.IsExcluded(context);
+        }
+    }
+}
