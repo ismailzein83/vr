6731ba554bd7a5f37cdd04189aa32795d84af0d4
Zeinab Mokdad <VANRISE\zeinab.mokdad>
2017-05-23 15:00:26 +0000

    adding VRMail
    git-tfs-id: [http://developmentvlan:8585/tfs/vanrise.collection]$/;C41479

---
+++ Vanrise/Code/Vanrise.Entities/VRMail/VRMail.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Vanrise.Entities
+{
+    public class VRMail
+    {
+        public string To { get; set; }
+        public string From { get; set; }
+        public string CC { get; set; }
+        public string Subject { get; set; }
+        public string Body { get; set; }
+        public List<int> AttachementFileIds { get; set; }
+    }
+}
