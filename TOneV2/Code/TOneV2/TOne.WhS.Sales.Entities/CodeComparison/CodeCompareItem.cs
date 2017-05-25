2d65e78b76120727fc2f4217594b0606b546a40f
Mortada Issa <VANRISE\mortada.issa>
2017-05-25 12:06:39 +0000

    Addin schema for code comparison feature
    git-tfs-id: [http://developmentvlan:8585/tfs/vanrise.collection]$/;C41538

---
+++ TOneV2/Code/TOneV2/TOne.WhS.Sales.Entities/CodeComparison/CodeCompareItem.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TOne.WhS.Sales.Entities
+{
+    public enum CodeCompareIndicator { None = 0, Highlight = 1 };
+
+    public enum CodeCompareAction { New = 0, Delete = 1 };
+
+    public class CodeCompareItem
+    {
+        public string Code { get; set; }
+
+        public string SaleZone { get; set; }
+
+        public string SaleCode { get; set; }
+
+        public CodeCompareIndicator SaleCodeIndicator { get; set; }
+
+        public List<CodeCompareSupplierItem> SupplierItems { get; set; }
+
+        public int OccurrenceInSuppliers { get; set; }
+
+        public CodeCompareIndicator OccurrenceInSuppliersIndicator { get; set; }
+
+        public int AbsenceInSuppliers { get; set; }
+
+        public CodeCompareIndicator AbsenceInSuppliersIndicator { get; set; }
+
+        public CodeCompareAction Action { get; set; }
+    }
+
+    public class CodeCompareSupplierItem
+    {
+        public string SupplierZone { get; set; }
+
+        public string SupplierCode { get; set; }
+
+        public CodeCompareIndicator SupplierCodeIndicator { get; set; }
+    }
+}
