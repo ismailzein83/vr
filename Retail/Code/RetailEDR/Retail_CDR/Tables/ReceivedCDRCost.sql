98ffb6f629e5a2604a8fb9d446a30f5ecce78e32
Wissam Ajamy <VANRISE\wissam.ajamy>
2017-05-26 08:57:48 +0000

    update structure from Retail_Dev_CDR
    git-tfs-id: [http://developmentvlan:8585/tfs/vanrise.collection]$/;C41567

---
+++ Retail/Code/RetailEDR/Retail_CDR/Tables/ReceivedCDRCost.sql
@@ -0,0 +1,10 @@
+﻿CREATE TABLE [Retail_CDR].[ReceivedCDRCost] (
+    [ID]                BIGINT          NULL,
+    [AttemptDateTime]   DATETIME        NULL,
+    [CGPN]              VARCHAR (100)   NULL,
+    [CDPN]              VARCHAR (100)   NULL,
+    [DurationInSeconds] DECIMAL (20, 4) NULL,
+    [Rate]              DECIMAL (20, 8) NULL,
+    [Amount]            DECIMAL (22, 6) NULL
+);
+
---
+++ Retail/Code/RetailEDR/Retail_CDR/User Defined Types/ReceivedCostCDRType.sql
@@ -0,0 +1,9 @@
+﻿CREATE TYPE [Retail_CDR].[ReceivedCostCDRType] AS TABLE (
+    [ID]                BIGINT          NULL,
+    [AttemptDateTime]   DATETIME        NULL,
+    [CGPN]              VARCHAR (100)   NULL,
+    [CDPN]              VARCHAR (100)   NULL,
+    [DurationInSeconds] DECIMAL (20, 4) NULL,
+    [Rate]              DECIMAL (20, 8) NULL,
+    [Amount]            DECIMAL (22, 6) NULL);
+
