f8603ced74da7c9f3156db76c970bd72fa4913f9
Ismail Zein <VANRISE\ismail.zein>
2017-05-26 06:28:59 +0000

    sql post script
    git-tfs-id: [http://developmentvlan:8585/tfs/vanrise.collection]$/;C41552

---
+++ TOneV2/Code/TOneV2/SQL.TOneConfiguration/VR_Invoice/Stored Procedures/sp_InvoiceAccount_Insert.sql
@@ -0,0 +1,25 @@
+﻿-- =============================================
+-- Author:		<Author,,Name>
+-- Create date: <Create Date,,>
+-- Description:	<Description,,>
+-- =============================================
+create PROCEDURE [VR_Invoice].[sp_InvoiceAccount_Insert]
+	-- Add the parameters for the stored procedure here
+	@InvoiceTypeId uniqueidentifier,
+	@PartnerId varchar(50),
+	@BED datetime = NULL,
+	@EED datetime = NULL,
+	@Status int,
+	@IsDeleted bit,
+	@ID bigint out 
+AS
+BEGIN
+	-- SET NOCOUNT ON added to prevent extra result sets from
+	-- interfering with SELECT statements.
+IF NOT EXISTS(select 1 from [VR_Invoice].InvoiceAccount where InvoiceTypeId = @InvoiceTypeId and PartnerId = @PartnerId )
+	BEGIN	
+		INSERT INTO [VR_Invoice].InvoiceAccount(InvoiceTypeId,PartnerId,BED,EED,[Status],IsDeleted) 
+		VALUES (@InvoiceTypeId,@PartnerId,@BED,@EED,@Status,@IsDeleted)
+		SET @ID = SCOPE_IDENTITY()
+	END
+END
--- TOneV2/Code/TOneV2/SQL.TOneConfiguration/VR_Invoice/Tables/InvoicePartner.sql
+++ TOneV2/Code/TOneV2/SQL.TOneConfiguration/VR_Invoice/Tables/InvoicePartner.sql
@@ -1,19 +1,6 @@
-﻿CREATE TABLE [VR_Invoice].[InvoicePartner] (
-    [ID]            BIGINT           IDENTITY (1, 1) NOT NULL,
-    [InvoiceTypeID] UNIQUEIDENTIFIER NOT NULL,
-    [PartnerID]     VARCHAR (50)     NOT NULL,
-    [BED]           DATETIME         NULL,
-    [EED]           DATETIME         NULL,
-    [Status]        INT              NOT NULL,
-    [IsDeleted]     BIT              NULL,
-    [CreatedTime]   DATETIME         CONSTRAINT [DF_InvoicePartner_CreatedTime] DEFAULT (getdate()) NULL,
-    [timestamp]     ROWVERSION       NULL,
-    CONSTRAINT [PK_InvoicePartner] PRIMARY KEY CLUSTERED ([ID] ASC),
-    CONSTRAINT [IX_InvoicePartner_TypeAndPartnerID] UNIQUE NONCLUSTERED ([InvoiceTypeID] ASC, [PartnerID] ASC)
-);
+﻿
 
 
 GO
-CREATE NONCLUSTERED INDEX [IX_InvoicePartner_StatusAndEffective]
-    ON [VR_Invoice].[InvoicePartner]([IsDeleted] ASC, [Status] ASC, [BED] ASC, [EED] ASC);
+
 
