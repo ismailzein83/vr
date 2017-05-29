957c0d8ed72472dfe1c71915dfac5e3255c70e37
Wissam Ajamy <VANRISE\wissam.ajamy>
2017-05-29 11:59:18 +0000

    Update structure from TOneV2_Dev
    git-tfs-id: [http://developmentvlan:8585/tfs/vanrise.collection]$/;C41621

---
+++ TOneV2/Code/TOneV2/SQL.TOneWhS_BE/TOneWhS_Sales/Stored Procedures/sp_RatePlan_Delete.sql
@@ -0,0 +1,12 @@
+﻿-- =============================================
+-- Author:		<Author,,Name>
+-- Create date: <Create Date,,>
+-- Description:	<Description,,>
+-- =============================================
+CREATE PROCEDURE TOneWhS_Sales.sp_RatePlan_Delete
+	@OwnerType tinyint,
+	@OwnerID int
+AS
+BEGIN
+	delete from TOneWhS_Sales.RatePlan where OwnerType = @OwnerType and OwnerID = @OwnerID
+END
