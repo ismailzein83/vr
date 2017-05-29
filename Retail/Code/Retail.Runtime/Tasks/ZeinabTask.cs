8bef9db651eb5ef4fc49f9e1f0f7d85f0177d78d
Zeinab Mokdad <VANRISE\zeinab.mokdad>
2017-05-29 08:22:51 +0000

    implementing bp instance
    git-tfs-id: [http://developmentvlan:8585/tfs/vanrise.collection]$/;C41605

--- Retail/Code/Retail.Runtime/Tasks/ZeinabTask.cs
+++ Retail/Code/Retail.Runtime/Tasks/ZeinabTask.cs
@@ -3,6 +3,11 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Vanrise.BusinessProcess;
+using Vanrise.Caching.Runtime;
+using Vanrise.Common.Business;
+using Vanrise.Queueing;
+using Vanrise.Runtime;
 
 namespace Retail.Runtime.Tasks
 {
@@ -10,7 +15,17 @@ namespace Retail.Runtime.Tasks
     {
         public void Execute()
         {
-            throw new NotImplementedException();
+            var runtimeServices = new List<RuntimeService>();
+
+            BusinessProcessService bpService = new BusinessProcessService() { Interval = new TimeSpan(0, 0, 2) };
+            runtimeServices.Add(bpService);
+
+            BPRegulatorRuntimeService bpRegulatorRuntimeService = new BPRegulatorRuntimeService { Interval = new TimeSpan(0, 0, 2) };
+            runtimeServices.Add(bpRegulatorRuntimeService);
+
+            RuntimeHost host = new RuntimeHost(runtimeServices);
+            host.Start();
+            Console.ReadKey();
         }
     }
 }
