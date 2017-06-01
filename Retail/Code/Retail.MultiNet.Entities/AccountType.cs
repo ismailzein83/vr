5e1123061ed7e1da0fa2a40dde8f34614d0efe4b
Ali Ballouk <VANRISE\ali.ballouk>
2017-06-01 11:16:09 +0000

    MultiNet Company extended Info Enums
    git-tfs-id: [http://developmentvlan:8585/tfs/vanrise.collection]$/;C41683

---
+++ Retail/Code/Retail.MultiNet.Entities/AccountType.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Retail.MultiNet.Entities
+{
+    public enum MultiNetAccountType { BusinessTrunk = 0 , POTS = 1, IPCentrex = 2 }
+
+}
---
+++ Retail/Code/Retail.MultiNet.Entities/AddressTypes.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Retail.MultiNet.Entities
+{
+    public enum AddressTypes { Billing = 0, Home = 1 , Office = 2, Sales =3 , Technical = 4 }
+
+}
