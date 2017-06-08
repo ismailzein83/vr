c444604ad550c44f8d41bcd1c82cccd40a2c58b0
Samer Haidar <VANRISE\samer.haidar>
2017-06-08 08:00:05 +0000

    Adding InvoiceDataSourceItem and LastInvoicesDataSourceSettings
    git-tfs-id: [http://developmentvlan:8585/tfs/vanrise.collection]$/;C41791

---
+++ Vanrise/Code/Vanrise.Invoice.Entities/InvoiceType/InvoiceDataSourceItem.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Vanrise.Invoice.Entities
+{
+    public class InvoiceDataSourceItem
+    {
+        public InvoiceDataSourceItem() { }
+        public string CurrencyName { get; set; }
+        public decimal  Amount { get; set; }
+        public IEnumerable<InvoiceDataSourceItem> GetInvoiceDataSourceItemRDLCSchema()
+        {
+            return null;
+        }
+    }
+}
---
+++ Vanrise/Code/Vanrise.Invoice.MainExtensions/InvoiceDataSourceSettings/LastInvoicesDataSourceSettings.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Vanrise.Invoice.Entities;
+using Vanrise.Invoice.Business;
+using Vanrise.Common;
+using Vanrise.Common.Business;
+namespace Vanrise.Invoice.MainExtensions
+{
+    public class LastInvoicesDataSourceSettings : InvoiceDataSourceSettings
+    {
+        public override Guid ConfigId
+        {
+            get { return new Guid("A06AF611-6996-488D-B6D9-75BFC775947C"); }
+        }
+        public int LastInvoices { get; set; }
+        public override IEnumerable<dynamic> GetDataSourceItems(IInvoiceDataSourceSettingsContext context)
+        {
+            var currentInvoice = context.InvoiceActionContext.GetInvoice;
+            var invoiceType = new InvoiceTypeManager().GetInvoiceType(currentInvoice.InvoiceTypeId);
+            var invoiceManager = new InvoiceManager();
+            var invoices =  invoiceManager.GetLasInvoices(currentInvoice.InvoiceTypeId, currentInvoice.PartnerId,currentInvoice.InvoiceId, this.LastInvoices);
+            List<InvoiceDataSourceItem> invoiceDataSourceItems = new List<InvoiceDataSourceItem>();
+            if(invoices != null)
+            {
+                invoiceType.Settings.ThrowIfNull("invoiceType.Settings");
+                invoiceType.Settings.CurrencyFieldName.ThrowIfNull("invoiceType.Settings.CurrencyFieldName");
+                invoiceType.Settings.AmountFieldName.ThrowIfNull("invoiceType.Settings.AmountFieldName");
+                var currencyManager = new CurrencyManager();
+                foreach(var invoice in invoices)
+                {
+                    var currencyId = Utilities.GetPropValueReader(invoiceType.Settings.CurrencyFieldName).GetPropertyValue(invoice.Details);
+                    var amount = Utilities.GetPropValueReader(invoiceType.Settings.AmountFieldName).GetPropertyValue(invoice.Details);
+                    invoiceDataSourceItems.Add(new InvoiceDataSourceItem
+                    {
+                        Amount = amount,
+                        CurrencyName = currencyManager.GetCurrencySymbol(currencyId)
+                    });
+                }
+            }
+            return invoiceDataSourceItems;
+        }
+    }
+}
