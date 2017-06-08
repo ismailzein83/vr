c0dcb79d771c0659f9ccee54b97f95c44bcdb410
Samer Haidar <VANRISE\samer.haidar>
2017-06-08 11:20:47 +0000

    Adding invtoaccbalancerelation projects
    git-tfs-id: [http://developmentvlan:8585/tfs/vanrise.collection]$/;C41803

---
+++ Retail/Code/Retail.InvToAccBalanceRelation.Business/AccountInvToAccBalanceRelationDefinitionExtendedSettings.cs
@@ -0,0 +1,57 @@
+﻿using Retail.BusinessEntity.Business;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Vanrise.InvToAccBalanceRelation.Entities;
+using Vanrise.Common;
+namespace Retail.InvToAccBalanceRelation.Business
+{
+    public class AccountInvToAccBalanceRelationDefinitionExtendedSettings : InvToAccBalanceRelationDefinitionExtendedSettings
+    {
+        public override Guid ConfigId
+        {
+            get { return new Guid("D879B75A-7F13-4543-8EDE-961327CB3E33"); }
+        }
+        public Guid AccountBEDefinitionId { get; set; }
+        public override List<InvoiceAccountInfo> GetBalanceInvoiceAccounts(IInvToAccBalanceRelGetBalanceInvoiceAccountsContext context)
+        {
+
+            FinancialAccountManager financialAccountManager = new FinancialAccountManager();
+            var financialAccountData = new FinancialAccountManager().GetFinancialAccountData(this.AccountBEDefinitionId, context.AccountId);
+            financialAccountData.ThrowIfNull("financialAccountData");
+            List<InvoiceAccountInfo> invoiceAccountInfo = new List<InvoiceAccountInfo>();
+            FinancialAccountDefinitionManager financialAccountDefinitionManager = new FinancialAccountDefinitionManager();
+            var accountBalanceSettings = financialAccountDefinitionManager.GetFinancialAccountDefinitionSettings(financialAccountData.FinancialAccount.FinancialAccountDefinitionId);
+            if(accountBalanceSettings.InvoiceTypeId.HasValue)
+            {
+                invoiceAccountInfo.Add(new InvoiceAccountInfo
+                {
+                    InvoiceTypeId = accountBalanceSettings.InvoiceTypeId.Value,
+                    PartnerId = context.AccountId
+                });
+            }
+            return invoiceAccountInfo;
+        }
+
+        public override List<BalanceAccountInfo> GetInvoiceBalanceAccounts(IInvToAccBalanceRelGetInvoiceBalanceAccountsContext context)
+        {
+            FinancialAccountManager financialAccountManager = new FinancialAccountManager();
+            var financialAccountData = new FinancialAccountManager().GetFinancialAccountData(this.AccountBEDefinitionId, context.PartnerId);
+            financialAccountData.ThrowIfNull("financialAccountData");
+            List<BalanceAccountInfo> balanceAccountInfo = new List<BalanceAccountInfo>();
+            FinancialAccountDefinitionManager financialAccountDefinitionManager = new FinancialAccountDefinitionManager();
+            var accountBalanceSettings = financialAccountDefinitionManager.GetFinancialAccountDefinitionSettings(financialAccountData.FinancialAccount.FinancialAccountDefinitionId);
+            if (accountBalanceSettings.BalanceAccountTypeId.HasValue)
+            {
+                balanceAccountInfo.Add(new BalanceAccountInfo
+                {
+                    AccountTypeId = accountBalanceSettings.BalanceAccountTypeId.Value,
+                    AccountId = context.PartnerId
+                });
+            }
+            return balanceAccountInfo;
+        }
+    }
+}
