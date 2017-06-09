962d161b7d1d9f1e5cb8c42a69a42e9ac51b13e6
mohammad.zhour <mohammad.zhour@vanrise.tfs.local>
2017-06-09 07:02:48 +0000

    add exteded setting to import supplier price list
    git-tfs-id: [http://developmentvlan:8585/tfs/vanrise.collection]$/;C41813

---
+++ TOneV2/Code/TOneV2/TOne.WhS.SupplierPriceList.Business/SupplierPriceListProcessBPSettings.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TOne.WhS.SupplierPriceList.BP.Arguments;
+using  Vanrise.Common;
+namespace TOne.WhS.SupplierPriceList.Business
+{
+    public class SupplierPriceListProcessBPSettings : Vanrise.BusinessProcess.Business.DefaultBPDefinitionExtendedSettings
+    {
+       public override bool CanRunBPInstance(Vanrise.BusinessProcess.Entities.IBPDefinitionCanRunBPInstanceContext context)
+       {
+           context.IntanceToRun.ThrowIfNull("context.IntanceToRun");
+           SupplierPriceListProcessInput inputArg = context.IntanceToRun.InputArgument.CastWithValidate<SupplierPriceListProcessInput>("context.IntanceToRun.InputArgument");
+           foreach (var startedBPInstance in context.GetStartedBPInstances())
+           {
+               SupplierPriceListProcessInput startedBPInstanceInputArg = startedBPInstance.InputArgument as SupplierPriceListProcessInput;
+               if (startedBPInstanceInputArg != null)
+               {
+                   if (startedBPInstanceInputArg.SupplierAccountId == inputArg.SupplierAccountId)
+                   {
+                       context.Reason = "Another process is running for the same account type";
+                       return false;
+                   }
+               }
+           }
+           return true;
+       }
+
+    }
+}
