3b1ec4f6f1bd925238a830004252b1404843a7d7
mohamad.elhariri <mohamad.elhariri@vanrise.tfs.local>
2017-06-16 11:02:02 +0000

    Added ConflictingRateCurrenciesCondition
    git-tfs-id: [http://developmentvlan:8585/tfs/vanrise.collection]$/;C41924

---
+++ TOneV2/Code/TOneV2/TOne.WhS.Sales.Business/BusinessRules/Conditions/ConflictingRateCurrenciesCondition.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TOne.WhS.Sales.Entities;
+
+namespace TOne.WhS.Sales.Business.BusinessRules
+{
+    public class ConflictingRateCurrenciesCondition : Vanrise.BusinessProcess.Entities.BusinessRuleCondition
+    {
+        public override bool ShouldValidate(Vanrise.BusinessProcess.Entities.IRuleTarget target)
+        {
+            return target is DataByZone;
+        }
+
+        public override bool Validate(Vanrise.BusinessProcess.Entities.IBusinessRuleConditionValidateContext context)
+        {
+            var zoneData = context.Target as DataByZone;
+
+
+
+            return true;
+        }
+
+        public override string GetMessage(Vanrise.BusinessProcess.Entities.IRuleTarget target)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
