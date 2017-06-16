ff0d6979530ee8ef65f425e6c3f7d4c566591020
mohammad.zhour <mohammad.zhour@vanrise.tfs.local>
2017-06-16 06:57:27 +0000

    added a new procedure to transaction DB checking for running instances with same Entity Id
    git-tfs-id: [http://developmentvlan:8585/tfs/vanrise.collection]$/;C41914

---
+++ TOneV2/Code/TOneV2/SQL.TOneTransaction/bp/Stored Procedures/sp_BPInstance_HasRunningInstance.sql
@@ -0,0 +1,21 @@
+﻿-- =============================================
+-- Author:		<Author,,Name>
+-- Create date: <Create Date,,>
+-- Description:	<Description,,>
+-- =============================================
+CREATE PROCEDURE [bp].[sp_BPInstance_HasRunningInstance]
+@definitionID uniqueidentifier,
+	@entityID varchar(255),
+	@Statuses varchar(max)
+AS
+BEGIN
+DECLARE @StatusesTable TABLE ([Status] int)
+	INSERT INTO @StatusesTable ([Status])
+	SELECT Convert(int, ParsedString) FROM bp.[ParseStringList](@Statuses)
+	
+  IF Exists ( SELECT top 1 null 	FROM	bp.[BPInstance] bp WITH(NOLOCK)
+	JOIN @StatusesTable statuses ON bp.ExecutionStatus = statuses.[Status] WHERE	DefinitionID = @definitionID AND EntityId=@entityID)
+	SELECT 1
+	ELSE SELECT 0
+  	
+END
