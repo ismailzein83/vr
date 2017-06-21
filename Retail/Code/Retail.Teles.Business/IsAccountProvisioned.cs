d86838c8bc2eec7fe8b53f671909853d2808f06d
Samer Haidar <VANRISE\samer.haidar>
2017-06-21 13:30:25 +0000

    adding IsAccountProvisioned
    git-tfs-id: [http://developmentvlan:8585/tfs/vanrise.collection]$/;C41959

---
+++ Retail/Code/Retail.Teles.Business/IsAccountProvisioned.cs
@@ -0,0 +1,47 @@
+﻿using Retail.BusinessEntity.Business;
+using Retail.BusinessEntity.Entities;
+using Retail.Teles.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Retail.Teles.Business
+{
+    public class IsAccountProvisioned : AccountGenericField
+    {
+        public override string Name
+        {
+            get { return "IsProvisioned"; }
+        }
+
+        public override string Title
+        {
+            get { return "Is Provisioned"; }
+        }
+
+        public override Vanrise.GenericData.Entities.DataRecordFieldType FieldType
+        {
+            get {
+                return new Vanrise.GenericData.MainExtensions.DataRecordFields.FieldBooleanType();
+            }
+        }
+
+        public override dynamic GetValue(IAccountGenericFieldContext context)
+        {
+            AccountBEManager accountBEManager = new AccountBEManager();
+            var enterpriseAccountMappingInfo = accountBEManager.GetExtendedSettings<EnterpriseAccountMappingInfo>(context.Account);
+            if (enterpriseAccountMappingInfo != null)
+            {
+                return true;
+            }
+            var siteAccountMappingInfo = accountBEManager.GetExtendedSettings<SiteAccountMappingInfo>(context.Account);
+            if (siteAccountMappingInfo != null)
+            {
+                return true;
+            }
+            return null;
+        }
+    }
+}
