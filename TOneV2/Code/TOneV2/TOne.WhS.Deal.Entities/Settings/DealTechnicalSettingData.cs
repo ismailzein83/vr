2fde3513aa237ff00162dfea4fb6c92a46c85763
Ali Atoui <VANRISE\ali.atoui>
2017-06-21 10:35:54 +0000

    Adding DealTechnicalSettingData
    git-tfs-id: [http://developmentvlan:8585/tfs/vanrise.collection]$/;C41955

---
+++ TOneV2/Code/TOneV2/TOne.WhS.Deal.Entities/Settings/DealTechnicalSettingData.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Vanrise.Reprocess.Entities;
+
+namespace TOne.WhS.Deal.Entities
+{
+    public class DealTechnicalSettingData
+    {
+        public Guid ReprocessDefinitionId { get; set; }
+
+        public ReprocessChunkTimeEnum ChunkTime { get; set; }
+    }
+}
