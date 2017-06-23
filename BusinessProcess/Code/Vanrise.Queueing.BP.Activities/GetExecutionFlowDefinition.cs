f59a322470846f0121e93d33938e4c83e2b58f28
anthony douaihy <VANRISE\anthony.douaihy>
2017-06-23 08:34:22 +0000

    Add Vanrise.Queueing.BP.Activities Project
    git-tfs-id: [http://developmentvlan:8585/tfs/vanrise.collection]$/;C42012

---
+++ BusinessProcess/Code/Vanrise.Queueing.BP.Activities/GetExecutionFlowDefinition.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Activities;
+using Vanrise.Queueing.Entities;
+
+namespace Vanrise.Queueing.BP.Activities
+{
+    public sealed class GetExecutionFlowDefinition : CodeActivity
+    {
+        [RequiredArgument]
+        public InArgument<Guid> ExecutionFlowDefinitionId { get; set; }
+
+        [RequiredArgument]
+        public OutArgument<QueueExecutionFlowDefinition> ExecutionFlowDefinition { get; set; }
+
+        protected override void Execute(CodeActivityContext context)
+        {
+            QueueExecutionFlowDefinitionManager manager = new QueueExecutionFlowDefinitionManager();
+            QueueExecutionFlowDefinition executionFlowDefinition = manager.GetExecutionFlowDefinition(this.ExecutionFlowDefinitionId.Get(context));
+
+            this.ExecutionFlowDefinition.Set(context, executionFlowDefinition);
+        }
+    }
+}
