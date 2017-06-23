03de02f9d5aab38fa5a855d9b64f641a3794941a
Ali Atoui <VANRISE\ali.atoui>
2017-06-23 07:56:25 +0000

    Adding DealEvaluatorPocessInput
    git-tfs-id: [http://developmentvlan:8585/tfs/vanrise.collection]$/;C42007

---
+++ TOneV2/Code/TOneV2/TOne.WhS.Deal.BP.Arguments/DealEvaluatorProcessInput.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TOne.WhS.Deal.BP.Arguments
+{
+    public class DealEvaluatorProcessInput : Vanrise.BusinessProcess.Entities.BaseProcessInputArgument
+    {
+        public override string GetTitle()
+        {
+            return String.Format("Deal Evaluator Process");
+        }
+    }
+}
