b0cc1e982e2873ee087627dbf2d76f321cd4f0cd
Ali Ballouk <VANRISE\ali.ballouk>
2017-06-28 13:32:12 +0000

    multinet did convertor implementation
    git-tfs-id: [http://developmentvlan:8585/tfs/vanrise.collection]$/;C42045

--- Retail/Code/Retail.MultiNet.Web/Retail_MultiNet/Directives/DID/retail-multinet-did-convertor-editor.js
+++ Retail/Code/Retail.MultiNet.Web/Retail_MultiNet/Directives/DID/retail-multinet-did-convertor-editor.js
@@ -1,1 +1,94 @@
-﻿
+﻿'use strict';
+
+app.directive('retailMultinetDidConvertorEditor', ['UtilsService', 'VRUIUtilsService',
+    function (UtilsService, VRUIUtilsService) {
+
+        var directiveDefinitionObject = {
+            restrict: 'E',
+            scope: {
+                onReady: '=',
+            },
+            controller: function ($scope, $element, $attrs) {
+                var ctrl = this;
+                var ctor = new retailMultiNetDidConvertorEditorCtor(ctrl, $scope, $attrs);
+                ctor.initializeController();
+            },
+            controllerAs: 'ctrl',
+            bindToController: true,
+            templateUrl: "/Client/Modules/Retail_MultiNet/Directives/DID/Templates/DIDConvertorEditor.html"
+        };
+
+        function retailMultiNetDidConvertorEditorCtor(ctrl, $scope, $attrs) {
+            this.initializeController = initializeController;
+
+            var accountDefinitionSelectorApi;
+            var accountDefinitionSelectorPromiseDeferred = UtilsService.createPromiseDeferred();
+
+
+            $scope.scopeModel = {};
+
+            $scope.scopeModel.onAccountDefinitionSelectorReady = function (api) {
+                accountDefinitionSelectorApi = api;
+                accountDefinitionSelectorPromiseDeferred.resolve();
+            };
+
+            function initializeController() {
+                defineAPI();
+            }
+
+            function defineAPI() {
+                var api = {};
+
+                api.load = function (payload) {
+
+                    if (payload != undefined) {
+                        $scope.scopeModel.accountColumn = payload.SourceAccountIdColumn;
+                        $scope.scopeModel.didColumn = payload.DIDColumn;
+                    }
+
+                    var promises = [];
+                    var loadAccountDefinitionTypePromise = getAccountDefinitionSelectorLoadPromise();
+                    promises.push(loadAccountDefinitionTypePromise);
+
+                    function getAccountDefinitionSelectorLoadPromise() {
+                        var businessEntityDefinitionSelectorLoadDeferred = UtilsService.createPromiseDeferred();
+
+                        accountDefinitionSelectorPromiseDeferred.promise.then(function () {
+                            var selectorPayload = {
+                                filter: {
+                                    Filters: [{
+                                        $type: "Retail.BusinessEntity.Business.AccountBEDefinitionFilter, Retail.BusinessEntity.Business"
+                                    }]
+                                }
+                            };
+
+                            if (payload != undefined) {
+                                selectorPayload.selectedIds = payload.AccountBEDefinitionId;
+                            }
+                            VRUIUtilsService.callDirectiveLoad(accountDefinitionSelectorApi, selectorPayload, businessEntityDefinitionSelectorLoadDeferred);
+                        });
+
+                        return businessEntityDefinitionSelectorLoadDeferred.promise;
+                    };
+
+                    return UtilsService.waitMultiplePromises(promises);
+                };
+
+                api.getData = function () {
+                    var data = {
+                        $type: "Retail.MultiNet.Business.Convertors.DIDConvertor, Retail.MultiNet.Business",
+                        Name: "MultiNet DID Convertor",
+                        DIDColumn: $scope.scopeModel.didColumn,
+                        SourceAccountIdColumn: $scope.scopeModel.accountColumn,
+                        AccountBEDefinitionId: accountDefinitionSelectorApi.getSelectedIds()
+                    };
+                    return data;
+                };
+
+                if (ctrl.onReady != null)
+                    ctrl.onReady(api);
+            }
+        }
+
+        return directiveDefinitionObject;
+    }]);
