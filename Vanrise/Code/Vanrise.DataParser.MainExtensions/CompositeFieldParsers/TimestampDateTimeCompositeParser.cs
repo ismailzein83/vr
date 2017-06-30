0f16745009a68b5c151dd5de037af21236622b3c
rabih.fashwal:2 <rabih.fashwal:2@vanrise.tfs.local>
2017-06-30 14:52:46 +0000

    Add Timestamp date time parser
    git-tfs-id: [http://developmentvlan:8585/tfs/vanrise.collection]$/;C42084

---
+++ Vanrise/Code/Vanrise.DataParser.MainExtensions/CompositeFieldParsers/TimestampDateTimeCompositeParser.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Vanrise.DataParser.Entities;
+
+namespace Vanrise.DataParser.MainExtensions.CompositeFieldParsers
+{
+    public class TimestampDateTimeCompositeParser : CompositeFieldsParser
+    {
+        public string FieldName { get; set; }
+        public string DateTimeFieldName { get; set; }
+        public DateTime DateTimeShift { get; set; }
+        public override void Execute(ICompositeFieldsParserContext context)
+        {
+            DateTime dateTimeField = (DateTime)context.Record.GetFieldValue(DateTimeFieldName);
+            TimeSpan timeSpan = dateTimeField - DateTimeShift;
+            context.Record.SetFieldValue(FieldName, timeSpan.TotalSeconds);
+        }
+    }
+}
