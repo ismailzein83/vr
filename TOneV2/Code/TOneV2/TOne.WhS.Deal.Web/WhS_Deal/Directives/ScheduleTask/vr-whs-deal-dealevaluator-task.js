c727feecac40837812367b014c39d0343d70eb08
anthony douaihy <VANRISE\anthony.douaihy>
2017-07-12 10:59:14 +0000

    add scheduled task for deal evaluator
    git-tfs-id: [http://developmentvlan:8585/tfs/vanrise.collection]$/;C42214

---
+++ TOneV2/Code/TOneV2/TOne.WhS.Deal.Web/WhS_Deal/Directives/ScheduleTask/vr-whs-deal-dealevaluator-task.js
@@ -0,0 +1,60 @@
+﻿"use strict";
+
+app.directive("vrWhsDealDealevaluatorTask", ['UtilsService', 'VRUIUtilsService',
+    function (UtilsService, VRUIUtilsService) {
+        var directiveDefinitionObject = {
+            restrict: "E",
+            scope: {
+                onReady: "="
+            },
+            controller: function ($scope, $element, $attrs) {
+                var ctrl = this;
+
+                var directiveConstructor = new DirectiveConstructor($scope, ctrl);
+                directiveConstructor.initializeController();
+            },
+            controllerAs: "ctrl",
+            bindToController: true,
+            compile: function (element, attrs) {
+                return {
+                    pre: function ($scope, iElem, iAttrs, ctrl) {
+
+                    }
+                };
+            },
+            templateUrl: "/Client/Modules/WhS_Deal/Directives/ScheduleTask/Templates/DealEvaluatorTaskTemplate.html"
+        };
+
+        function DirectiveConstructor($scope, ctrl) {
+            this.initializeController = initializeController;
+
+            function initializeController() {
+
+                defineAPI();
+            }
+            function defineAPI() {
+
+                var api = {};
+
+                api.getData = function () {
+                    return {
+                        $type: "TOne.WhS.Deal.BP.Arguments.DealEvaluatorProcessInput, TOne.WhS.Deal.BP.Arguments"
+                    };
+                };
+
+                api.getExpressionsData = function () {
+                    return null;
+                };
+
+                api.load = function (payload) {
+                    var promises = [];
+                    return UtilsService.waitMultiplePromises(promises);
+                };
+
+                if (ctrl.onReady != null)
+                    ctrl.onReady(api);
+            }
+        }
+
+        return directiveDefinitionObject;
+    }]);
