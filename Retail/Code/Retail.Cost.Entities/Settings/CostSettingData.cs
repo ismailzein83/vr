2aee08e125eff729c39ee334780a3bac2546d65b
Ali Atoui <VANRISE\ali.atoui>
2017-07-13 10:27:21 +0000

    Adding CostTechnicalSettingData
    git-tfs-id: [http://developmentvlan:8585/tfs/vanrise.collection]$/;C42228

---
+++ Retail/Code/Retail.Cost.Entities/Settings/CostSettingData.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Retail.Cost.Entities
+{
+    public class CostSettingData
+    {
+        public int MaxBatchDurationInMinutes { get; set; }
+         
+        public TimeSpan DurationMargin { get; set; }
+
+        public TimeSpan AttemptDateTimeMargin { get; set; }
+
+        public TimeSpan AttemptDateTimeOffset { get; set; } 
+    }
+}
