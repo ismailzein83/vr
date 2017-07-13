48608465c7daa9d1dc223b19f6928c612bbbfb72
Ali Atoui <VANRISE\ali.atoui>
2017-07-13 10:48:38 +0000

    Adding Cost WebVirtualDirectory and CostSettingsData
    git-tfs-id: [http://developmentvlan:8585/tfs/vanrise.collection]$/;C42230

---
+++ Retail/Code/Retail.Cost.Web/Retail_Cost/Settings/Directives/retail-cost-settings-editor.js
@@ -0,0 +1,61 @@
+﻿'use strict';
+
+app.directive('retailCostSettingsEditor', ['UtilsService', 'VRUIUtilsService',
+    function (UtilsService, VRUIUtilsService) {
+        return {
+            restrict: 'E',
+            scope: {
+                onReady: '=',
+                normalColNum: '@'
+            },
+            controller: function ($scope, $element, $attrs) {
+                var ctrl = this;
+                var ctor = new CostSettingsEditor($scope, ctrl, $attrs);
+                ctor.initializeController();
+            },
+            controllerAs: 'ctrl',
+            bindToController: true,
+            templateUrl: '/Client/Modules/Retail_Cost/Settings/Directives/Templates/CostSettingsEditorTemplate.html'
+        };
+
+        function CostSettingsEditor($scope, ctrl, $attrs) {
+            this.initializeController = initializeController;
+
+            function initializeController() {
+                $scope.scopeModel = {};
+
+                defineAPI();
+            }
+
+            function defineAPI() {
+
+                var api = {};
+
+                api.load = function (payload) {
+
+                    if (payload != undefined && payload.data != undefined) {
+                        $scope.scopeModel.maxBatchDurationInMinutes = payload.data.MaxBatchDurationInMinutes;
+                        $scope.scopeModel.durationMargin = payload.data.DurationMargin;
+                        $scope.scopeModel.attemptDateTimeMargin = payload.data.AttemptDateTimeMargin;
+                        $scope.scopeModel.attemptDateTimeOffset = payload.data.AttemptDateTimeOffset;
+                    }
+                };
+
+                api.getData = function () {
+
+                    var data = {
+                        $type: 'Retail.Cost.Entities.CostSettingData, Retail.Cost.Entities',
+                        MaxBatchDurationInMinutes: $scope.scopeModel.maxBatchDurationInMinutes,
+                        DurationMargin: $scope.scopeModel.maxBatchDurationInMinutes,
+                        AttemptDateTimeMargin: $scope.scopeModel.attemptDateTimeMargin,
+                        AttemptDateTimeOffset: $scope.scopeModel.attemptDateTimeOffset
+                    };
+
+                    return data;
+                };
+
+                if (ctrl.onReady != undefined && typeof (ctrl.onReady) == 'function')
+                    ctrl.onReady(api);
+            }
+        }
+    }]);
