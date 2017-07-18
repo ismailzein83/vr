d892be667b743ce2ee74ecfeede80b4ee478d2d9
Wissam Ajamy <VANRISE\wissam.ajamy>
2017-07-18 09:31:28 +0000

    Update structure from TOneConfiguration
    git-tfs-id: [http://developmentvlan:8585/tfs/vanrise.collection]$/;C42300

---
+++ TOneV2/Code/TOneV2/SQL.TOneConfiguration/VR_AccountBalance/Stored Procedures/sp_LiveBalance_TryUpdateStatus.sql
@@ -0,0 +1,24 @@
+﻿-- =============================================
+-- Author:		<Author,,Name>
+-- Create date: <Create Date,,>
+-- Description:	<Description,,>
+-- =============================================
+create PROCEDURE [VR_AccountBalance].[sp_LiveBalance_TryUpdateStatus] 
+	@AccountID varchar(50),
+	@AccountTypeID uniqueidentifier,
+	@BED datetime ,
+	@EED datetime,
+	@Status int,
+	@IsDeleted bit
+AS
+BEGIN
+IF  EXISTS(select 1 from [VR_AccountBalance].LiveBalance where AccountId = @AccountID and AccountTypeID =@AccountTypeID) 
+	BEGIN
+	UPDATE	[VR_AccountBalance].LiveBalance 
+	SET		BED = @BED,
+			EED = @EED,
+			[Status] =@Status,
+			IsDeleted = @IsDeleted
+	WHERE	 AccountId = @AccountID and AccountTypeID =@AccountTypeID
+	END
+END
