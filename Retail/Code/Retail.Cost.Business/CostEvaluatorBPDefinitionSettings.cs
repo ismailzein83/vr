e197b998c036216431c9a46653e77dc0319227c8
anthony douaihy <VANRISE\anthony.douaihy>
2017-07-19 07:32:34 +0000

    cost evaluator sync
    git-tfs-id: [http://developmentvlan:8585/tfs/vanrise.collection]$/;C42340

---
+++ Retail/Code/Retail.Cost.Business/CostEvaluatorBPDefinitionSettings.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using Retail.Cost.BP.Arguments;
+
+namespace Retail.Cost.Business
+{
+    public class CostEvaluatorBPDefinitionSettings : Vanrise.BusinessProcess.Business.DefaultBPDefinitionExtendedSettings
+    {
+        public override bool CanRunBPInstance(Vanrise.BusinessProcess.Entities.IBPDefinitionCanRunBPInstanceContext context)
+        {
+            foreach (var startedBPInstance in context.GetStartedBPInstances())
+            {
+                CostEvaluatorProcessInput startedBPInstanceCostEvaluatorArg = startedBPInstance.InputArgument as CostEvaluatorProcessInput;
+                if (startedBPInstanceCostEvaluatorArg != null)
+                {
+                    context.Reason = String.Format("Another Cost Evaluator instance is running");
+                    return false;
+                }
+            }
+
+            return true;
+        }
+    }
+}
