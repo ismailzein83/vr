87b3fa29a029a791e1082cdc5e0c2181e8b327cb
Ali Ballouk <VANRISE\ali.ballouk>
2017-07-19 08:19:32 +0000

    Branch Extended info update
    git-tfs-id: [http://developmentvlan:8585/tfs/vanrise.collection]$/;C42343

--- Retail/Code/Retail.MultiNet.Web/Retail_MultiNet/Directives/Account/Part/Runtime/retail-multinet-accounttype-part-runtime-branchextendedinfo.js
+++ Retail/Code/Retail.MultiNet.Web/Retail_MultiNet/Directives/Account/Part/Runtime/retail-multinet-accounttype-part-runtime-branchextendedinfo.js
@@ -30,13 +30,35 @@ app.directive('retailMultinetAccounttypePartRuntimeBranchextendedinfo', ["UtilsS
                 if (payload != undefined && payload.partSettings != undefined) {
                     $scope.scopeModel.branchCode = payload.partSettings.BranchCode;
                     $scope.scopeModel.contractReferenceNumber = payload.partSettings.ContractReferenceNumber;
+                    $scope.scopeModel.cNIC = payload.partSettings.CNIC;
+                    $scope.scopeModel.nTN = payload.partSettings.NTN;
+                    $scope.scopeModel.registrationNumber = payload.partSettings.RegistrationNumber;
+                    $scope.scopeModel.refNumber = payload.partSettings.RefNumber;
+                    $scope.scopeModel.passportNumber = payload.partSettings.PassportNumber;
+                    $scope.scopeModel.assignedNumber = payload.partSettings.AssignedNumber;
+                    $scope.scopeModel.pIN = payload.partSettings.PIN;
+                    $scope.scopeModel.billingAddress = payload.partSettings.BillingAddress;
+                    $scope.scopeModel.technicalAddress = payload.partSettings.TechnicalAddress;
+                    $scope.scopeModel.officeAddress = payload.partSettings.OfficeAddress;
+                    $scope.scopeModel.homeAddress = payload.partSettings.HomeAddress;
                 }
             };
             api.getData = function () {
                 return {
                     $type: 'Retail.MultiNet.Business.MultiNetBranchExtendedInfo, Retail.MultiNet.Business',
                     BranchCode: $scope.scopeModel.branchCode,
-                    ContractReferenceNumber: $scope.scopeModel.contractReferenceNumber
+                    ContractReferenceNumber: $scope.scopeModel.contractReferenceNumber,
+                    CNIC: $scope.scopeModel.cNIC,
+                    NTN: $scope.scopeModel.nTN,
+                    RegistrationNumber: $scope.scopeModel.registrationNumber,
+                    RefNumber: $scope.scopeModel.refNumber,
+                    PassportNumber: $scope.scopeModel.passportNumber,
+                    AssignedNumber: $scope.scopeModel.assignedNumber,
+                    PIN: $scope.scopeModel.pIN,
+                    BillingAddress: $scope.scopeModel.billingAddress,
+                    TechnicalAddress: $scope.scopeModel.technicalAddress,
+                    OfficeAddress: $scope.scopeModel.officeAddress,
+                    HomeAddress: $scope.scopeModel.homeAddress
                 }
             };
             if (ctrl.onReady != null)
