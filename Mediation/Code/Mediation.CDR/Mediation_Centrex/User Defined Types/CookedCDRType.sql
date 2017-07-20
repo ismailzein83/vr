adcfb79a49c83c1c19f43e31663afd84566d16e6
rabih.fashwal:2 <rabih.fashwal:2@vanrise.tfs.local>
2017-07-20 12:13:10 +0000

    Update Mediation CDR DB Schema
    git-tfs-id: [http://developmentvlan:8585/tfs/vanrise.collection]$/;C42400

--- Mediation/Code/Mediation.CDR/Mediation_Centrex/User Defined Types/CookedCDRType.sql
+++ Mediation/Code/Mediation.CDR/Mediation_Centrex/User Defined Types/CookedCDRType.sql
@@ -1,27 +1,31 @@
 ﻿CREATE TYPE [Mediation_Centrex].[CookedCDRType] AS TABLE (
-    [CallId]               NVARCHAR (200)  NULL,
+    [CallId]               NVARCHAR (500)  NULL,
     [ConnectDateTime]      DATETIME        NULL,
     [DisconnectDateTime]   DATETIME        NULL,
     [DisconnectReason]     VARCHAR (200)   NULL,
     [CallProgressState]    VARCHAR (200)   NULL,
     [Account]              VARCHAR (200)   NULL,
-    [OriginatorId]         VARCHAR (200)   NULL,
-    [OriginatorNumber]     VARCHAR (200)   NULL,
-    [OriginatorFromNumber] VARCHAR (200)   NULL,
-    [OriginalDialedNumber] VARCHAR (200)   NULL,
-    [TerminatorId]         VARCHAR (200)   NULL,
-    [TerminatorNumber]     VARCHAR (200)   NULL,
+    [OriginatorId]         VARCHAR (500)   NULL,
+    [OriginatorNumber]     VARCHAR (500)   NULL,
+    [OriginatorFromNumber] VARCHAR (500)   NULL,
+    [OriginalDialedNumber] VARCHAR (500)   NULL,
+    [TerminatorId]         VARCHAR (500)   NULL,
+    [TerminatorNumber]     VARCHAR (500)   NULL,
     [IncomingGwId]         VARCHAR (200)   NULL,
     [OutgoingGwId]         VARCHAR (200)   NULL,
     [TransferredCallId]    VARCHAR (200)   NULL,
-    [DurationInSeconds]    DECIMAL (10, 4) NULL,
+    [DurationInSeconds]    DECIMAL (20, 8) NULL,
     [AttemptDateTime]      DATETIME        NULL,
     [SendCallType]         TINYINT         NULL,
     [ReveiveCallType]      TINYINT         NULL,
     [FileName]             NVARCHAR (255)  NULL,
     [ReplacedCallId]       NVARCHAR (255)  NULL,
-    [OriginatorExtension]  VARCHAR (10)    NULL,
-    [TerminatorExtension]  VARCHAR (10)    NULL);
+    [OriginatorExtension]  VARCHAR (20)    NULL,
+    [TerminatorExtension]  VARCHAR (20)    NULL,
+    [OriginatorIp]         VARCHAR (100)   NULL,
+    [TerminatorIp]         VARCHAR (100)   NULL);
+
+
 
 
 
