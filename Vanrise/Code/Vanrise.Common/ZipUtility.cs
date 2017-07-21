f0e03f9c1bedb1a3fa556fa691f0cfdfdfda6035
Zeinab Mokdad <VANRISE\zeinab.mokdad>
2017-07-21 06:44:27 +0000

    adding reference to icsharp library
    git-tfs-id: [http://developmentvlan:8585/tfs/vanrise.collection]$/;C42410

--- Vanrise/Code/Vanrise.Common/ZipUtility.cs
+++ Vanrise/Code/Vanrise.Common/ZipUtility.cs
@@ -1,12 +1,54 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using ICSharpCode.SharpZipLib.Zip;
+using System;
+using System.IO;
 
 namespace Vanrise.Common
 {
     public class ZipUtility
     {
+        #region public Methods
+        public byte[] UnZip(byte[] p)
+        {
+            byte[] unzippedBytes = null;
+            using (ZipInputStream s = new ZipInputStream(new MemoryStream(p)))
+            {
+                if ((s.GetNextEntry()) != null)
+                {
+                    using (MemoryStream outputStream = new MemoryStream())
+                    {
+                        int size;
+                        byte[] data = new byte[2048];
+                        while (true)
+                        {
+                            size = s.Read(data, 0, data.Length);
+                            if (size > 0)
+                                outputStream.Write(data, 0, size);
+                            else
+                                break;
+                        }
+                        unzippedBytes = outputStream.ToArray();
+                    }
+                }
+            }
+            return unzippedBytes;
+        }
+        public MemoryStream Zip(byte[] bufferBytes, string fileName)
+        {
+            MemoryStream memStream = new MemoryStream();
+            using (ZipOutputStream s = new ZipOutputStream(memStream))
+            {
+                s.SetLevel(9); // highest level of compression
+                ZipEntry entry = new ZipEntry(fileName)
+                {
+                    DateTime = DateTime.Today
+                };
+
+                s.PutNextEntry(entry);
+                s.Write(bufferBytes, 0, bufferBytes.Length);
+            }
+            return memStream;
+        }
+
+        #endregion
     }
 }
