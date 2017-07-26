6031a0a808432bcc87b5b6ea6ac2541dc2ead563
Samer Haidar <VANRISE\samer.haidar>
2017-07-26 11:57:42 +0000

    adding new sps
    git-tfs-id: [http://developmentvlan:8585/tfs/vanrise.collection]$/;C42473

---
+++ TOneV2/Code/TOneV2/SQL.TOneConfiguration/VR_AccountBalance/Stored Procedures/sp_AccountUsage_GetLast.sql
@@ -0,0 +1,17 @@
+﻿-- =============================================
+-- Author:		<Author,,Name>
+-- Create date: <Create Date,,>
+-- Description:	<Description,,>
+-- =============================================
+create PROCEDURE [VR_AccountBalance].[sp_AccountUsage_GetLast]
+	@AccountTypeID uniqueidentifier,
+	@AccountId nvarchar(255)
+AS
+BEGIN
+	SELECT TOP(1) au.ID, au.AccountTypeID,TransactionTypeID, au.AccountID,au.CurrencyId,PeriodStart,PeriodEnd,UsageBalance, IsOverridden, OverriddenAmount, CorrectionProcessID
+	FROM VR_AccountBalance.AccountUsage au WITH(NOLOCK)
+	WHERE  ISNULL(IsOverridden, 0) = 0
+		   AND au.AccountTypeID = @AccountTypeID 
+		   AND au.AccountId= @AccountId
+	ORDER BY PeriodEnd DESC
+END
---
+++ TOneV2/Code/TOneV2/SQL.TOneConfiguration/VR_AccountBalance/Stored Procedures/sp_BillingTransaction_GetLast.sql
@@ -0,0 +1,17 @@
+﻿-- =============================================
+-- Author:		<Author,,Name>
+-- Create date: <Create Date,,>
+-- Description:	<Description,,>
+-- =============================================
+CREATE PROCEDURE [VR_AccountBalance].[sp_BillingTransaction_GetLast]
+	@AccountTypeID uniqueidentifier,
+	@AccountId nvarchar(255)
+AS
+BEGIN
+	SELECT TOP(1) bt.[ID],bt.AccountID,bt.AccountTypeID, TransactionTypeID,Amount,bt.CurrencyId,TransactionTime,Notes,Reference,IsBalanceUpdated,ClosingPeriodId, SourceID, Settings, bt.IsDeleted, IsSubtractedFromBalance
+	FROM VR_AccountBalance.BillingTransaction bt WITH(NOLOCK)
+	WHERE ISNULL(bt.IsDeleted, 0) = 0
+		  AND bt.AccountTypeID = @AccountTypeID 
+		  AND bt.AccountId= @AccountId
+	ORDER BY TransactionTime DESC
+END
