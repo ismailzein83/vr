7258c6751cec09743373edcd198b94d25caf91e2
rabih.fashwal:2 <rabih.fashwal:2@vanrise.tfs.local>
2017-07-27 09:38:26 +0000

    Update BEReceiveDefinitionBPExtentedSettings handle running BEBridge same instance
    git-tfs-id: [http://developmentvlan:8585/tfs/vanrise.collection]$/;C42497

--- Vanrise/Code/Vanrise.BEBridge.Business/BEReceiveDefinitionBPExtentedSettings.cs
+++ Vanrise/Code/Vanrise.BEBridge.Business/BEReceiveDefinitionBPExtentedSettings.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using Vanrise.BEBridge.BP.Arguments;
+using Vanrise.BEBridge.Entities;
 using Vanrise.BusinessProcess.Entities;
 using Vanrise.Common;
 using Vanrise.Security.Entities;
@@ -12,6 +13,29 @@ namespace Vanrise.BEBridge.Business
 {
     public class BEReceiveDefinitionBPExtentedSettings : BPDefinitionExtendedSettings
     {
+        public override bool CanRunBPInstance(IBPDefinitionCanRunBPInstanceContext context)
+        {
+            context.IntanceToRun.ThrowIfNull("context.IntanceToRun");
+            SourceBESyncProcessInput beBridgeProcessInput = context.IntanceToRun.InputArgument.CastWithValidate<SourceBESyncProcessInput>("context.IntanceToRun.InputArgument");
+            foreach (var startedInstance in context.GetStartedBPInstances())
+            {
+                SourceBESyncProcessInput processInput = startedInstance.InputArgument.CastWithValidate<SourceBESyncProcessInput>("context.IntanceToRun.InputArgument");
+                if (processInput != null)
+                {
+                    foreach (var beReceiveDefinitionId in processInput.BEReceiveDefinitionIds)
+                    {
+                        if (beBridgeProcessInput.BEReceiveDefinitionIds.Contains(beReceiveDefinitionId))
+                        {
+                            BEReceiveDefinition beReceiveDefinition = new BEReceiveDefinitionManager().GetBEReceiveDefinition(beReceiveDefinitionId);
+                            context.Reason = String.Format("Another {0} instance is running", beReceiveDefinition.Name);
+                            return false;
+                        }
+                    }
+                }
+            }
+
+            return true;
+        }
         public override RequiredPermissionSettings GetViewInstanceRequiredPermissions(IBPDefinitionGetViewInstanceRequiredPermissionsContext context)
         {
             var sourceBESyncProcessInput = context.InputArg.CastWithValidate<SourceBESyncProcessInput>("context.InputArg");
