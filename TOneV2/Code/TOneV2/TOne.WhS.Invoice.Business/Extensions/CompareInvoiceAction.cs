24e316ae1c7c00203bf6d533298245035ba06a1b
Samer Haidar <VANRISE\samer.haidar>
2017-07-27 10:55:14 +0000

    Adding CompareInvoiceAction
    git-tfs-id: [http://developmentvlan:8585/tfs/vanrise.collection]$/;C42499

---
+++ TOneV2/Code/TOneV2/TOne.WhS.Invoice.Business/Extensions/CompareInvoiceAction.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Vanrise.Invoice.Entities;
+
+namespace TOne.WhS.Invoice.Business.Extensions
+{
+    public class CompareInvoiceAction : InvoiceActionSettings
+    {
+        public override string ActionTypeName { get { return "CompareInvoiceAction"; } }
+        public override Guid ConfigId { get { return new Guid("CA8088B9-2887-4B85-B263-49C895088E66"); } }
+        public Guid ItemGroupingId { get; set; }
+        public Guid ZoneDimensionId { get; set; }
+        public Guid CarrierDimensionId { get; set; }
+        public Guid RateTypeDimensionId { get; set; }
+        public Guid CurrencyDimensionId { get; set; }
+        public Guid RateDimensionId { get; set; }
+       
+        public Guid NumberOfCallsMeasureId { get; set; }
+        public Guid AmountMeasureId { get; set; }
+        public Guid DurationMeasureId { get; set; }
+
+        public string PartnerLabel { get; set; }
+        public string PartnerAbbreviationLabel { get; set; }
+    }
+}
