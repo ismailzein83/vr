59b8fe859bc816aa7352892a5a55987582938ebe
Wissam Ajamy <VANRISE\wissam.ajamy>
2017-08-03 15:14:22 +0000

    move [queue].[ExecutionFlowDefinition] to configuration project
    git-tfs-id: [http://developmentvlan:8585/tfs/vanrise.collection]$/;C42612

--- TOneV2/Code/TOneV2/SQL.TOneTransaction/CDRAnalysis.PostDeployment.sql
+++ TOneV2/Code/TOneV2/SQL.TOneTransaction/CDRAnalysis.PostDeployment.sql
@@ -9,28 +9,7 @@ Post-Deployment Script Template
                SELECT * FROM [$(TableName)]					
 --------------------------------------------------------------------------------------
 */
---[queue].[ExecutionFlowDefinition]-----------------------------------------------------------------
-----------------------------------------------------------------------------------------------------
-set nocount on;
-set identity_insert [queue].[ExecutionFlowDefinition] on;
-;with cte_data([ID],[Name],[Title],[Stages])
-as (select * from (values
---//////////////////////////////////////////////////////////////////////////////////////////////////
-(1, N'NormalCDRImportDefinition', N'Normal CDR Import Definition', N'{"$type":"System.Collections.Generic.List`1[[Vanrise.Queueing.Entities.QueueExecutionFlowStage, Vanrise.Queueing.Entities]], mscorlib","$values":[{"$type":"Vanrise.Queueing.Entities.QueueExecutionFlowStage, Vanrise.Queueing.Entities","StageName":"Map Normal CDRs","QueueNameTemplate":"Queue_#FlowId#_#StageName#","QueueTitleTemplate":"#StageName# Queue (#FlowName#)","QueueItemType":{"$type":"Vanrise.Fzero.CDRImport.QueueActivators.NormalCDRFlowStageItemType, Vanrise.Fzero.CDRImport.QueueActivators"},"QueueActivator":{"$type":"Vanrise.Queueing.Entities.CustomQueueActivator, Vanrise.Queueing.Entities","FQTN":"Vanrise.Fzero.CDRImport.QueueActivators.NormalizeCDRActivator, Vanrise.Fzero.CDRImport.QueueActivators","ConfigId":3}},{"$type":"Vanrise.Queueing.Entities.QueueExecutionFlowStage, Vanrise.Queueing.Entities","StageName":"Save Normal CDRs","QueueNameTemplate":"Queue_#FlowId#_#StageName#","QueueTitleTemplate":"#StageName# Queue (#FlowName#)","QueueItemType":{"$type":"Vanrise.Fzero.CDRImport.QueueActivators.NormalCDRFlowStageItemType, Vanrise.Fzero.CDRImport.QueueActivators"},"QueueActivator":{"$type":"Vanrise.Queueing.Entities.CustomQueueActivator, Vanrise.Queueing.Entities","FQTN":"Vanrise.Fzero.CDRImport.QueueActivators.SaveCDRActivator, Vanrise.Fzero.CDRImport.QueueActivators","ConfigId":3}}]}')
---\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\
-)c([ID],[Name],[Title],[Stages]))
-merge	[queue].[ExecutionFlowDefinition] as t
-using	cte_data as s
-on		1=1 and t.[ID] = s.[ID]
-when matched then
-	update set
-	[Name] = s.[Name],[Title] = s.[Title],[Stages] = s.[Stages]
-when not matched by target then
-	insert([ID],[Name],[Title],[Stages])
-	values(s.[ID],s.[Name],s.[Title],s.[Stages])
-when not matched by source then
-	delete;
-set identity_insert [queue].[ExecutionFlowDefinition] off;
+
 
 
 
