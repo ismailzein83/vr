4f6e72380e0bf8f6c19490fe18ab2abe25b82771
Rodi Hassan <VANRISE\rodi.hassan>
2017-08-04 11:16:49 +0000

    Adding Rodi Task
    git-tfs-id: [http://developmentvlan:8585/tfs/vanrise.collection]$/;C42630

---
+++ TOneV2/Code/TOneV2/TestRuntime/Tasks/RodiTask.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TestRuntime;
+using Vanrise.BusinessProcess;
+using Vanrise.Runtime;
+
+namespace TOne.WhS.Runtime.Tasks
+{
+    public class RodiTask : ITask
+    {
+        public void Execute()
+        {
+            #region Runtime
+            var runtimeServices = new List<RuntimeService>();
+
+            BusinessProcessService bpService = new BusinessProcessService() { Interval = new TimeSpan(0, 0, 2) };
+            runtimeServices.Add(bpService);
+
+            BPRegulatorRuntimeService bpRegulatorRuntimeService = new BPRegulatorRuntimeService { Interval = new TimeSpan(0, 0, 2) };
+            runtimeServices.Add(bpRegulatorRuntimeService);
+
+            RuntimeHost host = new RuntimeHost(runtimeServices);
+            host.Start();
+            Console.ReadKey();
+            #endregion
+        }
+    }
+}
