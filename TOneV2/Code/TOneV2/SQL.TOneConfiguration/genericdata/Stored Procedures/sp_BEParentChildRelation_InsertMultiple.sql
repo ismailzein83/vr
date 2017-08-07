d749b0882d0c63d37e0bd7869a785c1cdfa0196b
Ali Atoui <VANRISE\ali.atoui>
2017-08-07 11:04:40 +0000

    Adjust BEParentChildRelation Scripts
    git-tfs-id: [http://developmentvlan:8585/tfs/vanrise.collection]$/;C42654

---
+++ TOneV2/Code/TOneV2/SQL.TOneConfiguration/genericdata/Stored Procedures/sp_BEParentChildRelation_InsertMultiple.sql
@@ -0,0 +1,9 @@
+﻿
+Create PROCEDURE [genericdata].[sp_BEParentChildRelation_InsertMultiple]
+	@BEParentChildRelations [genericdata].[BEParentChildRelationType] Readonly
+AS
+BEGIN
+	INSERT [genericdata].[BEParentChildRelation] ([RelationDefinitionID], [ParentBEID], [ChildBEID], [BED], [EED]) 
+	SELECT [RelationDefinitionID], [ParentBEID], [ChildBEID], [BED], [EED]
+	FROM @BEParentChildRelations
+END
---
+++ TOneV2/Code/TOneV2/SQL.TOneConfiguration/genericdata/User Defined Types/BEParentChildRelationType.sql
@@ -0,0 +1,7 @@
+﻿CREATE TYPE [genericdata].[BEParentChildRelationType] AS TABLE (
+    [RelationDefinitionID] UNIQUEIDENTIFIER NOT NULL,
+    [ParentBEID]           VARCHAR (50)     NOT NULL,
+    [ChildBEID]            VARCHAR (50)     NOT NULL,
+    [BED]                  DATETIME         NOT NULL,
+    [EED]                  DATETIME         NULL);
+
