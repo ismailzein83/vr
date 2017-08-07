e4a1940d1e76d8a36867658b8e773d5a1add3883
Ali Atoui <VANRISE\ali.atoui>
2017-08-07 11:01:47 +0000

    Adjust DID Scripts
    git-tfs-id: [http://developmentvlan:8585/tfs/vanrise.collection]$/;C42653

---
+++ Retail/Code/Retail/Retail_BE/Stored Procedures/sp_DID_InsertMultiple.sql
@@ -0,0 +1,9 @@
+﻿
+Create PROCEDURE [Retail_BE].[sp_DID_InsertMultiple]
+	@DIDs [Retail_BE].[DIDType] Readonly
+AS
+BEGIN
+	INSERT [Retail_BE].[DID] ([Settings], [SourceId]) 
+	SELECT [Settings], [SourceId]
+	FROM @DIDs
+END
---
+++ Retail/Code/Retail/Retail_BE/User Defined Types/DIDType.sql
@@ -0,0 +1,4 @@
+﻿CREATE TYPE [Retail_BE].[DIDType] AS TABLE (
+    [Settings] NVARCHAR (MAX) NULL,
+    [SourceId] NVARCHAR (50)  NULL);
+
