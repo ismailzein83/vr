e3a99a438453442d04e92bb12a0868745f6a46c4
mohammad.zhour <mohammad.zhour@vanrise.tfs.local>
2017-08-08 12:11:38 +0000

    Implementing Rule SetRateInPastForSPCondition
    git-tfs-id: [http://developmentvlan:8585/tfs/vanrise.collection]$/;C42689

--- TOneV2/Code/TOneV2/TOne.WhS.Sales.Business/BusinessRules/Conditions/Country/SetRateInPastForSPCondition.cs
+++ TOneV2/Code/TOneV2/TOne.WhS.Sales.Business/BusinessRules/Conditions/Country/SetRateInPastForSPCondition.cs
@@ -18,39 +18,39 @@ namespace TOne.WhS.Sales.Business.BusinessRules.Conditions.Country
 
         public override bool Validate(Vanrise.BusinessProcess.Entities.IBusinessRuleConditionValidateContext context)
         {
-            //IRatePlanContext ratePlanContext = context.GetExtension<IRatePlanContext>();
+            IRatePlanContext ratePlanContext = context.GetExtension<IRatePlanContext>();
 
-            //if (ratePlanContext.OwnerType == SalePriceListOwnerType.Customer || ratePlanContext.IsFirstSellingProductOffer.Value)
-            //    return true;
+            if (ratePlanContext.OwnerType == SalePriceListOwnerType.Customer || ratePlanContext.IsFirstSellingProductOffer.Value)
+                return true;
 
-            //var countryData = context.Target as CountryData;
+            var countryData = context.Target as CountryData;
 
-            //if (countryData.ZoneDataByZoneId != null)
-            //{
-            //    string countryName = new CountryManager().GetCountryName(countryData.CountryId);
-            //    string errorMessage = string.Format("Some of the zones of country '{0}' have rates with a BED that's less than today", countryName);
+            if (countryData.ZoneDataByZoneId != null)
+            {
+                string countryName = new CountryManager().GetCountryName(countryData.CountryId);
+                string errorMessage = string.Format("Some of the zones of country '{0}' have rates with a BED that's less than today", countryName);
 
-            //    foreach (var zoneElement in countryData.ZoneDataByZoneId)
-            //    {
-            //        if (zoneElement.Value.NormalRateToChange != null && zoneElement.Value.NormalRateToChange.BED < DateTime.Today)
-            //        {
-            //            context.Message = errorMessage;
-            //            return false;
-            //        }
+                foreach (var zoneElement in countryData.ZoneDataByZoneId)
+                {
+                    if (zoneElement.Value.NormalRateToChange != null && zoneElement.Value.NormalRateToChange.BED < DateTime.Today)
+                    {
+                        context.Message = errorMessage;
+                        return false;
+                    }
 
-            //        if (zoneElement.Value.OtherRatesToChange != null)
-            //        {
-            //            foreach (var rate in zoneElement.Value.OtherRatesToChange)
-            //            {
-            //                if (rate.BED < DateTime.Today)
-            //                {
-            //                    context.Message = errorMessage;
-            //                    return false;
-            //                }
-            //            }
-            //        }
-            //    }
-            //}
+                    if (zoneElement.Value.OtherRatesToChange != null)
+                    {
+                        foreach (var rate in zoneElement.Value.OtherRatesToChange)
+                        {
+                            if (rate.BED < DateTime.Today)
+                            {
+                                context.Message = errorMessage;
+                                return false;
+                            }
+                        }
+                    }
+                }
+            }
 
             return true;
         }
