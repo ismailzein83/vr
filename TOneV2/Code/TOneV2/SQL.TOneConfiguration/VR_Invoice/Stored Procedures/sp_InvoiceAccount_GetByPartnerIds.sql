29c6ca30cb5a58fe28ef70f9d434259ddbbaf78a
Wissam Ajamy <VANRISE\wissam.ajamy>
2017-08-08 06:37:58 +0000

    Update structure from TOneConfiguration
    git-tfs-id: [http://developmentvlan:8585/tfs/vanrise.collection]$/;C42666

---
+++ TOneV2/Code/TOneV2/SQL.TOneConfiguration/VR_Invoice/Stored Procedures/sp_InvoiceAccount_GetByPartnerIds.sql
@@ -0,0 +1,23 @@
+﻿-- =============================================
+-- Author:		<Author,,Name>
+-- Create date: <Create Date,,>
+-- Description:	<Description,,>
+-- =============================================
+CREATE PROCEDURE [VR_Invoice].sp_InvoiceAccount_GetByPartnerIds
+@InvoiceTypeId uniqueidentifier,
+ @PartnerIds VARCHAR(MAX)
+AS
+BEGIN
+
+   BEGIN
+	    DECLARE @PartnerIdsTable TABLE (PartnerId varchar(50))
+		INSERT INTO @PartnerIdsTable (PartnerId)
+		select ParsedString from [VR_Invoice].[ParseStringList](@PartnerIds)
+
+
+		SELECT	ID,InvoiceTypeId,PartnerId,BED,EED,[Status],IsDeleted
+		FROM	VR_Invoice.InvoiceAccount ia WITH(NOLOCK) 
+		WHERE (@PartnerIds  IS NULL OR ia.PartnerId IN (select PartnerId from @PartnerIdsTable))
+		AND InvoiceTypeId = @InvoiceTypeId
+	END
+END
