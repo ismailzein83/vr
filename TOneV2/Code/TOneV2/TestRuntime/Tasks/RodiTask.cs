bb88f455b8a1fecbe88e67e32daf1ef75bf76b88
Rodi Hassan <VANRISE\rodi.hassan>
2017-08-09 14:29:14 +0000

    Add rodi task
    git-tfs-id: [http://developmentvlan:8585/tfs/vanrise.collection]$/;C42728

--- TOneV2/Code/TOneV2/TestRuntime/Tasks/RodiTask.cs
+++ TOneV2/Code/TOneV2/TestRuntime/Tasks/RodiTask.cs
@@ -5,6 +5,7 @@ using System.Text;
 using System.Threading.Tasks;
 using TestRuntime;
 using Vanrise.BusinessProcess;
+using Vanrise.Queueing;
 using Vanrise.Runtime;
 
 namespace TOne.WhS.Runtime.Tasks
@@ -18,13 +19,23 @@ namespace TOne.WhS.Runtime.Tasks
 
             BusinessProcessService bpService = new BusinessProcessService() { Interval = new TimeSpan(0, 0, 2) };
             runtimeServices.Add(bpService);
+            BPRegulatorRuntimeService bpRegulatorService = new BPRegulatorRuntimeService() { Interval = new TimeSpan(0, 0, 2) };
+            QueueRegulatorRuntimeService queueRegulatorService = new QueueRegulatorRuntimeService() { Interval = new TimeSpan(0, 0, 2) };
+            QueueActivationRuntimeService queueActivationRuntimeService = new QueueActivationRuntimeService { Interval = new TimeSpan(0, 0, 2) };
+            QueueActivationService queueActivationService = new QueueActivationService() { Interval = new TimeSpan(0, 0, 2) };
+            SchedulerService schedulerService = new SchedulerService() { Interval = new TimeSpan(0, 0, 2) };
+            Vanrise.Integration.Business.DataSourceRuntimeService dsRuntimeService = new Vanrise.Integration.Business.DataSourceRuntimeService { Interval = new TimeSpan(0, 0, 2) };
 
-            BPRegulatorRuntimeService bpRegulatorRuntimeService = new BPRegulatorRuntimeService { Interval = new TimeSpan(0, 0, 2) };
-            runtimeServices.Add(bpRegulatorRuntimeService);
+            runtimeServices.Add(queueActivationService);
+            // runtimeServices.Add(schedulerService);
+            runtimeServices.Add(dsRuntimeService);
+            runtimeServices.Add(queueActivationRuntimeService);
+            runtimeServices.Add(bpRegulatorService);
+            runtimeServices.Add(queueRegulatorService);
 
             RuntimeHost host = new RuntimeHost(runtimeServices);
             host.Start();
-            Console.ReadKey();
+
             #endregion
         }
     }
