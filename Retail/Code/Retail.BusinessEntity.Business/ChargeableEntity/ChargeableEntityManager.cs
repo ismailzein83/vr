5f93a1da1659559010b63ee70828d843a9a04e0d
anthony douaihy <VANRISE\anthony.douaihy>
2017-08-11 07:54:42 +0000

    add ChargeableEntityManager
    git-tfs-id: [http://developmentvlan:8585/tfs/vanrise.collection]$/;C42749

---
+++ Retail/Code/Retail.BusinessEntity.Business/ChargeableEntity/ChargeableEntityManager.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using Vanrise.Entities;
+using Vanrise.Common;
+using Vanrise.Common.Business;
+using Retail.BusinessEntity.Entities;
+
+namespace Retail.BusinessEntity.Business
+{
+    public class ChargeableEntityManager
+    {
+        public ChargeableEntitySettings GetChargeableEntitySettings(Guid chargeableEntityId)
+        {
+            GenericLKUPManager genericLKUPManager = new GenericLKUPManager();
+            GenericLKUPItem genericLKUPItem = genericLKUPManager.GetGenericLKUPItem(chargeableEntityId);
+
+            genericLKUPItem.ThrowIfNull("genericLKUPItem", chargeableEntityId);
+            genericLKUPItem.Settings.ThrowIfNull("genericLKUPItem.Settings", chargeableEntityId);
+            genericLKUPItem.Settings.ExtendedSettings.ThrowIfNull("genericLKUPItem.Settings.ExtendedSettings", chargeableEntityId);
+
+            return genericLKUPItem.Settings.ExtendedSettings as ChargeableEntitySettings;
+        }
+    }
+}
