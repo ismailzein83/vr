f6a839a2debdf0141dadf9b1bfa7be484de5b32e
Wissam Ajamy <VANRISE\wissam.ajamy>
2017-08-17 09:18:18 +0000

    update structure from Retail_Dev_CDR
    git-tfs-id: [http://developmentvlan:8585/tfs/vanrise.collection]$/;C42817

--- Retail/Code/RetailEDR/Retail_EDR/Tables/RingoEvent.sql
+++ Retail/Code/RetailEDR/Retail_EDR/Tables/RingoEvent.sql
@@ -2,12 +2,12 @@
     [MSISDN]                 NVARCHAR (100)   NULL,
     [EventIdMvno]            INT              NULL,
     [EventId]                INT              NULL,
-    [Event]                  NVARCHAR (100)   NULL,
-    [Parameters]             NVARCHAR (100)   NULL,
+    [Event]                  VARCHAR (100)    NULL,
+    [Parameters]             VARCHAR (500)    NULL,
     [CreatedDate]            DATETIME         NULL,
     [AccountId]              BIGINT           NULL,
-    [FileName]               NVARCHAR (200)   NULL,
-    [PromotionCode]          NVARCHAR (200)   NULL,
+    [FileName]               VARCHAR (200)    NULL,
+    [PromotionCode]          VARCHAR (200)    NULL,
     [PromotionId]            INT              NULL,
     [ActivationDate]         DATETIME         NULL,
     [CustomerActivationDate] DATETIME         NULL,
@@ -15,6 +15,8 @@
     [Amount]                 INT              NULL,
     [Balance]                DECIMAL (20, 10) NULL
 );
+
+
 
 
 
