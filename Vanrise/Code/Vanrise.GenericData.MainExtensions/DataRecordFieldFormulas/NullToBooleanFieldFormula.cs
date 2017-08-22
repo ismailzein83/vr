b42bdddf85f1c3543157b58296349c2ec7a9cf0b
Ali Atoui <VANRISE\ali.atoui>
2017-08-22 12:21:39 +0000

    Adding NullToBooleanFieldFormula
    git-tfs-id: [http://developmentvlan:8585/tfs/vanrise.collection]$/;C42895

---
+++ Vanrise/Code/Vanrise.GenericData.MainExtensions/DataRecordFieldFormulas/NullToBooleanFieldFormula.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using Vanrise.GenericData.Entities;
+
+namespace Vanrise.GenericData.MainExtensions.DataRecordFieldFormulas
+{
+    public class NullToBooleanFieldFormula : DataRecordFieldFormula
+    {
+        public override Guid ConfigId { get { return new Guid("458EBD88-4537-4B57-B2AB-14AFAB2EF9B6"); } }
+
+        public string NullableFieldFieldName { get; set; }
+
+        public bool NullIsFalse { get; set; }
+
+        public override List<string> GetDependentFields(IDataRecordFieldFormulaGetDependentFieldsContext context)
+        {
+            return new List<string>() { NullableFieldFieldName };
+        }
+
+        public override dynamic CalculateValue(IDataRecordFieldFormulaCalculateValueContext context)
+        {
+            dynamic nullableField = context.GetFieldValue(NullableFieldFieldName);
+
+            if (NullIsFalse)
+                return nullableField == null ? false : true;
+            else
+                return nullableField == null ? true : false;
+        }
+
+        public override RecordFilter ConvertFilter(IDataRecordFieldFormulaConvertFilterContext context)
+        {
+            if (context.InitialFilter == null)
+                throw new ArgumentNullException("context.InitialFilter");
+
+            ObjectListRecordFilter objectListFilter = context.InitialFilter as ObjectListRecordFilter;
+            if (objectListFilter != null)
+            {
+                bool booleanFieldValue = Convert.ToBoolean(objectListFilter.Values.First());
+
+                if((NullIsFalse && !booleanFieldValue) || (!NullIsFalse && booleanFieldValue))
+                    return new EmptyRecordFilter() { FieldName = NullableFieldFieldName };
+                else
+                    return new NonEmptyRecordFilter() { FieldName = NullableFieldFieldName }; 
+            }
+
+            //EmptyRecordFilter emptyFilter = context.InitialFilter as EmptyRecordFilter;
+            //if (emptyFilter != null)
+            //    return new AlwaysFalseRecordFilter() { };
+
+            NonEmptyRecordFilter nonEmptyFilter = context.InitialFilter as NonEmptyRecordFilter;
+            if (nonEmptyFilter != null)
+                return null;
+
+            throw new Exception(String.Format("Invalid Record Filter '{0}'", context.InitialFilter.GetType()));
+        }
+    }
+}
---
+++ Vanrise/Code/Vanrise.GenericData.Web/VR_GenericData/Directives/MainExtensions/DataRecordFieldFormulas/vr-genericdata-datarecordtypefields-formula-nulltoboolean.js
@@ -0,0 +1,75 @@
+﻿'use strict';
+
+app.directive('vrGenericdataDatarecordtypefieldsFormulaNulltoboolean', ['UtilsService',
+    function (UtilsService) {
+
+        var directiveDefinitionObject = {
+            restrict: 'E',
+            scope: {
+                onReady: '='
+            },
+            controller: function ($scope, $element, $attrs) {
+
+                var ctrl = this;
+
+                var ctor = new textTypeCtor(ctrl, $scope);
+                ctor.initializeController();
+            },
+            controllerAs: 'ctrl',
+            bindToController: true,
+            compile: function (element, attrs) {
+                return {
+                    pre: function ($scope, iElem, iAttrs, ctrl) {
+
+                    }
+                };
+            },
+            templateUrl: function (element, attrs) {
+                return '/Client/Modules/VR_GenericData/Directives/MainExtensions/DataRecordFieldFormulas/Templates/NullToBooleanFieldFormulaTemplate.html';
+            }
+
+        };
+
+        function textTypeCtor(ctrl, $scope) {
+
+            var context;
+
+            function initializeController() {
+                $scope.fields = [];
+
+                defineAPI();
+            }
+
+            function defineAPI() {
+                var api = {};
+
+                api.load = function (payload) {
+
+                    if (payload != undefined) {
+                        var context = payload.context;
+                        if (context != undefined && context.getFields != undefined) {
+                            $scope.scopeModel.fields = context.getFields();
+                        }
+
+                        if (payload.formula != undefined) {
+                            $scope.scopeModel.selectedNullableFieldFieldName = UtilsService.getItemByVal($scope.scopeModel.fields, payload.formula.NullableFieldFieldName, "fieldName");
+                        }
+                    }
+                };
+
+                api.getData = function () {
+                    return {
+                        $type: "Vanrise.GenericData.MainExtensions.DataRecordFieldFormulas.NullToBooleanFieldFormula, Vanrise.GenericData.MainExtensions",
+                        NullableFieldFieldName: $scope.selectedFieldName != undefined ? $scope.selectedFieldName.fieldName : undefined
+                    };
+                };
+
+                if (ctrl.onReady != null)
+                    ctrl.onReady(api);
+            }
+
+            this.initializeController = initializeController;
+        }
+        return directiveDefinitionObject;
+    }
+]);
