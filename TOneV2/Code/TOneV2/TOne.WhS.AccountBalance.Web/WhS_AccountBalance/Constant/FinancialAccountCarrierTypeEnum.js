6f41e588f4bdc7885451622c57214ceb7051cc73
Samer Haidar <VANRISE\samer.haidar>
2017-08-23 09:38:13 +0000

    remove not needed projects
    git-tfs-id: [http://developmentvlan:8585/tfs/vanrise.collection]$/;C42910

--- TOneV2/Code/TOneV2/TOne.WhS.AccountBalance.Web/WhS_AccountBalance/Constant/FinancialAccountCarrierTypeEnum.js
+++
@@ -1,4 +0,0 @@
-﻿app.constant('WhS_AccountBalance_FinancialAccountCarrierTypeEnum', {
-    Profile: { value: 0, description: 'Profile', prefix: 'Prof' },
-    Account: { value: 1, description: 'Account', prefix: 'Acc' }
-});
--- TOneV2/Code/TOneV2/TOne.WhS.AccountBalance.Web/WhS_AccountBalance/Constant/FinancialAccountPropertyEnum.js
+++
@@ -1,3 +0,0 @@
-﻿app.constant('WhS_AccountBalance_FinancialAccountPropertyEnum', {
-    FinancialAccountId: { value: 0, description: "Financial Account Id" },
-});
