b3476c4b7fb76871f6f073ad9c1be5c9ebaba84d
Ali Atoui <VANRISE\ali.atoui>
2017-08-23 08:33:57 +0000

    Arranging DateTimeFieldTypeRuleFilterEditor
    git-tfs-id: [http://developmentvlan:8585/tfs/vanrise.collection]$/;C42906

--- Vanrise/Code/Vanrise.GenericData.Web/VR_GenericData/Directives/MainExtensions/FieldType/DateTime/vr-genericdata-fieldtype-datetime-rulefiltereditor.js
+++ Vanrise/Code/Vanrise.GenericData.Web/VR_GenericData/Directives/MainExtensions/FieldType/DateTime/vr-genericdata-fieldtype-datetime-rulefiltereditor.js
@@ -1,5 +1,6 @@
 ﻿'use strict';
-app.directive('vrGenericdataFieldtypeDatetimeRulefiltereditor', ['VR_GenericData_DateTimeRecordFilterOperatorEnum', 'UtilsService','VRUIUtilsService',
+
+app.directive('vrGenericdataFieldtypeDatetimeRulefiltereditor', ['VR_GenericData_DateTimeRecordFilterOperatorEnum', 'UtilsService', 'VRUIUtilsService',
     function (VR_GenericData_DateTimeRecordFilterOperatorEnum, UtilsService, VRUIUtilsService) {
 
         var directiveDefinitionObject = {
@@ -18,71 +19,80 @@ app.directive('vrGenericdataFieldtypeDatetimeRulefiltereditor', ['VR_GenericData
 
             },
             templateUrl: "/Client/Modules/VR_GenericData/Directives/MainExtensions/FieldType/DateTime/Templates/DateTimeFieldTypeRuleFilterEditor.html"
-
         };
 
         function recordTypeFieldItemEditorCtor(ctrl, $scope, $attrs) {
-            var dateFilterEditorApi;
-            var dateFilterReadyDeferred = UtilsService.createPromiseDeferred();
+            this.initializeController = initializeController;
 
             var selectedObj;
             var filterObj;
 
+            var dateFilterEditorAPI;
+            var dateFilterReadyDeferred = UtilsService.createPromiseDeferred();
+
+
             function initializeController() {
+
+                $scope.onDateFilterEditorReady = function (api) {
+                    dateFilterEditorAPI = api;
+                    dateFilterReadyDeferred.resolve();
+                };
+
                 defineAPI();
             }
-
-            $scope.onDateFilterEditorReady = function (api) {
-                dateFilterEditorApi = api;
-                dateFilterReadyDeferred.resolve();
-            };
 
             function defineAPI() {
                 var api = {};
 
                 api.load = function (payload) {
+
                     $scope.filters = UtilsService.getArrayEnum(VR_GenericData_DateTimeRecordFilterOperatorEnum);
                     $scope.selectedFilter = $scope.filters[0];
 
                     if (payload) {
                         selectedObj = payload.dataRecordTypeField;
+
                         if (payload.filterObj) {
                             $scope.selectedFilter = UtilsService.getItemByVal($scope.filters, payload.filterObj.CompareOperator, 'value');
                             filterObj = payload.filterObj;
                         }
+
                         var promises = [];
 
+                        //Loading DateFilter Directive
                         var dateFilterLoadDeferred = UtilsService.createPromiseDeferred();
-
                         dateFilterReadyDeferred.promise.then(function () {
-                            var payload = { fieldType: selectedObj.Type, fieldValue: filterObj != undefined ? filterObj.Value : null, fieldTitle: 'Date' };
-                            VRUIUtilsService.callDirectiveLoad(dateFilterEditorApi, payload, dateFilterLoadDeferred);
+
+                            var payload = {
+                                fieldType: selectedObj.Type,
+                                fieldValue: filterObj != undefined ? filterObj.Value : null,
+                                fieldTitle: 'Date'
+                            };
+                            VRUIUtilsService.callDirectiveLoad(dateFilterEditorAPI, payload, dateFilterLoadDeferred);
                         });
                         promises.push(dateFilterLoadDeferred.promise);
+
                         return UtilsService.waitMultiplePromises(promises);
                     }
-
                 };
 
                 api.getData = function () {
                     return {
                         $type: "Vanrise.GenericData.Entities.DateTimeRecordFilter, Vanrise.GenericData.Entities",
                         CompareOperator: $scope.selectedFilter.value,
-                        Value: dateFilterEditorApi.getData()
+                        Value: dateFilterEditorAPI.getData()
                     };
                 };
 
                 api.getExpression = function () {
-                    return $scope.selectedFilter.description + ' ' + dateFilterEditorApi.getData();
+                    return $scope.selectedFilter.description + ' ' + dateFilterEditorAPI.getData();
                 };
 
                 if (ctrl.onReady != null)
                     ctrl.onReady(api);
             }
-
-            this.initializeController = initializeController;
-
         }
+
         return directiveDefinitionObject;
     }]);
 
