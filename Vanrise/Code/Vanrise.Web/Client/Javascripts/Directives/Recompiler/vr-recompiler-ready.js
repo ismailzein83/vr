f3fa8c66f187d87e4100c05b573125d19f4814b7
Ismail Zein <VANRISE\ismail.zein>
2017-08-24 06:15:32 +0000

    added vrRecompilerReady directive
    git-tfs-id: [http://developmentvlan:8585/tfs/vanrise.collection]$/;C42926

---
+++ Vanrise/Code/Vanrise.Web/Client/Javascripts/Directives/Recompiler/vr-recompiler-ready.js
@@ -0,0 +1,19 @@
+﻿app.directive('vrRecompilerReady', ['$compile', '$parse', function ($compile, $parse) {
+
+    var directiveDefinitionObject = {
+        restrict: 'A',
+        scope: false,
+        link: function preLink(scope, iElement, iAttrs) {
+            var api = {
+                recompile: function () {
+                    $compile(iElement)(scope);
+                }
+            };
+            var onReadyMethod = $parse(iAttrs.vrRecompilerReady);
+            onReadyMethod(scope, { api: api });
+        }
+
+    };
+    return directiveDefinitionObject;
+
+}]);
