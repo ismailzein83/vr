db8db34ffbb519560cdd206680f355a9f11a1b4b
Ali Atoui <VANRISE\ali.atoui>
2017-08-29 07:42:41 +0000

    Adding DateTimeRoundFieldFormula
    git-tfs-id: [http://developmentvlan:8585/tfs/vanrise.collection]$/;C42982

---
+++ Vanrise/Code/Vanrise.GenericData.MainExtensions/DataRecordFieldFormulas/DateTimeRoundFieldFormula.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Vanrise.GenericData.Entities;
+
+namespace Vanrise.GenericData.MainExtensions.DataRecordFieldFormulas
+{
+    public class DateTimeRoundFieldFormula : DataRecordFieldFormula
+    {
+        public override Guid ConfigId { get { return new Guid("9A9E268C-0DC3-4488-8F11-CBEFF8D70E1D"); } }
+
+        public string DateTimeFieldName { get; set; }
+
+        public DateTimeRecordFilterComparisonPart ComparisonPart { get; set; } 
+        
+        public int? RoundingIntervalInMinutes { get; set; } 
+
+        public override List<string> GetDependentFields(IDataRecordFieldFormulaGetDependentFieldsContext context)
+        {
+            return new List<string>() { DateTimeFieldName };
+        }
+
+        public override dynamic CalculateValue(IDataRecordFieldFormulaCalculateValueContext context)
+        {
+            throw new NotImplementedException();
+        }
+
+        public override RecordFilter ConvertFilter(IDataRecordFieldFormulaConvertFilterContext context)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ Vanrise/Code/Vanrise.GenericData.Web/VR_GenericData/Directives/MainExtensions/DataRecordFieldFormulas/vr-genericdata-datarecordtypefields-formula-datetimeround.js
@@ -0,0 +1,73 @@
+﻿'use strict';
+
+app.directive('vrGenericdataDatarecordtypefieldsFormulaDatetimeround', ['UtilsService',
+    function (UtilsService) {
+
+        var directiveDefinitionObject = {
+            restrict: 'E',
+            scope: {
+                onReady: '='
+            },
+            controller: function ($scope, $element, $attrs) {
+                var ctrl = this;
+                var ctor = new dateTimeRoundCtor(ctrl, $scope);
+                ctor.initializeController();
+            },
+            controllerAs: 'ctrl',
+            bindToController: true,
+            compile: function (element, attrs) {
+                return {
+                    pre: function ($scope, iElem, iAttrs, ctrl) {
+
+                    }
+                };
+            },
+            templateUrl: function (element, attrs) {
+                return '/Client/Modules/VR_GenericData/Directives/MainExtensions/DataRecordFieldFormulas/Templates/DateTimeRoundFieldFormulaTemplate.html';
+            }
+        };
+
+        function dateTimeRoundCtor(ctrl, $scope) {
+            this.initializeController = initializeController;
+
+            var context;
+
+            function initializeController() {
+                $scope.scopeModel = {};
+                $scope.scopeModel.fields = [];
+
+                defineAPI();
+            }
+
+            function defineAPI() {
+                var api = {};
+
+                api.load = function (payload) {
+
+                    if (payload != undefined) {
+                        var context = payload.context;
+                        if (context != undefined && context.getFields != undefined) {
+                            $scope.scopeModel.fields = context.getFields();
+                        }
+
+                        if (payload.formula != undefined) {
+                            $scope.scopeModel.selectedDateTimeFieldName = UtilsService.getItemByVal($scope.scopeModel.fields, payload.formula.DateTimeFieldName, "fieldName");
+                        }
+                    }
+                };
+
+                api.getData = function () {
+                    return {
+                        $type: "Vanrise.GenericData.MainExtensions.DataRecordFieldFormulas.DateTimeRoundFieldFormula, Vanrise.GenericData.MainExtensions",
+                        DateTimeFieldName: $scope.scopeModel.selectedDateTimeFieldName.fieldName
+                    };
+                };
+
+                if (ctrl.onReady != null)
+                    ctrl.onReady(api);
+            }
+
+        }
+        return directiveDefinitionObject;
+    }
+]);
