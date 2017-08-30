83e3359998f4f23e4a5f24ca99d682a5a54b9efc
Wissam Ajamy <VANRISE\wissam.ajamy>
2017-08-30 12:15:05 +0000

    Update structure from TOneConfiguration
    git-tfs-id: [http://developmentvlan:8585/tfs/vanrise.collection]$/;C43011

---
+++ TOneV2/Code/TOneV2/SQL.TOneConfiguration/VR_Invoice/Stored Procedures/sp_PartnerInvoiceSetting_InsertOrUpdate.sql
@@ -0,0 +1,24 @@
+﻿-- =============================================
+-- Author:		<Author,,Name>
+-- Create date: <Create Date,,>
+-- Description:	<Description,,>
+-- =============================================
+CREATE PROCEDURE [VR_Invoice].[sp_PartnerInvoiceSetting_InsertOrUpdate]
+	@PartnerInvoiceSettingId uniqueidentifier,
+	@PartnerId varchar(50),
+	@InvoiceSettingId uniqueidentifier
+AS
+BEGIN
+IF NOT EXISTS(select 1 from VR_Invoice.PartnerInvoiceSetting where ID = @PartnerInvoiceSettingId) 
+BEGIN
+	Insert into VR_Invoice.PartnerInvoiceSetting(ID,PartnerID,InvoiceSettingID)
+	Values(@PartnerInvoiceSettingId,@PartnerId, @InvoiceSettingId)
+END
+ELSE
+	BEGIN
+	  Update VR_Invoice.PartnerInvoiceSetting
+		Set PartnerId = @PartnerId,
+		    InvoiceSettingId = @InvoiceSettingId
+	    Where   ID = @PartnerInvoiceSettingId
+	END
+END
