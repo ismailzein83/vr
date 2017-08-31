0ddc83e76104cba78275be366a6a083570969173
Ali Atoui <VANRISE\ali.atoui>
2017-08-31 08:05:55 +0000

    Adding SingleMathOperationFieldFormula
    git-tfs-id: [http://developmentvlan:8585/tfs/vanrise.collection]$/;C43030

---
+++ Vanrise/Code/Vanrise.GenericData.MainExtensions/DataRecordFieldFormulas/SingleMathOperationFieldFormula.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Vanrise.GenericData.Entities;
+
+namespace Vanrise.GenericData.MainExtensions.DataRecordFieldFormulas
+{
+    public class SingleMathOperationFieldFormula : DataRecordFieldFormula
+    {
+        public override Guid ConfigId { get { return new Guid("5B8471BA-3D9A-412D-A158-27BDEC9C4094"); } }
+
+        public override List<string> GetDependentFields(IDataRecordFieldFormulaGetDependentFieldsContext context)
+        {
+            throw new NotImplementedException();
+        }
+
+        public override dynamic CalculateValue(IDataRecordFieldFormulaCalculateValueContext context)
+        {
+            throw new NotImplementedException();
+        }
+
+        public override RecordFilter ConvertFilter(IDataRecordFieldFormulaConvertFilterContext context)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
--- Vanrise/Code/Vanrise.Web/Client/Javascripts/Directives/Recompiler/vr-recompiler-ready.js
+++ Vanrise/Code/Vanrise.Web/Client/Javascripts/Directives/Recompiler/vr-recompiler-ready.js
@@ -6,9 +6,35 @@
         link: function preLink(scope, iElement, iAttrs) {
             var api = {
                 recompile: function () {
+                    var originalHtml = iElement[0].innerHTML;
+                    //console.log('before');
+                    //console.log(iElement.html());
+                    //iElement.html('');
+                    //console.log('after empty');
+                    //console.log(iElement.html());
+                    ////$compile(iElement)(scope);
+                    //setTimeout(function () {
+                    //    iElement.html(originalHtml);
+                    //    console.log('after refill');
+                    //    console.log(iElement.html());
+                    //    $compile(iElement)(scope);
+                    //});
+                    //console.log(scope);
+                    //var parentScope = scope.$parent;
+                    //scope.$destroy();
+                    //var newScope = parentScope.$new();
+                    //$compile(iElement)(newScope);
+                    iElement.attr('ng-if', 'false');
                     $compile(iElement)(scope);
+                    setTimeout(function () {
+                        iElement.attr('ng-if', 'true');
+                        iElement[0].innerHTML = originalHtml;
+                        console.log(iElement[0].innerHTML);
+                        $compile(iElement[0])(scope);
+                    });                    
                 }
-            };
+            }; //console.log(iElement);
+            //iElement.attr('ng-if', 'true');
             var onReadyMethod = $parse(iAttrs.vrRecompilerReady);
             onReadyMethod(scope, { api: api });
         }
