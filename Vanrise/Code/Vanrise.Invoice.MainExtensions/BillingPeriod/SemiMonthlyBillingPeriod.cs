9bc73ee52ed5bb98b709e8b8f79ae9861427638f
Samer Haidar <VANRISE\samer.haidar>
2017-09-05 09:06:16 +0000

    Implementing semimonthly
    git-tfs-id: [http://developmentvlan:8585/tfs/vanrise.collection]$/;C43057

--- Vanrise/Code/Vanrise.Invoice.MainExtensions/BillingPeriod/SemiMonthlyBillingPeriod.cs
+++ Vanrise/Code/Vanrise.Invoice.MainExtensions/BillingPeriod/SemiMonthlyBillingPeriod.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Vanrise.Common;
 using Vanrise.Invoice.Entities;
 
 namespace Vanrise.Invoice.MainExtensions
@@ -13,7 +14,47 @@ namespace Vanrise.Invoice.MainExtensions
 
         public override BillingInterval GetPeriod(IBillingPeriodContext context)
         {
-            throw new NotImplementedException();
+            BillingInterval perviousBillingInterval = new Entities.BillingInterval();
+            if (context.PreviousPeriodEndDate.HasValue)
+            {
+
+                BillingInterval nextBillingInterval = new Entities.BillingInterval();
+                int fromDateDate = context.PreviousPeriodEndDate.Value.Day > 1 ? 16 : 1;
+                int toDateDate = fromDateDate == 1 ? 15 : context.PreviousPeriodEndDate.Value.GetLastDayOfMonth().Day;
+
+                nextBillingInterval.FromDate = new DateTime(context.PreviousPeriodEndDate.Value.Year, context.PreviousPeriodEndDate.Value.Month, fromDateDate);
+                nextBillingInterval.ToDate = new DateTime(context.PreviousPeriodEndDate.Value.Year, context.PreviousPeriodEndDate.Value.Month, toDateDate);
+                if (nextBillingInterval.ToDate > context.IssueDate)
+                {
+                    perviousBillingInterval = GetIntervalIfPreviousPeriodNotValid(context.IssueDate);
+                }
+                else
+                {
+                    perviousBillingInterval.FromDate = nextBillingInterval.FromDate;
+                    perviousBillingInterval.ToDate = nextBillingInterval.ToDate;
+                    while (nextBillingInterval.ToDate <= context.IssueDate)
+                    {
+                        perviousBillingInterval.FromDate = nextBillingInterval.FromDate;
+                        perviousBillingInterval.ToDate = nextBillingInterval.ToDate;
+                        nextBillingInterval.FromDate = nextBillingInterval.ToDate.AddDays(1);
+                        nextBillingInterval.ToDate = nextBillingInterval.FromDate.Day == 1 ? nextBillingInterval.FromDate.AddDays(14) : nextBillingInterval.FromDate.GetLastDayOfMonth();
+                    }
+                }
+            }
+            else
+            {
+                perviousBillingInterval = GetIntervalIfPreviousPeriodNotValid(context.IssueDate);
+            }
+            return perviousBillingInterval;
+        }
+        private BillingInterval GetIntervalIfPreviousPeriodNotValid(DateTime issueDate)
+        {
+            BillingInterval perviousBillingInterval = new BillingInterval();
+            int toDateDate = issueDate.Day > 15 ? 16 : 1;
+            perviousBillingInterval.ToDate = new DateTime(issueDate.Year, issueDate.Month, toDateDate).AddDays(-1);
+            int fromDateDate = toDateDate == 16 ? 1 : 16;
+            perviousBillingInterval.FromDate = new DateTime( perviousBillingInterval.ToDate.Year,perviousBillingInterval.ToDate.Month,fromDateDate);
+            return perviousBillingInterval;
         }
     }
 }
