c54998d757a2e823b6efd3fc014ff896ad0f8676
Rodi Hassan <VANRISE\rodi.hassan>
2017-09-07 08:07:54 +0000

    Adding New Settings directives
    git-tfs-id: [http://developmentvlan:8585/tfs/vanrise.collection]$/;C43105

---
+++ TOneV2/Code/TOneV2/TOne.WhS.BusinessEntity.Web/WhS_BusinessEntity/Directives/Settings/vr-whs-be-pricelistsettings-editor.js
@@ -0,0 +1,202 @@
+﻿'use strict';
+
+app.directive('vrWhsBePricelistsettingsEditor', ['UtilsService', 'VRUIUtilsService',
+    function (UtilsService, VRUIUtilsService) {
+
+        var directiveDefinitionObject = {
+            restrict: 'E',
+            scope: {
+                onReady: '='
+            },
+            controller: function ($scope, $element, $attrs) {
+                var ctrl = this;
+                var ctor = new pricelistSettingEditorCtor(ctrl, $scope, $attrs);
+                ctor.initializeController();
+            },
+            controllerAs: 'ctrl',
+            bindToController: true,
+            compile: function (element, attrs) {
+
+            },
+            templateUrl: "/Client/Modules/WhS_BusinessEntity/Directives/Settings/Templates/PriceListSettingsTemplate.html"
+        };
+
+        function pricelistSettingEditorCtor(ctrl, $scope, $attrs) {
+
+            this.initializeController = initializeController;
+
+            var salePLTemplateSelectorAPI;
+            var salePLTemplateSelectorReadyDeferred = UtilsService.createPromiseDeferred();
+
+            var mailMsgTemplateSelectorAPI;
+            var mailMsgTemplateSelectorReadyDeferred = UtilsService.createPromiseDeferred();
+
+            var priceListExtensionFormatSelectorAPI;
+            var priceListExtensionFormatSelectorReadyDeferred = UtilsService.createPromiseDeferred();
+
+            var priceListTypeSelectorAPI;
+            var priceListTypeSelectorReadyDeferred = UtilsService.createPromiseDeferred();
+
+            function initializeController() {
+
+                ctrl.onPriceListEmailTemplateSelectorReady = function (api) {
+                    mailMsgTemplateSelectorAPI = api;
+                    mailMsgTemplateSelectorReadyDeferred.resolve();
+                };
+
+                ctrl.onPriceListExtensionFormatSelectorReady = function (api) {
+                    priceListExtensionFormatSelectorAPI = api;
+                    priceListExtensionFormatSelectorReadyDeferred.resolve();
+                };
+
+                ctrl.onPriceListTypeSelectorReady = function (api) {
+                    priceListTypeSelectorAPI = api;
+                    priceListTypeSelectorReadyDeferred.resolve();
+                };
+
+                ctrl.onSalePLTemplateSelectorReady = function (api) {
+                    salePLTemplateSelectorAPI = api;
+                    salePLTemplateSelectorReadyDeferred.resolve();
+                };
+
+                defineAPI();
+            }
+
+            function defineAPI() {
+
+                var api = {};
+
+                api.load = function (payload) {
+
+                    var promises = [];
+                    var data;
+                    var directiveContext;
+                    var selectedMailMsgTemplateId;
+                    var selectedSalePLTemplateId;
+                    var selectedPriceListExtensionFormatId;
+                    var pricelistTypeId;
+                    var compressPriceListFile;
+
+                    if (payload != undefined) {
+                        data = payload.data;
+                        directiveContext = payload.directiveContext;
+                    }
+
+                    prepareDirectivesViewForContext(directiveContext);
+                    prepareDirectivesRequiredForContext(directiveContext);
+
+                    if (data != undefined) {
+                        selectedMailMsgTemplateId = data.DefaultSalePLMailTemplateId;
+                        selectedSalePLTemplateId = data.DefaultSalePLTemplateId;
+                        selectedPriceListExtensionFormatId = data.PriceListExtensionFormat;
+                        pricelistTypeId = data.PriceListType;
+                        compressPriceListFile = data.CompressPriceListFile;
+                    }
+
+                    if (ctrl.showCompressPriceListFile) {
+                        ctrl.compressPriceListFile = compressPriceListFile;
+                    }
+
+                    if (ctrl.showPricelistEmailTemplateSelector) {
+                        var loadMailMsgTemplateSelectorPromise = loadMailMsgTemplateSelector(selectedMailMsgTemplateId);
+                        promises.push(loadMailMsgTemplateSelectorPromise);
+                    }
+                    if (ctrl.showPricelistTemplateSelector) {
+                        var loadSalePLTemplateSelectorPromise = loadSalePLTemplateSelector(selectedSalePLTemplateId);
+                        promises.push(loadSalePLTemplateSelectorPromise);
+                    }
+                    if (ctrl.showPricelistExtensionFormatSelector) {
+                        var loadPriceListExtensionFormatSelectorPromise = loadPriceListExtensionFormatSelector(selectedPriceListExtensionFormatId);
+                        promises.push(loadPriceListExtensionFormatSelectorPromise);
+                    }
+                    if (ctrl.showPricelistTypeSelector) {
+                        var loadPriceListTypeSelectorPromise = loadPriceListTypeSelector(pricelistTypeId);
+                        promises.push(loadPriceListTypeSelectorPromise);
+                    }
+
+                    return UtilsService.waitMultiplePromises(promises);
+                };
+
+                api.getData = function () {
+                    return {
+                        DefaultSalePLMailTemplateId: (mailMsgTemplateSelectorAPI != undefined) ? mailMsgTemplateSelectorAPI.getSelectedIds() : undefined,
+                        DefaultSalePLTemplateId: (salePLTemplateSelectorAPI != undefined) ? salePLTemplateSelectorAPI.getSelectedIds() : undefined,
+                        PriceListExtensionFormat: (priceListExtensionFormatSelectorAPI != undefined) ? priceListExtensionFormatSelectorAPI.getSelectedIds() : undefined,
+                        PriceListType: (priceListTypeSelectorAPI != undefined) ? priceListTypeSelectorAPI.getSelectedIds() : undefined,
+                        CompressPriceListFile: ctrl.compressPriceListFile
+                    };
+                };
+
+                if (ctrl.onReady != null)
+                    ctrl.onReady(api);
+            }
+
+            function loadMailMsgTemplateSelector(selectedId) {
+                var mailMsgTemplateSelectorLoadDeferred = UtilsService.createPromiseDeferred();
+                mailMsgTemplateSelectorReadyDeferred.promise.then(function () {
+                    var mailMsgTemplateSelectorPayload = {
+                        selectedIds: selectedId,
+                        filter: {
+                            VRMailMessageTypeId: "f61f0b87-ee5b-4794-8b0f-6c0777006441"
+                        }
+                    };
+                    VRUIUtilsService.callDirectiveLoad(mailMsgTemplateSelectorAPI, mailMsgTemplateSelectorPayload, mailMsgTemplateSelectorLoadDeferred);
+                });
+                return mailMsgTemplateSelectorLoadDeferred.promise;
+            }
+
+            function loadSalePLTemplateSelector(selectedId) {
+                var salePLTemplateSelectorLoadDeferred = UtilsService.createPromiseDeferred();
+                salePLTemplateSelectorReadyDeferred.promise.then(function () {
+                    var salePLTemplateSelectorPayload = {
+                        selectedIds: selectedId
+                    };
+                    VRUIUtilsService.callDirectiveLoad(salePLTemplateSelectorAPI, salePLTemplateSelectorPayload, salePLTemplateSelectorLoadDeferred);
+                });
+                return salePLTemplateSelectorLoadDeferred.promise;
+            }
+
+            function loadPriceListExtensionFormatSelector(selectedIds) {
+                var priceListExtensionFormatSelectorLoadDeferred = UtilsService.createPromiseDeferred();
+                priceListExtensionFormatSelectorReadyDeferred.promise.then(function () {
+                    priceListExtensionFormatSelectorReadyDeferred = undefined;
+                    var priceListExtensionFormatSelectorPayload = {
+                        selectedIds: selectedIds
+                    };
+                    VRUIUtilsService.callDirectiveLoad(priceListExtensionFormatSelectorAPI, priceListExtensionFormatSelectorPayload, priceListExtensionFormatSelectorLoadDeferred);
+                });
+                return priceListExtensionFormatSelectorLoadDeferred.promise;
+            }
+
+            function loadPriceListTypeSelector(selectedIds) {
+                var priceListTypeSelectorLoadDeferred = UtilsService.createPromiseDeferred();
+                priceListTypeSelectorReadyDeferred.promise.then(function () {
+                    priceListTypeSelectorReadyDeferred = undefined;
+                    var priceListTypeSelectorPayload = {
+                        selectedIds: selectedIds
+                    };
+                    VRUIUtilsService.callDirectiveLoad(priceListTypeSelectorAPI, priceListTypeSelectorPayload, priceListTypeSelectorLoadDeferred);
+                });
+                return priceListTypeSelectorLoadDeferred.promise;
+            }
+
+            function prepareDirectivesViewForContext(directiveContext) {
+                ctrl.showPricelistTemplateSelector = (directiveContext == "System");
+                ctrl.showPricelistEmailTemplateSelector = (directiveContext == "System");
+                ctrl.showPricelistExtensionFormatSelector = (directiveContext == "System" || directiveContext == "Customer");
+                ctrl.showPricelistTypeSelector = (directiveContext == "System" || directiveContext == "Customer");
+                ctrl.showCompressPriceListFile = (directiveContext == "System" || directiveContext == "Customer");
+            }
+
+            function prepareDirectivesRequiredForContext(directiveContext) {
+                ctrl.isPricelistTemplateRequired = (directiveContext == "System");
+                ctrl.isPricelistEmailTemplateRequired = (directiveContext == "System");
+                ctrl.isPricelistEmailTemplateRequired = (directiveContext == "System");
+                ctrl.isPricelistTypeRequired = (directiveContext == "System");
+                ctrl.isCompressPriceListFileRequired = (directiveContext == "System");
+            }
+
+        }
+
+        return directiveDefinitionObject;
+    }]);
---
+++ TOneV2/Code/TOneV2/TOne.WhS.BusinessEntity.Web/WhS_BusinessEntity/Directives/Settings/vr-whs-be-pricingsettings-editor.js
@@ -0,0 +1,116 @@
+﻿'use strict';
+
+app.directive('vrWhsBePricingsettingsEditor', ['UtilsService', 'VRCommon_CurrencyAPIService',
+    function (UtilsService, VRCommon_CurrencyAPIService) {
+
+        var directiveDefinitionObject = {
+            restrict: 'E',
+            scope: {
+                onReady: '='
+            },
+            controller: function ($scope, $element, $attrs) {
+                var ctrl = this;
+                var ctor = new pricingSettingEditorCtor(ctrl, $scope, $attrs);
+                ctor.initializeController();
+            },
+            controllerAs: 'ctrl',
+            bindToController: true,
+            compile: function (element, attrs) {
+
+            },
+            templateUrl: "/Client/Modules/WhS_BusinessEntity/Directives/Settings/Templates/PricingSettingsTemplate.html"
+        };
+
+        function pricingSettingEditorCtor(ctrl, $scope, $attrs) {
+
+            this.initializeController = initializeController;
+
+            function initializeController() {
+                defineAPI();
+            }
+
+            function defineAPI() {
+
+                var api = {};
+
+                api.load = function (payload) {
+
+                    var promises = [];
+                    var data;
+                    var directiveContext;
+
+                    if (payload != undefined) {
+                        data = payload.data;
+                        directiveContext = payload.directiveContext;
+                    }
+                    
+                    prepareDirectivesViewForContext(directiveContext);
+                    prepareDirectivesRequiredForContext(directiveContext);
+
+                    promises.push(getSystemCurrency());
+
+                    loadStaticData(data);
+                    
+                    return UtilsService.waitMultiplePromises(promises);
+                };
+
+                api.getData = function () {
+                    return {
+                        DefaultRate: ctrl.defaultRate,
+                        EffectiveDateDayOffset: ctrl.effectiveDateDayOffset,
+                        RetroactiveDayOffset: ctrl.retroactiveDayOffset,
+                        MaximumRate: ctrl.maximumRate,
+                        NewRateDayOffset: ctrl.newRateDayOffset,
+                        IncreasedRateDayOffset: ctrl.increasedRateDayOffset,
+                        DecreasedRateDayOffset: ctrl.decreasedRateDayOffset,
+                    };
+                };
+
+                if (ctrl.onReady != null)
+                    ctrl.onReady(api);
+            }
+
+            function loadStaticData(data) {
+                if (data == undefined)
+                    return;
+
+                ctrl.defaultRate = data.DefaultRate;
+                ctrl.effectiveDateDayOffset = data.EffectiveDateDayOffset;
+                ctrl.retroactiveDayOffset = data.RetroactiveDayOffset;
+                ctrl.maximumRate = data.MaximumRate;
+                ctrl.newRateDayOffset = data.NewRateDayOffset;
+                ctrl.increasedRateDayOffset = data.IncreasedRateDayOffset;
+                ctrl.decreasedRateDayOffset = data.DecreasedRateDayOffset;
+            }
+
+            function getSystemCurrency() {
+                return VRCommon_CurrencyAPIService.GetSystemCurrency().then(function (response) {
+                    if (response != undefined) {
+                        ctrl.systemCurrencySymbol = response.Symbol;
+                    }
+                });
+            }
+
+            function prepareDirectivesViewForContext(directiveContext) {
+                ctrl.showDefaultRate = (directiveContext == "System");
+                ctrl.showMaxSaleRate = (directiveContext == "System");
+                ctrl.showEffectiveDateDayOffset = (directiveContext == "System");
+                ctrl.showRetroactiveDayOffset = (directiveContext == "System");
+                ctrl.showNewRateDayOffset = (directiveContext == "System" || directiveContext == "Customer" || directiveContext=="SellingProduct");
+                ctrl.showIncreasedRateDayOffset = (directiveContext == "System" || directiveContext == "Customer" || directiveContext == "SellingProduct");
+                ctrl.showDecreasedRateDayOffset = (directiveContext == "System" || directiveContext == "Customer" || directiveContext == "SellingProduct");
+            }
+
+            function prepareDirectivesRequiredForContext(directiveContext) {
+                ctrl.isDefaultRateRequired = (directiveContext == "System");
+                ctrl.isMaxSaleRateRequired = (directiveContext == "System");
+                ctrl.isEffectiveDateDayOffset = (directiveContext == "System");
+                ctrl.isRetroactiveDayOffset = (directiveContext == "System");
+                ctrl.isNewRateDayOffset = (directiveContext == "System");
+                ctrl.isIncreasedRateDayOffset = (directiveContext == "System");
+                ctrl.isDecreasedRateDayOffset = (directiveContext == "System");
+            }
+        }
+
+        return directiveDefinitionObject;
+    }]);
