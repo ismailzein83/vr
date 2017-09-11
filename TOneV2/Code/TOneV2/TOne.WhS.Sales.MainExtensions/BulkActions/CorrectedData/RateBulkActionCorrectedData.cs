7e3d9188d71f71a5ab978f1eec9773b6038fb581
mohamad.elhariri <mohamad.elhariri@vanrise.tfs.local>
2017-09-11 12:27:29 +0000

    Added BulkActionCorrectedData classes
    git-tfs-id: [http://developmentvlan:8585/tfs/vanrise.collection]$/;C43168

---
+++ TOneV2/Code/TOneV2/TOne.WhS.Sales.MainExtensions/BulkActions/CorrectedData/RateBulkActionCorrectedData.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TOne.WhS.Sales.Entities;
+
+namespace TOne.WhS.Sales.MainExtensions
+{
+    public class RateBulkActionCorrectedData : BulkActionCorrectedData
+    {
+    }
+}
