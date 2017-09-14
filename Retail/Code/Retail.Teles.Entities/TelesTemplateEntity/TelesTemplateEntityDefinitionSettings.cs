8cc9e8872cd7082b051f00b1782ff0dbe51524fc
Samer Haidar <VANRISE\samer.haidar>
2017-09-14 15:34:07 +0000

    adding teles template entity
    git-tfs-id: [http://developmentvlan:8585/tfs/vanrise.collection]$/;C43217

---
+++ Retail/Code/Retail.Teles.Entities/TelesTemplateEntity/TelesTemplateEntityDefinitionSettings.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Retail.Teles.Entities
+{
+    public class TelesTemplateEntityDefinitionSettings : Vanrise.Entities.GenericLKUPDefinitionExtendedSettings
+    {
+        public static Guid s_configId { get { return new Guid("8C4D41EB-C137-48B2-8EAA-B5428734831D"); } }
+        public override Guid ConfigId
+        {
+            get { return s_configId; }
+        }
+        public override string RuntimeEditor
+        {
+            get
+            {
+                return "retail-teles-telestemplateentitysettings";
+            }
+        }
+    }
+}
