be5b1c0a403a5fd5c0f3616b29c4d19f3e0841f4
rabih.fashwal:2 <rabih.fashwal:2@vanrise.tfs.local>
2017-09-22 10:54:00 +0000

    Add Invoice SPs
    git-tfs-id: [http://developmentvlan:8585/tfs/vanrise.collection]$/;C43347

---
+++ TOneV2/Code/TOneV2/SQL.TOneConfiguration/VR_Invoice/Stored Procedures/sp_Invoice_GetBySerialNumbers.sql
@@ -0,0 +1,35 @@
+﻿-- =============================================
+-- Author:		<Author,,Name>
+-- Create date: <Create Date,,>
+-- Description:	<Description,,>
+-- =============================================
+Create PROCEDURE [VR_Invoice].[sp_Invoice_GetBySerialNumbers]
+	@SerialNumbers nvarchar(max)
+AS
+BEGIN
+
+DECLARE @SerialNumbersTable TABLE (SerialNumber nvarchar(255))
+INSERT INTO @SerialNumbersTable (SerialNumber)
+select  ParsedString from [VR_Invoice].ParseStringList(@SerialNumbers)
+	
+	SELECT	vrIn.ID,
+			vrIn.InvoiceTypeID,
+			vrIn.PartnerID,SerialNumber,
+			vrIn.FromDate,
+			vrIn.ToDate,
+			vrIn.IssueDate,
+			vrIn.DueDate,
+			vrIn.Details,
+			vrIn.PaidDate,
+			vrIn.UserId,
+			vrIn.CreatedTime,
+			vrIn.LockDate,
+			vrIn.Notes,
+			vrIn.SourceId,
+			vrIn.IsAutomatic
+	FROM	VR_Invoice.Invoice vrIn with(nolock)  
+	where	
+			(@SerialNumbers is Null or vrIn.SerialNumber IN (SELECT SerialNumber FROM @SerialNumbersTable)) 
+			and ISNULL( vrIn.IsDeleted,0) = 0 AND ISNULL(vrIn.IsDraft, 0) = 0
+			
+END
---
+++ TOneV2/Code/TOneV2/SQL.TOneConfiguration/VR_Invoice/Stored Procedures/sp_Invoice_UpdateInvoicePaidById.sql
@@ -0,0 +1,15 @@
+﻿
+-- =============================================
+-- Author:		<Author,,Name>
+-- Create date: <Create Date,,>
+-- Description:	<Description,,>
+-- =============================================
+create PROCEDURE [VR_Invoice].[sp_Invoice_UpdateInvoicePaidById]
+		@InvoiceTypeId uniqueidentifier,
+		@InvoiceId bigint,
+		@PaidDate Datetime = null
+AS
+BEGIN
+	UPDATE VR_Invoice.Invoice set PaidDate = @PaidDate
+	where InvoiceTypeID = @InvoiceTypeId and ID = @InvoiceId
+END
