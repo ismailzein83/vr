b60390803cea7e5905b44c7352bdbec1321e58a2
Rodi Hassan <VANRISE\rodi.hassan>
2017-09-22 14:30:09 +0000

    initialize PricelistSettings for carrierAccount in CarrierAccountMigrator
    git-tfs-id: [http://developmentvlan:8585/tfs/vanrise.collection]$/;C43378

--- TOneV2/Code/TOneV2/TOne.WhS.DBSync.Business/Migrators/CarrierAccountMigrator.cs
+++ TOneV2/Code/TOneV2/TOne.WhS.DBSync.Business/Migrators/CarrierAccountMigrator.cs
@@ -57,7 +57,7 @@ namespace TOne.WhS.DBSync.Business
         public override CarrierAccount BuildItemFromSource(SourceCarrierAccount sourceItem)
         {
             CarrierAccountSettings carrierAccountSettings = new CarrierAccountSettings();
-            CarrierAccountCustomerSettings carrierAccountCustomerSettings = new CarrierAccountCustomerSettings();
+            CarrierAccountCustomerSettings carrierAccountCustomerSettings = new CarrierAccountCustomerSettings { PricelistSettings = new PricelistSettings()};
             CarrierAccountSupplierSettings carrierAccountSupplierSettings = new CarrierAccountSupplierSettings();
 
             CarrierProfile carrierProfile = null;
