affcc6ddabcce5eca133abc6673e17b2074464f3
Ali Ballouk <VANRISE\ali.ballouk>
2017-09-22 13:09:58 +0000

    fix pagination settings base on grid initial width
    git-tfs-id: [http://developmentvlan:8585/tfs/vanrise.collection]$/;C43367

--- Vanrise/Code/Vanrise.Web/Client/Javascripts/Directives/Pagination/vr-pagination-directive.js
+++ Vanrise/Code/Vanrise.Web/Client/Javascripts/Directives/Pagination/vr-pagination-directive.js
@@ -9,10 +9,13 @@ app.directive('vrPagination', ['UISettingsService', function (UISettingsService)
             pagersettings: '='
         },
         controller: function ($scope, $element, $attrs) {
+            $scope.$on("$destroy", function () {
+                $(window).unbind('resize', setDefaultSetting);
+            });
             var pagerCtrl = this;
             pagerCtrl.topCounts = [5, 10, 15, 20, 25, 30, 35, 40, 45, 50, 100];
             if (pagerCtrl.pagersettings != undefined) {
-                pagerCtrl.pagersettings.itemsPerPage = parseInt(UISettingsService.getUIParameterValue('GridPageSize') ||  pagerCtrl.topCounts[1]);
+                pagerCtrl.pagersettings.itemsPerPage = parseInt(UISettingsService.getUIParameterValue('GridPageSize') || pagerCtrl.topCounts[1]);
                 pagerCtrl.pagersettings.getPageInfo = function () {
                     var fromRow = (pagerCtrl.pagersettings.currentPage - 1) * pagerCtrl.pagersettings.itemsPerPage + 1;
                     return {
@@ -30,7 +33,35 @@ app.directive('vrPagination', ['UISettingsService', function (UISettingsService)
                     if (pagerCtrl.pagersettings.pageChanged != undefined)
                         pagerCtrl.pagersettings.pageChanged();
                 };
+
+                pagerCtrl.pagersettings.showBoundaryLinks = true;
+                pagerCtrl.pagersettings.showDirectionLinks = true;
+                pagerCtrl.pagersettings.maxSize = 10;
+
+
             }
+            pagerCtrl.setDefaultSetting = function () {
+                setDefaultSetting();
+            };
+            function setDefaultSetting() {
+                setTimeout(function () {
+                    var pagerwidth = $element.parents('vr-datagrid').find('.vr-datagrid').first().width();
+                    if (pagerwidth > 0) {
+                        if (pagerwidth - 250 < 300)
+                            pagerCtrl.pagersettings.maxSize = 5;
+                        if (pagerwidth - 250 < 200) {
+                            pagerCtrl.pagersettings.maxSize = 2;
+                        }
+                        if (pagerwidth - 250 < 150) {
+                            pagerCtrl.pagersettings.showBoundaryLinks = false;
+                            pagerCtrl.pagersettings.showDirectionLinks = false;
+                            pagerCtrl.pagersettings.maxSize = 1;
+                        }
+                    }
+                    console.log(pagerCtrl.pagersettings.maxSize)
+                },100)
+            }
+            $(window).on('resize', setDefaultSetting);
 
 
         },
@@ -40,6 +71,9 @@ app.directive('vrPagination', ['UISettingsService', function (UISettingsService)
             return {
                 pre: function ($scope, iElem, iAttrs, ctrl) {
                     //var ctrl = $scope.ctrl;
+                },
+                post: function ($scope, iElem, iAttrs, ctrl) {
+
                 }
             };
         },
