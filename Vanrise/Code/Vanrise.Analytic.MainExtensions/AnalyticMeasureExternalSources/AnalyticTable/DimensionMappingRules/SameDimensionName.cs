783a63fe39670bd3b4fb7bf842fefff17415ccad
Ismail Zein <VANRISE\ismail.zein>
2017-09-27 11:00:45 +0000

    added SameDimensionNameType to DimensionMappingRules SameDimensionName
    git-tfs-id: [http://developmentvlan:8585/tfs/vanrise.collection]$/;C43466

--- Vanrise/Code/Vanrise.Analytic.MainExtensions/AnalyticMeasureExternalSources/AnalyticTable/DimensionMappingRules/SameDimensionName.cs
+++ Vanrise/Code/Vanrise.Analytic.MainExtensions/AnalyticMeasureExternalSources/AnalyticTable/DimensionMappingRules/SameDimensionName.cs
@@ -6,6 +6,7 @@ using System.Threading.Tasks;
 
 namespace Vanrise.Analytic.MainExtensions.AnalyticMeasureExternalSources.AnalyticTable.DimensionMappingRules
 {
+    public enum SameDimensionNameType { AllDimensions = 0, SpecificDimensions = 1 }
     public class SameDimensionName : DimensionMappingRuleSettings
     {
         public override Guid ConfigId
@@ -13,10 +14,20 @@ namespace Vanrise.Analytic.MainExtensions.AnalyticMeasureExternalSources.Analyti
             get { return new Guid("D8B533F7-3375-4BD3-8D2C-65C137E7BD51"); }
         }
 
+        public SameDimensionNameType Type { get; set; }
+        public List<string> DimensionNames { get; set; }
+
         public override bool TryMapDimension(IDimensionMappingRuleTryMapDimensionContext context)
         {
-            context.MappedDimensionName = context.DimensionName;
-            return true;
+            if (this.Type == SameDimensionNameType.AllDimensions || (this.DimensionNames != null && this.DimensionNames.Contains(context.DimensionName)))
+            {
+                context.MappedDimensionName = context.DimensionName;
+                return true;
+            }
+            else
+            {
+                return false;
+            }
         }
     }
 }
