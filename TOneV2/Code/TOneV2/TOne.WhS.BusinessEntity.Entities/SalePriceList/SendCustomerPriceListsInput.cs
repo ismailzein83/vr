c5d64553a1e8c42379553bf5994e924a720ca82b
mohamad.elhariri <mohamad.elhariri@vanrise.tfs.local>
2017-09-28 11:49:00 +0000

    Added SendCustomerPriceListsInput
    git-tfs-id: [http://developmentvlan:8585/tfs/vanrise.collection]$/;C43504

---
+++ TOneV2/Code/TOneV2/TOne.WhS.BusinessEntity.Entities/SalePriceList/SendCustomerPriceListsInput.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TOne.WhS.BusinessEntity.Entities
+{
+    public class SendCustomerPriceListsInput
+    {
+        public long ProcessInstanceId { get; set; }
+        public bool SelectAll { get; set; }
+        public IEnumerable<int> SelectedPriceListIds { get; set; }
+        public IEnumerable<int> NotSelectedPriceListIds { get; set; }
+        public bool CompressAttachement { get; set; }
+    }
+}
