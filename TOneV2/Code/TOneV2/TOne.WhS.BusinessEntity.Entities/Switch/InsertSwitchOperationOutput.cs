ea682d44572ae0928f404b2bfb531d0c133320d1
Ali Atoui <VANRISE\ali.atoui>
2017-09-28 08:52:15 +0000

    Adding InsertSwitchOperationOutput and UpdateSwitchOperationOutput
    git-tfs-id: [http://developmentvlan:8585/tfs/vanrise.collection]$/;C43496

---
+++ TOneV2/Code/TOneV2/TOne.WhS.BusinessEntity.Entities/Switch/InsertSwitchOperationOutput.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace TOne.WhS.BusinessEntity.Entities
+{
+    public class InsertSwitchOperationOutput : Vanrise.Entities.InsertOperationOutput<SwitchDetail>
+    {
+        public List<string> ValidationMessages { get; set; }
+    }
+}
---
+++ TOneV2/Code/TOneV2/TOne.WhS.BusinessEntity.Entities/Switch/UpdateSwitchOperationOutput.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace TOne.WhS.BusinessEntity.Entities
+{
+    public class UpdateSwitchOperationOutput : Vanrise.Entities.UpdateOperationOutput<SwitchDetail>
+    {
+        public List<string> ValidationMessages { get; set; }
+    }
+}
