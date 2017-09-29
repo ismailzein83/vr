3c49878e65e10b11d11c5cfc0003c909f2d3476e
Samer Haidar <VANRISE\samer.haidar>
2017-09-29 13:57:02 +0000

    Adding downloadFileInvoiceAction
    git-tfs-id: [http://developmentvlan:8585/tfs/vanrise.collection]$/;C43546

---
+++ Vanrise/Code/Vanrise.Invoice.MainExtensions/InvoiceActionSettings/DownloadFileInvoiceAction.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Vanrise.Invoice.Entities;
+
+namespace Vanrise.Invoice.MainExtensions
+{
+    public class DownloadFileInvoiceAction : InvoiceActionSettings
+    {
+        public override Guid ConfigId { get { return new Guid("F36A2C18-9033-4F9E-AB7D-0730F630A81A"); } }
+        public override string ActionTypeName
+        {
+            get { return "DownloadFileInvoiceAction"; }
+        }
+    }
+}
