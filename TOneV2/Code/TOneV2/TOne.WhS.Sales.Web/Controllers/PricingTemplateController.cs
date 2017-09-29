d705c7c453af6b4cc042def155adc907baa5665c
Ali Atoui <VANRISE\ali.atoui>
2017-09-29 14:20:49 +0000

    Adding PricingTemplateController
    git-tfs-id: [http://developmentvlan:8585/tfs/vanrise.collection]$/;C43549

---
+++ TOneV2/Code/TOneV2/TOne.WhS.Sales.Web/Controllers/PricingTemplateController.cs
@@ -0,0 +1,43 @@
+﻿using System.Web.Http;
+using TOne.WhS.Sales.Business;
+using TOne.WhS.Sales.Entities;
+using Vanrise.Entities;
+using Vanrise.Web.Base;
+
+namespace TOne.WhS.Sales.Web.Controllers
+{
+    [JSONWithTypeAttribute]
+    [RoutePrefix(Constants.ROUTE_PREFIX + "PricingTemplate")]
+    public class PricingTemplateController : BaseAPIController
+    {
+        PricingTemplateManager manager = new PricingTemplateManager();
+
+        [HttpPost]
+        [Route("GetFilteredPricingTemplates")]
+        public object GetFilteredPricingTemplates(DataRetrievalInput<PricingTemplateQuery> input)
+        {
+            return GetWebResponse(input, manager.GetFilteredPricingTemplates(input));
+        }
+
+        [HttpGet]
+        [Route("GetPricingTemplate")]
+        public PricingTemplate GetPricingTemplate(int pricingTemplateId)
+        {
+            return manager.GetPricingTemplate(pricingTemplateId);
+        }
+
+        [HttpPost]
+        [Route("AddPricingTemplate")]
+        public InsertOperationOutput<PricingTemplateDetail> AddPricingTemplate(PricingTemplate pricingTemplate)
+        {
+            return manager.AddPricingTemplate(pricingTemplate);
+        }
+
+        [HttpPost]
+        [Route("UpdatePricingTemplate")]
+        public UpdateOperationOutput<PricingTemplateDetail> UpdatePricingTemplate(PricingTemplate pricingTemplate) 
+        {
+            return manager.UpdatePricingTemplate(pricingTemplate);
+        }
+    }
+}
