f13a2a7098a5201afea0e963f7cad740de414cbe
Samer Haidar <VANRISE\samer.haidar>
2017-10-02 11:14:31 +0000

    adding AccountTelesDIDsAndBusinessTrunksView
    git-tfs-id: [http://developmentvlan:8585/tfs/vanrise.collection]$/;C43566

---
+++ Retail/Code/Retail.Teles.Business/AccountTelesDIDsAndBusinessTrunksView.cs
@@ -0,0 +1,37 @@
+﻿    using Retail.BusinessEntity.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Retail.Teles.Business
+{
+    public class AccountTelesDIDsAndBusinessTrunksView : AccountViewDefinitionSettings
+    {
+        public override Guid ConfigId
+        {
+            get { return new Guid("F11DB886-8893-441F-B5A4-3261D43E8C0F"); }
+        }
+
+        public override string RuntimeEditor
+        {
+            get
+            {
+                return "retail-teles-accountTelesdidsandbusinesstrunks-view";
+            }
+            set
+            {
+
+            }
+        }
+
+        public override bool DoesUserHaveAccess(IAccountViewDefinitionCheckAccessContext context)
+        {
+            return base.DoesUserHaveAccess(context);
+        }
+        public Guid VRConnectionId { get; set; }
+    }
+}
+
+
