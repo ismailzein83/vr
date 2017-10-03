fbe681400eda453ee12b97add0e4d2e192e46140
mohammad.zhour <mohammad.zhour@vanrise.tfs.local>
2017-10-03 08:19:08 +0000

    add files
    git-tfs-id: [http://developmentvlan:8585/tfs/vanrise.collection]$/;C43582

---
+++ TOneV2/Code/TOneV2/TOne.WhS.BusinessEntity.Entities/SalePricelistChanges/RoutingProductPreview.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TOne.WhS.BusinessEntity.Entities.SalePricelistChanges
+{
+    public class RoutingProductPreview
+    {
+        public int CountryId { get; set; }
+        public long PriceListId { get; set; }
+        public string ZoneName { get; set; }
+        public long? ZoneId { get; set; }
+        public int RoutingProductId { get; set; }
+
+        public int? RecentRoutingProductId { get; set; }
+
+        public DateTime BED { get; set; }
+
+        public DateTime? EED { get; set; }
+        public int CustomerId { get; set; }
+    }
+}
