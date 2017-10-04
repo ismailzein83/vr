75925be16e7f70636e621a66df6ea747f919ab62
anthony douaihy <VANRISE\anthony.douaihy>
2017-10-04 07:50:38 +0000

    Add InvoiceGenerationProcess BP
    git-tfs-id: [http://developmentvlan:8585/tfs/vanrise.collection]$/;C43599

---
+++ Vanrise/Code/Vanrise.Invoice.BP.Arguments/InvoiceGenerationProcessInput.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Vanrise.Invoice.BP.Arguments
+{
+    public class InvoiceGenerationProcessInput : Vanrise.BusinessProcess.Entities.BaseProcessInputArgument
+    {
+        public override string GetTitle()
+        {
+            return "Invoice Generation Process";
+        }
+    }
+}
