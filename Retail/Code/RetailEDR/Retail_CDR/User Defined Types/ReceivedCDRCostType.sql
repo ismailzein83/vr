85daa93203c3ba50de034ff79afa3203b9aae246
anthony douaihy <VANRISE\anthony.douaihy>
2017-10-12 14:03:49 +0000

    Add ReceivedCDRCost table
    git-tfs-id: [http://developmentvlan:8585/tfs/vanrise.collection]$/;C43795

---
+++ Retail/Code/RetailEDR/Retail_CDR/User Defined Types/ReceivedCDRCostType.sql
@@ -0,0 +1,17 @@
+﻿CREATE TYPE [Retail_CDR].[ReceivedCDRCostType] AS TABLE (
+    [ID]                BIGINT          NULL,
+    [SourceID]          VARCHAR (255)   NULL,
+    [AttemptDateTime]   DATETIME        NULL,
+    [CGPN]              VARCHAR (100)   NULL,
+    [CDPN]              VARCHAR (100)   NULL,
+    [SupplierName]      NVARCHAR (255)  NULL,
+    [DurationInSeconds] DECIMAL (20, 4) NULL,
+    [CurrencyId]        INT             NULL,
+    [Rate]              DECIMAL (20, 8) NULL,
+    [Amount]            DECIMAL (22, 6) NULL,
+    [IsReRate]          BIT             NULL,
+    [FileName]          VARCHAR (255)   NULL,
+    [ExtraFields]       NVARCHAR (MAX)  NULL,
+    [ReceivedCGPN]      VARCHAR (100)   NULL,
+    [ReceivedCDPN]      VARCHAR (100)   NULL);
+
