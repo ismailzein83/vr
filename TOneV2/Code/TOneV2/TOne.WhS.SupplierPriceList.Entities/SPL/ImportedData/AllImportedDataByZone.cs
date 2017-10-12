7ca601f615bce89f257f4af9e2dca96350698f24
mohamad.elhariri <mohamad.elhariri@vanrise.tfs.local>
2017-10-12 09:44:01 +0000

    Added AllImportedDataByZone
    git-tfs-id: [http://developmentvlan:8585/tfs/vanrise.collection]$/;C43773

---
+++ TOneV2/Code/TOneV2/TOne.WhS.SupplierPriceList.Entities/SPL/ImportedData/AllImportedDataByZone.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TOne.WhS.SupplierPriceList.Entities.SPL;
+
+namespace TOne.WhS.SupplierPriceList.Entities
+{
+    public class AllImportedDataByZone
+    {
+        public List<ImportedDataByZone> ImportedDataByZoneList { get; set; }
+    }
+}
