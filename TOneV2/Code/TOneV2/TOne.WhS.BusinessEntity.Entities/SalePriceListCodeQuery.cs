af5268407b76588da58d14ebebfc938a337a5d79
Zeinab Mokdad <VANRISE\zeinab.mokdad>
2017-10-12 08:03:34 +0000

    adding  SalePriceListCodeQuery
    git-tfs-id: [http://developmentvlan:8585/tfs/vanrise.collection]$/;C43762

---
+++ TOneV2/Code/TOneV2/TOne.WhS.BusinessEntity.Entities/SalePriceListCodeQuery.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TOne.WhS.BusinessEntity.Entities
+{
+    public class SalePriceListCodeQuery
+    {
+        public int PriceListId { get; set; }
+        public long ZoneId { get; set; }
+    }
+}
