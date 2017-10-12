2d484a4ba95590bf2645f14fa066e10b51f8593d
Samer Haidar <VANRISE\samer.haidar>
2017-10-12 13:21:02 +0000

    Implementing exclude cdrs part
    git-tfs-id: [http://developmentvlan:8585/tfs/vanrise.collection]$/;C43790

---
+++ Retail/Code/Retail.MultiNet.Business/ExcludeCDRsInvoiceSettingPart.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Vanrise.Invoice.Entities;
+
+namespace Retail.MultiNet.Business
+{
+    public class ExcludeCDRsInvoiceSettingPart : InvoiceSettingPart
+    {
+        public bool ExcludeCDRs { get; set; }
+        public override Guid ConfigId
+        {
+            get { return new Guid("809622AB-15B3-4398-8CB8-1EA96ED90CAB"); }
+        }
+    }
+}
---
+++ Retail/Code/Retail.MultiNet.Web/Retail_MultiNet/Elements/InvoiceType/InvoiceSetting/retail-multinet-invoicesetting-runtime-excludecdrspart.js
@@ -0,0 +1,67 @@
+﻿'use strict';
+app.directive('retailMultinetInvoicesettingRuntimeExcludecdrspart', ['UtilsService', 'VRUIUtilsService',
+    function (UtilsService, VRUIUtilsService) {
+
+        var directiveDefinitionObject = {
+            restrict: 'E',
+            scope:
+            {
+                onReady: '=',
+                normalColNum: '@',
+            },
+            controller: function ($scope, $element, $attrs) {
+
+                var ctrl = this;
+
+                var ctor = new DirectiveCtor(ctrl, $scope);
+                ctor.initializeController();
+
+            },
+            controllerAs: 'ctrl',
+            bindToController: true,
+            compile: function (element, attrs) {
+                return {
+                    pre: function ($scope, iElem, iAttrs, ctrl) {
+
+                    }
+                };
+            },
+            templateUrl: function (element, attrs) {
+                return '/Client/Modules/Retail_MultiNet/Directives/Elements/InvoiceType/InvoiceSetting/Templates/ExcludeCDRsPartTemplate.html';
+            }
+
+        };
+
+        function DirectiveCtor(ctrl, $scope) {
+            var currentContext;
+
+            function initializeController() {
+                $scope.scopeModel = {};
+                defineAPI();
+            }
+
+            function defineAPI() {
+                var api = {};
+
+                api.load = function (payload) {
+                    if (payload != undefined && payload.fieldValue != undefined) {
+                        $scope.scopeModel.excludeCDRs = payload.fieldValue.ExcludeCDRs;
+                    }
+                };
+
+                api.getData = function () {
+                    return {
+                        $type: "Retail.MultiNet.Business.ExcludeCDRsInvoiceSettingPart,Retail.MultiNet.Business",
+                        ExcludeCDRs: $scope.scopeModel.excludeCDRs
+                    };
+                };
+
+                if (ctrl.onReady != null)
+                    ctrl.onReady(api);
+            }
+
+            this.initializeController = initializeController;
+        }
+        return directiveDefinitionObject;
+    }
+]);
