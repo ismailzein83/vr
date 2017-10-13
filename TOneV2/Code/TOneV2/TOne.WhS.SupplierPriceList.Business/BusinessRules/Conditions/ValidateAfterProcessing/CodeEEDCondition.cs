c22d9f7c3126d50efdc7bd48886e79d72e369ee5
mohamad.elhariri <mohamad.elhariri@vanrise.tfs.local>
2017-10-13 08:08:58 +0000

    Formatted the code
    git-tfs-id: [http://developmentvlan:8585/tfs/vanrise.collection]$/;C43819

--- TOneV2/Code/TOneV2/TOne.WhS.SupplierPriceList.Business/BusinessRules/Conditions/ValidateAfterProcessing/CodeEEDCondition.cs
+++ TOneV2/Code/TOneV2/TOne.WhS.SupplierPriceList.Business/BusinessRules/Conditions/ValidateAfterProcessing/CodeEEDCondition.cs
@@ -6,15 +6,12 @@ using Vanrise.Common;
 
 namespace TOne.WhS.SupplierPriceList.Business
 {
-    class CodeEEDCondition : BusinessRuleCondition
+    public class CodeEEDCondition : BusinessRuleCondition
     {
-       
-
         public override bool ShouldValidate(IRuleTarget target)
         {
             return (target as NotImportedCode != null);
         }
-
         public override bool Validate(IBusinessRuleConditionValidateContext context)
         {
             NotImportedCode notImportedCode = context.Target as NotImportedCode;
@@ -23,12 +20,11 @@ namespace TOne.WhS.SupplierPriceList.Business
 
             IImportSPLContext importSplContext = context.GetExtension<IImportSPLContext>();
             var result = !notImportedCode.EED.VRLessThanOrEqual(DateTime.Today.Add(importSplContext.CodeCloseDateOffset));
-            if(result == false)
+            if (result == false)
                 context.Message = string.Format("Code {0} has been closed in a period less than system period", notImportedCode.Code);
 
             return result;
         }
-
         public override string GetMessage(IRuleTarget target)
         {
             return string.Format("Code {0} has been closed in a period less than system period", (target as NotImportedCode).Code);
