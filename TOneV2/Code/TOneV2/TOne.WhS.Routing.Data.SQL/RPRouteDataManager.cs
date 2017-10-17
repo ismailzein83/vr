b64cfccefbf4836d65214c78442c10be3b78d16c
Ali Atoui <VANRISE\ali.atoui>
2017-10-17 10:54:24 +0000

    adjust serialiazation and deserialization methods during product cost generation process
    git-tfs-id: [http://developmentvlan:8585/tfs/vanrise.collection]$/;C43914

--- TOneV2/Code/TOneV2/TOne.WhS.Routing.Data.SQL/RPRouteDataManager.cs
+++ TOneV2/Code/TOneV2/TOne.WhS.Routing.Data.SQL/RPRouteDataManager.cs
@@ -4,22 +4,29 @@ using System.Data;
 using System.Data.SqlClient;
 using System.Linq;
 using System.Text;
-using System.Threading.Tasks;
 using TOne.WhS.Routing.Entities;
+using Vanrise.Common;
 using Vanrise.Data.SQL;
 
 namespace TOne.WhS.Routing.Data.SQL
 {
     public class RPRouteDataManager : RoutingDataManager, IRPRouteDataManager
     {
-        //static RPRouteDataManager()
-        //{
-        //    RPRouteOptionSupplier dummyRPRouteOptionSupplier = new RPRouteOptionSupplier();
-        //    RPRouteOption dummyRPRouteOption = new RPRouteOption();
-        //    RPRouteOptionSupplierZone dummyRPRouteOptionSupplierZone = new RPRouteOptionSupplierZone();
-        //}
-
         readonly string[] columns = { "RoutingProductId", "SaleZoneId", "SaleZoneServices", "ExecutedRuleId", "OptionsDetailsBySupplier", "OptionsByPolicy", "IsBlocked" };
+
+        const char RouteOptionSuppliersSeparator = '|';
+        const char RouteOptionSupplierPropertiesSeparator = '~';
+        const char SupplierZonesSeparator = '#';
+        const string SupplierZonesSeparatorAsString = "#";
+        const char SupplierZonePropertiesSeparator = '$';
+        const char SupplierServicesSeparator = '@';
+        const string SupplierServicesSeparatorAsString = "@";
+
+        const char PolicyRouteOptionsSeparator = '|';
+        const char PolicyRouteOptionPropertiesSeparator = '~';
+        const char RouteOptionsSeparator = '#';
+        const string RouteOptionsSeparatorAsString = "#";
+        const char RouteOptionPropertiesSeparator = '$';
 
         #region Public Methods
 
@@ -30,14 +37,13 @@ namespace TOne.WhS.Routing.Data.SQL
 
         public void WriteRecordToStream(Entities.RPRoute record, object dbApplyStream)
         {
-            string optionsDetailsBySupplier = Vanrise.Common.Serializer.Serialize(record.OptionsDetailsBySupplier, true);
-            string rpOptionsByPolicy = Vanrise.Common.Serializer.Serialize(record.RPOptionsByPolicy, true);
             string saleZoneServices = (record.SaleZoneServiceIds != null && record.SaleZoneServiceIds.Count > 0) ? string.Join(",", record.SaleZoneServiceIds) : null;
-
+            string optionsDetailsBySupplier = this.SerializeOptionsDetailsBySupplier(record.OptionsDetailsBySupplier);
+            string rpOptionsByPolicy = this.SerializeOptionsByPolicy(record.RPOptionsByPolicy);
 
             StreamForBulkInsert streamForBulkInsert = dbApplyStream as StreamForBulkInsert;
             streamForBulkInsert.WriteRecord("{0}^{1}^{2}^{3}^{4}^{5}^{6}", record.RoutingProductId, record.SaleZoneId, saleZoneServices, record.ExecutedRuleId,
-                optionsDetailsBySupplier, rpOptionsByPolicy, record.IsBlocked ? 1 : 0);
+                                                                           optionsDetailsBySupplier, rpOptionsByPolicy, record.IsBlocked ? 1 : 0);
         }
 
         public void ApplyProductRouteForDB(object preparedProductRoute)
@@ -110,7 +116,7 @@ namespace TOne.WhS.Routing.Data.SQL
             if (routeOptionsSerialized == null)
                 return null;
 
-            return Vanrise.Common.Serializer.Deserialize<Dictionary<Guid, IEnumerable<RPRouteOption>>>(routeOptionsSerialized.ToString());
+            return this.DeserializeOptionsByPolicy(routeOptionsSerialized.ToString());
         }
 
         public Dictionary<int, RPRouteOptionSupplier> GetRouteOptionSuppliers(int routingProductId, long saleZoneId)
@@ -124,7 +130,7 @@ namespace TOne.WhS.Routing.Data.SQL
             if (routeOptionSuppliersSerialized == null)
                 return null;
 
-            return Vanrise.Common.Serializer.Deserialize<Dictionary<int, RPRouteOptionSupplier>>(routeOptionSuppliersSerialized.ToString());
+            return this.DeserializeOptionsDetailsBySupplier(routeOptionSuppliersSerialized.ToString());
         }
 
         public void FinalizeProductRoute(Action<string> trackStep, int commandTimeoutInSeconds, int? maxDOP)
@@ -159,8 +165,8 @@ namespace TOne.WhS.Routing.Data.SQL
                 IsBlocked = (bool)reader["IsBlocked"],
                 SaleZoneName = reader["Name"] as string,
                 ExecutedRuleId = (int)reader["ExecutedRuleId"],
-                OptionsDetailsBySupplier = reader["OptionsDetailsBySupplier"] != null ? Vanrise.Common.Serializer.Deserialize<Dictionary<int, RPRouteOptionSupplier>>(reader["OptionsDetailsBySupplier"].ToString()) : null,
-                RPOptionsByPolicy = reader["OptionsByPolicy"] != null ? Vanrise.Common.Serializer.Deserialize<Dictionary<Guid, IEnumerable<RPRouteOption>>>(reader["OptionsByPolicy"].ToString()) : null
+                OptionsDetailsBySupplier = reader["OptionsDetailsBySupplier"] != null ? this.DeserializeOptionsDetailsBySupplier(reader["OptionsDetailsBySupplier"].ToString()) : null,
+                RPOptionsByPolicy = reader["OptionsByPolicy"] != null ? this.DeserializeOptionsByPolicy(reader["OptionsByPolicy"].ToString()) : null
             };
         }
 
@@ -179,6 +185,168 @@ namespace TOne.WhS.Routing.Data.SQL
             }
             dtRPZonesInfo.EndLoadData();
             return dtRPZonesInfo;
+        }
+
+        /// <summary>
+        /// SupplierID1~SZ1#SZ2#...#SZn~SupplierStatus~...~SupplierServiceWeight|SupplierID2~SZ1#SZ2#...#SZn~SupplierStatus~...~SupplierServiceWeight
+        /// SZ1 --> SupplierCode$SupplierZoneId$SupplierRate$SupplierServiceID1@SupplierServiceID2@...@SupplierServiceID1SupplierServiceID1n$IsBlocked$SupplierRateId
+        /// </summary>
+        /// <param name="optionsDetailsBySupplier"></param>
+        /// <returns></returns>
+        private string SerializeOptionsDetailsBySupplier(Dictionary<int, RPRouteOptionSupplier> optionsDetailsBySupplier)
+        {
+            StringBuilder str = new StringBuilder();
+
+            foreach (var routeOptionSupplier in optionsDetailsBySupplier.Values)
+            {
+                if (str.Length > 0)
+                    str.Append(RouteOptionSuppliersSeparator);
+
+                List<string> serializedSupplierZones = new List<string>();
+
+                foreach (var supplierZone in routeOptionSupplier.SupplierZones)
+                {
+                    string exactSupplierServiceIdsAsString = supplierZone.ExactSupplierServiceIds != null ? string.Join(SupplierServicesSeparatorAsString, supplierZone.ExactSupplierServiceIds) : string.Empty;
+                    serializedSupplierZones.Add(string.Format("{1}{0}{2}{0}{3}{0}{4}{0}{5}{0}{6}{0}{7}", SupplierZonePropertiesSeparator, supplierZone.SupplierCode, supplierZone.SupplierZoneId,
+                                                    supplierZone.SupplierRate, exactSupplierServiceIdsAsString, supplierZone.ExecutedRuleId, supplierZone.IsBlocked, supplierZone.SupplierRateId));
+                }
+
+                string serializedSupplierZonesAsString = string.Join(SupplierZonesSeparatorAsString, serializedSupplierZones);
+
+                str.AppendFormat("{1}{0}{2}{0}{3}{0}{4}{0}{5}{0}{6}", RouteOptionSupplierPropertiesSeparator, routeOptionSupplier.SupplierId, serializedSupplierZonesAsString,
+                                    routeOptionSupplier.NumberOfBlockedZones, routeOptionSupplier.NumberOfUnblockedZones, routeOptionSupplier.Percentage, routeOptionSupplier.SupplierServiceWeight);
+            }
+            return str.ToString();
+        }
+
+        public Dictionary<int, RPRouteOptionSupplier> DeserializeOptionsDetailsBySupplier(string serializedOptionsDetailsBySupplier)
+        {
+            if (string.IsNullOrEmpty(serializedOptionsDetailsBySupplier))
+                return null;
+
+            Dictionary<int, RPRouteOptionSupplier> optionsDetailsBySupplier = new Dictionary<int, RPRouteOptionSupplier>();
+
+            string[] lines = serializedOptionsDetailsBySupplier.Split(RouteOptionSuppliersSeparator);
+
+            foreach (var line in lines)
+            {
+                string[] parts = line.Split(RouteOptionSupplierPropertiesSeparator);
+
+                var routeOptionSupplier = new RPRouteOptionSupplier
+                {
+                    SupplierId = int.Parse(parts[0]),
+                    NumberOfBlockedZones = int.Parse(parts[2]),
+                    NumberOfUnblockedZones = int.Parse(parts[3]),
+                    Percentage = !string.IsNullOrEmpty(parts[4]) ? int.Parse(parts[4]) : default(int?),
+                    SupplierServiceWeight = int.Parse(parts[5])
+                };
+
+                string supplierZonesAsString = parts[1];
+                if (!string.IsNullOrEmpty(supplierZonesAsString))
+                {
+                    string[] supplierZones = supplierZonesAsString.Split(SupplierZonesSeparator);
+                    routeOptionSupplier.SupplierZones = new List<RPRouteOptionSupplierZone>();
+
+                    foreach (var supplierZone in supplierZones)
+                    {
+                        string[] supplierZoneParts = supplierZone.Split(SupplierZonePropertiesSeparator);
+
+                        var routeOptionSupplierZone = new RPRouteOptionSupplierZone();
+                        routeOptionSupplierZone.SupplierCode = supplierZoneParts[0];
+                        routeOptionSupplierZone.SupplierZoneId = long.Parse(supplierZoneParts[1]);
+                        routeOptionSupplierZone.SupplierRate = decimal.Parse(supplierZoneParts[2]);
+                        routeOptionSupplierZone.ExecutedRuleId = !string.IsNullOrEmpty(supplierZoneParts[4]) ? int.Parse(supplierZoneParts[4]) : default(int?);
+                        routeOptionSupplierZone.IsBlocked = Boolean.Parse(supplierZoneParts[5]);
+                        routeOptionSupplierZone.SupplierRateId = long.Parse(supplierZoneParts[6]);
+
+                        if (!string.IsNullOrEmpty(supplierZoneParts[3]))
+                            routeOptionSupplierZone.ExactSupplierServiceIds = new HashSet<int>(supplierZoneParts[3].Split(SupplierServicesSeparator).Select(itm => int.Parse(itm)));
+
+                        routeOptionSupplier.SupplierZones.Add(routeOptionSupplierZone);
+                    }
+                }
+
+                optionsDetailsBySupplier.Add(routeOptionSupplier.SupplierId, routeOptionSupplier);
+            }
+
+            return optionsDetailsBySupplier;
+        }
+
+        /// <summary>
+        /// PolicyID1~S1#S2#...#Sn|PolicyID2~S1#S2#...#Sn
+        /// S1 --> SupplierId$SupplierRate$...$SupplierZoneMatchHasClosedRate
+        /// </summary>
+        /// <param name="optionsByPolicy"></param>
+        /// <returns></returns>
+        private string SerializeOptionsByPolicy(Dictionary<Guid, IEnumerable<RPRouteOption>> optionsByPolicy)
+        {
+            StringBuilder str = new StringBuilder();
+            foreach (var kvp in optionsByPolicy)
+            {
+                Guid policyId = kvp.Key;
+                IEnumerable<RPRouteOption> routeOptions = kvp.Value;
+
+                if (str.Length > 0)
+                    str.Append(PolicyRouteOptionsSeparator);
+
+                List<string> serializedRouteOptions = new List<string>();
+
+                foreach (var routeOption in routeOptions)
+                {
+                    serializedRouteOptions.Add(string.Format("{1}{0}{2}{0}{3}{0}{4}{0}{5}{0}{6}{0}{7}{0}{8}", RouteOptionPropertiesSeparator, routeOption.SupplierId, routeOption.SupplierRate, routeOption.Percentage,
+                                        routeOption.OptionWeight, routeOption.SaleZoneId, Convert.ToInt32(routeOption.SupplierStatus), routeOption.SupplierServiceWeight, routeOption.SupplierZoneMatchHasClosedRate));
+                }
+
+                string serializadedRouteOptionsAsString = string.Join(RouteOptionsSeparatorAsString, serializedRouteOptions);
+
+                str.AppendFormat("{1}{0}{2}", PolicyRouteOptionPropertiesSeparator, policyId, serializadedRouteOptionsAsString);
+            }
+            return str.ToString();
+        }
+
+        public Dictionary<Guid, IEnumerable<RPRouteOption>> DeserializeOptionsByPolicy(string serializedOptionsDetailsBySupplier)
+        {
+            Dictionary<Guid, IEnumerable<RPRouteOption>> optionsByPolicy = new Dictionary<Guid, IEnumerable<RPRouteOption>>();
+
+            if (string.IsNullOrEmpty(serializedOptionsDetailsBySupplier))
+                return null;
+
+            string[] lines = serializedOptionsDetailsBySupplier.Split(PolicyRouteOptionsSeparator);
+
+            foreach (var line in lines)
+            {
+                string[] parts = line.Split(PolicyRouteOptionPropertiesSeparator);
+
+                Guid policyId = Guid.Parse(parts[0]);
+                string routeOptionsAsString = parts[1];
+
+                if (!string.IsNullOrEmpty(routeOptionsAsString))
+                {
+                    List<RPRouteOption> routeOptionsList = new List<RPRouteOption>();
+                    string[] routeOptions = routeOptionsAsString.Split(RouteOptionsSeparator);
+
+                    foreach (var routeOption in routeOptions)
+                    {
+                        string[] routeOptionParts = routeOption.Split(RouteOptionPropertiesSeparator);
+
+                        var rpRouteOption = new RPRouteOption();
+                        rpRouteOption.SupplierId = int.Parse(routeOptionParts[0]);
+                        rpRouteOption.SupplierRate = decimal.Parse(routeOptionParts[1]);
+                        rpRouteOption.Percentage = !string.IsNullOrEmpty(routeOptionParts[2]) ? int.Parse(routeOptionParts[2]) : default(int?);
+                        rpRouteOption.OptionWeight = decimal.Parse(routeOptionParts[3]);
+                        rpRouteOption.SaleZoneId = long.Parse(routeOptionParts[4]);
+                        rpRouteOption.SupplierStatus = (SupplierStatus)int.Parse(routeOptionParts[5]);
+                        rpRouteOption.SupplierServiceWeight = int.Parse(routeOptionParts[6]);
+                        rpRouteOption.SupplierZoneMatchHasClosedRate = Boolean.Parse(routeOptionParts[7]);
+
+                        routeOptionsList.Add(rpRouteOption);
+                    }
+
+                    optionsByPolicy.Add(policyId, routeOptionsList);
+                }
+            }
+
+            return optionsByPolicy;
         }
 
         #endregion
