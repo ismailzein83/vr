b5396e901b20f40bd2a87922c3ebe00ca6941fb2
Wissam Ajamy <VANRISE\wissam.ajamy>
2017-10-18 09:35:38 +0000

    Update structure from QualityMeasurement_Dev
    git-tfs-id: [http://developmentvlan:8585/tfs/vanrise.collection]$/;C43959

--- QualityMeasurement/Code/QualityMeasurement/QM_CLITester/Stored Procedures/sp_TestCall_GetUpdated.sql
+++ QualityMeasurement/Code/QualityMeasurement/QM_CLITester/Stored Procedures/sp_TestCall_GetUpdated.sql
@@ -5,48 +5,31 @@ CREATE PROCEDURE [QM_CLITester].[sp_TestCall_GetUpdated]
 	@UserId INT,
 	@NumberOfMinutes INT
 AS
-BEGIN
-	
-	IF (@TimestampAfter IS NULL)--If First Time Query, get the Last Test Calls
+BEGIN	
+	IF (@TimestampAfter IS NULL) --If First Time Query, get the Last Test Calls
+	BEGIN
 		SELECT @TimestampAfter = MIN([timestamp])
-		FROM (SELECT TOP (@NbOfRows) [timestamp] FROM [QM_CLITester].[TestCall]  WITH(NOLOCK) WHERE UserID = @UserId AND (DATEDIFF(MINUTE,CreationDate,GETDATE()) < @NumberOfMinutes) ORDER BY ID DESC) LastTestCalls
+		FROM 
+		(
+			SELECT TOP (@NbOfRows) [timestamp] 
+			FROM [QM_CLITester].[TestCall]  WITH(NOLOCK) 
+			WHERE UserID = @UserId AND (DATEDIFF(MINUTE,CreationDate,GETDATE()) < @NumberOfMinutes)
+			ORDER BY ID DESC
+		) LastTestCalls
+	END
 	
-	SELECT   [ID]
-		  ,[UserID]
-		  ,[SupplierID]
-		  ,[CountryID]
-		  ,[ZoneID]
-		  ,[ProfileID]
-		  ,[CreationDate]
-		  ,[CallTestStatus]
-		  ,[CallTestResult]
-		  ,[InitiateTestInformation]
-		  ,[TestProgress]
-		  ,[InitiationRetryCount]
-		  ,[GetProgressRetryCount]
-		  ,[FailureMessage]
-		  ,[timestamp]
-		  ,[BatchNumber]
-		  ,[ScheduleID]
-          ,[PDD]
-		  ,[MOS]
-		  ,[Duration]
-		  ,[RingDuration]
-		  ,[Quantity]
+	SELECT [ID],[UserID],[SupplierID],[CountryID],[ZoneID],[ProfileID],[CreationDate],[CallTestStatus],[CallTestResult],[InitiateTestInformation],[TestProgress],
+		   [InitiationRetryCount],[GetProgressRetryCount],[FailureMessage],[timestamp],[BatchNumber],[ScheduleID],[PDD],[MOS],[Duration],[RingDuration],[Quantity]
 	INTO #Result
 	FROM [QM_CLITester].[TestCall]  WITH(NOLOCK) 
-	WHERE 
-	 UserID = @UserId AND
-	([timestamp] >= @TimestampAfter) --ONLY Updated records 
-	AND
-	 DATEDIFF(MINUTE,CreationDate,GETDATE()) < @NumberOfMinutes
+	WHERE [UserID] = @UserId AND 
+		  [timestamp] > @TimestampAfter And 	
+		  DATEDIFF(MINUTE, CreationDate, GETDATE()) < @NumberOfMinutes --ONLY Updated records
 	
-	SELECT * FROM #Result
-	ORDER BY ID DESC
+	SELECT * FROM #Result ORDER BY ID DESC
 	
 	IF((SELECT COUNT(*) FROM #Result) = 0)
 		SELECT @TimestampAfter AS MaxTimestamp
 	ELSE
 		SELECT MAX([timestamp]) MaxTimestamp FROM #Result
-	
 END
