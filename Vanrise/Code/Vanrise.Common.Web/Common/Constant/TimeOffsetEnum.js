8e29b0ed6f2e18d33e14f21ee2fd05236a5b3e7e
Ali Ballouk <VANRISE\ali.ballouk>
2017-10-20 08:42:55 +0000

    update VRCommon_TimeOffsetEnum
    git-tfs-id: [http://developmentvlan:8585/tfs/vanrise.collection]$/;C44030

--- Vanrise/Code/Vanrise.Common.Web/Common/Constant/TimeOffsetEnum.js
+++ Vanrise/Code/Vanrise.Common.Web/Common/Constant/TimeOffsetEnum.js
@@ -33,41 +33,5 @@
     '10:30:00': { value: '10:30:00', description: '10:30' },
 
     '11:00:00': { value: '11:00:00', description: '11:00' },
-    '11:30:00': { value: '11:30:00', description: '11:30' },
-
-    '12:00:00': { value: '12:00:00', description: '12:00' },
-    '12:30:00': { value: '12:30:00', description: '12:30' },
-
-    '13:00:00': { value: '13:00:00', description: '13:00' },
-    '13:30:00': { value: '13:30:00', description: '13:30' },
-
-    '14:00:00': { value: '14:00:00', description: '14:00' },
-    '14:30:00': { value: '14:30:00', description: '14:30' },
-
-    '15:00:00': { value: '15:00:00', description: '15:00' },
-    '15:30:00': { value: '15:30:00', description: '15:30' },
-
-    '16:00:00': { value: '16:00:00', description: '16:00' },
-    '16:30:00': { value: '16:30:00', description: '16:30' },
-
-    '17:00:00': { value: '17:00:00', description: '17:00' },
-    '17:30:00': { value: '17:30:00', description: '17:30' },
-
-    '18:00:00': { value: '18:00:00', description: '18:00' },
-    '18:30:00': { value: '18:30:00', description: '18:30' },
-
-    '19:00:00': { value: '19:00:00', description: '19:00' },
-    '19:30:00': { value: '19:30:00', description: '19:30' },
-
-    '20:00:00': { value: '20:00:00', description: '20:00' },
-    '20:30:00': { value: '20:30:00', description: '20:30' },
-
-    '21:00:00': { value: '21:00:00', description: '21:00' },
-    '21:30:00': { value: '21:30:00', description: '21:30' },
-
-    '22:00:00': { value: '22:00:00', description: '22:00' },
-    '22:30:00': { value: '22:30:00', description: '22:30' },
-
-    '23:00:00': { value: '23:00:00', description: '23:00' },
-    '23:30:00': { value: '23:30:00', description: '23:30' }
+    '11:30:00': { value: '11:30:00', description: '11:30' }
 });
