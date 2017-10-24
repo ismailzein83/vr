a6118573e50e314d1bf99fae0a53003a8dc73419
Samer Haidar <VANRISE\samer.haidar>
2017-10-24 09:23:46 +0000

    adding AccountManagerView
    git-tfs-id: [http://developmentvlan:8585/tfs/vanrise.collection]$/;C44096

---
+++ Retail/Code/Retail.BusinessEntity.MainExtensions/AccountViews/AccountManagerView.cs
@@ -0,0 +1,36 @@
+﻿    using Retail.BusinessEntity.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Retail.BusinessEntity.MainExtensions.AccountViews
+{
+    public class AccountManagerView : AccountViewDefinitionSettings
+    {
+        public override Guid ConfigId
+        {
+            get { return new Guid("5A58B7C2-3179-42DE-9724-C66F6BEDB61B"); }
+        }
+
+        public override string RuntimeEditor
+        {
+            get
+            {
+                return "retail-be-accountmanager-view";
+            }
+            set
+            {
+
+            }
+        }
+
+        public override bool DoesUserHaveAccess(IAccountViewDefinitionCheckAccessContext context)
+        {
+            return base.DoesUserHaveAccess(context);
+        }
+    }
+}
+
+
