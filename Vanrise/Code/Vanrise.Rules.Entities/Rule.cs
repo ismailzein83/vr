533ffd971344a1cc0620eecb731c0f1664451099
Ali Atoui <VANRISE\ali.atoui>
2017-10-24 11:51:13 +0000

    Arranging Rule
    git-tfs-id: [http://developmentvlan:8585/tfs/vanrise.collection]$/;C44111

--- Vanrise/Code/Vanrise.Rules.Entities/Rule.cs
+++ Vanrise/Code/Vanrise.Rules.Entities/Rule.cs
@@ -1,8 +1,4 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace Vanrise.Rules.Entities
 {
@@ -13,8 +9,11 @@ namespace Vanrise.Rules.Entities
         public int TypeId { get; set; }
 
         public string RuleDetails { get; set; }
+
         public DateTime BED { get; set; }
+
         public DateTime? EED { get; set; }
+
         public string SourceId { get; set; }
     }
 }
