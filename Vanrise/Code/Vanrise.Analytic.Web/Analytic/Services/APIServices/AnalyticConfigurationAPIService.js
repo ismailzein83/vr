9495e8f0aed11e00e4eef89a0cf5cc020263fc2b
Ali Atoui <VANRISE\ali.atoui>
2017-10-25 14:15:14 +0000

    Arranging VR_Analytic_AnalyticConfigurationAPIService
    git-tfs-id: [http://developmentvlan:8585/tfs/vanrise.collection]$/;C44145

--- Vanrise/Code/Vanrise.Analytic.Web/Analytic/Services/APIServices/AnalyticConfigurationAPIService.js
+++ Vanrise/Code/Vanrise.Analytic.Web/Analytic/Services/APIServices/AnalyticConfigurationAPIService.js
@@ -1,6 +1,7 @@
 ﻿(function (appControllers) {
 
     "use strict";
+
     AnalyticConfigurationAPIService.$inject = ['BaseAPIService', 'VR_Analytic_ModuleConfig', 'UtilsService', 'SecurityService'];
 
     function AnalyticConfigurationAPIService(BaseAPIService, VR_Analytic_ModuleConfig, UtilsService, SecurityService) {
@@ -9,27 +10,30 @@
         function GetAnalyticReportSettingsTemplateConfigs() {
             return BaseAPIService.get(UtilsService.getServiceURL(VR_Analytic_ModuleConfig.moduleName, controllerName, "GetAnalyticReportSettingsTemplateConfigs"));
         }
+
         function GetWidgetsTemplateConfigs() {
             return BaseAPIService.get(UtilsService.getServiceURL(VR_Analytic_ModuleConfig.moduleName, controllerName, "GetWidgetsTemplateConfigs"));
         }
-        function GetRealTimeReportSettingsTemplateConfigs()
-        {
+
+        function GetRealTimeReportSettingsTemplateConfigs() {
             return BaseAPIService.get(UtilsService.getServiceURL(VR_Analytic_ModuleConfig.moduleName, controllerName, "GetRealTimeReportSettingsTemplateConfigs"));
         }
-        function GetRealTimeWidgetsTemplateConfigs()
-        {
+
+        function GetRealTimeWidgetsTemplateConfigs() {
             return BaseAPIService.get(UtilsService.getServiceURL(VR_Analytic_ModuleConfig.moduleName, controllerName, "GetRealTimeWidgetsTemplateConfigs"));
         }
+
         function GetMeasureStyleRuleTemplateConfigs() {
             return BaseAPIService.get(UtilsService.getServiceURL(VR_Analytic_ModuleConfig.moduleName, controllerName, "GetMeasureStyleRuleTemplateConfigs"));
         }
+
         function GetAnalyticDataProviderConfigs() {
             return BaseAPIService.get(UtilsService.getServiceURL(VR_Analytic_ModuleConfig.moduleName, controllerName, "GetAnalyticDataProviderConfigs"));
         }
+
         return ({
             GetAnalyticReportSettingsTemplateConfigs: GetAnalyticReportSettingsTemplateConfigs,
             GetWidgetsTemplateConfigs: GetWidgetsTemplateConfigs,
-
             GetRealTimeReportSettingsTemplateConfigs: GetRealTimeReportSettingsTemplateConfigs,
             GetRealTimeWidgetsTemplateConfigs: GetRealTimeWidgetsTemplateConfigs,
             GetMeasureStyleRuleTemplateConfigs: GetMeasureStyleRuleTemplateConfigs,
