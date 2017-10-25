32472bd168a6a4b7184245fe793b7d4a16d40971
Samer Haidar <VANRISE\samer.haidar>
2017-10-25 07:16:50 +0000

    Adding classes for retail account manager
    git-tfs-id: [http://developmentvlan:8585/tfs/vanrise.collection]$/;C44120

---
+++ Retail/Code/Retail.BusinessEntity.Business/Extensions/AccountManager/RetailAccountAssignmentDefinition.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Vanrise.AccountManager.Entities;
+
+namespace Retail.BusinessEntity.Business
+{
+    public class RetailAccountAssignmentDefinition : AccountManagerAssignmentDefinitionSettings
+    {
+        public override string GetAccountName(string accountId)
+        {
+            throw new NotImplementedException();
+        }
+        public Guid AccountBEDefinitionId { get; set; }
+        public Retail.BusinessEntity.Entities.AccountCondition AccountCondition { get; set; }
+    }
+}
---
+++ Retail/Code/Retail.BusinessEntity.Business/Extensions/AccountManager/RetailAccountManagerSubViewDefinition.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Vanrise.AccountManager.Entities;
+
+namespace Retail.BusinessEntity.Business
+{
+    public class RetailAccountManagerSubViewDefinition : AccountManagerSubViewDefinitionSettings
+    {
+        public override Guid ConfigId
+        {
+            get { throw new NotImplementedException(); }
+        }
+        public override string RuntimeEditor { get { throw new NotImplementedException(); } }
+        public Guid AccountManagerAssignementDefinitionId { get; set; }
+    }
+}
