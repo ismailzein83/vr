8555fab862af087443b3bb45b97f5bceb207a006
Ali Atoui <VANRISE\ali.atoui>
2017-10-25 13:49:05 +0000

    Arranging AnalyticConfigurationManager
    git-tfs-id: [http://developmentvlan:8585/tfs/vanrise.collection]$/;C44143

--- Vanrise/Code/Vanrise.Analytic.Business/AnalyticConfigurationManager.cs
+++ Vanrise/Code/Vanrise.Analytic.Business/AnalyticConfigurationManager.cs
@@ -1,52 +1,40 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using System.Collections.Generic;
 using Vanrise.Analytic.Entities;
 using Vanrise.Common.Business;
-using Vanrise.Entities;
 
 namespace Vanrise.Analytic.Business
 {
     public class AnalyticConfigurationManager
     {
+        ExtensionConfigurationManager _templateConfigManager = new ExtensionConfigurationManager();
+
         public IEnumerable<HistorySearchSetting> GetAnalyticReportSettingsTemplateConfigs()
         {
-            var templateConfigManager = new ExtensionConfigurationManager();
-            return templateConfigManager.GetExtensionConfigurations<HistorySearchSetting>(HistorySearchSetting.EXTENSION_TYPE);
+            return _templateConfigManager.GetExtensionConfigurations<HistorySearchSetting>(HistorySearchSetting.EXTENSION_TYPE);
         }
         public IEnumerable<WidgetDefinitionSetting> GetWidgetsTemplateConfigs()
         {
-            var templateConfigManager = new ExtensionConfigurationManager();
-            return templateConfigManager.GetExtensionConfigurations<WidgetDefinitionSetting>(WidgetDefinitionSetting.EXTENSION_TYPE);
+            return _templateConfigManager.GetExtensionConfigurations<WidgetDefinitionSetting>(WidgetDefinitionSetting.EXTENSION_TYPE);
         }
-
         public IEnumerable<RealTimeSearchSetting> GetRealTimeReportSettingsTemplateConfigs()
         {
-            var templateConfigManager = new ExtensionConfigurationManager();
-            return templateConfigManager.GetExtensionConfigurations<RealTimeSearchSetting>(RealTimeSearchSetting.EXTENSION_TYPE);
+            return _templateConfigManager.GetExtensionConfigurations<RealTimeSearchSetting>(RealTimeSearchSetting.EXTENSION_TYPE);
         }
         public IEnumerable<RealTimeWidgetSetting> GetRealTimeWidgetsTemplateConfigs()
         {
-            var templateConfigManager = new ExtensionConfigurationManager();
-            return templateConfigManager.GetExtensionConfigurations<RealTimeWidgetSetting>(RealTimeWidgetSetting.EXTENSION_TYPE);
+            return _templateConfigManager.GetExtensionConfigurations<RealTimeWidgetSetting>(RealTimeWidgetSetting.EXTENSION_TYPE);
         }
         public IEnumerable<MeasureStyleRuleTemplate> GetMeasureStyleRuleTemplateConfigs()
         {
-            var templateConfigManager = new ExtensionConfigurationManager();
-            return templateConfigManager.GetExtensionConfigurations<MeasureStyleRuleTemplate>(MeasureStyleRuleTemplate.EXTENSION_TYPE);
+            return _templateConfigManager.GetExtensionConfigurations<MeasureStyleRuleTemplate>(MeasureStyleRuleTemplate.EXTENSION_TYPE);
         }
         public IEnumerable<AnalyticItemActionTemplate> GetAnalyticItemActionsTemplateConfigs()
         {
-            var templateConfigManager = new ExtensionConfigurationManager();
-            return templateConfigManager.GetExtensionConfigurations<AnalyticItemActionTemplate>(AnalyticItemActionTemplate.EXTENSION_TYPE);
+            return _templateConfigManager.GetExtensionConfigurations<AnalyticItemActionTemplate>(AnalyticItemActionTemplate.EXTENSION_TYPE);
         }
-
         public IEnumerable<AnalyticDataProviderConfig> GetAnalyticDataProviderConfigs()
         {
-            var templateConfigManager = new ExtensionConfigurationManager();
-            return templateConfigManager.GetExtensionConfigurations<AnalyticDataProviderConfig>(AnalyticDataProviderConfig.EXTENSION_TYPE);
+            return _templateConfigManager.GetExtensionConfigurations<AnalyticDataProviderConfig>(AnalyticDataProviderConfig.EXTENSION_TYPE);
         }
     }
 }
