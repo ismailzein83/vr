31ee613c1849ba22d4b7efcfa5b5aaad57efff39
Ali Atoui <VANRISE\ali.atoui>
2017-10-25 08:02:42 +0000

    Arranging AnalyticDataProvider
    git-tfs-id: [http://developmentvlan:8585/tfs/vanrise.collection]$/;C44125

--- Vanrise/Code/Vanrise.Analytic.Business/MemoryAnalyticDataProvider/MemoryAnalyticDataProvider.cs
+++ Vanrise/Code/Vanrise.Analytic.Business/MemoryAnalyticDataProvider/MemoryAnalyticDataProvider.cs
@@ -1,8 +1,4 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using Vanrise.Analytic.Entities;
 using Vanrise.Common;
 
@@ -10,11 +6,7 @@ namespace Vanrise.Analytic.Business
 {
     public class MemoryAnalyticDataProvider : AnalyticDataProvider
     {
-
-        public override Guid ConfigId
-        {
-            get { return new Guid("4F72F80C-3928-460F-AAF5-CC18A78C8265"); }
-        }
+        public override Guid ConfigId { get { return new Guid("4F72F80C-3928-460F-AAF5-CC18A78C8265"); } }
 
         public MemoryAnalyticDataManager DataManager { get; set; }
 
