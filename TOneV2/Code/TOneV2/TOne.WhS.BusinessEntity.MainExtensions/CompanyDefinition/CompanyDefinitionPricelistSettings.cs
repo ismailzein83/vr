8e63fc724a82f64e3a8044c4d81e06c2f511f9e5
Rodi Hassan <VANRISE\rodi.hassan>
2017-10-26 05:01:48 +0000

    Implement Company pricelist settings class
    git-tfs-id: [http://developmentvlan:8585/tfs/vanrise.collection]$/;C44149

--- TOneV2/Code/TOneV2/TOne.WhS.BusinessEntity.MainExtensions/CompanyDefinition/CompanyDefinitionPricelistSettings.cs
+++ TOneV2/Code/TOneV2/TOne.WhS.BusinessEntity.MainExtensions/CompanyDefinition/CompanyDefinitionPricelistSettings.cs
@@ -3,14 +3,17 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using TOne.WhS.BusinessEntity.Entities;
 using Vanrise.Entities;
 
 namespace TOne.WhS.BusinessEntity.MainExtensions
 {
-    class CompanyDifinitionPricelistSettings : BaseCompanyDefinitionExtendedSetting 
+    public class CompanyDefinitionPricelistSettings : BaseCompanyDefinitionExtendedSetting 
     {
         public override Guid ConfigId { get { return new Guid("48AB58C0-8D37-4F6A-A5EC-5EAFE9AAA586"); } }
         public override string RuntimeEditor{ get { return "vr-whs-be-pricelistsettings-editor"; } }
 
     }
+
+    
 }
---
+++ TOneV2/Code/TOneV2/TOne.WhS.BusinessEntity.MainExtensions/CompanyDefinition/CompanyPricelistSettings.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TOne.WhS.BusinessEntity.Entities;
+using Vanrise.Entities;
+
+namespace TOne.WhS.BusinessEntity.MainExtensions
+{
+    public class CompanyPricelistSettings : BaseCompanyExtendedSettings
+    {
+        public override Guid ConfigId { get { return new Guid("48AB58C0-8D37-4F6A-A5EC-5EAFE9AAA586"); } }
+        public PricelistSettings PricelistSettings { get; set; }
+
+    }
+}
