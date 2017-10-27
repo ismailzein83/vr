045754fbb0ac1930f52af67e6a064e58ca792f20
Samer Haidar <VANRISE\samer.haidar>
2017-10-27 10:31:49 +0000

    adding SetInvoiceDeletedAction class
    git-tfs-id: [http://developmentvlan:8585/tfs/vanrise.collection]$/;C44189

---
+++ Vanrise/Code/Vanrise.Invoice.MainExtensions/InvoiceActionSettings/SetInvoiceDeletedAction.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Vanrise.Invoice.Entities;
+
+namespace Vanrise.Invoice.MainExtensions
+{
+    public class SetInvoiceDeletedAction : InvoiceActionSettings
+    {
+        public override Guid ConfigId { get { return new Guid("EBF6D7EB-EEC7-44B9-B1D4-F4A435680C14"); } }
+        public override string ActionTypeName
+        {
+            get { return "SetInvoiceDeletedAction"; }
+        }
+    }
+}
