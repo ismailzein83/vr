d5dbcb4b06c7250e733cde2a7fa35533be77d60e
Ali Atoui <VANRISE\ali.atoui>
2017-10-27 12:45:59 +0000

    Arranging VRRestAPIBEDefinitionSettings
    git-tfs-id: [http://developmentvlan:8585/tfs/vanrise.collection]$/;C44198

--- Vanrise/Code/Vanrise.GenericData.Business/BusinessEntity/VRRestAPIBEDefinitionSettings/VRRestAPIBEDefinitionSettings.cs
+++ Vanrise/Code/Vanrise.GenericData.Business/BusinessEntity/VRRestAPIBEDefinitionSettings/VRRestAPIBEDefinitionSettings.cs
@@ -1,8 +1,4 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using Vanrise.GenericData.Entities;
 
 namespace Vanrise.GenericData.Business
@@ -12,6 +8,14 @@ namespace Vanrise.GenericData.Business
         public static Guid s_configId = new Guid("B4F22FFB-B663-4F5F-AF53-ACBEF7224DFB");
         public override Guid ConfigId { get { return s_configId; } }
 
+        public override string ManagerFQTN { get { return "Vanrise.GenericData.Business.VRRestAPIBusinessEntityManager, Vanrise.GenericData.Business"; } }
+
+        public override string DefinitionEditor { get { return "vr-genericdata-restapibedefinitions-editor"; } }
+
+        public override string SelectorUIControl { get { return "vr-genericdata-businessentity-remoteselector"; } }
+
+        public override string GroupSelectorUIControl { get; set; }
+
         public override string IdType
         {
             get
@@ -19,23 +23,6 @@ namespace Vanrise.GenericData.Business
                 return new VRRestAPIBusinessEntityManager().GetBusinessEntityIdType(this.RemoteBEDefinitionId, this.ConnectionId);
             }
         }
-
-        public override string DefinitionEditor
-        {
-            get { return "vr-genericdata-restapibedefinitions-editor"; }
-        }
-
-        public override string SelectorUIControl
-        {
-            get { return "vr-genericdata-businessentity-remoteselector"; }
-        }
-
-        public override string ManagerFQTN
-        {
-            get { return "Vanrise.GenericData.Business.VRRestAPIBusinessEntityManager, Vanrise.GenericData.Business"; }
-        }
-
-        public override string GroupSelectorUIControl { get; set; }
 
         public Guid ConnectionId { get; set; }
         public Guid RemoteBEDefinitionId { get; set; }
