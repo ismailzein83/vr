0de78782d06a711b10512ca6b03a3a8d8d41bf28
Samer Haidar <VANRISE\samer.haidar>
2017-10-27 12:42:48 +0000

    update multinet script
    git-tfs-id: [http://developmentvlan:8585/tfs/vanrise.collection]$/;C44197

--- TOneV2/Code/TOneV2/TestRuntime/Tasks/SamerTask.cs
+++ TOneV2/Code/TOneV2/TestRuntime/Tasks/SamerTask.cs
@@ -10,6 +10,7 @@ using TOne.WhS.Invoice.Business.Extensions;
 using Vanrise.Analytic.Entities;
 using Vanrise.BusinessProcess;
 using Vanrise.BusinessProcess.Entities;
+using Vanrise.Entities;
 using Vanrise.GenericData.MainExtensions.DataRecordFields;
 using Vanrise.Invoice.Entities;
 using Vanrise.Invoice.MainExtensions;
@@ -23,21 +24,35 @@ namespace TestRuntime.Tasks
     {
         public void Execute()
         {
+
+            //VREventHandler eventHandler = new VREventHandler
+            //{
+            //   Name = "InvoiceCarrierAccountStatusChanged",
+            //   BED = new DateTime(2001,10,10),
+            //   EED = null,
+            //   VREventHandlerId = Guid.NewGuid(),
+            //   Settings = new VREventHandlerSettings
+            //   {
+            //       ExtendedSettings = new InvoiceCarrierAccountStatusChangedHandler()
+            //   }
+            //};
+            //var test5 = Vanrise.Common.Serializer.Serialize(eventHandler);
+            //Vanrise.Common.Serializer.Serialize(eventHandler);
           //  System.Threading.ThreadPool.SetMaxThreads(10000, 10000);
 
-           // BusinessProcessService bpService = new BusinessProcessService() { Interval = new TimeSpan(0, 0, 2) };
-           // QueueActivationService queueActivationService = new QueueActivationService() { Interval = new TimeSpan(0, 0, 2) };
-           // SchedulerService schedulerService = new SchedulerService() { Interval = new TimeSpan(0, 0, 2) };
+            //BusinessProcessService bpService = new BusinessProcessService() { Interval = new TimeSpan(0, 0, 2) };
+            //QueueActivationService queueActivationService = new QueueActivationService() { Interval = new TimeSpan(0, 0, 2) };
+            //SchedulerService schedulerService = new SchedulerService() { Interval = new TimeSpan(0, 0, 2) };
 
-           // var runtimeServices = new List<RuntimeService>();
-           //// runtimeServices.Add(queueActivationService);
+            //var runtimeServices = new List<RuntimeService>();
+            //runtimeServices.Add(queueActivationService);
 
-           // runtimeServices.Add(bpService);
+            //runtimeServices.Add(bpService);
 
-           //// runtimeServices.Add(schedulerService);
+            //runtimeServices.Add(schedulerService);
 
-           // RuntimeHost host = new RuntimeHost(runtimeServices);
-           // host.Start();
+            //RuntimeHost host = new RuntimeHost(runtimeServices);
+            //host.Start();
 
             ////var tree = Vanrise.Common.Serializer.Serialize(queueFlowTree);
             //var myflow = AllFlows.GetImportCDRFlow();
@@ -75,12 +90,14 @@ namespace TestRuntime.Tasks
             Vanrise.Integration.Business.DataSourceRuntimeService dsRuntimeService = new Vanrise.Integration.Business.DataSourceRuntimeService { Interval = new TimeSpan(0, 0, 2) };
            
             runtimeServices.Add(queueActivationService);
-           // runtimeServices.Add(schedulerService);
+            runtimeServices.Add(schedulerService);
             runtimeServices.Add(dsRuntimeService);
             runtimeServices.Add(queueActivationRuntimeService);
             runtimeServices.Add(bpRegulatorService);
             runtimeServices.Add(queueRegulatorService);
 
+            Vanrise.Common.Business.BigDataRuntimeService bigDataService = new Vanrise.Common.Business.BigDataRuntimeService { Interval = new TimeSpan(0, 0, 2) };
+            runtimeServices.Add(bigDataService);
             RuntimeHost host = new RuntimeHost(runtimeServices);
             host.Start();
 
