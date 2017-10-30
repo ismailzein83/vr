bf8738627843fe1bc6eec03148cc692afad330e1
mohamad.elhariri <mohamad.elhariri@vanrise.tfs.local>
2017-10-30 16:10:25 +0000

    Added AllCustomerCountriesToChange
    git-tfs-id: [http://developmentvlan:8585/tfs/vanrise.collection]$/;C44239

---
+++ TOneV2/Code/TOneV2/TOne.WhS.Sales.Entities/RateManagement/Actions/Country/AllCustomerCountriesToChange.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TOne.WhS.Sales.Entities.RateManagement.Actions.Country
+{
+    public class AllCustomerCountriesToChange : Vanrise.BusinessProcess.Entities.IRuleTarget
+    {
+        public IEnumerable<CustomerCountryToChange> CustomerCountriesToChange { get; set; }
+        public object Key
+        {
+            get { return null; }
+        }
+        public string TargetType
+        {
+            get { return "AllCustomerCountriesToChange"; }
+        }
+    }
+}
