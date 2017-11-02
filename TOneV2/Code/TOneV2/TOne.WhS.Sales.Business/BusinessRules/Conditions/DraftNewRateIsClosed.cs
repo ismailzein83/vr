778c5a2005305b354daa19b80f6f0cae5c1c29d5
mohamad.elhariri <mohamad.elhariri@vanrise.tfs.local>
2017-11-02 10:04:09 +0000

    Validated by country instead of by zone
    git-tfs-id: [http://developmentvlan:8585/tfs/vanrise.collection]$/;C44283

--- TOneV2/Code/TOneV2/TOne.WhS.Sales.Business/BusinessRules/Conditions/DraftNewRateIsClosed.cs
+++ TOneV2/Code/TOneV2/TOne.WhS.Sales.Business/BusinessRules/Conditions/DraftNewRateIsClosed.cs
@@ -11,42 +11,47 @@ namespace TOne.WhS.Sales.Business.BusinessRules
     {
         public override bool ShouldValidate(Vanrise.BusinessProcess.Entities.IRuleTarget target)
         {
-            return target is DataByZone;
+            return target is CountryData;
         }
         public override bool Validate(Vanrise.BusinessProcess.Entities.IBusinessRuleConditionValidateContext context)
         {
-            var zoneData = context.Target as DataByZone;
-
-            var invalidRateTypeNames = new List<string>();
             IRatePlanContext ratePlanContext = context.GetExtension<IRatePlanContext>();
 
             if (ratePlanContext.IsFirstSellingProductOffer.HasValue && ratePlanContext.IsFirstSellingProductOffer.Value)
-
                 return true;
-            if (zoneData.NormalRateToChange != null && zoneData.NormalRateToChange.EED.HasValue)
-            {
-                invalidRateTypeNames.Add("Normal");
-            }
 
-            if (zoneData.OtherRatesToChange != null && zoneData.OtherRatesToChange.Count > 0)
-            {
-                var rateTypeManager = new Vanrise.Common.Business.RateTypeManager();
+            var countryData = context.Target as CountryData;
 
-                foreach (RateToChange otherRateToChange in zoneData.OtherRatesToChange)
+            if (countryData.ZoneDataByZoneId == null || countryData.ZoneDataByZoneId.Count == 0)
+                return true;
+
+            var invalidZoneNames = new List<string>();
+
+            foreach (DataByZone zoneData in countryData.ZoneDataByZoneId.Values)
+            {
+                if (zoneData.NormalRateToChange != null && zoneData.NormalRateToChange.EED.HasValue)
                 {
-                    if (otherRateToChange.EED.HasValue)
+                    invalidZoneNames.Add(zoneData.ZoneName);
+                    break;
+                }
+
+                if (zoneData.OtherRatesToChange != null && zoneData.OtherRatesToChange.Count > 0)
+                {
+                    foreach (RateToChange otherRateToChange in zoneData.OtherRatesToChange)
                     {
-                        string rateTypeName = rateTypeManager.GetRateTypeName(otherRateToChange.RateTypeId.Value);
-                        if (rateTypeName == null)
-                            throw new Vanrise.Entities.DataIntegrityValidationException(string.Format("Name of RateType '{0}' was not found", otherRateToChange.RateTypeId.Value));
-                        invalidRateTypeNames.Add(rateTypeName);
+                        if (otherRateToChange.EED.HasValue)
+                        {
+                            invalidZoneNames.Add(zoneData.ZoneName);
+                            break;
+                        }
                     }
                 }
             }
 
-            if (invalidRateTypeNames.Count > 0)
+            if (invalidZoneNames.Count > 0)
             {
-                context.Message = string.Format("New rates of types'{0}' of zone '{1}' cannot be closed", string.Join(",", invalidRateTypeNames), zoneData.ZoneName);
+                string countryName = new Vanrise.Common.Business.CountryManager().GetCountryName(countryData.CountryId);
+                context.Message = string.Format("Cannot end the new rates of the following zones of country '{0}': {1}", countryName, string.Join(",", invalidZoneNames));
                 return false;
             }
 
