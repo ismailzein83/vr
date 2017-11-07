84ee719e752366a59c5999e37b525425bfeb97b6
Rodi Hassan <VANRISE\rodi.hassan>
2017-11-07 15:41:30 +0000

    add move pending effective code rule
    git-tfs-id: [http://developmentvlan:8585/tfs/vanrise.collection]$/;C44395

---
+++ TOneV2/Code/TOneV2/TOne.WhS.CodePreparation.Business/BusinessRules/Conditions/ValidateAfterProcessing/MovePendingEffectiveCodeCondition.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TOne.WhS.CodePreparation.Entities.Processing;
+using Vanrise.BusinessProcess.Entities;
+using Vanrise.Common;
+
+namespace TOne.WhS.CodePreparation.Business
+{
+    class MovePendingEffectiveCodeCondition : BusinessRuleCondition
+    {
+        public override bool ShouldValidate(IRuleTarget target)
+        {
+            return (target as ZoneToProcess != null);
+        }
+
+        public override bool Validate(IBusinessRuleConditionValidateContext context)
+        {
+            ZoneToProcess zoneToProcess = context.Target as ZoneToProcess;
+            var invalidCodes = new List<string>();
+
+            foreach (CodeToMove codeToMove in zoneToProcess.CodesToMove)
+            {
+                invalidCodes.Add(codeToMove.Code);
+            }
+
+            if (invalidCodes.Count > 0)
+            {
+                context.Message = string.Format("Cannot move codes ({0}) to zone '{1}' because they are pending effective", string.Join(",", invalidCodes), zoneToProcess.ZoneName);
+                return false;
+            }
+
+            return true;
+        }
+
+        public override string GetMessage(IRuleTarget target)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
