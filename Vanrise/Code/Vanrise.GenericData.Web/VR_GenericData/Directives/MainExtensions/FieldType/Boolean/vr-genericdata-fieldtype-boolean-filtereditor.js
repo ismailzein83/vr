fa397671f3012e373f97c2f8b8819f66d0d099af
Ali Atoui <VANRISE\ali.atoui>
2017-11-09 08:47:32 +0000

    Editing vr-genericdata-fieldtype-boolean-filtereditor
    git-tfs-id: [http://developmentvlan:8585/tfs/vanrise.collection]$/;C44418

--- Vanrise/Code/Vanrise.GenericData.Web/VR_GenericData/Directives/MainExtensions/FieldType/Boolean/vr-genericdata-fieldtype-boolean-filtereditor.js
+++ Vanrise/Code/Vanrise.GenericData.Web/VR_GenericData/Directives/MainExtensions/FieldType/Boolean/vr-genericdata-fieldtype-boolean-filtereditor.js
@@ -26,12 +26,10 @@
         function BooleanFieldTypeFilterEditor(ctrl, $scope, $attrs) {
             this.initializeController = initializeController;
 
-            var directiveAPI;
-
             function initializeController() {
-                ctrl.onDirectiveReady = function (api) {
-                    directiveAPI = api;
+                ctrl.datasource = [{ Text: "True", Value: "True" }, { Text: "False", Value: "False" }];
 
+                ctrl.onSelectorReady = function (api) {
                     if (ctrl.onReady != undefined) {
                         ctrl.onReady(getDirectiveAPI());
                     }
@@ -40,22 +38,36 @@
 
             function getDirectiveAPI() {
                 var api = {};
-                api.load = function (payload) {
-                    return directiveAPI.load(payload);
-                };
-                api.getValuesAsArray = function () {
-                    return directiveAPI.getData();
 
+                api.load = function (payload) {
+                    ctrl.label = payload != undefined ? payload.fieldTitle : "";
                 };
+
                 api.getData = function () {
-                    return directiveAPI.getData();
+                    var data = ctrl.selectedvalues;
+                    if (data == undefined)
+                        return undefined;
+                    return data.Value;
                 };
+
+                api.getValuesAsArray = function () {
+                    var value = api.getData();
+                    if (value == undefined)
+                        return undefined;
+                    return [value];
+                };
+
                 return api;
             }
         }
 
         function getDirectiveTemplate(attrs) {
-            return '<vr-genericdata-fieldtype-boolean-runtimeeditor on-ready="ctrl.onDirectiveReady" selectionmode="single" normal-col-num="{{ctrl.normalColNum}}" />';
+            return '<vr-columns colnum="{{ctrl.normalColNum}}">' +
+                       '<vr-select datatextfield="Text" datavaluefield="Value" isrequired="ctrl.isrequired" label="{{ctrl.label}}"' +
+                           '" datasource="ctrl.datasource" on-ready="ctrl.onSelectorReady" selectedvalues="ctrl.selectedvalues" onselectionchanged="ctrl.onselectionchanged"' +
+                           '" onselectitem="ctrl.onselectitem" ondeselectitem="ctrl.ondeselectitem" customvalidate="ctrl.customvalidate">' +
+                       '</vr-select>' +
+                   '</vr-columns>';
         }
     }
 
