ec0d1a180cfe075e18daa6224a777d5cb97927ff
Wissam Ajamy <VANRISE\wissam.ajamy>
2017-11-10 16:51:07 +0000

    Add FixesFullVersion path
    git-tfs-id: [http://developmentvlan:8585/tfs/vanrise.collection]$/;C44485

--- Vanrise/Code/Resources/BatchScripts/CarrierPortal.PartnerPortal/03-WebSite.bat
+++ Vanrise/Code/Resources/BatchScripts/CarrierPortal.PartnerPortal/03-WebSite.bat
@@ -6,6 +6,7 @@ Set PARSEARG="eol=; tokens=1,2,3,4* delims=/, "
 For /F %PARSEARG% %%i in (%CURRDATE%) Do SET YYYYMMDD=%%l%%j%%k
 
 ECHO CarrierPortal.PartnerPortal WebSite
+
 xcopy "C:\TFS\Vanrise\Code\Vanrise.Analytic.Web\Analytic" /S /E /R /y /v /i /z /Q "\\192.168.110.185\Fixes\WebSite\CarrierPortal.PartnerPortal\%YYYYMMDD%\Client\Modules\Analytic\"
 xcopy "C:\TFS\Vanrise\Code\Vanrise.GenericData.Web\VR_GenericData" /S /E /R /y /v /i /z /Q "\\192.168.110.185\Fixes\WebSite\CarrierPortal.PartnerPortal\%YYYYMMDD%\Client\Modules\VR_GenericData\"
 
@@ -15,4 +16,13 @@ xcopy "C:\TFS\CarrierPortal\Code\PartnerPortal.Invoice.Web\PartnerPortal_Invoice
 xcopy "C:\TFS\CarrierPortal\Code\CarrierPortal.Web\Web.config.PartnerPortal.exclude" /y /v /z /i /Q /R "\\192.168.110.185\Fixes\WebSite\CarrierPortal.PartnerPortal\%YYYYMMDD%\"
 rename "\\192.168.110.185\Fixes\WebSite\CarrierPortal.PartnerPortal\%YYYYMMDD%\Web.config.PartnerPortal.exclude" "Web.Update.PartnerPortal.config"
 
+ECHO CarrierPortal.PartnerPortal WebSite Full Version
 
+xcopy "C:\TFS\Vanrise\Code\Vanrise.Analytic.Web\Analytic" /S /E /R /y /v /i /z /Q										"\\192.168.110.185\FixesFullVersion\WebSite\CarrierPortal.PartnerPortal\%YYYYMMDD%\Client\Modules\Analytic\"
+xcopy "C:\TFS\Vanrise\Code\Vanrise.GenericData.Web\VR_GenericData" /S /E /R /y /v /i /z /Q								"\\192.168.110.185\FixesFullVersion\WebSite\CarrierPortal.PartnerPortal\%YYYYMMDD%\Client\Modules\VR_GenericData\"
+
+xcopy "C:\TFS\CarrierPortal\Code\PartnerPortal.CustomerAccess.Web\PartnerPortal_CustomerAccess" /S /E /R /y /v /i /z /Q "\\192.168.110.185\FixesFullVersion\WebSite\CarrierPortal.PartnerPortal\%YYYYMMDD%\Client\Modules\PartnerPortal_CustomerAccess\"
+xcopy "C:\TFS\CarrierPortal\Code\PartnerPortal.Invoice.Web\PartnerPortal_Invoice" /S /E /R /y /v /i /z /Q				"\\192.168.110.185\FixesFullVersion\WebSite\CarrierPortal.PartnerPortal\%YYYYMMDD%\Client\Modules\PartnerPortal_Invoice\"
+
+xcopy "C:\TFS\CarrierPortal\Code\CarrierPortal.Web\Web.config.PartnerPortal.exclude" /y /v /z /i /Q /R					"\\192.168.110.185\FixesFullVersion\WebSite\CarrierPortal.PartnerPortal\%YYYYMMDD%\"
+rename "\\192.168.110.185\FixesFullVersion\WebSite\CarrierPortal.PartnerPortal\%YYYYMMDD%\Web.config.PartnerPortal.exclude" "Web.Update.PartnerPortal.config"
