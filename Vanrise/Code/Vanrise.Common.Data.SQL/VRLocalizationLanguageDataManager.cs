f07fde7dfe2def0341a3a87198482c2baf54761b
Samer Haidar <VANRISE\samer.haidar>
2017-11-13 14:29:49 +0000

    Adding VRLocalizationLanguage Classes
    git-tfs-id: [http://developmentvlan:8585/tfs/vanrise.collection]$/;C44525

---
+++ Vanrise/Code/Vanrise.Common.Data.SQL/VRLocalizationLanguageDataManager.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Vanrise.Data.SQL;
+
+namespace Vanrise.Common.Data.SQL
+{
+    public class VRLocalizationLanguageDataManager:BaseSQLDataManager,IVRLocalizationLanguageDataManager
+    {
+        public VRLocalizationLanguageDataManager()
+            : base(GetConnectionStringName("VRLocalizationDBDBConnStringKey", "VRLocalizationDBDBConnString"))
+        {
+
+        }
+    }
+}
