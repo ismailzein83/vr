88b1d685047d48c170969c09fb47372fdc504853
Samer Haidar <VANRISE\samer.haidar>
2017-11-13 08:12:23 +0000

    adding class InvoiceBulkActionProcessInput
    git-tfs-id: [http://developmentvlan:8585/tfs/vanrise.collection]$/;C44494

---
+++ Vanrise/Code/Vanrise.Invoice.BP.Arguments/InvoiceBulkActionProcessInput.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Vanrise.Invoice.Entities;
+
+namespace Vanrise.Invoice.BP.Arguments
+{
+    public class InvoiceBulkActionProcessInput : Vanrise.BusinessProcess.Entities.BaseProcessInputArgument
+    {
+        public Guid InvoiceTypeId { get; set; }
+        public Guid InvoiceBulkActionIdentifier { get; set; }
+        public List<InvoiceBulkActionRuntime> InvoiceBulkActions { get; set; }
+
+        public override string GetTitle()
+        {
+            return "Invoice Bulk Action Process";
+        }
+    }
+}
