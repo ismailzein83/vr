3e869a25b5ac20cd41a2c6f7228630492b515f44
Wissam Ajamy <VANRISE\wissam.ajamy>
2017-11-14 07:55:20 +0000

    update structure from TOneV2_Dev
    git-tfs-id: [http://developmentvlan:8585/tfs/vanrise.collection]$/;C44532

--- TOneV2/Code/TOneV2/SQL.TOneWhS_BE/TOneWhS_BE/Tables/CodeGroup.sql
+++ TOneV2/Code/TOneV2/SQL.TOneWhS_BE/TOneWhS_BE/Tables/CodeGroup.sql
@@ -1,14 +1,16 @@
 ﻿CREATE TABLE [TOneWhS_BE].[CodeGroup] (
-    [ID]          INT           IDENTITY (1, 1) NOT NULL,
-    [CountryID]   INT           NOT NULL,
-    [Name]        NVARCHAR (50) NULL,
-    [Code]        VARCHAR (20)  NOT NULL,
-    [CreatedTime] DATETIME      CONSTRAINT [DF_CodeGroup_CreatedTime] DEFAULT (getdate()) NULL,
-    [SourceID]    VARCHAR (50)  NULL,
-    [timestamp]   ROWVERSION    NULL,
+    [ID]          INT            IDENTITY (1, 1) NOT NULL,
+    [CountryID]   INT            NOT NULL,
+    [Name]        NVARCHAR (200) NULL,
+    [Code]        VARCHAR (20)   NOT NULL,
+    [CreatedTime] DATETIME       CONSTRAINT [DF_CodeGroup_CreatedTime] DEFAULT (getdate()) NULL,
+    [SourceID]    VARCHAR (50)   NULL,
+    [timestamp]   ROWVERSION     NULL,
     CONSTRAINT [PK_CodeGroup_1] PRIMARY KEY CLUSTERED ([ID] ASC),
     CONSTRAINT [IX_CodeGroup_Code] UNIQUE NONCLUSTERED ([Code] ASC)
 );
+
+
 
 
 
