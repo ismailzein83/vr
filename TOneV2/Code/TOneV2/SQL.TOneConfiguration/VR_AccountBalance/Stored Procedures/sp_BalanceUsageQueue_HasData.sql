528b38855e5dcf49197fc5701157e0a0989ff385
Wissam Ajamy <VANRISE\wissam.ajamy>
2017-11-16 14:35:42 +0000

    Updat structure from TOneConfiguration
    git-tfs-id: [http://developmentvlan:8585/tfs/vanrise.collection]$/;C44621

---
+++ TOneV2/Code/TOneV2/SQL.TOneConfiguration/VR_AccountBalance/Stored Procedures/sp_BalanceUsageQueue_HasData.sql
@@ -0,0 +1,19 @@
+﻿-- =============================================
+-- Author:		<Author,,Name>
+-- Create date: <Create Date,,>
+-- Description:	<Description,,>
+-- =============================================
+CREATE PROCEDURE [VR_AccountBalance].[sp_BalanceUsageQueue_HasData]
+	@AccountTypeID uniqueidentifier
+AS
+BEGIN
+	IF EXISTS(Select TOP 1 ID from [VR_AccountBalance].[BalanceUsageQueue] WITH(NOLOCK) where AccountTypeID = @AccountTypeID)
+	BEGIN
+		SELECT 1
+	END
+
+	ELSE
+	BEGIN
+		SELECT 0
+	END
+END
---
+++ TOneV2/Code/TOneV2/SQL.TOneConfiguration/VR_AccountBalance/Stored Procedures/sp_BillingTransaction_HasData.sql
@@ -0,0 +1,20 @@
+﻿
+CREATE PROCEDURE [VR_AccountBalance].[sp_BillingTransaction_HasData]
+	@AccountTypeID uniqueidentifier
+AS
+BEGIN
+	IF EXISTS(Select TOP 1 ID from [VR_AccountBalance].BillingTransaction WITH(NOLOCK) 
+	where AccountTypeID = @AccountTypeID 
+	AND 
+	((ISNULL(IsDeleted, 0) = 0 AND ISNULL(IsBalanceUpdated, 0) = 0)
+	OR 
+	(ISNULL(IsDeleted, 0) = 1 AND ISNULL(IsBalanceUpdated, 0) = 1 and ISNULL(IsSubtractedFromBalance, 0) = 0)))
+	BEGIN
+		SELECT 1
+	END
+
+	ELSE
+	BEGIN
+		SELECT 0
+	END
+END
