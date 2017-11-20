fb350162f999f9694011bc6142f3d79327cc8e0e
anthony douaihy <VANRISE\anthony.douaihy>
2017-11-20 11:15:22 +0000

    add sp_LiveBalance_HasBalancesUpdateData script
    git-tfs-id: [http://developmentvlan:8585/tfs/vanrise.collection]$/;C44676

---
+++ TOneV2/Code/TOneV2/SQL.TOneConfiguration/VR_AccountBalance/Stored Procedures/sp_LiveBalance_HasBalancesUpdateData.sql
@@ -0,0 +1,23 @@
+﻿-- =============================================
+-- Author:		<Author,,Name>
+-- Create date: <Create Date,,>
+-- Description:	<Description,,>
+-- =============================================
+CREATE PROCEDURE [VR_AccountBalance].[sp_LiveBalance_HasBalancesUpdateData]
+@AccountTypeId uniqueidentifier
+
+AS
+BEGIN
+	IF EXISTS(Select TOP 1 ID from VR_AccountBalance.LiveBalance lb WITH(NOLOCK) 
+	where AccountTypeID = @AccountTypeID 
+	AND ((lb.[CurrentBalance] <= lb.NextAlertThreshold AND (lb.LastExecutedActionThreshold IS NULL OR lb.NextAlertThreshold  < lb.LastExecutedActionThreshold) AND ISNULL(IsDeleted,0) = 0) 
+		 OR (lb.[CurrentBalance] > lb.LastExecutedActionThreshold AND ISNULL(IsDeleted,0) = 0)))
+	BEGIN
+		SELECT 1
+	END
+
+	ELSE
+	BEGIN
+		SELECT 0
+	END
+END
