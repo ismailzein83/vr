daf0f04122dd56ab57633044d48eef9f22a8de7e
Zeinab Mokdad <VANRISE\zeinab.mokdad>
2017-11-20 09:24:41 +0000

    adding a new rule to check for moving closed codes to a new zone
    git-tfs-id: [http://developmentvlan:8585/tfs/vanrise.collection]$/;C44664

---
+++ TOneV2/Code/TOneV2/TOne.WhS.CodePreparation.Business/BusinessRules/Conditions/ValidateAfterProcessing/MoveClosedCodeToNewZoneCondition.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using TOne.WhS.CodePreparation.Entities.Processing;
+using Vanrise.BusinessProcess.Entities;
+using System.Linq;
+using System.Collections.Generic;
+
+namespace TOne.WhS.CodePreparation.Business
+{
+    public class MoveClosedCodeToNewZoneCondition : BusinessRuleCondition
+    {
+        public override bool ShouldValidate(IRuleTarget target)
+        {
+            return (target as ZoneToProcess != null);
+        }
+
+        public override bool Validate(IBusinessRuleConditionValidateContext context)
+        {
+            var invalidCodes = new List<string>();
+            ZoneToProcess zoneToProcess = context.Target as ZoneToProcess;
+            foreach (CodeToAdd codeToAdd in zoneToProcess.CodesToAdd)
+            {
+                if (codeToAdd.ChangedExistingCodes != null && codeToAdd.ChangedExistingCodes.Any(item => item.CodeEntity.Code == codeToAdd.Code && item.CodeEntity.EED.HasValue))
+                    invalidCodes.Add(codeToAdd.Code);
+            }
+            if (invalidCodes.Count > 0)
+            {
+                context.Message = string.Format("Cannot move codes ({0}) in zone '{1}' because code(s) are pending closed.", string.Join(",", invalidCodes), zoneToProcess.ZoneName);
+                return false;
+            }
+            return true;
+        }
+        public override string GetMessage(IRuleTarget target)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
