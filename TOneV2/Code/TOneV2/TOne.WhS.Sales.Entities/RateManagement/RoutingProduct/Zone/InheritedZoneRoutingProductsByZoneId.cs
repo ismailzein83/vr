510b933e4a9cf808803428612c050b606994bfb6
mohammad.zhour <mohammad.zhour@vanrise.tfs.local>
2017-11-20 09:58:46 +0000

    Add Files And Activity
    git-tfs-id: [http://developmentvlan:8585/tfs/vanrise.collection]$/;C44666

---
+++ TOneV2/Code/TOneV2/TOne.WhS.Sales.Entities/RateManagement/RoutingProduct/Zone/InheritedZoneRoutingProductsByZoneId.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TOne.WhS.BusinessEntity.Entities;
+
+namespace TOne.WhS.Sales.Entities
+{
+    public class InheritedZoneRoutingProductsByZoneId : Dictionary<long, ZoneInheritedRoutingProducts>
+    {
+
+    }
+    //public class ZoneInheritedRoutingProducts
+    //{
+    //    public List<SaleZoneRoutingProduct> RoutingProducts { get; set; }
+    //    public ZoneInheritedRoutingProducts()
+    //    {
+    //        RoutingProducts = new List<SaleZoneRoutingProduct>();
+    //    }
+    //}
+}
