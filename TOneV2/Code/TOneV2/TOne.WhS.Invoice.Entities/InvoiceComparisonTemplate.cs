15b69ceb36830d4731c44a36e95560c747369b50
Samer Haidar <VANRISE\samer.haidar>
2017-11-21 13:30:44 +0000

    Adding invoice comparison template managers and two projects
    git-tfs-id: [http://developmentvlan:8585/tfs/vanrise.collection]$/;C44720

---
+++ TOneV2/Code/TOneV2/TOne.WhS.Invoice.Entities/InvoiceComparisonTemplate.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Vanrise.ExcelConversion.Entities;
+
+namespace TOne.WhS.Invoice.Entities
+{
+   public class InvoiceComparisonTemplate
+    {
+        public long InvoiceComparisonTemplateId { get; set; }
+        public Guid InvoiceTypeId { get; set; }
+        public string PartnerId { get; set; }
+        public InvoiceComparisonTemplateDetails Details { get; set; }
+    }
+    public class InvoiceComparisonTemplateDetails
+    {
+        public ListMapping ListMapping { get; set; }
+        public string DateTimeFormat { get; set; }
+    }
+}
