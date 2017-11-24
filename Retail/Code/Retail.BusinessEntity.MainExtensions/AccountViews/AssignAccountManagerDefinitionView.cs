87f1fb03933a410502b7181b23afe422a151be43
Samer Haidar <VANRISE\samer.haidar>
2017-11-24 08:15:08 +0000

    add AssignAccountManagerDefinitionView
    git-tfs-id: [http://developmentvlan:8585/tfs/vanrise.collection]$/;C44742

---
+++ Retail/Code/Retail.BusinessEntity.MainExtensions/AccountViews/AssignAccountManagerDefinitionView.cs
@@ -0,0 +1,34 @@
+﻿using Retail.BusinessEntity.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Retail.BusinessEntity.MainExtensions.AccountBEActionTypes
+{
+    public class AssignAccountManagerDefinitionView : AccountViewDefinitionSettings
+    {
+        public override Guid ConfigId
+        {
+            get { return new Guid("A5483E0A-A8FB-418C-A04B-E7E58838F829"); }
+        }
+        public override string RuntimeEditor
+        {
+            get
+            {
+                return "";
+            }
+            set
+            {
+
+            }
+        }
+
+        public override bool DoesUserHaveAccess(IAccountViewDefinitionCheckAccessContext context)
+        {
+            return base.DoesUserHaveAccess(context);
+        }
+
+    }
+}
