2be280a79eb86fe12c317e062cce4dcedac45149
Ismail Zein <VANRISE\ismail.zein>
2017-11-27 10:57:46 +0000

    added VRPop3Connection skeleton
    git-tfs-id: [http://developmentvlan:8585/tfs/vanrise.collection]$/;C44776

---
+++ Vanrise/Code/Vanrise.Common.Business/VRmail/Pop3/VRPop3Connection.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Vanrise.Entities;
+
+namespace Vanrise.Common.Business
+{
+    /// <summary>
+    /// This connection can be only called in a single thread. 
+    /// </summary>
+    public class VRPop3Connection : VRConnectionSettings
+    {
+        public override Guid ConfigId
+        {
+            get { throw new NotImplementedException(); }
+        }
+
+        public TimeSpan TimeToKeepReadMessageIdsInState { get; set; }
+
+        public bool HasNewMessages(string senderIdentifier)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void ReadNewMessages(string senderIdentifier, Func<VRPop3MailMessageHeader, bool> mailMessageFilter, Action<VRPop3MailMessage> onMessageRead)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void SetMessagesRead(string senderIdentifier, List<VRPop3MailMessage> messages)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
