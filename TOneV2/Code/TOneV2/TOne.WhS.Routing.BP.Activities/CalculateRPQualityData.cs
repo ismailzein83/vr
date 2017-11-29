6b849bfac9577f95fa6f8ba4ab3da213008fb218
ahmad.almasri <ahmad.almasri@vanrise.tfs.local>
2017-11-29 15:33:35 +0000

    add CalculateRPQualityData to Product Route Process
    git-tfs-id: [http://developmentvlan:8585/tfs/vanrise.collection]$/;C44826

---
+++ TOneV2/Code/TOneV2/TOne.WhS.Routing.BP.Activities/CalculateRPQualityData.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Activities;
+using TOne.WhS.Routing.Business;
+
+namespace TOne.WhS.Routing.BP.Activities
+{
+    public sealed class CalculateRPQualityData : CodeActivity
+    {
+        [RequiredArgument]
+        public InArgument<DateTime> EffectiveDate { get; set; }
+
+        protected override void Execute(CodeActivityContext context)
+        {
+            DateTime effectiveDate = this.EffectiveDate.Get(context);
+        }
+    }
+}
