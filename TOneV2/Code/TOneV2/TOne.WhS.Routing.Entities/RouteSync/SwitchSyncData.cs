82c2a831ef84d904ccf5a14b7e53fc0e4b96170b
Ali Atoui <VANRISE\ali.atoui>
2017-12-04 08:44:09 +0000

    Adding SwitchSyncData
    git-tfs-id: [http://developmentvlan:8585/tfs/vanrise.collection]$/;C44837

---
+++ TOneV2/Code/TOneV2/TOne.WhS.Routing.Entities/RouteSync/SwitchSyncData.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TOne.WhS.Routing.Entities
+{
+    public class SwitchSyncData
+    {
+        public int SwitchId { get; set; } 
+
+        public int LastVersionNumber { get; set; }
+    }
+}
