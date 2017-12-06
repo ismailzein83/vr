7615cab85db0370b40aa4cf0dfa381338f67cd6d
mohamad.elhariri <mohamad.elhariri@vanrise.tfs.local>
2017-12-06 17:15:23 +0000

    Terminated the workflow when tasks time out
    git-tfs-id: [http://developmentvlan:8585/tfs/vanrise.collection]$/;C44895

---
+++ Xbooster/Code/CDRComparison.BP.Activities/SetDelayDuration.cs
@@ -0,0 +1,27 @@
+﻿using CDRComparison.Entities;
+using System;
+using System.Activities;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Vanrise.Common;
+
+namespace CDRComparison.BP.Activities
+{
+    public class SetDelayDuration : CodeActivity
+    {
+        #region Output Arguments
+        [RequiredArgument]
+        public OutArgument<TimeSpan> DelayDuration { get; set; }
+        #endregion
+
+        protected override void Execute(CodeActivityContext context)
+        {
+            CDRComparisonSettingData cdrComparisonSettings = new Vanrise.Common.Business.SettingManager().GetSetting<CDRComparisonSettingData>("CDRComparison_CDRComparisonSettings");
+            cdrComparisonSettings.ThrowIfNull("cdrComparisonSettings");
+            var delayDuration = new TimeSpan(0, 0, cdrComparisonSettings.TaskTimeoutInSeconds);
+            DelayDuration.Set(context, delayDuration);
+        }
+    }
+}
