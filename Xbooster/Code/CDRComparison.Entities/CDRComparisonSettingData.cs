c9effd13a0770cadbc25b06b722b8820ec70e3ba
mohamad.elhariri <mohamad.elhariri@vanrise.tfs.local>
2017-12-06 17:14:50 +0000

    Added the CDR Comparison settings and defined its Task Timeout parameter
    git-tfs-id: [http://developmentvlan:8585/tfs/vanrise.collection]$/;C44894

---
+++ Xbooster/Code/CDRComparison.Entities/CDRComparisonSettingData.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CDRComparison.Entities
+{
+    public class CDRComparisonSettingData : Vanrise.Entities.SettingData
+    {
+        public int TaskTimeoutInSeconds { get; set; }
+    }
+}
---
+++ Xbooster/Code/CDRComparison.Web/CDRComparison/Directives/cdrcomparison-cdrcomparisonsettings-editor.js
@@ -0,0 +1,58 @@
+﻿(function (app) {
+
+    'use strict';
+
+    CDRComparisonSettingsEditorDirective.$inject = [];
+
+    function CDRComparisonSettingsEditorDirective() {
+        return {
+            restrict: 'E',
+            scope: {
+                onReady: '=',
+                normalColNum: '@',
+                isrequired: '='
+            },
+            controller: function ($scope, $element, $attrs) {
+                var ctrl = this;
+                var cdrComparisonSettingsEditor = new CDRComparisonSettingsEditor($scope, ctrl, $attrs);
+                cdrComparisonSettingsEditor.initializeController();
+            },
+            controllerAs: 'cdrComparisonSettingsCtrl',
+            bindToController: true,
+            templateUrl: '/Client/Modules/CDRComparison/Directives/Templates/CDRComparisonSettingsTemplate.html'
+        };
+
+        function CDRComparisonSettingsEditor($scope, ctrl, $attrs) {
+
+            this.initializeController = initializeController;
+
+            function initializeController() {
+                $scope.scopeModel = {};
+                defineAPI();
+            }
+            function defineAPI() {
+                var api = {};
+
+                api.load = function (payload) {
+                    if (payload != undefined && payload.data != undefined) {
+                        $scope.scopeModel.taskTimeoutInSeconds = payload.data.TaskTimeoutInSeconds;
+                    }
+                };
+
+                api.getData = function () {
+                    return {
+                        $type: 'CDRComparison.Entities.CDRComparisonSettingData, CDRComparison.Entities',
+                        TaskTimeoutInSeconds: $scope.scopeModel.taskTimeoutInSeconds
+                    };
+                };
+
+                if (ctrl.onReady != undefined && typeof (ctrl.onReady) == 'function') {
+                    ctrl.onReady(api);
+                }
+            }
+        }
+    }
+
+    app.directive('cdrcomparisonCdrcomparisonsettingsEditor', CDRComparisonSettingsEditorDirective);
+
+})(app);
