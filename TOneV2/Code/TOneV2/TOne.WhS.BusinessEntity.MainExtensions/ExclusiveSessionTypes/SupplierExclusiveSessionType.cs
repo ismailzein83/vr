a9e17ca5845a584e7536b124855ca0c75eff4e40
Mortada Issa <VANRISE\mortada.issa>
2017-12-07 14:59:40 +0000

    Adding basic files for exclusive session types
    git-tfs-id: [http://developmentvlan:8585/tfs/vanrise.collection]$/;C44916

---
+++ TOneV2/Code/TOneV2/TOne.WhS.BusinessEntity.MainExtensions/ExclusiveSessionTypes/SupplierExclusiveSessionType.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Vanrise.Entities;
+
+namespace TOne.WhS.BusinessEntity.MainExtensions
+{
+    public class SupplierExclusiveSessionType : VRExclusiveSessionTypeExtendedSettings
+    {
+        public override Guid ConfigId
+        {
+            get { throw new NotImplementedException(); }
+        }
+
+        public override bool DoesUserHaveAdminAccess(IVRExclusiveSessionDoesUserHaveAdminAccessContext context)
+        {
+            throw new NotImplementedException();
+        }
+
+        public override bool DoesUserHaveTakeAccess(IVRExclusiveSessionDoesUserHaveTakeAccessContext context)
+        {
+            throw new NotImplementedException();
+        }
+
+        public override string GetTargetName(IVRExclusiveSessionGetTargetNameContext context)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
