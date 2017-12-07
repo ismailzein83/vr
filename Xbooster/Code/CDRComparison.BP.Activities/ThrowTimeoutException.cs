b27c7e7a989563d9ebabb855bd90ea37aa7de6f5
mohamad.elhariri <mohamad.elhariri@vanrise.tfs.local>
2017-12-07 17:12:35 +0000

    Threw a VRBusinessException from a custom activity instead of the built-in TerminateWorkflow activity in order to display a more user-friendly error message
    git-tfs-id: [http://developmentvlan:8585/tfs/vanrise.collection]$/;C44925

---
+++ Xbooster/Code/CDRComparison.BP.Activities/ThrowTimeoutException.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Activities;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CDRComparison.BP.Activities
+{
+    public class ThrowTimeoutException : CodeActivity
+    {
+        protected override void Execute(CodeActivityContext context)
+        {
+            throw new Vanrise.Entities.VRBusinessException("Task timed out");
+        }
+    }
+}
