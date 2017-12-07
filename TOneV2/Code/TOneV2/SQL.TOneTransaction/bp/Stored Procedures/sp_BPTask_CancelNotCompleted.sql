3bf9352287fce0e79b311d6e01a489ac234a21bc
Wissam Ajamy <VANRISE\wissam.ajamy>
2017-12-07 07:18:16 +0000

    Update structure
    git-tfs-id: [http://developmentvlan:8585/tfs/vanrise.collection]$/;C44898

---
+++ TOneV2/Code/TOneV2/SQL.TOneTransaction/bp/Stored Procedures/sp_BPTask_CancelNotCompleted.sql
@@ -0,0 +1,9 @@
+﻿CREATE PROCEDURE [bp].[sp_BPTask_CancelNotCompleted]
+       @ProcessInstanceId bigint
+AS
+BEGIN
+       -- Status: 50 = Completed, 60 = Cancelled
+       UPDATE	[bp].[BPTask] 
+	   SET		[Status] = 60 
+	   WHERE	ProcessInstanceID = @ProcessInstanceId and [Status] != 50
+END
