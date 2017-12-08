3ef198ad363d96a20725f60c95daf4708cb05345
Rodi Hassan <VANRISE\rodi.hassan>
2017-12-08 09:14:52 +0000

    Add files for exclusive session type
    git-tfs-id: [http://developmentvlan:8585/tfs/vanrise.collection]$/;C44932

---
+++ Vanrise/Code/Vanrise.Common.Web/Common/Directives/VRExclusiveSessionType/vr-common-exclusivesessiontype-extendedsettings.js
@@ -0,0 +1,131 @@
+﻿//(function (app) {
+
+//        'use strict';
+
+//        VRExclusiveSessionTypeExtendedSettingsDirective.$inject = ['VRCommon_VRExclusiveSessionTypeAPIService', 'UtilsService', 'VRUIUtilsService'];
+
+//        function VRExclusiveSessionTypeExtendedSettingsDirective(VRCommon_VRExclusiveSessionTypeAPIService, UtilsService, VRUIUtilsService) {
+//            return {
+//                restrict: "E",
+//                scope: {
+//                    onReady: "=",
+//                    normalColNum: '@',
+//                    label: '@',
+//                    customvalidate: '='
+//                },
+//                controller: function ($scope, $element, $attrs) {
+//                    var ctrl = this;
+//                    var ctor = new VRExclusiveSessionTypeExtendedSettingsCtor($scope, ctrl, $attrs);
+//                    ctor.initializeController();
+//                },
+//                controllerAs: "ctrl",
+//                bindToController: true,
+//                templateUrl: '/Client/Modules/Common/Directives/VRExclusiveSessionType/Templates/VRExclusiveSessionTypeExtendedSettingsTemplate.html'
+//            };
+
+//            function VRExclusiveSessionTypeExtendedSettingsCtor($scope, ctrl, $attrs) {
+//                this.initializeController = initializeController;
+
+//                var selectorAPI;
+
+//                var directiveAPI;
+//                var directiveReadyDeferred;
+
+//                function initializeController() {
+//                    $scope.scopeModel = {};
+//                    $scope.scopeModel.templateConfigs = [];
+//                    $scope.scopeModel.selectedTemplateConfig;
+
+//                    $scope.scopeModel.onSelectorReady = function (api) {
+//                        selectorAPI = api;
+//                        defineAPI();
+//                    };
+
+//                    $scope.scopeModel.onDirectiveReady = function (api) {
+//                        directiveAPI = api;
+//                        var setLoader = function (value) {
+//                            $scope.scopeModel.isLoadingDirective = value;
+//                        };
+//                        var directivePayload = {
+//                        };
+//                        VRUIUtilsService.callDirectiveLoadOrResolvePromise($scope, directiveAPI, directivePayload, setLoader, directiveReadyDeferred);
+//                    };
+//                }
+//                function defineAPI() {
+//                    var api = {};
+
+//                    api.load = function (payload) {
+//                        selectorAPI.clearDataSource();
+
+//                        var promises = [];
+//                        var vrExclusiveSessionTypeExtendedSettings;
+
+//                        if (payload != undefined) {
+//                            vrExclusiveSessionTypeExtendedSettings = payload.vrExclusiveSessionTypeExtendedSettings;
+//                        }
+
+//                        var getVRExclusiveSessionTypeExtendedSettingsConfigsPromise = GetVRExclusiveSessionTypeExtendedSettingsConfigsPromise();
+//                        promises.push(getVRExclusiveSessionTypeExtendedSettingsConfigsPromise);
+
+//                        if (vrExclusiveSessionTypeExtendedSettings != undefined) {
+//                            var loadDirectivePromise = loadDirective();
+//                            promises.push(loadDirectivePromise);
+//                        }
+
+
+//                        function GetVRExclusiveSessionTypeExtendedSettingsConfigsPromise() {
+//                            return VRCommon_VRExclusiveSessionTypeAPIService.GetVRExclusiveSessionTypeExtendedSettingsConfigs().then(function (response) {
+//                                if (response != undefined) {
+//                                    for (var i = 0; i < response.length; i++) {
+//                                        $scope.scopeModel.templateConfigs.push(response[i]);
+//                                    }
+//                                    if (vrExclusiveSessionTypeExtendedSettings != undefined) {
+//                                        $scope.scopeModel.selectedTemplateConfig =
+//                                            UtilsService.getItemByVal($scope.scopeModel.templateConfigs, vrExclusiveSessionTypeExtendedSettings.ConfigId, 'ExtensionConfigurationId');
+//                                    }
+//                                }
+//                            });
+//                        }
+//                        function loadDirective() {
+//                            directiveReadyDeferred = UtilsService.createPromiseDeferred();
+
+//                            var directiveLoadDeferred = UtilsService.createPromiseDeferred();
+
+//                            directiveReadyDeferred.promise.then(function () {
+//                                directiveReadyDeferred = undefined;
+
+//                                var directivePayload = {
+//                                    vrExclusiveSessionTypeExtendedSettings: vrExclusiveSessionTypeExtendedSettings
+//                                };
+//                                VRUIUtilsService.callDirectiveLoad(directiveAPI, directivePayload, directiveLoadDeferred);
+//                            });
+
+//                            return directiveLoadDeferred.promise;
+//                        }
+
+//                        return UtilsService.waitMultiplePromises(promises);
+//                    };
+
+//                    api.getData = function () {
+//                        var data;
+
+//                        if ($scope.scopeModel.selectedTemplateConfig != undefined && directiveAPI != undefined) {
+
+//                            data = directiveAPI.getData();
+//                            if (data != undefined) {
+//                                data.ConfigId = $scope.scopeModel.selectedTemplateConfig.ExtensionConfigurationId;
+//                            }
+//                        }
+//                        return data;
+//                    };
+
+//                    if (ctrl.onReady != null) {
+//                        ctrl.onReady(api);
+//                    }
+//                }
+//            }
+//        }
+
+//        app.directive('vrCommonExclusivesessiontypeExtendedsettings', VRExclusiveSessionTypeExtendedSettingsDirective);
+
+//    })(app);
---
+++ Vanrise/Code/Vanrise.Common.Web/Common/Directives/VRExclusiveSessionType/vr-common-exclusivesessiontype-settings.js
@@ -0,0 +1,93 @@
+﻿//'use strict';
+
+//app.directive('vrCommonExclusivesessiontypeSettings', ['UtilsService', 'VRUIUtilsService',
+//    function (UtilsService, VRUIUtilsService) {
+//        return {
+//            restrict: 'E',
+//            scope: {
+//                onReady: '=',
+//            },
+//            controller: function ($scope, $element, $attrs) {
+//                var ctrl = this;
+//                var ctor = new VRExclusiveSessionTypeSettingsCtor($scope, ctrl, $attrs);
+//                ctor.initializeController();
+//            },
+//            controllerAs: 'ctrl',
+//            bindToController: true,
+//            templateUrl: '/Client/Modules/Common/Directives/VRExclusiveSessionType/Templates/VRExclusiveSessionTypeSettingsTemplate.html'
+//        };
+
+//        function VRExclusiveSessionTypeSettingsCtor($scope, ctrl, $attrs) {
+//            this.initializeController = initializeController;
+
+//            var vrExclusiveSessionTypeExtendedSettingsDirectiveAPI;
+//            var vrExclusiveSessionTypeExtendedSettingsDirectiveReadyDeferred = UtilsService.createPromiseDeferred();
+
+//            function initializeController() {
+//                $scope.scopeModel = {};
+
+//                $scope.scopeModel.onVRExclusiveSessionTypeExtendedSettingsDirectiveReady = function (api) {
+//                    vrExclusiveSessionTypeExtendedSettingsDirectiveAPI = api;
+//                    vrExclusiveSessionTypeExtendedSettingsDirectiveReadyDeferred.resolve();
+//                };
+
+//                defineAPI();
+//            }
+//            function defineAPI() {
+//                var api = {};
+
+//                api.load = function (payload) {
+//                    var promises = [];
+
+//                    var vrExclusiveSessionTypeSettings, vrExclusiveSessionTypeExtendedSettings;
+
+//                    if (payload != undefined) {
+//                        var vrExclusiveSessionTypeEntity = payload.componentType;
+
+//                        if (vrExclusiveSessionTypeEntity != undefined) {
+//                            $scope.scopeModel.name = vrExclusiveSessionTypeEntity.Name;
+//                            vrExclusiveSessionTypeSettings = vrExclusiveSessionTypeEntity.Settings;
+//                        }
+
+//                        if (vrExclusiveSessionTypeSettings != undefined) {
+//                            vrExclusiveSessionTypeExtendedSettings = vrExclusiveSessionTypeSettings.ExtendedSettings;
+//                        }
+//                    }
+
+//                    //Loading ParentBEDefinitionRuntimeSelectorSettings selector
+//                    var vrExclusiveSessionTypeExtendedSettingsDirectiveLoadPromise = getVRExclusiveSessionTypeExtendedSettingsDirectiveLoadPromise();
+//                    promises.push(vrExclusiveSessionTypeExtendedSettingsDirectiveLoadPromise);
+
+
+//                    function getVRExclusiveSessionTypeExtendedSettingsDirectiveLoadPromise() {
+//                        var vrExclusiveSessionTypeExtendedSettingsDirectiveLoadDeferred = UtilsService.createPromiseDeferred();
+
+//                        vrExclusiveSessionTypeExtendedSettingsDirectiveReadyDeferred.promise.then(function () {
+//                            var vrExclusiveSessionTypeExtendedSettingsDirectivePayload = {
+//                                vrExclusiveSessionTypeExtendedSettings: vrExclusiveSessionTypeExtendedSettings
+//                            };
+//                            VRUIUtilsService.callDirectiveLoad(vrExclusiveSessionTypeExtendedSettingsDirectiveAPI, vrExclusiveSessionTypeExtendedSettingsDirectivePayload, vrExclusiveSessionTypeExtendedSettingsDirectiveLoadDeferred);
+//                        });
+
+//                        return vrExclusiveSessionTypeExtendedSettingsDirectiveLoadDeferred.promise;
+//                    }
+
+//                    return UtilsService.waitMultiplePromises(promises);
+//                };
+
+//                api.getData = function () {
+
+//                    return {
+//                        Name: $scope.scopeModel.name,
+//                        Settings: {
+//                            $type: "Vanrise.Entities.VRExclusiveSessionTypeSettings, Vanrise.Entities",
+//                            ExtendedSettings: vrExclusiveSessionTypeExtendedSettingsDirectiveAPI.getData()
+//                        }
+//                    };
+//                };
+
+//                if (ctrl.onReady != null)
+//                    ctrl.onReady(api);
+//            }
+//        }
+//    }]);
