8b63c2b250fbd6d1b931070b081ef58b49daf97e
Ismail Zein <VANRISE\ismail.zein>
2017-12-08 08:48:24 +0000

    sql project
    git-tfs-id: [http://developmentvlan:8585/tfs/vanrise.collection]$/;C44931

--- TOneV2/Code/TOneV2/SQL.TOneTransaction/runtime/Tables/RuntimeServiceInstance.sql
+++ TOneV2/Code/TOneV2/SQL.TOneTransaction/runtime/Tables/RuntimeServiceInstance.sql
@@ -5,6 +5,9 @@
     [ServiceInstanceInfo] NVARCHAR (MAX)   NULL,
     [timestamp]           ROWVERSION       NULL,
     [CreatedTime]         DATETIME         CONSTRAINT [DF_RuntimeService_CreatedTime] DEFAULT (getdate()) NULL,
-    CONSTRAINT [PK_RuntimeService] PRIMARY KEY CLUSTERED ([ID] ASC)
+    CONSTRAINT [PK_RuntimeService] PRIMARY KEY CLUSTERED ([ID] ASC),
+    CONSTRAINT [FK_RuntimeServiceInstance_RunningProcess] FOREIGN KEY ([ProcessID]) REFERENCES [runtime].[RunningProcess] ([ID])
 );
+
+
 
