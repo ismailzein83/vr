c6f793a5d2cdcace63291fb983b0c94a51e9e6b7
Rodi Hassan <VANRISE\rodi.hassan>
2017-12-13 08:40:25 +0000

    add files for sale are and supplier exclusive session
    git-tfs-id: [http://developmentvlan:8585/tfs/vanrise.collection]$/;C45013

--- TOneV2/Code/TOneV2/TOne.WhS.BusinessEntity.Web/WhS_BusinessEntity/Directives/ExclusiveSessionTypes/whs-be-saleareaexclusivesessiontype-editor.js
+++ TOneV2/Code/TOneV2/TOne.WhS.BusinessEntity.Web/WhS_BusinessEntity/Directives/ExclusiveSessionTypes/whs-be-saleareaexclusivesessiontype-editor.js
@@ -1,1 +1,52 @@
-﻿
+﻿//'use strict';
+
+//app.directive('whsBeSaleareaexclusivesessiontypeEditor', ['UtilsService', 'VRUIUtilsService',
+//    function (UtilsService, VRUIUtilsService) {
+//        return {
+//            restrict: 'E',
+//            scope: {
+//                onReady: '=',
+//            },
+//            controller: function ($scope, $element, $attrs) {
+//                var ctrl = this;
+//                var ctor = new SaleAreaExclusiveSessionTypeCtor($scope, ctrl, $attrs);
+//                ctor.initializeController();
+//            },
+//            controllerAs: 'ctrl',
+//            bindToController: true,
+//            templateUrl: '/Client/Modules/WhS_BusinessEntity/Directives/ExclusiveSessionTypes/Templates/SaleAreaExclusiveSessionTypeEditorTemplate.html'
+//        };
+
+//        function SaleAreaExclusiveSessionTypeCtor($scope, ctrl, $attrs) {
+//            this.initializeController = initializeController;
+
+//            function initializeController() {
+//                $scope.scopeModel = {};
+
+//                defineAPI();
+//            }
+//            function defineAPI() {
+//                var api = {};
+
+//                api.load = function (payload) {
+//                    var promises = [];
+
+//                    if (payload != undefined) {
+
+//                    }
+
+//                    return UtilsService.waitMultiplePromises(promises);
+//                };
+
+//                api.getData = function () {
+
+//                    return {
+//                        $type: "TOne.WhS.BusinessEntity.MainExtensions.SaleAreaExclusiveSessionType, TOne.WhS.BusinessEntity.MainExtensions"
+//                    };
+//                };
+
+//                if (ctrl.onReady != null)
+//                    ctrl.onReady(api);
+//            }
+//        }
+//    }]);
--- TOneV2/Code/TOneV2/TOne.WhS.BusinessEntity.Web/WhS_BusinessEntity/Directives/ExclusiveSessionTypes/whs-be-supplierexclusivesessiontype-editor.js
+++ TOneV2/Code/TOneV2/TOne.WhS.BusinessEntity.Web/WhS_BusinessEntity/Directives/ExclusiveSessionTypes/whs-be-supplierexclusivesessiontype-editor.js
@@ -1,1 +1,52 @@
-﻿
+﻿//'use strict';
+
+//app.directive('whsBeSupplierexclusivesessiontypeEditor', ['UtilsService', 'VRUIUtilsService',
+//    function (UtilsService, VRUIUtilsService) {
+//        return {
+//            restrict: 'E',
+//            scope: {
+//                onReady: '=',
+//            },
+//            controller: function ($scope, $element, $attrs) {
+//                var ctrl = this;
+//                var ctor = new SupplierExclusiveSessionTypeCtor($scope, ctrl, $attrs);
+//                ctor.initializeController();
+//            },
+//            controllerAs: 'ctrl',
+//            bindToController: true,
+//            templateUrl: '/Client/Modules/WhS_BusinessEntity/Directives/ExclusiveSessionTypes/Templates/SupplierExclusiveSessionTypeEditorTemplate.html'
+//        };
+
+//        function SupplierExclusiveSessionTypeCtor($scope, ctrl, $attrs) {
+//            this.initializeController = initializeController;
+
+//            function initializeController() {
+//                $scope.scopeModel = {};
+
+//                defineAPI();
+//            }
+//            function defineAPI() {
+//                var api = {};
+
+//                api.load = function (payload) {
+//                    var promises = [];
+
+//                    if (payload != undefined) {
+
+//                    }
+
+//                    return UtilsService.waitMultiplePromises(promises);
+//                };
+
+//                api.getData = function () {
+
+//                    return {
+//                        $type: "TOne.WhS.BusinessEntity.MainExtensions.SupplierExclusiveSessionType, TOne.WhS.BusinessEntity.MainExtensions"
+//                    };
+//                };
+
+//                if (ctrl.onReady != null)
+//                    ctrl.onReady(api);
+//            }
+//        }
+//    }]);
