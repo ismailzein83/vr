bfeb41e0d86c5f2d45ddca96d6544e847fa16769
Rodi Hassan <VANRISE\rodi.hassan>
2017-12-13 09:14:37 +0000

    add SessionLockSettings files
    git-tfs-id: [http://developmentvlan:8585/tfs/vanrise.collection]$/;C45017

---
+++ Vanrise/Code/Vanrise.Entities/Settings/SessionLockSettings/SessionLockSettings.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Vanrise.Entities
+{
+    public class SessionLockSettings : SettingData
+    {
+        public int TimeOutInSeconds { get; set; }
+        public int HeartbeatIntervalInSeconds { get; set; }
+    }
+}
