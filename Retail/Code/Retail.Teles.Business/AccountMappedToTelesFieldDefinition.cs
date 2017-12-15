57855c6321644c97f466caf08b7bf24835c8a4e0
Samer Haidar <VANRISE\samer.haidar>
2017-12-15 12:32:29 +0000

    Implementing AccountMappedToTelesFieldDefinition
    git-tfs-id: [http://developmentvlan:8585/tfs/vanrise.collection]$/;C45053

---
+++ Retail/Code/Retail.Teles.Business/AccountMappedToTelesFieldDefinition.cs
@@ -0,0 +1,25 @@
+﻿using Retail.BusinessEntity.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Retail.Teles.Business
+{
+    public class AccountMappedToTelesFieldDefinition : AccountExtraFieldDefinitionSettings
+    {
+        public override Guid ConfigId
+        {
+            get { return new Guid("B50FF2AB-E6D2-44A4-9681-F119FEC8ABFC"); }
+        }
+        public string FieldName { get; set; }
+        public string FieldTitle { get; set; }
+        public override IEnumerable<AccountGenericField> GetFields(IAccountExtraFieldSettingsContext context)
+        {
+            List<AccountGenericField> accountGenericFields = new List<AccountGenericField>();
+            accountGenericFields.Add(new AccountMappedToTelesGenericField(FieldName, FieldTitle));
+            return accountGenericFields;
+        }
+    }
+}
---
+++ Retail/Code/Retail.Teles.Business/AccountMappedToTelesGenericField.cs
@@ -0,0 +1,51 @@
+﻿using Retail.BusinessEntity.Business;
+using Retail.BusinessEntity.Entities;
+using Retail.Teles.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Retail.Teles.Business
+{
+    public class AccountMappedToTelesGenericField : AccountGenericField
+    {
+        string _fieldName;
+        string _fieldTitle;
+        public AccountMappedToTelesGenericField(string fieldName, string fieldTitle)
+        {
+            _fieldName = fieldName;
+            _fieldTitle = fieldTitle;
+        }
+        public override string Name
+        {
+            get { return _fieldName; }
+        }
+
+        public override string Title
+        {
+            get { return _fieldTitle; }
+        }
+
+        public override Vanrise.GenericData.Entities.DataRecordFieldType FieldType
+        {
+            get { return new Vanrise.GenericData.MainExtensions.DataRecordFields.FieldBooleanType(); }
+        }
+
+        public override dynamic GetValue(IAccountGenericFieldContext context)
+        {
+            AccountBEManager accountBEManager = new AccountBEManager();
+            var enterpriseAccountMappingInfo = accountBEManager.GetExtendedSettings<EnterpriseAccountMappingInfo>(context.Account);
+            if (enterpriseAccountMappingInfo != null)
+                return true;
+            var siteAccountMappingInfo = accountBEManager.GetExtendedSettings<SiteAccountMappingInfo>(context.Account);
+            if (siteAccountMappingInfo != null)
+                return true;
+            var userAccountMappingInfo = accountBEManager.GetExtendedSettings<UserAccountMappingInfo>(context.Account);
+            if (userAccountMappingInfo != null)
+                return true;
+            return false;
+        }
+    }
+}
