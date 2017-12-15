9d738f3e206b41c5ecc7a2d7eb0334e3501e7785
Ali Atoui <VANRISE\ali.atoui>
2017-12-15 14:03:13 +0000

    Arranging AnalyticReportManager
    git-tfs-id: [http://developmentvlan:8585/tfs/vanrise.collection]$/;C45057

--- Vanrise/Code/Vanrise.Analytic.Business/AnalyticReportManager.cs
+++ Vanrise/Code/Vanrise.Analytic.Business/AnalyticReportManager.cs
@@ -1,14 +1,13 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using Vanrise.Common;
-using Vanrise.Entities;
+using Vanrise.Analytic.Data;
 using Vanrise.Analytic.Entities;
 using Vanrise.Caching;
-using Vanrise.Analytic.Data;
+using Vanrise.Common;
 using Vanrise.Common.Business;
+using Vanrise.Entities;
+
 namespace Vanrise.Analytic.Business
 {
     public class AnalyticReportManager : IAnalyticReportManager
@@ -20,6 +19,7 @@ namespace Vanrise.Analytic.Business
         #endregion
 
         #region Public Methods
+
         public Vanrise.Entities.IDataRetrievalResult<AnalyticReportDetail> GetFilteredAnalyticReports(Vanrise.Entities.DataRetrievalInput<AnalyticReportQuery> input)
         {
             var analyticReports = GetCachedAnalyticReports();
@@ -29,6 +29,7 @@ namespace Vanrise.Analytic.Business
             VRActionLogger.Current.LogGetFilteredAction(AnalyticReportLoggableEntity.Instance, input);
             return Vanrise.Common.DataRetrievalManager.Instance.ProcessResult(input, analyticReports.ToBigResult(input, filterExpression, AnalyticReportDetailMapper));
         }
+
         public IEnumerable<AnalyticReportInfo> GetAnalyticReportsInfo(AnalyticReportInfoFilter filter)
         {
             var analyticReports = GetCachedAnalyticReports();
@@ -50,11 +51,13 @@ namespace Vanrise.Analytic.Business
                return analyticReport.Name;
             return null;
         }
+
         public AnalyticReport GetAnalyticReportById(Guid analyticReportId)
         {
             var analyticReports = GetCachedAnalyticReports();
             return analyticReports.GetRecord(analyticReportId);
         }
+
         public Vanrise.Entities.InsertOperationOutput<AnalyticReportDetail> AddAnalyticReport(AnalyticReport analyticReport)
         {
             InsertOperationOutput<AnalyticReportDetail> insertOperationOutput = new Vanrise.Entities.InsertOperationOutput<AnalyticReportDetail>();
@@ -103,11 +106,13 @@ namespace Vanrise.Analytic.Business
 
             return updateOperationOutput;
         }
+        
         public IEnumerable<AnalyticReportConfiguration> GetAnalyticReportConfigTypes()
         {
             ExtensionConfigurationManager manager = new ExtensionConfigurationManager();
             return manager.GetExtensionConfigurations<AnalyticReportConfiguration>(AnalyticReportConfiguration.EXTENSION_TYPE);
         }
+
         public AnalyticReportConfiguration GetAnalyticReportConfigTypeByName(string name)
         {
             ExtensionConfigurationManager manager = new ExtensionConfigurationManager();
@@ -116,6 +121,7 @@ namespace Vanrise.Analytic.Business
                 throw new NullReferenceException("analyticReportConfiguration");
             return analyticReportConfiguration;
         }
+
         #endregion
 
         #region Private Methods
@@ -130,6 +136,7 @@ namespace Vanrise.Analytic.Business
                     return analyticReports.ToDictionary(x => x.AnalyticReportId, analyticReport => analyticReport);
                 });
         }
+
         #endregion
 
         #region Private Classes
@@ -193,6 +200,7 @@ namespace Vanrise.Analytic.Business
         #endregion
 
         #region Mappers
+
         AnalyticReportDetail AnalyticReportDetailMapper(AnalyticReport analyticReport)
         {
             return new AnalyticReportDetail
@@ -208,6 +216,7 @@ namespace Vanrise.Analytic.Business
                 Name = analyticReport.Name,
             };
         }
+
         #endregion
     }
 }
