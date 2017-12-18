a95b3a9a3682a111ac489b2ecf10fba8aab1c887
Rodi Hassan <VANRISE\rodi.hassan>
2017-12-18 14:51:17 +0000

    add pop3 pricelist filter files
    git-tfs-id: [http://developmentvlan:8585/tfs/vanrise.collection]$/;C45069

---
+++ TOneV2/Code/TOneV2/TOne.WhS.BusinessEntity.Web/WhS_BusinessEntity/Directives/MainExtensions/POP3Filter/vr-whs-be-pop3supplierpricelistfilter.js
@@ -0,0 +1,52 @@
+﻿'use strict';
+
+app.directive('vrWhsBePop3supplierpricelistfilter', ['UtilsService', 'VRUIUtilsService',
+    function (UtilsService, VRUIUtilsService) {
+        return {
+            restrict: 'E',
+            scope: {
+                onReady: '=',
+            },
+            controller: function ($scope, $element, $attrs) {
+                var ctrl = this;
+                var ctor = new Pop3supplierpricelistfilter($scope, ctrl, $attrs);
+                ctor.initializeController();
+            },
+            controllerAs: 'ctrl',
+            bindToController: true,
+            templateUrl: '/Client/Modules/WhS_BusinessEntity/Directives/MainExtensions/POP3Filter/Templates/POP3SupplierPricelistFilterTemplate.html'
+        };
+
+        function Pop3supplierpricelistfilter($scope, ctrl, $attrs) {
+            this.initializeController = initializeController;
+
+            function initializeController() {
+                $scope.scopeModel = {};
+
+                defineAPI();
+            }
+            function defineAPI() {
+                var api = {};
+
+                api.load = function (payload) {
+                    var promises = [];
+
+                    if (payload != undefined) {
+
+                    }
+
+                    return UtilsService.waitMultiplePromises(promises);
+                };
+
+                api.getData = function () {
+
+                    return {
+                        $type: "TOne.WhS.BusinessEntity.MainExtensions.POP3, TOne.WhS.BusinessEntity.MainExtensions"
+                    };
+                };
+
+                if (ctrl.onReady != null)
+                    ctrl.onReady(api);
+            }
+        }
+    }]);
