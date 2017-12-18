6b8035d0b9f2593e609835482256c1eab39824fd
Rodi Hassan <VANRISE\rodi.hassan>
2017-12-18 14:07:47 +0000

    add pop3 SPL filter
    git-tfs-id: [http://developmentvlan:8585/tfs/vanrise.collection]$/;C45068

---
+++ TOneV2/Code/TOneV2/TOne.WhS.BusinessEntity.Entities/POP3Filter/POP3SupplierPricelistFilter.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TOne.WhS.BusinessEntity.Entities.POP3Filter
+{
+    class POP3SupplierPricelistFilter
+    {
+    }
+}
