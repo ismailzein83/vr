2836bc8df7133d6891b38e5fe489c913a1be310c
Wissam Ajamy <VANRISE\wissam.ajamy>
2017-12-21 12:16:53 +0000

    Add new SQL grouping
    git-tfs-id: [http://developmentvlan:8585/tfs/vanrise.collection]$/;C45121

--- Vanrise/Code/Resources/BatchScripts/Retail.Zajil/08-GroupSQLFiles.bat
+++ Vanrise/Code/Resources/BatchScripts/Retail.Zajil/08-GroupSQLFiles.bat
@@ -1,3 +1,3 @@
 ECHO OFF
 ECHO Retail.Zajil Group SQL Scripts
-start /b /w /D"C:\TFS\Vanrise\Code\Vanrise.HelperTools\bin\Release" Vanrise.HelperTools.exe "GRPSQLOverridden" "Retail.Zajil"
+start /b /w /D"C:\TFS\Vanrise\Code\Vanrise.HelperTools\bin\Release" Vanrise.HelperTools.exe "GRPSQL" "Retail.Zajil"
