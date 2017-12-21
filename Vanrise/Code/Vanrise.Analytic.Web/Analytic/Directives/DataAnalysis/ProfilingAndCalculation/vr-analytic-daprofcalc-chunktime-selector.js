689ba7ba0e0d0ba60c8859a228c1941ca83ff01e
anthony douaihy <VANRISE\anthony.douaihy>
2017-12-21 15:46:01 +0000

    add vrAnalyticDaprofcalcChunktimeSelector
    git-tfs-id: [http://developmentvlan:8585/tfs/vanrise.collection]$/;C45134

---
+++ Vanrise/Code/Vanrise.Analytic.Web/Analytic/Directives/DataAnalysis/ProfilingAndCalculation/vr-analytic-daprofcalc-chunktime-selector.js
@@ -0,0 +1,100 @@
+﻿'use strict';
+
+app.directive('vrAnalyticDaprofcalcChunktimeSelector', ['UtilsService', 'VRUIUtilsService', 'DAProfCalcChunkTimeEnum',
+    function (UtilsService, VRUIUtilsService, DAProfCalcChunkTimeEnum) {
+        return {
+            restrict: 'E',
+            scope: {
+                onReady: '=',
+                ismultipleselection: '@',
+                selectedvalues: '=',
+                onselectionchanged: '=',
+                onselectitem: '=',
+                ondeselectitem: '=',
+                isrequired: '=',
+                hideremoveicon: '@',
+                normalColNum: '@',
+                customvalidate: '='
+            },
+            controller: function ($scope, $element, $attrs) {
+
+                var ctrl = this;
+                ctrl.datasource = [];
+
+                ctrl.selectedvalues;
+                if ($attrs.ismultipleselection != undefined)
+                    ctrl.selectedvalues = [];
+
+                var ctor = new ChunkTimeCtor(ctrl, $scope, $attrs);
+                ctor.initializeController();
+            },
+            controllerAs: 'ctrl',
+            bindToController: true,
+            template: function (element, attrs) {
+                return getTemplate(attrs);
+            }
+        };
+
+        function ChunkTimeCtor(ctrl, $scope, attrs) {
+            this.initializeController = initializeController;
+
+            var selectorAPI;
+
+            function initializeController() {
+
+                ctrl.onSelectorReady = function (api) {
+                    selectorAPI = api;
+                    defineAPI();
+                };
+            }
+
+            function defineAPI() {
+                var api = {};
+
+                api.load = function (payload) {
+                    selectorAPI.clearDataSource();
+
+                    var selectedIds;
+                    var filter;
+
+                    if (payload != undefined) {
+                        selectedIds = payload.selectedIds;
+                        filter = payload.filter;
+                    }
+
+                    ctrl.datasource = UtilsService.getArrayEnum(DAProfCalcChunkTimeEnum);
+
+                    if (selectedIds != undefined) {
+                        VRUIUtilsService.setSelectedValues(selectedIds, 'value', attrs, ctrl);
+                    }
+                };
+
+                api.getSelectedIds = function () {
+                    return VRUIUtilsService.getIdSelectedIds('value', attrs, ctrl);
+                };
+
+                if (ctrl.onReady != null)
+                    ctrl.onReady(api);
+            }
+        }
+
+        function getTemplate(attrs) {
+
+            var multipleselection = "";
+            var label = "ChunkTime";
+
+            if (attrs.ismultipleselection != undefined) {
+                label = "ChunkTimes";
+                multipleselection = "ismultipleselection";
+            }
+            if (attrs.customlabel != undefined)
+                label = attrs.customlabel;
+
+            return '<vr-columns colnum="{{ctrl.normalColNum}}">' +
+                        '<vr-select ' + multipleselection + ' datatextfield="description" datavaluefield="value" isrequired="ctrl.isrequired" label="' + label + '" datasource="ctrl.datasource" on-ready="ctrl.onSelectorReady" '
+                            + ' selectedvalues="ctrl.selectedvalues" onselectionchanged="ctrl.onselectionchanged" entityName="' + label + '" onselectitem="ctrl.onselectitem" ondeselectitem="ctrl.ondeselectitem" '
+                            + ' hideremoveicon="ctrl.hideremoveicon" customvalidate="ctrl.customvalidate">' +
+                        '</vr-select>' +
+                    '</vr-columns>';
+        }
+    }]);
