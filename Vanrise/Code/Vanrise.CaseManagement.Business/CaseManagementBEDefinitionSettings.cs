94469d764f2ac8973ad94ad12e1bb55bca49b2fd
Samer Haidar <VANRISE\samer.haidar>
2017-12-28 11:41:02 +0000

    Adding Case ManagementProjects
    git-tfs-id: [http://developmentvlan:8585/tfs/vanrise.collection]$/;C45180

---
+++ Vanrise/Code/Vanrise.CaseManagement.Business/CaseManagementBEDefinitionSettings.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Vanrise.GenericData.Entities;
+
+namespace Vanrise.CaseManagement.Business
+{
+    public class CaseManagementBEDefinitionSettings : BusinessEntityDefinitionSettings
+    {
+        public override Guid ConfigId
+        {
+            get { throw new NotImplementedException(); }
+        }
+    }
+}
