cabd208474e767c0994c8dce4c31c56359e16173
Rodi Hassan <VANRISE\rodi.hassan>
2018-01-02 15:56:23 +0000

    Implement rate plan sub process
    git-tfs-id: [http://developmentvlan:8585/tfs/vanrise.collection]$/;C45202

---
+++ TOneV2/Code/TOneV2/TOne.WhS.Sales.BP.Arguments/RatePlanSubProcessInput.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TOne.WhS.BusinessEntity.Entities;
+
+using TOne.WhS.BusinessEntity.Business;
+
+using TOne.WhS.Sales.Entities;
+
+
+namespace TOne.WhS.Sales.BP.Arguments
+{
+    public class RatePlanSubProcessInput : Vanrise.BusinessProcess.Entities.BaseProcessInputArgument
+    {
+        public SalePriceListOwnerType OwnerType { get; set; }
+
+        public int OwnerId { get; set; }
+
+        public bool IsAdditionaOwner { get; set; }
+
+        public int CurrencyId { get; set; }
+
+        public DateTime EffectiveDate { get; set; }
+
+        public Changes Changes { get; set; }
+
+        public override string EntityId
+        {
+            get
+            {
+                return String.Format("{0}_{1}", (int)OwnerType, OwnerId);
+            }
+        }
+
+        public override string GetTitle()
+        {
+            string ownerTypeDescription = Vanrise.Common.Utilities.GetEnumDescription(this.OwnerType);
+            string ownerName;
+
+            switch (OwnerType)
+            {
+                case SalePriceListOwnerType.SellingProduct:
+                    var sellingProductManager = new SellingProductManager();
+                    ownerName = sellingProductManager.GetSellingProductName(OwnerId);
+                    break;
+                case SalePriceListOwnerType.Customer:
+                    var carrierAccountManager = new CarrierAccountManager();
+                    ownerName = carrierAccountManager.GetCarrierAccountName(OwnerId);
+                    break;
+                default:
+                    throw new NotImplementedException();
+            }
+
+            return String.Format("Started the #BPDefinitionTitle# process for {0} '{1}'", ownerTypeDescription, ownerName);
+        }
+    }
+}
---
+++ TOneV2/Code/TOneV2/TOne.WhS.Sales.BP.Arguments/RatePlanSubProcessOutput.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TOne.WhS.Sales.BP.Arguments
+{
+    public class RatePlanSubProcessOutput
+    {
+        public bool ContinueRatePlanProcess { get; set; }
+    }
+}
