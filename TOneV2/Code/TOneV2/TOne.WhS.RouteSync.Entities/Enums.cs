ea89fc51aed091188287b78bedc6d53ae1f91a50
anthony douaihy <VANRISE\anthony.douaihy>
2018-01-05 13:08:07 +0000

    add new project for route sync proxy
    git-tfs-id: [http://developmentvlan:8585/tfs/vanrise.collection]$/;C45246

---
+++ TOneV2/Code/TOneV2/TOne.WhS.RouteSync.Entities/Enums.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TOne.WhS.RouteSync.Entities
+{
+    public enum SwitchType
+    {
+        Direct = 0,
+        SendToProxy = 1
+    }
+}
