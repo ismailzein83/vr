bdf5cd89c4c22e7eb52143035792c33349f65abf
Wissam Ajamy <VANRISE\wissam.ajamy>
2018-01-10 13:19:46 +0000

    remove SyncApp starting version 20180110
    git-tfs-id: [http://developmentvlan:8585/tfs/vanrise.collection]$/;C45288

--- Vanrise/Code/Resources/BatchScripts/TOneV2/20-SyncApp.bat
+++ Vanrise/Code/Resources/BatchScripts/TOneV2/20-SyncApp.bat
@@ -7,5 +7,5 @@ Set PARSEARG="eol=; tokens=1,2,3,4* delims=/, "
 For /F %PARSEARG% %%i in (%CURRDATE%) Do SET YYYYMMDD=%%l%%j%%k
 
 ECHO.
-ECHO TOneV2 SyncApp
-xcopy "C:\TFS\TOneV2\Code\TOneV2\TOne.WhS.DBSync.App\bin\Release" /y /v /z /i /Q /R  "\\192.168.110.185\Fixes\WebSite\TOneV2\SyncApp\%YYYYMMDD%\" /EXCLUDE:C:\TFS\Vanrise\Code\Resources\BatchScripts\TOneV2\SyncApp-list-of-excluded-files.txt
+::ECHO TOneV2 SyncApp
+::xcopy "C:\TFS\TOneV2\Code\TOneV2\TOne.WhS.DBSync.App\bin\Release" /y /v /z /i /Q /R  "\\192.168.110.185\Fixes\WebSite\TOneV2\SyncApp\%YYYYMMDD%\" /EXCLUDE:C:\TFS\Vanrise\Code\Resources\BatchScripts\TOneV2\SyncApp-list-of-excluded-files.txt
