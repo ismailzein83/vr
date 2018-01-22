65a7d530d6a88ad1786149ded31ca5b02ac12c49
anthony douaihy <VANRISE\anthony.douaihy>
2018-01-22 11:42:04 +0000

    adjust sp_CDRCost_UpadeOverridenAfterDate post script
    git-tfs-id: [http://developmentvlan:8585/tfs/vanrise.collection]$/;C45377

--- Retail/Code/RetailEDR/Retail_CDR/Stored Procedures/sp_CDRCost_UpadeOverridenAfterDate_1.sql
+++ Retail/Code/RetailEDR/Retail_CDR/Stored Procedures/sp_CDRCost_UpadeOverridenAfterDate_1.sql
@@ -2,15 +2,19 @@
 @FromTime datetime
 AS
 BEGIN
+
 WITH CDRCostTemp AS 
 (
-SELECT ID, SourceID
-FROM [Retail_CDR].[CDRCost]
-where (@FromTime is null or AttemptDateTime >= @FromTime)
+SELECT
+ID, SourceID
+FROM [Retail_CDR].[CDRCost] with(nolock,index=[IX_Retail_CDR_CDRCost_AttempteDateTime])
+where(@FromTime is null or AttemptDateTime >= @FromTime)
 )
+
 UPDATE cdrCost1
 SET IsDeleted = 1
-FROM [Retail_CDR].[CDRCost] cdrCost1
+FROM [Retail_CDR].[CDRCost] cdrCost1 with(nolock,index=[IX_Retail_CDR_CDRCost_AttempteDateTime],index=[IX_CDRCost_ID])
 JOIN CDRCostTemp cdrCost2 ON cdrCost1.SourceID = cdrCost2.SourceID AND cdrCost2.ID > cdrCost1.ID
 where (@FromTime is null or cdrCost1.AttemptDateTime >= @FromTime)
+
 END
