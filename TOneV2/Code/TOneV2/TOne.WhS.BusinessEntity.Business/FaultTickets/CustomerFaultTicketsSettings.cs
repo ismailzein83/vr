d827539e7546762cd3289319cc3b8153bf758420
Samer Haidar <VANRISE\samer.haidar>
2018-01-24 13:00:28 +0000

    Implementing CustomerFaultTicketsSettings
    git-tfs-id: [http://developmentvlan:8585/tfs/vanrise.collection]$/;C45392

---
+++ TOneV2/Code/TOneV2/TOne.WhS.BusinessEntity.Business/FaultTickets/CustomerFaultTicketsSettings.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Vanrise.GenericData.Entities;
+
+namespace TOne.WhS.BusinessEntity.Business
+{
+    public class CustomerFaultTicketsSettings : GenericBEExtendedSettings
+    {
+        public override Guid ConfigId
+        {
+            get { return new Guid("3705144E-4BC8-45D8-94D9-96E9AF95353B"); }
+        }
+        public override object GetInfoByType(IGenericBEExtendedSettingsContext context)
+        {
+            if (context.InfoType == null)
+                return null;
+
+            switch (context.InfoType)
+            {
+                case "SerialNumberPattern": return "test";
+                default: return null;
+            }
+        }
+    }
+}
