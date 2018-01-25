5112a16fa576452b9c8936a586dd3fdba743f225
rabih.fashwal:2 <rabih.fashwal:2@vanrise.tfs.local>
2018-01-25 13:35:48 +0000

    Update SOM Client
    git-tfs-id: [http://developmentvlan:8585/tfs/vanrise.collection]$/;C45417

--- SOM/Code/BPMExtended.Main.Business/SOMClient.cs
+++ SOM/Code/BPMExtended.Main.Business/SOMClient.cs
@@ -5,14 +5,26 @@ using System.Text;
 using System.Threading.Tasks;
 using Vanrise.Common;
 using Vanrise.Security.Entities;
+using Terrasoft.Core.Configuration;
+using System.Web;
+using Terrasoft.Core;
 
 namespace BPMExtended.Main.Business
 {
     public class SOMClient : IDisposable
     {
+        public UserConnection BPM_UserConnection
+        {
+            get
+            {
+                return (UserConnection)HttpContext.Current.Session["UserConnection"];
+            }
+        }
+
         AuthenticationToken _authToken;
         public SOMClient()
         {
+
             CredentialsInput credentialsInput = new CredentialsInput() { Email = "admin@vanrise.com", Password = "1" };
             var output = Vanrise.Common.VRWebAPIClient.Post<CredentialsInput, AuthenticateOperationOutput<AuthenticationToken>>(GetSOMBaseURL(),
                  "/api/VR_Sec/Security/Authenticate", credentialsInput, null);
@@ -23,7 +35,7 @@ namespace BPMExtended.Main.Business
             _authToken = output.AuthenticationObject;
         }
 
-        public Q Post<T,Q>(string actionPath, T request)
+        public Q Post<T, Q>(string actionPath, T request)
         {
             Dictionary<string, string> headers = new Dictionary<string, string>();
             AddAuthenticationTokenToHeader(headers);
@@ -32,6 +44,17 @@ namespace BPMExtended.Main.Business
 
         public T Get<T>(string actionPath)
         {
+
+            CustomerRequestManager customerRequestManager = new Business.CustomerRequestManager();
+            customerRequestManager.CreateLineSubscriptionRequest(Entities.CustomerObjectType.Account, Guid.NewGuid(), new SOM.Main.BP.Arguments.LineSubscriptionRequest
+            {
+                CabinetId = "C1",
+                DPId = "D1",
+                PhoneNumber = "112233",
+                SwitchId = "S1"
+
+            });
+
             Dictionary<string, string> headers = new Dictionary<string, string>();
             AddAuthenticationTokenToHeader(headers);
             return VRWebAPIClient.Get<T>(GetSOMBaseURL(), actionPath, headers);
@@ -39,7 +62,9 @@ namespace BPMExtended.Main.Business
 
         private string GetSOMBaseURL()
         {
-            return "http://localhost:5559";
+            object somBaseURL;
+            SysSettings.TryGetValue(this.BPM_UserConnection, "SOM_Base_Address", out somBaseURL);
+            return somBaseURL as string;//"http://localhost:5559";
         }
 
         private void AddAuthenticationTokenToHeader(Dictionary<string, string> headers)
@@ -49,7 +74,7 @@ namespace BPMExtended.Main.Business
 
         public void Dispose()
         {
-            
+
         }
     }
 }
---
+++ SOM/Code/SOM.Main.Business/Inventory/InventoryItem.cs
@@ -0,0 +1,134 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SOM.Main.Business
+{
+    #region Mock Data
+    public class InventoryItem
+    {
+        public string PhoneNumber { get; set; }
+        public string MainSwitchId { get; set; }
+        public string MainGate { get; set; }
+        public string MainPort { get; set; }
+        public List<InventorySwitch> Switches { get; set; }
+
+    }
+    public class InventorySwitch
+    {
+        public string SwitchId { get; set; }
+        public List<SwitchDetail> SwitchDetails { get; set; }
+    }
+    public class SwitchDetail
+    {
+        public string Gate { get; set; }
+        public string Port { get; set; }
+        public List<string> AvailableNumbers { get; set; }
+    }
+    public class GenerateInvtoryMockData
+    {
+        public static Dictionary<string, InventoryItem> GetMockInventoryDetail()
+        {
+            Dictionary<string, InventoryItem> result = new Dictionary<string, InventoryItem>();
+
+            result.Add("112233", new InventoryItem
+            {
+                MainGate = "MG 1",
+                MainPort = "MP 1",
+                MainSwitchId = "MS 1",
+                PhoneNumber = "112233",
+                Switches = GetRandomSwitches("112233")
+            });
+
+
+            result.Add("111111", new InventoryItem
+            {
+                MainGate = "MG 1",
+                MainPort = "MP 2",
+                MainSwitchId = "MS 2",
+                PhoneNumber = "111111",
+                Switches = GetRandomSwitches("111111")
+
+            });
+
+            result.Add("111122", new InventoryItem
+            {
+                MainGate = "MG 2",
+                MainPort = "MP 3",
+                MainSwitchId = "MS 3",
+                PhoneNumber = "111122",
+                Switches = GetRandomSwitches("111122")
+
+            });
+
+
+            result.Add("778899", new InventoryItem
+            {
+                MainGate = "MG 2",
+                MainPort = "MP 4",
+                MainSwitchId = "MS 4",
+                PhoneNumber = "778899",
+                Switches = GetRandomSwitches("778899")
+            });
+
+            return result;
+        }
+
+        static List<InventorySwitch> GetRandomSwitches(string p)
+        {
+            Random rnd = new Random();
+            int nbItems = rnd.Next(1, 10);
+            List<InventorySwitch> result = new List<InventorySwitch>();
+            for (int i = 0; i < nbItems; i++)
+            {
+                result.Add(GetRandomInventorySwitch(p));
+            }
+            return result;
+        }
+
+        static InventorySwitch GetRandomInventorySwitch(string phoneNumber)
+        {
+            Random rnd = new Random();
+
+            return new InventorySwitch
+            {
+                SwitchId = string.Format("SW {0}", rnd.Next(5, 15).ToString()),
+                SwitchDetails = GetSwitchDetailsList(phoneNumber, rnd.Next(1, 10))
+            };
+        }
+
+        static List<SwitchDetail> GetSwitchDetailsList(string phoneNumber, int p)
+        {
+            List<SwitchDetail> result = new List<SwitchDetail>();
+
+            for (int i = 0; i < p; i++)
+            {
+                result.Add(GetRandomSwitchDetail(phoneNumber));
+            }
+            return result;
+        }
+
+        static SwitchDetail GetRandomSwitchDetail(string phoneNumber)
+        {
+            Random rnd = new Random();
+            SwitchDetail result = new SwitchDetail
+            {
+                Port = string.Format("Port {0}", rnd.Next(1, 15).ToString()),
+                Gate = string.Format("Gate {0}", rnd.Next(1, 15).ToString()),
+                AvailableNumbers = new List<string>()
+            };
+            int nbOfItems = rnd.Next(0, 9);
+            for (int i = 0; i < nbOfItems; i++)
+            {
+                result.AvailableNumbers.Add(rnd.Next(111111, 999999).ToString());
+            }
+            return result;
+        }
+
+    }
+
+    #endregion
+
+}
