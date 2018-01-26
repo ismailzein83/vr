dc31a35c67a39fd2f12579e45753bf691b82826d
Samer Haidar <VANRISE\samer.haidar>
2018-01-26 14:55:06 +0000

    adding CustomerFaultTicketSettings
    git-tfs-id: [http://developmentvlan:8585/tfs/vanrise.collection]$/;C45449

---
+++ TOneV2/Code/TOneV2/TOne.WhS.BusinessEntity.Entities/FaultTickets/CustomerFaultTicketSettings.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TOne.WhS.BusinessEntity.Entities
+{
+    public class CustomerFaultTicketSettings
+    {
+        public List<CustomerFaultTicketDescriptionSetting> DescriptionSettings { get; set; }
+    }
+    public class  CustomerFaultTicketDescriptionSetting
+    {
+        public string CodeNumber { get; set; }
+        public Guid ReasonId { get; set; }
+        public Guid? InternationalReleaseCodeId { get; set; }
+    }
+}
