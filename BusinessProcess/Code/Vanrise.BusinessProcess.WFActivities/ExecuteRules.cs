d5b13b128a34ddd8138f97541b18fcf19efdcf9d
Rodi Hassan <VANRISE\rodi.hassan>
2018-01-26 10:37:45 +0000

    add option to get the rules from the parent process and option to do not throw exception even if there is rules violation
    git-tfs-id: [http://developmentvlan:8585/tfs/vanrise.collection]$/;C45437

--- BusinessProcess/Code/Vanrise.BusinessProcess.WFActivities/ExecuteRules.cs
+++ BusinessProcess/Code/Vanrise.BusinessProcess.WFActivities/ExecuteRules.cs
@@ -4,6 +4,7 @@ using System.Collections.Generic;
 using Vanrise.BusinessProcess.Entities;
 using Vanrise.BusinessProcess.Business;
 using Vanrise.Entities;
+using Vanrise.Common;
 
 namespace Vanrise.BusinessProcess.WFActivities
 {
@@ -19,6 +20,13 @@ namespace Vanrise.BusinessProcess.WFActivities
 
         public InArgument<string> ParentMessagePrefix { get; set; }
 
+        public InArgument<bool> GetRulesFromParentProcess { get; set; }
+
+        public InArgument<bool> DoNotThrowExceptionOnRulesViolation { get; set; }
+
+        public OutArgument<bool> ViolatedRulesExist { get; set; }
+
+
         protected override void Execute(CodeActivityContext context)
         {
             IEnumerable<IRuleTarget> importedDataToValidate = this.ImportedDataToValidate.Get(context);
@@ -29,16 +37,41 @@ namespace Vanrise.BusinessProcess.WFActivities
 
             List<BPViolatedRule> violatedBusinessRulesByTarget = new List<BPViolatedRule>(); ;
             bool stopExecutionFlag;
-            
+
+            bool getRulesFromParentProcess = this.GetRulesFromParentProcess.Get(context);
+            bool doNotThrowExceptionOnRulesViolation = this.DoNotThrowExceptionOnRulesViolation.Get(context);
+
             BPBusinessRuleDefinitionManager bpBusinessRuleManager = new BPBusinessRuleDefinitionManager();
-            List<BPBusinessRuleDefinition> bpBusinessRules = bpBusinessRuleManager.GetBPBusinessRuleDefinitions(businessRulesKey, context.GetSharedInstanceData().InstanceInfo.DefinitionID);
+            List<BPBusinessRuleDefinition> bpBusinessRules = new List<BPBusinessRuleDefinition>();
+            Guid rulesDefinitionId;
+
+            if (getRulesFromParentProcess)
+            {
+                var parentProcessId = context.GetSharedInstanceData().InstanceInfo.ParentProcessID;
+                var bpInstanceManager = new BPInstanceManager();
+                if (!parentProcessId.HasValue)
+                    throw new ArgumentNullException("ParentProcessID not found");
+
+                var parentBPInstance = bpInstanceManager.GetBPInstance(parentProcessId.Value);
+                parentBPInstance.ThrowIfNull("Parent BPInstance not found");
+
+                rulesDefinitionId = parentBPInstance.DefinitionID;
+            }
+            else
+                rulesDefinitionId = context.GetSharedInstanceData().InstanceInfo.DefinitionID;
+
+            bpBusinessRules = bpBusinessRuleManager.GetBPBusinessRuleDefinitions(businessRulesKey, rulesDefinitionId);
 
             IEnumerable<BusinessRule> rules = BuildBusinessRules(bpBusinessRules);
             ExecuteValidation(rules, importedDataToValidate, context, violatedBusinessRulesByTarget, out stopExecutionFlag);
             AppendValidationMessages(context, violatedBusinessRulesByTarget, writeMessagesToParentProcess, parentMessagePrefix);
 
             if (stopExecutionFlag)
-                throw new VRBusinessException("One or more business rules were not satisfied and led to stop the execution of the worklfow");
+            {
+                this.ViolatedRulesExist.Set(context, true);
+                if (!doNotThrowExceptionOnRulesViolation)
+                    throw new VRBusinessException("One or more business rules were not satisfied and led to stop the execution of the worklfow");
+            }
         }
 
         private IEnumerable<BusinessRule> BuildBusinessRules(List<BPBusinessRuleDefinition> bpBusinessRules)
@@ -51,7 +84,7 @@ namespace Vanrise.BusinessProcess.WFActivities
             foreach (BPBusinessRuleDefinition bpBusinessRule in bpBusinessRules)
             {
                 BPBusinessRuleAction action = bpRuleActionManager.GetBusinessRuleAction(bpBusinessRule.BPBusinessRuleDefinitionId);
-                BusinessRule rule = new BusinessRule() 
+                BusinessRule rule = new BusinessRule()
                 {
                     BPBusinessRuleDefinitionId = bpBusinessRule.BPBusinessRuleDefinitionId,
                     Condition = bpBusinessRule.Settings.Condition,
@@ -69,7 +102,7 @@ namespace Vanrise.BusinessProcess.WFActivities
             List<Guid> violatedRulesThatStopsExecution = new List<Guid>();
             foreach (BusinessRule rule in rules)
             {
-                if (violatedRulesThatStopsExecution.Count > 0 && 
+                if (violatedRulesThatStopsExecution.Count > 0 &&
                     rule.ExecutionDependsOnRules != null &&
                     rule.ExecutionDependsOnRules.Exists(ruleDefinitionId => violatedRulesThatStopsExecution.Contains(ruleDefinitionId)))
                 {
@@ -123,7 +156,7 @@ namespace Vanrise.BusinessProcess.WFActivities
 
                 if (writeMessagesToParentProcess)
                     context.WriteTrackingMessageToParentProcess(MapSeverity(msg.Severity), string.Concat(parentMessagePrefix, msg.Message));
-                    
+
                 context.WriteTrackingMessage(MapSeverity(msg.Severity), msg.Message);
             }
 
