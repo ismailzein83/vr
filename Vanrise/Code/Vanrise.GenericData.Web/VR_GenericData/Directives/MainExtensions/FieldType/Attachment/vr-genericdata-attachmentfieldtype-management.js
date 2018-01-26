d4024da4b93692b954377330e3c27e3d358e8860
Ali Sakr <VANRISE\ali.sakr>
2018-01-26 12:23:45 +0000

    fixing issues on returning data
    git-tfs-id: [http://developmentvlan:8585/tfs/vanrise.collection]$/;C45444

--- Vanrise/Code/Vanrise.GenericData.Web/VR_GenericData/Directives/MainExtensions/FieldType/Attachment/vr-genericdata-attachmentfieldtype-management.js
+++ Vanrise/Code/Vanrise.GenericData.Web/VR_GenericData/Directives/MainExtensions/FieldType/Attachment/vr-genericdata-attachmentfieldtype-management.js
@@ -38,29 +38,36 @@ function (UtilsService, VRNotificationService, VRUIUtilsService) {
                 $scope.scopeModel.attachementFieldTypes.push({ Entity: attachementFieldType });
               
             };
-            $scope.scopeModel.save = function () {
-                console.log(getAttachementFieldTypeData());
-            };
             $scope.onGridReady = function (api) {
-                gridAPI = api;
                 if (ctrl.onReady != undefined && typeof (ctrl.onReady) == "function") {
                     ctrl.onReady(getDirectiveAPI());
                 }
                 function getDirectiveAPI() {
                     var directiveAPI = {};
                     directiveAPI.loadGrid = function (payload) {
-                        if(payload != undefined)
-                        {
+                        var promises = [];
+                        if (payload != undefined) {
                             if (payload.attachementFieldTypes != undefined) {
                                 for (var i = 0; i < payload.attachementFieldTypes.length; i++) {
-                                    var attachementFieldType = payload.attachementFieldTypes[i];
-                                    $scope.attachementFieldTypes.push({ Entity: attachementFieldType });
+                                    var attachementFieldType = {
+                                        Entity: {
+                                            file: {fileId:payload.attachementFieldTypes[i].FileId},
+                                            notes: payload.attachementFieldTypes[i].Notes,
+                                            createdTime: payload.attachementFieldTypes[i].CreatedTime
+                                        }
+                                    };
+                                    $scope.scopeModel.attachementFieldTypes.push(attachementFieldType);
                                 }
                             }
                         }
+                        return UtilsService.waitMultiplePromises(promises);
                     };
                     directiveAPI.getData = function () {
-                        return getAttachementFieldTypeData();
+                        var obj = {
+                            $type: "System.Collections.Generic.List`1[[Vanrise.GenericData.Entities.AttachmentFieldTypeEntity, Vanrise.GenericData.Entities]], mscorlib",
+                            $values: getAttachementFieldTypeData()
+                        };
+                        return obj;
                     };
                     return directiveAPI;
                 };
@@ -72,15 +79,14 @@ function (UtilsService, VRNotificationService, VRUIUtilsService) {
             var attachementFieldTypes = [];
             if ($scope.scopeModel.attachementFieldTypes != undefined) {
                 for (var i = 0; i < $scope.scopeModel.attachementFieldTypes.length; i++) {
-                    if ($scope.scopeModel.attachementFieldTypes.Entity != undefined) {
                         var attachementFieldType =
                             {
+                                $type: "Vanrise.GenericData.Entities.AttachmentFieldTypeEntity, Vanrise.GenericData.Entities",
                                 FileId: $scope.scopeModel.attachementFieldTypes[i].Entity.file.fileId,
                                 Notes: $scope.scopeModel.attachementFieldTypes[i].Entity.notes,
                                 CreatedTime: $scope.scopeModel.attachementFieldTypes[i].Entity.createdTime
                             };
                         attachementFieldTypes.push(attachementFieldType);
-                    }
                 }
             }
             return attachementFieldTypes;
