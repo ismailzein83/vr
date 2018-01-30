6a5a3580d06759d0d713492ad0111fa1ef0efea5
Zeinab Mokdad <VANRISE\zeinab.mokdad>
2018-01-30 13:11:24 +0000

    adding new class IStateBackupCanRestoreContext
    git-tfs-id: [http://developmentvlan:8585/tfs/vanrise.collection]$/;C45478

---
+++ TOneV2/Code/TOneV2/TOne.WhS.BusinessEntity.Entities/IStateBackupCanRestoreContext.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TOne.WhS.BusinessEntity.Entities
+{
+    public interface IStateBackupCanRestoreContext
+    {
+        int StateBackupId { get; set; }
+        string Description { get; set; }
+    }
+}
