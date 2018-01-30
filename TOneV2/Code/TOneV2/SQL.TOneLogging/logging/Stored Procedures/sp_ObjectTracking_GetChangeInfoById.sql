f963b306e2466c8cc89deb8946280df3a64ee844
Samer Haidar <VANRISE\samer.haidar>
2018-01-30 13:12:54 +0000

    adding new sp
    git-tfs-id: [http://developmentvlan:8585/tfs/vanrise.collection]$/;C45481

---
+++ TOneV2/Code/TOneV2/SQL.TOneLogging/logging/Stored Procedures/sp_ObjectTracking_GetChangeInfoById.sql
@@ -0,0 +1,16 @@
+﻿-- =============================================
+-- Author:		<Author,,Name>
+-- Create date: <Create Date,,>
+-- Description:	<Description,,>
+-- =============================================
+create PROCEDURE [logging].sp_ObjectTracking_GetChangeInfoById 
+	@objectTrackingId int
+AS
+BEGIN
+	SELECT	
+		ac.ChangeInfo
+		
+FROM	[logging].[ObjectTracking]  ac WITH(NOLOCK)
+WHERE	ac.ID=@objectTrackingId
+		
+END
