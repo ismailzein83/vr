8fc6578a12b8c3a930401b9b8dd16273aebb7457
Ali Atoui <VANRISE\ali.atoui>
2018-01-30 14:41:44 +0000

    adjust rule SPs post scripts
    git-tfs-id: [http://developmentvlan:8585/tfs/vanrise.collection]$/;C45483

---
+++ TOneV2/Code/TOneV2/SQL.TOneConfiguration/rules/Stored Procedures/sp_Rule_DeleteRuleAndRuleChanged.sql
@@ -0,0 +1,40 @@
+﻿
+
+-- =============================================
+-- Author:		<Author,,Name>
+-- Create date: <Create Date,,>
+-- Description:	<Description,,>
+-- =============================================
+CREATE PROCEDURE [rules].[sp_Rule_DeleteRuleAndRuleChanged] 
+	@RuleID INT,
+	@RuleTypeID INT,
+	@ActionType INT,
+	@InitialRule varchar(MAX)
+AS
+BEGIN
+	Begin Try
+	    Begin Transaction
+			Update [rules].[Rule]
+			set [IsDeleted] = 1
+			Where Id = @RuleID and TypeID = @RuleTypeID
+	    
+	    	IF NOT EXISTS(SELECT 1 FROM  [rules].[RuleChanged]  WHERE RuleID = @RuleID and RuleTypeId = @RuleTypeID)
+				Begin
+					Insert into [rules].[RuleChanged] ([RuleID], [RuleTypeID], [ActionType], [InitialRule])
+					values(@RuleID, @RuleTypeID, @ActionType, @InitialRule)
+				END
+			ELSE
+				BEGIN
+					Update [rules].[RuleChanged] 
+					Set ActionType = @ActionType, InitialRule = @InitialRule
+					WHERE RuleID = @RuleID and RuleTypeId = @RuleTypeID
+				END
+	    Commit Transaction
+	End Try
+	Begin CATCH
+	    declare @ErrorMessage nvarchar(max), @ErrorSeverity int, @ErrorState int;
+	    select @ErrorMessage = ERROR_MESSAGE() + ' Line ' + cast(ERROR_LINE() as nvarchar(5)), @ErrorSeverity = ERROR_SEVERITY(), @ErrorState = ERROR_STATE();
+	    rollback Transaction;
+	    raiserror (@ErrorMessage, @ErrorSeverity, @ErrorState);
+	End CATCH
+END
--- TOneV2/Code/TOneV2/SQL.TOneConfiguration/rules/Stored Procedures/sp_Rule_GetByType.sql
+++ TOneV2/Code/TOneV2/SQL.TOneConfiguration/rules/Stored Procedures/sp_Rule_GetByType.sql
@@ -7,8 +7,7 @@ CREATE PROCEDURE [rules].[sp_Rule_GetByType]
 	@TypeID INT
 AS
 BEGIN
-	SELECT	r.ID,r.TypeID,r.RuleDetails,r.BED,r.EED
+	SELECT	r.ID, r.TypeID, r.RuleDetails, r.BED, r.EED, r.IsDeleted
 	FROM	[rules].[Rule] r  WITH(NOLOCK) 
-	WHERE	r.TypeID=@TypeID and ISNULL(IsDeleted, 0) = 0
-
+	WHERE	r.TypeID = @TypeID --and ISNULL(IsDeleted, 0) = 0
 END
