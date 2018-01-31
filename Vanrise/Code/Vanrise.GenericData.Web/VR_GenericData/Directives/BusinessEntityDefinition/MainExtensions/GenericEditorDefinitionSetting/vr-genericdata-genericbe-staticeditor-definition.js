5a2db08209a3b6a80334082985977d346faf3aa0
Ali Ballouk <VANRISE\ali.ballouk>
2018-01-31 10:41:43 +0000

    GenericBEEditor startup
    git-tfs-id: [http://developmentvlan:8585/tfs/vanrise.collection]$/;C45492

---
+++ Vanrise/Code/Vanrise.GenericData.Web/VR_GenericData/Directives/BusinessEntityDefinition/MainExtensions/GenericEditorDefinitionSetting/vr-genericdata-genericbe-staticeditor-definition.js
@@ -0,0 +1,54 @@
+﻿(function (app) {
+
+    'use strict';
+
+    HistoryGenericBEDefinitionViewDirective.$inject = ['UtilsService', 'VRNotificationService'];
+
+    function HistoryGenericBEDefinitionViewDirective(UtilsService, VRNotificationService) {
+        return {
+            restrict: 'E',
+            scope: {
+                onReady: '=',
+                normalColNum:'@'
+            },
+            controller: function ($scope, $element, $attrs) {
+                var ctrl = this;
+                var ctor = new HistoryGenericBEDefinitionViewCtor($scope, ctrl);
+                ctor.initializeController();
+            },
+            controllerAs: 'ctrl',
+            bindToController: true,
+            templateUrl: '/Client/Modules/VR_GenericData/Directives/BusinessEntityDefinition/MainExtensions/GenericEditorDefinitionSetting/Templates/GenericBEStaticEditorDefiitionTemplate.html'
+        };
+
+        function HistoryGenericBEDefinitionViewCtor($scope, ctrl) {
+            this.initializeController = initializeController;
+            function initializeController() {
+                $scope.scopeModel = {};
+                defineAPI();
+            }
+
+            function defineAPI() {
+                var api = {};
+
+                api.load = function (payload) {
+
+                };
+
+
+                api.getData = function () {
+                    return {
+                        $type: "Vanrise.GenericData.MainExtensions.StaticEditorDefinitionSetting, Vanrise.GenericData.MainExtensions",
+                        DirectiveName: $scope.scopeModel.directiveName
+                    };
+                };
+                if (ctrl.onReady != undefined && typeof (ctrl.onReady) == 'function') {
+                    ctrl.onReady(api);
+                }
+            }
+        }
+    }
+
+    app.directive('vrGenericdataGenericbeHistorygridviewDefinition', HistoryGenericBEDefinitionViewDirective);
+
+})(app);
