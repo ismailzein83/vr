adc0143ec83a06039a487908571ffcd290416c8a
Samer Haidar <VANRISE\samer.haidar>
2018-02-01 12:31:46 +0000

    Update name space
    git-tfs-id: [http://developmentvlan:8585/tfs/vanrise.collection]$/;C45514

--- TOneV2/Code/TOneV2/TOne.WhS.BusinessEntity.Business/FaultTickets/CustomerFaultTicketsSettings.cs
+++ TOneV2/Code/TOneV2/TOne.WhS.BusinessEntity.Business/FaultTickets/CustomerFaultTicketsSettings.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Vanrise.GenericData.Business;
 using Vanrise.GenericData.Entities;
 
 namespace TOne.WhS.BusinessEntity.Business
