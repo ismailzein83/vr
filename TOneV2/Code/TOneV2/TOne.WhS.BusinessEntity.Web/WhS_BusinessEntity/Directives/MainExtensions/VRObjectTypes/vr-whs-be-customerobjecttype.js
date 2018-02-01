701910cf1af4f8fd2e143801d73bbbe83f73beed
Ali Ballouk <VANRISE\ali.ballouk>
2018-02-01 12:37:31 +0000

    customer fault ticket type settings directive implementation
    git-tfs-id: [http://developmentvlan:8585/tfs/vanrise.collection]$/;C45516

--- TOneV2/Code/TOneV2/TOne.WhS.BusinessEntity.Web/WhS_BusinessEntity/Directives/MainExtensions/VRObjectTypes/vr-whs-be-customerobjecttype.js
+++ TOneV2/Code/TOneV2/TOne.WhS.BusinessEntity.Web/WhS_BusinessEntity/Directives/MainExtensions/VRObjectTypes/vr-whs-be-customerobjecttype.js
@@ -17,7 +17,7 @@
             },
             controllerAs: "Ctrl",
             bindToController: true,
-            templateUrl:  '/Client/Modules/WhS_BusinessEntity/Directives/MainExtensions/VRObjectTypes/Templates/CustomerObjectTypeTemplate.html'
+            templateUrl: '/Client/Modules/WhS_BusinessEntity/Directives/MainExtensions/VRObjectTypes/Templates/CustomerObjectTypeTemplate.html'
 
 
         };
@@ -52,3 +52,63 @@
     app.directive('vrWhsBeCustomerobjecttype', CustomerObjectType);
 
 })(app);
+
+
+(function (app) {
+
+    'use strict';
+
+    customerFaultTicketType.$inject = ["UtilsService", 'VRUIUtilsService', 'VRNotificationService'];
+
+    function customerFaultTicketType(UtilsService, VRUIUtilsService, VRNotificationService) {
+        return {
+            restrict: "E",
+            scope: {
+                onReady: "=",
+            },
+            controller: function ($scope, $element, $attrs) {
+                var ctrl = this;
+                var ctor = new CustomerFaultCtor($scope, ctrl, $attrs);
+                ctor.initializeController();
+            },
+            controllerAs: "Ctrl",
+            bindToController: true,
+            template: function (element, attrs) {
+                return "";
+            }
+        };
+        function CustomerFaultCtor($scope, ctrl, $attrs) {
+            this.initializeController = initializeController;
+
+            function initializeController() {
+                $scope.scopeModel = {};
+                defineAPI();
+            }
+            function defineAPI() {
+                var api = {};
+
+                api.load = function (payload) {
+
+                };
+
+                api.getData = function () {
+                    var data = {
+                        $type: "TOne.WhS.BusinessEntity.Business.CustomerFaultTicketCustomObjectTypeSettings, TOne.WhS.BusinessEntity.Business"
+                    };
+                    return data;
+                };
+
+                if (ctrl.onReady != undefined && typeof (ctrl.onReady) == 'function') {
+                    ctrl.onReady(api);
+                }
+            }
+        }
+    }
+
+    app.directive('vrWhsBeCustomerfaulttickettypeSettings', customerFaultTicketType);
+
+})(app);
+
+
+
+
