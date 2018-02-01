feb11a7e4edbd50d8711a3d628c9d5bf0b8db64b
Wissam Ajamy <VANRISE\wissam.ajamy>
2018-02-01 08:34:58 +0000

    Update structure from TOneConfiguration
    git-tfs-id: [http://developmentvlan:8585/tfs/vanrise.collection]$/;C45500

---
+++ TOneV2/Code/TOneV2/SQL.TOneConfiguration/common/Stored Procedures/sp_Enumerations_Delete.sql
@@ -0,0 +1,5 @@
+﻿CREATE PROCEDURE [common].[sp_Enumerations_Delete]
+AS
+BEGIN
+Delete from common.Enumerations
+END
---
+++ TOneV2/Code/TOneV2/SQL.TOneConfiguration/common/Tables/Enumerations.sql
@@ -0,0 +1,7 @@
+﻿CREATE TABLE [common].[Enumerations] (
+    [ID]        INT           IDENTITY (1, 1) NOT NULL,
+    [NameSpace] VARCHAR (MAX) NULL,
+    [Name]      VARCHAR (MAX) NULL,
+    [Values]    VARCHAR (MAX) NULL
+);
+
