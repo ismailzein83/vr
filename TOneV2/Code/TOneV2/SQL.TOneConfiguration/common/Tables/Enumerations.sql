4aab1eb52eaccba12b44350f277b0d1f494d9cf3
Wissam Ajamy <VANRISE\wissam.ajamy>
2018-02-02 12:13:45 +0000

    update structure
    git-tfs-id: [http://developmentvlan:8585/tfs/vanrise.collection]$/;C45539

--- TOneV2/Code/TOneV2/SQL.TOneConfiguration/common/Tables/Enumerations.sql
+++ TOneV2/Code/TOneV2/SQL.TOneConfiguration/common/Tables/Enumerations.sql
@@ -1,7 +1,10 @@
 ﻿CREATE TABLE [common].[Enumerations] (
-    [ID]        INT           IDENTITY (1, 1) NOT NULL,
-    [NameSpace] VARCHAR (MAX) NULL,
-    [Name]      VARCHAR (MAX) NULL,
-    [Values]    VARCHAR (MAX) NULL
+    [ID]          INT           IDENTITY (1, 1) NOT NULL,
+    [NameSpace]   VARCHAR (MAX) NULL,
+    [Name]        VARCHAR (MAX) NULL,
+    [Description] VARCHAR (MAX) NULL,
+    CONSTRAINT [PK_Enumerations] PRIMARY KEY CLUSTERED ([ID] ASC)
 );
+
+
 
