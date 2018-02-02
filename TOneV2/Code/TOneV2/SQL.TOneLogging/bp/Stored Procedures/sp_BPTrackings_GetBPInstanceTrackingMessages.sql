5c35ef47f0be1f9e68524e15891c0157467acb5c
Wissam Ajamy <VANRISE\wissam.ajamy>
2018-02-02 11:52:36 +0000

    Update structure
    git-tfs-id: [http://developmentvlan:8585/tfs/vanrise.collection]$/;C45536

---
+++ TOneV2/Code/TOneV2/SQL.TOneLogging/bp/Stored Procedures/sp_BPTrackings_GetBPInstanceTrackingMessages.sql
@@ -0,0 +1,26 @@
+﻿Create PROCEDURE [bp].[sp_BPTrackings_GetBPInstanceTrackingMessages]	
+	@ProcessInstanceID bigint,
+	@Severities varchar(max)
+AS
+BEGIN
+	-- SET NOCOUNT ON added to prevent extra result sets from
+	-- interfering with SELECT statements.
+	SET NOCOUNT ON;
+	
+	DECLARE @SeveritiesTable TABLE (Severity int)
+	Insert @SeveritiesTable 
+	SELECT  ParsedString FROM bp.ParseStringList(@Severities)
+
+		SELECT     [ID]
+				  ,[ProcessInstanceID]
+				  ,[ParentProcessID]
+				  ,[TrackingMessage]
+				  ,bpt.[Severity]
+				  ,[EventTime]				  
+				  ,[ExceptionDetail]
+		FROM [bp].[BPTracking] as bpt WITH(NOLOCK) 
+		LEFT JOIN @SeveritiesTable as sv on (sv.Severity = bpt.Severity )  
+		where ( sv.Severity is not null OR (@Severities is null or @Severities = '') )
+			AND bpt.ProcessInstanceID = @ProcessInstanceID 
+		ORDER BY ID DESC
+END
