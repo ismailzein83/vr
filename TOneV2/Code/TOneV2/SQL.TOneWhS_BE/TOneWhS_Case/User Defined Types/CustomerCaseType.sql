9fadf4bc0b34309b8ab92600e447abc6ce84391f
Wissam Ajamy <VANRISE\wissam.ajamy>
2018-02-02 11:56:43 +0000

    update structure from TOneV2_Dev
    git-tfs-id: [http://developmentvlan:8585/tfs/vanrise.collection]$/;C45537

--- TOneV2/Code/TOneV2/SQL.TOneWhS_BE/TOneWhS_Case/User Defined Types/CustomerCaseType.sql
+++ TOneV2/Code/TOneV2/SQL.TOneWhS_BE/TOneWhS_Case/User Defined Types/CustomerCaseType.sql
@@ -1,25 +1,27 @@
 ﻿CREATE TYPE [TOneWhS_Case].[CustomerCaseType] AS TABLE (
-    [ID]               BIGINT           NULL,
-    [CaseTime]         DATETIME         NULL,
-    [CustomerId]       INT              NULL,
-    [SaleZoneId]       BIGINT           NULL,
-    [FromDate]         DATETIME         NULL,
-    [ToDate]           DATETIME         NULL,
-    [Attempts]         INT              NULL,
-    [ASR]              DECIMAL (20, 8)  NULL,
-    [ACD]              DECIMAL (20, 8)  NULL,
-    [CarrierReference] NVARCHAR (255)   NULL,
-    [Description]      NVARCHAR (1000)  NULL,
-    [StatusId]         UNIQUEIDENTIFIER NULL,
-    [Notes]            NVARCHAR (1000)  NULL,
-    [ReasonId]         UNIQUEIDENTIFIER NULL,
-    [OwnerReference]   NVARCHAR (1000)  NULL,
-    [Attachments]      NVARCHAR (MAX)   NULL,
-    [WorkGroupId]      UNIQUEIDENTIFIER NULL,
-    [Settings]         NVARCHAR (MAX)   NULL,
-    [ContactName]      NVARCHAR (255)   NULL,
-    [PhoneNumber]      NVARCHAR (255)   NULL,
-    [ContactEmails]    NVARCHAR (1000)  NULL);
+    [ID]                BIGINT           NULL,
+    [CaseTime]          DATETIME         NULL,
+    [CustomerId]        INT              NULL,
+    [SaleZoneId]        BIGINT           NULL,
+    [FromDate]          DATETIME         NULL,
+    [ToDate]            DATETIME         NULL,
+    [Attempts]          INT              NULL,
+    [ASR]               DECIMAL (20, 8)  NULL,
+    [ACD]               DECIMAL (20, 8)  NULL,
+    [CarrierReference]  NVARCHAR (255)   NULL,
+    [Description]       NVARCHAR (1000)  NULL,
+    [StatusId]          UNIQUEIDENTIFIER NULL,
+    [Notes]             NVARCHAR (1000)  NULL,
+    [OwnerReference]    NVARCHAR (1000)  NULL,
+    [Attachments]       NVARCHAR (MAX)   NULL,
+    [WorkGroupId]       UNIQUEIDENTIFIER NULL,
+    [Settings]          NVARCHAR (MAX)   NULL,
+    [ContactName]       NVARCHAR (255)   NULL,
+    [PhoneNumber]       NVARCHAR (255)   NULL,
+    [ContactEmails]     NVARCHAR (1000)  NULL,
+    [EscalationLevelId] UNIQUEIDENTIFIER NULL);
+
+
 
 
 
