59d90859579cd9c1e4305f6c0beb8fd5cb3268a9
Samer Haidar <VANRISE\samer.haidar>
2018-02-05 16:26:47 +0000

    adding FaultTicketStatusDefinitionFilter
    git-tfs-id: [http://developmentvlan:8585/tfs/vanrise.collection]$/;C45573

---
+++ TOneV2/Code/TOneV2/TOne.WhS.BusinessEntity.Business/FaultTickets/FaultTicketStatusDefinitionFilter.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Vanrise.Entities;
+
+namespace TOne.WhS.BusinessEntity.Business
+{
+    public class FaultTicketStatusDefinitionFilter : IStatusDefinitionFilter
+    {
+        public long? CaseId { get; set; }
+        public Guid BusinessEntityDefinitionId { get; set; }
+        public bool IsMatched(IStatusDefinitionFilterContext context)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
