5150ae1ea0a633a12757067e4702514f1a5f7a46
Ali Ballouk <VANRISE\ali.ballouk>
2018-02-06 09:36:18 +0000

    add modalWidth selector
    git-tfs-id: [http://developmentvlan:8585/tfs/vanrise.collection]$/;C45577

---
+++ Vanrise/Code/Vanrise.Common.Web/Common/Directives/ModalWidth/vr-common-modalwidth-selector.js
@@ -0,0 +1,115 @@
+﻿(function (app) {
+
+    'use strict';
+
+    ModalWidthSelectorDirective.$inject = ['VRCommon_ModalWidthEnum', 'UtilsService', 'VRUIUtilsService'];
+
+    function ModalWidthSelectorDirective(VRCommon_ModalWidthEnum, UtilsService, VRUIUtilsService) {
+
+        var directiveDefinitionObject = {
+            restrict: 'E',
+            scope: {
+                onReady: '=',
+                selectedvalues: '=',
+                onselectionchanged: '=',
+                onselectitem: '=',
+                ondeselectitem: '=',
+                ismultipleselection: '@',
+                isdisabled: '@',
+                isrequired: '=',
+                hideremoveicon: '@'
+            },
+            controller: function ($scope, $element, $attrs) {
+                var ctrl = this;
+                ctrl.datasource = [];
+
+                ctrl.selectedvalues;
+                if ($attrs.ismultipleselection != undefined && $attrs.ismultipleselection != null)
+                    ctrl.selectedvalues = [];
+                var modalWidthSelector = new ModalWidthSelector(ctrl, $scope, $attrs);
+                modalWidthSelector.initializeController();
+            },
+            controllerAs: 'ctrl',
+            bindToController: true,
+            template: function (element, attrs) {
+                return getDirectiveTemplate(attrs);
+            }
+        };
+
+        function ModalWidthSelector(ctrl, $scope, attrs) {
+            this.initializeController = initializeController;
+
+            var selectorAPI;
+
+            function initializeController() {
+                ctrl.onSelectorReady = function (api) {
+                    selectorAPI = api;
+
+                    if (ctrl.onReady && typeof ctrl.onReady == 'function') {
+                        ctrl.onReady(getDirectiveAPI());
+                    }
+                };
+            }
+
+            function getDirectiveAPI() {
+                var directiveAPI = {};
+
+                directiveAPI.load = function (payload) {
+                    selectorAPI.clearDataSource();
+                    ctrl.datasource = UtilsService.getArrayEnum(VRCommon_ModalWidthEnum);
+
+                    var selectedIds;
+
+                    if (payload) {
+                        selectedIds = payload.selectedIds;
+                    }
+
+                    if (selectedIds!=undefined) {
+                        VRUIUtilsService.setSelectedValues(selectedIds, 'value', attrs, ctrl);
+                    }
+                };
+
+                directiveAPI.getSelectedIds = function () {
+                    return VRUIUtilsService.getIdSelectedIds('value', attrs, ctrl);
+                };
+
+                return directiveAPI;
+            }
+        }
+
+        function getDirectiveTemplate(attrs) {
+            var label = attrs.label ? attrs.label : 'Modal Width';
+
+            var ismultipleselection = '';
+            if (attrs.ismultipleselection != undefined && attrs.ismultipleselection != null) {
+                label = label == 'Modal Width' ? 'Modals Width' : label;
+                ismultipleselection = ' ismultipleselection';
+            }
+
+            var hideremoveicon = (attrs.hideremoveicon != undefined && attrs.hideremoveicon != null) ? 'hideremoveicon' : null;
+
+            return '<div>'
+                + '<vr-select on-ready="ctrl.onSelectorReady"'
+                    + ' label="' + label + '"'
+                    + ' datasource="ctrl.datasource"'
+                    + ' selectedvalues="ctrl.selectedvalues"'
+                    + ' onselectionchanged="ctrl.onselectionchanged"'
+                    + ' onselectitem="ctrl.onselectitem"'
+                    + ' ondeselectitem="ctrl.ondeselectitem"'
+                    + ' datavaluefield="value"'
+                    + ' datatextfield="description"'
+                    + ismultipleselection
+                    + ' vr-disabled="ctrl.isdisabled"'
+                    + ' isrequired="ctrl.isrequired"'
+                    + hideremoveicon
+                    + ' entityName="' + label + '">'
+                + '</vr-select>'
+            + '</div>';
+        }
+
+        return directiveDefinitionObject;
+    }
+
+    app.directive('vrCommonModalwidthSelector', ModalWidthSelectorDirective);
+
+})(app);
