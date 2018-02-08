ebcfb83592f20179e21e22cf0f8751825c6f75a6
Ali Atoui <VANRISE\ali.atoui>
2018-02-08 08:02:46 +0000

    Arranging vr-whs-routing-rproute-grid
    git-tfs-id: [http://developmentvlan:8585/tfs/vanrise.collection]$/;C45601

--- TOneV2/Code/TOneV2/TOne.WhS.Routing.Web/WhS_Routing/Directives/RPRoute/vr-whs-routing-rproute-grid.js
+++ TOneV2/Code/TOneV2/TOne.WhS.Routing.Web/WhS_Routing/Directives/RPRoute/vr-whs-routing-rproute-grid.js
@@ -1,193 +1,192 @@
 ﻿"use strict";
 
 app.directive('vrWhsRoutingRprouteGrid', ['VRNotificationService', 'UtilsService', 'VRUIUtilsService', 'WhS_Routing_RPRouteAPIService', 'WhS_Routing_RouteRuleService', 'WhS_BE_ZoneRouteOptionsEnum',
-function (VRNotificationService, UtilsService, VRUIUtilsService, WhS_Routing_RPRouteAPIService, WhS_Routing_RouteRuleService, WhS_BE_ZoneRouteOptionsEnum) {
+    function (VRNotificationService, UtilsService, VRUIUtilsService, WhS_Routing_RPRouteAPIService, WhS_Routing_RouteRuleService, WhS_BE_ZoneRouteOptionsEnum) {
 
-    var directiveDefinitionObject = {
+        var directiveDefinitionObject = {
+            restrict: "E",
+            scope: {
+                onReady: "=",
+            },
+            controller: function ($scope, $element, $attrs) {
+                var ctrl = this;
 
-        restrict: "E",
-        scope: {
-            onReady: "=",
-        },
-        controller: function ($scope, $element, $attrs) {
-            var ctrl = this;
+                var ctor = new customerRouteGrid($scope, ctrl, $attrs);
+                ctor.initializeController();
+            },
+            controllerAs: "ctrl",
+            bindToController: true,
+            compile: function (element, attrs) {
 
-            var ctor = new customerRouteGrid($scope, ctrl, $attrs);
-            ctor.initializeController();
-        },
-        controllerAs: "ctrl",
-        bindToController: true,
-        compile: function (element, attrs) {
+            },
+            templateUrl: "/Client/Modules/WhS_Routing/Directives/RPRoute/Templates/RPRouteGridTemplate.html"
 
-        },
-        templateUrl: "/Client/Modules/WhS_Routing/Directives/RPRoute/Templates/RPRouteGridTemplate.html"
+        };
 
-    };
+        function customerRouteGrid($scope, ctrl, $attrs) {
+            this.initializeController = initializeController;
 
-    function customerRouteGrid($scope, ctrl, $attrs) {
-        var gridAPI;
-        var gridDrillDownTabsObj;
-        var routingDatabaseId;
-        var policies;
-        var defaultPolicyId;
-        var selectedPolicyConfigId;
-        var customerId;
-        var showInSystemCurrency;
-        var currencyId;
+            var routingDatabaseId;
+            var policies;
+            var defaultPolicyId;
+            var selectedPolicyConfigId;
+            var customerId;
+            var showInSystemCurrency;
+            var currencyId;
 
-        function initializeController() {
-            $scope.showGrid = false;
-            $scope.rpRoutes = [];
-            $scope.isCustomerSelected = false;
+            var gridAPI;
+            var gridDrillDownTabsObj;
 
-            var drillDownDefinitions = initDrillDownDefinitions();
-            gridDrillDownTabsObj = VRUIUtilsService.defineGridDrillDownTabs(drillDownDefinitions, gridAPI, $scope.gridMenuActions);
+            function initializeController() {
+                $scope.showGrid = false;
+                $scope.rpRoutes = [];
+                $scope.isCustomerSelected = false;
 
-            $scope.onGridReady = function (api) {
-                gridAPI = api;
+                $scope.onGridReady = function (api) {
+                    gridAPI = api;
+
+                    var drillDownDefinitions = initDrillDownDefinitions();
+                    gridDrillDownTabsObj = VRUIUtilsService.defineGridDrillDownTabs(drillDownDefinitions, gridAPI, $scope.gridMenuActions);
+
+                    defineAPI();
+                };
+
+                $scope.dataRetrievalFunction = function (dataRetrievalInput, onResponseReady) {
+                    var loadGridPromiseDeffered = UtilsService.createPromiseDeferred();
+
+                    WhS_Routing_RPRouteAPIService.GetFilteredRPRoutes(dataRetrievalInput)
+                       .then(function (response) {
+                           var promises = [];
+                           if (response && response.Data) {
+                               for (var i = 0; i < response.Data.length; i++) {
+                                   var rpRouteDetail = response.Data[i];
+                                   gridDrillDownTabsObj.setDrillDownExtensionObject(rpRouteDetail);
+                                   promises.push(setRouteOptionDetailsDirectiveonEachItem(rpRouteDetail));
+                               }
+                           }
+                           $scope.showGrid = true;
+                           onResponseReady(response);
+
+                           UtilsService.waitMultiplePromises(promises).then(function () {
+                               loadGridPromiseDeffered.resolve();
+                           }).catch(function (error) {
+                               loadGridPromiseDeffered.reject();
+                           });
+                       })
+                       .catch(function (error) {
+                           VRNotificationService.notifyException(error, $scope);
+                       });
+
+                    return loadGridPromiseDeffered.promise;
+                };
+
+                $scope.getRowStyle = function (dataItem) {
+
+                    var rowStyle;
+
+                    if (dataItem.IsBlocked)
+                        rowStyle = { CssClass: "bg-danger" };
+
+                    return rowStyle;
+                };
+
+                defineMenuActions();
+            }
+            function defineAPI() {
+                var api = {};
+
+                api.loadGrid = function (query) {
+
+                    routingDatabaseId = query.RoutingDatabaseId;
+                    policies = query.FilteredPolicies;
+                    defaultPolicyId = query.DefaultPolicyId;
+                    selectedPolicyConfigId = query.PolicyConfigId;
+                    customerId = query.CustomerId;
+                    showInSystemCurrency = query.ShowInSystemCurrency;
+                    currencyId = query.CurrencyId;
+
+                    if (query.CustomerId != undefined)
+                        $scope.isCustomerSelected = true;
+
+                    return gridAPI.retrieveData(query);
+                };
 
                 if (ctrl.onReady != undefined && typeof (ctrl.onReady) == "function")
-                    ctrl.onReady(getDirectiveAPI());
+                    ctrl.onReady(api);
+            }
 
-                function getDirectiveAPI() {
-
-                    var directiveAPI = {};
-
-                    directiveAPI.loadGrid = function (query) {
-
-                        routingDatabaseId = query.RoutingDatabaseId;
-                        policies = query.FilteredPolicies;
-                        defaultPolicyId = query.DefaultPolicyId;
-                        selectedPolicyConfigId = query.PolicyConfigId;
-                        customerId = query.CustomerId;
-                        showInSystemCurrency = query.ShowInSystemCurrency;
-                        currencyId = query.CurrencyId;
-
-                        if (query.CustomerId != undefined)
-                            $scope.isCustomerSelected = true;
-
-                        return gridAPI.retrieveData(query);
-                    };
-
-                    return directiveAPI;
+            function defineMenuActions() {
+                $scope.gridMenuActions = [{
+                    name: "Matching Rule",
+                    clicked: openRouteRuleEditor,
                 }
-            };
-
-            $scope.dataRetrievalFunction = function (dataRetrievalInput, onResponseReady) {
-                var loadGridPromiseDeffered = UtilsService.createPromiseDeferred();
-
-                WhS_Routing_RPRouteAPIService.GetFilteredRPRoutes(dataRetrievalInput)
-                   .then(function (response) {
-                       var promises = [];
-                       if (response && response.Data) {
-                           for (var i = 0; i < response.Data.length; i++) {
-                               var rpRouteDetail = response.Data[i];
-                               gridDrillDownTabsObj.setDrillDownExtensionObject(rpRouteDetail);
-                               promises.push(setRouteOptionDetailsDirectiveonEachItem(rpRouteDetail));
-                           }
-                       }
-                       $scope.showGrid = true;
-                       onResponseReady(response);
-
-                       UtilsService.waitMultiplePromises(promises).then(function () {
-                           loadGridPromiseDeffered.resolve();
-                       }).catch(function (error) {
-                           loadGridPromiseDeffered.reject();
-                       });
-                   })
-                   .catch(function (error) {
-                       VRNotificationService.notifyException(error, $scope);
-                   });
-
-                return loadGridPromiseDeffered.promise;
-            };
-
-            $scope.getRowStyle = function (dataItem) {
-
-                var rowStyle;
-
-                if (dataItem.IsBlocked)
-                    rowStyle = { CssClass: "bg-danger" };
-
-                return rowStyle;
-            };
-
-            defineMenuActions();
-        }
-
-        function defineMenuActions() {
-            $scope.gridMenuActions = [{
-                name: "Matching Rule",
-                clicked: openRouteRuleEditor,
+                ];
             }
-            ];
-        }
-        function openRouteRuleEditor(dataItem) {
-            WhS_Routing_RouteRuleService.viewRouteRule(dataItem.ExecutedRuleId);
-        }
 
-        function setRouteOptionDetailsDirectiveonEachItem(rpRouteDetail) {
-            var promises = [];
+            function openRouteRuleEditor(dataItem) {
+                WhS_Routing_RouteRuleService.viewRouteRule(dataItem.ExecutedRuleId);
+            }
 
-            rpRouteDetail.RouteOptionsLoadDeferred = UtilsService.createPromiseDeferred();
-            rpRouteDetail.onRouteOptionsReady = function (api) {
-                rpRouteDetail.RouteOptionsAPI = api;
+            function setRouteOptionDetailsDirectiveonEachItem(rpRouteDetail) {
+                var promises = [];
 
-                var payload = {
-                    RoutingDatabaseId: routingDatabaseId,
-                    SaleZoneId: rpRouteDetail.SaleZoneId,
-                    RoutingProductId: rpRouteDetail.RoutingProductId,
-                    RouteOptions: rpRouteDetail.RouteOptionsDetails,
-                    display: WhS_BE_ZoneRouteOptionsEnum.SupplierRateWithNameAndPercentage.value,
-                    currencyId: currencyId
-            };
-                VRUIUtilsService.callDirectiveLoad(rpRouteDetail.RouteOptionsAPI, payload, rpRouteDetail.RouteOptionsLoadDeferred);
-        };
-            promises.push(rpRouteDetail.RouteOptionsLoadDeferred.promise);
+                rpRouteDetail.RouteOptionsLoadDeferred = UtilsService.createPromiseDeferred();
+                rpRouteDetail.onRouteOptionsReady = function (api) {
+                    rpRouteDetail.RouteOptionsAPI = api;
 
-            rpRouteDetail.saleZoneServiceLoadDeferred = UtilsService.createPromiseDeferred();
-            rpRouteDetail.onServiceViewerReady = function (api) {
-                rpRouteDetail.serviceViewerAPI = api;
-
-                var serviceViewerPayload;
-                if (rpRouteDetail != undefined) {
-                    serviceViewerPayload = {
-                            selectedIds: rpRouteDetail.SaleZoneServiceIds
+                    var payload = {
+                        RoutingDatabaseId: routingDatabaseId,
+                        SaleZoneId: rpRouteDetail.SaleZoneId,
+                        RoutingProductId: rpRouteDetail.RoutingProductId,
+                        RouteOptions: rpRouteDetail.RouteOptionsDetails,
+                        display: WhS_BE_ZoneRouteOptionsEnum.SupplierRateWithNameAndPercentage.value,
+                        currencyId: currencyId
+                    };
+                    VRUIUtilsService.callDirectiveLoad(rpRouteDetail.RouteOptionsAPI, payload, rpRouteDetail.RouteOptionsLoadDeferred);
                 };
+                promises.push(rpRouteDetail.RouteOptionsLoadDeferred.promise);
+
+                rpRouteDetail.saleZoneServiceLoadDeferred = UtilsService.createPromiseDeferred();
+                rpRouteDetail.onServiceViewerReady = function (api) {
+                    rpRouteDetail.serviceViewerAPI = api;
+
+                    var serviceViewerPayload;
+                    if (rpRouteDetail != undefined) {
+                        serviceViewerPayload = {
+                            selectedIds: rpRouteDetail.SaleZoneServiceIds
+                        };
+                    }
+
+                    VRUIUtilsService.callDirectiveLoad(rpRouteDetail.serviceViewerAPI, serviceViewerPayload, rpRouteDetail.saleZoneServiceLoadDeferred);
+                };
+                promises.push(rpRouteDetail.saleZoneServiceLoadDeferred.promise);
+
+                return UtilsService.waitMultiplePromises(promises);
             }
 
-                VRUIUtilsService.callDirectiveLoad(rpRouteDetail.serviceViewerAPI, serviceViewerPayload, rpRouteDetail.saleZoneServiceLoadDeferred);
-        };
-            promises.push(rpRouteDetail.saleZoneServiceLoadDeferred.promise);
+            function initDrillDownDefinitions() {
+                var drillDownDefinition = {};
 
-            return UtilsService.waitMultiplePromises(promises);
-    }
+                drillDownDefinition.title = "Details";
+                drillDownDefinition.directive = "vr-whs-routing-rproute-details";
 
-        function initDrillDownDefinitions() {
-            var drillDownDefinition = {};
-
-            drillDownDefinition.title = "Details";
-            drillDownDefinition.directive = "vr-whs-routing-rproute-details";
-
-            drillDownDefinition.loadDirective = function (directiveAPI, rpRouteDetail) {
-                var payload = {
+                drillDownDefinition.loadDirective = function (directiveAPI, rpRouteDetail) {
+                    var payload = {
                         rpRouteDetail: rpRouteDetail,
                         routingDatabaseId: routingDatabaseId,
                         filteredPolicies: policies,
                         defaultPolicyId: selectedPolicyConfigId,
-                        customerId : customerId,
+                        customerId: customerId,
                         showInSystemCurrency: showInSystemCurrency,
                         currencyId: currencyId
-            };
+                    };
 
-                return directiveAPI.load(payload);
-        };
+                    return directiveAPI.load(payload);
+                };
 
-            return [drillDownDefinition];
-    }
+                return [drillDownDefinition];
+            }
+        }
 
-        this.initializeController = initializeController;
-}
-
-    return directiveDefinitionObject;
-}]);
+        return directiveDefinitionObject;
+    }]);
