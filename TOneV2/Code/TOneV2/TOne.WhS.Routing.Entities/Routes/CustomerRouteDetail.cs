c82528a2ca7f08cdc13eacafbfaba1202fc6359a
Ali Atoui <VANRISE\ali.atoui>
2018-02-13 15:57:28 +0000

    Adding CustomerRouteOptionDetail
    git-tfs-id: [http://developmentvlan:8585/tfs/vanrise.collection]$/;C45720

--- TOneV2/Code/TOneV2/TOne.WhS.Routing.Entities/Routes/CustomerRouteDetail.cs
+++ TOneV2/Code/TOneV2/TOne.WhS.Routing.Entities/Routes/CustomerRouteDetail.cs
@@ -20,35 +20,7 @@ namespace TOne.WhS.Routing.Entities
         public List<int> LinkedRouteRuleIds { get; set; }
         public string ExecutedRouteRuleName { get; set; }
         public string ExecutedRouteRuleSettingsTypeName { get; set; }
-        public int LinkedRouteRuleCount { get { return LinkedRouteRuleIds != null ? LinkedRouteRuleIds.Count : 0; } }
         public bool CanEditMatchingRule { get; set; }
-    }
-
-    public class BaseCustomerRouteOptionDetail
-    {
-        public int CustomerRouteOptionDetailId { get { return this.SupplierId; } }
-        public int SupplierId { get; set; }
-        public string SupplierName { get; set; }
-        public long SupplierZoneId { get; set; }
-        public string SupplierZoneName { get; set; }
-        public string SupplierCode { get; set; }
-        public Decimal SupplierRate { get; set; }
-        public bool IsBlocked { get; set; }
-        public List<int> ExactSupplierServiceIds { get; set; }
-        public int? ExecutedRuleId { get; set; }
-        public List<int> LinkedRouteOptionRuleIds { get; set; }
-        public int LinkedRouteOptionRuleCount { get { return LinkedRouteOptionRuleIds != null ? LinkedRouteOptionRuleIds.Count : 0; } }
-    }
-
-    public class CustomerRouteOptionDetail : BaseCustomerRouteOptionDetail
-    {
-        public int? Percentage { get; set; }
-
-        public List<CustomerRouteBackupOptionDetail> Backups { get; set; }
-    }
-
-    public class CustomerRouteBackupOptionDetail : BaseCustomerRouteOptionDetail
-    {
-
+        public int LinkedRouteRuleCount { get { return LinkedRouteRuleIds != null ? LinkedRouteRuleIds.Count : 0; } }
     }
 }
---
+++ TOneV2/Code/TOneV2/TOne.WhS.Routing.Entities/Routes/CustomerRouteOptionDetail.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TOne.WhS.Routing.Entities
+{
+    public class BaseCustomerRouteOptionDetail
+    {
+        public int CustomerRouteOptionDetailId { get { return this.SupplierId; } }
+        public int SupplierId { get; set; }
+        public string SupplierName { get; set; }
+        public long SupplierZoneId { get; set; }
+        public string SupplierZoneName { get; set; }
+        public string SupplierCode { get; set; }
+        public Decimal SupplierRate { get; set; }
+        public List<int> ExactSupplierServiceIds { get; set; }
+        public int? ExecutedRuleId { get; set; }
+        public List<int> LinkedRouteOptionRuleIds { get; set; }
+        public bool IsBlocked { get; set; }
+
+        //public bool IsForced { get; set; }
+        //public bool IsLossy { get; set; }
+
+        public int LinkedRouteOptionRuleCount { get { return LinkedRouteOptionRuleIds != null ? LinkedRouteOptionRuleIds.Count : 0; } }
+    }
+
+    public class CustomerRouteOptionDetail : BaseCustomerRouteOptionDetail
+    {
+        public int? Percentage { get; set; }
+
+        public List<CustomerRouteBackupOptionDetail> Backups { get; set; }
+    }
+
+    public class CustomerRouteBackupOptionDetail : BaseCustomerRouteOptionDetail
+    {
+
+    }
+}
