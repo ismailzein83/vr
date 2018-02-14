009db918ae028d241abb8a06e7d33f8dfa2ce6da
Ali Atoui <VANRISE\ali.atoui>
2018-02-14 14:02:58 +0000

    Adding RouteOptionEvaluatedStatusEnum
    git-tfs-id: [http://developmentvlan:8585/tfs/vanrise.collection]$/;C45734

---
+++ TOneV2/Code/TOneV2/TOne.WhS.Routing.Web/WhS_Routing/Constant/RouteOptionEvaluatedStatusEnum.js
@@ -0,0 +1,6 @@
+﻿app.constant('WhS_Routing_RouteOptionEvaluatedStatusEnum', {
+    Blocked: { value: 0, description: "Blocked", cssclass: "option-blocked" },
+    Lossy: { value: 1, description: "Lossy", cssclass: "option-lossy" },
+    Forced: { value: 2, description: "Forced", cssclass: "option-forced" },
+    MarketPrice: { value: 3, description: "Market Price", cssclass: "option-marketprice" }
+});
