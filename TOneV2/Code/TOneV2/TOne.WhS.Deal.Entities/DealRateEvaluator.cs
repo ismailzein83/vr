7516a54ab9782baf268d9d1164803b0f0eeb0fba
Mortada Issa <VANRISE\mortada.issa>
2018-02-14 14:19:05 +0000

    Adding the main skeleton for discount feature in volum commitment
    git-tfs-id: [http://developmentvlan:8585/tfs/vanrise.collection]$/;C45735

---
+++ TOneV2/Code/TOneV2/TOne.WhS.Deal.Entities/DealRateEvaluator.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TOne.WhS.Deal.Entities
+{
+    public abstract class DealRateEvaluator
+    {
+        public abstract void EvaluateRate(IDealRateEvaluatorContext context);
+    }
+
+    public interface IDealRateEvaluatorContext
+    {
+        List<long> ZoneIds { get; }
+
+        DateTime DealBED { get; }
+
+        DateTime DealEED { get; }
+
+        Dictionary<long, List<EvaluatedDealRate>> EvaluatedRatesByZoneId { get; }
+
+    }
+
+    public class EvaluatedDealRate
+    {
+        public Decimal Rate { get; set; }
+
+        //public int CurrencyId { get; set; }
+
+        public DateTime BED { get; set; }
+
+        public DateTime? EED { get; set; }
+
+    }
+}
---
+++ TOneV2/Code/TOneV2/TOne.WhS.Deal.MainExtensions/DealRateEvaluator/SaleRateEvaluator/FixedSaleRateEvaluator.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TOne.WhS.Deal.Entities;
+
+namespace TOne.WhS.Deal.MainExtensions
+{
+    public class FixedSaleRateEvaluator : DealRateEvaluator
+    {
+        public Decimal Rate { get; set; }
+
+        public override void EvaluateRate(IDealRateEvaluatorContext context)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
