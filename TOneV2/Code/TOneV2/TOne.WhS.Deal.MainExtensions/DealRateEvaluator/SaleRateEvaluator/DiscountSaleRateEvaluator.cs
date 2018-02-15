3737329d31b88c81f1a57c6a600f4148a4f4af4e
Zeinab Mokdad <VANRISE\zeinab.mokdad>
2018-02-15 09:53:46 +0000

    adding main extension for discount
    git-tfs-id: [http://developmentvlan:8585/tfs/vanrise.collection]$/;C45746

---
+++ TOneV2/Code/TOneV2/TOne.WhS.Deal.MainExtensions/DealRateEvaluator/SaleRateEvaluator/DiscountSaleRateEvaluator.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using TOne.WhS.Deal.Entities;
+
+namespace TOne.WhS.Deal.MainExtensions
+{
+    public class DiscountSaleRateEvaluator : DealSaleRateEvaluator
+    {
+        public int Discount { get; set; }
+        public override void EvaluateRate(IDealSaleRateEvaluatorContext context)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
