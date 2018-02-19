44137ba4ff0f0d6a277dcea1265b0a90c37792ea
Wissam Ajamy <VANRISE\wissam.ajamy>
2018-02-19 11:49:12 +0000

    Update structure from TOneV2_Dev
    git-tfs-id: [http://developmentvlan:8585/tfs/vanrise.collection]$/;C45825

---
+++ TOneV2/Code/TOneV2/SQL.TOneWhS_BE/TOneWhS_SPL/Stored Procedures/sp_SupplierZoneRate_Preview_GetNumberOfOtherRatesWithChangeType.sql
@@ -0,0 +1,13 @@
+﻿
+
+CREATE PROCEDURE  [TOneWhS_SPL].[sp_SupplierZoneRate_Preview_GetNumberOfOtherRatesWithChangeType]
+	@ProcessInstanceID_IN INT,
+	@RateChangeType_IN INT
+AS
+BEGIN
+
+	SELECT	COUNT(RateChangeType)
+	FROM	[TOneWhS_SPL].[SupplierOtherRate_Preview] WITH(NOLOCK)
+	WHERE	ProcessInstanceID = @ProcessInstanceID_IN and RateChangeType=@RateChangeType_IN
+	
+END
