61a97de5595b9f50cfcaf302a8fe436953d834e3
anthony douaihy <VANRISE\anthony.douaihy>
2018-02-20 14:03:02 +0000

    add AnthonyTask to runtime
    git-tfs-id: [http://developmentvlan:8585/tfs/vanrise.collection]$/;C45844

---
+++ Mediation/Code/Mediation.Runtime/Tasks/AnthonyTask.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Mediation.Runtime.Tasks
+{
+    public class AnthonyTask : ITask
+    {
+        public void Execute()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
