aa25ba9ac081c53c7cc989872a6cf928c781c32a
Samer Haidar <VANRISE\samer.haidar>
2018-02-21 16:21:52 +0000

    Refactoring
    git-tfs-id: [http://developmentvlan:8585/tfs/vanrise.collection]$/;C45861

--- TOneV2/Code/TOneV2/TOne.WhS.BusinessEntity.Business/FaultTickets/FaultTicketStatusDefinitionFilter.cs
+++ TOneV2/Code/TOneV2/TOne.WhS.BusinessEntity.Business/FaultTickets/FaultTicketStatusDefinitionFilter.cs
@@ -17,36 +17,35 @@ namespace TOne.WhS.BusinessEntity.Business
             var openStatusId = new Guid("7eb94106-43f1-43eb-8952-8f0b585fd7e5");
             var closedStatusId = new Guid("f299eb6d-b50c-4338-812f-142d4d8515ca");
             var pendingStatusId = new Guid("05a87955-dc2a-4e22-a879-6bea3c31690e");
-            if (CaseId == null)
+            if (!CaseId.HasValue)
             {
-                if (context.StatusDefinition.StatusDefinitionId == openStatusId )
+                if (context.StatusDefinition.StatusDefinitionId == openStatusId)
                     return true;
             }
             else
             {
                 GenericBusinessEntityManager genericBusinessEntityManager = new GenericBusinessEntityManager();
-                var caseInfo = genericBusinessEntityManager.GetGenericBusinessEntity(CaseId, BusinessEntityDefinitionId);
-                if (caseInfo != null)
+                var caseInfo = genericBusinessEntityManager.GetGenericBusinessEntity(CaseId.Value, BusinessEntityDefinitionId);
+                if (caseInfo != null && caseInfo.FieldValues != null)
                 {
                     object fieldValues;
                     if (caseInfo.FieldValues.TryGetValue("StatusId", out fieldValues))
                     {
-                        Guid oldStatusId = (Guid)fieldValues;
-                        if (oldStatusId != null)
+                        Guid oldStatusId = Guid.Parse(fieldValues.ToString());
+
+                        if (oldStatusId == context.StatusDefinition.StatusDefinitionId)
+                            return true;
+                        if (oldStatusId == openStatusId)
                         {
-                            if (oldStatusId == context.StatusDefinition.StatusDefinitionId)
+                            if (context.StatusDefinition.StatusDefinitionId == closedStatusId || context.StatusDefinition.StatusDefinitionId == pendingStatusId)
                                 return true;
-                            if (oldStatusId == openStatusId)
-                            {
-                                if (context.StatusDefinition.StatusDefinitionId == closedStatusId || context.StatusDefinition.StatusDefinitionId == pendingStatusId)
-                                    return true;
-                            }
-                            else if (oldStatusId == pendingStatusId)
-                            {
-                                if (context.StatusDefinition.StatusDefinitionId == closedStatusId)
-                                    return true;
-                            }
                         }
+                        else if (oldStatusId == pendingStatusId)
+                        {
+                            if (context.StatusDefinition.StatusDefinitionId == closedStatusId)
+                                return true;
+                        }
+
                     }
                 }
             }
