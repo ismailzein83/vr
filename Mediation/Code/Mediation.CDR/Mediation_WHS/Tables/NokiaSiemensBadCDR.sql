dbeb436cebc25d94cc2d51079392de9fab721037
anthony douaihy <VANRISE\anthony.douaihy>
2018-02-22 17:34:04 +0000

    adjust mediation CDR schema
    git-tfs-id: [http://developmentvlan:8585/tfs/vanrise.collection]$/;C45869

---
+++ Mediation/Code/Mediation.CDR/Mediation_WHS/Tables/NokiaSiemensBadCDR.sql
@@ -0,0 +1,15 @@
+﻿CREATE TABLE [Mediation_WHS].[NokiaSiemensBadCDR] (
+    [Id]                       BIGINT       NULL,
+    [BeginDate]                DATETIME     NULL,
+    [EndDate]                  DATETIME     NULL,
+    [DurationInSeconds]        INT          NULL,
+    [CallingPartyNumber]       VARCHAR (50) NULL,
+    [CalledPartyNumber]        VARCHAR (50) NULL,
+    [IncomingTrunkGroupNumber] VARCHAR (50) NULL,
+    [IncomingTrunkNumber]      INT          NULL,
+    [OutgoingTrunkGroupNumber] VARCHAR (50) NULL,
+    [OutgoingTrunkNumber]      INT          NULL,
+    [CauseValue]               INT          NULL,
+    [Zone]                     VARCHAR (50) NULL
+);
+
---
+++ Mediation/Code/Mediation.CDR/Mediation_WHS/User Defined Types/NokiaSiemensCDRType.sql
@@ -0,0 +1,14 @@
+﻿CREATE TYPE [Mediation_WHS].[NokiaSiemensCDRType] AS TABLE (
+    [Id]                       BIGINT       NULL,
+    [BeginDate]                DATETIME     NULL,
+    [EndDate]                  DATETIME     NULL,
+    [DurationInSeconds]        INT          NULL,
+    [CallingPartyNumber]       VARCHAR (50) NULL,
+    [CalledPartyNumber]        VARCHAR (50) NULL,
+    [IncomingTrunkGroupNumber] VARCHAR (50) NULL,
+    [IncomingTrunkNumber]      INT          NULL,
+    [OutgoingTrunkGroupNumber] VARCHAR (50) NULL,
+    [OutgoingTrunkNumber]      INT          NULL,
+    [CauseValue]               INT          NULL,
+    [Zone]                     VARCHAR (50) NULL);
+
