c2589b3aecda33cf67a5e5a0fd359ae57ee7a1c8
anthony douaihy <VANRISE\anthony.douaihy>
2018-02-23 13:15:48 +0000

    Adding FileName @NokiaSiemensCDRs table
    git-tfs-id: [http://developmentvlan:8585/tfs/vanrise.collection]$/;C45887

--- Mediation/Code/Mediation.CDR/Mediation_ICX/Tables/NokiaSiemensBadCDR.sql
+++ Mediation/Code/Mediation.CDR/Mediation_ICX/Tables/NokiaSiemensBadCDR.sql
@@ -1,15 +1,18 @@
 ﻿CREATE TABLE [Mediation_ICX].[NokiaSiemensBadCDR] (
-    [Id]                       BIGINT       NULL,
-    [BeginDate]                DATETIME     NULL,
-    [EndDate]                  DATETIME     NULL,
-    [DurationInSeconds]        INT          NULL,
-    [CallingPartyNumber]       VARCHAR (50) NULL,
-    [CalledPartyNumber]        VARCHAR (50) NULL,
-    [IncomingTrunkGroupNumber] VARCHAR (50) NULL,
-    [IncomingTrunkNumber]      INT          NULL,
-    [OutgoingTrunkGroupNumber] VARCHAR (50) NULL,
-    [OutgoingTrunkNumber]      INT          NULL,
-    [CauseValue]               INT          NULL,
-    [Zone]                     VARCHAR (50) NULL
+    [Id]                       BIGINT        NULL,
+    [BeginDate]                DATETIME      NULL,
+    [EndDate]                  DATETIME      NULL,
+    [DurationInSeconds]        INT           NULL,
+    [CallingPartyNumber]       VARCHAR (50)  NULL,
+    [CalledPartyNumber]        VARCHAR (50)  NULL,
+    [IncomingTrunkGroupNumber] VARCHAR (50)  NULL,
+    [IncomingTrunkNumber]      INT           NULL,
+    [OutgoingTrunkGroupNumber] VARCHAR (50)  NULL,
+    [OutgoingTrunkNumber]      INT           NULL,
+    [CauseValue]               INT           NULL,
+    [Zone]                     VARCHAR (50)  NULL,
+    [FileName]                 VARCHAR (255) NULL
 );
+
+
 
--- Mediation/Code/Mediation.CDR/Mediation_ICX/User Defined Types/NokiaSiemensBadCDRType.sql
+++ Mediation/Code/Mediation.CDR/Mediation_ICX/User Defined Types/NokiaSiemensBadCDRType.sql
@@ -1,14 +1,17 @@
 ﻿CREATE TYPE [Mediation_ICX].[NokiaSiemensBadCDRType] AS TABLE (
-    [Id]                       BIGINT       NULL,
-    [BeginDate]                DATETIME     NULL,
-    [EndDate]                  DATETIME     NULL,
-    [DurationInSeconds]        INT          NULL,
-    [CallingPartyNumber]       VARCHAR (50) NULL,
-    [CalledPartyNumber]        VARCHAR (50) NULL,
-    [IncomingTrunkGroupNumber] VARCHAR (50) NULL,
-    [IncomingTrunkNumber]      INT          NULL,
-    [OutgoingTrunkGroupNumber] VARCHAR (50) NULL,
-    [OutgoingTrunkNumber]      INT          NULL,
-    [CauseValue]               INT          NULL,
-    [Zone]                     VARCHAR (50) NULL);
+    [Id]                       BIGINT        NULL,
+    [BeginDate]                DATETIME      NULL,
+    [EndDate]                  DATETIME      NULL,
+    [DurationInSeconds]        INT           NULL,
+    [CallingPartyNumber]       VARCHAR (50)  NULL,
+    [CalledPartyNumber]        VARCHAR (50)  NULL,
+    [IncomingTrunkGroupNumber] VARCHAR (50)  NULL,
+    [IncomingTrunkNumber]      INT           NULL,
+    [OutgoingTrunkGroupNumber] VARCHAR (50)  NULL,
+    [OutgoingTrunkNumber]      INT           NULL,
+    [CauseValue]               INT           NULL,
+    [Zone]                     VARCHAR (50)  NULL,
+    [FileName]                 VARCHAR (255) NULL);
+
+
 
