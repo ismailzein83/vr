e9d334d9e7d79b9e6c0839e7ba6dc1f4a2f01033
Ali Atoui <VANRISE\ali.atoui>
2018-02-23 15:54:20 +0000

    Adding AliAtouiTask
    git-tfs-id: [http://developmentvlan:8585/tfs/vanrise.collection]$/;C45893

---
+++ Mediation/Code/Mediation.Runtime/Tasks/AliAtouiTask.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Mediation.Runtime.Tasks
+{
+    public class AliAtouiTask : ITask
+    {
+        public void Execute()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
