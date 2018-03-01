1165ebaf3e8b763707dea41584a022e466cbd528
Ali Ballouk <VANRISE\ali.ballouk>
2018-03-01 07:55:38 +0000

    Add Customer Sold Zones Entities
    git-tfs-id: [http://developmentvlan:8585/tfs/vanrise.collection]$/;C45939

---
+++ TOneV2/Code/TOneV2/TOne.WhS.BusinessEntity.Entities/CustomerSoldZones/CustomerSoldZones.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TOne.WhS.BusinessEntity.Entities
+{
+    public class CustomersSoldZone
+    {
+        public long ZoneId { get; set; }
+        public List<CustomerZoneData> CustomerZoneData { get; set; }
+        public int SaleCount { get; set; }
+    }
+
+    public class CustomerZoneData
+    {
+        public int CustomerId { get; set; }
+
+        public decimal Rate { get; set; }
+
+        public int RoutingProductId { get; set; }
+
+    }
+
+    public class CustomersSoldZoneDetail
+    {
+        public long ZoneId { get; set; }
+        public string Name { get; set; }
+        public IEnumerable<CustomerZoneDataDetail> CustomerZones { get; set; }
+        public int SaleCount { get; set; }
+    }
+
+    public class CustomerZoneDataDetail
+    {
+        public string CustomerName { get; set; }
+
+        public decimal Rate { get; set; }
+        
+        public int RoutingProductId { get; set; }
+
+    }
+}
---
+++ TOneV2/Code/TOneV2/TOne.WhS.BusinessEntity.Entities/CustomerSoldZones/CustomerSoldZonesQuery.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TOne.WhS.BusinessEntity.Entities
+{
+    public class CustomerSoldZonesQuery
+    {
+        public string ZoneName { get; set; }
+
+        public string Code { get; set; }
+
+        public int Top { get; set; }
+
+        public int SellingNumberPlanId { get; set; }
+
+        public List<int> CountriesIds { get; set; }
+
+        public List<int> CustomersIds { get; set; }
+
+        public List<int> RoutingProductsIds { get; set; }
+
+        public DateTime EffectiveOn { get; set; }
+
+        public int CurrencyId { get; set; }   
+
+    }
+
+    
+}
