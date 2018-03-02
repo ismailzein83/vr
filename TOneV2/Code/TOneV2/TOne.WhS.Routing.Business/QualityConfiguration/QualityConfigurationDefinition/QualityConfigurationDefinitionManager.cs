f03009c24e4ac6c05961fcaf87908458a46830bf
Ali Atoui <VANRISE\ali.atoui>
2018-03-02 14:16:47 +0000

    Adding QualityConfigurationDefinitionManager and TrafficStatisticExtendedSettings
    git-tfs-id: [http://developmentvlan:8585/tfs/vanrise.collection]$/;C45967

---
+++ TOneV2/Code/TOneV2/TOne.WhS.Routing.Business/QualityConfiguration/QualityConfigurationDefinition/QualityConfigurationDefinitionManager.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TOne.WhS.Routing.Entities;
+using Vanrise.Common.Business;
+using Vanrise.Common;
+
+namespace TOne.WhS.Routing.Business
+{
+    public class QualityConfigurationDefinitionManager
+    {
+        //#region Public Methods
+
+        //public IEnumerable<QualityConfigurationDefinitionInfo> GetBEParentChildRelationDefinitionsInfo(QualityConfigurationDefinitionFilter filter)
+        //{
+        //    Func<QualityConfigurationDefinition, bool> filterExpression = null;
+        //    if (filter != null)
+        //    {
+        //        filterExpression = (qualityConfigurationDefinition) => 
+        //        {
+        //            return true;
+        //        };
+        //    }
+
+        //    var beParentChildRelationDefinitions = this.GetCachedQualityConfigurationDefinitions();
+        //    return beParentChildRelationDefinitions.MapRecords(BEParentChildRelationDefinitionInfoMapper, filterExpression);
+        //} 
+
+        //public IEnumerable<QualityConfigurationDefinitionExtendedSettingsConfig> GetQualityConfigurationDefinitionExtendedSettingsConfigs()
+        //{
+        //    var manager = new ExtensionConfigurationManager();
+        //    return manager.GetExtensionConfigurations<QualityConfigurationDefinitionExtendedSettingsConfig>(QualityConfigurationDefinitionExtendedSettingsConfig.EXTENSION_TYPE);
+        //}
+
+        //#endregion
+
+        //#region Private Methods
+
+        //private Dictionary<Guid, QualityConfigurationDefinition> GetCachedQualityConfigurationDefinitions()
+        //{
+        //    VRComponentTypeManager vrComponentTypeManager = new Vanrise.Common.Business.VRComponentTypeManager();
+        //    return vrComponentTypeManager.GetCachedComponentTypes<QualityConfigurationDefinitionSettings, QualityConfigurationDefinition>();
+        //}
+
+        //#endregion
+
+        //#region Mappers
+
+        //public QualityConfigurationDefinitionInfo BEParentChildRelationDefinitionInfoMapper(QualityConfigurationDefinition qualityConfigurationDefinition)
+        //{
+        //    return new QualityConfigurationDefinitionInfo
+        //    {
+        //        QualityConfigurationDefinitionId = qualityConfigurationDefinition.VRComponentTypeId,
+        //        Name = qualityConfigurationDefinition.Name
+        //    };
+        //}
+
+        //#endregion
+    }
+}
---
+++ TOneV2/Code/TOneV2/TOne.WhS.Routing.Business/QualityConfiguration/QualityConfigurationDefinition/TrafficStatisticExtendedSettings.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TOne.WhS.Routing.Business
+{
+    public class TrafficStatisticExtendedSettings //: TOne.WhS.Routing.Entities.QualityConfigurationDefinitionExtendedSettings
+    {
+        //public override Guid ConfigId { get { return new Guid("E670425F-2F91-4C9A-BB03-8B85CD77BBD2"); } }
+
+        //public override string RuntimeEditor { get { return ""; } }
+
+        //public Guid  AnalyticTableId { get; set; }
+
+        //public List<string> IncludedMeasures { get; set; } 
+    }
+}
