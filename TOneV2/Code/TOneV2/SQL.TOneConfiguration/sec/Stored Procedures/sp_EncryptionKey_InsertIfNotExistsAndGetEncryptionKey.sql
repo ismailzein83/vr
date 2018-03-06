30a3abee041a3a3f501a1c91a6b664b2b38665fb
Ismail Zein <VANRISE\ismail.zein>
2018-03-06 08:22:24 +0000

    sql project
    git-tfs-id: [http://developmentvlan:8585/tfs/vanrise.collection]$/;C46014

---
+++ TOneV2/Code/TOneV2/SQL.TOneConfiguration/sec/Stored Procedures/sp_EncryptionKey_InsertIfNotExistsAndGetEncryptionKey.sql
@@ -0,0 +1,15 @@
+﻿CREATE PROCEDURE [sec].[sp_EncryptionKey_InsertIfNotExistsAndGetEncryptionKey]
+	@EncryptionKey nvarchar(255)
+AS
+BEGIN	
+	
+	--INSERT Encryption Key if not exists
+	IF NOT EXISTS (SELECT TOP 1 NULL FROM sec.[EncryptionKey] WITH(NOLOCK))
+	BEGIN
+		INSERT INTO sec.[EncryptionKey] ([EncryptionKey])
+		SELECT @EncryptionKey WHERE NOT EXISTS (SELECT TOP 1 NULL FROM sec.[EncryptionKey])
+	END
+	
+	SELECT TOP 1 EncryptionKey FROM sec.[EncryptionKey]
+	ORDER BY ID
+END
---
+++ TOneV2/Code/TOneV2/SQL.TOneConfiguration/sec/Tables/EncryptionKey.sql
@@ -0,0 +1,6 @@
+﻿CREATE TABLE [sec].[EncryptionKey] (
+    [ID]            INT            IDENTITY (1, 1) NOT NULL,
+    [EncryptionKey] NVARCHAR (255) NOT NULL,
+    CONSTRAINT [PK_EncryptionKey] PRIMARY KEY CLUSTERED ([ID] ASC)
+);
+
