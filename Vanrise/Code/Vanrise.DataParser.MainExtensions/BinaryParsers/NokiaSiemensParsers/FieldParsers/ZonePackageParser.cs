ebaf6f49015bbd63476578f416dd5136bf729014
anthony douaihy <VANRISE\anthony.douaihy>
2018-03-06 14:03:58 +0000

    add ZonePackageParser
    git-tfs-id: [http://developmentvlan:8585/tfs/vanrise.collection]$/;C46027

---
+++ Vanrise/Code/Vanrise.DataParser.MainExtensions/BinaryParsers/NokiaSiemensParsers/FieldParsers/ZonePackageParser.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using Vanrise.DataParser.Business;
+using Vanrise.DataParser.Entities;
+
+namespace Vanrise.DataParser.MainExtensions.BinaryParsers.NokiaSiemensParsers.FieldParsers
+{
+    public class ZonePackageParser : BinaryFieldParserSettings
+    {
+        public override Guid ConfigId { get { return new Guid("F1FC606C-F862-41CB-A832-1C670F555EF9"); } }
+        public string ZoneFieldName { get; set; }
+
+        public override void Execute(IBinaryFieldParserContext context)
+        {
+            
+        }
+    }
+}
