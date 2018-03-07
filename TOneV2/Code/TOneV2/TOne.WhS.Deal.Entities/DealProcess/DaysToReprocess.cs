fcd2ba25da0412f1b439979324c148f888bb4603
anthony douaihy <VANRISE\anthony.douaihy>
2018-03-07 16:50:04 +0000

    add deal entities
    git-tfs-id: [http://developmentvlan:8585/tfs/vanrise.collection]$/;C46050

---
+++ TOneV2/Code/TOneV2/TOne.WhS.Deal.Entities/DealProcess/DaysToReprocess.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace TOne.WhS.Deal.Entities
+{
+    public class DaysToReprocess
+    {
+        public long DaysToReprocessId { get; set; }
+        public DateTime Date { get; set; }
+        public bool IsSale { get; set; }
+        public int CarrierAccountId { get; set; }
+    }
+}
---
+++ TOneV2/Code/TOneV2/TOne.WhS.Deal.Entities/DealProcess/DealZoneRate.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace TOne.WhS.Deal.Entities
+{
+    public class DealZoneRate
+    {
+        public long DealZoneRateId { get; set; }
+        public int DealId { get; set; }
+        public int ZoneGroupNb { get; set; }
+        public bool IsSale { get; set; }
+        public int TierNb { get; set; }
+        public long ZoneId { get; set; }
+        public decimal? Rate { get; set; }
+        public DateTime BED { get; set; }
+        public DateTime? EED { get; set; }
+    }
+}
