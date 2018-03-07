11ad0b53cc8dc720911063499e54adc417733758
Ismail Zein <VANRISE\ismail.zein>
2018-03-07 17:06:43 +0000

    added SpecificDimensionMapping
    git-tfs-id: [http://developmentvlan:8585/tfs/vanrise.collection]$/;C46051

---
+++ Vanrise/Code/Vanrise.Analytic.MainExtensions/AnalyticMeasureExternalSources/AnalyticTable/DimensionMappingRules/SpecificDimensionMapping.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Vanrise.Analytic.MainExtensions.AnalyticMeasureExternalSources.AnalyticTable.DimensionMappingRules
+{
+    public class SpecificDimensionMapping : DimensionMappingRuleSettings
+    {
+        public override Guid ConfigId
+        {
+            get { return new Guid("1E93563D-CCA6-48F4-BD34-A21B26A3B773"); }
+        }
+
+        public string DimensionName { get; set; }
+
+        public string MappedDimensionName { get; set; }
+
+        public override bool TryMapDimension(IDimensionMappingRuleTryMapDimensionContext context)
+        {
+            if (context.DimensionName == this.DimensionName)
+            {
+                context.MappedDimensionName = this.MappedDimensionName;
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+    }
+}
