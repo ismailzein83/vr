e5a8d7fdc78d7ddad4c4fc03dd059314b6814e39
Wissam Ajamy <VANRISE\wissam.ajamy>
2018-03-12 08:49:54 +0000

    update structure from TOneV2_Dev
    git-tfs-id: [http://developmentvlan:8585/tfs/vanrise.collection]$/;C46117

---
+++ TOneV2/Code/TOneV2/SQL.TOneWhS_BE/TOneWhS_BE/Stored Procedures/sp_ANumberSaleCode_GetEffectiveBySellingNumberPlanId.sql
@@ -0,0 +1,24 @@
+﻿CREATE PROCEDURE [TOneWhS_BE].[sp_ANumberSaleCode_GetEffectiveBySellingNumberPlanId]	
+	@SellingNumberPlanId int ,
+	@EfectiveOn DateTime
+AS
+BEGIN
+	-- SET NOCOUNT ON added to prevent extra result sets from
+	-- interfering with SELECT statements.
+	SET NOCOUNT ON;
+	
+	    BEGIN
+
+		SELECT  salecode.ID,
+				salecode.ANumberGroupID,
+				salecode.SellingNumberPlanID,
+				salecode.Code,
+				salecode.BED,
+				salecode.EED
+		FROM [TOneWhS_BE].ANumberSaleCode salecode WITH(NOLOCK) 
+		WHERE	salecode.SellingNumberPlanID = @SellingNumberPlanId
+		and (salecode.EED is null or (salecode.EED<>salecode.BED and salecode.EED > @EfectiveOn))
+        
+		END
+	SET NOCOUNT OFF
+END
---
+++ TOneV2/Code/TOneV2/SQL.TOneWhS_BE/TOneWhS_Deal/Stored Procedures/sp_DealZoneRate_GetAll.sql
@@ -0,0 +1,10 @@
+﻿CREATE PROCEDURE [TOneWhS_Deal].[sp_DealZoneRate_GetAll]
+AS
+BEGIN
+	-- SET NOCOUNT ON added to prevent extra result sets from
+	-- interfering with SELECT statements.
+	SET NOCOUNT ON;
+	SELECT	dzr.ID,dzr.DealId,dzr.ZoneGroupNb,dzr.IsSale,dzr.TierNb,dzr.ZoneId,dzr.Rate,dzr.BED,dzr.EED
+	FROM	[TOneWhS_Deal].DealZoneRate dzr WITH(NOLOCK) 
+	SET NOCOUNT OFF
+END
