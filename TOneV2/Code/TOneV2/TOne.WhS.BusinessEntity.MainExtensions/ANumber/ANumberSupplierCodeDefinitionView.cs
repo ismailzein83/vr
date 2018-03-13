25ddd4b0ad14224f933d107d3206c1ebde585cb3
Ali Ballouk <VANRISE\ali.ballouk>
2018-03-13 13:05:50 +0000

    Add ANumberSupplierCodeDefinitionView class
    git-tfs-id: [http://developmentvlan:8585/tfs/vanrise.collection]$/;C46143

---
+++ TOneV2/Code/TOneV2/TOne.WhS.BusinessEntity.MainExtensions/ANumber/ANumberSupplierCodeDefinitionView.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Vanrise.GenericData.Business;
+
+namespace TOne.WhS.BusinessEntity.MainExtensions
+{
+    public class ANumberSupplierCodeDefinitionView : GenericBEViewDefinitionSettings
+    {
+        public override Guid ConfigId
+        {
+            get { return new Guid("6821BD9E-318C-49A9-B3B6-08AA3AAA4006"); }
+        }
+        public override string RuntimeDirective
+        {
+            get { return "whs-be-anumber-suppliercodegridview-runtime"; }
+        }
+
+        public override bool DoesUserHaveAccess(IGenericBEViewDefinitionCheckAccessContext context)
+        {
+            return true;//base.DoesUserHaveAccess(context);
+        }
+    }
+
+}
