3a9925f8434e508b941b0aa6cdfe779d6df769e7
Ali Atoui <VANRISE\ali.atoui>
2018-03-13 14:34:49 +0000

    Arranging PricingPackageDefinitionSettings
    git-tfs-id: [http://developmentvlan:8585/tfs/vanrise.collection]$/;C46148

--- Retail/Code/Retail.BusinessEntity.MainExtensions/PackageDefinitionExtendedSettings/PricingPackageDefinitionSettings.cs
+++ Retail/Code/Retail.BusinessEntity.MainExtensions/PackageDefinitionExtendedSettings/PricingPackageDefinitionSettings.cs
@@ -9,14 +9,8 @@ namespace Retail.BusinessEntity.MainExtensions
 {
     public class PricingPackageDefinitionSettings : PackageDefinitionExtendedSettings
     {
-        public override Guid ConfigId
-        {
-            get { return new Guid("76A889A4-9F93-4327-91C4-EE2F1EF2026E"); }
-            
-        }
-        public override string RuntimeEditor
-        {
-            get { return "retail-be-pricingpackagesettings-management"; } 
-        }
+        public override Guid ConfigId { get { return new Guid("76A889A4-9F93-4327-91C4-EE2F1EF2026E"); } }
+
+        public override string RuntimeEditor { get { return "retail-be-pricingpackagesettings-management"; } }
     }
 }
