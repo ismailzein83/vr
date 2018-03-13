9acb327d9c7e55854713662b3c0492561ce6aa33
Ali Atoui <VANRISE\ali.atoui>
2018-03-13 12:21:36 +0000

    Editing TrafficStatisticQCDefinitionSettings
    git-tfs-id: [http://developmentvlan:8585/tfs/vanrise.collection]$/;C46140

---
+++ TOneV2/Code/TOneV2/TOne.WhS.Routing.Business/QualityConfiguration/QualityConfigurationDefinition/TrafficStatisticQCDefinitionSettings.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TOne.WhS.Routing.Entities;
+
+namespace TOne.WhS.Routing.Business
+{
+    public class TrafficStatisticQCDefinitionSettings //: TOne.WhS.Routing.Entities.QualityConfigurationDefinitionExtendedSettings 
+    {
+        //public override Guid ConfigId { get { return new Guid("E670425F-2F91-4C9A-BB03-8B85CD77BBD2"); } }
+
+        //public override string RuntimeEditor { get { return ""; } }
+
+        //public Guid AnalyticTableId { get; set; }
+
+        //public List<string> IncludedMeasures { get; set; }
+
+        //public List<string> CustomerRouteDimensions { get; set; }
+
+        //public List<string> ProductRouteDimensions { get; set; } 
+
+        //public override Dictionary<Guid, InitializedQualityConfiguration> InitializeQualityConfigurations(List<RouteRuleQualityConfiguration> qualityConfigurations)
+        //{
+        //    return new TrafficStatisticQualityConfigurationManager().InitializeTrafficStatisticQualityConfigurations(qualityConfigurations);
+        //}
+    }
+
+    public class InitializedTrafficStatisticQualityConfiguration : InitializedQualityConfiguration
+    {
+        public IRouteRuleQualityConfigurationEvaluator Evaluator { get; set; }
+    }
+}
