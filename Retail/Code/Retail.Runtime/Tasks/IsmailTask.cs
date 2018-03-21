afacc9859da628e9e8bbf0a7133ee4c33e5575ed
Ismail Zein <VANRISE\ismail.zein>
2018-03-21 12:56:51 +0000

    added RA config files
    git-tfs-id: [http://developmentvlan:8585/tfs/vanrise.collection]$/;C46230

--- Retail/Code/Retail.Runtime/Tasks/IsmailTask.cs
+++ Retail/Code/Retail.Runtime/Tasks/IsmailTask.cs
@@ -94,6 +94,7 @@ namespace Retail.Runtime.Tasks
             Vanrise.Common.Business.OverriddenConfigurationManager overriddenConfigurationManager = new Vanrise.Common.Business.OverriddenConfigurationManager();
             var zajilScript = overriddenConfigurationManager.GenerateOverriddenConfigurationGroupScript(new Guid("CF1EAF73-93DB-416F-92FC-F8C0E7EE6EA7"));
             var multinetScript = overriddenConfigurationManager.GenerateOverriddenConfigurationGroupScript(new Guid("D79E9957-3EA5-49C1-AEDB-15F251BEDCDC"));
+            var raScript = overriddenConfigurationManager.GenerateOverriddenConfigurationGroupScript(new Guid("0ef03fe5-6a6b-4271-a97b-d9a14298cfea"));
             var devScript = overriddenConfigurationManager.GenerateOverriddenConfigurationDevScript();
             var runtimeServices = new List<RuntimeService>();
 
