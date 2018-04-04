28827a9d95bf60a73d283f71bdd76c6512a50dde
Ismail Zein <VANRISE\ismail.zein>
2018-04-04 05:01:35 +0000

    added CustomCodeBPArgument
    git-tfs-id: [http://developmentvlan:8585/tfs/vanrise.collection]$/;C46407

---
+++ BusinessProcess/Code/Vanrise.BusinessProcess.Extensions.WFTaskAction.Arguments/CustomCodeBPArgument.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Vanrise.BusinessProcess.Extensions.WFTaskAction.Arguments
+{
+    public class CustomCodeBPArgument : Vanrise.BusinessProcess.Entities.BaseProcessInputArgument
+    {
+        public override string GetTitle()
+        {
+            return string.Format("Custom Code: {0}", this.Name);
+        }
+        public string Name { get; set; }
+
+        public string ClassDefinitions { get; set; }
+
+        public string TaskCode { get; set; }
+    }
+
+    public interface ICustomCodeHandler
+    {
+        void Execute(ICustomCodeExecutionContext context);
+    }
+
+    public interface ICustomCodeExecutionContext
+    {
+        void LogException(Exception ex);
+
+        void LogError(string messageFormat, params object[] args);
+
+        void LogWarning(string messageFormat, params object[] args);
+
+        void LogInfo(string messageFormat, params object[] args);
+
+        void SendMail(string from, string to, string cc, string subject, string body);
+    }
+}
