246dd1e8b49f850b16388254f7d9741531e9aa90
Ismail Zein <VANRISE\ismail.zein>
2018-04-06 07:18:36 +0000

    sql project
    git-tfs-id: [http://developmentvlan:8585/tfs/vanrise.collection]$/;C46434

---
+++ TOneV2/Code/TOneV2/SQL.TOneLogging/logging/Stored Procedures/sp_LogEntry_Insert.sql
@@ -0,0 +1,48 @@
+﻿-- =============================================
+-- Author:		<Author,,Name>
+-- Create date: <Create Date,,>
+-- Description:	<Description,,>
+-- =============================================
+CREATE PROCEDURE logging.sp_LogEntry_Insert 
+	@MachineNameId int,
+	@ApplicationNameId int,
+	@AssemblyNameId int,
+	@TypeNameId int,
+	@MethodNameId int,
+	@EntryType int,
+	@EventType int,
+	@ViewRequiredPermissionSetId int,
+	@Message nvarchar(max),
+	@ExceptionDetail nvarchar(max),
+	@EventTime datetime
+AS
+BEGIN
+	-- SET NOCOUNT ON added to prevent extra result sets from
+	-- interfering with SELECT statements.
+	SET NOCOUNT ON;
+
+    INSERT INTO [logging].[LogEntry]
+           ([MachineNameId]
+           ,[ApplicationNameId]
+           ,[AssemblyNameId]
+           ,[TypeNameId]
+           ,[MethodNameId]
+           ,[EntryType]
+           ,[EventType]
+           ,[ViewRequiredPermissionSetId]
+           ,[Message]
+           ,[ExceptionDetail]
+           ,[EventTime])
+     VALUES
+           (@MachineNameId
+           ,@ApplicationNameId
+           ,@AssemblyNameId
+           ,@TypeNameId
+           ,@MethodNameId
+           ,@EntryType
+           ,@EventType
+           ,@ViewRequiredPermissionSetId
+           ,@Message
+           ,@ExceptionDetail
+           ,@EventTime)
+END
