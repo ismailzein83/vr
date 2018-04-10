11e5f4160673e87e255a2ffb9221e0e671784c23
Mortada Issa <VANRISE\mortada.issa>
2018-04-10 17:49:04 +0000

    Added A new settings to Vanrise Common related to National Data
    git-tfs-id: [http://developmentvlan:8585/tfs/vanrise.collection]$/;C46472

---
+++ Vanrise/Code/Vanrise.Common.Web/Common/Directives/Settings/NationalSettings/vr-common-national-settings-editor.js
@@ -0,0 +1,73 @@
+﻿'use strict';
+
+app.directive('vrCommonNationalSettingsEditor', ['UtilsService', 'VRUIUtilsService',
+    function (UtilsService, VRUIUtilsService) {
+
+        var directiveDefinitionObject = {
+            restrict: 'E',
+            scope: {
+                onReady: '=',
+            },
+            controller: function ($scope, $element, $attrs) {
+                var ctrl = this;
+                var ctor = new settingEditorCtor(ctrl, $scope, $attrs);
+                ctor.initializeController();
+            },
+            controllerAs: 'ctrl',
+            bindToController: true,
+            compile: function (element, attrs) {
+
+            },
+            templateUrl: "/Client/Modules/Common/Directives/Settings/NationalSettings/Templates/NationalSettings.html"
+        };
+
+        function settingEditorCtor(ctrl, $scope, $attrs) {
+            var countryDirectiveApi;
+            var countryReadyPromiseDeferred = UtilsService.createPromiseDeferred();
+
+            $scope.onCountryDirectiveReady = function (api) {
+                countryDirectiveApi = api;
+                countryReadyPromiseDeferred.resolve();
+            };
+
+            function initializeController() {
+                $scope.scopeModel = {};
+
+                countryReadyPromiseDeferred.promise.then(function () {
+                    defineAPI();
+                });
+            }
+
+            function defineAPI() {
+                var api = {};
+
+                api.load = function (payload) {
+
+                    var countryLoadPromiseDeferred = UtilsService.createPromiseDeferred();
+
+                    var countryPayload;
+                    if (payload != undefined && payload.data != undefined) {
+                        countryPayload = { selectedIds: payload.data.NationalCountries };
+                    }
+
+                    VRUIUtilsService.callDirectiveLoad(countryDirectiveApi, countryPayload, countryLoadPromiseDeferred);
+
+                    return countryLoadPromiseDeferred.promise;
+                };
+
+                api.getData = function () {
+                    return {
+                        $type: "Vanrise.Entities.NationalSettings, Vanrise.Entities",
+                        nationalCountries: countryDirectiveApi.getSelectedIds()
+                    };
+                };
+
+                if (ctrl.onReady != null)
+                    ctrl.onReady(api);
+            }
+
+            this.initializeController = initializeController;
+
+        }
+        return directiveDefinitionObject;
+    }]);
---
+++ Vanrise/Code/Vanrise.Entities/Settings/NationalSettings/NationalSettings.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Vanrise.Entities
+{
+    public class NationalSettings : SettingData
+    {
+        public const string SETTING_TYPE = "VR_Common_NationalSettings";
+
+        public List<int> NationalCountries { get; set; }
+    }
+}
