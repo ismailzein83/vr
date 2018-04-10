4fbbbf27b118fcc9f46f1b6bdbe92b1a8ccee850
Samer Haidar <VANRISE\samer.haidar>
2018-04-10 15:29:14 +0000

    Adding new files
    git-tfs-id: [http://developmentvlan:8585/tfs/vanrise.collection]$/;C46470

---
+++ Vanrise/Code/Vanrise.Common.Web/Controllers/SMSMessageTemplateController.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Vanrise.Common.Web.Controllers
+{
+    public class SMSMessageTemplateController
+    {
+    }
+}
