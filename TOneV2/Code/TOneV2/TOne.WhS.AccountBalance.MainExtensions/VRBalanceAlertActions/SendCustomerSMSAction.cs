582d3037346adf514e13b9750bb683139649c10e
Samer Haidar <VANRISE\samer.haidar>
2018-04-16 08:48:03 +0000

    adding SendCustomerSMSActionDefinition
    git-tfs-id: [http://developmentvlan:8585/tfs/vanrise.collection]$/;C46535

---
+++ TOneV2/Code/TOneV2/TOne.WhS.AccountBalance.MainExtensions/VRBalanceAlertActions/SendCustomerSMSAction.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Vanrise.Notification.Entities;
+
+namespace TOne.WhS.AccountBalance.MainExtensions.VRBalanceAlertActions
+{
+    public class SendCustomerSMSAction : VRAction
+    {
+        public Guid AccountSMSTemplateId { get; set; }
+
+        public Guid ProfileSMSTemplateId { get; set; }
+        public override void Execute(IVRActionExecutionContext context)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ TOneV2/Code/TOneV2/TOne.WhS.AccountBalance.MainExtensions/VRBalanceAlertActions/SendCustomerSMSActionDefinition.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TOne.WhS.AccountBalance.Business;
+using Vanrise.Notification.Entities;
+
+namespace TOne.WhS.AccountBalance.MainExtensions.VRBalanceAlertActions
+{
+    public class SendCustomerSMSActionDefinition : VRActionDefinitionExtendedSettings
+    {
+        public Guid AccountSMSMessageTypeId { get; set; }
+        public Guid ProfileSMSMessageTypeId { get; set; }
+        public Vanrise.Entities.SMSSendHandler Handler { get; set; }
+        public override Guid ConfigId
+        {
+            get { return new Guid("114CE0AC-4848-442A-B351-BE031F22E130"); }
+        }
+        public override string RuntimeEditor
+        {
+            get
+            {
+                return "";
+            }
+        }
+
+        public override bool IsApplicable(IVRActionDefinitionIsApplicableContext context)
+        {
+            return (context.Target as CustomerAccountBalanceRuleTargetType != null);
+        }
+    }
+}
