688bfd6e1785fe52fc212d71c108b018dd2b7bce
Samer Haidar <VANRISE\samer.haidar>
2018-04-16 08:47:07 +0000

    adding ExecuteDatabaseCommandSMSHandler
    git-tfs-id: [http://developmentvlan:8585/tfs/vanrise.collection]$/;C46534

--- Vanrise/Code/Vanrise.Common.Business/SMS/SMSManager.cs
+++ Vanrise/Code/Vanrise.Common.Business/SMS/SMSManager.cs
@@ -11,12 +11,30 @@ namespace Vanrise.Common.Business
     {
         public void SendSMS(string mobileNumber, string message, DateTime messageTime, SMSSendHandler smsSendHandler)
         {
+            smsSendHandler.ThrowIfNull("smsSendHandler");
+            smsSendHandler.Settings.ThrowIfNull("smsSendHandler.Settings");
 
+            SMSHandlerSendSMSContext context = new SMSHandlerSendSMSContext
+            {
+                Message = message,
+                MessageTime = messageTime,
+                MobileNumber = mobileNumber
+            };
+            smsSendHandler.Settings.SendSMS(context);
         }
 
         public void SendSMS(Guid smsTemplateId, Dictionary<string, dynamic> objects, SMSSendHandler smsSendHandler)
         {
 
         } 
+    }
+
+    public class SMSHandlerSendSMSContext : ISMSHandlerSendSMSContext
+    {
+        public string MobileNumber { get; set; }
+
+        public string Message { get; set; }
+
+        public DateTime MessageTime { get; set; }
     }
 }
---
+++ Vanrise/Code/Vanrise.Common.MainExtensions/SMSSendHandler/ExecuteDatabaseCommandSMSHandler.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Vanrise.Common.Business;
+using Vanrise.Entities;
+
+namespace Vanrise.Common.MainExtensions.SMSSendHandler
+{
+    public class ExecuteDatabaseCommandSMSHandler : SMSSendHandlerSettings
+    {
+        public override Guid ConfigId
+        {
+            get
+            {
+                return new Guid("44E97625-1B35-478A-918E-60F9C58678B4");
+            }
+        }
+        public Guid VRConnectionId { get; set; }
+        public string CommandQuery { get; set; }
+
+        public override void SendSMS(ISMSHandlerSendSMSContext context)
+        {
+
+            //SQLConnection settings = new VRConnectionManager().GetVRConnection(VRConnectionId).Settings as SQLConnection;
+            //string connectionString = (settings != null) ? settings.ConnectionString : null;
+            //if (String.IsNullOrEmpty(connectionString))
+            //    throw new NullReferenceException(String.Format("connection string is null or empty"));
+
+            //using (SqlConnection connection = new SqlConnection(connectionString))
+            //{
+            //    if (connection.State == System.Data.ConnectionState.Closed)
+            //        connection.Open();
+
+            //CommandQuery replace message and number in query 
+
+            //    var command = connection.CreateCommand();
+            //    command.CommandText = CommandQuery;
+            //    command.CommandTimeout = 60;
+            //    command.CommandType = System.Data.CommandType.Text;
+            //    command.ExecuteNonQuery();
+            //    connection.Close();
+            //}
+
+            throw new NotImplementedException();
+        }
+
+      
+    }
+}
