3f9a42c1d1a1c289b06ab9490a2ddd471f81b1a5
Ismail Zein <VANRISE\ismail.zein>
2018-04-18 14:25:17 +0000

    refactored Runtime to rely on RuntimeNodeConfiguration and RuntimeNode in DB
    git-tfs-id: [http://developmentvlan:8585/tfs/vanrise.collection]$/;C46602

--- Xbooster/Code/XBooster.Runtime/Tasks/IsmailTask.cs
+++ Xbooster/Code/XBooster.Runtime/Tasks/IsmailTask.cs
@@ -13,6 +13,8 @@ namespace XBooster.Runtime.Tasks
     {
         public void Execute()
         {
+            GenerateRuntimeNodeConfiguration();
+
             var runtimeServices = new List<RuntimeService>();
 
             BusinessProcessService businessProcessService = new BusinessProcessService() { Interval = new TimeSpan(0, 0, 2) };
@@ -23,6 +25,66 @@ namespace XBooster.Runtime.Tasks
 
             RuntimeHost host = new RuntimeHost(runtimeServices);
             host.Start();
+        }
+
+        private void GenerateRuntimeNodeConfiguration()
+        {
+            var runtimeNodeConfigSettings = new Vanrise.Runtime.Entities.RuntimeNodeConfigurationSettings
+            {
+                Processes = new Dictionary<Guid, RuntimeProcessConfiguration>()
+            };
+            var processSettings = new RuntimeProcessConfigurationSettings
+            {
+                NbOfInstances = 1,
+                IsEnabled = true,
+                Services = new Dictionary<Guid, RuntimeServiceConfiguration>
+                    {
+                        {
+                        new Guid("D1A060C1-F746-48EB-8793-B6EE8AE37902"),
+                         new RuntimeServiceConfiguration
+                         {
+                              Name = "Business Process Regulator Service",
+                              Settings = new RuntimeServiceConfigurationSettings
+                              {                                   
+                                    RuntimeService = new Vanrise.BusinessProcess.BPRegulatorRuntimeService { Interval = TimeSpan.FromSeconds(1) }
+                              }
+                         }
+                         }
+                    }
+            };
+            runtimeNodeConfigSettings.Processes.Add(new Guid("895CF5DB-C01B-4A5C-BD6C-545BAA7A807E"), new RuntimeProcessConfiguration
+            {
+                Name = "Business Process Regulator Service Process",
+                Settings = processSettings
+            });
+
+            processSettings = new RuntimeProcessConfigurationSettings
+            {
+                NbOfInstances = 3,
+                IsEnabled = true,
+                Services = new Dictionary<Guid, RuntimeServiceConfiguration>
+                    {
+                        {
+                        new Guid("D3A4AB4F-F49A-4F5F-9210-D5645F362651"),
+                         new RuntimeServiceConfiguration
+                         {
+                              Name = "Business Process Service",
+                              Settings = new RuntimeServiceConfigurationSettings
+                              {                                   
+                                    RuntimeService = new Vanrise.BusinessProcess.BusinessProcessService { Interval = TimeSpan.FromSeconds(1) }
+                              }
+                         }
+                         }
+                    }
+            };
+            runtimeNodeConfigSettings.Processes.Add(new Guid("B3AD53ED-420F-48DD-B2C9-231561B8B438"), new RuntimeProcessConfiguration
+            {
+                Name = "Business Process Services Process",
+                Settings = processSettings
+            });
+
+
+            var serializedNodeConfigSettings = Vanrise.Common.Serializer.Serialize(runtimeNodeConfigSettings);
         }
     }
 }
