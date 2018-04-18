fe3ee3acda39b8b430a9940fe2e48b2bc38e508b
Ismail Zein <VANRISE\ismail.zein>
2018-04-18 14:25:59 +0000

    refactored Runtime to rely on RuntimeNodeConfiguration and RuntimeNode in DB
    git-tfs-id: [http://developmentvlan:8585/tfs/vanrise.collection]$/;C46603

---
+++ QualityMeasurement/Code/QualityMeasurement.DevRuntime/Tasks/IsmailTask.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Vanrise.Runtime.Entities;
+
+namespace QM.Runtime.Tasks
+{
+    class IsmailTask : ITask
+    {
+        public void Execute()
+        {
+            GenerateRuntimeNodeConfiguration();
+        }
+
+        private void GenerateRuntimeNodeConfiguration()
+        {
+            var runtimeNodeConfigSettings = new Vanrise.Runtime.Entities.RuntimeNodeConfigurationSettings
+            {
+                Processes = new Dictionary<Guid, RuntimeProcessConfiguration>()
+            };
+            var processSettings = new RuntimeProcessConfigurationSettings
+            {
+                NbOfInstances = 1,
+                IsEnabled = true,
+                Services = new Dictionary<Guid, RuntimeServiceConfiguration>
+                    {
+                        {
+                        new Guid("C5205FB9-51C8-45E7-A6D8-D69CD097B3C4"),
+                         new RuntimeServiceConfiguration
+                         {
+                              Name = "Scheduler Service",
+                              Settings = new RuntimeServiceConfigurationSettings
+                              {                                   
+                                    RuntimeService = new Vanrise.Runtime.SchedulerService{ Interval = TimeSpan.FromSeconds(1) }
+                              }
+                         }
+                         }
+                    }
+            };
+            runtimeNodeConfigSettings.Processes.Add(new Guid("1FA8C599-FA3F-4B67-9934-FCB7B9AFE44E"), new RuntimeProcessConfiguration
+            {
+                Name = "Scheduler Service Process",
+                Settings = processSettings
+            });
+            
+            var serializedNodeConfigSettings = Vanrise.Common.Serializer.Serialize(runtimeNodeConfigSettings);
+        }
+    }
+}
