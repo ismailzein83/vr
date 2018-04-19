453f62fa9f6b9bf951431670bc0ab96e301a5302
Wissam Ajamy <VANRISE\wissam.ajamy>
2018-04-19 14:37:37 +0000

    update product info in plugin post scripts
    git-tfs-id: [http://developmentvlan:8585/tfs/vanrise.collection]$/;C46626

--- TOneV2/Code/TOneV2/SQL.TOneConfiguration/Retail.Ntegra.PostDeployment.sql
+++ TOneV2/Code/TOneV2/SQL.TOneConfiguration/Retail.Ntegra.PostDeployment.sql
@@ -9,6 +9,9 @@ Post-Deployment Script Template
                SELECT * FROM [$(TableName)]					
 --------------------------------------------------------------------------------------
 */
---[common].[Setting]--------------------------------------------------------------------------------------------------------------------------------------------------------------------------------beginset nocount on;;with cte_data([ID],[Name],[Type],[Category],[Settings],[Data],[IsTechnical])as (select * from (values--//////////////////////////////////////////////////////////////////////////////////////////////////('509E467B-4562-4CA6-A32E-E50473B74D2C','Product Info','VR_Common_ProductInfoTechnicalSettings','General','{"Editor" : "vr-common-productinfotechnicalsettings-editor"}','{"$type":"Vanrise.Entities.ProductInfoTechnicalSettings, Vanrise.Entities","ProductInfo":{"$type":"Vanrise.Entities.ProductInfo, Vanrise.Entities","ProductName":"Ntegra Retail","VersionNumber":"version #VersionNumber# ~ #VersionDate#"}}',1)--\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\)c([ID],[Name],[Type],[Category],[Settings],[Data],[IsTechnical]))merge	[common].[Setting] as tusing	cte_data as son		1=1 and t.[ID] = s.[ID]when matched then	update set	[Name] = s.[Name],[Type] = s.[Type],[Category] = s.[Category],[Settings] = s.[Settings],[Data] = s.[Data],[IsTechnical] = s.[IsTechnical]when not matched by target then	insert([ID],[Name],[Type],[Category],[Settings],[Data],[IsTechnical])	values(s.[ID],s.[Name],s.[Type],s.[Category],s.[Settings],s.[Data],s.[IsTechnical]);
+--[common].[Setting]----------------------------------------------------------------------------
 ----------------------------------------------------------------------------------------------------
-end
+Update	s
+SET		s.[Data] = REPLACE(s.[Data],'"ProductName":"Retail"','"ProductName":"Ntegra Retail"')
+from	[common].[Setting] s
+where	ID='509E467B-4562-4CA6-A32E-E50473B74D2C'
