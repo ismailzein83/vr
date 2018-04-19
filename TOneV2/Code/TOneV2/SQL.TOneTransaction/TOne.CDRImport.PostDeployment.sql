055a836962ae6fd045aff7245e284c5ce3620a54
Wissam Ajamy <VANRISE\wissam.ajamy>
2018-04-19 08:02:51 +0000

    remove unused post script
    git-tfs-id: [http://developmentvlan:8585/tfs/vanrise.collection]$/;C46609

--- TOneV2/Code/TOneV2/SQL.TOneTransaction/TOne.CDRImport.PostDeployment.sql
+++
@@ -1,9 +0,0 @@
-﻿/*
-Post-Deployment Script Template							
---------------------------------------------------------------------------------------
-The target of this script is to define default data source for CDR import processing				
---------------------------------------------------------------------------------------
-*/
-
-
-
