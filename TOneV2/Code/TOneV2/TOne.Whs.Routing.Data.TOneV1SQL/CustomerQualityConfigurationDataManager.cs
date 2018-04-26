5fbad7d3ef368c89bc42e04e874fa0d690e64232
anthony douaihy <VANRISE\anthony.douaihy>
2018-04-26 08:22:40 +0000

    add missing dataManagers in routing tone v1
    git-tfs-id: [http://developmentvlan:8585/tfs/vanrise.collection]$/;C46701

---
+++ TOneV2/Code/TOneV2/TOne.Whs.Routing.Data.TOneV1SQL/CustomerQualityConfigurationDataManager.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using TOne.WhS.Routing.Data;
+using TOne.WhS.Routing.Entities;
+using Vanrise.Data.SQL;
+
+namespace TOne.Whs.Routing.Data.TOneV1SQL
+{
+    public class CustomerQualityConfigurationDataManager : RoutingDataManager, ICustomerQualityConfigurationDataManager
+    {
+        public void ApplyQualityConfigurationsToDB(object qualityConfigurations)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IEnumerable<CustomerRouteQualityConfigurationData> GetCustomerRouteQualityConfigurationsData()
+        {
+            throw new NotImplementedException();
+        }
+
+        public object FinishDBApplyStream(object dbApplyStream)
+        {
+            throw new NotImplementedException();
+        }
+
+        public object InitialiazeStreamForDBApply()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void WriteRecordToStream(CustomerRouteQualityConfigurationData record, object dbApplyStream)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ TOneV2/Code/TOneV2/TOne.Whs.Routing.Data.TOneV1SQL/SwitchSyncDataDataManager.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections.Generic;
+using System.Data;
+using System.Text;
+using TOne.WhS.Routing.Entities;
+using System.Linq;
+using TOne.WhS.Routing.Data;
+
+namespace TOne.Whs.Routing.Data.TOneV1SQL
+{
+    public class SwitchSyncDataDataManager : RoutingDataManager, ISwitchSyncDataDataManager
+    {
+        public List<SwitchSyncData> GetSwitchSyncDataByIds(List<string> switchIds)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public void ApplySwitchesSyncData(List<string> switchIds, int versionNumber)
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+}
