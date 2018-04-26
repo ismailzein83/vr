0451a2ca00e982d7e6824b927ea93df4538633f4
Ali Sakr <VANRISE\ali.sakr>
2018-04-26 13:32:21 +0000

    adding rule
    git-tfs-id: [http://developmentvlan:8585/tfs/vanrise.collection]$/;C46709

---
+++ TOneV2/Code/TOneV2/TOne.WhS.Sales.Business/BusinessRules/Conditions/OtherRateBEDCondition.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TOne.WhS.Sales.Entities;
+using Vanrise.BusinessProcess.Entities;
+
+namespace TOne.WhS.Sales.Business
+{
+    public class OtherRateBEDCondition : BusinessRuleCondition
+    {
+      /*  public override bool ShouldValidate(IRuleTarget target)
+        {
+            return target is DataByZone;
+        }
+        public override bool Validate(IBusinessRuleConditionValidateContext context)
+        {
+            var zoneData = context.Target as DataByZone;
+            if (zoneData.EED != null && zoneData.OtherRatesToChange.Count() > 0 && zoneData.OtherRatesToChange.First().BED < zoneData.EED)
+            {
+                context.Message = string.Format("Other rate BED on  '{0}' must be after pending normal rate BED", zoneData.ZoneName);
+                return false;
+            }
+            return true;
+           
+        }
+        public override string GetMessage(IRuleTarget target)
+        {
+            throw new NotImplementedException();
+        }*/
+    }
+}
+
