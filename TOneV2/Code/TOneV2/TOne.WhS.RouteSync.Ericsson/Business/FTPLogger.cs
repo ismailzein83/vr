991fb4772f44d0839fb0d46e473166abff468172
anthony douaihy <VANRISE\anthony.douaihy>
2018-04-26 07:28:28 +0000

    implement FTPLogger
    git-tfs-id: [http://developmentvlan:8585/tfs/vanrise.collection]$/;C46698

--- TOneV2/Code/TOneV2/TOne.WhS.RouteSync.Ericsson/Business/FTPLogger.cs
+++ TOneV2/Code/TOneV2/TOne.WhS.RouteSync.Ericsson/Business/FTPLogger.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -13,5 +14,54 @@ namespace TOne.WhS.RouteSync.Ericsson.Business
         public override Guid ConfigId { get { return new Guid("4B424B30-083C-4999-B883-AF4555ECC819"); } }
 
         public FTPCommunicatorSettings FTPCommunicatorSettings { get; set; }
+
+
+        public override void LogRouteCases(ILogRouteCasesContext context)
+        {
+
+            MemoryStream ms = GenerateStream(context.CommandResults);
+
+            string errorMessage;
+            FTPCommunicator communicator = new FTPCommunicator(this.FTPCommunicatorSettings);
+            communicator.TryWriteFile(ms, "", out errorMessage);
+        }
+
+        public override void LogCarrierMappings(ILogCarrierMappingsContext context)
+        {
+            throw new NotImplementedException();
+        }
+
+        public override void LogRoutes(ILogRoutesContext context)
+        {
+            throw new NotImplementedException();
+        }
+
+        public override void LogCommands(ILogCommandsContext context)
+        {
+            throw new NotImplementedException();
+        }
+
+        private MemoryStream GenerateStream(List<CommandResult> commandResults)
+        {
+            if (commandResults == null)
+                return null;
+
+            StringBuilder strBuilder = new StringBuilder();
+            foreach (CommandResult commandResult in commandResults)
+            {
+                strBuilder.AppendLine(commandResult.Command);
+                if (commandResult.Output != null && commandResult.Output.Any())
+                {
+                    foreach (string commandOutput in commandResult.Output)
+                    {
+                        strBuilder.AppendLine(commandOutput);
+                    }
+                }
+                strBuilder.AppendLine();
+            }
+
+            byte[] byteArray = Encoding.UTF8.GetBytes(strBuilder.ToString());
+            return new MemoryStream(byteArray);
+        }
     }
 }
--- TOneV2/Code/TOneV2/TOne.WhS.RouteSync.Ericsson/Entities/SwitchLogger.cs
+++ TOneV2/Code/TOneV2/TOne.WhS.RouteSync.Ericsson/Entities/SwitchLogger.cs
@@ -11,7 +11,30 @@ namespace TOne.WhS.RouteSync.Ericsson.Entities
     {
         public abstract Guid ConfigId { get; }
         public bool IsActive { get; set; }
+
+        public abstract void LogRouteCases(ILogRouteCasesContext context);
+
+        public abstract void LogCarrierMappings(ILogCarrierMappingsContext context);
+
+        public abstract void LogRoutes(ILogRoutesContext context);
+
+        public abstract void LogCommands(ILogCommandsContext context);
     }
+
+    public class CommandResult
+    {
+        public string Command { get; set; }
+
+        public List<string> Output { get; set; }
+    }
+
+    public interface ILogRouteCasesContext { List<CommandResult> CommandResults { get; } }
+
+    public interface ILogCarrierMappingsContext { List<CommandResult> CommandResults { get; } }
+
+    public interface ILogRoutesContext { List<CommandResult> CommandResults { get; } }
+
+    public interface ILogCommandsContext { List<CommandResult> CommandResults { get; } }
 
     public class SwitchLoggerConfig : ExtensionConfiguration
     {
