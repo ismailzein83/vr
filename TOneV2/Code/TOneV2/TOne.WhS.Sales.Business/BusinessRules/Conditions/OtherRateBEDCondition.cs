d0a2e2b81a94f36ace19ef4469685aefa402b616
Ali Sakr <VANRISE\ali.sakr>
2018-04-27 08:12:35 +0000

    adding otherratebedCondition
    git-tfs-id: [http://developmentvlan:8585/tfs/vanrise.collection]$/;C46722

--- TOneV2/Code/TOneV2/TOne.WhS.Sales.Business/BusinessRules/Conditions/OtherRateBEDCondition.cs
+++ TOneV2/Code/TOneV2/TOne.WhS.Sales.Business/BusinessRules/Conditions/OtherRateBEDCondition.cs
@@ -10,25 +10,25 @@ namespace TOne.WhS.Sales.Business
 {
     public class OtherRateBEDCondition : BusinessRuleCondition
     {
-      /*  public override bool ShouldValidate(IRuleTarget target)
+        public override bool ShouldValidate(IRuleTarget target)
         {
             return target is DataByZone;
         }
         public override bool Validate(IBusinessRuleConditionValidateContext context)
         {
-            var zoneData = context.Target as DataByZone;
-            if (zoneData.EED != null && zoneData.OtherRatesToChange.Count() > 0 && zoneData.OtherRatesToChange.First().BED < zoneData.EED)
-            {
-                context.Message = string.Format("Other rate BED on  '{0}' must be after pending normal rate BED", zoneData.ZoneName);
-                return false;
-            }
-            return true;
+            //var zoneData = context.Target as DataByZone;
+            //if (zoneData.NormalRateToChange != null && zoneData.OtherRatesToChange.Count() > 0 && zoneData.OtherRatesToChange.First().BED < zoneData.NormalRateToChange.BED)
+            //{
+            //    context.Message = string.Format("Other rate BED on  '{0}' must be after pending normal rate BED", zoneData.ZoneName);
+            //    return true;
+            //}
+            return false;
            
         }
         public override string GetMessage(IRuleTarget target)
         {
             throw new NotImplementedException();
-        }*/
+        }
     }
 }
 
