05e7b15e90e084cbec5c197537c6204cdd32d5b2
rabih.fashwal:2 <rabih.fashwal:2@vanrise.tfs.local>
2018-04-30 07:41:08 +0000

    Huawei Data Parser
    git-tfs-id: [http://developmentvlan:8585/tfs/vanrise.collection]$/;C46736

---
+++ Vanrise/Code/Vanrise.DataParser.MainExtensions/BinaryParsers/HuaweiParser/RecordParsers/HeaderRecordParser.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Vanrise.DataParser.Business;
+using Vanrise.DataParser.Entities;
+
+namespace Vanrise.DataParser.MainExtensions.BinaryParsers.HuaweiParser.RecordParsers
+{
+    public class HeaderRecordParser : BinaryRecordParserSettings
+    {
+        public override Guid ConfigId { get { return new Guid("69826C72-B45A-4165-B52A-12BDDE67723F"); } }
+        public int HeaderLengthPosition { get; set; }
+        public int HeaderBytesLength { get; set; }
+        public int FileLengthPosition { get; set; }
+        public int FileBytesLength { get; set; }
+
+        public BinaryRecordParser RecordParser { get; set; }
+
+        public override void Execute(IBinaryRecordParserContext context)
+        {
+
+            byte[] data = ((MemoryStream)context.RecordStream).ToArray();
+
+            byte[] headerLengthData = new byte[HeaderBytesLength];
+            Array.Copy(data, HeaderLengthPosition, headerLengthData, 0, HeaderBytesLength);
+
+            int headerLength = ParserHelper.GetInt(headerLengthData, 0, HeaderBytesLength);
+            int dataLength = data.Length - headerLength;
+            byte[] parserData = new byte[dataLength];
+            Array.Copy(data, headerLength, parserData, 0, data.Length - headerLength);
+
+            BinaryParserHelper.ExecuteRecordParser(RecordParser, new MemoryStream(parserData), context);
+        }
+    }
+}
---
+++ Vanrise/Code/Vanrise.DataParser.MainExtensions/BinaryParsers/HuaweiParser/RecordParsers/HuaweiRecordParser.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Vanrise.DataParser.Business;
+using Vanrise.DataParser.Entities;
+using Vanrise.Common;
+
+namespace Vanrise.DataParser.MainExtensions.BinaryParsers.HuaweiParser.RecordParsers
+{
+    public class HuaweiRecordParser : BinaryRecordParserSettings
+    {
+        public int RecordLengthPosition { get; set; }
+        public int RecordByteLength { get; set; }
+        public int RecordTypePosition { get; set; }
+        public int RecordTypeByteLength { get; set; }
+        public int HeaderLength { get; set; }
+
+        public Dictionary<string, BinaryRecordParser> SubRecordsParsersByRecordType { get; set; }
+        public override Guid ConfigId
+        {
+            get { return new Guid("1BED7CDA-1E98-48C0-8CA0-85EB6C4AB300"); }
+        }
+
+        public override void Execute(IBinaryRecordParserContext context)
+        {
+            while (context.RecordStream.Length - context.RecordStream.Position > 0)
+            {
+                string recordType = "";
+                int recordLength = 0;
+                BinaryParserHelper.ReadBlockFromStream(context.RecordStream, HeaderLength, (packageLength) =>
+                {
+                    byte[] lengthData = new byte[RecordByteLength];
+                    Array.Copy(packageLength.Value, RecordLengthPosition, lengthData, 0, RecordByteLength);
+                    recordLength = ParserHelper.GetInt(lengthData, 0, RecordByteLength);
+
+                    byte[] recordTypeData = new byte[RecordTypeByteLength];
+                    Array.Copy(packageLength.Value, RecordTypePosition, recordTypeData, 0, RecordTypeByteLength);
+                    recordType = ParserHelper.ByteArrayToString(recordTypeData, false);
+
+                }, true);
+
+
+                byte[] data = null;
+                int dataLength = recordLength - HeaderLength + RecordTypeByteLength + RecordByteLength;
+                data = new byte[dataLength];
+                context.RecordStream.Read(data, 0, dataLength);
+
+                BinaryRecordParser subRecordsParser = null;
+                if (this.SubRecordsParsersByRecordType != null)
+                    this.SubRecordsParsersByRecordType.TryGetValue(recordType, out subRecordsParser);
+
+                if (subRecordsParser != null)
+                    BinaryParserHelper.ExecuteRecordParser(subRecordsParser, new MemoryStream(data), context);
+            }
+        }
+
+    }
+}
