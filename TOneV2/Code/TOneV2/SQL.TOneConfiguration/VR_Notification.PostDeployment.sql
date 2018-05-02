51a381ba80479ec2e916e1e50ac403b568984459
Wissam Ajamy <VANRISE\wissam.ajamy>
2018-05-02 11:06:39 +0000

    Update SystemAction items
    git-tfs-id: [http://developmentvlan:8585/tfs/vanrise.collection]$/;C46756

--- TOneV2/Code/TOneV2/SQL.TOneConfiguration/VR_Notification.PostDeployment.sql
+++ TOneV2/Code/TOneV2/SQL.TOneConfiguration/VR_Notification.PostDeployment.sql
@@ -54,12 +54,48 @@ when not matched by target then
 ----------------------------------------------------------------------------------------------------
 END
 
---sec.[View]----------------------------------------------------------------------------------------BEGINset nocount on;
-;with cte_data([ID],[Name],[Title],[Url],[Module],[ActionNames],[Audience],[Content],[Settings],[Type],[Rank])as (select * from (values--//////////////////////////////////////////////////////////////////////////////////////////////////('05234A49-DFA3-44C3-A552-80E6C793A67A','Action Rule Types','Action Rule Type','#/view/VR_Notification/Views/VRAlertRuleType/VRAlertRuleTypeManagement'	,'D018C0CD-F15F-486D-80C3-F9B87C3F47B8','VR_Notification/VRAlertRuleType/GetFilteredVRAlertRuleTypes',null,null,null,'372ED3CB-4B7B-4464-9ABF-59CD7B08BD23',45),('FD44ED02-9708-4F38-A237-E6C0BF35B634','Alert Levels','Alert Levels','#/view/VR_Notification/Views/VRAlertLevel/VRAlertLevelManagement'				,'D018C0CD-F15F-486D-80C3-F9B87C3F47B8','VR_Notification/VRAlertRuleType/GetFilteredVRAlertRuleTypes',NULL,NULL,NULL,'372ED3CB-4B7B-4464-9ABF-59CD7B08BD23',47),('FF5E429A-CFF0-4095-8AD0-987FD3CAD502','Action Rules','Action Rule','#/view/VR_Notification/Views/VRAlertRule/VRAlertRuleManagement'					,'1C7569FA-43C9-4853-AE4C-1152746A34FD',null,null,null,'{"$type":"Vanrise.Notification.Business.VRAlertRuleViewSettings, Vanrise.Notification.Business"}','372ED3CB-4B7B-4464-9ABF-59CD7B08BD23',100)--\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\)c([ID],[Name],[Title],[Url],[Module],[ActionNames],[Audience],[Content],[Settings],[Type],[Rank]))merge	[sec].[View] as tusing	cte_data as son		1=1 and t.[ID] = s.[ID]when matched then	update set	[Name] = s.[Name],[Title] = s.[Title],[Url] = s.[Url],[Module] = s.[Module],[ActionNames] = s.[ActionNames],[Audience] = s.[Audience],[Content] = s.[Content],[Settings] = s.[Settings],[Type] = s.[Type],[Rank] = s.[Rank]when not matched by target then	insert([ID],[Name],[Title],[Url],[Module],[ActionNames],[Audience],[Content],[Settings],[Type],[Rank])	values(s.[ID],s.[Name],s.[Title],s.[Url],s.[Module],s.[ActionNames],s.[Audience],s.[Content],s.[Settings],s.[Type],s.[Rank]);----------------------------------------------------------------------------------------------------END
+--sec.[View]----------------------------------------------------------------------------------------
+BEGIN
+set nocount on;
+;with cte_data([ID],[Name],[Title],[Url],[Module],[ActionNames],[Audience],[Content],[Settings],[Type],[Rank])
+as (select * from (values
+--//////////////////////////////////////////////////////////////////////////////////////////////////
+('05234A49-DFA3-44C3-A552-80E6C793A67A','Action Rule Types','Action Rule Type','#/view/VR_Notification/Views/VRAlertRuleType/VRAlertRuleTypeManagement'	,'D018C0CD-F15F-486D-80C3-F9B87C3F47B8','VR_Notification/VRAlertRuleType/GetFilteredVRAlertRuleTypes',null,null,null,'372ED3CB-4B7B-4464-9ABF-59CD7B08BD23',45),
+('FD44ED02-9708-4F38-A237-E6C0BF35B634','Alert Levels','Alert Levels','#/view/VR_Notification/Views/VRAlertLevel/VRAlertLevelManagement'				,'D018C0CD-F15F-486D-80C3-F9B87C3F47B8','VR_Notification/VRAlertRuleType/GetFilteredVRAlertRuleTypes',NULL,NULL,NULL,'372ED3CB-4B7B-4464-9ABF-59CD7B08BD23',47),
+
+('FF5E429A-CFF0-4095-8AD0-987FD3CAD502','Action Rules','Action Rule','#/view/VR_Notification/Views/VRAlertRule/VRAlertRuleManagement'					,'1C7569FA-43C9-4853-AE4C-1152746A34FD',null,null,null,'{"$type":"Vanrise.Notification.Business.VRAlertRuleViewSettings, Vanrise.Notification.Business"}','372ED3CB-4B7B-4464-9ABF-59CD7B08BD23',100)
+--\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\
+)c([ID],[Name],[Title],[Url],[Module],[ActionNames],[Audience],[Content],[Settings],[Type],[Rank]))
+merge	[sec].[View] as t
+using	cte_data as s
+on		1=1 and t.[ID] = s.[ID]
+when matched then
+	update set
+	[Name] = s.[Name],[Title] = s.[Title],[Url] = s.[Url],[Module] = s.[Module],[ActionNames] = s.[ActionNames],[Audience] = s.[Audience],[Content] = s.[Content],[Settings] = s.[Settings],[Type] = s.[Type],[Rank] = s.[Rank]
+when not matched by target then
+	insert([ID],[Name],[Title],[Url],[Module],[ActionNames],[Audience],[Content],[Settings],[Type],[Rank])
+	values(s.[ID],s.[Name],s.[Title],s.[Url],s.[Module],s.[ActionNames],s.[Audience],s.[Content],s.[Settings],s.[Type],s.[Rank]);
+----------------------------------------------------------------------------------------------------
+END
 
 --[sec].[BusinessEntity]-------------------6001 to 6300-------------------------------------------------------
 BEGIN
-set nocount on;;with cte_data([Id],[Name],[Title],[ModuleId],[BreakInheritance],[PermissionOptions])as (select * from (values--//////////////////////////////////////////////////////////////////////////////////////////////////('FE898B2B-4D50-4926-A6EF-207EDBACC21A','BusinessProcess_BP_Notifications','Notifications'					,'B6B8F582-4759-43FB-9220-AA7662C366EA',0,'["View", "StartInstance", "ScheduleTask"]')--\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\)c([Id],[Name],[Title],[ModuleId],[BreakInheritance],[PermissionOptions]))merge	[sec].[BusinessEntity] as tusing	cte_data as son		1=1 and t.[Id] = s.[Id]when matched then	update set	[Name] = s.[Name],[Title] = s.[Title],[ModuleId] = s.[ModuleId],[BreakInheritance] = s.[BreakInheritance],[PermissionOptions] = s.[PermissionOptions]when not matched by target then	insert([Id],[Name],[Title],[ModuleId],[BreakInheritance],[PermissionOptions])	values(s.[Id],s.[Name],s.[Title],s.[ModuleId],s.[BreakInheritance],s.[PermissionOptions]);
+set nocount on;
+;with cte_data([Id],[Name],[Title],[ModuleId],[BreakInheritance],[PermissionOptions])
+as (select * from (values
+--//////////////////////////////////////////////////////////////////////////////////////////////////
+('FE898B2B-4D50-4926-A6EF-207EDBACC21A','BusinessProcess_BP_Notifications','Notifications'					,'B6B8F582-4759-43FB-9220-AA7662C366EA',0,'["View", "StartInstance", "ScheduleTask"]')
+--\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\
+)c([Id],[Name],[Title],[ModuleId],[BreakInheritance],[PermissionOptions]))
+merge	[sec].[BusinessEntity] as t
+using	cte_data as s
+on		1=1 and t.[Id] = s.[Id]
+when matched then
+	update set
+	[Name] = s.[Name],[Title] = s.[Title],[ModuleId] = s.[ModuleId],[BreakInheritance] = s.[BreakInheritance],[PermissionOptions] = s.[PermissionOptions]
+when not matched by target then
+	insert([Id],[Name],[Title],[ModuleId],[BreakInheritance],[PermissionOptions])
+	values(s.[Id],s.[Name],s.[Title],s.[ModuleId],s.[BreakInheritance],s.[PermissionOptions]);
 --------------------------------------------------------------------------------------------------------------
 END
 
@@ -83,7 +119,40 @@ when not matched by target then
 	insert([ID],[Name],[Title],[FQTN],[Config])
 	values(s.[ID],s.[Name],s.[Title],s.[FQTN],s.[Config]);
 ----------------------------------------------------------------------------------------------------
-END--[sec].[SystemAction]------------------------------------------------------------------------------BEGINset nocount on;;with cte_data([Name],[RequiredPermissions])as (select * from (values--//////////////////////////////////////////////////////////////////////////////////////////////////('VR_Notification/VRAlertRuleType/GetFilteredVRAlertRuleTypes','VR_SystemConfiguration: View'),('VR_Notification/VRAlertRuleType/GetVRAlertRuleType',null),('VR_Notification/VRAlertRuleType/AddVRAlertRuleType','VR_SystemConfiguration: Add'),('VR_Notification/VRAlertRuleType/UpdateVRAlertRuleType','VR_SystemConfiguration: Edit'),('VR_Notification/VRAlertRuleType/GetVRAlertRuleTypeSettingsExtensionConfigs',null),('VR_Notification/VRAlertRuleType/GetVRAlertRuleTypesInfo',null),('VR_Notification/VRAlertRule/GetFilteredVRAlertRules','VR_AccountBalance_Notification: View Rules'),('VR_Notification/VRAlertRule/GetVRAlertRule',null),('VR_Notification/VRAlertRule/AddVRAlertRule','VR_AccountBalance_Notification: Add Rules'),('VR_Notification/VRAlertRule/UpdateVRAlertRule','VR_AccountBalance_Notification: Edit Rules')--\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\)c([Name],[RequiredPermissions]))merge	[sec].[SystemAction] as tusing	cte_data as son		1=1 and t.[Name] = s.[Name]when matched then	update set	[RequiredPermissions] = s.[RequiredPermissions]when not matched by target then	insert([Name],[RequiredPermissions])	values(s.[Name],s.[RequiredPermissions]);	----------------------------------------------------------------------------------------------------END--[genericdata].[BusinessEntityDefinition]----------------------------------------------------------
+END
+
+--[sec].[SystemAction]------------------------------------------------------------------------------
+BEGIN
+set nocount on;
+;with cte_data([Name],[RequiredPermissions])
+as (select * from (values
+--//////////////////////////////////////////////////////////////////////////////////////////////////
+('VR_Notification/VRAlertRuleType/GetFilteredVRAlertRuleTypes','VR_SystemConfiguration: View'),
+('VR_Notification/VRAlertRuleType/GetVRAlertRuleType',null),
+('VR_Notification/VRAlertRuleType/AddVRAlertRuleType','VR_SystemConfiguration: Add'),
+('VR_Notification/VRAlertRuleType/UpdateVRAlertRuleType','VR_SystemConfiguration: Edit'),
+('VR_Notification/VRAlertRuleType/GetVRAlertRuleTypeSettingsExtensionConfigs',null),
+('VR_Notification/VRAlertRuleType/GetVRAlertRuleTypesInfo',null),
+
+('VR_Notification/VRAlertRule/GetFilteredVRAlertRules',null),
+('VR_Notification/VRAlertRule/GetVRAlertRule',null),
+('VR_Notification/VRAlertRule/AddVRAlertRule',null),
+('VR_Notification/VRAlertRule/UpdateVRAlertRule',null)
+--\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\
+)c([Name],[RequiredPermissions]))
+merge	[sec].[SystemAction] as t
+using	cte_data as s
+on		1=1 and t.[Name] = s.[Name]
+when matched then
+	update set
+	[RequiredPermissions] = s.[RequiredPermissions]
+when not matched by target then
+	insert([Name],[RequiredPermissions])
+	values(s.[Name],s.[RequiredPermissions]);
+	----------------------------------------------------------------------------------------------------
+END
+
+--[genericdata].[BusinessEntityDefinition]----------------------------------------------------------
 ----------------------------------------------------------------------------------------------------
 set nocount on;
 ;with cte_data([ID],[Name],[Title],[Settings])
@@ -101,7 +170,8 @@ when matched then
 when not matched by target then
 	insert([ID],[Name],[Title],[Settings])
 	values(s.[ID],s.[Name],s.[Title],s.[Settings]);
---[VRNotification].[VRAlertLevel]-------------------------------------------------------------------
+
+--[VRNotification].[VRAlertLevel]-------------------------------------------------------------------
 ----------------------------------------------------------------------------------------------------
 set nocount on;
 ;with cte_data([ID],[Name],[BusinessEntityDefinitionID],[Settings])
@@ -122,4 +192,29 @@ when matched then
 	[Name] = s.[Name],[BusinessEntityDefinitionID] = s.[BusinessEntityDefinitionID],[Settings] = s.[Settings]
 when not matched by target then
 	insert([ID],[Name],[BusinessEntityDefinitionID],[Settings])
-	values(s.[ID],s.[Name],s.[BusinessEntityDefinitionID],s.[Settings]);--[logging].[LoggableEntity]----------------------------------------------------------------------------------------------------------------------------------------------------------------------------set nocount on;;with cte_data([ID],[UniqueName],[Settings])as (select * from (values--//////////////////////////////////////////////////////////////////////////////////////////////////('5C1F441E-3D2B-4794-A7EC-01DC5E277BD2','VR_Notification_AlertRuleType','{"$type":"Vanrise.Entities.VRLoggableEntitySettings, Vanrise.Entities","ViewHistoryItemClientActionName":"VR_Notification_AlertRuleType_ViewHistoryItem"}'),('2638419F-506C-44F9-89FE-1DB7C9F246B3','VR_Notification_AlertRule_08427b54-b63a-4b75-b42b-56e40425707a','{"$type":"Vanrise.Entities.VRLoggableEntitySettings, Vanrise.Entities","ViewHistoryItemClientActionName":"VR_Notification_AlertRule_ViewHistoryItem"}'),('861CF8F4-DB3F-4369-8F65-02811F68BCAC','VR_Common_ComponentType_fdd73530-067f-4160-ab71-7852303c785c','{"$type":"Vanrise.Entities.VRLoggableEntitySettings, Vanrise.Entities","ViewHistoryItemClientActionName":"VR_Common_ComponentType__ViewHistoryItem"}'),('1ED6247E-F252-4B3D-B5FE-9A14C204F51B','VR_Common_ComponentType_d96f17c8-29d7-4c0c-88dc-9d5fbca2178f','{"$type":"Vanrise.Entities.VRLoggableEntitySettings, Vanrise.Entities","ViewHistoryItemClientActionName":"VR_Common_ComponentType__ViewHistoryItem"}'),('937F3844-B545-4C1F-AD8D-D741D59E3487','VR_Notification_AlertLevel','{"$type":"Vanrise.Entities.VRLoggableEntitySettings, Vanrise.Entities","ViewHistoryItemClientActionName":"VR_Notification_AlertLevel_ViewHistoryItem"}')--\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\)c([ID],[UniqueName],[Settings]))merge	[logging].[LoggableEntity] as tusing	cte_data as son		1=1 and t.[ID] = s.[ID]when matched then	update set	[UniqueName] = s.[UniqueName],[Settings] = s.[Settings]when not matched by target then	insert([ID],[UniqueName],[Settings])	values(s.[ID],s.[UniqueName],s.[Settings]);
+	values(s.[ID],s.[Name],s.[BusinessEntityDefinitionID],s.[Settings]);
+
+
+--[logging].[LoggableEntity]------------------------------------------------------------------------
+----------------------------------------------------------------------------------------------------
+set nocount on;
+;with cte_data([ID],[UniqueName],[Settings])
+as (select * from (values
+--//////////////////////////////////////////////////////////////////////////////////////////////////
+('5C1F441E-3D2B-4794-A7EC-01DC5E277BD2','VR_Notification_AlertRuleType','{"$type":"Vanrise.Entities.VRLoggableEntitySettings, Vanrise.Entities","ViewHistoryItemClientActionName":"VR_Notification_AlertRuleType_ViewHistoryItem"}'),
+
+('2638419F-506C-44F9-89FE-1DB7C9F246B3','VR_Notification_AlertRule_08427b54-b63a-4b75-b42b-56e40425707a','{"$type":"Vanrise.Entities.VRLoggableEntitySettings, Vanrise.Entities","ViewHistoryItemClientActionName":"VR_Notification_AlertRule_ViewHistoryItem"}'),
+('861CF8F4-DB3F-4369-8F65-02811F68BCAC','VR_Common_ComponentType_fdd73530-067f-4160-ab71-7852303c785c','{"$type":"Vanrise.Entities.VRLoggableEntitySettings, Vanrise.Entities","ViewHistoryItemClientActionName":"VR_Common_ComponentType__ViewHistoryItem"}'),
+('1ED6247E-F252-4B3D-B5FE-9A14C204F51B','VR_Common_ComponentType_d96f17c8-29d7-4c0c-88dc-9d5fbca2178f','{"$type":"Vanrise.Entities.VRLoggableEntitySettings, Vanrise.Entities","ViewHistoryItemClientActionName":"VR_Common_ComponentType__ViewHistoryItem"}'),
+('937F3844-B545-4C1F-AD8D-D741D59E3487','VR_Notification_AlertLevel','{"$type":"Vanrise.Entities.VRLoggableEntitySettings, Vanrise.Entities","ViewHistoryItemClientActionName":"VR_Notification_AlertLevel_ViewHistoryItem"}')
+--\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\
+)c([ID],[UniqueName],[Settings]))
+merge	[logging].[LoggableEntity] as t
+using	cte_data as s
+on		1=1 and t.[ID] = s.[ID]
+when matched then
+	update set
+	[UniqueName] = s.[UniqueName],[Settings] = s.[Settings]
+when not matched by target then
+	insert([ID],[UniqueName],[Settings])
+	values(s.[ID],s.[UniqueName],s.[Settings]);
