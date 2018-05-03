45d43546106a6dda9bd3d9ae5f7fa564bddf5093
Samer Haidar <VANRISE\samer.haidar>
2018-05-03 10:29:09 +0000

    adding Classifications
    git-tfs-id: [http://developmentvlan:8585/tfs/vanrise.collection]$/;C46783

--- Retail/Code/Retail.BusinessEntity.Business/Extensions/SubscriberAccountBalanceSetting.cs
+++ Retail/Code/Retail.BusinessEntity.Business/Extensions/SubscriberAccountBalanceSetting.cs
@@ -11,6 +11,7 @@ namespace Retail.BusinessEntity.Business
     public class SubscriberAccountBalanceSetting : AccountTypeExtendedSettings
     {
         public Guid AccountBEDefinitionId { get; set; }
+        public List<AccountBalanceClassification> Classifications { get; set; }
 
         public override Guid ConfigId
         {
@@ -31,6 +32,10 @@ namespace Retail.BusinessEntity.Business
         {
             return new RetailAccountBalanceRuleTargetType { AccountBEDefinitionId = this.AccountBEDefinitionId };
         }
+    }
+    public class AccountBalanceClassification
+    {
+        public string AccountClassification { get; set; }
     }
 
     public class RetailAccountBalanceRuleTargetType : VRActionTargetType
