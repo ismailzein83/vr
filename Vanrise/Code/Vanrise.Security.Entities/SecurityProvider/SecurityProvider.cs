115f0d35747ab245f9e42858ab7a9347e7a47a5a
Ismail Zein <VANRISE\ismail.zein>
2018-05-10 15:03:56 +0000

    added SecurityProvider skeleton
    git-tfs-id: [http://developmentvlan:8585/tfs/vanrise.collection]$/;C46907

---
+++ Vanrise/Code/Vanrise.Security.Entities/SecurityProvider/SecurityProvider.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Vanrise.Security.Entities
+{
+    public class SecurityProvider
+    {
+        public Guid SecurityProviderId { get; set; }
+
+        public string Name { get; set; }
+
+        public Guid DefinitionId { get; set; }
+
+        public SecurityProviderSettings Settings { get; set; }
+    }
+
+    public class SecurityProviderSettings
+    {
+        public SecurityProviderExtendedSettings ExtendedSettings { get; set; }
+    }
+
+    public abstract class SecurityProviderExtendedSettings
+    {
+        public virtual string FindUserEditor { get; set; }
+
+        public virtual string AuthenticateUserEditor { get; set; }
+
+        public abstract bool Authenticate(ISecurityProviderAuthenticateContext context);
+    }
+
+    public interface ISecurityProviderAuthenticateContext
+    {
+        SecurityProviderAuthenticationPayload Payload { get; }
+
+        User AuthenticatedUser { set; }
+
+        string FailureMessage { get; }
+    }
+
+    public abstract class SecurityProviderAuthenticationPayload
+    {
+
+    }
+
+    public class EmailPasswordSecurityProviderAuthenticationPayload : SecurityProviderAuthenticationPayload
+    {
+        public string Email { get; set; }
+
+        public string Password { get; set; }
+    }
+}
---
+++ Vanrise/Code/Vanrise.Security.Entities/SecurityProvider/SecurityProviderDefinitionSettings.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Vanrise.Security.Entities
+{
+    public class SecurityProviderDefinitionSettings : Vanrise.Entities.VRComponentTypeSettings
+    {
+        public override Guid VRComponentTypeConfigId
+        {
+            get { return new Guid("3D3D8A9E-B8A7-4C74-8AF1-FFAAC7A80900"); }
+        }
+
+        public SecurityProviderDefinitionExtendedSettings ExtendedSettings { get; set; }
+    }
+
+    public abstract class SecurityProviderDefinitionExtendedSettings
+    {
+        public abstract Guid ConfigId { get; }
+
+        public virtual string RuntimeEditor { get; set; }
+    }
+}
