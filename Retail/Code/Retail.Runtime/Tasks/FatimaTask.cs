12c90f9cbf5dc6c85924461907b72530cf941854
Fatima Al Heck <VANRISE\fatima.alheck>
2018-05-11 12:48:20 +0000

    Adding Fatima Task
    git-tfs-id: [http://developmentvlan:8585/tfs/vanrise.collection]$/;C46925

---
+++ Retail/Code/Retail.Runtime/Tasks/FatimaTask.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Vanrise.BusinessProcess;
+using Vanrise.Queueing;
+using Vanrise.Runtime;
+
+namespace Retail.Runtime.Tasks
+{
+    public class FatimaTask : ITask
+    {
+        public void Execute()
+        {
+            var runtimeServices = new List<Vanrise.Runtime.Entities.RuntimeService>();
+            BusinessProcessService bpService = new BusinessProcessService() { Interval = new TimeSpan(0, 0, 2) };
+            runtimeServices.Add(bpService);
+            BPRegulatorRuntimeService bpRegulatorService = new BPRegulatorRuntimeService() { Interval = new TimeSpan(0, 0, 2) };
+            QueueRegulatorRuntimeService queueRegulatorService = new QueueRegulatorRuntimeService() { Interval = new TimeSpan(0, 0, 2) };
+            QueueActivationRuntimeService queueActivationRuntimeService = new QueueActivationRuntimeService { Interval = new TimeSpan(0, 0, 2) };
+            QueueActivationService queueActivationService = new QueueActivationService() { Interval = new TimeSpan(0, 0, 2) };
+            SchedulerService schedulerService = new SchedulerService() { Interval = new TimeSpan(0, 0, 2) };
+            Vanrise.Integration.Business.DataSourceRuntimeService dsRuntimeService = new Vanrise.Integration.Business.DataSourceRuntimeService { Interval = new TimeSpan(0, 0, 2) };
+
+            runtimeServices.Add(queueActivationService);
+            runtimeServices.Add(schedulerService);
+            runtimeServices.Add(dsRuntimeService);
+            runtimeServices.Add(queueActivationRuntimeService);
+            runtimeServices.Add(bpRegulatorService);
+            runtimeServices.Add(queueRegulatorService);
+
+            RuntimeHost host = new RuntimeHost(runtimeServices);
+            host.Start();
+
+        }
+    }
+}
