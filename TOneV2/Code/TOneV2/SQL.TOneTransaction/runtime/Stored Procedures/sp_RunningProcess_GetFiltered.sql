6ef8f5466a172cbeaaf271e11223a4a34d4e1f38
Wissam Ajamy <VANRISE\wissam.ajamy>
2018-05-14 12:43:49 +0000

    Update structure
    git-tfs-id: [http://developmentvlan:8585/tfs/vanrise.collection]$/;C46951

---
+++ TOneV2/Code/TOneV2/SQL.TOneTransaction/runtime/Stored Procedures/sp_RunningProcess_GetFiltered.sql
@@ -0,0 +1,15 @@
+﻿
+Create PROCEDURE [runtime].[sp_RunningProcess_GetFiltered]
+@RuntimeNodeInstanceID uniqueidentifier
+AS
+BEGIN
+           
+     SELECT [ID]
+	  ,OSProcessID
+	  ,[RuntimeNodeID]
+	  ,RuntimeNodeInstanceID
+      ,[StartedTime]
+      ,AdditionalInfo
+	 FROM [runtime].[RunningProcess] WITH(NOLOCK) 
+	 Where RuntimeNodeInstanceID= @RuntimeNodeInstanceID
+END
