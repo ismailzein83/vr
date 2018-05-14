c691e640879098e1ea4000c87c86a28a98c37812
Ismail Zein <VANRISE\ismail.zein>
2018-05-14 12:50:06 +0000

    added DBReplication
    git-tfs-id: [http://developmentvlan:8585/tfs/vanrise.collection]$/;C46952

---
+++ Vanrise/Code/Vanrise.Entities/DBReplication/DBReplicationDefinitionSettings.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Vanrise.Entities
+{
+    public class DBReplicationDefinitionSettings : VRComponentTypeSettings
+    {
+        public override Guid VRComponentTypeConfigId
+        {
+            get { return new Guid("BB07A3B5-E519-4A6C-B4C6-695069BBB64A"); }
+        }
+
+        public Dictionary<Guid, DBReplicationSourceDBConnectionDefinition> SourceDBConnectionDefinitions { get; set; }
+
+        public Dictionary<Guid, DBReplicationTargetDBConnectionDefinition> TargetDBConnectionDefinitions { get; set; }
+
+        public List<DBReplicationTableDefinitionSettings> Tables { get; set; }
+    }
+
+    public class DBReplicationSourceDBConnectionDefinition
+    {
+        public string Name { get; set; }
+    }
+
+    public class DBReplicationTargetDBConnectionDefinition
+    {
+        public string Name { get; set; }
+
+        public string ConnectionStringName { get; set; }
+
+        public string ConnectionStringAppSettingKey { get; set; }
+    }
+
+    public class DBReplicationTableDefinitionSettings
+    {
+        public string TableName { get; set; }
+
+        public Guid SourceDBConnectionDefinitionId { get; set; }
+
+        public Guid TargetDBConnectionDefinitionId { get; set; }
+
+        public string FilterDateTimeColumn { get; set; }
+    }
+}
---
+++ Vanrise/Code/Vanrise.Entities/DBReplication/DBReplicationSettings.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Vanrise.Entities
+{
+    public class DBReplicationSettings
+    {
+        public List<DBReplicationDBConnection> DBConnections { get; set; }
+    }
+
+    public class DBReplicationDBConnection
+    {
+        public string LinkedServerName { get; set; }
+
+        public List<DBReplicationDBConnectionSourceDefinition> SourceConnectionDefinitions { get; set; }
+    }
+
+    public class DBReplicationDBConnectionSourceDefinition
+    {
+        public Guid SourceDBConnectionDefinitionId { get; set; }
+    }
+}
