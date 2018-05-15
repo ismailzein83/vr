fe7f6b88ae369e30276a3bdff54211e646789310
Mortada Issa <VANRISE\mortada.issa>
2018-05-15 06:07:40 +0000

    Adding Business data provider for Swap Deal Progress
    git-tfs-id: [http://developmentvlan:8585/tfs/vanrise.collection]$/;C46955

---
+++ TOneV2/Code/TOneV2/TOne.WhS.Deal.Business/Extensions/DealBusinessObjectDataProviderSettings.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Vanrise.GenericData.Business;
+
+namespace TOne.WhS.Deal.Business
+{
+    public class DealBusinessObjectDataProviderSettings : BusinessObjectDataProviderExtendedSettings
+    {
+        public override Guid ConfigId
+        {
+            get { return new Guid("7b5d42b4-b05c-4f54-9683-a73dd0cebd68"); }
+        }
+
+        public override bool DoesSupportFilterOnAllFields
+        {
+            get { return false; }
+        }
+
+        public override void LoadRecords(IBusinessObjectDataProviderLoadRecordsContext context)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ TOneV2/Code/TOneV2/TOne.WhS.Deal.Web/WhS_Deal/Directives/Extensions/SwapDealProgress/vr-whs-swapdeal-businessobject-dataprovidersettings.js
@@ -0,0 +1,48 @@
+﻿"use strict";
+app.directive("vrWhsSwapdealBusinessobjectDataprovidersettings", ["UtilsService", "VRNotificationService", "VRUIUtilsService",
+function (UtilsService, VRNotificationService, VRUIUtilsService) {
+    var directiveDefinitionObject = {
+        restrict: "E",
+        scope: {
+            onReady: "="
+        },
+        controller: function ($scope, $element, $attrs) {
+            var ctrl = this;
+            var businessObject = new BusinessObject($scope, ctrl, $attrs);
+            businessObject.initializeController();
+        },
+        controllerAs: "ctrl",
+        bindToController: true,
+        templateUrl: '/Client/Modules/WhS_Deal/Directives/Extensions/SwapDealAnalysis/Outbound/SwapDealProgress/Templates/SwapDealBusinessObjectDataProviderSettingsTemplate.html'
+    };
+
+
+    function BusinessObject($scope, ctrl, $attrs) {
+        this.initializeController = initializeController;
+
+        function initializeController() {
+
+            $scope.scopeModel = {};
+            defineAPI();
+        }
+
+        function defineAPI() {
+            var api = {};
+
+            api.load = function (payload) {
+            };
+
+            api.getData = function () {
+                return {
+                    $type: "TOne.WhS.Deal.Business.DealBusinessObjectDataProviderSettings, TOne.WhS.Deal.Business"
+                };
+            };
+
+            if (ctrl.onReady != null)
+                ctrl.onReady(api);
+        }
+    }
+
+    return directiveDefinitionObject;
+}
+]);
