6f5ae05986e88aec7e6dd7c9040d46cc5b82bb1b
Fatima Al Heck <VANRISE\fatima.alheck>
2018-05-18 12:28:17 +0000

    Implementing Effective Period Directive
    git-tfs-id: [http://developmentvlan:8585/tfs/vanrise.collection]$/;C47043

---
+++ Vanrise/Code/Vanrise.Common.Web/Common/Directives/VREffectivePeriod/vr-common-effectiveperiod.js
@@ -0,0 +1,65 @@
+﻿(function (app) {
+
+    'use strict';
+
+    EffectivePeriodDirective.$inject = ['UtilsService', 'VRUIUtilsService'];
+
+    function EffectivePeriodDirective(UtilsService, VRUIUtilsService) {
+        return {
+            restrict: "E",
+            scope: {
+                onReady: "="
+            },
+            controller: function ($scope, $element, $attrs) {
+                var ctrl = this;
+                var ctor = new EffectivePeriod($scope, ctrl, $attrs);
+                ctor.initializeController();
+            },
+            controllerAs: "ctrl",
+            bindToController: true,
+
+            templateUrl: "/Client/Modules/Common/Directives/VREffectivePeriod/Templates/EffectivePeriodDirectiveTemplate.html"
+        };
+
+        function EffectivePeriod($scope, ctrl, $attrs) {
+            this.initializeController = initializeController;
+            
+            function initializeController() {
+
+                $scope.scopeModel.validateDates = function (date) {
+                    return UtilsService.validateDates($scope.scopeModel.beginEffectiveDate, $scope.scopeModel.endEffectiveDate);
+                };
+            
+                $scope.scopeModel.validateEEDDate = function (date) {
+                    return UtilsService.validateDates($scope.scopeModel.beginEffectiveDate, $scope.scopeModel.endEffectiveDate);
+                };
+
+                defineAPI();
+            }
+
+            function defineAPI() {
+                var api = {};
+
+                api.load = function (payload) {
+                    
+                    if (payload != undefined) {
+                        $scope.scopeModel.beginEffectiveDate = payload.BED;
+                        $scope.scopeModel.endEffectiveDate = payload.EED;
+                    }
+                };
+
+                api.getData = function () {
+                    return {
+                        BED: $scope.scopeModel.beginEffectiveDate,
+                        EED: $scope.scopeModel.endEffectiveDate
+                    };
+                };
+
+                if (ctrl.onReady != null) {
+                    ctrl.onReady(api);
+                }
+            }
+        }
+    }
+    app.directive('vrCommonEffectiveperiod', EffectivePeriodDirective);
+})(app);
---
+++ Vanrise/Code/Vanrise.GenericData.Web/VR_GenericData/Directives/GenericBusinessEntity/Runtime/Editor/vr-genericdata-genericbe-effectiveperiod-editor.js
@@ -0,0 +1,84 @@
+﻿(function (app) {
+
+    'use strict';
+
+    EffectivePeriodEditorDirective.$inject = ['UtilsService', 'VRUIUtilsService'];
+
+    function EffectivePeriodEditorDirective(UtilsService, VRUIUtilsService) {
+        return {
+            restrict: "E",
+            scope: {
+                onReady: "=",
+                label: '@'
+            },
+            controller: function ($scope, $element, $attrs) {
+                var ctrl = this;
+                var ctor = new EffectivePeriodEditor($scope, ctrl, $attrs);
+                ctor.initializeController();
+            },
+            controllerAs: "ctrl",
+            bindToController: true,
+
+            templateUrl: "/Client/Modules/VR_GenericData/Directives/GenericBusinessEntity/Runtime/Editor/Templates/EffectivePeriodEditorTemplate.html"
+        };
+
+        function EffectivePeriodEditor($scope, ctrl, $attrs) {
+            this.initializeController = initializeController;
+
+            var effectivePeriodAPI;
+            var effectivePeriodDirectiveReadyDeferred = UtilsService.createPromiseDeferred();
+
+            function initializeController() {
+                $scope.scopeModel = {};
+
+                $scope.scopeModel.onEffectivePeriodReady = function (api) {
+                    effectivePeriodAPI = api;
+                    effectivePeriodDirectiveReadyDeferred.resolve();
+                };
+
+                defineAPI();
+            }
+
+            function defineAPI() {
+                var api = {};
+
+                api.load = function (payload) {
+                        var promises = [];
+
+                        promises.push(loadEffectivePeriodDirective());
+
+                        function loadEffectivePeriodDirective() {
+                            var efectivePeriodDirectiveLoadDeferred = UtilsService.createPromiseDeferred();
+
+                            effectivePeriodDirectiveReadyDeferred.promise.then(function () {
+                                var effectivePeriodPayload;
+                                if (payload != undefined) {
+                                    effectivePeriodPayload = {
+                                        BED: payload.BED,
+                                        EED: payload.EED
+                                    };
+                                }
+                                VRUIUtilsService.callDirectiveLoad(effectivePeriodAPI, effectivePeriodPayload, efectivePeriodDirectiveLoadDeferred);
+                            });
+                            return efectivePeriodDirectiveLoadDeferred.promise;
+                        }
+
+                        return UtilsService.waitMultiplePromises(promises);
+                };
+
+                api.setData = function (obj) {
+                    var effectivePeriod = effectivePeriodAPI.getData();
+                    if (effectivePeriod != undefined) {
+                        obj.BED = effectivePeriod.BED;
+                        obj.EED = effectivePeriod.EED;
+                    }
+                };
+
+                if (ctrl.onReady != null) {
+                    ctrl.onReady(api);
+                }
+            }
+        }
+    }
+    app.directive('vrGenericdataGenericbeEffectiveperiodEditor', EffectivePeriodEditorDirective);
+})(app);
