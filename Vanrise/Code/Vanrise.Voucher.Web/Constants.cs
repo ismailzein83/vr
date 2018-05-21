f0b0ebfd3c81aa261cca0cd96ba70062d37a8a02
Samer Haidar <VANRISE\samer.haidar>
2018-05-21 13:21:49 +0000

    Adding Voucher Project
    git-tfs-id: [http://developmentvlan:8585/tfs/vanrise.collection]$/;C47074

---
+++ Vanrise/Code/Vanrise.Voucher.Web/Constants.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Vanrise.Voucher.Web
+{
+    public class Constants
+    {
+        public const string ROUTE_PREFIX = "api/VR_Voucher/";
+    }
+}
