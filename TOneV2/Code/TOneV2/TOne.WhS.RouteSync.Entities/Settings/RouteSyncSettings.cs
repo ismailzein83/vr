3b8e379aa0c9a70e5f6540e55521a9a9a1ac8729
Ali Atoui <VANRISE\ali.atoui>
2018-05-21 07:49:23 +0000

    Editing RouteSyncProcess
    git-tfs-id: [http://developmentvlan:8585/tfs/vanrise.collection]$/;C47054

--- TOneV2/Code/TOneV2/TOne.WhS.RouteSync.Entities/Settings/RouteSyncSettings.cs
+++ TOneV2/Code/TOneV2/TOne.WhS.RouteSync.Entities/Settings/RouteSyncSettings.cs
@@ -6,28 +6,34 @@ using System.Threading.Tasks;
 
 namespace TOne.WhS.RouteSync.Entities
 {
-	public class RouteSyncSettings : Vanrise.Entities.SettingData
-	{
-		public const string SETTING_TYPE = "WhS_RouteSync_Settings";
+    public class RouteSyncSettings : Vanrise.Entities.SettingData
+    {
+        public const string SETTING_TYPE = "WhS_RouteSync_Settings";
 
-		public RouteSyncProcess RouteSyncProcess { get; set; }
+        public RouteSyncProcess RouteSyncProcess { get; set; }
 
-		public Dictionary<Guid, RouteSynchronizerSwitchSettings> SwitchSettingsByConfigId { get; set; }
+        public Dictionary<Guid, RouteSynchronizerSwitchSettings> SwitchSettingsByConfigId { get; set; }
 
-	}
+    }
 
-	public class RouteSyncProcess
-	{
-		public int RouteBatchSize { get; set; }
+    public class RouteSyncProcess
+    {
+        public int RouteBatchSize { get; set; }
 
-		public int DifferentialRoutesPerTransaction { get; set; }
+        public int IndexCommandTimeoutInMinutes { get; set; }
 
-		public int IndexCommandTimeoutInMinutes { get; set; }
+        public bool ExecuteFullRouteSyncWhenPartialNotSupported { get; set; }
 
-		public bool ExecuteFullRouteSyncWhenPartialNotSupported { get; set; }
-	}
 
-	public abstract class RouteSynchronizerSwitchSettings
-	{
-	}
+        public int _differentialRoutesPerTransaction = 100;
+        public int DifferentialRoutesPerTransaction
+        {
+            get { return _differentialRoutesPerTransaction; }
+            set { _differentialRoutesPerTransaction = value; }
+        }
+    }
+
+    public abstract class RouteSynchronizerSwitchSettings
+    {
+    }
 }
