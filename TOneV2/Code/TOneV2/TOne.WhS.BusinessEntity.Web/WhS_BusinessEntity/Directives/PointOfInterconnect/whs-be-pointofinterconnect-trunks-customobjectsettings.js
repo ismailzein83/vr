56bdf4ccdb29c0b2c7cbbbd56bd1d49ff4d85718
Mohamad Abdallah <VANRISE\mohamad.abdallah>
2018-05-24 11:00:57 +0000

    Implementing POI directives
    git-tfs-id: [http://developmentvlan:8585/tfs/vanrise.collection]$/;C47110

--- TOneV2/Code/TOneV2/TOne.WhS.BusinessEntity.Web/WhS_BusinessEntity/Directives/PointOfInterconnect/whs-be-pointofinterconnect-trunks-customobjectsettings.js
+++ TOneV2/Code/TOneV2/TOne.WhS.BusinessEntity.Web/WhS_BusinessEntity/Directives/PointOfInterconnect/whs-be-pointofinterconnect-trunks-customobjectsettings.js
@@ -1,1 +1,55 @@
 ﻿
+
+(function (app) {
+
+    'use strict';
+
+    pointOfInterconnectType.$inject = ["UtilsService", 'VRUIUtilsService', 'VRNotificationService'];
+
+    function pointOfInterconnectType(UtilsService, VRUIUtilsService, VRNotificationService) {
+        return {
+            restrict: "E",
+            scope: {
+                onReady: "=",
+            },
+            controller: function ($scope, $element, $attrs) {
+                var ctrl = this;
+                var ctor = new pointOfInterconnectCtor($scope, ctrl, $attrs);
+                ctor.initializeController();
+            },
+            controllerAs: "Ctrl",
+            bindToController: true,
+            templateUrl: "/Client/Modules/WhS_BusinessEntity/Directives/PointOfInterconnect/Templates/PointOfInterconnectCustomObjectSettings.html"
+
+        };
+        function pointOfInterconnectCtor($scope, ctrl, $attrs) {
+            this.initializeController = initializeController;
+
+            function initializeController() {
+                $scope.scopeModel = {};
+                defineAPI();
+            }
+            function defineAPI() {
+                var api = {};
+
+                api.load = function (payload) {
+
+                };
+
+                api.getData = function () {
+                    var data = {
+                        $type: "TOne.WhS.BusinessEntity.Business.PointOfInterconnectTrunksObjectTypeSettings, TOne.WhS.BusinessEntity.Business"
+                    };
+                    return data;
+                };
+
+                if (ctrl.onReady != undefined && typeof (ctrl.onReady) == 'function') {
+                    ctrl.onReady(api);
+                }
+            }
+        }
+    }
+
+    app.directive('whsBePointofinterconnectTrunksCustomobjectsettings', pointOfInterconnectType);
+
+})(app);
--- TOneV2/Code/TOneV2/TOne.WhS.BusinessEntity.Web/WhS_BusinessEntity/Directives/PointOfInterconnect/whs-be-pointofinterconnect-trunks-staticeditor.js
+++ TOneV2/Code/TOneV2/TOne.WhS.BusinessEntity.Web/WhS_BusinessEntity/Directives/PointOfInterconnect/whs-be-pointofinterconnect-trunks-staticeditor.js
@@ -1,1 +1,112 @@
-﻿
+﻿'use strict';
+
+app.directive('whsBePointofinterconnectTrunksStaticeditor', ['UtilsService', 'VRUIUtilsService', 'VRDateTimeService', 'WhS_BE_SaleZoneAPIService', 'WhS_BE_FaultTicketAPIService',
+    function (UtilsService, VRUIUtilsService, VRDateTimeService, WhS_BE_SaleZoneAPIService, WhS_BE_FaultTicketAPIService) {
+        var directiveDefinitionObject = {
+            restrict: 'E',
+            scope: {
+                onReady: '=',
+                isrequired: '='
+            },
+            controller: function ($scope, $element, $attrs) {
+                var ctrl = this;
+                var ctor = new pointOfInterconnectStaticEditor(ctrl, $scope, $attrs);
+                ctor.initializeController();
+            },
+            controllerAs: 'ctrl',
+            bindToController: true,
+         
+            templateUrl: "/Client/Modules/WhS_BusinessEntity/Directives/PointOfInterconnect/Templates/PointOfInterconnectStaticEditorTemplate.html"
+        };
+
+        function pointOfInterconnectStaticEditor(ctrl, $scope, $attrs) {
+
+
+            this.initializeController = initializeController;
+            function initializeController() {
+
+                $scope.scopeModel = {};
+                $scope.scopeModel.trunks = [];
+                $scope.scopeModel.disabledAddTrunk = true;
+
+                $scope.scopeModel.addTrunk = function () {
+                    $scope.scopeModel.trunks.push({ trunk: $scope.scopeModel.TrunkValue, message: null });
+                    $scope.scopeModel.TrunkValue = undefined;
+                    $scope.scopeModel.disabledAddTrunk = true;
+                };
+
+                $scope.scopeModel.onTrunkValueChange = function (value) {
+                    $scope.scopeModel.disabledAddTrunk = (value == undefined && $scope.scopeModel.TrunkValue.length - 1 < 1) || UtilsService.getItemIndexByVal($scope.scopeModel.trunks, $scope.scopeModel.TrunkValue, "trunk") != -1;
+                };
+
+                $scope.scopeModel.getImportedTrunks = function (values) {
+                    $scope.scopeModel.Trunk.length = 0;
+                    for (var i = 0; i < values.length ; i++) {
+                        if (UtilsService.getItemIndexByVal($scope.scopeModel.trunks, values[i], "trunk") == -1)
+                            $scope.scopeModel.trunks.push({ trunk: values[i], message: null });
+                    }
+                };
+
+                $scope.scopeModel.validateTrunks = function () {
+                    if ($scope.scopeModel.trunks.length == 0)
+                        return "Enter at least one code.";
+                    if (ValidateTrunk() == false)
+                        return "Invalid trunks inputs.";
+                    return null;
+                };
+
+           
+                defineApi();
+
+            }
+
+
+            function ValidateTrunk() {
+                for (var i = 0; i < $scope.scopeModel.trunks.length ; i++) {
+                    if ($scope.scopeModel.trunks[i].message != null)
+                        return false;
+                }
+                return true;
+            }
+        
+
+            function defineApi() {
+
+                var api = {};
+
+                api.load = function (payload) {
+
+                    if (payload != undefined) {
+                        selectedValues = payload.selectedValues;
+
+                        if (selectedValues != undefined) {
+
+                            for (var i = 0; i < selectedValues.trunks.length ; i++) {
+                                $scope.scopeModel.trunks.push({ trunk: selectedValues.trunks[i], message: null });
+                            }
+
+                        }
+                    }
+   
+            };
+
+                api.setData = function (obj) {
+
+                    console.log("setdata");
+
+                        obj.trunksDetails = {
+
+                            trunks: $scope.scopeModel.trunks         
+                    }
+                   
+                };
+
+                if (ctrl.onReady != null) {
+                    ctrl.onReady(api);
+                }
+                
+            }
+
+        }
+        return directiveDefinitionObject;
+    }]);
