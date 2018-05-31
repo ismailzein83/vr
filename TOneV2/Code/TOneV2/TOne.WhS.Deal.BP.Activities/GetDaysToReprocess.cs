0610c0c5822fb29e975c8bd90dd5a970fcdc1ed9
Ali Atoui <VANRISE\ali.atoui>
2018-05-31 10:34:08 +0000

    Ordering DaysToReprocess @GetDaysToReprocess
    git-tfs-id: [http://developmentvlan:8585/tfs/vanrise.collection]$/;C47232

--- TOneV2/Code/TOneV2/TOne.WhS.Deal.BP.Activities/GetDaysToReprocess.cs
+++ TOneV2/Code/TOneV2/TOne.WhS.Deal.BP.Activities/GetDaysToReprocess.cs
@@ -1,43 +1,45 @@
 ﻿using System;
 using System.Activities;
 using System.Collections.Generic;
-using Vanrise.Common;
+using System.Linq;
 using TOne.WhS.Deal.Business;
 using TOne.WhS.Deal.Entities;
 
 namespace TOne.WhS.Deal.BP.Activities
 {
-	public sealed class GetDaysToReprocess : CodeActivity
-	{
-		[RequiredArgument]
-		public OutArgument<List<DayToReprocessSummary>> DaysToReprocessSummary { get; set; }
-		protected override void Execute(CodeActivityContext context)
-		{
-			var daysToReprocess = new DaysToReprocessManager().GetAllDaysToReprocess();
-			var daysToReprocessSummaryByDate = new Dictionary<DateTime, DayToReprocessSummary>();
+    public sealed class GetDaysToReprocess : CodeActivity
+    {
+        [RequiredArgument]
+        public OutArgument<List<DayToReprocessSummary>> DaysToReprocessSummary { get; set; }
 
-			List<DayToReprocessSummary> daysToReprocessSummary = null;
-			if (daysToReprocess != null)
-			{
-				daysToReprocessSummary = new List<DayToReprocessSummary>();
-				foreach (var dayToReprocess in daysToReprocess)
-				{
-					DayToReprocessSummary dayToReprocesSummary;
-					if (!daysToReprocessSummaryByDate.TryGetValue(dayToReprocess.Date, out dayToReprocesSummary))
-					{
-						dayToReprocesSummary = new DayToReprocessSummary() { Date = dayToReprocess.Date, CustomerIds = new List<int>(), SupplierIds = new List<int>() };
-						daysToReprocessSummaryByDate.Add(dayToReprocess.Date, dayToReprocesSummary);
-						daysToReprocessSummary.Add(dayToReprocesSummary);
-					}
+        protected override void Execute(CodeActivityContext context)
+        {
+            var daysToReprocess = new DaysToReprocessManager().GetAllDaysToReprocess();
+            var daysToReprocessSummaryByDate = new Dictionary<DateTime, DayToReprocessSummary>();
 
-					if (dayToReprocess.IsSale)
-						dayToReprocesSummary.CustomerIds.Add(dayToReprocess.CarrierAccountId);
-					else
-						dayToReprocesSummary.SupplierIds.Add(dayToReprocess.CarrierAccountId);
-				}
-			}
+            List<DayToReprocessSummary> daysToReprocessSummary = null;
+            if (daysToReprocess != null)
+            {
+                daysToReprocessSummary = new List<DayToReprocessSummary>();
+                foreach (var dayToReprocess in daysToReprocess)
+                {
+                    DayToReprocessSummary dayToReprocesSummary;
+                    if (!daysToReprocessSummaryByDate.TryGetValue(dayToReprocess.Date, out dayToReprocesSummary))
+                    {
+                        dayToReprocesSummary = new DayToReprocessSummary() { Date = dayToReprocess.Date, CustomerIds = new List<int>(), SupplierIds = new List<int>() };
+                        daysToReprocessSummaryByDate.Add(dayToReprocess.Date, dayToReprocesSummary);
+                        daysToReprocessSummary.Add(dayToReprocesSummary);
+                    }
 
-			this.DaysToReprocessSummary.Set(context, daysToReprocessSummary);
-		}
-	}
+                    if (dayToReprocess.IsSale)
+                        dayToReprocesSummary.CustomerIds.Add(dayToReprocess.CarrierAccountId);
+                    else
+                        dayToReprocesSummary.SupplierIds.Add(dayToReprocess.CarrierAccountId);
+                }
+            }
+
+            daysToReprocessSummary = daysToReprocessSummary != null ? daysToReprocessSummary.OrderByDescending(itm => itm.Date).ToList() : null;
+            this.DaysToReprocessSummary.Set(context, daysToReprocessSummary);
+        }
+    }
 }
