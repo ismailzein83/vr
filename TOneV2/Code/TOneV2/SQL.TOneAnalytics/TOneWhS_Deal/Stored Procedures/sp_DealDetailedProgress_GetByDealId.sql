048185e8f8d367c7e90b907ef92814a0f4ead6d5
Wissam Ajamy <VANRISE\wissam.ajamy>
2018-05-31 06:21:46 +0000

    Update structure from TOneV2_Dev_Analytics
    git-tfs-id: [http://developmentvlan:8585/tfs/vanrise.collection]$/;C47221

---
+++ TOneV2/Code/TOneV2/SQL.TOneAnalytics/TOneWhS_Deal/Stored Procedures/sp_DealDetailedProgress_GetByDealId.sql
@@ -0,0 +1,27 @@
+﻿CREATE PROCEDURE [TOneWhS_Deal].[sp_DealDetailedProgress_GetByDealId]
+	@DealIds nvarchar(max),
+	@FromDate datetime,
+	@ToDate datetime
+AS
+BEGIN
+		
+	Declare @DealIdsLocal nvarchar(max)
+	Declare @FromDateLocal Datetime
+	Declare @ToDateLocale Datetime
+
+	set @DealIdsLocal=@DealIds
+	set @FromDateLocal=	@FromDate
+	set @ToDateLocale =@ToDate
+
+	DECLARE @DealIDsTable TABLE (DealID int)
+	INSERT INTO @DealIDsTable (DealID)
+	select Convert(int, ParsedString) from [TOneWhS_Deal].[ParseStringList](@DealIdsLocal)
+
+	SELECT ddp.[ID],ddp.[DealID],ddp.[ZoneGroupNb],ddp.[IsSale],ddp.[TierNb],ddp.[RateTierNb],ddp.[FromTime],ddp.[ToTime],ddp.[ReachedDurationInSec],ddp.[CreatedTime]
+	FROM [TOneWhS_Deal].[DealDetailedProgress] ddp  WITH(NOLOCK) 
+	WHERE (@DealIds  is null or ddp.DealID in (select DealID from @DealIDsTable))
+	and (@FromDateLocal is null or ddp.FromTime >= @FromDateLocal)
+	and (@ToDateLocale is null or ddp.ToTime<@ToDateLocale)
+	and tierNb is not null
+
+END
