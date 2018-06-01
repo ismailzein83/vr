51aab04801ef46f6e9a68b3cbcba23061f9dee5e
Naim Saad <VANRISE\naim.saad>
2018-06-01 12:42:11 +0000

    Adding Building Files
    git-tfs-id: [http://developmentvlan:8585/tfs/vanrise.collection]$/;C47268

---
+++ Demo.project/Code/Demo.Module.Data.SQL/BuildingDataManager.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Vanrise.Data.SQL;
+using System.Data;
+using Demo.Module.Entities;
+using Newtonsoft.Json;
+
+namespace Demo.Module.Data.SQL
+{
+    class BuildingDataManager
+    {
+    }
+}
---
+++ Demo.project/Code/Demo.Module.Web/Controllers/BuildingController.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Http;
+using Vanrise.Web.Base;
+using Demo.Module.Entities;
+using Demo.Module.Business;
+using Vanrise.Entities;
+
+
+namespace Demo.Module.Web.Controllers
+{
+    [RoutePrefix(Constants.ROUTE_PREFIX + "Branch")]
+    [JSONWithTypeAttribute]
+    public class Demo_Module_BranchController : BaseAPIController
+    {
+    //    BranchManager branchManager = new BranchManager();
+       
+
+    //    [HttpPost]
+    //    [Route("AddBranch")]
+    //    public InsertOperationOutput<BranchDetails> AddBranch(Branch branch)
+    //    {
+    //        return branchManager.AddBranch(branch);
+    //    }
+
+    //    [HttpPost]
+    //    [Route("GetFilteredBranches")]
+    //    public object GetFilteredBranches(DataRetrievalInput<BranchQuery> input)
+    //    {
+    //        return GetWebResponse(input, branchManager.GetFilteredBranches(input));
+    //    }
+
+    //    [HttpGet]
+    //    [Route("GetBranchById")]
+    //    public Branch GetBranchById(int branchId)
+    //    {
+    //        return branchManager.GetBranchById(branchId);
+    //    }
+
+
+    //    [HttpPost]
+    //    [Route("UpdateBranch")]
+    //    public UpdateOperationOutput<BranchDetails> UpdateBranch(Branch branch)
+    //    {
+    //        return branchManager.UpdateBranch(branch);
+    //    }
+    }
+}
