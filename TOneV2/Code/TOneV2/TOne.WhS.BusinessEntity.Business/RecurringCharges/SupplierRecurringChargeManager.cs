3233041edafbe6ea75e14cda6586ed104a28d14d
Fatima Al Heck <VANRISE\fatima.alheck>
2018-06-01 08:20:11 +0000

    adding classes to businessEntity.Business
    git-tfs-id: [http://developmentvlan:8585/tfs/vanrise.collection]$/;C47247

---
+++ TOneV2/Code/TOneV2/TOne.WhS.BusinessEntity.Business/RecurringCharges/SupplierRecurringChargeManager.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Vanrise.GenericData.Business;
+using TOne.WhS.BusinessEntity.Entities.RecurringCharges;
+using Vanrise.GenericData.Entities;
+
+namespace TOne.WhS.BusinessEntity.Business.RecurringCharges
+{
+    public class SupplierRecurringChargeManager
+    {
+       
+
+    }
+}
