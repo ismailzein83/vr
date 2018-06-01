b47e1fd70ee1b9a88831fad2ae7f14a94a3a87fe
Samer Haidar <VANRISE\samer.haidar>
2018-06-01 11:04:47 +0000

    Adding new classes
    git-tfs-id: [http://developmentvlan:8585/tfs/vanrise.collection]$/;C47260

---
+++ Demo.project/Code/Demo.Module.Entities/Item/ItemDetails.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Demo.Module.Entities.Item
+{
+    public class ItemDetails
+    {
+        public int ItemId { get; set; }
+
+        public string ItemName { get; set; }
+
+        public string ProductName { get; set; }
+
+        public int ProductId { get; set; }
+
+    }
+}
---
+++ Demo.project/Code/Demo.Module.Entities/Item/ItemQuery.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Demo.Module.Entities.Item
+{
+    public class ItemQuery
+    {
+        public string Name { get; set; }
+
+        public List<int> ProductIds { get; set; }
+
+
+    }
+}
