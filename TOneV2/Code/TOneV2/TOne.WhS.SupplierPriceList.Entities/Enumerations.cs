daccdc2d1e00dc849421ad45d3544d9c4d8c7799
Rodi Hassan <VANRISE\rodi.hassan>
2018-06-04 08:28:10 +0000

    add ReceivedPricelistStatus enum
    git-tfs-id: [http://developmentvlan:8585/tfs/vanrise.collection]$/;C47279

--- TOneV2/Code/TOneV2/TOne.WhS.SupplierPriceList.Entities/Enumerations.cs
+++ TOneV2/Code/TOneV2/TOne.WhS.SupplierPriceList.Entities/Enumerations.cs
@@ -6,17 +6,25 @@ using System.Threading.Tasks;
 
 namespace TOne.WhS.SupplierPriceList.Entities
 {
-    public enum Status : short
-    {
-        NotChanged = 0,
-        New = 1,
-        Updated = 2,
-    }
+	public enum Status : short
+	{
+		NotChanged = 0,
+		New = 1,
+		Updated = 2,
+	}
 
-    public enum SupplierPriceListType
-    {
-        RateChange = 0,
-        Country = 1,
-        Full = 2
-    }
+	public enum SupplierPriceListType
+	{
+		RateChange = 0,
+		Country = 1,
+		Full = 2
+	}
+
+	public enum ReceivedPricelistStatus
+	{
+		New = 0,
+		Processing = 1,
+		Failed = 2,
+		Succeeded = 3
+	}
 }
