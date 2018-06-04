1fe432b76fae0d6115831c426954122a17bb7b30
Fatima Al Heck <VANRISE\fatima.alheck>
2018-06-04 13:28:51 +0000

    LimitResultFilterDefinitionSettings class
    git-tfs-id: [http://developmentvlan:8585/tfs/vanrise.collection]$/;C47305

---
+++ Vanrise/Code/Vanrise.GenericData.MainExtensions/GenericBusinessEntity/GenericBEFilterDefinition/LimitResultFilterDefinitionSettings.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Vanrise.GenericData.Business;
+using Vanrise.GenericData.Entities;
+
+namespace Vanrise.GenericData.MainExtensions
+{
+    public class LimitResultFilterDefinitionSettings : GenericBEFilterDefinitionSettings
+    {
+        public override Guid ConfigId
+        {
+            get { return new Guid("E2AE62B2-68D9-46C0-9071-58212741CD23"); }
+        }
+
+        public override string RuntimeEditor
+        {
+            get { return "vr-genericdata-genericbe-filterruntime-limitresult"; }
+        }
+
+        public int DefaultLimitResult { get; set; }
+    }
+}
