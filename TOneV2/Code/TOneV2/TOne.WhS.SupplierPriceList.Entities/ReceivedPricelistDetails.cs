071b17d9a3f9c621723ff7e4a7949f1b558c597b
Mohammad Yahfoufi <VANRISE\mohammad.yahfoufi>
2018-06-05 07:56:48 +0000

    Adding ReceivedPricelistDetails in Entities
    git-tfs-id: [http://developmentvlan:8585/tfs/vanrise.collection]$/;C47315

---
+++ TOneV2/Code/TOneV2/TOne.WhS.SupplierPriceList.Entities/ReceivedPricelistDetails.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace TOne.WhS.SupplierPriceList.Entities
+{
+	public class ReceivedPricelistDetails
+	{
+		public int SupplierId { get; set; }
+		public long FileId { get; set; }
+		public DateTime ReceivedDate { get; set; }
+		public SupplierPriceListType PricelistType { get; set; }
+		public ReceivedPricelistStatus Status { get; set; }
+		public int? PricelistId { get; set; }
+		public long? ProcessInstanceId { get; set; }
+	}
+}
