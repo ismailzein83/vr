3492aa77e86ae72192c28a4287db1ee6f8ecfb04
Mohammad Yahfoufi <VANRISE\mohammad.yahfoufi>
2018-06-06 12:51:36 +0000

    Allowing nullable values in ReceivedPricelist entity
    git-tfs-id: [http://developmentvlan:8585/tfs/vanrise.collection]$/;C47350

--- TOneV2/Code/TOneV2/TOne.WhS.SupplierPriceList.Entities/ReceivedPricelist.cs
+++ TOneV2/Code/TOneV2/TOne.WhS.SupplierPriceList.Entities/ReceivedPricelist.cs
@@ -5,11 +5,12 @@ namespace TOne.WhS.SupplierPriceList.Entities
 	public class ReceivedPricelist
 	{
 		public int SupplierId { get; set; }
-		public long FileId { get; set; }
+		public long? FileId { get; set; }
 		public DateTime ReceivedDate { get; set; }
-		public SupplierPriceListType PricelistType { get; set; }
+		public SupplierPriceListType? PricelistType { get; set; }
 		public ReceivedPricelistStatus Status { get; set; }
 		public int? PricelistId { get; set; }
 		public long? ProcessInstanceId { get; set; }
+        public DateTime? StartProcessingDate { get; set; }
 	}
 }
