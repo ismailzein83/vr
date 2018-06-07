5b037a78d495e31828927f8afd7e7fbd762cea2a
Ali Atoui <VANRISE\ali.atoui>
2018-06-07 13:49:50 +0000

    Arranging ISwapTableContext
    git-tfs-id: [http://developmentvlan:8585/tfs/vanrise.collection]$/;C47396

--- TOneV2/Code/TOneV2/TOne.WhS.RouteSync.Entities/Context/ISwapTableContext.cs
+++ TOneV2/Code/TOneV2/TOne.WhS.RouteSync.Entities/Context/ISwapTableContext.cs
@@ -5,35 +5,35 @@ namespace TOne.WhS.RouteSync.Entities
 {
     public interface ISwapTableContext
     {
+        string SwitchId { get; }
+
         string SwitchName { get; }
 
         Action<LogEntryType, string, object[]> WriteTrackingMessage { get; }
 
         int IndexesCommandTimeoutInSeconds { get; }
 
-        string SwitchId { get; }
-
         SwitchSyncOutput PreviousSwitchSyncOutput { get; }
 
-        SwitchSyncOutput SwitchSyncOutput { set; }
-
         Action<Exception, bool> WriteBusinessHandledException { get; }
+
+        SwitchSyncOutput SwitchSyncOutput { set; }
     }
 
     public class SwapTableContext : ISwapTableContext
     {
+        public string SwitchId { get; set; }
+
         public string SwitchName { get; set; }
 
         public Action<LogEntryType, string, object[]> WriteTrackingMessage { get; set; }
 
         public int IndexesCommandTimeoutInSeconds { get; set; }
 
-        public string SwitchId { get; set; }
-
-        public SwitchSyncOutput SwitchSyncOutput { get; set; }
-
         public SwitchSyncOutput PreviousSwitchSyncOutput { get; set; }
 
         public Action<Exception, bool> WriteBusinessHandledException { get; set; }
+
+        public SwitchSyncOutput SwitchSyncOutput { get; set; }
     }
 }
