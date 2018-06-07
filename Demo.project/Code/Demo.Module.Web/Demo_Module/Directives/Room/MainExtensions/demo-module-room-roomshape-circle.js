d5fa50f479e8c915aaae5c2b443f7207f322f499
Naim Saad <VANRISE\naim.saad>
2018-06-07 12:51:01 +0000

    Implementing RoomShape
    git-tfs-id: [http://developmentvlan:8585/tfs/vanrise.collection]$/;C47386

---
+++ Demo.project/Code/Demo.Module.Web/Demo_Module/Directives/Room/MainExtensions/demo-module-room-roomshape-circle.js
@@ -0,0 +1,63 @@
+﻿//"use strict";
+
+//app.directive("demoBestpracticesChildChildshapeCircle", ["UtilsService", "VRNotificationService", "VRUIUtilsService",
+//    function (UtilsService, VRNotificationService, VRUIUtilsService) {
+
+//        var directiveDefinitionObject = {
+
+//            restrict: "E",
+//            scope:
+//            {
+//                onReady: "=",
+//            },
+
+//            controller: function ($scope, $element, $attrs) {
+//                var ctrl = this;
+
+//                var ctor = new CircleShape($scope, ctrl, $attrs);
+//                ctor.initializeController();
+//            },
+
+//            controllerAs: "ctrl",
+//            bindToController: true,
+//            compile: function (element, attrs) {
+
+//            },
+//            templateUrl: "/Client/Modules/Demo_BestPractices/Elements/Child/Directives/MainExtensions/Templates/CircleShapeTemplate.html"
+//        };
+
+//        function CircleShape($scope, ctrl, $attrs) {
+//            this.initializeController = initializeController;
+
+//            function initializeController() {
+//                $scope.scopeModel = {};
+//                defineAPI();
+//            }
+
+//            function defineAPI() {
+//                var api = {};
+
+//                api.load = function (payload) {
+//                    if (payload != undefined && payload.childShapeEntity != undefined) {
+//                        $scope.scopeModel.radius = payload.childShapeEntity.Radius;
+//                    }
+//                    var promises = [];
+//                    return UtilsService.waitMultiplePromises(promises);
+//                };
+
+//                api.getData = function () {
+//                    return {
+//                        $type: "Demo.BestPractices.MainExtentions.Child.CircleShape,Demo.BestPractices.MainExtentions",
+//                        Radius: $scope.scopeModel.radius,
+//                    };
+//                };
+
+//                if (ctrl.onReady != null)
+//                    ctrl.onReady(api);
+//            }
+//        }
+
+//        return directiveDefinitionObject;
+
+//    }
+//]);
