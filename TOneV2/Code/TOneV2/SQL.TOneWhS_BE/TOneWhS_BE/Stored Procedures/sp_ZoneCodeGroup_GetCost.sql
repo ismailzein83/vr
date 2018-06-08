9943655d3251d93337004c69027e42300b9dab5d
Wissam Ajamy <VANRISE\wissam.ajamy>
2018-06-08 09:38:17 +0000

    update SPs
    git-tfs-id: [http://developmentvlan:8585/tfs/vanrise.collection]$/;C47409

--- TOneV2/Code/TOneV2/SQL.TOneWhS_BE/TOneWhS_BE/Stored Procedures/sp_ZoneCodeGroup_GetCost.sql
+++ TOneV2/Code/TOneV2/SQL.TOneWhS_BE/TOneWhS_BE/Stored Procedures/sp_ZoneCodeGroup_GetCost.sql
@@ -8,8 +8,8 @@ CREATE PROCEDURE [TOneWhS_BE].[sp_ZoneCodeGroup_GetCost]
 	@IsFuture BIT
 AS
 BEGIN
-	SELECT ZoneID, sc.Code as CodeGroup FROM [TOneWhS_BE].[SupplierCode] sc
-	JOIN [TOneWhS_BE].CodeGroup cg on cg.ID = sc.CodeGroupID
+	SELECT ZoneID, sc.Code as CodeGroup FROM [TOneWhS_BE].[SupplierCode] sc WITH(NOLOCK)
+	INNER JOIN [TOneWhS_BE].CodeGroup cg on cg.ID = sc.CodeGroupID
 	WHERE ((@IsFuture = 0 AND sc.BED <= @EffectiveOn AND  (sc.EED > @EffectiveOn OR sc.EED IS NULL))
 		   OR (@IsFuture = 1 AND (sc.BED > GETDATE() OR sc.EED IS NULL)))
 	AND sc.Code = cg.Code 
