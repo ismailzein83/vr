e146f765f71ebea753e821496eda9414fc7154f6
Ali Sakr <VANRISE\ali.sakr>
2018-06-12 10:49:24 +0000

    correcting warning message in case of rate increase
    git-tfs-id: [http://developmentvlan:8585/tfs/vanrise.collection]$/;C47455

--- TOneV2/Code/TOneV2/TOne.WhS.SupplierPriceList.Business/BusinessRules/Conditions/ValidateAfterProcessing/RateIncreasedCondition.cs
+++ TOneV2/Code/TOneV2/TOne.WhS.SupplierPriceList.Business/BusinessRules/Conditions/ValidateAfterProcessing/RateIncreasedCondition.cs
@@ -4,6 +4,7 @@ using TOne.WhS.SupplierPriceList.Entities.SPL;
 using Vanrise.BusinessProcess.Entities;
 using System.Linq;
 using System.Collections.Generic;
+using Vanrise.Common;
 
 namespace TOne.WhS.SupplierPriceList.Business
 {
@@ -21,20 +22,31 @@ namespace TOne.WhS.SupplierPriceList.Business
                 return true;
 
             var numberOfIncreasedRates = 0;
-
+            List<string> zonesWithIncreasedRates = new List<string>();
+            var longPrecision = new Vanrise.Common.Business.GeneralSettingsManager().GetLongPrecisionValue();
+            IImportSPLContext importSPLContext = context.GetExtension<IImportSPLContext>();
+            string codeEffectiveDateString = importSPLContext.CodeEffectiveDate.ToString(importSPLContext.DateFormat);
             foreach (ImportedRate importedRate in importedCountry.ImportedRates)
-            {
+             {
                 if (importedRate.ChangedExistingRates.Count() > 0 && importedRate.ChangedExistingRates.Any(x => importedRate.Rate > x.RateEntity.Rate))
+                {
                     numberOfIncreasedRates++;
+                    if (importedRate.BED < importSPLContext.CodeEffectiveDate)
+                    {
+                        importedRate.ProcessInfo.RecentExistingRate.ThrowIfNull("RecentExistingRate");
+                        var recentRate = Decimal.Round(importedRate.ProcessInfo.RecentExistingRate.ConvertedRate, longPrecision);
+                        var currentRate = Decimal.Round(importedRate.Rate, longPrecision);
+                        zonesWithIncreasedRates.Add(string.Format("Rate increase on zone '{0}' from '{1}' to '{2}' effective on '{3}' should be on '{4}'", importedRate.ZoneName, recentRate, currentRate, importedRate.BED.ToString(importSPLContext.DateFormat), codeEffectiveDateString));
+                    }
+                }
             }
 
             if (numberOfIncreasedRates > 0)
             {
                 string countryName = new Vanrise.Common.Business.CountryManager().GetCountryName(importedCountry.CountryId);
-                context.Message = string.Format("{0} increased rates in country '{1}'.", numberOfIncreasedRates, countryName);
+                context.Message = string.Format("{0} increased rate(s) in country '{1}'. {2}", numberOfIncreasedRates, countryName, string.Join(", ", zonesWithIncreasedRates));
                 return false;
             }
-
             return true;
         }
         public override string GetMessage(IRuleTarget target)
