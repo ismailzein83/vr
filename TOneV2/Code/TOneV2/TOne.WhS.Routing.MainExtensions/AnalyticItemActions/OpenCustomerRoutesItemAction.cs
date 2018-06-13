f40b8173af1d429c33f57b6595c4101dbffb786d
Rodi Hassan <VANRISE\rodi.hassan>
2018-06-13 08:13:42 +0000

    Add open customer routes action
    git-tfs-id: [http://developmentvlan:8585/tfs/vanrise.collection]$/;C47465

---
+++ TOneV2/Code/TOneV2/TOne.WhS.Routing.MainExtensions/AnalyticItemActions/OpenCustomerRoutesItemAction.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using Vanrise.Analytic.Entities;
+
+namespace TOne.WhS.Routing.MainExtensions.AnalyticItemActions
+{
+	public class OpenCustomerRoutesItemAction : AnalyticItemAction
+	{
+		public override Guid ConfigId { get { return new Guid("2AEDD1D5-26D1-4C0E-A49C-CE5E8355C300"); } }
+
+		public override void Execute(IAnalyticItemActionContext context)
+		{
+			throw new NotImplementedException();
+		}
+	}
+}
