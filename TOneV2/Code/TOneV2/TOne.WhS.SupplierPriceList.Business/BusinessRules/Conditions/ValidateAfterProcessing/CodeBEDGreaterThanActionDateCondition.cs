b6207ce68366c8899b2a6f804a1e84ae76b9c973
Ali Sakr <VANRISE\ali.sakr>
2018-06-14 13:43:07 +0000

    adding business rule for import SPL
    git-tfs-id: [http://developmentvlan:8585/tfs/vanrise.collection]$/;C47510

--- TOneV2/Code/TOneV2/TOne.WhS.SupplierPriceList.Business/BusinessRules/Conditions/ValidateAfterProcessing/CodeBEDGreaterThanActionDateCondition.cs
+++ TOneV2/Code/TOneV2/TOne.WhS.SupplierPriceList.Business/BusinessRules/Conditions/ValidateAfterProcessing/CodeBEDGreaterThanActionDateCondition.cs
@@ -50,4 +50,53 @@ namespace TOne.WhS.SupplierPriceList.Business
             throw new NotImplementedException();
         }
     }
+    public class ClosingZonesAboveThresholdCondition : Vanrise.BusinessProcess.Entities.BusinessRuleCondition
+    {
+        public override bool ShouldValidate(Vanrise.BusinessProcess.Entities.IRuleTarget target)
+        {
+            return target is AllZones;
+        }
+        public override bool Validate(Vanrise.BusinessProcess.Entities.IBusinessRuleConditionValidateContext context)
+        {
+             // AllImportedZones allImportedZones = context.Target as AllImportedZones;
+             // List<NotImportedZone> notImportedZones = context.Target as List<NotImportedZone>;
+             //if (notImportedZones.Count() > allImportedZones.Zones.Count())
+             //     return false;
+            return true;
+        }
+        public override string GetMessage(Vanrise.BusinessProcess.Entities.IRuleTarget target)
+        {
+            throw new NotImplementedException();
+        }
+    }
+    public class RenamedZonesCondition : Vanrise.BusinessProcess.Entities.BusinessRuleCondition
+    {
+        public override bool ShouldValidate(Vanrise.BusinessProcess.Entities.IRuleTarget target)
+        {
+            return target is AllImportedZones;
+        }
+        public override bool Validate(Vanrise.BusinessProcess.Entities.IBusinessRuleConditionValidateContext context)
+        {
+            //List<string>renamedZones = new List<string>();
+            //AllImportedZones allImportedZones = context.Target as AllImportedZones;
+            //if(allImportedZones.Zones ==null || allImportedZones.Zones.Count () == 0)
+            //    return true;
+            //foreach (var importedZone in allImportedZones.Zones)
+            //{
+            //    if (importedZone.ChangeType == ZoneChangeType.Renamed)
+            //        renamedZones.Add(importedZone.ZoneName);
+            //}
+            //if(renamedZones.Count()>0)
+            //{
+            //    context.Message = string.Format("The following zone(s) has been renamed :'{0}'", string.Join(",", renamedZones));
+            //    return false;
+            //}
+
+            return true;
+        }
+        public override string GetMessage(Vanrise.BusinessProcess.Entities.IRuleTarget target)
+        {
+            throw new NotImplementedException();
+        }
+    }
 }
