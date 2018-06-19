aa95c8e9e31608915d145513ce3d858c1c1de949
Fatima Al Heck <VANRISE\fatima.alheck>
2018-06-19 06:10:35 +0000

    Adding RecurringChargePeriodSettingsContext to source control
    git-tfs-id: [http://developmentvlan:8585/tfs/vanrise.collection]$/;C47512

---
+++ TOneV2/Code/TOneV2/TOne.WhS.BusinessEntity.Entities/RecurringCharges/RecurringChargePeriodSettingsContext.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TOne.WhS.BusinessEntity.Entities
+{
+     public class RecurringChargePeriodSettingsContext : IRecurringChargePeriodSettingsContext
+    {
+         public DateTime FromDate { set;  get; }
+         public DateTime ToDate { set;  get; }
+         public List<DateTime> Periods { set; get; }
+    }
+}
