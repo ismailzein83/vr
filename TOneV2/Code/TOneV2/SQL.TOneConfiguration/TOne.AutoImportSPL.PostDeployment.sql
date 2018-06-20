11a5dc74f6e4d03fc9c04fa6e949ae153667fa06
Wissam Ajamy <VANRISE\wissam.ajamy>
2018-06-20 15:02:02 +0000

    Add auto import  SPL post script
    git-tfs-id: [http://developmentvlan:8585/tfs/vanrise.collection]$/;C47588

---
+++ TOneV2/Code/TOneV2/SQL.TOneConfiguration/TOne.AutoImportSPL.PostDeployment.sql
@@ -0,0 +1,76 @@
+﻿/*
+Post-Deployment Script Template							
+--------------------------------------------------------------------------------------
+ This file contains SQL statements that will be appended to the build script.		
+ Use SQLCMD syntax to include a file in the post-deployment script.			
+ Example:      :r .\myfile.sql								
+ Use SQLCMD syntax to reference a variable in the post-deployment script.		
+ Example:      :setvar TableName MyTable							
+               SELECT * FROM [$(TableName)]					
+--------------------------------------------------------------------------------------
+*/
+--[common].[ExtensionConfiguration]---------------------------------------------------------------------
+begin
+set nocount on;
+;with cte_data([ID],[Name],[Title],[ConfigType],[Settings])
+as (select * from (values
+--//////////////////////////////////////////////////////////////////////////////////////////////////
+('7976A765-DD69-4A24-81B2-64C6C231BE79','Supplier Pricelist Received Mail Synchronizer','Supplier Pricelist Received Mail Synchronizer','VR_BEBridge_BESynchronizer','{"Editor":"vr-whs-be-receivedsupplierpricelistmessage-synchronizer"}'),
+('05382832-5CBB-46C0-8214-B3B81769FB80','POP3 Supplier Pricelist Filter','POP3 Supplier Pricelist Filter','VRCommon_Pop3MessageFilter'								,'{"Editor":"vr-whs-be-pop3supplierpricelistmessagefilter"}'),
+('0A5F1A1F-7A68-44EF-A71D-7BFD81D68F91','WhS_SupPL_VRObjectTypes_ReceivedPricelistObjectType','Received Pricelist','VR_Common_ObjectType'							,'{"Editor":"whs-spl-receivedpricelistobjecttype", "PropertyEvaluatorExtensionType": "WhS_SPL_ReceivedPricelist_PropertyEvaluator"}'),
+('BEC4CF34-A716-4FD9-A38B-FCF1BB56265B','WhS_SPL_ReceivedPricelist_PropertyEvaluator','Received Pricelist Property','WhS_SPL_ReceivedPricelist_PropertyEvaluator'	,'{"Editor":"whs-spl-receivedpricelistpropertyevaluator"}')
+--\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\
+)c([ID],[Name],[Title],[ConfigType],[Settings]))
+merge	[common].[ExtensionConfiguration] as t
+using	cte_data as s
+on		1=1 and t.[ID] = s.[ID]
+when matched then
+	update set
+	[Name] = s.[Name],[Title] = s.[Title],[ConfigType] = s.[ConfigType],[Settings] = s.[Settings]
+when not matched by target then
+	insert([ID],[Name],[Title],[ConfigType],[Settings])
+	values(s.[ID],s.[Name],s.[Title],s.[ConfigType],s.[Settings]);
+----------------------------------------------------------------------------------------------------
+end
+
+--[sec].[View]--------------------------------------------------------------------------------------
+----------------------------------------------------------------------------------------------------
+set nocount on;
+;with cte_data([ID],[Name],[Title],[Url],[Module],[ActionNames],[Audience],[Content],[Settings],[Type],[Rank],[IsDeleted])
+as (select * from (values
+--//////////////////////////////////////////////////////////////////////////////////////////////////
+('4073D6EB-E9DD-47B2-9FF4-C3C0B5672BC5','Received Supplier Pricelist','Received Supplier Pricelist','#/view/WhS_SupplierPriceList/Views/ReceivedSupplierPriceList','D66F9910-48EC-4FFD-8A09-C7960A6EE434','WhS_SupPL/ReceivedSupplierPriceList/GetFilteredReceivedSupplierPriceList',null,null,null,'372ED3CB-4B7B-4464-9ABF-59CD7B08BD23',20,null)
+--\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\
+)c([ID],[Name],[Title],[Url],[Module],[ActionNames],[Audience],[Content],[Settings],[Type],[Rank],[IsDeleted]))
+merge	[sec].[View] as t
+using	cte_data as s
+on		1=1 and t.[ID] = s.[ID]
+when matched then
+	update set
+	[Name] = s.[Name],[Title] = s.[Title],[Url] = s.[Url],[Module] = s.[Module],[ActionNames] = s.[ActionNames],[Audience] = s.[Audience],[Content] = s.[Content],[Settings] = s.[Settings],[Type] = s.[Type],[Rank] = s.[Rank],[IsDeleted] = s.[IsDeleted]
+when not matched by target then
+	insert([ID],[Name],[Title],[Url],[Module],[ActionNames],[Audience],[Content],[Settings],[Type],[Rank],[IsDeleted])
+	values(s.[ID],s.[Name],s.[Title],s.[Url],s.[Module],s.[ActionNames],s.[Audience],s.[Content],s.[Settings],s.[Type],s.[Rank],s.[IsDeleted]);
+
+
+--[sec].[SystemAction]------------------------------------------------------------------------------
+----------------------------------------------------------------------------------------------------
+set nocount on;
+;with cte_data([Name],[RequiredPermissions])
+as (select * from (values
+--//////////////////////////////////////////////////////////////////////////////////////////////////
+('WhS_SupPL/ReceivedSupplierPriceList/GetFilteredReceivedSupplierPriceList','WhS_BE_SupplierPricelist: Start Process')
+--\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\
+)c([Name],[RequiredPermissions]))
+merge	[sec].[SystemAction] as t
+using	cte_data as s
+on		1=1 and t.[Name] = s.[Name]
+when matched then
+	update set [RequiredPermissions] = s.[RequiredPermissions]
+when not matched by target then
+	insert([Name],[RequiredPermissions])
+	values(s.[Name],s.[RequiredPermissions]);
+
+--[common].[VRObjectTypeDefinition]--------------------------------------------------------------------------------------------------------------------------------------------------------------------set nocount on;;with cte_data([ID],[Name],[Settings])as (select * from (values--//////////////////////////////////////////////////////////////////////////////////////////////////('A03BA5D0-F45C-4EBA-91B1-1DFA9959D280','Received Pricelist','{"$type":"Vanrise.Entities.VRObjectTypeDefinitionSettings, Vanrise.Entities","ObjectType":{"$type":"TOne.WhS.SupplierPriceList.MainExtensions.VRObjectTypes.ReceivedPricelistObjectType, TOne.WhS.SupplierPriceList.MainExtensions","ConfigId":"0a5f1a1f-7a68-44ef-a71d-7bfd81d68f91"},"Properties":{"$type":"System.Collections.Generic.Dictionary`2[[System.String, mscorlib],[Vanrise.Entities.VRObjectTypePropertyDefinition, Vanrise.Entities]], mscorlib","Pricelist Type":{"$type":"Vanrise.Entities.VRObjectTypePropertyDefinition, Vanrise.Entities","Name":"Pricelist Type","Description":"Pricelist Type","PropertyEvaluator":{"$type":"TOne.WhS.SupplierPriceList.MainExtensions.VRObjectTypes.ReceivedPricelistPropertyEvaluator, TOne.WhS.SupplierPriceList.MainExtensions","ConfigId":"bec4cf34-a716-4fd9-a38b-fcf1bb56265b","ReceivedPricelistField":0}},"Pricelist Status":{"$type":"Vanrise.Entities.VRObjectTypePropertyDefinition, Vanrise.Entities","Name":"Pricelist Status","Description":"Pricelist Status","PropertyEvaluator":{"$type":"TOne.WhS.SupplierPriceList.MainExtensions.VRObjectTypes.ReceivedPricelistPropertyEvaluator, TOne.WhS.SupplierPriceList.MainExtensions","ConfigId":"bec4cf34-a716-4fd9-a38b-fcf1bb56265b","ReceivedPricelistField":1}},"Pricelist Name":{"$type":"Vanrise.Entities.VRObjectTypePropertyDefinition, Vanrise.Entities","Name":"Pricelist Name","Description":"Pricelist Name","PropertyEvaluator":{"$type":"TOne.WhS.SupplierPriceList.MainExtensions.VRObjectTypes.ReceivedPricelistPropertyEvaluator, TOne.WhS.SupplierPriceList.MainExtensions","ConfigId":"bec4cf34-a716-4fd9-a38b-fcf1bb56265b","ReceivedPricelistField":2}},"Supplier Name":{"$type":"Vanrise.Entities.VRObjectTypePropertyDefinition, Vanrise.Entities","Name":"Supplier Name","Description":"Supplier Name","PropertyEvaluator":{"$type":"TOne.WhS.SupplierPriceList.MainExtensions.VRObjectTypes.ReceivedPricelistPropertyEvaluator, TOne.WhS.SupplierPriceList.MainExtensions","ConfigId":"bec4cf34-a716-4fd9-a38b-fcf1bb56265b","ReceivedPricelistField":5}},"Supplier Email":{"$type":"Vanrise.Entities.VRObjectTypePropertyDefinition, Vanrise.Entities","Name":"Supplier Email","Description":"Supplier Email","PropertyEvaluator":{"$type":"TOne.WhS.SupplierPriceList.MainExtensions.VRObjectTypes.ReceivedPricelistPropertyEvaluator, TOne.WhS.SupplierPriceList.MainExtensions","ConfigId":"bec4cf34-a716-4fd9-a38b-fcf1bb56265b","ReceivedPricelistField":6}},"Errors":{"$type":"Vanrise.Entities.VRObjectTypePropertyDefinition, Vanrise.Entities","Name":"Errors","Description":"Errors","PropertyEvaluator":{"$type":"TOne.WhS.SupplierPriceList.MainExtensions.VRObjectTypes.ReceivedPricelistPropertyEvaluator, TOne.WhS.SupplierPriceList.MainExtensions","ConfigId":"bec4cf34-a716-4fd9-a38b-fcf1bb56265b","ReceivedPricelistField":7}}}}')--\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\)c([ID],[Name],[Settings]))merge	[common].[VRObjectTypeDefinition] as tusing	cte_data as son		1=1 and t.[ID] = s.[ID]when matched then	update set	[Name] = s.[Name],[Settings] = s.[Settings]when not matched by target then	insert([ID],[Name],[Settings])	values(s.[ID],s.[Name],s.[Settings]);
+
+--[common].[MailMessageType]------------------------------------------------------------------------------------------------------------------------------------------------------------------------set nocount on;;with cte_data([ID],[Name],[Settings])as (select * from (values--//////////////////////////////////////////////////////////////////////////////////////////////////('757F93C4-1886-4002-82AE-2FDA0E529A3C','Received Supplier Pricelist','{"$type":"Vanrise.Entities.VRMailMessageTypeSettings, Vanrise.Entities","Objects":{"$type":"Vanrise.Entities.VRObjectVariableCollection, Vanrise.Entities","Received Pricelist":{"$type":"Vanrise.Entities.VRObjectVariable, Vanrise.Entities","ObjectName":"Received Pricelist","VRObjectTypeDefinitionId":"a03ba5d0-f45c-4eba-91b1-1dfa9959d280"}}}')--\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\)c([ID],[Name],[Settings]))merge	[common].[MailMessageType] as tusing	cte_data as son		1=1 and t.[ID] = s.[ID]when matched then	update set	[Name] = s.[Name],[Settings] = s.[Settings]when not matched by target then	insert([ID],[Name],[Settings])	values(s.[ID],s.[Name],s.[Settings]);
--- TOneV2/Code/TOneV2/SQL.TOneConfiguration/VR_BEBridge.PostDeployment.sql
+++ TOneV2/Code/TOneV2/SQL.TOneConfiguration/VR_BEBridge.PostDeployment.sql
@@ -9,7 +9,37 @@ Post-Deployment Script Template
                SELECT * FROM [$(TableName)]					
 --------------------------------------------------------------------------------------
 */
---[common].[ExtensionConfiguration]---------------------------------------------------------------------beginset nocount on;;with cte_data([ID],[Name],[Title],[ConfigType],[Settings])as (select * from (values--//////////////////////////////////////////////////////////////////////////////////////////////////('DF73CDCE-01AB-4B55-9511-3A56B495A0D0','FTP Source Reader','FTP Source Reader','VR_BEBridge_SourceBeReaders'		,'{"Editor":"vr-bebridge-sourcebereaders-ftp-directive"}'),('3BECA9AA-8F75-447B-82FE-F50DDB183730','File Source Reader','File Source Reader','VR_BEBridge_SourceBeReaders'		,'{"Editor":"vr-bebridge-sourcebereaders-file-directive"}'),('B2AD979E-818C-46A2-8426-D60AE7F0F841','CRM Source Reader','CRM Source Reader','VR_BEBridge_SourceBeReaders'		,' {"Editor":"vr-bebridge-sourcebereaders-crm-directive"}'),('287A031D-D476-4B03-9478-76DCB9076A95','SQL Source Reader','SQL Source Reader','VR_BEBridge_SourceBeReaders'		,'{"Editor":"vr-bebridge-sourcebereaders-sql-directive"}'),('E761535C-9768-4047-8B61-FDE5D83A9A4A','Database Synchronizer','Database Synchronizer','VR_BEBridge_BESynchronizer','{"Editor":"vr-bebridge-synchronizer-database"}')--\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\)c([ID],[Name],[Title],[ConfigType],[Settings]))merge	[common].[ExtensionConfiguration] as tusing	cte_data as son		1=1 and t.[ID] = s.[ID]when matched then	update set	[Name] = s.[Name],[Title] = s.[Title],[ConfigType] = s.[ConfigType],[Settings] = s.[Settings]when not matched by target then	insert([ID],[Name],[Title],[ConfigType],[Settings])	values(s.[ID],s.[Name],s.[Title],s.[ConfigType],s.[Settings]);----------------------------------------------------------------------------------------------------end
+
+--[common].[ExtensionConfiguration]---------------------------------------------------------------------
+begin
+set nocount on;
+;with cte_data([ID],[Name],[Title],[ConfigType],[Settings])
+as (select * from (values
+--//////////////////////////////////////////////////////////////////////////////////////////////////
+('DF73CDCE-01AB-4B55-9511-3A56B495A0D0','FTP Source Reader','FTP Source Reader','VR_BEBridge_SourceBeReaders'		,'{"Editor":"vr-bebridge-sourcebereaders-ftp-directive"}'),
+('3BECA9AA-8F75-447B-82FE-F50DDB183730','File Source Reader','File Source Reader','VR_BEBridge_SourceBeReaders'		,'{"Editor":"vr-bebridge-sourcebereaders-file-directive"}'),
+('B2AD979E-818C-46A2-8426-D60AE7F0F841','CRM Source Reader','CRM Source Reader','VR_BEBridge_SourceBeReaders'		,' {"Editor":"vr-bebridge-sourcebereaders-crm-directive"}'),
+('287A031D-D476-4B03-9478-76DCB9076A95','SQL Source Reader','SQL Source Reader','VR_BEBridge_SourceBeReaders'		,'{"Editor":"vr-bebridge-sourcebereaders-sql-directive"}'),
+('E761535C-9768-4047-8B61-FDE5D83A9A4A','Database Synchronizer','Database Synchronizer','VR_BEBridge_BESynchronizer','{"Editor":"vr-bebridge-synchronizer-database"}'),
+
+('05A15B37-43CB-4D0D-B453-803645D25C03','POP3 Source Reader','POP3 Source Reader','VR_BEBridge_SourceBeReaders'							,'{"Editor":"vr-bebridge-sourcebereaders-pop3-directive"}'),
+('FD7A74F9-34DD-460A-87A4-288684A6C9B1','Received Mail Message Convertor','Received Mail Message Convertor','VR_BEBridge_BEConvertor'	,'{"Editor":"vr-common-receivedmailmessageconvertor-editor"}'),
+('E3BF7C73-14BA-402B-9158-A67D03635447','POP3 Connection','POP3 Connection','VRCommon_ConnectionConfig'									,'{"Editor":"vr-common-pop3connection-editor"}')
+--\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\
+)c([ID],[Name],[Title],[ConfigType],[Settings]))
+merge	[common].[ExtensionConfiguration] as t
+using	cte_data as s
+on		1=1 and t.[ID] = s.[ID]
+when matched then
+	update set
+	[Name] = s.[Name],[Title] = s.[Title],[ConfigType] = s.[ConfigType],[Settings] = s.[Settings]
+when not matched by target then
+	insert([ID],[Name],[Title],[ConfigType],[Settings])
+	values(s.[ID],s.[Name],s.[Title],s.[ConfigType],s.[Settings]);
+----------------------------------------------------------------------------------------------------
+end
+
+
 --[bp].[BPDefinition]----------------------5001 to 6000---------------------------------------------
 BEGIN
 set nocount on;
@@ -30,5 +60,72 @@ when not matched by target then
 	values(s.[ID],s.[Name],s.[Title],s.[FQTN],s.[Config]);
 ----------------------------------------------------------------------------------------------------
 END
---sec.module----------------------------------------------------------------------------------------beginset nocount on;;with cte_data([ID],[Name],[Url],[ParentId],[Icon],[Rank],[AllowDynamic])as (select * from (values--//////////////////////////////////////////////////////////////////////////////////////////////////('792AAF3B-1AD8-4C05-84C7-1C77E5A01289','BE Bridge',null,'D018C0CD-F15F-486D-80C3-F9B87C3F47B8',null,60,null)--\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\)c([ID],[Name],[Url],[ParentId],[Icon],[Rank],[AllowDynamic]))merge	[sec].[module] as tusing	cte_data as son		1=1 and t.[ID] = s.[ID]when matched then	update set	[Name] = s.[Name],[Url] = s.[Url],[ParentId] = s.[ParentId],[Icon] = s.[Icon],[Rank] = s.[Rank],[AllowDynamic] = s.[AllowDynamic]when not matched by target then	insert([ID],[Name],[Url],[ParentId],[Icon],[Rank],[AllowDynamic])	values(s.[ID],s.[Name],s.[Url],s.[ParentId],s.[Icon],s.[Rank],s.[AllowDynamic]);----------------------------------------------------------------------------------------------------end--[sec].[View]----------------------------------------------------------------------------------------beginset nocount on;
-;with cte_data([ID],[Name],[Title],[Url],[Module],[ActionNames],[Audience],[Content],[Settings],[Type],[Rank])as (select * from (values--//////////////////////////////////////////////////////////////////////////////////////////////////('B05D114E-8711-4621-A890-FB84E20FDA95','Receive Definition','Receive Definition','#/view/VR_BEBridge/Views/BEReceiveDefinition/BEReceiveDefinitionManagement','792AAF3B-1AD8-4C05-84C7-1C77E5A01289','VR_BEBridge/BERecieveDefinition/GetFilteredBeReceiveDefinitions',null,null,null,'372ED3CB-4B7B-4464-9ABF-59CD7B08BD23',2)--\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\)c([ID],[Name],[Title],[Url],[Module],[ActionNames],[Audience],[Content],[Settings],[Type],[Rank]))merge	[sec].[View] as tusing	cte_data as son		1=1 and t.[ID] = s.[ID]when matched then	update set	[Name] = s.[Name],[Title] = s.[Title],[Url] = s.[Url],[Module] = s.[Module],[ActionNames] = s.[ActionNames],[Audience] = s.[Audience],[Content] = s.[Content],[Settings] = s.[Settings],[Type] = s.[Type],[Rank] = s.[Rank]when not matched by target then	insert([ID],[Name],[Title],[Url],[Module],[ActionNames],[Audience],[Content],[Settings],[Type],[Rank])	values(s.[ID],s.[Name],s.[Title],s.[Url],s.[Module],s.[ActionNames],s.[Audience],s.[Content],s.[Settings],s.[Type],s.[Rank]);----------------------------------------------------------------------------------------------------end--[sec].[SystemAction]----------------------------------------------------------------------------------------------------------------------------------------------------------------------------------set nocount on;;with cte_data([Name],[RequiredPermissions])as (select * from (values--//////////////////////////////////////////////////////////////////////////////////////////////////('VR_BEBridge/BERecieveDefinition/GetBERecieveDefinitionsInfo',null),('VR_BEBridge/BERecieveDefinition/GetFilteredBeReceiveDefinitions','VR_SystemConfiguration: View'),('VR_BEBridge/BERecieveDefinition/GetReceiveDefinition',null),('VR_BEBridge/BERecieveDefinition/UpdateReceiveDefinition','VR_SystemConfiguration: Edit'),('VR_BEBridge/BERecieveDefinition/AddReceiveDefinition','VR_SystemConfiguration: Add'),('VR_BEBridge/BERecieveDefinition/GetSourceReaderExtensionConfigs',null),('VR_BEBridge/BERecieveDefinition/GetTargetSynchronizerExtensionConfigs',null),('VR_BEBridge/BERecieveDefinition/GetTargetConvertorExtensionConfigs',null)--\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\)c([Name],[RequiredPermissions]))merge	[sec].[SystemAction] as tusing	cte_data as son		1=1 and t.[Name] = s.[Name]when matched then	update set	[RequiredPermissions] = s.[RequiredPermissions]when not matched by target then	insert([Name],[RequiredPermissions])	values(s.[Name],s.[RequiredPermissions]);
+
+
+--sec.module----------------------------------------------------------------------------------------
+begin
+set nocount on;
+;with cte_data([ID],[Name],[Url],[ParentId],[Icon],[Rank],[AllowDynamic])
+as (select * from (values
+--//////////////////////////////////////////////////////////////////////////////////////////////////
+('792AAF3B-1AD8-4C05-84C7-1C77E5A01289','BE Bridge',null,'D018C0CD-F15F-486D-80C3-F9B87C3F47B8',null,60,null)
+--\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\
+)c([ID],[Name],[Url],[ParentId],[Icon],[Rank],[AllowDynamic]))
+merge	[sec].[module] as t
+using	cte_data as s
+on		1=1 and t.[ID] = s.[ID]
+when matched then
+	update set
+	[Name] = s.[Name],[Url] = s.[Url],[ParentId] = s.[ParentId],[Icon] = s.[Icon],[Rank] = s.[Rank],[AllowDynamic] = s.[AllowDynamic]
+when not matched by target then
+	insert([ID],[Name],[Url],[ParentId],[Icon],[Rank],[AllowDynamic])
+	values(s.[ID],s.[Name],s.[Url],s.[ParentId],s.[Icon],s.[Rank],s.[AllowDynamic]);
+----------------------------------------------------------------------------------------------------
+end
+
+--[sec].[View]----------------------------------------------------------------------------------------
+begin
+set nocount on;
+;with cte_data([ID],[Name],[Title],[Url],[Module],[ActionNames],[Audience],[Content],[Settings],[Type],[Rank])
+as (select * from (values
+--//////////////////////////////////////////////////////////////////////////////////////////////////
+('B05D114E-8711-4621-A890-FB84E20FDA95','Receive Definition','Receive Definition','#/view/VR_BEBridge/Views/BEReceiveDefinition/BEReceiveDefinitionManagement','792AAF3B-1AD8-4C05-84C7-1C77E5A01289','VR_BEBridge/BERecieveDefinition/GetFilteredBeReceiveDefinitions',null,null,null,'372ED3CB-4B7B-4464-9ABF-59CD7B08BD23',2)
+--\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\
+)c([ID],[Name],[Title],[Url],[Module],[ActionNames],[Audience],[Content],[Settings],[Type],[Rank]))
+merge	[sec].[View] as t
+using	cte_data as s
+on		1=1 and t.[ID] = s.[ID]
+when matched then
+	update set
+	[Name] = s.[Name],[Title] = s.[Title],[Url] = s.[Url],[Module] = s.[Module],[ActionNames] = s.[ActionNames],[Audience] = s.[Audience],[Content] = s.[Content],[Settings] = s.[Settings],[Type] = s.[Type],[Rank] = s.[Rank]
+when not matched by target then
+	insert([ID],[Name],[Title],[Url],[Module],[ActionNames],[Audience],[Content],[Settings],[Type],[Rank])
+	values(s.[ID],s.[Name],s.[Title],s.[Url],s.[Module],s.[ActionNames],s.[Audience],s.[Content],s.[Settings],s.[Type],s.[Rank]);
+----------------------------------------------------------------------------------------------------
+end
+
+--[sec].[SystemAction]------------------------------------------------------------------------------
+----------------------------------------------------------------------------------------------------
+set nocount on;
+;with cte_data([Name],[RequiredPermissions])
+as (select * from (values
+--//////////////////////////////////////////////////////////////////////////////////////////////////
+('VR_BEBridge/BERecieveDefinition/GetBERecieveDefinitionsInfo',null),
+('VR_BEBridge/BERecieveDefinition/GetFilteredBeReceiveDefinitions','VR_SystemConfiguration: View'),
+('VR_BEBridge/BERecieveDefinition/GetReceiveDefinition',null),
+('VR_BEBridge/BERecieveDefinition/UpdateReceiveDefinition','VR_SystemConfiguration: Edit'),
+('VR_BEBridge/BERecieveDefinition/AddReceiveDefinition','VR_SystemConfiguration: Add'),
+('VR_BEBridge/BERecieveDefinition/GetSourceReaderExtensionConfigs',null),
+('VR_BEBridge/BERecieveDefinition/GetTargetSynchronizerExtensionConfigs',null),
+('VR_BEBridge/BERecieveDefinition/GetTargetConvertorExtensionConfigs',null)
+--\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\
+)c([Name],[RequiredPermissions]))
+merge	[sec].[SystemAction] as t
+using	cte_data as s
+on		1=1 and t.[Name] = s.[Name]
+when matched then
+	update set
+	[RequiredPermissions] = s.[RequiredPermissions]
+when not matched by target then
+	insert([Name],[RequiredPermissions])
+	values(s.[Name],s.[RequiredPermissions]);
