42540c38c676adbb4196aeff7af3f3b3a410434a
Rodi Hassan <VANRISE\rodi.hassan>
2018-06-20 09:16:30 +0000

    add ErrorMessages to execute rules
    git-tfs-id: [http://developmentvlan:8585/tfs/vanrise.collection]$/;C47559

--- BusinessProcess/Code/Vanrise.BusinessProcess.WFActivities/ExecuteRules.cs
+++ BusinessProcess/Code/Vanrise.BusinessProcess.WFActivities/ExecuteRules.cs
@@ -8,175 +8,181 @@ using Vanrise.Common;
 
 namespace Vanrise.BusinessProcess.WFActivities
 {
-    public sealed class ExecuteRules : CodeActivity
-    {
-        [RequiredArgument]
-        public InArgument<IEnumerable<IRuleTarget>> ImportedDataToValidate { get; set; }
+	public sealed class ExecuteRules : CodeActivity
+	{
+		[RequiredArgument]
+		public InArgument<IEnumerable<IRuleTarget>> ImportedDataToValidate { get; set; }
 
-        [RequiredArgument]
-        public InArgument<string> BusinessRulesKey { get; set; }
+		[RequiredArgument]
+		public InArgument<string> BusinessRulesKey { get; set; }
 
-        public InArgument<bool> WriteMessagesToParentProcess { get; set; }
+		public InArgument<bool> WriteMessagesToParentProcess { get; set; }
 
-        public InArgument<string> ParentMessagePrefix { get; set; }
+		public InArgument<string> ParentMessagePrefix { get; set; }
 
-        public InArgument<bool> GetRulesFromParentProcess { get; set; }
+		public InArgument<bool> GetRulesFromParentProcess { get; set; }
 
-        public InArgument<bool> DoNotThrowExceptionOnRulesViolation { get; set; }
+		public InArgument<bool> DoNotThrowExceptionOnRulesViolation { get; set; }
 
-        public OutArgument<bool> ViolatedRulesExist { get; set; }
+		public OutArgument<bool> ViolatedRulesExist { get; set; }
 
+		public OutArgument<bool> ErrorMessages { get; set; }
 
-        protected override void Execute(CodeActivityContext context)
-        {
-            IEnumerable<IRuleTarget> importedDataToValidate = this.ImportedDataToValidate.Get(context);
-            string businessRulesKey = this.BusinessRulesKey.Get(context);
+		protected override void Execute(CodeActivityContext context)
+		{
+			List<BPValidationMessage> errorMessages = null;
+			IEnumerable<IRuleTarget> importedDataToValidate = this.ImportedDataToValidate.Get(context);
+			string businessRulesKey = this.BusinessRulesKey.Get(context);
 
-            bool writeMessagesToParentProcess = this.WriteMessagesToParentProcess.Get(context);
-            string parentMessagePrefix = this.ParentMessagePrefix.Get(context);
+			bool writeMessagesToParentProcess = this.WriteMessagesToParentProcess.Get(context);
+			string parentMessagePrefix = this.ParentMessagePrefix.Get(context);
 
-            List<BPViolatedRule> violatedBusinessRulesByTarget = new List<BPViolatedRule>(); ;
-            bool stopExecutionFlag;
+			List<BPViolatedRule> violatedBusinessRulesByTarget = new List<BPViolatedRule>(); ;
+			bool stopExecutionFlag;
 
-            bool getRulesFromParentProcess = this.GetRulesFromParentProcess.Get(context);
-            bool doNotThrowExceptionOnRulesViolation = this.DoNotThrowExceptionOnRulesViolation.Get(context);
+			bool getRulesFromParentProcess = this.GetRulesFromParentProcess.Get(context);
+			bool doNotThrowExceptionOnRulesViolation = this.DoNotThrowExceptionOnRulesViolation.Get(context);
 
-            BPBusinessRuleDefinitionManager bpBusinessRuleManager = new BPBusinessRuleDefinitionManager();
-            List<BPBusinessRuleDefinition> bpBusinessRules = new List<BPBusinessRuleDefinition>();
-            Guid rulesDefinitionId;
+			BPBusinessRuleDefinitionManager bpBusinessRuleManager = new BPBusinessRuleDefinitionManager();
+			List<BPBusinessRuleDefinition> bpBusinessRules = new List<BPBusinessRuleDefinition>();
+			Guid rulesDefinitionId;
 
-            if (getRulesFromParentProcess)
-            {
-                var parentProcessId = context.GetSharedInstanceData().InstanceInfo.ParentProcessID;
-                var bpInstanceManager = new BPInstanceManager();
-                if (!parentProcessId.HasValue)
-                    throw new ArgumentNullException("ParentProcessID not found");
+			if (getRulesFromParentProcess)
+			{
+				var parentProcessId = context.GetSharedInstanceData().InstanceInfo.ParentProcessID;
+				var bpInstanceManager = new BPInstanceManager();
+				if (!parentProcessId.HasValue)
+					throw new ArgumentNullException("ParentProcessID not found");
 
-                var parentBPInstance = bpInstanceManager.GetBPInstance(parentProcessId.Value);
-                parentBPInstance.ThrowIfNull("Parent BPInstance not found");
+				var parentBPInstance = bpInstanceManager.GetBPInstance(parentProcessId.Value);
+				parentBPInstance.ThrowIfNull("Parent BPInstance not found");
 
-                rulesDefinitionId = parentBPInstance.DefinitionID;
-            }
-            else
-                rulesDefinitionId = context.GetSharedInstanceData().InstanceInfo.DefinitionID;
+				rulesDefinitionId = parentBPInstance.DefinitionID;
+			}
+			else
+				rulesDefinitionId = context.GetSharedInstanceData().InstanceInfo.DefinitionID;
 
-            bpBusinessRules = bpBusinessRuleManager.GetBPBusinessRuleDefinitions(businessRulesKey, rulesDefinitionId);
+			bpBusinessRules = bpBusinessRuleManager.GetBPBusinessRuleDefinitions(businessRulesKey, rulesDefinitionId);
 
-            IEnumerable<BusinessRule> rules = BuildBusinessRules(bpBusinessRules);
-            ExecuteValidation(rules, importedDataToValidate, context, violatedBusinessRulesByTarget, out stopExecutionFlag);
-            AppendValidationMessages(context, violatedBusinessRulesByTarget, writeMessagesToParentProcess, parentMessagePrefix);
+			IEnumerable<BusinessRule> rules = BuildBusinessRules(bpBusinessRules);
+			ExecuteValidation(rules, importedDataToValidate, context, violatedBusinessRulesByTarget, out stopExecutionFlag);
+			AppendValidationMessages(context, violatedBusinessRulesByTarget, writeMessagesToParentProcess, parentMessagePrefix, out errorMessages);
 
-            if (stopExecutionFlag)
-            {
-                this.ViolatedRulesExist.Set(context, true);
-                if (!doNotThrowExceptionOnRulesViolation)
-                    throw new VRBusinessException("One or more business rules were not satisfied and led to stop the execution of the worklfow");
-            }
-        }
+			if (stopExecutionFlag)
+			{
+				this.ViolatedRulesExist.Set(context, true);
+				this.ErrorMessages.Set(context, errorMessages);
+				if (!doNotThrowExceptionOnRulesViolation)
+					throw new VRBusinessException("One or more business rules were not satisfied and led to stop the execution of the worklfow");
+			}
+		}
 
-        private IEnumerable<BusinessRule> BuildBusinessRules(List<BPBusinessRuleDefinition> bpBusinessRules)
-        {
-            if (bpBusinessRules == null)
-                return null;
-            List<BusinessRule> rules = new List<BusinessRule>();
-            BPBusinessRuleActionManager bpRuleActionManager = new BPBusinessRuleActionManager();
+		private IEnumerable<BusinessRule> BuildBusinessRules(List<BPBusinessRuleDefinition> bpBusinessRules)
+		{
+			if (bpBusinessRules == null)
+				return null;
+			List<BusinessRule> rules = new List<BusinessRule>();
+			BPBusinessRuleActionManager bpRuleActionManager = new BPBusinessRuleActionManager();
 
-            foreach (BPBusinessRuleDefinition bpBusinessRule in bpBusinessRules)
-            {
-                BPBusinessRuleAction action = bpRuleActionManager.GetBusinessRuleAction(bpBusinessRule.BPBusinessRuleDefinitionId);
-                BusinessRule rule = new BusinessRule()
-                {
-                    BPBusinessRuleDefinitionId = bpBusinessRule.BPBusinessRuleDefinitionId,
-                    Condition = bpBusinessRule.Settings.Condition,
-                    Action = action.Details.Settings.Action,
-                    ExecutionDependsOnRules = bpBusinessRule.Settings.ExecutionDependsOnRules
-                };
-                rules.Add(rule);
-            }
-            return rules;
-        }
+			foreach (BPBusinessRuleDefinition bpBusinessRule in bpBusinessRules)
+			{
+				BPBusinessRuleAction action = bpRuleActionManager.GetBusinessRuleAction(bpBusinessRule.BPBusinessRuleDefinitionId);
+				BusinessRule rule = new BusinessRule()
+				{
+					BPBusinessRuleDefinitionId = bpBusinessRule.BPBusinessRuleDefinitionId,
+					Condition = bpBusinessRule.Settings.Condition,
+					Action = action.Details.Settings.Action,
+					ExecutionDependsOnRules = bpBusinessRule.Settings.ExecutionDependsOnRules
+				};
+				rules.Add(rule);
+			}
+			return rules;
+		}
 
-        private void ExecuteValidation(IEnumerable<BusinessRule> rules, IEnumerable<IRuleTarget> targets, ActivityContext activityContext, List<BPViolatedRule> violatedBusinessRulesByTarget, out bool stopExecutionFlag)
-        {
-            stopExecutionFlag = false;
-            List<Guid> violatedRulesThatStopsExecution = new List<Guid>();
-            foreach (BusinessRule rule in rules)
-            {
-                if (violatedRulesThatStopsExecution.Count > 0 &&
-                    rule.ExecutionDependsOnRules != null &&
-                    rule.ExecutionDependsOnRules.Exists(ruleDefinitionId => violatedRulesThatStopsExecution.Contains(ruleDefinitionId)))
-                {
-                    violatedRulesThatStopsExecution.Add(rule.BPBusinessRuleDefinitionId);
-                    continue;
-                }
+		private void ExecuteValidation(IEnumerable<BusinessRule> rules, IEnumerable<IRuleTarget> targets, ActivityContext activityContext, List<BPViolatedRule> violatedBusinessRulesByTarget, out bool stopExecutionFlag)
+		{
+			stopExecutionFlag = false;
+			List<Guid> violatedRulesThatStopsExecution = new List<Guid>();
+			foreach (BusinessRule rule in rules)
+			{
+				if (violatedRulesThatStopsExecution.Count > 0 &&
+					rule.ExecutionDependsOnRules != null &&
+					rule.ExecutionDependsOnRules.Exists(ruleDefinitionId => violatedRulesThatStopsExecution.Contains(ruleDefinitionId)))
+				{
+					violatedRulesThatStopsExecution.Add(rule.BPBusinessRuleDefinitionId);
+					continue;
+				}
 
-                foreach (IRuleTarget target in targets)
-                {
-                    if (!rule.Condition.ShouldValidate(target))
-                        continue;
+				foreach (IRuleTarget target in targets)
+				{
+					if (!rule.Condition.ShouldValidate(target))
+						continue;
 
-                    IBusinessRuleConditionValidateContext validationContext = new BusinessRuleConditionValidateContext(activityContext) { Target = target };
-                    if (rule.Condition.Validate(validationContext))
-                        continue;
+					IBusinessRuleConditionValidateContext validationContext = new BusinessRuleConditionValidateContext(activityContext) { Target = target };
+					if (rule.Condition.Validate(validationContext))
+						continue;
 
-                    BusinessRuleActionExecutionContext actionContext = new BusinessRuleActionExecutionContext() { Target = target };
-                    rule.Action.Execute(actionContext);
-                    violatedBusinessRulesByTarget.Add(new BPViolatedRule() { Target = target, Rule = rule, Message = validationContext.Message });
-                    if (actionContext.StopExecution)
-                    {
-                        stopExecutionFlag = true;
-                    }
-                }
-                if (stopExecutionFlag == true)
-                    violatedRulesThatStopsExecution.Add(rule.BPBusinessRuleDefinitionId);
-            }
-        }
+					BusinessRuleActionExecutionContext actionContext = new BusinessRuleActionExecutionContext() { Target = target };
+					rule.Action.Execute(actionContext);
+					violatedBusinessRulesByTarget.Add(new BPViolatedRule() { Target = target, Rule = rule, Message = validationContext.Message });
+					if (actionContext.StopExecution)
+					{
+						stopExecutionFlag = true;
+					}
+				}
+				if (stopExecutionFlag == true)
+					violatedRulesThatStopsExecution.Add(rule.BPBusinessRuleDefinitionId);
+			}
+		}
 
-        private void AppendValidationMessages(CodeActivityContext context, List<BPViolatedRule> violatedBusinessRulesByTarget, bool writeMessagesToParentProcess, string parentMessagePrefix)
-        {
-            long processIntanceId = context.GetSharedInstanceData().InstanceInfo.ProcessInstanceID;
-            long? parentProcessId = context.GetSharedInstanceData().InstanceInfo.ParentProcessID;
+		private void AppendValidationMessages(CodeActivityContext context, List<BPViolatedRule> violatedBusinessRulesByTarget, bool writeMessagesToParentProcess, string parentMessagePrefix, out List<BPValidationMessage> errorMessages)
+		{
+			errorMessages = new List<BPValidationMessage>();
+			long processIntanceId = context.GetSharedInstanceData().InstanceInfo.ProcessInstanceID;
+			long? parentProcessId = context.GetSharedInstanceData().InstanceInfo.ParentProcessID;
 
-            violatedBusinessRulesByTarget.Sort((x, y) => x.CompareTo(y));
+			violatedBusinessRulesByTarget.Sort((x, y) => x.CompareTo(y));
 
-            List<BPValidationMessage> messages = new List<BPValidationMessage>();
-            foreach (BPViolatedRule violatedRule in violatedBusinessRulesByTarget)
-            {
-                BPValidationMessage msg = new BPValidationMessage()
-                {
-                    ProcessInstanceId = processIntanceId,
-                    ParentProcessId = parentProcessId,
-                    TargetKey = violatedRule.Target.Key,
-                    TargetType = violatedRule.Target.TargetType,
-                    Severity = violatedRule.Rule.Action.GetSeverity(),
-                    Message = violatedRule.Message
-                };
+			List<BPValidationMessage> messages = new List<BPValidationMessage>();
+			foreach (BPViolatedRule violatedRule in violatedBusinessRulesByTarget)
+			{
+				BPValidationMessage msg = new BPValidationMessage()
+				{
+					ProcessInstanceId = processIntanceId,
+					ParentProcessId = parentProcessId,
+					TargetKey = violatedRule.Target.Key,
+					TargetType = violatedRule.Target.TargetType,
+					Severity = violatedRule.Rule.Action.GetSeverity(),
+					Message = violatedRule.Message
+				};
 
-                messages.Add(msg);
+				messages.Add(msg);
+				if (msg.Severity == ActionSeverity.Error)
+					errorMessages.Add(msg);
 
-                if (writeMessagesToParentProcess)
-                    context.WriteTrackingMessageToParentProcess(MapSeverity(msg.Severity), string.Concat(parentMessagePrefix, msg.Message));
+				if (writeMessagesToParentProcess)
+					context.WriteTrackingMessageToParentProcess(MapSeverity(msg.Severity), string.Concat(parentMessagePrefix, msg.Message));
 
-                context.WriteTrackingMessage(MapSeverity(msg.Severity), msg.Message);
-            }
+				context.WriteTrackingMessage(MapSeverity(msg.Severity), msg.Message);
+			}
 
-            BPValidationMessageManager manager = new BPValidationMessageManager();
-            manager.Insert(messages);
-        }
+			BPValidationMessageManager manager = new BPValidationMessageManager();
+			manager.Insert(messages);
+		}
 
-        private LogEntryType MapSeverity(ActionSeverity actionSeverity)
-        {
-            switch (actionSeverity)
-            {
-                case ActionSeverity.Information:
-                    return LogEntryType.Information;
-                case ActionSeverity.Warning:
-                    return LogEntryType.Warning;
-                case ActionSeverity.Error:
-                    return LogEntryType.Error;
-                default:
-                    return LogEntryType.Verbose;
-            }
-        }
-    }
+		private LogEntryType MapSeverity(ActionSeverity actionSeverity)
+		{
+			switch (actionSeverity)
+			{
+				case ActionSeverity.Information:
+					return LogEntryType.Information;
+				case ActionSeverity.Warning:
+					return LogEntryType.Warning;
+				case ActionSeverity.Error:
+					return LogEntryType.Error;
+				default:
+					return LogEntryType.Verbose;
+			}
+		}
+	}
 }
