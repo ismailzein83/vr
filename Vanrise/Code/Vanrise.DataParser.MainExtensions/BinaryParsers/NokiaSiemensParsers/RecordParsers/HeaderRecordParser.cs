cdb71431c00e90fd7427922fbbc98303175f3407
Ali Atoui <VANRISE\ali.atoui>
2018-06-25 08:02:38 +0000

    Editing HeaderRecordParser
    git-tfs-id: [http://developmentvlan:8585/tfs/vanrise.collection]$/;C47668

--- Vanrise/Code/Vanrise.DataParser.MainExtensions/BinaryParsers/NokiaSiemensParsers/RecordParsers/HeaderRecordParser.cs
+++ Vanrise/Code/Vanrise.DataParser.MainExtensions/BinaryParsers/NokiaSiemensParsers/RecordParsers/HeaderRecordParser.cs
@@ -9,13 +9,13 @@ namespace Vanrise.DataParser.MainExtensions.BinaryParsers.NokiaSiemensParsers.Re
     {
         public override Guid ConfigId { get { return new Guid("67B49D8E-0285-4926-9AFB-8A63F6AF8DD6"); } }
         public int HeaderLength { get; set; }
-        public int RecordLengthIndex { get; set; }
+        public int HeaderTagLength { get; set; }
         public int RecordLengthByteLength { get; set; }
         public BinaryRecordParser PackageRecordParser { get; set; }
 
         public override void Execute(IBinaryRecordParserContext context)
         {
-            BinaryParserHelper.ReadBlockFromStream(context.RecordStream, 1, (headerTag) =>
+            BinaryParserHelper.ReadBlockFromStream(context.RecordStream, HeaderTagLength, (headerTag) =>
             {
                 int headerTagAsInt = ParserHelper.GetInt(headerTag.Value, 0, 1);
                 switch (headerTagAsInt)
@@ -27,19 +27,22 @@ namespace Vanrise.DataParser.MainExtensions.BinaryParsers.NokiaSiemensParsers.Re
                     case 128: break;
 
                     case 129:
-                        BinaryParserHelper.ReadBlockFromStream(context.RecordStream, 1, (recordByteLength) =>
+                        BinaryParserHelper.ReadBlockFromStream(context.RecordStream, RecordLengthByteLength, (recordByteLength) =>
                         {
-                            int recordLength = ParserHelper.GetInt(recordByteLength.Value, 0, 1);
-                            BinaryParserHelper.ReadBlockFromStream(context.RecordStream, recordLength - 2, (record) =>
+                            int recordLength = ParserHelper.HexToInt32(ParserHelper.ByteArrayToString(recordByteLength.Value, true));
+                            
+                            BinaryParserHelper.ReadBlockFromStream(context.RecordStream, recordLength - (HeaderTagLength + RecordLengthByteLength), (record) =>
                             {
                             }, true);
                         }, true);
                         break;
 
                     case 132:
-                        BinaryParserHelper.ReadBlockFromStream(context.RecordStream, HeaderLength - 1, (headerWithoutTag) =>
+                        BinaryParserHelper.ReadBlockFromStream(context.RecordStream, HeaderLength - HeaderTagLength, (headerWithoutTag) =>
                         {
-                            int recordLength = ParserHelper.GetInt(headerWithoutTag.Value, 0, RecordLengthByteLength);
+                            byte[] bytes = new byte[RecordLengthByteLength];
+                            Array.Copy(headerWithoutTag.Value, 0, bytes, 0, RecordLengthByteLength);
+                            int recordLength = ParserHelper.HexToInt32(ParserHelper.ByteArrayToString(bytes, true));
 
                             BinaryParserHelper.ReadBlockFromStream(context.RecordStream, recordLength - HeaderLength, (record) =>
                             {
