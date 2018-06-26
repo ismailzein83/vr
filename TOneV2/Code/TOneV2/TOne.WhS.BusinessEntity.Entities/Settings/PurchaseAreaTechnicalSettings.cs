caa0e58e6ee8e90ed56fdc6d473c77614e2c2725
Ali Sakr <VANRISE\ali.sakr>
2018-06-26 08:05:27 +0000

    Adding purchase area technical
    git-tfs-id: [http://developmentvlan:8585/tfs/vanrise.collection]$/;C47689

--- TOneV2/Code/TOneV2/TOne.WhS.BusinessEntity.Entities/Settings/PurchaseAreaTechnicalSettings.cs
+++ TOneV2/Code/TOneV2/TOne.WhS.BusinessEntity.Entities/Settings/PurchaseAreaTechnicalSettings.cs
@@ -3,10 +3,17 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Vanrise.Entities;
 
 namespace TOne.WhS.BusinessEntity.Entities
 {
-    class PurchaseAreaTechnicalSettings
+    public class PurchaseAreaTechnicalSettings : SettingData
     {
+        public PurchaseAreaTechnicalSettingsData PurchaseAreaSettingsData { get; set; }
     }
+    public class PurchaseAreaTechnicalSettingsData
+    {
+        public bool IsAutoImportActivated { get; set; }
+    }
+    
 }
