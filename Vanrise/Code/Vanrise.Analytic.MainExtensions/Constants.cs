230320b127c64156ed4afe6f43b8e045fe7634d6
yara.bousalman <yara.bousalman@vanrise.tfs.local>
2018-06-27 10:11:10 +0000

    Adding Constants class and references to Vanrise.Analytic.MainExtensions
    git-tfs-id: [http://developmentvlan:8585/tfs/vanrise.collection]$/;C47704

---
+++ Vanrise/Code/Vanrise.Analytic.MainExtensions/Constants.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Vanrise.Analytic.MainExtensions
+{
+    public class Constants
+    {
+        public const string ROUTE_PREFIX = "api/VR_Analytic/";
+    }
+}
