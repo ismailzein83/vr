a106c73c279d803b57f270cd27e528811baa8d0c
Wissam Ajamy <VANRISE\wissam.ajamy>
2018-06-28 15:12:40 +0000

    Move modules from BundleConfig to Web_ExtendedModulesNames
    git-tfs-id: [http://developmentvlan:8585/tfs/vanrise.collection]$/;C47759

--- TOneV2/Code/TOneV2/TOne.Web/App_Start/BundleConfig.cs
+++ TOneV2/Code/TOneV2/TOne.Web/App_Start/BundleConfig.cs
@@ -11,6 +11,30 @@ namespace TOne.Web
             Vanrise.Web.BundleConfig.RegisterBundles(bundles);
             var modulesJSBundle = Vanrise.Web.BundleConfig.CreateModulesScriptBundle().IncludeDirectory(
                 "~/Client/Modules", "*.js", true);
+                //"~/Client/Modules/Common", "*.js", true)
+                //.IncludeDirectory(
+                //"~/Client/Modules/Security", "*.js", true).IncludeDirectory(
+                //"~/Client/Modules/Runtime", "*.js", true).IncludeDirectory(
+                //"~/Client/Modules/Integration", "*.js", true).IncludeDirectory(
+                //"~/Client/Modules/BusinessProcess", "*.js", true).IncludeDirectory(
+                //"~/Client/Modules/Queueing", "*.js", true).IncludeDirectory(
+                //"~/Client/Modules/WhS_CodePreparation", "*.js", true).IncludeDirectory(
+                //"~/Client/Modules/WhS_BusinessEntity", "*.js", true).IncludeDirectory(
+                //"~/Client/Modules/WhS_SupplierPriceList", "*.js", true).IncludeDirectory(
+                //"~/Client/Modules/WhS_Sales", "*.js", true).IncludeDirectory(
+                //"~/Client/Modules/WhS_Routing", "*.js", true).IncludeDirectory(
+                //"~/Client/Modules/VR_Rules", "*.js", true).IncludeDirectory(
+                //"~/Client/Modules/WhS_Analytics", "*.js", true).IncludeDirectory(
+                //"~/Client/Modules/VR_GenericData", "*.js", true).IncludeDirectory(
+                //"~/Client/Modules/Analytic", "*.js", true).IncludeDirectory(
+                //"~/Client/Modules/ExcelConversion", "*.js", true).IncludeDirectory(
+                //"~/Client/Modules/Reprocess", "*.js", true).IncludeDirectory(
+                //"~/Client/Modules/WhS_RouteSync", "*.js", true).IncludeDirectory(
+                //"~/Client/Modules/VR_Invoice", "*.js", true).IncludeDirectory(
+                //"~/Client/Modules/WhS_Invoice", "*.js", true).IncludeDirectory(
+                //"~/Client/Modules/VR_Notification", "*.js", true).IncludeDirectory(
+                //"~/Client/Modules/WhS_Deal", "*.js", true).IncludeDirectory(
+                //"~/Client/Modules/VR_BEBridge", "*.js", true);
 
             bundles.Add(modulesJSBundle);
         }
