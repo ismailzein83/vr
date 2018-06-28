e80bd75315f0230ad730eae11b5581b9d02914ed
Samer Haidar <VANRISE\samer.haidar>
2018-06-28 12:54:48 +0000

    Adding new projects and references
    git-tfs-id: [http://developmentvlan:8585/tfs/vanrise.collection]$/;C47749

---
+++ Vanrise/Code/Vanrise.Voucher.Data/VoucherDataManagerFactory.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+using Vanrise.Common;
+
+namespace Vanrise.Voucher.Data
+{
+    public static class VoucherDataManagerFactory
+    {
+        static ObjectFactory s_objectFactory;
+        static InvoiceDataManagerFactory()
+        {
+            s_objectFactory = new ObjectFactory(Assembly.Load("Vanrise.Voucher.Data.SQL"));
+        }
+
+        public static T GetDataManager<T>() where T : class, IDataManager
+        {
+            return s_objectFactory.CreateObjectFromType<T>();
+        }
+    }
+}
