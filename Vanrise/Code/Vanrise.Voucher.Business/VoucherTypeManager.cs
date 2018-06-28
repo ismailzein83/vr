a6836cf77ce823bb4a27448e5e13b8087d062a68
Mohamad Abdallah <VANRISE\mohamad.abdallah>
2018-06-28 09:30:12 +0000

    implement GetCachedVoucherTypes() method
    git-tfs-id: [http://developmentvlan:8585/tfs/vanrise.collection]$/;C47732

--- Vanrise/Code/Vanrise.Voucher.Business/VoucherTypeManager.cs
+++ Vanrise/Code/Vanrise.Voucher.Business/VoucherTypeManager.cs
@@ -5,6 +5,8 @@ using System.Text;
 using System.Threading.Tasks;
 using Vanrise.Voucher.Entities;
 using Vanrise.Common;
+using Vanrise.GenericData.Business;
+using Vanrise.GenericData.Entities;
 namespace Vanrise.Voucher.Business
 {
     public class VoucherTypeManager
@@ -17,7 +19,39 @@ namespace Vanrise.Voucher.Business
         }
         private Dictionary<long,VoucherType> GetCachedVoucherTypes()
         {
-            throw new NotImplementedException();
+            GenericBusinessEntityManager genericBusinessEntityManager = new GenericBusinessEntityManager();
+            return genericBusinessEntityManager.GetCachedOrCreate("GetCachedSecurityProviders", _definitionId, () =>
+            {
+                List<GenericBusinessEntity> genericBusinessEntities = genericBusinessEntityManager.GetAllGenericBusinessEntities(_definitionId);
+                Dictionary<long, VoucherType> result = new Dictionary<long, VoucherType>();
+
+                if (genericBusinessEntities != null)
+                {
+                    foreach (GenericBusinessEntity genericBusinessEntity in genericBusinessEntities)
+                    {
+                        if (genericBusinessEntity.FieldValues == null)
+                            continue;
+
+                        VoucherType voucherType = new VoucherType()
+                        {
+                            VoucherTypeId = (long)genericBusinessEntity.FieldValues.GetRecord("VoucherTypeId"),
+                            Name = (string)genericBusinessEntity.FieldValues.GetRecord("Name"),
+                            Amount = (decimal)genericBusinessEntity.FieldValues.GetRecord("Amount"),
+                            CurrencyId = (int)genericBusinessEntity.FieldValues.GetRecord("CurrencyId"),
+                            Description = (string)genericBusinessEntity.FieldValues.GetRecord("Description"),
+                            Active = (bool)genericBusinessEntity.FieldValues.GetRecord("Active") ,
+                            CreatedTime = (DateTime)genericBusinessEntity.FieldValues.GetRecord("CreatedTime"),
+                            CreatedBy = (int)genericBusinessEntity.FieldValues.GetRecord("CreatedBy"),
+                            LastModifiedTime = (DateTime)genericBusinessEntity.FieldValues.GetRecord("LastModifiedTime"),
+                            LastModifiedBy = (int)genericBusinessEntity.FieldValues.GetRecord("LastModifiedBy"),
+
+                        };
+                        result.Add(voucherType.VoucherTypeId, voucherType);
+                    }
+                }
+
+                return result;
+            });
         }
 
     }
