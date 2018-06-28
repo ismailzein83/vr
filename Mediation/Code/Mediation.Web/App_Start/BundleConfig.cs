fee7f646cf2289c03c116ce2743c203760d79e89
Wissam Ajamy <VANRISE\wissam.ajamy>
2018-06-28 15:24:40 +0000

    Move modules from BundleConfig to Web_ExtendedModulesNames
    git-tfs-id: [http://developmentvlan:8585/tfs/vanrise.collection]$/;C47760

--- Mediation/Code/Mediation.Web/App_Start/BundleConfig.cs
+++ Mediation/Code/Mediation.Web/App_Start/BundleConfig.cs
@@ -10,15 +10,7 @@ namespace Mediation.Web
         {
             Vanrise.Web.BundleConfig.RegisterBundles(bundles);
             var modulesJSBundle = Vanrise.Web.BundleConfig.CreateModulesScriptBundle().IncludeDirectory(
-                "~/Client/Modules/Common", "*.js", true).IncludeDirectory(
-                "~/Client/Modules/Security", "*.js", true).IncludeDirectory(
-                "~/Client/Modules/Runtime", "*.js", true).IncludeDirectory(
-                "~/Client/Modules/Integration", "*.js", true).IncludeDirectory(
-                "~/Client/Modules/BusinessProcess", "*.js", true).IncludeDirectory(
-                "~/Client/Modules/Queueing", "*.js", true).IncludeDirectory(
-                "~/Client/Modules/VR_Rules", "*.js", true).IncludeDirectory(
-                "~/Client/Modules/VR_GenericData", "*.js", true).IncludeDirectory(
-                "~/Client/Modules/Mediation_Generic", "*.js", true);
+                "~/Client/Modules", "*.js", true);
             bundles.Add(modulesJSBundle);
         }
     }
