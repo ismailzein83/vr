2b972adb9470b27788a3b83ac2228a0dc208c4f3
yara.bousalman <yara.bousalman@vanrise.tfs.local>
2018-06-29 07:42:30 +0000

    Adding serial number parts implementations in Vanrise.Analytic.MainExtensions
    git-tfs-id: [http://developmentvlan:8585/tfs/vanrise.collection]$/;C47768

---
+++ Vanrise/Code/Vanrise.Analytic.MainExtensions/AutomatedReport/Handlers/VRAutomatedReportDateSerialNumberPart.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Vanrise.Analytic.Entities.AutomatedReport.Handler;
+using Vanrise.Entities;
+
+namespace Vanrise.Analytic.MainExtensions.AutomatedReport.Handlers
+{
+    public class VRAutomatedReportDateSerialNumberPart : VRConcatenatedPartSettings<IVRAutomatedReportSerialNumberPartConcatenatedPartContext>
+    {
+        public override Guid ConfigId { get { return new Guid("A194AAC8-0675-4100-8A8B-1FBE4105FE09"); } }
+        public string DateFormat { get; set; }
+        public override string GetPartText(IVRAutomatedReportSerialNumberPartConcatenatedPartContext context)
+        {
+            var date = DateTime.Now;
+            if (!String.IsNullOrEmpty(this.DateFormat))
+            {
+                return date.ToString(this.DateFormat);
+            }
+            return date.ToString();
+        }
+    }
+}
---
+++ Vanrise/Code/Vanrise.Analytic.MainExtensions/AutomatedReport/Handlers/VRAutomatedReportSequenceSerialNumberPart.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Vanrise.Analytic.Business;
+using Vanrise.Analytic.Entities.AutomatedReport.Handler;
+using Vanrise.Common.Business;
+using Vanrise.Entities;
+
+namespace Vanrise.Analytic.MainExtensions.AutomatedReport.Handlers
+{
+    public enum DateCounterType { Yearly = 0 }
+    public class VRAutomatedReportSequenceSerialNumberPart : VRConcatenatedPartSettings<IVRAutomatedReportSerialNumberPartConcatenatedPartContext>
+    {
+        public override Guid ConfigId { get { return new Guid("9CC73443-2A1A-4405-A1ED-1DE27B9DCB42"); } }
+        public DateCounterType? DateCounterType { get; set; }
+        public int PaddingLeft { get; set; }
+        public override string GetPartText(IVRAutomatedReportSerialNumberPartConcatenatedPartContext context)
+        {
+            StringBuilder sequenceKey = new StringBuilder();
+            StringBuilder sequenceGroup = new StringBuilder();
+            sequenceGroup.Append("OVERALL");
+
+            long initialSequenceValue = new Vanrise.Analytic.Business.ConfigManager().GetSerialNumberPartInitialSequence();
+
+            if (this.DateCounterType.HasValue)
+            {
+                if (sequenceKey.Length > 0)
+                    sequenceKey.Append("_");
+                sequenceGroup.Append("_");
+                sequenceGroup.Append(Common.Utilities.GetEnumDescription(this.DateCounterType.Value));
+                switch (this.DateCounterType)
+                {
+                    case Vanrise.Analytic.MainExtensions.AutomatedReport.Handlers.DateCounterType.Yearly:
+                        sequenceKey.Append(string.Format("{0}_{1}", DateTime.Today.Year, DateTime.Today.Year + 1));
+                        break;
+                }
+            }
+            VRSequenceManager manager = new VRSequenceManager();
+            Guid sequenceid = new Guid();
+            //fix sequence id
+            var sequenceNumber = manager.GetNextSequenceValue(sequenceGroup.ToString(), sequenceid, sequenceKey.ToString(), initialSequenceValue);
+            return sequenceNumber.ToString().PadLeft(this.PaddingLeft, '0');
+        }
+
+    }
+}
