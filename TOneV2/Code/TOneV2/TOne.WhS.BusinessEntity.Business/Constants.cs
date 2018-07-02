1c7b7d77849da8af897d530f0993b86cf8106bdf
Ali Sakr <VANRISE\ali.sakr>
2018-07-02 11:55:44 +0000

    Fixing issue
    git-tfs-id: [http://developmentvlan:8585/tfs/vanrise.collection]$/;C47809

--- TOneV2/Code/TOneV2/TOne.WhS.BusinessEntity.Business/Constants.cs
+++ TOneV2/Code/TOneV2/TOne.WhS.BusinessEntity.Business/Constants.cs
@@ -6,6 +6,7 @@
         public const string SaleAreaTechnicalSettings = "WhS_BE_SaleArea_TechnicalSettings";
 
         public const string PurchaseAreaSettings = "WhS_BE_PurchaseArea_Settings";
+        public const string PurchaseAreaTechnicalSettings = "WhS_BE_PurchaseArea_TechnicalSettings";
 
         public const string CDRImportSettings = "Whs_BE_CDRImport_Settings";
 
@@ -13,7 +14,9 @@
 
         public const string BusinessEntitySettingsData = "WhS_BE_Settings";
 
-        public const string FaultTicketsSettingsData = "WhS_BE_FaultTickets_Settings";
+        public const string AutoImportSettings = "WhS_BE_AutoImport_Settings";
 
+        public const string FaultTicketsSettingsData = "WhS_BE_FaultTickets_Settings";
+     
     }
 }
