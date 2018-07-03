8de4d45fb6fa52216b22c1400e2077e805956d0c
Samer Haidar <VANRISE\samer.haidar>
2018-07-03 07:25:12 +0000

    Sync for new modifications
    git-tfs-id: [http://developmentvlan:8585/tfs/vanrise.collection]$/;C47820

---
+++ TOneV2/Code/TOneV2/SQL.TOneConfiguration/common/Stored Procedures/sp_DataEncryptionKey_InsertIfNotExistsAndGetDataEncryptionKey.sql
@@ -0,0 +1,15 @@
+﻿create PROCEDURE [Common].[sp_DataEncryptionKey_InsertIfNotExistsAndGetDataEncryptionKey]
+	@EncryptionKey nvarchar(255)
+AS
+BEGIN	
+	
+	--INSERT Encryption Key if not exists
+	IF NOT EXISTS (SELECT TOP 1 NULL FROM [Common].DataEncryptionKey WITH(NOLOCK))
+	BEGIN
+		INSERT INTO [Common].DataEncryptionKey ([EncryptionKey])
+		SELECT @EncryptionKey WHERE NOT EXISTS (SELECT TOP 1 NULL FROM [Common].DataEncryptionKey)
+	END
+	
+	SELECT TOP 1 EncryptionKey FROM [Common].DataEncryptionKey
+	ORDER BY ID
+END
---
+++ TOneV2/Code/TOneV2/SQL.TOneConfiguration/common/Tables/DataEncryptionKey.sql
@@ -0,0 +1,6 @@
+﻿CREATE TABLE [common].[DataEncryptionKey] (
+    [ID]            INT            IDENTITY (1, 1) NOT NULL,
+    [EncryptionKey] NVARCHAR (255) NULL,
+    CONSTRAINT [PK_DataEncryptionKey] PRIMARY KEY CLUSTERED ([ID] ASC)
+);
+
