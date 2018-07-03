ee5bf011b9083b92a43de45df7dfa2ac28449fa2
Ali Atoui <VANRISE\ali.atoui>
2018-07-03 06:03:42 +0000

    Editing NokiaSiemens DBStructure
    git-tfs-id: [http://developmentvlan:8585/tfs/vanrise.collection]$/;C47816

--- Mediation/Code/Mediation.CDR/Mediation_ICX/Tables/NokiaSiemensCDR.sql
+++ Mediation/Code/Mediation.CDR/Mediation_ICX/Tables/NokiaSiemensCDR.sql
@@ -5,16 +5,25 @@
     [DurationInSeconds]        INT              NULL,
     [CallingPartyNumber]       VARCHAR (50)     NULL,
     [CalledPartyNumber]        VARCHAR (50)     NULL,
+    [CallingNADI]              INT              NULL,
+    [CalledNADI]               INT              NULL,
+    [CallingNPI]               INT              NULL,
+    [CalledNPI]                INT              NULL,
     [IncomingTrunkGroupNumber] VARCHAR (50)     NULL,
     [IncomingTrunkNumber]      INT              NULL,
     [OutgoingTrunkGroupNumber] VARCHAR (50)     NULL,
     [OutgoingTrunkNumber]      INT              NULL,
     [CauseValue]               INT              NULL,
     [Zone]                     INT              NULL,
+    [ConnectionIdentification] BIGINT           NULL,
+    [CodingStandard]           INT              NULL,
+    [Location]                 INT              NULL,
     [FileName]                 VARCHAR (255)    NULL,
     [DataSourceId]             UNIQUEIDENTIFIER NULL,
     CONSTRAINT [PK_NokiaSiemensCDR] PRIMARY KEY CLUSTERED ([Id] ASC)
 );
+
+
 
 
 
