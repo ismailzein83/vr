6cf9caa91ad90600177883c937f5f3e7246c8057
Ali Sakr <VANRISE\ali.sakr>
2018-07-03 09:04:50 +0000

    Added one new class to show effective rule action
    git-tfs-id: [http://developmentvlan:8585/tfs/vanrise.collection]$/;C47827

---
+++ BusinessProcess/Code/Vanrise.BusinessProcess.Entities/BusinessRules/BPBusinessRuleEffectiveAction.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Vanrise.BusinessProcess.Entities
+{
+    public class BPBusinessRuleEffectiveAction
+    {
+        public Guid RuleDefinitionId { get; set; }
+
+        public Guid ActionId { get; set; }
+
+        public bool IsOverriden { get; set; }
+    }
+}
