d0d470c1e711839ecfc95ea215ed8207db7536ff
Fatima Al Heck <VANRISE\fatima.alheck>
2018-07-05 11:37:20 +0000

    Adding GetRuntimeTypeDescription @FieldCustomObjectTypeSettings
    git-tfs-id: [http://developmentvlan:8585/tfs/vanrise.collection]$/;C47870

--- Retail/Code/Retail.BusinessEntity.Business/OperatorDeclarationServicesCustomObjectTypeSettings.cs
+++ Retail/Code/Retail.BusinessEntity.Business/OperatorDeclarationServicesCustomObjectTypeSettings.cs
@@ -1,36 +1,36 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using Retail.BusinessEntity.Entities;
+using System;
 using Vanrise.GenericData.Entities;
-using Retail.BusinessEntity.Entities;
-using Vanrise.Common;
 
 namespace Retail.BusinessEntity.Business
 {
     public class OperatorDeclarationServicesCustomObjectTypeSettings : FieldCustomObjectTypeSettings
     {
-        public override Guid ConfigId
-        {
-            get { return new Guid("DEDB89C0-370F-4DF8-BE63-EE60C73436F6"); }
-        }
+        public override Guid ConfigId { get { return new Guid("DEDB89C0-370F-4DF8-BE63-EE60C73436F6"); } }
+
         public override string GetDescription(IFieldCustomObjectTypeSettingsContext context)
         {
             return null;
         }
+
         public override bool AreEqual(Object newValue, Object oldValue)
         {
             return true;
         }
+
         public override Type GetNonNullableRuntimeType()
         {
             return typeof(OperatorDeclarationServices);
         }
+
         public override dynamic ParseNonNullValueToFieldType(object originalValue)
         {
             return originalValue as OperatorDeclarationServices;
         }
 
+        public override string GetRuntimeTypeDescription()
+        {
+            return "Operator Declaration Services";
+        }
     }
 }
