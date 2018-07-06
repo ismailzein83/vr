5e8e7bcfb6cd8f48adc8f8a9b65701c6698af951
anthony douaihy <VANRISE\anthony.douaihy>
2018-07-06 15:25:03 +0000

    add DBReplicationProcessInput
    git-tfs-id: [http://developmentvlan:8585/tfs/vanrise.collection]$/;C47915

---
+++ Vanrise/Code/Vanrise.Common.BP.Arguments/DBReplicationProcessInput.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using Vanrise.Entities;
+
+namespace Vanrise.Common.BP.Arguments
+{
+    public class DBReplicationProcessInput : Vanrise.BusinessProcess.Entities.BaseProcessInputArgument
+    {
+        public static Guid BPDefinitionId { get { return new Guid("29CFE0E9-A2B9-4692-B787-DC6F62B1C2A0"); } }
+
+        public DateTime FromTime { get; set; }
+
+        public DateTime ToTime { get; set; }
+
+        public DBReplicationSettings Settings { get; set; }
+
+        public override string GetTitle()
+        {
+            return String.Format("Database Replication from {0} to {1}", FromTime.ToString("yyyy-MM-dd HH:mm:ss"), ToTime.ToString("yyyy-MM-dd HH:mm:ss"));
+        }
+
+        public override void MapExpressionValues(Dictionary<string, object> evaluatedExpressions)
+        {
+            if (evaluatedExpressions.ContainsKey("ScheduleTime") && evaluatedExpressions.ContainsKey("DaysBack") && evaluatedExpressions.ContainsKey("NumberOfDays"))
+            {
+                var effectiveDate = (DateTime)evaluatedExpressions["ScheduleTime"];
+                this.FromTime = effectiveDate.Date.AddDays(-1 * int.Parse(evaluatedExpressions["DaysBack"].ToString()));
+                this.ToTime = this.FromTime.AddDays(int.Parse(evaluatedExpressions["NumberOfDays"].ToString()));
+            }
+        }
+    }
+}
