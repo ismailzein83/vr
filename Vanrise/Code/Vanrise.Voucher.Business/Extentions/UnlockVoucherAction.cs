43de7c7925dd1ccd4a9b7dbf166b3e053a3a4d72
Naim Saad <VANRISE\naim.saad>
2018-07-10 12:15:30 +0000

    Implementing Unlock Voucher Action
    git-tfs-id: [http://developmentvlan:8585/tfs/vanrise.collection]$/;C47955

---
+++ Vanrise/Code/Vanrise.Voucher.Business/Extentions/UnlockVoucherAction.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Vanrise.GenericData.Business;
+
+namespace Vanrise.Voucher.Business
+{
+    public class UnlockVoucherAction : GenericBEActionSettings
+    {
+        public override Guid ConfigId
+        {
+            get { return new Guid("71AD4B25-D3E7-47BF-BE5F-87623448263D"); }
+        }
+        public override string ActionTypeName { get { return "UnlockVoucher"; } }
+
+    }
+}
