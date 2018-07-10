138f9718e8f261a373fad0bb4772c739d1021aee
Mohammad Yahfoufi <VANRISE\mohammad.yahfoufi>
2018-07-10 09:51:26 +0000

    adding Task
    git-tfs-id: [http://developmentvlan:8585/tfs/vanrise.collection]$/;C47939

---
+++ TOneV2/Code/TOneV2/TestRuntime/Tasks/MohamadYahfoufiTask.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using Vanrise.BusinessProcess;
+using Vanrise.Caching.Runtime;
+using Vanrise.Queueing;
+using Vanrise.Runtime;
+using Vanrise.Runtime.Entities;
+
+namespace TestRuntime
+{
+    public class MohamadYahfoufiTask : ITask
+    {
+        public void Execute()
+        {
+            #region Runtime
+            var runtimeServices = new List<RuntimeService>();
+
+            BusinessProcessService bpService = new BusinessProcessService { Interval = new TimeSpan(0, 0, 2) };
+            runtimeServices.Add(bpService);
+
+            BPRegulatorRuntimeService bpRegulatorRuntimeService = new BPRegulatorRuntimeService { Interval = new TimeSpan(0, 0, 2) };
+            runtimeServices.Add(bpRegulatorRuntimeService);
+
+            //SchedulerService schedulerService = new SchedulerService { Interval = new TimeSpan(0, 0, 1) };
+            //runtimeServices.Add(schedulerService);
+
+            RuntimeHost host = new RuntimeHost(runtimeServices);
+            host.Start();
+            Console.ReadKey();
+            #endregion
+        }
+    }
+}
