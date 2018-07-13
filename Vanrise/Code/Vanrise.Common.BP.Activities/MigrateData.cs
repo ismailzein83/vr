f6432764320993faaeb257056df298926560cad7
anthony douaihy <VANRISE\anthony.douaihy>
2018-07-13 09:26:18 +0000

    add MigrateData activities to  Vanrise.Common.BP.Activities
    git-tfs-id: [http://developmentvlan:8585/tfs/vanrise.collection]$/;C48053

---
+++ Vanrise/Code/Vanrise.Common.BP.Activities/MigrateData.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Activities;
+using Vanrise.BusinessProcess;
+using Vanrise.Common.Data;
+using Vanrise.Entities;
+
+namespace Vanrise.Common.BP.Activities
+{
+    public sealed class MigrateData : BaseCodeActivity
+    {
+        [RequiredArgument]
+        public InArgument<DBReplicationSettings> DBReplicationSettings { get; set; }
+
+        [RequiredArgument]
+        public InArgument<DBReplicationDefinition> DBReplicatioDefinition { get; set; }
+
+        [RequiredArgument]
+        public InArgument<IDBReplicationDataManager> DBReplicationDataManager { get; set; }
+        
+        protected override void VRExecute(IBaseCodeActivityContext context)
+        {
+            //DBReplicationSettings dbReplicationSettings = this.DBReplicationSettings.Get(context.ActivityContext);
+            //DBReplicationDefinition dbReplicatioDefinition = this.DBReplicatioDefinition.Get(context.ActivityContext);
+            //IDBReplicationDataManager dbReplicationDataManager = this.DBReplicationDataManager.Get(context.ActivityContext);
+
+            //dbReplicationDataManager.MigrateData(new DBReplicationMigrateDataContext() { });
+
+        }
+    }
+}
