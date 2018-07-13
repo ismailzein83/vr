12f1464d1cd15b535d0eb89f274efcfd269967ce
anthony douaihy <VANRISE\anthony.douaihy>
2018-07-13 10:11:35 +0000

    add DBReplicationDefinitionManager
    git-tfs-id: [http://developmentvlan:8585/tfs/vanrise.collection]$/;C48059

---
+++ Vanrise/Code/Vanrise.Common.Business/DBReplicationDefinitionManager.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Vanrise.Entities;
+
+namespace Vanrise.Common.Business
+{
+    public class DBReplicationDefinitionManager
+    {
+        public DBReplicationDefinition GetDBReplicationDefinition(Guid dbReplicationDefinitionId)
+        {
+            var dbReplicationDefinitions = this.GetCachedDBReplicationDefinitions();
+            return dbReplicationDefinitions.FindRecord(x => x.VRComponentTypeId == dbReplicationDefinitionId);
+        }
+
+        private Dictionary<Guid, DBReplicationDefinition> GetCachedDBReplicationDefinitions()
+        {
+            VRComponentTypeManager vrComponentTypeManager = new Vanrise.Common.Business.VRComponentTypeManager();
+            return vrComponentTypeManager.GetCachedComponentTypes<DBReplicationDefinitionSettings, DBReplicationDefinition>();
+        }
+    }
+}
