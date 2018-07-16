fb98cdc48c7dbdca4d98a7495eaf5238be88fa71
Samer Haidar <VANRISE\samer.haidar>
2018-07-16 09:08:18 +0000

    Adding VRReport and IsEnabled
    git-tfs-id: [http://developmentvlan:8585/tfs/vanrise.collection]$/;C48104

---
+++ Vanrise/Code/Vanrise.Analytic.Entities/VRReport/VRReport.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Vanrise.Analytic.Entities
+{
+    public class VRReport
+    {
+        public long ReportId { get; set; }
+        public string Name { get; set; }
+        public VRReportSettings Settings { get; set; }
+    }
+    public class VRReportSettings
+    {
+        public List<VRAutomatedReportQuery> Queries { get; set; }
+        public VRReportAction Action { get; set; }
+    }
+    public abstract class VRReportAction
+    {
+        public string ActionName { get; set; }
+        public abstract Guid ConfigId { get; }
+    }
+}
