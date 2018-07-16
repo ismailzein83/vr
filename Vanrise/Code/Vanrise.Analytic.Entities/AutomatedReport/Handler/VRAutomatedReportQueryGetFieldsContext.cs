1b937d715a8041a3fa2124207b62e96bf239e747
yara.bousalman <yara.bousalman@vanrise.tfs.local>
2018-07-16 08:27:30 +0000

    Adding VRAutomatedReportQueryGetFieldsContext class to Vanrise.Analytic.Entities
    git-tfs-id: [http://developmentvlan:8585/tfs/vanrise.collection]$/;C48098

---
+++ Vanrise/Code/Vanrise.Analytic.Entities/AutomatedReport/Handler/VRAutomatedReportQueryGetFieldsContext.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Vanrise.Analytic.Entities;
+
+namespace Vanrise.Analytic.Entities
+{
+    public class VRAutomatedReportQueryGetFieldsContext : IVRAutomatedReportQueryGetFieldsContext
+    {
+        public Guid QueryDefinitionId { get; set; }
+    }
+}
