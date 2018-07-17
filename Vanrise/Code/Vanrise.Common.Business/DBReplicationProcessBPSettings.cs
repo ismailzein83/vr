fd5af1c49b9a9105c89708621c20a256580747e0
anthony douaihy <VANRISE\anthony.douaihy>
2018-07-17 11:32:13 +0000

    add DBReplicationProcessBPSettings
    git-tfs-id: [http://developmentvlan:8585/tfs/vanrise.collection]$/;C48129

---
+++ Vanrise/Code/Vanrise.Common.Business/DBReplicationProcessBPSettings.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using Vanrise.Common;
+using Vanrise.Common.BP.Arguments;
+
+namespace Vanrise.Common.Business
+{
+    public class DBReplicationProcessBPSettings : Vanrise.BusinessProcess.Business.DefaultBPDefinitionExtendedSettings
+    {
+        public override bool CanRunBPInstance(BusinessProcess.Entities.IBPDefinitionCanRunBPInstanceContext context)
+        {
+            context.IntanceToRun.ThrowIfNull("context.IntanceToRun");
+            DBReplicationProcessInput inputArg = context.IntanceToRun.InputArgument.CastWithValidate<DBReplicationProcessInput>("context.IntanceToRun.InputArgument");
+            foreach (var startedBPInstance in context.GetStartedBPInstances())
+            {
+                DBReplicationProcessInput startedBPInstanceInputArg = startedBPInstance.InputArgument as DBReplicationProcessInput;
+                if (startedBPInstanceInputArg != null)
+                {
+                    context.Reason = "Another Database replication process is running";
+                    return false;
+                }
+            }
+            return true;
+        }
+
+        public override bool StoreLastArgumentState { get { return true; } }
+    }
+}
