547f409c26a6ab8f71f2f6d9189efe5d5f62610f
Wissam Ajamy <VANRISE\wissam.ajamy>
2018-07-18 14:27:16 +0000

    fix script error
    git-tfs-id: [http://developmentvlan:8585/tfs/vanrise.collection]$/;C48151

--- Vanrise/Code/Resources/BatchScripts/Interconnect/01-Runtime.bat
+++ Vanrise/Code/Resources/BatchScripts/Interconnect/01-Runtime.bat
@@ -8,14 +8,13 @@ For /F %PARSEARG% %%i in (%CURRDATE%) Do SET YYYYMMDD=%%l%%j%%k
 
 ECHO.
 ECHO Interconnect Runtime
-xcopy "C:\TFS\Retail\Code\Retail.Runtime\bin\x64\Release" /y /v /z /i /Q /R		"\\192.168.110.185\Fixes\WebSite\Interconnect\Runtime\%YYYYMMDD%\" /EXCLUDE:C:\TFS\Vanrise\Code\Resources\BatchScripts\Interconnect\runtime-list-of-excluded-files.txt
-xcopy "C:\TFS\Retail\Code\Retail.Runtime\App.config.Interconnect.exclude" /y /v /z /i /Q /R	"\\192.168.110.185\Fixes\WebSite\Interconnect\Runtime\%YYYYMMDD%\"
-rename "\\192.168.110.185\Fixes\WebSite\Interconnect\Runtime\%YYYYMMDD%\App.config.Interconnect.exclude" "Interconnect.Runtime.exe.config"
-rename "\\192.168.110.185\Fixes\WebSite\Interconnect\Runtime\%YYYYMMDD%\Retail.Runtime.exe" "Interconnect.Runtime.exe"
-
-xcopy "C:\TFS\Retail\Code\Retail.Interconnect.Web\Retail_Interconnect\Reports"								/S /E /R /y /v /i /z /Q "\\192.168.110.185\Fixes\WebSite\Interconnect\Runtime\%YYYYMMDD%\Modules\Interconnect\Reports" /EXCLUDE:C:\TFS\Vanrise\Code\Resources\BatchScripts\Retail_Interconnect\runtime-list-of-excluded-files.txt
+xcopy "C:\TFS\Retail\Code\Retail.Runtime\bin\x64\Release" /y /v /z /i /Q /R								"\\192.168.110.185\Fixes\WebSite\Interconnect\Runtime\%YYYYMMDD%\" /EXCLUDE:C:\TFS\Vanrise\Code\Resources\BatchScripts\Interconnect\runtime-list-of-excluded-files.txt
+xcopy "C:\TFS\Retail\Code\Retail.Runtime\App.config.Interconnect.exclude" /y /v /z /i /Q /R				"\\192.168.110.185\Fixes\WebSite\Interconnect\Runtime\%YYYYMMDD%\"
+xcopy "C:\TFS\Retail\Code\Retail.Interconnect.Web\Retail_Interconnect\Reports"	/S /E /R /y /v /i /z /Q "\\192.168.110.185\Fixes\WebSite\Interconnect\Runtime\%YYYYMMDD%\Modules\Interconnect\Reports" /EXCLUDE:C:\TFS\Vanrise\Code\Resources\BatchScripts\Interconnect\runtime-list-of-excluded-files.txt
 
 xcopy "C:\TFS\Vanrise\Code\Resources\DLLs\Aspose.Cells.dll" /y /v /z /i /Q /R "\\192.168.110.185\Fixes\WebSite\Interconnect\Runtime\%YYYYMMDD%\"
 xcopy "C:\TFS\Vanrise\Code\Resources\DLLs\Aspose.Cells.lic" /y /v /z /i /Q /R "\\192.168.110.185\Fixes\WebSite\Interconnect\Runtime\%YYYYMMDD%\"
 
-del /s /q /f "\\192.168.110.185\Fixes\WebSite\Interconnect\Runtime\%YYYYMMDD%\*.pdb"
+rename			"\\192.168.110.185\Fixes\WebSite\Interconnect\Runtime\%YYYYMMDD%\App.config.Interconnect.exclude" "Interconnect.Runtime.exe.config"
+rename			"\\192.168.110.185\Fixes\WebSite\Interconnect\Runtime\%YYYYMMDD%\Retail.Runtime.exe" "Interconnect.Runtime.exe"
+del /s /q /f	"\\192.168.110.185\Fixes\WebSite\Interconnect\Runtime\%YYYYMMDD%\*.pdb"
