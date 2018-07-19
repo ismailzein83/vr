3df5370e608b41fd7387e66f8e97edfbb0052785
Rodi Hassan <VANRISE\rodi.hassan>
2018-07-19 09:24:40 +0000

    add trim option to vr-textarea
    git-tfs-id: [http://developmentvlan:8585/tfs/vanrise.collection]$/;C48170

--- Vanrise/Code/Vanrise.Web/Client/Javascripts/Directives/Inputs/Textarea/vr-textarea-directive.js
+++ Vanrise/Code/Vanrise.Web/Client/Javascripts/Directives/Inputs/Textarea/vr-textarea-directive.js
@@ -2,7 +2,7 @@
 
     "use strict";
 
-    vrTextarea.$inject = ['BaseDirService', 'VRValidationService', 'UtilsService','VRLocalizationService'];
+    vrTextarea.$inject = ['BaseDirService', 'VRValidationService', 'UtilsService', 'VRLocalizationService'];
 
     function vrTextarea(BaseDirService, VRValidationService, UtilsService, VRLocalizationService) {
 
@@ -10,8 +10,9 @@
             restrict: 'E',
             scope: {
                 value: '=',
+                trim: '=',
                 hint: '@',
-                placeholder:'@'
+                placeholder: '@'
             },
             controller: function ($scope, $element, $attrs) {
                 var ctrl = this;
@@ -48,7 +49,7 @@
                 //    validationOptions.requiredValue = true;
                 //if (attrs.customvalidate !== undefined)
                 //    validationOptions.customValidation = true;
-             
+
                 //var elementName = BaseDirService.prepareDirectiveHTMLForValidation(validationOptions, inputElement, inputElement, element.find('#rootDiv'));
                 return {
                     pre: function ($scope, iElem, iAttrs) {
@@ -116,13 +117,16 @@
 
                 var labelTemplate = '';
                 var label = "";
+                var trimExp = "";
 
                 if (attrs.label != undefined)
                     label = VRLocalizationService.getResourceValue(attrs.localizedlabel, attrs.label);
-                 
+
                 if (attrs.label != undefined)
                     labelTemplate = '<vr-label>' + label + '</vr-label>';
-               
+
+                if (attrs.trim != undefined)
+                    trimExp = 'ng-trim="{{ctrl.trim}}"';
 
                 var rows = 3;
                 if ($(element).parents('.vr-datagrid-celltext').length > 0)
@@ -130,10 +134,10 @@
                 if (attrs.rows != undefined)
                     rows = attrs.rows;
                 var textboxTemplate = '<div ng-mouseenter="showtd=true" ng-mouseleave="showtd=false" >'
-                        + '<vr-validator validate="ctrl.validate()" vr-textarea-element>'
-                        + '<textarea tabindex="{{ctrl.tabindex}}" readonly="ctrl.readOnly"  placeholder="{{ctrl.placelHolder}}" ng-readonly="ctrl.readOnly" id="mainInput"  ng-model="ctrl.value" ng-change="ctrl.notifyUserChange()" rows="' + rows + '" class="form-control vanrise-inpute" style="width: 100%; resize: none;" ></textarea>'
-                        + '</vr-validator>'
-                        + '<span ng-if="ctrl.hint!=undefined" bs-tooltip class="glyphicon glyphicon-question-sign hand-cursor vr-hint-input" html="true"   placement="bottom"  trigger="hover" ng-mouseenter="ctrl.adjustTooltipPosition($event)"  data-type="info" data-title="{{ctrl.hint}}"></span>';
+					+ '<vr-validator validate="ctrl.validate()" vr-textarea-element>'
+					+ '<textarea ' + trimExp + ' tabindex="{{ctrl.tabindex}}" readonly="ctrl.readOnly"  placeholder="{{ctrl.placelHolder}}" ng-readonly="ctrl.readOnly" id="mainInput"  ng-model="ctrl.value" ng-change="ctrl.notifyUserChange()" rows="' + rows + '" class="form-control vanrise-inpute" style="width: 100%; resize: none;" ></textarea>'
+					+ '</vr-validator>'
+					+ '<span ng-if="ctrl.hint!=undefined" bs-tooltip class="glyphicon glyphicon-question-sign hand-cursor vr-hint-input" html="true"   placement="bottom"  trigger="hover" ng-mouseenter="ctrl.adjustTooltipPosition($event)"  data-type="info" data-title="{{ctrl.hint}}"></span>';
                 + '</div>';
 
 
@@ -147,7 +151,7 @@
     }
 
     app.directive('vrTextarea', vrTextarea);
-    
+
 })(app);
 
 
