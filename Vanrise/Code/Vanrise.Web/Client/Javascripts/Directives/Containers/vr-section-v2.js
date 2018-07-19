2ad1f725d9157de1d0d40d22acae6384bd2cb943
Rodi Hassan <VANRISE\rodi.hassan>
2018-07-19 09:34:27 +0000

    modify vr-section-v2 according to workflow designer requirments (drag,remove)
    git-tfs-id: [http://developmentvlan:8585/tfs/vanrise.collection]$/;C48171

--- Vanrise/Code/Vanrise.Web/Client/Javascripts/Directives/Containers/vr-section-v2.js
+++ Vanrise/Code/Vanrise.Web/Client/Javascripts/Directives/Containers/vr-section-v2.js
@@ -2,32 +2,39 @@
 
 app.directive('vrSectionV2', ['UtilsService', 'MultiTranscludeService', function (UtilsService, MultiTranscludeService) {
 
-    var directiveDefinitionObject = {
-        restrict: 'E',
-        scope: {
-            menuactions: "="
-        },
-        transclude: true,
-        controller: function ($scope, $element, $attrs) {
-            var ctrl = this;
-            $scope.classlevel = "1";
-            $scope.header = $attrs.header;
-            $scope.collapsible = $attrs.collapsible != undefined;
-            if ($attrs.level != undefined && $attrs.level == "2")
-                $scope.classlevel = " panel-vr-child ";
-            $scope.expandname = 'expanded_' + UtilsService.replaceAll(UtilsService.guid(), '-', '');
+	var directiveDefinitionObject = {
+		restrict: 'E',
+		scope: {
+			menuactions: "=",
+			onRemove: "=",
+			header: "="
+		},
+		transclude: true,
+		controller: function ($scope, $element, $attrs) {
+			var ctrl = this;
+			$scope.classlevel = "1";
+			$scope.header = ctrl.header;
+			$scope.dragable = $attrs.dragable != undefined;
+			$scope.collapsible = $attrs.collapsible != undefined;
+			if ($attrs.level != undefined && $attrs.level == "2")
+				$scope.classlevel = " panel-vr-child ";
+			$scope.expandname = 'expanded_' + UtilsService.replaceAll(UtilsService.guid(), '-', '');
+		},
+		controllerAs: 'sectionCtrl',
+		bindToController: true,
+		template: function (attrs) {
+			var htmlTempalte = '<div class="panel-primary panel-vr" ng-class="classlevel" >'
+				+ '<div class="panel-heading" ng-init="expandname=true" expanded="{{expandname}}">'
+				+ '<span class="glyphicon glyphicon-th-list handeldrag hand-cursor drag-icon" ng-show="dragable" ></span>'
+				+ '<span style="padding-left: 4px;" class="hand-cursor collapsible-icon glyphicon " ng-show="collapsible" ng-click=" expandname =!expandname " ng-class=" expandname ?\'glyphicon-collapse-up\':\'glyphicon-collapse-down\'" ></span><label>{{header}}</label>'
+				+ '<span class="section-menu"  style="absolute: relative; right: 10px;" ng-if="sectionCtrl.menuactions.length > 0" > <vr-button type="SectionAction" menuactions="sectionCtrl.menuactions" isasynchronous="true" ></vr-button></span> '
+				+ '<span class="hand-cursor glyphicon glyphicon-remove" style="position: absolute; right: 5px; top: 12px;" ng-show="sectionCtrl.onRemove != undefined" ng-click="sectionCtrl.onRemove()" ></span></div>'
+				+ '<div class="panel-body" ng-show="expandname"  ng-transclude></div></div>';
 
-        },
-        controllerAs: 'sectionCtrl',
-        bindToController: true,
-        template: function (attrs) {
-            var htmlTempalte = '<div class="panel-primary panel-vr" ng-class="classlevel" >'
-                + '<div class="panel-heading" ng-init="expandname=true" expanded="{{expandname}}"><label><span style="padding-right: 3px;" class="hand-cursor collapsible-icon glyphicon " ng-show="collapsible" ng-click=" expandname =!expandname " ng-class=" expandname ?\'glyphicon-collapse-up\':\'glyphicon-collapse-down\'" ></span>{{header}}</label><div class="section-menu" ng-if="sectionCtrl.menuactions.length > 0"><vr-button type="SectionAction" menuactions="sectionCtrl.menuactions" isasynchronous="true" ></vr-button></div></div>'
-                + '<div class="panel-body" ng-show="expandname"  ng-transclude></div></div>';
-            return htmlTempalte;
-        }
-    };
+			return htmlTempalte;
+		}
+	};
 
-    return directiveDefinitionObject;
+	return directiveDefinitionObject;
 
 }]);
