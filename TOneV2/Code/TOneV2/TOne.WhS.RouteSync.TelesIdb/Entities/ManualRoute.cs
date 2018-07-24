f2f281449a8a41a5f8656fae4f0c953b5402d2dc
Rodi Hassan <VANRISE\rodi.hassan>
2018-07-24 11:56:29 +0000

    Add manualroute class for TelesIdb switch
    git-tfs-id: [http://developmentvlan:8585/tfs/vanrise.collection]$/;C48322

---
+++ TOneV2/Code/TOneV2/TOne.WhS.RouteSync.TelesIdb/Entities/ManualRoute.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TOne.WhS.RouteSync.TelesIdb.Entities
+{
+	public class ManualRoute
+	{
+		public string Pref { get; set; }
+		public string Route { get; set; }
+		public bool IncludeSubcodes { get; set; }
+	}
+}
