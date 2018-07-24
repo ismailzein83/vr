d49218f1ba1e13019ac4c4a196824dabf3b245d1
Ali Atoui <VANRISE\ali.atoui>
2018-07-24 06:31:04 +0000

    Editing TimestampDateTimeCompositeParser
    git-tfs-id: [http://developmentvlan:8585/tfs/vanrise.collection]$/;C48292

--- Vanrise/Code/Vanrise.DataParser.MainExtensions/CompositeFieldParsers/TimestampDateTimeCompositeParser.cs
+++ Vanrise/Code/Vanrise.DataParser.MainExtensions/CompositeFieldParsers/TimestampDateTimeCompositeParser.cs
@@ -9,22 +9,27 @@ namespace Vanrise.DataParser.MainExtensions.CompositeFieldParsers
 {
     public class TimestampDateTimeCompositeParser : CompositeFieldsParser
     {
-        public override Guid ConfigId
-        {
-            get { return new Guid("51D08426-F968-4A8C-B52B-90FF55E8630E"); }
-        }
+        public override Guid ConfigId { get { return new Guid("51D08426-F968-4A8C-B52B-90FF55E8630E"); } }
         public string FieldName { get; set; }
         public string DateTimeFieldName { get; set; }
         public DateTime DateTimeShift { get; set; }
+
         public override void Execute(ICompositeFieldsParserContext context)
         {
-            TimeSpan timeSpan = new TimeSpan();
-            DateTime dateTimeField = (DateTime)context.Record.GetFieldValue(DateTimeFieldName);
-            if (dateTimeField != DateTime.MinValue)
+            try
             {
-                timeSpan = dateTimeField - DateTimeShift;
+                TimeSpan timeSpan = new TimeSpan();
+                DateTime? dateTimeField = (DateTime?)context.Record.GetFieldValue(DateTimeFieldName);
+                if (dateTimeField.HasValue && dateTimeField.Value != DateTime.MinValue)
+                {
+                    timeSpan = dateTimeField.Value - DateTimeShift;
+                }
+                context.Record.SetFieldValue(FieldName, timeSpan.TotalSeconds);
             }
-            context.Record.SetFieldValue(FieldName, timeSpan.TotalSeconds);
+            catch (Exception ex)
+            {
+                throw ex;
+            }
         }
     }
 }
