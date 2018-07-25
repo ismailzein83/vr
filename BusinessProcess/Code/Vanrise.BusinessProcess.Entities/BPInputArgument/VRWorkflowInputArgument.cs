df842988e366bd4a6c77eed62ba4d4cb6a1203e1
Ali Atoui <VANRISE\ali.atoui>
2018-07-25 10:22:18 +0000

    Adding VRWorkflowInputArgument Adding VRWorkflowFilter Adding VRWorkflowInfo
    git-tfs-id: [http://developmentvlan:8585/tfs/vanrise.collection]$/;C48364

---
+++ BusinessProcess/Code/Vanrise.BusinessProcess.Entities/BPInputArgument/VRWorkflowInputArgument.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Vanrise.BusinessProcess.Entities
+{
+    public class VRWorkflowInputArgument
+    {
+        public string ProcessName { get { return string.Format("VRWorkflowInputArgument_{0}", BPDefinitionId.ToString("N")); } }
+
+        public Guid BPDefinitionId { get; set; }
+
+        public Dictionary<string, object> Arguments { get; set; }
+
+        //public string EntityId { get; set; }
+
+        public int UserId { get; set; }
+
+        //public abstract string GetTitle();
+
+        public string GetDefinitionTitle()
+        {
+            return BusinessManagerFactory.GetManager<IBPDefinitionManager>().GetDefinitionTitle(this.ProcessName);
+        }
+
+        public void MapExpressionValues(Dictionary<string, object> evaluatedExpressions)
+        {
+
+        }
+
+        public void PrepareArgumentForExecutionFromTask(IProcessInputArgumentPrepareArgumentForExecutionFromTaskContext context)
+        {
+
+        }
+    }
+}
---
+++ BusinessProcess/Code/Vanrise.BusinessProcess.Entities/VRWorkflow/VRWorkflowInfo.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Vanrise.BusinessProcess.Entities
+{
+    public class VRWorkflowInfo
+    {
+        public Guid VRWorkflowId { get; set; }
+
+        public string Name { get; set; }
+    }
+}
