6c7feeed21c2da0fcb2f31d1ecf635072546042a
Ali Atoui <VANRISE\ali.atoui>
2018-07-25 10:37:11 +0000

    Adding bp-vr-workflow-selector
    git-tfs-id: [http://developmentvlan:8585/tfs/vanrise.collection]$/;C48366

---
+++ BusinessProcess/Code/Vanrise.BusinessProcess.Web/BusinessProcess/Directives/VRWorkflow/bp-vr-workflow-selector.js
@@ -0,0 +1,113 @@
+﻿'use strict';
+
+app.directive('businessprocessVrWorkflowSelector', ['BusinessProcess_VRWorkflowAPIService', 'UtilsService', 'VRUIUtilsService',
+    function (BusinessProcess_VRWorkflowAPIService, UtilsService, VRUIUtilsService) {
+        return {
+            restrict: 'E',
+            scope: {
+                onReady: '=',
+                ismultipleselection: '@',
+                selectedvalues: '=',
+                onselectionchanged: '=',
+                onselectitem: '=',
+                ondeselectitem: '=',
+                isrequired: '=',
+                normalColNum: '@',
+                customvalidate: '='
+            },
+            controller: function ($scope, $element, $attrs) {
+
+                var ctrl = this;
+                ctrl.datasource = [];
+
+                ctrl.selectedvalues;
+                if ($attrs.ismultipleselection != undefined)
+                    ctrl.selectedvalues = [];
+
+                var ctor = new VRWorkflowSelectorCtor(ctrl, $scope, $attrs);
+                ctor.initializeController();
+            },
+            controllerAs: 'ctrl',
+            bindToController: true,
+            template: function (element, attrs) {
+                return getTemplate(attrs);
+            }
+        };
+
+        function getTemplate(attrs) {
+
+            var multipleselection = "";
+            var label = "Workflow";
+
+            if (attrs.ismultipleselection != undefined) {
+                label = "Workflows";
+                multipleselection = "ismultipleselection";
+            }
+
+            if (attrs.customlabel != undefined)
+                label = attrs.customlabel;
+
+            var hidelabel = "";
+            if (attrs.hidelabel != undefined)
+                hidelabel = "hidelabel";
+
+            var hideremoveicon = "";
+            if (attrs.hideremoveicon != undefined)
+                hideremoveicon = "hideremoveicon";
+
+            return '<vr-columns colnum="{{ctrl.normalColNum}}">' +
+                       '<vr-select ' + multipleselection + ' datatextfield="Name" datavaluefield="VRWorkflowId" isrequired="ctrl.isrequired" ' + hidelabel + ' label="' + label +
+                           '" datasource="ctrl.datasource" on-ready="ctrl.onSelectorReady" selectedvalues="ctrl.selectedvalues" onselectionchanged="ctrl.onselectionchanged" entityName="' + label +
+                           '" onselectitem="ctrl.onselectitem" ondeselectitem="ctrl.ondeselectitem" ' + hideremoveicon + ' customvalidate="ctrl.customvalidate">' +
+                       '</vr-select>' +
+                   '</vr-columns>';
+        }
+
+        function VRWorkflowSelectorCtor(ctrl, $scope, attrs) {
+            this.initializeController = initializeController;
+
+            var selectorAPI;
+
+            function initializeController() {
+                ctrl.onSelectorReady = function (api) {
+                    selectorAPI = api;
+                    defineAPI();
+                };
+            }
+
+            function defineAPI() {
+                var api = {};
+
+                api.load = function (payload) {
+
+                    var selectedIds;
+                    var filter;
+
+                    if (payload != undefined) {
+                        selectedIds = payload.selectedIds;
+                        filter = payload.filter;
+                    }
+
+                    return BusinessProcess_VRWorkflowAPIService.GetVRWorkflowsInfo(UtilsService.serializetoJson(filter)).then(function (response) {
+                        selectorAPI.clearDataSource();
+                        if (response != null) {
+                            for (var i = 0; i < response.length; i++) {
+                                ctrl.datasource.push(response[i]);
+                            }
+
+                            if (selectedIds != undefined) {
+                                VRUIUtilsService.setSelectedValues(selectedIds, 'VRWorkflowId', attrs, ctrl);
+                            }
+                        }
+                    });
+                };
+
+                api.getSelectedIds = function () {
+                    return VRUIUtilsService.getIdSelectedIds('VRWorkflowId', attrs, ctrl);
+                };
+
+                if (ctrl.onReady != null)
+                    ctrl.onReady(api);
+            }
+        }
+    }]);
