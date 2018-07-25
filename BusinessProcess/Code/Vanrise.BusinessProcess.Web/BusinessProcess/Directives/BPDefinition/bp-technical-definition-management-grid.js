d1c47ae1ac0c365dab21506d8c6a16b56420de08
Ali Atoui <VANRISE\ali.atoui>
2018-07-25 13:11:35 +0000

    Arranging bp-technical-definition-management-grid
    git-tfs-id: [http://developmentvlan:8585/tfs/vanrise.collection]$/;C48368

--- BusinessProcess/Code/Vanrise.BusinessProcess.Web/BusinessProcess/Directives/BPDefinition/bp-technical-definition-management-grid.js
+++ BusinessProcess/Code/Vanrise.BusinessProcess.Web/BusinessProcess/Directives/BPDefinition/bp-technical-definition-management-grid.js
@@ -1,60 +1,42 @@
 ﻿"use strict";
 
 app.directive("businessprocessBpTechnicalDefinitionManagementGrid", ["UtilsService", "VRNotificationService", "BusinessProcess_BPDefinitionAPIService", "BusinessProcess_BPDefinitionService", "VRUIUtilsService",
-function (UtilsService, VRNotificationService, BusinessProcess_BPDefinitionAPIService, BusinessProcess_BPDefinitionService, VRUIUtilsService) {
+    function (UtilsService, VRNotificationService, BusinessProcess_BPDefinitionAPIService, BusinessProcess_BPDefinitionService, VRUIUtilsService) {
 
-    var directiveDefinitionObject = {
+        var directiveDefinitionObject = {
+            restrict: "E",
+            scope: {
+                onReady: "="
+            },
+            controller: function ($scope, $element, $attrs) {
+                var ctrl = this;
+                var bpDefinitionManagementGrid = new BPDefinitionManagementGrid($scope, ctrl, $attrs);
+                bpDefinitionManagementGrid.initializeController();
+            },
+            controllerAs: "ctrl",
+            bindToController: true,
+            templateUrl: "/Client/Modules/BusinessProcess/Directives/BPDefinition/Templates/BPTechnicalDefinitionGridTemplate.html"
+        };
 
-        restrict: "E",
-        scope: {
-            onReady: "="
-        },
-        controller: function ($scope, $element, $attrs) {
-            var ctrl = this;
+        function BPDefinitionManagementGrid($scope, ctrl, $attrs) {
+            this.initializeController = initializeController;
 
-            var bpDefinitionManagementGrid = new BPDefinitionManagementGrid($scope, ctrl, $attrs);
-            bpDefinitionManagementGrid.initializeController();
-        },
-        controllerAs: "ctrl",
-        bindToController: true,
-        compile: function (element, attrs) {
+            var gridAPI;
+            var gridDrillDownTabsObj;
 
-        },
-        templateUrl: "/Client/Modules/BusinessProcess/Directives/BPDefinition/Templates/BPTechnicalDefinitionGridTemplate.html"
 
-    };
+            function initializeController() {
+                $scope.bfDefinitions = [];
 
-    function BPDefinitionManagementGrid($scope, ctrl, $attrs) {
+                $scope.onGridReady = function (api) {
+                    gridAPI = api;
+                    var drillDownDefinitions = BusinessProcess_BPDefinitionService.getDrillDownDefinition();
+                    gridDrillDownTabsObj = VRUIUtilsService.defineGridDrillDownTabs(drillDownDefinitions, gridAPI, $scope.gridMenuActions);
+                    defineAPI();
+                };
 
-        var gridAPI;
-        var gridDrillDownTabsObj;
-
-        this.initializeController = initializeController;
-
-        function initializeController() {
-
-            $scope.bfDefinitions = [];
-            defineMenuActions();
-
-            $scope.onGridReady = function (api) {
-                gridAPI = api;
-                var drillDownDefinitions = BusinessProcess_BPDefinitionService.getDrillDownDefinition();
-                gridDrillDownTabsObj = VRUIUtilsService.defineGridDrillDownTabs(drillDownDefinitions, gridAPI, $scope.gridMenuActions);
-                if (ctrl.onReady != undefined && typeof (ctrl.onReady) == "function")
-                    ctrl.onReady(getDirectiveAPI());
-
-                function getDirectiveAPI() {
-                    var directiveAPI = {};
-                    directiveAPI.loadGrid = function (query) {
-                        return gridAPI.retrieveData(query);
-                    };
-                    return directiveAPI;
-                }
-            };
-
-            $scope.dataRetrievalFunction = function (dataRetrievalInput, onResponseReady) {
-                return BusinessProcess_BPDefinitionAPIService.GetFilteredBPDefinitionsForTechnical(dataRetrievalInput)
-                    .then(function (response) {
+                $scope.dataRetrievalFunction = function (dataRetrievalInput, onResponseReady) {
+                    return BusinessProcess_BPDefinitionAPIService.GetFilteredBPDefinitionsForTechnical(dataRetrievalInput).then(function (response) {
                         if (response.Data != undefined) {
                             for (var i = 0; i < response.Data.length; i++) {
                                 gridDrillDownTabsObj.setDrillDownExtensionObject(response.Data[i]);
@@ -65,36 +47,43 @@ function (UtilsService, VRNotificationService, BusinessProcess_BPDefinitionAPISe
                     .catch(function (error) {
                         VRNotificationService.notifyException(error, $scope);
                     });
-            };
+                };
 
+                defineMenuActions();
+            }
+            function defineAPI() {
+                var directiveAPI = {};
+
+                directiveAPI.loadGrid = function (query) {
+                    return gridAPI.retrieveData(query);
+                };
+
+                if (ctrl.onReady != undefined && typeof (ctrl.onReady) == "function")
+                    ctrl.onReady(directiveAPI);
+            }
+
+            function defineMenuActions() {
+                $scope.gridMenuActions = function () {
+                    var menuActions = [{
+                        name: "Edit",
+                        clicked: edit,
+                        haspermission: hasUpdateBPDefinitionPermission
+
+                    }];
+                    return menuActions;
+                };
+            }
+            function edit(dataItem) {
+                var onBPDefinitionUpdated = function (updatedBPDefinition) {
+                    gridDrillDownTabsObj.setDrillDownExtensionObject(updatedBPDefinition);
+                    gridAPI.itemUpdated(updatedBPDefinition);
+                };
+                BusinessProcess_BPDefinitionService.editBusinessProcessDefinition(dataItem.Entity.BPDefinitionID, onBPDefinitionUpdated);
+            }
+            function hasUpdateBPDefinitionPermission() {
+                return BusinessProcess_BPDefinitionAPIService.HasUpdateBPDefinitionPermission();
+            }
         }
 
-
-        function defineMenuActions() {          
-            $scope.gridMenuActions = function () {
-                var menuActions = [{
-                    name: "Edit",
-                    clicked: edit,
-                    haspermission: hasUpdateBPDefinitionPermission
-
-                }];
-                return menuActions;
-            };
-        }
-
-        function hasUpdateBPDefinitionPermission() {
-            return BusinessProcess_BPDefinitionAPIService.HasUpdateBPDefinitionPermission();
-        }
-        function edit(dataItem) {
-            var onBPDefinitionUpdated = function (updatedBPDefinition) {
-                gridDrillDownTabsObj.setDrillDownExtensionObject(updatedBPDefinition);
-                gridAPI.itemUpdated(updatedBPDefinition);
-            };
-            BusinessProcess_BPDefinitionService.editBusinessProcessDefinition(dataItem.Entity.BPDefinitionID, onBPDefinitionUpdated);
-           
-        }
-    }
-
-    return directiveDefinitionObject;
-
-}]);
+        return directiveDefinitionObject;
+    }]);
