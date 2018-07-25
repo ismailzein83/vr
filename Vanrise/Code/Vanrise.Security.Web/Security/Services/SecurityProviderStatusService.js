9eabb02254484292c65eb2a48970f565708d7997
Naim Saad <VANRISE\naim.saad>
2018-07-25 17:17:10 +0000

    Confirmation pop up for security provider
    git-tfs-id: [http://developmentvlan:8585/tfs/vanrise.collection]$/;C48377

--- Vanrise/Code/Vanrise.Security.Web/Security/Services/SecurityProviderStatusService.js
+++ Vanrise/Code/Vanrise.Security.Web/Security/Services/SecurityProviderStatusService.js
@@ -2,31 +2,34 @@
 
     'use strict';
 
-    SecurityProviderStatusService.$inject = ['VRNotificationService', 'VR_Sec_SecurityProviderAPIService', 'VR_GenericData_GenericBEActionService'];
+    SecurityProviderStatusService.$inject = ['VRNotificationService', 'VR_Sec_SecurityProviderAPIService', 'VR_GenericData_GenericBEActionService', 'UtilsService'];
 
-    function SecurityProviderStatusService(VRNotificationService,VR_Sec_SecurityProviderAPIService, VR_GenericData_GenericBEActionService) {
+    function SecurityProviderStatusService(VRNotificationService, VR_Sec_SecurityProviderAPIService, VR_GenericData_GenericBEActionService, UtilsService) {
 
         function changeSecurityProviderStatus() {
 
             var actionType = {
                 ActionTypeName: "SecurityProviderStatus",
                 ExecuteAction: function (payload) {
+                    var promiseDeffered = UtilsService.createPromiseDeferred();
                     if (payload == undefined)
                         return;
                     var genericBusinessEntityId = payload.genericBusinessEntityId;
                     var isEnabled = payload.genericBEAction.Settings.SetEnable;
-                    VR_Sec_SecurityProviderAPIService.ChangeSecurityProviderStatus(genericBusinessEntityId, isEnabled).then(function (response) {
-                        if (response != undefined)
-                        {
-                            if(VRNotificationService.notifyOnItemUpdated("Security provider", response, response.Message))
-                            {
-                                payload.onItemUpdated(response.UpdatedObject);
-                            }
+                    VRNotificationService.showConfirmation().then(function (response) {
+                        if (response) {
+                            VR_Sec_SecurityProviderAPIService.ChangeSecurityProviderStatus(genericBusinessEntityId, isEnabled).then(function (response) {
+                                if (response != undefined) {
+                                    if (VRNotificationService.notifyOnItemUpdated("Security provider", response, response.Message)) {
+                                        payload.onItemUpdated(response.UpdatedObject);
+                                    }
+                                }
+                                promiseDeffered.resolve(response);
+                            });
                         }
-
-                       
+                        else promiseDeffered.resolve(response);
                     });
-
+                    return promiseDeffered.promise;
                 }
             };
             VR_GenericData_GenericBEActionService.registerActionType(actionType);
