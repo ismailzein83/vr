0bae55fdd1f06d57285ca54dfc1001d2e2b5bc08
anthony douaihy <VANRISE\anthony.douaihy>
2018-07-25 14:35:45 +0000

    add VRWorkflowId to BPDefinition
    git-tfs-id: [http://developmentvlan:8585/tfs/vanrise.collection]$/;C48372

--- BusinessProcess/Code/Vanrise.BusinessProcess.Entities/BPDefinition.cs
+++ BusinessProcess/Code/Vanrise.BusinessProcess.Entities/BPDefinition.cs
@@ -17,7 +17,8 @@ namespace Vanrise.BusinessProcess.Entities
         public Type WorkflowType { get; set; } // Type should inherit System.Activities.Activity
 
         public BPConfiguration Configuration { get; set; }
-      
+
+        public Guid? VRWorkflowId { get; set; }
     }
 
     public class BPConfiguration
