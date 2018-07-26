c3c57cf346b67cb9d4baa0b106155f85fc5ac83d
Fatima Al Heck <VANRISE\fatima.alheck>
2018-07-26 12:26:15 +0000

    Add SwitchDBReplicationPreInsert Extension
    git-tfs-id: [http://developmentvlan:8585/tfs/vanrise.collection]$/;C48405

---
+++ TOneV2/Code/TOneV2/TOne.WhS.BusinessEntity.MainExtensions/DBReplication/SwitchDBReplicationPreInsert.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Vanrise.Entities;
+
+namespace TOne.WhS.BusinessEntity.MainExtensions
+{
+    public class SwitchDBReplicationPreInsert : DBReplicationPreInsert
+    {
+        public override Guid ConfigId { get { return new Guid("5f22d483-6913-4943-bbbb-804559bef664"); } }
+        public override void Execute(IDBReplicationPreInsertExecuteContext context)
+        {
+
+        }
+    }
+}
---
+++ TOneV2/Code/TOneV2/TOne.WhS.BusinessEntity.Web/WhS_BusinessEntity/Directives/DBReplication/vr-whs-be-dbreplicationpreinsert-switch.js
@@ -0,0 +1,43 @@
+﻿'use strict';
+
+app.directive('vrWhsBeDbreplicationpreinsertSwitch', [
+    function () {
+        return {
+            restrict: 'E',
+            scope: {
+                onReady: '='
+            },
+            controller: function ($scope, $element, $attrs) {
+
+                var ctrl = this;
+
+                var ctor = new DBReplicationPreInsertSwitchCtor(ctrl, $scope, $attrs);
+                ctor.initializeController();
+            },
+            controllerAs: 'ctrl',
+            bindToController: true,
+        };
+
+        function DBReplicationPreInsertSwitchCtor(ctrl, $scope, attrs) {
+            this.initializeController = initializeController;
+
+            function initializeController() {
+                defineAPI();
+            }
+
+            function defineAPI() {
+                var api = {};
+
+                api.load = function (payload) { };
+
+                api.getData = function () {
+                    return {
+                        $type: "TOne.WhS.BusinessEntity.MainExtensions.SwitchDBReplicationPreInsert,TOne.WhS.BusinessEntity.MainExtensions"
+                    };
+                };
+
+                if (ctrl.onReady != null)
+                    ctrl.onReady(api);
+            }
+        }
+    }]);
