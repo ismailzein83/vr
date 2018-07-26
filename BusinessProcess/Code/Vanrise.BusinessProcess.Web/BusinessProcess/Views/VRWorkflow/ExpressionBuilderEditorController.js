7026ddd6ec901abc18d7bd06a19232761479f32a
Rodi Hassan <VANRISE\rodi.hassan>
2018-07-26 08:00:19 +0000

    use appendAtCursorPosition in expression builder and hide grid menu in compilation result
    git-tfs-id: [http://developmentvlan:8585/tfs/vanrise.collection]$/;C48389

--- BusinessProcess/Code/Vanrise.BusinessProcess.Web/BusinessProcess/Views/VRWorkflow/ExpressionBuilderEditorController.js
+++ BusinessProcess/Code/Vanrise.BusinessProcess.Web/BusinessProcess/Views/VRWorkflow/ExpressionBuilderEditorController.js
@@ -6,6 +6,7 @@
 
 	function ExpressionBuilderEditorController($scope, UtilsService, VRNotificationService, VRNavigationService, VRUIUtilsService) {
 
+		var textAreaAPI;
 		loadParameters();
 		defineScope();
 		load();
@@ -21,52 +22,19 @@
 		}
 
 		function defineScope() {
+
+			$scope.scopeModel.onTextAreaReady = function (api) {
+				textAreaAPI = api;
+			};
+
 			$scope.scopeModel.saveExpressionValue = function () {
 				return setExpressionValue();
 			};
 
 			$scope.scopeModel.insertText = function (item) {
-				var textToAdd = item.Name;
-				var input = document.getElementById("resid");
-				if (input == undefined) { return; }
-				var scrollTop = input.scrollTop;
-				var cursorPosition = 0;
-				var browser = ((input.selectionStart || input.selectionStart == "0") ? "ch" : (document.selection ? "IE" : false));
-				var inputLength = input.value.length;
-				if (browser == "IE") {
-					input.focus();
-					var range = document.selection.createRange();
-					range.moveStart("character", -inputLength);
-					cursorPosition = range.text.length;
+				if (textAreaAPI != undefined) {
+					textAreaAPI.appendAtCursorPosition(item.Name);
 				}
-				else if (browser == "ch") { cursorPosition = input.selectionStart; }
-
-				var pre = (input.value).substring(0, cursorPosition);
-				var post = (input.value).substring(cursorPosition, inputLength);
-				input.value = pre + textToAdd + post;
-				cursorPosition = cursorPosition + textToAdd.length;
-				if (browser == "IE") {
-					input.focus();
-					var range = document.selection.createRange();
-					range.moveStart("character", -inputLength);
-					range.moveStart("character", cursorPosition);
-					range.moveEnd("character", 0);
-					range.select();
-				}
-				else if (browser == "ch") {
-					input.selectionStart = cursorPosition;
-					input.selectionEnd = cursorPosition;
-					input.focus();
-				}
-				input.scrollTop = scrollTop;
-				angular.element(input).trigger('input');
-			};
-
-			$scope.scopeModel.itemClicked = function (item) {
-				if ($scope.scopeModel.expressionValue == undefined)
-					$scope.scopeModel.expressionValue = item.Name;
-				else
-					$scope.scopeModel.expressionValue += item.Name;
 			};
 
 			$scope.scopeModel.close = function () {
