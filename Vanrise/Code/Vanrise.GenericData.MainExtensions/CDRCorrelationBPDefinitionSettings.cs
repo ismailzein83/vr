06ff555001c1e91c7ab0a7112823c033c00a6cfb
anthony douaihy <VANRISE\anthony.douaihy>
2018-07-27 15:20:16 +0000

    add CDRCorrelationBPDefinitionSettings
    git-tfs-id: [http://developmentvlan:8585/tfs/vanrise.collection]$/;C48494

---
+++ Vanrise/Code/Vanrise.GenericData.MainExtensions/CDRCorrelationBPDefinitionSettings.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using Vanrise.Common;
+using Vanrise.GenericData.BP.Arguments;
+
+namespace Vanrise.GenericData.MainExtensions
+{
+    public class CDRCorrelationBPDefinitionSettings : Vanrise.BusinessProcess.Business.DefaultBPDefinitionExtendedSettings
+    {
+        public override bool CanRunBPInstance(Vanrise.BusinessProcess.Entities.IBPDefinitionCanRunBPInstanceContext context)
+        {
+            context.IntanceToRun.ThrowIfNull("context.IntanceToRun");
+            CDRCorrelationProcessInput inputArg = context.IntanceToRun.InputArgument.CastWithValidate<CDRCorrelationProcessInput>("context.IntanceToRun.InputArgument");
+
+            foreach (var startedBPInstance in context.GetStartedBPInstances())
+            {
+                CDRCorrelationProcessInput startedBPInstanceCDRCorrelationArg = startedBPInstance.InputArgument as CDRCorrelationProcessInput;
+                if (startedBPInstanceCDRCorrelationArg != null)
+                {
+                    context.Reason = String.Format("Another CDR Correlation instance is running");
+                    return false;
+                }
+            }
+
+            return true;
+        }
+    }
+}
