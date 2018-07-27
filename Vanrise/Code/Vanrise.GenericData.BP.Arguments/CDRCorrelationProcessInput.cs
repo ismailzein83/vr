e94778489770f7679ea77d7d06ff7413245df8ee
anthony douaihy <VANRISE\anthony.douaihy>
2018-07-27 08:18:45 +0000

    Add CDRCorrelation Argument and BP
    git-tfs-id: [http://developmentvlan:8585/tfs/vanrise.collection]$/;C48450

---
+++ Vanrise/Code/Vanrise.GenericData.BP.Arguments/CDRCorrelationProcessInput.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Vanrise.BusinessProcess.Entities;
+
+namespace Vanrise.GenericData.BP.Arguments
+{
+    public class CDRCorrelationProcessInput : BaseProcessInputArgument
+    {
+        public TimeSpan DateTimeMargin { get; set; }
+
+        public TimeSpan DurationMargin { get; set; }
+
+        public override string GetTitle()
+        {
+            return "CDR Correlation Process";
+        }
+    }
+}
