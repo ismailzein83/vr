40f310647df8369b8c71cfef83620a0750f0902f
yara.bousalman <yara.bousalman@vanrise.tfs.local>
2018-07-30 10:20:15 +0000

    Adding GenericBEDefinitionTypeEnum.js and vr-genericdata-genericbedefinition-remoteselector.js files
    git-tfs-id: [http://developmentvlan:8585/tfs/vanrise.collection]$/;C48513

---
+++ Vanrise/Code/Vanrise.GenericData.Web/VR_GenericData/Constants/GenericBEDefinitionTypeEnum.js
@@ -0,0 +1,4 @@
+﻿app.constant('VR_GenericData_GenericBEDefinitionTypeEnum', {
+    RecordStorage: { value: 0, description: 'Record Storage'},
+    Remote: { value: 1, description: 'Remote'},
+});
---
+++ Vanrise/Code/Vanrise.GenericData.Web/VR_GenericData/Directives/GenericBusinessEntity/vr-genericdata-genericbedefinition-remoteselector.js
@@ -0,0 +1,107 @@
+﻿'use strict';
+
+app.directive('vrGenericdataGenericbedefinitionRemoteselector', ['UtilsService', 'VRUIUtilsService', 'VR_GenericData_GenericBEDefinitionAPIService',
+    function (UtilsService, VRUIUtilsService, VR_GenericData_GenericBEDefinitionAPIService) {
+        return {
+            restrict: 'E',
+            scope: {
+                onReady: '=',
+                ismultipleselection: '@',
+                selectedvalues: '=',
+                onselectionchanged: '=',
+                onselectitem: '=',
+                ondeselectitem: '=',
+                isrequired: '=',
+                hideremoveicon: '@',
+                normalColNum: '@',
+                customvalidate: '='
+            },
+            controller: function ($scope, $element, $attrs) {
+
+                var ctrl = this;
+                ctrl.datasource = [];
+
+                ctrl.selectedvalues;
+                if ($attrs.ismultipleselection != undefined)
+                    ctrl.selectedvalues = [];
+
+                var ctor = new GenericBEDefinitionRemoteSelectorCtor(ctrl, $scope, $attrs);
+                ctor.initializeController();
+            },
+            controllerAs: 'ctrl',
+            bindToController: true,
+            template: function (element, attrs) {
+                return getTemplate(attrs);
+            }
+        };
+
+        function GenericBEDefinitionRemoteSelectorCtor(ctrl, $scope, attrs) {
+            this.initializeController = initializeController;
+
+            var selectorAPI;
+
+            function initializeController() {
+
+                ctrl.onSelectorReady = function (api) {
+                    selectorAPI = api;
+                    defineAPI();
+                };
+            }
+            function defineAPI() {
+                var api = {};
+
+                api.load = function (payload) {
+                    selectorAPI.clearDataSource();
+
+                    var selectedIds;
+                    var filter;
+                    var connectionId;
+
+                    if (payload != undefined) {
+                        selectedIds = payload.selectedIds;
+                        filter = payload.filter;
+                        connectionId = payload.connectionId;
+                    }
+
+                    return VR_GenericData_GenericBEDefinitionAPIService.GetRemoteGenericBEDefinitionInfo(connectionId, UtilsService.serializetoJson(filter)).then(function (response) {
+                        if (response != null) {
+                            for (var i = 0; i < response.length; i++) {
+                                ctrl.datasource.push(response[i]);
+                            }
+
+                            if (selectedIds != undefined) {
+                                VRUIUtilsService.setSelectedValues(selectedIds, 'DataRecordStorageId', attrs, ctrl);
+                            }
+                        }
+                    });
+                };
+
+                api.getSelectedIds = function () {
+                    return VRUIUtilsService.getIdSelectedIds('DataRecordStorageId', attrs, ctrl);
+                };
+
+                if (ctrl.onReady != null)
+                    ctrl.onReady(api);
+            }
+        }
+
+        function getTemplate(attrs) {
+
+            var multipleselection = "";
+            var label = "Remote Storage";
+
+            if (attrs.ismultipleselection != undefined) {
+                label = "Remote Storages";
+                multipleselection = "ismultipleselection";
+            }
+            if (attrs.customlabel != undefined)
+                label = attrs.customlabel;
+
+            return '<vr-columns colnum="{{ctrl.normalColNum}}">' +
+                        '<vr-select ' + multipleselection + ' datatextfield="Name" datavaluefield="DataRecordStorageId" isrequired="ctrl.isrequired" label="' + label + '" datasource="ctrl.datasource" on-ready="ctrl.onSelectorReady" '
+                            + ' selectedvalues="ctrl.selectedvalues" onselectionchanged="ctrl.onselectionchanged" entityName="' + label + '" onselectitem="ctrl.onselectitem" ondeselectitem="ctrl.ondeselectitem" '
+                            + ' hideremoveicon="ctrl.hideremoveicon" customvalidate="ctrl.customvalidate">' +
+                        '</vr-select>' +
+                    '</vr-columns>';
+        }
+    }]);
