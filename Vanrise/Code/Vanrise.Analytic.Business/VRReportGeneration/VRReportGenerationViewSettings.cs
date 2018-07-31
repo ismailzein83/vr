36692dee98cd63b603b95e6038430d504d6e1f86
Samer Haidar <VANRISE\samer.haidar>
2018-07-31 07:19:14 +0000

    adding VRReportGenerationViewSettings
    git-tfs-id: [http://developmentvlan:8585/tfs/vanrise.collection]$/;C48538

---
+++ Vanrise/Code/Vanrise.Analytic.Business/VRReportGeneration/VRReportGenerationViewSettings.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Vanrise.Security.Entities;
+
+namespace Vanrise.Analytic.Business
+{
+    public class VRReportGenerationViewSettings:ViewSettings
+    {
+        public override string GetURL(View view)
+        {
+            return view.Url;
+        }
+
+        public override bool DoesUserHaveAccess(IViewUserAccessContext context)
+        {
+            /// return true if it has permission on at least one query 
+            return true;
+        }
+    }
+}
