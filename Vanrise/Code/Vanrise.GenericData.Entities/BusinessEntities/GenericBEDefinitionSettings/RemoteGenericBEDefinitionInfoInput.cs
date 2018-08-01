8f955eb81df43aaeb53ac58e5cfdde6eb2f5e2a8
yara.bousalman <yara.bousalman@vanrise.tfs.local>
2018-08-01 13:13:25 +0000

    Adding classes to Vanrise.GenericData.Entities project
    git-tfs-id: [http://developmentvlan:8585/tfs/vanrise.collection]$/;C48589

---
+++ Vanrise/Code/Vanrise.GenericData.Entities/BusinessEntities/GenericBEDefinitionSettings/RemoteGenericBEDefinitionInfoInput.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Vanrise.GenericData.Entities
+{
+   
+    public class RemoteGenericBEDefinitionInfoInput
+    {
+        public Guid VRConnectionId { get; set; }
+
+        public string Filter { get; set; }
+    }
+}
---
+++ Vanrise/Code/Vanrise.GenericData.Entities/GenericBusinessEntity/GenericBEDefinitionFilter.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Vanrise.GenericData.Entities
+{
+    public class GenericBEDefinitionFilter
+    {
+        public Guid? VRConnectionId { get; set; }
+
+    }
+}
