e45feed49a7b45e801fe5f6c2e00e32f7cdcd87a
Samer Haidar <VANRISE\samer.haidar>
2018-08-01 11:37:35 +0000

    adding GenericBEDefinitionView
    git-tfs-id: [http://developmentvlan:8585/tfs/vanrise.collection]$/;C48583

---
+++ Vanrise/Code/Vanrise.GenericData.MainExtensions/GenericBusinessEntity/GenericBEViewDefinition/GenericBEDefinitionView.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Vanrise.GenericData.Business;
+
+namespace Vanrise.GenericData.MainExtensions
+{
+    public class GenericBEDefinitionView : GenericBEViewDefinitionSettings
+    {
+        public override Guid ConfigId
+        {
+            get { return new Guid("77EEF180-EED0-46A8-BBAB-F19BFDB43F60"); }
+        }
+
+        public override string RuntimeDirective
+        {
+            get { return ""; }
+        }
+        public override bool DoesUserHaveAccess(IGenericBEViewDefinitionCheckAccessContext context)
+        {
+            return base.DoesUserHaveAccess(context);
+        }
+        public Guid GenericBEDefinitionId { get; set; }
+
+        public List<GenericBEDefinitionViewColumnMapping> Mappings { get; set; }
+
+    }
+    public class GenericBEDefinitionViewColumnMapping
+    {
+        public string ParentColumnName { get; set; }
+
+        public string SubviewColumnName { get; set; }
+    }
+}
