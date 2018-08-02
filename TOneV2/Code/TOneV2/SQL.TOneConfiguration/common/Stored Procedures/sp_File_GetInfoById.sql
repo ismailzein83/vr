a87f94aa1b9aa9a35a06d03fdceb9fe15e9edd90
Wissam Ajamy <VANRISE\wissam.ajamy>
2018-08-02 14:46:52 +0000

    Update structure from ToneConfiguration
    git-tfs-id: [http://developmentvlan:8585/tfs/vanrise.collection]$/;C48633

--- TOneV2/Code/TOneV2/SQL.TOneConfiguration/common/Stored Procedures/sp_File_GetInfoById.sql
+++ TOneV2/Code/TOneV2/SQL.TOneConfiguration/common/Stored Procedures/sp_File_GetInfoById.sql
@@ -3,7 +3,7 @@ CREATE PROCEDURE [common].[sp_File_GetInfoById]
 	@Id bigint
 AS
 BEGIN
-	SELECT [Id], [Name], [Extension], [IsUsed], [ModuleName], [UserID], IsTemp, ConfigID, Settings, [CreatedTime]
+	SELECT [Id], [Name], [Extension], [IsUsed], [ModuleName], [UserID], IsTemp, ConfigID, Settings, [CreatedTime],FileUniqueId
 	FROM [common].[File] WITH(NOLOCK) 
 	WHERE Id = @Id
 END
