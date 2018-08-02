b392c33da88917e180b25c33b7d586e9e188bc45
Wissam Ajamy <VANRISE\wissam.ajamy>
2018-08-02 08:23:05 +0000

    Update structure from TOneConfiguration add FileUniqueId
    git-tfs-id: [http://developmentvlan:8585/tfs/vanrise.collection]$/;C48604

---
+++ TOneV2/Code/TOneV2/SQL.TOneConfiguration/common/Stored Procedures/sp_File_GetInfoByUniqueId.sql
@@ -0,0 +1,9 @@
+﻿
+Create PROCEDURE [common].[sp_File_GetInfoByUniqueId]
+	@fileUniqueId uniqueidentifier
+AS
+BEGIN
+	SELECT [Id], [Name], [Extension], [IsUsed], [ModuleName], [UserID], IsTemp, ConfigID, Settings, [CreatedTime], [FileUniqueId]
+	FROM [common].[File] WITH(NOLOCK) 
+	WHERE FileUniqueId = @fileUniqueId
+END
