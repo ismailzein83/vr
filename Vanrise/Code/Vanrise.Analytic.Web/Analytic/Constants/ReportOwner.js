96fa888b499ed7af72d328d015cc520fc92d278d
Naim Saad <VANRISE\naim.saad>
2018-08-02 12:35:34 +0000

    Fixing Report Owner Enum description
    git-tfs-id: [http://developmentvlan:8585/tfs/vanrise.collection]$/;C48619

--- Vanrise/Code/Vanrise.Analytic.Web/Analytic/Constants/ReportOwner.js
+++ Vanrise/Code/Vanrise.Analytic.Web/Analytic/Constants/ReportOwner.js
@@ -1,4 +1,4 @@
 ﻿app.constant('VR_Analytic_ReportOwner', {
-    AllReports: { value: 0, description: "AllReports" },
-    OnlyMyReports: { value: 1, description: "OnlyMyReports" }
+    AllReports: { value: 0, description: "All Reports" },
+    OnlyMyReports: { value: 1, description: "Only My Reports" }
 });
