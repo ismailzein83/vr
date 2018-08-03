50d56d1bbb3675a498d1a5359290b8a8b64c46f1
Wissam Ajamy <VANRISE\wissam.ajamy>
2018-08-03 09:42:33 +0000

    Update structure from TOneConfiguration
    git-tfs-id: [http://developmentvlan:8585/tfs/vanrise.collection]$/;C48653

---
+++ TOneV2/Code/TOneV2/SQL.TOneConfiguration/common/Stored Procedures/sp_File_SetUsedAndUpdateSettingsByFileUniqueId.sql
@@ -0,0 +1,15 @@
+﻿
+CREATE PROCEDURE [common].[sp_File_SetUsedAndUpdateSettingsByFileUniqueId]
+	-- Add the parameters for the stored procedure here
+	@FileUniqueId uniqueidentifier,
+	@ConfigID uniqueidentifier,
+	@Settings NVARCHAR(MAX)
+AS
+BEGIN
+	
+		UPDATE	[common].[File]
+		SET		[IsTemp] = 0,
+				ConfigID = @ConfigID,
+				Settings = @Settings
+		WHERE	FileUniqueId = @FileUniqueId
+END
