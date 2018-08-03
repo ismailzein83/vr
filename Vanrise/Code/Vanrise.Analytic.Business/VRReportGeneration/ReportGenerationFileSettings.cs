41a9e42c9f226bce3b1cb38d9532081cbddc3d94
Naim Saad <VANRISE\naim.saad>
2018-08-03 06:55:19 +0000

    Implementing ReportGenerationFileSettings
    git-tfs-id: [http://developmentvlan:8585/tfs/vanrise.collection]$/;C48642

---
+++ Vanrise/Code/Vanrise.Analytic.Business/VRReportGeneration/ReportGenerationFileSettings.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Vanrise.Analytic.Business
+{
+    public class ReportGenerationFileSettings : Vanrise.Entities.VRFileExtendedSettings
+    {
+        public override Guid ConfigId
+        {
+            get { return new Guid("641D41B6-2EE6-43D1-8F09-2DB827F48A2F");  }
+        }
+
+        public override bool DoesUserHaveViewAccess(Vanrise.Entities.IVRFileDoesUserHaveViewAccessContext context)
+        {
+            return true;
+        }
+    }
+}
