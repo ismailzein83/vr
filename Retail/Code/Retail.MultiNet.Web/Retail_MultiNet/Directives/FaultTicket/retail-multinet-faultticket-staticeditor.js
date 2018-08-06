ea86ffc5390a94355c51da2f57ee05b9e7ac8d9a
Naim Saad <VANRISE\naim.saad>
2018-08-06 10:09:49 +0000

    Implementing FaultTicketStaticEditor
    git-tfs-id: [http://developmentvlan:8585/tfs/vanrise.collection]$/;C48673

---
+++ Retail/Code/Retail.MultiNet.Web/Retail_MultiNet/Directives/FaultTicket/retail-multinet-faultticket-staticeditor.js
@@ -0,0 +1,52 @@
+﻿'use strict';
+
+app.directive('retailMultinetFaultticketStaticeditor', ['UtilsService', 'VRUIUtilsService', 'VRDateTimeService', 'WhS_BE_SaleZoneAPIService', 'WhS_BE_FaultTicketAPIService',
+    function (UtilsService, VRUIUtilsService, VRDateTimeService, WhS_BE_SaleZoneAPIService, WhS_BE_FaultTicketAPIService) {
+        var directiveDefinitionObject = {
+            restrict: 'E',
+            scope: {
+                onReady: '=',
+                isrequired: '='
+            },
+            controller: function ($scope, $element, $attrs) {
+                var ctrl = this;
+                var ctor = new retailMultinetFaultticketStaticeditor(ctrl, $scope, $attrs);
+                ctor.initializeController();
+            },
+            controllerAs: 'ctrl',
+            bindToController: true,
+            compile: function (element, attrs) {
+            },
+            templateUrl: "/Client/Modules/Retail_MultiNet/Directives/FaultTicket/Templates/FaultTicketStaticEditor.html"
+        };
+
+        function retailMultinetFaultticketStaticeditor(ctrl, $scope, $attrs) {
+          
+
+            this.initializeController = initializeController;
+            function initializeController() {
+                $scope.scopeModel = {};
+
+                $scope.scopeModel.codeNumberList = [];
+
+               
+            }
+
+            function defineApi() {
+                var api = {};
+                api.load = function (payload) {                   
+
+                };
+
+                api.setData = function (faultTicketObject) {
+                    
+                };
+
+                if (ctrl.onReady != null)
+                    ctrl.onReady(api);
+            }
+
+
+        }
+        return directiveDefinitionObject;
+    }]);
