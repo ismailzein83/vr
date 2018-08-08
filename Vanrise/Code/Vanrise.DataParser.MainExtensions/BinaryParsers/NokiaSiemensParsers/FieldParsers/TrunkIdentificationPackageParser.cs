8fe7510b29e9350aa83b5827c2e3fec168fa30b3
Ali Atoui <VANRISE\ali.atoui>
2018-08-08 13:33:51 +0000

    Implementing TrunkIdentificationCICPackageParser
    git-tfs-id: [http://developmentvlan:8585/tfs/vanrise.collection]$/;C48766

--- Vanrise/Code/Vanrise.DataParser.MainExtensions/BinaryParsers/NokiaSiemensParsers/FieldParsers/TrunkIdentificationPackageParser.cs
+++ Vanrise/Code/Vanrise.DataParser.MainExtensions/BinaryParsers/NokiaSiemensParsers/FieldParsers/TrunkIdentificationPackageParser.cs
@@ -22,4 +22,18 @@ namespace Vanrise.DataParser.MainExtensions.BinaryParsers.NokiaSiemensParsers.Fi
             context.Record.SetFieldValue(this.TrunkNumberFieldName, trunkNumber);
         }
     }
+
+    public class TrunkIdentificationCICPackageParser : BinaryFieldParserSettings
+    {
+        public override Guid ConfigId { get { return new Guid("CEEDF4FA-E2F7-47BC-91AB-EC176AB8B519"); } }
+        public string TrunkGroupNumberCICFieldName { get; set; }
+
+        public override void Execute(IBinaryFieldParserContext context)
+        {
+            byte[] trunkGroupNumberCICBytes = ParserHelper.ExtractFromByteArray(context.FieldValue, 0, 6, false);
+            string trunkGroupNumberCIC = System.Text.Encoding.UTF8.GetString(trunkGroupNumberCICBytes);
+
+            context.Record.SetFieldValue(this.TrunkGroupNumberCICFieldName, trunkGroupNumberCIC);
+        }
+    }
 }
