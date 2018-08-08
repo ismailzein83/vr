82d9cb1a88eca598cc821a34c30c2e32d120db0b
Naim Saad <VANRISE\naim.saad>
2018-08-08 14:07:46 +0000

    Implementing CheckUnpricedCDRsInvoiceSettingPart
    git-tfs-id: [http://developmentvlan:8585/tfs/vanrise.collection]$/;C48770

---
+++ TOneV2/Code/TOneV2/TOne.WhS.Invoice.Business/Extensions/CheckUnpricedCDRsInvoiceSettingPart.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TOne.WhS.Invoice.Business.Extensions
+{
+    public class CheckUnpricedCDRsInvoiceSettingPart : Vanrise.Invoice.Entities.InvoiceSettingPart
+    {
+        public bool IsEnabled { get; set; }
+        public override Guid ConfigId
+        {
+            get { return new Guid("28840BD1-3C9A-4793-8A46-64992F823C43"); }
+        }
+    }
+}
