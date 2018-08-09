1e08a215c3f28573da83726a9416f327bc6fade4
Samer Haidar <VANRISE\samer.haidar>
2018-08-09 07:05:46 +0000

    adding IGenericBusinessEntityFilter
    git-tfs-id: [http://developmentvlan:8585/tfs/vanrise.collection]$/;C48780

--- Vanrise/Code/Vanrise.GenericData.Entities/GenericBusinessEntity/GenericBusinessEntityFilter.cs
+++ Vanrise/Code/Vanrise.GenericData.Entities/GenericBusinessEntity/GenericBusinessEntityFilter.cs
@@ -8,5 +8,22 @@ namespace Vanrise.GenericData.Entities
 {
     public class GenericBusinessEntityInfoFilter
     {
+        public IEnumerable<IGenericBusinessEntityFilter> Filters { get; set; }
+    }
+
+    public interface IGenericBusinessEntityFilter
+    {
+        bool IsMatch(IGenericBusinessEntityFilterContext context);
+    }
+
+    public interface IGenericBusinessEntityFilterContext
+    {
+        GenericBusinessEntity GenericBusinessEntity { get; }
+
+    }
+
+    public class GenericBusinessEntityFilterContext : IGenericBusinessEntityFilterContext
+    {
+        GenericBusinessEntity GenericBusinessEntity { get; set; }
     }
 }
