bee7ef46827ce4295e9c85aa0180f666053a5a30
Naim Saad <VANRISE\naim.saad>
2018-08-09 19:16:20 +0000

    Implementing FaultTicketsSettings
    git-tfs-id: [http://developmentvlan:8585/tfs/vanrise.collection]$/;C48813

---
+++ Retail/Code/Retail.BusinessEntity.Business/FaultTickets/FaultTicketsSettings.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Vanrise.GenericData.Business;
+
+namespace Retail.BusinessEntity.Business
+{
+    public class FaultTicketsSettings : GenericBEExtendedSettings
+    {
+        public override Guid ConfigId
+        {
+            get { return new Guid("B24015FB-D7C9-4051-9FA6-9521EBC3EF9D"); }
+        }
+        //public override object GetInfoByType(IGenericBEExtendedSettingsContext context)
+        //{
+        //    if (context.InfoType == null)
+        //        return null;
+
+        //    switch (context.InfoType)
+        //    {
+        //        case "SerialNumberPattern": return new ConfigManager().GetFaultTicketsSerialNumberPattern();
+        //        case "SerialNumberInitialSequence": return new ConfigManager().GetFaultTicketsSerialNumberInitialSequence();
+        //        case "OpenTicketMailTemplate": return new ConfigManager().GetFaultTicketsOpenMailTemplateId();
+        //        case "PendingTicketMailTemplate": return new ConfigManager().GetFaultTicketsPendingMailTemplateId();
+        //        case "ClosedTicketMailTemplate": return new ConfigManager().GetFaultTicketsClosedMailTemplateId();
+                
+        //    }
+        //}
+    }
+}
