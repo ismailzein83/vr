b18b6850c332a1a7bb1e3a99571f038d6bd6c648
Samer Haidar <VANRISE\samer.haidar>
2018-08-10 08:19:41 +0000

    Adding CDRRecordStorageSource
    git-tfs-id: [http://developmentvlan:8585/tfs/vanrise.collection]$/;C48828

---
+++ TOneV2/Code/TOneV2/TOne.WhS.CDRComparison.Business/CDRRecordStorageSource.cs
@@ -0,0 +1,37 @@
+﻿using CDRComparison.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Vanrise.GenericData.Entities;
+
+namespace TOne.WhS.CDRComparison.Business
+{
+    public class CDRRecordStorageSource : CDRSource
+    {
+        public override Guid ConfigId
+        {
+            get { return new Guid("D70BCB92-C0DB-45DA-9E7B-3F8059DA5873"); }
+        }
+
+        public override void ReadCDRs(IReadCDRsFromSourceContext context)
+        {
+           // throw new NotImplementedException();
+        }
+
+        public override CDRSample ReadSample(IReadSampleFromSourceContext context)
+        {
+            return null;
+           // throw new NotImplementedException();
+        }
+        public Guid DataRecordStorageId { get; set; }
+        public DateTime FromDate { get; set; }
+        public DateTime ToDate { get; set; }
+        public List<DataRecordFilter> Filters { get; set; }
+        public RecordFilterGroup FilterGroup { get; set; }
+        public int? LimitResult { get; set; }
+        public OrderDirection Direction { get; set; }
+
+    }
+}
