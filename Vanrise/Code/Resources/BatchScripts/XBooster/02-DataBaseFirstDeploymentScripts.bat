b3bb6f633604a6cd41b9e30a9048f78c652a120e
Wissam Ajamy <VANRISE\wissam.ajamy>
2018-08-10 08:14:16 +0000

    Add VR_GenericData Module
    git-tfs-id: [http://developmentvlan:8585/tfs/vanrise.collection]$/;C48827

--- Vanrise/Code/Resources/BatchScripts/XBooster/02-DataBaseFirstDeploymentScripts.bat
+++ Vanrise/Code/Resources/BatchScripts/XBooster/02-DataBaseFirstDeploymentScripts.bat
@@ -7,20 +7,16 @@ For /F %PARSEARG% %%i in (%CURRDATE%) Do SET YYYYMMDD=%%l%%j%%k
 
 ECHO XBooster DataBase First Deployment Scripts
 
-xcopy "C:\TFS\TOneV2\Code\TOneV2\SQL.TOneConfiguration\Security.PostDeployment.sql" /y /v /z /i /Q /R  "\\192.168.110.185\Fixes\WebSite\XBooster\%YYYYMMDD%\DataBaseFirstDeploymentScripts\Configuration\"
-xcopy "C:\TFS\TOneV2\Code\TOneV2\SQL.TOneConfiguration\Common.PostDeployment.sql" /y /v /z /i /Q /R  "\\192.168.110.185\Fixes\WebSite\XBooster\%YYYYMMDD%\DataBaseFirstDeploymentScripts\Configuration\"
-xcopy "C:\TFS\TOneV2\Code\TOneV2\SQL.TOneConfiguration\Runtime.PostDeployment.sql" /y /v /z /i /Q /R  "\\192.168.110.185\Fixes\WebSite\XBooster\%YYYYMMDD%\DataBaseFirstDeploymentScripts\Configuration\"
-xcopy "C:\TFS\TOneV2\Code\TOneV2\SQL.TOneConfiguration\BusinessProcess.PostDeployment.sql" /y /v /z /i /Q /R  "\\192.168.110.185\Fixes\WebSite\XBooster\%YYYYMMDD%\DataBaseFirstDeploymentScripts\Configuration\"
-xcopy "C:\TFS\TOneV2\Code\TOneV2\SQL.TOneConfiguration\VR_Rules.PostDeployment.sql" /y /v /z /i /Q /R  "\\192.168.110.185\Fixes\WebSite\XBooster\%YYYYMMDD%\DataBaseFirstDeploymentScripts\Configuration\"
-xcopy "C:\TFS\TOneV2\Code\TOneV2\SQL.TOneConfiguration\ExcelConversion.PostDeployment.sql" /y /v /z /i /Q /R  "\\192.168.110.185\Fixes\WebSite\XBooster\%YYYYMMDD%\DataBaseFirstDeploymentScripts\Configuration\"
-
-xcopy "C:\TFS\TOneV2\Code\TOneV2\SQL.TOneConfiguration\Xbooster.PostDeployment.sql" /y /v /z /i /Q /R  "\\192.168.110.185\Fixes\WebSite\XBooster\%YYYYMMDD%\DataBaseFirstDeploymentScripts\Configuration\"
-xcopy "C:\TFS\TOneV2\Code\TOneV2\SQL.TOneConfiguration\User_Guides.PostDeployment.sql" /y /v /z /i /Q /R  "\\192.168.110.185\Fixes\WebSite\XBooster\%YYYYMMDD%\DataBaseFirstDeploymentScripts\Configuration\"
-
-xcopy "C:\TFS\TOneV2\Code\TOneV2\SQL.TOneConfiguration\Configuration.txt" /y /v /z /i /Q /R  "\\192.168.110.185\Fixes\WebSite\XBooster\%YYYYMMDD%\DataBaseFirstDeploymentScripts\Configuration\"
-
+xcopy "C:\TFS\TOneV2\Code\TOneV2\SQL.TOneConfiguration\Security.PostDeployment.sql" /y /v /z /i /Q /R				"\\192.168.110.185\Fixes\WebSite\XBooster\%YYYYMMDD%\DataBaseFirstDeploymentScripts\Configuration\"
+xcopy "C:\TFS\TOneV2\Code\TOneV2\SQL.TOneConfiguration\Common.PostDeployment.sql" /y /v /z /i /Q /R					"\\192.168.110.185\Fixes\WebSite\XBooster\%YYYYMMDD%\DataBaseFirstDeploymentScripts\Configuration\"
+xcopy "C:\TFS\TOneV2\Code\TOneV2\SQL.TOneConfiguration\Generic.PostDeployment.sql" /y /v /z /i /Q /R				"\\192.168.110.185\Fixes\WebSite\XBooster\%YYYYMMDD%\DataBaseFirstDeploymentScripts\Configuration\"
+xcopy "C:\TFS\TOneV2\Code\TOneV2\SQL.TOneConfiguration\Runtime.PostDeployment.sql" /y /v /z /i /Q /R				"\\192.168.110.185\Fixes\WebSite\XBooster\%YYYYMMDD%\DataBaseFirstDeploymentScripts\Configuration\"
+xcopy "C:\TFS\TOneV2\Code\TOneV2\SQL.TOneConfiguration\BusinessProcess.PostDeployment.sql" /y /v /z /i /Q /R		"\\192.168.110.185\Fixes\WebSite\XBooster\%YYYYMMDD%\DataBaseFirstDeploymentScripts\Configuration\"
+xcopy "C:\TFS\TOneV2\Code\TOneV2\SQL.TOneConfiguration\VR_Rules.PostDeployment.sql" /y /v /z /i /Q /R				"\\192.168.110.185\Fixes\WebSite\XBooster\%YYYYMMDD%\DataBaseFirstDeploymentScripts\Configuration\"
+xcopy "C:\TFS\TOneV2\Code\TOneV2\SQL.TOneConfiguration\ExcelConversion.PostDeployment.sql" /y /v /z /i /Q /R		"\\192.168.110.185\Fixes\WebSite\XBooster\%YYYYMMDD%\DataBaseFirstDeploymentScripts\Configuration\"
+xcopy "C:\TFS\TOneV2\Code\TOneV2\SQL.TOneConfiguration\Xbooster.PostDeployment.sql" /y /v /z /i /Q /R				"\\192.168.110.185\Fixes\WebSite\XBooster\%YYYYMMDD%\DataBaseFirstDeploymentScripts\Configuration\"
+xcopy "C:\TFS\TOneV2\Code\TOneV2\SQL.TOneConfiguration\User_Guides.PostDeployment.sql" /y /v /z /i /Q /R			"\\192.168.110.185\Fixes\WebSite\XBooster\%YYYYMMDD%\DataBaseFirstDeploymentScripts\Configuration\"
+xcopy "C:\TFS\TOneV2\Code\TOneV2\SQL.TOneConfiguration\Configuration.txt" /y /v /z /i /Q /R							"\\192.168.110.185\Fixes\WebSite\XBooster\%YYYYMMDD%\DataBaseFirstDeploymentScripts\Configuration\"
+xcopy "C:\TFS\TOneV2\Code\TOneV2\SQL.TOneTransaction\BusinessProcess.PostDeployment.sql" /y /v /z /i /Q /R			"\\192.168.110.185\Fixes\WebSite\XBooster\%YYYYMMDD%\DataBaseFirstDeploymentScripts\Transaction\"
+xcopy "C:\TFS\TOneV2\Code\TOneV2\SQL.TOneLogging\Script.PostDeployment.sql" /y /v /z /i /Q /R						"\\192.168.110.185\Fixes\WebSite\XBooster\%YYYYMMDD%\DataBaseFirstDeploymentScripts\Logging\"
 start /b /w /D"C:\TFS\Vanrise\Code\Vanrise.HelperTools\bin\Release" Vanrise.HelperTools.exe "Enumerations" "XBooster"
-
-xcopy "C:\TFS\TOneV2\Code\TOneV2\SQL.TOneTransaction\BusinessProcess.PostDeployment.sql" /y /v /z /i /Q /R  "\\192.168.110.185\Fixes\WebSite\XBooster\%YYYYMMDD%\DataBaseFirstDeploymentScripts\Transaction\"
-
-xcopy "C:\TFS\TOneV2\Code\TOneV2\SQL.TOneLogging\Script.PostDeployment.sql" /y /v /z /i /Q /R  "\\192.168.110.185\Fixes\WebSite\XBooster\%YYYYMMDD%\DataBaseFirstDeploymentScripts\Logging\"
--- Vanrise/Code/Resources/BatchScripts/XBooster/30-WebSite_FullVersion.bat
+++ Vanrise/Code/Resources/BatchScripts/XBooster/30-WebSite_FullVersion.bat
@@ -8,18 +8,17 @@ For /F %PARSEARG% %%i in (%CURRDATE%) Do SET YYYYMMDD=%%l%%j%%k
 ECHO XBooster WebSite Full Version
 
 xcopy "C:\Publish\XBooster" /S /E /R /y /v /i /z /Q																	"\\192.168.110.185\FixesFullVersion\WebSite\XBooster\%YYYYMMDD%\" /EXCLUDE:C:\TFS\Vanrise\Code\Resources\BatchScripts\XBooster\list-of-excluded-files.txt
-xcopy "C:\TFS\Xbooster\Code\Xbooster.Web\Web.config.exclude" /y /v /z /i /Q /R										"\\192.168.110.185\FixesFullVersion\WebSite\XBooster\%YYYYMMDD%\"
-rename "\\192.168.110.185\FixesFullVersion\WebSite\XBooster\%YYYYMMDD%\Web.config.exclude" "Web.config"
-
 xcopy "C:\TFS\Vanrise\Code\Resources\DLLs\Aspose.Cells.lic" /y /v /z /i /Q /R										"\\192.168.110.185\FixesFullVersion\WebSite\XBooster\%YYYYMMDD%\Bin\"
 xcopy "C:\TFS\Vanrise\Code\Vanrise.Web\Client" /S /E /R /y /v /i /z /Q												"\\192.168.110.185\FixesFullVersion\WebSite\XBooster\%YYYYMMDD%\Client\"
 xcopy "C:\TFS\Vanrise\Code\Vanrise.Security.Web\Security" /S /E /R /y /v /i /z /Q									"\\192.168.110.185\FixesFullVersion\WebSite\XBooster\%YYYYMMDD%\Client\Modules\Security\"
 xcopy "C:\TFS\Vanrise\Code\Vanrise.Common.Web\Common" /S /E /R /y /v /i /z /Q										"\\192.168.110.185\FixesFullVersion\WebSite\XBooster\%YYYYMMDD%\Client\Modules\Common\"
+xcopy "C:\TFS\Vanrise\Code\Vanrise.GenericData.Web\VR_GenericData" /S /E /R /y /v /i /z /Q							"\\192.168.110.185\Fixes\WebSite\XBooster\%YYYYMMDD%\Client\Modules\VR_GenericData\"
 xcopy "C:\TFS\Vanrise\Code\Vanrise.Runtime.Web\Runtime" /S /E /R /y /v /i /z /Q										"\\192.168.110.185\FixesFullVersion\WebSite\XBooster\%YYYYMMDD%\Client\Modules\Runtime\"
 xcopy "C:\TFS\BusinessProcess\Code\Vanrise.BusinessProcess.Web\BusinessProcess" /S /E /R /y /v /i /z /Q				"\\192.168.110.185\FixesFullVersion\WebSite\XBooster\%YYYYMMDD%\Client\Modules\BusinessProcess\"
 xcopy "C:\TFS\Vanrise\Code\Vanrise.Rules.Web\VR_Rules" /S /E /R /y /v /i /z /Q										"\\192.168.110.185\FixesFullVersion\WebSite\XBooster\%YYYYMMDD%\Client\Modules\VR_Rules\"
 xcopy "C:\TFS\Vanrise\Code\Vanrise.GenericData.Web\VR_GenericData" /S /E /R /y /v /i /z /Q							"\\192.168.110.185\FixesFullVersion\WebSite\XBooster\%YYYYMMDD%\Client\Modules\VR_GenericData\"
-
 xcopy "C:\TFS\Xbooster\Code\CDRComparison.Web\CDRComparison" /S /E /R /y /v /i /z /Q								"\\192.168.110.185\FixesFullVersion\WebSite\XBooster\%YYYYMMDD%\Client\Modules\CDRComparison\"
 xcopy "C:\TFS\Vanrise\Code\Vanrise.ExcelConversion.Web\ExcelConversion" /S /E /R /y /v /i /z /Q						"\\192.168.110.185\FixesFullVersion\WebSite\XBooster\%YYYYMMDD%\Client\Modules\ExcelConversion\"
 xcopy "C:\TFS\Xbooster\Code\XBooster.PriceListConversion.Web\XBooster_PriceListConversion" /S /E /R /y /v /i /z /Q	"\\192.168.110.185\FixesFullVersion\WebSite\XBooster\%YYYYMMDD%\Client\Modules\XBooster_PriceListConversion\"
+xcopy "C:\TFS\Xbooster\Code\Xbooster.Web\Web.config.exclude" /y /v /z /i /Q /R										"\\192.168.110.185\FixesFullVersion\WebSite\XBooster\%YYYYMMDD%\"
+rename "\\192.168.110.185\FixesFullVersion\WebSite\XBooster\%YYYYMMDD%\Web.config.exclude" "Web.config"
