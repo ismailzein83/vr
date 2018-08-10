f9dd47dccd848168f6d4afc6ef87d6a60507b451
yara.bousalman <yara.bousalman@vanrise.tfs.local>
2018-08-10 06:02:48 +0000

    Adding GenericBusinessEntityFilter
    git-tfs-id: [http://developmentvlan:8585/tfs/vanrise.collection]$/;C48814

---
+++ Vanrise/Code/Vanrise.Invoice.Business/GenericBusinessEntityFilter.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Vanrise.GenericData.Entities;
+using Vanrise.Common;
+namespace Vanrise.Invoice.Business
+{
+    class GenericBusinessEntityFilter : IGenericBusinessEntityFilter
+    {
+        public Guid InvoiceTypeId { get; set; }
+
+        public bool IsMatch(IGenericBusinessEntityFilterContext context)
+        {
+            if (context != null) 
+            {
+                if (context.GenericBusinessEntityDetail != null && context.GenericBusinessEntityDetail.FieldValues != null && context.GenericBusinessEntityDetail.FieldValues.Count > 0)
+                {
+                    var genericBEValue = context.GenericBusinessEntityDetail.FieldValues.GetRecord("InvoiceTypeId");
+                    if (genericBEValue != null)
+                    {
+                        if ((Guid)genericBEValue.Value != this.InvoiceTypeId)
+                        {
+                            return false;
+                        }
+                    }
+                   
+                }
+            }
+            return false;
+        } 
+    }
+}
