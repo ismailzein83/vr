c24c33cc8181e486a63429a653df95e8a1b99741
Rodi Hassan <VANRISE\rodi.hassan>
2018-08-14 15:33:31 +0000

    implement VRHttpConnectionFilter
    git-tfs-id: [http://developmentvlan:8585/tfs/vanrise.collection]$/;C48893

--- Vanrise/Code/Vanrise.Common.Business/VRConnections/MainImplementations/VRHttpConnection.cs
+++ Vanrise/Code/Vanrise.Common.Business/VRConnections/MainImplementations/VRHttpConnection.cs
@@ -7,255 +7,274 @@ using Vanrise.Entities;
 
 namespace Vanrise.Common.Business
 {
-    public class VRHttpConnection : VRConnectionSettings
-    {
-        public override Guid ConfigId
-        {
-            get { return new Guid("071D54D2-463B-4404-8219-45FCD539FF01"); }
-        }
+	public class VRHttpConnection : VRConnectionSettings
+	{
+		public override Guid ConfigId
+		{
+			get { return s_ConfigId; }
+		}
 
-        public string BaseURL { get; set; }
+		public static Guid s_ConfigId = new Guid("071D54D2-463B-4404-8219-45FCD539FF01");
 
-        public List<VRHttpHeader> Headers { get; set; }
+		public string BaseURL { get; set; }
 
-        public virtual bool TrySendRequest(string actionPath, VRHttpMethod httpMethod, VRHttpMessageFormat messageFormat, Dictionary<string, string> urlParameters,
-            Dictionary<string, string> headers, string body, Action<VRHttpResponse> onResponseReceived, bool throwIfError, Action<VRHttpFault> onError)
-        {
-            bool isSucceeded = false;
-            VRHttpResponse response = null;
-            VRHttpFault fault = null;
-            try
-            {
-                using (var client = new System.Net.Http.HttpClient())
-                {
-                    client.BaseAddress = new Uri(this.BaseURL);
-                    string messageFormatValue = Utilities.GetEnumAttribute<VRHttpMessageFormat, VRHttpMessageFormatAttribute>(messageFormat).Value;
-                    client.DefaultRequestHeaders.Accept.Add(new System.Net.Http.Headers.MediaTypeWithQualityHeaderValue(messageFormatValue));
-                    string actionWithParameters = BuildActionWithParameters(actionPath, urlParameters);
-                    using (System.Net.Http.HttpRequestMessage request = new System.Net.Http.HttpRequestMessage(MapVRHttpMethod(httpMethod), actionWithParameters))
-                    {
-                        AddHeaders(request, headers);
+		public List<VRHttpHeader> Headers { get; set; }
 
-                        if (!String.IsNullOrEmpty(body))
-                            request.Content = new System.Net.Http.StringContent(body, System.Text.Encoding.UTF8, messageFormatValue);
+		public virtual bool TrySendRequest(string actionPath, VRHttpMethod httpMethod, VRHttpMessageFormat messageFormat, Dictionary<string, string> urlParameters,
+			Dictionary<string, string> headers, string body, Action<VRHttpResponse> onResponseReceived, bool throwIfError, Action<VRHttpFault> onError)
+		{
+			bool isSucceeded = false;
+			VRHttpResponse response = null;
+			VRHttpFault fault = null;
+			try
+			{
+				using (var client = new System.Net.Http.HttpClient())
+				{
+					client.BaseAddress = new Uri(this.BaseURL);
+					string messageFormatValue = Utilities.GetEnumAttribute<VRHttpMessageFormat, VRHttpMessageFormatAttribute>(messageFormat).Value;
+					client.DefaultRequestHeaders.Accept.Add(new System.Net.Http.Headers.MediaTypeWithQualityHeaderValue(messageFormatValue));
+					string actionWithParameters = BuildActionWithParameters(actionPath, urlParameters);
+					using (System.Net.Http.HttpRequestMessage request = new System.Net.Http.HttpRequestMessage(MapVRHttpMethod(httpMethod), actionWithParameters))
+					{
+						AddHeaders(request, headers);
 
-                        using (var responseTask = client.SendAsync(request))
-                        {
-                            responseTask.Wait();
-                            System.Net.Http.HttpResponseMessage responseMessage = responseTask.Result;
-                            string responseString = responseMessage.Content.ReadAsStringAsync().Result;
-                            if (responseTask.Exception == null && responseMessage.IsSuccessStatusCode)
-                            {
-                                isSucceeded = true;
-                                response = new VRHttpResponse(responseString, responseMessage.StatusCode, messageFormat);
-                            }
-                            else
-                            {
-                                fault = new VRHttpFault(responseTask.Exception, responseString, responseMessage.StatusCode, messageFormat);
-                            }
-                        }
-                    }
-                }
-            }
-            catch (Exception ex)
-            {
-                onError(new VRHttpFault(ex));
-                if (throwIfError)
-                    throw;
-            }
-            if (fault != null)
-            {
-                onError(fault);
-                if (throwIfError)
-                    throw fault.Exception;
-            }
-            else if (response != null)
-            {
-                onResponseReceived(response);
-            }
-            return isSucceeded;
-        }
+						if (!String.IsNullOrEmpty(body))
+							request.Content = new System.Net.Http.StringContent(body, System.Text.Encoding.UTF8, messageFormatValue);
 
-        private System.Net.Http.HttpMethod MapVRHttpMethod(VRHttpMethod vrHttpMethod)
-        {
-            switch (vrHttpMethod)
-            {
-                case VRHttpMethod.Get: return System.Net.Http.HttpMethod.Get;
-                case VRHttpMethod.Post: return System.Net.Http.HttpMethod.Post;
-                case VRHttpMethod.Put: return System.Net.Http.HttpMethod.Put;
-                case VRHttpMethod.Delete: return System.Net.Http.HttpMethod.Delete;
-                default: throw new NotSupportedException(string.Format("unsupported vrHttpMethod '{0}'", vrHttpMethod.ToString()));
-            }
-        }
+						using (var responseTask = client.SendAsync(request))
+						{
+							responseTask.Wait();
+							System.Net.Http.HttpResponseMessage responseMessage = responseTask.Result;
+							string responseString = responseMessage.Content.ReadAsStringAsync().Result;
+							if (responseTask.Exception == null && responseMessage.IsSuccessStatusCode)
+							{
+								isSucceeded = true;
+								response = new VRHttpResponse(responseString, responseMessage.StatusCode, messageFormat);
+							}
+							else
+							{
+								fault = new VRHttpFault(responseTask.Exception, responseString, responseMessage.StatusCode, messageFormat);
+							}
+						}
+					}
+				}
+			}
+			catch (Exception ex)
+			{
+				onError(new VRHttpFault(ex));
+				if (throwIfError)
+					throw;
+			}
+			if (fault != null)
+			{
+				onError(fault);
+				if (throwIfError)
+					throw fault.Exception;
+			}
+			else if (response != null)
+			{
+				onResponseReceived(response);
+			}
+			return isSucceeded;
+		}
 
-        private string BuildActionWithParameters(string actionPath, Dictionary<string, string> urlParameters)
-        {
-            actionPath.ThrowIfNull("actionPath");
-            if (urlParameters != null && urlParameters.Count > 0)
-                return string.Concat(actionPath, "?", string.Join("&", urlParameters.Select(itm => string.Concat(itm.Key, "=", itm.Value))));
-            else
-                return actionPath;
-        }
+		private System.Net.Http.HttpMethod MapVRHttpMethod(VRHttpMethod vrHttpMethod)
+		{
+			switch (vrHttpMethod)
+			{
+				case VRHttpMethod.Get: return System.Net.Http.HttpMethod.Get;
+				case VRHttpMethod.Post: return System.Net.Http.HttpMethod.Post;
+				case VRHttpMethod.Put: return System.Net.Http.HttpMethod.Put;
+				case VRHttpMethod.Delete: return System.Net.Http.HttpMethod.Delete;
+				default: throw new NotSupportedException(string.Format("unsupported vrHttpMethod '{0}'", vrHttpMethod.ToString()));
+			}
+		}
 
-        private void AddHeaders(System.Net.Http.HttpRequestMessage request, Dictionary<string, string> requestHeaders)
-        {
-            if (this.Headers != null)
-            {
-                foreach (var header in this.Headers)
-                {
-                    request.Headers.Add(header.Name, header.Value);
-                }
-            }
-            if (requestHeaders != null)
-            {
-                foreach (var headerEntry in requestHeaders)
-                {
-                    request.Headers.Add(headerEntry.Key, headerEntry.Value);
-                }
-            }
-        }
-    }
+		private string BuildActionWithParameters(string actionPath, Dictionary<string, string> urlParameters)
+		{
+			actionPath.ThrowIfNull("actionPath");
+			if (urlParameters != null && urlParameters.Count > 0)
+				return string.Concat(actionPath, "?", string.Join("&", urlParameters.Select(itm => string.Concat(itm.Key, "=", itm.Value))));
+			else
+				return actionPath;
+		}
 
-    public class VRHttpHeader
-    {
-        public string Name { get; set; }
+		private void AddHeaders(System.Net.Http.HttpRequestMessage request, Dictionary<string, string> requestHeaders)
+		{
+			if (this.Headers != null)
+			{
+				foreach (var header in this.Headers)
+				{
+					request.Headers.Add(header.Name, header.Value);
+				}
+			}
+			if (requestHeaders != null)
+			{
+				foreach (var headerEntry in requestHeaders)
+				{
+					request.Headers.Add(headerEntry.Key, headerEntry.Value);
+				}
+			}
+		}
+	}
 
-        public string Value { get; set; }
-    }
+	public class VRHttpConnectionFilter : IVRConnectionFilter
+	{
+		public bool IsMatched(VRConnection vrConnection)
+		{
+			if (vrConnection == null)
+				throw new NullReferenceException("connection");
 
-    public class VRHttpResponse
-    {
-        VRHttpMessageFormat _messageFormat;
+			if (vrConnection.Settings == null)
+				throw new NullReferenceException("vrConnection.Settings");
 
-        internal VRHttpResponse(string response, System.Net.HttpStatusCode statusCode, VRHttpMessageFormat messageFormat)
-        {
-            this.Response = response;
-            this.StatusCode = statusCode;
-            _messageFormat = messageFormat;
-        }
+			if (vrConnection.Settings.ConfigId != VRHttpConnection.s_ConfigId)
+				return false;
 
-        public string Response { get; private set; }
+			return true;
+		}
+	}
 
-        public System.Net.HttpStatusCode StatusCode { get; private set; }
+	public class VRHttpHeader
+	{
+		public string Name { get; set; }
 
-        public T DeserializeResponse<T>()
-        {
-            if (String.IsNullOrEmpty(this.Response))
-            {
-                return default(T);
-            }
-            else
-            {
-                switch (_messageFormat)
-                {
-                    case VRHttpMessageFormat.ApplicationJSON:
-                        return Common.Serializer.Deserialize<T>(this.Response);
-                    case VRHttpMessageFormat.ApplicationXML:
-                    case VRHttpMessageFormat.TextXML:
-                        return new VRXmlSerializer().Deserialize<T>(this.Response);
-                    default: throw new NotSupportedException(String.Format("_messageFormat: '{0}'", _messageFormat.ToString()));
-                }
+		public string Value { get; set; }
+	}
 
-            }
-        }
-    }
+	public class VRHttpResponse
+	{
+		VRHttpMessageFormat _messageFormat;
 
-    public class VRHttpFault
-    {
-        VRHttpMessageFormat _messageFormat;
+		internal VRHttpResponse(string response, System.Net.HttpStatusCode statusCode, VRHttpMessageFormat messageFormat)
+		{
+			this.Response = response;
+			this.StatusCode = statusCode;
+			_messageFormat = messageFormat;
+		}
 
-        string _errorMessage;
-        Exception _exception;
+		public string Response { get; private set; }
 
-        public VRHttpFault(Exception exception, string response, System.Net.HttpStatusCode statusCode, VRHttpMessageFormat messageFormat)
-        {
-            this.StringResponse = response;
-            this.StatusCode = statusCode;
-            _messageFormat = messageFormat;
-            _exception = exception;
-            if (exception != null)
-                _errorMessage = _exception.Message;
-        }
+		public System.Net.HttpStatusCode StatusCode { get; private set; }
 
-        public VRHttpFault(Exception exception)
-        {
-            _exception = exception;
-            _errorMessage = exception.Message;
-            this.StatusCode = System.Net.HttpStatusCode.ServiceUnavailable;
-            this.StringResponse = "Request not sent!";
-        }
+		public T DeserializeResponse<T>()
+		{
+			if (String.IsNullOrEmpty(this.Response))
+			{
+				return default(T);
+			}
+			else
+			{
+				switch (_messageFormat)
+				{
+					case VRHttpMessageFormat.ApplicationJSON:
+						return Common.Serializer.Deserialize<T>(this.Response);
+					case VRHttpMessageFormat.ApplicationXML:
+					case VRHttpMessageFormat.TextXML:
+						return new VRXmlSerializer().Deserialize<T>(this.Response);
+					default: throw new NotSupportedException(String.Format("_messageFormat: '{0}'", _messageFormat.ToString()));
+				}
 
-        public string StringResponse { get; private set; }
+			}
+		}
+	}
 
-        public string ErrorMessage
-        {
-            get
-            {
-                ParseFault();
-                return _errorMessage;
-            }
-        }
+	public class VRHttpFault
+	{
+		VRHttpMessageFormat _messageFormat;
 
-        public Exception Exception
-        {
-            get
-            {
-                ParseFault();
-                return _exception;
-            }
-        }
+		string _errorMessage;
+		Exception _exception;
 
-        public System.Net.HttpStatusCode StatusCode { get; private set; }
+		public VRHttpFault(Exception exception, string response, System.Net.HttpStatusCode statusCode, VRHttpMessageFormat messageFormat)
+		{
+			this.StringResponse = response;
+			this.StatusCode = statusCode;
+			_messageFormat = messageFormat;
+			_exception = exception;
+			if (exception != null)
+				_errorMessage = _exception.Message;
+		}
 
-        void ParseFault()
-        {
-            if (_exception != null)
-                return;
-            switch (_messageFormat)
-            {
-                case VRHttpMessageFormat.ApplicationJSON:
-                    var jsonFault = Serializer.Deserialize<JSONFault>(this.StringResponse);
-                    if (jsonFault.Message != null && jsonFault.ExceptionMessage != null)
-                    {
-                        _errorMessage = jsonFault.Message;
-                        _exception = new Exception(string.Format("{0}: {1}. Stack Trace: {2}", jsonFault.Message, jsonFault.ExceptionMessage, jsonFault.StackTrace));
-                    }
-                    break;
-                case VRHttpMessageFormat.ApplicationXML:
-                case VRHttpMessageFormat.TextXML:
-                    var soapFault = new VRXmlSerializer().Deserialize<Fault>(this.StringResponse);
-                    if (soapFault.faultcode != null && soapFault.faultstring != null)
-                    {
-                        _errorMessage = soapFault.faultstring;
-                        _exception = new Exception(string.Format("{0}: {1}", soapFault.faultcode, soapFault.faultstring));
-                    }
-                    break;
-                default: throw new NotSupportedException(String.Format("_messageFormat: '{0}'", _messageFormat.ToString()));
-            }
-            if (_exception == null)
-                _exception = new Exception(this.StringResponse);
+		public VRHttpFault(Exception exception)
+		{
+			_exception = exception;
+			_errorMessage = exception.Message;
+			this.StatusCode = System.Net.HttpStatusCode.ServiceUnavailable;
+			this.StringResponse = "Request not sent!";
+		}
 
-            if (_errorMessage == null)
-                _errorMessage = _exception.Message;
-        }
+		public string StringResponse { get; private set; }
 
-        /// <summary>
-        /// SOAP Fault
-        /// </summary>
-        private class Fault
-        {
-            public string faultcode { get; set; }
+		public string ErrorMessage
+		{
+			get
+			{
+				ParseFault();
+				return _errorMessage;
+			}
+		}
 
-            public string faultstring { get; set; }
-        }
+		public Exception Exception
+		{
+			get
+			{
+				ParseFault();
+				return _exception;
+			}
+		}
 
-        private class JSONFault
-        {
-            public string Message { get; set; }
+		public System.Net.HttpStatusCode StatusCode { get; private set; }
 
-            public string ExceptionMessage { get; set; }
+		void ParseFault()
+		{
+			if (_exception != null)
+				return;
+			switch (_messageFormat)
+			{
+				case VRHttpMessageFormat.ApplicationJSON:
+					var jsonFault = Serializer.Deserialize<JSONFault>(this.StringResponse);
+					if (jsonFault.Message != null && jsonFault.ExceptionMessage != null)
+					{
+						_errorMessage = jsonFault.Message;
+						_exception = new Exception(string.Format("{0}: {1}. Stack Trace: {2}", jsonFault.Message, jsonFault.ExceptionMessage, jsonFault.StackTrace));
+					}
+					break;
+				case VRHttpMessageFormat.ApplicationXML:
+				case VRHttpMessageFormat.TextXML:
+					var soapFault = new VRXmlSerializer().Deserialize<Fault>(this.StringResponse);
+					if (soapFault.faultcode != null && soapFault.faultstring != null)
+					{
+						_errorMessage = soapFault.faultstring;
+						_exception = new Exception(string.Format("{0}: {1}", soapFault.faultcode, soapFault.faultstring));
+					}
+					break;
+				default: throw new NotSupportedException(String.Format("_messageFormat: '{0}'", _messageFormat.ToString()));
+			}
+			if (_exception == null)
+				_exception = new Exception(this.StringResponse);
 
-            public string StackTrace { get; set; }
-        }
-    }
+			if (_errorMessage == null)
+				_errorMessage = _exception.Message;
+		}
+
+		/// <summary>
+		/// SOAP Fault
+		/// </summary>
+		private class Fault
+		{
+			public string faultcode { get; set; }
+
+			public string faultstring { get; set; }
+		}
+
+		private class JSONFault
+		{
+			public string Message { get; set; }
+
+			public string ExceptionMessage { get; set; }
+
+			public string StackTrace { get; set; }
+		}
+	}
 }
