81565b607c3825fb78a28d242f3e375715ba576a
Samer Haidar <VANRISE\samer.haidar>
2018-08-14 13:51:16 +0000

    Adding VRComment Managers
    git-tfs-id: [http://developmentvlan:8585/tfs/vanrise.collection]$/;C48890

---
+++ Vanrise/Code/Vanrise.Entities/VRComment/VRComment.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Vanrise.Entities
+{
+    public class VRComment
+    {
+        public long VRCommentId { get; set; }
+        public Guid DefinitionId { get; set; }
+        public string ObjectId { get; set; }
+        public string Content { get; set; }
+        public int CreatedBy { get; set; }
+        public int LastModifiedBy { get; set; }
+        public DateTime CreatedTime { get; set; }
+        public DateTime LastModifiedTime { get; set; }
+    }
+}
