4ab0d984f4917690dde1fcdf5dbbc7ef945df101
Fatima Al Heck <VANRISE\fatima.alheck>
2018-08-14 08:34:10 +0000

    Implementing VRWorkflowCustomClassType
    git-tfs-id: [http://developmentvlan:8585/tfs/vanrise.collection]$/;C48879

--- BusinessProcess/Code/Vanrise.BusinessProcess.MainExtensions/VRWorkflowVariableTypes/VRWorkflowCustomClassType.cs
+++ BusinessProcess/Code/Vanrise.BusinessProcess.MainExtensions/VRWorkflowVariableTypes/VRWorkflowCustomClassType.cs
@@ -6,9 +6,21 @@ using System.Threading.Tasks;
 using Vanrise.BusinessProcess.Entities;
 using Vanrise.Entities;
 
-namespace Vanrise.BusinessProcess.MainExtensions
+namespace Vanrise.BusinessProcess.MainExtensions.VRWorkflowVariableTypes
 {
     public class VRWorkflowCustomClassType : VRWorkflowVariableType
     {
+        public override Guid ConfigId { get { return new Guid("A6078B0f-EFa2-414F-8a25-549628DA1762"); } }
+        public VRCustomClassType FieldType { get; set; }
+
+        public override Type GetRuntimeType(IVRWorkflowVariableTypeGetRuntimeTypeContext context)
+        {
+            throw new NotImplementedException();
+        }
+
+        public override string GetRuntimeTypeDescription()
+        {
+            throw new NotImplementedException();
+        }
     }
 }
