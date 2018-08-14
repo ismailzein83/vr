020d9ffe6f5fbce7dd73c0ced65708cccbaf2fd1
Fatima Al Heck <VANRISE\fatima.alheck>
2018-08-14 08:03:42 +0000

    Adding VRWorkflowCustomClassType class and directive
    git-tfs-id: [http://developmentvlan:8585/tfs/vanrise.collection]$/;C48876

---
+++ BusinessProcess/Code/Vanrise.BusinessProcess.MainExtensions/VRWorkflowVariableTypes/VRWorkflowCustomClassType.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Vanrise.BusinessProcess.Entities;
+using Vanrise.Entities;
+
+namespace Vanrise.BusinessProcess.MainExtensions
+{
+    public class VRWorkflowCustomClassType : VRWorkflowVariableType
+    {
+    }
+}
