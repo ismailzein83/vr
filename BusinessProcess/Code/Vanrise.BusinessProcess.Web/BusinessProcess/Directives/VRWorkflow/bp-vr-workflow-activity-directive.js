bed9bd8152efa87a5b5d514755b256c6ebd73555
Rodi Hassan <VANRISE\rodi.hassan>
2018-08-15 07:25:16 +0000

    add workflowID to activity directive
    git-tfs-id: [http://developmentvlan:8585/tfs/vanrise.collection]$/;C48910

--- BusinessProcess/Code/Vanrise.BusinessProcess.Web/BusinessProcess/Directives/VRWorkflow/bp-vr-workflow-activity-directive.js
+++ BusinessProcess/Code/Vanrise.BusinessProcess.Web/BusinessProcess/Directives/VRWorkflow/bp-vr-workflow-activity-directive.js
@@ -2,169 +2,158 @@
 
 app.directive("businessprocessVrWorkflowActivityDirective", ['UtilsService', 'VRUIUtilsService', 'BusinessProcess_VRWorkflowService',
 	function (UtilsService, VRUIUtilsService, BusinessProcess_VRWorkflowService) {
-	    var directiveDefinitionObject = {
-	        restrict: "E",
-	        scope: {
-	            dragdropsetting: '=',
-	            onRemove: '=',
-	            onReady: "="
-	        },
-	        controller: function ($scope, $element, $attrs) {
-	            var ctrl = this;
+		var directiveDefinitionObject = {
+			restrict: "E",
+			scope: {
+				dragdropsetting: '=',
+				onRemove: '=',
+				onReady: "="
+			},
+			controller: function ($scope, $element, $attrs) {
+				var ctrl = this;
 
-	            var directiveConstructor = new DirectiveConstructor($scope, ctrl);
-	            directiveConstructor.initializeController();
-	        },
-	        controllerAs: "ctrl",
-	        bindToController: true,
-	        templateUrl: '/Client/Modules/BusinessProcess/Directives/VRWorkflow/Templates/VRWorkflowActivityDirectiveTemplate.html'
-	    };
+				var directiveConstructor = new DirectiveConstructor($scope, ctrl);
+				directiveConstructor.initializeController();
+			},
+			controllerAs: "ctrl",
+			bindToController: true,
+			templateUrl: '/Client/Modules/BusinessProcess/Directives/VRWorkflow/Templates/VRWorkflowActivityDirectiveTemplate.html'
+		};
 
-	    function DirectiveConstructor($scope, ctrl) {
-	        var vRWorkflowActivityId;
-	        var vRWorkflowActivitySettings;
-	        var context;
+		function DirectiveConstructor($scope, ctrl) {
+			var vRWorkflowActivityId;
+			var vRWorkflowActivitySettings;
+			var context;
+			var inEditor;
 
-	        var directiveWraperAPI;
-	        var directiveWraperReadyPromiseDeferred = UtilsService.createPromiseDeferred();
+			var directiveWraperAPI;
+			var directiveWraperReadyPromiseDeferred = UtilsService.createPromiseDeferred();
 
-	        this.initializeController = initializeController;
+			this.initializeController = initializeController;
 
-	        function showErrorWarningIcon(value) {
-	            $scope.scopeModel.hasError = value;
-	            if (context != undefined && context.showErrorWarningIcon != undefined)
-	                context.showErrorWarningIcon(value);
-	        }
+			function showErrorWarningIcon(value) {
+				$scope.scopeModel.hasError = value;
+				if (context != undefined && context.showErrorWarningIcon != undefined)
+					context.showErrorWarningIcon(value);
+			}
 
-	        function initializeController() {
-	            $scope.scopeModel = {};
-	            $scope.scopeModel.actions = [];
-	            $scope.scopeModel.dragdropsetting = ctrl.dragdropsetting;
+			function initializeController() {
+				$scope.scopeModel = {};
+				$scope.scopeModel.actions = [];
+				$scope.scopeModel.dragdropsetting = ctrl.dragdropsetting;
 
-	            $scope.scopeModel.hasErrorsValidator = function () {
-	                if (context != undefined && vRWorkflowActivityId != undefined) {
-	                    var errors = context.doesActivityhaveErrors(vRWorkflowActivityId);
-	                    if (errors != undefined) {
-	                        if (context != undefined && context.showErrorWarningIcon != undefined)
-	                            context.showErrorWarningIcon(true);
-	                        var errorMessage = '';
-	                        for (var i = 0; i < errors.length; i++) {
-	                            errorMessage += (i + 1) + ') ' + errors[i] + '\n';
-	                        }
-	                        return errorMessage;
-	                    }
-	                }
-	                return null;
-	            };
-	            $scope.scopeModel.onDirectiveWraperReady = function (api) {
-	                directiveWraperAPI = api;
-	                directiveWraperReadyPromiseDeferred.resolve();
-	            };
+				$scope.scopeModel.hasErrorsValidator = function () {
+					if (!inEditor && context != undefined && vRWorkflowActivityId != undefined) {
+						var errors = context.doesActivityhaveErrors(vRWorkflowActivityId);
+						if (errors != undefined) {
+							if (context != undefined && context.showErrorWarningIcon != undefined)
+								context.showErrorWarningIcon(true);
+							var errorMessage = '';
+							for (var i = 0; i < errors.length; i++) {
+								errorMessage += (i + 1) + ') ' + errors[i] + '\n';
+							}
+							return errorMessage;
+						}
+					}
+					return null;
+				};
+				$scope.scopeModel.onDirectiveWraperReady = function (api) {
+					directiveWraperAPI = api;
+					directiveWraperReadyPromiseDeferred.resolve();
+				};
 
-	            defineAPI();
-	        }
+				defineAPI();
+			}
 
-	        function resetErrorsFunction() {
-	            $scope.scopeModel.hasError = false;
-	        };
+			function resetErrorsFunction() {
+				$scope.scopeModel.hasError = false;
+			};
 
-	        function defineAPI() {
-	            //function openVariablesEditor() {
-	            //var onSaveVariables = function (activityVariables) {
+			function defineAPI() {
 
-	            //vRWorkflowActivitySettings.Variables = activityVariables;
-	            //};
-	            //BusinessProcess_VRWorkflowService.openVariablesEditor(onSaveVariables, vRWorkflowActivitySettings.Variables, context.parentVariables);
-	            //}
+				var api = {};
+				api.load = function (payload) {
+					var setMenuAction = function (menuAction) {
+						$scope.scopeModel.actions.push(menuAction);
+					};
+					var loadPromiseDeferred = UtilsService.createPromiseDeferred();
+					//if (payload != undefined) {
+					//	inEditor = payload.inEditor;
+					//}
 
-	            var api = {};
-	            api.load = function (payload) {
-	                var setMenuAction = function (menuAction) {
-	                    $scope.scopeModel.actions.push(menuAction);
-	                };
-	                //console.log("Activity load ");
-	                var loadPromiseDeferred = UtilsService.createPromiseDeferred();
-	                if (payload != undefined && payload.Settings != null) {
-	                    //console.log("Activity directive load directive with id ", payload.VRWorkflowActivityId);
-	                    context = payload.Context;
-	                    vRWorkflowActivityId = payload.VRWorkflowActivityId;
-	                    $scope.scopeModel.onRemove = function () {
-	                        ctrl.onRemove(payload.VRWorkflowActivityId);
-	                        if (context != undefined && context.removeFromList != undefined) {
-	                          
-	                            context.removeFromList(vRWorkflowActivityId);
-	                        }
-	                    };
-	                    $scope.scopeModel.editor = payload.Settings.Editor;
-	                    $scope.scopeModel.header = payload.Settings.Title;
-	                    //if (payload.Settings.Title == "Sequence") {
-	                    //$scope.scopeModel.actions = [{
-	                    //	name: "Variables",
-	                    //	clicked: openVariablesEditor
-	                    //}];
-	                    //}
-	                    
-	                    vRWorkflowActivitySettings = payload.Settings;
-	                    if (context != undefined) {
-	                        if (context.addToList != undefined)
-	                            context.addToList(vRWorkflowActivityId, resetErrorsFunction);
-	                    }
+					if (payload != undefined && payload.Settings != null) {
+						context = payload.Context;
+						vRWorkflowActivityId = payload.VRWorkflowActivityId;
+						$scope.scopeModel.onRemove = function () {
+							ctrl.onRemove(payload.VRWorkflowActivityId);
+							if (context != undefined && context.removeFromList != undefined) {
 
-	                    var childContext = {};
-	                    childContext.showErrorWarningIcon = showErrorWarningIcon;
-	                    if (context != undefined) {
-	                        childContext.getWorkflowArguments = context.getWorkflowArguments;
-	                        childContext.addToList = context.addToList;
-	                        childContext.removeFromList = context.removeFromList;
-	                        childContext.reserveVariableName = context.reserveVariableName;
-	                        childContext.reserveVariableNames = context.reserveVariableNames;
-	                        childContext.eraseVariableName = context.eraseVariableName;
-	                        childContext.isVariableNameReserved = context.isVariableNameReserved;
-	                        childContext.doesActivityhaveErrors = context.doesActivityhaveErrors;
-	                        childContext.getParentVariables = function () {
-	                            var parentVars = [];
-	                            if (context.getParentVariables != undefined)
-	                                parentVars = parentVars.concat(context.getParentVariables());
-	                            return parentVars;
-	                        };
-	                    }
-	                    directiveWraperReadyPromiseDeferred.promise.then(function () {
-	                        //console.log("Activity directive load after promise directive with id ", vRWorkflowActivityId);
-	                        var payload = {
-	                            Context: childContext,
-	                            SetMenuAction: setMenuAction,
-	                            Settings: vRWorkflowActivitySettings
-	                        };
-	                        directiveWraperAPI.load(payload);
-	                        loadPromiseDeferred.resolve();
-	                    });
-	                }
-	                else {
-	                    loadPromiseDeferred.resolve();
-	                }
-	                return loadPromiseDeferred.promise;
-	            };
+								context.removeFromList(vRWorkflowActivityId);
+							}
+						};
+						$scope.scopeModel.editor = payload.Settings.Editor;
+						$scope.scopeModel.header = payload.Settings.Title;
 
-	            api.getData = function () {
-	                //console.log("Activity directive try to get directive data with id ", vRWorkflowActivityId);
-	                var settings = {};
-	                if (directiveWraperAPI != null)
-	                    settings = directiveWraperAPI.getData();
-	                //if (vRWorkflowActivitySettings.Variables != undefined)
-	                //settings.Variables = vRWorkflowActivitySettings.Variables
-	                settings.Title = $scope.scopeModel.header;
-	                settings.Editor = $scope.scopeModel.editor;
-	                return {
-	                    VRWorkflowActivityId: vRWorkflowActivityId,
-	                    Settings: settings
-	                };
-	            };
+						vRWorkflowActivitySettings = payload.Settings;
+						if (context != undefined) {
+							if (context.addToList != undefined)
+								context.addToList(vRWorkflowActivityId, resetErrorsFunction);
+						}
 
-	            if (ctrl.onReady != null)
-	                ctrl.onReady(api);
-	        }
+						var childContext = {};
+						childContext.showErrorWarningIcon = showErrorWarningIcon;
+						if (context != undefined) {
+							//childContext.inEditor = inEditor;
+							childContext.vrWorkflowId = context.vrWorkflowId;
+							childContext.getWorkflowArguments = context.getWorkflowArguments;
+							childContext.addToList = context.addToList;
+							childContext.removeFromList = context.removeFromList;
+							childContext.reserveVariableName = context.reserveVariableName;
+							childContext.reserveVariableNames = context.reserveVariableNames;
+							childContext.eraseVariableName = context.eraseVariableName;
+							childContext.isVariableNameReserved = context.isVariableNameReserved;
+							childContext.doesActivityhaveErrors = context.doesActivityhaveErrors;
+							childContext.getParentVariables = function () {
+								var parentVars = [];
+								if (context.getParentVariables != undefined)
+									parentVars = parentVars.concat(context.getParentVariables());
+								return parentVars;
+							};
+						}
+						directiveWraperReadyPromiseDeferred.promise.then(function () {
+							var payload = {
+								//inEditor: inEditor,
+								Context: childContext,
+								SetMenuAction: setMenuAction,
+								Settings: vRWorkflowActivitySettings
+							};
+							directiveWraperAPI.load(payload);
+							loadPromiseDeferred.resolve();
+						});
+					}
+					else {
+						loadPromiseDeferred.resolve();
+					}
+					return loadPromiseDeferred.promise;
+				};
 
-	    }
+				api.getData = function () {
+					var settings = {};
+					if (directiveWraperAPI != null)
+						settings = directiveWraperAPI.getData();
+					settings.Title = $scope.scopeModel.header;
+					settings.Editor = $scope.scopeModel.editor;
+					return {
+						VRWorkflowActivityId: vRWorkflowActivityId,
+						Settings: settings
+					};
+				};
 
-	    return directiveDefinitionObject;
+				if (ctrl.onReady != null)
+					ctrl.onReady(api);
+			}
+
+		}
+
+		return directiveDefinitionObject;
 	}]);
