6714282c4cea53e8fee2d1536abba230ae740197
Rodi Hassan <VANRISE\rodi.hassan>
2018-08-15 06:19:06 +0000

    add left alignment to toolbox
    git-tfs-id: [http://developmentvlan:8585/tfs/vanrise.collection]$/;C48896

--- Vanrise/Code/Vanrise.Web/Client/Javascripts/Directives/ToolBox/vr-toolbox-directive.js
+++ Vanrise/Code/Vanrise.Web/Client/Javascripts/Directives/ToolBox/vr-toolbox-directive.js
@@ -1,66 +1,69 @@
 ﻿'use strict';
 app.directive('vrToolbox', ['BaseDirService', function (BaseDirService) {
-    var directiveDefinitionObject = {
-        restrict: 'E',
-        scope: {
-            datasource: '=',          
-            datavaluefield: '@',
-            datatextfield: '@',
-            dataiconfield: '@',
-            groupname:'@'
-        },
-        controller: function ($scope, $element, $attrs) {
-            var ctrl = this;
-            ctrl.getObjectProperty = function (item, property) {
-                return BaseDirService.getObjectProperty(item, property);
-            };
-            ctrl.getObjectText = function (item) {
-                if (ctrl.datatextfield) return ctrl.getObjectProperty(item, ctrl.datatextfield);
-                return item;
-            };
+	var directiveDefinitionObject = {
+		restrict: 'E',
+		scope: {
+			datasource: '=',
+			datavaluefield: '@',
+			datatextfield: '@',
+			dataiconfield: '@',
+			groupname: '@'
+		},
+		controller: function ($scope, $element, $attrs) {
+			var ctrl = this;
+			ctrl.getObjectProperty = function (item, property) {
+				return BaseDirService.getObjectProperty(item, property);
+			};
+			ctrl.getObjectText = function (item) {
+				if (ctrl.datatextfield) return ctrl.getObjectProperty(item, ctrl.datatextfield);
+				return item;
+			};
 
-            ctrl.getObjectIcon = function (item) {
-                 return ctrl.getObjectProperty(item, ctrl.dataiconfield);
-            };
-            ctrl.getObjectValue = function (item) {
-                if (ctrl.datavaluefield) return ctrl.getObjectProperty(item, ctrl.datavaluefield);
-                return item;
-            };
-            ctrl.config = {
-                group: {
-                    name: ctrl.groupname,
-                    pull: "clone",
-                    put: false
-                },
-                sort: false
-            };
-          
-        },
-        controllerAs: 'ctrl',
-        bindToController: true,
-        template: function (element, attrs) {        
-            var labelTemplate = '';
-            if (attrs.label != undefined)
-                labelTemplate = '<vr-label>' + attrs.label + '</vr-label>';
-            var icontemplate = '';
+			ctrl.getObjectIcon = function (item) {
+				return ctrl.getObjectProperty(item, ctrl.dataiconfield);
+			};
+			ctrl.getObjectValue = function (item) {
+				if (ctrl.datavaluefield) return ctrl.getObjectProperty(item, ctrl.datavaluefield);
+				return item;
+			};
+			ctrl.config = {
+				group: {
+					name: ctrl.groupname,
+					pull: "clone",
+					put: false
+				},
+				sort: false
+			};
+
+		},
+		controllerAs: 'ctrl',
+		bindToController: true,
+		template: function (element, attrs) {
+			var labelTemplate = '';
+			if (attrs.label != undefined)
+				labelTemplate = '<vr-label>' + attrs.label + '</vr-label>';
+			var icontemplate = '';
 			if (attrs.dataiconfield != undefined && attrs.dataiconfield != '')
 				icontemplate = '<span class="icon"><vr-icon icontype="ctrl.getObjectIcon(item)"></vr-icon></span>';
 
 			var layoutClass = "";
 			if (attrs.verticallayout != undefined)
-				layoutClass = "vertical-alignment";
-			var mainTemplate = '<div  ng-sortable="ctrl.config" class="vr-toolbox ' + layoutClass+'" >'
-                                 + '<div class="item" ng-repeat="item in ctrl.datasource">'
-                                     + icontemplate
-                                     + '<span title="{{ctrl.getObjectText(item)}}">{{ctrl.getObjectText(item)}}</span>'
-                                 + '</div>'
-                              + '</div>';
-            return  labelTemplate + mainTemplate ;
-        }
+				layoutClass = " vertical-alignment";
+			if (attrs.leftalignment != undefined)
+				layoutClass += " left-alignment";
 
-    };
+			var mainTemplate = '<div  ng-sortable="ctrl.config" class="vr-toolbox' + layoutClass + '" >'
+				+ '<div class="item" ng-repeat="item in ctrl.datasource">'
+				+ icontemplate
+				+ '<span title="{{ctrl.getObjectText(item)}}">{{ctrl.getObjectText(item)}}</span>'
+				+ '</div>'
+				+ '</div>';
+			return labelTemplate + mainTemplate;
+		}
 
-    return directiveDefinitionObject;
+	};
+
+	return directiveDefinitionObject;
 
 }]);
 
