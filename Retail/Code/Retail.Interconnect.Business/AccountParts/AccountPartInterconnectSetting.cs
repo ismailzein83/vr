ff8fdd566254cbdefb64b740ecb6a20810ab7817
Zeinab Mokdad <VANRISE\zeinab.mokdad>
2018-08-16 10:36:06 +0000

    Adding Account Part setting
    git-tfs-id: [http://developmentvlan:8585/tfs/vanrise.collection]$/;C48950

---
+++ Retail/Code/Retail.Interconnect.Business/AccountParts/AccountPartInterconnectSetting.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using Retail.BusinessEntity.Entities;
+
+namespace Retail.Interconnect.Business
+{
+    public class AccountPartInterconnectSetting : AccountPartSettings
+    {
+        public override Guid ConfigId
+        {
+            get { throw new NotImplementedException(); }
+        }
+        public bool RepresentASwitch { get; set; }
+    }
+}
---
+++ Retail/Code/Retail.Interconnect.Business/AccountParts/AccountPartInterconnectSettingDefinition.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using Retail.BusinessEntity.Entities;
+
+namespace Retail.Interconnect.Business
+{
+    public class AccountPartInterconnectSettingDefinition : AccountPartDefinitionSettings
+    {
+        public override Guid ConfigId { get { return new Guid("1ECEEBC8-9EE2-4690-9B10-32351F8F0096"); } }
+        public override bool IsPartValid(IAccountPartDefinitionIsPartValidContext context)
+        {
+            return true;
+        }
+    }
+}
