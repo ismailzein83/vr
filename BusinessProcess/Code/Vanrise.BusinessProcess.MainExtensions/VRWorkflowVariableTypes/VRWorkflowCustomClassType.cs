c95031152b278d80c4cabfd3e6a71567b9c4f98d
Fatima Al Heck <VANRISE\fatima.alheck>
2018-08-16 08:50:16 +0000

    Implement VRWorkflowCustomClassType
    git-tfs-id: [http://developmentvlan:8585/tfs/vanrise.collection]$/;C48943

--- BusinessProcess/Code/Vanrise.BusinessProcess.MainExtensions/VRWorkflowVariableTypes/VRWorkflowCustomClassType.cs
+++ BusinessProcess/Code/Vanrise.BusinessProcess.MainExtensions/VRWorkflowVariableTypes/VRWorkflowCustomClassType.cs
@@ -1,9 +1,6 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using Vanrise.BusinessProcess.Entities;
+using Vanrise.Common.Business;
 using Vanrise.Entities;
 
 namespace Vanrise.BusinessProcess.MainExtensions.VRWorkflowVariableTypes
@@ -15,12 +12,38 @@ namespace Vanrise.BusinessProcess.MainExtensions.VRWorkflowVariableTypes
 
         public override Type GetRuntimeType(IVRWorkflowVariableTypeGetRuntimeTypeContext context)
         {
-            throw new NotImplementedException();
+            if (string.IsNullOrEmpty(this.FieldType.Namespace))
+                throw new NullReferenceException("FieldType.Namespace");
+
+            if (string.IsNullOrEmpty(this.FieldType.ClassName))
+                throw new NullReferenceException("FieldType.ClassName");
+
+            Type outputType = null;
+            if (string.IsNullOrEmpty(this.FieldType.AssemblyName))
+            {
+                VRNamespaceManager vrNamespaceManager = new VRNamespaceManager();
+                outputType = vrNamespaceManager.GetNamespaceType(this.FieldType.Namespace, this.FieldType.ClassName);
+
+                if (outputType == null)
+                    outputType = Type.GetType(string.Format("{0}.{1}", this.FieldType.Namespace, this.FieldType.ClassName));
+            }
+            else
+            {
+                outputType = Type.GetType(string.Format("{0}.{1}, {2}", this.FieldType.Namespace, this.FieldType.ClassName, this.FieldType.AssemblyName));
+            }
+
+            if (outputType == null)
+            {
+                string assemblyOutput = string.IsNullOrEmpty(this.FieldType.AssemblyName) ? "" : string.Format(", Assembly Name: '{0}'", this.FieldType.AssemblyName);
+                throw new Exception(String.Format("Unable to get Type for Namespace :'{0}', Class name: '{1}'{2}", this.FieldType.Namespace, this.FieldType.ClassName, assemblyOutput));
+            }
+
+            return outputType;
         }
 
         public override string GetRuntimeTypeDescription()
         {
-            throw new NotImplementedException();
+            return string.Format("{0}.{1}", this.FieldType.Namespace, this.FieldType.ClassName);
         }
     }
 }
--- BusinessProcess/Code/Vanrise.BusinessProcess.Web/BusinessProcess/Directives/MainExtensions/VRWorkflow/bp-vr-workflow-customclasstype.js
+++ BusinessProcess/Code/Vanrise.BusinessProcess.Web/BusinessProcess/Directives/MainExtensions/VRWorkflow/bp-vr-workflow-customclasstype.js
@@ -1,1 +1,84 @@
-﻿
+﻿'use strict';
+
+app.directive('businessprocessVrWorkflowCustomclasstype', ['UtilsService', 'VRUIUtilsService',
+    function (UtilsService, VRUIUtilsService) {
+
+        var directiveDefinitionObject = {
+            restrict: 'E',
+            scope: {
+                onReady: '=',
+            },
+            controller: function ($scope, $element, $attrs) {
+                var ctrl = this;
+                var ctor = new WorkflowCustomClassTypeDirectiveCtor(ctrl, $scope, $attrs);
+                ctor.initializeController();
+            },
+            controllerAs: 'ctrl',
+            bindToController: true,
+            compile: function (element, attrs) {
+                return {
+                    pre: function ($scope, iElem, iAttrs, ctrl) {
+
+                    }
+                };
+            },
+            templateUrl: "/Client/Modules/BusinessProcess/Directives/MainExtensions/VRWorkflow/Templates/VRWorkflowCustomClassTypeTemplate.html"
+        };
+
+        function WorkflowCustomClassTypeDirectiveCtor(ctrl, $scope, attrs) {
+            this.initializeController = initializeController;
+
+            var customClassTypeDirectiveAPI;
+            var customClassTypeDirectiveReadyDeferred = UtilsService.createPromiseDeferred();
+
+            function initializeController() {
+                $scope.scopeModel = {};
+
+                $scope.scopeModel.onCustomClassTypeDirectiveReady = function (api) {
+                    customClassTypeDirectiveAPI = api;
+                    customClassTypeDirectiveReadyDeferred.resolve();
+                };
+
+                defineAPI();
+            }
+
+            function defineAPI() {
+                var api = {};
+
+                api.load = function (payload) {
+                    var promises = [];
+
+                    var customClassTypeDirectiveLoadPromise = getCustomClassTypeDirectivePromise();
+                    promises.push(customClassTypeDirectiveLoadPromise);
+
+                    function getCustomClassTypeDirectivePromise() {
+                        var customClassTypeDirectiveLoadDeferred = UtilsService.createPromiseDeferred();
+
+                        customClassTypeDirectiveReadyDeferred.promise.then(function () {
+                            var customClassTypeDirectivePayload;
+                            if (payload != undefined) {
+                                customClassTypeDirectivePayload = payload.FieldType;
+                            }
+                            VRUIUtilsService.callDirectiveLoad(customClassTypeDirectiveAPI, customClassTypeDirectivePayload, customClassTypeDirectiveLoadDeferred);
+                        });
+
+                        return customClassTypeDirectiveLoadDeferred.promise;
+                    }
+
+                    return UtilsService.waitMultiplePromises(promises);
+                };
+
+                api.getData = function () {
+                    return {
+                        $type: "Vanrise.BusinessProcess.MainExtensions.VRWorkflowVariableTypes.VRWorkflowCustomClassType, Vanrise.BusinessProcess.MainExtensions",
+                        FieldType: customClassTypeDirectiveAPI.getData()
+                    };
+                };
+
+                if (ctrl.onReady != null)
+                    ctrl.onReady(api);
+            }
+        }
+
+        return directiveDefinitionObject;
+    }]);
