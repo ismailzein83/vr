1fee966bdef5a5e423411c9e4d00e9287250436b
yara.bousalman <yara.bousalman@vanrise.tfs.local>
2018-08-27 07:08:26 +0000

    Adding EncryptedCalculatedFieldFormula file
    git-tfs-id: [http://developmentvlan:8585/tfs/vanrise.collection]$/;C49022

---
+++ Vanrise/Code/Vanrise.GenericData.MainExtensions/DataRecordFieldFormulas/EncryptedCalculatedFieldFormula.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Vanrise.GenericData.Entities;
+
+namespace Vanrise.GenericData.MainExtensions.DataRecordFieldFormulas
+{
+    public class EncryptedCalculatedFieldFormula 
+    {
+        public override Guid ConfigId { get { return new Guid("B9B7715F-4AFE-402C-AD47-A1C0B2FF8AB1"); } }
+
+    }
+}
