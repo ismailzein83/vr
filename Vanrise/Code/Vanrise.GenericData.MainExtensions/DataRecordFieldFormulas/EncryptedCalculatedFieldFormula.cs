e1c42eb3f35839bb9809c0bad37a9506d7b7e80d
yara.bousalman <yara.bousalman@vanrise.tfs.local>
2018-08-28 13:20:13 +0000

    Adding EncryptedCalculatedFieldFormula.cs file
    git-tfs-id: [http://developmentvlan:8585/tfs/vanrise.collection]$/;C49074

--- Vanrise/Code/Vanrise.GenericData.MainExtensions/DataRecordFieldFormulas/EncryptedCalculatedFieldFormula.cs
+++ Vanrise/Code/Vanrise.GenericData.MainExtensions/DataRecordFieldFormulas/EncryptedCalculatedFieldFormula.cs
@@ -3,13 +3,46 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Vanrise.Common;
+using Vanrise.Common.Business;
 using Vanrise.GenericData.Entities;
 
 namespace Vanrise.GenericData.MainExtensions.DataRecordFieldFormulas
 {
-    public class EncryptedCalculatedFieldFormula 
+    public class EncryptedCalculatedFieldFormula : DataRecordFieldFormula
     {
-        public Guid ConfigId { get { return new Guid("B9B7715F-4AFE-402C-AD47-A1C0B2FF8AB1"); } }
+        public override Guid ConfigId { get { return new Guid("B9B7715F-4AFE-402C-AD47-A1C0B2FF8AB1"); } }
+        public bool Decrypt { get; set; }
+        public string FieldName { get; set; }
+        public override List<string> GetDependentFields(IDataRecordFieldFormulaGetDependentFieldsContext context)
+        {
+            return new List<string>() { FieldName };
+        }
+        public override dynamic CalculateValue(IDataRecordFieldFormulaCalculateValueContext context)
+        {
+            var fieldValue = context.GetFieldValue(FieldName);
+            string key = DataEncryptionKeyManager.GetLocalTokenDataDecryptionKey();
+            if (Decrypt)
+            {
+                return Cryptography.Decrypt(fieldValue, key);
+            }
+            else
+            {
+                return Cryptography.Encrypt(fieldValue, key);
+            }
+        }
 
+        public override RecordFilter ConvertFilter(IDataRecordFieldFormulaConvertFilterContext context)
+        {
+            EmptyRecordFilter emptyFilter = context.InitialFilter as EmptyRecordFilter;
+            if (emptyFilter != null)
+                return new AlwaysFalseRecordFilter() { };
+
+            NonEmptyRecordFilter nonEmptyFilter = context.InitialFilter as NonEmptyRecordFilter;
+            if (nonEmptyFilter != null)
+                return null;
+
+            throw new Exception(String.Format("Invalid Record Filter '{0}'", context.InitialFilter.GetType()));
+        }
     }
 }
