0978ad60028f46584833708a4f7b10e38be8b009
yara.bousalman <yara.bousalman@vanrise.tfs.local>
2018-08-28 13:36:28 +0000

    Adding EncryptedCalculatedFieldFormula files to Vanrise.GenericData.Web project
    git-tfs-id: [http://developmentvlan:8585/tfs/vanrise.collection]$/;C49075

---
+++ Vanrise/Code/Vanrise.GenericData.Web/VR_GenericData/Directives/MainExtensions/DataRecordFieldFormulas/vr-genericdata-datarecordtypefields-formula-encryptedcalculated.js
@@ -0,0 +1,73 @@
+﻿'use strict';
+app.directive('vrGenericdataDatarecordtypefieldsFormulaEncryptedcalculated', ['UtilsService',
+    function (UtilsService) {
+
+        var directiveDefinitionObject = {
+            restrict: 'E',
+            scope: {
+                onReady: '='
+            },
+            controller: function ($scope, $element, $attrs) {
+                var ctrl = this;
+                var ctor = new EncryptedCalculatedCtor(ctrl, $scope);
+                ctor.initializeController();
+            },
+            controllerAs: 'ctrl',
+            bindToController: true,
+            compile: function (element, attrs) {
+                return {
+                    pre: function ($scope, iElem, iAttrs, ctrl) {
+
+                    }
+                };
+            },
+            templateUrl: function (element, attrs) {
+                return '/Client/Modules/VR_GenericData/Directives/MainExtensions/DataRecordFieldFormulas/Templates/EncryptedCalculatedFieldFormulaTemplate.html';
+            }
+        };
+
+        function EncryptedCalculatedCtor(ctrl, $scope) {
+
+            function initializeController() {
+                $scope.scopeModel = {};
+                $scope.scopeModel.fields = [];
+
+                defineAPI();
+            }
+
+            function defineAPI() {
+                var api = {};
+
+                api.load = function (payload) {
+                    if (payload != undefined) {
+                        var context = payload.context;
+                        if (context != undefined && context.getFields != undefined) {
+                            $scope.scopeModel.fields = context.getFields();
+                        }
+                        if (payload.formula != undefined) {
+                            $scope.scopeModel.selectedFieldName = UtilsService.getItemByVal($scope.scopeModel.fields, payload.formula.FieldName, "fieldName");
+                            $scope.scopeModel.decrypt = payload.formula.Decrypt;
+                        }
+                        else {
+                            $scope.scopeModel.decrypt = false;
+                        }
+                    }
+                };
+
+                api.getData = function () {
+                    return {
+                        $type: "Vanrise.GenericData.MainExtensions.DataRecordFieldFormulas.EncryptedCalculatedFieldFormula, Vanrise.GenericData.MainExtensions",
+                        Decrypt: $scope.scopeModel.decrypt,
+                        FieldName: $scope.scopeModel.selectedFieldName.fieldName
+                    };
+                };
+
+                if (ctrl.onReady != null)
+                    ctrl.onReady(api);
+            }
+
+            this.initializeController = initializeController;
+        }
+        return directiveDefinitionObject;
+    }
+]);
