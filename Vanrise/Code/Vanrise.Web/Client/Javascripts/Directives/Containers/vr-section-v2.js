5314a7e294d0eaefb4c91ba78de9b105c37f12d0
Rodi Hassan <VANRISE\rodi.hassan>
2018-08-30 11:03:26 +0000

    Fix bug in section header
    git-tfs-id: [http://developmentvlan:8585/tfs/vanrise.collection]$/;C49101

--- Vanrise/Code/Vanrise.Web/Client/Javascripts/Directives/Containers/vr-section-v2.js
+++ Vanrise/Code/Vanrise.Web/Client/Javascripts/Directives/Containers/vr-section-v2.js
@@ -8,13 +8,12 @@ app.directive('vrSectionV2', ['UtilsService', 'MultiTranscludeService', function
 			menuactions: "=",
 			onRemove: "=",
 			header: "=",
-		    warning:"="
+			warning: "="
 		},
 		transclude: true,
 		controller: function ($scope, $element, $attrs) {
 			var ctrl = this;
 			$scope.classlevel = "1";
-			$scope.header = ctrl.header;
 			$scope.dragable = $attrs.dragable != undefined;
 			$scope.collapsible = $attrs.collapsible != undefined;
 			if ($attrs.level != undefined && $attrs.level == "2")
@@ -25,17 +24,17 @@ app.directive('vrSectionV2', ['UtilsService', 'MultiTranscludeService', function
 		controllerAs: 'sectionCtrl',
 		bindToController: true,
 		template: function (attrs) {
-		    var focusClass = "";
-		    if (attrs.focusonclick != undefined) {
-		        focusClass = "vr-clickable-panel";
-		    }
-		    var htmlTempalte = '<div class="panel-primary panel-vr ' + focusClass + '" id="{{sectionId}}" ng-click="$root.addFocusPanelClass($event)" ng-class="classlevel" >'
+			var focusClass = "";
+			if (attrs.focusonclick != undefined) {
+				focusClass = "vr-clickable-panel";
+			}
+			var htmlTempalte = '<div class="panel-primary panel-vr ' + focusClass + '" id="{{sectionId}}" ng-click="$root.addFocusPanelClass($event)" ng-class="classlevel" >'
 				+ '<div class="panel-heading" ng-init="expandname=true" expanded="{{expandname}}">'
 				+ '<span class="glyphicon glyphicon-th-list handeldrag hand-cursor drag-icon" ng-show="dragable" ></span>'
-				+ '<span style="padding-left: 4px;" class="hand-cursor collapsible-icon glyphicon " ng-show="collapsible" ng-click=" expandname =!expandname " ng-class=" expandname ?\'glyphicon-collapse-up\':\'glyphicon-collapse-down\'" ></span><label>{{header}}</label>'
+				+ '<span style="padding-left: 4px;" class="hand-cursor collapsible-icon glyphicon " ng-show="collapsible" ng-click=" expandname =!expandname " ng-class=" expandname ?\'glyphicon-collapse-up\':\'glyphicon-collapse-down\'" ></span><label>{{sectionCtrl.header}}</label>'
 				+ '<span class="section-menu"  style="absolute: relative; right: 10px;" ng-if="sectionCtrl.menuactions.length > 0" > <vr-button type="SectionAction" menuactions="sectionCtrl.menuactions" isasynchronous="true" ></vr-button></span> '
 				+ '<span class="warning-icon"  style="position: absolute;top: 7px;" ng-style="{\'right\': sectionCtrl.menuactions.length > 0 ? \'80px\' : \'20px\'}" ng-if="sectionCtrl.warning!=\'\'" > <vr-warning  value="{{sectionCtrl.warning}}"></vr-warning></span> '
-	            + '<span class="hand-cursor glyphicon glyphicon-remove" style="position: absolute; right: 5px; top: 12px;" ng-show="sectionCtrl.onRemove != undefined" ng-click="sectionCtrl.onRemove()" ></span></div>'
+				+ '<span class="hand-cursor glyphicon glyphicon-remove" style="position: absolute; right: 5px; top: 12px;" ng-show="sectionCtrl.onRemove != undefined" ng-click="sectionCtrl.onRemove()" ></span></div>'
 				+ '<div class="panel-body" ng-show="expandname"  ng-transclude></div></div>';
 
 			return htmlTempalte;
