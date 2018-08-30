d26a0320da4ae233a5785fcb6d858acb429192fe
Wissam Ajamy <VANRISE\wissam.ajamy>
2018-08-30 15:39:56 +0000

    update runtime configuration file
    git-tfs-id: [http://developmentvlan:8585/tfs/vanrise.collection]$/;C49111

--- Vanrise/Code/Resources/BatchScripts/SOM/01-Runtime.bat
+++ Vanrise/Code/Resources/BatchScripts/SOM/01-Runtime.bat
@@ -10,6 +10,6 @@ ECHO.
 ECHO SOM Runtime
 xcopy "C:\TFS\SOM\Code\SOM.Runtime\bin\x64\Release" 				/y /v /z /i /Q /R  "\\192.168.110.185\Fixes\WebSite\BIL\Runtime\%YYYYMMDD%\" /EXCLUDE:C:\TFS\Vanrise\Code\Resources\BatchScripts\SOM\runtime-list-of-excluded-files.txt
 xcopy "C:\TFS\SOM\Code\SOM.Runtime\app.config.exclude" 				/y /v /z /i /Q /R "\\192.168.110.185\Fixes\WebSite\BIL\Runtime\%YYYYMMDD%\"
-rename "\\192.168.110.185\Fixes\WebSite\BIL\Runtime\%YYYYMMDD%\app.config.exclude" "TOne.WhS.Runtime.exe.config"
+rename "\\192.168.110.185\Fixes\WebSite\BIL\Runtime\%YYYYMMDD%\app.config.exclude" "SOM.Runtime.exe.config"
 
 del /s /q /f "\\192.168.110.185\Fixes\WebSite\BIL\Runtime\%YYYYMMDD%\*.pdb"
