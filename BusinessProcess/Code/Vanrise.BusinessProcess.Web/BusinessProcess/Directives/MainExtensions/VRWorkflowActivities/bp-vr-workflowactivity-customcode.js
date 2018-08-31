9289170df51222990b270293ea42c2d67da13a84
Rodi Hassan <VANRISE\rodi.hassan>
2018-08-31 16:56:13 +0000

    modify number of rows for expression builder in custom code and return promise in custom code directive load
    git-tfs-id: [http://developmentvlan:8585/tfs/vanrise.collection]$/;C49143

--- BusinessProcess/Code/Vanrise.BusinessProcess.Web/BusinessProcess/Directives/MainExtensions/VRWorkflowActivities/bp-vr-workflowactivity-customcode.js
+++ BusinessProcess/Code/Vanrise.BusinessProcess.Web/BusinessProcess/Directives/MainExtensions/VRWorkflowActivities/bp-vr-workflowactivity-customcode.js
@@ -33,7 +33,8 @@ app.directive('businessprocessVrWorkflowactivityCustomcode', ['UtilsService', 'V
 	        function defineAPI() {
 	            var api = {};
 
-	            api.load = function (payload) {
+				api.load = function (payload) {
+					var promises = [];
 	                if (payload != undefined) {
 	                    if (payload.Settings != undefined) {
 	                        $scope.scopeModel.isVRWorkflowActivityDisabled = payload.Settings.IsDisabled;
@@ -44,7 +45,8 @@ app.directive('businessprocessVrWorkflowactivityCustomcode', ['UtilsService', 'V
 
 	                    if (payload.Context != null)
 	                        $scope.scopeModel.context = payload.Context;
-	                }
+					}
+					return UtilsService.waitMultiplePromises(promises);
 	            };
 
 	            api.getData = function () {
