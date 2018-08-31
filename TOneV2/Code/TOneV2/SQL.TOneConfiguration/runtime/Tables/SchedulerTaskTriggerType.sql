65924f4a2d4689ccd573933fb4026ea5f548172e
Wissam Ajamy <VANRISE\wissam.ajamy>
2018-08-31 08:14:16 +0000

    update field size to varchar(MAX)
    git-tfs-id: [http://developmentvlan:8585/tfs/vanrise.collection]$/;C49116

--- TOneV2/Code/TOneV2/SQL.TOneConfiguration/runtime/Tables/SchedulerTaskTriggerType.sql
+++ TOneV2/Code/TOneV2/SQL.TOneConfiguration/runtime/Tables/SchedulerTaskTriggerType.sql
@@ -1,10 +1,12 @@
 ﻿CREATE TABLE [runtime].[SchedulerTaskTriggerType] (
     [ID]              UNIQUEIDENTIFIER NOT NULL,
     [Name]            VARCHAR (50)     NOT NULL,
-    [TriggerTypeInfo] VARCHAR (300)    NOT NULL,
+    [TriggerTypeInfo] VARCHAR (MAX)    NOT NULL,
     [timestamp]       ROWVERSION       NULL,
     CONSTRAINT [PK_SchedulerTaskTriggerTemplate] PRIMARY KEY CLUSTERED ([ID] ASC)
 );
+
+
 
 
 
