d31fdc0bb236131b52a6e41268904d3a5d860f35
Zeinab Mokdad <VANRISE\zeinab.mokdad>
2018-08-31 13:04:26 +0000

    7141:ST | Routing Implementation | New Requirement ------6575 for SalePRicelist in ToneV2
    git-tfs-id: [http://developmentvlan:8585/tfs/vanrise.collection]$/;C49127

--- TOneV2/Code/TOneV2/TOne.WhS.BusinessEntity.Web/WhS_BusinessEntity/Constant/SalePriceListTemplateSettingsCodeOnEachRowBEFieldEnum.js
+++ TOneV2/Code/TOneV2/TOne.WhS.BusinessEntity.Web/WhS_BusinessEntity/Constant/SalePriceListTemplateSettingsCodeOnEachRowBEFieldEnum.js
@@ -2,7 +2,7 @@
     Zone: { value: 0, description: "Zone" },
     Code: { value: 1, description: "Code" },
     CodeBED: { value: 2, description: "Code BED" },
-    CodeEED: { value: 3, description: "Code EED" },
+    CodeEED: { value: 3, description: "Rate&CodeEED (reflecting Last modification)" },
     Rate: { value: 4, description: "Rate" },
     RateBED: { value: 5, description: "Rate BED" },
     RateEED: { value: 6, description: "Rate EED" },
@@ -11,6 +11,6 @@
     Currency: { value: 9, description: "Currency" },
     CodeChangeType: { value: 10, description: "Code Change Type" },
     Increment: { value: 11, description: "Increment" },
-    CodeRateChangeType: { value: 12, description: "Code And Rate Change Type" },
-    EffectiveDate: { value: 13, description: "Effective Date" }
+    CodeRateChangeType: { value: 12, description: "Code and Rate Change Type(reflecting Last modification)" },
+    EffectiveDate: { value: 13, description: "Rate&CodeBED (reflecting Last modification)" }
 });
