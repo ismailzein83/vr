a32b5bb701591345a42b77cd199ba371ae2f8e71
Ali Atoui <VANRISE\ali.atoui>
2018-09-04 13:23:27 +0000

    Adding PassThroughCustomerRateEvaluator @CarrierAccountCustomerSettings
    git-tfs-id: [http://developmentvlan:8585/tfs/vanrise.collection]$/;C49201

--- TOneV2/Code/TOneV2/TOne.WhS.BusinessEntity.Entities/Carriers/CarrierAccounts/CarrierAccountCustomerSettings.cs
+++ TOneV2/Code/TOneV2/TOne.WhS.BusinessEntity.Entities/Carriers/CarrierAccounts/CarrierAccountCustomerSettings.cs
@@ -6,14 +6,44 @@ using System.Threading.Tasks;
 
 namespace TOne.WhS.BusinessEntity.Entities
 {
-
     public class CarrierAccountCustomerSettings
     {
         public int? DefaultRoutingProductId { get; set; }
+
         public RoutingStatus RoutingStatus { get; set; }
+
         public int? TimeZoneId { get; set; }
+
         public bool InvoiceTimeZone { get; set; }
+
+        public bool IsPassThrough { get; set; }
+
+        /// <summary>
+        /// null if IsPassThrough false
+        /// </summary>
+        public PassThroughCustomerRateEvaluator PassThroughCustomerRateEvaluator { get; set; }
+
         public PricingSettings PricingSettings { get; set; }
+
         public PricelistSettings PricelistSettings { get; set; }
+    }
+
+    public abstract class PassThroughCustomerRateEvaluator
+    {
+        public abstract decimal? EvaluateCustomerRate(IPassThroughEvaluateCustomerRateContext context);
+    }
+
+    public interface IPassThroughEvaluateCustomerRateContext
+    {
+        decimal? CostRate { get; }
+
+        int CostCurrencyId { get; }
+    }
+
+    public class PassThroughEvaluateCustomerRateContext : IPassThroughEvaluateCustomerRateContext
+    {
+        public decimal? CostRate { get; set; }
+
+        public int CostCurrencyId { get; set; }
     }
 }
---
+++ TOneV2/Code/TOneV2/TOne.WhS.BusinessEntity.MainExtensions/PassThroughCustomerRateEvaluator/CostWithPercentageMarginRateEvaluator.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TOne.WhS.BusinessEntity.Entities;
+
+namespace TOne.WhS.BusinessEntity.MainExtensions.PassThroughCustomerRateEvaluator
+{
+    public class CostWithPercentageMarginRateEvaluator : TOne.WhS.BusinessEntity.Entities.PassThroughCustomerRateEvaluator
+    {
+        public int Percentage { get; set; }
+
+        public override decimal? EvaluateCustomerRate(IPassThroughEvaluateCustomerRateContext context)
+        {
+            if (!context.CostRate.HasValue)
+                return null;
+
+            return context.CostRate.Value + (context.CostRate.Value * Percentage / 100);
+        }
+    }
+}
