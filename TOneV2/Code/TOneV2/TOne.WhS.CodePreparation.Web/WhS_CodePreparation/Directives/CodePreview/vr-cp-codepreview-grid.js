c63ec299310954fdbd2529016727a8d367a29128
Rodi Hassan <VANRISE\rodi.hassan>
2018-09-04 10:54:52 +0000

    fix bug in moved icon and BED and EED in code preview when grouped by codes
    git-tfs-id: [http://developmentvlan:8585/tfs/vanrise.collection]$/;C49190

--- TOneV2/Code/TOneV2/TOne.WhS.CodePreparation.Web/WhS_CodePreparation/Directives/CodePreview/vr-cp-codepreview-grid.js
+++ TOneV2/Code/TOneV2/TOne.WhS.CodePreparation.Web/WhS_CodePreparation/Directives/CodePreview/vr-cp-codepreview-grid.js
@@ -1,115 +1,115 @@
 ﻿"use strict";
 
 app.directive("vrCpCodepreviewGrid", ["WhS_CP_CodePreparationPreviewAPIService", "WhS_CP_CodeChangeTypeEnum", "VRNotificationService",
-function (WhS_CP_CodePreparationPreviewAPIService, WhS_CP_CodeChangeTypeEnum, VRNotificationService) {
+	function (WhS_CP_CodePreparationPreviewAPIService, WhS_CP_CodeChangeTypeEnum, VRNotificationService) {
 
-    var directiveDefinitionObject = {
-        restrict: "E",
-        scope: {
-            onReady: "="
-        },
-        controller: function ($scope, $element, $attrs) {
-            var ctrl = this;
+		var directiveDefinitionObject = {
+			restrict: "E",
+			scope: {
+				onReady: "="
+			},
+			controller: function ($scope, $element, $attrs) {
+				var ctrl = this;
 
-            var codePreviewGrid = new CodePreviewGrid($scope, ctrl, $attrs);
-            codePreviewGrid.initializeController();
-        },
-        controllerAs: "ctrl",
-        bindToController: true,
-        compile: function (element, attrs) {
+				var codePreviewGrid = new CodePreviewGrid($scope, ctrl, $attrs);
+				codePreviewGrid.initializeController();
+			},
+			controllerAs: "ctrl",
+			bindToController: true,
+			compile: function (element, attrs) {
 
-        },
-        templateUrl: "/Client/Modules/WhS_CodePreparation/Directives/CodePreview/Templates/CodePreparationCodePreviewGridTemplate.html"
+			},
+			templateUrl: "/Client/Modules/WhS_CodePreparation/Directives/CodePreview/Templates/CodePreparationCodePreviewGridTemplate.html"
 
-    };
+		};
 
-    function CodePreviewGrid($scope, ctrl, $attrs) {
+		function CodePreviewGrid($scope, ctrl, $attrs) {
 
-        var gridAPI;
-        var zoneName;
-        this.initializeController = initializeController;
+			var gridAPI;
+			var zoneName;
+			this.initializeController = initializeController;
 
-        function initializeController() {
+			function initializeController() {
 
-            $scope.showZoneName = true;
-            $scope.changedCodes = [];
-            $scope.onGridReady = function (api) {
-                gridAPI = api;
+				$scope.showZoneName = true;
+				$scope.changedCodes = [];
+				$scope.onGridReady = function (api) {
+					gridAPI = api;
 
-                if (ctrl.onReady != undefined && typeof (ctrl.onReady) == "function")
-                    ctrl.onReady(getDirectiveAPI());
-                function getDirectiveAPI() {
+					if (ctrl.onReady != undefined && typeof (ctrl.onReady) == "function")
+						ctrl.onReady(getDirectiveAPI());
+					function getDirectiveAPI() {
 
-                    var directiveAPI = {};
-                    directiveAPI.load = function (query) {
-                        zoneName = query.ZoneName;
-                        $scope.showZoneName = query.ShowZoneName != undefined ? query.ShowZoneName : true;
-                        return gridAPI.retrieveData(query);
-                    };
+						var directiveAPI = {};
+						directiveAPI.load = function (query) {
+							zoneName = query.ZoneName;
+							$scope.showZoneName = query.ShowZoneName != undefined ? query.ShowZoneName : true;
+							return gridAPI.retrieveData(query);
+						};
 
-                    return directiveAPI;
-                }
-            };
-
-
-            $scope.dataRetrievalFunction = function (dataRetrievalInput, onResponseReady) {
-                return WhS_CP_CodePreparationPreviewAPIService.GetFilteredCodePreview(dataRetrievalInput)
-                    .then(function (response) {
-                        if (response && response.Data) {
-                            for (var i = 0; i < response.Data.length; i++) {
-                                mapDataNeeded(response.Data[i]);
-                            }
-                        }
-
-                        onResponseReady(response);
-                    })
-                    .catch(function (error) {
-                        VRNotificationService.notifyException(error, $scope);
-                    });
-            };
-
-        }
+						return directiveAPI;
+					}
+				};
 
 
-        function mapDataNeeded(dataItem) {
-            dataItem.showMovedTo = false;
-            dataItem.showMovedFrom = false;
-            switch (dataItem.Entity.ChangeType) {
-                case WhS_CP_CodeChangeTypeEnum.New.value:
-                    dataItem.codeStatusIconUrl = WhS_CP_CodeChangeTypeEnum.New.icon;
-                    dataItem.codeStatusIconTooltip = WhS_CP_CodeChangeTypeEnum.New.label;
-                    break;
+				$scope.dataRetrievalFunction = function (dataRetrievalInput, onResponseReady) {
+					return WhS_CP_CodePreparationPreviewAPIService.GetFilteredCodePreview(dataRetrievalInput)
+						.then(function (response) {
+							if (response && response.Data) {
+								for (var i = 0; i < response.Data.length; i++) {
+									mapDataNeeded(response.Data[i]);
+								}
+							}
 
-                case WhS_CP_CodeChangeTypeEnum.Closed.value:
-                    dataItem.codeStatusIconUrl = WhS_CP_CodeChangeTypeEnum.Closed.icon;
-                    dataItem.codeStatusIconTooltip = WhS_CP_CodeChangeTypeEnum.Closed.label;
-                    break;
+							onResponseReady(response);
+						})
+						.catch(function (error) {
+							VRNotificationService.notifyException(error, $scope);
+						});
+				};
 
-                case WhS_CP_CodeChangeTypeEnum.Moved.value:
-                    dataItem.showMovedTo = true;
-                    dataItem.showMovedFrom = true;
-                    if (dataItem.Entity.ZoneName != zoneName) {
-                        dataItem.codeStatusIconUrl = WhS_CP_CodeChangeTypeEnum.MovedFrom.icon;
-                        dataItem.codeStatusIconTooltip = WhS_CP_CodeChangeTypeEnum.Moved.label + " to " + dataItem.Entity.ZoneName;
-                        }
-                        else {
-                        dataItem.codeStatusIconUrl = WhS_CP_CodeChangeTypeEnum.MovedTo.icon;
-                        dataItem.codeStatusIconTooltip = WhS_CP_CodeChangeTypeEnum.Moved.label + " from " + dataItem.Entity.RecentZoneName;
-                        dataItem.Entity.BED = dataItem.Entity.EED;
-                        dataItem.Entity.EED = "";
-                        }
-                    break;
-            }
-            
-            if (zoneName == dataItem.Entity.ZoneName)
-                dataItem.showMovedTo = false;
+			}
 
-            if (dataItem.Entity.RecentZoneName== null || zoneName == dataItem.Entity.RecentZoneName)
-                dataItem.showMovedFrom = false;
-        }
 
-    }
+			function mapDataNeeded(dataItem) {
+				dataItem.showMovedTo = false;
+				dataItem.showMovedFrom = false;
+				switch (dataItem.Entity.ChangeType) {
+					case WhS_CP_CodeChangeTypeEnum.New.value:
+						dataItem.codeStatusIconUrl = WhS_CP_CodeChangeTypeEnum.New.icon;
+						dataItem.codeStatusIconTooltip = WhS_CP_CodeChangeTypeEnum.New.label;
+						break;
 
-    return directiveDefinitionObject;
+					case WhS_CP_CodeChangeTypeEnum.Closed.value:
+						dataItem.codeStatusIconUrl = WhS_CP_CodeChangeTypeEnum.Closed.icon;
+						dataItem.codeStatusIconTooltip = WhS_CP_CodeChangeTypeEnum.Closed.label;
+						break;
 
-}]);
+					case WhS_CP_CodeChangeTypeEnum.Moved.value:
+						dataItem.showMovedTo = true;
+						dataItem.showMovedFrom = true;
+						if (zoneName != undefined && zoneName != '' && dataItem.Entity.ZoneName != zoneName) {
+							dataItem.codeStatusIconUrl = WhS_CP_CodeChangeTypeEnum.MovedFrom.icon;
+							dataItem.codeStatusIconTooltip = WhS_CP_CodeChangeTypeEnum.Moved.label + " to " + dataItem.Entity.ZoneName;
+						}
+						else {
+							dataItem.codeStatusIconUrl = WhS_CP_CodeChangeTypeEnum.MovedTo.icon;
+							dataItem.codeStatusIconTooltip = WhS_CP_CodeChangeTypeEnum.Moved.label + " from " + dataItem.Entity.RecentZoneName;
+							dataItem.Entity.BED = dataItem.Entity.EED;
+							dataItem.Entity.EED = "";
+						}
+						break;
+				}
+
+				if (zoneName == dataItem.Entity.ZoneName)
+					dataItem.showMovedTo = false;
+
+				if (dataItem.Entity.RecentZoneName == null || zoneName == dataItem.Entity.RecentZoneName)
+					dataItem.showMovedFrom = false;
+			}
+
+		}
+
+		return directiveDefinitionObject;
+
+	}]);
