006c2f4834280fb4c3753927ebecb19e7f4acd19
Ayman Zinji <VANRISE\ayman.zinji>
2018-09-06 10:50:11 +0000

    mics
    git-tfs-id: [http://developmentvlan:8585/tfs/vanrise.collection]$/;C49260

---
+++ SOM/Code/BPMExtended.Main.Business/Inventory/MicsManager.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using BPMExtended.Main.Common;
+using BPMExtended.Main.Entities;
+
+namespace BPMExtended.Main.Business
+{
+    public class MicsManager
+    {
+        public void GetMics(string phoneNumber)
+        {
+           
+        }
+    }
+}
---
+++ SOM/Code/BPMExtended.Main.Common/MicsMockDataGenerator.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using BPMExtended.Main.Entities;
+
+namespace BPMExtended.Main.Common
+{
+    public static class MicsMockDataGenerator
+    {
+        public static List<MIC> GetAllMics()
+        {
+            return new List<MIC>
+            {
+                new MIC
+                {
+                    SwitchName = "Midan001.SW"
+                },
+                new MIC
+                {
+                    SwitchName = "Mazzeh001.SW"
+                },
+                new MIC
+                {
+                    SwitchName = "Damascus001.SW"
+                }
+            };
+        }
+    }
+}
