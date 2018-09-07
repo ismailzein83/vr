64d268296b155012b11887bd3f1c2d1c884e81ab
Samer Haidar <VANRISE\samer.haidar>
2018-09-07 07:39:17 +0000

    Adding TranslationRuleEntity
    git-tfs-id: [http://developmentvlan:8585/tfs/vanrise.collection]$/;C49275

--- NetworkProvisioning/Code/NP.IVSwitch.Entities/TranslationRule/TranslationRule.cs
+++ NetworkProvisioning/Code/NP.IVSwitch.Entities/TranslationRule/TranslationRule.cs
@@ -6,12 +6,41 @@ using System.Threading.Tasks;
 
 namespace NP.IVSwitch.Entities
 {
+    public enum EngineType { Fast = 0, Regex = 1 }
+    public enum PrefixSign { Plus = 0, Minus = 1 }
+    public enum CLIType { FixedCLI = 0 , PoolBasedCLI = 1 }
     public class TranslationRule
     {
         public int TranslationRuleId { get; set; }
         public String Name { get; set; }
+        public EngineType EngineType { get; set; }
+        public PrefixSign? PrefixSign { get; set; }
         public String DNISPattern { get; set; }
-        public String CLIPattern { get; set; }
+        public CLIType CLIType { get; set; }
+        public FixedCLISettings FixedCLISettings { get; set; }
+        public PoolBasedCLISettings PoolBasedCLISettings { get; set; }
         public DateTime CreationDate { get; set; }
+    }
+
+    public class FixedCLISettings
+    {
+        public String CLIPattern { get; set; }
+    }
+    public class PoolBasedCLISettings
+    {
+        public List<string> CLIPatterns { get; set; }
+        public string Prefix { get; set; }
+        public string Destination { get; set; }
+        public int RandMin { get; set; }
+        public int RandMax { get; set; }
+        public string DisplayName { get; set; }
+    }
+    public class TranslationRuleToAdd
+    {
+
+    }
+    public class TranslationRuleToUpdate
+    {
+
     }
 }
--- NetworkProvisioning/Code/NP.IVSwitch.Entities/TranslationRule/TranslationRuleDetail.cs
+++ NetworkProvisioning/Code/NP.IVSwitch.Entities/TranslationRule/TranslationRuleDetail.cs
@@ -9,6 +9,12 @@ namespace NP.IVSwitch.Entities
     public class TranslationRuleDetail
     {
         public TranslationRule Entity { get; set; }
+        public String Name { get; set; }
+        public EngineType EngineType { get; set; }
+        public PrefixSign? PrefixSign { get; set; }
+        public String DNISPattern { get; set; }
+        public CLIType CLIType { get; set; }
+        public DateTime CreationDate { get; set; }
 
     }
 }
