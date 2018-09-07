dfb6699d745d5e45c2515ce6f416ac916f5ddd6f
Ali Atoui <VANRISE\ali.atoui>
2018-09-07 13:07:27 +0000

    Adding TestingBPInstanceBeforeInsertHandler and TestingBPInstanceAfterInsertHandler
    git-tfs-id: [http://developmentvlan:8585/tfs/vanrise.collection]$/;C49290

--- BusinessProcess/Code/Vanrise.BusinessProcess.Entities/BPInstance/BPInstanceAfterInsertHandler.cs
+++ BusinessProcess/Code/Vanrise.BusinessProcess.Entities/BPInstance/BPInstanceAfterInsertHandler.cs
@@ -17,10 +17,22 @@ namespace Vanrise.BusinessProcess.Entities
     public interface IBPInstanceAfterInsertHandlerExecuteContext
     {
         BPInstance BPInstance { get; }
+        object CustomData { get; }
     }
 
     public class BPInstanceAfterInsertHandlerExecuteContext : IBPInstanceAfterInsertHandlerExecuteContext
     {
         public BPInstance BPInstance { get; set; }
+        public object CustomData { get; set; }
+    }
+
+    public class TestingBPInstanceAfterInsertHandler : BPInstanceAfterInsertHandler
+    {
+        public override Guid ConfigId { get { return new Guid("9DC2DA9E-9ACE-42B1-86FF-D7A7BEA20BDD"); } }
+
+        public override void Execute(IBPInstanceBeforeInsertHandlerExecuteContext context)
+        {
+            throw new NotImplementedException();
+        }
     }
 }
--- BusinessProcess/Code/Vanrise.BusinessProcess.Entities/BPInstance/BPInstanceBeforeInsertHandler.cs
+++ BusinessProcess/Code/Vanrise.BusinessProcess.Entities/BPInstance/BPInstanceBeforeInsertHandler.cs
@@ -12,15 +12,63 @@ namespace Vanrise.BusinessProcess.Entities
         public abstract Guid ConfigId { get; }
 
         public abstract void Execute(IBPInstanceBeforeInsertHandlerExecuteContext context);
+
+        //public virtual string GetStartProcessOutputCode(IBeforeInsertHandlerGetStartProcessOutputCodeContext context)
+        //{
+        //    return "public BPInstanceBeforeInsertHandler BPInstanceBeforeInsertHandler { get; set; }";
+        //}
+
+        //public virtual void SetStartProcessOutput(IBeforeInsertHandlerSetStartProcessOutputContext context)
+        //{
+        //    SetObjectProperty("BPInstanceBeforeInsertHandler", "1", context.StartProcessOutput as object);
+        //}
+
+        //private void SetObjectProperty(string propertyName, string value, object obj)
+        //{
+        //    System.Reflection.PropertyInfo propertyInfo = obj.GetType().GetProperty(propertyName);
+        //    // make sure object has the property we are after
+        //    if (propertyInfo != null)
+        //        propertyInfo.SetValue(obj, value, null);
+        //}
     }
 
     public interface IBPInstanceBeforeInsertHandlerExecuteContext
     {
         BPInstanceToAdd BPInstanceToAdd { get; }
+        object CustomData { set; }
     }
 
     public class BPInstanceBeforeInsertHandlerExecuteContext : IBPInstanceBeforeInsertHandlerExecuteContext
     {
-        public BPInstanceToAdd BPInstanceToAdd { get; set; } 
+        public BPInstanceToAdd BPInstanceToAdd { get; set; }
+        public object CustomData { get; set; }
+    }
+
+    //public interface IBeforeInsertHandlerGetStartProcessOutputCodeContext
+    //{
+    //}
+
+    //public class BeforeInsertHandlerGetStartProcessOutputCodeContext : IBeforeInsertHandlerGetStartProcessOutputCodeContext
+    //{
+    //}
+
+    //public interface IBeforeInsertHandlerSetStartProcessOutputContext
+    //{
+    //    dynamic StartProcessOutput { get; set; }
+    //}
+
+    //public class BeforeInsertHandlerSetStartProcessOutputContext : IBeforeInsertHandlerSetStartProcessOutputContext
+    //{
+    //    public dynamic StartProcessOutput { get; set; }
+    //}
+
+    public class TestingBPInstanceBeforeInsertHandler : BPInstanceBeforeInsertHandler
+    {
+        public override Guid ConfigId { get { return new Guid("371F8454-DAF0-4001-86FE-057B6BCDDA31"); } }
+
+        public override void Execute(IBPInstanceBeforeInsertHandlerExecuteContext context)
+        {
+            throw new NotImplementedException();
+        }
     }
 }
