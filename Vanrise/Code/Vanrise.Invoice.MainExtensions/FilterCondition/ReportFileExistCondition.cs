db0c6b153dcc7347ee173d096afd885b879794a0
Naim Saad <VANRISE\naim.saad>
2018-09-10 14:15:20 +0000

    Implementing ReportFileExistCondition
    git-tfs-id: [http://developmentvlan:8585/tfs/vanrise.collection]$/;C49321

---
+++ Vanrise/Code/Vanrise.Invoice.MainExtensions/FilterCondition/ReportFileExistCondition.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Vanrise.Invoice.Entities;
+using Vanrise.Common;
+using System.Web;
+using System.IO;
+
+namespace Vanrise.Invoice.MainExtensions
+{
+    public class ReportFileExistCondition : InvoiceGridActionFilterCondition
+    {
+        public override Guid ConfigId
+        {
+            get { return new Guid("77D7261A-E2C6-4A49-B6FA-4010A07F2C2B"); }
+        }
+        public override bool IsFilterMatch(IInvoiceGridActionFilterConditionContext context)
+        {
+            //OpenRDLCReportAction openRDLCReportAction = context.InvoiceAction.Settings.CastWithValidate<OpenRDLCReportAction>("Empty Action");
+            //string reportURL = openRDLCReportAction.ReportURL;
+            //string reportRuntimeURL = openRDLCReportAction.ReportRuntimeURL;
+            //string ReportUrl = null;
+            //if (HttpContext.Current != null)
+            //{
+            //    ReportUrl = HttpContext.Current.Server.MapPath(reportURL);
+            //}
+            //else
+            //{
+            //    string currentDir = Path.GetDirectoryName(System.Reflection.Assembly.GetAssembly(typeof(OpenRDLCReportActionManager)).Location);
+            //    ReportUrl = Path.Combine(currentDir, reportRuntimeURL);
+            //}
+            //if (!File.Exists(ReportUrl))
+            //    return false;
+            return true;
+        }
+    }
+}
