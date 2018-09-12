2d628eca19cc9b7a05a5ce3837289ce296f0f88e
yara.bousalman <yara.bousalman@vanrise.tfs.local>
2018-09-12 12:21:30 +0000

    Checking in PoolBasedCLIGroup classes
    git-tfs-id: [http://developmentvlan:8585/tfs/vanrise.collection]$/;C49333

---
+++ NetworkProvisioning/Code/NP.IVSwitch.Business/PoolBasedCLIGroupCustomObjectTypeSettings.cs
@@ -0,0 +1,46 @@
+﻿using NP.IVSwitch.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Vanrise.Common;
+using Vanrise.GenericData.Business;
+using Vanrise.GenericData.Entities;
+
+namespace NP.IVSwitch.Business
+{
+    public class PoolBasedCLIGroupCustomObjectTypeSettings : FieldCustomObjectTypeSettings
+    {
+        public override Guid ConfigId { get { return new Guid("7CC89311-C9AA-4F9A-9BEB-4549362AEA4F"); } }
+
+        public override string GetDescription(IFieldCustomObjectTypeSettingsContext context)
+        {
+            return null;
+        }
+
+        public override bool AreEqual(Object newValue, Object oldValue)
+        {
+            return true;
+        }
+
+        public override Type GetNonNullableRuntimeType()
+        {
+            return typeof(PoolBasedCLIDetailsCollection);
+        }
+
+        public override dynamic ParseNonNullValueToFieldType(object originalValue)
+        {
+            var castedOriginalValue = originalValue as List<PoolBasedCLIDetails>;
+            if (castedOriginalValue != null)
+                return castedOriginalValue;
+            else
+                return Utilities.ConvertJsonToList<PoolBasedCLIDetails>(originalValue);
+        }
+
+        public override string GetRuntimeTypeDescription()
+        {
+            return "Pool-Based CLI Group";
+        }
+    }
+}
---
+++ NetworkProvisioning/Code/NP.IVSwitch.Entities/TranslationRule/PoolBasedCLIDetails.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NP.IVSwitch.Entities
+{
+    public class PoolBasedCLIDetailsCollection : List<PoolBasedCLIDetails>
+    {
+
+    }
+    public class PoolBasedCLIDetails
+    {
+        public string CLIPattern { get; set; }
+    }
+}
