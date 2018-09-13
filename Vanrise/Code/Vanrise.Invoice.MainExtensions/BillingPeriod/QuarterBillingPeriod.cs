d5a5d6b3c86747da9f71e982190193ddb6ce5f22
Samer Haidar <VANRISE\samer.haidar>
2018-09-13 11:05:42 +0000

    Implementing QuarterBillingPeriod
    git-tfs-id: [http://developmentvlan:8585/tfs/vanrise.collection]$/;C49347

---
+++ Vanrise/Code/Vanrise.Invoice.MainExtensions/BillingPeriod/QuarterBillingPeriod.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Vanrise.Invoice.Entities;
+
+namespace Vanrise.Invoice.MainExtensions
+{
+    public class QuarterBillingPeriod : BillingPeriod
+    {
+        public override Guid ConfigId
+        {
+            get { return new Guid("93330DE5-EEC8-461C-988B-E4E0C22BC541"); }
+        }
+
+        public override List<BillingInterval> GetPeriod(IBillingPeriodContext context)
+        {
+            List<BillingInterval> billingIntervalList = new List<BillingInterval>();
+           
+            var fromDate = new DateTime(context.IssueDate.Year, context.IssueDate.Month, 1);
+            var toDate = new DateTime(context.IssueDate.Year, context.IssueDate.Month, 1);
+
+            var perviousBillingInterval = new BillingInterval
+            {
+                FromDate = fromDate.AddMonths(-3),
+                ToDate = toDate.AddDays(-1)
+            };
+            billingIntervalList.Add(perviousBillingInterval);
+           
+            return billingIntervalList;
+        }
+        public override string GetDescription()
+        {
+            return "Quarter";
+        }
+    }
+}
