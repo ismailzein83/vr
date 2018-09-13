7e23e577f8de09f2822be775cdd3bea34b6f85e8
yara.bousalman <yara.bousalman@vanrise.tfs.local>
2018-09-13 14:03:02 +0000

    Adding DeleteGenericBEAction.cs class
    git-tfs-id: [http://developmentvlan:8585/tfs/vanrise.collection]$/;C49358

---
+++ Vanrise/Code/Vanrise.GenericData.MainExtensions/GenericBusinessEntity/GenericBEActions/DeleteGenericBEAction.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Vanrise.GenericData.Business;
+
+namespace Vanrise.GenericData.MainExtensions
+{
+    public class DeleteGenericBEAction : GenericBEActionSettings
+    {
+        public override Guid ConfigId
+        {
+            get { return new Guid("6BC1FB84-F28D-476A-81FA-A11FC4E5CC06"); }
+        }
+        public override string ActionTypeName { get { return "DeleteGenericBEAction"; } }
+
+        public override bool DoesUserHaveAccess(IGenericBEActionDefinitionCheckAccessContext context)
+        {
+            return new GenericBusinessEntityManager().DoesUserHaveEditAccess(context.UserId, context.BusinessEntityDefinitionId);
+        }
+    }
+}
