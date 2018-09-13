770c071860bc2bd2e0fe556bded95b8c7b625d0b
Naim Saad <VANRISE\naim.saad>
2018-09-13 13:57:22 +0000

    Implementing QuarterBillingPeriod
    git-tfs-id: [http://developmentvlan:8585/tfs/vanrise.collection]$/;C49356

---
+++ Vanrise/Code/Vanrise.Invoice.Web/VR_Invoice/Directives/BillingCycle/MainExtensions/BillingPeriod/vr-invoice-billingperiod-quarter.js
@@ -0,0 +1,59 @@
+﻿"use strict";
+
+app.directive("vrInvoiceBillingperiodQuarter", ["UtilsService", "VRNotificationService", "VRUIUtilsService",
+    function (UtilsService, VRNotificationService, VRUIUtilsService) {
+
+        var directiveDefinitionObject = {
+
+            restrict: "E",
+            scope:
+            {
+                onReady: "=",
+            },
+            controller: function ($scope, $element, $attrs) {
+                var ctrl = this;
+
+                var ctor = new QuarterBillingPeriod($scope, ctrl, $attrs);
+                ctor.initializeController();
+            },
+            controllerAs: "ctrl",
+            bindToController: true,
+            compile: function (element, attrs) {
+
+            },
+            templateUrl: "/Client/Modules/VR_Invoice/Directives/BillingCycle/MainExtensions/BillingPeriod/Templates/QuarterBillingPeriodTemplate.html"
+
+        };
+
+        function QuarterBillingPeriod($scope, ctrl, $attrs) {
+            this.initializeController = initializeController;
+            function initializeController() {
+                $scope.scopeModel = {};
+                defineAPI();
+            }
+
+            function defineAPI() {
+                var api = {};
+
+                api.load = function (payload) {
+                    if (payload != undefined) {
+                    }
+                    var promises = [];
+                    return UtilsService.waitMultiplePromises(promises);
+                };
+
+                api.getData = function () {
+                    return {
+                        $type: "Vanrise.Invoice.MainExtensions.QuarterBillingPeriod ,Vanrise.Invoice.MainExtensions",
+                    };
+                };
+
+                if (ctrl.onReady != null)
+                    ctrl.onReady(api);
+            }
+        }
+
+        return directiveDefinitionObject;
+
+    }
+]);
