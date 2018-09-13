ddf2fc4cb9666d35d1b11735ae198ee294094f3c
anthony douaihy <VANRISE\anthony.douaihy>
2018-09-13 13:57:24 +0000

    add DataRecordTypeCompositeRecord
    git-tfs-id: [http://developmentvlan:8585/tfs/vanrise.collection]$/;C49357

---
+++ Vanrise/Code/Vanrise.GenericData.MainExtensions/CompositeRecordCondition/DataRecordTypeCompositeRecord.cs
@@ -0,0 +1,18 @@
+﻿//using System;
+//using Vanrise.GenericData.Business;
+//using Vanrise.GenericData.Entities;
+
+//namespace Vanrise.GenericData.MainExtensions.CompositeRecordCondition
+//{
+//    public class DataRecordTypeCompositeRecord : CompositeRecordConditionSettings
+//    {
+//        public override Guid ConfigId { get { return new Guid("25127A16-D2EB-47A1-97C0-FFA45C2C31A0"); } }
+
+//        public Guid DataRecordTypeId { get; set; }
+
+//        public override void GetFields(ICompositeRecordConditionSettingsGetFieldsContext context)
+//        {
+//            context.Fields = new DataRecordTypeManager().GetDataRecordTypeFields(this.DataRecordTypeId);
+//        }
+//    }
+//}
