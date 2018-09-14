442e71276590929483fe31ac76dd1a3f8761e905
Naim Saad <VANRISE\naim.saad>
2018-09-14 13:56:12 +0000

    Implementing ReportFileExist
    git-tfs-id: [http://developmentvlan:8585/tfs/vanrise.collection]$/;C49393

---
+++ Vanrise/Code/Vanrise.Invoice.Web/VR_Invoice/Directives/InvoiceType/InvoiceGridSettings/MainExtensions/InvoiceFilterCondition/vr-invoicetype-invoicefiltercondition-reportfileexist.js
@@ -0,0 +1,67 @@
+﻿"use strict";
+
+app.directive("vrInvoicetypeInvoicefilterconditionReportfileexist", ["UtilsService", "VRNotificationService", "VRUIUtilsService",
+    function (UtilsService, VRNotificationService, VRUIUtilsService) {
+
+        var directiveDefinitionObject = {
+
+            restrict: "E",
+            scope:
+            {
+                onReady: "=",
+            },
+            controller: function ($scope, $element, $attrs) {
+                var ctrl = this;
+
+                var ctor = new ReportFileExistCondition($scope, ctrl, $attrs);
+                ctor.initializeController();
+            },
+            controllerAs: "ctrl",
+            bindToController: true,
+            compile: function (element, attrs) {
+
+            },
+            templateUrl: "/Client/Modules/VR_Invoice/Directives/InvoiceType/InvoiceGridSettings/MainExtensions/InvoiceFilterCondition/Templates/ReportFileExistConditionTemplate.html"
+
+        };
+
+        function ReportFileExistCondition($scope, ctrl, $attrs) {
+            this.initializeController = initializeController;
+
+            var context;
+            function initializeController() {
+                $scope.scopeModel = {};
+
+                defineAPI();
+            }
+
+            function defineAPI() {
+                var api = {};
+
+                api.load = function (payload) {
+                    var promises = [];
+
+                    return UtilsService.waitMultiplePromises(promises);
+                };
+
+                api.getData = function () {
+                    return {
+                        $type: "Vanrise.Invoice.MainExtensions.ReportFileExistCondition ,Vanrise.Invoice.MainExtensions"
+                    };
+                };
+
+                if (ctrl.onReady != null)
+                    ctrl.onReady(api);
+            }
+            function getContext() {
+                var currentContext = context;
+                if (currentContext == undefined)
+                    currentContext = {};
+                return currentContext;
+            }
+        }
+
+        return directiveDefinitionObject;
+
+    }
+]);
