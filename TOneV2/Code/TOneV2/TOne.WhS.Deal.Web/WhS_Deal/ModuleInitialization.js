3f2213843d9fbe848c543fd5f395607b99834217
Mohammad Yahfoufi <VANRISE\mohammad.yahfoufi>
2018-09-14 08:36:19 +0000

    Hiding Substitute Rate and Route Rule View Tabs at Swap Deals
    git-tfs-id: [http://developmentvlan:8585/tfs/vanrise.collection]$/;C49373

--- TOneV2/Code/TOneV2/TOne.WhS.Deal.Web/WhS_Deal/ModuleInitialization.js
+++ TOneV2/Code/TOneV2/TOne.WhS.Deal.Web/WhS_Deal/ModuleInitialization.js
@@ -2,8 +2,8 @@
 function (WhS_Deal_VolumeCommitmentService, WhS_Deal_SwapDealService) {
     WhS_Deal_VolumeCommitmentService.registerObjectTrackingDrillDownToVolCommitmentDeal();
     WhS_Deal_VolumeCommitmentService.registerHistoryViewAction();
-    WhS_Deal_SwapDealService.registerSwapDealSellRouteRuleViewToSwapDeal();
-    WhS_Deal_SwapDealService.registerSwapDealBuyRouteRuleViewToSwapDeal();
+   // WhS_Deal_SwapDealService.registerSwapDealSellRouteRuleViewToSwapDeal();
+    //WhS_Deal_SwapDealService.registerSwapDealBuyRouteRuleViewToSwapDeal();
     WhS_Deal_SwapDealService.registerObjectTrackingDrillDownToSwapDeal();
     WhS_Deal_SwapDealService.registerHistoryViewAction();
 }]);
