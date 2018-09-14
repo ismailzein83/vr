4d4ebb45c3a1c273945b15cf02c54db0750159b8
yara.bousalman <yara.bousalman@vanrise.tfs.local>
2018-09-14 09:17:58 +0000

    DeleteGenericBEAction related classes
    git-tfs-id: [http://developmentvlan:8585/tfs/vanrise.collection]$/;C49374

---
+++ Vanrise/Code/Vanrise.GenericData.Entities/GenericBusinessEntity/DeleteGenericBusinessEntityInput.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Vanrise.GenericData.Entities
+{
+    public class DeleteGenericBusinessEntityInput
+    {
+        public List<object> GenericBusinessEntityIds { get; set; }
+
+        public Guid BusinessEntityDefinitionId { get; set; }
+    }
+}
