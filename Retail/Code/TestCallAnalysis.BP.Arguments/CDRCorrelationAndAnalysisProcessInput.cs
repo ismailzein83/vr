c146aeadce90e352af7d5a36cbd5e00a87e045a5
Samer Haidar <VANRISE\samer.haidar>
2018-09-17 07:12:54 +0000

    Adding projects for test call analysis
    git-tfs-id: [http://developmentvlan:8585/tfs/vanrise.collection]$/;C49408

---
+++ Retail/Code/TestCallAnalysis.BP.Arguments/CDRCorrelationAndAnalysisProcessInput.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Vanrise.BusinessProcess.Entities;
+
+namespace TestCallAnalysis.BP.Arguments
+{
+    public class CDRCorrelationAndAnalysisProcessInput : BaseProcessInputArgument
+    {
+        public TimeSpan DateTimeMargin { get; set; }
+
+        public override string GetTitle()
+        {
+            return "CDR Correlation And Analysis Process";
+        }
+    }
+}
---
+++ Retail/Code/TestCallAnalysis.BP.Arguments/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following 
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("TestCallAnalysis.BP.Arguments")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("TestCallAnalysis.BP.Arguments")]
+[assembly: AssemblyCopyright("Copyright ©  2018")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible 
+// to COM components.  If you need to access a type in this assembly from 
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("17de9304-12cb-4441-8616-e1a4ce5a5b24")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version 
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Build and Revision Numbers 
+// by using the '*' as shown below:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
