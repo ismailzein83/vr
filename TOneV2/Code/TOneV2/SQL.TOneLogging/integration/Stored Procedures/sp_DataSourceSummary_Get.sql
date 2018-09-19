9568ceafe7ce8afab36fdaa357128bae037eda19
Fatima Al Heck <VANRISE\fatima.alheck>
2018-09-19 15:50:27 +0000

    Add post scripts for data source summary alerts
    git-tfs-id: [http://developmentvlan:8585/tfs/vanrise.collection]$/;C49512

---
+++ TOneV2/Code/TOneV2/SQL.TOneLogging/integration/Stored Procedures/sp_DataSourceSummary_Get.sql
@@ -0,0 +1,30 @@
+﻿CREATE PROCEDURE [integration].[sp_DataSourceSummary_Get]
+	@DataSourceIDs varchar(max),
+	@FromTime DateTime
+
+AS
+BEGIN
+
+		DECLARE @EnabledDataSourcesTable TABLE (EnabledDataSourceID uniqueidentifier)
+		INSERT INTO @EnabledDataSourcesTable (EnabledDataSourceID)
+		select Convert(uniqueidentifier, ParsedString) from [integration].[ParseStringList](@DataSourceIDs)
+
+	SELECT   	DSImportedBatch.[DataSourceId],
+				MAX([LogEntryTime]) as LastImportedBatchTime,
+				COUNT(*) as NbImportedBatch, 
+				SUM([RecordsCount]) as TotalRecordCount,
+				MAX([RecordsCount]) as MaxRecordCount,
+				MIN([RecordsCount]) as MinRecordCount,
+				MAX([BatchSize]) as MaxBatchSize,
+				MIN([BatchSize]) as MinBatchSize,
+				SUM(CASE WHEN [MappingResult] = 2 THEN 1 ELSE 0 END) as NbInvalidBatch,
+				SUM(CASE WHEN [MappingResult] = 3 THEN 1 ELSE 0 END) as NbEmptyBatch
+					
+	FROM [TOneWFTracking].[integration].[DataSourceImportedBatch] DSImportedBatch
+
+	WHERE [LogEntryTime] >= @FromTime
+	 AND (DSImportedBatch.[DataSourceId] IN (select EnabledDataSourceID from @EnabledDataSourcesTable))
+
+	GROUP BY [DataSourceId]
+
+END
