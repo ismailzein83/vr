3f23c4e00d73cb72fe371e4b11728a1687fc6954
Rodi Hassan <VANRISE\rodi.hassan>
2018-09-21 10:28:27 +0000

    modify mail message evaluator and use vr-textbox-multiple for "To"
    git-tfs-id: [http://developmentvlan:8585/tfs/vanrise.collection]$/;C49522

--- Vanrise/Code/Vanrise.Common.Web/Common/Views/VRMail/VRMailMessageEvaluatorController.js
+++ Vanrise/Code/Vanrise.Common.Web/Common/Views/VRMail/VRMailMessageEvaluatorController.js
@@ -1,104 +1,104 @@
 ﻿(function (appControllers) {
 
-    "use strict";
+	"use strict";
 
-    emailEditorContorller.$inject = ['$scope', 'VRNotificationService', 'VRNavigationService', 'UtilsService', 'VRUIUtilsService', 'VRCommon_VRMailAPIService'];
+	emailEditorContorller.$inject = ['$scope', 'VRNotificationService', 'VRNavigationService', 'UtilsService', 'VRUIUtilsService', 'VRCommon_VRMailAPIService'];
 
-    function emailEditorContorller($scope, VRNotificationService, VRNavigationService, UtilsService, VRUIUtilsService, VRCommon_VRMailAPIService) {
+	function emailEditorContorller($scope, VRNotificationService, VRNavigationService, UtilsService, VRUIUtilsService, VRCommon_VRMailAPIService) {
 
-        var emailObject;
-        var saleVrFiles;
-        var fileAPI;
-        defineScope();
-        loadParameters();
-        loadAllControls();
+		var emailObject;
+		var saleVrFiles;
+		var fileAPI;
+		defineScope();
+		loadParameters();
+		loadAllControls();
 
-        function loadParameters() {
-            var parameters = VRNavigationService.getParameters($scope);
-            if (parameters != undefined) {
-                emailObject = parameters.evaluatedEmail;
-                saleVrFiles = parameters.saleVrFiles;
-                //$scope.scopeModel.fileId = fileId;
-            }
-        }
+		function loadParameters() {
+			var parameters = VRNavigationService.getParameters($scope);
+			if (parameters != undefined) {
+				emailObject = parameters.evaluatedEmail;
+				saleVrFiles = parameters.saleVrFiles;
+				//$scope.scopeModel.fileId = fileId;
+			}
+		}
 
-        function defineScope() {
-            $scope.scopeModel = {};
-            $scope.scopeModel.uploadedAttachements = [];
-            $scope.scopeModel.Attachements = [];
-            $scope.scopeModel.confirmEmail = function () {
-                return confirmEmail();
-            };
-            $scope.scopeModel.onUploadedAttachementFileReady = function (api) {
-                fileAPI = api;
-            };
-            $scope.scopeModel.close = function () {
-                $scope.modalContext.closeModal();
-            };
+		function defineScope() {
+			$scope.scopeModel = {};
+			$scope.scopeModel.uploadedAttachements = [];
+			$scope.scopeModel.Attachements = [];
+			$scope.scopeModel.confirmEmail = function () {
+				return confirmEmail();
+			};
+			$scope.scopeModel.onUploadedAttachementFileReady = function (api) {
+				fileAPI = api;
+			};
+			$scope.scopeModel.close = function () {
+				$scope.modalContext.closeModal();
+			};
 
-            function confirmEmail() {
-                var emailObject = buildEmailObjFromScope();
-                if ($scope.onSalePriceListSendingEmail != undefined)
-                    $scope.onSalePriceListSendingEmail(emailObject);
-                $scope.modalContext.closeModal();
-            }
+			function confirmEmail() {
+				var emailObject = buildEmailObjFromScope();
+				if ($scope.onSalePriceListSendingEmail != undefined)
+					$scope.onSalePriceListSendingEmail(emailObject);
+				$scope.modalContext.closeModal();
+			}
 
-            $scope.scopeModel.downloadAttachement = function (attachedfileId) {
-                return VRCommon_VRMailAPIService.DownloadAttachement(attachedfileId).then(function (response) {
-                    UtilsService.downloadFile(response.data, response.headers);
-                });
-            };
-            $scope.scopeModel.addUploadedAttachement = function (obj) {
-                if (obj != undefined) {
-                    $scope.scopeModel.uploadedAttachements.push(obj);
-                    fileAPI.clearFileUploader();
-                }
-            };
-        }
+			$scope.scopeModel.downloadAttachement = function (attachedfileId) {
+				return VRCommon_VRMailAPIService.DownloadAttachement(attachedfileId).then(function (response) {
+					UtilsService.downloadFile(response.data, response.headers);
+				});
+			};
+			$scope.scopeModel.addUploadedAttachement = function (obj) {
+				if (obj != undefined) {
+					$scope.scopeModel.uploadedAttachements.push(obj);
+					fileAPI.clearFileUploader();
+				}
+			};
+		}
 
-        function loadAllControls() {
-            $scope.scopeModel.Attachements = saleVrFiles;
-            function setTitle() {
-                $scope.title = "Email";
-            }
+		function loadAllControls() {
+			$scope.scopeModel.Attachements = saleVrFiles;
+			function setTitle() {
+				$scope.title = "Email";
+			}
 
-            function loadStaticData() {
-                if (emailObject != undefined) {
-                    $scope.scopeModel.from = emailObject.From;
-                    $scope.scopeModel.cc = emailObject.CC;
-                    $scope.scopeModel.to = emailObject.To;
-                    $scope.scopeModel.subject = emailObject.Subject;
-                    $scope.scopeModel.body = emailObject.Body;
-                }
-                $scope.scopeModel.UploadpriceListSheet = null;
-            }
-            return UtilsService.waitMultipleAsyncOperations([setTitle, loadStaticData])
-                  .catch(function (error) {
-                      VRNotificationService.notifyExceptionWithClose(error, $scope);
-                  })
-                 .finally(function () {
-                     $scope.scopeModel.isLoading = false;
-                 });
-        }
+			function loadStaticData() {
+				if (emailObject != undefined) {
+					$scope.scopeModel.from = emailObject.From;
+					$scope.scopeModel.cc = emailObject.CC;
+					$scope.scopeModel.to = emailObject.To != undefined ? emailObject.To.split(';').filter(value => value && value != undefined && value != "" && /\S/.test(value)) : [];
+					$scope.scopeModel.subject = emailObject.Subject;
+					$scope.scopeModel.body = emailObject.Body;
+				}
+				$scope.scopeModel.UploadpriceListSheet = null;
+			}
+			return UtilsService.waitMultipleAsyncOperations([setTitle, loadStaticData])
+				.catch(function (error) {
+					VRNotificationService.notifyExceptionWithClose(error, $scope);
+				})
+				.finally(function () {
+					$scope.scopeModel.isLoading = false;
+				});
+		}
 
-        function buildEmailObjFromScope() {
-            var attachementFileIds = $scope.scopeModel.uploadedAttachements.map(function (a) { return a.fileId; });
+		function buildEmailObjFromScope() {
+			var attachementFileIds = $scope.scopeModel.uploadedAttachements.map(function (a) { return a.fileId; });
 
-            for (var i = 0; i < saleVrFiles.length; i++) {
-                attachementFileIds.push(saleVrFiles[i].FileId);
-            }
+			for (var i = 0; i < saleVrFiles.length; i++) {
+				attachementFileIds.push(saleVrFiles[i].FileId);
+			}
 
-            var obj = {
-                From: $scope.scopeModel.from,
-                CC: $scope.scopeModel.cc,
-                To: $scope.scopeModel.to,
-                Subject: $scope.scopeModel.subject,
-                Body: $scope.scopeModel.body,
-                AttachementFileIds: attachementFileIds
-            };
-            return obj;
-        }
-    }
+			var obj = {
+				From: $scope.scopeModel.from,
+				CC: $scope.scopeModel.cc,
+				To: $scope.scopeModel.to.join(';'),
+				Subject: $scope.scopeModel.subject,
+				Body: $scope.scopeModel.body,
+				AttachementFileIds: attachementFileIds
+			};
+			return obj;
+		}
+	}
 
-    appControllers.controller('VRCommon_VRMailMessageEvaluatorController', emailEditorContorller);
+	appControllers.controller('VRCommon_VRMailMessageEvaluatorController', emailEditorContorller);
 })(appControllers);
