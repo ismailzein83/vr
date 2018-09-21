743ad198790baa32429eeda2cae7b88b5b4b65bc
Ali Sakr <VANRISE\ali.sakr>
2018-09-21 11:47:42 +0000

    Adding ExcludedItem Class
    git-tfs-id: [http://developmentvlan:8585/tfs/vanrise.collection]$/;C49528

---
+++ TOneV2/Code/TOneV2/TOne.WhS.Sales.Entities/RatePlanning/ExcludedItem.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TOne.WhS.Sales.Entities.RatePlanning
+{
+    public class ExcludedItem
+    {
+        public string ItemId { get; set; }
+        public string ItemName { get; set; }
+        public string Reason { get; set; }
+        public int ParentId { get; set; }
+        public long ProcessInstanceId { get; set; }
+    }
+    public enum ItemTypeEnum
+    {
+        Country = 0,
+        Zone = 1
+    }
+}
