31f864ba3453fbe0c47bd6a01dc1a911db2cfee2
Zeinab Mokdad <VANRISE\zeinab.mokdad>
2018-09-25 14:43:57 +0000

    Adding business rule for code with characters> 20
    git-tfs-id: [http://developmentvlan:8585/tfs/vanrise.collection]$/;C49591

---
+++ TOneV2/Code/TOneV2/TOne.WhS.CodePreparation.Business/BusinessRules/Conditions/ValidateZones/CodeLenghtCondition.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using Vanrise.BusinessProcess.Entities;
+using TOne.WhS.CodePreparation.Entities.Processing;
+
+namespace TOne.WhS.CodePreparation.Business
+{
+    public class CodeLenghtCondition : BusinessRuleCondition
+    {
+        public override bool ShouldValidate(IRuleTarget target)
+        {
+            return target as AllZonesToProcess != null;
+        }
+
+        public override bool Validate(IBusinessRuleConditionValidateContext context)
+        {
+            AllZonesToProcess allZonesToProcess = context.Target as AllZonesToProcess;
+            var invalidZones = new List<string>();
+
+            foreach (var zoneToProcess in allZonesToProcess.Zones)
+            {
+                foreach (var codeToAdd in zoneToProcess.CodesToAdd)
+                {
+                    if (codeToAdd.Code.Length > 20)
+                    {
+                        invalidZones.Add(zoneToProcess.ZoneName);
+                        break;
+                    }
+                }
+            }
+
+            if (invalidZones.Count > 0)
+            {
+                context.Message = string.Format("Following zones have code that exceeds the maximum number of characters (20) : {0}.", string.Join(", ", invalidZones));
+                return false;
+            }
+            return true;
+        }
+
+        public override string GetMessage(IRuleTarget target)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
