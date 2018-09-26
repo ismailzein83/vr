336c07fcacb525f725adf816851fd31371f80a14
Naim Saad <VANRISE\naim.saad>
2018-09-26 15:09:04 +0000

    Implementing RouteRuleApplyOptionsPercentageContext
    git-tfs-id: [http://developmentvlan:8585/tfs/vanrise.collection]$/;C49678

---
+++ TOneV2/Code/TOneV2/TOne.WhS.Routing.Entities/RouteRuleApplyOptionsPercentageContext.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TOne.WhS.Routing.Entities
+{
+    public interface IRouteRuleApplyOptionsPercentageContext
+    {
+        List<RouteOption> Options { get; set; }
+        List<RouteOptionRuleTarget> FinalRouteOptionRuleTargets { get; }
+        RoutingDatabase RoutingDatabase { get; }
+    }
+
+    public class RouteRuleApplyOptionsPercentageContext : IRouteRuleApplyOptionsPercentageContext
+    {
+        public List<RouteOption> Options { get; set; }
+        public List<RouteOptionRuleTarget> FinalRouteOptionRuleTargets { get; set; }
+        public RoutingDatabase RoutingDatabase { get; set; }
+    }
+}
