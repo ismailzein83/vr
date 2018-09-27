a2aa06ff7acc7094f1bd37257ea23677da46851d
yara.bousalman <yara.bousalman@vanrise.tfs.local>
2018-09-27 16:15:39 +0000

    Adding GenericBEOnBeforeGetFilteredHandlerSettingsConfig to Vanrise.GenericData.Entities
    git-tfs-id: [http://developmentvlan:8585/tfs/vanrise.collection]$/;C49746

---
+++ Vanrise/Code/Vanrise.GenericData.Entities/BusinessEntities/GenericBEDefinitionSettings/GenericBEOnBeforeGetFilteredHandlerSettingsConfig.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Vanrise.Entities;
+
+namespace Vanrise.GenericData.Entities
+{
+    public class GenericBEOnBeforeGetFilteredHandlerSettingsConfig 
+    {
+ 
+    }
+}
