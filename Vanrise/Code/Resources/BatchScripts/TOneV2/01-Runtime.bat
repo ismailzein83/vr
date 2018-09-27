3de2dbb59b860e61dec1b914975bf1727d59bcb3
Wissam Ajamy <VANRISE\wissam.ajamy>
2018-09-27 09:32:11 +0000

    update DRLCs path in runtime to \Runtime\%YYYYMMDD%\Modules\WhS_Invoice\Reports"
    git-tfs-id: [http://developmentvlan:8585/tfs/vanrise.collection]$/;C49718

--- Vanrise/Code/Resources/BatchScripts/TOneV2/01-Runtime.bat
+++ Vanrise/Code/Resources/BatchScripts/TOneV2/01-Runtime.bat
@@ -9,7 +9,7 @@ For /F %PARSEARG% %%i in (%CURRDATE%) Do SET YYYYMMDD=%%l%%j%%k
 ECHO.
 ECHO TOneV2 Runtime
 xcopy "C:\TFS\TOneV2\Code\TOneV2\TestRuntime\bin\x64\Release" 							/y /v /z /i /Q /R  "\\192.168.110.185\Fixes\WebSite\TOneV2\Runtime\%YYYYMMDD%\" /EXCLUDE:C:\TFS\Vanrise\Code\Resources\BatchScripts\TOneV2\runtime-list-of-excluded-files.txt
-xcopy "C:\TFS\TOneV2\Code\TOneV2\TOne.WhS.Invoice.Web\WhS_Invoice\Reports"				/S /E /R /y /v /i /z /Q "\\192.168.110.185\Fixes\WebSite\TOneV2\Runtime\%YYYYMMDD%\Reports" /EXCLUDE:C:\TFS\Vanrise\Code\Resources\BatchScripts\TOneV2\runtime-list-of-excluded-files.txt
+xcopy "C:\TFS\TOneV2\Code\TOneV2\TOne.WhS.Invoice.Web\WhS_Invoice\Reports"				/S /E /R /y /v /i /z /Q "\\192.168.110.185\Fixes\WebSite\TOneV2\Runtime\%YYYYMMDD%\Modules\WhS_Invoice\Reports" /EXCLUDE:C:\TFS\Vanrise\Code\Resources\BatchScripts\TOneV2\runtime-list-of-excluded-files.txt
 
 xcopy "C:\TFS\TOneV2\Code\TOneV2\TestRuntime\app.config.exclude" 						/y /v /z /i /Q /R "\\192.168.110.185\Fixes\WebSite\TOneV2\Runtime\%YYYYMMDD%\"
 rename "\\192.168.110.185\Fixes\WebSite\TOneV2\Runtime\%YYYYMMDD%\app.config.exclude" "TOne.WhS.Runtime.exe.config"
