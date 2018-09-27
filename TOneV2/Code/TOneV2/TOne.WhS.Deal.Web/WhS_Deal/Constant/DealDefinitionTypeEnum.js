4967ec6ac73096913d0049d444c00c0cdaf28cc7
Mohammad Yahfoufi <VANRISE\mohammad.yahfoufi>
2018-09-27 07:52:50 +0000

    Adding DealDefinitionTypeEnum
    git-tfs-id: [http://developmentvlan:8585/tfs/vanrise.collection]$/;C49709

---
+++ TOneV2/Code/TOneV2/TOne.WhS.Deal.Web/WhS_Deal/Constant/DealDefinitionTypeEnum.js
@@ -0,0 +1,4 @@
+﻿app.constant("WhS_Deal_DealDefinitionTypeEnum", {
+    SwapDeal: { value: '63C1310D-FDEA-4AC7-BDE1-58FD11E4EC65', description: "Swap Deal" },
+    VolCommitment: { value: 'B606E88C-4AE5-4BF0-BCE5-10D456A092F5', description: "Volume Commitment" }
+});
