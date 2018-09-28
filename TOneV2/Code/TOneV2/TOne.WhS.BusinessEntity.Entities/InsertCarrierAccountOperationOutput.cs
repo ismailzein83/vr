ad02c1f6c551af8152be0d32f02fe3aaa14ae09b
Rodi Hassan <VANRISE\rodi.hassan>
2018-09-28 11:45:37 +0000

    fix bug in edit and add carrier account with supplier auto import settings
    git-tfs-id: [http://developmentvlan:8585/tfs/vanrise.collection]$/;C49760

---
+++ TOneV2/Code/TOneV2/TOne.WhS.BusinessEntity.Entities/InsertCarrierAccountOperationOutput.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace TOne.WhS.BusinessEntity.Entities
+{
+	public class InsertCarrierAccountOperationOutput<T> : Vanrise.Entities.InsertOperationOutput<T>
+	{
+		public List<string> ValidationMessages { get; set; }
+	}
+}
---
+++ TOneV2/Code/TOneV2/TOne.WhS.BusinessEntity.Entities/UpdateCarrierAccountOperationOutput.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace TOne.WhS.BusinessEntity.Entities
+{
+	public class UpdateCarrierAccountOperationOutput<T> : Vanrise.Entities.UpdateOperationOutput<T>
+	{
+		public List<string> ValidationMessages { get; set; }
+	}
+}
