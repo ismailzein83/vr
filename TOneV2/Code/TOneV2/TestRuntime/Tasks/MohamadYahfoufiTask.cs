8b55b6db7fd0838a6208d29374c9dd398e5dd72a
Mohammad Yahfoufi <VANRISE\mohammad.yahfoufi>
2018-09-28 12:26:11 +0000

    Changing MohamadYahfoufi Task
    git-tfs-id: [http://developmentvlan:8585/tfs/vanrise.collection]$/;C49765

--- TOneV2/Code/TOneV2/TestRuntime/Tasks/MohamadYahfoufiTask.cs
+++ TOneV2/Code/TOneV2/TestRuntime/Tasks/MohamadYahfoufiTask.cs
@@ -33,16 +33,59 @@ namespace TestRuntime
             //}
             //return;
 
+            //var runtimeServices = new List<RuntimeService>();
+
+            //BusinessProcessService bpService = new BusinessProcessService { Interval = new TimeSpan(0, 0, 2) };
+            //runtimeServices.Add(bpService);
+
+            //BPRegulatorRuntimeService bpRegulatorRuntimeService = new BPRegulatorRuntimeService { Interval = new TimeSpan(0, 0, 2) };
+            //runtimeServices.Add(bpRegulatorRuntimeService);
+
+            //SchedulerService schedulerService = new SchedulerService { Interval = new TimeSpan(0, 0, 1) };
+            //runtimeServices.Add(schedulerService);
+
+            //RuntimeHost host = new RuntimeHost(runtimeServices);
+            //host.Start();
+            //Console.ReadKey();
+
+
             var runtimeServices = new List<RuntimeService>();
 
-            BusinessProcessService bpService = new BusinessProcessService { Interval = new TimeSpan(0, 0, 2) };
+            BusinessProcessService bpService = new BusinessProcessService() { Interval = new TimeSpan(0, 0, 2) };
             runtimeServices.Add(bpService);
+
+            QueueRegulatorRuntimeService queueRegulatorService = new QueueRegulatorRuntimeService() { Interval = new TimeSpan(0, 0, 2) };
+            runtimeServices.Add(queueRegulatorService);
+
+            QueueActivationRuntimeService queueActivationService = new QueueActivationRuntimeService() { Interval = new TimeSpan(0, 0, 2) };
+            runtimeServices.Add(queueActivationService);
+
+            SummaryQueueActivationRuntimeService summaryQueueActivationService = new SummaryQueueActivationRuntimeService() { Interval = new TimeSpan(0, 0, 2) };
+            runtimeServices.Add(summaryQueueActivationService);
+
+            //SchedulerService schedulerService = new SchedulerService() { Interval = new TimeSpan(0, 0, 1) };
+            //runtimeServices.Add(schedulerService);
+
+            Vanrise.Common.Business.BigDataRuntimeService bigDataService = new Vanrise.Common.Business.BigDataRuntimeService { Interval = new TimeSpan(0, 0, 2) };
+            runtimeServices.Add(bigDataService);
+
+            //Vanrise.Integration.Business.DataSourceRuntimeService dsRuntimeService = new Vanrise.Integration.Business.DataSourceRuntimeService { Interval = new TimeSpan(0, 0, 2) };
+            //runtimeServices.Add(dsRuntimeService);
 
             BPRegulatorRuntimeService bpRegulatorRuntimeService = new BPRegulatorRuntimeService { Interval = new TimeSpan(0, 0, 2) };
             runtimeServices.Add(bpRegulatorRuntimeService);
 
-            //SchedulerService schedulerService = new SchedulerService { Interval = new TimeSpan(0, 0, 1) };
-            //runtimeServices.Add(schedulerService);
+            CachingRuntimeService cachingRuntimeService = new CachingRuntimeService { Interval = new TimeSpan(0, 0, 2) };
+            runtimeServices.Add(cachingRuntimeService);
+
+            CachingDistributorRuntimeService cachingDistributorRuntimeService = new CachingDistributorRuntimeService { Interval = new TimeSpan(0, 0, 2) };
+            runtimeServices.Add(cachingDistributorRuntimeService);
+
+            //DataGroupingExecutorRuntimeService dataGroupingExecutorRuntimeService = new Vanrise.Common.Business.DataGroupingExecutorRuntimeService() { Interval = new TimeSpan(0, 0, 2) };
+            //runtimeServices.Add(dataGroupingExecutorRuntimeService);
+
+            //DataGroupingDistributorRuntimeService dataGroupingDistributorRuntimeService = new Vanrise.Common.Business.DataGroupingDistributorRuntimeService() { Interval = new TimeSpan(0, 0, 2) };
+            //runtimeServices.Add(dataGroupingDistributorRuntimeService);
 
             RuntimeHost host = new RuntimeHost(runtimeServices);
             host.Start();
@@ -51,3 +94,5 @@ namespace TestRuntime
         }
     }
 }
+
+
