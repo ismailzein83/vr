49cd44ede6e74d6a16affa5af697b9f970aad9e4
yara.bousalman <yara.bousalman@vanrise.tfs.local>
2018-10-01 15:15:16 +0000

    Adding RemoteFileController.cs to Vanrise.Common.Web project
    git-tfs-id: [http://developmentvlan:8585/tfs/vanrise.collection]$/;C49820

---
+++ Vanrise/Code/Vanrise.Common.Web/Controllers/RemoteFileController.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Web;
+using System.Web.Http;
+using Vanrise.Entities;
+using Vanrise.Web.Base;
+
+namespace Vanrise.Common.Web.Controllers
+{
+    public class VRCommon_RemoteFileController 
+    {
+       
+    }
+}
