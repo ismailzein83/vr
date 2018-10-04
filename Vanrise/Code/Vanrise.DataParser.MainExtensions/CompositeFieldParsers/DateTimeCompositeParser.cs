604291db9cab53d80afe49e23598e725b1332d4e
Ali Atoui <VANRISE\ali.atoui>
2018-10-04 11:43:35 +0000

    Adding CopyFieldValueCompositeParser
    git-tfs-id: [http://developmentvlan:8585/tfs/vanrise.collection]$/;C49902

--- Vanrise/Code/Vanrise.DataParser.MainExtensions/CompositeFieldParsers/DateTimeCompositeParser.cs
+++ Vanrise/Code/Vanrise.DataParser.MainExtensions/CompositeFieldParsers/DateTimeCompositeParser.cs
@@ -38,4 +38,22 @@ namespace Vanrise.DataParser.MainExtensions.CompositeFieldParsers
             context.Record.SetFieldValue(FieldName, value);
         }
     }
+
+    public class CopyFieldValueCompositeParser : CompositeFieldsParser
+    {
+        public override Guid ConfigId { get { return new Guid("36C3F6CD-0886-402E-BE97-CD57343EA755"); } }
+        public string SourceFieldName { get; set; }
+        public string TargetFieldName { get; set; }
+        public bool OverrideOnlyIfDefault { get; set; }
+
+        public override void Execute(ICompositeFieldsParserContext context)
+        {
+            object targetFieldValue = context.Record.GetFieldValue(TargetFieldName);
+            if (OverrideOnlyIfDefault && targetFieldValue != null)
+                return;
+
+            object sourceFieldValue = context.Record.GetFieldValue(SourceFieldName);
+            context.Record.SetFieldValue(TargetFieldName, sourceFieldValue);
+        }
+    }
 }
