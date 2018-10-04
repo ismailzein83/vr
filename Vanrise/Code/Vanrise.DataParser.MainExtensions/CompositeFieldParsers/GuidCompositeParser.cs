5d7f840e06b5863f9c250f7bff26e34a2e25b9bc
Ali Atoui <VANRISE\ali.atoui>
2018-10-04 10:39:34 +0000

    Arranging GuidCompositeParser
    git-tfs-id: [http://developmentvlan:8585/tfs/vanrise.collection]$/;C49895

--- Vanrise/Code/Vanrise.DataParser.MainExtensions/CompositeFieldParsers/GuidCompositeParser.cs
+++ Vanrise/Code/Vanrise.DataParser.MainExtensions/CompositeFieldParsers/GuidCompositeParser.cs
@@ -9,11 +9,9 @@ namespace Vanrise.DataParser.MainExtensions.CompositeFieldParsers
 {
     public class GuidCompositeParser : CompositeFieldsParser
     {
-        public override Guid ConfigId
-        {
-            get { return new Guid("43720AC7-DCB3-4994-B82A-3749950E9E15"); }
-        }
+        public override Guid ConfigId { get { return new Guid("43720AC7-DCB3-4994-B82A-3749950E9E15"); } }
         public string FieldName { get; set; }
+
         public override void Execute(ICompositeFieldsParserContext context)
         {
             context.Record.SetFieldValue(FieldName, Guid.NewGuid().ToString());
