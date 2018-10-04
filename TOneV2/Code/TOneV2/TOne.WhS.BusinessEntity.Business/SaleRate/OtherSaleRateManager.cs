6df078ab96dcc23aa51728193b1f65bab6546584
Rodi Hassan <VANRISE\rodi.hassan>
2018-10-04 06:18:52 +0000

    fix bug in show other rate is system currency and show correct other rate type for zone
    git-tfs-id: [http://developmentvlan:8585/tfs/vanrise.collection]$/;C49882

--- TOneV2/Code/TOneV2/TOne.WhS.BusinessEntity.Business/SaleRate/OtherSaleRateManager.cs
+++ TOneV2/Code/TOneV2/TOne.WhS.BusinessEntity.Business/SaleRate/OtherSaleRateManager.cs
@@ -1,135 +1,133 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using TOne.WhS.BusinessEntity.Entities;
 using Vanrise.Common;
 using Vanrise.Entities;
 
 namespace TOne.WhS.BusinessEntity.Business
 {
-    public class OtherSaleRateManager
-    {
-        #region Fields
+	public class OtherSaleRateManager
+	{
+		#region Fields
 
-        private Vanrise.Common.Business.CurrencyManager _currencyManager = new Vanrise.Common.Business.CurrencyManager();
+		private Vanrise.Common.Business.CurrencyManager _currencyManager = new Vanrise.Common.Business.CurrencyManager();
 
-        #endregion
+		#endregion
 
-        public IEnumerable<SaleRateDetail> GetOtherSaleRates(OtherSaleRateQuery query)
-        {
-            IEnumerable<RateTypeInfo> allRateTypes = new Vanrise.Common.Business.RateTypeManager().GetAllRateTypes();
-            if (allRateTypes == null || allRateTypes.Count() == 0)
-                return null;
+		public IEnumerable<SaleRateDetail> GetOtherSaleRates(OtherSaleRateQuery query)
+		{
+			IEnumerable<RateTypeInfo> allRateTypes = new Vanrise.Common.Business.RateTypeManager().GetAllRateTypes();
+			if (allRateTypes == null || allRateTypes.Count() == 0)
+				return null;
 
-            IEnumerable<long> zoneIds = new List<long>() { query.ZoneId };
-            return (query.OwnerType == SalePriceListOwnerType.SellingProduct) ? GetSellingProductZoneOtherRates(query, zoneIds, allRateTypes) : GetCustomerZoneOtherRates(query, zoneIds, query.ZoneId, allRateTypes);
-        }
+			IEnumerable<long> zoneIds = new List<long>() { query.ZoneId };
+			return (query.OwnerType == SalePriceListOwnerType.SellingProduct) ? GetSellingProductZoneOtherRates(query, zoneIds, allRateTypes) : GetCustomerZoneOtherRates(query, zoneIds, query.ZoneId, allRateTypes);
+		}
 
-        #region Private Methods
+		#region Private Methods
 
-        private IEnumerable<SaleRateDetail> GetSellingProductZoneOtherRates(OtherSaleRateQuery query, IEnumerable<long> zoneIds, IEnumerable<RateTypeInfo> allRateTypes)
-        {
-            var saleRates = new List<SaleRateDetail>();
-            int currencyId = query.IsSystemCurrency ? new Vanrise.Common.Business.ConfigManager().GetSystemCurrencyId() : query.CurrencyId;
+		private IEnumerable<SaleRateDetail> GetSellingProductZoneOtherRates(OtherSaleRateQuery query, IEnumerable<long> zoneIds, IEnumerable<RateTypeInfo> allRateTypes)
+		{
+			var saleRates = new List<SaleRateDetail>();
+			int currencyId = query.IsSystemCurrency ? new Vanrise.Common.Business.ConfigManager().GetSystemCurrencyId() : query.CurrencyId;
 
-            var productZoneRateHistoryLocator = new ProductZoneRateHistoryLocator(new ProductZoneRateHistoryReader(CreateListFromItem(query.OwnerId), zoneIds, false, true));
+			var productZoneRateHistoryLocator = new ProductZoneRateHistoryLocator(new ProductZoneRateHistoryReader(CreateListFromItem(query.OwnerId), zoneIds, false, true));
 
-            foreach (RateTypeInfo rateType in allRateTypes)
-            {
-                SaleRateHistoryRecord saleRateHistoryRecord = productZoneRateHistoryLocator.GetProductZoneRateHistoryRecord(query.OwnerId, query.ZoneName, rateType.RateTypeId, currencyId, query.EffectiveOn);
-                if (saleRateHistoryRecord != null)
-                    AddSaleRate(saleRates, saleRateHistoryRecord, rateType, query.ZoneName, query.ZoneId, query.CountryId, currencyId, query.IsSystemCurrency);
-            }
+			foreach (RateTypeInfo rateType in allRateTypes)
+			{
+				SaleRateHistoryRecord saleRateHistoryRecord = productZoneRateHistoryLocator.GetProductZoneRateHistoryRecord(query.OwnerId, query.ZoneName, rateType.RateTypeId, currencyId, query.EffectiveOn);
+				if (saleRateHistoryRecord != null)
+					AddSaleRate(saleRates, saleRateHistoryRecord, rateType, query.ZoneName, query.ZoneId, query.CountryId, currencyId, query.IsSystemCurrency);
+			}
 
-            return saleRates;
-        }
+			return saleRates;
+		}
 
-        private IEnumerable<SaleRateDetail> GetCustomerZoneOtherRates(OtherSaleRateQuery query, IEnumerable<long> zoneIds, long zoneId, IEnumerable<RateTypeInfo> allRateTypes)
-        {
-            int longPrecisionValue = new Vanrise.Common.Business.GeneralSettingsManager().GetLongPrecisionValue();
-            int sellingProductId = new CarrierAccountManager().GetSellingProductId(query.OwnerId);
-            var customerZoneRateHistoryLocatorV2 = new CustomerZoneRateHistoryLocator(new CustomerZoneRateHistoryReader(CreateListFromItem(query.OwnerId), CreateListFromItem(sellingProductId), zoneIds, false, true));
+		private IEnumerable<SaleRateDetail> GetCustomerZoneOtherRates(OtherSaleRateQuery query, IEnumerable<long> zoneIds, long zoneId, IEnumerable<RateTypeInfo> allRateTypes)
+		{
+			int longPrecisionValue = new Vanrise.Common.Business.GeneralSettingsManager().GetLongPrecisionValue();
+			int sellingProductId = new CarrierAccountManager().GetSellingProductId(query.OwnerId);
+			var customerZoneRateHistoryLocatorV2 = new CustomerZoneRateHistoryLocator(new CustomerZoneRateHistoryReader(CreateListFromItem(query.OwnerId), CreateListFromItem(sellingProductId), zoneIds, false, true));
 
-            IEnumerable<RateTypeInfo> customerZoneRateTypes = GetCustomerZoneRateTypes(query.OwnerId, zoneId, allRateTypes);
-            int currencyId = query.IsSystemCurrency ? new Vanrise.Common.Business.ConfigManager().GetSystemCurrencyId() : query.CurrencyId;
+			IEnumerable<RateTypeInfo> customerZoneRateTypes = GetCustomerZoneRateTypes(query.OwnerId, zoneId, allRateTypes, query.EffectiveOn);
+			int currencyId = query.IsSystemCurrency ? new Vanrise.Common.Business.ConfigManager().GetSystemCurrencyId() : query.CurrencyId;
 
-            if (customerZoneRateTypes == null || customerZoneRateTypes.Count() == 0)
-                return null;
+			if (customerZoneRateTypes == null || customerZoneRateTypes.Count() == 0)
+				return null;
 
-            var saleRates = new List<SaleRateDetail>();
+			var saleRates = new List<SaleRateDetail>();
 
-            foreach (RateTypeInfo rateType in customerZoneRateTypes)
-            {
-                SaleRateHistoryRecord saleRateHistoryRecord = customerZoneRateHistoryLocatorV2.GetCustomerZoneRateHistoryRecord(query.OwnerId, sellingProductId, query.ZoneName, rateType.RateTypeId, query.CountryId, query.EffectiveOn, query.CurrencyId, longPrecisionValue);
-                if (saleRateHistoryRecord != null)
-                    AddSaleRate(saleRates, saleRateHistoryRecord, rateType, query.ZoneName, query.ZoneId, query.CountryId, currencyId, query.IsSystemCurrency);
-            }
+			foreach (RateTypeInfo rateType in customerZoneRateTypes)
+			{
+				SaleRateHistoryRecord saleRateHistoryRecord = customerZoneRateHistoryLocatorV2.GetCustomerZoneRateHistoryRecord(query.OwnerId, sellingProductId, query.ZoneName, rateType.RateTypeId, query.CountryId, query.EffectiveOn, currencyId, longPrecisionValue);
+				if (saleRateHistoryRecord != null)
+					AddSaleRate(saleRates, saleRateHistoryRecord, rateType, query.ZoneName, query.ZoneId, query.CountryId, currencyId, query.IsSystemCurrency);
+			}
 
-            return saleRates;
-        }
+			return saleRates;
+		}
 
-        private IEnumerable<RateTypeInfo> GetCustomerZoneRateTypes(int customerId, long zoneId, IEnumerable<RateTypeInfo> allRateTypes)
-        {
-            IEnumerable<int> rateTypeIds = Helper.GetRateTypeIds(customerId, zoneId, null);
-            return (rateTypeIds != null && rateTypeIds.Count() > 0) ? allRateTypes.FindAllRecords(x => rateTypeIds.Contains(x.RateTypeId)) : null;
-        }
+		private IEnumerable<RateTypeInfo> GetCustomerZoneRateTypes(int customerId, long zoneId, IEnumerable<RateTypeInfo> allRateTypes, DateTime effectiveOn)
+		{
+			IEnumerable<int> rateTypeIds = Helper.GetRateTypeIds(customerId, zoneId, effectiveOn);
+			return (rateTypeIds != null && rateTypeIds.Count() > 0) ? allRateTypes.FindAllRecords(x => rateTypeIds.Contains(x.RateTypeId)) : null;
+		}
 
-        private void AddSaleRate(List<SaleRateDetail> saleRates, SaleRateHistoryRecord saleRateHistoryRecord, RateTypeInfo rateType, string zoneName, long zoneId, int countryId, int currencyId, bool isSystemCurrency)
-        {
-            var saleRate = new SaleRateDetail();
+		private void AddSaleRate(List<SaleRateDetail> saleRates, SaleRateHistoryRecord saleRateHistoryRecord, RateTypeInfo rateType, string zoneName, long zoneId, int countryId, int currencyId, bool isSystemCurrency)
+		{
+			var saleRate = new SaleRateDetail();
 
-            saleRate.Entity = new SaleRate()
-            {
-                SaleRateId = saleRateHistoryRecord.SaleRateId,
-                RateTypeId = rateType.RateTypeId,
-                ZoneId = zoneId,
-                PriceListId = saleRateHistoryRecord.PriceListId,
-                CurrencyId = saleRateHistoryRecord.CurrencyId, // CustomerZoneRateHistoryLocator gets the pricelist's currency if the rate's currency was not found
-                Rate = saleRateHistoryRecord.Rate,
-                BED = saleRateHistoryRecord.BED,
-                EED = saleRateHistoryRecord.EED,
-                SourceId = saleRateHistoryRecord.SourceId,
-                RateChange = saleRateHistoryRecord.ChangeType
-            };
-            saleRate.ZoneName = zoneName;
-            saleRate.CountryId = countryId;
-            saleRate.RateTypeName = rateType.Name;
-            saleRate.DisplayedCurrency = _currencyManager.GetCurrencySymbol(isSystemCurrency ? currencyId : saleRateHistoryRecord.CurrencyId);
-            saleRate.DisplayedRate = isSystemCurrency ? saleRateHistoryRecord.ConvertedRate : saleRateHistoryRecord.Rate;
-            saleRate.IsRateInherited = saleRateHistoryRecord.SellingProductId.HasValue;
+			saleRate.Entity = new SaleRate()
+			{
+				SaleRateId = saleRateHistoryRecord.SaleRateId,
+				RateTypeId = rateType.RateTypeId,
+				ZoneId = zoneId,
+				PriceListId = saleRateHistoryRecord.PriceListId,
+				CurrencyId = saleRateHistoryRecord.CurrencyId, // CustomerZoneRateHistoryLocator gets the pricelist's currency if the rate's currency was not found
+				Rate = saleRateHistoryRecord.Rate,
+				BED = saleRateHistoryRecord.BED,
+				EED = saleRateHistoryRecord.EED,
+				SourceId = saleRateHistoryRecord.SourceId,
+				RateChange = saleRateHistoryRecord.ChangeType
+			};
+			saleRate.ZoneName = zoneName;
+			saleRate.CountryId = countryId;
+			saleRate.RateTypeName = rateType.Name;
+			saleRate.DisplayedCurrency = _currencyManager.GetCurrencySymbol(isSystemCurrency ? currencyId : saleRateHistoryRecord.CurrencyId);
+			saleRate.DisplayedRate = isSystemCurrency ? saleRateHistoryRecord.ConvertedRate : saleRateHistoryRecord.Rate;
+			saleRate.IsRateInherited = saleRateHistoryRecord.SellingProductId.HasValue;
 
-            saleRates.Add(saleRate);
-        }
+			saleRates.Add(saleRate);
+		}
 
-        private IEnumerable<T> CreateListFromItem<T>(T item)
-        {
-            return new List<T>() { item };
-        }
+		private IEnumerable<T> CreateListFromItem<T>(T item)
+		{
+			return new List<T>() { item };
+		}
 
-        #endregion
-    }
+		#endregion
+	}
 
-    public class OtherSaleRateQuery
-    {
-        public int SellingNumberPlanId { get; set; }
+	public class OtherSaleRateQuery
+	{
+		public int SellingNumberPlanId { get; set; }
 
-        public string ZoneName { get; set; }
+		public string ZoneName { get; set; }
 
-        public long ZoneId { get; set; }
+		public long ZoneId { get; set; }
 
-        public int CountryId { get; set; }
+		public int CountryId { get; set; }
 
-        public SalePriceListOwnerType OwnerType { get; set; }
+		public SalePriceListOwnerType OwnerType { get; set; }
 
-        public int OwnerId { get; set; }
+		public int OwnerId { get; set; }
 
-        public DateTime EffectiveOn { get; set; }
+		public DateTime EffectiveOn { get; set; }
 
-        public int CurrencyId { get; set; }
+		public int CurrencyId { get; set; }
 
-        public bool IsSystemCurrency { get; set; }
+		public bool IsSystemCurrency { get; set; }
 
-    }
+	}
 }
