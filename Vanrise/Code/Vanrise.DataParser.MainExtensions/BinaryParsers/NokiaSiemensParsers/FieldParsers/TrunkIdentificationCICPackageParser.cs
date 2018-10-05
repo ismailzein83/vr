5e17018c170056615e7fa4ae2771f9bede866019
Ali Atoui <VANRISE\ali.atoui>
2018-10-05 10:58:35 +0000

    Moving CopyFieldValueCompositeParser and TrunkIdentificationCICPackageParser
    git-tfs-id: [http://developmentvlan:8585/tfs/vanrise.collection]$/;C49942

---
+++ Vanrise/Code/Vanrise.DataParser.MainExtensions/BinaryParsers/NokiaSiemensParsers/FieldParsers/TrunkIdentificationCICPackageParser.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using Vanrise.DataParser.Business;
+using Vanrise.DataParser.Entities;
+
+namespace Vanrise.DataParser.MainExtensions.BinaryParsers.NokiaSiemensParsers.FieldParsers
+{
+    public class TrunkIdentificationCICPackageParser : BinaryFieldParserSettings
+    {
+        public override Guid ConfigId { get { return new Guid("CEEDF4FA-E2F7-47BC-91AB-EC176AB8B519"); } }
+        public string TrunkGroupNumberCICFieldName { get; set; }
+
+        public override void Execute(IBinaryFieldParserContext context)
+        {
+            byte[] trunkGroupNumberCICBytes = ParserHelper.ExtractFromByteArray(context.FieldValue, 0, 6, false);
+            string trunkGroupNumberCIC = System.Text.Encoding.UTF8.GetString(trunkGroupNumberCICBytes);
+
+            context.Record.SetFieldValue(this.TrunkGroupNumberCICFieldName, trunkGroupNumberCIC);
+        }
+    }
+}
