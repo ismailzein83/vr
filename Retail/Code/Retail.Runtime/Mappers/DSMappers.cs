8925014e62987192cb4e922998dd08f8e86f06fb
Ali Atoui <VANRISE\ali.atoui>
2018-10-05 10:59:00 +0000

    Adding DSMappers class
    git-tfs-id: [http://developmentvlan:8585/tfs/vanrise.collection]$/;C49943

---
+++ Retail/Code/Retail.Runtime/Mappers/DSMappers.cs
@@ -0,0 +1,93 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Vanrise.Integration.Entities;
+
+namespace Retail.Runtime
+{
+    public static class DSMappers
+    {
+        public static Vanrise.Integration.Entities.MappingOutput MapDataImportICX_SQL(Guid dataSourceId, IImportedData data, MappedBatchItemsToEnqueue mappedBatches, List<Object> failedRecordIdentifiers)
+        {
+            LogVerbose("Started");
+
+            var dataList = new List<dynamic>();
+            var dataRecordTypeManager = new Vanrise.GenericData.Business.DataRecordTypeManager();
+            Type dataRuntimeType = dataRecordTypeManager.GetDataRecordRuntimeType("ICX_RawData");
+
+            int batchSize = 50000;
+            var dataRecordVanriseType = new Vanrise.GenericData.Entities.DataRecordVanriseType("ICX_RawData");
+
+            var importedData = ((Vanrise.Integration.Entities.DBReaderImportedData)(data));
+
+            Guid batchIdentifier = Guid.NewGuid();
+
+            IDataReader reader = importedData.Reader;
+
+            int rowCount = 0;
+            while (reader.Read())
+            {
+                dynamic dataRecord = Activator.CreateInstance(dataRuntimeType) as dynamic;
+
+                dataRecord.RecordDateTime = Utils.GetReaderValue<DateTime>(reader, "RecordDateTime");
+
+                string userName = reader["UserName"] as string;
+                dataRecord.ISPName = userName.Substring(userName.IndexOf("@") + 1);
+                dataRecord.UserName = userName;
+                dataRecord.SessionId = reader["AcctSessionId"] as string;
+                dataRecord.InputOctets = (decimal?)Utils.GetReaderValue<long?>(reader, "AcctInputOctets");
+                dataRecord.OutputOctets = (decimal?)Utils.GetReaderValue<long?>(reader, "AcctOutputOctets");
+                dataRecord.DataSourceId = dataSourceId;
+                dataRecord.BatchIdentifier = batchIdentifier;
+                dataList.Add(dataRecord);
+
+                importedData.LastImportedId = reader["Id"];
+
+                rowCount++;
+                if (rowCount == batchSize)
+                    break;
+            }
+
+            if (dataList.Count > 0)
+            {
+                long startingId;
+                Vanrise.Common.Business.IDManager.Instance.ReserveIDRange(dataRecordVanriseType, rowCount, out startingId);
+                long currentDataId = startingId;
+
+                foreach (var dataRecord in dataList)
+                {
+                    dataRecord.Id = currentDataId;
+                    currentDataId++;
+                }
+                var batch = Vanrise.GenericData.QueueActivators.DataRecordBatch.CreateBatchFromRecords(dataList, "#RECORDSCOUNT# of Raw dataList", "ICX_RawData");
+                mappedBatches.Add("Prepare Data Stage", batch);
+            }
+            else
+            {
+                importedData.IsEmpty = true;
+            }
+
+            Vanrise.Integration.Entities.MappingOutput result = new Vanrise.Integration.Entities.MappingOutput();
+            result.Result = Vanrise.Integration.Entities.MappingResult.Valid;
+            LogVerbose("Finished");
+
+            return result;
+        }
+
+        private static void LogVerbose(string Message)
+        {
+            Console.WriteLine(Message);
+        }
+
+        private class Utils
+        {
+            public static T GetReaderValue<T>(IDataReader reader, string fieldName)
+            {
+                return reader[fieldName] != DBNull.Value ? (T)reader[fieldName] : default(T);
+            }
+        }
+    }
+}
