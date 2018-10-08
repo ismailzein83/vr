c4b8b723981d52015ddfb677ba4a72a1d1b86f74
Samer Haidar <VANRISE\samer.haidar>
2018-10-08 15:04:31 +0000

    Adding MonthlyRecuringCharge
    git-tfs-id: [http://developmentvlan:8585/tfs/vanrise.collection]$/;C49989

---
+++ TOneV2/Code/TOneV2/TOne.WhS.BusinessEntity.MainExtensions/RecurringCharges/MonthlyRecuringCharge.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TOne.WhS.BusinessEntity.Entities;
+
+namespace TOne.WhS.BusinessEntity.MainExtensions.RecurringCharges
+{
+    public class MonthlyRecuringCharge : RecurringChargePeriodSettings
+    {
+        public override Guid ConfigId
+        {
+            get { return new Guid("A1D0F08B-B8F7-4B9C-9C83-49DABD2A68D5"); }
+        }
+        public int Day { get; set; }
+        public override void Execute(IRecurringChargePeriodSettingsContext context)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
