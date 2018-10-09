2a8596e8a91f72c3806f57c76009bd02962155b8
yara.bousalman <yara.bousalman@vanrise.tfs.local>
2018-10-09 12:46:17 +0000

    Implementing monthly recurring charges to Recurring charge period in financial accounts
    git-tfs-id: [http://developmentvlan:8585/tfs/vanrise.collection]$/;C50021

--- TOneV2/Code/TOneV2/TOne.WhS.BusinessEntity.MainExtensions/RecurringCharges/MonthlyRecuringCharge.cs
+++ TOneV2/Code/TOneV2/TOne.WhS.BusinessEntity.MainExtensions/RecurringCharges/MonthlyRecuringCharge.cs
@@ -16,7 +16,135 @@ namespace TOne.WhS.BusinessEntity.MainExtensions.RecurringCharges
         public int Day { get; set; }
         public override void Execute(IRecurringChargePeriodSettingsContext context)
         {
-            throw new NotImplementedException();
+            List<RecurringChargePeriodOutput> periodsList = new List<RecurringChargePeriodOutput>();
+            if (context.FromDate.Year == context.ToDate.Year)
+            {
+                if (context.FromDate.Month == context.ToDate.Month)
+                {
+                    if (this.Day >= context.FromDate.Day && this.Day <= context.ToDate.Day)
+                    {
+                        int daysInMonth = DateTime.DaysInMonth(context.FromDate.Year, context.FromDate.Month);
+                        int day = this.Day;
+                        if (day > daysInMonth)
+                            day = daysInMonth;
+                        var fromDate = new DateTime(context.FromDate.Year, context.FromDate.Month, day);
+                        var toDate = fromDate.AddMonths(1).AddDays(-1);
+                        periodsList.Add(new RecurringChargePeriodOutput
+                        {
+                            From = fromDate,
+                            To = toDate,
+                        });
+                    }
+                }
+                else
+                {
+                    for (var i = context.FromDate.Month; i < context.ToDate.Month; i++)
+                    {
+                        if(i==context.FromDate.Month && this.Day < context.FromDate.Day)
+                        {
+                            continue;
+                        }
+                        int daysInMonth = DateTime.DaysInMonth(context.FromDate.Year, i);
+                        int day = this.Day;
+                        if (day > daysInMonth)
+                            day = daysInMonth;
+                        var fromDate = new DateTime(context.FromDate.Year, i, day);
+                        var toDate = fromDate.AddMonths(1).AddDays(-1);
+                        if (i == context.ToDate.Month - 1)
+                        {
+                            if (toDate.Month==context.ToDate.Month && toDate.Day > context.ToDate.Day)
+                            {
+                                break;
+                            }
+                        }
+                        periodsList.Add(new RecurringChargePeriodOutput
+                        {
+                            From = fromDate,
+                            To = toDate,
+                        });
+                    }
+                }
+            }
+            else
+            {
+                int currentYear = context.FromDate.Year;
+                while (currentYear <= context.ToDate.Year)
+                {
+                    if (currentYear == context.FromDate.Year)
+                    {
+                        int currentMonth = context.FromDate.Month;
+                        if (this.Day < context.FromDate.Day)
+                            currentMonth++;
+                        while (currentMonth <= 12)
+                        {
+                            int daysInMonth = DateTime.DaysInMonth(currentYear, currentMonth);
+                            int day = this.Day;
+                            if (day > daysInMonth)
+                                day = daysInMonth;
+                            var fromDate = new DateTime(currentYear, currentMonth, day);
+                            var toDate = fromDate.AddMonths(1).AddDays(-1);
+                            periodsList.Add(new RecurringChargePeriodOutput
+                            {
+                                From = fromDate,
+                                To = toDate,
+                            });
+                            currentMonth++;
+                        }
+                    }
+                    else if (currentYear == context.ToDate.Year)
+                    {
+                        int currentMonth = 1;
+                        int lastMonth = context.ToDate.Month;
+                        if (this.Day > context.ToDate.Day)
+                            lastMonth--;
+                        while (currentMonth < lastMonth)
+                        {
+                            int daysInMonth = DateTime.DaysInMonth(currentYear, currentMonth);
+                            int day = this.Day;
+                            if (day > daysInMonth)
+                                day = daysInMonth;
+
+                            var fromDate = new DateTime(currentYear, currentMonth, day);
+                            var toDate = fromDate.AddMonths(1).AddDays(-1);
+                            if (currentMonth == lastMonth - 1)
+                            {
+                                if (toDate.Month==context.ToDate.Month && toDate.Day > context.ToDate.Day)
+                                {
+                                    break;
+                                }
+                            }
+                            periodsList.Add(new RecurringChargePeriodOutput
+                            {
+                                From = fromDate,
+                                To = toDate,
+                            });
+                            currentMonth++;
+                        }
+                    }
+                    else
+                    {
+                        int currentMonth = 1;
+                        while (currentMonth <= 12)
+                        {
+                            int daysInMonth = DateTime.DaysInMonth(currentYear, currentMonth);
+                            int day = this.Day;
+                            if (day > daysInMonth)
+                                day = daysInMonth;
+
+                            var fromDate = new DateTime(currentYear, currentMonth, day);
+                            var toDate = fromDate.AddMonths(1).AddDays(-1);
+                            periodsList.Add(new RecurringChargePeriodOutput
+                            {
+                                From = fromDate,
+                                To = toDate,
+                            });
+                            currentMonth++;
+                        }
+                    }
+                    currentYear++;
+                }
+            }
+            context.Periods = periodsList;
         }
     }
 }
--- TOneV2/Code/TOneV2/TOne.WhS.BusinessEntity.Web/WhS_BusinessEntity/Directives/RecurringCharges/MainExtensions/whs-be-recurringchargeperiodsettings-monthly.js
+++ TOneV2/Code/TOneV2/TOne.WhS.BusinessEntity.Web/WhS_BusinessEntity/Directives/RecurringCharges/MainExtensions/whs-be-recurringchargeperiodsettings-monthly.js
@@ -1,1 +1,87 @@
-﻿
+﻿"use strict";
+
+app.directive("whsBeRecurringchargeperiodsettingsMonthly", ["UtilsService", "VRNotificationService", "VRUIUtilsService",
+    function (UtilsService, VRNotificationService, VRUIUtilsService) {
+
+        var directiveDefinitionObject = {
+
+            restrict: "E",
+            scope:
+            {
+                onReady: "=",
+            },
+            controller: function ($scope, $element, $attrs) {
+                var ctrl = this;
+
+                var ctor = new MonthlyRecurringCharge($scope, ctrl, $attrs);
+                ctor.initializeController();
+            },
+            controllerAs: "ctrl",
+            bindToController: true,
+            compile: function (element, attrs) {
+
+            },
+            templateUrl: "/Client/Modules/WhS_BusinessEntity/Directives/RecurringCharges/MainExtensions/Templates/MonthlyRecurringChargePeriodTemplate.html"
+
+        };
+
+        function MonthlyRecurringCharge($scope, ctrl, $attrs) {
+            this.initializeController = initializeController;
+
+            var nbOfDaysSelectorAPI;
+            var nbOfDaysSelectorReadyDeferred = UtilsService.createPromiseDeferred();
+
+            function initializeController() {
+                $scope.scopeModel = {};
+                $scope.scopeModel.days = [];
+
+                $scope.scopeModel.onNbOfDaysSelectorReady = function (api) {
+                    nbOfDaysSelectorAPI = api;
+                    nbOfDaysSelectorReadyDeferred.resolve();
+                };
+
+                defineAPI();
+            }
+
+            function defineAPI() {
+                var api = {};
+
+                api.load = function (payload) {
+                    var promises = [];
+                    promises.push(loadNbOfDaysSelector());
+
+                    function loadNbOfDaysSelector() {
+                        var nbOfDaysSelectorLoadDeferred = UtilsService.createPromiseDeferred();
+
+                        nbOfDaysSelectorReadyDeferred.promise.then(function () {
+                            $scope.scopeModel.days.length = 0;
+                            for (var i = 1; i <= 31; i++) {
+                                $scope.scopeModel.days.push({ day: i, dayDescription: "" + i + "" });
+                            }
+                            if (payload != undefined && payload.extendedSettings != undefined && payload.extendedSettings.Day != undefined) {
+                                $scope.scopeModel.selectedDay = UtilsService.getItemByVal($scope.scopeModel.days, payload.extendedSettings.Day, "day");
+                            }
+                            nbOfDaysSelectorLoadDeferred.resolve();
+                        });
+                        return nbOfDaysSelectorLoadDeferred.promise;
+                    }
+
+                    return UtilsService.waitMultiplePromises(promises);
+                };
+
+                api.getData = function () {
+                    return {
+                        $type: "TOne.WhS.BusinessEntity.MainExtensions.RecurringCharges.MonthlyRecuringCharge ,TOne.WhS.BusinessEntity.MainExtensions",
+                        Day: $scope.scopeModel.selectedDay!=undefined ? $scope.scopeModel.selectedDay.day : undefined
+                    };
+                };
+
+                if (ctrl.onReady != null)
+                    ctrl.onReady(api);
+            }
+        }
+
+        return directiveDefinitionObject;
+
+    }
+]);
