503f53ab011ebdebc8ca1cb3920bb7beb47a1147
yara.bousalman <yara.bousalman@vanrise.tfs.local>
2018-10-10 07:18:52 +0000

    IsDefaultSecurityProviderFieldFormula directive and template
    git-tfs-id: [http://developmentvlan:8585/tfs/vanrise.collection]$/;C50033

--- Vanrise/Code/Vanrise.GenericData.Web/VR_GenericData/Directives/MainExtensions/DataRecordFieldFormulas/vr-genericdata-datarecordtypefields-formula-isdefaultsecurityprovider.js
+++ Vanrise/Code/Vanrise.GenericData.Web/VR_GenericData/Directives/MainExtensions/DataRecordFieldFormulas/vr-genericdata-datarecordtypefields-formula-isdefaultsecurityprovider.js
@@ -1,1 +1,65 @@
-﻿
+﻿'use strict';
+
+app.directive('vrGenericdataDatarecordtypefieldsFormulaIsdefaultsecurityprovider', ['UtilsService', 
+    function (UtilsService) {
+
+        var directiveDefinitionObject = {
+            restrict: 'E',
+            scope: {
+                onReady: '='
+            },
+            controller: function ($scope, $element, $attrs) {
+                var ctrl = this;
+                var ctor = new IsDefaultSecurityProvider(ctrl, $scope);
+                ctor.initializeController();
+            },
+            controllerAs: 'ctrl',
+            bindToController: true,
+            compile: function (element, attrs) {
+                return {
+                    pre: function ($scope, iElem, iAttrs, ctrl) {
+
+                    }
+                };
+            },
+            templateUrl: function (element, attrs) {
+                return '/Client/Modules/VR_GenericData/Directives/MainExtensions/DataRecordFieldFormulas/Templates/IsDefaultSecurityProviderFieldFormulaTemplate.html';
+            }
+        };
+
+        function IsDefaultSecurityProvider(ctrl, $scope) {
+            this.initializeController = initializeController;
+
+            function initializeController() {
+                $scope.scopeModel = {};
+
+                defineAPI();
+            }
+
+            function defineAPI() {
+                var api = {};
+
+                api.load = function (payload) {
+
+                    if (payload != undefined) {
+
+                        if (payload.formula != undefined) {
+                            $scope.scopeModel.fieldName = payload.formula.FieldName;
+                        }
+                    }
+                };
+
+                api.getData = function () {
+                    return {
+                        $type: "Vanrise.GenericData.MainExtensions.DataRecordFieldFormulas.IsDefaultSecurityProviderFieldFormula, Vanrise.GenericData.MainExtensions",
+                        FieldName: $scope.scopeModel.fieldName,
+                    };
+                };
+
+                if (ctrl.onReady != null)
+                    ctrl.onReady(api);
+            }
+        }
+        return directiveDefinitionObject;
+    }
+]);
