727c5f67fef89ef3fe3f94161af30999b03cd42a
yara.bousalman <yara.bousalman@vanrise.tfs.local>
2018-10-10 05:32:19 +0000

    Primary check-in for IsDefaultSecurityProviderFieldFormula for testing purposes
    git-tfs-id: [http://developmentvlan:8585/tfs/vanrise.collection]$/;C50029

--- Vanrise/Code/Vanrise.GenericData.MainExtensions/DataRecordFieldFormulas/IsDefaultSecurityProviderFieldFormula.cs
+++ Vanrise/Code/Vanrise.GenericData.MainExtensions/DataRecordFieldFormulas/IsDefaultSecurityProviderFieldFormula.cs
@@ -3,10 +3,53 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Vanrise.GenericData.Entities;
+using Vanrise.Security.Business;
 
 namespace Vanrise.GenericData.MainExtensions.DataRecordFieldFormulas
 {
-    public class IsDefaultSecurityProviderFieldFormula
+    public class IsDefaultSecurityProviderFieldFormula : DataRecordFieldFormula
     {
+        public override Guid ConfigId { get { return new Guid("ECBC9B85-98F1-4828-8FA3-161B9E639F1C"); } }
+
+        public string FieldName { get; set; }
+
+        public override List<string> GetDependentFields(IDataRecordFieldFormulaGetDependentFieldsContext context)
+        {
+            return new List<string>() { FieldName };
+        }
+
+        public override dynamic CalculateValue(IDataRecordFieldFormulaCalculateValueContext context)
+        {
+            Guid defaultSecurityProviderId = new ConfigManager().GetDefaultSecurityProviderId();
+            var securityProviderId = context.GetFieldValue(FieldName);
+            return securityProviderId == defaultSecurityProviderId;
+        }
+
+        public override RecordFilter ConvertFilter(IDataRecordFieldFormulaConvertFilterContext context)
+        {
+            if (context.InitialFilter == null)
+                throw new ArgumentNullException("context.InitialFilter");
+
+            EmptyRecordFilter emptyFilter = context.InitialFilter as EmptyRecordFilter;
+            if (emptyFilter != null)
+                return new AlwaysFalseRecordFilter() { };
+
+            NonEmptyRecordFilter nonEmptyFilter = context.InitialFilter as NonEmptyRecordFilter;
+            if (nonEmptyFilter != null)
+                return null;
+            StringRecordFilter stringRecordFilter = context.InitialFilter as StringRecordFilter;
+            if (stringRecordFilter != null)
+            {
+                var fieldValue = stringRecordFilter.Value;
+                return new StringRecordFilter()
+                {
+                    FieldName = FieldName,
+                    Value = fieldValue,
+                    CompareOperator = StringRecordFilterOperator.Equals
+                };
+            }
+            throw new Exception(String.Format("Invalid Record Filter '{0}'", context.InitialFilter.GetType()));
+        }
     }
 }
