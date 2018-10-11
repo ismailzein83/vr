5cb3f084ea1f18166c70c50a73a330445d88d005
Samer Haidar <VANRISE\samer.haidar>
2018-10-11 08:22:04 +0000

    Adding BuildCDRCorrelationFilterGroup
    git-tfs-id: [http://developmentvlan:8585/tfs/vanrise.collection]$/;C50077

---
+++ Retail/Code/TestCallAnalysis.BP.Activities/BuildCDRCorrelationFilterGroup.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Activities;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TestCallAnalysis.BP.Activities
+{
+    public sealed class BuildCDRCorrelationFilterGroup : CodeActivity
+    {
+        [RequiredArgument]
+        public InArgument<TimeSpan> DateTimeMargin { get; set; }
+
+        [RequiredArgument]
+        public InArgument<TimeSpan> BatchIntervalTime { get; set; }
+        protected override void Execute(CodeActivityContext context)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
