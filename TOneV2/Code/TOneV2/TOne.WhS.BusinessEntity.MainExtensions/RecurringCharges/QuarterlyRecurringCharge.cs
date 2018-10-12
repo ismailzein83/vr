0ed48ed242be72b07a3a869cfd52e6b4518840d2
yara.bousalman <yara.bousalman@vanrise.tfs.local>
2018-10-12 09:40:48 +0000

    Primary check in for QuarterlyRecurringCharge files for testing purposes
    git-tfs-id: [http://developmentvlan:8585/tfs/vanrise.collection]$/;C50130

--- TOneV2/Code/TOneV2/TOne.WhS.BusinessEntity.MainExtensions/RecurringCharges/QuarterlyRecurringCharge.cs
+++ TOneV2/Code/TOneV2/TOne.WhS.BusinessEntity.MainExtensions/RecurringCharges/QuarterlyRecurringCharge.cs
@@ -3,10 +3,122 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using TOne.WhS.BusinessEntity.Entities;
 
 namespace TOne.WhS.BusinessEntity.MainExtensions.RecurringCharges
 {
-    public class QuarterlyRecurringCharge
+    public class QuarterlyRecurringCharge : RecurringChargePeriodSettings
     {
+        public override Guid ConfigId
+        {
+            get { return new Guid("A5A8B72B-F834-4DAA-80F8-25157C2C3D37"); }
+        }
+
+        public override void Execute(IRecurringChargePeriodSettingsContext context)
+        {
+            List<RecurringChargePeriodOutput> periodsList = new List<RecurringChargePeriodOutput>();
+            if (context.FromDate.Year == context.ToDate.Year)
+            {
+                if (context.FromDate.Month == context.ToDate.Month)
+                {
+                    var fromDate = new DateTime(context.FromDate.Year, context.FromDate.Month, 1);
+                    var toDate = fromDate.AddMonths(3).AddDays(-1);
+                    periodsList.Add(new RecurringChargePeriodOutput
+                    {
+                        From = fromDate,
+                        To = toDate,
+                    });
+                }
+                else
+                {
+                    for (var i = context.FromDate.Month; i <= context.ToDate.Month; i+=3)
+                    {
+                        if (i == context.FromDate.Month && 1 < context.FromDate.Day)
+                        {
+                            continue;
+                        }
+                        var fromDate = new DateTime(context.FromDate.Year, i, 1);
+                        var toDate = fromDate.AddMonths(3).AddDays(-1);
+                        if (i == context.ToDate.Month)
+                        {
+                            if (toDate.Month == context.ToDate.Month && toDate.Day > context.ToDate.Day)
+                            {
+                                break;
+                            }
+                        }
+                        periodsList.Add(new RecurringChargePeriodOutput
+                        {
+                            From = fromDate,
+                            To = toDate,
+                        });
+                    }
+                }
+            }
+            else
+            {
+                int currentYear = context.FromDate.Year;
+                while (currentYear <= context.ToDate.Year)
+                {
+                    if (currentYear == context.FromDate.Year)
+                    {
+                        int currentMonth = context.FromDate.Month;
+                        if (1 < context.FromDate.Day)
+                            currentMonth += 3;
+
+                        while (currentMonth <= 12)
+                        {
+                            var fromDate = new DateTime(currentYear, currentMonth, 1);
+                            var toDate = fromDate.AddMonths(3).AddDays(-1);
+                            periodsList.Add(new RecurringChargePeriodOutput
+                            {
+                                From = fromDate,
+                                To = toDate,
+                            });
+                            currentMonth+=3;
+                        }
+                    }
+                    else if (currentYear == context.ToDate.Year)
+                    {
+                        int currentMonth = 1;
+                        while (currentMonth <= context.ToDate.Month)
+                        {
+                            var fromDate = new DateTime(currentYear, currentMonth, 1);
+                            var toDate = fromDate.AddMonths(3).AddDays(-1);
+                            if (currentMonth == context.ToDate.Month)
+                            {
+                                if (toDate.Month == context.ToDate.Month && toDate.Day > context.ToDate.Day)
+                                {
+                                    break;
+                                }
+                            }
+                            periodsList.Add(new RecurringChargePeriodOutput
+                            {
+                                From = fromDate,
+                                To = toDate,
+                            });
+                            currentMonth += 3;
+
+                        }
+                    }
+                    else
+                    {
+                        int currentMonth = 1;
+                        while (currentMonth <= 12)
+                        {
+                            var fromDate = new DateTime(currentYear, currentMonth, 1);
+                            var toDate = fromDate.AddMonths(3).AddDays(-1);
+                            periodsList.Add(new RecurringChargePeriodOutput
+                            {
+                                From = fromDate,
+                                To = toDate,
+                            });
+                            currentMonth += 3;
+                        }
+                    }
+                    currentYear++;
+                }
+            }
+            context.Periods = periodsList;
+        }
     }
 }
--- TOneV2/Code/TOneV2/TOne.WhS.BusinessEntity.Web/WhS_BusinessEntity/Directives/RecurringCharges/MainExtensions/whs-be-recurringchargeperiodsettings-quarterly.js
+++ TOneV2/Code/TOneV2/TOne.WhS.BusinessEntity.Web/WhS_BusinessEntity/Directives/RecurringCharges/MainExtensions/whs-be-recurringchargeperiodsettings-quarterly.js
@@ -1,1 +1,61 @@
-﻿
+﻿"use strict";
+
+app.directive("whsBeRecurringchargeperiodsettingsQuarterly", ["UtilsService", "VRNotificationService", "VRUIUtilsService",
+    function (UtilsService, VRNotificationService, VRUIUtilsService) {
+
+        var directiveDefinitionObject = {
+
+            restrict: "E",
+            scope:
+            {
+                onReady: "=",
+            },
+            controller: function ($scope, $element, $attrs) {
+                var ctrl = this;
+
+                var ctor = new QuarterlyRecurringCharge($scope, ctrl, $attrs);
+                ctor.initializeController();
+            },
+            controllerAs: "ctrl",
+            bindToController: true,
+            compile: function (element, attrs) {
+
+            },
+            templateUrl: "/Client/Modules/WhS_BusinessEntity/Directives/RecurringCharges/MainExtensions/Templates/QuarterlyRecurringChargePeriodTemplate.html"
+
+        };
+
+        function QuarterlyRecurringCharge($scope, ctrl, $attrs) {
+            this.initializeController = initializeController;
+
+
+            function initializeController() {
+                $scope.scopeModel = {};
+
+                defineAPI();
+            }
+
+            function defineAPI() {
+                var api = {};
+
+                api.load = function (payload) {
+                    var promises = [];
+
+                    return UtilsService.waitMultiplePromises(promises);
+                };
+
+                api.getData = function () {
+                    return {
+                        $type: "TOne.WhS.BusinessEntity.MainExtensions.RecurringCharges.QuarterlyRecurringCharge,TOne.WhS.BusinessEntity.MainExtensions",
+                    };
+                };
+
+                if (ctrl.onReady != null)
+                    ctrl.onReady(api);
+            }
+        }
+
+        return directiveDefinitionObject;
+
+    }
+]);
