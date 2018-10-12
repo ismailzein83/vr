66acf2c3b4544361e541d63644aff2778d271b81
Ali Sakr <VANRISE\ali.sakr>
2018-10-12 13:21:44 +0000

    Adding new business rule
    git-tfs-id: [http://developmentvlan:8585/tfs/vanrise.collection]$/;C50147

---
+++ TOneV2/Code/TOneV2/TOne.WhS.SupplierPriceList.Business/BusinessRules/Conditions/ValidateZones/EmptyPriceListCondition.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TOne.WhS.SupplierPriceList.Entities;
+using Vanrise.BusinessProcess.Entities;
+
+namespace TOne.WhS.SupplierPriceList.Business
+{
+    public class EmptyPriceListCondition : BusinessRuleCondition
+    {
+        public override bool ShouldValidate(IRuleTarget target)
+        {
+            return (target as AllImportedDataByZone != null);
+        }
+
+        public override bool Validate(IBusinessRuleConditionValidateContext context)
+        {
+            //AllImportedDataByZone allImportedDataByZone = context.Target as AllImportedDataByZone;
+            //var invalidZones = new HashSet<string>();
+
+            //if (allImportedDataByZone.ImportedDataByZoneList.Count() == 0)
+            //{
+            //    context.Message = string.Format("The imported pricelist is empty");
+            //    return false;
+            //}
+
+            return true;
+        }
+
+        public override string GetMessage(IRuleTarget target)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
