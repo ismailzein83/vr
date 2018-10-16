c6e0799d1a19f314461f49467d5ae5c5f9fda512
Wissam Ajamy <VANRISE\wissam.ajamy>
2018-10-16 08:30:56 +0000

    update structure from TOneConfiguration: invoice history
    git-tfs-id: [http://developmentvlan:8585/tfs/vanrise.collection]$/;C50202

---
+++ TOneV2/Code/TOneV2/SQL.TOneConfiguration/VR_Invoice/Stored Procedures/sp_PartnerInvoiceSetting_GetById.sql
@@ -0,0 +1,12 @@
+﻿-- =============================================
+-- Author:		<Author,,Name>
+-- Create date: <Create Date,,>
+-- Description:	<Description,,>
+-- =============================================
+Create PROCEDURE [VR_Invoice].[sp_PartnerInvoiceSetting_GetById]
+	@Id uniqueidentifier
+AS
+BEGIN
+	Select ID, PartnerId, InvoiceSettingID, Details FROM VR_Invoice.PartnerInvoiceSetting
+	WHERE ID = @Id
+END
