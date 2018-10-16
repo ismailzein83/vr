26a73b6d751230edc3f43c288c3b39aad5363ed4
Ali Atoui <VANRISE\ali.atoui>
2018-10-16 08:17:18 +0000

    Arranging IImportedData
    git-tfs-id: [http://developmentvlan:8585/tfs/vanrise.collection]$/;C50200

--- Vanrise/Code/Vanrise.Integration.Entities/Receive/ImportedDataTypes/IImportedData.cs
+++ Vanrise/Code/Vanrise.Integration.Entities/Receive/ImportedDataTypes/IImportedData.cs
@@ -8,12 +8,12 @@ namespace Vanrise.Integration.Entities
 
         long? BatchSize { get; }
 
-        void OnDisposed();
-
-        bool IsMultipleReadings { get; }
-
         bool IsEmpty { get; }
 
         bool IsFile { get; }
+
+        bool IsMultipleReadings { get; }
+
+        void OnDisposed();
     }
 }
