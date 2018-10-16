e61dfcf381edc23a8bb84d402502414fe2f68419
yara.bousalman <yara.bousalman@vanrise.tfs.local>
2018-10-16 08:53:11 +0000

    Preliminary check-in of SetDefaultSecurityProviderAction for testing purposes
    git-tfs-id: [http://developmentvlan:8585/tfs/vanrise.collection]$/;C50205

--- Vanrise/Code/Vanrise.Security.MainExtensions/GenericBEActions/SetDefaultSecurityProviderAction.cs
+++ Vanrise/Code/Vanrise.Security.MainExtensions/GenericBEActions/SetDefaultSecurityProviderAction.cs
@@ -3,10 +3,19 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Vanrise.GenericData.Business;
 
 namespace Vanrise.Security.MainExtensions.GenericBEActions
 {
-    class SetDefaultSecurityProviderAction
+    public class SetDefaultSecurityProviderAction : GenericBEActionSettings
     {
+        public override Guid ConfigId => new Guid("D44A8DED-2E6B-4226-99C7-BAA6895BECBE");
+
+        public override string ActionTypeName { get => "SetDefaultSecurityProviderAction"; }
+
+        public override bool DoesUserHaveAccess(IGenericBEActionDefinitionCheckAccessContext context)
+        {
+            return new GenericBusinessEntityManager().DoesUserHaveEditAccess(context.UserId, context.BusinessEntityDefinitionId);
+        }
     }
 }
