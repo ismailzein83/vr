bbf84d4a547ca1ba9b1b1aa6ccafd25621c54eff
Ali Atoui <VANRISE\ali.atoui>
2018-10-16 08:16:11 +0000

    Arranging TextFileImportedData
    git-tfs-id: [http://developmentvlan:8585/tfs/vanrise.collection]$/;C50199

--- Vanrise/Code/Vanrise.Integration.Entities/Receive/ImportedDataTypes/TextImportedData.cs
+++ Vanrise/Code/Vanrise.Integration.Entities/Receive/ImportedDataTypes/TextImportedData.cs
@@ -1,47 +1,28 @@
-﻿
+﻿using System;
+
 namespace Vanrise.Integration.Entities
 {
     public class TextFileImportedData : IImportedData
     {
         public string FileName { get; set; }
+
         public string Content { get; set; }
 
         public long Size { get; set; }
 
-        public string Description
-        {
-            get { return FileName; }
-        }
+        public string Description { get { return FileName; } }
 
+        public long? BatchSize { get { return this.Size; } }
 
-        public long? BatchSize
-        {
-            get { return this.Size; }
-        }
+        public bool IsEmpty { get; set; }
+
+        public bool IsFile { get { return true; } }
+
+        public bool IsMultipleReadings { get { return false; } }
 
         public void OnDisposed()
         {
 
-        }
-
-        public bool IsMultipleReadings
-        {
-            get { return false; }
-        }
-
-        public bool IsEmpty
-        {
-            get;
-            set;
-        }
-
-
-        public bool IsFile
-        {
-            get
-            {
-                return true;
-            }
         }
     }
 }
