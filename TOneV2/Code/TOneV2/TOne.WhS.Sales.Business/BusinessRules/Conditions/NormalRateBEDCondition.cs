59d656ac27056222238169c2a23479f9e5e761a0
Mohammad Yahfoufi <VANRISE\mohammad.yahfoufi>
2018-10-17 13:03:26 +0000

    Implementing NormalRateBEDCondition Business Rule
    git-tfs-id: [http://developmentvlan:8585/tfs/vanrise.collection]$/;C50264

--- TOneV2/Code/TOneV2/TOne.WhS.Sales.Business/BusinessRules/Conditions/NormalRateBEDCondition.cs
+++ TOneV2/Code/TOneV2/TOne.WhS.Sales.Business/BusinessRules/Conditions/NormalRateBEDCondition.cs
@@ -18,61 +18,43 @@ namespace TOne.WhS.Sales.Business
 		}
 		public override bool Validate(IBusinessRuleConditionValidateContext context)
 		{
-			//var ratePlanContext = context.GetExtension<IRatePlanContext>();
-			////TODO: Throw a new data integrity validation exception
+			var ratePlanContext = context.GetExtension<IRatePlanContext>();
+			//TODO: Throw a new data integrity validation exception
 
-			//if (ratePlanContext.OwnerType == SalePriceListOwnerType.Customer)
-			//{
-			//	CarrierAccountManager carrierAccountManager = new CarrierAccountManager();
+			if (ratePlanContext.OwnerType == SalePriceListOwnerType.Customer)
+			{
+				CarrierAccountManager carrierAccountManager = new CarrierAccountManager();
 
-			//	int sellingProductId = carrierAccountManager.GetSellingProductId(ratePlanContext.OwnerId);
+				int sellingProductId = carrierAccountManager.GetSellingProductId(ratePlanContext.OwnerId);
+				var lastRateLocator = ratePlanContext.LastRateLocator;
 
+				var allZoneData = context.Target as AllDataByZone;
 
-			//	var customerZoneRateHistoryReader = ratePlanContext.CustomerZoneRateHistoryReader;
-			//	var lasRateLocator = ratePlanContext.LastRateLocator;
-			//	var customerZoneRateHistoryLocator = new CustomerZoneRateHistoryLocator(customerZoneRateHistoryReader);
-			//	var allZoneData = context.Target as AllDataByZone;
+				List<string> zoneWithNormalBEDLessThanOtherRatesBED = new List<string>();
 
-			//	List<string> zoneWithInvalidBED = new List<string>();
-			//	List<string> zoneWithInvalidOtherRateBED = new List<string>();
+				foreach (var zoneData in allZoneData.DataByZoneList)
+				{
+					if (zoneData.NormalRateToChange != null)
+					{
+						var lastCustomerRate = lastRateLocator.GetCustomerZoneRate(ratePlanContext.OwnerId, sellingProductId, zoneData.ZoneId);
 
-			//	foreach (var zoneData in allZoneData.DataByZoneList)
-			//	{
-			//		if (zoneData.OtherRatesToChange.Count() > 0)
-			//		{
-			//			var currentCustomerRate = customerZoneRateHistoryLocator.GetCustomerZoneRateHistoryRecord(ratePlanContext.OwnerId, sellingProductId, zoneData.ZoneName, null, zoneData.CountryId, zoneData.OtherRatesToChange.First().BED, ratePlanContext.CurrencyId, ratePlanContext.LongPrecision);
-			//			var lastCustomerRate = lasRateLocator.GetCustomerZoneRate(ratePlanContext.OwnerId, sellingProductId, zoneData.ZoneId);
+						if (lastCustomerRate == null)
+							throw new VRBusinessException(string.Format("Zone {0} has no rates set neither for customer nor for selling product", zoneData.ZoneName));
+						if (lastCustomerRate.RatesByRateType != null && lastCustomerRate.RatesByRateType.Values.Any())
+						{
+							var max = lastCustomerRate.RatesByRateType.Values.MaxBy(item => item.BED).BED;
+							if (zoneData.NormalRateToChange.BED < max)
+								zoneWithNormalBEDLessThanOtherRatesBED.Add(zoneData.ZoneName);
+						}
+					}
+				}
 
-			//			if (lastCustomerRate == null)
-			//				throw new VRBusinessException(string.Format("Zone {0} has no rates set neither for customer nor for selling product", zoneData.ZoneName));
-			//			if (lastCustomerRate.RatesByRateType != null && lastCustomerRate.RatesByRateType.Values.Any())
-			//			{
-			//				var max = lastCustomerRate.RatesByRateType.Values.MaxBy(item => item.BED).BED;
-			//				if (zoneData.OtherRatesToChange.First().BED < max)
-			//					zoneWithInvalidOtherRateBED.Add(zoneData.ZoneName);
-			//			}
-			//			if (zoneData.OtherRatesToChange.First().BED < lastCustomerRate.Rate.BED && (zoneData.NormalRateToChange != null && currentCustomerRate != null && zoneData.OtherRatesToChange.First().BED < zoneData.NormalRateToChange.BED))
-			//			{
-			//				zoneWithInvalidBED.Add(zoneData.ZoneName);
-			//			}
-			//		}
-
-   //                 if (zoneData.NormalRateToChange!=null)
-   //                 {
-
-   //                 }
-
-			//	}
-
-			//	if (zoneWithInvalidOtherRateBED.Count > 0)
-			//		context.Message = string.Format("Zone(s) '{0}' are having new other rates with BED less than the BED of existing other rates", string.Join(",", zoneWithInvalidOtherRateBED));
-
-			//	if (zoneWithInvalidBED.Count > 0)
-			//		context.Message = string.Format("Zone(s) '{0}' are having other rates with BED less than the BED of their normal rates", string.Join(",", zoneWithInvalidBED));
-
-			//	if (zoneWithInvalidBED.Count > 0 || zoneWithInvalidOtherRateBED.Count > 0)
-			//		return false;
-			//}
+				if (zoneWithNormalBEDLessThanOtherRatesBED.Count > 0)
+                {
+                    context.Message = string.Format("Zone(s) '{0}' are having new normal rate with BED less than the BED of existing other rates", string.Join(",", zoneWithNormalBEDLessThanOtherRatesBED));
+                    return false;
+                }
+			}
 			return true;
 		}
 		public override string GetMessage(IRuleTarget target)
