5026ef7e40278fbe51c2e02c3d9a13df036232cc
Ali Atoui <VANRISE\ali.atoui>
2018-10-17 06:18:54 +0000

    Arranging AnalyticConfigurationManager
    git-tfs-id: [http://developmentvlan:8585/tfs/vanrise.collection]$/;C50237

--- Vanrise/Code/Vanrise.Analytic.Business/AnalyticConfigurationManager.cs
+++ Vanrise/Code/Vanrise.Analytic.Business/AnalyticConfigurationManager.cs
@@ -12,34 +12,42 @@ namespace Vanrise.Analytic.Business
         {
             return _templateConfigManager.GetExtensionConfigurations<HistorySearchSetting>(HistorySearchSetting.EXTENSION_TYPE);
         }
+
         public IEnumerable<WidgetDefinitionSetting> GetWidgetsTemplateConfigs()
         {
             return _templateConfigManager.GetExtensionConfigurations<WidgetDefinitionSetting>(WidgetDefinitionSetting.EXTENSION_TYPE);
         }
+
         public IEnumerable<RealTimeSearchSetting> GetRealTimeReportSettingsTemplateConfigs()
         {
             return _templateConfigManager.GetExtensionConfigurations<RealTimeSearchSetting>(RealTimeSearchSetting.EXTENSION_TYPE);
         }
+
         public IEnumerable<RealTimeWidgetSetting> GetRealTimeWidgetsTemplateConfigs()
         {
             return _templateConfigManager.GetExtensionConfigurations<RealTimeWidgetSetting>(RealTimeWidgetSetting.EXTENSION_TYPE);
         }
+
         public IEnumerable<MeasureStyleRuleTemplate> GetMeasureStyleRuleTemplateConfigs()
         {
             return _templateConfigManager.GetExtensionConfigurations<MeasureStyleRuleTemplate>(MeasureStyleRuleTemplate.EXTENSION_TYPE);
         }
+
         public IEnumerable<AnalyticItemActionTemplate> GetAnalyticItemActionsTemplateConfigs()
         {
             return _templateConfigManager.GetExtensionConfigurations<AnalyticItemActionTemplate>(AnalyticItemActionTemplate.EXTENSION_TYPE);
         }
+
         public IEnumerable<AnalyticDataProviderConfig> GetAnalyticDataProviderConfigs()
         {
             return _templateConfigManager.GetExtensionConfigurations<AnalyticDataProviderConfig>(AnalyticDataProviderConfig.EXTENSION_TYPE);
         }
+
         public IEnumerable<VRRestAPIAnalyticQueryInterceptorConfig> GetVRRestAPIAnalyticQueryInterceptorConfigs()
         {
             return _templateConfigManager.GetExtensionConfigurations<VRRestAPIAnalyticQueryInterceptorConfig>(VRRestAPIAnalyticQueryInterceptorConfig.EXTENSION_TYPE);
         }
+
         public IEnumerable<DRSearchPageSubviewDefinitionSettingsConfig> GetDRSearchPageSubviewDefinitionSettingsConfigs()
         {
             return _templateConfigManager.GetExtensionConfigurations<DRSearchPageSubviewDefinitionSettingsConfig>(DRSearchPageSubviewDefinitionSettingsConfig.EXTENSION_TYPE);
@@ -49,10 +57,12 @@ namespace Vanrise.Analytic.Business
         {
             return _templateConfigManager.GetExtensionConfigurations<MeasureExternalSourceSettingConfig>(MeasureExternalSourceSettingConfig.EXTENSION_TYPE);
         }
+
         public IEnumerable<DimensionMappingRuleSettingConfig> GetDimensionMappingRuleSettingConfigs()
         {
             return _templateConfigManager.GetExtensionConfigurations<DimensionMappingRuleSettingConfig>(DimensionMappingRuleSettingConfig.EXTENSION_TYPE);
         }
+
         public IEnumerable<MeasureMappingRuleSettingConfig> GetMeasureMappingRuleSettingConfigs()
         {
             return _templateConfigManager.GetExtensionConfigurations<MeasureMappingRuleSettingConfig>(MeasureMappingRuleSettingConfig.EXTENSION_TYPE);
