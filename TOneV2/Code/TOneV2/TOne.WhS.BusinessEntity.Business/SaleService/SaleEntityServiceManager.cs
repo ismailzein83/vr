0fac47965bcd3f8553f59226cb5a2b04aa60c14c
Rodi Hassan <VANRISE\rodi.hassan>
2018-10-17 14:08:55 +0000

    fixing bug in getting zones routing product when customer is deleted (SAMA)
    git-tfs-id: [http://developmentvlan:8585/tfs/vanrise.collection]$/;C50266

--- TOneV2/Code/TOneV2/TOne.WhS.BusinessEntity.Business/SaleService/SaleEntityServiceManager.cs
+++ TOneV2/Code/TOneV2/TOne.WhS.BusinessEntity.Business/SaleService/SaleEntityServiceManager.cs
@@ -1,138 +1,143 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using TOne.WhS.BusinessEntity.Data;
 using TOne.WhS.BusinessEntity.Entities;
 using Vanrise.Common;
 using Vanrise.Common.Business;
-using Vanrise.Entities;
 
 namespace TOne.WhS.BusinessEntity.Business
 {
-    public class SaleEntityServiceManager
-    {
-        public IEnumerable<SaleEntityDefaultService> GetDefaultServicesEffectiveAfter(SalePriceListOwnerType ownerType, int ownerId, DateTime minimumDate)
-        {
-            var dataManager = BEDataManagerFactory.GetDataManager<ISaleEntityServiceDataManager>();
-            return dataManager.GetDefaultServicesEffectiveAfter(ownerType, ownerId, minimumDate);
-        }
+	public class SaleEntityServiceManager
+	{
+		public IEnumerable<SaleEntityDefaultService> GetDefaultServicesEffectiveAfter(SalePriceListOwnerType ownerType, int ownerId, DateTime minimumDate)
+		{
+			var dataManager = BEDataManagerFactory.GetDataManager<ISaleEntityServiceDataManager>();
+			return dataManager.GetDefaultServicesEffectiveAfter(ownerType, ownerId, minimumDate);
+		}
 
-        public IEnumerable<SaleEntityZoneService> GetZoneServicesEffectiveAfter(SalePriceListOwnerType ownerType, int ownerId, DateTime minimumDate)
-        {
-            var dataManager = BEDataManagerFactory.GetDataManager<ISaleEntityServiceDataManager>();
-            return dataManager.GetZoneServicesEffectiveAfter(ownerType, ownerId, minimumDate);
-        }
+		public IEnumerable<SaleEntityZoneService> GetZoneServicesEffectiveAfter(SalePriceListOwnerType ownerType, int ownerId, DateTime minimumDate)
+		{
+			var dataManager = BEDataManagerFactory.GetDataManager<ISaleEntityServiceDataManager>();
+			return dataManager.GetZoneServicesEffectiveAfter(ownerType, ownerId, minimumDate);
+		}
 
-        public Vanrise.Entities.IDataRetrievalResult<SaleEntityZoneServiceDetail> GetFilteredSaleEntityZoneServices(Vanrise.Entities.DataRetrievalInput<SaleEntityZoneServiceQuery> input)
-        {
-            return BigDataManager.Instance.RetrieveData(input, new SaleEntityZoneServiceRequestHandler());
-        }
+		public Vanrise.Entities.IDataRetrievalResult<SaleEntityZoneServiceDetail> GetFilteredSaleEntityZoneServices(Vanrise.Entities.DataRetrievalInput<SaleEntityZoneServiceQuery> input)
+		{
+			return BigDataManager.Instance.RetrieveData(input, new SaleEntityZoneServiceRequestHandler());
+		}
 
-        public IEnumerable<SaleEntityZoneService> GetSaleZonesServicesEffectiveAfter(int sellingNumberPlanId, DateTime effectiveOn)
-        {
+		public IEnumerable<SaleEntityZoneService> GetSaleZonesServicesEffectiveAfter(int sellingNumberPlanId, DateTime effectiveOn)
+		{
 
-            ISaleEntityServiceDataManager dataManager = BEDataManagerFactory.GetDataManager<ISaleEntityServiceDataManager>();
-            return dataManager.GetSaleZonesServicesEffectiveAfter(sellingNumberPlanId, effectiveOn);
-        }
+			ISaleEntityServiceDataManager dataManager = BEDataManagerFactory.GetDataManager<ISaleEntityServiceDataManager>();
+			return dataManager.GetSaleZonesServicesEffectiveAfter(sellingNumberPlanId, effectiveOn);
+		}
 
-        public IEnumerable<SaleZoneRoutingProduct> GetSaleZonesRoutingProductsEffectiveAfter(int sellingNumberPlanId, DateTime effectiveOn)
-        {
+		public IEnumerable<SaleZoneRoutingProduct> GetSaleZonesRoutingProductsEffectiveAfter(int sellingNumberPlanId, DateTime effectiveOn)
+		{
+			ISaleEntityServiceDataManager dataManager = BEDataManagerFactory.GetDataManager<ISaleEntityServiceDataManager>();
+			var allSaleZoneRoutingProducts = dataManager.GetSaleZoneRoutingProductsEffectiveAfter(sellingNumberPlanId, effectiveOn);
+			var filteredSaleZoneRoutingProducts = new List<SaleZoneRoutingProduct>();
+			CarrierAccountManager carrierAccountManager = new CarrierAccountManager();
+			foreach (var zoneRoutingProduct in allSaleZoneRoutingProducts)
+			{
+				if (!(zoneRoutingProduct.OwnerType == SalePriceListOwnerType.Customer && carrierAccountManager.IsCarrierAccountDeleted(zoneRoutingProduct.OwnerId)))
+					filteredSaleZoneRoutingProducts.Add(zoneRoutingProduct);
+			}
 
-            ISaleEntityServiceDataManager dataManager = BEDataManagerFactory.GetDataManager<ISaleEntityServiceDataManager>();
-            return dataManager.GetSaleZoneRoutingProductsEffectiveAfter(sellingNumberPlanId, effectiveOn);
-        }
+			return filteredSaleZoneRoutingProducts;
+		}
 
-        public long ReserveIdRange(int numberOfIds)
-        {
-            long startingId;
-            IDManager.Instance.ReserveIDRange(this.GetSaleEntityServiceType(), numberOfIds, out startingId);
-            return startingId;
-        }
+		public long ReserveIdRange(int numberOfIds)
+		{
+			long startingId;
+			IDManager.Instance.ReserveIDRange(this.GetSaleEntityServiceType(), numberOfIds, out startingId);
+			return startingId;
+		}
 
-        public int GetSaleEntityServiceTypeId()
-        {
-            return Vanrise.Common.Business.TypeManager.Instance.GetTypeId(this.GetSaleEntityServiceType());
-        }
+		public int GetSaleEntityServiceTypeId()
+		{
+			return Vanrise.Common.Business.TypeManager.Instance.GetTypeId(this.GetSaleEntityServiceType());
+		}
 
-        public Type GetSaleEntityServiceType()
-        {
-            return this.GetType();
-        }
+		public Type GetSaleEntityServiceType()
+		{
+			return this.GetType();
+		}
 
-        
-        private class SaleEntityZoneServiceRequestHandler : BigDataRequestHandler<SaleEntityZoneServiceQuery, SaleEntityZoneServiceDetail, SaleEntityZoneServiceDetail>
-        {
-            public override SaleEntityZoneServiceDetail EntityDetailMapper(SaleEntityZoneServiceDetail entity)
-            {
-               // SaleEntityServiceManager manager = new SaleEntityServiceManager();
-                return entity;
-            }
 
-            public override IEnumerable<SaleEntityZoneServiceDetail> RetrieveAllData(Vanrise.Entities.DataRetrievalInput<SaleEntityZoneServiceQuery> input)
-            {
-                IEnumerable<SaleZone> saleZones = new SaleZoneManager().GetSaleZonesByOwner(input.Query.OwnerType,input.Query.OwnerId , input.Query.SellingNumberPlanId,input.Query.EffectiveOn, false);
-                List<SaleEntityZoneServiceDetail> saleServiceDetails = new List<SaleEntityZoneServiceDetail>();
+		private class SaleEntityZoneServiceRequestHandler : BigDataRequestHandler<SaleEntityZoneServiceQuery, SaleEntityZoneServiceDetail, SaleEntityZoneServiceDetail>
+		{
+			public override SaleEntityZoneServiceDetail EntityDetailMapper(SaleEntityZoneServiceDetail entity)
+			{
+				// SaleEntityServiceManager manager = new SaleEntityServiceManager();
+				return entity;
+			}
 
-                var serviceLocator = new SaleEntityServiceLocator(new  SaleEntityServiceReadWithCache(input.Query.EffectiveOn));
+			public override IEnumerable<SaleEntityZoneServiceDetail> RetrieveAllData(Vanrise.Entities.DataRetrievalInput<SaleEntityZoneServiceQuery> input)
+			{
+				IEnumerable<SaleZone> saleZones = new SaleZoneManager().GetSaleZonesByOwner(input.Query.OwnerType, input.Query.OwnerId, input.Query.SellingNumberPlanId, input.Query.EffectiveOn, false);
+				List<SaleEntityZoneServiceDetail> saleServiceDetails = new List<SaleEntityZoneServiceDetail>();
 
-               
-                if (saleZones != null)
-                {
-                    var filteredSaleZone = saleZones.FindAllRecords(sz => input.Query.ZonesIds == null || input.Query.ZonesIds.Contains(sz.SaleZoneId));
-                    if (input.Query.OwnerType == SalePriceListOwnerType.SellingProduct)
-                    {
-                        foreach (SaleZone saleZone in filteredSaleZone)
-                        {
-                            SaleEntityService service = serviceLocator.GetSellingProductZoneService(input.Query.OwnerId, saleZone.SaleZoneId);
-                            if (service != null)
-                            {
-                                SaleEntityZoneServiceDetail saleEntityZoneServiceDetail = SaleEntityZoneServiceDetailMapper(service, service.Source != SaleEntityServiceSource.ProductZone, saleZone.SaleZoneId);
-                                saleServiceDetails.Add(saleEntityZoneServiceDetail);
-                            }
+				var serviceLocator = new SaleEntityServiceLocator(new SaleEntityServiceReadWithCache(input.Query.EffectiveOn));
 
-                        }
-                    }
-                    else
-                    {
-                        int? sellingProductId = null;
 
-                        var customerSellingProductManager = new CustomerSellingProductManager();
-                        sellingProductId = customerSellingProductManager.GetEffectiveSellingProductId(input.Query.OwnerId, input.Query.EffectiveOn, false);
-                        if (!sellingProductId.HasValue)
-                            return saleServiceDetails;
-                        foreach (SaleZone saleZone in filteredSaleZone)
-                        {
-                            SaleEntityService service = serviceLocator.GetCustomerZoneService(input.Query.OwnerId, sellingProductId.Value, saleZone.SaleZoneId);
-                            if (service != null)
-                            {
-                                SaleEntityZoneServiceDetail saleEntityZoneServiceDetail = SaleEntityZoneServiceDetailMapper(service, service.Source != SaleEntityServiceSource.CustomerZone, saleZone.SaleZoneId);
-                                saleServiceDetails.Add(saleEntityZoneServiceDetail);
-                          
-                            }
-                        }
-                    }
-                }
-                return saleServiceDetails;
-            }
+				if (saleZones != null)
+				{
+					var filteredSaleZone = saleZones.FindAllRecords(sz => input.Query.ZonesIds == null || input.Query.ZonesIds.Contains(sz.SaleZoneId));
+					if (input.Query.OwnerType == SalePriceListOwnerType.SellingProduct)
+					{
+						foreach (SaleZone saleZone in filteredSaleZone)
+						{
+							SaleEntityService service = serviceLocator.GetSellingProductZoneService(input.Query.OwnerId, saleZone.SaleZoneId);
+							if (service != null)
+							{
+								SaleEntityZoneServiceDetail saleEntityZoneServiceDetail = SaleEntityZoneServiceDetailMapper(service, service.Source != SaleEntityServiceSource.ProductZone, saleZone.SaleZoneId);
+								saleServiceDetails.Add(saleEntityZoneServiceDetail);
+							}
 
-            private SaleEntityZoneServiceDetail SaleEntityZoneServiceDetailMapper(SaleEntityService saleEntityZoneService, bool IsServiceInherited, long ZoneId)
-            {
-                SaleEntityZoneServiceDetail detail = new SaleEntityZoneServiceDetail()
-                {
-                    Entity = saleEntityZoneService
-                };
-                SaleZone saleZone = new SaleZoneManager().GetSaleZone(ZoneId);
-                if (saleZone == null)
-                    throw new NullReferenceException();
+						}
+					}
+					else
+					{
+						int? sellingProductId = null;
 
-                detail.ZoneName = saleZone.Name;
-                detail.Services = saleEntityZoneService.Services.Select(x => x.ServiceId).ToList();
-                detail.IsServiceInherited = IsServiceInherited;
-                return detail;
-            }
-        }
+						var customerSellingProductManager = new CustomerSellingProductManager();
+						sellingProductId = customerSellingProductManager.GetEffectiveSellingProductId(input.Query.OwnerId, input.Query.EffectiveOn, false);
+						if (!sellingProductId.HasValue)
+							return saleServiceDetails;
+						foreach (SaleZone saleZone in filteredSaleZone)
+						{
+							SaleEntityService service = serviceLocator.GetCustomerZoneService(input.Query.OwnerId, sellingProductId.Value, saleZone.SaleZoneId);
+							if (service != null)
+							{
+								SaleEntityZoneServiceDetail saleEntityZoneServiceDetail = SaleEntityZoneServiceDetailMapper(service, service.Source != SaleEntityServiceSource.CustomerZone, saleZone.SaleZoneId);
+								saleServiceDetails.Add(saleEntityZoneServiceDetail);
 
-    }
+							}
+						}
+					}
+				}
+				return saleServiceDetails;
+			}
+
+			private SaleEntityZoneServiceDetail SaleEntityZoneServiceDetailMapper(SaleEntityService saleEntityZoneService, bool IsServiceInherited, long ZoneId)
+			{
+				SaleEntityZoneServiceDetail detail = new SaleEntityZoneServiceDetail()
+				{
+					Entity = saleEntityZoneService
+				};
+				SaleZone saleZone = new SaleZoneManager().GetSaleZone(ZoneId);
+				if (saleZone == null)
+					throw new NullReferenceException();
+
+				detail.ZoneName = saleZone.Name;
+				detail.Services = saleEntityZoneService.Services.Select(x => x.ServiceId).ToList();
+				detail.IsServiceInherited = IsServiceInherited;
+				return detail;
+			}
+		}
+
+	}
 }
