825767584dffacec0819ef3170924d1708c64a81
Ali Atoui <VANRISE\ali.atoui>
2018-10-17 06:51:43 +0000

    Adding DealDAProfCalcAlertRuleFilterDefinition and DealDAProfCalcAlertRuleFilter
    git-tfs-id: [http://developmentvlan:8585/tfs/vanrise.collection]$/;C50239

---
+++ TOneV2/Code/TOneV2/TOne.WhS.Deal.MainExtensions/DataAnalysis/ProfilingAndCalculation/DealDAProfCalcAlertRuleFilter.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using Vanrise.Analytic.Entities;
+using Vanrise.GenericData.Entities;
+
+namespace TOne.WhS.Deal.MainExtensions.DataAnalysis.ProfilingAndCalculation
+{
+    public enum DealDAProfCalcAlertRuleFilterType { Sale = 0, Cost = 1, SaleOrCost = 2 }
+
+    public class DealDAProfCalcAlertRuleFilter : DAProfCalcAlertRuleFilter
+    {
+        public override Guid ConfigId { get { return new Guid("E22E8134-2C77-4F8C-85CA-F8D26206D42C"); } }
+
+        public DealDAProfCalcAlertRuleFilterType DealAlertRuleFilterType { get; set; }
+
+        public override RecordFilterGroup GetRecordFilterGroup(IDAProfCalcGetRecordFilterGroupContext context)
+        {
+            switch (this.DealAlertRuleFilterType)
+            {
+                case DealDAProfCalcAlertRuleFilterType.Sale:
+                    RecordFilterGroup saleDealRecordFilterGroup = new RecordFilterGroup();
+                    saleDealRecordFilterGroup.Filters = new List<RecordFilter>();
+                    saleDealRecordFilterGroup.Filters.Add(GetSaleDealRecordFilter());
+                    return saleDealRecordFilterGroup;
+
+                case DealDAProfCalcAlertRuleFilterType.Cost:
+                    RecordFilterGroup costDealRecordFilterGroup = new RecordFilterGroup();
+                    costDealRecordFilterGroup.Filters = new List<RecordFilter>();
+                    costDealRecordFilterGroup.Filters.Add(GetCostDealRecordFilter());
+                    return costDealRecordFilterGroup;
+
+                case DealDAProfCalcAlertRuleFilterType.SaleOrCost:
+                    RecordFilterGroup saleOrCostDealRecordFilterGroup = new RecordFilterGroup();
+                    saleOrCostDealRecordFilterGroup.LogicalOperator = RecordQueryLogicalOperator.Or;
+                    saleOrCostDealRecordFilterGroup.Filters = new List<RecordFilter>();
+                    saleOrCostDealRecordFilterGroup.Filters.Add(GetSaleDealRecordFilter());
+                    saleOrCostDealRecordFilterGroup.Filters.Add(GetCostDealRecordFilter());
+                    return saleOrCostDealRecordFilterGroup;
+
+                default: throw new NotSupportedException(string.Format("DealAlertRuleFilterType '{0}'", this.DealAlertRuleFilterType));
+            }
+        }
+
+        public RecordFilter GetSaleDealRecordFilter()
+        {
+            RecordFilter saleDealRecordFilter = new NonEmptyRecordFilter();
+            saleDealRecordFilter.FieldName = "SaleDealId";
+            return saleDealRecordFilter;
+        }
+
+        public RecordFilter GetCostDealRecordFilter()
+        {
+            RecordFilter costDealRecordFilter = new NonEmptyRecordFilter();
+            costDealRecordFilter.FieldName = "CostDealId";
+            return costDealRecordFilter;
+        }
+    }
+}
---
+++ TOneV2/Code/TOneV2/TOne.WhS.Deal.MainExtensions/DataAnalysis/ProfilingAndCalculation/DealDAProfCalcAlertRuleFilterDefinition.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Vanrise.Analytic.Entities;
+
+namespace TOne.WhS.Deal.MainExtensions.DataAnalysis.ProfilingAndCalculation
+{
+    public class DealDAProfCalcAlertRuleFilterDefinition : DAProfCalcAlertRuleFilterDefinition
+    {
+        public override Guid ConfigId { get { return new Guid("9CA79A5E-D8F5-4927-96D3-0F7568F87265"); } }
+
+        public override string RuntimeEditor { get { return ""; } }
+    }
+}
