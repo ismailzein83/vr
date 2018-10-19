cd108081c732cb8b07fbf5f9f5f4054471a75003
Mortada Issa <VANRISE\mortada.issa>
2018-10-19 15:10:51 +0000

    Adding connectors to communicate with VR connection
    git-tfs-id: [http://developmentvlan:8585/tfs/vanrise.collection]$/;C50377

---
+++ SOM/Code/SOM.Main.Business/Connectors/HttpConnector.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Vanrise.Common.Business;
+using Vanrise.Entities;
+using Vanrise.Common;
+using Newtonsoft.Json;
+
+namespace SOM.Main.Business
+{
+    public abstract class HttpConnector
+    {
+        //TODO: expose Http method parameter to be used by BSCS as XML and JSON for Activara
+        //TODO: replace JSONCOnvert with Vanrise Serializer
+        //TODO: change the way we are building the request from BPM Extended to SOM
+
+        protected abstract Guid GetConnectionId();
+
+        private VRHttpConnection _vrHttpConnection;
+
+        static JsonSerializerSettings s_Settings = new JsonSerializerSettings
+        {
+            MissingMemberHandling = MissingMemberHandling.Ignore,
+            NullValueHandling = NullValueHandling.Ignore,
+            TypeNameHandling = TypeNameHandling.All
+        };
+
+        public HttpConnector()
+        {
+            if (this.GetConnectionId() == Guid.Empty)
+                throw new ArgumentException("ConnectionId is empty");
+
+            VRConnectionManager connectionManager = new VRConnectionManager();
+            var vrConnection = connectionManager.GetVRConnection(this.GetConnectionId());
+            this._vrHttpConnection = vrConnection.Settings.CastWithValidate<VRHttpConnection>("vrConnection.Settings", this.GetConnectionId());
+        }
+
+        public T Get<T>(string actionPath, Dictionary<string, string> parameters)
+        {
+            T response = default(T);
+
+            Action<VRHttpResponse> onResponseReceived = x => {
+                response = JsonConvert.DeserializeObject<T>(x.Response, s_Settings);
+            };
+
+            this._vrHttpConnection.TrySendRequest(actionPath, VRHttpMethod.Get, 
+                VRHttpMessageFormat.ApplicationJSON, parameters, null, null, onResponseReceived, true, null);
+
+            return response;
+        }
+
+        public Q POST<T, Q>(string actionPath, T data)
+        {
+            Q response = default(Q);
+
+            Action<VRHttpResponse> onResponseReceived = x => {
+                response = JsonConvert.DeserializeObject<Q>(x.Response, s_Settings);
+            };
+
+            string body = JsonConvert.SerializeObject(data, Formatting.None, s_Settings);
+
+            this._vrHttpConnection.TrySendRequest(actionPath, VRHttpMethod.Post,
+                VRHttpMessageFormat.ApplicationJSON, null, null, body, onResponseReceived, true, null);
+
+            return response;
+        }
+    }
+}
---
+++ SOM/Code/SOM.Main.Business/Connectors/InventoryConnector.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SOM.Main.Business.Connectors
+{
+    public class InventoryConnector : HttpConnector
+    {
+        protected override Guid GetConnectionId()
+        {
+            return new Guid("8D0E04EE-3917-4E3D-B0AB-EF56DA217745");
+        }
+    }
+}
