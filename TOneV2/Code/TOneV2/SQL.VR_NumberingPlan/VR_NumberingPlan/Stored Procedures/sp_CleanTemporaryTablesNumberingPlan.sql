eca532880c6e8d87913fe1a664c337a608d73200
Wissam Ajamy <VANRISE\wissam.ajamy>
2018-10-19 11:25:22 +0000

    Add missing SP
    git-tfs-id: [http://developmentvlan:8585/tfs/vanrise.collection]$/;C50353

---
+++ TOneV2/Code/TOneV2/SQL.VR_NumberingPlan/VR_NumberingPlan/Stored Procedures/sp_CleanTemporaryTablesNumberingPlan.sql
@@ -0,0 +1,28 @@
+﻿-- =============================================
+-- Author:		<Author,,Name>
+-- Create date: <Create Date,,>
+-- Description:	<Description,,>
+-- =============================================
+CREATE PROCEDURE [VR_NumberingPlan].sp_CleanTemporaryTablesNumberingPlan
+@ProcessInstanceId bigint
+AS
+BEGIN
+	delete  from [VR_NumberingPlan].CP_SaleCode_Changed
+	where ProcessInstanceID = @ProcessInstanceId
+
+	delete  from  [VR_NumberingPlan].CP_SaleCode_New
+	where ProcessInstanceID = @ProcessInstanceId
+	
+	delete  from  [VR_NumberingPlan].CP_SaleZone_Changed
+	where ProcessInstanceID = @ProcessInstanceId
+
+	delete  from  [VR_NumberingPlan].CP_SaleZone_New
+	where ProcessInstanceID = @ProcessInstanceId
+	
+	delete  from  [VR_NumberingPlan].SaleCode_Preview
+	where ProcessInstanceID = @ProcessInstanceId
+	
+	delete  from  [VR_NumberingPlan].SaleZone_Preview
+	where ProcessInstanceID = @ProcessInstanceId
+
+END
