63fb9de2d7842c763b8d52d848192f60493f11b0
Ali Atoui <VANRISE\ali.atoui>
2018-10-22 08:29:52 +0000

    Adding ConfigManager and Constants
    git-tfs-id: [http://developmentvlan:8585/tfs/vanrise.collection]$/;C50387

---
+++ Vanrise/Code/Vanrise.Integration.Business/ConfigManager.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using Vanrise.Common;
+using Vanrise.Common.Business;
+using Vanrise.Integration.Entities;
+
+namespace Vanrise.Integration.Business
+{
+    public class ConfigManager
+    {
+        public FileDataSourceDefinition GetFileDataSourceDefinition(Guid fileDataSourceDefinitionId)
+        {
+            FileDataSourceSettings fileDataSourceSettings = GetFileDataSourceSettings();
+            fileDataSourceSettings.FileDataSourceDefinitions.ThrowIfNull("fileDataSourceSettings.FileDataSourceDefinitions");
+            FileDataSourceDefinition fileDataSourceDefinition = fileDataSourceSettings.FileDataSourceDefinitions.FindRecord(itm => itm.FileDataSourceDefinitionId == fileDataSourceDefinitionId);
+            fileDataSourceDefinition.ThrowIfNull("fileDataSourceDefinition", fileDataSourceDefinitionId);
+
+            return fileDataSourceDefinition;
+        }
+
+        private FileDataSourceSettings GetFileDataSourceSettings()
+        {
+            DataSourceSettingData dataSourceSettingData = GetDataSourceSettingData();
+            dataSourceSettingData.FileDataSourceSettings.ThrowIfNull("dataSourceSettingData.FileDataSourceSettings");
+
+            return dataSourceSettingData.FileDataSourceSettings;
+        }
+
+        private DataSourceSettingData GetDataSourceSettingData()
+        {
+            SettingManager settingManager = new SettingManager();
+            DataSourceSettingData dataSourceSettingData = settingManager.GetSetting<DataSourceSettingData>(Constants.DataSourceSettings);
+            dataSourceSettingData.ThrowIfNull("dataSourceSettingData");
+
+            return dataSourceSettingData;
+        }
+    }
+}
---
+++ Vanrise/Code/Vanrise.Integration.Business/Constants.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Vanrise.Integration.Business
+{
+    public static class Constants
+    {
+        public const string DataSourceSettings = "VR_Integration_DataSourceSettings";
+    }
+}
