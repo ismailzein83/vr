70cf2d37a90cc1522778385e7f16353278ca0e89
Samer Haidar <VANRISE\samer.haidar>
2018-10-22 14:40:39 +0000

    Adding GeneratedScriptItem
    git-tfs-id: [http://developmentvlan:8585/tfs/vanrise.collection]$/;C50404

---
+++ Vanrise/Code/Vanrise.Entities/Tools/GeneratedScriptItem.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Vanrise.Entities.Tools
+{
+    public class GeneratedScriptItem
+    {
+        public List<GeneratedScriptItemTable> Tables { get; set; }
+    }
+
+    public class GeneratedScriptItemTable
+    {
+        public string ConnectionString { get; set; }
+        public string Schema { get; set; }
+        public string TableName { get; set; }
+        public List<GeneratedScriptItemTableColumn> InsertColumns { get; set; }
+        public List<GeneratedScriptItemTableColumn> UpdateColumns { get; set; }
+        public List<GeneratedScriptItemTableColumn> IdentifierColumns { get; set; }
+        public List<GeneratedScriptItemTableRow> DataRows { get; set; }
+
+    }
+    public class GeneratedScriptItemTableColumn
+    {
+        public string ColumnName { get; set; }
+    }
+
+    public class GeneratedScriptItemTableRow
+    {
+        public Dictionary<string, Object> FieldValues { get; set; }
+    }
+}
