01a85dd36cbe1a58c4ffb7b0cecb29d5d178ef3a
Wissam Ajamy <VANRISE\wissam.ajamy>
2018-10-23 13:02:57 +0000

    update structure
    git-tfs-id: [http://developmentvlan:8585/tfs/vanrise.collection]$/;C50434

--- Retail/Code/ISP/ISP/Tables/ISPEvent.sql
+++ Retail/Code/ISP/ISP/Tables/ISPEvent.sql
@@ -1,11 +1,15 @@
 ﻿CREATE TABLE [ISP].[ISPEvent] (
-    [StartDate]  DATETIME        NULL,
-    [EndDate]    DATETIME        NULL,
-    [Username]   NVARCHAR (255)  NULL,
-    [IP]         VARCHAR (50)    NULL,
-    [Service]    VARCHAR (255)   NULL,
-    [MacAddress] VARCHAR (50)    NULL,
-    [Download]   DECIMAL (24, 6) NULL,
-    [Upload]     DECIMAL (24, 6) NULL
+    [StartDate]   DATETIME        NULL,
+    [EndDate]     DATETIME        NULL,
+    [Username]    NVARCHAR (255)  NULL,
+    [IP]          VARCHAR (50)    NULL,
+    [Service]     VARCHAR (255)   NULL,
+    [MacAddress]  VARCHAR (50)    NULL,
+    [Download]    DECIMAL (24, 6) NULL,
+    [Upload]      DECIMAL (24, 6) NULL,
+    [Package]     VARCHAR (255)   NULL,
+    [AccountType] VARCHAR (50)    NULL
 );
+
+
 
