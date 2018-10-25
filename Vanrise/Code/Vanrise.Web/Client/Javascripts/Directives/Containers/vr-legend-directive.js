fbd774c1736793549f17144a5a4f8f6ecd750339
Ali Ballouk <VANRISE\ali.ballouk>
2018-10-25 12:51:01 +0000

    fix legend MainContent Position calculation in small height screen
    git-tfs-id: [http://developmentvlan:8585/tfs/vanrise.collection]$/;C50510

--- Vanrise/Code/Vanrise.Web/Client/Javascripts/Directives/Containers/vr-legend-directive.js
+++ Vanrise/Code/Vanrise.Web/Client/Javascripts/Directives/Containers/vr-legend-directive.js
@@ -1,5 +1,6 @@
 ﻿'use strict';
-app.directive('vrLegend', ['$compile', 'UtilsService', function ($compile, UtilsService) {
+app.directive( 'vrLegend', ['$compile', 'UtilsService', function ( $compile, UtilsService )
+{
     var directiveDefinitionObject = {
         restrict: 'E',
         scope: {
@@ -7,81 +8,95 @@ app.directive('vrLegend', ['$compile', 'UtilsService', function ($compile, Utils
             content: '@',
         },
         controllerAs: 'legendCtrl',
-        controller: function ($scope, $element, $attrs) {
-            $scope.$on("$destroy", function () {
-                $(window).unbind('scroll', hideLegendContent);
-                $(window).unbind('resize', hideLegendContent);
-            });
+        controller: function ( $scope, $element, $attrs )
+        {
+            $scope.$on( "$destroy", function ()
+            {
+                $( window ).unbind( 'scroll', hideLegendContent );
+                $( window ).unbind( 'resize', hideLegendContent );
+            } );
             var ctrl = this;
             var showContent = false;
             ctrl.id = UtilsService.guid();
-            ctrl.toogleLegendContent = function (evt) {
-                if (showContent == false) {
+            ctrl.toogleLegendContent = function ( evt )
+            {
+                if ( showContent == false )
+                {
                     showLegendContent()
                 }
-                else {
+                else
+                {
                     hideLegendContent();
                 }
             };
 
-            function showLegendContent() {
+            function showLegendContent()
+            {
                 calculatePosition();
-                $element.find('#MainContent').slideDown("slow", function () {
+                $element.find( '#MainContent' ).slideDown( "slow", function ()
+                {
                     showContent = true;
-                    $(document).bind('click', boundDocumentLengendOutside);
-                });
+                    $( document ).bind( 'click', boundDocumentLengendOutside );
+                } );
             }
 
-            function hideLegendContent() {
-                $element.find('#MainContent').slideUp("slow", function () {
+            function hideLegendContent()
+            {
+                $element.find( '#MainContent' ).slideUp( "slow", function ()
+                {
                     showContent = false;
-                    $(document).unbind('click', boundDocumentLengendOutside);
-                });
+                    $( document ).unbind( 'click', boundDocumentLengendOutside );
+                } );
             }
 
             //function hideAllother
 
 
-            function boundDocumentLengendOutside(e) {
-                if (!$(e.target).parents().hasClass('vr-legend-' + ctrl.id) || $(e.target).hasClass('legend-header-container-' + ctrl.id))
+            function boundDocumentLengendOutside( e )
+            {
+                if ( !$( e.target ).parents().hasClass( 'vr-legend-' + ctrl.id ) || $( e.target ).hasClass( 'legend-header-container-' + ctrl.id ) )
                     hideLegendContent();
             }
-            $scope.$on('hide-all-menu', function (event, args) {
+            $scope.$on( 'hide-all-menu', function ( event, args )
+            {
                 hideLegendContent();
-            });
-            $scope.$on('hide-all-select', function (event, args) {
+            } );
+            $scope.$on( 'hide-all-select', function ( event, args )
+            {
                 hideLegendContent();
-            });
-            $(window).on('scroll', hideLegendContent);
-            $(window).on('resize', hideLegendContent);
+            } );
+            $( window ).on( 'scroll', hideLegendContent );
+            $( window ).on( 'resize', hideLegendContent );
             ctrl.hideLegend = hideLegendContent;
-            function calculatePosition() {
-                var self = $element.find('#LegendHeader').parent();
-                var selfHeight = $(self).height();
-                var selfOffset = $(self).offset();
-                var legend = $element.find('#MainContent');
-                var legendHeigth = $(legend).height();
+            function calculatePosition()
+            {
+                var self = $element.find( '#LegendHeader' ).parent();
+                var selfHeight = $( self ).height();
+                var selfOffset = $( self ).offset();
+                var legend = $element.find( '#MainContent' );
+                var legendHeigth = $( legend ).height();
                 var top = 0;
-                var basetop = selfOffset.top - $(window).scrollTop() + selfHeight - 10;
-                var baseleft = selfOffset.left - $(window).scrollLeft();
-                top = (innerHeight - basetop < legendHeigth ) ? basetop - (legendHeigth) - selfHeight : basetop;
-                $(legend).css({position: 'fixed',top: top ,left: baseleft, width: self.width()});
+                var basetop = selfOffset.top - $( window ).scrollTop() + selfHeight - 10;
+                var baseleft = selfOffset.left - $( window ).scrollLeft();
+                top = ( ( ( innerHeight - basetop ) < legendHeigth ) && basetop > ( innerHeight - basetop ) ) ? basetop - ( legendHeigth ) - selfHeight : basetop;
+                $( legend ).css( { position: 'fixed', top: top, left: baseleft, width: self.width() } );
             };
 
-            var htmlContent = $compile(ctrl.content)($scope.$new());
-            $element.find('#LengendContent').html(htmlContent);
+            var htmlContent = $compile( ctrl.content )( $scope.$new() );
+            $element.find( '#LengendContent' ).html( htmlContent );
         },
         bindToController: true,
-        template: function (element, attrs) {
+        template: function ( element, attrs )
+        {
             var template = '<div class="vr-legend-{{legendCtrl.id}}">'
-                              + '<div class="legend-header-container-{{legendCtrl.id}}"><label  ng-click="legendCtrl.toogleLegendContent($event)" id="LegendHeader" class="legend-header">{{legendCtrl.header}}</label></div>'
-                              + '<div  id="MainContent" class="main-content">'
-                                  + '<div class="legend-container">'
-                                    + '<div class="legend-close" ng-click="legendCtrl.hideLegend($event)">X</div>'
-                                    + '<div id="LengendContent"></div>'
-                                  + '</div>'
-                               + '</div>'
-                         + '</div>';
+                + '<div class="legend-header-container-{{legendCtrl.id}}"><label  ng-click="legendCtrl.toogleLegendContent($event)" id="LegendHeader" class="legend-header">{{legendCtrl.header}}</label></div>'
+                + '<div  id="MainContent" class="main-content">'
+                + '<div class="legend-container">'
+                + '<div class="legend-close" ng-click="legendCtrl.hideLegend($event)">X</div>'
+                + '<div id="LengendContent"></div>'
+                + '</div>'
+                + '</div>'
+                + '</div>';
 
             return template;
         }
@@ -89,7 +104,7 @@ app.directive('vrLegend', ['$compile', 'UtilsService', function ($compile, Utils
     };
 
     return directiveDefinitionObject;
-}]);
+}] );
 
 
 
