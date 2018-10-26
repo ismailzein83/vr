40b18198e918f09a2389f0dcec732c029eb29810
Ali Atoui <VANRISE\ali.atoui>
2018-10-26 14:30:38 +0000

    Adding DealReprocessFilterDefinition Directives Editing Deals.PostDep.sql
    git-tfs-id: [http://developmentvlan:8585/tfs/vanrise.collection]$/;C50559

---
+++ TOneV2/Code/TOneV2/TOne.WhS.Deal.Web/WhS_Deal/Directives/Extensions/ReprocessFilterDefinition/vr-whs-deal-reprocessfilterdefinition-definition.js
@@ -0,0 +1,60 @@
+﻿(function (app) {
+
+    'use strict';
+
+    DealReprocessFilterDefinitionDefinition.$inject = ['UtilsService'];
+
+    function DealReprocessFilterDefinitionDefinition(UtilsService) {
+        return {
+            restrict: 'E',
+            scope: {
+                onReady: '='
+            },
+            controller: function ($scope, $element, $attrs) {
+                var ctrl = this;
+                var ctor = new DealReprocessFilterDefinitionDefinitionCtor($scope, ctrl);
+                ctor.initializeController();
+            },
+            controllerAs: 'ctrl',
+            bindToController: true,
+            templateUrl: '/Client/Modules/WhS_Deal/Directives/Extensions/ReprocessFilterDefinition/Templates/DealReprocessFilterDefinitionTemplate.html'
+        };
+
+        function DealReprocessFilterDefinitionDefinitionCtor($scope, ctrl) {
+            this.initializeController = initializeController;
+
+            function initializeController() {
+                defineAPI();
+            }
+
+            function defineAPI() {
+                var api = {};
+
+                api.load = function (payload) {
+
+                    if (payload != undefined) {
+                        var context = payload.context;
+
+                        if (payload.filterDefinition == undefined) {
+                            context.onFieldAdded("Customer");
+                            context.onFieldAdded("Supplier");
+                            context.onFieldAdded("Type");
+                        }
+                    }
+                };
+
+                api.getData = function () {
+                    return {
+                        $type: 'TOne.WhS.Deal.MainExtensions.ReprocessFilterDefinition.DealReprocessFilterDefinition, TOne.WhS.Deal.MainExtensions'
+                    };
+                };
+
+                if (ctrl.onReady != undefined && typeof (ctrl.onReady) == 'function') {
+                    ctrl.onReady(api);
+                }
+            }
+        }
+    }
+
+    app.directive('vrWhsDealReprocessfilterdefinitionDefinition', DealReprocessFilterDefinitionDefinition);
+})(app);
