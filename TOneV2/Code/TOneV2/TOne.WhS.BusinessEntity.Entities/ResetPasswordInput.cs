772535bbe0943b72a7cbefa523d13903f8196a86
yara.bousalman <yara.bousalman@vanrise.tfs.local>
2018-10-29 13:05:04 +0000

    Adding ResetPasswordInput.cs to TOne.WhS.BusinessEntity.Entities project
    git-tfs-id: [http://developmentvlan:8585/tfs/vanrise.collection]$/;C50579

---
+++ TOneV2/Code/TOneV2/TOne.WhS.BusinessEntity.Entities/ResetPasswordInput.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TOne.WhS.BusinessEntity.Entities
+{
+    public class ResetPasswordInput
+    {
+        public int CarrierProfileId { get; set; }
+        public string Password { get; set; }
+        public int UserId { get; set; }
+    }
+}
