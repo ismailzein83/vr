e3e11aee68683889eb733dbf36a1674c5f4e22b2
Ali Atoui <VANRISE\ali.atoui>
2018-10-29 13:06:33 +0000

    Adding RouteSucceededDataManager
    git-tfs-id: [http://developmentvlan:8585/tfs/vanrise.collection]$/;C50580

---
+++ TOneV2/Code/TOneV2/TOne.WhS.RouteSync.Huawei.SQL/RouteSucceededDataManager.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TOne.WhS.RouteSync.Huawei.Data;
+using TOne.WhS.RouteSync.Huawei.Entities;
+using Vanrise.Data.SQL;
+
+namespace TOne.WhS.RouteSync.Huawei.SQL
+{
+    public class RouteSucceededDataManager //: BaseSQLDataManager, IRouteSucceededDataManager
+    {
+        //public string SwitchId { get; set; }
+
+        //public void SaveRoutesSucceededToDB(Dictionary<int, List<HuaweiRouteWithCommands>> routesWithCommandsByRSSN)
+        //{
+        //    throw new NotImplementedException();
+        //}
+    }
+}
