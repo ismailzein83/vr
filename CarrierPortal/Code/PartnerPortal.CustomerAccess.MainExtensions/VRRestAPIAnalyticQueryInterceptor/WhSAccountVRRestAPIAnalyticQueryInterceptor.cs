8157df20cc0f44b4e09877399c8a7e727e5f757d
yara.bousalman <yara.bousalman@vanrise.tfs.local>
2018-10-30 07:52:43 +0000

    Adding WhSAccountVRRestAPIAnalyticQueryInterceptor to PartnerPortal.CustomerAccess.MainExtensions
    git-tfs-id: [http://developmentvlan:8585/tfs/vanrise.collection]$/;C50601

---
+++ CarrierPortal/Code/PartnerPortal.CustomerAccess.MainExtensions/VRRestAPIAnalyticQueryInterceptor/WhSAccountVRRestAPIAnalyticQueryInterceptor.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Vanrise.Analytic.Entities;
+
+namespace PartnerPortal.CustomerAccess.MainExtensions.VRRestAPIAnalyticQueryInterceptor
+{
+    public class WhSAccountVRRestAPIAnalyticQueryInterceptor : Vanrise.Analytic.Entities.VRRestAPIAnalyticQueryInterceptor
+    {
+        public override Guid ConfigId { get { return new Guid("2cf9eade-97ab-467f-b24b-35345324930a"); } }
+
+        public override void PrepareQuery(IVRRestAPIAnalyticQueryInterceptorContext context)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
