1c9957db16c7ffff9538205cc6545c89894a2826
Samer Haidar <VANRISE\samer.haidar>
2018-10-31 09:47:26 +0000

    Adding SpecificDaysTimePeriod
    git-tfs-id: [http://developmentvlan:8585/tfs/vanrise.collection]$/;C50633

---
+++ Vanrise/Code/Vanrise.Common.MainExtensions/VRTimePeriod/SpecificDaysTimePeriod.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Vanrise.Entities;
+
+namespace Vanrise.Common.MainExtensions
+{
+    public class SpecificDaysTimePeriod : VRTimePeriod
+    {
+        public override Guid ConfigId { get { return new Guid("FB9B7430-6FE8-418C-98EB-49730B562DE8"); } }
+        public int DaysBack { get; set; }
+        public int NumberOfDays { get; set; }
+        public override void GetTimePeriod(IVRTimePeriodContext context)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
