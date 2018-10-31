cef71106b5afac728a16ae9adc141d27a7519c54
Mohammad Yahfoufi <VANRISE\mohammad.yahfoufi>
2018-10-31 12:25:22 +0000

    Adding MaxAggregate file
    git-tfs-id: [http://developmentvlan:8585/tfs/vanrise.collection]$/;C50641

---
+++ Vanrise/Code/Vanrise.Analytic.MainExtensions/DARecordAggregates/MaxAggregate.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using Vanrise.Analytic.Entities;
+
+namespace Vanrise.Analytic.MainExtensions.DARecordAggregates
+{
+    public class MaxAggregate : DARecordAggregate
+    {
+        public override Guid ConfigId { get { return  new Guid("F50143C9-62B4-4A6F-8C94-B9EED7970DCF"); } }
+
+        public string MaxFieldName { get; set; }
+
+        public override DARecordAggregateState CreateState(IDARecordAggregateCreateStateContext context)
+        {
+            return new MaxAggregateState();
+        }
+        public override void Evaluate(IDARecordAggregateEvaluationContext context)
+        {
+            if (context.Record == null)
+                //return;
+                throw new ArgumentNullException("context.Record");
+          //  var fieldValue = context.Record.GetFieldValue(MaxFieldName);
+            var fieldValue = Common.Utilities.GetPropValueReader(this.MaxFieldName).GetPropertyValue(context.Record);
+            (context.State as MaxAggregateState).Max = Math.Max(fieldValue, (context.State as MaxAggregateState).Max) ;
+        }
+
+        public override dynamic GetResult(IDARecordAggregateGetResultContext context)
+        {
+            return (context.State as MaxAggregateState).Max;
+        }
+
+        public override void UpdateExistingFromNew(IDARecordAggregateUpdateExistingFromNewContext context)
+        {
+            var ExistingMax = (context.ExistingState as MaxAggregateState).Max;
+            var CurrentMax = (context.NewState as MaxAggregateState).Max;
+
+            (context.ExistingState as MaxAggregateState).Max = Math.Max(ExistingMax, CurrentMax);
+        } 
+    }
+
+    public class MaxAggregateState : DARecordAggregateState
+    {
+        public dynamic Max { get; set; }
+    }
+}
