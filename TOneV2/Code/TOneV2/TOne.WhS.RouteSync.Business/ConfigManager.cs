ce70538f819c8372daa54807b1d70f66b8acc5af
Ali Atoui <VANRISE\ali.atoui>
2018-10-31 15:42:55 +0000

    Arranging ConfigManager
    git-tfs-id: [http://developmentvlan:8585/tfs/vanrise.collection]$/;C50651

--- TOneV2/Code/TOneV2/TOne.WhS.RouteSync.Business/ConfigManager.cs
+++ TOneV2/Code/TOneV2/TOne.WhS.RouteSync.Business/ConfigManager.cs
@@ -5,93 +5,101 @@ using Vanrise.Common;
 
 namespace TOne.WhS.RouteSync.Business
 {
-	public class ConfigManager
-	{
-		public int GetRouteSyncProcessRouteBatchSize()
-		{
-			RouteSyncProcess routeSyncProcess = GetRouteSyncProcessSettings();
-			return routeSyncProcess.RouteBatchSize;
-		}
+    public class ConfigManager
+    {
+        #region Public Methods
 
-		public int GetRouteSyncProcessDifferentialRoutesPerTransaction()
-		{
-			RouteSyncProcess routeSyncProcess = GetRouteSyncProcessSettings();
-			return routeSyncProcess.DifferentialRoutesPerTransaction;
-		}
+        public int GetRouteSyncProcessRouteBatchSize()
+        {
+            RouteSyncProcess routeSyncProcess = GetRouteSyncProcessSettings();
+            return routeSyncProcess.RouteBatchSize;
+        }
 
-		public int GetRouteSyncProcessIndexesCommandTimeoutInSeconds()
-		{
-			RouteSyncProcess routeSyncProcess = GetRouteSyncProcessSettings();
-			return routeSyncProcess.IndexCommandTimeoutInMinutes * 60;
-		}
+        public int GetRouteSyncProcessDifferentialRoutesPerTransaction()
+        {
+            RouteSyncProcess routeSyncProcess = GetRouteSyncProcessSettings();
+            return routeSyncProcess.DifferentialRoutesPerTransaction;
+        }
 
-		public bool GetRouteSyncProcessExecuteFullRouteSyncWhenPartialNotSupported()
-		{
-			RouteSyncProcess routeSyncProcess = GetRouteSyncProcessSettings();
-			return routeSyncProcess.ExecuteFullRouteSyncWhenPartialNotSupported;
-		}
+        public int GetRouteSyncProcessIndexesCommandTimeoutInSeconds()
+        {
+            RouteSyncProcess routeSyncProcess = GetRouteSyncProcessSettings();
+            return routeSyncProcess.IndexCommandTimeoutInMinutes * 60;
+        }
 
-		public SwitchInfoGetter GetSwitchInfoGetter()
-		{
-			Vanrise.Common.Business.SettingManager settingManager = new Vanrise.Common.Business.SettingManager();
-			return settingManager.GetCachedOrCreate("WhS_RouteSync_ConfigurationManager_GetSwitchInfoGetter",
-				() =>
-				{
-					var routeSyncTechnicalSettings = GetRouteSyncTechnicalSettings(settingManager);
-					if (routeSyncTechnicalSettings.SwitchInfoGetter == null)
-						throw new NullReferenceException("routeSyncTechnicalSettings.SwitchInfoGetter");
-					return routeSyncTechnicalSettings.SwitchInfoGetter;
-				});
-		}
+        public bool GetRouteSyncProcessExecuteFullRouteSyncWhenPartialNotSupported()
+        {
+            RouteSyncProcess routeSyncProcess = GetRouteSyncProcessSettings();
+            return routeSyncProcess.ExecuteFullRouteSyncWhenPartialNotSupported;
+        }
 
-		private RouteSyncProcess GetRouteSyncProcessSettings()
-		{
-			Vanrise.Common.Business.SettingManager settingManager = new Vanrise.Common.Business.SettingManager();
-			return settingManager.GetCachedOrCreate("WhS_RouteSync_ConfigurationManager_GetRouteSyncProcessSettings",
-				() =>
-				{
-					var routeSyncSettings = GetRouteSyncSettings(settingManager);
-					if (routeSyncSettings.RouteSyncProcess == null)
-						throw new NullReferenceException("routeSyncSettings.RouteSyncProcess");
-					return routeSyncSettings.RouteSyncProcess;
-				});
-		}
-		
-		public RouteSynchronizerSwitchSettings GetRouteSynchronizerSwitchSettings(Guid configId)
-		{
-			var switchSettingsByConfigId = GetRouteSynchronizerSwitchSettings();
-			var switchSettings = switchSettingsByConfigId.GetRecord(configId);
-			switchSettings.ThrowIfNull(string.Format("No RouteSynchronizerSwitchSettings found with config id {0}", configId));
-			return switchSettings;
-		}
+        public SwitchInfoGetter GetSwitchInfoGetter()
+        {
+            Vanrise.Common.Business.SettingManager settingManager = new Vanrise.Common.Business.SettingManager();
+            return settingManager.GetCachedOrCreate("WhS_RouteSync_ConfigurationManager_GetSwitchInfoGetter",
+                () =>
+                {
+                    var routeSyncTechnicalSettings = GetRouteSyncTechnicalSettings(settingManager);
+                    if (routeSyncTechnicalSettings.SwitchInfoGetter == null)
+                        throw new NullReferenceException("routeSyncTechnicalSettings.SwitchInfoGetter");
+                    return routeSyncTechnicalSettings.SwitchInfoGetter;
+                });
+        }
 
-		private Dictionary<Guid, RouteSynchronizerSwitchSettings> GetRouteSynchronizerSwitchSettings()
-		{
-			Vanrise.Common.Business.SettingManager settingManager = new Vanrise.Common.Business.SettingManager();
-			return settingManager.GetCachedOrCreate("WhS_RouteSync_ConfigurationManager_GetRouteSynchronizerSwitchSettings",
-				() =>
-				{
-					var routeSyncSettings = GetRouteSyncSettings(settingManager);
-					if (routeSyncSettings.SwitchSettingsByConfigId == null)
-						throw new NullReferenceException("routeSyncSettings.SwitchSettingsByConfigId");
-					return routeSyncSettings.SwitchSettingsByConfigId;
-				});
-		}
+        public RouteSynchronizerSwitchSettings GetRouteSynchronizerSwitchSettings(Guid configId)
+        {
+            var switchSettingsByConfigId = GetRouteSynchronizerSwitchSettings();
+            var switchSettings = switchSettingsByConfigId.GetRecord(configId);
+            switchSettings.ThrowIfNull(string.Format("No RouteSynchronizerSwitchSettings found with config id {0}", configId));
+            return switchSettings;
+        }
 
-		private RouteSyncTechnicalSettings GetRouteSyncTechnicalSettings(Vanrise.Common.Business.SettingManager settingManager)
-		{
-			var routeSyncTechnicalSettings = settingManager.GetSetting<RouteSyncTechnicalSettings>(RouteSyncTechnicalSettings.SETTING_TYPE);
-			if (routeSyncTechnicalSettings == null)
-				throw new NullReferenceException("routeSyncTechnicalSettings");
-			return routeSyncTechnicalSettings;
-		}
+        #endregion
 
-		private RouteSyncSettings GetRouteSyncSettings(Vanrise.Common.Business.SettingManager settingManager)
-		{
-			var routeSyncSettings = settingManager.GetSetting<RouteSyncSettings>(RouteSyncSettings.SETTING_TYPE);
-			if (routeSyncSettings == null)
-				throw new NullReferenceException("routeSyncSettings");
-			return routeSyncSettings;
-		}
-	}
+        #region Private Methods
+
+        private RouteSyncProcess GetRouteSyncProcessSettings()
+        {
+            Vanrise.Common.Business.SettingManager settingManager = new Vanrise.Common.Business.SettingManager();
+            return settingManager.GetCachedOrCreate("WhS_RouteSync_ConfigurationManager_GetRouteSyncProcessSettings",
+                    () =>
+                    {
+                        var routeSyncSettings = GetRouteSyncSettings(settingManager);
+                        if (routeSyncSettings.RouteSyncProcess == null)
+                            throw new NullReferenceException("routeSyncSettings.RouteSyncProcess");
+                        return routeSyncSettings.RouteSyncProcess;
+                    });
+        }
+
+        private Dictionary<Guid, RouteSynchronizerSwitchSettings> GetRouteSynchronizerSwitchSettings()
+        {
+            Vanrise.Common.Business.SettingManager settingManager = new Vanrise.Common.Business.SettingManager();
+            return settingManager.GetCachedOrCreate("WhS_RouteSync_ConfigurationManager_GetRouteSynchronizerSwitchSettings", 
+                    () =>
+                    {
+                        var routeSyncSettings = GetRouteSyncSettings(settingManager);
+                        if (routeSyncSettings.SwitchSettingsByConfigId == null)
+                            throw new NullReferenceException("routeSyncSettings.SwitchSettingsByConfigId");
+                        return routeSyncSettings.SwitchSettingsByConfigId;
+                    });
+        }
+
+        private RouteSyncSettings GetRouteSyncSettings(Vanrise.Common.Business.SettingManager settingManager)
+        {
+            var routeSyncSettings = settingManager.GetSetting<RouteSyncSettings>(RouteSyncSettings.SETTING_TYPE);
+            if (routeSyncSettings == null)
+                throw new NullReferenceException("routeSyncSettings");
+            return routeSyncSettings;
+        }
+
+        private RouteSyncTechnicalSettings GetRouteSyncTechnicalSettings(Vanrise.Common.Business.SettingManager settingManager)
+        {
+            var routeSyncTechnicalSettings = settingManager.GetSetting<RouteSyncTechnicalSettings>(RouteSyncTechnicalSettings.SETTING_TYPE);
+            if (routeSyncTechnicalSettings == null)
+                throw new NullReferenceException("routeSyncTechnicalSettings");
+            return routeSyncTechnicalSettings;
+        }
+
+        #endregion
+    }
 }
