d91e38be84f9f048f959813e786f1c130e94e432
Samer Haidar <VANRISE\samer.haidar>
2018-11-01 10:18:30 +0000

    adding WhSCarrierAccountsBEDefinition
    git-tfs-id: [http://developmentvlan:8585/tfs/vanrise.collection]$/;C50670

---
+++ CarrierPortal/Code/CP.WhS.Business/WhSCarrierAccountsBEDefinition.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Vanrise.GenericData.Entities;
+
+namespace CP.WhS.Business
+{
+    public  class WhSCarrierAccountsBEDefinition : BusinessEntityDefinitionSettings
+    {
+        public static Guid s_configId = new Guid("1D04AAD6-B9DB-4608-8635-470568A4D193");
+        public override Guid ConfigId { get { return s_configId; } }
+
+        public override string DefinitionEditor { get { return "cp-whs-carrieraccountsbedefinition-editor"; } }
+
+        public override string IdType { get { return "System.Int32"; } }
+
+        public override string SelectorUIControl { get { return "cp-whs-carrieraccounts-selector"; } }
+
+        public override string ManagerFQTN { get { return "CP.WhS.Business.WhSCarrierAccountBEManager, CP.WhS.Business"; } }
+
+        public bool GetCustomers { get; set; }
+        public bool GetSuppliers { get; set; }
+        public Guid VRConnectionId { get; set; }
+    }
+}
