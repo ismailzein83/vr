fadc1ead81507b12201c0333199111921659ba4d
yara.bousalman <yara.bousalman@vanrise.tfs.local>
2018-11-01 14:13:07 +0000

    Adding needed managers
    git-tfs-id: [http://developmentvlan:8585/tfs/vanrise.collection]$/;C50683

---
+++ CarrierPortal/Code/CP.WhS.Business/WhSCarrierAccountBEManager.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+using TOne.WhS.BusinessEntity.Entities;
+using Vanrise.Entities;
+using Vanrise.GenericData.Entities;
+
+namespace CP.WhS.Business
+{
+    public class WhSCarrierAccountBEManager : BaseBusinessEntityManager
+    {
+
+        #region Public Methods
+       
+        #endregion
+        #region BaseBusinessEntityManager
+        public override List<dynamic> GetAllEntities(IBusinessEntityGetAllContext context)
+        {
+            throw new NotImplementedException();
+        }
+
+        public override dynamic GetEntity(IBusinessEntityGetByIdContext context)
+        {
+            throw new NotImplementedException();
+        }
+
+        public override string GetEntityDescription(IBusinessEntityDescriptionContext context)
+        {
+            throw new NotImplementedException();
+        }
+
+        public override dynamic GetEntityId(IBusinessEntityIdContext context)
+        {
+            throw new NotImplementedException();
+        }
+
+        public override IEnumerable<dynamic> GetIdsByParentEntityId(IBusinessEntityGetIdsByParentEntityIdContext context)
+        {
+            throw new NotImplementedException();
+        }
+
+        public override dynamic GetParentEntityId(IBusinessEntityGetParentEntityIdContext context)
+        {
+            throw new NotImplementedException();
+        }
+
+        public override bool IsCacheExpired(IBusinessEntityIsCacheExpiredContext context, ref DateTime? lastCheckTime)
+        {
+            throw new NotImplementedException();
+        }
+
+        public override dynamic MapEntityToInfo(IBusinessEntityMapToInfoContext context)
+        {
+            throw new NotImplementedException();
+        }
+        #endregion
+    }
+}
