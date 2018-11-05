f260b2422b73c535420ddfe081a904c4ec85eef4
yara.bousalman <yara.bousalman@vanrise.tfs.local>
2018-11-05 11:08:26 +0000

    Adding PortalConnectionManager
    git-tfs-id: [http://developmentvlan:8585/tfs/vanrise.collection]$/;C50757

---
+++ CarrierPortal/Code/CP.WhS.Business/PortalConnectionManager.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Vanrise.Common.Business;
+
+namespace CP.WhS.Business
+{
+    public class PortalConnectionManager
+    {
+        VRConnectionManager _connectionManager = new VRConnectionManager();
+        static Guid WhSConnectionId = new Guid("B8058F6A-6545-465A-9DCA-6A4157ECFECB");
+        public VRInterAppRestConnection GetWhSConnectionSettings()
+        {
+            var vrConnection = _connectionManager.GetVRConnection<VRInterAppRestConnection>(WhSConnectionId);
+            return vrConnection.Settings as VRInterAppRestConnection;
+        }
+
+        public VRInterAppRestConnection GetConnectionSettings(Guid connectionId)
+        {
+            var vrConnection = _connectionManager.GetVRConnection<VRInterAppRestConnection>(connectionId);
+            return vrConnection.Settings as VRInterAppRestConnection;
+        }
+
+        public Guid GetWhSConnectionId()
+        {
+            return WhSConnectionId;
+        }
+
+    }
+}
