157957d05bcfa348b6eb6bf4518479656a3095c4
yara.bousalman <yara.bousalman@vanrise.tfs.local>
2018-11-05 08:32:27 +0000

    Adding DataRecordFieldChoiceDataManager to RDB project
    git-tfs-id: [http://developmentvlan:8585/tfs/vanrise.collection]$/;C50752

--- Vanrise/Code/Vanrise.GenericData.Data.RDB/DataRecordFieldChoiceDataManager.cs
+++ Vanrise/Code/Vanrise.GenericData.Data.RDB/DataRecordFieldChoiceDataManager.cs
@@ -3,10 +3,98 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Vanrise.Data.RDB;
+using Vanrise.GenericData.Entities;
 
 namespace Vanrise.GenericData.Data.RDB
 {
-    public class DataRecordFieldChoiceDataManager
+    public class DataRecordFieldChoiceDataManager : IDataRecordFieldChoiceDataManager
     {
+        #region RDB
+        static string TABLE_NAME = "genericdata_DataRecordFieldChoice";
+        static string TABLE_ALIAS = "drfc";
+        const string COL_ID = "ID";
+        const string COL_Name = "Name";
+        const string COL_Settings = "Settings";
+        const string COL_CreateTime = "CreateTime";
+
+
+        static DataRecordFieldChoiceDataManager()
+        {
+            var columns = new Dictionary<string, RDBTableColumnDefinition>();
+            columns.Add(COL_ID, new RDBTableColumnDefinition { DataType = RDBDataType.UniqueIdentifier });
+            columns.Add(COL_Name, new RDBTableColumnDefinition { DataType = RDBDataType.NVarchar, Size = 255 });
+            columns.Add(COL_Settings, new RDBTableColumnDefinition { DataType = RDBDataType.NVarchar });
+            columns.Add(COL_CreateTime, new RDBTableColumnDefinition { DataType = RDBDataType.DateTime });
+            RDBSchemaManager.Current.RegisterDefaultTableDefinition(TABLE_NAME, new RDBTableDefinition
+            {
+                DBSchemaName = "genericdata",
+                DBTableName = "DataRecordFieldChoice",
+                Columns = columns,
+                IdColumnName = COL_ID
+            });
+        }
+        #endregion
+        #region Private Methods
+        BaseRDBDataProvider GetDataProvider()
+        {
+            return RDBDataProviderFactory.CreateProvider("VR_GenericData_DataRecordFieldChoice", "ConfigurationDBConnStringKey", "ConfigurationDBConnStringKey");
+        }
+        #endregion
+
+        #region Mappers
+        DataRecordFieldChoice DataRecordFieldChoiceMapper(IRDBDataReader reader)
+        {
+            return new DataRecordFieldChoice()
+            {
+                DataRecordFieldChoiceId = reader.GetGuid(COL_ID),
+                Name = reader.GetString(COL_Name),
+                Settings = Vanrise.Common.Serializer.Deserialize<DataRecordFieldChoiceSettings>(reader.GetString(COL_Settings))
+            };
+        }
+        #endregion
+        #region IDataRecordFieldChoiceDataManager
+        public bool AddDataRecordFieldChoice(DataRecordFieldChoice dataRecordFieldChoice)
+        {
+            var queryContext = new RDBQueryContext(GetDataProvider());
+            var insertQuery = queryContext.AddInsertQuery();
+            insertQuery.IntoTable(TABLE_NAME);
+            var ifNotExists = insertQuery.IfNotExists(TABLE_ALIAS);
+            ifNotExists.EqualsCondition(COL_Name).Value(dataRecordFieldChoice.Name);
+            insertQuery.Column(COL_ID).Value(dataRecordFieldChoice.DataRecordFieldChoiceId);
+            insertQuery.Column(COL_Name).Value(dataRecordFieldChoice.Name);
+            insertQuery.Column(COL_Settings).Value(Vanrise.Common.Serializer.Serialize(dataRecordFieldChoice.Settings));
+            return queryContext.ExecuteNonQuery() > 0;
+        }
+
+        public bool AreDataRecordFieldChoicesUpdated(ref object updateHandle)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IEnumerable<DataRecordFieldChoice> GetDataRecordFieldChoices()
+        {
+            var queryContext = new RDBQueryContext(GetDataProvider());
+            var selectQuery = queryContext.AddSelectQuery();
+            selectQuery.From(TABLE_NAME, TABLE_ALIAS, null, true);
+            selectQuery.SelectColumns().Columns(COL_ID, COL_Name, COL_Settings);
+            selectQuery.Sort().ByColumn(COL_Name, RDBSortDirection.ASC);
+            return queryContext.GetItems<DataRecordFieldChoice>(DataRecordFieldChoiceMapper);
+        }
+
+        public bool UpdateDataRecordFieldChoice(DataRecordFieldChoice dataRecordFieldChoice)
+        {
+            var queryContext = new RDBQueryContext(GetDataProvider());
+            var updateQuery = queryContext.AddUpdateQuery();
+            updateQuery.FromTable(TABLE_NAME);
+            var ifNotExists = updateQuery.IfNotExists(TABLE_ALIAS);
+            ifNotExists.NotEqualsCondition(COL_ID).Value(dataRecordFieldChoice.DataRecordFieldChoiceId);
+            ifNotExists.EqualsCondition(COL_Name).Value(dataRecordFieldChoice.Name);
+            updateQuery.Column(COL_Name).Value(dataRecordFieldChoice.Name);
+            updateQuery.Column(COL_Settings).Value(Vanrise.Common.Serializer.Serialize(dataRecordFieldChoice.Settings));
+            updateQuery.Where().EqualsCondition(COL_ID).Value(dataRecordFieldChoice.DataRecordFieldChoiceId);
+            return queryContext.ExecuteNonQuery() > 0;
+        }
+        #endregion
     }
 }
