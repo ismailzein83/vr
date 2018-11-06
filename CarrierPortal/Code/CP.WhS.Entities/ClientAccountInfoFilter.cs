318f2b5fcb0945242c132fb62dee004a99bef963
yara.bousalman <yara.bousalman@vanrise.tfs.local>
2018-11-06 13:29:18 +0000

    Adding Client entities
    git-tfs-id: [http://developmentvlan:8585/tfs/vanrise.collection]$/;C50782

---
+++ CarrierPortal/Code/CP.WhS.Entities/ClientAccountInfoFilter.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CP.WhS.Entities
+{
+    public class ClientAccountInfoFilter
+    {
+        public bool GetCustomers { get; set; }
+        public bool GetSuppliers { get; set; }
+        public Guid? BusinessEntityDefinitionId { get; set; }
+    }
+}
---
+++ CarrierPortal/Code/CP.WhS.Entities/ClientSupplierZoneInfoFilter.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CP.WhS.Entities
+{
+    public class ClientSupplierZoneInfoFilter
+    {
+        public Guid? BusinessEntityDefinitionId { get; set; }
+
+        public int SupplierId { get; set; }
+
+    }
+}
