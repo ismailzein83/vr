7d7b32364c0116c5ccd9825ec1f3cab807ab6712
yara.bousalman <yara.bousalman@vanrise.tfs.local>
2018-11-07 14:47:40 +0000

    Filtering Repeated numbers data according to accessible carrier accounts only
    git-tfs-id: [http://developmentvlan:8585/tfs/vanrise.collection]$/;C50809

--- CarrierPortal/Code/CP.WhS.Business/RepeatedNumbersManager.cs
+++ CarrierPortal/Code/CP.WhS.Business/RepeatedNumbersManager.cs
@@ -17,6 +17,41 @@ namespace CP.WhS.Business
             var connectionSettings = new PortalConnectionManager().GetWhSConnectionSettings();
             var clonedInput = Utilities.CloneObject<DataRetrievalInput<RepeatedNumberQuery>>(query);
             clonedInput.IsAPICall = true;
+            WhSCarrierAccountBEManager whSCarrierAccountBEManager = new WhSCarrierAccountBEManager();
+            var accessibleCustomers = whSCarrierAccountBEManager.GetRemoteCarrierAccountsInfo(new Entities.ClientAccountInfoFilter() { GetCustomers = true });
+            var accessibleSuppliers = whSCarrierAccountBEManager.GetRemoteCarrierAccountsInfo(new Entities.ClientAccountInfoFilter() { GetSuppliers = true });
+            if (clonedInput.Query.Filter != null)
+            {
+                if (clonedInput.Query.Filter.CustomerIds != null && query.Query.Filter.CustomerIds.Count > 0)
+                {
+                    foreach (var customerId in query.Query.Filter.CustomerIds)
+                    {
+                        if (accessibleCustomers.FindRecord(x => x.AccountId == customerId) == null)
+                            return null;
+                    }
+                }
+                else if (clonedInput.Query.Filter.SupplierIds != null && query.Query.Filter.SupplierIds.Count > 0)
+                {
+                    foreach (var supplierId in query.Query.Filter.SupplierIds)
+                    {
+                        if (accessibleSuppliers.FindRecord(x => x.AccountId == supplierId) == null)
+                            return null;
+                    }
+                }
+                else
+                {
+                    clonedInput.Query.Filter.CustomerIds = accessibleCustomers.Select(x => x.AccountId).ToList();
+                    clonedInput.Query.Filter.SupplierIds = accessibleSuppliers.Select(x => x.AccountId).ToList();
+                }
+            }
+            else
+            {
+                clonedInput.Query.Filter = new RepeatedNumberFilter()
+                {
+                    CustomerIds = accessibleCustomers.Select(x => x.AccountId).ToList(),
+                    SupplierIds = accessibleSuppliers.Select(x => x.AccountId).ToList()
+                };
+            }
             if (clonedInput.DataRetrievalResultType == DataRetrievalResultType.Excel)
             {
                 return connectionSettings.Post<DataRetrievalInput<RepeatedNumberQuery>, RemoteExcelResult<RepeatedNumberDetail>>("/api/WhS_Analytics/RepeatedNumber/GetAllFilteredRepeatedNumbers", clonedInput);
