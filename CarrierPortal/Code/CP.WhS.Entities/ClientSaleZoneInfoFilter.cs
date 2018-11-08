88422a3414bf4b0e19a8fb43ecd91e1483703883
yara.bousalman <yara.bousalman@vanrise.tfs.local>
2018-11-08 12:01:59 +0000

    Adding ClientSaleZoneInfoFilter class
    git-tfs-id: [http://developmentvlan:8585/tfs/vanrise.collection]$/;C50832

---
+++ CarrierPortal/Code/CP.WhS.Entities/ClientSaleZoneInfoFilter.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CP.WhS.Entities
+{
+    public class ClientSaleZoneInfoFilter
+    {
+        public Guid? BusinessEntityDefinitionId { get; set; }
+    }
+}
