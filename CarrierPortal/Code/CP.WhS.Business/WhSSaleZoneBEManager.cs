2b49e421305b3b544f214c92f47c545e1ba94d1f
yara.bousalman <yara.bousalman@vanrise.tfs.local>
2018-11-08 11:57:02 +0000

    Adding WhSSaleZonesBEDefinition classes
    git-tfs-id: [http://developmentvlan:8585/tfs/vanrise.collection]$/;C50830

---
+++ CarrierPortal/Code/CP.WhS.Business/WhSSaleZoneBEManager.cs
@@ -0,0 +1,123 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+using TOne.WhS.BusinessEntity.APIEntities;
+using Vanrise.GenericData.Entities;
+using Vanrise.Security.Business;
+using Vanrise.Common;
+using CP.WhS.Entities;
+using TOne.WhS.BusinessEntity.Entities;
+
+namespace CP.WhS.Business
+{
+    public class WhSSaleZoneBEManager : BaseBusinessEntityManager
+    {
+        #region Public Methods
+        public IEnumerable<ClientSaleZoneInfo> GetRemoteSaleZonesInfo(ClientSaleZoneInfoFilter filter)
+        {
+            Func<ClientSaleZoneInfo, bool> filterExpression = (saleZoneInfo) =>
+            {
+                return true;
+            };
+            var userId = SecurityContext.Current.GetLoggedInUserId();
+            return GetCachedClientWhSSaleZonesInfoBySaleZoneId(userId).FindAllRecords(filterExpression);
+        }
+        public ClientSaleZoneInfo GetClientSaleZoneInfo(long saleZoneId)
+        {
+            var userId = SecurityContext.Current.GetLoggedInUserId();
+            return GetCachedClientWhSSaleZonesInfoBySaleZoneId(userId).GetRecord(saleZoneId);
+        }
+        public string GetSaleZoneName(long saleZoneId)
+        {
+            var saleZone = GetClientSaleZoneInfo(saleZoneId);
+            return saleZone.Name;
+        }
+
+        public IEnumerable<ClientSaleZoneInfo> GetSaleZoneInfoByIds(List<long> selectedIds)
+        {
+            var userId = SecurityContext.Current.GetLoggedInUserId();
+            List<ClientSaleZoneInfo> saleZones = new List<ClientSaleZoneInfo>();
+            var saleZonesInfo = GetCachedClientWhSSaleZonesInfoBySaleZoneId(userId);
+            foreach (var id in selectedIds)
+            {
+                ClientSaleZoneInfo saleZone;
+                if (saleZonesInfo.TryGetValue(id, out saleZone))
+                {
+                    saleZones.Add(saleZone);
+                }
+            }
+            return saleZones;
+        }
+        #endregion
+
+        #region Private Classes
+        private class CacheManager : Vanrise.Caching.BaseCacheManager
+        {
+            protected override bool IsTimeExpirable
+            {
+                get { return true; }
+            }
+        }
+
+        #endregion
+
+        #region Private Methods
+        private Dictionary<long, ClientSaleZoneInfo> GetCachedClientWhSSaleZonesInfoBySaleZoneId(int userId)
+        {
+            return Vanrise.Caching.CacheManagerFactory.GetCacheManager<CacheManager>().GetOrCreateObject(string.Format("GetCachedClientWhSSaleZonesInfoBySaleZoneId_{0}", userId),
+              () =>
+              {
+                  PortalConnectionManager connectionManager = new PortalConnectionManager();
+                  var connectionSettings = connectionManager.GetWhSConnectionSettings();
+                  var saleZones = connectionSettings.Get<IEnumerable<ClientSaleZoneInfo>>(string.Format("/api/WhS_BE/SaleZone/GetClientSaleZonesInfo?serializedFilter={0}", Serializer.Serialize(new SaleZoneInfoFilter())));
+                  return saleZones!=null ? saleZones.ToDictionary(x=>x.SaleZoneId, x=>x) : null;
+              });
+        }
+        #endregion
+        #region BaseBusinessEntityManager
+        public override List<dynamic> GetAllEntities(IBusinessEntityGetAllContext context)
+        {
+            throw new NotImplementedException();
+        }
+
+        public override dynamic GetEntity(IBusinessEntityGetByIdContext context)
+        {
+            throw new NotImplementedException();
+        }
+
+        public override string GetEntityDescription(IBusinessEntityDescriptionContext context)
+        {
+            return GetSaleZoneName(Convert.ToInt64(context.EntityId));
+        }
+
+        public override dynamic GetEntityId(IBusinessEntityIdContext context)
+        {
+            throw new NotImplementedException();
+        }
+
+        public override IEnumerable<dynamic> GetIdsByParentEntityId(IBusinessEntityGetIdsByParentEntityIdContext context)
+        {
+            throw new NotImplementedException();
+        }
+
+        public override dynamic GetParentEntityId(IBusinessEntityGetParentEntityIdContext context)
+        {
+            throw new NotImplementedException();
+        }
+
+        public override bool IsCacheExpired(IBusinessEntityIsCacheExpiredContext context, ref DateTime? lastCheckTime)
+        {
+            throw new NotImplementedException();
+        }
+
+        public override dynamic MapEntityToInfo(IBusinessEntityMapToInfoContext context)
+        {
+            throw new NotImplementedException();
+        }
+        #endregion
+
+    }
+}
---
+++ CarrierPortal/Code/CP.WhS.Business/WhSSaleZonesBEDefinition.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Vanrise.GenericData.Entities;
+
+namespace CP.WhS.Business
+{
+    public class WhSSaleZonesBEDefinition : BusinessEntityDefinitionSettings
+    {
+        public static Guid s_configId = new Guid("AEEF5962-D050-4F40-B914-E26977775FDD");
+        public override Guid ConfigId { get { return s_configId; } }
+
+        public override string DefinitionEditor { get { return "cp-whs-salezonesbedefinition-editor"; } }
+
+        public override string IdType { get { return "System.Int64"; } }
+
+        public override string SelectorUIControl { get { return "cp-whs-salezones-selector"; } }
+
+        public override string ManagerFQTN { get { return "CP.WhS.Business.WhSSaleZoneBEManager, CP.WhS.Business"; } }
+
+        public Guid VRConnectionId { get; set; }
+    }
+}
