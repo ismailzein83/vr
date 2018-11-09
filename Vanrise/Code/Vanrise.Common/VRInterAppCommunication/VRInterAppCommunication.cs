47c27f32dfc0686aafee1c58445004dc1d2705b4
Ismail Zein <VANRISE\ismail.zein>
2018-11-09 16:16:33 +0000

    added InterAppCommunication
    git-tfs-id: [http://developmentvlan:8585/tfs/vanrise.collection]$/;C50903

---
+++ Vanrise/Code/Vanrise.Common/VRInterAppCommunication/VRInterAppCommunication.cs
@@ -0,0 +1,331 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Net.Sockets;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Vanrise.Common
+{
+    public static class VRInterAppCommunication
+    {
+        #region Static
+
+        static int s_tcpPortRangeStart;
+        static int s_tcpPortRangeEnd;
+        static int s_tcpServiceHostingRetries;
+
+        static Dictionary<string, VRCommunicationRegisteredServiceInfo> s_registeredServicesByName = new Dictionary<string, VRCommunicationRegisteredServiceInfo>();
+
+        static Dictionary<Type, Type> s_ProxiesByTypes = new Dictionary<Type, Type>();
+
+        static Object s_lockObj = new object();
+        static TcpListener s_tcpListener;
+        static string s_portNumber;
+
+        static VRInterAppCommunication()
+        {
+            if (!int.TryParse(ConfigurationManager.AppSettings["TCPPortRangeStart"], out s_tcpPortRangeStart))
+                s_tcpPortRangeStart = 40000;
+            if (!int.TryParse(ConfigurationManager.AppSettings["TCPPortRangeEnd"], out s_tcpPortRangeEnd))
+                s_tcpPortRangeEnd = 50000;
+            if (!int.TryParse(ConfigurationManager.AppSettings["TCPServiceHostingRetries"], out s_tcpServiceHostingRetries))
+                s_tcpServiceHostingRetries = 1000;
+            AddServiceToRegisteredServices(typeof(VRInterAppCommunicationServiceManager), typeof(IVRInterAppCommunicationServiceManager), "VRInterAppCommunicationServiceManager");
+        }
+
+        #endregion
+
+        #region Public Methods
+
+        public static void RegisterService(Type serviceType, Type contractType, out string serviceUrl)
+        {
+            if (!contractType.IsInterface)
+                throw new Exception($"contractType '{contractType}' is not an Interface");
+            if (!contractType.IsAssignableFrom(serviceType))
+                throw new Exception($"serviceType '{serviceType}' doesnt implement contractType '{contractType}'");
+            StartTCPServiceIfNotStarted();
+            string serviceName = string.Concat(serviceType.FullName, "/", Guid.NewGuid());
+            serviceUrl = string.Concat(Environment.MachineName, ":", s_portNumber, ":", serviceName);
+            AddServiceToRegisteredServices(serviceType, contractType, serviceName);
+        }
+
+        
+        public static bool TryCreateServiceClient<T>(string serviceURL, Action<T> onClientReady) where T : class
+        {
+            VRInterAppCommunicationProxy proxy = null;
+            try
+            {
+                proxy = new VRInterAppCommunicationProxy(serviceURL);
+                proxy.Connect();
+            }
+            catch
+            {
+                LoggerFactory.GetLogger().WriteWarning("cannot connect to Service. Service URL '{0}'", serviceURL);
+                return false;
+            }
+
+            onClientReady(GetProxy<T>(serviceURL));
+            return true;
+        }
+
+        public static void CreateServiceClient<T>(string serviceURL, Action<T> onClientReady) where T : class
+        {
+            T proxy = GetProxy<T>(serviceURL);
+            onClientReady(proxy);
+        }
+
+
+
+        #endregion
+
+        #region Private Methods
+
+        private static void AddServiceToRegisteredServices(Type serviceType, Type contractType, string serviceName)
+        {
+            lock (s_registeredServicesByName)
+            {
+                VRCommunicationRegisteredServiceInfo serviceInfo = new VRCommunicationRegisteredServiceInfo
+                {
+                    ServiceInstance = Activator.CreateInstance(serviceType),
+                    Methods = new Dictionary<string, MethodInfo>()
+                };
+                foreach (var methodInfo in contractType.GetMethods())
+                {
+                    if (serviceInfo.Methods.ContainsKey(methodInfo.Name))
+                        throw new Exception($"duplicate method names not supported. Method Name '{methodInfo.Name}'");
+                    serviceInfo.Methods.Add(methodInfo.Name, methodInfo);
+                }
+                s_registeredServicesByName.Add(serviceName, serviceInfo);
+            }
+        }
+
+        private static void StartTCPServiceIfNotStarted()
+        {
+            if (s_tcpListener == null)
+            {
+                lock (s_lockObj)
+                {
+                    if (s_tcpListener == null)
+                    {
+                        var random = new Random();
+                        for (int i = 0; i < s_tcpServiceHostingRetries; i++)
+                        {
+                            int portNumber = random.Next(s_tcpPortRangeStart, s_tcpPortRangeEnd);
+                            bool rethrowIfError = (i == (s_tcpServiceHostingRetries - 1));//last iteration
+                            try
+                            {
+                                s_tcpListener = new TcpListener(IPAddress.Any, portNumber);
+                                s_tcpListener.Start();
+                                s_portNumber = portNumber.ToString();
+                                OpenThreadToListenToTCPRequests();
+                                LoggerFactory.GetLogger().WriteInformation("InterApp Communication Service registered successfully. Machine Name '{0}' Port Number '{1}'", Environment.MachineName, portNumber);
+                                break;
+                            }
+                            catch(Exception ex)
+                            {
+                                LoggerFactory.GetLogger().WriteWarning("Could not register InterApp Communication Service. Machine Name '{0}' Port Number '{1}. Error: {2}", Environment.MachineName, portNumber, ex);
+                                if (rethrowIfError)
+                                    throw;
+                            }
+                        }
+                    }
+                }
+            }
+        }
+
+        private static void OpenThreadToListenToTCPRequests()
+        {
+            Task t = new Task(() =>
+            {
+                using (TcpClient tcpClient = s_tcpListener.AcceptTcpClient())
+                {
+                    OpenThreadToListenToTCPRequests();
+                    using (NetworkStream s = tcpClient.GetStream())
+                    {
+                        using (StreamReader sr = new StreamReader(s))
+                        {
+                            using (StreamWriter sw = new StreamWriter(s))
+                            {
+                                sw.AutoFlush = true;
+                                string receivedValue = sr.ReadLine();
+                                VRTCPResponse response = new VRTCPResponse();
+                                try
+                                {
+                                    VRTCPRequest tcpRequest = Serializer.Deserialize(receivedValue).CastWithValidate<VRTCPRequest>("tcpRequest");
+                                    VRCommunicationRegisteredServiceInfo registeredService;
+                                    if (!s_registeredServicesByName.TryGetValue(tcpRequest.ServiceName, out registeredService))
+                                        throw new Exception($"Service '{tcpRequest.ServiceName}' not registered");
+                                    MethodInfo methodInfo;
+                                    if (!registeredService.Methods.TryGetValue(tcpRequest.MethodName, out methodInfo))
+                                        throw new Exception($"Method '{tcpRequest.MethodName}' not found in service '{tcpRequest.ServiceName}'");
+                                    var returnedValue = methodInfo.Invoke(registeredService.ServiceInstance, tcpRequest.Parameters);
+                                    if (methodInfo.ReturnType != typeof(void))
+                                    {
+                                        response.Response = Serializer.Serialize(returnedValue);   
+                                    }
+                                    response.IsSucceeded = true;
+                                }
+                                catch (Exception ex)
+                                {
+                                    LoggerFactory.GetExceptionLogger().WriteException(ex);
+                                    response.IsSucceeded = false;
+                                    response.Response = ex.ToString();
+                                }
+                                sw.WriteLine(Serializer.Serialize(response));
+                            }
+                        }
+                        s.Close();
+                    }
+                }
+            });
+            t.Start();
+        }
+
+        private static T GetProxy<T>(string serviceUrl) where T : class
+        {
+            Type interfaceType = typeof(T);
+            Type proxyType;
+            if(!s_ProxiesByTypes.TryGetValue(interfaceType, out proxyType))
+            {
+                lock(s_ProxiesByTypes)
+                {
+                    if(!s_ProxiesByTypes.TryGetValue(interfaceType, out proxyType))
+                    {
+                        StringBuilder classDefinitionBuilder = new StringBuilder(@" 
+                using System;
+
+                namespace #NAMESPACE#
+                {
+                    public class #CLASSNAME# : Vanrise.Common.VRInterAppCommunicationProxy, #INTERFACEFQTN#
+                    {   
+                        public #CLASSNAME#(string serviceUrl) : base(serviceUrl)
+                        {
+                        }
+
+                        #METHODS#
+                    }
+                }
+                ");
+
+                        classDefinitionBuilder.Replace("#INTERFACEFQTN#", interfaceType.FullName);
+
+                        StringBuilder methodsBuilder = new StringBuilder();
+
+                        foreach (var methodInfo in interfaceType.GetMethods())
+                        {
+                            methodsBuilder.Append("public ");
+                            if (methodInfo.ReturnType == typeof(void))
+                                methodsBuilder.Append("void");
+                            else
+                                methodsBuilder.Append(CSharpCompiler.TypeToString(methodInfo.ReturnType));
+                            methodsBuilder.Append($" {methodInfo.Name}(");
+                            bool isFirstParameter = true;
+                            foreach (var parameterInfo in methodInfo.GetParameters())
+                            {
+                                if (!isFirstParameter)
+                                    methodsBuilder.Append(", ");
+                                isFirstParameter = false;
+                                methodsBuilder.Append($"{CSharpCompiler.TypeToString(parameterInfo.ParameterType)} {parameterInfo.Name}");
+                            }
+                            methodsBuilder.Append(")");
+                            methodsBuilder.AppendLine();
+                            methodsBuilder.Append("{");
+                            methodsBuilder.AppendLine();
+                            if (methodInfo.ReturnType != typeof(void))
+                                methodsBuilder.Append("return ");
+                            methodsBuilder.Append("base.SendRequest");
+                            if (methodInfo.ReturnType != typeof(void))
+                                methodsBuilder.Append($"<{CSharpCompiler.TypeToString(methodInfo.ReturnType)}>");
+                            methodsBuilder.Append($"(\"{methodInfo.Name}\"");
+                            
+                            foreach (var parameterInfo in methodInfo.GetParameters())
+                            {
+                                methodsBuilder.Append(", ");
+                                methodsBuilder.Append(parameterInfo.Name);
+                            }
+                            methodsBuilder.Append(");");
+                            methodsBuilder.AppendLine();
+                            methodsBuilder.Append("}");
+                            methodsBuilder.AppendLine();
+                        }
+                        classDefinitionBuilder.Replace("#METHODS#", methodsBuilder.ToString());
+
+                        string classNamespace = CSharpCompiler.GenerateUniqueNamespace("Vanrise.Common");
+                        string className = interfaceType.Name.Substring(1);
+                        classDefinitionBuilder.Replace("#NAMESPACE#", classNamespace);
+                        classDefinitionBuilder.Replace("#CLASSNAME#", className);
+                        string fullTypeName = String.Format("{0}.{1}", classNamespace, className);
+                        CSharpCompilationOutput compilationOutput;
+                        if (!CSharpCompiler.TryCompileClass(classDefinitionBuilder.ToString(), out compilationOutput))
+                        {
+                            StringBuilder errorsBuilder = new StringBuilder();
+                            if (compilationOutput.ErrorMessages != null)
+                            {
+                                foreach (var errorMessage in compilationOutput.ErrorMessages)
+                                {
+                                    errorsBuilder.AppendLine(errorMessage);
+                                }
+                            }
+                            throw new Exception(String.Format("Compile Error when building executor type for PropValueReader. Errors: {0}",
+                                errorsBuilder));
+                        }
+                        proxyType = compilationOutput.OutputAssembly.GetType(fullTypeName);
+                        proxyType.ThrowIfNull("proxyType");
+                        s_ProxiesByTypes.Add(interfaceType, proxyType);
+                    }
+                }
+            }
+            return Activator.CreateInstance(proxyType, serviceUrl).CastWithValidate<T>("proxyObj");
+        }
+
+        #endregion
+
+        #region Private Classes
+
+        private class VRCommunicationRegisteredServiceInfo
+        {
+            public Object ServiceInstance { get; set; }
+
+            public Dictionary<string, MethodInfo> Methods { get; set; }
+
+        }
+
+        private interface IVRInterAppCommunicationServiceManager
+        {
+            bool IsServiceRegistered(string serviceName);
+        }
+
+
+        private class VRInterAppCommunicationServiceManager : IVRInterAppCommunicationServiceManager
+        {
+            public bool IsServiceRegistered(string serviceName)
+            {
+                return s_registeredServicesByName.ContainsKey(serviceName);
+            }
+        }
+
+        #endregion
+    }
+
+    public class VRTCPRequest
+    {
+        public string ServiceName { get; set; }
+
+        public string MethodName { get; set; }
+
+        public object[] Parameters { get; set; }
+    }
+
+    public class VRTCPResponse
+    {
+        public bool IsSucceeded { get; set; }
+
+        public string Response { get; set; }
+    }
+}
---
+++ Vanrise/Code/Vanrise.Common/VRInterAppCommunication/VRInterAppCommunicationProxy.cs
@@ -0,0 +1,107 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Vanrise.Common
+{
+    public interface IVRInterAppCommunicationProxy
+    {
+        void SendRequest(string methodName, params object[] parameters);
+        T SendRequest<T>(string methodName, params object[] parameters);
+    }
+
+    public class VRInterAppCommunicationProxy : IVRInterAppCommunicationProxy
+    {
+        string _serviceURL;
+        string _machineName;
+        int _portNumber;
+        string _serviceName;
+
+        public VRInterAppCommunicationProxy(string serviceURL)
+        {
+            serviceURL.ThrowIfNull("serviceURL");
+            _serviceURL = serviceURL;
+            string[] parts = serviceURL.Split(':');
+            if (parts.Length != 3)
+                throw new Exception($"Invalid Service URL '{serviceURL}'");
+            _machineName = parts[0];
+            if (!int.TryParse(parts[1], out _portNumber))
+                throw new Exception($"Invalid Port Number '{parts[1]}'");
+            _serviceName = parts[2];
+        }
+
+        internal void Connect()
+        {
+            using (var tcpClient = new TcpClient())
+            {
+                tcpClient.Connect(_machineName, _portNumber);
+                if (!SendRequest<bool>("VRInterAppCommunicationServiceManager", "PrivateSendRequest", this._serviceName))
+                    throw new Exception($"Service '{this._serviceName}' not registered on machine '{_machineName}' port number '{_portNumber}'");
+                tcpClient.Close();
+            }
+        }
+
+        public T SendRequest<T>(string methodName, params Object[] parameters)
+        {
+            var serializedResponse = PrivateSendRequest(methodName, true, parameters);
+            return serializedResponse != null ? Serializer.Deserialize<T>(serializedResponse) : default(T);
+        }
+
+        public void SendRequest(string methodName, params Object[] parameters)
+        {
+            PrivateSendRequest(methodName, false, parameters);
+        }
+
+        string PrivateSendRequest(string methodName, bool withResponse, params Object[] parameters)
+        {
+            return PrivateSendRequest(_serviceName, methodName, withResponse, parameters);
+        }
+
+        string PrivateSendRequest(string serviceName, string methodName, bool withResponse, params Object[] parameters)
+        {
+            VRTCPRequest tcpRequest = new VRTCPRequest
+            {
+                ServiceName = serviceName,
+                MethodName = methodName,
+                Parameters = parameters
+            };
+            string response = null;
+            using (var tcpClient = new TcpClient())
+            {
+                tcpClient.Connect(_machineName, _portNumber);
+                using (var stream = tcpClient.GetStream())
+                {
+                    using (var streamWriter = new StreamWriter(stream))
+                    {
+                        using (var streamReader = new StreamReader(stream))
+                        {
+                            streamWriter.AutoFlush = true;
+                            streamWriter.WriteLine(Serializer.Serialize(tcpRequest));
+                            string serializedResponse = streamReader.ReadLine();
+                            VRTCPResponse tcpResponse = Serializer.Deserialize<VRTCPResponse>(serializedResponse);
+                            tcpResponse.ThrowIfNull("tcpResponse");
+                            if (tcpResponse.IsSucceeded)
+                            {
+                                if (withResponse)
+                                {
+                                    response = tcpResponse.Response;
+                                }
+                            }
+                            else
+                            {
+                                throw new Exception($"Error when calling method '{methodName}' on serviceUrl '{_serviceURL}'. Error is '{tcpResponse.Response}'");
+                            }
+                        }
+                    }
+                    stream.Close();
+                }
+                tcpClient.Close();
+            }
+            return response;
+        }
+    }
+}
