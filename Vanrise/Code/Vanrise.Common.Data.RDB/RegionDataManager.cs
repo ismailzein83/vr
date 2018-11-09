ccbc1780f8c001ed3556d02d5e849e85d2bf1a28
Hassan Makhour <VANRISE\hassan.makhour>
2018-11-09 09:00:05 +0000

    RDB Classes
    git-tfs-id: [http://developmentvlan:8585/tfs/vanrise.collection]$/;C50864

---
+++ Vanrise/Code/Vanrise.Common.Data.RDB/RegionDataManager.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Vanrise.Data.RDB;
+using Vanrise.Entities;
+
+namespace Vanrise.Common.Data.RDB
+{
+	public class RegionDataManager
+	{
+		
+	}
+
+}
