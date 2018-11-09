a6a78e043e37eb4617fd6de748ae4f017d9335d9
Ali Sakr <VANRISE\ali.sakr>
2018-11-09 16:01:16 +0000

    adding KPISettings class for component Settings
    git-tfs-id: [http://developmentvlan:8585/tfs/vanrise.collection]$/;C50901

---
+++ Vanrise/Code/Vanrise.Analytic.Entities/Settings/KPISettings.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Vanrise.Entities;
+
+namespace Vanrise.Analytic.Entities
+{
+    public class KPISettings : SettingData
+    {
+        public List<AnalyticTableKPISettings> AnalyticTablesKPISettings { get; set; }
+    }
+    public class AnalyticTableKPISettings
+    {
+        public Guid AnalyticTableId { get; set; }
+        public List<MeasureStyleRule> MeasureStyleRules { get; set; }
+    }
+}
