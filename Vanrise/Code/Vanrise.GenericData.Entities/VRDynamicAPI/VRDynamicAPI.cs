9091f09844b83a5414a81301bf7f27d7408c66c3
Samer Haidar <VANRISE\samer.haidar>
2018-11-12 15:44:05 +0000

    Adding VRDynamicAPI
    git-tfs-id: [http://developmentvlan:8585/tfs/vanrise.collection]$/;C50936

---
+++ Vanrise/Code/Vanrise.GenericData.Entities/VRDynamicAPI/VRDynamicAPI.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Vanrise.GenericData.Entities
+{
+    public class VRDynamicAPI
+    {
+        public int VRDynamicAPIId { get; set; }
+        public string Name { get; set; }
+        public int ModuleId{ get; set; }
+        public List<VRDynamicAPIMethod> Methods { get; set; }
+    }
+    public class VRDynamicAPIMethod
+    {
+        public Guid VRDynamicAPIMethodId { get; set; }
+        public string Name { get; set; }
+        public VRDynamicAPIMethodSettings Settings { get; set; }
+    }
+    public abstract class VRDynamicAPIMethodSettings
+    {
+        public abstract Guid ConfigId { get; }
+    }
+
+    public enum VRDynamicAPIMethodType { Get = 1, Post = 2 }
+    public class VRCustomCodeDynamicAPIMethod : VRDynamicAPIMethodSettings
+    {
+        public override Guid ConfigId { get { return new Guid("98B21112-0364-4646-AA26-80263481DCDD"); } }
+        public string MethodBody { get; set; }
+        public VRDynamicAPIMethodType MethodType { get; set; }
+        public List<VRDynamicAPIMethodParameter> InParameters { get; set; }
+        public string ReturnType { get; set; }
+    }
+    public class VRDynamicAPIMethodParameter
+    {
+        public string ParameterName { get; set; }
+        public string ParameterType { get; set; }
+    }
+}
