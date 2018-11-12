0d2d96ba426045ab3c6794d3f27497ed72183490
Ali Sakr <VANRISE\ali.sakr>
2018-11-12 07:15:52 +0000

    adding AnalyticTableClientDetailClass
    git-tfs-id: [http://developmentvlan:8585/tfs/vanrise.collection]$/;C50908

---
+++ Vanrise/Code/Vanrise.Analytic.Entities/AnalyticTableClientDetail.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Vanrise.Analytic.Entities
+{
+    public class AnalyticTableClientDetail
+    {
+        public Guid AnalyticTableId { get; set; }
+        public string Name { get; set; }
+    }
+}
