c264501fa88517bc862b43e1355f10a90363e7a0
Wissam Ajamy <VANRISE\wissam.ajamy>
2018-11-13 08:56:33 +0000

    Add /batch scripts for CarrierPortal.TOne
    git-tfs-id: [http://developmentvlan:8585/tfs/vanrise.collection]$/;C50946

---
+++ Vanrise/Code/Resources/BatchScripts/CarrierPortal.TOne/03-WebSite.bat
@@ -0,0 +1,10 @@
+ECHO OFF
+::Make Directory from current date as YYYYMMDD
+Set CURRDATE=%TEMP%\CURRDATE.TMP
+DATE /T > %CURRDATE%
+Set PARSEARG="eol=; tokens=1,2,3,4* delims=/, "
+For /F %PARSEARG% %%i in (%CURRDATE%) Do SET YYYYMMDD=%%l%%j%%k
+
+ECHO CarrierPortal.TOne WebSite
+
+xcopy "C:\TFS\CarrierPortal\Code\CP.WhS.Web\CP_WhS" /S /E /R /y /v /i /z /Q "\\192.168.110.185\Fixes\WebSite\CP.TOne\%YYYYMMDD%\Client\Modules\CP_WhS\"
---
+++ Vanrise/Code/Resources/BatchScripts/CarrierPortal.TOne/04-GroupSQLFiles.bat
@@ -0,0 +1,3 @@
+ECHO OFF
+ECHO CarrierPortal.TOne Group SQL Scripts
+start /b /w /D"C:\TFS\Vanrise\Code\Vanrise.HelperTools\bin\Release" Vanrise.HelperTools.exe "GRPSQLOverridden" "CP.TOne"
