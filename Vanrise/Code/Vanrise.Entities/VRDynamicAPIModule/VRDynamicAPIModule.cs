1cb97ae77bae12b5a432106bb7827a76816b4c14
DevInternship User <VANRISE\Dev.InternshipUser>
2018-11-13 12:03:34 +0000

    Adding classes VRDynamicAPI
    git-tfs-id: [http://developmentvlan:8585/tfs/vanrise.collection]$/;C50959

---
+++ Vanrise/Code/Vanrise.Entities/VRDynamicAPIModule/VRDynamicAPIModule.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Vanrise.Entities
+{
+    public class VRDynamicAPIModule
+    {
+        public int VRDynamicAPIModuleId { get; set; }
+        public string Name { get; set; }
+        public DateTime CreatedTime { get; set; }
+        public int CreatedBy { get; set; }
+        public DateTime LastModifiedTime { get; set; }
+        public int LastModifiedBy { get; set; }
+    }
+}
