f80cf07a8730796a1e797e046410a573a45f826f
Hassan Makhour <VANRISE\hassan.makhour>
2018-11-13 14:20:36 +0000

    Adding HassanMakhourTask
    git-tfs-id: [http://developmentvlan:8585/tfs/vanrise.collection]$/;C50968

---
+++ TOneV2/Code/TOneV2/TestRuntime/Tasks/HassanMakhourTask.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using Vanrise.BusinessProcess;
+using Vanrise.Runtime;
+using Vanrise.Runtime.Entities;
+
+namespace TestRuntime
+{
+	public class HassanMakhourTask : ITask
+	{
+		public void Execute()
+		{
+			var runtimeServices = new List<RuntimeService>();
+
+			BusinessProcessService bpService = new BusinessProcessService() { Interval = new TimeSpan(0, 0, 2) };
+			runtimeServices.Add(bpService);
+
+			BPRegulatorRuntimeService bpRegulatorRuntimeService = new BPRegulatorRuntimeService { Interval = new TimeSpan(0, 0, 2) };
+			runtimeServices.Add(bpRegulatorRuntimeService);
+
+			RuntimeHost host = new RuntimeHost(runtimeServices);
+			host.Start();
+			Console.ReadKey();
+		}
+	}
+
+}
