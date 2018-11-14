121d70397e92ef9b4ce9f8e379b9d50819ee87f4
yara.bousalman <yara.bousalman@vanrise.tfs.local>
2018-11-14 11:16:48 +0000

    Adding ReportGenerationCustomCodeSettings
    git-tfs-id: [http://developmentvlan:8585/tfs/vanrise.collection]$/;C51008

---
+++ Vanrise/Code/Vanrise.Analytic.Business/ReportGenerationCustomCodeSettings.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Vanrise.Entities;
+
+namespace Vanrise.Analytic.Business
+{
+    public class ReportGenerationCustomCodeSettings : VRComponentTypeSettings
+    {
+        public override Guid VRComponentTypeConfigId { get { return new Guid("92B03E4E-37FB-48A1-ACEE-841135C30B5E"); } }
+
+        public string CustomCode { get; set; }
+
+        public string GetCustomCode()
+        {
+            return CustomCode;
+        }
+    }
+}
