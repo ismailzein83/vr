c010393d937837584a671a030fe9fb06d46099f2
Ali Sakr <VANRISE\ali.sakr>
2018-11-15 14:01:29 +0000

    adding new function to config manager
    git-tfs-id: [http://developmentvlan:8585/tfs/vanrise.collection]$/;C51071

--- Vanrise/Code/Vanrise.Analytic.Business/ConfigManager.cs
+++ Vanrise/Code/Vanrise.Analytic.Business/ConfigManager.cs
@@ -25,5 +25,14 @@ namespace Vanrise.Analytic.Business
             automatedReportSettings.ThrowIfNull("automatedReportSettings");
             return automatedReportSettings;
         }
+        public List<MeasureStyleRule> GetAnalytictableKPISettings (Guid analyticTableId)
+        {
+            SettingManager settingManager = new SettingManager();
+            KPISettings kpiSettings = settingManager.GetSetting<KPISettings>(KPISettings.SETTING_TYPE);
+            kpiSettings.ThrowIfNull("KPISettings");
+            var analyticTableKPISettings = kpiSettings.AnalyticTablesKPISettings.FindRecord(x => x.AnalyticTableId == analyticTableId);
+            analyticTableKPISettings.ThrowIfNull("AnalyticTableKPISettings");
+            return analyticTableKPISettings.MeasureStyleRules;
+        }
     }
 }
