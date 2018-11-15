bbef6e8dfe2f16494479a4e4680c5c3824a70d46
yara.bousalman <yara.bousalman@vanrise.tfs.local>
2018-11-15 12:58:45 +0000

    Adding new implementation for AutomatedReportFileGenerator
    git-tfs-id: [http://developmentvlan:8585/tfs/vanrise.collection]$/;C51066

--- Vanrise/Code/Vanrise.Analytic.MainExtensions/AutomatedReport/FileGenerators/ReportGenerationCustomCodeFileGenerator.cs
+++ Vanrise/Code/Vanrise.Analytic.MainExtensions/AutomatedReport/FileGenerators/ReportGenerationCustomCodeFileGenerator.cs
@@ -13,7 +13,7 @@ namespace Vanrise.Analytic.MainExtensions.AutomatedReport.FileGenerators
         {
             get { return new Guid("482D20EB-78B0-4633-B6F3-4B93B2ED1190"); }
         }
-
+        public Guid ReportGenerationCustomCodeFileGeneratorId { get; set; }
         public override VRAutomatedReportGeneratedFile GenerateFile(IVRAutomatedReportFileGeneratorGenerateFileContext context)
         {
             throw new NotImplementedException();
