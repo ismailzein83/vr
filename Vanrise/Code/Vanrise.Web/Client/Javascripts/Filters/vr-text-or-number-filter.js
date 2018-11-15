803a71d0a630462b644d7a5a24e62009e2e7ebff
Rodi Hassan <VANRISE\rodi.hassan>
2018-11-15 09:33:03 +0000

    fix bug in vrtextOrNumber filter
    git-tfs-id: [http://developmentvlan:8585/tfs/vanrise.collection]$/;C51048

--- Vanrise/Code/Vanrise.Web/Client/Javascripts/Filters/vr-text-or-number-filter.js
+++ Vanrise/Code/Vanrise.Web/Client/Javascripts/Filters/vr-text-or-number-filter.js
@@ -1,12 +1,12 @@
 ﻿'use strict';
 app.filter('vrtextOrNumber', ['$filter', function ($filter) {
-	return function (input, fractionSize) {
-		if (input == undefined || input == null || input == '')
-			return null;
-		if (isNaN(input)) {
-			return input;
-		} else {
-			return $filter('number')(input, fractionSize);
-		}
-	};
+    return function (input, fractionSize) {
+        if (input == undefined || input == null || input === '' || input === "")
+            return null;
+        if (isNaN(input)) {
+            return input;
+        } else {
+            return $filter('number')(input, fractionSize);
+        }
+    };
 }]);
