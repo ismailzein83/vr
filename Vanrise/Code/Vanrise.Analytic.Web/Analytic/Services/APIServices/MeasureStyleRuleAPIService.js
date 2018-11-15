223ad6df8a091d7d4651a803c7a54923c6b7a371
Ali Sakr <VANRISE\ali.sakr>
2018-11-15 12:54:55 +0000

    adding controller and api service for measure styles
    git-tfs-id: [http://developmentvlan:8585/tfs/vanrise.collection]$/;C51065

---
+++ Vanrise/Code/Vanrise.Analytic.Web/Analytic/Services/APIServices/MeasureStyleRuleAPIService.js
@@ -0,0 +1,21 @@
+﻿(function (appControllers) {
+
+    "use strict";
+    measureStyleRuleAPIService.$inject = ['BaseAPIService', 'UtilsService', 'VR_Analytic_ModuleConfig'];
+
+    function measureStyleRuleAPIService(BaseAPIService, UtilsService, VR_Analytic_ModuleConfig) {
+
+        var controllerName = 'MeasureStyleRuleController';
+
+        function GetAnalytictableKPISettings(analyticTableId) {
+            return BaseAPIService.get(UtilsService.getServiceURL(VR_Analytic_ModuleConfig.moduleName, controllerName, 'GetAnalytictableKPISettings'), { analyticTableId: analyticTableId });
+        }
+
+        return ({
+            GetAnalytictableKPISettings: GetAnalytictableKPISettings,
+        });
+    }
+
+    appControllers.service('VR_Analytic_MeasureStyleRuleAPIService', measureStyleRuleAPIService);
+
+})(appControllers); 
