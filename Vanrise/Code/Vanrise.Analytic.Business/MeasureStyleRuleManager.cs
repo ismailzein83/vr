1df3ca0f970e0da48184017cefe5ecb4db79bee2
Ali Sakr <VANRISE\ali.sakr>
2018-11-15 15:45:27 +0000

    adding MeasureStyleRuleManager
    git-tfs-id: [http://developmentvlan:8585/tfs/vanrise.collection]$/;C51076

--- Vanrise/Code/Vanrise.Analytic.Business/MeasureStyleRuleManager.cs
+++ Vanrise/Code/Vanrise.Analytic.Business/MeasureStyleRuleManager.cs
@@ -7,17 +7,18 @@ using Vanrise.Analytic.Entities;
 
 namespace Vanrise.Analytic.Business
 {
-  public class MeasureStyleRuleManager
+  public class MeasureStyleRuleManager 
     {
-        public MeasureStyleRuleEditorRuntime GetMeasureStyleRuleEditorRuntime (List <MeasureStyleRule> measureStyleRules,Guid analyticTableId)
-        {
-            MeasureStyleRuleEditorRuntime measureStyleRuleEditorRuntime = new MeasureStyleRuleEditorRuntime();
-            foreach (var rule in measureStyleRules)
-            {
+        //public MeasureStyleRuleEditorRuntime GetMeasureStyleRuleEditorRuntime (List <MeasureStyleRule> measureStyleRules,Guid analyticTableId)
+        //{
+        //    MeasureStyleRuleEditorRuntime measureStyleRuleEditorRuntime = new MeasureStyleRuleEditorRuntime();
+        //    List<MeasureStyleRuleDetail> measureStyleRuleDetails = new List<MeasureStyleRuleDetail>();
+        //    foreach (var rule in measureStyleRules)
+        //    {
                
-            }
+        //    }
 
-            return measureStyleRuleEditorRuntime;
-        }
+        //    return measureStyleRuleEditorRuntime;
+        //}
     }
 }
