54884f0ec3a31a5b7b7b1392a04646b11733c527
yara.bousalman <yara.bousalman@vanrise.tfs.local>
2018-11-15 12:14:52 +0000

    Adding ReportGenerationCustomCodeFileGenerator
    git-tfs-id: [http://developmentvlan:8585/tfs/vanrise.collection]$/;C51061

---
+++ Vanrise/Code/Vanrise.Analytic.MainExtensions/AutomatedReport/FileGenerators/ReportGenerationCustomCodeFileGenerator.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Vanrise.Analytic.Entities;
+
+namespace Vanrise.Analytic.MainExtensions.AutomatedReport.FileGenerators
+{
+    public class ReportGenerationCustomCodeFileGenerator : VRAutomatedReportFileGeneratorSettings
+    {
+        public override Guid ConfigId
+        {
+            get { return new Guid("482D20EB-78B0-4633-B6F3-4B93B2ED1190"); }
+        }
+
+        public override VRAutomatedReportGeneratedFile GenerateFile(IVRAutomatedReportFileGeneratorGenerateFileContext context)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
