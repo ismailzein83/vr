4205bf8dc22f99bc8eccfed4f31b1e93e8612495
yara.bousalman <yara.bousalman@vanrise.tfs.local>
2018-11-15 13:11:57 +0000

    Adding report generation custom code files
    git-tfs-id: [http://developmentvlan:8585/tfs/vanrise.collection]$/;C51067

---
+++ Vanrise/Code/Vanrise.Analytic.Web/Analytic/Directives/MainExtensions/AutomatedReport/FileGenerator/CustomCode/vr-analytic-reportgenerationcustomcode-filegenerator.js
@@ -0,0 +1,61 @@
+﻿"use strict";
+app.directive("vrAnalyticReportgenerationcustomcodeFilegenerator", ["UtilsService",
+    function (UtilsService) {
+        var directiveDefinitionObject = {
+            restrict: "E",
+            scope: {
+                onReady: "=",
+            },
+            controller: function ($scope, $element, $attrs) {
+                var ctrl = this;
+                var flatFile = new FlatFile($scope, ctrl, $attrs);
+                flatFile.initializeController();
+            },
+            controllerAs: "ctrl",
+            bindToController: true,
+            templateUrl: "/Client/Modules/Analytic/Directives/VRRe/AutomatedReport/FileGenerator/FlatFile/Templates/FlatFileGeneratorTemplate.html"
+        };
+
+
+        function FlatFile($scope, ctrl, $attrs) {
+            this.initializeController = initializeController;
+
+
+            function initializeController() {
+
+                UtilsService.waitMultiplePromises(promises).then(function () {
+                    defineAPI();
+                });
+            }
+
+            function defineAPI() {
+                var api = {};
+
+                api.load = function (payload) {
+                    $scope.scopeModel.isLoading = true;
+                    var promises = [];
+                    if (payload != undefined) {
+
+                    }
+                    return UtilsService.waitMultiplePromises(promises).then(function () {
+                        $scope.scopeModel.isLoading = false;
+
+                    });
+
+                };
+
+
+                api.getData = function () {
+                    var obj = {
+                        $type: "Vanrise.Analytic.MainExtensions.AutomatedReport.FileGenerators.FlatFileGenerator,Vanrise.Analytic.MainExtensions",
+                    };
+                    return obj;
+                };
+
+                if (ctrl.onReady != null)
+                    ctrl.onReady(api);
+            }
+
+            return directiveDefinitionObject;
+        }
+]);
---
+++ Vanrise/Code/Vanrise.Analytic.Web/Controllers/ReportGenerationCustomCodeSettingsController.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Http;
+using Vanrise.Analytic.Business;
+using Vanrise.Analytic.Entities;
+using Vanrise.Web.Base;
+
+namespace Vanrise.Analytic.Web.Controllers
+{
+    [JSONWithTypeAttribute]
+    [RoutePrefix(Constants.ROUTE_PREFIX + "ReportGenerationCustomCodeSettings")]
+    public class ReportGenerationCustomCodeSettingsController : BaseAPIController
+    {
+        ReportGenerationCustomCodeSettingsManager _manager = new ReportGenerationCustomCodeSettingsManager();
+
+        [HttpGet]
+        [Route("GetReportGenerationCustomCodeSettingsInfo")]
+        public IEnumerable<ReportGenerationCustomCodeDefinitionInfo> GetReportGenerationCustomCodeSettingsInfo()
+        {
+            return _manager.GetReportGenerationCustomCodeSettingsInfo();
+        }
+    }
+}
