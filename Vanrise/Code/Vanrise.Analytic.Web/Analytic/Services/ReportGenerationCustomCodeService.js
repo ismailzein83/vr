bb7e37b8bf5e74089a66e917eb28368aee5ea2a8
yara.bousalman <yara.bousalman@vanrise.tfs.local>
2018-11-16 15:01:38 +0000

    Adding CustomCodeCompilationErrorsEditor files
    git-tfs-id: [http://developmentvlan:8585/tfs/vanrise.collection]$/;C51109

---
+++ Vanrise/Code/Vanrise.Analytic.Web/Analytic/Services/ReportGenerationCustomCodeService.js
@@ -0,0 +1,20 @@
+﻿})(appControllers); (function (appControllers) {
+    'use stict';
+    ReportGenerationCustomCodeService.$inject = ['VRModalService'];
+    function ReportGenerationCustomCodeService(VRModalService) {
+        function showCustomCodeCompilationErrors(errorMessages) {
+            var modalSettings = {};
+            modalSettings.onScopeReady = function (modalScope) {
+            };
+
+            var modalParameters = {
+                errorMessages: errorMessages
+            };
+            VRModalService.showModal('/Client/Modules/Analytic/Directives/VRReportGeneration/Templates/CustomCodeCompilationErrorsEditorTemplate.html', modalParameters, modalSettings);
+        }
+        return {
+            showCustomCodeCompilationErrors: showCustomCodeCompilationErrors
+        };
+    }
+    appControllers.service('VR_Analytic_ReportGenerationCustomCodeService', ReportGenerationCustomCodeService);
+})(appControllers);
