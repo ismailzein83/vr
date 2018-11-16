2eab634d8cb16d42cb371689cc76e6e79b94d222
Samer Haidar <VANRISE\samer.haidar>
2018-11-16 08:17:00 +0000

    adding ReportGenerationCustomCodeContext
    git-tfs-id: [http://developmentvlan:8585/tfs/vanrise.collection]$/;C51083

---
+++ Vanrise/Code/Vanrise.Analytic.Business/ReportGenerationCustomCodeContext.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Vanrise.Analytic.Entities;
+using Vanrise.Common.Excel;
+
+namespace Vanrise.Analytic.Business
+{
+    public interface IReportGenerationCustomCodeContext
+    {
+        VRExcelFile CreateExcelFile();
+     //   VRAutomatedReportResolvedDataList GetDataList();
+
+    }
+    public class ReportGenerationCustomCodeContext : IReportGenerationCustomCodeContext
+    {
+        VRAutomatedReportResolvedDataList _dataListResolver;
+        public ReportGenerationCustomCodeContext()
+        {
+
+        }
+        public ReportGenerationCustomCodeContext(VRAutomatedReportResolvedDataList dataListResolver)
+        {
+            _dataListResolver = dataListResolver;
+        }
+        public VRExcelFile CreateExcelFile()
+        {
+            return new VRExcelFile();
+        }
+
+        //public VRAutomatedReportResolvedDataList GetDataList()
+        //{
+        //    return _dataListResolver;
+        //}
+
+
+    }
+}
