4f2bc8ba0b089fa11e0cbc2d6d501030293f5b95
Ali Sakr <VANRISE\ali.sakr>
2018-11-16 12:57:43 +0000

    adding MeasureStyleRuleManager
    git-tfs-id: [http://developmentvlan:8585/tfs/vanrise.collection]$/;C51104

---
+++ Vanrise/Code/Vanrise.Analytic.Entities/MeasureStyleRuleEditorRuntime.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Vanrise.Analytic.Entities
+{
+    public class MeasureStyleRuleEditorRuntime
+    {
+        public List<MeasureStyleRuleDetail> MeasureStyleRuleDetails { get; set; }
+    }
+    public enum StyleCodeEnum // to be removed when the reference will be added
+    {
+        [Description("Red")]
+        Red = 0,
+
+        [Description("Green")]
+        Green = 1,
+
+        [Description("Yellow")]
+        Yellow = 2,
+
+        [Description("Blue")]
+        Blue = 3,
+
+        [Description("Red Text Value")]
+        RedTextValue = 4,
+
+        [Description("Green Text Value")]
+        GreenTextValue = 5
+    }
+}
