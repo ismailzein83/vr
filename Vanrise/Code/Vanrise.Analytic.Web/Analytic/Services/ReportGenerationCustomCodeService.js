c0040ec211671204ecf02a996f095a0188915995
yara.bousalman <yara.bousalman@vanrise.tfs.local>
2018-11-16 15:19:15 +0000

    Fixing ReportGenerationCustomCodeService method
    git-tfs-id: [http://developmentvlan:8585/tfs/vanrise.collection]$/;C51110

--- Vanrise/Code/Vanrise.Analytic.Web/Analytic/Services/ReportGenerationCustomCodeService.js
+++ Vanrise/Code/Vanrise.Analytic.Web/Analytic/Services/ReportGenerationCustomCodeService.js
@@ -1,14 +1,15 @@
-﻿})(appControllers); (function (appControllers) {
+﻿(function (appControllers) {
     'use stict';
     ReportGenerationCustomCodeService.$inject = ['VRModalService'];
     function ReportGenerationCustomCodeService(VRModalService) {
-        function showCustomCodeCompilationErrors(errorMessages) {
+        function showCustomCodeCompilationErrors(errorMessages, context) {
             var modalSettings = {};
             modalSettings.onScopeReady = function (modalScope) {
             };
 
             var modalParameters = {
-                errorMessages: errorMessages
+                errorMessages: errorMessages,
+                context: context
             };
             VRModalService.showModal('/Client/Modules/Analytic/Directives/VRReportGeneration/Templates/CustomCodeCompilationErrorsEditorTemplate.html', modalParameters, modalSettings);
         }
