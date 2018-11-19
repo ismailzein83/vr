b1aaf6178ebe71927519f3053eca5846eaf3f07a
Ismail Zein <VANRISE\ismail.zein>
2018-11-19 10:35:52 +0000

    integrated Vanrise.Web.Host with VRWebBundle
    git-tfs-id: [http://developmentvlan:8585/tfs/vanrise.collection]$/;C51134

--- Vanrise/Code/Vanrise.Web.Host/Global.asax.cs
+++ Vanrise/Code/Vanrise.Web.Host/Global.asax.cs
@@ -11,24 +11,5 @@ namespace Vanrise.Web.Host
 {
     public class WebApiApplication : Vanrise.Web.WebApiApplication
     {
-        protected override void RegisterWebAPI()
-        {
-            GlobalConfiguration.Configure(WebApiConfig.Register);
-        }
-
-        public override void RegisterGlobalFilters()
-        {
-            FilterConfig.RegisterGlobalFilters(GlobalFilters.Filters);
-        }
-
-        public override void RegisterRoutes()
-        {
-            RouteConfig.RegisterRoutes(RouteTable.Routes);
-        }
-
-        public override void RegisterBundles()
-        {
-            BundleConfig.RegisterBundles(BundleTable.Bundles);
-        }
     }
 }
