41b0f786bb8ebc75e090bc6c95ff4ce991c3e5a4
Wissam Ajamy <VANRISE\wissam.ajamy>
2018-11-26 14:22:29 +0000

    Add SQL.NetworkRental
    git-tfs-id: [http://developmentvlan:8585/tfs/vanrise.collection]$/;C51271

---
+++ Retail/Code/SQL.NetworkRental/NetworkRentalManager/User Defined Types/ProductType.sql
@@ -0,0 +1,6 @@
+﻿CREATE TYPE [NetworkRentalManager].[ProductType] AS TABLE (
+    [ID]          BIGINT         NULL,
+    [Name]        NVARCHAR (255) NULL,
+    [ProductType] INT            NULL,
+    [CreatedTime] DATETIME       NULL);
+
