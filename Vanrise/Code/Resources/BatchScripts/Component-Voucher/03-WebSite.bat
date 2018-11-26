412a24a754033ae9ef584597e1e8c788ae954fda
Wissam Ajamy <VANRISE\wissam.ajamy>
2018-11-26 16:04:24 +0000

    update web module for Voucher
    git-tfs-id: [http://developmentvlan:8585/tfs/vanrise.collection]$/;C51290

--- Vanrise/Code/Resources/BatchScripts/Component-Voucher/03-WebSite.bat
+++ Vanrise/Code/Resources/BatchScripts/Component-Voucher/03-WebSite.bat
@@ -6,3 +6,10 @@ Set PARSEARG="eol=; tokens=1,2,3,4* delims=/, "
 For /F %PARSEARG% %%i in (%CURRDATE%) Do SET YYYYMMDD=%%l%%j%%k
 
 ECHO Component-Voucher WebSite
+
+xcopy "C:\TFS\Vanrise\Code\Output\Vanrise.Voucher.Web.dll" 		/y /v /z /i /Q /R  		"\\192.168.110.185\Fixes\WebSite\Component-Voucher\%YYYYMMDD%\bin\"
+xcopy "C:\TFS\Vanrise\Code\Output\Vanrise.Voucher.Entities.dll" /y /v /z /i /Q /R  		"\\192.168.110.185\Fixes\WebSite\Component-Voucher\%YYYYMMDD%\bin\"
+xcopy "C:\TFS\Vanrise\Code\Output\Vanrise.Voucher.Business.dll" /y /v /z /i /Q /R  		"\\192.168.110.185\Fixes\WebSite\Component-Voucher\%YYYYMMDD%\bin\"
+xcopy "C:\TFS\Vanrise\Code\Output\Vanrise.Voucher.Data.dll" 	/y /v /z /i /Q /R  		"\\192.168.110.185\Fixes\WebSite\Component-Voucher\%YYYYMMDD%\bin\"
+xcopy "C:\TFS\Vanrise\Code\Output\Vanrise.Voucher.Data.SQL.dll" /y /v /z /i /Q /R  		"\\192.168.110.185\Fixes\WebSite\Component-Voucher\%YYYYMMDD%\bin\"
+xcopy "C:\TFS\Vanrise\Code\Vanrise.Voucher.Web\VR_Voucher" /S /E /R /y /v /i /z /Q		"\\192.168.110.185\Fixes\WebSite\Component-Voucher\%YYYYMMDD%\Client\Modules\VR_Voucher\"
