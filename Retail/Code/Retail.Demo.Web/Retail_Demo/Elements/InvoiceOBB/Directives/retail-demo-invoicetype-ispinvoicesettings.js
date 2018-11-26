bd2e2f9490de985bbdd551ca05caffa78798a2ca
Samer Haidar <VANRISE\samer.haidar>
2018-11-26 18:33:46 +0000

    adding new directive
    git-tfs-id: [http://developmentvlan:8585/tfs/vanrise.collection]$/;C51297

---
+++ Retail/Code/Retail.Demo.Web/Retail_Demo/Elements/InvoiceOBB/Directives/retail-demo-invoicetype-ispinvoicesettings.js
@@ -0,0 +1,92 @@
+﻿"use strict";
+
+app.directive("retailDemoInvoicetypeIspinvoicesettings", ["UtilsService", "VRNotificationService", "VRUIUtilsService",
+    function (UtilsService, VRNotificationService, VRUIUtilsService) {
+
+        var directiveDefinitionObject = {
+
+            restrict: "E",
+            scope:
+            {
+                onReady: "=",
+            },
+            controller: function ($scope, $element, $attrs) {
+                var ctrl = this;
+
+                var ctor = new InterconnectInvoiceSettings($scope, ctrl, $attrs);
+                ctor.initializeController();
+            },
+            controllerAs: "ctrl",
+            bindToController: true,
+            compile: function (element, attrs) {
+
+            },
+            templateUrl: "/Client/Modules/Retail_Demo/Elements/InvoiceOBB/Directives/Templates/ISPInvoiceSettingsTemplate.html"
+
+        };
+
+        function InterconnectInvoiceSettings($scope, ctrl, $attrs) {
+            this.initializeController = initializeController;
+
+            var beDefinitionSelectorAPI;
+            var beDefinitionSelectorPromiseDeferred = UtilsService.createPromiseDeferred();
+
+            function initializeController() {
+                $scope.scopeModel = {};
+                $scope.scopeModel.invoiceType = UtilsService.getArrayEnum(retail_Interconnect_InvoiceType);
+                $scope.scopeModel.onBusinessEntityDefinitionSelectorReady = function (api) {
+                    beDefinitionSelectorAPI = api;
+                    beDefinitionSelectorPromiseDeferred.resolve();
+                };
+
+                defineAPI();
+            }
+
+            function defineAPI() {
+                var api = {};
+
+                api.load = function (payload) {
+                    var promises = [];
+                    if (payload != undefined && payload.extendedSettingsEntity != undefined) {
+                        $scope.scopeModel.selectedInvoiceType = UtilsService.getItemByVal($scope.scopeModel.invoiceType, payload.extendedSettingsEntity.Type, "value");
+                    }
+                    promises.push(getBusinessEntityDefinitionSelectorLoadPromise());
+
+                    function getBusinessEntityDefinitionSelectorLoadPromise() {
+                        var businessEntityDefinitionSelectorLoadDeferred = UtilsService.createPromiseDeferred();
+
+                        beDefinitionSelectorPromiseDeferred.promise.then(function () {
+                            var selectorPayload = {
+                                filter: {
+                                    Filters: [{
+                                        $type: "Retail.BusinessEntity.Business.AccountBEDefinitionFilter, Retail.BusinessEntity.Business"
+                                    }]
+                                }
+                            };
+                            if (payload != undefined && payload.extendedSettingsEntity != undefined) {
+                                selectorPayload.selectedIds = payload.extendedSettingsEntity.AccountBEDefinitionId;
+                            }
+                            VRUIUtilsService.callDirectiveLoad(beDefinitionSelectorAPI, selectorPayload, businessEntityDefinitionSelectorLoadDeferred);
+                        });
+                        return businessEntityDefinitionSelectorLoadDeferred.promise;
+                    }
+
+                    return UtilsService.waitMultiplePromises(promises);
+                };
+
+
+                api.getData = function () {
+                    return {
+                        $type: "Retail.Demo.Business.ISPInvoiceSettings, Retail.Demo.Business",
+                        AccountBEDefinitionId: beDefinitionSelectorAPI.getSelectedIds(),
+                    };
+                };
+
+                if (ctrl.onReady != null)
+                    ctrl.onReady(api);
+            }
+        }
+
+        return directiveDefinitionObject;
+    }
+]);
