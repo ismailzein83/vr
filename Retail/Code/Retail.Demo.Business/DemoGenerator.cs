1e53633f60f550dc75f3ebad7a3c23de3060769c
Naim Saad <VANRISE\naim.saad>
2018-11-26 15:13:41 +0000

    Implementing OrdersDefinition
    git-tfs-id: [http://developmentvlan:8585/tfs/vanrise.collection]$/;C51280

---
+++ Retail/Code/Retail.Demo.Business/DemoGenerator.cs
@@ -0,0 +1,22 @@
+﻿
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Vanrise.Invoice.Entities;
+
+namespace Retail.Demo.Business
+{
+    public class DemoInvoiceGenerator : InvoiceGenerator
+
+    {
+        public override void GenerateInvoice(IInvoiceGenerationContext context)
+        {
+        //   List<GeneratedInvoiceItemSet> generatedInvoiceItemSets = BuildGeneratedInvoiceItemSet();
+
+        }
+    }
+    
+
+}
---
+++ Retail/Code/Retail.Demo.Business/NewOrders.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Retail.Demo.Business
+{
+    public class NewOrders
+    {
+        String ProductName { get; set; }
+        int NoOfOrders { get; set; }
+        Decimal TotalTarrif { get; set; }
+    }
+}
