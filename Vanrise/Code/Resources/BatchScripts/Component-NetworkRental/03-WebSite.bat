1f545cb23ad1e9900221fda48cc93d23e4fa857f
Wissam Ajamy <VANRISE\wissam.ajamy>
2018-11-27 13:22:50 +0000

    Adding Retail_Demo Module
    git-tfs-id: [http://developmentvlan:8585/tfs/vanrise.collection]$/;C51345

--- Vanrise/Code/Resources/BatchScripts/Component-NetworkRental/03-WebSite.bat
+++ Vanrise/Code/Resources/BatchScripts/Component-NetworkRental/03-WebSite.bat
@@ -6,3 +6,5 @@ Set PARSEARG="eol=; tokens=1,2,3,4* delims=/, "
 For /F %PARSEARG% %%i in (%CURRDATE%) Do SET YYYYMMDD=%%l%%j%%k
 
 ECHO Component-NetworkRental WebSite
+
+xcopy "C:\TFS\Retail\Code\Retail.Demo.Web\Retail_Demo" /S /E /R /y /v /i /z /Q										"\\192.168.110.185\Fixes\WebSite\Component-NetworkRental\%YYYYMMDD%\Client\Modules\Retail_Demo\"
