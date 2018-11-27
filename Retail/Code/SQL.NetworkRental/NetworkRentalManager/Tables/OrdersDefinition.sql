83604cc71cecfc827877cee99ac5924e3be85efe
Wissam Ajamy <VANRISE\wissam.ajamy>
2018-11-27 09:54:07 +0000

    update structure
    git-tfs-id: [http://developmentvlan:8585/tfs/vanrise.collection]$/;C51322

--- Retail/Code/SQL.NetworkRental/NetworkRentalManager/Tables/OrdersDefinition.sql
+++ Retail/Code/SQL.NetworkRental/NetworkRentalManager/Tables/OrdersDefinition.sql
@@ -1,14 +1,24 @@
 ﻿CREATE TABLE [NetworkRentalManager].[OrdersDefinition] (
-    [ID]               BIGINT   IDENTITY (1, 1) NOT NULL,
-    [OrderType]        INT      NULL,
-    [StatusId]         INT      NULL,
-    [AccountId]        BIGINT   NULL,
-    [ProductId]        INT      NULL,
-    [CreatedTime]      DATETIME CONSTRAINT [DF_OrdersDefinition_CreatedTime] DEFAULT (getdate()) NULL,
-    [CreatedBy]        INT      NULL,
-    [LastModifiedTime] DATETIME NULL,
-    [LastModifiedBy]   INT      NULL,
-    [SubContractorId]  BIGINT   NULL,
+    [ID]                     BIGINT          IDENTITY (1, 1) NOT NULL,
+    [OrderType]              INT             NULL,
+    [StatusId]               INT             NULL,
+    [AccountId]              BIGINT          NULL,
+    [ProductId]              INT             NULL,
+    [CreatedTime]            DATETIME        CONSTRAINT [DF_OrdersDefinition_CreatedTime] DEFAULT (getdate()) NULL,
+    [CreatedBy]              INT             NULL,
+    [LastModifiedTime]       DATETIME        NULL,
+    [LastModifiedBy]         INT             NULL,
+    [SubContractorId]        BIGINT          NULL,
+    [ServiceID]              NVARCHAR (255)  NULL,
+    [ContractTenure]         INT             NULL,
+    [BillStartDate]          DATETIME        NULL,
+    [BillingFrequency]       NVARCHAR (255)  NULL,
+    [InstallationDate]       DATETIME        NULL,
+    [MRC]                    DECIMAL (20, 4) NULL,
+    [NRC]                    DECIMAL (20, 4) NULL,
+    [OneTimeHardwareCharges] DECIMAL (20, 4) NULL,
     CONSTRAINT [PK_OrdersDefinition] PRIMARY KEY CLUSTERED ([ID] ASC)
 );
+
+
 
--- Retail/Code/SQL.NetworkRental/NetworkRentalManager/User Defined Types/OrdersDefinitionType.sql
+++ Retail/Code/SQL.NetworkRental/NetworkRentalManager/User Defined Types/OrdersDefinitionType.sql
@@ -1,12 +1,22 @@
 ﻿CREATE TYPE [NetworkRentalManager].[OrdersDefinitionType] AS TABLE (
-    [ID]               BIGINT   NULL,
-    [OrderType]        INT      NULL,
-    [StatusId]         INT      NULL,
-    [AccountId]        BIGINT   NULL,
-    [ProductId]        INT      NULL,
-    [CreatedTime]      DATETIME NULL,
-    [CreatedBy]        INT      NULL,
-    [LastModifiedTime] DATETIME NULL,
-    [LastModifiedBy]   INT      NULL,
-    [SubContractorId]  BIGINT   NULL);
+    [ID]                     BIGINT          NULL,
+    [OrderType]              INT             NULL,
+    [StatusId]               INT             NULL,
+    [AccountId]              BIGINT          NULL,
+    [ProductId]              INT             NULL,
+    [CreatedTime]            DATETIME        NULL,
+    [CreatedBy]              INT             NULL,
+    [LastModifiedTime]       DATETIME        NULL,
+    [LastModifiedBy]         INT             NULL,
+    [SubContractorId]        BIGINT          NULL,
+    [ServiceID]              NVARCHAR (255)  NULL,
+    [ContractTenure]         INT             NULL,
+    [BillStartDate]          DATETIME        NULL,
+    [BillingFrequency]       NVARCHAR (255)  NULL,
+    [InstallationDate]       DATETIME        NULL,
+    [MRC]                    DECIMAL (20, 4) NULL,
+    [NRC]                    DECIMAL (20, 4) NULL,
+    [OneTimeHardwareCharges] DECIMAL (20, 4) NULL);
+
+
 
