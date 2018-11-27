e79f589602e3952d88530b857d9509f274743185
Naim Saad <VANRISE\naim.saad>
2018-11-27 07:14:48 +0000

    Implementing Demo DataManager
    git-tfs-id: [http://developmentvlan:8585/tfs/vanrise.collection]$/;C51305

--- Retail/Code/Retail.Demo.Data.SQL/DemoDataManager.cs
+++ Retail/Code/Retail.Demo.Data.SQL/DemoDataManager.cs
@@ -60,7 +60,8 @@ namespace Retail.Demo.Data.SQL
 
             return new ActiveServices()
             {
-                ProductName = reader["Name"] as string,
+                ProductName = reader["ProductName"] as string,
+                NoOfOrders = (int)reader["NoOfOrders"],
                 Cycle = reader["BillingFrequency"] as string,
                 TotalTarrif = (decimal)reader["MRC"]
             };
@@ -70,21 +71,22 @@ namespace Retail.Demo.Data.SQL
 
             return new NewOrders()
             {
-                ProductName = reader["Name"] as string,
+                ProductName = reader["ProductName"] as string,
+                NoOfOrders = (int)reader["NoOfOrders"],
                 TotalTarrif = (decimal)reader["NRC"]
             };
         }
         #region Queries
 
         private StringBuilder query_GetActiveServices = new StringBuilder(@"use[Retail_Dev_ISP];
-                                                                            select Pr.Name  as ProductName, od.BillingFrequency, Sum(od.MRC) as MRC
+                                                                            select Pr.Name  as ProductName,count(Pr.Name) as NoOfOrders, od.BillingFrequency, Sum(od.MRC) as MRC
                                                                             FROM[Retail_Dev_ISP].[NetworkRentalManager].[OrdersDefinition] as od
                                                                             Join[Retail_Dev_ISP].[NetworkRentalManager].[Product] as Pr on OD.ProductId=pr.ID
                                                                             Group by Pr.Name,od.BillingFrequency
                                                                             ");
 
         private StringBuilder query_GetNewOrders = new StringBuilder(@"use[Retail_Dev_ISP];
-                                                                       select Pr.Name  as ProductName, Sum(od.MRC) as NRC
+                                                                       select Pr.Name  as ProductName,count(Pr.Name) as NoOfOrders, Sum(od.MRC) as NRC
                                                                        FROM[Retail_Dev_ISP].[NetworkRentalManager].[OrdersDefinition] as od
                                                                        JOin[Retail_Dev_ISP].[NetworkRentalManager].[Product] as Pr on OD.ProductId=pr.ID
                                                                        Group by Pr.Name,od.BillingFrequency
