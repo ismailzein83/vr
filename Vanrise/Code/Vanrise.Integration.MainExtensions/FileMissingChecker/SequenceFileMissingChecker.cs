4d2e9c0bf5238eb071e17b6389bf5b42eef9656f
Ali Slayman <VANRISE\ali.slayman>
2018-11-29 07:54:37 +0000

    Renaming SequenceFileDataSourceNaming to SequenceFileMissingChecker
    git-tfs-id: [http://developmentvlan:8585/tfs/vanrise.collection]$/;C51408

--- Vanrise/Code/Vanrise.Integration.MainExtensions/FileMissingChecker/SequenceFileMissingChecker.cs
+++ Vanrise/Code/Vanrise.Integration.MainExtensions/FileMissingChecker/SequenceFileMissingChecker.cs
@@ -7,9 +7,9 @@ using Vanrise.Integration.Entities;
 
 namespace Vanrise.Integration.MainExtensions.FileMissingChecker
 {
-    public class SequenceFileDataSourceNaming : FileMissingCheckerSettings
+    public class SequenceFileMissingChecker : FileMissingCheckerSettings
     {
-        public override Guid ConfigId => throw new NotImplementedException();
+        public override Guid ConfigId { get { return new Guid("FA37168F-25B8-44B2-8D27-CA0DD3E3265E"); } }
 
         public override void CheckMissingFiles(ICheckMissingFilesContext context)
         {
