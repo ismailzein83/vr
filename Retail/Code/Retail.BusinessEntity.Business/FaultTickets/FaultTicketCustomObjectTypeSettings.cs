d5ba8a08ecaf7b1269d98255cea5b47de29f7a78
yara.bousalman <yara.bousalman@vanrise.tfs.local>
2018-11-30 10:46:23 +0000

    Fixing Fault Ticket custom object for history grid
    git-tfs-id: [http://developmentvlan:8585/tfs/vanrise.collection]$/;C51464

--- Retail/Code/Retail.BusinessEntity.Business/FaultTickets/FaultTicketCustomObjectTypeSettings.cs
+++ Retail/Code/Retail.BusinessEntity.Business/FaultTickets/FaultTicketCustomObjectTypeSettings.cs
@@ -17,11 +17,60 @@ namespace Retail.BusinessEntity.Business
 
         public override string GetDescription(IFieldCustomObjectTypeSettingsContext context)
         {
+            var valueObject = context.FieldValue as List<FaultTicketDescriptionSettingDetails>;
+            if (valueObject == null)
+                valueObject = Utilities.ConvertJsonToList<FaultTicketDescriptionSettingDetails>(context.FieldValue);
+            if (valueObject != null)
+            {
+                StringBuilder description = new StringBuilder();
+
+                foreach (var value in valueObject)
+                {
+                    if (description.Length > 0)
+                        description.Append(", ");
+                    description.Append(value.TicketReasonDescription);
+                    description.Append(" ");
+                    description.Append(value.Type);
+                    if(value.Note!=null)
+                    {
+                        description.Append(" ");
+                        description.Append(value.Note);
+                    }
+                }
+                return description.ToString();
+            }
             return null;
         }
 
         public override bool AreEqual(Object newValue, Object oldValue)
         {
+            var oldValueObject = oldValue as List<FaultTicketDescriptionSettingDetails>;
+            var newValueObject = newValue as List<FaultTicketDescriptionSettingDetails>;
+
+            if (oldValueObject == null)
+                oldValueObject = Utilities.ConvertJsonToList<FaultTicketDescriptionSettingDetails>(oldValue);
+            if (newValueObject == null)
+                newValueObject = Utilities.ConvertJsonToList<FaultTicketDescriptionSettingDetails>(newValue);
+
+            if ((oldValueObject == null && newValueObject == null) || (oldValueObject.Count == 0 && newValueObject.Count == 0))
+                return true;
+            if (oldValueObject == null || oldValueObject.Count == 0 || newValueObject == null || newValueObject.Count == 0)
+                return false;
+
+            foreach (var oldValueItem in oldValueObject)
+            {
+                var newValueItem = newValueObject.FindRecord(x => x.TicketReasonId == oldValueItem.TicketReasonId && x.TicketReasonDescription == oldValueItem.TicketReasonDescription && x.Type == oldValueItem.Type  && x.Note == oldValueItem.Note);
+                if (newValueItem == null)
+                    return false;
+            }
+
+            foreach (var newValueItem in newValueObject)
+            {
+                var oldValueItem = oldValueObject.FindRecord(x => x.TicketReasonId == newValueItem.TicketReasonId && x.TicketReasonDescription == newValueItem.TicketReasonDescription && x.Type == newValueItem.Type && x.Note == newValueItem.Note);
+                if (oldValueItem == null)
+                    return false;
+            }
+
             return true;
         }
 
