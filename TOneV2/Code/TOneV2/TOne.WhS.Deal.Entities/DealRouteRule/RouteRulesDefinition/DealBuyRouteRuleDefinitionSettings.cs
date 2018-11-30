2dde9cb1f642ad380a16abf94deb54d26396317f
Fatima Al Heck <VANRISE\fatima.alheck>
2018-11-30 11:32:01 +0000

    change component type from SwapDealBuyRouteRuleDefinitionSettings to DealBuyRouteRuleDefinitionSettings
    git-tfs-id: [http://developmentvlan:8585/tfs/vanrise.collection]$/;C51468

---
+++ TOneV2/Code/TOneV2/TOne.WhS.Deal.Entities/DealRouteRule/RouteRulesDefinition/DealBuyRouteRuleDefinitionSettings.cs
@@ -0,0 +1,9 @@
+﻿using System;
+
+namespace TOne.WhS.Deal.Entities
+{
+    public class DealBuyRouteRuleDefinitionSettings : Vanrise.Entities.VRRuleDefinitionExtendedSettings
+    {
+        public override Guid ConfigId { get { return new Guid("9B2DB188-650C-4F19-8BCD-15BECC653E46"); } }
+    }
+}
