89d2bc4e8a14f2c6a313c91aee856d025083c361
Ali Slayman <VANRISE\ali.slayman>
2018-11-30 15:34:17 +0000

    Adding FileDataSourceDefinitionInfo and FileDataSourceDefinitionInfoFilter
    git-tfs-id: [http://developmentvlan:8585/tfs/vanrise.collection]$/;C51483

---
+++ Vanrise/Code/Vanrise.Integration.Entities/FileDataSourceDefinition/FileDataSourceDefinitionInfo.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Vanrise.Integration.Entities
+{
+    public class FileDataSourceDefinitionInfo
+    {
+        public Guid FileDataSourceDefinitionId { get; set; }
+
+        public string Name { get; set; }
+    }
+}
