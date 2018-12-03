a6c614cecbb27fd2769b7700f5aca750dc9a2bd6
Ali Atoui <VANRISE\ali.atoui>
2018-12-03 09:15:11 +0000

    Editing AliAtouiTask
    git-tfs-id: [http://developmentvlan:8585/tfs/vanrise.collection]$/;C51494

--- Mediation/Code/Mediation.Runtime/Tasks/AliAtouiTask.cs
+++ Mediation/Code/Mediation.Runtime/Tasks/AliAtouiTask.cs
@@ -6,6 +6,8 @@ using Vanrise.Common.Business;
 using Vanrise.Queueing;
 using Vanrise.Runtime;
 using Mediation.Runtime.DataParser;
+using Vanrise.Runtime.Entities;
+using Vanrise.DataParser.Business;
 
 namespace Mediation.Runtime.Tasks
 {
@@ -13,50 +15,71 @@ namespace Mediation.Runtime.Tasks
     {
         public void Execute()
         {
-            //var runtimeServices = new List<RuntimeService>();
+            #region DataParserTesterTask
+            DataParserTesterTask.DataParserTesterTask_Main();
+            #endregion
 
-            //BusinessProcessService bpService = new BusinessProcessService() { Interval = new TimeSpan(0, 0, 2) };
-            //runtimeServices.Add(bpService);
+            #region RuntimeTask
+            //RuntimeTask.RuntimeTask_Main();
+            #endregion
+        }
+    }
 
-            //QueueRegulatorRuntimeService queueRegulatorService = new QueueRegulatorRuntimeService() { Interval = new TimeSpan(0, 0, 2) };
-            //runtimeServices.Add(queueRegulatorService);
-
-            //QueueActivationRuntimeService queueActivationService = new QueueActivationRuntimeService() { Interval = new TimeSpan(0, 0, 2) };
-            //runtimeServices.Add(queueActivationService);
-
-            //SummaryQueueActivationRuntimeService summaryQueueActivationService = new SummaryQueueActivationRuntimeService() { Interval = new TimeSpan(0, 0, 2) };
-            //runtimeServices.Add(summaryQueueActivationService);
-
-            //SchedulerService schedulerService = new SchedulerService() { Interval = new TimeSpan(0, 0, 1) };
-            //runtimeServices.Add(schedulerService);
-
-            //Vanrise.Common.Business.BigDataRuntimeService bigDataService = new Vanrise.Common.Business.BigDataRuntimeService { Interval = new TimeSpan(0, 0, 2) };
-            //runtimeServices.Add(bigDataService);
-
-            //Vanrise.Integration.Business.DataSourceRuntimeService dsRuntimeService = new Vanrise.Integration.Business.DataSourceRuntimeService { Interval = new TimeSpan(0, 0, 2) };
-            //runtimeServices.Add(dsRuntimeService);
-
-            //BPRegulatorRuntimeService bpRegulatorRuntimeService = new BPRegulatorRuntimeService { Interval = new TimeSpan(0, 0, 2) };
-            //runtimeServices.Add(bpRegulatorRuntimeService);
-
-            //CachingRuntimeService cachingRuntimeService = new CachingRuntimeService { Interval = new TimeSpan(0, 0, 2) };
-            //runtimeServices.Add(cachingRuntimeService);
-
-            //CachingDistributorRuntimeService cachingDistributorRuntimeService = new CachingDistributorRuntimeService { Interval = new TimeSpan(0, 0, 2) };
-            //runtimeServices.Add(cachingDistributorRuntimeService);
-
-            //DataGroupingExecutorRuntimeService dataGroupingExecutorRuntimeService = new Vanrise.Common.Business.DataGroupingExecutorRuntimeService() { Interval = new TimeSpan(0, 0, 2) };
-            //runtimeServices.Add(dataGroupingExecutorRuntimeService);
-
-            //DataGroupingDistributorRuntimeService dataGroupingDistributorRuntimeService = new Vanrise.Common.Business.DataGroupingDistributorRuntimeService() { Interval = new TimeSpan(0, 0, 2) };
-            //runtimeServices.Add(dataGroupingDistributorRuntimeService);
-
-            //RuntimeHost host = new RuntimeHost(runtimeServices);
-            //host.Start();
-            //Console.ReadKey();
-
+    public class DataParserTesterTask
+    {
+        public static void DataParserTesterTask_Main()
+        {
             DataParserTester tester = new DataParserTester();
             tester.GenerateMediationSettings();
+        }
+    }
+
+    public class RuntimeTask
+    {
+        public static void RuntimeTask_Main()
+        {
+            var runtimeServices = new List<RuntimeService>();
+
+            SchedulerService schedulerService = new SchedulerService() { Interval = new TimeSpan(0, 0, 1) };
+            runtimeServices.Add(schedulerService);
+
+            BPRegulatorRuntimeService bpRegulatorRuntimeService = new BPRegulatorRuntimeService { Interval = new TimeSpan(0, 0, 2) };
+            runtimeServices.Add(bpRegulatorRuntimeService);
+
+            BusinessProcessService bpService = new BusinessProcessService() { Interval = new TimeSpan(0, 0, 2) };
+            runtimeServices.Add(bpService);
+
+            QueueRegulatorRuntimeService queueRegulatorService = new QueueRegulatorRuntimeService() { Interval = new TimeSpan(0, 0, 2) };
+            runtimeServices.Add(queueRegulatorService);
+
+            QueueActivationRuntimeService queueActivationService = new QueueActivationRuntimeService() { Interval = new TimeSpan(0, 0, 2) };
+            runtimeServices.Add(queueActivationService);
+
+            SummaryQueueActivationRuntimeService summaryQueueActivationService = new SummaryQueueActivationRuntimeService() { Interval = new TimeSpan(0, 0, 2) };
+            runtimeServices.Add(summaryQueueActivationService);
+
+            Vanrise.Integration.Business.DataSourceRuntimeService dsRuntimeService = new Vanrise.Integration.Business.DataSourceRuntimeService { Interval = new TimeSpan(0, 0, 2) };
+            runtimeServices.Add(dsRuntimeService);
+
+            Vanrise.Common.Business.BigDataRuntimeService bigDataService = new Vanrise.Common.Business.BigDataRuntimeService { Interval = new TimeSpan(0, 0, 2) };
+            runtimeServices.Add(bigDataService);
+
+            CachingRuntimeService cachingRuntimeService = new CachingRuntimeService { Interval = new TimeSpan(0, 0, 2) };
+            runtimeServices.Add(cachingRuntimeService);
+
+            CachingDistributorRuntimeService cachingDistributorRuntimeService = new CachingDistributorRuntimeService { Interval = new TimeSpan(0, 0, 2) };
+            runtimeServices.Add(cachingDistributorRuntimeService);
+
+            DataGroupingExecutorRuntimeService dataGroupingExecutorRuntimeService = new Vanrise.Common.Business.DataGroupingExecutorRuntimeService() { Interval = new TimeSpan(0, 0, 2) };
+            runtimeServices.Add(dataGroupingExecutorRuntimeService);
+
+            DataGroupingDistributorRuntimeService dataGroupingDistributorRuntimeService = new Vanrise.Common.Business.DataGroupingDistributorRuntimeService() { Interval = new TimeSpan(0, 0, 2) };
+            runtimeServices.Add(dataGroupingDistributorRuntimeService);
+
+            RuntimeHost host = new RuntimeHost(runtimeServices);
+            host.Start();
+
+            Console.ReadKey();
         }
     }
 }
