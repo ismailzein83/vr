b91966b5a839d32b501baf76785bf7a0ce7148ec
Samer Haidar <VANRISE\samer.haidar>
2018-12-03 14:16:40 +0000

    Adding VRDynamicCode
    git-tfs-id: [http://developmentvlan:8585/tfs/vanrise.collection]$/;C51510

--- Vanrise/Code/Vanrise.Entities/VRDynamicAPI/VRDynamicAPIQuery.cs
+++ Vanrise/Code/Vanrise.Entities/VRDynamicAPI/VRDynamicAPIQuery.cs
@@ -11,4 +11,35 @@ namespace Vanrise.Entities
         public string Name { get; set; }
         public int? VRDynamicAPIModuleId { get; set; }
     }
+
+
+
+    public class VRDynamicCode
+    {
+        public Guid VRDynamicCodeId { get; set; }
+        public string Title { get; set; }
+        public VRDynamicCodeSettings Settings { get; set; }
+    }
+    public abstract class VRDynamicCodeSettings
+    {
+        public abstract Guid ConfigId { get; }
+        public abstract string Generate(IVRDynamicCodeSettingsContext context);
+     }
+    public interface IVRDynamicCodeSettingsContext
+    {
+
+    }
+    public class VRDynamicCodeSettingsContext: IVRDynamicCodeSettingsContext
+    {
+
+    }
+    public class CustomCodeDynamicCodeSettings : VRDynamicCodeSettings
+    {
+        public override Guid ConfigId { get { return new Guid("BDD289DF-573C-44A1-9A95-D0DE2ED9DD71"); } }
+        public string CustomCode { get; set; }
+        public override string Generate(IVRDynamicCodeSettingsContext context)
+        {
+            return CustomCode;
+        }
+    }
 }
