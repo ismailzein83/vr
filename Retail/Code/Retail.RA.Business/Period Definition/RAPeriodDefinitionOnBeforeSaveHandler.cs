ef64c3faa9bec1392575323bf105a106935b74e4
Ali Sakr <VANRISE\ali.sakr>
2018-12-03 11:56:29 +0000

    adding onBeforeSave Class
    git-tfs-id: [http://developmentvlan:8585/tfs/vanrise.collection]$/;C51502

--- Retail/Code/Retail.RA.Business/Period Definition/RAPeriodDefinitionOnBeforeSaveHandler.cs
+++ Retail/Code/Retail.RA.Business/Period Definition/RAPeriodDefinitionOnBeforeSaveHandler.cs
@@ -8,19 +8,18 @@ using Vanrise.GenericData.Business;
 
 namespace Retail.RA.Business
 {
-    public class RAPeriodDefinitionOnBeforeSaveHandler: GenericBEOnBeforeInsertHandler
+    public class RAPeriodDefinitionOnBeforeSaveHandler : GenericBEOnBeforeInsertHandler
     {
         public override Guid ConfigId { get { return new Guid("56EE0B5C-DA52-4B8C-B205-FE536F969659"); } }
         public override void Execute(IGenericBEOnBeforeInsertHandlerContext context)
         {
-            //context.ThrowIfNull("context");
-            //context.GenericBusinessEntity.ThrowIfNull("context.GenericBusinessEntity");
-            //context.GenericBusinessEntity.FieldValues.ThrowIfNull("context.GenericBusinessEntity.FieldValues");
-            //var from = context.GenericBusinessEntity.FieldValues.GetRecord("From");
-            //var to = context.GenericBusinessEntity.FieldValues.GetRecord("To");
-            //var repeat = context.GenericBusinessEntity.FieldValues.GetRecord("Repeat");
-
-
+            context.ThrowIfNull("context");
+            context.GenericBusinessEntity.ThrowIfNull("context.GenericBusinessEntity");
+            context.GenericBusinessEntity.FieldValues.ThrowIfNull("context.GenericBusinessEntity.FieldValues");
+            var from = context.GenericBusinessEntity.FieldValues.GetRecord("From");
+            var to = context.GenericBusinessEntity.FieldValues.GetRecord("To");
+            var name = string.Join("-", ((DateTime)from).ToString("dd/MM/yyyy"), ((DateTime)from).ToString("dd/MM/yyyy"));
+            context.GenericBusinessEntity.FieldValues.Add("Name", name);
         }
     }
 }
