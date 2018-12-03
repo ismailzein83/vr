e4ab47ce247f9c9ca6dedc924d37e13579431458
Wissam Ajamy <VANRISE\wissam.ajamy>
2018-12-03 14:48:29 +0000

    remove update script on connections
    git-tfs-id: [http://developmentvlan:8585/tfs/vanrise.collection]$/;C51514

--- TOneV2/Code/TOneV2/SQL.TOneConfiguration/CarrierPortal.TOnePortal.PostDeployment.sql
+++ TOneV2/Code/TOneV2/SQL.TOneConfiguration/CarrierPortal.TOnePortal.PostDeployment.sql
@@ -321,9 +321,9 @@ as (select * from (values
 merge	[common].[Connection] as t
 using	cte_data as s
 on		1=1 and t.[ID] = s.[ID]
-when matched then
-	update set
-	[Name] = s.[Name],[Settings] = s.[Settings]
+--when matched then
+--	update set
+--	[Name] = s.[Name],[Settings] = s.[Settings]
 when not matched by target then
 	insert([ID],[Name],[Settings])
 	values(s.[ID],s.[Name],s.[Settings]);
