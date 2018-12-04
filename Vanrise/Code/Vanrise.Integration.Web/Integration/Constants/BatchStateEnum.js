4df8f0f7e9310a5bdce59b8a1d453b8b28c36613
Ali Atoui <VANRISE\ali.atoui>
2018-12-04 15:59:53 +0000

    Moving VR_Integration_BatchStateEnum to new js file
    git-tfs-id: [http://developmentvlan:8585/tfs/vanrise.collection]$/;C51550

---
+++ Vanrise/Code/Vanrise.Integration.Web/Integration/Constants/BatchStateEnum.js
@@ -0,0 +1,6 @@
+﻿app.constant('VR_Integration_BatchStateEnum', {
+    Normal: { value: 0, description: "Normal" },
+    Missing: { value: 1, description: "Missing" },
+    Delayed: { value: 2, description: "Delayed" },
+    Duplicated: { value: 3, description: "Duplicated" }
+});
