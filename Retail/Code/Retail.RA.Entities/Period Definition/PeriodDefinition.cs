bedf0ecdd5bd608f61f66eeb002a8ffa86d00734
Zeinab Mokdad <VANRISE\zeinab.mokdad>
2018-12-04 13:12:24 +0000

    addiing Retail.RA.Entities
    git-tfs-id: [http://developmentvlan:8585/tfs/vanrise.collection]$/;C51539

---
+++ Retail/Code/Retail.RA.Entities/Period Definition/PeriodDefinition.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Retail.RA.Entities
+{
+    public class PeriodDefinition
+    {
+        public int PeriodDefinitionId { get; set; }
+        public string Name { get; set; }
+        public DateTime FromDate { get; set; }
+        public DateTime ToDate { get; set; }
+    }
+}
