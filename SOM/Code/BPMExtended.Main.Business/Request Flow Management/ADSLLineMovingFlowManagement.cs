1d5565c12bcaaa30abd226fab433f016f04afe10
Ayman Zinji <VANRISE\ayman.zinji>
2018-12-04 09:47:39 +0000

    adsl line moving , adsl line termination flow management
    git-tfs-id: [http://developmentvlan:8585/tfs/vanrise.collection]$/;C51531

---
+++ SOM/Code/BPMExtended.Main.Business/Request Flow Management/ADSLLineMovingFlowManagement.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Terrasoft.Core;
+using Terrasoft.Core.Configuration;
+using Terrasoft.Core.DB;
+using Terrasoft.Core.Entities;
+using System.Web;
+
+namespace BPMExtended.Main.Business
+{
+    
+    public class ADSLLineMovingFlowManagement
+    {
+        public UserConnection BPM_UserConnection
+        {
+            get
+            {
+                return (UserConnection)HttpContext.Current.Session["UserConnection"];
+            }
+        }
+        string welcomeStep = "013B161F-FB2C-4072-82CC-60852FE53039";
+        string chooseContractStep = "BE540720-169E-456B-B0D0-6D243D5B7F82";
+        string printStep = "4F032C27-BBB9-420B-A064-6A88E7E532A0";
+        string DSLAMPortStep = "0E26138A-89CA-4A06-9370-D4EB9060B639";
+        string paymentStep = "ED21BA09-A2BB-4E4F-920A-26912132265A";
+        string mDFStep = "6935960E-B310-49FF-835C-3333E2EEB73B";
+        string destinationMDFStep = "ADB8BBEC-C23A-430E-BF3A-221444F0E589";
+        string completedStep = "F3384A9D-A1D3-4F2C-8985-8987639522DE";
+
+        public string GetNextStep(string id, string currentStepId)
+        {
+            string nextStepId = "";
+            bool isonsameswitch = false;
+            //get request from bpm
+            Guid idd = new Guid(id.ToUpper());
+            // Creation of query instance with "City" root schema. 
+            var esq = new EntitySchemaQuery(BPM_UserConnection.EntitySchemaManager, "StADSLLineMoving");
+            esq.AddColumn("Id");
+            esq.AddColumn("StSelectedTelephonyContract");
+            esq.AddColumn("StTelephonyContractId");
+            // Creation of the first filter instance.
+            var esqFirstFilter = esq.CreateFilterWithParameters(FilterComparisonType.Equal, "Id", idd);
+            // Adding created filters to query collection. 
+            esq.Filters.Add(esqFirstFilter);
+            // Objects, i.e. query results, filtered by two filters, will be included into this collection.
+            var entities = esq.GetEntityCollection(BPM_UserConnection);
+            if (entities.Count > 0)
+            {
+                var selectedcontract = entities[0].GetColumnValue("StSelectedTelephonyContract");
+                var pilotcontract = entities[0].GetColumnValue("StTelephonyContractId");
+                InventoryManager manager = new InventoryManager();
+                isonsameswitch = manager.IsNumbersOnSameSwitch(selectedcontract.ToString(), pilotcontract.ToString());
+            }
+
+            switch (currentStepId.ToLower())
+            {
+                ///welcome step
+                case "013B161F-FB2C-4072-82CC-60852FE53039": nextStepId = chooseContractStep; break;
+                ///choose contract step
+                case "BE540720-169E-456B-B0D0-6D243D5B7F82": nextStepId = printStep; break;
+                ///print step
+                case "4F032C27-BBB9-420B-A064-6A88E7E532A0":
+                    
+                    nextStepId = isonsameswitch ? paymentStep :  DSLAMPortStep; break;
+                /// DSLAM Step
+                case "0E26138A-89CA-4A06-9370-D4EB9060B639": nextStepId = paymentStep; break;
+                /*payment step*/
+                case "ED21BA09-A2BB-4E4F-920A-26912132265A": nextStepId = isonsameswitch? mDFStep : destinationMDFStep ; break;
+                /// MDF Step
+                case "6935960E-B310-49FF-835C-3333E2EEB73B": nextStepId = destinationMDFStep; break;
+                /// Destination MDF Step
+                case "ADB8BBEC-C23A-430E-BF3A-221444F0E589": nextStepId = completedStep; break;
+            }
+            return nextStepId;
+        }
+    }
+}
---
+++ SOM/Code/BPMExtended.Main.Business/Request Flow Management/ADSLLineTerminationFlowManagement.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Terrasoft.Core;
+using Terrasoft.Core.Configuration;
+using Terrasoft.Core.DB;
+using Terrasoft.Core.Entities;
+using System.Web;
+
+namespace BPMExtended.Main.Business
+{
+    public class ADSLLineTerminationFlowManagement
+    {
+        public UserConnection BPM_UserConnection
+        {
+            get
+            {
+                return (UserConnection)HttpContext.Current.Session["UserConnection"];
+            }
+        }
+        string welcomeStep = "B8721EA5-0A7E-43BC-8E8D-8C186537FD73";
+        string printStep = "0705ADD3-50F2-42B4-8247-4B67E85E166C";
+        string ReasonStep = "DA251EA7-47C1-42AD-B4EC-A131EDD9587A";
+        string PDNStep = "DFD53946-609E-4A86-AD49-3C41BF076D20";
+        string mDFStep = "0EB0E1DB-FBC1-40D1-9B90-11C52DECE61A";
+        string completedStep = "639E4A4C-1752-42E3-83CD-85674B6E9903";
+
+        public string GetNextStep(string id, string currentStepId)
+        {
+            string nextStepId = "";
+            bool ismanualswitch = false;
+            //get request from bpm
+            Guid idd = new Guid(id.ToUpper());
+            // Creation of query instance with "City" root schema. 
+            var esq = new EntitySchemaQuery(BPM_UserConnection.EntitySchemaManager, "StADSLLineMoving");
+            esq.AddColumn("Id");
+            esq.AddColumn("StContractId");
+            // Creation of the first filter instance.
+            var esqFirstFilter = esq.CreateFilterWithParameters(FilterComparisonType.Equal, "Id", idd);
+            // Adding created filters to query collection. 
+            esq.Filters.Add(esqFirstFilter);
+            // Objects, i.e. query results, filtered by two filters, will be included into this collection.
+            var entities = esq.GetEntityCollection(BPM_UserConnection);
+            if (entities.Count > 0)
+            {
+                var contractId = entities[0].GetColumnValue("StContractId");
+                InventoryManager manager = new InventoryManager();
+                ismanualswitch = manager.IsManualSwitch(contractId.ToString());
+            }
+
+            switch (currentStepId.ToLower())
+            {
+                ///welcome step
+                case "B8721EA5-0A7E-43BC-8E8D-8C186537FD73": nextStepId = printStep; break;
+                ///print step
+                case "0705ADD3-50F2-42B4-8247-4B67E85E166C": nextStepId = ReasonStep; break;
+                ///reason step
+                case "DA251EA7-47C1-42AD-B4EC-A131EDD9587A":nextStepId = ismanualswitch ? PDNStep : mDFStep; break;
+                /// PDN Step
+                case "DFD53946-609E-4A86-AD49-3C41BF076D20": nextStepId = mDFStep; break;
+                /// MDF Step
+                case "0EB0E1DB-FBC1-40D1-9B90-11C52DECE61A": nextStepId = completedStep; break;
+            }
+            return nextStepId;
+        }
+    }
+}
