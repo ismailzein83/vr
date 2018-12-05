e7eaca0e3f72c3b237efdbdc749743a8b74a7967
Zeinab Mokdad <VANRISE\zeinab.mokdad>
2018-12-05 15:47:43 +0000

    addiing OperatorDirectionServicesMappedCellExtensionConfiguration
    git-tfs-id: [http://developmentvlan:8585/tfs/vanrise.collection]$/;C51601

---
+++ Retail/Code/Retail.RA.Entities/OperatorDeclaration/OperatorDirectionServicesMappedCellExtensionConfiguration.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using Vanrise.Entities;
+
+namespace Retail.RA.Entities
+{
+    public class OperatorDirectionServicesMappedCellExtensionConfiguration : ExtensionConfiguration
+    {
+        public const string EXTENSION_TYPE = "Retail_BE_OperatorDeclarationServiceConfig";
+
+        public string Editor { get; set; }
+    }
+}
