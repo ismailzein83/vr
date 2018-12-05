732a575502bfcdf0dcf023be135d173fdf522a55
Ali Sakr <VANRISE\ali.sakr>
2018-12-05 12:37:13 +0000

    adding reference and changing name
    git-tfs-id: [http://developmentvlan:8585/tfs/vanrise.collection]$/;C51583

---
+++ Retail/Code/Retail.RA.Entities/Special Numbers/SpecialNumberCustomObjectTypeSettings.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Vanrise.GenericData.Entities;
+
+namespace Retail.RA.Entities
+{
+   public class SpecialNumberCustomObjectTypeSettings
+    {
+        //public override string GetDescription(IFieldCustomObjectTypeSettingsContext context)
+        //{
+        //    return null;
+        //}
+        //public override bool AreEqual(Object newValue, Object oldValue)
+        //{
+           
+        //    return true;
+        //}
+    }
+}
