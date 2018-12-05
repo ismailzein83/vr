22f1ae20d2aaa74c004e17f7822a59549d142a1d
Zeinab Mokdad <VANRISE\zeinab.mokdad>
2018-12-05 11:57:50 +0000

    adding operatorDeclaration class
    git-tfs-id: [http://developmentvlan:8585/tfs/vanrise.collection]$/;C51575

---
+++ Retail/Code/Retail.RA.Entities/OperatorDeclaration/OperatorDeclaration.cs
@@ -0,0 +1,12 @@
+﻿using Retail.BusinessEntity.Entities;
+
+namespace Retail.RA.Entities
+{
+    public class OperatorDeclaration
+    {
+        public long ID { get; set; }
+        public long OperatorId { get; set; }
+        public int PeriodId { get; set; }
+        public OperatorDeclarationService OperatorDeclarationService { get; set; }
+    }
+}
