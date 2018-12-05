b5d7b83a3958df179c6c3bff1ac1ee030e51c2ad
Ali Sakr <VANRISE\ali.sakr>
2018-12-05 13:50:51 +0000

    adding custom object directive and normal directive
    git-tfs-id: [http://developmentvlan:8585/tfs/vanrise.collection]$/;C51588

---
+++ Retail/Code/Retail.RA.Web/Retail_RA/Directives/SpecialNumbers/retail-ra-specialnumbers-customobjectsettings.js
@@ -0,0 +1,55 @@
+
+
+(function (app) {
+
+    'use strict';
+
+    specialNumbers.$inject = ["UtilsService", 'VRUIUtilsService', 'VRNotificationService'];
+
+    function faultTicketType(UtilsService, VRUIUtilsService, VRNotificationService) {
+        return {
+            restrict: "E",
+            scope: {
+                onReady: "=",
+            },
+            controller: function ($scope, $element, $attrs) {
+                var ctrl = this;
+                var ctor = new FaultCtor($scope, ctrl, $attrs);
+                ctor.initializeController();
+            },
+            controllerAs: "Ctrl",
+            bindToController: true,
+            templateUrl: '/Client/Modules/Retail_RA/Directives/SpecialNumbers/SpecialNumbersCustomObjectSettingsTemplate.html'
+
+        };
+        function FaultCtor($scope, ctrl, $attrs) {
+            this.initializeController = initializeController;
+
+            function initializeController() {
+                $scope.scopeModel = {};
+                defineAPI();
+            }
+            function defineAPI() {
+                var api = {};
+
+                api.load = function (payload) {
+
+                };
+
+                api.getData = function () {
+                    var data = {
+                        $type: "Retail.RA.Entities.SpecialNumberCustomObjectTypeSettings,Retail.RA.Entities"
+                    };
+                    return data;
+                };
+
+                if (ctrl.onReady != undefined && typeof (ctrl.onReady) == 'function') {
+                    ctrl.onReady(api);
+                }
+            }
+        }
+    }
+
+    app.directive('retailRaSpecialnumbersCustomobjectsettings', specialNumbers);
+
+})(app);
