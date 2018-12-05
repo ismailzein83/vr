c8409e1af7177948cc3492d17e1bb20decb42017
Ali Sakr <VANRISE\ali.sakr>
2018-12-05 13:18:53 +0000

    adding class for special numbers and class for special numbers custom object and adding new reference
    git-tfs-id: [http://developmentvlan:8585/tfs/vanrise.collection]$/;C51586

--- Retail/Code/Retail.RA.Entities/Special Numbers/SpecialNumberCustomObjectTypeSettings.cs
+++ Retail/Code/Retail.RA.Entities/Special Numbers/SpecialNumberCustomObjectTypeSettings.cs
@@ -1,22 +1,43 @@
-﻿using System;
+﻿using Retail.RA.Entities;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using Vanrise.GenericData.Entities;
+using Vanrise.Common;
 
 namespace Retail.RA.Entities
 {
-   public class SpecialNumberCustomObjectTypeSettings
+    public class SpecialNumberCustomObjectTypeSettings : FieldCustomObjectTypeSettings
     {
-        //public override string GetDescription(IFieldCustomObjectTypeSettingsContext context)
-        //{
-        //    return null;
-        //}
-        //public override bool AreEqual(Object newValue, Object oldValue)
-        //{
-           
-        //    return true;
-        //}
+        public override Guid ConfigId { get { return new Guid("320B61F3-FF99-4E54-B6CE-9EF5DA6128F2"); } }
+        public override string GetDescription(IFieldCustomObjectTypeSettingsContext context)
+        {
+            return null;
+        }
+        public override bool AreEqual(Object newValue, Object oldValue)
+        {
+
+            return true;
+        }
+        public override Type GetNonNullableRuntimeType()
+        {
+            return typeof(SpecialNumbersSetting);
+        }
+        public override dynamic ParseNonNullValueToFieldType(object originalValue)
+        {
+            var castedOriginalValue = originalValue as SpecialNumbersSetting;
+            if (castedOriginalValue != null)
+            {
+                return castedOriginalValue;
+            }
+            else
+                return Serializer.Deserialize<SpecialNumbersSetting>(originalValue.ToString());
+        }
+        public override string GetRuntimeTypeDescription()
+        {
+            return "Special Numbers";
+        }
     }
 }
---
+++ Retail/Code/Retail.RA.Entities/Special Numbers/SpecialNumbersSetting.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Retail.RA.Entities
+{
+    public class SpecialNumbersSetting
+    {
+        public List<int> Numbers { get; set; }
+        public List<SpecialNumbersRange> Range { get; set; }
+    }
+    public class SpecialNumbersRange
+    {
+        public int From { get; set; }
+        public int To { get; set; }
+    }
+}
