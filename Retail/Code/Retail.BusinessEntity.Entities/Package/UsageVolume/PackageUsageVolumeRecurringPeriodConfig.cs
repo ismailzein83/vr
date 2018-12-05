613dc55fb0446cd01a82749ece69e835710d7b85
Fatima Al Heck <VANRISE\fatima.alheck>
2018-12-05 10:27:28 +0000

    Adding PackageUsageVolumeRecurringPeriodConfig
    git-tfs-id: [http://developmentvlan:8585/tfs/vanrise.collection]$/;C51570

---
+++ Retail/Code/Retail.BusinessEntity.Entities/Package/UsageVolume/PackageUsageVolumeRecurringPeriodConfig.cs
@@ -0,0 +1,11 @@
+﻿using Vanrise.Entities;
+
+namespace Retail.BusinessEntity.Entities
+{
+    public class PackageUsageVolumeRecurringPeriodConfig : ExtensionConfiguration
+    {
+        public const string EXTENSION_TYPE = "Retail_BE_PackageUsageVolumeRecurringPeriodConfig";
+
+        public string Editor { get; set; }
+    }
+}
