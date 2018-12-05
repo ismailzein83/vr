ffc654818552fd8077346251837b45bd67914d4b
Zeinab Mokdad <VANRISE\zeinab.mokdad>
2018-12-05 13:47:31 +0000

    adding OperatorDirectionServices
    git-tfs-id: [http://developmentvlan:8585/tfs/vanrise.collection]$/;C51587

---
+++ Retail/Code/Retail.RA.Web/Controller/OperatorDirectionServicesController.cs
@@ -0,0 +1,19 @@
+﻿using System.Web.Http;
+using Vanrise.Web.Base;
+using System.Collections.Generic;
+using Retail.RA.Business;
+using Retail.RA.Entities;
+
+namespace Retail.RA.Web.Controller
+{
+    public class OperatorDirectionServicesController : BaseAPIController
+    {
+        [HttpGet]
+        [Route("GetMappedCellsExtensionConfigs")]
+        public IEnumerable<OperatorDirectionServicesMappedCellExtensionConfiguration> GetMappedCellsExtensionConfigs()
+        {
+            var manager = new OperatorDeclarationServicesManager();
+            return manager.GetMappedCellsExtensionConfigs();
+        }
+    }
+}
---
+++ Retail/Code/Retail.RA.Web/Retail_RA/Directives/OperatorDeclarationServices/MainExtensions/retail-be-operatordeclarationservice-sms.js
@@ -0,0 +1,96 @@
+﻿(function (app) {
+
+    'use strict';
+
+    OperatorDeclarationServiceSMS.$inject = ['UtilsService', 'VRUIUtilsService'];
+
+    function OperatorDeclarationServiceSMS(UtilsService, VRUIUtilsService) {
+        return {
+            restrict: "E",
+            scope: {
+                onReady: "=",
+                normalColNum: '@',
+                label: '@',
+                customvalidate: '='
+            },
+            controller: function ($scope, $element, $attrs) {
+                var ctrl = this;
+                var ctor = new OperatorDeclarationServiceSMSCtor($scope, ctrl, $attrs);
+                ctor.initializeController();
+            },
+            controllerAs: "ctrl",
+            bindToController: true,
+            templateUrl: "/Client/Modules/Retail_RA/Directives/OperatorDeclarationServices/MainExtensions/Templates/OperatorDeclarationServiceSMSTemplate.html"
+        };
+
+        function OperatorDeclarationServiceSMSCtor($scope, ctrl, $attrs) {
+            this.initializeController = initializeController;
+
+            var trafficDirectionSelectorAPI;
+            var trafficDirectionSelectorReadyDeferred = UtilsService.createPromiseDeferred();
+
+            function initializeController() {
+                $scope.scopeModel = {};
+
+                $scope.scopeModel.onTrafficDirectionSelectorReady = function (api) {
+                    trafficDirectionSelectorAPI = api;
+                    trafficDirectionSelectorReadyDeferred.resolve();
+                };
+
+                defineAPI();
+            }
+            function defineAPI() {
+                var api = {};
+
+                api.load = function (payload) {
+                    var promises = [];
+                    var SMSEntity;
+
+                    if (payload != undefined) {
+
+                        SMSEntity = payload.settings;
+                        $scope.scopeModel.amount = SMSEntity.Amount;
+                        $scope.scopeModel.numberOfSMSs = SMSEntity.NumberOfSMSs;
+                    }
+
+                    var trafficDirectionSelectorLoadPromise = loadTrafficDirectionSelector();
+
+                    promises.push(trafficDirectionSelectorLoadPromise);
+
+                    function loadTrafficDirectionSelector() {
+                        var trafficDirectionSelectorLoadDeferred = UtilsService.createPromiseDeferred();
+                        trafficDirectionSelectorReadyDeferred.promise.then(function () {
+                            var payload;
+
+                            if (SMSEntity != undefined)
+                                payload = {
+                                    selectedIds: SMSEntity.TrafficDirection
+                                };
+                            VRUIUtilsService.callDirectiveLoad(trafficDirectionSelectorAPI, payload, trafficDirectionSelectorLoadDeferred);
+                        });
+                        return trafficDirectionSelectorLoadDeferred.promise;
+                    }
+
+                    return UtilsService.waitMultiplePromises(promises);
+
+                };
+
+                api.getData = function () {
+                    return {
+                        $type: "Retail.RA.MainExtensions.SMS,Retail.RA.MainExtensions",
+                        TrafficDirection: trafficDirectionSelectorAPI.getSelectedIds(),
+                        NumberOfSMSs: $scope.scopeModel.numberOfSMSs,
+                        Amount: $scope.scopeModel.amount
+                    };
+                };
+
+                if (ctrl.onReady != null) {
+                    ctrl.onReady(api);
+                }
+            }
+        }
+    }
+
+    app.directive('retailBeOperatordeclarationserviceSms', OperatorDeclarationServiceSMS);
+
+})(app);
