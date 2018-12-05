f0202a4a49884af20e90832ab6ad8cc358468cd0
Zeinab Mokdad <VANRISE\zeinab.mokdad>
2018-12-05 15:47:10 +0000

    adding operator declaration service
    git-tfs-id: [http://developmentvlan:8585/tfs/vanrise.collection]$/;C51600

---
+++ Retail/Code/Retail.RA.Business/OperatorDeclaration/OperatorDeclarationManager.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using Vanrise.Common;
+using Retail.RA.Entities;
+using System.Collections.Generic;
+using Vanrise.GenericData.Entities;
+
+namespace Retail.RA.Business
+{
+    public class OperatorDeclarationManager
+    {
+        static Guid beDefinitionId = new Guid("6E73A6E0-B3E2-4930-86ED-9EF509D56960");
+        private Dictionary<long, OperatorDeclaration> GetCachedOperatorDeclarations()
+        {
+            IGenericBusinessEntityManager genericBusinessEntityManager = Vanrise.GenericData.Entities.BusinessManagerFactory.GetManager<IGenericBusinessEntityManager>();
+            return genericBusinessEntityManager.GetCachedOrCreate("GetCachedOperatorDeclarations", beDefinitionId, () =>
+            {
+                List<GenericBusinessEntity> genericBusinessEntities = genericBusinessEntityManager.GetAllGenericBusinessEntities(beDefinitionId);
+                Dictionary<long, OperatorDeclaration> operatorDeclarationByPeriod = new Dictionary<long, OperatorDeclaration>();
+                Dictionary<int, OperatorDeclaration> operatorDeclarationById = new Dictionary<int, OperatorDeclaration>();
+
+                if (genericBusinessEntities != null)
+                {
+                    foreach (GenericBusinessEntity genericBusinessEntity in genericBusinessEntities)
+                    {
+                        if (genericBusinessEntity.FieldValues == null)
+                            continue;
+
+                        OperatorDeclaration operatorDeclaration = new OperatorDeclaration
+                        {
+                            ID = (int)genericBusinessEntity.FieldValues.GetRecord("ID"),
+                            OperatorId = (int)genericBusinessEntity.FieldValues.GetRecord("OperatorId")
+                        };
+                        operatorDeclarationByPeriod.Add(operatorDeclaration.ID, operatorDeclaration);
+                    }
+                }
+
+                return operatorDeclarationByPeriod;
+            });
+        }
+    }
+}
---
+++ Retail/Code/Retail.RA.Business/OperatorDeclaration/OperatorDeclarationServicesManager.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using Retail.RA.Entities;
+using System.Collections.Generic;
+
+
+namespace Retail.RA.Business
+{
+    public class OperatorDeclarationServicesManager
+    {
+        public IEnumerable<OperatorDirectionServicesMappedCellExtensionConfiguration> GetMappedCellsExtensionConfigs()
+        {
+            var extensionConfigManager = new Vanrise.Common.Business.ExtensionConfigurationManager();
+            return extensionConfigManager.GetExtensionConfigurations<OperatorDirectionServicesMappedCellExtensionConfiguration>(OperatorDirectionServicesMappedCellExtensionConfiguration.EXTENSION_TYPE);
+        }
+    }
+}
