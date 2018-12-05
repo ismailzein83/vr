3ca8328760afd0841d1eb87eb14381f4f0526767
Mohammad Sheaib <VANRISE\mohammad.sheaib>
2018-12-05 08:56:06 +0000

    Implementing Generated Script Part 2
    git-tfs-id: [http://developmentvlan:8585/tfs/vanrise.collection]$/;C51560

--- Vanrise/Code/Vanrise.Entities/Tools/GeneratedScriptItem.cs
+++ Vanrise/Code/Vanrise.Entities/Tools/GeneratedScriptItem.cs
@@ -4,19 +4,28 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace Vanrise.Entities.Tools
+namespace Vanrise.Entities
 {
     public class GeneratedScriptItem
     {
-        public List<GeneratedScriptItemTable> Tables { get; set; }
+        public GeneratedScriptType Type { get; set; }
+        public GeneratedScriptItemTables Tables { get; set; }
     }
+
+    public class GeneratedScriptItemTables
+    {
+        public  List<GeneratedScriptItemTable> Scripts { get; set; }
+
+    }
+
+    public enum GeneratedScriptType { SQL = 1 }
 
     public class GeneratedScriptItemTable
     {
         public Guid ConnectionId { get; set; }
         public string Schema { get; set; }
         public string TableName { get; set; }
-        public GeneratedScriptItemTableSettings GeneratedScriptItemTableSettings { get; set; }
+        public GeneratedScriptItemTableSettings Settings { get; set; }
 
     }
     public class GeneratedScriptItemTableColumn
@@ -32,59 +41,37 @@ namespace Vanrise.Entities.Tools
     public abstract class GeneratedScriptItemTableSettings
     {
         public abstract Guid ConfigId { get; }
-        public abstract string ExceptionMessage(GeneratedScriptItemTable generatedScriptItemTable);
+        public abstract bool Validate(IGeneratedScriptItemTableValidationContext context);
+        public abstract string GenerateQuery(IGeneratedScriptItemTableContext context);
+
     }
 
-    public class DeleteGeneratedScriptItem : GeneratedScriptItemTableSettings
+    public interface IGeneratedScriptItemTableContext
     {
-        public override Guid ConfigId
-        {
-            get { return new Guid("411F4A51-1B9A-4221-9F16-1A05C1877EB2"); }
-
-        }
-
-        public override string ExceptionMessage(GeneratedScriptItemTable generatedScriptItemTable)
-        {
-            if (generatedScriptItemTable.ConnectionId == null) return ("Check ConnectionId");
-            if (generatedScriptItemTable.Schema == null) return ("Check Schema");
-            if (generatedScriptItemTable.TableName == null) return ("Check Table");
-            if (generatedScriptItemTable.GeneratedScriptItemTableSettings == null) return ("Check Settings");
-            if (this.IdentifierColumn == null) return ("Check IdentifierColumn");
-            if (this.KeyValues == null) return ("Check KeyValues");
-
-            return null;
-        }
-        public GeneratedScriptItemTableColumn IdentifierColumn { get; set; }
-        public List<string> KeyValues { get; set; }
+         GeneratedScriptItemTable Item { get; }
+         GeneratedScriptType Type {  get; }
     }
-
-    public class MergeGeneratedScriptItem : GeneratedScriptItemTableSettings
+    public class GeneratedScriptItemTableContext : IGeneratedScriptItemTableContext
     {
-        public override Guid ConfigId
-        {
-            get { return new Guid("9837CAD1-1F8F-4EDF-8AAB-25864C4EF842"); }
-
-        }
-
-        public override string ExceptionMessage(GeneratedScriptItemTable generatedScriptItemTable)
-        {
-            if (generatedScriptItemTable.ConnectionId == null) return ("Check ConnectionId");
-            if (generatedScriptItemTable.Schema == null) return ("Check Schema");
-            if (generatedScriptItemTable.TableName == null) return ("Check Table");
-            if (generatedScriptItemTable.GeneratedScriptItemTableSettings == null) return ("Check Settings");
-            if (this.IdentifierColumns == null) return ("Check IdentifierColumns");
-            if (this.UpdateColumns == null) return ("Check UpdateColumns");
-            if (this.InsertColumns == null) return ("Check InsertColumns");
-            if (this.DataRows == null) return ("Check DataRows");
-
-            return null;
-        }
-        public List<GeneratedScriptItemTableColumn> InsertColumns { get; set; }
-        public List<GeneratedScriptItemTableColumn> UpdateColumns { get; set; }
-        public List<GeneratedScriptItemTableColumn> IdentifierColumns { get; set; }
-        public List<GeneratedScriptItemTableRow> DataRows { get; set; }
-
+        public GeneratedScriptItemTable Item { get; set; }
+        public GeneratedScriptType Type { get; set; }
     }
+    public interface IGeneratedScriptItemTableValidationContext
+    {
+         List<string> Errors { set; }
+    }
+    public class GeneratedScriptValidationContext: IGeneratedScriptItemTableValidationContext
+    {
+        public List<string> Errors { get; set; }
+    }
+    public class GeneratedScriptItemValidatorOutput
+    {
+        public string TableTitle { get; set; }
+        public List<string> Errors { get; set; }
+        public bool IsValidated { get; set; }
+    }
+
+
 
     public class GeneratedScriptItemTableSettingsConfig : ExtensionConfiguration
     {
