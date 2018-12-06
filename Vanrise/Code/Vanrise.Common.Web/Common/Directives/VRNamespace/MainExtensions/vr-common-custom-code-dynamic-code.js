50619b48e749d6af83247f8ff6ae46cace7c764b
Mohammad Sheaib <VANRISE\mohammad.sheaib>
2018-12-06 15:09:20 +0000

    VRCommonCustomCodeDynamicAPI
    git-tfs-id: [http://developmentvlan:8585/tfs/vanrise.collection]$/;C51629

---
+++ Vanrise/Code/Vanrise.Common.Web/Common/Directives/VRNamespace/MainExtensions/vr-common-custom-code-dynamic-code.js
@@ -0,0 +1,51 @@
+﻿'use strict';
+
+app.directive('vrCommonCustomCodeDynamicCode', ['VRUIUtilsService', 'VRNotificationService',  'VRCommon_VRNamespaceService',
+    function (VRUIUtilsService, VRNotificationService,  VRCommon_VRNamespaceService) {
+        return {
+            restrict: 'E',
+            scope: {
+                onReady: '=',
+            },
+            controller: function ($scope, $element, $attrs) {
+                var ctrl = this;
+                var ctor = new VRCommonCustomCodeDynamicCodeDirectiveCtor($scope, ctrl, $attrs);
+                ctor.initializeController();
+            },
+            controllerAs: 'ctrl',
+            bindToController: true,
+            templateUrl: '/Client/Modules/Common/Directives/VRNamespace/MainExtensions/Templates/VRCustomCodeDynamicCode.html'
+        };
+
+        function VRCommonCustomCodeDynamicCodeDirectiveCtor($scope, ctrl, $attrs) {
+            this.initializeController = initializeController;
+
+            var gridAPI;
+            function initializeController() {
+                $scope.scopeModel = {};
+                $scope.scopeModel.menuActions = [];
+                defineAPI();
+            }
+
+            function defineAPI() {
+                var api = {};
+
+                api.load = function (payload) {
+                    if (payload!=undefined)
+                        $scope.scopeModel.customCode = payload.vrCustomCodeSettingsEntity.CustomCode;
+                };
+
+                api.getData = function () {
+                    return {
+                        $type: "Vanrise.Entities.CustomCodeDynamicCodeSettings,Vanrise.Entities",
+                        CustomCode: $scope.scopeModel.customCode
+                    };
+                };
+
+                if (ctrl.onReady != null)
+                    ctrl.onReady(api);
+            }
+
+         
+        }
+    }]);
