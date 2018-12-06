31ccc1b7d7c60d5913d8ebb904775bdb42bd550d
Samer Haidar <VANRISE\samer.haidar>
2018-12-06 17:20:41 +0000

    Adding new directive vrGenericdataFieldtypeAttachmentRuntimeeditor
    git-tfs-id: [http://developmentvlan:8585/tfs/vanrise.collection]$/;C51634

---
+++ Vanrise/Code/Vanrise.GenericData.Web/VR_GenericData/Directives/MainExtensions/FieldType/Attachment/vr-genericdata-fieldtype-attachment-runtimeeditor.js
@@ -0,0 +1,71 @@
+﻿'use strict';
+app.directive('vrGenericdataFieldtypeAttachmentRuntimeeditor', ['UtilsService','VRUIUtilsService',
+    function (UtilsService, VRUIUtilsService) {
+
+        var directiveDefinitionObject = {
+            restrict: 'E',
+            scope:
+            {
+                onReady: '=',
+                normalColNum: '@'
+            },
+            controller: function ($scope, $element, $attrs) {
+
+                var ctrl = this;
+                var ctor = new attachmentTypeCtor(ctrl, $scope);
+                ctor.initializeController();
+
+            },
+            controllerAs: 'ctrl',
+            bindToController: true,
+            template: function (element, attrs) {
+                return '<vr-columns colnum="12" >'
+                         + '<vr-genericdata-attachmentfieldtype-management on-ready="ctrl.onDirectiveReady" ></vr-genericdata-attachmentfieldtype-management>'
+                     + '</vr-columns>';
+            }
+
+        };
+
+        function attachmentTypeCtor(ctrl, $scope) {
+
+            var directiveReadyAPI;
+            var directiveReadyPromiseDeferred = UtilsService.createPromiseDeferred();
+
+            function initializeController() {
+
+                ctrl.onDirectiveReady = function (api) {
+                    directiveReadyAPI = api;
+                    directiveReadyPromiseDeferred.resolve();
+                };
+                directiveReadyPromiseDeferred.promise.then(function () {
+                    defineAPI();
+                });
+            }
+
+            function defineAPI() {
+                var api = {};
+
+                api.load = function (payload) {
+                    var promises = [];
+
+                    var payloadVariable = {
+                        attachementFieldTypes: payload != undefined ? payload.fieldValue : undefined
+                    };
+                    promises.push(directiveReadyAPI.loadGrid(payloadVariable));
+
+                    return UtilsService.waitMultiplePromises(promises);
+                };
+
+                api.getData = function () {
+                    return directiveReadyAPI.getData();
+                };
+
+                if (ctrl.onReady != null)
+                    ctrl.onReady(api);
+            }
+
+            this.initializeController = initializeController;
+        }
+        return directiveDefinitionObject;
+    }
+]);
