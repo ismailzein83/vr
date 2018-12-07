f72634e4b1cf489d7a87bc9babfa391ea229aa4c
Samer Haidar <VANRISE\samer.haidar>
2018-12-07 10:04:01 +0000

    Modify HttpProxyDynamicCodeSettings
    git-tfs-id: [http://developmentvlan:8585/tfs/vanrise.collection]$/;C51645

--- Vanrise/Code/Vanrise.Common.MainExtensions/VRDynamicCode/HttpProxyDynamicCodeSettings.cs
+++ Vanrise/Code/Vanrise.Common.MainExtensions/VRDynamicCode/HttpProxyDynamicCodeSettings.cs
@@ -11,6 +11,16 @@ namespace Vanrise.Common.MainExtensions.VRDynamicCode
     {
         public override Guid ConfigId { get { return new Guid("5F14D26D-7B43-41BE-9A3A-6BA0A7EB8316"); } }
         public Guid ConnectionId { get; set; }
+        public string ClassName { get; set; }
+        public List<HttpProxyMethod> Methods { get; set; }
+        public override string Generate(IVRDynamicCodeSettingsContext context)
+        {
+            return "";
+        }
+    }
+    public class HttpProxyMethod
+    {
+        public string MethodName { get; set; }
         public VRHttpMethod MethodType { get; set; }
         public VRHttpMessageFormat MessageFormat { get; set; }
         public string ActionPath { get; set; }
@@ -19,15 +29,24 @@ namespace Vanrise.Common.MainExtensions.VRDynamicCode
         public Dictionary<string, string> Headers { get; set; }
         public List<HttpProxyParameter> Parameters { get; set; }
         public Dictionary<string, string> URLParameters { get; set; }
-        public override string Generate(IVRDynamicCodeSettingsContext context)
+        public string Generate(IHttpProxyMethodContext context)
         {
             return "";
         }
     }
+
+    public interface IHttpProxyMethodContext
+    {
+        Guid ConnectionId { get; }
+    }
+    public class HttpProxyMethodContext : IHttpProxyMethodContext
+    {
+        public Guid ConnectionId { get; set; }
+    }
     public class HttpProxyParameter
     {
         public string ParameterName { get; set; }
-        public string ParameterValue { get; set; }
+        public string ParameterType{ get; set; }
         public bool IncludeInHeader { get; set; }
         public bool IncludeInBody { get; set; }
         public bool IncludeInURL { get; set; }
