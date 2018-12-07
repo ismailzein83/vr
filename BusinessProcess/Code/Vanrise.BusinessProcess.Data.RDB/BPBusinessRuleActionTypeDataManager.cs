814a7a8d464a5863e1b648d619c4683fb7626aeb
Naim Saad <VANRISE\naim.saad>
2018-12-07 15:46:07 +0000

    Implementing RDB BPBusinessRuleActionTypeDataManager
    git-tfs-id: [http://developmentvlan:8585/tfs/vanrise.collection]$/;C51671

---
+++ BusinessProcess/Code/Vanrise.BusinessProcess.Data.RDB/BPBusinessRuleActionTypeDataManager.cs
@@ -0,0 +1,22 @@
+﻿//using System;
+//using System.Collections.Generic;
+//using System.Linq;
+//using System.Text;
+//using System.Threading.Tasks;
+//using Vanrise.BusinessProcess.Entities;
+
+//namespace Vanrise.BusinessProcess.Data.RDB
+//{
+//    public class BPBusinessRuleActionTypeDataManager : IBPBusinessRuleActionTypeDataManager
+//    {
+//        public bool AreBPBusinessRuleActionTypesUpdated(ref object updateHandle)
+//        {
+//            throw new NotImplementedException();
+//        }
+
+//        public List<BPBusinessRuleActionType> GetBPBusinessRuleActionTypes()
+//        {
+//            throw new NotImplementedException();
+//        }
+//    }
+//}
