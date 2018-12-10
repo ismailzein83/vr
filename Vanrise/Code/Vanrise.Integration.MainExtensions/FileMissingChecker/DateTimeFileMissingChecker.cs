09129fca0a498d6a5c95a0e268f7eb87ca9b821f
Ali Atoui <VANRISE\ali.atoui>
2018-12-10 15:06:02 +0000

    Editing SequenceFileMissingChecker and DateTimeFileMissingChecker
    git-tfs-id: [http://developmentvlan:8585/tfs/vanrise.collection]$/;C51712

--- Vanrise/Code/Vanrise.Integration.MainExtensions/FileMissingChecker/DateTimeFileMissingChecker.cs
+++ Vanrise/Code/Vanrise.Integration.MainExtensions/FileMissingChecker/DateTimeFileMissingChecker.cs
@@ -3,6 +3,7 @@ using Vanrise.Integration.Entities;
 
 namespace Vanrise.Integration.MainExtensions.FileMissingChecker
 {
+    //Should be Deleted
     public class DateTimeFileMissingChecker : Vanrise.Integration.Entities.FileMissingChecker
     {
         public override Guid ConfigId { get { return new Guid("AF88B648-2FAD-4A7E-8240-564019CF4BC3"); } }
