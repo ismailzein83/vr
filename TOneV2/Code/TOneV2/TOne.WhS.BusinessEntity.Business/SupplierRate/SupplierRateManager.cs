1ffe7699a4fe55701931ab25556ccea06495c2d6
Rodi Hassan <VANRISE\rodi.hassan>
2018-12-10 13:44:15 +0000

    Add comma to rate greater than or equal to 1000 in export sale / purchase rates
    git-tfs-id: [http://developmentvlan:8585/tfs/vanrise.collection]$/;C51697

--- TOneV2/Code/TOneV2/TOne.WhS.BusinessEntity.Business/SupplierRate/SupplierRateManager.cs
+++ TOneV2/Code/TOneV2/TOne.WhS.BusinessEntity.Business/SupplierRate/SupplierRateManager.cs
@@ -3,9 +3,9 @@ using System.Collections.Generic;
 using System.Linq;
 using TOne.WhS.BusinessEntity.Data;
 using TOne.WhS.BusinessEntity.Entities;
-using Vanrise.Entities;
-using Vanrise.Common.Business;
 using Vanrise.Common;
+using Vanrise.Common.Business;
+using Vanrise.Entities;
 namespace TOne.WhS.BusinessEntity.Business
 {
     public class SupplierRateManager
@@ -210,7 +210,7 @@ namespace TOne.WhS.BusinessEntity.Business
                 return new SupplierRateDetail
                 {
                     Entity = supplierRate,
-                    SupplierId=priceList.SupplierId,
+                    SupplierId = priceList.SupplierId,
                     SupplierZoneName = _supplierRateManager.GetSupplierZoneName(supplierRate.ZoneId),
                     DisplayedCurrency = _currencyManager.GetCurrencySymbol(currencyValueId),
                     DisplayedRate = (systemCurrencyId != null) ? _currencyExchangeRateManager.ConvertValueToCurrency(supplierRate.Rate, currencyId, currencyValueId, effectiveOn) : supplierRate.Rate,
@@ -249,7 +249,7 @@ namespace TOne.WhS.BusinessEntity.Business
 
                 sheet.Header.Cells.Add(new ExportExcelHeaderCell { Title = "ID" });
                 sheet.Header.Cells.Add(new ExportExcelHeaderCell { Title = "Zone" });
-                sheet.Header.Cells.Add(new ExportExcelHeaderCell { Title = "Normal Rate" });
+                sheet.Header.Cells.Add(new ExportExcelHeaderCell { Title = "Normal Rate", CellType = ExcelCellType.Number, NumberType = NumberType.LongDecimal });
                 sheet.Header.Cells.Add(new ExportExcelHeaderCell { Title = "Rate Change" });
                 sheet.Header.Cells.Add(new ExportExcelHeaderCell { Title = "Currency" });
                 sheet.Header.Cells.Add(new ExportExcelHeaderCell { Title = "BED", CellType = ExcelCellType.DateTime, DateTimeType = DateTimeType.Date });
