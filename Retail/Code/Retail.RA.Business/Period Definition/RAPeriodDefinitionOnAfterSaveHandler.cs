74e1c48b2260e34699270284713f4394cb4ab59d
Ali Sakr <VANRISE\ali.sakr>
2018-12-10 14:12:37 +0000

    fixing issue
    git-tfs-id: [http://developmentvlan:8585/tfs/vanrise.collection]$/;C51702

--- Retail/Code/Retail.RA.Business/Period Definition/RAPeriodDefinitionOnAfterSaveHandler.cs
+++ Retail/Code/Retail.RA.Business/Period Definition/RAPeriodDefinitionOnAfterSaveHandler.cs
@@ -12,39 +12,39 @@ namespace Retail.RA.Business
 {
     public class RAPeriodDefinitionOnAfterSaveHandler : GenericBEOnAfterSaveHandler
     {
-//        public override Guid ConfigId { get { return new Guid("db7ea63c-0407-4712-a95c-7fc253db8fdf"); } }
-//        public override void Execute(IGenericBEOnAfterSaveHandlerContext context)
-//        {
-//            IGenericBusinessEntityManager genericBusinessEntityManager = Vanrise.GenericData.Entities.BusinessManagerFactory.GetManager<IGenericBusinessEntityManager>();
-//            context.ThrowIfNull("context");
-//            context.NewEntity.ThrowIfNull("context.NewEntity");
-//            context.NewEntity.FieldValues.ThrowIfNull("context.NewEntity.FieldValues");
-//            if (context.BusinessEntityDefinitionId == null)
-//                throw new NullReferenceException("BusinessEntityDefinitionId");
-//            var from = (DateTime)context.NewEntity.FieldValues.GetRecord("From");
-//            if (from == null)
-//                throw new NullReferenceException("from");
-//            var to = (DateTime)context.NewEntity.FieldValues.GetRecord("To");
-//            if (to == null)
-//                throw new NullReferenceException("to");
-//            var repeat = context.NewEntity.FieldValues.GetRecord("Repeat");
-//            if(repeat != null)
-//            {
-//                for (var i = 0; i < (int)repeat; i++)
-//                {
-//                    var genericBusinessEntityToAdd = new GenericBusinessEntityToAdd
-//                    {
-//                        BusinessEntityDefinitionId = context.BusinessEntityDefinitionId,
-//                        FieldValues = new Dictionary<string, object>()
-//                    };
+        public override Guid ConfigId { get { return new Guid("db7ea63c-0407-4712-a95c-7fc253db8fdf"); } }
+        public override void Execute(IGenericBEOnAfterSaveHandlerContext context)
+        {
+            //            IGenericBusinessEntityManager genericBusinessEntityManager = Vanrise.GenericData.Entities.BusinessManagerFactory.GetManager<IGenericBusinessEntityManager>();
+            //            context.ThrowIfNull("context");
+            //            context.NewEntity.ThrowIfNull("context.NewEntity");
+            //            context.NewEntity.FieldValues.ThrowIfNull("context.NewEntity.FieldValues");
+            //            if (context.BusinessEntityDefinitionId == null)
+            //                throw new NullReferenceException("BusinessEntityDefinitionId");
+            //            var from = (DateTime)context.NewEntity.FieldValues.GetRecord("From");
+            //            if (from == null)
+            //                throw new NullReferenceException("from");
+            //            var to = (DateTime)context.NewEntity.FieldValues.GetRecord("To");
+            //            if (to == null)
+            //                throw new NullReferenceException("to");
+            //            var repeat = context.NewEntity.FieldValues.GetRecord("Repeat");
+            //            if(repeat != null)
+            //            {
+            //                for (var i = 0; i < (int)repeat; i++)
+            //                {
+            //                    var genericBusinessEntityToAdd = new GenericBusinessEntityToAdd
+            //                    {
+            //                        BusinessEntityDefinitionId = context.BusinessEntityDefinitionId,
+            //                        FieldValues = new Dictionary<string, object>()
+            //                    };
 
-//                    genericBusinessEntityToAdd.FieldValues.Add("From", from.AddMonths(1));
-//                    genericBusinessEntityToAdd.FieldValues.Add("URL", applicationURL);
-//                    var insertOperationOutput = genericBusinessEntityManager.AddGenericBusinessEntity(genericBusinessEntityToAdd);
-//                }
-//}
-//            }
+            //                    genericBusinessEntityToAdd.FieldValues.Add("From", from.AddMonths(1));
+            //                    genericBusinessEntityToAdd.FieldValues.Add("URL", applicationURL);
+            //                    var insertOperationOutput = genericBusinessEntityManager.AddGenericBusinessEntity(genericBusinessEntityToAdd);
+            //                }
+            //}
+            //            }
 
-//        }
+        }        
     }
 }
