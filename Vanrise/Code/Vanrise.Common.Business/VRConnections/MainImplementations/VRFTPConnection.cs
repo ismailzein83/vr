0aaf5f23d26b4e8bacfdf70745fe4da58c2ba634
Mohammad Sheaib <VANRISE\mohammad.sheaib>
2018-12-11 15:24:36 +0000

    adding VRFTPConnection
    git-tfs-id: [http://developmentvlan:8585/tfs/vanrise.collection]$/;C51744

---
+++ Vanrise/Code/Vanrise.Common.Business/VRConnections/MainImplementations/VRFTPConnection.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using Vanrise.Entities;
+using Vanrise.Security.Entities;
+using Vanrise.Common;
+
+namespace Vanrise.Common.Business
+{
+    public class VRFTPConnection : VRConnectionSettings
+    {
+        public override Guid ConfigId { get { return s_ConfigId; } }
+
+        public static Guid s_ConfigId = new Guid("661BD539-9F80-4510-A91C-75D7A5543D41");
+
+        public FTPCommunicatorSettings FTPCommunicatorSettings { get; set; }
+        public bool TrySendFTP(Stream stream, string fileName, string subDirectory, out string errorMessage)
+        {
+            return new FTPCommunicator(FTPCommunicatorSettings).TryWriteFile(stream, fileName, subDirectory, out errorMessage);
+        }
+    }
+    public class FTPConnectionFilter : IVRConnectionFilter
+    {
+        public bool IsMatched(VRConnection vrConnection)
+        {
+            vrConnection.ThrowIfNull("vrConnection");
+            vrConnection.Settings.ThrowIfNull("vrConnection.Settings", vrConnection.VRConnectionId);
+
+            if (vrConnection.Settings.ConfigId != VRFTPConnection.s_ConfigId)
+                return false;
+
+            return true;
+        }
+    }
+
+}
