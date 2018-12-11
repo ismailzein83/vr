d66f4729949a10b10ee34c50a058622f32255805
yara.bousalman <yara.bousalman@vanrise.tfs.local>
2018-12-11 12:53:42 +0000

    Adding call method dynamic API files
    git-tfs-id: [http://developmentvlan:8585/tfs/vanrise.collection]$/;C51737

---
+++ Vanrise/Code/Vanrise.Common.MainExtensions/VRDynamicAPI/VRCallMethodDynamicAPI.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Vanrise.Entities;
+
+namespace Vanrise.Common.MainExtensions.VRDynamicAPI
+{
+    public class VRCallMethodDynamicAPI : VRDynamicAPIMethodSettings
+    {
+        public override Guid ConfigId { get { return new Guid("AAC62543-CF85-4F0D-BB42-121C7B699816"); } }
+        public Guid NamespaceId { get; set; }
+        public Guid ClassId { get; set; }
+        public Guid MethodId { get; set; }
+        public override void Evaluate(IVRDynamicAPIMethodSettingsContext vrDynamicAPIMethodSettingsContext)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
