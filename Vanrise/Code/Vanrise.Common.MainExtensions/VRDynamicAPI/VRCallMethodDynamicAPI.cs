d123ddbeddc18c7697b0686a1f1b64fd39ac8902
yara.bousalman <yara.bousalman@vanrise.tfs.local>
2018-12-12 07:48:11 +0000

    Changing VRCallMethodDynamicAPI properties
    git-tfs-id: [http://developmentvlan:8585/tfs/vanrise.collection]$/;C51750

--- Vanrise/Code/Vanrise.Common.MainExtensions/VRDynamicAPI/VRCallMethodDynamicAPI.cs
+++ Vanrise/Code/Vanrise.Common.MainExtensions/VRDynamicAPI/VRCallMethodDynamicAPI.cs
@@ -11,8 +11,8 @@ namespace Vanrise.Common.MainExtensions.VRDynamicAPI
     {
         public override Guid ConfigId { get { return new Guid("AAC62543-CF85-4F0D-BB42-121C7B699816"); } }
         public Guid NamespaceId { get; set; }
-        public Guid ClassId { get; set; }
-        public Guid MethodId { get; set; }
+        public string ClassName { get; set; }
+        public string MethodName { get; set; }
         public override void Evaluate(IVRDynamicAPIMethodSettingsContext vrDynamicAPIMethodSettingsContext)
         {
             throw new NotImplementedException();
