4f1e818d83483c5ca2432721ab57c4599337e64a
Zeinab Mokdad <VANRISE\zeinab.mokdad>
2018-12-14 08:37:55 +0000

    Making sync cache related to SwitchCache-CarrierAccountCache-EndPointCache-RouteCache
    git-tfs-id: [http://developmentvlan:8585/tfs/vanrise.collection]$/;C51836

--- TOneV2/Code/TOneV2/TOne.WhS.RouteSync.IVSwitch/ConfigurationCacheManager.cs
+++ TOneV2/Code/TOneV2/TOne.WhS.RouteSync.IVSwitch/ConfigurationCacheManager.cs
@@ -1,17 +1,26 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using NP.IVSwitch.Entities;
 using TOne.WhS.BusinessEntity.Business;
 
 namespace TOne.WhS.RouteSync.IVSwitch
 {
     public class ConfigurationCacheManager : Vanrise.Caching.BaseCacheManager
     {
+        DateTime? _carrierProfileLastCheck;
+        DateTime? _switchLastCheck;
+        DateTime? _routeLastCheck;
+        DateTime? _enpPointLastCheck;
         protected override bool IsTimeExpirable
         {
             get { return true; }
+        }
+        protected override bool ShouldSetCacheExpired(object parameter)
+        {
+            return Vanrise.Caching.CacheManagerFactory.GetCacheManager<CarrierAccountManager.CacheManager>().IsCacheExpired(ref _carrierProfileLastCheck)
+                   | Vanrise.Caching.CacheManagerFactory.GetCacheManager<SwitchManager.CacheManager>().IsCacheExpired(ref _switchLastCheck)
+                   | NPManagerFactory.GetManager<IRouteManager>().IsCacheExpired(ref _routeLastCheck)
+                   | NPManagerFactory.GetManager<IEndPointManager>().IsCacheExpired(ref _enpPointLastCheck);
+
         }
     }
 }
