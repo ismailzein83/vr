9ea3dfe42186bcdcb1eea471ebf37341dc1aa35f
Fatima Al Heck <VANRISE\fatima.alheck>
2018-12-14 09:55:15 +0000

    Adding HoldRequestDataManager to Vanrise.Queueing.Data.RDB
    git-tfs-id: [http://developmentvlan:8585/tfs/vanrise.collection]$/;C51843

---
+++ BusinessProcess/Code/Vanrise.Queueing.Data.RDB/HoldRequestDataManager.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Vanrise.Queueing.Data.RDB
+{
+    class HoldRequestDataManager : IHoldRequestDataManager
+    {
+        public bool AreHoldRequestsUpdated(ref object updateHandle)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Delete(long holdRequestId)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void DeleteHoldRequestByBPInstanceId(long bpInstanceId)
+        {
+            throw new NotImplementedException();
+        }
+
+        public List<Queueing.Entities.HoldRequest> GetAllHoldRequests()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Vanrise.Entities.DateTimeRange GetDBDateTimeRange()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Insert(long BPInstanceID, Guid executionFlowDefinitionId, DateTime from, DateTime to, List<int> queuesToHold, List<int> queuesToProcess, Queueing.Entities.HoldRequestStatus status)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void UpdateStatus(long holdRequestId, Queueing.Entities.HoldRequestStatus status)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
