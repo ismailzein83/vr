be6f3a82cb86ec5fec93c70ec59c844bb012be86
Ali Slayman <VANRISE\ali.slayman>
2018-12-17 15:46:29 +0000

    Adding TextPatterHelper directives
    git-tfs-id: [http://developmentvlan:8585/tfs/vanrise.collection]$/;C51904

---
+++ Vanrise/Code/Vanrise.Common.Web/Common/Constant/PatternPartsEnum.js
@@ -0,0 +1,16 @@
+﻿//app.constant('VR_Common_PatternPartsEnum', {
+//    LongYearPatternPart: { Name: "YYYY", Description: "Long Year Pattern (ex: 2018)" },
+//    ShortYearPatternPart: { Name: "YY", Description: "Short Year Pattern (ex: 18)" },
+//    LongMonthPatternPart: { Name: "MMM", Description: "Long Month Pattern (ex: Nov)" },
+//    ShortMonthPatternPart: { Name: "MM", Description: "Short Month Pattern (ex: 11)" },
+//    DayPatternPart: { Name: "DD", Description: "Day Pattern (ex: 27)" },
+//    HourPatternPart: { Name: "hh", Description: "Hour Pattern (ex: 16)" },
+//    SecondPatternPart: { Name: "ss", Description: "Second Pattern (ex: 43)" },
+//    S4PatternPart: { Name: "S4", Description: "Sequence Pattern of length 4 (ex: 1234)" },
+//    S5PatternPart: { Name: "S5", Description: "Sequence Pattern of length 5 (ex: 12345)" }, 
+//    S6PatternPart: { Name: "S6", Description: "Sequence Pattern of length 6 (ex: 123456)" }, 
+//    S7PatternPart: { Name: "S7", Description: "Sequence Pattern of length 7 (ex: 1234567)" }, 
+//    S8PatternPart: { Name: "S8", Description: "Sequence Pattern of length 8 (ex: 12345678)" }, 
+//    S9PatternPart: { Name: "S9", Description: "Sequence Pattern of length 9 (ex: 123456789)" }, 
+//    S10PatternPart: { Name: "S10", Description: "Sequence Pattern of length 10 (ex: 1234567891)" }
+//});
---
+++ Vanrise/Code/Vanrise.Common.Web/Common/Directives/VRTextPattern/vr-common-textpatternbuilder.js
@@ -0,0 +1,116 @@
+﻿//"use strict";
+
+//app.directive("vrCommonTextpatternbuilder", ["UtilsService", "VRNotificationService", "VRUIUtilsService", "VRModalService",
+//    function (UtilsService, VRNotificationService, VRUIUtilsService, VRModalService) {
+
+//        var directiveDefinitionObject = {
+//            restrict: "E",
+//            scope:{
+//                onReady: "=",
+//                normalColNum: '@',
+//                isrequired: "="
+//            },
+//            controller: function ($scope, $element, $attrs) {
+//                var ctrl = this;
+//                var ctor = new TextPatternBuilderCtor($scope, ctrl, $attrs);
+//                ctor.initializeController();
+//            },
+//            controllerAs: "ctrl",
+//            bindToController: true,
+//            template: function (element, attrs) {
+//                return getTemplate(attrs);
+//            }       
+//        };
+
+//        function getTemplate(attrs) {
+
+//            var withemptyline = 'withemptyline';
+//            if (attrs.hidelabel != undefined)
+//                withemptyline = '';
+
+//            var label = "Text Pattern";
+//            if (attrs.label != undefined)
+//                label = attrs.label;
+
+//            var template = '<vr-columns colnum="{{ctrl.normalColNum}}">'
+//                + '<vr-label ng-if="ctrl.hidelabel ==undefined">' + label + '</vr-label>'
+//                + '<vr-textbox value="ctrl.fullTextPattern" isrequired="ctrl.isrequired"></vr-textbox>'
+//                + '</vr-columns>'
+//                + '<vr-columns width="normal" ' + withemptyline + '>'
+//                + '<vr-button type="Help" data-onclick="scopeModel.openPatternHelper" standalone></vr-button>'
+//                + '</vr-columns>';
+//            return template;
+
+//        }
+
+//        function TextPatternBuilderCtor($scope, ctrl, $attrs) {
+//            this.initializeController = initializeController;
+
+//            var parts;
+//            var context;
+//            var patternSeparator = "#";
+
+//            function initializeController() {
+//                $scope.scopeModel = {};
+
+//                $scope.scopeModel.openPatternHelper = function () {
+//                    var modalSettings = {};
+
+//                    modalSettings.onScopeReady = function (modalScope) {
+//                        modalScope.onSetPattern = function (patternName) {
+//                            if (ctrl.fullTextPattern == undefined)
+//                                ctrl.fullTextPattern = "";
+//                            ctrl.fullTextPattern += patternSeparator + patternName + patternSeparator;
+//                        };
+//                    };
+//                    var parameter = {
+//                        context: getContext()
+//                    };
+//                    VRModalService.showModal('/Client/Modules/Common/Directives/VRTextPattern/Templates/PatternHelperTemplate.html', parameter, modalSettings);
+//                };
+
+//                defineAPI();
+//            }
+
+//            function defineAPI() {
+//                var api = {};
+
+//                api.load = function (payload) {
+
+//                    if (payload != undefined) {
+//                        context = payload.context;
+//                        parts = payload.parts;
+//                        ctrl.fullTextPattern = payload.fullTextPattern;
+
+//                        if (payload.patternSeparator != undefined)
+//                            patternSeparator = payload.patternSeparator;
+
+//                        var promises = [];
+//                        return UtilsService.waitMultiplePromises(promises);
+//                    }
+//                };
+
+//                api.getData = function () {
+//                    return ctrl.fullTextPattern;
+//                };
+
+//                if (ctrl.onReady != null && typeof (ctrl.onReady) == "function")
+//                    ctrl.onReady(api);
+//            }
+
+//            function getContext() {
+//                var currentContext = context;
+//                if (currentContext != undefined)
+//                    return currentContext;
+
+//                currentContext = {};
+//                currentContext.getParts = function () {
+//                    return parts;
+//                };
+//                return currentContext;
+//            }
+//        }
+
+//        return directiveDefinitionObject;
+//    }
+//]);
