461057e7e416917928e335d4a525ff762e273965
Wissam Ajamy <VANRISE\wissam.ajamy>
2018-12-18 07:56:32 +0000

    Remove unused SP
    git-tfs-id: [http://developmentvlan:8585/tfs/vanrise.collection]$/;C51918

--- TOneV2/Code/TOneV2/SQL.TOneConfiguration/Analytic/Stored Procedures/SP_SchemaConfiguration_GetByType.sql
+++
@@ -1,8 +0,0 @@
-﻿CREATE procedure [Analytic].[SP_SchemaConfiguration_GetByType]
-@Type INT
-AS
-BEGIN
-	SELECT	sc.ID,sc.Name,sc.DisplayName,sc.[Type],sc.Configuration 
-	FROM	Analytic.SchemaConfiguration sc  WITH(NOLOCK) 
-	WHERE	sc.[Type]=@Type
-END
