1c426118ad15a0ed5c8cb9ce094838b429b0fddb
Rodi Hassan <VANRISE\rodi.hassan>
2018-12-20 09:40:05 +0000

    implement tax rules
    git-tfs-id: [http://developmentvlan:8585/tfs/vanrise.collection]$/;C51998

---
+++ Retail/Code/Retail.RA.Web/Controller/RaRulesController.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections.Generic;
+using System.Web.Http;
+using Retail.RA.Business;
+using Retail.RA.Business.TaxRules;
+using Vanrise.Web.Base;
+
+namespace Retail.RA.Web.Controller
+{
+    [RoutePrefix(Constants.ROUTE_PREFIX + "RaRules")]
+    public class RaRulesController : BaseAPIController
+    {
+        [HttpGet]
+        [Route("GetVoiceTaxRuleTemplates")]
+        public IEnumerable<VoiceTaxRuleSettingsConfig> GetVoiceTaxRuleTemplates()
+        {
+            TaxRuleManager manager = new TaxRuleManager();
+            return manager.GetVoiceTaxRuleTemplates();
+        }
+
+        [HttpGet]
+        [Route("GetSMSTaxRuleTemplates")]
+        public IEnumerable<SMSTaxRuleSettingsConfig> GetSMSTaxRuleTemplates()
+        {
+            TaxRuleManager manager = new TaxRuleManager();
+            return manager.GetSMSTaxRuleTemplates();
+        }
+    }
+}
---
+++ Retail/Code/Retail.RA.Web/Retail_RA/Services/APIServices/TaxRuleAPIService.js
@@ -0,0 +1,26 @@
+﻿(function (appControllers) {
+
+    'use strict';
+
+    RulesAPIService.$inject = ['BaseAPIService', 'UtilsService', 'Retail_RA_ModuleConfig'];
+
+    function RulesAPIService(BaseAPIService, UtilsService, Retail_RA_ModuleConfig) {
+        var controllerName = 'RaRules';
+
+        function GetVoiceTaxRuleTemplates() {
+            return BaseAPIService.get(UtilsService.getServiceURL(Retail_RA_ModuleConfig.moduleName, controllerName, "GetVoiceTaxRuleTemplates"));
+        }
+
+        function GetSMSTaxRuleTemplates() {
+            return BaseAPIService.get(UtilsService.getServiceURL(Retail_RA_ModuleConfig.moduleName, controllerName, "GetSMSTaxRuleTemplates"));
+        }
+
+        return {
+            GetVoiceTaxRuleTemplates: GetVoiceTaxRuleTemplates,
+            GetSMSTaxRuleTemplates: GetSMSTaxRuleTemplates
+        };
+    }
+
+    appControllers.service('RA_RulesAPIService', RulesAPIService);
+
+})(appControllers);
