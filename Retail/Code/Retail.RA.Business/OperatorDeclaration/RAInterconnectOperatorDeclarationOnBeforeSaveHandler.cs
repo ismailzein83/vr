dd76408d6e136b5d311e675189320f19c4ceff03
Ali Sakr <VANRISE\ali.sakr>
2018-12-21 12:32:44 +0000

    fixing issue
    git-tfs-id: [http://developmentvlan:8585/tfs/vanrise.collection]$/;C52033

--- Retail/Code/Retail.RA.Business/OperatorDeclaration/RAInterconnectOperatorDeclarationOnBeforeSaveHandler.cs
+++ Retail/Code/Retail.RA.Business/OperatorDeclaration/RAInterconnectOperatorDeclarationOnBeforeSaveHandler.cs
@@ -19,14 +19,13 @@ namespace Retail.RA.Business
             IcxOperatorDeclarationManager operatorDeclarationManager = new IcxOperatorDeclarationManager();
             var operatorDeclarations = operatorDeclarationManager.GetAllOperatorDecalarations();
             var id = context.GenericBusinessEntity.FieldValues.GetRecord("ID");
-            id.ThrowIfNull("ID");
             var operatorId = context.GenericBusinessEntity.FieldValues.GetRecord("Operator");
             operatorId.ThrowIfNull("operatorId");
             var periodId = context.GenericBusinessEntity.FieldValues.GetRecord("Period");
             periodId.ThrowIfNull("periodId");
 
-            var operatorDeclaration = operatorDeclarations.FindRecord(x => x.PeriodId == Convert.ToInt32(periodId) && x.OperatorId == Convert.ToInt64(operatorId) && x.ID != Convert.ToInt64(id));
-            if (operatorDeclaration != null)
+            var operatorDeclaration = operatorDeclarations.FindRecord(x => x.PeriodId == Convert.ToInt32(periodId) && x.OperatorId == Convert.ToInt64(operatorId) );
+            if (operatorDeclaration != null && (id == null || operatorDeclaration.ID != Convert.ToInt64(id)))
             {
                 context.OutputResult.Result = false;
                 context.OutputResult.Messages.Add("An operator declaration is already created with the same period declaration");
