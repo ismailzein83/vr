8540a0334d3f651b948a89f9539ed2436004b279
Naim Saad <VANRISE\naim.saad>
2018-12-24 09:28:39 +0000

    Implementing BPInstancePersistenceDataManager RDB
    git-tfs-id: [http://developmentvlan:8585/tfs/vanrise.collection]$/;C52060

---
+++ BusinessProcess/Code/Vanrise.BusinessProcess.Data.RDB/BPInstancePersistenceDataManager.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Vanrise.BusinessProcess.Data.RDB
+{
+    public class BPInstancePersistenceDataManager : IBPInstancePersistenceDataManager
+    {
+        public void DeleteState(long processInstanceId)
+        {
+            throw new NotImplementedException();
+        }
+
+        public string GetInstanceState(long processInstanceId)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void InsertOrUpdateInstance(long processInstanceId, string instanceState)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
