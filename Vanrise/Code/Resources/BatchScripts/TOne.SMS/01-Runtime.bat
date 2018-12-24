627994b4164c1f42e707a945dc4453769311f7e7
Wissam Ajamy <VANRISE\wissam.ajamy>
2018-12-24 09:30:23 +0000

    Add TOne.SMS project
    git-tfs-id: [http://developmentvlan:8585/tfs/vanrise.collection]$/;C52061

---
+++ Vanrise/Code/Resources/BatchScripts/TOne.SMS/01-Runtime.bat
@@ -0,0 +1,18 @@
+ECHO OFF
+::Make Directory from current date as YYYYMMDD
+
+Set CURRDATE=%TEMP%\CURRDATE.TMP
+DATE /T > %CURRDATE%
+Set PARSEARG="eol=; tokens=1,2,3,4* delims=/, "
+For /F %PARSEARG% %%i in (%CURRDATE%) Do SET YYYYMMDD=%%l%%j%%k
+
+ECHO.
+ECHO TOne.SMS Runtime
+xcopy "C:\TFS\Retail\Code\Retail.Runtime\bin\x64\Release" /y /v /z /i /Q /R			"\\192.168.110.185\Fixes\WebSite\TOne.SMS\Runtime\%YYYYMMDD%\" /EXCLUDE:C:\TFS\Vanrise\Code\Resources\BatchScripts\TOne.SMS\runtime-list-of-excluded-files.txt
+xcopy "C:\TFS\Retail\Code\Retail.Runtime\App.config.SMS-WHS.exclude" /y /v /z /i /Q /R	"\\192.168.110.185\Fixes\WebSite\TOne.SMS\Runtime\%YYYYMMDD%\"
+xcopy "C:\TFS\Vanrise\Code\Resources\DLLs\Aspose.Cells.dll" /y /v /z /i /Q /R		"\\192.168.110.185\Fixes\WebSite\TOne.SMS\Runtime\%YYYYMMDD%\"
+xcopy "C:\TFS\Vanrise\Code\Resources\DLLs\Aspose.Cells.lic" /y /v /z /i /Q /R		"\\192.168.110.185\Fixes\WebSite\TOne.SMS\Runtime\%YYYYMMDD%\"
+
+rename "\\192.168.110.185\Fixes\WebSite\TOne.SMS\Runtime\%YYYYMMDD%\App.config.SMS-WHS.exclude" "TOne.SMS.Runtime.exe.config"
+rename "\\192.168.110.185\Fixes\WebSite\TOne.SMS\Runtime\%YYYYMMDD%\Retail.Runtime.exe" "TOne.SMS.Runtime.exe"
+del /s /q /f "\\192.168.110.185\Fixes\WebSite\TOne.SMS\Runtime\%YYYYMMDD%\*.pdb"
---
+++ Vanrise/Code/Resources/BatchScripts/TOne.SMS/03-WebSite.bat
@@ -0,0 +1,41 @@
+ECHO OFF
+::Make Directory from current date as YYYYMMDD
+Set CURRDATE=%TEMP%\CURRDATE.TMP
+DATE /T > %CURRDATE%
+Set PARSEARG="eol=; tokens=1,2,3,4* delims=/, "
+For /F %PARSEARG% %%i in (%CURRDATE%) Do SET YYYYMMDD=%%l%%j%%k
+
+ECHO.
+ECHO TOne.SMS WebSite
+
+xcopy "C:\Publish\Retail" /S /E /R /y /v /i /z /Q																	"\\192.168.110.185\Fixes\WebSite\TOne.SMS\%YYYYMMDD%\" /EXCLUDE:C:\TFS\Vanrise\Code\Resources\BatchScripts\TOne.SMS\list-of-excluded-files.txt
+xcopy "C:\TFS\Retail\Code\Retail.Web\Web.config.SMS-WHS.exclude" /y /v /z /i /Q /R									"\\192.168.110.185\Fixes\WebSite\TOne.SMS\%YYYYMMDD%\"
+xcopy "C:\TFS\Vanrise\Code\Resources\DLLs\Aspose.Cells.lic" /y /v /z /i /Q /R										"\\192.168.110.185\Fixes\WebSite\TOne.SMS\%YYYYMMDD%\Bin\"
+xcopy "C:\TFS\Vanrise\Code\Vanrise.Web\Client" /S /E /R /y /v /i /z /Q												"\\192.168.110.185\Fixes\WebSite\TOne.SMS\%YYYYMMDD%\Client\" /EXCLUDE:C:\TFS\Vanrise\Code\Resources\BatchScripts\TOne.SMS\Client-list-of-excluded-files.txt
+xcopy "C:\TFS\Vanrise\Code\Vanrise.Security.Web\Security" /S /E /R /y /v /i /z /Q									"\\192.168.110.185\Fixes\WebSite\TOne.SMS\%YYYYMMDD%\Client\Modules\Security\"
+xcopy "C:\TFS\Vanrise\Code\Vanrise.Common.Web\Common" /S /E /R /y /v /i /z /Q										"\\192.168.110.185\Fixes\WebSite\TOne.SMS\%YYYYMMDD%\Client\Modules\Common\"
+xcopy "C:\TFS\Vanrise\Code\Vanrise.AccountBalance.Web\VR_AccountBalance" /S /E /R /y /v /i /z /Q					"\\192.168.110.185\Fixes\WebSite\TOne.SMS\%YYYYMMDD%\Client\Modules\VR_AccountBalance\"
+xcopy "C:\TFS\Vanrise\Code\Vanrise.Notification.Web\VR_Notification" /S /E /R /y /v /i /z /Q						"\\192.168.110.185\Fixes\WebSite\TOne.SMS\%YYYYMMDD%\Client\Modules\VR_Notification\"
+xcopy "C:\TFS\Vanrise\Code\Vanrise.ExcelConversion.Web\ExcelConversion" /S /E /R /y /v /i /z /Q						"\\192.168.110.185\Fixes\WebSite\TOne.SMS\%YYYYMMDD%\Client\Modules\ExcelConversion\"
+xcopy "C:\TFS\Vanrise\Code\Vanrise.Runtime.Web\Runtime" /S /E /R /y /v /i /z /Q										"\\192.168.110.185\Fixes\WebSite\TOne.SMS\%YYYYMMDD%\Client\Modules\Runtime\"
+xcopy "C:\TFS\BusinessProcess\Code\Vanrise.Queueing.Web\Queueing" /S /E /R /y /v /i /z /Q							"\\192.168.110.185\Fixes\WebSite\TOne.SMS\%YYYYMMDD%\Client\Modules\Queueing\"
+xcopy "C:\TFS\Vanrise\Code\Vanrise.Rules.Web\VR_Rules" /S /E /R /y /v /i /z /Q										"\\192.168.110.185\Fixes\WebSite\TOne.SMS\%YYYYMMDD%\Client\Modules\VR_Rules\"
+xcopy "C:\TFS\Vanrise\Code\Vanrise.Integration.Web\Integration" /S /E /R /y /v /i /z /Q								"\\192.168.110.185\Fixes\WebSite\TOne.SMS\%YYYYMMDD%\Client\Modules\Integration\"
+xcopy "C:\TFS\Vanrise\Code\Vanrise.GenericData.Web\VR_GenericData" /S /E /R /y /v /i /z /Q							"\\192.168.110.185\Fixes\WebSite\TOne.SMS\%YYYYMMDD%\Client\Modules\VR_GenericData\"
+xcopy "C:\TFS\Vanrise\Code\Vanrise.Analytic.Web\Analytic" /S /E /R /y /v /i /z /Q									"\\192.168.110.185\Fixes\WebSite\TOne.SMS\%YYYYMMDD%\Client\Modules\Analytic\"
+xcopy "C:\TFS\BusinessProcess\Code\Vanrise.BusinessProcess.Web\BusinessProcess" /S /E /R /y /v /i /z /Q				"\\192.168.110.185\Fixes\WebSite\TOne.SMS\%YYYYMMDD%\Client\Modules\BusinessProcess\"
+xcopy "C:\TFS\Vanrise\Code\Vanrise.NumberingPlan.Web\VR_NumberingPlan" /S /E /R /y /v /i /z /Q						"\\192.168.110.185\Fixes\WebSite\TOne.SMS\%YYYYMMDD%\Client\Modules\VR_NumberingPlan\"
+xcopy "C:\TFS\Vanrise\Code\Vanrise.Invoice.Web\VR_Invoice" /S /E /R /y /v /i /z /Q									"\\192.168.110.185\Fixes\WebSite\TOne.SMS\%YYYYMMDD%\Client\Modules\VR_Invoice\"
+xcopy "C:\TFS\Vanrise\Code\Vanrise.InvToAccBalanceRelation.Web\VR_InvToAccBalanceRelation" /S /E /R /y /v /i /z /Q	"\\192.168.110.185\Fixes\WebSite\TOne.SMS\%YYYYMMDD%\Client\Modules\VR_InvToAccBalanceRelation\"
+xcopy "C:\TFS\BusinessProcess\Code\Vanrise.Reprocess.Web\Reprocess" /S /E /R /y /v /i /z /Q							"\\192.168.110.185\Fixes\WebSite\TOne.SMS\%YYYYMMDD%\Client\Modules\Reprocess\"
+xcopy "C:\TFS\Retail\Code\Retail.BusinessEntity.Web\Retail_BusinessEntity" /S /E /R /y /v /i /z /Q					"\\192.168.110.185\Fixes\WebSite\TOne.SMS\%YYYYMMDD%\Client\Modules\Retail_BusinessEntity\"
+xcopy "C:\TFS\Retail\Code\Retail.Invoice.Web\Retail_Invoice" /S /E /R /y /v /i /z /Q								"\\192.168.110.185\Fixes\WebSite\TOne.SMS\%YYYYMMDD%\Client\Modules\Retail_Invoice\"
+xcopy "C:\TFS\Retail\Code\Retail.Demo.Web\Retail_Demo" /S /E /R /y /v /i /z /Q										"\\192.168.110.185\Fixes\WebSite\TOne.SMS\%YYYYMMDD%\Client\Modules\Retail_Demo\"
+xcopy "C:\TFS\Retail\Code\Retail.Web\Images\SMS-WHS-logoonheader.png" /S /R /y /v /i /z /Q							"\\192.168.110.185\Fixes\WebSite\TOne.SMS\%YYYYMMDD%\Images\"
+xcopy "C:\TFS\Retail\Code\Retail.Web\Images\SMS-WHS-login.png" /S /R /y /v /i /z /Q									"\\192.168.110.185\Fixes\WebSite\TOne.SMS\%YYYYMMDD%\Images\"
+xcopy "C:\TFS\Retail\Code\Retail.Web\Images\SMS-WHS-iconheader.ico" /S /R /y /v /i /z /Q							"\\192.168.110.185\Fixes\WebSite\TOne.SMS\%YYYYMMDD%\Images\"
+
+rename "\\192.168.110.185\Fixes\WebSite\TOne.SMS\%YYYYMMDD%\Web.config.SMS-WHS.exclude" "Web.config"
+rename "\\192.168.110.185\Fixes\WebSite\TOne.SMS\%YYYYMMDD%\Images\SMS-WHS-logoonheader.png" "logoonheader.png"
+rename "\\192.168.110.185\Fixes\WebSite\TOne.SMS\%YYYYMMDD%\Images\SMS-WHS-login.png" "login.png"
+rename "\\192.168.110.185\Fixes\WebSite\TOne.SMS\%YYYYMMDD%\Images\SMS-WHS-iconheader.ico" "iconheader.ico"
