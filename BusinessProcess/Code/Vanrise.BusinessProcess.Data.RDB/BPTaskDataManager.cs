a267c0bb8889b08a00ddb97fb032cdae80023bae
Naim Saad <VANRISE\naim.saad>
2018-12-24 12:11:36 +0000

    Implementing BPTask RDB
    git-tfs-id: [http://developmentvlan:8585/tfs/vanrise.collection]$/;C52074

---
+++ BusinessProcess/Code/Vanrise.BusinessProcess.Data.RDB/BPTaskDataManager.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Vanrise.BusinessProcess.Entities;
+
+namespace Vanrise.BusinessProcess.Data.RDB
+{
+    public class BPTaskDataManager : IBPTaskDataManager
+    {
+        public void CancelNotCompletedTasks(long processInstanceId)
+        {
+            throw new NotImplementedException();
+        }
+
+        public List<BPTask> GetBeforeId(long lessThanID, int nbOfRows, int? processInstanceId, int? userId)
+        {
+            throw new NotImplementedException();
+        }
+
+        public BPTask GetTask(long taskId)
+        {
+            throw new NotImplementedException();
+        }
+
+        public List<BPTask> GetUpdated(ref object lastUpdateHandle, int nbOfRows, int? processInstanceId, int? userId)
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool InsertTask(string title, long processInstanceId, Guid typeId, List<int> assignedUserIds, BPTaskStatus bpTaskStatus, BPTaskData taskData, string assignedUsersDescription, out long taskId)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void UpdateTaskExecution(ExecuteBPTaskInput input, BPTaskStatus bpTaskStatus)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
