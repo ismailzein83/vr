8913fdfa27830e3bc719c96b3ffd6340e2e9f0ff
Naim Saad <VANRISE\naim.saad>
2018-12-24 08:24:04 +0000

    Implementing BPDataManager RDB
    git-tfs-id: [http://developmentvlan:8585/tfs/vanrise.collection]$/;C52051

---
+++ BusinessProcess/Code/Vanrise.BusinessProcess.Data.RDB/BPDataManager.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Vanrise.BusinessProcess.Data.RDB
+{
+    public class BPDataManager : IBPDataManager
+    {
+        public T GetDefinitionObjectState<T>(Guid definitionId, string objectKey)
+        {
+            throw new NotImplementedException();
+        }
+
+        public int InsertDefinitionObjectState(Guid definitionId, string objectKey, object objectValue)
+        {
+            throw new NotImplementedException();
+        }
+
+        public int UpdateDefinitionObjectState(Guid definitionId, string objectKey, object objectValue)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
