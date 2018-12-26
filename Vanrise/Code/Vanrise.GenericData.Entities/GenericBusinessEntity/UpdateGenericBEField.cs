aef38d019b3294e320abe29a12a8d1afbd8a1353
Mohammad Sheaib <VANRISE\mohammad.sheaib>
2018-12-26 16:00:26 +0000

    Moving classes to GenericData.Entities
    git-tfs-id: [http://developmentvlan:8585/tfs/vanrise.collection]$/;C52112

---
+++ Vanrise/Code/Vanrise.GenericData.Entities/GenericBusinessEntity/UpdateGenericBEField.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Vanrise.GenericData.Entities
+{
+    public class GetUpdateBulkActionsRuntimeInput
+    {
+        public Guid DataRecordTypeId { get; set; }
+        public List<UpdateGenericBEField> Fields { get; set; } 
+
+    }
+    public class UpdateGenericBEField
+    {
+        public string FieldName { get; set; }
+        public bool IsRequired { get; set; }
+        public object DefaultValue { get; set; }
+        public UpdateGenericBEFieldState FieldState { get; set; }
+    }
+    public enum UpdateGenericBEFieldState { ReadOnly = 1, Editable = 2, NotVisible = 3 }
+
+}
