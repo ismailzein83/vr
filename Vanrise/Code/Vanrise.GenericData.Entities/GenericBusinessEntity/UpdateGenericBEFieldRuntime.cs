58a5e23b8903c055b608a33c5efebe83e16b47ec
Mohammad Sheaib <VANRISE\mohammad.sheaib>
2018-12-26 16:08:18 +0000

    added Update Generic BE BulkAction classes
    git-tfs-id: [http://developmentvlan:8585/tfs/vanrise.collection]$/;C52113

---
+++ Vanrise/Code/Vanrise.GenericData.Entities/GenericBusinessEntity/UpdateGenericBEFieldRuntime.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Vanrise.GenericData.Entities
+{
+    public class UpdateGenericBEFieldRuntime
+    {
+        public string Name { get; set; }
+        public string Title { get; set; }
+        public DataRecordFieldType Type { get; set; }
+        public string RuntimeEditor { get; set; }
+    }
+}
