57170d2a16e27873e9c36426b60a740435b04336
Naim Saad <VANRISE\naim.saad>
2018-12-26 12:32:18 +0000

    Implementing BPTaskTypeDataManager RDB
    git-tfs-id: [http://developmentvlan:8585/tfs/vanrise.collection]$/;C52092

---
+++ BusinessProcess/Code/Vanrise.BusinessProcess.Data.RDB/BPTaskTypeDataManager.cs
@@ -0,0 +1,22 @@
+﻿//using System;
+//using System.Collections.Generic;
+//using System.Linq;
+//using System.Text;
+//using System.Threading.Tasks;
+//using Vanrise.BusinessProcess.Entities;
+
+//namespace Vanrise.BusinessProcess.Data.RDB
+//{
+//    public class BPTaskTypeDataManager : IBPTaskTypeDataManager
+//    {
+//        public bool AreBPTaskTypesUpdated(ref object updateHandle)
+//        {
+//            throw new NotImplementedException();
+//        }
+
+//        public IEnumerable<BPTaskType> GetBPTaskTypes()
+//        {
+//            throw new NotImplementedException();
+//        }
+//    }
+//}
