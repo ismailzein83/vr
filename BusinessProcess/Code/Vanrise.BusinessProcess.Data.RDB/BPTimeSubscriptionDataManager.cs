663ca859baeaf1a461c75a3ddb5958bc40e660d2
Naim Saad <VANRISE\naim.saad>
2018-12-26 13:51:52 +0000

    Implementing BPTimeSubscriptionDataManager RDB
    git-tfs-id: [http://developmentvlan:8585/tfs/vanrise.collection]$/;C52099

---
+++ BusinessProcess/Code/Vanrise.BusinessProcess.Data.RDB/BPTimeSubscriptionDataManager.cs
@@ -0,0 +1,27 @@
+﻿//using System;
+//using System.Collections.Generic;
+//using System.Linq;
+//using System.Text;
+//using System.Threading.Tasks;
+//using Vanrise.BusinessProcess.Entities;
+
+//namespace Vanrise.BusinessProcess.Data.RDB
+//{
+//    public class BPTimeSubscriptionDataManager : IBPTimeSubscriptionDataManager
+//    {
+//        public bool DeleteBPTimeSubscription(long bpTimeSubscriptionId)
+//        {
+//            throw new NotImplementedException();
+//        }
+
+//        public IEnumerable<BPTimeSubscription> GetDueBPTimeSubscriptions()
+//        {
+//            throw new NotImplementedException();
+//        }
+
+//        public int InsertBPTimeSubscription(long processInstanceId, string bookmarkName, TimeSpan delay, BPTimeSubscriptionPayload payload)
+//        {
+//            throw new NotImplementedException();
+//        }
+//    }
+//}
