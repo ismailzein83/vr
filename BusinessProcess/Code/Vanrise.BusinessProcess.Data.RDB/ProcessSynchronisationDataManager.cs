a4dc7f1f544cfc46cf65cca1cead91c3d5f9ab93
Naim Saad <VANRISE\naim.saad>
2018-12-26 14:42:16 +0000

    Implementing ProcessSynchronisationDataManager RDB
    git-tfs-id: [http://developmentvlan:8585/tfs/vanrise.collection]$/;C52104

---
+++ BusinessProcess/Code/Vanrise.BusinessProcess.Data.RDB/ProcessSynchronisationDataManager.cs
@@ -0,0 +1,42 @@
+﻿//using System;
+//using System.Collections.Generic;
+//using System.Linq;
+//using System.Text;
+//using System.Threading.Tasks;
+//using Vanrise.BusinessProcess.Entities;
+
+//namespace Vanrise.BusinessProcess.Data.RDB
+//{
+//    public class ProcessSynchronisationDataManager : IProcessSynchronisationDataManager
+//    {
+//        public bool AreProcessSynchronisationsUpdated(ref object updateHandle)
+//        {
+//            throw new NotImplementedException();
+//        }
+
+//        public bool DisableProcessSynchronisation(Guid processSynchronisationId, int lastModifiedBy)
+//        {
+//            throw new NotImplementedException();
+//        }
+
+//        public bool EnableProcessSynchronisation(Guid processSynchronisationId, int lastModifiedBy)
+//        {
+//            throw new NotImplementedException();
+//        }
+
+//        public List<ProcessSynchronisation> GetProcessSynchronisations()
+//        {
+//            throw new NotImplementedException();
+//        }
+
+//        public bool InsertProcessSynchronisation(Guid processSynchronisationId, ProcessSynchronisationToAdd processSynchronisationToAdd, int createdBy)
+//        {
+//            throw new NotImplementedException();
+//        }
+
+//        public bool UpdateProcessSynchronisation(ProcessSynchronisationToUpdate processSynchronisationToUpdate, int lastModifiedBy)
+//        {
+//            throw new NotImplementedException();
+//        }
+//    }
+//}
