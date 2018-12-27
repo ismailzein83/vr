9a8d7651f3c2471304fc66dc6ce6e8afbbeca7bf
Naim Saad <VANRISE\naim.saad>
2018-12-27 13:50:37 +0000

    Implementing BPValidationMessageDataManager RDB
    git-tfs-id: [http://developmentvlan:8585/tfs/vanrise.collection]$/;C52130

---
+++ BusinessProcess/Code/Vanrise.BusinessProcess.Data.RDB/BPValidationMessageDataManager.cs
@@ -0,0 +1,33 @@
+﻿//using System;
+//using System.Collections.Generic;
+//using System.Linq;
+//using System.Text;
+//using System.Threading.Tasks;
+//using Vanrise.BusinessProcess.Entities;
+//using Vanrise.Entities;
+
+//namespace Vanrise.BusinessProcess.Data.RDB
+//{
+//    public class BPValidationMessageDataManager : IBPValidationMessageDataManager
+//    {
+//        public List<BPValidationMessage> GetBeforeId(BPValidationMessageBeforeIdInput input)
+//        {
+//            throw new NotImplementedException();
+//        }
+
+//        public BigResult<BPValidationMessageDetail> GetFilteredBPValidationMessage(DataRetrievalInput<BPValidationMessageQuery> input)
+//        {
+//            throw new NotImplementedException();
+//        }
+
+//        public List<BPValidationMessage> GetUpdated(BPValidationMessageUpdateInput input)
+//        {
+//            throw new NotImplementedException();
+//        }
+
+//        public void Insert(IEnumerable<BPValidationMessage> messages)
+//        {
+//            throw new NotImplementedException();
+//        }
+//    }
+//}
