150afba403dc939c402130671f83cd0a29b37d8f
Ali Slayman <VANRISE\ali.slayman>
2018-12-27 13:03:23 +0000

    - changed platform of Runtime to X64 -  added AliSlaymanTask
    git-tfs-id: [http://developmentvlan:8585/tfs/vanrise.collection]$/;C52124

---
+++ TOneV2/Code/TOneV2/TestRuntime/Tasks/AliSlaymanTask.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using TOne.WhS.RouteSync.Entities;
+using TOne.WhS.RouteSync.Ericsson;
+using Vanrise.BusinessProcess;
+using Vanrise.Caching.Runtime;
+using Vanrise.Common.Business;
+using Vanrise.Queueing;
+using Vanrise.Runtime;
+using Vanrise.Runtime.Entities;
+
+namespace TestRuntime.Tasks
+{
+    class AliSlaymanTask : ITask
+    {
+        public void Execute()
+        {
+            var runtimeServices = new List<RuntimeService>();
+
+            BusinessProcessService bpService = new BusinessProcessService() { Interval = new TimeSpan(0, 0, 2) };
+            runtimeServices.Add(bpService);
+
+            BPRegulatorRuntimeService bpRegulatorRuntimeService = new BPRegulatorRuntimeService { Interval = new TimeSpan(0, 0, 2) };
+            runtimeServices.Add(bpRegulatorRuntimeService);
+
+            RuntimeHost host = new RuntimeHost(runtimeServices);
+            host.Start();
+
+            Console.ReadKey();
+        }
+    }
+}
