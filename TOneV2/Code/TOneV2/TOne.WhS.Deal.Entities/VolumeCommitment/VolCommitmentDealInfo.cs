f2ca244f2bb272b91ab396153249f9cd52d583a3
Ali Slayman <VANRISE\ali.slayman>
2018-12-27 16:11:51 +0000

    Editing VolCommitmentDealInfo
    git-tfs-id: [http://developmentvlan:8585/tfs/vanrise.collection]$/;C52147

--- TOneV2/Code/TOneV2/TOne.WhS.Deal.Entities/VolumeCommitment/VolCommitmentDealInfo.cs
+++ TOneV2/Code/TOneV2/TOne.WhS.Deal.Entities/VolumeCommitment/VolCommitmentDealInfo.cs
@@ -6,7 +6,7 @@ using System.Threading.Tasks;
 
 namespace TOne.WhS.Deal.Entities.VolumeCommitment
 {
-    class VolCommitmentDealInfo
+    class VolCommitmentDealInfo : DealDefinitionInfo
     {
     }
 }
