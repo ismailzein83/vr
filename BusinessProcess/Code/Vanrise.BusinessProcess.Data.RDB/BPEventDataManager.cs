29fea68791ab759fe54d568ab8bdaf40b4131a1d
Naim Saad <VANRISE\naim.saad>
2018-12-27 10:33:15 +0000

    Implementing BPEventDataManager RDB
    git-tfs-id: [http://developmentvlan:8585/tfs/vanrise.collection]$/;C52117

---
+++ BusinessProcess/Code/Vanrise.BusinessProcess.Data.RDB/BPEventDataManager.cs
@@ -0,0 +1,37 @@
+﻿//using System;
+//using System.Collections.Generic;
+//using System.Linq;
+//using System.Text;
+//using System.Threading.Tasks;
+//using Vanrise.BusinessProcess.Entities;
+
+//namespace Vanrise.BusinessProcess.Data.RDB
+//{
+//    public class BPEventDataManager : IBPEventDataManager
+//    {
+//        public void DeleteEvent(long eventId)
+//        {
+//            throw new NotImplementedException();
+//        }
+
+//        public void DeleteProcessInstanceEvents(long processInstanceId)
+//        {
+//            throw new NotImplementedException();
+//        }
+
+//        public List<long> GetEventsDistinctProcessInstanceIds()
+//        {
+//            throw new NotImplementedException();
+//        }
+
+//        public IEnumerable<BPEvent> GetInstancesEvents(List<long> instancesIds)
+//        {
+//            throw new NotImplementedException();
+//        }
+
+//        public int InsertEvent(long processInstanceId, string bookmarkName, object eventData)
+//        {
+//            throw new NotImplementedException();
+//        }
+//    }
+//}
