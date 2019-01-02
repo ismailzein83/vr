9600cc84a224365602748e8957a1fd5fe1489490
Ali Slayman <VANRISE\ali.slayman>
2019-01-02 14:25:16 +0000

    using VRTimerService @vrQueueingQueueinstanceGrid
    git-tfs-id: [http://developmentvlan:8585/tfs/vanrise.collection]$/;C52274

--- BusinessProcess/Code/Vanrise.Queueing.Web/Queueing/Directives/QueueInstance/vr-queueing-queueinstance-grid.js
+++ BusinessProcess/Code/Vanrise.Queueing.Web/Queueing/Directives/QueueInstance/vr-queueing-queueinstance-grid.js
@@ -1,10 +1,9 @@
 ﻿"use strict";
 
-app.directive("vrQueueingQueueinstanceGrid", ["VR_Queueing_QueueInstanceAPIService", "VR_Queueing_QueueInstanceService", "VR_Queueing_QueueItemHeaderAPIService", "VR_Queueing_ExecutionFlowService", 'VRNotificationService', 'VR_Queueing_QueueItemStatusEnum', 'LabelColorsEnum',
-    function (VR_Queueing_QueueInstanceAPIService, VR_Queueing_QueueInstanceService, VR_Queueing_QueueItemHeaderAPIService, VR_Queueing_ExecutionFlowService, VRNotificationService, VR_Queueing_QueueItemStatusEnum, LabelColorsEnum) {
+app.directive("vrQueueingQueueinstanceGrid", ["VR_Queueing_QueueInstanceAPIService", "VR_Queueing_QueueInstanceService", "VR_Queueing_QueueItemHeaderAPIService", "VRNotificationService", "VR_Queueing_QueueItemStatusEnum", "LabelColorsEnum", "VRTimerService",
+    function (VR_Queueing_QueueInstanceAPIService, VR_Queueing_QueueInstanceService, VR_Queueing_QueueItemHeaderAPIService, VRNotificationService, VR_Queueing_QueueItemStatusEnum, LabelColorsEnum, VRTimerService) {
 
         var directiveDefinitionObject = {
-
             restrict: "E",
             scope: {
                 onReady: "="
@@ -16,34 +15,40 @@ app.directive("vrQueueingQueueinstanceGrid", ["VR_Queueing_QueueInstanceAPIServi
             },
             controllerAs: "ctrl",
             bindToController: true,
-            compile: function (element, attrs) {
-
-            },
             templateUrl: "/Client/Modules/Queueing/Directives/QueueInstance/Templates/QueueInstanceGrid.html"
-
         };
 
         function ExecutionFlowGrid($scope, ctrl, $attrs) {
-
-            var gridAPI;
-            var isFirstTimeLoaded = false;
             this.initializeController = initializeController;
 
+            var gridAPI;
+
             function initializeController() {
+                $scope.queueInstances = [];
+
+                $scope.onGridReady = function (api) {
+                    gridAPI = api;
+                    defineAPI();
+                };
+
+                $scope.dataRetrievalFunction = function (dataRetrievalInput, onResponseReady) {
+                    return VR_Queueing_QueueInstanceAPIService.GetFilteredQueueInstances(dataRetrievalInput).then(function (queueInstancesResponse) {
+                        onResponseReady(queueInstancesResponse);
+                        createTimer();
+                    }).catch(function (error) {
+                        VRNotificationService.notifyExceptionWithClose(error, $scope);
+                    });
+                };
 
                 $scope.getNewItemsColor = function (dataItem) {
                     var color = undefined;
                     var newItemsCount = dataItem.newItemsCount;
                     if (newItemsCount != undefined) {
-
                         if (newItemsCount >= 5 && newItemsCount < 15)
                             color = LabelColorsEnum.Warning.color;
-
                         else if (newItemsCount > 15)
                             color = LabelColorsEnum.Error.color;
-
                     }
-
                     return color;
                 };
 
@@ -63,7 +68,6 @@ app.directive("vrQueueingQueueinstanceGrid", ["VR_Queueing_QueueInstanceAPIServi
                     return color;
                 };
 
-
                 $scope.getSuspendedColor = function (dataItem) {
                     var color = undefined;
                     var suspendedCount = dataItem.suspendedCount;
@@ -75,94 +79,63 @@ app.directive("vrQueueingQueueinstanceGrid", ["VR_Queueing_QueueInstanceAPIServi
                     return color;
                 };
 
-                
-
-
-                $scope.queueInstances = [];
-                $scope.ongridReady = function (api) {
-                    gridAPI = api;
-
-                    if (ctrl.onReady != undefined && typeof (ctrl.onReady) == "function") {
-                        ctrl.onReady(getDirectiveAPI());
-
-                    }
-
-                    function getDirectiveAPI() {
-
-                        var directiveAPI = {};
-                        directiveAPI.loadGrid = function (query) {
-
-                            return gridAPI.retrieveData(query);
-                        };
-
-                        return directiveAPI;
-                    }
-                };
-
-
-                $scope.dataRetrievalFunction = function (dataRetrievalInput, onResponseReady) {
-                    var allResult = [];
-                    return VR_Queueing_QueueInstanceAPIService.GetFilteredQueueInstances(dataRetrievalInput)
-                        .then(function (queueInstancesResponse) {
-                            onResponseReady(queueInstancesResponse);
-                            RefreshDataGrid();
-                            var timer;
-                            if (!isFirstTimeLoaded) {
-                                isFirstTimeLoaded = true;
-                                RefreshDataGrid();
-                                timer = setInterval(function(){
-                                    RefreshDataGrid();                                
-                                }, 5000);
-                            }
-
-                            $scope.$on('$destroy', function () {                               
-                                clearInterval(timer);
-                            });
-                        })
-                        .catch(function (error) {
-                            VRNotificationService.notifyExceptionWithClose(error, $scope);
-                        });
-                };
-
                 defineMenuActions();
             }
 
-            function RefreshDataGrid() {
+            function defineAPI() {
+                var api = {};
 
-                if ($scope.queueInstances != undefined) {
-                    VR_Queueing_QueueItemHeaderAPIService.GetItemStatusSummary().then(function (itemStatusSummaryResponse) {
-                        var newItemsCount = 0;
-                        var underProcessingCount = 0;
-                        var suspendedCount = 0;
-                        if ($scope.queueInstances != undefined) {
-                            for (var i = 0; i < $scope.queueInstances.length; i++) {
-                                newItemsCount = 0;
-                                underProcessingCount = 0;
-                                suspendedCount = 0;
-                                for (var j = 0; j < itemStatusSummaryResponse.length; j++) {
-                                    var status = itemStatusSummaryResponse[j].Status;
-                                    if (itemStatusSummaryResponse[j].QueueId == $scope.queueInstances[i].Entity.QueueInstanceId) {
-                                        if (status == VR_Queueing_QueueItemStatusEnum.New.value) {
-                                            newItemsCount += itemStatusSummaryResponse[j].Count;
-                                        }
-                                        else if (status == VR_Queueing_QueueItemStatusEnum.Processing.value || status == VR_Queueing_QueueItemStatusEnum.Failed.value) {
-                                            underProcessingCount += itemStatusSummaryResponse[j].Count;
-                                        }
-                                        else if (status == VR_Queueing_QueueItemStatusEnum.Suspended.value) {
-                                            suspendedCount += itemStatusSummaryResponse[j].Count;
-                                        }
-                                    }
-                                }
+                api.loadGrid = function (query) {
+                    return gridAPI.retrieveData(query);
+                };
 
-                                $scope.queueInstances[i].newItemsCount = newItemsCount;
-                                $scope.queueInstances[i].underProcessingCount = underProcessingCount;
-                                $scope.queueInstances[i].suspendedCount = suspendedCount;
+                if (ctrl.onReady != undefined && typeof (ctrl.onReady) == "function") {
+                    ctrl.onReady(api);
+                }
+            }
+
+            function createTimer() {
+                if ($scope.job) {
+                    VRTimerService.unregisterJob($scope.job);
+                }
+
+                VRTimerService.registerJob(onTimerElapsed, $scope, 5);
+            }
+
+            function onTimerElapsed() {
+                return VR_Queueing_QueueItemHeaderAPIService.GetItemStatusSummary().then(function (itemStatusSummaryResponse) {
+                    manipulateDataUpdated(itemStatusSummaryResponse);
+                }).finally(function () {
+                    $scope.isLoading = false;
+                });
+            }
+
+            function manipulateDataUpdated(itemStatusSummaryResponse) {
+
+                for (var i = 0; i < $scope.queueInstances.length; i++) {
+                    var newItemsCount = 0;
+                    var underProcessingCount = 0;
+                    var suspendedCount = 0;
+
+                    for (var j = 0; j < itemStatusSummaryResponse.length; j++) {
+                        var status = itemStatusSummaryResponse[j].Status;
+                        if (itemStatusSummaryResponse[j].QueueId == $scope.queueInstances[i].Entity.QueueInstanceId) {
+                            if (status == VR_Queueing_QueueItemStatusEnum.New.value) {
+                                newItemsCount += itemStatusSummaryResponse[j].Count;
+                            }
+                            else if (status == VR_Queueing_QueueItemStatusEnum.Processing.value || status == VR_Queueing_QueueItemStatusEnum.Failed.value) {
+                                underProcessingCount += itemStatusSummaryResponse[j].Count;
+                            }
+                            else if (status == VR_Queueing_QueueItemStatusEnum.Suspended.value) {
+                                suspendedCount += itemStatusSummaryResponse[j].Count;
                             }
                         }
-                    });
-                }
-              
+                    }
 
+                    $scope.queueInstances[i].newItemsCount = newItemsCount;
+                    $scope.queueInstances[i].underProcessingCount = underProcessingCount;
+                    $scope.queueInstances[i].suspendedCount = suspendedCount;
+                }
             }
 
             function defineMenuActions() {
@@ -173,15 +146,10 @@ app.directive("vrQueueingQueueinstanceGrid", ["VR_Queueing_QueueInstanceAPIServi
                             queueID: dataItem.Entity.QueueInstanceId
                         };
                         VR_Queueing_QueueInstanceService.showQueueItemInstances(parameters);
-
                     }
                 }];
             }
-
         }
 
-
-
         return directiveDefinitionObject;
-
     }]);
