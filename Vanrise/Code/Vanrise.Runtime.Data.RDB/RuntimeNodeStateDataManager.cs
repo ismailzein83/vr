8f23adfb16e57b7221b5d0940d696b558320b825
Naim Saad <VANRISE\naim.saad>
2019-01-02 09:29:42 +0000

    Implementing RuntimeNodeStateDataManager
    git-tfs-id: [http://developmentvlan:8585/tfs/vanrise.collection]$/;C52251

---
+++ Vanrise/Code/Vanrise.Runtime.Data.RDB/RuntimeNodeStateDataManager.cs
@@ -0,0 +1,32 @@
+﻿//using System;
+//using System.Collections.Generic;
+//using System.Linq;
+//using System.Text;
+//using System.Threading.Tasks;
+//using Vanrise.Runtime.Entities;
+
+//namespace Vanrise.Runtime.Data.RDB
+//{
+//    public class RuntimeNodeStateDataManager : IRuntimeNodeStateDataManager
+//    {
+//        public List<RuntimeNodeState> GetAllNodes()
+//        {
+//            throw new NotImplementedException();
+//        }
+
+//        public RuntimeNodeState GetNodeState(Guid runtimeNodeId)
+//        {
+//            throw new NotImplementedException();
+//        }
+
+//        public bool TrySetInstanceStarted(Guid runtimeNodeId, Guid serviceInstanceId, string machineName, int osProcessId, string osProcessName, string serviceURL, TimeSpan heartBeatTimeout)
+//        {
+//            throw new NotImplementedException();
+//        }
+
+//        public bool TryUpdateHeartBeat(Guid runtimeNodeId, Guid serviceInstanceId, decimal cpuUsage, decimal availableRAM, string diskInfos, int nbOfEnabledProcesses, int nbOfRunningProcesses)
+//        {
+//            throw new NotImplementedException();
+//        }
+//    }
+//}
