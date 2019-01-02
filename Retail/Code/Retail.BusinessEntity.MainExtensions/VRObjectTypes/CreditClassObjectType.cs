f4ea1612cc353406ded73e67cc469a1d37f09186
yara.bousalman <yara.bousalman@vanrise.tfs.local>
2019-01-02 13:29:38 +0000

    Adding credit class object type and property evaluator
    git-tfs-id: [http://developmentvlan:8585/tfs/vanrise.collection]$/;C52269

---
+++ Retail/Code/Retail.BusinessEntity.MainExtensions/VRObjectTypes/CreditClassObjectType.cs
@@ -0,0 +1,27 @@
+﻿using Retail.BusinessEntity.Business;
+using Retail.BusinessEntity.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Vanrise.Entities;
+
+namespace Retail.BusinessEntity.MainExtensions.VRObjectTypes
+{
+    public class CreditClassObjectType : VRObjectType
+    {
+        public override Guid ConfigId { get { return new Guid("2876106F-53E6-438F-8999-B88F479164C4"); } }
+        public Guid AccountBEDefinitionId { get; set; }
+        public override object CreateObject(IVRObjectTypeCreateObjectContext context)
+        {
+            AccountBEManager accountBEManager = new AccountBEManager();
+            Account account = accountBEManager.GetAccount(AccountBEDefinitionId, (long)context.ObjectId);
+
+            if (account == null)
+                throw new DataIntegrityValidationException(string.Format("Account not found for AccountBEDefinitionId : '{0}' and Account ID: '{1}'", AccountBEDefinitionId, context.ObjectId));
+
+            return account;
+        }
+    }
+}
---
+++ Retail/Code/Retail.BusinessEntity.MainExtensions/VRObjectTypes/CreditClassPropertyEvaluator.cs
@@ -0,0 +1,41 @@
+﻿using Retail.BusinessEntity.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+using Vanrise.Entities;
+using Vanrise.Common;
+using Retail.BusinessEntity.Business;
+using Vanrise.Common.Business;
+
+namespace Retail.BusinessEntity.MainExtensions.VRObjectTypes
+{
+    public enum CreditClassField { CreditLimit = 0, CreditLimitCurrency = 1, ConvertedCreditLimit = 2 }
+    public class CreditClassPropertyEvaluator : VRObjectPropertyEvaluator
+    {
+        public override Guid ConfigId { get { return new Guid("73E1C37B-BB45-442E-B7DA-19BA1597E0D8"); } }
+        public CreditClassField CreditClassField { get; set; }
+        public bool UseDescription { get; set; }
+        public override dynamic GetPropertyValue(IVRObjectPropertyEvaluatorContext context)
+        {
+            CreditClass creditClass = context.Object as CreditClass;
+            creditClass.ThrowIfNull("creditClass");
+            creditClass.Settings.ThrowIfNull("creditClass.Settings");
+            switch (CreditClassField)
+            {
+                case CreditClassField.ConvertedCreditLimit:
+                    return new CreditClassManager().GetConvertedCreditClassBalanceLimit(creditClass.Settings.BalanceLimit, creditClass.Settings.CurrencyId);
+                case CreditClassField.CreditLimit:
+                    return creditClass.Settings.BalanceLimit;
+                case CreditClassField.CreditLimitCurrency:
+                    if (UseDescription)
+                        return new CurrencyManager().GetCurrencyName(creditClass.Settings.CurrencyId);
+                    else
+                        return creditClass.Settings.CurrencyId;
+            }
+            return null;
+        }
+    }
+}
