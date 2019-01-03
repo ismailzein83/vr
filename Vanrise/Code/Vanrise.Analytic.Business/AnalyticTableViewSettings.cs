d822b089aa0a39767b9c8ca4a6476d19d60b150e
Hassan Makhour <VANRISE\hassan.makhour>
2019-01-03 10:22:46 +0000

    AnalyticTableViewSettings for switch dashboard security
    git-tfs-id: [http://developmentvlan:8585/tfs/vanrise.collection]$/;C52293

---
+++ Vanrise/Code/Vanrise.Analytic.Business/AnalyticTableViewSettings.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Vanrise.Security.Entities;
+
+namespace Vanrise.Analytic.Business
+{
+	public class AnalyticTableViewSettings:ViewSettings
+	{
+		public Guid AnalyticTableId { get; set; }
+		public virtual string GetURL(View view)
+		{
+			return view.Url;
+		}
+		public override bool DoesUserHaveAccess(IViewUserAccessContext context)
+		{
+			AnalyticTableManager _analytictableManager = new AnalyticTableManager();
+			return _analytictableManager.DoesUserHaveAccess(context.UserId, AnalyticTableId);
+		}
+	}
+}
