f453a2638455edfef847025c66848e9fee084458
Ali Atoui <VANRISE\ali.atoui>
2019-01-03 12:46:39 +0000

    Arranging ParserType
    git-tfs-id: [http://developmentvlan:8585/tfs/vanrise.collection]$/;C52299

--- Vanrise/Code/Vanrise.DataParser.Data/IParserTypeDataManager.cs
+++ Vanrise/Code/Vanrise.DataParser.Data/IParserTypeDataManager.cs
@@ -1,16 +1,16 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using System.Collections.Generic;
 using Vanrise.DataParser.Entities;
+
 namespace Vanrise.DataParser.Data
 {
     public interface IParserTypeDataManager:IDataManager
     {
         List<ParserType> GetParserTypes();
+
         bool AreParserTypesUpdated(ref object updateHandle);
+
         bool Update(Entities.ParserType parserType);
+
         bool Insert(ParserType parserType);
     }
 }
