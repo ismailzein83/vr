32c425884c49fe363dbe49c13b2f95c498e75dea
Rodi Hassan <VANRISE\rodi.hassan>
2019-01-03 10:38:05 +0000

    Implement MNCSettings
    git-tfs-id: [http://developmentvlan:8585/tfs/vanrise.collection]$/;C52294

---
+++ Retail/Code/Retail.RA.Business/MNC/MNCSettingsCustomObjectTypeSettings.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using Vanrise.GenericData.Entities;
+
+namespace Retail.RA.Business
+{
+    public class MNCSettingsCustomObjectTypeSettings : FieldCustomObjectTypeSettings
+    {
+        public override Guid ConfigId { get { return new Guid("E4B2A13C-66DB-462C-973E-F06765056B1D"); } }
+
+        public override string GetDescription(IFieldCustomObjectTypeSettingsContext context)
+        {
+            return null;
+        }
+
+        public override bool AreEqual(Object newValue, Object oldValue)
+        {
+            return true;
+        }
+
+        public override Type GetNonNullableRuntimeType()
+        {
+            return typeof(MNCSettings);
+        }
+
+        public override dynamic ParseNonNullValueToFieldType(object originalValue)
+        {
+            return originalValue as MNCSettings;
+        }
+
+        public override string GetRuntimeTypeDescription()
+        {
+            return "MNC Settings";
+        }
+    }
+
+    public class MNCSettings
+    {
+        public List<MNCCodes> Codes { get; set; }
+    }
+
+    public class MNCCodes
+    {
+        public string Code { get; set; }
+    }
+}
