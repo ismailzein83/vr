c53c748bd81f5d2a5a224e213efd6d2ce37b8c11
Naim Saad <VANRISE\naim.saad>
2019-01-03 08:45:38 +0000

    Implementing RuntimeServiceInstanceDataManager RDB
    git-tfs-id: [http://developmentvlan:8585/tfs/vanrise.collection]$/;C52284

---
+++ Vanrise/Code/Vanrise.Runtime.Data.RDB/RuntimeServiceInstanceDataManager.cs
@@ -0,0 +1,32 @@
+﻿//using System;
+//using System.Collections.Generic;
+//using System.Linq;
+//using System.Text;
+//using System.Threading.Tasks;
+//using Vanrise.Runtime.Entities;
+
+//namespace Vanrise.Runtime.Data.RDB
+//{
+//    public class RuntimeServiceInstanceDataManager : IRuntimeServiceInstanceDataManager
+//    {
+//        public void AddService(RuntimeServiceInstance serviceInstance)
+//        {
+//            throw new NotImplementedException();
+//        }
+
+//        public bool AreServiceInstancesUpdated(ref object updateHandle)
+//        {
+//            throw new NotImplementedException();
+//        }
+
+//        public void DeleteByProcessId(int runtimeProcessId)
+//        {
+//            throw new NotImplementedException();
+//        }
+
+//        public List<RuntimeServiceInstance> GetServices()
+//        {
+//            throw new NotImplementedException();
+//        }
+//    }
+//}
