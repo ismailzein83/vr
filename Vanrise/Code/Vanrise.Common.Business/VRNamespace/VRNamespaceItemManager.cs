25d13dc8577c672eb4edf29d3d24d826eb762fab
DevInternship User <VANRISE\Dev.InternshipUser>
2019-01-04 10:00:56 +0000

    adding files
    git-tfs-id: [http://developmentvlan:8585/tfs/vanrise.collection]$/;C52334

---
+++ Vanrise/Code/Vanrise.Common.Business/VRNamespace/VRNamespaceItemManager.cs
@@ -0,0 +1,263 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+using Vanrise.Common.Data;
+using Vanrise.Entities;
+using Vanrise.Security.Entities;
+
+namespace Vanrise.Common.Business
+{
+    public class VRNamespaceItemManager
+    {
+        VRNamespaceManager _vrNamespaceManager = new VRNamespaceManager();
+
+        #region Public Methods
+
+        public IDataRetrievalResult<VRNamespaceItemDetail> GetFilteredVRNamespaceItems(DataRetrievalInput<VRNamespaceItemQuery> input)
+        {
+            var allVRNamespaceItems = this.GetCachedVRNamespaceItems();
+            Func<VRNamespaceItem, bool> filterExpression = (namespaceItem) =>
+            {
+                if (input.Query.Name != null && !namespaceItem.Name.ToLower().Contains(input.Query.Name.ToLower()))
+                    return false;
+
+                if (input.Query.NameSpaceId.HasValue && input.Query.NameSpaceId.Value != namespaceItem.VRNamespaceId)
+                    return false;
+
+                return true;
+            };
+            return Vanrise.Common.DataRetrievalManager.Instance.ProcessResult(input, allVRNamespaceItems.ToBigResult(input, filterExpression, VRNamespaceItemDetailMapper));
+        }
+
+        public VRNamespaceItem GetVRNamespaceItem(Guid vrNamespaceItemId)
+        {
+            Dictionary<Guid, VRNamespaceItem> cachedVRNamespaceItems = this.GetCachedVRNamespaceItems();
+            return cachedVRNamespaceItems.GetRecord(vrNamespaceItemId);
+        }
+
+
+        public Vanrise.Entities.InsertOperationOutput<VRNamespaceItemDetail> AddVRNamespaceItem(VRNamespaceItem vrNamespaceItem)
+        {
+            var insertOperationOutput = new Vanrise.Entities.InsertOperationOutput<VRNamespaceItemDetail>();
+
+            insertOperationOutput.Result = Vanrise.Entities.InsertOperationResult.Failed;
+            insertOperationOutput.InsertedObject = null;
+
+            IVRNamespaceItemDataManager dataManager = CommonDataManagerFactory.GetDataManager<IVRNamespaceItemDataManager>();
+
+            vrNamespaceItem.VRNamespaceItemId = Guid.NewGuid();
+
+            int loggedInUserId = ContextFactory.GetContext().GetLoggedInUserId();
+            vrNamespaceItem.CreatedBy = loggedInUserId;
+            vrNamespaceItem.LastModifiedBy = loggedInUserId;
+
+            VRNamespaceItemCompilationOutput output;
+            if (TryCompileNamespaceItem(vrNamespaceItem, null, out output))
+            {
+
+                if (dataManager.Insert(vrNamespaceItem))
+                {
+                    Vanrise.Caching.CacheManagerFactory.GetCacheManager<CacheManager>().SetCacheExpired();
+                    insertOperationOutput.Result = Vanrise.Entities.InsertOperationResult.Succeeded;
+                    insertOperationOutput.InsertedObject = VRNamespaceItemDetailMapper(this.GetVRNamespaceItem(vrNamespaceItem.VRNamespaceItemId));
+                }
+                else
+                {
+                    insertOperationOutput.Result = Vanrise.Entities.InsertOperationResult.SameExists;
+                }
+            }
+
+            return insertOperationOutput;
+        }
+
+        public Vanrise.Entities.UpdateOperationOutput<VRNamespaceItemDetail> UpdateVRNamespaceItem(VRNamespaceItem vrNamespaceItem)
+        {
+
+            var updateOperationOutput = new Vanrise.Entities.UpdateOperationOutput<VRNamespaceItemDetail>();
+            updateOperationOutput.Result = Vanrise.Entities.UpdateOperationResult.Failed;
+            updateOperationOutput.UpdatedObject = null;
+
+            vrNamespaceItem.LastModifiedBy = ContextFactory.GetContext().GetLoggedInUserId();
+
+            IVRNamespaceItemDataManager dataManager = CommonDataManagerFactory.GetDataManager<IVRNamespaceItemDataManager>();
+
+
+            VRNamespaceItemCompilationOutput output;
+            if (TryCompileNamespaceItem(vrNamespaceItem, null, out output))
+            {
+                if (dataManager.Update(vrNamespaceItem))
+                {
+                    Vanrise.Caching.CacheManagerFactory.GetCacheManager<CacheManager>().SetCacheExpired();
+                    updateOperationOutput.Result = Vanrise.Entities.UpdateOperationResult.Succeeded;
+                    updateOperationOutput.UpdatedObject = VRNamespaceItemDetailMapper(this.GetVRNamespaceItem(vrNamespaceItem.VRNamespaceItemId));
+                }
+                else
+                {
+                    updateOperationOutput.Result = Vanrise.Entities.UpdateOperationResult.SameExists;
+                }
+            }
+
+            return updateOperationOutput;
+        }
+        public IEnumerable<VRDynamicCodeConfig> GetVRDynamicCodeSettingsConfigs()
+        {
+            var extensionConfigurationManager = new ExtensionConfigurationManager();
+            return extensionConfigurationManager.GetExtensionConfigurations<VRDynamicCodeConfig>(VRDynamicCodeConfig.EXTENSION_TYPE);
+        }
+
+        public VRNamespaceItemCompilationOutput TryCompileNamespaceItem(VRNamespaceItem vrNamespaceItem)
+        {
+            VRNamespaceItemCompilationOutput output;
+            if (TryCompileNamespaceItem(vrNamespaceItem, null, out output))
+                CSharpCompiler.SetExpiredAssembly(output.OutputAssembly.FullName);
+
+            return output;
+        }
+
+        public IEnumerable<VRNamespaceItem> GetVRNamespaceItemsByNamespaceId(Guid vrNamespaceId)
+        {
+            var allVRNamespaceItems = this.GetCachedVRNamespaceItems();
+            return allVRNamespaceItems.FindAllRecords(x => x.VRNamespaceId == vrNamespaceId);
+        }
+
+        #endregion
+
+        #region Private Methods
+
+        private Dictionary<Guid, VRNamespaceItem> GetCachedVRNamespaceItems()
+        {
+            return Vanrise.Caching.CacheManagerFactory.GetCacheManager<CacheManager>().GetOrCreateObject("GetCachedVRNamespaceItems",
+               () =>
+               {
+                   IVRNamespaceItemDataManager dataManager = CommonDataManagerFactory.GetDataManager<IVRNamespaceItemDataManager>();
+                   return dataManager.GetVRNamespaceItems().ToDictionary(x => x.VRNamespaceItemId, x => x);
+               });
+        }
+
+        private bool TryCompileNamespaceItem(VRNamespaceItem vrNamespaceItem, string uniqueNamespaceItemName, out VRNamespaceItemCompilationOutput namespaceItemCompilationOutput)
+        {
+            StringBuilder classDefinition = new StringBuilder(@"using System;
+            using System.Collections.Generic;
+            using System.IO;
+            using System.Data;
+            using System.Linq;
+            using Vanrise.Common;
+            using Vanrise.GenericData.Business;
+            using Vanrise.GenericData.Entities;");
+
+
+            classDefinition.AppendLine(BuildClassDefinition(vrNamespaceItem));
+
+            if (!string.IsNullOrEmpty(uniqueNamespaceItemName))
+                classDefinition.AppendLine(GenerateDummyNamespaceItem(uniqueNamespaceItemName));
+
+            CSharpCompilationOutput compilationOutput;
+            if (!CSharpCompiler.TryCompileClass(classDefinition.ToString(), out compilationOutput, true))
+            {
+                StringBuilder errorsBuilder = new StringBuilder();
+                if (compilationOutput.ErrorMessages != null)
+                {
+                    foreach (var errorMessage in compilationOutput.ErrorMessages)
+                        errorsBuilder.AppendLine(errorMessage);
+                }
+
+                namespaceItemCompilationOutput = new VRNamespaceItemCompilationOutput
+                {
+                    OutputAssembly = compilationOutput.OutputAssembly,
+                    Result = false,
+                    ErrorMessages = compilationOutput.ErrorMessages
+                };
+            }
+            else
+            {
+                namespaceItemCompilationOutput = new VRNamespaceItemCompilationOutput
+                {
+                    OutputAssembly = compilationOutput.OutputAssembly,
+                    Result = true,
+                    ErrorMessages = null
+                };
+            }
+            return namespaceItemCompilationOutput.Result;
+        }
+
+        private string GenerateDummyNamespaceItem(string uniqueNamespaceItemName)
+        {
+            return string.Concat("namespace ", uniqueNamespaceItemName, "{public class DummyClass{", "}}");
+        }
+
+        private string BuildClassDefinition(VRNamespaceItem vrNamespaceItem)
+        {
+            VRNamespace vrNamespace = _vrNamespaceManager.GetVRNamespace(vrNamespaceItem.VRNamespaceId);
+            StringBuilder classDefinitionBuilder = new StringBuilder(@" 
+                namespace #NAMESPACE#
+                {
+                    #NAMESPACEMEMBERS#
+
+                    #CLASSCODE#
+                }");
+
+            StringBuilder classCode = new StringBuilder();
+            StringBuilder namespaceMembers = new StringBuilder();
+            if (vrNamespaceItem.Settings != null && vrNamespaceItem.Settings.Code != null)
+            {
+                var context = new VRDynamicCodeSettingsContext();
+                classCode.Append(vrNamespaceItem.Settings.Code.Generate(context));
+                if (context.NamespaceMembers != null)
+                {
+                    if (namespaceMembers.Length > 0)
+                        namespaceMembers.AppendLine();
+                    namespaceMembers.Append(context.NamespaceMembers);
+                }
+            }
+
+            classDefinitionBuilder.Replace("#NAMESPACE#", vrNamespace.Name);
+            classDefinitionBuilder.Replace("#CLASSCODE#", classCode.ToString());
+            classDefinitionBuilder.Replace("#NAMESPACEMEMBERS#", namespaceMembers.ToString());
+
+            return classDefinitionBuilder.ToString();
+        }
+
+       
+        #endregion
+
+        #region Classes
+
+        private class CacheManager : Vanrise.Caching.BaseCacheManager
+        {
+            IVRNamespaceItemDataManager _dataManager = CommonDataManagerFactory.GetDataManager<IVRNamespaceItemDataManager>();
+            object _updateHandle;
+
+            protected override bool ShouldSetCacheExpired(object parameter)
+            {
+                return _dataManager.AreVRNamespaceItemUpdated(ref _updateHandle);
+            }
+        }
+
+        public class VRNamespaceItemCompilationOutput
+        {
+            public Assembly OutputAssembly { get; set; }
+            public bool Result { get; set; }
+            public List<string> ErrorMessages { get; set; }
+        }
+
+        #endregion
+
+        #region Mappers
+
+        public VRNamespaceItemDetail VRNamespaceItemDetailMapper(VRNamespaceItem vrNamespaceItem)
+        {
+            VRNamespaceItemDetail vrNamespaceItemDetail = new VRNamespaceItemDetail()
+            {
+                VRNamespaceItemId = vrNamespaceItem.VRNamespaceItemId,
+                Name = vrNamespaceItem.Name,
+                CreatedByDescription = BEManagerFactory.GetManager<IUserManager>().GetUserName(vrNamespaceItem.CreatedBy)
+            };
+            return vrNamespaceItemDetail;
+        }
+
+        #endregion
+    }
+}
