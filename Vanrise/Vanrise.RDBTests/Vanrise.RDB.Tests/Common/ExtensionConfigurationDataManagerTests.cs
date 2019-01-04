4fc1dd43717ce4880efad52d362eb86b45243190
Samer Haidar <VANRISE\samer.haidar>
2019-01-04 10:28:06 +0000

    Adding ExtensionConfigurationDataManagerTests
    git-tfs-id: [http://developmentvlan:8585/tfs/vanrise.collection]$/;C52341

---
+++ Vanrise/Vanrise.RDBTests/Vanrise.RDB.Tests/Common/ExtensionConfigurationDataManagerTests.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Vanrise.Common.Data;
+using Vanrise.Entities;
+using Vanrise.RDBTests.Common;
+namespace Vanrise.RDB.Tests.Common
+{
+    [TestClass]
+    public class ExtensionConfigurationDataManagerTests
+    {
+        const string DBTABLE_NAME_Rule = "[ExtensionConfiguration]";
+        IExtensionConfigurationDataManager _rdbDataManager = RDBDataManagerFactory.GetDataManager<IExtensionConfigurationDataManager>();
+        IExtensionConfigurationDataManager _sqlDataManager = SQLDataManagerFactory.GetDataManager<IExtensionConfigurationDataManager>();
+        [TestMethod]
+        public void GetExtensionConfigurationsByType()
+        {
+            var ruleChanged1 = _sqlDataManager.GetExtensionConfigurationsByType<TextManipulationActionSettingsConfig>(TextManipulationActionSettingsConfig.EXTENSION_TYPE);
+            var ruleChanged2 = _rdbDataManager.GetExtensionConfigurationsByType<TextManipulationActionSettingsConfig>(TextManipulationActionSettingsConfig.EXTENSION_TYPE);
+            UTUtilities.AssertObjectsAreSimilar(ruleChanged1, ruleChanged2);
+        }
+    }
+}
