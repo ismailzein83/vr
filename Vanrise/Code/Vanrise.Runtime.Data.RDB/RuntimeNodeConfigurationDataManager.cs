9d79323fc1c3aac0a10fa48eac9c00550d031596
Naim Saad <VANRISE\naim.saad>
2019-01-04 14:26:15 +0000

    Implementing RuntimeServiceInstanceDataManager RDB
    git-tfs-id: [http://developmentvlan:8585/tfs/vanrise.collection]$/;C52372

---
+++ Vanrise/Code/Vanrise.Runtime.Data.RDB/RuntimeNodeConfigurationDataManager.cs
@@ -0,0 +1,32 @@
+﻿//using System;
+//using System.Collections.Generic;
+//using System.Linq;
+//using System.Text;
+//using System.Threading.Tasks;
+//using Vanrise.Runtime.Entities;
+
+//namespace Vanrise.Runtime.Data.RDB
+//{
+//    public class RuntimeNodeConfigurationDataManager : IRuntimeNodeConfigurationDataManager
+//    {
+//        public bool AreRuntimeNodeConfigurationUpdated(ref object updateHandle)
+//        {
+//            throw new NotImplementedException();
+//        }
+
+//        public List<RuntimeNodeConfiguration> GetAllNodeConfigurations()
+//        {
+//            throw new NotImplementedException();
+//        }
+
+//        public bool Insert(RuntimeNodeConfiguration runtimeNodeConfiguration)
+//        {
+//            throw new NotImplementedException();
+//        }
+
+//        public bool Update(RuntimeNodeConfiguration runtimeNodeConfiguration)
+//        {
+//            throw new NotImplementedException();
+//        }
+//    }
+//}
