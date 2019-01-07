e9dd90efbfe6a0507c1d887f6da7507c398cb308
Ali Sakr <VANRISE\ali.sakr>
2019-01-07 14:55:10 +0000

    adding StatusDefinitionStyle class
    git-tfs-id: [http://developmentvlan:8585/tfs/vanrise.collection]$/;C52405

---
+++ Vanrise/Code/Vanrise.Entities/StatusDefinition/StatusDefinitionStyle.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Vanrise.Entities
+{
+   public class StatusDefinitionStyle
+    {
+        public Guid StatusDefinitionId { get; set; }
+        public string Name { get; set; }
+        public StyleFormatingSettings StyleFormatingSettings { get; set; }
+    }
+}
