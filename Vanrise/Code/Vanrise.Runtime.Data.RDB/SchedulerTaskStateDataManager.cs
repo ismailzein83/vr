cfece11078d416f440ea30b84a27d2e4b41f50b6
Naim Saad <VANRISE\naim.saad>
2019-01-07 12:37:43 +0000

    Implementing SchedulerTaskStateDataManager RDB
    git-tfs-id: [http://developmentvlan:8585/tfs/vanrise.collection]$/;C52398

---
+++ Vanrise/Code/Vanrise.Runtime.Data.RDB/SchedulerTaskStateDataManager.cs
@@ -0,0 +1,57 @@
+﻿//using System;
+//using System.Collections.Generic;
+//using System.Linq;
+//using System.Text;
+//using System.Threading.Tasks;
+//using Vanrise.Runtime.Entities;
+
+//namespace Vanrise.Runtime.Data.RDB
+//{
+//    class SchedulerTaskStateDataManager : ISchedulerTaskStateDataManager
+//    {
+//        public void DeleteTaskState(Guid taskId)
+//        {
+//            throw new NotImplementedException();
+//        }
+
+//        public List<SchedulerTaskState> GetAllScheduleTaskStates()
+//        {
+//            throw new NotImplementedException();
+//        }
+
+//        public SchedulerTaskState GetSchedulerTaskStateByTaskId(Guid taskId)
+//        {
+//            throw new NotImplementedException();
+//        }
+
+//        public List<SchedulerTaskState> GetSchedulerTaskStateByTaskIds(List<Guid> taskIds)
+//        {
+//            throw new NotImplementedException();
+//        }
+
+//        public void InsertSchedulerTaskState(Guid taskId)
+//        {
+//            throw new NotImplementedException();
+//        }
+
+//        public void RunSchedulerTask(Guid taskId, bool allowRunIfEnabled)
+//        {
+//            throw new NotImplementedException();
+//        }
+
+//        public bool TryLockTask(Guid taskId, int currentRuntimeProcessId, IEnumerable<int> runningRuntimeProcessesIds)
+//        {
+//            throw new NotImplementedException();
+//        }
+
+//        public void UnlockTask(Guid taskId)
+//        {
+//            throw new NotImplementedException();
+//        }
+
+//        public bool UpdateTaskState(SchedulerTaskState taskStateObject)
+//        {
+//            throw new NotImplementedException();
+//        }
+//    }
+//}
