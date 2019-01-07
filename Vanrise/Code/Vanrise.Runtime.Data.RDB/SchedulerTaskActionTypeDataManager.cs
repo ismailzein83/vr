c449c45bf2125ec58e5af2acf5a17f7649b8a39f
Naim Saad <VANRISE\naim.saad>
2019-01-07 08:36:15 +0000

    Implementing SchedulerTaskActionTypeDataManager RDB
    git-tfs-id: [http://developmentvlan:8585/tfs/vanrise.collection]$/;C52384

---
+++ Vanrise/Code/Vanrise.Runtime.Data.RDB/SchedulerTaskActionTypeDataManager.cs
@@ -0,0 +1,22 @@
+﻿//using System;
+//using System.Collections.Generic;
+//using System.Linq;
+//using System.Text;
+//using System.Threading.Tasks;
+//using Vanrise.Runtime.Entities;
+
+//namespace Vanrise.Runtime.Data.RDB
+//{
+//    class SchedulerTaskActionTypeDataManager : ISchedulerTaskActionTypeDataManager
+//    {
+//        public bool AreSchedulerTaskActionTypesUpdated(ref object updateHandle)
+//        {
+//            throw new NotImplementedException();
+//        }
+
+//        public List<SchedulerTaskActionType> GetAll()
+//        {
+//            throw new NotImplementedException();
+//        }
+//    }
+//}
