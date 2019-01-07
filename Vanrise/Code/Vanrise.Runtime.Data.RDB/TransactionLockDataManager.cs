5c2fcfd29120ea87d147da93977c72a6fa5ece6c
Naim Saad <VANRISE\naim.saad>
2019-01-07 13:46:38 +0000

    Implementing TransactionLockDataManager RDB
    git-tfs-id: [http://developmentvlan:8585/tfs/vanrise.collection]$/;C52402

---
+++ Vanrise/Code/Vanrise.Runtime.Data.RDB/TransactionLockDataManager.cs
@@ -0,0 +1,32 @@
+﻿//using System;
+//using System.Collections.Generic;
+//using System.Linq;
+//using System.Text;
+//using System.Threading.Tasks;
+//using Vanrise.Runtime.Entities;
+
+//namespace Vanrise.Runtime.Data.RDB
+//{
+//    class TransactionLockDataManager : ITransactionLockDataManager
+//    {
+//        public void Add(TransactionLockItem lockItem)
+//        {
+//            throw new NotImplementedException();
+//        }
+
+//        public void Delete(Guid lockItemId)
+//        {
+//            throw new NotImplementedException();
+//        }
+
+//        public List<TransactionLockItem> GetAll()
+//        {
+//            throw new NotImplementedException();
+//        }
+
+//        public List<TransactionLockItem> GetLocksForNotRunningProcesses(List<int> runningProcessesIds)
+//        {
+//            throw new NotImplementedException();
+//        }
+//    }
+//}
