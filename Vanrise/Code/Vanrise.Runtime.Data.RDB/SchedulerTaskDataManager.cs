166bef033b3abb905b3128d2cc68022808fbcc63
Naim Saad <VANRISE\naim.saad>
2019-01-07 08:57:51 +0000

    Implementing SchedulerTaskDataManager RDB
    git-tfs-id: [http://developmentvlan:8585/tfs/vanrise.collection]$/;C52389

---
+++ Vanrise/Code/Vanrise.Runtime.Data.RDB/SchedulerTaskDataManager.cs
@@ -0,0 +1,47 @@
+﻿//using System;
+//using System.Collections.Generic;
+//using System.Linq;
+//using System.Text;
+//using System.Threading.Tasks;
+//using Vanrise.Runtime.Entities;
+
+//namespace Vanrise.Runtime.Data.RDB
+//{
+//    class SchedulerTaskDataManager : ISchedulerTaskDataManager
+//    {
+//        public bool AddTask(SchedulerTask taskObject)
+//        {
+//            throw new NotImplementedException();
+//        }
+
+//        public bool AreSchedulerTasksUpdated(ref object updateHandle)
+//        {
+//            throw new NotImplementedException();
+//        }
+
+//        public bool DeleteTask(Guid taskId)
+//        {
+//            throw new NotImplementedException();
+//        }
+
+//        public bool DisableTask(Guid taskId)
+//        {
+//            throw new NotImplementedException();
+//        }
+
+//        public bool EnableTask(Guid taskId)
+//        {
+//            throw new NotImplementedException();
+//        }
+
+//        public IEnumerable<SchedulerTask> GetSchedulerTasks()
+//        {
+//            throw new NotImplementedException();
+//        }
+
+//        public bool UpdateTaskInfo(Guid taskId, string name, bool isEnabled, Guid triggerTypeId, Guid actionTypeId, SchedulerTaskSettings taskSettings)
+//        {
+//            throw new NotImplementedException();
+//        }
+//    }
+//}
