80d15cf150ea26eed5f3a22e63a2ce543bbcdfd4
Ali Slayman <VANRISE\ali.slayman>
2019-01-07 15:04:41 +0000

    Adding ImportedBatchProcessState
    git-tfs-id: [http://developmentvlan:8585/tfs/vanrise.collection]$/;C52407

---
+++ Vanrise/Code/Vanrise.Integration.Entities/Receive/ImportedBatchProcessState.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Vanrise.Entities;
+
+namespace Vanrise.Integration.Entities.Receive
+{
+    public class ImportedBatchProcessState : ProcessState
+    {
+        public override Guid Id { get; }
+
+    }
+}
