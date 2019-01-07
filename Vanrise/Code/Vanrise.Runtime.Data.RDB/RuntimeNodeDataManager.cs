54e242a78e3892dc81aa3dff10aa0494c1155e0a
Naim Saad <VANRISE\naim.saad>
2019-01-07 12:08:23 +0000

    Implementing RuntimeNodeDataManager RDB
    git-tfs-id: [http://developmentvlan:8585/tfs/vanrise.collection]$/;C52396

---
+++ Vanrise/Code/Vanrise.Runtime.Data.RDB/RuntimeNodeDataManager.cs
@@ -0,0 +1,32 @@
+﻿//using System;
+//using System.Collections.Generic;
+//using System.Linq;
+//using System.Text;
+//using System.Threading.Tasks;
+//using Vanrise.Runtime.Entities;
+
+//namespace Vanrise.Runtime.Data.RDB
+//{
+//    class RuntimeNodeDataManager : IRuntimeNodeDataManager
+//    {
+//        public bool AreRuntimeNodeUpdated(ref object updateHandle)
+//        {
+//            throw new NotImplementedException();
+//        }
+
+//        public List<RuntimeNode> GetAllNodes()
+//        {
+//            throw new NotImplementedException();
+//        }
+
+//        public bool Insert(RuntimeNode runtimeNode)
+//        {
+//            throw new NotImplementedException();
+//        }
+
+//        public bool Update(RuntimeNode runtimeNode)
+//        {
+//            throw new NotImplementedException();
+//        }
+//    }
+//}
