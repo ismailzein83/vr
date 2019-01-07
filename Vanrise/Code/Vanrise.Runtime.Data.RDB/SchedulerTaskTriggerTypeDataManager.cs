3ed202b5ea40151db1bb30a111e42ae72a5508c4
Naim Saad <VANRISE\naim.saad>
2019-01-07 09:24:29 +0000

    Implementing SchedulerTaskTriggerTypeDataManager RDB
    git-tfs-id: [http://developmentvlan:8585/tfs/vanrise.collection]$/;C52390

---
+++ Vanrise/Code/Vanrise.Runtime.Data.RDB/SchedulerTaskTriggerTypeDataManager.cs
@@ -0,0 +1,22 @@
+﻿//using System;
+//using System.Collections.Generic;
+//using System.Linq;
+//using System.Text;
+//using System.Threading.Tasks;
+//using Vanrise.Runtime.Entities;
+
+//namespace Vanrise.Runtime.Data.RDB
+//{
+//    class SchedulerTaskTriggerTypeDataManager : ISchedulerTaskTriggerTypeDataManager
+//    {
+//        public bool AreSchedulerTaskTriggerTypesUpdated(ref object updateHandle)
+//        {
+//            throw new NotImplementedException();
+//        }
+
+//        public List<SchedulerTaskTriggerType> GetAll()
+//        {
+//            throw new NotImplementedException();
+//        }
+//    }
+//}
