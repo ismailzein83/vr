739ffe99b6d2f07d0575014910b905cd4fe8e007
Ali Sakr <VANRISE\ali.sakr>
2019-01-08 10:17:17 +0000

    adding status definition viewer directive
    git-tfs-id: [http://developmentvlan:8585/tfs/vanrise.collection]$/;C52423

---
+++ Vanrise/Code/Vanrise.Common.Web/Common/Directives/StatusDefinition/vr-common-statusdefinition-viewer.js
@@ -0,0 +1,91 @@
+﻿//(function (app) {
+
+//    'use strict';
+
+//    StatusDefinitionViewer.$inject = ['UtilsService', 'VRUIUtilsService', 'VR_Common_StatusDefinitionAPIService','VR_Analytic_AnalyticTableService'];
+
+//    function StatusDefinitionViewer(UtilsService, VRUIUtilsService, VR_Common_StatusDefinitionAPIService, VR_Analytic_AnalyticTableService) {
+//        return {
+//            restrict: 'E',
+//            scope: {
+//                onReady: '=',
+//            },
+//            controller: function ($scope, $element, $attrs) {
+//                var ctrl = this;
+//                var statusDefinitionViewer = new StatusDefinitionViewer(ctrl, $scope, $attrs);
+//                statusDefinitionViewer.initializeController();
+//            },
+//            controllerAs: 'ctrl',
+//            bindToController: true,
+//            compile: function (element, attrs) {
+//                return {
+//                    pre: function ($scope, iElem, iAttrs, ctrl) {
+
+//                    }
+//                };
+//            },
+
+//            templateUrl: function (element, attrs) {
+//                return '/Client/Modules/Common/Directives/StatusDefinition/Templates/StatusDefinitionViewerTemplate.html';
+//            }
+//        };
+
+//        function StatusDefinitionViewer(ctrl, $scope, attrs) {
+//            this.initializeController = initializeController;
+
+//            var highlightedId;
+//            var statusBeDefinitionId;
+//            var analyticTableId;
+
+//            function initializeController() {
+//                $scope.scopeModel = {};
+//                $scope.statusDefinitionStyles = [];
+//                $scope.onHelpBoxClicked = function () {
+//                    VR_Analytic_AnalyticTableService.viewMeasureStyles(analyticTableId,statusBeDefinitionId,highlightedId);
+//                };
+//                $scope.showHelpBox = false;
+//                defineAPI();
+//            }
+
+//            function defineAPI() {
+
+//                var api = {};
+
+//                api.load = function (payload) {
+//                    if (payload != undefined) {
+//                        statusBeDefinitionId = payload.statusBeDefinitionId;
+//                        $scope.showHelpBox = statusBeDefinitionId != undefined;
+//                        highlightedId = payload.highlightedId;
+//                        analyticTableId = payload.analyticTableId;
+
+
+//                        if (statusBeDefinitionId != undefined) {
+//                            VR_Common_StatusDefinitionAPIService.GetStatusDefinitionStylesByBusinessEntityId(statusBeDefinitionId).then(function (response) {
+//                                if (response != undefined) {
+//                                    for (var i = 0; i < response.length; i++) {
+//                                        var statusDefinitionStyle = response[i];
+//                                        if (statusDefinitionStyle.StatusDefinitionId == highlightedId)
+//                                            statusDefinitionStyle.LabelClass = "bold-label"
+//                                        $scope.statusDefinitionStyles.push(statusDefinitionStyle);
+//                                    }
+//                                }
+
+//                            });
+//                        }
+//                    }
+//                };
+
+//                api.getData = function () {
+
+//                };
+
+//                if (ctrl.onReady != undefined && typeof (ctrl.onReady) == 'function') {
+//                    ctrl.onReady(api);
+//                }
+//            }
+//        }
+//    }
+
+//    app.directive('vrCommonStatusdefinitionViewer', StatusDefinitionViewer);
+
+//})(app);
