2ba4d387dcc69b421aff1fb01337f45896a6ed6f
yara.bousalman <yara.bousalman@vanrise.tfs.local>
2019-01-08 14:07:42 +0000

    Adding CookieNameDataManager and EncryptionKeyDataManager
    git-tfs-id: [http://developmentvlan:8585/tfs/vanrise.collection]$/;C52443

--- Vanrise/Code/Vanrise.Security.Data.RDB/CookieNameDataManager.cs
+++ Vanrise/Code/Vanrise.Security.Data.RDB/CookieNameDataManager.cs
@@ -1,12 +1,64 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using Vanrise.Data.RDB;
 
 namespace Vanrise.Security.Data.RDB
 {
-    class CookieNameDataManager
+    public class CookieNameDataManager : ICookieNameDataManager
     {
+        #region RDB
+        static string TABLE_NAME = "sec_CookieName";
+        static string TABLE_ALIAS = "cookie";
+        const string COL_ID = "ID";
+        const string COL_CookieName = "CookieName";
+
+
+        static CookieNameDataManager()
+        {
+            var columns = new Dictionary<string, RDBTableColumnDefinition>();
+            columns.Add(COL_ID, new RDBTableColumnDefinition { DataType = RDBDataType.Int });
+            columns.Add(COL_CookieName, new RDBTableColumnDefinition { DataType = RDBDataType.NVarchar, Size = 255 });
+            RDBSchemaManager.Current.RegisterDefaultTableDefinition(TABLE_NAME, new RDBTableDefinition
+            {
+                DBSchemaName = "sec",
+                DBTableName = "CookieName",
+                Columns = columns,
+                IdColumnName = COL_ID
+            });
+        }
+
+        #endregion
+
+        #region Private Methods
+        BaseRDBDataProvider GetDataProvider()
+        {
+            return RDBDataProviderFactory.CreateProvider("VR_Sec", "SecurityDBConnStringKey", "SecurityDBConnString");
+        }
+        #endregion
+
+        #region ICookieNameDataManager
+        public string InsertIfNotExistsAndGetCookieName(string nameToInsertIfNotExists)
+        {
+            var selectQueryContext = new RDBQueryContext(GetDataProvider());
+            var selectQuery = selectQueryContext.AddSelectQuery();
+            selectQuery.From(TABLE_NAME, TABLE_ALIAS, 1, true);
+            selectQuery.SelectColumns().Column(COL_CookieName);
+            var cookieName = selectQueryContext.ExecuteScalar().StringValue;
+            if (cookieName == null)
+            {
+                var insertQueryContext = new RDBQueryContext(GetDataProvider());
+                var insertQuery = insertQueryContext.AddInsertQuery();
+                insertQuery.IntoTable(TABLE_NAME);
+                insertQuery.Column(COL_CookieName).Value(nameToInsertIfNotExists);
+                insertQueryContext.ExecuteNonQuery();
+            }
+            var selectQueryContext2 = new RDBQueryContext(GetDataProvider());
+            var selectQuery2 = selectQueryContext2.AddSelectQuery();
+            selectQuery2.From(TABLE_NAME, TABLE_ALIAS, 1, true);
+            selectQuery2.SelectColumns().Column(COL_CookieName);
+            selectQuery2.Sort().ByColumn(COL_ID, RDBSortDirection.ASC);
+            return selectQueryContext2.ExecuteScalar().StringValue;
+        }
+        #endregion
     }
 }
--- Vanrise/Code/Vanrise.Security.Data.RDB/EncryptionKeyDataManager.cs
+++ Vanrise/Code/Vanrise.Security.Data.RDB/EncryptionKeyDataManager.cs
@@ -3,10 +3,64 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Vanrise.Data.RDB;
 
 namespace Vanrise.Security.Data.RDB
 {
-    class EncryptionKeyDataManager
+    public class EncryptionKeyDataManager : IEncryptionKeyDataManager
     {
+        #region RDB
+        static string TABLE_NAME = "sec_EncryptionKey";
+        static string TABLE_ALIAS = "key";
+        const string COL_ID = "ID";
+        const string COL_EncryptionKey = "EncryptionKey";
+
+
+        static EncryptionKeyDataManager()
+        {
+            var columns = new Dictionary<string, RDBTableColumnDefinition>();
+            columns.Add(COL_ID, new RDBTableColumnDefinition { DataType = RDBDataType.Int });
+            columns.Add(COL_EncryptionKey, new RDBTableColumnDefinition { DataType = RDBDataType.NVarchar, Size = 255 });
+            RDBSchemaManager.Current.RegisterDefaultTableDefinition(TABLE_NAME, new RDBTableDefinition
+            {
+                DBSchemaName = "sec",
+                DBTableName = "EncryptionKey",
+                Columns = columns,
+                IdColumnName = COL_ID
+            });
+        }
+        #endregion
+
+        #region Private Methods
+        BaseRDBDataProvider GetDataProvider()
+        {
+            return RDBDataProviderFactory.CreateProvider("VR_Sec", "SecurityDBConnStringKey", "SecurityDBConnString");
+        }
+        #endregion
+
+        #region IEncryptionKeyDataManager
+        public string InsertIfNotExistsAndGetEncryptionKey(string keyToInsertIfNotExists)
+        {
+            var selectQueryContext = new RDBQueryContext(GetDataProvider());
+            var selectQuery = selectQueryContext.AddSelectQuery();
+            selectQuery.From(TABLE_NAME, TABLE_ALIAS, 1, true);
+            selectQuery.SelectColumns().Column(COL_EncryptionKey);
+            var encryptionKey = selectQueryContext.ExecuteScalar().StringValue;
+            if (encryptionKey == null)
+            {
+                var insertQueryContext = new RDBQueryContext(GetDataProvider());
+                var insertQuery = insertQueryContext.AddInsertQuery();
+                insertQuery.IntoTable(TABLE_NAME);
+                insertQuery.Column(COL_EncryptionKey).Value(keyToInsertIfNotExists);
+                insertQueryContext.ExecuteNonQuery();
+            }
+            var selectQueryContext2 = new RDBQueryContext(GetDataProvider());
+            var selectQuery2 = selectQueryContext2.AddSelectQuery();
+            selectQuery2.From(TABLE_NAME, TABLE_ALIAS, 1, true);
+            selectQuery2.SelectColumns().Column(COL_EncryptionKey);
+            selectQuery2.Sort().ByColumn(COL_ID, RDBSortDirection.ASC);
+            return selectQueryContext2.ExecuteScalar().StringValue;
+        }
+        #endregion
     }
 }
