27939f51306d80e89c258fe00f6c36b37f7da401
Samer Haidar <VANRISE\samer.haidar>
2019-01-08 12:29:53 +0000

    RDBDataType
    git-tfs-id: [http://developmentvlan:8585/tfs/vanrise.collection]$/;C52427

--- Vanrise/Code/Vanrise.Data.RDB/BaseAndDefs/RDBDataType.cs
+++ Vanrise/Code/Vanrise.Data.RDB/BaseAndDefs/RDBDataType.cs
@@ -1,10 +1,33 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace Vanrise.Data.RDB
 {
-    public enum RDBDataType { Varchar = 0, NVarchar = 1, Int = 2, BigInt = 3, Decimal = 4, DateTime = 5, UniqueIdentifier = 6, Boolean = 7, VarBinary = 8, Cursor = 9}
+    public enum RDBDataType
+    {
+        [Description("Varchar")]
+        Varchar = 0,
+        [Description("NVarchar")]
+        NVarchar = 1,
+        [Description("Int")]
+        Int = 2,
+        [Description("BigInt")]
+        BigInt = 3,
+        [Description("Decimal")]
+        Decimal = 4,
+        [Description("DateTime")]
+        DateTime = 5,
+        [Description("UniqueIdentifier")]
+        UniqueIdentifier = 6,
+        [Description("Boolean")]
+        Boolean = 7,
+        [Description("VarBinary")]
+        VarBinary = 8,
+        [Description("Cursor")]
+        Cursor = 9
+    }
 }
