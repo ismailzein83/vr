271925f71f9c78071aa3c7b2d16b14158c92b0a2
Samer Haidar <VANRISE\samer.haidar>
2019-01-08 12:47:34 +0000

    adding RDBDataTypeAttribute
    git-tfs-id: [http://developmentvlan:8585/tfs/vanrise.collection]$/;C52430

--- Vanrise/Code/Vanrise.Data.RDB/BaseAndDefs/RDBDataType.cs
+++ Vanrise/Code/Vanrise.Data.RDB/BaseAndDefs/RDBDataType.cs
@@ -9,25 +9,47 @@ namespace Vanrise.Data.RDB
 {
     public enum RDBDataType
     {
-        [Description("Varchar")]
+        [RDBDataTypeAttribute(Description = "Varchar", RequireSize = true)]
         Varchar = 0,
-        [Description("NVarchar")]
+        [RDBDataTypeAttribute(Description = "NVarchar", RequireSize = true)]
         NVarchar = 1,
-        [Description("Int")]
+        [RDBDataTypeAttribute(Description = "Int")]
         Int = 2,
-        [Description("BigInt")]
+        [RDBDataTypeAttribute(Description = "BigInt")]
         BigInt = 3,
-        [Description("Decimal")]
+        [RDBDataTypeAttribute(Description = "Decimal", RequireSize = true, RequirePrecision =true)]
         Decimal = 4,
-        [Description("DateTime")]
+        [RDBDataTypeAttribute(Description = "DateTime")]
         DateTime = 5,
-        [Description("UniqueIdentifier")]
+        [RDBDataTypeAttribute(Description = "UniqueIdentifier")]
         UniqueIdentifier = 6,
-        [Description("Boolean")]
+        [RDBDataTypeAttribute(Description = "Boolean")]
         Boolean = 7,
-        [Description("VarBinary")]
+        [RDBDataTypeAttribute(Description = "VarBinary", RequireSize = true)]
         VarBinary = 8,
-        [Description("Cursor")]
+        [RDBDataTypeAttribute(Description = "Cursor")]
         Cursor = 9
     }
+    public class RDBDataTypeAttribute : Attribute
+    {
+        static Dictionary<RDBDataType, RDBDataTypeAttribute> _cachedAttributes;
+        static RDBDataTypeAttribute()
+        {
+            _cachedAttributes = new Dictionary<RDBDataType, RDBDataTypeAttribute>();
+            foreach (var member in typeof(RDBDataType).GetFields())
+            {
+                RDBDataTypeAttribute mbrAttribute = member.GetCustomAttributes(typeof(RDBDataTypeAttribute), true).FirstOrDefault() as RDBDataTypeAttribute;
+                if (mbrAttribute != null)
+                    _cachedAttributes.Add((RDBDataType)Enum.Parse(typeof(RDBDataType), member.Name), mbrAttribute);
+            }
+        }
+        public string Description { get; set; }
+        public bool RequireSize { get; set; }
+        public bool RequirePrecision { get; set; }
+        public static RDBDataTypeAttribute GetAttribute(RDBDataType rdbDataType)
+        {
+            return _cachedAttributes[rdbDataType];
+        }
+    }
+
 }
