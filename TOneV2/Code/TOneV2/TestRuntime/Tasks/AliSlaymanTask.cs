77ddbf34beab8693dd2708da4aeb171f8670b2f7
Ali Slayman <VANRISE\ali.slayman>
2019-01-09 08:04:49 +0000

    Adding AliSlaymanTask
    git-tfs-id: [http://developmentvlan:8585/tfs/vanrise.collection]$/;C52461

--- TOneV2/Code/TOneV2/TestRuntime/Tasks/AliSlaymanTask.cs
+++ TOneV2/Code/TOneV2/TestRuntime/Tasks/AliSlaymanTask.cs
@@ -2,18 +2,29 @@
 using System.Collections.Generic;
 using TOne.WhS.RouteSync.Entities;
 using TOne.WhS.RouteSync.Ericsson;
+using TOne.WhS.Routing.Entities;
 using Vanrise.BusinessProcess;
 using Vanrise.Caching.Runtime;
 using Vanrise.Common.Business;
+//using Vanrise.DatabaseStructure;
 using Vanrise.Queueing;
 using Vanrise.Runtime;
 using Vanrise.Runtime.Entities;
+using System.Linq;
+using Vanrise.Common;
 
 namespace TestRuntime.Tasks
 {
     class AliSlaymanTask : ITask
     {
         public void Execute()
+        {
+            //VRDBStructure structure = GetMSSQLDBStructure(".\\MSSQL2014", "SlaymanDB", "sa", "p@ssw0rd");
+            //Console.ReadKey();
+        }
+
+
+        private void BPRegulatorTest()
         {
             var runtimeServices = new List<RuntimeService>();
 
@@ -25,8 +36,11 @@ namespace TestRuntime.Tasks
 
             RuntimeHost host = new RuntimeHost(runtimeServices);
             host.Start();
-
-            Console.ReadKey();
         }
+
+        //private VRDBStructure GetMSSQLDBStructure(string Server = "192.168.110.185", string Database = "SecurityModel", string UserID = "Development", string Password = "dev!123")
+        //{
+        //    return new VRDBStructureManager().GetMSSQLDBStructure(Server, Database, UserID, Password);
+        //}
     }
 }
