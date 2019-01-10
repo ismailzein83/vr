805f55d94171ffbfbcf0954d00d1809652260ff8
Ali Ballouk <VANRISE\ali.ballouk>
2019-01-10 12:19:08 +0000

    Adding DataSourceImportedBatch GetFiltered sp to support BigDataService
    git-tfs-id: [http://developmentvlan:8585/tfs/vanrise.collection]$/;C52521

---
+++ TOneV2/Code/TOneV2/SQL.TOneLogging/integration/Stored Procedures/sp_DataSourceImportedBatch_GetFiltered.sql
@@ -0,0 +1,43 @@
+﻿         -- =============================================
+-- Author:		<Author,,Name>
+-- Create date: <Create Date,,>
+-- Description:	<Description,,>
+-- =============================================
+CREATE PROCEDURE [integration].[sp_DataSourceImportedBatch_GetFiltered]
+	
+	@DataSourceId uniqueidentifier = NULL,
+	@BatchName NVARCHAR(1000) = NULL,
+	@MappingResults nvarchar(max),
+	@From DATETIME = NULL,
+	@To DATETIME = NULL,
+	@Top int
+AS
+
+SET NOCOUNT ON;
+
+	BEGIN
+		DECLARE @MappingResultsTable TABLE (MappingResult int)
+		INSERT INTO @MappingResultsTable (MappingResult)
+		select Convert(int, ParsedString) from [bp].[ParseStringList](@MappingResults)
+
+	select TOP(@Top) [ID]
+	,[BatchDescription]
+	,[BatchSize]
+	,[BatchState]
+	,[RecordsCount]
+	,[MappingResult]
+	,[MapperMessage]
+	,[QueueItemIds]
+	,[LogEntryTime]
+	,[BatchStart]
+	,[BatchEnd]
+	FROM [integration].[DataSourceImportedBatch] as ib WITH(NOLOCK) 
+	WHERE 
+		(@DataSourceId IS NULL OR DataSourceId = @DataSourceId) 
+		AND (@BatchName IS NULL OR BatchDescription LIKE '%' + @BatchName + '%') 
+		AND (@MappingResults is null  or ib.MappingResult in (select MappingResult from @MappingResultsTable))
+		AND (@From IS NULL OR LogEntryTime >= @From) 
+		AND (@To IS NULL OR LogEntryTime <= @To)
+	ORDER BY ID DESC
+	END
+SET NOCOUNT OFF
---
+++ TOneV2/Code/TOneV2/SQL.TOneLogging/integration/Stored Procedures/sp_DataSourceLog_GetFiltered.sql
@@ -0,0 +1,34 @@
+﻿-- =============================================
+-- Author:		<Author,,Name>
+-- Create date: <Create Date,,>
+-- Description:	<Description,,>
+-- =============================================
+CREATE PROCEDURE [integration].[sp_DataSourceLog_GetFiltered]
+	@DataSourceId uniqueidentifier = NULL,
+	@Severities nvarchar(max),
+	@From DATETIME = NULL,
+	@To DATETIME = NULL,
+	@Top int
+AS
+
+SET NOCOUNT ON;
+
+	BEGIN
+		DECLARE @SeveritiesTable TABLE (Severity int)
+		INSERT INTO @SeveritiesTable (Severity)
+		select Convert(int, ParsedString) from [bp].[ParseStringList](@Severities)
+
+	select TOP(@Top) [ID],
+			[DataSourceId],
+			[Severity],
+			[Message],
+			[LogEntryTime]
+	FROM [integration].[DataSourceLog] as ib WITH(NOLOCK) 
+	WHERE 
+		(@DataSourceId IS NULL OR DataSourceId = @DataSourceId)
+		AND (@Severities is null or Severity IN (SELECT Severity FROM @SeveritiesTable)) 
+		AND (@From IS NULL OR LogEntryTime >= @From) 
+		AND (@To IS NULL OR LogEntryTime <= @To)
+		ORDER BY ID DESC
+	END
+SET NOCOUNT OFF
