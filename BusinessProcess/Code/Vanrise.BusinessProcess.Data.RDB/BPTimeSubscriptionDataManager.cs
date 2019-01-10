debf948a9913956d00218c7c715f833a2ef3516e
Naim Saad <VANRISE\naim.saad>
2019-01-10 11:51:47 +0000

    Commenting RDB Classes
    git-tfs-id: [http://developmentvlan:8585/tfs/vanrise.collection]$/;C52511

--- BusinessProcess/Code/Vanrise.BusinessProcess.Data.RDB/BPTimeSubscriptionDataManager.cs
+++ BusinessProcess/Code/Vanrise.BusinessProcess.Data.RDB/BPTimeSubscriptionDataManager.cs
@@ -1,109 +1,109 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using Vanrise.BusinessProcess.Entities;
-using Vanrise.Data.RDB;
+﻿//using System;
+//using System.Collections.Generic;
+//using System.Linq;
+//using System.Text;
+//using System.Threading.Tasks;
+//using Vanrise.BusinessProcess.Entities;
+//using Vanrise.Data.RDB;
 
-namespace Vanrise.BusinessProcess.Data.RDB
-{
-    public class BPTimeSubscriptionDataManager : IBPTimeSubscriptionDataManager
-    {
-        static string TABLE_NAME = "bp_BPTimeSubscription";
-        static string TABLE_ALIAS = "TimeSub";
+//namespace Vanrise.BusinessProcess.Data.RDB
+//{
+//    public class BPTimeSubscriptionDataManager : IBPTimeSubscriptionDataManager
+//    {
+//        static string TABLE_NAME = "bp_BPTimeSubscription";
+//        static string TABLE_ALIAS = "TimeSub";
 
-        const string COL_ID = "ID";
-        const string COL_ProcessInstanceID = "ProcessInstanceID";
-        const string COL_Bookmark = "Bookmark";
-        const string COL_DueTime = "DueTime";
-        const string COL_Payload = "Payload";
-        const string COL_CreatedTime = "CreatedTime";
+//        const string COL_ID = "ID";
+//        const string COL_ProcessInstanceID = "ProcessInstanceID";
+//        const string COL_Bookmark = "Bookmark";
+//        const string COL_DueTime = "DueTime";
+//        const string COL_Payload = "Payload";
+//        const string COL_CreatedTime = "CreatedTime";
 
 
-        static BPTimeSubscriptionDataManager()
-        {
-            var columns = new Dictionary<string, RDBTableColumnDefinition>();
-            columns.Add(COL_ID, new RDBTableColumnDefinition { DataType = RDBDataType.BigInt });
-            columns.Add(COL_ProcessInstanceID, new RDBTableColumnDefinition { DataType = RDBDataType.BigInt });
-            columns.Add(COL_Bookmark, new RDBTableColumnDefinition { DataType = RDBDataType.Varchar, Size = 100 });
-            columns.Add(COL_DueTime, new RDBTableColumnDefinition { DataType = RDBDataType.DateTime });
-            columns.Add(COL_Payload, new RDBTableColumnDefinition { DataType = RDBDataType.Varchar });
-            columns.Add(COL_CreatedTime, new RDBTableColumnDefinition { DataType = RDBDataType.DateTime });
-            RDBSchemaManager.Current.RegisterDefaultTableDefinition(TABLE_NAME, new RDBTableDefinition
-            {
-                DBSchemaName = "bp",
-                DBTableName = "BPTimeSubscription",
-                Columns = columns,
-                IdColumnName = COL_ID,
-                CreatedTimeColumnName = COL_CreatedTime
-            });
-        }
-        BaseRDBDataProvider GetDataProvider()
-        {
-            return RDBDataProviderFactory.CreateProvider("BusinessProcess", "BusinessProcessDBConnStringKey", "BusinessProcessDBConnString");
-        }
+//        static BPTimeSubscriptionDataManager()
+//        {
+//            var columns = new Dictionary<string, RDBTableColumnDefinition>();
+//            columns.Add(COL_ID, new RDBTableColumnDefinition { DataType = RDBDataType.BigInt });
+//            columns.Add(COL_ProcessInstanceID, new RDBTableColumnDefinition { DataType = RDBDataType.BigInt });
+//            columns.Add(COL_Bookmark, new RDBTableColumnDefinition { DataType = RDBDataType.Varchar, Size = 100 });
+//            columns.Add(COL_DueTime, new RDBTableColumnDefinition { DataType = RDBDataType.DateTime });
+//            columns.Add(COL_Payload, new RDBTableColumnDefinition { DataType = RDBDataType.Varchar });
+//            columns.Add(COL_CreatedTime, new RDBTableColumnDefinition { DataType = RDBDataType.DateTime });
+//            RDBSchemaManager.Current.RegisterDefaultTableDefinition(TABLE_NAME, new RDBTableDefinition
+//            {
+//                DBSchemaName = "bp",
+//                DBTableName = "BPTimeSubscription",
+//                Columns = columns,
+//                IdColumnName = COL_ID,
+//                CreatedTimeColumnName = COL_CreatedTime
+//            });
+//        }
+//        BaseRDBDataProvider GetDataProvider()
+//        {
+//            return RDBDataProviderFactory.CreateProvider("BusinessProcess", "BusinessProcessDBConnStringKey", "BusinessProcessDBConnString");
+//        }
 
-        #region Public Methods
-        public bool DeleteBPTimeSubscription(long bpTimeSubscriptionId)
-        {
-            var queryContext = new RDBQueryContext(GetDataProvider());
+//        #region Public Methods
+//        public bool DeleteBPTimeSubscription(long bpTimeSubscriptionId)
+//        {
+//            var queryContext = new RDBQueryContext(GetDataProvider());
 
-            var deleteQuery = queryContext.AddDeleteQuery();
-            deleteQuery.FromTable(TABLE_NAME);
+//            var deleteQuery = queryContext.AddDeleteQuery();
+//            deleteQuery.FromTable(TABLE_NAME);
 
-            var where = deleteQuery.Where();
-            where.EqualsCondition(COL_ID).Value(bpTimeSubscriptionId);
+//            var where = deleteQuery.Where();
+//            where.EqualsCondition(COL_ID).Value(bpTimeSubscriptionId);
 
-            return queryContext.ExecuteNonQuery() > 0;
-        }
-        public IEnumerable<BPTimeSubscription> GetDueBPTimeSubscriptions()
-        {
-            var queryContext = new RDBQueryContext(GetDataProvider());
+//            return queryContext.ExecuteNonQuery() > 0;
+//        }
+//        public IEnumerable<BPTimeSubscription> GetDueBPTimeSubscriptions()
+//        {
+//            var queryContext = new RDBQueryContext(GetDataProvider());
 
-            var selectQuery = queryContext.AddSelectQuery();
-            selectQuery.From(TABLE_NAME, TABLE_ALIAS, null, true);
-            selectQuery.SelectColumns().AllTableColumns(TABLE_ALIAS);
+//            var selectQuery = queryContext.AddSelectQuery();
+//            selectQuery.From(TABLE_NAME, TABLE_ALIAS, null, true);
+//            selectQuery.SelectColumns().AllTableColumns(TABLE_ALIAS);
 
-            var where = selectQuery.Where();
-            where.LessOrEqualCondition(COL_DueTime).DateNow();
+//            var where = selectQuery.Where();
+//            where.LessOrEqualCondition(COL_DueTime).DateNow();
 
-            return queryContext.GetItems(BPTimeSubscriptionMapper);
-        }
-        public int InsertBPTimeSubscription(long processInstanceId, string bookmarkName, TimeSpan delay, BPTimeSubscriptionPayload payload)
-        {
-            var queryContext = new RDBQueryContext(GetDataProvider());
+//            return queryContext.GetItems(BPTimeSubscriptionMapper);
+//        }
+//        public int InsertBPTimeSubscription(long processInstanceId, string bookmarkName, TimeSpan delay, BPTimeSubscriptionPayload payload)
+//        {
+//            var queryContext = new RDBQueryContext(GetDataProvider());
 
-            var insertQuery = queryContext.AddInsertQuery();
-            insertQuery.IntoTable(TABLE_NAME);
-            insertQuery.Column(COL_ProcessInstanceID).Value(processInstanceId);
-            insertQuery.Column(COL_Bookmark).Value(bookmarkName);
+//            var insertQuery = queryContext.AddInsertQuery();
+//            insertQuery.IntoTable(TABLE_NAME);
+//            insertQuery.Column(COL_ProcessInstanceID).Value(processInstanceId);
+//            insertQuery.Column(COL_Bookmark).Value(bookmarkName);
 
-            var dateTimeValueToAddContext = insertQuery.Column(COL_DueTime).DateTimeAdd(RDBDateTimeAddInterval.Seconds);
-            dateTimeValueToAddContext.ValueToAdd().Value((int)delay.TotalSeconds);
-            dateTimeValueToAddContext.ValueToAdd().DateNow();
+//            var dateTimeValueToAddContext = insertQuery.Column(COL_DueTime).DateTimeAdd(RDBDateTimeAddInterval.Seconds);
+//            dateTimeValueToAddContext.DateTime().DateNow();
+//            dateTimeValueToAddContext.ValueToAdd().Value((int)delay.TotalSeconds);
 
-            if (payload != null)
-                insertQuery.Column(COL_Payload).Value(Vanrise.Common.Serializer.Serialize(payload));
+//            if (payload != null)
+//                insertQuery.Column(COL_Payload).Value(Vanrise.Common.Serializer.Serialize(payload));
 
-            return queryContext.ExecuteNonQuery();
-        }
-        #endregion
+//            return queryContext.ExecuteNonQuery();
+//        }
+//        #endregion
 
-        #region Private Methods
-        BPTimeSubscription BPTimeSubscriptionMapper(IRDBDataReader reader)
-        {
-            string serializedPayload = reader.GetString("Payload");
+//        #region Private Methods
+//        BPTimeSubscription BPTimeSubscriptionMapper(IRDBDataReader reader)
+//        {
+//            string serializedPayload = reader.GetString("Payload");
 
-            return new BPTimeSubscription
-            {
-                BPTimeSubscriptionId = reader.GetLong("ID"),
-                ProcessInstanceId = reader.GetLong("ProcessInstanceID"),
-                Bookmark = reader.GetString("Bookmark"),
-                Payload = !string.IsNullOrEmpty(serializedPayload) ? Vanrise.Common.Serializer.Deserialize<BPTimeSubscriptionPayload>(serializedPayload) : null,
-                DueTime = reader.GetDateTime("DueTime")
-            };
-        }
-        #endregion
-    }
-}
+//            return new BPTimeSubscription
+//            {
+//                BPTimeSubscriptionId = reader.GetLong("ID"),
+//                ProcessInstanceId = reader.GetLong("ProcessInstanceID"),
+//                Bookmark = reader.GetString("Bookmark"),
+//                Payload = !string.IsNullOrEmpty(serializedPayload) ? Vanrise.Common.Serializer.Deserialize<BPTimeSubscriptionPayload>(serializedPayload) : null,
+//                DueTime = reader.GetDateTime("DueTime")
+//            };
+//        }
+//        #endregion
+//    }
+//}
--- BusinessProcess/Code/Vanrise.BusinessProcess.Data.RDB/BPValidationMessageDataManager.cs
+++ BusinessProcess/Code/Vanrise.BusinessProcess.Data.RDB/BPValidationMessageDataManager.cs
@@ -4,30 +4,151 @@
 //using System.Text;
 //using System.Threading.Tasks;
 //using Vanrise.BusinessProcess.Entities;
+//using Vanrise.Common;
+//using Vanrise.Data.RDB;
 //using Vanrise.Entities;
 
 //namespace Vanrise.BusinessProcess.Data.RDB
 //{
 //    public class BPValidationMessageDataManager : IBPValidationMessageDataManager
 //    {
+
+//        static string TABLE_NAME = "bp_BPValidationMessage";
+//        static string TABLE_ALIAS = "ValidationMsg";
+
+//        const string COL_ID = "ID";
+//        const string COL_ProcessInstanceID = "ProcessInstanceID";
+//        const string COL_ParentProcessID = "ParentProcessID";
+//        const string COL_TargetKey = "TargetKey";
+//        const string COL_TargetType = "TargetType";
+//        const string COL_Severity = "Severity";
+//        const string COL_Message = "Message";
+
+
+//        static BPValidationMessageDataManager()
+//        {
+//            var columns = new Dictionary<string, RDBTableColumnDefinition>();
+//            columns.Add(COL_ID, new RDBTableColumnDefinition { DataType = RDBDataType.BigInt });
+//            columns.Add(COL_ProcessInstanceID, new RDBTableColumnDefinition { DataType = RDBDataType.BigInt });
+//            columns.Add(COL_ParentProcessID, new RDBTableColumnDefinition { DataType = RDBDataType.BigInt });
+//            columns.Add(COL_TargetKey, new RDBTableColumnDefinition { DataType = RDBDataType.Varchar, Size = 900 });
+//            columns.Add(COL_TargetType, new RDBTableColumnDefinition { DataType = RDBDataType.Varchar, Size = 50 });
+//            columns.Add(COL_Severity, new RDBTableColumnDefinition { DataType = RDBDataType.Int });
+//            columns.Add(COL_Message, new RDBTableColumnDefinition { DataType = RDBDataType.NVarchar });
+//            RDBSchemaManager.Current.RegisterDefaultTableDefinition(TABLE_NAME, new RDBTableDefinition
+//            {
+//                DBSchemaName = "bp",
+//                DBTableName = "BPValidationMessage",
+//                Columns = columns
+//            });
+//        }
+//        BaseRDBDataProvider GetDataProvider()
+//        {
+//            return RDBDataProviderFactory.CreateProvider("BusinessProcessTracking", "BusinessProcessTrackingDBConnStringKey", "BusinessProcessTrackingDBConnString");
+//        }
+
+//        #region Public Methods
 //        public List<BPValidationMessage> GetBeforeId(BPValidationMessageBeforeIdInput input)
 //        {
-//            throw new NotImplementedException();
-//        }
+//            var queryContext = new RDBQueryContext(GetDataProvider());
 
-//        public BigResult<BPValidationMessageDetail> GetFilteredBPValidationMessage(DataRetrievalInput<BPValidationMessageQuery> input)
+//            var selectQuery = queryContext.AddSelectQuery();
+//            selectQuery.From(TABLE_NAME, TABLE_ALIAS, input.NbOfRows, true);
+//            selectQuery.SelectColumns().AllTableColumns(TABLE_ALIAS);
+
+//            var whereContext = selectQuery.Where();
+//            whereContext.LessThanCondition(COL_ID).Value(input.LessThanID);
+//            whereContext.EqualsCondition(COL_ProcessInstanceID).Value(input.BPInstanceID);
+
+//            var sortContext = selectQuery.Sort();
+//            sortContext.ByColumn(COL_ID, RDBSortDirection.DESC);
+
+//            return queryContext.GetItems(BPValidationMessageMapper);
+//        }
+//        public IEnumerable<BPValidationMessage> GetFilteredBPValidationMessage(DataRetrievalInput<BPValidationMessageQuery> input)
 //        {
-//            throw new NotImplementedException();
-//        }
+//            var queryContext = new RDBQueryContext(GetDataProvider());
 
+//            var selectQuery = queryContext.AddSelectQuery();
+//            selectQuery.From(TABLE_NAME, TABLE_ALIAS, null, true);
+//            selectQuery.SelectColumns().AllTableColumns(TABLE_ALIAS);
+
+//            var whereContext = selectQuery.Where();
+//            if (input.Query.Severities != null && input.Query.Severities.Count > 0)
+//                whereContext.ListCondition(COL_Severity, RDBListConditionOperator.IN, input.Query.Severities.Select(itm => (int)itm));
+//            whereContext.EqualsCondition(COL_ProcessInstanceID).Value(input.Query.ProcessInstanceId);
+
+//            return queryContext.GetItems(BPValidationMessageMapper);
+//        }
 //        public List<BPValidationMessage> GetUpdated(BPValidationMessageUpdateInput input)
 //        {
-//            throw new NotImplementedException();
-//        }
+//            List<BPValidationMessage> bpValidationMessages = new List<BPValidationMessage>();
 
-//        public void Insert(IEnumerable<BPValidationMessage> messages)
-//        {
-//            throw new NotImplementedException();
+//            var queryContext = new RDBQueryContext(GetDataProvider());
+
+//            if (input.GreaterThanID == default(long))
+//            {
+//                var selectQuery = queryContext.AddSelectQuery();
+//                selectQuery.From(TABLE_NAME, TABLE_ALIAS, input.NbOfRows, true);
+//                selectQuery.SelectColumns().AllTableColumns(TABLE_ALIAS);
+
+//                var whereContext = selectQuery.Where();
+//                whereContext.EqualsCondition(COL_ProcessInstanceID).Value(input.BPInstanceID);
+
+//                var sortContext = selectQuery.Sort();
+//                sortContext.ByColumn(COL_ID, RDBSortDirection.DESC);
+//            }
+//            else
+//            {
+//                var selectQuery = queryContext.AddSelectQuery();
+//                selectQuery.From(TABLE_NAME, TABLE_ALIAS, input.NbOfRows, true);
+//                selectQuery.SelectColumns().AllTableColumns(TABLE_ALIAS);
+
+//                var whereContext = selectQuery.Where();
+//                whereContext.EqualsCondition(COL_ProcessInstanceID).Value(input.BPInstanceID);
+//                whereContext.GreaterThanCondition(COL_ID).Value(input.GreaterThanID);
+
+//                var sortContext = selectQuery.Sort();
+//                sortContext.ByColumn(COL_ID, RDBSortDirection.DESC);
+
+//            }
+//            return queryContext.GetItems(BPValidationMessageMapper);
 //        }
+//        public void Insert(IEnumerable<Entities.BPValidationMessage> messages)
+//        {
+//            var queryContext = new RDBQueryContext(GetDataProvider());
+//            var multipleInsertQuery = queryContext.AddInsertMultipleRowsQuery();
+//            multipleInsertQuery.IntoTable(TABLE_NAME);
+//            foreach (BPValidationMessage msg in messages)
+//            {
+//                var rowInsertContext = multipleInsertQuery.AddRow();
+//                rowInsertContext.Column(COL_ProcessInstanceID).Value(msg.ProcessInstanceId);
+//                if (msg.ParentProcessId.HasValue)
+//                    rowInsertContext.Column(COL_ParentProcessID).Value(msg.ParentProcessId.Value);
+//                if (msg.TargetKey != null)
+//                    rowInsertContext.Column(COL_TargetKey).Value(msg.TargetKey.ToString());
+//                rowInsertContext.Column(COL_TargetType).Value(msg.TargetType);
+//                rowInsertContext.Column(COL_Severity).Value((int)msg.Severity);
+//                rowInsertContext.Column(COL_Message).Value(msg.Message);
+//            }
+//            queryContext.ExecuteNonQuery();
+//        }
+//        #endregion
+//        #region Mappers
+//        private BPValidationMessage BPValidationMessageMapper(IRDBDataReader reader)
+//        {
+//            BPValidationMessage bpValidationMessage = new BPValidationMessage()
+//            {
+//                ValidationMessageId = reader.GetLong("ID"),
+//                ProcessInstanceId = reader.GetLong("ProcessInstanceID"),
+//                ParentProcessId = reader.GetNullableLong("ParentProcessId"),
+//                TargetKey = reader.GetString("TargetKey"),
+//                TargetType = reader.GetString("TargetType"),
+//                Severity = (ActionSeverity)reader.GetInt("Severity"),
+//                Message = reader.GetString("Message")
+//            };
+//            return bpValidationMessage;
+//        }
+//        #endregion
 //    }
 //}
