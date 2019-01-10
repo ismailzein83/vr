f9adaf9db8aa0ee4d4bc1a73581a4d7be35fe85c
yara.bousalman <yara.bousalman@vanrise.tfs.local>
2019-01-10 10:05:38 +0000

    Adding datamanagers to RDB project
    git-tfs-id: [http://developmentvlan:8585/tfs/vanrise.collection]$/;C52505

--- TOneV2/Code/TOneV2/TOne.WhS.CodePreparation.Data.RDB/ChangedSaleRateDataManager.cs
+++ TOneV2/Code/TOneV2/TOne.WhS.CodePreparation.Data.RDB/ChangedSaleRateDataManager.cs
@@ -3,10 +3,83 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using TOne.WhS.CodePreparation.Entities.Processing;
+using Vanrise.Data.RDB;
+using Vanrise.Common;
 
 namespace TOne.WhS.CodePreparation.Data.RDB
 {
-    class ChangedSaleRateDataManager
+    class ChangedSaleRateDataManager : IChangedSaleRateDataManager
     {
+        #region RDB
+        static string TABLE_NAME = "TOneWhS_BE_CP_SaleRate_Changed";
+        const string COL_ID = "ID";
+        const string COL_ProcessInstanceID = "ProcessInstanceID";
+        const string COL_EED = "EED";
+
+
+        static ChangedSaleRateDataManager()
+        {
+            var columns = new Dictionary<string, RDBTableColumnDefinition>();
+            columns.Add(COL_ID, new RDBTableColumnDefinition { DataType = RDBDataType.BigInt });
+            columns.Add(COL_ProcessInstanceID, new RDBTableColumnDefinition { DataType = RDBDataType.BigInt });
+            columns.Add(COL_EED, new RDBTableColumnDefinition { DataType = RDBDataType.DateTime });
+            RDBSchemaManager.Current.RegisterDefaultTableDefinition(TABLE_NAME, new RDBTableDefinition
+            {
+                DBSchemaName = "TOneWhS_BE",
+                DBTableName = "CP_SaleRate_Changed",
+                Columns = columns
+            });
+        }
+
+        #endregion
+
+        #region Private Methods
+        BaseRDBDataProvider GetDataProvider()
+        {
+            return RDBDataProviderFactory.CreateProvider("WhS_CodePrep", "TOneWhS_BE_DBConnStringKey", "TOneWhS_BE_DBConnString");
+        }
+        #endregion
+
+        #region IChangedSaleRateDataManager
+        public long ProcessInstanceId
+        {
+            set
+            {
+                _processInstanceID = value;
+            }
+        }
+        long _processInstanceID;
+        readonly string[] _columns = { COL_ID, COL_ProcessInstanceID, COL_EED };
+
+        public void ApplyChangedRatesToDB(object preparedObject)
+        {
+            preparedObject.CastWithValidate<RDBBulkInsertQueryContext>("preparedObject").Apply();
+        }
+
+        public object FinishDBApplyStream(object dbApplyStream)
+        {
+            RDBBulkInsertQueryContext bulkInsertContext = dbApplyStream.CastWithValidate<RDBBulkInsertQueryContext>("dbApplyStream");
+            bulkInsertContext.CloseStream();
+            return bulkInsertContext;
+        }
+
+        public object InitialiazeStreamForDBApply()
+        {
+            var queryContext = new RDBQueryContext(GetDataProvider());
+            var streamForBulkInsert = queryContext.StartBulkInsert();
+            streamForBulkInsert.IntoTable(TABLE_NAME, '^', _columns);
+            return streamForBulkInsert;
+        }
+
+        public void WriteRecordToStream(ChangedRate record, object dbApplyStream)
+        {
+            RDBBulkInsertQueryContext bulkInsertContext = dbApplyStream.CastWithValidate<RDBBulkInsertQueryContext>("dbApplyStream");
+            var recordContext = bulkInsertContext.WriteRecord();
+            recordContext.Value(record.EntityId);
+            recordContext.Value(_processInstanceID);
+            recordContext.Value(record.EED);
+        }
+        #endregion
     }
 }
