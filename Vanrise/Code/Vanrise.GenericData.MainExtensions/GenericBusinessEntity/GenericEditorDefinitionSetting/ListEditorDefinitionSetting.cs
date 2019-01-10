53ae89f44b210dd1e8ec55be0bf34a0b21f4c3d2
Ali Sakr <VANRISE\ali.sakr>
2019-01-10 09:47:37 +0000

    Add ListEditorDefinitinSetting class
    git-tfs-id: [http://developmentvlan:8585/tfs/vanrise.collection]$/;C52503

---
+++ Vanrise/Code/Vanrise.GenericData.MainExtensions/GenericBusinessEntity/GenericEditorDefinitionSetting/ListEditorDefinitionSetting.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Vanrise.GenericData.Entities;
+
+namespace Vanrise.GenericData.MainExtensions
+{
+   public  class ListEditorDefinitionSetting : VRGenericEditorDefinitionSetting
+    {
+        public override Guid ConfigId
+        {
+            get { return new Guid("99458A8F-28B4-44F9-A8F0-EF20ED53061C"); }
+        }
+        public override string RuntimeEditor
+        {
+            get
+            {
+                return "vr-genericdata-listeditor-runtime";
+            }
+        }
+    }
+}
