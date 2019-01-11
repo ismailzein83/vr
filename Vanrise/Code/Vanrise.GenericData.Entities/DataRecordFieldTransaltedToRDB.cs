6930ccc9c747df7f1d012bb217b523bd6e10c380
DevInternship User <VANRISE\Dev.InternshipUser>
2019-01-11 11:07:21 +0000

    adding DataRecordFieldTransaltedToRDB
    git-tfs-id: [http://developmentvlan:8585/tfs/vanrise.collection]$/;C52541

---
+++ Vanrise/Code/Vanrise.GenericData.Entities/DataRecordFieldTransaltedToRDB.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Vanrise.Entities;
+
+namespace Vanrise.GenericData.Entities
+{
+    public class DataRecordFieldTransaltedToRDB
+    {
+        public string FieldName { get; set; }
+
+        public RDBDataType RDBDataType { get; set; }
+        public int? Size { get; set; }
+        public int? Precision { get; set; }
+        public bool IsUnique { get; set; }
+    }
+}
