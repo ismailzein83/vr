52a29ca1256c20be3af2ac8ff6b3afb4b3ea251d
Ismail Zein <VANRISE\ismail.zein>
2019-01-11 12:26:32 +0000

    sql project
    git-tfs-id: [http://developmentvlan:8585/tfs/vanrise.collection]$/;C52547

---
+++ Mediation/Code/Mediation.CDR/Mobile_EDR/Tables/MobileCDRFileMetadata.sql
@@ -0,0 +1,24 @@
+﻿CREATE TABLE [Mobile_EDR].[MobileCDRFileMetadata] (
+    [ID]                       BIGINT           IDENTITY (1, 1) NOT NULL,
+    [RuntimeNodeID]            UNIQUEIDENTIFIER NULL,
+    [ParentFolderRelativePath] NVARCHAR (1000)  NULL,
+    [FileNames]                NVARCHAR (MAX)   NULL,
+    [FromTime]                 DATETIME         NULL,
+    [ToTime]                   DATETIME         NULL,
+    [NbOfRecords]              BIGINT           NULL,
+    [MinTime]                  DATETIME         NULL,
+    [MaxTime]                  DATETIME         NULL,
+    [MinID]                    BIGINT           NULL,
+    [MaxID]                    BIGINT           NULL,
+    [IsReady]                  BIT              NOT NULL,
+    [CreatedTime]              DATETIME         CONSTRAINT [DF_MobileCDRFileMetadata_CreatedTime] DEFAULT (getdate()) NULL,
+    [LastModifiedTime]         DATETIME         NULL,
+    [timestamp]                ROWVERSION       NULL,
+    CONSTRAINT [PK_MobileCDRFileMetadata] PRIMARY KEY CLUSTERED ([ID] ASC)
+);
+
+
+GO
+CREATE NONCLUSTERED INDEX [IX_MobileCDRFileMetadata_TimeAndIDBorders]
+    ON [Mobile_EDR].[MobileCDRFileMetadata]([FromTime] ASC, [ToTime] ASC, [MinID] ASC, [MaxID] ASC);
+
