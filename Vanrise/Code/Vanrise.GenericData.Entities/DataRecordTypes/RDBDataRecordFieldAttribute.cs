e29ba6c8698544c42e27ea556ad763eb55b28584
DevInternship User <VANRISE\Dev.InternshipUser>
2019-01-11 11:11:58 +0000

    adding RDBDataRecordFieldAttribute
    git-tfs-id: [http://developmentvlan:8585/tfs/vanrise.collection]$/;C52542

---
+++ Vanrise/Code/Vanrise.GenericData.Entities/DataRecordTypes/RDBDataRecordFieldAttribute.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Vanrise.Entities;
+
+namespace Vanrise.GenericData.Entities.DataRecordTypes
+{
+    public class RDBDataRecordFieldAttribute
+    {
+        public RDBDataType RDBDataType { get; set; }
+        public int? Size { get; set; }
+        public int? Precision { get; set; }
+    }
+}
