78ded4283a27cd237a6421443be7f358051a2df4
Naim Saad <VANRISE\naim.saad>
2019-01-14 11:15:34 +0000

    Adding Commented ZoneCodeGroupDataManager RDB
    git-tfs-id: [http://developmentvlan:8585/tfs/vanrise.collection]$/;C52587

---
+++ TOneV2/Code/TOneV2/TOne.WhS.BusinessEntity.Data.RDB/ZoneCodeGroupDataManager.cs
@@ -0,0 +1,22 @@
+﻿//using System;
+//using System.Collections.Generic;
+//using System.Linq;
+//using System.Text;
+//using System.Threading.Tasks;
+//using TOne.WhS.BusinessEntity.Entities;
+
+//namespace TOne.WhS.BusinessEntity.Data.RDB
+//{
+//    class ZoneCodeGroupDataManager : IZoneCodeGroupDataManager
+//    {
+//        public List<ZoneCodeGroup> GetCostZoneCodeGroups(DateTime? effectiveOn, bool isFuture)
+//        {
+//            throw new NotImplementedException();
+//        }
+
+//        public List<ZoneCodeGroup> GetSaleZoneCodeGroups(DateTime? effectiveOn, bool isFuture)
+//        {
+//            throw new NotImplementedException();
+//        }
+//    }
+//}
