a1a75c0d19709b20997793260e2bec1708cc3b0d
Ali Atoui <VANRISE\ali.atoui>
2019-01-14 15:04:08 +0000

    Adding PositionedBitsBinaryFieldParser
    git-tfs-id: [http://developmentvlan:8585/tfs/vanrise.collection]$/;C52601

---
+++ Vanrise/Code/Vanrise.DataParser.MainExtensions/BinaryParsers/Common/FieldParsers/PositionedBitsBinaryFieldParser.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using Vanrise.DataParser.Entities;
+
+namespace Vanrise.DataParser.MainExtensions.BinaryParsers.Common.FieldParsers
+{
+    public class PositionedBitsBinaryFieldParser : BinaryFieldParserSettings
+    {
+        public override Guid ConfigId => throw new NotImplementedException();
+
+        public List<PositionedBitFieldParser> BitParsers { get; set; }
+
+        public override void Execute(IBinaryFieldParserContext context)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
