3fcd9c0c079742a8ca4340adce6d9c0991368ddd
Samer Haidar <VANRISE\samer.haidar>
2019-01-14 13:14:09 +0000

    adding BusinessEntityStatusHistoryDataManagerTests
    git-tfs-id: [http://developmentvlan:8585/tfs/vanrise.collection]$/;C52593

---
+++ Vanrise/Vanrise.RDBTests/Vanrise.RDB.Tests/GenericData/BusinessEntityStatusHistoryDataManagerTests.cs
@@ -0,0 +1,45 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Vanrise.GenericData.Data;
+using Vanrise.RDBTests.Common;
+
+namespace Vanrise.RDB.Tests.GenericData
+{
+    [TestClass]
+    public class BusinessEntityStatusHistoryDataManagerTests
+    {
+        const string DBTABLE_NAME_BusinessEntityStatusHistory = "BusinessEntityStatusHistory";
+
+        IBusinessEntityStatusHistoryDataManager _rdbDataManager = RDBDataManagerFactory.GetDataManager<IBusinessEntityStatusHistoryDataManager>();
+        IBusinessEntityStatusHistoryDataManager _sqlDataManager = SQLDataManagerFactory.GetDataManager<IBusinessEntityStatusHistoryDataManager>();
+        [TestMethod]
+        public void AddSelectBusinessEntityStatusHistory()
+        {
+            UTUtilities.TruncateTable(Constants.CONNSTRING_NAME_CONFIG, Constants.DBSCHEMA_NAME_GenericData, DBTABLE_NAME_BusinessEntityStatusHistory);
+            Guid beDefinition = Guid.NewGuid();
+            string beEntityId = "GenericData";
+            string fileName = "Test RDB File";
+            Guid statusId = Guid.NewGuid();
+            Guid prestatusId = Guid.NewGuid();
+
+            var rdbInsertResponse = _rdbDataManager.Insert(beDefinition, beEntityId, fileName, statusId, prestatusId);
+            var sqlInsertResponse = _sqlDataManager.Insert(beDefinition, beEntityId, fileName, statusId, prestatusId);
+            UTUtilities.AssertValuesAreEqual(rdbInsertResponse, sqlInsertResponse);
+
+            var rdbResponse = _rdbDataManager.GetLastBusinessEntityStatusHistory(beDefinition, beEntityId, fileName);
+
+            rdbResponse.StatusChangedDate = rdbResponse.StatusChangedDate.Date;
+            var sqlResponse = _sqlDataManager.GetLastBusinessEntityStatusHistory(beDefinition, beEntityId, fileName);
+            sqlResponse.StatusChangedDate = sqlResponse.StatusChangedDate.Date;
+
+            UTUtilities.AssertObjectsAreSimilar(rdbResponse, sqlResponse);
+
+        }
+
+
+    }
+}
