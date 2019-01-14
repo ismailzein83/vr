581e64e8d9285185e7e0328fbb75bb471e164adb
Naim Saad <VANRISE\naim.saad>
2019-01-14 08:05:38 +0000

    add sp_BPValidationMessage_GetFiltered + remove sp_BPValidationMessage_CreateTempForFiltered
    git-tfs-id: [http://developmentvlan:8585/tfs/vanrise.collection]$/;C52565

---
+++ TOneV2/Code/TOneV2/SQL.TOneLogging/bp/Stored Procedures/sp_BPValidationMessage_GetFiltered.sql
@@ -0,0 +1,16 @@
+﻿CREATE PROCEDURE [bp].[sp_BPValidationMessage_GetFiltered]
+	@ProcessInstanceId BIGINT,
+	@ArrSeverityID nvarchar(max)
+AS
+BEGIN
+	SELECT [ID]
+	  ,[ProcessInstanceID]
+	  ,[ParentProcessID]
+	  ,[TargetKey]
+	  ,[TargetType]
+	  ,[Severity]
+	  ,[Message]
+	FROM bp.[BPValidationMessage] WITH(NOLOCK)
+	WHERE (@ArrSeverityID is NULL or Severity in (SELECT ParsedString FROM ParseStringList(@ArrSeverityID))) 
+	And ProcessInstanceID = @ProcessInstanceId 
+END
