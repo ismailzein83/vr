b7ca224d3181de0fbd1d41eeb426675a5c136253
Ali Slayman <VANRISE\ali.slayman>
2019-01-15 12:57:52 +0000

    Adding AliSlaymanTask and SMSBuisenessEntity dlls
    git-tfs-id: [http://developmentvlan:8585/tfs/vanrise.collection]$/;C52633

--- TOneV2/Code/TOneV2/TestRuntime/Tasks/AliSlaymanTask.cs
+++ TOneV2/Code/TOneV2/TestRuntime/Tasks/AliSlaymanTask.cs
@@ -6,12 +6,14 @@ using TOne.WhS.Routing.Entities;
 using Vanrise.BusinessProcess;
 using Vanrise.Caching.Runtime;
 using Vanrise.Common.Business;
-//using Vanrise.DatabaseStructure;
 using Vanrise.Queueing;
 using Vanrise.Runtime;
 using Vanrise.Runtime.Entities;
 using System.Linq;
 using Vanrise.Common;
+using Vanrise.DatabaseStructure;
+using Vanrise.DatabaseStructure.DBConvertors;
+using static Vanrise.DatabaseStructure.VRDBStructureManager;
 
 namespace TestRuntime.Tasks
 {
@@ -19,8 +21,26 @@ namespace TestRuntime.Tasks
     {
         public void Execute()
         {
-            //VRDBStructure structure = GetMSSQLDBStructure(".\\MSSQL2014", "SlaymanDB", "sa", "p@ssw0rd");
-            //Console.ReadKey();
+            //VRDBStructure structure = GetMSSQLDBStructure("192.168.110.185", "TOneConfiguration", "Development", "dev!123");
+            VRDBStructure structure = GetMSSQLDBStructure();
+            string script = GetMySqlScript(structure);
+
+            //List<test> tests = new List<test> {
+            //new test() {  id = 1 , name = "Ali" },
+            //new test() {  id = 2 , name = "Alia" },
+            //new test() {  id = 3 , name = "Alie" },
+            //new test() {  id = 4 , name = "Alio" }
+            //};
+
+            //string primaryColumnKeys = string.Join(",", tests.Select(item => item.name).ToList());
+
+            Console.ReadKey();
+        }
+
+        class test
+        {
+            public int id { get; set; }
+            public string name { get; set; }
         }
 
 
@@ -38,9 +58,17 @@ namespace TestRuntime.Tasks
             host.Start();
         }
 
-        //private VRDBStructure GetMSSQLDBStructure(string Server = "192.168.110.185", string Database = "SecurityModel", string UserID = "Development", string Password = "dev!123")
-        //{
-        //    return new VRDBStructureManager().GetMSSQLDBStructure(Server, Database, UserID, Password);
-        //}
+        private VRDBStructure GetMSSQLDBStructure(string Server = "192.168.110.185", string Database = "SecurityModel", string UserID = "Development", string Password = "dev!123")
+        {
+            return new VRDBStructureManager().GetMSSQLDBStructure(Server, Database, UserID, Password);
+        }
+
+        private string GetMySqlScript(VRDBStructure vrdbStructure)
+        {
+            VRDBStructureConvertorGenerateConvertedScriptContext vRDBStructureConvertorGenerateConvertedScriptContext = new VRDBStructureConvertorGenerateConvertedScriptContext(vrdbStructure);
+            MySqlDBConvertor mySqlDBConvertor = new MySqlDBConvertor();
+            return mySqlDBConvertor.GenerateConvertedScript(vRDBStructureConvertorGenerateConvertedScriptContext);
+        }
+
     }
 }
