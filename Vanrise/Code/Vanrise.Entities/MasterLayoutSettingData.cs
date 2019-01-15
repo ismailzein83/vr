b58ed91911f18bb1b523b1686e91ab56bfbe2923
Ali Ballouk <VANRISE\ali.ballouk>
2019-01-15 10:03:17 +0000

    add MasterlayoutSettingData class
    git-tfs-id: [http://developmentvlan:8585/tfs/vanrise.collection]$/;C52614

---
+++ Vanrise/Code/Vanrise.Entities/MasterLayoutSettingData.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Vanrise.Entities
+{
+    public class MasterLayoutSettingData : SettingData
+    {
+        public MasterLayoutMenuOption Menu { get; set; }
+
+        public bool ExpandedMenu { get; set; }
+
+        public bool IsBreadcrumbVisible { get; set; }
+
+        public bool ShowApplicationTiles { get; set; }
+
+        public bool ShowModuleTiles { get; set; }
+
+    }
+   
+    public enum MasterLayoutMenuOption
+    {
+        
+        FullMenu = 0,
+
+        ModuleFilteredMenu = 1,
+
+        NoMenu = 2
+    }
+}
