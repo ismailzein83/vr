84e10965d4b8604dc304a4b2ecbf597fc2c37cc3
yara.bousalman <yara.bousalman@vanrise.tfs.local>
2019-01-16 11:27:16 +0000

    Adding properties to CountryCodesPartSettings
    git-tfs-id: [http://developmentvlan:8585/tfs/vanrise.collection]$/;C52675

--- Retail/Code/TestCallAnalysis.Business/CountryCodesPartSettings.cs
+++ Retail/Code/TestCallAnalysis.Business/CountryCodesPartSettings.cs
@@ -3,10 +3,14 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Retail.BusinessEntity.Entities;
+using TestCallAnalysis.Entities;
 
 namespace TestCallAnalysis.Business
 {
-    class CountryCodesPartSettings
+    public class CountryCodesPartSettings : AccountPartSettings
     {
+        public override Guid ConfigId { get { return new Guid("E1174D46-B8C9-46D1-A993-F1E14565C551"); } }
+        public List<CountryCode> CountryCodes { get; set; }
     }
 }
