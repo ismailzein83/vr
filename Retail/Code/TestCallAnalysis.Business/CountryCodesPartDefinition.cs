e0b73526253330cdd7ffc83f4e8d15eb90e3e078
yara.bousalman <yara.bousalman@vanrise.tfs.local>
2019-01-16 09:53:35 +0000

    Adding CountryCodesPartDefinition
    git-tfs-id: [http://developmentvlan:8585/tfs/vanrise.collection]$/;C52663

--- Retail/Code/TestCallAnalysis.Business/CountryCodesPartDefinition.cs
+++ Retail/Code/TestCallAnalysis.Business/CountryCodesPartDefinition.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Retail.BusinessEntity.Entities;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -7,7 +8,14 @@ using TestCallAnalysis.Entities;
 
 namespace TestCallAnalysis.Business
 {
-    class CountryCodesPartDefinition
+    public class CountryCodesPartDefinition : AccountPartDefinitionSettings
     {
+        public override Guid ConfigId { get { return new Guid("51D18FDD-336C-4713-8258-304149B775FC"); } }
+
+        public List<CountryCode> CountryCodes { get; set; }
+        public override bool IsPartValid(IAccountPartDefinitionIsPartValidContext context)
+        {
+            throw new NotImplementedException();
+        }
     }
 }
--- Retail/Code/TestCallAnalysis.Entities/CountryCode.cs
+++ Retail/Code/TestCallAnalysis.Entities/CountryCode.cs
@@ -6,7 +6,8 @@ using System.Threading.Tasks;
 
 namespace TestCallAnalysis.Entities
 {
-    class CountryCode
+    public class CountryCode
     {
+        public string Code { get; set; }
     }
 }
