7b2f1ee6407e52a82f2248417c0b7f47e1bca1f2
Mohammad Sheaib <VANRISE\mohammad.sheaib>
2019-01-17 16:51:08 +0000

    Adding AccountCodeCustomObject
    git-tfs-id: [http://developmentvlan:8585/tfs/vanrise.collection]$/;C52737

---
+++ TOneV2/Code/TOneV2/TOne.WhS.Jazz.Business/AccountCodeCustomObject.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using TOne.WhS.BusinessEntity.Business;
+using Vanrise.GenericData.Entities;
+
+namespace TOne.WhS.Jazz.Business
+{
+public class AccountCodeCustomObject : FieldCustomObjectTypeSettings
+    {
+
+        public override Guid ConfigId { get { return new Guid("A474BA09-750B-4CD7-B842-0BAA550FE108"); } }
+
+        public override string GetDescription(IFieldCustomObjectTypeSettingsContext context)
+        {
+            return null;
+        }
+
+        public override bool AreEqual(Object newValue, Object oldValue)
+        {
+            return true;
+        }
+
+        public override Type GetNonNullableRuntimeType()
+        {
+            return typeof(WhsJazzAccountCodeCarriers);
+        }
+
+        public override dynamic ParseNonNullValueToFieldType(object originalValue)
+        {
+                return originalValue as WhsJazzAccountCodeCarriers;
+        }
+
+        public override string GetRuntimeTypeDescription()
+        {
+            return "Account Code Custom Object";
+        }
+    }
+}
---
+++ TOneV2/Code/TOneV2/TOne.WhS.Jazz.Entities/WhsJazzAccountCodeCustomObject.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+
+namespace TOne.WhS.BusinessEntity.Business
+{
+public class WhsJazzAccountCodeCarriers
+    {
+        public List<WhsJazzAccountCodeCarrierAccount> Carriers { get; set; }
+
+    }
+    public class WhsJazzAccountCodeCarrierAccount
+    {
+        public Guid WhsJazzCarrierAccountId { get; set; }
+    }
+}
