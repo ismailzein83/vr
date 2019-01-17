0d3957b652230786d08466696912bb91dda927d2
Hassan Makhour <VANRISE\hassan.makhour>
2019-01-17 15:14:32 +0000

    UtilityDataManager implementation(RDB)
    git-tfs-id: [http://developmentvlan:8585/tfs/vanrise.collection]$/;C52734

---
+++ Vanrise/Code/Vanrise.Common.Data.RDB/UtilityDataManager.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Vanrise.Data.RDB;
+using Vanrise.Entities;
+
+namespace Vanrise.Common.Data.RDB
+{
+	public class UtilityDataManager : IUtilityDataManager
+	{
+		#region Local Variables
+		#endregion
+
+		#region Constructors
+		#endregion
+
+		#region Public Methods
+		public bool CheckIfDefaultOrInvalid(DateTime? dateTime)
+		{
+			var queryContext = new RDBQueryContext(GetDataProvider());
+			return queryContext.CheckIfDefaultOrInvalidDBTime(dateTime);
+		}
+
+		public DateTimeRange GetDateTimeRange()
+		{
+			var queryContext = new RDBQueryContext(GetDataProvider());
+			return queryContext.GetDBDateTimeRange();
+		}
+		#endregion
+
+		#region Private Methods
+		private BaseRDBDataProvider GetDataProvider()
+		{
+			return RDBDataProviderFactory.CreateProvider("vrCommon", "ConfigurationDBConnStringKey", "ConfigurationDBConnStringKey");
+		}
+
+		#endregion
+
+		#region Mappers
+		#endregion
+
+	}
+}
