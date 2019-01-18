8d2560b5045d3bf45c0b02bb38b8c5ac31b6a628
Rodi Hassan <VANRISE\rodi.hassan>
2019-01-18 08:33:21 +0000

    add call interceprot files
    git-tfs-id: [http://developmentvlan:8585/tfs/vanrise.collection]$/;C52740

---
+++ SOM/Code/SOM.ST.Business/HttpConnectionCallInterceptor/BPMOnlineInterceptor.cs
@@ -0,0 +1,18 @@
+﻿//using System;
+//using Vanrise.Common.Business;
+
+//namespace SOM.ST.Business.ConnectionCallInterceptor
+//{
+//    public class BPMOnlineInterceptor : VRHttpConnectionCallInterceptor
+//    {
+//        public override void InterceptRequest(IVRHttpConnectionInterceptRequestContext context)
+//        {
+//            throw new System.NotImplementedException();
+//        }
+
+//        public override void InterceptResponse(IVRHttpConnectionInterceptResponseContext context)
+//        {
+//            throw new System.NotImplementedException();
+//        }
+//    }
+//}
---
+++ SOM/Code/SOM.ST.Business/HttpConnectionCallInterceptor/BSCSOnlineInterceptor.cs
@@ -0,0 +1,23 @@
+﻿//using Vanrise.Common.Business;
+
+//namespace SOM.ST.Business.ConnectionCallInterceptor
+//{
+//    public class BSCSOnlineInterceptor : VRHttpConnectionCallInterceptor
+//    {
+//        public string UserName { get; set; }
+
+//        public string Password { get; set; }
+
+//        public string AuthenticationServiceURI { get; set; }
+
+//        public override void InterceptRequest(IVRHttpConnectionInterceptRequestContext context)
+//        {
+//            var fullAuthenticationServiceURI = string.Concat(context.Connection.BaseURL, AuthenticationServiceURI);
+//        }
+
+//        public override void InterceptResponse(IVRHttpConnectionInterceptResponseContext context)
+//        {
+//            throw new System.NotImplementedException();
+//        }
+//    }
+//}
