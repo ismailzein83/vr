a7af65709f72064f8fe8fa0d2d74395b5b423aa7
Samer Haidar <VANRISE\samer.haidar>
2019-01-22 09:26:31 +0000

    adding CaseCDRChangeStatusAction
    git-tfs-id: [http://developmentvlan:8585/tfs/vanrise.collection]$/;C52853

---
+++ Retail/Code/TestCallAnalysis.Business/CaseCDRChangeStatusAction.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Vanrise.GenericData.Business;
+
+namespace TestCallAnalysis.Business
+{
+    public class CaseCDRChangeStatusAction : GenericBEActionSettings
+    {
+        public override string ActionKind { get { return ""; } }
+
+        public override Guid ConfigId { get { return new Guid("7ED6B43C-0470-4293-BD99-E6C7D1DA355D"); } }
+    }
+}
