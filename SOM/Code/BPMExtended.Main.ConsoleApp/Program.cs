463c7f06d9d8389eb23265d91e0142f509258380
Mortada Issa <VANRISE\mortada.issa>
2019-01-23 12:09:06 +0000

    Adding a project to test flows ran from BPM extended
    git-tfs-id: [http://developmentvlan:8585/tfs/vanrise.collection]$/;C52890

---
+++ SOM/Code/BPMExtended.Main.ConsoleApp/Program.cs
@@ -0,0 +1,35 @@
+﻿using BPMExtended.Main.Common;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BPMExtended.Main.ConsoleApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            using (SOMClient client = new SOMClient())
+            {
+                var resp = client.Post<TestInput,WFResponse>("", new TestInput());
+            }
+        }
+    }
+
+    public class TestInput
+    {
+        public TestInputArgument InputArgument { get; set; }
+    }
+
+    public class TestInputArgument
+    {
+        public string CustomerId { get; set; }
+    }
+
+    public class WFResponse
+    {
+
+    }
+}
