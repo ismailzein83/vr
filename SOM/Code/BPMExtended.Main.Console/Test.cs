2b58ca08793152279a03dc5f115d77ed28b71a99
Mortada Issa <VANRISE\mortada.issa>
2019-01-23 11:55:42 +0000

    Removing the project to recreate it as a console app
    git-tfs-id: [http://developmentvlan:8585/tfs/vanrise.collection]$/;C52889

--- SOM/Code/BPMExtended.Main.Console/Test.cs
+++ SOM/Code/BPMExtended.Main.Console/Test.cs
@@ -8,6 +8,16 @@ namespace BPMExtended.Main.Console
 {
     public class Test
     {
+        
+    }   
+    
+    public class TestInput
+    {
+        public TestInputArgument InputArgument { get; set; }
+    }
 
+    public class TestInputArgument
+    {
+        public string CustomerId { get; set; }
     }
 }
