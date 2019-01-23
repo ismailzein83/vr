350024987a9937e5ce8cc76eac92f2740fbde0d4
Samer Haidar <VANRISE\samer.haidar>
2019-01-23 13:25:45 +0000

    Fixing invoice Settlement invoice and refactoring
    git-tfs-id: [http://developmentvlan:8585/tfs/vanrise.collection]$/;C52898

--- TOneV2/Code/TOneV2/TOne.WhS.Invoice.Entities/CustomerInvoiceDetails.cs
+++ TOneV2/Code/TOneV2/TOne.WhS.Invoice.Entities/CustomerInvoiceDetails.cs
@@ -7,36 +7,19 @@ using TOne.WhS.BusinessEntity.Entities;
 
 namespace TOne.WhS.Invoice.Entities
 {
-    public class CustomerInvoiceDetails
+    public class CustomerInvoiceDetails : BaseInvoiceDetails
     {
-        public int TotalNumberOfCalls { get; set; }
         public Decimal SaleAmount { get; set; }
         public Decimal OriginalSaleAmount { get; set; }
-        public decimal Duration { get; set; }
-        public string PartnerType { get; set; }
         public int SaleCurrencyId { get; set; }
         public string SaleCurrency { get; set; }
         public int OriginalSaleCurrencyId { get; set; }
         public string OriginalSaleCurrency { get; set; }
-        public Decimal TotalAmount { get; set; }
-        public int? TimeZoneId { get; set; }
         public int CountryId { get; set; }
         public int SupplierId { get; set; }
         public int SupplierZoneId { get; set; }
-        public decimal AmountAfterCommission { get; set; }
-        public decimal OriginalAmountAfterCommission { get; set; }
-        public List<AttachementFile> AttachementFiles { get; set; }
         public string CustomerCurrency { get; set; }
-        public Decimal TotalAmountAfterCommission { get; set; }
-        public Decimal TotalOriginalAmountAfterCommission { get; set; }
-        public CommissionType? CommissionType { get; set; }
-        public decimal? Commission { get; set; }
-        public bool DisplayComission { get; set; }
-        public string Offset { get; set; }
-        public string Reference { get; set; }
         public CustomerInvoiceDetails() { }
-        public Dictionary<int, OriginalDataCurrrency> OriginalAmountByCurrency { get; set; }
-
         public IEnumerable<CustomerInvoiceDetails> GetCustomerInvoiceDetailsRDLCSchema()
         {
             return null;
--- TOneV2/Code/TOneV2/TOne.WhS.Invoice.Entities/SupplierInvoiceDetails.cs
+++ TOneV2/Code/TOneV2/TOne.WhS.Invoice.Entities/SupplierInvoiceDetails.cs
@@ -7,40 +7,42 @@ using TOne.WhS.BusinessEntity.Entities;
 
 namespace TOne.WhS.Invoice.Entities
 {
-    public class SupplierInvoiceDetails
+    public class BaseInvoiceDetails
     {
         public int TotalNumberOfCalls { get; set; }
-        public decimal CostAmount { get; set; }
-        public Decimal OriginalCostAmount { get; set; }
-
+        public string Offset { get; set; }
         public decimal Duration { get; set; }
+        public CommissionType? CommissionType { get; set; }
+        public decimal? Commission { get; set; }
+        public bool DisplayComission { get; set; }
         public string PartnerType { get; set; }
-        public int SupplierCurrencyId { get; set; }
-        public string SupplierCurrency { get; set; }
-        public int OriginalSupplierCurrencyId { get; set; }
-        public string OriginalSupplierCurrency { get; set; }
         public Decimal TotalAmount { get; set; }
         public int? TimeZoneId { get; set; }
-        public string Reference { get; set; }
-        public List<AttachementFile> AttachementFiles { get; set; }
         public decimal AmountAfterCommission { get; set; }
         public decimal OriginalAmountAfterCommission { get; set; }
         public Decimal TotalAmountAfterCommission { get; set; }
         public Decimal TotalOriginalAmountAfterCommission { get; set; }
-        public CommissionType? CommissionType { get; set; }
-
-        public decimal? Commission { get; set; }
-        public bool DisplayComission { get; set; }
-        public string Offset { get; set; }
-
         public Dictionary<int, OriginalDataCurrrency> OriginalAmountByCurrency { get; set; }
+        public string Reference { get; set; }
+        public List<AttachementFile> AttachementFiles { get; set; }
 
+    }
+    public class SupplierInvoiceDetails: BaseInvoiceDetails
+    {
+        public decimal CostAmount { get; set; }
+        public Decimal OriginalCostAmount { get; set; }
+        public int SupplierCurrencyId { get; set; }
+        public string SupplierCurrency { get; set; }
+        public int OriginalSupplierCurrencyId { get; set; }
+        public string OriginalSupplierCurrency { get; set; }
         public SupplierInvoiceDetails() { }
         public IEnumerable<SupplierInvoiceDetails> GetSupplierInvoiceDetailsRDLCSchema()
         {
             return null;
         }
     }
+
+
     public class AttachementFile
     {
         public long FileId { get; set; }
