7d8f6e0669545a593038aa7167fafef5a0cf7f74
Naim Saad <VANRISE\naim.saad>
2019-01-24 15:05:17 +0000

    Implementing ErlangBCalculatorParameters
    git-tfs-id: [http://developmentvlan:8585/tfs/vanrise.collection]$/;C52933

---
+++ Vanrise/Code/Vanrise.Entities/ErlangBCalculatorParameters.cs
@@ -0,0 +1,19 @@
+﻿//using System;
+//using System.Collections.Generic;
+//using System.Linq;
+//using System.Text;
+//using System.Threading.Tasks;
+
+//namespace Vanrise.Entities
+//{
+//    public class ErlangBCalculatorParameters
+//    {
+//        public int NumberOfDevices { get; set; }
+//        public decimal BlockingPercentage { get; set; }
+//    }
+//    public class ErlangBValueEntity
+//    {
+//        public ErlangBCalculatorParameters ErlangBCalculatorParameters { get; set; }
+//        public Decimal Value { get; set; }
+//    }
+//}
