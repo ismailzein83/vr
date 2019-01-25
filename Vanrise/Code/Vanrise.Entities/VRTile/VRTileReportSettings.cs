a65dfee3234f5f70363c6123967fec06a03b3006
Ali Sakr <VANRISE\ali.sakr>
2019-01-25 07:55:09 +0000

    adding VRTileReportSettings
    git-tfs-id: [http://developmentvlan:8585/tfs/vanrise.collection]$/;C52942

---
+++ Vanrise/Code/Vanrise.Entities/VRTile/VRTileReportSettings.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Vanrise.Entities
+{
+    public class VRTileReportSettings
+    {
+        public List<VRTile> VRTiles { get; set; }
+    }
+}
