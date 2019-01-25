f6e796964ee7a6b81f94b80eae50955d051aa067
Ali Sakr <VANRISE\ali.sakr>
2019-01-25 07:55:42 +0000

    adding custom object for VRTileReport
    git-tfs-id: [http://developmentvlan:8585/tfs/vanrise.collection]$/;C52943

---
+++ Vanrise/Code/Vanrise.Common.Business/VRTile/VRTileCustomObjectTypeSettings.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Vanrise.Common;
+using Vanrise.GenericData.Entities;
+
+
+namespace Vanrise.Entities
+{
+    public class VRTileCustomObjectTypeSettings : FieldCustomObjectTypeSettings
+    {
+        public override Guid ConfigId { get { return new Guid("46B078B8-3A93-47A1-AC4A-AC1C97E76526"); } }
+        public override string GetDescription(IFieldCustomObjectTypeSettingsContext context)
+        {
+            return null;
+        }
+        public override bool AreEqual(Object newValue, Object oldValue)
+        {
+
+            return true;
+        }
+        public override Type GetNonNullableRuntimeType()
+        {
+            return typeof(VRTileReportSettings);
+        }
+        public override dynamic ParseNonNullValueToFieldType(object originalValue)
+        {
+            var castedOriginalValue = originalValue as VRTileReportSettings;
+            if (castedOriginalValue != null)
+            {
+                return castedOriginalValue;
+            }
+            else
+                return Serializer.Deserialize<VRTileReportSettings>(originalValue.ToString());
+        }
+        public override string GetRuntimeTypeDescription()
+        {
+            return "VRTileReport";
+        }
+    }
+}
