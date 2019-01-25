e4f2c0133d56cff8bbb532ed70b85c14092979e7
Ali Atoui <VANRISE\ali.atoui>
2019-01-25 12:19:07 +0000

    Adding withNoLock @RDBJoinContext
    git-tfs-id: [http://developmentvlan:8585/tfs/vanrise.collection]$/;C52952

--- Vanrise/Code/Vanrise.Data.RDB/Join/RDBJoinContext.cs
+++ Vanrise/Code/Vanrise.Data.RDB/Join/RDBJoinContext.cs
@@ -1,8 +1,5 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace Vanrise.Data.RDB
 {
@@ -26,7 +23,12 @@ namespace Vanrise.Data.RDB
 
         public void JoinOnEqualOtherTableColumn(RDBJoinType joinType, string tableName, string tableAlias, string columnName, string otherTableAlias, string otherTableColumnName)
         {
-            JoinOnEqualOtherTableColumn(joinType, new RDBTableDefinitionQuerySource(tableName), tableAlias, columnName, otherTableAlias, otherTableColumnName);
+            JoinOnEqualOtherTableColumn(joinType, tableName, tableAlias, columnName, otherTableAlias, otherTableColumnName, false);
+        }
+
+        public void JoinOnEqualOtherTableColumn(RDBJoinType joinType, string tableName, string tableAlias, string columnName, string otherTableAlias, string otherTableColumnName, bool withNoLock)
+        {
+            JoinOnEqualOtherTableColumn(joinType, new RDBTableDefinitionQuerySource(tableName), tableAlias, columnName, otherTableAlias, otherTableColumnName, withNoLock);
         }
 
         public void JoinOnEqualOtherTableColumn(IRDBTableQuerySource table, string tableAlias, string columnName, string otherTableAlias, string otherTableColumnName)
@@ -36,8 +38,15 @@ namespace Vanrise.Data.RDB
 
         public void JoinOnEqualOtherTableColumn(RDBJoinType joinType, IRDBTableQuerySource table, string tableAlias, string columnName, string otherTableAlias, string otherTableColumnName)
         {
+            JoinOnEqualOtherTableColumn(joinType, table, tableAlias, columnName, otherTableAlias, otherTableColumnName, false);
+        }
+
+        public void JoinOnEqualOtherTableColumn(RDBJoinType joinType, IRDBTableQuerySource table, string tableAlias, string columnName, string otherTableAlias, string otherTableColumnName, bool withNoLock)
+        {
             var join = AddJoin(table, tableAlias);
             join.JoinType = joinType;
+            join.WithNoLock = withNoLock;
+
             join.Condition = new RDBCompareCondition
             {
                 Expression1 = new RDBColumnExpression
@@ -62,10 +71,7 @@ namespace Vanrise.Data.RDB
         public RDBJoinStatementContext Join(IRDBTableQuerySource table, string tableAlias)
         {
             var join = AddJoin(table, tableAlias);
-            return new RDBJoinStatementContext(
-                _queryBuilderContext,
-                join,
-                _tableAlias);
+            return new RDBJoinStatementContext(_queryBuilderContext, join, _tableAlias);
         }
 
         public RDBJoinSelectContext JoinSelect(string inlineQueryAlias)
@@ -78,11 +84,8 @@ namespace Vanrise.Data.RDB
         private RDBJoin AddJoin(IRDBTableQuerySource table, string tableAlias)
         {
             _queryBuilderContext.AddTableAlias(table, tableAlias);
-            var join = new RDBJoin
-            {
-                Table = table,
-                TableAlias = tableAlias
-            };
+
+            RDBJoin join = new RDBJoin { Table = table, TableAlias = tableAlias };
             _joins.Add(join);
             return join;
         }
