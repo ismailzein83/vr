4820963479118cd152aff2193c60c1ac3a0c72d0
Ali Sakr <VANRISE\ali.sakr>
2019-01-28 07:31:48 +0000

    adding figure tiles directives
    git-tfs-id: [http://developmentvlan:8585/tfs/vanrise.collection]$/;C52970

---
+++ Vanrise/Code/Vanrise.Common.Web/Common/Directives/FiguresTileSettings/vr-common-figurestilesettings-definition.js
@@ -0,0 +1,108 @@
+﻿//(function (app) {
+
+//    'use strict';
+
+//    FigurestilesettingsDefinition.$inject = ['UtilsService', 'VRUIUtilsService'];
+
+//    function FigurestilesettingsDefinition(UtilsService, VRUIUtilsService) {
+
+//        var directiveDefinitionObject = {
+//            restrict: 'E',
+//            scope: {
+//                onReady: '=',
+//            },
+//            controller: function ($scope, $element, $attrs) {
+//                var ctrl = this;
+//                var figurestilesettingsDefinition = new FigurestilesettingsDefinition(ctrl, $scope, $attrs);
+//                figurestilesettingsDefinition.initializeController();
+//            },
+//            controllerAs: 'ctrl',
+//            bindToController: true,
+//            compile: function (element, attrs) {
+//                return {
+//                    pre: function ($scope, iElem, iAttrs, ctrl) {
+
+//                    }
+//                };
+//            },
+//            templateUrl: '/Client/Modules/Common/Directives/FiguresTileSettings/Templates/FiguresTileSettingsDefinitionTemplate.html'
+//        };
+
+//        function FigurestilesettingsDefinition(ctrl, $scope, attrs) {
+//            this.initializeController = initializeController;
+
+//            var viewSelectorAPI;
+//            var viewSelectorReadyPromiseDeferred = UtilsService.createPromiseDeferred();
+
+//            var queryGridAPI;
+//            var queryGridReadyPromiseDeferred = UtilsService.createPromiseDeferred();
+
+//            function initializeController() {
+
+//                $scope.scopeModel = {};
+
+//                $scope.scopeModel.onViewsSelectorReady = function (api) {
+//                    viewSelectorAPI = api;
+//                    viewSelectorReadyPromiseDeferred.resolve();
+//                };
+
+//                $scope.scopeModel.onQueriesGridReady = function (api) {
+//                    queryGridAPI = api;
+//                    queryGridReadyPromiseDeferred.resolve();
+//                };
+
+//                defineAPI();
+//            }
+
+//            function defineAPI() {
+//                var api = {};
+
+//                api.load = function (payload) {
+//                    var promises = [];
+
+//                    var viewSelectorPromise = loadViewSelector();
+//                    promises.push(viewSelectorPromise);
+
+//                    var gridQueriesPromise = loadQueriesGrid();
+//                    promises.push(gridQueriesPromise);
+
+
+//                    return UtilsService.waitMultiplePromises(promises);
+//                };
+
+//                api.getData = function () {
+
+//                };
+
+//                if (ctrl.onReady != null)
+//                    ctrl.onReady(api);
+
+//                function loadViewSelector() {
+//                    var viewSelectorLoadDeferred = UtilsService.createPromiseDeferred();
+
+//                    viewSelectorReadyPromiseDeferred.promise.then(function () {
+//                        var viewSelectorPayload = { selectedIds: undefined};
+//                        VRUIUtilsService.callDirectiveLoad(viewSelectorAPI, viewSelectorPayload, viewSelectorLoadDeferred);
+//                    });
+
+//                    return viewSelectorLoadDeferred.promise;
+//                }
+//                function loadQueriesGrid() {
+//                    var gridQueriesLoadDeferred = UtilsService.createPromiseDeferred();
+
+//                    queryGridReadyPromiseDeferred.promise.then(function () {
+//                        var gridQueriesPayload = {};
+//                        VRUIUtilsService.callDirectiveLoad(queryGridAPI, gridQueriesPayload, gridQueriesLoadDeferred);
+//                    });
+
+//                    return gridQueriesLoadDeferred.promise;
+//                }
+
+//            }
+//        }
+//        return directiveDefinitionObject;
+//    }
+
+//    app.directive('vrCommonFigurestilesettingsDefinition', FigurestilesettingsDefinition);
+
+//})(app);
---
+++ Vanrise/Code/Vanrise.Common.Web/Common/Directives/VRTile/vr-common-tile-figurestile-queries.js
@@ -0,0 +1,81 @@
+﻿//'use strict';
+
+//app.directive('vrCommonTileFigurestileQueries', ['UtilsService', 'VRUIUtilsService', 'VRCommon_VRTileService',
+//    function (UtilsService, VRUIUtilsService, VRCommon_VRTileService) {
+
+//        return {
+//            restrict: 'E',
+//            scope: {
+//                onReady: '=',
+//            },
+//            controller: function ($scope, $element, $attrs) {
+//                var ctrl = this;
+//                var ctor = new FigurestileQueries(ctrl, $scope, $attrs);
+//                ctor.initializeController();
+//            },
+//            controllerAs: 'ctrl',
+//            bindToController: true,
+//            compile: function (element, attrs) {
+
+//            },
+//            templateUrl: '/Client/Modules/Common/Directives/VRTile/Templates/FiguresTileQueriesTemplate.html'
+//        };
+
+//        function FigurestileQueries(ctrl, $scope, $attrs) {
+//            this.initializeController = initializeController;
+
+//            var gridAPI;
+
+//            function initializeController() {
+//                ctrl.datasource = [];
+//                ctrl.isValid = function () {
+                    
+//                };
+//                ctrl.addQuery = function () {
+                   
+//                };
+//                ctrl.removeQuery = function (dataItem) {
+                
+//                };
+//                defineMenuActions();
+//                defineAPI();
+//            }
+//            function defineAPI() {
+//                var api = {};
+
+//                api.load = function (payload) {
+//                    var promises = [];
+
+                    
+//                    return UtilsService.waitMultiplePromises(promises);
+
+//                };
+
+//                api.getData = function () {
+                  
+//                };
+
+//                if (ctrl.onReady != null)
+//                    ctrl.onReady(api);
+
+
+//            }
+//            function defineMenuActions() {
+//                var defaultMenuActions = [{
+//                    name: "Edit",
+//                    clicked: editFigureTileQuery
+//                }];
+//                $scope.gridMenuActions = function (dataItem) {
+//                    return defaultMenuActions;
+//                };
+//            }
+
+//            function editFigureTileQuery(obj) {
+//                var onVRTileUpdated = function (figureTileQuery) {
+//                    var index = ctrl.datasource.indexOf(obj);
+//                    ctrl.datasource[index] = { Entity: figureTileQuery };
+//                };
+//                VRCommon_VRTileService.editVRTile(vrTileObj.Entity, onVRTileUpdated, ctrl.datasource);
+//            }
+//        }
+//    }]);
