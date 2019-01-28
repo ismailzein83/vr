fb1535aa5a4313487cfe693362534039b5158fba
Samer Haidar <VANRISE\samer.haidar>
2019-01-28 15:02:06 +0000

    Adding ScheduledReportProcess Workflow
    git-tfs-id: [http://developmentvlan:8585/tfs/vanrise.collection]$/;C52986

---
+++ Vanrise/Code/Vanrise.Analytic.BP.Arguments/VRScheduledReportProcessInput.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Vanrise.BusinessProcess.Entities;
+
+namespace Vanrise.Analytic.BP.Arguments
+{
+    public class VRScheduledReportProcessInput : BaseProcessInputArgument
+    {
+        public override string GetTitle()
+        {
+            return "Scheduled Report";
+        }
+        public Guid ScheduledReportId { get; set; }
+    }
+}
