649227d4cbb8b0cec5f441ce83797b8f5ff07004
Samer Haidar <VANRISE\samer.haidar>
2019-01-29 09:52:44 +0000

    Adding TOneModuleManager
    git-tfs-id: [http://developmentvlan:8585/tfs/vanrise.collection]$/;C52999

---
+++ TOneV2/Code/TOneV2/TOne.WhS.BusinessEntity.Business/TOneModuleManager.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Vanrise.Analytic.Business;
+
+namespace TOne.WhS.BusinessEntity.Business
+{
+    public class TOneModuleManager
+    {
+        static Guid voiceBillingStatAnalyticTableId = new Guid("4c1aaa1b-675b-420f-8e60-26b0747ca79b");
+        static Guid smsBillingStatAnalyticTableId = new Guid("");
+        AnalyticTableManager analyticTableManager = new AnalyticTableManager();
+        public bool IsSMSModuleEnabled()
+        {
+            return analyticTableManager.IsAnalyticTableExist(smsBillingStatAnalyticTableId);
+        }
+        public bool IsVoiceModuleEnabled()
+        {
+            return analyticTableManager.IsAnalyticTableExist(voiceBillingStatAnalyticTableId);
+        }
+    }
+}
