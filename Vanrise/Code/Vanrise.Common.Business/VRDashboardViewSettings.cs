b47ae27265eeb62d2ef7d6829cceb90ab22e6616
Fatima Hammoud <VANRISE\fatima.hammoud>
2019-01-29 13:02:50 +0000

    adding VRDashboardViewSettings
    git-tfs-id: [http://developmentvlan:8585/tfs/vanrise.collection]$/;C53005

---
+++ Vanrise/Code/Vanrise.Common.Business/VRDashboardViewSettings.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Vanrise.Security.Entities;
+
+namespace Vanrise.Common.Business
+{
+    public class VRDashboardViewSettings : ViewSettings
+    {
+        public List<VRDashboardViewSettingsItem> DashboardDefinitionItems { get; set; }
+
+        public override string GetURL(View view)
+        {
+            return String.Format("#/viewwithparams/Common/Views/VRDashboard/VRDashboardManagement/{{\"viewId\":\"{0}\"}}", view.ViewId);
+        }
+    }
+
+    public class VRDashboardViewSettingsItem
+    {
+        Guid DashboardDefinitionId { get; set; }
+    }
+}
