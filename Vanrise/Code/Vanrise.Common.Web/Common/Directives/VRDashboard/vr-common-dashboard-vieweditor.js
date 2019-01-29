6d6e53e0707e68c9c30fe6310a5d4f7445e34795
Fatima Hammoud <VANRISE\fatima.hammoud>
2019-01-29 13:06:10 +0000

    Adding new files
    git-tfs-id: [http://developmentvlan:8585/tfs/vanrise.collection]$/;C53008

---
+++ Vanrise/Code/Vanrise.Common.Web/Common/Directives/VRDashboard/vr-common-dashboard-vieweditor.js
@@ -0,0 +1,99 @@
+﻿//"use strict";
+
+//app.directive("vrCommonDashboardVieweditor", ["UtilsService", "VRUIUtilsService", "VR_GenericData_GenericBEDefinitionAPIService",
+//    function (UtilsService, VRUIUtilsService, VR_GenericData_GenericBEDefinitionAPIService) {
+
+//        var directiveDefinitionObject = {
+//            restrict: "E",
+//            scope:
+//            {
+//                onReady: "="
+//            },
+//            controller: function ($scope, $element, $attrs) {
+//                var ctrl = this;
+//                var ctor = new ViewEditorCtor($scope, ctrl, $attrs);
+//                ctor.initializeController();
+//            },
+//            controllerAs: "ctrl",
+//            bindToController: true,
+//            compile: function (element, attrs) {
+
+//            },
+//            templateUrl: "/Client/Modules/Common/Directives/VRDashboard/Templates/VRDashboardViewEditor.html"
+//        };
+//        function ViewEditorCtor($scope, ctrl, $attrs) {
+//            this.initializeController = initializeController;
+
+//            var vrDashboardDirectiveApi;
+//            var vrDashboardDirectivePromiseDeferred = UtilsService.createPromiseDeferred();
+
+//            function initializeController() {
+//                $scope.scopeModel = {};
+
+//                $scope.scopeModel.onVRDashboardDirectiveReady = function (api) {
+//                    vrDashboardDirectiveApi = api;
+//                    console.log(vrDashboardDirectiveApi);
+//                    vrDashboardDirectivePromiseDeferred.resolve();
+//                };
+
+//                UtilsService.waitMultiplePromises([vrDashboardDirectivePromiseDeferred.promise]).then(function () {
+//                    defineAPI();
+//                });
+//            }
+
+//            function defineAPI() {
+//                var api = {};
+
+//                api.load = function (payload) {
+//                    console.log(payload);
+//                    var promises = [];
+
+//                    if (payload != undefined) {
+//                    }
+//                    promises.push(loadVRDashboardDirective());
+//                   // promises.push(loadVRTileBusinessEntity());
+
+//                    function loadVRDashboardDirective() {
+//                        var vrDashboardPayload;
+//                        //if (payload != undefined) {
+//                        //    vrDashboardPayload = {
+//                        //        businessEntityDefinitionId: '6243CA7F-A14C-41BE-BE48-86322D835CA6'
+//                        //    };
+//                        //}
+//                        vrDashboardPayload = {
+//                            businessEntityDefinitionId: '6243CA7F-A14C-41BE-BE48-86322D835CA6'
+//                        };
+//                        return vrDashboardDirectiveApi.load(vrDashboardPayload);
+//                    }
+
+//                    return UtilsService.waitMultiplePromises(promises);
+//                };
+
+//                api.getData = function () {
+//                    console.log(vrDashboardDirectiveApi.getSelectedIds());
+//                    return {
+//                        $type: "Vanrise.Common.Business.VRDashboardViewSettings, Vanrise.Common.Business",
+//                        DashBoardDefinitionItems: [{
+//                            DashBoardDefinitionId: vrDashboardDirectiveApi.getSelectedIds(),
+//                        }]
+//                    };
+//                };
+
+//                if (ctrl.onReady != null)
+//                    ctrl.onReady(api);
+
+//                function loadVRTileBusinessEntity() {
+//                    console.log("loadVRTileBusinessEntity");
+//                    var businessEntityDefinitionId = '6243CA7F-A14C-41BE-BE48-86322D835CA6';
+//                    return VR_GenericData_GenericBEDefinitionAPIService.GetGenericBEGridDefinition(businessEntityDefinitionId).then(function (response) {
+//                        console.log(response);
+//                    });
+//                }
+//            };
+
+
+//        }
+
+//        return directiveDefinitionObject;
+//    }
+//]);
---
+++ Vanrise/Code/Vanrise.Common.Web/Common/Views/VRDashboard/VRDashboardManagementController.js
@@ -0,0 +1,83 @@
+﻿//"use strict";
+//VRDashBoardManagementController.$inject = ['$scope', 'VRUIUtilsService', 'VRNavigationService', 'VR_Sec_ViewAPIService', 'UtilsService', 'VRNotificationService', 'ColumnWidthEnum'];
+
+//function VRDashBoardManagementController($scope, VRUIUtilsService, VRNavigationService, VR_Sec_ViewAPIService, UtilsService, VRNotificationService, ColumnWidthEnum) {
+//    var viewId;
+
+//    var viewEntity;
+
+//    loadParameters();
+//    defineScope();
+//    load();
+
+//    function loadParameters() {
+//        var parameters = VRNavigationService.getParameters($scope);
+//        if (parameters != null && parameters.viewId != undefined) {
+//            viewId = parameters.viewId;
+//        }
+//    }
+
+//    function defineScope() {
+//        $scope.scopeModel = {};
+//        $scope.scopeModel.vrTiles = [];
+//    }
+
+//    function load() {
+//        $scope.scopeModel.isLoading = true;
+//        getView().then(function () {
+//            loadAllControls();
+//        }).catch(function () {
+//            VRNotificationService.notifyExceptionWithClose(error, $scope);
+//            $scope.scopeModel.isLoading = false;
+//        });
+//    }
+
+//    function loadAllControls() {
+
+//        function loadVRTiles() {
+//            var promises = [];
+//            if (viewEntity != undefined && viewEntity.Settings != undefined && viewEntity.Settings.VRTileViewData != undefined) {
+//                var tiles = viewEntity.Settings.VRTileViewData.VRTiles;
+//                if (tiles != undefined) {
+//                    for (var i = 0, tilesLength = tiles.length; i < tilesLength; i++) {
+//                        var tileEntity = tiles[i];
+//                        addTile(tileEntity);
+//                    }
+//                }
+//            }
+//            function addTile(tileEntity) {
+//                var columnWidthObj = UtilsService.getEnum(ColumnWidthEnum, "value", tileEntity.Settings.NumberOfColumns);
+//                var tile = {
+//                    name: tileEntity.Name,
+//                    runtimeEditor: tileEntity.Settings.ExtendedSettings.RuntimeEditor,
+//                    columnWidth: columnWidthObj != undefined ? columnWidthObj.numberOfColumns : undefined
+//                };
+//                tile.onVRTileDirectiveReady = function (api) {
+//                    tile.tileAPI = api;
+//                    var setLoader = function (value) {
+//                        $scope.scopeModel.isDirectiveLoading = value;
+//                    };
+//                    var payload = { definitionSettings: tileEntity.Settings.ExtendedSettings };
+//                    VRUIUtilsService.callDirectiveLoadOrResolvePromise($scope, tile.tileAPI, payload, setLoader);
+//                };
+//                $scope.scopeModel.vrTiles.push(tile);
+//            }
+//            return UtilsService.waitMultiplePromises(promises);
+//        }
+
+//        return UtilsService.waitMultipleAsyncOperations([loadVRTiles]).then(function () {
+//        }).finally(function () {
+//            $scope.scopeModel.isLoading = false;
+//        }).catch(function (error) {
+//            VRNotificationService.notifyExceptionWithClose(error, $scope);
+//        });
+
+//    }
+
+//    function getView() {
+//        return VR_Sec_ViewAPIService.GetView(viewId).then(function (viewEntityObj) {
+//            viewEntity = viewEntityObj;
+//        });
+//    }
+//}
+//appControllers.controller('VRCommon_VRDashBoardManagementController', VRDashBoardManagementController);
