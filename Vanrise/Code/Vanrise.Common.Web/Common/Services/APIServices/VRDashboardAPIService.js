ec7a973f20443435e6c7618385c91b53e8a3a66d
Fatima Hammoud <VANRISE\fatima.hammoud>
2019-01-30 11:41:15 +0000

    add  VRDashboardController &  VRDashboardAPIService
    git-tfs-id: [http://developmentvlan:8585/tfs/vanrise.collection]$/;C53035

---
+++ Vanrise/Code/Vanrise.Common.Web/Common/Services/APIServices/VRDashboardAPIService.js
@@ -0,0 +1,22 @@
+﻿//(function (appControllers) {
+
+//    "use strict";
+//    VRDashboardAPIService.$inject = ['BaseAPIService', 'UtilsService', 'VRCommon_ModuleConfig', 'SecurityService'];
+
+//    function VRDashboardAPIService(BaseAPIService, UtilsService, VRCommon_ModuleConfig, SecurityService) {
+
+//        var controllerName = 'VRDashboard';
+
+//        function GetTileExtendedSettingsConfigs() {
+//            return BaseAPIService.get(UtilsService.getServiceURL(VRCommon_ModuleConfig.moduleName, controllerName, "GetTileExtendedSettingsConfigs"));
+//        }
+
+//        return ({
+//            GetTileExtendedSettingsConfigs: GetTileExtendedSettingsConfigs
+
+//        });
+//    }
+
+//    appControllers.service('VRCommon_VRDashboardAPIService', VRDashboardAPIService);
+
+//})(appControllers);
---
+++ Vanrise/Code/Vanrise.Common.Web/Controllers/VRDashboardController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Http;
+using Vanrise.Web.Base;
+
+namespace Vanrise.Common.Web.Controllers
+{
+    [RoutePrefix(Constants.ROUTE_PREFIX + "VRDashboard")]
+    [JSONWithTypeAttribute]
+
+    public class VRDashboardController
+    {
+        
+    }
+}
