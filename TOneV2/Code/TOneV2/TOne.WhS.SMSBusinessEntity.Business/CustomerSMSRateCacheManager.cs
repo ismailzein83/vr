d875cd63ab1bfb84199d2e78ccf8dc497331bdc0
Ali Slayman <VANRISE\ali.slayman>
2019-01-31 13:00:40 +0000

    Adding references to SMSBusinessEntity.Business
    git-tfs-id: [http://developmentvlan:8585/tfs/vanrise.collection]$/;C53051

--- TOneV2/Code/TOneV2/TOne.WhS.SMSBusinessEntity.Business/CustomerSMSRateCacheManager.cs
+++ TOneV2/Code/TOneV2/TOne.WhS.SMSBusinessEntity.Business/CustomerSMSRateCacheManager.cs
@@ -1,12 +1,34 @@
-﻿//using System;
-//using System.Collections.Generic;
-//using System.Linq;
-//using System.Text;
-//using System.Threading.Tasks;
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TOne.WhS.SMSBusinessEntity.Data;
+using Vanrise.Caching;
 
-//namespace TOne.WhS.SMSBusinessEntity.Business
-//{
-//    public class CustomerSMSRateCacheManager
-//    {
-//    }
-//}
+namespace TOne.WhS.SMSBusinessEntity.Business
+{
+    public class CustomerSMSRateCacheManager : BaseCacheManager
+    {
+        ICustomerSMSRateDataManager _dataManager = CustomerSMSRateDataManagerFactory.GetDataManager<ICustomerSMSRateDataManager>();
+        object _updateHandle;
+
+        public override Vanrise.Caching.CacheObjectSize ApproximateObjectSize
+        {
+            get
+            {
+                return Vanrise.Caching.CacheObjectSize.ExtraLarge;
+            }
+        }
+
+        public override T GetOrCreateObject<T>(object cacheName, Func<T> createObject)
+        {
+            return GetOrCreateObject(cacheName, BECacheExpirationChecker.Instance, createObject);
+        }
+
+        protected override bool ShouldSetCacheExpired(object parameter)
+        {
+            return _dataManager.AreSaleRatesUpdated(ref _updateHandle);
+        }
+    }
+}
