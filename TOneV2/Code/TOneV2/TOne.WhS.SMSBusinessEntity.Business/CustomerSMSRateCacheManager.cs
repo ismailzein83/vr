d3a9926fdd0b58c45283097a78d1b2182c340e40
Ali Slayman <VANRISE\ali.slayman>
2019-01-31 13:24:32 +0000

    Editing CustomerSMSRateCacheManager
    git-tfs-id: [http://developmentvlan:8585/tfs/vanrise.collection]$/;C53053

--- TOneV2/Code/TOneV2/TOne.WhS.SMSBusinessEntity.Business/CustomerSMSRateCacheManager.cs
+++ TOneV2/Code/TOneV2/TOne.WhS.SMSBusinessEntity.Business/CustomerSMSRateCacheManager.cs
@@ -4,31 +4,10 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using TOne.WhS.SMSBusinessEntity.Data;
-using Vanrise.Caching;
 
 namespace TOne.WhS.SMSBusinessEntity.Business
 {
-    public class CustomerSMSRateCacheManager : BaseCacheManager
+    public class CustomerSMSRateCacheManager : Vanrise.Caching.BaseCacheManager
     {
-        ICustomerSMSRateDataManager _dataManager = CustomerSMSRateDataManagerFactory.GetDataManager<ICustomerSMSRateDataManager>();
-        object _updateHandle;
-
-        public override Vanrise.Caching.CacheObjectSize ApproximateObjectSize
-        {
-            get
-            {
-                return Vanrise.Caching.CacheObjectSize.ExtraLarge;
-            }
-        }
-
-        public override T GetOrCreateObject<T>(object cacheName, Func<T> createObject)
-        {
-            return GetOrCreateObject(cacheName, BECacheExpirationChecker.Instance, createObject);
-        }
-
-        protected override bool ShouldSetCacheExpired(object parameter)
-        {
-            return _dataManager.AreSaleRatesUpdated(ref _updateHandle);
-        }
     }
 }
