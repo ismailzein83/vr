b8f58ea50d915558dfdfed3a188797a4c55d97f8
Ali Sakr <VANRISE\ali.sakr>
2019-02-01 08:17:21 +0000

    moving class FiguresTileDisplayItem
    git-tfs-id: [http://developmentvlan:8585/tfs/vanrise.collection]$/;C53067

--- Vanrise/Code/Vanrise.Common.MainExtensions/VRTile/FiguresTileSettings.cs
+++ Vanrise/Code/Vanrise.Common.MainExtensions/VRTile/FiguresTileSettings.cs
@@ -11,17 +11,17 @@ namespace Vanrise.Common.MainExtensions.VRTile
     {
         public override Guid ConfigId { get { return new Guid("BE0BBEC6-F506-4805-AF5C-068843EF7481"); } }
 
-        public override string RuntimeEditor { get { return ""; } }
+        public override string RuntimeEditor { get { return "vr-common-figurestilesettings-runtime"; } }
         public Guid? ViewId { get; set; }
         public List<FiguresTileQuery> Queries { get; set; }
         public List<FiguresTileDisplayItem> ItemsToDisplay { get; set; }
     }
-    public class FiguresTileDisplayItem
-    {
-        public Guid FiguresTileQueryId { get; set; }
-        public string Name { get; set; }
-        public string Title { get; set; }
-    }
-
+    //public class FiguresTileDisplayItem
+    //{
+    //    public Guid FiguresTileQueryId { get; set; }
+    //    public string Name { get; set; }
+    //    public string Title { get; set; }
+    //    public string QueryName { get; set; }
+    //}
 
 }
