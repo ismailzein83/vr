ca04eaf58667e67c22ad4a16c031b2a4878fa7bc
Ali Slayman <VANRISE\ali.slayman>
2019-02-04 07:56:39 +0000

    Adding CustomerSMSRateItem to SMSBusinessEntity.Entities
    git-tfs-id: [http://developmentvlan:8585/tfs/vanrise.collection]$/;C53091

---
+++ TOneV2/Code/TOneV2/TOne.WhS.SMSBusinessEntity.Entities/CustomerSMSRate/CustomerSMSRateItem.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TOne.WhS.SMSBusinessEntity.Entities
+{
+    public class CustomerSMSRateItem
+    {
+        public CustomerSMSRate CurrentRate { get; set; }
+
+        public CustomerSMSRate FutureRate { get; set; }
+    }
+}
