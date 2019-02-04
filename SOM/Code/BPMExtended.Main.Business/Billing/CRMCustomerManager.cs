ffdbd3f684f99953c275d5e1380199f3e3b8cedd
Mohammad Yahfoufi <VANRISE\mohammad.yahfoufi>
2019-02-04 14:41:21 +0000

    Adding PostToOM Functions
    git-tfs-id: [http://developmentvlan:8585/tfs/vanrise.collection]$/;C53103

--- SOM/Code/BPMExtended.Main.Business/Billing/CRMCustomerManager.cs
+++ SOM/Code/BPMExtended.Main.Business/Billing/CRMCustomerManager.cs
@@ -187,6 +187,30 @@ namespace BPMExtended.Main.Business
 
         }
 
+        public void PostADSLComplaintToOM(Guid requestId)
+        {
+            UserConnection connection = (UserConnection)HttpContext.Current.Session["UserConnection"];
+            var update = new Update(connection, "StRequestHeader").Set("StStatusId", Column.Parameter("8057E9A4-24DE-484D-B202-0D189F5B7758"))
+                .Where("StRequestId").IsEqual(Column.Parameter(requestId));
+            update.Execute();
+        }
+
+        public void PostTelephonyTechnicalComplaintToOM(Guid requestId)
+        {
+            UserConnection connection = (UserConnection)HttpContext.Current.Session["UserConnection"];
+            var update = new Update(connection, "StRequestHeader").Set("StStatusId", Column.Parameter("8057E9A4-24DE-484D-B202-0D189F5B7758"))
+                .Where("StRequestId").IsEqual(Column.Parameter(requestId));
+            update.Execute();
+        }
+
+        public void PostLeasedLinelComplaintToOM(Guid requestId)
+        {
+            UserConnection connection = (UserConnection)HttpContext.Current.Session["UserConnection"];
+            var update = new Update(connection, "StRequestHeader").Set("StStatusId", Column.Parameter("8057E9A4-24DE-484D-B202-0D189F5B7758"))
+                .Where("StRequestId").IsEqual(Column.Parameter(requestId));
+            update.Execute();
+        }
+
         public void ChangeLeasedLineSpeedRequestHeaderStatusToCompleted(Guid requestId)
         {
             UserConnection connection = (UserConnection)HttpContext.Current.Session["UserConnection"];
