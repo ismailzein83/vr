de58f613f53a6f0afdc873a7753e024d5c01aed9
Fatima Hammoud <VANRISE\fatima.hammoud>
2019-02-05 14:49:20 +0000

    adding RDBDataProvider Extenstion
    git-tfs-id: [http://developmentvlan:8585/tfs/vanrise.collection]$/;C53137

---
+++ Vanrise/Code/Vanrise.Analytic.Web/Analytic/Directives/MainExtensions/AnalyticDataProvider/vr-analytic-rdb-dataprovider.js
@@ -0,0 +1,53 @@
+﻿(function (app) {
+
+    'use strict';
+
+    RDBDataProviderDirective.$inject = ["UtilsService", 'VRUIUtilsService', 'VRNotificationService'];
+
+    function RDBDataProviderDirective(UtilsService, VRUIUtilsService, VRNotificationService) {
+        return {
+            restrict: "E",
+            scope: {
+                onReady: "=",
+            },
+            controller: function ($scope, $element, $attrs) {
+                var ctrl = this;
+                var sqlDataProvider = new SqlDataProvider($scope, ctrl, $attrs);
+                sqlDataProvider.initializeController();
+            },
+            controllerAs: "Ctrl",
+            bindToController: true,
+            templateUrl: "/Client/Modules/Analytic/Directives/MainExtensions/AnalyticDataProvider/Templates/RDBAnalyticDataProviderTemplate.html"
+
+        };
+        function SqlDataProvider($scope, ctrl, $attrs) {
+            this.initializeController = initializeController;
+
+            function initializeController() {
+                $scope.scopeModel = {};
+                defineAPI();
+            }
+            function defineAPI() {
+                var api = {};
+
+                api.load = function (payload) {
+
+                };
+
+                api.getData = function () {
+                    var data = {
+                        $type: "Vanrise.Analytic.Data.SQL.SQLAnalyticDataProvider, Vanrise.Analytic.Data.SQL"
+                    };
+                    return data;
+                };
+
+                if (ctrl.onReady != undefined && typeof (ctrl.onReady) == 'function') {
+                    ctrl.onReady(api);
+                }
+            }
+        }
+    }
+
+    app.directive('vrAnalyticRDBDataprovider', RDBDataProviderDirective);
+
+})(app);
