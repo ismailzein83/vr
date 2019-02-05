5021a34c5ee71be8be641eada3a7af8964cffbaa
Ali Slayman <VANRISE\ali.slayman>
2019-02-05 08:56:09 +0000

    Adding MobileCountryInfo and MobileCountryInfoFilter
    git-tfs-id: [http://developmentvlan:8585/tfs/vanrise.collection]$/;C53119

---
+++ Vanrise/Code/Vanrise.MobileNetwork/MobileCountry/MobileCountryInfo.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Vanrise.MobileNetwork.Entities
+{
+    public class MobileCountryInfo
+    {
+        public int Id { get; set; }
+
+        public string MobileCountryName { get; set; }
+    }
+}
