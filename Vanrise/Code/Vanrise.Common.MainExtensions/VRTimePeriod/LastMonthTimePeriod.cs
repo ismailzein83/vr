7bcff6b2f059a2fe548371617db09e480fb89062
Ali Sakr <VANRISE\ali.sakr>
2019-02-05 13:19:04 +0000

    adding new extension for last month
    git-tfs-id: [http://developmentvlan:8585/tfs/vanrise.collection]$/;C53129

---
+++ Vanrise/Code/Vanrise.Common.MainExtensions/VRTimePeriod/LastMonthTimePeriod.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using Vanrise.Entities;
+
+namespace Vanrise.Common.MainExtensions
+{
+    public class LastMonthTimePeriod : VRTimePeriod
+    {
+        public override Guid ConfigId { get { return new Guid("F48249D1-C20D-4B36-BBEA-8AAF8A87B18E"); } }
+
+        public override void GetTimePeriod(IVRTimePeriodContext context)
+        {
+            var newDate = context.EffectiveDate.AddMonths(-1);
+            context.FromTime = new DateTime(newDate.Year, newDate.Month, 1);
+            context.ToTime = new DateTime(newDate.Year,newDate.Month,newDate.GetLastDayOfMonth().Day);
+        }
+    }
+}
