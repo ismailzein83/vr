275e8560bafd0aac9414c343ed5fbe7f848aebea
Wissam Ajamy <VANRISE\wissam.ajamy>
2019-02-05 15:04:50 +0000

    Adding ExcelConversion Module
    git-tfs-id: [http://developmentvlan:8585/tfs/vanrise.collection]$/;C53142

--- Vanrise/Code/Resources/BatchScripts/CarrierPortal/02-Runtime.bat
+++ Vanrise/Code/Resources/BatchScripts/CarrierPortal/02-Runtime.bat
@@ -10,6 +10,8 @@ ECHO.
 ECHO CarrierPortal Runtime
 xcopy "C:\TFS\CarrierPortal\Code\CarrierPortal.DevRuntime\bin\x64\Release" /y /v /z /i /Q /R	"\\192.168.110.185\Fixes\WebSite\CarrierPortal\Runtime\%YYYYMMDD%\" /EXCLUDE:C:\TFS\Vanrise\Code\Resources\BatchScripts\Common\CarrierPortalSource\runtime-list-of-excluded-files.txt
 xcopy "C:\TFS\CarrierPortal\Code\CarrierPortal.DevRuntime\app.config.exclude" /y /v /z /i /Q /R "\\192.168.110.185\Fixes\WebSite\CarrierPortal\Runtime\%YYYYMMDD%\"
+xcopy "C:\TFS\Vanrise\Code\Resources\DLLs\Aspose.Cells.dll" /y /v /z /i /Q /R					"\\192.168.110.185\Fixes\WebSite\CarrierPortal\Runtime\%YYYYMMDD%\"
+xcopy "C:\TFS\Vanrise\Code\Resources\DLLs\Aspose.Cells.lic" /y /v /z /i /Q /R					"\\192.168.110.185\Fixes\WebSite\CarrierPortal\Runtime\%YYYYMMDD%\"
 rename																							"\\192.168.110.185\Fixes\WebSite\CarrierPortal\Runtime\%YYYYMMDD%\app.config.exclude"			"CarrierPortal.Runtime.exe.config"
 rename																							"\\192.168.110.185\Fixes\WebSite\CarrierPortal\Runtime\%YYYYMMDD%\CarrierPortal.DevRuntime.exe" "CarrierPortal.Runtime.exe"
 del /s /q /f																					"\\192.168.110.185\Fixes\WebSite\CarrierPortal\Runtime\%YYYYMMDD%\*.pdb"
