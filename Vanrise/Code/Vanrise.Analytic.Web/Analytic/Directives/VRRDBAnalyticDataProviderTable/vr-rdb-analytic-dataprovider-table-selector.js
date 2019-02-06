c74a0fc987a2a7c7dc7c191fedf220acfd7ec002
Fatima Hammoud <VANRISE\fatima.hammoud>
2019-02-06 07:43:52 +0000

    adding rdbAnalyticDataProviderTableSelector
    git-tfs-id: [http://developmentvlan:8585/tfs/vanrise.collection]$/;C53149

---
+++ Vanrise/Code/Vanrise.Analytic.Web/Analytic/Directives/VRRDBAnalyticDataProviderTable/vr-rdb-analytic-dataprovider-table-selector.js
@@ -0,0 +1,32 @@
+﻿(function (appControllers) {
+
+    "use strict";
+    RdbAnalyticDataproviderTableSelector.$inject = ['UtilsService', 'VRUIUtilsService', 'VR_Analytic_AnalyticConfigurationAPIService'];
+    function RdbAnalyticDataproviderTableSelector(UtilsService, VRUIUtilsService, VR_Analytic_AnalyticConfigurationAPIService) {
+        var directiveDefinitionObject = {
+            restrict: "E",
+            scope: {
+                onReady: "=",
+                normalColNum: '@',
+                isrequired: '=',
+
+            },
+            controller: function ($scope, $element, $attrs) {
+                var ctrl = this;
+                var RdbAnalyticDataproviderTableSelector = new RdbAnalyticDataproviderTableSelector($scope, ctrl, $attrs);
+                RdbAnalyticDataproviderTableSelector.initializeController();
+            },
+            controllerAs: "ctrl",
+            bindToController: true,
+            template: 
+        };
+        function RdbAnalyticDataproviderTableSelector($scope, ctrl, $attrs) {
+
+        }
+
+
+        return directiveDefinitionObject;
+    }
+    
+    app.directive("vrRdbAnalyticDataproviderTableSelector", RdbAnalyticDataproviderTableSelector);
+})(appControllers);
