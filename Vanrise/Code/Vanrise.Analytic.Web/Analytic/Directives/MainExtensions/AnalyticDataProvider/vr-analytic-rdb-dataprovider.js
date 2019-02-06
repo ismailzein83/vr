77b9f92e071c1c0cee12829a84f02b33e8afd896
Fatima Hammoud <VANRISE\fatima.hammoud>
2019-02-06 12:15:17 +0000

    add token
    git-tfs-id: [http://developmentvlan:8585/tfs/vanrise.collection]$/;C53174

--- Vanrise/Code/Vanrise.Analytic.Web/Analytic/Directives/MainExtensions/AnalyticDataProvider/vr-analytic-rdb-dataprovider.js
+++ Vanrise/Code/Vanrise.Analytic.Web/Analytic/Directives/MainExtensions/AnalyticDataProvider/vr-analytic-rdb-dataprovider.js
@@ -9,45 +9,105 @@
             restrict: "E",
             scope: {
                 onReady: "=",
+                normalColNum: '@',
             },
             controller: function ($scope, $element, $attrs) {
                 var ctrl = this;
-                var sqlDataProvider = new SqlDataProvider($scope, ctrl, $attrs);
-                sqlDataProvider.initializeController();
+                var rdbDataProvider = new RDBDataProvider($scope, ctrl, $attrs);
+                rdbDataProvider.initializeController();
             },
-            controllerAs: "Ctrl",
+            controllerAs: "rdbCtrl",
             bindToController: true,
             templateUrl: "/Client/Modules/Analytic/Directives/MainExtensions/AnalyticDataProvider/Templates/RDBAnalyticDataProviderTemplate.html"
 
         };
-        function SqlDataProvider($scope, ctrl, $attrs) {
+        function RDBDataProvider($scope, ctrl, $attrs) {
             this.initializeController = initializeController;
+
+            var rdbTableSelectorAPI;
+            var rdbTableReadyPromiseDeferred = UtilsService.createPromiseDeferred();
+            var rdbTableSelectorSelectionChangedDeferred;
 
             function initializeController() {
                 $scope.scopeModel = {};
+
+                //$scope.scopeModel.onRDBTableSelectorReady = function (api) {
+                //    rdbTableSelectorAPI = api;
+                //    rdbTableReadyPromiseDeferred.resolve();
+                //};
+
+                //$scope.scopeModel.onRDBTableSelectionChanged = function (dataItem) {
+                //    if (dataItem != undefined) {
+                //        if (rdbTableSelectorSelectionChangedDeferred != undefined) {
+                //            rdbTableSelectorSelectionChangedDeferred.resolve();
+                //        }
+                //    }
+                //    else {
+                //        ///////
+                //    }
+                //};
                 defineAPI();
             }
             function defineAPI() {
                 var api = {};
 
                 api.load = function (payload) {
+                    var promises = [];
 
-                };
+                    //    var isEditMode;
+                    //    var rdbAnalyticDataProviderTable
+                    //    if (payload != undefined && payload.analyticDataProviderSettings != undefined) {
+                    //        $scope.scopeModel.moduleName = payload.analyticDataProviderSettings.ModuleName;
+                    //        rdbAnalyticDataProviderTable = payload.analyticDataProviderSettings.RDBAnalyticDataProviderTable;
 
-                api.getData = function () {
-                    var data = {
-                        $type: "Vanrise.Analytic.Data.SQL.SQLAnalyticDataProvider, Vanrise.Analytic.Data.SQL"
-                    };
-                    return data;
-                };
+                    //        isEditMode = true;
+                    //        rdbTableSelectorSelectionChangedDeferred = UtilsService.createPromiseDeferred();
+                    //    }
 
-                if (ctrl.onReady != undefined && typeof (ctrl.onReady) == 'function') {
-                    ctrl.onReady(api);
+                    //    //Loading RDB Table selector
+                    //    var rdbTableSelectorLoadPromise = getRdbTableSelectorLoadPromise();
+                    //    promises.push(rdbTableSelectorLoadPromise);
+
+                    //    if (isEditMode) {
+                    //       // var analyticTableSelectorLoadPromise = getAnalyticTableSelectorLoadPromise();
+                    //        //promises.push(analyticTableSelectorLoadPromise);
+                    //    }
+
+                    //    function getRdbTableSelectorLoadPromise() {
+                    //        var rdbTableSelectorLoadDeferred = UtilsService.createPromiseDeferred();
+
+                    //        rdbTableReadyPromiseDeferred.promise.then(function () {
+
+                    //            var rdbTableSelectorPayload = {
+                    //                filter: {
+                    //                    Filters: [{
+                    //                        $type: "Vanrise.GenericData.RDBDataStorage.RDBDataRecordStorageFilter, Vanrise.GenericData.RDBDataStorage"
+                    //                    }]
+                    //                }
+                    //            };
+                    //            VRUIUtilsService.callDirectiveLoad(rdbTableSelectorAPI, rdbTableSelectorPayload, rdbTableSelectorLoadDeferred);
+                    //        });
+
+                    //        return rdbTableSelectorLoadDeferred.promise;
+                    //    }
+                    //};
+
+                    //api.getData = function () {
+                    //    var data = {
+                    //        $type: "Vanrise.Analytic.Data.RDB.RDBAnalyticDataProvider, Vanrise.Analytic.Data.RDB",
+                    //        ModuleName: $scope.scopeModel.moduleName,
+                    //        //Table: 
+                    //    };
+                    //    return data;
+                    //};
+
+                    //if (ctrl.onReady != undefined && typeof (ctrl.onReady) == 'function') {
+                    //    ctrl.onReady(api);
+                    //}
                 }
             }
         }
     }
-
-    app.directive('vrAnalyticRDBDataprovider', RDBDataProviderDirective);
+    app.directive('vrAnalyticRdbDataprovider', RDBDataProviderDirective);
 
 })(app);
