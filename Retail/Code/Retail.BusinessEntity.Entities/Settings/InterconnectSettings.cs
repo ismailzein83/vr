e265e96e98280d0253a6b1ef6faa8991bd8d635a
yara.bousalman <yara.bousalman@vanrise.tfs.local>
2019-02-06 07:23:33 +0000

    Changing file names
    git-tfs-id: [http://developmentvlan:8585/tfs/vanrise.collection]$/;C53147

---
+++ Retail/Code/Retail.BusinessEntity.Entities/Settings/InterconnectSettings.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Vanrise.Entities;
+
+namespace Retail.BusinessEntity.Entities
+{
+    public class InterconnectSettings : SettingData
+    {
+        public const string SETTING_TYPE = "Retail_BE_InterconnectSettings";
+        public string LocalOperatorName { get; set; }
+        private long _localOperatorId = -999999;
+        public long LocalOperatorID { get { return _localOperatorId; } set { _localOperatorId = value; } }
+    }
+}
---
+++ Retail/Code/Retail.BusinessEntity.Web/Retail_BusinessEntity/Directives/Settings/retail-be-interconnectsettings-editor.js
@@ -0,0 +1,63 @@
+﻿(function (app) {
+
+    'use strict';
+
+    InterconnectSettingsDirective.$inject = ['UtilsService', 'VRUIUtilsService'];
+
+    function InterconnectSettingsDirective(UtilsService, VRUIUtilsService) {
+        return {
+            restrict: "E",
+            scope: {
+                onReady: "="
+            },
+            controller: function ($scope, $element, $attrs) {
+                var ctrl = this;
+                var ctor = new InterconnectSettings($scope, ctrl, $attrs);
+                ctor.initializeController();
+            },
+            controllerAs: "ctrl",
+            bindToController: true,
+            templateUrl: "/Client/Modules/Retail_BusinessEntity/Directives/Settings/Templates/InterconnectSettingsEditorTemplate.html"
+        };
+
+
+        function InterconnectSettings($scope, ctrl, $attrs) {
+            this.initializeController = initializeController;
+
+            function initializeController() {
+                $scope.scopeModel = {};
+
+                defineAPI();
+            }
+            function defineAPI() {
+                var api = {};
+
+                api.load = function (payload) {
+
+                    var promises = [];
+                    if (payload != undefined && payload.data != undefined)
+                    {
+                        $scope.scopeModel.localOperatorName = payload.data.LocalOperatorName;
+                        $scope.scopeModel.localOperatorID = payload.data.LocalOperatorID;
+                    }
+
+                    return UtilsService.waitMultiplePromises(promises);
+                };
+
+                api.getData = function () {
+                    return {
+                        $type: "Retail.BusinessEntity.Entities.InterconnectSettings,Retail.BusinessEntity.Entities",
+                        LocalOperatorName: $scope.scopeModel.localOperatorName,
+                        LocalOperatorID : $scope.scopeModel.localOperatorID
+                    };
+                };
+
+                if (ctrl.onReady != null) {
+                    ctrl.onReady(api);
+                }
+            }
+        }
+    }
+
+    app.directive('retailBeInterconnectsettingsEditor', InterconnectSettingsDirective);
+})(app);
