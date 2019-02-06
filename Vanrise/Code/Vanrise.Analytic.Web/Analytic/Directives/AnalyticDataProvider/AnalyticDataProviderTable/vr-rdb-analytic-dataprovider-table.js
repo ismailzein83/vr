7da4056a27dcb495711c68a59faa227d798dc72a
Fatima Hammoud <VANRISE\fatima.hammoud>
2019-02-06 11:23:29 +0000

    adding vrRdbAnalyticDataproviderTable directive
    git-tfs-id: [http://developmentvlan:8585/tfs/vanrise.collection]$/;C53170

---
+++ Vanrise/Code/Vanrise.Analytic.Web/Analytic/Directives/AnalyticDataProvider/AnalyticDataProviderTable/vr-rdb-analytic-dataprovider-table.js
@@ -0,0 +1,47 @@
+﻿(function (app) {
+
+    'use strict';
+
+    RdbAnalyticDataproviderTableSelector.$inject = ['VR_Analytic_AnalyticConfigurationAPIService', 'UtilsService', 'VRUIUtilsService'];
+
+    function RdbAnalyticDataproviderTableSelector(VR_Analytic_AnalyticConfigurationAPIService, UtilsService, VRUIUtilsService) {
+        return {
+            restrict: "E",
+            scope: {
+                onReady: "=",
+                normalColNum: '@',
+                label: '@',
+                customvalidate: '='
+            },
+            controller: function ($scope, $element, $attrs) {
+                var ctrl = this;
+                var analyticDataProviderSettings = new AnalyticDataProviderSettings($scope, ctrl, $attrs);
+                analyticDataProviderSettings.initializeController();
+            },
+            controllerAs: "providerCtrl",
+            bindToController: true
+           // templateUrl: 
+        };
+
+        function AnalyticDataProviderSettings($scope, ctrl, $attrs) {
+            this.initializeController = initializeController;
+
+            function initializeController() {
+                $scope.scopeModel = {};
+                defineAPI();
+            }
+
+            function defineAPI() {
+                var api = {};
+
+                if (ctrl.onReady != null) {
+                    ctrl.onReady(api);
+                }
+            }
+        }
+    }
+    vrRdbAnalyticDataproviderTable
+
+    app.directive('vrRdbAnalyticDataproviderTable', RdbAnalyticDataproviderTableSelector);
+
+})(app);
