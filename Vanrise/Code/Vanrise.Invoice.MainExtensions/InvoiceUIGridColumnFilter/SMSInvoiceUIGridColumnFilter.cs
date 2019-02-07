8f41f8111a6d2ee678a08b1f1cc64c78cbfc48c0
yara.bousalman <yara.bousalman@vanrise.tfs.local>
2019-02-07 08:10:59 +0000

    Adding filter implementations in invoice type
    git-tfs-id: [http://developmentvlan:8585/tfs/vanrise.collection]$/;C53208

---
+++ Vanrise/Code/Vanrise.Invoice.MainExtensions/InvoiceUIGridColumnFilter/SMSInvoiceUIGridColumnFilter.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Vanrise.Invoice.Entities;
+
+namespace Vanrise.Invoice.MainExtensions
+{
+    public class SMSInvoiceUIGridColumnFilter : InvoiceUIGridColumnFilter
+    {
+        public override Guid ConfigId
+        {
+            get { return new Guid("15ceed9b-0c8f-4446-8bbe-92e84857ae31"); }
+        }
+
+        public override bool IsFilterMatch(IInvoiceUIGridColumnFilterContext context)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
