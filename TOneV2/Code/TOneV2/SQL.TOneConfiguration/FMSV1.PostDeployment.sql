6dd67e5f59915f8c3b21746b00dc72fb2bff85a1
Wissam Ajamy <VANRISE\wissam.ajamy>
2019-02-07 16:14:31 +0000

    Adding 'System Logs' view and add script to remove useless views
    git-tfs-id: [http://developmentvlan:8585/tfs/vanrise.collection]$/;C53237

--- TOneV2/Code/TOneV2/SQL.TOneConfiguration/FMSV1.PostDeployment.sql
+++ TOneV2/Code/TOneV2/SQL.TOneConfiguration/FMSV1.PostDeployment.sql
@@ -10,6 +10,24 @@ Post-Deployment Script Template
 --------------------------------------------------------------------------------------
 */
 
+--delete useless views from FMSV1 product such 'My Scheduler Service', 'Style Definitions', 'Organizational Charts', Lookups, etc...
+delete from [sec].[View] where [Id] in ('C65ED28A-36D0-4047-BEC5-030D35B02308',--'My Scheduler Service'
+										'66DE2441-8A96-41E7-94EA-9F8AF38A3515',--'Style Definitions'
+										'DCF8CA21-852C-41B9-9101-6990E545509D',--'Organizational Charts'
+										'52C580DE-C91F-45E2-8E3A-46E0BA9E7EFD',--'Component Types'
+										'8AC4B99E-01A0-41D1-AE54-09E679309086',--'Status Definitions'
+										'604B2CB5-B839-4E51-8D13-3C1C84D05DEE',--'Countries'
+										'A1CE55FE-6CF4-4F15-9BC2-8E1F8DF68561',--'Regions'
+										'25994374-CB99-475B-8047-3CDB7474A083',--'Cities'
+										'9F691B87-4936-4C4C-A757-4B3E12F7E1D9', --'Currencies'
+										'E5CA33D9-18AC-4BA1-8E8E-FB476ECAA9A9', --'Exchange Rates'
+										'0F111ADC-B7F6-46A4-81BC-72FFDEB305EB', --'Time Zone'
+										'2CF7E0BE-1396-4305-AA27-11070ACFC18F',--'Application Visibilities'
+										'4D7BF410-E4C6-4D6F-B519-D6B5C2C2F712',--'Rate Types'
+										'2D39B12D-8FBF-4D4E-B2A5-5E3FE57580DF'--,'Locked Sessions'
+										)
+GO
+
 --[common].[Setting]---------------------------201 to 300-------------------------------------------
 BEGIN
 set nocount on;
@@ -30,3 +48,22 @@ when not matched by target then
 	values(s.[ID],s.[Name],s.[Type],s.[Category],s.[Settings],s.[Data],s.[IsTechnical]);
 ----------------------------------------------------------------------------------------------------
 END
+
+--[sec].[View]--------------------------------------------------------------------------------------
+begin
+set nocount on;
+;with cte_data([ID],[Name],[Title],[Url],[Module],[ActionNames],[Audience],[Content],[Settings],[Type],[Rank])
+as (select * from (values
+--//////////////////////////////////////////////////////////////////////////////////////////////////
+('7079BD63-BFE2-4519-9B1B-8158A2F3A12A','System Logs','System Logs',null,'BAAF681E-AB1C-4A64-9A35-3F3951398881',null,null,null,'{"$type":"Vanrise.Common.Business.MasterLogViewSettings, Vanrise.Common.Business","Items":[{"PermissionName":"VRCommon_System_Log: View General Logs","Directive":"vr-log-entry-search","Title":"General"},{"PermissionName":"VRCommon_System_Log: View Action Audit","Directive":"vr-common-actionaudit-search","Title":"Action Audit"}]}','372ED3CB-4B7B-4464-9ABF-59CD7B08BD23',15)
+--\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\
+)c([ID],[Name],[Title],[Url],[Module],[ActionNames],[Audience],[Content],[Settings],[Type],[Rank]))
+merge	[sec].[View] as t
+using	cte_data as s
+on		1=1 and t.[ID] = s.[ID]
+when matched then
+	update set
+	[Name] = s.[Name],[Title] = s.[Title],[Url] = s.[Url],[Module] = s.[Module],[ActionNames] = s.[ActionNames],[Settings] = s.[Settings],[Type] = s.[Type],[Rank] = s.[Rank]
+when not matched by target then
+	insert([ID],[Name],[Title],[Url],[Module],[ActionNames],[Audience],[Content],[Settings],[Type],[Rank])
+	values(s.[ID],s.[Name],s.[Title],s.[Url],s.[Module],s.[ActionNames],s.[Audience],s.[Content],s.[Settings],s.[Type],s.[Rank]);
