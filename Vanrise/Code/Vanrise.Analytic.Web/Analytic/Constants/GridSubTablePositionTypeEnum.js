4a1ced215eaa5e7b77e12e71b9c714515e3bd946
Naim Saad <VANRISE\naim.saad>
2019-02-07 14:02:02 +0000

    Implementing GridSubTablePositionType Enum and directive
    git-tfs-id: [http://developmentvlan:8585/tfs/vanrise.collection]$/;C53231

---
+++ Vanrise/Code/Vanrise.Analytic.Web/Analytic/Constants/GridSubTablePositionTypeEnum.js
@@ -0,0 +1,6 @@
+﻿//app.constant('VR_Analytic_GridSubTablePositionTypeEnum', {
+//    BeforeAllMeasures: { value: 1, description: 'Before All Measures', showMeasure: false },
+//    AfterAllMeasures: { value: 2, description: 'After All Measures', showMeasure: false },
+//    BeforeSpecificMeasures: { value: 3, description: 'Before Specific Measures', showMeasure: true },
+//    AfterSpecificMeasures: { value: 0, description: 'After Specific Measures', showMeasure: true }
+//});
