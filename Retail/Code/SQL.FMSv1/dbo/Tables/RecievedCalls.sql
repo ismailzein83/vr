a30ccdff552da0a1670b6a2c2453f84bd456f33c
Ismail Zein <VANRISE\ismail.zein>
2019-02-11 13:24:09 +0000

    added FMSv1 SQL project
    git-tfs-id: [http://developmentvlan:8585/tfs/vanrise.collection]$/;C53294

---
+++ Retail/Code/SQL.FMSv1/dbo/Tables/RecievedCalls.sql
@@ -0,0 +1,18 @@
+﻿CREATE TABLE [dbo].[RecievedCalls] (
+    [ID]                 INT           IDENTITY (1, 1) NOT NULL,
+    [SourceID]           INT           NOT NULL,
+    [MobileOperatorID]   INT           NULL,
+    [DurationInSeconds]  INT           NOT NULL,
+    [a_number]           VARCHAR (100) NULL,
+    [b_number]           VARCHAR (100) NOT NULL,
+    [CLI]                VARCHAR (100) NULL,
+    [AttemptDateTime]    DATETIME2 (0) NOT NULL,
+    [ImportID]           INT           NOT NULL,
+    [Carrier]            VARCHAR (100) NULL,
+    [ClientID]           INT           NULL,
+    [GeneratedCallID]    INT           NULL,
+    [OriginationNetwork] VARCHAR (100) NULL,
+    [Reference]          VARCHAR (50)  NULL,
+    [Type]               VARCHAR (5)   NULL
+);
+
