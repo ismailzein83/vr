78834e1debcf9d4c587700990dcf4f922c76e67d
Ali Sakr <VANRISE\ali.sakr>
2019-02-11 13:08:54 +0000

    adding new extension configuration for tab tiles
    git-tfs-id: [http://developmentvlan:8585/tfs/vanrise.collection]$/;C53293

---
+++ Vanrise/Code/Vanrise.Common.MainExtensions/VRTile/TabsTiles.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Vanrise.Entities;
+
+
+namespace Vanrise.Common.MainExtensions.VRTile
+{
+    public class TabsTiles : VRTileExtendedSettings
+    {
+        public override Guid ConfigId { get { return new Guid("A3F635B1-2C76-4419-A636-0A6625048DF9"); } }
+
+        public override string RuntimeEditor { get { return "vr-common-tabtiles-runtime"; } }
+
+        public List<TabTile> TabTiles { get; set; } 
+    }
+}
