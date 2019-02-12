99d21cacdfd78944630ef4d693c5ff33a887394e
Zeinab Mokdad <VANRISE\zeinab.mokdad>
2019-02-12 16:34:41 +0000

    adding DisputeSettingsData
    git-tfs-id: [http://developmentvlan:8585/tfs/vanrise.collection]$/;C53325

---
+++ Retail/Code/Retail.RA.Entities/DisputeSetting/DisputeSettingsData.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using Vanrise.Entities;
+
+namespace Retail.RA.Entities
+{
+    public class DisputeSettingsData : SettingData
+    {
+        public string SerialNumberPattern { get; set; }
+        public long InitialSequence { get; set; }
+    }
+}
