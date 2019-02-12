1eee97ae10d9564d14c6c4ce7419fdd3bf24439a
Zeinab Mokdad <VANRISE\zeinab.mokdad>
2019-02-12 08:22:19 +0000

    adding Dipute - extension configuration
    git-tfs-id: [http://developmentvlan:8585/tfs/vanrise.collection]$/;C53305

---
+++ Retail/Code/Retail.RA.Business/Dispute/DisputeSettings.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using Vanrise.GenericData.Business;
+
+namespace Retail.RA.Business
+{
+    public class DisputeSettings : GenericBEExtendedSettings
+    {
+        public override Guid ConfigId => new Guid("7D1E3160-A230-4A67-BD35-9221D03A0270");
+
+        public override object GetInfoByType(IGenericBEExtendedSettingsContext context)
+        {
+            if (context.InfoType == null)
+                return null;
+
+            switch (context.InfoType)
+            {
+                case "SerialNumberPattern": return null;
+
+                default: return null;
+            }
+        }
+    }
+}
---
+++ Retail/Code/Retail.RA.Web/Retail_RA/Directives/DisputeManagement/retail-ra-dispute-settings.js
@@ -0,0 +1,53 @@
+﻿(function (app) {
+
+    'use strict';
+
+    retailRaDisputeSettings.$inject = ["UtilsService", 'VRUIUtilsService'];
+
+    function retailRaDisputeSettings(UtilsService, VRUIUtilsService) {
+        return {
+            restrict: "E",
+            scope: {
+                onReady: "=",
+            },
+            controller: function ($scope, $element, $attrs) {
+                var ctrl = this;
+                var ctor = new SettingsCtor($scope, ctrl, $attrs);
+                ctor.initializeController();
+            },
+            controllerAs: "Ctrl",
+            bindToController: true,
+            templateUrl: "/Client/Modules/Retail_RA/Directives/DisputeManagement/Templates/DisputeSettingsTemplate.html"
+
+        };
+        function SettingsCtor($scope, ctrl, $attrs) {
+            this.initializeController = initializeController;
+
+            function initializeController() {
+                $scope.scopeModel = {};
+                defineAPI();
+            }
+            function defineAPI() {
+                var api = {};
+
+                api.load = function (payload) {
+
+                };
+
+                api.getData = function () {
+                    var data = {
+                        $type: "Retail.RA.Business.DisputeSettings, Retail.RA.Business"
+                    };
+                    return data;
+                };
+
+                if (ctrl.onReady != undefined && typeof (ctrl.onReady) == 'function') {
+                    ctrl.onReady(api);
+                }
+            }
+        }
+    }
+
+    app.directive('retailRaDisputeSettings', retailRaDisputeSettings);
+
+})(app);
