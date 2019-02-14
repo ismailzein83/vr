40e903d1252649dfd4528377e2f8a928a4a19db4
Ali Atoui <VANRISE\ali.atoui>
2019-02-14 10:38:45 +0000

    Adding DiagnosticsParser @HuaweiFieldParsers
    git-tfs-id: [http://developmentvlan:8585/tfs/vanrise.collection]$/;C53382

---
+++ Vanrise/Code/Vanrise.DataParser.MainExtensions/BinaryParsers/HuaweiParser/FieldParsers/DiagnosticsParser.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using Vanrise.DataParser.Business;
+using Vanrise.DataParser.Entities;
+
+namespace Vanrise.DataParser.MainExtensions.BinaryParsers.HuaweiParser.FieldParsers
+{
+    public class DiagnosticsParser : BinaryFieldParserSettings
+    {
+        public override Guid ConfigId { get { return new Guid("759CFA83-240C-4184-BD13-3F72090D3111"); } }
+        public string FieldName { get; set; }
+
+        public override void Execute(IBinaryFieldParserContext context)
+        {
+            //As Per Discussion with Ali Taher Diagnostic length is always 2 bytes
+            //First byte is ignored and second one is used taking the less significant seven bits and then converting it to hexadecimal.
+
+            int byteAsInt = context.FieldValue[1] & 0x7F;
+            string byteArrayAsString = ParserHelper.GetHexFromInt(byteAsInt);
+
+            context.Record.SetFieldValue(this.FieldName, byteArrayAsString);
+        }
+    }
+}
