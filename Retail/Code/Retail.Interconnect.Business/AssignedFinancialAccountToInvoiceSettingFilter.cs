8f10407dac3d0f5e6f6889cc3e59d839082f706b
yara.bousalman <yara.bousalman@vanrise.tfs.local>
2019-02-15 09:47:46 +0000

    Fixing partner selector in invoice settings
    git-tfs-id: [http://developmentvlan:8585/tfs/vanrise.collection]$/;C53415

--- Retail/Code/Retail.Interconnect.Business/AssignedFinancialAccountToInvoiceSettingFilter.cs
+++ Retail/Code/Retail.Interconnect.Business/AssignedFinancialAccountToInvoiceSettingFilter.cs
@@ -6,6 +6,8 @@ using System.Text;
 using System.Threading.Tasks;
 using Vanrise.Invoice.Business.InvoicePartnerFilter;
 using Retail.BusinessEntity.Business;
+using Vanrise.Invoice.Business;
+using Vanrise.Common;
 
 namespace Retail.Interconnect.Business
 {
@@ -13,8 +15,16 @@ namespace Retail.Interconnect.Business
     {
         public bool IsExcluded(IAccountFilterContext context)
         {
+            InvoiceTypeManager invoiceTypeManager = new InvoiceTypeManager();
+            var invoiceTypeExtendedSettings = invoiceTypeManager.GetInvoiceTypeExtendedSettings(base.InvoiceTypeId);
+            var interconnectInvoiceSettings = invoiceTypeExtendedSettings.CastWithValidate<InterconnectInvoiceSettings>("invoiceTypeExtendedSettings", base.InvoiceTypeId);
+            string classification;
+            if (interconnectInvoiceSettings.Type == InterconnectInvoiceType.Customer)
+                classification = "Customer";
+            else
+                classification = "Supplier";
             FinancialAccountManager manager = new FinancialAccountManager();
-            var financialAccounts = manager.GetFinancialAccounts(context.AccountBEDefinitionId, context.Account.AccountId, false);
+            var financialAccounts = manager.GetFinancialAccounts(context.AccountBEDefinitionId, context.Account.AccountId, false, classification);
             if (financialAccounts != null && financialAccounts.All(x => !base.IsMatched(x.FinancialAccountId)))
                 return true;
             return false;
@@ -22,6 +32,11 @@ namespace Retail.Interconnect.Business
 
         public bool IsMatched(IFinancialAccountFilterContext context)
         {
+            var financialAccountMananger = new FinancialAccountManager();
+            var financialAccountData = financialAccountMananger.GetFinancialAccountData(context.AccountBEDefinitionId, context.FinancialAccountId);
+            financialAccountData.ThrowIfNull("financialAccountData", string.Format("AccountBEDefinitionId:{0}, FinancialAccountId:{1}", context.AccountBEDefinitionId, context.FinancialAccountId));
+            if (financialAccountData.InvoiceTypeIds == null || !financialAccountData.InvoiceTypeIds.Contains(base.InvoiceTypeId))
+                return false;   
             if (this.EditablePartnerId != null && context.FinancialAccountId == this.EditablePartnerId)
                 return true;
             if (!base.IsMatched(context.FinancialAccountId))
