e84464b9f1974e6c62a61c2b12753341b4841e49
Ali Sakr <VANRISE\ali.sakr>
2019-02-15 08:40:59 +0000

    adding Range class
    git-tfs-id: [http://developmentvlan:8585/tfs/vanrise.collection]$/;C53405

---
+++ Vanrise/Code/Vanrise.Entities/Range/Range.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Vanrise.Entities
+{
+    public class Range
+    {
+        public decimal From { get; set; }
+        public decimal To { get; set; }
+        public string Name { get; set; }
+        public Guid StyleDefinitionId { get; set; }
+    }
+}
