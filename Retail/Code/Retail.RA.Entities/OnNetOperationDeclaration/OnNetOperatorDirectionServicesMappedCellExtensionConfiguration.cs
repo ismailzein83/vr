2259505fffe20dcf8dd7561c112cd6691675b313
Fatima Hammoud <VANRISE\fatima.hammoud>
2019-02-19 08:04:15 +0000

    Add class OnNetOperatorDirectionServicesMappedCellExtensionConfiguration
    git-tfs-id: [http://developmentvlan:8585/tfs/vanrise.collection]$/;C53494

---
+++ Retail/Code/Retail.RA.Entities/OnNetOperationDeclaration/OnNetOperatorDirectionServicesMappedCellExtensionConfiguration.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Vanrise.Entities;
+
+namespace Retail.RA.Entities.OnNetOperationDeclaration
+{
+    public class OnNetOperatorDirectionServicesMappedCellExtensionConfiguration : ExtensionConfiguration
+    {
+        public const string EXTENSION_TYPE = "Retail_BE_OnNetOperatorDeclarationServiceConfig";
+
+        public string Editor { get; set; }
+    }
+}
