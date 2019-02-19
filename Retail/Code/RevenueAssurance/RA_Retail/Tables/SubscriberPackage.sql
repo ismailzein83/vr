3fc2475f2d096c7692fa71f398cfaf13fa3aa82d
Wissam Ajamy <VANRISE\wissam.ajamy>
2019-02-19 16:27:00 +0000

    Adding RA_OnNet structure from Retail_Dev_RA
    git-tfs-id: [http://developmentvlan:8585/tfs/vanrise.collection]$/;C53558

---
+++ Retail/Code/RevenueAssurance/RA_Retail/Tables/SubscriberPackage.sql
@@ -0,0 +1,13 @@
+﻿CREATE TABLE [RA_Retail].[SubscriberPackage] (
+    [ID]               BIGINT   IDENTITY (1, 1) NOT NULL,
+    [SubscriberId]     BIGINT   NULL,
+    [PackageId]        INT      NULL,
+    [BED]              DATETIME NULL,
+    [CreatedTime]      DATETIME NULL,
+    [EED]              DATETIME NULL,
+    [CreatedBy]        INT      NULL,
+    [LastModifiedTime] DATETIME NULL,
+    [LastModifiedBy]   INT      NULL,
+    PRIMARY KEY CLUSTERED ([ID] ASC)
+);
+
