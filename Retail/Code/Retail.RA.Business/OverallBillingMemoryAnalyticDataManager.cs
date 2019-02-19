a39c6118194ed382c298fa416643d723ea34524d
Rodi Hassan <VANRISE\rodi.hassan>
2019-02-19 10:43:03 +0000

    fix bug in GetRawMemoryRecord for ICXOverallBilling table
    git-tfs-id: [http://developmentvlan:8585/tfs/vanrise.collection]$/;C53510

--- Retail/Code/Retail.RA.Business/OverallBillingMemoryAnalyticDataManager.cs
+++ Retail/Code/Retail.RA.Business/OverallBillingMemoryAnalyticDataManager.cs
@@ -602,7 +602,7 @@ namespace Retail.RA.Business
             rawMemoryRecord.FieldValues.Add("TrafficDirection", overallBillingRecord.TrafficDirection);
             rawMemoryRecord.FieldValues.Add("Income", overallBillingRecord.Income);
 
-            rawMemoryRecord.FieldValues.Add("CalculatedNumberOfSMS", overallBillingRecord.CalculatedDurationInMin);
+            rawMemoryRecord.FieldValues.Add("CalculatedDurationInMin", overallBillingRecord.CalculatedDurationInMin);
             rawMemoryRecord.FieldValues.Add("CalculatedNumberOfCDR", overallBillingRecord.CalculatedNumberOfCDR);
             rawMemoryRecord.FieldValues.Add("CalculatedNumberOfSMS", overallBillingRecord.CalculatedNumberOfSMS);
             rawMemoryRecord.FieldValues.Add("CalculatedRevenue", overallBillingRecord.CalculatedRevenue);
@@ -611,7 +611,7 @@ namespace Retail.RA.Business
             rawMemoryRecord.FieldValues.Add("DeclaredNumberOfCDR", overallBillingRecord.DeclaredNumberOfCDR);
             rawMemoryRecord.FieldValues.Add("DeclaredNumberOfSMS", overallBillingRecord.DeclaredNumberOfSMS);
             rawMemoryRecord.FieldValues.Add("DeclaredRevenue", overallBillingRecord.DeclaredRevenue);
-
+                       
             return rawMemoryRecord;
         }
 
