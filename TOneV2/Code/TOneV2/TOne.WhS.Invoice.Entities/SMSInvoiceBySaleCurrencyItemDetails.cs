30556392fa894e1f68daf011910772816bb246a2
Hassan Makhour <VANRISE\hassan.makhour>
2019-02-19 14:57:57 +0000

    SMSInvoice Entities
    git-tfs-id: [http://developmentvlan:8585/tfs/vanrise.collection]$/;C53549

---
+++ TOneV2/Code/TOneV2/TOne.WhS.Invoice.Entities/SMSInvoiceBySaleCurrencyItemDetails.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TOne.WhS.Invoice.Entities
+{
+	public class SMSInvoiceBySaleCurrencyItemDetails
+	{
+		public DateTime FromDate { get; set; }
+		public DateTime ToDate { get; set; }
+		public decimal AmountAfterCommission { get; set; }
+		public decimal AmountAfterCommissionWithTaxes { get; set; }
+		public decimal TotalRecurringChargeAmount { get; set; }
+		public int NumberOfSMS { get; set; }
+		public int CurrencyId { get; set; }
+		public decimal Amount { get; set; }
+		public string Month { get; set; }
+	}
+}
