608c73e859f82046c2e68739639adb314ff53705
Ali Ballouk <VANRISE\ali.ballouk>
2019-02-20 13:41:29 +0000

    update figure layout + Adding item custom css class
    git-tfs-id: [http://developmentvlan:8585/tfs/vanrise.collection]$/;C53631

--- Vanrise/Code/Vanrise.Web/Client/Javascripts/Directives/Containers/vr-figure-directive.js
+++ Vanrise/Code/Vanrise.Web/Client/Javascripts/Directives/Containers/vr-figure-directive.js
@@ -14,6 +14,7 @@
                 imgpath: '@',
                 datasource: '=',
                 header: "@",
+                maxitemsperrow: '@'
             },
             controller: function ($scope, $element, $attrs) {
                 var ctrl = this;
@@ -22,6 +23,38 @@
                 ctrl.onClickEvent = function () {
                     if ($attrs.target)
                         $element.find("#link").click();
+                };
+
+                var maxItemsPerRow = ctrl.maxitemsperrow != undefined ? parseInt(ctrl.maxitemsperrow) : 1;
+                var colNumber,mdColnumber;
+                switch (maxItemsPerRow) {
+                    case 1: colNumber = 12;mdColnumber = 12; break;
+                    case 2: colNumber = 6;mdColnumber = 12; break;
+                    case 3: colNumber = 4;mdColnumber = 6; break;
+                    case 4: colNumber = 3;mdColnumber = 4; break;
+                    case 5: colNumber = 3; mdColnumber =4; break;
+                    case 6: colNumber = 2; mdColnumber = 3; break;
+                }
+                if (colNumber == undefined) {
+                    if (maxItemsPerRow < 12)
+                        colNumber = 2;
+                    else colNumber = 1;
+                }
+                ctrl.colNumber = colNumber;
+                ctrl.mdColNumber = mdColnumber;
+
+                ctrl.getObjectClass = function (item) {
+                    if (typeof item["className"]) {
+                        var color = item["className"];
+                        if (typeof color === 'string' || typeof color instanceof String) {
+                            return color+ "-figure";
+                        }
+                        else if (typeof color === 'object' || color instanceof Object) {
+                            switch (color.UniqueName) {
+                                case "VR_AccountBalance_StyleFormating_CSSClass": return color.ClassName + "-figure";
+                            }
+                        }
+                    }
                 };
 
             },
@@ -35,12 +68,14 @@
                                           + '<img ng-src="{{vrFigure.imgpath}}" style="padding-top: 5px;"  />'
                                         + '</div>'
                                          + '<div class="figure-data-content">'
-                                         + '<div style="width:100%;min-height: 141px;" >'
-                                              + '<div style="width:100% ; padding-top: 15px;"  ng-repeat="i in vrFigure.datasource">'
-                                                   + ' <div class="figure-item" class="tilevalue">'
-                                                    + '  <div class="item-value" ng-class="!i.name ? \'centered\':\'\'">{{i.value}}</div><div ng-if="i.name" class="item-label" title="{{i.name}}"><div>{{i.name}}</div></div>'
-                                                    + ' </div>'
-                                              + '</div>'
+                                         + '<div style="width:100%;min-height: 141px;position: relative;" >'
+                                             + '<div style="position: absolute;top: 50%;transform: translate(0px,-50%);text-align: center;width: 100%;">'
+                                                  + '<div class="col-lg-{{vrFigure.colNumber}} col-md-{{vrFigure.mdColNumber}}"  ng-repeat="i in vrFigure.datasource">'
+                                                       + ' <div class="figure-item" class="tilevalue ">'
+                                                        + '  <div class="item-value {{!i.name ? \'centered\':\'\'}}" ng-class="vrFigure.getObjectClass(i)">{{i.value}}</div><div ng-if="i.name" class="item-label" title="{{i.name}}">{{i.name}}</div>'
+                                                        + '</div>'
+                                                  + '</div>'
+                                             + '</div>'
                                           + '</div>'
                                      + '</div>'
                                 + '</div>';
