e7a63c228914966717d7401a59f4618cb88c4479
Naim Saad <VANRISE\naim.saad>
2019-02-21 09:14:03 +0000

    Implementing C4SwitchInterconnectionOnBeforeSaveHandler
    git-tfs-id: [http://developmentvlan:8585/tfs/vanrise.collection]$/;C53683

---
+++ Retail/Code/RecordAnalysis.Business/C4SwitchInterconnectionOnBeforeSaveHandler.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Vanrise.GenericData.Business;
+
+namespace RecordAnalysis.Business
+{
+    public class C4SwitchInterconnectionOnBeforeSaveHandler : GenericBEOnBeforeInsertHandler
+    {
+        public override Guid ConfigId { get { return new Guid("40FF1961-EC19-44AB-A738-0F32E54F5164"); } }
+
+        public override void Execute(IGenericBEOnBeforeInsertHandlerContext context)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
