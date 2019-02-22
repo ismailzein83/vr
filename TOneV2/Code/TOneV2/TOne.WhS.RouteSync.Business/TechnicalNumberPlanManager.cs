be9d6f41ff73d9500f0c9bbd7dad97d191c5b33b
Ali Slayman <VANRISE\ali.slayman>
2019-02-22 06:39:01 +0000

    Adding TechnicalNumberPlanManager
    git-tfs-id: [http://developmentvlan:8585/tfs/vanrise.collection]$/;C53737

---
+++ TOneV2/Code/TOneV2/TOne.WhS.RouteSync.Business/TechnicalNumberPlanManager.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TOne.WhS.RouteSync.Business
+{
+    public class TechnicalNumberPlanManager
+    {
+        static readonly Guid BeDefinitionId = new Guid("B78CBB67-5CF6-4997-8080-8E6DDA848338");
+    }
+}
