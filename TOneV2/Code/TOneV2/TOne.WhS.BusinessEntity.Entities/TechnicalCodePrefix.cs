455b4eb0afe7d5f32abad06b5f525bdd2259a8e5
Ali Slayman <VANRISE\ali.slayman>
2019-02-26 10:02:01 +0000

    Adding TechnicalCodePrefix Class and remove old classes
    git-tfs-id: [http://developmentvlan:8585/tfs/vanrise.collection]$/;C53819

---
+++ TOneV2/Code/TOneV2/TOne.WhS.BusinessEntity.Entities/TechnicalCodePrefix.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections.Generic;
+using Vanrise.Common;
+
+namespace TOne.WhS.BusinessEntity.Entities
+{
+    public class TechnicalCodePrefix : ICode
+    {
+        public int ID { get; set; }
+
+        public string Code { get; set; }
+
+        public int ZoneID { get; set; }
+    }
+}
