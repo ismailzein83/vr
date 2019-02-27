9f2ba20cb237d44b78992a5d0afb1e932154597d
Mortada Issa <VANRISE\mortada.issa>
2019-02-27 09:56:41 +0000

    updating arguments
    git-tfs-id: [http://developmentvlan:8585/tfs/vanrise.collection]$/;C53841

--- SOM/Code/BPMExtended.Main.SOM.Entities/Billing/WorkflowInput/CreateCustomerInput.cs
+++ SOM/Code/BPMExtended.Main.SOM.Entities/Billing/WorkflowInput/CreateCustomerInput.cs
@@ -13,133 +13,28 @@ namespace BPMExtended.Main.SOMAPI
 
     public class CreateCustomerInputArgument
     {
-        public Guid? ContactId { get; set; }
+        public BillingCommonInputArgument CommonArgument { get; set; }
 
-        public Guid? AccountId { get; set; }
+        public int CustomerCategoryId { get; set; }
 
-        public CreateCustomerContactInfo ContactInfo { get; set; }
+        public int CountryId { get; set; }
 
-        public CreateCustomerAccountInfo AccountInfo { get; set; }
+        public string CityName { get; set; }
 
-        public class CreateCustomerContactInfo
+        public CreateCustomerContactInput ContactInput { get; set; }
+
+        public CreateCustomerAccountInput AccountInput { get; set; }
+
+        public class CreateCustomerContactInput
         {
-            public int CustomerCategoryId { get; set; }
-
             public string FirstName { get; set; }
 
             public string LastName { get; set; }
-
-            public int CountryId { get; set; }
-
-            public string CityName { get; set; }
         }
 
-        public class CreateCustomerAccountInfo
+        public class CreateCustomerAccountInput
         {
-            public int CustomerCategoryId { get; set; }
 
-            public int CountryId { get; set; }
-
-            public string CityName { get; set; }
         }
-
-        //public string Test()
-        //{
-        //    var customerNewElement = new CustomerCreateRequest.CustomerCreateRequestCustomerElement();
-        //    customerNewElement.prgCode = ContactInfo.CustomerCategoryId;
-        //    customerNewElement.paymentResp = true;
-
-        //    var addressWriteElement = new CustomerCreateRequest.CustomerCreateRequestAddressElement();
-        //    addressWriteElement.adrSeq = 0;
-        //    addressWriteElement.adrFname = ContactInfo.FirstName;
-        //    addressWriteElement.adrLname = ContactInfo.LastName;
-        //    addressWriteElement.countryId = ContactInfo.CountryId;
-        //    addressWriteElement.adrCity = ContactInfo.CityName;
-
-        //    CustomerCreateRequest request = new CustomerCreateRequest();
-        //    request.inputAttributes = new CustomerCreateRequest.CustomerCreateInputAttributes();
-        //    request.inputAttributes.customerNew = customerNewElement;
-        //    request.inputAttributes.addresses = new List<CustomerCreateRequest.CustomerCreateRequestAddressElement>();
-        //    request.inputAttributes.addresses.Add(addressWriteElement);
-
-        //    return "serialized request";
-        //}
     }
-
-    #region To Be Removed
-
-    //public class CustomerCreateRequest : BaseRequest
-    //{
-    //    public CustomerCreateInputAttributes inputAttributes { get; set; }
-
-    //    public class CustomerCreateInputAttributes
-    //    {
-    //        public CustomerCreateRequestCustomerElement customerNew { get; set; }
-
-    //        public List<CustomerCreateRequestAddressElement> addresses { get; set; }
-    //    }
-
-    //    public class CustomerCreateRequestCustomerElement
-    //    {
-    //        public bool paymentResp { get; set; }
-
-    //        public int prgCode { get; set; }
-    //    }
-
-    //    public class CustomerCreateRequestAddressElement
-    //    {
-    //        public long adrSeq { get; set; }
-
-    //        public string adrFname { get; set; }
-
-    //        public string adrLname { get; set; }
-
-    //        public int countryId { get; set; }
-
-    //        public string adrCity { get; set; }
-
-    //    }
-    //}
-
-    //public class laMemberCreateRequest : BaseRequest
-    //{
-
-    //}
-
-    //public class BaseRequest
-    //{
-    //    public Dictionary<string,string> sessionChangeRequest { get; set; }
-
-    //    public BaseRequest()
-    //    {
-    //        this.sessionChangeRequest = new Dictionary<string, string>();
-    //        this.sessionChangeRequest.Add("BU_ID", "2");
-    //    }
-    //}
-
-    //public class CustomerCreateResponse : BaseResponse
-    //{
-    //    public CustomerCreateResponseCustomerElement customerNew { get; set; }
-
-    //    public List<CustomerCreateResponseAddressElement> addresses { get; set; }
-
-    //    public class CustomerCreateResponseCustomerElement
-    //    {
-    //        public string csId { get; set; }
-
-    //        public string csIdPub { get; set; }
-    //    }
-
-    //    public class CustomerCreateResponseAddressElement
-    //    {
-    //        public long adrSeq { get; set; }
-    //    }
-    //}
-
-    //public class BaseResponse
-    //{
-
-    //}
-
-    #endregion
 }
