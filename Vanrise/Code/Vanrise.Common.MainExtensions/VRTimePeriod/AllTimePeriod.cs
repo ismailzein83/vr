fa0a5460d7d76b341438c4ef9feea27b4d61e09e
Zeinab Mokdad <VANRISE\zeinab.mokdad>
2019-02-27 12:38:48 +0000

    adding AllPeriod
    git-tfs-id: [http://developmentvlan:8585/tfs/vanrise.collection]$/;C53844

---
+++ Vanrise/Code/Vanrise.Common.MainExtensions/VRTimePeriod/AllTimePeriod.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using Vanrise.Entities;
+
+namespace Vanrise.Common.MainExtensions
+{
+    public class AllTimePeriod : VRTimePeriod
+    {
+        public override Guid ConfigId => new Guid("E3A08709-67B9-4FCB-A54C-5AD11035A9FF");
+
+        public override void GetTimePeriod(IVRTimePeriodContext context)
+        {
+            context.FromTime = new DateTime(2000, 1, 1);
+            context.ToTime = DateTime.Now;
+        }
+    }
+}
---
+++ Vanrise/Code/Vanrise.Common.Web/Common/Directives/VRTimePeriod/MainExtensions/vr-common-timeperiod-allperiod.js
@@ -0,0 +1,62 @@
+﻿"use strict";
+
+app.directive("vrCommonTimeperiodAllperiod", ['UtilsService', function (UtilsService) {
+
+    var directiveDefinitionObject = {
+        restrict: "E",
+        scope: {
+            onReady: "="
+        },
+        controller: function ($scope, $element, $attrs) {
+            var ctrl = this;
+
+            var ctor = new DirectiveConstructor($scope, ctrl);
+            ctor.initializeController();
+        },
+        controllerAs: "ctrl",
+        bindToController: true,
+        compile: function (element, attrs) {
+            return {
+                pre: function ($scope, iElem, iAttrs, ctrl) {
+
+                }
+            };
+        },
+        templateUrl: function (element, attrs) {
+            return getDirectiveTemplateUrl();
+        }
+    };
+
+    function getDirectiveTemplateUrl() {
+        return "/Client/Modules/Common/Directives/VRTimePeriod/MainExtensions/Templates/AllPeriodDirectiveTemplate.html";
+    }
+
+    function DirectiveConstructor($scope, ctrl) {
+        this.initializeController = initializeController;
+
+
+        function initializeController() {
+            defineAPI();
+        }
+
+        function defineAPI() {
+            var api = {};
+
+            api.getData = function () {
+                return {
+                    $type: "Vanrise.Common.MainExtensions.AllTimePeriod, Vanrise.Common.MainExtensions"
+                };
+            };
+
+            api.load = function (payload) {
+                var promises = [];
+                return UtilsService.waitMultiplePromises(promises);
+            };
+
+            if (ctrl.onReady != undefined)
+                ctrl.onReady(api);
+        }
+    }
+
+    return directiveDefinitionObject;
+}]);
