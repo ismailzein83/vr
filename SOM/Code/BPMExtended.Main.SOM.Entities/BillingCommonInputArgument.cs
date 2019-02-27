db0e9bfdca8c976bf6371c10cbbb49efd41bdfd5
Mortada Issa <VANRISE\mortada.issa>
2019-02-27 09:56:20 +0000

    Updating structure of Input arguments
    git-tfs-id: [http://developmentvlan:8585/tfs/vanrise.collection]$/;C53840

---
+++ SOM/Code/BPMExtended.Main.SOM.Entities/BillingCommonInputArgument.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BPMExtended.Main.SOMAPI
+{
+    public class BillingCommonInputArgument
+    {
+        public Guid? ContactId { get; set; }
+
+        public Guid? AccountId { get; set; }
+
+        public string ContractId { get; set; }
+    }
+}
