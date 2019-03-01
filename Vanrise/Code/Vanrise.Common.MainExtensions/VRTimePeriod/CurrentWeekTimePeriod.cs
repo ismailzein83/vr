9804cb6e70006829fb3f95ab9a649c2b7b61897a
Zeinab Mokdad <VANRISE\zeinab.mokdad>
2019-03-01 08:07:03 +0000

    adding CurrentWeekTimePeriod
    git-tfs-id: [http://developmentvlan:8585/tfs/vanrise.collection]$/;C53878

---
+++ Vanrise/Code/Vanrise.Common.MainExtensions/VRTimePeriod/CurrentWeekTimePeriod.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using Vanrise.Entities;
+
+namespace Vanrise.Common.MainExtensions
+{
+    public class CurrentWeekTimePeriod : VRTimePeriod
+    {
+        public override Guid ConfigId => new Guid("858E92C1-9FBA-45A6-B4BF-9458E53C2EC8");
+        public override void GetTimePeriod(IVRTimePeriodContext context)
+        {
+            DateTime startOfWeek = GetStartOfWeek(context.EffectiveDate, DayOfWeek.Monday);
+            context.FromTime = startOfWeek;
+            context.ToTime = context.EffectiveDate;
+        }
+        public DateTime GetStartOfWeek(DateTime currentDate, DayOfWeek startOfWeek)
+        {
+            return currentDate.AddDays(-(DateTime.Today.DayOfWeek - startOfWeek));
+        }
+    }
+}
