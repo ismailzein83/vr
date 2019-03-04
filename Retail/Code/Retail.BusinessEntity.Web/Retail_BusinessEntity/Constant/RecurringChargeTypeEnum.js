9087a10ec76bb6d90c1e50b714bca056faee130c
yara.bousalman <yara.bousalman@vanrise.tfs.local>
2019-03-04 13:02:41 +0000

    Adding recurring charge type settings implementation to GenericBEExtendedSettings
    git-tfs-id: [http://developmentvlan:8585/tfs/vanrise.collection]$/;C53925

---
+++ Retail/Code/Retail.BusinessEntity.Web/Retail_BusinessEntity/Constant/RecurringChargeTypeEnum.js
@@ -0,0 +1,4 @@
+﻿app.constant('Retail_BE_RecurringChargeTypeEnum', {
+    Customer: { value: 0, description: "Customer" },
+    Supplier: { value: 1, description: "Supplier"}
+});
---
+++ Retail/Code/Retail.BusinessEntity.Web/Retail_BusinessEntity/Directives/Extensions/RecurringChargeTypeSettings/retail-be-recurringchargetype-settings.js
@@ -0,0 +1,66 @@
+﻿"use strict";
+app.directive("retailBeRecurringchargetypeSettings", ["UtilsService", "VRNotificationService", "VRUIUtilsService","Retail_BE_RecurringChargeTypeEnum",
+    function (UtilsService, VRNotificationService, VRUIUtilsService, Retail_BE_RecurringChargeTypeEnum) {
+
+        var directiveDefinitionObject = {
+
+            restrict: "E",
+            scope:
+            {
+                onReady: "="
+            },
+            controller: function ($scope, $element, $attrs) {
+                var ctrl = this;
+
+                var ctor = new RecurringChargeTypeSettings($scope, ctrl, $attrs);
+                ctor.initializeController();
+            },
+            controllerAs: "ctrl",
+            bindToController: true,
+            compile: function (element, attrs) {
+
+            },
+            templateUrl: "/Client/Modules/Retail_BusinessEntity/Directives/Extensions/RecurringChargeTypeSettings/Templates/RecurringChargeTypeSettingsTemplate.html"
+
+        };
+
+        function RecurringChargeTypeSettings($scope, ctrl, $attrs) {
+            this.initializeController = initializeController;
+
+            function initializeController() {
+                $scope.scopeModel = {};
+                $scope.scopeModel.recurringChargeTypes = UtilsService.getArrayEnum(Retail_BE_RecurringChargeTypeEnum);
+
+                defineAPI();
+            }
+
+            function defineAPI() {
+                var api = {};
+
+                api.load = function (payload) {
+                    
+                    var promises = [];
+
+                    if (payload != undefined && payload.settings != undefined) {
+                        $scope.scopeModel.selectedRecurringChargeType = UtilsService.getItemByVal($scope.scopeModel.recurringChargeTypes, payload.settings.Type, 'value');
+                    }
+
+                    return UtilsService.waitMultiplePromises(promises);
+                };
+
+                api.getData = function () {
+                    return {
+                        $type: "Retail.BusinessEntity.Business.RecurringChargeTypeSettings, Retail.BusinessEntity.Business",
+                        Type: $scope.scopeModel.selectedRecurringChargeType != undefined ? $scope.scopeModel.selectedRecurringChargeType.value : undefined
+                    };
+                };
+
+                if (ctrl.onReady != null)
+                    ctrl.onReady(api);
+            }
+        }
+
+        return directiveDefinitionObject;
+
+    }
+]);
