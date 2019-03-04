d10871a11b4cc9df5fb971880ee7bb2547e74fcb
yara.bousalman <yara.bousalman@vanrise.tfs.local>
2019-03-04 10:53:38 +0000

    Adding recurring charge classes
    git-tfs-id: [http://developmentvlan:8585/tfs/vanrise.collection]$/;C53913

---
+++ Retail/Code/Retail.BusinessEntity.Business/RecurringCharges/RecurringChargeTypeSettings.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Vanrise.GenericData.Business;
+
+namespace Retail.BusinessEntity.Business
+{
+    public enum RecurringChargeType { Customer = 0, Supplier = 1 }
+    public class RecurringChargeTypeSettings : GenericBEExtendedSettings
+    {
+        public override Guid ConfigId { get { return new Guid("2289C6CB-81B0-449C-9063-1AD156450B18"); } }
+
+        public RecurringChargeType Type { get; set; }
+    }
+}
