d6dd518eba30daa3388350421a9738ce7305aadf
Fatima Al Heck <VANRISE\fatima.alheck>
2019-03-04 09:41:44 +0000

    Adding FatimaAlHeckTask
    git-tfs-id: [http://developmentvlan:8585/tfs/vanrise.collection]$/;C53904

---
+++ Mediation/Code/Mediation.Runtime/Tasks/FatimaAlHeckTask.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using Vanrise.BusinessProcess;
+using Vanrise.DataParser.Business;
+using Vanrise.Queueing;
+using Vanrise.Runtime;
+using Vanrise.Runtime.Entities;
+using Vanrise.Common;
+using System.Collections;
+using Mediation.Runtime.DataParser;
+
+namespace Mediation.Runtime.Tasks
+{
+    public class FatimaAlHeckTask : ITask
+    {
+        public void Execute()
+        {
+            #region DataParserTesterTask
+            DataParserTesterTask.DataParserTesterTask_Main();
+            #endregion
+        }
+    }
+}
