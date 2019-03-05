563b2f17594a115bc20a20ddcac809fb2b9caca9
yara.bousalman <yara.bousalman@vanrise.tfs.local>
2019-03-05 14:04:24 +0000

    Adding Gateway Table
    git-tfs-id: [http://developmentvlan:8585/tfs/vanrise.collection]$/;C53971

---
+++ Retail/Code/Interconnect/ICX_BE/Tables/Gateway.sql
@@ -0,0 +1,11 @@
+﻿CREATE TABLE [ICX_BE].[Gateway] (
+    [ID]               INT            IDENTITY (1, 1) NOT NULL,
+    [Name]             NVARCHAR (255) NULL,
+    [CreatedTime]      DATETIME       NULL,
+    [CreatedBy]        INT            NULL,
+    [LastModifiedTime] DATETIME       NULL,
+    [LastModifiedBy]   INT            NULL,
+    [timestamp]        ROWVERSION     NULL,
+    CONSTRAINT [PK_Gateway] PRIMARY KEY CLUSTERED ([ID] ASC)
+);
+
