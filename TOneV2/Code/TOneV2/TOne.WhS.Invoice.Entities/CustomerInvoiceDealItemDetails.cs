111a0182e58054184089442933c10f71156d4d50
yara.bousalman <yara.bousalman@vanrise.tfs.local>
2019-03-08 13:06:23 +0000

    Adding CustomerInvoiceDealItemDetails
    git-tfs-id: [http://developmentvlan:8585/tfs/vanrise.collection]$/;C54094

---
+++ TOneV2/Code/TOneV2/TOne.WhS.Invoice.Entities/CustomerInvoiceDealItemDetails.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TOne.WhS.Invoice.Entities
+{
+    public class CustomerInvoiceDealItemDetails
+    {
+        public long? SaleDealZoneGroupNb { get; set; }
+        public int? SaleDealTierNb { get; set; }
+        public int? SaleDeal { get; set; }
+        public decimal? SaleDealRateTierNb { get; set; }
+        public decimal Duration { get; set; }
+        public decimal Amount { get; set; }
+        public int NumberOfCalls { get; set; }
+    }
+}
