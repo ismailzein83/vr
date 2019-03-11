2cd3745e9819f4e62b90da72321c260db2aa1115
Ali Atoui <VANRISE\ali.atoui>
2019-03-11 16:11:02 +0000

    Adding TOne.WhS.RouteSync.Cataleya Project Adding CataleyaSWSync class
    git-tfs-id: [http://developmentvlan:8585/tfs/vanrise.collection]$/;C54150

---
+++ TOneV2/Code/TOneV2/TOne.WhS.RouteSync.Cataleya/CataleyaSWSync.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using TOne.WhS.RouteSync.Cataleya.Entities;
+using TOne.WhS.RouteSync.Entities;
+
+namespace TOne.WhS.RouteSync.Cataleya
+{
+    public class CataleyaSWSync : SwitchRouteSynchronizer
+    {
+        public override Guid ConfigId { get { return new Guid("D770F53B-057F-4BB8-BF20-883A2DBC510B"); } }
+
+        public string MappingSeparator { get; set; }
+
+        public int? NumberOfMappings { get; set; }
+
+        public int NumberOfOptions { get; set; }
+
+        public Dictionary<string, CarrierMapping> CarrierMappings { get; set; }
+
+        public Guid VRConnectionId { get; set; }
+
+
+        #region Public Methods
+
+        public override void ApplySwitchRouteSyncRoutes(ISwitchRouteSynchronizerApplyRoutesContext context)
+        {
+            throw new NotImplementedException();
+        }
+
+        public override void ConvertRoutes(ISwitchRouteSynchronizerConvertRoutesContext context)
+        {
+            throw new NotImplementedException();
+        }
+
+        public override void Finalize(ISwitchRouteSynchronizerFinalizeContext context)
+        {
+            throw new NotImplementedException();
+        }
+
+        public override void Initialize(ISwitchRouteSynchronizerInitializeContext context)
+        {
+            throw new NotImplementedException();
+        }
+
+        public override object PrepareDataForApply(ISwitchRouteSynchronizerPrepareDataForApplyContext context)
+        {
+            throw new NotImplementedException();
+        }
+
+        public override void RemoveConnection(ISwitchRouteSynchronizerRemoveConnectionContext context)
+        {
+            throw new NotImplementedException();
+        }
+
+        #endregion
+    }
+}
---
+++ TOneV2/Code/TOneV2/TOne.WhS.RouteSync.Cataleya/Entities/CarrierMapping.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections.Generic;
+
+namespace TOne.WhS.RouteSync.Cataleya.Entities
+{
+    public class CarrierMapping
+    {
+        public int CarrierId { get; set; }
+
+        public List<CustomerMapping> CustomerMappings { get; set; }
+
+        public List<SupplierMapping> SupplierMappings { get; set; }
+    }
+
+    public class CustomerMapping
+    {
+        public string Mapping { get; set; }
+    }
+
+    public class SupplierMapping
+    {
+        public string Mapping { get; set; }
+    }
+}
