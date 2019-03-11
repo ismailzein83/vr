1f8dbfcfe00555c4e0e85276848811afa6d36810
yara.bousalman <yara.bousalman@vanrise.tfs.local>
2019-03-11 15:30:46 +0000

    Adding BaseBPTaskTypeSettingsCustomObject
    git-tfs-id: [http://developmentvlan:8585/tfs/vanrise.collection]$/;C54127

---
+++ BusinessProcess/Code/Vanrise.BusinessProcess.Business/BaseBPTaskTypeSettingsCustomObject.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+using Vanrise.GenericData.Entities;
+
+namespace Vanrise.BusinessProcess.Business
+{
+    public class BaseBPTaskTypeSettingsCustomObject : FieldCustomObjectTypeSettings
+    {
+        public override Guid ConfigId { get { return new Guid("9C73540F-97E8-4F7A-BD02-594F6DD29741"); } }
+
+        public override bool AreEqual(object newValue, object oldValue)
+        {
+            throw new NotImplementedException();
+        }
+
+        public override string GetDescription(IFieldCustomObjectTypeSettingsContext context)
+        {
+            throw new NotImplementedException();
+        }
+
+        public override Type GetNonNullableRuntimeType()
+        {
+            throw new NotImplementedException();
+        }
+
+        public override string GetRuntimeTypeDescription()
+        {
+            throw new NotImplementedException();
+        }
+
+        public override dynamic ParseNonNullValueToFieldType(object originalValue)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
