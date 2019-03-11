538b8543cd32af9d056a1da4f9dcd29cb21128c5
yara.bousalman <yara.bousalman@vanrise.tfs.local>
2019-03-11 12:26:21 +0000

    Adding SupplierInvoiceDealItemDetails
    git-tfs-id: [http://developmentvlan:8585/tfs/vanrise.collection]$/;C54113

---
+++ TOneV2/Code/TOneV2/TOne.WhS.Invoice.Entities/SupplierInvoiceDealItemDetails.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TOne.WhS.Invoice.Entities
+{
+    public class SupplierInvoiceDealItemDetails
+    {
+        public long? CostDealZoneGroupNb { get; set; }
+        public int? CostDealTierNb { get; set; }
+        public int? CostDeal { get; set; }
+        public decimal? CostDealRateTierNb { get; set; }
+        public decimal Duration { get; set; }
+        public decimal Amount { get; set; }
+        public int NumberOfCalls { get; set; }
+        public int Currency { get; set; }
+    }
+}
