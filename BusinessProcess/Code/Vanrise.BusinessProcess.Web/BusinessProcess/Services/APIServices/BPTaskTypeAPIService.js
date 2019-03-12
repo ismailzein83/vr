ee891cfdb4a8c0e3b758a793687d2e0114515bab
Fatima Hammoud <VANRISE\fatima.hammoud>
2019-03-12 11:10:47 +0000

    adding GetBPTaskType  and GetBPTaskTypesInfo methods
    git-tfs-id: [http://developmentvlan:8585/tfs/vanrise.collection]$/;C54256

--- BusinessProcess/Code/Vanrise.BusinessProcess.Web/BusinessProcess/Services/APIServices/BPTaskTypeAPIService.js
+++ BusinessProcess/Code/Vanrise.BusinessProcess.Web/BusinessProcess/Services/APIServices/BPTaskTypeAPIService.js
@@ -4,19 +4,34 @@
     BusinessProcess_BPTaskTypeAPIService.$inject = ['BaseAPIService', 'UtilsService', 'BusinessProcess_BP_ModuleConfig'];
 
     function BusinessProcess_BPTaskTypeAPIService(BaseAPIService, UtilsService, BusinessProcess_BP_ModuleConfig) {
+
         function GetBPTaskTypeByTaskId(taskId) {
             return BaseAPIService.get(UtilsService.getServiceURL(BusinessProcess_BP_ModuleConfig.moduleName, "BPTaskType", "GetBPTaskTypeByTaskId"), {
                 taskId: taskId
             });
+        }
 
-            function GetBaseBPTaskTypeSettingsConfigs() {
-                return BaseAPIService.get(UtilsService.getServiceURL(BusinessProcess_BP_ModuleConfig.moduleName, "BPTaskType", "GetBaseBPTaskTypeSettingsConfigs"));
-            }
+        function GetBaseBPTaskTypeSettingsConfigs() {
+            return BaseAPIService.get(UtilsService.getServiceURL(BusinessProcess_BP_ModuleConfig.moduleName, "BPTaskType", "GetBaseBPTaskTypeSettingsConfigs"));
+        };
+
+        function GetBPTaskType(taskTypeId) {
+            return BaseAPIService.get(UtilsService.getServiceURL(BusinessProcess_BP_ModuleConfig.moduleName, "BPTaskType", "GetBPTaskType"), {
+                taskTypeId: taskTypeId
+            });
+        }
+
+        function GetBPTaskTypesInfo(filter) {
+            return BaseAPIService.get(UtilsService.getServiceURL(BusinessProcess_BP_ModuleConfig.moduleName, "BPTaskType", "GetBPTaskTypesInfo"), {
+                filter: filter
+            });
         }
 
         return ({
             GetBPTaskTypeByTaskId: GetBPTaskTypeByTaskId,
-            GetBaseBPTaskTypeSettingsConfigs: GetBaseBPTaskTypeSettingsConfigs
+            GetBaseBPTaskTypeSettingsConfigs: GetBaseBPTaskTypeSettingsConfigs,
+            GetBPTaskType: GetBPTaskType,
+            GetBPTaskTypesInfo: GetBPTaskTypesInfo,
         });
     }
 
