19d22a5435434c07651b9602e0467a87d82fd93e
yara.bousalman <yara.bousalman@vanrise.tfs.local>
2019-03-12 12:42:15 +0000

    Implementing methods in BaseBPTaskTypeSettingsCustomObject
    git-tfs-id: [http://developmentvlan:8585/tfs/vanrise.collection]$/;C54261

--- BusinessProcess/Code/Vanrise.BusinessProcess.Business/BaseBPTaskTypeSettingsCustomObject.cs
+++ BusinessProcess/Code/Vanrise.BusinessProcess.Business/BaseBPTaskTypeSettingsCustomObject.cs
@@ -4,6 +4,8 @@ using System.Linq;
 using System.Runtime.CompilerServices;
 using System.Text;
 using System.Threading.Tasks;
+using Vanrise.BusinessProcess.Entities;
+using Vanrise.Common;
 using Vanrise.GenericData.Entities;
 
 namespace Vanrise.BusinessProcess.Business
@@ -19,12 +21,12 @@ namespace Vanrise.BusinessProcess.Business
 
         public override string GetDescription(IFieldCustomObjectTypeSettingsContext context)
         {
-            throw new NotImplementedException();
+            return null;
         }
 
         public override Type GetNonNullableRuntimeType()
         {
-            throw new NotImplementedException();
+            return typeof(BaseBPTaskTypeSettings);
         }
 
         public override string GetRuntimeTypeDescription()
@@ -34,7 +36,13 @@ namespace Vanrise.BusinessProcess.Business
 
         public override dynamic ParseNonNullValueToFieldType(object originalValue)
         {
-            throw new NotImplementedException();
+            var castedOriginalValue = originalValue as BaseBPTaskTypeSettings;
+            if (castedOriginalValue != null)
+            {
+                return castedOriginalValue;
+            }
+            else
+                return Serializer.Deserialize<BaseBPTaskTypeSettings>(originalValue.ToString());
         }
     }
 }
