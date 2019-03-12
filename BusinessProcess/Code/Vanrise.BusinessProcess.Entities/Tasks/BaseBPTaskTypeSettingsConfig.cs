14e31b0a29d2d495bc7ea70cb32e0ad0e6aa98ac
yara.bousalman <yara.bousalman@vanrise.tfs.local>
2019-03-12 10:08:46 +0000

    Adding BaseBPTaskTypeSettingsConfig
    git-tfs-id: [http://developmentvlan:8585/tfs/vanrise.collection]$/;C54226

---
+++ BusinessProcess/Code/Vanrise.BusinessProcess.Entities/Tasks/BaseBPTaskTypeSettingsConfig.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Vanrise.BusinessProcess.Entities
+{
+    public class BaseBPTaskTypeSettingsConfig
+    {
+        public const string EXTENSION_TYPE = "BusinessProcess_BP_BaseBPTaskTypeSettingsConfig";
+
+        public string Editor { get; set; }
+    }
+}
