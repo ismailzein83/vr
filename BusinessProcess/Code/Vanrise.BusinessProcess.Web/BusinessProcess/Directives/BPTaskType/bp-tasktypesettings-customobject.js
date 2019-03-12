508d6cfced6e20be076fa78d5bfe61f43dcb12ce
yara.bousalman <yara.bousalman@vanrise.tfs.local>
2019-03-12 08:14:49 +0000

    Adding bp task type settings files
    git-tfs-id: [http://developmentvlan:8585/tfs/vanrise.collection]$/;C54196

---
+++ BusinessProcess/Code/Vanrise.BusinessProcess.Web/BusinessProcess/Directives/BPTaskType/bp-tasktypesettings-customobject.js
@@ -0,0 +1,51 @@
+﻿(function (app) {
+
+    'use strict';
+
+    function bpTaskTypeCustomObjectSettings() {
+        return {
+            restrict: "E",
+            scope: {
+                onReady: "="
+            },
+            controller: function ($scope, $element, $attrs) {
+                var ctrl = this;
+                var ctor = new SettingsCtor($scope, ctrl, $attrs);
+                ctor.initializeController();
+            },
+            controllerAs: "Ctrl",
+            bindToController: true,
+            templateUrl: "/Client/Modules/BusinessProcess/Directives/BPTaskType/Templates/BPTaskTypeSettingsTemplate.html"
+
+        };
+        function SettingsCtor($scope, ctrl, $attrs) {
+            this.initializeController = initializeController;
+
+            function initializeController() {
+                $scope.scopeModel = {};
+                defineAPI();
+            }
+            function defineAPI() {
+                var api = {};
+
+                api.load = function (payload) {
+
+                };
+
+                api.getData = function () {
+                    var data = {
+                        $type: "Vanrise.BusinessProcess.Business.BaseBPTaskTypeSettingsCustomObject,Vanrise.BusinessProcess.Business"
+                    };
+                    return data;
+                };
+
+                if (ctrl.onReady != undefined && typeof (ctrl.onReady) == 'function') {
+                    ctrl.onReady(api);
+                }
+            }
+        } 
+    }
+
+    app.directive('bpTasktypesettingsCustomobject', bpTaskTypeCustomObjectSettings);
+
+})(app);
