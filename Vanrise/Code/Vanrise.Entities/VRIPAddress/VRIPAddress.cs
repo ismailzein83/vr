cb6ed5181e49b4f9cf63f589152c902e7023dcff
Ali Atoui <VANRISE\ali.atoui>
2019-03-12 13:51:53 +0000

    Adding VRIPAddress
    git-tfs-id: [http://developmentvlan:8585/tfs/vanrise.collection]$/;C54269

---
+++ Vanrise/Code/Vanrise.Entities/VRIPAddress/VRIPAddress.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace Vanrise.Entities
+{
+    public enum VRIPAddressType { IPv4 = 0, IPv6 = 1 }
+
+    public class VRIPAddress
+    {
+        public VRIPAddressType Type { get; set; }
+
+        public string IPAddress { get; set; }
+
+        public int SubnetPrefixLength { get; set; }
+    }
+}
