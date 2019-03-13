022a1a61230223225562acc067faee0837c8b274
Samer Haidar <VANRISE\samer.haidar>
2019-03-13 13:09:18 +0000

    Change Data Store settings
    git-tfs-id: [http://developmentvlan:8585/tfs/vanrise.collection]$/;C54303

--- TOneV2/Code/TOneV2/SQL.TOneConfiguration/Voucher.PostDeployment.sql
+++ TOneV2/Code/TOneV2/SQL.TOneConfiguration/Voucher.PostDeployment.sql
@@ -106,7 +106,7 @@ set nocount on;
 ;with cte_data([ID],[Name],[Settings])
 as (select * from (values
 --//////////////////////////////////////////////////////////////////////////////////////////////////
-('864060FF-69B2-4C63-976F-8349D2821AD2','VoucherDB','{"$type":"Vanrise.GenericData.SQLDataStorage.SQLDataStoreSettings, Vanrise.GenericData.SQLDataStorage","ConfigId":"2aeec2de-ec44-4698-aaef-8e9dbf669d1e","ConnectionStringName":"VoucherDBConnString","IsRemoteDataStore":false}')
+('864060FF-69B2-4C63-976F-8349D2821AD2','VoucherDB','{"$type":"Vanrise.GenericData.SQLDataStorage.SQLDataStoreSettings, Vanrise.GenericData.SQLDataStorage","ConfigId":"2aeec2de-ec44-4698-aaef-8e9dbf669d1e","ConnectionStringAppSettingName":"VoucherDBConnStringKey","IsRemoteDataStore":false}')
 --\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\
 )c([ID],[Name],[Settings]))
 merge	[genericdata].[DataStore] as t
