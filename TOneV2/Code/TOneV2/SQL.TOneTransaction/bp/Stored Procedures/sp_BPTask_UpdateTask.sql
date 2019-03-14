3999bea9cd5207d48e045f32cd75a3976e4e9959
yara.bousalman <yara.bousalman@vanrise.tfs.local>
2019-03-14 11:01:02 +0000

    Adding stored procedure
    git-tfs-id: [http://developmentvlan:8585/tfs/vanrise.collection]$/;C54335

---
+++ TOneV2/Code/TOneV2/SQL.TOneTransaction/bp/Stored Procedures/sp_BPTask_UpdateTask.sql
@@ -0,0 +1,15 @@
+﻿-- =============================================
+-- Author:		<Author,,Name>
+-- Create date: <Create Date,,>
+-- Description:	<Description,,>
+-- =============================================
+Create PROCEDURE [bp].[sp_BPTask_UpdateTask]
+	@TaskID bigint,
+	@TaskData nvarchar(max)
+AS
+BEGIN
+Update [bp].[BPTask] set [TaskData] = @TaskData,
+						 [LastUpdatedTime] = GETDATE()
+where ID = @TaskID
+
+END
