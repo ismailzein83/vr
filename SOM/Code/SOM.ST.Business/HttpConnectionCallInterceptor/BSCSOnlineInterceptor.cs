ff34d94c62474ed25a2ab2a5008e8a61683ae106
Rodi Hassan <VANRISE\rodi.hassan>
2019-03-14 13:38:50 +0000

    implement BSCSOnlineInterceptor
    git-tfs-id: [http://developmentvlan:8585/tfs/vanrise.collection]$/;C54347

--- SOM/Code/SOM.ST.Business/HttpConnectionCallInterceptor/BSCSOnlineInterceptor.cs
+++ SOM/Code/SOM.ST.Business/HttpConnectionCallInterceptor/BSCSOnlineInterceptor.cs
@@ -1,23 +1,40 @@
-﻿//using Vanrise.Common.Business;
+﻿using System;
+using Vanrise.Common.Business;
 
-//namespace SOM.ST.Business.ConnectionCallInterceptor
-//{
-//    public class BSCSOnlineInterceptor : VRHttpConnectionCallInterceptor
-//    {
-//        public string UserName { get; set; }
+namespace SOM.ST.Business
+{
+    public class BSCSOnlineInterceptor : VRHttpConnectionCallInterceptor
+    {
+        public string UserName { get; set; }
 
-//        public string Password { get; set; }
+        public string Password { get; set; }
 
-//        public string AuthenticationServiceURI { get; set; }
+        public override Guid ConfigId { get { return new Guid("461B7474-9B19-4B90-AEAB-63BA37245E53"); } }
 
-//        public override void InterceptRequest(IVRHttpConnectionInterceptRequestContext context)
-//        {
-//            var fullAuthenticationServiceURI = string.Concat(context.Connection.BaseURL, AuthenticationServiceURI);
-//        }
+        private string HeaderText = @"
+                <soapenv:Header>
+                    <wsse:Security soapenv:mustUnderstand=""1"" xmlns:wsse=""http://docs.oasis-open.org/wss/2004/01/oasis-200401-wss-wssecurity-secext-1.0.xsd"" xmlns:wsu=""http://docs.oasis-open.org/wss/2004/01/oasis-200401-wss-wssecurity-utility-1.0.xsd"">
+                        <wsse:UsernameToken wsu:Id=""UsernameToken-8FC6ED7B8E6B25B81515409786233051"">
+                            <wsse:Username>##username##</wsse:Username>
+                            <wsse:Password Type=""http://docs.oasis-open.org/wss/2004/01/oasis-200401-wss-username-token-profile-1.0#PasswordText"">##password##</wsse:Password>
+                            <wsse:Nonce EncodingType=""http://docs.oasis-open.org/wss/2004/01/oasis-200401-wss-soap-message-security-1.0#Base64Binary"">67ah3oBQJcOJ0QsYfJ+W9w==</wsse:Nonce>
+                            <wsu:Created>2018-10-31T09:37:03.298Z</wsu:Created>
+                        </wsse:UsernameToken>
+                    </wsse:Security>
+                </soapenv:Header>
+                     ";
 
-//        public override void InterceptResponse(IVRHttpConnectionInterceptResponseContext context)
-//        {
-//            throw new System.NotImplementedException();
-//        }
-//    }
-//}
+        public override void InterceptRequest(IVRHttpConnectionInterceptRequestContext context)
+        {
+            var headerText = HeaderText;
+            headerText = headerText.Replace("##username##", UserName);
+            headerText = headerText.Replace("##password##", Password);
+            context.Body = context.Body.Replace("<soapenv:Header />", headerText);
+        }
+
+        public override void InterceptResponse(IVRHttpConnectionInterceptResponseContext context)
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+}
