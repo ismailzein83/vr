848335c0bdba90a817347e482fb59b6b4208d803
Mohammad Yahfoufi <VANRISE\mohammad.yahfoufi>
2019-03-15 12:29:01 +0000

    Adding BusinessEntity Manager
    git-tfs-id: [http://developmentvlan:8585/tfs/vanrise.collection]$/;C54411

---
+++ SOM/Code/BPMExtended.Main.Business/BusinessEntityManager.cs
@@ -0,0 +1,31 @@
+﻿using BPMExtended.Main.Common;
+using BPMExtended.Main.Entities;
+using BPMExtended.Main.SOMAPI;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Web;
+using Terrasoft.Core;
+using Terrasoft.Core.Entities;
+
+namespace BPMExtended.Main.Business
+{
+    public class BusinessEntityManager
+    {
+        //public UserConnection BPM_UserConnection
+        //{
+        //    get
+        //    {
+        //        return (UserConnection)HttpContext.Current.Session["UserConnection"];
+        //    }
+        //}
+       
+        public Array GetLineOfBusniessArray()
+        {
+            return Enum.GetValues(typeof(LineOfBusiness));
+        }
+
+    }
+}
