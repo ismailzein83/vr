5edb959b125c6adff2ffb8efbf1f9fab2a453c11
yara.bousalman <yara.bousalman@vanrise.tfs.local>
2019-03-18 15:21:14 +0000

    Adding bp-vr-workflow-manualexeceditor and corresponding files
    git-tfs-id: [http://developmentvlan:8585/tfs/vanrise.collection]$/;C54482

---
+++ BusinessProcess/Code/Vanrise.BusinessProcess.Web/BusinessProcess/Directives/VRWorkflow/bp-vr-workflow-manualexeceditor.js
@@ -0,0 +1,54 @@
+﻿"use strict";
+
+app.directive("bpVrWorkflowManualexeceditor", ['UtilsService', 'VRUIUtilsService',
+    function (UtilsService, VRUIUtilsService) {
+        var directiveDefinitionObject = {
+            restrict: "E",
+            scope: {
+                onReady: "="
+            },
+            controller: function ($scope, $element, $attrs) {
+                var ctrl = this;
+                var ctor = new ManualExecEditor(ctrl, $scope, $attrs);
+                ctor.initializeController();
+            },
+            controllerAs: "ctrl",
+            bindToController: true,
+            templateUrl: "/Client/Modules/BusinessProcess/Directives/VRWorkflow/Templates/VRWorkflowManualExecEditorTemplate.html"
+        };
+
+        function ManualExecEditor(ctrl, $scope, $attrs) {
+            this.initializeController = initializeController;
+
+
+            function initializeController() {
+                $scope.scopeModel = {};
+
+                defineAPI();
+            }
+
+            function defineAPI() {
+
+                var api = {};
+
+                api.load = function (payload) {
+                    var promises = [];
+
+
+                    return UtilsService.waitMultiplePromises(promises);
+                };
+
+                api.getData = function () {
+                    return {
+
+                    };
+                };
+
+                if (ctrl.onReady != null)
+                    ctrl.onReady(api);
+            }
+
+        }
+
+        return directiveDefinitionObject;
+    }]);
---
+++ BusinessProcess/Code/Vanrise.BusinessProcess.Web/BusinessProcess/Services/APIServices/DynamicBusinessProcessAPIService.js
@@ -0,0 +1,19 @@
+﻿(function (appControllers) {
+
+    "use strict";
+
+    DynamicBusinessProcessAPIService.$inject = ['BaseAPIService', 'UtilsService', 'SecurityService'];
+
+    function DynamicBusinessProcessAPIService(BaseAPIService, UtilsService, SecurityService) {
+
+        function StartProcess(processName, input) {
+            return BaseAPIService.post(UtilsService.getServiceURL('DynamicBusinessProcess_BP', processName, "StartProcess"), input);
+        }
+
+        return ({
+            StartProcess: StartProcess
+        });
+    }
+
+    appControllers.service('BusinessProcess_DynamicBusinessProcessAPIService', DynamicBusinessProcessAPIService);
+})(appControllers);
