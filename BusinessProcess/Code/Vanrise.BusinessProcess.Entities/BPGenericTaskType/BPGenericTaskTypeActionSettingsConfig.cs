a1479152d618b3d4e037a01e9c087c543dd5f4d9
Fatima Hammoud <VANRISE\fatima.hammoud>
2019-03-19 11:51:50 +0000

    adding BPGenericTaskTypeActionSettingsConfig class
    git-tfs-id: [http://developmentvlan:8585/tfs/vanrise.collection]$/;C54511

---
+++ BusinessProcess/Code/Vanrise.BusinessProcess.Entities/BPGenericTaskType/BPGenericTaskTypeActionSettingsConfig.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Vanrise.Entities;
+
+namespace Vanrise.BusinessProcess.Entities
+{
+    public class BPGenericTaskTypeActionSettingsConfig: ExtensionConfiguration
+    {
+        public const string EXTENSION_TYPE = "BP_Generic_TaskType_ActionSettings";
+
+        public string Editor { get; set; }
+    }
+}
