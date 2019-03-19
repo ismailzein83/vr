17080000ca4d22e4b1862efa8ef2671074d638cc
Mohammad Sheaib <VANRISE\mohammad.sheaib>
2019-03-19 17:17:49 +0000

    Generic BE Account Subview Implementation
    git-tfs-id: [http://developmentvlan:8585/tfs/vanrise.collection]$/;C54536

---
+++ Retail/Code/Retail.BusinessEntity.MainExtensions/AccountViews/GenericBEAccountSubview.cs
@@ -0,0 +1,39 @@
+﻿    using Retail.BusinessEntity.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Vanrise.GenericData.Entities;
+
+namespace Retail.BusinessEntity.MainExtensions.AccountViews
+{
+   public  class GenericBEAccountSubview : AccountViewDefinitionSettings
+    {
+        public override Guid ConfigId
+        {
+            get { return new Guid("E3148386-1049-416A-876F-19C6C42B30A0"); }
+        }
+
+        public DataRecordField BusinessEntityDefinitionId { get; set; }
+        public DataRecordField AccountIdMappingField { get; set; }
+        public string AccountBEDefinitionMappingField { get; set; }
+        public override string RuntimeEditor
+        {
+            get
+            {
+                return "retail-be-genericbeaccount-view";
+            }
+            set
+            {
+
+            }
+        }
+
+        public override bool DoesUserHaveAccess(IAccountViewDefinitionCheckAccessContext context)
+        {
+            return base.DoesUserHaveAccess(context);
+        }
+    }
+}
+
