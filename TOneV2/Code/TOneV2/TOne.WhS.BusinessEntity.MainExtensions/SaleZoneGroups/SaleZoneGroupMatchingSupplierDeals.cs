73320945e1767b2e91ea3c69b2ca140e6ca1bf33
Fatima Al Heck <VANRISE\fatima.alheck>
2019-03-19 15:47:04 +0000

    Adding SaleZoneGroupMatchingSupplierDeals class, an implementation of SaleZoneGroupSettings class
    git-tfs-id: [http://developmentvlan:8585/tfs/vanrise.collection]$/;C54530

---
+++ TOneV2/Code/TOneV2/TOne.WhS.BusinessEntity.MainExtensions/SaleZoneGroups/SaleZoneGroupMatchingSupplierDeals.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using TOne.WhS.BusinessEntity.Entities;
+
+namespace TOne.WhS.BusinessEntity.MainExtensions.SaleZoneGroups
+{
+    public class SaleZoneGroupMatchingSupplierDeals : SaleZoneGroupSettings
+    {
+        public override Guid ConfigId { get { return new Guid("8A2D2C28-A3B8-48BD-AE0E-0BE382CE6170"); } }
+
+        public List<long> ZoneIds { get; set; }
+
+        public List<int> SupplierDealIds { get; set; }
+
+        public override IEnumerable<long> GetZoneIds(ISaleZoneGroupContext context)
+        {
+            throw new NotImplementedException();
+        }
+
+        public override string GetDescription(ISaleZoneGroupContext context)
+        {
+            throw new NotImplementedException();
+        }
+
+        public override void CleanDeletedZoneIds(ISaleZoneGroupCleanupContext context)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
