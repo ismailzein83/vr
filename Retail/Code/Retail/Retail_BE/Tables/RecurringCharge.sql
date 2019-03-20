78f4a429530733001a06354951453c53d3fea506
Samer Haidar <VANRISE\samer.haidar>
2019-03-20 13:11:37 +0000

    Sync Database by adding two new tables
    git-tfs-id: [http://developmentvlan:8585/tfs/vanrise.collection]$/;C54562

---
+++ Retail/Code/Retail/Retail_BE/Tables/RecurringCharge.sql
@@ -0,0 +1,19 @@
+﻿CREATE TABLE [Retail_BE].[RecurringCharge] (
+    [ID]                    BIGINT          IDENTITY (1, 1) NOT NULL,
+    [Amount]                DECIMAL (22, 6) NULL,
+    [CurrencyId]            INT             NULL,
+    [BED]                   DATETIME        NULL,
+    [EED]                   DATETIME        NULL,
+    [CreatedTime]           DATETIME        NULL,
+    [LastModifiedTime]      DATETIME        NULL,
+    [CreatedBy]             INT             NULL,
+    [LastModifiedBy]        INT             NULL,
+    [Classification]        NVARCHAR (40)   NULL,
+    [timestamp]             ROWVERSION      NULL,
+    [FinancialAccountId]    NVARCHAR (40)   NULL,
+    [RecurringChargeTypeId] BIGINT          NULL,
+    [DuePeriod]             INT             NULL,
+    [RecurringChargePeriod] NVARCHAR (MAX)  NULL,
+    CONSTRAINT [PK__Customer__3214EC273138400F] PRIMARY KEY CLUSTERED ([ID] ASC)
+);
+
---
+++ Retail/Code/Retail/Retail_BE/Tables/RecurringChargeType.sql
@@ -0,0 +1,11 @@
+﻿CREATE TABLE [Retail_BE].[RecurringChargeType] (
+    [ID]               BIGINT         IDENTITY (1, 1) NOT NULL,
+    [Name]             NVARCHAR (255) NULL,
+    [CreatedTime]      DATETIME       NULL,
+    [CreatedBy]        INT            NULL,
+    [LastModifiedBy]   INT            NULL,
+    [LastModifiedTime] DATETIME       NULL,
+    [timestamp]        ROWVERSION     NULL,
+    CONSTRAINT [PK__Recurrin__3214EC2738D961D7] PRIMARY KEY CLUSTERED ([ID] ASC)
+);
+
