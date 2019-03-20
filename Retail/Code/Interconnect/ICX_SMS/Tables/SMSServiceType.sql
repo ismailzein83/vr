e8a1cefee04a1b69bac27ca55df255881e1aa6f6
Samer Haidar <VANRISE\samer.haidar>
2019-03-20 13:50:12 +0000

    Syn SMS ServiceType
    git-tfs-id: [http://developmentvlan:8585/tfs/vanrise.collection]$/;C54566

---
+++ Retail/Code/Interconnect/ICX_SMS/Tables/SMSServiceType.sql
@@ -0,0 +1,12 @@
+﻿CREATE TABLE [ICX_SMS].[SMSServiceType] (
+    [ID]               INT           IDENTITY (1, 1) NOT NULL,
+    [Name]             NVARCHAR (40) NULL,
+    [Symbol]           NVARCHAR (40) NULL,
+    [CreatedTime]      DATETIME      NULL,
+    [LastModifiedTime] DATETIME      NULL,
+    [CreatedBy]        INT           NULL,
+    [LastModifiedBy]   INT           NULL,
+    [timestamp]        ROWVERSION    NULL,
+    PRIMARY KEY CLUSTERED ([ID] ASC)
+);
+
