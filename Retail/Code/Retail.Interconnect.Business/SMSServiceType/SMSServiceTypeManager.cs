88981c44a3cc1b773818b6b43b87e18e109a7044
Fatima Hammoud <VANRISE\fatima.hammoud>
2019-03-20 07:59:46 +0000

    adding SMSServiceType class and manager
    git-tfs-id: [http://developmentvlan:8585/tfs/vanrise.collection]$/;C54540

---
+++ Retail/Code/Retail.Interconnect.Business/SMSServiceType/SMSServiceTypeManager.cs
@@ -0,0 +1,53 @@
+﻿using Retail.Interconnect.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Vanrise.Common;
+using Vanrise.GenericData.Entities;
+
+namespace Retail.Interconnect.Business
+{
+    public class SMSServiceTypeManager
+    {
+        static Guid businessEntityDefinitionId = new Guid("D5153143-A6EF-44D1-A6F8-FEA6B399D853");
+
+        #region Public Methods
+        #endregion
+
+        #region Private Methods
+        private Dictionary<int, SMSServiceType> GetCachedSMSServicesType()
+        {
+            IGenericBusinessEntityManager genericBusinessEntityManager = Vanrise.GenericData.Entities.BusinessManagerFactory.GetManager<IGenericBusinessEntityManager>();
+            return genericBusinessEntityManager.GetCachedOrCreate("GetCachedSMSServicesType", businessEntityDefinitionId, () =>
+            {
+                Dictionary<int, SMSServiceType> result = new Dictionary<int, SMSServiceType>();
+                IEnumerable<GenericBusinessEntity> genericBusinessEntities = genericBusinessEntityManager.GetAllGenericBusinessEntities(businessEntityDefinitionId);
+                if (genericBusinessEntities != null)
+                {
+                    foreach (GenericBusinessEntity genericBusinessEntity in genericBusinessEntities)
+                    {
+                        SMSServiceType smsServiceType = new SMSServiceType()
+                        {
+                            SMSServiceTypeId = (int)genericBusinessEntity.FieldValues.GetRecord("ID"),
+                            Name = genericBusinessEntity.FieldValues.GetRecord("Name") as string,
+                            Symbol = genericBusinessEntity.FieldValues.GetRecord("Symbol") as string,
+                            CreatedTime = (DateTime)genericBusinessEntity.FieldValues.GetRecord("CreatedTime"),
+                            LastModifiedTime = (DateTime)genericBusinessEntity.FieldValues.GetRecord("LastModifiedTime"),
+                            CreatedBy = (int)genericBusinessEntity.FieldValues.GetRecord("CreatedBy"),
+                            LastModifiedBy = (int)genericBusinessEntity.FieldValues.GetRecord("LastModifiedBy"),
+                        };
+                        result.Add(smsServiceType.SMSServiceTypeId, smsServiceType);
+                    }
+                }
+                return result;
+            });
+        }
+        #endregion
+
+
+        #region Mappers
+        #endregion
+    }
+}
---
+++ Retail/Code/Retail.Interconnect.Entities/SMSServiceType/SMSServiceType.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Retail.Interconnect.Entities
+{
+    public class SMSServiceType
+    {
+        public int SMSServiceTypeId { get; set; }
+
+        public string Name { get; set; }
+
+        public string Symbol { get; set; }
+
+        public DateTime CreatedTime { get; set; }
+
+        public DateTime LastModifiedTime { get; set; }
+
+        public int CreatedBy { get; set; }
+
+        public int LastModifiedBy { get; set; }
+    }
+}
