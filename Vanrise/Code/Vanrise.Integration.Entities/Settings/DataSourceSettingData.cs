4004f68804f3e6bc6a0f5d5bc4fd4968a9e62c22
Ali Slayman <VANRISE\ali.slayman>
2019-03-20 12:22:41 +0000

    Editing DataSourceSettingData
    git-tfs-id: [http://developmentvlan:8585/tfs/vanrise.collection]$/;C54558

--- Vanrise/Code/Vanrise.Integration.Entities/Settings/DataSourceSettingData.cs
+++ Vanrise/Code/Vanrise.Integration.Entities/Settings/DataSourceSettingData.cs
@@ -1,5 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Linq;
+using System.Text;
 using Vanrise.Entities;
 
 namespace Vanrise.Integration.Entities
@@ -7,6 +9,31 @@ namespace Vanrise.Integration.Entities
     public class DataSourceSettingData : SettingData
     {
         public FileDataSourceSettings FileDataSourceSettings { get; set; }
+
+        public override bool IsValid(ISettingDataValidationContext context)
+        {
+            if (FileDataSourceSettings == null)
+            {
+                context.ErrorMessage = "Data source settings is empty";
+                return false;
+            }
+
+            StringBuilder errorMessages = new StringBuilder();
+
+            if (FileDataSourceSettings.FileDataSourceDefinitions == null || FileDataSourceSettings.FileDataSourceDefinitions.Count == 0)
+                errorMessages.AppendLine("File Import Exception Setting is empty");
+            else if (FileDataSourceSettings.FileDataSourceDefinitions.Any(item => string.IsNullOrEmpty(item.Name)))
+                errorMessages.AppendLine("File Import Exception Setting is invalid");
+
+            if (FileDataSourceSettings.PeakTimeRanges != null && FileDataSourceSettings.PeakTimeRanges.Count > 0 && FileDataSourceSettings.PeakTimeRanges.Any(item => item.From == null || item.To == null))
+                errorMessages.AppendLine("Peak Time Ranges don't have valid values");
+
+            if (errorMessages.Length == 0)
+                return true;
+
+            context.ErrorMessage = errorMessages.ToString();
+            return false;
+        }
     }
 
     public class FileDataSourceSettings
