2552312a983b84f423769baccb180db76bff562b
Fatima Hammoud <VANRISE\fatima.hammoud>
2019-03-20 14:42:32 +0000

    add  StartBPProcessActionSettings class
    git-tfs-id: [http://developmentvlan:8585/tfs/vanrise.collection]$/;C54573

---
+++ Retail/Code/Retail.BusinessEntity.MainExtensions/AccountBEActionTypes/StartBPProcessActionSettings.cs
@@ -0,0 +1,26 @@
+﻿using Retail.BusinessEntity.Business;
+using Retail.BusinessEntity.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Retail.BusinessEntity.MainExtensions.AccountBEActionTypes
+{
+    public class StartBPProcessActionSettings : AccountActionDefinitionSettings
+    {
+        public override Guid ConfigId { get { return new Guid("A5FDAE33-89A4-457E-889F-CFA004B74395"); } }
+        public Guid BPDefinitionId { get; set; }
+        public string AccountIdInputFieldName { get; set; }
+        public string AccountBEDefinitionIdInputFieldName { get; set; }
+        public override string ClientActionName
+        {
+            get { return "Start BP Process"; }
+        }
+        public override bool DoesUserHaveAccess(IAccountActionDefinitionCheckAccessContext context)
+        {
+            return new AccountBEDefinitionManager().DoesUserHaveEditAccess(context.UserId, context.AccountBEDefinitionId);
+        }
+    }
+}
