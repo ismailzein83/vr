6385db191536ed0be7b3d5cf5b3371a0f43bd632
Naim Saad <VANRISE\naim.saad>
2019-03-20 19:22:18 +0000

    Implementing OperatorBankDetails Classes
    git-tfs-id: [http://developmentvlan:8585/tfs/vanrise.collection]$/;C54580

---
+++ Retail/Code/Retail.BusinessEntity.MainExtensions/OperatorBankDetailsDataSourceSettings.cs
@@ -0,0 +1,77 @@
+﻿using Retail.BusinessEntity.Business;
+using Retail.BusinessEntity.Entities;
+using Retail.BusinessEntity.MainExtensions.AccountParts;
+using System;
+using System.Collections.Generic;
+using Vanrise.Common;
+using Vanrise.Common.Business;
+using Vanrise.Entities;
+using Vanrise.Invoice.Business;
+using Vanrise.Invoice.Entities;
+
+namespace Retail.BusinessEntity.MainExtensions
+{
+    public class OperatorBankDetailsDataSourceSettings : InvoiceDataSourceSettings
+    {
+        public override Guid ConfigId { get { return new Guid("F0871B89-11BC-42D0-BA88-244DE96F581A"); } }
+
+        public override IEnumerable<dynamic> GetDataSourceItems(IInvoiceDataSourceSettingsContext context)
+        {
+            var invoice = context.InvoiceActionContext.GetInvoice();
+            FinancialAccountManager financialAccountManager = new FinancialAccountManager();
+            AccountBEManager accountBEManager = new AccountBEManager();
+
+            var invoiceTypeExtendedSettings = new InvoiceTypeManager().GetInvoiceTypeExtendedSettings(invoice.InvoiceTypeId);
+            var invoiceSettings = invoiceTypeExtendedSettings.CastWithValidate<BaseRetailInvoiceTypeSettings>("invoiceTypeExtendedSettings", invoice.InvoiceTypeId);
+
+            var financialAccountMananger = new FinancialAccountManager();
+            var financialAccountData = financialAccountMananger.GetFinancialAccountData(invoiceSettings.AccountBEDefinitionId, invoice.PartnerId);
+
+            IEnumerable<BankDetail> bankDetails = null;
+
+            if (financialAccountData != null && financialAccountData.Account != null && financialAccountData.Account.Settings != null && financialAccountData.Account.Settings.Parts != null)
+            {
+                foreach (var accountPart in financialAccountData.Account.Settings.Parts)
+                {
+                    if (accountPart.Value.Settings.ConfigId != AccountPartFinancial._ConfigId)
+                        continue;
+
+                    var accountPartSettings = accountPart.Value.Settings as AccountPartFinancial;
+                    if (accountPartSettings != null && accountPartSettings.OperatorBanks != null)
+                    {
+                        bankDetails = accountPartSettings.OperatorBanks;
+                    }
+                    break;
+                }
+            }
+
+            List<BankDetailsDetail> bankDetailsList = new List<BankDetailsDetail>();
+            if (bankDetails != null)
+            {
+                CurrencyManager currencyManager = new CurrencyManager();
+                foreach (var bankDetail in bankDetails)
+                {
+                    bankDetailsList.Add(new BankDetailsDetail
+                    {
+                        AccountCode = bankDetail.AccountCode,
+                        SortCode = bankDetail.SortCode,
+                        SwiftCode = bankDetail.SwiftCode,
+                        AccountHolder = bankDetail.AccountHolder,
+                        AccountNumber = bankDetail.AccountNumber,
+                        Address = bankDetail.Address,
+                        Bank = bankDetail.Bank,
+                        IBAN = bankDetail.IBAN,
+                        CurrencyId = bankDetail.CurrencyId,
+                        CurrencyName = currencyManager.GetCurrencySymbol(bankDetail.CurrencyId),
+                        ChannelName = bankDetail.ChannelName,
+                        CorrespondentBank = bankDetail.CorrespondentBank,
+                        CorrespondentBankSwiftCode = bankDetail.CorrespondentBankSwiftCode,
+                        ACH = bankDetail.ACH,
+                        ABARoutingNumber = bankDetail.ABARoutingNumber
+                    });
+                }
+            }
+            return bankDetailsList;
+        }
+    }
+}
