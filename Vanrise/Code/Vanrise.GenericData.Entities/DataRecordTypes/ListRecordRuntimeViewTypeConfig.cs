f404b8a0d9c576bf3af891b944dc81da5fd77c91
Mohammad Sheaib <VANRISE\mohammad.sheaib>
2019-03-21 08:44:24 +0000

    DataRecordTypeList ExtensionConfiguration
    git-tfs-id: [http://developmentvlan:8585/tfs/vanrise.collection]$/;C54602

---
+++ Vanrise/Code/Vanrise.GenericData.Entities/DataRecordTypes/ListRecordRuntimeViewTypeConfig.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using Vanrise.Entities;
+namespace Vanrise.GenericData.Entities
+{
+    public class ListRecordRuntimeViewTypeConfig : ExtensionConfiguration
+    {
+        public const string EXTENSION_TYPE = "VR_GenericData_ListRecordRuntimeViewType";
+        public string Editor { get; set; }
+        public string RuntimeEditor { get; set; }
+        public string FilterEditor { get; set; }
+        public bool IsSupportedInGenericRule { get; set; }
+        public string RuleFilterEditor { get; set; }
+    }
+}
