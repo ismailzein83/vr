9d80b2e4731240d9a1ec30f07238f540f394b96c
Hassan Makhour <VANRISE\hassan.makhour>
2019-03-21 07:21:42 +0000

    openBPInstanceViewer class
    git-tfs-id: [http://developmentvlan:8585/tfs/vanrise.collection]$/;C54581

---
+++ BusinessProcess/Code/Vanrise.BusinessProcess.Business/Extensions/OpenBPInstanceViewerAction.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Vanrise.GenericData.Business;
+
+namespace Vanrise.BusinessProcess.Business.Extensions
+{
+	public class OpenBPInstanceViewerAction : GenericBEActionSettings
+	{
+		public override string ActionKind {
+			get { return "OpenBPInstanceViewer"; }
+
+		}
+		public override Guid ConfigId {
+			get { return new Guid("0AD405A3-9BCF-4796-9A1A-6CC2C3F9AAA3"); }
+		}
+
+
+	}
+}
+
