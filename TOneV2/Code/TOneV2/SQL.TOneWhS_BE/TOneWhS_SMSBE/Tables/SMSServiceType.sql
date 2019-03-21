296457175cad0eee17f182f4ab336195ef59bed7
Samer Haidar <VANRISE\samer.haidar>
2019-03-21 09:07:07 +0000

    Moving some scripts and handling some issues
    git-tfs-id: [http://developmentvlan:8585/tfs/vanrise.collection]$/;C54605

---
+++ TOneV2/Code/TOneV2/SQL.TOneWhS_BE/TOneWhS_SMSBE/Tables/SMSServiceType.sql
@@ -0,0 +1,12 @@
+﻿CREATE TABLE [TOneWhS_SMSBE].[SMSServiceType] (
+    [ID]               INT           IDENTITY (1, 1) NOT NULL,
+    [Name]             NVARCHAR (40) NULL,
+    [CreatedTime]      DATETIME      NULL,
+    [LastModifiedTime] DATETIME      NULL,
+    [CreatedBy]        INT           NULL,
+    [LastModifiedBy]   INT           NULL,
+    [timestamp]        ROWVERSION    NULL,
+    [Symbol]           NVARCHAR (40) NULL,
+    PRIMARY KEY CLUSTERED ([ID] ASC)
+);
+
