dd17f4e9628bdb82151af7732adf359125fcee8a
Fatima Al Heck <VANRISE\fatima.alheck>
2019-03-22 15:18:28 +0000

    Editing SaleZoneGroupMatchingSupplierDeals class
    git-tfs-id: [http://developmentvlan:8585/tfs/vanrise.collection]$/;C54689

--- TOneV2/Code/TOneV2/TOne.WhS.BusinessEntity.MainExtensions/SaleZoneGroups/SaleZoneGroupMatchingSupplierDeals.cs
+++ TOneV2/Code/TOneV2/TOne.WhS.BusinessEntity.MainExtensions/SaleZoneGroups/SaleZoneGroupMatchingSupplierDeals.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using TOne.WhS.BusinessEntity.Business;
 using TOne.WhS.BusinessEntity.Entities;
 
 namespace TOne.WhS.BusinessEntity.MainExtensions.SaleZoneGroups
@@ -14,17 +15,39 @@ namespace TOne.WhS.BusinessEntity.MainExtensions.SaleZoneGroups
 
         public override IEnumerable<long> GetZoneIds(ISaleZoneGroupContext context)
         {
-            throw new NotImplementedException();
+            return this.ZoneIds != null ? this.ZoneIds : new List<long>();
         }
 
         public override string GetDescription(ISaleZoneGroupContext context)
         {
-            throw new NotImplementedException();
+            var validZoneIds = context != null ? context.GetGroupZoneIds(this) : this.ZoneIds;
+            if (validZoneIds != null)
+            {
+                SaleZoneManager manager = new SaleZoneManager();
+                return manager.GetDescription(base.SellingNumberPlanId, validZoneIds);
+            }
+            else
+                return null;
         }
 
         public override void CleanDeletedZoneIds(ISaleZoneGroupCleanupContext context)
         {
-            throw new NotImplementedException();
+            context.Result = SaleZoneGroupCleanupResult.NoChange;
+
+            if (this.ZoneIds != null && this.ZoneIds.Count > 0)
+            {
+                foreach (int deletedZoneId in context.DeletedSaleZoneIds)
+                {
+                    if (this.ZoneIds.Contains(deletedZoneId))
+                    {
+                        context.Result = SaleZoneGroupCleanupResult.ZonesUpdated;
+                        this.ZoneIds.Remove(deletedZoneId);
+                    }
+                }
+
+                if (this.ZoneIds.Count == 0)
+                    context.Result = SaleZoneGroupCleanupResult.AllZonesRemoved;
+            }
         }
     }
 }
