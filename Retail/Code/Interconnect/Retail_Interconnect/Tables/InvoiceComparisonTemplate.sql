3c1abb43acee12561f34b88223725a8d963fdedc
Samer Haidar <VANRISE\samer.haidar>
2019-03-22 13:11:39 +0000

    Adding enw table InvoiceComparisonTemplate
    git-tfs-id: [http://developmentvlan:8585/tfs/vanrise.collection]$/;C54679

---
+++ Retail/Code/Interconnect/Retail_Interconnect/Tables/InvoiceComparisonTemplate.sql
@@ -0,0 +1,11 @@
+﻿CREATE TABLE [Retail_Interconnect].[InvoiceComparisonTemplate] (
+    [ID]               BIGINT           IDENTITY (1, 1) NOT NULL,
+    [InvoiceTypeId]    UNIQUEIDENTIFIER NULL,
+    [PartnerId]        NVARCHAR (255)   NULL,
+    [Details]          NVARCHAR (MAX)   NULL,
+    [CreatedTime]      DATETIME         CONSTRAINT [DF_InvoiceComparisonTemplate_CreatedTime] DEFAULT (getdate()) NULL,
+    [timestamp]        ROWVERSION       NULL,
+    [LastModifiedTime] DATETIME         CONSTRAINT [DF_InvoiceComparisonTemplate_LastModifiedTime] DEFAULT (getdate()) NULL,
+    CONSTRAINT [PK_InvoiceComparisonTemplate] PRIMARY KEY CLUSTERED ([ID] ASC)
+);
+
