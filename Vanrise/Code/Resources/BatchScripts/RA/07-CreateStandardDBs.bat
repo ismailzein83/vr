6ce621785192de0df5560f8cb0fb0275de82341a
Wissam Ajamy <VANRISE\wissam.ajamy>
2019-03-25 07:29:01 +0000

    separate RA application
    git-tfs-id: [http://developmentvlan:8585/tfs/vanrise.collection]$/;C54695

--- Vanrise/Code/Resources/BatchScripts/RA/07-CreateStandardDBs.bat
+++ Vanrise/Code/Resources/BatchScripts/RA/07-CreateStandardDBs.bat
@@ -8,3 +8,12 @@ For /F %PARSEARG% %%i in (%CURRDATE%) Do SET YYYYMMDD=%%l%%j%%k
 ECHO RA Create Standard DBs Structure files
 xcopy "C:\TFS\TOneV2\Code\TOneV2\SQL.TOneConfiguration\DBsStructure.txt" /y /v /z /i /Q /R  "\\192.168.110.185\Fixes\WebSite\RA\%YYYYMMDD%\DataBaseFirstDeploymentScripts\DBsStructure\"
 start /b /w /D"C:\TFS\Vanrise\Code\Vanrise.HelperTools\bin\Release" Vanrise.HelperTools.exe "DBs" "RA"
+
+xcopy "C:\TFS\TOneV2\Code\TOneV2\SQL.TOneConfiguration\DBsStructure.txt" /y /v /z /i /Q /R  "\\192.168.110.185\Fixes\WebSite\RA.ICX\%YYYYMMDD%\DataBaseFirstDeploymentScripts\DBsStructure\"
+start /b /w /D"C:\TFS\Vanrise\Code\Vanrise.HelperTools\bin\Release" Vanrise.HelperTools.exe "DBs" "RA.ICX"
+
+xcopy "C:\TFS\TOneV2\Code\TOneV2\SQL.TOneConfiguration\DBsStructure.txt" /y /v /z /i /Q /R  "\\192.168.110.185\Fixes\WebSite\RA.INTL\%YYYYMMDD%\DataBaseFirstDeploymentScripts\DBsStructure\"
+start /b /w /D"C:\TFS\Vanrise\Code\Vanrise.HelperTools\bin\Release" Vanrise.HelperTools.exe "DBs" "RA.INTL"
+
+xcopy "C:\TFS\TOneV2\Code\TOneV2\SQL.TOneConfiguration\DBsStructure.txt" /y /v /z /i /Q /R  "\\192.168.110.185\Fixes\WebSite\RA.Retail\%YYYYMMDD%\DataBaseFirstDeploymentScripts\DBsStructure\"
+start /b /w /D"C:\TFS\Vanrise\Code\Vanrise.HelperTools\bin\Release" Vanrise.HelperTools.exe "DBs" "RA.Retail"
--- Vanrise/Code/Resources/BatchScripts/RA/08-GroupSQLFiles.bat
+++ Vanrise/Code/Resources/BatchScripts/RA/08-GroupSQLFiles.bat
@@ -1,3 +1,6 @@
 ECHO OFF
 ECHO Retail Group SQL Scripts
 start /b /w /D"C:\TFS\Vanrise\Code\Vanrise.HelperTools\bin\Release" Vanrise.HelperTools.exe "GRPSQL" "RA"
+start /b /w /D"C:\TFS\Vanrise\Code\Vanrise.HelperTools\bin\Release" Vanrise.HelperTools.exe "GRPSQLOverridden" "RA.ICX"
+start /b /w /D"C:\TFS\Vanrise\Code\Vanrise.HelperTools\bin\Release" Vanrise.HelperTools.exe "GRPSQLOverridden" "RA.INTL"
+start /b /w /D"C:\TFS\Vanrise\Code\Vanrise.HelperTools\bin\Release" Vanrise.HelperTools.exe "GRPSQLOverridden" "RA.Retail"
