74b800bc54b377a58cadfe714f0e09ad545ee5f1
Fatima Hammoud <VANRISE\fatima.hammoud>
2019-03-26 13:39:04 +0000

    adding some properties
    git-tfs-id: [http://developmentvlan:8585/tfs/vanrise.collection]$/;C54766

--- Vanrise/Code/Vanrise.Security.MainExtensions/GenericBEActions/StartBPProcessAction.cs
+++ Vanrise/Code/Vanrise.Security.MainExtensions/GenericBEActions/StartBPProcessAction.cs
@@ -4,13 +4,22 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using Vanrise.GenericData.Business;
+using Vanrise.GenericData.Entities;
 
 namespace Vanrise.Security.MainExtensions.GenericBEActions
 {
     public class StartBPProcessAction : GenericBEActionSettings
     {
         public override Guid ConfigId { get { return new Guid("0E9730F4-1197-456B-9261-E745FE4AFB3B"); } }
+        public Guid BPDefinitionId { get; set; }
+        public List<GenricBEIdInputFieldName> GenreicBEInputFields { get; set; }
         public override string ActionTypeName { get { return "StartBPProcess"; } }
         public override string ActionKind { get { return "StartBPProcessAction"; } }
+    }
+
+    public class GenricBEIdInputFieldName
+    {
+        public string FieldName { get; set; }
+        public Guid DataRecordTypeId { get; set; }
     }
 }
