cbe89cf410e7ddc7e645a0dbc44b0472afa4c332
Fatima Hammoud <VANRISE\fatima.hammoud>
2019-03-26 13:00:24 +0000

    adding StartBPProcessAction class
    git-tfs-id: [http://developmentvlan:8585/tfs/vanrise.collection]$/;C54759

---
+++ Vanrise/Code/Vanrise.Security.MainExtensions/GenericBEActions/StartBPProcessAction.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Vanrise.GenericData.Business;
+
+namespace Vanrise.Security.MainExtensions.GenericBEActions
+{
+    public class StartBPProcessAction : GenericBEActionSettings
+    {
+        public override Guid ConfigId { get { return new Guid("0E9730F4-1197-456B-9261-E745FE4AFB3B"); } }
+        public override string ActionTypeName { get { return "StartBPProcess"; } }
+        public override string ActionKind { get { return "StartBPProcessAction"; } }
+    }
+}
