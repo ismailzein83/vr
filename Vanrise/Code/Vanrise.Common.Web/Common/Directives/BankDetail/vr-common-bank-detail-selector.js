08d693d3d8c37fc2d6caf38ad699788e4d594b16
Zeinab Mokdad <VANRISE\zeinab.mokdad>
2019-03-26 13:48:39 +0000

    Fixing Bank details drop down
    git-tfs-id: [http://developmentvlan:8585/tfs/vanrise.collection]$/;C54769

--- Vanrise/Code/Vanrise.Common.Web/Common/Directives/BankDetail/vr-common-bank-detail-selector.js
+++ Vanrise/Code/Vanrise.Common.Web/Common/Directives/BankDetail/vr-common-bank-detail-selector.js
@@ -1,121 +1,126 @@
 ﻿'use strict';
 app.directive('vrCommonBankDetailSelector', ['VRCommon_BankDetailAPIService', 'UtilsService', 'VRUIUtilsService', 'VRCommon_BankDetailService',
-function (VRCommon_BankDetailAPIService, UtilsService, VRUIUtilsService, VRCommon_BankDetailService) {
+    function (VRCommon_BankDetailAPIService, UtilsService, VRUIUtilsService, VRCommon_BankDetailService) {
 
-    var directiveDefinitionObject = {
-        restrict: 'E',
-        scope: {
-            onReady: '=',
-            ismultipleselection: "@",
-            isdisabled: "=",
-            onselectionchanged: '=',
-            isrequired: "@",
-            selectedvalues: '=',
-            normalColNum: '@'
+        var directiveDefinitionObject = {
+            restrict: 'E',
+            scope: {
+                onReady: '=',
+                ismultipleselection: "@",
+                isdisabled: "=",
+                onselectionchanged: '=',
+                isrequired: "@",
+                selectedvalues: '=',
+                normalColNum: '@'
 
-        },
-        controller: function ($scope, $element, $attrs) {
+            },
+            controller: function ($scope, $element, $attrs) {
 
-            var ctrl = this;
+                var ctrl = this;
 
-            ctrl.selectedvalues;
-            if ($attrs.ismultipleselection != undefined)
-                ctrl.selectedvalues = [];
+                ctrl.selectedvalues;
+                if ($attrs.ismultipleselection != undefined)
+                    ctrl.selectedvalues = [];
 
-            ctrl.datasource = [];
+                ctrl.datasource = [];
 
 
-            $scope.addNewBank = function () {
-                var onBankAdded = function (bankObj) {
-                    ctrl.datasource.push(bankObj);
-                    if ($attrs.ismultipleselection != undefined)
-                        ctrl.selectedvalues.push(bankObj);
-                    else
-                        ctrl.selectedvalues = bankObj;
+                $scope.addNewBank = function () {
+                    var onBankAdded = function (bankObj) {
+                        var bankDetailsInfo =
+                        {
+                            BankDetailId: bankObj.BankDetailId,
+                            ChannelName: bankObj.ChannelName == undefined ? bankObj.Bank : bankObj.ChannelName
+                        };
+                        ctrl.datasource.push(bankDetailsInfo);
+                        if ($attrs.ismultipleselection != undefined)
+                            ctrl.selectedvalues.push(bankDetailsInfo);
+                        else
+                            ctrl.selectedvalues = bankDetailsInfo;
+                    };
+                    VRCommon_BankDetailService.addBankDetail(onBankAdded, true);
                 };
-                VRCommon_BankDetailService.addBankDetail(onBankAdded, true);
-            };
 
-            var ctor = new bankDetailCtor(ctrl, $scope, $attrs);
-            ctor.initializeController();
-        },
-        controllerAs: 'ctrl',
-        bindToController: true,
-        compile: function (element, attrs) {
-            return {
-                pre: function ($scope, iElem, iAttrs, ctrl) {
+                var ctor = new bankDetailCtor(ctrl, $scope, $attrs);
+                ctor.initializeController();
+            },
+            controllerAs: 'ctrl',
+            bindToController: true,
+            compile: function (element, attrs) {
+                return {
+                    pre: function ($scope, iElem, iAttrs, ctrl) {
 
-                }
-            };
-        },
-        template: function (element, attrs) {
-            return getTemplate(attrs);
+                    }
+                };
+            },
+            template: function (element, attrs) {
+                return getTemplate(attrs);
+            }
+
+        };
+
+
+        function getTemplate(attrs) {
+
+            var multipleselection = "";
+            var label = "Bank Details";
+            if (attrs.ismultipleselection != undefined) {
+                label = "Bank Details";
+                multipleselection = "ismultipleselection";
+            }
+
+            var required = "";
+            if (attrs.isrequired != undefined)
+                required = "isrequired";
+
+            var addCliked = '';
+            if (attrs.showaddbutton != undefined)
+                addCliked = 'onaddclicked="addNewBank"';
+
+            return '<vr-columns colnum="{{ctrl.normalColNum}}" >'
+                + '<vr-select ' + multipleselection + ' ' + addCliked + ' datatextfield="ChannelName" datavaluefield="BankDetailId" '
+                + required + ' label="' + label + '" datasource="ctrl.datasource" selectedvalues="ctrl.selectedvalues"   onselectionchanged="ctrl.onselectionchanged" on-ready="onSelectorReady"></vr-select>'
+                + '</vr-columns>';
         }
 
-    };
+        function bankDetailCtor(ctrl, $scope, $attrs) {
+            var selectorAPI;
+            function initializeController() {
+                $scope.onSelectorReady = function (api) {
+                    selectorAPI = api;
+                    defineAPI();
+                };
+            }
 
-
-    function getTemplate(attrs) {
-
-        var multipleselection = "";
-        var label = "Bank Details";
-        if (attrs.ismultipleselection != undefined) {
-            label = "Bank Details";
-            multipleselection = "ismultipleselection";
-        }
-
-        var required = "";
-        if (attrs.isrequired != undefined)
-            required = "isrequired";
-
-        var addCliked = '';
-        if (attrs.showaddbutton != undefined)
-            addCliked = 'onaddclicked="addNewBank"';
-
-        return '<vr-columns colnum="{{ctrl.normalColNum}}" >'
-            + '<vr-select ' + multipleselection + ' ' + addCliked + ' datatextfield="ChannelName" datavaluefield="BankDetailId" '
-        + required + ' label="' + label + '" datasource="ctrl.datasource" selectedvalues="ctrl.selectedvalues"   onselectionchanged="ctrl.onselectionchanged" on-ready="onSelectorReady"></vr-select>'
-           + '</vr-columns>';
-    }
-
-    function bankDetailCtor(ctrl, $scope, $attrs) {
-        var selectorAPI;
-        function initializeController() {
-            $scope.onSelectorReady = function (api) {
-                selectorAPI = api;
-                defineAPI();
-            };
-        }
-
-        function defineAPI() {
-            var api = {};
-            api.getSelectedIds = function () {
-                return VRUIUtilsService.getIdSelectedIds('BankDetailId', $attrs, ctrl);
-            };
-            api.load = function (payload) {
-                selectorAPI.clearDataSource();
-                var selectedIds;
-                var selectIfSingleItem;
-                if (payload != undefined) {
-                    selectedIds = payload.selectedIds;
-                    selectIfSingleItem = payload.selectifsingleitem;
-                }
-                return VRCommon_BankDetailAPIService.GetBankDetailsInfo().then(function (response) {
-                    angular.forEach(response, function (item) {
+            function defineAPI() {
+                var api = {};
+                api.getSelectedIds = function () {
+                    return VRUIUtilsService.getIdSelectedIds('BankDetailId', $attrs, ctrl);
+                };
+                api.load = function (payload) {
+                    selectorAPI.clearDataSource();
+                    var selectedIds;
+                    var selectIfSingleItem;
+                    if (payload != undefined) {
+                        selectedIds = payload.selectedIds;
+                        selectIfSingleItem = payload.selectifsingleitem;
+                    }
+                    return VRCommon_BankDetailAPIService.GetBankDetailsInfo().then(function (response) {
+                        angular.forEach(response, function (item) {
                             ctrl.datasource.push(item);
+                        });
+                        if (selectedIds != undefined)
+                            VRUIUtilsService.setSelectedValues(selectedIds, 'BankDetailId', $attrs, ctrl);
+                        else if (selectedIds == undefined && selectIfSingleItem)
+                            selectorAPI.selectIfSingleItem();
+
                     });
-                    if (selectedIds != undefined)
-                        VRUIUtilsService.setSelectedValues(selectedIds, 'BankDetailId', $attrs, ctrl);
-                    else if (selectedIds == undefined && selectIfSingleItem)
-                        selectorAPI.selectIfSingleItem();
+                };
 
-                });
-            };
-
-            if (ctrl.onReady != null)
-                ctrl.onReady(api);
+                if (ctrl.onReady != null)
+                    ctrl.onReady(api);
+            }
+            this.initializeController = initializeController;
         }
-        this.initializeController = initializeController;
-    }
-    return directiveDefinitionObject;
-}]);
+        return directiveDefinitionObject;
+    }]);
