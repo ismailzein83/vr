7523a2f59d769405f47481eb70f11c2d46b1f89e
yara.bousalman <yara.bousalman@vanrise.tfs.local>
2019-03-28 09:15:47 +0000

    Adding with no lock to Assign query
    git-tfs-id: [http://developmentvlan:8585/tfs/vanrise.collection]$/;C54844

--- TOneV2/Code/TOneV2/SQL.TOneTransaction/bp/Stored Procedures/sp_BPTask_Assign.sql
+++ TOneV2/Code/TOneV2/SQL.TOneTransaction/bp/Stored Procedures/sp_BPTask_Assign.sql
@@ -8,10 +8,10 @@ CREATE PROCEDURE [bp].[sp_BPTask_Assign]
 	@UserId int
 AS
 BEGIN
-	IF NOT EXISTS(SELECT 1 from [bp].[BPTask] where ID = @TaskID AND [TakenBy] IS NOT NULL)
+	IF NOT EXISTS(SELECT 1 from [bp].[BPTask] WITH(NOLOCK) where ID = @TaskID AND [TakenBy] IS NOT NULL)
 	BEGIN
 	Update [bp].[BPTask] set [TakenBy] = @UserId,
 							 [LastUpdatedTime] = GETDATE()
-	where ID = @TaskID AND NOT EXISTS(SELECT 1 from [bp].[BPTask] where ID = @TaskID AND [TakenBy] IS NOT NULL)
+	where ID = @TaskID AND NOT EXISTS(SELECT 1 from [bp].[BPTask] WITH(NOLOCK) where ID = @TaskID AND [TakenBy] IS NOT NULL)
 	END
 END
