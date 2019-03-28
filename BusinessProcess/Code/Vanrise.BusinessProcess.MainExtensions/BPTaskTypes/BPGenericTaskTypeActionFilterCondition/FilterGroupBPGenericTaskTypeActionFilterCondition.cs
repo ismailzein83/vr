746f1a8fd1b3f81bbb47ef47b0d39c76ef255ea0
yara.bousalman <yara.bousalman@vanrise.tfs.local>
2019-03-28 14:47:29 +0000

    Adding task type action filter implementations
    git-tfs-id: [http://developmentvlan:8585/tfs/vanrise.collection]$/;C54868

---
+++ BusinessProcess/Code/Vanrise.BusinessProcess.MainExtensions/BPTaskTypes/BPGenericTaskTypeActionFilterCondition/FilterGroupBPGenericTaskTypeActionFilterCondition.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Vanrise.BusinessProcess.Entities;
+
+namespace Vanrise.BusinessProcess.MainExtensions
+{
+    public class FilterGroupBPGenericTaskTypeActionFilterCondition : BPGenericTaskTypeActionFilterCondition
+    {
+        public override Guid ConfigId { get { return new Guid("68DEA8B2-8817-40EE-977D-4EE683CE5092"); } }
+
+        public override bool IsFilterMatch(IBPGenericTaskTypeActionFilterConditionContext context)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ BusinessProcess/Code/Vanrise.BusinessProcess.MainExtensions/BPTaskTypes/BPGenericTaskTypeActionFilterCondition/TakenByBPGenericTaskTypeActionFilterCondition.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Vanrise.BusinessProcess.Entities;
+
+namespace Vanrise.BusinessProcess.MainExtensions
+{
+    public class TakenByBPGenericTaskTypeActionFilterCondition : BPGenericTaskTypeActionFilterCondition
+    {
+        public override Guid ConfigId { get { return new Guid("9B435BE4-43CD-4942-B5D3-95C8927D6885"); } }
+
+        public override bool IsFilterMatch(IBPGenericTaskTypeActionFilterConditionContext context)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
