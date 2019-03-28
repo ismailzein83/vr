90002f6bbe0e96e6564d3e7ab111dac277af42bd
yara.bousalman <yara.bousalman@vanrise.tfs.local>
2019-03-28 15:53:25 +0000

    Adding filter condition implementations
    git-tfs-id: [http://developmentvlan:8585/tfs/vanrise.collection]$/;C54871

---
+++ BusinessProcess/Code/Vanrise.BusinessProcess.Web/BusinessProcess/Directives/MainExtensions/BPGenericTaskTypeActionFilterCondition/bp-generictasktypeactionfiltercondition-filtergroup.js
@@ -0,0 +1,58 @@
+﻿'use strict';
+
+app.directive('bpGenerictasktypeactionfilterconditionFiltergroup', ['UtilsService', 'VRUIUtilsService',
+    function (UtilsService, VRUIUtilsService) {
+
+        var directiveDefinitionObject = {
+            restrict: 'E',
+            scope: {
+                onReady: '='
+            },
+            controller: function ($scope, $element, $attrs) {
+                var ctrl = this;
+                var ctor = new FilterCondition(ctrl, $scope, $attrs);
+                ctor.initializeController();
+            },
+            controllerAs: 'ctrl',
+            bindToController: true,
+            compile: function (element, attrs) {
+                return {
+
+                };
+            },
+            templateUrl: "/Client/Modules/BusinessProcess/Directives/MainExtensions/BPGenericTaskTypeActionFilterCondition/Templates/FilterGroupFilterCondition.html"
+        };
+
+        function FilterCondition(ctrl, $scope, attrs) {
+            this.initializeController = initializeController;
+
+
+            function initializeController() {
+                $scope.scopeModel = {};
+
+
+                defineAPI();
+            }
+
+            function defineAPI() {
+                var api = {};
+
+                api.load = function (payload) {
+                    var promises = [];
+
+                    return UtilsService.waitMultiplePromises(promises);
+                };
+
+                api.getData = function () {
+                    return {
+                        $type: "Vanrise.BusinessProcess.MainExtensions.BPTaskTypes.FilterGroupBPGenericTaskTypeActionFilterCondition, Vanrise.BusinessProcess.MainExtensions"
+                    };
+                };
+
+                if (ctrl.onReady != null)
+                    ctrl.onReady(api);
+            }
+        }
+
+        return directiveDefinitionObject;
+    }]);
---
+++ BusinessProcess/Code/Vanrise.BusinessProcess.Web/BusinessProcess/Directives/MainExtensions/BPGenericTaskTypeActionFilterCondition/bp-generictasktypeactionfiltercondition-istaken.js
@@ -0,0 +1,58 @@
+﻿'use strict';
+
+app.directive('bpGenerictasktypeactionfilterconditionIstaken', ['UtilsService', 'VRUIUtilsService',
+    function (UtilsService, VRUIUtilsService) {
+
+        var directiveDefinitionObject = {
+            restrict: 'E',
+            scope: {
+                onReady: '='
+            },
+            controller: function ($scope, $element, $attrs) {
+                var ctrl = this;
+                var ctor = new FilterCondition(ctrl, $scope, $attrs);
+                ctor.initializeController();
+            },
+            controllerAs: 'ctrl',
+            bindToController: true,
+            compile: function (element, attrs) {
+                return {
+
+                };
+            },
+            templateUrl: "/Client/Modules/BusinessProcess/Directives/MainExtensions/BPGenericTaskTypeActionFilterCondition/Templates/IsTakenFilterCondition.html"
+        };
+
+        function FilterCondition(ctrl, $scope, attrs) {
+            this.initializeController = initializeController;
+
+
+            function initializeController() {
+                $scope.scopeModel = {};
+
+
+                defineAPI();
+            }
+
+            function defineAPI() {
+                var api = {};
+
+                api.load = function (payload) {
+                    var promises = [];
+
+                    return UtilsService.waitMultiplePromises(promises);
+                };
+
+                api.getData = function () {
+                    return {
+                        $type: "Vanrise.BusinessProcess.MainExtensions.BPTaskTypes.IsTakenBPGenericTaskTypeActionFilterCondition, Vanrise.BusinessProcess.MainExtensions"
+                    };
+                };
+
+                if (ctrl.onReady != null)
+                    ctrl.onReady(api);
+            }
+        }
+
+        return directiveDefinitionObject;
+    }]);
