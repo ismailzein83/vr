02fea1f3ebc03117e9fa89219a85a2057355c21f
Fatima Hammoud <VANRISE\fatima.hammoud>
2019-03-28 09:15:51 +0000

    adding ListAccountActionSettings class
    git-tfs-id: [http://developmentvlan:8585/tfs/vanrise.collection]$/;C54845

---
+++ Retail/Code/Retail.BusinessEntity.MainExtensions/AccountBEActionTypes/ListAccountActionSettings.cs
@@ -0,0 +1,18 @@
+﻿using Retail.BusinessEntity.Business;
+using Retail.BusinessEntity.Entities;
+using System;
+using System.Collections.Generic;
+
+namespace Retail.BusinessEntity.MainExtensions.AccountBEActionTypes
+{
+    public class ListAccountActionSettings : AccountActionDefinitionSettings
+    {
+        public override Guid ConfigId { get { return new Guid("32ABDAB1-2995-4BBE-8262-EA4D2045F932"); } }
+        public override string ClientActionName { get { return "ListAccountAction"; } }
+        public override bool DoesUserHaveAccess(IAccountActionDefinitionCheckAccessContext context)
+        {
+            return new AccountBEDefinitionManager().DoesUserHaveEditAccess(context.UserId, context.AccountBEDefinitionId);
+        }
+        public List<AccountActionDefinition> ActionDefinitions { get; set; }
+    }
+}
