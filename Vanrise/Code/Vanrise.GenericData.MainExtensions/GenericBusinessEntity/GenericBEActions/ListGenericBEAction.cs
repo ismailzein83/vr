4c17a947ff2fe1918d51ba07b4df1ba4db6bc257
Fatima Hammoud <VANRISE\fatima.hammoud>
2019-03-28 07:43:06 +0000

    adding ListGenericBEAction class
    git-tfs-id: [http://developmentvlan:8585/tfs/vanrise.collection]$/;C54834

---
+++ Vanrise/Code/Vanrise.GenericData.MainExtensions/GenericBusinessEntity/GenericBEActions/ListGenericBEAction.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Vanrise.GenericData.Business;
+
+namespace Vanrise.GenericData.MainExtensions.GenericBusinessEntity.GenericBEActions
+{
+    public class ListGenericBEAction : GenericBEActionSettings
+    {
+        public override Guid ConfigId { get { return new Guid("1E1C7DD8-29BE-4614-8F06-C9DC4D0D3E3E"); } }
+        public override string ActionKind { get { return "ListGenericBEAction"; } }
+        public override string ActionTypeName { get { return "ListGenericBEAction"; } }
+        public List<GenericBEAction> GenericBEActions { get; set; }
+    }
+}
