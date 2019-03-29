1251f0dc5b56160b3e8b2984b1e6c73e7dff70a7
Ayman Zinji <VANRISE\ayman.zinji>
2019-03-29 13:48:38 +0000

    adding GetRequestNextId
    git-tfs-id: [http://developmentvlan:8585/tfs/vanrise.collection]$/;C54890

--- SOM/Code/BPMExtended.Main.Business/Billing/IDManager.cs
+++ SOM/Code/BPMExtended.Main.Business/Billing/IDManager.cs
@@ -27,21 +27,16 @@ namespace BPMExtended.Main.Business
             }
             return item;
         }
-        //public static string GetRandomCustomerId()
-        //{
-        //    string CUSTOMER_ID_1 = "2408EDDB-ABAB-4507-905C-20386B7EC106";
-        //    string CUSTOMER_ID_2 = "C0A5D017-9343-43B9-9820-0D813C74B1F4";
-        //    string CUSTOMER_ID_3 = "448C0FB8-536E-41D0-A178-1218261E6252";
 
-        //    List<string> customerIds = new List<string>();
-        //    customerIds.Add(CUSTOMER_ID_1);
-        //    customerIds.Add(CUSTOMER_ID_2);
-        //    customerIds.Add(CUSTOMER_ID_3);
+        public string GetRequestNextId()
+        {
+            string item;
 
-        //    Random rnd = new Random();
-        //    int index = rnd.Next(0, 2);
-
-        //    return customerIds[index];
-        //}
+            using (SOMClient client = new SOMClient())
+            {
+                item = client.Get<string>(String.Format("api/SOM.ST/Common/GetRequestNextId"));
+            }
+            return item;
+        }
     }
 }
