a22773211099fdb7e7489a6fd5adefa094632440
Ali Slayman <VANRISE\ali.slayman>
2019-04-01 08:46:33 +0000

    add timestamp to [common].[ProcessState]
    git-tfs-id: [http://developmentvlan:8585/tfs/vanrise.collection]$/;C54924

--- TOneV2/Code/TOneV2/SQL.TOneTransaction/common/Tables/ProcessState.sql
+++ TOneV2/Code/TOneV2/SQL.TOneTransaction/common/Tables/ProcessState.sql
@@ -1,10 +1,13 @@
 ﻿CREATE TABLE [common].[ProcessState] (
     [UniqueName]       VARCHAR (255)  NOT NULL,
     [Settings]         NVARCHAR (MAX) NOT NULL,
-    [LastModifiedTime] DATETIME       CONSTRAINT [DF_ProcessState_LastModifiedTime] DEFAULT (getdate()) NULL,
-    [CreatedTime]      DATETIME       CONSTRAINT [DF_ProcessState_CreatedTime] DEFAULT (getdate()) NULL,
+    [LastModifiedTime] DATETIME       CONSTRAINT [DF_ProcessState_LastModifiedTime] DEFAULT (getdate()) NOT NULL,
+    [CreatedTime]      DATETIME       CONSTRAINT [DF_ProcessState_CreatedTime] DEFAULT (getdate()) NOT NULL,
+    [timestamp]        ROWVERSION     NULL,
     CONSTRAINT [PK_ProcessState] PRIMARY KEY CLUSTERED ([UniqueName] ASC)
 );
+
+
 
 
 
