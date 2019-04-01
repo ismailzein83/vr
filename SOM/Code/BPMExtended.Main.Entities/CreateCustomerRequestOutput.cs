901683c0f6767f12494d9cc66f3274a7092bd592
Rodi Hassan <VANRISE\rodi.hassan>
2019-04-01 09:53:56 +0000

    implement classes for service responses
    git-tfs-id: [http://developmentvlan:8585/tfs/vanrise.collection]$/;C54934

--- SOM/Code/BPMExtended.Main.Entities/CreateCustomerRequestOutput.cs
+++ SOM/Code/BPMExtended.Main.Entities/CreateCustomerRequestOutput.cs
@@ -1,12 +1,11 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace BPMExtended.Main.Entities
 {
     public class CreateCustomerRequestOutput
     {
+        public int CustomerId { get; set; }
+        public string   PublicCustomerId { get; set; }
+        public int AddressId { get; set; }
     }
 }
---
+++ SOM/Code/BPMExtended.Main.SOM.Entities/Billing/ServiceResponse/PaymentPlanTemplate.cs
@@ -0,0 +1,10 @@
+﻿using System;
+
+namespace BPMExtended.Main.SOMAPI
+{
+    public class PaymentPlanTemplate
+    {
+        public string Id { get; set; }
+        public string Name { get; set; }
+    }
+}
