57e9550990b40c26545f570354ceb2ce5df990de
Fatima Al Heck <VANRISE\fatima.alheck>
2019-04-01 15:36:47 +0000

    Adding SMSInvalidandRaw & SMSProblems views and adding 'OriginationNumberPrefix', 'DestinationMCC', 'DestinationMNC', 'DestinationNumberPrefix' fields to BillingSMS
    git-tfs-id: [http://developmentvlan:8585/tfs/vanrise.collection]$/;C54954

---
+++ TOneV2/Code/TOneV2/SQL.TOneSMS/TOneWhS_SMS/Views/vw_SMSInvalidAndRaw.sql
@@ -0,0 +1,8 @@
+﻿CREATE VIEW [TOneWhS_SMS].[vw_SMSInvalidAndRaw]
+AS
+SELECT invalidSMS.SMSId, invalidSMS.SwitchId as Switch, invalidSMS.SentDateTime, invalidSMS.CustomerID, invalidSMS.SupplierID, invalidSMS.DestinationMCC,
+	   invalidSMS.DestinationMNC, invalidSMS.DestinationMN_Id, invalidSMS.Receiver, invalidSMS.OrigReceiverOut, invalidSMS.CostRateId, 
+	   invalidSMS.SaleRateId, invalidSMS.SaleFinancialAccountId, invalidSMS.CostFinancialAccountId, 
+	   sms.IN_CARRIER as InCarrier, sms.OUT_CARRIER as OutCarrier
+FROM   TOneWhS_SMS.BillingSMS_Invalid AS invalidSMS WITH (NOLOCK) 
+INNER JOIN  TOneWhS_SMS.SMS AS sms WITH (NOLOCK) ON invalidSMS.SMSId = sms.Id
---
+++ TOneV2/Code/TOneV2/SQL.TOneSMS/TOneWhS_SMS/Views/vw_SMSProblems.sql
@@ -0,0 +1,16 @@
+﻿
+
+
+
+
+CREATE VIEW [TOneWhS_SMS].[vw_SMSProblems]
+AS
+SELECT SMSId, SwitchId, SentDateTime, CustomerID, SupplierID, DestinationMCC, DestinationMNC, DestinationMN_Id, Receiver, OrigReceiverOut, CostRateId, 
+	   SaleRateId, SaleFinancialAccountId, CostFinancialAccountId
+FROM  TOneWhS_SMS.BillingSMS_Invalid WITH (NOLOCK)
+
+UNION ALL
+
+SELECT SMSId, SwitchId, SentDateTime, CustomerID, SupplierID, DestinationMCC, DestinationMNC, DestinationMN_Id, Receiver, OrigReceiverOut, CostRateID, 
+	   SaleRateID, SaleFinancialAccountId, CostFinancialAccountId
+FROM  TOneWhS_SMS.BillingSMS_PartialPriced WITH (NOLOCK)
