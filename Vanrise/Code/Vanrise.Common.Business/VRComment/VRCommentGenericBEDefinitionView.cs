b511bfba83f0807368db0999abf2f261170caef0
Mohammad Sheaib <VANRISE\mohammad.sheaib>
2019-04-01 14:17:59 +0000

    Comment View Generic BE Subview
    git-tfs-id: [http://developmentvlan:8585/tfs/vanrise.collection]$/;C54949

---
+++ Vanrise/Code/Vanrise.Common.Business/VRComment/VRCommentGenericBEDefinitionView.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Vanrise.GenericData.Business;
+
+namespace Vanrise.GenericData.MainExtensions
+{
+    public class VRCommentGenericBEDefinitionView : GenericBEViewDefinitionSettings
+    {
+        public override Guid ConfigId
+        {
+            get { return new Guid("F0FCE857-0119-4895-9459-648E14EFA60A"); }
+        }
+
+        public override string RuntimeDirective
+        {
+            get { return "vr-common-comment-genericbeview-runtime"; }
+        }
+        public override bool DoesUserHaveAccess(IGenericBEViewDefinitionCheckAccessContext context)
+        {
+            return base.DoesUserHaveAccess(context);
+        }
+        public Guid CommentBEDefinitionId { get; set; }
+    }
+}
