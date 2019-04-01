6000ff64403058ac278bfbfed2b53a5b794f59e4
Ali Slayman <VANRISE\ali.slayman>
2019-04-01 08:44:47 +0000

    remove process state from configuration DB
    git-tfs-id: [http://developmentvlan:8585/tfs/vanrise.collection]$/;C54923

--- TOneV2/Code/TOneV2/SQL.TOneConfiguration/common/Stored Procedures/sp_ProcessState_GetAll.sql
+++
@@ -1,7 +0,0 @@
-﻿
-CREATE PROCEDURE [common].[sp_ProcessState_GetAll]
-AS
-BEGIN
-	Select UniqueName, Settings 
-	from common.ProcessState WITH(NOLOCK)
-END
--- TOneV2/Code/TOneV2/SQL.TOneConfiguration/common/Tables/ProcessState.sql
+++
@@ -1,8 +0,0 @@
-﻿CREATE TABLE [common].[ProcessState] (
-    [UniqueName] VARCHAR (255)  NOT NULL,
-    [Settings]   NVARCHAR (MAX) NOT NULL,
-    CONSTRAINT [PK_ProcessState] PRIMARY KEY CLUSTERED ([UniqueName] ASC)
-);
-
-
-
