a443c94fd37c37da3a81dfb6a5bbea7dca00e8a0
Mohammad Sheaib <VANRISE\mohammad.sheaib>
2019-04-02 07:55:06 +0000

    Comment Generic BE View(Done)
    git-tfs-id: [http://developmentvlan:8585/tfs/vanrise.collection]$/;C54966

---
+++ Vanrise/Code/Vanrise.Common.Web/Common/Directives/VRComment/vr-common-comment-genericbeview-definition.js
@@ -0,0 +1,79 @@
+﻿'use strict';
+
+app.directive('vrCommonCommentGenericbeviewDefinition', ['VRCommon_VRCommentAPIService', 'UtilsService', 'VRUIUtilsService',
+
+    function (VRCommon_VRCommentAPIService, UtilsService, VRUIUtilsService) {
+        return {
+            restrict: 'E',
+            scope: {
+                onReady: '='
+            },
+            controller: function ($scope, $element, $attrs) {
+
+                var ctrl = this;
+
+                if ($attrs.ismultipleselection != undefined)
+                    ctrl.selectedvalues = [];
+
+                var comment = new Comment(ctrl, $scope, $attrs);
+                comment.initializeController();
+
+            },
+            controllerAs: 'ctrl',
+            bindToController: true,
+            templateUrl: '/Client/Modules/Common/Directives/VRComment/Templates/CommentGenericBEViewDefinition.html'
+        };
+
+        function Comment(ctrl, $scope, attrs) {
+            var beDefinitionSelectorApi;
+            var beDefinitionSelectorPromiseDeferred = UtilsService.createPromiseDeferred();
+            $scope.scopeModel = {};
+            this.initializeController = initializeController;
+
+            function initializeController() {
+
+                $scope.scopeModel.onBusinessEntityDefinitionSelectorReady = function (api) {
+                    beDefinitionSelectorApi = api;
+                    beDefinitionSelectorPromiseDeferred.resolve();
+                };
+                defineApi();
+            }
+            function defineApi() {
+                var api = {};
+
+                api.load = function (payload) {
+                   
+                    var rootPromiseNode = {
+                        promises: [loadBusinessEntityDefinitionSelector()]
+                    };
+                    function loadBusinessEntityDefinitionSelector() {
+                        var businessEntityDefinitionSelectorLoadDeferred = UtilsService.createPromiseDeferred();
+
+                        beDefinitionSelectorPromiseDeferred.promise.then(function () {
+                            var payloadSelector = {
+                                selectedIds: (payload != undefined && payload.parameterEntity != undefined) ? payload.parameterEntity.CommentBEDefinitionId : undefined,
+                                filter: {
+                                    Filters: [{
+                                        $type: "Vanrise.Common.Business.CommentBEDefinitionFilter, Vanrise.Common.Business"
+                                    }]
+                                }
+                            };
+                            VRUIUtilsService.callDirectiveLoad(beDefinitionSelectorApi, payloadSelector, businessEntityDefinitionSelectorLoadDeferred);
+                        });
+                        return businessEntityDefinitionSelectorLoadDeferred.promise;
+                    }
+                    return UtilsService.waitPromiseNode(rootPromiseNode);
+                };
+                api.getData = function () {
+
+                    return {
+                        $type: "Vanrise.Common.Business.VRCommentGenericBEDefinitionView, Vanrise.Common.Business",
+                        CommentBEDefinitionId: beDefinitionSelectorApi.getSelectedIds()
+                    };
+                };
+                if (ctrl.onReady != undefined && typeof (ctrl.onReady) == 'function') {
+                    ctrl.onReady(api);
+                }
+            }
+        }
+    }]);
---
+++ Vanrise/Code/Vanrise.Common.Web/Common/Directives/VRComment/vr-common-comment-genericbeview-runtime.js
@@ -0,0 +1,77 @@
+﻿'use strict';
+
+app.directive('vrCommonCommentGenericbeviewRuntime', ['VRCommon_VRCommentAPIService', 'UtilsService', 'VRUIUtilsService',
+
+    function (VRCommon_VRCommentAPIService, UtilsService, VRUIUtilsService) {
+        return {
+            restrict: 'E',
+            scope: {
+                onReady: '='
+            },
+            controller: function ($scope, $element, $attrs) {
+
+                var ctrl = this;
+
+                if ($attrs.ismultipleselection != undefined)
+                    ctrl.selectedvalues = [];
+
+                var comment = new Comment(ctrl, $scope, $attrs);
+                comment.initializeController();
+
+            },
+            controllerAs: 'ctrl',
+            bindToController: true,
+            templateUrl: '/Client/Modules/Common/Directives/VRComment/Templates/CommentGenericBEViewRuntime.html'
+        };
+
+        function Comment(ctrl, $scope, attrs) {
+
+            var vrCommentGridAPI;
+            var vrCommentGridReadyDeferred = UtilsService.createPromiseDeferred();
+
+            $scope.scopeModel = {};
+            this.initializeController = initializeController;
+
+            function initializeController() {
+
+                $scope.scopeModel.onVRCommentReady = function (api) {
+                    vrCommentGridAPI = api;
+                    vrCommentGridReadyDeferred.resolve();
+                };
+
+                defineApi();
+            }
+
+            function loadVRComment(commentPayload) {
+                var vrCommentLoadDeferred = UtilsService.createPromiseDeferred();
+                vrCommentGridReadyDeferred.promise.then(function () {
+
+                    VRUIUtilsService.callDirectiveLoad(vrCommentGridAPI, commentPayload, vrCommentLoadDeferred);
+                });
+                return vrCommentLoadDeferred.promise;
+            }
+            function defineApi() {
+                var api = {};
+
+                api.load = function (payload) {
+                    var commentPayload = {};
+                    
+                    if (payload != undefined) {
+                        var genericBEGridView = payload.genericBEGridView;
+                        commentPayload = {
+                            objectId: payload.genericBusinessEntityId,
+                            definitionId: (genericBEGridView != undefined && genericBEGridView.Settings != undefined) ? genericBEGridView.Settings.CommentBEDefinitionId : undefined
+                        };
+                    }
+                    var rootPromiseNode = {
+                        promises: [loadVRComment(commentPayload)]
+                    };
+                    return UtilsService.waitPromiseNode(rootPromiseNode);
+                };
+
+                if (ctrl.onReady != undefined && typeof (ctrl.onReady) == 'function') {
+                    ctrl.onReady(api);
+                }
+            }
+        };
+    }]);
