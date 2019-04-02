1830495d3cee05751987a6b2aaa708871ec9fe4d
Mohammad Sheaib <VANRISE\mohammad.sheaib>
2019-04-02 05:40:05 +0000

    Comment GenericBE View Class
    git-tfs-id: [http://developmentvlan:8585/tfs/vanrise.collection]$/;C54958

--- Vanrise/Code/Vanrise.Common.Business/VRComment/VRCommentGenericBEDefinitionView.cs
+++ Vanrise/Code/Vanrise.Common.Business/VRComment/VRCommentGenericBEDefinitionView.cs
@@ -5,7 +5,7 @@ using System.Text;
 using System.Threading.Tasks;
 using Vanrise.GenericData.Business;
 
-namespace Vanrise.GenericData.MainExtensions
+namespace Vanrise.Common.Business
 {
     public class VRCommentGenericBEDefinitionView : GenericBEViewDefinitionSettings
     {
