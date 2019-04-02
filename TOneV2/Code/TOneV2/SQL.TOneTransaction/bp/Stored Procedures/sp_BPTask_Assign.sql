dc236855196ab91814050a3a693c976cb4b3c2ad
yara.bousalman <yara.bousalman@vanrise.tfs.local>
2019-04-02 06:54:41 +0000

    Refactoring [bp].[sp_BPTask_Assign]
    git-tfs-id: [http://developmentvlan:8585/tfs/vanrise.collection]$/;C54963

--- TOneV2/Code/TOneV2/SQL.TOneTransaction/bp/Stored Procedures/sp_BPTask_Assign.sql
+++ TOneV2/Code/TOneV2/SQL.TOneTransaction/bp/Stored Procedures/sp_BPTask_Assign.sql
@@ -8,10 +8,7 @@ CREATE PROCEDURE [bp].[sp_BPTask_Assign]
 	@UserId int
 AS
 BEGIN
-	IF NOT EXISTS(SELECT 1 from [bp].[BPTask] WITH(NOLOCK) where ID = @TaskID AND [TakenBy] IS NOT NULL)
-	BEGIN
 	Update [bp].[BPTask] set [TakenBy] = @UserId,
 							 [LastUpdatedTime] = GETDATE()
-	where ID = @TaskID AND NOT EXISTS(SELECT 1 from [bp].[BPTask] WITH(NOLOCK) where ID = @TaskID AND [TakenBy] IS NOT NULL)
-	END
+	where [ID] = @TaskID AND [TakenBy] IS NULL
 END
