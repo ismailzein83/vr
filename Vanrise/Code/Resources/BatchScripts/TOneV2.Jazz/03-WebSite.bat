10ac212fa12519d4dda56ae4cfd867c8eaccb388
Wissam Ajamy <VANRISE\wissam.ajamy>
2019-04-03 17:39:53 +0000

    --rollback --55038: update batch script to get dlls from module and not publish folder
    git-tfs-id: [http://developmentvlan:8585/tfs/vanrise.collection]$/;C55039

--- Vanrise/Code/Resources/BatchScripts/TOneV2.Jazz/03-WebSite.bat
+++ Vanrise/Code/Resources/BatchScripts/TOneV2.Jazz/03-WebSite.bat
@@ -6,8 +6,8 @@ Set PARSEARG="eol=; tokens=1,2,3,4* delims=/, "
 For /F %PARSEARG% %%i in (%CURRDATE%) Do SET YYYYMMDD=%%l%%j%%k
 
 ECHO TOneV2.Jazz WebSite
-xcopy "C:\TFS\TOneV2\Code\TOneV2\TOne.Web\Web.config.JAZZ.exclud" /y /v /z /i /Q /R				"\\192.168.110.185\Fixes\WebSite\TOneV2.Jazz\%YYYYMMDD%\Web.Update.Jazz.config*"
-xcopy "C:\TFS\TOneV2\Code\TOneV2\TOne.WhS.Jazz.Web\WhS_Jazz" /S /E /R /y /v /i /z /Q			"\\192.168.110.185\Fixes\WebSite\TOneV2.Jazz\%YYYYMMDD%\Client\Modules\WhS_Jazz\"
-xcopy "C:\TFS\TOneV2\Code\TOneV2\TOne.WhS.Jazz.Web\bin\TOne.WhS.Jazz*" /S /E /R /y /v /i /z /Q	"\\192.168.110.185\Fixes\WebSite\TOneV2.Jazz\%YYYYMMDD%\Bin\"
-del /s /q /f																					"\\192.168.110.185\Fixes\WebSite\TOneV2.Jazz\%YYYYMMDD%\Bin\*.pdb"
-del /s /q /f																					"\\192.168.110.185\Fixes\WebSite\TOneV2.Jazz\%YYYYMMDD%\Bin\*.config"
+xcopy "C:\TFS\TOneV2\Code\TOneV2\TOne.Web\Web.config.JAZZ.exclud" /y /v /z /i /Q /R	"\\192.168.110.185\Fixes\WebSite\TOneV2.Jazz\%YYYYMMDD%\Web.Update.Jazz.config*"
+xcopy "C:\TFS\TOneV2\Code\TOneV2\TOne.WhS.Jazz.Web\WhS_Jazz" /S /E /R /y /v /i /z /Q	"\\192.168.110.185\Fixes\WebSite\TOneV2.Jazz\%YYYYMMDD%\Client\Modules\WhS_Jazz\"
+xcopy "C:\Publish\TOneV2\bin\TOne.WhS.Jazz*" /S /E /R /y /v /i /z /Q					"\\192.168.110.185\Fixes\WebSite\TOneV2.Jazz\%YYYYMMDD%\Bin\"
+del /s /q /f																			"\\192.168.110.185\Fixes\WebSite\TOneV2.Jazz\%YYYYMMDD%\Bin\*.pdb"
+del /s /q /f																			"\\192.168.110.185\Fixes\WebSite\TOneV2.Jazz\%YYYYMMDD%\Bin\*.config"
