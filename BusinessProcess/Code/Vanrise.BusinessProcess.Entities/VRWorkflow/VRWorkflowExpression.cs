4d06492a2327e7f0b82f154d5b981beb8bcfa737
Ismail Zein <VANRISE\ismail.zein>
2019-04-03 06:24:59 +0000

    added VRWorkflowExpression
    git-tfs-id: [http://developmentvlan:8585/tfs/vanrise.collection]$/;C55008

---
+++ BusinessProcess/Code/Vanrise.BusinessProcess.Entities/VRWorkflow/VRWorkflowExpression.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Vanrise.BusinessProcess.Entities
+{
+    public abstract class VRWorkflowExpression
+    {
+        public abstract Guid ConfigId { get; }
+
+        public abstract string GetCode(IVRWorkflowExpressionGetCodeContext context);
+    }
+
+    public interface IVRWorkflowExpressionGetCodeContext
+    {
+    }
+
+    public class VRWorkflowCodeExpression : VRWorkflowExpression
+    {
+        public override Guid ConfigId => new Guid("46408CC8-8D65-4CAC-B30A-1F625E6DC5ED");
+
+        public string CodeExpression { get; set; }
+
+        public override string GetCode(IVRWorkflowExpressionGetCodeContext context)
+        {
+            return this.CodeExpression;
+        }
+    }
+
+    public class VRWorkflowExpressionJsonConverter : Newtonsoft.Json.JsonConverter
+    {
+        public override bool CanConvert(Type objectType)
+        {
+            return true;
+        }
+
+        public override bool CanWrite
+        {
+            get
+            {
+                return false;
+            }
+        }
+
+        public override object ReadJson(Newtonsoft.Json.JsonReader reader, Type objectType, object existingValue, Newtonsoft.Json.JsonSerializer serializer)
+        {
+            Newtonsoft.Json.Linq.JToken token = Newtonsoft.Json.Linq.JToken.Load(reader);
+
+            if (token.Type == Newtonsoft.Json.Linq.JTokenType.String)
+                return new VRWorkflowCodeExpression { CodeExpression = token.ToObject<string>() };
+            else
+            {
+                return Vanrise.Common.Serializer.Deserialize(token.ToString());
+            }
+        }
+
+        public override void WriteJson(Newtonsoft.Json.JsonWriter writer, object value, Newtonsoft.Json.JsonSerializer serializer)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
