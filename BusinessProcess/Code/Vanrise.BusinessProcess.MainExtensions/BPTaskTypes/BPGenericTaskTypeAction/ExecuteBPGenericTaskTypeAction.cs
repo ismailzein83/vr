c6c96776aeae1a8ccef7fe0382f31046e0e464b4
yara.bousalman <yara.bousalman@vanrise.tfs.local>
2019-04-03 10:40:17 +0000

    Adding properties to ExecuteBPGenericTaskTypeAction
    git-tfs-id: [http://developmentvlan:8585/tfs/vanrise.collection]$/;C55016

--- BusinessProcess/Code/Vanrise.BusinessProcess.MainExtensions/BPTaskTypes/BPGenericTaskTypeAction/ExecuteBPGenericTaskTypeAction.cs
+++ BusinessProcess/Code/Vanrise.BusinessProcess.MainExtensions/BPTaskTypes/BPGenericTaskTypeAction/ExecuteBPGenericTaskTypeAction.cs
@@ -12,5 +12,14 @@ namespace Vanrise.BusinessProcess.MainExtensions
     {
         public override string ActionTypeName { get { return "Execute"; } }
         public override Guid ConfigId { get { return new Guid("36F6C817-A40F-4F8C-A2D6-9377EAF2169D"); } }
+        public List<ExecuteBPGenericTaskTypeActionDefaultFieldValue> DefaultFieldValues { get; set; }
+        public string DecisionMappingField { get; set; }
+        public string NotesMappingField { get; set; }
+
+    }
+    public class ExecuteBPGenericTaskTypeActionDefaultFieldValue
+    {
+        public string FieldName { get; set; }
+        public object FieldValue { get; set; }
     }
 }
