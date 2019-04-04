e5946dd942c58ec7013782c5bf5febb7f4110dbc
Ali Ballouk <VANRISE\ali.ballouk>
2019-04-04 12:25:48 +0000

    exclude setting name from update in SQL PROCEDURE
    git-tfs-id: [http://developmentvlan:8585/tfs/vanrise.collection]$/;C55057

--- TOneV2/Code/TOneV2/SQL.TOneConfiguration/common/Stored Procedures/sp_Setting_Update.sql
+++ TOneV2/Code/TOneV2/SQL.TOneConfiguration/common/Stored Procedures/sp_Setting_Update.sql
@@ -1,14 +1,9 @@
 ﻿CREATE PROCEDURE [common].[sp_Setting_Update]
 	@ID uniqueidentifier,
-	@Name nvarchar(255),
 	@Data nvarchar(max)
 AS
 BEGIN
-IF NOT EXISTS(SELECT 1 FROM common.Setting WHERE ID != @ID and Name = @Name)
-	BEGIN
-		Update common.Setting
-		Set [Name] = @Name,
-		[Data] = @Data
-		Where ID = @ID
-	END
+	Update common.Setting
+	Set [Data] = @Data
+	Where ID = @ID
 END
