47503b586920f9e2e6533d4c06a039208608204e
Naim Saad <VANRISE\naim.saad>
2019-04-04 14:03:16 +0000

    Implementing InterconnectSettlement Classes
    git-tfs-id: [http://developmentvlan:8585/tfs/vanrise.collection]$/;C55065

---
+++ Retail/Code/Retail.Interconnect.Business/InterconnectSettlementGenerationCustomSection.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Vanrise.Common;
+using Vanrise.Invoice.Entities;
+
+namespace Retail.Interconnect.Business
+{
+    public class InterconnectSettlementGenerationCustomSection : GenerationCustomSection
+    {
+
+        //InterconnectInvoiceSettlementManager _interconnectInvoiceSettlementManager = new InterconnectInvoiceSettlementManager();
+
+        Guid _customerInvoiceTypeId;
+        Guid _supplierInvoiceTypeId;
+        public InterconnectSettlementGenerationCustomSection(Guid customerInvoiceTypeId, Guid sipplierInvoiceTypeId)
+        {
+            _customerInvoiceTypeId = customerInvoiceTypeId;
+            _supplierInvoiceTypeId = sipplierInvoiceTypeId;
+        }
+        public override string GenerationCustomSectionDirective
+        {
+            get
+            {
+                //return "whs-invoicetype-generationcustomsection-settlement";
+                return "";
+            }
+        }
+        public override void EvaluateGenerationCustomPayload(IGetGenerationCustomPayloadContext context)
+        {
+
+            //List<Vanrise.Invoice.Entities.Invoice> supplierInvoices = new List<Vanrise.Invoice.Entities.Invoice>();
+
+            //List<Vanrise.Invoice.Entities.Invoice> customerInvoices = new List<Vanrise.Invoice.Entities.Invoice>();
+            //List<InvoiceItem> supplierInvoiceItems = new List<InvoiceItem>(), customerInvoiceItems = new List<InvoiceItem>();
+
+            //IEnumerable<string> partnerIds = context.InvoiceGenerationInfo.MapRecords(x => x.PartnerId);
+            //if (context.InvoiceGenerationInfo != null)
+            //{
+            //    foreach (var generationCustomPayload in context.InvoiceGenerationInfo)
+            //    {
+            //        generationCustomPayload.CustomPayload = _interconnectInvoiceSettlementManager.EvaluateGenerationCustomPayload(_customerInvoiceTypeId, _supplierInvoiceTypeId, partnerIds, generationCustomPayload.PartnerId, context.InvoiceTypeId, context.MinimumFrom, context.MaximumTo, supplierInvoices, supplierInvoiceItems, customerInvoices, customerInvoiceItems, false);
+            //    }
+            //}
+        }
+    }
+}
---
+++ Retail/Code/Retail.Interconnect.Business/InterconnectSettlementInvoiceSettings.cs
@@ -0,0 +1,81 @@
+﻿using Retail.BusinessEntity.Business;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Vanrise.Invoice.Entities;
+
+namespace Retail.Interconnect.Business
+{
+    public class InterconnectSettlementInvoiceSettings : BaseRetailInvoiceTypeSettings
+    {
+        public override Guid ConfigId
+        {
+            get { return new Guid("C0AF312F-C1AB-4F77-A9C9-663BBF9AD71F"); }
+        }
+        public Guid CustomerInvoiceTypeId { get; set; }
+        public Guid SupplierInvoiceTypeId { get; set; }
+
+        public override GenerationCustomSection GenerationCustomSection
+        {
+            get { return new InterconnectSettlementGenerationCustomSection(CustomerInvoiceTypeId, SupplierInvoiceTypeId); }
+        }
+        public override dynamic GetInfo(Vanrise.Invoice.Entities.IInvoiceTypeExtendedSettingsInfoContext context)
+        {
+            //AccountBEManager accountBEManager = new AccountBEManager();
+            //FinancialAccountManager financialAccountManager = new FinancialAccountManager();
+            //var financialAccountData = financialAccountManager.GetFinancialAccountData(this.AccountBEDefinitionId, context.Invoice.PartnerId);
+
+            //switch (context.InfoType)
+            //{
+            //    case "MailTemplate":
+            //        long accountId = Convert.ToInt32(financialAccountData.Account.AccountId);
+            //        var account = accountBEManager.GetAccount(this.AccountBEDefinitionId, accountId);
+            //        Dictionary<string, dynamic> objects = new Dictionary<string, dynamic>();
+            //        objects.Add("Operator", account);
+            //        objects.Add("Invoice", context.Invoice);
+            //        return objects;
+            //    case "BankDetails":
+            //        {
+            //            #region BankDetails
+            //            return accountBEManager.GetBankDetailsIds(this.AccountBEDefinitionId, financialAccountData.Account.AccountId);
+            //            #endregion
+            //        }
+            //}
+            return null;
+        }
+
+        public override void GetInitialPeriodInfo(Vanrise.Invoice.Entities.IInitialPeriodInfoContext context)
+        {
+            FinancialAccountManager financialAccountManager = new FinancialAccountManager();
+            var financialAccountData = financialAccountManager.GetFinancialAccountData(this.AccountBEDefinitionId, context.PartnerId);
+            context.PartnerCreationDate = financialAccountData.Account.CreatedTime;
+
+        }
+
+        public override Vanrise.Invoice.Entities.InvoiceGenerator GetInvoiceGenerator()
+        {
+            return null;
+            // return new InterconnectSettlementInvoiceGenerator(this.AccountBEDefinitionId, this.CustomerInvoiceTypeId, this.SupplierInvoiceTypeId);
+        }
+
+        public override IEnumerable<string> GetPartnerIds(Vanrise.Invoice.Entities.IExtendedSettingsPartnerIdsContext context)
+        {
+            switch (context.PartnerRetrievalType)
+            {
+                case PartnerRetrievalType.GetActive:
+                case PartnerRetrievalType.GetAll:
+                    FinancialAccountManager financialAccountManager = new FinancialAccountManager();
+                    return financialAccountManager.GetAllFinancialAccountsIds(this.AccountBEDefinitionId);
+                default:
+                    return null;
+            }
+        }
+
+        public override Vanrise.Invoice.Entities.InvoicePartnerManager GetPartnerManager()
+        {
+            return new InterconnectPartnerSettings(this.AccountBEDefinitionId);
+        }
+    }
+}
