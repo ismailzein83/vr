2d0f893dce8ba63aae8b4a110fccdcb4fd1f86c4
Mohammad Yahfoufi <VANRISE\mohammad.yahfoufi>
2019-04-05 11:55:05 +0000

    Adding Service Detail Functions
    git-tfs-id: [http://developmentvlan:8585/tfs/vanrise.collection]$/;C55091

--- SOM/Code/BPMExtended.Main.Business/Billing/ServiceManager.cs
+++ SOM/Code/BPMExtended.Main.Business/Billing/ServiceManager.cs
@@ -3,8 +3,11 @@ using BPMExtended.Main.Entities;
 using BPMExtended.Main.SOMAPI;
 using System;
 using System.Collections.Generic;
+using System.Linq;
 using System.Web;
 using Terrasoft.Core;
+using Terrasoft.Core.DB;
+using Terrasoft.Core.Entities;
 
 namespace BPMExtended.Main.Business
 {
@@ -49,6 +52,79 @@ namespace BPMExtended.Main.Business
             return servicesInfoItems;
         }
 
+        public List<POSServiceDetail> GetPOSServiceDetail(string catalogId, string catalogName, string sourceSchemaName)
+        {
+            List<string> POSServicesids = new List<string>();
+            EntitySchemaQuery esq;
+            IEntitySchemaQueryFilterItem esqFirstFilter;
+            esq = new EntitySchemaQuery(BPM_UserConnection.EntitySchemaManager, sourceSchemaName);
+            var POSId = esq.AddColumn("StPOSServiceID");
+            esqFirstFilter = esq.CreateFilterWithParameters(FilterComparisonType.Equal, catalogName, catalogId);
+            esq.Filters.Add(esqFirstFilter);
+
+            var entities = esq.GetEntityCollection(BPM_UserConnection);
+            if (entities.Count > 0)
+            {
+                foreach (var item in entities)
+                {
+                    POSServicesids.Add(item.GetTypedColumnValue<string>(POSId.Name));
+                }
+            }
+
+
+            var POSServiceDetailItems = new List<POSServiceDetail>();
+            using (SOMClient client = new SOMClient())
+            {
+                List<ServiceDefinition> items = client.Get<List<ServiceDefinition>>(String.Format("api/SOM.ST/Billing/GetPOSServices"));
+                foreach (var item in items)
+                {
+                    var POSServiceDetailItem = POSServiceDefinitionToDetailMapper(item);
+                    POSServiceDetailItems.Add(POSServiceDetailItem);
+                }
+            }
+
+            var posServices = new List<POSServiceDetail>();
+            posServices = POSServiceDetailItems.Where(p => !POSServicesids.Any(p2 => p2.ToString() == p.Id.ToString())).ToList();
+            return posServices;
+        }
+
+        public List<ServiceDetail> GetServiceDetail(string catalogId, string catalogName, string sourceSchemaName)
+        {
+            List<string> Servicesids = new List<string>();
+            EntitySchemaQuery esq;
+            IEntitySchemaQueryFilterItem esqFirstFilter;
+            esq = new EntitySchemaQuery(BPM_UserConnection.EntitySchemaManager, sourceSchemaName);
+            var POSId = esq.AddColumn("StServiceID");
+            esqFirstFilter = esq.CreateFilterWithParameters(FilterComparisonType.Equal, catalogName, catalogId);
+            esq.Filters.Add(esqFirstFilter);
+
+            var entities = esq.GetEntityCollection(BPM_UserConnection);
+            if (entities.Count > 0)
+            {
+                foreach (var item in entities)
+                {
+                    Servicesids.Add(item.GetTypedColumnValue<string>(POSId.Name));
+                }
+            }
+
+
+            var serviceDetailItems = new List<ServiceDetail>();
+            using (SOMClient client = new SOMClient())
+            {
+                List<ServiceDefinition> items = client.Get<List<ServiceDefinition>>(String.Format("api/SOM.ST/Billing/GetServicesDefinition"));
+                foreach (var item in items)
+                {
+                    var serviceDetailItem = ServiceDefinitionToDetailMapper(item);
+                    serviceDetailItems.Add(serviceDetailItem);
+                }
+            }
+
+            var posServices = new List<ServiceDetail>();
+            posServices = serviceDetailItems.Where(p => !serviceDetailItems.Any(p2 => p2.ToString() == p.ServiceId.ToString())).ToList();
+            return posServices;
+        }
+
+
         #region Mappers
         public ServiceInfo ServiceDefinitionToInfoMapper(ServiceDefinition item)
         {
@@ -56,6 +132,25 @@ namespace BPMExtended.Main.Business
             {
                 Name = item.Title,
                 ServiceId = item.PublicId
+
+            };
+        }
+        public ServiceDetail ServiceDefinitionToDetailMapper(ServiceDefinition item)
+        {
+            return new ServiceDetail
+            {
+                Name = item.Title,
+                ServiceId = item.PublicId
+
+            };
+        }
+
+        public POSServiceDetail POSServiceDefinitionToDetailMapper(ServiceDefinition item)
+        {
+            return new POSServiceDetail
+            {
+                Name = item.Title,
+                Id = item.PublicId
 
             };
         }
