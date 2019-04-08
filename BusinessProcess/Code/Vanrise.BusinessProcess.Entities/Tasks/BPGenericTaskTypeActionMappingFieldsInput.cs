ac00473c20e3d64283486a52d319a36ba7b15753
yara.bousalman <yara.bousalman@vanrise.tfs.local>
2019-04-08 07:57:26 +0000

    Adding files to Vanrise.BusinessProcess.Entities
    git-tfs-id: [http://developmentvlan:8585/tfs/vanrise.collection]$/;C55131

---
+++ BusinessProcess/Code/Vanrise.BusinessProcess.Entities/Tasks/BPGenericTaskTypeActionMappingFieldsInput.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Vanrise.BusinessProcess.Entities
+{
+    public class BPGenericTaskTypeActionMappingFieldsInput
+    {
+        public string DecisionFieldName { get; set; }
+        public string NotesFieldName { get; set; }
+        public object DecisionFieldValue { get; set; }
+        public object NotesFieldValue { get; set; }
+        public Guid DataRecordTypeId { get; set; }
+    }
+}
---
+++ BusinessProcess/Code/Vanrise.BusinessProcess.Entities/Tasks/BPGenericTaskTypeActionMappingFieldsOutput.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Vanrise.BusinessProcess.Entities
+{
+    public class BPGenericTaskTypeActionMappingFieldsOutput
+    {
+        public string DecisionFieldDescription { get; set; }
+        public string NotesFieldDescription { get; set; }
+    }
+}
