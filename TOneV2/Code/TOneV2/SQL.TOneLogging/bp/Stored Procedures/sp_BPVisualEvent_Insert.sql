aee7409397ea9a91424a85f4baeaaf92ce6d0ffe
Ismail Zein <VANRISE\ismail.zein>
2019-04-08 11:17:53 +0000

    sql project
    git-tfs-id: [http://developmentvlan:8585/tfs/vanrise.collection]$/;C55145

---
+++ TOneV2/Code/TOneV2/SQL.TOneLogging/bp/Stored Procedures/sp_BPVisualEvent_Insert.sql
@@ -0,0 +1,29 @@
+﻿
+-- =============================================
+-- Author:		<Author,,Name>
+-- Create date: <Create Date,,>
+-- Description:	<Description,,>
+-- =============================================
+CREATE PROCEDURE [bp].[sp_BPVisualEvent_Insert]
+	@ProcessInstanceID bigint,
+	@ActivityID uniqueidentifier,
+	@Title nvarchar(max),
+	@EventTypeID uniqueidentifier,
+	@EventPayload nvarchar(max)
+AS
+BEGIN
+	INSERT INTO [bp].[BPVisualEvent]
+           ([ProcessInstanceID]
+		   ,ActivityID
+		   ,Title
+		   ,EventTypeID
+           ,[EventPayload]
+           ,[CreatedTime])
+     VALUES
+           (@ProcessInstanceID
+		   ,@ActivityID
+		   ,@Title
+		   ,@EventTypeID
+           ,@EventPayload
+           ,GETDATE())
+END
---
+++ TOneV2/Code/TOneV2/SQL.TOneLogging/bp/Tables/BPVisualEvent.sql
@@ -0,0 +1,16 @@
+﻿CREATE TABLE [bp].[BPVisualEvent] (
+    [ID]                BIGINT           IDENTITY (1, 1) NOT NULL,
+    [ProcessInstanceID] BIGINT           NOT NULL,
+    [ActivityID]        UNIQUEIDENTIFIER NOT NULL,
+    [Title]             NVARCHAR (MAX)   NULL,
+    [EventTypeID]       UNIQUEIDENTIFIER NOT NULL,
+    [EventPayload]      NVARCHAR (MAX)   NOT NULL,
+    [CreatedTime]       DATETIME         CONSTRAINT [DF_BPVisualEvent_CreatedTime] DEFAULT (getdate()) NULL,
+    CONSTRAINT [PK_BPVisualEvent] PRIMARY KEY NONCLUSTERED ([ID] ASC)
+);
+
+
+GO
+CREATE CLUSTERED INDEX [IX_BPVisualEvent_ProcessInstanceAndId]
+    ON [bp].[BPVisualEvent]([ProcessInstanceID] ASC, [ID] ASC);
+
