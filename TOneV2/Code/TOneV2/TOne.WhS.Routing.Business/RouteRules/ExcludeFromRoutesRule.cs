5c7dcc713eadf0743b203272fb716bc6f8362b7d
Ali Slayman <VANRISE\ali.slayman>
2019-04-09 07:21:36 +0000

    Implement ExcludeFromRoutesRule in Routing
    git-tfs-id: [http://developmentvlan:8585/tfs/vanrise.collection]$/;C55157

---
+++ TOneV2/Code/TOneV2/TOne.WhS.Routing.Business/RouteRules/ExcludeFromRoutesRule.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using TOne.WhS.Routing.Entities;
+using System.Linq;
+
+namespace TOne.WhS.Routing.Business
+{
+    public class ExcludeFromRoutesRule : RouteRuleSettings
+    {
+        public override Guid ConfigId { get { return new Guid("F966A804-E906-4DBD-8A8F-748805B8FE2E"); } }
+
+        public override void ApplyRuleToRPOptions(IRPRouteRuleExecutionContext context, ref IEnumerable<RPRouteOption> options)
+        {
+            throw new NotImplementedException();
+        }
+
+        public override bool AreSuppliersIncluded(IRouteRuleAreSuppliersIncludedContext context)
+        {
+            throw new NotImplementedException();
+        }
+
+        public override RouteRuleSettings BuildLinkedRouteRuleSettings(ILinkedRouteRuleContext context)
+        {
+            throw new NotImplementedException();
+        }
+
+        public override void CreateSupplierZoneOptionsForRP(IRPRouteRuleExecutionContext context, RouteRuleTarget target)
+        {
+            throw new NotImplementedException();
+        }
+
+        public override void ExecuteForSaleEntity(ISaleEntityRouteRuleExecutionContext context, RouteRuleTarget target)
+        {
+            throw new NotImplementedException();
+        }
+
+        public override string GetSuppliersDescription()
+        {
+            throw new NotImplementedException();
+        }
+
+        //public override bool ShouldCreateRoute()
+        //{
+        //    return false;
+        //}
+    }
+}
