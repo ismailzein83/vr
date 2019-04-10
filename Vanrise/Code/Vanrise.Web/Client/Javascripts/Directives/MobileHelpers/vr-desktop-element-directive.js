bb9cfd6e79f09aca9f692e9aac36c340d6445605
Ali Ballouk <VANRISE\ali.ballouk>
2019-04-10 13:17:42 +0000

    adding mobile helpers elements
    git-tfs-id: [http://developmentvlan:8585/tfs/vanrise.collection]$/;C55198

---
+++ Vanrise/Code/Vanrise.Web/Client/Javascripts/Directives/MobileHelpers/vr-desktop-element-directive.js
@@ -0,0 +1,56 @@
+﻿(function (app) {
+
+    "use strict";
+
+    app.directive('vrDesktopElement',['$animate' ,'MobileService',function ($animate,MobileService) {      
+
+
+        var directiveDefinitionObject =  {
+            multiElement: true,
+            transclude: 'element',
+            priority: 600,
+            terminal: true,
+            restrict: 'A',
+            $$tlb: true,
+            link: function ($scope, $element, $attr, ctrl, $transclude) {
+                var block, childScope, previousElements;
+                var value = MobileService.isMobile() == false;
+                    if (value) {
+                        if (!childScope) {
+                            $transclude(function (clone, newScope) {
+                                childScope = newScope;
+                                clone[clone.length++] = document.createComment(' end  vrDesktopElement ');
+                                block = {
+                                    clone: clone
+                                };
+                                $animate.enter(clone, $element.parent(), $element);
+                            });
+                        }
+                    } else {
+                        if (previousElements) {
+                            previousElements.remove();
+                            previousElements = null;
+                        }
+                        if (childScope) {
+                            childScope.$destroy();
+                            childScope = null;
+                        }
+                        if (block) {
+                            previousElements = getBlockNodes(block.clone);
+                            $animate.leave(previousElements).then(function () {
+                                previousElements = null;
+                            });
+                            block = null;
+                        }
+                    }
+            }
+        };
+
+
+        return directiveDefinitionObject;
+
+    }]);
+
+
+})(app);
+
