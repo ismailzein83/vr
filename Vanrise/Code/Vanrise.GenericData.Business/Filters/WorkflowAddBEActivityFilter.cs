e8fae7d122d062f1aa791d4ed749227d5e4f1ff0
Mohammad Sheaib <VANRISE\mohammad.sheaib>
2019-04-11 12:20:48 +0000

    WorkFlow Add & Update BE Activity Filters
    git-tfs-id: [http://developmentvlan:8585/tfs/vanrise.collection]$/;C55232

---
+++ Vanrise/Code/Vanrise.GenericData.Business/Filters/WorkflowAddBEActivityFilter.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Vanrise.Entities;
+using Vanrise.GenericData.Entities;
+
+namespace Vanrise.GenericData.Business
+{
+    public class WorkflowAddBEActivityFilter : IBusinessEntityDefinitionFilter
+    {
+        public bool IsMatched(IBusinessEntityDefinitionFilterContext context)
+        {
+            if (context.entityDefinition == null || context.entityDefinition.Settings == null || context.entityDefinition.Settings.WorkFlowUpdateBEActivityEditor == null)
+                return false;
+
+            return true;
+        }
+    }
+}
---
+++ Vanrise/Code/Vanrise.GenericData.Business/Filters/WorkflowUpdateBEActivityFilter.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Vanrise.Entities;
+using Vanrise.GenericData.Entities;
+
+namespace Vanrise.GenericData.Business
+{
+    public class WorkflowUpdateBEActivityFilter : IBusinessEntityDefinitionFilter
+    {
+        public bool IsMatched(IBusinessEntityDefinitionFilterContext context)
+        {
+            if (context.entityDefinition == null || context.entityDefinition.Settings == null || context.entityDefinition.Settings.WorkFlowAddBEActivityEditor == null)
+                return false;
+
+            return true;
+        }
+    }
+}
