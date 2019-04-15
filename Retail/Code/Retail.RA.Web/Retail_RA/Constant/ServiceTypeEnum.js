ec1fdb1e4747c7b84d529fdaf7ff834f55b1815d
yara.bousalman <yara.bousalman@vanrise.tfs.local>
2019-04-15 08:21:03 +0000

    Adding ServiceTypeEnum
    git-tfs-id: [http://developmentvlan:8585/tfs/vanrise.collection]$/;C55254

---
+++ Retail/Code/Retail.RA.Web/Retail_RA/Constant/ServiceTypeEnum.js
@@ -0,0 +1,6 @@
+﻿app.constant('Retail_Ra_ServiceTypeEnum', {
+    Total: { value: 0, description: "Total" },
+    Onnet: { value: 1, description: "On-net" },
+    Offnet: { value: 1, description: "Off-net" },
+    International: { value: 1, description: "International" }
+});
