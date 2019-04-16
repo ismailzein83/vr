ac912692088b4eabd6e9b78dd4d4b64acae2565b
Ali Slayman <VANRISE\ali.slayman>
2019-04-16 10:36:43 +0000

    Implement ParentDataRecordTypeExtraFields class and adding specific JS and HTML
    git-tfs-id: [http://developmentvlan:8585/tfs/vanrise.collection]$/;C55279

---
+++ Vanrise/Code/Vanrise.GenericData.MainExtensions/DataRecordTypeExtraFields/ParentDataRecordTypeExtraFields.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Vanrise.GenericData.Entities;
+
+namespace Vanrise.GenericData.MainExtensions
+{
+    public class ParentDataRecordTypeExtraFields : DataRecordTypeExtraField
+    {
+        public override Guid ConfigId { get { return new Guid("043E058A-0E0E-40E2-82AB-FE04C896E615"); } }
+
+        public Guid DataRecordTypeId { get; set; }
+
+        public override List<DataRecordField> GetFields(IDataRecordExtraFieldContext context)
+        {
+            return null;
+        }
+    }
+}
---
+++ Vanrise/Code/Vanrise.GenericData.Web/VR_GenericData/Directives/RecordType/vr-genericdata-datarecordtypeextrafields-parentrecordtype.js
@@ -0,0 +1,90 @@
+﻿'use strict';
+
+app.directive('vrAnalyticDataanalysisextrafields', ['UtilsService', 'VRUIUtilsService',
+    function (UtilsService, VRUIUtilsService) {
+
+        var directiveDefinitionObject = {
+            restrict: 'E',
+            scope: {
+                onReady: '=',
+                normalColNum: '@'
+            },
+            controller: function ($scope, $element, $attrs) {
+
+                var ctrl = this;
+                var ctor = new extraFieldCtor(ctrl, $scope, $attrs);
+                ctor.initializeController();
+            },
+            controllerAs: 'ctrl',
+            bindToController: true,
+            compile: function (element, attrs) {
+
+            },
+            templateUrl: '/Client/Modules/VR_GenericData/Directives/RecordType/Templates/ParentDataRecordTypeExtraFieldsTemplate.html'
+        };
+
+
+        function extraFieldCtor(ctrl, $scope, $attrs) {
+            this.initializeController = initializeController;
+            $scope.scopeModel = {};
+
+            var selectedDataRecordType;
+
+            var dataRecordTypeSelectorReadyPromiseDeferred = UtilsService.createPromiseDeferred();
+            var datarecordTypeSelectorDirectiveApi;
+
+            function initializeController() {
+
+                $scope.scopeModel.onDataRecordTypeSelectorReady = function (api) {
+                    datarecordTypeSelectorDirectiveApi = api;
+                    dataRecordTypeSelectorReadyPromiseDeferred.resolve();
+                };
+               
+                defineAPI();
+            }
+
+            function defineAPI() {
+                var api = {};
+
+                api.load = function (payload) {
+                    var promises = [];
+
+                    if (payload != undefined)
+                        selectedDataRecordType = payload.DataRecordTypeId;
+
+                    var dataRecordTypeSelectorLoadedPromise = loadDataRecordTypeSelector();
+                    promises.push(dataRecordTypeSelectorLoadedPromise);
+
+                    function loadDataRecordTypeSelector() {
+                        var dataRecordTypeSelectorLoadedPromiseDeferred = UtilsService.createPromiseDeferred();
+
+                        dataRecordTypeSelectorReadyPromiseDeferred.promise.then(function () {
+
+                            var dataRecordTypeSelectorPayload;
+
+                            if (selectedDataRecordType != undefined)
+                                dataRecordTypeSelectorPayload.selectedIds = selectedDataRecordType;
+
+                            VRUIUtilsService.callDirectiveLoad(datarecordTypeSelectorDirectiveApi, dataRecordTypeSelectorPayload, dataRecordTypeSelectorLoadedPromiseDeferred);
+                        });
+                        return dataRecordTypeSelectorLoadedPromiseDeferred.promise;
+                    }
+
+                    return UtilsService.waitMultiplePromises(promises);
+                };
+
+
+                api.getData = function () {
+                    return {
+                        $type: 'Vanrise.GenericData.MainExtensions.ParentDataRecordTypeExtraFields, Vanrise.GenericData.MainExtensions',
+                        DataRecordTypeId: datarecordTypeSelectorDirectiveApi.getSelectedIds()
+                    };
+                };
+
+                if (ctrl.onReady != null && typeof(ctrl.onReady) == "function")
+                    ctrl.onReady(api);
+            }
+        }
+
+        return directiveDefinitionObject;
+    }]);
