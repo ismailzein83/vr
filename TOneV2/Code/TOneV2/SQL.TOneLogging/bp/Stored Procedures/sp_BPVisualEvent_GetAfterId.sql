2a2939d0a6609749cd9f96febf2fd86e413693c3
Fatima Hammoud <VANRISE\fatima.hammoud>
2019-04-17 08:44:05 +0000

    Sync New SP for BPVisualEvent
    git-tfs-id: [http://developmentvlan:8585/tfs/vanrise.collection]$/;C55313

---
+++ TOneV2/Code/TOneV2/SQL.TOneLogging/bp/Stored Procedures/sp_BPVisualEvent_GetAfterId.sql
@@ -0,0 +1,44 @@
+﻿-- =============================================
+-- Author:		<Author,,Name>
+-- Create date: <Create Date,,>
+-- Description:	<Description,,>
+-- =============================================
+CREATE PROCEDURE [bp].[sp_BPVisualEvent_GetAfterId]
+	@GreaterThanID BIGINT,
+	@BPInstanceID INT
+AS
+BEGIN
+IF (@GreaterThanID IS NULL)
+	BEGIN
+	SELECT  [ID]
+			,[ProcessInstanceID]
+			,[ActivityID]
+			,[Title]
+			,[EventTypeID]
+			,[EventPayload]
+			,[CreatedTime]
+            INTO #temp_table
+			FROM [bp].[BPVisualEvent] WITH(NOLOCK) 
+            WHERE ProcessInstanceID  = @BPInstanceID
+            
+            SELECT * FROM #temp_table
+	END
+	
+	ELSE
+	BEGIN
+	SELECT  [ID]
+			,[ProcessInstanceID]
+			,[ActivityID]
+			,[Title]
+			,[EventTypeID]
+			,[EventPayload]
+			,[CreatedTime]
+            INTO #temp2_table
+            FROM [bp].[BPVisualEvent] WITH(NOLOCK) 
+			WHERE ProcessInstanceID  = @BPInstanceID 
+			AND ID >@GreaterThanID
+            ORDER BY ID
+            
+            SELECT * FROM #temp2_table
+	END
+END
