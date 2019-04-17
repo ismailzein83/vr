cdd9eaf2283c438d5bb82814a1b2987cc6b3936d
yara.bousalman <yara.bousalman@vanrise.tfs.local>
2019-04-17 06:43:26 +0000

    Adding OnNetOperatorDeclaration
    git-tfs-id: [http://developmentvlan:8585/tfs/vanrise.collection]$/;C55301

---
+++ Retail/Code/Retail.RA.Entities/OnNetOperationDeclaration/OnNetOperatorDeclaration.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Retail.RA.Entities
+{
+    public class OnNetOperatorDeclaration
+    {
+        public long ID { get; set; }
+        public long OperatorID { get; set; }
+        public int PeriodID { get; set; }
+        public OnNetOperatorDeclarationServices OperatorDeclarationServices { get; set; }
+    }
+}
