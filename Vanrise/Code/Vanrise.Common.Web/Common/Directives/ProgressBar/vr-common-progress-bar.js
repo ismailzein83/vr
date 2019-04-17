8d4c116ec0775b29a5941a5e8e2fd9c7b9be7e00
Mohammad Sheaib <VANRISE\mohammad.sheaib>
2019-04-17 13:26:38 +0000

    Progress Bar
    git-tfs-id: [http://developmentvlan:8585/tfs/vanrise.collection]$/;C55337

---
+++ Vanrise/Code/Vanrise.Common.Web/Common/Directives/ProgressBar/vr-common-progress-bar.js
@@ -0,0 +1,26 @@
+﻿'use strict';
+
+app.directive('vrCommonProgressBar', [ 'UtilsService', 'VRUIUtilsService',
+
+    function ( UtilsService, VRUIUtilsService) {
+        return {
+            restrict: 'E',
+            scope: {
+                items: '='
+            },
+            controller: function ($scope, $element, $attrs) {
+
+                var ctrl = this;
+
+                $(function () {
+                    $('[data-toggle="tooltip"]').tooltip()
+                }); 
+
+            },
+            controllerAs: 'ctrl',
+            bindToController: true,
+            templateUrl: '/Client/Modules/Common/Directives/ProgressBar/Templates/ProgressBarTemplate.html'
+        };
+
+        
+    }]); 
