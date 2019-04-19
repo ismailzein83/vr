366ab6fd6140ce82b8b3de991b69882a40625393
yara.bousalman <yara.bousalman@vanrise.tfs.local>
2019-04-19 13:10:56 +0000

    Adding TransactionTypeLKUPBEDefinitionSettings .js and .html files
    git-tfs-id: [http://developmentvlan:8585/tfs/vanrise.collection]$/;C55400

---
+++ Retail/Code/Retail.RA.Web/Retail_RA/Directives/TransactionType/ra-retail-lookupbeextendedsettings-transactiontype.js
@@ -0,0 +1,63 @@
+﻿'use strict';
+app.directive('raRetailLookupbeextendedsettingsTransactiontype', ['UtilsService', 'VRUIUtilsService',
+    function (UtilsService, VRUIUtilsService) {
+
+        var directiveDefinitionObject = {
+            restrict: 'E',
+            scope:
+            {
+                onReady: '='
+            },
+            controller: function ($scope, $element, $attrs) {
+
+                var ctrl = this;
+
+                var ctor = new LookUpBEExtendedSettings(ctrl, $scope);
+                ctor.initializeController();
+
+            },
+            controllerAs: 'ctrl',
+            bindToController: true,
+            compile: function (element, attrs) {
+                return {
+                    pre: function ($scope, iElem, iAttrs, ctrl) {
+
+                    }
+                };
+            },
+            templateUrl: '/Client/Modules/Retail_RA/Directives/TransactionType/Templates/TransactionTypeLKUpBEExtendedSettingsTemplate.html'
+
+        };
+
+        function LookUpBEExtendedSettings(ctrl, $scope) {
+            this.initializeController = initializeController;
+
+            function initializeController() {
+
+                defineAPI();
+            }
+
+            function defineAPI() {
+                var api = {};
+
+                api.load = function (payload) {
+                    var rootPromiseNode = {
+                        promises: []
+                    };
+                    return UtilsService.waitPromiseNode(rootPromiseNode);
+                };
+
+                api.getData = function () {
+                    return {
+                        $type: "Retail.RA.Business.TransactionTypeLKUPBEDefinitionSettings, Retail.RA.Business"
+                    };
+                };
+
+                if (ctrl.onReady != null)
+                    ctrl.onReady(api);
+            }
+        }
+
+        return directiveDefinitionObject;
+    }
+]);
