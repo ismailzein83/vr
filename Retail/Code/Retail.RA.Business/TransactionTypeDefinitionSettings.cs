17d6d9460e7f6b7c34f8eac0c79e556ed325a12b
yara.bousalman <yara.bousalman@vanrise.tfs.local>
2019-04-19 06:42:00 +0000

    Adding TransactionTypeLKUPBEDefinitionSettings
    git-tfs-id: [http://developmentvlan:8585/tfs/vanrise.collection]$/;C55362

---
+++ Retail/Code/Retail.RA.Business/TransactionTypeDefinitionSettings.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Vanrise.Entities;
+using Vanrise.GenericData.Entities;
+
+namespace Retail.RA.Business
+{
+    public class TransactionTypeLKUPBEDefinitionSettings : LKUPBEDefinitionExtendedSettings
+    {
+        public override Guid ConfigId { get { return new Guid("BA619742-DF5C-4448-A607-E38AD7B43D86"); } }
+
+        public override Dictionary<string, LKUPBusinessEntityItem> GetAllLKUPBusinessEntityItems(ILKUPBusinessEntityExtendedSettingsContext context)
+        {
+            throw new NotImplementedException();
+        }
+
+        public override bool IsCacheExpired(ref DateTime? lastCheckTime)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
