cbbeaa9306666788cf29cba4c14007be3c6609c8
Rodi Hassan <VANRISE\rodi.hassan>
2019-04-24 12:53:52 +0000

    modify TelephonyContract classes
    git-tfs-id: [http://developmentvlan:8585/tfs/vanrise.collection]$/;C55475

--- SOM/Code/BPMExtended.Main.SOM.Entities/Billing/ServiceResponse/CustomerContact.cs
+++ SOM/Code/BPMExtended.Main.SOM.Entities/Billing/ServiceResponse/CustomerContact.cs
@@ -22,6 +22,7 @@ namespace BPMExtended.Main.SOMAPI
         public string Id { get; set; }
         public string CustomerId { get; set; }
         public long RateplanId { get; set; }
+        public string RateplanName { get; set; }
         public string PhoneNumber { get; set; }
         public int Status { get; set; }
         public DateTime? ActivationDate { get; set; }
@@ -33,10 +34,12 @@ namespace BPMExtended.Main.SOMAPI
         public string Id { get; set; }
         public string CustomerId { get; set; }
         public long RateplanId { get; set; }
+        public string RateplanName { get; set; }
         public string PhoneNumber { get; set; }
         public string LinePathId { get; set; }
         public string Street { get; set; }
         public string City { get; set; }
+        public Decimal? Balance { get; set; }
         public int Status { get; set; }
         public DateTime? ActivationDate { get; set; }
         public DateTime? LastStatusChangeDate { get; set; }
