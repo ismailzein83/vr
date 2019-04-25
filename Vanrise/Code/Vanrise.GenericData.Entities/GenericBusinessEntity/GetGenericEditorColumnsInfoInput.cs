cf3a61599cf127d058e9a025b8fc56daaccefc80
Mohammad Sheaib <VANRISE\mohammad.sheaib>
2019-04-25 05:48:18 +0000

    GenericData Entities (Commented)
    git-tfs-id: [http://developmentvlan:8585/tfs/vanrise.collection]$/;C55484

---
+++ Vanrise/Code/Vanrise.GenericData.Entities/GenericBusinessEntity/GetGenericEditorColumnsInfoInput.cs
@@ -0,0 +1,14 @@
+﻿//using System;
+//using System.Collections.Generic;
+//using System.Linq;
+//using System.Text;
+//using System.Threading.Tasks;
+
+//namespace Vanrise.GenericData.Entities
+//{
+//    public class GetGenericEditorColumnsInfoInput
+//    {
+//        public VRGenericEditorDefinitionSetting GenericEditorDefinitionSetting { get; set; }
+//        public Guid DataRecordTypeId { get; set; }
+//    }
+//}
