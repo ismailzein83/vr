ae8cc42ebbe042b0fe8795b2e4fe532f719ab6ec
Fatima Hammoud <VANRISE\fatima.hammoud>
2019-04-25 06:19:24 +0000

    Adding new class "RDBDataRecordStorageJoinSettingsConfig"
    git-tfs-id: [http://developmentvlan:8585/tfs/vanrise.collection]$/;C55486

---
+++ Vanrise/Code/Vanrise.GenericData.Entities/DataStorage/DataRecordStorage/RDBDataRecordStorageJoinSettingsConfig.cs
@@ -0,0 +1,10 @@
+﻿using Vanrise.Entities;
+
+namespace Vanrise.GenericData.Entities
+{
+    public class RDBDataRecordStorageJoinSettingsConfig: ExtensionConfiguration
+    {
+        public const string EXTENSION_TYPE = "VR_GenericData_RDBDataRecordStorageJoinSettingsConfig";
+        public string Editor { get; set; }
+     }
+}
