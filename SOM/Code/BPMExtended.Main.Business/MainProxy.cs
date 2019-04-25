50595339370a4852f461316037f4c4e5a06ddfe2
Ayman Zinji <VANRISE\ayman.zinji>
2019-04-25 13:54:55 +0000

    insert into error log object
    git-tfs-id: [http://developmentvlan:8585/tfs/vanrise.collection]$/;C55495

--- SOM/Code/BPMExtended.Main.Business/MainProxy.cs
+++ SOM/Code/BPMExtended.Main.Business/MainProxy.cs
@@ -3,11 +3,21 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Web;
+using Terrasoft.Core;
+using Terrasoft.Core.Entities;
 
 namespace BPMExtended.Main.Business
 {
     public class MainProxy
     {
+        public UserConnection BPM_UserConnection
+        {
+            get
+            {
+                return (UserConnection)HttpContext.Current.Session["UserConnection"];
+            }
+        }
         public void InvokeMethod(Action action)
         {
             try
@@ -29,6 +39,15 @@ namespace BPMExtended.Main.Business
             }
             catch (Exception ex)
             {
+                EntitySchema schema = BPM_UserConnection.EntitySchemaManager.GetInstanceByName("StErrorLog");
+                Entity errorLog = schema.CreateEntity(BPM_UserConnection);
+
+                errorLog.SetColumnValue("Id", Guid.NewGuid());
+                errorLog.SetColumnValue("StIP", BPM_UserConnection.CurrentUser.ClientIP);
+                errorLog.SetColumnValue("StUserId", BPM_UserConnection.CurrentUser.ContactId);
+                errorLog.SetColumnValue("StMessage", ex.Message);
+                errorLog.SetColumnValue("StDetails", ex.ToString());
+                errorLog.Save();
                 Console.WriteLine("An error has occurred. Exception details {0}", ex.Message);
                 throw ex;
             }
