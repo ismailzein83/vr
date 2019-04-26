cd6d79377ccc91ae53c8d8dfa03cbf6befdf417e
yara.bousalman <yara.bousalman@vanrise.tfs.local>
2019-04-26 13:18:43 +0000

    Changing location of file
    git-tfs-id: [http://developmentvlan:8585/tfs/vanrise.collection]$/;C55519

--- Vanrise/Code/Vanrise.AccountBalance.MainExtensions/VRObjectTypes/AccountBalancePropertyEvaluator.cs
+++ Vanrise/Code/Vanrise.AccountBalance.MainExtensions/VRObjectTypes/AccountBalancePropertyEvaluator.cs
@@ -3,10 +3,12 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Vanrise.Entities;
 
 namespace Vanrise.AccountBalance.MainExtensions
 {
     public class AccountBalancePropertyEvaluator
     {
+       
     }
 }
