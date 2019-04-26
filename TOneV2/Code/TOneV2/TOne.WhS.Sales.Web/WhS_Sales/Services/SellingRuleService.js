a9b897d9744e892ecea29d9279fb59ccd858c24c
Hassan Makhour <VANRISE\hassan.makhour>
2019-04-26 09:48:28 +0000

    Supplier Target Match services Temporarily(waiting ali saker to check out the web)
    git-tfs-id: [http://developmentvlan:8585/tfs/vanrise.collection]$/;C55511

--- TOneV2/Code/TOneV2/TOne.WhS.Sales.Web/WhS_Sales/Services/SellingRuleService.js
+++ TOneV2/Code/TOneV2/TOne.WhS.Sales.Web/WhS_Sales/Services/SellingRuleService.js
@@ -60,3 +60,31 @@
     appControllers.service('WhS_Sales_SellingRuleService', SellingRuleService);
 
 })(appControllers);
+(function (appControllers) {
+
+	'use strict';
+
+	SupplierTargetMatchService.$inject = ['VRModalService', 'VRNotificationService'];
+
+	function SupplierTargetMatchService(VRModalService, VRNotificationService) {
+		return ({
+			exportSupplierTargetMatch: exportSupplierTargetMatch,
+		});
+
+		function exportSupplierTargetMatch(onExportSupplierTargetMatch) {
+			var settings = {
+			};
+
+			var parameters;
+
+			settings.onScopeReady = function (modalScope) {
+				modalScope.onExportSupplierTargetMatch = onExportSupplierTargetMatch;
+			};
+
+			VRModalService.showModal('/Client/Modules/WhS_Sales/Views/SupplierTargetMatch/ExportSupplierTargetMatchEditor.html', parameters, settings);
+		}
+	}
+
+	appControllers.service('WhS_Sales_SupplierTargetMatchService', SupplierTargetMatchService);
+
+})(appControllers);
