33d23b8416f8d3a59e08015ed3608dfdd21376b6
Hassan Makhour <VANRISE\hassan.makhour>
2019-05-02 08:08:18 +0000

    SupplierTargetMatch Service and export editor
    git-tfs-id: [http://developmentvlan:8585/tfs/vanrise.collection]$/;C55558

---
+++ TOneV2/Code/TOneV2/TOne.WhS.Sales.Web/WhS_Sales/Services/SupplierTargetMatchService.js
@@ -0,0 +1,28 @@
+﻿(function (appControllers) {
+
+	'use strict';
+
+	SupplierTargetMatchService.$inject = ['VRModalService', 'VRNotificationService'];
+
+	function SupplierTargetMatchService(VRModalService, VRNotificationService) {
+		return ({
+			exportSupplierTargetMatch: exportSupplierTargetMatch,
+		});
+
+		function exportSupplierTargetMatch(onExportSupplierTargetMatch) {
+			var settings = {
+			};
+
+			var parameters;
+
+			settings.onScopeReady = function (modalScope) {
+				modalScope.onExportSupplierTargetMatch = onExportSupplierTargetMatch;
+			};
+
+			VRModalService.showModal('/Client/Modules/WhS_Sales/Views/SupplierTargetMatch/ExportSupplierTargetMatchEditor.html', parameters, settings);
+		}
+	}
+
+	appControllers.service('WhS_Sales_SupplierTargetMatchService', SupplierTargetMatchService);
+
+})(appControllers);
