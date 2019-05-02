4482794088586e7256bd8805ec80fa9fbcd99939
Ali Sakr <VANRISE\ali.sakr>
2019-05-02 07:04:27 +0000

    check in for Sales.Web
    git-tfs-id: [http://developmentvlan:8585/tfs/vanrise.collection]$/;C55546

--- TOneV2/Code/TOneV2/TOne.WhS.Sales.Web/WhS_Sales/Services/ExcludedItemsService.js
+++ TOneV2/Code/TOneV2/TOne.WhS.Sales.Web/WhS_Sales/Services/ExcludedItemsService.js
@@ -7,14 +7,13 @@
     function ExcludedItemsService(VRModalService) {
 
         function viewExcludedItems(subscriberProcessInstanceId) {
-            console.log("in service");
             var settings = {};
             var parameters = {
                 subscriberProcessInstanceId: subscriberProcessInstanceId,
             };
             
             VRModalService.showModal('/Client/Modules/WhS_Sales/Views/ExcludedItem.html', parameters, settings);
-        };
+        }
 
         return {
             viewExcludedItems: viewExcludedItems,
