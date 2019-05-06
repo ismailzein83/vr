6f8871dbb31020c1a976f72e664ce751c4536cea
Mohammad Sheaib <VANRISE\mohammad.sheaib>
2019-05-06 13:24:42 +0000

    PermanentFilterSettings Classes
    git-tfs-id: [http://developmentvlan:8585/tfs/vanrise.collection]$/;C55636

---
+++ Vanrise/Code/Vanrise.Analytic.Entities/PermanentFilterSettings.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Vanrise.Entities;
+using Vanrise.GenericData.Entities;
+
+namespace Vanrise.Analytic.Entities
+{
+    public abstract class PermanentFilterSettings
+    {
+        public abstract Guid ConfigId { get; }
+
+        public abstract void ConvertToRecordFilter();
+
+    }
+    public class FilterGroupAnalyticTablePermanentFilter: PermanentFilterSettings
+    {
+        public override Guid ConfigId => new Guid("3DC90928C-8854-4973-9A4F-EC2F2EA4CADE");
+        public override void ConvertToRecordFilter()
+        {
+            throw new NotImplementedException();
+        }
+        public RecordFilterGroup RecordFilterGroup { get; set; }
+
+    }
+
+    public class PermanentFilterSettingsConfig : ExtensionConfiguration
+    {
+        public const string EXTENSION_TYPE = "VR_Analytic_PermanentFilterSettings";
+        public string Editor { get; set; }
+    }
+}
