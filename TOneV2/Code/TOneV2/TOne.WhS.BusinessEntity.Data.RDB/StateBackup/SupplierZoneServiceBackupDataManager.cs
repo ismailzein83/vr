de462bfae2378fea75804f9dfd545e3b5d1ab668
Zeinab Mokdad <VANRISE\zeinab.mokdad>
2019-05-06 08:18:07 +0000

    making tableAlias pubilc
    git-tfs-id: [http://developmentvlan:8585/tfs/vanrise.collection]$/;C55616

--- TOneV2/Code/TOneV2/TOne.WhS.BusinessEntity.Data.RDB/StateBackup/SupplierZoneServiceBackupDataManager.cs
+++ TOneV2/Code/TOneV2/TOne.WhS.BusinessEntity.Data.RDB/StateBackup/SupplierZoneServiceBackupDataManager.cs
@@ -8,7 +8,7 @@ namespace TOne.WhS.BusinessEntity.Data.RDB
     {
         #region RDB
 
-        static string TABLE_ALIAS = "supzbkup";
+        public static string TABLE_ALIAS = "supzbkup";
         public static string TABLE_NAME = "TOneWhS_BE_Bkup_SupplierZoneService";
         const string COL_ID = "ID";
         const string COL_ZoneID = "ZoneID";
