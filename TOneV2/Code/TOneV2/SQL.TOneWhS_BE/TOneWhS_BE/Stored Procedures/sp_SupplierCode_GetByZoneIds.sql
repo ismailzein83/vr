fd5c6be70354544b219ed6eb238476ac27072018
Zeinab Mokdad <VANRISE\zeinab.mokdad>
2019-05-07 11:27:06 +0000

    adding 2 new stored procedure to export sale rate and supplier rate
    git-tfs-id: [http://developmentvlan:8585/tfs/vanrise.collection]$/;C55662

---
+++ TOneV2/Code/TOneV2/SQL.TOneWhS_BE/TOneWhS_BE/Stored Procedures/sp_SupplierCode_GetByZoneIds.sql
@@ -0,0 +1,22 @@
+﻿
+-- ===================================================
+-- Description:	SP to get all SupplierCodes by ZoneIds
+-- ===================================================
+
+CREATE PROCEDURE [TOneWhS_BE].[sp_SupplierCode_GetByZoneIds]
+	@ZonesIDs varchar(max),
+	@effectiveDate DateTime
+AS
+BEGIN
+	SET NOCOUNT ON;
+
+	DECLARE @ZonesIDsTable TABLE (ZoneID INT)
+	INSERT INTO @ZonesIDsTable (ZoneID)
+	SELECT CONVERT(INT, ParsedString) FROM [TOneWhS_BE].[ParseStringList](@ZonesIDs)
+		
+	SELECT  [ID],[Code],[ZoneID],[BED],[EED],[CodeGroupID],[SourceID]
+	FROM	[TOneWhS_BE].[SupplierCode] sc WITH(NOLOCK) 
+	WHERE	[ZoneID] IN (SELECT ZoneID FROM @ZonesIDsTable)
+			AND (sc.EED IS NULL OR sc.EED > @effectiveDate)
+        
+END
---
+++ TOneV2/Code/TOneV2/SQL.TOneWhS_BE/TOneWhS_BE/Stored Procedures/sp_SupplierOtherRate_GetByZoneIds.sql
@@ -0,0 +1,23 @@
+﻿create PROCEDURE  [TOneWhS_BE].[sp_SupplierOtherRate_GetByZoneIds]
+(
+	@ZoneID nvarchar(max), 
+	@EffectiveOn DateTime
+)
+	AS
+	BEGIN
+		SET NOCOUNT ON
+
+		DECLARE @zoneIDsTable TABLE (ZoneId bigint)
+		INSERT INTO @zoneIDsTable (ZoneId)
+		SELECT Convert(int, ParsedString) from [TOneWhS_BE].[ParseStringList](@ZoneID)
+
+		 SELECT rate.[ID], rate.[PriceListID], rate.[ZoneID], rate.[CurrencyID], rate.[Rate],rate.RateTypeID, rate.[BED], rate.[EED], rate.[timestamp],rate.Change
+         FROM	[TOneWhS_BE].[SupplierRate] rate WITH(NOLOCK) 
+				inner join [TOneWhS_BE].[SupplierPriceList] priceList WITH(NOLOCK) on rate.PriceListID=priceList.ID
+
+		 WHERE	rate.ZoneID in(select ZoneId from @zoneIDsTable) and rate.RateTypeID is not null
+				AND   (rate.BED < = @EffectiveOn   and (rate.EED is null or rate.EED  > @EffectiveOn) )
+			
+		
+		SET NOCOUNT OFF
+	END
