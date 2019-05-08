6ce57be51fb2a8ea83fcacca315de2946be78db2
Mohammad Sheaib <VANRISE\mohammad.sheaib>
2019-05-08 09:11:28 +0000

    AccountManagerAnalyticPermanentFilter Class
    git-tfs-id: [http://developmentvlan:8585/tfs/vanrise.collection]$/;C55680

---
+++ TOneV2/Code/TOneV2/TOne.WhS.BusinessEntity.Business/AccountManager/AccountManagerAnalyticPermanentFilter.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Vanrise.Analytic.Entities;
+
+namespace TOne.WhS.BusinessEntity.Business
+{
+   public class AccountManagerAnalyticPermanentFilter: AnalyticTablePermanentFilterSettings
+    {
+        public override Guid ConfigId => new Guid("4A3AD674-9ADB-40C6-BEFD-A1813F08F333");
+        public override void ConvertToRecordFilter()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
