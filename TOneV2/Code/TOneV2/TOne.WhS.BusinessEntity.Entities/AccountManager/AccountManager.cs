0b92f5c543a2699470b5374390ed357836bd990e
Fatima Hammoud <VANRISE\fatima.hammoud>
2019-05-09 07:29:41 +0000

    Adding new classes : AccountManager and AccountManagerAssignment
    git-tfs-id: [http://developmentvlan:8585/tfs/vanrise.collection]$/;C55696

---
+++ TOneV2/Code/TOneV2/TOne.WhS.BusinessEntity.Entities/AccountManager/AccountManager.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TOne.WhS.BusinessEntity.Entities
+{
+    public class AccountManager
+    {
+        public int AccountManagerId { get; set; }
+        public int ParentId { get; set; }
+        public int UserId { get; set; }
+        public DateTime CreatedTime { get; set; }
+        public DateTime LastModifiedTime { get; set; }
+        public int CreatedBy { get; set; }
+        public int LastModifiedBy { get; set; }
+    }
+}
---
+++ TOneV2/Code/TOneV2/TOne.WhS.BusinessEntity.Entities/AccountManager/AccountManagerAssignment.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TOne.WhS.BusinessEntity.Entities
+{
+    public class AccountManagerAssignment
+    {
+        public int AccountManagerAssignmentId { get; set; }
+        public int AccountManagerId { get; set; }
+        public int CarrierAccountId { get; set; }
+        public bool CustomerAssigned { get; set; }
+        public bool SupplierAssigned { get; set; }
+        public DateTime BED { get; set; }
+        public DateTime? EED { get; set; }
+        public DateTime CreatedTime { get; set; }
+        public DateTime LastModifiedTime { get; set; }
+        public int CreatedBy { get; set; }
+        public int LastModifiedBy { get; set; }
+    }
+}
