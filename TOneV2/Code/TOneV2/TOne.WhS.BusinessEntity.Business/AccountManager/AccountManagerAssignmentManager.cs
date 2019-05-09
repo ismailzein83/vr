26f6dc158e0fc6e0bbce245dab92e4ae16b3cd44
Fatima Hammoud <VANRISE\fatima.hammoud>
2019-05-09 07:23:23 +0000

    Adding AccountManagerAssignmentManager
    git-tfs-id: [http://developmentvlan:8585/tfs/vanrise.collection]$/;C55695

--- TOneV2/Code/TOneV2/TOne.WhS.BusinessEntity.Business/AccountManager/AccountManagerAssignmentManager.cs
+++ TOneV2/Code/TOneV2/TOne.WhS.BusinessEntity.Business/AccountManager/AccountManagerAssignmentManager.cs
@@ -11,74 +11,79 @@ namespace TOne.WhS.BusinessEntity.Business
 {
     public class AccountManagerAssignmentManager
     {
-        //static Guid businessEntityDefinitionId = new Guid("a2e4b8e6-2605-40e6-8184-8798add51bab");
+        static Guid businessEntityDefinitionId = new Guid("a2e4b8e6-2605-40e6-8184-8798add51bab");
 
-        //#region Public Methods
+        #region Public Methods
 
-        //public IEnumerable<AccountManagerAssignment> GetAllAccountManagerAssignments()
-        //{
-        //    return GetCachedAccountManagerAssignment().Values;
-        //}
+        public IEnumerable<AccountManagerAssignment> GetAllAccountManagersAssignment()
+        {
+            return GetCachedAccountManagerAssignment().Values;
+        }
 
-        //public AccountManagerAssignment GetAccountManagerAssignmentById(int accountManagerAssignmentId)
-        //{
-        //    return GetCachedAccountManagerAssignment().GetRecord(accountManagerAssignmentId);
-        //}
+        public AccountManagerAssignment GetAccountManagerAssignmentById(int accountManagerAssignmentId)
+        {
+            return GetCachedAccountManagerAssignment().GetRecord(accountManagerAssignmentId);
+        }
 
-        //public AccountManagerAssignment GetAccountManagerAssignmentByCarrierAccountId(int carrierAccountId)
-        //{
-        //    return GetCachedAccountManagerAssignment().FindRecord(itm => itm.CarrierAccountId == carrierAccountId);
-        //}
+        public AccountManagerAssignment GetAccountManagerAssignmentByCarrierAccountId(int carrierAccountId)
+        {
+            return GetCachedAccountManagerAssignment().FindRecord(itm => itm.CarrierAccountId == carrierAccountId);
+        }
 
-        //public IEnumerable<int> GetAffectedCarrierAccountIds()
-        //{
-        //    List<int> affectedCarrierAccountIds = new List<int>();
-        //    var allAccountManagerAssignments = GetCachedAccountManagerAssignment().Values;
-        //    foreach(var assignment in allAccountManagerAssignments)
-        //    {
-        //        affectedCarrierAccountIds.Add(assignment.CarrierAccountId);
-        //    }
-        //    return affectedCarrierAccountIds;
-        //}
+        public IEnumerable<AccountManagerAssignment> GetAllAccountManagersAssignmentByCarrierAccountId(int carrierAccountId)
+        {
+            return GetCachedAccountManagerAssignment().FindAllRecords(itm => itm.CarrierAccountId == carrierAccountId);
+        }
 
-        //#endregion
+        public IEnumerable<int> GetAffectedCarrierAccountIds()
+        {
+            List<int> affectedCarrierAccountIds = new List<int>();
+            var allAccountManagerAssignments = GetCachedAccountManagerAssignment().Values;
+            foreach (var assignment in allAccountManagerAssignments)
+            {
+                affectedCarrierAccountIds.Add(assignment.CarrierAccountId);
+            }
+            return affectedCarrierAccountIds;
+        }
 
-        //#region Private Methods
-        //private Dictionary<int, AccountManagerAssignment> GetCachedAccountManagerAssignment()
-        //{
-        //    IGenericBusinessEntityManager genericBusinessEntityManager = Vanrise.GenericData.Entities.BusinessManagerFactory.GetManager<IGenericBusinessEntityManager>();
-        //    return genericBusinessEntityManager.GetCachedOrCreate("GetCachedAccountManagerAssignment", businessEntityDefinitionId, () =>
-        //    {
-        //        Dictionary<int, AccountManagerAssignment> result = new Dictionary<int, AccountManagerAssignment>();
-        //        IEnumerable<GenericBusinessEntity> genericBusinessEntities = genericBusinessEntityManager.GetAllGenericBusinessEntities(businessEntityDefinitionId);
-        //        if (genericBusinessEntities != null)
-        //        {
-        //            foreach (GenericBusinessEntity genericBusinessEntity in genericBusinessEntities)
-        //            {
-        //                AccountManagerAssignment accountManagerAssignment = new AccountManagerAssignment()
-        //                {
-        //                    AccountManagerAssignmentId = (int)genericBusinessEntity.FieldValues.GetRecord("ID"),
-        //                    AccountManagerId = (int)genericBusinessEntity.FieldValues.GetRecord("AccountManagerId"),
-        //                    CarrierAccountId = (int)genericBusinessEntity.FieldValues.GetRecord("CarrierAccountId"),
-        //                    CustomerAssigned = (bool)genericBusinessEntity.FieldValues.GetRecord("CustomerAssigned"),
-        //                    SupplierAssigned = (bool)genericBusinessEntity.FieldValues.GetRecord("SupplierAssigned"),
-        //                    BED = (DateTime)genericBusinessEntity.FieldValues.GetRecord("BED"),
-        //                    EED = (DateTime?)genericBusinessEntity.FieldValues.GetRecord("EED"),
-        //                    CreatedTime = (DateTime)genericBusinessEntity.FieldValues.GetRecord("CreatedTime"),
-        //                    LastModifiedTime = (DateTime)genericBusinessEntity.FieldValues.GetRecord("LastModifiedTime"),
-        //                    CreatedBy = (int)genericBusinessEntity.FieldValues.GetRecord("CreatedBy"),
-        //                    LastModifiedBy = (int)genericBusinessEntity.FieldValues.GetRecord("LastModifiedBy"),
-        //                };
-        //                result.Add(accountManagerAssignment.AccountManagerAssignmentId, accountManagerAssignment);
-        //            }
-        //        }
-        //        return result;
-        //    });
-        //}
-        //#endregion
+        #endregion
+
+        #region Private Methods
+        private Dictionary<int, AccountManagerAssignment> GetCachedAccountManagerAssignment()
+        {
+            IGenericBusinessEntityManager genericBusinessEntityManager = Vanrise.GenericData.Entities.BusinessManagerFactory.GetManager<IGenericBusinessEntityManager>();
+            return genericBusinessEntityManager.GetCachedOrCreate("GetCachedAccountManagerAssignment", businessEntityDefinitionId, () =>
+            {
+                Dictionary<int, AccountManagerAssignment> result = new Dictionary<int, AccountManagerAssignment>();
+                IEnumerable<GenericBusinessEntity> genericBusinessEntities = genericBusinessEntityManager.GetAllGenericBusinessEntities(businessEntityDefinitionId);
+                if (genericBusinessEntities != null)
+                {
+                    foreach (GenericBusinessEntity genericBusinessEntity in genericBusinessEntities)
+                    {
+                        AccountManagerAssignment accountManagerAssignment = new AccountManagerAssignment()
+                        {
+                            AccountManagerAssignmentId = (int)genericBusinessEntity.FieldValues.GetRecord("ID"),
+                            AccountManagerId = (int)genericBusinessEntity.FieldValues.GetRecord("AccountManagerId"),
+                            CarrierAccountId = (int)genericBusinessEntity.FieldValues.GetRecord("CarrierAccountId"),
+                            CustomerAssigned = (bool)genericBusinessEntity.FieldValues.GetRecord("CustomerAssigned"),
+                            SupplierAssigned = (bool)genericBusinessEntity.FieldValues.GetRecord("SupplierAssigned"),
+                            BED = (DateTime)genericBusinessEntity.FieldValues.GetRecord("BED"),
+                            EED = (DateTime?)genericBusinessEntity.FieldValues.GetRecord("EED"),
+                            CreatedTime = (DateTime)genericBusinessEntity.FieldValues.GetRecord("CreatedTime"),
+                            LastModifiedTime = (DateTime)genericBusinessEntity.FieldValues.GetRecord("LastModifiedTime"),
+                            CreatedBy = (int)genericBusinessEntity.FieldValues.GetRecord("CreatedBy"),
+                            LastModifiedBy = (int)genericBusinessEntity.FieldValues.GetRecord("LastModifiedBy"),
+                        };
+                        result.Add(accountManagerAssignment.AccountManagerAssignmentId, accountManagerAssignment);
+                    }
+                }
+                return result;
+            });
+        }
+        #endregion
 
 
-        //#region Mappers
-        //#endregion
+        #region Mappers
+        #endregion
     }
 }
