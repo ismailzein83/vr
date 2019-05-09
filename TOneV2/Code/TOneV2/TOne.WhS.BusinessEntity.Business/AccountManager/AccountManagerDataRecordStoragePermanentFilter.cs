da6d8a595f4df162e0b5ce34b1e5be68f2fecb07
Mohammad Sheaib <VANRISE\mohammad.sheaib>
2019-05-09 07:30:47 +0000

    AccountManagerDataRecordStoragePermanentFilter Class
    git-tfs-id: [http://developmentvlan:8585/tfs/vanrise.collection]$/;C55697

---
+++ TOneV2/Code/TOneV2/TOne.WhS.BusinessEntity.Business/AccountManager/AccountManagerDataRecordStoragePermanentFilter.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Vanrise.GenericData.Entities;
+
+namespace TOne.WhS.BusinessEntity.Business
+{
+   public class AccountManagerDataRecordStoragePermanentFilter : DataRecordStoragePermanentFilterSettings
+    {
+        public override Guid ConfigId => new Guid("A6F4D0D4-3562-4151-8ED8-984CE7A83C20");
+        public override void ConvertToRecordFilter()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
