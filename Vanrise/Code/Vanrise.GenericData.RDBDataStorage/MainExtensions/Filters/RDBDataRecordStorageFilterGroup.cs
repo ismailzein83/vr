f95c05d23d879207a3ddece92c9375f7806903a5
Fatima Hammoud <VANRISE\fatima.hammoud>
2019-05-10 12:20:50 +0000

    Add new class "RDBDataRecordStorageSettingsFilterGroup"
    git-tfs-id: [http://developmentvlan:8585/tfs/vanrise.collection]$/;C55739

---
+++ Vanrise/Code/Vanrise.GenericData.RDBDataStorage/MainExtensions/Filters/RDBDataRecordStorageFilterGroup.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Vanrise.GenericData.Entities;
+
+namespace Vanrise.GenericData.RDBDataStorage.MainExtensions.Filters
+{
+    public class RDBDataRecordStorageSettingsFilterGroup : RDBDataRecordStorageSettingsFilter
+    {
+        public override Guid ConfigId { get { return new Guid("1D2F3E14-AD64-41C6-AE5F-475D1CB75D2D"); } }
+
+        public RecordFilterGroup RecordFilterGroup { get; set; }
+    }
+}
