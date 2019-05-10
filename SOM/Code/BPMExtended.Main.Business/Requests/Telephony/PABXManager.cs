000f7f7d426eabdeb926193abc0c14ef6e64cb4f
Mohamad Abdallah <VANRISE\mohamad.abdallah>
2019-05-10 11:21:19 +0000

    start working with pabx
    git-tfs-id: [http://developmentvlan:8585/tfs/vanrise.collection]$/;C55738

---
+++ SOM/Code/BPMExtended.Main.Business/Requests/Telephony/PABXManager.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Web;
+using Terrasoft.Core;
+
+namespace BPMExtended.Main.Business
+{
+    public class PABXManager
+    {
+        #region User Connection
+        public UserConnection BPM_UserConnection
+        {
+            get
+            {
+                return (UserConnection)HttpContext.Current.Session["UserConnection"];
+            }
+        }
+        #endregion
+
+        #region Public
+
+
+
+        #endregion
+
+
+    }
+}
