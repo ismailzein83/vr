c3ed51bf283b16a95cb234d59a6647c5febc60ac
Ali Atoui <VANRISE\ali.atoui>
2019-05-13 12:30:50 +0000

    Adding CallRestAPIEditorDefinitionSetting, RowsContainerEditorDefinitionSetting and GenericFieldsEditorDefinitionSetting
    git-tfs-id: [http://developmentvlan:8585/tfs/vanrise.collection]$/;C55762

---
+++ Vanrise/Code/Vanrise.GenericData.MainExtensions/GenericBusinessEntity/GenericEditorDefinitionSetting/GenericFieldsEditorDefinitionSetting.cs
@@ -0,0 +1,15 @@
+﻿//using System;
+//using System.Collections.Generic;
+//using Vanrise.GenericData.Entities;
+
+//namespace Vanrise.GenericData.MainExtensions
+//{
+//    public class GenericFieldsEditorDefinitionSetting : VRGenericEditorDefinitionSetting
+//    {
+//        public override Guid ConfigId { get { return new Guid("F290120F-E657-439F-9897-3D1AB8C6E107"); } }
+
+//        public override string RuntimeEditor { get { return "vr-genericdata-genericfieldseditorsetting-runtime"; } }
+
+//        public List<GenericEditorField> Fields { get; set; }
+//    }
+//}
---
+++ Vanrise/Code/Vanrise.GenericData.MainExtensions/GenericBusinessEntity/GenericEditorDefinitionSetting/RowsContainerEditorDefinitionSetting.cs
@@ -0,0 +1,20 @@
+﻿//using System;
+//using System.Collections.Generic;
+//using Vanrise.GenericData.Entities;
+
+//namespace Vanrise.GenericData.MainExtensions
+//{
+//    public class RowsContainerEditorDefinitionSetting : VRGenericEditorDefinitionSetting
+//    {
+//        public override Guid ConfigId { get { return new Guid("747F6659-2541-4008-A9CF-56A604E3F63C"); } }
+
+//        public override string RuntimeEditor { get { return "vr-genericdata-rowscontainereditor-runtime"; } }
+
+//        public List<VRRowContainer> RowContainers { get; set; }
+//    }
+
+//    public class VRRowContainer
+//    {
+//        public List<VRGenericEditorDefinitionSetting> RowSettings { get; set; }
+//    }
+//}
