e9f24bbc3f44f0c05115bbbfde7b737ba69f632e
Fatima Al Heck <VANRISE\fatima.alheck>
2019-05-14 11:59:30 +0000

    Adding FieldComparisonCondition class (inherits from CompositeRecordCondition) and its corresponding directives
    git-tfs-id: [http://developmentvlan:8585/tfs/vanrise.collection]$/;C55791

---
+++ Vanrise/Code/Vanrise.GenericData.MainExtensions/CompositeRecordCondition/Runtime/FieldComparisonCondition.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using Vanrise.GenericData.Entities;
+
+namespace Vanrise.GenericData.MainExtensions.CompositeRecordCondition.Runtime
+{
+    public class FieldComparisonCondition : Entities.CompositeRecordCondition
+    {
+        public override Guid ConfigId { get { return new Guid("0D3624F1-6F7D-46D4-8A00-0D9F5DD67CAA"); } }
+
+        public override bool Evaluate(ICompositeRecordConditionEvaluateContext context)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
