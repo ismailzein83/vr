989198da06cb7f003ad90f54ab696dd376530908
Samer Haidar <VANRISE\samer.haidar>
2019-05-14 08:57:22 +0000

    Adding SendEmailGenericBEAction
    git-tfs-id: [http://developmentvlan:8585/tfs/vanrise.collection]$/;C55777

---
+++ Vanrise/Code/Vanrise.GenericData.MainExtensions/GenericBusinessEntity/GenericBEActions/SendEmailGenericBEAction.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Vanrise.GenericData.Business;
+
+namespace Vanrise.GenericData.MainExtensions.GenericBusinessEntity.GenericBEActions
+{
+    public class SendEmailGenericBEAction : GenericBEActionSettings
+    {
+        public override Guid ConfigId
+        {
+            get { return new Guid("CBAFD46F-676F-4CC1-ADA4-4DF7ED762C95"); }
+        }
+        public override string ActionTypeName { get { return "SendEmailGenericBEAction"; } }
+        public override string ActionKind { get { return "SendEmail"; } }
+        public Guid? MailMessageTypeId { get; set; }
+        public string EntityObjectName { get; set; }
+        public string InfoType { get; set; }
+        public List<SendEmailObjectInfo> SendEmailObjectsInfo { get; set; }
+        public override bool DoesUserHaveAccess(IGenericBEActionDefinitionCheckAccessContext context)
+        {
+            return base.DoesUserHaveAccess(context);
+        }
+    }
+    public class SendEmailObjectInfo
+    {
+        public Guid SendEmailObjectInfoId { get; set; }
+        public string InfoType { get; set; }
+        public string ObjectName { get; set; }
+
+    }
+}
