f2c06614249d286dfc32d2ad21faf058f243ca62
Hassan Makhour <VANRISE\hassan.makhour>
2019-05-15 09:39:01 +0000

    EndPointRouteStatus handler
    git-tfs-id: [http://developmentvlan:8585/tfs/vanrise.collection]$/;C55815

---
+++ NetworkProvisioning/Code/NP.IVSwitch.Business/EndPointRouteStatusChangedChangedHandler.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TOne.WhS.BusinessEntity.Business.CarrierAccounts;
+using Vanrise.Entities;
+
+namespace NP.IVSwitch.Business
+{
+	public class EndPointRouteStatusChangedHandler : CarrierAccountStatusChangedEventHandler
+	{
+		public override Guid ConfigId {
+			get { return new Guid("C7001AB0-AF60-4BEC-BB9E-C91EAC6DDD28"); }
+		}
+
+		public override void Execute(IVREventHandlerContext context)
+		{
+			//throw new NotImplementedException();
+		}
+	}
+}
