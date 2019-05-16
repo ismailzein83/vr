b2153069a3d640dffcc0255a5dfcdba0b607cd1e
yara.bousalman <yara.bousalman@vanrise.tfs.local>
2019-05-16 12:20:25 +0000

    Adding enums
    git-tfs-id: [http://developmentvlan:8585/tfs/vanrise.collection]$/;C55852

---
+++ Retail/Code/Retail.RA.Web/Retail_RA/Constant/SMSTaxChargeTypeEnum.js
@@ -0,0 +1,4 @@
+﻿app.constant('Retail_RA_SMSTaxChargeTypeEnum', {
+    PerSMS: { value: 0, description: "Per SMS" },
+    Percentage: { value: 1, description: "Percentage" }
+});
---
+++ Retail/Code/Retail.RA.Web/Retail_RA/Constant/VoiceTaxChargeTypeEnum.js
@@ -0,0 +1,4 @@
+﻿app.constant('Retail_RA_VoiceTaxChargeTypeEnum', {
+    PerMinute: { value: 0, description: "Per minute" },
+    Percentage: { value: 1, description: "Percentage" }
+});
