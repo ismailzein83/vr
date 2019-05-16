3c78ea668cd8f8f53cd0de7a1b308d2b9a7b06ec
yara.bousalman <yara.bousalman@vanrise.tfs.local>
2019-05-16 11:39:16 +0000

    Adding prepaid and postpaid tax rule definition settings
    git-tfs-id: [http://developmentvlan:8585/tfs/vanrise.collection]$/;C55849

---
+++ Retail/Code/Retail.RA.Business/TaxRules/Postpaid/PostpaidTaxRuleDefinitionSettings.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Vanrise.GenericData.Entities;
+
+namespace Retail.RA.Business
+{
+    public class PostpaidTaxRuleDefinitionSettings : GenericRuleDefinitionSettings
+    {
+        public override Guid ConfigId { get { return new Guid("34F5527C-E05D-49BC-9359-8CBD889EB224"); } }
+
+        public override List<string> GetFieldNames()
+        {
+            return new List<string> { };
+        }
+    }
+}
