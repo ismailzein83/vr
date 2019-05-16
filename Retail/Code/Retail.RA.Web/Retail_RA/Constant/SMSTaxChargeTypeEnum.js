8fbb0d86011b7e7aa68de3091f1415809a1d8476
yara.bousalman <yara.bousalman@vanrise.tfs.local>
2019-05-16 12:45:03 +0000

    deleting unwanted files
    git-tfs-id: [http://developmentvlan:8585/tfs/vanrise.collection]$/;C55860

--- Retail/Code/Retail.RA.Web/Retail_RA/Constant/SMSTaxChargeTypeEnum.js
+++
@@ -1,4 +0,0 @@
-﻿app.constant('Retail_RA_SMSTaxChargeTypeEnum', {
-    PerSMS: { value: 0, description: "Per SMS" },
-    Percentage: { value: 1, description: "Percentage" }
-});
--- Retail/Code/Retail.RA.Web/Retail_RA/Constant/VoiceTaxChargeTypeEnum.js
+++
@@ -1,4 +0,0 @@
-﻿app.constant('Retail_RA_VoiceTaxChargeTypeEnum', {
-    PerMinute: { value: 0, description: "Per minute" },
-    Percentage: { value: 1, description: "Percentage" }
-});
