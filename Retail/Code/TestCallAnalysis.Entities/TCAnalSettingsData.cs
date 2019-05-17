a6c06cacbcc650b539ceedc4af0cf24a341bfeb6
Fatima Hammoud <VANRISE\fatima.hammoud>
2019-05-17 11:51:16 +0000

    Adding new class TCAnalSettingsData
    git-tfs-id: [http://developmentvlan:8585/tfs/vanrise.collection]$/;C55875

---
+++ Retail/Code/TestCallAnalysis.Entities/TCAnalSettingsData.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Vanrise.Entities;
+
+namespace TestCallAnalysis.Entities
+{
+    public class TCAnalSettingsData : SettingData
+    {
+        public static string SETTING_TYPE = "Retail_TCAnal_InstanceSettings";
+        public TimeSpan TimeMargin { get; set; }
+        public TimeSpan TimeOutMargin { get; set; }
+    }
+}
