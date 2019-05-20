d36e0091e61d453dca285f5091ae95be1fce568d
Ali Sakr <VANRISE\ali.sakr>
2019-05-20 10:51:58 +0000

    adding new properties
    git-tfs-id: [http://developmentvlan:8585/tfs/vanrise.collection]$/;C55895

--- TOneV2/Code/TOneV2/TOne.WhS.BusinessEntity.Entities/AccountManager/AccountManagerSettings.cs
+++ TOneV2/Code/TOneV2/TOne.WhS.BusinessEntity.Entities/AccountManager/AccountManagerSettings.cs
@@ -3,10 +3,16 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Vanrise.Entities;
 
-namespace TOne.WhS.BusinessEntity.Entities.AccountManager
+namespace TOne.WhS.BusinessEntity.Entities
 {
-    class AccountManagerSettings
+    public class AccountManagerSettings : SettingData
     {
+        public CustomerFiltering CustomerFiltering { get; set; }
+    }
+    public class CustomerFiltering
+    {
+        public bool RatePlan { get; set; }
     }
 }
