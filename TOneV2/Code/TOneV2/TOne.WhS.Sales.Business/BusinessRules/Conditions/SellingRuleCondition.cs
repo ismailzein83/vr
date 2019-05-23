90b600dcc7dfcc94ded4e9501417a5b23151603e
Ali Sakr <VANRISE\ali.sakr>
2019-05-23 11:51:53 +0000

    fixing issue
    git-tfs-id: [http://developmentvlan:8585/tfs/vanrise.collection]$/;C55990

--- TOneV2/Code/TOneV2/TOne.WhS.Sales.Business/BusinessRules/Conditions/SellingRuleCondition.cs
+++ TOneV2/Code/TOneV2/TOne.WhS.Sales.Business/BusinessRules/Conditions/SellingRuleCondition.cs
@@ -3,11 +3,35 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using TOne.WhS.Sales.Entities;
+using Vanrise.Common;
 
 namespace TOne.WhS.Sales.Business.BusinessRules
 {
     public class SellingRuleCondition : Vanrise.BusinessProcess.Entities.BusinessRuleCondition
     {
+        public override bool ShouldValidate(Vanrise.BusinessProcess.Entities.IRuleTarget target)
+        {
+            return target is AllDataByZone;
+        }
+        public override bool Validate(Vanrise.BusinessProcess.Entities.IBusinessRuleConditionValidateContext context)
+        {
+            List<string> invalidZones = new List<string>();
+            var ratePlanContext = context.GetExtension<IRatePlanContext>();
+            ratePlanContext.ThrowIfNull("ratePlanContext");
 
+         
+
+            if (invalidZones.Count > 0)
+            {
+                return false;
+            }
+
+            return true;
+        }
+        public override string GetMessage(Vanrise.BusinessProcess.Entities.IRuleTarget target)
+        {
+            throw new NotImplementedException();
+        }
     }
 }
