bad871ed7872c200421f8e52e8bc0f27acfb79a5
Ali Sakr <VANRISE\ali.sakr>
2019-05-23 11:26:56 +0000

    adding new Business Rule condition
    git-tfs-id: [http://developmentvlan:8585/tfs/vanrise.collection]$/;C55989

---
+++ TOneV2/Code/TOneV2/TOne.WhS.Sales.Business/BusinessRules/Conditions/SellingRuleCondition.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TOne.WhS.Sales.Business.BusinessRules
+{
+    public class SellingRuleCondition : Vanrise.BusinessProcess.Entities.BusinessRuleCondition
+    {
+
+    }
+}
