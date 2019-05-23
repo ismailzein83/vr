af1745b67c8c66766c17e6e5eb27182ebed49476
Mohammad Sheaib <VANRISE\mohammad.sheaib>
2019-05-23 14:09:35 +0000

    Adding ConditionFilterAnalyticTablePermanentFilter Class
    git-tfs-id: [http://developmentvlan:8585/tfs/vanrise.collection]$/;C55999

--- Vanrise/Code/Vanrise.Analytic.Entities/AnalyticTablePermanentFilterSettings.cs
+++ Vanrise/Code/Vanrise.Analytic.Entities/AnalyticTablePermanentFilterSettings.cs
@@ -25,7 +25,41 @@ namespace Vanrise.Analytic.Entities
         public RecordFilterGroup RecordFilterGroup { get; set; }
 
     }
+    public class ConditionFilterAnalyticTablePermanentFilter : AnalyticTablePermanentFilterSettings
+    {
+        public override Guid ConfigId => new Guid("A4FCECC7-375F-4069-B0A5-7CCD1AE974CB");
+        public override RecordFilterGroup ConvertToRecordFilter(IAnalyticTablePermanentFilterContext context)
+        {
+            var recordFilterGroup = new RecordFilterGroup()
+            {
+                LogicalOperator = LogicalOperator,
+                Filters = new List<RecordFilter>()
+            };
+            if (ConditionFilterItems != null && ConditionFilterItems.Count > 0)
+            {
+                foreach (var filter in ConditionFilterItems)
+                {
+                    if (filter.Settings != null)
+                    {
+                        AnalyticTablePermanentFilterContext filterContext = new AnalyticTablePermanentFilterContext();
+                        recordFilterGroup.Filters.Add(filter.Settings.ConvertToRecordFilter(filterContext));
+                    }
+                }
+            }
+            return recordFilterGroup;
+        }
 
+        public RecordQueryLogicalOperator LogicalOperator { get; set; }
+
+        public List<AnalyticTableConditionFilterItem> ConditionFilterItems { get; set; }
+    }
+
+    public class AnalyticTableConditionFilterItem
+    {
+        public Guid ConditionFilterItemId { get; set; }
+        public string Name { get; set; }
+        public AnalyticTablePermanentFilterSettings Settings { get; set; }
+    }
     public class AnalyticTablePermanentFilterSettingsConfig : ExtensionConfiguration
     {
         public const string EXTENSION_TYPE = "VR_Analytic_PermanentFilterSettings";
