a94c824d7a3775193d4d9803072887dd4627e142
yara.bousalman <yara.bousalman@vanrise.tfs.local>
2019-05-28 12:38:08 +0000

    Adding RetailReconciliationMemoryAnalyticDataManager
    git-tfs-id: [http://developmentvlan:8585/tfs/vanrise.collection]$/;C56089

---
+++ Retail/Code/Retail.RA.Business/Reconciliation/RetailReconciliation.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Vanrise.Analytic.Business;
+using Vanrise.Analytic.Entities;
+
+namespace Retail.RA.Business
+{
+    public class RetailReconciliationMemoryAnalyticDataManager : MemoryAnalyticDataManager
+    {
+        #region MemoryAnalyticDataManager
+        public override List<RawMemoryRecord> GetRawRecords(AnalyticQuery query, List<string> neededFieldNames)
+        {
+            throw new NotImplementedException();
+        }
+        #endregion
+    }
+}
