a0fed603123039c82902e5bfb07c448ac2e45795
Mohammad Yahfoufi <VANRISE\mohammad.yahfoufi>
2019-05-28 09:05:13 +0000

    adding Invoice Manager
    git-tfs-id: [http://developmentvlan:8585/tfs/vanrise.collection]$/;C56071

---
+++ SOM/Code/BPMExtended.Main.Business/Billing/InvoiceManager.cs
@@ -0,0 +1,23 @@
+﻿using BPMExtended.Main.Common;
+using BPMExtended.Main.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using BPMExtended.Main.SOMAPI;
+
+namespace BPMExtended.Main.Business
+{
+    public class InvoiceManager
+    {
+
+        #region Public
+        
+        #endregion
+
+        #region Mappers
+       
+        #endregion
+    }
+}
