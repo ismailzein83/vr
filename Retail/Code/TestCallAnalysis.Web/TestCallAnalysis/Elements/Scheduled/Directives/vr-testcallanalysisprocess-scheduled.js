04927b08418f43f467e92a5e00c69444f167537b
Fatima Hammoud <VANRISE\fatima.hammoud>
2019-05-28 09:11:20 +0000

    Adding scheduled editor for testCallAnalysis process
    git-tfs-id: [http://developmentvlan:8585/tfs/vanrise.collection]$/;C56073

---
+++ Retail/Code/TestCallAnalysis.Web/TestCallAnalysis/Elements/Scheduled/Directives/vr-testcallanalysisprocess-scheduled.js
@@ -0,0 +1,58 @@
+﻿"use strict";
+
+app.directive("vrTestcallanalysisprocessScheduled", ['UtilsService', 'VRUIUtilsService',
+    function (UtilsService, VRUIUtilsService) {
+
+    var directiveDefinitionObject = {
+        restrict: "E",
+        scope: {
+            onReady: "="
+        },
+        controller: function ($scope, $element, $attrs) {
+            var ctrl = this;
+
+            var directiveConstructor = new DirectiveConstructor($scope, ctrl);
+            directiveConstructor.initializeController();
+        },
+        controllerAs: "ctrl",
+        bindToController: true,
+        compile: function (element, attrs) {
+            return {
+                pre: function ($scope, iElem, iAttrs, ctrl) {
+
+                }
+            };
+        },
+        templateUrl: "/Client/Modules/TestCallAnalysis/Elements/Scheduled/Directives/Templates/TestCallAnalysisProcessScheduledTemplate.html"
+    };
+
+    function DirectiveConstructor($scope, ctrl) {
+        this.initializeController = initializeController;
+
+        function initializeController() {
+            $scope.scopeModel = {};
+
+            defineAPI();
+        }
+
+        function defineAPI() {
+            var api = {};
+
+            api.load = function (payload) {
+                var promises = [];
+                return UtilsService.waitMultiplePromises(promises);
+            };
+
+            api.getData = function () {
+                return {
+                    $type: "TestCallAnalysis.BP.Arguments.CDRCorrelationAndAnalysisProcessInput, TestCallAnalysis.BP.Arguments",
+                };
+            };
+
+            if (ctrl.onReady != null)
+                ctrl.onReady(api);
+        }
+    }
+
+    return directiveDefinitionObject;
+}]);
