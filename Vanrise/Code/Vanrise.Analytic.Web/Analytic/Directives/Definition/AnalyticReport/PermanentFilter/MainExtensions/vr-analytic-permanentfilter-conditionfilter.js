a4f807f938ed7cc8ea09046c9e98184448cfc542
Mohammad Sheaib <VANRISE\mohammad.sheaib>
2019-05-29 07:43:56 +0000

    Commented
    git-tfs-id: [http://developmentvlan:8585/tfs/vanrise.collection]$/;C56102

---
+++ Vanrise/Code/Vanrise.Analytic.Web/Analytic/Directives/Definition/AnalyticReport/PermanentFilter/MainExtensions/vr-analytic-permanentfilter-conditionfilter.js
@@ -0,0 +1,148 @@
+﻿//(function (app) {
+
+//    'use strict';
+
+//    PermanentfilterConditionfilter.$inject = ['VRUIUtilsService', 'UtilsService', 'VR_Analytic_AnalyticTableService'];
+
+//    function PermanentfilterConditionfilter(VRUIUtilsService, UtilsService, VR_Analytic_AnalyticTableService) {
+//        return {
+//            restrict: 'E',
+//            scope: {
+//                onReady: '='
+//            },
+//            controller: function ($scope, $element, $attrs) {
+//                var ctrl = this;
+//                var permanentFilterConfitionFilter = new PermanentFilterConditionFilter($scope, ctrl, $attrs);
+//                permanentFilterConfitionFilter.initializeController();
+//            },
+//            controllerAs: 'ctrl',
+//            bindToController: true,
+//            compile: function (element, attrs) {
+
+//            },
+//            templateUrl: "/Client/Modules/Analytic/Directives/Definition/AnalyticReport/PermanentFilter/MainExtensions/Templates/PermanentFilterConditionFilter.html"
+//        };
+
+//        function PermanentFilterConditionFilter($scope, ctrl, $attrs) {
+
+//            this.initializeController = initializeController;
+
+//            var gridAPI;
+//            var analyticTableId;
+//            var logicalOperator;
+//            var logicalOperatorDirectiveAPI;
+//            var logicalOperatorDirectiveReadyPromiseDeferred = UtilsService.createPromiseDeferred();
+
+//            ctrl.datasource = [];
+
+//            var conditionFilterItems;
+
+//            function initializeController() {
+//                $scope.scopeModel = {};
+
+//                ctrl.onGridReady = function (api) {
+//                    gridAPI = api;
+//                    defineAPI();
+//                }; 
+//                $scope.scopeModel.addConditionFilterItem = function () {
+//                    var onConditionFilterItemAdded = function (row) {
+//                        ctrl.datasource.push(row);
+//                    };
+//                    VR_Analytic_AnalyticTableService.addConditionFilterItem(analyticTableId, onConditionFilterItemAdded);
+//                };
+//                $scope.scopeModel.onLogicalOperatorDirectiveReady = function (api) {
+//                    logicalOperatorDirectiveAPI = api;
+//                    logicalOperatorDirectiveReadyPromiseDeferred.resolve();
+//                };
+
+//                $scope.scopeModel.onDeleteRow = function (dataItem) {
+//                    var index = ctrl.datasource.indexOf(dataItem);
+//                    ctrl.datasource.splice(index, 1);
+//                };  
+//                defineMenuActions();
+              
+//            }
+     
+//            function defineAPI() {
+//                var api = {};
+
+//                api.load = function (payload) {
+//                    if (payload != undefined) {
+//                        analyticTableId = payload.analyticTableId;
+//                        logicalOperator = payload.settings != undefined ? payload.settings.LogicalOperator : undefined;
+//                        conditionFilterItems = payload.settings != undefined ? payload.settings.ConditionFilterItems : undefined;
+//                    }
+
+//                    if (conditionFilterItems != undefined && conditionFilterItems.length > 0) {
+//                        for (var i = 0; i < conditionFilterItems.length; i++) {
+//                            ctrl.datasource.push({ Entity: conditionFilterItems[i] });
+//                        }
+//                    }
+//                    var rootPromiseNode = { promises: [loadLogicalOperatorDirective()] };
+
+//                    return UtilsService.waitPromiseNode(rootPromiseNode);
+//                };
+
+//                api.getData = function () {
+//                    return {
+//                        $type: "Vanrise.Analytic.Entities.ConditionFilterAnalyticTablePermanentFilter,Vanrise.Analytic.Entities",
+//                        ConditionFilterItems: getConditionFilterItems(),
+//                        LogicalOperator: logicalOperatorDirectiveAPI.getData()
+//                    };
+//                };
+
+//                if (ctrl.onReady != null) {
+//                    ctrl.onReady(api);
+//                }
+//            }
+
+//            function getConditionFilterItems() {
+//                var items = [];
+//                if (ctrl.datasource.length > 0) {
+//                    for (var j = 0; j < ctrl.datasource.length; j++) {
+//                        items.push(ctrl.datasource[j].Entity);
+//                    }
+//                }
+//                return items;
+//            }
+//            function loadLogicalOperatorDirective() {
+//                var logicalOperatorDirectiveLoadedPromiseDeferred = UtilsService.createPromiseDeferred();
+
+//                logicalOperatorDirectiveReadyPromiseDeferred.promise.then(function () {
+
+//                    var logicalOperatorDirectivePayload;
+//                    if (conditionFilterItems != undefined) {
+//                        logicalOperatorDirectivePayload = { LogicalOperator: logicalOperator };
+//                    }
+//                    VRUIUtilsService.callDirectiveLoad(logicalOperatorDirectiveAPI, logicalOperatorDirectivePayload, logicalOperatorDirectiveLoadedPromiseDeferred);
+//                });
+
+//                return logicalOperatorDirectiveLoadedPromiseDeferred.promise;
+//            }
+
+//            function defineMenuActions() {
+//                var defaultMenuActions = [
+//                    {
+//                        name: "Edit",
+//                        clicked: editConditionFilterItem
+//                    }];
+
+//                ctrl.gridMenuActions = function (dataItem) {
+//                    return defaultMenuActions;
+//                };
+//            }
+
+//            function editConditionFilterItem(dataItem) {
+//                var onConditionFilterItemUpdated = function (conditionFilterItem) {
+//                    var index = ctrl.datasource.indexOf(dataItem);
+//                    ctrl.datasource[index] = conditionFilterItem;
+//                };
+
+//                VR_Analytic_AnalyticTableService.editConditionFilterItem(analyticTableId,dataItem.Entity, onConditionFilterItemUpdated);
+//            }
+//        }
+//    }
+
+//    app.directive('vrAnalyticPermanentfilterConditionfilter', PermanentfilterConditionfilter);
+
+//})(app);
---
+++ Vanrise/Code/Vanrise.Analytic.Web/Analytic/Views/GenericAnalytic/Definition/AnalyticPermanentFilterConditionFilterItemEditorController.js
@@ -0,0 +1,120 @@
+﻿//(function (appControllers) {
+//    "use strict";
+//    conditionFilterItemEditorController.$inject = ['$scope', 'VRNotificationService', 'VRNavigationService', 'UtilsService', 'VRUIUtilsService'];
+
+//    function conditionFilterItemEditorController($scope, VRNotificationService, VRNavigationService, UtilsService, VRUIUtilsService) {
+
+//        var isEditMode;
+//        var entity;
+//        var permanentFilterSettingsAPI;
+//        var permanentFilterSettingsReadyDeferred = UtilsService.createPromiseDeferred();
+//        var analyticTableId;
+//        $scope.scopeModel = {};
+
+//        loadParameters();
+//        defineScope();
+//        load();
+
+//        function loadParameters() {
+//            var parameters = VRNavigationService.getParameters($scope); 
+//            if (parameters != undefined && parameters != null) {
+//                analyticTableId = parameters.analyticTableId;
+//                entity = parameters.entity;
+//            }
+//            isEditMode = (entity != undefined);
+//        }
+
+//        function defineScope() {
+//            $scope.scopeModel.saveConditionFilterItem = function () {
+
+//                if (isEditMode)
+//                    return updateDataRow();
+//                else
+//                    return insertDataRow();
+//            };
+
+//            $scope.scopeModel.close = function () {
+//                $scope.modalContext.closeModal();
+//            };
+
+//            $scope.scopeModel.onPermanentFilterSettingsDirectiveReady = function (api) {
+//                permanentFilterSettingsAPI = api;
+//                permanentFilterSettingsReadyDeferred.resolve();
+//            };
+//        }
+
+//        function loadPermanentFilterSettingsDirective() {
+//            var loadPermanentFilterSettingsPromiseDeferred = UtilsService.createPromiseDeferred();
+//            permanentFilterSettingsReadyDeferred.promise.then(function () {
+//                    var payload = {
+//                        analyticTableId: analyticTableId,
+//                        settings: entity != undefined  ? entity.Settings : undefined
+//                    };
+//                VRUIUtilsService.callDirectiveLoad(permanentFilterSettingsAPI, payload, loadPermanentFilterSettingsPromiseDeferred);
+//            });
+//            return loadPermanentFilterSettingsPromiseDeferred.promise;
+//        }
+
+//        function load() {
+//            $scope.scopeModel.isLoading = true;
+
+//            if (isEditMode) {
+//                loadAllControls().finally(function () {
+//                    $scope.scopeModel.isLoading = false;
+//                });
+//            }
+//            else
+//                loadAllControls();
+//        }
+
+//        function loadAllControls() {
+
+//            function setTitle() {
+//                if (isEditMode)
+//                    $scope.title = UtilsService.buildTitleForUpdateEditor(entity.Name,"Condition Filter");
+//                else
+//                    $scope.title = UtilsService.buildTitleForAddEditor("Condition Filter");
+//            }
+//            function loadStaticData() {
+//                $scope.scopeModel.Name = entity != undefined ? entity.Name : undefined;
+//            }
+//            return UtilsService.waitMultipleAsyncOperations([loadPermanentFilterSettingsDirective, loadStaticData,setTitle]).catch(function (error) {
+//                VRNotificationService.notifyExceptionWithClose(error, $scope);
+//            }).finally(function () {
+//                $scope.scopeModel.isLoading = false;
+//            });
+//        }
+
+//        function buildDataRowFromScope() {
+
+//            var obj = {
+//                ConditionFilterItemId: UtilsService.guid(),
+//                Name: $scope.scopeModel.Name,
+//                Settings: permanentFilterSettingsAPI.getData()
+//            };
+//            return { Entity: obj };
+//        }
+
+
+//        function insertDataRow() {
+//            $scope.scopeModel.isLoading = true;
+//            if ($scope.onConditionFilterItemAdded != undefined) {
+//                $scope.onConditionFilterItemAdded(buildDataRowFromScope());
+//            }
+//            $scope.modalContext.closeModal();
+//            $scope.scopeModel.isLoading = true;
+//        }
+
+//        function updateDataRow() {
+//            $scope.scopeModel.isLoading = true;
+//            if ($scope.onConditionFilterItemUpdated != undefined) {
+//                $scope.onConditionFilterItemUpdated(buildDataRowFromScope());
+//            }
+//            $scope.modalContext.closeModal();
+//            $scope.scopeModel.isLoading = true;
+
+//        }
+
+//    }
+//    appControllers.controller('VR_Analytic_AnalyticPermanentFilterConditionFilterItemEditorController', conditionFilterItemEditorController);
+//})(appControllers);
