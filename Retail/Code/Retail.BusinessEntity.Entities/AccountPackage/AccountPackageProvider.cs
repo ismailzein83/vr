51cc3aac295f3fce960f8d4d6a61862ec1c7ced7
Ali Atoui <VANRISE\ali.atoui>
2019-05-30 12:37:18 +0000

    Arranging AccountPackageProvider
    git-tfs-id: [http://developmentvlan:8585/tfs/vanrise.collection]$/;C56135

--- Retail/Code/Retail.BusinessEntity.Entities/AccountPackage/AccountPackageProvider.cs
+++ Retail/Code/Retail.BusinessEntity.Entities/AccountPackage/AccountPackageProvider.cs
@@ -7,22 +7,63 @@ namespace Retail.BusinessEntity.Entities
     public abstract class AccountPackageProvider : GenericBEAdditionalSettings
     {
         public abstract Dictionary<AccountEventTime, List<RetailAccountPackage>> GetRetailAccountPackages(IAccountPackageProviderGetRetailAccountPackagesContext context);
-        public abstract bool DoesUserHaveViewPackageAccess(IPackageDefinitionAccessContext context);
-        public abstract bool DoesUserHaveAddPackageAccess(IPackageDefinitionAccessContext context);
-        public abstract bool DoesUserHaveEditPackageAccess(IPackageDefinitionAccessContext context);
 
         public virtual bool TryGetAccount(IAccountPackageProviderTryGetAccountContext context)
         {
             context.Account = null;
             return false;
         }
+
+        public abstract bool DoesUserHaveViewPackageAccess(IPackageDefinitionAccessContext context);
+
+        public abstract bool DoesUserHaveAddPackageAccess(IPackageDefinitionAccessContext context);
+
+        public abstract bool DoesUserHaveEditPackageAccess(IPackageDefinitionAccessContext context);
     }
 
     public interface IAccountPackageProviderGetRetailAccountPackagesContext
     {
         Guid AccountBEDefinitionId { get; }
-
         List<AccountEventTime> AccountEventTimeList { get; }
+    }
+
+
+    public class AccountPackageProviderGetRetailAccountPackagesContext : IAccountPackageProviderGetRetailAccountPackagesContext
+    {
+        public Guid AccountBEDefinitionId { get; set; }
+        public List<AccountEventTime> AccountEventTimeList { get; set; }
+    }
+
+    public interface IAccountPackageProviderTryGetAccountContext
+    {
+        Guid AccountBEDefinitionId { get; }
+        long AccountId { get; }
+        object Account { set; }
+    }
+
+    public class AccountPackageProviderTryGetAccountContext : IAccountPackageProviderTryGetAccountContext
+    {
+        public Guid AccountBEDefinitionId { get; set; }
+        public long AccountId { get; set; }
+        public object Account { get; set; }
+    }
+
+    public struct AccountEventTime
+    {
+        public long AccountId { get; set; }
+        public DateTime EventTime { get; set; }
+
+        public override int GetHashCode()
+        {
+            return this.AccountId.GetHashCode() + this.EventTime.GetHashCode();
+        }
+    }
+
+    public interface IPackageDefinitionAccessContext
+    {
+        Guid PackageDefinitionId { get; }
+        int UserId { get; }
+        Guid AccountBEDefinitionId { get; }
     }
 
     public class PackageDefinitionAccessContext : IPackageDefinitionAccessContext
@@ -30,47 +71,5 @@ namespace Retail.BusinessEntity.Entities
         public Guid PackageDefinitionId { get; set; }
         public int UserId { get; set; }
         public Guid AccountBEDefinitionId { get; set; }
-    }
-    public interface IPackageDefinitionAccessContext
-    {
-        Guid PackageDefinitionId { get; }
-        int UserId { get;}
-        Guid AccountBEDefinitionId { get;  }
-    }
-    public class AccountPackageProviderGetRetailAccountPackagesContext : IAccountPackageProviderGetRetailAccountPackagesContext
-    {
-        public Guid AccountBEDefinitionId { get; set; }
-
-        public List<AccountEventTime> AccountEventTimeList { get; set; }
-    }
-
-    public interface IAccountPackageProviderTryGetAccountContext
-    {
-        Guid AccountBEDefinitionId { get; }
-
-        long AccountId { get; }
-
-        object Account { set; }
-    }
-
-    public class AccountPackageProviderTryGetAccountContext : IAccountPackageProviderTryGetAccountContext
-    {
-        public Guid AccountBEDefinitionId { get; set; }
-
-        public long AccountId { get; set; }
-
-        public object Account { get; set; }
-    }
-
-    public struct AccountEventTime
-    {
-        public long AccountId { get; set; }
-
-        public DateTime EventTime { get; set; }
-
-        public override int GetHashCode()
-        {
-            return this.AccountId.GetHashCode() + this.EventTime.GetHashCode();
-        }
     }
 }
