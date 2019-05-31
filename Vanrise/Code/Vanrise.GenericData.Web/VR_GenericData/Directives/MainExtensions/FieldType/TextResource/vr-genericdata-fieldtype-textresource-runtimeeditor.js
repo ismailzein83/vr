85c351575b7ee0238193f472850b831431d6d0f9
Mohammad Sheaib <VANRISE\mohammad.sheaib>
2019-05-31 10:03:01 +0000

    FieldTextResourceType Defintion + Runtime
    git-tfs-id: [http://developmentvlan:8585/tfs/vanrise.collection]$/;C56154

--- Vanrise/Code/Vanrise.GenericData.Web/VR_GenericData/Directives/MainExtensions/FieldType/TextResource/vr-genericdata-fieldtype-textresource-runtimeeditor.js
+++ Vanrise/Code/Vanrise.GenericData.Web/VR_GenericData/Directives/MainExtensions/FieldType/TextResource/vr-genericdata-fieldtype-textresource-runtimeeditor.js
@@ -1,139 +1,140 @@
-﻿//'use strict';
-//app.directive('vrGenericdataFieldtypeTextresourceRuntimeeditor', ['UtilsService', 'VRUIUtilsService', function (UtilsService, VRUIUtilsService) {
+﻿'use strict';
+app.directive('vrGenericdataFieldtypeTextresourceRuntimeeditor', ['UtilsService', 'VRUIUtilsService', function (UtilsService, VRUIUtilsService) {
 
-//    var directiveDefinitionObject = {
-//        restrict: 'E',
-//        scope: {
-//            onReady: '=',
-//            selectionmode: '@',
-//            normalColNum: '@',
-//            isrequired: '='
-//        },
-//        controller: function ($scope, $element, $attrs) {
-//            var ctrl = this;
-//            $scope.scopeModel = {};
-//            var ctor = new textCtor(ctrl, $scope, $attrs);
-//            ctor.initializeController();
-//        },
-//        controllerAs: 'runtimeEditorCtrl',
-//        bindToController: true,
-//        compile: function (element, attrs) {
-//            return {
-//                pre: function ($scope, iElem, iAttrs, ctrl) {
+    var directiveDefinitionObject = {
+        restrict: 'E',
+        scope: {
+            onReady: '=',
+            selectionmode: '@',
+            normalColNum: '@',
+            isrequired: '='
+        },
+        controller: function ($scope, $element, $attrs) {
+            var ctrl = this;
+            $scope.scopeModel = {};
+            var ctor = new textResourceCtor(ctrl, $scope, $attrs);
+            ctor.initializeController();
+        },
+        controllerAs: 'runtimeEditorCtrl',
+        bindToController: true,
+        compile: function (element, attrs) {
+            return {
+                pre: function ($scope, iElem, iAttrs, ctrl) {
 
-//                }
-//            };
-//        },
-//        templateUrl: function (element, attrs) {
-//            return '/Client/Modules/VR_GenericData/Directives/MainExtensions/FieldType/TextResource/Templates/TextResourceFieldTypeRuntimeTemplate.html';
-//        }
-//    };
+                }
+            };
+        },
+        templateUrl: function (element, attrs) {
+            return '/Client/Modules/VR_GenericData/Directives/MainExtensions/FieldType/TextResource/Templates/TextResourceFieldTypeRuntimeTemplate.html';
+        }
+    };
 
-//    function textCtor(ctrl, $scope, $attrs) {
+    function textResourceCtor(ctrl, $scope, $attrs) {
 
-//        this.initializeController = initializeController;
+        this.initializeController = initializeController;
 
-//        var gridAPI;
-//        function initializeController() {
-//            $scope.scopeModel = {};
-//            $scope.scopeModel.translatedValues = [];
-//            $scope.scopeModel.onGridReady = function (api) {
-//                gridAPI = api;
-//                defineAPI();
-//            }; 
-//            $scope.scopeModel.addTranslatedValue = function () {
-//                var dataItem = {
+        var gridAPI;
+        function initializeController() {
+            $scope.scopeModel = {};
+            $scope.scopeModel.translatedValues = [];
+            $scope.scopeModel.normalColNum = ctrl.normalColNum; 
+            $scope.scopeModel.onGridReady = function (api) {
+                gridAPI = api;
+                defineAPI();
+            }; 
+            $scope.scopeModel.addTranslatedValue = function () {
+                var dataItem = {
                  
-//                };
-//                dataItem.onLanguageSelectorReady = function (api) {
-//                    dataItem.languageSeletorAPI = api;
-//                    var setLoader = function (value) { dataItem.isLanguageSelectorLoading = value; };
-//                    VRUIUtilsService.callDirectiveLoadOrResolvePromise($scope, dataItem.languageSeletorAPI, undefined, setLoader);
-//                };
-//                $scope.scopeModel.translatedValues.push(dataItem);
-//            };
-//            $scope.scopeModel.onDeleteRow = function (dataItem) {
-//                var index = $scope.scopeModel.translatedValues.indexOf(dataItem);
-//                $scope.scopeModel.translatedValues.splice(index, 1);
-//            };  
-//        }
+                };
+                dataItem.onLanguageSelectorReady = function (api) {
+                    dataItem.languageSeletorAPI = api;
+                    var setLoader = function (value) { dataItem.isLanguageSelectorLoading = value; };
+                    VRUIUtilsService.callDirectiveLoadOrResolvePromise($scope, dataItem.languageSeletorAPI, undefined, setLoader);
+                };
+                $scope.scopeModel.translatedValues.push(dataItem);
+            };
+            $scope.scopeModel.onDeleteRow = function (dataItem) {
+                var index = $scope.scopeModel.translatedValues.indexOf(dataItem);
+                $scope.scopeModel.translatedValues.splice(index, 1);
+            };  
+        }
 
 
-//        function defineAPI() {
-//            var api = {};
+        function defineAPI() {
+            var api = {};
 
-//            api.load = function (payload) {
+            api.load = function (payload) {
               
-//                var fieldValue;
-//                var promises = [];
-//                if (payload != undefined) {
+                var fieldValue;
+                var promises = [];
+                if (payload != undefined) {
                  
-//                    fieldValue = payload.fieldValue;
-//                }
-//                if (fieldValue != undefined && fieldValue.TranslatedValues) {
-//                    var translatedValues = fieldValue.TranslatedValues;
+                    fieldValue = payload.fieldValue;
+                }
+                if (fieldValue != undefined && fieldValue.TranslatedValues) {
+                    var translatedValues = fieldValue.TranslatedValues;
 
-//                    for (var languageId in translatedValues) {
-//                        if (languageId != "$type") {
-//                            var translationPayload = {
-//                                languageId: languageId,
-//                                translation: translatedValues[languageId]
-//                            };
+                    for (var languageId in translatedValues) {
+                        if (languageId != "$type") {
+                            var translationPayload = {
+                                languageId: languageId,
+                                translation: translatedValues[languageId]
+                            };
 
-//                            var translationItem = {
-//                                payload: translationPayload,
-//                                languageReadyPromiseDeferred: UtilsService.createPromiseDeferred(),
-//                                languageLoadPromiseDeferred: UtilsService.createPromiseDeferred()
-//                            };
-//                            promises.push(translationItem.languageLoadPromiseDeferred.promise);
-//                            addSelectedTranslatedValue(translationItem);
-//                        }
-//                    }
-//                }
-//                return UtilsService.waitPromiseNode({ promises: promises });
+                            var translationItem = {
+                                payload: translationPayload,
+                                languageReadyPromiseDeferred: UtilsService.createPromiseDeferred(),
+                                languageLoadPromiseDeferred: UtilsService.createPromiseDeferred()
+                            };
+                            promises.push(translationItem.languageLoadPromiseDeferred.promise);
+                            addSelectedTranslatedValue(translationItem);
+                        }
+                    }
+                }
+                return UtilsService.waitPromiseNode({ promises: promises });
 
-//            };
-//            function addSelectedTranslatedValue(translationItem) {
+            };
+            function addSelectedTranslatedValue(translationItem) {
 
-//                var languagePayload;
-//                var dataItem = {};
-//                if (translationItem.payload != undefined) {
-//                    dataItem.Translation = translationItem.payload.translation;
-//                    languagePayload = { selectedIds: translationItem.payload.languageId };
-//                }
-//                dataItem.onLanguageSelectorReady = function (api) {
-//                    dataItem.languageSeletorAPI = api;
-//                    translationItem.languageReadyPromiseDeferred.resolve();
-//                };
+                var languagePayload;
+                var dataItem = {};
+                if (translationItem.payload != undefined) {
+                    dataItem.Translation = translationItem.payload.translation;
+                    languagePayload = { selectedIds: translationItem.payload.languageId };
+                }
+                dataItem.onLanguageSelectorReady = function (api) {
+                    dataItem.languageSeletorAPI = api;
+                    translationItem.languageReadyPromiseDeferred.resolve();
+                };
 
-//                translationItem.languageReadyPromiseDeferred.promise
-//                    .then(function () {
-//                        VRUIUtilsService.callDirectiveLoad(dataItem.languageSeletorAPI, languagePayload, translationItem.languageLoadPromiseDeferred);
-//                    });
+                translationItem.languageReadyPromiseDeferred.promise
+                    .then(function () {
+                        VRUIUtilsService.callDirectiveLoad(dataItem.languageSeletorAPI, languagePayload, translationItem.languageLoadPromiseDeferred);
+                    });
 
-//                $scope.scopeModel.translatedValues.push(dataItem);
+                $scope.scopeModel.translatedValues.push(dataItem);
 
-//            }
+            }
 
-//            api.getData = function () {
-//                var retVal = {};
-//                for (var i = 0; i < $scope.scopeModel.translatedValues.length; i++) {
-//                    var translatedValue = $scope.scopeModel.translatedValues[i];
-//                    retVal[translatedValue.languageSeletorAPI.getSelectedIds()] = translatedValue.Translation;
+            api.getData = function () {
+                var retVal = {};
+                for (var i = 0; i < $scope.scopeModel.translatedValues.length; i++) {
+                    var translatedValue = $scope.scopeModel.translatedValues[i];
+                    retVal[translatedValue.languageSeletorAPI.getSelectedIds()] = translatedValue.Translation;
 
-//                }
-//                return {
-//                    $type: "Vanrise.GenericData.Entities.TextResourceFieldTypeEntity,Vanrise.GenericData.Entities",
-//                    TranslatedValues:retVal
-//                };
-//            };
+                }
+                return {
+                    $type: "Vanrise.GenericData.Entities.TextResourceFieldTypeEntity,Vanrise.GenericData.Entities",
+                    TranslatedValues:retVal
+                };
+            };
 
-//            if (ctrl.onReady != null)
-//                ctrl.onReady(api);
-//        }
+            if (ctrl.onReady != null)
+                ctrl.onReady(api);
+        }
 
-//    }
+    }
 
-//    return directiveDefinitionObject;
-//}]);
+    return directiveDefinitionObject;
+}]);
 
--- Vanrise/Code/Vanrise.GenericData.Web/VR_GenericData/Directives/MainExtensions/FieldType/TextResource/vr-genericdata-fieldtype-textresource.js
+++ Vanrise/Code/Vanrise.GenericData.Web/VR_GenericData/Directives/MainExtensions/FieldType/TextResource/vr-genericdata-fieldtype-textresource.js
@@ -1,62 +1,62 @@
-﻿//'use strict';
-//app.directive('vrGenericdataFieldtypeTextresource', ['UtilsService',
-//    function (UtilsService) {
+﻿'use strict';
+app.directive('vrGenericdataFieldtypeTextresource', ['UtilsService',
+    function (UtilsService) {
 
-//        var directiveDefinitionObject = {
-//            restrict: 'E',
-//            scope:
-//            {
-//                onReady: '=',
-//                normalColNum: '@'
-//            },
-//            controller: function ($scope, $element, $attrs) {
+        var directiveDefinitionObject = {
+            restrict: 'E',
+            scope:
+            {
+                onReady: '=',
+                normalColNum: '@'
+            },
+            controller: function ($scope, $element, $attrs) {
 
-//                var ctrl = this;
+                var ctrl = this;
 
-//                var ctor = new textResourceTypeCtor(ctrl, $scope);
-//                ctor.initializeController();
+                var ctor = new textResourceTypeCtor(ctrl, $scope);
+                ctor.initializeController();
 
-//            },
-//            controllerAs: 'ctrl',
-//            bindToController: true,
-//            compile: function (element, attrs) {
-//                return {
-//                    pre: function ($scope, iElem, iAttrs, ctrl) {
+            },
+            controllerAs: 'ctrl',
+            bindToController: true,
+            compile: function (element, attrs) {
+                return {
+                    pre: function ($scope, iElem, iAttrs, ctrl) {
 
-//                    }
-//                };
-//            },
-//            templateUrl: function (element, attrs) {
-//                return '/Client/Modules/VR_GenericData/Directives/MainExtensions/FieldType/TextResource/Templates/TextResourceFieldTypeTemplate.html';
-//            }
+                    }
+                };
+            },
+            templateUrl: function (element, attrs) {
+                return '/Client/Modules/VR_GenericData/Directives/MainExtensions/FieldType/TextResource/Templates/TextResourceFieldTypeTemplate.html';
+            }
 
-//        };
+        };
 
-//        function textResourceTypeCtor(ctrl, $scope) {
+        function textResourceTypeCtor(ctrl, $scope) {
 
-//            function initializeController() {
-//                defineAPI();
-//            }
+            function initializeController() {
+                defineAPI();
+            }
 
-//            function defineAPI() {
-//                var api = {};
+            function defineAPI() {
+                var api = {};
 
-//                api.load = function (payload) {
+                api.load = function (payload) {
                    
-//                };
+                };
 
-//                api.getData = function () {
-//                    return {
-//                        $type: "Vanrise.GenericData.MainExtensions.DataRecordFields.FieldTextResourceType, Vanrise.GenericData.MainExtensions",
-//                    };
-//                };
+                api.getData = function () {
+                    return {
+                        $type: "Vanrise.GenericData.MainExtensions.DataRecordFields.FieldTextResourceType, Vanrise.GenericData.MainExtensions",
+                    };
+                };
 
-//                if (ctrl.onReady != null)
-//                    ctrl.onReady(api);
-//            }
+                if (ctrl.onReady != null)
+                    ctrl.onReady(api);
+            }
 
-//            this.initializeController = initializeController;
-//        }
-//        return directiveDefinitionObject;
-//    }
-//]);
+            this.initializeController = initializeController;
+        }
+        return directiveDefinitionObject;
+    }
+]);
