8976967211ccb66e008a55afd26e127750844b5a
Mohammad Sheaib <VANRISE\mohammad.sheaib>
2019-05-31 09:05:48 +0000

    Commented
    git-tfs-id: [http://developmentvlan:8585/tfs/vanrise.collection]$/;C56153

---
+++ Vanrise/Code/Vanrise.GenericData.Web/VR_GenericData/Directives/MainExtensions/FieldType/TextResource/vr-genericdata-fieldtype-textresource.js
@@ -0,0 +1,62 @@
+﻿//'use strict';
+//app.directive('vrGenericdataFieldtypeTextresource', ['UtilsService',
+//    function (UtilsService) {
+
+//        var directiveDefinitionObject = {
+//            restrict: 'E',
+//            scope:
+//            {
+//                onReady: '=',
+//                normalColNum: '@'
+//            },
+//            controller: function ($scope, $element, $attrs) {
+
+//                var ctrl = this;
+
+//                var ctor = new textResourceTypeCtor(ctrl, $scope);
+//                ctor.initializeController();
+
+//            },
+//            controllerAs: 'ctrl',
+//            bindToController: true,
+//            compile: function (element, attrs) {
+//                return {
+//                    pre: function ($scope, iElem, iAttrs, ctrl) {
+
+//                    }
+//                };
+//            },
+//            templateUrl: function (element, attrs) {
+//                return '/Client/Modules/VR_GenericData/Directives/MainExtensions/FieldType/TextResource/Templates/TextResourceFieldTypeTemplate.html';
+//            }
+
+//        };
+
+//        function textResourceTypeCtor(ctrl, $scope) {
+
+//            function initializeController() {
+//                defineAPI();
+//            }
+
+//            function defineAPI() {
+//                var api = {};
+
+//                api.load = function (payload) {
+                   
+//                };
+
+//                api.getData = function () {
+//                    return {
+//                        $type: "Vanrise.GenericData.MainExtensions.DataRecordFields.FieldTextResourceType, Vanrise.GenericData.MainExtensions",
+//                    };
+//                };
+
+//                if (ctrl.onReady != null)
+//                    ctrl.onReady(api);
+//            }
+
+//            this.initializeController = initializeController;
+//        }
+//        return directiveDefinitionObject;
+//    }
+//]);
