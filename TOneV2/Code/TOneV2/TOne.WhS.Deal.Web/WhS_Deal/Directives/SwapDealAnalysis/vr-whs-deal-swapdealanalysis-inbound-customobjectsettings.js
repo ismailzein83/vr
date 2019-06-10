cfa47d16ece18a60b806f35187167a9eb7ebd5ac
Zeinab Mokdad <VANRISE\zeinab.mokdad>
2019-06-10 09:01:33 +0000

    adding inbound and outbound custom object
    git-tfs-id: [http://developmentvlan:8585/tfs/vanrise.collection]$/;C56194

---
+++ TOneV2/Code/TOneV2/TOne.WhS.Deal.Web/WhS_Deal/Directives/SwapDealAnalysis/vr-whs-deal-swapdealanalysis-inbound-customobjectsettings.js
@@ -0,0 +1,53 @@
+﻿(function (app) {
+
+    'use strict';
+
+    customerFaultTicketType.$inject = ["UtilsService", 'VRUIUtilsService', 'VRNotificationService'];
+
+    function customerFaultTicketType(UtilsService, VRUIUtilsService, VRNotificationService) {
+        return {
+            restrict: "E",
+            scope: {
+                onReady: "=",
+            },
+            controller: function ($scope, $element, $attrs) {
+                var ctrl = this;
+                var ctor = new CustomerFaultCtor($scope, ctrl, $attrs);
+                ctor.initializeController();
+            },
+            controllerAs: "Ctrl",
+            bindToController: true,
+            templateUrl: "/Client/Modules/WhS_Deal/Directives/SwapDealAnalysis/Templates/SwapDealAnalysisInboundCustomObjectTemplate.html"
+
+        };
+        function CustomerFaultCtor($scope, ctrl, $attrs) {
+            this.initializeController = initializeController;
+
+            function initializeController() {
+                $scope.scopeModel = {};
+                defineAPI();
+            }
+            function defineAPI() {
+                var api = {};
+
+                api.load = function (payload) {
+
+                };
+
+                api.getData = function () {
+                    var data = {
+                        $type: "TOne.WhS.BusinessEntity.Business.CustomerFaultTicketCustomObjectTypeSettings, TOne.WhS.BusinessEntity.Business"
+                    };
+                    return data;
+                };
+
+                if (ctrl.onReady != undefined && typeof (ctrl.onReady) == 'function') {
+                    ctrl.onReady(api);
+                }
+            }
+        }
+    }
+
+    app.directive('whsBeCustomerfaultticketCustomobjectsettings', customerFaultTicketType);
+
+})(app);
