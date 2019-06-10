75e8240fa291a77b497db9f00ff1899cca8d16a2
Zeinab Mokdad <VANRISE\zeinab.mokdad>
2019-06-10 14:30:06 +0000

    adding swap deal analysis inbund as custom object
    git-tfs-id: [http://developmentvlan:8585/tfs/vanrise.collection]$/;C56202

--- TOneV2/Code/TOneV2/TOne.WhS.Deal.Business/SwapDealAnalysis/SwapDealInboundCustomObjectTypeSettings.cs
+++ TOneV2/Code/TOneV2/TOne.WhS.Deal.Business/SwapDealAnalysis/SwapDealInboundCustomObjectTypeSettings.cs
@@ -1,12 +1,51 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
 using System.Text;
-using System.Threading.Tasks;
+using Vanrise.Common;
+using TOne.WhS.Deal.Entities;
+using System.Collections.Generic;
+using Vanrise.GenericData.Entities;
 
 namespace TOne.WhS.Deal.Business
 {
-    public class SwapDealInboundCustomObjectTypeSettings
+    public class SwapDealInboundCustomObjectTypeSettings : FieldCustomObjectTypeSettings
     {
+        public override Guid ConfigId => new Guid("E30E4332-DB9F-414D-B58A-8F06E07D0D41");
+
+        public override bool AreEqual(object newValue, object oldValue)
+        {
+            throw new NotImplementedException();
+        }
+
+        public override string GetDescription(IFieldCustomObjectTypeSettingsContext context)
+        {
+            var valueObject = context.FieldValue as List<SwapDealAnalysisInbound>;
+            if (valueObject == null)
+                valueObject = Utilities.ConvertJsonToList<SwapDealAnalysisInbound>(context.FieldValue);
+            if (valueObject != null)
+            {
+                StringBuilder description = new StringBuilder();
+                return description.ToString();
+            }
+            return null;
+        }
+
+        public override Type GetNonNullableRuntimeType()
+        {
+            return typeof(SwapDealAnalysisInboundCollection);
+        }
+
+        public override string GetRuntimeTypeDescription()
+        {
+            return "Swap Deal Analysis Inbound";
+        }
+
+        public override dynamic ParseNonNullValueToFieldType(object originalValue)
+        {
+            var castedOriginalValue = originalValue as List<SwapDealAnalysisInbound>;
+            if (castedOriginalValue != null)
+                return castedOriginalValue;
+            else
+                return Utilities.ConvertJsonToList<SwapDealAnalysisInbound>(originalValue);
+        }
     }
 }
