35ee0d944796dc4ed544a26e60d1050ac4d8456c
Zeinab Mokdad <VANRISE\zeinab.mokdad>
2019-06-10 14:32:33 +0000

    removing implementation
    git-tfs-id: [http://developmentvlan:8585/tfs/vanrise.collection]$/;C56203

--- TOneV2/Code/TOneV2/TOne.WhS.Deal.Business/SwapDealAnalysis/SwapDealInboundCustomObjectTypeSettings.cs
+++ TOneV2/Code/TOneV2/TOne.WhS.Deal.Business/SwapDealAnalysis/SwapDealInboundCustomObjectTypeSettings.cs
@@ -9,7 +9,7 @@ namespace TOne.WhS.Deal.Business
 {
     public class SwapDealInboundCustomObjectTypeSettings : FieldCustomObjectTypeSettings
     {
-        public override Guid ConfigId => new Guid("E30E4332-DB9F-414D-B58A-8F06E07D0D41");
+        public override Guid ConfigId => throw new NotImplementedException();
 
         public override bool AreEqual(object newValue, object oldValue)
         {
@@ -18,34 +18,22 @@ namespace TOne.WhS.Deal.Business
 
         public override string GetDescription(IFieldCustomObjectTypeSettingsContext context)
         {
-            var valueObject = context.FieldValue as List<SwapDealAnalysisInbound>;
-            if (valueObject == null)
-                valueObject = Utilities.ConvertJsonToList<SwapDealAnalysisInbound>(context.FieldValue);
-            if (valueObject != null)
-            {
-                StringBuilder description = new StringBuilder();
-                return description.ToString();
-            }
-            return null;
+            throw new NotImplementedException();
         }
 
         public override Type GetNonNullableRuntimeType()
         {
-            return typeof(SwapDealAnalysisInboundCollection);
+            throw new NotImplementedException();
         }
 
         public override string GetRuntimeTypeDescription()
         {
-            return "Swap Deal Analysis Inbound";
+            throw new NotImplementedException();
         }
 
         public override dynamic ParseNonNullValueToFieldType(object originalValue)
         {
-            var castedOriginalValue = originalValue as List<SwapDealAnalysisInbound>;
-            if (castedOriginalValue != null)
-                return castedOriginalValue;
-            else
-                return Utilities.ConvertJsonToList<SwapDealAnalysisInbound>(originalValue);
+            throw new NotImplementedException();
         }
     }
 }
