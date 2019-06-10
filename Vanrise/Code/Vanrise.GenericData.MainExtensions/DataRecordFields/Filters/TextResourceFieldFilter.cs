f63baff0ab5e7d5635445bc4fedb0d00bca8dfc7
Mohammad Sheaib <VANRISE\mohammad.sheaib>
2019-06-10 14:13:37 +0000

    commented
    git-tfs-id: [http://developmentvlan:8585/tfs/vanrise.collection]$/;C56200

---
+++ Vanrise/Code/Vanrise.GenericData.MainExtensions/DataRecordFields/Filters/TextResourceFieldFilter.cs
@@ -0,0 +1,22 @@
+﻿//using System;
+//using System.Collections.Generic;
+//using System.Linq;
+//using System.Text;
+//using System.Threading.Tasks;
+//using Vanrise.GenericData.Entities;
+
+//namespace Vanrise.GenericData.MainExtensions.DataRecordFields.Filters
+//{
+//    public class TextResourceFieldFilter : IDataRecordFieldFilter
+//    {
+//        public bool IsExcluded(IDataRecordFieldFilterContext context)
+//        {
+//            if (context.DataRecordField != null && context.DataRecordField != null)
+//            {
+//                if (!(context.DataRecordField.Type is FieldTextResourceType))
+//                    return true;
+//            }
+//            return false;
+//        }
+//    }
+//}
