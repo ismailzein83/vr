c0bf16a339f0cdd470091bd819dbeab13685dc36
Ali Atoui <VANRISE\ali.atoui>
2019-06-10 12:07:42 +0000

    Adding DealTimePeriod and DealTimePeriodConfig
    git-tfs-id: [http://developmentvlan:8585/tfs/vanrise.collection]$/;C56199

---
+++ TOneV2/Code/TOneV2/TOne.WhS.Deal.Entities/DealTimePeriod/DealTimePeriodConfig.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TOne.WhS.Deal.Entities
+{
+    public class DealTimePeriodConfig : Vanrise.Entities.ExtensionConfiguration
+    {
+        public const string EXTENSION_TYPE = "WhS_Deal_DealTimePeriodConfig";
+
+        public string Editor { get; set; }
+    }
+}
--- TOneV2/Code/TOneV2/TOne.WhS.Deal.Entities/RecurringType.cs
+++ TOneV2/Code/TOneV2/TOne.WhS.Deal.Entities/RecurringType.cs
@@ -10,16 +10,4 @@ namespace TOne.WhS.Deal.Entities
         [Description("Monthly")]
         Monthly = 1,
     }
-
-    public abstract class DealTimePeriod  : Vanrise.Entities.VRTimePeriod
-    {
-
-    }
-
-    public class DealTimePeriodConfig : Vanrise.Entities.ExtensionConfiguration
-    {
-        public const string EXTENSION_TYPE = "WhS_Deal_DealTimePeriodConfig";
-
-        public string Editor { get; set; }
-    }
 }
