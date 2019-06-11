f97a830b9c2f7758fb57393475bc511319cbf97f
Zeinab Mokdad <VANRISE\zeinab.mokdad>
2019-06-11 06:34:21 +0000

    adding custom object for del analysis outboun
    git-tfs-id: [http://developmentvlan:8585/tfs/vanrise.collection]$/;C56206

--- TOneV2/Code/TOneV2/TOne.WhS.Deal.Business/SwapDealAnalysis/SwapDealOutboundCustomObjectTypeSettings.cs
+++ TOneV2/Code/TOneV2/TOne.WhS.Deal.Business/SwapDealAnalysis/SwapDealOutboundCustomObjectTypeSettings.cs
@@ -1,12 +1,35 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using Vanrise.GenericData.Entities;
 
 namespace TOne.WhS.Deal.Business
 {
-    public class SwapDealOutboundCustomObjectTypeSettings
+    public class SwapDealOutboundCustomObjectTypeSettings : FieldCustomObjectTypeSettings
     {
+        public override Guid ConfigId => new Guid("76F467C2-E440-4B11-A42C-59F69FDDBCB7");
+
+        public override bool AreEqual(object newValue, object oldValue)
+        {
+            return true;
+        }
+
+        public override string GetDescription(IFieldCustomObjectTypeSettingsContext context)
+        {
+            throw new NotImplementedException();
+        }
+
+        public override Type GetNonNullableRuntimeType()
+        {
+            throw new NotImplementedException();
+        }
+
+        public override string GetRuntimeTypeDescription()
+        {
+            throw new NotImplementedException();
+        }
+
+        public override dynamic ParseNonNullValueToFieldType(object originalValue)
+        {
+            throw new NotImplementedException();
+        }
     }
 }
