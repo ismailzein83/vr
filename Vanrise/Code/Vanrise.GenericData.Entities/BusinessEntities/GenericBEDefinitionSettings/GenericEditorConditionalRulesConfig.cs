93bf0f0859b5580d77c054316ac429abc344f681
Ali Slayman <VANRISE\ali.slayman>
2019-06-11 07:56:37 +0000

    Adding GenericEditorConditionalRulesConfig
    git-tfs-id: [http://developmentvlan:8585/tfs/vanrise.collection]$/;C56211

---
+++ Vanrise/Code/Vanrise.GenericData.Entities/BusinessEntities/GenericBEDefinitionSettings/GenericEditorConditionalRulesConfig.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections.Generic;
+using Vanrise.Entities;
+
+namespace Vanrise.GenericData.Entities
+{
+    public class GenericEditorConditionalRulesConfig : ExtensionConfiguration
+    {
+        public const string EXTENSION_TYPE = "VR_GenericData_GenericEditorConditionalRulesConfig";
+
+        public string Editor { get; set; }
+    }
+}
