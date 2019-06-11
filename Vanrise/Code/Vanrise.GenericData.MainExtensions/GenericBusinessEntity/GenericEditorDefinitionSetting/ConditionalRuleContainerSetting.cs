796533cd98f5981871daa78f5cc5a4f33fefa4ec
Ali Slayman <VANRISE\ali.slayman>
2019-06-11 09:39:22 +0000

    Adding GenericEditorFieldValueContainerRule and ConditionalRuleContainerSetting main extensions
    git-tfs-id: [http://developmentvlan:8585/tfs/vanrise.collection]$/;C56214

---
+++ Vanrise/Code/Vanrise.GenericData.MainExtensions/GenericBusinessEntity/GenericEditorDefinitionSetting/ConditionalRuleContainerSetting.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Vanrise.Entities;
+using Vanrise.GenericData.Entities;
+
+namespace Vanrise.GenericData.MainExtensions
+{
+    public class ConditionalRuleContainerSetting : VRGenericEditorDefinitionSetting
+    {
+        public override Guid ConfigId { get { return new Guid("FA9A6147-205C-43F7-BA81-73EB530F245F"); } }
+
+        public VRGenericEditorDefinitionSetting EditorType { get; set; }
+
+        public GenericEditorConditionalRule GenericEditorConditionalRule { get; set; }
+    }
+
+    public abstract class GenericEditorConditionalRule
+    {
+        public abstract Guid ConfigId { get; }
+    }
+}
---
+++ Vanrise/Code/Vanrise.GenericData.MainExtensions/GenericBusinessEntity/GenericEditorDefinitionSetting/GenericEditorContainerRules/GenericEditorFieldValueContainerRule.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Vanrise.GenericData.Entities;
+
+namespace Vanrise.GenericData.MainExtensions
+{
+    public class GenericEditorFieldValueContainerRule : GenericEditorConditionalRule
+    {
+        public override Guid ConfigId { get { return new Guid("A8811BE9-6311-4FB8-9F88-71FD015F639A"); } }
+        public RecordQueryLogicalOperator LogicalOperator { get; set; }
+        public List<RecordFilter> Conditions { get; set; }
+    }
+}
