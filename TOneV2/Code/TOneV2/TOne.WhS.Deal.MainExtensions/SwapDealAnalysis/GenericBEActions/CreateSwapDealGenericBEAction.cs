8c4835206fcaf50aa102cc75a11c4042fbfe236c
Zeinab Mokdad <VANRISE\zeinab.mokdad>
2019-06-12 14:17:57 +0000

    adding create swap deal generic action
    git-tfs-id: [http://developmentvlan:8585/tfs/vanrise.collection]$/;C56265

---
+++ TOneV2/Code/TOneV2/TOne.WhS.Deal.MainExtensions/SwapDealAnalysis/GenericBEActions/CreateSwapDealGenericBEAction.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using Vanrise.GenericData.Business;
+
+namespace TOne.WhS.Deal.MainExtensions
+{
+    public class CreateSwapDealGenericBEAction : GenericBEActionSettings
+    {
+        public override string ActionKind => "CreateSwapDeal";
+
+        public override string ActionTypeName => "CreateSwapDealGenericBEAction";
+
+        public override Guid ConfigId => new Guid("E5666599-FF4C-423E-BC6A-724B4A68CA69");
+    }
+}
