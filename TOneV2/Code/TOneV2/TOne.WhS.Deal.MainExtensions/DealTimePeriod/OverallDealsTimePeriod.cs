39afa2399d4464d1bb571fe2fe6638d417ff5bfb
Ali Atoui <VANRISE\ali.atoui>
2019-06-12 13:24:51 +0000

    Adding DealTimePeriod Extensions
    git-tfs-id: [http://developmentvlan:8585/tfs/vanrise.collection]$/;C56258

---
+++ TOneV2/Code/TOneV2/TOne.WhS.Deal.MainExtensions/DealTimePeriod/OverallDealsTimePeriod.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using TOne.WhS.Deal.Business;
+using TOne.WhS.Deal.Entities;
+using Vanrise.Entities;
+
+namespace TOne.WhS.Deal.MainExtensions.DealTimePeriod
+{
+    public class OverallDealsTimePeriod : TOne.WhS.Deal.Entities.DealTimePeriod
+    {
+        public override Guid ConfigId => new Guid("CED02013-65C0-49B9-97A7-7F557FF945BA");
+
+        public override void GetTimePeriod(IVRTimePeriodContext context)
+        {
+            DateTime fromTime = DateTime.MaxValue;
+
+            DealDefinitionManager dealDefinitionManager = new DealDefinitionManager();
+            Dictionary<int, DealDefinition> dealDefinitions = dealDefinitionManager.GetAllCachedDealDefinitions();
+
+            foreach (var dealDefinition in dealDefinitions.Values)
+            {
+                if (dealDefinition.Settings.Status == DealStatus.Draft)
+                    continue;
+
+                if (dealDefinition.Settings.RealBED < fromTime)
+                    fromTime = dealDefinition.Settings.RealBED;
+            }
+
+            context.ToTime = context.EffectiveDate;
+            context.FromTime = fromTime;
+        }
+    }
+}
