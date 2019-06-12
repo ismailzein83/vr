6f1a44dc42ce2392402276b995e5b3ca0aa3e422
Fatima Hammoud <VANRISE\fatima.hammoud>
2019-06-12 10:44:20 +0000

    Adding commented some files
    git-tfs-id: [http://developmentvlan:8585/tfs/vanrise.collection]$/;C56240

--- Vanrise/Code/Vanrise.GenericData.Web/VR_GenericData/Directives/GenericBusinessEntity/Definition/GenericBEFilterDefinition/vr-genericdata-genericbe-filterdefinition-genericfilter.js
+++ Vanrise/Code/Vanrise.GenericData.Web/VR_GenericData/Directives/GenericBusinessEntity/Definition/GenericBEFilterDefinition/vr-genericdata-genericbe-filterdefinition-genericfilter.js
@@ -2,9 +2,9 @@
 
     'use strict';
 
-    FilterDefinitionGenericFilterDirective.$inject = ['UtilsService', 'VRNotificationService', 'VRUIUtilsService'];
+    FilterDefinitionGenericFilterDirective.$inject = ['UtilsService', 'VRNotificationService', 'VRUIUtilsService', 'VR_GenericData_DataRecordFieldAPIService'];
 
-    function FilterDefinitionGenericFilterDirective(UtilsService, VRNotificationService, VRUIUtilsService) {
+    function FilterDefinitionGenericFilterDirective(UtilsService, VRNotificationService, VRUIUtilsService, VR_GenericData_DataRecordFieldAPIService) {
         return {
             restrict: 'E',
             scope: {
@@ -23,12 +23,21 @@
         function GenericFilterCtor($scope, ctrl) {
 
             var context;
+            var settings;
             var firstLoad = true;
+            var dataRecordFieldTypes = [];
+            var defaultFieldsValues;
+            var fieldType;
+
             var dataRecordTypeFieldsSelectorAPI;
             var dataRecordTypeFieldsSelectorReadyPromiseDeferred = UtilsService.createPromiseDeferred();
+            var dataRecordTypePromiseDeferred;
 
-			//var localizationTextResourceSelectorAPI;
-			//var localizationTextResourceSelectorReadyPromiseDeferred = UtilsService.createPromiseDeferred();
+            //var localizationTextResourceSelectorAPI;
+            //var localizationTextResourceSelectorReadyPromiseDeferred = UtilsService.createPromiseDeferred();
+
+           // var fieldTypeRuntimeDirectiveAPI;
+           // var fieldTypeRuntimeReadyPromiseDeferred = UtilsService.createPromiseDeferred();
 
             this.initializeController = initializeController;
             function initializeController() {
@@ -48,12 +57,54 @@
                         $scope.scopeModel.fieldTitle = undefined;
                     }
 
-				};
+                };
 
-				//$scope.scopeModel.onLocalizationTextResourceDirectiveReady = function (api) {
-				//	localizationTextResourceSelectorAPI = api;
-				//	localizationTextResourceSelectorReadyPromiseDeferred.resolve();
-				//};
+                //$scope.scopeModel.onDataRecordTypeFieldsSelectionChanged = function () {
+                //    var item = dataRecordTypeFieldsSelectorAPI.getSelectedValue();
+
+                //    if (item != undefined) {
+                //        if (dataRecordTypePromiseDeferred != undefined) {
+                //            dataRecordTypePromiseDeferred.resolve();
+                //        }
+                //        else {
+                //            $scope.scopeModel.fieldTitle = item.Title;
+                //            $scope.scopeModel.fieldTypeRuntimeDirective = undefined;
+                //            fieldTypeRuntimeReadyPromiseDeferred = UtilsService.createPromiseDeferred();
+
+                //            fieldType = context.getFieldType(item.Name);
+                //            var dataRecordFieldType = UtilsService.getItemByVal(dataRecordFieldTypes, fieldType.ConfigId, "ExtensionConfigurationId");
+
+                //            if (dataRecordFieldType != undefined && dataRecordFieldType.FilterEditor != undefined) {
+                //                $scope.scopeModel.fieldTypeRuntimeDirective = dataRecordFieldType.FilterEditor;
+
+                //                fieldTypeRuntimeReadyPromiseDeferred.promise.then(function () {
+                //                    var setLoader = function (value) { $scope.scopeModel.isFieldTypeRumtimeDirectiveLoading = value; };
+                //                    var directivePayload = {
+                //                        fieldTitle: "Default value",
+                //                        fieldType: fieldType,
+                //                        fieldName: item.Name,
+                //                    };
+                //                    VRUIUtilsService.callDirectiveLoadOrResolvePromise($scope, fieldTypeRuntimeDirectiveAPI, directivePayload, setLoader);
+                //                });
+                //            }
+                //        }
+                //    }
+                //    else {
+                //        $scope.scopeModel.fieldTitle = undefined;
+                //       // fieldTypeRuntimeDirectiveAPI = undefined;
+                //    }
+                };
+
+                //$scope.scopeModel.onLocalizationTextResourceDirectiveReady = function (api) {
+                //	localizationTextResourceSelectorAPI = api;
+                //	localizationTextResourceSelectorReadyPromiseDeferred.resolve();
+                //};
+
+                //$scope.scopeModel.onFieldTypeRumtimeDirectiveReady = function (api) {
+                //    fieldTypeRuntimeDirectiveAPI = api;
+                //    console.log(api);
+                //    fieldTypeRuntimeReadyPromiseDeferred.resolve();
+                //};
 
                 defineAPI();
             }
@@ -61,19 +112,37 @@
             function defineAPI() {
                 var api = {};
                 api.getData = function () {
+                    //var filterValues;
+                    //if (fieldTypeRuntimeDirectiveAPI != undefined) {
+                    //    filterValues = fieldTypeRuntimeDirectiveAPI.getValuesAsArray();
+                    //}
+                    //var finalArray = [];
+                    //if (filterValues != undefined) {
+                    //    for (var i = 0; i < filterValues.length; i++) {
+                    //        var filterValue = filterValues[i];
+                    //        if (filterValue != undefined) {
+                    //            finalArray.push(filterValue);
+                    //        }
+                    //    }
+                    //}
                     return {
                         $type: "Vanrise.GenericData.MainExtensions.GenericFilterDefinitionSettings, Vanrise.GenericData.MainExtensions",
                         FieldName: dataRecordTypeFieldsSelectorAPI.getSelectedIds(),
                         FieldTitle: $scope.scopeModel.fieldTitle,
-						IsRequired: $scope.scopeModel.isRequired,
-						//TextResourceKey: localizationTextResourceSelectorAPI.getSelectedValues()
+                        IsRequired: $scope.scopeModel.isRequired,
+                        //DefaultFieldValues: (finalArray != undefined && finalArray.length != 0) ? finalArray : undefined
+                        //TextResourceKey: localizationTextResourceSelectorAPI.getSelectedValues()
                     };
                 };
 
                 api.load = function (payload) {
+                    var initialPromises = [];
+                    console.log(payload);
                     if (payload != undefined) {
                         context = payload.context;
+                        settings = payload.settings;
                     }
+
                     function loadDataRecordTitleFieldsSelector() {
                         var loadDataRecordTypeFieldsSelectorPromiseDeferred = UtilsService.createPromiseDeferred();
                         dataRecordTypeFieldsSelectorReadyPromiseDeferred.promise.then(function () {
@@ -93,27 +162,92 @@
                             $scope.scopeModel.fieldTitle = payload.settings.FieldTitle;
                             $scope.scopeModel.isRequired = payload.settings.IsRequired;
                         }
+                    }
 
-					}
-					function loadLocalizationTextResourceSelector() {
-						var loadSelectorPromiseDeferred = UtilsService.createPromiseDeferred();
-						localizationTextResourceSelectorReadyPromiseDeferred.promise.then(function () {
-							var directivePayload = {
-								selectedValue: (payload!=undefined && payload.settings != undefined) ? payload.settings.TextResourceKey : undefined
+                    function loadLocalizationTextResourceSelector() {
+                        var loadSelectorPromiseDeferred = UtilsService.createPromiseDeferred();
+                        localizationTextResourceSelectorReadyPromiseDeferred.promise.then(function () {
+                            var directivePayload = {
+                                selectedValue: (payload != undefined && payload.settings != undefined) ? payload.settings.TextResourceKey : undefined
 
-							};
-							VRUIUtilsService.callDirectiveLoad(localizationTextResourceSelectorAPI, directivePayload, loadSelectorPromiseDeferred);
-						});
-						return loadSelectorPromiseDeferred.promise;
-					}
+                            };
+                            VRUIUtilsService.callDirectiveLoad(localizationTextResourceSelectorAPI, directivePayload, loadSelectorPromiseDeferred);
+                        });
+                        return loadSelectorPromiseDeferred.promise;
+                    }
 
+                    //function getDataRecordFieldTypeConfigs() {
+                    //    return VR_GenericData_DataRecordFieldAPIService.GetDataRecordFieldTypeConfigs().then(function (response) {
+                    //        for (var i = 0; i < response.length; i++) {
+                    //            var dataRecordFieldType = response[i];
+                    //            dataRecordFieldTypes.push(dataRecordFieldType);
+                    //        }
+                    //    });
+                    //}
 
-					return UtilsService.waitMultipleAsyncOperations([loadStaticData, loadDataRecordTitleFieldsSelector]).then(function () {
+                    //function getFieldTypeRuntimeDirective() {
+                    //    getDataRecordFieldTypeConfigs().then(function () {
+                    //        if (settings != undefined) {
+                    //            fieldType = context.getFieldType(settings.FieldName);
+                    //            var dataRecordFieldType = UtilsService.getItemByVal(dataRecordFieldTypes, fieldType.ConfigId, "ExtensionConfigurationId");
+                    //            if (dataRecordFieldType != undefined && dataRecordFieldType.FilterEditor != undefined) {
+                    //                $scope.scopeModel.fieldTypeRuntimeDirective = dataRecordFieldType.FilterEditor;
+                    //            }
+                    //        }
+                    //    }).catch(function () {
+                    //        $scope.scopeModel.fieldTypeRuntimeDirective = undefined;
+                    //    });
+                    //}
+
+                    //function loadFieldTypeRuntimeEditor() {
+                    //    var loadRuntimeEditorPromiseDeferred = UtilsService.createPromiseDeferred();
+
+                    //    fieldTypeRuntimeReadyPromiseDeferred.promise.then(function () {
+                    //        dataRecordTypePromiseDeferred = UtilsService.createPromiseDeferred();
+                    //        var directivePayload = {
+                    //            fieldTitle: "Default Value(s)",
+                    //            fieldType: fieldType,
+                    //            fieldName: settings != undefined ? settings.FieldName : undefined,
+                    //            fieldValue: settings != undefined ? settings.DefaultFieldValues : undefined
+                    //        };
+                    //        VRUIUtilsService.callDirectiveLoad(fieldTypeRuntimeDirectiveAPI, directivePayload, loadRuntimeEditorPromiseDeferred);
+                    //    });
+                    //    return loadRuntimeEditorPromiseDeferred.promise;
+                    //}
+
+                    return UtilsService.waitMultipleAsyncOperations([loadStaticData, loadDataRecordTitleFieldsSelector, getDataRecordFieldTypeConfigs]).then(function () {
                     }).finally(function () {
+                        dataRecordTypePromiseDeferred = undefined;
                         setTimeout(function () {
                             firstLoad = false;
-                        },1);
+                        }, 1);
                     });
+
+
+                    //loadStaticData();
+                    //initialPromises.push(getFieldTypeRuntimeDirective());
+                    //initialPromises.push(loadDataRecordTitleFieldsSelector());
+
+                    //var rootPromiseNode = {
+                    //    promises: initialPromises,
+                    //    getChildNode: function () {
+                    //        var directivePromises = [];
+
+                    //        directivePromises.push(loadFieldTypeRuntimeEditor());
+
+                    //        return {
+                    //            promises: directivePromises
+                    //            }
+                    //        };
+                    //    }
+                    //};
+
+                    //return UtilsService.waitPromiseNode(rootPromiseNode).finally(function () {
+                    //    dataRecordTypePromiseDeferred = undefined;
+                    //    setTimeout(function () {
+                    //        firstLoad = false;
+                    //    }, 1);
+                    //});
                 };
 
 
