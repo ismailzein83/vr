7feb3aae0d142074742b2e36c7c61ca088b5b53c
yara.bousalman <yara.bousalman@vanrise.tfs.local>
2019-06-13 09:56:01 +0000

    Adding RetailBEChargeSettings related files
    git-tfs-id: [http://developmentvlan:8585/tfs/vanrise.collection]$/;C56287

---
+++ Retail/Code/Retail.BusinessEntity.Business/RetailBECharge/RetailBEChargeManager.cs
@@ -0,0 +1,19 @@
+﻿using Retail.BusinessEntity.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Vanrise.Common.Business;
+
+namespace Retail.BusinessEntity.Business
+{
+    public class RetailBEChargeManager
+    {
+        public IEnumerable<RetailBEChargeSettingsConfig> GetRetailBEChargeSettingsConfigs()
+        {
+            var extensionConfigurationManager = new ExtensionConfigurationManager();
+            return extensionConfigurationManager.GetExtensionConfigurations<RetailBEChargeSettingsConfig>(RetailBEChargeSettingsConfig.EXTENSION_TYPE);
+        }
+    }
+}
