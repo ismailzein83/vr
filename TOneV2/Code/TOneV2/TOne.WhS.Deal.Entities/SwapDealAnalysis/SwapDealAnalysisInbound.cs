c1e33fe0c8d0d4920b4c2d129edead3f3ef78a62
Zeinab Mokdad <VANRISE\zeinab.mokdad>
2019-06-13 08:33:26 +0000

    adding SwapDealAnalysis Inbound  and outbound
    git-tfs-id: [http://developmentvlan:8585/tfs/vanrise.collection]$/;C56280

---
+++ TOneV2/Code/TOneV2/TOne.WhS.Deal.Entities/SwapDealAnalysis/SwapDealAnalysisInbound.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace TOne.WhS.Deal.Entities
+{
+    public class SwapDealAnalysisInbound
+    {
+        public InboundCollection SwapDealAnalysisInbounds { get; set; }
+    }
+    public class InboundCollection : List<Inbound>
+    {
+
+    }
+    public class Inbound
+    {
+        public int CountryId { get; set; }
+        public string GroupName { get; set; }
+        public decimal CurrentRate { get; set; }
+        public int DailyVolume { get; set; }
+        public int Volume { get; set; }
+        public decimal DealRate { get; set; }
+        public SwapDealAnalysisInboundItemRateCalcMethod ItemCalculationMethod { get; set; }
+        public Guid CalculationMethodId { get; set; }
+        public List<long> SaleZoneIds { get; set; }
+    }
+}
---
+++ TOneV2/Code/TOneV2/TOne.WhS.Deal.Entities/SwapDealAnalysis/SwapDealAnalysisOutbound.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace TOne.WhS.Deal.Entities
+{
+    public class SwapDealAnalysisOutbound
+    {
+        public OutboundCollection SwapDealAnalysisOutbounds { get; set; }
+    }
+    public class OutboundCollection : List<Outbound>
+    {
+
+    }
+    public class Outbound
+    {
+        public int CountryId { get; set; }
+        public decimal CurrentRate { get; set; }
+        public int DailyVolume { get; set; }
+        public int Volume { get; set; }
+        public decimal DealRate { get; set; }
+        public SwapDealAnalysisOutboundItemRateCalcMethod ItemCalculationMethod { get; set; }
+        public Guid CalculationMethodId { get; set; }
+        public List<long> SupplierZoneIds { get; set; }
+        public string GroupName { get; set; }
+        public decimal Revenue { get; set; }
+        public decimal Savings { get; set; }
+    }
+}
