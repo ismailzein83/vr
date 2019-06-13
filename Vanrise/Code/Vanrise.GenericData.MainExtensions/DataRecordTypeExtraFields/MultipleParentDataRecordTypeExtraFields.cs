737e9140bb14217e8878c0fea55718c5fae1fbd8
Fatima Al Heck <VANRISE\fatima.alheck>
2019-06-13 06:51:44 +0000

    Adding files for "MultipleParentDataRecordTypeExtraFields" implementation
    git-tfs-id: [http://developmentvlan:8585/tfs/vanrise.collection]$/;C56274

---
+++ Vanrise/Code/Vanrise.GenericData.MainExtensions/DataRecordTypeExtraFields/MultipleParentDataRecordTypeExtraFields.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Vanrise.GenericData.Entities;
+
+namespace Vanrise.GenericData.MainExtensions.DataRecordTypeExtraFields
+{
+    class MultipleParentDataRecordTypeExtraFields : DataRecordTypeExtraField
+    {
+        public override Guid ConfigId { get { return new Guid("466EECEE-F3AC-4880-8818-8FEDD8D92BA8"); } }
+
+        public override List<DataRecordField> GetFields(IDataRecordExtraFieldContext context)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ Vanrise/Code/Vanrise.GenericData.Web/VR_GenericData/Directives/RecordType/vr-genericdata-datarecordtypextrafields-multipleparentrecordtype.js
@@ -0,0 +1,57 @@
+﻿'use strict';
+
+app.directive('vrGenericdataDatarecordtypextrafieldsMultipleparentrecordtype', ['UtilsService', 'VRUIUtilsService',
+    function (UtilsService, VRUIUtilsService) {
+
+        var directiveDefinitionObject = {
+            restrict: 'E',
+            scope: {
+                onReady: '=',
+                normalColNum: '@'
+            },
+            controller: function ($scope, $element, $attrs) {
+
+                var ctrl = this;
+                var ctor = new extraFieldCtor(ctrl, $scope, $attrs);
+                ctor.initializeController();
+            },
+            controllerAs: 'ctrl',
+            bindToController: true,
+            compile: function (element, attrs) {
+
+            },
+            templateUrl: '/Client/Modules/VR_GenericData/Directives/RecordType/Templates/MultipleParentDataRecordTypeExtraFieldsTemplate.html'
+        };
+
+
+        function extraFieldCtor(ctrl, $scope, $attrs) {
+            this.initializeController = initializeController;
+            $scope.scopeModel = {};
+
+
+            function initializeController() {
+
+                defineAPI();
+            }
+
+            function defineAPI() {
+                var api = {};
+
+                api.load = function (payload) {
+                    var promises = [];
+
+
+                };
+
+
+                api.getData = function () {
+
+                };
+
+                if (ctrl.onReady != null && typeof (ctrl.onReady) == "function")
+                    ctrl.onReady(api);
+            }
+        }
+
+        return directiveDefinitionObject;
+    }]);
