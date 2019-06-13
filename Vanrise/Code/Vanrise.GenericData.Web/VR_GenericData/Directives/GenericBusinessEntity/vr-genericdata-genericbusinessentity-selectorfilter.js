ec0965510eab0a410d87bec863385cf9c2ffe196
anthony douaihy <VANRISE\anthony.douaihy>
2019-06-13 06:38:05 +0000

    add vrGenericdataGenericbusinessentitySelectorfilter
    git-tfs-id: [http://developmentvlan:8585/tfs/vanrise.collection]$/;C56270

---
+++ Vanrise/Code/Vanrise.GenericData.Web/VR_GenericData/Directives/GenericBusinessEntity/vr-genericdata-genericbusinessentity-selectorfilter.js
@@ -0,0 +1,52 @@
+﻿"use strict";
+app.directive("vrGenericdataGenericbusinessentitySelectorfilter", ["UtilsService", "VRUIUtilsService", 'VR_GenericData_GenericBusinessEntityAPIService',
+    function (UtilsService, VRUIUtilsService, VR_GenericData_GenericBusinessEntityAPIService) {
+
+        var directiveDefinitionObject = {
+            restrict: "E",
+            scope: {
+                onReady: "="
+            },
+            controller: function ($scope, $element, $attrs) {
+                var ctrl = this;
+                var ctor = new GenericBusinessEntitySelectorFilterCtor($scope, ctrl, $attrs);
+                ctor.initializeController();
+            },
+            controllerAs: "ctrl",
+            bindToController: true,
+            templateUrl: "/Client/Modules/VR_GenericData/Directives/GenericBusinessEntity/Templates/GenericBusinessEntitySelectorFilter.html"
+        };
+
+        function GenericBusinessEntitySelectorFilterCtor($scope, ctrl, $attrs) {
+            this.initializeController = initializeController;
+
+            function initializeController() {
+                $scope.scopeModel = {};
+
+                defineAPI();
+            }
+
+            function defineAPI() {
+                var api = {};
+
+                api.load = function (payload) {
+                    var promises = [];
+
+                    if (payload != undefined) {
+                       
+                    }
+
+                    return UtilsService.waitMultiplePromises(promises);
+                };
+
+                api.getData = function () {
+                    
+                };
+
+                if (ctrl.onReady != null)
+                    ctrl.onReady(api);
+            }
+        }
+        return directiveDefinitionObject;
+    }
+]);
