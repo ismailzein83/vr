d87fddedaa04dbb2467641717d6b0c5f9f62a63d
yara.bousalman <yara.bousalman@vanrise.tfs.local>
2019-06-13 07:54:08 +0000

    Adding RetailBEChargeCustomObjectTypeSettings
    git-tfs-id: [http://developmentvlan:8585/tfs/vanrise.collection]$/;C56278

---
+++ Retail/Code/Retail.BusinessEntity.Business/RetailBEChargeCustomObjectTypeSettings.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+using Vanrise.GenericData.Entities;
+
+namespace Retail.BusinessEntity.Business
+{
+    public class RetailBEChargeCustomObjectTypeSettings : FieldCustomObjectTypeSettings
+    {
+        public override Guid ConfigId { get { return new Guid("0C984560-77EE-4BE7-8EFB-A0FFBE54FCBE"); } }
+
+        public override bool AreEqual(object newValue, object oldValue)
+        {
+            throw new NotImplementedException();
+        }
+
+        public override string GetDescription(IFieldCustomObjectTypeSettingsContext context)
+        {
+            throw new NotImplementedException();
+        }
+
+        public override Type GetNonNullableRuntimeType()
+        {
+            throw new NotImplementedException();
+        }
+
+        public override string GetRuntimeTypeDescription()
+        {
+            throw new NotImplementedException();
+        }
+
+        public override dynamic ParseNonNullValueToFieldType(object originalValue)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
