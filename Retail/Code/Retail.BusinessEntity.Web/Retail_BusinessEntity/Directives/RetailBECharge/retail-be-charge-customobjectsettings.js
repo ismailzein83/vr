7bfdcf7aca202ecf9cb234bae842397d2cb22374
yara.bousalman <yara.bousalman@vanrise.tfs.local>
2019-06-13 08:21:10 +0000

    Adding retail-be-charge-customobjectsettings
    git-tfs-id: [http://developmentvlan:8585/tfs/vanrise.collection]$/;C56279

---
+++ Retail/Code/Retail.BusinessEntity.Web/Retail_BusinessEntity/Directives/RetailBECharge/retail-be-charge-customobjectsettings.js
@@ -0,0 +1,53 @@
+﻿(function (app) {
+
+    'use strict';
+
+    RetailBEChargeCustomObjectSettings.$inject = ["UtilsService", 'VRUIUtilsService', 'VRNotificationService'];
+
+    function RetailBEChargeCustomObjectSettings(UtilsService, VRUIUtilsService, VRNotificationService) {
+        return {
+            restrict: "E",
+            scope: {
+                onReady: "=",
+            },
+            controller: function ($scope, $element, $attrs) {
+                var ctrl = this;
+                var ctor = new RetailBEChargeCustomObjectSettingsCtor($scope, ctrl, $attrs);
+                ctor.initializeController();
+            },
+            controllerAs: "Ctrl",
+            bindToController: true,
+            templateUrl: "/Client/Modules/Retail_BusinessEntity/Directives/RetailBECharge/Templates/RetailBEChargeCustomObjectSettingsTemplate.html"
+
+        };
+        function RetailBEChargeCustomObjectSettingsCtor($scope, ctrl, $attrs) {
+            this.initializeController = initializeController;
+
+            function initializeController() {
+                $scope.scopeModel = {};
+                defineAPI();
+            }
+            function defineAPI() {
+                var api = {};
+
+                api.load = function (payload) {
+
+                };
+
+                api.getData = function () {
+                    var data = {
+                        $type: "Retail.BusinessEntity.Business.RetailBEChargeCustomObjectTypeSettings, Retail.BusinessEntity.Business"
+                    };
+                    return data;
+                };
+
+                if (ctrl.onReady != undefined && typeof (ctrl.onReady) == 'function') {
+                    ctrl.onReady(api);
+                }
+            }
+        }
+    }
+
+    app.directive('retailBeChargeCustomobjectsettings', faultTicketType);
+
+})(app);
