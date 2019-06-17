8ac011d9cf81d01c1a488bd8db6493bbeb61b883
Samer Haidar <VANRISE\samer.haidar>
2019-06-17 08:36:42 +0000

    Adding GenericFinancialAccountManager
    git-tfs-id: [http://developmentvlan:8585/tfs/vanrise.collection]$/;C56366

---
+++ Vanrise/Code/Vanrise.GenericData.Business/GenericFinancialAccountManager.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Vanrise.GenericData.Entities;
+using Vanrise.Common;
+using Vanrise.Common.Business;
+using Vanrise.Entities;
+
+namespace Vanrise.GenericData.Business
+{
+    public class GenericFinancialAccountManager
+    {
+        GenericFinancialAccountConfiguration _configuration;
+        public GenericFinancialAccountManager(GenericFinancialAccountConfiguration configuration)
+        {
+            configuration.ThrowIfNull("financialAccountMappingFieldsConfigration");
+            _configuration = configuration;
+        }
+        public GenericFinancialAccount GetFinancialAccount(string accountId)
+        {
+            var entity = new GenericBusinessEntityManager().GetGenericBusinessEntity(accountId, _configuration.FinancialAccountBEDefinitionId);
+            if (entity == null)
+                return null;
+            return GenericFinancialAccountMapper(entity);
+        }
+        private GenericFinancialAccount GenericFinancialAccountMapper(GenericBusinessEntity genericBusinessEntity)
+        {
+
+            GenericFinancialAccount genericFinancialAccount = new GenericFinancialAccount();
+
+            if (_configuration.BEDFieldName != null)
+            {
+                genericFinancialAccount.BED = (DateTime?)genericBusinessEntity.FieldValues.GetRecord(_configuration.BEDFieldName);
+            }
+            if (_configuration.EEDFieldName != null)
+            {
+                genericFinancialAccount.EED = (DateTime?)genericBusinessEntity.FieldValues.GetRecord(_configuration.EEDFieldName);
+            }
+            if (_configuration.FinancialAccountIdFieldName != null)
+            {
+                genericFinancialAccount.Name = genericBusinessEntity.FieldValues.GetRecord(_configuration.FinancialAccountIdFieldName) as string;
+
+            }
+            if (_configuration.FinancialAccountIdFieldName != null)
+            {
+                genericFinancialAccount.FinancialAccountId = genericBusinessEntity.FieldValues.GetRecord(_configuration.FinancialAccountIdFieldName) as string;
+            }
+            if (_configuration.StatusFieldName != null)
+            {
+                var statusDefinitionId = (Guid)genericBusinessEntity.FieldValues.GetRecord(_configuration.StatusFieldName);
+                var statusDefinition = new StatusDefinitionManager().GetStatusDefinition(statusDefinitionId);
+                statusDefinition.ThrowIfNull("statusDefinition", statusDefinitionId);
+                statusDefinition.Settings.ThrowIfNull("statusDefinition.Settings", statusDefinitionId);
+                genericFinancialAccount.Status = statusDefinition.Settings.IsActive ? VRAccountStatus.Active : VRAccountStatus.InActive;
+            }
+            return genericFinancialAccount;
+        }
+    }
+}
---
+++ Vanrise/Code/Vanrise.GenericData.Entities/GenericFinancialAccount.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Vanrise.Entities;
+
+namespace Vanrise.GenericData.Entities
+{
+    public class GenericFinancialAccount
+    {
+        public string FinancialAccountId { get; set; }
+        public string Name { get; set; }
+        public VRAccountStatus Status { get; set; }
+        public DateTime? BED { get; set; }
+        public DateTime? EED { get; set; }
+    }
+    public class GenericFinancialAccountConfiguration
+    {
+        public Guid FinancialAccountBEDefinitionId { get; set; }
+        public string FinancialAccountIdFieldName { get; set; }
+        public string AccountNameFieldName { get; set; }
+        public string StatusFieldName { get; set; }
+        public string BEDFieldName { get; set; }
+        public string EEDFieldName { get; set; }
+    }
+}
