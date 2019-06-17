11e1d36a97c16bbb2e005d6b3bc7709f8dead091
Fatima Hammoud <VANRISE\fatima.hammoud>
2019-06-17 12:10:10 +0000

    Adding "CarrierAccountBERuntimeSelectorFilter" class and its corresponding directive
    git-tfs-id: [http://developmentvlan:8585/tfs/vanrise.collection]$/;C56377

---
+++ TOneV2/Code/TOneV2/TOne.WhS.BusinessEntity.Business/CarrierAccounts/CarrierAccountBERuntimeSelectorFilter.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TOne.WhS.BusinessEntity.Entities;
+using Vanrise.GenericData.Entities;
+
+namespace TOne.WhS.BusinessEntity.Business
+{
+    public class CarrierAccountBERuntimeSelectorFilter : BERuntimeSelectorFilter, ICarrierAccountFilter
+    {
+        public DataTypeEnum DataType { get; set; }
+        public bool IsExcluded(ICarrierAccountFilterContext context)
+        {
+            throw new NotImplementedException();
+        }
+
+        public override bool IsMatched(IBERuntimeSelectorFilterSelectorFilterContext context)
+        {
+            ConfigManager configManager = new ConfigManager();
+
+            bool getFilteredData = false;
+            if (this.DataType == DataTypeEnum.Traffic)
+                getFilteredData = configManager.GetTrafficCarrierAccountFiltering();
+            else if (this.DataType == DataTypeEnum.Billing)
+                getFilteredData = configManager.GetBillingCarrierAccountFiltering();
+
+            if (getFilteredData)
+            {
+                AccountManagerAssignmentManager accountManagerAssignmentManager = new AccountManagerAssignmentManager();
+                IEnumerable<AccountManagerAssignment> accountManagerAssignments;
+                bool result = accountManagerAssignmentManager.TryGetCurrentUserAccountManagerAssignments(out accountManagerAssignments);
+                if (accountManagerAssignments != null)
+                {
+                    foreach (var accountManagerAssignement in accountManagerAssignments)
+                    {
+                        if (accountManagerAssignement.CarrierAccountId == (int)context.BusinessEntityId)
+                            return true;
+                    }
+                }
+            }
+            return false;
+        }
+    }
+}
---
+++ TOneV2/Code/TOneV2/TOne.WhS.BusinessEntity.Web/WhS_BusinessEntity/Directives/CarrierAccount/vr-whs-be-carrieraccount-runtimeselector-filter.js
@@ -0,0 +1,68 @@
+﻿'use strict';
+
+app.directive('vrWhsBeCarrieraccountRuntimeselectorFilter', ['UtilsService', 'VRUIUtilsService','WhS_BE_AccountManagerDataTypeEnum',
+    function (UtilsService, VRUIUtilsService, WhS_BE_AccountManagerDataTypeEnum) {
+        return {
+            restrict: 'E',
+            scope: {
+                onReady: '=',
+            },
+            controller: function ($scope, $element, $attrs) {
+                var ctrl = this;
+
+                ctrl.datasource = [];
+                ctrl.selectedvalues;
+
+                var ctor = new CarrierAccountBERuntimeSelectorFilterCtor(ctrl, $scope, $attrs);
+                ctor.initializeController();
+            },
+            templateUrl: "/Client/Modules/WhS_BusinessEntity/Directives/CarrierAccount/Templates/CarrierAccountBERuntimeSelectorFilterTemplate.html"
+        };
+
+        function CarrierAccountBERuntimeSelectorFilterCtor(ctrl, $scope, attrs) {
+            this.initializeController = initializeController;
+
+            var dataTypeSelectorAPI;
+            var dataTypeSelectorReadyPromiseDefereed = UtilsService.createPromiseDeferred();
+
+            function initializeController() {
+                $scope.scopeModel = {};
+                $scope.scopeModel.dataTypes = [];
+
+                $scope.scopeModel.onDataTypeSelectorReady = function (api) {
+                    dataTypeSelectorAPI = api;
+                    dataTypeSelectorReadyPromiseDefereed.resolve();
+                };
+
+                UtilsService.waitMultiplePromises([dataTypeSelectorReadyPromiseDefereed.promise]).then(function () {
+                    defineAPI();
+                });
+            }
+
+            function defineAPI() {
+                var api = {};
+
+                api.load = function (payload) {
+                    var promises = [];
+                    $scope.scopeModel.dataTypes.length = 0;
+
+                    $scope.scopeModel.dataTypes = UtilsService.getArrayEnum(WhS_BE_AccountManagerDataTypeEnum);
+
+                   // if (payload.settings != undefined) {
+                      //  $scope.scopeModel.selectedDataType = UtilsService.getItemByVal($scope.scopeModel.dataTypes, payload.settings.DataType, 'value');
+                    //}
+                    return UtilsService.waitMultiplePromises(promises);
+                };
+
+                api.getData = function () {
+                    return {
+                        $type: "TOne.WhS.BusinessEntity.Business.CarrierAccountBERuntimeSelectorFilter, TOne.WhS.BusinessEntity.Business",
+                        DataType: $scope.scopeModel.selectedDataType.value
+                    };
+                };
+
+                if (ctrl.onReady != null)
+                    ctrl.onReady(api);
+            }
+        }
+    }]);
