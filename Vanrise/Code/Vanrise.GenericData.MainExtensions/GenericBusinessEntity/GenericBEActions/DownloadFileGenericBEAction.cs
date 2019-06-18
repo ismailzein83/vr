d24f96023c3202a4a4312c2a3aae04407c0b68fd
Samer Haidar <VANRISE\samer.haidar>
2019-06-18 11:58:42 +0000

    adding DownloadFileGenericBEAction
    git-tfs-id: [http://developmentvlan:8585/tfs/vanrise.collection]$/;C56412

---
+++ Vanrise/Code/Vanrise.GenericData.MainExtensions/GenericBusinessEntity/GenericBEActions/DownloadFileGenericBEAction.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Vanrise.GenericData.Business;
+
+namespace Vanrise.GenericData.MainExtensions.GenericBusinessEntity.GenericBEActions
+{
+    public class DownloadFileGenericBEAction : GenericBEActionSettings
+    {
+        public override Guid ConfigId
+        {
+            get { return new Guid("8EF5476F-37EB-436F-8558-E3181AB78C82"); }
+        }
+        public override string ActionTypeName { get { return "DownloadFileGenericBEAction"; } }
+
+        public override string ActionKind { get { return "DownloadFile"; } }
+        public string FileIdFieldName { get; set; }
+    }
+}
