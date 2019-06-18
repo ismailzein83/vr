97602a41a44157196fc396db3906756c7748199e
yara.bousalman <yara.bousalman@vanrise.tfs.local>
2019-06-18 08:22:00 +0000

    Adding new implementation to recurring charge period
    git-tfs-id: [http://developmentvlan:8585/tfs/vanrise.collection]$/;C56402

---
+++ Retail/Code/Retail.BusinessEntity.MainExtensions/FinancialRecurringChargePeriod/BillingCycleRecurringCharge.cs
@@ -0,0 +1,27 @@
+﻿using Retail.BusinessEntity.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Retail.BusinessEntity.MainExtensions.FinancialRecurringChargePeriod
+{
+    public class BillingCycleRecurringCharge : RecurringChargePeriodSettings
+    {
+        public override Guid ConfigId { get { return new Guid("01FD5D1B-484B-4AD4-887C-CE3843A96060"); } }
+
+        public override void Execute(IFinancialRecurringChargePeriodSettingsContext context)
+        {
+            context.Periods = new List<RecurringChargePeriodOutput>
+            {
+                new RecurringChargePeriodOutput
+                {
+                    From = context.FromDate,
+                    To = context.ToDate,
+                    RecurringChargeDate = context.ToDate
+                }
+            };
+        }
+    }
+}
