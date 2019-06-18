2f974cbf93a9408152a7c3d868fff260a80c0185
Fatima Hammoud <VANRISE\fatima.hammoud>
2019-06-18 09:25:30 +0000

    Renaming from "GenericItemInformationView" to "GenericEditorSubView"
    git-tfs-id: [http://developmentvlan:8585/tfs/vanrise.collection]$/;C56409

--- Vanrise/Code/Vanrise.GenericData.MainExtensions/GenericBusinessEntity/GenericBEViewDefinition/GenericEditorSubView.cs
+++ Vanrise/Code/Vanrise.GenericData.MainExtensions/GenericBusinessEntity/GenericBEViewDefinition/GenericEditorSubView.cs
@@ -8,10 +8,10 @@ using Vanrise.GenericData.Entities;
 
 namespace Vanrise.GenericData.MainExtensions
 {
-    public class GenericItemInformationView : GenericBEViewDefinitionSettings
+    public class GenericEditorSubView : GenericBEViewDefinitionSettings
     {
         public override Guid ConfigId { get { return new Guid("A4C1333D-886B-4C94-BEFF-E1A32ADD69BE"); } }
-        public override string RuntimeDirective { get { return "vr-genericdata-genericbe-genericiteminfoview-runtime"; } }
+        public override string RuntimeDirective { get { return "vr-genericdata-genericbe-genericeditorsubview-runtime"; } }
         public override bool DoesUserHaveAccess(IGenericBEViewDefinitionCheckAccessContext context)
         {
             return base.DoesUserHaveAccess(context);
---
+++ Vanrise/Code/Vanrise.GenericData.Web/VR_GenericData/Directives/GenericBusinessEntity/Definition/GenericBEViewDefinition/vr-genericdata-genericbe-genericeditorsubview-definition.js
@@ -0,0 +1,108 @@
+﻿
+//(function (app) {
+
+//    'use strict';
+
+//    GenericEditorSubViewDefinitionDirective.$inject = ['UtilsService', 'VRUIUtilsService'];
+
+//    function GenericEditorSubViewDefinitionDirective(UtilsService, VRUIUtilsService) {
+//        return {
+//            restrict: 'E',
+//            scope: {
+//                onReady: '=',
+//            },
+//            controller: function ($scope, $element, $attrs) {
+//                var ctrl = this;
+//                var ctor = new GenericEditorSubViewDefinitionCtrol($scope, ctrl);
+//                ctor.initializeController();
+//            },
+//            controllerAs: 'ctrl',
+//            bindToController: true,
+//            templateUrl: '/Client/Modules/VR_GenericData/Directives/GenericBusinessEntity/Definition/GenericBEViewDefinition/Templates/GenericEditorSubViewViewTemplate.html'
+//        };
+
+//        function GenericEditorSubViewDefinitionCtrol($scope, ctrl) {
+//            this.initializeController = initializeController;
+
+//            var context;
+//            var editorSettings;
+
+//            var editorDefinitionAPI;
+//            var editorDefinitionReadyPromiseDeferred = UtilsService.createPromiseDeferred();
+
+//            function initializeController() {
+//                $scope.scopeModel = {};
+
+//                $scope.scopeModel.onGenericBEEditorDefinitionDirectiveReady = function (api) {
+//                    editorDefinitionAPI = api;
+//                    editorDefinitionReadyPromiseDeferred.resolve();
+//                };
+
+//                defineAPI();
+//            }
+
+//            function defineAPI() {
+//                var api = {};
+
+//                api.load = function (payload) {
+//                    console.log(payload);
+
+//                    var initialPromises = [];
+//                    if (payload != undefined) {
+//                        context = payload.context;
+//                        var parameterEntity = payload.parameterEntity;
+//                        if (parameterEntity != undefined)
+//                           editorSettings =  parameterEntity.GenericEditorDefinitionSetting;
+//                    }
+
+//                    initialPromises.push(loadEditorDefinitionDirective());
+//                    var rootPromiseNode = {
+//                        promises: initialPromises,
+//                        getChildNode: function () {
+//                            var directivePromises = [];
+//                            return {
+//                                promises: directivePromises,
+//                            };
+//                        }
+//                    };
+
+//                    return UtilsService.waitPromiseNode(rootPromiseNode);
+//                };
+
+
+//                api.getData = function () {
+//                    return {
+//                        $type: "Vanrise.GenericData.MainExtensions.GenericItemInformationView, Vanrise.GenericData.MainExtensions"
+//                    };
+//                };
+//                if (ctrl.onReady != undefined && typeof (ctrl.onReady) == 'function') {
+//                    ctrl.onReady(api);
+//                }
+//            }
+
+//            function loadEditorDefinitionDirective() {
+//                var loadEditorDefinitionDirectivePromiseDeferred = UtilsService.createPromiseDeferred();
+//                editorDefinitionReadyPromiseDeferred.promise.then(function () {
+//                    var editorPayload = {
+//                        settings: editorSettings,
+//                        context: getContext()
+//                    };
+
+//                    VRUIUtilsService.callDirectiveLoad(editorDefinitionAPI, editorPayload, loadEditorDefinitionDirectivePromiseDeferred);
+//                });
+//                return loadEditorDefinitionDirectivePromiseDeferred.promise;
+//            }
+
+//            function getContext() {
+//                var currentContext = context;
+//                if (currentContext == undefined)
+//                    currentContext = {};
+
+//                return currentContext;
+//            }
+//        }
+//    }
+
+//    app.directive('vrGenericdataGenericbeGenericeditorsubviewDefinition', GenericEditorSubViewDefinitionDirective);
+
+//})(app);
