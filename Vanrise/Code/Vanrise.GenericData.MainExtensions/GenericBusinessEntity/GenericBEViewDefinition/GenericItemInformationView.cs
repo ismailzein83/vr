f1f70c233400aa401f9747d5a86f1be4654ab92d
Fatima Hammoud <VANRISE\fatima.hammoud>
2019-06-18 08:36:14 +0000

    Adding  new class "GenericItemInformationView"
    git-tfs-id: [http://developmentvlan:8585/tfs/vanrise.collection]$/;C56404

---
+++ Vanrise/Code/Vanrise.GenericData.MainExtensions/GenericBusinessEntity/GenericBEViewDefinition/GenericItemInformationView.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Vanrise.GenericData.Business;
+using Vanrise.GenericData.Entities;
+
+namespace Vanrise.GenericData.MainExtensions
+{
+    public class GenericItemInformationView : GenericBEViewDefinitionSettings
+    {
+        public override Guid ConfigId { get { return new Guid("A4C1333D-886B-4C94-BEFF-E1A32ADD69BE"); } }
+        public override string RuntimeDirective { get { return "vr-genericdata-genericbe-genericiteminfoview-runtime"; } }
+        public override bool DoesUserHaveAccess(IGenericBEViewDefinitionCheckAccessContext context)
+        {
+            return base.DoesUserHaveAccess(context);
+        }
+        public VRGenericEditorDefinitionSetting GenericEditorDefinitionSetting { get; set; }
+    }
+}
