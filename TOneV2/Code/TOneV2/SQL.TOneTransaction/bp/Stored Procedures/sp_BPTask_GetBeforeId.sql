4c4eb133c1cfdcb82d4d0aa8a05af98ed2547030
Naim Saad <VANRISE\naim.saad>
2019-06-19 08:41:34 +0000

    Implementing filters at getbeforeid and getupdated
    git-tfs-id: [http://developmentvlan:8585/tfs/vanrise.collection]$/;C56431

--- TOneV2/Code/TOneV2/SQL.TOneTransaction/bp/Stored Procedures/sp_BPTask_GetBeforeId.sql
+++ TOneV2/Code/TOneV2/SQL.TOneTransaction/bp/Stored Procedures/sp_BPTask_GetBeforeId.sql
@@ -2,10 +2,20 @@
 	@LessThanID BIGINT,
 	@NbOfRows INT,
 	@ProcessInstanceId BIGINT,
-	@UserId int
+	@UserId int , 
+	@BpTaskStatuses varchar(max),
+	@TaskTypeIds varchar(max),
+	@Title nvarchar(255)
 AS
 BEGIN
 
+DECLARE @TaskTypeIdsTable TABLE (TypeId uniqueidentifier)
+	INSERT INTO @TaskTypeIdsTable (TypeId)
+	select Convert(uniqueidentifier, ParsedString) from [bp].[ParseStringList](@TaskTypeIds)
+
+DECLARE @BpTaskStatusesTable TABLE ([Status] int)
+	INSERT INTO @BpTaskStatusesTable ([Status])
+	select Convert(int, ParsedString) from [bp].[ParseStringList](@BpTaskStatuses)
 
 SELECT TOP(@NbOfRows)[ID]
 			, [ProcessInstanceID]
@@ -27,7 +37,9 @@ SELECT TOP(@NbOfRows)[ID]
 	WHERE	ID < @LessThanID 
 			AND (@ProcessInstanceId is null or ProcessInstanceID = @ProcessInstanceId)
             AND (@UserId is null or ',' + AssignedUsers + ',' like '%,' +CONVERT(varchar(100), @UserId)  +',%')
-	
+			AND (@TaskTypeIds is null or [TypeID] in (select TypeId from @TaskTypeIdsTable))
+			AND (@Title is null or Title like '%' + @Title + '%')
+			AND (@BpTaskStatuses is null or [Status] not in (select [Status] from @BpTaskStatusesTable))
 	
 	ORDER BY ID DESC
 END
--- TOneV2/Code/TOneV2/SQL.TOneTransaction/bp/Stored Procedures/sp_BPTask_GetUpdated.sql
+++ TOneV2/Code/TOneV2/SQL.TOneTransaction/bp/Stored Procedures/sp_BPTask_GetUpdated.sql
@@ -3,9 +3,21 @@
 	@AfterId bigint,
 	@NbOfRows INT,
 	@ProcessInstanceId BIGINT,
-	@UserId int
+	@UserId int , 
+	@BpTaskStatuses varchar(max),
+	@TaskTypeIds varchar(max),
+	@Title nvarchar(255)
+	 
 AS
 BEGIN
+
+DECLARE @TaskTypeIdsTable TABLE (TypeId uniqueidentifier)
+	INSERT INTO @TaskTypeIdsTable (TypeId)
+	select Convert(uniqueidentifier, ParsedString) from [bp].[ParseStringList](@TaskTypeIds)
+
+DECLARE @BpTaskStatusesTable TABLE ([Status] int)
+	INSERT INTO @BpTaskStatusesTable ([Status])
+	select Convert(int, ParsedString) from [bp].[ParseStringList](@BpTaskStatuses)
 
 IF (@AfterLastUpdateTime IS NULL)
 	BEGIN
@@ -28,7 +40,10 @@ IF (@AfterLastUpdateTime IS NULL)
             FROM [BP].[BPTask]  WITH(NOLOCK) 
             
             WHERE (@ProcessInstanceId is null or ProcessInstanceID = @ProcessInstanceId)
-            AND (@UserId is null or ',' + AssignedUsers + ',' like '%,' +CONVERT(varchar(100), @UserId)  +',%')
+			AND   (@TaskTypeIds is null or [TypeID] in (select TypeId from @TaskTypeIdsTable))
+			AND   (@Title is null or Title like '%' + @Title + '%')
+			AND   (@BpTaskStatuses is null or [Status] not in (select [Status] from @BpTaskStatusesTable))
+            AND   (@UserId is null or ',' + AssignedUsers + ',' like '%,' +CONVERT(varchar(100), @UserId)  +',%')
             ORDER BY ID DESC
             
             SELECT * FROM #temp_table
@@ -55,6 +70,8 @@ ELSE
             FROM [BP].[BPTask]  WITH(NOLOCK) 
             
             WHERE (@ProcessInstanceId is null or ProcessInstanceID = @ProcessInstanceId)
+			AND  (@TaskTypeIds is null or [TypeID] in (select TypeId from @TaskTypeIdsTable))
+			AND(@Title is null or Title like '%' + @Title + '%')
             AND (@UserId is null or ',' + AssignedUsers + ',' like '%,' +CONVERT(varchar(100), @UserId)  +',%')
             AND (LastUpdatedTime > @AfterLastUpdateTime OR (LastUpdatedTime = @AfterLastUpdateTime AND ID > @AfterId)) --ONLY Updated records
             ORDER BY [LastUpdatedTime], [ID]
