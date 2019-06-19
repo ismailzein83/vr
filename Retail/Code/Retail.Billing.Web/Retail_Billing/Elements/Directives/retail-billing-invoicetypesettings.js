6f4ea1979af9f7be2653477fe95221b77dd96538
Naim Saad <VANRISE\naim.saad>
2019-06-19 15:57:53 +0000

    Implementing Billing Invoice Type Settings
    git-tfs-id: [http://developmentvlan:8585/tfs/vanrise.collection]$/;C56461

---
+++ Retail/Code/Retail.Billing.Web/Retail_Billing/Elements/Directives/retail-billing-invoicetypesettings.js
@@ -0,0 +1,85 @@
+﻿"use strict";
+
+app.directive("retailBillingInvoicetypesettings", ["UtilsService", "VRNotificationService", "VRUIUtilsService", "Retail_Interconnect_InvoiceType",
+    function (UtilsService, VRNotificationService, VRUIUtilsService, Retail_Interconnect_InvoiceType) {
+
+        var directiveDefinitionObject = {
+
+            restrict: "E",
+            scope:
+            {
+                onReady: "=",
+            },
+            controller: function ($scope, $element, $attrs) {
+                var ctrl = this;
+
+                var ctor = new BillingInvoiceSettings($scope, ctrl, $attrs);
+                ctor.initializeController();
+            },
+            controllerAs: "ctrl",
+            bindToController: true,
+            compile: function (element, attrs) {
+
+            },
+            templateUrl: "/Client/Modules/Retail_Billing/Elements/Directives/Templates/BillingInvoiceTypeSettingsTemplate.html"
+
+        };
+
+        function BillingInvoiceSettings($scope, ctrl, $attrs) {
+            this.initializeController = initializeController;
+
+            var genericInvoiceSettingsDirectiveAPI;
+            var genericInvoiceSettingsDirectivePromiseDeferred = UtilsService.createPromiseDeferred();
+
+            function initializeController() {
+                $scope.scopeModel = {};
+
+                $scope.scopeModel.onGenericInvoiceSettingsReady = function (api) {
+                    genericInvoiceSettingsDirectiveAPI = api;
+                    genericInvoiceSettingsDirectivePromiseDeferred.resolve();
+                };
+
+                defineAPI();
+            }
+
+            function defineAPI() {
+                var api = {};
+
+                api.load = function (payload) {
+                    var promises = [];
+                    promises.push(getGenericInvoiceSettingsDirectiveLoadPromise());
+
+                    function getGenericInvoiceSettingsDirectiveLoadPromise() {
+                        var genericInvoiceSettingsDirectiveLoadDeferred = UtilsService.createPromiseDeferred();
+
+                        genericInvoiceSettingsDirectivePromiseDeferred.promise.then(function () {
+                            var directivePayload;
+                            if (payload != undefined && payload.extendedSettingsEntity != undefined) {
+                                directivePayload = payload.extendedSettingsEntity;
+                            }
+                            VRUIUtilsService.callDirectiveLoad(genericInvoiceSettingsDirectiveAPI, directivePayload, genericInvoiceSettingsDirectiveLoadDeferred);
+                        });
+                        return genericInvoiceSettingsDirectiveLoadDeferred.promise;
+                    }
+
+                    return UtilsService.waitMultiplePromises(promises);
+                };
+
+
+                api.getData = function () {
+                    var obj = {
+                        $type: "Retail.Billing.Business.BillingInvoiceSettings, Retail.Billing.Business",
+                    };
+
+                    genericInvoiceSettingsDirectiveAPI.setData(obj);
+                    return obj;
+                };
+
+                if (ctrl.onReady != null)
+                    ctrl.onReady(api);
+            }
+        }
+
+        return directiveDefinitionObject;
+    }
+]);
