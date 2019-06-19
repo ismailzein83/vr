246173d8be21204a89f02eafef4da9889f9bab57
Naim Saad <VANRISE\naim.saad>
2019-06-19 13:46:41 +0000

    Implementing Billing Invoice Settings
    git-tfs-id: [http://developmentvlan:8585/tfs/vanrise.collection]$/;C56449

---
+++ Retail/Code/Retail.Billing.Business/BillingInvoiceSettings.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+using Vanrise.Invoice.Entities;
+using Vanrise.Invoice.MainExtensions;
+
+namespace Retail.Billing.Business
+{
+    public class BillingInvoiceSettings : GenericInvoiceSettings
+    {
+        public override Guid ConfigId { get { return new Guid("1535DBF0-01E9-4E8B-9566-A278F357CC80"); } }
+
+        public override dynamic GetInfo(IInvoiceTypeExtendedSettingsInfoContext context)
+        {
+            return null;
+        }
+
+        public override void GetInitialPeriodInfo(IInitialPeriodInfoContext context)
+        {
+        }
+
+        public override InvoiceGenerator GetInvoiceGenerator()
+        {
+            return null;
+        }
+
+        public override IEnumerable<string> GetPartnerIds(IExtendedSettingsPartnerIdsContext context)
+        {
+            return null;
+        }
+
+        public override InvoicePartnerManager GetPartnerManager()
+        {
+            return null;
+        }
+    }
+}
