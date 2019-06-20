0c3b3ece4c4d5643e615dcfa0c8501932b7c5f4d
Naim Saad <VANRISE\naim.saad>
2019-06-20 11:22:21 +0000

    Fixing namespace
    git-tfs-id: [http://developmentvlan:8585/tfs/vanrise.collection]$/;C56505

--- Vanrise/Code/Vanrise.AccountBalance.MainExtensions/GenericFinancialAccountBalanceSetting.cs
+++ Vanrise/Code/Vanrise.AccountBalance.MainExtensions/GenericFinancialAccountBalanceSetting.cs
@@ -1,21 +1,20 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using Vanrise.AccountBalance.Business;
 using Vanrise.AccountBalance.Entities;
 using Vanrise.GenericData.Entities;
 using Vanrise.Notification.Entities;
 
-namespace Vanrise.AccountManager.MainExtensions
+namespace Vanrise.AccountBalance.MainExtensions
 {
     public class GenericFinancialAccountBalanceSetting : AccountTypeExtendedSettings
     {
         public override Guid ConfigId => new Guid("9B248C32-B9A6-4B87-9D0A-8CF7FD6FB11E");
 
-        public override string AccountSelector => throw new NotImplementedException();
-        public  GenericFinancialAccountConfiguration Configuration { get; set; }
+        public override string AccountSelector
+        {
+            get { return "retail-be-extendedsettings-account-selector"; }
+        }
+        public GenericFinancialAccountConfiguration Configuration { get; set; }
         public override IAccountManager GetAccountManager()
         {
             return new GenericFinancialAccountBalanceManager(Configuration);
--- Vanrise/Code/Vanrise.AccountBalance.MainExtensions/GenericSendEmailActionDefinitionSettings.cs
+++ Vanrise/Code/Vanrise.AccountBalance.MainExtensions/GenericSendEmailActionDefinitionSettings.cs
@@ -1,9 +1,4 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using Vanrise.AccountManager.MainExtensions;
 using Vanrise.Notification.Entities;
 
 namespace Vanrise.AccountBalance.MainExtensions
