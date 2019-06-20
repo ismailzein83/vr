8addcd75905ad18670660e65764f73e489bed0a6
Mohammad Sheaib <VANRISE\mohammad.sheaib>
2019-06-20 15:28:26 +0000

    GenericBEStatusHistorySubview
    git-tfs-id: [http://developmentvlan:8585/tfs/vanrise.collection]$/;C56530

---
+++ Vanrise/Code/Vanrise.GenericData.MainExtensions/GenericBusinessEntity/GenericBEViewDefinition/GenericBEStatusHistorySubview.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Vanrise.GenericData.Business;
+using Vanrise.GenericData.Entities;
+
+namespace Vanrise.GenericData.MainExtensions
+{
+    public class GenericBEStatusHistorySubview : GenericBEViewDefinitionSettings
+    {
+        public override Guid ConfigId
+        {
+            get { return new Guid("03F03D74-D44B-45BC-A2D3-79512D5D2C53"); }
+        }
+        public override string RuntimeDirective 
+        {
+            get { return "vr-genericdata-genericbe-statushistorysubview-runtime"; }
+        }
+        public string MappingColumnName { get; set; }
+        public override bool DoesUserHaveAccess(IGenericBEViewDefinitionCheckAccessContext context)
+        {
+            return base.DoesUserHaveAccess(context);
+        }
+    }
+}
