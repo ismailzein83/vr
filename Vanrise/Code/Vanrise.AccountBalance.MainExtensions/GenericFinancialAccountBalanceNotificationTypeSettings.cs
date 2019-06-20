5b0a979ec90256f21161cd2c41e76adfdacacab1
Samer Haidar <VANRISE\samer.haidar>
2019-06-20 18:32:29 +0000

    adding GenericFinancialAccountBalanceNotificationTypeSettings
    git-tfs-id: [http://developmentvlan:8585/tfs/vanrise.collection]$/;C56537

---
+++ Vanrise/Code/Vanrise.AccountBalance.MainExtensions/GenericFinancialAccountBalanceNotificationTypeSettings.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Vanrise.AccountBalance.Business;
+
+namespace Vanrise.AccountBalance.MainExtensions
+{
+    public class GenericFinancialAccountBalanceNotificationTypeSettings : AccountBalanceNotificationTypeExtendedSettings
+    {
+        public override Guid ConfigId { get { return new Guid("B49238AC-54E2-4F3F-8C91-C864B7389566"); } }
+
+        public override string NotificationQueryEditor { get { return "vr-accountbalance-financialaccountbalancenotificationtype-searcheditor"; } }
+
+        public Guid FinancialAccountBEDefinitionId { get; set; }
+
+        public override bool IsVRNotificationMatched(IAccountBalanceNotificationTypeIsMatchedContext context)
+        {
+            if (context.AccountBalanceNotificationExtendedQuery == null)
+                return true;
+
+            var extendedQuery = context.AccountBalanceNotificationExtendedQuery as GenericFinanciaAccountBalanceNotificationExtendedQuery;
+            if (extendedQuery == null)
+                return false;
+
+            if (extendedQuery.AccountIds != null && !extendedQuery.AccountIds.Contains(context.EventPayload.EntityId))
+                return false;
+
+            return true;
+        }
+    }
+    public class GenericFinanciaAccountBalanceNotificationExtendedQuery : AccountBalanceNotificationExtendedQuery
+    {
+        public List<string> AccountIds { get; set; }
+    }
+}
