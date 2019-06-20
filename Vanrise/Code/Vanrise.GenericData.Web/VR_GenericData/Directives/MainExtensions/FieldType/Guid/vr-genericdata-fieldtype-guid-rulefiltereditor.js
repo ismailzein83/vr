a78b51d34704f8e55a4d2e68b4503b1e8f8f94c4
Mohammad Sheaib <VANRISE\mohammad.sheaib>
2019-06-20 14:10:57 +0000

    Guid Rule Filter
    git-tfs-id: [http://developmentvlan:8585/tfs/vanrise.collection]$/;C56521

--- Vanrise/Code/Vanrise.GenericData.Web/VR_GenericData/Directives/MainExtensions/FieldType/Guid/vr-genericdata-fieldtype-guid-rulefiltereditor.js
+++ Vanrise/Code/Vanrise.GenericData.Web/VR_GenericData/Directives/MainExtensions/FieldType/Guid/vr-genericdata-fieldtype-guid-rulefiltereditor.js
@@ -1,6 +1,6 @@
 ﻿'use strict';
-app.directive('vrGenericdataFieldtypeGuidRulefiltereditor', ['VR_GenericData_StringRecordFilterOperatorEnum', 'UtilsService',
-    function (VR_GenericData_StringRecordFilterOperatorEnum, UtilsService) {
+app.directive('vrGenericdataFieldtypeGuidRulefiltereditor', ['VR_GenericData_StringRecordFilterOperatorEnum', 'UtilsService', 'VRUIUtilsService',
+    function (VR_GenericData_StringRecordFilterOperatorEnum, UtilsService, VRUIUtilsService) {
 
         var directiveDefinitionObject = {
             restrict: 'E',
@@ -18,33 +18,59 @@ app.directive('vrGenericdataFieldtypeGuidRulefiltereditor', ['VR_GenericData_Str
 
             },
             templateUrl: "/Client/Modules/VR_GenericData/Directives/MainExtensions/FieldType/Guid/Templates/GuidFieldTypeRuleFilterEditor.html"
+
         };
 
         function recordTypeFieldItemEditorCtor(ctrl, $scope, $attrs) {
-
             function initializeController() {
                 defineAPI();
             }
-
+            var textFilterEditorApi;
+            var textFilterReadyDeferred = UtilsService.createPromiseDeferred();
+            var filterObj;
+            var dataRecordTypeField;
+            $scope.onTextFilterEditorReady = function (api) {
+                textFilterEditorApi = api;
+                textFilterReadyDeferred.resolve();
+            };
             function defineAPI() {
                 var api = {};
 
                 api.load = function (payload) {
-                    if (payload && payload.filterObj) {
-                        $scope.guidValue = payload.filterObj.GuidValue;
+                    $scope.filters = UtilsService.getArrayEnum(VR_GenericData_StringRecordFilterOperatorEnum);
+                    $scope.selectedFilter = $scope.filters[0];
+                    var promises = [];
+                    if (payload) {
+                        dataRecordTypeField = payload.dataRecordTypeField;
+                        filterObj = payload.filterObj;
+                        if (filterObj != undefined) {
+                            $scope.selectedFilter = UtilsService.getItemByVal($scope.filters, filterObj.CompareOperator, 'value');
+                        }
+                        var textFilterLoadDeferred = UtilsService.createPromiseDeferred();
+
+                        textFilterReadyDeferred.promise.then(function () {
+                            var textFilterPayload = {
+                                fieldType: dataRecordTypeField != undefined ? dataRecordTypeField.Type : undefined,
+                                fieldValue: filterObj != undefined ? filterObj.Value : undefined
+                            };
+                            VRUIUtilsService.callDirectiveLoad(textFilterEditorApi, textFilterPayload, textFilterLoadDeferred);
+                        });
+                        promises.push(textFilterLoadDeferred.promise);
                     }
+                    return UtilsService.waitMultiplePromises(promises);
+
                 };
 
                 api.getData = function () {
                     return {
-                        $type: "Vanrise.GenericData.Entities.GuidRecordFilter, Vanrise.GenericData.Entities",
-                        GuidValue: $scope.guidValue
-
+                        $type: "Vanrise.GenericData.Entities.StringRecordFilter, Vanrise.GenericData.Entities",
+                        CompareOperator: $scope.selectedFilter.value,
+                        Value: textFilterEditorApi.getData()
                     };
                 };
 
                 api.getExpression = function () {
-                    return $scope.guidValue;
+                    return $scope.selectedFilter.description + ' ' + textFilterEditorApi.getData();
                 };
 
                 if (ctrl.onReady != null)
