836431d5e91721bb3730400dc485a012600348fa
Hassan Makhour <VANRISE\hassan.makhour>
2019-06-21 11:16:48 +0000

    financial account balance notification and generic send email action implementation
    git-tfs-id: [http://developmentvlan:8585/tfs/vanrise.collection]$/;C56560

---
+++ Vanrise/Code/Vanrise.AccountBalance.Web/VR_AccountBalance/Directives/AccountBalanceNotification/vr-accountbalance-financialaccountbalance-notification-type.js
@@ -0,0 +1,65 @@
+﻿'use strict';
+       
+app.directive('vrAccountbalanceFinancialaccountbalanceNotificationType', ['UtilsService', 'VRUIUtilsService', 'VR_AccountBalance_AccountBalanceNotificationTypeAPIService',
+	function (UtilsService, VRUIUtilsService, VR_AccountBalance_AccountBalanceNotificationTypeAPIService) {
+		return {
+			restrict: 'E',
+			scope: {
+				onReady: '=',
+				normalColNum: '@'
+			},
+			controller: function ($scope, $element, $attrs) {
+				var ctrl = this;
+				var ctor = new FinancialAccountBalanceNotificationTypeSettings($scope, ctrl, $attrs);
+				ctor.initializeController();
+			},
+			controllerAs: 'ctrl',
+			bindToController: true,
+			templateUrl: "/Client/Modules/VR_AccountBalance/Directives/AccountBalanceNotification/Templates/AccountBalanceFinancialAccountBalanceNotificationTypeTemplate.html"
+		};
+
+		function FinancialAccountBalanceNotificationTypeSettings($scope, ctrl, $attrs) {
+			this.initializeController = initializeController;
+
+			var beDefinitionSelectorAPI;
+			var beDefinitionSelectorDeferredReady = UtilsService.createPromiseDeferred();
+			
+			function initializeController() {
+				$scope.scopeModel = {};
+				$scope.scopeModel.onBusinessEntityDefinitionSelectorReady = function (api) {
+					beDefinitionSelectorAPI = api;
+					beDefinitionSelectorDeferredReady.resolve();
+				};
+				defineAPI();
+			}
+
+			function defineAPI() {
+				var api = {};
+				api.load = function (payload) {
+					var beDefinitionId;
+					var promises = [];
+					if (payload != undefined && payload.accountBalanceNotificationTypeExtendedSettings != undefined)
+						beDefinitionId = payload.accountBalanceNotificationTypeExtendedSettings.FinancialAccountBEDefinitionId;
+					promises.push(loadSelector());
+
+					function loadSelector() {
+						var selectorLoadPromiseDeferred = UtilsService.createPromiseDeferred();
+						beDefinitionSelectorDeferredReady.promise.then(function () {
+							var selectorPayload = { selectedIds: beDefinitionId };
+							VRUIUtilsService.callDirectiveLoad(beDefinitionSelectorAPI, selectorPayload, selectorLoadPromiseDeferred);
+						});
+						return selectorLoadPromiseDeferred.promise;
+					}
+					return UtilsService.waitMultiplePromises(promises);
+				};
+				api.getData = function () {
+					return {
+						$type: "Vanrise.AccountBalance.MainExtensions.GenericFinancialAccountBalanceNotificationTypeSettings,Vanrise.AccountBalance.MainExtensions",
+						FinancialAccountBEDefinitionId: beDefinitionSelectorAPI.getSelectedIds()
+					};
+				};
+				if (ctrl.onReady != null)
+					ctrl.onReady(api);
+			}
+		}
+	}]);
---
+++ Vanrise/Code/Vanrise.GenericData.Web/VR_GenericData/Directives/MainExtensions/DataRecord/VRActionDefinition/vr-genericdata-datarecord-vractiondefinition-extendedsettings-genericsendemail.js
@@ -0,0 +1,82 @@
+﻿app.directive('vrGenericdataDatarecordVractiondefinitionExtendedsettingsGenericsendemail', ['UtilsService', 'VRUIUtilsService', 'VRCommon_VRMailMessageTypeAPIService',
+	function (UtilsService, VRUIUtilsService, VRCommon_VRMailMessageTypeAPIService) {
+		return {
+			restrict: 'E',
+			scope: {
+				onReady: '='
+			},
+			controller: function ($scope, $element, $attrs) {
+				var ctrl = this;
+				var ctor = new genericSendEmailActionDefinition($scope, ctrl, $attrs);
+				ctor.initializeController();
+			},
+			controllerAs: 'ctrl',
+			bindToController: true,
+			templateUrl: '/Client/Modules/VR_GenericData/Directives/MainExtensions/DataRecord/VRActionDefinition/Templates/VRActionDefinitionGenericSendEmail.html'
+		};
+
+		function genericSendEmailActionDefinition($scope, ctrl, $attrs) {
+			this.initializeController = initializeController;
+
+			var beDefinitionSelectorAPI;
+			var beDefinitionSelectorDeferredReady = UtilsService.createPromiseDeferred();
+
+			var mailMessageTypeSelectorAPI;
+			var mailMessageTypeSelectorDeferredReady = UtilsService.createPromiseDeferred();
+			function initializeController() {
+				$scope.scopeModel = {};
+				$scope.scopeModel.onBusinessEntityDefinitionSelectorReady = function (api) {
+					beDefinitionSelectorAPI = api;
+					beDefinitionSelectorDeferredReady.resolve();
+				};
+				$scope.scopeModel.onMailMessageTemplateSelectorReady = function (api) {
+					mailMessageTypeSelectorAPI = api;
+					mailMessageTypeSelectorDeferredReady.resolve();
+				};
+				defineAPI();
+			};
+
+			function defineAPI() {
+				var api = {};
+				api.load = function (payload) {
+					var promises = [];
+					var beDefinitionId;
+					var mailMessageTypeId;
+					if (payload != undefined && payload.Settings != undefined && payload.Settings.ExtendedSettings) {
+						beDefinitionId = payload.Settings.ExtendedSettings.FinancialAccountBEDefinitionId;
+						mailMessageTypeId = payload.Settings.ExtendedSettings.MailMessageTypeId;;
+					}
+					promises.push(loadBEDefinitionSelector());
+					promises.push(loadMailMessageTemplateSelector());
+
+					function loadBEDefinitionSelector() {
+						var selectorLoadPromiseDeferred = UtilsService.createPromiseDeferred();
+						beDefinitionSelectorDeferredReady.promise.then(function () {
+							var selectorPayload = { selectedIds: beDefinitionId };
+							VRUIUtilsService.callDirectiveLoad(beDefinitionSelectorAPI, selectorPayload, selectorLoadPromiseDeferred);
+						});
+						return selectorLoadPromiseDeferred.promise;
+					}
+
+					function loadMailMessageTemplateSelector() {
+						var selectorLoadPromiseDeferred = UtilsService.createPromiseDeferred();
+						beDefinitionSelectorDeferredReady.promise.then(function () {
+							var selectorPayload = { selectedIds: mailMessageTypeId };
+							VRUIUtilsService.callDirectiveLoad(mailMessageTypeSelectorAPI, selectorPayload, selectorLoadPromiseDeferred);
+						});
+						return selectorLoadPromiseDeferred.promise;
+					}
+					return UtilsService.waitMultiplePromises(promises);
+				};
+				api.getData = function () {
+					return {
+						$type: 'Vanrise.AccountBalance.MainExtensions.GenericSendEmailActionDefinitionSettings,Vanrise.AccountBalance.MainExtensions',
+						FinancialAccountBEDefinitionId: beDefinitionSelectorAPI.getSelectedIds(),
+						MailMessageTypeId: mailMessageTypeSelectorAPI.getSelectedIds()
+					};
+				};
+				if (ctrl.onReady != null)
+					ctrl.onReady(api);
+			};
+		}
+	}]);
