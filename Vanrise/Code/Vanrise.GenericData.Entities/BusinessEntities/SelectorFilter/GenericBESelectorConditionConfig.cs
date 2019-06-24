e12618a9488b29bf6fef25862238a202bc50a865
Fatima Al Heck <VANRISE\fatima.alheck>
2019-06-24 07:44:16 +0000

    Adding GenericBESelectorConditionConfig
    git-tfs-id: [http://developmentvlan:8585/tfs/vanrise.collection]$/;C56587

---
+++ Vanrise/Code/Vanrise.GenericData.Entities/BusinessEntities/SelectorFilter/GenericBESelectorConditionConfig.cs
@@ -0,0 +1,10 @@
+﻿using Vanrise.Entities;
+
+namespace Vanrise.GenericData.Entities
+{
+    public class GenericBESelectorConditionConfig : ExtensionConfiguration
+    {
+        public const string EXTENSION_TYPE = "VR_GenericData_GenericBESelectorConditionConfig";
+        public string Editor { get; set; }
+    }
+}
