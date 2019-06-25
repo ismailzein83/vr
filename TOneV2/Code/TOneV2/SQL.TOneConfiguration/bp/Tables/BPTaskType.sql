a045b59cbd68dcee465d5d885574552fca2e5647
Ali Slayman <VANRISE\ali.slayman>
2019-06-25 07:06:53 +0000

    Adding Title property to BPTaskType Scripts and Table
    git-tfs-id: [http://developmentvlan:8585/tfs/vanrise.collection]$/;C56632

--- TOneV2/Code/TOneV2/SQL.TOneConfiguration/bp/Tables/BPTaskType.sql
+++ TOneV2/Code/TOneV2/SQL.TOneConfiguration/bp/Tables/BPTaskType.sql
@@ -2,11 +2,14 @@
     [ID]               UNIQUEIDENTIFIER CONSTRAINT [DF__BPTaskType__ID__1F5986F6] DEFAULT (newid()) NOT NULL,
     [DevProjectID]     UNIQUEIDENTIFIER NULL,
     [Name]             VARCHAR (255)    NOT NULL,
+    [Title]            VARCHAR (255)    NULL,
     [Settings]         NVARCHAR (MAX)   NOT NULL,
     [timestamp]        ROWVERSION       NOT NULL,
     [LastModifiedTime] DATETIME         CONSTRAINT [DF_BPTaskType_LastModifiedTime] DEFAULT (getdate()) NULL,
     CONSTRAINT [pk_BPTaskType] PRIMARY KEY CLUSTERED ([ID] ASC)
 );
+
+
 
 
 
