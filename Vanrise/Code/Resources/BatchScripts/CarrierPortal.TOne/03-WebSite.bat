8b31a2711ec5094a1c54a8b99aaa6cd00d44fee0
Wissam Ajamy <VANRISE\wissam.ajamy>
2019-06-25 13:20:29 +0000

    Adding PartnerPortal_CustomerAccess
    git-tfs-id: [http://developmentvlan:8585/tfs/vanrise.collection]$/;C56651

--- Vanrise/Code/Resources/BatchScripts/CarrierPortal.TOne/03-WebSite.bat
+++ Vanrise/Code/Resources/BatchScripts/CarrierPortal.TOne/03-WebSite.bat
@@ -7,8 +7,11 @@ For /F %PARSEARG% %%i in (%CURRDATE%) Do SET YYYYMMDD=%%l%%j%%k
 
 ECHO CarrierPortal.TOne WebSite
 
-xcopy "C:\TFS\CarrierPortal\Code\CP.WhS.Web\CP_WhS" /S /E /R /y /v /i /z /Q				"\\192.168.110.185\Fixes\WebSite\CP.TOne\%YYYYMMDD%\Client\Modules\CP_WhS\"
-xcopy "C:\TFS\CarrierPortal\Code\CarrierPortal.Web\bin\CP.WhS*" /S /E /R /y /v /i /z /Q	"\\192.168.110.185\Fixes\WebSite\CP.TOne\%YYYYMMDD%\Bin\"
-xcopy "C:\TFS\CarrierPortal\Code\CarrierPortal.Web\bin\TOne*" /S /E /R /y /v /i /z /Q	"\\192.168.110.185\Fixes\WebSite\CP.TOne\%YYYYMMDD%\Bin\"
-del /s /q /f																			"\\192.168.110.185\Fixes\WebSite\CP.TOne\%YYYYMMDD%\Bin\*.pdb"
-del /s /q /f																			"\\192.168.110.185\Fixes\WebSite\CP.TOne\%YYYYMMDD%\Bin\*.config"
+xcopy "C:\TFS\CarrierPortal\Code\CP.WhS.Web\CP_WhS" /S /E /R /y /v /i /z /Q												"\\192.168.110.185\Fixes\WebSite\CP.TOne\%YYYYMMDD%\Client\Modules\CP_WhS\"
+xcopy "C:\TFS\CarrierPortal\Code\CarrierPortal.Web\bin\CP.WhS*" /S /E /R /y /v /i /z /Q									"\\192.168.110.185\Fixes\WebSite\CP.TOne\%YYYYMMDD%\Bin\"
+xcopy "C:\TFS\CarrierPortal\Code\PartnerPortal.CustomerAccess.Web\PartnerPortal_CustomerAccess" /S /E /R /y /v /i /z /Q	"\\192.168.110.185\Fixes\WebSite\CP.TOne\%YYYYMMDD%\Client\Modules\PartnerPortal_CustomerAccess\"
+xcopy "C:\TFS\CarrierPortal\Code\CarrierPortal.Web\bin\PartnerPortal.CustomerAccess*" /S /E /R /y /v /i /z /Q			"\\192.168.110.185\Fixes\WebSite\CP.TOne\%YYYYMMDD%\Bin\"
+
+xcopy "C:\TFS\CarrierPortal\Code\CarrierPortal.Web\bin\TOne*" /S /E /R /y /v /i /z /Q									"\\192.168.110.185\Fixes\WebSite\CP.TOne\%YYYYMMDD%\Bin\"
+del /s /q /f																											"\\192.168.110.185\Fixes\WebSite\CP.TOne\%YYYYMMDD%\Bin\*.pdb"
+del /s /q /f																											"\\192.168.110.185\Fixes\WebSite\CP.TOne\%YYYYMMDD%\Bin\*.config"
