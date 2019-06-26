292e744770c5c2e9b65900e4b8f5f7fd555f8211
Ali Atoui <VANRISE\ali.atoui>
2019-06-26 13:25:57 +0000

    Editing NIM Module
    git-tfs-id: [http://developmentvlan:8585/tfs/vanrise.collection]$/;C56723

---
+++ Retail/Code/Retail.NIM.Web/Controllers/FreeFTTHPathController.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Retail.NIM.Web.Controllers
+{
+    public class FreeFTTHPathController
+    {
+    }
+}
