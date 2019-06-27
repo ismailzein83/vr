5701871f6a528d1d045996fa15053d5a8d543b16
Ali Ballouk <VANRISE\ali.ballouk>
2019-06-27 07:49:22 +0000

    adding sp_View_UpdateAudiences
    git-tfs-id: [http://developmentvlan:8585/tfs/vanrise.collection]$/;C56737

---
+++ TOneV2/Code/TOneV2/SQL.TOneConfiguration/sec/Stored Procedures/sp_View_UpdateAudiences.sql
@@ -0,0 +1,17 @@
+﻿-- =============================================
+-- Author:		<Author,,Name>
+-- Create date: <Create Date,,>
+-- Description:	<Description,,>
+-- =============================================
+create PROCEDURE [sec].[sp_View_UpdateAudiences]
+	@pageID uniqueidentifier ,	
+	@Audience NVARCHAR(255)
+	
+AS
+BEGIN
+	
+UPDATE	sec.[View]
+	SET Audience=@Audience,
+		LastModifiedTime = GETDATE()
+	WHERE	Id = @pageID
+END
