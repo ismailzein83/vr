95b284023497b852904d4a63a8699c26e4e7af7e
Ismail Zein <VANRISE\ismail.zein>
2019-06-27 14:05:36 +0000

    added Visualization functionality to ForEach
    git-tfs-id: [http://developmentvlan:8585/tfs/vanrise.collection]$/;C56770

--- BusinessProcess/Code/Vanrise.BusinessProcess.MainExtensions/VRWorkflowActivities/VRWorkflowForEachActivity.cs
+++ BusinessProcess/Code/Vanrise.BusinessProcess.MainExtensions/VRWorkflowActivities/VRWorkflowForEachActivity.cs
@@ -67,6 +67,54 @@ namespace Vanrise.BusinessProcess.MainExtensions.VRWorkflowActivities
                             }
                         }
                     }
+
+                    public class #CLASSNAME#_InsertNewIterationVisualEventActivity : BaseCodeActivity
+                    {
+                        protected override void VRExecute(IBaseCodeActivityContext context)
+                        {
+                            var executionContext = new #CLASSNAME#_InsertNewIterationVisualEventActivity_ExecutionContext(context.ActivityContext);
+                            executionContext.Execute();
+                        }
+                    }
+
+                    public class #CLASSNAME#_InsertNewIterationVisualEventActivity_ExecutionContext : #BASEEXECUTIONCLASSNAME#
+                    {
+                        ActivityContext _activityContext;
+                        public #CLASSNAME#_InsertNewIterationVisualEventActivity_ExecutionContext(ActivityContext activityContext) 
+                            : base (activityContext)
+                        {
+                            _activityContext = activityContext;
+                        }
+
+                        public void Execute()
+                        {
+                            #INSERTNEWITERATIONVISUALEVENTCODE#
+                        }
+                    }
+
+                    public class #CLASSNAME#_InsertCompletedVisualEventActivity : BaseCodeActivity
+                    {
+                        protected override void VRExecute(IBaseCodeActivityContext context)
+                        {
+                            var executionContext = new #CLASSNAME#_InsertCompletedVisualEventActivity_ExecutionContext(context.ActivityContext);
+                            executionContext.Execute();
+                        }
+                    }
+
+                    public class #CLASSNAME#_InsertCompletedVisualEventActivity_ExecutionContext : #BASEEXECUTIONCLASSNAME#
+                    {
+                        ActivityContext _activityContext;
+                        public #CLASSNAME#_InsertCompletedVisualEventActivity_ExecutionContext(ActivityContext activityContext) 
+                            : base (activityContext)
+                        {
+                            _activityContext = activityContext;
+                        }
+
+                        public void Execute()
+                        {
+                            #INSERTCOMPLETEDVISUALEVENTCODE#
+                        }
+                    }
                 }");
 
             String iterationVariableRuntimeType = CSharpCompiler.TypeToString(this.IterationVariableType.GetRuntimeType(null));
@@ -82,32 +130,107 @@ namespace Vanrise.BusinessProcess.MainExtensions.VRWorkflowActivities
             nmSpaceCodeBuilder.Replace("#LISTNAME#", this.List.GetCode(null));
             nmSpaceCodeBuilder.Replace("#ITERATIONVARIABLERUNTIMETYPE#", iterationVariableRuntimeType);
 
+            var insertNewIterationVisualEventInput = new GenerateInsertVisualEventCodeInput
+            {
+                ActivityContextVariableName = "_activityContext",
+                ActivityId = context.VRWorkflowActivityId,
+                EventTitle = @"""ForEach Activity New Iteration""",
+                EventTypeId = CodeGenerationHelper.VISUALEVENTTYPE_NEWITERATION
+            };
+            nmSpaceCodeBuilder.Replace("#INSERTNEWITERATIONVISUALEVENTCODE#",
+                context.GenerateInsertVisualEventCode(insertNewIterationVisualEventInput));
+
+            var insertCompletedVisualEventInput = new GenerateInsertVisualEventCodeInput
+            {
+                ActivityContextVariableName = "_activityContext",
+                ActivityId = context.VRWorkflowActivityId,
+                EventTitle = $@"""ForEach Activity completed""",
+                EventTypeId = CodeGenerationHelper.VISUALEVENTTYPE_COMPLETED
+            };
+            nmSpaceCodeBuilder.Replace("#INSERTCOMPLETEDVISUALEVENTCODE#",
+                context.GenerateInsertVisualEventCode(insertCompletedVisualEventInput));
+
 
             context.AddFullNamespaceCode(nmSpaceCodeBuilder.ToString());
 
             StringBuilder codeBuilder = new StringBuilder();
 
             codeBuilder.Append(@"
-            new System.Activities.Statements.ForEach<#ITERATIONVARIABLERUNTIMETYPE#>
-            {
-                Values =  new System.Activities.InArgument<IEnumerable<#ITERATIONVARIABLERUNTIMETYPE#>>((activityContext) => new #NAMESPACE#.#CLASSNAME#(activityContext).List),
-                Body = new System.Activities.ActivityAction<#ITERATIONVARIABLERUNTIMETYPE#>
+                    new Sequence
+                        {
+                            Activities =
+                            {
+                                new System.Activities.Statements.ForEach<#ITERATIONVARIABLERUNTIMETYPE#>
                                 {
-                                     Argument = new System.Activities.DelegateInArgument<#ITERATIONVARIABLERUNTIMETYPE#>(""#ITERATIONVARIABLENAME#""),
-                                     Handler = #ACTIVITYCODE#                      
-                                }
-            }");
+                                    Values =  new System.Activities.InArgument<IEnumerable<#ITERATIONVARIABLERUNTIMETYPE#>>((activityContext) => new #NAMESPACE#.#CLASSNAME#(activityContext).List),
+                                    Body = new System.Activities.ActivityAction<#ITERATIONVARIABLERUNTIMETYPE#>
+                                                    {
+                                                         Argument = new System.Activities.DelegateInArgument<#ITERATIONVARIABLERUNTIMETYPE#>(""#ITERATIONVARIABLENAME#""),
+                                                         Handler = new Sequence
+                                                                {
+                                                                    Activities =
+                                                                    {
+                                                                        new #NAMESPACE#.#CLASSNAME#_InsertNewIterationVisualEventActivity(),
+                                                                        #ACTIVITYCODE#
+                                                                    }
+                                                                }                    
+                                                    }
+                                },
+                                new #NAMESPACE#.#CLASSNAME#_InsertCompletedVisualEventActivity()
+                            }
+                        }
+
+            ");
 
             codeBuilder.Replace("#NAMESPACE#", nmSpaceName);
             codeBuilder.Replace("#CLASSNAME#", className);
             codeBuilder.Replace("#ITERATIONVARIABLERUNTIMETYPE#", iterationVariableRuntimeType);
             codeBuilder.Replace("#ITERATIONVARIABLENAME#", this.IterationVariableName);
 
-            var newContext = childContext.CreateChildContext(null);
+            var newContext = childContext.CreateChildContext(context.GenerateInsertVisualEventCode);
             newContext.VRWorkflowActivityId = this.Activity.VRWorkflowActivityId;
 
             codeBuilder.Replace("#ACTIVITYCODE#", this.Activity.Settings.GenerateWFActivityCode(newContext));
             return codeBuilder.ToString();
         }
+
+        public override BPVisualItemDefinition GetVisualItemDefinition(IVRWorkflowActivityGetVisualItemDefinitionContext context)
+        {
+            if (this.Activity != null && this.Activity.Settings != null)
+            {
+                var childActivityVisualItem = this.Activity.Settings.GetVisualItemDefinition(context);
+                if (childActivityVisualItem != null)
+                {
+                    return new BPVisualItemDefinition
+                    {
+                        Settings = new BPForEachVisualItemDefinition
+                        {
+                            ChildActivityId = this.Activity.VRWorkflowActivityId,
+                            ChildActivityVisualItemDefinition = childActivityVisualItem
+                        }
+                    };
+                }
+                else
+                {
+                    return null;
+                }
+            }
+            else
+            {
+                return null;
+            }
+        }
+    }
+
+
+    public class BPForEachVisualItemDefinition : BPVisualItemDefinitionSettings
+    {
+        public override Guid ConfigId { get { return new Guid("8D691741-D0B6-4CF9-8A33-998E0DCC0B0D"); } }
+
+        public override string Editor { get { return "bp-workflow-activitysettings-visualitemdefiniton-foreach"; } }
+        
+        public Guid ChildActivityId { get; set; }
+
+        public BPVisualItemDefinition ChildActivityVisualItemDefinition { get; set; }
     }
 }
