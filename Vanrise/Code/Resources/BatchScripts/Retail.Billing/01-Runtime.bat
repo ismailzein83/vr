b89c2d581a75caa345f7f199440d710365e04dbb
Wissam Ajamy <VANRISE\wissam.ajamy>
2019-06-28 06:26:50 +0000

    Adding Reports to Runtime
    git-tfs-id: [http://developmentvlan:8585/tfs/vanrise.collection]$/;C56781

--- Vanrise/Code/Resources/BatchScripts/Retail.Billing/01-Runtime.bat
+++ Vanrise/Code/Resources/BatchScripts/Retail.Billing/01-Runtime.bat
@@ -8,10 +8,11 @@ For /F %PARSEARG% %%i in (%CURRDATE%) Do SET YYYYMMDD=%%l%%j%%k
 
 ECHO.
 ECHO Retail.Billing Runtime
-xcopy "C:\TFS\Retail\Code\Retail.Runtime\bin\x64\Release" /y /v /z /i /Q /R							"\\192.168.110.185\Fixes\WebSite\Retail.Billing\Runtime\%YYYYMMDD%\" /EXCLUDE:C:\TFS\Vanrise\Code\Resources\BatchScripts\Common\RetailSource\runtime-list-of-excluded-files.txt
-xcopy "C:\TFS\Retail\Code\Retail.Runtime\App.config.Retail.Billing.exclude" /y /v /z /i /Q /R		"\\192.168.110.185\Fixes\WebSite\Retail.Billing\Runtime\%YYYYMMDD%\"
-rename																								"\\192.168.110.185\Fixes\WebSite\Retail.Billing\Runtime\%YYYYMMDD%\App.config.Retail.Billing.exclude"	"Retail.Billing.Runtime.exe.config"
-rename																								"\\192.168.110.185\Fixes\WebSite\Retail.Billing\Runtime\%YYYYMMDD%\Retail.Runtime.exe"					"Retail.Billing.Runtime.exe"
-xcopy "C:\TFS\Vanrise\Code\Resources\DLLs\Aspose.Cells.lic" /y /v /z /i /Q /R						"\\192.168.110.185\Fixes\WebSite\Retail.Billing\Runtime\%YYYYMMDD%\"
-xcopy "C:\TFS\Retail\Code\Retail.Runtime\bin\x64\Release\Retail.Billing*" /S /E /R /y /v /i /z /Q	"\\192.168.110.185\Fixes\WebSite\Retail.Billing\Runtime\%YYYYMMDD%\"
-del /s /q /f																						"\\192.168.110.185\Fixes\WebSite\Retail.Billing\Runtime\%YYYYMMDD%\*.pdb"
+xcopy "C:\TFS\Retail\Code\Retail.Runtime\bin\x64\Release" /y /v /z /i /Q /R										"\\192.168.110.185\Fixes\WebSite\Retail.Billing\Runtime\%YYYYMMDD%\" /EXCLUDE:C:\TFS\Vanrise\Code\Resources\BatchScripts\Common\RetailSource\runtime-list-of-excluded-files.txt
+xcopy "C:\TFS\Retail\Code\Retail.Runtime\App.config.Retail.Billing.exclude" /y /v /z /i /Q /R					"\\192.168.110.185\Fixes\WebSite\Retail.Billing\Runtime\%YYYYMMDD%\"
+rename																											"\\192.168.110.185\Fixes\WebSite\Retail.Billing\Runtime\%YYYYMMDD%\App.config.Retail.Billing.exclude"	"Retail.Billing.Runtime.exe.config"
+rename																											"\\192.168.110.185\Fixes\WebSite\Retail.Billing\Runtime\%YYYYMMDD%\Retail.Runtime.exe"					"Retail.Billing.Runtime.exe"
+xcopy "C:\TFS\Vanrise\Code\Resources\DLLs\Aspose.Cells.lic" /y /v /z /i /Q /R									"\\192.168.110.185\Fixes\WebSite\Retail.Billing\Runtime\%YYYYMMDD%\"
+xcopy "C:\TFS\Retail\Code\Retail.Runtime\bin\x64\Release\Retail.Billing*" /S /E /R /y /v /i /z /Q				"\\192.168.110.185\Fixes\WebSite\Retail.Billing\Runtime\%YYYYMMDD%\"
+xcopy "C:\TFS\Retail\Code\Retail.Billing.Web\Retail_Billing\Elements\Reports\*.rdlc" /S /E /R /y /v /i /z /Q	"\\192.168.110.185\Fixes\WebSite\Retail.MultiNet\Runtime\%YYYYMMDD%\Modules\Retail_Billing\Elements\Reports"
+del /s /q /f																									"\\192.168.110.185\Fixes\WebSite\Retail.Billing\Runtime\%YYYYMMDD%\*.pdb"
