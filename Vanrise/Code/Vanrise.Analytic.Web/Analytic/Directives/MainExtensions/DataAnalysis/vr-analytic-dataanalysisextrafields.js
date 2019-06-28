672d607e355198b15749eb8aac1e9a2190b1a845
Fatima Al Heck <VANRISE\fatima.alheck>
2019-06-28 06:33:52 +0000

    adding selectionChangedPromiseDeferred for dependency between selectors
    git-tfs-id: [http://developmentvlan:8585/tfs/vanrise.collection]$/;C56782

--- Vanrise/Code/Vanrise.Analytic.Web/Analytic/Directives/MainExtensions/DataAnalysis/vr-analytic-dataanalysisextrafields.js
+++ Vanrise/Code/Vanrise.Analytic.Web/Analytic/Directives/MainExtensions/DataAnalysis/vr-analytic-dataanalysisextrafields.js
@@ -1,123 +1,118 @@
 ﻿'use strict';
 
 app.directive('vrAnalyticDataanalysisextrafields', ['UtilsService', 'VRUIUtilsService',
-function (UtilsService, VRUIUtilsService) {
+    function (UtilsService, VRUIUtilsService) {
 
-    var directiveDefinitionObject = {
-        restrict: 'E',
-        scope: {
-            onReady: '=',
-            normalColNum: '@'
-        },
-        controller: function ($scope, $element, $attrs) {
+        var directiveDefinitionObject = {
+            restrict: 'E',
+            scope: {
+                onReady: '=',
+                normalColNum: '@'
+            },
+            controller: function ($scope, $element, $attrs) {
 
-            var ctrl = this;
-            var ctor = new extraFieldCtor(ctrl, $scope, $attrs);
-            ctor.initializeController();
-        },
-        controllerAs: 'ctrl',
-        bindToController: true,
-        compile: function (element, attrs) {
+                var ctrl = this;
+                var ctor = new extraFieldCtor(ctrl, $scope, $attrs);
+                ctor.initializeController();
+            },
+            controllerAs: 'ctrl',
+            bindToController: true,
+            compile: function (element, attrs) {
 
-        },
-        templateUrl: '/Client/Modules/Analytic/Directives/MainExtensions/DataAnalysis/Templates/DataAnalysisExtraFields.html'
-    };
+            },
+            templateUrl: '/Client/Modules/Analytic/Directives/MainExtensions/DataAnalysis/Templates/DataAnalysisExtraFields.html'
+        };
 
 
-    function extraFieldCtor(ctrl, $scope, $attrs) {
-        this.initializeController = initializeController;
-        $scope.scopeModel = {};
+        function extraFieldCtor(ctrl, $scope, $attrs) {
+            this.initializeController = initializeController;
+            $scope.scopeModel = {};
 
-        var dataAnalysisDefinitionSelectorReadyPromiseDeferred = UtilsService.createPromiseDeferred();
-        var dataAnalysisDefinitionSelectorDirectiveApi;
+            var dataAnalysisDefinitionSelectorReadyPromiseDeferred = UtilsService.createPromiseDeferred();
+            var dataAnalysisDefinitionSelectionChangedPromiseDeferred;
+            var dataAnalysisDefinitionSelectorDirectiveApi;
 
-        var dataAnalysisDefinitionItemSelectorReadyPromiseDeferred;
-        var dataAnalysisDefinitionItemSelectorDirectiveApi;
+            var dataAnalysisDefinitionItemSelectorReadyPromiseDeferred = UtilsService.createPromiseDeferred();
+            var dataAnalysisDefinitionItemSelectorDirectiveApi;
 
-        function initializeController() {
+            function initializeController() {
 
-            $scope.scopeModel.onDataAnalysisDefinitionSelectorReady = function (api) {
-                dataAnalysisDefinitionSelectorDirectiveApi = api;
-                dataAnalysisDefinitionSelectorReadyPromiseDeferred.resolve();
-            };
-
-            $scope.scopeModel.onDataAnalysisItemDefinitionSelectorReady = function (api) {
-                dataAnalysisDefinitionItemSelectorDirectiveApi = api;
-                var setLoader = function (value) {
-                    $scope.scopeModel.isDirectiveLoading = value;
+                $scope.scopeModel.onDataAnalysisDefinitionSelectorReady = function (api) {
+                    dataAnalysisDefinitionSelectorDirectiveApi = api;
+                    dataAnalysisDefinitionSelectorReadyPromiseDeferred.resolve();
                 };
-                var daPayload = { filter: { Filters: [] }, dataAnalysisDefinitionId: dataAnalysisDefinitionSelectorDirectiveApi.getSelectedIds() };
-                daPayload.filter.Filters.push({ $type: "Vanrise.Analytic.Entities.DAProfCalcDataAnalysisItemDefinitionFilter, Vanrise.Analytic.Entities" });
 
-                VRUIUtilsService.callDirectiveLoadOrResolvePromise($scope, dataAnalysisDefinitionItemSelectorDirectiveApi, daPayload, setLoader, dataAnalysisDefinitionItemSelectorReadyPromiseDeferred);
-            };
+                $scope.scopeModel.onDataAnalysisItemDefinitionSelectorReady = function (api) {
+                    dataAnalysisDefinitionItemSelectorDirectiveApi = api;
+                    dataAnalysisDefinitionItemSelectorReadyPromiseDeferred.resolve();
+                };
 
-            $scope.scopeModel.onDataAnalysisDefinitionSelectionChanged = function () {
-                if (dataAnalysisDefinitionItemSelectorDirectiveApi != undefined) {
-                    var setLoader = function (value) {
-                        $scope.scopeModel.isDirectiveLoading = value;
-                    };
-                    var daPayload = { filter: { Filters: [] }, dataAnalysisDefinitionId: dataAnalysisDefinitionSelectorDirectiveApi.getSelectedIds() };
-                    daPayload.filter.Filters.push({ $type: "Vanrise.Analytic.Entities.DAProfCalcDataAnalysisItemDefinitionFilter, Vanrise.Analytic.Entities" });
+                $scope.scopeModel.onDataAnalysisDefinitionSelectionChanged = function () {                  
+                    if (dataAnalysisDefinitionSelectorDirectiveApi.getSelectedIds() !=undefined) {
+                        var setLoader = function (value) {
+                            $scope.scopeModel.isDirectiveLoading = value;
+                        };
+                        var daPayload = { filter: { Filters: [] }, dataAnalysisDefinitionId: dataAnalysisDefinitionSelectorDirectiveApi.getSelectedIds() };
+                        daPayload.filter.Filters.push({ $type: "Vanrise.Analytic.Entities.DAProfCalcDataAnalysisItemDefinitionFilter, Vanrise.Analytic.Entities" });
 
-                    VRUIUtilsService.callDirectiveLoadOrResolvePromise($scope, dataAnalysisDefinitionItemSelectorDirectiveApi, daPayload, setLoader, dataAnalysisDefinitionItemSelectorReadyPromiseDeferred);
-                }
-            };
-            defineAPI();
-        }
-        function defineAPI() {
-            var api = {};
+                        VRUIUtilsService.callDirectiveLoadOrResolvePromise($scope, dataAnalysisDefinitionItemSelectorDirectiveApi, daPayload, setLoader, dataAnalysisDefinitionSelectionChangedPromiseDeferred);
+                    }
+                };
 
-            api.load = function (payload) {
-                var promises = [];
+                defineAPI();
+            }
+            function defineAPI() {
+                var api = {};
 
-                var loadDataAnalysisDefinitionPromiseDeferred = UtilsService.createPromiseDeferred();
+                api.load = function (payload) {
+                    var promises = [];
 
-                dataAnalysisDefinitionSelectorReadyPromiseDeferred.promise.then(function () {
-                    var dataAnalysisDefinitionPayload = { filter: { Filters: [] } };
-                    dataAnalysisDefinitionPayload.filter.Filters.push({ $type: "Vanrise.Analytic.Entities.DAProfCalcDataAnalysisDefinitionFilter, Vanrise.Analytic.Entities" });
+                    var loadDataAnalysisDefinitionPromiseDeferred = UtilsService.createPromiseDeferred();
+
+                    dataAnalysisDefinitionSelectorReadyPromiseDeferred.promise.then(function () {
+                        var dataAnalysisDefinitionPayload = { filter: { Filters: [] } };
+                        dataAnalysisDefinitionPayload.filter.Filters.push({ $type: "Vanrise.Analytic.Entities.DAProfCalcDataAnalysisDefinitionFilter, Vanrise.Analytic.Entities" });
+
+                        if (payload != undefined) {
+
+                            dataAnalysisDefinitionPayload.selectedIds = payload.DataAnalysisDefinitionId;
+                        }
+                        VRUIUtilsService.callDirectiveLoad(dataAnalysisDefinitionSelectorDirectiveApi, dataAnalysisDefinitionPayload, loadDataAnalysisDefinitionPromiseDeferred);
+                    });
+                    promises.push(loadDataAnalysisDefinitionPromiseDeferred.promise);
 
                     if (payload != undefined) {
+                        dataAnalysisDefinitionSelectionChangedPromiseDeferred = UtilsService.createPromiseDeferred();
 
-                        dataAnalysisDefinitionPayload.selectedIds = payload.DataAnalysisDefinitionId;
+                        var dataAnalysisDefinitionItemSelectorLoadDeferred = UtilsService.createPromiseDeferred();
+                        UtilsService.waitMultiplePromises([dataAnalysisDefinitionItemSelectorReadyPromiseDeferred.promise, dataAnalysisDefinitionSelectionChangedPromiseDeferred.promise]).then(function () {
+                            dataAnalysisDefinitionSelectionChangedPromiseDeferred = undefined;
+
+                            var dataAnalysisDefinitionItemPayload = { filter: { Filters: [] }, selectedIds: payload.DataAnalysisItemDefinitionId, dataAnalysisDefinitionId: payload.DataAnalysisDefinitionId };
+                            dataAnalysisDefinitionItemPayload.filter.Filters.push({ $type: "Vanrise.Analytic.Entities.DAProfCalcDataAnalysisItemDefinitionFilter, Vanrise.Analytic.Entities" });
+
+                            VRUIUtilsService.callDirectiveLoad(dataAnalysisDefinitionItemSelectorDirectiveApi, dataAnalysisDefinitionItemPayload, dataAnalysisDefinitionItemSelectorLoadDeferred);
+                        });
+
+                        promises.push(dataAnalysisDefinitionItemSelectorLoadDeferred.promise);
                     }
-                    VRUIUtilsService.callDirectiveLoad(dataAnalysisDefinitionSelectorDirectiveApi, dataAnalysisDefinitionPayload, loadDataAnalysisDefinitionPromiseDeferred);
-                });
-                promises.push(loadDataAnalysisDefinitionPromiseDeferred.promise);
 
-                if (payload != undefined) {
-                    dataAnalysisDefinitionItemSelectorReadyPromiseDeferred = UtilsService.createPromiseDeferred();
-
-                    var dataAnalysisDefinitionItemSelectorLoadDeferred = UtilsService.createPromiseDeferred();
-
-                    dataAnalysisDefinitionItemSelectorReadyPromiseDeferred.promise.then(function () {
-                        dataAnalysisDefinitionItemSelectorReadyPromiseDeferred = undefined;
-
-                        var dataAnalysisDefinitionItemPayload = { filter: { Filters: [] }, selectedIds: payload.DataAnalysisItemDefinitionId, dataAnalysisDefinitionId: payload.DataAnalysisDefinitionId };
-                        dataAnalysisDefinitionItemPayload.filter.Filters.push({ $type: "Vanrise.Analytic.Entities.DAProfCalcDataAnalysisItemDefinitionFilter, Vanrise.Analytic.Entities" });
-
-                        VRUIUtilsService.callDirectiveLoad(dataAnalysisDefinitionItemSelectorDirectiveApi, dataAnalysisDefinitionItemPayload, dataAnalysisDefinitionItemSelectorLoadDeferred);
-                    });
-
-                    promises.push(dataAnalysisDefinitionItemSelectorLoadDeferred.promise);
-                }
-
-                return UtilsService.waitMultiplePromises(promises);
-            };
-
-
-            api.getData = function () {
-                return {
-                    $type: 'Vanrise.Analytic.Business.DAProfCalcRecordTypeExtraFields,Vanrise.Analytic.Business',
-                    DataAnalysisItemDefinitionId: dataAnalysisDefinitionItemSelectorDirectiveApi.getSelectedIds(),
-                    DataAnalysisDefinitionId: dataAnalysisDefinitionSelectorDirectiveApi.getSelectedIds(),
+                    return UtilsService.waitMultiplePromises(promises);
                 };
+
+
+                api.getData = function () {
+                    return {
+                        $type: 'Vanrise.Analytic.Business.DAProfCalcRecordTypeExtraFields,Vanrise.Analytic.Business',
+                        DataAnalysisItemDefinitionId: dataAnalysisDefinitionItemSelectorDirectiveApi.getSelectedIds(),
+                        DataAnalysisDefinitionId: dataAnalysisDefinitionSelectorDirectiveApi.getSelectedIds()
+                    };
+                };
+
+                if (ctrl.onReady != null)
+                    ctrl.onReady(api);
             };
+        }
 
-            if (ctrl.onReady != null)
-                ctrl.onReady(api);
-        };
-    }
-
-    return directiveDefinitionObject;
-}]);
+        return directiveDefinitionObject;
+    }]);
