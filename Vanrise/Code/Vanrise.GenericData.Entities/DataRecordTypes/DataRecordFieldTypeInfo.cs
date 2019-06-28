7c2fba14e4740b0c980342b6b95bb6490aceb856
Mohammad Sheaib <VANRISE\mohammad.sheaib>
2019-06-28 12:04:42 +0000

    Commented
    git-tfs-id: [http://developmentvlan:8585/tfs/vanrise.collection]$/;C56797

---
+++ Vanrise/Code/Vanrise.GenericData.Entities/DataRecordTypes/DataRecordFieldTypeInfo.cs
@@ -0,0 +1,14 @@
+﻿//using System;
+//using System.Collections.Generic;
+//using System.Linq;
+//using System.Text;
+//using System.Threading.Tasks;
+
+//namespace Vanrise.GenericData.Entities
+//{
+//    public class DataRecordFieldTypeInfo
+//    {
+//        public string FieldDescription { get; set; }
+//        public Guid StyleDefinitionId { get; set; }
+//    }
+//}
