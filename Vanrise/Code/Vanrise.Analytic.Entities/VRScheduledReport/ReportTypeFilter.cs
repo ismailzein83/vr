7aa300a7eaf8882f0cfaf4d53cd272cb3571a471
Fatima Hammoud <VANRISE\fatima.hammoud>
2019-07-01 06:38:11 +0000

    Adding two classes "ReportTypeInfo" and  "ReportTypeFilter"
    git-tfs-id: [http://developmentvlan:8585/tfs/vanrise.collection]$/;C56812

---
+++ Vanrise/Code/Vanrise.Analytic.Entities/VRScheduledReport/ReportTypeFilter.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Vanrise.Common;
+using Vanrise.Entities;
+
+namespace Vanrise.Analytic.Entities
+{
+    public class ReportTypeFilter : IComponentTypeFilter
+    {
+        public bool IsMatched(IComponentTypeFilterContext context)
+        {
+            context.ThrowIfNull("context", context);
+            var componentType = context.componentType;
+
+            componentType.ThrowIfNull("ComponentType", componentType.VRComponentTypeId);
+
+            if (componentType.Settings is VRReportTypeDefinitionSettings)
+                return true;
+
+            else
+                return false;
+        }
+    }
+}
---
+++ Vanrise/Code/Vanrise.Analytic.Entities/VRScheduledReport/ReportTypeInfo.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Vanrise.Analytic.Entities
+{
+    public class ReportTypeInfo
+    {
+        public Guid VRScheduledReportId { get; set; }
+        public string Name { get; set; }
+    }
+}
