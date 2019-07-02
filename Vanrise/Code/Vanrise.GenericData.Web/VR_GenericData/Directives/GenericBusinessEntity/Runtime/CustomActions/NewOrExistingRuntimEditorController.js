0fd1467f587eea7764adfac063fa5cce1b7e71f2
Zeinab Mokdad <VANRISE\zeinab.mokdad>
2019-07-02 14:15:02 +0000

    fixing newOrExisting custom editor
    git-tfs-id: [http://developmentvlan:8585/tfs/vanrise.collection]$/;C56860

--- Vanrise/Code/Vanrise.GenericData.Web/VR_GenericData/Directives/GenericBusinessEntity/Runtime/CustomActions/NewOrExistingRuntimEditorController.js
+++ Vanrise/Code/Vanrise.GenericData.Web/VR_GenericData/Directives/GenericBusinessEntity/Runtime/CustomActions/NewOrExistingRuntimEditorController.js
@@ -17,7 +17,6 @@
         function loadParameters() {
             var parameters = VRNavigationService.getParameters($scope);
             if (parameters != undefined && parameters != null) {
-                // dataRecordTypeId = parameters.dataRecordTypeId;
                 parentFieldValues = parameters.parentFieldValues;
                 customAction = parameters.customAction;
                 businessEntityDefinitionId = parameters.businessEntityDefinitionId;
@@ -25,81 +24,101 @@
         }
 
         function defineScope() {
-            $scope.scopeModel.hasBEentity = function () {
+            $scope.scopeModel.getBEentity = function () {
                 $scope.scopeModel.isLoading = true;
-                var filters = [];
-                var firstNameFilter =
-                {
-                    FieldName: "FirstName",
-                    FilterValues: [$scope.firstName]
-                };
-                filters.push(firstNameFilter);
-                var lastNameFilter =
-                {
-                    FieldName: "LastName",
-                    FilterValues: [$scope.lastName]
-                };
-                filters.push(lastNameFilter);
-                var middleNameFilter =
-                {
-                    FieldName: "MiddleName",
-                    FilterValues: [$scope.middleName]
-                };
-                filters.push(middleNameFilter);
-                var filter =
-                {
-                    FieldFilters: filters
-                };
-                VR_GenericData_GenericBusinessEntityAPIService.GetGenericBusinessEntityInfo(businessEntityDefinitionId, UtilsService.serializetoJson(filter))
+
+                var filter = buildFilter();
+                $scope.modalContext.closeModal();
+                var promiseDeferred = UtilsService.createPromiseDeferred();
+
+                 VR_GenericData_GenericBusinessEntityAPIService.GetGenericBusinessEntityInfo(businessEntityDefinitionId, UtilsService.serializetoJson(filter))
                     .then(function (response) {
                         if (response != undefined && response.length != 0) {
-                            console.log(response);
+                            var firstEntityId = response[0].GenericBusinessEntityId;
+                            var idFieldValue =
+                            {
+                                ID: firstEntityId
+                            };
+                            parentFieldValues.trigerSearch(idFieldValue).then(function () {
+                                parentFieldValues.expendRow(firstEntityId);
+                                promiseDeferred.resolve();
+                            }).catch(function (error) {
+                                VRNotificationService.notifyExceptionWithClose(error, $scope);
+                                promiseDeferred.reject();
+                            }).finally(function () {
+                                $scope.scopeModel.isLoading = false;
+                            });
                         }
                         else {
-                            var onGenericBusinessEntityAdded = function () { };
-                            VR_GenericData_GenericBusinessEntityService.addGenericBusinessEntity(onGenericBusinessEntityAdded, businessEntityDefinitionId, fieldValues);
+                            var fieldValues = buildFieldValues();
+                            promiseDeferred.resolve();
+                            //var onGenericBusinessEntityAdded = function () { };
+                            VR_GenericData_GenericBusinessEntityService.addGenericBusinessEntity(undefined, businessEntityDefinitionId, "large", fieldValues);
                         }
                     }).catch(function (error) {
-                        VRNotificationService.notifyExceptionWithClose(error, $scope);
-                    }).finally(function () {
                         $scope.scopeModel.isLoading = false;
-                        $scope.modalContext.closeModal();
+                        VRNotificationService.notifyExceptionWithClose(error, $scope);
+                        promiseDeferred.reject();
                     });
-                //VR_GenericData_GenericBusinessEntityAPIService.GetGenericBEEntityId(buildEntityScope()).then(function (response) {
-                //    if (response == undefined) {
-                //        var fieldValues =
-                //        {
-                //            FirstName: $scope.firstName,
-                //            middleName: $scope.middleName,
-                //            lastName: $scope.lastName
-                //        };
-                //        var onGenericBusinessEntityAdded = function () { };
-                //        VR_GenericData_GenericBusinessEntityService.addGenericBusinessEntity(onGenericBusinessEntityAdded, businessEntityDefinitionId, fieldValues);
-                //    }
-                //}).catch(function (error) {
-                //    VRNotificationService.notifyExceptionWithClose(error, $scope);
-                //}).finally(function () {
-                //    $scope.scopeModel.isLoading = false;
-                //    $scope.modalContext.closeModal();
-                //});
+                return promiseDeferred;
             };
             $scope.scopeModel.close = function () {
                 $scope.modalContext.closeModal();
             };
-
         }
 
         function load() {
             $scope.title = "Add";
         }
-
-        function buildEntityScope() {
-            return {
-                BusinessEntityDefinitionId: businessEntityDefinitionId,
-                FirstName: $scope.firstName,
-                middleName: $scope.middleName,
-                lastName: $scope.lastName
+        function buildFieldValues() {
+            var fieldValues =
+            {
+                FirstName: {
+                    value: $scope.firstName
+                },
+                MiddleName: {
+                    value: $scope.middleName
+                },
+                LastName: {
+                    value: $scope.lastName
+                },
+                MotherName: {
+                    value: $scope.motherName
+                },
+                BirthPlace: {
+                    value: $scope.birthPlace
+                },
+                BirthDate: {
+                    value: $scope.birthDate
+                }
             };
+            return fieldValues;
+        }
+        function buildFilter() {
+            var filters = [];
+            var firstNameFilter =
+            {
+                FieldName: "FirstName",
+                FilterValues: [$scope.firstName]
+            };
+            filters.push(firstNameFilter);
+            var lastNameFilter =
+            {
+                FieldName: "LastName",
+                FilterValues: [$scope.lastName]
+            };
+            filters.push(lastNameFilter);
+            var middleNameFilter =
+            {
+                FieldName: "MiddleName",
+                FilterValues: [$scope.middleName]
+            };
+            filters.push(middleNameFilter);
+            var filter =
+            {
+                FieldFilters: filters
+            };
+            return filter;
         }
     }
     appControllers.controller('VR_GenericData_NewOrExistingRuntimeEditorController', NewOrExistingRuntimEditorController);
