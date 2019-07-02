e29bdbc9d280ca5a7411d552f2cbc38d0f7a3d86
Zeinab Mokdad <VANRISE\zeinab.mokdad>
2019-07-02 12:02:42 +0000

    adding GenericBENewOrExistingCustomAction
    git-tfs-id: [http://developmentvlan:8585/tfs/vanrise.collection]$/;C56851

---
+++ Vanrise/Code/Vanrise.GenericData.MainExtensions/GenericBusinessEntity/GenericBECustomAction/GenericBENewOrExistingCustomAction.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using Vanrise.GenericData.Business;
+using Vanrise.GenericData.Entities;
+
+namespace Vanrise.GenericData.MainExtensions.GenericBusinessEntity.GenericBECustomAction
+{
+    public class GenericBENewOrExistingCustomAction : GenericBECustomActionSettings
+    {
+        public override Guid ConfigId => new Guid("AE78708F-DFD0-49E7-8B60-909AC4D8ABD2");
+        public override string ActionTypeName => "NewOrExistingCustomAction";
+    }
+}
