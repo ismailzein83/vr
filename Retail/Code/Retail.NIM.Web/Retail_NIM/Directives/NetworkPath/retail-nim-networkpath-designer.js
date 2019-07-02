c2a417be396317853d40e3b65a838762636c8cf3
Ali Ballouk <VANRISE\ali.ballouk>
2019-07-02 15:03:45 +0000

    adding Retail_NIM web path to web.config + (networkpath directive base implementation)
    git-tfs-id: [http://developmentvlan:8585/tfs/vanrise.collection]$/;C56863

---
+++ Retail/Code/Retail.NIM.Web/Retail_NIM/Directives/NetworkPath/retail-nim-networkpath-designer.js
@@ -0,0 +1,72 @@
+﻿"use strict";
+
+app.directive("retailNimNetworkpathDesigner", ["UtilsService", "VRNotificationService", "VRUIUtilsService",
+    function (UtilsService, VRNotificationService, VRUIUtilsService) {
+
+        var directiveDefinitionObject = {
+
+            restrict: "E",
+            scope:
+            {
+                onReady: "=",
+            },
+            controller: function ($scope, $element, $attrs) {
+                var ctrl = this;
+
+                var ctor = new NetworkpathDesigner($scope, ctrl, $attrs);
+                ctor.initializeController();
+            },
+            controllerAs: "ctrl",
+            bindToController: true,
+            templateUrl: "/Client/Modules/Retail_NIM/Directives/NetworkPath/Templates/NetworkPathTemplate.html"
+
+        };
+
+        function NetworkpathDesigner($scope, ctrl, $attrs) {
+            ctrl.networkNodes = [];
+            ctrl.networkConnecters = [];
+            this.initializeController = initializeController;
+            $scope.scopeModel = {};
+                      
+
+            ctrl.getOutConnecterPortNumber = function (nodeId) {
+                var connection = UtilsService.getItemByVal(ctrl.networkConnecters, nodeId, 'SourceNodeId');
+                return connection && connection.SourcePortNumber;
+            };
+
+            ctrl.getInConnecterPortNumber = function (nodeId) {
+                var connection = UtilsService.getItemByVal(ctrl.networkConnecters, nodeId, 'SourceNodeId');
+                return connection && connection.DestinationPortNumber;
+            };
+
+            
+
+            function initializeController() {
+                $scope.scopeModel = {};
+                defineAPI();
+            }
+
+            function defineAPI() {
+                var api = {};
+
+                api.load = function (payload) {
+                    var promises = [];
+                    ctrl.networkNodes.length = 0 ;
+                    ctrl.networkConnecters.length = 0;
+                    if (payload) {
+                        ctrl.networkNodes = payload.networkNodes || [];
+                        ctrl.networkConnecters = payload.networkConnecters || [];
+                    }                   
+                    return UtilsService.waitMultiplePromises(promises);
+
+                };
+               
+                if (ctrl.onReady != null)
+                    ctrl.onReady(api);
+            }
+        }
+
+        return directiveDefinitionObject;
+
+    }
+]);
