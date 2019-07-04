82123d1f68a3ff87c57ded77e2c72092f294996d
Fatima Hammoud <VANRISE\fatima.hammoud>
2019-07-04 11:50:06 +0000

    Adding new class "StartBPProcessCustomAction"
    git-tfs-id: [http://developmentvlan:8585/tfs/vanrise.collection]$/;C56926

---
+++ BusinessProcess/Code/Vanrise.BusinessProcess.MainExtensions/GenericBECustomActionSettings/StartBPProcessCustomAction.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Vanrise.GenericData.Business;
+
+namespace Vanrise.BusinessProcess.MainExtensions
+{
+    public class StartBPProcessCustomAction : GenericBECustomActionSettings
+    {
+        public override Guid ConfigId { get { return new Guid("F13D539A-3688-427A-818B-7B4717BA1D26"); } }
+        public override string ActionTypeName { get { return "StartBPProcessCustomAction"; } }
+        public Guid BPDefinitionId { get; set; }
+        public List<InputArgumentMapping> InputArgumentsMapping { get; set; }
+    }
+}
