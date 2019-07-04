cb39b3d646ca3090bc9c047b9f5b68547db7a81c
Fatima Hammoud <VANRISE\fatima.hammoud>
2019-07-04 12:01:35 +0000

    Adding StartBPProcessCustomAction directive (commented)
    git-tfs-id: [http://developmentvlan:8585/tfs/vanrise.collection]$/;C56928

---
+++ BusinessProcess/Code/Vanrise.BusinessProcess.Web/BusinessProcess/Directives/MainExtensions/GenericBECustomActionSettings/bp-genericbe-startbpprocess-customaction.js
@@ -0,0 +1,34 @@
+﻿//"use strict";
+
+//app.directive("bpGenericbeStartbpprocessCustomaction", ["UtilsService", "VRUIUtilsService", 'BusinessProcess_BPDefinitionAPIService', 'BusinessProcess_VRWorkflowAPIService', 'VRNotificationService', 'VRWorkflowArgumentDirectionEnum',
+//    function (UtilsService, VRUIUtilsService, BusinessProcess_BPDefinitionAPIService, BusinessProcess_VRWorkflowAPIService, VRNotificationService, VRWorkflowArgumentDirectionEnum) {
+
+//        var directiveDefinitionObject = {
+//            restrict: "E",
+//            scope: {
+//                onReady: "="
+//            },
+//            controller: function ($scope, $element, $attrs) {
+//                var ctrl = this;
+//                var ctor = new StartBPProcessCustomActionCtor($scope, ctrl, $attrs);
+//                ctor.initializeController();
+//            },
+//            controllerAs: "ctrl",
+//            bindToController: true,
+//            templateUrl: "/Client/Modules/BusinessProcess/Directives/MainExtensions/GenericBECustomActionSettings/Templates/StartBPProcessCustomActionTemplate.html"
+//        };
+
+//        function StartBPProcessCustomActionCtor($scope, ctrl) {
+//            this.initializeController = initializeController;
+
+//            function initializeController() {
+//                $scope.scopeModel = {};
+          
+//                defineAPI();
+//            }
+
+//            function defineAPI() {
+//            }
+
+//        return directiveDefinitionObject;
+//    }]);
