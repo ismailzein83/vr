2c39eb939e31a9f134c29861babdfefc1bde6bd5
Hassan Makhour <VANRISE\hassan.makhour>
2019-07-04 11:42:02 +0000

    Open RDLC Report Task Type
    git-tfs-id: [http://developmentvlan:8585/tfs/vanrise.collection]$/;C56925

---
+++ BusinessProcess/Code/Vanrise.BusinessProcess.MainExtensions/BPTaskTypes/BPGenericTaskTypeAction/OpenRDLCReportBPGenericTaskTypeAction.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Vanrise.BusinessProcess.Entities;
+
+namespace Vanrise.BusinessProcess.MainExtensions
+{
+	public class OpenRDLCReportBPGenericTaskTypeAction: BPGenericTaskTypeActionSettings
+	{
+		public override string ActionTypeName { get { return "OpenRDLCReport"; } }
+		public override Guid ConfigId { get { return new Guid("0E9FCADE-7E43-48F7-860D-3C563BBBB205"); } }
+	}
+}
