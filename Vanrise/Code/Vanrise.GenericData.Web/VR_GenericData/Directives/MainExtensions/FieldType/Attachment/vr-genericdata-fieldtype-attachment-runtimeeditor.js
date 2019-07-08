da6de24128641f4f79bc6388f6d32674f9915c23
Ali Slayman <VANRISE\ali.slayman>
2019-07-08 09:50:03 +0000

    Adding  api.setFieldValues function to fieldtype-attachment-runtimeeditor
    git-tfs-id: [http://developmentvlan:8585/tfs/vanrise.collection]$/;C57007

--- Vanrise/Code/Vanrise.GenericData.Web/VR_GenericData/Directives/MainExtensions/FieldType/Attachment/vr-genericdata-fieldtype-attachment-runtimeeditor.js
+++ Vanrise/Code/Vanrise.GenericData.Web/VR_GenericData/Directives/MainExtensions/FieldType/Attachment/vr-genericdata-fieldtype-attachment-runtimeeditor.js
@@ -1,5 +1,5 @@
 ﻿'use strict';
-app.directive('vrGenericdataFieldtypeAttachmentRuntimeeditor', ['UtilsService','VRUIUtilsService',
+app.directive('vrGenericdataFieldtypeAttachmentRuntimeeditor', ['UtilsService', 'VRUIUtilsService',
     function (UtilsService, VRUIUtilsService) {
 
         var directiveDefinitionObject = {
@@ -19,14 +19,15 @@ app.directive('vrGenericdataFieldtypeAttachmentRuntimeeditor', ['UtilsService','
             controllerAs: 'ctrl',
             bindToController: true,
             template: function (element, attrs) {
-                return '<vr-columns colnum="12" >'
-                         + '<vr-genericdata-attachmentfieldtype-management on-ready="ctrl.onDirectiveReady" ></vr-genericdata-attachmentfieldtype-management>'
-                     + '</vr-columns>';
+                return '<vr-columns colnum="12" vr-loader="scopeModel.isDirectiveLoading">'
+                    + '<vr-genericdata-attachmentfieldtype-management on-ready="ctrl.onDirectiveReady" ></vr-genericdata-attachmentfieldtype-management>'
+                    + '</vr-columns>';
             }
 
         };
 
         function attachmentTypeCtor(ctrl, $scope) {
+            var fieldName;
 
             var directiveReadyAPI;
             var directiveReadyPromiseDeferred = UtilsService.createPromiseDeferred();
@@ -48,9 +49,14 @@ app.directive('vrGenericdataFieldtypeAttachmentRuntimeeditor', ['UtilsService','
                 api.load = function (payload) {
                     var promises = [];
 
-                    var payloadVariable = {
-                        attachementFieldTypes: payload != undefined ? payload.fieldValue : undefined
-                    };
+                    var payloadVariable;
+                    if (payload != undefined) {
+                        fieldName = payload.fieldName;
+                        payloadVariable = {
+                            attachementFieldTypes: payload.fieldValue
+                        };
+                    }
+
                     promises.push(directiveReadyAPI.loadGrid(payloadVariable));
 
                     return UtilsService.waitMultiplePromises(promises);
@@ -58,6 +64,26 @@ app.directive('vrGenericdataFieldtypeAttachmentRuntimeeditor', ['UtilsService','
 
                 api.getData = function () {
                     return directiveReadyAPI.getData();
+                };
+
+                api.setFieldValues = function (fieldValuesByNames) {
+                    if (fieldValuesByNames == undefined || !(fieldName in fieldValuesByNames))
+                        return;
+
+                    var setFieldValuesDeferred = UtilsService.createPromiseDeferred();
+
+                    var payloadVariable = {
+                        attachementFieldTypes: fieldValuesByNames[fieldName]
+                    };
+
+                    ctrl.isDirectiveLoading = true;
+
+                    directiveReadyAPI.loadGrid(payloadVariable).then(function () {
+                        setFieldValuesDeferred.resolve();
+                        ctrl.isDirectiveLoading = true;
+                    });
+
+                    return setFieldValuesDeferred.promise;
                 };
 
                 if (ctrl.onReady != null)
