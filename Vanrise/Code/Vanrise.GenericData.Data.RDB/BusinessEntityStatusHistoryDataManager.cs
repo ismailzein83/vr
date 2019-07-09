3bf633fa27505b18bf517aad2cb6376e631a59ce
Hassan Makhour <VANRISE\hassan.makhour>
2019-07-09 07:01:47 +0000

    businessentitystatusdefinition rdb
    git-tfs-id: [http://developmentvlan:8585/tfs/vanrise.collection]$/;C57038

--- Vanrise/Code/Vanrise.GenericData.Data.RDB/BusinessEntityStatusHistoryDataManager.cs
+++ Vanrise/Code/Vanrise.GenericData.Data.RDB/BusinessEntityStatusHistoryDataManager.cs
@@ -9,124 +9,128 @@ using Vanrise.Entities;
 
 namespace Vanrise.GenericData.Data.RDB
 {
-    public class BusinessEntityStatusHistoryDataManager : IBusinessEntityStatusHistoryDataManager
-    {
-        #region RDB
+	public class BusinessEntityStatusHistoryDataManager : IBusinessEntityStatusHistoryDataManager
+	{
+		#region RDB
 
-        static string TABLE_NAME = "genericdata_BusinessEntityStatusHistory";
-        static string TABLE_ALIAS = "statusHistory";
-        const string COL_ID = "ID";
-        const string COL_BusinessEntityDefinitionID = "BusinessEntityDefinitionID";
-        const string COL_BusinessEntityID = "BusinessEntityID";
-        const string COL_FieldName = "FieldName";
-        const string COL_StatusID = "StatusID";
-        const string COL_PreviousStatusID = "PreviousStatusID";
-        const string COL_StatusChangedDate = "StatusChangedDate";
-        const string COL_IsDeleted = "IsDeleted";
-        const string COL_CreatedTime = "CreatedTime";
-        const string COL_MoreInfo = "MoreInfo";
-        const string COL_PreviousMoreInfo = "PreviousMoreInfo";
+		static string TABLE_NAME = "genericdata_BusinessEntityStatusHistory";
+		static string TABLE_ALIAS = "statusHistory";
+		const string COL_ID = "ID";
+		const string COL_BusinessEntityDefinitionID = "BusinessEntityDefinitionID";
+		const string COL_BusinessEntityID = "BusinessEntityID";
+		const string COL_FieldName = "FieldName";
+		const string COL_StatusID = "StatusID";
+		const string COL_PreviousStatusID = "PreviousStatusID";
+		const string COL_StatusChangedDate = "StatusChangedDate";
+		const string COL_IsDeleted = "IsDeleted";
+		const string COL_CreatedTime = "CreatedTime";
+		const string COL_MoreInfo = "MoreInfo";
+		const string COL_PreviousMoreInfo = "PreviousMoreInfo";
 
 
-        static BusinessEntityStatusHistoryDataManager()
-        {
-            var columns = new Dictionary<string, RDBTableColumnDefinition>();
-            columns.Add(COL_ID, new RDBTableColumnDefinition { DataType = RDBDataType.BigInt });
-            columns.Add(COL_BusinessEntityDefinitionID, new RDBTableColumnDefinition { DataType = RDBDataType.UniqueIdentifier });
-            columns.Add(COL_BusinessEntityID, new RDBTableColumnDefinition { DataType = RDBDataType.Varchar, Size = 50 });
-            columns.Add(COL_FieldName, new RDBTableColumnDefinition { DataType = RDBDataType.Varchar, Size = 50 });
-            columns.Add(COL_StatusID, new RDBTableColumnDefinition { DataType = RDBDataType.UniqueIdentifier });
-            columns.Add(COL_PreviousStatusID, new RDBTableColumnDefinition { DataType = RDBDataType.UniqueIdentifier });
-            columns.Add(COL_StatusChangedDate, new RDBTableColumnDefinition { DataType = RDBDataType.DateTime });
-            columns.Add(COL_IsDeleted, new RDBTableColumnDefinition { DataType = RDBDataType.Boolean });
-            columns.Add(COL_MoreInfo, new RDBTableColumnDefinition { DataType = RDBDataType.NVarchar });
-            columns.Add(COL_PreviousMoreInfo, new RDBTableColumnDefinition { DataType = RDBDataType.NVarchar });
+		static BusinessEntityStatusHistoryDataManager()
+		{
+			var columns = new Dictionary<string, RDBTableColumnDefinition>();
+			columns.Add(COL_ID, new RDBTableColumnDefinition { DataType = RDBDataType.BigInt });
+			columns.Add(COL_BusinessEntityDefinitionID, new RDBTableColumnDefinition { DataType = RDBDataType.UniqueIdentifier });
+			columns.Add(COL_BusinessEntityID, new RDBTableColumnDefinition { DataType = RDBDataType.Varchar, Size = 50 });
+			columns.Add(COL_FieldName, new RDBTableColumnDefinition { DataType = RDBDataType.Varchar, Size = 50 });
+			columns.Add(COL_StatusID, new RDBTableColumnDefinition { DataType = RDBDataType.UniqueIdentifier });
+			columns.Add(COL_PreviousStatusID, new RDBTableColumnDefinition { DataType = RDBDataType.UniqueIdentifier });
+			columns.Add(COL_StatusChangedDate, new RDBTableColumnDefinition { DataType = RDBDataType.DateTime });
+			columns.Add(COL_IsDeleted, new RDBTableColumnDefinition { DataType = RDBDataType.Boolean });
+			columns.Add(COL_MoreInfo, new RDBTableColumnDefinition { DataType = RDBDataType.NVarchar });
+			columns.Add(COL_PreviousMoreInfo, new RDBTableColumnDefinition { DataType = RDBDataType.NVarchar });
 
-            columns.Add(COL_CreatedTime, new RDBTableColumnDefinition { DataType = RDBDataType.DateTime });
-            RDBSchemaManager.Current.RegisterDefaultTableDefinition(TABLE_NAME, new RDBTableDefinition
-            {
-                DBSchemaName = "genericdata",
-                DBTableName = "BusinessEntityStatusHistory",
-                Columns = columns,
-                IdColumnName = COL_ID,
-                CreatedTimeColumnName = COL_CreatedTime
-            });
-        }
+			columns.Add(COL_CreatedTime, new RDBTableColumnDefinition { DataType = RDBDataType.DateTime });
+			RDBSchemaManager.Current.RegisterDefaultTableDefinition(TABLE_NAME, new RDBTableDefinition
+			{
+				DBSchemaName = "genericdata",
+				DBTableName = "BusinessEntityStatusHistory",
+				Columns = columns,
+				IdColumnName = COL_ID,
+				CreatedTimeColumnName = COL_CreatedTime
+			});
+		}
 
-        #endregion
+		#endregion
 
-        #region Mappers
-        BusinessEntityStatusHistory BusinessEntityStatusHistoryMapper(IRDBDataReader reader)
-        {
-            return new BusinessEntityStatusHistory
-            {
-                StatusId = reader.GetGuid(COL_StatusID),
-                StatusChangedDate = reader.GetDateTime(COL_StatusChangedDate),
-                FieldName = reader.GetString(COL_FieldName),
-                IsDeleted = reader.GetBoolean(COL_IsDeleted),
-                PreviousStatusId = reader.GetNullableGuid(COL_PreviousStatusID),
-                BusinessEntityStatusHistoryId = reader.GetLong(COL_ID),
-                BusinessEntityDefinitionId = reader.GetGuid(COL_BusinessEntityDefinitionID),
-                BusinessEntityId = reader.GetString(COL_BusinessEntityID),
-                MoreInfo = reader.GetString(COL_MoreInfo),
-                PreviousMoreInfo = reader.GetString(COL_PreviousMoreInfo),
+		#region Mappers
+		BusinessEntityStatusHistory BusinessEntityStatusHistoryMapper(IRDBDataReader reader)
+		{
+			return new BusinessEntityStatusHistory
+			{
+				StatusId = reader.GetGuid(COL_StatusID),
+				StatusChangedDate = reader.GetDateTime(COL_StatusChangedDate),
+				FieldName = reader.GetString(COL_FieldName),
+				IsDeleted = reader.GetBoolean(COL_IsDeleted),
+				PreviousStatusId = reader.GetNullableGuid(COL_PreviousStatusID),
+				BusinessEntityStatusHistoryId = reader.GetLong(COL_ID),
+				BusinessEntityDefinitionId = reader.GetGuid(COL_BusinessEntityDefinitionID),
+				BusinessEntityId = reader.GetString(COL_BusinessEntityID),
+				MoreInfo = reader.GetString(COL_MoreInfo),
+				PreviousMoreInfo = reader.GetString(COL_PreviousMoreInfo),
 
-            };
-        }
-        #endregion
+			};
+		}
+		#endregion
 
-        #region Private Methods
-        BaseRDBDataProvider GetDataProvider()
-        {
-            return RDBDataProviderFactory.CreateProvider("VR_GenericData", "ConfigurationDBConnStringKey", "ConfigurationDBConnStringKey");
-        }
-        #endregion
-        public IEnumerable<BusinessEntityStatusHistory> GetFilteredBusinessEntitiesStatusHistory(DataRetrievalInput<BusinessEntityStatusHistoryQuery> input)
-        {
-            var queryContext = new RDBQueryContext(GetDataProvider());
-            var selectQuery = queryContext.AddSelectQuery();
-            selectQuery.From(TABLE_NAME, TABLE_ALIAS, null, true);
-            selectQuery.SelectColumns().AllTableColumns(TABLE_ALIAS);
+		#region Private Methods
+		BaseRDBDataProvider GetDataProvider()
+		{
+			return RDBDataProviderFactory.CreateProvider("VR_GenericData", "ConfigurationDBConnStringKey", "ConfigurationDBConnStringKey");
+		}
+		#endregion
+		public IEnumerable<BusinessEntityStatusHistory> GetFilteredBusinessEntitiesStatusHistory(DataRetrievalInput<BusinessEntityStatusHistoryQuery> input)
+		{
+			var queryContext = new RDBQueryContext(GetDataProvider());
+			var selectQuery = queryContext.AddSelectQuery();
+			selectQuery.From(TABLE_NAME, TABLE_ALIAS, null, true);
+			selectQuery.SelectColumns().AllTableColumns(TABLE_ALIAS);
 
-            var whereStatement = selectQuery.Where();
-            whereStatement.EqualsCondition(COL_BusinessEntityDefinitionID).Value(input.Query.BusinessEntityDefinitionId);
-            whereStatement.EqualsCondition(COL_BusinessEntityID).Value(input.Query.BusinessEntityId);
+			var whereStatement = selectQuery.Where();
+			whereStatement.EqualsCondition(COL_BusinessEntityDefinitionID).Value(input.Query.BusinessEntityDefinitionId);
+			whereStatement.EqualsCondition(COL_BusinessEntityID).Value(input.Query.BusinessEntityId);
+			if (!String.IsNullOrEmpty(input.Query.FieldName))
+			{
+				whereStatement.EqualsCondition(COL_FieldName).Value(input.Query.FieldName);
+			}
 
-            return queryContext.GetItems(BusinessEntityStatusHistoryMapper);
-        }
+			return queryContext.GetItems(BusinessEntityStatusHistoryMapper);
+		}
 
-        public BusinessEntityStatusHistory GetLastBusinessEntityStatusHistory(Guid businessEntityDefinitionId, string businessEntityId, string fieldName)
-        {
-            var queryContext = new RDBQueryContext(GetDataProvider());
-            var selectQuery = queryContext.AddSelectQuery();
-            selectQuery.From(TABLE_NAME, TABLE_ALIAS, 1, false);
-            selectQuery.SelectColumns().AllTableColumns(TABLE_ALIAS);
-            var whereStatement = selectQuery.Where();
-            whereStatement.ConditionIfColumnNotNull(COL_IsDeleted).EqualsCondition(COL_IsDeleted).Value(false);
-            whereStatement.EqualsCondition(COL_BusinessEntityDefinitionID).Value(businessEntityDefinitionId);
-            whereStatement.EqualsCondition(COL_BusinessEntityID).Value(businessEntityId);
-            whereStatement.EqualsCondition(COL_FieldName).Value(fieldName);
-            selectQuery.Sort().ByColumn(COL_StatusChangedDate, RDBSortDirection.DESC);
-            return queryContext.GetItem(BusinessEntityStatusHistoryMapper);
-        }
+		public BusinessEntityStatusHistory GetLastBusinessEntityStatusHistory(Guid businessEntityDefinitionId, string businessEntityId, string fieldName)
+		{
+			var queryContext = new RDBQueryContext(GetDataProvider());
+			var selectQuery = queryContext.AddSelectQuery();
+			selectQuery.From(TABLE_NAME, TABLE_ALIAS, 1, false);
+			selectQuery.SelectColumns().AllTableColumns(TABLE_ALIAS);
+			var whereStatement = selectQuery.Where();
+			whereStatement.ConditionIfColumnNotNull(COL_IsDeleted).EqualsCondition(COL_IsDeleted).Value(false);
+			whereStatement.EqualsCondition(COL_BusinessEntityDefinitionID).Value(businessEntityDefinitionId);
+			whereStatement.EqualsCondition(COL_BusinessEntityID).Value(businessEntityId);
+			whereStatement.EqualsCondition(COL_FieldName).Value(fieldName);
+			selectQuery.Sort().ByColumn(COL_StatusChangedDate, RDBSortDirection.DESC);
+			return queryContext.GetItem(BusinessEntityStatusHistoryMapper);
+		}
 
-        public bool Insert(Guid businessEntityDefinitionId, string businessEntityId, string fieldName, Guid statusId, Guid? previousStatusId, string moreInfo, string previousMoreInfo)
-        {
-            var queryContext = new RDBQueryContext(GetDataProvider());
-            var insertQuery = queryContext.AddInsertQuery();
-            insertQuery.IntoTable(TABLE_NAME);
-            insertQuery.Column(COL_BusinessEntityDefinitionID).Value(businessEntityDefinitionId);
-            insertQuery.Column(COL_BusinessEntityID).Value(businessEntityId);
-            insertQuery.Column(COL_FieldName).Value(fieldName);
-            insertQuery.Column(COL_StatusID).Value(statusId);
-            if (previousStatusId.HasValue)
-                insertQuery.Column(COL_PreviousStatusID).Value(previousStatusId.Value);
-            insertQuery.Column(COL_StatusChangedDate).DateNow();
-            insertQuery.Column(COL_IsDeleted).Value(false);
-            insertQuery.Column(COL_MoreInfo).Value(moreInfo);
-            insertQuery.Column(COL_PreviousMoreInfo).Value(previousMoreInfo);
-            return queryContext.ExecuteNonQuery() > 0;
-        }
+		public bool Insert(Guid businessEntityDefinitionId, string businessEntityId, string fieldName, Guid statusId, Guid? previousStatusId, string moreInfo, string previousMoreInfo)
+		{
+			var queryContext = new RDBQueryContext(GetDataProvider());
+			var insertQuery = queryContext.AddInsertQuery();
+			insertQuery.IntoTable(TABLE_NAME);
+			insertQuery.Column(COL_BusinessEntityDefinitionID).Value(businessEntityDefinitionId);
+			insertQuery.Column(COL_BusinessEntityID).Value(businessEntityId);
+			insertQuery.Column(COL_FieldName).Value(fieldName);
+			insertQuery.Column(COL_StatusID).Value(statusId);
+			if (previousStatusId.HasValue)
+				insertQuery.Column(COL_PreviousStatusID).Value(previousStatusId.Value);
+			insertQuery.Column(COL_StatusChangedDate).DateNow();
+			insertQuery.Column(COL_IsDeleted).Value(false);
+			insertQuery.Column(COL_MoreInfo).Value(moreInfo);
+			insertQuery.Column(COL_PreviousMoreInfo).Value(previousMoreInfo);
+			return queryContext.ExecuteNonQuery() > 0;
+		}
 
-    }
+	}
 }
