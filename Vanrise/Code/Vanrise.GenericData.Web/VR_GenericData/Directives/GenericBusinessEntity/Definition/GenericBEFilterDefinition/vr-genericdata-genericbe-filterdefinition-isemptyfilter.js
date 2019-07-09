b065cd03628f688e014c3f0180fc7e56ef41663b
Hassan Makhour <VANRISE\hassan.makhour>
2019-07-09 07:31:25 +0000

    load field title when data record field selector changed
    git-tfs-id: [http://developmentvlan:8585/tfs/vanrise.collection]$/;C57042

--- Vanrise/Code/Vanrise.GenericData.Web/VR_GenericData/Directives/GenericBusinessEntity/Definition/GenericBEFilterDefinition/vr-genericdata-genericbe-filterdefinition-isemptyfilter.js
+++ Vanrise/Code/Vanrise.GenericData.Web/VR_GenericData/Directives/GenericBusinessEntity/Definition/GenericBEFilterDefinition/vr-genericdata-genericbe-filterdefinition-isemptyfilter.js
@@ -1,59 +1,71 @@
 ﻿(function (app) {
 
-    'use strict';
+	'use strict';
 
-    IsEmptyFilterDefintionSettingsDirective.$inject = ['UtilsService', 'VRUIUtilsService', 'VRLocalizationService'];
+	IsEmptyFilterDefintionSettingsDirective.$inject = ['UtilsService', 'VRUIUtilsService', 'VRLocalizationService'];
 
-    function IsEmptyFilterDefintionSettingsDirective(UtilsService, VRUIUtilsService, VRLocalizationService) {
-        return {
-            restrict: "E",
-            scope: {
-                onReady: "=",
-            },
-            controller: function ($scope, $element, $attrs) {
-                var ctrl = this;
-                var ctor = new IsEmptyFilterDefintionSettingsController($scope, ctrl, $attrs);
-                ctor.initializeController();
-            },
-            controllerAs: "ctrl",
-            bindToController: true,
-            templateUrl: "/Client/Modules/VR_GenericData/Directives/GenericBusinessEntity/Definition/GenericBEFilterDefinition/Templates/IsEmptyFilterDefinitonTemplate.html"
-        };
+	function IsEmptyFilterDefintionSettingsDirective(UtilsService, VRUIUtilsService, VRLocalizationService) {
+		return {
+			restrict: "E",
+			scope: {
+				onReady: "=",
+			},
+			controller: function ($scope, $element, $attrs) {
+				var ctrl = this;
+				var ctor = new IsEmptyFilterDefintionSettingsController($scope, ctrl, $attrs);
+				ctor.initializeController();
+			},
+			controllerAs: "ctrl",
+			bindToController: true,
+			templateUrl: "/Client/Modules/VR_GenericData/Directives/GenericBusinessEntity/Definition/GenericBEFilterDefinition/Templates/IsEmptyFilterDefinitonTemplate.html"
+		};
 
 
-        function IsEmptyFilterDefintionSettingsController($scope, ctrl, $attrs) {
-            this.initializeController = initializeController;
+		function IsEmptyFilterDefintionSettingsController($scope, ctrl, $attrs) {
+			this.initializeController = initializeController;
 
-            var context;
-            var settings;
+			var context;
+			var settings;
 
-            var fieldGridAPI;
-            var fieldGridReadyDeferred = UtilsService.createPromiseDeferred();
+			var fieldGridAPI;
+			var fieldGridReadyDeferred = UtilsService.createPromiseDeferred();
 
-            var fieldSelectorAPI;
+			var fieldSelectorAPI;
 			var fieldSelectoReadyDeferred = UtilsService.createPromiseDeferred();
 
 			var textResourceSelectorAPI;
 			var textResourceSelectorDeferredReady = UtilsService.createPromiseDeferred();
-            function initializeController() {
-                $scope.scopeModel = {};
-                $scope.scopeModel.fields = [];
-                $scope.scopeModel.showResourceKeyColumn = VRLocalizationService.isLocalizationEnabled();
 
-                $scope.scopeModel.onFieldGridReady = function (api) {
-                    fieldGridAPI = api;
-                    fieldGridReadyDeferred.resolve();
-                    defineAPI();
+			var fillSelectorDeferredReady = UtilsService.createPromiseDeferred();
+			function initializeController() {
+				$scope.scopeModel = {};
+				$scope.scopeModel.fields = [];
+				$scope.scopeModel.showResourceKeyColumn = VRLocalizationService.isLocalizationEnabled();
+
+				$scope.scopeModel.onDataRecordTypeSelectorChanged = function (option) {
+					if (option != undefined) {
+						if (fillSelectorDeferredReady != undefined) {
+							fillSelectorDeferredReady.resolve();
+						}
+						else {
+							$scope.scopeModel.fieldTitle = option.Title;
+						}
+					}
+				};
+				$scope.scopeModel.onFieldGridReady = function (api) {
+					fieldGridAPI = api;
+					fieldGridReadyDeferred.resolve();
+					defineAPI();
 				};
 
 				$scope.scopeModel.onTextResourceSelectorReady = function (api) {
 					textResourceSelectorAPI = api;
 					textResourceSelectorDeferredReady.resolve();
 				};
-                $scope.scopeModel.onDataRecordTypeFieldsSelectorReady = function (api) {
-                    fieldSelectorAPI = api;
-                    fieldSelectoReadyDeferred.resolve();
-                };
+				$scope.scopeModel.onDataRecordTypeFieldsSelectorReady = function (api) {
+					fieldSelectorAPI = api;
+					fieldSelectoReadyDeferred.resolve();
+				};
 
 				$scope.scopeModel.validateGrid = function () {
 					if ($scope.scopeModel.fields.length > 0 && checkDuplicateDefaultField())
@@ -64,106 +76,108 @@
 
 					return null;
 				};
-            }
+			}
 
-            function defineAPI() {
-                var api = {};
+			function defineAPI() {
+				var api = {};
 
 				api.load = function (payload) {
-                    fieldGridAPI.clearDataSource();
-                    var initialPromises = [];
-               
-                    if (payload != undefined) {
-                        context = payload.context;
-                        settings = payload.settings;
+					fieldGridAPI.clearDataSource();
+					var initialPromises = [];
 
-                        if (settings != undefined) {
+					if (payload != undefined) {
+						context = payload.context;
+						settings = payload.settings;
+
+						if (settings != undefined) {
 							$scope.scopeModel.isRequired = settings.IsRequired;
 							$scope.scopeModel.fieldTitle = settings.FieldTitle;
-                        }
-                    }
+						}
+					}
 
 					initialPromises.push(loadFieldsSelector());
 					initialPromises.push(loadLocalizationTextResourceSelector());
 
-                    var rootPromiseNode = {
-                        promises: initialPromises,
-                        getChildNode: function () {
-                            var directivePromises = [];
+					var rootPromiseNode = {
+						promises: initialPromises,
+						getChildNode: function () {
+							var directivePromises = [];
 
-                            $scope.scopeModel.isGridLoading = true;
+							$scope.scopeModel.isGridLoading = true;
 
-                            var dataItemAllField = {
-                                field: 'All',
-                                payload: (settings != undefined) ? settings.AllField : undefined,
-                                readyResourceKeyPromiseDeferred: UtilsService.createPromiseDeferred(),
-                                loadResourceKeyPromiseDeferred: UtilsService.createPromiseDeferred()
-                            };
-                            addItemToGrid(dataItemAllField);
+							var dataItemAllField = {
+								field: 'All',
+								payload: (settings != undefined) ? settings.AllField : undefined,
+								readyResourceKeyPromiseDeferred: UtilsService.createPromiseDeferred(),
+								loadResourceKeyPromiseDeferred: UtilsService.createPromiseDeferred()
+							};
+							addItemToGrid(dataItemAllField);
 
-                            var dataItemNullField = {
-                                field: 'Null',
-                                payload: (settings != undefined) ? settings.NullField : undefined,
-                                readyResourceKeyPromiseDeferred: UtilsService.createPromiseDeferred(),
-                                loadResourceKeyPromiseDeferred: UtilsService.createPromiseDeferred()
-                            };
-                            addItemToGrid(dataItemNullField);
+							var dataItemNullField = {
+								field: 'Null',
+								payload: (settings != undefined) ? settings.NullField : undefined,
+								readyResourceKeyPromiseDeferred: UtilsService.createPromiseDeferred(),
+								loadResourceKeyPromiseDeferred: UtilsService.createPromiseDeferred()
+							};
+							addItemToGrid(dataItemNullField);
 
-                            var dataItemNotNullField = {
-                                field: 'Not Null',
-                                payload: (settings != undefined) ? settings.NotNullField : undefined,
-                                readyResourceKeyPromiseDeferred: UtilsService.createPromiseDeferred(),
-                                loadResourceKeyPromiseDeferred: UtilsService.createPromiseDeferred()
-                            };
-                            addItemToGrid(dataItemNotNullField);
+							var dataItemNotNullField = {
+								field: 'Not Null',
+								payload: (settings != undefined) ? settings.NotNullField : undefined,
+								readyResourceKeyPromiseDeferred: UtilsService.createPromiseDeferred(),
+								loadResourceKeyPromiseDeferred: UtilsService.createPromiseDeferred()
+							};
+							addItemToGrid(dataItemNotNullField);
 
-                            if ($scope.scopeModel.showResourceKeyColumn) {
-                                directivePromises.push(dataItemAllField.loadResourceKeyPromiseDeferred.promise);
-                                directivePromises.push(dataItemNullField.loadResourceKeyPromiseDeferred.promise);
-                                directivePromises.push(dataItemNotNullField.loadResourceKeyPromiseDeferred.promise);
-                            }
+							if ($scope.scopeModel.showResourceKeyColumn) {
+								directivePromises.push(dataItemAllField.loadResourceKeyPromiseDeferred.promise);
+								directivePromises.push(dataItemNullField.loadResourceKeyPromiseDeferred.promise);
+								directivePromises.push(dataItemNotNullField.loadResourceKeyPromiseDeferred.promise);
+							}
 
-                            $scope.scopeModel.isGridLoading = false;
+							$scope.scopeModel.isGridLoading = false;
 
-                            return {
-                                promises: directivePromises,
+							return {
+								promises: directivePromises,
 
-                            };
-                        }
-                    };
+							};
+						}
+					};
 
-                    return UtilsService.waitPromiseNode(rootPromiseNode);
-                };
+					return UtilsService.waitPromiseNode(rootPromiseNode).finally(function () {
+						fillSelectorDeferredReady = undefined;
+					});
+				};
 
-                api.getData = function () {
-                    return {
-                        $type: "Vanrise.GenericData.MainExtensions.IsEmptyFilterDefinitonSettings, Vanrise.GenericData.MainExtensions",
-                        IsRequired: $scope.scopeModel.isRequired,
-                        FieldName: fieldSelectorAPI.getSelectedIds(),
-                        AllField: getFieldObj($scope.scopeModel.fields[0]),
-                        NullField: getFieldObj($scope.scopeModel.fields[1]),
+				api.getData = function () {
+					return {
+						$type: "Vanrise.GenericData.MainExtensions.IsEmptyFilterDefinitonSettings, Vanrise.GenericData.MainExtensions",
+						IsRequired: $scope.scopeModel.isRequired,
+						FieldName: fieldSelectorAPI.getSelectedIds(),
+						AllField: getFieldObj($scope.scopeModel.fields[0]),
+						NullField: getFieldObj($scope.scopeModel.fields[1]),
 						NotNullField: getFieldObj($scope.scopeModel.fields[2]),
 						FieldTitle: $scope.scopeModel.fieldTitle,
 						TextResourceKey: textResourceSelectorAPI.getSelectedValues()
-                    };
-                };
+					};
+				};
 
-                if (ctrl.onReady != null) {
-                    ctrl.onReady(api);
-                }
-            }
+				if (ctrl.onReady != null) {
+					ctrl.onReady(api);
+				}
+			}
 
-            function loadFieldsSelector() {
-                var fieldSelectorLoadDeferred = UtilsService.createPromiseDeferred();
+			function loadFieldsSelector() {
+				var fieldSelectorLoadDeferred = UtilsService.createPromiseDeferred();
 
 				fieldSelectoReadyDeferred.promise.then(function () {
-                    var dataRecordTypeFieldSelectorPayload = {
-                        dataRecordTypeId: context.getDataRecordTypeId(),
-                        selectedIds: settings != undefined ? settings.FieldName : undefined
-                    };
-                    VRUIUtilsService.callDirectiveLoad(fieldSelectorAPI, dataRecordTypeFieldSelectorPayload, fieldSelectorLoadDeferred);
-                });
-                return fieldSelectorLoadDeferred.promise;
+					var dataRecordTypeFieldSelectorPayload = {
+						dataRecordTypeId: context.getDataRecordTypeId(),
+						selectedIds: settings != undefined ? settings.FieldName : undefined
+					};
+					VRUIUtilsService.callDirectiveLoad(fieldSelectorAPI, dataRecordTypeFieldSelectorPayload, fieldSelectorLoadDeferred);
+				});
+				return fieldSelectorLoadDeferred.promise;
 			}
 
 			function loadLocalizationTextResourceSelector() {
@@ -178,68 +192,68 @@
 			}
 
 
-            function addItemToGrid(dataItem) {
-                var payload = dataItem.payload;
+			function addItemToGrid(dataItem) {
+				var payload = dataItem.payload;
 
-                if (payload != undefined) {
-                    dataItem.title = payload.Title;
-                    dataItem.isDefault = payload.IsDefault;
-                    dataItem.removeFromSelector = payload.RemoveFromSelector;
-                }
+				if (payload != undefined) {
+					dataItem.title = payload.Title;
+					dataItem.isDefault = payload.IsDefault;
+					dataItem.removeFromSelector = payload.RemoveFromSelector;
+				}
 
-                dataItem.onResourceKeySelectorReady = function (api) {
-                    dataItem.resourceKeyAPI = api;
-                    dataItem.readyResourceKeyPromiseDeferred.resolve();
-                };
+				dataItem.onResourceKeySelectorReady = function (api) {
+					dataItem.resourceKeyAPI = api;
+					dataItem.readyResourceKeyPromiseDeferred.resolve();
+				};
 
-                dataItem.readyResourceKeyPromiseDeferred.promise.then(function () {
-                    var resourceKeySelectorPayload = {
-                        selectedResourceKey: payload != undefined ? payload.Resourcekey : undefined
-                    };
-                    VRUIUtilsService.callDirectiveLoad(dataItem.resourceKeyAPI, resourceKeySelectorPayload, dataItem.loadResourceKeyPromiseDeferred);
-                });
+				dataItem.readyResourceKeyPromiseDeferred.promise.then(function () {
+					var resourceKeySelectorPayload = {
+						selectedResourceKey: payload != undefined ? payload.Resourcekey : undefined
+					};
+					VRUIUtilsService.callDirectiveLoad(dataItem.resourceKeyAPI, resourceKeySelectorPayload, dataItem.loadResourceKeyPromiseDeferred);
+				});
 
-                $scope.scopeModel.fields.push(dataItem);
-            }
+				$scope.scopeModel.fields.push(dataItem);
+			}
 
-            function getFieldObj(column) {
-                var obj = {};
-                if (column != undefined) {
-                    obj.Title = column.title;
-                    obj.Resourcekey = (column.resourceKeyAPI) ? column.resourceKeyAPI.getResourceKey() : undefined;
-                    obj.IsDefault = column.isDefault;
-                    obj.RemoveFromSelector = column.removeFromSelector;
-                }
-                return obj;
-            }
+			function getFieldObj(column) {
+				var obj = {};
+				if (column != undefined) {
+					obj.Title = column.title;
+					obj.Resourcekey = (column.resourceKeyAPI) ? column.resourceKeyAPI.getResourceKey() : undefined;
+					obj.IsDefault = column.isDefault;
+					obj.RemoveFromSelector = column.removeFromSelector;
+				}
+				return obj;
+			}
 
-            function checkDuplicateDefaultField() {
-                var fieldLength = $scope.scopeModel.fields.length;
-                for (var i = 0; i < fieldLength; i++) {
-                    var currentItem = $scope.scopeModel.fields[i];
-                    for (var j = i + 1; j < fieldLength; j++) {
-                        if ($scope.scopeModel.fields[j].isDefault && currentItem.isDefault)
-                            return true;
-                    }
-                }
-                return false;
-            }
+			function checkDuplicateDefaultField() {
+				var fieldLength = $scope.scopeModel.fields.length;
+				for (var i = 0; i < fieldLength; i++) {
+					var currentItem = $scope.scopeModel.fields[i];
+					for (var j = i + 1; j < fieldLength; j++) {
+						if ($scope.scopeModel.fields[j].isDefault && currentItem.isDefault)
+							return true;
+					}
+				}
+				return false;
+			}
 
-            function checkRemoveFromSelector() {
-                var fieldLength = $scope.scopeModel.fields.length;
-                var counter = 0;
-                for (var i = 0; i < fieldLength; i++) {
-                    var currentItem = $scope.scopeModel.fields[i];
-                    if (currentItem.removeFromSelector)
-                        counter++;
-                }
-                if (counter == fieldLength)
-                    return true;
-                return false;
-            }
-        }
-    }
+			function checkRemoveFromSelector() {
+				var fieldLength = $scope.scopeModel.fields.length;
+				var counter = 0;
+				for (var i = 0; i < fieldLength; i++) {
+					var currentItem = $scope.scopeModel.fields[i];
+					if (currentItem.removeFromSelector)
+						counter++;
+				}
+				if (counter == fieldLength)
+					return true;
+				return false;
+			}
+		}
+	}
 
-    app.directive('vrGenericdataGenericbeFilterdefinitionIsemptyfilter', IsEmptyFilterDefintionSettingsDirective);
+	app.directive('vrGenericdataGenericbeFilterdefinitionIsemptyfilter', IsEmptyFilterDefintionSettingsDirective);
 
 })(app);
