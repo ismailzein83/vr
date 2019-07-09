0af5600ac125eae651fb3bb0812915e5d766282b
Samer Haidar <VANRISE\samer.haidar>
2019-07-09 07:30:17 +0000

    Update sp_BusinessEntityStatusHistory_GetFiltered
    git-tfs-id: [http://developmentvlan:8585/tfs/vanrise.collection]$/;C57041

--- TOneV2/Code/TOneV2/SQL.TOneConfiguration/genericdata/Stored Procedures/sp_BusinessEntityStatusHistory_GetFiltered.sql
+++ TOneV2/Code/TOneV2/SQL.TOneConfiguration/genericdata/Stored Procedures/sp_BusinessEntityStatusHistory_GetFiltered.sql
@@ -3,12 +3,13 @@
 -- Create date: <Create Date,,>
 -- Description:	<Description,,>
 -- =============================================
-Create PROCEDURE [genericdata].[sp_BusinessEntityStatusHistory_GetFiltered]
-@BusinessEntityDefinitionId uniqueidentifier,
-	@BusinessEntityId varchar(50)
+CREATE PROCEDURE [genericdata].[sp_BusinessEntityStatusHistory_GetFiltered]
+    @BusinessEntityDefinitionId uniqueidentifier,
+	@BusinessEntityId varchar(50),
+	@FieldName varchar(50)
 AS
 BEGIN
 	SELECT	sh.ID,sh.BusinessEntityDefinitionID,sh.BusinessEntityID,sh.FieldName,sh.IsDeleted,sh.CreatedTime,sh.MoreInfo,sh.PreviousMoreInfo,sh.PreviousStatusID,sh.StatusChangedDate,sh.StatusID
 	FROM	[genericdata].BusinessEntityStatusHistory sh WITH(NOLOCK) 
-	where sh.BusinessEntityDefinitionID=@BusinessEntityDefinitionId AND sh.BusinessEntityID=@BusinessEntityId
+	where    sh.BusinessEntityDefinitionID=@BusinessEntityDefinitionId AND sh.BusinessEntityID=@BusinessEntityId AND (@FieldName is NULL OR @FieldName = sh.FieldName)
 END
