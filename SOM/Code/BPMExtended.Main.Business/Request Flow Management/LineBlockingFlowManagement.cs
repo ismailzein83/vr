1f642c3a15f986af0cc9973e3e081122b6fd311a
Mortada Issa <VANRISE\mortada.issa>
2019-07-11 07:48:17 +0000

    adding default option to steps
    git-tfs-id: [http://developmentvlan:8585/tfs/vanrise.collection]$/;C57078

--- SOM/Code/BPMExtended.Main.Business/Request Flow Management/LineBlockingFlowManagement.cs
+++ SOM/Code/BPMExtended.Main.Business/Request Flow Management/LineBlockingFlowManagement.cs
@@ -33,6 +33,7 @@ namespace BPMExtended.Main.Business
                 case paymentStep: nextStepId = attachmentstep; break;
                 case attachmentstep: nextStepId = submittedtoom; break;
                 case submittedtoom: nextStepId = completedStep; break;
+                default: throw new InvalidOperationException(string.Format("Step not found. Id = {0}, current step id= {1}", id, currentStepId));
             }
             return nextStepId;
         }
