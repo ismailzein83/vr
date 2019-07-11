37ea599f39d6b06b14878f9b13227d12d26f4a19
Fatima Hammoud <VANRISE\fatima.hammoud>
2019-07-11 09:44:58 +0000

    Adding class "PrepaidTaxMappingStep"
    git-tfs-id: [http://developmentvlan:8585/tfs/vanrise.collection]$/;C57086

---
+++ Retail/Code/Retail.RA.Business/TaxRules/Prepaid/PrepaidTaxMappingStep.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Vanrise.GenericData.Transformation.Entities;
+
+namespace Retail.RA.Business
+{
+    public class PrepaidTaxMappingStep : BaseGenericRuleMappingStep
+    {
+        public override Guid ConfigId { get { return new Guid("C8AE398C-9B0D-433B-88FD-57B6EBF3482B"); } }
+
+        #region TopUp
+        public string TotalAmount { get; set; }
+        public string TotalTaxValue { set; get; }
+        #endregion
+
+        public override void GenerateExecutionCode(IDataTransformationCodeGenerationContext context)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
