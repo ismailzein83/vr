b1f0189cd69c7d969801c4b8ade6868035ff8756
Mohammad Yahfoufi <VANRISE\mohammad.yahfoufi>
2019-07-15 07:18:14 +0000

    CreatePaymentPlanFlowManagement new steps
    git-tfs-id: [http://developmentvlan:8585/tfs/vanrise.collection]$/;C57118

--- SOM/Code/BPMExtended.Main.Business/Request Flow Management/CreatePaymentPlanFlowManagement.cs
+++ SOM/Code/BPMExtended.Main.Business/Request Flow Management/CreatePaymentPlanFlowManagement.cs
@@ -12,6 +12,8 @@ namespace BPMExtended.Main.Business
         const string paymentTemplate = "119AC20E-C0B1-4699-8250-F057E6E070EF";
         const string print = "994DB20F-4F9F-4320-B730-538ACD72AC30";
         const string technicalStep = "D51B409B-85DE-40D3-A0D1-2D96B509306E";
+        const string submitToOM = "E8B4AF7E-095B-4BF0-8B11-8C7FDBEE57F9";
+        const string completed = "63130DA4-C53E-435D-8855-A4A715852D3A";
 
         public string GetNextStep(string id, string currentStepId)
         {
@@ -21,6 +23,8 @@ namespace BPMExtended.Main.Business
                 case startingProcess: nextStepId = paymentTemplate; break;
                 case paymentTemplate: nextStepId = print; break;
                 case print: nextStepId = technicalStep; break;
+                case technicalStep: nextStepId = submitToOM; break;
+                case submitToOM: nextStepId = completed; break;
             }
             return nextStepId;
         }
