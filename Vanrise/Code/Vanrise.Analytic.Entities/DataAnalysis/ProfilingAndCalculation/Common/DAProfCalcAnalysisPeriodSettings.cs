3de1b55de187b29f05d24be88bea1e9c5aaebdcb
Naim Saad <VANRISE\naim.saad>
2019-07-16 11:49:21 +0000

    Implementing DAProfCalcAnalysisPeriodSettings class
    git-tfs-id: [http://developmentvlan:8585/tfs/vanrise.collection]$/;C57151

---
+++ Vanrise/Code/Vanrise.Analytic.Entities/DataAnalysis/ProfilingAndCalculation/Common/DAProfCalcAnalysisPeriodSettings.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Vanrise.Analytic.Entities
+{
+    public abstract class DAProfCalcAnalysisPeriodSettings
+    {
+        public abstract Guid ConfigId { get; }
+
+        public abstract int GetPeriodInMinutes(IDAProfCalcAnalysisPeriodContext context);
+    }
+
+    public interface IDAProfCalcAnalysisPeriodContext
+    {
+        DateTime EffectiveDate { get; }
+    }
+
+    public class DAProfCalcAnalysisPeriodContext : IDAProfCalcAnalysisPeriodContext
+    {
+        public DateTime EffectiveDate { get; set; }
+    }
+}
