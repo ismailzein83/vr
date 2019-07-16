d06835f456db7c06a9f975183ceba579c5bae993
Ismail Zein <VANRISE\ismail.zein>
2019-07-16 15:00:15 +0000

    change bundle table structure
    git-tfs-id: [http://developmentvlan:8585/tfs/vanrise.collection]$/;C57162

--- Retail/Retail.SQL/SQL.Retail.Billing/RetailBilling/Tables/Bundle.sql
+++ Retail/Retail.SQL/SQL.Retail.Billing/RetailBilling/Tables/Bundle.sql
@@ -1,11 +1,13 @@
 ﻿CREATE TABLE [RetailBilling].[Bundle] (
-    [ID]               BIGINT         IDENTITY (1, 1) NOT NULL,
-    [Name]             NVARCHAR (255) NULL,
-    [CreatedTime]      DATETIME       NULL,
-    [CreatedBy]        INT            NULL,
-    [LastModifiedTime] DATETIME       NULL,
-    [LastModifiedBy]   INT            NULL,
-    [timestamp]        ROWVERSION     NULL,
-    CONSTRAINT [PK_Bundles] PRIMARY KEY CLUSTERED ([ID] ASC)
+    [ID]               UNIQUEIDENTIFIER NULL,
+    [Name]             NVARCHAR (255)   NULL,
+    [Size]             DECIMAL (20, 8)  NULL,
+    [CreatedTime]      DATETIME         NULL,
+    [CreatedBy]        INT              NULL,
+    [LastModifiedTime] DATETIME         NULL,
+    [LastModifiedBy]   INT              NULL,
+    [timestamp]        ROWVERSION       NULL
 );
+
+
 
