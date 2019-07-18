4c413fbcf2290e97912b2efec03630e89790ae2a
Ali Slayman <VANRISE\ali.slayman>
2019-07-18 13:15:11 +0000

    Adding ExecuteActionTypeEditorDefinitionSetting abstract class
    git-tfs-id: [http://developmentvlan:8585/tfs/vanrise.collection]$/;C57206

---
+++ Vanrise/Code/Vanrise.GenericData.Entities/GenericEditorDefinitionSetting/ExecuteActionTypeEditorDefinitionSetting.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Vanrise.GenericData.Entities
+{
+    public abstract class ExecuteActionTypeEditorDefinitionSetting
+    {
+        public abstract Guid ConfigId { get; }
+        public virtual string ActionRuntimeEditor { get; set; }
+    }
+}
