547d9f2bbf92897ce7e0a7b4f03fab204845ab6f
Fatima Hammoud <VANRISE\fatima.hammoud>
2019-07-22 07:53:21 +0000

    Adding commented some files
    git-tfs-id: [http://developmentvlan:8585/tfs/vanrise.collection]$/;C57243

---
+++ Vanrise/Code/Vanrise.GenericData.Web/VR_GenericData/Directives/BusinessEntityDefinition/vr-genericdata-fieldtype-genericdesigneditor.js
@@ -0,0 +1,70 @@
+﻿//"use strict";
+//app.directive("vrGenericdataFieldtypeGenericdesigneditor", ["UtilsService", "VRUIUtilsService","VR_GenericData_GenericBEDefinitionService",
+//    function (UtilsService, VRUIUtilsService, VR_GenericData_GenericBEDefinitionService) {
+//        var directiveDefinitionObject = {
+//            restrict: "E",
+//            scope: {
+//                onReady: "=",
+//            },
+//            controller: function ($scope, $element, $attrs) {
+//                var ctrl = this;
+//                var fieldTypeGenericEditorDesign = new FieldTypeGenericEditorDesignCtrl($scope, ctrl, $attrs, $element);
+//                fieldTypeGenericEditorDesign.initializeController();
+//            },
+//            controllerAs: "ctrl",
+//            bindToController: true,
+//            templateUrl: "/Client/Modules/VR_GenericData/Directives/BusinessEntityDefinition/Templates/FieldTypeGenericEditorDesign.html"
+//        };
+//        function FieldTypeGenericEditorDesignCtrl($scope, ctrl, $attrs, $element) {
+//            this.initializeController = initializeController;
+
+//            var context;
+//            var fieldTypeEntity;
+
+//            function initializeController() {
+//                $scope.scopeModel = {};
+
+//                $scope.scopeModel.openSettingsModal = function () {
+//                    var onFiledTypeSettingsChanged = function (fieldTypeItem) {
+//                    };
+
+//                    VR_GenericData_GenericBEDefinitionService.openFieldTypeGenericEditorDesignModal(onFiledTypeSettingsChanged, fieldTypeEntity, getContext());
+//                };
+
+//                defineAPI();
+//            }
+
+//            function defineAPI() {
+//                var api = {};
+//                api.load = function (payload) {
+
+//                    if (payload != undefined) {
+//                        context = payload.context;
+//                       // fieldTypeEntity = payload.
+//                    }
+
+//                    var rootPromiseNode = {
+//                        promises: []
+//                    };
+
+//                    return UtilsService.waitPromiseNode(rootPromiseNode);
+//                };
+
+//                api.getData = function () {
+
+//                };
+
+//                if (ctrl.onReady != null)
+//                    ctrl.onReady(api);
+//            }
+
+//            function getContext() {
+//                var currentContext = context;
+//                if (currentContext == undefined)
+//                    currentContext = {};
+//                return currentContext;
+//            }
+//        }
+//        return directiveDefinitionObject;
+//    }
+//]);
---
+++ Vanrise/Code/Vanrise.GenericData.Web/VR_GenericData/Directives/GenericBusinessEntity/Definition/Editor/FieldTypeGenericDesignEditorController.js
@@ -0,0 +1,104 @@
+﻿//(function (appControllers) {
+
+//    "use strict";
+
+//    FieldTypeGenericDesignEditorController.$inject = ['$scope', 'UtilsService', 'VRNotificationService', 'VRNavigationService', 'VRUIUtilsService'];
+
+//    function FieldTypeGenericDesignEditorController($scope, UtilsService, VRNotificationService, VRNavigationService, VRUIUtilsService) {
+
+//        var fieldTypeEntitySettings;
+//        var context;
+
+//        var fieldTypeRuntimeDirectiveAPI;
+//        var fieldTypeRuntimeReadyPromiseDeferred = UtilsService.createPromiseDeferred();
+
+//        loadParameters();
+//        defineScope();
+//        load();
+
+//        function loadParameters() {
+//            var parameters = VRNavigationService.getParameters($scope);
+//            if (parameters != undefined) {
+//                fieldTypeEntitySettings = parameters.fieldTypeEntity;
+//                context = parameters.context;
+//            }
+//        }
+
+//        function defineScope() {
+//            $scope.scopeModel = {};
+
+//            $scope.scopeModel.onFieldTypeRumtimeDirectiveReady = function (api) {
+//                fieldTypeRuntimeDirectiveAPI = api;
+//                fieldTypeRuntimeReadyPromiseDeferred.resolve();
+//            };
+
+//            $scope.scopeModel.saveFieldTypeSettings = function () {
+              
+//            };
+
+//            $scope.scopeModel.close = function () {
+//                $scope.modalContext.closeModal();
+//            };
+
+//        }
+
+//        function load() {
+//            loadAllControls();
+
+//            function loadAllControls() {
+//                $scope.scopeModel.isLoading = true;
+//                function setTitle() {
+//                    $scope.title = UtilsService.buildTitleForUpdateEditor(fieldTypeEntitySettings.FieldPath, 'Generic Field Type Settings');
+//                }
+
+//                function loadStaticData() {
+//                  if
+//                }
+
+//                function loadAdditionalSettingsSelective() {
+//                    var additionalSettingsSelectiveLoadPromiseDeferred = UtilsService.createPromiseDeferred();
+//                    additionalSettingsSelectiveReadyPromiseDeferred.promise.then(function () {
+//                        var additionalSettingsPayload = additionalSettings != undefined ? additionalSettings.Settings : undefined;
+//                        VRUIUtilsService.callDirectiveLoad(additionalSettingsSelectiveAPI, additionalSettingsPayload, additionalSettingsSelectiveLoadPromiseDeferred);
+//                    });
+//                    return additionalSettingsSelectiveLoadPromiseDeferred.promise;
+//                }
+
+
+//                return UtilsService.waitMultipleAsyncOperations([loadStaticData, setTitle, loadAdditionalSettingsSelective]).then(function () {
+//                }).finally(function () {
+//                    $scope.scopeModel.isLoading = false;
+//                }).catch(function (error) {
+//                    VRNotificationService.notifyExceptionWithClose(error, $scope);
+//                });
+
+//            }
+
+//        }
+     
+
+//        function loadFieldTypeRuntimeDirective() {
+//            fieldTypeRuntimeLoadPromiseDeferred == UtilsService.createPromiseDeferred();
+//            fieldTypeRuntimeReadyPromiseDeferred.promise.then(function () {
+//                var directivePayload = {
+//                    fieldTitle: "Default Value",
+//                    fieldType: fieldTypeEntitySettings.fieldType,
+//                    fieldName: fieldTypeEntitySettings.FieldPath,
+//                    fieldValue: fieldTypeEntitySettings.DefaultFieldValue
+//                };
+//                VRUIUtilsService.callDirectiveLoad(fieldTypeRuntimeDirectiveAPI, directivePayload, fieldTypeRuntimeLoadPromiseDeferred);
+//            });
+//            return fieldTypeRuntimeLoadPromiseDeferred.promise;
+//        }
+
+//        function getContext() {
+//            var currentContext = context;
+//            if (currentContext == undefined)
+//                currentContext = {};
+//            return currentContext;
+//        }
+
+//    }
+
+//    appControllers.controller('VR_GenericData_FieldTypeGenericDesignEditorController', FieldTypeGenericDesignEditorController);
+//})(appControllers);
