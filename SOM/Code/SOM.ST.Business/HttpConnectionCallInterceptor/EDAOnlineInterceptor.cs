60653af2e6200aba0bd431f7c0c0daea76ce86f4
Rodi Hassan <VANRISE\rodi.hassan>
2019-07-22 10:25:17 +0000

    Add EDA Interceptor
    git-tfs-id: [http://developmentvlan:8585/tfs/vanrise.collection]$/;C57250

---
+++ SOM/Code/SOM.ST.Business/HttpConnectionCallInterceptor/EDAOnlineInterceptor.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using Vanrise.Common.Business;
+
+namespace SOM.ST.Business
+{
+    public class EDAOnlineInterceptor : VRHttpConnectionCallInterceptor
+    {
+        public string SessionId { get; set; }
+
+        public override Guid ConfigId { get { return new Guid("461B7474-9B19-4B90-AEAB-63BA37245E53"); } }
+
+        private string HeaderText = @"
+                <soapenv:Header>
+                    <cai3:SessionId  xmlns:cai3=""http://schemas.ericsson.com/cai3g1.2/"" >${##SessionId##}</cai3:SessionId>
+                </soapenv:Header>";
+
+        public override void InterceptRequest(IVRHttpConnectionInterceptRequestContext context)
+        {
+            var headerText = HeaderText;
+            headerText = headerText.Replace("##SessionId##", SessionId);
+            context.Body = context.Body.Replace("<soapenv:Header />", headerText);
+        }
+
+        public override void InterceptResponse(IVRHttpConnectionInterceptResponseContext context)
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+}
