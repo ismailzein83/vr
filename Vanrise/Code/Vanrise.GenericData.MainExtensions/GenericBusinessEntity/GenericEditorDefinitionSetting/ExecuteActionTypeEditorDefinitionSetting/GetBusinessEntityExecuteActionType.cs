0740eda6a08d9ccb407c2ccc80033ee00ffcf9d5
Ali Slayman <VANRISE\ali.slayman>
2019-07-22 07:31:39 +0000

    Add GetBusinessEntityExecuteActionType class
    git-tfs-id: [http://developmentvlan:8585/tfs/vanrise.collection]$/;C57242

---
+++ Vanrise/Code/Vanrise.GenericData.MainExtensions/GenericBusinessEntity/GenericEditorDefinitionSetting/ExecuteActionTypeEditorDefinitionSetting/GetBusinessEntityExecuteActionType.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Vanrise.GenericData.Entities;
+
+namespace Vanrise.GenericData.MainExtensions
+{
+    public class GetBusinessEntityExecuteActionType : ExecuteActionTypeEditorDefinitionSetting
+    {
+        public override Guid ConfigId { get { return new Guid("7D9BE3A4-9E86-41B4-A954-DB4FB330901A"); } }
+    }
+}
