a6acbf49d2de42d57d4c7df91bcfd6e78f30820a
Mohammad Sheaib <VANRISE\mohammad.sheaib>
2019-07-23 09:10:09 +0000

    Commented
    git-tfs-id: [http://developmentvlan:8585/tfs/vanrise.collection]$/;C57268

---
+++ BusinessProcess/Code/Vanrise.BusinessProcess.Web/BusinessProcess/Directives/VRWorkflow/bp-vr-workflow-activity-fieldtype-expressionbuilder.js
@@ -0,0 +1,151 @@
+﻿//'use strict';
+
+//app.directive('businessprocessVrWorkflowActivityFieldtypeExpressionbuilder', ['UtilsService', 'VRUIUtilsService', 'BusinessProcess_VRWorkflowService', 'BusinessProcess_CustomCodeTaskService',
+//	function (UtilsService, VRUIUtilsService, BusinessProcess_VRWorkflowService, BusinessProcess_CustomCodeTaskService) {
+
+//		var directiveDefinitionObject = {
+//			restrict: 'E',
+//			scope: {
+//                isrequired: '=',
+//                onReady: '=',
+//				label: '@',
+//				hidelabel: '=',
+//				nbOfRows: '='
+//			},
+//			controller: function ($scope, $element, $attrs) {
+//				var ctrl = this;
+//				if (ctrl.nbOfRows != undefined)
+//					$scope.nbOfRows = ctrl.nbOfRows;
+//				else
+//					$scope.nbOfRows = 1;
+//				var ctor = new workflowAssign(ctrl, $scope, $attrs);
+//				ctor.initializeController();
+//			},
+//			controllerAs: 'ctrl',
+//			bindToController: true,
+//			compile: function (element, attrs) {
+
+//			},
+//            templateUrl: '/Client/Modules/BusinessProcess/Directives/VRWorkflow/Templates/VRWorkflowActivityFieldTypeExpressionBuilderTemplate.html'
+//		};
+
+//		function workflowAssign(ctrl, $scope, $attrs) {
+
+//			this.initializeController = initializeController;
+//            function initializeController() {
+//                $scope.scopeModel = {};
+//                var context;
+//                var codeExpression;
+//                var fieldValue;
+//                var fieldType = {
+//                    $type: "Vanrise.GenericData.MainExtensions.DataRecordFields.FieldTextType, Vanrise.GenericData.MainExtensions",
+//                    CanRoundValue: false,
+//                    ConfigId: "3f29315e-b542-43b8-9618-7de216cd9653",
+//                    ConvertFilterMethod: null,
+//                    DetailViewerEditor: "vr-genericdata-datarecordfield-defaultdetailviewer",
+//                    DifferenceEditor: null,
+//                    GetValueMethod: null,
+//                    Hint: null,
+//                    OrderType: 1,
+//                    RuntimeEditor: "vr-genericdata-fieldtype-text-runtimeeditor",
+//                    RuntimeViewSettingEditor: null,
+//                    StoreValueSerialized: false,
+//                    TextType: undefined,
+//                    ViewerEditor: "vr-genericdata-fieldtype-text-viewereditor"
+//                };
+//				if (ctrl.label == undefined)
+//                    ctrl.label = "Value";
+//                //setTimeout(function () {
+//                //    if (ctrl.value != undefined) {
+//                //        $scope.scopeModel.codeExpression = ctrl.value.CodeExpression; 
+//                //        $scope.$apply();
+//                //    }
+//                //    $scope.$watch('ctrl.value', function (newValue) {
+//                //        if (newValue) {
+//                //            if (newValue.CodeExpression != undefined) {
+//                //                $scope.scopeModel.codeExpression = newValue.CodeExpression;
+//                //            }
+//                //            else if (newValue.Value != undefined) {
+//                //                $scope.scopeModel.codeExpression = newValue.Value;
+//                //            }
+//                //        }
+//                //        else
+//                //            $scope.scopeModel.codeExpression = undefined;
+//                //    });
+//                //    $scope.$watch('scopeModel.codeExpression', function (newCodeExpression) {
+//                //        if (newCodeExpression != undefined && newCodeExpression != '') {
+//                //            ctrl.value = {
+//                //                "$type": "Vanrise.BusinessProcess.Entities.VRWorkflowCodeExpression, Vanrise.BusinessProcess.Entities",
+//                //                "CodeExpression": newCodeExpression
+//                //            };
+//                //        }
+//                //    });
+//                //    $scope.$watch('scopeModel.fieldValue', function (newFieldValue) {
+//                //        if (newFieldValue != undefined && newFieldValue != '') {
+//                //            ctrl.value = {
+//                //                "$type": "Vanrise.BusinessProcess.Entities.VRWorkflowFieldTypeExpression, Vanrise.BusinessProcess.Entities",
+//                //                "Value": newFieldValue
+//                //            };
+//                //        }
+//                //    });
+//                //}, 500);
+
+//                $scope.scopeModel.disableExpression = function () {
+//                    return false;
+//                };
+//				$scope.scopeModel.openExpressionBuilder = function () {
+//                    var args = context != undefined ? context.getWorkflowArguments() : undefined;
+//                    var vars = context != undefined ? context.getParentVariables() : undefined;
+//					var onSetExpressionBuilder = function (expressionBuilderValue) {
+//                        $scope.scopeModel.expression = expressionBuilderValue;
+//                    };
+//                    var onSetFieldValue = function (value) {
+//                        fieldValue = value;
+//                    };
+//                    BusinessProcess_VRWorkflowService.openFieldTypeExpressionBuilder(onSetExpressionBuilder, onSetFieldValue, args, vars, fieldType, fieldValue, $scope.scopeModel.expression );
+//                };
+
+//                defineAPI();
+
+//                function defineAPI() {
+//                    var api = {};
+
+//                    api.load = function (payload) {
+
+//                        if (payload != undefined) {
+//                            context = payload.context;
+//                            fieldValue = payload.fieldValue;
+//                            $scope.scopeModel.expression  = payload.codeExpression;
+//                        }
+
+//                        var rootPromiseNode = {
+//                            promises: []
+//                        };
+//                        return UtilsService.waitPromiseNode(rootPromiseNode);
+//                    };
+
+//                    api.getData = function () {
+//                        if ($scope.scopeModel.expression  != undefined && $scope.scopeModel.expression  != '') {
+//                            return {
+//                                $type: "Vanrise.BusinessProcess.Entities.VRWorkflowCodeExpression, Vanrise.BusinessProcess.Entities",
+//                                CodeExpression: $scope.scopeModel.expression 
+//                            };
+//                        }
+//                        else if (fieldValue != undefined && fieldValue != '') {
+//                            return {
+//                                $type: "Vanrise.BusinessProcess.Entities.VRWorkflowFieldTypeExpression, Vanrise.BusinessProcess.Entities",
+//                                Value: fieldValue
+//                            }
+//                        }
+//                    };
+
+//                    if (ctrl.onReady != null)
+//                        ctrl.onReady(api);
+//                }
+
+//            }
+
+
+//		}
+//		return directiveDefinitionObject;
+//	}]);
---
+++ BusinessProcess/Code/Vanrise.BusinessProcess.Web/BusinessProcess/Views/VRWorkflow/FieldTypeExpressionBuilderEditorController.js
@@ -0,0 +1,111 @@
+﻿//(function (appControllers) {
+
+//	"use strict";
+
+//	ExpressionBuilderEditorController.$inject = ['$scope', 'UtilsService', 'VRNotificationService', 'VRNavigationService', 'VRUIUtilsService'];
+
+//	function ExpressionBuilderEditorController($scope, UtilsService, VRNotificationService, VRNavigationService, VRUIUtilsService) {
+
+//        var textAreaAPI;
+//        var isEditMode;
+//        var fieldValue;
+//        var runtimeEditorAPI;
+//        var runtimeEditorReadyDeferred = UtilsService.createPromiseDeferred();
+
+//		loadParameters();
+//		defineScope();
+//		load();
+
+//		function loadParameters() {
+//			$scope.scopeModel = {};
+//			var parameters = VRNavigationService.getParameters($scope);
+//			if (parameters != undefined && parameters != null) {
+//				$scope.scopeModel.expressionValue = parameters.ExpressionValue;
+//				$scope.scopeModel.arguments = parameters.Arguments;
+//                $scope.scopeModel.variables = parameters.Variables;
+//                $scope.scopeModel.fieldType = parameters.FieldType;
+//                fieldValue = parameters.fieldValue;
+//            }
+//            if ($scope.scopeModel.fieldType) {
+//                $scope.scopeModel.runTimeEditor = $scope.scopeModel.fieldType.RuntimeEditor;
+//                isEditMode = $scope.scopeModel.fieldType != undefined;
+
+//            }
+//		}
+
+//		function defineScope() {
+
+//			$scope.scopeModel.onTextAreaReady = function (api) {
+//				textAreaAPI = api;
+//            };
+
+//            $scope.scopeModel.onFieldDirectiveReady = function (api) {
+//                runtimeEditorAPI = api;
+//                runtimeEditorReadyDeferred.resolve();
+//            };
+
+//			$scope.scopeModel.saveValue = function () {
+//                return setValue();
+//			};
+
+//			$scope.scopeModel.insertText = function (item) {
+//				if (textAreaAPI != undefined) {
+//					textAreaAPI.appendAtCursorPosition(item.Name);
+//				}
+//			};
+
+//			$scope.scopeModel.close = function () {
+//				$scope.modalContext.closeModal();
+//			};
+//		}
+//        function loadEditorRuntimeDirective() {
+//            var runtimeEditorLoadDeferred = UtilsService.createPromiseDeferred();
+//            runtimeEditorReadyDeferred.promise.then(function () {
+
+//                var payload = {
+//                    fieldType: $scope.scopeModel.fieldType,
+//                    fieldValue: fieldValue,
+//                };
+
+//                VRUIUtilsService.callDirectiveLoad(runtimeEditorAPI, payload, runtimeEditorLoadDeferred);
+//            });
+
+//            return runtimeEditorLoadDeferred.promise;
+//        }
+//        function load() {
+
+
+//            $scope.scopeModel.isLoading = true;
+
+//            if (isEditMode) {
+//                loadAllControls().finally(function () {
+//                });
+//            }
+//            else
+//                loadAllControls();
+//        }
+//        function loadAllControls() {
+
+//            $scope.title = UtilsService.buildTitleForUpdateEditor('Expression Builder');
+
+//            var promises = [];
+//            if ($scope.scopeModel.fieldType != undefined)
+//                promises.push(loadEditorRuntimeDirective());
+//            return UtilsService.waitPromiseNode({ promises: promises }).catch(function (error) {
+//                VRNotificationService.notifyExceptionWithClose(error, $scope);
+//            }).finally(function () {
+//                $scope.scopeModel.isLoading = false;
+//            });
+//        }
+
+//        function setValue() {
+//            if ($scope.onSetExpressionBuilder != undefined && $scope.scopeModel.expressionValue != undefined && $scope.scopeModel.expressionValue!='')
+//                $scope.onSetExpressionBuilder($scope.scopeModel.expressionValue);
+//            if ($scope.onSetFieldValue != undefined && runtimeEditorAPI != undefined && runtimeEditorAPI.getData() != undefined) 
+//                $scope.onSetFieldValue(runtimeEditorAPI.getData());
+//			$scope.modalContext.closeModal();
+//		}
+//	}
+
+//	appControllers.controller('BusinessProcess_VRWorkflow_FieldTypeExpressionBuilderController', ExpressionBuilderEditorController);
+//})(appControllers);
