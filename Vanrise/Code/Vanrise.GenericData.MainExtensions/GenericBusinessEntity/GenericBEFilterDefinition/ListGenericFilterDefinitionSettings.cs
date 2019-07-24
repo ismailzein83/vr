8ad5f2568c60d2943fed06c523b50db73eff955a
Hassan Makhour <VANRISE\hassan.makhour>
2019-07-24 12:42:15 +0000

    ListGenericFilter
    git-tfs-id: [http://developmentvlan:8585/tfs/vanrise.collection]$/;C57302

---
+++ Vanrise/Code/Vanrise.GenericData.MainExtensions/GenericBusinessEntity/GenericBEFilterDefinition/ListGenericFilterDefinitionSettings.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Vanrise.GenericData.Business;
+
+namespace Vanrise.GenericData.MainExtensions
+{
+	public class ListGenericFilterDefinitionSettings : GenericBEFilterDefinitionSettings
+	{
+		public List<GenericFilterDefinitionSettings> ListGenericFilters { get; set; }
+
+		public override Guid ConfigId { get { return new Guid("F5EC6398-6E04-4E2F-8CAB-4DF004F9839E"); } }
+
+		public override string RuntimeEditor { get { return "vr-genericdata-genericbe-listfilterruntime-generic"; } }
+	}
+}
