89a9ec1e69016063ec6e5f060fa78860cc93604e
Zeinab Mokdad <VANRISE\zeinab.mokdad>
2019-07-24 07:06:30 +0000

    adding wholedealDataProviderSettings
    git-tfs-id: [http://developmentvlan:8585/tfs/vanrise.collection]$/;C57292

---
+++ TOneV2/Code/TOneV2/TOne.WhS.Deal.Business/Extensions/WholeDealProgressDataProviderSettings.cs
@@ -0,0 +1,91 @@
+﻿using System;
+using Vanrise.Common;
+using TOne.WhS.Deal.Entities;
+using System.Collections.Generic;
+using Vanrise.GenericData.Business;
+
+namespace TOne.WhS.Deal.Business
+{
+    public class WholeDealProgressDataProviderSettings : BusinessObjectDataProviderExtendedSettings
+    {
+        public override Guid ConfigId => new Guid("3F99CF31-4298-482D-9E1C-BA0F3005F9CC");
+
+        public override bool DoesSupportFilterOnAllFields => false;
+
+        public override void LoadRecords(IBusinessObjectDataProviderLoadRecordsContext context)
+        {
+            if (!context.FromTime.HasValue)
+                throw new NullReferenceException("context.FromTime");
+
+            if (!context.ToTime.HasValue)
+                throw new NullReferenceException("context.ToTime");
+
+            var swapDealManager = new SwapDealManager();
+            IEnumerable<DealDefinition> deals = swapDealManager.GetSwapDealsBetweenDate(context.FromTime.Value, context.ToTime.Value);
+
+            DateTime minDealBED = DateTime.Now;
+            List<int> dealIds = new List<int>();
+            foreach (var deal in deals)
+            {
+                dealIds.Add(deal.DealId);
+                minDealBED = minDealBED < deal.Settings.BeginDate ? minDealBED : deal.Settings.BeginDate;
+            }
+
+            var dealDetailedProgressManager = new DealDetailedProgressManager();
+            IEnumerable<DealDetailedProgress> dealDetailProgresses = dealDetailedProgressManager.GetDealsDetailedProgress(dealIds, minDealBED, context.ToTime.Value);
+
+            var dealDefinitionManager = new DealDefinitionManager();
+            var dealProgressByDealIds = new Dictionary<int, DealProgress>();
+            foreach (var dealDetailProgress in dealDetailProgresses)
+            {
+                var dealId = dealDetailProgress.DealId;
+                if (!dealProgressByDealIds.TryGetValue(dealId, out var dealProgress))
+                {
+                    DealDefinition dealDefinition = dealDefinitionManager.GetDeal(dealId);
+                    dealProgress = new DealProgress
+                    {
+                        DealID = dealId,
+                        DealBED = dealDefinition.Settings.RealBED,
+                        DealEED = dealDefinition.Settings.RealEED
+                    };
+                    dealProgressByDealIds.Add(dealId, dealProgress);
+                }
+
+                if (dealDetailProgress.IsSale)
+                    dealProgress.SaleProgressInMinutes += dealDetailProgress.ReachedDurationInSeconds / 60;
+                else
+                    dealProgress.CostProgressInMinutes += dealDetailProgress.ReachedDurationInSeconds / 60;
+                 
+                dealProgress.SaleCostProgressDifference = dealProgress.SaleProgressInMinutes - dealProgress.CostProgressInMinutes;
+            }
+
+            foreach (var dataRecord in dealProgressByDealIds.Values)
+            {
+                context.OnRecordLoaded(DataRecordObjectMapper(dataRecord), DateTime.Now);
+            }
+        }
+        private DataRecordObject DataRecordObjectMapper(DealProgress dealProgress)
+        {
+            var swapDealObject = new Dictionary<string, object>
+            {
+                {"Deal", dealProgress.DealID},
+                {"DealBED", dealProgress.DealBED},
+                {"DealEED", dealProgress.DealEED},
+                {"SaleProgressInMinutes", dealProgress.SaleProgressInMinutes},
+                {"CostProgressInMinutes", dealProgress.CostProgressInMinutes},
+                { "DealProgress",dealProgress.SaleCostProgressDifference}
+            };
+            return new DataRecordObject(new Guid("c87d22db-f35a-42e2-b22d-0d376f0e6898"), swapDealObject);
+        }
+
+        private class DealProgress
+        {
+            public int DealID { get; set; }
+            public DateTime DealBED { get; set; }
+            public DateTime? DealEED { get; set; }
+            public decimal SaleProgressInMinutes { get; set; }
+            public decimal CostProgressInMinutes { get; set; }
+            public decimal SaleCostProgressDifference { get; set; }
+        }
+    }
+}
---
+++ TOneV2/Code/TOneV2/TOne.WhS.Deal.Web/WhS_Deal/Directives/Extensions/SwapDealProgress/vr-whs-swapdeal-wholedealprogress-dataprovidersettings.js
@@ -0,0 +1,48 @@
+﻿"use strict";
+app.directive("vrWhsSwapdealWholedealprogressDataprovidersettings", [
+    function () {
+        var directiveDefinitionObject = {
+            restrict: "E",
+            scope: {
+                onReady: "="
+            },
+            controller: function ($scope, $element, $attrs) {
+                var ctrl = this;
+                var businessObject = new BusinessObject($scope, ctrl, $attrs);
+                businessObject.initializeController();
+            },
+            controllerAs: "ctrl",
+            bindToController: true,
+            templateUrl: '/Client/Modules/WhS_Deal/Directives/Extensions/SwapDealProgress/Templates/SwapDealWholeDealProgressDataProviderSettingsTemplate.html'
+        };
+
+
+        function BusinessObject($scope, ctrl, $attrs) {
+            this.initializeController = initializeController;
+
+            function initializeController() {
+
+                $scope.scopeModel = {};
+                defineAPI();
+            }
+
+            function defineAPI() {
+                var api = {};
+
+                api.load = function (payload) {
+                };
+
+                api.getData = function () {
+                    return {
+                        $type: "TOne.WhS.Deal.Business.WholeDealProgressDataProviderSettings, TOne.WhS.Deal.Business"
+                    };
+                };
+
+                if (ctrl.onReady != null)
+                    ctrl.onReady(api);
+            }
+        }
+
+        return directiveDefinitionObject;
+    }
+]);
