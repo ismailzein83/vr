864f27ad0f4daa3cd33c2598f4dee7c78b42449e
Ali Slayman <VANRISE\ali.slayman>
2019-07-24 11:08:19 +0000

    Rename DateTimeValidationSeatEnum to DateTimeValidationTargetEnum
    git-tfs-id: [http://developmentvlan:8585/tfs/vanrise.collection]$/;C57299

--- Vanrise/Code/Vanrise.GenericData.Web/VR_GenericData/Constants/DateTimeValidationTargetEnum.js
+++ Vanrise/Code/Vanrise.GenericData.Web/VR_GenericData/Constants/DateTimeValidationTargetEnum.js
@@ -1,5 +1,5 @@
-﻿app.constant('VR_GenericData_DateTimeValidationSeatEnum', {
+﻿app.constant('VR_GenericData_DateTimeValidationTargetEnum', {
     Today: { value: 0, description: 'Today', showFieldsSelector: false },
     Now: { value: 1, description: 'Now', showFieldsSelector: false},
     SpecificField: { value: 2, description: 'Specific Field', showFieldsSelector: true }
-}); 
+});
