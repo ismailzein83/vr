7972d56d00143e92d775c78a5d6ddcc7608947ec
Wissam Ajamy <VANRISE\wissam.ajamy>
2019-07-29 11:49:09 +0000

    rename config to Web.config.SMS.exclud
    git-tfs-id: [http://developmentvlan:8585/tfs/vanrise.collection]$/;C57390

--- Vanrise/Code/Resources/BatchScripts/TOneV2.SMS/03-WebSite.bat
+++ Vanrise/Code/Resources/BatchScripts/TOneV2.SMS/03-WebSite.bat
@@ -6,8 +6,7 @@ Set PARSEARG="eol=; tokens=1,2,3,4* delims=/, "
 For /F %PARSEARG% %%i in (%CURRDATE%) Do SET YYYYMMDD=%%l%%j%%k
 
 ECHO TOneV2.SMS WebSite
-xcopy "C:\TFS\TOneV2\Code\TOneV2\TOne.Web\Web.config.SMS.exclude" /y /v /z /i /Q /R								"\\192.168.110.185\Fixes\WebSite\TOneV2.SMS\%YYYYMMDD%\"
-rename																											"\\192.168.110.185\Fixes\WebSite\TOneV2.SMS\%YYYYMMDD%\Web.config.SMS.exclude" "Web.Update.SMS.config"
+xcopy "C:\TFS\TOneV2\Code\TOneV2\TOne.Web\Web.config.SMS.exclud" /y /v /z /i /Q /R								"\\192.168.110.185\Fixes\WebSite\TOneV2.SMS\%YYYYMMDD%\Web.config*"
 xcopy "C:\TFS\Vanrise\Code\Vanrise.MobileNetwork.Web\VR_MobileNetwork" /S /E /R /y /v /i /z /Q					"\\192.168.110.185\Fixes\WebSite\TOneV2.SMS\%YYYYMMDD%\Client\Modules\VR_MobileNetwork\"
 xcopy "C:\Publish\TOneV2\bin\Vanrise.MobileNetwork*" /S /E /R /y /v /i /z /Q									"\\192.168.110.185\Fixes\WebSite\TOneV2.SMS\%YYYYMMDD%\Bin\"
 xcopy "C:\TFS\TOneV2\Code\TOneV2\TOne.WhS.SMSBusinessEntity.Web\WhS_SMSBusinessEntity" /S /E /R /y /v /i /z /Q	"\\192.168.110.185\Fixes\WebSite\TOneV2.SMS\%YYYYMMDD%\Client\Modules\WhS_SMSBusinessEntity\"
