22a2346972604bc19229e1fb64d34f1c6ef92da0
Ali Ballouk <VANRISE\ali.ballouk>
2019-07-29 12:48:15 +0000

    fix section header elements  alignment
    git-tfs-id: [http://developmentvlan:8585/tfs/vanrise.collection]$/;C57394

--- Vanrise/Code/Vanrise.Web/Client/Javascripts/Directives/Containers/vr-section-v2.js
+++ Vanrise/Code/Vanrise.Web/Client/Javascripts/Directives/Containers/vr-section-v2.js
@@ -38,10 +38,10 @@ app.directive('vrSectionV2', ['UtilsService', 'MultiTranscludeService', function
 			}
 			var htmlTempalte = '<div class="panel-primary panel-vr ' + focusClass + '"  ng-class="classlevel" >'
 				    + '<div class="panel-heading" ng-init="expandname=true" expanded="{{expandname}}" id="{{sectionId}}" ng-click="$root.addFocusPanelClass($event)">'
-				        + '<span class="glyphicon glyphicon-th-list handeldrag hand-cursor drag-icon" ng-show="dragable || sectionCtrl.enablesortable" ></span>'
+				        + '<span style="padding:0px 2px;" class="glyphicon glyphicon-th-list handeldrag hand-cursor drag-icon" ng-show="dragable || sectionCtrl.enablesortable" ></span>'
                         + '<span style="padding:0px 2px;" class="hand-cursor collapsible-icon glyphicon " ng-show="collapsible" ng-click=" expandname =!expandname " ng-class=" expandname ?\'glyphicon-collapse-up\':\'glyphicon-collapse-down\'" ></span>'
-                        + '<label ng-if="!sectionCtrl.headerEditable">{{ sectionCtrl.header }}</label>'
-                        + '<span  ng-if="sectionCtrl.headerEditable"><span title="{{sectionCtrl.header}}" class="edit-header-container single-line {{dragable || sectionCtrl.enablesortable ? \'share-space\' : \'\'}} {{ !sectionCtrl.header ? \'required-header\' : \'\' }}"   contenteditable="true" ng-model="sectionCtrl.header"></span></span>'
+                        + '<label style="padding:0px 2px;" ng-if="!sectionCtrl.headerEditable">{{ sectionCtrl.header }}</label>'
+                        + '<span style="padding:0px 2px;" ng-if="sectionCtrl.headerEditable"><span title="{{sectionCtrl.header}}" class="edit-header-container single-line {{dragable || sectionCtrl.enablesortable ? \'share-space\' : \'\'}} {{ !sectionCtrl.header ? \'required-header\' : \'\' }}"   contenteditable="true" ng-model="sectionCtrl.header"></span></span>'
                         + '<span ng-if="sectionCtrl.validationContext.validate() != null" class="hand-cursor section-validation-sign"  title="has validation errors!">*</span> '
                         + '<span class="section-menu"  style="position: absolute; right: 9px;top:-1px;" ng-if="sectionCtrl.menuactions.length > 0" > <vr-button type="SectionAction" menuactions="sectionCtrl.menuactions" isasynchronous="true" ></vr-button></span> '
                         + '<span class="warning-icon {{sectionCtrl.menuactions.length > 0 ? \'share-space\' : \'\'}}"  style="position: absolute;top:4px;" ng-style="{\'right\': sectionCtrl.menuactions.length > 0 ? \'80px\' : \'20px\'}" ng-if="sectionCtrl.warning != undefined && sectionCtrl.warning!=\'\'" > <vr-warning  value="{{sectionCtrl.warning}}"></vr-warning></span>'
