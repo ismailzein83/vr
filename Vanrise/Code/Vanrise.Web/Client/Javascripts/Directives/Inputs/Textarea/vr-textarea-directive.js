8f2e1ba200ae35773941e6acf70191646609e426
Ali Ballouk <VANRISE\ali.ballouk>
2019-07-30 10:56:16 +0000

    adding custom height attribute
    git-tfs-id: [http://developmentvlan:8585/tfs/vanrise.collection]$/;C57414

--- Vanrise/Code/Vanrise.Web/Client/Javascripts/Directives/Inputs/Textarea/vr-textarea-directive.js
+++ Vanrise/Code/Vanrise.Web/Client/Javascripts/Directives/Inputs/Textarea/vr-textarea-directive.js
@@ -193,11 +193,15 @@
                     labelTemplate = '';
 
 				if (attrs.trim != undefined)
-					trimExp = 'ng-trim="{{ctrl.trim}}"';
+				    trimExp = 'ng-trim="{{ctrl.trim}}"';
+
+				var heightStyle = "";
+				if (attrs.height != undefined)
+				    heightStyle = "height:" + parseInt(attrs.height) + "px";
 
 				var textboxTemplate = '<div ng-mouseenter="showtd=true" ng-mouseleave="showtd=false" >'
 					+ '<vr-validator validate="ctrl.validate()" vr-textarea-element>'
-					+ '<textarea id="{{ctrl.textAreaGuid}}"' + trimExp + ' tabindex="{{ctrl.tabindex}}" readonly="ctrl.readOnly"  placeholder="{{ctrl.placelHolder}}" ng-readonly="ctrl.readOnly" id="mainInput"  ng-model="ctrl.value" ng-change="ctrl.notifyUserChange()" rows="{{ctrl.rows}}"  class="form-control vanrise-inpute" style="width: 100%; resize: none;" ></textarea>'
+					+ '<textarea id="{{ctrl.textAreaGuid}}"' + trimExp + ' tabindex="{{ctrl.tabindex}}" readonly="ctrl.readOnly"  placeholder="{{ctrl.placelHolder}}" ng-readonly="ctrl.readOnly" id="mainInput"  ng-model="ctrl.value" ng-change="ctrl.notifyUserChange()" rows="{{ctrl.rows}}"  class="form-control vanrise-inpute" style="width: 100%; resize: none;' + heightStyle + ';" ></textarea>'
 					+ '</vr-validator>'
 					+ '<span ng-if="ctrl.hint!=undefined" bs-tooltip class="glyphicon glyphicon-question-sign hand-cursor vr-hint-input" html="true"   placement="bottom"  trigger="hover" ng-mouseenter="ctrl.adjustTooltipPosition($event)"  data-type="info" data-title="{{ctrl.hint}}"></span>';
 				+ '</div>';
