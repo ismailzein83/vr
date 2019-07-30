ddc108e60c5523c0cac8771585e457a7b7de0c9f
Ali Ballouk <VANRISE\ali.ballouk>
2019-07-30 10:57:23 +0000

    adding custom maxheigth attribute
    git-tfs-id: [http://developmentvlan:8585/tfs/vanrise.collection]$/;C57415

--- Vanrise/Code/Vanrise.Web/Client/Javascripts/Directives/Containers/vr-row-directive.js
+++ Vanrise/Code/Vanrise.Web/Client/Javascripts/Directives/Containers/vr-row-directive.js
@@ -10,7 +10,7 @@ app.directive('vrRow', ['$compile', function ($compile) {
             var newElement = '<div class="row' + (removeline != undefined ? ' remove-line' : ' style-row') + '">' + tElement.html() + '</div>';
             //if (removeline === undefined)
             //    newElement += '<div style="padding: 0px 10px;"><div style="width:100%; border-bottom : 1px solid #F0F0F0;"> </div></div>';
-            tElement.html(newElement);            
+            tElement.html(newElement);
         }
     };
 
@@ -25,11 +25,16 @@ app.directive('vrScrollableContainer', ['$compile', 'MobileService', function ($
         restrict: 'E',
         scope: false,
         compile: function (tElement, tAttrs) {
-            var margin = parseInt(tAttrs.margin);
+            var margin = parseInt(tAttrs.margin) || 0;
+            var maxheigth = parseInt(tAttrs.maxheigth);
             var containerHeightTrim = MobileService.isMobile() ? 63 : 122;
             var maxHeigth = window.innerHeight - (containerHeightTrim + margin);
-            var finalMaxHeigth = maxHeigth > 200 && maxHeigth || 200;
-            var newElement = '<div style="max-height:' + finalMaxHeigth + 'px;overflow-y:auto;overflow-x:hidden;">' + tElement.html() + '</div>';
+            var finalMaxHeigth;
+            if (maxheigth)
+                finalMaxHeigth = maxheigth;
+            else
+                finalMaxHeigth = maxHeigth > 200 && maxHeigth || 200;
+            var newElement = '<div style="max-height:' + finalMaxHeigth + 'px;overflow-y:auto;overflow-x:auto;">' + tElement.html() + '</div>';
             tElement.html(newElement);
         }
     };
