21a568313c805244004acaa7a973383f200a959f
Fatima Al Heck <VANRISE\fatima.alheck>
2019-08-01 07:43:44 +0000

    Adding empty files for implementing "GenericBEUploadCustomAction"
    git-tfs-id: [http://developmentvlan:8585/tfs/vanrise.collection]$/;C57443

---
+++ Vanrise/Code/Vanrise.GenericData.MainExtensions/GenericBusinessEntity/GenericBECustomAction/GenericBEUploadCustomAction.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Vanrise.GenericData.Business;
+
+namespace Vanrise.GenericData.MainExtensions.GenericBusinessEntity.GenericBECustomAction
+{
+    class GenericBEUploadCustomAction : GenericBECustomActionSettings
+    {
+        public override Guid ConfigId => new Guid ("{1F7BEC62-A10C-4F3E-AC71-BDB0A7B670FF}");
+
+        public override string ActionTypeName => "UploadCustomAction";
+    }
+}
---
+++ Vanrise/Code/Vanrise.GenericData.Web/VR_GenericData/Directives/GenericBusinessEntity/Definition/GenericBECustomActionDefinition/vr-genericdata-genericbe-upload-customaction-definition.js
@@ -0,0 +1,60 @@
+﻿(function (app) {
+
+    'use strict';
+
+    UploadGenericBEDefinitionCustomActionDirective.$inject = ['UtilsService', 'VRUIUtilsService', 'VRNotificationService'];
+
+    function UploadGenericBEDefinitionCustomActionDirective(UtilsService, VRUIUtilsService, VRNotificationService) {
+        return {
+            restrict: 'E',
+            scope: {
+                onReady: '=',
+            },
+            controller: function ($scope, $element, $attrs) {
+                var ctrl = this;
+                var ctor = new AddCustomActionCtor($scope, ctrl);
+                ctor.initializeController();
+            },
+            controllerAs: 'ctrl',
+            bindToController: true,
+            templateUrl: '/Client/Modules/VR_GenericData/Directives/GenericBusinessEntity/Definition/GenericBECustomActionDefinition/Templates/UploadGenericBEDefinitionCustomActionTemplate.html'
+        };
+
+        function AddCustomActionCtor($scope, ctrl) {
+
+            this.initializeController = initializeController;
+
+            function initializeController() {
+                $scope.scopeModel = {};
+
+                defineAPI();
+            }
+
+
+            function defineAPI() {
+                var api = {};
+          
+                api.load = function (payload) {
+
+                    if (payload != undefined) {
+                      
+                    }
+                    var promises = [];
+
+                    return UtilsService.waitPromiseNode(promises);
+                };
+
+                api.getData = function () {
+                   
+                };
+                if (ctrl.onReady != undefined && typeof (ctrl.onReady) == 'function') {
+                    ctrl.onReady(api);
+                }
+            }
+
+        }
+    }
+
+    app.directive('vrGenericdataGenericbeUploadCustomactionDefinition', UploadGenericBEDefinitionCustomActionDirective);
+
+})(app);
