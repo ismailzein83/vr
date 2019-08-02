bca2cece081be78075efcdff89e4ed61b496c27c
Zeinab Mokdad <VANRISE\zeinab.mokdad>
2019-08-02 11:39:23 +0000

    adding TOne.WhS.BusinessEntity.BP.Arguments and TOne.WhS.BusinessEntity.BP
    git-tfs-id: [http://developmentvlan:8585/tfs/vanrise.collection]$/;C57462

---
+++ TOneV2/Code/TOneV2/TOne.WhS.BusinessEntity.Arguments/SalePricelistEmailSenderProcessInput .cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace TOne.WhS.BusinessEntity.BP.Arguments
+{
+    public class SalePricelistEmailSenderProcessInput : Vanrise.BusinessProcess.Entities.BaseProcessInputArgument
+    {
+        public List<int> CustomerIds { get; set; }
+        public Period Period { get; set; }
+
+        public override string GetTitle()
+        {
+            return String.Format("#BPDefinitionTitle#");
+        }
+    }
+    public enum Period
+    {
+        Daily = 0,
+        Monthly = 1,
+        Yearly = 2
+    }
+}
---
+++ TOneV2/Code/TOneV2/TOne.WhS.BusinessEntity.Web/WhS_BusinessEntity/Directives/ScheduleTask/vr-whs-be-salepricelistemailsendertask.js
@@ -0,0 +1,60 @@
+﻿"use strict";
+app.directive("vWhsBeSalepricelistemailsendertask", ['UtilsService',
+    function (UtilsService) {
+        var directiveDefinitionObject = {
+            restrict: "E",
+            scope: {
+                onReady: "="
+            },
+            controller: function ($scope, $element, $attrs) {
+                var ctrl = this;
+
+                var directiveConstructor = new DirectiveConstructor($scope, ctrl);
+                directiveConstructor.initializeController();
+            },
+            controllerAs: "ctrl",
+            bindToController: true,
+            compile: function (element, attrs) {
+                return {
+                    pre: function ($scope, iElem, iAttrs, ctrl) {
+
+                    }
+                };
+            },
+            templateUrl: "/Client/Modules/WhS_BusinessEntity/Directives/ScheduleTask/Templates/SalePricelistEmailSenderTaskTemplate.html"
+        };
+
+        function DirectiveConstructor($scope, ctrl) {
+            this.initializeController = initializeController;
+
+            function initializeController() {
+                defineAPI();
+            }
+
+            function defineAPI() {
+
+                var api = {};
+
+                api.getData = function () {
+                    return {
+                        $type: 'TOne.WhS.BusinessEntity.BP.Arguments.SalePricelistEmailSenderProcessInput, TOne.WhS.BusinessEntity.BP.Arguments'
+                    };
+                };
+
+                api.getExpressionsData = function () {
+                    return null;
+                };
+
+                api.load = function (payload) {
+                    var promises = [];
+
+                    return UtilsService.waitMultiplePromises(promises);
+                };
+
+                if (ctrl.onReady != null)
+                    ctrl.onReady(api);
+            }
+        }
+
+        return directiveDefinitionObject;
+    }]);
