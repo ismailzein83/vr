fb35be820a7d39f82b3c7fda94628e1d5459d886
Ali Sakr <VANRISE\ali.sakr>
2019-08-14 11:21:18 +0000

    adding new views for overall and deleting the not needed
    git-tfs-id: [http://developmentvlan:8585/tfs/vanrise.collection]$/;C57543

---
+++ Retail/Retail.SQL/SQL.RA.ICX.Analytics/RA_ICX_Analytics/Views/vw_OverallBillingStats.sql
@@ -0,0 +1,10 @@
+﻿
+CREATE VIEW [RA_ICX_Analytics].[vw_OverallBillingStats]
+AS
+SELECT        voice.[BatchStart], voice.[OperatorID], voice.[NumberOfCDRs], 0 AS NumberOfSMSs, voice.[TrafficDirection], voice.[Rate], voice.[TotalRevenue], voice.[TotalIncome], voice.[TotalDurationInSeconds], 
+                         voice.[TotalSaleDurationInSeconds], voice.[InterconnectOperatorID], voice.[CurrencyID]
+/*		,voice.[MaximumDurationInSeconds]*/ FROM RA_ICX_Analytics.BillingStatsDaily AS voice
+UNION ALL
+SELECT        sms.[BatchStart], sms.[OperatorID], 0 AS NumberOfCDRs, sms.[NumberOfSMSs], sms.[TrafficDirection], sms.[Rate], sms.[TotalRevenue], sms.[TotalIncome], 0 AS TotalDurationInSeconds, 
+                         0 AS TotalSaleDurationInSeconds, sms.[CurrencyID], sms.[InterconnectOperator]
+/*		, sms.[DestinationMobileCountry]*/ FROM RA_ICX_SMSAnalytics.BillingStatsDaily AS sms
---
+++ Retail/Retail.SQL/SQL.RA.INTL.Analytics/RA_INTL_Analytics/Views/vw_OverallBillingStats.sql
@@ -0,0 +1,10 @@
+﻿
+CREATE VIEW [RA_INTL_Analytics].[vw_OverallBillingStats]
+AS
+SELECT        voice.[BatchStart], voice.[OperatorID], voice.[NumberOfCDRs], 0 AS NumberOfSMSs, voice.[TrafficDirection], voice.[Rate], voice.[TotalRevenue], voice.[TotalIncome], voice.[TotalDurationInSeconds], 
+                         voice.[TotalSaleDurationInSeconds], voice.[CurrencyID]
+/*		,voice.[MaximumDurationInSeconds]*/ FROM RA_INTL_Analytics.BillingStatsDaily AS voice
+UNION ALL
+SELECT        sms.[BatchStart], sms.[OperatorID], 0 AS NumberOfCDRs, sms.[NumberOfSMSs], sms.[TrafficDirection], sms.[Rate], sms.[TotalRevenue], sms.[TotalIncome], 0 AS TotalDurationInSeconds, 
+                         0 AS TotalSaleDurationInSeconds, sms.[CurrencyID]
+/*		, sms.[DestinationMobileCountry]*/ FROM RA_INTL_SMSAnalytics.BillingStatsDaily AS sms
