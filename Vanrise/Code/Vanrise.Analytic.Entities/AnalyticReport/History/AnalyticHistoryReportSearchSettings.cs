dcaa01b7c9cd9f7fa06a604e831ac2c0d5c4eeb2
Ali Atoui <VANRISE\ali.atoui>
2019-08-14 14:08:49 +0000

    Arranging AnalyticHistoryReportSearchSettings
    git-tfs-id: [http://developmentvlan:8585/tfs/vanrise.collection]$/;C57552

--- Vanrise/Code/Vanrise.Analytic.Entities/AnalyticReport/History/AnalyticHistoryReportSearchSettings.cs
+++ Vanrise/Code/Vanrise.Analytic.Entities/AnalyticReport/History/AnalyticHistoryReportSearchSettings.cs
@@ -1,28 +1,27 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace Vanrise.Analytic.Entities
 {
     public abstract class AnalyticHistoryReportSearchSettings
     {
         public abstract Guid ConfigId { get; }
+
         public virtual void ApplyTranslation(IAnalyticHistoryReportTranslationContext context)
         {
-
-
         }
     }
+
     public interface IAnalyticHistoryReportTranslationContext
     {
-         Guid LanguageId { get;}
-         Guid AnalyticTableId { get;}
+        Guid LanguageId { get; }
+
+        Guid AnalyticTableId { get; }
     }
-    public class AnalyticHistoryReportTranslationContext: IAnalyticHistoryReportTranslationContext
+
+    public class AnalyticHistoryReportTranslationContext : IAnalyticHistoryReportTranslationContext
     {
         public Guid LanguageId { get; set; }
+
         public Guid AnalyticTableId { get; set; }
     }
 }
