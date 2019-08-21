ef78a865f3b5516805a0955d531d917b070b2587
Wissam Ajamy <VANRISE\wissam.ajamy>
2019-08-21 08:09:44 +0000

    Update structure
    git-tfs-id: [http://developmentvlan:8585/tfs/vanrise.collection]$/;C57640

--- TOneV2/Code/TOneV2/SQL.TOneConfiguration/common/Stored Procedures/sp_DataEncryptionKey_InsertIfNotExistsAndGetDataEncryptionKey.sql
+++ TOneV2/Code/TOneV2/SQL.TOneConfiguration/common/Stored Procedures/sp_DataEncryptionKey_InsertIfNotExistsAndGetDataEncryptionKey.sql
@@ -1,15 +1,15 @@
-﻿create PROCEDURE [Common].[sp_DataEncryptionKey_InsertIfNotExistsAndGetDataEncryptionKey]
+﻿CREATE PROCEDURE [common].[sp_DataEncryptionKey_InsertIfNotExistsAndGetDataEncryptionKey]
 	@EncryptionKey nvarchar(255)
 AS
 BEGIN	
 	
 	--INSERT Encryption Key if not exists
-	IF NOT EXISTS (SELECT TOP 1 NULL FROM [Common].DataEncryptionKey WITH(NOLOCK))
+	IF NOT EXISTS (SELECT TOP 1 NULL FROM [common].[DataEncryptionKey] WITH(NOLOCK))
 	BEGIN
-		INSERT INTO [Common].DataEncryptionKey ([EncryptionKey])
-		SELECT @EncryptionKey WHERE NOT EXISTS (SELECT TOP 1 NULL FROM [Common].DataEncryptionKey)
+		INSERT INTO [common].[DataEncryptionKey] ([EncryptionKey])
+		SELECT @EncryptionKey WHERE NOT EXISTS (SELECT TOP 1 NULL FROM [common].[DataEncryptionKey])
 	END
 	
-	SELECT TOP 1 EncryptionKey FROM [Common].DataEncryptionKey
+	SELECT TOP 1 EncryptionKey FROM [common].[DataEncryptionKey]
 	ORDER BY ID
 END
