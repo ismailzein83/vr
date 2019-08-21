b2763f24214d46fd0662dcc25281fad4ddc2dac0
Wissam Ajamy <VANRISE\wissam.ajamy>
2019-08-21 12:01:52 +0000

    Adding Retail_RecordAnalysis.PostDeployment.json
    git-tfs-id: [http://developmentvlan:8585/tfs/vanrise.collection]$/;C57652

--- Vanrise/Code/Resources/BatchScripts/Inspkt/02-DataBaseFirstDeploymentScripts.bat
+++ Vanrise/Code/Resources/BatchScripts/Inspkt/02-DataBaseFirstDeploymentScripts.bat
@@ -24,7 +24,7 @@ xcopy "C:\TFS\TOneV2\Code\TOneV2\SQL.TOneConfiguration\VR_NumberingPlan.PostDepl
 xcopy "C:\TFS\TOneV2\Code\TOneV2\SQL.TOneConfiguration\Reprocess.PostDeployment.sql" 				/y /v /z /i /Q /R  "\\192.168.110.185\Fixes\WebSite\Inspkt\%YYYYMMDD%\DataBaseFirstDeploymentScripts\Configuration\"
 xcopy "C:\TFS\TOneV2\Code\TOneV2\SQL.TOneConfiguration\Retail.PostDeployment.sql" 					/y /v /z /i /Q /R  "\\192.168.110.185\Fixes\WebSite\Inspkt\%YYYYMMDD%\DataBaseFirstDeploymentScripts\Configuration\"
 xcopy "C:\TFS\TOneV2\Code\TOneV2\SQL.TOneConfiguration\Retail.SwitchReleaseCause.PostDeployment.sql"/y /v /z /i /Q /R  "\\192.168.110.185\Fixes\WebSite\Inspkt\%YYYYMMDD%\DataBaseFirstDeploymentScripts\Configuration\"
-xcopy "C:\TFS\TOneV2\Code\TOneV2\SQL.TOneConfiguration\Retail_RecordAnalysis.PostDeployment.sql" 	/y /v /z /i /Q /R  "\\192.168.110.185\Fixes\WebSite\Inspkt\%YYYYMMDD%\DataBaseFirstDeploymentScripts\Configuration\"
+xcopy "C:\TFS\TOneV2\Code\TOneV2\SQL.TOneConfiguration\Retail_RecordAnalysis.PostDeployment.json" 	/y /v /z /i /Q /R  "\\192.168.110.185\Fixes\WebSite\Inspkt\%YYYYMMDD%\DataBaseFirstDeploymentScripts\Configuration\"
 xcopy "C:\TFS\TOneV2\Code\TOneV2\SQL.TOneConfiguration\ProbesAnalytics.PostDeployment.sql" 			/y /v /z /i /Q /R  "\\192.168.110.185\Fixes\WebSite\Inspkt\%YYYYMMDD%\DataBaseFirstDeploymentScripts\Configuration\"
 xcopy "C:\TFS\TOneV2\Code\TOneV2\SQL.TOneConfiguration\Configuration.txt" 							/y /v /z /i /Q /R  "\\192.168.110.185\Fixes\WebSite\Inspkt\%YYYYMMDD%\DataBaseFirstDeploymentScripts\Configuration\"
 xcopy "C:\TFS\TOneV2\Code\TOneV2\SQL.TOneTransaction\BusinessProcess.PostDeployment.sql" 			/y /v /z /i /Q /R  "\\192.168.110.185\Fixes\WebSite\Inspkt\%YYYYMMDD%\DataBaseFirstDeploymentScripts\Transaction\"
