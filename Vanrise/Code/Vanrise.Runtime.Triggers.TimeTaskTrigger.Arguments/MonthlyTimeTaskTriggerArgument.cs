a36e8fa6f3dce6d47eb5083faca2132c78f9905d
Mohamad Kalakesh <VANRISE\mohamad.kalakesh>
2019-08-23 12:22:52 +0000

    Adding MonthlyTimeTaskTriggerArgument - MonthlyTimeSchedulerTaskTrigger
    git-tfs-id: [http://developmentvlan:8585/tfs/vanrise.collection]$/;C57713

---
+++ Vanrise/Code/Vanrise.Runtime.Triggers.TimeTaskTrigger.Arguments/MonthlyTimeTaskTriggerArgument.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Vanrise.Runtime.Triggers.TimeTaskTrigger.Arguments
+{
+    public enum MonthlyEnum { SpecificDay, LastDay }
+    public class MonthlyTimeTaskTriggerArgument : DailyTimeTaskTriggerArgument
+    {
+        public List<DayOfMonth> ScheduledDays { get; set; }
+    }
+
+    public class DayOfMonth
+    {
+        public MonthlyEnum MonthlyValue { get; set; }
+
+        public int SpecificDay { get; set; }
+    }
+}
