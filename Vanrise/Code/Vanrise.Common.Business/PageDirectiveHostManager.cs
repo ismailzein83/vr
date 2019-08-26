6ff0191ed6c59f26973fa7f85546724fde0dac6a
Ali Ballouk <VANRISE\ali.ballouk>
2019-08-26 07:18:27 +0000

    Adding PageDirectiveHost base back-end files
    git-tfs-id: [http://developmentvlan:8585/tfs/vanrise.collection]$/;C57726

---
+++ Vanrise/Code/Vanrise.Common.Business/PageDirectiveHostManager.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Vanrise.Common;
+using Vanrise.Entities;
+using System.Globalization;
+using Vanrise.Common.Business;
+using System.ComponentModel;
+using Vanrise.Security.Entities;
+
+namespace Vanrise.Common.Business
+{
+    public class PageDirectiveHostManager
+    {
+        #region Public Methods
+
+        public bool DoesUserHaveViewAccess(PageDirectiveHost pageHost)
+        {
+            return DoesUserHaveAccess(pageHost.PermissionName);
+        }
+
+        public PageDirectiveHostInfo GetPageDirectiveHostInfo(Guid viewId)
+        {
+
+            View view = BEManagerFactory.GetManager<IViewManager>().GetView(viewId);
+            PageDirectiveHostViewSettings settings = view.Settings as PageDirectiveHostViewSettings;
+            settings.PageDirectiveHost.ThrowIfNull("Page Directive Host");
+            return PageDirectiveHostInfoMapper(settings.PageDirectiveHost);
+
+        }
+
+        #endregion
+
+        #region Private Methods
+
+        private bool DoesUserHaveAccess(string permissionName)
+        {
+            return ContextFactory.GetContext().IsAllowed(permissionName);
+        }
+
+        PageDirectiveHostInfo PageDirectiveHostInfoMapper(PageDirectiveHost PageDirectiveHost)
+        {
+            return new PageDirectiveHostInfo()
+            {
+                Title = PageDirectiveHost.Title,
+                Directive = PageDirectiveHost.Directive,
+                Data = PageDirectiveHost.Data
+            };
+        }
+        #endregion
+    }
+
+}
---
+++ Vanrise/Code/Vanrise.Common.Business/PageDirectiveHostViewSettings.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Vanrise.Security.Entities;
+
+namespace Vanrise.Common.Business
+{
+    public class PageDirectiveHostViewSettings : ViewSettings
+    {
+
+        public PageDirectiveHost PageDirectiveHost { get; set; }
+        public override string GetURL(Security.Entities.View view)
+        {
+            return String.Format("#/viewwithparams/Common/Views/PageDirectiveHost/PageDirectiveHostManagement/{{\"viewId\":\"{0}\"}}", view.ViewId);
+        }
+
+        public override bool DoesUserHaveAccess(IViewUserAccessContext context)
+        {
+            return new PageDirectiveHostManager().DoesUserHaveViewAccess(this.PageDirectiveHost);
+        }
+    }
+
+    public class PageDirectiveHost
+    {
+        public string PermissionName { set; get; }
+        public string Directive { get; set; }
+        public string Title { get; set; }
+        public PageHostData Data { get; set; }
+    }
+     
+    public class PageDirectiveHostInfo
+    {
+        public string Directive { get; set; }
+
+        public string Title { get; set; }
+        public PageHostData Data { get; set; }
+
+    }
+
+    public abstract class PageHostData{ };
+}
