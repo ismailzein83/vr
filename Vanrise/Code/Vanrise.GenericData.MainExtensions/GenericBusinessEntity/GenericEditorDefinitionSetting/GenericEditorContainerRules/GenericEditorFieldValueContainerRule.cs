a9260f3e6427afc6197ddf31e7f5838fb4dbcfe9
Ali Slayman <VANRISE\ali.slayman>
2019-08-27 09:48:48 +0000

    Add LogicalOperator to GenericEditorFieldValueConditionalRule class
    git-tfs-id: [http://developmentvlan:8585/tfs/vanrise.collection]$/;C57757

--- Vanrise/Code/Vanrise.GenericData.MainExtensions/GenericBusinessEntity/GenericEditorDefinitionSetting/GenericEditorContainerRules/GenericEditorFieldValueContainerRule.cs
+++ Vanrise/Code/Vanrise.GenericData.MainExtensions/GenericBusinessEntity/GenericEditorDefinitionSetting/GenericEditorContainerRules/GenericEditorFieldValueContainerRule.cs
@@ -1,13 +1,18 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel;
+using Vanrise.GenericData.Entities;
 
 namespace Vanrise.GenericData.MainExtensions
 {
+
     public class GenericEditorFieldValueConditionalRule : GenericEditorConditionalRule
     {
         public override Guid ConfigId { get { return new Guid("A8811BE9-6311-4FB8-9F88-71FD015F639A"); } }
 
         public override string ActionName { get { return "FieldValueConditionalRuleAction"; } }
+
+        public RecordQueryLogicalOperator LogicalOperator { get; set; }
 
         public List<GenericEditorFieldValueCondition> Conditions { get; set; }
     }
