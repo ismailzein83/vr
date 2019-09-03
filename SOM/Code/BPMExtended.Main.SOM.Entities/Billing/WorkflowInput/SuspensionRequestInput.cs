7f4116ed6cb2d8db26dfa0f68b55ddd00eee37c8
Hassan Makhour <VANRISE\hassan.makhour>
2019-09-03 09:02:33 +0000

    Suspension Request Input
    git-tfs-id: [http://developmentvlan:8585/tfs/vanrise.collection]$/;C57826

---
+++ SOM/Code/BPMExtended.Main.SOM.Entities/Billing/WorkflowInput/SuspensionRequestInput.cs
@@ -0,0 +1,17 @@
+﻿using BPMExtended.Main.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BPMExtended.Main.SOMAPI
+{
+   public class SuspensionRequestInput
+    {
+        public CommonInputArgument CommonInputArgument { get; set; }
+        public PaymentData PaymentData { get; set; }
+        public string PhoneNumber { get; set; }
+        public string RequestId { get; set; }
+    }
+}
