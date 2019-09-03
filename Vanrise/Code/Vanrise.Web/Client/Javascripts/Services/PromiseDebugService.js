f984e7e85bc0b91e442e3985e5b11a1849ef09d7
Ali Slayman <VANRISE\ali.slayman>
2019-09-03 06:44:11 +0000

    Add PromiseDebugService
    git-tfs-id: [http://developmentvlan:8585/tfs/vanrise.collection]$/;C57822

---
+++ Vanrise/Code/Vanrise.Web/Client/Javascripts/Services/PromiseDebugService.js
@@ -0,0 +1,64 @@
+﻿(function (appControllers) {
+
+    "use strict";
+
+    //PromiseDebugService.$inject = [];
+
+    function PromiseDebugService() {
+
+        var registeredPromises = [];
+
+        function registerPromise(promiseObj) {
+
+            registeredPromises.push(promiseObj);
+
+            registredVerbose(promiseObj.name, "color: blue");
+
+            promiseObj.promise.then(function () {
+                var pendingPromisesName = [];
+                var resolvedPromises = [];
+                var pendingPromises = [];
+
+                if (registeredPromises.length > 0) {
+                    for (var i = 0; i < registeredPromises.length; i++) {
+                        var currentPromiseObj = registeredPromises[i];
+                        if (currentPromiseObj.promise["$$state"].status == 0) {
+                            pendingPromisesName.push(currentPromiseObj.name);
+                            pendingPromises.push(currentPromiseObj);
+                        }
+                        else {
+                            resolvedPromises.push(currentPromiseObj.name);
+                        }
+                    }
+                }
+
+                registeredPromises = pendingPromises;
+
+                if (pendingPromisesName.length == 0) {
+                    allResolvedVerbose(resolvedPromises, "color: green");
+                }
+                else {
+                    resolvedVerbose(resolvedPromises, pendingPromisesName, "color: green", "color: red");
+                }
+            });
+        }
+
+        function registredVerbose(registredPromiseName, color) {
+            console.debug("Registred: %c" + registredPromiseName, color);
+        }
+
+        function resolvedVerbose(resolvedPromisesName, pendingPromisesName, resolvedColor, pendingColor) {
+            console.debug("Resolved: %c" + resolvedPromisesName + "  %c|  Pending: %c" + pendingPromisesName, resolvedColor, "color: black", pendingColor);
+        }
+
+        function allResolvedVerbose(lastResolvedPromiseName, color) {
+            console.debug("%cResolved: " + lastResolvedPromiseName + "  |  No Pending Promises", color);
+        }
+
+        return ({
+            registerPromise: registerPromise
+        });
+    }
+    appControllers.service('VRPromiseDebugService', PromiseDebugService);
+
+})(appControllers);
