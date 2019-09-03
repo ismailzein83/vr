5092ea41f8807394513513a122a1e6fa4fc16276
Ali Slayman <VANRISE\ali.slayman>
2019-09-03 09:10:52 +0000

    Add 360 Degree View Editors and Directives
    git-tfs-id: [http://developmentvlan:8585/tfs/vanrise.collection]$/;C57827

---
+++ Vanrise/Code/Vanrise.GenericData.Web/VR_GenericData/Directives/GenericBusinessEntity/Runtime/Management/vr-genericdata-genericbusinessentity-360degree-viewer.js
@@ -0,0 +1,129 @@
+﻿(function (app) {
+
+    'use strict';
+
+    GenericBE360DegreeViewer.$inject = ['UtilsService', 'VRUIUtilsService', 'VR_GenericData_GenericBEDefinitionAPIService'];
+
+    function GenericBE360DegreeViewer(UtilsService, VRUIUtilsService, VR_GenericData_GenericBEDefinitionAPIService) {
+        return {
+            restrict: "E",
+            scope: {
+                onReady: "="
+            },
+            controller: function ($scope, $element, $attrs) {
+                var ctrl = this;
+                var ctor = new GenericBE360DegreeViewer($scope, ctrl, $attrs);
+                ctor.initializeController();
+            },
+            controllerAs: "ctrl",
+            bindToController: true,
+
+            templateUrl: "/Client/Modules/VR_GenericData/Directives/GenericBusinessEntity/Runtime/Management/Templates/GenericBE360DegreeViewerTemplate.html"
+        };
+
+        function GenericBE360DegreeViewer($scope, ctrl) {
+            this.initializeController = initializeController;
+
+            var idFieldName;
+            var businessEntityDefinitionId;
+            var genericBEDefinitionSettings;
+            
+
+            var gridDirectiveAPI;
+            var gridDirectiveReadyDeferred = UtilsService.createPromiseDeferred();
+
+            function initializeController() {
+                $scope.scopeModel = {};
+
+                $scope.scopeModel.onGridDirectiveReady = function (api) {
+                    gridDirectiveAPI = api;
+                    gridDirectiveReadyDeferred.resolve();
+                };
+
+                defineAPI();
+            }
+
+            function defineAPI() {
+                var api = {};
+
+                api.load = function (payload) {
+                    var initialPromises = [];
+
+                    if (payload != undefined) {
+                       var parentBusinessEntityDefinitionId = payload.parentBusinessEntityDefinitionId;
+                       var genericBusinessEntityId = payload.genericBusinessEntityId;
+                       var fieldName = payload.fieldName;
+                       var modalContext = payload.modalContext;
+                        var hideCloseButton = payload.hideCloseButton;
+                    }
+
+                    var getGenericBEField360DegreeViewDataPromise = getGenericBEField360DegreeViewData();
+                    initialPromises.push(getGenericBEField360DegreeViewDataPromise);
+
+                    var rootPromiseNode = {
+                        promises: initialPromises,
+                        getChildNode: function () {
+                            var directivePromises = [loadGrid()];
+                            return {
+                                promises: directivePromises
+                            };
+                        }
+                    };
+
+                    function getGenericBEField360DegreeViewData() {
+                        var getGenericBEField360DegreeViewDataLoadDeferred = UtilsService.createPromiseDeferred();
+
+                        var input = {
+                            ParentBusinessEntityDefinitionId: parentBusinessEntityDefinitionId,
+                            FieldName: fieldName
+                        };
+
+                        VR_GenericData_GenericBEDefinitionAPIService.GetGenericBEField360DegreeViewData(input).then(function (response) {
+                            if (response) {
+                                businessEntityDefinitionId = response.BusinessEntityDefinitionId;
+                                idFieldName = response.IdFieldName;
+                                genericBEDefinitionSettings = response.GenericBEDefinitionSettings;
+                            }
+
+                            getGenericBEField360DegreeViewDataLoadDeferred.resolve();
+                        });
+
+                        return getGenericBEField360DegreeViewDataLoadDeferred.promise;
+                    }
+
+                    function loadGrid() {
+                        var gridDirectiveLoadDeferred = UtilsService.createPromiseDeferred();
+
+                        gridDirectiveReadyDeferred.promise.then(function () {
+                            var gridPayload = {
+                                query: {
+                                    FilterGroup: undefined,
+                                    Filters: [{ FieldName: idFieldName, FilterValues: [genericBusinessEntityId] }],
+                                    OrderType: genericBEDefinitionSettings.OrderType,
+                                    AdvancedOrderOptions: genericBEDefinitionSettings.AdvancedOrderOptions
+                                },
+                                businessEntityDefinitionId: businessEntityDefinitionId,
+                                modalContext: modalContext,
+                                fullScreenModeOn: true,
+                                hideCloseButton: hideCloseButton
+                            };
+
+                            VRUIUtilsService.callDirectiveLoad(gridDirectiveAPI, gridPayload, gridDirectiveLoadDeferred);
+                        });
+
+                        return gridDirectiveLoadDeferred.promise;
+                    }
+
+                    return UtilsService.waitPromiseNode(rootPromiseNode);
+                };
+
+                if (ctrl.onReady != undefined && typeof (ctrl.onReady) == 'function') {
+                    ctrl.onReady(api);
+                }
+            }
+        }
+    }
+
+    app.directive('vrGenericdataGenericbusinessentity360degreeViewer', GenericBE360DegreeViewer);
+
+})(app);
---
+++ Vanrise/Code/Vanrise.GenericData.Web/VR_GenericData/Views/GenericBusinessEntity/Runtime/GenericBEThreeSixtyDegreeViewManagementController.js
@@ -0,0 +1,86 @@
+﻿(function (appControllers) {
+
+    'use strict';
+
+    GenericBEThreeSixtyDegreeViewManagementController.$inject = ['$scope', 'VR_GenericData_GenericBEDefinitionAPIService', 'VR_GenericData_GenericBusinessEntityAPIService', 'UtilsService', 'VRUIUtilsService', 'VRNavigationService', 'VRNotificationService'];
+
+    function GenericBEThreeSixtyDegreeViewManagementController($scope, VR_GenericData_GenericBEDefinitionAPIService, VR_GenericData_GenericBusinessEntityAPIService, UtilsService, VRUIUtilsService, VRNavigationService, VRNotificationService) {
+
+        var genericBusinessEntityId;
+        var parentBusinessEntityDefinitionId;
+        var fieldName;
+
+        var businessEntityDefinitionId;
+
+        var genericBE360degreeViewerDirectiveAPI;
+        var genericBE360degreeViewerDirectiveReadyDeferred = UtilsService.createPromiseDeferred();
+
+        loadParameters();
+        defineScope();
+        load();
+
+        function loadParameters() {
+            var parameters = VRNavigationService.getParameters($scope);
+
+            if (parameters != undefined) {
+                parentBusinessEntityDefinitionId = parameters.parentBusinessEntityDefinitionId;
+                genericBusinessEntityId = parameters.genericBusinessEntityId;
+                fieldName = parameters.fieldName;
+            }
+        }
+
+        function defineScope() {
+            $scope.scopeModel = {};
+
+            $scope.scopeModel.onGenericBE360degreeViewerDirectiveReady = function (api) {
+                genericBE360degreeViewerDirectiveAPI = api;
+                genericBE360degreeViewerDirectiveReadyDeferred.resolve();
+            };
+            
+            $scope.scopeModel.close = function () {
+                $scope.modalContext.closeModal();
+            };
+        }
+
+        function load() {
+            $scope.scopeModel.isLoading = true;
+            loadAllControls();
+        }
+
+        function loadAllControls() {
+
+            function loadGenericBE360degreeViewerDirective() {
+                var genericBE360degreeViewerDirectiveLoadDeferred = UtilsService.createPromiseDeferred();
+
+                genericBE360degreeViewerDirectiveReadyDeferred.promise.then(function () {
+                    var genericBE360degreeViewerDirectivePayload = {
+                        parentBusinessEntityDefinitionId: parentBusinessEntityDefinitionId,
+                        genericBusinessEntityId: genericBusinessEntityId,
+                        fieldName: fieldName,
+                        modalContext: getModalContext(),
+                        hideCloseButton: true
+                    };
+                    VRUIUtilsService.callDirectiveLoad(genericBE360degreeViewerDirectiveAPI, genericBE360degreeViewerDirectivePayload, genericBE360degreeViewerDirectiveLoadDeferred);
+                });
+
+                return genericBE360degreeViewerDirectiveLoadDeferred.promise;
+            }
+
+            return UtilsService.waitMultipleAsyncOperations([loadGenericBE360degreeViewerDirective]).catch(function (error) {
+                VRNotificationService.notifyExceptionWithClose(error, $scope);
+            }).finally(function () {
+                $scope.scopeModel.isLoading = false;
+            });
+        }
+
+        function getModalContext() {
+            return {
+                closeModal: function () {
+                    $scope.modalContext.closeModal();
+                }
+            };
+        }
+    }
+
+    appControllers.controller('VR_GenericData_GenericBEThreeSixtyDegreeViewManagementController', GenericBEThreeSixtyDegreeViewManagementController);
+})(appControllers);
