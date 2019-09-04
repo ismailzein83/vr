31bc626c18c5685cd3197ec8a5f4607465831c6e
Naim Saad <VANRISE\naim.saad>
2019-09-04 08:06:08 +0000

    update invoice item in order to set TotalFullAmount = TotalAmountAfterCommission in case TotalFullAmount not available
    git-tfs-id: [http://developmentvlan:8585/tfs/vanrise.collection]$/;C57853

--- TOneV2/Code/TOneV2/ResolveInvoiceIssue/Program.cs
+++ TOneV2/Code/TOneV2/ResolveInvoiceIssue/Program.cs
@@ -1,21 +1,29 @@
 ﻿using System;
-using System.Collections.Generic;
 using System.Data;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using TOne.WhS.Invoice.Entities;
 using Vanrise.Common;
 using Vanrise.Data.SQL;
 
 namespace ResolveInvoiceIssue
 {
+    #region Public Classes
+
     public class Invoice
     {
         public long InvoiceId { get; set; }
         public dynamic Details { get; set; }
         public Guid InvoiceTypeId { get; set; }
     }
+
+    public class InvoiceItemDetail
+    {
+        public long InvoiceItemId { get; set; }
+        public Guid InvoiceTypeId { get; set; }
+        public dynamic Details { get; set; }
+    }
+
+    #endregion
+
     class Program
     {
         static void Main(string[] args)
@@ -23,14 +31,6 @@ namespace ResolveInvoiceIssue
 
             new TestMethod().Method();
         }
-        public class Class2
-        {
-            public int ID { get; set; }
-            public int? OrderBy { get; set; }
-            public string Name { get; set; }
-        }
-
-
 
         public class TestMethod : BaseSQLDataManager
         {
@@ -42,17 +42,17 @@ namespace ResolveInvoiceIssue
             {
                 try
                 {
-                    BackUpInvoiceTable();
+                    BackUpInvoiceTables();
                     var customerInvoiceTypeId = new Guid("EADC10C8-FFD7-4EE3-9501-0B2CE09029AD");
                     var supplierInvoiceTypeId = new Guid("5FD8DF00-71E8-4EDB-BBEC-108EE3CD66B3");
                     var invoices = GetItemsText($"Select ID,Details,InvoiceTypeId From  VR_Invoice.Invoice where Details not like '%TotalInvoiceAmount\"%' and invoiceTypeId in ('{supplierInvoiceTypeId}','{customerInvoiceTypeId}')", InvoiceMapper, (cmd) => { });
+
                     if (invoices != null)
                     {
-                        int suplierInvoices = 0;
-                        int customerInvoices = 0;
-                        int invoicesCount = invoices.Count;
+                        int updatedSupplierInvoicesCount = 0;
+                        int updatedcustomerInvoicesCount = 0;
 
-                        Console.WriteLine("{0} invoices to be updated.", invoicesCount);
+                        Console.WriteLine("{0} invoices to be updated.", invoices.Count);
 
                         foreach (var invoice in invoices)
                         {
@@ -62,7 +62,7 @@ namespace ResolveInvoiceIssue
                                 customerInvoiceDetail.TotalInvoiceAmount = customerInvoiceDetail.TotalAmountAfterCommission;
                                 if (ExecuteNonQueryText($"Update  VR_Invoice.Invoice set Details = '{Serializer.Serialize(customerInvoiceDetail)}' where ID = {invoice.InvoiceId}", null) > 0)
                                 {
-                                    customerInvoices++;
+                                    updatedcustomerInvoicesCount++;
                                     Console.WriteLine("InvoiceId {0} updated.", invoice.InvoiceId);
                                 }
                                 else
@@ -77,26 +77,83 @@ namespace ResolveInvoiceIssue
                                 supplierInvoiceDetail.TotalInvoiceAmount = supplierInvoiceDetail.TotalAmountAfterCommission;
                                 if (ExecuteNonQueryText($"Update  VR_Invoice.Invoice set Details = '{Serializer.Serialize(supplierInvoiceDetail)}' where ID = {invoice.InvoiceId}", null) > 0)
                                 {
-                                    suplierInvoices++;
+                                    updatedSupplierInvoicesCount++;
                                     Console.WriteLine("InvoiceId {0} updated.", invoice.InvoiceId);
                                 }
                                 else
                                 {
                                     Console.WriteLine("InvoiceId {0} failed to updated.", invoice.InvoiceId);
                                 }
-
-
                             }
-
                         }
-                        if (suplierInvoices > 0) Console.WriteLine("{0} supplier invoices are updated.", suplierInvoices);
-                        if (customerInvoices > 0) Console.WriteLine("{0} customer invoices are updated.", customerInvoices);
+
+                        if (updatedSupplierInvoicesCount > 0)
+                            Console.WriteLine($"{updatedSupplierInvoicesCount} supplier invoices are updated.");
+
+                        if (updatedcustomerInvoicesCount > 0)
+                            Console.WriteLine($"{updatedcustomerInvoicesCount} customer invoices are updated.");
 
                     }
                     else
                     {
                         Console.WriteLine("All invoices are already updated.");
                     }
+
+                    var invoiceItems = GetItemsText($"SELECT it.[ID] ID" +
+                        $", InvoiceTypeID " +
+                        $", it.[Details] Details " +
+                        $"FROM VR_Invoice.InvoiceItem as it join VR_Invoice.Invoice as inv on it.InvoiceID = inv.ID where ItemSetName = 'GroupingByCurrency' AND it.Details not like '%TotalFullAmount\"%' and invoiceTypeId in ('{supplierInvoiceTypeId}','{customerInvoiceTypeId}')", InvoiceItemDetailMapper, (cmd) => { });
+
+                    if (invoiceItems != null)
+                    {
+                        int supplierInvoiceItems = 0;
+                        int customerInvoiceItems = 0;
+
+                        Console.WriteLine("{0} invoice items to be updated.", invoiceItems.Count);
+
+                        foreach (var invoiceItem in invoiceItems)
+                        {
+                            if (invoiceItem.InvoiceTypeId == customerInvoiceTypeId)
+                            {
+                                var customerInvoiceDetail = Serializer.Deserialize<CustomerInvoiceBySaleCurrencyItemDetails>(invoiceItem.Details);
+                                customerInvoiceDetail.TotalFullAmount = customerInvoiceDetail.AmountAfterCommissionWithTaxes;
+                                if (ExecuteNonQueryText($"Update  VR_Invoice.InvoiceItem set Details = '{Serializer.Serialize(customerInvoiceDetail)}' where ID = {invoiceItem.InvoiceItemId}", null) > 0)
+                                {
+                                    customerInvoiceItems++;
+                                    Console.WriteLine("InvoiceItemId {0} updated.", invoiceItem.InvoiceItemId);
+                                }
+                                else
+                                {
+                                    Console.WriteLine("InvoiceItemId {0} failed to updated.", invoiceItem.InvoiceItemId);
+                                }
+                            }
+                            else if (invoiceItem.InvoiceTypeId == supplierInvoiceTypeId)
+                            {
+                                var supplierInvoiceDetail = Serializer.Deserialize<SupplierInvoiceBySaleCurrencyItemDetails>(invoiceItem.Details);
+                                supplierInvoiceDetail.TotalFullAmount = supplierInvoiceDetail.AmountAfterCommissionWithTaxes;
+                                if (ExecuteNonQueryText($"Update  VR_Invoice.InvoiceItem set Details = '{Serializer.Serialize(supplierInvoiceDetail)}' where ID = {invoiceItem.InvoiceItemId}", null) > 0)
+                                {
+                                    supplierInvoiceItems++;
+                                    Console.WriteLine("InvoiceItemId {0} updated.", invoiceItem.InvoiceItemId);
+                                }
+                                else
+                                {
+                                    Console.WriteLine("InvoiceItemId {0} failed to updated.", invoiceItem.InvoiceItemId);
+                                }
+                            }
+                        }
+
+                        if (supplierInvoiceItems > 0)
+                            Console.WriteLine($"{supplierInvoiceItems} supplier invoices are updated.");
+
+                        if (customerInvoiceItems > 0)
+                            Console.WriteLine($"{customerInvoiceItems} customer invoices are updated.");
+                    }
+                    else
+                    {
+                        Console.WriteLine("All invoice items are already updated.");
+                    }
+
                 }
                 catch (Exception ex)
                 {
@@ -106,10 +163,18 @@ namespace ResolveInvoiceIssue
                 Console.ReadLine();
             }
 
-            private void BackUpInvoiceTable()
+            #region Private Methods
+
+            private void BackUpInvoiceTables()
             {
-                ExecuteNonQueryText($"Select * into VR_Invoice.Invoice_{DateTime.Now:yyyy_MM_dd_hh_mm_ss} from VR_Invoice.Invoice", null);
+                DateTime now = DateTime.Now;
+                ExecuteNonQueryText($"Select * into VR_Invoice.Invoice_{now:yyyy_MM_dd_hh_mm_ss} from VR_Invoice.Invoice " +
+                    $"Select * into VR_Invoice.InvoiceItem_{now:yyyy_MM_dd_hh_mm_ss} from VR_Invoice.InvoiceItem", null);
             }
+
+            #endregion
+
+            #region Mappers
 
             Invoice InvoiceMapper(IDataReader reader)
             {
@@ -120,6 +185,18 @@ namespace ResolveInvoiceIssue
                     InvoiceTypeId = GetReaderValue<Guid>(reader, "InvoiceTypeID")
                 };
             }
+
+            InvoiceItemDetail InvoiceItemDetailMapper(IDataReader reader)
+            {
+                return new InvoiceItemDetail
+                {
+                    Details = GetReaderValue<dynamic>(reader, "Details"),
+                    InvoiceItemId = GetReaderValue<long>(reader, "ID"),
+                    InvoiceTypeId = GetReaderValue<Guid>(reader, "InvoiceTypeID")
+                };
+            }
+
+            #endregion
         }
 
     }
