64aa4c6dae097fd65b11df04465847d4ad9297cc
Mohamad Kalakesh <VANRISE\mohamad.kalakesh>
2019-09-04 06:52:17 +0000

    Arranging VR_Runtime_SchedulerTaskService
    git-tfs-id: [http://developmentvlan:8585/tfs/vanrise.collection]$/;C57847

--- Vanrise/Code/Vanrise.Runtime.Web/Runtime/Services/SchedulerTaskService.js
+++ Vanrise/Code/Vanrise.Runtime.Web/Runtime/Services/SchedulerTaskService.js
@@ -5,55 +5,42 @@
     SchedulerTaskService.$inject = ['VRModalService', 'UtilsService', 'VRCommon_ObjectTrackingService'];
 
     function SchedulerTaskService(VRModalService, UtilsService, VRCommon_ObjectTrackingService) {
+
         var drillDownDefinitions = [];
-        return ({
-            addTask: addTask,
-            editTask: editTask,
-            showAddTaskModal: showAddTaskModal,
-            addDrillDownDefinition: addDrillDownDefinition,
-            getDrillDownDefinition: getDrillDownDefinition,
-            registerObjectTrackingDrillDownToSchedulerTaskService: registerObjectTrackingDrillDownToSchedulerTaskService
-        });
 
         function addTask(onTaskAdded) {
-            var settings = {};
             var parameters = {};
 
-            settings.onScopeReady = function (modalScope) {               
+            var modalSettings = {};
+            modalSettings.onScopeReady = function (modalScope) {
                 modalScope.onTaskAdded = onTaskAdded;
             };
-            var editor = '/Client/Modules/Runtime/Views/SchedulerTaskEditor.html';
-            VRModalService.showModal(editor, parameters, settings);
+            VRModalService.showModal('/Client/Modules/Runtime/Views/SchedulerTaskEditor.html', parameters, modalSettings);
         }
-
         function editTask(taskId, onTaskUpdated) {
-            var modalSettings = {};
             var parameters = {
                 taskId: taskId
             };
-            var editor = '/Client/Modules/Runtime/Views/SchedulerTaskEditor.html';
 
+            var modalSettings = {};
             modalSettings.onScopeReady = function (modalScope) {
                 modalScope.onTaskUpdated = onTaskUpdated;
             };
-            VRModalService.showModal(editor, parameters, modalSettings);
+            VRModalService.showModal('/Client/Modules/Runtime/Views/SchedulerTaskEditor.html', parameters, modalSettings);
         }
 
         function showAddTaskModal(bpDefinitionId, onTaskAdded) {
-            var settings = {
-            };
             //'7a35f562-319b-47b3-8258-ec1a704a82eb' is the related action type id for workflow
             var parameters = {
                 additionalParameter: { bpDefinitionID: bpDefinitionId, actionTypeId: '7a35f562-319b-47b3-8258-ec1a704a82eb' }
             };
 
-            settings.onScopeReady = function (modalScope) {
+            var modalSettings = {};
+            modalSettings.onScopeReady = function (modalScope) {
                 modalScope.title = "Schedule Task";
                 modalScope.onTaskAdded = onTaskAdded;
-
             };
-
-            VRModalService.showModal('/Client/Modules/Runtime/Views/SchedulerTaskEditor.html', parameters, settings);
+            VRModalService.showModal('/Client/Modules/Runtime/Views/SchedulerTaskEditor.html', parameters, modalSettings);
 
         }
 
@@ -64,35 +51,39 @@
         function registerObjectTrackingDrillDownToSchedulerTaskService() {
 
             var drillDownDefinition = {};
-
             drillDownDefinition.title = VRCommon_ObjectTrackingService.getObjectTrackingGridTitle();
             drillDownDefinition.directive = "vr-common-objecttracking-grid";
 
             drillDownDefinition.loadDirective = function (directiveAPI, ScheduelerItem) {
                 ScheduelerItem.objectTrackingGridAPI = directiveAPI;
-                
+
                 var query = {
                     ObjectId: ScheduelerItem.Entity.TaskId,
                     EntityUniqueName: getEntityUniqueName(),
-
                 };
                 return ScheduelerItem.objectTrackingGridAPI.load(query);
             };
 
             addDrillDownDefinition(drillDownDefinition);
-
         }
         function addDrillDownDefinition(drillDownDefinition) {
-
             drillDownDefinitions.push(drillDownDefinition);
         }
 
         function getDrillDownDefinition() {
             return drillDownDefinitions;
         }
+
+        return {
+            addTask: addTask,
+            editTask: editTask,
+            showAddTaskModal: showAddTaskModal,
+            addDrillDownDefinition: addDrillDownDefinition,
+            getDrillDownDefinition: getDrillDownDefinition,
+            registerObjectTrackingDrillDownToSchedulerTaskService: registerObjectTrackingDrillDownToSchedulerTaskService
+        };
+
     }
 
     appControllers.service('VR_Runtime_SchedulerTaskService', SchedulerTaskService);
-
 })(appControllers);
-
