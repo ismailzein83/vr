4eb2b6c69c273e036ea466dd6472ec6b130974c2
Fatima Al Heck <VANRISE\fatima.alheck>
2019-09-06 14:06:06 +0000

    Adding "ICXSMSInvalidAndRaw" and "ICXSMSProblems" views
    git-tfs-id: [http://developmentvlan:8585/tfs/vanrise.collection]$/;C57907

---
+++ Retail/Code/SQL.InterconnectSMSEDR/ICX_SMS/Views/vw_ICXSMSInvalidAndRaw.sql
@@ -0,0 +1,9 @@
+﻿
+CREATE VIEW [ICX_SMS].[vw_ICXSMSInvalidAndRaw]
+AS
+SELECT invalidSMS.Id, invalidSMS.GatewayId as Gateway, invalidSMS.SentDateTime, invalidSMS.OperatorID, 
+	   invalidSMS.OriginationMCC, invalidSMS.OriginationMNC, invalidSMS.OriginationMobileNetwork,invalidSMS.OriginationMobileCountry,
+	   invalidSMS.DestinationMCC,invalidSMS.DestinationMNC, invalidSMS.DestinationMobileNetwork, invalidSMS.DestinationMobileCountry,
+	   invalidSMS.Sender, invalidSMS.Receiver, invalidSMS.BillingType, invalidSMS.TrafficDirection, sms.Trunk
+FROM   [Retail_Dev_ICX_SMS].[ICX_SMS].[BillingSMS_Invalid] AS invalidSMS WITH (NOLOCK) 
+INNER JOIN  [Retail_Dev_ICX_SMS].[ICX_SMS].[SMS] AS sms WITH (NOLOCK) ON invalidSMS.Id = sms.Id
---
+++ Retail/Code/SQL.InterconnectSMSEDR/ICX_SMS/Views/vw_ICXSMSProblems.sql
@@ -0,0 +1,6 @@
+﻿
+CREATE VIEW [ICX_SMS].[vw_ICXSMSProblems]
+AS
+SELECT ID, GatewayID, SentDateTime, OperatorID, OriginationMCC,  OriginationMNC, OriginationMobileNetwork, OriginationMobileCountry,
+	   DestinationMCC, DestinationMNC, DestinationMobileNetwork, DestinationMobileCountry, Sender , Receiver, Amount, BillingType, TrafficDirection
+FROM  [ICX_SMS].[BillingSMS_Invalid] WITH (NOLOCK)
