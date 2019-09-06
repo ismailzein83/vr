af24afefe29c7590a8f95535c735d25b0bd92a37
Fatima Al Heck <VANRISE\fatima.alheck>
2019-09-06 14:01:43 +0000

    Adding "ICXCDRInvalidAndRaw" and "ICXCDRProblems" views
    git-tfs-id: [http://developmentvlan:8585/tfs/vanrise.collection]$/;C57905

---
+++ Retail/Code/InterconnectCDR/ICX_CDR/Views/vw_ICXCDRInvalidAndRaw.sql
@@ -0,0 +1,8 @@
+﻿
+
+CREATE VIEW [ICX_CDR].[vw_ICXCDRInvalidAndRaw]
+AS
+SELECT invalidCDR.CDRId, invalidCDR.SwitchId, invalidCDR.AttemptDateTime, invalidCDR.DurationInSeconds, invalidCDR.CGPN, invalidCDR.CDPN, invalidCDR.OperatorID, 
+	   invalidCDR.OriginationZoneID, invalidCDR.DestinationZoneID, invalidCDR.BillingType, invalidCDR.TrafficDirection, cdr.InTrunk, cdr.OutTrunk
+FROM   ICX_CDR.BillingCDR_Invalid AS invalidCDR WITH (NOLOCK) 
+INNER JOIN  ICX_CDR.CDR AS cdr WITH (NOLOCK) ON invalidCDR.CDRId = cdr.Id
---
+++ Retail/Code/InterconnectCDR/ICX_CDR/Views/vw_ICXCDRProblems.sql
@@ -0,0 +1,6 @@
+﻿
+
+CREATE VIEW [ICX_CDR].[vw_ICXCDRProblems]
+AS
+SELECT CDRId, SwitchId, AttemptDateTime, DurationInSeconds, CGPN, CDPN, OperatorID, OriginationZoneId, DestinationZoneId, BillingType, TrafficDirection
+FROM  [ICX_CDR].[BillingCDR_Invalid] WITH (NOLOCK)
