d72253f581dbca4f24dccfedb16e3f776ea510c5
Wissam Ajamy <VANRISE\wissam.ajamy>
2019-09-09 08:36:33 +0000

    Adding [DevProjectID] Field
    git-tfs-id: [http://developmentvlan:8585/tfs/vanrise.collection]$/;C57923

--- Mediation/Code/Mediation/Mediation_Generic/Tables/MediationDefinition.sql
+++ Mediation/Code/Mediation/Mediation_Generic/Tables/MediationDefinition.sql
@@ -1,11 +1,14 @@
 ﻿CREATE TABLE [Mediation_Generic].[MediationDefinition] (
-    [ID]          UNIQUEIDENTIFIER NOT NULL,
-    [Name]        VARCHAR (255)    NOT NULL,
-    [Details]     NVARCHAR (MAX)   NOT NULL,
-    [CreatedTime] DATETIME         CONSTRAINT [DF_MediationSettingDefinition_CreatedTime] DEFAULT (getdate()) NULL,
-    [timestamp]   ROWVERSION       NULL,
+    [ID]           UNIQUEIDENTIFIER NOT NULL,
+    [DevProjectID] UNIQUEIDENTIFIER NULL,
+    [Name]         VARCHAR (255)    NOT NULL,
+    [Details]      NVARCHAR (MAX)   NOT NULL,
+    [CreatedTime]  DATETIME         CONSTRAINT [DF_MediationSettingDefinition_CreatedTime] DEFAULT (getdate()) NULL,
+    [timestamp]    ROWVERSION       NULL,
     CONSTRAINT [PK_MediationDefinition] PRIMARY KEY CLUSTERED ([ID] ASC)
 );
+
+
 
 
 
