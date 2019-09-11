4e9ab26414b48d086d7431ae23a5a7a70f6b0cb0
Wissam Ajamy <VANRISE\wissam.ajamy>
2019-09-11 12:50:54 +0000

    Adding DEvProjectID to VRNumberPrefixType table
    git-tfs-id: [http://developmentvlan:8585/tfs/vanrise.collection]$/;C57969

--- TOneV2/Code/TOneV2/SQL.TOneConfiguration/genericdata/Tables/VRNumberPrefixType.sql
+++ TOneV2/Code/TOneV2/SQL.TOneConfiguration/genericdata/Tables/VRNumberPrefixType.sql
@@ -1,5 +1,6 @@
 ﻿CREATE TABLE [genericdata].[VRNumberPrefixType] (
     [Id]               UNIQUEIDENTIFIER NOT NULL,
+    [DevProjectID]     UNIQUEIDENTIFIER NULL,
     [Name]             VARCHAR (255)    NULL,
     [CreatedBy]        INT              NULL,
     [CreatedTime]      DATETIME         NULL,
@@ -7,6 +8,8 @@
     [LastModifiedTime] DATETIME         NULL,
     [timestamp]        ROWVERSION       NULL,
     CONSTRAINT [PK_VRNumberPrefixType] PRIMARY KEY CLUSTERED ([Id] ASC),
-    UNIQUE NONCLUSTERED ([Name] ASC)
+    CONSTRAINT [UQ__VRNumber__737584F6004AEFF1] UNIQUE NONCLUSTERED ([Name] ASC)
 );
+
+
 
