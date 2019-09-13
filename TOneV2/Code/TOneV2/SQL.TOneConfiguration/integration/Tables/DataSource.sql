1f6b2a802cf69bb6d668fd05ca37d6c1ff751c7a
Wissam Ajamy <VANRISE\wissam.ajamy>
2019-09-13 15:00:58 +0000

    Adding DevProjectID field
    git-tfs-id: [http://developmentvlan:8585/tfs/vanrise.collection]$/;C58036

--- TOneV2/Code/TOneV2/SQL.TOneConfiguration/integration/Tables/DataSource.sql
+++ TOneV2/Code/TOneV2/SQL.TOneConfiguration/integration/Tables/DataSource.sql
@@ -1,15 +1,18 @@
 ﻿CREATE TABLE [integration].[DataSource] (
-    [ID]           UNIQUEIDENTIFIER CONSTRAINT [DF__DataSource__ID__485B9C89] DEFAULT (newid()) NOT NULL,
-    [Name]         VARCHAR (100)    NOT NULL,
-    [AdapterID]    UNIQUEIDENTIFIER NULL,
-    [AdapterState] VARCHAR (1000)   NULL,
-    [TaskId]       UNIQUEIDENTIFIER NOT NULL,
-    [Settings]     VARCHAR (MAX)    NOT NULL,
-	[LastModifiedTime] [datetime]	NULL CONSTRAINT [DF_DataSource_LastModifiedTime]  DEFAULT (getdate()),
-    [CreatedTime]  DATETIME         CONSTRAINT [DF_DataSource_CreatedTime] DEFAULT (getdate()) NULL,
-    [timestamp]    ROWVERSION       NULL,
+    [ID]               UNIQUEIDENTIFIER CONSTRAINT [DF__DataSource__ID__485B9C89] DEFAULT (newid()) NOT NULL,
+    [DevProjectID]     UNIQUEIDENTIFIER NULL,
+    [Name]             VARCHAR (100)    NOT NULL,
+    [AdapterID]        UNIQUEIDENTIFIER NULL,
+    [AdapterState]     VARCHAR (1000)   NULL,
+    [TaskId]           UNIQUEIDENTIFIER NOT NULL,
+    [Settings]         VARCHAR (MAX)    NOT NULL,
+    [LastModifiedTime] DATETIME         CONSTRAINT [DF_DataSource_LastModifiedTime] DEFAULT (getdate()) NULL,
+    [CreatedTime]      DATETIME         CONSTRAINT [DF_DataSource_CreatedTime] DEFAULT (getdate()) NULL,
+    [timestamp]        ROWVERSION       NULL,
     CONSTRAINT [pk_DataSource] PRIMARY KEY CLUSTERED ([ID] ASC)
 );
+
+
 
 
 
