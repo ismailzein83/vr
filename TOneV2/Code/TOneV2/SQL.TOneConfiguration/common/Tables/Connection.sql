dd90363227ac13628070664f1a7eb1befec54f9a
Wissam Ajamy <VANRISE\wissam.ajamy>
2019-09-13 12:35:51 +0000

    Ading DevProjectID field
    git-tfs-id: [http://developmentvlan:8585/tfs/vanrise.collection]$/;C58023

--- TOneV2/Code/TOneV2/SQL.TOneConfiguration/common/Tables/Connection.sql
+++ TOneV2/Code/TOneV2/SQL.TOneConfiguration/common/Tables/Connection.sql
@@ -1,5 +1,6 @@
 ﻿CREATE TABLE [common].[Connection] (
     [ID]               UNIQUEIDENTIFIER NOT NULL,
+    [DevProjectID]     UNIQUEIDENTIFIER NULL,
     [Name]             NVARCHAR (255)   NOT NULL,
     [Settings]         NVARCHAR (MAX)   NOT NULL,
     [CreatedTime]      DATETIME         CONSTRAINT [DF_Connection_CreatedTime] DEFAULT (getdate()) NOT NULL,
@@ -7,6 +8,8 @@
     [LastModifiedTime] DATETIME         CONSTRAINT [DF_Connection_LastModifiedTime] DEFAULT (getdate()) NULL,
     CONSTRAINT [PK_common.Connection] PRIMARY KEY CLUSTERED ([ID] ASC)
 );
+
+
 
 
 
