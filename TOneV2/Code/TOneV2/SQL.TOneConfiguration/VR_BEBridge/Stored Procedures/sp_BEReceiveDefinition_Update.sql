10138a3bcb061a1dbd77526aa006bd08bd6f3386
Wissam Ajamy <VANRISE\wissam.ajamy>
2019-09-19 06:33:19 +0000

    Adding DevProjectID
    git-tfs-id: [http://developmentvlan:8585/tfs/vanrise.collection]$/;C58141

--- TOneV2/Code/TOneV2/SQL.TOneConfiguration/VR_BEBridge/Stored Procedures/sp_BEReceiveDefinition_Update.sql
+++ TOneV2/Code/TOneV2/SQL.TOneConfiguration/VR_BEBridge/Stored Procedures/sp_BEReceiveDefinition_Update.sql
@@ -2,13 +2,14 @@
 CREATE PROCEDURE [VR_BEBridge].[sp_BEReceiveDefinition_Update]
 	@ID uniqueidentifier,
 	@Name NVARCHAR(255),
+	@DevProjectId  uniqueidentifier,
 	@Settings nvarchar(MAX)
 AS
 BEGIN
 IF NOT EXISTS(SELECT 1 FROM VR_BEBridge.BEReceiveDefinition WHERE ID != @ID and Name = @Name )
 	BEGIN
 		update VR_BEBridge.BEReceiveDefinition
-		set  Name = @Name ,Settings= @Settings, LastModifiedTime = GETDATE()
+		set  Name = @Name,DevProjectID=@DevProjectId,Settings= @Settings, LastModifiedTime = GETDATE()
 		where  ID = @ID
 	END
 
