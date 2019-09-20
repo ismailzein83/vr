67f63d0a75cd395d3359617a0ee83677cc743627
Ali Atoui <VANRISE\ali.atoui>
2019-09-20 11:47:52 +0000

    Adding HuaweiSoftX3000SWSync
    git-tfs-id: [http://developmentvlan:8585/tfs/vanrise.collection]$/;C58184

---
+++ TOneV2/Code/TOneV2/TOne.WhS.RouteSync.Huawei/SoftX3000/Entities/CarrierMapping.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TOne.WhS.RouteSync.Huawei.SoftX3000.Entities
+{
+    public class CarrierMapping
+    {
+        public int CarrierId { get; set; }
+
+        public CustomerMapping CustomerMapping { get; set; }
+
+        public SupplierMapping SupplierMapping { get; set; }
+    }
+
+    public class CustomerMapping
+    {
+        public int RSSC { get; set; }
+
+        public int DNSet { get; set; }
+    }
+
+    public class SupplierMapping
+    {
+        public int SRT { get; set; }
+    }
+}
---
+++ TOneV2/Code/TOneV2/TOne.WhS.RouteSync.Huawei/SoftX3000/HuaweiSoftX3000SWSync.cs
@@ -0,0 +1,96 @@
+﻿using System;
+using System.Collections.Generic;
+using TOne.WhS.RouteSync.Entities;
+using TOne.WhS.RouteSync.Huawei.SoftX3000.Entities;
+
+namespace TOne.WhS.RouteSync.Huawei.SoftX3000
+{
+    public class HuaweiSoftX3000SWSync : SwitchRouteSynchronizer
+    {
+        public override Guid ConfigId { get { return new Guid("2574530B-B9FD-44E2-BD0C-D1F46DB80E68"); } }
+
+        public override bool SupportSyncWithinRouteBuild { get { return false; } }
+
+        public int NumberOfOptions { get; set; }
+
+        public Dictionary<string, CarrierMapping> CarrierMappings { get; set; }
+
+        public List<Huawei.Entities.HuaweiSSHCommunication> SwitchCommunicationList { get; set; }
+
+        public Dictionary<string, string> OverriddenRSSNsInRSName { get; set; }
+
+        public List<Huawei.Entities.SwitchLogger> SwitchLoggerList { get; set; }
+
+        private Dictionary<int, CustomerMapping> _customerMappingsByRSSC;
+        private Dictionary<int, CustomerMapping> CustomerMappingsByRSSC
+        {
+            get
+            {
+                if (_customerMappingsByRSSC != null)
+                    return _customerMappingsByRSSC;
+
+                if (CarrierMappings == null || CarrierMappings.Count == 0)
+                    return null;
+
+                _customerMappingsByRSSC = new Dictionary<int, CustomerMapping>();
+
+                foreach (var carrierMappingKvp in CarrierMappings)
+                {
+                    var carrierMapping = carrierMappingKvp.Value;
+                    if (carrierMapping.CustomerMapping != null)
+                        _customerMappingsByRSSC.Add(carrierMapping.CustomerMapping.RSSC, carrierMapping.CustomerMapping);
+                }
+
+                if (_customerMappingsByRSSC.Count == 0)
+                    _customerMappingsByRSSC = null;
+
+                return _customerMappingsByRSSC;
+            }
+        }
+
+
+        #region Public Methods
+
+        public override void Initialize(ISwitchRouteSynchronizerInitializeContext context)
+        {
+            throw new NotImplementedException();
+        }
+
+        public override void ConvertRoutes(ISwitchRouteSynchronizerConvertRoutesContext context)
+        {
+            throw new NotImplementedException();
+        }
+
+        public override void onAllRoutesConverted(ISwitchRouteSynchronizerOnAllRoutesConvertedContext context)
+        {
+            throw new NotImplementedException();
+        }
+
+        public override object PrepareDataForApply(ISwitchRouteSynchronizerPrepareDataForApplyContext context)
+        {
+            throw new NotImplementedException();
+        }
+
+        public override void ApplySwitchRouteSyncRoutes(ISwitchRouteSynchronizerApplyRoutesContext context)
+        {
+            throw new NotImplementedException();
+        }
+
+        public override void Finalize(ISwitchRouteSynchronizerFinalizeContext context)
+        {
+            throw new NotImplementedException();
+        }
+
+        public override void RemoveConnection(ISwitchRouteSynchronizerRemoveConnectionContext context)
+        {
+            throw new NotImplementedException();
+        }
+
+        public override bool IsSwitchRouteSynchronizerValid(IIsSwitchRouteSynchronizerValidContext context)
+        {
+            throw new NotImplementedException();
+        }
+
+        #endregion
+    }
+}
