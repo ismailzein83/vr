8af5805b610ec49adab9a338abadf2c6a92cd3cf
Fatima Al Heck <VANRISE\fatima.alheck>
2019-09-25 07:53:49 +0000

    Selecting "Rate" column in "vw_ICXCDRProblems" view
    git-tfs-id: [http://developmentvlan:8585/tfs/vanrise.collection]$/;C58273

--- Retail/Code/InterconnectCDR/ICX_CDR/Views/vw_ICXCDRProblems.sql
+++ Retail/Code/InterconnectCDR/ICX_CDR/Views/vw_ICXCDRProblems.sql
@@ -1,6 +1,7 @@
 ﻿
 
+
 CREATE VIEW [ICX_CDR].[vw_ICXCDRProblems]
 AS
-SELECT CDRId, SwitchId, AttemptDateTime, DurationInSeconds, CGPN, CDPN, OperatorID, OriginationZoneId, DestinationZoneId, BillingType, TrafficDirection
+SELECT CDRId, SwitchId, AttemptDateTime, DurationInSeconds, CGPN, CDPN, OperatorID, OriginationZoneId, DestinationZoneId,Rate, BillingType, TrafficDirection
 FROM  [ICX_CDR].[BillingCDR_Invalid] WITH (NOLOCK)
