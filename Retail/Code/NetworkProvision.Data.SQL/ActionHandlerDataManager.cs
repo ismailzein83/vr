535f808016199d262863cfc19293ab4afe90d193
Hassan Jradi <VANRISE\hassan.jradi>
2019-09-26 08:39:05 +0000

    add ActionHandlerDataManager RDB and SQL
    git-tfs-id: [http://developmentvlan:8585/tfs/vanrise.collection]$/;C58302

---
+++ Retail/Code/NetworkProvision.Data.SQL/ActionHandlerDataManager.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NetworkProvision.Data.SQL
+{
+    public class ActionHandlerDataManager : IActionHandlerDataManager
+    {
+        public bool AreActionHandlersUpdated(ref object updateHandle)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
