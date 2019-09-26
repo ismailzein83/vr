ef9c077a7dc31956f189eb0d7b0c9b68fa1eee39
Hassan Makhour <VANRISE\hassan.makhour>
2019-09-26 13:32:11 +0000

    hassan makhour task
    git-tfs-id: [http://developmentvlan:8585/tfs/vanrise.collection]$/;C58314

---
+++ SOM/Code/SOM.Runtime/Tasks/HassanMakhourTask.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SOM.Runtime.Tasks
+{
+    public class HassanMakhourTask : ITask
+    {
+        public void Execute()
+        {
+            RuntimeProcess();
+        }
+        private void RuntimeProcess() { }
+    }
+}
