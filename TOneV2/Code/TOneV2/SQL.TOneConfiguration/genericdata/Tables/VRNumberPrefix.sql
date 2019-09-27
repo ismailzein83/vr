2ebf6f8ada37a980952cfbc32bfec6e602db74cc
Fatima Al Heck <VANRISE\fatima.alheck>
2019-09-27 14:03:23 +0000

    Adding IsExact property to "VRNumberPrefix" entity Adding Short Numbers to NumberPrefix table
    git-tfs-id: [http://developmentvlan:8585/tfs/vanrise.collection]$/;C58344

--- TOneV2/Code/TOneV2/SQL.TOneConfiguration/genericdata/Tables/VRNumberPrefix.sql
+++ TOneV2/Code/TOneV2/SQL.TOneConfiguration/genericdata/Tables/VRNumberPrefix.sql
@@ -2,12 +2,15 @@
     [Id]               INT              IDENTITY (1, 1) NOT NULL,
     [Number]           VARCHAR (20)     NULL,
     [Type]             UNIQUEIDENTIFIER NULL,
+    [IsExact]          BIT              NULL,
     [CreatedBy]        INT              NULL,
     [CreatedTime]      DATETIME         NULL,
     [LastModifiedBy]   INT              NULL,
     [LastModifiedTime] DATETIME         NULL,
     [timestamp]        ROWVERSION       NULL,
     CONSTRAINT [PK_VRNumberPrefix] PRIMARY KEY CLUSTERED ([Id] ASC),
-    UNIQUE NONCLUSTERED ([Number] ASC)
+    CONSTRAINT [UQ__VRNumber__78A1A19D7DE38492] UNIQUE NONCLUSTERED ([Number] ASC)
 );
+
+
 
--- TOneV2/Code/TOneV2/SQL.TOneConfiguration/genericdata/User Defined Types/VRNumberPrefixType.sql
+++ TOneV2/Code/TOneV2/SQL.TOneConfiguration/genericdata/User Defined Types/VRNumberPrefixType.sql
@@ -2,8 +2,11 @@
     [Id]               INT              NULL,
     [Number]           VARCHAR (20)     NULL,
     [Type]             UNIQUEIDENTIFIER NULL,
+    [IsExact]          BIT              NULL,
     [CreatedBy]        INT              NULL,
     [CreatedTime]      DATETIME         NULL,
     [LastModifiedBy]   INT              NULL,
     [LastModifiedTime] DATETIME         NULL);
+
+
 
