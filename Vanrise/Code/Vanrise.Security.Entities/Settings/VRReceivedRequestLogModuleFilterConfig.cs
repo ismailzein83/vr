38608f3eda717b51da56131a54b71663509ba379
Fatima Hammoud <VANRISE\fatima.hammoud>
2019-09-27 14:38:47 +0000

    Changing namespace
    git-tfs-id: [http://developmentvlan:8585/tfs/vanrise.collection]$/;C58349

--- Vanrise/Code/Vanrise.Security.Entities/Settings/VRReceivedRequestLogModuleFilterConfig.cs
+++ Vanrise/Code/Vanrise.Security.Entities/Settings/VRReceivedRequestLogModuleFilterConfig.cs
@@ -5,7 +5,7 @@ using System.Text;
 using System.Threading.Tasks;
 using Vanrise.Entities;
 
-namespace Vanrise.Security.Entities.Settings
+namespace Vanrise.Security.Entities
 {
     public class VRReceivedRequestLogModuleFilterConfig: ExtensionConfiguration
     {
