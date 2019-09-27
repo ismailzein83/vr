05e8c1378b1a48872391b2eade690c7d1d0eadfe
Fatima Hammoud <VANRISE\fatima.hammoud>
2019-09-27 14:26:59 +0000

    Adding new extension configuration "VRReceivedRequestLogModuleFilterConfig"
    git-tfs-id: [http://developmentvlan:8585/tfs/vanrise.collection]$/;C58345

---
+++ Vanrise/Code/Vanrise.Security.Entities/Settings/VRReceivedRequestLogModuleFilterConfig.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Vanrise.Entities;
+
+namespace Vanrise.Security.Entities.Settings
+{
+    public class VRReceivedRequestLogModuleFilterConfig: ExtensionConfiguration
+    {
+        public const string EXTENSION_TYPE = "VR_Security_ReceivedRequestLog_ModuleFilter";
+        public string Editor { get; set; }
+    }
+}
