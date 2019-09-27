b0a3806d9662717c5acc61693fd789a85da6fb82
Naim Saad <VANRISE\naim.saad>
2019-09-27 06:50:31 +0000

    Adding RetailBillingChargeCustomObjectTypeSettings
    git-tfs-id: [http://developmentvlan:8585/tfs/vanrise.collection]$/;C58320

---
+++ Retail/Code/Retail.Billing.MainExtensions/RetailBillingCharge/RetailBillingChargeCustomObjectTypeSettings.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using Vanrise.GenericData.Entities;
+
+namespace Retail.Billing.MainExtensions.RetailBillingCharge
+{
+    public class RetailBillingChargeCustomObjectTypeSettings : FieldCustomObjectTypeSettings
+    {
+        public override Guid ConfigId { get { return new Guid("29cc92fd-9abb-4271-b1de-22181906d7f6"); } }
+
+        public override bool AreEqual(object newValue, object oldValue)
+        {
+            return true;
+        }
+        public override string SelectorUIControl { get { return "retail-billing-charge-customobject"; } }
+
+        public override string GetDescription(IFieldCustomObjectTypeSettingsContext context)
+        {
+            return "Retail Billing Charge";
+        }
+
+        public override Type GetNonNullableRuntimeType()
+        {
+            return typeof(Retail.Billing.Entities.RetailBillingCharge);
+        }
+
+        public override string GetRuntimeTypeDescription()
+        {
+            throw new NotImplementedException();
+        }
+
+        public override dynamic ParseNonNullValueToFieldType(object originalValue)
+        {
+            var castedOriginalValue = originalValue as Retail.Billing.Entities.RetailBillingCharge;
+            return castedOriginalValue;
+        }
+    }
+}
