1dec90410c522ccfd6b6cc8c6f9204d3b0f7f5ad
Fatima Hammoud <VANRISE\fatima.hammoud>
2019-09-30 07:22:54 +0000

    Adding commented some files
    git-tfs-id: [http://developmentvlan:8585/tfs/vanrise.collection]$/;C58356

---
+++ Vanrise/Code/Vanrise.Common.Web/Common/Services/APIServices/ReceivedRequestLogAPIService.js
@@ -0,0 +1,22 @@
+﻿//(function (appControllers) {
+
+//    "use strict";
+//    ReceivedRequestLogAPIService.$inject = ['BaseAPIService', 'UtilsService', 'VRCommon_ModuleConfig'];
+
+//    function ReceivedRequestLogAPIService(BaseAPIService, UtilsService, VRCommon_ModuleConfig) {
+
+//        var controllerName = 'ReceivedRequestLog';
+
+//        function GetReceivedRequestLogFilterModuleConfigs() {
+//            return BaseAPIService.get(UtilsService.getServiceURL(VRCommon_ModuleConfig.moduleName, controllerName, "GetReceivedRequestLogFilterModuleConfigs"));
+//        }
+
+//        return ({
+//            GetReceivedRequestLogFilterModuleConfigs: GetReceivedRequestLogFilterModuleConfigs
+
+//        });
+//    }
+
+//    appControllers.service('VRCommon_ReceivedRequestLogAPIService', ReceivedRequestLogAPIService);
+
+//})(appControllers);
---
+++ Vanrise/Code/Vanrise.Common.Web/Controllers/ReceivedRequestLogController.cs
@@ -0,0 +1,24 @@
+﻿//using System;
+//using System.Collections.Generic;
+//using System.Linq;
+//using System.Web.Http;
+//using Vanrise.Common.Business;
+
+//using Vanrise.Entities;
+//using Vanrise.Web.Base;
+
+//namespace Vanrise.Common.Web.Controllers
+//{
+//    [RoutePrefix(Constants.ROUTE_PREFIX + "ReceivedRequestLog")]
+//    [JSONWithTypeAttribute]
+//    public class ReceivedRequestLogController: BaseAPIController
+//    {
+//        [HttpGet]
+//        [Route("GetReceivedRequestLogFilterModuleConfigs")]
+//        public IEnumerable<VRReceivedRequestLogModuleFilterConfig> GetReceivedRequestLogFilterModuleConfigs()
+//        {
+//            ReceivedRequestLogManager manager = new ReceivedRequestLogManager();
+//            return manager.GetReceivedRequestLogFilterModuleConfigs();
+//        }
+//    }
+//}
