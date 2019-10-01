880a901aeef84784929c9b469df33039adc01a43
Mohammad Yahfoufi <VANRISE\mohammad.yahfoufi>
2019-10-01 15:25:28 +0000

    Adding New Fields To Technical Details
    git-tfs-id: [http://developmentvlan:8585/tfs/vanrise.collection]$/;C58396

--- SOM/Code/BPMExtended.Main.SOM.Entities/Inventory/ServiceResponse/TechnicalDetails.cs
+++ SOM/Code/BPMExtended.Main.SOM.Entities/Inventory/ServiceResponse/TechnicalDetails.cs
@@ -32,12 +32,28 @@ namespace BPMExtended.Main.SOMAPI
         public string SWITCH_TYPE { get; set; }
         public string DEV_TYPE { get; set; }
         public string DevName { get; set; }
+        public string DeviceStatus { get; set; }
         public string PrimrayMuxPortName { get; set; }
         public string DPPortId { get; set; }
 
         public string DSlam { get; set; }
         public string DSlamPort { get; set; }
         public string DSlamOMC { get; set; }
+
+        public string MDFPortStatus { get; set; }
+        public string DPPort { get; set; }
+        public string DPPortStatus { get; set; }
+
+        public string MUXPortName { get; set; }
+        public string MUXPortStatus { get; set; }
+
+        public string TransmitterStatus { get; set; }
+        public string TransmitterModule { get; set; }
+
+        public string ReceiverStatus { get; set; }
+        public string ReceiverPortStatus { get; set; }
+        public string TransmitterPortStatus { get; set; }
+
 
         public string Transmitter { get; set; }
         public string TransmitterPort { get; set; }
@@ -78,52 +94,6 @@ namespace BPMExtended.Main.SOMAPI
         public string CabinetId { get; set; }
         public string SiteId { get; set; }
         public string SiteName { get; set; }
-        //public string PhoneType { get; set; }
-        //public PhoneStatus PhoneStatus { get; set; }
-        //public string PhoneNumber { get; set; }
-        //public string PhoneCategory { get; set; }
-        //public string VerticalMDF { get; set; }
-        //public string PhoneID { get; set; }
-        //public string MDFPort { get; set; }
-        //public string Cabinet { get; set; }
-        //public string CabinetId { get; set; }
-        //public string CabinetPrimaryPort { get; set; }
-        //public string CabinetSecondaryPort { get; set; }
-        //public string DP { get; set; }
-        //public List<string> DPPorts { get; set; }
-        //public List<string> DPSecondaryPorts { get; set; }
-        //public string Switch { get; set; }
-        //public string SwitchId { get; set; }
-        //public string SwitchOMC { get; set; }
-        //public string SWITCH_TYPE { get; set; }
-        //public string PathId { get; set; }
-        //public string PathStatus { get; set; }
-        //public string DSlam { get; set; }
-        //public string DSlamPort { get; set; }
-        //public string DSlamOMC { get; set; }
-        //public string Transmitter { get; set; }
-        //public string TransmitterPort { get; set; }
-        //public string Receiver { get; set; }
-        //public string ReceiverPort { get; set; }
-        //public string MSANName { get; set; }
-        //public string MSAN_EID { get; set; }
-        //public string MSAN_TID { get; set; }
-        //public string MSAN_OMC { get; set; }
-        //public string MSANType { get; set; }
-        //public string DPId { get; set; }
-        //public string DPPortId { get; set; }
-        //public bool IsMultiplexed { get; set; }
-        //public string SwitchStatus { get; set; }
-        //public string MDFStatus { get; set; }
-        //public string CabinetStatus { get; set; }
-        //public string DPStatus { get; set; }
-        //public string IsSwitchFaulty { get; set; }
-        //public string IsMDFFaulty { get; set; }
-        //public string IsCabinetFaulty { get; set; }
-        //public string IsDPFaulty { get; set; }
-        //public string MDF { get; set; }
-        //public string DEV_TYPE { get; set; }
-        //public string DevName { get; set; }
-        //public string PrimaryMuxPortName { get; set; }
+
     }
 }
