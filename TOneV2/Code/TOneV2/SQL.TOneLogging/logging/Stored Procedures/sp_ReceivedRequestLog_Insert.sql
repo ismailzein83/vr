72ec1cb94624fe3b6cc933f79252b34a9b8d03e8
Fatima Hammoud <VANRISE\fatima.hammoud>
2019-10-01 08:55:53 +0000

    Adding new table "ReceivedRequestLog"  into logging + its sp
    git-tfs-id: [http://developmentvlan:8585/tfs/vanrise.collection]$/;C58382

---
+++ TOneV2/Code/TOneV2/SQL.TOneLogging/logging/Stored Procedures/sp_ReceivedRequestLog_Insert.sql
@@ -0,0 +1,60 @@
+﻿-- =============================================
+-- Author:		<Author,,Name>
+-- Create date: <Create Date,,>
+-- Description:	<Description,,>
+-- =============================================
+CREATE PROCEDURE [logging].[sp_ReceivedRequestLog_Insert]
+    @ActionName nvarchar(50),
+	@Method nvarchar(50),
+	@ModuleName nvarchar(50),
+	@ControllerName nvarchar(50),
+	@URI nvarchar(max),
+	@Path nvarchar(max),
+	@RequestHeader nvarchar(max),
+	@Arguments nvarchar(max),
+	@RequestBody nvarchar(max),
+	@ResponseHeader nvarchar(max),
+	@ResponseStatusCode nvarchar(10),
+	@IsSucceded bit,
+	@BodyResponse nvarchar(MAX),
+	@StartDateTime datetime,
+	@UserId int
+	
+AS
+BEGIN
+	INSERT INTO [logging].[ReceivedRequestLog]
+           (ActionName
+		   ,Method
+		   ,ModuleName
+		   ,ControllerName
+           ,URI
+           ,[Path]
+		   ,RequestHeader
+		   ,Arguments
+		   ,RequestBody
+		   ,ResponseHeader
+		   ,ResponseStatusCode
+		   ,IsSucceded
+		   ,ResponseBody
+		   ,UserId
+		   ,StartDateTime
+		   ,EndDateTime
+		   )
+     VALUES
+           (@ActionName
+		   ,@Method
+		   ,@ModuleName
+		   ,@ControllerName
+           ,@URI
+           ,@Path
+		   ,@RequestHeader
+		   ,@Arguments
+		   ,@RequestBody
+		   ,@ResponseHeader
+		   ,@ResponseStatusCode
+		   ,@IsSucceded
+		   ,@BodyResponse
+		   ,@UserId
+		   ,@StartDateTime
+		   ,GETDATE())
+END
---
+++ TOneV2/Code/TOneV2/SQL.TOneLogging/logging/Tables/ReceivedRequestLog.sql
@@ -0,0 +1,20 @@
+﻿CREATE TABLE [logging].[ReceivedRequestLog] (
+    [ID]                 BIGINT         IDENTITY (1, 1) NOT NULL,
+    [ActionName]         NVARCHAR (50)  NOT NULL,
+    [Method]             NVARCHAR (50)  NOT NULL,
+    [ModuleName]         NVARCHAR (50)  NOT NULL,
+    [ControllerName]     NVARCHAR (50)  NOT NULL,
+    [URI]                NVARCHAR (MAX) NOT NULL,
+    [Path]               NVARCHAR (MAX) NOT NULL,
+    [RequestHeader]      NVARCHAR (MAX) NULL,
+    [Arguments]          NVARCHAR (MAX) NULL,
+    [RequestBody]        NVARCHAR (MAX) NULL,
+    [ResponseHeader]     NVARCHAR (MAX) NULL,
+    [ResponseStatusCode] NVARCHAR (10)  NULL,
+    [IsSucceded]         BIT            NOT NULL,
+    [ResponseBody]       NVARCHAR (MAX) NULL,
+    [UserId]             INT            NULL,
+    [StartDateTime]      DATETIME       NULL,
+    [EndDateTime]        DATETIME       NULL
+);
+
