4fcfa85c035d3586e194021958595b086fbd009d
Hassan Jradi <VANRISE\hassan.jradi>
2019-10-02 11:56:58 +0000

    Add VRHttpConnectionLog
    git-tfs-id: [http://developmentvlan:8585/tfs/vanrise.collection]$/;C58406

---
+++ TOneV2/Code/TOneV2/SQL.TOneLogging/logging/Stored Procedures/sp_VRHttpConnectionLog_Insert.sql
@@ -0,0 +1,45 @@
+﻿CREATE PROCEDURE [logging].[sp_VRHttpConnectionLog_Insert]
+	@VRHttpConnectionId uniqueidentifier,
+	@BaseURL            nvarchar(MAX),
+	@Path				nvarchar(MAX),
+	@Parameters			nvarchar(MAX),
+	@RequestHeaders		nvarchar(MAX),
+	@RequestBody		nvarchar(MAX),
+	@RequestTime		datetime,
+	@ResponseHeaders	nvarchar(MAX),
+	@Response			nvarchar(MAX),
+	@ResponseTime		datetime,
+	@ResponseStatusCode	int,
+	@IsSucceded			bit,
+	@Exception			nvarchar(MAX)
+AS
+BEGIN
+	INSERT INTO [TOneWFTracking].[logging].[VRHttpConnectionLog]
+	   ([VRHttpConnectionId]
+       ,[BaseURL]
+       ,[Path]
+       ,[Parameters]
+       ,[RequestHeaders]
+       ,[RequestBody]
+       ,[RequestTime]
+       ,[ResponseHeaders]
+       ,[Response]
+       ,[ResponseTime]
+       ,[ResponseStatusCode]
+       ,[IsSucceded]
+       ,[Exception])
+	  VALUES
+	  (@VRHttpConnectionId,
+	   @BaseURL,
+	   @Path,
+	   @Parameters,
+	   @RequestHeaders,
+	   @RequestBody,
+	   @RequestTime,
+	   @ResponseHeaders,
+	   @Response,
+	   @ResponseTime,
+	   @ResponseStatusCode,
+	   @IsSucceded,
+	   @Exception)
+END
---
+++ TOneV2/Code/TOneV2/SQL.TOneLogging/logging/Tables/VRHttpConnectionLog.sql
@@ -0,0 +1,23 @@
+﻿CREATE TABLE [logging].[VRHttpConnectionLog] (
+    [Id]                 BIGINT           NOT NULL,
+    [VRHttpConnectionId] UNIQUEIDENTIFIER NULL,
+    [BaseURL]            NVARCHAR (MAX)   NULL,
+    [Path]               NVARCHAR (MAX)   NULL,
+    [Parameters]         NVARCHAR (MAX)   NULL,
+    [RequestHeaders]     NVARCHAR (MAX)   NULL,
+    [RequestBody]        NVARCHAR (MAX)   NULL,
+    [RequestTime]        DATETIME         NOT NULL,
+    [ResponseHeaders]    NVARCHAR (MAX)   NULL,
+    [Response]           NVARCHAR (MAX)   NULL,
+    [ResponseTime]       DATETIME         NULL,
+    [ResponseStatusCode] INT              NULL,
+    [IsSucceded]         BIT              NULL,
+    [Exception]          NVARCHAR (MAX)   NULL,
+    CONSTRAINT [PK_VRHttpConnectionLog] PRIMARY KEY NONCLUSTERED ([Id] ASC)
+);
+
+
+GO
+CREATE CLUSTERED INDEX [IX_VRHttpConnectionLog_RequestTime]
+    ON [logging].[VRHttpConnectionLog]([RequestTime] ASC);
+
