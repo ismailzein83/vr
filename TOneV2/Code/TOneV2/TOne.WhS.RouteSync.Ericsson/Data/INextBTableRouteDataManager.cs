81ecbed434ba501ab579af53048ffdb74c771f3f
Rodi Hassan <VANRISE\rodi.hassan>
2019-10-03 14:08:23 +0000

    add INextBTableRouteDataManager
    git-tfs-id: [http://developmentvlan:8585/tfs/vanrise.collection]$/;C58458

---
+++ TOneV2/Code/TOneV2/TOne.WhS.RouteSync.Ericsson/Data/INextBTableRouteDataManager.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Vanrise.Data;
+
+namespace TOne.WhS.RouteSync.Ericsson.Data
+{
+    public interface INextBTableRouteDataManager : IDataManager, IBulkApplyDataManager<NextBTableConvertedRoute>
+    {
+        string SwitchId { get; set; }
+
+        List<NextBTableConvertedRoute> GetAllNextBTableRoute();
+
+        void Initialize(INextBTableRouteInitializeContext context);
+
+        void InsertNextBTableRoutes(IEnumerable<NextBTableConvertedRoute> routes);
+    }
+}
