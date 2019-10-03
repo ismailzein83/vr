514af03717b8f998835e8bc4ec9e1d2dfa51962f
Rodi Hassan <VANRISE\rodi.hassan>
2019-10-03 07:40:31 +0000

    fix issue in VRHttpConnectionLogDataManager
    git-tfs-id: [http://developmentvlan:8585/tfs/vanrise.collection]$/;C58433

--- Vanrise/Code/Vanrise.Common.Data.SQL/VRHttpConnectionLogDataManager.cs
+++ Vanrise/Code/Vanrise.Common.Data.SQL/VRHttpConnectionLogDataManager.cs
@@ -10,10 +10,15 @@ namespace Vanrise.Common.Data.SQL
 {
     public class VRHttpConnectionLogDataManager : BaseSQLDataManager, IVRHttpConnectionLogDataManager
     {
+        public VRHttpConnectionLogDataManager()
+    : base(GetConnectionStringName("LoggingDBConnStringKey", "LogDBConnString"))
+        {
+
+        }
         public bool Insert(Guid VRHttpConnectionId, string BaseURL, string Path, string Parameters, string RequestHeaders, string RequestBody, DateTime RequestTime,
             string ResponseHeaders, string Response, DateTime? ResponseTime, HttpStatusCode? ResponseStatusCode, bool IsSucceded, string Exception)
         {
-            return (ExecuteNonQuerySP("sp_VRHttpConnectionLog_Insert", VRHttpConnectionId, BaseURL, Path, Parameters, RequestHeaders, RequestBody, RequestTime,
+            return (ExecuteNonQuerySP("logging.sp_VRHttpConnectionLog_Insert", VRHttpConnectionId, BaseURL, Path, Parameters, RequestHeaders, RequestBody, RequestTime,
              ResponseHeaders, Response, ResponseTime, ResponseStatusCode, IsSucceded, Exception) > 0);
         }
     }
