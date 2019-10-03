e80a303ae323b43c3f7f2d01e722327dfac752cf
Wissam Ajamy <VANRISE\wissam.ajamy>
2019-10-03 12:49:29 +0000

    fix build error
    git-tfs-id: [http://developmentvlan:8585/tfs/vanrise.collection]$/;C58451

--- Retail/Retail.SQL/SQL.TestCallAnalysis_CDR/TCAnal_CDR/Views/vw_SentReportRecordsTypeA.sql
+++ Retail/Retail.SQL/SQL.TestCallAnalysis_CDR/TCAnal_CDR/Views/vw_SentReportRecordsTypeA.sql
@@ -1,9 +1,10 @@
 ﻿
 
 
+
 CREATE VIEW [TCAnal_CDR].[vw_SentReportRecordsTypeA]
 AS
-SELECT reportrecord.CaseId, CASE WHEN report.Type = 1 THEN 1 else 0 end as IsSend,report.SentTime
-FROM [Retail_Dev_TestCallAnalysis_CDR].[TCAnal_CDR].[ReportRecords] reportrecord 
-JOIN [Retail_Dev_TestCallAnalysis_CDR].[TCAnal_CDR].[Report] report on  report.Id = reportrecord.ReportId
+SELECT	reportrecord.CaseId, CASE WHEN report.Type = 1 THEN 1 else 0 end as IsSend,report.SentTime
+FROM	[TCAnal_CDR].[ReportRecords] reportrecord 
+JOIN	[TCAnal_CDR].[Report] report on  report.Id = reportrecord.ReportId
 WHERE report.Type = 1
--- Retail/Retail.SQL/SQL.TestCallAnalysis_CDR/TCAnal_CDR/Views/vw_SentReportRecordsTypeC.sql
+++ Retail/Retail.SQL/SQL.TestCallAnalysis_CDR/TCAnal_CDR/Views/vw_SentReportRecordsTypeC.sql
@@ -1,9 +1,10 @@
 ﻿
 
 
+
 CREATE VIEW [TCAnal_CDR].[vw_SentReportRecordsTypeC]
 AS
 SELECT reportrecord.CaseId, CASE WHEN report.Type = 3 THEN 1 else 0 end as IsSend,report.SentTime
-FROM [Retail_Dev_TestCallAnalysis_CDR].[TCAnal_CDR].[ReportRecords] reportrecord 
-JOIN [Retail_Dev_TestCallAnalysis_CDR].[TCAnal_CDR].[Report] report on  report.Id = reportrecord.ReportId
+FROM [TCAnal_CDR].[ReportRecords] reportrecord 
+JOIN [TCAnal_CDR].[Report] report on  report.Id = reportrecord.ReportId
 WHERE report.Type = 3
