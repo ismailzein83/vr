2948b4dee6040a28de8bc304d854867e6ef644f6
Wissam Ajamy <VANRISE\wissam.ajamy>
2019-10-04 13:30:54 +0000

    update path
    git-tfs-id: [http://developmentvlan:8585/tfs/vanrise.collection]$/;C58487

--- Vanrise/Code/Resources/BatchScripts/PATool/All.bat
+++ Vanrise/Code/Resources/BatchScripts/PATool/All.bat
@@ -2,7 +2,7 @@ ECHO OFF
 ECHO.
 ECHO This script will transfer PA website, plug-ins, and switches to its related published destinations
 pause
-FOR %%i in (C:\TFS\Vanrise\Code\Resources\BatchScripts\PA\*.bat) do IF NOT "%%i" == %0 CALL %%i 
+FOR %%i in (C:\TFS\Vanrise\Code\Resources\BatchScripts\PATool\*.bat) do IF NOT "%%i" == %0 CALL %%i 
 ECHO.	
 ECHO End of script
 pause
