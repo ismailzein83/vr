89911e7fe1f6db83b7c0ef14862c51e68035b3c1
Wissam Ajamy <VANRISE\wissam.ajamy>
2019-10-04 13:25:25 +0000

    Adding Productivity accelerator project
    git-tfs-id: [http://developmentvlan:8585/tfs/vanrise.collection]$/;C58485

---
+++ Vanrise/Code/Resources/BatchScripts/PATool/02-DataBaseFirstDeploymentScripts.bat
@@ -0,0 +1,19 @@
+ECHO OFF
+::Make Directory from current date as YYYYMMDD
+Set CURRDATE=%TEMP%\CURRDATE.TMP
+DATE /T > %CURRDATE%
+Set PARSEARG="eol=; tokens=1,2,3,4* delims=/, "
+For /F %PARSEARG% %%i in (%CURRDATE%) Do SET YYYYMMDD=%%l%%j%%k
+
+ECHO PA DataBase First Deployment Scripts
+
+xcopy "C:\TFS\TOneV2\Code\TOneV2\SQL.TOneConfiguration\VRJSON_PostScripts\Common\VR_Common.json" /y /v /z /i /Q /R					"\\192.168.110.185\Fixes\WebSite\PA\%YYYYMMDD%\DataBaseFirstDeploymentScripts\Configuration\"
+xcopy "C:\TFS\TOneV2\Code\TOneV2\SQL.TOneConfiguration\VRJSON_PostScripts\Common\VR_Generic.json" /y /v /z /i /Q /R					"\\192.168.110.185\Fixes\WebSite\PA\%YYYYMMDD%\DataBaseFirstDeploymentScripts\Configuration\"
+xcopy "C:\TFS\TOneV2\Code\TOneV2\SQL.TOneConfiguration\VRJSON_PostScripts\Common\VR_Security.json" /y /v /z /i /Q /R				"\\192.168.110.185\Fixes\WebSite\PA\%YYYYMMDD%\DataBaseFirstDeploymentScripts\Configuration\"
+xcopy "C:\TFS\TOneV2\Code\TOneV2\SQL.TOneConfiguration\VRJSON_PostScripts\Common\VR_Analytic.json" /y /v /z /i /Q /R				"\\192.168.110.185\Fixes\WebSite\PA\%YYYYMMDD%\DataBaseFirstDeploymentScripts\Configuration\"
+xcopy "C:\TFS\TOneV2\Code\TOneV2\SQL.TOneConfiguration\VRJSON_PostScripts\Common\VR_ExcelConversion.json" 		/y /v /z /i /Q /R	"\\192.168.110.185\Fixes\WebSite\PA\%YYYYMMDD%\DataBaseFirstDeploymentScripts\Configuration\"
+xcopy "C:\TFS\TOneV2\Code\TOneV2\SQL.TOneConfiguration\VRJSON_PostScripts\Common\VR_Notification.json" /y /v /z /i /Q /R			"\\192.168.110.185\Fixes\WebSite\PA\%YYYYMMDD%\DataBaseFirstDeploymentScripts\Configuration\"
+xcopy "C:\TFS\TOneV2\Code\TOneV2\SQL.TOneConfiguration\VRJSON_PostScripts\PA\PATool.json" /y /v /z /i /Q /R							"\\192.168.110.185\Fixes\WebSite\PA\%YYYYMMDD%\DataBaseFirstDeploymentScripts\Configuration\"
+xcopy "C:\TFS\TOneV2\Code\TOneV2\SQL.TOneConfiguration\Configuration.txt" /y /v /z /i /Q /R											"\\192.168.110.185\Fixes\WebSite\PA\%YYYYMMDD%\DataBaseFirstDeploymentScripts\Configuration\"
+xcopy "C:\TFS\TOneV2\Code\TOneV2\SQL.TOneTransaction\BusinessProcess.PostDeployment.sql" /y /v /z /i /Q /R							"\\192.168.110.185\Fixes\WebSite\PA\%YYYYMMDD%\DataBaseFirstDeploymentScripts\Transaction\"
+xcopy "C:\TFS\TOneV2\Code\TOneV2\SQL.TOneLogging\Script.PostDeployment.sql" /y /v /z /i /Q /R										"\\192.168.110.185\Fixes\WebSite\PA\%YYYYMMDD%\DataBaseFirstDeploymentScripts\Logging\"
---
+++ Vanrise/Code/Resources/BatchScripts/PATool/03-WebSite.bat
@@ -0,0 +1,32 @@
+ECHO OFF
+::Make Directory from current date as YYYYMMDD
+Set CURRDATE=%TEMP%\CURRDATE.TMP
+DATE /T > %CURRDATE%
+Set PARSEARG="eol=; tokens=1,2,3,4* delims=/, "
+For /F %PARSEARG% %%i in (%CURRDATE%) Do SET YYYYMMDD=%%l%%j%%k
+
+ECHO PA WebSite
+
+xcopy "C:\Publish\Vanrise.Web.Host" /S /E /R /y /v /i /z /Q												"\\192.168.110.185\Fixes\WebSite\PA\%YYYYMMDD%\" /EXCLUDE:C:\TFS\Vanrise\Code\Resources\BatchScripts\PA\list-of-excluded-files.txt
+xcopy "C:\TFS\Vanrise\Code\Vanrise.Web\Client" /S /E /R /y /v /i /z /Q									"\\192.168.110.185\Fixes\WebSite\PA\%YYYYMMDD%\Client\" /EXCLUDE:C:\TFS\Vanrise\Code\Resources\BatchScripts\Common\Client-list-of-excluded-files.txt
+xcopy "C:\TFS\TOneV2\Code\TOneV2\TOne.Web\Web.config.PA.exclud" /y /v /z /i /Q /R						"\\192.168.110.185\Fixes\WebSite\PA\%YYYYMMDD%\Web.config*"
+::by default load flat theme
+xcopy "C:\TFS\TOneV2\Code\TOneV2\TOne.Web\Images\flat-DevTool-logoonheader.png" /S /R /y /v /i /z /Q	"\\192.168.110.185\Fixes\WebSite\PA\%YYYYMMDD%\Images\logoonheader.png*"
+xcopy "C:\TFS\TOneV2\Code\TOneV2\TOne.Web\Images\flat-DevTool-login.png" /S /R /y /v /i /z /Q			"\\192.168.110.185\Fixes\WebSite\PA\%YYYYMMDD%\Images\login.png*"
+xcopy "C:\TFS\TOneV2\Code\TOneV2\TOne.Web\Images\flat-tone-iconheader.ico" /S /R /y /v /i /z /Q			"\\192.168.110.185\Fixes\WebSite\PA\%YYYYMMDD%\Images\iconheader.ico*"
+xcopy "C:\TFS\Vanrise\Code\Vanrise.Web\Client\Images\new-flat-01.png" 		 /S /R /y /v /i /z /Q		"\\192.168.110.185\Fixes\WebSite\PA\%YYYYMMDD%\Client\Images\01.png*"
+xcopy "C:\TFS\Vanrise\Code\Vanrise.Web\Client\Images\new-flat-02.png" /S /R /y /v /i /z /Q				"\\192.168.110.185\Fixes\WebSite\PA\%YYYYMMDD%\Client\Images\02.png*"
+xcopy "C:\TFS\Vanrise\Code\Vanrise.Web\Client\Images\new-flat-member.png" /S /R /y /v /i /z /Q			"\\192.168.110.185\Fixes\WebSite\PA\%YYYYMMDD%\Client\Images\member.png*"
+xcopy "C:\TFS\Vanrise\Code\Vanrise.Web\Client\Images\new-flat-support.png" /S /R /y /v /i /z /Q			"\\192.168.110.185\Fixes\WebSite\PA\%YYYYMMDD%\Client\Images\support.png*"
+xcopy "C:\TFS\Vanrise\Code\Vanrise.Web\Client\CSViews\Home\Index-flat.cshtml" /S /R /y /v /i /z /Q		"\\192.168.110.185\Fixes\WebSite\PA\%YYYYMMDD%\Client\CSViews\Home\Index.cshtml*"
+xcopy "C:\TFS\Vanrise\Code\Vanrise.Web\Client\CSViews\Security\Login-flat.cshtml" /S /R /y /v /i /z /Q	"\\192.168.110.185\Fixes\WebSite\PA\%YYYYMMDD%\Client\CSViews\Security\Login.cshtml*"
+xcopy "C:\TFS\Vanrise\Code\Resources\DLLs\Aspose.Cells.lic" /y /v /z /i /Q /R							"\\192.168.110.185\Fixes\WebSite\PA\%YYYYMMDD%\Bin\"
+xcopy "C:\TFS\Vanrise\Code\Resources\DLLs\ICSharpCode.SharpZipLib.dll" /y /v /z /i /Q /R				"\\192.168.110.185\Fixes\WebSite\PA\%YYYYMMDD%\Bin\"
+xcopy "C:\TFS\Vanrise\Code\Vanrise.Security.Web\Security" /S /E /R /y /v /i /z /Q						"\\192.168.110.185\Fixes\WebSite\PA\%YYYYMMDD%\Client\Modules\Security\"
+xcopy "C:\TFS\Vanrise\Code\Vanrise.Common.Web\Common" /S /E /R /y /v /i /z /Q							"\\192.168.110.185\Fixes\WebSite\PA\%YYYYMMDD%\Client\Modules\Common\"
+xcopy "C:\TFS\Vanrise\Code\Vanrise.Notification.Web\VR_Notification" /S /E /R /y /v /i /z /Q			"\\192.168.110.185\Fixes\WebSite\PA\%YYYYMMDD%\Client\Modules\VR_Notification\"
+xcopy "C:\TFS\Vanrise\Code\Vanrise.GenericData.Web\VR_GenericData" /S /E /R /y /v /i /z /Q				"\\192.168.110.185\Fixes\WebSite\PA\%YYYYMMDD%\Client\Modules\VR_GenericData\"
+xcopy "C:\TFS\Vanrise\Code\Vanrise.Analytic.Web\Analytic" /S /E /R /y /v /i /z /Q						"\\192.168.110.185\Fixes\WebSite\PA\%YYYYMMDD%\Client\Modules\Analytic\"
+xcopy "C:\TFS\Vanrise\Code\Vanrise.ExcelConversion.Web\ExcelConversion" /S /E /R /y /v /i /z /Q			"\\192.168.110.185\Fixes\WebSite\PA\%YYYYMMDD%\Client\Modules\ExcelConversion\"
+del /s /q /f																							"\\192.168.110.185\Fixes\WebSite\PA\%YYYYMMDD%\Bin\*.pdb"
+del /s /q /f																							"\\192.168.110.185\Fixes\WebSite\PA\%YYYYMMDD%\Bin\*.config"
