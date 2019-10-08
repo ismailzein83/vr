67bc21be90966a9003fa2dc4f476156f4d383503
Ali Atoui <VANRISE\ali.atoui>
2019-10-08 15:18:56 +0000

    Adding RecordFilterCustomObjectTypeSettings
    git-tfs-id: [http://developmentvlan:8585/tfs/vanrise.collection]$/;C58558

---
+++ Vanrise/Code/Vanrise.GenericData.MainExtensions/CustomObject/RecordFilterCustomObjectTypeSettings.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using Vanrise.GenericData.Entities;
+
+namespace Vanrise.GenericData.MainExtensions.CustomObject
+{
+    public class RecordFilterCustomObjectTypeSettings : FieldCustomObjectTypeSettings
+    {
+        public override Guid ConfigId => throw new NotImplementedException();
+
+        public Guid DataRecordTypeID { get; set; }
+
+        public override bool AreEqual(object newValue, object oldValue)
+        {
+            throw new NotImplementedException();
+        }
+
+        public override string GetDescription(IFieldCustomObjectTypeSettingsContext context)
+        {
+            throw new NotImplementedException();
+        }
+
+        public override Type GetNonNullableRuntimeType()
+        {
+            throw new NotImplementedException();
+        }
+
+        public override string GetRuntimeTypeDescription()
+        {
+            throw new NotImplementedException();
+        }
+
+        public override dynamic ParseNonNullValueToFieldType(object originalValue)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
