dc55985c281262444ea39c5214af7193a5a4e6fe
Ali Atoui <VANRISE\ali.atoui>
2019-10-09 09:00:22 +0000

    Arranging FieldCustomObjectTypeSettings
    git-tfs-id: [http://developmentvlan:8585/tfs/vanrise.collection]$/;C58572

--- Vanrise/Code/Vanrise.GenericData.Entities/DataRecordTypes/FieldCustomObjectTypeSettings.cs
+++ Vanrise/Code/Vanrise.GenericData.Entities/DataRecordTypes/FieldCustomObjectTypeSettings.cs
@@ -1,49 +1,39 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace Vanrise.GenericData.Entities
 {
-
     public abstract class FieldCustomObjectTypeSettings
     {
         public abstract Guid ConfigId { get; }
-        public abstract string GetDescription(IFieldCustomObjectTypeSettingsContext context);
-        public abstract Type GetNonNullableRuntimeType();
-        public abstract bool AreEqual(Object newValue, Object oldValue);
-        public abstract dynamic ParseNonNullValueToFieldType(Object originalValue);
-        public abstract string GetRuntimeTypeDescription();
-        public virtual string SelectorUIControl
-        {
-            get
-            {
-                return null;
-            }
-        }
 
-        public virtual string RuleFilterSelectorUIControl
-        {
-            get
-            {
-                return this.SelectorUIControl;
-            }
-        }
+        public virtual string SelectorUIControl { get { return null; } }
+
+        public virtual string RuleFilterSelectorUIControl { get { return this.SelectorUIControl; } }
+
+        public abstract Type GetNonNullableRuntimeType();
+
+        public abstract string GetRuntimeTypeDescription();
+
+        public abstract string GetDescription(IFieldCustomObjectTypeSettingsContext context);
+
+        public abstract dynamic ParseNonNullValueToFieldType(Object originalValue);
+
+        public abstract bool AreEqual(Object newValue, Object oldValue);
 
         public virtual bool IsMatched(IFieldCustomObjectTypeSettingsContext context)
         {
             return true;
         }
+
         public virtual void GetValueByDescription(IFieldCustomObjectTypeSettingsGetValueByDescriptionContext context)
         {
             throw new NotImplementedException();
         }
-       
     }
+
     public interface IFieldCustomObjectTypeSettingsContext
     {
         object FieldValue { get; }
-        RecordFilter RecordFilter { get;  }
+        RecordFilter RecordFilter { get; }
     }
 }
