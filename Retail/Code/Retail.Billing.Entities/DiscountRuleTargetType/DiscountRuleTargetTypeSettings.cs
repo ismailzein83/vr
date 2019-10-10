2a10cac678361cc5d1c6e39178862bcae6874fce
Mohammad Sheaib <VANRISE\mohammad.sheaib>
2019-10-10 14:43:10 +0000

    Modifying DiscountRuleTargetTypeSettings
    git-tfs-id: [http://developmentvlan:8585/tfs/vanrise.collection]$/;C58609

--- Retail/Code/Retail.Billing.Entities/DiscountRuleTargetType/DiscountRuleTargetTypeSettings.cs
+++ Retail/Code/Retail.Billing.Entities/DiscountRuleTargetType/DiscountRuleTargetTypeSettings.cs
@@ -8,7 +8,7 @@ namespace Retail.Billing.Entities
 {
     public class DiscountRuleTargetTypeSettings : Vanrise.Entities.VRComponentTypeSettings
     {
-        public override Guid VRComponentTypeConfigId => throw new NotImplementedException();
+        public override Guid VRComponentTypeConfigId => new Guid("91441947-14A0-4AB6-AC7E-A023BF9C809B");
 
         public DiscountRuleTargetTypeExtendedSettings ExtendedSettings { get; set; }
     }
