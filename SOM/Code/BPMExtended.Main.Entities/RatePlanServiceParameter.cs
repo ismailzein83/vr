189bb83f5b215235cbd5f6392d79c74d1c2a1dc1
Hassan Makhour <VANRISE\hassan.makhour>
2019-10-15 12:41:01 +0000

    working with service parameter
    git-tfs-id: [http://developmentvlan:8585/tfs/vanrise.collection]$/;C58689

---
+++ SOM/Code/BPMExtended.Main.Entities/RatePlanServiceParameter.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BPMExtended.Main.Entities
+{
+    public class RatePlanServiceParameter
+    {
+        public string ParameterName { get; set; }
+        public ParameterType ParameterType { get; set; }
+    }
+    public enum ParameterType
+    {
+        TB = 0,
+        CB = 1,
+        LB = 2
+    }
+}
