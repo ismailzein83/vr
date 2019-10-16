3eda0b52bf829236e99a537de2d30bcdae8f6077
Ayman Zinji <VANRISE\ayman.zinji>
2019-10-16 11:31:43 +0000

    work order manager
    git-tfs-id: [http://developmentvlan:8585/tfs/vanrise.collection]$/;C58713

---
+++ SOM/Code/BPMExtended.Main.Business/Work Orders/WorkOrdersManager.cs
@@ -0,0 +1,17 @@
+﻿using BPMExtended.Main.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BPMExtended.Main.Business
+{
+    public class WorkOrdersManager
+    {
+        public string CreateWorkOrder(WorkOrder w)
+        {
+            return "success";
+        }
+    }
+}
