cd58c58264d2ad3809f457df7e4bb783cc83dd67
Mohamad Abdallah <VANRISE\mohamad.abdallah>
2019-10-17 13:59:11 +0000

    fixed birthdate
    git-tfs-id: [http://developmentvlan:8585/tfs/vanrise.collection]$/;C58744

--- SOM/Code/BPMExtended.Main.Business/Billing/CRMCustomerManager.cs
+++ SOM/Code/BPMExtended.Main.Business/Billing/CRMCustomerManager.cs
@@ -981,7 +981,7 @@ namespace BPMExtended.Main.Business
                         AddressSeq = long.Parse(addressId.ToString()),
                         Street = street.ToString(),
                         AddressNotes = notes.ToString(),
-                        Birthdate = birthDate.ToString("dd/MM/yyyy"),
+                        Birthdate = birthDate.ToString("MM/dd/yyyy"),
                         BusinessPhone = businessPhone.ToString(),
                         Career = career.ToString(),
                         DocumentIdType = documentIdType.ToString(),
