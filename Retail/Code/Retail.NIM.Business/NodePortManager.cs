ae44ca4cdf383e8da06b3cf474e6006041651109
Samer Haidar <VANRISE\samer.haidar>
2019-10-23 12:57:28 +0000

    Implementing ReserveConnection and ReservePort
    git-tfs-id: [http://developmentvlan:8585/tfs/vanrise.collection]$/;C58790

--- Retail/Code/Retail.NIM.Business/NodePortManager.cs
+++ Retail/Code/Retail.NIM.Business/NodePortManager.cs
@@ -3,24 +3,152 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Vanrise.GenericData.Business;
+using Vanrise.GenericData.Entities;
+using Vanrise.Common;
 
 namespace Retail.NIM.Business
 {
-    public  class NodePortManager
+    public class NodePortManager
     {
         static Guid _connectionDefinitionId = new Guid("e74d9d5d-cc59-4b40-8626-048a755c054c");
+        static Guid _nodePortDefinitionId = new Guid("04868fe5-9944-4e2b-b4d2-de9c5f73e2f4");
+        static Guid _freeNodeStatus = new Guid("a11d2835-89ed-442c-9646-c1f9b23ff213");
+
+        GenericBusinessEntityManager _genericBusinessEntityManager = new GenericBusinessEntityManager();
         public ReserveConnectionOutput ReserveConnection(ReserveConnectionInput input)
         {
-            return null;
+            var filter = new Vanrise.GenericData.Entities.RecordFilterGroup
+            {
+                Filters = new List<Vanrise.GenericData.Entities.RecordFilter>
+                {
+                    new ObjectListRecordFilter
+                    {
+                        FieldName ="Port1Node",
+                        Values =  new List<object> {input.Port1NodeId}
+                    }
+                   ,new ObjectListRecordFilter
+                    {
+                        FieldName ="Port2Node",
+                        Values =  new List<object> {input.Port2NodeId}
+                    }
+                   ,new ObjectListRecordFilter
+                   {
+                        FieldName = "Port1Status",
+                        Values = new List<object> { _freeNodeStatus.ToString() }
+                   }
+                   ,new ObjectListRecordFilter
+                   {
+                       FieldName = "Port2Status",
+                       Values = new List<object> { _freeNodeStatus.ToString() }
+                   }
+                }
+            };
+
+            if (input.Port1PartTypeId.HasValue)
+            {
+                filter.Filters.Add(new ObjectListRecordFilter
+                {
+                    FieldName = "Port1PartType",
+                    Values = new List<object> { input.Port1PartTypeId.Value }
+                });
+            }
+
+            if (input.Port2PartTypeId.HasValue)
+            {
+                filter.Filters.Add(new ObjectListRecordFilter
+                {
+                    FieldName = "Port2PartType",
+                    Values = new List<object> { input.Port2PartTypeId.Value }
+                });
+            }
+
+            var entities = _genericBusinessEntityManager.GetAllGenericBusinessEntities(_connectionDefinitionId, null, filter);
+
+            if (entities == null || entities.Count() == 0)
+                return null;
+
+            var firstItem = entities.First();
+
+            var port1id = (long)firstItem.FieldValues.GetRecord("Port1");
+
+            var port2id = (long)firstItem.FieldValues.GetRecord("Port2");
+
+            var reservedPort1 = ReservePort(port1id);
+
+            var reservedPort2 = ReservePort(port2id);
+
+            return new ReserveConnectionOutput
+            {
+                Port1 = reservedPort1,
+                Port2 = reservedPort2
+            };
         }
 
         public ReservePortOutput ReservePort(ReservePortInput input)
         {
-            return null;
+
+            var reserveStatus = new Guid("c51bb41b-b31a-45ba-b12e-8f521b0323eb");
+            var updatedEntity = _genericBusinessEntityManager.UpdateGenericBusinessEntity(new GenericBusinessEntityToUpdate
+            {
+                BusinessEntityDefinitionId = _nodePortDefinitionId,
+                FieldValues = new Dictionary<string, object> { { "Status", reserveStatus } },
+                FilterGroup = new RecordFilterGroup
+                {
+                    Filters = new List<Vanrise.GenericData.Entities.RecordFilter>
+                    {
+                        new ObjectListRecordFilter
+                        {
+                              FieldName = "Node",
+                              Values = new List<object> { input.NodeId }
+                        },
+                        new ObjectListRecordFilter
+                        {
+                              FieldName = "Status",
+                              Values = new List<object> { _freeNodeStatus }
+                        }
+                   }
+                }
+            });
+            if (updatedEntity.Result == Vanrise.Entities.UpdateOperationResult.Failed)
+                return null;
+
+            return new ReservePortOutput
+            {
+                PortId = (long)updatedEntity.UpdatedObject.FieldValues.GetRecord("ID").Value,
+                Number = updatedEntity.UpdatedObject.FieldValues.GetRecord("Number").Description
+            };
+
         }
-        public ReservePortOutput ReservePort(int portId)
+        public ReservePortOutput ReservePort(long portId)
         {
-            return null;
+            var reserveStatus = new Guid("c51bb41b-b31a-45ba-b12e-8f521b0323eb");
+            var updatedEntity = _genericBusinessEntityManager.UpdateGenericBusinessEntity(new GenericBusinessEntityToUpdate
+            {
+                BusinessEntityDefinitionId = _nodePortDefinitionId,
+                FieldValues = new Dictionary<string, object> { { "Status", reserveStatus } },
+                GenericBusinessEntityId = portId,
+                FilterGroup =new RecordFilterGroup
+                {
+                    Filters = new List<Vanrise.GenericData.Entities.RecordFilter>
+                    {
+                        new ObjectListRecordFilter
+                        {
+                              FieldName = "Status",
+                              Values = new List<object> { _freeNodeStatus }
+                        }
+                   }
+                }
+            });
+
+            if (updatedEntity.Result == Vanrise.Entities.UpdateOperationResult.Failed)
+                return null;
+
+            return new ReservePortOutput
+            {
+                PortId = portId,
+                Number = updatedEntity.UpdatedObject.FieldValues.GetRecord("Number").Description
+            };
         }
     }
     public class ReserveConnectionOutput
@@ -31,8 +159,11 @@ namespace Retail.NIM.Business
     public class ReserveConnectionInput
     {
         public Guid ConnectionTypeId { get; set; }
-        public long NodeId { get; set; }
-        public Guid? PartTypeId { get; set; }
+        public long Port1NodeId { get; set; }
+        public long Port2NodeId { get; set; }
+        public Guid? Port1PartTypeId { get; set; }
+        public Guid? Port2PartTypeId { get; set; }
+
     }
     public class ReservePortInput
     {
@@ -41,7 +172,7 @@ namespace Retail.NIM.Business
     }
     public class ReservePortOutput
     {
-        public long NodeId { get; set; }
+        public long PortId { get; set; }
         public string Number { get; set; }
     }
 }
