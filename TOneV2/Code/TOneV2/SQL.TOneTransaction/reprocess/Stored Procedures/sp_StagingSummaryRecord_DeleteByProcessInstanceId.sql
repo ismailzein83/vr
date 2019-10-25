90cd9db6be3aab5f1490c154772aa0a3a487406c
anthony douaihy <VANRISE\anthony.douaihy>
2019-10-25 15:24:32 +0000

    add sp_StagingSummaryRecord_DeleteByProcessInstanceId SP
    git-tfs-id: [http://developmentvlan:8585/tfs/vanrise.collection]$/;C58829

---
+++ TOneV2/Code/TOneV2/SQL.TOneTransaction/reprocess/Stored Procedures/sp_StagingSummaryRecord_DeleteByProcessInstanceId.sql
@@ -0,0 +1,9 @@
+﻿
+CREATE PROCEDURE [reprocess].[sp_StagingSummaryRecord_DeleteByProcessInstanceId] 
+	@ProcessInstanceId bigint
+
+AS
+BEGIN
+	Delete reprocess.StagingSummaryRecord  from reprocess.StagingSummaryRecord WITH (NOLOCK)
+    where ProcessInstanceId = @ProcessInstanceId
+END
