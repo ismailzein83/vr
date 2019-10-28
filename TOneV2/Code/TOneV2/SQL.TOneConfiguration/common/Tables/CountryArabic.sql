e24c4664ba79cafeb1bb6bd829bea61787fbbd16
Fatima Al Heck <VANRISE\fatima.alheck>
2019-10-28 16:37:31 +0000

    QualityNet post scripts
    git-tfs-id: [http://developmentvlan:8585/tfs/vanrise.collection]$/;C58850

---
+++ TOneV2/Code/TOneV2/SQL.TOneConfiguration/common/Tables/CountryArabic.sql
@@ -0,0 +1,11 @@
+﻿CREATE TABLE [common].[CountryArabic] (
+    [CountryId]        INT            NOT NULL,
+    [ArabicName]       NVARCHAR (255) NULL,
+    [CreatedBy]        INT            NULL,
+    [CreatedTime]      DATETIME       NULL,
+    [LastModifiedBy]   INT            NULL,
+    [LastModifiedTime] DATETIME       NULL,
+    [timestamp]        ROWVERSION     NULL,
+    PRIMARY KEY CLUSTERED ([CountryId] ASC)
+);
+
