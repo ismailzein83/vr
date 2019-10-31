af05e428cb3d3da2025d893d64ade564947c514c
Mohammad Yahfoufi <VANRISE\mohammad.yahfoufi>
2019-10-31 08:55:07 +0000

    adding new function (apply installments) and new input and updating previous functions - Ali Sakr
    git-tfs-id: [http://developmentvlan:8585/tfs/vanrise.collection]$/;C58912

--- SOM/Code/BPMExtended.Main.Business/Billing/InstallmentManager.cs
+++ SOM/Code/BPMExtended.Main.Business/Billing/InstallmentManager.cs
@@ -1,5 +1,6 @@
 ﻿using BPMExtended.Main.Common;
 using BPMExtended.Main.Entities;
+using BPMExtended.Main.SOMAPI;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -24,14 +25,25 @@ namespace BPMExtended.Main.Business
             }
             return installmentTemplateInfoItems;
         }
-        public List<InstallmentDetail> GetInstallments(string customerId,string paymentPlanTemplateId,string invoiceCode,string invoiceAmount,string additionalFees,string lateFee,string firstPayment,string reductionRate,string currency,string startDate,string approvalId)
+        public List<InstallmentDetail> GetInstallments(string customerId, string paymentPlanTemplateId, string invoiceCode, string invoiceAmount, string additionalFees, string lateFee, string firstPayment, string reductionRate, string currency, string startDate, string approvalId)
         {
-
+            var input = new SimulateInstallmentInput {
+                CustomerId = customerId,
+                PaymentPlanTemplateId = paymentPlanTemplateId,
+                InvoiceCode = invoiceCode,
+                InvoiceAmount = invoiceAmount,
+                AdditionalFees = additionalFees,
+                LateFee = lateFee,
+                FirstPayment = firstPayment,
+                ReductionRate = reductionRate,
+                Currency = currency,
+                StartDate = startDate,
+                ApprovalId = approvalId
+            };
             var installmentsDetailItems = new List<InstallmentDetail>();
             using (SOMClient client = new SOMClient())
             {
-                List<Installment> items = client.Get<List<Installment>>(String.Format("api/SOM.ST/Billing/SimulateInstallment?CustomerId={0}&PaymentPlanTemplateId={1}&InvoiceId={2}&InvoiceAmount={3}&AdditionalFees={4}&LateFee={5}&FirstPayment={6}&ReductionRate={7}&Currency={8}&StartDate={9}&ApprovalId={10}"
-                    , customerId, paymentPlanTemplateId, invoiceCode, invoiceAmount, additionalFees, lateFee, firstPayment, reductionRate, currency, startDate, approvalId));
+                List<Installment> items = client.Post<SimulateInstallmentInput, List<Installment>>("api/SOM.ST/Billing/SimulateInstallment", input);
                 foreach (var item in items)
                 {
                     var detailItem = InstallmentToDetailMapper(item);
@@ -40,12 +52,34 @@ namespace BPMExtended.Main.Business
             }
             return installmentsDetailItems;
         }
+        public void ApplyInstallment(string customerId, string paymentPlanTemplateId, string invoiceCode, string invoiceAmount, string additionalFees, string lateFee, string firstPayment, string reductionRate, string currency, string startDate, string approvalId)
+        {
+            var input = new SimulateInstallmentInput
+            {
+                CustomerId = customerId,
+                PaymentPlanTemplateId = paymentPlanTemplateId,
+                InvoiceCode = invoiceCode,
+                InvoiceAmount = invoiceAmount,
+                AdditionalFees = additionalFees,
+                LateFee = lateFee,
+                FirstPayment = firstPayment,
+                ReductionRate = reductionRate,
+                Currency = currency,
+                StartDate = startDate,
+                ApprovalId = approvalId
+            };
+            var ProcessInstanceId = new SOMRequestOutput();
+            using (SOMClient client = new SOMClient())
+            {
+                ProcessInstanceId = client.Post<SimulateInstallmentInput, SOMRequestOutput>("api/DynamicBusinessProcess_BP/ApplyInstallment/StartProcess", input);
+            }
+        }
         public InvoiceAmountData GetInvoiceAmountData(string invoiceId)
         {
             InvoiceAmountData invoiceAmountData = new InvoiceAmountData();
             using (SOMClient client = new SOMClient())
             {
-                 invoiceAmountData = client.Get<InvoiceAmountData>(String.Format("/api/SOM.ST/Billing/GetInvoiceAmountsForInstallment?invoiceId={0}",invoiceId));
+                invoiceAmountData = client.Get<InvoiceAmountData>(String.Format("/api/SOM.ST/Billing/GetInvoiceAmountsForInstallment?invoiceId={0}", invoiceId));
             }
             return invoiceAmountData;
         }
@@ -62,10 +96,10 @@ namespace BPMExtended.Main.Business
         {
             return new InstallmentDetail
             {
-               Id = item.Id,
-               Amount = item.Amount,
-               Currency = item.Currency,
-               Date = item.Date
+                Id = item.Id,
+                Amount = item.Amount,
+                Currency = item.Currency,
+                Date = item.Date
 
             };
         }
---
+++ SOM/Code/BPMExtended.Main.Entities/SimulateInstallmentInput.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BPMExtended.Main.Entities
+{
+    public class SimulateInstallmentInput
+    {
+        public string CustomerId { get; set; }
+        public string PaymentPlanTemplateId { get; set; }
+        public string InvoiceCode { get; set; }
+        public string InvoiceAmount { get; set; }
+        public string AdditionalFees { get; set; }
+        public string LateFee { get; set; }
+        public string FirstPayment { get; set; }
+        public string ReductionRate { get; set; }
+        public string Currency { get; set; }
+        public string StartDate { get; set; }
+        public string ApprovalId { get; set; }
+    }
+}
