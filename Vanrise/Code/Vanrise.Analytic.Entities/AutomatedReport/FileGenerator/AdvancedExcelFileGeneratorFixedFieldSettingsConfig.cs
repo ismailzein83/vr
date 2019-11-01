149df6aeea57ea60e7ed79e2aa6ccbf8c24a6234
Fatima Hammoud <VANRISE\fatima.hammoud>
2019-11-01 07:59:47 +0000

    Adding new class AdvancedExcelFileGeneratorFixedFieldSettingsConfig
    git-tfs-id: [http://developmentvlan:8585/tfs/vanrise.collection]$/;C58937

---
+++ Vanrise/Code/Vanrise.Analytic.Entities/AutomatedReport/FileGenerator/AdvancedExcelFileGeneratorFixedFieldSettingsConfig.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Vanrise.Entities;
+
+namespace Vanrise.Analytic.Entities
+{
+    public class AdvancedExcelFileGeneratorFixedFieldSettingsConfig : ExtensionConfiguration
+    {
+        public const string EXTENSION_TYPE = "VR_Analytic_FileGenerator_AutomatedReport_ExcelFile_FixedField";
+        public string Editor { get; set; }
+    }
+}
