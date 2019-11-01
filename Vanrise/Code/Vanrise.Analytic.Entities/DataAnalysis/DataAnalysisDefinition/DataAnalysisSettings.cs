f903cc53fe60164a8c61c098b638bd16be48cecd
Ali Slayman <VANRISE\ali.slayman>
2019-11-01 10:32:40 +0000

    Add DataAnalysisSettings class
    git-tfs-id: [http://developmentvlan:8585/tfs/vanrise.collection]$/;C58945

---
+++ Vanrise/Code/Vanrise.Analytic.Entities/DataAnalysis/DataAnalysisDefinition/DataAnalysisSettings.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Vanrise.Entities;
+
+namespace Vanrise.Analytic.Entities
+{
+    public class DataAnalysisSettings : SettingData
+    {
+        public const string SETTING_TYPE = "VR_Analytic_DataAnalysisSettings";
+        public Dictionary<Guid, DataAnalysisItemTab> DataAnalysisItemsSettingsByItemId { get; set; }
+    }
+
+    public class DataAnalysisItemTab
+    {
+        public Guid DataAnalysisItemDefinitionId { get; set; }
+
+        public ParameterSettings ParameterSettings { get; set; }
+    }
+
+    public class ParameterSettings
+    {
+        public Dictionary<string, Object> ParameterValues { get; set; }
+    }
+}
