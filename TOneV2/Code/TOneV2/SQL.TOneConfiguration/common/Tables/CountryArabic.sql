4db20746b5ba99e39740557ee05f7ba13296a541
Fatima Al Heck <VANRISE\fatima.alheck>
2019-11-01 07:31:34 +0000

    adjust countryArabic post script
    git-tfs-id: [http://developmentvlan:8585/tfs/vanrise.collection]$/;C58936

--- TOneV2/Code/TOneV2/SQL.TOneConfiguration/common/Tables/CountryArabic.sql
+++ TOneV2/Code/TOneV2/SQL.TOneConfiguration/common/Tables/CountryArabic.sql
@@ -1,11 +1,14 @@
 ﻿CREATE TABLE [common].[CountryArabic] (
+    [Id]               INT            IDENTITY (1, 1) NOT NULL,
     [CountryId]        INT            NOT NULL,
-    [ArabicName]       NVARCHAR (255) NULL,
+    [ArabicName]       NVARCHAR (255) NOT NULL,
     [CreatedBy]        INT            NULL,
     [CreatedTime]      DATETIME       NULL,
     [LastModifiedBy]   INT            NULL,
     [LastModifiedTime] DATETIME       NULL,
     [timestamp]        ROWVERSION     NULL,
-    PRIMARY KEY CLUSTERED ([CountryId] ASC)
+    CONSTRAINT [PK__CountryA__10D1609F74E42A3D] PRIMARY KEY CLUSTERED ([Id] ASC)
 );
+
+
 
