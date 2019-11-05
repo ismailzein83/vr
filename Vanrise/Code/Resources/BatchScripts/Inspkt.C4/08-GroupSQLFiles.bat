b15373dffd250402af8790545e7f04a4a955a354
Wissam Ajamy <VANRISE\wissam.ajamy>
2019-11-05 13:44:54 +0000

    update groupSQL to be GRPSQLOverridden
    git-tfs-id: [http://developmentvlan:8585/tfs/vanrise.collection]$/;C58995

--- Vanrise/Code/Resources/BatchScripts/Inspkt.C4/08-GroupSQLFiles.bat
+++ Vanrise/Code/Resources/BatchScripts/Inspkt.C4/08-GroupSQLFiles.bat
@@ -1,3 +1,3 @@
 ECHO OFF
 ECHO Inspkt.C4 Group SQL Scripts
-start /b /w /D"C:\TFS\Vanrise\Code\Vanrise.HelperTools\bin\Release" Vanrise.HelperTools.exe "GRPSQL" "Inspkt.C4" "Inspkt"
+start /b /w /D"C:\TFS\Vanrise\Code\Vanrise.HelperTools\bin\Release" Vanrise.HelperTools.exe "GRPSQLOverridden" "Inspkt.C4" "Inspkt"
--- Vanrise/Code/Resources/BatchScripts/Inspkt.C5/08-GroupSQLFiles.bat
+++ Vanrise/Code/Resources/BatchScripts/Inspkt.C5/08-GroupSQLFiles.bat
@@ -1,3 +1,3 @@
 ECHO OFF
 ECHO Inspkt.C5 Group SQL Scripts
-start /b /w /D"C:\TFS\Vanrise\Code\Vanrise.HelperTools\bin\Release" Vanrise.HelperTools.exe "GRPSQL" "Inspkt.C5" "Inspkt"
+start /b /w /D"C:\TFS\Vanrise\Code\Vanrise.HelperTools\bin\Release" Vanrise.HelperTools.exe "GRPSQLOverridden" "Inspkt.C5" "Inspkt"
