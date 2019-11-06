fa3c4a62d9a0addb20ae79070fc620e48567dc0b
Wissam Ajamy <VANRISE\wissam.ajamy>
2019-11-06 11:07:49 +0000

    update field rank
    git-tfs-id: [http://developmentvlan:8585/tfs/vanrise.collection]$/;C59012

--- TOneV2/Code/TOneV2/SQL.TOneConfiguration/genericdata/Tables/VRNumberPrefixType.sql
+++ TOneV2/Code/TOneV2/SQL.TOneConfiguration/genericdata/Tables/VRNumberPrefixType.sql
@@ -1,7 +1,7 @@
 ﻿CREATE TABLE [genericdata].[VRNumberPrefixType] (
     [Id]               UNIQUEIDENTIFIER NOT NULL,
-    [Name]             VARCHAR (255)    NULL,
     [DevProjectID]     UNIQUEIDENTIFIER NULL,
+    [Name]             VARCHAR (255)    NULL,
     [CreatedBy]        INT              NULL,
     [CreatedTime]      DATETIME         NULL,
     [LastModifiedBy]   INT              NULL,
@@ -10,6 +10,8 @@
     CONSTRAINT [PK_VRNumberPrefixType] PRIMARY KEY CLUSTERED ([Id] ASC),
     CONSTRAINT [UQ__VRNumber__737584F6004AEFF1] UNIQUE NONCLUSTERED ([Name] ASC)
 );
+
+
 
 
 
--- TOneV2/Code/TOneV2/SQL.TOneConfiguration/genericdata/User Defined Types/VRNumberPrefixTypeType.sql
+++ TOneV2/Code/TOneV2/SQL.TOneConfiguration/genericdata/User Defined Types/VRNumberPrefixTypeType.sql
@@ -1,11 +1,13 @@
 ﻿CREATE TYPE [genericdata].[VRNumberPrefixTypeType] AS TABLE (
     [Id]               UNIQUEIDENTIFIER NULL,
-    [Name]             VARCHAR (255)    NULL,
     [DevProjectID]     UNIQUEIDENTIFIER NULL,
+    [Name]             VARCHAR (255)    NULL,
     [CreatedBy]        INT              NULL,
     [CreatedTime]      DATETIME         NULL,
     [LastModifiedBy]   INT              NULL,
     [LastModifiedTime] DATETIME         NULL);
+
+
 
 
 
