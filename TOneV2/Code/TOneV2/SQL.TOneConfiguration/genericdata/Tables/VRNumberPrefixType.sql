3541bdb7cc60dddc09f1415090a570bcdd45cf8c
Ali Atoui <VANRISE\ali.atoui>
2019-11-06 07:32:32 +0000

    Removing DevProject from NumberPrefixType BE Transfering OnNet NumberPrefixType from Multinet.json to Mobilis.json
    git-tfs-id: [http://developmentvlan:8585/tfs/vanrise.collection]$/;C59002

--- TOneV2/Code/TOneV2/SQL.TOneConfiguration/genericdata/Tables/VRNumberPrefixType.sql
+++ TOneV2/Code/TOneV2/SQL.TOneConfiguration/genericdata/Tables/VRNumberPrefixType.sql
@@ -1,6 +1,5 @@
 ﻿CREATE TABLE [genericdata].[VRNumberPrefixType] (
     [Id]               UNIQUEIDENTIFIER NOT NULL,
-    [DevProjectID]     UNIQUEIDENTIFIER NULL,
     [Name]             VARCHAR (255)    NULL,
     [CreatedBy]        INT              NULL,
     [CreatedTime]      DATETIME         NULL,
@@ -10,6 +9,8 @@
     CONSTRAINT [PK_VRNumberPrefixType] PRIMARY KEY CLUSTERED ([Id] ASC),
     CONSTRAINT [UQ__VRNumber__737584F6004AEFF1] UNIQUE NONCLUSTERED ([Name] ASC)
 );
+
+
 
 
 
