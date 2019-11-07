72f6093cae52b9e6b36911e241909b3b7527d729
Samer Haidar <VANRISE\samer.haidar>
2019-11-07 12:35:55 +0000

    Adding enw tasks and Report Viewer references
    git-tfs-id: [http://developmentvlan:8585/tfs/vanrise.collection]$/;C59039

---
+++ Retail/Code/Retail.Runtime/Tasks/FatimaHammoudTask.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Retail.Runtime.Tasks
+{
+    public class FatimaHammoudTask : ITask
+    {
+        public void Execute()
+        {
+        }
+    }
+}
