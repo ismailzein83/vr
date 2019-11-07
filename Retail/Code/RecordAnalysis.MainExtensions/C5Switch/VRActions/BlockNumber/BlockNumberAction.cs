f09422af829405753468db303038964e8d96cb31
anthony douaihy <VANRISE\anthony.douaihy>
2019-11-07 17:16:49 +0000

    Add BlockNumber action for C5 Inspkt
    git-tfs-id: [http://developmentvlan:8585/tfs/vanrise.collection]$/;C59055

---
+++ Retail/Code/RecordAnalysis.MainExtensions/C5Switch/VRActions/BlockNumber/BlockNumberAction.cs
@@ -0,0 +1,23 @@
+﻿using RecordAnalysis.Business;
+using RecordAnalysis.Entities;
+using System;
+using Vanrise.GenericData.Business;
+using Vanrise.GenericData.Notification;
+using Vanrise.Notification.Entities;
+
+namespace RecordAnalysis.MainExtensions.C5Switch.VRActions.BlockNumber
+{
+    public class BlockNumberAction : VRAction
+    {
+        public override void Execute(IVRActionExecutionContext context)
+        {
+            DataRecordAlertRuleActionEventPayload payload = context.EventPayload as DataRecordAlertRuleActionEventPayload;
+            Type dataRecordRuntimeType = new DataRecordTypeManager().GetDataRecordRuntimeType(payload.DataRecordTypeId);
+
+            dynamic record = Activator.CreateInstance(dataRecordRuntimeType, payload.OutputRecords);
+
+            string command = $"Blocking Number: {record.MSISDN}";
+            new C4CommandManager().AddC4Command(C4CommandType.BlockDestinationNumberOnMSC, command);
+        }
+    }
+}
---
+++ Retail/Code/RecordAnalysis.MainExtensions/C5Switch/VRActions/BlockNumber/BlockNumberDefinitionSettings.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using Vanrise.GenericData.Notification;
+using Vanrise.Notification.Entities;
+
+namespace RecordAnalysis.MainExtensions.C5Switch.VRActions.BlockNumber
+{
+    public class BlockNumberDefinitionSettings : VRActionDefinitionExtendedSettings
+    {
+        public override Guid ConfigId { get { return new Guid("3CD9A858-F5D9-4305-8EFB-264C1660D301"); } }
+
+        public override string RuntimeEditor { get { return "rec-anal-action-c5switch-blocknumber"; } }
+
+        public Guid DataRecordTypeId { get; set; }
+
+        public string NumberFieldName { get; set; }
+
+        public override bool IsApplicable(IVRActionDefinitionIsApplicableContext context)
+        {
+            DataRecordActionTargetType dataRecordActionTargetType = context.Target as DataRecordActionTargetType;
+            if (dataRecordActionTargetType == null)
+                return false;
+
+            if (dataRecordActionTargetType.DataRecordTypeId != DataRecordTypeId)
+                return false;
+
+            if (dataRecordActionTargetType.AvailableDataRecordFieldNames == null || dataRecordActionTargetType.AvailableDataRecordFieldNames.Count == 0)
+                return false;
+
+            if (!dataRecordActionTargetType.AvailableDataRecordFieldNames.Contains(NumberFieldName))
+                return false;
+
+            return true;
+        }
+    }
+}
