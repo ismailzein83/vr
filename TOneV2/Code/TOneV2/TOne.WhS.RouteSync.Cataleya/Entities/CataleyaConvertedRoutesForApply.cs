7df859b73e141587cc6d76f3a86519742cf01f03
Naim Saad <VANRISE\naim.saad>
2019-11-08 10:39:57 +0000

    Adding CataleyaConvertedRoutesForApply Class
    git-tfs-id: [http://developmentvlan:8585/tfs/vanrise.collection]$/;C59072

---
+++ TOneV2/Code/TOneV2/TOne.WhS.RouteSync.Cataleya/Entities/CataleyaConvertedRoutesForApply.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections.Generic;
+
+namespace TOne.WhS.RouteSync.Cataleya.Entities
+{
+    public class CataleyaConvertedRoutesForApply
+    {
+        public List<CataleyaConvertedRoute> Routes {get;set;}
+        public string RouteTableName { get; set; }
+    }
+}
