c36f9f21825895edd5c47e730c3170636e6c4364
Mohamad Kalakesh <VANRISE\mohamad.kalakesh>
2019-11-08 10:47:58 +0000

    Adding CataleyaManager and CataleyaDataManagerConfig
    git-tfs-id: [http://developmentvlan:8585/tfs/vanrise.collection]$/;C59073

---
+++ TOneV2/Code/TOneV2/TOne.WhS.RouteSync.Cataleya/Business/CataleyaManager.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections.Generic;
+using TOne.WhS.RouteSync.Cataleya.Entities;
+using Vanrise.Common.Business;
+
+namespace TOne.WhS.RouteSync.Cataleya.Business
+{
+    public class CataleyaManager
+    {
+        public IEnumerable<CataleyaDataManagerConfig> GetCataleyaDataManagerConfigs()
+        {
+            var templateConfigManager = new ExtensionConfigurationManager();
+            return templateConfigManager.GetExtensionConfigurations<CataleyaDataManagerConfig>(CataleyaDataManagerConfig.EXTENSION_TYPE);
+        }
+    }
+}
---
+++ TOneV2/Code/TOneV2/TOne.WhS.RouteSync.Cataleya/Entities/CataleyaDataManagerConfig.cs
@@ -0,0 +1,11 @@
+﻿using Vanrise.Entities;
+
+namespace TOne.WhS.RouteSync.Cataleya.Entities
+{
+    public class CataleyaDataManagerConfig : ExtensionConfiguration
+    {
+        public const string EXTENSION_TYPE = "WhS_RouteSync_CataleyaDataManager";
+
+        public string Editor { get; set; }
+    }
+}
