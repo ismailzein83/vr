dca86dbd88a8bf3bded6541eaa33d4af6904045e
Mohamad Abdallah <VANRISE\mohamad.abdallah>
2019-11-11 12:11:19 +0000

    line moving
    git-tfs-id: [http://developmentvlan:8585/tfs/vanrise.collection]$/;C59111

--- SOM/Code/BPMExtended.Main.Business/Requests/Telephony/LineMovingManager.cs
+++ SOM/Code/BPMExtended.Main.Business/Requests/Telephony/LineMovingManager.cs
@@ -26,7 +26,17 @@ namespace BPMExtended.Main.Business
         #endregion
 
         #region public
-       
+
+        public List<ServiceData> GetNotApplicableServicesOnSwitch(string switchId, string contractId)
+        {
+            List<ServiceData> items = new List<ServiceData>();
+            using (SOMClient client = new SOMClient())
+            {
+                items = client.Get<List<ServiceData>>(String.Format("api/SOM.ST/Billing/GetCustomer?switchId={0}&contractId={1}", switchId, contractId));
+            }
+            return items;
+        }
+
         public void SwitchDeleteSubscriptionLineMoving(Guid requestId)
         {
             //Get Data from StLineSubscriptionRequest table
@@ -194,22 +204,6 @@ namespace BPMExtended.Main.Business
             esq.AddColumn("StNewLinePathID");
             esq.AddColumn("StOldLinePathId");
             esq.AddColumn("StIsNewSwitch");
-            esq.AddColumn("StOperationAddedFees");
-            esq.AddColumn("StIsPaid");
-            esq.AddColumn("StProvince");
-            esq.AddColumn("StProvince.Id");
-            esq.AddColumn("StCity");
-            esq.AddColumn("StCity.Id");
-            esq.AddColumn("StArea");
-            esq.AddColumn("StArea.Id");
-            esq.AddColumn("StTown");
-            esq.AddColumn("StTown.Id");
-            esq.AddColumn("StLocation");
-            esq.AddColumn("StLocation.Id");
-            esq.AddColumn("StStreet");
-            esq.AddColumn("StBuildingNumber");
-            esq.AddColumn("StFloor");
-            esq.AddColumn("StAddressNotes");
 
             esqFirstFilter = esq.CreateFilterWithParameters(FilterComparisonType.Equal, "Id", requestId);
             esq.Filters.Add(esqFirstFilter);
@@ -217,58 +211,19 @@ namespace BPMExtended.Main.Business
             var entities = esq.GetEntityCollection(BPM_UserConnection);
             if (entities.Count > 0)
             {
-                var customerId = entities[0].GetColumnValue("StCustomerId");
                 var contractId = entities[0].GetColumnValue("StContractID");
                 var oldLinePathId = entities[0].GetColumnValue("StOldLinePathId");
                 var newLinePathID = entities[0].GetColumnValue("StNewLinePathID");
-                var newSwitch = entities[0].GetColumnValue("StIsNewSwitch");
-                string fees = entities[0].GetColumnValue("StOperationAddedFees").ToString();
-                var isPaid = entities[0].GetColumnValue("StIsPaid");
-                var street = entities[0].GetColumnValue("StStreet");
-                var notes = entities[0].GetColumnValue("StAddressNotes");
-                var building = entities[0].GetColumnValue("StBuildingNumber");
-                var floor = entities[0].GetColumnValue("StFloor");
-                var city = entities[0].GetColumnValue("StCityName");
-                var area = entities[0].GetColumnValue("StAreaName");
-                var location = entities[0].GetColumnValue("StLocationName");
-                var town = entities[0].GetColumnValue("StTownName");
-                var province = entities[0].GetColumnValue("StProvinceName");
-
-
 
                 SOMRequestInput<LineMovingInput> somRequestInput = new SOMRequestInput<LineMovingInput>
                 {
 
                     InputArguments = new LineMovingInput
                     {
-                        CommonInputArgument = new CommonInputArgument()
-                        {
-                            ContractId = contractId.ToString(),
-                            RequestId = requestId.ToString(),
-                            CustomerId = customerId.ToString()
-                        },
                         OldLinePathId = oldLinePathId.ToString(),
                         NewLinePathId = newLinePathID.ToString(),
-                        SameSwitch = !((bool)newSwitch),
-                        Address = new Address
-                        {
-                            Sequence = new ContractManager().GetContractAddressAndDirectoryInfo(contractId.ToString()).Address.Sequence.ToString(),//new CRMCustomerManager().GetCustomerAddress(customerId.ToString()).Sequence.ToString(),
-                            StateProvince = province.ToString(),
-                            City = city.ToString(),
-                            Town = town.ToString(),
-                            Region = area.ToString(),
-                            Street = street.ToString(),
-                            Building = building.ToString(),
-                            Floor = floor.ToString(),
-                            Notes = notes.ToString(),
-                            LocationType = location.ToString()
-
-                        },
-                        PaymentData = new PaymentData()
-                        {
-                            Fees = JsonConvert.DeserializeObject<List<SaleService>>(fees),
-                            IsPaid = (bool)isPaid
-                        }
+                        RequestId = requestId.ToString(),
+                        ContractId = contractId.ToString()
                     },
                 };
 
@@ -276,7 +231,7 @@ namespace BPMExtended.Main.Business
                 //call api
                 using (var client = new SOMClient())
                 {
-                    output = client.Post<SOMRequestInput<LineMovingInput>, SOMRequestOutput>("api/DynamicBusinessProcess_BP/ST_Tel_LineMove/StartProcess", somRequestInput);
+                    output = client.Post<SOMRequestInput<LineMovingInput>, SOMRequestOutput>("api/DynamicBusinessProcess_BP/CreateNewSubscriptionForLineMove/StartProcess", somRequestInput);
                 }
                 var manager = new BusinessEntityManager();
                 manager.InsertSOMRequestToProcessInstancesLogs(requestId, output);
@@ -285,6 +240,54 @@ namespace BPMExtended.Main.Business
 
         }
 
+        public void CreateSubscriptionHandleADSL(Guid requestId)
+        {
+            //Get Data from StLineSubscriptionRequest table
+            EntitySchemaQuery esq;
+            IEntitySchemaQueryFilterItem esqFirstFilter;
+            SOMRequestOutput output;
+
+            esq = new EntitySchemaQuery(BPM_UserConnection.EntitySchemaManager, "StLineMovingRequest");
+            esq.AddColumn("StContractID");
+            esq.AddColumn("StCustomerId");
+            esq.AddColumn("StNewLinePathID");
+            esq.AddColumn("StOldLinePathId");
+            esq.AddColumn("StIsNewSwitch");
+
+            esqFirstFilter = esq.CreateFilterWithParameters(FilterComparisonType.Equal, "Id", requestId);
+            esq.Filters.Add(esqFirstFilter);
+
+            var entities = esq.GetEntityCollection(BPM_UserConnection);
+            if (entities.Count > 0)
+            {
+                var contractId = entities[0].GetColumnValue("StContractID");
+                var oldLinePathId = entities[0].GetColumnValue("StOldLinePathId");
+                var newLinePathID = entities[0].GetColumnValue("StNewLinePathID");
+
+                SOMRequestInput<LineMovingInput> somRequestInput = new SOMRequestInput<LineMovingInput>
+                {
+
+                    InputArguments = new LineMovingInput
+                    {
+                        OldLinePathId = oldLinePathId.ToString(),
+                        NewLinePathId = newLinePathID.ToString(),
+                        RequestId = requestId.ToString(),
+                        ContractId = contractId.ToString()
+                    },
+                };
+
+
+                //call api
+                using (var client = new SOMClient())
+                {
+                    output = client.Post<SOMRequestInput<LineMovingInput>, SOMRequestOutput>("api/DynamicBusinessProcess_BP/CreateSubscriptionHandleADSL/StartProcess", somRequestInput);
+                }
+                var manager = new BusinessEntityManager();
+                manager.InsertSOMRequestToProcessInstancesLogs(requestId, output);
+
+            }
+
+        }
 
         public void PostLineMovingSubmitNewSwitch(Guid requestId)
         {
