90ae8276098885014f98ed875f1506cb8ad0a35a
anthony douaihy <VANRISE\anthony.douaihy>
2019-11-11 08:50:58 +0000

    adjust CurrentWeekTimePeriod
    git-tfs-id: [http://developmentvlan:8585/tfs/vanrise.collection]$/;C59107

--- Vanrise/Code/Vanrise.Common.MainExtensions/VRTimePeriod/CurrentWeekTimePeriod.cs
+++ Vanrise/Code/Vanrise.Common.MainExtensions/VRTimePeriod/CurrentWeekTimePeriod.cs
@@ -8,13 +8,8 @@ namespace Vanrise.Common.MainExtensions
         public override Guid ConfigId => new Guid("858E92C1-9FBA-45A6-B4BF-9458E53C2EC8");
         public override void GetTimePeriod(IVRTimePeriodContext context)
         {
-            DateTime startOfWeek = GetStartOfWeek(context.EffectiveDate, DayOfWeek.Monday);
-            context.FromTime = startOfWeek;
+            context.FromTime = Utilities.GetMonday(context.EffectiveDate);
             context.ToTime = context.EffectiveDate;
-        }
-        public DateTime GetStartOfWeek(DateTime currentDate, DayOfWeek startOfWeek)
-        {
-            return currentDate.AddDays(-(DateTime.Today.DayOfWeek - startOfWeek)).Date;
         }
     }
 }
