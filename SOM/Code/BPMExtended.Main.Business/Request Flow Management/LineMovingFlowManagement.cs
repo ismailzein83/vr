cdfea35d817dd97f3b4889fd35c591ad65eecac4
Mohamad Abdallah <VANRISE\mohamad.abdallah>
2019-11-11 07:56:11 +0000

    working with line moving
    git-tfs-id: [http://developmentvlan:8585/tfs/vanrise.collection]$/;C59103

--- SOM/Code/BPMExtended.Main.Business/Request Flow Management/LineMovingFlowManagement.cs
+++ SOM/Code/BPMExtended.Main.Business/Request Flow Management/LineMovingFlowManagement.cs
@@ -28,6 +28,7 @@ namespace BPMExtended.Main.Business
         const string print = "36DBA738-559E-4C0D-BCBC-02FCA872FBAA";
         const string address = "810A0B83-67F3-4875-A8CD-9ECDDE4E7ABE";
         const string freeReservation = "B6833EFF-5653-4DC8-AB32-F3973A86278C";
+        const string InapplicableServices = "99EB9396-A885-4582-9C9C-EC71BEC8C43E";
 
         const string adslStep = "4745C0D7-747D-4C5F-AABB-6F0343B4D917";
         const string waitingList = "B12DB4FE-5EF9-4CD7-96CA-B21E7C007EA3";
@@ -48,7 +49,8 @@ namespace BPMExtended.Main.Business
             switch (currentStepId.ToUpper())
             {
                 case startingProcess: nextStepId = nearbyNumber; break;
-                case nearbyNumber: nextStepId = print; break;
+                case nearbyNumber: nextStepId = InapplicableServices; break;
+                case InapplicableServices: nextStepId = print; break;
                 case print: nextStepId = freeReservation; break;
                 case freeReservation: nextStepId = isWaitingList ? waitingList : hasADSL? adslStep:address; break;
                 case waitingList: nextStepId = freeReservation; break;
