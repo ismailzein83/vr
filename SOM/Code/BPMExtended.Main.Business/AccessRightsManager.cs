4c3371eaa97cbaf66f9d829cb0eb139168bb2fd9
Ali Sakr <VANRISE\ali.sakr>
2019-11-12 16:10:03 +0000

    adding new Manager For security
    git-tfs-id: [http://developmentvlan:8585/tfs/vanrise.collection]$/;C59136

---
+++ SOM/Code/BPMExtended.Main.Business/AccessRightsManager.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Web;
+using Terrasoft.Core;
+
+namespace BPMExtended.Main.Business
+{
+    public class AccessRightsManager
+    {
+        public UserConnection BPM_UserConnection
+        {
+            get
+            {
+                return (UserConnection)HttpContext.Current.Session["UserConnection"];
+            }
+        }
+        public bool CanManageResidential()
+        {
+            return BPM_UserConnection.DBSecurityEngine.GetCanExecuteOperation("CanManageResidential");
+        }
+        public bool CanManageEntreprise()
+        {
+            return BPM_UserConnection.DBSecurityEngine.GetCanExecuteOperation("CanManageEntreprise");
+        }
+        public bool CanManageOfficial()
+        {
+            return BPM_UserConnection.DBSecurityEngine.GetCanExecuteOperation("CanManageOfficial");
+        }
+    }
+}
