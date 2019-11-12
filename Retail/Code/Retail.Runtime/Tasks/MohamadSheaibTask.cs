a6dd389193c6ac846f8fe1ece9877678bdb668e5
Mohammad Sheaib <VANRISE\mohammad.sheaib>
2019-11-12 15:36:14 +0000

    added MohamadSheaibTask
    git-tfs-id: [http://developmentvlan:8585/tfs/vanrise.collection]$/;C59135

---
+++ Retail/Code/Retail.Runtime/Tasks/MohamadSheaibTask.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Retail.Runtime.Tasks
+{
+    public class MohamadSheaibTask : ITask
+    {
+        public void Execute()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
