8bc89a6cda9dbbc9e60f8f024beb45886a73192f
Mohamad Saab <VANRISE\mohamad.saab>
2019-11-14 08:38:19 +0000

    Adding UserRGDefinition and UserRGManager
    git-tfs-id: [http://developmentvlan:8585/tfs/vanrise.collection]$/;C59189

---
+++ Retail/Code/Retail.Teles.Business/Provisioning/ChangeUsersRGs/UserRGDefinition.cs
@@ -0,0 +1,40 @@
+﻿using Retail.BusinessEntity.Entities;
+using Retail.Teles.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Vanrise.Notification.Entities;
+
+namespace Retail.Teles.Business
+{
+    public class UserRGDefinition : VRActionDefinitionExtendedSettings
+    {
+        public override Guid ConfigId
+        {
+            get { return new Guid("BDDFED8C-4D51-4D53-8D71-C9399CA91517"); }
+        }
+
+        public Guid VRConnectionId { get; set; }
+        public Guid CompanyTypeId { get; set; }
+        public Guid SiteTypeId { get; set; }
+        public Guid? UserTypeId { get; set; }
+        public bool SaveChangesToAccountState { get; set; }
+
+        /// <summary>
+        /// only applicable if SaveChangesToAccountState is true
+        /// </summary>
+        public string ActionType { get; set; }
+        public NewRGNoMatchHandling NewRGNoMatchHandling { get; set; }
+        public NewRGMultiMatchHandling NewRGMultiMatchHandling { get; set; }
+        public RoutingGroupCondition NewRoutingGroupCondition { get; set; }
+        public ExistingRGNoMatchHandling ExistingRGNoMatchHandling { get; set; }
+        public RoutingGroupCondition ExistingRoutingGroupCondition { get; set; }
+
+        public override bool IsApplicable(IVRActionDefinitionIsApplicableContext context)
+        {
+            return true;
+        }
+    }
+}
