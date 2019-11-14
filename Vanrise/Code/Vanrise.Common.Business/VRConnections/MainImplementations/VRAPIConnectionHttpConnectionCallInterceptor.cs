27cace6e6eabe21da472696441b6721201332704
Naim Saad <VANRISE\naim.saad>
2019-11-14 14:24:44 +0000

    Adding VRAPIConnectionHttpConnectionCallInterceptor
    git-tfs-id: [http://developmentvlan:8585/tfs/vanrise.collection]$/;C59209

---
+++ Vanrise/Code/Vanrise.Common.Business/VRConnections/MainImplementations/VRAPIConnectionHttpConnectionCallInterceptor.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Vanrise.Common.Business
+{
+    public class VRAPIConnectionHttpConnectionCallInterceptor : VRHttpConnectionCallInterceptor
+    {
+        public override Guid ConfigId { get { return new Guid("E0E15373-0F94-45A2-BBEE-73ACEB3D3301"); } }
+        public string Username { get; set; }
+        public string Password { get; set; }
+
+        public override void InterceptRequest(IVRHttpConnectionInterceptRequestContext context)
+        {
+            throw new NotImplementedException();
+        }
+
+        public override void InterceptResponse(IVRHttpConnectionInterceptResponseContext context)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ Vanrise/Code/Vanrise.Common.Web/Common/Directives/MainExtensions/ConnectionTypes/vr-common-httpconnection-apiconnectioninterceptor-editor.js
@@ -0,0 +1,56 @@
+﻿'use strict';
+
+app.directive('vrCommonHttpconnectionApiconnectioninterceptorEditor', ['UtilsService', 'VRUIUtilsService',
+    function (UtilsService, VRUIUtilsService) {
+        return {
+            restrict: 'E',
+            scope: {
+                onReady: '='
+            },
+            controller: function ($scope, $element, $attrs) {
+                var ctrl = this;
+                var ctor = new HttpConnectionAPIInterceptorCtor(ctrl, $scope, $attrs);
+                ctor.initializeController();
+            },
+            controllerAs: 'ctrl',
+            bindToController: true,
+            templateUrl: "/Client/Modules/Common/Directives/MainExtensions/ConnectionTypes/Templates/HttpConnectionAPIConnectionInterceptorEditorTemplate.html"
+        };
+
+        function HttpConnectionAPIInterceptorCtor(ctrl, $scope, attrs) {
+            this.initializeController = initializeController;
+
+            function initializeController() {
+                $scope.scopeModel = {};
+
+                defineAPI();
+            }
+
+            function defineAPI() {
+                var api = {};
+
+                api.load = function (payload) {
+                    var promises = [];
+
+                    if (payload != undefined && payload.Interceptor != undefined) {
+                        $scope.scopeModel.username = payload.Interceptor.Username;
+                        $scope.scopeModel.password = payload.Interceptor.Password;
+                    }
+
+                    return UtilsService.waitMultiplePromises(promises);
+                };
+
+                api.getData = function () {
+
+                    return {
+                        $type: "Vanrise.Common.Business.VRAPIConnectionHttpConnectionCallInterceptor, Vanrise.Common.Business",
+                        Username: $scope.scopeModel.username,
+                        Password: $scope.scopeModel.password
+                    };
+                };
+
+                if (ctrl.onReady != null)
+                    ctrl.onReady(api);
+            }
+        }
+    }]);
