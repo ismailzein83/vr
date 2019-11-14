78cf439fadb87fdbaa21ea2a9716905e1b472880
Ali Ballouk <VANRISE\ali.ballouk>
2019-11-14 10:33:56 +0000

    Adding PesonilizationSaveTypeOptions enum
    git-tfs-id: [http://developmentvlan:8585/tfs/vanrise.collection]$/;C59197

---
+++ Vanrise/Code/Vanrise.Common.Web/Common/Constant/PesonilizationSaveTypeOptions.js
@@ -0,0 +1,5 @@
+﻿app.constant('VRCommon_PesonilizationSaveTypeOptions', {
+    Current: { value: 1, name: "Current" , basicOption : false},
+    AsDefault: { value: 2, name: "As Default", basicOption: true },
+    NewCopy: { value: 3, name: "New Copy", basicOption: true }
+});
