a86793f380b5d18b9619c2d6f988271140b86c04
Ali Ballouk <VANRISE\ali.ballouk>
2019-11-14 10:45:00 +0000

    Adding save-type-selector directive
    git-tfs-id: [http://developmentvlan:8585/tfs/vanrise.collection]$/;C59199

---
+++ Vanrise/Code/Vanrise.Common.Web/Common/Directives/Personalization/vr-common-savetype-selector.js
@@ -0,0 +1,110 @@
+﻿'use strict';
+
+app.directive('vrCommonSavetypeSelector', ['UtilsService', 'VRUIUtilsService', 'VRCommon_PesonilizationSaveTypeOptions',
+    function (UtilsService, VRUIUtilsService, VRCommon_PesonilizationSaveTypeOptions) {
+        return {
+            restrict: 'E',
+            scope: {
+                onReady: '=',
+                ismultipleselection: '@',
+                selectedvalues: '=',
+                onselectionchanged: '=',
+                onselectitem: '=',
+                ondeselectitem: '=',
+                isrequired: '=',
+                hideremoveicon: '@',
+                customvalidate: '='
+            },
+            controller: function ($scope, $element, $attrs) {
+
+                var ctrl = this;
+                ctrl.datasource = [];
+
+                ctrl.selectedvalues;
+                if ($attrs.ismultipleselection != undefined)
+                    ctrl.selectedvalues = [];
+
+                var ctor = new SaveTypeCtor(ctrl, $scope, $attrs);
+                ctor.initializeController();
+
+            },
+            controllerAs: 'ctrl',
+            bindToController: true,
+            template: function (element, attrs) {
+                return getTemplate(attrs);
+            }
+        };
+
+        function SaveTypeCtor(ctrl, $scope, attrs) {
+            this.initializeController = initializeController;
+
+            var selectorAPI;
+
+            function initializeController() {
+
+                ctrl.onSelectorReady = function (api) {
+                    selectorAPI = api;
+                    defineAPI();
+                };
+            }
+
+            function defineAPI() {
+                var api = {};
+
+                api.load = function (payload) {
+                    selectorAPI.clearDataSource();
+
+                    var selectedIds;
+                    var filter;
+                    var setDefaultValue;
+
+                    if (payload != undefined) {
+                        selectedIds = payload.selectedIds;
+                        filter = payload.filter;
+                        setDefaultValue = payload.setDefaultValue;
+                    }
+                    var saveTypes = UtilsService.getArrayEnum(VRCommon_PesonilizationSaveTypeOptions);
+
+                    if (saveTypes != null) {
+                        for (var i = 0; i < saveTypes.length; i++) {
+                            ctrl.datasource.push(saveTypes[i]);
+                        }
+
+                        if (selectedIds != undefined) {
+                            VRUIUtilsService.setSelectedValues(selectedIds, 'value', attrs, ctrl);
+                        }
+                        
+                    }
+                };
+
+                api.getSelectedIds = function () {
+                    return VRUIUtilsService.getIdSelectedIds('value', attrs, ctrl);
+                };
+
+                if (ctrl.onReady != null)
+                    ctrl.onReady(api);
+            }
+        }
+
+        function getTemplate(attrs) {
+
+            var multipleselection = "";
+            var label = "Save Type";
+
+            if (attrs.ismultipleselection != undefined) {
+                label = "Save Types";
+                multipleselection = "ismultipleselection";
+            }
+            if (attrs.customlabel != undefined)
+                label = attrs.customlabel;
+
+            var hideremoveicon = '';
+            if (attrs.hideremoveicon != undefined)
+                hideremoveicon = ' hideremoveicon ';
+
+            return     '<vr-select ' + multipleselection + ' datatextfield="name" datavaluefield="value" isrequired="ctrl.isrequired" label="' + label + '" datasource="ctrl.datasource" on-ready="ctrl.onSelectorReady" '
+                            + ' selectedvalues="ctrl.selectedvalues" onselectionchanged="ctrl.onselectionchanged" entityName="' + label + '" onselectitem="ctrl.onselectitem" ondeselectitem="ctrl.ondeselectitem" '
+                            + hideremoveicon + ' customvalidate="ctrl.customvalidate">' +
+                        '</vr-select>';
+        }
+    }]);
