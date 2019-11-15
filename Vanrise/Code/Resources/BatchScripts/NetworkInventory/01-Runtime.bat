002d2556ed22cc658fb1793f55b13730b6b57817
Wissam Ajamy <VANRISE\wissam.ajamy>
2019-11-15 16:41:15 +0000

    comment runtime publish
    git-tfs-id: [http://developmentvlan:8585/tfs/vanrise.collection]$/;C59256

--- Vanrise/Code/Resources/BatchScripts/NetworkInventory/01-Runtime.bat
+++ Vanrise/Code/Resources/BatchScripts/NetworkInventory/01-Runtime.bat
@@ -8,19 +8,19 @@ For /F %PARSEARG% %%i in (%CURRDATE%) Do SET YYYYMMDD=%%l%%j%%k
 
 ECHO.
 ECHO NetworkInventory Runtime
-xcopy "C:\TFS\Retail\Code\Retail.Runtime\bin\x64\Release" /y /v /z /i /Q /R						"\\192.168.110.185\Fixes\WebSite\NetworkInventory\Runtime\%YYYYMMDD%\" /EXCLUDE:C:\TFS\Vanrise\Code\Resources\BatchScripts\Common\RetailSource\runtime-list-of-excluded-files.txt
-xcopy "C:\TFS\Retail\Code\Retail.Runtime\App.config.NetworkInventory.exclude" /y /v /z /i /Q /R	"\\192.168.110.185\Fixes\WebSite\NetworkInventory\Runtime\%YYYYMMDD%\"
-rename																							"\\192.168.110.185\Fixes\WebSite\NetworkInventory\Runtime\%YYYYMMDD%\App.config.NetworkInventory.exclude" "NetworkInventory.Runtime.exe.config"
-rename																							"\\192.168.110.185\Fixes\WebSite\NetworkInventory\Runtime\%YYYYMMDD%\Retail.Runtime.exe" "NetworkInventory.Runtime.exe"
-xcopy "C:\TFS\Vanrise\Code\Resources\DLLs\Aspose.Cells.lic" /y /v /z /i /Q /R					"\\192.168.110.185\Fixes\WebSite\NetworkInventory\Runtime\%YYYYMMDD%\"
-del /s /q /f																					"\\192.168.110.185\Fixes\WebSite\NetworkInventory\Runtime\%YYYYMMDD%\Retail.*"
-xcopy "C:\TFS\Retail\Code\Retail.Runtime\bin\x64\Release\Retail.NIM*" /S /E /R /y /v /i /z /Q	"\\192.168.110.185\Fixes\WebSite\NetworkInventory\Runtime\%YYYYMMDD%\"
-del /s /q /f																					"\\192.168.110.185\Fixes\WebSite\NetworkInventory\Runtime\%YYYYMMDD%\*.pdb"
-del /s /q /f																					"\\192.168.110.185\Fixes\WebSite\NetworkInventory\Runtime\%YYYYMMDD%\Vanrise.Integration.*"
-del /s /q /f																					"\\192.168.110.185\Fixes\WebSite\NetworkInventory\Runtime\%YYYYMMDD%\Vanrise.Reprocess.*"
-del /s /q /f																					"\\192.168.110.185\Fixes\WebSite\NetworkInventory\Runtime\%YYYYMMDD%\Vanrise.AccountManager.*"
-del /s /q /f																					"\\192.168.110.185\Fixes\WebSite\NetworkInventory\Runtime\%YYYYMMDD%\Vanrise.AccountBalance.*"
-del /s /q /f																					"\\192.168.110.185\Fixes\WebSite\NetworkInventory\Runtime\%YYYYMMDD%\Vanrise.Invoice.*"
-del /s /q /f																					"\\192.168.110.185\Fixes\WebSite\NetworkInventory\Runtime\%YYYYMMDD%\Vanrise.InvToAccBalanceRelation.*"
-del /s /q /f																					"\\192.168.110.185\Fixes\WebSite\NetworkInventory\Runtime\%YYYYMMDD%\Vanrise.NumberingPlan.*"
+::xcopy "C:\TFS\Retail\Code\Retail.Runtime\bin\x64\Release" /y /v /z /i /Q /R						"\\192.168.110.185\Fixes\WebSite\NetworkInventory\Runtime\%YYYYMMDD%\" /EXCLUDE:C:\TFS\Vanrise\Code\Resources\BatchScripts\Common\RetailSource\runtime-list-of-excluded-files.txt
+::xcopy "C:\TFS\Retail\Code\Retail.Runtime\App.config.NetworkInventory.exclude" /y /v /z /i /Q /R	"\\192.168.110.185\Fixes\WebSite\NetworkInventory\Runtime\%YYYYMMDD%\"
+::rename																							"\\192.168.110.185\Fixes\WebSite\NetworkInventory\Runtime\%YYYYMMDD%\App.config.NetworkInventory.exclude" "NetworkInventory.Runtime.exe.config"
+::rename																							"\\192.168.110.185\Fixes\WebSite\NetworkInventory\Runtime\%YYYYMMDD%\Retail.Runtime.exe" "NetworkInventory.Runtime.exe"
+::xcopy "C:\TFS\Vanrise\Code\Resources\DLLs\Aspose.Cells.lic" /y /v /z /i /Q /R					"\\192.168.110.185\Fixes\WebSite\NetworkInventory\Runtime\%YYYYMMDD%\"
+::del /s /q /f																					"\\192.168.110.185\Fixes\WebSite\NetworkInventory\Runtime\%YYYYMMDD%\Retail.*"
+::xcopy "C:\TFS\Retail\Code\Retail.Runtime\bin\x64\Release\Retail.NIM*" /S /E /R /y /v /i /z /Q	"\\192.168.110.185\Fixes\WebSite\NetworkInventory\Runtime\%YYYYMMDD%\"
+::del /s /q /f																					"\\192.168.110.185\Fixes\WebSite\NetworkInventory\Runtime\%YYYYMMDD%\*.pdb"
+::del /s /q /f																					"\\192.168.110.185\Fixes\WebSite\NetworkInventory\Runtime\%YYYYMMDD%\Vanrise.Integration.*"
+::del /s /q /f																					"\\192.168.110.185\Fixes\WebSite\NetworkInventory\Runtime\%YYYYMMDD%\Vanrise.Reprocess.*"
+::del /s /q /f																					"\\192.168.110.185\Fixes\WebSite\NetworkInventory\Runtime\%YYYYMMDD%\Vanrise.AccountManager.*"
+::del /s /q /f																					"\\192.168.110.185\Fixes\WebSite\NetworkInventory\Runtime\%YYYYMMDD%\Vanrise.AccountBalance.*"
+::del /s /q /f																					"\\192.168.110.185\Fixes\WebSite\NetworkInventory\Runtime\%YYYYMMDD%\Vanrise.Invoice.*"
+::del /s /q /f																					"\\192.168.110.185\Fixes\WebSite\NetworkInventory\Runtime\%YYYYMMDD%\Vanrise.InvToAccBalanceRelation.*"
+::del /s /q /f																					"\\192.168.110.185\Fixes\WebSite\NetworkInventory\Runtime\%YYYYMMDD%\Vanrise.NumberingPlan.*"
 
