9db993540785b58b3d58de8fced0c08672b963cc
Samer Haidar <VANRISE\samer.haidar>
2019-11-20 16:21:59 +0000

    Adding title equal to name by default
    git-tfs-id: [http://developmentvlan:8585/tfs/vanrise.collection]$/;C59381

--- Vanrise/Code/Vanrise.Analytic.Entities/AnalyticReport/AnalyticReport.cs
+++ Vanrise/Code/Vanrise.Analytic.Entities/AnalyticReport/AnalyticReport.cs
@@ -9,12 +9,18 @@ namespace Vanrise.Analytic.Entities
     public enum AccessType { Public = 0, Private = 1 }
     public class AnalyticReport
     {
+        public AnalyticReport()
+        {
+            _title = Name;
+        }
         public Guid AnalyticReportId { get; set; }
         public Guid? DevProjectId { get; set; }
         public int UserID { get; set; }
         public AccessType AccessType { get; set; }
         public string Name { get; set; }
-        public string Title { get; set; }
+
+        string _title;
+        public string Title { get { return _title; } set { _title = value; } }
 
         public AnalyticReportSettings Settings { get; set; }
     }
