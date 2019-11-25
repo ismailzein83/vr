1880bcb800cbefbf710d02b8289d315b4434b551
Mohammad Sheaib <VANRISE\mohammad.sheaib>
2019-11-25 13:54:41 +0000

    Adding CompileGenericBEAction
    git-tfs-id: [http://developmentvlan:8585/tfs/vanrise.collection]$/;C59459

---
+++ Vanrise/Code/Vanrise.GenericData.MainExtensions/GenericBusinessEntity/GenericBEActions/CompileGenericBEAction.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using Vanrise.GenericData.Business;
+
+namespace Vanrise.GenericData.MainExtensions
+{
+    public class CompileGenericBEAction : GenericBEActionSettings
+    {
+        public override Guid ConfigId
+        {
+            get { return new Guid("637FD561-2DAA-4BC3-8DF6-C00245EF78EC"); }
+        }
+        public override string ActionTypeName { get { return "CompileGenericBEAction"; } }
+
+        public override string ActionKind { get { return "Compile"; } }
+
+    }
+}
