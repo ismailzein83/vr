75fdcf1e27c1f696ab9c1057da2764efed4c4e56
Mohamad Saab <VANRISE\mohamad.saab>
2019-11-25 09:30:05 +0000

    Adding AccountPartGenericEditor extension
    git-tfs-id: [http://developmentvlan:8585/tfs/vanrise.collection]$/;C59439

---
+++ Retail/Code/Retail.BusinessEntity.MainExtensions/AccountParts/AccountPartGenericEditor.cs
@@ -0,0 +1,21 @@
+﻿using Retail.BusinessEntity.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Retail.BusinessEntity.MainExtensions.AccountParts
+{
+    public class AccountPartGenericEditor : AccountPartSettings
+    {
+        public override Guid ConfigId
+        {
+            get
+            {
+                return Guid.NewGuid();
+            }
+        };
+        public dynamic DataRecord { get; set; }
+    }
+}
---
+++ Retail/Code/Retail.BusinessEntity.MainExtensions/AccountParts/AccountPartGenericEditorDefinition.cs
@@ -0,0 +1,25 @@
+﻿using Retail.BusinessEntity.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Vanrise.Common;
+using Vanrise.GenericData.Entities;
+
+namespace Retail.BusinessEntity.MainExtensions.AccountParts
+{
+    public class AccountPartGenericEditorDefinition : AccountPartDefinitionSettings
+    {
+        public override Guid ConfigId => new Guid("5B3C2BA0-2ACD-4013-AC24-68F36DDF241D");
+
+        public Guid DataRecordTypeId { get; set; }
+
+        public VRGenericEditorDefinitionSetting Settings { get; set; }
+        public override bool IsPartValid(IAccountPartDefinitionIsPartValidContext context)
+        {
+            AccountPartGenericEditor part = context.AccountPartSettings.CastWithValidate<AccountPartGenericEditor>("context.AccountPartSettings");
+            return true;
+        }
+    }
+}
