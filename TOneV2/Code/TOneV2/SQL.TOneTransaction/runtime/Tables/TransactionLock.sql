f60e10ddc01d9a412c8b729a65bceac28199d7c3
Ismail Zein <VANRISE\ismail.zein>
2019-11-26 10:45:36 +0000

    sql project
    git-tfs-id: [http://developmentvlan:8585/tfs/vanrise.collection]$/;C59471

--- TOneV2/Code/TOneV2/SQL.TOneTransaction/runtime/Tables/TransactionLock.sql
+++ TOneV2/Code/TOneV2/SQL.TOneTransaction/runtime/Tables/TransactionLock.sql
@@ -3,6 +3,9 @@
     [TransactionUniqueName] NVARCHAR (255)   NOT NULL,
     [ProcessID]             INT              NOT NULL,
     [CreatedTime]           DATETIME         CONSTRAINT [DF_TransactionLock_CreatedTime] DEFAULT (getdate()) NULL,
-    CONSTRAINT [PK_TransactionLock] PRIMARY KEY CLUSTERED ([ID] ASC)
+    CONSTRAINT [PK_TransactionLock] PRIMARY KEY CLUSTERED ([ID] ASC),
+    CONSTRAINT [IX_TransactionLock_TransactionUniqueName] UNIQUE NONCLUSTERED ([TransactionUniqueName] ASC)
 );
+
+
 
