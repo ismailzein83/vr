89f82602c2f8269f5024f831b9b020dcbedce661
Ali Ballouk <VANRISE\ali.ballouk>
2019-12-02 18:24:59 +0000

    adding new Named Personalization Items files
    git-tfs-id: [http://developmentvlan:8585/tfs/vanrise.collection]$/;C59608

---
+++ Vanrise/Code/Vanrise.Common.Web/Common/Directives/Personalization/NamedPersonalizationItemsEditorController.js
@@ -0,0 +1,105 @@
+﻿(function (appControllers) {
+
+    "use strict";
+
+    namedPersonalizationItemsEditorController.$inject = ["$scope", "UtilsService", "VRNotificationService", "VRNavigationService", "VRUIUtilsService", "VR_Common_EntityPersonalizationAPIService"];
+
+    function namedPersonalizationItemsEditorController($scope, UtilsService, VRNotificationService, VRNavigationService, VRUIUtilsService, VR_Common_EntityPersonalizationAPIService) {
+
+        var entityUniqueNames;
+        var personilizationItemName;
+        var selectedIsGlobal;
+        var sharedSettingsGridAPI;
+        var sharedSettingsGridReadyDeferred = UtilsService.createPromiseDeferred();
+
+        var mySettingsGridAPI;
+        var mySettingsGridReadyDeferred = UtilsService.createPromiseDeferred();
+
+        loadParameters();
+        defineScope();
+        load();
+
+        function loadParameters() {
+            var parameters = VRNavigationService.getParameters($scope);
+            if (parameters != undefined && parameters != null) {
+                entityUniqueNames = parameters.entityUniqueNames;
+                personilizationItemName = parameters.personilizationItemName;
+                selectedIsGlobal = parameters.selectedIsGlobal;
+            }
+        }
+        function defineScope() {
+
+            $scope.scopeModel = {};
+            $scope.scopeModel.onSharedSettingsGridReady = function (api) {
+                sharedSettingsGridAPI = api;
+                sharedSettingsGridReadyDeferred.resolve();
+            };
+
+            $scope.scopeModel.onMySettingsGridReady = function (api) {
+                mySettingsGridAPI = api;
+                mySettingsGridReadyDeferred.resolve();
+            };
+
+            $scope.scopeModel.onSettingsClicked = function (data) {
+                if ($scope.onSettingsClicked != undefined) {
+                    $scope.onSettingsClicked(data);
+                    $scope.modalContext.closeModal();
+                }
+            };
+
+            $scope.scopeModel.close = function () {
+                $scope.modalContext.closeModal();
+            };
+
+        }
+
+        function load() {
+
+            $scope.scopeModel.isLoading = true;
+            return UtilsService.waitMultipleAsyncOperations([loadSharedSettingsGrid, loadMySettingsGrid])
+            .catch(function (error) {
+                VRNotificationService.notifyExceptionWithClose(error, $scope);
+            })
+            .finally(function () {
+                $scope.scopeModel.isLoading = false;
+            });
+        }
+
+
+        function loadSharedSettingsGrid() {
+            var sharedSettingsGridLoadDeferred = UtilsService.createPromiseDeferred();
+            sharedSettingsGridReadyDeferred.promise.then(function () {
+                var payload = {
+                    query: {
+                        EntityUniqueNames: entityUniqueNames,
+                        IsGlobal: true
+                    },
+                    selectedName: personilizationItemName,
+                    selectedIsGlobal: selectedIsGlobal
+                };
+                VRUIUtilsService.callDirectiveLoad(sharedSettingsGridAPI, payload, sharedSettingsGridLoadDeferred);
+            });
+            return sharedSettingsGridLoadDeferred.promise;
+        }
+
+        function loadMySettingsGrid() {
+            var mySettingsGridLoadDeferred = UtilsService.createPromiseDeferred();
+            mySettingsGridReadyDeferred.promise.then(function () {
+                var payload = {
+                    query: {
+                        EntityUniqueNames: entityUniqueNames,
+                        IsGlobal: false
+                    },
+                    selectedName: personilizationItemName,
+                    selectedIsGlobal: selectedIsGlobal
+                };
+                VRUIUtilsService.callDirectiveLoad(mySettingsGridAPI, payload, mySettingsGridLoadDeferred);
+            });
+            return mySettingsGridLoadDeferred.promise;
+        }
+
+
+    }
+
+    appControllers.controller("VRCommon_NamedPersonalizationItemsEditorController", namedPersonalizationItemsEditorController);
+})(appControllers);
---
+++ Vanrise/Code/Vanrise.Common.Web/Common/Directives/Personalization/vr-common-namedpersonalizationitem-grid.js
@@ -0,0 +1,92 @@
+﻿"use strict";
+
+app.directive("vrCommonNamedpersonalizationitemGrid", ["UtilsService", "VRNotificationService", "VR_Common_EntityPersonalizationAPIService", "VRCommon_CountryService", "VRUIUtilsService", "VRCommon_ObjectTrackingService",
+function (UtilsService, VRNotificationService, VR_Common_EntityPersonalizationAPIService, VRCommon_CountryService, VRUIUtilsService, VRCommon_ObjectTrackingService) {
+
+    var directiveDefinitionObject = {
+
+        restrict: "E",
+        scope: {
+            onReady: "=",
+            onsettingsclicked: "="
+        },
+        controller: function ($scope, $element, $attrs) {
+            var ctrl = this;
+            var grid = new PersonalizationItemGrid($scope, ctrl, $attrs);
+            grid.initializeController();
+        },
+        controllerAs: "ctrl",
+        bindToController: true,
+        compile: function (element, attrs) {
+
+        },
+        templateUrl: "/Client/Modules/Common/Directives/Personalization/Templates/NamedPersonalizationItemGridTemplate.html"
+
+    };
+
+    function PersonalizationItemGrid($scope, ctrl, $attrs) {
+
+        ctrl.onSettingClick = function (dataItem) {
+            if (ctrl.onsettingsclicked != undefined && typeof (ctrl.onsettingsclicked) == "function") {
+                var data = {
+                    Name: dataItem.Name,
+                    NamedEntityPersonalizations: dataItem.NamedEntityPersonalizations,
+                    IsGlobal: dataItem.IsGlobal
+                };
+                return ctrl.onsettingsclicked(data);
+            }               
+        };
+
+        ctrl.markSelectedSettingRow = function (dataItem) {
+            if (dataItem.Name == selectedName && selectedIsGlobal == dataItem.IsGlobal) {
+                return { CssClass: 'alert-success' };
+            }
+        };
+        var gridAPI;
+        this.initializeController = initializeController;
+        var selectedName;
+        var selectedIsGlobal;
+        function initializeController() {
+           
+            $scope.personalizationItems = [];
+            $scope.onGridReady = function (api) {
+
+                gridAPI = api;
+
+                if (ctrl.onReady != undefined && typeof (ctrl.onReady) == "function")
+                    ctrl.onReady(getDirectiveAPI());
+
+                function getDirectiveAPI() {
+                   
+                    var directiveAPI = {};
+
+                    directiveAPI.load = function (payload) {
+                        var query;
+                        if (payload != undefined) {
+                            query = payload.query;
+                            selectedName = payload.selectedName;
+                            selectedIsGlobal = payload.selectedIsGlobal;
+                        }
+                        return gridAPI.retrieveData(query);
+
+                    };
+
+                    return directiveAPI;
+                }
+            };
+            $scope.dataRetrievalFunction = function (dataRetrievalInput, onResponseReady) {
+                return VR_Common_EntityPersonalizationAPIService.GetFilteredNamedEntityPersonalization(dataRetrievalInput)
+                    .then(function (response) {                          
+                        onResponseReady(response);
+                    })
+                    .catch(function (error) {
+                        VRNotificationService.notifyException(error, $scope);
+                    });
+            };            
+        }
+              
+    }
+
+    return directiveDefinitionObject;
+
+}]);
