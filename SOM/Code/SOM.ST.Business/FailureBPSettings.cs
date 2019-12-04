4b3a4339cce24f35cd2df417fe0edf986e1799bb
Fatima Hammoud <VANRISE\fatima.hammoud>
2019-12-04 15:26:12 +0000

    Change logic in FailureBPSettings for SOM
    git-tfs-id: [http://developmentvlan:8585/tfs/vanrise.collection]$/;C59685

--- SOM/Code/SOM.ST.Business/FailureBPSettings.cs
+++ SOM/Code/SOM.ST.Business/FailureBPSettings.cs
@@ -4,58 +4,44 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using Vanrise.BusinessProcess.Entities;
+using Vanrise.Common;
+using Vanrise.Common.Business;
 using Vanrise.Entities;
 
 namespace SOM.ST.Business
 {
     public class FailureBPSettings : Vanrise.BusinessProcess.Business.DefaultBPDefinitionExtendedSettings
     {
+        static Guid bpmConnection = new Guid("E9159CBF-974F-4F0B-9D14-BC2414736DD4"); 
         public override void OnBPExecutionCompleted(Vanrise.BusinessProcess.Entities.IBPDefinitionBPExecutionCompletedContext context)
         {
-            var commonInput = context.BPInstance.InputArgument as CommonInput;
+            context.ThrowIfNull("context");
+            context.BPInstance.ThrowIfNull("context.BPInstance");
+            context.BPInstance.InputArgument.ThrowIfNull("context.BPInstance.InputArgument");
+
+            dynamic inputDynamic = context.BPInstance.InputArgument;
+            var requestId = (string)inputDynamic.CommonInputArgument.RequestId;
+            requestId.ThrowIfNull("CommonInputArgument.RequestId");
 
             if (context.BPInstance.Status != BPInstanceStatus.Completed)
             {
                 var path = "/0/rest/StRequestManager/SetRequestFailed";
                 string body = null;
-
                 var urlParameters = new Dictionary<string, string>();
-                urlParameters.Add("requestId", commonInput.CommonInputArgument.RequestId);
+                urlParameters.Add("requestId", requestId);
 
+                var vrConnection = new VRConnectionManager().GetVRConnection<VRHttpConnection>(bpmConnection);
+                VRHttpConnection connectionSettings = vrConnection.Settings as VRHttpConnection;
                 var headers = new Dictionary<string, string>();
-                var connection = new Vanrise.Common.Business.VRHttpConnection()
-                {
-                    BaseURL = "http://192.168.25.9:7121",
-                    Headers = new List<Vanrise.Common.Business.VRHttpHeader>(),
-                    Interceptor = new SOM.ST.Business.BPMOnlineInterceptor
-                    {
-                        UserName = "Supervisor",
-                        Password = "Supervisor",
-                        AuthenticationServiceURI = "/ServiceModel/AuthService.svc/Login"
-                    }
-                };
-                connection.TrySendRequest(path, VRHttpMethod.Get, VRHttpMessageFormat.ApplicationJSON, urlParameters, headers, body, null, true, null);
+                connectionSettings.TrySendRequest(path, VRHttpMethod.Get, VRHttpMessageFormat.ApplicationJSON, urlParameters, headers, body, null, true, null);
             }
             base.OnBPExecutionCompleted(context);
         }
     }
 
-    public class CommonInput : BaseProcessInputArgument
-    {
-        public CommonInputArgument CommonInputArgument { get; set; }
-        public override string GetTitle()
-        {
-            return this.ProcessName;
-        }
-    }
-
     public class CommonInputArgument
     {
-        public string ContactId { get; set; }
-        public string AccountId { get; set; }
-        public string ContractId { get; set; }
-        public string CustomerId { get; set; }
         public string RequestId { get; set; }
-        public string SwitchId { get; set; }
     }
 }
+
