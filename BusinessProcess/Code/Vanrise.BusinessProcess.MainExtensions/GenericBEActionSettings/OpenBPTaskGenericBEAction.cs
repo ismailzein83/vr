76d86e9bba8f8ac11111fc24518ab00f50445c1c
Mohammad Sheaib <VANRISE\mohammad.sheaib>
2019-12-04 10:20:58 +0000

    Adding OpenBPTask Generic Action
    git-tfs-id: [http://developmentvlan:8585/tfs/vanrise.collection]$/;C59662

---
+++ BusinessProcess/Code/Vanrise.BusinessProcess.MainExtensions/GenericBEActionSettings/OpenBPTaskGenericBEAction.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Vanrise.GenericData.Business;
+
+namespace Vanrise.BusinessProcess.MainExtensions
+{
+    public class OpenBPTaskGenericBEAction : GenericBEActionSettings
+    {
+        public override Guid ConfigId
+        {
+            get { return new Guid("00D55F71-95F6-4C63-A823-F7F8E4CE1372"); }
+        }
+        public override string ActionTypeName { get { return "OpenBPTaskGenericBEAction"; } }
+
+        public override string ActionKind { get { return "OpenBPTask"; } }
+
+        public string TaskIdFieldName { get; set; }
+    }
+}
